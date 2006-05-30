aa70fb76895ca00077bdf032b3d57cf37b1743df
Brian Chan <brian.chan@liferay.com>
2006-05-30 19:33:18 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@584 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-ejb/src/com/liferay/portlet/mail/action/SendMessageAction.java
+++ portal-ejb/src/com/liferay/portlet/mail/action/SendMessageAction.java
@@ -32,7 +32,6 @@ import com.liferay.portal.util.WebKeys;
 import com.liferay.portlet.ActionResponseImpl;
 import com.liferay.portlet.mail.MailMessageException;
 import com.liferay.portlet.mail.model.MailReceipt;
-import com.liferay.portlet.mail.service.spring.MailReceiptLocalServiceUtil;
 import com.liferay.portlet.mail.util.MailUtil;
 import com.liferay.portlet.mail.util.multiaccount.MailAccount;
 import com.liferay.portlet.mail.util.multiaccount.MailAccounts;
--- sql/portal-data-release.sql
+++ sql/portal-data-release.sql
@@ -1,1 +1,1 @@
-insert into Release_ (releaseId, createDate, modifiedDate, buildNumber) values ('1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 2963);
+insert into Release_ (releaseId, createDate, modifiedDate, buildNumber) values ('1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 2964);
