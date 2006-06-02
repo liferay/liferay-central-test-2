ab87eee61602806ad731fa29068affb161530693
Ming-Gih Lam <minggih.lam@liferay.com>
2006-06-02 16:15:23 +0000

    Fix bug with multiple instances
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@644 05bdf26c-840f-0410-9ced-eb539d925f36

--- portlets/sample-json-portlet.war/click_image.js
+++ portlets/sample-json-portlet.war/click_image.js
@@ -1,18 +1,27 @@
 toString : function() {
 	var main = document.createElement("div");
 	var clickMe = document.createElement("span");
+	var id;
+
+	if (this.id) {
+		id = this.id
+	}
+	else {
+		date = new Date();
+		id = "" + date.getTime();
+	}
 	
 	clickMe.innerHTML = "Click me.";
-	clickMe.setAttribute("onclick", "document.getElementById('click_image').style.display = 'inline'; this.style.display = 'none'");
-	clickMe.id = "click_link";
+	clickMe.setAttribute("onclick", "document.getElementById('" + id + "image').style.display = 'inline'; this.style.display = 'none'");
+	clickMe.id = id + "link";
 	clickMe.style.cursor = "pointer";
 	
 	var image = document.createElement("img");
 
 	image.src = "http://www.liferay.com/image/company_logo?img_id=liferay.com";
-	image.setAttribute("onclick", "document.getElementById('click_link').style.display = 'inline'; this.style.display = 'none'");
+	image.setAttribute("onclick", "document.getElementById('" + id + "link').style.display = 'inline'; this.style.display = 'none'");
 	image.style.cursor = "pointer";
-	image.id = "click_image";
+	image.id = id + "image";
 	image.style.display = "none";
 	
 	main.appendChild(clickMe);
