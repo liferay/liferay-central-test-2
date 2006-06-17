c8bb0d73c4807a0ceb2912f42a2f2ffc30555781
rajesh kumar <mailtorajeshv@gmail.com>
2006-06-17 19:54:23 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@813 05bdf26c-840f-0410-9ced-eb539d925f36

--- sql/portal-data-release.sql
+++ sql/portal-data-release.sql
@@ -1,1 +1,1 @@
-insert into Release_ (releaseId, createDate, modifiedDate, buildNumber) values ('1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 3013);
+insert into Release_ (releaseId, createDate, modifiedDate, buildNumber) values ('1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 3014);
