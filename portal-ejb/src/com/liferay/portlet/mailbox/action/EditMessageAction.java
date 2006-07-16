2321f4409922202fd918f5852f6a3e74da9e4071
Alexander Chow <alexander.chow@liferay.com>
2006-07-16 22:30:24 +0000

    Fixed composition code.
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@1070 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-ejb/src/com/liferay/portlet/mailbox/action/EditMessageAction.java
+++ portal-ejb/src/com/liferay/portlet/mailbox/action/EditMessageAction.java
@@ -23,9 +23,11 @@
 package com.liferay.portlet.mailbox.action;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -37,6 +39,8 @@ import javax.portlet.PortletConfig;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -115,6 +119,7 @@ public class EditMessageAction extends PortletAction {
 			}
 		}
 		catch (Exception e) {
+			_log.error("Unable to send message");
 			//setForward(req, "portlet.mailbox.error");
 		}
 	}
@@ -143,42 +148,56 @@ public class EditMessageAction extends PortletAction {
 	private Address [] _getAddresses(String mailingList) throws Exception {
 		String [] entries = mailingList.split(",|;");
 		
-		Address [] addresses = new Address[entries.length];
+		List addresses = new ArrayList();
 		for (int i = 0; i < entries.length; i++) {
-			addresses[i] = _getAddress(entries[i]);
+			String entry = entries[i].trim();
+			if (Validator.isNotNull(entry)) {
+				addresses.add(_getAddress(entry));
+			}
 		}
 
-		return addresses;
+		return (Address [])addresses.toArray(new Address [] {});
 	}
 	
 	private InternetAddress _getAddress(String entry) throws Exception {
 		InternetAddress ia = new InternetAddress();
 		
-		entry = entry.trim();
+		String [] parts = entry.split(StringPool.LESS_THAN);
 		
-		if (entry.endsWith(StringPool.GREATER_THAN)) {
-			int iaBeg = entry.indexOf(StringPool.LESS_THAN);
-			if (iaBeg < 0) {
+		if (parts.length == 2) {
+			String name = parts[0].trim();
+
+			if (name.endsWith(StringPool.QUOTE) || 
+				name.endsWith(StringPool.APOSTROPHE)) {
+				name = name.substring(0, name.length() - 1);
+			}
+			
+			if (name.startsWith(StringPool.QUOTE) ||
+				name.startsWith(StringPool.APOSTROPHE)) {
+				name = name.substring(1);
+			}
+			
+			String address = parts[1].trim();
+
+			if (address.endsWith(StringPool.GREATER_THAN)) {
+				address = address.substring(0, address.length() - 1);
+			}
+
+			if (!Validator.isAddress(address)) {
 				// throw exception
 			}
-			else {
-				String address = 
-					entry.substring(iaBeg + 1, entry.length() - 2).trim();
-				String name = entry.substring(0, iaBeg - 1).trim();
-				
-				if (!Validator.isAddress(address)) {
-					// throw exception
-				}
 
-				ia.setAddress(address);
-				ia.setPersonal(name);
+			ia.setAddress(address.trim());
+			ia.setPersonal(name.trim());
+		}
+		else if (parts.length == 1) {
+			if (!Validator.isAddress(parts[0])) {
+				// throw exception
 			}
+			ia.setAddress(parts[0].trim());
 		}
 		else {
-			if (!Validator.isAddress(entry)) {
-				// throw exception
-			}
-			ia.setAddress(entry);
+			// throw exception
 		}
 		
 		return ia;
@@ -187,19 +206,19 @@ public class EditMessageAction extends PortletAction {
 	private Map _getAttachments(UploadPortletRequest uploadReq) throws Exception {
 		Map attachments = new HashMap();
 
-		String imagePrefix = "attachment_";
+		String prefix = "attachment";
 
 		Enumeration enu = uploadReq.getParameterNames();
 
 		while (enu.hasMoreElements()) {
 			String name = (String)enu.nextElement();
 
-			if (name.startsWith(imagePrefix)) {
+			if (name.startsWith(prefix)) {
 				File file = uploadReq.getFile(name);
 				byte[] bytes = FileUtil.getBytes(file);
 
 				if (bytes != null && bytes.length > 0) {
-					attachments.put(file.getName(), bytes);
+					attachments.put(uploadReq.getFileName(name), bytes);
 				}
 			}
 		}
@@ -221,4 +240,6 @@ public class EditMessageAction extends PortletAction {
 			getForward(req, "portlet.mailbox.edit_message"));
 	}
 
+	private static Log _log = LogFactory.getLog(EditMessageAction.class);
+	
 }
