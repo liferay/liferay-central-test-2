38979f2f16f6bd89c98e6b1993edb69a294eef9f
Brian Chan <brian.chan@liferay.com>
2006-08-17 01:07:35 +0000

    LEP-1108
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@1692 05bdf26c-840f-0410-9ced-eb539d925f36

--- sql/portal-data-release.sql
+++ sql/portal-data-release.sql
@@ -1,1 +1,1 @@
-insert into Release_ (releaseId, createDate, modifiedDate, buildNumber) values ('1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 3135);
+insert into Release_ (releaseId, createDate, modifiedDate, buildNumber) values ('1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 3136);
