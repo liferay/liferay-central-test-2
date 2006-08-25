1145124a25b0ddbe1b1daa6928babb807f7599b1
Brian Chan <brian.chan@liferay.com>
2006-08-25 01:11:24 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@1835 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-ejb/src/com/liferay/portal/util/ReleaseInfo.java
+++ portal-ejb/src/com/liferay/portal/util/ReleaseInfo.java
@@ -51,7 +51,7 @@ public class ReleaseInfo {
 
 	static String codeName = "Cowper";
 
-	static String build = "3153";
+	static String build = "3154";
 
 	static String date = "August 24, 2006";
 
--- sql/portal-data-release.sql
+++ sql/portal-data-release.sql
@@ -1,1 +1,1 @@
-insert into Release_ (releaseId, createDate, modifiedDate, buildNumber) values ('1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 3153);
+insert into Release_ (releaseId, createDate, modifiedDate, buildNumber) values ('1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 3154);
