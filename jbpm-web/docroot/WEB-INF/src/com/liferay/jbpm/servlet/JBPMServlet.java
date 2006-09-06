951c458c180b7372f628b9daf9543cf3f3587e35
Brian Chan <brian.chan@liferay.com>
2006-09-06 22:46:58 +0000

    LEP-1250
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@2017 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ jbpm-web/docroot/WEB-INF/src/com/liferay/jbpm/servlet/JBPMServlet.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-2006 Liferay, LLC. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.jbpm.servlet;
+
+import com.liferay.jbpm.WorkflowComponent;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.StackTraceUtil;
+
+import java.io.IOException;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * <a href="JBPMServlet.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author  Charles May
+ *
+ */
+public class JBPMServlet extends HttpServlet {
+
+	public void service(HttpServletRequest req, HttpServletResponse res)
+		throws IOException, ServletException {
+
+		WorkflowComponent workflowComponent = new WorkflowComponent();
+
+		String result = workflowComponent.process(req, res);
+
+		res.setContentType("text/xml");
+
+		ServletOutputStream out = res.getOutputStream();
+
+		try {
+			if (!res.isCommitted()) {
+				out.print(result);
+			}
+		}
+		catch (Exception e) {
+			_log.warn(StackTraceUtil.getStackTrace(e));
+		}
+		finally {
+			out.flush();
+			out.close();
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(JBPMServlet.class);
+
+}
---
+++ servicemix-web/docroot/WEB-INF/src/com/liferay/jbi/util/URLTransformComponent.java
@@ -0,0 +1,131 @@
+/**
+ * Copyright (c) 2000-2006 Liferay, LLC. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.jbi.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.net.URL;
+import java.net.URLConnection;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.jbi.messaging.MessageExchange;
+import javax.jbi.messaging.MessagingException;
+import javax.jbi.messaging.NormalizedMessage;
+
+import org.servicemix.components.util.TransformComponentSupport;
+import org.servicemix.jbi.jaxp.StringSource;
+
+/**
+ * <a href="URLTransformComponent.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author  Charles May
+ *
+ */
+public class URLTransformComponent extends TransformComponentSupport {
+
+	public String getUrl() {
+		return _url;
+	}
+
+	public void setUrl(String url) {
+		_url = url;
+	}
+
+	public String getUrlResult(Map parameterMap) throws MessagingException {
+		StringBuffer url = new StringBuffer();
+
+		url.append(_url);
+
+		if (_url.indexOf("?") == -1) {
+			url.append("?");
+		}
+
+		Iterator itr = parameterMap.entrySet().iterator();
+
+		while (itr.hasNext()) {
+			Map.Entry entry = (Map.Entry)itr.next();
+
+			String key = (String)entry.getKey();
+			String value = (String)entry.getValue();
+
+			url.append("&");
+			url.append(key);
+			url.append("=");
+			url.append(value);
+		}
+
+		StringBuffer result = new StringBuffer();
+
+		try {
+	        URL urlObj = new URL(url.toString());
+
+			URLConnection con = urlObj.openConnection();
+
+			BufferedReader br = new BufferedReader(
+				new InputStreamReader(con.getInputStream()));
+
+	        String s = null;
+
+			while ((s = br.readLine()) != null) {
+				result.append(s);
+			}
+
+	        br.close();
+		}
+		catch (IOException ioe) {
+			throw new MessagingException(ioe.getMessage());
+		}
+
+        return result.toString();
+	}
+
+	public boolean transform(
+			MessageExchange exchange, NormalizedMessage in,
+			NormalizedMessage out)
+		throws MessagingException {
+
+    	Map parameterMap = new HashMap();
+
+		Iterator itr = in.getPropertyNames().iterator();
+
+		while (itr.hasNext()) {
+			String key = (String)itr.next();
+
+			String value = (String)in.getProperty(key);
+
+			parameterMap.put(key, value);
+		}
+
+		out.setContent(new StringSource(getUrlResult(parameterMap)));
+
+		return true;
+	}
+
+	private String _url;
+
+}
