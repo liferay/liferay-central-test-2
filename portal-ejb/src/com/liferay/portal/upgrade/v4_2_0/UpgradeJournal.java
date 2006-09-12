0fa03c8a29e8163909c24d9a364a64166ed862a9
Brian Chan <brian.chan@liferay.com>
2006-09-12 03:30:57 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@2081 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-ejb/src/com/liferay/portal/upgrade/v4_2_0/UpgradeJournal.java
+++ portal-ejb/src/com/liferay/portal/upgrade/v4_2_0/UpgradeJournal.java
@@ -22,13 +22,9 @@
 
 package com.liferay.portal.upgrade.v4_2_0;
 
-import com.liferay.portal.model.Company;
-import com.liferay.portal.service.spring.CompanyLocalServiceUtil;
 import com.liferay.portal.upgrade.UpgradeException;
 import com.liferay.portal.upgrade.UpgradeProcess;
-import com.liferay.portlet.journal.service.spring.JournalContentSearchLocalServiceUtil;
-
-import java.util.Iterator;
+import com.liferay.portal.util.PropsUtil;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -53,14 +49,7 @@ public class UpgradeJournal extends UpgradeProcess {
 	}
 
 	private void _upgradeJournalContentSearch() throws Exception {
-		Iterator itr = CompanyLocalServiceUtil.getCompanies().iterator();
-
-		while (itr.hasNext()) {
-			Company company = (Company)itr.next();
-
-			JournalContentSearchLocalServiceUtil.checkContentSearches(
-				company.getCompanyId());
-		}
+		PropsUtil.set(PropsUtil.JOURNAL_SYNC_CONTENT_SEARCH_ON_STARTUP, "true");
 	}
 
 	private static Log _log = LogFactory.getLog(UpgradeJournal.class);
--- portal-ejb/src/com/liferay/portal/util/ReleaseInfo.java
+++ portal-ejb/src/com/liferay/portal/util/ReleaseInfo.java
@@ -51,7 +51,7 @@ public class ReleaseInfo {
 
 	static String codeName = "Cowper";
 
-	static String build = "3200";
+	static String build = "3201";
 
 	static String date = "September 11, 2006";
 
