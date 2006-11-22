1661deb75a22c6dc94f2d4a92a92ddd7237130bc
Brian Chan <brian.chan@liferay.com>
2006-11-22 21:23:18 +0000

    LEP-1666
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@3035 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-ejb/src/com/liferay/portal/model/BaseModel.java
+++ portal-ejb/src/com/liferay/portal/model/BaseModel.java
@@ -35,6 +35,12 @@ import java.io.Serializable;
  */
 public abstract class BaseModel implements Cloneable, Comparable, Serializable {
 
+	/**
+	 * @deprecated See LEP-1666.
+	 */	
+	public static boolean VALUE_OBJECT_CACHEABLE = GetterUtil.getBoolean(
+		PropsUtil.get(PropsUtil.VALUE_OBJECT_CACHEABLE));
+
 	public static boolean XSS_ALLOW = GetterUtil.getBoolean(
 		PropsUtil.get(PropsUtil.XSS_ALLOW));
 
