8aa249786ea27c956546c98669821be3731f521d
Brian Chan <brian.chan@liferay.com>
2006-12-12 23:53:19 +0000

    Build fixes
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@3281 05bdf26c-840f-0410-9ced-eb539d925f36

--- tools/ext_tmpl/servers/pramati/server/bin/setup.bat
+++
@@ -1,4 +0,0 @@
-@ ECHO Generated batch file to setup Environment Variables for Pramati Server 4.1
-set install_root=%cd%\..
-set PATH=%JAVA_HOME%\bin;%PATH%
-set CLASSPATH=%JAVA_HOME%\lib\tools.jar;%install_root%\lib\pramati\classpath.jar;
