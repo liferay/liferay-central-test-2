48f44c277b85bf46524c8ebb4dd25e5eaa7f0650
Alexander Chow <alexander.chow@liferay.com>
2006-12-19 01:36:00 +0000

    LEP-1832
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@3376 05bdf26c-840f-0410-9ced-eb539d925f36

--- sql/portal-data-company.sql
+++ sql/portal-data-company.sql
@@ -4,7 +4,6 @@
 
 insert into Company (companyId, portalURL, homeURL, mx) values ('liferay.com', 'localhost:8080', 'localhost:8080', 'liferay.com');
 insert into Account_ (accountId, companyId, userId, userName, createDate, modifiedDate, parentAccountId, name, legalName, legalId, legalType, sicCode, tickerSymbol, industry, type_, size_) values ('liferay.com', 'default', 'liferay.com.default', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '-1', 'Liferay', 'Liferay, LLC', '', '', '', '', '', '', '');
-insert into EmailAddress (emailAddressId, companyId, userId, className, classPK, address, typeId, primary_) values ('1', 'default', 'liferay.com.default', 'com.liferay.portal.model.Account', 'liferay.com', 'test@liferay.com', '10004', TRUE);
 
 ##
 ## Groups
@@ -109,7 +108,6 @@ insert into Role_ (roleId, companyId, name) values ('4', 'liferay.com', 'User');
 
 insert into User_ (userId, companyId, createDate, modifiedDate, password_, passwordEncrypted, passwordReset, emailAddress, greeting, loginDate, failedLoginAttempts, agreedToTermsOfUse, active_) values ('liferay.com.default', 'default', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'password', FALSE, FALSE, 'default@liferay.com', 'Welcome!', CURRENT_TIMESTAMP, 0, TRUE, TRUE);
 insert into Contact_ (contactId, companyId, userId, userName, createDate, modifiedDate, accountId, parentContactId, firstName, middleName, lastName, nickName, male, birthday) values ('liferay.com.default', 'default', 'liferay.com.default', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'default', '-1', '', '', '', '', TRUE, '01/01/1970');
-insert into EmailAddress (emailAddressId, companyId, userId, className, classPK, address, typeId, primary_) values ('2', 'default', 'liferay.com.default', 'com.liferay.portal.model.Contact', 'liferay.com.default', 'default@liferay.com', '11003', TRUE);
 
 ##
 ## User (test@liferay.com)
@@ -117,7 +115,6 @@ insert into EmailAddress (emailAddressId, companyId, userId, className, classPK,
 
 insert into User_ (userId, companyId, createDate, modifiedDate, password_, passwordEncrypted, passwordReset, emailAddress, greeting, loginDate, failedLoginAttempts, agreedToTermsOfUse, active_) values ('liferay.com.1', 'liferay.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'qUqP5cyxm6YcTAhz05Hph5gvu9M=', TRUE, FALSE, 'test@liferay.com', 'Welcome Joe Bloggs!', CURRENT_TIMESTAMP, 0, TRUE, TRUE);
 insert into Contact_ (contactId, companyId, userId, userName, createDate, modifiedDate, accountId, parentContactId, firstName, middleName, lastName, nickName, male, birthday) values ('liferay.com.1', 'default', 'liferay.com', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'liferay.com', '-1', 'Joe', '', 'Bloggs', 'Duke', TRUE, '01/01/1970');
-insert into EmailAddress (emailAddressId, companyId, userId, className, classPK, address, typeId, primary_) values ('3', 'liferay.com', 'liferay.com.1', 'com.liferay.portal.model.Contact', 'liferay.com.1', 'test@liferay.com', '11003', TRUE);
 
 insert into Group_ (groupId, companyId, className, classPK, parentGroupId, name, friendlyURL) values ('15', 'liferay.com', 'com.liferay.portal.model.User', 'liferay.com.1', '-1', '15', '/joebloggs');
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PRI.15', 'liferay.com', '15', '15', TRUE, 'classic', '01', 1.0);
@@ -141,7 +138,6 @@ insert into Users_Roles values ('liferay.com.1', '4');
 
 insert into User_ (userId, companyId, createDate, modifiedDate, password_, passwordEncrypted, passwordReset, emailAddress, greeting, loginDate, failedLoginAttempts, agreedToTermsOfUse, active_) values ('liferay.com.35', 'liferay.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'qUqP5cyxm6YcTAhz05Hph5gvu9M=', TRUE, TRUE, 'test.mail@liferay.com', 'Welcome Test Mail!', CURRENT_TIMESTAMP, 0, TRUE, TRUE);
 insert into Contact_ (contactId, companyId, userId, userName, createDate, modifiedDate, accountId, parentContactId, firstName, middleName, lastName, nickName, male, birthday) values ('liferay.com.35', 'default', 'liferay.com', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'liferay.com', '-1', 'Test', '', 'Mail', '', TRUE, '01/01/1970');
-insert into EmailAddress (emailAddressId, companyId, userId, className, classPK, address, typeId, primary_) values ('4', 'liferay.com', 'liferay.com.35', 'com.liferay.portal.model.Contact', 'liferay.com.35', 'test.mail@liferay.com', '11003', TRUE);
 
 insert into Group_ (groupId, companyId, className, classPK, parentGroupId, name, friendlyURL) values ('16', 'liferay.com', 'com.liferay.portal.model.User', 'liferay.com.35', '-1', '16', '');
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PRI.16', 'liferay.com', '16', '16', TRUE, 'classic', '01', 0.0);
