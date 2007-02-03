a712278fc1ff93c75e742e676bf20f83183b5966
Brian Chan <brian.chan@liferay.com>
2007-02-03 01:40:46 +0000

    Update copyright notice
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@4060 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-service/src/com/liferay/portal/model/ContactSoap.java
+++ portal-service/src/com/liferay/portal/model/ContactSoap.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000-2006 Liferay, Inc. All rights reserved.
+ * Copyright (c) 2000-2007 Liferay, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -31,7 +31,7 @@ import java.util.List;
 /**
  * <a href="ContactSoap.java.html"><b><i>View Source</i></b></a>
  *
- * @author  Brian Wing Shun Chan
+ * @author Brian Wing Shun Chan
  *
  */
 public class ContactSoap implements Serializable {
