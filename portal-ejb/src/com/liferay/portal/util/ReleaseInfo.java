c4e3bcdd33b0deb181f150f5bcbf63fb226417f2
Brian Chan <brian.chan@liferay.com>
2007-02-19 18:08:54 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@4397 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-ejb/src/com/liferay/portal/util/ReleaseInfo.java
+++ portal-ejb/src/com/liferay/portal/util/ReleaseInfo.java
@@ -51,9 +51,9 @@ public class ReleaseInfo {
 
 	static String codeName = "Machen";
 
-	static String build = "3717";
+	static String build = "3719";
 
-	static String date = "February 16, 2007";
+	static String date = "February 17, 2007";
 
 	static String releaseInfo =
 		name + " " + version + " (" + codeName + " / Build " + build + " / " +
