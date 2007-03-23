99611e1b6c2e6e0860cab814cf68a3f23ee80f47
Alexander Chow <alexander.chow@liferay.com>
2007-03-23 21:17:48 +0000

    LEP-2441
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@5151 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-ejb/src/com/liferay/portal/tools/util/DBUtil.java
@@ -0,0 +1,433 @@
+/**
+ * Copyright (c) 2000-2007 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portal.tools.util;
+
+import com.liferay.portal.kernel.util.StringMaker;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.velocity.VelocityUtil;
+import com.liferay.util.FileUtil;
+import com.liferay.util.StringUtil;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+/**
+ * <a href="DBUtil.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Alexander Chow
+ *
+ */
+public abstract class DBUtil {
+
+	public static final int DB_TYPE_DB2 = 1;
+	public static final int DB_TYPE_DERBY = 2;
+	public static final int DB_TYPE_FIREBIRD = 3;
+	public static final int DB_TYPE_INTERBASE = 4;
+	public static final int DB_TYPE_JDATASTORE = 5;
+	public static final int DB_TYPE_HYPERSONIC = 6;
+	public static final int DB_TYPE_MYSQL = 7;
+	public static final int DB_TYPE_ORACLE = 8;
+	public static final int DB_TYPE_POSTGRESQL = 9;
+	public static final int DB_TYPE_SAP = 10;
+	public static final int DB_TYPE_SQLSERVER = 11;
+	public static final int DB_TYPE_SYBASE = 12;
+
+	public static DBUtil getInstance(int dbType) {
+		DBUtil dbUtil = null;
+
+		if (dbType == DB_TYPE_DB2) {
+			dbUtil = DB2Util.getInstance();
+		}
+		else if (dbType == DB_TYPE_DERBY) {
+			dbUtil = DerbyUtil.getInstance();
+		}
+		else if (dbType == DB_TYPE_FIREBIRD) {
+			dbUtil = FirebirdUtil.getInstance();
+		}
+		else if (dbType == DB_TYPE_INTERBASE) {
+			dbUtil = InterBaseUtil.getInstance();
+		}
+		else if (dbType == DB_TYPE_JDATASTORE) {
+			dbUtil = JDataStoreUtil.getInstance();
+		}
+		else if (dbType == DB_TYPE_HYPERSONIC) {
+			dbUtil = HypersonicUtil.getInstance();
+		}
+		else if (dbType == DB_TYPE_MYSQL) {
+			dbUtil = MySQLUtil.getInstance();
+		}
+		else if (dbType == DB_TYPE_ORACLE) {
+			dbUtil = OracleUtil.getInstance();
+		}
+		else if (dbType == DB_TYPE_POSTGRESQL) {
+			dbUtil = PostgreSQLUtil.getInstance();
+		}
+		else if (dbType == DB_TYPE_SAP) {
+			dbUtil = SAPUtil.getInstance();
+		}
+		else if (dbType == DB_TYPE_SQLSERVER) {
+			dbUtil = SQLServerUtil.getInstance();
+		}
+		else if (dbType == DB_TYPE_SYBASE) {
+			dbUtil = SybaseUtil.getInstance();
+		}
+
+		return dbUtil;
+	}
+
+	public void buildCreateFile(String databaseName) throws IOException {
+		buildCreateFile(databaseName, true);
+		buildCreateFile(databaseName, false);
+	}
+
+	public abstract String buildSQL(String template) throws IOException;
+
+	public void buildSQLFile(String fileName) throws IOException {
+
+		// Note: any modifications here are overridden in the OracleUtil
+
+		String template = buildTemplate(fileName);
+
+		template = buildSQL(template);
+
+		FileUtil.write(
+			"../sql/" + fileName + "/" + fileName + "-" + getServerName() +
+				".sql",
+			template);
+	}
+
+	protected abstract void buildCreateFile(
+			String databaseName, boolean minimal)
+		throws IOException;
+
+	protected abstract String[] getTemplate();
+
+	protected abstract String getServerName();
+
+	protected abstract String reword(String data) throws IOException;
+
+	protected String buildTemplate(String fileName)
+		throws IOException {
+
+		File file = new File("../sql/" + fileName + ".sql");
+
+		String template = FileUtil.read(file);
+
+		if (fileName.equals("portal") || fileName.equals("portal-minimal") ||
+			fileName.equals("update-3.6.0-4.0.0")) {
+
+			BufferedReader br = new BufferedReader(new StringReader(template));
+
+			StringMaker sm = new StringMaker();
+
+			String line = null;
+
+			while ((line = br.readLine()) != null) {
+				if (line.startsWith("@include ")) {
+					int pos = line.indexOf(" ");
+
+					String includeFileName =
+						line.substring(pos + 1, line.length());
+
+					String include = FileUtil.read("../sql/" + includeFileName);
+
+					if (includeFileName.endsWith(".vm")) {
+						try {
+							include = _evaluateVM(include);
+						}
+						catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+
+					include = convertTimestamp(include);
+					include =
+						StringUtil.replace(include, TEMPLATE, getTemplate());
+
+					sm.append(include);
+					sm.append("\n\n");
+				}
+				else {
+					sm.append(line);
+					sm.append("\n");
+				}
+			}
+
+			br.close();
+
+			template = sm.toString();
+		}
+
+		if (fileName.equals("indexes") && (this instanceof SybaseUtil)) {
+			template = removeBooleanIndexes(template);
+		}
+
+		return template;
+	}
+
+	protected static String[] buildColumnNameTokens(String line) {
+		String[] words = StringUtil.split(line, " ");
+
+		if (words.length == 7) {
+			words[5] = "not null;";
+		}
+
+		String[] template = {
+			words[1], words[2], words[3], words[4], words[5]
+		};
+
+		return template;
+	}
+
+	protected static String[] buildColumnTypeTokens(String line) {
+		String[] words = StringUtil.split(line, " ");
+
+		String nullable = "";
+
+		if (words.length == 6) {
+			nullable = "not null;";
+		}
+		else if (words.length == 5) {
+			nullable = words[4];
+		}
+		else if (words.length == 4) {
+			nullable = "not null;";
+
+			if (words[3].endsWith(";")) {
+				words[3] = words[3].substring(0, words[3].length() - 1);
+			}
+		}
+
+		String[] template = {
+			words[1], words[2], "", words[3], nullable
+		};
+
+		return template;
+	}
+
+	protected String convertTimestamp(String data) {
+		String s = null;
+
+		if (this instanceof MySQLUtil) {
+			s = StringUtil.replace(data, "SPECIFIC_TIMESTAMP_", "");
+		}
+		else {
+			s = data.replaceAll(
+				"SPECIFIC_TIMESTAMP_" + "\\d+", "CURRENT_TIMESTAMP");
+		}
+
+		return s;
+	}
+
+	protected String getMinimalSuffix(boolean minimal) {
+		if (minimal) {
+			return "-minimal";
+		}
+		else {
+			return StringPool.BLANK;
+		}
+	}
+
+	protected String readSQL(String fileName, String comments, String eol)
+		throws IOException {
+
+		BufferedReader br = new BufferedReader(
+			new FileReader(new File(fileName)));
+
+		StringMaker sm = new StringMaker();
+
+		String line = null;
+
+		while ((line = br.readLine()) != null) {
+			if (!line.startsWith(comments)) {
+				line = StringUtil.replace(
+					line,
+					new String[] {"\n", "\t"},
+					new String[] {"", ""});
+
+				if (line.endsWith(";")) {
+					sm.append(line.substring(0, line.length() - 1));
+					sm.append(eol);
+				}
+				else {
+					sm.append(line);
+				}
+			}
+		}
+
+		br.close();
+
+		return sm.toString();
+	}
+
+	protected String removeBooleanIndexes(String data) throws IOException {
+		String portalData = FileUtil.read("../sql/portal-tables.sql");
+
+		BufferedReader br = new BufferedReader(new StringReader(data));
+
+		StringMaker sm = new StringMaker();
+
+		String line = null;
+
+		while ((line = br.readLine()) != null) {
+			boolean append = true;
+
+			int x = line.indexOf(" on ");
+
+			if (x != -1) {
+				int y = line.indexOf(" (", x);
+
+				String table = line.substring(x + 4, y);
+
+				x = y + 2;
+				y = line.indexOf(")", x);
+
+				String[] columns = StringUtil.split(line.substring(x, y));
+
+				x = portalData.indexOf("create table " + table + " (");
+				y = portalData.indexOf(");", x);
+
+				String portalTableData = portalData.substring(x, y);
+
+				for (int i = 0; i < columns.length; i++) {
+					if (portalTableData.indexOf(
+							columns[i].trim() + " BOOLEAN") != -1) {
+
+						append = false;
+
+						break;
+					}
+				}
+			}
+
+			if (append) {
+				sm.append(line);
+				sm.append("\n");
+			}
+		}
+
+		br.close();
+
+		return sm.toString();
+	}
+
+	protected String removeInserts(String data) throws IOException {
+		BufferedReader br = new BufferedReader(new StringReader(data));
+
+		StringMaker sm = new StringMaker();
+
+		String line = null;
+
+		while ((line = br.readLine()) != null) {
+			if (!line.startsWith("insert into ") &&
+				!line.startsWith("update ")) {
+
+				sm.append(line);
+				sm.append("\n");
+			}
+		}
+
+		br.close();
+
+		return sm.toString();
+	}
+
+	protected String removeLongInserts(String data) throws IOException {
+		BufferedReader br = new BufferedReader(new StringReader(data));
+
+		StringMaker sm = new StringMaker();
+
+		String line = null;
+
+		while ((line = br.readLine()) != null) {
+			if (!line.startsWith("insert into Image") &&
+				!line.startsWith("insert into JournalArticle") &&
+				!line.startsWith("insert into JournalStructure") &&
+				!line.startsWith("insert into JournalTemplate") &&
+				!line.startsWith("insert into ShoppingItem")) {
+
+				sm.append(line);
+				sm.append("\n");
+			}
+		}
+
+		br.close();
+
+		return sm.toString();
+	}
+
+	protected String removeNull(String content) {
+		content = StringUtil.replace(content, " not null", " not_null");
+		content = StringUtil.replace(content, " null", "");
+		content = StringUtil.replace(content, " not_null", " not null");
+
+		return content;
+	}
+
+	private String _evaluateVM(String template) throws Exception {
+		template = VelocityUtil.evaluate(template);
+
+		// Trim insert statements because it breaks MySQL Query Browser
+
+		BufferedReader br = new BufferedReader(new StringReader(template));
+
+		StringMaker sm = new StringMaker();
+
+		String line = null;
+
+		while ((line = br.readLine()) != null) {
+			line = line.trim();
+
+			sm.append(line);
+			sm.append("\n");
+		}
+
+		br.close();
+
+		template = sm.toString();
+		template = StringUtil.replace(template, "\n\n\n", "\n\n");
+
+		return template;
+	}
+
+	protected static String ALTER_COLUMN_TYPE = "alter_column_type ";
+
+	protected  static String ALTER_COLUMN_NAME = "alter_column_name ";
+
+	protected static String[] REWORD_TEMPLATE = {
+		"@table@", "@old-column@", "@new-column@", "@type@", "@nullable@"
+	};
+
+	protected static String[] TEMPLATE = {
+		"##", "TRUE", "FALSE",
+		"'01/01/1970'", "CURRENT_TIMESTAMP",
+		" BOOLEAN", " DATE", " DOUBLE",
+		" INTEGER", " LONG",
+		" STRING", " TEXT", " VARCHAR",
+		" IDENTITY", "COMMIT_TRANSACTION"
+	};
+
+}
