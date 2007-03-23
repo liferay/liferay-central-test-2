665517ef521740c92fe9abbf408f208f565dcad8
Brian Chan <brian.chan@liferay.com>
2007-03-23 00:56:28 +0000

    LEP-2433
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@5130 05bdf26c-840f-0410-9ced-eb539d925f36

--- sql/portal-data-company.sql
+++ sql/portal-data-company.sql
@@ -22,67 +22,67 @@ insert into Layout (layoutId, ownerId, companyId, parentLayoutId, name, type_, t
 ## Organizations
 ##
 
-insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('1', 'liferay.com', '-1', 'Liferay USA', FALSE, '5', '19', 1400, '');
+insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('1', 'liferay.com', '-1', 'Liferay USA', FALSE, '5', '19', 12017, '');
 
 insert into Group_ (groupId, companyId, className, classPK, parentGroupId, liveGroupId, name, friendlyURL, active_) values (4, 'liferay.com', 'com.liferay.portal.model.Organization', '1', -1, -1, '4', '', TRUE);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PRI.4', 'liferay.com', 4, '4', TRUE, 'classic', '01', 1.0);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PUB.4', 'liferay.com', 4, '4', FALSE, 'classic', '01', 0.0);
 
-insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('2', 'liferay.com', '1', 'Liferay Los Angeles', FALSE, '5', '19', 1400, '');
+insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('2', 'liferay.com', '1', 'Liferay Los Angeles', FALSE, '5', '19', 12017, '');
 
 insert into Group_ (groupId, companyId, className, classPK, parentGroupId, liveGroupId, name, friendlyURL, active_) values (5, 'liferay.com', 'com.liferay.portal.model.Organization', '2', -1, -1, '5', '', TRUE);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PRI.5', 'liferay.com', 5, '5', TRUE, 'classic', '01', 1.0);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PUB.5', 'liferay.com', 5, '5', FALSE, 'classic', '01', 0.0);
 
-insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('3', 'liferay.com', '1', 'Liferay San Francisco', FALSE, '5', '19', 1400, '');
+insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('3', 'liferay.com', '1', 'Liferay San Francisco', FALSE, '5', '19', 12017, '');
 
 insert into Group_ (groupId, companyId, className, classPK, parentGroupId, liveGroupId, name, friendlyURL, active_) values (6, 'liferay.com', 'com.liferay.portal.model.Organization', '3', -1, -1, '6', '', TRUE);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PRI.6', 'liferay.com', 6, '6', TRUE, 'classic', '01', 1.0);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PUB.6', 'liferay.com', 6, '6', FALSE, 'classic', '01', 0.0);
 
-insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('4', 'liferay.com', '1', 'Liferay Chicago', FALSE, '14', '19', 1400, '');
+insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('4', 'liferay.com', '1', 'Liferay Chicago', FALSE, '14', '19', 12017, '');
 
 insert into Group_ (groupId, companyId, className, classPK, parentGroupId, liveGroupId, name, friendlyURL, active_) values (7, 'liferay.com', 'com.liferay.portal.model.Organization', '4', -1, -1, '7', '', TRUE);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PRI.7', 'liferay.com', 7, '7', TRUE, 'classic', '01', 1.0);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PUB.7', 'liferay.com', 7, '7', FALSE, 'classic', '01', 0.0);
 
-insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('5', 'liferay.com', '1', 'Liferay New York', FALSE, '33', '19', 1400, '');
+insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('5', 'liferay.com', '1', 'Liferay New York', FALSE, '33', '19', 12017, '');
 
 insert into Group_ (groupId, companyId, className, classPK, parentGroupId, liveGroupId, name, friendlyURL, active_) values (8, 'liferay.com', 'com.liferay.portal.model.Organization', '5', -1, -1, '8', '', TRUE);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PRI.8', 'liferay.com', 8, '8', TRUE, 'classic', '01', 1.0);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PUB.8', 'liferay.com', 8, '8', FALSE, 'classic', '01', 0.0);
 
-insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('6', 'liferay.com', '-1', 'Liferay Europe', FALSE, '', '15', 1400, '');
+insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('6', 'liferay.com', '-1', 'Liferay Europe', FALSE, '', '15', 12017, '');
 
 insert into Group_ (groupId, companyId, className, classPK, parentGroupId, liveGroupId, name, friendlyURL, active_) values (9, 'liferay.com', 'com.liferay.portal.model.Organization', '6', -1, -1, '9', '', TRUE);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PRI.9', 'liferay.com', 9, '9', TRUE, 'classic', '01', 1.0);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PUB.9', 'liferay.com', 9, '9', FALSE, 'classic', '01', 0.0);
 
-insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('7', 'liferay.com', '6', 'Liferay London', FALSE, '', '18', 1400, '');
+insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('7', 'liferay.com', '6', 'Liferay London', FALSE, '', '18', 12017, '');
 
 insert into Group_ (groupId, companyId, className, classPK, parentGroupId, liveGroupId, name, friendlyURL, active_) values (10, 'liferay.com', 'com.liferay.portal.model.Organization', '7', -1, -1, '10', '', TRUE);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PRI.10', 'liferay.com', 10, '10', TRUE, 'classic', '01', 1.0);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PUB.10', 'liferay.com', 10, '10', FALSE, 'classic', '01', 0.0);
 
-insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('8', 'liferay.com', '6', 'Liferay Madrid', FALSE, '', '15', 1400, '');
+insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('8', 'liferay.com', '6', 'Liferay Madrid', FALSE, '', '15', 12017, '');
 
 insert into Group_ (groupId, companyId, className, classPK, parentGroupId, liveGroupId, name, friendlyURL, active_) values (11, 'liferay.com', 'com.liferay.portal.model.Organization', '8', -1, -1, '11', '', TRUE);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PRI.11', 'liferay.com', 11, '11', TRUE, 'classic', '01', 1.0);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PUB.11', 'liferay.com', 11, '11', FALSE, 'classic', '01', 0.0);
 
-insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('9', 'liferay.com', '-1', 'Liferay Asia', FALSE, '', '2', 1400, '');
+insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('9', 'liferay.com', '-1', 'Liferay Asia', FALSE, '', '2', 12017, '');
 
 insert into Group_ (groupId, companyId, className, classPK, parentGroupId, liveGroupId, name, friendlyURL, active_) values (12, 'liferay.com', 'com.liferay.portal.model.Organization', '9', -1, -1, '12', '', TRUE);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PRI.12', 'liferay.com', 12, '12', TRUE, 'classic', '01', 1.0);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PUB.12', 'liferay.com', 12, '12', FALSE, 'classic', '01', 0.0);
 
-insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('10', 'liferay.com', '9', 'Liferay Hong Kong', FALSE, '', '2', 1400, '');
+insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('10', 'liferay.com', '9', 'Liferay Hong Kong', FALSE, '', '2', 12017, '');
 
 insert into Group_ (groupId, companyId, className, classPK, parentGroupId, liveGroupId, name, friendlyURL, active_) values (13, 'liferay.com', 'com.liferay.portal.model.Organization', '10', -1, -1, '13', '', TRUE);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PRI.13', 'liferay.com', 13, '13', TRUE, 'classic', '01', 1.0);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PUB.13', 'liferay.com', 13, '13', FALSE, 'classic', '01', 0.0);
 
-insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('11', 'liferay.com', '9', 'Liferay Shanghai', FALSE, '', '2', 1400, '');
+insert into Organization_ (organizationId, companyId, parentOrganizationId, name, recursable, regionId, countryId, statusId, comments) values ('11', 'liferay.com', '9', 'Liferay Shanghai', FALSE, '', '2', 12017, '');
 
 insert into Group_ (groupId, companyId, className, classPK, parentGroupId, liveGroupId, name, friendlyURL, active_) values (14, 'liferay.com', 'com.liferay.portal.model.Organization', '11', -1, -1, '14', '', TRUE);
 insert into LayoutSet (ownerId, companyId, groupId, userId, privateLayout, themeId, colorSchemeId, pageCount) values ('PRI.14', 'liferay.com', 14, '14', TRUE, 'classic', '01', 1.0);
