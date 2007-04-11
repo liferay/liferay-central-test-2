d34f8e1ce187959c98d8ab04995ef91007a86230
Brian Chan <brian.chan@liferay.com>
2007-04-11 04:39:05 +0000

    Better error message
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@5481 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-ejb/src/com/liferay/portal/service/impl/PortletLocalServiceImpl.java
+++ portal-ejb/src/com/liferay/portal/service/impl/PortletLocalServiceImpl.java
@@ -116,7 +116,8 @@ public class PortletLocalServiceImpl extends PortletLocalServiceBaseImpl {
 		portletId = PortalUtil.getJsSafePortletName(portletId);
 
 		if (companyId.equals(UserImpl.DEFAULT)) {
-			throw new SystemException();
+			throw new SystemException(
+				"Company id should never be " + UserImpl.DEFAULT);
 		}
 
 		Portlet portlet = null;
