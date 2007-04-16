1a8ae32b53fde3fbc39e7d5780e6e1225e7c8a49
Brian Chan <brian.chan@liferay.com>
2007-04-16 16:21:58 +0000

    LEP-2252
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@5576 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-ejb/src/com/liferay/portlet/wiki/util/Indexer.java
+++ portal-ejb/src/com/liferay/portlet/wiki/util/Indexer.java
@@ -110,42 +110,40 @@ public class Indexer implements com.liferay.portal.kernel.search.Indexer {
 	public static void deletePages(String companyId, String nodeId)
 		throws IOException, ParseException {
 
-		synchronized (IndexWriter.class) {
-			BooleanQuery booleanQuery = new BooleanQuery();
+		BooleanQuery booleanQuery = new BooleanQuery();
 
-			LuceneUtil.addRequiredTerm(
-				booleanQuery, LuceneFields.PORTLET_ID, PORTLET_ID);
+		LuceneUtil.addRequiredTerm(
+			booleanQuery, LuceneFields.PORTLET_ID, PORTLET_ID);
 
-			LuceneUtil.addRequiredTerm(booleanQuery, "nodeId", nodeId);
+		LuceneUtil.addRequiredTerm(booleanQuery, "nodeId", nodeId);
 
-			Searcher searcher = LuceneUtil.getSearcher(companyId);
+		Searcher searcher = LuceneUtil.getSearcher(companyId);
 
-			Hits hits = searcher.search(booleanQuery);
+		Hits hits = searcher.search(booleanQuery);
 
-			if (hits.length() > 0) {
-				IndexReader reader = null;
+		if (hits.length() > 0) {
+			IndexReader reader = null;
 
-				try {
-					LuceneUtil.acquireLock(companyId);
+			try {
+				LuceneUtil.acquireLock(companyId);
 
-					reader = LuceneUtil.getReader(companyId);
+				reader = LuceneUtil.getReader(companyId);
 
-					for (int i = 0; i < hits.length(); i++) {
-						Document doc = hits.doc(i);
+				for (int i = 0; i < hits.length(); i++) {
+					Document doc = hits.doc(i);
 
-						Field field = doc.getField(LuceneFields.UID);
+					Field field = doc.getField(LuceneFields.UID);
 
-						reader.deleteDocuments(
-							new Term(LuceneFields.UID, field.stringValue()));
-					}
+					reader.deleteDocuments(
+						new Term(LuceneFields.UID, field.stringValue()));
 				}
-				finally {
-					if (reader != null) {
-						reader.close();
-					}
-
-					LuceneUtil.releaseLock(companyId);
+			}
+			finally {
+				if (reader != null) {
+					reader.close();
 				}
+
+				LuceneUtil.releaseLock(companyId);
 			}
 		}
 	}
