196dd02ba5dd0231139593d6a60cea72668e310c
Brian Chan <brian.chan@liferay.com>
2007-04-23 23:36:07 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@5768 05bdf26c-840f-0410-9ced-eb539d925f36

--- sql/portal-data-release.sql
+++ sql/portal-data-release.sql
@@ -1,1 +1,1 @@
-insert into Release_ (releaseId, createDate, modifiedDate, buildNumber, verified) values ('1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 3961, TRUE);
+insert into Release_ (releaseId, createDate, modifiedDate, buildNumber, verified) values ('1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 3962, TRUE);
