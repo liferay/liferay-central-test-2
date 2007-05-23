675d79f127bbfc65ab5403d5342ce4ecb29a03cb
Brian Chan <brian.chan@liferay.com>
2007-05-23 04:00:32 +0000

    Ant build script fixes
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@6421 05bdf26c-840f-0410-9ced-eb539d925f36

--- tools/ext_tmpl/servers/orion/bin/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ ! $JAVA_HOME ]
-then
-	echo JAVA_HOME not defined.
-	exit
-fi
-
-$JAVA_HOME/bin/java -Xmx512m -Dfile.encoding=UTF-8 -Duser.timezone=GMT -jar ../orion/orion.jar -config ../config/server.xml -userThreads
