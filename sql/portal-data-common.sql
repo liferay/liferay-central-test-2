c9bc758ccdf2e92cf6c6b0f5cc238bbe190840fe
Raymond Auge <raymond.auge@liferay.com>
2007-05-25 16:37:29 +0000

    LEP-2815
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@6488 05bdf26c-840f-0410-9ced-eb539d925f36

--- sql/portal-data-common.sql
+++ sql/portal-data-common.sql
@@ -279,6 +279,19 @@ insert into Region (regionId, countryId, regionCode, name, active_) values (49, 
 insert into Region (regionId, countryId, regionCode, name, active_) values (50, '19', 'WV', 'West Virginia', TRUE);
 insert into Region (regionId, countryId, regionCode, name, active_) values (51, '19', 'WI', 'Wisconsin', TRUE);
 insert into Region (regionId, countryId, regionCode, name, active_) values (52, '19', 'WY', 'Wyoming', TRUE);
+insert into Region (regionId, countryId, regionCode, name, active_) values (53, '1', 'AL', 'Alberta', TRUE);
+insert into Region (regionId, countryId, regionCode, name, active_) values (54, '1', 'BC', 'British Columbia', TRUE);
+insert into Region (regionId, countryId, regionCode, name, active_) values (55, '1', 'MB', 'Manitoba', TRUE);
+insert into Region (regionId, countryId, regionCode, name, active_) values (56, '1', 'NB', 'New Brunswick', TRUE);
+insert into Region (regionId, countryId, regionCode, name, active_) values (57, '1', 'NL', 'Newfoundland and Labrador', TRUE);
+insert into Region (regionId, countryId, regionCode, name, active_) values (58, '1', 'NT', 'Northwest Territories', TRUE);
+insert into Region (regionId, countryId, regionCode, name, active_) values (59, '1', 'NS', 'Nova Scotia', TRUE);
+insert into Region (regionId, countryId, regionCode, name, active_) values (60, '1', 'NU', 'Nunavut', TRUE);
+insert into Region (regionId, countryId, regionCode, name, active_) values (61, '1', 'ON', 'Ontario', TRUE);
+insert into Region (regionId, countryId, regionCode, name, active_) values (62, '1', 'PE', 'Prince Edward Island', TRUE);
+insert into Region (regionId, countryId, regionCode, name, active_) values (63, '1', 'QC', 'Quebec', TRUE);
+insert into Region (regionId, countryId, regionCode, name, active_) values (64, '1', 'SK', 'Saskatchewan', TRUE);
+insert into Region (regionId, countryId, regionCode, name, active_) values (65, '1', 'YT', 'Yukon', TRUE);
 
 ##
 ## List types for accounts
