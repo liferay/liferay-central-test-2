fb66be5b710c6bdb6d2502621971861b3ff8205b
Brian Chan <brian.chan@liferay.com>
2007-06-07 01:04:41 +0000

    Javascript fixes for edit look and feel
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@6748 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-impl/src/com/liferay/portlet/portletconfiguration/action/UpdateLookAndFeelAction.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2000-2007 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portlet.portletconfiguration.action;
+
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.model.Layout;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.service.permission.PortletPermission;
+import com.liferay.portal.struts.JSONAction;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.WebKeys;
+import com.liferay.portlet.CachePortlet;
+import com.liferay.portlet.PortletPreferencesFactory;
+import com.liferay.util.ParamUtil;
+
+import javax.portlet.PortletPreferences;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ * <a href="UpdateLookAndFeelAction.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Ming-Gih Lam
+ *
+ */
+public class UpdateLookAndFeelAction extends JSONAction {
+
+	public String getJSON(
+			ActionMapping mapping, ActionForm form, HttpServletRequest req,
+			HttpServletResponse res)
+		throws Exception {
+
+		HttpSession ses = req.getSession();
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)req.getAttribute(WebKeys.THEME_DISPLAY);
+
+		Layout layout = themeDisplay.getLayout();
+
+		PermissionChecker permissionChecker =
+			themeDisplay.getPermissionChecker();
+
+		String portletId = ParamUtil.getString(req, "portletId");
+
+		if (!PortletPermission.contains(
+				permissionChecker, themeDisplay.getPlid(), portletId,
+				ActionKeys.CONFIGURATION)) {
+
+			return null;
+		}
+
+		//String languageId = LanguageUtil.getLanguageId(req);
+		//String title = ParamUtil.getString(req, "title");
+		String css = ParamUtil.getString(req, "css");
+System.out.println("## css " + css);
+		PortletPreferences portletSetup =
+			PortletPreferencesFactory.getPortletSetup(layout, portletId);
+
+		//portletSetup.setValue("portlet-setup-title-" + languageId, title);
+		//portletSetup.setValue("portlet-setup-use-custom-title", "true");
+		portletSetup.setValue("portlet-setup-css", css);
+
+		portletSetup.store();
+
+		CachePortlet.clearResponse(ses, layout.getPrimaryKey(), portletId);
+
+		return null;
+	}
+
+}
