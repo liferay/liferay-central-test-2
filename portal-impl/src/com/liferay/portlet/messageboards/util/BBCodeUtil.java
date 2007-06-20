a109ae00774307af797ae050b399439642471087
Bruno Farache <bruno.farache@liferay.com>
2007-06-20 18:34:44 +0000

    LEP-2715
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@7053 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/messageboards/util/BBCodeUtil.java
+++ portal-impl/src/com/liferay/portlet/messageboards/util/BBCodeUtil.java
@@ -102,8 +102,10 @@ public class BBCodeUtil {
 	};
 
 	public static String getHTML(String bbcode) {
-		String html = StringUtil.replace(bbcode, _BBCODE_TAGS, _HTML_TAGS);
-
+		String html = Html.escape(bbcode, false);
+		
+		html = StringUtil.replace(html, _BBCODE_TAGS, _HTML_TAGS);
+		
 		for (int i = 0; i < EMOTICONS.length; i++) {
 			String imgTag = "<img src='" + EMOTICONS[i][0] + "' />";
 
