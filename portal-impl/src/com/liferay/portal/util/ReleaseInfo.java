4364434d5ab7a8d760d317e71550350e80e87e9d
Brian Chan <brian.chan@liferay.com>
2007-07-04 23:37:58 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@7512 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/util/ReleaseInfo.java
+++ portal-impl/src/com/liferay/portal/util/ReleaseInfo.java
@@ -42,9 +42,9 @@ public class ReleaseInfo {
 
 	static String codeName = "Owen";
 
-	static String build = "4313";
+	static String build = "4314";
 
-	static String date = "June 29, 2007";
+	static String date = "July 4, 2007";
 
 	static String releaseInfo =
 		name + " " + version + " (" + codeName + " / Build " + build + " / " +
