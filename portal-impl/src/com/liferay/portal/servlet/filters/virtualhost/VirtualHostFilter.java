b2d5233b603db8c29411db556192dd241a845a3a
Brian Chan <brian.chan@liferay.com>
2007-07-12 21:22:27 +0000

    LEP-3248
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@7702 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/servlet/filters/virtualhost/VirtualHostFilter.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/virtualhost/VirtualHostFilter.java
@@ -23,6 +23,7 @@
 package com.liferay.portal.servlet.filters.virtualhost;
 
 import com.liferay.portal.LayoutFriendlyURLException;
+import com.liferay.portal.NoSuchLayoutException;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.model.LayoutSet;
 import com.liferay.portal.model.impl.LayoutImpl;
@@ -50,6 +51,14 @@ import org.apache.commons.logging.LogFactory;
 
 /**
  * <a href="VirtualHostFilter.java.html"><b><i>View Source</i></b></a>
+ *
+ * <p>
+ * This filter is used to provide virtual host functionality. However, this
+ * filter is still required even if you do not use virtual hosting because this
+ * filter sets the company id in the request so that subsequent calls in the
+ * thread have the company id properly set. This filter must also always be the
+ * first filter in the list of filters.
+ * </p>
  *
  * @author Joel Kozikowski
  * @author Brian Wing Shun Chan
@@ -79,14 +88,31 @@ public class VirtualHostFilter implements Filter {
 
 		HttpServletRequest httpReq = (HttpServletRequest)req;
 
-		if (_log.isDebugEnabled()) {
-			_log.debug("Received " + httpReq.getRequestURL());
-		}
+		// Company id needs to always be called here so that it's properly set
+		// in subsequent calls
 
 		long companyId = PortalInstances.getCompanyId(httpReq);
 
 		if (_log.isDebugEnabled()) {
 			_log.debug("Company id " + companyId);
+		}
+
+		if (!USE_VIRTUAL_HOST_FILTER) {
+			chain.doFilter(req, res);
+
+			return;
+		}
+
+		String requestURL = httpReq.getRequestURL().toString();
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Received " + requestURL);
+		}
+
+		if (!isValidRequestURL(requestURL)) {
+			chain.doFilter(req, res);
+
+			return;
 		}
 
 		String contextPath = PortalUtil.getPathContext();
@@ -107,38 +133,45 @@ public class VirtualHostFilter implements Filter {
 			_log.debug("Friendly URL " + friendlyURL);
 		}
 
-		String redirect = null;
-
-		if (USE_VIRTUAL_HOST_FILTER && isValidFriendlyURL(friendlyURL)) {
-			String mainPath = PortalUtil.PATH_MAIN;
-
-			LayoutSet layoutSet = (LayoutSet)req.getAttribute(
-				WebKeys.VIRTUAL_HOST_LAYOUT_SET);
-
-			if (layoutSet != null) {
-				try {
-					redirect = PortalUtil.getLayoutActualURL(
-						layoutSet.getGroupId(), layoutSet.isPrivateLayout(),
-						mainPath, friendlyURL);
-				}
-				catch (Exception e) {
-					_log.error(e, e);
-				}
-			}
-		}
-
-		if (redirect != null) {
-			if (_log.isDebugEnabled()) {
-				_log.debug("Redirect to " + redirect);
-			}
-
-			RequestDispatcher rd = _ctx.getRequestDispatcher(redirect);
-
-			rd.forward(req, res);
-		}
-		else {
+		if (!isValidFriendlyURL(friendlyURL)) {
 			chain.doFilter(req, res);
+
+			return;
 		}
+
+		LayoutSet layoutSet = (LayoutSet)req.getAttribute(
+			WebKeys.VIRTUAL_HOST_LAYOUT_SET);
+
+		if (layoutSet != null) {
+			try {
+				String mainPath = PortalUtil.PATH_MAIN;
+
+				String redirect = PortalUtil.getLayoutActualURL(
+					layoutSet.getGroupId(), layoutSet.isPrivateLayout(),
+					mainPath, friendlyURL);
+
+				if (_log.isDebugEnabled()) {
+					_log.debug("Redirect to " + redirect);
+				}
+
+				RequestDispatcher rd = _ctx.getRequestDispatcher(redirect);
+
+				rd.forward(req, res);
+
+				return;
+			}
+			catch (NoSuchLayoutException nsle) {
+				nsle.printStackTrace();
+				if (_log.isWarnEnabled()) {
+					_log.warn(nsle.getMessage());
+				}
+			}
+			catch (Exception e) {
+				_log.error(e, e);
+			}
+		}
+
+		chain.doFilter(req, res);
 	}
 
 	public void destroy() {
@@ -146,6 +179,7 @@ public class VirtualHostFilter implements Filter {
 
 	protected boolean isValidFriendlyURL(String friendlyURL) {
 		if (PortalInstances.isIgnorePath(friendlyURL) ||
+			friendlyURL.startsWith(_PATH_C) ||
 			friendlyURL.startsWith(_PATH_HTML) ||
 			friendlyURL.startsWith(_PATH_IMAGE) ||
 			friendlyURL.startsWith(_PATH_WAP)) {
@@ -164,7 +198,55 @@ public class VirtualHostFilter implements Filter {
 		return true;
 	}
 
+	protected boolean isValidRequestURL(String requestURL) {
+		if (requestURL.endsWith(_EXT_C) || requestURL.endsWith(_EXT_CSS) || requestURL.endsWith(_EXT_GIF) ||
+			requestURL.endsWith(_EXT_IMAGE_COMPANY_LOGO) ||
+			requestURL.endsWith(_EXT_ICO) || requestURL.endsWith(_EXT_JS) ||
+			requestURL.endsWith(_EXT_JPEG) ||
+			requestURL.endsWith(_EXT_PORTAL_CSS_CACHED) ||
+			requestURL.endsWith(_EXT_PORTAL_JAVASCRIPT_CACHED) ||
+			requestURL.endsWith(_EXT_PORTAL_LAYOUT) ||
+			requestURL.endsWith(_EXT_PORTAL_LOGIN) ||
+			requestURL.endsWith(_EXT_PORTAL_LOGOUT) ||
+			requestURL.endsWith(_EXT_PNG)) {
+
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+
 	private static Log _log = LogFactory.getLog(VirtualHostFilter.class);
+
+	private static String _EXT_C = "/c";
+
+	private static String _EXT_CSS = ".css";
+
+	private static String _EXT_GIF = ".gif";
+
+	private static String _EXT_IMAGE_COMPANY_LOGO = "/image/company_logo";
+
+	private static String _EXT_ICO = ".ico";
+
+	private static String _EXT_JS = ".js";
+
+	private static String _EXT_JPEG = ".jpeg";
+
+	private static String _EXT_PORTAL_CSS_CACHED = "/portal/css_cached";
+
+	private static String _EXT_PORTAL_JAVASCRIPT_CACHED =
+		"/portal/javascript_cached";
+
+	private static String _EXT_PORTAL_LAYOUT = "/portal/layout";
+
+	private static String _EXT_PORTAL_LOGIN = "/portal/login";
+
+	private static String _EXT_PORTAL_LOGOUT = "/portal/logout";
+
+	private static String _EXT_PNG = ".png";
+
+	private static String _PATH_C = "/c/";
 
 	private static String _PATH_HTML = "/html/";
 
