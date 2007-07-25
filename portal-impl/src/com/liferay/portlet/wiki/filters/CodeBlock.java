59c1d7bb61429ea6e28e6f76e4f41b48daf3b48e
Brian Chan <brian.chan@liferay.com>
2007-07-25 06:42:50 +0000

    LEP-3334 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@8009 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/wiki/filters/CodeBlock.java
+++ portal-impl/src/com/liferay/portlet/wiki/filters/CodeBlock.java
@@ -1,81 +1,109 @@
-package com.liferay.portlet.wiki.filters;
+/**
+ * Copyright (c) 2000-2007 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.stringtree.factory.AbstractStringFetcher;
-import org.stringtree.regex.Matcher;
-import org.stringtree.regex.Pattern;
+package com.liferay.portlet.wiki.filters;
 
 import com.liferay.portal.kernel.util.StringMaker;
 import com.liferay.util.StringUtil;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.stringtree.factory.AbstractStringFetcher;
+import org.stringtree.regex.Matcher;
+import org.stringtree.regex.Pattern;
+
+/**
+ * <a href="CodeBlock.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Raymond Augé
+ *
+ */
 public class CodeBlock extends AbstractStringFetcher {
-	
+
 	public Object getObject(String content) {
-		
 		StringMaker sm = new StringMaker();
-		
-		Matcher matcher = pattern.matcher(content);
-		
-		boolean found = matcher.find();
-		int groupCount = matcher.groupCount();
-		
-		if (found && groupCount >= 4) {
+
+		Matcher matcher = _pattern.matcher(content);
+
+		if (matcher.find() && (matcher.groupCount() >= 4)) {
 			sm.append("<div class=\"wiki-code\">");
-			
+
 			String[] lines = matcher.group(2).split("\n");
-			
+
 			for (int i = 0; i < lines.length; i++) {
 				if (i != 0) {
-					sm.append("<br/>");
+					sm.append("<br />");
 				}
 
 				String translation = StringUtil.replace(
-						lines[i],
-						new String[] {
-							"\\s",
-							"<",
-							">",
-							"=",
-							"\"",
-							"'",
-							"\t"
-						},
-						new String[] {
-							"&nbsp;",
-							"&lt;",
-							"&gt;",
-							"&#x003D;",
-							"&#0034;",
-							"&#0039;",
-							"&nbsp;&#8594;&nbsp;"
-						});
-				
-				int padlength = String.valueOf(lines.length).length() - 
+					lines[i],
+					new String[] {
+						"\\s",
+						"<",
+						">",
+						"=",
+						"\"",
+						"'",
+						"\t"
+					},
+					new String[] {
+						"&nbsp;",
+						"&lt;",
+						"&gt;",
+						"&#x003D;",
+						"&#0034;",
+						"&#0039;",
+						"&nbsp;&#8594;&nbsp;"
+					});
+
+				int padlength =
+					String.valueOf(lines.length).length() -
 					String.valueOf(i + 1).length();
+
 				String padding = "";
-				
+
 				for (int j = 0; j < padlength; j++) {
 					padding += "&#0149;";
 				}
-					
+
 				sm.append("<span class=\"wiki-code-lines\">");
 				sm.append(padding + (i + 1));
 				sm.append("</span>");
-				
+
 				sm.append(translation);
 			}
-			
+
 			sm.append("</div>");
-			
+
 			content = sm.toString();
 		}
-		
+
 		return content;
 	}
-	
-	private static Pattern pattern = Pattern.compile("(\\[code\\])((.|\n)*?)(\\[/code\\])");
-	
+
+	private static Pattern _pattern = Pattern.compile(
+		"(\\[code\\])((.|\n)*?)(\\[/code\\])");
+
 	private static Log _log = LogFactory.getLog(CodeBlock.class);
 
 }
--- portal-impl/src/com/liferay/portlet/wiki/filters/MediaWikiLocalLink.java
+++ portal-impl/src/com/liferay/portlet/wiki/filters/MediaWikiLocalLink.java
@@ -1,7 +1,30 @@
+/**
+ * Copyright (c) 2000-2007 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.liferay.portlet.wiki.filters;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import org.stringtree.factory.AbstractStringFetcher;
 import org.stringtree.factory.Container;
 import org.stringtree.factory.Fetcher;
@@ -9,57 +32,57 @@ import org.stringtree.juicer.Initialisable;
 import org.stringtree.regex.Matcher;
 import org.stringtree.regex.Pattern;
 
-import com.efsol.friki.NamePreserver;
+/**
+ * <a href="MediaWikiLocalLink.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Raymond Augé
+ *
+ */
+public class MediaWikiLocalLink
+	extends AbstractStringFetcher implements Initialisable {
 
-public class MediaWikiLocalLink 
-	extends AbstractStringFetcher
-	implements Initialisable
-{
-	private Container pages;
-
-	public MediaWikiLocalLink(Container pages)
-	{
-		this.pages = pages;
-	}
-
-	public MediaWikiLocalLink()
-	{
+	public MediaWikiLocalLink() {
 		this(null);
 	}
-	
-	public Object getObject(String key)
-	{
-		String ret = key;
-		
-		Matcher match = pattern.matcher(key);
-		
-		if (match.find()) {
-			key = match.group(3);
-			
-			String title = match.group(6);
-			
-			if (pages.contains(_encodeKey(key.trim()))){
-				ret = "\nview{" + title.trim() + "," + _encodeKey(key.trim()) + "}\n";
-			}
-			else{
-				ret = "\nedit{" + title.trim() + "," + _encodeKey(key.trim()) + "}\n";
-			}
-		}
-		
-		return ret;
+
+	public MediaWikiLocalLink(Container pages) {
+		_pages = pages;
 	}
 
-	public void init(Fetcher context)
-	{
-		pages = (Container)context.getObject("wiki.pages");
+	public void init(Fetcher context) {
+		_pages = (Container)context.getObject("wiki.pages");
 	}
-	
-	private String _encodeKey(String key) {
+
+	public Object getObject(String key) {
+		String value = key;
+
+		Matcher match = _pattern.matcher(key);
+
+		if (match.find()) {
+			key = encodeKey(match.group(3).trim());
+
+			String title = match.group(6).trim();
+
+			if (_pages.contains(key)) {
+				value = "\nview{" + title + "," + key + "}\n";
+			}
+			else{
+				value = "\nedit{" + title + "," + key + "}\n";
+			}
+		}
+
+		return value;
+	}
+
+	protected String encodeKey(String key) {
 		return key.replaceAll("\\W", "_");
 	}
-	
-	private static Pattern pattern = Pattern.compile("(^|\\p{Punct}|\\p{Space})(\\[\\s*(((\\p{Lu}\\p{Ll}+)\\s?)+)\\s*\\|\\s*((([\\p{Alpha}\\p{Digit}]+)\\s?)+)\\s*\\])(\\z|\\n|\\p{Punct}|\\p{Space})");
-	
+
+	private static Pattern _pattern = Pattern.compile(
+		"(^|\\p{Punct}|\\p{Space})(\\[\\s*(((\\p{Lu}\\p{Ll}+)\\s?)+)\\s*\\|\\s*((([\\p{Alpha}\\p{Digit}]+)\\s?)+)\\s*\\])(\\z|\\n|\\p{Punct}|\\p{Space})");
+
 	private static Log _log = LogFactory.getLog(MediaWikiLocalLink.class);
+
+	private Container _pages;
 
 }
