701eb54758168838637b86ac7ddf7a2875448815
Brian Chan <brian.chan@liferay.com>
2007-07-31 07:03:56 +0000

    LEP-3404
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@8122 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ filters/header/src/com/liferay/filters/header/HeaderFilter.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-2007 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.filters.header;
+
+import java.io.IOException;
+
+import java.util.Enumeration;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * <a href="HeaderFilter.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ *
+ */
+public class HeaderFilter implements Filter {
+
+	public void init(FilterConfig config) {
+		_config = config;
+	}
+
+	public void doFilter(
+			ServletRequest req, ServletResponse res, FilterChain chain)
+		throws IOException, ServletException {
+
+		HttpServletResponse httpRes = (HttpServletResponse)res;
+
+		Enumeration enu = _config.getInitParameterNames();
+
+		while (enu.hasMoreElements()) {
+			String name = (String)enu.nextElement();
+
+			String value = _config.getInitParameter(name);
+
+			httpRes.addHeader(name, value);
+		}
+
+		chain.doFilter(req, res);
+	}
+
+	public void destroy() {
+	}
+
+	private FilterConfig _config;
+
+}
