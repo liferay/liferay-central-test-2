eda54590a3abdf884511532f1f5e26dc47f3be2b
Brian Chan <brian.chan@liferay.com>
2007-08-03 18:53:58 +0000

    Should just log an error, not set the companyid
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@8242 05bdf26c-840f-0410-9ced-eb539d925f36

--- util-bridges/src/com/liferay/util/bridges/jsf/common/LanguageManagedBean.java
+++ util-bridges/src/com/liferay/util/bridges/jsf/common/LanguageManagedBean.java
@@ -56,8 +56,7 @@ public class LanguageManagedBean implements Map {
 		_companyId = JSFPortletUtil.getCompanyId(facesContext);
 
 		if (_companyId == 0) {
-			_log.error("Unable to determine companyId from facesContext");
-			_companyId = 1;
+			_log.error("Unable to determine company id from facesContext");
 		}
 	}
 
@@ -98,7 +97,7 @@ public class LanguageManagedBean implements Map {
 			if (_log.isDebugEnabled()) {
 				_log.debug(
 					"{companyId=" + _companyId + ", locale=" + locale +
-					", key=" + key + ", value=" + value);
+						", key=" + key + ", value=" + value);
 			}
 		}
 
