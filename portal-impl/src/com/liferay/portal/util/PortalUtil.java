eea503c29b69642efdd19c5cca62bc86f695ed3a
Michael Young <michael.young@liferay.com>
2007-08-06 17:48:29 +0000

    LEP-3443 UTF-8 form method=post content broken in recent trunks
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@8282 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/util/PortalUtil.java
+++ portal-impl/src/com/liferay/portal/util/PortalUtil.java
@@ -1443,11 +1443,6 @@ public class PortalUtil {
 
 			// LEP-766
 
-			String strutsCharEncoding =
-				PropsUtil.get(PropsUtil.STRUTS_CHAR_ENCODING);
-
-			req.setCharacterEncoding(strutsCharEncoding);
-
 			res.setContentType(Constants.TEXT_HTML + "; charset=UTF-8");
 
 			rd.include(req, res);
