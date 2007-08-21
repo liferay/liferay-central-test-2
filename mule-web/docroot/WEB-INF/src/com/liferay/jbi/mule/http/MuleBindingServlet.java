4fd1bb8c8f877721e7111a9a8ddf508a1af7c17b
Brian Chan <brian.chan@liferay.com>
2007-08-21 18:32:54 +0000

    Backport 4.3.1 changes
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@8657 05bdf26c-840f-0410-9ced-eb539d925f36

--- mule-web/docroot/WEB-INF/src/com/liferay/jbi/mule/http/MuleBindingServlet.java
+++ mule-web/docroot/WEB-INF/src/com/liferay/jbi/mule/http/MuleBindingServlet.java
@@ -25,7 +25,7 @@ package com.liferay.jbi.mule.http;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.servlet.HttpHeaders;
-import com.liferay.util.ContentTypes;
+import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.util.servlet.ServletResponseUtil;
 import com.liferay.util.servlet.UploadServletRequest;
 
