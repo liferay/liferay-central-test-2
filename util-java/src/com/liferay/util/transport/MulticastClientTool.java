0db9d7ecfaf38bb55846c72aa4ccec5fc7da8ea3
Brian Chan <brian.chan@liferay.com>
2007-09-14 22:52:14 +0000

    Multicast tool, fix for jdk 1.4, format source
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@9434 05bdf26c-840f-0410-9ced-eb539d925f36

--- util-java/src/com/liferay/util/transport/MulticastClientTool.java
+++ util-java/src/com/liferay/util/transport/MulticastClientTool.java
@@ -1,43 +1,80 @@
+/**
+ * Copyright (c) 2000-2007 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.liferay.util.transport;
+
+import com.liferay.portal.kernel.util.GetterUtil;
 
 import java.net.DatagramPacket;
 
 /**
- * A client that listens for multicast messages at a designated port.  You may
- * use this to for potential multicast issues when tuning distributed caches and
- * etc.
+ * <a href="MulticastClientTool.java.html"><b><i>View Source</i></b></a>
+ *
+ * <p>
+ * A client that listens for multicast messages at a designated port. You may
+ * use this to for potential multicast issues when tuning distributed caches.
+ * </p>
  *
  * @author Michael C. Han
- * @version $Revision$
+ *
  */
 public class MulticastClientTool {
-    public static void main(String[] args) {
+
+	public static void main(String[] args) {
         try {
-            int port = Integer.parseInt(args[1]);
+            int port = GetterUtil.getInteger(args[1]);
 
-            MulticastTransport transport =
-                new MulticastTransport(new DatagramHandler() {
-                    public void process(DatagramPacket packet) {
-                        String s =
-                            new String(packet.getData(), 0, packet.getLength());
-                        System.out.println(s);
-                    }
+			DatagramHandler handler = new DatagramHandler() {
 
-                    public void errorReceived(Throwable t) {
-                        t.printStackTrace();
-                    }
-                }, args[0], port);
-            transport.connect();
-            synchronized (transport) {
+				public void process(DatagramPacket packet) {
+					String s = new String(
+						packet.getData(), 0, packet.getLength());
+
+					System.out.println(s);
+				}
+
+				public void errorReceived(Throwable t) {
+					t.printStackTrace();
+				}
+
+			};
+
+            MulticastTransport transport = new MulticastTransport(
+				handler, args[0], port);
+
+			transport.connect();
+
+			synchronized (transport) {
                 transport.wait();
             }
-        }// end try
+        }
         catch (Exception e) {
             e.printStackTrace();
-            System.err.println(e);
-            System.err.println
-                ("Usage: java MulticastClientTool multicastAddress port");
-            System.exit(1);
+
+			System.err.println(
+				"Usage: java MulticastClientTool multicastAddress port");
+
+			System.exit(1);
         }
-    }    
+    }
+
 }
--- util-java/src/com/liferay/util/transport/MulticastTransport.java
+++ util-java/src/com/liferay/util/transport/MulticastTransport.java
@@ -1,23 +1,51 @@
+/**
+ * Copyright (c) 2000-2007 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.liferay.util.transport;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import java.io.IOException;
+
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
- * The MulticastTransport will send strings across a specified multicast address.
- * It will also listen for messages and hand them to the appropriate
+ * <a href="MulticastTransport.java.html"><b><i>View Source</i></b></a>
+ *
+ * <p>
+ * The MulticastTransport will send strings across a specified multicast
+ * address. It will also listen for messages and hand them to the appropriate
  * DatagramHandler.
+ * </p>
  *
  * @author Michael C. Han
- * @version $Revision$
+ *
  */
 public class MulticastTransport extends Thread implements Transport {
-    public MulticastTransport(DatagramHandler handler,
+
+	public MulticastTransport(DatagramHandler handler,
                               String host, int port) {
         super("MulticastListener-" + host + port);
         setDaemon(true);
@@ -25,7 +53,6 @@ public class MulticastTransport extends Thread implements Transport {
         _host = host;
         _port = port;
     }
-
 
     public synchronized void connect()
         throws IOException {
@@ -71,7 +98,6 @@ public class MulticastTransport extends Thread implements Transport {
         return _connected;
     }
 
-    @Override
     public void run() {
         try {
             while (_connected) {
