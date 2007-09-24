d091d8998ef517bb783140b6907511aa1877506a
Neil Griffin <neil.griffin@liferay.com>
2007-09-24 20:53:24 +0000

    LEP-3845
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@9643 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/RenderRequestImpl.java
+++ portal-impl/src/com/liferay/portlet/RenderRequestImpl.java
@@ -353,6 +353,8 @@ public class RenderRequestImpl implements RenderRequest {
 				names.add(name);
 			}
 		}
+		// LEP-3845
+		names.add(RenderRequest.USER_INFO);
 
 		return Collections.enumeration(names);
 	}
