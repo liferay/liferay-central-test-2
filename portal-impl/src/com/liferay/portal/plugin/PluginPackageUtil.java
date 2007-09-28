06613d8c8fb4ec26ea560715e15e8c2e6e8dec1a
Brian Chan <brian.chan@liferay.com>
2007-09-28 23:07:08 +0000

    LEP-3874
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@9779 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/plugin/PluginPackageUtil.java
+++ portal-impl/src/com/liferay/portal/plugin/PluginPackageUtil.java
@@ -63,6 +63,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
