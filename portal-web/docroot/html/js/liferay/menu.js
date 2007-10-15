c36ddedacbf06e5b2745971ba83b13759b4efce5
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2007-10-15 17:25:31 +0000

    LEP-3992 - Control icons in result set is hard to use
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@10179 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-web/docroot/html/js/liferay/menu.js
@@ -0,0 +1,29 @@
+Liferay.Menu = new Class({
+	initialize: function(params) {
+		var instance = this;
+
+		instance._button = jQuery(params.button, params.context || document);
+		instance._menu = instance._button.find('ul:first');
+		instance._trigger = instance._button.find(params.trigger);
+
+		if (instance._menu.length) {
+			instance._run();
+		}
+	},
+	_run: function() {
+		var instance = this;
+
+		instance._trigger.find('ul:first li:last-child').addClass('last');
+
+		instance._trigger.hover(
+			function() {
+				var trigger = jQuery(this);
+				trigger.parent().addClass('visible');
+			},
+			function() {
+				var trigger = jQuery(this);
+				trigger.parent().removeClass('visible');
+			}
+		);
+	}
+});
