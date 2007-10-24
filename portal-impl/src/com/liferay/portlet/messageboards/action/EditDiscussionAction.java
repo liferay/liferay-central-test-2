b92bb794ec8e981844b255f50603fc060269f81a
Brian Chan <brian.chan@liferay.com>
2007-10-24 06:06:19 +0000

    LEP-4068
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@10490 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/messageboards/action/EditDiscussionAction.java
+++ portal-impl/src/com/liferay/portlet/messageboards/action/EditDiscussionAction.java
@@ -83,6 +83,8 @@ public class EditDiscussionAction extends PortletAction {
 					 e instanceof MessageSubjectException) {
 
 				SessionErrors.add(req, e.getClass().getName());
+
+				sendRedirect(req, res);
 			}
 			else {
 				throw e;
