d18a3eebfc93fe5c05de43983aa2061a7717e60f
Bruno Farache <bruno.farache@liferay.com>
2007-10-31 17:38:55 +0000

    LEP-4105 see jira comments
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@10657 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/calendar/lar/CalendarPortletDataHandlerImpl.java
+++ portal-impl/src/com/liferay/portlet/calendar/lar/CalendarPortletDataHandlerImpl.java
@@ -214,52 +214,65 @@ public class CalendarPortletDataHandlerImpl implements PortletDataHandler {
 			parameterMap, PortletDataHandlerKeys.MERGE_DATA,
 			_enableExport.getDefaultState());
 
-		CalEvent existingEvent = CalEventUtil.fetchByUUID_G(
-			event.getUuid(), context.getGroupId());
+		long plid = context.getPlid();
 
-		if (existingEvent == null) {
-			long plid = context.getPlid();
+		Date startDate = event.getStartDate();
 
-			Date startDate = event.getStartDate();
+		int startDateMonth = 0;
+		int startDateDay = 0;
+		int startDateYear = 0;
+		int startDateHour = 0;
+		int startDateMinute = 0;
 
-			int startDateMonth = 0;
-			int startDateDay = 0;
-			int startDateYear = 0;
-			int startDateHour = 0;
-			int startDateMinute = 0;
+		if (startDate != null) {
+			Calendar startCal = CalendarFactoryUtil.getCalendar();
 
-			if (startDate != null) {
-				Calendar startCal = CalendarFactoryUtil.getCalendar();
+			startCal.setTime(startDate);
 
-				startCal.setTime(startDate);
+			startDateMonth = startCal.get(Calendar.MONTH);
+			startDateDay = startCal.get(Calendar.DATE);
+			startDateYear = startCal.get(Calendar.YEAR);
+			startDateHour = startCal.get(Calendar.HOUR);
+			startDateMinute = startCal.get(Calendar.MINUTE);
+		}
 
-				startDateMonth = startCal.get(Calendar.MONTH);
-				startDateDay = startCal.get(Calendar.DATE);
-				startDateYear = startCal.get(Calendar.YEAR);
-				startDateHour = startCal.get(Calendar.HOUR);
-				startDateMinute = startCal.get(Calendar.MINUTE);
-			}
+		Date endDate = event.getEndDate();
 
-			Date endDate = event.getEndDate();
+		int endDateMonth = 0;
+		int endDateDay = 0;
+		int endDateYear = 0;
 
-			int endDateMonth = 0;
-			int endDateDay = 0;
-			int endDateYear = 0;
+		if (endDate != null) {
+			Calendar endCal = CalendarFactoryUtil.getCalendar();
 
-			if (endDate != null) {
-				Calendar endCal = CalendarFactoryUtil.getCalendar();
+			endCal.setTime(endDate);
 
-				endCal.setTime(endDate);
+			endDateMonth = endCal.get(Calendar.MONTH);
+			endDateDay = endCal.get(Calendar.DATE);
+			endDateYear = endCal.get(Calendar.YEAR);
+		}
 
-				endDateMonth = endCal.get(Calendar.MONTH);
-				endDateDay = endCal.get(Calendar.DATE);
-				endDateYear = endCal.get(Calendar.YEAR);
-			}
+		boolean addCommunityPermissions = true;
+		boolean addGuestPermissions = true;
 
-			boolean addCommunityPermissions = true;
-			boolean addGuestPermissions = true;
+		if (!mergeData) {
+			CalEventLocalServiceUtil.addEvent(
+				event.getUserId(), plid, event.getTitle(),
+				event.getDescription(), startDateMonth, startDateDay,
+				startDateYear, startDateHour, startDateMinute,
+				endDateMonth, endDateDay, endDateYear,
+				event.getDurationHour(), event.getDurationMinute(),
+				event.getAllDay(), event.getTimeZoneSensitive(),
+				event.getType(), event.getRepeating(),
+				event.getRecurrenceObj(), event.getRemindBy(),
+				event.getFirstReminder(), event.getSecondReminder(),
+				addCommunityPermissions, addGuestPermissions);
+		}
+		else {
+			CalEvent existingEvent = CalEventUtil.fetchByUUID_G(
+				event.getUuid(), context.getGroupId());
 
-			if (mergeData) {
+			if (existingEvent == null) {
 				CalEventLocalServiceUtil.addEvent(
 					event.getUuid(), event.getUserId(), plid, event.getTitle(),
 					event.getDescription(), startDateMonth, startDateDay,
@@ -273,24 +286,11 @@ public class CalendarPortletDataHandlerImpl implements PortletDataHandler {
 					addCommunityPermissions, addGuestPermissions);
 			}
 			else {
-				CalEventLocalServiceUtil.addEvent(
-					event.getUserId(), plid, event.getTitle(),
-					event.getDescription(), startDateMonth, startDateDay,
-					startDateYear, startDateHour, startDateMinute,
-					endDateMonth, endDateDay, endDateYear,
-					event.getDurationHour(), event.getDurationMinute(),
-					event.getAllDay(), event.getTimeZoneSensitive(),
-					event.getType(), event.getRepeating(),
-					event.getRecurrenceObj(), event.getRemindBy(),
-					event.getFirstReminder(), event.getSecondReminder(),
-					addCommunityPermissions, addGuestPermissions);
-			}
-		}
-		else {
-			event.setGroupId(existingEvent.getGroupId());
-			event.setPrimaryKey(existingEvent.getPrimaryKey());
+				event.setGroupId(existingEvent.getGroupId());
+				event.setPrimaryKey(existingEvent.getPrimaryKey());
 
-			CalEventUtil.update(event, true);
+				CalEventUtil.update(event, true);
+			}
 		}
 	}
 
