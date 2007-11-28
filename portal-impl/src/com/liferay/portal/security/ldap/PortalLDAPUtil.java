76d2b8444256bf66d6c5b2a230a074ec6fd3a155
Scott Lee <scott.lee@liferay.com>
2007-11-28 18:26:55 +0000

    LEP-3254 LDAP GetUser uses custom filter
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@11491 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/security/ldap/PortalLDAPUtil.java
+++ portal-impl/src/com/liferay/portal/security/ldap/PortalLDAPUtil.java
@@ -342,11 +342,21 @@ public class PortalLDAPUtil {
 
 		String baseDN = PrefsPropsUtil.getString(
 			companyId, PropsUtil.LDAP_BASE_DN);
-		String filter = getAuthSearchFilter(companyId, "*", screenName, "*");
+
+		Properties userMappings = getUserMappings(companyId);
+
+		StringMaker filter = new StringMaker();
+
+		filter.append(StringPool.OPEN_PARENTHESIS);
+		filter.append(userMappings.getProperty("screenName"));
+		filter.append(StringPool.EQUAL);
+		filter.append(screenName);
+		filter.append(StringPool.CLOSE_PARENTHESIS);
+
 		SearchControls cons = new SearchControls(
 			SearchControls.SUBTREE_SCOPE, 1, 0, null, false, false);
 
-		NamingEnumeration enu = ctx.search(baseDN, filter, cons);
+		NamingEnumeration enu = ctx.search(baseDN, filter.toString(), cons);
 
 		ctx.close();
 
