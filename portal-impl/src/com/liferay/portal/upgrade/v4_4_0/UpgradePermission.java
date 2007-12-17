93855b228725bf1d61acb112b16724c1155e2995
Brian Chan <brian.chan@liferay.com>
2007-12-17 06:41:39 +0000

    LEP-4525
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@12075 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/upgrade/v4_4_0/UpgradePermission.java
+++ portal-impl/src/com/liferay/portal/upgrade/v4_4_0/UpgradePermission.java
@@ -27,6 +27,7 @@ import com.liferay.portal.model.Location;
 import com.liferay.portal.model.Organization;
 import com.liferay.portal.model.Role;
 import com.liferay.portal.model.UserGroup;
+import com.liferay.portal.model.impl.ResourceImpl;
 import com.liferay.portal.spring.hibernate.HibernateUtil;
 import com.liferay.portal.upgrade.UpgradeException;
 import com.liferay.portal.upgrade.UpgradeProcess;
@@ -58,7 +59,8 @@ public class UpgradePermission extends UpgradeProcess {
 		}
 	}
 
-	protected void deleteStaleRolePermissions(String roleName)
+	protected void deletePermissionByActionIdAndResourceName(
+			String actionId, String resourceName)
 		throws Exception {
 
 		Connection con = null;
@@ -68,7 +70,138 @@ public class UpgradePermission extends UpgradeProcess {
 		try {
 			con = HibernateUtil.getConnection();
 
-			ps = con.prepareStatement(_DELETE_STALE_PERMISSIONS_SQL);
+			ps = con.prepareStatement(_GET_PERMISSION_SQL);
+
+			ps.setString(1, actionId);
+			ps.setString(2, resourceName);
+
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				long permissionId = rs.getLong("permissionId");
+
+				deletePermissionByPermissionId(permissionId);
+			}
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+	}
+
+	protected void deletePermissionByPermissionId(long permissionId)
+		throws Exception {
+
+		runSQL(
+			"delete from Permission_ where permissionId = " + permissionId);
+		runSQL(
+			"delete from Groups_Permissions where permissionId = " +
+				permissionId);
+		runSQL(
+			"delete from Roles_Permissions where permissionId = " +
+				permissionId);
+		runSQL(
+			"delete from Users_Permissions where permissionId = " +
+				permissionId);
+	}
+
+	protected void deletePermissionByResourceId(long resourceId)
+		throws Exception {
+
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = HibernateUtil.getConnection();
+
+			ps = con.prepareStatement(
+				"select permissionId from Permission_ where resourceId = ?");
+
+			ps.setLong(1, resourceId);
+
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				long permissionId = rs.getLong("permissionId");
+
+				deletePermissionByPermissionId(permissionId);
+			}
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+	}
+
+	protected void deleteResource(long codeId) throws Exception {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = HibernateUtil.getConnection();
+
+			ps = con.prepareStatement(
+				"select resourceId from Resource_ where codeId = ?");
+
+			ps.setLong(1, codeId);
+
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				long resourceId = rs.getLong("resourceId");
+
+				deletePermissionByResourceId(resourceId);
+
+				runSQL(
+					"delete from Resource_ where resourceId = " + resourceId);
+			}
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+	}
+
+	protected void deleteResourceCode(String resourceName)
+		throws Exception {
+
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = HibernateUtil.getConnection();
+
+			ps = con.prepareStatement(
+				"select codeId from ResourceCode where name = ?");
+
+			ps.setString(1, resourceName);
+
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				long codeId = rs.getLong("codeId");
+
+				deleteResource(codeId);
+
+				runSQL(
+					"delete from ResourceCode where name = '" + resourceName +
+						"'");
+			}
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+	}
+
+	protected void deleteRolesPermissions(String roleName) throws Exception {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = HibernateUtil.getConnection();
+
+			ps = con.prepareStatement(_GET_ROLES_PERMISSIONS_SQL);
 
 			ps.setString(1, roleName);
 
@@ -86,25 +219,65 @@ public class UpgradePermission extends UpgradeProcess {
 		}
 	}
 
+	protected void deleteUsersPermissions(int scope) throws Exception {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = HibernateUtil.getConnection();
+
+			ps = con.prepareStatement(_GET_USERS_PERMISSIONS_SQL);
+
+			ps.setLong(1, scope);
+
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				long permissionId = rs.getLong("permissionId");
+
+				runSQL(
+					"delete from Users_Permissions where permissionId = " +
+						permissionId);
+			}
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+	}
+
 	protected void doUpgrade() throws Exception {
 		runSQL("delete from OrgGroupPermission");
 
-		deleteStaleRolePermissions("Community Administrator");
-		deleteStaleRolePermissions("Community Owner");
-		deleteStaleRolePermissions("Organization Administrator");
+		for (int i = 0; i < _DELETE_PERMISSIONS.length; i++) {
+			Object[] permission = _DELETE_PERMISSIONS[i];
 
-		for (int i = 0; i < _PERMISSIONS.length; i++) {
-			Object[] permission = _PERMISSIONS[i];
+			String actionId = (String)permission[0];
+			String resourceName = ((Class)permission[1]).getName();
+
+			deletePermissionByActionIdAndResourceName(actionId, resourceName);
+		}
+
+		for (int i = 0; i < _UPDATE_PERMISSIONS.length; i++) {
+			Object[] permission = _UPDATE_PERMISSIONS[i];
 
 			String oldActionId = (String)permission[0];
 			String newActionId = (String)permission[1];
 			String resourceName = ((Class)permission[2]).getName();
 
-			renameActionIds(oldActionId, newActionId, resourceName);
+			updatePermission(oldActionId, newActionId, resourceName);
 		}
+
+		deleteResourceCode("com.liferay.portlet.blogs.model.BlogsCategory");
+
+		deleteRolesPermissions("Community Administrator");
+		deleteRolesPermissions("Community Owner");
+		deleteRolesPermissions("Organization Administrator");
+
+		deleteUsersPermissions(ResourceImpl.SCOPE_GROUP);
 	}
 
-	protected void renameActionIds(
+	protected void updatePermission(
 			String oldActionId, String newActionId, String resourceName)
 		throws Exception {
 
@@ -115,7 +288,7 @@ public class UpgradePermission extends UpgradeProcess {
 		try {
 			con = HibernateUtil.getConnection();
 
-			ps = con.prepareStatement(_RENAME_ACTION_IDS_SQL);
+			ps = con.prepareStatement(_GET_PERMISSION_SQL);
 
 			ps.setString(1, oldActionId);
 			ps.setString(2, resourceName);
@@ -135,7 +308,63 @@ public class UpgradePermission extends UpgradeProcess {
 		}
 	}
 
-	private static Object[][] _PERMISSIONS = new Object[][] {
+	private static final String _GET_PERMISSION_SQL =
+		"select Permission_.permissionId from Permission_ inner join " +
+			"Resource_ on Resource_.resourceId = Permission_.resourceId " +
+				"inner join ResourceCode on ResourceCode.codeId = " +
+					"Resource_.codeId where Permission_.actionId = ? and " +
+						"ResourceCode.name = ?";
+
+	private static final String _GET_ROLES_PERMISSIONS_SQL =
+		"select Roles_Permissions.roleId from Roles_Permissions inner join " +
+			"Role_ on Role_.roleId = Roles_Permissions.roleId where " +
+				"Role_.name = ?";
+
+	private static final String _GET_USERS_PERMISSIONS_SQL =
+		"select Users_Permissions.permissionId from Users_Permissions inner " +
+			"join Permission_ on Permission_.permissionId = " +
+				"Users_Permissions.permissionId inner join Resource_ on " +
+					"Resource_.resourceId = Permission_.resourceId inner " +
+						"join ResourceCode on ResourceCode.codeId = " +
+							"Resource_.codeId where ResourceCode.scope = ?";
+
+	private static Object[][] _DELETE_PERMISSIONS = new Object[][] {
+		new Object[] {
+			"ADMINISTRATE", Group.class
+		},
+		new Object[] {
+			"ADD_USER", Location.class
+		},
+		new Object[] {
+			"ADD_USER", Organization.class
+		},
+		new Object[] {
+			"DELETE_USER", Location.class
+		},
+		new Object[] {
+			"DELETE_USER", Organization.class
+		},
+		new Object[] {
+			"PERMISSIONS_USER", Location.class
+		},
+		new Object[] {
+			"PERMISSIONS_USER", Organization.class
+		},
+		new Object[] {
+			"UPDATE_USER", Location.class
+		},
+		new Object[] {
+			"UPDATE_USER", Organization.class
+		},
+		new Object[] {
+			"VIEW_USER", Location.class
+		},
+		new Object[] {
+			"VIEW_USER", Organization.class
+		}
+	};
+
+	private static Object[][] _UPDATE_PERMISSIONS = new Object[][] {
 		new Object[] {
 			"ADD_LOCATION", "MANAGE_SUBORGANIZATIONS", Organization.class
 		},
@@ -158,18 +387,6 @@ public class UpgradePermission extends UpgradeProcess {
 			"ASSIGN_USERS", "ASSIGN_MEMBERS", UserGroup.class
 		}
 	};
-
-	private static final String _DELETE_STALE_PERMISSIONS_SQL =
-		"select Roles_Permissions.roleId from Roles_Permissions inner join " +
-			"Role_ on Role_.roleId = Roles_Permissions.roleId and " +
-				"Role_.name = ?";
-
-	private static final String _RENAME_ACTION_IDS_SQL =
-		"select Permission_.permissionId from Permission_ inner join " +
-			"Resource_ on Resource_.resourceId = Permission_.resourceId " +
-				"inner join ResourceCode on ResourceCode.codeId = " +
-					"Resource_.codeId where Permission_.actionId = ? and " +
-						"ResourceCode.name = ?";
 
 	private static Log _log = LogFactory.getLog(UpgradePermission.class);
 
