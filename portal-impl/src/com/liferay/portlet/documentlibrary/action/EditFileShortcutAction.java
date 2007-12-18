8b4e9c835806281a4ac956ed63727663d20352b2
Brian Chan <brian.chan@liferay.com>
2007-12-18 20:15:31 +0000

    Format imports
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@12123 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/documentlibrary/action/EditFileShortcutAction.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/action/EditFileShortcutAction.java
@@ -30,8 +30,8 @@ import com.liferay.portlet.documentlibrary.FileShortcutPermissionException;
 import com.liferay.portlet.documentlibrary.NoSuchFileEntryException;
 import com.liferay.portlet.documentlibrary.NoSuchFileShortcutException;
 import com.liferay.portlet.documentlibrary.service.DLFileShortcutServiceUtil;
-import com.liferay.util.servlet.SessionErrors;
 import com.liferay.util.Html;
+import com.liferay.util.servlet.SessionErrors;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
