22d0f37fd32456522447f35b37d303494d5f0634
Brian Chan <brian.chan@liferay.com>
2008-01-21 05:58:33 +0000

    LEP-4853 LEP-4854
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@13117 05bdf26c-840f-0410-9ced-eb539d925f36

--- tools/ext_tmpl/servers/oc4j/bin/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ ! $JAVA_HOME ]
-then
-	echo JAVA_HOME not defined.
-	exit
-fi
-
-$JAVA_HOME/bin/java -Xmx512m -Dfile.encoding=UTF-8 -Duser.timezone=GMT -jar ../oc4j/j2ee/home/oc4j.jar -config ../config/server.xml -userThreads
