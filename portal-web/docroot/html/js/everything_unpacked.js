596716a6b4185e45c7405b66b16f53f20b1498a8
Brian Chan <brian.chan@liferay.com>
2008-02-01 03:42:26 +0000

    Update js
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@13416 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/docroot/html/js/everything_unpacked.js
+++ portal-web/docroot/html/js/everything_unpacked.js
@@ -19711,59 +19711,106 @@ Liferay.Notice = new Class({
 		instance._noticeClass = 'popup-alert-notice';
 		instance._useCloseButton = true;
 		instance._onClose = params.onClose;
-		
-		if (params.closeText !== false) {
-			instance._closeText = params.closeText || Liferay.Language.get('close');
-		} 
-		else {
-			instance._useCloseButton = false;
-			instance._closeText = '';
-		}
-		
+		instance._closeText = params.closeText;
+
 		instance._useToggleButton = false;
 		instance._hideText = '';
 		instance._showText = '';
-		
+
 		if (params.toggleText !== false) {
-			params.toggleText = jQuery.extend(
+			instance.toggleText = jQuery.extend(
 				{
-					hide: null, 
+					hide: null,
 					show: null
-				}, 
+				},
 			params.toggleText);
-			
-			instance._hideText = params.toggleText.hide || Liferay.Language.get('hide');
-			instance._showText = params.toggleText.show || Liferay.Language.get('show');
+
 			instance._useToggleButton = true;
-		} 
-		
+		}
+
 		if (instance._noticeType == 'warning') {
 			instance._noticeClass = 'popup-alert-warning';
 		}
-		
+
 		if (params.noticeClass) {
 			instance._noticeClass += ' ' + params.noticeClass;
 		}
-		
+
 		instance._content = params.content || '';
-		
+
 		instance._createHTML();
-		
+
 		return instance._notice;
+	},
+
+	setClosing: function() {
+		var instance = this;
+
+		var staticAlerts = jQuery('.popup-alert-notice, .popup-alert-warning').not('[@dynamic=true]');
+
+		instance._useCloseButton = true;
+		instance._addCloseButton(staticAlerts);
 	},
 
 	_createHTML: function() {
 		var instance = this;
-		
-		var notice = jQuery('<div class="' + instance._noticeClass + '"><div class="popup-alert-content"></div></div>');
-		
+
+		var notice = jQuery('<div class="' + instance._noticeClass + '" dynamic="true"><div class="popup-alert-content"></div></div>');
+
 		notice.html(instance._content);
-		
+
+		instance._addCloseButton(notice);
+		instance._addToggleButton(notice);
+
+		notice.appendTo('body');
+
+		instance._notice = notice;
+	},
+
+	_addCloseButton: function(notice) {
+		var instance = this;
+
+		if (instance._closeText !== false) {
+			instance._closeText = instance._closeText || Liferay.Language.get('close');
+		}
+		else {
+			instance._useCloseButton = false;
+			instance._closeText = '';
+		}
+
+		if (instance._useCloseButton) {
+			var html = '<input class="submit popup-alert-close" type="submit" value="' + instance._closeText + '" />';
+
+			notice.append(html);
+
+			var closeButton = notice.find('.popup-alert-close');
+			closeButton.click(
+				function() {
+					notice.slideUp('normal',
+						function() {
+							notice.remove();
+						}
+					);
+
+					if (instance._onClose) {
+						instance._onClose();
+					}
+				}
+			);
+		}
+	},
+
+	_addToggleButton: function(notice) {
+		var instance = this;
+
 		if (instance._useToggleButton) {
+			instance._hideText = instance._toggleText.hide || Liferay.Language.get('hide');
+			instance._showText = instance._toggleText.show || Liferay.Language.get('show');
+
 			var toggleButton = jQuery('<a class="toggle-button" href="javascript:;"><span>' + instance._hideText + '</span></a>');
 			var toggleSpan = toggleButton.find('span');
 			var height = 0;
-			
+
 			toggleButton.toggle(
 				function() {
 					notice.slideUp();
@@ -19774,34 +19821,9 @@ Liferay.Notice = new Class({
 					toggleSpan.text(instance._hideText);
 				}
 			);
-			
+
 			notice.append(toggleButton);
 		}
-		
-		if (instance._useCloseButton) {
-			var html = '<input class="submit popup-alert-close" type="submit" value="' + instance._closeText + '" />';
-			
-			notice.append(html);
-			
-			var closeButton = notice.find('.popup-alert-close');
-			closeButton.click(
-				function() {
-					notice.slideUp('normal', 
-						function() {
-							notice.remove();
-						}
-					);
-					
-					if (instance._onClose) {
-						instance._onClose();
-					}
-				}
-			);
-		}
-
-		notice.appendTo('body');
-
-		instance._notice = notice;
 	}
 });
 Liferay.Navigation = new Class({
@@ -20520,11 +20542,21 @@ Liferay.Session = {
 		);
 		document.title = instance._originalTitle;
 
+		if (instance._sessionWarning) {
+			clearTimeout(instance._sessionWarning);
+		}
+
 		instance._sessionWarning = setTimeout(
 			function() {
-				instance.warn();
+				if (!instance.autoExtend) {
+					instance.warn();
+				}
+				else {
+					instance.extend();
+				}
 			},
-		instance.sessionTimeout);
+			instance.sessionTimeout
+		);
 
 		instance.setCookie();
 	},
