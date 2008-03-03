36dbc88d0285e342b79772e565a3228cce149d0b
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2008-03-03 03:28:25 +0000

    LEP-5280 - Potential Javascript memory leaks with the dock
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@13988 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/docroot/html/js/liferay/dock.js
+++ portal-web/docroot/html/js/liferay/dock.js
@@ -49,18 +49,7 @@ Liferay.Dock = {
 			);
 
 			var dockOver = function(event) {
-				jQuery(document).one(
-					'click',
-					function(internalEvent) {
-						var currentEl = jQuery(internalEvent.target);
-						var dockParent = currentEl.parents('.lfr-dock');
-
-						if ((dockParent.length == 0) && !currentEl.is('.lfr-dock')) {
-							instance._toggle('hide');
-						}
-					}
-				);
-
+				instance._setCloser();
 				instance._toggle('show');
 			};
 
@@ -107,6 +96,26 @@ Liferay.Dock = {
 			dockList.addClass('expanded');
 		}
 	},
+	
+	_setCloser: function() {
+		var instance = this;
+
+		if (!instance._hovered) {
+			jQuery(document).one(
+				'click',
+				function(event) {
+					var currentEl = jQuery(event.target);
+					var dockParent = currentEl.parents('.lfr-dock');
+					if ((dockParent.length == 0) && !currentEl.is('.lfr-dock')) {
+						instance._toggle('hide');
+						instance._hovered = false;
+					}
+				}
+			);
+
+			instance._hovered = true;
+		}
+	},
 
 	_toggle: function(state) {
 		var instance = this;
@@ -126,5 +135,7 @@ Liferay.Dock = {
 			dockList.toggle();
 			dock.toggleClass('expanded');
 		}
-	}
+	},
+	
+	_hovered: false
 };
