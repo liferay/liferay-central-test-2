2962f5e85179759c3aa042c2c812d5f4f4da1832
Brian Chan <brian.chan@liferay.com>
2008-03-12 01:18:57 +0000

    LEP-5273 Recommitted fix without sourced formatting changes of original file
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@14121 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/cache/MultiVMPoolImpl.java
+++ portal-impl/src/com/liferay/portal/cache/MultiVMPoolImpl.java
@@ -37,129 +37,133 @@ import java.util.Set;
  *
  * @author Brian Wing Shun Chan
  * @author Michael Young
+ *
  */
 public class MultiVMPoolImpl implements MultiVMPool {
 
-    public void clear() {
-        _portalCacheManager.clearAll();
-    }
+	public void clear() {
+		_portalCacheManager.clearAll();
+	}
 
-    public void clear(String name) {
-        PortalCache portalCache = getCache(name);
+	public void clear(String name) {
+		PortalCache portalCache = getCache(name);
 
-        portalCache.removeAll();
-    }
+		portalCache.removeAll();
+	}
 
-    public void clearGroup(Map<String, Set<String>> groups, String groupKey,
-                           PortalCache portalCache) {
+	public void clearGroup(
+		Map<String, Set<String>> groups, String groupKey,
+		PortalCache portalCache) {
 
-        synchronized (groups) {
-            if (!groups.containsKey(groupKey)) {
-                return;
-            }
+		synchronized (groups) {
+			if (!groups.containsKey(groupKey)) {
+				return;
+			}
 
-            Set<String> groupKeys = groups.get(groupKey);
+			Set<String> groupKeys = groups.get(groupKey);
 
-            String[] keys = groupKeys.toArray(new String[groupKeys.size()]);
+			String[] keys = groupKeys.toArray(new String[groupKeys.size()]);
 
-            for (int i = 0; i < keys.length; i++) {
-                String key = keys[i];
+			for (int i = 0; i < keys.length; i++) {
+				String key = keys[i];
 
-                // The functionality here pretty much mimics OSCache groups. It is
-                // not necessary to remove the keys in dependent groups because they
-                // will be cleared when the group itself is cleared, resulting in a
-                // performance boost.
+				// The functionality here pretty much mimics OSCache groups. It
+				// is not necessary to remove the keys in dependent groups
+				// because they will be cleared when the group itself is
+				// cleared, resulting in a performance boost.
 
-                portalCache.remove(key);
-            }
+				portalCache.remove(key);
+			}
 
-            groupKeys.clear();
-        }
-    }
+			groupKeys.clear();
+		}
+	}
 
-    public Object get(String name, String key) {
-        PortalCache portalCache = getCache(name);
+	public Object get(String name, String key) {
+		PortalCache portalCache = getCache(name);
 
-        return get(portalCache, key);
-    }
+		return get(portalCache, key);
+	}
 
-    public Object get(PortalCache portalCache, String key) {
-        return portalCache.get(key);
-    }
+	public Object get(PortalCache portalCache, String key) {
+		return portalCache.get(key);
+	}
 
-    public PortalCache getCache(String name) {
-        return _portalCacheManager.getCache(name);
-    }
+	public PortalCache getCache(String name) {
+		return _portalCacheManager.getCache(name);
+	}
 
-    public void put(String name, String key, Object obj) {
-        PortalCache portalCache = getCache(name);
+	public void put(String name, String key, Object obj) {
+		PortalCache portalCache = getCache(name);
 
-        put(portalCache, key, obj);
-    }
+		put(portalCache, key, obj);
+	}
 
-    public void put(PortalCache portalCache, String key, Object obj) {
-        portalCache.put(key, obj);
-    }
+	public void put(PortalCache portalCache, String key, Object obj) {
+		portalCache.put(key, obj);
+	}
 
-    public void put(
-            PortalCache portalCache, String key,
-            Map<String, Set<String>> groups,
-            String groupKey, Object obj) {
+	public void put(
+		PortalCache portalCache, String key, Map<String, Set<String>> groups,
+		String groupKey, Object obj) {
 
-        put(portalCache, key, obj);
+		put(portalCache, key, obj);
 
-        updateGroup(groups, groupKey, key);
-    }
+		updateGroup(groups, groupKey, key);
+	}
 
-    public void put(String name, String key, Serializable obj) {
-        PortalCache portalCache = getCache(name);
+	public void put(String name, String key, Serializable obj) {
+		PortalCache portalCache = getCache(name);
 
-        put(portalCache, key, obj);
-    }
+		put(portalCache, key, obj);
+	}
 
-    public void put(PortalCache portalCache, String key, Serializable obj) {
-        portalCache.put(key, obj);
-    }
+	public void put(PortalCache portalCache, String key, Serializable obj) {
+		portalCache.put(key, obj);
+	}
 
-    public void put(PortalCache portalCache, String key,
-                    Map<String, Set<String>> groups,
-                    String groupKey, Serializable obj) {
+	public void put(
+		PortalCache portalCache, String key, Map<String, Set<String>> groups,
+		String groupKey, Serializable obj) {
 
-        put(portalCache, key, obj);
+		put(portalCache, key, obj);
 
-        updateGroup(groups, groupKey, key);
-    }
+		updateGroup(groups, groupKey, key);
+	}
 
-    public void remove(String name, String key) {
-        PortalCache portalCache = getCache(name);
+	public void remove(String name, String key) {
+		PortalCache portalCache = getCache(name);
 
-        remove(portalCache, key);
-    }
+		remove(portalCache, key);
+	}
 
-    public void remove(PortalCache portalCache, String key) {
-        portalCache.remove(key);
-    }
+	public void remove(PortalCache portalCache, String key) {
+		portalCache.remove(key);
+	}
 
-    public void setPortalCacheManager(PortalCacheManager portalCacheManager) {
-        _portalCacheManager = portalCacheManager;
-    }
+	public void setPortalCacheManager(PortalCacheManager portalCacheManager) {
+		_portalCacheManager = portalCacheManager;
+	}
 
-    public void updateGroup(Map<String, Set<String>> groups,
-                            String groupKey, String key) {
+	public void updateGroup(
+		Map<String, Set<String>> groups, String groupKey, String key) {
 
-        synchronized (groups) {
-            Set<String> groupKeys = groups.get(groupKey);
+		synchronized (groups) {
+			Set<String> groupKeys = null;
 
-            if (groupKeys == null) {
-                groupKeys = new ConcurrentHashSet<String>();
+			if (groups.containsKey(groupKey)) {
+				groupKeys = groups.get(groupKey);
+			}
+			else {
+				groupKeys = new ConcurrentHashSet<String>();
 
-                groups.put(groupKey, groupKeys);
-            }
+				groups.put(groupKey, groupKeys);
+			}
 
-            groupKeys.add(key);
-        }
-    }
+			groupKeys.add(key);
+		}
+	}
 
-    private PortalCacheManager _portalCacheManager;
+	private PortalCacheManager _portalCacheManager;
 
 }
