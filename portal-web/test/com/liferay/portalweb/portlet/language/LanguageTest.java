fc44f0f31a2195d32597e03e4af12b2b9428c0cb
Michael Saechang <michael.saechang@liferay.com>
2008-03-18 23:17:22 +0000

    LEP-4977
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@14325 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/test/com/liferay/portalweb/portlet/language/LanguageTest.java
+++ portal-web/test/com/liferay/portalweb/portlet/language/LanguageTest.java
@@ -35,77 +35,428 @@ public class LanguageTest extends BaseTestCase {
 		selenium.click(
 			"//img[@alt='\u0627\u0644\u0639\u0631\u0628\u064a\u0629 (\u0627\u0644\u0633\u0639\u0648\u062f\u064a\u0629)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent(
-				"\u0623\u0636\u0641 \u0635\u0641\u062d\u0629"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent(
+							"\u0623\u0636\u0641 \u0635\u0641\u062d\u0629")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click("//img[@alt='catal\u00e0 (Andorra)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent("Afegir p\u00e0gina"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent("Afegir p\u00e0gina")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click("//img[@alt='catal\u00e0 (Espanya)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent("Afegir p\u00e0gina"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent("Afegir p\u00e0gina")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click("//img[@alt='\u4e2d\u6587 (\u4e2d\u56fd)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent("\u589e\u52a0\u9875\u9762"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent("\u589e\u52a0\u9875\u9762")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click("//img[@alt='\u4e2d\u6587 (\u53f0\u7063)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent("\u589e\u52a0\u9801\u9762"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent("\u589e\u52a0\u9801\u9762")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click(
 			"//img[@alt='\u010de\u0161tina (\u010cesk\u00e1 republika)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent("P\u0159idej str\u00e1nku"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent("P\u0159idej str\u00e1nku")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click("//img[@alt='Nederlands (Nederland)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent("Pagina toevoegen"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent("Pagina toevoegen")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click("//img[@alt='suomi (Suomi)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent("Lis\u00e4\u00e4 sivu"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent("Lis\u00e4\u00e4 sivu")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click("//img[@alt='fran\u00e7ais (France)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent("Ajoutez une page"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent("Ajoutez une page")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click("//img[@alt='Deutsch (Deutschland)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent("Seite hinzuf\u00fcgen"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent("Seite hinzuf\u00fcgen")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click(
 			"//img[@alt='\u03b5\u03bb\u03bb\u03b7\u03bd\u03b9\u03ba\u03ac (\u0395\u03bb\u03bb\u03ac\u03b4\u03b1)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent(
-				"\u03a0\u03c1\u03bf\u03c3\u03b8\u03ae\u03ba\u03b7 \u03a3\u03b5\u03bb\u03af\u03b4\u03b1\u03c2"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent(
+							"\u03a0\u03c1\u03bf\u03c3\u03b8\u03ae\u03ba\u03b7 \u03a3\u03b5\u03bb\u03af\u03b4\u03b1\u03c2")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click("//img[@alt='magyar (Magyarorsz\u00e1g)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent("Add Page"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent("Add Page")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click("//img[@alt='italiano (Italia)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent("Aggiungi Pagina"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent("Aggiungi Pagina")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click("//img[@alt='\u65e5\u672c\u8a9e (\u65e5\u672c)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent(
-				"\u30da\u30fc\u30b8\u3092\u52a0\u3048\u306a\u3055\u3044"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent(
+							"\u30da\u30fc\u30b8\u3092\u52a0\u3048\u306a\u3055\u3044")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click("//img[@alt='Persian (Iran)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent(
-				"\u0627\u0636\u0627\u0641\u0647 \u06a9\u0631\u062f\u0646 \u062c\u062f\u06cc\u062f"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent(
+							"\u0627\u0636\u0627\u0641\u0647 \u06a9\u0631\u062f\u0646 \u062c\u062f\u06cc\u062f")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click("//img[@alt='Ti\u1ebfng Vi\u1ec7t (Vi\u1ec7t Nam)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent("Th\u00eam trang"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent("Th\u00eam trang")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click("//img[@alt='T\u00fcrk\u00e7e (T\u00fcrkiye)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent("Sayfa Ekle"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent("Sayfa Ekle")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click("//img[@alt='svenska (Sverige)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent("L\u00e4gg till sida"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent("L\u00e4gg till sida")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click("//img[@alt='espa\u00f1ol (Espa\u00f1a)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent("A\u00f1adir p\u00e1gina"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent("A\u00f1adir p\u00e1gina")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click(
 			"//img[@alt='\u0440\u0443\u0441\u0441\u043a\u0438\u0439 (\u0420\u043e\u0441\u0441\u0438\u044f)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent(
-				"\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c \u0441\u0442\u0440\u0430\u043d\u0438\u0446\u0443"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent(
+							"\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c \u0441\u0442\u0440\u0430\u043d\u0438\u0446\u0443")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click("//img[@alt='portugu\u00eas (Brasil)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent("Adicione A P\u00e1gina"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent("Adicione A P\u00e1gina")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
 		selenium.click("//img[@alt='English (United States)']");
 		selenium.waitForPageToLoad("30000");
-		verifyTrue(selenium.isTextPresent("Add Page"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isTextPresent("Add Page")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
 	}
 }
