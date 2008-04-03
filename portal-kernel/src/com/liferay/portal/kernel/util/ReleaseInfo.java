a67c4e7930ab3e287899f4a89d7f8ff7d3463cde
Brian Chan <brian.chan@liferay.com>
2008-04-03 00:45:04 +0000

    5.0.0
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@14691 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java
@@ -36,13 +36,13 @@ public class ReleaseInfo {
 
 	static String name = "Liferay Enterprise Portal";
 
-	static String version = "4.4.0";
+	static String version = "5.0.0";
 
-	static String codeName = "Spurgeon";
+	static String codeName = "Luther";
 
-	static String build = "4400";
+	static String build = "5000";
 
-	static String date = "January 29, 2008";
+	static String date = "April 3, 2008";
 
 	static String releaseInfo =
 		name + " " + version + " (" + codeName + " / Build " + build + " / " +
--- sql/portal-data-release.sql
+++ sql/portal-data-release.sql
@@ -1,1 +1,1 @@
-insert into Release_ (releaseId, createDate, modifiedDate, buildNumber, verified) values (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 4400, FALSE);
+insert into Release_ (releaseId, createDate, modifiedDate, buildNumber, verified) values (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 5000, FALSE);
