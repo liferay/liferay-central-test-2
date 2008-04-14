24e95bdcc21fcc3f8315ddf7710dfc1ec6ce6aa8
Brian Chan <brian.chan@liferay.com>
2008-04-14 22:53:36 +0000

    Remove unused word
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@15086 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java
@@ -34,7 +34,7 @@ import java.util.Date;
  */
 public class ReleaseInfo {
 
-	static String name = "Liferay Enterprise Portal";
+	static String name = "Liferay Portal";
 
 	static String version = "5.0.2";
 
