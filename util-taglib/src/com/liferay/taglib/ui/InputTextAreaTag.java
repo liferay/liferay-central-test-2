850195652ab1e625d70d685d57aa238dfe7360ee
Brian Chan <brian.chan@liferay.com>
2008-04-20 17:56:28 +0000

    LEP-5736
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@15231 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ util-taglib/src/com/liferay/taglib/ui/InputTextAreaTag.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-2008 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.taglib.ui;
+
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.taglib.util.IncludeTag;
+
+import javax.servlet.ServletRequest;
+
+/**
+ * <a href="InputTextAreaTag.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ *
+ */
+public class InputTextAreaTag extends IncludeTag {
+
+	public int doStartTag() {
+		ServletRequest req = pageContext.getRequest();
+
+		req.setAttribute("liferay-ui:input-textarea:param", _param);
+		req.setAttribute(
+			"liferay-ui:input-textarea:defaultValue", _defaultValue);
+		req.setAttribute(
+			"liferay-ui:input-textarea:disabled", String.valueOf(_disabled));
+
+		return EVAL_BODY_BUFFERED;
+	}
+
+	public void setParam(String param) {
+		_param = param;
+	}
+
+	public void setDefaultValue(String defaultValue) {
+		_defaultValue = defaultValue;
+	}
+
+	public void setDisabled(boolean disabled) {
+		_disabled = disabled;
+	}
+
+	protected String getDefaultPage() {
+		return _PAGE;
+	}
+
+	private static final String _PAGE =
+		"/html/taglib/ui/input_textarea/page.jsp";
+
+	private String _param;
+	private String _defaultValue = StringPool.BLANK;
+	private boolean _disabled;
+
+}
