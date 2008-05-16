2d0ca065dc05bbe134ca88bcaaf525342d7143d7
Brian Chan <brian.chan@liferay.com>
2008-05-16 18:26:40 +0000

    LEP-5973
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@15890 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-impl/src/com/liferay/portal/servlet/I18nServlet.java
@@ -0,0 +1,118 @@
+/**
+ * Copyright (c) 2000-2008 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portal.servlet;
+
+import com.liferay.portal.NoSuchLayoutException;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portal.util.WebKeys;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * <a href="I18nServlet.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ *
+ */
+public class I18nServlet extends HttpServlet {
+
+	public void service(HttpServletRequest req, HttpServletResponse res)
+		throws IOException, ServletException {
+
+		try {
+			String[] i18nData = getI18nData(req);
+
+			if (i18nData == null) {
+				PortalUtil.sendError(
+					HttpServletResponse.SC_NOT_FOUND,
+					new NoSuchLayoutException(), req, res);
+			}
+			else {
+				String languageId = i18nData[0];
+				String redirect = i18nData[1];
+
+				req.setAttribute(WebKeys.I18N_LANGUAGE_ID, languageId);
+
+				ServletContext ctx = getServletContext();
+
+				RequestDispatcher rd = ctx.getRequestDispatcher(redirect);
+
+				rd.forward(req, res);
+			}
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			PortalUtil.sendError(
+				HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e, req, res);
+		}
+	}
+
+	protected String[] getI18nData(HttpServletRequest req) {
+		String path = GetterUtil.getString(req.getPathInfo());
+
+		if (Validator.isNull(path)) {
+			return null;
+		}
+
+		String languageId = req.getServletPath();
+
+		int pos = languageId.lastIndexOf(StringPool.SLASH);
+
+		languageId = languageId.substring(pos + 1);
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Language ID " + languageId);
+		}
+
+		if (Validator.isNull(languageId)) {
+			return null;
+		}
+
+		String redirect = path;
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Redirect " + redirect);
+		}
+
+		return new String[] {languageId, redirect};
+	}
+
+	private static Log _log = LogFactory.getLog(I18nServlet.class);
+
+	private String _languageId;
+
+}
