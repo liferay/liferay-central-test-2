d8ce7450c1b39d9efd31cc25227300627d2a8bb2
Brian Chan <brian.chan@liferay.com>
2008-06-04 17:07:19 +0000

    LEP-6199 Rollback, see comments in JIRA
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@16477 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-service/src/com/liferay/portal/theme/ThemeDisplay.java
+++ portal-service/src/com/liferay/portal/theme/ThemeDisplay.java
@@ -333,10 +333,6 @@ public class ThemeDisplay implements Serializable {
 		return _theme.isWapTheme();
 	}
 
-	public void setWapTheme( boolean wapTheme ) {
-		_theme.setWapTheme( wapTheme );
-	}
-
 	public void setLookAndFeel(Theme theme, ColorScheme colorScheme) {
 		setLookAndFeel(getPathContext(), theme, colorScheme);
 	}
