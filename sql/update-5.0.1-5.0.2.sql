6fdfa50c93842b38faec4fd7bef0a359c3fb71f1
Bruno Farache <bruno.farache@liferay.com>
2008-06-12 15:12:07 +0000

    LEP-6278
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@16755 05bdf26c-840f-0410-9ced-eb539d925f36

--- sql/update-5.0.1-5.0.2.sql
+++ sql/update-5.0.1-5.0.2.sql
@@ -29,126 +29,126 @@ create table ExpandoValue (
 	data_ STRING null
 );
 
-create table quartz_job_details (
-	job_name VARCHAR(80) not null,
-	job_group VARCHAR(80) not null,
-	description VARCHAR(120) null,
-	job_class_name VARCHAR(128) not null,
-	is_durable VARCHAR(1) not null,
-	is_volatile VARCHAR(1) not null,
-	is_stateful VARCHAR(1) not null,
-	requests_recovery VARCHAR(1) not null,
-	job_data BLOB null,
-	primary key (job_name, job_group)
+create table QUARTZ_JOB_DETAILS (
+	JOB_NAME VARCHAR(80) not null,
+	JOB_GROUP VARCHAR(80) not null,
+	DESCRIPTION VARCHAR(120) null,
+	JOB_CLASS_NAME VARCHAR(128) not null,
+	IS_DURABLE VARCHAR(1) not null,
+	IS_VOLATILE VARCHAR(1) not null,
+	IS_STATEFUL VARCHAR(1) not null,
+	REQUESTS_RECOVERY VARCHAR(1) not null,
+	JOB_DATA BLOB null,
+	primary key (JOB_NAME, JOB_GROUP)
 );
 
-create table quartz_job_listeners (
-	job_name  VARCHAR(80) not null,
-	job_group VARCHAR(80) not null,
-	job_listener VARCHAR(80) not null,
-	primary key (job_name, job_group, job_listener)
+create table QUARTZ_JOB_LISTENERS (
+	JOB_NAME VARCHAR(80) not null,
+	JOB_GROUP VARCHAR(80) not null,
+	JOB_LISTENER VARCHAR(80) not null,
+	primary key (JOB_NAME, JOB_GROUP, JOB_LISTENER)
 );
 
-create table quartz_triggers (
-	trigger_name VARCHAR(80) not null,
-	trigger_group VARCHAR(80) not null,
-	job_name VARCHAR(80) not null,
-	job_group VARCHAR(80) not null,
-	is_volatile VARCHAR(1) not null,
-	description VARCHAR(120) null,
-	next_fire_time LONG null,
-	prev_fire_time LONG null,
-	priority INTEGER null,
-	trigger_state VARCHAR(16) not null,
-	trigger_type VARCHAR(8) not null,
-	start_time LONG not null,
-	end_time LONG null,
-	calendar_name VARCHAR(80) null,
-	misfire_instr INTEGER null,
-	job_data BLOB null,
-	primary key (trigger_name, trigger_group)
+create table QUARTZ_TRIGGERS (
+	TRIGGER_NAME VARCHAR(80) not null,
+	TRIGGER_GROUP VARCHAR(80) not null,
+	JOB_NAME VARCHAR(80) not null,
+	JOB_GROUP VARCHAR(80) not null,
+	IS_VOLATILE VARCHAR(1) not null,
+	DESCRIPTION VARCHAR(120) null,
+	NEXT_FIRE_TIME LONG null,
+	PREV_FIRE_TIME LONG null,
+	PRIORITY INTEGER null,
+	TRIGGER_STATE VARCHAR(16) not null,
+	TRIGGER_TYPE VARCHAR(8) not null,
+	START_TIME LONG not null,
+	END_TIME LONG null,
+	CALENDAR_NAME VARCHAR(80) null,
+	MISFIRE_INSTR INTEGER null,
+	JOB_DATA BLOB null,
+	primary key (TRIGGER_NAME, TRIGGER_GROUP)
 );
 
-create table quartz_simple_triggers (
-	trigger_name VARCHAR(80) not null,
-	trigger_group VARCHAR(80) not null,
-	repeat_count LONG not null,
-	repeat_interval LONG not null,
-	times_triggered LONG not null,
-	primary key (trigger_name, trigger_group)
+create table QUARTZ_SIMPLE_TRIGGERS (
+	TRIGGER_NAME VARCHAR(80) not null,
+	TRIGGER_GROUP VARCHAR(80) not null,
+	REPEAT_COUNT LONG not null,
+	REPEAT_INTERVAL LONG not null,
+	TIMES_TRIGGERED LONG not null,
+	primary key (TRIGGER_NAME, TRIGGER_GROUP)
 );
 
-create table quartz_cron_triggers (
-	trigger_name VARCHAR(80) not null,
-	trigger_group VARCHAR(80) not null,
-	cron_expression VARCHAR(80) not null,
-	time_zone_id VARCHAR(80),
-	primary key (trigger_name, trigger_group)
+create table QUARTZ_CRON_TRIGGERS (
+	TRIGGER_NAME VARCHAR(80) not null,
+	TRIGGER_GROUP VARCHAR(80) not null,
+	CRON_EXPRESSION VARCHAR(80) not null,
+	TIME_ZONE_ID VARCHAR(80),
+	primary key (TRIGGER_NAME, TRIGGER_GROUP)
 );
 
-create table quartz_blob_triggers (
-	trigger_name VARCHAR(80) not null,
-	trigger_group VARCHAR(80) not null,
-	blob_data BLOB null,
-	primary key (trigger_name, trigger_group)
+create table QUARTZ_BLOB_TRIGGERS (
+	TRIGGER_NAME VARCHAR(80) not null,
+	TRIGGER_GROUP VARCHAR(80) not null,
+	BLOB_DATA BLOB null,
+	primary key (TRIGGER_NAME, TRIGGER_GROUP)
 );
 
-create table quartz_trigger_listeners (
-    trigger_name  VARCHAR(80) not null,
-    trigger_group VARCHAR(80) not null,
-    trigger_listener VARCHAR(80) not null,
-	primary key (trigger_name, trigger_group, trigger_listener)
+create table QUARTZ_TRIGGER_LISTENERS (
+    TRIGGER_NAME VARCHAR(80) not null,
+    TRIGGER_GROUP VARCHAR(80) not null,
+    TRIGGER_LISTENER VARCHAR(80) not null,
+	primary key (TRIGGER_NAME, TRIGGER_GROUP, TRIGGER_LISTENER)
 );
 
-create table quartz_calendars (
-	calendar_name  VARCHAR(80) not null primary key,
-	calendar BLOB not null
+create table QUARTZ_CALENDARS (
+	CALENDAR_NAME VARCHAR(80) not null primary key,
+	CALENDAR BLOB not null
 );
 
-create table quartz_paused_trigger_grps (
-	trigger_group  VARCHAR(80) not null primary key
+create table QUARTZ_PAUSED_TRIGGER_GRPS (
+	TRIGGER_GROUP VARCHAR(80) not null primary key
 );
 
-create table quartz_fired_triggers (
-	entry_id VARCHAR(95) not null primary key,
-	trigger_name VARCHAR(80) not null,
-	trigger_group VARCHAR(80) not null,
-	is_volatile VARCHAR(1) not null,
-	instance_name VARCHAR(80) not null,
-	fired_time LONG not null,
-	priority INTEGER not null,
-	state VARCHAR(16) not null,
-	job_name VARCHAR(80) null,
-	job_group VARCHAR(80) null,
-	is_stateful VARCHAR(1) null,
-	requests_recovery VARCHAR(1) null
+create table QUARTZ_FIRED_TRIGGERS (
+	ENTRY_ID VARCHAR(95) not null primary key,
+	TRIGGER_NAME VARCHAR(80) not null,
+	TRIGGER_GROUP VARCHAR(80) not null,
+	IS_VOLATILE VARCHAR(1) not null,
+	INSTANCE_NAME VARCHAR(80) not null,
+	FIRED_TIME LONG not null,
+	PRIORITY INTEGER not null,
+	STATE VARCHAR(16) not null,
+	JOB_NAME VARCHAR(80) null,
+	JOB_GROUP VARCHAR(80) null,
+	IS_STATEFUL VARCHAR(1) null,
+	REQUESTS_RECOVERY VARCHAR(1) null
 );
 
-create table quartz_scheduler_state (
-	instance_name VARCHAR(80) not null primary key,
-	last_checkin_time LONG not null,
-	checkin_interval LONG not null
+create table QUARTZ_SCHEDULER_STATE (
+	INSTANCE_NAME VARCHAR(80) not null primary key,
+	LAST_CHECKIN_TIME LONG not null,
+	CHECKIN_INTERVAL LONG not null
 );
 
-create table quartz_locks (
-	lock_name  VARCHAR(40) not null primary key
+create table QUARTZ_LOCKS (
+	LOCK_NAME VARCHAR(40) not null primary key
 );
 
-insert into quartz_locks values('TRIGGER_ACCESS');
-insert into quartz_locks values('JOB_ACCESS');
-insert into quartz_locks values('CALENDAR_ACCESS');
-insert into quartz_locks values('STATE_ACCESS');
-insert into quartz_locks values('MISFIRE_ACCESS');
+insert into QUARTZ_LOCKS values('TRIGGER_ACCESS');
+insert into QUARTZ_LOCKS values('JOB_ACCESS');
+insert into QUARTZ_LOCKS values('CALENDAR_ACCESS');
+insert into QUARTZ_LOCKS values('STATE_ACCESS');
+insert into QUARTZ_LOCKS values('MISFIRE_ACCESS');
 
-create index IX_quartz_triggers_state on quartz_triggers(trigger_state);
-create index IX_quartz_triggers_next_fire_time on quartz_triggers(next_fire_time);
-create index IX_quartz_triggers_state_next_fire_time on quartz_triggers(trigger_state, next_fire_time);
-create index IX_quartz_fired_triggers_trigger_name on quartz_fired_triggers(trigger_name);
-create index IX_quartz_fired_triggers_trigger_group on quartz_fired_triggers(trigger_group);
-create index IX_quartz_fired_triggers_trigger_name_group on quartz_fired_triggers(trigger_name, trigger_group);
-create index IX_quartz_fired_triggers_instance_name on quartz_fired_triggers(instance_name);
-create index IX_quartz_fired_triggers_job_name on quartz_fired_triggers(job_name);
-create index IX_quartz_fired_triggers_job_group on quartz_fired_triggers(job_group);
+create index IX_quartz_triggers_state on QUARTZ_TRIGGERS(TRIGGER_STATE);
+create index IX_quartz_triggers_next_fire_time on QUARTZ_TRIGGERS(NEXT_FIRE_TIME);
+create index IX_quartz_triggers_state_next_fire_time on QUARTZ_TRIGGERS(TRIGGER_STATE, NEXT_FIRE_TIME);
+create index IX_quartz_fired_triggers_trigger_name on QUARTZ_FIRED_TRIGGERS(TRIGGER_NAME);
+create index IX_quartz_fired_triggers_trigger_group on QUARTZ_FIRED_TRIGGERS(TRIGGER_GROUP);
+create index IX_quartz_fired_triggers_trigger_name_group on QUARTZ_FIRED_TRIGGERS(TRIGGER_NAME, TRIGGER_GROUP);
+create index IX_quartz_fired_triggers_instance_name on QUARTZ_FIRED_TRIGGERS(INSTANCE_NAME);
+create index IX_quartz_fired_triggers_job_name on QUARTZ_FIRED_TRIGGERS(JOB_NAME);
+create index IX_quartz_fired_triggers_job_group on QUARTZ_FIRED_TRIGGERS(JOB_GROUP);
 
 drop table SocialActivity;
 create table SocialActivity (
