89ebd190ca0502840a1843688867a66e7302f761
Brian Chan <brian.chan@liferay.com>
2008-06-17 20:14:55 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@16908 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/image/ImageProcessorImpl.java
+++ portal-impl/src/com/liferay/portal/image/ImageProcessorImpl.java
@@ -24,8 +24,8 @@ package com.liferay.portal.image;
 
 import com.liferay.portal.kernel.image.ImageBag;
 import com.liferay.portal.kernel.image.ImageProcessor;
-import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.JavaProps;
+import com.liferay.portal.util.FileImpl;
 
 import com.sun.media.jai.codec.ImageCodec;
 import com.sun.media.jai.codec.ImageDecoder;
@@ -149,7 +149,7 @@ public class ImageProcessorImpl implements ImageProcessor {
 	}
 
 	public ImageBag read(File file) throws IOException {
-		return read(FileUtil.getBytes(file));
+		return read(_fileUtil.getBytes(file));
 	}
 
 	public ImageBag read(byte[] bytes) {
@@ -250,5 +250,7 @@ public class ImageProcessorImpl implements ImageProcessor {
 	}
 
 	private static Log _log = LogFactory.getLog(ImageProcessorImpl.class);
+
+	private static FileImpl _fileUtil = new FileImpl();
 
 }
