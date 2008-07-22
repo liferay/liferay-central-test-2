5f0a8104b7fe2efbe84ad22436590d6ba11ca09f
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2008-07-22 16:53:52 +0000

    LEP-6611 - on safari, classic theme "add page" is hidden
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@18348 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/docroot/html/js/liferay/dock.js
+++ portal-web/docroot/html/js/liferay/dock.js
@@ -104,6 +104,10 @@ Liferay.Dock = {
 
 				var top = parseInt(obj.css('top'));
 
+				if (Liferay.Browser.is_safari && isNaN(top)) {
+					top = -1;
+				}
+
 				if (!isNaN(top) && top != 0) {
 					position = '';
 					zIndex = '';
