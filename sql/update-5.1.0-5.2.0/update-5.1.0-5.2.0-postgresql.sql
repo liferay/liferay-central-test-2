d99abc4e049d82bba608902832fc1d00889a436a
Jorge Ferrer <jorge.ferrer@liferay.com>
2008-07-23 14:18:14 +0000

    LEP-6831
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@18415 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ sql/update-5.1.0-5.2.0/update-5.1.0-5.2.0-postgresql.sql
@@ -0,0 +1,16 @@
+create table Vocabulary (
+	vocabularyId bigint not null primary key,
+	groupId bigint,
+	companyId bigint,
+	userId bigint,
+	userName varchar(75) null,
+	createDate timestamp null,
+	modifiedDate timestamp null,
+	name varchar(75) null,
+	description varchar(75) null,
+	folksonomy bool
+);
+
+alter table TagsEntry add groupId bigint;
+alter table TagsEntry add vocabularyId bigint;
+alter table TagsEntry add parentEntryId bigint;
