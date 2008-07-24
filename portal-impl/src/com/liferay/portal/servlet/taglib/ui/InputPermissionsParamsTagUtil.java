bb8a1d2d8542599c899ae504dcc69416e1b8e9d7
Jorge Ferrer <jorge.ferrer@liferay.com>
2008-07-24 20:55:23 +0000

    LEP-6863
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@18529 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-impl/src/com/liferay/portal/servlet/taglib/ui/InputPermissionsParamsTagUtil.java
@@ -0,0 +1,116 @@
+/**
+ * Copyright (c) 2000-2008 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portal.servlet.taglib.ui;
+
+import com.liferay.portal.kernel.util.JavaConstants;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.Layout;
+import com.liferay.portal.security.permission.ResourceActionsUtil;
+import com.liferay.portal.util.WebKeys;
+
+import java.util.List;
+
+import javax.portlet.RenderResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * <a href="InputPermissionsParamsTagUtil.java.html"><b><i>View Source</i></b>
+ * </a>
+ *
+ * @author Brian Chan
+ * @author Jorge Ferrer
+ *
+ */
+public class InputPermissionsParamsTagUtil {
+
+	public static void doEndTag(String modelName, PageContext pageContext)
+		throws JspException {
+
+		try {
+			HttpServletRequest request =
+				(HttpServletRequest)pageContext.getRequest();
+
+			RenderResponse renderResponse =
+				(RenderResponse)request.getAttribute(
+					JavaConstants.JAVAX_PORTLET_RESPONSE);
+
+			Layout layout = (Layout)request.getAttribute(WebKeys.LAYOUT);
+
+			Group group = layout.getGroup();
+
+			List<String> supportedActions =
+				ResourceActionsUtil.getModelResourceActions(modelName);
+			List<String> communityDefaultActions =
+				ResourceActionsUtil.getModelResourceCommunityDefaultActions(
+					modelName);
+			List<String> guestDefaultActions =
+				ResourceActionsUtil.getModelResourceGuestDefaultActions(
+					modelName);
+			List<String> guestUnsupportedActions =
+				ResourceActionsUtil.getModelResourceGuestUnsupportedActions(
+					modelName);
+
+			StringBuilder sb = new StringBuilder();
+
+			for (int i = 0; i < supportedActions.size(); i++) {
+				String action = supportedActions.get(i);
+
+				boolean communityChecked = communityDefaultActions.contains(
+					action);
+				boolean guestChecked = guestDefaultActions.contains(action);
+				boolean guestDisabled = guestUnsupportedActions.contains(
+					action);
+
+				if (guestDisabled) {
+					guestChecked = false;
+				}
+
+				if (group.isCommunity() || group.isOrganization()) {
+					if (communityChecked) {
+						sb.append(StringPool.AMPERSAND);
+						sb.append(renderResponse.getNamespace());
+						sb.append("communityPermissions=");
+						sb.append(action);
+					}
+				}
+
+				if (guestChecked) {
+					sb.append(StringPool.AMPERSAND);
+					sb.append(renderResponse.getNamespace());
+					sb.append("guestPermissions=");
+					sb.append(action);
+				}
+			}
+
+			pageContext.getOut().print(sb.toString());
+		}
+		catch (Exception e) {
+			throw new JspException(e);
+		}
+	}
+
+}
--- util-taglib/src/com/liferay/taglib/ui/InputPermissionsParamsTag.java
+++ util-taglib/src/com/liferay/taglib/ui/InputPermissionsParamsTag.java
@@ -22,98 +22,89 @@
 
 package com.liferay.taglib.ui;
 
-import com.liferay.portal.kernel.util.JavaConstants;
+import com.liferay.portal.kernel.util.MethodInvoker;
+import com.liferay.portal.kernel.util.MethodWrapper;
+import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
 import com.liferay.portal.kernel.util.StringPool;
-import com.liferay.portal.model.Group;
-import com.liferay.portal.model.Layout;
-import com.liferay.portal.security.permission.ResourceActionsUtil;
-import com.liferay.portal.util.WebKeys;
 
-import java.util.List;
-
-import javax.portlet.RenderResponse;
-
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.TagSupport;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * <a href="InputPermissionsParamsTag.java.html"><b><i>View Source</i></b></a>
  *
  * @author Brian Wing Shun Chan
+ * @author Jorge Ferrer
  *
  */
 public class InputPermissionsParamsTag extends TagSupport {
 
-	public int doEndTag() throws JspException {
+	public static String doTag(
+			String modelName, PageContext pageContext)
+		throws Exception {
+
+		Object returnObj = null;
+
+		ClassLoader contextClassLoader =
+			Thread.currentThread().getContextClassLoader();
+
 		try {
-			HttpServletRequest request =
-				(HttpServletRequest)pageContext.getRequest();
+			Thread.currentThread().setContextClassLoader(
+				PortalClassLoaderUtil.getClassLoader());
 
-			RenderResponse renderResponse =
-				(RenderResponse)request.getAttribute(
-					JavaConstants.JAVAX_PORTLET_RESPONSE);
+			MethodWrapper methodWrapper = new MethodWrapper(
+				_TAG_CLASS, _TAG_DO_END_METHOD,
+				new Object[] {modelName, pageContext}
+			);
 
-			Layout layout = (Layout)request.getAttribute(WebKeys.LAYOUT);
-
-			Group group = layout.getGroup();
-
-			List<String> supportedActions =
-				ResourceActionsUtil.getModelResourceActions(_modelName);
-			List<String> communityDefaultActions =
-				ResourceActionsUtil.getModelResourceCommunityDefaultActions(
-					_modelName);
-			List<String> guestDefaultActions =
-				ResourceActionsUtil.getModelResourceGuestDefaultActions(
-					_modelName);
-			List<String> guestUnsupportedActions =
-				ResourceActionsUtil.getModelResourceGuestUnsupportedActions(
-					_modelName);
-
-			StringBuilder sb = new StringBuilder();
-
-			for (int i = 0; i < supportedActions.size(); i++) {
-				String action = supportedActions.get(i);
-
-				boolean communityChecked = communityDefaultActions.contains(
-					action);
-				boolean guestChecked = guestDefaultActions.contains(action);
-				boolean guestDisabled = guestUnsupportedActions.contains(
-					action);
-
-				if (guestDisabled) {
-					guestChecked = false;
-				}
-
-				if (group.isCommunity() || group.isOrganization()) {
-					if (communityChecked) {
-						sb.append(StringPool.AMPERSAND);
-						sb.append(renderResponse.getNamespace());
-						sb.append("communityPermissions=");
-						sb.append(action);
-					}
-				}
-
-				if (guestChecked) {
-					sb.append(StringPool.AMPERSAND);
-					sb.append(renderResponse.getNamespace());
-					sb.append("guestPermissions=");
-					sb.append(action);
-				}
-			}
-
-			pageContext.getOut().print(sb.toString());
-
-			return EVAL_PAGE;
+			returnObj = MethodInvoker.invoke(methodWrapper);
 		}
 		catch (Exception e) {
-			throw new JspException(e);
+			_log.error(e, e);
 		}
+		finally {
+			Thread.currentThread().setContextClassLoader(contextClassLoader);
+		}
+
+		if (returnObj != null) {
+			return returnObj.toString();
+		}
+		else {
+			return StringPool.BLANK;
+		}
+	}
+
+	public int doEndTag() throws JspException {
+		try {
+			doTag(_modelName, pageContext);
+		}
+		catch (Exception e) {
+			if (e instanceof JspException) {
+				throw (JspException)e;
+			}
+			else {
+				throw new JspException(e);
+			}
+		}
+
+		return EVAL_PAGE;
 	}
 
 	public void setModelName(String modelName) {
 		_modelName = modelName;
 	}
+
+	private static final String _TAG_CLASS =
+		"com.liferay.portal.servlet.taglib.ui.InputPermissionsParamsTagUtil";
+
+	private static final String _TAG_DO_END_METHOD = "doEndTag";
+
+	private static Log _log = LogFactory.getLog(
+		InputPermissionsParamsTag.class);
 
 	private String _modelName;
 
