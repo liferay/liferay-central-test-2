a9dddb89c5ad68686b903f7daedfcc4ba93dfb39
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2008-07-24 17:56:54 +0000

    LEP-6861 - Inline Editor popups don't close when clicking cancel or update
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@18515 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/docroot/html/js/liferay/popup.js
+++ portal-web/docroot/html/js/liferay/popup.js
@@ -234,7 +234,7 @@ jQuery.extend(
 
 			var obj = el;
 
-			if (!el.jQuery) {
+			if (!el.jquery) {
 				obj = jQuery(el);
 			}
 
@@ -242,7 +242,7 @@ jQuery.extend(
 				obj = obj.parents('.ui-dialog-content');
 			}
 
-			obj.trigger('dialogclose');
+			obj.dialog('close');
 		},
 
 		update: function(id, url) {
