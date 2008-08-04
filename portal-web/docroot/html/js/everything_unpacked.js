829e5b5547d68a2aac873e75e5837aeb71efbc01
Brian Chan <brian.chan@liferay.com>
2008-08-04 19:58:25 +0000

    Pack CSS, JS
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@18902 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/docroot/html/js/everything_unpacked.js
+++ portal-web/docroot/html/js/everything_unpacked.js
@@ -14634,6 +14634,432 @@ $.Autocompleter.Selection = function(field, start, end) {
 	});
 	
 })(jQuery);
+/*
+ * Treeview pre-1.4.1 - jQuery plugin to hide and show branches of a tree
+ * 
+ * http://bassistance.de/jquery-plugins/jquery-plugin-treeview/
+ * http://docs.jquery.com/Plugins/Treeview
+ *
+ * Copyright (c) 2007 Jörn Zaefferer
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ *
+ * Revision: $Id: jquery.treeview.js 5759 2008-07-01 07:50:28Z joern.zaefferer $
+ *
+ */
+
+;(function($) {
+
+	$.extend($.fn, {
+		swapClass: function(c1, c2) {
+			var c1Elements = this.filter('.' + c1);
+			this.filter('.' + c2).removeClass(c2).addClass(c1);
+			c1Elements.removeClass(c1).addClass(c2);
+			return this;
+		},
+		replaceClass: function(c1, c2) {
+			return this.filter('.' + c1).removeClass(c1).addClass(c2).end();
+		},
+		hoverClass: function(className) {
+			className = className || "hover";
+			return this.hover(function() {
+				$(this).addClass(className);
+			}, function() {
+				$(this).removeClass(className);
+			});
+		},
+		heightToggle: function(animated, callback) {
+			animated ?
+				this.animate({ height: "toggle" }, animated, callback) :
+				this.each(function(){
+					jQuery(this)[ jQuery(this).is(":hidden") ? "show" : "hide" ]();
+					if(callback)
+						callback.apply(this, arguments);
+				});
+		},
+		heightHide: function(animated, callback) {
+			if (animated) {
+				this.animate({ height: "hide" }, animated, callback);
+			} else {
+				this.hide();
+				if (callback)
+					this.each(callback);				
+			}
+		},
+		prepareBranches: function(settings) {
+			if (!settings.prerendered) {
+				// mark last tree items
+				this.filter(":last-child:not(ul)").addClass(CLASSES.last);
+				// collapse whole tree, or only those marked as closed, anyway except those marked as open
+				this.filter((settings.collapsed ? "" : "." + CLASSES.closed) + ":not(." + CLASSES.open + ")").find(">ul").hide();
+			}
+			// return all items with sublists
+			return this.filter(":has(>ul)");
+		},
+		applyClasses: function(settings, toggler) {
+			this.filter(":has(>ul):not(:has(>a))").find(">span").unbind("click.treeview").bind("click.treeview", function(event) {
+				// don't handle click events on children, eg. checkboxes
+				if ( this == event.target )
+					toggler.apply($(this).next());
+			}).add( $("a", this) ).hoverClass();
+			
+			if (!settings.prerendered) {
+				// handle closed ones first
+				this.filter(":has(>ul:hidden)")
+						.addClass(CLASSES.expandable)
+						.replaceClass(CLASSES.last, CLASSES.lastExpandable);
+						
+				// handle open ones
+				this.not(":has(>ul:hidden)")
+						.addClass(CLASSES.collapsable)
+						.replaceClass(CLASSES.last, CLASSES.lastCollapsable);
+						
+	            // create hitarea if not present
+				var hitarea = this.find("div." + CLASSES.hitarea);
+				if (!hitarea.length)
+					hitarea = this.prepend("<div class=\"" + CLASSES.hitarea + "\"/>").find("div." + CLASSES.hitarea);
+				hitarea.removeClass().addClass(CLASSES.hitarea).each(function() {
+					var classes = "";
+					$.each($(this).parent().attr("class").split(" "), function() {
+						classes += this + "-hitarea ";
+					});
+					$(this).addClass( classes );
+				})
+			}
+			
+			// apply event to hitarea
+			this.find("div." + CLASSES.hitarea).click( toggler );
+		},
+		treeview: function(settings) {
+			
+			settings = $.extend({
+				cookieId: "treeview"
+			}, settings);
+			
+			if ( settings.toggle ) {
+				var callback = settings.toggle;
+				settings.toggle = function() {
+					return callback.apply($(this).parent()[0], arguments);
+				};
+			}
+		
+			// factory for treecontroller
+			function treeController(tree, control) {
+				// factory for click handlers
+				function handler(filter) {
+					return function() {
+						// reuse toggle event handler, applying the elements to toggle
+						// start searching for all hitareas
+						toggler.apply( $("div." + CLASSES.hitarea, tree).filter(function() {
+							// for plain toggle, no filter is provided, otherwise we need to check the parent element
+							return filter ? $(this).parent("." + filter).length : true;
+						}) );
+						return false;
+					};
+				}
+				// click on first element to collapse tree
+				$("a:eq(0)", control).click( handler(CLASSES.collapsable) );
+				// click on second to expand tree
+				$("a:eq(1)", control).click( handler(CLASSES.expandable) );
+				// click on third to toggle tree
+				$("a:eq(2)", control).click( handler() ); 
+			}
+		
+			// handle toggle event
+			function toggler() {
+				$(this)
+					.parent()
+					// swap classes for hitarea
+					.find(">.hitarea")
+						.swapClass( CLASSES.collapsableHitarea, CLASSES.expandableHitarea )
+						.swapClass( CLASSES.lastCollapsableHitarea, CLASSES.lastExpandableHitarea )
+					.end()
+					// swap classes for parent li
+					.swapClass( CLASSES.collapsable, CLASSES.expandable )
+					.swapClass( CLASSES.lastCollapsable, CLASSES.lastExpandable )
+					// find child lists
+					.find( ">ul" )
+					// toggle them
+					.heightToggle( settings.animated, settings.toggle );
+				if ( settings.unique ) {
+					$(this).parent()
+						.siblings()
+						// swap classes for hitarea
+						.find(">.hitarea")
+							.replaceClass( CLASSES.collapsableHitarea, CLASSES.expandableHitarea )
+							.replaceClass( CLASSES.lastCollapsableHitarea, CLASSES.lastExpandableHitarea )
+						.end()
+						.replaceClass( CLASSES.collapsable, CLASSES.expandable )
+						.replaceClass( CLASSES.lastCollapsable, CLASSES.lastExpandable )
+						.find( ">ul" )
+						.heightHide( settings.animated, settings.toggle );
+				}
+			}
+			this.data("toggler", toggler);
+			
+			function serialize() {
+				function binary(arg) {
+					return arg ? 1 : 0;
+				}
+				var data = [];
+				branches.each(function(i, e) {
+					data[i] = $(e).is(":has(>ul:visible)") ? 1 : 0;
+				});
+				$.cookie(settings.cookieId, data.join(""), settings.cookieOptions );
+			}
+			
+			function deserialize() {
+				var stored = $.cookie(settings.cookieId);
+				if ( stored ) {
+					var data = stored.split("");
+					branches.each(function(i, e) {
+						$(e).find(">ul")[ parseInt(data[i]) ? "show" : "hide" ]();
+					});
+				}
+			}
+			
+			// add treeview class to activate styles
+			this.addClass("treeview");
+			
+			// prepare branches and find all tree items with child lists
+			var branches = this.find("li").prepareBranches(settings);
+			
+			switch(settings.persist) {
+			case "cookie":
+				var toggleCallback = settings.toggle;
+				settings.toggle = function() {
+					serialize();
+					if (toggleCallback) {
+						toggleCallback.apply(this, arguments);
+					}
+				};
+				deserialize();
+				break;
+			case "location":
+				var current = this.find("a").filter(function() { return this.href.toLowerCase() == location.href.toLowerCase(); });
+				if ( current.length ) {
+					current.addClass("selected").parents("ul, li").add( current.next() ).show();
+				}
+				break;
+			}
+			
+			branches.applyClasses(settings, toggler);
+				
+			// if control option is set, create the treecontroller and show it
+			if ( settings.control ) {
+				treeController(this, settings.control);
+				$(settings.control).show();
+			}
+			
+			return this;
+		}
+	});
+	
+	// classes used by the plugin
+	// need to be styled via external stylesheet, see first example
+	$.treeview = {};
+	var CLASSES = ($.treeview.classes = {
+		open: "open",
+		closed: "closed",
+		expandable: "expandable",
+		expandableHitarea: "expandable-hitarea",
+		lastExpandableHitarea: "lastExpandable-hitarea",
+		collapsable: "collapsable",
+		collapsableHitarea: "collapsable-hitarea",
+		lastCollapsableHitarea: "lastCollapsable-hitarea",
+		lastCollapsable: "lastCollapsable",
+		lastExpandable: "lastExpandable",
+		last: "last",
+		hitarea: "hitarea"
+	});
+	
+	// provide backwards compability
+	$.fn.Treeview = $.fn.treeview;
+	
+})(jQuery);
+(function($) {
+
+$.widget("ui.tree", {
+	init: function() {
+
+		var self = this;
+		this.identifier = (new Date()).getTime()+Math.random();
+
+		this.element.sortable({
+			items: this.options.sortOn,
+			scope: this.identifier,
+			distance: this.options.distance,
+			placeholder: "ui-tree-placeholder",
+			start: function(e, ui) {
+				var inst = $(this).data("sortable");
+				inst.placeholder.hide();
+				inst.helperProportions.height = inst.currentItem.find(self.options.dropOn).length ? inst.currentItem.find(self.options.dropOn).outerHeight() : inst.currentItem.outerHeight();
+				inst._preserveHelperProportions = true;
+				inst.refreshPositions(true);
+				
+				self.originalParent = ui.item.parent();
+				
+				(self.options.start && self.options.start.apply(this, [e, ui]));
+			},
+			stop: function(e, ui) {
+				var sortable = $(this).data("sortable");
+				$(sortable.options.items, sortable.element).removeClass(sortable.options.sortIndicatorDown).removeClass(sortable.options.sortIndicatorUp);
+				
+				if ( self.originalParent.is(':empty') )
+					self.originalParent.remove();
+				
+				(self.options.stop && self.options.stop.apply(this, [e, ui]));
+			},
+			sortIndicator: function(e, item, append, hardRefresh) {
+	
+				append ? append[0].appendChild(this.placeholder[0]) : item.item[0].parentNode.insertBefore(this.placeholder[0], (this.direction == 'down' ? item.item[0] : item.item[0].nextSibling));
+
+				$(this.options.items, this.element).removeClass(self.options.sortIndicatorDown).removeClass(self.options.sortIndicatorUp);
+				item.item.addClass(this.direction == "down" ? self.options.sortIndicatorDown : self.options.sortIndicatorUp);
+	
+			}
+		});
+		
+		//Make certain nodes droppable
+		$(this.options.dropOn, this.element).droppable({
+			accept: this.options.sortOn,
+			hoverClass: this.options.dropHoverClass,
+			tolerance: "pointer",
+			scope: this.identifier,
+			over: function(e, ui) {
+				$(self.options.sortOn, self.element).removeClass(self.options.sortIndicatorDown).removeClass(self.options.sortIndicatorUp);
+				self.overDroppable = true;
+				self.trigger('over', e, ui);
+			},
+			out: function(e, ui) {
+				self.overDroppable = false;
+				(self.options.out && self.options.out.apply(this, [e, ui]));
+			},
+			drop: function(e, ui) {
+				
+				var ul = $(this).parent().find("ul");
+				if(!ul.length) var ul = $("<ul></ul>").appendTo($(this).parent());
+				
+				ui.draggable.appendTo( $(this).parent().find("> ul") );
+				
+				self.element.data("sortable")._noFinalSort = true;
+				
+				(self.options.drop && self.options.drop.apply(this, [e, ui]));
+			}
+		});
+
+	}
+});
+
+$.extend($.ui.tree, {
+	defaults: {
+		sortOn: "*",
+		dropOn: "div",
+		dropHoverClass: "ui-tree-hover",
+		sortIndicatorDown: "hover-down",
+		sortIndicatorUp: "hover-up"
+	}
+});
+
+})(jQuery);
+;(function( $ ){
+
+	var $scrollTo = $.scrollTo = function( target, duration, settings ){
+		$scrollTo.window().scrollTo( target, duration, settings );
+	};
+
+	$scrollTo.defaults = {
+		axis:'y',
+		duration:1
+	};
+
+	//returns the element that needs to be animated to scroll the window
+	$scrollTo.window = function(){
+		return $( $.browser.safari ? 'body' : 'html' );
+	};
+
+	$.fn.scrollTo = function( target, duration, settings ){
+		if( typeof duration == 'object' ){
+			settings = duration;
+			duration = 0;
+		}
+		settings = $.extend( {}, $scrollTo.defaults, settings );
+		duration = duration || settings.speed || settings.duration;//speed is still recognized for backwards compatibility
+		settings.queue = settings.queue && settings.axis.length > 1;//make sure the settings are given right
+		if( settings.queue )
+			duration /= 2;//let's keep the overall speed, the same.
+		settings.offset = both( settings.offset );
+		settings.over = both( settings.over );
+
+		return this.each(function(){
+			var elem = this, $elem = $(elem),
+				t = target, toff, attr = {},
+				win = $elem.is('html,body');
+			switch( typeof t ){
+				case 'number'://will pass the regex
+				case 'string':
+					if( /^([+-]=)?\d+(px)?$/.test(t) ){
+						t = both( t );
+						break;//we are done
+					}
+					t = $(t,this);// relative selector, no break!
+				case 'object':
+					if( t.is || t.style )//DOM/jQuery
+						toff = (t = $(t)).offset();//get the real position of the target 
+			}
+			$.each( settings.axis.split(''), function( i, axis ){
+				var Pos	= axis == 'x' ? 'Left' : 'Top',
+					pos = Pos.toLowerCase(),
+					key = 'scroll' + Pos,
+					act = elem[key],
+					Dim = axis == 'x' ? 'Width' : 'Height',
+					dim = Dim.toLowerCase();
+
+				if( toff ){//jQuery/DOM
+					attr[key] = toff[pos] + ( win ? 0 : act - $elem.offset()[pos] );
+
+					if( settings.margin ){//if it's a dom element, reduce the margin
+						attr[key] -= parseInt(t.css('margin'+Pos)) || 0;
+						attr[key] -= parseInt(t.css('border'+Pos+'Width')) || 0;
+					}
+					
+					attr[key] += settings.offset[pos] || 0;//add/deduct the offset
+					
+					if( settings.over[pos] )//scroll to a fraction of its width/height
+						attr[key] += t[dim]() * settings.over[pos];
+				}else
+					attr[key] = t[pos];//remove the unnecesary 'px'
+
+				if( /^\d+$/.test(attr[key]) )//number or 'number'
+					attr[key] = attr[key] <= 0 ? 0 : Math.min( attr[key], max(Dim) );//check the limits
+
+				if( !i && settings.queue ){//queueing each axis is required					
+					if( act != attr[key] )//don't waste time animating, if there's no need.
+						animate( settings.onAfterFirst );//intermediate animation
+					delete attr[key];//don't animate this axis again in the next iteration.
+				}
+			});			
+			animate( settings.onAfter );			
+
+			function animate( callback ){
+				$elem.animate( attr, duration, settings.easing, callback && function(){
+					callback.call(this, target);
+				});
+			};
+			function max( Dim ){
+				var el = win ? $.browser.opera ? document.body : document.documentElement : elem;
+				return el['scroll'+Dim] - el['client'+Dim];
+			};
+		});
+	};
+
+	function both( val ){
+		return typeof val == 'object' ? val : { top:val, left:val };
+	};
+
+})( jQuery );
 
 
 /*
