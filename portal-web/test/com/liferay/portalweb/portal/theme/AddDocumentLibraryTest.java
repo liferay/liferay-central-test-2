49107ef13b634fa249d2bfe448b2d4246e408835
Michael Saechang <michael.saechang@liferay.com>
2008-08-11 22:15:57 +0000

    LEP-7068
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@19113 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-web/test/com/liferay/portalweb/portal/theme/AddDocumentLibraryTest.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2000-2008 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portalweb.portal.theme;
+
+import com.liferay.portalweb.portal.BaseTestCase;
+import com.liferay.portalweb.portal.util.RuntimeVariables;
+
+/**
+ * <a href="AddDocumentLibraryTest.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ *
+ */
+public class AddDocumentLibraryTest extends BaseTestCase {
+	public void testAddDocumentLibrary() throws Exception {
+		selenium.click("link=Add Application");
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isElementPresent(
+							"//div[@id=\"CMS-DocumentLibrary\"]")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.click("//div[@id=\"CMS-DocumentLibrary\"]/p/a");
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isElementPresent("//input[@value='Add Folder']")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.click("//input[@value='Add Folder']");
+		selenium.waitForPageToLoad("30000");
+		selenium.type("_20_name", RuntimeVariables.replace("Theme Testing"));
+		selenium.type("_20_description",
+			RuntimeVariables.replace("Screengrab Me!"));
+		selenium.click("//input[@value='Save']");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=Theme Testing");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("//input[@value='Add Document']");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=Use the classic uploader.");
+		selenium.captureEntirePageScreenshot("L:\\portal\\build\\portal-web\\test-output\\brochure\\AddDocumentLibraryTest.jpg",
+			"");
+		selenium.click("link=Return to Full Page");
+		selenium.waitForPageToLoad("30000");
+	}
+}
---
+++ portal-web/test/com/liferay/portalweb/portal/theme/SelectThemeTest.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-2008 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portalweb.portal.theme;
+
+import com.liferay.portalweb.portal.BaseTestCase;
+
+/**
+ * <a href="SelectThemeTest.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ *
+ */
+public class SelectThemeTest extends BaseTestCase {
+	public void testSelectTheme() throws Exception {
+		selenium.click("link=Manage Pages");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=Look and Feel");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("//input[@name='_88_themeId' and @value='brochure']");
+		selenium.waitForPageToLoad("30000");
+		selenium.captureEntirePageScreenshot("L:\\portal\\build\\portal-web\\test-output\\brochure\\SelectThemeTest.jpg",
+			"");
+		selenium.click("link=Return to Full Page");
+		selenium.waitForPageToLoad("30000");
+	}
+}
