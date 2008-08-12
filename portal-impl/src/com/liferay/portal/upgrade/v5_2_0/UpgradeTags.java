c631954e583ccc348c807f922e0a74ecbe1fe165
Brian Chan <brian.chan@liferay.com>
2008-08-12 18:34:05 +0000

    LEP-7082
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@19159 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/upgrade/v5_2_0/UpgradeTags.java
+++ portal-impl/src/com/liferay/portal/upgrade/v5_2_0/UpgradeTags.java
@@ -22,6 +22,7 @@
 
 package com.liferay.portal.upgrade.v5_2_0;
 
+import com.liferay.counter.service.CounterLocalServiceUtil;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.StringPool;
@@ -29,6 +30,7 @@ import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.upgrade.UpgradeException;
 import com.liferay.portal.upgrade.UpgradeProcess;
 import com.liferay.portal.util.PropsValues;
+import com.liferay.portlet.tags.NoSuchEntryException;
 import com.liferay.portlet.tags.NoSuchVocabularyException;
 import com.liferay.portlet.tags.model.TagsVocabulary;
 import com.liferay.portlet.tags.service.TagsVocabularyLocalServiceUtil;
@@ -36,6 +38,7 @@ import com.liferay.portlet.tags.service.TagsVocabularyLocalServiceUtil;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Timestamp;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -47,6 +50,8 @@ import org.apache.commons.logging.LogFactory;
  * <a href="UpgradeTags.java.html"><b><i>View Source</i></b></a>
  *
  * @author Jorge Ferrer
+ * @author Brian Wing Shun Chan
+ *
  */
 public class UpgradeTags extends UpgradeProcess {
 
@@ -54,11 +59,166 @@ public class UpgradeTags extends UpgradeProcess {
 		_log.info("Upgrading");
 
 		try {
-			upgradeEntryGroupId();
-			upgradePropertyCategory();
+			updateGroupId();
+			updateCategory();
 		}
 		catch (Exception e) {
 			throw new UpgradeException(e);
+		}
+	}
+
+	protected long copyEntry(long groupId, long entryId) throws Exception {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = DataAccess.getConnection();
+
+			ps = con.prepareStatement(
+				"select * from TagsEntry where entryId = ?");
+
+			ps.setLong(1, entryId);
+
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				long companyId = rs.getLong("companyId");
+				long userId = rs.getLong("userId");
+				String userName = rs.getString("userName");
+				Timestamp createDate = rs.getTimestamp("createDate");
+				Timestamp modifiedDate = rs.getTimestamp("modifiedDate");
+				String name = rs.getString("name");
+
+				long newEntryId = CounterLocalServiceUtil.increment();
+
+				ps = con.prepareStatement(
+					"insert into TagsEntry (entryId, groupId, companyId, " +
+						"userId, userName, createDate, modifiedDate, name) " +
+							"values (?, ?, ?, ?, ?, ?, ?, ?)");
+
+				ps.setLong(1, newEntryId);
+				ps.setLong(2, groupId);
+				ps.setLong(3, companyId);
+				ps.setLong(4, userId);
+				ps.setString(5, userName);
+				ps.setTimestamp(6, createDate);
+				ps.setTimestamp(7, modifiedDate);
+				ps.setString(8, name);
+
+				ps.executeUpdate();
+
+				ps.close();
+
+				copyProperties(entryId, newEntryId);
+
+				return newEntryId;
+			}
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+
+		throw new NoSuchEntryException(
+			"No TagsEntry exists with the primary key " + entryId);
+	}
+
+	public void copyProperties(long entryId, long newEntryId) throws Exception {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = DataAccess.getConnection();
+
+			ps = con.prepareStatement(
+				"select * from TagsProperty where entryId = ?");
+
+			ps.setLong(1, entryId);
+
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				long companyId = rs.getLong("companyId");
+				long userId = rs.getLong("userId");
+				String userName = rs.getString("userName");
+				Timestamp createDate = rs.getTimestamp("createDate");
+				Timestamp modifiedDate = rs.getTimestamp("modifiedDate");
+				String key = rs.getString("key_");
+				String value = rs.getString("value");
+
+				long newPropertyId = CounterLocalServiceUtil.increment();
+
+				ps = con.prepareStatement(
+					"insert into TagsProperty (propertyId, companyId, " +
+						"userId, userName, createDate, modifiedDate, " +
+							"entryId, key_, value) values (?, ?, ?, ?, ?, ?, " +
+								"?, ?, ?)");
+
+				ps.setLong(1, newPropertyId);
+				ps.setLong(2, companyId);
+				ps.setLong(3, userId);
+				ps.setString(4, userName);
+				ps.setTimestamp(5, createDate);
+				ps.setTimestamp(6, modifiedDate);
+				ps.setLong(7, newEntryId);
+				ps.setString(8, key);
+				ps.setString(9, value);
+
+				ps.executeUpdate();
+
+				ps.close();
+			}
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+	}
+
+	protected void deleteEntries() throws Exception {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = DataAccess.getConnection();
+
+			ps = con.prepareStatement(
+				"select entryId from TagsEntry where groupId = 0");
+
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				long entryId = rs.getLong("entryId");
+
+				ps = con.prepareStatement(
+					"delete from TagsAssets_TagsEntries where entryId = ?");
+
+				ps.setLong(1, entryId);
+
+				ps.executeUpdate();
+
+				ps.close();
+
+				ps = con.prepareStatement(
+					"delete from TagsProperty where entryId = ?");
+
+				ps.setLong(1, entryId);
+
+				ps.executeUpdate();
+
+				ps.close();
+			}
+
+			ps = con.prepareStatement(
+				"delete from TagsEntry where groupId = 0");
+
+			ps.executeUpdate();
+
+			ps.close();
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
 		}
 	}
 
@@ -95,7 +255,7 @@ public class UpgradeTags extends UpgradeProcess {
 		return vocabulary.getVocabularyId();
 	}
 
-	protected void upgradePropertyCategory() throws Exception {
+	protected void updateCategory() throws Exception {
 		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
@@ -105,7 +265,7 @@ public class UpgradeTags extends UpgradeProcess {
 
 			ps = con.prepareStatement(
 				"select TE.entryId, TE.groupId, TE.userId, TP.propertyId, " +
-					"TP.value from TagsEntry as TE, TagsProperty as TP where " +
+					"TP.value from TagsEntry TE, TagsProperty TP where " +
 						"TE.entryId = TP.entryId and TE.vocabularyId <= 0 " +
 							"and TP.key_ = 'category'");
 
@@ -128,6 +288,8 @@ public class UpgradeTags extends UpgradeProcess {
 
 				ps.executeUpdate();
 
+				ps.close();
+
 				ps = con.prepareStatement(
 					"delete from TagsProperty where propertyId = ?");
 
@@ -143,7 +305,45 @@ public class UpgradeTags extends UpgradeProcess {
 		}
 	}
 
-	protected void upgradeEntryGroupId() throws Exception {
+	protected void updateGroupId() throws Exception {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = DataAccess.getConnection();
+
+			ps = con.prepareStatement(
+				"select TA.assetId, TA.groupId, TA_TE.entryId from " +
+					"TagsAssets_TagsEntries TA_TE inner join TagsAsset TA on " +
+						"TA.assetId = TA_TE.assetId");
+
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				long assetId = rs.getLong("TA.assetId");
+				long groupId = rs.getLong("TA.groupId");
+				long entryId = rs.getLong("TA_TE.entryId");
+
+				long newEntryId = copyEntry(groupId, entryId);
+
+				ps = con.prepareStatement(
+					"insert into TagsAssets_TagsEntries (assetId, entryId) " +
+						"values (?, ?)");
+
+				ps.setLong(1, assetId);
+				ps.setLong(2, newEntryId);
+
+				ps.executeUpdate();
+
+				ps.close();
+			}
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+
+		deleteEntries();
 	}
 
 	private String[] _DEFAULT_CATEGORY_PROPERTY_VALUES = new String[] {
