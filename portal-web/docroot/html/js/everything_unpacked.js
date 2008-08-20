f420b4914a99e3693cd3fb84cb9571dd50503b9d
Brian Chan <brian.chan@liferay.com>
2008-08-20 20:30:08 +0000

    Pack JS
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@19413 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/docroot/html/js/everything_unpacked.js
+++ portal-web/docroot/html/js/everything_unpacked.js
@@ -6112,36 +6112,40 @@ $.widget("ui.tree", {
 			scope: this.identifier,
 			distance: this.options.distance,
 			placeholder: "ui-tree-placeholder",
+			helper: this.options.helper,
+			handle: this.options.handle,
+			scroll: this.options.scroll,
+			appendTo: this.options.appendTo,
 			start: function(e, ui) {
 				var inst = $(this).data("sortable");
 				inst.placeholder.hide();
 				inst.helperProportions.height = inst.currentItem.find(self.options.dropOn).length ? inst.currentItem.find(self.options.dropOn).outerHeight() : inst.currentItem.outerHeight();
 				inst._preserveHelperProportions = true;
 				inst.refreshPositions(true);
-				
+
 				self.originalParent = ui.item.parent();
-				
+
 				(self.options.start && self.options.start.apply(this, [e, ui]));
 			},
 			stop: function(e, ui) {
 				var sortable = $(this).data("sortable");
-				$(sortable.options.items, sortable.element).removeClass(sortable.options.sortIndicatorDown).removeClass(sortable.options.sortIndicatorUp);
-				
+				$(sortable.options.items, sortable.element).removeClass(self.options.sortIndicatorDown).removeClass(self.options.sortIndicatorUp);
+
 				if ( self.originalParent.is(':empty') )
 					self.originalParent.remove();
-				
+
 				(self.options.stop && self.options.stop.apply(this, [e, ui]));
 			},
 			sortIndicator: function(e, item, append, hardRefresh) {
-	
+
 				append ? append[0].appendChild(this.placeholder[0]) : item.item[0].parentNode.insertBefore(this.placeholder[0], (this.direction == 'down' ? item.item[0] : item.item[0].nextSibling));
 
 				$(this.options.items, this.element).removeClass(self.options.sortIndicatorDown).removeClass(self.options.sortIndicatorUp);
 				item.item.addClass(this.direction == "down" ? self.options.sortIndicatorDown : self.options.sortIndicatorUp);
-	
+
 			}
 		});
-		
+
 		//Make certain nodes droppable
 		$(this.options.dropOn, this.element).droppable({
 			accept: this.options.sortOn,
@@ -6158,14 +6162,14 @@ $.widget("ui.tree", {
 				(self.options.out && self.options.out.apply(this, [e, ui]));
 			},
 			drop: function(e, ui) {
-				
+
 				var ul = $(this).parent().find("ul");
 				if(!ul.length) var ul = $("<ul></ul>").appendTo($(this).parent());
-				
+
 				ui.draggable.appendTo( $(this).parent().find("> ul") );
-				
+
 				self.element.data("sortable")._noFinalSort = true;
-				
+
 				(self.options.drop && self.options.drop.apply(this, [e, ui]));
 			}
 		});
