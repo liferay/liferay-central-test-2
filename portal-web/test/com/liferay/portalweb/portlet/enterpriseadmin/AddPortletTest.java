3c5ded128f62ad9526a89f178f8e3eed3f721d7b
JR Houn <jr.houn@liferay.com>
2008-09-08 16:38:39 +0000

    LEP-7388 - Selenium Test Update
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@20093 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/test/com/liferay/portalweb/portlet/enterpriseadmin/AddPortletTest.java
+++ portal-web/test/com/liferay/portalweb/portlet/enterpriseadmin/AddPortletTest.java
@@ -41,7 +41,7 @@ public class AddPortletTest extends BaseTestCase {
 
 			try {
 				if (selenium.isElementPresent(
-							"//div[@id=\"Admin-EnterpriseAdmin\"]")) {
+							"//div[@id='Admin-Portal-EnterpriseAdmin']")) {
 					break;
 				}
 			}
@@ -51,7 +51,7 @@ public class AddPortletTest extends BaseTestCase {
 			Thread.sleep(1000);
 		}
 
-		selenium.click("//div[@id=\"Admin-EnterpriseAdmin\"]/p/a");
+		selenium.click("//div[@id='Admin-Portal-EnterpriseAdmin']/p/a");
 
 		for (int second = 0;; second++) {
 			if (second >= 60) {
