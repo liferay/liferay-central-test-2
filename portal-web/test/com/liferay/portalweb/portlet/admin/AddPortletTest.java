a11695d138f4d77b3a21277242c1764ac24db3d7
JR Houn <jr.houn@liferay.com>
2008-09-08 17:21:19 +0000

    LEP-7388
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@20099 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/test/com/liferay/portalweb/portlet/admin/AddPortletTest.java
+++ portal-web/test/com/liferay/portalweb/portlet/admin/AddPortletTest.java
@@ -40,7 +40,7 @@ public class AddPortletTest extends BaseTestCase {
 			}
 
 			try {
-				if (selenium.isElementPresent("//div[@id=\"Admin-Admin\"]")) {
+				if (selenium.isElementPresent("//div[@id='Admin-Server-Admin']")) {
 					break;
 				}
 			}
@@ -50,7 +50,7 @@ public class AddPortletTest extends BaseTestCase {
 			Thread.sleep(1000);
 		}
 
-		selenium.click("//div[@id=\"Admin-Admin\"]/p/a");
+		selenium.click("//div[@id='Admin-Server-Admin']/p/a");
 
 		for (int second = 0;; second++) {
 			if (second >= 60) {
