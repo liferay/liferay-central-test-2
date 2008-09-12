f0be7998d8354a46d65d13adbca2c859b6f4f30a
Ganesh Ram <ganesh.ram@sun.com>
2008-09-12 10:35:37 +0000

    LEP-7452
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@20339 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-kernel/src/com/liferay/portal/kernel/messaging/DestinationNames.java
+++ portal-kernel/src/com/liferay/portal/kernel/messaging/DestinationNames.java
@@ -59,6 +59,8 @@ public interface DestinationNames {
 		"liferay/message_boards_mailing_list";
 
 	public static final String RUON = "liferay/ruon";
+	
+	public static final String SAW = "liferay/saw";
 
 	public static final String SCHEDULER = "liferay/scheduler";
 
