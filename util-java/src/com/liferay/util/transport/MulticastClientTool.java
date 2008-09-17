ec84e6af3c36b9a1107c89bf66226e4a287d9742
Raymond Auge <raymond.auge@liferay.com>
2008-09-17 17:44:58 +0000

    LEP-7556
    http://support.liferay.com/browse/LEP-7556
    
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@20517 05bdf26c-840f-0410-9ced-eb539d925f36

--- util-java/src/com/liferay/util/transport/MulticastClientTool.java
+++ util-java/src/com/liferay/util/transport/MulticastClientTool.java
@@ -22,9 +22,18 @@
 
 package com.liferay.util.transport;
 
-import com.liferay.portal.kernel.util.GetterUtil;
-
 import java.net.DatagramPacket;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.GZIPInputStream;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * <a href="MulticastClientTool.java.html"><b><i>View Source</i></b></a>
@@ -35,31 +44,90 @@ import java.net.DatagramPacket;
  * </p>
  *
  * @author Michael C. Han
+ * @author Raymond Augé
  *
  */
 public class MulticastClientTool {
 
 	public static void main(String[] args) {
-		try {
-			int port = GetterUtil.getInteger(args[1]);
+        try {
+        	Map<String, Object> argsMap = processArgs(args);
+
+        	Integer port = (Integer)argsMap.get("port");
+			String host = (String)argsMap.get("host");
+			final Boolean gzip = (Boolean)argsMap.get("gzip");
+			final Boolean shortData = (Boolean)argsMap.get("short");
 
 			DatagramHandler handler = new DatagramHandler() {
-
 				public void process(DatagramPacket packet) {
-					String s = new String(
-						packet.getData(), 0, packet.getLength());
+					byte payload[] = packet.getData();
 
-					System.out.println(s);
+					if (gzip.booleanValue()) {
+						byte ungzipped[] = new byte[0];
+
+						try {
+	                        GZIPInputStream inputStream = new GZIPInputStream(
+	                        	new ByteArrayInputStream(payload));
+	                        ByteArrayOutputStream byteArrayOutputStream =
+	                        	new ByteArrayOutputStream(payload.length);
+
+	                        byte buffer[] = new byte[1500];
+
+	                        int bytesRead = 0;
+
+	                        do {
+	                            if (bytesRead == -1) {
+	                                break;
+								}
+
+	                            bytesRead = inputStream.read(buffer, 0, 1500);
+
+	                            if (bytesRead != -1) {
+	                                byteArrayOutputStream.write(
+	                                	buffer, 0, bytesRead);
+								}
+	                    	} while (true);
+
+							ungzipped = byteArrayOutputStream.toByteArray();
+							inputStream.close();
+							byteArrayOutputStream.close();
+
+							payload = ungzipped;
+						}
+						catch (IOException e) {
+							_log.error(e);
+						}
+					}
+
+					if (shortData.booleanValue()) {
+						byte[] temp = new byte[96];
+						System.arraycopy(payload, 0, temp, 0, 96);
+						payload = temp;
+					}
+
+					StringBuilder sb = new StringBuilder();
+					sb.append("[");
+					sb.append(packet.getSocketAddress());
+					sb.append("] ");
+					sb.append(new String(payload));
+
+					_log.info(sb);
 				}
 
 				public void errorReceived(Throwable t) {
-					t.printStackTrace();
+					_log.error(t);
 				}
 
 			};
 
 			MulticastTransport transport = new MulticastTransport(
-				handler, args[0], port);
+				handler, host, port);
+
+			if (shortData.booleanValue()) {
+				_log.info("Truncating to 96 bytes.");
+			}
+
+			_log.info("Started up and waiting...");
 
 			transport.connect();
 
@@ -68,13 +136,52 @@ public class MulticastClientTool {
 			}
 		}
 		catch (Exception e) {
-			e.printStackTrace();
+			_log.error(e);
 
 			System.err.println(
-				"Usage: java MulticastClientTool multicastAddress port");
+				"Usage: java -classpath " +
+				"util-java.jar:commons-logging.jar " +
+				"com.liferay.util.transport.MulticastClientTool [-g] [-s] " +
+				"-h [multicastAddress] -p [port]");
 
 			System.exit(1);
 		}
 	}
+
+	private static Map<String, Object> processArgs(String[] args)
+		throws Exception {
+
+		Map<String, Object> argsMap = new HashMap<String, Object>();
+
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equals("-g")) {
+				argsMap.put("gzip", Boolean.TRUE);
+			}
+			else if (args[i].equals("-s")) {
+				argsMap.put("short", Boolean.TRUE);
+				i++;
+			}
+			else if (args[i].equals("-h")) {
+				argsMap.put("host", args[i + 1]);
+				i++;
+			}
+			else if (args[i].equals("-p")) {
+				argsMap.put("port", new Integer(args[i + 1]));
+				i++;
+			}
+		}
+
+		if (!argsMap.containsKey("gzip")) {
+			argsMap.put("gzip", Boolean.FALSE);
+		}
+		if (!argsMap.containsKey("short")) {
+			argsMap.put("short", Boolean.FALSE);
+		}
+
+		return argsMap;
+	}
+
+    private static final Log _log = LogFactory.getLog(
+    	MulticastClientTool.class);
 
 }
