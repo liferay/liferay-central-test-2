d43df4a01a8dae11cfaf2b004dfcb417605d56f8
JR Houn <jr.houn@liferay.com>
2008-09-30 18:41:41 +0000

    LEP-7691
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@20978 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/test/com/liferay/portalweb/portal/session/DeletePageTest.java
+++ portal-web/test/com/liferay/portalweb/portal/session/DeletePageTest.java
@@ -20,26 +20,32 @@
  * SOFTWARE.
  */
 
-package com.liferay.portalweb.portal.session;
+package com.liferay.portalweb.portlet.assetpublisher;
 
-import com.liferay.portalweb.portal.BaseTests;
+import com.liferay.portalweb.portal.BaseTestCase;
+import com.liferay.portalweb.portal.util.RuntimeVariables;
 
 /**
- * <a href="SessionExpirationTests.java.html"><b><i>View Source</i></b></a>
+ * <a href="DeletePageTest.java.html"><b><i>View Source</i></b></a>
  *
  * @author Brian Wing Shun Chan
  *
  */
-public class SessionExpirationTests extends BaseTests {
-
-	public SessionExpirationTests() {
-		addTestSuite(AddPageTest.class);
-		addTestSuite(SignInWithOutRememberMeTest.class);
-		addTestSuite(ExtendSessionTest.class);
-		addTestSuite(ConfirmSessionExpireTest.class);
-		addTestSuite(RememberMeLoginTest.class);
-		addTestSuite(ConfirmNoSessionExpireTest.class);
-		addTestSuite(DeletePageTest.class);
+public class DeletePageTest extends BaseTestCase {
+	public void testDeletePage() throws Exception {
+		selenium.click(RuntimeVariables.replace(
+				"//div[@id='navigation']/ul/li[1]/a/span"));
+		selenium.waitForPageToLoad("30000");
+		selenium.click(RuntimeVariables.replace("link=Manage Pages"));
+		selenium.waitForPageToLoad("30000");
+		selenium.click(RuntimeVariables.replace(
+				"//div[@id='_88_layoutsTreeOutput']/ul/li[2]/ul/li[3]/a/span"));
+		selenium.waitForPageToLoad("30000");
+		selenium.click(RuntimeVariables.replace("//input[@value='Delete']"));
+		selenium.waitForPageToLoad("30000");
+		assertTrue(selenium.getConfirmation()
+						   .matches("^Are you sure you want to delete the selected page[\\s\\S]$"));
+		selenium.click(RuntimeVariables.replace("link=Return to Full Page"));
+		selenium.waitForPageToLoad("30000");
 	}
-
 }
