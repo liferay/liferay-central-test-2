849875a82451654ee772b359571a48caba5872aa
Raymond Auge <raymond.auge@liferay.com>
2008-10-15 13:19:16 +0000

    LEP-7707
    http://support.liferay.com/browse/LEP-7707
    null
    
    
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@21593 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ util-taglib/src/com/liferay/taglib/ui/CustomAttributeListTag.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2000-2008 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.taglib.ui;
+
+import com.liferay.taglib.util.IncludeTag;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * <a href="RatingsTag.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ *
+ */
+public class CustomAttributeListTag extends IncludeTag {
+
+	public int doStartTag() {
+		HttpServletRequest request =
+			(HttpServletRequest)pageContext.getRequest();
+
+		request.setAttribute(
+			"liferay-ui:custom-attribute-list:className", _className);
+		request.setAttribute(
+			"liferay-ui:custom-attribute-list:classPK",
+			String.valueOf(_classPK));
+		request.setAttribute(
+			"liferay-ui:custom-attribute-list:editable",
+			String.valueOf(_editable));
+		request.setAttribute(
+			"liferay-ui:custom-attribute-list:label", String.valueOf(_label));
+
+		return EVAL_BODY_BUFFERED;
+	}
+
+	public void setClassName(String className) {
+		_className = className;
+	}
+
+	public void setClassPK(long classPK) {
+		_classPK = classPK;
+	}
+
+	protected String getDefaultPage() {
+		return _PAGE;
+	}
+
+	public void setEditable(boolean editable) {
+		_editable = editable;
+	}
+
+	public void setLabel(boolean label) {
+		_label = label;
+	}
+
+	private static final String _PAGE = "/html/taglib/ui/custom-attribute-list/page.jsp";
+
+	private String _className;
+	private long _classPK;
+	private boolean _editable = false;
+	private boolean _label = false;
+
+}
