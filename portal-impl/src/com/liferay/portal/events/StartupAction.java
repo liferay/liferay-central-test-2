ab6d1f8bbb99b981d67b46b0ff8dfa9a91699ce2
Brian Chan <brian.chan@liferay.com>
2008-10-21 21:43:46 +0000

    LPS-236 Rollback, see comments
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@21825 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/events/StartupAction.java
+++ portal-impl/src/com/liferay/portal/events/StartupAction.java
@@ -36,13 +36,11 @@ import com.liferay.portal.kernel.scheduler.SchedulerEngineUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.InstancePool;
 import com.liferay.portal.kernel.util.ReleaseInfo;
-import com.liferay.portal.model.Company;
 import com.liferay.portal.model.CompanyConstants;
 import com.liferay.portal.model.Release;
 import com.liferay.portal.scheduler.SchedulerEngineProxy;
 import com.liferay.portal.search.lucene.LuceneUtil;
 import com.liferay.portal.service.ClassNameLocalServiceUtil;
-import com.liferay.portal.service.CompanyLocalServiceUtil;
 import com.liferay.portal.service.ReleaseLocalServiceUtil;
 import com.liferay.portal.tools.sql.DBUtil;
 import com.liferay.portal.upgrade.UpgradeProcess;
@@ -50,8 +48,6 @@ import com.liferay.portal.util.PropsKeys;
 import com.liferay.portal.util.PropsUtil;
 import com.liferay.portal.velocity.LiferayResourceLoader;
 import com.liferay.portal.verify.VerifyProcess;
-
-import java.util.List;
 
 import org.apache.commons.collections.ExtendedProperties;
 import org.apache.commons.logging.Log;
@@ -194,15 +190,6 @@ public class StartupAction extends SimpleAction {
 
 				if (_log.isInfoEnabled()) {
 					_log.info("Running upgrade " + upgradeProcesses[i]);
-				}
-
-				// Check Companies before upgrade
-
-				List<Company> companies =
-					CompanyLocalServiceUtil.getCompanies();
-
-				for (Company company : companies) {
-					CompanyLocalServiceUtil.checkCompany(company.getWebId());
 				}
 
 				upgradeProcess.upgrade();
