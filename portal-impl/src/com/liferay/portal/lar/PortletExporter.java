9d4f90b1a967612f6f7171502d6d25a8bab2a4c3
Brian Chan <brian.chan@liferay.com>
2008-10-23 17:39:05 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@21889 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/lar/PortletExporter.java
+++ portal-impl/src/com/liferay/portal/lar/PortletExporter.java
@@ -74,7 +74,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.apache.commons.lang.time.StopWatch;
 import org.apache.commons.logging.Log;
