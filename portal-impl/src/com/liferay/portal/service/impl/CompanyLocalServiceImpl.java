d61b188edbfac5e8f6297534e4104e55eed6d75e
Raymond Auge <raymond.auge@liferay.com>
2008-10-26 03:19:32 +0000

    LPS-296
    http://support.liferay.com/browse/LPS-296
    
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@21984 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
@@ -38,7 +38,9 @@ import com.liferay.portal.kernel.search.BooleanQueryFactoryUtil;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Hits;
 import com.liferay.portal.kernel.search.SearchEngineUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.TimeZoneUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.Account;
 import com.liferay.portal.model.Company;
@@ -229,8 +231,8 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 			defaultUser.setPassword("password");
 			defaultUser.setScreenName(String.valueOf(defaultUser.getUserId()));
 			defaultUser.setEmailAddress("default@" + company.getMx());
-			defaultUser.setLanguageId(null);
-			defaultUser.setTimeZoneId(null);
+			defaultUser.setLanguageId(LocaleUtil.getDefault().toString());
+			defaultUser.setTimeZoneId(TimeZoneUtil.getDefault().getID());
 			defaultUser.setGreeting(
 				LanguageUtil.format(
 					companyId, defaultUser.getLocale(), "welcome-x",
