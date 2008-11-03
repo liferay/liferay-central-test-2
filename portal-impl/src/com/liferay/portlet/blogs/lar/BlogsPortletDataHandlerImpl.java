b53acb613bf4dff4239fd2ff1a7c9e5d180743ee
Brian Chan <brian.chan@liferay.com>
2008-11-03 19:57:52 +0000

    LPS-410 Rollback, Ray, can you recheck this in but have the logic exist in something like com.liferay.portlet.blogs.lar.WordPressImporter
    This way, we can add more and more of these without making BlogsPortletDataHandlerImpl larger and larger.
    
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@22342 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/blogs/lar/BlogsPortletDataHandlerImpl.java
+++ portal-impl/src/com/liferay/portlet/blogs/lar/BlogsPortletDataHandlerImpl.java
@@ -25,15 +25,9 @@ package com.liferay.portlet.blogs.lar;
 import com.liferay.portal.PortalException;
 import com.liferay.portal.SystemException;
 import com.liferay.portal.kernel.util.CalendarFactoryUtil;
-import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.xml.Document;
-import com.liferay.portal.kernel.xml.DocumentException;
 import com.liferay.portal.kernel.xml.Element;
-import com.liferay.portal.kernel.xml.Namespace;
-import com.liferay.portal.kernel.xml.Node;
 import com.liferay.portal.kernel.xml.SAXReaderUtil;
 import com.liferay.portal.lar.PortletDataContext;
 import com.liferay.portal.lar.PortletDataException;
@@ -41,32 +35,16 @@ import com.liferay.portal.lar.PortletDataHandler;
 import com.liferay.portal.lar.PortletDataHandlerBoolean;
 import com.liferay.portal.lar.PortletDataHandlerControl;
 import com.liferay.portal.lar.PortletDataHandlerKeys;
-import com.liferay.portal.model.User;
-import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.PortletKeys;
 import com.liferay.portlet.blogs.model.BlogsEntry;
 import com.liferay.portlet.blogs.service.BlogsEntryLocalServiceUtil;
 import com.liferay.portlet.blogs.service.persistence.BlogsEntryUtil;
-import com.liferay.portlet.messageboards.model.MBMessage;
-import com.liferay.portlet.messageboards.model.MBMessageDisplay;
-import com.liferay.portlet.messageboards.service.MBMessageLocalServiceUtil;
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.TimeZone;
 
 import javax.portlet.PortletPreferences;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 /**
  * <a href="BlogsPortletDataHandlerImpl.java.html"><b><i>View Source</i></b></a>
@@ -130,7 +108,7 @@ public class BlogsPortletDataHandlerImpl implements PortletDataHandler {
 
 	public PortletDataHandlerControl[] getImportControls() {
 		return new PortletDataHandlerControl[] {
-			_entries, _comments, _ratings, _tags, _wordpress
+			_entries, _comments, _ratings, _tags
 		};
 	}
 
@@ -159,188 +137,10 @@ public class BlogsPortletDataHandlerImpl implements PortletDataHandler {
 				importEntry(context, entry);
 			}
 
-			if (context.getBooleanParameter(_NAMESPACE, "wordpress")) {
-				importWordpress(context);
-			}
-
 			return null;
 		}
 		catch (Exception e) {
 			throw new PortletDataException(e);
-		}
-	}
-
-	protected void importWordpress(PortletDataContext context)
-		throws PortalException, SystemException {
-
-		Map<String, Long> userMap = getWordpressUserMap(context);
-
-		String path = getWordpressPath(context, "wordpress.xml");
-
-		String fileData = context.getZipEntryAsString(path);
-
-		if (Validator.isNull(fileData)) {
-			return;
-		}
-
-		Document wordpressDoc = null;
-
-		try {
-			wordpressDoc = SAXReaderUtil.read(fileData);
-
-		}
-		catch (DocumentException de) {
-			_log.error(
-				"Processing " + path + ": " + de.getMessage());
-
-			return;
-		}
-
-		User defaultUser = UserLocalServiceUtil.getDefaultUser(
-			context.getCompanyId());
-
-		Element root = wordpressDoc.getRootElement();
-
-		List<Element> itemEls = root.element("channel").elements("item");
-
-		DateFormat dateFormater = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		dateFormater.setTimeZone(TimeZone.getTimeZone("UTC"));
-
-		Calendar cal = Calendar.getInstance();
-
-		for (Element itemEl : itemEls) {
-			long userId = context.getUserId(null);
-
-			String creator = itemEl.elementText(
-				SAXReaderUtil.createQName("creator", _dcNs));
-
-			if (userMap.containsKey(creator)) {
-				userId = userMap.get(creator);
-			}
-
-			long plid = context.getPlid();
-
-			String title = itemEl.elementTextTrim("title");
-
-			if (Validator.isNull(title)) {
-				title = itemEl.elementTextTrim(
-					SAXReaderUtil.createQName("post_name", _wpNs));
-			}
-
-			String content = itemEl.elementText(
-				SAXReaderUtil.createQName("encoded", _contentNs));
-
-			content = content.replaceAll("\\n", "\n<br/>");
-
-			String dateText = itemEl.elementTextTrim(
-				SAXReaderUtil.createQName("post_date_gmt", _wpNs));
-
-			Date postDate = new Date();
-
-			try {
-				postDate = dateFormater.parse(dateText);
-			}
-			catch (ParseException pe) {
-				_log.warn("Parse " + dateText + ": " + pe.getMessage());
-			}
-
-			cal.setTime(postDate);
-
-			int displayDateMonth = cal.get(Calendar.MONTH);
-			int displayDateDay = cal.get(Calendar.DAY_OF_MONTH);
-			int displayDateYear = cal.get(Calendar.YEAR);
-			int displayDateHour = cal.get(Calendar.HOUR_OF_DAY);
-			int displayDateMinute = cal.get(Calendar.MINUTE);
-
-			String statusText = itemEl.elementTextTrim(
-				SAXReaderUtil.createQName("status", _wpNs));
-
-			boolean draft = statusText.equalsIgnoreCase("draft");
-
-			String pingStatusText = itemEl.elementTextTrim(
-				SAXReaderUtil.createQName("ping_status", _wpNs));
-
-			boolean allowTrackbacks =
-				pingStatusText.equalsIgnoreCase("open");
-
-			String[] tagsEntries = null;
-
-			String categoryText = itemEl.elementTextTrim("category");
-
-			if (Validator.isNotNull(categoryText)) {
-				tagsEntries = new String[] {categoryText};
-			}
-
-			BlogsEntry entry = null;
-
-			try {
-				entry = BlogsEntryLocalServiceUtil.addEntry(
-					userId, plid, title, content.toString(),
-					displayDateMonth, displayDateDay,
-					displayDateYear, displayDateHour,
-					displayDateMinute, draft, allowTrackbacks, null,
-					tagsEntries, true, true, null);
-			}
-			catch (Exception e) {
-				_log.error(
-					"Entry " + title + ": " + e.getMessage());
-
-				continue;
-			}
-
-			MBMessageDisplay messageDisplay =
-				MBMessageLocalServiceUtil.getDiscussionMessageDisplay(
-					userId, BlogsEntry.class.getName(), entry.getEntryId());
-
-			Map<Long, Long> messageIdMap = new HashMap<Long, Long>();
-
-			List<Node> commentNodes = itemEl.selectNodes(
-				"wp:comment", "wp:comment_parent/text()");
-
-			long groupId = context.getGroupId();
-
-			for (Node commentNode : commentNodes) {
-				Element commentEl = (Element)commentNode;
-
-				long commentId = GetterUtil.getLong(
-					commentEl.elementTextTrim(
-						SAXReaderUtil.createQName("comment_id", _wpNs)));
-
-				String commentContent = commentEl.elementTextTrim(
-					SAXReaderUtil.createQName("comment_content", _wpNs));
-
-				if (Validator.isNull(commentContent)) {
-					continue;
-				}
-
-				String commentAuthor = commentEl.elementTextTrim(
-					SAXReaderUtil.createQName(
-						"comment_author", _wpNs));
-
-				commentAuthor = commentAuthor.substring(
-					0, Math.min(75, commentAuthor.length()));
-
-				long commentParentId = GetterUtil.getLong(
-					commentEl.elementTextTrim(
-						SAXReaderUtil.createQName("comment_parent", _wpNs)));
-
-				if (commentParentId == 0) {
-					commentParentId =
-						messageDisplay.getMessage().getMessageId();
-				}
-				else {
-					commentParentId = messageIdMap.get(commentParentId);
-				}
-
-				MBMessage message =
-					MBMessageLocalServiceUtil.addDiscussionMessage(
-						defaultUser.getUserId(), commentAuthor, groupId,
-						BlogsEntry.class.getName(), entry.getEntryId(),
-						messageDisplay.getThread().getThreadId(),
-						commentParentId, null, commentContent);
-
-				messageIdMap.put(commentId, message.getMessageId());
-			}
 		}
 	}
 
@@ -392,18 +192,6 @@ public class BlogsPortletDataHandlerImpl implements PortletDataHandler {
 		sb.append("/entries/");
 		sb.append(entry.getEntryId());
 		sb.append(".xml");
-
-		return sb.toString();
-	}
-
-	protected String getWordpressPath(
-		PortletDataContext context, String fileName) {
-
-		StringBuilder sb = new StringBuilder();
-
-		sb.append(context.getImportPortletPath(PortletKeys.BLOGS));
-		sb.append(StringPool.SLASH);
-		sb.append(fileName);
 
 		return sb.toString();
 	}
@@ -491,49 +279,6 @@ public class BlogsPortletDataHandlerImpl implements PortletDataHandler {
 		}
 	}
 
-	protected Map<String, Long> getWordpressUserMap(PortletDataContext context) {
-
-		Map<String, Long> userMap = new HashMap<String, Long>();
-
-		String path = getWordpressPath(context, "wordpress-user-map.xml");
-
-		String fileData = context.getZipEntryAsString(path);
-
-		if (Validator.isNull(fileData)) {
-			return userMap;
-		}
-
-		try {
-			Document wordpressUserMapDoc = SAXReaderUtil.read(fileData);
-
-			Element root = wordpressUserMapDoc.getRootElement();
-
-			List<Element> userEls = root.elements("wordpress-user");
-
-			for (Element userEl : userEls) {
-				try {
-					User user = UserLocalServiceUtil.getUserByEmailAddress(
-						context.getCompanyId(),
-						userEl.attributeValue("email-address"));
-
-					userMap.put(
-						userEl.getTextTrim(),
-						user.getUserId());
-				}
-				catch (Exception e) {
-				}
-
-			}
-		}
-		catch(DocumentException de) {
-		}
-
-		return userMap;
-	}
-
-	private static Log _log = LogFactory.getLog(
-		BlogsPortletDataHandlerImpl.class);
-
 	private static final String _NAMESPACE = "blogs";
 
 	private static final PortletDataHandlerBoolean _entries =
@@ -547,20 +292,5 @@ public class BlogsPortletDataHandlerImpl implements PortletDataHandler {
 
 	private static final PortletDataHandlerBoolean _tags =
 		new PortletDataHandlerBoolean(_NAMESPACE, "tags");
-
-	private static final PortletDataHandlerBoolean _wordpress =
-		new PortletDataHandlerBoolean(_NAMESPACE, "wordpress");
-
-	private static final Namespace _contentNs = SAXReaderUtil.createNamespace(
-		"content", "http://purl.org/rss/1.0/modules/content/");
-
-	private static final Namespace _wfwNs = SAXReaderUtil.createNamespace(
-		"wfw", "http://wellformedweb.org/CommentAPI/");
-
-	private static final Namespace _dcNs = SAXReaderUtil.createNamespace(
-		"dc", "http://purl.org/dc/elements/1.1/");
-
-	private static final Namespace _wpNs = SAXReaderUtil.createNamespace(
-		"wp", "http://wordpress.org/export/1.0/");
 
 }
