1e277da699ffee77817ac41a0cd8ed6a1a15ad4a
Brian Chan <brian.chan@liferay.com>
2008-11-06 10:14:31 +0000

    LPS-453 Rollback
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@22433 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/tags/service/impl/TagsEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/tags/service/impl/TagsEntryLocalServiceImpl.java
@@ -110,7 +110,7 @@ public class TagsEntryLocalServiceImpl extends TagsEntryLocalServiceBaseImpl {
 		// Entry
 
 		User user = userPersistence.findByPrimaryKey(userId);
-		name = name.trim();
+		name = name.trim().toLowerCase();
 
 		if (Validator.isNull(vocabularyName)) {
 			vocabularyName = PropsValues.TAGS_VOCABULARY_DEFAULT;
@@ -121,6 +121,13 @@ public class TagsEntryLocalServiceImpl extends TagsEntryLocalServiceBaseImpl {
 		}
 
 		Date now = new Date();
+
+		validate(name);
+
+		if (hasEntry(groupId, name)) {
+			throw new DuplicateEntryException(
+				"A tag entry with the name " + name + " already exists");
+		}
 
 		long entryId = counterLocalService.increment();
 
@@ -143,6 +150,8 @@ public class TagsEntryLocalServiceImpl extends TagsEntryLocalServiceBaseImpl {
 			entry.setParentEntryId(TagsEntryConstants.DEFAULT_PARENT_ENTRY_ID);
 		}
 
+		entry.setName(name);
+
 		TagsVocabulary vocabulary = null;
 
 		try {
@@ -161,19 +170,6 @@ public class TagsEntryLocalServiceImpl extends TagsEntryLocalServiceBaseImpl {
 		}
 
 		entry.setVocabularyId(vocabulary.getVocabularyId());
-
-		if (vocabulary.isFolksonomy()) {
-			name = name.toLowerCase();
-		}
-
-		validate(name);
-
-		if (hasEntry(groupId, name)) {
-			throw new DuplicateEntryException(
-				"A tag entry with the name " + name + " already exists");
-		}
-
-		entry.setName(name);
 
 		tagsEntryPersistence.update(entry, false);
 
@@ -532,13 +528,21 @@ public class TagsEntryLocalServiceImpl extends TagsEntryLocalServiceBaseImpl {
 
 		// Entry
 
-		name = name.trim();
+		name = name.trim().toLowerCase();
 
 		if (Validator.isNull(vocabularyName)) {
 			vocabularyName = PropsValues.TAGS_VOCABULARY_DEFAULT;
 		}
 
+		validate(name);
+
 		TagsEntry entry = tagsEntryPersistence.findByPrimaryKey(entryId);
+
+		if (!entry.getName().equals(name) &&
+			hasEntry(entry.getGroupId(), name)) {
+
+			throw new DuplicateEntryException();
+		}
 
 		entry.setModifiedDate(new Date());
 
@@ -551,6 +555,8 @@ public class TagsEntryLocalServiceImpl extends TagsEntryLocalServiceBaseImpl {
 		else {
 			entry.setParentEntryId(TagsEntryConstants.DEFAULT_PARENT_ENTRY_ID);
 		}
+
+		entry.setName(name);
 
 		TagsVocabulary vocabulary = null;
 
@@ -570,32 +576,6 @@ public class TagsEntryLocalServiceImpl extends TagsEntryLocalServiceBaseImpl {
 		}
 
 		entry.setVocabularyId(vocabulary.getVocabularyId());
-
-		if (vocabulary.isFolksonomy()) {
-			name = name.toLowerCase();
-
-			if (!entry.getName().equals(name) &&
-				hasEntry(entry.getGroupId(), name)) {
-
-				throw new DuplicateEntryException();
-			}
-		}
-		else {
-			if (!entry.getName().equals(name)) {
-				TagsEntry existingEntry = tagsEntryPersistence.fetchByG_N(
-					entry.getGroupId(), name);
-
-				if (existingEntry != null &&
-				   (existingEntry.getEntryId() != entry.getEntryId())) {
-
-					throw new DuplicateEntryException();
-				}
-			}
-		}
-
-		validate(name);
-
-		entry.setName(name);
 
 		tagsEntryPersistence.update(entry, false);
 
