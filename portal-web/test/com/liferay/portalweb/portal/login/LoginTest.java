968f08c47de5ed95d1ad01cf01562dd74a4c338e
JR Houn <jr.houn@liferay.com>
2008-11-11 22:57:00 +0000

    LPS-359
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@22612 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/test/com/liferay/portalweb/portal/login/LoginTest.java
+++ portal-web/test/com/liferay/portalweb/portal/login/LoginTest.java
@@ -36,29 +36,11 @@ public class LoginTest extends BaseTestCase {
 		selenium.open("/web/guest/home");
 		selenium.click(RuntimeVariables.replace("link=Sign In"));
 		selenium.waitForPageToLoad("30000");
-		selenium.type("login", RuntimeVariables.replace("test@liferay.com"));
-		selenium.type("password", RuntimeVariables.replace("test"));
-		selenium.click("rememberMeCheckbox");
-		selenium.click(RuntimeVariables.replace(
-				"document.getElementById('tabs1already-registeredTabsSection').getElementsByTagName('div')[0].getElementsByTagName('form')[0].getElementsByTagName('fieldset')[0].getElementsByTagName('div')[3].getElementsByTagName('input')[0]"));
+		selenium.type("_58_login", RuntimeVariables.replace("test@liferay.com"));
+		selenium.type("_58_password", RuntimeVariables.replace("test"));
+		selenium.click("_58_rememberMeCheckbox");
+		selenium.click(RuntimeVariables.replace("//input[@value='Sign In']"));
 		selenium.waitForPageToLoad("30000");
-
-		for (int second = 0;; second++) {
-			if (second >= 60) {
-				fail("timeout");
-			}
-
-			try {
-				if (selenium.isElementPresent("//input[@value='I Agree']")) {
-					break;
-				}
-			}
-			catch (Exception e) {
-			}
-
-			Thread.sleep(1000);
-		}
-
 		selenium.click(RuntimeVariables.replace("//input[@value='I Agree']"));
 		selenium.waitForPageToLoad("30000");
 		selenium.type("reminderQueryAnswer", RuntimeVariables.replace("Test"));
