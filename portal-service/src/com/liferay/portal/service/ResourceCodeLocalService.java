bddfedd94a86ec526447e11f5bcb045947e68824
Michael Young <michael.young@liferay.com>
2008-12-02 14:44:42 +0000

    LPS-829 Correct Fix
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@23356 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-service/src/com/liferay/portal/service/ResourceCodeLocalService.java
+++ portal-service/src/com/liferay/portal/service/ResourceCodeLocalService.java
@@ -76,7 +76,6 @@ public interface ResourceCodeLocalService {
 		com.liferay.portal.kernel.dao.orm.DynamicQuery dynamicQuery, int start,
 		int end) throws com.liferay.portal.SystemException;
 
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.ResourceCode getResourceCode(long codeId)
 		throws com.liferay.portal.SystemException,
 			com.liferay.portal.PortalException;
@@ -93,7 +92,6 @@ public interface ResourceCodeLocalService {
 		com.liferay.portal.model.ResourceCode resourceCode)
 		throws com.liferay.portal.SystemException;
 
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.ResourceCode getResourceCode(
 		long companyId, java.lang.String name, int scope)
 		throws com.liferay.portal.SystemException;
