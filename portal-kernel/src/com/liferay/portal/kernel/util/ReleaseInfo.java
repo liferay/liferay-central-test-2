8c45792d2f31264bc7d93877babfb01f7ec9dcfd
Brian Chan <brian.chan@liferay.com>
2008-12-03 22:26:42 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@23453 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java
@@ -34,7 +34,7 @@ import java.util.Date;
  */
 public class ReleaseInfo {
 
-	static String name = "Liferay Portal";
+	static String name = "Liferay Portal Standard Edition";
 
 	static String version = "5.2.0";
 
