b6ba1481a4debd75a595281a4383ff5c44aaa6c8
Samuel Kong <samuel.kong@liferay.com>
2008-12-10 01:28:44 +0000

    LPS-659
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@23628 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-impl/src/com/liferay/portal/upgrade/v5_2_0/UpgradeDocumentLibrary.java
@@ -0,0 +1,129 @@
+/**
+ * Copyright (c) 2000-2008 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portal.upgrade.v5_2_0;
+
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.model.Layout;
+import com.liferay.portal.service.LayoutLocalServiceUtil;
+import com.liferay.portal.upgrade.UpgradeException;
+import com.liferay.portal.upgrade.UpgradeProcess;
+import com.liferay.portlet.PortletPreferencesImpl;
+import com.liferay.portlet.PortletPreferencesSerializer;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * <a href="UpgradeDocumentLibrary.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Samuel Kong
+ *
+ */
+public class UpgradeDocumentLibrary extends UpgradeProcess {
+
+	public void upgrade() throws UpgradeException {
+		_log.info("Upgrading");
+
+		try {
+			doUpgrade();
+		}
+		catch (Exception e) {
+			throw new UpgradeException(e);
+		}
+	}
+
+	protected void doUpgrade() throws Exception {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = DataAccess.getConnection();
+
+			ps = con.prepareStatement(
+				"select portletPreferencesId, ownerId, ownerType, plid, " +
+					"portletId, preferences from PortletPreferences where " +
+						"portletId = 20");
+
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				long portletPreferencesId = rs.getLong("portletPreferencesId");
+				long ownerId = rs.getLong("ownerId");
+				int ownerType = rs.getInt("ownerType");
+				long plid = rs.getLong("plid");
+				String portletId = rs.getString("portletId");
+				String preferences = rs.getString("preferences");
+
+				Layout layout = LayoutLocalServiceUtil.getLayout(plid);
+
+				String newPreferences = upgradePreferences(
+					layout.getCompanyId(), ownerId, ownerType, plid, portletId,
+					preferences);
+
+				ps = con.prepareStatement(
+					"update PortletPreferences set preferences = ? where " +
+						"portletPreferencesId = ?");
+
+				ps.setString(1, newPreferences);
+				ps.setLong(2, portletPreferencesId);
+
+				ps.executeUpdate();
+
+				ps.close();
+			}
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+	}
+
+	protected String upgradePreferences(
+			long companyId, long ownerId, int ownerType, long plid,
+			String portletId, String xml)
+		throws Exception {
+
+		PortletPreferencesImpl preferences =
+			PortletPreferencesSerializer.fromXML(
+				companyId, ownerId, ownerType, plid, portletId, xml);
+
+		String fileEntryColumns = preferences.getValue(
+			"fileEntryColumns", StringPool.BLANK);
+
+		fileEntryColumns = StringUtil.replace(
+			fileEntryColumns, "document", "name");
+
+		preferences.setValue("fileEntryColumns", fileEntryColumns);
+
+		return PortletPreferencesSerializer.toXML(preferences);
+	}
+
+	private static Log _log = LogFactory.getLog(UpgradeDocumentLibrary.class);
+
+}
