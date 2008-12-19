7f42611a6b76f0ff9798824ff8dfecba6eca5c3b
JR Houn <jr.houn@liferay.com>
2008-12-19 19:33:58 +0000

    Minor Navigation portlet testing update.
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@24048 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/test/com/liferay/portalweb/portlet/navigation/AddPortletTest.java
+++ portal-web/test/com/liferay/portalweb/portlet/navigation/AddPortletTest.java
@@ -40,7 +40,8 @@ public class AddPortletTest extends BaseTestCase {
 			}
 
 			try {
-				if (selenium.isElementPresent("//div[@id=\"CMS-Navigation\"]")) {
+				if (selenium.isElementPresent(
+							"//div[@id='ContentManagement-CategoriesNavigation']/p/a")) {
 					break;
 				}
 			}
@@ -50,7 +51,8 @@ public class AddPortletTest extends BaseTestCase {
 			Thread.sleep(1000);
 		}
 
-		selenium.click("//div[@id=\"CMS-Navigation\"]/p/a");
+		selenium.click(
+			"//div[@id='ContentManagement-CategoriesNavigation']/p/a");
 
 		for (int second = 0;; second++) {
 			if (second >= 60) {
