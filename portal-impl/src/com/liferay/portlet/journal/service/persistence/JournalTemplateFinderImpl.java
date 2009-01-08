7cfa82f49ac09ee1a91f112e2eaa78cd6f95cedf
prakash reddy <prakash.r@sun.com>
2009-01-08 20:03:42 +0000

    LPS-1373
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@24688 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/journal/service/persistence/JournalTemplateFinderImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/persistence/JournalTemplateFinderImpl.java
@@ -45,6 +45,7 @@ import java.util.List;
  *
  * @author Brian Wing Shun Chan
  * @author Bruno Farache
+ * @author Prakash Reddy
  *
  */
 public class JournalTemplateFinderImpl
@@ -250,15 +251,24 @@ public class JournalTemplateFinderImpl
 				sql, "templateId", StringPool.LIKE, false, templateIds);
 
 			if (structureIdComparator.equals(StringPool.NOT_EQUAL)) {
-				String replaceWith =
+				String insertText =
 					"structureId != ? AND structureId IS NOT NULL";
 
 				if (CustomSQLUtil.isVendorOracle()) {
-					replaceWith = "structureId IS NOT NULL";
+					insertText = "structureId IS NOT NULL";
 				}
 
-				sql = StringUtil.replace(
-					sql, "structureId = ? [$AND_OR_NULL_CHECK$]", replaceWith);
+				insertText = StringUtil.add(
+					" AND (", insertText, StringPool.BLANK);
+
+				insertText = StringUtil.add(insertText, ") ", StringPool.BLANK);
+
+				String textToBeReplaced =
+					"(structureId = ? [$AND_OR_NULL_CHECK$]) " +
+					"[$AND_OR_CONNECTOR$]";
+
+				sql = StringUtil.replace(sql, textToBeReplaced, "");
+				sql = StringUtil.insert(sql, insertText, sql.indexOf("ORDER"));
 			}
 
 			sql = CustomSQLUtil.replaceKeywords(
@@ -283,6 +293,14 @@ public class JournalTemplateFinderImpl
 
 			qPos.add(templateIds, 2);
 
+			if (structureIdComparator.equals(StringPool.EQUAL)) {
+				qPos.add(structureId);
+				qPos.add(structureId);
+			}
+
+			qPos.add(names, 2);
+			qPos.add(descriptions, 2);
+
 			if (structureIdComparator.equals(StringPool.NOT_EQUAL)) {
 				if (CustomSQLUtil.isVendorOracle()) {
 				}
@@ -290,16 +308,6 @@ public class JournalTemplateFinderImpl
 					qPos.add(structureId);
 				}
 			}
-			else {
-				qPos.add(structureId);
-			}
-
-			if (structureIdComparator.equals(StringPool.EQUAL)) {
-				qPos.add(structureId);
-			}
-
-			qPos.add(names, 2);
-			qPos.add(descriptions, 2);
 
 			return (List<JournalTemplate>)QueryUtil.list(
 				q, getDialect(), start, end);
