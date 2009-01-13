ec5c8ef90289d935070bd4fc329f0de204855f40
Brian Chan <brian.chan@liferay.com>
2009-01-13 20:29:42 +0000

    LPS-1600
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@24959 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ util-taglib/src/com/liferay/taglib/util/GetUrlTag.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-2009 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.taglib.util;
+
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Time;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.webcache.WebCacheItem;
+import com.liferay.portal.kernel.webcache.WebCachePoolUtil;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.BodyTagSupport;
+
+/**
+ * <a href="GetUrlTag.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ *
+ */
+public class GetUrlTag extends BodyTagSupport {
+
+	public int doEndTag() throws JspException {
+		try {
+			WebCacheItem wci = new GetUrlWebCacheItem(_url, _expires);
+
+			String content = (String)WebCachePoolUtil.get(
+				GetUrlTag.class.getName() + StringPool.PERIOD + _url, wci);
+
+			if (Validator.isNotNull(_var)) {
+				pageContext.setAttribute(_var, content);
+			}
+			else {
+				pageContext.getOut().print(content);
+			}
+
+			return EVAL_PAGE;
+		}
+		catch (Exception e) {
+			throw new JspException(e);
+		}
+	}
+
+	public void setUrl(String url) {
+		_url = url;
+	}
+
+	public void setExpires(long expires) {
+		_expires = expires;
+	}
+
+	public void setVar(String var) {
+		_var = var;
+	}
+
+	private String _url;
+	private long _expires = Time.WEEK;
+	private String _var;
+
+}
---
+++ util-taglib/src/com/liferay/taglib/util/GetUrlWebCacheItem.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-2009 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.taglib.util;
+
+import com.liferay.portal.kernel.util.HttpUtil;
+import com.liferay.portal.kernel.webcache.WebCacheException;
+import com.liferay.portal.kernel.webcache.WebCacheItem;
+
+/**
+ * <a href="GetUrlWebCacheItem.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ *
+ */
+public class GetUrlWebCacheItem implements WebCacheItem {
+
+	public GetUrlWebCacheItem(String url, long refreshTime) {
+		_url = url;
+		_refreshTime = refreshTime;
+	}
+
+	public Object convert(String key) throws WebCacheException {
+		String url = _url;
+
+		String content = null;
+
+		try {
+			content = HttpUtil.URLtoString(_url);
+		}
+		catch (Exception e) {
+			throw new WebCacheException(url + " " + e.toString());
+		}
+
+		return content;
+	}
+
+	public long getRefreshTime() {
+		return _refreshTime;
+	}
+
+	private String _url;
+	private long _refreshTime;
+
+}
