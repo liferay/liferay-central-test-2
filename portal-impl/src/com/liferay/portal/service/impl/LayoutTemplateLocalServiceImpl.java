b3d09a0344fd0f986003705049cbad28e7419fff
Brian Chan <brian.chan@liferay.com>
2009-01-27 18:39:50 +0000

    LPS-1790 Rollback
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@25567 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/service/impl/LayoutTemplateLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutTemplateLocalServiceImpl.java
@@ -349,14 +349,6 @@ public class LayoutTemplateLocalServiceImpl
 
 			String layoutTemplateId = layoutTemplate.attributeValue("id");
 
-			for (ObjectValuePair<String, Boolean> ovp : layoutTemplateIds) {
-				if( ovp.getKey().equals( layoutTemplateId ) ) {
-					_log.error( "Ignoring Layout Template with duplicate id \"" 
-							+ layoutTemplateId + "\"." );
-					continue;
-				}
-			}
-
 			if (layoutTemplateIds != null) {
 				ObjectValuePair<String, Boolean> ovp =
 					new ObjectValuePair<String, Boolean>(
