a0cac072392358a4c722569ce0afbb4312429086
Michael Han <michael.han@liferay.com>
2009-01-31 04:58:34 +0000

    LPS-1857 Adding JMock classes for mock object testing Removing circular dependencies in the test code between portal-kernel and portal-impl and between util-java and portal-impl
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@25718 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-kernel/test/com/liferay/portal/kernel/messaging/jmx/DestinationManagerTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2000-2008 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portal.kernel.messaging.jmx;
+
+import com.liferay.portal.kernel.messaging.Destination;
+
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.management.MBeanServer;
+import java.lang.management.ManagementFactory;
+
+/**
+ * <a href="TestMessagingDestinationManager.java.html"><b><i>View
+ * Source</i></b></a>
+ *
+ * @author Michael C. Han
+ */
+@RunWith(JMock.class)
+public class DestinationManagerTest {
+    @Test()
+    public void testRegisterMBean()
+            throws Exception {
+        _mbeanServer.registerMBean(
+                new DestinationManager(_context.mock(Destination.class)),
+                DestinationManager.createObjectName("sample"));
+
+        Assert.assertTrue(
+                _mbeanServer.isRegistered(
+                        DestinationManager.createObjectName("sample")));
+    }
+
+    @Before
+    public void setup() {
+        _mbeanServer = ManagementFactory.getPlatformMBeanServer();
+        _context = new JUnit4Mockery();
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    private MBeanServer _mbeanServer;
+    private JUnit4Mockery _context;
+}
---
+++ portal-kernel/test/com/liferay/portal/kernel/messaging/jmx/DestinationStatisticsManagerTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2000-2008 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portal.kernel.messaging.jmx;
+
+import com.liferay.portal.kernel.messaging.Destination;
+
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
+
+import org.junit.After;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+
+import java.lang.management.ManagementFactory;
+
+import javax.management.MBeanServer;
+
+/**
+ * <a href="TestDestinationStatisticsManager.java.html"><b><i>View
+ * Source</i></b></a>
+ *
+ * @author Michael C. Han
+ */
+@RunWith(JMock.class)
+public class DestinationStatisticsManagerTest {
+
+    @Test()
+    public void testRegisterMBean()
+            throws Exception {
+        _mbeanServer.registerMBean(
+                new DestinationStatisticsManager(
+                        _context.mock(Destination.class)),
+                DestinationStatisticsManager.createObjectName("sample"));
+
+        assertTrue(
+                _mbeanServer.isRegistered(
+                        DestinationStatisticsManager.createObjectName(
+                                "sample")));
+    }
+
+    @Before
+    public void setup() {
+        _mbeanServer = ManagementFactory.getPlatformMBeanServer();
+        _context = new JUnit4Mockery();
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    private MBeanServer _mbeanServer;
+    private JUnit4Mockery _context;
+}
