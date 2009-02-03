7648dd4d21196105bba8f8c575dbb135814155fc
Brian Chan <brian.chan@liferay.com>
2009-02-03 03:59:36 +0000

    LPS-1898
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@25772 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/events/ServicePreAction.java
+++ portal-impl/src/com/liferay/portal/events/ServicePreAction.java
@@ -42,6 +42,7 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.ServerDetector;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
@@ -1314,6 +1315,10 @@ public class ServicePreAction extends Action {
 
 		boolean themeJsFastLoad = ParamUtil.getBoolean(
 			request, "js_fast_load", PropsValues.JAVASCRIPT_FAST_LOAD);
+
+		if (ServerDetector.isWebSphere()) {
+			themeJsFastLoad = false;
+		}
 
 		String lifecycle = ParamUtil.getString(request, "p_p_lifecycle");
 
