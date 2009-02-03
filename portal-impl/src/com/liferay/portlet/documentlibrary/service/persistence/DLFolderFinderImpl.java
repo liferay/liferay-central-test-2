0c3c75a39c7ca503a3002658c6e0e339d1ca5221
prakash reddy <prakash.r@sun.com>
2009-02-03 18:59:50 +0000

    LPS-1905
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@25786 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/persistence/DLFolderFinderImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/persistence/DLFolderFinderImpl.java
@@ -190,7 +190,7 @@ public class DLFolderFinderImpl
 
 			SQLQuery q = session.createSQLQuery(sql);
 
-			q.addScalar("folderId", Type.LONG);
+			q.addScalar("parentFolderId", Type.LONG);
 			q.addScalar("name", Type.STRING);
 			q.addScalar("title", Type.STRING);
 			q.addScalar("fileShortcutId", Type.LONG);
@@ -263,7 +263,7 @@ public class DLFolderFinderImpl
 
 			SQLQuery q = session.createSQLQuery(sql);
 
-			q.addScalar("folderId", Type.LONG);
+			q.addScalar("parentFolderId", Type.LONG);
 			q.addScalar("name", Type.STRING);
 			q.addScalar("title", Type.STRING);
 			q.addScalar("fileShortcutId", Type.LONG);
