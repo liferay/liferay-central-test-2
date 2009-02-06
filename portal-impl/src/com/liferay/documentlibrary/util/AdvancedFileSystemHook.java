4506f34ef39c964da81e9de7e0bdd93ab7b542e6
Jorge Ferrer <jorge.ferrer@liferay.com>
2009-02-06 14:27:43 +0000

    LPS-1976 New file system hook for document library for maximum scalability and easier maintenance
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@25926 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-impl/src/com/liferay/documentlibrary/util/AdvancedFileSystemHook.java
@@ -0,0 +1,120 @@
+/**
+ * Copyright (c) 2000-2008 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.documentlibrary.util;
+
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.io.File;
+
+/**
+ * <a href="AdvancedFileSystemHook.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Jorge Ferrer
+ */
+public class AdvancedFileSystemHook extends FileSystemHook {
+
+	protected void buildPath(StringBuilder sb, String fileNameFragment) {
+		if (fileNameFragment.length() <= 2) {
+			return;
+		}
+
+		if (getDepth(sb.toString()) > 3) {
+			return;
+		}
+		
+		sb.append(fileNameFragment.substring(0, 2) + StringPool.SLASH);
+		buildPath(sb, fileNameFragment.substring(2));
+	}
+
+	protected int getDepth(String path) {
+		String[] fragments = StringUtil.split(path, StringPool.SLASH);
+
+		return fragments.length;
+	}
+
+	protected File getDirNameDir(
+		long companyId, long repositoryId, String dirName) {
+
+		File repositoryDir = getRepositoryDir(companyId, repositoryId);
+
+		File fileNameDir = new File(
+			repositoryDir + StringPool.SLASH + dirName);
+
+		return fileNameDir;
+	}
+
+	protected File getFileNameVersionFile(
+		long companyId, long repositoryId, String fileName, double version) {
+
+		String ext = StringPool.PERIOD + FileUtil.getExtension(fileName);
+
+		int pos = fileName.lastIndexOf(StringPool.SLASH);
+
+		if (pos == -1) {
+			StringBuilder sb = new StringBuilder();
+
+			String fileNameFragment = removeExtension(fileName);
+
+			if (fileNameFragment.startsWith("DLFE-")) {
+				fileNameFragment = fileNameFragment.substring(5);
+				sb.append("DLFE" + StringPool.SLASH);
+			}
+
+			buildPath(sb, fileNameFragment);
+
+			File repositoryDir = getRepositoryDir(companyId, repositoryId);
+
+			return new File(
+				repositoryDir + StringPool.SLASH + sb.toString() +
+					StringPool.SLASH + fileNameFragment + ext +
+						StringPool.SLASH + fileNameFragment +
+							StringPool.UNDERLINE + version + ext);
+		}
+		else {
+			File fileNameDir = getDirNameDir(companyId, repositoryId, fileName);
+
+			String fileNameFragment = removeExtension(
+				fileName.substring(pos + 1));
+
+			File fileNameVersionFile = new File(
+				fileNameDir + StringPool.SLASH +
+					fileNameFragment + StringPool.UNDERLINE + version + ext);
+
+			return fileNameVersionFile;
+		}
+	}
+
+	protected String removeExtension(String fileName) {
+		String ext = FileUtil.getExtension(fileName);
+
+		if (ext != null) {
+			fileName = fileName.substring(
+				0, fileName.length() - ext.length() - 1);
+		}
+
+		return  fileName;
+	}
+
+}
