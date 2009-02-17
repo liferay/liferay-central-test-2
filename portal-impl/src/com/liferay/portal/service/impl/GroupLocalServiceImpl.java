e85a0d29e69cabcd66d09ade6fa9999af8035b1c
Brian Chan <brian.chan@liferay.com>
2009-02-17 18:52:01 +0000

    LPS-2114 Forgot to fix several uses of that method, which broke more things
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@26524 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -135,7 +135,7 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 		}
 
 		validateFriendlyURL(
-			groupId, user.getCompanyId(), classNameId, classPK, friendlyURL);
+			user.getCompanyId(), groupId, classNameId, classPK, friendlyURL);
 
 		Group group = groupPersistence.create(groupId);
 
@@ -663,7 +663,7 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 			group.getClassPK(), StringPool.BLANK, friendlyURL);
 
 		validateFriendlyURL(
-			group.getGroupId(), group.getCompanyId(), group.getClassNameId(),
+			group.getCompanyId(), group.getGroupId(), group.getClassNameId(),
 			group.getClassPK(), friendlyURL);
 
 		group.setFriendlyURL(friendlyURL);
@@ -697,7 +697,7 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 		}
 
 		validateFriendlyURL(
-			group.getGroupId(), group.getCompanyId(), group.getClassNameId(),
+			group.getCompanyId(), group.getGroupId(), group.getClassNameId(),
 			group.getClassPK(), friendlyURL);
 
 		group.setName(name);
