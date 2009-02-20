07063ff2450ac0a853eef3f76ec453f36c0a11bd
Brett Swaim <brett.swaim@liferay.com>
2009-02-20 02:47:18 +0000

    [LPS-2169] Moving thread permission checking to MBMessagePermission
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@26662 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/messageboards/service/permission/MBMessagePermission.java
+++ portal-impl/src/com/liferay/portlet/messageboards/service/permission/MBMessagePermission.java
@@ -28,8 +28,11 @@ import com.liferay.portal.security.auth.PrincipalException;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portlet.messageboards.model.MBCategory;
 import com.liferay.portlet.messageboards.model.MBMessage;
+import com.liferay.portlet.messageboards.model.MBThread;
 import com.liferay.portlet.messageboards.service.MBBanLocalServiceUtil;
 import com.liferay.portlet.messageboards.service.MBMessageLocalServiceUtil;
+import com.liferay.portlet.messageboards.service.MBMessageServiceUtil;
+import com.liferay.portlet.messageboards.service.MBThreadLocalServiceUtil;
 
 /**
  * <a href="MBMessagePermission.java.html"><b><i>View Source</i></b></a>
@@ -72,9 +75,11 @@ public class MBMessagePermission {
 	public static boolean contains(
 			PermissionChecker permissionChecker, MBMessage message,
 			String actionId)
-		throws SystemException {
+			throws SystemException, PortalException {
 
 		MBCategory category = message.getCategory();
+		MBThread thread = MBThreadLocalServiceUtil.getThread(message.getThreadId());
+		MBMessage rootMessage = MBMessageLocalServiceUtil.getMessage(thread.getRootMessageId());
 
 		long groupId = 0;
 
@@ -93,6 +98,12 @@ public class MBMessagePermission {
 				message.getMessageId(), message.getUserId(), actionId)) {
 
 			return true;
+		}
+
+		if (!permissionChecker.hasPermission(
+				groupId, MBMessage.class.getName(),
+				rootMessage.getMessageId(), actionId)) {
+			return false;
 		}
 
 		return permissionChecker.hasPermission(
