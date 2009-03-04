f61f13cf565c915d58222b8146f0eb47b8d5acc1
Amos Fong <amos.fong@liferay.com>
2009-03-04 23:59:15 +0000

    LPS-1606
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@27152 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/docroot/html/js/liferay/layout_exporter.js
+++ portal-web/docroot/html/js/liferay/layout_exporter.js
@@ -52,21 +52,25 @@ Liferay.LayoutExporter = {
 
 		var contents =
 			"<div>" +
-				"<form action='" + url + "' method='post'>" +
-					"<textarea name='" + namespace + "description' style='height: 100px; width: 284px;'></textarea><br /><br />";
+				"<form action='" + url + "' method='post'>";
 
 		if (reviewers.length > 0) {
-			contents += Liferay.Language.get('reviewer') + " <select name='" + namespace + "reviewUserId'>";
+			contents += "<textarea name='" + namespace + "description' style='height: 100px; width: 284px;'></textarea><br /><br />" +
+						Liferay.Language.get('reviewer') + " <select name='" + namespace + "reviewUserId'>";
 
 			for (var i = 0; i < reviewers.length; i++) {
 				contents += "<option value='" + reviewers[i].userId + "'>" + reviewers[i].fullName + "</option>";
 			}
 
-			contents += "</select><br /><br />";
+			contents += "</select><br /><br />" +
+						"<input type='submit' value='" + Liferay.Language.get('proceed') + "' />";
+		}
+		else {
+			contents += Liferay.Language.get('no-reviewers-were-found') + "<br />" +
+						Liferay.Language.get('please-contact-the-administrator-to-assign-reviewers') + "<br /><br />";
 		}
 
 		contents +=
-					"<input type='submit' value='" + Liferay.Language.get('proceed') + "' />" +
 					"<input type='button' value='" + Liferay.Language.get('cancel') + "' onClick='Expanse.Popup.close(this);' />" +
 				"</form>" +
 			"</div>";
