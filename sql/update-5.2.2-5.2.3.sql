d51da016e7a9a3df0d5b971a6621f9bec78f756c
Brian Chan <brian.chan@liferay.com>
2009-03-09 19:07:38 +0000

    LPS-2172 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@27367 05bdf26c-840f-0410-9ced-eb539d925f36

--- sql/update-5.2.2-5.2.3.sql
+++ sql/update-5.2.2-5.2.3.sql
@@ -1,3 +1,3 @@
 update Group_ set name = classPK where classPK > 0;
 
-update Region set regionCode='AB' where countryId=1 and name='Alberta';
+update Region set regionCode = 'AB' where countryId = 1 and name = 'Alberta';
