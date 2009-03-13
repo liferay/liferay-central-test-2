f16755e05749859a65ab8bdb7d559cfec1e397e5
Brian Chan <brian.chan@liferay.com>
2009-03-13 17:13:01 +0000

    LPS-2361 Upgrade sql
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@27739 05bdf26c-840f-0410-9ced-eb539d925f36

--- sql/update-5.2.2-5.2.3.sql
+++ sql/update-5.2.2-5.2.3.sql
@@ -1,3 +1,10 @@
 update Group_ set name = classPK where classPK > 0 and name = '';
 
 update Region set regionCode = 'AB' where countryId = 1 and name = 'Alberta';
+
+create table Shard (
+	shardId LONG not null primary key,
+	classNameId LONG,
+	classPK LONG,
+	name VARCHAR(75) null
+);
