c1cad1ff6a6efb5fcee11eb12dbda2bb3622efa1
Brian Chan <brian.chan@liferay.com>
2009-03-30 01:08:36 +0000

    LPS-2683
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@28621 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-impl/src/com/liferay/portal/cache/MemoryPortalCache.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-2009 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portal.cache;
+
+import com.liferay.portal.kernel.cache.PortalCache;
+
+import java.io.Serializable;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * <a href="MemoryPortalCache.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ *
+ */
+public class MemoryPortalCache implements PortalCache {
+
+	public MemoryPortalCache(int initialCapacity) {
+		 _map = new ConcurrentHashMap<String, Object>(initialCapacity);
+	}
+
+	public Object get(String key) {
+		return _map.get(key);
+	}
+
+	public void put(String key, Object obj) {
+		_map.put(key, obj);
+	}
+
+	public void put(String key, Object obj, int timeToLive) {
+		_map.put(key, obj);
+	}
+
+	public void put(String key, Serializable obj) {
+		_map.put(key, obj);
+	}
+
+	public void put(String key, Serializable obj, int timeToLive) {
+		_map.put(key, obj);
+	}
+
+	public void remove(String key) {
+		_map.remove(key);
+	}
+
+	public void removeAll() {
+		_map.clear();
+	}
+
+	private Map<String, Object> _map;
+
+}
---
+++ portal-impl/src/com/liferay/portal/cache/MemoryPortalCacheManager.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-2009 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portal.cache;
+
+import com.liferay.portal.kernel.cache.PortalCache;
+import com.liferay.portal.kernel.cache.PortalCacheManager;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * <a href="MemoryPortalCacheManager.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ *
+ */
+public class MemoryPortalCacheManager
+	implements InitializingBean, PortalCacheManager {
+
+	public void afterPropertiesSet() {
+		_cacheMap = new ConcurrentHashMap<String, PortalCache>(
+			_cacheManagerInitialCapacity);
+	}
+
+	public void clearAll() {
+		_cacheMap.clear();
+	}
+
+	public PortalCache getCache(String name) {
+		PortalCache cache = _cacheMap.get(name);
+
+		if (cache == null) {
+			cache = new MemoryPortalCache(_cacheInitialCapacity);
+
+			_cacheMap.put(name, cache);
+		}
+
+		return cache;
+	}
+
+	public void setCacheInitialCapacity(int cacheInitialCapacity) {
+		_cacheInitialCapacity = cacheInitialCapacity;
+	}
+
+	public void setCacheManagerInitialCapacity(
+		int cacheManagerInitialCapacity) {
+
+		_cacheManagerInitialCapacity = cacheManagerInitialCapacity;
+	}
+
+	private int _cacheInitialCapacity = 10000;
+	private int _cacheManagerInitialCapacity = 10000;
+	private Map<String, PortalCache> _cacheMap;
+
+}
