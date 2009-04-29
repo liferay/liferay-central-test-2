6ae468b5ede192ebac9c0eeafedfa24c7af306ee
Brian Chan <brian.chan@liferay.com>
2009-04-29 18:02:56 +0000

    LPS-3145 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@30584 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/tools/sql/PostgreSQLUtil.java
+++ portal-impl/src/com/liferay/portal/tools/sql/PostgreSQLUtil.java
@@ -111,7 +111,8 @@ public class PostgreSQLUtil extends DBUtil {
 				String[] template = buildColumnTypeTokens(line);
 
 				line = StringUtil.replace(
-					"alter table @table@ alter @old-column@ type @type@ USING @old-column@::@type@;",
+					"alter table @table@ alter @old-column@ type @type@ " +
+						"using @old-column@::@type@;",
 					REWORD_TEMPLATE, template);
 			}
 			else if (line.indexOf(DROP_PRIMARY_KEY) != -1) {
