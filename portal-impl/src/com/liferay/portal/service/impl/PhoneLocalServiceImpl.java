b9211f6a8424e8bcc0026855fd2812f2594eaf05
Brian Chan <brian.chan@liferay.com>
2009-05-04 03:47:36 +0000

    LPS-3185 Rollback, there must be a better way to register the type than to just not check it
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@30771 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/service/impl/PhoneLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/PhoneLocalServiceImpl.java
@@ -26,9 +26,6 @@ import com.liferay.portal.PhoneNumberException;
 import com.liferay.portal.PortalException;
 import com.liferay.portal.SystemException;
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.model.Account;
-import com.liferay.portal.model.Contact;
-import com.liferay.portal.model.Organization;
 import com.liferay.portal.model.Phone;
 import com.liferay.portal.model.User;
 import com.liferay.portal.model.impl.ListTypeImpl;
@@ -157,12 +154,7 @@ public class PhoneLocalServiceImpl extends PhoneLocalServiceBaseImpl {
 			classPK = phone.getClassPK();
 		}
 
-		if ((classNameId == PortalUtil.getClassNameId(Account.class)) ||
-			(classNameId == PortalUtil.getClassNameId(Contact.class)) ||
-			(classNameId == PortalUtil.getClassNameId(Organization.class))) {
-
-			listTypeService.validate(typeId, classNameId, ListTypeImpl.PHONE);
-		}
+		listTypeService.validate(typeId, classNameId, ListTypeImpl.PHONE);
 
 		validate(phoneId, companyId, classNameId, classPK, primary);
 	}
