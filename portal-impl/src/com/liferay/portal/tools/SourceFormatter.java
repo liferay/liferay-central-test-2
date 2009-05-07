00ae4e845f9d83fef1ab97b5af2bf2bdfd739a61
Brian Chan <brian.chan@liferay.com>
2009-05-07 19:33:16 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@31120 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/tools/SourceFormatter.java
+++ portal-impl/src/com/liferay/portal/tools/SourceFormatter.java
@@ -755,6 +755,7 @@ public class SourceFormatter {
 				"**\\service\\http\\*ServiceHttp.java",
 				"**\\service\\http\\*ServiceJSON.java",
 				"**\\service\\http\\*ServiceSoap.java",
+				"**\\service\\messaging\\*ClpMessageListener.java",
 				"**\\service\\persistence\\*Finder.java",
 				"**\\service\\persistence\\*Persistence.java",
 				"**\\service\\persistence\\*PersistenceImpl.java",
