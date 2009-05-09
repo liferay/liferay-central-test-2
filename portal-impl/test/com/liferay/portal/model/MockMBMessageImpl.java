e73b65beee99018a71059f238160fa12d781e3bb
Michael Han <michael.han@liferay.com>
2009-05-09 16:20:24 +0000

    LPS-1843 forgot to add proper impl
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@31250 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/test/com/liferay/portal/model/MockMBMessageImpl.java
+++ portal-impl/test/com/liferay/portal/model/MockMBMessageImpl.java
@@ -22,10 +22,287 @@
 
 package com.liferay.portal.model;
 
+import com.liferay.portal.PortalException;
+import com.liferay.portal.SystemException;
+import com.liferay.portlet.messageboards.model.MBCategory;
+import com.liferay.portlet.messageboards.model.MBMessage;
+import com.liferay.portlet.messageboards.model.MBThread;
+
+import java.util.Date;
+
 /**
  * <a href="MockMBMessageImpl.java.html"><b><i>View Source</i></b></a>
  *
  * @author Michael C. Han
  */
-public class MockMBMessageImpl {
+public class MockMBMessageImpl
+	extends MockBaseModelImpl<MBMessage> implements MBMessage {
+
+	public String getUuid() {
+		return _uuid;
+	}
+
+	public void setUuid(String uuid) {
+		_uuid = uuid;
+	}
+
+	public String getOriginalUuid() {
+		return _originalUuid;
+	}
+
+	public void setOriginalUuid(String originalUuid) {
+		_originalUuid = originalUuid;
+	}
+
+	public long getMessageId() {
+		return _messageId;
+	}
+
+	public void setMessageId(long messageId) {
+		_messageId = messageId;
+	}
+
+	public long getGroupId() {
+		return _groupId;
+	}
+
+	public void setGroupId(long groupId) {
+		_groupId = groupId;
+	}
+
+	public long getOriginalGroupId() {
+		return _originalGroupId;
+	}
+
+	public void setOriginalGroupId(long originalGroupId) {
+		_originalGroupId = originalGroupId;
+	}
+
+	public boolean isSetOriginalGroupId() {
+		return _setOriginalGroupId;
+	}
+
+	public void setSetOriginalGroupId(boolean setOriginalGroupId) {
+		_setOriginalGroupId = setOriginalGroupId;
+	}
+
+	public long getCompanyId() {
+		return _companyId;
+	}
+
+	public void setCompanyId(long companyId) {
+		_companyId = companyId;
+	}
+
+	public long getUserId() {
+		return _userId;
+	}
+
+	public void setUserId(long userId) {
+		_userId = userId;
+	}
+
+	public String getUserName() {
+		return _userName;
+	}
+
+	public void setUserName(String userName) {
+		_userName = userName;
+	}
+
+	public Date getCreateDate() {
+		return _createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		_createDate = createDate;
+	}
+
+	public Date getModifiedDate() {
+		return _modifiedDate;
+	}
+
+	public void setModifiedDate(Date modifiedDate) {
+		_modifiedDate = modifiedDate;
+	}
+
+	public long getClassNameId() {
+		return _classNameId;
+	}
+
+	public void setClassNameId(long classNameId) {
+		_classNameId = classNameId;
+	}
+
+	public long getClassPK() {
+		return _classPK;
+	}
+
+	public void setClassPK(long classPK) {
+		_classPK = classPK;
+	}
+
+	public long getCategoryId() {
+		return _categoryId;
+	}
+
+	public void setCategoryId(long categoryId) {
+		_categoryId = categoryId;
+	}
+
+	public long getThreadId() {
+		return _threadId;
+	}
+
+	public void setThreadId(long threadId) {
+		_threadId = threadId;
+	}
+
+	public long getParentMessageId() {
+		return _parentMessageId;
+	}
+
+	public void setParentMessageId(long parentMessageId) {
+		_parentMessageId = parentMessageId;
+	}
+
+	public String getSubject() {
+		return _subject;
+	}
+
+	public void setSubject(String subject) {
+		_subject = subject;
+	}
+
+	public String getBody() {
+		return _body;
+	}
+
+	public void setBody(String body) {
+		_body = body;
+	}
+
+	public boolean isAttachments() {
+		return _attachments;
+	}
+
+	public void setAttachments(boolean attachments) {
+		_attachments = attachments;
+	}
+
+	public boolean isAnonymous() {
+		return _anonymous;
+	}
+
+	public void setAnonymous(boolean anonymous) {
+		_anonymous = anonymous;
+	}
+
+	public double getPriority() {
+		return _priority;
+	}
+
+	public void setPriority(double priority) {
+		_priority = priority;
+	}
+
+	public String getUserUuid() throws SystemException {
+		return null;
+	}
+
+	public void setUserUuid(String userUuid) {
+
+	}
+
+	public MBCategory getCategory() {
+		return null;
+	}
+
+	public MBThread getThread() throws PortalException, SystemException {
+		return null;
+	}
+
+	public boolean isRoot() {
+		return false;
+	}
+
+	public boolean isReply() {
+		return false;
+	}
+
+	public boolean isDiscussion() {
+		return false;
+	}
+
+	public String getBody(boolean translate) {
+		return null;
+	}
+
+	public String getThreadAttachmentsDir() {
+		return null;
+	}
+
+	public String getAttachmentsDir() {
+		return null;
+	}
+
+	public void setAttachmentsDir(String attachmentsDir) {
+
+	}
+
+	public String[] getAttachmentsFiles()
+		throws PortalException, SystemException {
+		return new String[0];
+	}
+
+	public String[] getTagsEntries() throws SystemException {
+		return new String[0];
+	}
+
+	public long getPrimaryKey() {
+		return 0;
+	}
+
+	public void setPrimaryKey(long pk) {
+
+	}
+
+	public String getClassName() {
+		return null;
+	}
+
+	public boolean getAttachments() {
+		return isAttachments();  
+	}
+
+	public boolean getAnonymous() {
+		return isAnonymous();
+	}
+
+	public MBMessage toEscapedModel() {
+		return null;
+	}
+
+	private String _uuid;
+	private String _originalUuid;
+	private long _messageId;
+	private long _groupId;
+	private long _originalGroupId;
+	private boolean _setOriginalGroupId;
+	private long _companyId;
+	private long _userId;
+	private String _userName;
+	private Date _createDate;
+	private Date _modifiedDate;
+	private long _classNameId;
+	private long _classPK;
+	private long _categoryId;
+	private long _threadId;
+	private long _parentMessageId;
+	private String _subject;
+	private String _body;
+	private boolean _attachments;
+	private boolean _anonymous;
+	private double _priority;
+
 }
