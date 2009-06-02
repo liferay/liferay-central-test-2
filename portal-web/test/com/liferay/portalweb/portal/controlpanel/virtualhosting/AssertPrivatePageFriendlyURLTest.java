324ad2002fb3bd9d0316eeb28d4a8e3ed056bde5
Michael Hashimoto <michael.hashimoto@liferay.com>
2009-06-02 00:13:16 +0000

    LPS-3562
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@32410 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-web/test/com/liferay/portalweb/portal/controlpanel/virtualhosting/AssertPrivatePageFriendlyURLTest.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-2009 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portalweb.portal.controlpanel.virtualhosting;
+
+import com.liferay.portalweb.portal.BaseTestCase;
+import com.liferay.portalweb.portal.util.RuntimeVariables;
+
+/**
+ * <a href="AssertPrivatePageFriendlyURLTest.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ *
+ */
+public class AssertPrivatePageFriendlyURLTest extends BaseTestCase {
+	public void testAssertPrivatePageFriendlyURL() throws Exception {
+		selenium.open("/group/alpha/");
+		Thread.sleep(5000);
+		assertEquals("http://localhost:8080/group/alpha/",
+			selenium.getLocation());
+		assertEquals("Virtual Hosting Community", selenium.getText("//h1/span"));
+		assertTrue(selenium.isElementPresent("link=Private Page"));
+		selenium.click(RuntimeVariables.replace("link=Private Page"));
+		selenium.waitForPageToLoad("30000");
+		assertEquals("http://localhost:8080/group/alpha/private-page",
+			selenium.getLocation());
+	}
+}
---
+++ portal-web/test/com/liferay/portalweb/portal/controlpanel/virtualhosting/AssertPrivatePageHostURLTest.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-2009 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portalweb.portal.controlpanel.virtualhosting;
+
+import com.liferay.portalweb.portal.BaseTestCase;
+import com.liferay.portalweb.portal.util.RuntimeVariables;
+
+/**
+ * <a href="AssertPrivatePageHostURLTest.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ *
+ */
+public class AssertPrivatePageHostURLTest extends BaseTestCase {
+	public void testAssertPrivatePageHostURL() throws Exception {
+		int label = 1;
+
+		while (label >= 1) {
+			switch (label) {
+			case 1:
+				selenium.open("http://www.baker.com:8080/");
+				Thread.sleep(5000);
+
+				boolean LoggedOut = selenium.isElementPresent("_58_login");
+
+				if (!LoggedOut) {
+					label = 2;
+
+					continue;
+				}
+
+				selenium.typeKeys("_58_login",
+					RuntimeVariables.replace("test@lifera.com"));
+				selenium.type("_58_login",
+					RuntimeVariables.replace("test@liferay.com"));
+				selenium.typeKeys("_58_password",
+					RuntimeVariables.replace("test"));
+				selenium.type("_58_password", RuntimeVariables.replace("test"));
+				selenium.click("_58_rememberMeCheckbox");
+				selenium.click(RuntimeVariables.replace(
+						"//input[@value='Sign In']"));
+				selenium.waitForPageToLoad("30000");
+
+			case 2:
+				assertEquals("http://www.baker.com:8080/",
+					selenium.getLocation());
+				assertEquals("Virtual Hosting Community",
+					selenium.getText("//h1/span"));
+				assertTrue(selenium.isElementPresent("link=Private Page"));
+				selenium.click(RuntimeVariables.replace("link=Private Page"));
+				selenium.waitForPageToLoad("30000");
+				assertEquals("http://www.baker.com:8080/private-page",
+					selenium.getLocation());
+
+			case 100:
+				label = -1;
+			}
+		}
+	}
+}
