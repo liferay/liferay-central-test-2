72056f4eb6f7c2d815387b9fc5212a3bac711194
Brian Chan <brian.chan@liferay.com>
2009-06-10 06:55:49 +0000

    LPS-3585 Javac error
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@32770 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/MonitoringPortletManager.java
+++ portal-impl/src/com/liferay/portlet/MonitoringPortletManager.java
@@ -31,46 +31,46 @@ package com.liferay.portlet;
 public class MonitoringPortletManager implements MonitoringPortletManagerMBean {
 
 	public boolean isMonitoringPortletActionRequest() {
-		return MonitoringPortletImpl.isMonitoringPortletActionRequest();
+		return MonitoringPortlet.isMonitoringPortletActionRequest();
 	}
 
 	public boolean isMonitoringPortletEventRequest() {
-		return MonitoringPortletImpl.isMonitoringPortletEventRequest();
+		return MonitoringPortlet.isMonitoringPortletEventRequest();
 	}
 
 	public boolean isMonitoringPortletRenderRequest() {
-		return MonitoringPortletImpl.isMonitoringPortletRenderRequest();
+		return MonitoringPortlet.isMonitoringPortletRenderRequest();
 	}
 
 	public boolean isMonitoringPortletResourceRequest() {
-		return MonitoringPortletImpl.isMonitoringPortletResourceRequest();
+		return MonitoringPortlet.isMonitoringPortletResourceRequest();
 	}
 
 	public void setMonitoringPortletActionRequest(
 		boolean monitoringPortletActionRequest) {
 
-		MonitoringPortletImpl.setMonitoringPortletActionRequest(
+		MonitoringPortlet.setMonitoringPortletActionRequest(
 			monitoringPortletActionRequest);
 	}
 
 	public void setMonitoringPortletEventRequest(
 		boolean monitoringPortletEventRequest) {
 
-		MonitoringPortletImpl.setMonitoringPortletEventRequest(
+		MonitoringPortlet.setMonitoringPortletEventRequest(
 			monitoringPortletEventRequest);
 	}
 
 	public void setMonitoringPortletRenderRequest(
 		boolean monitoringPortletRenderRequest) {
 
-		MonitoringPortletImpl.setMonitoringPortletRenderRequest(
+		MonitoringPortlet.setMonitoringPortletRenderRequest(
 			monitoringPortletRenderRequest);
 	}
 
 	public void setMonitoringPortletResourceRequest(
 		boolean monitoringPortletResourceRequest) {
 
-		MonitoringPortletImpl.setMonitoringPortletResourceRequest(
+		MonitoringPortlet.setMonitoringPortletResourceRequest(
 			monitoringPortletResourceRequest);
 	}
 
