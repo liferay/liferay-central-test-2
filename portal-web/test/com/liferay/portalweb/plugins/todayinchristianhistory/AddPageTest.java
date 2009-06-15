cc0ef90eeeefb11b2a84b7de8019a8afcead801a
Michael Hashimoto <michael.hashimoto@liferay.com>
2009-06-15 20:35:34 +0000

    LPS-3601
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@33042 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/test/com/liferay/portalweb/plugins/todayinchristianhistory/AddPageTest.java
+++ portal-web/test/com/liferay/portalweb/plugins/todayinchristianhistory/AddPageTest.java
@@ -39,7 +39,8 @@ public class AddPageTest extends BaseTestCase {
 			}
 
 			try {
-				if (selenium.isElementPresent("//li[2]/a/span[1]")) {
+				if (selenium.isElementPresent(
+							"//div[@id='_145_myPlacesContainer']/ul/li[2]/a/span[1]")) {
 					break;
 				}
 			}
@@ -49,7 +50,8 @@ public class AddPageTest extends BaseTestCase {
 			Thread.sleep(1000);
 		}
 
-		selenium.click(RuntimeVariables.replace("//li[2]/a/span[1]"));
+		selenium.click(RuntimeVariables.replace(
+				"//div[@id='_145_myPlacesContainer']/ul/li[2]/a/span[1]"));
 		selenium.waitForPageToLoad("30000");
 		selenium.click(RuntimeVariables.replace("link=Manage Pages"));
 		selenium.waitForPageToLoad("30000");
