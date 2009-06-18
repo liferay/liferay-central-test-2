66e3b2b46bc08e30e949efe0bf511d964b67dc90
Brian Chan <brian.chan@liferay.com>
2009-06-18 21:46:21 +0000

    Missing copyright headers
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@33184 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/monitoring/jmx/PortletManager.java
+++ portal-impl/src/com/liferay/portal/monitoring/jmx/PortletManager.java
@@ -30,8 +30,7 @@ import com.liferay.portal.monitoring.statistics.portlet.ServerStatistics;
 import java.util.Set;
 
 /**
- * <a href="PortletManager.java.html"><b><i>View
- * Source</i></b></a>
+ * <a href="PortletManager.java.html"><b><i>View Source</i></b></a>
  *
  * @author Michael C. Han
  * @author Brian Wing Shun Chan
