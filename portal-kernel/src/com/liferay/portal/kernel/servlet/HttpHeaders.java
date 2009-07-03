e5eb412e74ca024bef3df62df252cee523aacf54
Brian Chan <brian.chan@liferay.com>
2009-07-03 01:26:42 +0000

    LPS-3955
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@33796 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-kernel/src/com/liferay/portal/kernel/servlet/HttpHeaders.java
+++ portal-kernel/src/com/liferay/portal/kernel/servlet/HttpHeaders.java
@@ -50,6 +50,8 @@ public interface HttpHeaders {
 
 	public static final String CONTENT_ID = "Content-ID";
 
+	public static final String CONTENT_LENGTH = "Content-Length";
+
 	public static final String CONTENT_TYPE = "Content-Type";
 
 	public static final String EXPIRES = "Expires";
