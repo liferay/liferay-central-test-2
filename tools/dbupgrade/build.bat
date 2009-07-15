0d35f525f6efdb4cbbd850003212ee34814ed44a
Brian Chan <brian.chan@liferay.com>
2009-07-15 17:07:05 +0000

    LPS-4119 Rolling back, going to use the same code but in a different directory, and will do as one commit so it's easier to backport
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@34447 05bdf26c-840f-0410-9ced-eb539d925f36

--- tools/dbupgrade/build.bat
+++
@@ -1,7 +0,0 @@
-copy ..\..\lib\global\*.jar lib
-copy ..\..\lib\portal\*.jar lib
-copy ..\..\lib\development\*.jar lib
-copy ..\..\portal-impl\*.jar lib
-copy ..\..\portal-kernel\*.jar lib
-copy ..\..\portal-service\*.jar lib
-copy ..\..\util-java\*.jar lib
--- tools/dbupgrade/build.sh
+++
@@ -1,7 +0,0 @@
-cp ../../lib/global/*.jar lib
-cp ../../lib/portal/*.jar lib
-cp ../../lib/development/*.jar lib
-cp ../../portal-impl/*.jar lib
-cp ../../portal-kernel/*.jar lib
-cp ../../portal-service/*.jar lib
-cp ../../util-java/*.jar lib
