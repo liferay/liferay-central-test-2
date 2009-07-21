7f3a0029671e12b308e1826e5898d0bc4523e274
Brian Chan <brian.chan@liferay.com>
2009-07-21 04:21:27 +0000

    Javadocs
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@34688 05bdf26c-840f-0410-9ced-eb539d925f36

--- tools/ext_tmpl/ext-impl/src/com/ext/portlet/reports/service/persistence/ReportsEntryPersistenceImpl.java
+++ tools/ext_tmpl/ext-impl/src/com/ext/portlet/reports/service/persistence/ReportsEntryPersistenceImpl.java
@@ -186,6 +186,9 @@ public class ReportsEntryPersistenceImpl extends BasePersistenceImpl
         return reportsEntry;
     }
 
+    /**
+     * @deprecated Use <code>update(ReportsEntry reportsEntry, boolean merge)</code>.
+     */
     public ReportsEntry update(ReportsEntry reportsEntry)
         throws SystemException {
         if (_log.isWarnEnabled()) {
@@ -196,6 +199,19 @@ public class ReportsEntryPersistenceImpl extends BasePersistenceImpl
         return update(reportsEntry, false);
     }
 
+    /**
+     * Add, update, or merge, the entity. This method also calls the model
+     * listeners to trigger the proper events associated with adding, deleting,
+     * or updating an entity.
+     *
+     * @param                reportsEntry the entity to add, update, or merge
+     * @param                merge boolean value for whether to merge the entity. The
+     *                                default value is false. Setting merge to true is more
+     *                                expensive and should only be true when reportsEntry is
+     *                                transient. See LEP-5473 for a detailed discussion of this
+     *                                method.
+     * @return                true if the portlet can be displayed via Ajax
+     */
     public ReportsEntry update(ReportsEntry reportsEntry, boolean merge)
         throws SystemException {
         boolean isNew = reportsEntry.isNew();
