99fd8b13d0ca535d57eda5f6422c3ab14cbd617f
Brian Chan <brian.chan@liferay.com>
2009-07-31 01:49:10 +0000

    Javadocs
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@35177 05bdf26c-840f-0410-9ced-eb539d925f36

--- classes/Test.java
+++ classes/Test.java
@@ -20,6 +20,11 @@
  * SOFTWARE.
  */
 
+/**
+ * <a href="Test.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ */
 public class Test {
 
 	public static void main(String[] args) throws Exception {
