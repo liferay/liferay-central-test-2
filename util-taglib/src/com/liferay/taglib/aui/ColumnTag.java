d0c2942caab70db649806baeb513a79890cf2b25
Julio Camarero <julio.camarero@liferay.com>
2009-08-04 15:38:50 +0000

    LPS-4439 Add aui:layout and aui:column
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@35331 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ util-taglib/src/com/liferay/taglib/aui/ColumnTag.java
@@ -0,0 +1,155 @@
+/**
+ * Copyright (c) 2000-2009 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.taglib.aui;
+
+import com.liferay.portal.kernel.servlet.PortalIncludeUtil;
+import com.liferay.portal.kernel.util.ServerDetector;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.taglib.util.IncludeTag;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.DynamicAttributes;
+
+/**
+ * <a href="ColumnTag.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Julio Camarero
+ * @author Jorge Ferrer
+ * @author Brian Wing Shun Chan
+ */
+public class ColumnTag extends IncludeTag implements DynamicAttributes {
+
+	public int doEndTag() throws JspException {
+		try{
+			PortalIncludeUtil.include(pageContext, getEndPage());
+
+			return EVAL_PAGE;
+		}
+		catch (Exception e) {
+			throw new JspException(e);
+		}
+		finally {
+			if (!ServerDetector.isResin()) {
+				_cssClass = null;
+				_columnWidth = 0;
+				_dynamicAttributes.clear();
+				_endPage = null;
+				_first = false;
+				_last = false;
+				_startPage = null;
+			}
+		}
+	}
+
+	public int doStartTag() throws JspException {
+		try{
+			HttpServletRequest request =
+				(HttpServletRequest)pageContext.getRequest();
+
+			request.setAttribute(
+				"aui:column:columnWidth", String.valueOf(_columnWidth));
+			request.setAttribute("aui:column:cssClass", _cssClass);
+			request.setAttribute(
+				"aui:column:dynamicAttributes", _dynamicAttributes);
+			request.setAttribute(
+				"aui:column:first", String.valueOf(_first));
+			request.setAttribute(
+				"aui:column:last", String.valueOf(_last));
+
+			PortalIncludeUtil.include(pageContext, getStartPage());
+
+			return EVAL_BODY_INCLUDE;
+		}
+		catch (Exception e) {
+			throw new JspException(e);
+		}
+	}
+
+	public String getEndPage() {
+		if (Validator.isNull(_endPage)) {
+			return _END_PAGE;
+		}
+		else {
+			return _endPage;
+		}
+	}
+
+	public String getStartPage() {
+		if (Validator.isNull(_startPage)) {
+			return _START_PAGE;
+		}
+		else {
+			return _startPage;
+		}
+	}
+
+	public void setColumnWidth(int columnWidth) {
+		_columnWidth = columnWidth;
+	}
+
+	public void setCssClass(String cssClass) {
+		_cssClass = cssClass;
+	}
+
+	public void setDynamicAttribute(
+		String uri, String localName, Object value) {
+
+		_dynamicAttributes.put(localName, value);
+	}
+
+	public void setEndPage(String endPage) {
+		_endPage = endPage;
+	}
+
+	public void setFirst(boolean first) {
+		_first = first;
+	}
+
+	public void setLast(boolean last) {
+		_last = last;
+	}
+
+	public void setStartPage(String startPage) {
+		_startPage = startPage;
+	}
+
+	private static final String _END_PAGE =
+		"/html/taglib/aui/column/end.jsp";
+
+	private static final String _START_PAGE =
+		"/html/taglib/aui/column/start.jsp";
+
+	private int _columnWidth;
+	private String _cssClass;
+	private Map<String, Object> _dynamicAttributes =
+		new HashMap<String, Object>();
+	private String _endPage;
+	private boolean _first;
+	private boolean _last;
+	private String _startPage;
+
+}
