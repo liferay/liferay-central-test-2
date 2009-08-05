dad6032ff3f48ac35812bf60b0fdb880dc1e907a
Michael Hashimoto <michael.hashimoto@liferay.com>
2009-08-05 22:16:58 +0000

    LPS-4459
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@35390 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/test/com/liferay/portalweb/portal/ControlPanelTestSuite.java
+++ portal-web/test/com/liferay/portalweb/portal/ControlPanelTestSuite.java
@@ -23,6 +23,7 @@
 package com.liferay.portalweb.portal;
 
 import com.liferay.portalweb.portal.controlpanel.admin.AdminTests;
+import com.liferay.portalweb.portal.controlpanel.blogs.BlogsTests;
 import com.liferay.portalweb.portal.controlpanel.bookmarks.BookmarksTests;
 import com.liferay.portalweb.portal.controlpanel.calendar.CalendarTests;
 import com.liferay.portalweb.portal.controlpanel.communities.CommunitiesTests;
@@ -48,6 +49,7 @@ public class ControlPanelTestSuite extends BaseTests {
 
 		testSuite.addTest(LoginTests.suite());
 		testSuite.addTest(AdminTests.suite());
+		testSuite.addTest(BlogsTests.suite());
 		testSuite.addTest(BookmarksTests.suite());
 		testSuite.addTest(CalendarTests.suite());
 		testSuite.addTest(CommunitiesTests.suite());
