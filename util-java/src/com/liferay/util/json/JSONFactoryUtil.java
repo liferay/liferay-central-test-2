c0a940b66aa0ec016233021101d56748a5cd9851
Brian Chan <brian.chan@liferay.com>
2009-08-13 21:30:39 +0000

    LPS-4534
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@35727 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ util-java/src/com/liferay/util/json/JSONFactoryUtil.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2000-2009 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.util.json;
+
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import org.jabsorb.JSONSerializer;
+import org.jabsorb.serializer.MarshallException;
+import org.jabsorb.serializer.UnmarshallException;
+
+/**
+ * <a href="JSONFactoryUtil.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ */
+public class JSONFactoryUtil {
+
+	public static Object deserialize(JSONObject jsonObj) {
+		return _instance._deserialize(jsonObj);
+	}
+
+	public static Object deserialize(String json) {
+		return _instance._deserialize(json);
+	}
+
+	public static String serialize(Object obj) {
+		return _instance._serialize(obj);
+	}
+
+	private JSONFactoryUtil() {
+		_serializer = new JSONSerializer();
+
+		 try {
+			 _serializer.registerDefaultSerializers();
+		 }
+		 catch (Exception e) {
+			 _log.error(e, e);
+		 }
+	}
+
+	private Object _deserialize(JSONObject jsonObj) {
+		return _deserialize(jsonObj.toString());
+	}
+
+	private Object _deserialize(String json) {
+		try {
+			return _serializer.fromJSON(json);
+		}
+		catch (UnmarshallException ue) {
+			 _log.error(ue, ue);
+
+			throw new IllegalStateException("Unable to deserialize oject", ue);
+		}
+	}
+
+	private String _serialize(Object obj) {
+		try {
+			return _serializer.toJSON(obj);
+		}
+		catch (MarshallException me) {
+			_log.error(me, me);
+
+			throw new IllegalStateException("Unable to serialize oject", me);
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(JSONFactoryUtil.class);
+
+	private static JSONFactoryUtil _instance = new JSONFactoryUtil();
+
+	private JSONSerializer _serializer;
+
+}
