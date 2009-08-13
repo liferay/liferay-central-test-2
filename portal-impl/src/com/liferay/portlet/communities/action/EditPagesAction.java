2c7719155be55ed723e108bc7bc8c7be0bea8a1c
Douglas Wong <douglas.wong@liferay.com>
2009-08-13 00:18:12 +0000

    LPS-4488
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@35672 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/communities/action/EditPagesAction.java
+++ portal-impl/src/com/liferay/portlet/communities/action/EditPagesAction.java
@@ -325,10 +325,7 @@ public class EditPagesAction extends PortletAction {
 		PermissionChecker permissionChecker =
 			themeDisplay.getPermissionChecker();
 
-		String tabs1 = ParamUtil.getString(portletRequest, "tabs1");
-
 		long groupId = ParamUtil.getLong(portletRequest, "groupId");
-		boolean privateLayout = tabs1.equals("private-pages");
 
 		Group group = GroupLocalServiceUtil.getGroup(groupId);
 
@@ -393,10 +390,8 @@ public class EditPagesAction extends PortletAction {
 				permissionChecker, groupUserId, organizationIds,
 				ActionKeys.UPDATE);
 
-			if ((privateLayout &&
-				 !PropsValues.LAYOUT_USER_PRIVATE_LAYOUTS_MODIFIABLE) ||
-				(!privateLayout &&
-				 !PropsValues.LAYOUT_USER_PUBLIC_LAYOUTS_MODIFIABLE)) {
+			if (!PropsValues.LAYOUT_USER_PRIVATE_LAYOUTS_MODIFIABLE &&
+				 !PropsValues.LAYOUT_USER_PUBLIC_LAYOUTS_MODIFIABLE) {
 
 				throw new PrincipalException();
 			}
