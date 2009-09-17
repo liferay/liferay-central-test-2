035e5a237a8a834d265e5892b071a0d1ea75b7e4
Julio Camarero <julio.camarero@liferay.com>
2009-09-17 17:23:43 +0000

    LPS-5034 Adapt Bookmarks to AssetFramework
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@37690 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-impl/src/com/liferay/portlet/bookmarks/asset/BookmarksEntryAssetRenderer.java
@@ -0,0 +1,134 @@
+/**
+ * Copyright (c) 2000-2009 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portlet.bookmarks.asset;
+
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portal.util.WebKeys;
+import com.liferay.portlet.PortletRequestImpl;
+import com.liferay.portlet.asset.model.BaseAssetRenderer;
+import com.liferay.portlet.bookmarks.model.BookmarksEntry;
+import com.liferay.portlet.bookmarks.service.permission.BookmarksPermission;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * <a href="BookmarksEntryAssetRenderer.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Julio Camarero
+ */
+public class BookmarksEntryAssetRenderer extends BaseAssetRenderer {
+
+	public BookmarksEntryAssetRenderer(BookmarksEntry entry) {
+		_entry = entry;
+	}
+
+	public long getClassPK() {
+		return _entry.getEntryId();
+	}
+
+	public long getGroupId() {
+		return _entry.getGroupId();
+	}
+
+	public String getSummary() {
+		return HtmlUtil.stripHtml(_entry.getComments());
+	}
+
+	public String getTitle() {
+		return _entry.getName();
+	}
+
+	public PortletURL getURLEdit(
+		PortletRequest portletRequest, LiferayPortletResponse portletResponse) {
+
+		PortletRequestImpl portletRequestImpl =
+			(PortletRequestImpl)portletRequest;
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)portletRequestImpl.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		PortletURL editPortletURL = null;
+
+		if (BookmarksPermission.contains(
+				themeDisplay.getPermissionChecker(),
+				themeDisplay.getScopeGroupId(), ActionKeys.ADD_ENTRY)) {
+
+			editPortletURL = portletResponse.createRenderURL(
+				PortletKeys.BOOKMARKS);
+
+			editPortletURL.setParameter(
+				"struts_action", "/bookmarks/edit_entry");
+			editPortletURL.setParameter(
+				"folderId", String.valueOf(_entry.getFolderId()));
+ 			editPortletURL.setParameter(
+				 "entryId", String.valueOf(_entry.getEntryId()));
+		}
+
+		return editPortletURL;
+	}
+
+	public String getURLViewInContext(
+		PortletRequest portletRequest,
+		LiferayPortletResponse portletResponse, String noSuchEntryRedirect) {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		return themeDisplay.getPathMain() + "/bookmarks/open_entry?entryId=" +
+			_entry.getEntryId();
+	}
+
+	public long getUserId() {
+		return _entry.getUserId();
+	}
+
+	public boolean isPrintable() {
+		return true;
+	}
+
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse,
+			String template)
+		throws Exception {
+
+		if (template.equals(TEMPLATE_FULL_CONTENT)) {
+			renderRequest.setAttribute(WebKeys.BOOKMARKS_ENTRY, _entry);
+
+			return "/html/portlet/bookmarks/asset/" + template + ".jsp";
+		}
+		else {
+			return null;
+		}
+	}
+
+	private BookmarksEntry _entry;
+
+}
---
+++ portal-impl/src/com/liferay/portlet/bookmarks/asset/BookmarksEntryAssetRendererFactory.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2000-2009 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portlet.bookmarks.asset;
+
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portal.util.WebKeys;
+import com.liferay.portlet.PortletRequestImpl;
+import com.liferay.portlet.asset.model.AssetRenderer;
+import com.liferay.portlet.asset.model.BaseAssetRendererFactory;
+import com.liferay.portlet.assetpublisher.util.AssetPublisherUtil;
+import com.liferay.portlet.bookmarks.model.BookmarksEntry;
+import com.liferay.portlet.bookmarks.service.BookmarksEntryLocalServiceUtil;
+import com.liferay.portlet.bookmarks.service.permission.BookmarksPermission;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
+/**
+ * <a href="BookmarksEntryAssetRendererFactory.java.html"><b><i>View Source</i>
+ * </b></a>
+ *
+ * @author Julio Camarero
+ */
+public class BookmarksEntryAssetRendererFactory
+	extends BaseAssetRendererFactory {
+
+	public static final String CLASS_NAME =BookmarksEntry.class.getName();
+
+	public static final String TYPE = "bookmark";
+
+	public AssetRenderer getAssetRenderer(long classPK) throws Exception {
+		BookmarksEntry entry = BookmarksEntryLocalServiceUtil.getEntry(classPK);
+
+		return new BookmarksEntryAssetRenderer(entry);
+	}
+
+	public String getClassName() {
+		return CLASS_NAME;
+	}
+
+	public String getType() {
+		return TYPE;
+	}
+
+	public PortletURL getURLAdd(
+		PortletRequest portletRequest, LiferayPortletResponse portletResponse) {
+		PortletRequestImpl portletRequestImpl =
+			(PortletRequestImpl)portletRequest;
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)portletRequestImpl.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		PortletURL addAssetURL = null;
+
+		if (BookmarksPermission.contains(
+				themeDisplay.getPermissionChecker(),
+				themeDisplay.getScopeGroupId(), ActionKeys.ADD_ENTRY)) {
+
+			addAssetURL = portletResponse.createRenderURL(
+				PortletKeys.BOOKMARKS);
+
+			addAssetURL.setParameter(
+				"struts_action", "/bookmarks/edit_entry");
+			addAssetURL.setParameter(
+				"groupId", String.valueOf(themeDisplay.getScopeGroupId()));
+			addAssetURL.setParameter(
+				"folderId",
+				String.valueOf(
+					AssetPublisherUtil.getRecentFolderId(
+						portletRequest, CLASS_NAME)));
+		}
+
+		return addAssetURL;
+	}
+
+}
