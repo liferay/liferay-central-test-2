ea858d14fc205c78240a42d76a8e27a237434fa8
Brian Chan <brian.chan@liferay.com>
2009-10-10 05:02:10 +0000

    LPS-5309 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@38759 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/communities/action/GetLayoutsAction.java
+++ portal-impl/src/com/liferay/portlet/communities/action/GetLayoutsAction.java
@@ -22,14 +22,6 @@
 
 package com.liferay.portlet.communities.action;
 
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.struts.action.ActionForm;
-import org.apache.struts.action.ActionMapping;
-
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
@@ -38,47 +30,57 @@ import com.liferay.portal.model.Layout;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
 import com.liferay.portal.struts.JSONAction;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
+
 /**
- * <a href="LayoutAction.java.html"><b><i>View Source</i></b></a>
+ * <a href="GetLayoutsAction.java.html"><b><i>View Source</i></b></a>
  *
  * @author Eduardo Lundgren
  */
+public class GetLayoutsAction extends JSONAction {
 
-public class LayoutAction extends JSONAction {
-
-	public String getJSON(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
+	public String getJSON(
+			ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response)
+		throws Exception {
 
 		long groupId = ParamUtil.getLong(request, "groupId");
 		boolean privateLayout = ParamUtil.getBoolean(request, "privateLayout");
 		long parentLayoutId = ParamUtil.getLong(request, "parentLayoutId");
-		
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
 		List<Layout> layouts = LayoutLocalServiceUtil.getLayouts(
 			groupId, privateLayout, parentLayoutId);
-		
-		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
-		
+
 		for (Layout layout : layouts) {
-			JSONObject jsonObj = JSONFactoryUtil.createJSONObject();
-			
-			boolean hasChildren = (layout.getChildren().size() > 0);
-			
-			jsonObj.put("hasChildren", hasChildren);
-			jsonObj.put("layoutId", layout.getLayoutId());
-			jsonObj.put("name", layout.getName());
-			jsonObj.put("parentLayoutId", layout.getParentLayoutId());
-			jsonObj.put("plid", layout.getPlid());
-			jsonObj.put("priority", layout.getPriority());
-			jsonObj.put("privateLayout", layout.getPrivateLayout());
-			jsonObj.put("type", layout.getType());
-			
-			jsonArray.put(jsonObj);
+			JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+			boolean hasChildren = false;
+
+			if (layout.getChildren().size() > 0) {
+				hasChildren = true;
+			}
+
+			jsonObject.put("hasChildren", hasChildren);
+			jsonObject.put("layoutId", layout.getLayoutId());
+			jsonObject.put("name", layout.getName());
+			jsonObject.put("parentLayoutId", layout.getParentLayoutId());
+			jsonObject.put("plid", layout.getPlid());
+			jsonObject.put("priority", layout.getPriority());
+			jsonObject.put("privateLayout", layout.getPrivateLayout());
+			jsonObject.put("type", layout.getType());
+
+			jsonArray.put(jsonObject);
 		}
-		
+
 		return jsonArray.toString();
 	}
 
 }
-
-
