b0bfc4e36b34aea68deb960dc937fe8b8562855b
Micha Kiener <micha.kiener@mimacom.ch>
2009-10-21 04:52:22 +0000

    LPS-5470, generic persistence manager
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@39361 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-impl/src/com/liferay/portal/service/persistence/ModelIdentityImpl.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-2009 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portal.service.persistence;
+
+import java.io.Serializable;
+
+
+/**
+ * <a href="ModelIdentityImpl.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Micha Kiener
+ */
+public class ModelIdentityImpl implements ModelIdentity {
+
+	public String getContextName() {
+		return _contextName;
+	}
+
+	public String getModelClassName() {
+		return _modelClassName;
+	}
+
+	public Serializable getPrimaryKey() {
+		return _primaryKey;
+	}
+	
+	public void setContextName(String contextName) {
+		_contextName = contextName;
+	}
+	
+	public void setModelClassName(String modelClassName) {
+		_modelClassName = modelClassName;
+	}
+	
+	public void setPrimaryKey(Serializable primaryKey) {
+		_primaryKey = primaryKey;
+	}
+
+	private String _contextName;
+	private String _modelClassName;
+	private Serializable _primaryKey;
+}
