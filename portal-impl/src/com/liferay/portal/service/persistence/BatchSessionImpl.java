2bee7b0371b382d4cacd9b3e55014203ae938826
Douglas Wong <douglas.wong@liferay.com>
2009-11-06 19:41:44 +0000

    LPS-5671
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@40287 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/service/persistence/BatchSessionImpl.java
+++ portal-impl/src/com/liferay/portal/service/persistence/BatchSessionImpl.java
@@ -76,7 +76,9 @@ public class BatchSessionImpl implements BatchSession {
 			return;
 		}
 
-		if ((_counter.get() % PropsValues.HIBERNATE_JDBC_BATCH_SIZE) == 0) {
+		if ((PropsValues.HIBERNATE_JDBC_BATCH_SIZE == 0) || 
+			((_counter.get() % PropsValues.HIBERNATE_JDBC_BATCH_SIZE) == 0)) {
+
 			session.flush();
 			session.clear();
 		}
