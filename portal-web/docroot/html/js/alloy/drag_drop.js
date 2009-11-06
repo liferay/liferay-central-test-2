bd940c42b995f21b701b77af5a2f09b691fd8447
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2009-11-06 02:01:48 +0000

    LPS-5701 - Removing old Alloy/YUI2 components
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@40253 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/docroot/html/js/alloy/drag_drop.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	YAHOO.util.DDProxy.dragElId = 'EXP_PROXY';
-
-	Alloy.DragDrop = YAHOO.util.DragDropMgr;
-
-	Alloy.Draggable = new Alloy.Class(YAHOO.util.DD);
-	Alloy.DragProxy = new Alloy.Class(YAHOO.util.DDProxy);
-	Alloy.Droppable = new Alloy.Class(YAHOO.util.DDTarget);
-})();
--- portal-web/docroot/html/js/alloy/event.js
+++
@@ -1,8 +0,0 @@
-(function() {
-	Alloy.Event = YAHOO.util.Event;
-	Alloy.Event.un = Alloy.Event.removeListener;
-
-	Alloy.CustomEvent = new Alloy.Class(YAHOO.util.CustomEvent);
-
-	Alloy.KeyListener = new Alloy.Class(YAHOO.util.KeyListener);
-})();
