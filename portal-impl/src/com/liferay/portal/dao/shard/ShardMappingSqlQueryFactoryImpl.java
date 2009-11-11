8dd717451510192a8cd91536f4a6cee468fb6b06
Alexander Chow <alexander.chow@liferay.com>
2009-11-11 00:20:53 +0000

    LPS-5830 Trying to create a new instance in a sharded environment throws exceptions
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@40545 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-impl/src/com/liferay/portal/dao/shard/ShardMappingSqlQueryFactoryImpl.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-2009 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portal.dao.shard;
+
+import com.liferay.portal.kernel.dao.jdbc.MappingSqlQuery;
+import com.liferay.portal.kernel.dao.jdbc.MappingSqlQueryFactory;
+import com.liferay.portal.kernel.dao.jdbc.RowMapper;
+
+import javax.sql.DataSource;
+
+/**
+ * <a href="ShardMappingSqlQueryFactoryImpl.java.html"><b><i>View Source</i>
+ * </b></a>
+ *
+ * @author Alexander Chow
+ */
+public class ShardMappingSqlQueryFactoryImpl implements MappingSqlQueryFactory {
+
+	public MappingSqlQuery getMappingSqlQuery(
+		DataSource dataSource, String sql, int[] types, RowMapper rowMapper) {
+
+		return new ShardMappingSqlQueryImpl(dataSource, sql, types, rowMapper);
+	}
+
+}
