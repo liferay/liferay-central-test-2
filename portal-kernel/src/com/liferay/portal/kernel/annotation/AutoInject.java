d09c469c5dfb549e7f8e39ef6095aef3f4481806
Brian Chan <brian.chan@liferay.com>
2009-11-12 19:36:38 +0000

    LPS-5861 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@40704 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-kernel/src/com/liferay/portal/kernel/annotation/AutoInject.java
+++ portal-kernel/src/com/liferay/portal/kernel/annotation/AutoInject.java
@@ -31,7 +31,6 @@ import java.lang.annotation.Target;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
-
 /**
  * <a href="AutoInject.java.html"><b><i>View Source</i></b></a>
  *
