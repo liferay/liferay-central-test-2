78c1d817e4c6820df4483a8a8141e2f83c346ee6
Brian Chan <brian.chan@liferay.com>
2009-11-14 01:24:38 +0000

    Regenerate
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@40817 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/test/com/liferay/portal/service/persistence/ReleasePersistenceTest.java
+++ portal-impl/test/com/liferay/portal/service/persistence/ReleasePersistenceTest.java
@@ -42,8 +42,7 @@ public class ReleasePersistenceTest extends BasePersistenceTestCase {
 	public void setUp() throws Exception {
 		super.setUp();
 
-		_persistence = (ReleasePersistence)PortalBeanLocatorUtil.locate(ReleasePersistence.class.getName() +
-				".impl");
+		_persistence = (ReleasePersistence)PortalBeanLocatorUtil.locate(ReleasePersistence.class.getName());
 	}
 
 	public void testCreate() throws Exception {
