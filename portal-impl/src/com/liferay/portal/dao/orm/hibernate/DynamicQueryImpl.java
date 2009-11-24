323e1b973782c72e86270caadc8882e84a08d7b2
Brian Chan <brian.chan@liferay.com>
2009-11-24 00:29:06 +0000

    LPS-6093
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@41485 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/dao/orm/hibernate/DynamicQueryImpl.java
+++ portal-impl/src/com/liferay/portal/dao/orm/hibernate/DynamicQueryImpl.java
@@ -29,6 +29,9 @@ import com.liferay.portal.kernel.dao.orm.Projection;
 import com.liferay.portal.kernel.dao.orm.Session;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.UnmodifiableList;
+import com.liferay.portal.spring.hibernate.SessionInvocationHandler;
+
+import java.lang.reflect.Proxy;
 
 import java.util.List;
 
@@ -65,6 +68,13 @@ public class DynamicQueryImpl implements DynamicQuery {
 	public void compile(Session session) {
 		org.hibernate.Session hibernateSession =
 			(org.hibernate.Session)session.getWrappedSession();
+
+		SessionInvocationHandler sessionInvocationHandler =
+			(SessionInvocationHandler)Proxy.getInvocationHandler(
+				hibernateSession);
+
+		hibernateSession =
+			(org.hibernate.Session)sessionInvocationHandler.getSession();
 
 		_criteria = _detachedCriteria.getExecutableCriteria(hibernateSession);
 
