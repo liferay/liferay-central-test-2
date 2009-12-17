63bb5deb4e80cab9b128aabd9cfc881a6fbb0155
Amos Fong <amos.fong@liferay.com>
2009-12-17 21:44:13 +0000

    LPS-6604
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@42768 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-service/src/com/liferay/portal/security/auth/DefaultScreenNameGenerator.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2000-2009 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portal.security.auth;
+
+import com.liferay.portal.NoSuchGroupException;
+import com.liferay.portal.NoSuchUserException;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.service.UserLocalServiceUtil;
+
+/**
+ * <a href="DefaultScreenNameGenerator.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ * @author Alexander Chow
+ */
+public class DefaultScreenNameGenerator implements ScreenNameGenerator {
+
+	public String generate(long companyId, long userId, String emailAddress)
+		throws Exception {
+
+		String screenName = null;
+
+		if (Validator.isNotNull(emailAddress)) {
+			screenName = StringUtil.extractFirst(
+				emailAddress, StringPool.AT).toLowerCase();
+
+			screenName = StringUtil.replace(
+				screenName,
+				new String[] {StringPool.SLASH, StringPool.UNDERLINE},
+				new String[] {StringPool.PERIOD, StringPool.PERIOD});
+
+			if (screenName.equals(ScreenNameValidator.CYRUS) ||
+				screenName.equals(ScreenNameValidator.POSTFIX)) {
+
+				screenName += StringPool.PERIOD + userId;
+			}
+		}
+		else {
+			screenName = String.valueOf(userId);
+		}
+
+		try {
+			UserLocalServiceUtil.getUserByScreenName(companyId, screenName);
+		}
+		catch (NoSuchUserException nsue) {
+			try {
+				GroupLocalServiceUtil.getFriendlyURLGroup(
+					companyId, StringPool.SLASH + screenName);
+			}
+			catch (NoSuchGroupException nsge) {
+				return screenName;
+			}
+		}
+
+		for (int i = 1;; i++) {
+			String tempScreenName = screenName + StringPool.PERIOD + i;
+
+			try {
+				UserLocalServiceUtil.getUserByScreenName(
+					companyId, tempScreenName);
+			}
+			catch (NoSuchUserException nsue) {
+				try {
+					GroupLocalServiceUtil.getFriendlyURLGroup(
+						companyId, StringPool.SLASH + tempScreenName);
+				}
+				catch (NoSuchGroupException nsge) {
+					screenName = tempScreenName;
+
+					break;
+				}
+			}
+		}
+
+		return screenName;
+	}
+
+}
--- portal-service/src/com/liferay/portal/security/auth/ScreenNameGenerator.java
+++ portal-service/src/com/liferay/portal/security/auth/ScreenNameGenerator.java
@@ -22,80 +22,15 @@
 
 package com.liferay.portal.security.auth;
 
-import com.liferay.portal.NoSuchGroupException;
-import com.liferay.portal.NoSuchUserException;
-import com.liferay.portal.kernel.util.StringPool;
-import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.service.GroupLocalServiceUtil;
-import com.liferay.portal.service.UserLocalServiceUtil;
-
 /**
  * <a href="ScreenNameGenerator.java.html"><b><i>View Source</i></b></a>
  *
  * @author Brian Wing Shun Chan
  * @author Alexander Chow
  */
-public class ScreenNameGenerator {
+public interface ScreenNameGenerator {
 
 	public String generate(long companyId, long userId, String emailAddress)
-		throws Exception {
-
-		String screenName = null;
-
-		if (Validator.isNotNull(emailAddress)) {
-			screenName = StringUtil.extractFirst(
-				emailAddress, StringPool.AT).toLowerCase();
-
-			screenName = StringUtil.replace(
-				screenName,
-				new String[] {StringPool.SLASH, StringPool.UNDERLINE},
-				new String[] {StringPool.PERIOD, StringPool.PERIOD});
-
-			if (screenName.equals(ScreenNameValidator.CYRUS) ||
-				screenName.equals(ScreenNameValidator.POSTFIX)) {
-
-				screenName += StringPool.PERIOD + userId;
-			}
-		}
-		else {
-			screenName = String.valueOf(userId);
-		}
-
-		try {
-			UserLocalServiceUtil.getUserByScreenName(companyId, screenName);
-		}
-		catch (NoSuchUserException nsue) {
-			try {
-				GroupLocalServiceUtil.getFriendlyURLGroup(
-					companyId, StringPool.SLASH + screenName);
-			}
-			catch (NoSuchGroupException nsge) {
-				return screenName;
-			}
-		}
-
-		for (int i = 1;; i++) {
-			String tempScreenName = screenName + StringPool.PERIOD + i;
-
-			try {
-				UserLocalServiceUtil.getUserByScreenName(
-					companyId, tempScreenName);
-			}
-			catch (NoSuchUserException nsue) {
-				try {
-					GroupLocalServiceUtil.getFriendlyURLGroup(
-						companyId, StringPool.SLASH + tempScreenName);
-				}
-				catch (NoSuchGroupException nsge) {
-					screenName = tempScreenName;
-
-					break;
-				}
-			}
-		}
-
-		return screenName;
-	}
+		throws Exception;
 
 }
