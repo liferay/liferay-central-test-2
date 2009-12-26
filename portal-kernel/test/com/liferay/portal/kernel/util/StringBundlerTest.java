daaf263ee082f0ec59ac88adc8a929d75ff44198
Brian Chan <brian.chan@liferay.com>
2009-12-26 20:49:14 +0000

    LPS-6072 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@43063 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-kernel/test/com/liferay/portal/kernel/util/StringBundlerTest.java
+++ portal-kernel/test/com/liferay/portal/kernel/util/StringBundlerTest.java
@@ -31,112 +31,155 @@ import com.liferay.portal.kernel.test.TestCase;
  */
 public class StringBundlerTest extends TestCase {
 
-	public void testConstruct() {
-		// Default construct
+	public void testConstructor() {
+
+		// Default constructor
+
 		StringBundler sb = new StringBundler();
+
 		assertEquals(0, sb.index());
 		assertEquals(16, sb.capacity());
 
-		// Construct with capacity
+		// Constructor with capacity
+
 		sb = new StringBundler(32);
+
 		assertEquals(0, sb.index());
 		assertEquals(32, sb.capacity());
 
-		// Construct with a String
+		// Constructor with a String
+
 		sb = new StringBundler("test");
+
 		assertEquals(1, sb.index());
 		assertEquals("test", sb.stringAt(0));
 		assertEquals(16, sb.capacity());
 	}
 
 	public void testAppend() {
-		// Null append
+
+		// Append null
+
 		StringBundler sb = new StringBundler();
+
 		sb.append(null);
+
 		assertEquals(1, sb.index());
 		assertEquals(StringPool.NULL, sb.stringAt(0));
 
-		// Append with out grow
+		// Append without growing
+
 		sb = new StringBundler();
+
 		sb.append("test1");
+
 		assertEquals(1, sb.index());
 		assertEquals(16, sb.capacity());
 		assertEquals("test1", sb.stringAt(0));
+
 		sb.append("test2");
+
 		assertEquals(2, sb.index());
 		assertEquals(16, sb.capacity());
 		assertEquals("test1", sb.stringAt(0));
 		assertEquals("test2", sb.stringAt(1));
+
 		sb.append("test3");
+
 		assertEquals(3, sb.index());
 		assertEquals(16, sb.capacity());
 		assertEquals("test1", sb.stringAt(0));
 		assertEquals("test2", sb.stringAt(1));
 		assertEquals("test3", sb.stringAt(2));
 
-		// Append with grow
+		// Append with growth
+
 		sb = new StringBundler(2);
+
 		sb.append("test1");
+
 		assertEquals(1, sb.index());
 		assertEquals(2, sb.capacity());
 		assertEquals("test1", sb.stringAt(0));
+
 		sb.append("test2");
+
 		assertEquals(2, sb.index());
 		assertEquals(2, sb.capacity());
 		assertEquals("test1", sb.stringAt(0));
 		assertEquals("test2", sb.stringAt(1));
+
 		sb.append("test3");
+
 		assertEquals(3, sb.index());
 		assertEquals(4, sb.capacity());
+
 		assertEquals("test1", sb.stringAt(0));
 		assertEquals("test2", sb.stringAt(1));
 		assertEquals("test3", sb.stringAt(2));
 	}
 
 	public void testToString() {
+
 		// Empty toString
+
 		StringBundler sb = new StringBundler();
+
 		assertEquals(StringPool.BLANK, sb.toString());
 
-		// Using String.concat()
+		// String.concat()
+
 		sb.append("test1");
 		sb.append("test2");
 		sb.append("test3");
+
 		assertEquals("test1test2test3", sb.toString());
 
-		//Using StringBuilder
+		// StringBuilder
+
 		sb.append("test4");
+
 		assertEquals("test1test2test3test4", sb.toString());
 	}
 
 	public void testSetIndex() {
+
 		// Negative index
-		boolean hasException = false;
+
 		StringBundler sb = new StringBundler();
+
 		try {
 			sb.setIndex(-1);
+
+			fail();
 		}
-		catch(ArrayIndexOutOfBoundsException ex) {
-			hasException = true;
+		catch (ArrayIndexOutOfBoundsException aioobe) {
 		}
 
-		assertTrue(hasException);
+		// New index equals current index
 
-		// New index equals current index, nothing should happen
 		sb = new StringBundler(4);
+
 		sb.append("test1");
 		sb.append("test2");
-		assertEquals(2, sb.index());
-		assertEquals("test1", sb.stringAt(0));
-		assertEquals("test2", sb.stringAt(1));
-		sb.setIndex(2);
+
 		assertEquals(2, sb.index());
 		assertEquals("test1", sb.stringAt(0));
 		assertEquals("test2", sb.stringAt(1));
 
-		// New index is bigger than current index, but smaller than array size
+		sb.setIndex(2);
+
+		assertEquals(2, sb.index());
+		assertEquals("test1", sb.stringAt(0));
+		assertEquals("test2", sb.stringAt(1));
+
+		// New index is larger than the current index but smaller than the array
+		// size
+
 		assertEquals(4, sb.capacity());
+
 		sb.setIndex(4);
+
 		assertEquals(4, sb.capacity());
 		assertEquals(4, sb.index());
 		assertEquals("test1", sb.stringAt(0));
@@ -144,8 +187,10 @@ public class StringBundlerTest extends TestCase {
 		assertEquals(StringPool.BLANK, sb.stringAt(2));
 		assertEquals(StringPool.BLANK, sb.stringAt(3));
 
-		// New index is bigger than current index and array size
+		// New index is larger than the current index and array size
+
 		sb.setIndex(6);
+
 		assertEquals(6, sb.capacity());
 		assertEquals(6, sb.index());
 		assertEquals("test1", sb.stringAt(0));
@@ -156,19 +201,20 @@ public class StringBundlerTest extends TestCase {
 		assertEquals(StringPool.BLANK, sb.stringAt(5));
 
 		// New index is smaller than current index
+
 		sb.setIndex(1);
+
 		assertEquals(6, sb.capacity());
 		assertEquals(1, sb.index());
 		assertEquals("test1", sb.stringAt(0));
-		hasException = false;
+
 		try {
 			assertEquals(null, sb.stringAt(1));
-		}
-		catch(ArrayIndexOutOfBoundsException ex) {
-			hasException = true;
-		}
 
-		assertTrue(hasException);
+			fail();
+		}
+		catch (ArrayIndexOutOfBoundsException aioobe) {
+		}
 	}
 
 }
