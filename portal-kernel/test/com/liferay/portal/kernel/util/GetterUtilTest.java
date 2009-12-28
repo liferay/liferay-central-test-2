2025b683d921d5fb5cc7f146feeb6518af1626a3
Shuyang Zhou <shuyang.zhou@liferay.com>
2009-12-28 11:53:45 +0000

    LPS-6731 Add unit test
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@43086 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-kernel/test/com/liferay/portal/kernel/util/GetterUtilTest.java
@@ -0,0 +1,141 @@
+/**
+ * Copyright (c) 2000-2009 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portal.kernel.util;
+
+import com.liferay.portal.kernel.test.TestCase;
+
+/**
+ * <a href="GetterUtilTest.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Shuyang Zhou
+ */
+public class GetterUtilTest extends TestCase {
+
+	public void testGetInt() {
+
+		//Wrong first char
+		int result = GetterUtil.get("e123", -1);
+		assertEquals(-1, result);
+
+		//Wrong middle char
+		result = GetterUtil.get("12e3", -1);
+		assertEquals(-1, result);
+
+		//Start with '+'
+		result = GetterUtil.get("+123", -1);
+		assertEquals(123, result);
+
+		//Start with '-'
+		result = GetterUtil.get("-123", -1);
+		assertEquals(-123, result);
+
+		//Max int
+		result = GetterUtil.get(Integer.toString(Integer.MAX_VALUE), -1);
+		assertEquals(Integer.MAX_VALUE, result);
+
+		//Min int
+		result = GetterUtil.get(Integer.toString(Integer.MIN_VALUE), -1);
+		assertEquals(Integer.MIN_VALUE, result);
+
+		//Bigger than Max int
+		result = GetterUtil.get(Integer.toString(Integer.MAX_VALUE) + "0", -1);
+		assertEquals(-1, result);
+
+		//Smaller than Min int
+		result = GetterUtil.get(Integer.toString(Integer.MIN_VALUE) + "0", -1);
+		assertEquals(-1, result);
+	}
+
+	public void testGetLong() {
+
+		//Wrong first char
+		long result = GetterUtil.get("e123", -1L);
+		assertEquals(-1L, result);
+
+		//Wrong middle char
+		result = GetterUtil.get("12e3", -1L);
+		assertEquals(-1L, result);
+
+		//Start with '+'
+		result = GetterUtil.get("+123", -1L);
+		assertEquals(123L, result);
+
+		//Start with '-'
+		result = GetterUtil.get("-123", -1L);
+		assertEquals(-123L, result);
+
+		//Max long
+		result = GetterUtil.get(Long.toString(Long.MAX_VALUE), -1L);
+		assertEquals(Long.MAX_VALUE, result);
+
+		//Min long
+		result = GetterUtil.get(Long.toString(Long.MIN_VALUE), -1L);
+		assertEquals(Long.MIN_VALUE, result);
+
+		//Bigger than Max long
+		result = GetterUtil.get(Long.toString(Long.MAX_VALUE) + "0", -1L);
+		assertEquals(-1L, result);
+
+		//Smaller than Min long
+		result = GetterUtil.get(Long.toString(Long.MIN_VALUE) + "0", -1L);
+		assertEquals(-1L, result);
+	}
+
+	public void testGetShort() {
+
+		//Wrong first char
+		short result = GetterUtil.get("e123", (short) -1);
+		assertEquals((short) -1, result);
+
+		//Wrong middle char
+		result = GetterUtil.get("12e3", (short) -1);
+		assertEquals((short) -1, result);
+
+		//Start with '+'
+		result = GetterUtil.get("+123", (short) -1);
+		assertEquals((short) 123, result);
+
+		//Start with '-'
+		result = GetterUtil.get("-123", (short) -1);
+		assertEquals((short) -123, result);
+
+		//Max short
+		result = GetterUtil.get(Short.toString(Short.MAX_VALUE), (short) -1);
+		assertEquals(Short.MAX_VALUE, result);
+
+		//Min short
+		result = GetterUtil.get(Short.toString(Short.MIN_VALUE), (short) -1);
+		assertEquals(Short.MIN_VALUE, result);
+
+		//Bigger than Max short
+		result = GetterUtil.get(
+			Short.toString(Short.MAX_VALUE) + "0", (short) -1);
+		assertEquals((short) -1, result);
+
+		//Smaller than Min short
+		result = GetterUtil.get(
+			Short.toString(Short.MIN_VALUE) + "0", (short) -1);
+		assertEquals((short) -1, result);
+	}
+
+}
