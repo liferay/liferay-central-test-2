5b1ef9b573226aa192445554189e82f648e7e247
Brian Chan <brian.chan@liferay.com>
2010-01-08 22:12:53 +0000

    LPS-6891
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@43582 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-impl/src/com/liferay/portal/upgrade/v5_3_0/util/ResourceActionTable.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-2009 Liferay, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.liferay.portal.upgrade.v5_3_0.util;
+
+import java.sql.Types;
+
+/**
+ * <a href="ResourceActionTable.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ */
+public class ResourceActionTable {
+
+	public static final String TABLE_NAME = "ResourceAction";
+
+	public static final Object[][] TABLE_COLUMNS = {
+		{"resourceActionId", new Integer(Types.BIGINT)},
+		{"name", new Integer(Types.VARCHAR)},
+		{"actionId", new Integer(Types.VARCHAR)},
+		{"bitwiseValue", new Integer(Types.BIGINT)}
+	};
+
+	public static final String TABLE_SQL_CREATE = "create table ResourceAction (resourceActionId LONG not null primary key,name VARCHAR(255) null,actionId VARCHAR(75) null,bitwiseValue LONG)";
+
+	public static final String TABLE_SQL_DROP = "drop table ResourceAction";
+
+}
