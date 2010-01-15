e0dc2ec153911e25b075b3c6399eecb2442e4f07
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2010-01-15 22:17:24 +0000

    LPS-7036 Tabs don't work in any theme
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@43998 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/docroot/html/js/liferay/portal.js
+++ portal-web/docroot/html/js/liferay/portal.js
@@ -1,17 +1,16 @@
 Liferay.Portal = {};
 
 Liferay.Portal.Tabs = {
-	show: function() {}
-};
-
-AUI().ready(
-	function(A) {
-		Liferay.Portal.Tabs.show = function(namespace, names, id) {
-			var tab = AUI().one('#' + namespace + id + 'TabsId');
-			var panel = AUI().one('#' + namespace + id + 'TabsSection');
+	show: function(namespace, names, id) {
+		var callback = function(A) {
+			var tab = A.one('#' + namespace + id + 'TabsId');
+			var panel = A.one('#' + namespace + id + 'TabsSection');
 
 			if (tab) {
 				tab.radioClass('aui-selected');
+				tab.radioClass('aui-state-active');
+				tab.radioClass('aui-tab-active');
+				tab.radioClass('current');
 			}
 
 			if (panel) {
@@ -23,15 +22,24 @@ AUI().ready(
 			names.splice(index, 1);
 
 			for (var i = 0; i < names.length; i++) {
-				el = AUI().one('#' + namespace + names[i] + 'TabsSection');
+				el = A.one('#' + namespace + names[i] + 'TabsSection');
 
 				if (el) {
 					el.hide();
 				}
 			}
 		};
+
+		var A = AUI();
+
+		if (A.one) {
+			callback(A);
+		}
+		else {
+			AUI().use('node', callback);
+		}
 	}
-);
+};
 
 (function() {
 	var elementsCache = {};
