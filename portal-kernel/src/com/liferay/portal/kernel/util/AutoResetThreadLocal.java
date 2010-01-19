5e5f684e39dba90f1b690c78930e293d42ef7767
Brian Chan <brian.chan@liferay.com>
2010-01-19 23:54:32 +0000

    LPS-7123 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@44201 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-kernel/src/com/liferay/portal/kernel/util/AutoResetThreadLocal.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/AutoResetThreadLocal.java
@@ -41,6 +41,7 @@ public class AutoResetThreadLocal<T> extends InitialThreadLocal<T> {
 
 	protected T initialValue() {
 		ThreadLocalRegistry.registerThreadLocal(this);
+
 		return super.initialValue();
 	}
 
