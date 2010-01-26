0e156ee516286b02b9f31c7776a1a983d8cada57
Brian Chan <brian.chan@liferay.com>
2010-01-26 18:08:15 +0000

    LPS-5620 Ran "ant build-upgrade-table"
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@44626 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/upgrade/v6_0_0/util/JournalFeedTable.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_0_0/util/JournalFeedTable.java
@@ -27,36 +27,37 @@ import java.sql.Types;
 /**
  * <a href="JournalFeedTable.java.html"><b><i>View Source</i></b></a>
  *
- * @author Zsigmond Rab
+ * @author	  Zsigmond Rab
+ * @generated
  */
 public class JournalFeedTable {
 
 	public static final String TABLE_NAME = "JournalFeed";
 
 	public static final Object[][] TABLE_COLUMNS = {
-		{ "uuid_", new Integer(Types.VARCHAR) },
-		{ "id_", new Integer(Types.BIGINT) },
-		{ "groupId", new Integer(Types.BIGINT) },
-		{ "companyId", new Integer(Types.BIGINT) },
-		{ "userId", new Integer(Types.BIGINT) },
-		{ "userName", new Integer(Types.VARCHAR) },
-		{ "createDate", new Integer(Types.TIMESTAMP) },
-		{ "modifiedDate", new Integer(Types.TIMESTAMP) },
-		{ "feedId", new Integer(Types.VARCHAR) },
-		{ "name", new Integer(Types.VARCHAR) },
-		{ "description", new Integer(Types.VARCHAR) },
-		{ "type_", new Integer(Types.VARCHAR) },
-		{ "structureId", new Integer(Types.VARCHAR) },
-		{ "templateId", new Integer(Types.VARCHAR) },
-		{ "rendererTemplateId", new Integer(Types.VARCHAR) },
-		{ "delta", new Integer(Types.INTEGER) },
-		{ "orderByCol", new Integer(Types.VARCHAR) },
-		{ "orderByType", new Integer(Types.VARCHAR) },
-		{ "targetLayoutFriendlyUrl", new Integer(Types.VARCHAR) },
-		{ "targetPortletId", new Integer(Types.VARCHAR) },
-		{ "contentField", new Integer(Types.VARCHAR) },
-		{ "feedType", new Integer(Types.VARCHAR) },
-		{ "feedVersion", new Integer(Types.DOUBLE) }
+		{"uuid_", new Integer(Types.VARCHAR)},
+		{"id_", new Integer(Types.BIGINT)},
+		{"groupId", new Integer(Types.BIGINT)},
+		{"companyId", new Integer(Types.BIGINT)},
+		{"userId", new Integer(Types.BIGINT)},
+		{"userName", new Integer(Types.VARCHAR)},
+		{"createDate", new Integer(Types.TIMESTAMP)},
+		{"modifiedDate", new Integer(Types.TIMESTAMP)},
+		{"feedId", new Integer(Types.VARCHAR)},
+		{"name", new Integer(Types.VARCHAR)},
+		{"description", new Integer(Types.VARCHAR)},
+		{"type_", new Integer(Types.VARCHAR)},
+		{"structureId", new Integer(Types.VARCHAR)},
+		{"templateId", new Integer(Types.VARCHAR)},
+		{"rendererTemplateId", new Integer(Types.VARCHAR)},
+		{"delta", new Integer(Types.INTEGER)},
+		{"orderByCol", new Integer(Types.VARCHAR)},
+		{"orderByType", new Integer(Types.VARCHAR)},
+		{"targetLayoutFriendlyUrl", new Integer(Types.VARCHAR)},
+		{"targetPortletId", new Integer(Types.VARCHAR)},
+		{"contentField", new Integer(Types.VARCHAR)},
+		{"feedType", new Integer(Types.VARCHAR)},
+		{"feedVersion", new Integer(Types.DOUBLE)}
 	};
 
 	public static final String TABLE_SQL_CREATE = "create table JournalFeed (uuid_ VARCHAR(75) null,id_ LONG not null primary key,groupId LONG,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,feedId VARCHAR(75) null,name VARCHAR(75) null,description STRING null,type_ VARCHAR(75) null,structureId VARCHAR(75) null,templateId VARCHAR(75) null,rendererTemplateId VARCHAR(75) null,delta INTEGER,orderByCol VARCHAR(75) null,orderByType VARCHAR(75) null,targetLayoutFriendlyUrl VARCHAR(255) null,targetPortletId VARCHAR(75) null,contentField VARCHAR(75) null,feedType VARCHAR(75) null,feedVersion DOUBLE)";
--- portal-impl/src/com/liferay/portal/upgrade/v6_0_0/util/ShoppingItemTable.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_0_0/util/ShoppingItemTable.java
@@ -27,47 +27,48 @@ import java.sql.Types;
 /**
  * <a href="ShoppingItemTable.java.html"><b><i>View Source</i></b></a>
  *
- * @author Zsigmond Rab
+ * @author	  Zsigmond Rab
+ * @generated
  */
 public class ShoppingItemTable {
 
 	public static final String TABLE_NAME = "ShoppingItem";
 
 	public static final Object[][] TABLE_COLUMNS = {
-		{ "itemId", new Integer(Types.BIGINT) },
-		{ "groupId", new Integer(Types.BIGINT) },
-		{ "companyId", new Integer(Types.BIGINT) },
-		{ "userId", new Integer(Types.BIGINT) },
-		{ "userName", new Integer(Types.VARCHAR) },
-		{ "createDate", new Integer(Types.TIMESTAMP) },
-		{ "modifiedDate", new Integer(Types.TIMESTAMP) },
-		{ "categoryId", new Integer(Types.BIGINT) },
-		{ "sku", new Integer(Types.VARCHAR) },
-		{ "name", new Integer(Types.VARCHAR) },
-		{ "description", new Integer(Types.VARCHAR) },
-		{ "properties", new Integer(Types.VARCHAR) },
-		{ "fields_", new Integer(Types.BOOLEAN) },
-		{ "fieldsQuantities", new Integer(Types.VARCHAR) },
-		{ "minQuantity", new Integer(Types.INTEGER) },
-		{ "maxQuantity", new Integer(Types.INTEGER) },
-		{ "price", new Integer(Types.DOUBLE) },
-		{ "discount", new Integer(Types.DOUBLE) },
-		{ "taxable", new Integer(Types.BOOLEAN) },
-		{ "shipping", new Integer(Types.DOUBLE) },
-		{ "useShippingFormula", new Integer(Types.BOOLEAN) },
-		{ "requiresShipping", new Integer(Types.BOOLEAN) },
-		{ "stockQuantity", new Integer(Types.INTEGER) },
-		{ "featured_", new Integer(Types.BOOLEAN) },
-		{ "sale_", new Integer(Types.BOOLEAN) },
-		{ "smallImage", new Integer(Types.BOOLEAN) },
-		{ "smallImageId", new Integer(Types.BIGINT) },
-		{ "smallImageURL", new Integer(Types.VARCHAR) },
-		{ "mediumImage", new Integer(Types.BOOLEAN) },
-		{ "mediumImageId", new Integer(Types.BIGINT) },
-		{ "mediumImageURL", new Integer(Types.VARCHAR) },
-		{ "largeImage", new Integer(Types.BOOLEAN) },
-		{ "largeImageId", new Integer(Types.BIGINT) },
-		{ "largeImageURL", new Integer(Types.VARCHAR) }
+		{"itemId", new Integer(Types.BIGINT)},
+		{"groupId", new Integer(Types.BIGINT)},
+		{"companyId", new Integer(Types.BIGINT)},
+		{"userId", new Integer(Types.BIGINT)},
+		{"userName", new Integer(Types.VARCHAR)},
+		{"createDate", new Integer(Types.TIMESTAMP)},
+		{"modifiedDate", new Integer(Types.TIMESTAMP)},
+		{"categoryId", new Integer(Types.BIGINT)},
+		{"sku", new Integer(Types.VARCHAR)},
+		{"name", new Integer(Types.VARCHAR)},
+		{"description", new Integer(Types.VARCHAR)},
+		{"properties", new Integer(Types.VARCHAR)},
+		{"fields_", new Integer(Types.BOOLEAN)},
+		{"fieldsQuantities", new Integer(Types.VARCHAR)},
+		{"minQuantity", new Integer(Types.INTEGER)},
+		{"maxQuantity", new Integer(Types.INTEGER)},
+		{"price", new Integer(Types.DOUBLE)},
+		{"discount", new Integer(Types.DOUBLE)},
+		{"taxable", new Integer(Types.BOOLEAN)},
+		{"shipping", new Integer(Types.DOUBLE)},
+		{"useShippingFormula", new Integer(Types.BOOLEAN)},
+		{"requiresShipping", new Integer(Types.BOOLEAN)},
+		{"stockQuantity", new Integer(Types.INTEGER)},
+		{"featured_", new Integer(Types.BOOLEAN)},
+		{"sale_", new Integer(Types.BOOLEAN)},
+		{"smallImage", new Integer(Types.BOOLEAN)},
+		{"smallImageId", new Integer(Types.BIGINT)},
+		{"smallImageURL", new Integer(Types.VARCHAR)},
+		{"mediumImage", new Integer(Types.BOOLEAN)},
+		{"mediumImageId", new Integer(Types.BIGINT)},
+		{"mediumImageURL", new Integer(Types.VARCHAR)},
+		{"largeImage", new Integer(Types.BOOLEAN)},
+		{"largeImageId", new Integer(Types.BIGINT)},
+		{"largeImageURL", new Integer(Types.VARCHAR)}
 	};
 
 	public static final String TABLE_SQL_CREATE = "create table ShoppingItem (itemId LONG not null primary key,groupId LONG,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,categoryId LONG,sku VARCHAR(75) null,name VARCHAR(200) null,description STRING null,properties STRING null,fields_ BOOLEAN,fieldsQuantities STRING null,minQuantity INTEGER,maxQuantity INTEGER,price DOUBLE,discount DOUBLE,taxable BOOLEAN,shipping DOUBLE,useShippingFormula BOOLEAN,requiresShipping BOOLEAN,stockQuantity INTEGER,featured_ BOOLEAN,sale_ BOOLEAN,smallImage BOOLEAN,smallImageId LONG,smallImageURL STRING null,mediumImage BOOLEAN,mediumImageId LONG,mediumImageURL STRING null,largeImage BOOLEAN,largeImageId LONG,largeImageURL STRING null)";
