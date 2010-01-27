c5ab0da72631bc7a982f434968f909aab6681399
Zsigmond Rab <zsigmond.rab@liferay.com>
2010-01-27 09:21:32 +0000

    LPS-2832
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@44724 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/journal/action/EditArticleAction.java
+++ portal-impl/src/com/liferay/portlet/journal/action/EditArticleAction.java
@@ -23,6 +23,7 @@
 package com.liferay.portlet.journal.action;
 
 import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.upload.UploadPortletRequest;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.FileUtil;
@@ -176,6 +177,11 @@ public class EditArticleAction extends PortletAction {
 
 		try {
 			String cmd = ParamUtil.getString(renderRequest, Constants.CMD);
+
+			if (cmd.equals(StringPool.BLANK)) {
+				SessionMessages.clear(renderRequest);
+				SessionErrors.clear(renderRequest);
+			}
 
 			if (!cmd.equals(Constants.ADD)) {
 				ActionUtil.getArticle(renderRequest);
