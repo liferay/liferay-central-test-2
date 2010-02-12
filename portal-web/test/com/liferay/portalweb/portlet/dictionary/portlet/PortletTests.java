273045eb89928ca4f12e3eb5389dff0609f1c9a6
Michael Hashimoto <michael.hashimoto@liferay.com>
2010-02-12 01:47:22 +0000

    LPS-7207
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@45906 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/test/com/liferay/portalweb/portlet/dictionary/portlet/PortletTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/dictionary/portlet/PortletTests.java
@@ -20,25 +20,29 @@
  * SOFTWARE.
  */
 
-package com.liferay.portalweb.portlet.dictionary;
+package com.liferay.portalweb.portlet.dictionary.portlet;
 
 import com.liferay.portalweb.portal.BaseTests;
-import com.liferay.portalweb.portlet.dictionary.portlet.PortletTests;
+import com.liferay.portalweb.portlet.dictionary.portlet.addportlet.AddPortletTests;
+import com.liferay.portalweb.portlet.dictionary.portlet.addportletduplicate.AddPortletDuplicateTests;
+import com.liferay.portalweb.portlet.dictionary.portlet.removeportlet.RemovePortletTests;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
 /**
- * <a href="DictionaryTests.java.html"><b><i>View Source</i></b></a>
+ * <a href="PortletTests.java.html"><b><i>View Source</i></b></a>
  *
  * @author Brian Wing Shun Chan
  */
-public class DictionaryTests extends BaseTests {
+public class PortletTests extends BaseTests {
 
 	public static Test suite() {
 		TestSuite testSuite = new TestSuite();
 
-		testSuite.addTest(PortletTests.suite());
+		testSuite.addTest(AddPortletTests.suite());
+		testSuite.addTest(AddPortletDuplicateTests.suite());
+		testSuite.addTest(RemovePortletTests.suite());
 
 		return testSuite;
 	}
--- portal-web/test/com/liferay/portalweb/portlet/dictionary/portlet/addportletduplicate/AddPortletDuplicateTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/dictionary/portlet/addportletduplicate/AddPortletDuplicateTests.java
@@ -20,25 +20,27 @@
  * SOFTWARE.
  */
 
-package com.liferay.portalweb.portlet.dictionary;
+package com.liferay.portalweb.portlet.dictionary.portlet.addportletduplicate;
 
 import com.liferay.portalweb.portal.BaseTests;
-import com.liferay.portalweb.portlet.dictionary.portlet.PortletTests;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
 /**
- * <a href="DictionaryTests.java.html"><b><i>View Source</i></b></a>
+ * <a href="AddPortletDuplicateTests.java.html"><b><i>View Source</i></b></a>
  *
  * @author Brian Wing Shun Chan
  */
-public class DictionaryTests extends BaseTests {
+public class AddPortletDuplicateTests extends BaseTests {
 
 	public static Test suite() {
 		TestSuite testSuite = new TestSuite();
 
-		testSuite.addTest(PortletTests.suite());
+		testSuite.addTestSuite(AddPageTest.class);
+		testSuite.addTestSuite(AddPortletTest.class);
+		testSuite.addTestSuite(AddPortletDuplicateTest.class);
+		testSuite.addTestSuite(TearDownTest.class);
 
 		return testSuite;
 	}
