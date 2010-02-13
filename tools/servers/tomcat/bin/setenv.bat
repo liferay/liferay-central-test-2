4bfc67310838700b30250fe6c83e457a2ab67651
Brian Chan <brian.chan@liferay.com>
2010-02-13 01:42:23 +0000

    LPS-7714
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@46015 05bdf26c-840f-0410-9ced-eb539d925f36

--- tools/servers/tomcat/bin/setenv.bat
+++ tools/servers/tomcat/bin/setenv.bat
@@ -4,4 +4,4 @@ if not "%JAVA_HOME%" == "" (
 
 set JRE_HOME=%CATALINA_HOME%/jre@java.version@/win
 
-set "JAVA_OPTS=%JAVA_OPTS% -Dfile.encoding=UTF8 -Djava.security.auth.login.config=%CATALINA_HOME%/conf/jaas.config -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
+set "JAVA_OPTS=%JAVA_OPTS% -Dfile.encoding=UTF8 -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
--- tools/servers/tomcat/bin/setenv.sh
+++ tools/servers/tomcat/bin/setenv.sh
@@ -1,1 +1,1 @@
-JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8 -Djava.security.auth.login.config=$CATALINA_HOME/conf/jaas.config -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
+JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8 -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
