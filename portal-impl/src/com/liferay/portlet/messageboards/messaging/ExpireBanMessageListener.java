cb814d5566cd7c5a46641b96ee19de4d0b88a31f
Tina Tian <tina.tian@liferay.com>
2010-02-19 07:13:30 +0000

    LPS-7722 Migrate messageboards portlet's scheduler
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@46384 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/messageboards/messaging/ExpireBanMessageListener.java
+++ portal-impl/src/com/liferay/portlet/messageboards/messaging/ExpireBanMessageListener.java
@@ -20,43 +20,34 @@
  * SOFTWARE.
  */
 
-package com.liferay.portlet.messageboards.job;
+package com.liferay.portlet.messageboards.messaging;
 
-import com.liferay.portal.kernel.job.IntervalJob;
-import com.liferay.portal.kernel.job.JobExecutionContext;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.PropsKeys;
-import com.liferay.portal.kernel.util.Time;
-import com.liferay.portal.util.PrefsPropsUtil;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageListener;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.portlet.messageboards.service.MBBanLocalServiceUtil;
 
 /**
- * <a href="ExpireBanJob.java.html"><b><i>View Source</i></b></a>
+ * <a href="ExpireBanMessageListener.java.html"><b><i>View Source</i>
+ * </b></a>
  *
  * @author Michael Young
+ * @author Tina Tian
  */
-public class ExpireBanJob implements IntervalJob {
+public class ExpireBanMessageListener implements MessageListener {
 
-	public ExpireBanJob() {
-		try {
-			long rawInterval = PrefsPropsUtil.getLong(
-				PropsKeys.MESSAGE_BOARDS_EXPIRE_BAN_JOB_INTERVAL,
-				PropsValues.MESSAGE_BOARDS_EXPIRE_BAN_JOB_INTERVAL);
-
+	public void receive(Message message) {
+		if (PropsValues.MESSAGE_BOARDS_EXPIRE_BAN_INTERVAL <= 0) {
 			if (_log.isDebugEnabled()) {
-				_log.debug("Interval " + rawInterval + " minutes");
+				_log.debug(
+					"Auto expire of banned message board users is disabled");
 			}
 
-			_interval = rawInterval * Time.MINUTE;
+			return;
 		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-	}
 
-	public void execute(JobExecutionContext context) {
 		try {
 			MBBanLocalServiceUtil.expireBans();
 		}
@@ -65,12 +56,7 @@ public class ExpireBanJob implements IntervalJob {
 		}
 	}
 
-	public long getInterval() {
-		return _interval;
-	}
-
-	private static Log _log = LogFactoryUtil.getLog(ExpireBanJob.class);
-
-	private long _interval;
+	private static Log _log = LogFactoryUtil.getLog(
+		ExpireBanMessageListener.class);
 
 }
