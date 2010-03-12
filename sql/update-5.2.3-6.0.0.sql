ef65da56de9ddd6d7d0146661462721a441c4dfa
Brian Chan <brian.chan@liferay.com>
2010-03-12 23:30:57 +0000

    LPS-8314
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@48075 05bdf26c-840f-0410-9ced-eb539d925f36

--- sql/update-5.2.3-6.0.0.sql
+++ sql/update-5.2.3-6.0.0.sql
@@ -122,19 +122,36 @@ COMMIT_TRANSACTION;
 update BlogsEntry set allowPingbacks = TRUE;
 update BlogsEntry set status = 0 where draft = FALSE;
 update BlogsEntry set status = 2 where draft = TRUE;
+update BlogsEntry set statusByUserId = userId where draft = FALSE;
+update BlogsEntry set statusByUserName = userName where draft = FALSE;
+update BlogsEntry set statusDate = createDate where draft = FALSE;
 
-alter table DLFileEntry add pendingVersion DOUBLE;
+alter table DLFileEntry add pendingVersion VARCHAR(75) null;
 
 alter table DLFileShortcut add status INTEGER;
 alter table DLFileShortcut add statusByUserId LONG;
 alter table DLFileShortcut add statusByUserName VARCHAR(75);
 alter table DLFileShortcut add statusDate DATE;
 
+COMMIT_TRANSACTION;
+
+update DLFileShortcut set status = 0;
+update DLFileShortcut set statusByUserId = userId;
+update DLFileShortcut set statusByUserName = userId;
+update DLFileShortcut set statusDate = createDate;
+
 alter table DLFileVersion add description STRING null;
 alter table DLFileVersion add status INTEGER;
 alter table DLFileVersion add statusByUserId LONG;
 alter table DLFileVersion add statusByUserName VARCHAR(75);
 alter table DLFileVersion add statusDate DATE;
+
+COMMIT_TRANSACTION;
+
+update DLFileVersion set status = 0;
+update DLFileVersion set statusByUserId = userId;
+update DLFileVersion set statusByUserName = userId;
+update DLFileVersion set statusDate = createDate;
 
 alter table JournalArticle add status INTEGER;
 alter table JournalArticle add statusByUserId LONG;
@@ -144,7 +161,7 @@ alter table JournalArticle add statusDate DATE;
 COMMIT_TRANSACTION;
 
 update JournalArticle set status = 0 where approved = TRUE;
-update JournalArticle set status = 1 where approved = FALSE;
+update JournalArticle set status = 0 where approved = FALSE;
 update JournalArticle set statusByUserId = approvedByUserId;
 update JournalArticle set statusByUserName = approvedByUserName;
 update JournalArticle set statusDate = approvedDate where expired = FALSE;
@@ -195,6 +212,11 @@ alter table MBMessage add statusDate DATE;
 COMMIT_TRANSACTION;
 
 update MBMessage set allowPingbacks = TRUE;
+update MBMessage set status = 0;
+update MBMessage set statusByUserId = userId;
+update MBMessage set statusByUserName = userId;
+update MBMessage set statusDate = createDate;
+
 alter table MBThread add status INTEGER;
 alter table MBThread add statusByUserId LONG;
 alter table MBThread add statusByUserName VARCHAR(75);
@@ -202,7 +224,7 @@ alter table MBThread add statusDate DATE;
 
 COMMIT_TRANSACTION;
 
-update MBThread set status = 1;
+update MBThread set status = 0;
 
 alter table Release_ add servletContextName VARCHAR(75);
 
@@ -223,6 +245,13 @@ alter table WikiPage add status INTEGER;
 alter table WikiPage add statusByUserId LONG;
 alter table WikiPage add statusByUserName VARCHAR(75);
 alter table WikiPage add statusDate DATE;
+
+COMMIT_TRANSACTION;
+
+update WikiPage set status = 0;
+update WikiPage set statusByUserId = userId;
+update WikiPage set statusByUserName = userId;
+update WikiPage set statusDate = createDate;
 
 create table WorkflowDefinitionLink (
 	workflowDefinitionLinkId LONG not null primary key,
