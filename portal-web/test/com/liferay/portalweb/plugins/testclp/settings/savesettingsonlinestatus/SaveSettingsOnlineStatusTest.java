ec2fc91874b82d8407c5cd66b4510bd8b92a90f8
Michael Hashimoto <michael.hashimoto@liferay.com>
2010-03-16 23:56:13 +0000

    LPS-8406
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@48320 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-web/test/com/liferay/portalweb/plugins/testclp/settings/savesettingsonlinestatus/SaveSettingsOnlineStatusTest.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portalweb.plugins.testclp.settings.savesettingsonlinestatus;
+
+import com.liferay.portalweb.portal.BaseTestCase;
+import com.liferay.portalweb.portal.util.RuntimeVariables;
+
+/**
+ * <a href="SaveSettingsOnlineStatusTest.java.html"><b><i>View Source</i></b>
+ * </a>
+ *
+ * @author Brian Wing Shun Chan
+ */
+public class SaveSettingsOnlineStatusTest extends BaseTestCase {
+	public void testSaveSettingsOnlineStatus() throws Exception {
+		selenium.open("/web/guest/home/");
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isElementPresent("link=Test CLP Test Page")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.clickAt("link=Test CLP Test Page", RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		assertEquals(RuntimeVariables.replace(""),
+			selenium.getText(
+				"//div[@class='portlet-body']/table/tbody/tr[2]/td[7]"));
+		selenium.clickAt("//ul[@class='chat-tabs']/li[2]/div[1]/span",
+			RuntimeVariables.replace(""));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isVisible("statusMessage")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.type("statusMessage",
+			RuntimeVariables.replace("status message."));
+		selenium.clickAt("saveSettings", RuntimeVariables.replace(""));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (!selenium.isElementPresent(
+							"//li[@class='chat-settings saved']")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		assertFalse(selenium.isElementPresent(
+				"//li[@class='chat-settings saved']"));
+		selenium.open("/web/guest/home/");
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isElementPresent("link=Test CLP Test Page")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.clickAt("link=Test CLP Test Page", RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		assertEquals(RuntimeVariables.replace("status message."),
+			selenium.getText(
+				"//div[@class='portlet-body']/table/tbody/tr[2]/td[7]"));
+	}
+}
---
+++ portal-web/test/com/liferay/portalweb/plugins/testclp/settings/savesettingsonlinestatus/TearDownSettingsTest.java
@@ -0,0 +1,143 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portalweb.plugins.testclp.settings.savesettingsonlinestatus;
+
+import com.liferay.portalweb.portal.BaseTestCase;
+import com.liferay.portalweb.portal.util.RuntimeVariables;
+
+/**
+ * <a href="TearDownSettingsTest.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ */
+public class TearDownSettingsTest extends BaseTestCase {
+	public void testTearDownSettings() throws Exception {
+		int label = 1;
+
+		while (label >= 1) {
+			switch (label) {
+			case 1:
+				selenium.open("/web/guest/home/");
+
+				for (int second = 0;; second++) {
+					if (second >= 60) {
+						fail("timeout");
+					}
+
+					try {
+						if (selenium.isElementPresent("link=Test CLP Test Page")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				selenium.clickAt("link=Test CLP Test Page",
+					RuntimeVariables.replace(""));
+				selenium.waitForPageToLoad("30000");
+				selenium.clickAt("//ul[@class='chat-tabs']/li[2]/div[1]/span",
+					RuntimeVariables.replace(""));
+
+				for (int second = 0;; second++) {
+					if (second >= 60) {
+						fail("timeout");
+					}
+
+					try {
+						if (selenium.isVisible("statusMessage")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				selenium.type("statusMessage", RuntimeVariables.replace(""));
+
+				boolean onlineStatusChecked = selenium.isChecked("onlineStatus");
+
+				if (onlineStatusChecked) {
+					label = 2;
+
+					continue;
+				}
+
+				selenium.clickAt("onlineStatus", RuntimeVariables.replace(""));
+
+			case 2:
+
+				boolean playSoundChecked = selenium.isChecked("playSound");
+
+				if (playSoundChecked) {
+					label = 3;
+
+					continue;
+				}
+
+				selenium.clickAt("playSound", RuntimeVariables.replace(""));
+
+			case 3:
+				selenium.clickAt("saveSettings", RuntimeVariables.replace(""));
+
+				for (int second = 0;; second++) {
+					if (second >= 60) {
+						fail("timeout");
+					}
+
+					try {
+						if (!selenium.isElementPresent(
+									"//li[@class='chat-settings saved']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				selenium.clickAt("saveSettings", RuntimeVariables.replace(""));
+
+				for (int second = 0;; second++) {
+					if (second >= 60) {
+						fail("timeout");
+					}
+
+					try {
+						if (!selenium.isElementPresent(
+									"//li[@class='chat-settings saved']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				assertFalse(selenium.isElementPresent(
+						"//li[@class='chat-settings saved']"));
+
+			case 100:
+				label = -1;
+			}
+		}
+	}
+}
