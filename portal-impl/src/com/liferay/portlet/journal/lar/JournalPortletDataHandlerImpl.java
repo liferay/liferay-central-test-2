775b811162b051eedf398b9256c9f9d2489ac777
Juan Fernandez <juan.fernandez@liferay.com>
2010-03-23 12:58:29 +0000

    LPS-8124 deleting articles in staging will not delete the articles from live when publishing web content
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@48952 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/journal/lar/JournalPortletDataHandlerImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/lar/JournalPortletDataHandlerImpl.java
@@ -1411,8 +1411,8 @@ public class JournalPortletDataHandlerImpl extends BasePortletDataHandler {
 
 	public PortletDataHandlerControl[] getExportControls() {
 		return new PortletDataHandlerControl[] {
-			_articles, _structuresTemplatesAndFeeds, _embeddedAssets, _images,
-			_categories, _comments, _ratings, _tags
+			_deleteExistingData, _articles, _structuresTemplatesAndFeeds,
+			_embeddedAssets, _images, _categories, _comments, _ratings, _tags
 		};
 	}
 
@@ -1429,6 +1429,12 @@ public class JournalPortletDataHandlerImpl extends BasePortletDataHandler {
 		throws PortletDataException {
 
 		try {
+			if (context.getBooleanParameter(
+				_NAMESPACE, "delete-existing-data")) {
+
+				deleteData(context, portletId, preferences);
+			}
+
 			Document doc = SAXReaderUtil.read(data);
 
 			Element root = doc.getRootElement();
@@ -1772,6 +1778,9 @@ public class JournalPortletDataHandlerImpl extends BasePortletDataHandler {
 	private static final boolean _ALWAYS_EXPORTABLE = true;
 
 	private static final String _NAMESPACE = "journal";
+
+	private static final PortletDataHandlerBoolean _deleteExistingData =
+		new PortletDataHandlerBoolean(_NAMESPACE, "delete-existing-data");
 
 	private static final PortletDataHandlerBoolean _embeddedAssets =
 		new PortletDataHandlerBoolean(_NAMESPACE, "embedded-assets");
