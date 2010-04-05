caa93cf5d98d4cb1fda54583671b935d56f7ca36
Brian Chan <brian.chan@liferay.com>
2010-04-05 20:52:44 +0000

    LPS-8781 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@49619 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/servlet/ComboServlet.java
+++ portal-impl/src/com/liferay/portal/servlet/ComboServlet.java
@@ -14,25 +14,23 @@
 
 package com.liferay.portal.servlet;
 
-import com.liferay.portal.cache.key.JavaMD5CacheKeyGenerator;
+import com.liferay.portal.kernel.cache.key.CacheKeyGeneratorUtil;
 import com.liferay.portal.kernel.servlet.ServletContextUtil;
 import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.util.SystemProperties;
+import com.liferay.util.servlet.ServletResponseUtil;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import java.util.Enumeration;
 import java.util.Map;
 
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -46,13 +44,11 @@ public class ComboServlet extends HttpServlet {
 
 	public void service(
 			HttpServletRequest request, HttpServletResponse response)
-		throws IOException, ServletException {
+		throws IOException {
 
 		Map<String, String[]> parameterMap = request.getParameterMap();
 
-		int totalParameters = parameterMap.size();
-
-		if (totalParameters == 0) {
+		if (parameterMap.size() == 0) {
 			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
 
 			return;
@@ -60,63 +56,49 @@ public class ComboServlet extends HttpServlet {
 
 		String content = null;
 
-		String cacheFileName = getCacheFileName(request);
+		File cacheFile = getCacheFile(request);
 
-		File cacheDataFile = new File(cacheFileName);
-
-		if (cacheDataFile.exists()) {
-			content = FileUtil.read(cacheDataFile);
+		if (cacheFile.exists()) {
+			content = FileUtil.read(cacheFile);
 		}
 		else {
-			_sb = new StringBundler(totalParameters);
+			StringBundler sb = new StringBundler(parameterMap.size());
 
 			for (String modulePath : parameterMap.keySet()) {
-				File file = getFile(request, modulePath);
+				File file = getFile(modulePath);
 
 				if (file != null) {
 					String moduleContent = FileUtil.read(file);
 
-					_sb.append(moduleContent);
+					sb.append(moduleContent);
 				}
 			}
 
-			content = _sb.toString();
+			content = sb.toString();
 
-			if (Validator.isNotNull(content)) {
-				FileUtil.write(cacheDataFile, content);
-			}
+			FileUtil.write(cacheFile, content);
 		}
 
 		String contentType = ContentTypes.TEXT_JAVASCRIPT;
 
-		String firstModuleName =
+		String firstModulePath =
 			(String)request.getParameterNames().nextElement();
 
-		String extension = FileUtil.getExtension(firstModuleName);
+		String extension = FileUtil.getExtension(firstModulePath);
 
-		if ((extension != null) &&
-			extension.equalsIgnoreCase(_CSS_EXTENSION)) {
-
+		if (extension.equalsIgnoreCase(_CSS_EXTENSION)) {
 			contentType = ContentTypes.TEXT_CSS;
 		}
 
 		response.setContentType(contentType);
 
-		PrintWriter pw = response.getWriter();
-
-		pw.write(content);
-
-		pw.close();
+		ServletResponseUtil.write(response, content);
 	}
 
-	protected String getCacheFileName(HttpServletRequest request)
-		throws IOException {
-
+	protected File getCacheFile(HttpServletRequest request) throws IOException {
 		StringBundler sb = new StringBundler(5);
 
-		String requestURI = request.getRequestURI();
-
-		sb.append(requestURI);
+		sb.append(request.getRequestURI());
 
 		String queryString = request.getQueryString();
 
@@ -125,70 +107,71 @@ public class ComboServlet extends HttpServlet {
 			sb.append(queryString);
 		}
 
-		long modifiedTimes = 0;
+		long lastModified = 0;
 
 		Enumeration<String> enu = request.getParameterNames();
 
 		while (enu.hasMoreElements()) {
 			String modulePath = enu.nextElement();
 
-			File file = getFile(request, modulePath);
+			File file = getFile(modulePath);
 
 			if (file != null) {
-				modifiedTimes += file.lastModified();
+				lastModified += file.lastModified();
 			}
 		}
 
-		sb.append(StringPool.AMPERSAND);
-		sb.append(modifiedTimes);
+		if (lastModified > 0) {
+			sb.append(StringPool.AMPERSAND);
+			sb.append(lastModified);
+		}
 
-		String cacheFileName = sb.toString();
+		String cacheFileName = _TEMP_DIR.concat(
+			CacheKeyGeneratorUtil.getCacheKey(
+				ComboServlet.class.getName(), sb.toString()));
 
-		JavaMD5CacheKeyGenerator cacheKeyGenerator =
-			new JavaMD5CacheKeyGenerator();
-
-		String cacheKey = cacheKeyGenerator.getCacheKey(cacheFileName);
-
-		StringBundler sbPath = new StringBundler(2);
-
-		sbPath.append(_tempDir);
-		sbPath.append(cacheKey);
-
-		return sbPath.toString();
+		return new File(cacheFileName);
 	}
 
-	protected File getFile(
-			HttpServletRequest request, String modulePath)
-		throws IOException {
-
+	protected File getFile(String path) throws IOException {
 		ServletContext servletContext = getServletContext();
 
 		String basePath = ServletContextUtil.getRealPath(
 			servletContext, _ALLOY_DIR);
 
-		String realPath = ServletContextUtil.getRealPath(
-			servletContext, modulePath);
-
-		if ((basePath == null) || (realPath == null)) {
+		if (basePath == null) {
 			return null;
 		}
 
 		basePath = StringUtil.replace(
 			basePath, StringPool.BACK_SLASH, StringPool.SLASH);
 
-		realPath = StringUtil.replace(
-			realPath, StringPool.BACK_SLASH, StringPool.SLASH);
-
 		File baseDir = new File(basePath);
-		File file = new File(realPath);
 
-		if (baseDir.exists() && file.exists()) {
-			String rawBasePath = baseDir.getCanonicalPath();
-			String rawFilePath = file.getCanonicalPath();
+		if (!baseDir.exists()) {
+			return null;
+		}
 
-			if (rawFilePath.indexOf(rawBasePath) == 0) {
-				return file;
-			}
+		String filePath = ServletContextUtil.getRealPath(servletContext, path);
+
+		if (filePath == null) {
+			return null;
+		}
+
+		filePath = StringUtil.replace(
+			filePath, StringPool.BACK_SLASH, StringPool.SLASH);
+
+		File file = new File(filePath);
+
+		if (!file.exists()) {
+			return null;
+		}
+
+		String baseCanonicalPath = baseDir.getCanonicalPath();
+		String fileCanonicalPath = file.getCanonicalPath();
+
+		if (fileCanonicalPath.indexOf(baseCanonicalPath) == 0) {
+			return file;
 		}
 
 		return null;
@@ -199,10 +182,6 @@ public class ComboServlet extends HttpServlet {
 	private static final String _CSS_EXTENSION = "css";
 
 	private static final String _TEMP_DIR =
-		SystemProperties.get(SystemProperties.TMP_DIR) +
-			"/liferay/combo/portal/";
-
-	private StringBundler _sb;
-	private String _tempDir = _TEMP_DIR;
+		SystemProperties.get(SystemProperties.TMP_DIR) + "/liferay/combo/";
 
 }
