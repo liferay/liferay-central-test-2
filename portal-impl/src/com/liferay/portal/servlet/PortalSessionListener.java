b7b65531ee61dc5f0804311d369ae3bd0e885868
Michael Young <michael.young@liferay.com>
2010-04-21 21:37:06 +0000

    LPS-9143
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@50701 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/servlet/PortalSessionListener.java
+++ portal-impl/src/com/liferay/portal/servlet/PortalSessionListener.java
@@ -16,24 +16,15 @@ package com.liferay.portal.servlet;
 
 import com.liferay.portal.events.EventsProcessorUtil;
 import com.liferay.portal.kernel.events.ActionException;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.messaging.DestinationNames;
-import com.liferay.portal.kernel.messaging.MessageBusUtil;
+import com.liferay.portal.kernel.util.PortalInitableUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
-import com.liferay.portal.model.User;
-import com.liferay.portal.service.UserLocalServiceUtil;
-import com.liferay.portal.util.PortalInstances;
 import com.liferay.portal.util.PropsValues;
-import com.liferay.portal.util.WebKeys;
 
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
-
-import org.apache.struts.Globals;
 
 /**
  * <a href="PortalSessionListener.java.html"><b><i>View Source</i></b></a>
@@ -64,91 +55,10 @@ public class PortalSessionListener implements HttpSessionListener {
 	}
 
 	public void sessionDestroyed(HttpSessionEvent event) {
-		if (PropsValues.SESSION_DISABLED) {
-			return;
-		}
+		SessionListenerInitable sessionListenerInitable =
+			new SessionListenerInitable(event);
 
-		HttpSession session = event.getSession();
-
-		PortalSessionContext.remove(session.getId());
-
-		try {
-			Long userIdObj = (Long)session.getAttribute(WebKeys.USER_ID);
-
-			if (userIdObj == null) {
-				_log.warn("User id is not in the session");
-			}
-
-			if (userIdObj == null) {
-				return;
-			}
-
-			// Language
-
-			session.removeAttribute(Globals.LOCALE_KEY);
-
-			// Live users
-
-			if (PropsValues.LIVE_USERS_ENABLED) {
-				long userId = userIdObj.longValue();
-				long companyId = getCompanyId(userId);
-				String sessionId = session.getId();
-
-				JSONObject jsonObj = JSONFactoryUtil.createJSONObject();
-
-				jsonObj.put("command", "signOut");
-				jsonObj.put("companyId", companyId);
-				jsonObj.put("userId", userId);
-				jsonObj.put("sessionId", sessionId);
-
-				MessageBusUtil.sendMessage(
-					DestinationNames.LIVE_USERS, jsonObj);
-			}
-		}
-		catch (IllegalStateException ise) {
-			_log.warn("Please upgrade to a servlet 2.4 compliant container");
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-
-		session.removeAttribute(WebKeys.PORTLET_SESSION_TRACKER);
-
-		// Process session destroyed events
-
-		try {
-			EventsProcessorUtil.process(
-				PropsKeys.SERVLET_SESSION_DESTROY_EVENTS,
-				PropsValues.SERVLET_SESSION_DESTROY_EVENTS, session);
-		}
-		catch (ActionException ae) {
-			_log.error(ae, ae);
-		}
-	}
-
-	protected long getCompanyId(long userId) throws Exception {
-		long[] companyIds = PortalInstances.getCompanyIds();
-
-		long companyId = 0;
-
-		if (companyIds.length == 1) {
-			companyId = companyIds[0];
-		}
-		else if (companyIds.length > 1) {
-			try {
-				User user = UserLocalServiceUtil.getUserById(userId);
-
-				companyId = user.getCompanyId();
-			}
-			catch (Exception e) {
-				if (_log.isWarnEnabled()) {
-					_log.warn(
-						"Unable to set the company id for user " + userId, e);
-				}
-			}
-		}
-
-		return companyId;
+		PortalInitableUtil.init(sessionListenerInitable);
 	}
 
 	private static Log _log = LogFactoryUtil.getLog(
---
+++ portal-impl/src/com/liferay/portal/servlet/SessionListenerInitable.java
@@ -0,0 +1,143 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.servlet;
+
+import com.liferay.portal.events.EventsProcessorUtil;
+import com.liferay.portal.kernel.events.ActionException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.messaging.DestinationNames;
+import com.liferay.portal.kernel.messaging.MessageBusUtil;
+import com.liferay.portal.kernel.util.PortalInitable;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portal.util.PortalInstances;
+import com.liferay.portal.util.PropsValues;
+import com.liferay.portal.util.WebKeys;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+
+import org.apache.struts.Globals;
+
+/**
+ * <a href="SessionListenerInitable.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Michael Young
+ *
+ */
+public class SessionListenerInitable implements PortalInitable {
+
+	public SessionListenerInitable(HttpSessionEvent event) {
+		_event = event;
+	}
+
+	public void portalInit() {
+		if (PropsValues.SESSION_DISABLED) {
+			return;
+		}
+
+		HttpSession session = _event.getSession();
+
+		PortalSessionContext.remove(session.getId());
+
+		try {
+			Long userIdObj = (Long)session.getAttribute(WebKeys.USER_ID);
+
+			if (userIdObj == null) {
+				_log.warn("User id is not in the session");
+			}
+
+			if (userIdObj == null) {
+				return;
+			}
+
+			// Language
+
+			session.removeAttribute(Globals.LOCALE_KEY);
+
+			// Live users
+
+			if (PropsValues.LIVE_USERS_ENABLED) {
+				long userId = userIdObj.longValue();
+				long companyId = getCompanyId(userId);
+				String sessionId = session.getId();
+
+				JSONObject jsonObj = JSONFactoryUtil.createJSONObject();
+
+				jsonObj.put("command", "signOut");
+				jsonObj.put("companyId", companyId);
+				jsonObj.put("userId", userId);
+				jsonObj.put("sessionId", sessionId);
+
+				MessageBusUtil.sendMessage(
+					DestinationNames.LIVE_USERS, jsonObj);
+			}
+		}
+		catch (IllegalStateException ise) {
+			_log.warn("Please upgrade to a servlet 2.4 compliant container");
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
+
+		session.removeAttribute(WebKeys.PORTLET_SESSION_TRACKER);
+
+		// Process session destroyed events
+
+		try {
+			EventsProcessorUtil.process(
+				PropsKeys.SERVLET_SESSION_DESTROY_EVENTS,
+				PropsValues.SERVLET_SESSION_DESTROY_EVENTS, session);
+		}
+		catch (ActionException ae) {
+			_log.error(ae, ae);
+		}
+	}
+
+	protected long getCompanyId(long userId) throws Exception {
+		long[] companyIds = PortalInstances.getCompanyIds();
+
+		long companyId = 0;
+
+		if (companyIds.length == 1) {
+			companyId = companyIds[0];
+		}
+		else if (companyIds.length > 1) {
+			try {
+				User user = UserLocalServiceUtil.getUserById(userId);
+
+				companyId = user.getCompanyId();
+			}
+			catch (Exception e) {
+				if (_log.isWarnEnabled()) {
+					_log.warn(
+						"Unable to set the company id for user " + userId, e);
+				}
+			}
+		}
+
+		return companyId;
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(
+		SessionListenerInitable.class);
+
+	private HttpSessionEvent _event;
+
+}
