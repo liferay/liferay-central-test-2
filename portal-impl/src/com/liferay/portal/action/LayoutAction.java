44a7f4a7f49f2c22c3e09ce73b15f6ca93d87368
Juan Fernandez <juan.fernandez@liferay.com>
2010-04-26 14:19:22 +0000

    LPS-9115 Abuse Reporting - Submitting an abuse report causes exceptions
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@51003 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/action/LayoutAction.java
+++ portal-impl/src/com/liferay/portal/action/LayoutAction.java
@@ -907,24 +907,26 @@ public class LayoutAction extends Action {
 
 		renderResponseImpl.transferHeaders(response);
 
-		if (stringResponse.isCalledGetOutputStream()) {
-			UnsyncByteArrayOutputStream ubaos =
-				stringResponse.getUnsyncByteArrayOutputStream();
+		if (Validator.isNotNull(stringResponse)) {
+			if (stringResponse.isCalledGetOutputStream()) {
+				UnsyncByteArrayOutputStream ubaos =
+					stringResponse.getUnsyncByteArrayOutputStream();
 
-			InputStream is = new UnsyncByteArrayInputStream(
-				ubaos.unsafeGetByteArray(), 0, ubaos.size());
+				InputStream is = new UnsyncByteArrayInputStream(
+					ubaos.unsafeGetByteArray(), 0, ubaos.size());
 
-			ServletResponseUtil.sendFile(
-				request, response, renderResponseImpl.getResourceName(), is,
-				renderResponseImpl.getContentType());
-		}
-		else {
-			byte[] content = stringResponse.getString().getBytes(
-				StringPool.UTF8);
+				ServletResponseUtil.sendFile(
+					request, response, renderResponseImpl.getResourceName(), is,
+					renderResponseImpl.getContentType());
+			}
+			else {
+				byte[] content = stringResponse.getString().getBytes(
+					StringPool.UTF8);
 
-			ServletResponseUtil.sendFile(
-				request, response, renderResponseImpl.getResourceName(),
-				content, renderResponseImpl.getContentType());
+				ServletResponseUtil.sendFile(
+					request, response, renderResponseImpl.getResourceName(),
+					content, renderResponseImpl.getContentType());
+			}
 		}
 
 		renderRequestImpl.cleanUp();
