6398996ea62d2436d16a4056b7bd4eb979462fd3
Brian Chan <brian.chan@liferay.com>
2010-04-26 15:01:07 +0000

    LPS-9269 Manual
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@51007 05bdf26c-840f-0410-9ced-eb539d925f36

--- sql/update-6.0.1-6.0.2.sql
+++ sql/update-6.0.1-6.0.2.sql
@@ -1,1 +1,45 @@
+alter table AssetEntry add socialInformationK DOUBLE;
+alter table AssetEntry add socialInformationB DOUBLE;
+alter table AssetEntry add socialInformationEquity DOUBLE;
+
 alter table LayoutSet add settings_ TEXT null;
+
+create table SocialEquityHistory (
+	equityHistoryId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	createDate DATE null,
+	personalEquity INTEGER
+);
+
+create table SocialEquityLog (
+	equityLogId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	assetEntryId LONG,
+	actionId VARCHAR(75) null,
+	actionDate INTEGER,
+	type_ INTEGER,
+	value INTEGER,
+	validity INTEGER
+);
+
+create table SocialEquitySetting (
+	equitySettingId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	classNameId LONG,
+	actionId VARCHAR(75) null,
+	type_ INTEGER,
+	value INTEGER,
+	validity INTEGER,
+	active_ BOOLEAN
+);
+
+alter table User_ add socialContributionEquity DOUBLE;
+alter table User_ add socialParticipationK DOUBLE;
+alter table User_ add socialParticipationB DOUBLE;
+alter table User_ add socialParticipationEquity DOUBLE;
+alter table User_ add socialPersonalEquity DOUBLE;
