e43567d162f7827ee75ded9d70ab23b2fa8f2ce9
Brian Chan <brian.chan@liferay.com>
2010-05-05 09:01:16 +0000

    LPS-9405 Rollback, see comments
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@51796 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
@@ -109,7 +109,7 @@ public class DLFileEntryLocalServiceImpl
 			String extraSettings, File file, ServiceContext serviceContext)
 		throws PortalException, SystemException {
 
-		if (file == null || file.length() == 0) {
+		if (file == null) {
 			throw new FileSizeException();
 		}
 
