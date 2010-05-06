810bbe79220e9469d2f04cda5c9b4cdeaf5da789
Jorge Ferrer <jorge.ferrer@liferay.com>
2010-05-06 14:16:08 +0000

    LPS-9458 Hide control panel workflow portlets when no workflow engine is deployed
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@51906 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-impl/src/com/liferay/portal/workflow/WorkflowControlPanelEntry.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow;
+
+import com.liferay.portal.kernel.workflow.WorkflowEngineManagerUtil;
+import com.liferay.portal.model.Portlet;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.BaseControlPanelEntry;
+
+/**
+ * <a href="WorkflowControlPanelEntry.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Jorge Ferrer
+ */
+public class WorkflowControlPanelEntry extends BaseControlPanelEntry {
+
+	public boolean isVisible(
+			Portlet portlet, String category, ThemeDisplay themeDisplay)
+		throws Exception {
+
+		if (WorkflowEngineManagerUtil.isDeployed()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public boolean isVisible(
+			PermissionChecker permissionChecker, Portlet portlet)
+		throws Exception {
+
+		if (WorkflowEngineManagerUtil.isDeployed()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+}
