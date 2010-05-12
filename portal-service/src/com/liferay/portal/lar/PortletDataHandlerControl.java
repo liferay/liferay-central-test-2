4ebba5241fbff90347502abb4c997bf6e8b6e22a
Brian Chan <brian.chan@liferay.com>
2010-05-12 09:36:11 +0000

    LPS-9533 Readd deprecated classes
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@52425 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-service/src/com/liferay/portal/lar/PortletDataHandlerControl.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.lar;
+
+/**
+ * <a href="PortletDataHandlerControl.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author	   Raymond Augé
+ * @deprecated This class has been repackaged at
+ *			   <code>com.liferay.portal.kernel.lar</code>.
+ */
+public class PortletDataHandlerControl
+	extends com.liferay.portal.kernel.lar.PortletDataHandlerControl {
+
+	public PortletDataHandlerControl(String namespace, String controlName) {
+		super(namespace, controlName);
+	}
+
+	public PortletDataHandlerControl(
+		String namespace, String controlName, boolean disabled) {
+
+		super(namespace, controlName, disabled);
+	}
+
+}
