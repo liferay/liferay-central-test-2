6b023b4e8d2e9ce588d4643835d4c1c130f62d20
Brian Chan <brian.chan@liferay.com>
2010-05-19 00:30:42 +0000

    LPS-9680 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@53116 05bdf26c-840f-0410-9ced-eb539d925f36

--- support-maven/plugins/liferay-maven-plugin/src/main/java/com/liferay/maven/plugins/ServiceBuilderMojo.java
+++ support-maven/plugins/liferay-maven-plugin/src/main/java/com/liferay/maven/plugins/ServiceBuilderMojo.java
@@ -14,27 +14,21 @@
 
 package com.liferay.maven.plugins;
 
-import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
-import com.liferay.portal.spring.util.SpringUtil;
 import com.liferay.portal.tools.servicebuilder.ServiceBuilder;
 import com.liferay.portal.util.InitUtil;
 import com.liferay.portal.util.PropsUtil;
-import com.liferay.portal.util.PropsValues;
-import com.liferay.util.SystemProperties;
 
 import java.io.File;
-import java.util.List;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.model.Dependency;
 
 /**
  * <a href="ServiceBuilderMojo.java.html"><b><i>View Source</i></b></a>
  *
  * Builds Liferay Service Builder services.
- * 
+ *
  * @author Mika Koivisto
  * @author Thiago Moreira
  * @goal   build-service
@@ -55,204 +49,169 @@ public class ServiceBuilderMojo extends AbstractMojo {
 		File inputFile = new File(serviceFileName);
 
 		if (!inputFile.exists()) {
-			getLog().warn(
-				"No service.xml file (" + inputFile.getAbsolutePath() + 
-					") found! Skipping service generation!");
+			getLog().warn(inputFile.getAbsolutePath() + " does not exist");
+
 			return;
 		}
 
-		getLog().info("Building service from " + serviceFileName);
+		getLog().info("Building from " + serviceFileName);
 
 		PropsUtil.set("spring.configs", "META-INF/service-builder-spring.xml");
-		PropsUtil.set(PropsKeys.RESOURCE_ACTIONS_READ_PORTLET_RESOURCES, "false");
+		PropsUtil.set(
+			PropsKeys.RESOURCE_ACTIONS_READ_PORTLET_RESOURCES, "false");
 
 		InitUtil.initWithSpring();
 
 		new ServiceBuilder(
 			serviceFileName, hbmFileName, ormFileName, modelHintsFileName,
-			springFileName,	springBaseFileName,
-			springDynamicDataSourceFileName, springHibernateFileName,
-			springInfrastructureFileName, springShardDataSourceFileName,
-			apiDir, implDir, jsonFileName, null, sqlDir, sqlFileName,
-			sqlIndexesFileName, sqlIndexesPropertiesFileName,
-			sqlSequencesFileName, autoNamespaceTables, beanLocatorUtil,
-			propsUtil, pluginName, null);
+			springFileName,	springBaseFileName, springDynamicDataSourceFileName,
+			springHibernateFileName, springInfrastructureFileName,
+			springShardDataSourceFileName, apiDir, implDir, jsonFileName, null,
+			sqlDir, sqlFileName, sqlIndexesFileName,
+			sqlIndexesPropertiesFileName, sqlSequencesFileName,
+			autoNamespaceTables, beanLocatorUtil, propsUtil, pluginName, null);
 	}
 
 	/**
-	 * Location of the service file.
-	 * 
-	 * @parameter default-value="${basedir}/src/main/webapp/WEB-INF/service.xml"
-	 * @required
-	 */
-	private String serviceFileName;
-
-	/**
-	 * Location of the portlet hibernate file.
-	 * 
-	 * @parameter default-value="${basedir}/src/main/resources/META-INF/portlet-hbm.xml"
-	 * @required
-	 */
-	private String hbmFileName;
-
-	/**
-	 * Location of the portlet orm file.
-	 * 
-	 * @parameter default-value="${basedir}/src/main/resources/META-INF/portlet-orm.xml"
-	 * @required
-	 */
-	private String ormFileName;
-
-	/**
-	 * Location of the portlet model hints file.
-	 * 
-	 * @parameter default-value="${basedir}/src/main/resources/META-INF/portlet-model-hints.xml"
-	 * @required
-	 */
-	private String modelHintsFileName;
-
-	/**
-	 * Location of the portlet spring file.
-	 * 
-	 * @parameter default-value="${basedir}/src/main/resources/META-INF/portlet-spring.xml"
-	 * @required
-	 */
-	private String springFileName;
-
-	/**
-	 * Location of the base spring file.
-	 * 
-	 * @parameter default-value="${basedir}/src/main/resources/META-INF/base-spring.xml"
-	 * @required
-	 */
-	private String springBaseFileName;
-
-	/**
-	 * Location of the dynamic data source spring file.
-	 * 
-	 * @parameter default-value="${basedir}/src/main/resources/META-INF/dynamic-data-source-spring.xml"
-	 * @required
-	 */
-	private String springDynamicDataSourceFileName;
-
-	/**
-	 * Location of the hibernate spring file.
-	 * 
-	 * @parameter default-value="${basedir}/src/main/resources/META-INF/hibernate-spring.xml"
-	 * @required
-	 */
-	private String springHibernateFileName;
-
-	/**
-	 * Location of the infrastructure spring file.
-	 * 
-	 * @parameter default-value="${basedir}/src/main/resources/META-INF/infrastructure-spring.xml"
-	 * @required
-	 */
-	private String springInfrastructureFileName;
-
-	/**
-	 * Location of the shard data source spring file.
-	 * 
-	 * @parameter default-value="${basedir}/src/main/resources/META-INF/shard-data-source-spring.xml"
-	 * @required
-	 */
-	private String springShardDataSourceFileName;
-
-	/**
-	 * Location of the generated api classes directory.
-	 * 
 	 * @parameter default-value="${basedir}/src/main/java"
 	 * @required
 	 */
 	private String apiDir;
 
 	/**
-	 * Location of the generated implementation classes directory.
-	 * 
-	 * @parameter default-value="${basedir}/src/main/java"
-	 * @required
-	 */
-	private String implDir;
-
-	/**
-	 * Name of the service.js file.
-	 * 
-	 * @parameter default-value="${basedir}/src/main/webapp/html/js/liferay/service.js"
-	 * @required
-	 */
-	private String jsonFileName;
-
-	/**
-	 * Location of the generated sql directory.
-	 * 
-	 * @parameter default-value="${basedir}/src/main/webapp/WEB-INF/sql"
-	 * @required
-	 */
-	private String sqlDir;
-
-	/**
-	 * Name of the tables.sql file.
-	 * 
-	 * @parameter default-value="tables.sql"
-	 * @required
-	 */
-	private String sqlFileName;
-
-	/**
-	 * Name of the indexes.sql file.
-	 * 
-	 * @parameter default-value="indexes.sql"
-	 * @required
-	 */
-	private String sqlIndexesFileName;
-
-	/**
-	 * Name of the indexes.properties file.
-	 * 
-	 * @parameter default-value="indexes.properties"
-	 * @required
-	 */
-	private String sqlIndexesPropertiesFileName;
-
-	/**
-	 * Name of the sequences.sql file.
-	 * 
-	 * @parameter default-value="sequences.sql"
-	 * @required
-	 */
-	private String sqlSequencesFileName;
-
-	/**
-	 * Set the auto creation of namespaced tables
-	 * 
 	 * @parameter default-value="true"
 	 * @required
 	 */
 	private boolean autoNamespaceTables;
 
 	/**
-	 * Set the bean locator class 
-	 * 
 	 * @parameter default-value="com.liferay.util.bean.PortletBeanLocatorUtil"
 	 * @required
 	 */
 	private String beanLocatorUtil;
 
 	/**
-	 * Set the system property class 
-	 * 
+	 * @parameter default-value=
+				  "${basedir}/src/main/resources/META-INF/portlet-hbm.xml"
+	 * @required
+	 */
+	private String hbmFileName;
+
+	/**
+	 * @parameter default-value="${basedir}/src/main/java"
+	 * @required
+	 */
+	private String implDir;
+
+	/**
+	 * @parameter default-value=
+				  "${basedir}/src/main/webapp/html/js/liferay/service.js"
+	 * @required
+	 */
+	private String jsonFileName;
+
+	/**
+	 * @parameter default-value=
+				  "${basedir}/src/main/resources/META-INF/portlet-model-hints.xml"
+	 * @required
+	 */
+	private String modelHintsFileName;
+
+	/**
+	 * @parameter default-value=
+				  "${basedir}/src/main/resources/META-INF/portlet-orm.xml"
+	 * @required
+	 */
+	private String ormFileName;
+
+	/**
+	 * @parameter expression="${project.artifactId}"
+	 * @required
+	 */
+	private String pluginName;
+
+	/**
 	 * @parameter default-value="com.liferay.util.service.ServiceProps"
 	 * @required
 	 */
 	private String propsUtil;
 
 	/**
-	 * Set the plugin name
-	 * 
-	 * @parameter expression="${project.artifactId}"
+	 * @parameter default-value="${basedir}/src/main/webapp/WEB-INF/service.xml"
 	 * @required
 	 */
-	private String pluginName;
+	private String serviceFileName;
+
+	/**
+	 * @parameter default-value=
+				  "${basedir}/src/main/resources/META-INF/base-spring.xml"
+	 * @required
+	 */
+	private String springBaseFileName;
+
+	/**
+	 * @parameter default-value=
+				  "${basedir}/src/main/resources/META-INF/dynamic-data-source-spring.xml"
+	 * @required
+	 */
+	private String springDynamicDataSourceFileName;
+
+	/**
+	 * @parameter default-value=
+				  "${basedir}/src/main/resources/META-INF/portlet-spring.xml"
+	 * @required
+	 */
+	private String springFileName;
+
+	/**
+	 * @parameter default-value=
+				  "${basedir}/src/main/resources/META-INF/hibernate-spring.xml"
+	 * @required
+	 */
+	private String springHibernateFileName;
+
+	/**
+	 * @parameter default-value=
+				  "${basedir}/src/main/resources/META-INF/infrastructure-spring.xml"
+	 * @required
+	 */
+	private String springInfrastructureFileName;
+
+	/**
+	 * @parameter default-value=
+				  "${basedir}/src/main/resources/META-INF/shard-data-source-spring.xml"
+	 * @required
+	 */
+	private String springShardDataSourceFileName;
+
+	/**
+	 * @parameter default-value="${basedir}/src/main/webapp/WEB-INF/sql"
+	 * @required
+	 */
+	private String sqlDir;
+
+	/**
+	 * @parameter default-value="tables.sql"
+	 * @required
+	 */
+	private String sqlFileName;
+
+	/**
+	 * @parameter default-value="indexes.sql"
+	 * @required
+	 */
+	private String sqlIndexesFileName;
+
+	/**
+	 * @parameter default-value="indexes.properties"
+	 * @required
+	 */
+	private String sqlIndexesPropertiesFileName;
+
+	/**
+	 * @parameter default-value="sequences.sql"
+	 * @required
+	 */
+	private String sqlSequencesFileName;
 
 }
