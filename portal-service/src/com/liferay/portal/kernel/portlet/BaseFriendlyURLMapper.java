da4de0438fec06c6d2d0e48d7833f37288120f6e
Brian Chan <brian.chan@liferay.com>
2010-05-27 23:11:52 +0000

    LPS-9920
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@54033 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-service/src/com/liferay/portal/kernel/portlet/BaseFriendlyURLMapper.java
+++ portal-service/src/com/liferay/portal/kernel/portlet/BaseFriendlyURLMapper.java
@@ -16,7 +16,7 @@ package com.liferay.portal.kernel.portlet;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.PortalClassInvoker;
+import com.liferay.portal.util.PortalUtil;
 
 import java.util.Map;
 
@@ -74,7 +74,7 @@ public abstract class BaseFriendlyURLMapper implements FriendlyURLMapper {
 		Map<String, String[]> params, String name, String value) {
 
 		try {
-			if (!_isReservedParameter(name)) {
+			if (!PortalUtil.isReservedParameter(name)) {
 				Map<String, String> prpIdentifers =
 					FriendlyURLMapperThreadLocal.getPRPIdentifiers();
 
@@ -95,7 +95,7 @@ public abstract class BaseFriendlyURLMapper implements FriendlyURLMapper {
 
 	protected String getNamespace() {
 		try {
-			return _getPortletNamespace(getPortletId());
+			return PortalUtil.getPortletNamespace(getPortletId());
 		}
 		catch (Exception e) {
 			_log.error(e, e);
@@ -104,41 +104,7 @@ public abstract class BaseFriendlyURLMapper implements FriendlyURLMapper {
 		}
 	}
 
-	private String _getPortletNamespace(String portletId)
-		throws Exception {
-
-		Object returnObj = PortalClassInvoker.invoke(
-			_CLASS, _METHOD_GETPORTLETNAMESPACE, portletId, false);
-
-		if (returnObj != null) {
-			return (String)returnObj;
-		}
-		else {
-			return null;
-		}
-	}
-
-	private boolean _isReservedParameter(String name) throws Exception {
-		Object returnObj = PortalClassInvoker.invoke(
-			_CLASS, _METHOD_ISRESERVEDPARAMETER, name, false);
-
-		if (returnObj != null) {
-			return (Boolean)returnObj;
-		}
-		else {
-			return false;
-		}
-	}
-
 	private static final boolean _CHECK_MAPPING_WITH_PREFIX = true;
-
-	private static final String _CLASS = "com.liferay.portal.util.PortalUtil";
-
-	private static final String _METHOD_GETPORTLETNAMESPACE =
-		"getPortletNamespace";
-
-	private static final String _METHOD_ISRESERVEDPARAMETER =
-		"isReservedParameter";
 
 	private static Log _log = LogFactoryUtil.getLog(
 		BaseFriendlyURLMapper.class);
