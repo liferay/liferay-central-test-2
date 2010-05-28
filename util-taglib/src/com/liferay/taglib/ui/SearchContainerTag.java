2586bc8da7f1a36d56bc3f25f187f14e5935e83f
Brian Chan <brian.chan@liferay.com>
2010-05-28 17:51:22 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@54101 05bdf26c-840f-0410-9ced-eb539d925f36

--- util-taglib/src/com/liferay/taglib/ui/SearchContainerTag.java
+++ util-taglib/src/com/liferay/taglib/ui/SearchContainerTag.java
@@ -87,7 +87,7 @@ public class SearchContainerTag extends ParamAndPropertyAncestorTagImpl {
 			if (_searchContainer == null) {
 				_searchContainer = new SearchContainer(
 					portletRequest, _displayTerms, _searchTerms, getCurParam(),
-					getDelta(), _iteratorURL,  null, _emptyResultsMessage);
+					getDelta(), _iteratorURL, null, _emptyResultsMessage);
 			}
 
 			_searchContainer.setDeltaConfigurable(_deltaConfigurable);
