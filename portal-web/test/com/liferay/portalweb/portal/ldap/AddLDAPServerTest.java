6419f2414aea3378a029d64e305b03a3be0deb70
JR Houn <jr.houn@liferay.com>
2010-05-29 00:47:31 +0000

    Test fixes
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@54181 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-web/test/com/liferay/portalweb/portal/ldap/AddLDAPServerTest.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portalweb.portal.ldap;
+
+import com.liferay.portalweb.portal.BaseTestCase;
+import com.liferay.portalweb.portal.util.RuntimeVariables;
+
+/**
+ * <a href="AddLDAPServerTest.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ */
+public class AddLDAPServerTest extends BaseTestCase {
+	public void testAddLDAPServer() throws Exception {
+		selenium.click(RuntimeVariables.replace("link=Control Panel"));
+		selenium.waitForPageToLoad("30000");
+		selenium.click(RuntimeVariables.replace("link=Settings"));
+		selenium.waitForPageToLoad("30000");
+		selenium.click("authenticationLink");
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isVisible("link=LDAP")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.click("link=LDAP");
+		selenium.clickAt("//input[@value=\"Add\"]", RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isVisible("//span/input")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.type("//span/input", RuntimeVariables.replace("Test LDAP 1"));
+		selenium.click("//input[@name='_130_defaultLdap' and @value='apache']");
+		selenium.click("//input[@value='Reset Values']");
+		selenium.click(RuntimeVariables.replace("//input[@value='Save']"));
+		selenium.waitForPageToLoad("30000");
+		assertTrue(selenium.isTextPresent(
+				"Your request processed successfully."));
+	}
+}
