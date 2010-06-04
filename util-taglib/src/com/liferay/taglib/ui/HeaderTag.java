07d00dce500debb27741385db971229df0eb045c
Brian Chan <brian.chan@liferay.com>
2010-06-04 16:51:16 +0000

    LPS-10075 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@54802 05bdf26c-840f-0410-9ced-eb539d925f36

--- util-taglib/src/com/liferay/taglib/ui/HeaderTag.java
+++ util-taglib/src/com/liferay/taglib/ui/HeaderTag.java
@@ -26,17 +26,17 @@ import javax.servlet.http.HttpServletRequest;
 public class HeaderTag extends IncludeTag {
 
 	protected void setAttributes(HttpServletRequest request) {
-		request.setAttribute("liferay-ui:header:title", _title);
 		request.setAttribute("liferay-ui:header:backLabel", _backLabel);
 		request.setAttribute("liferay-ui:header:backURL", _backURL);
 		request.setAttribute("liferay-ui:header:cssClass", _cssClass);
+		request.setAttribute("liferay-ui:header:title", _title);
 	}
 
 	protected void cleanUp() {
-		_title = null;
 		_backLabel = null;
 		_backURL = null;
 		_cssClass = null;
+		_title = null;
 	}
 
 	protected String getPage() {
@@ -45,10 +45,6 @@ public class HeaderTag extends IncludeTag {
 
 	protected boolean isCleanUpSetAttributes() {
 		return _CLEAN_UP_SET_ATTRIBUTES;
-	}
-
-	public void setTitle(String title) {
-		_title = title;
 	}
 
 	public void setBackLabel(String backLabel) {
@@ -63,13 +59,17 @@ public class HeaderTag extends IncludeTag {
 		_cssClass = cssClass;
 	}
 
+	public void setTitle(String title) {
+		_title = title;
+	}
+
 	private static final boolean _CLEAN_UP_SET_ATTRIBUTES = true;
 
 	private static final String _PAGE = "/html/taglib/ui/header/page.jsp";
 
-	private String _title;
 	private String _backLabel;
 	private String _backURL;
 	private String _cssClass;
+	private String _title;
 
 }
