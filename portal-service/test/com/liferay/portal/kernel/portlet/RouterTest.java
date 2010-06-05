759e6cb6f7e1dd673d31d5a1fa3bcc9326fc79f3
Brian Chan <brian.chan@liferay.com>
2010-06-05 00:47:10 +0000

    LPS-9954 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@54889 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-service/test/com/liferay/portal/kernel/portlet/RouterTest.java
+++ portal-service/test/com/liferay/portal/kernel/portlet/RouterTest.java
@@ -15,7 +15,6 @@
 package com.liferay.portal.kernel.portlet;
 
 import com.liferay.portal.kernel.test.TestCase;
-import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.util.InitUtil;
 
 import java.util.HashMap;
@@ -31,148 +30,161 @@ public class RouterTest extends TestCase {
 
 	public RouterTest() {
 		InitUtil.initWithSpring();
-		initRouter();
 	}
 
-	public void testUrlReproduction() {
-		assertUrlReproduced("GET/boxes/16");
-		assertUrlReproduced("GET/boxes/25.xml");
-		assertUrlReproduced("POST/boxes/8");
-		assertUrlReproduced("POST/boxes/34.xml");
-		assertUrlReproduced("GET/boxes/new");
-		assertUrlReproduced("GET/boxes/8/export");
-		assertUrlReproduced("GET/boxes");
-		assertUrlReproduced("GET/boxes.xml");
-		assertUrlReproduced("POST/boxes");
-		assertUrlReproduced("POST/boxes.xml");
-		assertUrlReproduced("POST/boxes.xml?hello=something");
+	public void setUp() {
+		_router = new Router();
+
+		Route route = _router.addRoute("GET/{controller}");
+
+		route.addDefaultParameter("action", "index");
+		route.addDefaultParameter("format", "html");
+		route.addDefaultParameter("method", "GET");
+
+		route = _router.addRoute("GET/{controller}.{format}");
+
+		route.addDefaultParameter("action", "index");
+		route.addDefaultParameter("method", "GET");
+
+		route = _router.addRoute("POST/{controller}");
+
+		route.addDefaultParameter("action", "create");
+		route.addDefaultParameter("format", "html");
+		route.addDefaultParameter("method", "POST");
+
+		route = _router.addRoute("POST/{controller}.{format}");
+
+		route.addDefaultParameter("action", "create");
+		route.addDefaultParameter("method", "POST");
+
+		route = _router.addRoute("GET/{controller}/{\\d+:id}");
+
+		route.addDefaultParameter("action", "view");
+		route.addDefaultParameter("format", "html");
+		route.addDefaultParameter("method", "GET");
+
+		route = _router.addRoute("GET/{controller}/{\\d+:id}.{format}");
+
+		route.addDefaultParameter("action", "view");
+		route.addDefaultParameter("method", "GET");
+
+		route = _router.addRoute("POST/{controller}/{\\d+:id}");
+
+		route.addDefaultParameter("action", "update");
+		route.addDefaultParameter("format", "html");
+		route.addDefaultParameter("method", "POST");
+
+		route = _router.addRoute("POST/{controller}/{\\d+:id}.{format}");
+
+		route.addDefaultParameter("action", "update");
+		route.addDefaultParameter("method", "POST");
+
+		route = _router.addRoute("{method}/{controller}/{\\d+:id}/{action}");
+
+		route.addDefaultParameter("format", "html");
+
+		route = _router.addRoute(
+			"{method}/{controller}/{\\d+:id}/{action}.{format}");
+
+		route = _router.addRoute("{method}/{controller}/{action}");
+
+		route.addDefaultParameter("format", "html");
+
+		route = _router.addRoute("{method}/{controller}/{action}.{format}");
 	}
-	
-	public void testRoutePriority() {
-		assertUrlReproduces("GET/boxes/index?place=house", "GET/boxes?place=house");
+
+	public void testPriority() {
+		assertEqualsParametersToUrl(
+			"GET/boxes/index?place=house", "GET/boxes?place=house");
 	}
-	
-	public void testRouteOverridesQueryString() {
-		assertUrlReproduces("GET/boxes/5/something?action=nowhere", "GET/boxes/5/something");
+
+	public void testQueryStringOverride() {
+		assertEqualsParametersToUrl(
+			"GET/boxes/5/something?action=nowhere", "GET/boxes/5/something");
 	}
-	
+
+	public void testReproduction() {
+		assertEqualsParametersToUrl("GET/boxes/16");
+		assertEqualsParametersToUrl("GET/boxes/25.xml");
+		assertEqualsParametersToUrl("POST/boxes/8");
+		assertEqualsParametersToUrl("POST/boxes/34.xml");
+		assertEqualsParametersToUrl("GET/boxes/new");
+		assertEqualsParametersToUrl("GET/boxes/8/export");
+		assertEqualsParametersToUrl("GET/boxes");
+		assertEqualsParametersToUrl("GET/boxes.xml");
+		assertEqualsParametersToUrl("POST/boxes");
+		assertEqualsParametersToUrl("POST/boxes.xml");
+		assertEqualsParametersToUrl("POST/boxes.xml?hello=something");
+	}
+
 	public void testUrlToParameters() {
-		assertUrlProducesParameters(
+		assertEqualsUrlToParameters(
 			"GET/boxes/16",
 			"id=16&action=view&method=GET&format=html&controller=boxes");
-		assertUrlProducesParameters(
+		assertEqualsUrlToParameters(
 			"GET/boxes/25.xml",
 			"id=25&action=view&method=GET&controller=boxes&format=xml");
-		assertUrlProducesParameters(
+		assertEqualsUrlToParameters(
 			"POST/boxes/8",
 			"id=8&action=update&method=POST&format=html&controller=boxes");
-		assertUrlProducesParameters(
+		assertEqualsUrlToParameters(
 			"POST/boxes/34.xml",
 			"id=34&action=update&method=POST&controller=boxes&format=xml");
-		assertUrlProducesParameters(
+		assertEqualsUrlToParameters(
 			"GET/boxes/new",
 			"action=new&method=GET&format=html&controller=boxes");
-		assertUrlProducesParameters(
+		assertEqualsUrlToParameters(
 			"GET/boxes/8/export",
 			"id=8&action=export&method=GET&format=html&controller=boxes");
-		assertUrlProducesParameters(
+		assertEqualsUrlToParameters(
 			"GET/boxes",
 			"action=index&method=GET&format=html&controller=boxes");
-		assertUrlProducesParameters(
+		assertEqualsUrlToParameters(
 			"GET/boxes.xml",
 			"action=index&method=GET&controller=boxes&format=xml");
-		assertUrlProducesParameters(
+		assertEqualsUrlToParameters(
 			"POST/boxes",
 			"action=create&method=POST&format=html&controller=boxes");
-		assertUrlProducesParameters(
+		assertEqualsUrlToParameters(
 			"POST/boxes.xml",
 			"action=create&method=POST&controller=boxes&format=xml");
 	}
-	
-	protected void assertUrlReproduced(String url) {
-		assertUrlReproduces(url, url);
+
+	protected void assertEquals(
+		Map<String, String> expected, Map<String, String> actual) {
+
+		assertEquals(expected.size(), actual.size());
+
+		for (Map.Entry<String, String> entry : expected.entrySet()) {
+			String name = entry.getKey();
+			String value = entry.getValue();
+
+			assertEquals(value, actual.get(name));
+		}
 	}
-	
-	protected void assertUrlReproduces(String url, String expected) {
+
+	protected void assertEqualsParametersToUrl(String url) {
+		assertEqualsParametersToUrl(url, url);
+	}
+
+	protected void assertEqualsParametersToUrl(String url, String expectedUrl) {
 		Map<String, String> parameters = _router.urlToParameters(url);
 
 		String generatedUrl = _router.parametersToUrl(parameters);
 
-		assertEquals(expected, generatedUrl);
+		assertEquals(expectedUrl, generatedUrl);
 	}
-	
-	protected void assertUrlProducesParameters(
-		String url, String queryStringParameters) {
-		
+
+	protected void assertEqualsUrlToParameters(String url, String queryString) {
 		Map<String, String> parameters = new HashMap<String, String>();
-		Router.queryStringToParameters(queryStringParameters, parameters);
-		
+
+		_router.queryStringToParameters(queryString, parameters);
+
 		Map<String, String> generatedParameters = _router.urlToParameters(url);
+
 		assertEquals(parameters, generatedParameters);
 	}
-	
-	protected void assertEquals(
-		Map<String, String> expected, Map<String, String> actual) {
-		
-		for (Map.Entry<String, String> entry : expected.entrySet()) {
-			String name = entry.getKey();
-			String value = entry.getValue();
-			assertEquals(value, actual.remove(name));
-		}
-		
-		assertEquals(0, actual.size());
-	}
-	
-	protected void initRouter() {
-		_router = new Router();
 
-		Route route = _router.addRoute("GET/{controller}");
-		route.addDefaultParameter("action", "index");
-		route.addDefaultParameter("method", "GET");
-		route.addDefaultParameter("format", "html");
-		
-		route = _router.addRoute("GET/{controller}.{format}");
-		route.addDefaultParameter("action", "index");
-		route.addDefaultParameter("method", "GET");
-		
-		route = _router.addRoute("POST/{controller}");
-		route.addDefaultParameter("action", "create");
-		route.addDefaultParameter("method", "POST");
-		route.addDefaultParameter("format", "html");
-		
-		route = _router.addRoute("POST/{controller}.{format}");
-		route.addDefaultParameter("action", "create");
-		route.addDefaultParameter("method", "POST");
-		
-		route = _router.addRoute("GET/{controller}/{\\d+:id}");
-		route.addDefaultParameter("action", "view");
-		route.addDefaultParameter("method", "GET");
-		route.addDefaultParameter("format", "html");
-		
-		route = _router.addRoute("GET/{controller}/{\\d+:id}.{format}");
-		route.addDefaultParameter("action", "view");
-		route.addDefaultParameter("method", "GET");
-		
-		route = _router.addRoute("POST/{controller}/{\\d+:id}");
-		route.addDefaultParameter("action", "update");
-		route.addDefaultParameter("method", "POST");
-		route.addDefaultParameter("format", "html");
-		
-		route = _router.addRoute("POST/{controller}/{\\d+:id}.{format}");
-		route.addDefaultParameter("action", "update");
-		route.addDefaultParameter("method", "POST");
-		
-		route = _router.addRoute("{method}/{controller}/{\\d+:id}/{action}");
-		route.addDefaultParameter("format", "html");
-		
-		route = _router.addRoute("{method}/{controller}/{\\d+:id}/{action}.{format}");
-		
-		route = _router.addRoute("{method}/{controller}/{action}");
-		route.addDefaultParameter("format", "html");
-		
-		route = _router.addRoute("{method}/{controller}/{action}.{format}");
-	}
-	
-	protected Router _router;
+	private Router _router;
 
 }
