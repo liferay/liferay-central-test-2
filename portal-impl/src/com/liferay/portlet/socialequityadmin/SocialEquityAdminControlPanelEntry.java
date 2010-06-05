9e63f136739f35440605caab01402a21d3d33da9
Brian Chan <brian.chan@liferay.com>
2010-06-05 17:15:48 +0000

    LPS-9269
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@54920 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-impl/src/com/liferay/portlet/socialequityadmin/SocialEquityAdminControlPanelEntry.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.socialequityadmin;
+
+import com.liferay.portal.model.Portlet;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PropsValues;
+import com.liferay.portlet.BaseControlPanelEntry;
+
+/**
+ * <a href="SocialEquityAdminControlPanelEntry.java.html"><b><i>View Source</i>
+ * </b></a>
+ *
+ * @author Brian Wing Shun Chan
+ */
+public class SocialEquityAdminControlPanelEntry extends BaseControlPanelEntry {
+
+	public boolean isVisible(
+			Portlet portlet, String category, ThemeDisplay themeDisplay)
+		throws Exception {
+
+		if (PropsValues.SOCIAL_EQUITY_EQUITY_LOG_ENABLED) {
+			return super.isVisible(portlet, category, themeDisplay);
+		}
+		else {
+			return false;
+		}
+	}
+
+	public boolean isVisible(
+			PermissionChecker permissionChecker, Portlet portlet)
+		throws Exception {
+
+		return false;
+	}
+
+}
