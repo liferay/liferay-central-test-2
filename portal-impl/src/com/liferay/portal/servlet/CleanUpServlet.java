e4b085ebfb73cb5f09fae39abf4009313cbbc369
Brian Chan <brian.chan@liferay.com>
2010-06-06 01:40:11 +0000

    LPS-10093
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@54934 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-impl/src/com/liferay/portal/servlet/CleanUpServlet.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.servlet;
+
+import com.liferay.portal.kernel.util.ThreadLocalRegistry;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * <a href="CleanUpServlet.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ */
+public class CleanUpServlet extends HttpServlet {
+
+	public void destroy() {
+		ThreadLocalRegistry.resetThreadLocals();
+	}
+
+	public void init() {
+		ThreadLocalRegistry.resetThreadLocals();
+	}
+
+}
