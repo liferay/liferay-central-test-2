d5a2558f979144e0c1deb3b6c1887c2dcf4a74f4
Brian Chan <brian.chan@liferay.com>
2010-06-07 18:51:19 +0000

    LPS-10106 SortFactory
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@55005 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-service/src/com/liferay/portal/kernel/search/SortFactoryUtil.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.search;
+
+/**
+ * <a href="SortFactoryUtil.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ */
+public class SortFactoryUtil {
+
+	public static Sort create(String fieldName, boolean reverse) {
+		return getSortFactory().create(fieldName, reverse);
+	}
+
+	public static Sort create(String fieldName, int type, boolean reverse) {
+		return getSortFactory().create(fieldName, type, reverse);
+	}
+
+	public static SortFactory getSortFactory() {
+		return _sortFactory;
+	}
+
+	public void setSortFactory(SortFactory sortFactory) {
+		_sortFactory = sortFactory;
+	}
+
+	private static SortFactory _sortFactory;
+
+}
