52e43ff694c6fed634fff153d9e24fdfc29bc6af
Brian Chan <brian.chan@liferay.com>
2010-06-07 18:54:01 +0000

    LPS-10106 SortFactory
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@55007 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-service/src/com/liferay/portal/kernel/search/SortFactoryImpl.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.search;
+
+/**
+ * <a href="SortFactoryImpl.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ */
+public class SortFactoryImpl implements SortFactory {
+
+	public Sort create(String fieldName, boolean reverse) {
+		return new Sort(fieldName, reverse);
+	}
+
+	public Sort create(String fieldName, int type, boolean reverse) {
+		return new Sort(fieldName, type, reverse);
+	}
+
+}
