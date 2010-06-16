13817d356ed9e8180a8b09993be861893c1f92a6
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2010-06-16 12:37:15 +0000

    LPS-9543 Pagination functionality in Web Content List portlet does not work
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@55620 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/journal/service/persistence/JournalArticleFinderImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/persistence/JournalArticleFinderImpl.java
@@ -15,6 +15,7 @@
 package com.liferay.portlet.journal.service.persistence;
 
 import com.liferay.portal.kernel.dao.orm.QueryPos;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.dao.orm.SQLQuery;
 import com.liferay.portal.kernel.dao.orm.Session;
 import com.liferay.portal.kernel.dao.orm.Type;
@@ -555,7 +556,8 @@ public class JournalArticleFinderImpl
 
 			List<JournalArticle> articles = new ArrayList<JournalArticle>();
 
-			Iterator<Object[]> itr = q.list().iterator();
+			Iterator<Object[]> itr = ((List<Object[]>) QueryUtil.list(
+				q, getDialect(), start, end)).iterator();
 
 			while (itr.hasNext()) {
 				Object value = itr.next();
