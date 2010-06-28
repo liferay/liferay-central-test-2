201145807bfeb61217e070d82610f0676bf1b421
Shuyang Zhou <shuyang.zhou@liferay.com>
2010-06-28 02:19:30 +0000

    LPS-10612 Workaround Property loading bug
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@56582 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/cluster/ClusterExecutorImpl.java
+++ portal-impl/src/com/liferay/portal/cluster/ClusterExecutorImpl.java
@@ -54,6 +54,7 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.locks.Lock;
@@ -482,8 +483,11 @@ public class ClusterExecutorImpl
 	}
 
 	protected void initChannels() {
-		String controlProperty =
-			PropsUtil.get(PropsKeys.CLUSTER_LINK_CHANNEL_PROPERTIES_CONTROL);
+		Properties controlProperties = PropsUtil.getProperties(
+			PropsKeys.CLUSTER_LINK_CHANNEL_PROPERTIES_CONTROL, false);
+
+		String controlProperty = controlProperties.getProperty(
+			PropsKeys.CLUSTER_LINK_CHANNEL_PROPERTIES_CONTROL);
 
 		ClusterRequestReceiver clusterInvokeReceiver =
 			new ClusterRequestReceiver(this);
