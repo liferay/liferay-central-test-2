1dcc0eecdaf0eec9cdfe4cdeedbc1d8bb72e3101
Michael Han <michael.han@liferay.com>
2010-06-30 05:54:24 +0000

    LPS-10755
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@56944 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/monitoring/MonitorNames.java
+++ portal-impl/src/com/liferay/portal/monitoring/MonitorNames.java
@@ -26,4 +26,6 @@ public interface MonitorNames {
 
 	public static final String PORTLET = "com.liferay.monitoring.Portlet";
 
+	public static final String SERVICE = "com.liferay.monitoring.Service";
+
 }
