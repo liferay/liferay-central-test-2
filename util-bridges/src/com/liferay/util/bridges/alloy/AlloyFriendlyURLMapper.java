717a13f8057ed058122bd0fa18e5f01b77ceda71
Connor Mckay <connor.mckay@liferay.com>
2010-07-01 21:04:06 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@57203 05bdf26c-840f-0410-9ced-eb539d925f36

--- util-bridges/src/com/liferay/util/bridges/alloy/AlloyFriendlyURLMapper.java
+++ util-bridges/src/com/liferay/util/bridges/alloy/AlloyFriendlyURLMapper.java
@@ -13,6 +13,7 @@
  */
 
 package com.liferay.util.bridges.alloy;
+
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.DefaultFriendlyURLMapper;
