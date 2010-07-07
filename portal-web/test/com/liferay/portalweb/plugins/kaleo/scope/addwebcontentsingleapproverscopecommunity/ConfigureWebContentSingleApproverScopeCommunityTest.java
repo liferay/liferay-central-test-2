d1653510823b639ec706f17fa62fa8abe55e3836
Michael Hashimoto <michael.hashimoto@liferay.com>
2010-07-07 22:04:08 +0000

    LPS-7644
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@57680 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-web/test/com/liferay/portalweb/plugins/kaleo/scope/addwebcontentsingleapproverscopecommunity/ConfigureWebContentSingleApproverScopeCommunityTest.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portalweb.plugins.kaleo.scope.addwebcontentsingleapproverscopecommunity;
+
+import com.liferay.portalweb.portal.BaseTestCase;
+import com.liferay.portalweb.portal.util.RuntimeVariables;
+
+/**
+ * <a href="ConfigureWebContentSingleApproverScopeCommunityTest.java.html"><b>
+ * <i>View Source</i></b></a>
+ *
+ * @author Brian Wing Shun Chan
+ */
+public class ConfigureWebContentSingleApproverScopeCommunityTest
+	extends BaseTestCase {
+	public void testConfigureWebContentSingleApproverScopeCommunity()
+		throws Exception {
+		selenium.open("/web/guest/home/");
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isElementPresent("link=Control Panel")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.clickAt("link=Control Panel", RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		selenium.clickAt("link=Workflow Configuration",
+			RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		selenium.clickAt("//div/span/a", RuntimeVariables.replace(""));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isVisible("//div[4]/div/div[1]/div[2]/ul/li[1]/a")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.clickAt("//div[4]/div/div[1]/div[2]/ul/li[1]/a",
+			RuntimeVariables.replace("Community Name"));
+		selenium.waitForPageToLoad("30000");
+		assertEquals(RuntimeVariables.replace("Community Name"),
+			selenium.getText("//div/span/a"));
+		selenium.select("_152_workflowDefinitionName@com.liferay.portlet.journal.model.JournalArticle",
+			RuntimeVariables.replace("label=Single Approver (Version 1)"));
+		selenium.clickAt("//input[@value='Save']", RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		assertEquals(RuntimeVariables.replace(
+				"Your request processed successfully."),
+			selenium.getText("//section/div/div/div/div"));
+		assertEquals("Single Approver (Version 1)",
+			selenium.getSelectedLabel(
+				"_152_workflowDefinitionName@com.liferay.portlet.journal.model.JournalArticle"));
+	}
+}
