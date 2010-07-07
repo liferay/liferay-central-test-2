4a371ecbde918fde12b8a7a1b345f25d5bde9a6c
Connor Mckay <connor.mckay@liferay.com>
2010-07-07 20:13:21 +0000

    LPS-11057 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@57658 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/tools/JavadocFormatter.java
+++ portal-impl/src/com/liferay/portal/tools/JavadocFormatter.java
@@ -47,10 +47,10 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.tools.ant.DirectoryScanner;
 
