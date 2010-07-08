74b3f768ccde25a9fad12c6cd061ec192d90760c
Connor Mckay <connor.mckay@liferay.com>
2010-07-08 22:58:41 +0000

    Javadoc formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@57835 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-service/src/com/liferay/portal/service/UserLocalService.java
+++ portal-service/src/com/liferay/portal/service/UserLocalService.java
@@ -31,7 +31,7 @@ import com.liferay.portal.kernel.exception.SystemException;
  * <p>
  * This interface defines the service. The default implementation is
  * {@link
- * com.liferay.portal.service.impl.UserLocalServiceImpl}}.
+ * com.liferay.portal.service.impl.UserLocalServiceImpl}.
  * Modify methods in that class and rerun ServiceBuilder to populate this class
  * and all other generated classes.
  * </p>
