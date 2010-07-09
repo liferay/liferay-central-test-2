3467e08125a4cae62c66cb764a7efa225dcae796
Douglas Wong <douglas.wong@liferay.com>
2010-07-09 00:01:54 +0000

    LPS-10724
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@57838 05bdf26c-840f-0410-9ced-eb539d925f36

--- sql/update-5.2.8-6.0.3.sql
+++ sql/update-5.2.8-6.0.3.sql
@@ -388,6 +388,15 @@ alter table AssetEntry add classUuid VARCHAR(75) null;
 
 alter table DLFileEntry add extension VARCHAR(75) null;
 
+alter table DLFileVersion add extension VARCHAR(75) null;
+alter table DLFileVersion add title VARCHAR(255) null;
+alter table DLFileVersion add changeLog STRING null;
+alter table DLFileVersion add extraSettings TEXT null;
+
+COMMIT_TRANSACTION;
+
+update DLFileVersion set changeLog = description;
+
 alter table Layout add uuid_ VARCHAR(75) null;
 
 alter table MBMessage add rootMessageId LONG;
