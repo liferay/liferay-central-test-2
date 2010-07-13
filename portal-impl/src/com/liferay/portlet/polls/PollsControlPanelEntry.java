903a69a13a17856437719650e98f2f6716d34dda
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2010-07-13 15:43:34 +0000

    LPS-4934 Content "Questions" in Shared Global is invisible in Polls Display portlet
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@58120 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-impl/src/com/liferay/portlet/polls/PollsControlPanelEntry.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.polls;
+
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.Portlet;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.BaseControlPanelEntry;
+
+/**
+ * @author Sergio González
+ */
+public class PollsControlPanelEntry extends BaseControlPanelEntry {
+
+	public boolean isVisible(
+			Portlet portlet, String category, ThemeDisplay themeDisplay)
+		throws Exception {
+
+		Group group = themeDisplay.getScopeGroup();
+
+		if (!group.isCompany()) {
+			return super.isVisible(portlet, category, themeDisplay);
+		}
+		else {
+			return false;
+		}
+	}
+
+	public boolean isVisible(
+			PermissionChecker permissionChecker, Portlet portlet)
+		throws Exception {
+
+		return false;
+	}
+
+}
