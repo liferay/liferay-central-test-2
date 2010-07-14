c1b0c77d7956ca09ad13670538c27edca15d6f11
Brian Chan <brian.chan@liferay.com>
2010-07-14 17:00:21 +0000

    LPS-11223 Update sql
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@58267 05bdf26c-840f-0410-9ced-eb539d925f36

--- sql/update-5.2.8-6.0.3.sql
+++ sql/update-5.2.8-6.0.3.sql
@@ -405,7 +405,68 @@ COMMIT_TRANSACTION;
 
 update MBMessage set rootMessageId = (select rootMessageId from MBThread where MBThread.threadId = MBMessage.threadId);
 
-alter table SocialEquityUser add rank INTEGER;
+drop table SocialEquityAssetEntry;
+create table SocialEquityAssetEntry (
+	equityAssetEntryId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	assetEntryId LONG,
+	informationK DOUBLE,
+	informationB DOUBLE
+);
+
+drop table SocialEquityHistory;
+create table SocialEquityHistory (
+	equityHistoryId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	createDate DATE null,
+	personalEquity INTEGER
+);
+
+drop table SocialEquityLog;
+create table SocialEquityLog (
+	equityLogId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	assetEntryId LONG,
+	actionId VARCHAR(75) null,
+	actionDate INTEGER,
+	active_ BOOLEAN,
+	expiration INTEGER,
+	type_ INTEGER,
+	value INTEGER
+);
+
+drop table SocialEquitySetting;
+create table SocialEquitySetting (
+	equitySettingId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	classNameId LONG,
+	actionId VARCHAR(75) null,
+	dailyLimit INTEGER,
+	lifespan INTEGER,
+	type_ INTEGER,
+	uniqueEntry BOOLEAN,
+	value INTEGER
+);
+
+drop table SocialEquityUser;
+create table SocialEquityUser (
+	equityUserId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	contributionK DOUBLE,
+	contributionB DOUBLE,
+	participationK DOUBLE,
+	participationB DOUBLE,
+	rank INTEGER
+);
 
 alter table User_ add facebookId LONG;
 
