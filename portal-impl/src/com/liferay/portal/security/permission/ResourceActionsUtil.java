974a2dbbc944ce47ff3951c57d8c76a8d865f553
Brian Chan <brian.chan@liferay.com>
2010-07-15 16:17:17 +0000

    LPS-11223 Sort
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@58351 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/security/permission/ResourceActionsUtil.java
+++ portal-impl/src/com/liferay/portal/security/permission/ResourceActionsUtil.java
@@ -1137,34 +1137,34 @@ public class ResourceActionsUtil {
 			return;
 		}
 
-		int informationValue = GetterUtil.getInteger(
-			socialEquityMappingElement.elementText("information-value"));
-		int informationLifespan = GetterUtil.getInteger(
-			socialEquityMappingElement.elementText("information-lifespan"));
 		int informationDailyLimit = GetterUtil.getInteger(
 			socialEquityMappingElement.elementText("information-daily-limit"));
-		int participationValue = GetterUtil.getInteger(
-			socialEquityMappingElement.elementText("participation-value"));
-		int participationLifespan = GetterUtil.getInteger(
-			socialEquityMappingElement.elementText("participation-lifespan"));
+		int informationLifespan = GetterUtil.getInteger(
+			socialEquityMappingElement.elementText("information-lifespan"));
+		int informationValue = GetterUtil.getInteger(
+			socialEquityMappingElement.elementText("information-value"));
 		int participationDailyLimit = GetterUtil.getInteger(
 			socialEquityMappingElement.elementText(
 				"participation-daily-limit"));
+		int participationLifespan = GetterUtil.getInteger(
+			socialEquityMappingElement.elementText("participation-lifespan"));
+		int participationValue = GetterUtil.getInteger(
+			socialEquityMappingElement.elementText("participation-value"));
 
 		SocialEquityActionMapping socialEquityActionMapping =
 			new SocialEquityActionMapping();
 
 		socialEquityActionMapping.setActionId(actionKey);
 		socialEquityActionMapping.setClassName(name);
-		socialEquityActionMapping.setInformationValue(informationValue);
-		socialEquityActionMapping.setInformationLifespan(informationLifespan);
 		socialEquityActionMapping.setInformationDailyLimit(
 			informationDailyLimit);
-		socialEquityActionMapping.setParticipationValue(participationValue);
-		socialEquityActionMapping.setParticipationLifespan(
-			participationLifespan);
+		socialEquityActionMapping.setInformationLifespan(informationLifespan);
+		socialEquityActionMapping.setInformationValue(informationValue);
 		socialEquityActionMapping.setParticipationDailyLimit(
 			participationDailyLimit);
+		socialEquityActionMapping.setParticipationLifespan(
+			participationLifespan);
+		socialEquityActionMapping.setParticipationValue(participationValue);
 
 		Map<String, SocialEquityActionMapping> socialEquityActionMappings =
 			_socialEquityActionMappings.get(name);
