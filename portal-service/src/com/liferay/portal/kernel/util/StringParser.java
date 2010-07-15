58cbd41085fb5ef92adc46efd8e9ada4a29cecfb
Connor Mckay <connor.mckay@liferay.com>
2010-07-15 23:41:16 +0000

    StringParser javadocs
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@58386 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-service/src/com/liferay/portal/kernel/util/StringParser.java
+++ portal-service/src/com/liferay/portal/kernel/util/StringParser.java
@@ -175,6 +175,20 @@ public class StringParser {
 		return s;
 	}
 
+	/**
+	 * Escapes the special characters in the string so that they will have no
+	 * special meaning in a regular expression.
+	 *
+	 * <p>
+	 * This method differs from {@link Pattern#quote(String)} by escaping each
+	 * special character with a backslash, rather than enclosing the entire
+	 * string in special quote tags. This allows the escaped string to be
+	 * manipulated or have sections replaced with non-literal sequences.
+	 * </p>
+	 *
+	 * @param  s the string to escape
+	 * @return the escaped string
+	 */
 	public static String escapeRegex(String s) {
 		Matcher matcher = _escapeRegexPattern.matcher(s);
 
