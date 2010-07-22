5fa9b1fc394b3740ab1f3762bd89a0a5aa6fdee0
Brian Chan <brian.chan@liferay.com>
2010-07-22 16:50:53 +0000

    LPS-11478 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@58869 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-service/src/com/liferay/portal/kernel/messaging/SerialDestination.java
+++ portal-service/src/com/liferay/portal/kernel/messaging/SerialDestination.java
@@ -31,6 +31,7 @@ public class SerialDestination extends BaseDestination {
 
 	public SerialDestination() {
 		super();
+
 		setWorkersCoreSize(_WORKERS_CORE_SIZE);
 		setWorkersMaxSize(_WORKERS_MAX_SIZE);
 	}
@@ -65,8 +66,8 @@ public class SerialDestination extends BaseDestination {
 		threadPoolExecutor.execute(runnable);
 	}
 
-	protected static final int _WORKERS_CORE_SIZE = 1;
+	private static final int _WORKERS_CORE_SIZE = 1;
 
-	protected static final int _WORKERS_MAX_SIZE = 1;
+	private static final int _WORKERS_MAX_SIZE = 1;
 
 }
