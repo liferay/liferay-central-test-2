9e2573fa51761f710a9a415da23eb128c6d212ff
Shuyang Zhou <shuyang.zhou@liferay.com>
2010-07-24 04:21:34 +0000

    LPS-10606 Refactor to use DelayQueue
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@59025 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/servlet/filters/secure/NonceUtil.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/secure/NonceUtil.java
@@ -20,12 +20,9 @@ import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.model.Company;
 import com.liferay.portal.service.CompanyLocalServiceUtil;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Alexander Chow
@@ -49,14 +46,7 @@ public class NonceUtil {
 		String nonce = DigesterUtil.digestHex(
 			Digester.MD5, remoteAddress, String.valueOf(timestamp), companyKey);
 
-		_readWriteLock.writeLock().lock();
-
-		try {
-			_nonceMap.put(timestamp, nonce);
-		}
-		finally {
-			_readWriteLock.writeLock().unlock();
-		}
+		_nonceQueue.put(new NonceDelayed(nonce));
 
 		return nonce;
 	}
@@ -64,57 +54,75 @@ public class NonceUtil {
 	public static boolean verify(String nonce) {
 		_cleanUp();
 
-		_readWriteLock.readLock().lock();
-
-		try {
-			return _nonceMap.containsValue(nonce);
-		}
-		finally {
-			_readWriteLock.readLock().unlock();
-		}
+		return _nonceQueue.contains(new NonceDelayed(nonce));
 	}
 
 	private static void _cleanUp() {
-		long expired = System.currentTimeMillis() - _NONCE_EXPIRATION;
+		while (_nonceQueue.poll() != null);
+	}
 
-		List<Long> times = new ArrayList<Long>();
+	private static class NonceDelayed implements Delayed {
 
-		// Find expired nonces
+		public NonceDelayed(String nonce) {
+			if (nonce == null) {
+				throw new NullPointerException("Null nonce");
+			}
+			_nonce = nonce;
+			_createdMilliseconds = System.currentTimeMillis();
+		}
 
-		_readWriteLock.readLock().lock();
+		public long getDelay(TimeUnit unit) {
+			long leftDelayTime = _NONCE_EXPIRATION + _createdMilliseconds -
+				System.currentTimeMillis();
+			return unit.convert(leftDelayTime, TimeUnit.MILLISECONDS);
+		}
 
-		try {
-			for (long time : _nonceMap.keySet()) {
-				if (time <= expired) {
-					times.add(time);
-				}
-				else {
-					break;
-				}
+		public int compareTo(Delayed o) {
+			if (!(o instanceof NonceDelayed)) {
+				throw new IllegalArgumentException("Wrong Type : "
+					+ o.getClass());
+			}
+			// Do not cast result into int, it may cause int overflow or
+			// underflow
+			long result = _createdMilliseconds -
+				((NonceDelayed) o)._createdMilliseconds;
+			if (result == 0) {
+				return 0;
+			}
+			else if (result > 0) {
+				return 1;
+			}
+			else {
+				return -1;
 			}
 		}
-		finally {
-			_readWriteLock.readLock().unlock();
-		}
 
-		// Purge expired nonces
-
-		_readWriteLock.writeLock().lock();
-
-		try {
-			for (long time : times) {
-				_nonceMap.remove(time);
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
 			}
+
+			if (!(obj instanceof NonceDelayed)) {
+				return false;
+			}
+
+			NonceDelayed nonceDelayed = (NonceDelayed) obj;
+
+			return _nonce.equals(nonceDelayed._nonce);
 		}
-		finally {
-			_readWriteLock.writeLock().unlock();
+
+		public int hashCode() {
+			return _nonce.hashCode();
 		}
+
+		private final long _createdMilliseconds;
+		private final String _nonce;
+
 	}
 
 	private static final long _NONCE_EXPIRATION = 10 * Time.MINUTE;
 
-	private static ReadWriteLock _readWriteLock = new ReentrantReadWriteLock();
-	private static Map<Long, String> _nonceMap =
-		new LinkedHashMap<Long, String>();
+	private static final DelayQueue<NonceDelayed> _nonceQueue =
+		new DelayQueue<NonceDelayed>();
 
 }
