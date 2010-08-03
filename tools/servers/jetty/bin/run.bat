5d9dac5ce6e337ca0b4909d559728a94e82af7f7
Brian Chan <brian.chan@liferay.com>
2010-08-03 22:27:24 +0000

    LPS-11833
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@59538 05bdf26c-840f-0410-9ced-eb539d925f36

--- tools/servers/jetty/bin/run.bat
+++ tools/servers/jetty/bin/run.bat
@@ -4,7 +4,7 @@ if "" == "%JAVA_HOME%" goto errorJavaHome
 
 set "JAVA_OPTS=-Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -DSTART=../etc/start.config -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
 
-%JAVA_HOME%/bin/java %JAVA_OPTS% -jar ../start.jar ../etc/jetty.xml
+"%JAVA_HOME%/bin/java" %JAVA_OPTS% -jar ../start.jar ../etc/jetty.xml
 
 goto end
 
