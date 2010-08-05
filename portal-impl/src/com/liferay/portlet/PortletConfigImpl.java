2a154f23edf7ed3bf6d92c82324e4fe8d0e2b968
Michael Young <michael.young@liferay.com>
2010-08-05 22:00:06 +0000

    LPS-11174
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@59706 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/PortletConfigImpl.java
+++ portal-impl/src/com/liferay/portlet/PortletConfigImpl.java
@@ -24,6 +24,7 @@ import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Utf8PropertyResourceBundle;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.Portlet;
 import com.liferay.portal.model.PortletApp;
@@ -38,7 +39,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -162,7 +162,7 @@ public class PortletConfigImpl implements LiferayPortletConfig {
 					sb.append(portletInfo.getDescription());
 					sb.append(StringPool.NEW_LINE);
 
-					resourceBundle = new PropertyResourceBundle(
+					resourceBundle = new Utf8PropertyResourceBundle(
 						new UnsyncByteArrayInputStream(
 							sb.toString().getBytes()));
 				}
