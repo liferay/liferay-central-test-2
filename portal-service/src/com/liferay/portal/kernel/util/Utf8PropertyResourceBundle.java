5d170ec7523294bb267a15b3c4525bc4f7578f34
Michael Young <michael.young@liferay.com>
2010-08-05 22:00:17 +0000

    LPS-11174
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@59707 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-service/src/com/liferay/portal/kernel/util/Utf8PropertyResourceBundle.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.portal.kernel.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+
+import java.util.PropertyResourceBundle;
+
+/**
+ * @author Michael Young
+ */
+public class Utf8PropertyResourceBundle extends PropertyResourceBundle {
+
+	public Utf8PropertyResourceBundle(InputStream stream) throws IOException {
+		super(stream);
+	}
+
+	public Utf8PropertyResourceBundle(Reader reader) throws IOException {
+		super(reader);
+	}
+
+	public Object handleGetObject(String key) {
+		String value = (String)super.handleGetObject(key);
+
+		try {
+			return new String(value.getBytes("ISO-8859-1"), "UTF-8");
+		}
+		catch (UnsupportedEncodingException e) {
+			return null;
+		}
+	}
+
+}
