5050c0ac7bbcea64b446e29ea6262cd7988de48a
Brian Chan <brian.chan@liferay.com>
2010-08-05 17:24:10 +0000

    LPS-11631 Make UpgradeDuplicates go first
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@59671 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_5_2_3.java
+++ portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_5_2_3.java
@@ -44,12 +44,14 @@ public class UpgradeProcess_5_2_3 extends UpgradeProcess {
 
 	protected void doUpgrade() throws Exception {
 		upgrade(UpgradeSchema.class);
+		upgrade(UpgradeDuplicates.class);
 		upgrade(UpgradeBookmarks.class);
 		upgrade(UpgradeCalendar.class);
 		upgrade(UpgradeDocumentLibrary.class);
 		upgrade(UpgradeGroup.class);
 		upgrade(UpgradeImageGallery.class);
 		upgrade(UpgradeLayout.class);
+		upgrade(UpgradeMessageBoards.class);
 		upgrade(UpgradeResource.class);
 		upgrade(UpgradeResourceCode.class);
 		upgrade(UpgradeRole.class);
@@ -57,8 +59,6 @@ public class UpgradeProcess_5_2_3 extends UpgradeProcess {
 		upgrade(UpgradeTags.class);
 		upgrade(UpgradeUser.class);
 		upgrade(UpgradeWiki.class);
-		upgrade(UpgradeDuplicates.class);
-		upgrade(UpgradeMessageBoards.class);
 		upgrade(DropIndexes.class);
 	}
 
