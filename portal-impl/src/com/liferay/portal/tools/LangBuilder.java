fe05f88d6cfbff1365527d0875673bf3152718fa
Brian Chan <brian.chan@liferay.com>
2010-08-10 18:51:17 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@59978 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/tools/LangBuilder.java
+++ portal-impl/src/com/liferay/portal/tools/LangBuilder.java
@@ -31,6 +31,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+
 import java.util.Properties;
 import java.util.Set;
 import java.util.TreeSet;
