c8cebd20c9b42408ac415abe5902604e10d1c57a
Gavin Wan <gavin.wan@liferay.com>
2010-09-07 09:25:12 +0000

    LPS-9286
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@62018 05bdf26c-840f-0410-9ced-eb539d925f36

--- util-bridges/src/com/liferay/util/bridges/php/PHPPortlet.java
+++ util-bridges/src/com/liferay/util/bridges/php/PHPPortlet.java
@@ -20,6 +20,7 @@ import com.liferay.portal.kernel.servlet.PortletServletObjectsFactory;
 import com.liferay.portal.kernel.servlet.ServletObjectsFactory;
 import com.liferay.portal.kernel.servlet.StringServletResponse;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.util.bridges.common.ScriptPostProcess;
 import com.liferay.util.servlet.DynamicServletConfig;
 
 import java.io.IOException;
@@ -42,8 +43,6 @@ import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.portals.bridges.common.ScriptPostProcess;
 
 /**
  * @author Jorge Ferrer
