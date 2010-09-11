139bc257a0f25c522a295677da0273a2022e4d0e
Minhchau Dang <minhchau.dang@liferay.com>
2010-09-11 00:48:18 +0000

    LPS-11292 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@62257 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/servlet/filters/redirect/AbsoluteRedirectsFilter.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/redirect/AbsoluteRedirectsFilter.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.portal.servlet.filters.redirect;
 
 import com.liferay.portal.kernel.log.Log;
@@ -69,9 +83,11 @@ public class AbsoluteRedirectsFilter extends BasePortalFilter {
 			}
 		}
 
-		processFilter(AbsoluteRedirectsFilter.class, request, response, filterChain);
+		processFilter(
+			AbsoluteRedirectsFilter.class, request, response, filterChain);
 	}
 
-	private static Log _log = LogFactoryUtil.getLog(AbsoluteRedirectsFilter.class);
+	private static Log _log = LogFactoryUtil.getLog(
+		AbsoluteRedirectsFilter.class);
 
 }
