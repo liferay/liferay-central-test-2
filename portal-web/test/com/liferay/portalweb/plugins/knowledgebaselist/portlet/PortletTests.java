52db7914c685245c8e8a6aa5673de10249aad0ec
Brian Chan <brian.chan@liferay.com>
2010-09-15 16:44:45 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@62414 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/test/com/liferay/portalweb/plugins/knowledgebaselist/portlet/PortletTests.java
+++ portal-web/test/com/liferay/portalweb/plugins/knowledgebaselist/portlet/PortletTests.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.portalweb.plugins.knowledgebaselist.portlet;
 
 import com.liferay.portalweb.plugins.knowledgebaselist.portlet.addportletkbl.AddPortletKBLTests;
