32625430e1a103f4d5eb431c741c30885b8f73fa
Michael Hashimoto <michael.hashimoto@liferay.com>
2010-10-07 01:52:43 +0000

    LPS-13040
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@63717 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-web/test/com/liferay/portalweb/portal/dbupgrade/sampledata605/documentlibrary/documentversion/AddDocumentVersionTest.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portalweb.portal.dbupgrade.sampledata605.documentlibrary.documentversion;
+
+import com.liferay.portalweb.portal.BaseTestCase;
+import com.liferay.portalweb.portal.util.RuntimeVariables;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class AddDocumentVersionTest extends BaseTestCase {
+	public void testAddDocumentVersion() throws Exception {
+		selenium.open("/web/guest/home/");
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isElementPresent("link=Control Panel")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.clickAt("link=Control Panel", RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		selenium.clickAt("link=Communities", RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		selenium.type("_134_name",
+			RuntimeVariables.replace(
+				"Document Library Document Version Community"));
+		selenium.clickAt("//input[@value='Search']",
+			RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		selenium.clickAt("//td[2]/a", RuntimeVariables.replace("Open"));
+		selenium.waitForPageToLoad("30000");
+		selenium.clickAt("link=Document Library Page",
+			RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		selenium.clickAt("//a/strong", RuntimeVariables.replace("Test1 Folder1"));
+		selenium.waitForPageToLoad("30000");
+		selenium.clickAt("//td[5]/ul/li/strong/a",
+			RuntimeVariables.replace("Actions"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isVisible(
+							"//div[@class='lfr-component lfr-menu-list']/ul/li[2]/a")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		assertEquals(RuntimeVariables.replace("Edit"),
+			selenium.getText(
+				"//div[@class='lfr-component lfr-menu-list']/ul/li[2]/a"));
+		selenium.click(RuntimeVariables.replace(
+				"//div[@class='lfr-component lfr-menu-list']/ul/li[2]/a"));
+		selenium.waitForPageToLoad("30000");
+		selenium.type("_20_file",
+			RuntimeVariables.replace(
+				"L:\\projects\\6.0.x\\build\\portal-web\\test\\com\\liferay\\portalweb\\portal\\dbupgrade\\sampledata605\\documentlibrary\\documentversion\\dependencies\\test_document2.txt"));
+		selenium.type("_20_title", RuntimeVariables.replace("Test2 Document2"));
+		selenium.type("_20_description",
+			RuntimeVariables.replace("This is test2 document2."));
+		selenium.clickAt("//input[@value='Publish']",
+			RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		assertEquals(RuntimeVariables.replace("Test2 Document2"),
+			selenium.getText("//a/span/span"));
+		assertEquals(RuntimeVariables.replace("This is test2 document2."),
+			selenium.getText("//a/div"));
+		selenium.clickAt("//a/span/span",
+			RuntimeVariables.replace("Test2 Document2"));
+		selenium.waitForPageToLoad("30000");
+		assertEquals(RuntimeVariables.replace("1.1"),
+			selenium.getText("//span[1]/strong"));
+		assertEquals(RuntimeVariables.replace("1.1"),
+			selenium.getText("//tr[3]/td[1]/a"));
+		assertEquals(RuntimeVariables.replace("1.0"),
+			selenium.getText("//tr[4]/td[1]/a"));
+	}
+}
---
+++ portal-web/test/com/liferay/portalweb/portal/dbupgrade/viewsampledatalatest/ViewSampleData605LatestTests.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-2010 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portalweb.portal.dbupgrade.viewsampledatalatest;
+
+import com.liferay.portalweb.portal.BaseTests;
+import com.liferay.portalweb.portal.dbupgrade.viewsampledatalatest.address.Address605LatestTests;
+import com.liferay.portalweb.portal.dbupgrade.viewsampledatalatest.announcements.Announcements605LatestTests;
+import com.liferay.portalweb.portal.dbupgrade.viewsampledatalatest.announcementsdelivery.AnnouncementsDelivery605LatestTests;
+import com.liferay.portalweb.portal.dbupgrade.viewsampledatalatest.calendar.Calendar605LatestTests;
+import com.liferay.portalweb.portal.dbupgrade.viewsampledatalatest.documentlibrary.DocumentLibrary605LatestTests;
+import com.liferay.portalweb.portal.dbupgrade.viewsampledatalatest.expando.Expando605LatestTests;
+import com.liferay.portalweb.portal.dbupgrade.viewsampledatalatest.groups.Groups605LatestTests;
+import com.liferay.portalweb.portal.dbupgrade.viewsampledatalatest.login.LoginTests;
+import com.liferay.portalweb.portal.dbupgrade.viewsampledatalatest.organizations.Organizations605LatestTests;
+import com.liferay.portalweb.portal.dbupgrade.viewsampledatalatest.portletpermissions.PortletPermissions605LatestTests;
+import com.liferay.portalweb.portal.dbupgrade.viewsampledatalatest.shopping.Shopping605LatestTests;
+import com.liferay.portalweb.portal.dbupgrade.viewsampledatalatest.social.Social605LatestTests;
+import com.liferay.portalweb.portal.dbupgrade.viewsampledatalatest.stagingcommunity.StagingCommunity605LatestTests;
+import com.liferay.portalweb.portal.dbupgrade.viewsampledatalatest.stagingorganization.StagingOrganization605LatestTests;
+import com.liferay.portalweb.portal.dbupgrade.viewsampledatalatest.tags.Tags605LatestTests;
+import com.liferay.portalweb.portal.dbupgrade.viewsampledatalatest.webcontent.WebContent605LatestTests;
+import com.liferay.portalweb.portal.dbupgrade.viewsampledatalatest.wiki.Wiki605LatestTests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class ViewSampleData605LatestTests extends BaseTests {
+
+	public static Test suite() {
+		TestSuite testSuite = new TestSuite();
+
+		testSuite.addTest(LoginTests.suite());
+		testSuite.addTest(Address605LatestTests.suite());
+		testSuite.addTest(Announcements605LatestTests.suite());
+		testSuite.addTest(AnnouncementsDelivery605LatestTests.suite());
+		testSuite.addTest(Calendar605LatestTests.suite());
+		testSuite.addTest(DocumentLibrary605LatestTests.suite());
+		testSuite.addTest(Expando605LatestTests.suite());
+		testSuite.addTest(Groups605LatestTests.suite());
+		testSuite.addTest(Organizations605LatestTests.suite());
+		testSuite.addTest(PortletPermissions605LatestTests.suite());
+		testSuite.addTest(Shopping605LatestTests.suite());
+		testSuite.addTest(Social605LatestTests.suite());
+		testSuite.addTest(StagingCommunity605LatestTests.suite());
+		testSuite.addTest(StagingOrganization605LatestTests.suite());
+		testSuite.addTest(Tags605LatestTests.suite());
+		testSuite.addTest(WebContent605LatestTests.suite());
+		testSuite.addTest(Wiki605LatestTests.suite());
+
+		return testSuite;
+	}
+
+}
