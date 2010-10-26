80c09c3fe8745ec76c3fcd03e8b9b499c4cbc4fe
Brian Chan <brian.chan@liferay.com>
2010-10-26 12:56:49 +0000

    LPS-13411
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@64858 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/tools/ArgumentUtil.java
+++ portal-impl/src/com/liferay/portal/tools/ArgumentUtil.java
@@ -25,17 +25,19 @@ public class ArgumentUtil {
 	public static Map<String, String> getArguments(String[] args) {
 		Map<String, String> arguments = new HashMap<String, String>();
 
-		for(String arg : args) {
-			int index = arg.indexOf('=');
-			if (index <= 0) {
-				throw new IllegalArgumentException("Bad argument : " + arg);
+		for (String arg : args) {
+			int pos = arg.indexOf('=');
+
+			if (pos <= 0) {
+				throw new IllegalArgumentException("Bad argument " + arg);
 			}
 
-			String key = arg.substring(0, index).trim();
-			String value = arg.substring(index + 1).trim();
+			String key = arg.substring(0, pos).trim();
+			String value = arg.substring(pos + 1).trim();
 
 			if (key.startsWith("-D")) {
 				key = key.substring(2);
+
 				System.setProperty(key, value);
 			}
 			else {
