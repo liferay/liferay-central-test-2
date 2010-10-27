db319fa7dec0c2f46a644a1cdab6cdfd6dc44d9a
Brian Chan <brian.chan@liferay.com>
2010-10-27 05:31:30 +0000

    LPS-13413
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@64921 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-service/src/com/liferay/portal/kernel/io/DummyOutputStream.java
+++ portal-service/src/com/liferay/portal/kernel/io/DummyOutputStream.java
@@ -17,8 +17,6 @@ package com.liferay.portal.kernel.io;
 import java.io.OutputStream;
 
 /**
- * <a href="DummyOutputStream.java.html"><b><i>View Source</i></b></a>
- *
  * @author Brian Wing Shun Chan
  */
 public class DummyOutputStream extends OutputStream {
