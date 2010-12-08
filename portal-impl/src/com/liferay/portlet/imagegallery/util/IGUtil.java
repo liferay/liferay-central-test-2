969e7974be666b941f27ce419e74c39c9a53eb02
Brian Chan <brian.chan@liferay.com>
2010-12-08 18:39:03 +0000

    LPS-14198 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@67871 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/imagegallery/util/IGUtil.java
+++ portal-impl/src/com/liferay/portlet/imagegallery/util/IGUtil.java
@@ -87,7 +87,6 @@ public class IGUtil {
 			portletURL.setWindowState(LiferayWindowState.POP_UP);
 
 			portletURL.setParameter("struts_action", strutsAction);
-
 			portletURL.setParameter(
 				"groupId", String.valueOf(folder.getGroupId()));
 
@@ -104,22 +103,19 @@ public class IGUtil {
 		Collections.reverse(ancestorFolders);
 
 		for (IGFolder ancestorFolder : ancestorFolders) {
-
-			portletURL.setParameter(
-				"folderId", String.valueOf(ancestorFolder.getFolderId()));
-
 			portletURL.setParameter(
 				"groupId", String.valueOf(folder.getGroupId()));
+			portletURL.setParameter(
+				"folderId", String.valueOf(ancestorFolder.getFolderId()));
 
 			PortalUtil.addPortletBreadcrumbEntry(
 				request, ancestorFolder.getName(), portletURL.toString());
 		}
 
 		portletURL.setParameter(
-			"folderId", String.valueOf(folder.getFolderId()));
-
-		portletURL.setParameter(
 			"groupId", String.valueOf(folder.getGroupId()));
+		portletURL.setParameter(
+			"folderId", String.valueOf(folder.getFolderId()));
 
 		PortalUtil.addPortletBreadcrumbEntry(
 			request, folder.getName(), portletURL.toString());
