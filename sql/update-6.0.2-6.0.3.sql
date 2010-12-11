ba9c62256288f0d42cda936634ae857a51b60ebc
Brian Chan <brian.chan@liferay.com>
2010-12-11 00:18:13 +0000

    LPS-14274
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@68119 05bdf26c-840f-0410-9ced-eb539d925f36

--- sql/update-6.0.2-6.0.3.sql
+++ sql/update-6.0.2-6.0.3.sql
@@ -15,6 +15,8 @@ alter table Layout add uuid_ VARCHAR(75) null;
 
 alter table MBMessage add rootMessageId LONG;
 
+COMMIT_TRANSACTION;
+
 update MBMessage set rootMessageId = (select rootMessageId from MBThread where MBThread.threadId = MBMessage.threadId);
 
 alter table PasswordPolicy add minSymbols INTEGER;
