52ea7250d16ebb8dcc01936652c38d40cdcb1132
Julio Camarero <julio.camarero@liferay.com>
2010-12-21 16:53:37 +0000

    LPS-10398 Simplify configuration for Navigation Portlet
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@68687 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/navigation/action/ConfigurationActionImpl.java
+++ portal-impl/src/com/liferay/portlet/navigation/action/ConfigurationActionImpl.java
@@ -15,15 +15,8 @@
 package com.liferay.portlet.navigation.action;
 
 import com.liferay.portal.kernel.portlet.BaseConfigurationAction;
-import com.liferay.portal.kernel.servlet.SessionMessages;
-import com.liferay.portal.kernel.util.Constants;
-import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portlet.PortletPreferencesFactoryUtil;
 
-import javax.portlet.ActionRequest;
-import javax.portlet.ActionResponse;
 import javax.portlet.PortletConfig;
-import javax.portlet.PortletPreferences;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
@@ -31,56 +24,6 @@ import javax.portlet.RenderResponse;
  * @author Brian Wing Shun Chan
  */
 public class ConfigurationActionImpl extends BaseConfigurationAction {
-
-	public void processAction(
-			PortletConfig portletConfig, ActionRequest actionRequest,
-			ActionResponse actionResponse)
-		throws Exception {
-
-		String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
-
-		if (!cmd.equals(Constants.UPDATE)) {
-			return;
-		}
-
-		String displayStyle = ParamUtil.getString(
-			actionRequest, "displayStyle");
-		String bulletStyle = ParamUtil.getString(actionRequest, "bulletStyle");
-		String headerType = ParamUtil.getString(actionRequest, "headerType");
-		String rootLayoutType = ParamUtil.getString(
-			actionRequest, "rootLayoutType");
-		int rootLayoutLevel = ParamUtil.getInteger(
-			actionRequest, "rootLayoutLevel");
-		String includedLayouts = ParamUtil.getString(
-			actionRequest, "includedLayouts");
-		boolean nestedChildren = ParamUtil.getBoolean(
-			actionRequest, "nestedChildren");
-
-		String portletResource = ParamUtil.getString(
-			actionRequest, "portletResource");
-
-		PortletPreferences preferences =
-			PortletPreferencesFactoryUtil.getPortletSetup(
-				actionRequest, portletResource);
-
-		preferences.setValue("display-style", displayStyle);
-		preferences.setValue("bullet-style", bulletStyle);
-
-		if (displayStyle.equals("[custom]")) {
-			preferences.setValue("header-type", headerType);
-			preferences.setValue("root-layout-type", rootLayoutType);
-			preferences.setValue(
-				"root-layout-level", String.valueOf(rootLayoutLevel));
-			preferences.setValue("included-layouts", includedLayouts);
-			preferences.setValue(
-				"nested-children", String.valueOf(nestedChildren));
-		}
-
-		preferences.store();
-
-		SessionMessages.add(
-			actionRequest, portletConfig.getPortletName() + ".doConfigure");
-	}
 
 	public String render(
 			PortletConfig portletConfig, RenderRequest renderRequest,
