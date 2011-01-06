5d7f7716860d204492e1c36bc9b0843a1e69a2ba
Michael Hashimoto <michael.hashimoto@liferay.com>
2011-01-06 23:15:32 +0000

    LPS-6878
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@69793 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-web/test/com/liferay/portalweb/properties/usersscreennameallownumeric/users/user/erroradduserscreennamenumbercommunityid/ErrorAddUserScreenNameNumberCommunityIdTest.java
@@ -0,0 +1,127 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portalweb.properties.usersscreennameallownumeric.users.user.erroradduserscreennamenumbercommunityid;
+
+import com.liferay.portalweb.portal.BaseTestCase;
+import com.liferay.portalweb.portal.util.RuntimeVariables;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class ErrorAddUserScreenNameNumberCommunityIdTest extends BaseTestCase {
+	public void testErrorAddUserScreenNameNumberCommunityId()
+		throws Exception {
+		selenium.open("/web/guest/home/");
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isElementPresent("link=Control Panel")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.saveScreenShotAndSource();
+		selenium.clickAt("link=Control Panel", RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		selenium.saveScreenShotAndSource();
+		selenium.clickAt("link=Communities", RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		selenium.saveScreenShotAndSource();
+		selenium.clickAt("//tr[4]/td[6]/span/ul/li/strong/a",
+			RuntimeVariables.replace("Actions"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isVisible(
+							"//div[@class='lfr-component lfr-menu-list']/ul/li[1]/a")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.saveScreenShotAndSource();
+		assertEquals(RuntimeVariables.replace("Edit"),
+			selenium.getText(
+				"//div[@class='lfr-component lfr-menu-list']/ul/li[1]/a"));
+		selenium.click(RuntimeVariables.replace(
+				"//div[@class='lfr-component lfr-menu-list']/ul/li[1]/a"));
+		selenium.waitForPageToLoad("30000");
+		selenium.saveScreenShotAndSource();
+
+		String CommunityID = selenium.getFirstNumber(
+				"//fieldset/div/div[1]/div");
+		RuntimeVariables.setValue("CommunityID", CommunityID);
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isElementPresent("link=Users")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.saveScreenShotAndSource();
+		selenium.clickAt("link=Users", RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		selenium.saveScreenShotAndSource();
+		selenium.clickAt("link=Add", RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		selenium.saveScreenShotAndSource();
+		selenium.type("_125_screenName",
+			RuntimeVariables.replace(RuntimeVariables.getValue("CommunityID")));
+		selenium.saveScreenShotAndSource();
+		selenium.type("_125_emailAddress",
+			RuntimeVariables.replace("testA@selenium.com"));
+		selenium.saveScreenShotAndSource();
+		selenium.type("_125_firstName", RuntimeVariables.replace("testA"));
+		selenium.saveScreenShotAndSource();
+		selenium.type("_125_lastName", RuntimeVariables.replace("testA"));
+		selenium.saveScreenShotAndSource();
+		selenium.clickAt("//input[@value='Save']", RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		selenium.saveScreenShotAndSource();
+		assertEquals(RuntimeVariables.replace(
+				"Your request failed to complete."),
+			selenium.getText("//div[@class='portlet-msg-error']"));
+		assertEquals(RuntimeVariables.replace(
+				"Please enter a valid screen name."),
+			selenium.getText("xPath=(//div[@class='portlet-msg-error'])[2]"));
+	}
+}
---
+++ portal-web/test/com/liferay/portalweb/properties/usersscreennameallownumeric/users/user/erroradduserscreennamenumberorganizationid/ErrorAddUserScreenNameNumberOrganizationIdTest.java
@@ -0,0 +1,153 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portalweb.properties.usersscreennameallownumeric.users.user.erroradduserscreennamenumberorganizationid;
+
+import com.liferay.portalweb.portal.BaseTestCase;
+import com.liferay.portalweb.portal.util.RuntimeVariables;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class ErrorAddUserScreenNameNumberOrganizationIdTest extends BaseTestCase {
+	public void testErrorAddUserScreenNameNumberOrganizationId()
+		throws Exception {
+		selenium.open("/web/guest/home/");
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isElementPresent("link=Control Panel")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.saveScreenShotAndSource();
+		selenium.clickAt("link=Control Panel", RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		selenium.saveScreenShotAndSource();
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isElementPresent("link=Organizations")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.saveScreenShotAndSource();
+		selenium.clickAt("link=Organizations", RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		selenium.saveScreenShotAndSource();
+		selenium.type("_126_keywords",
+			RuntimeVariables.replace("Test Organization"));
+		selenium.saveScreenShotAndSource();
+		selenium.clickAt("//input[@value='Search']",
+			RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		selenium.saveScreenShotAndSource();
+		assertEquals(RuntimeVariables.replace("Test Organization"),
+			selenium.getText("//td[2]//strong"));
+		selenium.clickAt("//strong/a", RuntimeVariables.replace("Actions"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isVisible(
+							"//div[@class='lfr-component lfr-menu-list']/ul/li[1]/a")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.saveScreenShotAndSource();
+		assertEquals(RuntimeVariables.replace("Edit"),
+			selenium.getText(
+				"//div[@class='lfr-component lfr-menu-list']/ul/li[1]/a"));
+		selenium.click(RuntimeVariables.replace(
+				"//div[@class='lfr-component lfr-menu-list']/ul/li[1]/a"));
+		selenium.waitForPageToLoad("30000");
+		selenium.saveScreenShotAndSource();
+
+		String OrganizationID = selenium.getFirstNumber(
+				"//fieldset[1]/div/div[3]/div");
+		RuntimeVariables.setValue("OrganizationID", OrganizationID);
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isElementPresent("link=Users")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.saveScreenShotAndSource();
+		selenium.clickAt("link=Users", RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		selenium.saveScreenShotAndSource();
+		selenium.clickAt("link=Add", RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		selenium.saveScreenShotAndSource();
+		selenium.type("_125_screenName",
+			RuntimeVariables.replace(RuntimeVariables.getValue("OrganizationID")));
+		selenium.saveScreenShotAndSource();
+		selenium.type("_125_emailAddress",
+			RuntimeVariables.replace("testA@selenium.com"));
+		selenium.saveScreenShotAndSource();
+		selenium.type("_125_firstName", RuntimeVariables.replace("testA"));
+		selenium.saveScreenShotAndSource();
+		selenium.type("_125_lastName", RuntimeVariables.replace("testA"));
+		selenium.saveScreenShotAndSource();
+		selenium.clickAt("//input[@value='Save']", RuntimeVariables.replace(""));
+		selenium.waitForPageToLoad("30000");
+		selenium.saveScreenShotAndSource();
+		assertEquals(RuntimeVariables.replace(
+				"Your request failed to complete."),
+			selenium.getText("//div[@class='portlet-msg-error']"));
+		assertEquals(RuntimeVariables.replace(
+				"Please enter a valid screen name."),
+			selenium.getText("xPath=(//div[@class='portlet-msg-error'])[2]"));
+	}
+}
