0651f110111ecfc6a0ed9357cf18401c96d38d12
Brian Chan <brian.chan@liferay.com>
2011-01-12 00:48:16 +0000

    Source formatting - sorting, no changes
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@70149 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-service/src/com/liferay/portal/kernel/dao/search/ResultRow.java
+++ portal-service/src/com/liferay/portal/kernel/dao/search/ResultRow.java
@@ -50,250 +50,10 @@ public class ResultRow {
 		_entries = new ArrayList<SearchEntry>();
 	}
 
-	public Object getObject() {
-		return _obj;
-	}
-
-	public void setObject(Object obj) {
-		_obj = obj;
-	}
-
-	public String getPrimaryKey() {
-		return _primaryKey;
-	}
-
-	public void setPrimaryKey(String primaryKey) {
-		_primaryKey = primaryKey;
-	}
-
-	public int getPos() {
-		return _pos;
-	}
-
-	public boolean isBold() {
-		return _bold;
-	}
-
-	public void setBold(boolean bold) {
-		_bold = bold;
-	}
-
-	public boolean isRestricted() {
-		return _restricted;
-	}
-
-	public void setRestricted(boolean restricted) {
-		_restricted = restricted;
-	}
-
-	public String getClassName() {
-		return _className;
-	}
-
-	public void setClassName(String className) {
-		_className = className;
-	}
-
-	public String getClassHoverName() {
-		return _classHoverName;
-	}
-
-	public void setClassHoverName(String classHoverName) {
-		_classHoverName = classHoverName;
-	}
-
-	public List<SearchEntry> getEntries() {
-		return _entries;
-	}
-
-	public Object getParameter(String param) {
-		if (_params == null) {
-			_params = new HashMap<String, Object>();
-		}
-
-		return _params.get(param);
-	}
-
-	public void setParameter(String param, Object value) {
-		if (_params == null) {
-			_params = new HashMap<String, Object>();
-		}
-
-		_params.put(param, value);
-	}
-
-	// Text with name
-
-	public void addText(String name) {
-		addText(_entries.size(), name);
-	}
-
-	public void addText(int index, String name) {
-		addText(
-			index, SearchEntry.DEFAULT_ALIGN, SearchEntry.DEFAULT_VALIGN,
-			SearchEntry.DEFAULT_COLSPAN, name);
-	}
-
-	public void addText(String align, String valign, String name) {
-		addText(
-			_entries.size(), align, valign, SearchEntry.DEFAULT_COLSPAN, name);
-	}
-
-	public void addText(
-		String align, String valign, int colspan, String name) {
-
-		addText(_entries.size(), align, valign, colspan, name);
-	}
-
-	public void addText(
-		int index, String align, String valign, int colspan, String name) {
-
-		TextSearchEntry textSearchEntry = new TextSearchEntry();
-
-		textSearchEntry.setAlign(align);
-		textSearchEntry.setColspan(colspan);
-		textSearchEntry.setName(name);
-		textSearchEntry.setValign(valign);
-
-		_entries.add(index, textSearchEntry);
-	}
-
-	// Text with name and href
-
-	public void addText(String name, String href) {
-		addText(_entries.size(), name, href);
-	}
-
-	public void addText(int index, String name, String href) {
-		addText(
-			index, SearchEntry.DEFAULT_ALIGN, SearchEntry.DEFAULT_VALIGN,
-			SearchEntry.DEFAULT_COLSPAN, name, href);
-	}
-
-	public void addText(String align, String valign, String name, String href) {
-		addText(
-			_entries.size(), align, valign, SearchEntry.DEFAULT_COLSPAN, name,
-			href);
-	}
-
-	public void addText(
-		String align, String valign, int colspan, String name, String href) {
-
-		addText(_entries.size(), align, valign, colspan, name, href);
-	}
-
-	public void addText(
-		int index, String align, String valign, int colspan, String name,
-		String href) {
-
-		if (_restricted) {
-			href = null;
-		}
-
-		TextSearchEntry textSearchEntry = new TextSearchEntry();
-
-		textSearchEntry.setAlign(align);
-		textSearchEntry.setColspan(colspan);
-		textSearchEntry.setHref(href);
-		textSearchEntry.setName(name);
-		textSearchEntry.setValign(valign);
-
-		_entries.add(index, textSearchEntry);
-	}
-
-	// Text with name and portlet URL
-
-	public void addText(String name, PortletURL portletURL) {
-		if (portletURL == null) {
-			addText(name);
-		}
-		else {
-			addText(name, portletURL.toString());
-		}
-	}
-
-	public void addText(int index, String name, PortletURL portletURL) {
-		if (portletURL == null) {
-			addText(index, name);
-		}
-		else {
-			addText(index, name, portletURL.toString());
-		}
-	}
-
-	public void addText(
-		String align, String valign, String name, PortletURL portletURL) {
-
-		addText(
-			align, valign, SearchEntry.DEFAULT_COLSPAN, name, portletURL);
-	}
-
-	public void addText(
-		String align, String valign, int colspan, String name,
-		PortletURL portletURL) {
-
-		if (portletURL == null) {
-			addText(align, valign, colspan, name);
-		}
-		else {
-			addText(align, valign, colspan, name, portletURL.toString());
-		}
-	}
-
-	public void addText(
-		int index, String align, String valign, int colspan, String name,
-		PortletURL portletURL) {
-
-		if (portletURL == null) {
-			addText(index, align, valign, colspan, name);
-		}
-		else {
-			addText(index, align, valign, colspan, name, portletURL.toString());
-		}
-	}
-
-	// Text with search entry
-
-	public void addText(TextSearchEntry searchEntry) {
-		if (_restricted) {
-			searchEntry.setHref(null);
-		}
-
-		_entries.add(_entries.size(), searchEntry);
-	}
-
-	public void addText(int index, TextSearchEntry searchEntry) {
-		if (_restricted) {
-			searchEntry.setHref(null);
-		}
-
-		_entries.add(index, searchEntry);
-	}
-
-	// Button with name and href
-
-	public void addButton(String name, String href) {
-		addButton(_entries.size(), name, href);
-	}
-
 	public void addButton(int index, String name, String href) {
 		addButton(
 			index, SearchEntry.DEFAULT_ALIGN, SearchEntry.DEFAULT_VALIGN,
 			SearchEntry.DEFAULT_COLSPAN, name, href);
-	}
-
-	public void addButton(
-		String align, String valign, String name, String href) {
-
-		addButton(
-			_entries.size(), align, valign, SearchEntry.DEFAULT_COLSPAN, name,
-			href);
-	}
-
-	public void addButton(
-		String align, String valign, int colspan, String name, String href) {
-
-		addButton(_entries.size(), align, valign, colspan, name, href);
 	}
 
 	public void addButton(
@@ -315,25 +75,38 @@ public class ResultRow {
 		_entries.add(index, buttonSearchEntry);
 	}
 
-	// JSP
-
-	public void addJSP(String path) {
-		addJSP(_entries.size(), path);
+	public void addButton(String name, String href) {
+		addButton(_entries.size(), name, href);
 	}
 
-	public void addJSP(String align, String valign, String path) {
-		addJSP(
-			_entries.size(), align, valign, SearchEntry.DEFAULT_COLSPAN, path);
+	public void addButton(
+		String align, String valign, int colspan, String name, String href) {
+
+		addButton(_entries.size(), align, valign, colspan, name, href);
 	}
 
-	public void addJSP(String align, String valign, int colspan, String path) {
-		addJSP(_entries.size(), align, valign, colspan, path);
+	public void addButton(
+		String align, String valign, String name, String href) {
+
+		addButton(
+			_entries.size(), align, valign, SearchEntry.DEFAULT_COLSPAN, name,
+			href);
 	}
 
 	public void addJSP(int index, String path) {
 		addJSP(
 			index, SearchEntry.DEFAULT_ALIGN, SearchEntry.DEFAULT_VALIGN,
 			SearchEntry.DEFAULT_COLSPAN, path);
+	}
+
+	public void addJSP(
+		int index, String path, ServletContext servletContext,
+		HttpServletRequest request, HttpServletResponse response) {
+
+		addJSP(
+			index, SearchEntry.DEFAULT_ALIGN, SearchEntry.DEFAULT_VALIGN,
+			SearchEntry.DEFAULT_COLSPAN, path, servletContext, request,
+			response);
 	}
 
 	public void addJSP(
@@ -347,44 +120,6 @@ public class ResultRow {
 		jspSearchEntry.setValign(valign);
 
 		_entries.add(index, jspSearchEntry);
-	}
-
-	// JSP with portlet context
-
-	public void addJSP(
-		String path, ServletContext servletContext, HttpServletRequest request,
-		HttpServletResponse response) {
-
-		addJSP(_entries.size(), path, servletContext, request, response);
-	}
-
-	public void addJSP(
-		String align, String valign, String path, ServletContext servletContext,
-		HttpServletRequest request, HttpServletResponse response) {
-
-		addJSP(
-			_entries.size(), align, valign, SearchEntry.DEFAULT_COLSPAN, path,
-			servletContext, request, response);
-	}
-
-	public void addJSP(
-		String align, String valign, int colspan, String path,
-		ServletContext servletContext, HttpServletRequest request,
-		HttpServletResponse response) {
-
-		addJSP(
-			_entries.size(), align, valign, colspan, path, servletContext,
-			request, response);
-	}
-
-	public void addJSP(
-		int index, String path, ServletContext servletContext,
-		HttpServletRequest request, HttpServletResponse response) {
-
-		addJSP(
-			index, SearchEntry.DEFAULT_ALIGN, SearchEntry.DEFAULT_VALIGN,
-			SearchEntry.DEFAULT_COLSPAN, path, servletContext, request,
-			response);
 	}
 
 	public void addJSP(
@@ -405,7 +140,44 @@ public class ResultRow {
 		_entries.add(index, jspSearchEntry);
 	}
 
-	// Score
+	public void addJSP(String path) {
+		addJSP(_entries.size(), path);
+	}
+
+	public void addJSP(
+		String path, ServletContext servletContext, HttpServletRequest request,
+		HttpServletResponse response) {
+
+		addJSP(_entries.size(), path, servletContext, request, response);
+	}
+
+	public void addJSP(String align, String valign, int colspan, String path) {
+		addJSP(_entries.size(), align, valign, colspan, path);
+	}
+
+	public void addJSP(
+		String align, String valign, int colspan, String path,
+		ServletContext servletContext, HttpServletRequest request,
+		HttpServletResponse response) {
+
+		addJSP(
+			_entries.size(), align, valign, colspan, path, servletContext,
+			request, response);
+	}
+
+	public void addJSP(String align, String valign, String path) {
+		addJSP(
+			_entries.size(), align, valign, SearchEntry.DEFAULT_COLSPAN, path);
+	}
+
+	public void addJSP(
+		String align, String valign, String path, ServletContext servletContext,
+		HttpServletRequest request, HttpServletResponse response) {
+
+		addJSP(
+			_entries.size(), align, valign, SearchEntry.DEFAULT_COLSPAN, path,
+			servletContext, request, response);
+	}
 
 	public void addScore(float score) {
 		addScore(_entries.size(), score);
@@ -419,14 +191,226 @@ public class ResultRow {
 		_entries.add(index, scoreSearchEntry);
 	}
 
-	private Object _obj;
-	private String _primaryKey;
-	private int _pos;
+	public void addText(int index, String name) {
+		addText(
+			index, SearchEntry.DEFAULT_ALIGN, SearchEntry.DEFAULT_VALIGN,
+			SearchEntry.DEFAULT_COLSPAN, name);
+	}
+
+	public void addText(int index, String name, PortletURL portletURL) {
+		if (portletURL == null) {
+			addText(index, name);
+		}
+		else {
+			addText(index, name, portletURL.toString());
+		}
+	}
+
+	public void addText(int index, String name, String href) {
+		addText(
+			index, SearchEntry.DEFAULT_ALIGN, SearchEntry.DEFAULT_VALIGN,
+			SearchEntry.DEFAULT_COLSPAN, name, href);
+	}
+
+	public void addText(
+		int index, String align, String valign, int colspan, String name) {
+
+		TextSearchEntry textSearchEntry = new TextSearchEntry();
+
+		textSearchEntry.setAlign(align);
+		textSearchEntry.setColspan(colspan);
+		textSearchEntry.setName(name);
+		textSearchEntry.setValign(valign);
+
+		_entries.add(index, textSearchEntry);
+	}
+
+	public void addText(
+		int index, String align, String valign, int colspan, String name,
+		PortletURL portletURL) {
+
+		if (portletURL == null) {
+			addText(index, align, valign, colspan, name);
+		}
+		else {
+			addText(index, align, valign, colspan, name, portletURL.toString());
+		}
+	}
+
+	public void addText(
+		int index, String align, String valign, int colspan, String name,
+		String href) {
+
+		if (_restricted) {
+			href = null;
+		}
+
+		TextSearchEntry textSearchEntry = new TextSearchEntry();
+
+		textSearchEntry.setAlign(align);
+		textSearchEntry.setColspan(colspan);
+		textSearchEntry.setHref(href);
+		textSearchEntry.setName(name);
+		textSearchEntry.setValign(valign);
+
+		_entries.add(index, textSearchEntry);
+	}
+
+	public void addText(int index, TextSearchEntry searchEntry) {
+		if (_restricted) {
+			searchEntry.setHref(null);
+		}
+
+		_entries.add(index, searchEntry);
+	}
+
+	public void addText(String name) {
+		addText(_entries.size(), name);
+	}
+
+	public void addText(String name, PortletURL portletURL) {
+		if (portletURL == null) {
+			addText(name);
+		}
+		else {
+			addText(name, portletURL.toString());
+		}
+	}
+
+	public void addText(String name, String href) {
+		addText(_entries.size(), name, href);
+	}
+
+	public void addText(
+		String align, String valign, int colspan, String name) {
+
+		addText(_entries.size(), align, valign, colspan, name);
+	}
+
+	public void addText(
+		String align, String valign, int colspan, String name,
+		PortletURL portletURL) {
+
+		if (portletURL == null) {
+			addText(align, valign, colspan, name);
+		}
+		else {
+			addText(align, valign, colspan, name, portletURL.toString());
+		}
+	}
+
+	public void addText(
+		String align, String valign, int colspan, String name, String href) {
+
+		addText(_entries.size(), align, valign, colspan, name, href);
+	}
+
+	public void addText(String align, String valign, String name) {
+		addText(
+			_entries.size(), align, valign, SearchEntry.DEFAULT_COLSPAN, name);
+	}
+
+	public void addText(
+		String align, String valign, String name, PortletURL portletURL) {
+
+		addText(
+			align, valign, SearchEntry.DEFAULT_COLSPAN, name, portletURL);
+	}
+
+	public void addText(String align, String valign, String name, String href) {
+		addText(
+			_entries.size(), align, valign, SearchEntry.DEFAULT_COLSPAN, name,
+			href);
+	}
+
+	public void addText(TextSearchEntry searchEntry) {
+		if (_restricted) {
+			searchEntry.setHref(null);
+		}
+
+		_entries.add(_entries.size(), searchEntry);
+	}
+
+	public String getClassHoverName() {
+		return _classHoverName;
+	}
+
+	public String getClassName() {
+		return _className;
+	}
+
+	public List<SearchEntry> getEntries() {
+		return _entries;
+	}
+
+	public Object getObject() {
+		return _obj;
+	}
+
+	public Object getParameter(String param) {
+		if (_params == null) {
+			_params = new HashMap<String, Object>();
+		}
+
+		return _params.get(param);
+	}
+
+	public int getPos() {
+		return _pos;
+	}
+
+	public String getPrimaryKey() {
+		return _primaryKey;
+	}
+
+	public boolean isBold() {
+		return _bold;
+	}
+
+	public boolean isRestricted() {
+		return _restricted;
+	}
+
+	public void setBold(boolean bold) {
+		_bold = bold;
+	}
+
+	public void setClassHoverName(String classHoverName) {
+		_classHoverName = classHoverName;
+	}
+
+	public void setClassName(String className) {
+		_className = className;
+	}
+
+	public void setObject(Object obj) {
+		_obj = obj;
+	}
+
+	public void setParameter(String param, Object value) {
+		if (_params == null) {
+			_params = new HashMap<String, Object>();
+		}
+
+		_params.put(param, value);
+	}
+
+	public void setPrimaryKey(String primaryKey) {
+		_primaryKey = primaryKey;
+	}
+
+	public void setRestricted(boolean restricted) {
+		_restricted = restricted;
+	}
+
 	private boolean _bold;
-	private boolean _restricted;
-	private String _className;
 	private String _classHoverName;
+	private String _className;
 	private List<SearchEntry> _entries;
+	private Object _obj;
 	private Map<String, Object> _params;
+	private int _pos;
+	private String _primaryKey;
+	private boolean _restricted;
 
 }
