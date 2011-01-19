1e92d5c95e16136a3688acf7398c862a68d749c2
Brian Chan <brian.chan@liferay.com>
2011-01-19 19:30:57 +0000

    LPS-7397 Simpler fix so that interface doesn't look polluted
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@70799 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/scheduler/SchedulerProxyAdvice.java
+++ portal-impl/src/com/liferay/portal/scheduler/SchedulerProxyAdvice.java
@@ -14,20 +14,17 @@
 
 package com.liferay.portal.scheduler;
 
-import com.liferay.portal.kernel.scheduler.StringLimiter;
-import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.scheduler.SchedulerEngine;
 import com.liferay.util.aspectj.AspectJUtil;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.lang.reflect.TypeVariable;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 
 /**
  * @author Tina Tian
+ * @author Brian Wing Shun Chan
  */
 public class SchedulerProxyAdvice {
 
@@ -36,97 +33,54 @@ public class SchedulerProxyAdvice {
 
 		Object[] arguments = proceedingJoinPoint.getArgs();
 
-		if (arguments.length != 0) {
-			_handleAnnotations(proceedingJoinPoint, arguments);
-		}
+		fixArguments(proceedingJoinPoint);
 
 		return proceedingJoinPoint.proceed(arguments);
 	}
 
-	private void _doHanldeAnnotation(
-		Annotation annotation, Object[] arguments, int argumentIndex) {
-
-		Object argument = arguments[argumentIndex];
-
+	protected String fixArgumentMaxLength(String argument, int maxLength) {
 		if (argument == null) {
+			return null;
+		}
+
+		if (argument.length() > maxLength) {
+			argument = argument.substring(0, maxLength);
+		}
+
+		return argument;
+	}
+
+	protected void fixArguments(ProceedingJoinPoint proceedingJoinPoint)
+		throws Exception {
+
+		Object[] arguments = proceedingJoinPoint.getArgs();
+
+		if (arguments.length == 0) {
 			return;
 		}
 
-		String stringArgument = (String)argument;
-
-		int maxLength = ((StringLimiter)annotation).maxLength();
-
-		if (stringArgument.length() > maxLength) {
-			arguments[argumentIndex] = stringArgument.substring(0, maxLength);
-		}
-	}
-
-	private String _getMethodUUID(Method method) {
-		Class<?>[] parameterTypes = method.getParameterTypes();
-
-		StringBundler sb = new StringBundler(2 * parameterTypes.length + 1);
-
-		sb.append(method.getName());
-		sb.append("_");
-
-		for (int i = 0; i < parameterTypes.length; i++) {
-			sb.append(parameterTypes[i].getName());
-			
-			if (i != parameterTypes.length - 1) {
-				sb.append("_");
-			}
-		}
-
-		return sb.toString();
-	}
-
-	private void _handleAnnotations(
-			ProceedingJoinPoint proceedingJoinPoint, Object[] arguments)
-		throws Throwable {
-
 		Method method = AspectJUtil.getMethod(proceedingJoinPoint);
-		Annotation[][] annotationArrays = method.getParameterAnnotations();
 
-		String methodUUID = _getMethodUUID(method);
+		TypeVariable<Method>[] typeParameters = method.getTypeParameters();
 
-		int[] indexes = _annotationCache.get(methodUUID);
+		for (int i = 0; i < typeParameters.length; i++) {
+			TypeVariable<Method> typeParameter = typeParameters[i];
 
-		if (indexes == null) {
-			indexes = new int[arguments.length];
+			String typeParameterName = typeParameter.getName();
 
-			for (int i = 0; i < annotationArrays.length; i++) {
-				Annotation[] annotationArray = annotationArrays[i];
+			if (typeParameterName.equals("groupName")) {
+				String groupName = (String)arguments[i];
 
-				if (annotationArray.length <= 0) {
-					continue;
-				}
-
-				for (int j = 0; j < annotationArray.length; j++) {
-					Annotation annotation = annotationArray[j];
-					if ((annotation.annotationType() == StringLimiter.class) &&
-						(arguments[i] instanceof String)) {
-
-						indexes[i] = j + 1;
-						break;
-					}
-				}
+				arguments[i] = fixArgumentMaxLength(
+					groupName, SchedulerEngine.GROUP_NAME_MAX_LENGTH);
 			}
-		}
+			else if (typeParameterName.equals("jobName")) {
+				String jobName = (String)arguments[i];
 
-		for (int i = 0; i < indexes.length; i++) {
-			int annotationIndex = indexes[i];
-
-			if (annotationIndex <= 0) {
-				continue;
+				arguments[i] = fixArgumentMaxLength(
+					jobName, SchedulerEngine.JOB_NAME_MAX_LENGTH);
 			}
-
-			Annotation annotation = annotationArrays[i][annotationIndex - 1];
-
-			_doHanldeAnnotation(annotation, arguments, i);
 		}
 	}
-
-	private Map<String, int[]> _annotationCache =
-		new ConcurrentHashMap<String, int[]>();
 
 }
