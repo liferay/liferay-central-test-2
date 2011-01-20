1fac2c2a2cb8189b149f5d1eae5c7728a9fbe035
Angelo Jefferson <angelo.jefferson@liferay.com>
2011-01-20 02:41:05 +0000

    LPS-14806
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@70898 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBMessageServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBMessageServiceImpl.java
@@ -228,7 +228,7 @@ public class MBMessageServiceImpl extends MBMessageServiceBaseImpl {
 
 			groupId = group.getGroupId();
 			categoryId = MBCategoryConstants.DEFAULT_PARENT_CATEGORY_ID;
-			name = group.getName();
+			name = group.getDescriptiveName();
 			description = group.getDescription();
 		}
 
