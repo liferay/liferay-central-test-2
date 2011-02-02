ed828ac2061cf82545f0ebc5c37fe8d2460ce914
Michael Hashimoto <michael.hashimoto@liferay.com>
2011-02-02 02:02:30 +0000

    LPS-15088
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@72048 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-web/test/com/liferay/portalweb/portlet/addapplication/shopping/searchamazonrankings/SearchAmazonRankingsTest.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portalweb.portlet.addapplication.shopping.searchamazonrankings;
+
+import com.liferay.portalweb.portal.BaseTestCase;
+import com.liferay.portalweb.portal.util.RuntimeVariables;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class SearchAmazonRankingsTest extends BaseTestCase {
+	public void testSearchAmazonRankings() throws Exception {
+		selenium.open("/web/guest/home/");
+		selenium.clickAt("_145_addApplication", RuntimeVariables.replace(""));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isVisible("layout_configuration_content")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.saveScreenShotAndSource();
+		selenium.type("layout_configuration_content",
+			RuntimeVariables.replace(""));
+		selenium.saveScreenShotAndSource();
+		selenium.typeKeys("layout_configuration_content",
+			RuntimeVariables.replace("a"));
+		selenium.saveScreenShotAndSource();
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isVisible("//div[@title='Amazon Rankings']")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.saveScreenShotAndSource();
+		assertTrue(selenium.isVisible("//div[@title='Amazon Rankings']"));
+	}
+}
