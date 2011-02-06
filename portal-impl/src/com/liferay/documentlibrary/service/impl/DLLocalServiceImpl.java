7c663e51b8d27e43563ff112cdde572f94825bcc
Brian Chan <brian.chan@liferay.com>
2011-02-06 19:24:15 +0000

    LPS-15128 Rollback - let's review this on Monday
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@72304 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/documentlibrary/service/impl/DLLocalServiceImpl.java
+++ portal-impl/src/com/liferay/documentlibrary/service/impl/DLLocalServiceImpl.java
@@ -63,7 +63,7 @@ public class DLLocalServiceImpl implements DLLocalService {
 	public void addDirectory(long companyId, long repositoryId, String dirName)
 		throws PortalException, SystemException {
 
-		if (!isValidName(dirName, true) || dirName.equals("/")) {
+		if (!isValidName(dirName) || dirName.equals("/")) {
 			throw new DirectoryNameException(dirName);
 		}
 
@@ -342,7 +342,7 @@ public class DLLocalServiceImpl implements DLLocalService {
 	public void validate(String fileName, boolean validateFileExtension)
 		throws PortalException, SystemException {
 
-		if (!isValidName(fileName, false)) {
+		if (!isValidName(fileName)) {
 			throw new FileNameException(fileName);
 		}
 
@@ -450,9 +450,10 @@ public class DLLocalServiceImpl implements DLLocalService {
 		}
 	}
 
-	protected boolean isValidName(String name, boolean directory) {
+	protected boolean isValidName(String name) {
 		if ((name == null) ||
 			(name.contains("\\\\")) ||
+			(name.contains("/")) ||
 			(name.contains("\\")) ||
 			(name.contains("//")) ||
 			(name.contains(":")) ||
@@ -470,10 +471,6 @@ public class DLLocalServiceImpl implements DLLocalService {
 			(name.contains("\\..")) ||
 			(name.contains("/.."))) {
 
-			return false;
-		}
-
-		if (directory && name.contains("/")) {
 			return false;
 		}
 
