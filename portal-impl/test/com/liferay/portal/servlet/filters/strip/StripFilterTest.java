9dd9af9eeda950bb6db5aeff9e5e49a1ea1d2c0b
Raymond Auge <raymond.auge@liferay.com>
2011-02-11 16:35:45 +0000

    LPS-14981 Fix tests
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@72742 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/test/com/liferay/portal/servlet/filters/strip/StripFilterTest.java
+++ portal-impl/test/com/liferay/portal/servlet/filters/strip/StripFilterTest.java
@@ -277,7 +277,7 @@ public class StripFilterTest extends TestCase {
 		CharBuffer charBuffer = CharBuffer.allocate(0);
 		StringWriter stringWriter = new StringWriter();
 
-		stripFilter.skipWhiteSpace(charBuffer, stringWriter);
+		stripFilter.skipWhiteSpace(charBuffer, stringWriter, true);
 
 		assertEquals("", stringWriter.toString());
 		assertEquals(0, charBuffer.position());
@@ -287,7 +287,7 @@ public class StripFilterTest extends TestCase {
 		charBuffer = CharBuffer.wrap("abc \t\r\n");
 		stringWriter = new StringWriter();
 
-		stripFilter.skipWhiteSpace(charBuffer, stringWriter);
+		stripFilter.skipWhiteSpace(charBuffer, stringWriter, true);
 
 		assertEquals("", stringWriter.toString());
 		assertEquals(0, charBuffer.position());
@@ -297,7 +297,7 @@ public class StripFilterTest extends TestCase {
 		charBuffer = CharBuffer.wrap(" ");
 		stringWriter = new StringWriter();
 
-		stripFilter.skipWhiteSpace(charBuffer, stringWriter);
+		stripFilter.skipWhiteSpace(charBuffer, stringWriter, true);
 
 		assertEquals(" ", stringWriter.toString());
 		assertEquals(1, charBuffer.position());
@@ -305,7 +305,7 @@ public class StripFilterTest extends TestCase {
 		charBuffer = CharBuffer.wrap("\t");
 		stringWriter = new StringWriter();
 
-		stripFilter.skipWhiteSpace(charBuffer, stringWriter);
+		stripFilter.skipWhiteSpace(charBuffer, stringWriter, true);
 
 		assertEquals(" ", stringWriter.toString());
 		assertEquals(1, charBuffer.position());
@@ -313,7 +313,7 @@ public class StripFilterTest extends TestCase {
 		charBuffer = CharBuffer.wrap("\r");
 		stringWriter = new StringWriter();
 
-		stripFilter.skipWhiteSpace(charBuffer, stringWriter);
+		stripFilter.skipWhiteSpace(charBuffer, stringWriter, true);
 
 		assertEquals(" ", stringWriter.toString());
 		assertEquals(1, charBuffer.position());
@@ -321,7 +321,7 @@ public class StripFilterTest extends TestCase {
 		charBuffer = CharBuffer.wrap("\n");
 		stringWriter = new StringWriter();
 
-		stripFilter.skipWhiteSpace(charBuffer, stringWriter);
+		stripFilter.skipWhiteSpace(charBuffer, stringWriter, true);
 
 		assertEquals(" ", stringWriter.toString());
 		assertEquals(1, charBuffer.position());
@@ -331,7 +331,7 @@ public class StripFilterTest extends TestCase {
 		charBuffer = CharBuffer.wrap(" \t\r\n");
 		stringWriter = new StringWriter();
 
-		stripFilter.skipWhiteSpace(charBuffer, stringWriter);
+		stripFilter.skipWhiteSpace(charBuffer, stringWriter, true);
 
 		assertEquals(" ", stringWriter.toString());
 		assertEquals(4, charBuffer.position());
