7f793e492145e2d4792dd03852118f62ca64aba1
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2011-02-15 00:19:07 +0000

    LPS-14356 - Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@72968 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/docroot/html/portlet/forms/js/main.js
+++ portal-web/docroot/html/portlet/forms/js/main.js
@@ -1,332 +1,333 @@
-var parentWindow = window.parent;
-
-if (parentWindow) {
-	Liferay = parentWindow.Liferay;
-}
-
 AUI().add(
 	'liferay-portlet-forms',
 	function(A) {
-		var D = A.DataType;
-		var Lang = A.Lang,
-			isObject = Lang.isObject,
+		var Lang = A.Lang;
 
-			isFormBuilderField = function(v) {
-				return (v instanceof A.FormBuilderField);
-			};
+		var DataType = A.DataType;
 
-		var LiferayFormBuilder = A.Component.create({
+		var FormBuilderField = A.FormBuilderField;
 
-			NAME: 'liferay-form-builder',
+		var instanceOf = A.instanceOf;
 
-			EXTENDS: A.FormBuilder,
+		var MAP_ATTR_LABEL = {
+			name: 'label'
+		};
 
-			ATTRS: {
+		var MAP_ATTR_PREDEFINED_VALUE = {
+			name: 'predefinedValue'
+		};
 
-				availableFields: {
-					value: {
-						'text': {
-							fieldLabel: 'Text',
-							iconClass: 'aui-form-builder-field-icon aui-form-builder-field-icon-text',
-							label: Liferay.Language.get('text-box')
+		var MAP_ATTR_REQUIRED = {
+			name: 'required'
+		};
+
+		var MAP_ATTR_SHOW_LABEL = {
+			name: 'showLabel'
+		};
+
+		var MAP_ELEMENT_DATA = {
+			attributeList: '',
+			nodeName: ''
+		};
+
+		var STR_CDATA_CLOSE = ']]>';
+
+		var STR_CDATA_OPEN = '<![CDATA[';
+
+		var STR_BLANK = '';
+
+		var STR_SPACE = ' ';
+
+		var TPL_ELEMENT = '<{nodeName}{attributeList}></{nodeName}>';
+
+		var LiferayFormBuilder = A.Component.create(
+			{
+				ATTRS: {
+					availableFields: {
+						value: {
+							'text': {
+								fieldLabel: Liferay.Language.get('text-box'),
+								iconClass: 'aui-form-builder-field-icon aui-form-builder-field-icon-text',
+								label: Liferay.Language.get('text-box')
+							},
+
+							'textarea': {
+								fieldLabel: Liferay.Language.get('text-area'),
+								iconClass: 'aui-form-builder-field-icon aui-form-builder-field-icon-textarea',
+								label: Liferay.Language.get('text-area')
+							},
+
+							'checkbox': {
+								fieldLabel: Liferay.Language.get('checkbox'),
+								iconClass: 'aui-form-builder-field-icon aui-form-builder-field-icon-checkbox',
+								label: Liferay.Language.get('checkbox')
+							},
+
+							'button': {
+								fieldLabel: Liferay.Language.get('button'),
+								iconClass: 'aui-form-builder-field-icon aui-form-builder-field-icon-button',
+								label: Liferay.Language.get('button')
+							},
+
+							'select': {
+								fieldLabel: Liferay.Language.get('select-option'),
+								iconClass: 'aui-form-builder-field-icon aui-form-builder-field-icon-select',
+								label: Liferay.Language.get('select-option')
+							},
+
+							'radio': {
+								fieldLabel: Liferay.Language.get('radio-buttons'),
+								iconClass: 'aui-form-builder-field-icon aui-form-builder-field-icon-radio',
+								label: Liferay.Language.get('radio-buttons')
+							},
+
+							'fieldset': {
+								fieldLabel: Liferay.Language.get('fieldset'),
+								iconClass: 'aui-form-builder-field-icon aui-form-builder-field-icon-fieldset',
+								label: Liferay.Language.get('fieldset')
+							},
+
+							'fileupload': {
+								fieldLabel: Liferay.Language.get('file-upload'),
+								iconClass: 'aui-form-builder-field-icon aui-form-builder-field-icon-fileupload',
+								label: Liferay.Language.get('file-upload')
+							}
 						},
-
-						'textarea': {
-							fieldLabel: 'Text Area',
-							iconClass: 'aui-form-builder-field-icon aui-form-builder-field-icon-textarea',
-							label: Liferay.Language.get('text-area')
-						},
-
-						'checkbox': {
-							fieldLabel: 'Checkbox',
-							iconClass: 'aui-form-builder-field-icon aui-form-builder-field-icon-checkbox',
-							label: Liferay.Language.get('checkbox')
-						},
-
-						'button': {
-							fieldLabel: 'Button',
-							iconClass: 'aui-form-builder-field-icon aui-form-builder-field-icon-button',
-							label: Liferay.Language.get('button')
-						},
-
-						'select': {
-							fieldLabel: 'Select',
-							iconClass: 'aui-form-builder-field-icon aui-form-builder-field-icon-select',
-							label: Liferay.Language.get('select-option')
-						},
-
-						'radio': {
-							fieldLabel: 'Radio',
-							iconClass: 'aui-form-builder-field-icon aui-form-builder-field-icon-radio',
-							label: Liferay.Language.get('radio-buttons')
-						},
-
-						'fieldset': {
-							fieldLabel: 'Fieldset',
-							iconClass: 'aui-form-builder-field-icon aui-form-builder-field-icon-fieldset',
-							label: Liferay.Language.get('fieldset')
-						},
-
-						'fileupload': {
-							fieldLabel: 'File Upload',
-							iconClass: 'aui-form-builder-field-icon aui-form-builder-field-icon-fileupload',
-							label: Liferay.Language.get('file-upload')
-						}
+						validator: Lang.isObject
 					},
-					validator: isObject
-				},
 
-				portletNamespace: {
-					value: ''
-				},
+					portletNamespace: {
+						value: STR_BLANK
+					},
 
-				portletResourceNamespace: {
-					value: ''
-				},
+					portletResourceNamespace: {
+						value: STR_BLANK
+					},
 
-				strings: {
-					value: {
-						'stringEmptySelection': Liferay.Language.get('no-field-selected')
-					}
-				}
-			},
-
-			prototype: {
-
-				initializer: function() {
-					var instance = this;
-
-					LiferayFormBuilder.superclass.initializer.apply(instance, arguments);
-
-					instance.addTarget(Liferay.FormBuilderEvents);
-				},
-
-				getXSD: function() {
-					var instance = this;
-					var fields = instance.get('fields');
-					var buffer = [];
-
-					var root = instance._createDynamicNode('root');
-
-					buffer.push(root.openTag);
-
-					A.each(
-						fields,
-						function(item, index, collection) {
-							instance._appendStructureTypeElementAndMetaData(item, buffer);
+					strings: {
+						value: {
+							'stringEmptySelection': Liferay.Language.get('no-field-selected')
 						}
-					);
-
-					buffer.push(root.closeTag);
-					
-					return buffer.join('');
-				},
-
-				getNodeTypeContent: function() {
-					var instance = this;
-
-					return 'dynamic-content';
-				},
-
-				normalizeValue: function(value) {
-					var instance = this;
-
-					if (A.Lang.isUndefined(value)) {
-						value = '';
 					}
-
-					return value;
 				},
 
-				_appendStructureChildren: function(field, buffer, generateArticleContent) {
-					var instance = this;
-					var children = field.get('fields');
+				EXTENDS: A.FormBuilder,
 
-					A.each(
-						children,
-						function(item, index, collection) {
-							instance._appendStructureTypeElementAndMetaData(item, buffer, generateArticleContent);
-						}
-					);
-				},
+				NAME: 'liferayformbuilder',
 
-				_appendStructureFieldOptionsBuffer: function(field, buffer, generateArticleContent) {
-					var instance = this;
+				prototype: {
+					initializer: function() {
+						var instance = this;
 
-					var type = field.get('fieldType');
-					var options = field.get('options');
+						instance.addTarget(window.parent.Liferay);
+					},
 
-					if (options) {
+					getXSD: function() {
+						var instance = this;
+
+						var fields = instance.get('fields');
+						var buffer = [];
+
+						var root = instance._createDynamicNode('root');
+
+						buffer.push(root.openTag);
+
 						A.each(
-							options,
+							fields,
 							function(item, index, collection) {
-								var optionKey = instance._formatOptionsKey(item.name);
-								var optionValue = item.value;
-								
-								var typeElementOption = instance._createDynamicNode(
-									'dynamic-element',
-									{
-										name: optionKey,
-										type: 'option',
-										value: optionValue
-									}
-								);
-
-								buffer.push(typeElementOption.openTag + typeElementOption.closeTag);
+								instance._appendStructureTypeElementAndMetaData(item, buffer);
 							}
 						);
-					}
-				},
 
-				_appendStructureTypeElementAndMetaData: function(field, buffer, generateArticleContent) {
-					var instance = this;
+						buffer.push(root.closeTag);
 
-					var type = field.get('type');
+						return buffer.join(STR_BLANK);
+					},
 
-					var typeElement = instance._createDynamicNode(
-						'dynamic-element',
-						{
-							name: encodeURI(field.get('name')),
-							type: type
+					normalizeValue: function(value) {
+						var instance = this;
+
+						if (Lang.isUndefined(value)) {
+							value = STR_BLANK;
 						}
-					);
 
-					var nodeTypeContent = instance.getNodeTypeContent();
-					var typeContent = instance._createDynamicNode(nodeTypeContent);
-					var metadata = instance._createDynamicNode('meta-data');
+						return value;
+					},
 
-					var entryRequired = instance._createDynamicNode(
-						'entry',
-						{
-							name: 'required'
-						}
-					);
+					_appendStructureChildren: function(field, buffer, generateArticleContent) {
+						var instance = this;
 
-					var label = instance._createDynamicNode(
-						'entry',
-						{
-							name: 'label'
-						}
-					);
+						var children = field.get('fields');
 
-					var predefinedValue = instance._createDynamicNode(
-						'entry',
-						{
-							name: 'predefinedValue'
-						}
-					);
-
-					var showLabel = instance._createDynamicNode(
-						'entry',
-						{
-							name: 'showLabel'
-						}
-					);
-
-					buffer.push(typeElement.openTag);
-
-					instance._appendStructureFieldOptionsBuffer(field, buffer);
-
-					instance._appendStructureChildren(field, buffer, generateArticleContent);
-
-					buffer.push(metadata.openTag);
-
-					var requiredVal = instance.normalizeValue(
-						field.get('required')
-					);
-					buffer.push(entryRequired.openTag);
-					buffer.push('<![CDATA[' + requiredVal + ']]>');
-					buffer.push(entryRequired.closeTag);
-
-					var fieldLabelVal = instance.normalizeValue(
-						field.get('label')
-					);
-					buffer.push(label.openTag);
-					buffer.push('<![CDATA[' + fieldLabelVal + ']]>');
-					buffer.push(label.closeTag);
-
-					var predefinedValueVal = instance.normalizeValue(
-						field.get('predefinedValue')
-					);
-					buffer.push(predefinedValue.openTag);
-					buffer.push('<![CDATA[' + predefinedValueVal + ']]>');
-					buffer.push(predefinedValue.closeTag);
-
-					var showLabelVal = instance.normalizeValue(
-						field.get('showLabel')
-					);
-					buffer.push(showLabel.openTag);
-					buffer.push('<![CDATA[' + showLabelVal + ']]>');
-					buffer.push(showLabel.closeTag);
-
-					buffer.push(metadata.closeTag);
-
-					buffer.push(typeElement.closeTag);
-				},
-
-				_createDynamicNode: function(nodeName, attributeMap) {
-					var instance = this;
-
-					var attrs = [];
-					var typeElement = [];
-
-					if (!nodeName) {
-						nodeName = 'dynamic-element';
-					}
-
-					var typeElementModel = ['<', nodeName, (attributeMap ? ' ' : ''), , '>', ,'</', nodeName, '>'];
-
-					A.each(
-						attributeMap || {},
-						function(item, index, collection) {
-							if (item !== undefined) {
-								attrs.push([index, '="', item, '" '].join(''));
+						A.each(
+							children,
+							function(item, index, collection) {
+								instance._appendStructureTypeElementAndMetaData(item, buffer, generateArticleContent);
 							}
+						);
+					},
+
+					_appendStructureFieldOptionsBuffer: function(field, buffer, generateArticleContent) {
+						var instance = this;
+
+						var type = field.get('fieldType');
+						var options = field.get('options');
+
+						if (options) {
+							A.each(
+								options,
+								function(item, index, collection) {
+									var optionKey = instance._formatOptionsKey(item.name);
+									var optionValue = item.value;
+
+									var typeElementOption = instance._createDynamicNode(
+										'dynamic-element',
+										{
+											name: optionKey,
+											type: 'option',
+											value: optionValue
+										}
+									);
+
+									buffer.push(typeElementOption.openTag + typeElementOption.closeTag);
+								}
+							);
 						}
-					);
+					},
 
-					typeElementModel[3] = attrs.join('').replace(/[\s]+$/g, '');
-					typeElement = typeElementModel.join('').replace(/></, '>><<').replace(/ +>/, '>').split(/></);
+					_appendStructureTypeElementAndMetaData: function(field, buffer, generateArticleContent) {
+						var instance = this;
 
-					return {
-						closeTag: typeElement[1],
-						openTag: typeElement[0]
-					};
-				},
-
-				_formatOptionsKey: function(s) {
-					return s.replace(/\W+/g, ' ').replace(/^\W+|\W+$/g, '').replace(/ /g, '_');
-				},
-
-				_onClickFieldDelete: function(event) {
-					var instance = this;
-					var target = event.currentTarget;
-					var fields = instance.get('fields');
-
-					var fieldBB = target.ancestor('.aui-form-builder-field');
-					var field = fieldBB.getData('field');
-
-					if (field) {
-						var parent = field.get('parent');
-
-						if ((fields.length > 1) || isFormBuilderField(parent)) {
-							var selectedField = instance.selectedField;
-
-							if (field == selectedField ||
-								field.contains(selectedField, true)) {
-
-								instance._tabs.selectTab(0);
+						var typeElement = instance._createDynamicNode(
+							'dynamic-element',
+							{
+								name: encodeURI(field.get('name')),
+								type: field.get('type')
 							}
+						);
 
-							parent.removeField(field);
+						var metadata = instance._createDynamicNode('meta-data');
 
-							field.set('selected', false);
+						var entryRequired = instance._createDynamicNode('entry', MAP_ATTR_REQUIRED);
+
+						var label = instance._createDynamicNode('entry', MAP_ATTR_LABEL);
+
+						var predefinedValue = instance._createDynamicNode('entry', MAP_ATTR_PREDEFINED_VALUE);
+
+						var showLabel = instance._createDynamicNode('entry', MAP_ATTR_SHOW_LABEL);
+
+						buffer.push(typeElement.openTag);
+
+						instance._appendStructureFieldOptionsBuffer(field, buffer);
+
+						instance._appendStructureChildren(field, buffer, generateArticleContent);
+
+						buffer.push(metadata.openTag);
+
+						var requiredVal = instance.normalizeValue(field.get('required'));
+
+						buffer.push(
+							entryRequired.openTag,
+							STR_CDATA_OPEN + requiredVal + STR_CDATA_CLOSE,
+							entryRequired.closeTag
+						);
+
+						var fieldLabelVal = instance.normalizeValue(field.get('label'));
+
+						buffer.push(
+							label.openTag,
+							STR_CDATA_OPEN + fieldLabelVal + STR_CDATA_CLOSE,
+							label.closeTag
+						);
+
+						var predefinedValueVal = instance.normalizeValue(field.get('predefinedValue'));
+
+						buffer.push(
+							predefinedValue.openTag,
+							STR_CDATA_OPEN + predefinedValueVal + STR_CDATA_CLOSE,
+							predefinedValue.closeTag
+						);
+
+						var showLabelVal = instance.normalizeValue(field.get('showLabel'));
+
+						buffer.push(
+							showLabel.openTag,
+							STR_CDATA_OPEN + showLabelVal + STR_CDATA_CLOSE,
+							showLabel.closeTag
+						);
+
+						buffer.push(metadata.closeTag, typeElement.closeTag);
+					},
+
+					_createDynamicNode: function(nodeName, attributeMap) {
+						var instance = this;
+
+						var attrs = [];
+						var typeElement = [];
+
+						if (!nodeName) {
+							nodeName = 'dynamic-element';
+						}
+
+						MAP_ELEMENT_DATA.attributeList = STR_BLANK;
+						MAP_ELEMENT_DATA.nodeName = nodeName;
+
+						if (attributeMap) {
+							A.each(
+								attributeMap,
+								function(item, index, collection) {
+									if (item !== undefined) {
+										attrs.push([index, '="', item, '" '].join(STR_BLANK));
+									}
+								}
+							);
+
+							MAP_ELEMENT_DATA.attributeList = STR_SPACE + attrs.join(STR_BLANK);
+						}
+
+						typeElement = Lang.sub(TPL_ELEMENT, MAP_ELEMENT_DATA);
+						typeElement = typeElement.replace(/\s?(>)(<)/, '$1$1$2$2').split(/></);
+
+						return {
+							closeTag: typeElement[1],
+							openTag: typeElement[0]
+						};
+					},
+
+					_formatOptionsKey: function(s) {
+						return s.replace(/\W+/g, STR_SPACE).replace(/^\W+|\W+$/g, STR_BLANK).replace(/ /g, '_');
+					},
+
+					_onClickFieldDelete: function(event) {
+						var instance = this;
+
+						var target = event.currentTarget;
+						var fields = instance.get('fields');
+
+						var fieldBB = target.ancestor('.aui-form-builder-field');
+						var field = fieldBB.getData('field');
+
+						if (field) {
+							var parent = field.get('parent');
+
+							if (fields.length > 1 || instanceOf(value, FormBuilderField)) {
+								var selectedField = instance.selectedField;
+
+								if (field == selectedField ||
+									field.contains(selectedField, true)) {
+
+									instance._tabs.selectTab(0);
+								}
+
+								parent.removeField(field);
+
+								field.set('selected', false);
+							}
 						}
 					}
 				}
 			}
-
-		});
+		);
 
 		Liferay.FormBuilder = LiferayFormBuilder;
 	},
