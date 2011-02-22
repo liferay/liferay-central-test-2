fef9f8321de64ae94cc709859a9782640ea71c07
Juan Fernandez <juan.fernandez@liferay.com>
2011-02-22 17:42:23 +0000

    LPS-5906 Upgrade process
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@73642 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-impl/src/com/liferay/portal/upgrade/v6_1_0/UpgradeJournalArticle.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.v6_1_0;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.upgrade.util.UpgradeTable;
+import com.liferay.portal.kernel.upgrade.util.UpgradeTableFactoryUtil;
+import com.liferay.portal.upgrade.v6_1_0.util.JournalArticleTable;
+
+/**
+ * @author Juan Fernández
+ */
+public class UpgradeJournalArticle extends UpgradeProcess {
+
+	protected void doUpgrade() throws Exception {
+		try {
+			runSQL("alter_column_type JournalArticle title STRING null");
+		}
+		catch (Exception e) {
+
+			UpgradeTable upgradeTable = UpgradeTableFactoryUtil.getUpgradeTable(
+				JournalArticleTable.TABLE_NAME,
+				JournalArticleTable.TABLE_COLUMNS);
+
+			upgradeTable.setCreateSQL(JournalArticleTable.TABLE_SQL_CREATE);
+			upgradeTable.setIndexesSQL(
+				JournalArticleTable.TABLE_SQL_ADD_INDEXES);
+
+			upgradeTable.updateTable();
+		}
+	}
+
+}
