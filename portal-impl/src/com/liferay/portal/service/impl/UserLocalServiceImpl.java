80ab888e9a232d96e94416b0ab32703b6c2975d6
Connor Mckay <connor.mckay@liferay.com>
2011-02-23 02:13:10 +0000

    LRDOCS-61 revert of an accidental change
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@73706 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -1533,7 +1533,7 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		Group group = user.getGroup();
 
 		if (group != null) {
-			groupLocalService.deleteGroup(group.getGroupId());
+			groupLocalService.deleteGroup(group);
 		}
 
 		// Portrait
