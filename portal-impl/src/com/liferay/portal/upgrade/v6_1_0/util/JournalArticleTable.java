42e21433300f0227108fecb302bc6e5c36bb6a90
Brian Chan <brian.chan@liferay.com>
2011-02-23 19:35:39 +0000

    LPS-5906 Ran "ant build-upgrade-table"
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@73752 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/upgrade/v6_1_0/util/JournalArticleTable.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_1_0/util/JournalArticleTable.java
@@ -17,7 +17,7 @@ package com.liferay.portal.upgrade.v6_1_0.util;
 import java.sql.Types;
 
 /**
- * @author	Juan Fernández
+ * @author	  Brian Wing Shun Chan
  * @generated
  */
 public class JournalArticleTable {
@@ -25,38 +25,39 @@ public class JournalArticleTable {
 	public static final String TABLE_NAME = "JournalArticle";
 
 	public static final Object[][] TABLE_COLUMNS = {
-			{ "uuid_", Types.VARCHAR },
-			{ "id_", Types.BIGINT },
-			{ "resourcePrimKey", Types.BIGINT },
-			{ "groupId", Types.BIGINT },
-			{ "companyId", Types.BIGINT },
-			{ "userId", Types.BIGINT },
-			{ "userName", Types.VARCHAR },
-			{ "createDate", Types.TIMESTAMP },
-			{ "modifiedDate", Types.TIMESTAMP },
-			{ "articleId", Types.VARCHAR },
-			{ "version", Types.DOUBLE },
-			{ "title", Types.VARCHAR },
-			{ "urlTitle", Types.VARCHAR },
-			{ "description", Types.VARCHAR },
-			{ "content", Types.CLOB },
-			{ "type_", Types.VARCHAR },
-			{ "structureId", Types.VARCHAR },
-			{ "templateId", Types.VARCHAR },
-			{ "displayDate", Types.TIMESTAMP },
-			{ "expirationDate", Types.TIMESTAMP },
-			{ "reviewDate", Types.TIMESTAMP },
-			{ "indexable", Types.BOOLEAN },
-			{ "smallImage", Types.BOOLEAN },
-			{ "smallImageId", Types.BIGINT },
-			{ "smallImageURL", Types.VARCHAR },
-			{ "status", Types.INTEGER },
-			{ "statusByUserId", Types.BIGINT },
-			{ "statusByUserName", Types.VARCHAR },
-			{ "statusDate", Types.TIMESTAMP }
-		};
+		{"uuid_", Types.VARCHAR},
+		{"id_", Types.BIGINT},
+		{"resourcePrimKey", Types.BIGINT},
+		{"groupId", Types.BIGINT},
+		{"companyId", Types.BIGINT},
+		{"userId", Types.BIGINT},
+		{"userName", Types.VARCHAR},
+		{"createDate", Types.TIMESTAMP},
+		{"modifiedDate", Types.TIMESTAMP},
+		{"articleId", Types.VARCHAR},
+		{"version", Types.DOUBLE},
+		{"title", Types.VARCHAR},
+		{"urlTitle", Types.VARCHAR},
+		{"description", Types.VARCHAR},
+		{"content", Types.CLOB},
+		{"type_", Types.VARCHAR},
+		{"structureId", Types.VARCHAR},
+		{"templateId", Types.VARCHAR},
+		{"displayDate", Types.TIMESTAMP},
+		{"expirationDate", Types.TIMESTAMP},
+		{"reviewDate", Types.TIMESTAMP},
+		{"indexable", Types.BOOLEAN},
+		{"smallImage", Types.BOOLEAN},
+		{"smallImageId", Types.BIGINT},
+		{"smallImageURL", Types.VARCHAR},
+		{"status", Types.INTEGER},
+		{"statusByUserId", Types.BIGINT},
+		{"statusByUserName", Types.VARCHAR},
+		{"statusDate", Types.TIMESTAMP}
+	};
 
 	public static final String TABLE_SQL_CREATE = "create table JournalArticle (uuid_ VARCHAR(75) null,id_ LONG not null primary key,resourcePrimKey LONG,groupId LONG,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,articleId VARCHAR(75) null,version DOUBLE,title STRING null,urlTitle VARCHAR(150) null,description STRING null,content TEXT null,type_ VARCHAR(75) null,structureId VARCHAR(75) null,templateId VARCHAR(75) null,displayDate DATE null,expirationDate DATE null,reviewDate DATE null,indexable BOOLEAN,smallImage BOOLEAN,smallImageId LONG,smallImageURL STRING null,status INTEGER,statusByUserId LONG,statusByUserName VARCHAR(75) null,statusDate DATE null)";
+
 	public static final String TABLE_SQL_DROP = "drop table JournalArticle";
 
 	public static final String[] TABLE_SQL_ADD_INDEXES = {
@@ -77,4 +78,5 @@ public class JournalArticleTable {
 		"create index IX_F029602F on JournalArticle (uuid_)",
 		"create unique index IX_3463D95B on JournalArticle (uuid_, groupId)"
 	};
+
 }
