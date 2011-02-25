d7a684618452c22802eeca096240cab2063164f5
Brian Chan <brian.chan@liferay.com>
2011-02-25 05:36:07 +0000

    LPS-15478 Rollback, currently breaks in Resin and WebSphere and Weblogic, will explain in comments
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@73947 05bdf26c-840f-0410-9ced-eb539d925f36

--- util-taglib/src/com/liferay/taglib/aui/ButtonTag.java
+++ util-taglib/src/com/liferay/taglib/aui/ButtonTag.java
@@ -15,32 +15,110 @@
 package com.liferay.taglib.aui;
 
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.taglib.aui.base.BaseButtonTag;
+import com.liferay.taglib.util.IncludeTag;
+
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Julio Camarero
- * @author Eduardo Lundgren
- * @author Bruno Basto
- * @author Nathan Cavanaugh
+ * @author Jorge Ferrer
+ * @author Brian Wing Shun Chan
  */
-public class ButtonTag extends BaseButtonTag {
+public class ButtonTag extends IncludeTag {
+
+	public void setCssClass(String cssClass) {
+		_cssClass = cssClass;
+	}
+
+	public void setData(Map<String,Object> data) {
+		_data = data;
+	}
+
+	public void setDisabled(boolean disabled) {
+		_disabled = disabled;
+	}
+
+	public void setInputCssClass(String inputCssClass) {
+		_inputCssClass = inputCssClass;
+	}
+
+	public void setName(String name) {
+		_name = name;
+	}
+
+	public void setOnClick(String onClick) {
+		_onClick = onClick;
+	}
+
+	public void setType(String type) {
+		_type = type;
+	}
+
+	public void setValue(String value) {
+		_value = value;
+	}
+
+	protected void cleanUp() {
+		_cssClass = null;
+		_data = null;
+		_disabled = false;
+		_inputCssClass = null;
+		_name = null;
+		_onClick = null;
+		_type = "button";
+		_value = null;
+	}
+
+	protected String getPage() {
+		return _PAGE;
+	}
+
+	protected boolean isCleanUpSetAttributes() {
+		return _CLEAN_UP_SET_ATTRIBUTES;
+	}
 
 	protected void setAttributes(HttpServletRequest request) {
-		if (Validator.isNull(_value)) {
+		String value = _value;
+
+		if (Validator.isNull(value)) {
 			if (_type.equals("submit")) {
-				_value = "save";
+				value = "save";
 			}
 			else if (_type.equals("cancel")) {
-				_value = "cancel";
+				value = "cancel";
 			}
 			else if (_type.equals("reset")) {
-				_value = "reset";
+				value = "reset";
 			}
 		}
 
-		super.setAttributes(request);
+		request.setAttribute("aui:button:cssClass", _cssClass);
+		request.setAttribute(
+			"aui:button:customAttributes", getCustomAttributes());
+		request.setAttribute("aui:button:data", _data);
+		request.setAttribute("aui:button:disabled", String.valueOf(_disabled));
+		request.setAttribute(
+			"aui:button:dynamicAttributes", getDynamicAttributes());
+		request.setAttribute("aui:button:inputCssClass", _inputCssClass);
+		request.setAttribute("aui:button:name", _name);
+		request.setAttribute("aui:button:onClick", _onClick);
+		request.setAttribute("aui:button:type", _type);
+		request.setAttribute("aui:button:value", value);
 	}
+
+	private static final boolean _CLEAN_UP_SET_ATTRIBUTES = true;
+
+	private static final String _PAGE = "/html/taglib/aui/button/page.jsp";
+
+	private String _cssClass;
+	private Map<String, Object> _data;
+	private boolean _disabled;
+	private String _inputCssClass;
+	private String _name;
+	private String _onClick;
+	private String _type = "button";
+	private String _value;
 
 }
