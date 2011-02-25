add899eff8345dd202c3f2f738707b828e74139a
Brian Chan <brian.chan@liferay.com>
2011-02-25 19:17:46 +0000

    LPS-15090 Rollback, breaks tests
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@74012 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/documentlibrary/webdav/DLWebDAVStorageImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/webdav/DLWebDAVStorageImpl.java
@@ -761,13 +761,6 @@ public class DLWebDAVStorageImpl extends BaseWebDAVStorageImpl {
 
 				DLAppServiceUtil.unlockFileEntry(
 					fileEntry.getFileEntryId(), token);
-
-				String title = fileEntry.getTitle();
-
-				if (title.startsWith(_APPLE_DOUBLE_PREFIX)) {
-					DLAppServiceUtil.deleteFileEntry(
-						fileEntry.getFileEntryId());
-				}
 			}
 			else {
 				Folder folder = (Folder)resource.getModel();
@@ -993,8 +986,6 @@ public class DLWebDAVStorageImpl extends BaseWebDAVStorageImpl {
 
 		return resource;
 	}
-
-	private static final String _APPLE_DOUBLE_PREFIX = "._";
 
 	private static Log _log = LogFactoryUtil.getLog(DLWebDAVStorageImpl.class);
 
