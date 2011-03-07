ed5766c66229974ca9c2690ee6ed7462cf66bcd7
Raymond Auge <raymond.auge@liferay.com>
2011-03-07 20:56:44 +0000

    LPS-15297 Can't set portlet preferences for theme embedded portlets
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@74688 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/service/impl/PortletPreferencesLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/PortletPreferencesLocalServiceImpl.java
@@ -307,8 +307,9 @@ public class PortletPreferencesLocalServiceImpl
 					ownerId, ownerType, plid, portletId);
 
 			if (portletPreferences == null) {
-				if ((portlet != null) && portlet.isUndeployedPortlet() &&
-					PortletPreferencesThreadLocal.isStrict()) {
+				if (((portlet != null) && portlet.isUndeployedPortlet() &&
+						PortletPreferencesThreadLocal.isStrict()) ||
+					(defaultPreferences == null)) {
 
 					return new PortletPreferencesImpl();
 				}
