5764defd3fe9251e105a6dd2c65b7d1224ad9602
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2011-03-14 20:08:13 +0000

    LPS-15480 Taglib Builder - Porting AUI Button
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@75138 05bdf26c-840f-0410-9ced-eb539d925f36

--- util-taglib/src/com/liferay/taglib/aui/ButtonTag.java
+++ util-taglib/src/com/liferay/taglib/aui/ButtonTag.java
@@ -15,9 +15,7 @@
 package com.liferay.taglib.aui;
 
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.taglib.util.IncludeTag;
-
-import java.util.Map;
+import com.liferay.taglib.aui.base.BaseButtonTag;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -26,60 +24,11 @@ import javax.servlet.http.HttpServletRequest;
  * @author Jorge Ferrer
  * @author Brian Wing Shun Chan
  */
-public class ButtonTag extends IncludeTag {
-
-	public void setCssClass(String cssClass) {
-		_cssClass = cssClass;
-	}
-
-	public void setData(Map<String,Object> data) {
-		_data = data;
-	}
-
-	public void setDisabled(boolean disabled) {
-		_disabled = disabled;
-	}
-
-	public void setInputCssClass(String inputCssClass) {
-		_inputCssClass = inputCssClass;
-	}
-
-	public void setName(String name) {
-		_name = name;
-	}
-
-	public void setOnClick(String onClick) {
-		_onClick = onClick;
-	}
-
-	public void setType(String type) {
-		_type = type;
-	}
-
-	public void setValue(String value) {
-		_value = value;
-	}
-
-	protected void cleanUp() {
-		_cssClass = null;
-		_data = null;
-		_disabled = false;
-		_inputCssClass = null;
-		_name = null;
-		_onClick = null;
-		_type = "button";
-		_value = null;
-	}
-
-	protected String getPage() {
-		return _PAGE;
-	}
-
-	protected boolean isCleanUpSetAttributes() {
-		return _CLEAN_UP_SET_ATTRIBUTES;
-	}
+public class ButtonTag extends BaseButtonTag {
 
 	protected void setAttributes(HttpServletRequest request) {
+		super.setAttributes(request);
+
 		String value = _value;
 
 		if (Validator.isNull(value)) {
@@ -94,31 +43,7 @@ public class ButtonTag extends IncludeTag {
 			}
 		}
 
-		request.setAttribute("aui:button:cssClass", _cssClass);
-		request.setAttribute(
-			"aui:button:customAttributes", getCustomAttributes());
-		request.setAttribute("aui:button:data", _data);
-		request.setAttribute("aui:button:disabled", String.valueOf(_disabled));
-		request.setAttribute(
-			"aui:button:dynamicAttributes", getDynamicAttributes());
-		request.setAttribute("aui:button:inputCssClass", _inputCssClass);
-		request.setAttribute("aui:button:name", _name);
-		request.setAttribute("aui:button:onClick", _onClick);
-		request.setAttribute("aui:button:type", _type);
-		request.setAttribute("aui:button:value", value);
+		setNamespacedAttribute(request, "value", value);
 	}
-
-	private static final boolean _CLEAN_UP_SET_ATTRIBUTES = true;
-
-	private static final String _PAGE = "/html/taglib/aui/button/page.jsp";
-
-	private String _cssClass;
-	private Map<String, Object> _data;
-	private boolean _disabled;
-	private String _inputCssClass;
-	private String _name;
-	private String _onClick;
-	private String _type = "button";
-	private String _value;
 
 }
