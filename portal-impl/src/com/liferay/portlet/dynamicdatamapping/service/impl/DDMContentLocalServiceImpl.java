23972d730702a2d3bca4f94867426c9db317b308
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2011-03-17 01:12:11 +0000

    LPS-15881 Removing contentKey
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@75402 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/impl/DDMContentLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/impl/DDMContentLocalServiceImpl.java
@@ -16,15 +16,12 @@ package com.liferay.portlet.dynamicdatamapping.service.impl;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.xml.DocumentException;
 import com.liferay.portal.kernel.xml.SAXReaderUtil;
 import com.liferay.portal.model.User;
 import com.liferay.portal.service.ServiceContext;
-import com.liferay.portlet.dynamicdatamapping.ContentDuplicateContentKeyException;
 import com.liferay.portlet.dynamicdatamapping.ContentException;
-import com.liferay.portlet.dynamicdatamapping.ContentKeyException;
 import com.liferay.portlet.dynamicdatamapping.ContentNameException;
 import com.liferay.portlet.dynamicdatamapping.ContentXmlException;
 import com.liferay.portlet.dynamicdatamapping.model.DDMContent;
@@ -41,8 +38,7 @@ import java.util.List;
 public class DDMContentLocalServiceImpl extends DDMContentLocalServiceBaseImpl {
 
 	public DDMContent addContent(
-			long userId, long groupId, String contentKey,
-			boolean autoContentKey, String name, String description,
+			long userId, long groupId, String name, String description,
 			String xml, ServiceContext serviceContext)
 		throws PortalException, SystemException {
 
@@ -50,12 +46,6 @@ public class DDMContentLocalServiceImpl extends DDMContentLocalServiceBaseImpl {
 
 		User user = userPersistence.findByPrimaryKey(
 			serviceContext.getUserId());
-
-		contentKey = contentKey.trim().toUpperCase();
-
-		if (autoContentKey) {
-			contentKey = String.valueOf(counterLocalService.increment());
-		}
 
 		try {
 			xml = DDMXMLUtil.formatXML(xml);
@@ -66,7 +56,7 @@ public class DDMContentLocalServiceImpl extends DDMContentLocalServiceBaseImpl {
 
 		Date now = new Date();
 
-		validate(groupId, name, xml, contentKey, autoContentKey);
+		validate(groupId, name, xml);
 
 		long contentId = counterLocalService.increment();
 
@@ -79,7 +69,6 @@ public class DDMContentLocalServiceImpl extends DDMContentLocalServiceBaseImpl {
 		content.setUserName(user.getFullName());
 		content.setCreateDate(serviceContext.getCreateDate(now));
 		content.setModifiedDate(serviceContext.getModifiedDate(now));
-		content.setContentKey(contentKey);
 		content.setName(name);
 		content.setDescription(description);
 		content.setXml(xml);
@@ -97,15 +86,6 @@ public class DDMContentLocalServiceImpl extends DDMContentLocalServiceBaseImpl {
 		ddmContentPersistence.remove(content);
 	}
 
-	public void deleteContent(long groupId, String contentKey)
-		throws PortalException, SystemException {
-
-		DDMContent content =
-			ddmContentPersistence.findByG_C(groupId, contentKey);
-
-		deleteContent(content);
-	}
-
 	public void deleteContents(long groupId)
 		throws PortalException, SystemException {
 
@@ -120,12 +100,6 @@ public class DDMContentLocalServiceImpl extends DDMContentLocalServiceBaseImpl {
 		throws PortalException, SystemException {
 
 		return ddmContentPersistence.findByPrimaryKey(contentId);
-	}
-
-	public DDMContent getContent(long groupId, String contentKey)
-		throws PortalException, SystemException {
-
-		return ddmContentPersistence.findByG_C(groupId, contentKey);
 	}
 
 	public List<DDMContent> getContents() throws SystemException {
@@ -147,8 +121,8 @@ public class DDMContentLocalServiceImpl extends DDMContentLocalServiceBaseImpl {
 	}
 
 	public DDMContent updateContent(
-			long groupId, String contentKey, String name,
-			String description, String xml, ServiceContext serviceContext)
+			long groupId, long contentId, String name, String description,
+			String xml, ServiceContext serviceContext)
 		throws PortalException, SystemException {
 
 		try {
@@ -160,8 +134,7 @@ public class DDMContentLocalServiceImpl extends DDMContentLocalServiceBaseImpl {
 
 		validate(groupId, name, xml);
 
-		DDMContent content =
-			ddmContentPersistence.findByG_C(groupId, contentKey);
+		DDMContent content = ddmContentPersistence.findByPrimaryKey(contentId);
 
 		content.setModifiedDate(serviceContext.getModifiedDate(null));
 		content.setName(name);
@@ -181,33 +154,6 @@ public class DDMContentLocalServiceImpl extends DDMContentLocalServiceBaseImpl {
 		}
 
 		validateXml(xml);
-	}
-
-	protected void validate(
-			long groupId, String name, String xml, String contentKey,
-			boolean autoContentKey)
-		throws PortalException, SystemException {
-
-		if (!autoContentKey) {
-			validate(contentKey);
-
-			DDMContent content = ddmContentPersistence.fetchByG_C(
-				groupId, contentKey);
-
-			if (content != null) {
-				throw new ContentDuplicateContentKeyException();
-			}
-		}
-
-		validate(groupId, name, xml);
-	}
-
-	protected void validate(String contentKey) throws PortalException {
-		if ((Validator.isNull(contentKey)) ||
-			(contentKey.indexOf(CharPool.SPACE) != -1)) {
-
-			throw new ContentKeyException();
-		}
 	}
 
 	protected void validateXml(String xml) throws PortalException {
