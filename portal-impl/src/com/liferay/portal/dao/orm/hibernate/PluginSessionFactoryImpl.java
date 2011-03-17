7d3e1fed78e4278bfd755f8118478f7ec341e441
Shuyang Zhou <shuyang.zhou@liferay.com>
2011-03-17 14:14:57 +0000

    LPS-15904 Enable plugins to share portal's transaction when they are using default DataSource
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@75453 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-impl/src/com/liferay/portal/dao/orm/hibernate/PluginSessionFactoryImpl.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.dao.orm.hibernate;
+
+import com.liferay.portal.kernel.dao.jdbc.CurrentConnectionUtil;
+import com.liferay.portal.kernel.dao.orm.ORMException;
+import com.liferay.portal.kernel.dao.orm.Session;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.util.PropsValues;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.hibernate.engine.SessionFactoryImplementor;
+
+/**
+ * @author Shuyang Zhou
+ */
+public class PluginSessionFactoryImpl extends SessionFactoryImpl {
+
+	public Session openSession() throws ORMException {
+
+		SessionFactoryImplementor sessionFactoryImplementor =
+			getSessionFactoryImplementor();
+
+		org.hibernate.Session session = null;
+
+		if (PropsValues.SPRING_HIBERNATE_SESSION_DELEGATED) {
+			Connection connection = CurrentConnectionUtil.getCurrentConnection(
+				_dataSource);
+			if (connection == null) {
+				session = sessionFactoryImplementor.getCurrentSession();
+			}
+			else {
+				session = sessionFactoryImplementor.openSession(connection);
+			}
+		}
+		else {
+			session = sessionFactoryImplementor.openSession();
+		}
+
+		if (_log.isDebugEnabled()) {
+			org.hibernate.impl.SessionImpl sessionImpl =
+				(org.hibernate.impl.SessionImpl)session;
+
+			_log.debug(
+				"Session is using connection release mode " +
+					sessionImpl.getConnectionReleaseMode());
+		}
+
+		return wrapSession(session);
+	}
+
+	public void setDataSource(DataSource dataSource) {
+		_dataSource = dataSource;
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(
+		PluginSessionFactoryImpl.class);
+
+	private DataSource _dataSource;
+
+}
---
+++ portal-service/src/com/liferay/portal/kernel/dao/jdbc/CurrentConnectionUtil.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.dao.jdbc;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Shuyang Zhou
+ */
+public class CurrentConnectionUtil {
+
+	public static Connection getCurrentConnection(DataSource dataSource) {
+		return _currentConnection.getCurrentConnection(dataSource);
+	}
+
+	public void setCurrentConnection(CurrentConnection currentConnection) {
+		_currentConnection = currentConnection;
+	}
+
+	private static CurrentConnection _currentConnection;
+
+}
