503c6516e01a197060f3217cc915f2b9c7b59714
Brian Chan <brian.chan@liferay.com>
2011-03-18 23:21:03 +0000

    LPS-14612 Rollback, names are confusing
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@75642 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/dynamicdatamapping/storage/XMLStorageAdapter.java
+++ portal-impl/src/com/liferay/portlet/dynamicdatamapping/storage/XMLStorageAdapter.java
@@ -14,73 +14,9 @@
 
 package com.liferay.portlet.dynamicdatamapping.storage;
 
-import com.liferay.portal.kernel.util.OrderByComparator;
-import com.liferay.portal.service.ServiceContext;
-
-import java.io.Serializable;
-
-import java.util.List;
-import java.util.Map;
-
 /**
  * @author Eduardo Lundgren
  * @author Brian Wing Shun Chan
  */
 public class XMLStorageAdapter extends BaseStorageAdapter {
-
-	protected long doCreate(
-			long companyId, long structureId, Map<String, Serializable> fields,
-			ServiceContext serviceContext)
-		throws Exception {
-
-		throw new UnsupportedOperationException();
-	}
-
-	protected void doDelete(long classPK) throws Exception {
-	}
-
-	protected void doDeleteAll(long structureId) throws Exception {
-	}
-
-	protected Map<Long, List<Map<String, Serializable>>> doGetAllFields(
-			long[] structureIds, List<String> fieldNames, OrderByComparator obc)
-		throws Exception {
-
-		return null;
-	}
-
-	protected Map<Long, Map<String, Serializable>> doGetFields(
-			long[] classPKs, List<String> fieldNames)
-		throws Exception {
-
-		return null;
-	}
-
-	protected List<Map<String, Serializable>> doGetFields(
-			long[] classPKs, List<String> fieldNames, OrderByComparator obc)
-		throws Exception {
-
-		return null;
-	}
-
-	protected List<Map<String, Serializable>> doQuery(
-			long structureId, List<String> fieldNames, String whereClause,
-			OrderByComparator obc)
-		throws Exception {
-
-		return null;
-	}
-
-	protected int doQueryCount(long structureId, String whereClause)
-		throws Exception {
-
-		return 0;
-	}
-
-	protected void doUpdate(
-			long classPK, Map<String, Serializable> fields, boolean merge,
-			ServiceContext serviceContext)
-		throws Exception {
-	}
-
 }
