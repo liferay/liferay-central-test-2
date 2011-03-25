881822a5c65f8ba52b5d9ce2bfeaa19c4f32bffd
Brett Ripley <brett.ripley@liferay.com>
2011-03-25 17:43:43 +0000

    LPS-15822
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@76185 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/blogs/action/FindEntryAction.java
+++ portal-impl/src/com/liferay/portlet/blogs/action/FindEntryAction.java
@@ -71,8 +71,6 @@ public class FindEntryAction extends Action {
 				plid = getPlid(plid, portletId, entryId);
 			}
 
-			String urlTitle = getUrlTitle(entryId);
-
 			PortletURL portletURL = new PortletURLImpl(
 				request, portletId, plid, PortletRequest.RENDER_PHASE);
 
@@ -88,12 +86,7 @@ public class FindEntryAction extends Action {
 			portletURL.setParameter("struts_action", strutsAction);
 
 			if (!showAllEntries) {
-				if (Validator.isNotNull(urlTitle)) {
-					portletURL.setParameter("urlTitle", urlTitle);
-				}
-				else {
 					portletURL.setParameter("entryId", String.valueOf(entryId));
-				}
 			}
 
 			response.sendRedirect(portletURL.toString());
@@ -166,23 +159,6 @@ public class FindEntryAction extends Action {
 		}
 
 		return strutsAction;
-	}
-
-	protected String getUrlTitle(long entryId) {
-		String urlTitle = StringPool.BLANK;
-
-		try {
-			BlogsEntry entry = BlogsEntryLocalServiceUtil.getEntry(entryId);
-
-			urlTitle = entry.getUrlTitle();
-		}
-		catch (Exception e) {
-			if (_log.isWarnEnabled()) {
-				_log.warn(e);
-			}
-		}
-
-		return urlTitle;
 	}
 
 	private static Log _log = LogFactoryUtil.getLog(FindEntryAction.class);
