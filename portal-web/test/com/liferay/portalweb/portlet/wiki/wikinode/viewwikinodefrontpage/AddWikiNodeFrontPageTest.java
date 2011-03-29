801ceb15ebc0059d74f6dca4d0c49b67ff99d5e2
Michael Hashimoto <michael.hashimoto@liferay.com>
2011-03-29 22:30:15 +0000

    LPS-7168
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@76548 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/test/com/liferay/portalweb/portlet/wiki/wikinode/viewwikinodefrontpage/AddWikiNodeFrontPageTest.java
+++ portal-web/test/com/liferay/portalweb/portlet/wiki/wikinode/viewwikinodefrontpage/AddWikiNodeFrontPageTest.java
@@ -121,7 +121,7 @@ public class AddWikiNodeFrontPageTest extends BaseTestCase {
 		selenium.waitForPageToLoad("30000");
 		selenium.saveScreenShotAndSource();
 		assertEquals(RuntimeVariables.replace(
-				"Your request processed successfully."),
+				"Your request completed successfully."),
 			selenium.getText("//div[@class='portlet-msg-success']"));
 		assertEquals(RuntimeVariables.replace(
 				"This is a wiki node frontpage test."),
