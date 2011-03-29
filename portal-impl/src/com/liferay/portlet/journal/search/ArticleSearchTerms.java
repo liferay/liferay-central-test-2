93731b6b132be9504da6dd744cb62bc410aaaf76
Dale Shan <dale.shan@liferay.com>
2011-03-29 06:30:03 +0000

    LPS-16129
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@76449 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/journal/search/ArticleSearchTerms.java
+++ portal-impl/src/com/liferay/portlet/journal/search/ArticleSearchTerms.java
@@ -45,7 +45,7 @@ public class ArticleSearchTerms extends ArticleDisplayTerms {
 		templateId = DAOParamUtil.getString(portletRequest, TEMPLATE_ID);
 		title = DAOParamUtil.getLike(portletRequest, TITLE);
 		type = DAOParamUtil.getString(portletRequest, TYPE);
-		version = ParamUtil.getDouble(portletRequest, VERSION);
+		version = ParamUtil.getDouble(portletRequest, VERSION, -1);
 	}
 
 	public Date getReviewDate() {
