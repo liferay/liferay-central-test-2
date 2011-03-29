1036f20f24d50d36627b1f690a03da98bfeb4b7d
Brian Chan <brian.chan@liferay.com>
2011-03-29 05:14:10 +0000

    LPS-15351 Contains bad duplicate logic, need you to recheck it in one layer at a time so it can be reviewed
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@76426 05bdf26c-840f-0410-9ced-eb539d925f36

--- tools/servers/tcat/bin/setenv.bat
+++
@@ -1,9 +0,0 @@
-if exist "%CATALINA_HOME%/jre${jdk.windows.version}/win" (
-	if not "%JAVA_HOME%" == "" (
-		set JAVA_HOME=
-	)
-
-	set "JRE_HOME=%CATALINA_HOME%/jre${jdk.windows.version}/win"
-)
-
-set "JAVA_OPTS=%JAVA_OPTS% -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
