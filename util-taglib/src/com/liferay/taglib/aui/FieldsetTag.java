5802dac99c504e09e4d411bd3f07b06c13ca65ec
Bruno Basto <bruno.basto@liferay.com>
2011-03-29 20:48:50 +0000

    LPS-16165 Taglib Builder - Porting AUI Fieldset
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@76536 05bdf26c-840f-0410-9ced-eb539d925f36

--- util-taglib/src/com/liferay/taglib/aui/FieldsetTag.java
+++ util-taglib/src/com/liferay/taglib/aui/FieldsetTag.java
@@ -14,9 +14,8 @@
 
 package com.liferay.taglib.aui;
 
-import com.liferay.taglib.util.IncludeTag;
+import com.liferay.taglib.aui.base.BaseFieldsetTag;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspWriter;
 
 /**
@@ -24,37 +23,7 @@ import javax.servlet.jsp.JspWriter;
  * @author Jorge Ferrer
  * @author Brian Wing Shun Chan
  */
-public class FieldsetTag extends IncludeTag {
-
-	public void setColumn(boolean column) {
-		_column = column;
-	}
-
-	public void setCssClass(String cssClass) {
-		_cssClass = cssClass;
-	}
-
-	public void setLabel(String label) {
-		_label = label;
-	}
-
-	protected void cleanUp() {
-		_column = false;
-		_cssClass = null;
-		_label = null;
-	}
-
-	protected String getEndPage() {
-		return _END_PAGE;
-	}
-
-	protected String getStartPage() {
-		return _START_PAGE;
-	}
-
-	protected boolean isCleanUpSetAttributes() {
-		return _CLEAN_UP_SET_ATTRIBUTES;
-	}
+public class FieldsetTag extends BaseFieldsetTag {
 
 	protected int processEndTag() throws Exception {
 		JspWriter jspWriter = pageContext.getOut();
@@ -63,25 +32,5 @@ public class FieldsetTag extends IncludeTag {
 
 		return EVAL_PAGE;
 	}
-
-	protected void setAttributes(HttpServletRequest request) {
-		request.setAttribute("aui:fieldset:column", String.valueOf(_column));
-		request.setAttribute("aui:fieldset:cssClass", _cssClass);
-		request.setAttribute(
-			"aui:fieldset:dynamicAttributes", getDynamicAttributes());
-		request.setAttribute("aui:fieldset:label", _label);
-	}
-
-	private static final boolean _CLEAN_UP_SET_ATTRIBUTES = true;
-
-	private static final String _END_PAGE =
-		"/html/taglib/aui/fieldset/end.jsp";
-
-	private static final String _START_PAGE =
-		"/html/taglib/aui/fieldset/start.jsp";
-
-	private boolean _column;
-	private String _cssClass;
-	private String _label;
 
 }
---
+++ util-taglib/src/com/liferay/taglib/aui/base/BaseFieldsetTag.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.taglib.aui.base;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+
+/**
+ * @author Eduardo Lundgren
+ * @author Bruno Basto
+ * @author Nathan Cavanaugh
+ * @author Julio Camarero
+ * @generated
+ */
+public class BaseFieldsetTag extends com.liferay.taglib.util.IncludeTag {
+
+	public int doStartTag() throws JspException {
+		setAttributeNamespace(_ATTRIBUTE_NAMESPACE);
+
+		return super.doStartTag();
+	}
+
+	public java.lang.Boolean getColumn() {
+		return _column;
+	}
+
+	public java.lang.String getCssClass() {
+		return _cssClass;
+	}
+
+	public java.lang.String getLabel() {
+		return _label;
+	}
+
+	public void setColumn(java.lang.Boolean column) {
+		_column = column;
+
+		setScopedAttribute("column", column);
+	}
+
+	public void setCssClass(java.lang.String cssClass) {
+		_cssClass = cssClass;
+
+		setScopedAttribute("cssClass", cssClass);
+	}
+
+	public void setLabel(java.lang.String label) {
+		_label = label;
+
+		setScopedAttribute("label", label);
+	}
+
+	protected void cleanUp() {
+		_column = null;
+		_cssClass = null;
+		_label = null;
+	}
+
+	protected String getEndPage() {
+		return _END_PAGE;
+	}
+
+	protected String getStartPage() {
+		return _START_PAGE;
+	}
+
+
+	protected void setAttributes(HttpServletRequest request) {
+		setNamespacedAttribute(request, "column", _column);
+		setNamespacedAttribute(request, "cssClass", _cssClass);
+		setNamespacedAttribute(request, "label", _label);
+	}
+
+	protected static final String _ATTRIBUTE_NAMESPACE = "aui:fieldset:";
+
+	private static final String _END_PAGE =
+		"/html/taglib/aui/fieldset/end.jsp";
+
+	private static final String _START_PAGE =
+		"/html/taglib/aui/fieldset/start.jsp";
+
+	protected java.lang.Boolean _column;
+	protected java.lang.String _cssClass;
+	protected java.lang.String _label;
+
+}
