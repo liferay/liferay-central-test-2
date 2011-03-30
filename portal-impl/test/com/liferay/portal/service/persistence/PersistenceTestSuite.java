0c4064be503631ea6993ef3af6eb12d0ad9949fa
Brian Chan <brian.chan@liferay.com>
2011-03-30 06:30:15 +0000

    Tests
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@76609 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/test/com/liferay/portal/service/persistence/PersistenceTestSuite.java
+++ portal-impl/test/com/liferay/portal/service/persistence/PersistenceTestSuite.java
@@ -38,6 +38,7 @@ import com.liferay.portlet.documentlibrary.service.persistence.DLFileShortcutPer
 import com.liferay.portlet.documentlibrary.service.persistence.DLFileVersionPersistenceTest;
 import com.liferay.portlet.documentlibrary.service.persistence.DLFolderPersistenceTest;
 import com.liferay.portlet.dynamicdatamapping.service.persistence.DDMContentPersistenceTest;
+import com.liferay.portlet.dynamicdatamapping.service.persistence.DDMListPersistenceTest;
 import com.liferay.portlet.dynamicdatamapping.service.persistence.DDMStorageLinkPersistenceTest;
 import com.liferay.portlet.dynamicdatamapping.service.persistence.DDMStructureLinkPersistenceTest;
 import com.liferay.portlet.dynamicdatamapping.service.persistence.DDMStructurePersistenceTest;
@@ -197,6 +198,7 @@ public class PersistenceTestSuite extends TestSuite {
 		testSuite.addTestSuite(DLFolderPersistenceTest.class);
 
 		testSuite.addTestSuite(DDMContentPersistenceTest.class);
+		testSuite.addTestSuite(DDMListPersistenceTest.class);
 		testSuite.addTestSuite(DDMStorageLinkPersistenceTest.class);
 		testSuite.addTestSuite(DDMStructureLinkPersistenceTest.class);
 		testSuite.addTestSuite(DDMStructurePersistenceTest.class);
