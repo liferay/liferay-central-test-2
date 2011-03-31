2b6a651326263539b5885c30f930e3a5cc3ad09c
Brian Chan <brian.chan@liferay.com>
2011-03-31 05:41:16 +0000

    LPS-16207 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@76723 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/servlet/LuceneServlet.java
+++ portal-impl/src/com/liferay/portal/servlet/LuceneServlet.java
@@ -14,12 +14,12 @@
 
 package com.liferay.portal.servlet;
 
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.search.lucene.LuceneHelperUtil;
 import com.liferay.portal.security.auth.TransientTokenUtil;
+import com.liferay.portal.service.CompanyLocalServiceUtil;
 
 import java.io.IOException;
 
@@ -36,75 +36,46 @@ public class LuceneServlet extends HttpServlet {
 			HttpServletRequest request, HttpServletResponse response)
 		throws IOException {
 
-		String pathInfo = request.getPathInfo();
-		if (!Validator.equals(LUCENE_DUMP_INDEX_URL_PATH, pathInfo)) {
-			if (_log.isWarnEnabled()) {
-				_log.warn("Request for path : " + pathInfo +
-					" is not supported.");
-			}
+		String pathInfo = GetterUtil.getString(request.getPathInfo());
 
+		if (!pathInfo.equals("/dump")) {
 			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+
 			return;
 		}
 
-		// Checking token
-		String token = request.getParameter(LUCENE_DUMP_INDEX_TOKEN);
+		String transientToken = ParamUtil.getString(request, "transientToken");
 
-		if (Validator.isNull(token)) {
-			if (_log.isWarnEnabled()) {
-				_log.warn("Request does not have an index dump token");
-			}
-
+		if (Validator.isNull(transientToken)) {
 			response.sendError(HttpServletResponse.SC_FORBIDDEN);
+
 			return;
 		}
 
-		boolean valid = TransientTokenUtil.checkToken(token);
-
-		if (!valid) {
-			if (_log.isWarnEnabled()) {
-				_log.warn("Request does not have a valid index dump token");
-			}
-
+		if (!TransientTokenUtil.checkToken(transientToken)) {
 			response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-		}
 
-		// Checking companyId
-		String companyIdString = request.getParameter(
-			LUCENE_DUMP_INDEX_COMPANY_ID);
-
-		if (Validator.isNull(companyIdString)) {
-			if (_log.isWarnEnabled()) {
-				_log.warn("Request does not have an index dump companyId");
-			}
-
-			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
 			return;
 		}
 
-		long companyId = GetterUtil.getLong(companyIdString, -1);
+		long companyId = ParamUtil.getLong(request, "companyId");
 
-		if (companyId == -1) {
-			if (_log.isWarnEnabled()) {
-				_log.warn("Request does not have a valid index dump companyId");
-			}
-
+		if (companyId <= 0) {
 			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+
 			return;
 		}
 
-		// Dump out indexes
+		try {
+			CompanyLocalServiceUtil.getCompanyById(companyId);
+		}
+		catch (Exception e) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+
+			return;
+		}
+
 		LuceneHelperUtil.dumpIndex(companyId, response.getOutputStream());
 	}
-
-	public static final String LUCENE_DUMP_INDEX_COMPANY_ID =
-		"LUCENE_DUMP_INDEX_COMPANY_ID";
-
-	public static final String LUCENE_DUMP_INDEX_TOKEN =
-		"LUCENE_DUMP_INDEX_TOKEN";
-
-	public static final String LUCENE_DUMP_INDEX_URL_PATH = "/dump";
-
-	private static Log _log = LogFactoryUtil.getLog(LuceneServlet.class);
 
 }
