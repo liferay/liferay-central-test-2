1ad4b333cb9d17b4754ee5631d1f88fd69817bbe
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2011-04-07 05:36:07 +0000

    LPS-16356 DDMTemplate (manual)
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@77495 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/impl/DDMTemplateLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/impl/DDMTemplateLocalServiceImpl.java
@@ -14,11 +14,195 @@
 
 package com.liferay.portlet.dynamicdatamapping.service.impl;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.ResourceConstants;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portlet.dynamicdatamapping.model.DDMTemplate;
 import com.liferay.portlet.dynamicdatamapping.service.base.DDMTemplateLocalServiceBaseImpl;
+import com.liferay.portlet.journal.TemplateNameException;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author Brian Wing Shun Chan
+ * @author Eduardo Lundgren
  */
 public class DDMTemplateLocalServiceImpl
 	extends DDMTemplateLocalServiceBaseImpl {
+
+	public DDMTemplate addTemplate(
+			long structureId, String name, String description, String type,
+			String language, String script, ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		// View
+
+		User user = userPersistence.findByPrimaryKey(
+			serviceContext.getUserId());
+
+		Date now = new Date();
+
+		validate(name);
+
+		long templateId = counterLocalService.increment();
+
+		DDMTemplate template =
+			ddmTemplatePersistence.create(templateId);
+
+		template.setUuid(serviceContext.getUuid());
+		template.setGroupId(serviceContext.getScopeGroupId());
+		template.setCompanyId(user.getCompanyId());
+		template.setUserId(user.getUserId());
+		template.setUserName(user.getFullName());
+		template.setCreateDate(serviceContext.getCreateDate(now));
+		template.setModifiedDate(serviceContext.getModifiedDate(now));
+		template.setStructureId(structureId);
+		template.setName(name);
+		template.setDescription(description);
+		template.setType(type);
+		template.setLanguage(language);
+		template.setScript(script);
+
+		ddmTemplatePersistence.update(template, false);
+
+		// Resources
+
+		if (serviceContext.getAddCommunityPermissions() ||
+			serviceContext.getAddGuestPermissions()) {
+
+			addTemplateResources(
+				template, serviceContext.getAddCommunityPermissions(),
+				serviceContext.getAddGuestPermissions());
+		}
+		else {
+			addTemplateResources(
+				template, serviceContext.getCommunityPermissions(),
+				serviceContext.getGuestPermissions());
+		}
+
+		return template;
+	}
+
+	public void addTemplateResources(
+			DDMTemplate template, boolean addCommunityPermissions,
+			boolean addGuestPermissions)
+		throws PortalException, SystemException {
+
+		resourceLocalService.addResources(
+			template.getCompanyId(), template.getGroupId(),
+			template.getUserId(), DDMTemplate.class.getName(),
+			template.getTemplateId(), false, addCommunityPermissions,
+			addGuestPermissions);
+	}
+
+	public void addTemplateResources(
+			DDMTemplate template, String[] communityPermissions,
+			String[] guestPermissions)
+		throws PortalException, SystemException {
+
+		resourceLocalService.addModelResources(
+			template.getCompanyId(), template.getGroupId(),
+			template.getUserId(), DDMTemplate.class.getName(),
+			template.getTemplateId(), communityPermissions,
+			guestPermissions);
+	}
+
+	public void deleteTemplate(DDMTemplate template)
+		throws PortalException, SystemException {
+
+		// View
+
+		ddmTemplatePersistence.remove(template);
+
+		// Resources
+
+		resourceLocalService.deleteResource(
+			template.getCompanyId(), DDMTemplate.class.getName(),
+			ResourceConstants.SCOPE_INDIVIDUAL, template.getTemplateId());
+	}
+
+	public void deleteTemplate(long templateId)
+		throws PortalException, SystemException {
+
+		DDMTemplate template =
+			ddmTemplatePersistence.findByPrimaryKey(templateId);
+
+		deleteTemplate(template);
+	}
+
+	public List<DDMTemplate> search(
+			long companyId, long groupId, String keywords, int start, int end,
+			OrderByComparator orderByComparator)
+		throws SystemException {
+
+		return ddmTemplateFinder.findByKeywords(
+			companyId, groupId, keywords, start, end, orderByComparator);
+	}
+
+	public List<DDMTemplate> search(
+			long companyId, long groupId, String name, String description,
+			String type, String language, boolean andOperator, int start,
+			int end, OrderByComparator orderByComparator)
+		throws SystemException {
+
+		return ddmTemplateFinder.findByC_G_N_D_T_L(
+			companyId, groupId, name, description, type, language, andOperator,
+			start, end, orderByComparator);
+	}
+
+	public int searchCount(long companyId, long groupId, String keywords)
+		throws SystemException {
+
+		return ddmTemplateFinder.countByKeywords(companyId, groupId, keywords);
+	}
+
+	public int searchCount(
+			long companyId, long groupId, String name, String description,
+			String type, String language, boolean andOperator)
+		throws SystemException {
+
+		return ddmTemplateFinder.countByC_G_N_D_T_L(
+			companyId, groupId, name, description, type, language, andOperator);
+	}
+
+	public DDMTemplate updateTemplate(
+			long templateId, String name, String description, String type,
+			String language, String script, ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		validate(name);
+
+		Date now = new Date();
+
+		DDMTemplate template =
+			ddmTemplateLocalService.getDDMTemplate(templateId);
+
+		template.setModifiedDate(serviceContext.getModifiedDate(now));
+		template.setName(name);
+		template.setDescription(description);
+		template.setType(type);
+		template.setLanguage(language);
+		template.setScript(script);
+
+		ddmTemplatePersistence.update(template, false);
+
+		return template;
+	}
+
+	protected void validate(String name)
+		throws PortalException {
+
+		if (Validator.isNull(name) || Validator.isNumber(name) ||
+			name.contains(StringPool.SPACE)) {
+
+			throw new TemplateNameException();
+		}
+	}
+
 }
--- portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/impl/DDMTemplateServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/impl/DDMTemplateServiceImpl.java
@@ -14,10 +14,55 @@
 
 package com.liferay.portlet.dynamicdatamapping.service.impl;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portlet.dynamicdatamapping.model.DDMTemplate;
 import com.liferay.portlet.dynamicdatamapping.service.base.DDMTemplateServiceBaseImpl;
+import com.liferay.portlet.dynamicdatamapping.service.permission.DDMPermission;
+import com.liferay.portlet.dynamicdatamapping.service.permission.DDMTemplatePermission;
 
 /**
  * @author Brian Wing Shun Chan
+ * @author Eduardo Lundgren
  */
 public class DDMTemplateServiceImpl extends DDMTemplateServiceBaseImpl {
+
+	public DDMTemplate addTemplate(
+			long structureId, String name, String description, String type,
+			String language, String script, ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		DDMPermission.check(
+			getPermissionChecker(), serviceContext.getScopeGroupId(),
+			ActionKeys.ADD_TEMPLATE);
+
+		return ddmTemplateLocalService.addTemplate(
+			structureId, name, description, type, language, script,
+			serviceContext);
+	}
+
+	public void deleteTemplate(long templateId)
+		throws PortalException, SystemException {
+
+		DDMTemplatePermission.check(
+			getPermissionChecker(), templateId, ActionKeys.DELETE);
+
+		ddmTemplateLocalService.deleteTemplate(templateId);
+	}
+
+	public DDMTemplate updateTemplate(
+			long templateId, String name, String description, String type,
+			String language, String script, ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		DDMTemplatePermission.check(
+			getPermissionChecker(), templateId, ActionKeys.UPDATE);
+
+		return ddmTemplateLocalService.updateTemplate(
+			templateId, name, description, type, language, script,
+			serviceContext);
+	}
+
 }
