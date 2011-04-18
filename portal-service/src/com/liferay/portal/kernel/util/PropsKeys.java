f792cdf8afee90f1e705031b02e96bfe49a99e19
Brian Chan <brian.chan@liferay.com>
2011-04-18 04:26:02 +0000

    Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@78364 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -310,7 +310,7 @@ public interface PropsKeys {
 	public static final String CDN_HOST_HTTPS = "cdn.host.https";
 
 	public static final String CLUSTER_EXECUTOR_HEARTBEAT_INTERVAL = "cluster.executor.heartbeat.interval";
-	
+
 	public static final String CLUSTER_EXECUTOR_DEBUG_ENABLED = "cluster.executor.debug.enabled";
 
 	public static final String CLUSTER_LINK_AUTODETECT_ADDRESS = "cluster.link.autodetect.address";
