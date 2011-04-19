915a6bc49eea34dd11a709914690f63f14de9824
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2011-04-19 18:33:11 +0000

    LPS-16573 - Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@78556 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-web/docroot/html/js/editor/ckeditor_diffs/plugins/bbcode/plugin.js
+++ portal-web/docroot/html/js/editor/ckeditor_diffs/plugins/bbcode/plugin.js
@@ -21,35 +21,42 @@
 					}
 				);
 
-				var stylePre = new CKEDITOR.style(
+				var preElement = new CKEDITOR.style(
 					{
 						element: 'pre'
 					}
 				);
 
-				stylePre._.enterMode = editor.config.enterMode;
+				preElement._.enterMode = editor.config.enterMode;
 
-				editor.ui.addButton( 'Code',
-				{
-					label : 'Code',
-					icon: CKEDITOR.config.imagesPath + 'code.png',
-					click : function() {
-						editor.focus();
-						editor.fire( 'saveSnapshot' );
+				editor.ui.addButton(
+					'Code',
+					{
+						label : 'Code',
+						icon: CKEDITOR.config.imagesPath + 'code.png',
+						click : function() {
+							editor.focus();
+							editor.fire('saveSnapshot');
 
-						var elementPath = new CKEDITOR.dom.elementPath( editor.getSelection().getStartElement() );
+							var elementPath = new CKEDITOR.dom.elementPath(editor.getSelection().getStartElement());
 
-						stylePre[ stylePre.checkActive( elementPath ) ? 'remove' : 'apply' ]( editor.document );
+							var elementAction = 'apply';
 
-						setTimeout(
-							function()
-							{
-								editor.fire( 'saveSnapshot' );
-							},
-							0
-						);
+							if (preElement.checkActive(elementPath)) {
+								elementAction = 'remove';
+							}
+
+							preElement[elementAction](editor.document);
+
+							setTimeout(
+								function() {
+									editor.fire('saveSnapshot');
+								},
+								0
+							);
+						}
 					}
-				});
+				);
 			}
 		}
 	);
