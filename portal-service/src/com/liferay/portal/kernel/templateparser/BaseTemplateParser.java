fb6eb96f373f0eea28a273b704a0055832894bb0
Marcellus Tavares <marcellus.tavares@liferay.com>
2011-04-21 16:40:33 +0000

    LPS-16605 Recheckin
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@78765 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-service/src/com/liferay/portal/kernel/templateparser/BaseTemplateParser.java
+++ portal-service/src/com/liferay/portal/kernel/templateparser/BaseTemplateParser.java
@@ -14,12 +14,29 @@
 
 package com.liferay.portal.kernel.templateparser;
 
+import com.liferay.portal.kernel.io.unsync.UnsyncStringWriter;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.xml.Document;
+import com.liferay.portal.kernel.xml.DocumentException;
+import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portal.kernel.xml.SAXReaderUtil;
+import com.liferay.portal.model.Company;
+import com.liferay.portal.security.permission.PermissionThreadLocal;
+import com.liferay.portal.service.CompanyLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
 
+import java.io.IOException;
+import java.io.Writer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * @author Brian Wing Shun Chan
+ * @author Marcellus Tavares
  */
 public abstract class BaseTemplateParser implements TemplateParser {
 
@@ -40,12 +57,160 @@ public abstract class BaseTemplateParser implements TemplateParser {
 		}
 	}
 
+	protected boolean doMergeTemplate(
+			String templateId, String script, TemplateContext context,
+			Writer writer, String errorTemplateId, String errorTemplateContent)
+		throws Exception {
+
+		return false;
+	}
+
 	protected String doTransform(
 			ThemeDisplay themeDisplay, Map<String, String> tokens,
 			String viewMode, String languageId, String xml, String script)
 		throws Exception {
 
+		UnsyncStringWriter unsyncStringWriter = new UnsyncStringWriter();
+
+		boolean load = false;
+
+		try	{
+			TemplateContext context = getTemplateContext();
+
+			Document doc = SAXReaderUtil.read(xml);
+
+			Element root = doc.getRootElement();
+
+			List<TemplateNode> nodes = getTemplateNodes(themeDisplay, root);
+
+			if (nodes != null) {
+				for (TemplateNode node : nodes) {
+					context.put(node.getName(), node);
+				}
+			}
+
+			context.put(
+				"xmlRequest", root.element("request").asXML());
+			context.put(
+				"request", insertRequestVariables(root.element("request")));
+
+			long companyId = GetterUtil.getLong(tokens.get("company_id"));
+			Company company = CompanyLocalServiceUtil.getCompanyById(companyId);
+			long groupId = GetterUtil.getLong(tokens.get("group_id"));
+			String templateId = tokens.get("template_id");
+
+			context.put("company", company);
+			context.put("companyId", String.valueOf(companyId));
+			context.put("groupId", String.valueOf(groupId));
+			context.put("viewMode", viewMode);
+			context.put("locale", LocaleUtil.fromLanguageId(languageId));
+			context.put(
+				"permissionChecker",
+				PermissionThreadLocal.getPermissionChecker());
+
+			populateCustomContext(context);
+
+			String engineTemplateId = companyId + groupId + templateId;
+
+			long companyGroupId = GetterUtil.getLong(
+				tokens.get("company_group_id"));
+
+			if (companyGroupId > 0) {
+				engineTemplateId = companyId + companyGroupId + templateId;
+			}
+
+			String errorTemplateId = getErrorTemplateId();
+			String errorTemplateContent = getErrorTemplateContent();
+
+			load = doMergeTemplate(
+				engineTemplateId, script, context, unsyncStringWriter,
+				errorTemplateId, errorTemplateContent);
+		}
+		catch (Exception e) {
+			if (e instanceof DocumentException) {
+				throw new TransformException("Unable to read XML document", e);
+			}
+			else if (e instanceof IOException) {
+				throw new TransformException("Error reading template", e);
+			}
+			else if (e instanceof TransformException) {
+				throw (TransformException)e;
+			}
+			else {
+				throw new TransformException("Unhandled exception", e);
+			}
+		}
+
+		if (!load) {
+			throw new TransformException(
+				"Unable to dynamically load transform script");
+		}
+
+		return unsyncStringWriter.toString();
+	}
+
+	protected String getErrorTemplateContent() {
 		return null;
+	}
+
+	protected String getErrorTemplateId() {
+		return null;
+	}
+
+	protected TemplateContext getTemplateContext() {
+		return null;
+	}
+
+	protected List<TemplateNode> getTemplateNodes(
+			ThemeDisplay themeDisplay, Element root)
+		throws TransformException {
+
+		return null;
+	}
+
+	protected Map<String, Object> insertRequestVariables(Element parent) {
+		Map<String, Object> map = new HashMap<String, Object>();
+
+		if (parent == null) {
+			return map;
+		}
+
+		for (Element el : parent.elements()) {
+			String name = el.getName();
+
+			if (name.equals("attribute")) {
+				map.put(el.elementText("name"), el.elementText("value"));
+			}
+			else if (name.equals("parameter")) {
+				name = el.element("name").getText();
+
+				List<Element> valueEls = el.elements("value");
+
+				if (valueEls.size() == 1) {
+					map.put(name, (valueEls.get(0)).getText());
+				}
+				else {
+					List<String> values = new ArrayList<String>();
+
+					for (Element valueEl : valueEls) {
+						values.add(valueEl.getText());
+					}
+
+					map.put(name, values);
+				}
+			}
+			else if (el.elements().size() > 0) {
+				map.put(name, insertRequestVariables(el));
+			}
+			else {
+				map.put(name, el.getText());
+			}
+		}
+
+		return map;
+	}
+
+	protected void populateCustomContext(TemplateContext templateContext) {
 	}
 
 }
