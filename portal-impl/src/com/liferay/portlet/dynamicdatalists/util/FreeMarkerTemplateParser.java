650084b2ba9005abc0674c92d3cf71a93eafdade
Marcellus Tavares <marcellus.tavares@liferay.com>
2011-04-27 16:48:34 +0000

    LPS-16762 Template Parsers
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@79269 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-impl/src/com/liferay/portlet/dynamicdatalists/util/FreeMarkerTemplateParser.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.dynamicdatalists.util;
+
+import com.liferay.portal.kernel.templateparser.TemplateContext;
+import com.liferay.portal.kernel.templateparser.TemplateNode;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portal.util.PropsValues;
+import com.liferay.util.ContentUtil;
+import com.liferay.util.PwdGenerator;
+
+import java.util.List;
+
+/**
+ * @author Marcellus Tavares
+ */
+public class FreeMarkerTemplateParser extends
+	com.liferay.portlet.journal.util.FreeMarkerTemplateParser {
+
+	protected String getErrorTemplateContent() {
+		return ContentUtil.get(
+			PropsValues.DYNAMIC_DATA_LISTS_ERROR_TEMPLATE_FREEMARKER);
+	}
+
+	protected String getErrorTemplateId() {
+		return PropsValues.DYNAMIC_DATA_LISTS_ERROR_TEMPLATE_FREEMARKER;
+	}
+
+	protected List<TemplateNode> getTemplateNodes(Element element)
+		throws Exception {
+
+		return super.getTemplateNodes(element);
+	}
+
+	protected void populateTemplateContext(TemplateContext templateContext)
+		throws Exception {
+
+		super.populateTemplateContext(templateContext);
+
+		String randomNamespace =
+			PwdGenerator.getPassword(PwdGenerator.KEY3, 4) +
+				StringPool.UNDERLINE;
+
+		templateContext.put("randomNamespace", randomNamespace);
+	}
+
+}
