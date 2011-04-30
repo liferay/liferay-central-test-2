b463c7a249a59320817df0f80f7fefa1a6098d22
Michael Han <michael.han@liferay.com>
2011-04-30 01:00:43 +0000

    LPS-16893
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@79634 05bdf26c-840f-0410-9ced-eb539d925f36

--- util-java/src/com/liferay/util/mail/MailEngine.java
+++ util-java/src/com/liferay/util/mail/MailEngine.java
@@ -25,6 +25,7 @@ import com.liferay.portal.kernel.mail.MailMessage;
 import com.liferay.portal.kernel.mail.SMTPAccount;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.InfrastructureUtil;
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.io.File;
@@ -224,6 +225,10 @@ public class MailEngine {
 
 			if (bcc != null) {
 				message.setRecipients(Message.RecipientType.BCC, bcc);
+			}
+
+			if (Validator.isNull(subject)) {
+				subject = StringPool.BLANK;
 			}
 
 			message.setSubject(subject);
