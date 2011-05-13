4d89667ff8a803db709a99416f4ae4b60f4e141e
Hugo Huijser <hugo.huijser@liferay.com>
2011-05-13 00:25:59 +0000

    LPS-17201 forgot to update primKey in the upgrade
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@80495 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/upgrade/v6_1_0/UpgradeResourcePermission.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_1_0/UpgradeResourcePermission.java
@@ -16,6 +16,7 @@ package com.liferay.portal.upgrade.v6_1_0;
 
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.model.GroupConstants;
 import com.liferay.portal.model.ResourceConstants;
 import com.liferay.portal.model.RoleConstants;
 
@@ -25,13 +26,15 @@ import com.liferay.portal.model.RoleConstants;
 public class UpgradeResourcePermission extends UpgradeProcess {
 
 	protected void doUpgrade() throws Exception {
-		StringBundler sb = new StringBundler(6);
+		StringBundler sb = new StringBundler(8);
 
 		sb.append("update ResourcePermission inner join Role_ on ");
 		sb.append("ResourcePermission.roleId = Role_.roleId set ");
 		sb.append("ResourcePermission.scope = ");
 		sb.append(ResourceConstants.SCOPE_GROUP_TEMPLATE);
-		sb.append(" where Role_.type_ = ");
+		sb.append(", ResourcePermission.primKey = '");
+		sb.append(String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID));
+		sb.append("' where Role_.type_ = ");
 		sb.append(RoleConstants.TYPE_PROVIDER);
 
 		runSQL(sb.toString());
