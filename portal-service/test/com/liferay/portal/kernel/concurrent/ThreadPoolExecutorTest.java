aa973dd170f8fcb1e316008109d04236bc99b41a
Shuyang Zhou <shuyang.zhou@liferay.com>
2011-05-19 02:08:22 +0000

    LPS-14986 A possible fix for unit test timing issue
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@81060 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-service/test/com/liferay/portal/kernel/concurrent/ThreadPoolExecutorTest.java
+++ portal-service/test/com/liferay/portal/kernel/concurrent/ThreadPoolExecutorTest.java
@@ -438,15 +438,16 @@ public class ThreadPoolExecutorTest extends TestCase {
 		ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
 			1, 2, TestUtil.KEEPALIVE_TIME, TimeUnit.MILLISECONDS, true, 3);
 
-		long startTime = System.currentTimeMillis();
+		long startTime = System.nanoTime();
 
 		assertFalse(
 			threadPoolExecutor.awaitTermination(
 				TestUtil.KEEPALIVE_TIME, TimeUnit.MILLISECONDS));
 
-		long waitTime = System.currentTimeMillis() - startTime;
+		long waitTime = System.nanoTime() - startTime;
 
-		assertTrue(waitTime >= TestUtil.KEEPALIVE_TIME);
+		assertTrue(waitTime >= TimeUnit.MILLISECONDS.toNanos(
+			TestUtil.KEEPALIVE_TIME));
 	}
 
 	public void testAwaitTermination2() throws InterruptedException {
