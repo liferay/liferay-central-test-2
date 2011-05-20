e2247aef87642411466b37c29fee6f9e86a8d2f2
Miguel Pastor <miguel.pastor@liferay.com>
2011-05-20 08:17:15 +0000

    LPS-17436: Creole Parser
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@81193 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-impl/src/com/liferay/parsers/creole/ast/TextNode.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.parsers.creole.ast;
+
+/**
+ * @author Miguel Pastor
+ */
+public abstract class TextNode extends BaseParentableNode {
+
+	public TextNode(int tokenType) {
+		super(tokenType);
+	}
+
+	public TextNode(String content) {
+		_content = content;
+	}
+
+	public TextNode(ASTNode node) {
+		super((CollectionNode) node);
+	}
+
+	public boolean hasContent() {
+		return _content != null;
+	}
+
+	public String getContent() {
+		return _content;
+	}
+
+	private String _content;
+
+}
