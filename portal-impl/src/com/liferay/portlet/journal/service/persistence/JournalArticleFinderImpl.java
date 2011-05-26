07ffc0b8f5df6dfa73d75df9a778eb109b62ab33
Brian Chan <brian.chan@liferay.com>
2011-05-26 06:07:11 +0000

    LPS-17565 Recheckin
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@81742 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/journal/service/persistence/JournalArticleFinderImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/persistence/JournalArticleFinderImpl.java
@@ -686,21 +686,8 @@ public class JournalArticleFinderImpl
 			}
 
 			sql = CustomSQLUtil.replaceAndOperator(sql, andOperator);
-
-			if ((articleIds != null) &&
-				((articleIds.length > 1) ||
-				 ((articleIds.length == 1) && (articleIds[0] != null)))) {
-
-				sql = StringUtil.replace(
-					sql, "MAX(version) AS tempVersion",
-					"version AS tempVersion");
-				sql = StringUtil.replace(sql, "[$GROUP_BY_CLAUSE$]", "");
-			}
-			else {
-				sql = StringUtil.replace(
-					sql, "[$GROUP_BY_CLAUSE$]", "GROUP BY groupId, articleId");
-			}
-
+			sql = StringUtil.replace(
+				sql, "[$GROUP_BY_CLAUSE$]", "GROUP BY groupId, articleId");
 			sql = CustomSQLUtil.replaceOrderBy(sql, orderByComparator);
 
 			if (inlineSQLHelper) {
