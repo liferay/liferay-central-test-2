031faed5fcff8685a6d70b7aafec27ee5aed69b3
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2011-06-06 14:52:06 +0000

    LPS-17905 Selecting all files checks the select all checkbox
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@82725 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/documentlibrary/search/EntriesChecker.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/search/EntriesChecker.java
@@ -16,6 +16,7 @@ package com.liferay.portlet.documentlibrary.search;
 
 import com.liferay.portal.kernel.dao.search.RowChecker;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.util.Validator;
 
 /**
  * @author Sergio González
@@ -30,6 +31,37 @@ public class EntriesChecker extends RowChecker {
 
 	public String getAllRowsCheckBox() {
 		return null;
+	}
+
+	public String getRowCheckBox(boolean checked, String primaryKey) {
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("<input ");
+
+		if (checked) {
+			sb.append("checked ");
+		}
+
+		sb.append("name=\"");
+		sb.append(getRowIds());
+		sb.append("\" type=\"checkbox\" value=\"");
+		sb.append(primaryKey);
+		sb.append("\" ");
+
+		if (Validator.isNotNull(getAllRowIds())) {
+			sb.append("onClick=\"Liferay.Util.checkAllBox(");
+			sb.append("AUI().one(this).ancestor('");
+			sb.append("table.taglib-search-iterator'), '");
+			sb.append(getRowIds());
+			sb.append("', ");
+			sb.append(getAllRowIds());
+			sb.append("Checkbox); ");
+		}
+
+		sb.append(">");
+
+		return sb.toString();
 	}
 
 }
