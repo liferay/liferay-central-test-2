b7fc002854bf5344705598a0845846134a1413b8
Connor Mckay <connor.mckay@liferay.com>
2011-06-09 23:05:09 +0000

    Javadoc formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@83192 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -3043,9 +3043,9 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 	 *         more information see {@link
 	 *         com.liferay.portal.service.persistence.UserFinder}.
 	 * @param  andSearch whether every field must match its keywords, or just
-	 *         one. For example, &quot;users with the first name 'bob' and last
-	 *         name 'smith'&quot; vs &quot;users with the first name 'bob' or
-	 *         the last name 'smith'&quot;.
+	 *         one field. For example, &quot;users with the first name 'bob' and
+	 *         last name 'smith'&quot; vs &quot;users with the first name 'bob'
+	 *         or the last name 'smith'&quot;.
 	 * @param  start the lower bound of the range of users
 	 * @param  end the upper bound of the range of users (not inclusive)
 	 * @param  obc the comparator to order the users by (optionally
@@ -3094,9 +3094,9 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 	 *         more information see {@link
 	 *         com.liferay.portlet.usersadmin.util.UserIndexer}.
 	 * @param  andSearch whether every field must match its keywords, or just
-	 *         one. For example, &quot;users with the first name 'bob' and last
-	 *         name 'smith'&quot; vs &quot;users with the first name 'bob' or
-	 *         the last name 'smith'&quot;.
+	 *         one field. For example, &quot;users with the first name 'bob' and
+	 *         last name 'smith'&quot; vs &quot;users with the first name 'bob'
+	 *         or the last name 'smith'&quot;.
 	 * @param  start the lower bound of the range of users
 	 * @param  end the upper bound of the range of users (not inclusive)
 	 * @param  sort the field and direction to sort by (optionally
@@ -3156,9 +3156,9 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 	 *         more information see {@link
 	 *         com.liferay.portal.service.persistence.UserFinder}.
 	 * @param  andSearch whether every field must match its keywords, or just
-	 *         one. For example, &quot;users with the first name 'bob' and last
-	 *         name 'smith'&quot; vs &quot;users with the first name 'bob' or
-	 *         the last name 'smith'&quot;.
+	 *         one field. For example, &quot;users with the first name 'bob' and
+	 *         last name 'smith'&quot; vs &quot;users with the first name 'bob'
+	 *         or the last name 'smith'&quot;.
 	 * @return the number of matching users
 	 * @throws SystemException if a system exception occurred
 	 */
