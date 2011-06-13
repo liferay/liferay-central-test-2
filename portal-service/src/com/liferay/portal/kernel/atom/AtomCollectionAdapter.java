89ddc9754f5f666dfae56341dbd9dcd7154f95de
Brian Chan <brian.chan@liferay.com>
2011-06-13 22:38:41 +0000

    LPS-18036 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@83412 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-service/src/com/liferay/portal/kernel/atom/AtomCollectionAdapter.java
+++ portal-service/src/com/liferay/portal/kernel/atom/AtomCollectionAdapter.java
@@ -23,14 +23,23 @@ import java.util.List;
 public interface AtomCollectionAdapter<E> {
 
 	public static final int SC_BAD_CONTENT = 422;
+
 	public static final int SC_BAD_REQUEST = 400;
+
 	public static final int SC_CONFLICT = 409;
+
 	public static final int SC_CREATED = 201;
+
 	public static final int SC_FORBIDDEN = 403;
+
 	public static final int SC_INTERNAL_SERVER_ERROR = 500;
+
 	public static final int SC_NOT_FOUND = 404;
+
 	public static final int SC_NOT_MODIFIED = 304;
+
 	public static final int SC_OK = 200;
+
 	public static final int SC_UNAUTHORIZED = 401;
 
 	public void deleteEntry(String resourceName) throws AtomException;
