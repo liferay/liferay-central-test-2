06c4bc354d153ead731e248bc764f593f3cbc05b
Brian Chan <brian.chan@liferay.com>
2011-06-14 23:08:33 +0000

    LPS-18024 Source formatting
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@83553 05bdf26c-840f-0410-9ced-eb539d925f36

--- portal-impl/src/com/liferay/portlet/passwordpoliciesadmin/action/EditPasswordPolicyAssignmentsAction.java
+++ portal-impl/src/com/liferay/portlet/passwordpoliciesadmin/action/EditPasswordPolicyAssignmentsAction.java
@@ -68,7 +68,8 @@ public class EditPasswordPolicyAssignmentsAction extends PortletAction {
 
 				SessionErrors.add(actionRequest, e.getClass().getName());
 
-				setForward(actionRequest, "portlet.password_policies_admin.error");
+				setForward(
+					actionRequest, "portlet.password_policies_admin.error");
 			}
 			else {
 				throw e;
@@ -90,7 +91,8 @@ public class EditPasswordPolicyAssignmentsAction extends PortletAction {
 
 				SessionErrors.add(renderRequest, e.getClass().getName());
 
-				return mapping.findForward("portlet.password_policies_admin.error");
+				return mapping.findForward(
+					"portlet.password_policies_admin.error");
 			}
 			else {
 				throw e;
