e2928fb2c9f0d66adab8f86588145c407aa396ea
Brian Chan <brian.chan@liferay.com>
2011-06-15 21:09:42 +0000

    LPS-15969 We're going to deprecate the package com.liferay.documentlibrary in favor of com.liferay.portlet.documentlibrary -> so migrate from Content to DLContent
    git-svn-id: svn://svn.liferay.com/repos/public/portal/trunk@83628 05bdf26c-840f-0410-9ced-eb539d925f36

---
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLContentLocalServiceImpl.java
@@ -0,0 +1,137 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.documentlibrary.service.impl;
+
+import com.liferay.portal.kernel.dao.jdbc.OutputBlob;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
+import com.liferay.portlet.documentlibrary.NoSuchContentException;
+import com.liferay.portlet.documentlibrary.model.DLContent;
+import com.liferay.portlet.documentlibrary.service.base.DLContentLocalServiceBaseImpl;
+
+import java.io.InputStream;
+
+import java.util.List;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class DLContentLocalServiceImpl extends DLContentLocalServiceBaseImpl {
+
+	public DLContent addContent(
+			long companyId, String portletId, long groupId, long repositoryId,
+			String path, String version, byte[] bytes)
+		throws SystemException {
+
+		long contentId = counterLocalService.increment();
+
+		DLContent dlContent = dlContentPersistence.create(contentId);
+
+		dlContent.setGroupId(groupId);
+		dlContent.setCompanyId(companyId);
+		dlContent.setRepositoryId(repositoryId);
+		dlContent.setPath(path);
+		dlContent.setVersion(version);
+
+		OutputBlob dataOutputBlob = new OutputBlob(
+			new UnsyncByteArrayInputStream(bytes), bytes.length);
+
+		dlContent.setData(dataOutputBlob);
+
+		dlContent.setSize(bytes.length);
+
+		dlContentPersistence.update(dlContent, false);
+
+		return dlContent;
+	}
+
+	public DLContent addContent(
+			long companyId, String portletId, long groupId, long repositoryId,
+			String path, String version, InputStream inputStream, long size)
+		throws SystemException {
+
+		long contentId = counterLocalService.increment();
+
+		DLContent dlContent = dlContentPersistence.create(contentId);
+
+		dlContent.setGroupId(groupId);
+		dlContent.setCompanyId(companyId);
+		dlContent.setRepositoryId(repositoryId);
+		dlContent.setPath(path);
+		dlContent.setVersion(version);
+
+		OutputBlob dataOutputBlob = new OutputBlob(inputStream, size);
+
+		dlContent.setData(dataOutputBlob);
+
+		dlContent.setSize(size);
+
+		dlContentPersistence.update(dlContent, false);
+
+		return dlContent;
+	}
+
+	public void deleteContent(
+			long companyId, String portletId, long repositoryId, String path,
+			String version)
+		throws PortalException, SystemException {
+
+		dlContentPersistence.removeByC_P_R_P_V(
+			companyId, portletId, repositoryId, path, version);
+	}
+
+	public void deleteContents(
+			long companyId, String portletId, long repositoryId, String path)
+		throws SystemException {
+
+		dlContentPersistence.removeByC_P_R_P(
+			companyId, portletId, repositoryId, path);
+	}
+
+	public DLContent getContent(
+			long companyId, String portletId, long repositoryId, String path,
+			String version)
+		throws NoSuchContentException, SystemException {
+
+		return dlContentPersistence.findByC_P_R_P_V(
+			companyId, portletId, repositoryId, path, version);
+	}
+
+	public List<DLContent> getContents(
+			long companyId, String portletId, long repositoryId, String path)
+		throws SystemException {
+
+		return dlContentPersistence.findByC_P_R_P(
+			companyId, portletId, repositoryId, path);
+	}
+
+	public boolean hasContent(
+			long companyId, String portletId, long repositoryId, String path,
+			String version)
+		throws SystemException {
+
+		int count = dlContentPersistence.countByC_P_R_P_V(
+			companyId, portletId, repositoryId, path, version);
+
+		if (count > 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+}
--- portal-impl/test/com/liferay/portal/service/persistence/BasePersistenceTestCase.java
+++ portal-impl/test/com/liferay/portal/service/persistence/BasePersistenceTestCase.java
@@ -14,8 +14,12 @@
 
 package com.liferay.portal.service.persistence;
 
+import com.liferay.portal.kernel.dao.jdbc.OutputBlob;
+import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
 import com.liferay.portal.util.BaseTestCase;
 import com.liferay.portal.util.PropsValues;
+
+import java.sql.Blob;
 
 /**
  * @author Brian Wing Shun Chan
@@ -32,6 +36,15 @@ public class BasePersistenceTestCase extends BaseTestCase {
 		super.tearDown();
 
 		PropsValues.SPRING_HIBERNATE_SESSION_DELEGATED = true;
+	}
+
+	protected Blob randomBlob() throws Exception {
+		String randomString = randomString();
+
+		byte[] randomBytes = randomString.getBytes();
+
+		return new OutputBlob(
+			new UnsyncByteArrayInputStream(randomBytes), randomBytes.length);
 	}
 
 }
