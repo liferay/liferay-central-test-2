da9c95478366fd33decadfe43c4ca6c0d4e75ad4
Albert Lee <albert.lee@liferay.com>
2011-07-13 07:34:08 +0800

    test-fixes-071211 plugins/mail

--- portal-web/test/com/liferay/portalweb/plugins/mail/portlet/addportlet/AddPortletMailTest.java
+++ portal-web/test/com/liferay/portalweb/plugins/mail/portlet/addportlet/AddPortletMailTest.java
@@ -44,8 +44,12 @@ public class AddPortletMailTest extends BaseTestCase {
 		selenium.click(RuntimeVariables.replace("link=Mail Test Page"));
 		selenium.waitForPageToLoad("30000");
 		selenium.saveScreenShotAndSource();
-		selenium.clickAt("_145_addApplication", RuntimeVariables.replace(""));
-		Thread.sleep(5000);
+		assertTrue(selenium.isPartialText("//a[@id='_145_addApplication']",
+				"More"));
+		selenium.clickAt("//a[@id='_145_addApplication']",
+			RuntimeVariables.replace("More"));
+		assertTrue(selenium.isPartialText("//a[@id='_145_addApplication']",
+				"More"));
 
 		for (int second = 0;; second++) {
 			if (second >= 60) {
@@ -53,28 +57,7 @@ public class AddPortletMailTest extends BaseTestCase {
 			}
 
 			try {
-				if (selenium.isVisible("layout_configuration_content")) {
-					break;
-				}
-			}
-			catch (Exception e) {
-			}
-
-			Thread.sleep(1000);
-		}
-
-		selenium.saveScreenShotAndSource();
-		selenium.typeKeys("layout_configuration_content",
-			RuntimeVariables.replace("m"));
-		selenium.saveScreenShotAndSource();
-
-		for (int second = 0;; second++) {
-			if (second >= 60) {
-				fail("timeout");
-			}
-
-			try {
-				if (selenium.isVisible("//div[@title='Mail']/p/a")) {
+				if (selenium.isElementPresent("//div[@title='Mail']/p/a")) {
 					break;
 				}
 			}
@@ -86,7 +69,7 @@ public class AddPortletMailTest extends BaseTestCase {
 
 		selenium.saveScreenShotAndSource();
 		selenium.clickAt("//div[@title='Mail']/p/a",
-			RuntimeVariables.replace(""));
+			RuntimeVariables.replace("Add"));
 
 		for (int second = 0;; second++) {
 			if (second >= 60) {
