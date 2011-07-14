aef60d520672627269d725f1c2cd1acfaf9e891f
Raymond Auge <raymond.auge@liferay.com>
2011-07-13 22:27:17 -0400

    LPS-18798 Portlet Scoping Lost On Staging

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -125,6 +125,7 @@ import com.liferay.portal.service.permission.LayoutSetPrototypePermissionUtil;
 import com.liferay.portal.service.permission.OrganizationPermissionUtil;
 import com.liferay.portal.service.permission.PortletPermissionUtil;
 import com.liferay.portal.service.permission.UserPermissionUtil;
+import com.liferay.portal.service.persistence.LayoutUtil;
 import com.liferay.portal.servlet.ImageServlet;
 import com.liferay.portal.servlet.filters.i18n.I18nFilter;
 import com.liferay.portal.servlet.filters.secure.NonceUtil;
@@ -3147,7 +3148,18 @@ public class PortalImpl implements Portal {
 				if (liveGroup.isStaged() &&
 					!liveGroup.isStagedPortlet(portletId)) {
 
-					scopeGroupId = liveGroup.getGroupId();
+					Layout liveScopedLayout = LayoutUtil.fetchByUUID_G(
+						layout.getUuid(), liveGroup.getGroupId());
+
+					if ((liveScopedLayout != null) &&
+						liveScopedLayout.hasScopeGroup()) {
+
+						scopeGroupId = getScopeGroupId(
+							liveScopedLayout, portletId);
+					}
+					else {
+						scopeGroupId = liveGroup.getGroupId();
+					}
 				}
 			}
 		}
