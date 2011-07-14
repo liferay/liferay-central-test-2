12372adabb094e70358adbd749256602be24fd1c
Kenneth Chang <kenneth.chang@liferay.com>
2011-07-14 16:34:36 -0700

    LPS-18931 fixed comment time stamp in remote staging

--- portal-impl/src/com/liferay/portal/lar/PortletDataContextImpl.java
+++ portal-impl/src/com/liferay/portal/lar/PortletDataContextImpl.java
@@ -924,12 +924,14 @@ public class PortletDataContextImpl implements PortletDataContext {
 								message.getBody(), serviceContext);
 					}
 					else {
+						serviceContext.setModifiedDate(message.getModifiedDate());
+
 						importedMessage =
 							MBMessageLocalServiceUtil.updateDiscussionMessage(
 								userId, existingMessage.getMessageId(),
 								clazz.getName(), newClassPK,
 								message.getSubject(), message.getBody(),
-								WorkflowConstants.ACTION_PUBLISH);
+								WorkflowConstants.ACTION_PUBLISH, serviceContext);
 					}
 				}
 				else {
--- portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBMessageLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBMessageLocalServiceImpl.java
@@ -1247,6 +1247,31 @@ public class MBMessageLocalServiceImpl extends MBMessageLocalServiceBaseImpl {
 			allowPingbacks, serviceContext);
 	}
 
+	public MBMessage updateDiscussionMessage(
+			long userId, long messageId, String className, long classPK,
+			String subject, String body, int workflowAction, ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		if (Validator.isNull(subject)) {
+			subject = body.substring(0, Math.min(body.length(), 50)) + "...";
+		}
+
+		List<ObjectValuePair<String, byte[]>> files =
+			new ArrayList<ObjectValuePair<String, byte[]>>();
+		List<String> existingFiles = new ArrayList<String>();
+		double priority = 0.0;
+		boolean allowPingbacks = false;
+
+		serviceContext.setAttribute("className", className);
+		serviceContext.setAttribute("classPK", String.valueOf(classPK));
+
+		serviceContext.setWorkflowAction(workflowAction);
+
+		return updateMessage(
+			userId, messageId, subject, body, files, existingFiles, priority,
+			allowPingbacks, serviceContext);
+	}
+
 	public MBMessage updateMessage(
 			long userId, long messageId, String subject, String body,
 			List<ObjectValuePair<String, byte[]>> files,
