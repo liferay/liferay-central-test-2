2146b9ed13b10782515b4ad81c4af3f0bec89016
Minhchau <mdang@mdang-latitude6510.home>
2011-07-20 21:12:18 -0700

    LPS-19009 Fix handling of order by columns that are not included in the outer select

--- portal-impl/src/com/liferay/portal/dao/orm/hibernate/SQLServerLimitStringUtil.java
+++ portal-impl/src/com/liferay/portal/dao/orm/hibernate/SQLServerLimitStringUtil.java
@@ -18,6 +18,7 @@ import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -38,7 +39,8 @@ public class SQLServerLimitStringUtil {
 		int orderByPos = sqlLowerCase.lastIndexOf(" order by ");
 
 		String selectFromWhere = null;
-		String orderBy = null;
+
+		String orderBy = StringPool.BLANK;
 
 		if (orderByPos > 0) {
 			selectFromWhere = sql.substring(fromPos, orderByPos);
@@ -46,18 +48,30 @@ public class SQLServerLimitStringUtil {
 		}
 		else {
 			selectFromWhere = sql.substring(fromPos);
-			orderBy = "CURRENT_TIMESTAMP";
 		}
 
-		String[] orderByColumns = _getOrderByColumns(selectFrom, orderBy);
+		String[] splitOrderBy = _splitOrderBy(selectFrom, orderBy);
 
-		StringBundler sb = new StringBundler(11);
+		String innerOrderBy = splitOrderBy[0];
+		String outerOrderBy = splitOrderBy[1];
 
-		sb.append("select * from (select *, row_number() over (order by ");
-		sb.append(StringUtil.merge(orderByColumns, StringPool.COMMA));
+		String[] splitSelectFrom = _splitSelectFrom(
+			selectFrom, innerOrderBy, limit);
+
+		String innerSelectFrom = splitSelectFrom[0];
+		String outerSelectFrom = splitSelectFrom[1];
+
+		StringBundler sb = new StringBundler(15);
+
+		sb.append(outerSelectFrom);
+		sb.append(" from (");
+		sb.append(outerSelectFrom);
+		sb.append(", row_number() over (");
+		sb.append(outerOrderBy);
 		sb.append(") as _page_row_num from (");
-		sb.append(selectFrom);
+		sb.append(innerSelectFrom);
 		sb.append(selectFromWhere);
+		sb.append(innerOrderBy);
 		sb.append(" ) _temp_table_1 ) _temp_table_2");
 		sb.append(" where _page_row_num between ");
 		sb.append(offset + 1);
@@ -68,30 +82,34 @@ public class SQLServerLimitStringUtil {
 		return sb.toString();
 	}
 
-	private static String[] _getOrderByColumns(
+	private static final String[] _splitOrderBy(
 		String selectFrom, String orderBy) {
+
+		StringBundler innerOrderBy = new StringBundler();
+		StringBundler outerOrderBy = new StringBundler();
 
 		String[] orderByColumns = StringUtil.split(orderBy, StringPool.COMMA);
 
-		for (int i = 0; i < orderByColumns.length; i++) {
-			String orderByColumn = orderByColumns[i].trim();
+		for (String orderByColumn : orderByColumns) {
+			orderByColumn = orderByColumn.trim();
 
-			String orderByColumnName = null;
-			String orderByType = null;
+			String orderByColumnName = orderByColumn;
+			String orderByType = "ASC";
 
-			int columnPos = orderByColumn.indexOf(CharPool.SPACE);
+			int spacePos = orderByColumn.lastIndexOf(CharPool.SPACE);
 
-			if (columnPos == -1) {
-				orderByColumnName = orderByColumn;
-				orderByType = "ASC";
-			}
-			else {
-				orderByColumnName = orderByColumn.substring(0, columnPos);
-				orderByType = orderByColumn.substring(columnPos + 1);
+			if (spacePos != -1) {
+				int parenPos = orderByColumn.indexOf(
+					CharPool.OPEN_PARENTHESIS, spacePos);
+
+				if (parenPos == -1) {
+					orderByColumnName = orderByColumn.substring(0, spacePos);
+					orderByType = orderByColumn.substring(spacePos + 1);
+				}
 			}
 
 			String patternString = "\\Q".concat(orderByColumnName).concat(
-				"\\E as ([^\\s,]+)");
+				"\\E as (\\w+)");
 
 			Pattern pattern = Pattern.compile(
 				patternString, Pattern.CASE_INSENSITIVE);
@@ -102,17 +120,92 @@ public class SQLServerLimitStringUtil {
 				orderByColumnName = matcher.group(1);
 			}
 
-			int pos = orderByColumnName.lastIndexOf(CharPool.PERIOD);
+			if (selectFrom.contains(orderByColumnName)) {
+				if (outerOrderBy.length() == 0) {
+					outerOrderBy.append(" order by ");
+				}
+				else {
+					outerOrderBy.append(StringPool.COMMA);
+				}
 
-			if (pos != -1) {
-				orderByColumnName = orderByColumnName.substring(pos + 1);
+				matcher = _QUALIFIED_COLUMN_PATTERN.matcher(orderByColumnName);
+
+				orderByColumnName = matcher.replaceAll("$1");
+
+				outerOrderBy.append(orderByColumnName);
+				outerOrderBy.append(StringPool.SPACE);
+				outerOrderBy.append(orderByType);
 			}
+			else {
+				if (innerOrderBy.length() == 0) {
+					innerOrderBy.append(" order by ");
+				}
+				else {
+					innerOrderBy.append(StringPool.COMMA);
+				}
 
-			orderByColumns[i] = orderByColumnName.concat(
-				StringPool.SPACE).concat(orderByType);
+				innerOrderBy.append(orderByColumnName);
+				innerOrderBy.append(StringPool.SPACE);
+				innerOrderBy.append(orderByType);
+			}
 		}
 
-		return orderByColumns;
+		if (outerOrderBy.length() == 0) {
+			outerOrderBy.append(" order by CURRENT_TIMESTAMP");
+
+		}
+
+		return new String[] {
+			innerOrderBy.toString(), outerOrderBy.toString()
+		};
 	}
+
+	private static String[] _splitSelectFrom(
+		String selectFrom, String innerOrderBy, int limit) {
+
+		String innerSelectFrom = selectFrom;
+
+		if (Validator.isNotNull(innerOrderBy)) {
+			Matcher matcher = _SELECT_PATTERN.matcher(innerSelectFrom);
+
+			innerSelectFrom = matcher.replaceAll(
+				"select top ".concat(String.valueOf(limit)).concat(
+					StringPool.SPACE));
+		}
+
+		String outerSelectFrom = selectFrom;
+
+		while (outerSelectFrom.charAt(0) == CharPool.OPEN_PARENTHESIS) {
+			outerSelectFrom = outerSelectFrom.substring(1);
+		}
+
+		Matcher matcher = _COLUMN_ALIAS_PATTERN.matcher(outerSelectFrom);
+
+		outerSelectFrom = matcher.replaceAll("$1");
+
+		matcher = _DISTINCT_PATTERN.matcher(outerSelectFrom);
+
+		outerSelectFrom = matcher.replaceAll(StringPool.SPACE);
+
+		matcher = _QUALIFIED_COLUMN_PATTERN.matcher(outerSelectFrom);
+
+		outerSelectFrom = matcher.replaceAll("$1");
+
+		return new String[] {
+			innerSelectFrom, outerSelectFrom
+		};
+	}
+
+	private static final Pattern _COLUMN_ALIAS_PATTERN = Pattern.compile(
+		"[\\w\\.]+ AS (\\w+)", Pattern.CASE_INSENSITIVE);
+
+	private static final Pattern _DISTINCT_PATTERN = Pattern.compile(
+		" DISTINCT ", Pattern.CASE_INSENSITIVE);
+
+	private static final Pattern _QUALIFIED_COLUMN_PATTERN = Pattern.compile(
+		"\\w+\\.(\\w+)");
+
+	private static final Pattern _SELECT_PATTERN = Pattern.compile(
+		"SELECT ", Pattern.CASE_INSENSITIVE);
 
 }
