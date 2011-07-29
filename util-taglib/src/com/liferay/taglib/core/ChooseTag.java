53fdefb6d601b2e87150860cb277a3f4ae898b66
shuyangzhou <shuyang.zhou@liferay.com>
2011-07-29 17:40:04 +0800

    LPS-19382 Swap out the highly used core jstl tags

---
+++ util-taglib/src/com/liferay/taglib/core/ChooseTag.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.taglib.core;
+
+import com.liferay.portal.kernel.servlet.taglib.TagSupport;
+
+/**
+ * @author Shuyang Zhou
+ */
+public class ChooseTag extends TagSupport {
+
+	public boolean canRun() {
+		return !_ran;
+	}
+
+	public int doStartTag() {
+		_ran = false;
+		return EVAL_BODY_INCLUDE;
+	}
+
+	public void markRan() {
+		if (_ran) {
+			throw new IllegalStateException("Other sub tag has already ran.");
+		}
+		_ran = true;
+	}
+
+	public void release() {
+		super.release();
+		_ran = false;
+	}
+
+	private boolean _ran;
+
+}
