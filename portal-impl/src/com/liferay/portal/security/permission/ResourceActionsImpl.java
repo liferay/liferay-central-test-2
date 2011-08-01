e1ef0d2836510cdd99561ae257d34c7aa061630a
brettripley <brett.ripley@liferay.com>
2011-08-01 17:15:53 -0500

    LPS-19158

--- portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
+++ portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
@@ -42,6 +42,7 @@ import com.liferay.portal.model.RoleConstants;
 import com.liferay.portal.model.User;
 import com.liferay.portal.model.UserGroup;
 import com.liferay.portal.service.PortletLocalService;
+import com.liferay.portal.service.GroupServiceUtil;
 import com.liferay.portal.service.ResourceActionLocalService;
 import com.liferay.portal.service.RoleLocalService;
 import com.liferay.portal.util.Portal;
@@ -815,6 +816,16 @@ public class ResourceActionsImpl implements ResourceActions {
 		}
 		else {
 			if (group != null) {
+				if (group.isLayout()) {
+					long parentGroupId = group.getParentGroupId();
+					try {
+						Group parentGroup =
+							GroupServiceUtil.getGroup(parentGroupId);
+						group = parentGroup;
+					}
+					catch (Exception e) {
+					}
+				}
 				if (group.isOrganization()) {
 					types = new int[] {
 						RoleConstants.TYPE_REGULAR,
