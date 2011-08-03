20c45b5c87176e2e96609e281f7c7b78f33b0f6a
Kenneth Chang <kenneth.chang@liferay.com>
2011-08-03 10:59:16 -0700

    LPS-19310 truncate long full name

--- portal-service/src/com/liferay/portal/security/auth/DefaultFullNameGenerator.java
+++ portal-service/src/com/liferay/portal/security/auth/DefaultFullNameGenerator.java
@@ -14,12 +14,15 @@
 
 package com.liferay.portal.security.auth;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.service.impl.UserLocalServiceImpl;
 
 /**
  * @author Michael C. Han
@@ -31,16 +34,54 @@ public class DefaultFullNameGenerator implements FullNameGenerator {
 
 		StringBundler sb = new StringBundler();
 
+		if (Validator.isNull(middleName)) {
+			middleName = "";
+		}
+
+		if (Validator.isNull(lastName)) {
+			lastName = "";
+		}
+
+		int totalLength = firstName.length() + middleName.length() +
+							lastName.length() + 2;
+
+		boolean tooLong = false;
+
+		while (totalLength > 75) {
+			if (middleName.length() > 1) {
+				middleName = middleName.substring(0,1);
+			}
+			else if (firstName.length() > 1) {
+				firstName = firstName.substring(0,1);
+			}
+			else {
+				lastName = lastName.substring(0, 71);
+			}
+
+			totalLength = firstName.length() + middleName.length() +
+							lastName.length() + 2;
+
+			tooLong = true;
+		}
+
 		sb.append(firstName);
 
-		if (Validator.isNotNull(middleName)) {
+		if (!middleName.equals("")) {
 			sb.append(StringPool.SPACE);
 			sb.append(middleName);
 		}
 
-		if (Validator.isNotNull(lastName)) {
+		if (!lastName.equals("")) {
 			sb.append(StringPool.SPACE);
 			sb.append(lastName);
+		}
+
+		if (tooLong) {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					"Full name length is greater than 75 characters. " +
+						"Generating truncated version.");
+			}
 		}
 
 		return sb.toString();
@@ -80,4 +121,5 @@ public class DefaultFullNameGenerator implements FullNameGenerator {
 		return new String[] {firstName, middleName, lastName};
 	}
 
+	private static Log _log = LogFactoryUtil.getLog(DefaultFullNameGenerator.class);
 }
