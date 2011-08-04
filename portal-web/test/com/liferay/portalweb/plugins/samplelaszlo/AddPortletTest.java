345d6626021c659fae0b10b02af4f2dfdc6c1dd4
Albert Lee <albert.lee@liferay.com>
2011-08-05 07:23:59 +0800

    Test Fixes - plugins/samplelaszlo

--- portal-web/test/com/liferay/portalweb/plugins/samplelaszlo/AddPortletTest.java
+++ portal-web/test/com/liferay/portalweb/plugins/samplelaszlo/AddPortletTest.java
@@ -44,8 +44,8 @@ public class AddPortletTest extends BaseTestCase {
 		selenium.click(RuntimeVariables.replace("link=Sample Laszlo Test Page"));
 		selenium.waitForPageToLoad("30000");
 		selenium.saveScreenShotAndSource();
-		selenium.clickAt("_145_addApplication", RuntimeVariables.replace(""));
-		Thread.sleep(5000);
+		selenium.clickAt("//a[@id='_145_addApplication']",
+			RuntimeVariables.replace("More"));
 
 		for (int second = 0;; second++) {
 			if (second >= 60) {
@@ -53,28 +53,8 @@ public class AddPortletTest extends BaseTestCase {
 			}
 
 			try {
-				if (selenium.isVisible("layout_configuration_content")) {
-					break;
-				}
-			}
-			catch (Exception e) {
-			}
-
-			Thread.sleep(1000);
-		}
-
-		selenium.saveScreenShotAndSource();
-		selenium.typeKeys("layout_configuration_content",
-			RuntimeVariables.replace("s"));
-		selenium.saveScreenShotAndSource();
-
-		for (int second = 0;; second++) {
-			if (second >= 60) {
-				fail("timeout");
-			}
-
-			try {
-				if (selenium.isVisible("//div[@title='Sample Laszlo']/p/a")) {
+				if (selenium.isElementPresent(
+							"//div[@title='Sample Laszlo']/p/a")) {
 					break;
 				}
 			}
@@ -86,7 +66,7 @@ public class AddPortletTest extends BaseTestCase {
 
 		selenium.saveScreenShotAndSource();
 		selenium.clickAt("//div[@title='Sample Laszlo']/p/a",
-			RuntimeVariables.replace(""));
+			RuntimeVariables.replace("Add"));
 
 		for (int second = 0;; second++) {
 			if (second >= 60) {
