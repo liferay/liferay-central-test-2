e0da287ad86399ad307c1cf697006fa4b81688fd
Brian Chan <brian.chan@liferay.com>
2011-08-04 14:21:46 -0700

    LPS-19205

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
@@ -1368,6 +1368,10 @@ public class DLFileEntryLocalServiceImpl
 
 		long oldDataRepositoryId = dlFileEntry.getDataRepositoryId();
 
+		validateFile(
+			dlFileEntry.getGroupId(), newFolderId, dlFileEntry.getFileEntryId(),
+			dlFileEntry.getTitle());
+
 		if (DLStoreUtil.hasFile(
 				user.getCompanyId(),
 				DLFolderConstants.getDataRepositoryId(
