5f3047c3b4613516356906a79c52cb14995f34e4
Albert Lee <albert.lee@liferay.com>
2011-08-05 08:03:21 +0800

    Test Fixes - plugins/sampleuisearchcontainertaglib

--- portal-web/test/com/liferay/portalweb/plugins/sampleuisearchcontainertaglib/AddPortletTest.java
+++ portal-web/test/com/liferay/portalweb/plugins/sampleuisearchcontainertaglib/AddPortletTest.java
@@ -43,11 +43,14 @@ public class AddPortletTest extends BaseTestCase {
 
 		selenium.saveScreenShotAndSource();
 		selenium.clickAt("link=Sample UI Search Container Tag Libs Test Page",
-			RuntimeVariables.replace(""));
+			RuntimeVariables.replace(
+				"Sample UI Search Container Tag Libs Test Page"));
 		selenium.waitForPageToLoad("30000");
 		selenium.saveScreenShotAndSource();
-		selenium.clickAt("_145_addApplication", RuntimeVariables.replace(""));
-		Thread.sleep(5000);
+		assertTrue(selenium.isPartialText("//a[@id='_145_addApplication']",
+				"More"));
+		selenium.clickAt("//a[@id='_145_addApplication']",
+			RuntimeVariables.replace("More"));
 
 		for (int second = 0;; second++) {
 			if (second >= 60) {
@@ -55,28 +58,7 @@ public class AddPortletTest extends BaseTestCase {
 			}
 
 			try {
-				if (selenium.isVisible("layout_configuration_content")) {
-					break;
-				}
-			}
-			catch (Exception e) {
-			}
-
-			Thread.sleep(1000);
-		}
-
-		selenium.saveScreenShotAndSource();
-		selenium.typeKeys("layout_configuration_content",
-			RuntimeVariables.replace("s"));
-		selenium.saveScreenShotAndSource();
-
-		for (int second = 0;; second++) {
-			if (second >= 60) {
-				fail("timeout");
-			}
-
-			try {
-				if (selenium.isVisible(
+				if (selenium.isElementPresent(
 							"//div[@title='Sample UI Search Container Taglib']/p/a")) {
 					break;
 				}
@@ -89,7 +71,7 @@ public class AddPortletTest extends BaseTestCase {
 
 		selenium.saveScreenShotAndSource();
 		selenium.clickAt("//div[@title='Sample UI Search Container Taglib']/p/a",
-			RuntimeVariables.replace(""));
+			RuntimeVariables.replace("Add"));
 
 		for (int second = 0;; second++) {
 			if (second >= 60) {
