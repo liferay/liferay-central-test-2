6e2c2d224c59fa2714423e6dc6558a897f58c5d9
Brian Chan <brian.chan@liferay.com>
2011-08-09 14:27:53 -0700

    LPS-18772 HookFactory is already hard coded to only return DLHook, so no need to change in those places

--- portal-impl/src/com/liferay/portal/service/impl/ImageLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/ImageLocalServiceImpl.java
@@ -16,7 +16,7 @@ package com.liferay.portal.service.impl;
 
 import com.liferay.portal.ImageTypeException;
 import com.liferay.portal.NoSuchImageException;
-import com.liferay.portal.image.DLHook;
+import com.liferay.portal.image.HookFactory;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.image.Hook;
@@ -154,7 +154,7 @@ public class ImageLocalServiceImpl extends ImageLocalServiceBaseImpl {
 
 				imagePersistence.remove(imageId);
 
-				Hook hook = new DLHook();
+				Hook hook = HookFactory.getInstance();
 
 				hook.deleteImage(image);
 			}
