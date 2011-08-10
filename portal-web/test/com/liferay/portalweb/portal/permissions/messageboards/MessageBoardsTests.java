34200c7871def253002310b4e1ea667c70d76420
Chas Austin <charles.austin@liferay.com>
2011-08-11 05:14:50 +0800

    Test Fixes

--- portal-web/test/com/liferay/portalweb/portal/permissions/messageboards/MessageBoardsTests.java
+++ portal-web/test/com/liferay/portalweb/portal/permissions/messageboards/MessageBoardsTests.java
@@ -47,7 +47,7 @@ public class MessageBoardsTests extends BaseTests {
 		testSuite.addTestSuite(MA_DeleteMessageTest.class);
 		testSuite.addTestSuite(MA_DeleteCategoryTest.class);
 		testSuite.addTestSuite(MA_AddCategoryTest.class);
-		testSuite.addTestSuite(MA_AddMessageTest.class);
+		testSuite.addTestSuite(MA_AddThreadTest.class);
 		testSuite.addTestSuite(LogoutTest.class);
 		testSuite.addTestSuite(Member_LoginTest.class);
 		testSuite.addTestSuite(Member_AssertActionsTest.class);
