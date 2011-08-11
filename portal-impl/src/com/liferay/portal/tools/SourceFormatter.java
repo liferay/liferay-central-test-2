318114a817dfbcd529196b1c17612067fdbf1a72
Brian Chan <brian.chan@liferay.com>
2011-08-11 15:46:56 -0700

    Fix source formatter

--- portal-impl/src/com/liferay/portal/tools/SourceFormatter.java
+++ portal-impl/src/com/liferay/portal/tools/SourceFormatter.java
@@ -843,10 +843,6 @@ public class SourceFormatter {
 					fileName, "package: " + fileName);
 			}
 
-			if (newContent.indexOf("  {") != -1) {
-				_sourceFormatterHelper.printError(fileName, "{:" + fileName);
-			}
-
 			if (!newContent.endsWith("\n\n}") &&
 				!newContent.endsWith("{\n}")) {
 
@@ -912,6 +908,11 @@ public class SourceFormatter {
 					_sourceFormatterHelper.printError(
 						fileName, "tab: " + fileName + " " + lineCount);
 				}
+			}
+
+			if (line.contains("  {") && !line.matches("\\s*\\*.*")) {
+				_sourceFormatterHelper.printError(
+					fileName, "{:" + fileName + " " + lineCount);
 			}
 
 			StringBuilder lineSB = new StringBuilder();
