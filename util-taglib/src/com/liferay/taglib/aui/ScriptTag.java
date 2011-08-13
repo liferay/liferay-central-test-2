fccdf0b76d8210ae430de097e5ad383ffcf7c176
Brian Chan <brian.chan@liferay.com>
2011-08-13 00:23:57 -0700

    LPS-19970 Source formatting

--- util-taglib/src/com/liferay/taglib/aui/ScriptTag.java
+++ util-taglib/src/com/liferay/taglib/aui/ScriptTag.java
@@ -98,6 +98,7 @@ public class ScriptTag extends BaseScriptTag {
 
 		try {
 			StringBundler bodyContentSB = getBodyContentAsStringBundler();
+
 			String use = getUse();
 
 			if (positionInline) {
