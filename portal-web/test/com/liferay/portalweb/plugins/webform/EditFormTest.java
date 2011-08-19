50731651555b7b8fcc3df59cde4d8e774235cca6
alee8888 <albert.lee@liferay.com>
2011-08-20 02:36:43 +0800

    Test Fixes - plugins/webform

--- portal-web/test/com/liferay/portalweb/plugins/webform/EditFormTest.java
+++ portal-web/test/com/liferay/portalweb/plugins/webform/EditFormTest.java
@@ -81,6 +81,7 @@ public class EditFormTest extends BaseTestCase {
 		selenium.saveScreenShotAndSource();
 		selenium.clickAt("//div[@class='lfr-component lfr-menu-list']/ul/li[2]/a",
 			RuntimeVariables.replace("Configuration"));
+		Thread.sleep(5000);
 
 		for (int second = 0;; second++) {
 			if (second >= 60) {
