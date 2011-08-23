ecef24746530d705cc5877e60567e84ee3da0fcb
Brian Chiu <brian.chiu@liferay.com>
2011-08-23 15:55:36 -0700

    Test Fix Edit - portlet\messageboards

--- portal-web/test/com/liferay/portalweb/portlet/messageboards/lar/importlar/AssertImportLARTest.java
+++ portal-web/test/com/liferay/portalweb/portlet/messageboards/lar/importlar/AssertImportLARTest.java
@@ -101,6 +101,52 @@ public class AssertImportLARTest extends BaseTestCase {
 		assertEquals(RuntimeVariables.replace("1"),
 			selenium.getText(
 				"//div[2]/div[2]/div/div[1]/table/tbody/tr[3]/td[4]/a"));
+		selenium.clickAt("//div[2]/div[2]/div/div[1]/table/tbody/tr[3]/td[1]/a",
+			RuntimeVariables.replace("T\u00e9st M\u00e9ssag\u00e9 Edited"));
+		selenium.waitForPageToLoad("30000");
+		selenium.saveScreenShotAndSource();
+		assertEquals(RuntimeVariables.replace(
+				"This is a t\u00e9st m\u00e9ssag\u00e9 edited!"),
+			selenium.getText("//div[@class='thread-body']"));
+		selenium.open("/web/guest/home/");
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isVisible(
+							"link=M\u00e9ssag\u00e9 Boards T\u00e9st Pag\u00e9")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.saveScreenShotAndSource();
+		selenium.clickAt("link=M\u00e9ssag\u00e9 Boards T\u00e9st Pag\u00e9",
+			RuntimeVariables.replace(
+				"M\u00e9ssag\u00e9 Boards T\u00e9st Pag\u00e9"));
+		selenium.waitForPageToLoad("30000");
+		selenium.saveScreenShotAndSource();
+		assertEquals(RuntimeVariables.replace(
+				"T\u00e9st Cat\u00e9gory\nThis is a t\u00e9st cat\u00e9gory!"),
+			selenium.getText("//td[1]/a"));
+		selenium.clickAt("//td[1]/a",
+			RuntimeVariables.replace(
+				"T\u00e9st Cat\u00e9gory\nThis is a t\u00e9st cat\u00e9gory!"));
+		selenium.waitForPageToLoad("30000");
+		selenium.saveScreenShotAndSource();
+		assertEquals(RuntimeVariables.replace("T\u00e9st Subcat\u00e9gory"),
+			selenium.getText("//a/strong"));
+		selenium.clickAt("//a/strong",
+			RuntimeVariables.replace("T\u00e9st Subcat\u00e9gory"));
+		selenium.waitForPageToLoad("30000");
+		selenium.saveScreenShotAndSource();
 		assertEquals(RuntimeVariables.replace("RE: T\u00e9st M\u00e9ssag\u00e9"),
 			selenium.getText("//tr[4]/td[1]/a"));
 		assertEquals(RuntimeVariables.replace("Joe Bloggs"),
