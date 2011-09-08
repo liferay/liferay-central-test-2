874303245a2cbdc91b6ab9de7c3ad6c40d8292f7
Sergio González <sergio.gonzalez@liferay.com>
2011-09-08 19:40:48 +0200

    LPS-20950

--- portal-impl/src/com/liferay/portal/upgrade/v6_1_0/UpgradeDLSync.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_1_0/UpgradeDLSync.java
@@ -31,14 +31,14 @@ import java.sql.ResultSet;
  */
 public class UpgradeDLSync extends UpgradeProcess {
 
-	protected void updateDlSync() throws Exception {
+	protected void updateDLSync() throws Exception {
 
-		Connection con = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
+		Connection con1 = null;
+		PreparedStatement ps1 = null;
+		ResultSet rs1 = null;
 
 		try {
-			con = DataAccess.getConnection();
+			con1 = DataAccess.getConnection();
 
 			StringBundler sb = new StringBundler(10);
 
@@ -46,46 +46,70 @@ public class UpgradeDLSync extends UpgradeProcess {
 			sb.append("DLFileEntry.groupId as groupId, DLFileEntry.companyId");
 			sb.append(" as companyId, DLFileEntry.createDate as createDate, ");
 			sb.append("DLFileEntry.folderId as parentFolderId, \"file\" as ");
-			sb.append("type from DLFileEntry UNION ALL");
+			sb.append("type from DLFileEntry UNION ALL ");
 			sb.append("SELECT DLFolder.folderId as id, DLFolder.groupId as ");
 			sb.append("groupId, DLFolder.companyId as companyId, ");
 			sb.append("DLFolder.createDate as createDate, ");
 			sb.append("DLFolder.parentFolderId as parentFolderId, ");
 			sb.append("\"folder\" as type  from DLFolder");
 
-			String sql = sb.toString();
+			String sql1 = sb.toString();
 
-			ps = con.prepareStatement(sql);
+			ps1 = con1.prepareStatement(sql1);
 
-			rs = ps.executeQuery();
+			rs1 = ps1.executeQuery();
 
-			while (rs.next()) {
+			while (rs1.next()) {
 				long syncId = increment();
-				long id = rs.getLong("id");
-				long groupId = rs.getLong("groupId");
-				long companyId = rs.getLong("companyId");
-				Date createDate = rs.getDate("creationDate");
-				long parentFolderId = rs.getLong("parentFolderId");
-				String type = rs.getString("type");
+				long id = rs1.getLong("id");
+				long groupId = rs1.getLong("groupId");
+				long companyId = rs1.getLong("companyId");
+				Date createDate = rs1.getDate("createDate");
+				long parentFolderId = rs1.getLong("parentFolderId");
+				String type = rs1.getString("type");
 
-				runSQL(
-					"insert into DLSync (syncId, companyId, createDate, " +
-						"modifiedDate, fileId, repositoryId, parentFolderId," +
-							" event, type_) values (" + syncId + ", " +
-								companyId + ", " + createDate + ", " +
-									createDate + ", " + id + ", " + groupId +
-										", " + parentFolderId + ", " +
-											"\"add\", " + type + ")");
+				Connection con2 = null;
+				PreparedStatement ps2 = null;
+
+				try {
+					con2 = DataAccess.getConnection();
+
+					StringBundler sb2 = new StringBundler(4);
+
+					sb2.append("insert into DLSync (syncId, companyId, ");
+					sb2.append("createDate, modifiedDate, fileId, ");
+					sb2.append("repositoryId, parentFolderId, event, type_) ");
+					sb2.append("values (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+					String sql2 = sb2.toString();
+
+					ps2 = con2.prepareStatement(sql2);
+
+					ps2.setLong(1, syncId);
+					ps2.setLong(2, companyId);
+					ps2.setDate(3, createDate);
+					ps2.setDate(4, createDate);
+					ps2.setLong(5, id);
+					ps2.setLong(6, groupId);
+					ps2.setLong(7, parentFolderId);
+					ps2.setString(8, "add");
+					ps2.setString(9, type);
+
+					ps2.executeUpdate();
+				}
+				finally {
+					DataAccess.cleanUp(con2, ps2);
+				}
 			}
 		}
 		finally {
-			DataAccess.cleanUp(con, ps, rs);
+			DataAccess.cleanUp(con1, ps1, rs1);
 		}
 	}
 
 	@Override
 	protected void doUpgrade() throws Exception {
-		updateDlSync();
+		updateDLSync();
 	}
 
 	public long increment() throws SystemException {
