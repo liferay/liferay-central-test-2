b30105efa4295c4d6cc0a37faaf25bfc1100bf78
Brian Chan <brian.chan@liferay.com>
2011-09-16 11:42:58 -0700

    LPS-21232 Source formatting

--- portal-service/src/com/liferay/portal/kernel/util/StreamUtil.java
+++ portal-service/src/com/liferay/portal/kernel/util/StreamUtil.java
@@ -22,6 +22,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+
 import java.nio.channels.Channel;
 import java.nio.channels.FileChannel;
 
