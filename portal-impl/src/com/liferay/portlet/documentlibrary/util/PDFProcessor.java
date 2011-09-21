4504895f0f189639fa2ce60323cae8777623cee3
Brian Chan <brian.chan@liferay.com>
2011-09-20 23:03:25 -0700

    LPS-20793 Source formatting

--- portal-impl/src/com/liferay/portlet/documentlibrary/util/PDFProcessor.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/util/PDFProcessor.java
@@ -323,7 +323,9 @@ public class PDFProcessor extends DLPreviewableProcessor {
 		IMOperation imOperation = new IMOperation();
 
 		imOperation.background("White");
+
 		imOperation.flatten();
+
 		imOperation.density(dpi, dpi);
 
 		if (height != 0) {
