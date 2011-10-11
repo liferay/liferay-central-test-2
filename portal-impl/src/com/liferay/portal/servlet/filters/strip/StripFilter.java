6eaa788cf39645501ba66258c5136a625a65024b
Brian Chan <brian.chan@liferay.com>
2011-10-11 04:44:01 -0700

    LPS-21922 Source formatting

--- portal-impl/src/com/liferay/portal/servlet/filters/strip/StripFilter.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/strip/StripFilter.java
@@ -391,7 +391,8 @@ public class StripFilter extends BasePortalFilter {
 
 		if (KMPSearch.search(
 				charBuffer, 0, endPos, 
-				_MARKER_TYPE_JS, _MARKER_TYPE_JS_NEXTS) == -1) {
+				_MARKER_TYPE_JAVASCRIPT, _MARKER_TYPE_JAVASCRIPT_NEXTS) == -1) {
+
 			return;
 		}
 
@@ -599,11 +600,6 @@ public class StripFilter extends BasePortalFilter {
 
 	private static final char[] _MARKER_INPUT_OPEN = "input".toCharArray();
 
-	private static final String _MARKER_TYPE_JS = "type=\"text/javascript\"";
-	
-	private static final int[] _MARKER_TYPE_JS_NEXTS = 
-		KMPSearch.generateNexts(_MARKER_TYPE_JS);
-	
 	private static final String _MARKER_PRE_CLOSE = "/pre>";
 
 	private static final int[] _MARKER_PRE_CLOSE_NEXTS =
@@ -633,6 +629,12 @@ public class StripFilter extends BasePortalFilter {
 
 	private static final char[] _MARKER_TEXTAREA_OPEN =
 		"textarea ".toCharArray();
+
+	private static final String _MARKER_TYPE_JAVASCRIPT =
+		"type=\"text/javascript\"";
+
+	private static final int[] _MARKER_TYPE_JAVASCRIPT_NEXTS = 
+		KMPSearch.generateNexts(_MARKER_TYPE_JAVASCRIPT);
 
 	private static final String _STRIP = "strip";
 
