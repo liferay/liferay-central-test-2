3cf898bff693dc85ea5b7c6614946b160b8d2051
michaelhashimoto <michael.hashimoto@liferay.com>
2011-10-11 17:00:36 -0700

    Test Fixes - portal/permissions/messageboards

--- portal-web/test/com/liferay/portalweb/portal/permissions/messageboards/SA_AddSiteMembersTest.java
+++ portal-web/test/com/liferay/portalweb/portal/permissions/messageboards/SA_AddSiteMembersTest.java
@@ -108,12 +108,38 @@ public class SA_AddSiteMembersTest extends BaseTestCase {
 		selenium.clickAt("//input[@name='_174_allRowIds']",
 			RuntimeVariables.replace("Check All"));
 		assertTrue(selenium.isChecked("//input[@name='_174_allRowIds']"));
-		selenium.clickAt("//input[@value='Update Associations']",
-			RuntimeVariables.replace("Update Associations"));
+		selenium.clickAt("//input[@value='Save']",
+			RuntimeVariables.replace("Save"));
 		selenium.waitForPageToLoad("30000");
 		assertEquals(RuntimeVariables.replace(
 				"Your request completed successfully."),
 			selenium.getText("//div[@class='portlet-msg-success']"));
+		selenium.clickAt("link=Add Members",
+			RuntimeVariables.replace("Add Members"));
+
+		for (int second = 0;; second++) {
+			if (second >= 90) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isVisible(
+							"//div[@class='lfr-component lfr-menu-list']/ul/li[1]/a")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		assertEquals(RuntimeVariables.replace("User"),
+			selenium.getText(
+				"//div[@class='lfr-component lfr-menu-list']/ul/li[1]/a"));
+		selenium.clickAt("//div[@class='lfr-component lfr-menu-list']/ul/li[1]/a",
+			RuntimeVariables.replace("User"));
+		selenium.waitForPageToLoad("30000");
 		selenium.type("//input[@name='_174_keywords']",
 			RuntimeVariables.replace("Member"));
 		selenium.clickAt("//input[@value='Search']",
@@ -123,8 +149,8 @@ public class SA_AddSiteMembersTest extends BaseTestCase {
 		selenium.clickAt("//input[@name='_174_allRowIds']",
 			RuntimeVariables.replace("Check All"));
 		assertTrue(selenium.isChecked("//input[@name='_174_allRowIds']"));
-		selenium.clickAt("//input[@value='Update Associations']",
-			RuntimeVariables.replace("Update Associations"));
+		selenium.clickAt("//input[@value='Save']",
+			RuntimeVariables.replace("Save"));
 		selenium.waitForPageToLoad("30000");
 		assertEquals(RuntimeVariables.replace(
 				"Your request completed successfully."),
