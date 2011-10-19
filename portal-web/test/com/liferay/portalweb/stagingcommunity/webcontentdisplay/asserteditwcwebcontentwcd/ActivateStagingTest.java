1ea340fcd3067adb2f2536a605f1cffa4cffbaea
Ken Duenwald <ken.duenwald@liferay.com>
2011-10-18 17:58:28 -0700

    Test Fixes - stagingcommunity/webcontentdisplay

--- portal-web/test/com/liferay/portalweb/stagingcommunity/webcontentdisplay/asserteditwcwebcontentwcd/ActivateStagingTest.java
+++ portal-web/test/com/liferay/portalweb/stagingcommunity/webcontentdisplay/asserteditwcwebcontentwcd/ActivateStagingTest.java
@@ -56,8 +56,9 @@ public class ActivateStagingTest extends BaseTestCase {
 					RuntimeVariables.replace("Search"));
 				selenium.waitForPageToLoad("30000");
 				assertEquals(RuntimeVariables.replace("Actions"),
-					selenium.getText("//td[6]/span/ul/li/strong/a/span"));
-				selenium.click("//td[6]/span/ul/li/strong/a/span");
+					selenium.getText("//td[7]/span/ul/li/strong/a/span"));
+				selenium.clickAt("//td[7]/span/ul/li/strong/a/span",
+					RuntimeVariables.replace("Actions"));
 
 				for (int second = 0;; second++) {
 					if (second >= 90) {
