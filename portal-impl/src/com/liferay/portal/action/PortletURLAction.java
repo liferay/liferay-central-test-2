95fdb983c7060b8f3f3bc5c3dc6f2066cff4faa1
Brian Chan <brian.chan@liferay.com>
2011-10-20 04:01:03 -0700

    LPS-20373 "" should be valid I think

--- portal-impl/src/com/liferay/portal/action/PortletURLAction.java
+++ portal-impl/src/com/liferay/portal/action/PortletURLAction.java
@@ -168,7 +168,7 @@ public class PortletURLAction extends Action {
 				String key = entry.getKey();
 				String value = entry.getValue();
 
-				if (Validator.isNotNull(key) && Validator.isNotNull(value)) {
+				if ((key != null) && (value != null)) {
 					portletURL.setParameter(key, value);
 				}
 			}
