316454c5741ee7ee03bd4093f52e2ab74d05b296
Brian Chan <brian.chan@liferay.com>
2011-10-27 13:51:35 -0700

    LPS-21672 Catch exception when using setup wizard on a db that already exists

--- portal-impl/src/com/liferay/portal/events/ChannelHubAppStartupAction.java
+++ portal-impl/src/com/liferay/portal/events/ChannelHubAppStartupAction.java
@@ -19,6 +19,7 @@ import com.liferay.portal.kernel.events.SimpleAction;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.notifications.ChannelHubManagerUtil;
+import com.liferay.portal.kernel.notifications.DuplicateChannelHubException;
 import com.liferay.portal.kernel.util.GetterUtil;
 
 /**
@@ -37,6 +38,11 @@ public class ChannelHubAppStartupAction extends SimpleAction {
 			}
 
 			ChannelHubManagerUtil.createChannelHub(companyId);
+		}
+		catch (DuplicateChannelHubException dche) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(dche.getMessage());
+			}
 		}
 		catch (Exception e) {
 			throw new ActionException(e);
