2f60c9310e0c9076c3fcb6fe9a57ebc4a8f513b0
Ken Duenwald <ken.duenwald@liferay.com>
2011-10-27 10:18:16 -0700

    LPS-19448

---
+++ portal-web/test/com/liferay/portalweb/plugins/kaleo/wiki/wikipage/viewwikipagecompleted/ViewWikiPageCompletedTests.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portalweb.plugins.kaleo.wiki.wikipage.viewwikipagecompleted;
+
+import com.liferay.portalweb.portal.BaseTests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class ViewWikiPageCompletedTests extends BaseTests {
+
+	public static Test suite() {
+		TestSuite testSuite = new TestSuite();
+
+		testSuite.addTestSuite(ConfigureWikiPageSingleApproverTest.class);
+		testSuite.addTestSuite(AddPageWikiTest.class);
+		testSuite.addTestSuite(AddPortletWikiTest.class);
+		testSuite.addTestSuite(AddWikiPageTest.class);
+		testSuite.addTestSuite(AssignToMeWikiPageActionsTest.class);
+		testSuite.addTestSuite(ApproveWikiPageActionsTest.class);
+		testSuite.addTestSuite(ViewWikiPageCompletedTest.class);
+		testSuite.addTestSuite(SignOutTest.class);
+		testSuite.addTestSuite(Guest_ViewWikiPageCompletedTest.class);
+		testSuite.addTestSuite(SignInTest.class);
+		testSuite.addTestSuite(TearDownWikiNodeTest.class);
+		testSuite.addTestSuite(TearDownPageTest.class);
+		testSuite.addTestSuite(TearDownWorkflowConfigurationTest.class);
+
+		return testSuite;
+	}
+
+}
