fd8f337d0f24eee04f64b2ac9d8d05fc34e83a57
Chas Austin <charles.austin@liferay.com>
2011-10-28 14:56:27 -0700

    LPS-21110

--- portal-web/test/com/liferay/portalweb/portlet/documentlibrarydisplay/portlet/PortletTestPlan.java
+++ portal-web/test/com/liferay/portalweb/portlet/documentlibrarydisplay/portlet/PortletTestPlan.java
@@ -15,6 +15,7 @@
 package com.liferay.portalweb.portlet.documentlibrarydisplay.portlet;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
+import com.liferay.portalweb.portlet.documentlibrarydisplay.portlet.adddocumentportlet.AddDocumentPortletTests;
 import com.liferay.portalweb.portlet.documentlibrarydisplay.portlet.addportlet.AddPortletTests;
 import com.liferay.portalweb.portlet.documentlibrarydisplay.portlet.addportletmultiple.AddPortletMultipleTests;
 import com.liferay.portalweb.portlet.documentlibrarydisplay.portlet.configureportletshowfoldersearch.ConfigurePortletShowFolderSearchTests;
@@ -31,6 +32,7 @@ public class PortletTestPlan extends BaseTestSuite {
 	public static Test suite() {
 		TestSuite testSuite = new TestSuite();
 
+		testSuite.addTest(AddDocumentPortletTests.suite());
 		testSuite.addTest(AddPortletTests.suite());
 		testSuite.addTest(AddPortletMultipleTests.suite());
 		testSuite.addTest(ConfigurePortletShowFolderSearchTests.suite());
