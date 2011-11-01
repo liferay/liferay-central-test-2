e64966b0e3a02cecc1f941d5880e5fea30e352d8
michaelhashimoto <michael.hashimoto@liferay.com>
2011-10-31 17:17:57 -0700

    Test Fixes - portal/controlpanel/virtualhosting

--- portal-web/test/com/liferay/portalweb/portal/controlpanel/virtualhosting/RestorePublicPageURLTest.java
+++ portal-web/test/com/liferay/portalweb/portal/controlpanel/virtualhosting/RestorePublicPageURLTest.java
@@ -51,8 +51,8 @@ public class RestorePublicPageURLTest extends BaseTestCase {
 			RuntimeVariables.replace("Search"));
 		selenium.waitForPageToLoad("30000");
 		assertEquals(RuntimeVariables.replace("Actions"),
-			selenium.getText("//td[7]/span/ul/li/strong/a/span"));
-		selenium.clickAt("//td[7]/span/ul/li/strong/a/span",
+			selenium.getText("//span[@title='Actions']/ul/li/strong/a/span"));
+		selenium.clickAt("//span[@title='Actions']/ul/li/strong/a/span",
 			RuntimeVariables.replace("Actions"));
 
 		for (int second = 0;; second++) {
