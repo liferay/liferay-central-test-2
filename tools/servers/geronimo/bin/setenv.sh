43b0674d474a7a8138d261e463cf000101526868
Minhchau Dang <mdang@mdang-latitude-e6510.home>
2011-11-17 12:24:50 -0800

    LPS-22764 Increase permanent generation for default bundle

--- tools/servers/geronimo/bin/setenv.sh
+++ tools/servers/geronimo/bin/setenv.sh
@@ -1,1 +1,1 @@
-JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8 -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
+JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8 -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=512m"
