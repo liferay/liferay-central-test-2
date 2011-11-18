e08ef6f9f0ff89361d88fca686b20d38e38fad78
Brian Chan <brian.chan@liferay.com>
2011-11-18 10:46:35 -0800

    LPS-21498 Shinn's fix was correct

--- portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBStatsUserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBStatsUserLocalServiceImpl.java
@@ -108,7 +108,13 @@ public class MBStatsUserLocalServiceImpl
 
 		dynamicQuery.add(PropertyFactoryUtil.forName("userId").eq(userId));
 
-		return mbStatsUserLocalService.dynamicQueryCount(dynamicQuery);
+		List<Long> results = mbStatsUserLocalService.dynamicQuery(dynamicQuery);
+
+		if (results.isEmpty()) {
+			return 0;
+		}
+
+		return results.get(0);
 	}
 
 	public MBStatsUser getStatsUser(long groupId, long userId)
