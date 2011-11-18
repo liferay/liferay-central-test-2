6d0ac861579522b901ecf9b779337facb74dfc81
michaelhashimoto <michael.hashimoto@liferay.com>
2011-11-18 13:40:13 -0800

    Test Fixes - portal/session

--- portal-web/test/com/liferay/portalweb/portal/SessionSmokeTestSuite.java
+++ portal-web/test/com/liferay/portalweb/portal/SessionSmokeTestSuite.java
@@ -23,7 +23,7 @@ import junit.framework.TestSuite;
 /**
  * @author Brian Wing Shun Chan
  */
-public class SessionTestSuite extends BaseTests {
+public class SessionSmokeTestSuite extends BaseTests {
 
 	public static Test suite() {
 		TestSuite testSuite = new TestSuite();
