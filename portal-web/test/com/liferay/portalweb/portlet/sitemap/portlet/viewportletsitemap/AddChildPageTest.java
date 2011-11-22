9a2c4ef8384461f955f6e0b80b97d99bca594fdb
Ken Duenwald <ken.duenwald@liferay.com>
2011-11-21 18:17:57 -0800

    Test Fixes - portlet/sitemap

--- portal-web/test/com/liferay/portalweb/portlet/sitemap/portlet/viewportletsitemap/AddChildPageTest.java
+++ portal-web/test/com/liferay/portalweb/portlet/sitemap/portlet/viewportletsitemap/AddChildPageTest.java
@@ -129,7 +129,7 @@ public class AddChildPageTest extends BaseTestCase {
 					RuntimeVariables.replace("Add Page"));
 				selenium.waitForPageToLoad("30000");
 				assertEquals(RuntimeVariables.replace(
-						"Your request completed successfully."),
+						"Your request completed successfully. The page will be refreshed when you close this dialog. Alternatively you can hide this dialog."),
 					selenium.getText("//div[@class='portlet-msg-success']"));
 				selenium.open("/web/guest/home/");
 
