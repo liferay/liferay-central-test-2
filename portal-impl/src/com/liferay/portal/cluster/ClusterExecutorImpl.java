54d5009bf96aa55f8f3c8321c1bbac9fc0825bd6
amos.fong <amos.fong@liferay.com>
2011-11-25 17:59:14 +0800

    LPS-23463

--- portal-impl/src/com/liferay/portal/cluster/ClusterExecutorImpl.java
+++ portal-impl/src/com/liferay/portal/cluster/ClusterExecutorImpl.java
@@ -377,8 +377,9 @@ public class ClusterExecutorImpl
 		ObjectValuePair<Address, ClusterNode> liveInstance =
 			new ObjectValuePair<Address, ClusterNode>(address, clusterNode);
 
-		Long oldExpirationTime = _liveInstances.put(
-			liveInstance, expirationTime);
+		Long oldExpirationTime = _liveInstances.remove(liveInstance);
+
+		_liveInstances.put(liveInstance, expirationTime);
 
 		if ((oldExpirationTime != null) ||
 			((_localAddress != null) && _localAddress.equals(address))) {
