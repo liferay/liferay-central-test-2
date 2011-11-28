2d35dbfaccffb90346384e50cef68b69e7cefc93
Brian Chan <brian.chan@liferay.com>
2011-11-27 20:48:34 -0800

    LPS-13834 Simpler fix

--- portal-service/src/com/liferay/portal/util/SubscriptionSender.java
+++ portal-service/src/com/liferay/portal/util/SubscriptionSender.java
@@ -27,6 +27,7 @@ import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.MessageBusUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ObjectValuePair;
 import com.liferay.portal.kernel.util.StringPool;
@@ -34,12 +35,14 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.Company;
 import com.liferay.portal.model.Group;
+import com.liferay.portal.model.LayoutSet;
 import com.liferay.portal.model.Subscription;
 import com.liferay.portal.model.User;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.service.CompanyLocalServiceUtil;
 import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.service.LayoutSetLocalServiceUtil;
 import com.liferay.portal.service.SubscriptionLocalServiceUtil;
 import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.service.permission.SubscriptionPermissionUtil;
@@ -209,7 +212,8 @@ public class SubscriptionSender implements Serializable {
 		setContextAttribute("[$COMPANY_ID$]", company.getCompanyId());
 		setContextAttribute("[$COMPANY_MX$]", company.getMx());
 		setContextAttribute("[$COMPANY_NAME$]", company.getName());
-		setContextAttribute("[$PORTAL_URL$]", company.getVirtualHostname());
+		setContextAttribute(
+			"[$PORTAL_URL$]", getPortalURL(company.getVirtualHostname()));
 
 		if (groupId > 0) {
 			Group group = GroupLocalServiceUtil.getGroup(groupId);
@@ -348,6 +352,40 @@ public class SubscriptionSender implements Serializable {
 
 		SubscriptionLocalServiceUtil.deleteSubscription(
 			subscription.getSubscriptionId());
+	}
+
+	protected String getPortalURL(String virtualHostname)
+		throws PortalException, SystemException {
+
+		String portalURL = PortalUtil.getPortalURL(
+			virtualHostname, Http.HTTP_PORT, false);
+
+		if (groupId <= 0) {
+			return portalURL;
+		}
+
+		Group group = GroupLocalServiceUtil.getGroup(groupId);
+
+		if (group.hasPublicLayouts()) {
+			LayoutSet layoutSet = LayoutSetLocalServiceUtil.getLayoutSet(
+				groupId, false);
+
+			if (Validator.isNotNull(layoutSet.getVirtualHostname())) {
+				portalURL = PortalUtil.getPortalURL(
+					layoutSet.getVirtualHostname(), Http.HTTP_PORT, false);
+			}
+		}
+		else if (group.hasPrivateLayouts()) {
+			LayoutSet layoutSet = LayoutSetLocalServiceUtil.getLayoutSet(
+				groupId, true);
+
+			if (Validator.isNotNull(layoutSet.getVirtualHostname())) {
+				portalURL = PortalUtil.getPortalURL(
+					layoutSet.getVirtualHostname(), Http.HTTP_PORT, false);
+			}
+		}
+
+		return portalURL;
 	}
 
 	protected boolean hasPermission(Subscription subscription, User user)
