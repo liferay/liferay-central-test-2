ba3cb7f0b7409724c8e9e7ac6591287ccadb0157
Ken Duenwald <ken.duenwald@liferay.com>
2011-11-29 17:12:16 -0800

    Test Fixes - portlet/blogsaggregator

--- portal-web/test/com/liferay/portalweb/portlet/blogsaggregator/portlet/configureportletdisplaystyletitle/ConfigurePortletDisplayStyleTitleTest.java
+++ portal-web/test/com/liferay/portalweb/portlet/blogsaggregator/portlet/configureportletdisplaystyletitle/ConfigurePortletDisplayStyleTitleTest.java
@@ -44,6 +44,7 @@ public class ConfigurePortletDisplayStyleTitleTest extends BaseTestCase {
 		selenium.clickAt("link=Blogs Aggregator Test Page",
 			RuntimeVariables.replace("Blogs Aggregator Test Page"));
 		selenium.waitForPageToLoad("30000");
+		Thread.sleep(5000);
 		assertEquals(RuntimeVariables.replace("Options"),
 			selenium.getText("//strong/a"));
 		selenium.clickAt("//strong/a", RuntimeVariables.replace("Options"));
