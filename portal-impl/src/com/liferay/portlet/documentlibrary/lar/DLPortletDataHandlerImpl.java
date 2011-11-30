1ddab55f848c351279f3f4a0c296147f4958149b
Brian Chan <brian.chan@liferay.com>
2011-11-30 15:06:36 -0800

    LPS-23456 Source formatting

--- portal-impl/src/com/liferay/portlet/documentlibrary/lar/DLPortletDataHandlerImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/lar/DLPortletDataHandlerImpl.java
@@ -73,7 +73,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 import javax.portlet.PortletPreferences;
 
