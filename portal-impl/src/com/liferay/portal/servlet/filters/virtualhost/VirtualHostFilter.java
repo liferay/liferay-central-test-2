liferay/liferay-central-test-2

commit 61c36685863c546b7ac7c05ab85f6bf335a3e5f9

LPS-23561

portal-impl/src/com/liferay/portal/servlet/filters/virtualhost/VirtualHostFilter.java
@@ -61,6 +61,16 @@ public class VirtualHostFilter extends BasePortalFilter {
 		super.init(filterConfig);
 
 		_servletContext = filterConfig.getServletContext();
+
+		_slashedKeywords =
+			new String[PropsValues.LAYOUT_FRIENDLY_URL_KEYWORDS.length];
+
+		for (int i = 0; i < PropsValues.LAYOUT_FRIENDLY_URL_KEYWORDS.length;
+				i++) {
+
+			_slashedKeywords[i] = StringUtil.quote(
+				PropsValues.LAYOUT_FRIENDLY_URL_KEYWORDS[i], StringPool.SLASH);
+		}
 	}
 
 	@Override
@@ -81,40 +91,18 @@ public class VirtualHostFilter extends BasePortalFilter {
 		friendlyURL = friendlyURL.toLowerCase();
 
 		if (PortalInstances.isVirtualHostsIgnorePath(friendlyURL) ||
-			friendlyURL.startsWith(
-				_PRIVATE_GROUP_SERVLET_MAPPING + StringPool.SLASH) ||
-			friendlyURL.startsWith(
-				_PUBLIC_GROUP_SERVLET_MAPPING + StringPool.SLASH) ||
-			friendlyURL.startsWith(
-				_PRIVATE_USER_SERVLET_MAPPING + StringPool.SLASH) ||
-			friendlyURL.startsWith(_PATH_C) ||
-			friendlyURL.startsWith(_PATH_COMBO) ||
-			friendlyURL.startsWith(_PATH_DELEGATE) ||
-			friendlyURL.startsWith(_PATH_DISPLAY_CHART) ||
-			friendlyURL.startsWith(_PATH_DTD) ||
-			friendlyURL.startsWith(_PATH_ELOQUA) ||
-			friendlyURL.startsWith(_PATH_FACEBOOK) ||
-			friendlyURL.startsWith(_PATH_GOOGLE_GADGET) ||
-			friendlyURL.startsWith(_PATH_HTML) ||
-			friendlyURL.startsWith(_PATH_IMAGE) ||
-			friendlyURL.startsWith(_PATH_LANGUAGE) ||
-			friendlyURL.startsWith(_PATH_LUCENE) ||
-			friendlyURL.startsWith(_PATH_NETVIBES) ||
-			friendlyURL.startsWith(_PATH_OSGI) ||
-			friendlyURL.startsWith(_PATH_PBHS) ||
-			friendlyURL.startsWith(_PATH_POLLER) ||
-			friendlyURL.startsWith(_PATH_REST) ||
-			friendlyURL.startsWith(_PATH_ROBOTS_TXT) ||
-			friendlyURL.startsWith(_PATH_SHAREPOINT) ||
-			friendlyURL.startsWith(_PATH_SITEMAP_XML) ||
-			friendlyURL.startsWith(_PATH_SOFTWARE_CATALOG) ||
 			friendlyURL.startsWith(_PATH_VTI) ||
-			friendlyURL.startsWith(_PATH_WAP) ||
-			friendlyURL.startsWith(_PATH_WIDGET) ||
-			friendlyURL.startsWith(_PATH_XMLRPC) ||
-			friendlyURL.startsWith(_PATH_TUNNEL_WEB)) {
+			friendlyURL.startsWith(_PRIVATE_GROUP_SERVLET_MAPPING_SLASH) ||
+			friendlyURL.startsWith(_PRIVATE_USER_SERVLET_MAPPING_SLASH) ||
+			friendlyURL.startsWith(_PUBLIC_GROUP_SERVLET_MAPPING_SLASH)) {
 
 			return false;
+		}
+
+		for (String keyword : _slashedKeywords) {
+			if (friendlyURL.startsWith(keyword)) {
+				return false;
+			}
 		}
 
 		int code = LayoutImpl.validateFriendlyURL(friendlyURL);
@@ -309,71 +297,31 @@ public class VirtualHostFilter extends BasePortalFilter {
 		}
 	}
 
-	private static final String _PATH_C = "/c/";
-
-	private static final String _PATH_COMBO = "/combo/";
-
-	private static final String _PATH_DELEGATE = "/delegate/";
-
-	private static final String _PATH_DISPLAY_CHART = "/display_chart";
-
 	private static final String _PATH_DOCUMENTS = "/documents/";
 
-	private static final String _PATH_DTD = "/dtd/";
-
-	private static final String _PATH_ELOQUA = "/elqNow/";
-
-	private static final String _PATH_FACEBOOK = "/facebook/";
-
-	private static final String _PATH_GOOGLE_GADGET = "/google_gadget/";
-
-	private static final String _PATH_HTML = "/html/";
-
-	private static final String _PATH_IMAGE = "/image/";
-
-	private static final String _PATH_LANGUAGE = "/language/";
-
-	private static final String _PATH_LUCENE = "/lucene/";
-
-	private static final String _PATH_NETVIBES = "/netvibes/";
-
-	private static final String _PATH_OSGI = "/osgi/";
-
-	private static final String _PATH_PBHS = "/pbhs/";
-
-	private static final String _PATH_POLLER = "/poller/";
-
-	private static final String _PATH_REST = "/rest/";
-
-	private static final String _PATH_ROBOTS_TXT = "/robots.txt";
-
-	private static final String _PATH_SHAREPOINT = "/sharepoint/";
-
-	private static final String _PATH_SITEMAP_XML = "/sitemap.xml";
-
-	private static final String _PATH_SOFTWARE_CATALOG = "/software_catalog";
-
 	private static final String _PATH_VTI = "/_vti_";
-
-	private static final String _PATH_WAP = "/wap/";
-
-	private static final String _PATH_WIDGET = "/widget/";
-
-	private static final String _PATH_XMLRPC = "/xmlrpc/";
-
-	private static final String _PATH_TUNNEL_WEB = "/tunnel-web/";
 
 	private static final String _PRIVATE_GROUP_SERVLET_MAPPING =
 		PropsValues.LAYOUT_FRIENDLY_URL_PRIVATE_GROUP_SERVLET_MAPPING;
 
+	private static final String _PRIVATE_GROUP_SERVLET_MAPPING_SLASH =
+		_PRIVATE_GROUP_SERVLET_MAPPING + StringPool.SLASH;
+
 	private static final String _PRIVATE_USER_SERVLET_MAPPING =
 		PropsValues.LAYOUT_FRIENDLY_URL_PRIVATE_USER_SERVLET_MAPPING;
+
+	private static final String _PRIVATE_USER_SERVLET_MAPPING_SLASH =
+		_PRIVATE_USER_SERVLET_MAPPING + StringPool.SLASH;
 
 	private static final String _PUBLIC_GROUP_SERVLET_MAPPING =
 		PropsValues.LAYOUT_FRIENDLY_URL_PUBLIC_SERVLET_MAPPING;
 
+	private static final String _PUBLIC_GROUP_SERVLET_MAPPING_SLASH =
+		_PUBLIC_GROUP_SERVLET_MAPPING + StringPool.SLASH;
+
 	private static Log _log = LogFactoryUtil.getLog(VirtualHostFilter.class);
 
 	private ServletContext _servletContext;
+	private String[] _slashedKeywords;
 
 }

portal-impl/src/com/liferay/portal/model/impl/LayoutImpl.java
@@ -24,7 +24,6 @@ import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
@@ -50,7 +49,6 @@ import com.liferay.portal.util.CookieKeys;
 import com.liferay.portal.util.LayoutClone;
 import com.liferay.portal.util.LayoutCloneFactory;
 import com.liferay.portal.util.PortalUtil;
-import com.liferay.portal.util.PropsUtil;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.portal.util.WebKeys;
 import com.liferay.portlet.PortletURLImpl;
@@ -112,13 +110,10 @@ public class LayoutImpl extends LayoutBaseImpl {
 	public static void validateFriendlyURLKeyword(String friendlyURL)
 		throws LayoutFriendlyURLException {
 
-		String[] keywords = PropsUtil.getArray(
-			PropsKeys.LAYOUT_FRIENDLY_URL_KEYWORDS);
-
-		for (String keyword : keywords) {
-			if ((friendlyURL.indexOf(
-					StringPool.SLASH + keyword + StringPool.SLASH) != -1) ||
-				(friendlyURL.endsWith(StringPool.SLASH + keyword))) {
+		for (String keyword : PropsValues.LAYOUT_FRIENDLY_URL_KEYWORDS) {
+			if (friendlyURL.contains(
+					StringUtil.quote(keyword, StringPool.SLASH)) ||
+				friendlyURL.endsWith(StringPool.SLASH + keyword)) {
 
 				LayoutFriendlyURLException lfurle =
 					new LayoutFriendlyURLException(

portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -727,6 +727,8 @@ public class PropsValues {
 
 	public static String[] LAYOUT_FORM_UPDATE = PropsUtil.getArray(PropsKeys.LAYOUT_FORM_UPDATE);
 
+	public static final String[] LAYOUT_FRIENDLY_URL_KEYWORDS = PropsUtil.getArray(PropsKeys.LAYOUT_FRIENDLY_URL_KEYWORDS);
+
 	public static final String LAYOUT_FRIENDLY_URL_PAGE_NOT_FOUND = PropsUtil.get(PropsKeys.LAYOUT_FRIENDLY_URL_PAGE_NOT_FOUND);
 
 	public static final String LAYOUT_FRIENDLY_URL_PRIVATE_GROUP_SERVLET_MAPPING = PropsUtil.get(PropsKeys.LAYOUT_FRIENDLY_URL_PRIVATE_GROUP_SERVLET_MAPPING);

portal-impl/src/portal.properties
@@ -4245,7 +4245,42 @@
     #
     # Set the reserved keywords that cannot be used in a friendly URL.
     #
-    layout.friendly.url.keywords=c,combo,delegate,display_chart,dtd,facebook,google_gadget,group,html,image,language,netvibes,page,pbhs,poller,public,private,rss,sharepoint,sitemap.xml,tags,software_catalog,_vti_,wap,web,widget,wsrp,xmlrpc
+    layout.friendly.url.keywords=\
+        _vti_,\
+        c,\
+        combo,\
+        delegate,\
+        display_chart,\
+        documents,\
+        dtd,\
+        elqNow,\
+        facebook,\
+        google_gadget,\
+        group,\
+        html,\
+        image,\
+        language,\
+        lucene,\
+        netvibes,\
+        osgi,\
+        page,\
+        pbhs,\
+        poller,\
+        private,\
+        public,\
+        rest,\
+        robots.txt,\
+        rss,\
+        sharepoint,\
+        sitemap.xml,\
+        software_catalog,\
+        tags,\
+        tunnel-web,\
+        wap,\
+        web,\
+        widget,\
+        wsrp,\
+        xmlrpc
 
     #
     # Set this to true if guest users should see the maximize window icon.

commit fb04ac4e0be82b931f3f6214d841301db99b0e7f

LPS-23643 Even better fix, because of regression I created in 61c36685863c546b7ac7c05ab85f6bf335a3e5f9

portal-impl/src/com/liferay/portal/servlet/filters/virtualhost/VirtualHostFilter.java
@@ -70,7 +70,10 @@ public class VirtualHostFilter extends BasePortalFilter {
 
 			String keyword = PropsValues.LAYOUT_FRIENDLY_URL_KEYWORDS[i];
 
-			if (keyword.contains(StringPool.PERIOD)) {
+			if (keyword.contains(StringPool.PERIOD) ||
+				keyword.equals("_vti_") || keyword.equals("display_chart") ||
+				keyword.equals("software_catalog")) {
+
 				keyword = StringPool.SLASH + keyword;
 			}
 			else {
@@ -99,8 +102,6 @@ public class VirtualHostFilter extends BasePortalFilter {
 		friendlyURL = friendlyURL.toLowerCase();
 
 		if (PortalInstances.isVirtualHostsIgnorePath(friendlyURL) ||
-			friendlyURL.startsWith(_PATH_DISPLAY_CHART) ||
-			friendlyURL.startsWith(_PATH_VTI) ||
 			friendlyURL.startsWith(_PRIVATE_GROUP_SERVLET_MAPPING_SLASH) ||
 			friendlyURL.startsWith(_PRIVATE_USER_SERVLET_MAPPING_SLASH) ||
 			friendlyURL.startsWith(_PUBLIC_GROUP_SERVLET_MAPPING_SLASH)) {
@@ -306,11 +307,7 @@ public class VirtualHostFilter extends BasePortalFilter {
 		}
 	}
 
-	private static final String _PATH_DISPLAY_CHART = "/display_chart";
-
 	private static final String _PATH_DOCUMENTS = "/documents/";
-
-	private static final String _PATH_VTI = "/_vti_";
 
 	private static final String _PRIVATE_GROUP_SERVLET_MAPPING =
 		PropsValues.LAYOUT_FRIENDLY_URL_PRIVATE_GROUP_SERVLET_MAPPING;