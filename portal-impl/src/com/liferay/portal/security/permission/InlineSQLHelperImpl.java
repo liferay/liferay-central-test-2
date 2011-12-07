1ffd7d1c071db0f1b95acfa0712b407a11dad2ff
Sergio González <sergio.gonzalez@liferay.com>
2011-12-06 17:21:15 -0800

    LPS-23767

--- portal-impl/src/com/liferay/portal/security/permission/InlineSQLHelperImpl.java
+++ portal-impl/src/com/liferay/portal/security/permission/InlineSQLHelperImpl.java
@@ -438,15 +438,19 @@ public class InlineSQLHelperImpl implements InlineSQLHelper {
 
 		long userId = getUserId();
 
+		boolean hasPreviousPermissionGroup = false;
+
 		for (int j = 0; j < groupIds.length; j++) {
 			long groupId = groupIds[j];
 
 			if (!permissionChecker.hasPermission(
 					groupId, className, 0, ActionKeys.VIEW)) {
 
-				if (j > 0) {
+				if ((j > 0) && hasPreviousPermissionGroup) {
 					sb.append(" OR ");
 				}
+
+				hasPreviousPermissionGroup = true;
 
 				sb.append("(");
 				sb.append(
