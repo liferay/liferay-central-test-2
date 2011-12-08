93fcc34ac44e65de795ff75b586c3416f13f56c7
Matthew Kong <matthew.kong@liferay.com>
2011-12-08 14:16:44 -0800

    LPS-22498 Adding comment to web content causes error if a user is subscribed

--- portal-impl/src/com/liferay/portal/service/permission/SubscriptionPermissionImpl.java
+++ portal-impl/src/com/liferay/portal/service/permission/SubscriptionPermissionImpl.java
@@ -27,6 +27,7 @@ import com.liferay.portlet.blogs.model.BlogsEntry;
 import com.liferay.portlet.blogs.service.BlogsEntryLocalServiceUtil;
 import com.liferay.portlet.blogs.service.permission.BlogsPermission;
 import com.liferay.portlet.journal.model.JournalArticle;
+import com.liferay.portlet.journal.service.JournalArticleLocalServiceUtil;
 import com.liferay.portlet.journal.service.permission.JournalPermission;
 import com.liferay.portlet.messageboards.model.MBCategory;
 import com.liferay.portlet.messageboards.model.MBThread;
@@ -84,8 +85,13 @@ public class SubscriptionPermissionImpl implements SubscriptionPermission {
 				permissionChecker, groupId, ActionKeys.SUBSCRIBE);
 		}
 		else if (className.equals(JournalArticle.class.getName())) {
+			JournalArticle journalArticle = JournalArticleLocalServiceUtil.
+				getLatestArticle(classPK);
+
+			long groupId = journalArticle.getGroupId();
+
 			return JournalPermission.contains(
-				permissionChecker, classPK, ActionKeys.SUBSCRIBE);
+				permissionChecker, groupId, ActionKeys.SUBSCRIBE);
 		}
 		else if (className.equals(MBCategory.class.getName())) {
 			Group group = GroupLocalServiceUtil.fetchGroup(classPK);
