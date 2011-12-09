8c69ac0c331ad7b4eaab5823cf8505f76b73175a
tina tian <tina.tian@liferay.com>
2011-12-09 13:20:17 +0800

    LPS-23943 SQL server with cluster settings failed to start tomcat server

--- portal-impl/src/com/liferay/portal/scheduler/quartz/QuartzSchedulerEngine.java
+++ portal-impl/src/com/liferay/portal/scheduler/quartz/QuartzSchedulerEngine.java
@@ -708,17 +708,26 @@ public class QuartzSchedulerEngine implements SchedulerEngine {
 
 		Properties properties = PropsUtil.getProperties(propertiesPrefix, true);
 
-		if (useQuartzCluster && PropsValues.CLUSTER_LINK_ENABLED) {
+		if (useQuartzCluster) {
 			DB db = DBFactoryUtil.getDB();
 
 			String dbType = db.getType();
 
-			if (dbType.equals(DB.TYPE_HYPERSONIC)) {
-				_log.error("Unable to cluster scheduler on Hypersonic");
+			if (dbType.equals(DB.TYPE_SQLSERVER)) {
+				properties.setProperty(
+					"org.quartz.jobStore.selectWithLockSQL",
+					"SELECT * FROM {0}LOCKS UPDLOCK WHERE LOCK_NAME = ?");
 			}
-			else {
-				properties.put(
-					"org.quartz.jobStore.isClustered", Boolean.TRUE.toString());
+
+			if (PropsValues.CLUSTER_LINK_ENABLED) {
+				if (dbType.equals(DB.TYPE_HYPERSONIC)) {
+					_log.error("Unable to cluster scheduler on Hypersonic");
+				}
+				else {
+					properties.put(
+						"org.quartz.jobStore.isClustered",
+						Boolean.TRUE.toString());
+				}
 			}
 		}
 
