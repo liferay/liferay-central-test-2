146eb0c617212272b5bca976014c6cc13e79916d
Ken Duenwald <ken.duenwald@liferay.com>
2011-12-12 09:30:11 -0800

    Test Fixes - stagingcommunity/documentlibrary

--- portal-web/test/com/liferay/portalweb/stagingcommunity/documentlibrary/document/deletelivepagesitestaginglocallivedl/AddSiteTest.java
+++ portal-web/test/com/liferay/portalweb/stagingcommunity/documentlibrary/document/deletelivepagesitestaginglocallivedl/AddSiteTest.java
@@ -67,11 +67,11 @@ public class AddSiteTest extends BaseTestCase {
 			Thread.sleep(1000);
 		}
 
-		assertEquals(RuntimeVariables.replace("Site"),
+		assertEquals(RuntimeVariables.replace("Blank Site"),
 			selenium.getText(
 				"//div[@class='lfr-component lfr-menu-list']/ul/li[1]/a"));
 		selenium.clickAt("//div[@class='lfr-component lfr-menu-list']/ul/li[1]/a",
-			RuntimeVariables.replace("Site"));
+			RuntimeVariables.replace("Blank Site"));
 		selenium.waitForPageToLoad("30000");
 		selenium.type("//input[@id='_134_name']",
 			RuntimeVariables.replace("Site Name"));
