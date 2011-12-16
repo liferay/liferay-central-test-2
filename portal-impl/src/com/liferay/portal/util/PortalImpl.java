fed7788fbf5d5e575444fa8dd43727031dbaee59
Julio Camarero <julio.camarero@liferay.com>
2011-12-16 11:12:14 +0100

    LPS-20389 fix possible ArrayIndexOutOfBoundsException

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -853,7 +853,7 @@ public class PortalImpl implements Portal {
 		String[] canonicalURLParts = canonicalURL.split(
 			virtualHost.concat("(:\\d{1,5})?"), 2);
 
-		if ((canonicalURLParts.length > 0) &&
+		if ((canonicalURLParts.length > 1) &&
 			Validator.isNotNull(canonicalURLParts[1])) {
 
 			int pos = canonicalURL.indexOf(
@@ -5624,7 +5624,9 @@ public class PortalImpl implements Portal {
 						virtualHostname = company.getVirtualHostname();
 					}
 
-					if (!virtualHostname.equalsIgnoreCase(_LOCALHOST)) {
+					if (canonicalURL ||
+						!virtualHostname.equalsIgnoreCase(_LOCALHOST)) {
+
 						portalURL = getPortalURL(
 							virtualHostname, themeDisplay.getServerPort(),
 							themeDisplay.isSecure());
