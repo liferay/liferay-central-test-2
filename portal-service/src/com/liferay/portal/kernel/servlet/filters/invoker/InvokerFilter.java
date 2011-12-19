7b56144b2da2451b61cd078d3042d6fd32a15cff
Brian Chan <brian.chan@liferay.com>
2011-12-19 09:26:35 -0800

    LPS-24209 Source formatting

--- portal-service/src/com/liferay/portal/kernel/servlet/filters/invoker/InvokerFilter.java
+++ portal-service/src/com/liferay/portal/kernel/servlet/filters/invoker/InvokerFilter.java
@@ -20,7 +20,6 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.BasePortalLifecycle;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.JavaConstants;
-import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.StringPool;
