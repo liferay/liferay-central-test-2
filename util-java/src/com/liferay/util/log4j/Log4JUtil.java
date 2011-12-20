9a0b0c660130076c7d95149b8c34493748f17edb
Hugo Huijser <hugo.huijser@liferay.com>
2011-12-20 12:10:07 -0800

    LPS-24280 add comment

--- util-java/src/com/liferay/util/log4j/Log4JUtil.java
+++ util-java/src/com/liferay/util/log4j/Log4JUtil.java
@@ -70,7 +70,7 @@ public class Log4JUtil {
 			return;
 		}
 
-		// See LPS-6029 and LPS-8865
+		// See LPS-6029, LPS-8865 and LPS-24280
 
 		DOMConfigurator domConfigurator = new DOMConfigurator();
 
