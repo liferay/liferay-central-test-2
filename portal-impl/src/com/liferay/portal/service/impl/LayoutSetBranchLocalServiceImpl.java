40d3cd45daee33a8f5a334b057cdaaa39ea0229f
Julio Camarero <julio.camarero@liferay.com>
2011-12-22 18:04:59 +0100

    LPS-24058 User is unable to create a page variation from an added site variation

--- portal-impl/src/com/liferay/portal/service/impl/LayoutSetBranchLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutSetBranchLocalServiceImpl.java
@@ -121,9 +121,16 @@ public class LayoutSetBranchLocalServiceImpl
 					copyLayoutSetBranchId, true);
 
 			for (LayoutRevision layoutRevision : layoutRevisions) {
+				LayoutBranch layoutBranch =
+					layoutBranchLocalService.addLayoutBranch(
+						layoutSetBranchId, layoutRevision.getPlid(),
+						LayoutBranchConstants.MASTER_BRANCH_NAME,
+						LayoutBranchConstants.MASTER_BRANCH_DESCRIPTION, true,
+						serviceContext);
+
 				layoutRevisionLocalService.addLayoutRevision(
 					userId, layoutSetBranchId,
-					layoutRevision.getLayoutBranchId(),
+					layoutBranch.getLayoutBranchId(),
 					LayoutRevisionConstants.DEFAULT_PARENT_LAYOUT_REVISION_ID,
 					true, layoutRevision.getPlid(),
 					layoutRevision.getPrivateLayout(), layoutRevision.getName(),
