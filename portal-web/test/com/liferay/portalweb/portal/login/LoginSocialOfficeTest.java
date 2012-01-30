5cf62605fc3288e8f986329c2c74fbe031f460c8
Mandy Zia <mandy.zia@liferay.com>
2012-01-30 11:29:16 -0800

    Test Fixes - portal/login

--- portal-web/test/com/liferay/portalweb/portal/login/LoginSocialOfficeTest.java
+++ portal-web/test/com/liferay/portalweb/portal/login/LoginSocialOfficeTest.java
@@ -22,124 +22,358 @@ import com.liferay.portalweb.portal.util.RuntimeVariables;
  */
 public class LoginSocialOfficeTest extends BaseTestCase {
 	public void testLoginSocialOffice() throws Exception {
-		selenium.setTimeout("180000");
-		selenium.open("/web/guest/home");
-		loadRequiredJavaScriptModules();
+		int label = 1;
 
-		for (int second = 0;; second++) {
-			if (second >= 90) {
-				fail("timeout");
-			}
+		while (label >= 1) {
+			switch (label) {
+			case 1:
+				selenium.setTimeout("180000");
+				selenium.open("/web/guest/home");
+				loadRequiredJavaScriptModules();
 
-			try {
-				if (selenium.isVisible("link=Sign In")) {
-					break;
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (selenium.isVisible("link=Sign In")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
 				}
-			}
-			catch (Exception e) {
-			}
 
-			Thread.sleep(1000);
-		}
+				selenium.clickAt("link=Sign In",
+					RuntimeVariables.replace("Sign In"));
+				selenium.waitForPageToLoad("30000");
+				loadRequiredJavaScriptModules();
 
-		selenium.clickAt("link=Sign In", RuntimeVariables.replace(""));
-		selenium.waitForPageToLoad("30000");
-		loadRequiredJavaScriptModules();
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
 
-		for (int second = 0;; second++) {
-			if (second >= 90) {
-				fail("timeout");
-			}
+					try {
+						if (selenium.isVisible("//input[@id='_58_login']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
 
-			try {
-				if (selenium.isVisible("_58_login")) {
-					break;
+					Thread.sleep(1000);
 				}
-			}
-			catch (Exception e) {
-			}
 
-			Thread.sleep(1000);
-		}
+				selenium.type("//input[@id='_58_login']",
+					RuntimeVariables.replace("test@liferay.com"));
+				selenium.type("_58_password", RuntimeVariables.replace("test"));
 
-		selenium.type("_58_login", RuntimeVariables.replace("test@liferay.com"));
-		selenium.type("_58_password", RuntimeVariables.replace("test"));
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
 
-		for (int second = 0;; second++) {
-			if (second >= 90) {
-				fail("timeout");
-			}
+					try {
+						if (selenium.isVisible(
+									"//input[@id='_58_rememberMeCheckbox']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
 
-			try {
-				if (selenium.isVisible("_58_rememberMeCheckbox")) {
-					break;
+					Thread.sleep(1000);
 				}
-			}
-			catch (Exception e) {
-			}
 
-			Thread.sleep(1000);
-		}
+				selenium.click("//input[@id='_58_rememberMeCheckbox']");
 
-		selenium.click("_58_rememberMeCheckbox");
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
 
-		for (int second = 0;; second++) {
-			if (second >= 90) {
-				fail("timeout");
-			}
+					try {
+						if (selenium.isVisible("//input[@value='Sign In']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
 
-			try {
-				if (selenium.isVisible("//input[@value='Sign In']")) {
-					break;
+					Thread.sleep(1000);
 				}
-			}
-			catch (Exception e) {
-			}
 
-			Thread.sleep(1000);
-		}
+				selenium.clickAt("//input[@value='Sign In']",
+					RuntimeVariables.replace("Sign In"));
+				selenium.waitForPageToLoad("30000");
+				loadRequiredJavaScriptModules();
 
-		selenium.clickAt("//input[@value='Sign In']",
-			RuntimeVariables.replace(""));
-		selenium.waitForPageToLoad("30000");
-		loadRequiredJavaScriptModules();
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
 
-		for (int second = 0;; second++) {
-			if (second >= 90) {
-				fail("timeout");
-			}
+					try {
+						if (selenium.isVisible("reminderQueryAnswer")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
 
-			try {
-				if (selenium.isVisible("reminderQueryAnswer")) {
-					break;
+					Thread.sleep(1000);
 				}
-			}
-			catch (Exception e) {
-			}
 
-			Thread.sleep(1000);
-		}
+				selenium.type("reminderQueryAnswer",
+					RuntimeVariables.replace("Test"));
 
-		selenium.type("reminderQueryAnswer", RuntimeVariables.replace("Test"));
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
 
-		for (int second = 0;; second++) {
-			if (second >= 90) {
-				fail("timeout");
-			}
+					try {
+						if (selenium.isVisible("//input[@value='Save']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
 
-			try {
-				if (selenium.isVisible("//input[@value='Save']")) {
-					break;
+					Thread.sleep(1000);
 				}
-			}
-			catch (Exception e) {
-			}
 
-			Thread.sleep(1000);
+				selenium.clickAt("//input[@value='Save']",
+					RuntimeVariables.replace("Save"));
+				selenium.waitForPageToLoad("30000");
+				loadRequiredJavaScriptModules();
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (selenium.isVisible("link=Control Panel")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				selenium.clickAt("link=Control Panel",
+					RuntimeVariables.replace("Control Panel"));
+				selenium.waitForPageToLoad("30000");
+				loadRequiredJavaScriptModules();
+				selenium.clickAt("link=Users and Organizations",
+					RuntimeVariables.replace("Users and Organizations"));
+				selenium.waitForPageToLoad("30000");
+				loadRequiredJavaScriptModules();
+				assertEquals(RuntimeVariables.replace("Search All Users"),
+					selenium.getText("link=Search All Users"));
+				selenium.clickAt("link=Search All Users",
+					RuntimeVariables.replace("Search All Users"));
+				selenium.waitForPageToLoad("30000");
+				loadRequiredJavaScriptModules();
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (selenium.isVisible(
+									"//input[@id='_125_toggle_id_users_admin_user_searchkeywords']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				assertTrue(selenium.isVisible(
+						"//input[@id='_125_toggle_id_users_admin_user_searchkeywords']"));
+				selenium.type("//input[@id='_125_toggle_id_users_admin_user_searchkeywords']",
+					RuntimeVariables.replace("test@liferay.com"));
+				selenium.clickAt("//input[@value='Search']",
+					RuntimeVariables.replace("Search"));
+				assertEquals(RuntimeVariables.replace("Joe"),
+					selenium.getText(
+						"//tr[contains(@class, 'results-row')]/td[2]/a"));
+				selenium.clickAt("//tr[contains(@class, 'results-row')]/td[2]/a",
+					RuntimeVariables.replace("Joe"));
+				selenium.waitForPageToLoad("30000");
+				loadRequiredJavaScriptModules();
+				assertTrue(selenium.isPartialText("//a[@id='_125_rolesLink']",
+						"Roles"));
+				selenium.clickAt("//a[@id='_125_rolesLink']",
+					RuntimeVariables.replace("Roles"));
+				assertEquals(RuntimeVariables.replace("Administrator"),
+					selenium.getText("//tr[contains(., 'Administrator')]/td"));
+				assertEquals(RuntimeVariables.replace("Power User"),
+					selenium.getText("//tr[contains(., 'Power User')]/td"));
+				assertEquals(RuntimeVariables.replace("Select"),
+					selenium.getText("//div[6]/span/a/span"));
+				selenium.clickAt("//div[6]/span/a/span",
+					RuntimeVariables.replace("Select"));
+				Thread.sleep(5000);
+				selenium.selectWindow("title=Users and Organizations");
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (RuntimeVariables.replace("Roles")
+												.equals(selenium.getText(
+										"//h1[@class='header-title']/span"))) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				assertEquals(RuntimeVariables.replace("Roles"),
+					selenium.getText("//h1[@class='header-title']/span"));
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (selenium.isVisible("//input[@id='_125_keywords']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				selenium.type("//input[@id='_125_keywords']",
+					RuntimeVariables.replace("Social Office"));
+				selenium.clickAt("//input[@value='Search']",
+					RuntimeVariables.replace("Social Office"));
+				selenium.waitForPageToLoad("30000");
+				loadRequiredJavaScriptModules();
+				assertEquals(RuntimeVariables.replace("Social Office User"),
+					selenium.getText(
+						"//tr[contains(., 'Social Office User')]/td"));
+				selenium.click("//tr[contains(., 'Social Office User')]/td");
+				selenium.selectWindow("null");
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (RuntimeVariables.replace("Social Office User")
+												.equals(selenium.getText(
+										"//tr[contains(., 'Social Office User')]/td"))) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				assertEquals(RuntimeVariables.replace("Social Office User"),
+					selenium.getText(
+						"//tr[contains(., 'Social Office User')]/td"));
+				selenium.clickAt("//input[@value='Save']",
+					RuntimeVariables.replace("Save"));
+				selenium.waitForPageToLoad("30000");
+				loadRequiredJavaScriptModules();
+				assertEquals(RuntimeVariables.replace(
+						"Your request completed successfully."),
+					selenium.getText("//div[@class='portlet-msg-success']"));
+				selenium.open("/web/guest/home/");
+				loadRequiredJavaScriptModules();
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (selenium.isVisible("link=Sign Out")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				selenium.clickAt("link=Sign Out",
+					RuntimeVariables.replace("Sign Out"));
+				selenium.waitForPageToLoad("30000");
+				loadRequiredJavaScriptModules();
+				assertTrue(selenium.isVisible("//input[@value='Sign In']"));
+				selenium.open("/web/guest/home/");
+				loadRequiredJavaScriptModules();
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (selenium.isVisible("//input[@id='_58_login']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				selenium.type("//input[@id='_58_login']",
+					RuntimeVariables.replace("test@liferay.com"));
+				selenium.type("//input[@id='_58_password']",
+					RuntimeVariables.replace("test"));
+
+				boolean rememberMeCheckboxChecked2 = selenium.isChecked(
+						"_58_rememberMeCheckbox");
+
+				if (rememberMeCheckboxChecked2) {
+					label = 2;
+
+					continue;
+				}
+
+				selenium.clickAt("//input[@id='_58_rememberMeCheckbox']",
+					RuntimeVariables.replace("Remember Me Checkbox"));
+
+			case 2:
+				selenium.clickAt("//input[@value='Sign In']",
+					RuntimeVariables.replace("Sign In"));
+				selenium.waitForPageToLoad("30000");
+				loadRequiredJavaScriptModules();
+
+			case 100:
+				label = -1;
+			}
 		}
-
-		selenium.clickAt("//input[@value='Save']", RuntimeVariables.replace(""));
-		selenium.waitForPageToLoad("30000");
-		loadRequiredJavaScriptModules();
 	}
 }
