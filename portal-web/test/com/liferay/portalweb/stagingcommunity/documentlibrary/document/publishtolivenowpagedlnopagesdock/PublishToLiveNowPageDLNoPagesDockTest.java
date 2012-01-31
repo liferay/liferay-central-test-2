91e13cb2ea81b03462efe5eabd91108b240d09cb
Ken Duenwald <ken.duenwald@liferay.com>
2012-01-31 11:22:43 -0800

    Test Fixes - stagingcommunity/documentlibrary

--- portal-web/test/com/liferay/portalweb/stagingcommunity/documentlibrary/document/publishtolivenowpagedlnopagesdock/PublishToLiveNowPageDLNoPagesDockTest.java
+++ portal-web/test/com/liferay/portalweb/stagingcommunity/documentlibrary/document/publishtolivenowpagedlnopagesdock/PublishToLiveNowPageDLNoPagesDockTest.java
@@ -270,12 +270,31 @@ public class PublishToLiveNowPageDLNoPagesDockTest extends BaseTestCase {
 					Thread.sleep(1000);
 				}
 
-				selenium.click(RuntimeVariables.replace(
-						"//input[@value='Publish']"));
-				selenium.waitForPageToLoad("30000");
-				loadRequiredJavaScriptModules();
+				selenium.clickAt("//input[@value='Publish']",
+					RuntimeVariables.replace("Publish"));
 				assertTrue(selenium.getConfirmation()
 								   .matches("^Are you sure you want to publish these pages[\\s\\S]$"));
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (selenium.isVisible(
+									"//div[@class='portlet-msg-success']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				assertEquals(RuntimeVariables.replace(
+						"Your request completed successfully."),
+					selenium.getText("//div[@class='portlet-msg-success']"));
 				selenium.open("/web/site-name-staging/");
 				loadRequiredJavaScriptModules();
 
