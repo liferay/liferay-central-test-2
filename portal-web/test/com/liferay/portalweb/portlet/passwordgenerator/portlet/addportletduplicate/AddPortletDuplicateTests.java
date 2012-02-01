c944117ec18546fd5da6359ac7ea36dbec585e58
Chas Austin <charles.austin@liferay.com>
2012-02-01 11:14:16 -0800

    Test Fixes - portlet/passwordgenerator

--- portal-web/test/com/liferay/portalweb/portlet/passwordgenerator/portlet/addportletduplicate/AddPortletDuplicateTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/passwordgenerator/portlet/addportletduplicate/AddPortletDuplicateTests.java
@@ -16,6 +16,8 @@ package com.liferay.portalweb.portlet.passwordgenerator.portlet.addportletduplic
 
 import com.liferay.portalweb.portal.BaseTestSuite;
 import com.liferay.portalweb.portal.util.TearDownPageTest;
+import com.liferay.portalweb.portlet.passwordgenerator.portlet.addportlet.AddPagePGTest;
+import com.liferay.portalweb.portlet.passwordgenerator.portlet.addportlet.AddPortletPGTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
