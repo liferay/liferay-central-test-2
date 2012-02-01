baeb66bd141ceecb1784abba3966c85e213ba84a
Michael Hashimoto <michael.hashimoto@liferay.com>
2012-01-31 17:48:39 -0800

    Remove duplicate files

--- portal-web/test/com/liferay/portalweb/portlet/blogsaggregator/portlet/configureportletdisplaystyletitle/ConfigurePortletDisplayStyleTitleTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/blogsaggregator/portlet/configureportletdisplaystyletitle/ConfigurePortletDisplayStyleTitleTests.java
@@ -16,6 +16,8 @@ package com.liferay.portalweb.portlet.blogsaggregator.portlet.configureportletdi
 
 import com.liferay.portalweb.portal.BaseTestSuite;
 import com.liferay.portalweb.portal.util.TearDownPageTest;
+import com.liferay.portalweb.portlet.blogsaggregator.blogsentry.viewblogsentryba.AddPageBATest;
+import com.liferay.portalweb.portlet.blogsaggregator.blogsentry.viewblogsentryba.AddPortletBATest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
