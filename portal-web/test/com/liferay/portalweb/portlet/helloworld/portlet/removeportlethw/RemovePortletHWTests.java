0e4b23c6e973c78da694233a573aed646416def6
Chas Austin <charles.austin@liferay.com>
2012-02-01 10:26:26 -0800

    Test Fixes - portlet/helloworld

--- portal-web/test/com/liferay/portalweb/portlet/helloworld/portlet/removeportlethw/RemovePortletHWTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/helloworld/portlet/removeportlethw/RemovePortletHWTests.java
@@ -16,6 +16,8 @@ package com.liferay.portalweb.portlet.helloworld.portlet.removeportlethw;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
 import com.liferay.portalweb.portal.util.TearDownPageTest;
+import com.liferay.portalweb.portlet.helloworld.portlet.addportlethw.AddPageHWTest;
+import com.liferay.portalweb.portlet.helloworld.portlet.addportlethw.AddPortletHWTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
