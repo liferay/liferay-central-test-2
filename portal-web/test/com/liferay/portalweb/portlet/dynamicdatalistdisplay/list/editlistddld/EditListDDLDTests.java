5afdcf974f5c60d8c8f5930ffebfd83a194d7691
Chas Austin <charles.austin@liferay.com>
2012-02-01 10:16:21 -0800

    Test Fixes - portlet/dynamicdatalistdisplay

--- portal-web/test/com/liferay/portalweb/portlet/dynamicdatalistdisplay/list/editlistddld/EditListDDLDTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/dynamicdatalistdisplay/list/editlistddld/EditListDDLDTests.java
@@ -16,6 +16,8 @@ package com.liferay.portalweb.portlet.dynamicdatalistdisplay.list.editlistddld;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
 import com.liferay.portalweb.portal.util.TearDownPageTest;
+import com.liferay.portalweb.portlet.dynamicdatalistdisplay.list.addlistddld.AddPageDDLDTest;
+import com.liferay.portalweb.portlet.dynamicdatalistdisplay.list.addlistddld.AddPortletDDLDTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
--- portal-web/test/com/liferay/portalweb/portlet/dynamicdatalistdisplay/record/editrecordddld/EditRecordDDLDTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/dynamicdatalistdisplay/record/editrecordddld/EditRecordDDLDTests.java
@@ -16,6 +16,8 @@ package com.liferay.portalweb.portlet.dynamicdatalistdisplay.record.editrecorddd
 
 import com.liferay.portalweb.portal.BaseTestSuite;
 import com.liferay.portalweb.portal.util.TearDownPageTest;
+import com.liferay.portalweb.portlet.dynamicdatalistdisplay.list.addlistddld.AddPageDDLDTest;
+import com.liferay.portalweb.portlet.dynamicdatalistdisplay.list.addlistddld.AddPortletDDLDTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
