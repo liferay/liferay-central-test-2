8adfe757e3a9136a7b0074a8fe4a09a0471c7b7b
Chas Austin <charles.austin@liferay.com>
2012-02-01 11:49:46 -0800

    Test Fixes - portlet/sitemap

--- portal-web/test/com/liferay/portalweb/portlet/sitemap/portlet/removeportlet/RemovePortletTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/sitemap/portlet/removeportlet/RemovePortletTests.java
@@ -16,6 +16,8 @@ package com.liferay.portalweb.portlet.sitemap.portlet.removeportlet;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
 import com.liferay.portalweb.portal.util.TearDownPageTest;
+import com.liferay.portalweb.portlet.sitemap.portlet.addportlet.AddPageSMTest;
+import com.liferay.portalweb.portlet.sitemap.portlet.addportlet.AddPortletSMTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
