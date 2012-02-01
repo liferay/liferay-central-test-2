edd730d0d795271bdc853981bb793341cbf140a3
Chas Austin <charles.austin@liferay.com>
2012-02-01 10:13:16 -0800

    Test Fixes - portlet/directory

--- portal-web/test/com/liferay/portalweb/portlet/directory/usergroup/viewusergroupuser/ViewUserGroupUserTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/directory/usergroup/viewusergroupuser/ViewUserGroupUserTests.java
@@ -16,6 +16,8 @@ package com.liferay.portalweb.portlet.directory.usergroup.viewusergroupuser;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
 import com.liferay.portalweb.portal.util.TearDownPageTest;
+import com.liferay.portalweb.portlet.directory.organizations.advancedsearchorganization.AddPageDirectoryTest;
+import com.liferay.portalweb.portlet.directory.organizations.advancedsearchorganization.AddPortletDirectoryTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
