cc667646a9eacbf7c63f6c4246387af2e272d86f
Chas Austin <charles.austin@liferay.com>
2012-02-01 11:33:36 -0800

    Test Fixes - portlet/search

--- portal-web/test/com/liferay/portalweb/portlet/search/portlet/addportletduplicate/AddPortletDuplicateTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/search/portlet/addportletduplicate/AddPortletDuplicateTests.java
@@ -16,6 +16,8 @@ package com.liferay.portalweb.portlet.search.portlet.addportletduplicate;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
 import com.liferay.portalweb.portal.util.TearDownPageTest;
+import com.liferay.portalweb.portlet.search.portlet.addportlet.AddPageSearchTest;
+import com.liferay.portalweb.portlet.search.portlet.addportlet.AddPortletSearchTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
