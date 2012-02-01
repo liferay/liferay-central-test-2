8bf5cc159553344821bbc81c39a320975348afab
Chas Austin <charles.austin@liferay.com>
2012-02-01 10:51:13 -0800

    Test Fixes - portlet/nestedportlets

--- portal-web/test/com/liferay/portalweb/portlet/nestedportlets/portlet/removeportlet/RemovePortletTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/nestedportlets/portlet/removeportlet/RemovePortletTests.java
@@ -16,6 +16,8 @@ package com.liferay.portalweb.portlet.nestedportlets.portlet.removeportlet;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
 import com.liferay.portalweb.portal.util.TearDownPageTest;
+import com.liferay.portalweb.portlet.nestedportlets.portlet.addportlet.AddPageNPTest;
+import com.liferay.portalweb.portlet.nestedportlets.portlet.addportlet.AddPortletNPTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
