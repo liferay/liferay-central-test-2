72ddff928e59cd1cdc687c026aa8b69ff358a122
Chas Austin <charles.austin@liferay.com>
2012-02-01 11:59:02 -0800

    Test Fixes - portlet/unitconverter

--- portal-web/test/com/liferay/portalweb/portlet/unitconverter/unit/convertunit/ConvertUnitTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/unitconverter/unit/convertunit/ConvertUnitTests.java
@@ -16,6 +16,8 @@ package com.liferay.portalweb.portlet.unitconverter.unit.convertunit;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
 import com.liferay.portalweb.portal.util.TearDownPageTest;
+import com.liferay.portalweb.portlet.unitconverter.portlet.addportlet.AddPageUCTest;
+import com.liferay.portalweb.portlet.unitconverter.portlet.addportlet.AddPortletUCTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
