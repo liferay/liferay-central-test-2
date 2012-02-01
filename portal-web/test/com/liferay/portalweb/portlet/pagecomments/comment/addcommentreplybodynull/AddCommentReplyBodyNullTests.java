1f3a9a1faad743f4213e1bb9cbf9fe6166448b3a
Chas Austin <charles.austin@liferay.com>
2012-02-01 11:00:41 -0800

    Test Fixes - portlet/pagecomments

--- portal-web/test/com/liferay/portalweb/portlet/pagecomments/comment/addcommentreplybodynull/AddCommentReplyBodyNullTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/pagecomments/comment/addcommentreplybodynull/AddCommentReplyBodyNullTests.java
@@ -16,6 +16,8 @@ package com.liferay.portalweb.portlet.pagecomments.comment.addcommentreplybodynu
 
 import com.liferay.portalweb.portal.BaseTestSuite;
 import com.liferay.portalweb.portal.util.TearDownPageTest;
+import com.liferay.portalweb.portlet.pagecomments.portlet.addportlet.AddPagePCTest;
+import com.liferay.portalweb.portlet.pagecomments.portlet.addportlet.AddPortletPCTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
