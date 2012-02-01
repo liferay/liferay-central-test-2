d3bd50dae938251e1a4b8bfb7cf6219f89ede793
Chas Austin <charles.austin@liferay.com>
2012-02-01 11:18:56 -0800

    Test Fixes - portlet/pollsdisplay

--- portal-web/test/com/liferay/portalweb/portlet/pollsdisplay/portlet/addportletmultiple/AddPortletMultipleTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/pollsdisplay/portlet/addportletmultiple/AddPortletMultipleTests.java
@@ -16,6 +16,7 @@ package com.liferay.portalweb.portlet.pollsdisplay.portlet.addportletmultiple;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
 import com.liferay.portalweb.portal.util.TearDownPageTest;
+import com.liferay.portalweb.portlet.pollsdisplay.portlet.addportlet.AddPagePDTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
--- portal-web/test/com/liferay/portalweb/portlet/pollsdisplay/portlet/removeportlet/RemovePortletTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/pollsdisplay/portlet/removeportlet/RemovePortletTests.java
@@ -16,6 +16,8 @@ package com.liferay.portalweb.portlet.pollsdisplay.portlet.removeportlet;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
 import com.liferay.portalweb.portal.util.TearDownPageTest;
+import com.liferay.portalweb.portlet.pollsdisplay.portlet.addportlet.AddPagePDTest;
+import com.liferay.portalweb.portlet.pollsdisplay.portlet.addportlet.AddPortletPDTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
