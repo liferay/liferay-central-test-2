a1202fe49014a04ab57a6ea0a49735f8a7a764d3
Chas Austin <charles.austin@liferay.com>
2012-02-01 12:09:05 -0800

    Test Fixes - portlet/words

--- portal-web/test/com/liferay/portalweb/portlet/words/portlet/addportletwordsduplicate/AddPortletWordsDuplicateTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/words/portlet/addportletwordsduplicate/AddPortletWordsDuplicateTests.java
@@ -16,6 +16,8 @@ package com.liferay.portalweb.portlet.words.portlet.addportletwordsduplicate;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
 import com.liferay.portalweb.portal.util.TearDownPageTest;
+import com.liferay.portalweb.portlet.words.portlet.addportletwords.AddPageWordsTest;
+import com.liferay.portalweb.portlet.words.portlet.addportletwords.AddPortletWordsTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
