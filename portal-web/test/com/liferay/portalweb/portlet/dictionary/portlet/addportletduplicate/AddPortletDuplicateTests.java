6519ee2b34508a1878d1d70deb02935c7fe63a17
Chas Austin <charles.austin@liferay.com>
2012-02-01 10:08:05 -0800

    Test Fixes - portlet/dictionary

--- portal-web/test/com/liferay/portalweb/portlet/dictionary/portlet/addportletduplicate/AddPortletDuplicateTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/dictionary/portlet/addportletduplicate/AddPortletDuplicateTests.java
@@ -16,6 +16,8 @@ package com.liferay.portalweb.portlet.dictionary.portlet.addportletduplicate;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
 import com.liferay.portalweb.portal.util.TearDownPageTest;
+import com.liferay.portalweb.portlet.dictionary.portlet.addportlet.AddPageDictionaryTest;
+import com.liferay.portalweb.portlet.dictionary.portlet.addportlet.AddPortletDictionaryTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
