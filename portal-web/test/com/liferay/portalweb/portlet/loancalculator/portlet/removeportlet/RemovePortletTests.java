9f7d1b5fb9bcff9feb24692acd9b2de561328ac8
Chas Austin <charles.austin@liferay.com>
2012-02-01 10:42:32 -0800

    Test Fixes - portlet/loancalculator

--- portal-web/test/com/liferay/portalweb/portlet/loancalculator/portlet/removeportlet/RemovePortletTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/loancalculator/portlet/removeportlet/RemovePortletTests.java
@@ -16,6 +16,8 @@ package com.liferay.portalweb.portlet.loancalculator.portlet.removeportlet;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
 import com.liferay.portalweb.portal.util.TearDownPageTest;
+import com.liferay.portalweb.portlet.loancalculator.portlet.addportlet.AddPageLCTest;
+import com.liferay.portalweb.portlet.loancalculator.portlet.addportlet.AddPortletLCTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
