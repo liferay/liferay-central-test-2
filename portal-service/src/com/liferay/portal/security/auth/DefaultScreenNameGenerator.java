054b916750f2794d8a30303ceee68d5ee7737596
Brian Chan <brian.chan@liferay.com>
2012-02-07 12:25:38 -0800

    LPS-25399 Fix wrong logic

--- portal-service/src/com/liferay/portal/security/auth/DefaultScreenNameGenerator.java
+++ portal-service/src/com/liferay/portal/security/auth/DefaultScreenNameGenerator.java
@@ -46,10 +46,10 @@ public class DefaultScreenNameGenerator implements ScreenNameGenerator {
 				emailAddress, CharPool.AT).toLowerCase();
 
 			for (char c : screenName.toCharArray()) {
-				if ((!Validator.isChar(c) && (c != CharPool.DASH) &&
-					(c != CharPool.PERIOD)) ||
-					(!_USERS_SCREEN_NAME_ALLOW_NUMERIC &&
-						Validator.isDigit(c))) {
+				if ((!_USERS_SCREEN_NAME_ALLOW_NUMERIC &&
+					 Validator.isDigit(c)) ||
+					(!Validator.isChar(c) && (c != CharPool.DASH) &&
+					 (c != CharPool.PERIOD)) {
 
 					screenName = StringUtil.replace(
 						screenName, c, CharPool.PERIOD);
