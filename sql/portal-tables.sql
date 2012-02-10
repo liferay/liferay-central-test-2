9add058411dfb1c312a58b6793627b7b709d77e4
Alexander Chow <alexander.chow@liferay.com>
2012-02-10 11:30:50 +0000

    LPS-25483 Remove Legacy "Vocabulary" table from portal-tables.sq

--- sql/portal-tables.sql
+++ sql/portal-tables.sql
@@ -2147,19 +2147,6 @@ create table VirtualHost (
 	hostname VARCHAR(75) null
 );
 
-create table Vocabulary (
-	vocabularyId LONG not null primary key,
-	groupId LONG,
-	companyId LONG,
-	userId LONG,
-	userName VARCHAR(75) null,
-	createDate DATE null,
-	modifiedDate DATE null,
-	name VARCHAR(75) null,
-	description VARCHAR(75) null,
-	folksonomy BOOLEAN
-);
-
 create table WebDAVProps (
 	webDavPropsId LONG not null primary key,
 	companyId LONG,
