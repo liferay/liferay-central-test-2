f226331f88bd1445aa33e3321d75eb462466caf2
Brian Chan <brian.chan@liferay.com>
2012-02-15 09:38:34 -0800

    LPS-25575 Source formatting

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -5393,14 +5393,15 @@ public class PortalImpl implements Portal {
 
 			String redirectPath = HttpUtil.getPath(redirect);
 
-			int pos1 = redirect.indexOf(redirectPath);
-
-			String prefix = redirect.substring(0, pos1);
-			String queryString = HttpUtil.getQueryString(redirect);
-
 			if (!redirectPath.endsWith(oldPath)) {
 				return redirect;
 			}
+
+			int pos = redirect.indexOf(redirectPath);
+
+			String prefix = redirect.substring(0, pos);
+
+			String queryString = HttpUtil.getQueryString(redirect);
 
 			String redirectParam = HttpUtil.getParameter(
 				redirect, "redirect", false);
@@ -5409,13 +5410,14 @@ public class PortalImpl implements Portal {
 				String newRedirectParam = StringUtil.replace(
 					redirectParam, HttpUtil.encodeURL(oldPath),
 					HttpUtil.encodeURL(newPath));
+
 				queryString = StringUtil.replace(
 					queryString, redirectParam, newRedirectParam);
 			}
 
-			int pos2 = redirectPath.lastIndexOf(oldPath);
+			pos = redirectPath.lastIndexOf(oldPath);
 
-			prefix += redirectPath.substring(0, pos2);
+			prefix += redirectPath.substring(0, pos);
 
 			redirect = prefix + newPath;
 
