2a0dfb179a3d355004ad6966a0ce9a1dfe107858
Miguel Pastor <miguel.pastor@liferay.com>
2012-02-21 13:10:40 +0100

    LPS-25663 Unit testing and integration testing examples. This is an integration test using the Liferay Junit runner. This test does not need to use the transactional listener support, only populate the application context

--- portal-impl/test/com/liferay/portlet/wiki/engine/creole/TranslationToXHTMLTests.java
+++ portal-impl/test/com/liferay/portlet/wiki/engine/creole/TranslationToXHTMLTests.java
@@ -16,234 +16,280 @@ package com.liferay.portlet.wiki.engine.creole;
 
 import com.liferay.portal.parsers.creole.visitor.impl.XhtmlTranslationVisitor;
 
+import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
 import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * @author Miguel Pastor
  */
+@RunWith(LiferayIntegrationJUnitTestRunner.class)
 public class TranslationToXHTMLTests extends AbstractWikiParserTests {
 
-	public void testEscapedEscapedCharacter() {
+	@Test
+	public void escapedEscapedCharacter() {
 		Assert.assertEquals(
 			"<p>~&#034;~ is escaped&#034; </p>", translate("escape-2.creole"));
 	}
 
-	public void testParseCorrectlyBoldContentInListItems() {
+	@Test
+	public void parseCorrectlyBoldContentInListItems() {
 		Assert.assertEquals(
 			"<ul><li> <strong>abcdefg</strong></li></ul>",
 			translate("list-6.creole"));
 	}
 
-	public void testParseCorrectlyItalicContentInListItems() {
+	@Test
+	public void parseCorrectlyItalicContentInListItems() {
 		Assert.assertEquals(
 			"<ul><li> <em>abcdefg</em></li></ul>", translate("list-5.creole"));
 	}
 
-	public void testParseCorrectlyMixedHorizontalBlocks() {
+	@Test
+	public void parseCorrectlyMixedHorizontalBlocks() {
 		Assert.assertEquals(
 			"<h1>Before Horizontal section</h1><hr/><pre>\tNo wiki section " +
 				"after Horizontal section</pre>",
 			translate("horizontal-3.creole"));
 	}
 
-	public void testParseCorrectlyMultipleHeadingBlocks() {
+	@Test
+	public void parseCorrectlyMultipleHeadingBlocks() {
 		Assert.assertEquals(
 			"<h1>Level 1</h1><h2>Level 2</h2><h3>Level 3</h3>",
 			translate("heading-10.creole"));
 	}
 
-	public void testParseCorrectlyNoClosedFirstHeadingBlock() {
+	@Test
+	public void parseCorrectlyNoClosedFirstHeadingBlock() {
 		Assert.assertEquals(
 			"<h1>This is a non closed heading</h1>",
 			translate("heading-3.creole"));
 	}
 
-	public void testParseCorrectlyNoClosedSecondHeadingBlock() {
+	@Test
+	public void parseCorrectlyNoClosedSecondHeadingBlock() {
 		Assert.assertEquals(
 			"<h2>This is a non closed heading</h2>",
 			translate("heading-6.creole"));
 	}
 
-	public void testParseCorrectlyNoClosedThirdHeadingBlock() {
+	@Test
+	public void parseCorrectlyNoClosedThirdHeadingBlock() {
 		Assert.assertEquals("<h3>Level 3</h3>", translate("heading-7.creole"));
 	}
 
-	public void testParseCorrectlyNoWikiBlockWitBraces() {
+	@Test
+	public void parseCorrectlyNoWikiBlockWitBraces() {
 		Assert.assertEquals(
 			"<pre>{\nfoo\n}\n</pre>", translate("nowikiblock-7.creole"));
 	}
 
-	public void testParseCorrectlyNoWikiBlockWitMultipleAndText() {
+	@Test
+	public void parseCorrectlyNoWikiBlockWitMultipleAndText() {
 		Assert.assertEquals(
 			"<pre>public interface Foo {\nvoid foo();\n}\n</pre><p>Outside " +
 				"preserve </p>",
 			translate("nowikiblock-9.creole"));
 	}
 
-	public void testParseCorrectlyNoWikiBlockWitMultipleBraces() {
+	@Test
+	public void parseCorrectlyNoWikiBlockWitMultipleBraces() {
 		Assert.assertEquals(
 			"<pre>public interface Foo {\nvoid foo();\n}\n</pre>",
 			translate("nowikiblock-8.creole"));
 	}
 
-	public void testParseCorrectlyOneEmptyFirstHeadingBlock() {
+	@Test
+	public void parseCorrectlyOneEmptyFirstHeadingBlock() {
 		Assert.assertEquals("<h1>  </h1>", translate("heading-2.creole"));
 	}
 
-	public void testParseCorrectlyOneEmptyNoWikiBlock() {
+	@Test
+	public void parseCorrectlyOneEmptyNoWikiBlock() {
 		Assert.assertEquals("<pre></pre>", translate("nowikiblock-3.creole"));
 	}
 
-	public void testParseCorrectlyOneEmptySecondHeadingBlock() {
+	@Test
+	public void parseCorrectlyOneEmptySecondHeadingBlock() {
 		Assert.assertEquals("<h2>  </h2>", translate("heading-5.creole"));
 	}
 
-	public void testParseCorrectlyOneEmptyThirdHeadingBlock() {
+	@Test
+	public void parseCorrectlyOneEmptyThirdHeadingBlock() {
 		Assert.assertEquals("<h3>  </h3>", translate("heading-8.creole"));
 	}
 
-	public void testParseCorrectlyOneHorizontalBlocks() {
+	@Test
+	public void parseCorrectlyOneHorizontalBlocks() {
 		Assert.assertEquals("<hr/>", translate("horizontal-1.creole"));
 	}
 
-	public void testParseCorrectlyOneItemFirstLevel() {
+	@Test
+	public void parseCorrectlyOneItemFirstLevel() {
 		Assert.assertEquals(
 			"<ul><li>ABCDEFG</li></ul>", translate("list-1.creole"));
 	}
 
-	public void testParseCorrectlyOneNonEmptyFirstHeadingBlock() {
+	@Test
+	public void parseCorrectlyOneNonEmptyFirstHeadingBlock() {
 		Assert.assertEquals(
 			"<h1> Level 1 (largest) </h1>", translate("heading-1.creole"));
 	}
 
-	public void testParseCorrectlyOneNonEmptyNoWikiBlock() {
+	@Test
+	public void parseCorrectlyOneNonEmptyNoWikiBlock() {
 		Assert.assertEquals(
 			"<pre>This is a non \\empty\\ block</pre>",
 			translate("nowikiblock-4.creole"));
 	}
 
-	public void testParseCorrectlyOneNonEmptyNoWikiBlockWitBraces() {
+	@Test
+	public void parseCorrectlyOneNonEmptyNoWikiBlockWitBraces() {
 		Assert.assertEquals(
 			"<p>Preserving </p><pre>.lfr-helper{span}</pre>",
 			translate("nowikiblock-6.creole"));
 	}
 
-	public void testParseCorrectlyOneNonEmptyNoWikiBlockWitMultipleLines() {
+	@Test
+	public void parseCorrectlyOneNonEmptyNoWikiBlockWitMultipleLines() {
 		Assert.assertEquals(
 			"<pre>Multiple\nlines</pre>", translate("nowikiblock-5.creole"));
 	}
 
-	public void testParseCorrectlyOneNonEmptySecondHeadingBlock() {
+	@Test
+	public void parseCorrectlyOneNonEmptySecondHeadingBlock() {
 		Assert.assertEquals("<h2>Level 2</h2>", translate("heading-4.creole"));
 	}
 
-	public void testParseCorrectlyOneNonEmptyThirdHeadingBlock() {
+	@Test
+	public void parseCorrectlyOneNonEmptyThirdHeadingBlock() {
 		Assert.assertEquals(
 			"<h3>This is a non closed heading</h3>",
 			translate("heading-9.creole"));
 	}
 
-	public void testParseCorrectlyOneOrderedItemFirstLevel() {
+	@Test
+	public void parseCorrectlyOneOrderedItemFirstLevel() {
 		Assert.assertEquals(
 			"<ol><li>ABCDEFG</li></ol>", translate("list-7.creole"));
 	}
 
-	public void testParseCorrectlyOrderedNestedLevels() {
+	@Test
+	public void parseCorrectlyOrderedNestedLevels() {
 		Assert.assertEquals(
 			"<ol><li>a</li><ol><li>a.1</li></ol><li>b</li><ol><li>b.1</li>" +
 				"<li>b.2</li><li>b.3</li></ol><li>c</li></ol>",
 			translate("list-10.creole"));
 	}
 
-	public void testParseCorrectlyThreeItemFirstLevel() {
+	@Test
+	public void parseCorrectlyThreeItemFirstLevel() {
 		Assert.assertEquals(
 			"<ul><li>1</li><li>2</li><li>3</li></ul>",
 			translate("list-3.creole"));
 	}
 
-	public void testParseCorrectlyThreeNoWikiBlock() {
+	@Test
+	public void parseCorrectlyThreeNoWikiBlock() {
 		Assert.assertEquals(
 			"<pre>1111</pre><pre>2222</pre><pre>3333</pre>",
 			translate("nowikiblock-2.creole"));
 	}
 
-	public void testParseCorrectlyThreeOrderedItemFirstLevel() {
+	@Test
+	public void parseCorrectlyThreeOrderedItemFirstLevel() {
 		Assert.assertEquals(
 			"<ol><li>1</li><li>2</li><li>3</li></ol>",
 			translate("list-9.creole"));
 	}
 
-	public void testParseCorrectlyTwoHorizontalBlocks() {
+	@Test
+	public void parseCorrectlyTwoHorizontalBlocks() {
 		Assert.assertEquals("<hr/><hr/>", translate("horizontal-2.creole"));
 	}
 
-	public void testParseCorrectlyTwoItemFirstLevel() {
+	@Test
+	public void parseCorrectlyTwoItemFirstLevel() {
 		Assert.assertEquals(
 			"<ul><li>1</li><li>2</li></ul>", translate("list-2.creole"));
 	}
 
-	public void testParseCorrectlyTwoOrderedItemFirstLevel() {
+	@Test
+	public void parseCorrectlyTwoOrderedItemFirstLevel() {
 		Assert.assertEquals(
 			"<ol><li>1</li><li>2</li></ol>", translate("list-8.creole"));
 	}
 
-	public void testParseEmpyImageTag() {
+	@Test
+	public void parseEmpyImageTag() {
 		Assert.assertEquals(
 			"<p><img src=\"\" /> </p>", translate("image-4.creole"));
 	}
 
-	public void testParseImageAndTextInListItem() {
+	@Test
+	public void parseImageAndTextInListItem() {
 		Assert.assertEquals(
 			"<ul><li><img src=\"imageLink\" alt=\"altText\"/> end.</li></ul>",
 			translate("list-17.creole"));
 	}
 
-	public void testParseImageInListItem() {
+	@Test
+	public void parseImageInListItem() {
 		Assert.assertEquals(
 			"<ul><li><img src=\"imageLink\" alt=\"altText\"/></li></ul>",
 			translate("list-16.creole"));
 	}
 
-	public void testParseLinkEmpty() {
+	@Test
+	public void parseLinkEmpty() {
 		Assert.assertEquals("<p> </p>", translate("link-8.creole"));
 	}
 
-	public void testParseLinkEmptyInHeader() {
+	@Test
+	public void parseLinkEmptyInHeader() {
 		Assert.assertEquals("<h2>  </h2>", translate("link-9.creole"));
 	}
 
-	public void testParseLinkInListItem() {
+	@Test
+	public void parseLinkInListItem() {
 		Assert.assertEquals(
 			"<ul><li><a href=\"l\">a</a></li></ul>",
 			translate("list-13.creole"));
 	}
 
-	public void testParseLinkInListItemMixedText() {
+	@Test
+	public void parseLinkInListItemMixedText() {
 		Assert.assertEquals(
 			"<ul><li>This is an item with a link <a href=\"l\">a</a> inside " +
 				"text</li></ul>",
 			translate("list-12.creole"));
 	}
 
-	public void testParseLinkInListItemWithPreText() {
+	@Test
+	public void parseLinkInListItemWithPreText() {
 		Assert.assertEquals(
 			"<ul><li>This is an item with a link <a href=\"l\">a</a></li></ul>",
 			translate("list-11.creole"));
 	}
 
-	public void testParseLinkWithNoAlt() {
+	@Test
+	public void parseLinkWithNoAlt() {
 		Assert.assertEquals(
 			"<p><a href=\"Link\">Link</a> </p>", translate("link-7.creole"));
 	}
 
-	public void testParseMultilineTextParagraph() {
+	@Test
+	public void parseMultilineTextParagraph() {
 		Assert.assertEquals(
 			"<p>Simple P0 Simple P1 Simple P2 Simple P3 Simple P4 Simple P5 " +
 				"Simple P6 Simple P7 Simple P8 Simple P9 </p>",
 			translate("text-2.creole"));
 	}
 
-	public void testParseMultipleImageTags() {
+	@Test
+	public void parseMultipleImageTags() {
 		Assert.assertEquals(
 			"<p><img src=\"L1\" alt=\"A1\"/><img src=\"L2\" alt=\"A2\"/><img " +
 				"src=\"L3\" alt=\"A3\"/><img src=\"L4\" alt=\"A4\"/><img " +
@@ -251,14 +297,16 @@ public class TranslationToXHTMLTests extends AbstractWikiParserTests {
 			translate("image-5.creole"));
 	}
 
-	public void testParseMultipleLinkTags() {
+	@Test
+	public void parseMultipleLinkTags() {
 		Assert.assertEquals(
 			"<p><a href=\"L\">A</a> <a href=\"L\">A</a> <a href=\"L\">A</a> " +
 				"</p>",
 			translate("link-3.creole"));
 	}
 
-	public void testParseNestedLists() {
+	@Test
+	public void parseNestedLists() {
 		Assert.assertEquals(
 			"<ul><li> 1</li><li> 2</li><ul><li> 2.1</li><ul><li> 2.1.1</li>" +
 				"<ul><li> 2.1.1.1</li><li> 2.1.1.2</li></ul><li> 2.1.2</li>" +
@@ -267,20 +315,23 @@ public class TranslationToXHTMLTests extends AbstractWikiParserTests {
 			translate("list-18.creole"));
 	}
 
-	public void testParseNoWikiAndTextInListItem() {
+	@Test
+	public void parseNoWikiAndTextInListItem() {
 		Assert.assertEquals(
 			"<ul><li><pre>This is nowiki inside a list item</pre> and <em>" +
 				"italics</em></li></ul>",
 			translate("list-15.creole"));
 	}
 
-	public void testParseNoWikiInListItem() {
+	@Test
+	public void parseNoWikiInListItem() {
 		Assert.assertEquals(
 			"<ul><li><pre>This is nowiki inside a list item</pre></li></ul>",
 			translate("list-14.creole"));
 	}
 
-	public void testParseOnlySpacesContentInImageTag() {
+	@Test
+	public void parseOnlySpacesContentInImageTag() {
 		Assert.assertEquals(
 			"<p><img src=\"L1\" alt=\"A1\"/><img src=\"L2\" alt=\"A2\"/>"  +
 				"<img src=\"L3\" alt=\"A3\"/><img src=\"L4\" alt=\"A4\"/>" +
@@ -288,65 +339,76 @@ public class TranslationToXHTMLTests extends AbstractWikiParserTests {
 			translate("image-5.creole"));
 	}
 
-	public void testParseSimpleImageTag() {
+	@Test
+	public void parseSimpleImageTag() {
 		Assert.assertEquals(
 			"<p><img src=\"link\" alt=\"alternative text\"/> </p>",
 			translate("image-1.creole"));
 	}
 
-	public void testParseSimpleImageTagWithNoAlternative() {
+	@Test
+	public void parseSimpleImageTagWithNoAlternative() {
 		Assert.assertEquals(
 			"<p><img src=\"link\" /> </p>", translate("image-2.creole"));
 	}
 
-	public void testParseSimpleLinkTag() {
+	@Test
+	public void parseSimpleLinkTag() {
 		Assert.assertEquals(
 			"<p><a href=\"link\">alternative text</a> </p>",
 			translate("link-1.creole"));
 	}
 
-	public void testParseSimpleLinkTagWithoutDescription() {
+	@Test
+	public void parseSimpleLinkTagWithoutDescription() {
 		Assert.assertEquals(
 			"<p><a href=\"link\">link</a> </p>", translate("link-2.creole"));
 	}
 
-	public void testParseSimpleTextBoldAndItalics() {
+	@Test
+	public void parseSimpleTextBoldAndItalics() {
 		Assert.assertEquals(
 			"<p>Text <strong><em>ItalicAndBold</em></strong> </p>",
 			translate("text-6.creole"));
 	}
 
-	public void testParseSimpleTextParagraph() {
+	@Test
+	public void parseSimpleTextParagraph() {
 		Assert.assertEquals(
 			"<p>Simple paragraph </p>", translate("text-1.creole"));
 	}
 
-	public void testParseSimpleTextWithBold() {
+	@Test
+	public void parseSimpleTextWithBold() {
 		Assert.assertEquals(
 			"<p>Text with some content in <strong>bold</strong> </p>",
 			translate("text-4.creole"));
 	}
 
-	public void testParseSimpleTextWithBoldAndItalics() {
+	@Test
+	public void parseSimpleTextWithBoldAndItalics() {
 		Assert.assertEquals(
 			"<p>Text with some content in <strong>bold</strong> and with " +
 				"some content in <em>italic</em> </p>",
 			translate("text-5.creole"));
 	}
 
-	public void testParseSimpleTextWithForcedEndline() {
+	@Test
+	public void parseSimpleTextWithForcedEndline() {
 		Assert.assertEquals(
 			"<p>Text with <br/>forced line break </p>",
 			translate("text-7.creole"));
 	}
 
-	public void testParseSimpleTextWithItalics() {
+	@Test
+	public void parseSimpleTextWithItalics() {
 		Assert.assertEquals(
 			"<p>Text with some content in <em>italic</em> </p>",
 			translate("text-3.creole"));
 	}
 
-	public void testParseTableMultipleRowsAndCOlumns() {
+	@Test
+	public void parseTableMultipleRowsAndCOlumns() {
 		Assert.assertEquals(
 			"<table><tr><th>H1</th><th>H2</th><th>H3</th><th>H4</th></tr>" +
 				"<tr><td>C1</td><td>C2</td><td>C3</td><td>C4</td></tr><tr>" +
@@ -356,22 +418,25 @@ public class TranslationToXHTMLTests extends AbstractWikiParserTests {
 			translate("table-2.creole"));
 	}
 
-	public void testParseTableOneRowOneColumn() {
+	@Test
+	public void parseTableOneRowOneColumn() {
 		Assert.assertEquals(
 			"<table><tr><th>H1</th></tr><tr><td>C1.1</td></tr></table>",
 			translate("table-1.creole"));
 	}
 
-	public void testSimpleEscapedCharacter() {
+	@Test
+	public void simpleEscapedCharacter() {
 		Assert.assertEquals(
 			"<p>ESCAPED1 Esto no está escaped </p>",
 			translate("escape-1.creole"));
 	}
 
-	public void testTranslateOneNoWikiBlock() {
+	@Test
+	public void translateOneNoWikiBlock() {
 		Assert.assertEquals(
-			"<pre>\t//This// does **not** get [[formatted]]</pre>",
-			translate("nowikiblock-1.creole"));
+				"<pre>\t//This// does **not** get [[formatted]]</pre>",
+				translate("nowikiblock-1.creole"));
 	}
 
 	protected String translate(String fileName) {
