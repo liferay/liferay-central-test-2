a822a0801abf787d70638fea84d9fd383f2162ad
Miguel Pastor <miguel.pastor@liferay.com>
2012-02-21 16:24:03 +0100

    LPS-25663 Unit testing and integration testing examples. More unit testing examples (no application context available) using PowerMock and Mockito

---
+++ portal-impl/test/com/liferay/portal/metadata/TikaRawMetadataProcessorTests.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.metadata;
+
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.util.PrefsPropsUtil;
+import com.liferay.portal.util.PropsValues;
+
+import java.io.InputStream;
+
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import org.xml.sax.ContentHandler;
+
+import static org.powermock.api.mockito.PowerMockito.*;
+
+/**
+ * @author Miguel Pastor
+ */
+@PrepareForTest(PrefsPropsUtil.class)
+@RunWith(PowerMockRunner.class)
+public class TikaRawMetadataProcessorTests {
+
+	@Test
+	public void extractMetadataFromInputStream() {
+
+		mockStatic(PrefsPropsUtil.class);
+
+		try {
+			when(PrefsPropsUtil.getBoolean(
+				PropsKeys.XUGGLER_ENABLED, PropsValues.XUGGLER_ENABLED)).
+					thenReturn(Boolean.FALSE);
+
+			ContentHandler contentHandler = Mockito.any(ContentHandler.class);
+			Metadata metadata = Mockito.any(Metadata.class);
+			InputStream inputStream = Mockito.any(InputStream.class);
+			ParseContext parseContext = Mockito.any(ParseContext.class);
+
+			_parser.parse(inputStream, contentHandler, metadata, parseContext);
+
+			doAnswer(new Answer<Object>() {
+				@Override
+				public Object answer(InvocationOnMock invocationOnMock)
+					throws Throwable {
+
+					return new Metadata();
+				}
+			}).when(_parser).parse(
+				inputStream, contentHandler, metadata, parseContext);
+
+			metadata = _tikaRawMetadataProcessor.extractMetadata(
+				"pdf", "application/pdf", inputStream);
+
+			Assert.assertNotNull(metadata);
+			Assert.assertEquals(0, metadata.size());
+
+		} catch (Exception e) {
+			Assert.fail("Unexpected error");
+		}
+	}
+
+	@Mock
+	private Parser _parser;
+
+	@InjectMocks
+	private TikaRawMetadataProcessor _tikaRawMetadataProcessor =
+		new TikaRawMetadataProcessor();
+
+}
