5e6c85d1d9a3dc147358d552b386b19310e11193
Minhchau Dang <mdang@mdang-latitude-e6510.home>
2012-02-22 13:17:50 -0800

    LPS-25679 Add findByMissingAction to reduce number of queries for VerifyPermission

--- portal-impl/src/com/liferay/portal/service/persistence/ResourceFinderImpl.java
+++ portal-impl/src/com/liferay/portal/service/persistence/ResourceFinderImpl.java
@@ -35,6 +35,9 @@ public class ResourceFinderImpl
 	public static final String FIND_BY_CONTAINER_RESOURCE =
 		ResourceFinder.class.getName() + ".findByContainerResource";
 
+	public static final String FIND_BY_MISSING_ACTION =
+		ResourceFinder.class.getName() + ".findByMissingAction";
+
 	public static final String FIND_BY_NAME =
 		ResourceFinder.class.getName() + ".findByName";
 
@@ -65,6 +68,35 @@ public class ResourceFinderImpl
 			QueryPos qPos = QueryPos.getInstance(q);
 
 			qPos.add(codeId);
+
+			return q.list(true);
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
+
+	public List<Resource> findByMissingAction(long codeId, String actionId)
+		throws SystemException {
+
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = CustomSQLUtil.get(FIND_BY_MISSING_ACTION);
+
+			SQLQuery q = session.createSQLQuery(sql);
+
+			q.addEntity("Resource_", ResourceImpl.class);
+
+			QueryPos qPos = QueryPos.getInstance(q);
+
+			qPos.add(codeId);
+			qPos.add(actionId);
 
 			return q.list(true);
 		}
