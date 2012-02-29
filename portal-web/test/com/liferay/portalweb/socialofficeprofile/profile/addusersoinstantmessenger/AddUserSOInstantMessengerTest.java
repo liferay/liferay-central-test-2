19fa68254a1ee815e68fca2c2630b04795be9235
Ken Duenwald <ken.duenwald@liferay.com>
2012-02-29 14:54:08 -0800

    Test Fixes - socialofficeprofile/profile

--- portal-web/test/com/liferay/portalweb/socialofficeprofile/profile/addusersoinstantmessenger/AddUserSOInstantMessengerTest.java
+++ portal-web/test/com/liferay/portalweb/socialofficeprofile/profile/addusersoinstantmessenger/AddUserSOInstantMessengerTest.java
@@ -31,7 +31,7 @@ public class AddUserSOInstantMessengerTest extends BaseTestCase {
 			}
 
 			try {
-				if (selenium.isVisible("link=Control Panel")) {
+				if (selenium.isElementPresent("link=Control Panel")) {
 					break;
 				}
 			}
