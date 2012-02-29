578f3fc455d4160c1dac6b9b10a17f8028405bcb
Ken Duenwald <ken.duenwald@liferay.com>
2012-02-29 09:27:43 -0800

    Test Fixes - stagingcommunity/documentlibrary

--- portal-web/test/com/liferay/portalweb/stagingcommunity/documentlibrary/document/adddldocumentsitestaginglocallivenodl/ActivateSiteStagingLocalLiveNoDLTest.java
+++ portal-web/test/com/liferay/portalweb/stagingcommunity/documentlibrary/document/adddldocumentsitestaginglocallivenodl/ActivateSiteStagingLocalLiveNoDLTest.java
@@ -37,7 +37,7 @@ public class ActivateSiteStagingLocalLiveNoDLTest extends BaseTestCase {
 					}
 
 					try {
-						if (selenium.isVisible("link=Control Panel")) {
+						if (selenium.isElementPresent("link=Control Panel")) {
 							break;
 						}
 					}
--- portal-web/test/com/liferay/portalweb/stagingcommunity/documentlibrary/document/viewactivatedeactivatesitestaginglocallivedl/ActivateDeactivateSiteStagingLocalLiveDLTest.java
+++ portal-web/test/com/liferay/portalweb/stagingcommunity/documentlibrary/document/viewactivatedeactivatesitestaginglocallivedl/ActivateDeactivateSiteStagingLocalLiveDLTest.java
@@ -37,7 +37,7 @@ public class ActivateDeactivateSiteStagingLocalLiveDLTest extends BaseTestCase {
 					}
 
 					try {
-						if (selenium.isVisible("link=Control Panel")) {
+						if (selenium.isElementPresent("link=Control Panel")) {
 							break;
 						}
 					}
