c365d932a2403d18907f310e8a2b5f65f96b40e4
Brian Chiu <brian.chiu@liferay.com>
2012-03-01 17:35:34 -0800

    Test Fixes - portal/dbupgrade/transfersampledatalatest/blogs

--- portal-web/test/com/liferay/portalweb/portal/dbupgrade/transfersampledatalatest/blogs/pagescopelarcp/AddPage2BlogsPageScopeTest.java
+++ portal-web/test/com/liferay/portalweb/portal/dbupgrade/transfersampledatalatest/blogs/pagescopelarcp/AddPage2BlogsPageScopeTest.java
@@ -94,7 +94,7 @@ public class AddPage2BlogsPageScopeTest extends BaseTestCase {
 			}
 
 			try {
-				if (selenium.isVisible("//input[@id='_156_name_en_US']")) {
+				if (selenium.isVisible("//input[@name='_156_name_en_US']")) {
 					break;
 				}
 			}
@@ -104,7 +104,7 @@ public class AddPage2BlogsPageScopeTest extends BaseTestCase {
 			Thread.sleep(1000);
 		}
 
-		selenium.type("//input[@id='_156_name_en_US']",
+		selenium.type("//input[@name='_156_name_en_US']",
 			RuntimeVariables.replace("Blogs Test Page2"));
 		selenium.clickAt("//input[@value='Add Page']",
 			RuntimeVariables.replace("Add Page"));
