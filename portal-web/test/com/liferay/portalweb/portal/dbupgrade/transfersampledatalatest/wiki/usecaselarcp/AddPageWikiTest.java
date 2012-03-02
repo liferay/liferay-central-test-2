29c4a016519cfb07024b4ed6ad0c17db6ea2134b
Brian Chiu <brian.chiu@liferay.com>
2012-03-01 17:36:40 -0800

    Test Fixes - portal/dbupgrade/transfersampledatalatest/wiki

--- portal-web/test/com/liferay/portalweb/portal/dbupgrade/transfersampledatalatest/wiki/usecaselarcp/AddPageWikiTest.java
+++ portal-web/test/com/liferay/portalweb/portal/dbupgrade/transfersampledatalatest/wiki/usecaselarcp/AddPageWikiTest.java
@@ -94,7 +94,7 @@ public class AddPageWikiTest extends BaseTestCase {
 			}
 
 			try {
-				if (selenium.isVisible("//input[@id='_156_name_en_US']")) {
+				if (selenium.isVisible("//input[@name='_156_name_en_US']")) {
 					break;
 				}
 			}
@@ -104,7 +104,7 @@ public class AddPageWikiTest extends BaseTestCase {
 			Thread.sleep(1000);
 		}
 
-		selenium.type("//input[@id='_156_name_en_US']",
+		selenium.type("//input[@name='_156_name_en_US']",
 			RuntimeVariables.replace("Wiki Test Page"));
 		selenium.clickAt("//input[@value='Add Page']",
 			RuntimeVariables.replace("Add Page"));
