419a28be236d5e73bc83d234ec292573e31487d5
Matthew Kong <matthew.kong@liferay.com>
2012-03-07 16:46:22 -0800

    LPS-25760 Better just fixing sql

--- portal-impl/src/com/liferay/portal/upgrade/v6_1_0/UpgradeVirtualHost.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_1_0/UpgradeVirtualHost.java
@@ -17,7 +17,6 @@ package com.liferay.portal.upgrade.v6_1_0;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.StringPool;
-import com.liferay.portal.kernel.util.Validator;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -32,10 +31,6 @@ public class UpgradeVirtualHost extends UpgradeProcess {
 			long virtualHostId, long companyId, long layoutSetId,
 			String hostname)
 		throws Exception {
-
-		if (Validator.isNull(hostname)) {
-			return;
-		}
 
 		runSQL(
 			"insert into VirtualHost (virtualHostId, companyId, layoutSetId, " +
@@ -59,7 +54,7 @@ public class UpgradeVirtualHost extends UpgradeProcess {
 
 			ps = con.prepareStatement(
 				"select companyId, virtualHost from Company where " +
-					"virtualHost != ? or virtualHost is not null");
+					"virtualHost != ? and virtualHost is not null");
 
 			ps.setString(1, StringPool.BLANK);
 
@@ -91,7 +86,7 @@ public class UpgradeVirtualHost extends UpgradeProcess {
 
 			ps = con.prepareStatement(
 				"select layoutSetId, companyId, virtualHost from LayoutSet " +
-					"where virtualHost != ? or virtualHost is not null");
+					"where virtualHost != ? and virtualHost is not null");
 
 			ps.setString(1, StringPool.BLANK);
 
