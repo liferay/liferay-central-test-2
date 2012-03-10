6349e17f20b374b5c7297536d51583f00c396182
Ken Duenwald <ken.duenwald@liferay.com>
2012-03-09 16:36:11 -0800

    Test Fixes - socialofficehome/tasks

--- portal-web/test/com/liferay/portalweb/socialofficehome/tasks/task/addtaskstaskassignedtoconnection/AddTasksTaskAssignedToConnectionTests.java
+++ portal-web/test/com/liferay/portalweb/socialofficehome/tasks/task/addtaskstaskassignedtoconnection/AddTasksTaskAssignedToConnectionTests.java
@@ -15,7 +15,7 @@
 package com.liferay.portalweb.socialofficehome.tasks.task.addtaskstaskassignedtoconnection;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
-import com.liferay.portalweb.portal.controlpanel.users.user.signin.SignOutTest;
+import com.liferay.portalweb.portal.controlpanel.users.user.signin.SignOutSOTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -27,18 +27,18 @@ public class AddTasksTaskAssignedToConnectionTests extends BaseTestSuite {
 	public static Test suite() {
 		TestSuite testSuite = new TestSuite();
 		testSuite.addTestSuite(AddSOUserTest.class);
-		testSuite.addTestSuite(SignOutTest.class);
+		testSuite.addTestSuite(SignOutSOTest.class);
 		testSuite.addTestSuite(SOUs_SignInTest.class);
 		testSuite.addTestSuite(SOUs_AddAsConnectionCCUserTest.class);
-		testSuite.addTestSuite(SignOutTest.class);
+		testSuite.addTestSuite(SignOutSOTest.class);
 		testSuite.addTestSuite(SOSignInTest.class);
 		testSuite.addTestSuite(ConfirmNotificationsAddConnnectionTest.class);
 		testSuite.addTestSuite(AddTasksTaskAssignedToConnectionTest.class);
 		testSuite.addTestSuite(ViewTasksTaskAssignedToConnectionTest.class);
-		testSuite.addTestSuite(SignOutTest.class);
+		testSuite.addTestSuite(SignOutSOTest.class);
 		testSuite.addTestSuite(SOUs_SignInTest.class);
 		testSuite.addTestSuite(SOUs_ViewTasksTaskAssignedToConnectionTest.class);
-		testSuite.addTestSuite(SignOutTest.class);
+		testSuite.addTestSuite(SignOutSOTest.class);
 		testSuite.addTestSuite(SOSignInTest.class);
 		testSuite.addTestSuite(TearDownUserTest.class);
 		testSuite.addTestSuite(TearDownTasksTaskTest.class);
