6285e65a9700f56295f8cd49eb1fbb8d11dae338
Ken Duenwald <ken.duenwald@liferay.com>
2012-03-09 16:30:40 -0800

    Test Fixes - socialofficehome/activities

--- portal-web/test/com/liferay/portalweb/socialofficehome/activities/mbentryactivity/sousviewmbentryactivityconnections/SOUs_ViewMBEntryActivityConnectionsTests.java
+++ portal-web/test/com/liferay/portalweb/socialofficehome/activities/mbentryactivity/sousviewmbentryactivityconnections/SOUs_ViewMBEntryActivityConnectionsTests.java
@@ -15,7 +15,7 @@
 package com.liferay.portalweb.socialofficehome.activities.mbentryactivity.sousviewmbentryactivityconnections;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
-import com.liferay.portalweb.portal.controlpanel.users.user.signin.SignOutTest;
+import com.liferay.portalweb.portal.controlpanel.users.user.signin.SignOutSOTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -29,17 +29,17 @@ public class SOUs_ViewMBEntryActivityConnectionsTests extends BaseTestSuite {
 		testSuite.addTestSuite(AddSOUserTest.class);
 		testSuite.addTestSuite(AddMicroblogsContentViewableByConnectionsTest.class);
 		testSuite.addTestSuite(ViewMicroblogsContentViewableByConnectionsTest.class);
-		testSuite.addTestSuite(SignOutTest.class);
+		testSuite.addTestSuite(SignOutSOTest.class);
 		testSuite.addTestSuite(SOUs_SignInTest.class);
 		testSuite.addTestSuite(SOUs_ViewMicroblogsContentViewableByConnectionsTest.class);
 		testSuite.addTestSuite(SOUs_AddAsConnectionCCUserTest.class);
-		testSuite.addTestSuite(SignOutTest.class);
+		testSuite.addTestSuite(SignOutSOTest.class);
 		testSuite.addTestSuite(SOSignInTest.class);
 		testSuite.addTestSuite(ConfirmNotificationsAddConnnectionTest.class);
-		testSuite.addTestSuite(SignOutTest.class);
+		testSuite.addTestSuite(SignOutSOTest.class);
 		testSuite.addTestSuite(SOUs_SignInTest.class);
 		testSuite.addTestSuite(SOUs_ViewMBEntryActivityConnectionsTest.class);
-		testSuite.addTestSuite(SignOutTest.class);
+		testSuite.addTestSuite(SignOutSOTest.class);
 		testSuite.addTestSuite(SOSignInTest.class);
 		testSuite.addTestSuite(TearDownWHEntryContentTest.class);
 		testSuite.addTestSuite(TearDownUserTest.class);
--- portal-web/test/com/liferay/portalweb/socialofficehome/activities/mbentryactivity/sousviewmbentryactivityfollowing/SOUs_ViewMBEntryActivityFollowingTests.java
+++ portal-web/test/com/liferay/portalweb/socialofficehome/activities/mbentryactivity/sousviewmbentryactivityfollowing/SOUs_ViewMBEntryActivityFollowingTests.java
@@ -15,7 +15,7 @@
 package com.liferay.portalweb.socialofficehome.activities.mbentryactivity.sousviewmbentryactivityfollowing;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
-import com.liferay.portalweb.portal.controlpanel.users.user.signin.SignOutTest;
+import com.liferay.portalweb.portal.controlpanel.users.user.signin.SignOutSOTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -29,12 +29,12 @@ public class SOUs_ViewMBEntryActivityFollowingTests extends BaseTestSuite {
 		testSuite.addTestSuite(AddSOUserTest.class);
 		testSuite.addTestSuite(AddMicroblogsContentViewableByFollowersTest.class);
 		testSuite.addTestSuite(ViewMicroblogsContentViewableByFollowersTest.class);
-		testSuite.addTestSuite(SignOutTest.class);
+		testSuite.addTestSuite(SignOutSOTest.class);
 		testSuite.addTestSuite(SOUs_SignInTest.class);
 		testSuite.addTestSuite(SOUs_ViewMicroblogsContentViewableByFollowersTest.class);
 		testSuite.addTestSuite(SOUs_AddFollowingCCUserTest.class);
 		testSuite.addTestSuite(SOUs_ViewMBEntryActivityFollowingTest.class);
-		testSuite.addTestSuite(SignOutTest.class);
+		testSuite.addTestSuite(SignOutSOTest.class);
 		testSuite.addTestSuite(SOSignInTest.class);
 		testSuite.addTestSuite(TearDownWHEntryContentTest.class);
 		testSuite.addTestSuite(TearDownUserTest.class);
