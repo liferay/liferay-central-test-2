36b204c6e1502629d02cf7d0e0a4693ec902b50e
Ivica Čardić <ivica.cardic@liferay.com>
2012-03-13 17:17:09 +0100

    LPS-25861

--- portal-impl/src/com/liferay/portal/verify/VerifyOracle.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyOracle.java
@@ -38,7 +38,7 @@ public class VerifyOracle extends VerifyProcess {
 
 		String dbType = db.getType();
 
-		if (true || !dbType.equals(DB.TYPE_ORACLE)) {
+		if (!dbType.equals(DB.TYPE_ORACLE)) {
 			return;
 		}
 
@@ -51,7 +51,8 @@ public class VerifyOracle extends VerifyProcess {
 
 			ps = con.prepareStatement(
 				"select table_name, column_name, data_length from " +
-					"user_tab_columns where data_type = 'VARCHAR2'");
+					"user_tab_columns where data_type = 'VARCHAR2' and " +
+					"char_used = 'B'");
 
 			rs = ps.executeQuery();
 
