c634fcc79e2c22af3668939858a189b2227f41e6
Brian Chan <brian.chan@liferay.com>
2012-03-14 11:40:24 -0700

    LPS-25861 Source formatting

--- portal-impl/src/com/liferay/portal/verify/VerifyOracle.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyOracle.java
@@ -51,7 +51,7 @@ public class VerifyOracle extends VerifyProcess {
 
 			ps = con.prepareStatement(
 				"select table_name, column_name, data_length from " +
-					"user_tab_columns where data_type = 'VARCHAR2' AND " +
+					"user_tab_columns where data_type = 'VARCHAR2' and " +
 						"char_used = 'B'");
 
 			rs = ps.executeQuery();
