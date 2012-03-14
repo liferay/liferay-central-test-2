9a03833a230a3d312cbe9e46337f2904824bb575
Julio Camarero <julio.camarero@liferay.com>
2012-03-14 11:50:42 +0100

    LPS-26033 Unit test for friendlyURLNormalizer

---
+++ portal-impl/test/unit/com/liferay/portal/util/FriendlyURLNormalizerImplTest.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.util;
+
+import com.liferay.portal.kernel.test.TestCase;
+import com.liferay.portal.kernel.util.StringPool;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ * @author Julio Camarero
+ */
+public class FriendlyURLNormalizerImplTest extends TestCase {
+	
+	@Test
+	public void testNormalizeBlank() {
+		Assert.assertEquals(
+			StringPool.BLANK,
+			_friendlyURLNormalizerImpl.normalize(StringPool.BLANK));
+	}
+
+	@Test
+	public void testNormalizeNull() {
+		Assert.assertEquals(
+			null, _friendlyURLNormalizerImpl.normalize(null));
+	}
+
+	@Test
+	public void testNormalizeSentenceWithBlanks() {
+		Assert.assertEquals(
+			"sentence-with-blanks",
+			_friendlyURLNormalizerImpl.normalize("sentence with blanks"));
+	}
+
+	@Test
+	public void testNormalizeSentenceWithCapitalLetters() {
+		Assert.assertEquals(
+			"sentence-with-capital-letters",
+			_friendlyURLNormalizerImpl.normalize(
+				"Sentence WITH CaPital leTTerS"));
+	}
+
+	@Test
+	public void testNormalizeSentenceWithDash() {
+		Assert.assertEquals(
+			"sentence-with-dash",
+			_friendlyURLNormalizerImpl.normalize("sentence -with-dash"));
+	}
+
+	@Test
+	public void testNormalizeSentenceWithDoubleBlanks() {
+		Assert.assertEquals(
+			"sentence-with-double-blanks",
+			_friendlyURLNormalizerImpl.normalize(
+				"sentence with   double  blanks"));
+	}
+
+	@Test
+	public void testNormalizeSentenceWithSpecialCharacters() {
+		Assert.assertEquals(
+			"sentence-with-special-characters",
+			_friendlyURLNormalizerImpl.normalize(
+				"sentence&: =()with !@special# %+characters"));
+	}
+
+	@Test
+	public void testNormalizeSimpleWord() {
+		Assert.assertEquals(
+			"word", _friendlyURLNormalizerImpl.normalize("word"));
+	}
+
+	@Test
+	public void testNormalizeSpace() {
+		Assert.assertEquals(
+			StringPool.SPACE,
+			_friendlyURLNormalizerImpl.normalize(StringPool.SPACE));
+	}
+
+	@Test
+	public void testNormalizeWordReplacingChars() {
+		char[] replaceChars = {'e', 'w'};
+
+		Assert.assertEquals(
+			"s-nt-nc-ith-r-plac-chars",
+			_friendlyURLNormalizerImpl.normalize(
+				"sentence with replace chars", replaceChars));
+	}
+
+	@Test
+	public void testNormalizeWordWithNonASCIICharacters() {
+		Assert.assertEquals(
+			"wordnc", _friendlyURLNormalizerImpl.normalize("wordñÇ"));
+	}
+
+	private FriendlyURLNormalizerImpl _friendlyURLNormalizerImpl =
+		new FriendlyURLNormalizerImpl();
+
+}
