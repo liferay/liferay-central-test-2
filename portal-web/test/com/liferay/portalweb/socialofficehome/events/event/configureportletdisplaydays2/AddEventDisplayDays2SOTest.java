a7e0bc93f1a0022404d577786210169164c383c2
Ken Duenwald <ken.duenwald@liferay.com>
2012-03-15 12:16:15 -0700

    SOS-892

---
+++ portal-web/test/com/liferay/portalweb/socialofficehome/events/event/configureportletdisplaydays2/AddEventDisplayDays2SOTest.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portalweb.socialofficehome.events.event.configureportletdisplaydays2;
+
+import com.liferay.portalweb.portal.BaseTestCase;
+import com.liferay.portalweb.portal.util.RuntimeVariables;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class AddEventDisplayDays2SOTest extends BaseTestCase {
+	public void testAddEventDisplayDays2SO() throws Exception {
+		selenium.open("/user/joebloggs/home1");
+		loadRequiredJavaScriptModules();
+
+		for (int second = 0;; second++) {
+			if (second >= 90) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isVisible("link=Calendar Test Page")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.clickAt("link=Calendar Test Page",
+			RuntimeVariables.replace("Calendar Test Page"));
+		selenium.waitForPageToLoad("30000");
+		loadRequiredJavaScriptModules();
+		selenium.clickAt("//input[@value='Add Event']",
+			RuntimeVariables.replace("Add Event"));
+		selenium.waitForPageToLoad("30000");
+		loadRequiredJavaScriptModules();
+		selenium.type("//input[@id='_8_title']",
+			RuntimeVariables.replace("Calendar Event Title"));
+		selenium.keyPress("//select[@id='_8_startDateDay']",
+			RuntimeVariables.replace("\\40"));
+		Thread.sleep(5000);
+
+		for (int second = 0;; second++) {
+			if (second >= 90) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isVisible(
+							"//td[@id='cke_contents__8_editor']/iframe")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.selectFrame("//td[@id='cke_contents__8_editor']/iframe");
+		selenium.type("//body",
+			RuntimeVariables.replace("Calendar Event Description"));
+		selenium.selectFrame("relative=top");
+		selenium.clickAt("//input[@value='Save']",
+			RuntimeVariables.replace("Save"));
+		selenium.waitForPageToLoad("30000");
+		loadRequiredJavaScriptModules();
+		assertEquals(RuntimeVariables.replace(
+				"Your request completed successfully."),
+			selenium.getText("//div[@class='portlet-msg-success']"));
+		assertEquals(RuntimeVariables.replace("Events"),
+			selenium.getText("link=Events"));
+		selenium.clickAt("link=Events", RuntimeVariables.replace("Events"));
+		selenium.waitForPageToLoad("30000");
+		loadRequiredJavaScriptModules();
+		assertTrue(selenium.isElementPresent("//td[2]/a"));
+		assertEquals(RuntimeVariables.replace("Calendar Event Title"),
+			selenium.getText("//td[3]/a"));
+		assertEquals(RuntimeVariables.replace("Anniversary"),
+			selenium.getText("//td[4]/a"));
+	}
+}
