e46f3d842917cd832a958ce76446ae488343db13
Catherine Lui <catherine.lui@liferay.com>
2012-03-20 14:06:38 -0700

    LPS-25995

--- portal-web/test/com/liferay/portalweb/portal/util/liferayselenium/WebDriverToSeleniumBridge.java
+++ portal-web/test/com/liferay/portalweb/portal/util/liferayselenium/WebDriverToSeleniumBridge.java
@@ -25,6 +25,7 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 /**
@@ -131,7 +132,23 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public void clickAt(String locator, String coordString) {
-		throw new UnsupportedOperationException();
+		Actions actions = new Actions(this);
+
+		WebElement webElement = getWebElement(locator);
+
+		if (coordString.contains(",")) {
+			String [] coords = coordString.split(",");
+
+			int x = Integer.parseInt(coords[0]);
+
+			int y = Integer.parseInt(coords[1]);
+
+			actions.moveToElement(webElement, x, y);
+			actions.click();
+		}
+		else {
+			webElement.click();
+		}
 	}
 
 	@Override
