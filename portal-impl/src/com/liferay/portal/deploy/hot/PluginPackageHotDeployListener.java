528dd5cd189e177a6ba1a72da3639cbd45620858
Brian Chan <brian.chan@liferay.com>
2012-03-26 18:24:16 -0700

    LPS-26321 Read liferay-plugin-package.properties to register and unregister PACL policies.

--- portal-impl/src/com/liferay/portal/deploy/hot/PluginPackageHotDeployListener.java
+++ portal-impl/src/com/liferay/portal/deploy/hot/PluginPackageHotDeployListener.java
@@ -29,10 +29,14 @@ import com.liferay.portal.kernel.plugin.PluginPackage;
 import com.liferay.portal.kernel.servlet.ServletContextPool;
 import com.liferay.portal.kernel.util.AggregateClassLoader;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
+import com.liferay.portal.kernel.util.PropertiesUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.plugin.PluginPackageUtil;
+import com.liferay.portal.security.pacl.PACLPolicy;
+import com.liferay.portal.security.pacl.PACLPolicyManager;
 import com.liferay.portal.service.ServiceComponentLocalServiceUtil;
 
 import java.net.URL;
@@ -121,6 +125,8 @@ public class PluginPackageHotDeployListener extends BaseHotDeployListener {
 
 		reconfigureCaches(classLoader);
 
+		registerPACL(servletContext, portletClassLoader);
+
 		if (_log.isInfoEnabled()) {
 			_log.info(
 				"Plugin package " + pluginPackage.getModuleId() +
@@ -156,6 +162,8 @@ public class PluginPackageHotDeployListener extends BaseHotDeployListener {
 			servletContext, hotDeployEvent.getContextClassLoader());
 
 		unregisterClpMessageListeners(servletContext);
+
+		unregisterPACL(hotDeployEvent.getContextClassLoader());
 
 		if (_log.isInfoEnabled()) {
 			_log.info(
@@ -325,6 +333,33 @@ public class PluginPackageHotDeployListener extends BaseHotDeployListener {
 
 			liferayEhcacheRegionFactory.reconfigureCaches(configurationFile);
 		}
+	}
+
+	protected void registerPACL(
+			ServletContext servletContext, ClassLoader classLoader)
+		throws Exception {
+
+		Properties properties = null;
+
+		String propertiesString = HttpUtil.URLtoString(
+			servletContext.getResource(
+				"/WEB-INF/liferay-plugin-package.properties"));
+
+		if (propertiesString != null) {
+			properties = PropertiesUtil.load(propertiesString);
+		}
+		else {
+			properties = new Properties();
+		}
+
+		PACLPolicy paclPolicy = PACLPolicyManager.buildPACLPolicy(
+			servletContext.getServletContextName(), properties);
+
+		PACLPolicyManager.register(classLoader, paclPolicy);
+	}
+
+	protected void unregisterPACL(ClassLoader classLoader) {
+		PACLPolicyManager.unregister(classLoader);
 	}
 
 	private static final String _MULTI_VM_PORTAL_CACHE_MANAGER_BEAN_NAME =
---
+++ portal-impl/src/com/liferay/portal/security/pacl/PACLPolicyManager.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.pacl;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.security.lang.PortalSecurityManager;
+import com.liferay.portal.spring.aop.ServiceBeanAopProxy;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class PACLPolicyManager {
+
+	public static PACLPolicy buildPACLPolicy(
+		String servletContextName, Properties properties) {
+
+		boolean active = GetterUtil.getBoolean(
+			properties.get("security-manager-enabled"));
+
+		PACLPolicy paclPolicy = null;
+
+		if (active) {
+			paclPolicy = new ActivePACLPolicy(servletContextName, properties);
+		}
+		else {
+			paclPolicy = new InactivePACLPolicy(servletContextName, properties);
+		}
+
+		return paclPolicy;
+	}
+
+	public static int getActiveCount() {
+		return _activeCount;
+	}
+
+	public static PACLPolicy getDefaultPACLPolicy() {
+		return _defaultPACLPolicy;
+	}
+
+	public static PACLPolicy getPACLPolicy(ClassLoader classLoader) {
+		return _paclPolicies.get(classLoader);
+	}
+
+	public static boolean isActive() {
+		if (_activeCount > 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public static void register(
+		ClassLoader classLoader, PACLPolicy paclPolicy) {
+
+		_paclPolicies.put(classLoader, paclPolicy);
+
+		if (!paclPolicy.isActive()) {
+			return;
+		}
+
+		_activeCount++;
+
+		if ((_activeCount == 1) &&
+			(System.getSecurityManager() instanceof PortalSecurityManager)) {
+
+			if (_log.isInfoEnabled()) {
+				_log.info("Activating PACL policy manager");
+			}
+
+			ServiceBeanAopProxy.clearMethodInterceptorCache();
+		}
+	}
+
+	public static void unregister(ClassLoader classLoader) {
+		PACLPolicy paclPolicy = _paclPolicies.remove(classLoader);
+
+		if ((paclPolicy == null) || !paclPolicy.isActive()) {
+			return;
+		}
+
+		_activeCount--;
+
+		if ((_activeCount == 0) &&
+			(System.getSecurityManager() instanceof PortalSecurityManager)) {
+
+			if (_log.isInfoEnabled()) {
+				_log.info("Disabling PACL policy manager");
+			}
+
+			ServiceBeanAopProxy.clearMethodInterceptorCache();
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(PACLPolicyManager.class);
+
+	private static int _activeCount;
+	private static PACLPolicy _defaultPACLPolicy = new InactivePACLPolicy(
+		StringPool.BLANK, new Properties());
+	private static Map<ClassLoader, PACLPolicy> _paclPolicies =
+		new HashMap<ClassLoader, PACLPolicy>();
+
+}
