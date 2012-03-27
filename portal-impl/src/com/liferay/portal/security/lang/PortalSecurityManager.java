a52b9890ab494bae2d951b0f38d2a19103e6b981
Brian Chan <brian.chan@liferay.com>
2012-03-26 18:31:09 -0700

    LPS-26321 First cut of the security manager.

--- portal-impl/src/com/liferay/portal/security/lang/PortalSecurityManager.java
+++ portal-impl/src/com/liferay/portal/security/lang/PortalSecurityManager.java
@@ -14,6 +14,11 @@
 
 package com.liferay.portal.security.lang;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.security.pacl.PACLClassUtil;
+import com.liferay.portal.security.pacl.PACLPolicy;
+
 import java.security.Permission;
 
 /**
@@ -22,8 +27,72 @@ import java.security.Permission;
 public class PortalSecurityManager extends SecurityManager {
 
 	@Override
+	public void checkConnect(String host, int port) {
+		if (port == -1) {
+			if (_logCheckConnect.isInfoEnabled()) {
+				_logCheckConnect.info("Always allow resolving of host " + host);
+			}
+
+			return;
+		}
+
+		PACLPolicy paclPolicy = getPACLPolicy(
+			_logCheckConnect.isDebugEnabled());
+
+		if (_logCheckConnect.isInfoEnabled()) {
+			_logCheckConnect.info(
+				"Checking permissions to connect to host " + host +
+					" on port " + port + " with PACL policy " + paclPolicy);
+		}
+
+		if (paclPolicy != null) {
+			if (!paclPolicy.isSocketConnect(host, port)) {
+				throw new SecurityException(
+					"Attempted to connect to host " + host + " on port " +
+						port);
+			}
+		}
+
+		super.checkConnect(host, port);
+	}
+
+	@Override
+	public void checkListen(int port) {
+		PACLPolicy paclPolicy = getPACLPolicy(_logCheckListen.isDebugEnabled());
+
+		if (_logCheckListen.isInfoEnabled()) {
+			_logCheckListen.info(
+				"Checking permissions to listen on port " + port +
+					" with PACL policy " + paclPolicy);
+		}
+
+		if (paclPolicy != null) {
+			if (!paclPolicy.isSocketListen(port)) {
+				throw new SecurityException(
+					"Attempted to listen on port " + port);
+			}
+		}
+
+		super.checkListen(port);
+	}
+
+	@Override
+	public void checkPackageAccess(String pkg) {
+		if (pkg.startsWith("sun.reflect")) {
+		}
+
+		super.checkPackageAccess(pkg);
+	}
+
+	@Override
 	public void checkPermission(Permission permission) {
-		if (permission.getName().equals(_PERMISSION_EXIT_VM)) {
+		String name = permission.getName();
+
+		if (name.equals(_PERMISSION_SET_SECURITY_MANAGER)) {
+			throw new SecurityException(
+				"Attempted to set another security manager");
+		}
+		else if (name.equals(_PERMISSION_EXIT_VM)) {
 			Thread.dumpStack();
 		}
 	}
@@ -32,6 +101,20 @@ public class PortalSecurityManager extends SecurityManager {
 	public void checkPermission(Permission permission, Object context) {
 	}
 
+	protected PACLPolicy getPACLPolicy(boolean debug) {
+		return PACLClassUtil.getPACLPolicyBySecurityManagerClassContext(
+			getClassContext(), debug);
+	}
+
 	private static final String _PERMISSION_EXIT_VM = "exitVM";
+
+	private static final String _PERMISSION_SET_SECURITY_MANAGER =
+		"setSecurityManager";
+
+	private static Log _logCheckConnect = LogFactoryUtil.getLog(
+		PortalSecurityManager.class.getName() + "#checkConnect");
+
+	private static Log _logCheckListen = LogFactoryUtil.getLog(
+		PortalSecurityManager.class.getName() + "#checkListen");
 
 }
