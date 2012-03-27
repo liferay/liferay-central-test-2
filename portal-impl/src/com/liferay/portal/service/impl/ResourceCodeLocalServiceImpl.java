558e86c41e03fa2115a94557e0c081278a9bd27a
Igor Beslic <igor.beslic@liferay.com>
2012-03-27 23:52:03 +0200

    LPS-26306 remove non 6 permissons algorithm - implementation changed

--- portal-impl/src/com/liferay/portal/service/impl/ResourceCodeLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/ResourceCodeLocalServiceImpl.java
@@ -17,18 +17,13 @@ package com.liferay.portal.service.impl;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.transaction.Propagation;
-import com.liferay.portal.kernel.transaction.Transactional;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.ResourceCode;
-import com.liferay.portal.model.ResourceConstants;
 import com.liferay.portal.service.base.ResourceCodeLocalServiceBaseImpl;
-import com.liferay.portal.util.PropsValues;
 
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -69,41 +64,6 @@ public class ResourceCodeLocalServiceImpl
 		}
 
 		return resourceCode;
-	}
-
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-	public void checkResourceCodes() throws SystemException {
-		if (PropsValues.PERMISSIONS_USER_CHECK_ALGORITHM == 6) {
-			return;
-		}
-
-		if (_resourceCodes.isEmpty()) {
-			List<ResourceCode> resourceCodes =
-				resourceCodePersistence.findAll();
-
-			for (ResourceCode resourceCode : resourceCodes) {
-				String key = encodeKey(
-					resourceCode.getCompanyId(), resourceCode.getName(),
-					resourceCode.getScope());
-
-				_resourceCodes.put(key, resourceCode);
-			}
-		}
-	}
-
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-	public void checkResourceCodes(long companyId, String name)
-		throws SystemException {
-
-		if (PropsValues.PERMISSIONS_USER_CHECK_ALGORITHM == 6) {
-			return;
-		}
-
-		getResourceCode(companyId, name, ResourceConstants.SCOPE_COMPANY);
-		getResourceCode(companyId, name, ResourceConstants.SCOPE_GROUP);
-		getResourceCode(
-			companyId, name, ResourceConstants.SCOPE_GROUP_TEMPLATE);
-		getResourceCode(companyId, name, ResourceConstants.SCOPE_INDIVIDUAL);
 	}
 
 	public ResourceCode getResourceCode(long companyId, String name, int scope)
