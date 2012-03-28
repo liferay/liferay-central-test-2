23985754fe84bdad24c8bf9c1fcea18ca7ec1373
Manuel de la Peña <manuel.delapenya@liferay.com>
2012-03-28 10:40:14 +0200

    Testing DateUtil

---
+++ portal-service/test/unit/com/liferay/portal/kernel/util/DateUtilTest.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.util;
+
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.text.DateFormat;
+
+/**
+ * @author Alexander Chow
+ * @author Manuel de la Peña
+ * @author Ray Augé
+ */
+@RunWith(PowerMockRunner.class)
+public class DateUtilTest extends PowerMockito {
+
+
+}
