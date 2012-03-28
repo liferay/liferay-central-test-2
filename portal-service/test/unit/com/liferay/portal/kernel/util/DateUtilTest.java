815570e90139c54af74982cbaca6c9e4f25034ac
Raymond Auge <raymond.auge@liferay.com>
2012-03-28 12:53:24 +0200

    LPS-26390 DateUtil tests

--- portal-service/test/unit/com/liferay/portal/kernel/util/DateUtilTest.java
+++ portal-service/test/unit/com/liferay/portal/kernel/util/DateUtilTest.java
@@ -14,19 +14,77 @@
 
 package com.liferay.portal.kernel.util;
 
-import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.modules.junit4.PowerMockRunner;
+import java.util.TimeZone;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
  * @author Alexander Chow
  * @author Manuel de la Peña
  * @author Ray Augé
  */
+@PrepareForTest(DateFormatFactoryUtil.class)
 @RunWith(PowerMockRunner.class)
 public class DateUtilTest extends PowerMockito {
 
+	@Test
+	public void testGetUTCFormat() {
+		_testGetUTCFormat("19721223", "yyyyMMdd");
+	}
+
+	public void _testGetUTCFormat(String date, String pattern) {
+		mockStatic(DateFormatFactoryUtil.class);
+
+		when(
+			DateFormatFactoryUtil.getSimpleDateFormat(
+				Mockito.anyString(), Mockito.any(TimeZone.class))
+		).thenAnswer(
+			new Answer<SimpleDateFormat>() {
+				public SimpleDateFormat answer(InvocationOnMock invocation)
+					throws Throwable {
+
+					return new TestSimpleDateFormat(
+						(String)invocation.getArguments()[0]);
+				}
+			}
+		);
+
+		DateFormat utcFormat = DateUtil.getUTCFormat(date);
+
+		Assert.assertNotNull(utcFormat);
+
+		Assert.assertTrue(utcFormat instanceof SimpleDateFormat);
+
+		TestSimpleDateFormat sdf = (TestSimpleDateFormat)utcFormat;
+
+		Assert.assertEquals(sdf.getPattern(), pattern);
+	}
+
+	class TestSimpleDateFormat extends SimpleDateFormat {
+
+		public TestSimpleDateFormat(String pattern) {
+			super(pattern);
+
+			_pattern = pattern;
+		}
+
+		public String getPattern() {
+			return _pattern;
+		}
+
+		private String _pattern;
+	}
 
 }
