dfab46614e94911d4a2e9ed37a7fcbeb5b639c3a
Miguel Pastor <miguel.pastor@liferay.com>
2012-03-29 10:13:51 +0200

    LPS-26390 Source formatting

--- portal-service/test/unit/com/liferay/portlet/social/util/SocialCounterPeriodUtilTest.java
+++ portal-service/test/unit/com/liferay/portlet/social/util/SocialCounterPeriodUtilTest.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 package com.liferay.portlet.social.util;
 
 import com.liferay.portal.kernel.util.PropsKeys;
@@ -14,6 +27,7 @@ import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
 @PrepareForTest(PropsUtil.class)
 @RunWith(PowerMockRunner.class)
 public class SocialCounterPeriodUtilTest extends PowerMockito {
