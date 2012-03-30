75a0af951ac6a9b2f3344c8cf76ca047b8184878
Brian Chan <brian.chan@liferay.com>
2012-03-30 15:48:01 -0700

    LPS-26390 Source formatting

--- portal-service/test/unit/com/liferay/portlet/social/util/SocialCounterPeriodUtilTest.java
+++ portal-service/test/unit/com/liferay/portlet/social/util/SocialCounterPeriodUtilTest.java
@@ -11,16 +11,17 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.portlet.social.util;
 
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
 
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -28,93 +29,74 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+/**
+ * @author Zsolt Berentey
+ */
 @PrepareForTest(PropsUtil.class)
 @RunWith(PowerMockRunner.class)
 public class SocialCounterPeriodUtilTest extends PowerMockito {
 
-	@Test
-	public void testGetActivityDay() {
-		_initMocks();
-
-		Calendar calendar = new GregorianCalendar(2011, Calendar.JANUARY, 1);
-
-		int activityDay = SocialCounterPeriodUtil.getActivityDay(calendar);
-
-		Assert.assertEquals(0, activityDay);
-
-		activityDay = SocialCounterPeriodUtil.getActivityDay(
-			calendar.getTimeInMillis());
-
-		Assert.assertEquals(0, activityDay);
-
-		calendar = new GregorianCalendar(2011, Calendar.FEBRUARY, 1);
-
-		activityDay = SocialCounterPeriodUtil.getActivityDay(calendar);
-
-		Assert.assertEquals(31, activityDay);
-
-		activityDay = SocialCounterPeriodUtil.getActivityDay(
-			calendar.getTimeInMillis());
-
-		Assert.assertEquals(31, activityDay);
-	}
-
-	@Test
-	public void testGetDate() {
-		_initMocks();
-
-		Date activityDate = SocialCounterPeriodUtil.getDate(0);
-
-		Calendar calendar = new GregorianCalendar(2011, Calendar.JANUARY, 1);
-
-		Assert.assertEquals(calendar.getTime(), activityDate);
-
-		calendar = new GregorianCalendar(2011, Calendar.FEBRUARY, 1);
-
-		activityDate = SocialCounterPeriodUtil.getDate(31);
-
-		Assert.assertEquals(calendar.getTime(), activityDate);
-	}
-
-	@Test
-	public void testGetPeriodLength() {
-		_initMocks();
-
-		int periodLength = SocialCounterPeriodUtil.getPeriodLength();
-
-		Assert.assertEquals(1, periodLength);
-
-		periodLength = SocialCounterPeriodUtil.getPeriodLength(-1);
-
-		Assert.assertEquals(1, periodLength);
-	}
-
-	@Test
-	public void testGetStartPeriod() {
-		_initMocks();
-
-		Calendar calendar = new GregorianCalendar(2011, Calendar.JANUARY, 15);
-
-		int offset = SocialCounterPeriodUtil.getStartPeriod(
-			calendar.getTimeInMillis());
-
-		Assert.assertEquals(14, offset);
-
-		int currentStartPeriod = SocialCounterPeriodUtil.getStartPeriod();
-
-		Assert.assertEquals(
-			currentStartPeriod, SocialCounterPeriodUtil.getStartPeriod(0));
-
-		Assert.assertEquals(
-			currentStartPeriod - 1, SocialCounterPeriodUtil.getStartPeriod(-1));
-	}
-
-	private void _initMocks() {
+	@Before
+	public void setUp() throws Exception {
 		mockStatic(PropsUtil.class);
 
 		when(
 			PropsUtil.get(PropsKeys.SOCIAL_ACTIVITY_COUNTER_PERIOD_LENGTH)
 		).thenReturn("1");
+	}
+
+	@Test
+	public void testGetActivityDay() {
+		Calendar calendar = new GregorianCalendar(2011, Calendar.JANUARY, 1);
+
+		Assert.assertEquals(
+			0, SocialCounterPeriodUtil.getActivityDay(calendar));
+		Assert.assertEquals(
+			0,
+			SocialCounterPeriodUtil.getActivityDay(calendar.getTimeInMillis()));
+
+		calendar = new GregorianCalendar(2011, Calendar.FEBRUARY, 1);
+
+		Assert.assertEquals(
+			31, SocialCounterPeriodUtil.getActivityDay(calendar));
+		Assert.assertEquals(
+			31,
+			SocialCounterPeriodUtil.getActivityDay(calendar.getTimeInMillis()));
+	}
+
+	@Test
+	public void testGetDate() {
+		Calendar calendar = new GregorianCalendar(2011, Calendar.JANUARY, 1);
+
+		Assert.assertEquals(
+			calendar.getTime(), SocialCounterPeriodUtil.getDate(0));
+
+		calendar = new GregorianCalendar(2011, Calendar.FEBRUARY, 1);
+
+		Assert.assertEquals(
+			calendar.getTime(), SocialCounterPeriodUtil.getDate(31));
+	}
+
+	@Test
+	public void testGetPeriodLength() {
+		Assert.assertEquals(1, SocialCounterPeriodUtil.getPeriodLength());
+		Assert.assertEquals(1, SocialCounterPeriodUtil.getPeriodLength(-1));
+	}
+
+	@Test
+	public void testGetStartPeriod() {
+		Calendar calendar = new GregorianCalendar(2011, Calendar.JANUARY, 15);
+
+		Assert.assertEquals(
+			14,
+			SocialCounterPeriodUtil.getStartPeriod(calendar.getTimeInMillis()));
+
+		int startPeriod = SocialCounterPeriodUtil.getStartPeriod();
+
+		Assert.assertEquals(
+			startPeriod, SocialCounterPeriodUtil.getStartPeriod(0));
+		Assert.assertEquals(
+			startPeriod - 1, SocialCounterPeriodUtil.getStartPeriod(-1));
 	}
 
 }
