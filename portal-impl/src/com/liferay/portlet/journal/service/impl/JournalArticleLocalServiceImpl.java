f48ab4fedf28882f22f4f517259fe34b8f4173ea
Shinn Lok <shinn.lok@liferay.com>
2012-04-03 14:38:33 -0700

    LPS-26108 Don't permission check

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -2161,7 +2161,7 @@ public class JournalArticleLocalServiceImpl
 
 		JournalArticle article = null;
 
-		User user = userService.getUserById(oldArticle.getUserId());
+		User user = userLocalService.getUserById(oldArticle.getUserId());
 
 		if (!oldArticle.isDraft()) {
 			double newVersion = MathUtil.format(oldVersion + 0.1, 1, 1);
