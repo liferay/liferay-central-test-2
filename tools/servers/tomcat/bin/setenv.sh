843046fb5994a1567073d981118d90de65a62377
Olaf Kock <olaf.kock@liferay.com>
2012-04-03 11:26:53 +0200

    LPS-26464 Changing memory settings to be in CATALINA_OPTS instead of JAVA_OPTS

--- tools/servers/tomcat/bin/setenv.sh
+++ tools/servers/tomcat/bin/setenv.sh
@@ -1,1 +1,1 @@
-JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8 -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
+CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8 -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
