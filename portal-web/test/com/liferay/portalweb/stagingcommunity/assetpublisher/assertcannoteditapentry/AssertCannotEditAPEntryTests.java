ae74aebdca9eddd9dfd794cc382afd809acc89de
Ken Duenwald <ken.duenwald@liferay.com>
2012-04-09 13:47:10 -0700

    Test Fixes - stagingcommunity/assetpublisher

--- portal-web/test/com/liferay/portalweb/stagingcommunity/assetpublisher/assertcannoteditapentry/AssertCannotEditAPEntryTests.java
+++ portal-web/test/com/liferay/portalweb/stagingcommunity/assetpublisher/assertcannoteditapentry/AssertCannotEditAPEntryTests.java
@@ -15,6 +15,7 @@
 package com.liferay.portalweb.stagingcommunity.assetpublisher.assertcannoteditapentry;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
+import com.liferay.portalweb.portal.controlpanel.sites.site.addsite.AddSiteTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
