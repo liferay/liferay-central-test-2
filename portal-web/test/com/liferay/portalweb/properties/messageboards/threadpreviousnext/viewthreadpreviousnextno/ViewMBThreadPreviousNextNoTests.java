265d0e7752fc60b99b5f93d2ad6f5170a1b426fa
serena206x <he.song@liferay.com>
2012-04-09 16:11:44 +0800

    LPS-24843

---
+++ portal-web/test/com/liferay/portalweb/properties/messageboards/threadpreviousnext/viewthreadpreviousnextno/ViewMBThreadPreviousNextNoTests.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portalweb.properties.messageboards.threadpreviousnext.viewthreadpreviousnextno;
+
+import com.liferay.portalweb.portal.BaseTestSuite;
+import com.liferay.portalweb.portal.util.TearDownPageTest;
+import com.liferay.portalweb.portlet.messageboards.portlet.addportlet.AddPageMBTest;
+import com.liferay.portalweb.portlet.messageboards.portlet.addportlet.AddPortletMBTest;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class ViewMBThreadPreviousNextNoTests extends BaseTestSuite {
+	public static Test suite() {
+		TestSuite testSuite = new TestSuite();
+		testSuite.addTestSuite(AddPageMBTest.class);
+		testSuite.addTestSuite(AddPortletMBTest.class);
+		testSuite.addTestSuite(AddThread1Test.class);
+		testSuite.addTestSuite(AddThread2Test.class);
+		testSuite.addTestSuite(ViewMBThreadPreviousNextNoTest.class);
+		testSuite.addTestSuite(TearDownPageTest.class);
+		testSuite.addTestSuite(TearDownThreadTest.class);
+
+		return testSuite;
+	}
+}
