1ac9c6e45a212e3729bbd396427df706539b5f17
Ken Duenwald <ken.duenwald@liferay.com>
2012-04-09 13:47:48 -0700

    Test Fixes - stagingcommunity/sites

--- portal-web/test/com/liferay/portalweb/stagingcommunity/sites/sitepagesvariation/addsitepagesvariation/AddSitePagesVariationTests.java
+++ portal-web/test/com/liferay/portalweb/stagingcommunity/sites/sitepagesvariation/addsitepagesvariation/AddSitePagesVariationTests.java
@@ -15,6 +15,7 @@
 package com.liferay.portalweb.stagingcommunity.sites.sitepagesvariation.addsitepagesvariation;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
+import com.liferay.portalweb.portal.controlpanel.sites.site.addsite.AddSiteTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
