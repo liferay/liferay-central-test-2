d40a89b6bc5d2d6167d4c5f468b80dd5ed478a54
Ryan Park <ryan.park@liferay.com>
2012-04-12 16:12:19 -0700

    LPS-24398 Source formatting

--- portal-service/src/com/liferay/portal/service/LayoutLocalServiceWrapper.java
+++ portal-service/src/com/liferay/portal/service/LayoutLocalServiceWrapper.java
@@ -1365,21 +1365,6 @@ public class LayoutLocalServiceWrapper implements LayoutLocalService,
 	}
 
 	/**
-	* Updates the priorities of the layouts within the group after the
-	* LayoutSetPrototype is updated.
-	*
-	* @param groupId the primary key of the group
-	* @param privateLayout whether the layout is private to the group
-	* @throws PortalException if a matching layout could not be found
-	* @throws SystemException if a system exception occurred
-	*/
-	public void updatePriorities(long groupId, boolean privateLayout)
-		throws com.liferay.portal.kernel.exception.PortalException,
-			com.liferay.portal.kernel.exception.SystemException {
-		_layoutLocalService.updatePriorities(groupId, privateLayout);
-	}
-
-	/**
 	* Updates the priority of the layout matching the group, layout ID, and
 	* privacy.
 	*
@@ -1414,6 +1399,21 @@ public class LayoutLocalServiceWrapper implements LayoutLocalService,
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException {
 		return _layoutLocalService.updatePriority(plid, priority);
+	}
+
+	/**
+	* Updates the priorities of the layouts within the group after the
+	* LayoutSetPrototype is updated.
+	*
+	* @param groupId the primary key of the group
+	* @param privateLayout whether the layout is private to the group
+	* @throws PortalException if a matching layout could not be found
+	* @throws SystemException if a system exception occurred
+	*/
+	public void updatePriorities(long groupId, boolean privateLayout)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		_layoutLocalService.updatePriorities(groupId, privateLayout);
 	}
 
 	public void updateScopedPortletNames(long groupId, boolean privateLayout,
