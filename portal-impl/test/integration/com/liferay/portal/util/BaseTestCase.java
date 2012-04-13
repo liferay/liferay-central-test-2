a05af362bad50dd295f59bb0ef090dc58f015a8a
Hugo Huijser <hugo.huijser@liferay.com>
2012-04-13 10:03:27 -0700

    Source formatting - remove imports that are in same package

--- portal-impl/test/integration/com/liferay/portal/util/BaseTestCase.java
+++ portal-impl/test/integration/com/liferay/portal/util/BaseTestCase.java
@@ -16,7 +16,6 @@ package com.liferay.portal.util;
 
 import com.liferay.counter.service.CounterLocalServiceUtil;
 import com.liferay.portal.kernel.test.TestCase;
-import com.liferay.portal.util.InitUtil;
 import com.liferay.util.PwdGenerator;
 
 import java.util.Calendar;
