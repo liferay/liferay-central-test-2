514e5d710dd3f3d373bb45cd8ffc4560dd76373d
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2012-04-14 14:05:02 -0700

    LPS-26441 - Source formatting

--- portal-web/docroot/html/js/liferay/store.js
+++ portal-web/docroot/html/js/liferay/store.js
@@ -2,62 +2,126 @@ AUI.add(
 	'liferay-store',
 	function(A) {
 		var Lang = A.Lang;
-
 		var isObject = Lang.isObject;
 
 		var TOKEN_SERIALIZE = 'serialize://';
 
 		var Store = function(key, value) {
-			var config = {
-				after: {},
-				data: {}
-			};
+			var instance = Store;
+
+			var method;
 
 			if (Lang.isFunction(value)) {
-				var cmd = 'get';
-				config.data.key = key;
+				method = 'get';
 
 				if (Lang.isArray(key)) {
-					cmd = 'getAll';
-					config.dataType = 'json';
+					method = 'getAll';
 				}
-
-				config.data.cmd = cmd;
-
-				var callback = value;
-
-				config.after.success = function(event) {
-					var responseData = this.get('responseData');
-
-					if (Lang.isString(responseData) && responseData.indexOf(TOKEN_SERIALIZE) === 0) {
-						try {
-							responseData = A.JSON.parse(responseData.substring(TOKEN_SERIALIZE.length));
-						}
-						catch (e) {
-						}
-					}
-
-					callback(responseData);
-				};
 			}
 			else {
-				if (isObject(key)) {
-					config.data = key;
-				}
-				else {
-					config.data[key] = value;
-				}
+				method = 'set';
 
-				if (isObject(value)) {
-					config.data[key] = TOKEN_SERIALIZE + A.JSON.stringify(value);
+				if (isObject(key)) {
+					method = 'setAll';
+				}
+				else if (arguments.length == 1) {
+					method = null;
 				}
 			}
 
-			A.io.request(
-				themeDisplay.getPathMain() + '/portal/session_click',
-				config
-			);
+			if (method) {
+				instance[method].apply(Store, arguments);
+			}
 		};
+
+		A.mix(
+			Store,
+			{
+				get: function(key, callback) {
+					var instance = this;
+
+					instance._getValues('get', key, callback);
+				},
+
+				getAll: function(keys, callback) {
+					var instance = this;
+
+					instance._getValues('getAll', keys, callback);
+				},
+
+				set: function(key, value) {
+					var instance = this;
+
+					var obj = {};
+
+					if (isObject(value)) {
+						value = TOKEN_SERIALIZE + A.JSON.stringify(value);
+					}
+
+					obj[key] = value;
+
+					instance._setValues(obj);
+				},
+
+				setAll: function(obj) {
+					var instance = this;
+
+					instance._setValues(obj);
+				},
+
+				_getValues: function(cmd, key, callback) {
+					var instance = this;
+
+					var config = {
+						after: {
+							success: function(event) {
+								var responseData = this.get('responseData');
+
+								if (Lang.isString(responseData) && responseData.indexOf(TOKEN_SERIALIZE) === 0) {
+									try {
+										responseData = A.JSON.parse(responseData.substring(TOKEN_SERIALIZE.length));
+									}
+									catch (e) {
+									}
+								}
+
+								callback(responseData);
+							}
+						},
+						data: {
+							cmd: cmd
+						}
+					};
+
+					config.data.key = key;
+
+					if (cmd == 'getAll') {
+						config.dataType = 'json';
+					}
+
+					instance._ioRequest(config);
+				},
+
+				_ioRequest: function(config) {
+					var instance = this;
+
+					A.io.request(
+						themeDisplay.getPathMain() + '/portal/session_click',
+						config
+					);
+				},
+
+				_setValues: function(data) {
+					var instance = this;
+
+					instance._ioRequest(
+						{
+							data: data
+						}
+					);
+				}
+			}
+		);
 
 		Liferay.Store = Store;
 	},
