5db9083df07b55a0be83c067790e6cdbd40f0560
Brian Chan <brian.chan@liferay.com>
2012-04-16 15:05:44 -0700

    LPS-26306 Remove unused code

--- portal-impl/src/com/liferay/portal/lar/LayoutCache.java
+++ portal-impl/src/com/liferay/portal/lar/LayoutCache.java
@@ -18,14 +18,9 @@ import com.liferay.portal.NoSuchRoleException;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.util.OrderByComparator;
-import com.liferay.portal.kernel.util.StringBundler;
-import com.liferay.portal.kernel.util.StringPool;
-import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Organization;
 import com.liferay.portal.model.OrganizationConstants;
-import com.liferay.portal.model.Resource;
 import com.liferay.portal.model.Role;
 import com.liferay.portal.model.Team;
 import com.liferay.portal.model.User;
@@ -33,14 +28,12 @@ import com.liferay.portal.model.UserGroup;
 import com.liferay.portal.security.permission.ResourceActionsUtil;
 import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.OrganizationLocalServiceUtil;
-import com.liferay.portal.service.ResourceLocalServiceUtil;
 import com.liferay.portal.service.RoleLocalServiceUtil;
 import com.liferay.portal.service.TeamLocalServiceUtil;
 import com.liferay.portal.service.UserGroupLocalServiceUtil;
 import com.liferay.portal.service.UserLocalServiceUtil;
 
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -195,32 +188,6 @@ public class LayoutCache {
 		return users;
 	}
 
-	protected Resource getResource(
-		long companyId, long groupId, String resourceName, int scope,
-		String resourcePrimKey, boolean portletActions) {
-
-		StringBundler sb = new StringBundler(5);
-
-		sb.append(resourceName);
-		sb.append(StringPool.PIPE);
-		sb.append(scope);
-		sb.append(StringPool.PIPE);
-		sb.append(resourcePrimKey);
-
-		String key = sb.toString();
-
-		Resource resource = resourcesMap.get(key);
-
-		if (resource == null) {
-			resource = ResourceLocalServiceUtil.getResource(
-				companyId, resourceName, scope, resourcePrimKey);
-
-			resourcesMap.put(key, resource);
-		}
-
-		return resource;
-	}
-
 	protected Role getRole(long companyId, String roleName)
 		throws PortalException, SystemException {
 
@@ -237,41 +204,6 @@ public class LayoutCache {
 		}
 
 		return role;
-	}
-
-	protected User getUser(long companyId, long groupId, String uuid)
-		throws SystemException {
-
-		List<User> users = usersMap.get(uuid);
-
-		if (users == null) {
-			LinkedHashMap<String, Object> params =
-				new LinkedHashMap<String, Object>();
-
-			params.put("usersGroups", new Long(groupId));
-
-			try {
-				User user = UserLocalServiceUtil.getUserByUuidAndCompanyId(
-					uuid, companyId);
-
-				users = UserLocalServiceUtil.search(
-					companyId, null, null, null, user.getScreenName(), null,
-					WorkflowConstants.STATUS_APPROVED, params, true, 0, 1,
-					(OrderByComparator)null);
-
-			}
-			catch (PortalException pe) {
-			}
-
-			usersMap.put(uuid, users);
-		}
-
-		if (users.size() == 0) {
-			return null;
-		}
-		else {
-			return users.get(0);
-		}
 	}
 
 	protected List<Role> getUserRoles(long userId) throws SystemException {
@@ -293,12 +225,8 @@ public class LayoutCache {
 		new HashMap<Long, List<Role>>();
 	protected Map<Long, List<User>> groupUsersMap =
 		new HashMap<Long, List<User>>();
-	protected Map<String, Resource> resourcesMap =
-		new HashMap<String, Resource>();
 	protected Map<String, Role> rolesMap = new HashMap<String, Role>();
 	protected Map<Long, List<Role>> userRolesMap =
 		new HashMap<Long, List<Role>>();
-	protected Map<String, List<User>> usersMap =
-		new HashMap<String, List<User>>();
 
 }
