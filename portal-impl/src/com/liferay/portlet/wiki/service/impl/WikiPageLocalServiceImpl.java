c5a8c52276fea6982fc7f2349a4cebd57c69d6a5
Hugo Huijser <hugo.huijser@liferay.com>
2012-04-18 14:09:22 -0700

    LPS-26542 Source formatting

--- portal-impl/src/com/liferay/portlet/wiki/service/impl/WikiPageLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/wiki/service/impl/WikiPageLocalServiceImpl.java
@@ -1563,7 +1563,7 @@ public class WikiPageLocalServiceImpl extends WikiPageLocalServiceBaseImpl {
 
 		String portletId = serviceContext.getPortletId();
 
-		if ((portletId == null) ||
+		if (Validator.isNull(portletId) ||
 			!portletId.startsWith(PortletKeys.WIKI_DISPLAY)) {
 
 			preferences = ServiceContextUtil.getPortletPreferences(
