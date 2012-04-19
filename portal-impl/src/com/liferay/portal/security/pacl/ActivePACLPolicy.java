40fd5446d072e2617f3dc496806ec7752298e1ce
Brian Chan <brian.chan@liferay.com>
2012-04-19 15:18:25 -0700

    LPS-26321 Initialize checkers from properties

--- portal-impl/src/com/liferay/portal/security/pacl/ActivePACLPolicy.java
+++ portal-impl/src/com/liferay/portal/security/pacl/ActivePACLPolicy.java
@@ -14,19 +14,11 @@
 
 package com.liferay.portal.security.pacl;
 
-import com.liferay.portal.security.pacl.checker.BaseChecker;
-import com.liferay.portal.security.pacl.checker.FileChecker;
-import com.liferay.portal.security.pacl.checker.HookChecker;
-import com.liferay.portal.security.pacl.checker.RuntimeChecker;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.security.pacl.checker.Checker;
 import com.liferay.portal.security.pacl.checker.SQLChecker;
-import com.liferay.portal.security.pacl.checker.ServiceChecker;
-import com.liferay.portal.security.pacl.checker.SocketChecker;
-import com.liferay.portal.security.pacl.permission.PortalHookPermission;
-import com.liferay.portal.security.pacl.permission.PortalServicePermission;
-
-import java.io.FilePermission;
-
-import java.net.SocketPermission;
 
 import java.security.Permission;
 
@@ -45,54 +37,24 @@ public class ActivePACLPolicy extends BasePACLPolicy {
 
 		super(servletContextName, classLoader, properties);
 
-		// We can probably instantiate these from a props list?
-
-		_fileChecker = new FileChecker(this);
-		_hookChecker = new HookChecker(this);
-		_runtimeChecker = new RuntimeChecker(this);
-		_serviceChecker = new ServiceChecker(this);
-		_socketChecker = new SocketChecker(this);
-		_sqlChecker = new SQLChecker(this);
-
-		//_checkerMap.put(AllPermission.class.getName(), _allChecker);
-		//_checkerMap.put(AuthPermission.class.getName(), _allChecker);
-		//_checkerMap.put(AWTPermission.class.getName(), _allChecker);
-		//_checkerMap.put(DelegationPermission.class.getName(), _allChecker);
-		_checkerMap.put(FilePermission.class.getName(), _fileChecker);
-		_checkerMap.put(PortalHookPermission.class.getName(), _hookChecker);
-		//_checkerMap.put(LoggingPermission.class.getName(), _allChecker);
-		//_checkerMap.put(ManagementPermission.class.getName(), _allChecker);
-		//_checkerMap.put(MBeanPermission.class.getName(), _allChecker);
-		//_checkerMap.put(MBeanServerPermission.class.getName(), _allChecker);
-		//_checkerMap.put(MBeanTrustPermission.class.getName(), _allChecker);
-		//_checkerMap.put(NetPermission.class.getName(), _allChecker);
-		_checkerMap.put(
-			PortalServicePermission.class.getName(), _serviceChecker);
-		//_checkerMap.put(PortalSQLPermission.class.getName(), _sqlChecker);
-		//_checkerMap.put(PrivateCredentialPermission.class.getName(), _allChecker);
-		//_checkerMap.put(PropertyPermission.class.getName(), _allChecker);
-		//_checkerMap.put(ReflectPermission.class.getName(), _allChecker);
-		_checkerMap.put(RuntimePermission.class.getName(), _runtimeChecker);
-		//_checkerMap.put(SecurityPermission.class.getName(), _allChecker);
-		//_checkerMap.put(SerializablePermission.class.getName(), _allChecker);
-		//_checkerMap.put(ServicePermission.class.getName(), _allChecker);
-		//_checkerMap.put(SQLPermission.class.getName(), _allChecker);
-		_checkerMap.put(SocketPermission.class.getName(), _socketChecker);
-		//_checkerMap.put(SSLPermission.class.getName(), _allChecker);
-		//_checkerMap.put(SubjectDelegationPermission.class.getName(), _allChecker);
-		//_checkerMap.put(UnresolvedPermission.class.getName(), _allChecker);
+		try {
+			initCheckers();
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
 	}
 
 	public void checkPermission(Permission permission) {
-		BaseChecker checker = getChecker(permission.getClass().getName());
+		Checker checker = getChecker(permission.getClass().getName());
 
 		if (checker != null) {
 			checker.checkPermission(permission);
 		}
 	}
 
-	public BaseChecker getChecker(String permissionClassName) {
-		return _checkerMap.get(permissionClassName);
+	public Checker getChecker(String permissionClassName) {
+		return _checkers.get(permissionClassName);
 	}
 
 	public boolean hasSQL(String sql) {
@@ -103,13 +65,45 @@ public class ActivePACLPolicy extends BasePACLPolicy {
 		return true;
 	}
 
-	private Map<String, BaseChecker> _checkerMap =
-		new HashMap<String, BaseChecker>();
-	private FileChecker _fileChecker;
-	private HookChecker _hookChecker;
-	private RuntimeChecker _runtimeChecker;
-	private ServiceChecker _serviceChecker;
-	private SocketChecker _socketChecker;
+	protected void initCheckers() throws Exception {
+		Class<?> clazz = getClass();
+
+		ClassLoader classLoader = clazz.getClassLoader();
+
+		Properties portalProperties = PropsUtil.getProperties(
+			"portal.security.manager.pacl.policy.checker", false);
+
+		for (Map.Entry<Object, Object> entry : portalProperties.entrySet()) {
+			String key = (String)entry.getKey();
+
+			int x = key.indexOf("[");
+			int y = key.indexOf("]");
+
+			String permissionClassName = key.substring(x + 1, y);
+
+			String checkerClassName = (String)entry.getValue();
+
+			Class<?> checkerClass = classLoader.loadClass(checkerClassName);
+
+			Checker checker = (Checker)checkerClass.newInstance();
+
+			checker.setPACLPolicy(this);
+
+			checker.afterPropertiesSet();
+
+			_checkers.put(permissionClassName, checker);
+		}
+
+		_sqlChecker = new SQLChecker();
+
+		_sqlChecker.setPACLPolicy(this);
+
+		_sqlChecker.afterPropertiesSet();
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(ActivePACLPolicy.class);
+
+	private Map<String, Checker> _checkers = new HashMap<String, Checker>();
 	private SQLChecker _sqlChecker;
 
 }
