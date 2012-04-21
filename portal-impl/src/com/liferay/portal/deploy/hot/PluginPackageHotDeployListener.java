53977b09bd12830ec98b29cafb34299ca02b3197
Brian Chan <brian.chan@liferay.com>
2012-04-21 14:52:54 -0700

    LPS-26321 calls during the undeploy lifecycle of a portlet should still be protected. Remove PACL policy manager only after eveything has been undeployed. Add PACL policy manager before anything is deployed.

--- portal-impl/src/com/liferay/portal/deploy/hot/PluginPackageHotDeployListener.java
+++ portal-impl/src/com/liferay/portal/deploy/hot/PluginPackageHotDeployListener.java
@@ -163,8 +163,6 @@ public class PluginPackageHotDeployListener extends BaseHotDeployListener {
 
 		unregisterClpMessageListeners(servletContext);
 
-		unregisterPACL(hotDeployEvent.getContextClassLoader());
-
 		if (_log.isInfoEnabled()) {
 			_log.info(
 				"Plugin package " + pluginPackage.getModuleId() +
@@ -356,10 +354,6 @@ public class PluginPackageHotDeployListener extends BaseHotDeployListener {
 			servletContext.getServletContextName(), classLoader, properties);
 
 		PACLPolicyManager.register(classLoader, paclPolicy);
-	}
-
-	protected void unregisterPACL(ClassLoader classLoader) {
-		PACLPolicyManager.unregister(classLoader);
 	}
 
 	private static final String _MULTI_VM_PORTAL_CACHE_MANAGER_BEAN_NAME =
