70b7791a0854870585aa24761197c08e4fae7e42
Brian Chan <brian.chan@liferay.com>
2012-04-23 11:06:33 -0700

    LPS-26916 Alphabetize

--- sql/update-6.1.1-6.2.0.sql
+++ sql/update-6.1.1-6.2.0.sql
@@ -230,9 +230,9 @@ update Country set name = 'yemen' where name = 'Yemen';
 update Country set name = 'zambia' where name = 'Zambia';
 update Country set name = 'zimbabwe' where name = 'Zimbabwe';
 
-alter table JournalArticle add folderId LONG;
-
 drop table Groups_Permissions;
+
+alter table JournalArticle add folderId LONG;
 
 drop table OrgGroupPermission;
 
