bef6eac97b1fee788cc7f4f0e1f6943feb38dfc5
Ken Duenwald <ken.duenwald@liferay.com>
2012-04-23 18:07:06 -0700

    Test Fixes - socialofficehome/notifications

--- portal-web/test/com/liferay/portalweb/socialofficehome/notifications/notification/requestprofileaddasconnection/ViewIgnoreAddAsConnectionProfileUserTest.java
+++ portal-web/test/com/liferay/portalweb/socialofficehome/notifications/notification/requestprofileaddasconnection/ViewIgnoreAddAsConnectionProfileUserTest.java
@@ -130,7 +130,7 @@ public class ViewIgnoreAddAsConnectionProfileUserTest extends BaseTestCase {
 				"//div[contains(@class, 'contacts-profile')]/div/div[2]/div[3]"));
 		assertFalse(selenium.isElementPresent(
 				"//div[contains(@class, 'contacts-center-home-content')]"));
-		assertEquals(RuntimeVariables.replace("Add as Connection"),
+		assertEquals(RuntimeVariables.replace("Add Connection"),
 			selenium.getText(
 				"//button[@id='_1_WAR_contactsportlet_addConnectionButton']"));
 		assertFalse(selenium.isVisible(
