51efeb2a77ae7d0af4f79a15af8059f61710a331
Brian Chan <brian.chan@liferay.com>
2012-04-25 11:19:55 -0700

    LPS-26321 Revert back changes that didn't use isJSPCompiler for now. For some reason, doesn't work on my machine. JSP compilation takes 30-45 minutes per JSP without rolling it back. Not the case for Ray. So will need to investigate.

--- portal-impl/src/com/liferay/portal/security/pacl/checker/BaseChecker.java
+++ portal-impl/src/com/liferay/portal/security/pacl/checker/BaseChecker.java
@@ -14,11 +14,16 @@
 
 package com.liferay.portal.security.pacl.checker;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.security.pacl.PACLClassLoaderUtil;
 import com.liferay.portal.security.pacl.PACLConstants;
 import com.liferay.portal.security.pacl.PACLPolicy;
 
 import java.util.Properties;
 import java.util.Set;
+
+import sun.reflect.Reflection;
 
 /**
  * @author Brian Wing Shun Chan
@@ -81,6 +86,62 @@ public abstract class BaseChecker implements Checker, PACLConstants {
 	protected Set<String> getPropertySet(String key) {
 		return _paclPolicy.getPropertySet(key);
 	}
+
+	protected boolean isJSPCompiler(String subject, String action) {
+		for (int i = 1;; i++) {
+			Class<?> callerClass = Reflection.getCallerClass(i);
+
+			if (callerClass == null) {
+				break;
+			}
+
+			String callerClassName = callerClass.getName();
+
+			if (callerClassName.startsWith(
+					_PACKAGE_ORG_APACHE_JASPER_COMPILER) ||
+				callerClassName.startsWith(
+					_PACKAGE_ORG_APACHE_JASPER_XMLPARSER) ||
+				callerClassName.startsWith(
+					_PACKAGE_ORG_APACHE_NAMING_RESOURCES) ||
+				callerClassName.equals(_ClASS_NAME_JASPER_LOADER)) {
+
+				ClassLoader callerClassLoader =
+					PACLClassLoaderUtil.getClassLoader(callerClass);
+
+				if (callerClassLoader != _commonClassLoader) {
+					_log.error(
+						"A plugin is hijacking the JSP compiler via " +
+							callerClassName + " to " + action + " " + subject);
+
+					return false;
+				}
+
+				if (_log.isDebugEnabled()) {
+					_log.debug(
+						"Allowing the JSP compiler via " + callerClassName +
+							" to " + action + " " + subject);
+				}
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private static final String _ClASS_NAME_JASPER_LOADER =
+		"org.apache.jasper.servlet.JasperLoader";
+
+	private static final String _PACKAGE_ORG_APACHE_JASPER_COMPILER =
+		"org.apache.jasper.compiler.";
+
+	private static final String _PACKAGE_ORG_APACHE_JASPER_XMLPARSER =
+		"org.apache.jasper.xmlparser.";
+
+	private static final String _PACKAGE_ORG_APACHE_NAMING_RESOURCES =
+		"org.apache.naming.resources";
+
+	private static Log _log = LogFactoryUtil.getLog(BaseChecker.class);
 
 	private ClassLoader _commonClassLoader;
 	private PACLPolicy _paclPolicy;
