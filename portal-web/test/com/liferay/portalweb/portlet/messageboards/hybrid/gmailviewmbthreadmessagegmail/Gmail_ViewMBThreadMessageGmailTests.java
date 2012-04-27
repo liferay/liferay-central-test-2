fae2032114c04ecd3b8ac3398ed6454a014250a0
Kiyoshi Lee <kiyoshi.lee@liferay.com>
2012-04-27 16:20:37 -0700

    LPS-27044

---
+++ portal-web/test/com/liferay/portalweb/portlet/messageboards/hybrid/gmailviewmbthreadmessagegmail/Gmail_ViewMBThreadMessageGmailTests.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portalweb.portlet.messageboards.hybrid.gmailviewmbthreadmessagegmail;
+
+import com.liferay.portalweb.portal.BaseTestSuite;
+import com.liferay.portalweb.portal.controlpanel.sites.site.addmemberssiteuser.AddMembersSiteUserTest;
+import com.liferay.portalweb.portal.controlpanel.sites.site.addsite.AddSiteTest;
+import com.liferay.portalweb.portal.controlpanel.sites.siteportlet.addportletsite.AddPageMBSiteTest;
+import com.liferay.portalweb.portal.controlpanel.sites.siteportlet.addportletsite.AddPortletMBSiteTest;
+import com.liferay.portalweb.portlet.messageboards.message.gmailviewmbthreadmessagegmail.AddMBCategorySiteTest;
+import com.liferay.portalweb.portlet.messageboards.message.gmailviewmbthreadmessagegmail.AddUserGmailTest;
+import com.liferay.portalweb.portlet.messageboards.message.gmailviewmbthreadmessagegmail.ConfigurePortletEmailFromAddressTest;
+import com.liferay.portalweb.portlet.messageboards.message.gmailviewmbthreadmessagegmail.EditMBCategoryMailingListActiveActionsTest;
+import com.liferay.portalweb.portlet.messageboards.message.gmailviewmbthreadmessagegmail.PostNewMBCategoryThreadMessageSiteTest;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class Gmail_ViewMBThreadMessageGmailTests extends BaseTestSuite {
+	public static Test suite() {
+		TestSuite testSuite = new TestSuite();
+		testSuite.addTestSuite(AddSiteTest.class);
+		testSuite.addTestSuite(AddUserGmailTest.class);
+		testSuite.addTestSuite(AddMembersSiteUserTest.class);
+		testSuite.addTestSuite(AddPageMBSiteTest.class);
+		testSuite.addTestSuite(AddPortletMBSiteTest.class);
+		testSuite.addTestSuite(ConfigurePortletEmailFromAddressTest.class);
+		testSuite.addTestSuite(AddMBCategorySiteTest.class);
+		testSuite.addTestSuite(EditMBCategoryMailingListActiveActionsTest.class);
+		testSuite.addTestSuite(PostNewMBCategoryThreadMessageSiteTest.class);
+
+		return testSuite;
+	}
+}
