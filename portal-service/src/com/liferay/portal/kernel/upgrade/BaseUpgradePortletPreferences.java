a49c6bf00ec4304149550fea9d36a5ef454e1329
Minhchau Dang <minhchau.dang@liferay.com>
2012-04-27 14:19:20 -0700

    LPS-27043 Only retrieve necessary columns

--- portal-service/src/com/liferay/portal/kernel/upgrade/BaseUpgradePortletPreferences.java
+++ portal-service/src/com/liferay/portal/kernel/upgrade/BaseUpgradePortletPreferences.java
@@ -80,7 +80,7 @@ public abstract class BaseUpgradePortletPreferences extends UpgradeProcess {
 		try {
 			con = DataAccess.getConnection();
 
-			ps = con.prepareStatement(_GET_GROUP);
+			ps = con.prepareStatement(_GET_GROUP_COMPANY_ID);
 
 			ps.setLong(1, groupId);
 
@@ -314,11 +314,12 @@ public abstract class BaseUpgradePortletPreferences extends UpgradeProcess {
 			String portletId, String xml)
 		throws Exception;
 
-	private static final String _GET_GROUP =
-		"select * from Group_ where groupId = ?";
+	private static final String _GET_GROUP_COMPANY_ID =
+		"select companyId from Group_ where groupId = ?";
 
 	private static final String _GET_LAYOUT =
-		"select * from Layout where plid = ?";
+		"select groupId, companyId, privateLayout, layoutId from Layout " +
+			"where plid = ?";
 
 	private static final String _GET_LAYOUT_UUID =
 		"select uuid_ from Layout where groupId = ? and privateLayout = ? " +
