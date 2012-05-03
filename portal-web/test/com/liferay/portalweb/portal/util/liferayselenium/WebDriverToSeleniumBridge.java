5e9d5dcfeb6780d7b4ae199cdf60d127d9ed0786
Michael Hashimoto <michael.hashimoto@liferay.com>
2012-05-03 14:57:35 -0700

    LPS-25995 Copied "WebDriverToSeleniumBridge.java" from "ee-6.1.x" GIT ID 45781727c8bca4e4e84e7cb6feb713b7e654ceaf

--- portal-web/test/com/liferay/portalweb/portal/util/liferayselenium/WebDriverToSeleniumBridge.java
+++ portal-web/test/com/liferay/portalweb/portal/util/liferayselenium/WebDriverToSeleniumBridge.java
@@ -14,13 +14,26 @@
 
 package com.liferay.portalweb.portal.util.liferayselenium;
 
+import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portalweb.portal.util.TestPropsValues;
 
 import com.thoughtworks.selenium.Selenium;
 
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverBackedSelenium;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 /**
  * @author Brian Wing Shun Chan
@@ -30,6 +43,8 @@ public class WebDriverToSeleniumBridge
 
 	public WebDriverToSeleniumBridge(WebDriver webDriver) {
 		super(webDriver);
+
+		initKeys();
 	}
 
 	public void addCustomRequestHeader(String key, String value) {
@@ -47,7 +62,21 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public void addSelection(String locator, String optionLocator) {
-		throw new UnsupportedOperationException();
+		Select select = new Select(getWebElement(locator));
+
+		if (optionLocator.startsWith("index=")) {
+			select.selectByIndex(
+				GetterUtil.getInteger(optionLocator.substring(6)));
+		}
+		else if (optionLocator.startsWith("label=")) {
+			select.selectByVisibleText(optionLocator.substring(6));
+		}
+		else if (optionLocator.startsWith("value=")) {
+			select.selectByValue(optionLocator.substring(6));
+		}
+		else {
+			select.selectByVisibleText(optionLocator);
+		}
 	}
 
 	public void allowNativeXpath(String allow) {
@@ -95,7 +124,11 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public void check(String locator) {
-		throw new UnsupportedOperationException();
+		WebElement webElement = getWebElement(locator);
+
+		if (!webElement.isSelected()) {
+			webElement.click();
+		}
 	}
 
 	public void chooseCancelOnNextConfirmation() {
@@ -107,16 +140,34 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public void click(String locator) {
-		throw new UnsupportedOperationException();
+		WebElement webElement = getWebElement(locator);
+
+		webElement.click();
 	}
 
 	public void clickAt(String locator, String coordString) {
-		throw new UnsupportedOperationException();
+		Actions actions = new Actions(this);
+
+		WebElement webElement = getWebElement(locator);
+
+		if (coordString.contains(",")) {
+			String[] coords = coordString.split(",");
+
+			int x = GetterUtil.getInteger(coords[0]);
+			int y = GetterUtil.getInteger(coords[1]);
+
+			actions.moveToElement(webElement, x, y);
+
+			actions.click();
+		}
+		else {
+			webElement.click();
+		}
 	}
 
 	@Override
 	public void close() {
-		throw new UnsupportedOperationException();
+		super.close();
 	}
 
 	public void contextMenu(String locator) {
@@ -152,7 +203,15 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public void doubleClick(String locator) {
-		throw new UnsupportedOperationException();
+		Actions actions = new Actions(this);
+
+		WebElement webElement = getWebElement(locator);
+
+		actions.doubleClick(webElement);
+
+		actions.build();
+
+		actions.perform();
 	}
 
 	public void doubleClickAt(String locator, String coordString) {
@@ -167,7 +226,19 @@ public class WebDriverToSeleniumBridge
 		String locatorOfObjectToBeDragged,
 		String locatorOfDragDestinationObject) {
 
-		throw new UnsupportedOperationException();
+		Actions actions = new Actions(this);
+
+		WebElement objectToBeDraggedWebElement = getWebElement(
+			locatorOfObjectToBeDragged);
+		WebElement dragDestinationObjectWebElement = getWebElement(
+			locatorOfDragDestinationObject);
+
+		actions.dragAndDrop(
+			objectToBeDraggedWebElement, dragDestinationObjectWebElement);
+
+		actions.build();
+
+		actions.perform();
 	}
 
 	public void dragdrop(String locator, String movementsString) {
@@ -183,7 +254,11 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public String getAlert() {
-		throw new UnsupportedOperationException();
+		WebDriver.TargetLocator targetLocator = switchTo();
+
+		Alert alert = targetLocator.alert();
+
+		return alert.getText();
 	}
 
 	public String[] getAllButtons() {
@@ -211,7 +286,15 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public String getAttribute(String attributeLocator) {
-		throw new UnsupportedOperationException();
+		int pos = attributeLocator.lastIndexOf(CharPool.AT);
+
+		String locator = attributeLocator.substring(0, pos - 1);
+
+		WebElement webElement = getWebElement(locator);
+
+		String attribute = attributeLocator.substring(pos);
+
+		return webElement.getAttribute(attribute);
 	}
 
 	public String[] getAttributeFromAllWindows(String attributeName) {
@@ -219,11 +302,19 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public String getBodyText() {
-		throw new UnsupportedOperationException();
+		WebElement webElement = findElement(By.tagName("body"));
+
+		return webElement.getText();
 	}
 
 	public String getConfirmation() {
-		throw new UnsupportedOperationException();
+		WebDriver.TargetLocator targetLocator = switchTo();
+
+		Alert alert = targetLocator.alert();
+
+		acceptConfirmation();
+
+		return alert.getText();
 	}
 
 	public String getCookie() {
@@ -271,11 +362,11 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public String getHtmlSource() {
-		throw new UnsupportedOperationException();
+		return getPageSource();
 	}
 
 	public String getLocation() {
-		throw new UnsupportedOperationException();
+		return getCurrentUrl();
 	}
 
 	public String getLog() {
@@ -307,11 +398,26 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public String getSelectedLabel(String selectLocator) {
-		throw new UnsupportedOperationException();
+		WebElement selectLocatorWebElement = getWebElement(selectLocator);
+
+		Select select = new Select(selectLocatorWebElement);
+
+		WebElement firstSelectedOptionWebElement =
+			select.getFirstSelectedOption();
+
+		return firstSelectedOptionWebElement.getText();
 	}
 
 	public String[] getSelectedLabels(String selectLocator) {
-		throw new UnsupportedOperationException();
+		WebElement selectLocatorWebElement = getWebElement(selectLocator);
+
+		Select select = new Select(selectLocatorWebElement);
+
+		List<WebElement> allSelectedOptionsWebElements =
+			select.getAllSelectedOptions();
+
+		return StringUtil.split(
+			ListUtil.toString(allSelectedOptionsWebElements, "text"));
 	}
 
 	public String getSelectedValue(String selectLocator) {
@@ -335,16 +441,20 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public String getText(String locator) {
-		throw new UnsupportedOperationException();
+		WebElement webElement = getWebElement(locator);
+
+		return webElement.getText();
 	}
 
 	@Override
 	public String getTitle() {
-		throw new UnsupportedOperationException();
+		return super.getTitle();
 	}
 
 	public String getValue(String locator) {
-		throw new UnsupportedOperationException();
+		WebElement webElement = getWebElement(locator);
+
+		return webElement.getAttribute("value");
 	}
 
 	public boolean getWhetherThisFrameMatchFrameExpression(
@@ -380,7 +490,9 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public boolean isChecked(String locator) {
-		throw new UnsupportedOperationException();
+		WebElement webElement = getWebElement(locator);
+
+		return webElement.isSelected();
 	}
 
 	public boolean isConfirmationPresent() {
@@ -396,7 +508,9 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public boolean isElementPresent(String locator) {
-		throw new UnsupportedOperationException();
+		List<WebElement> webElements = getWebElements(locator);
+
+		return !webElements.isEmpty();
 	}
 
 	public boolean isOrdered(String locator1, String locator2) {
@@ -412,11 +526,17 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public boolean isTextPresent(String pattern) {
-		throw new UnsupportedOperationException();
+		WebElement webElement = findElement(By.tagName("body"));
+
+		String text = webElement.getText();
+
+		return text.contains(pattern);
 	}
 
 	public boolean isVisible(String locator) {
-		throw new UnsupportedOperationException();
+		WebElement webElement = getWebElement(locator);
+
+		return webElement.isDisplayed();
 	}
 
 	public void keyDown(String locator, String keySequence) {
@@ -428,7 +548,26 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public void keyPress(String locator, String keySequence) {
-		throw new UnsupportedOperationException();
+		Actions actions = new Actions(this);
+
+		WebElement webElement = getWebElement(locator);
+
+		if (locator.startsWith("\\")) {
+			int index = GetterUtil.getInteger(keySequence.substring(1));
+
+			Keys keys = _keysArray[index];
+
+			actions.keyDown(webElement, keys);
+
+			actions.keyUp(webElement, keys);
+
+			actions.build();
+
+			actions.perform();
+		}
+		else {
+			webElement.sendKeys(keySequence);
+		}
 	}
 
 	public void keyPressNative(String keycode) {
@@ -452,7 +591,10 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public void mouseDown(String locator) {
-		throw new UnsupportedOperationException();
+		WebDriverBackedSelenium webDriverBackedSelenium =
+			new WebDriverBackedSelenium(this, getCurrentUrl());
+
+		webDriverBackedSelenium.mouseDown(locator);
 	}
 
 	public void mouseDownAt(String locator, String coordString) {
@@ -468,7 +610,10 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public void mouseMove(String locator) {
-		throw new UnsupportedOperationException();
+		WebDriverBackedSelenium webDriverBackedSelenium =
+			new WebDriverBackedSelenium(this, getCurrentUrl());
+
+		webDriverBackedSelenium.mouseMove(locator);
 	}
 
 	public void mouseMoveAt(String locator, String coordString) {
@@ -480,11 +625,17 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public void mouseOver(String locator) {
-		throw new UnsupportedOperationException();
+		WebDriverBackedSelenium webDriverBackedSelenium =
+			new WebDriverBackedSelenium(this, getCurrentUrl());
+
+		webDriverBackedSelenium.mouseOver(locator);
 	}
 
 	public void mouseUp(String locator) {
-		throw new UnsupportedOperationException();
+		WebDriverBackedSelenium webDriverBackedSelenium =
+			new WebDriverBackedSelenium(this, getCurrentUrl());
+
+		webDriverBackedSelenium.mouseUp(locator);
 	}
 
 	public void mouseUpAt(String locator, String coordString) {
@@ -500,7 +651,12 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public void open(String url) {
-		throw new UnsupportedOperationException();
+		if (url.startsWith("/")) {
+			get(TestPropsValues.PORTAL_URL + url);
+		}
+		else {
+			get(url);
+		}
 	}
 
 	public void open(String url, String ignoreResponseCode) {
@@ -508,11 +664,13 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public void openWindow(String url, String windowID) {
-		throw new UnsupportedOperationException();
+		open(url);
 	}
 
 	public void refresh() {
-		throw new UnsupportedOperationException();
+		WebDriver.Navigation navigation = navigate();
+
+		navigation.refresh();
 	}
 
 	public void removeAllSelections(String locator) {
@@ -540,11 +698,38 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public void select(String selectLocator, String optionLocator) {
-		throw new UnsupportedOperationException();
+		WebElement webElement = getWebElement(selectLocator);
+
+		Select select = new Select(webElement);
+
+		if (optionLocator.startsWith("index=")) {
+			select.selectByIndex(
+				GetterUtil.getInteger(optionLocator.substring(6)));
+		}
+		else if (optionLocator.startsWith("label=")) {
+			select.selectByVisibleText(optionLocator.substring(6));
+		}
+		else if (optionLocator.startsWith("value=")) {
+			select.selectByValue(optionLocator.substring(6));
+		}
+		else {
+			select.selectByVisibleText(optionLocator);
+		}
 	}
 
 	public void selectFrame(String locator) {
-		throw new UnsupportedOperationException();
+		if (locator.equals("relative=top")) {
+			WebDriver.TargetLocator targetLocator = switchTo();
+
+			targetLocator.defaultContent();
+		}
+		else {
+			WebElement webElement = getWebElement(locator);
+
+			WebDriver.TargetLocator targetLocator = switchTo();
+
+			targetLocator.frame(webElement);
+		}
 	}
 
 	public void selectPopUp(String windowID) {
@@ -552,7 +737,30 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public void selectWindow(String windowID) {
-		throw new UnsupportedOperationException();
+		Set<String> windowHandles = getWindowHandles();
+
+		if (!windowHandles.isEmpty()) {
+			String title = windowID;
+
+			if (title.startsWith("title=")) {
+				title = title.substring(6);
+			}
+
+			for (String windowHandle : windowHandles) {
+				WebDriver.TargetLocator targetLocator = switchTo();
+
+				targetLocator.window(windowHandle);
+
+				if (title.equals(getTitle())) {
+					return;
+				}
+			}
+		}
+		else if (windowID.equals("null")) {
+			WebDriver.TargetLocator targetLocator = switchTo();
+
+			targetLocator.defaultContent();
+		}
 	}
 
 	public void setBrowserLogLevel(String logLevel) {
@@ -629,15 +837,29 @@ public class WebDriverToSeleniumBridge
 	}
 
 	public void type(String locator, String value) {
-		throw new UnsupportedOperationException();
+		WebElement webElement = getWebElement(locator);
+
+		if (webElement.isEnabled()) {
+			webElement.clear();
+
+			webElement.sendKeys(value);
+		}
 	}
 
 	public void typeKeys(String locator, String value) {
-		throw new UnsupportedOperationException();
+		WebElement webElement = getWebElement(locator);
+
+		if (webElement.isEnabled()) {
+			webElement.sendKeys(value);
+		}
 	}
 
 	public void uncheck(String locator) {
-		throw new UnsupportedOperationException();
+		WebElement webElement = getWebElement(locator);
+
+		if (webElement.isSelected()) {
+			webElement.click();
+		}
 	}
 
 	public void useXpathLibrary(String libraryName) {
@@ -666,5 +888,137 @@ public class WebDriverToSeleniumBridge
 	public void windowMaximize() {
 		throw new UnsupportedOperationException();
 	}
+
+	protected void acceptConfirmation() {
+		WebDriver.TargetLocator targetLocator = switchTo();
+
+		Alert alert = targetLocator.alert();
+
+		alert.accept();
+	}
+
+	protected WebElement getWebElement(String locator) {
+		if (locator.startsWith("//")) {
+			return findElement(By.xpath(locator));
+		}
+		else if (locator.startsWith("class=")) {
+			return findElement(By.className(locator.substring(6)));
+		}
+		else if (locator.startsWith("css=")) {
+			return findElement(By.cssSelector(locator.substring(4)));
+		}
+		else if (locator.startsWith("link=")) {
+			return findElement(By.linkText(locator.substring(5)));
+		}
+		else if (locator.startsWith("name=")) {
+			return findElement(By.name(locator.substring(5)));
+		}
+		else if (locator.startsWith("tag=")) {
+			return findElement(By.tagName(locator.substring(4)));
+		}
+		else if (locator.startsWith("xpath=")) {
+			return findElement(By.xpath(locator.substring(6)));
+		}
+		else {
+			return findElement(By.id(locator));
+		}
+	}
+
+	protected List<WebElement> getWebElements(String locator) {
+		if (locator.startsWith("//")) {
+			return findElements(By.xpath(locator));
+		}
+		else if (locator.startsWith("class=")) {
+			return findElements(By.className(locator.substring(6)));
+		}
+		else if (locator.startsWith("css=")) {
+			return findElements(By.cssSelector(locator.substring(4)));
+		}
+		else if (locator.startsWith("link=")) {
+			return findElements(By.linkText(locator.substring(5)));
+		}
+		else if (locator.startsWith("name=")) {
+			return findElements(By.name(locator.substring(5)));
+		}
+		else if (locator.startsWith("tag=")) {
+			return findElements(By.tagName(locator.substring(4)));
+		}
+		else if (locator.startsWith("xpath=")) {
+			return findElements(By.xpath(locator.substring(6)));
+		}
+		else {
+			return findElements(By.id(locator));
+		}
+	}
+
+	protected void initKeys() {
+
+		// ASCII to WebDriver
+
+		_keysArray[107] = Keys.ADD;
+		_keysArray[18] = Keys.ALT;
+		_keysArray[40] = Keys.ARROW_DOWN;
+		_keysArray[37] = Keys.ARROW_LEFT;
+		_keysArray[39] = Keys.ARROW_RIGHT;
+		_keysArray[38] = Keys.ARROW_UP;
+		_keysArray[8] = Keys.BACK_SPACE;
+		//keyTable[] = Keys.CANCEL;
+		//keyTable[] = Keys.CLEAR;
+		//keyTable[] = Keys.COMMAND;
+		_keysArray[17] = Keys.CONTROL;
+		_keysArray[110] = Keys.DECIMAL;
+		_keysArray[46] = Keys.DELETE;
+		_keysArray[111] = Keys.DIVIDE;
+		//keyTable[] = Keys.DOWN;
+		//keyTable[] = Keys.END;
+		_keysArray[13] = Keys.ENTER;
+		//keyTable[] = Keys.EQUALS;
+		_keysArray[27] = Keys.ESCAPE;
+		_keysArray[112] = Keys.F1;
+		_keysArray[121] = Keys.F10;
+		_keysArray[122] = Keys.F11;
+		_keysArray[123] = Keys.F12;
+		_keysArray[113] = Keys.F2;
+		_keysArray[114] = Keys.F3;
+		_keysArray[115] = Keys.F4;
+		_keysArray[116] = Keys.F5;
+		_keysArray[117] = Keys.F6;
+		_keysArray[118] = Keys.F7;
+		_keysArray[119] = Keys.F8;
+		_keysArray[120] = Keys.F9;
+		//keyTable[] = Keys.HELP;
+		_keysArray[36] = Keys.HOME;
+		_keysArray[45] = Keys.INSERT;
+		//keyTable[] = Keys.LEFT;
+		//keyTable[] = Keys.LEFT_ALT;
+		//keyTable[] = Keys.LEFT_CONTROL;
+		//keyTable[] = Keys.LEFT_SHIFT;
+		//keyTable[] = Keys.META;
+		//keyTable[] = Keys.NULL;
+		_keysArray[96] = Keys.NUMPAD0;
+		_keysArray[97] = Keys.NUMPAD1;
+		_keysArray[98] = Keys.NUMPAD2;
+		_keysArray[99] = Keys.NUMPAD3;
+		_keysArray[100] = Keys.NUMPAD4;
+		_keysArray[101] = Keys.NUMPAD5;
+		_keysArray[102] = Keys.NUMPAD6;
+		_keysArray[103] = Keys.NUMPAD7;
+		_keysArray[104] = Keys.NUMPAD8;
+		_keysArray[105] = Keys.NUMPAD9;
+		_keysArray[34] = Keys.PAGE_DOWN;
+		_keysArray[33] = Keys.PAGE_UP;
+		_keysArray[19] = Keys.PAUSE;
+		//keyTable[] = Keys.RETURN;
+		//keyTable[] = Keys.RIGHT;
+		//keyTable[] = Keys.SEMICOLON;
+		//keyTable[] = Keys.SEPARATOR;
+		_keysArray[16] = Keys.SHIFT;
+		_keysArray[32] = Keys.SPACE;
+		_keysArray[109] = Keys.SUBTRACT;
+		_keysArray[9] = Keys.TAB;
+		//keyTable[] = Keys.UP;
+	}
+
+	private Keys[] _keysArray = new Keys[128];
 
 }
