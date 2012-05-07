juliocamarero/liferay-portal

LPS-27028 Added 'Removed-By' column to trash

portal-impl/src/com/liferay/portlet/trash/service/impl/TrashEntryLocalServiceImpl.java
@@ -18,6 +18,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.ObjectValuePair;
 import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.model.User;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portlet.trash.model.TrashEntry;
 import com.liferay.portlet.trash.model.TrashVersion;
@@ -37,7 +38,7 @@ public class TrashEntryLocalServiceImpl extends TrashEntryLocalServiceBaseImpl {
 	/**
 	 * Moves an entry to trash.
 	 *
-	 * @param  companyId the primary key of the entry's company
+	 * @param  userId the primary key of the user removing the entity
 	 * @param  groupId the primary key of the entry's group
 	 * @param  className the class name of the entity
 	 * @param  classPK the primary key of the entity
@@ -50,11 +51,12 @@ public class TrashEntryLocalServiceImpl extends TrashEntryLocalServiceBaseImpl {
 	 * @throws SystemException if a system exception occurred
 	 */
 	public TrashEntry addTrashEntry(
-			long companyId, long groupId, String className, long classPK,
+			long userId, long groupId, String className, long classPK,
 			int status, List<ObjectValuePair<Long, Integer>> versions,
 			UnicodeProperties typeSettingsProperties)
-		throws SystemException {
+		throws PortalException, SystemException {
 
+		User user = userPersistence.findByPrimaryKey(userId);
 		long classNameId = PortalUtil.getClassNameId(className);
 
 		long entryId = counterLocalService.increment();
@@ -62,10 +64,12 @@ public class TrashEntryLocalServiceImpl extends TrashEntryLocalServiceBaseImpl {
 		TrashEntry trashEntry = trashEntryPersistence.create(entryId);
 
 		trashEntry.setGroupId(groupId);
-		trashEntry.setCompanyId(companyId);
+		trashEntry.setCompanyId(user.getCompanyId());
 		trashEntry.setCreateDate(new Date());
 		trashEntry.setClassNameId(classNameId);
 		trashEntry.setClassPK(classPK);
+		trashEntry.setDeletedByUserId(user.getUserId());
+		trashEntry.setDeletedByUserName(user.getFullName());
 
 		if (typeSettingsProperties != null) {
 			trashEntry.setTypeSettingsProperties(typeSettingsProperties);

portal-web/docroot/html/portlet/trash/view.jsp
@@ -119,6 +119,11 @@ portletURL.setParameter("tabs1", tabs1);
 				value="<%= dateFormatDateTime.format(entry.getCreateDate()) %>"
 			/>
 
+			<liferay-ui:search-container-column-text
+				name="removed-by"
+				value="<%= entry.getDeletedByUserName() %>"
+			/>
+
 			<liferay-ui:search-container-column-jsp
 				align="right"
 				path="/html/portlet/trash/trash_entry_actions.jsp"

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portlet/trash/service/impl/TrashEntryLocalServiceImpl.java
@@ -18,6 +18,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.ObjectValuePair;
 import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.model.User;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portlet.trash.model.TrashEntry;
 import com.liferay.portlet.trash.model.TrashVersion;
@@ -37,7 +38,7 @@ public class TrashEntryLocalServiceImpl extends TrashEntryLocalServiceBaseImpl {
 	/**
 	 * Moves an entry to trash.
 	 *
-	 * @param  companyId the primary key of the entry's company
+	 * @param  userId the primary key of the user removing the entity
 	 * @param  groupId the primary key of the entry's group
 	 * @param  className the class name of the entity
 	 * @param  classPK the primary key of the entity
@@ -50,11 +51,12 @@ public class TrashEntryLocalServiceImpl extends TrashEntryLocalServiceBaseImpl {
 	 * @throws SystemException if a system exception occurred
 	 */
 	public TrashEntry addTrashEntry(
-			long companyId, long groupId, String className, long classPK,
+			long userId, long groupId, String className, long classPK,
 			int status, List<ObjectValuePair<Long, Integer>> versions,
 			UnicodeProperties typeSettingsProperties)
-		throws SystemException {
+		throws PortalException, SystemException {
 
+		User user = userPersistence.findByPrimaryKey(userId);
 		long classNameId = PortalUtil.getClassNameId(className);
 
 		long entryId = counterLocalService.increment();
@@ -62,7 +64,9 @@ public class TrashEntryLocalServiceImpl extends TrashEntryLocalServiceBaseImpl {
 		TrashEntry trashEntry = trashEntryPersistence.create(entryId);
 
 		trashEntry.setGroupId(groupId);
-		trashEntry.setCompanyId(companyId);
+		trashEntry.setCompanyId(user.getCompanyId());
+		trashEntry.setUserId(user.getUserId());
+		trashEntry.setUserName(user.getFullName());
 		trashEntry.setCreateDate(new Date());
 		trashEntry.setClassNameId(classNameId);
 		trashEntry.setClassPK(classPK);

portal-web/docroot/html/portlet/trash/view.jsp
@@ -121,6 +121,11 @@ portletURL.setParameter("tabs1", tabs1);
 				value="<%= dateFormatDateTime.format(entry.getCreateDate()) %>"
 			/>
 
+			<liferay-ui:search-container-column-text
+				name="removed-by"
+				value="<%= entry.getUserName() %>"
+			/>
+
 			<liferay-ui:search-container-column-jsp
 				align="right"
 				path="/html/portlet/trash/trash_entry_actions.jsp"