juliocamarero/liferay-portal

LPS-27028 Generated

portal-impl/test/integration/com/liferay/portlet/trash/service/persistence/TrashEntryPersistenceTest.java
@@ -97,6 +97,10 @@ public class TrashEntryPersistenceTest {
 
 		newTrashEntry.setStatus(ServiceTestUtil.nextInt());
 
+		newTrashEntry.setDeletedByUserId(ServiceTestUtil.nextLong());
+
+		newTrashEntry.setDeletedByUserName(ServiceTestUtil.randomString());
+
 		_persistence.update(newTrashEntry, false);
 
 		TrashEntry existingTrashEntry = _persistence.findByPrimaryKey(newTrashEntry.getPrimaryKey());
@@ -118,6 +122,10 @@ public class TrashEntryPersistenceTest {
 			newTrashEntry.getTypeSettings());
 		Assert.assertEquals(existingTrashEntry.getStatus(),
 			newTrashEntry.getStatus());
+		Assert.assertEquals(existingTrashEntry.getDeletedByUserId(),
+			newTrashEntry.getDeletedByUserId());
+		Assert.assertEquals(existingTrashEntry.getDeletedByUserName(),
+			newTrashEntry.getDeletedByUserName());
 	}
 
 	@Test
@@ -268,6 +276,10 @@ public class TrashEntryPersistenceTest {
 		trashEntry.setTypeSettings(ServiceTestUtil.randomString());
 
 		trashEntry.setStatus(ServiceTestUtil.nextInt());
+
+		trashEntry.setDeletedByUserId(ServiceTestUtil.nextLong());
+
+		trashEntry.setDeletedByUserName(ServiceTestUtil.randomString());
 
 		_persistence.update(trashEntry, false);
 

liferay/liferay-central-test-2

portal-impl/test/integration/com/liferay/portlet/trash/service/persistence/TrashEntryPersistenceTest.java
@@ -87,6 +87,10 @@ public class TrashEntryPersistenceTest {
 
 		newTrashEntry.setCompanyId(ServiceTestUtil.nextLong());
 
+		newTrashEntry.setUserId(ServiceTestUtil.nextLong());
+
+		newTrashEntry.setUserName(ServiceTestUtil.randomString());
+
 		newTrashEntry.setCreateDate(ServiceTestUtil.nextDate());
 
 		newTrashEntry.setClassNameId(ServiceTestUtil.nextLong());
@@ -107,6 +111,10 @@ public class TrashEntryPersistenceTest {
 			newTrashEntry.getGroupId());
 		Assert.assertEquals(existingTrashEntry.getCompanyId(),
 			newTrashEntry.getCompanyId());
+		Assert.assertEquals(existingTrashEntry.getUserId(),
+			newTrashEntry.getUserId());
+		Assert.assertEquals(existingTrashEntry.getUserName(),
+			newTrashEntry.getUserName());
 		Assert.assertEquals(Time.getShortTimestamp(
 				existingTrashEntry.getCreateDate()),
 			Time.getShortTimestamp(newTrashEntry.getCreateDate()));
@@ -258,6 +266,10 @@ public class TrashEntryPersistenceTest {
 		trashEntry.setGroupId(ServiceTestUtil.nextLong());
 
 		trashEntry.setCompanyId(ServiceTestUtil.nextLong());
+
+		trashEntry.setUserId(ServiceTestUtil.nextLong());
+
+		trashEntry.setUserName(ServiceTestUtil.randomString());
 
 		trashEntry.setCreateDate(ServiceTestUtil.nextDate());
 