0b41c145e67b2e3548a0d28fba4e29bc43f102e1
Brian Chan <brian.chan@liferay.com>
2012-05-08 14:18:15 -0700

    LPS-27177 Source formatting

--- util-taglib/src/com/liferay/taglib/util/VelocityTaglib.java
+++ util-taglib/src/com/liferay/taglib/util/VelocityTaglib.java
@@ -474,6 +474,17 @@ public class VelocityTaglib {
 		mySitesTag.runTag();
 	}
 
+	public void permissionsURL(
+			String redirect, String modelResource,
+			String modelResourceDescription, long resourceGroupId,
+			String resourcePrimKey, String windowState, int[] roleTypes)
+		throws Exception {
+
+		PermissionsURLTag.doTag(
+			redirect, modelResource, modelResourceDescription, resourceGroupId,
+			resourcePrimKey, windowState, null, roleTypes, _pageContext);
+	}
+
 	/**
 	 * @deprecated {@link #permissionsURL(String, String, String, long, String,
 	 *             String, int[])}
@@ -487,17 +498,6 @@ public class VelocityTaglib {
 		permissionsURL(
 			redirect, modelResourceDescription, modelResourceDescription, 0,
 			resourcePrimKey, windowState, roleTypes);
-	}
-
-	public void permissionsURL(
-			String redirect, String modelResource,
-			String modelResourceDescription, long resourceGroupId,
-			String resourcePrimKey, String windowState, int[] roleTypes)
-		throws Exception {
-
-		PermissionsURLTag.doTag(
-			redirect, modelResource, modelResourceDescription, resourceGroupId,
-			resourcePrimKey, windowState, null, roleTypes, _pageContext);
 	}
 
 	public void renderURL(long plid, String portletName, String queryString)
