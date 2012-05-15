64133f0f084361386af854a86fab1379abf27f9d
Preston Crary <nathaniel.crary@liferay.com>
2012-05-15 11:18:31 -0700

    LPS-27087 user's name was double escaped

--- portal-service/src/com/liferay/portal/util/SubscriptionSender.java
+++ portal-service/src/com/liferay/portal/util/SubscriptionSender.java
@@ -92,8 +92,7 @@ public class SubscriptionSender implements Serializable {
 
 	public void addRuntimeSubscribers(String toAddress, String toName) {
 		ObjectValuePair<String, String> ovp =
-			new ObjectValuePair<String, String>(
-				toAddress, HtmlUtil.escape(toName));
+			new ObjectValuePair<String, String>(toAddress, toName);
 
 		_runtimeSubscribersOVPs.add(ovp);
 	}
