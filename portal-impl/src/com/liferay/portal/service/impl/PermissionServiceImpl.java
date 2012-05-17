6afd2f77f9776bfdd1758db8b505d13d04bb695b
Brian Chan <brian.chan@liferay.com>
2012-05-17 15:00:37 -0700

    LPS-27341 Source formatting

--- portal-impl/src/com/liferay/portal/service/impl/PermissionServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/PermissionServiceImpl.java
@@ -270,7 +270,7 @@ public class PermissionServiceImpl extends PermissionServiceBaseImpl {
 			if (resourceBlockLocalService.isSupported(name)) {
 				PermissionedModel permissionedModel =
 					resourceBlockLocalService.getPermissionedModel(
-						name, Long.valueOf(primKey));
+						name, GetterUtil.getLong(primKey));
 
 				if (permissionedModel instanceof GroupedModel) {
 					GroupedModel groupedModel = (GroupedModel)permissionedModel;
