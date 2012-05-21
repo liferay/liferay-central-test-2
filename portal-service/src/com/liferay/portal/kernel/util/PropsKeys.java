a75968b213974fb6f20f5492af46a823821431a9
samuel.kong <samuel.kong@liferay.com>
2012-05-21 21:39:12 +0800

    LPS-27403 Fix for LPS-18184 is insufficient

--- portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -1749,7 +1749,7 @@ public interface PropsKeys {
 
 	public static final String RESOURCE_REPOSITORIES_ROOT = "resource.repositories.root";
 
-	public static final String REST_PROXY_DOMAINS_ALLOWED = "rest.proxy.domains.allowed";
+	public static final String REST_PROXY_URL_PREFIXES_ALLOWED = "rest.proxy.url.prefixes.allowed";
 
 	public static final String ROBOTS_TXT_WITH_SITEMAP = "robots.txt.with.sitemap";
 
