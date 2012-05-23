414fd86ba68ad1a45f7074340bc3eb2c26f267ba
Mandy Zia <mandy.zia@liferay.com>
2012-05-23 14:49:04 -0700

    SOS-894

--- portal-web/test/com/liferay/portalweb/socialofficeprofile/profile/souseditadditionalemailaddressnullprofile/SOUs_EditAdditionalEmailAddressNullProfileTests.java
+++ portal-web/test/com/liferay/portalweb/socialofficeprofile/profile/souseditadditionalemailaddressnullprofile/SOUs_EditAdditionalEmailAddressNullProfileTests.java
@@ -15,6 +15,10 @@
 package com.liferay.portalweb.socialofficeprofile.profile.souseditadditionalemailaddressnullprofile;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
+import com.liferay.portalweb.socialoffice.users.user.addsouser.AddSOUserTest;
+import com.liferay.portalweb.socialoffice.users.user.addsouser.TearDownSOUserTest;
+import com.liferay.portalweb.socialoffice.users.user.editsouserpassword.EditSOUserPasswordTest;
+import com.liferay.portalweb.socialoffice.users.user.selectregularrolessouser.SelectRegularRolesSOUserTest;
 import com.liferay.portalweb.socialoffice.users.user.signinso.SOUs_SignInSOTest;
 import com.liferay.portalweb.socialoffice.users.user.signinso.SignInSOTest;
 import com.liferay.portalweb.socialoffice.users.user.signinso.SignOutSOTest;
@@ -30,13 +34,15 @@ public class SOUs_EditAdditionalEmailAddressNullProfileTests
 	public static Test suite() {
 		TestSuite testSuite = new TestSuite();
 		testSuite.addTestSuite(AddSOUserTest.class);
+		testSuite.addTestSuite(SelectRegularRolesSOUserTest.class);
+		testSuite.addTestSuite(EditSOUserPasswordTest.class);
 		testSuite.addTestSuite(SignOutSOTest.class);
 		testSuite.addTestSuite(SOUs_SignInSOTest.class);
 		testSuite.addTestSuite(SOUs_AddAdditionalEmailAddressProfileTest.class);
 		testSuite.addTestSuite(SOUs_EditAdditionalEmailAddressNullProfileTest.class);
 		testSuite.addTestSuite(SignOutSOTest.class);
 		testSuite.addTestSuite(SignInSOTest.class);
-		testSuite.addTestSuite(TearDownUserTest.class);
+		testSuite.addTestSuite(TearDownSOUserTest.class);
 
 		return testSuite;
 	}
