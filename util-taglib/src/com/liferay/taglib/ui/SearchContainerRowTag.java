12bee0bf3305eb6a1c4e0258d49ce23922f3b918
Brian Chan <brian.chan@liferay.com>
2012-05-25 11:56:35 -0700

    LPS-27210 Sort

--- util-taglib/src/com/liferay/taglib/ui/SearchContainerRowTag.java
+++ util-taglib/src/com/liferay/taglib/ui/SearchContainerRowTag.java
@@ -310,7 +310,7 @@ public class SearchContainerRowTag<R>
 			else {
 				rowId = FriendlyURLNormalizerUtil.normalize(
 					String.valueOf(rowIdObj),
-					new char[] {CharPool.SLASH, CharPool.PERIOD});
+					new char[] {CharPool.PERIOD, CharPool.SLASH});
 			}
 		}
 
