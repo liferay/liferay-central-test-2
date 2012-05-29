fee981b989707a2b2cf0635f42954d38b4ae3c29
Jeffrey Yang <jeffrey.yang@liferay.com>
2012-05-29 12:09:05 -0700

    LPS-26880

---
+++ portal-web/test/com/liferay/portalweb/security/json/user/user1adduserjson/User1_AddUserJSONTest.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portalweb.security.json.user.user1adduserjson;
+
+import com.liferay.portalweb.portal.BaseTestCase;
+import com.liferay.portalweb.portal.util.RuntimeVariables;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class User1_AddUserJSONTest extends BaseTestCase {
+	public void testUser1_AddUserJSON() throws Exception {
+		selenium.open("/web/guest/home/");
+		loadRequiredJavaScriptModules();
+		selenium.open("/api/jsonws");
+		loadRequiredJavaScriptModules();
+		selenium.type("//input[@id='serviceSearch']",
+			RuntimeVariables.replace("add-user"));
+
+		for (int second = 0;; second++) {
+			if (second >= 90) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isVisible(
+							"xpath=(//li[@class='lfr-api-signature']/a)[2]")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		assertEquals(RuntimeVariables.replace("add-user"),
+			selenium.getText("xpath=(//li[@class='lfr-api-signature']/a)[2]"));
+		selenium.clickAt("xpath=(//li[@class='lfr-api-signature']/a)[2]",
+			RuntimeVariables.replace("add-user"));
+		selenium.waitForPageToLoad("30000");
+		loadRequiredJavaScriptModules();
+		selenium.type("//input[@name='companyId']",
+			RuntimeVariables.replace("1"));
+		selenium.clickAt("//input[@id='fieldFalse1']",
+			RuntimeVariables.replace("false"));
+		selenium.type("//input[@name='password1']",
+			RuntimeVariables.replace("password"));
+		selenium.type("//input[@name='password2']",
+			RuntimeVariables.replace("password"));
+		selenium.clickAt("//input[@id='fieldFalse4']",
+			RuntimeVariables.replace("false"));
+		selenium.type("//input[@name='screenName']",
+			RuntimeVariables.replace("usersn2"));
+		selenium.type("//input[@name='emailAddress']",
+			RuntimeVariables.replace("userea2@liferay.com"));
+		selenium.type("//input[@name='facebookId']",
+			RuntimeVariables.replace("0"));
+		selenium.type("//input[@name='locale']",
+			RuntimeVariables.replace("en_US"));
+		selenium.type("//input[@name='firstName']",
+			RuntimeVariables.replace("userfn2"));
+		selenium.type("//input[@name='lastName']",
+			RuntimeVariables.replace("userln2"));
+		selenium.type("//input[@name='prefixId']", RuntimeVariables.replace("0"));
+		selenium.type("//input[@name='suffixId']", RuntimeVariables.replace("0"));
+		selenium.type("//input[@name='birthdayMonth']",
+			RuntimeVariables.replace("7"));
+		selenium.type("//input[@name='birthdayDay']",
+			RuntimeVariables.replace("20"));
+		selenium.type("//input[@name='birthdayYear']",
+			RuntimeVariables.replace("1888"));
+		selenium.type("//input[@name='roleIds']", RuntimeVariables.replace("15"));
+		selenium.clickAt("//input[@id='fieldFalse24']",
+			RuntimeVariables.replace("False"));
+		selenium.clickAt("//input[@value='Invoke']",
+			RuntimeVariables.replace("Invoke"));
+		Thread.sleep(5000);
+		assertEquals(RuntimeVariables.replace(
+				"{\n \"exception\": \"com.liferay.portal.security.auth.PrincipalException\"\n}"),
+			selenium.getText("//pre[@id='serviceOutput']"));
+	}
+}
