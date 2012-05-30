9e7c58531e908fdcf9a62c827654934c1a893560
Brian Chan <brian.chan@liferay.com>
2012-05-30 10:40:56 -0700

    LPS-27616 Fix jspc error

--- portal-impl/src/com/liferay/portal/util/PropsValues.java
+++ portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -62,7 +62,7 @@ public class PropsValues {
 
 	public static final int ANNOUNCEMENTS_ENTRY_CHECK_INTERVAL = GetterUtil.getInteger(PropsUtil.get(PropsKeys.ANNOUNCEMENTS_ENTRY_CHECK_INTERVAL));
 
-	public static final String[] ANNOUNCEMENTS_ENTRY_PAGE_DELTA_VALUES = PropsUtil.getArray(PropsKeys.ANNOUNCEMENTS_ENTRY_PAGE_DELTA_VALUES);
+	public static final int[] ANNOUNCEMENTS_ENTRY_PAGE_DELTA_VALUES = GetterUtil.getIntegerValues(PropsUtil.getArray(PropsKeys.ANNOUNCEMENTS_ENTRY_PAGE_DELTA_VALUES));
 
 	public static final String[] APPLICATION_SHUTDOWN_EVENTS = PropsUtil.getArray(PropsKeys.APPLICATION_SHUTDOWN_EVENTS);
 
