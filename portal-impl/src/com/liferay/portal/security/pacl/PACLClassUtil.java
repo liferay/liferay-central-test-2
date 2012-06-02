brianchandotcom/liferay-portal

LPS-26321 Allow Tomcat's JdbcLeakPrevention when redeploying test-pacl-portlet

portal-impl/src/com/liferay/portal/security/pacl/PACLClassUtil.java
@@ -17,8 +17,12 @@ package com.liferay.portal.security.pacl;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.AggregateClassLoader;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.security.lang.PortalSecurityManagerThreadLocal;
 import com.liferay.portal.spring.util.FilterClassLoader;
+
+import java.net.URL;
 
 import sun.reflect.Reflection;
 
@@ -37,6 +41,32 @@ public class PACLClassUtil {
 		}
 		finally {
 			PortalSecurityManagerThreadLocal.setEnabled(enabled);
+		}
+	}
+
+	public static String getClassLocation(Class<?> clazz) {
+		boolean checkGetClassLoader =
+			PortalSecurityManagerThreadLocal.isCheckGetClassLoader();
+
+		try {
+			PortalSecurityManagerThreadLocal.setCheckGetClassLoader(false);
+
+			ClassLoader classLoader = clazz.getClassLoader();
+
+			String className = clazz.getName();
+
+			String name = StringUtil.replace(
+				className, StringPool.PERIOD, StringPool.SLASH);
+
+			name += ".class";
+
+			URL url = classLoader.getResource(name);
+
+			return url.toString();
+		}
+		finally {
+			PortalSecurityManagerThreadLocal.setCheckGetClassLoader(
+				checkGetClassLoader);
 		}
 	}
 

portal-impl/src/com/liferay/portal/security/pacl/checker/RuntimeChecker.java
@@ -17,9 +17,12 @@ package com.liferay.portal.security.pacl.checker;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.BaseAsyncDestination;
+import com.liferay.portal.kernel.util.PathUtil;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.security.lang.PortalSecurityManagerThreadLocal;
 import com.liferay.portal.security.pacl.PACLClassLoaderUtil;
+import com.liferay.portal.security.pacl.PACLClassUtil;
 
 import com.sun.jmx.interceptor.DefaultMBeanServerInterceptor;
 
@@ -201,6 +204,9 @@ public class RuntimeChecker extends BaseReflectChecker {
 		}
 
 		if (callerClass6 == Class.class) {
+			if (isTomcatJdbcLeakPrevention(callerClass7)) {
+				return true;
+			}
 		}
 		else if (callerClass6 == ClassLoader.class) {
 			Thread currentThread = Thread.currentThread();
@@ -334,6 +340,31 @@ public class RuntimeChecker extends BaseReflectChecker {
 		return false;
 	}
 
+	protected boolean isTomcatJdbcLeakPrevention(Class<?> clazz) {
+		String className = clazz.getName();
+
+		if (!className.equals(_JDBC_LEAK_PREVENTION)) {
+			return false;
+		}
+
+		String expectedClassLocation =
+			PathUtil.toUnixPath(
+				System.getProperty("catalina.home") + "/lib/catalina.jar!/");
+
+		expectedClassLocation += StringUtil.replace(
+			className, StringPool.PERIOD, StringPool.SLASH);
+		expectedClassLocation += ".class";
+
+		String actualClassLocation = PACLClassUtil.getClassLocation(clazz);
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Expected class location " + expectedClassLocation);
+			_log.debug("Actual class location " + actualClassLocation);
+		}
+
+		return actualClassLocation.endsWith(expectedClassLocation);
+	}
+
 	protected void logCreateClassLoader(Class<?> callerClass, int frame) {
 		if (_log.isInfoEnabled()) {
 			_log.info(
@@ -379,6 +410,9 @@ public class RuntimeChecker extends BaseReflectChecker {
 
 	private static final String _CLASS_NAME_PROCESS_IMPL =
 		"java.lang.ProcessImpl$";
+
+	private static final String _JDBC_LEAK_PREVENTION =
+		"org.apache.catalina.loader.JdbcLeakPrevention";
 
 	private static final String _METHOD_NAME_GET_SYSTEM_CLASS_LOADER =
 		"getSystemClassLoader";

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portal/security/pacl/PACLClassUtil.java
@@ -17,8 +17,12 @@ package com.liferay.portal.security.pacl;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.AggregateClassLoader;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.security.lang.PortalSecurityManagerThreadLocal;
 import com.liferay.portal.spring.util.FilterClassLoader;
+
+import java.net.URL;
 
 import sun.reflect.Reflection;
 
@@ -34,6 +38,34 @@ public class PACLClassUtil {
 			PortalSecurityManagerThreadLocal.setEnabled(false);
 
 			return _instance._getCallerClassLoader(callerClass);
+		}
+		finally {
+			PortalSecurityManagerThreadLocal.setEnabled(enabled);
+		}
+	}
+
+	public static String getClassLocation(Class<?> clazz) {
+		boolean enabled = PortalSecurityManagerThreadLocal.isEnabled();
+
+		try {
+			PortalSecurityManagerThreadLocal.setEnabled(false);
+
+			ClassLoader classLoader = clazz.getClassLoader();
+
+			if (classLoader == null) {
+				return StringPool.BLANK;
+			}
+
+			String className = clazz.getName();
+
+			String name = StringUtil.replace(
+				className, StringPool.PERIOD, StringPool.SLASH);
+
+			name += ".class";
+
+			URL url = classLoader.getResource(name);
+
+			return url.toString();
 		}
 		finally {
 			PortalSecurityManagerThreadLocal.setEnabled(enabled);

portal-impl/src/com/liferay/portal/security/pacl/checker/RuntimeChecker.java
@@ -17,9 +17,12 @@ package com.liferay.portal.security.pacl.checker;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.BaseAsyncDestination;
+import com.liferay.portal.kernel.util.PathUtil;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.security.lang.PortalSecurityManagerThreadLocal;
 import com.liferay.portal.security.pacl.PACLClassLoaderUtil;
+import com.liferay.portal.security.pacl.PACLClassUtil;
 
 import java.security.Permission;
 
@@ -199,6 +202,9 @@ public class RuntimeChecker extends BaseReflectChecker {
 		}
 
 		if (callerClass6 == Class.class) {
+			if (isTomcatJdbcLeakPrevention(callerClass7)) {
+				return true;
+			}
 		}
 		else if (callerClass6 == ClassLoader.class) {
 			Thread currentThread = Thread.currentThread();
@@ -349,6 +355,31 @@ public class RuntimeChecker extends BaseReflectChecker {
 		return false;
 	}
 
+	protected boolean isTomcatJdbcLeakPrevention(Class<?> clazz) {
+		String className = clazz.getName();
+
+		if (!className.equals(_JDBC_LEAK_PREVENTION)) {
+			return false;
+		}
+
+		String actualClassLocation = PACLClassUtil.getClassLocation(clazz);
+
+		String expectedClassLocation =
+			PathUtil.toUnixPath(
+				System.getProperty("catalina.base") + "/lib/catalina.jar!/");
+
+		expectedClassLocation += StringUtil.replace(
+			className, StringPool.PERIOD, StringPool.SLASH);
+		expectedClassLocation += ".class";
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Actual class location " + actualClassLocation);
+			_log.debug("Expected class location " + expectedClassLocation);
+		}
+
+		return actualClassLocation.endsWith(expectedClassLocation);
+	}
+
 	protected void logCreateClassLoader(Class<?> callerClass, int frame) {
 		if (_log.isInfoEnabled()) {
 			_log.info(
@@ -397,6 +428,9 @@ public class RuntimeChecker extends BaseReflectChecker {
 
 	private static final String _CLASS_NAME_PROCESS_IMPL =
 		"java.lang.ProcessImpl$";
+
+	private static final String _JDBC_LEAK_PREVENTION =
+		"org.apache.catalina.loader.JdbcLeakPrevention";
 
 	private static final String _METHOD_NAME_GET_SYSTEM_CLASS_LOADER =
 		"getSystemClassLoader";