caedd6fa508c1c52f08e01a19967e8e5547ab316
Juan Fernández <juan.fernandez@liferay.com>
2012-06-04 12:50:58 +0200

    LPS-26355 JournalFolder tests

---
+++ portal-impl/test/integration/com/liferay/portlet/journal/JournalFolderTest.java
@@ -0,0 +1,149 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.journal;
+
+import com.liferay.portal.kernel.dao.orm.FinderCacheUtil;
+import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceTestUtil;
+import com.liferay.portal.test.EnvironmentExecutionTestListener;
+import com.liferay.portal.test.ExecutionTestListeners;
+import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
+import com.liferay.portal.test.TransactionalExecutionTestListener;
+import com.liferay.portal.util.TestPropsValues;
+import com.liferay.portlet.journal.model.JournalArticle;
+import com.liferay.portlet.journal.model.JournalFolder;
+import com.liferay.portlet.journal.service.JournalArticleLocalServiceUtil;
+import com.liferay.portlet.journal.service.JournalFolderLocalServiceUtil;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * <a href="JournalFolderTest.java.html"><b><i>View Source</i></b></a>
+ *
+ * @author Juan Fernández
+ */
+@ExecutionTestListeners(
+	listeners = {
+		EnvironmentExecutionTestListener.class,
+		TransactionalExecutionTestListener.class
+	})
+@RunWith(LiferayIntegrationJUnitTestRunner.class)
+public class JournalFolderTest {
+
+	@Before
+	public void setUp() {
+		FinderCacheUtil.clearCache();
+	}
+
+	@Test
+	public void testContent() throws Exception {
+		Group group = ServiceTestUtil.addGroup("Test Group");
+
+		JournalFolder folder = _addFolder(group.getGroupId(), 0, "Test Folder");
+
+		JournalArticle article = _addArticle(
+			group.getGroupId(), folder.getFolderId(), "Test Article",
+			"This is a test article.");
+
+		Assert.assertTrue(article.getFolderId() == folder.getFolderId());
+
+		JournalFolderLocalServiceUtil.deleteFolder(folder);
+		GroupLocalServiceUtil.deleteGroup(group);
+	}
+
+	@Test
+	@Transactional
+	public void testSubfolders() throws Exception {
+
+		Group group = ServiceTestUtil.addGroup("Test Group");
+
+		JournalFolder folder1 = _addFolder(group.getGroupId(), 0, "Test 1");
+		JournalFolder folder11 = _addFolder(
+			group.getGroupId(), folder1.getFolderId(), "Test 1.1");
+		JournalFolder folder111 = _addFolder(
+			group.getGroupId(), folder11.getFolderId(), "Test 1.1.1");
+
+		Assert.assertTrue(folder1.isRoot());
+		Assert.assertFalse(folder11.isRoot());
+		Assert.assertFalse(folder111.isRoot());
+
+		Assert.assertEquals(folder1.getFolderId(),
+			folder11.getParentFolderId());
+
+		Assert.assertEquals(folder11.getFolderId(),
+			folder111.getParentFolderId());
+	}
+
+	protected JournalArticle _addArticle(
+			long groupId, long folderId, String name, String content)
+		throws Exception {
+
+		Locale englishLocale = new Locale("en", "US");
+
+		Map<Locale, String> titleMap = new HashMap<Locale, String>();
+		Map<Locale, String> descriptionMap = new HashMap<Locale, String>();
+
+		titleMap.put(englishLocale, name);
+
+		ServiceContext serviceContext = ServiceTestUtil.getServiceContext();
+
+		StringBundler sb = new StringBundler();
+
+		sb.append("<?xml version=\"1.0\"?><root available-locales=");
+		sb.append("\"en_US\" default-locale=\"en_US\">");
+		sb.append("<static-content language-id=\"en_US\"><![CDATA[<p>");
+		sb.append(content);
+		sb.append("</p>]]>");
+		sb.append("</static-content></root>");
+
+		return JournalArticleLocalServiceUtil.addArticle(
+			TestPropsValues.getUserId(), groupId, folderId, 0, 0, "", true, 1,
+			titleMap, descriptionMap, sb.toString(), "general", null, null,
+			null, 1, 1, 1965, 0, 0, 0, 0, 0, 0, 0, true, 0, 0, 0, 0, 0, true,
+			false, false, null, null, null, null, serviceContext);
+	}
+
+	protected JournalFolder _addFolder(
+			long groupId, long parentFolderId, String name)
+		throws Exception {
+
+		JournalFolder folder = JournalFolderLocalServiceUtil.fetchFolder(
+			groupId, name);
+
+		if (folder != null) {
+			return folder;
+		}
+
+		String description = "This is a test folder";
+
+		ServiceContext serviceContext = ServiceTestUtil.getServiceContext();
+
+		return JournalFolderLocalServiceUtil.addFolder(
+			TestPropsValues.getUserId(), groupId, parentFolderId, name,
+			description, serviceContext);
+	}
+
+}
