06fd838de084bccee8e18bfdce964a1dd6d4f3de
Brian Chan <brian.chan@liferay.com>
2012-06-12 14:36:28 -0700

    LPS-27781 More flexible fix

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -571,8 +571,8 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 				RequiredGroupException.SYSTEM_GROUP);
 		}
 
-		if (groupPersistence.countByChildrenSites(
-				group.getCompanyId(), group.getGroupId()) > 0) {
+		if (groupPersistence.countByC_P_S(
+				group.getCompanyId(), group.getGroupId(), true) > 0) {
 
 			throw new RequiredGroupException(
 				String.valueOf(group.getGroupId()),
