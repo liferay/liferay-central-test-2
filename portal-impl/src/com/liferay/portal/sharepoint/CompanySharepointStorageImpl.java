9eb9163ce127301261324e3231887d7affa87aa6
Alexander Chow <alexander.chow@liferay.com>
2012-06-12 18:00:45 +0100

    LPS-27897 SharepointServlet improperly handling spaces in folders

--- portal-impl/src/com/liferay/portal/sharepoint/CompanySharepointStorageImpl.java
+++ portal-impl/src/com/liferay/portal/sharepoint/CompanySharepointStorageImpl.java
@@ -14,17 +14,10 @@
 
 package com.liferay.portal.sharepoint;
 
-import com.liferay.portal.kernel.dao.orm.QueryUtil;
-import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.webdav.WebDAVUtil;
 import com.liferay.portal.model.Group;
-import com.liferay.portal.model.Organization;
-import com.liferay.portal.model.User;
-import com.liferay.portal.service.GroupLocalServiceUtil;
-import com.liferay.portal.service.OrganizationLocalServiceUtil;
-import com.liferay.portal.service.UserServiceUtil;
 
-import java.util.LinkedHashMap;
 import java.util.List;
 
 /**
@@ -38,64 +31,19 @@ public class CompanySharepointStorageImpl extends BaseSharepointStorageImpl {
 
 		Tree foldersTree = new Tree();
 
-		LinkedHashMap<String, Object> groupParams =
-			new LinkedHashMap<String, Object>();
-
-		groupParams.put("usersGroups", new Long(sharepointRequest.getUserId()));
-
-		List<Group> groups = GroupLocalServiceUtil.search(
-			sharepointRequest.getCompanyId(), null, null, groupParams,
-			QueryUtil.ALL_POS, QueryUtil.ALL_POS);
-
-		Group userGroup = GroupLocalServiceUtil.getUserGroup(
-			sharepointRequest.getCompanyId(), sharepointRequest.getUserId());
-
-		groups.add(userGroup);
-
-		List<Organization> organizations =
-			OrganizationLocalServiceUtil.getUserOrganizations(
-				sharepointRequest.getUserId());
-
-		for (Organization organization : organizations) {
-			groups.add(organization.getGroup());
-		}
+		List<Group> groups = WebDAVUtil.getGroups(sharepointRequest.getUser());
 
 		for (Group group : groups) {
-			String path = getGroupPath(group);
+			String name = group.getFriendlyURL();
 
-			foldersTree.addChild(getFolderTree(path));
+			name = name.substring(1);
+
+			foldersTree.addChild(getFolderTree(name));
 		}
 
 		foldersTree.addChild(getFolderTree(StringPool.BLANK));
 
 		return foldersTree;
-	}
-
-	protected String getGroupPath(Group group) throws Exception {
-		StringBundler sb = new StringBundler(5);
-
-		String name = group.getName();
-
-		if (group.isUser()) {
-			User user = UserServiceUtil.getUserById(group.getClassPK());
-
-			name = user.getFullName();
-		}
-		else if (group.isOrganization()) {
-			Organization organization =
-				OrganizationLocalServiceUtil.getOrganization(
-					group.getOrganizationId());
-
-			name = organization.getName();
-		}
-
-		sb.append(name);
-		sb.append(StringPool.SPACE);
-		sb.append(StringPool.OPEN_BRACKET);
-		sb.append(group.getGroupId());
-		sb.append(StringPool.CLOSE_BRACKET);
-
-		return sb.toString();
 	}
 
 }
