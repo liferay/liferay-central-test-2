60da755c5ad741908c53670b0a6687bf23615a9e
Albert Lee <albert.lee@liferay.com>
2012-06-14 08:01:01 -0700

    LPS-27926

---
+++ portal-web/test/com/liferay/portalweb/portal/controlpanel/organizations/organizationpage/addorganizationprivatepage/AddOrganizationPrivatePageTest.java
@@ -0,0 +1,180 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portalweb.portal.controlpanel.organizations.organizationpage.addorganizationprivatepage;
+
+import com.liferay.portalweb.portal.BaseTestCase;
+import com.liferay.portalweb.portal.util.RuntimeVariables;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class AddOrganizationPrivatePageTest extends BaseTestCase {
+	public void testAddOrganizationPrivatePage() throws Exception {
+		int label = 1;
+
+		while (label >= 1) {
+			switch (label) {
+			case 1:
+				selenium.open("/web/guest/home/");
+				loadRequiredJavaScriptModules();
+				assertEquals(RuntimeVariables.replace("Go to"),
+					selenium.getText("//li[@id='_145_mySites']/a/span"));
+				selenium.mouseOver("//li[@id='_145_mySites']/a/span");
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (selenium.isVisible("link=Control Panel")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				selenium.clickAt("link=Control Panel",
+					RuntimeVariables.replace("Control Panel"));
+				selenium.waitForPageToLoad("30000");
+				loadRequiredJavaScriptModules();
+				selenium.clickAt("link=Sites", RuntimeVariables.replace("Sites"));
+				selenium.waitForPageToLoad("30000");
+				loadRequiredJavaScriptModules();
+				selenium.type("//input[@id='_134_name']",
+					RuntimeVariables.replace("Name"));
+				selenium.clickAt("//input[@value='Search']",
+					RuntimeVariables.replace("Search"));
+				selenium.waitForPageToLoad("30000");
+				loadRequiredJavaScriptModules();
+				assertEquals(RuntimeVariables.replace(
+						"Organization Name\nBelongs to an organization of type Regular Organization."),
+					selenium.getText("//tr[3]/td"));
+				assertEquals(RuntimeVariables.replace("Private"),
+					selenium.getText("//td[2]/a"));
+				assertEquals(RuntimeVariables.replace("1 Organization"),
+					selenium.getText("//td[3]/div"));
+				assertEquals(RuntimeVariables.replace("Yes"),
+					selenium.getText("//tr[3]/td[4]"));
+				assertEquals(RuntimeVariables.replace("Actions"),
+					selenium.getText(
+						"//span[@title='Actions']/ul/li/strong/a/span"));
+				selenium.clickAt("//span[@title='Actions']/ul/li/strong/a/span",
+					RuntimeVariables.replace("Actions"));
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (selenium.isVisible(
+									"//div[@class='lfr-component lfr-menu-list']/ul/li/a[contains(.,'Manage Pages')]")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				assertEquals(RuntimeVariables.replace("Manage Pages"),
+					selenium.getText(
+						"//div[@class='lfr-component lfr-menu-list']/ul/li/a[contains(.,'Manage Pages')]"));
+				selenium.click(RuntimeVariables.replace(
+						"//div[@class='lfr-component lfr-menu-list']/ul/li/a[contains(.,'Manage Pages')]"));
+				selenium.waitForPageToLoad("30000");
+				loadRequiredJavaScriptModules();
+				selenium.clickAt("link=Private Pages",
+					RuntimeVariables.replace("Private Pages"));
+				selenium.waitForPageToLoad("30000");
+				loadRequiredJavaScriptModules();
+				assertEquals(RuntimeVariables.replace("Add Page"),
+					selenium.getText(
+						"//button[@type='button']/span[contains(.,'Add Page')]"));
+				selenium.clickAt("//button[@type='button']/span[contains(.,'Add Page')]",
+					RuntimeVariables.replace("Add Page"));
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (selenium.isVisible("//input[@id='_156_name_en_US']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				selenium.type("//input[@id='_156_name_en_US']",
+					RuntimeVariables.replace("Private Page"));
+				selenium.clickAt("//input[@value='Add Page']",
+					RuntimeVariables.replace("Add Page"));
+				selenium.waitForPageToLoad("30000");
+				loadRequiredJavaScriptModules();
+				assertEquals(RuntimeVariables.replace(
+						"Your request completed successfully."),
+					selenium.getText("//div[@class='portlet-msg-success']"));
+
+				boolean pagePresent = selenium.isVisible(
+						"//li/ul/li/div/div[3]/a");
+
+				if (pagePresent) {
+					label = 2;
+
+					continue;
+				}
+
+				selenium.clickAt("//div[@class='aui-tree-hitarea']",
+					RuntimeVariables.replace("Drop Down Arrow"));
+
+			case 2:
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (RuntimeVariables.replace("Private Page")
+												.equals(selenium.getText(
+										"//li/ul/li/div/div[3]/a"))) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				assertEquals(RuntimeVariables.replace("Private Page"),
+					selenium.getText("//li/ul/li/div/div[3]/a"));
+
+			case 100:
+				label = -1;
+			}
+		}
+	}
+}
---
+++ portal-web/test/com/liferay/portalweb/portal/controlpanel/organizations/organizationpage/addorganizationprivatepage/ViewOrganizationPrivatePageTest.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portalweb.portal.controlpanel.organizations.organizationpage.addorganizationprivatepage;
+
+import com.liferay.portalweb.portal.BaseTestCase;
+import com.liferay.portalweb.portal.util.RuntimeVariables;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class ViewOrganizationPrivatePageTest extends BaseTestCase {
+	public void testViewOrganizationPrivatePage() throws Exception {
+		selenium.open("/group/organization-name/");
+		loadRequiredJavaScriptModules();
+		assertEquals(RuntimeVariables.replace("Organization Name"),
+			selenium.getText("//h1[@class='site-title']/span"));
+		assertEquals(RuntimeVariables.replace("Private Page"),
+			selenium.getText(
+				"//nav[@id='navigation']/ul/li/a/span[contains(.,'Private Page')]"));
+		assertEquals(RuntimeVariables.replace("Liferay"),
+			selenium.getText(
+				"//ul[@class='breadcrumbs breadcrumbs-horizontal lfr-component']/li/span/a[contains(.,'Liferay')]"));
+		assertEquals(RuntimeVariables.replace("Organization Name"),
+			selenium.getText(
+				"//ul[@class='breadcrumbs breadcrumbs-horizontal lfr-component']/li/span/a[contains(.,'Organization Name')]"));
+		assertEquals(RuntimeVariables.replace("Private Page"),
+			selenium.getText(
+				"//ul[@class='breadcrumbs breadcrumbs-horizontal lfr-component']/li/span/a[contains(.,'Private Page')]"));
+	}
+}
