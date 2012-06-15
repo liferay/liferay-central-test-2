93f418cc281678bff10b7a8a61b9c8e03a4caf7b
Miguel Pastor <miguel.pastor@liferay.com>
2012-06-15 08:23:22 +0200

    LPS-26827 Source formatting

--- portal-impl/test/integration/com/liferay/portal/test/AssertUtils.java
+++ portal-impl/test/integration/com/liferay/portal/test/AssertUtils.java
@@ -31,6 +31,12 @@ import org.junit.Assert;
  */
 public class AssertUtils {
 
+	public static void assertArrayEquals(
+		double[] expectedArray, double[] actualArray) {
+
+		Assert.assertArrayEquals(expectedArray, actualArray, 0);
+	}
+
 	public static void assertEquals(Blob expectedBlob, Blob actualBlob)
 		throws Exception {
 
