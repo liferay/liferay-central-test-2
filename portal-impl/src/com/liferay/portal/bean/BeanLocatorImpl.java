dca7f14b5a05102c337deeb01e2a8a460c06ac7d
Michael Han <michael.han@liferay.com>
2012-06-17 18:39:51 -0500

    LPS-27974 - need to retrieve interfaces of parent classes

--- portal-impl/src/com/liferay/portal/bean/BeanLocatorImpl.java
+++ portal-impl/src/com/liferay/portal/bean/BeanLocatorImpl.java
@@ -185,6 +185,20 @@ public class BeanLocatorImpl implements BeanLocator {
 
 		Class<?> clazz = object.getClass();
 
+		getInterfaces(interfaceClasses, clazz);
+
+		Class superClazz = clazz.getSuperclass();
+
+		if (superClazz != null) {
+			getInterfaces(interfaceClasses, superClazz);
+		}
+
+		return interfaceClasses.toArray(new Class<?>[interfaceClasses.size()]);
+	}
+
+	protected void getInterfaces(
+		List<Class<?>> interfaceClasses, Class<?> clazz) {
+
 		for (Class<?> interfaceClass : clazz.getInterfaces()) {
 			try {
 				interfaceClasses.add(
@@ -193,8 +207,6 @@ public class BeanLocatorImpl implements BeanLocator {
 			catch (ClassNotFoundException cnfe) {
 			}
 		}
-
-		return interfaceClasses.toArray(new Class<?>[interfaceClasses.size()]);
 	}
 
 	private static Log _log = LogFactoryUtil.getLog(BeanLocatorImpl.class);
