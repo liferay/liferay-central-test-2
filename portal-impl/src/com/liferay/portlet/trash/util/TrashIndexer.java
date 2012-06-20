929e3315a6a4533a92f198e18ef1a9bf305545ad
Julio Camarero <julio.camarero@liferay.com>
2012-06-20 13:30:07 +0200

    LPS-27029 Missing file

---
+++ portal-impl/src/com/liferay/portlet/trash/util/TrashIndexer.java
@@ -0,0 +1,204 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.trash.util;
+
+import com.liferay.portal.kernel.search.BaseIndexer;
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.BooleanQuery;
+import com.liferay.portal.kernel.search.BooleanQueryFactoryUtil;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.SearchException;
+import com.liferay.portal.kernel.search.Summary;
+import com.liferay.portal.kernel.trash.TrashHandler;
+import com.liferay.portal.kernel.trash.TrashHandlerRegistryUtil;
+import com.liferay.portal.kernel.trash.TrashRenderer;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portlet.trash.model.TrashEntry;
+
+import java.util.LinkedHashMap;
+import java.util.Locale;
+
+import javax.portlet.PortletURL;
+
+/**
+ * @author Julio Camarero
+ */
+public class TrashIndexer extends BaseIndexer {
+
+	public static final String[] CLASS_NAMES = {TrashEntry.class.getName()};
+
+	public static final String PORTLET_ID = PortletKeys.TRASH;
+
+	public TrashIndexer() {
+		setFilterSearch(true);
+		setPermissionAware(true);
+	}
+
+	public String[] getClassNames() {
+		return CLASS_NAMES;
+	}
+
+	@Override
+	public BooleanQuery getFullQuery(SearchContext searchContext)
+		throws SearchException {
+
+		try {
+			BooleanQuery contextQuery = BooleanQueryFactoryUtil.create(
+				searchContext);
+
+			contextQuery.addTerm(
+				Field.COMPANY_ID, String.valueOf(searchContext.getCompanyId()),
+				false, BooleanClauseOccur.MUST);
+
+			BooleanQuery groupQuery = BooleanQueryFactoryUtil.create(
+				searchContext);
+
+			for (long groupId : searchContext.getGroupIds()) {
+				groupQuery.addTerm(
+					Field.GROUP_ID, String.valueOf(groupId), false,
+					BooleanClauseOccur.SHOULD);
+			}
+
+			contextQuery.add(groupQuery, BooleanClauseOccur.MUST);
+
+			BooleanQuery fullQuery = createFullQuery(
+				contextQuery, searchContext);
+
+			return fullQuery;
+		}
+		catch (SearchException se) {
+			throw se;
+		}
+		catch (Exception e) {
+			throw new SearchException(e);
+		}
+	}
+
+	public String getPortletId() {
+		return PORTLET_ID;
+	}
+
+	@Override
+	public boolean hasPermission(
+			PermissionChecker permissionChecker, String entryClassName,
+			long entryClassPK, String actionId)
+		throws Exception {
+
+		if (actionId.equals(ActionKeys.VIEW)) {
+			TrashHandler trashHandler =
+				TrashHandlerRegistryUtil.getTrashHandler(entryClassName);
+
+			TrashRenderer trashRenderer = trashHandler.getTrashRenderer(
+				entryClassPK);
+
+			return trashRenderer.hasViewPermission(permissionChecker);
+		}
+
+		return false;
+	}
+
+	@Override
+	public void postProcessContextQuery(
+			BooleanQuery contextQuery, SearchContext searchContext)
+		throws Exception {
+
+		searchContext.setEntryClassNames(null);
+
+		contextQuery.addRequiredTerm(
+			Field.STATUS, WorkflowConstants.STATUS_IN_TRASH);
+
+		LinkedHashMap<String, Object> params =
+			(LinkedHashMap<String, Object>)searchContext.getAttribute("params");
+
+		if (params != null) {
+			String expandoAttributes = (String)params.get("expandoAttributes");
+
+			if (Validator.isNotNull(expandoAttributes)) {
+				addSearchExpando(
+					contextQuery, searchContext, expandoAttributes);
+			}
+		}
+	}
+
+	@Override
+	public void postProcessSearchQuery(
+			BooleanQuery searchQuery, SearchContext searchContext)
+		throws Exception {
+
+		if (searchContext.getAttributes() == null) {
+			return;
+		}
+
+		addSearchTerm(searchQuery, searchContext, Field.CONTENT, true);
+		addSearchTerm(searchQuery, searchContext, Field.TITLE, true);
+		addSearchTerm(searchQuery, searchContext, Field.REMOVED_BY, true);
+		addSearchTerm(searchQuery, searchContext, Field.USER_NAME, true);
+		addSearchTerm(searchQuery, searchContext, Field.TYPE, false);
+	}
+
+	@Override
+	protected void doDelete(Object obj) {
+	}
+
+	@Override
+	protected Document doGetDocument(Object obj) {
+		return null;
+	}
+
+	@Override
+	protected String doGetSortField(String orderByCol) {
+		if (orderByCol.equals("removed-date")) {
+			return Field.REMOVED_DATE;
+		}
+		else if (orderByCol.equals("removed-by")) {
+			return Field.REMOVED_BY;
+		}
+		else {
+			return orderByCol;
+		}
+	}
+
+	@Override
+	protected Summary doGetSummary(
+		Document document, Locale locale, String snippet,
+		PortletURL portletURL) {
+
+		return null;
+	}
+
+	@Override
+	protected void doReindex(Object obj) {
+	}
+
+	@Override
+	protected void doReindex(String className, long classPK) {
+	}
+
+	@Override
+	protected void doReindex(String[] ids) {
+	}
+
+	@Override
+	protected String getPortletId(SearchContext searchContext) {
+		return PORTLET_ID;
+	}
+
+}
