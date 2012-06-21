29fc1d45273496d26bdf2d7cc39c4b965543b00a
Brian Chan <brian.chan@liferay.com>
2012-06-21 11:43:46 -0700

    LPS-28092 Required for LPS-28034

--- portal-service/src/com/liferay/portal/kernel/webdav/WebDAVUtil.java
+++ portal-service/src/com/liferay/portal/kernel/webdav/WebDAVUtil.java
@@ -27,6 +27,7 @@ import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Time;
+import com.liferay.portal.kernel.util.UniqueList;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.xml.Namespace;
 import com.liferay.portal.kernel.xml.SAXReaderUtil;
@@ -36,7 +37,6 @@ import com.liferay.portal.model.User;
 import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.util.comparator.GroupFriendlyURLComparator;
-import com.liferay.util.UniqueList;
 
 import java.util.ArrayList;
 import java.util.Collection;
