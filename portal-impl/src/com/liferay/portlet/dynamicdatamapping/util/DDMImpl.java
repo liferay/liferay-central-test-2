022902e689b2e60f017fe40820f11b3fdd132af6
KocsisDaniel <daniel.kocsis@liferay.com>
2012-06-25 11:28:36 +0200

    LPS-28030 Source formatting

--- portal-impl/src/com/liferay/portlet/dynamicdatamapping/util/DDMImpl.java
+++ portal-impl/src/com/liferay/portlet/dynamicdatamapping/util/DDMImpl.java
@@ -136,7 +136,7 @@ public class DDMImpl implements DDM {
 				fieldValue = String.valueOf(fieldValueDate.getTime());
 			}
 
-			if (fieldValue == null ||
+			if ((fieldValue == null) ||
 				fieldDataType.equals(FieldConstants.FILE_UPLOAD)) {
 
 				continue;
