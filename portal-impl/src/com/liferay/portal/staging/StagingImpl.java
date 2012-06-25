a7d084ceaad16cdf378908f868420847585222a6
Mate Thurzo <mate.thurzo@liferay.com>
2012-06-25 14:11:04 +0200

    LPS-28123 Source formatting

--- portal-impl/src/com/liferay/portal/staging/StagingImpl.java
+++ portal-impl/src/com/liferay/portal/staging/StagingImpl.java
@@ -922,11 +922,11 @@ public class StagingImpl implements Staging {
 			Map<String, String[]> parameterMap, Date startDate, Date endDate)
 		throws Exception {
 
+		lockGroup(userId, targetGroupId);
+
 		parameterMap.put(
 			PortletDataHandlerKeys.PERFORM_DIRECT_BINARY_IMPORT,
 			new String[] {Boolean.TRUE.toString()});
-
-		lockGroup(userId, targetGroupId);
 
 		File file = LayoutLocalServiceUtil.exportLayoutsAsFile(
 			sourceGroupId, privateLayout, layoutIds, parameterMap, startDate,
@@ -1631,12 +1631,11 @@ public class StagingImpl implements Staging {
 
 			throw new DuplicateLockException(lock);
 		}
-		else {
-			LockLocalServiceUtil.lock(
-				userId, Staging.class.getName(), String.valueOf(targetGroupId),
-				StagingImpl.class.getName(), false,
-				StagingConstants.LOCK_EXPIRATION_TIME);
-		}
+
+		LockLocalServiceUtil.lock(
+			userId, Staging.class.getName(), String.valueOf(targetGroupId),
+			StagingImpl.class.getName(), false,
+			StagingConstants.LOCK_EXPIRATION_TIME);
 	}
 
 	protected void publishLayouts(
