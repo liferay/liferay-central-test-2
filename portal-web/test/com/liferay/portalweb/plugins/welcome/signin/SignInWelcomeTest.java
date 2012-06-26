ad02757eb3dc5b532d9a2eb2f8cb4ed3cb72c382
Brian Chiu <brian.chiu@liferay.com>
2012-06-26 16:44:06 -0700

    LPS-28185

---
+++ portal-web/test/com/liferay/portalweb/plugins/welcome/signin/SignInWelcomeTest.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portalweb.plugins.welcome.signin;
+
+import com.liferay.portalweb.portal.BaseTestCase;
+import com.liferay.portalweb.portal.util.RuntimeVariables;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class SignInWelcomeTest extends BaseTestCase {
+	public void testSignInWelcome() throws Exception {
+		selenium.open("/web/guest/what-we-do/");
+		loadRequiredJavaScriptModules();
+		selenium.clickAt("link=Sign In", RuntimeVariables.replace("Sign In"));
+		selenium.waitForPageToLoad("30000");
+		loadRequiredJavaScriptModules();
+		selenium.type("//input[@id='_58_login']",
+			RuntimeVariables.replace("test@liferay.com"));
+		selenium.type("//input[@id='_58_password']",
+			RuntimeVariables.replace("test"));
+		assertFalse(selenium.isChecked("//input[@id='_58_rememberMeCheckbox']"));
+		selenium.clickAt("//input[@id='_58_rememberMeCheckbox']",
+			RuntimeVariables.replace("Remember Me"));
+		assertTrue(selenium.isChecked("//input[@id='_58_rememberMeCheckbox']"));
+		selenium.clickAt("//input[@value='Sign In']",
+			RuntimeVariables.replace("Sign In"));
+		selenium.waitForPageToLoad("30000");
+		loadRequiredJavaScriptModules();
+		selenium.clickAt("//input[@value='I Agree']",
+			RuntimeVariables.replace("I Agree"));
+		selenium.waitForPageToLoad("30000");
+		loadRequiredJavaScriptModules();
+		assertEquals(RuntimeVariables.replace("Please choose a reminder query."),
+			selenium.getText("//div[@class='portlet-msg-info']"));
+		selenium.type("//input[@id='reminderQueryAnswer']",
+			RuntimeVariables.replace("test"));
+		selenium.clickAt("//input[@value='Save']",
+			RuntimeVariables.replace("Save"));
+		selenium.waitForPageToLoad("30000");
+		loadRequiredJavaScriptModules();
+		assertEquals(RuntimeVariables.replace("Joe Bloggs"),
+			selenium.getText("//a[contains(@class,'user-fullname')]"));
+	}
+}
---
+++ portal-web/test/com/liferay/portalweb/plugins/welcome/smoke/TearDownPageWelcomeTest.java
@@ -0,0 +1,407 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portalweb.plugins.welcome.smoke;
+
+import com.liferay.portalweb.portal.BaseTestCase;
+import com.liferay.portalweb.portal.util.RuntimeVariables;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class TearDownPageWelcomeTest extends BaseTestCase {
+	public void testTearDownPageWelcome() throws Exception {
+		int label = 1;
+
+		while (label >= 1) {
+			switch (label) {
+			case 1:
+				selenium.open("/web/guest/what-we-do/");
+				loadRequiredJavaScriptModules();
+				selenium.clickAt("//nav[@id='navigation']",
+					RuntimeVariables.replace("Navigation"));
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (selenium.isElementPresent(
+									"//script[contains(@src,'/aui/sortable/sortable-min.js')]")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				boolean page1Present = selenium.isElementPresent(
+						"xPath=(//li[contains(@class,'lfr-nav-deletable')])/a/span");
+
+				if (!page1Present) {
+					label = 2;
+
+					continue;
+				}
+
+				selenium.mouseOver(
+					"xPath=(//li[contains(@class,'lfr-nav-deletable')])/a/span");
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (selenium.isVisible(
+									"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				assertEquals(RuntimeVariables.replace("X"),
+					selenium.getText(
+						"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']"));
+				selenium.click(
+					"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']");
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if ("Are you sure you want to delete this page?".equals(
+									selenium.getConfirmation())) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (!selenium.isElementPresent(
+									"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				boolean page2Present = selenium.isElementPresent(
+						"xPath=(//li[contains(@class,'lfr-nav-deletable')])/a/span");
+
+				if (!page2Present) {
+					label = 3;
+
+					continue;
+				}
+
+				selenium.mouseOver(
+					"xPath=(//li[contains(@class,'lfr-nav-deletable')])/a/span");
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (selenium.isVisible(
+									"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				assertEquals(RuntimeVariables.replace("X"),
+					selenium.getText(
+						"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']"));
+				selenium.click(
+					"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']");
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if ("Are you sure you want to delete this page?".equals(
+									selenium.getConfirmation())) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (!selenium.isElementPresent(
+									"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				boolean page3Present = selenium.isElementPresent(
+						"xPath=(//li[contains(@class,'lfr-nav-deletable')])/a/span");
+
+				if (!page3Present) {
+					label = 4;
+
+					continue;
+				}
+
+				selenium.mouseOver(
+					"xPath=(//li[contains(@class,'lfr-nav-deletable')])/a/span");
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (selenium.isVisible(
+									"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				assertEquals(RuntimeVariables.replace("X"),
+					selenium.getText(
+						"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']"));
+				selenium.click(
+					"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']");
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if ("Are you sure you want to delete this page?".equals(
+									selenium.getConfirmation())) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (!selenium.isElementPresent(
+									"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				boolean page4Present = selenium.isElementPresent(
+						"xPath=(//li[contains(@class,'lfr-nav-deletable')])/a/span");
+
+				if (!page4Present) {
+					label = 5;
+
+					continue;
+				}
+
+				selenium.mouseOver(
+					"xPath=(//li[contains(@class,'lfr-nav-deletable')])/a/span");
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (selenium.isVisible(
+									"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				assertEquals(RuntimeVariables.replace("X"),
+					selenium.getText(
+						"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']"));
+				selenium.click(
+					"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']");
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if ("Are you sure you want to delete this page?".equals(
+									selenium.getConfirmation())) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (!selenium.isElementPresent(
+									"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				boolean page5Present = selenium.isElementPresent(
+						"xPath=(//li[contains(@class,'lfr-nav-deletable')])/a/span");
+
+				if (!page5Present) {
+					label = 6;
+
+					continue;
+				}
+
+				selenium.mouseOver(
+					"xPath=(//li[contains(@class,'lfr-nav-deletable')])/a/span");
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (selenium.isVisible(
+									"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				assertEquals(RuntimeVariables.replace("X"),
+					selenium.getText(
+						"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']"));
+				selenium.click(
+					"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']");
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if ("Are you sure you want to delete this page?".equals(
+									selenium.getConfirmation())) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+				for (int second = 0;; second++) {
+					if (second >= 90) {
+						fail("timeout");
+					}
+
+					try {
+						if (!selenium.isElementPresent(
+									"xPath=(//li[contains(@class,'lfr-nav-deletable')])/span[@class='delete-tab']")) {
+							break;
+						}
+					}
+					catch (Exception e) {
+					}
+
+					Thread.sleep(1000);
+				}
+
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+			case 100:
+				label = -1;
+			}
+		}
+	}
+}
