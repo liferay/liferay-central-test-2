822e16b1427123df130c9511e1ccd42f201a1bee
Raymond Auge <raymond.auge@liferay.com>
2012-07-04 18:36:06 -0400

    LPS-28371 PACL - Failure during plugin startup if it's not exploded (if it's still a zip file)

--- portal-impl/src/com/liferay/portal/security/pacl/checker/FileChecker.java
+++ portal-impl/src/com/liferay/portal/security/pacl/checker/FileChecker.java
@@ -60,7 +60,12 @@ import sun.reflect.Reflection;
 public class FileChecker extends BaseChecker {
 
 	public void afterPropertiesSet() {
-		_rootDir = WebDirDetector.getRootDir(getClassLoader());
+		try {
+			_rootDir = WebDirDetector.getRootDir(getClassLoader());
+		}
+		catch (Exception e) {
+			// this means the WAR is probably not exploded
+		}
 
 		if (_log.isDebugEnabled()) {
 			_log.debug("Root directory " + _rootDir);
@@ -334,7 +339,9 @@ public class FileChecker extends BaseChecker {
 
 		// Plugin
 
-		paths.add(_rootDir + "-");
+		if (_rootDir != null) {
+			paths.add(_rootDir + "-");
+		}
 
 		// Portal
 
