20e852a2d546eaa71d214fe0f844bf9e54089b8d
Albert Lee <albert.lee@liferay.com>
2012-07-05 12:54:35 -0700

    Test Fixes - portlet/assetpublisher/igimage

--- portal-web/test/com/liferay/portalweb/portlet/assetpublisher/igimage/viewconfigureportletavailableigimageap/ViewConfigurePortletAvailableIGImageAPTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/assetpublisher/igimage/viewconfigureportletavailableigimageap/ViewConfigurePortletAvailableIGImageAPTests.java
@@ -18,6 +18,7 @@ import com.liferay.portalweb.portal.BaseTestSuite;
 import com.liferay.portalweb.portal.util.TearDownPageTest;
 import com.liferay.portalweb.portlet.assetpublisher.portlet.addportletap.AddPageAPTest;
 import com.liferay.portalweb.portlet.assetpublisher.portlet.addportletap.AddPortletAPTest;
+import com.liferay.portalweb.portlet.documentsandmedia.dmdocument.adddmdocument.TearDownDMDocumentTest;
 import com.liferay.portalweb.portlet.documentsandmedia.portlet.addportletdm.AddPageDMTest;
 import com.liferay.portalweb.portlet.documentsandmedia.portlet.addportletdm.AddPortletDMTest;
 
@@ -38,7 +39,7 @@ public class ViewConfigurePortletAvailableIGImageAPTests extends BaseTestSuite {
 		testSuite.addTestSuite(AddNewIGFolderImageAPActionsTest.class);
 		testSuite.addTestSuite(ConfigurePortletAvailableIGImageTest.class);
 		testSuite.addTestSuite(ViewConfigurePortletAvailableIGImageAPTest.class);
-		testSuite.addTestSuite(TearDownDLDocumentTest.class);
+		testSuite.addTestSuite(TearDownDMDocumentTest.class);
 		testSuite.addTestSuite(TearDownPageTest.class);
 
 		return testSuite;
--- portal-web/test/com/liferay/portalweb/portlet/assetpublisher/igimage/viewconfigureportletcurrentigimageap/ViewConfigurePortletCurrentIGImageAPTests.java
+++ portal-web/test/com/liferay/portalweb/portlet/assetpublisher/igimage/viewconfigureportletcurrentigimageap/ViewConfigurePortletCurrentIGImageAPTests.java
@@ -18,6 +18,7 @@ import com.liferay.portalweb.portal.BaseTestSuite;
 import com.liferay.portalweb.portal.util.TearDownPageTest;
 import com.liferay.portalweb.portlet.assetpublisher.portlet.addportletap.AddPageAPTest;
 import com.liferay.portalweb.portlet.assetpublisher.portlet.addportletap.AddPortletAPTest;
+import com.liferay.portalweb.portlet.documentsandmedia.dmdocument.adddmdocument.TearDownDMDocumentTest;
 import com.liferay.portalweb.portlet.documentsandmedia.portlet.addportletdm.AddPageDMTest;
 import com.liferay.portalweb.portlet.documentsandmedia.portlet.addportletdm.AddPortletDMTest;
 
@@ -40,7 +41,7 @@ public class ViewConfigurePortletCurrentIGImageAPTests extends BaseTestSuite {
 		testSuite.addTestSuite(ViewConfigurePortletAvailableIGImageAPTest.class);
 		testSuite.addTestSuite(ConfigurePortletCurrentIGImageTest.class);
 		testSuite.addTestSuite(ViewConfigurePortletCurrentIGImageAPTest.class);
-		testSuite.addTestSuite(TearDownDLDocumentTest.class);
+		testSuite.addTestSuite(TearDownDMDocumentTest.class);
 		testSuite.addTestSuite(TearDownPageTest.class);
 
 		return testSuite;
