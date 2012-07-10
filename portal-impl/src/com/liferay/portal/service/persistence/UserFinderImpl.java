9781c95df750c77ef2bc72e061af37f99cbb96ba
Brian Chan <brian.chan@liferay.com>
2012-07-10 12:59:16 -0700

    LPS-28336 Source formatting

--- portal-impl/src/com/liferay/portal/service/persistence/UserFinderImpl.java
+++ portal-impl/src/com/liferay/portal/service/persistence/UserFinderImpl.java
@@ -865,9 +865,7 @@ public class UserFinderImpl
 				join = sb.toString();
 			}
 			else {
-				// EMPTY lists should appear in the query
-
-				join = "WHERE ( (Organization_.treePath LIKE ?) )";
+				join = "WHERE (Organization_.treePath LIKE ?)";
 			}
 		}
 		else if (key.equals("usersPasswordPolicies")) {
@@ -988,9 +986,6 @@ public class UserFinderImpl
 					}
 				}
 				else {
-
-					// EMPTY list should appear in the query
-
 					qPos.add("%/ /%");
 				}
 			}
