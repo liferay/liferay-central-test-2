9eec4af95e64d3885af240395051e2e9270b3abb
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2012-07-10 15:41:52 -0300

    LPS-27752 Source Formatting

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -3321,7 +3321,6 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 			user.setPasswordReset(true);
 
 			userPersistence.update(user, false);
-
 		}
 		else {
 			if (!PwdEncryptor.PASSWORDS_ENCRYPTION_ALGORITHM.equals(
