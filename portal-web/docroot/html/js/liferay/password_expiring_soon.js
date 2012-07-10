1141931e54aa3f2c5f3c10410d55a215a3e1610e
Iliyan Peychev <iliyan.peychev@liferay.com>
2012-07-10 13:40:28 +0200

    LPS-26005 - Source formatting

--- portal-web/docroot/html/js/liferay/password_expiring_soon.js
+++ portal-web/docroot/html/js/liferay/password_expiring_soon.js
@@ -1,45 +1,24 @@
-/*
- * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
-
 AUI.add(
 	'liferay-password-expiring-soon',
 	function(A) {
 		var PasswordExpiringSoon = A.Component.create(
 			{
 				EXTENDS: A.Base,
+
 				NAME: 'passwordexpiringsoon',
+
 				prototype: {
 					initializer: function(config) {
 						var instance = this;
 
-						var banner = instance._banner;
-
-						instance._expiringSoonText = Liferay.Language.get('warning-your-password-will-expire-soon');
-						instance._closeText = Liferay.Language.get('close');
-
-						banner = new Liferay.Notice(
+						new Liferay.Notice(
 							{
-								closeText: instance._closeText,
-								content: instance._expiringSoonText,
+								closeText: Liferay.Language.get('close'),
+								content: Liferay.Language.get('warning-your-password-will-expire-soon'),
 								noticeClass: 'aui-helper-hidden',
 								toggleText: false
 							}
-						);
-
-						instance._banner = banner;
-
-						banner.show();
+						).show();
 					}
 				}
 			}
