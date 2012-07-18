253fd504dc849f14730ba8eabc5f442152358009
Minhchau Dang <minhchau.dang@liferay.com>
2012-07-17 18:03:35 -0700

    LPS-28661 Add missing index during upgrade schema

--- sql/update-5.2.2-5.2.3.sql
+++ sql/update-5.2.2-5.2.3.sql
@@ -81,6 +81,8 @@ create table ResourcePermission (
 	actionIds LONG
 );
 
+create unique index IX_8D83D0CE on ResourcePermission (companyId, name, scope, primKey, roleId);
+
 create table Shard (
 	shardId LONG not null primary key,
 	classNameId LONG,
