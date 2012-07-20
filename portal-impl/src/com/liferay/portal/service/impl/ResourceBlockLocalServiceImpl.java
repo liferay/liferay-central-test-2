e9972a3e3ea5f7f452cd716e37121d0bd20d5a81
Brian Chan <brian.chan@liferay.com>
2012-07-20 14:57:02 -0700

    Javadocs

--- portal-impl/src/com/liferay/portal/service/impl/ResourceBlockLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/ResourceBlockLocalServiceImpl.java
@@ -142,6 +142,10 @@ public class ResourceBlockLocalServiceImpl
 	 *
 	 * @param  companyId the primary key of the resource block's company
 	 * @param  groupId the primary key of the resource block's group
+	 * @param  name the resource block's name
+	 * @param  permissionsHash the resource block's permission hash
+	 * @param  resourceBlockPermissionsContainer the resource block's
+	 *         permissions container
 	 * @return the new resource block
 	 * @throws SystemException if a system exception occurred
 	 */
@@ -378,6 +382,8 @@ public class ResourceBlockLocalServiceImpl
 	 * zero.
 	 *
 	 * @param  resourceBlockId the primary key of the resource block
+	 * @throws PortalException if a resource block with the primary key could
+	 *         not be found
 	 * @throws SystemException if a system exception occurred
 	 */
 	public void releaseResourceBlock(long resourceBlockId)
@@ -528,6 +534,8 @@ public class ResourceBlockLocalServiceImpl
 	 * the database.
 	 *
 	 * @param  resourceBlockId the primary key of the resource block
+	 * @throws PortalException if a resource block with the primary key could
+	 *         not be found
 	 * @throws SystemException if a system exception occurred
 	 */
 	public void retainResourceBlock(long resourceBlockId)
