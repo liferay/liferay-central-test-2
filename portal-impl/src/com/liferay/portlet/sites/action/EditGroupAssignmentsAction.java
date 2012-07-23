aa8ee3691bb58f10332a0c87fe5dddf25b2e542b
Mate Thurzo <mate.thurzo@liferay.com>
2012-07-23 22:07:13 +0200

    LPS-28703 Applying refactored method name

--- portal-impl/src/com/liferay/portlet/sites/action/EditGroupAssignmentsAction.java
+++ portal-impl/src/com/liferay/portlet/sites/action/EditGroupAssignmentsAction.java
@@ -175,7 +175,7 @@ public class EditGroupAssignmentsAction extends PortletAction {
 
 		UserServiceUtil.addGroupUsers(groupId, addUserIds, serviceContext);
 		UserServiceUtil.unsetGroupUsers(groupId, removeUserIds, serviceContext);
-		UserServiceUtil.unsetUserTeams(groupId, removeUserIds);
+		UserServiceUtil.unsetGroupTeamsUsers(groupId, removeUserIds);
 
 		LiveUsers.joinGroup(themeDisplay.getCompanyId(), groupId, addUserIds);
 		LiveUsers.leaveGroup(
