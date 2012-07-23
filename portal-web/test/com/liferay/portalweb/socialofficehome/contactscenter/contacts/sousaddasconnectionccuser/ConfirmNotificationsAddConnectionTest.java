c04f3d3496c77192ce324801d68bfb79515031f3
Ken Duenwald <ken.duenwald@liferay.com>
2012-07-23 11:52:30 -0700

    Test Fixes - socialofficehome

--- portal-web/test/com/liferay/portalweb/socialofficehome/contactscenter/contacts/sousaddasconnectionccuser/ConfirmNotificationsAddConnectionTest.java
+++ portal-web/test/com/liferay/portalweb/socialofficehome/contactscenter/contacts/sousaddasconnectionccuser/ConfirmNotificationsAddConnectionTest.java
@@ -48,7 +48,7 @@ public class ConfirmNotificationsAddConnectionTest extends BaseTestCase {
 		}
 
 		assertEquals(RuntimeVariables.replace(
-				"Social01 would like to add you as a connection."),
+				"Social01 Office01 User01 would like to add you as a connection."),
 			selenium.getText("//div[@class='notification-entry']/div/span"));
 		assertEquals(RuntimeVariables.replace("Confirm"),
 			selenium.getText("//div[@class='request']/span/a"));
