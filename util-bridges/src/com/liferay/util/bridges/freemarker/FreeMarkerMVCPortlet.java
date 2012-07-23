f614203a3f7255a48de965f4714cd5cdf0edb46d
Raymond Auge <raymond.auge@liferay.com>
2012-07-23 15:41:51 -0400

    LPS-28798 FreeMarkerMVCPortlet

---
+++ util-bridges/src/com/liferay/util/bridges/freemarker/FreeMarkerMVCPortlet.java
@@ -0,0 +1,152 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.util.bridges.freemarker;
+
+import com.liferay.portal.kernel.io.unsync.UnsyncStringWriter;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.template.Template;
+import com.liferay.portal.kernel.template.TemplateContextType;
+import com.liferay.portal.kernel.template.TemplateException;
+import com.liferay.portal.kernel.template.TemplateManager;
+import com.liferay.portal.kernel.template.TemplateManagerUtil;
+import com.liferay.portal.kernel.template.TemplateResource;
+import com.liferay.portal.kernel.template.TemplateResourceLoaderUtil;
+import com.liferay.portal.kernel.template.TemplateTaglibSupportProvider;
+import com.liferay.portal.kernel.util.ServiceLoader;
+import com.liferay.portal.kernel.util.UnsyncPrintWriterPool;
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import java.util.List;
+
+import javax.portlet.MimeResponse;
+import javax.portlet.PortletContext;
+import javax.portlet.PortletException;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+
+/**
+ * @author Raymond Augé
+ */
+public class FreeMarkerMVCPortlet extends MVCPortlet {
+
+	@Override
+	protected void include(
+			String path, PortletRequest portletRequest,
+			PortletResponse portletResponse, String lifecycle)
+		throws IOException, PortletException {
+
+		PortletContext portletContext = getPortletContext();
+
+		String servletContextName = portletContext.getPortletContextName();
+
+		String resourcePath = servletContextName.concat(
+			TemplateResource.SERVLET_SEPARATOR).concat(path);
+
+		boolean resourceExists = false;
+
+		try {
+			resourceExists = TemplateResourceLoaderUtil.hasTemplateResource(
+				TemplateManager.FREEMARKER, resourcePath);
+		}
+		catch (TemplateException te) {
+			throw new IOException(te);
+		}
+
+		if (!resourceExists) {
+			_log.error(path + " is not a valid include");
+		}
+		else {
+			try {
+				TemplateResource templateResource =
+					TemplateResourceLoaderUtil.getTemplateResource(
+						TemplateManager.FREEMARKER, resourcePath);
+
+				Template template = TemplateManagerUtil.getTemplate(
+					TemplateManager.FREEMARKER, templateResource,
+					TemplateContextType.CLASS_LOADER);
+
+				TemplateTaglibSupportProvider taglibSupportProvider =
+					getTaglibSupportProvider();
+
+				if (taglibSupportProvider != null) {
+					taglibSupportProvider.addTaglibSupport(
+						template, servletContextName, portletRequest,
+						portletResponse);
+				}
+
+				template.put("portletContext", getPortletContext());
+				template.put(
+					"userInfo",
+					portletRequest.getAttribute(PortletRequest.USER_INFO));
+
+				Writer writer = null;
+
+				if (portletResponse instanceof MimeResponse) {
+					MimeResponse mimeResponse = (MimeResponse)portletResponse;
+
+					writer = UnsyncPrintWriterPool.borrow(
+						mimeResponse.getWriter());
+				}
+				else {
+					writer = new UnsyncStringWriter();
+				}
+
+				template.processTemplate(writer);
+			}
+			catch (Exception e) {
+				throw new PortletException(e);
+			}
+		}
+
+		if (clearRequestParameters) {
+			if (lifecycle.equals(PortletRequest.RENDER_PHASE)) {
+				portletResponse.setProperty("clear-request-parameters", "true");
+			}
+		}
+	}
+
+	@Override
+	public void destroy() {
+		super.destroy();
+
+		TemplateManagerUtil.destroy(getClass().getClassLoader());
+	}
+
+	protected TemplateTaglibSupportProvider getTaglibSupportProvider()
+		throws Exception {
+
+		if (_taglibSupportProvider != null) {
+			return _taglibSupportProvider;
+		}
+
+		List<TemplateTaglibSupportProvider> providers = ServiceLoader.load(
+			TemplateTaglibSupportProvider.class);
+
+		if (!providers.isEmpty()) {
+			_taglibSupportProvider = providers.get(0);
+		}
+
+		return _taglibSupportProvider;
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(FreeMarkerMVCPortlet.class);
+
+	private TemplateTaglibSupportProvider _taglibSupportProvider;
+
+}
---
+++ util-java/src/com/liferay/util/freemarker/FreeMarkerTaglibSupportProvider.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.util.freemarker;
+
+import com.liferay.portal.kernel.servlet.PortletServlet;
+import com.liferay.portal.kernel.servlet.ServletContextPool;
+import com.liferay.portal.kernel.template.Template;
+import com.liferay.portal.kernel.template.TemplateResource;
+import com.liferay.portal.kernel.template.TemplateTaglibSupportProvider;
+import com.liferay.portal.util.PortalUtil;
+
+import freemarker.ext.servlet.HttpRequestHashModel;
+import freemarker.ext.servlet.ServletContextHashModel;
+import freemarker.template.ObjectWrapper;
+import freemarker.template.TemplateHashModel;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Raymond Augé
+ */
+public class FreeMarkerTaglibSupportProvider
+	implements TemplateTaglibSupportProvider {
+
+	public void addTaglibSupport(
+			Template template, String servletContextName,
+			PortletRequest portletRequest, PortletResponse portletResponse)
+		throws Exception {
+
+		ServletContext servletContext = ServletContextPool.get(
+			servletContextName);
+
+		HttpServletRequest request = PortalUtil.getHttpServletRequest(
+			portletRequest);
+		HttpServletResponse response = PortalUtil.getHttpServletResponse(
+			portletResponse);
+
+		template.prepare(request);
+
+		template.put(
+			"fullTemplatesPath", servletContextName.concat(
+				TemplateResource.SERVLET_SEPARATOR));
+
+		TemplateHashModel taglibsFactory =
+			FreeMarkerTaglibFactoryUtil.createTaglibFactory(servletContext);
+
+		template.put("PortletJspTagLibs", taglibsFactory);
+
+		ServletConfig servletConfig =
+			(ServletConfig)portletRequest.getAttribute(
+				PortletServlet.PORTLET_SERVLET_CONFIG);
+
+		final PortletServlet portletServlet = new PortletServlet();
+
+		portletServlet.init(servletConfig);
+
+		ServletContextHashModel servletContextHashModel =
+			new ServletContextHashModel(
+				portletServlet, ObjectWrapper.DEFAULT_WRAPPER);
+
+		template.put("Application", servletContextHashModel);
+
+		HttpRequestHashModel httpRequestHashModel = new HttpRequestHashModel(
+			request, response, ObjectWrapper.DEFAULT_WRAPPER);
+
+		template.put("Request", httpRequestHashModel);
+	}
+
+	private static final String _SERVLET_SEPARATOR = "_SERVLET_CONTEXT_";
+
+}
