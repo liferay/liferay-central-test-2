juliocamarero/liferay-portal

LPS-28930 Apply new methods

portal-web/docroot/html/portlet/polls_display/view.jsp
@@ -112,9 +112,7 @@ boolean showIconsActions = themeDisplay.isSignedIn() && (hasConfigurationPermiss
 <c:if test="<%= hasViewPermission && showIconsActions %>">
 
 	<%
-	Group controlPanelGroup = GroupLocalServiceUtil.getGroup(themeDisplay.getCompanyId(), GroupConstants.CONTROL_PANEL);
-
-	long controlPanelPlid = LayoutLocalServiceUtil.getDefaultPlid(controlPanelGroup.getGroupId(), true);
+	long controlPanelPlid = PortalUtil.getControlPanelPlid(themeDisplay.getCompanyId());
 
 	PortletURL redirectURL = liferayPortletResponse.createRenderURL();
 

portal-web/docroot/html/portlet/portlet_configuration/edit_permissions.jsp
@@ -128,9 +128,7 @@ renderPortletURL.setParameter("resourceGroupId", String.valueOf(resourceGroupId)
 renderPortletURL.setParameter("resourcePrimKey", resourcePrimKey);
 renderPortletURL.setParameter("roleTypes", roleTypesParam);
 
-Group controlPanelGroup = GroupLocalServiceUtil.getGroup(company.getCompanyId(), GroupConstants.CONTROL_PANEL);
-
-long controlPanelPlid = LayoutLocalServiceUtil.getDefaultPlid(controlPanelGroup.getGroupId(), true);
+long controlPanelPlid = PortalUtil.getControlPanelPlid(themeDisplay.getCompanyId());
 
 PortletURLImpl definePermissionsURL = new PortletURLImpl(request, PortletKeys.ROLES_ADMIN, controlPanelPlid, PortletRequest.RENDER_PHASE);
 

portal-web/docroot/html/portlet/staging_bar/view_layout_revision_details.jsp
@@ -92,9 +92,7 @@ else {
 					handler: function(event) {
 
 						<%
-						Group controlPanelGroup = GroupLocalServiceUtil.getGroup(themeDisplay.getCompanyId(), GroupConstants.CONTROL_PANEL);
-
-						long controlPanelPlid = LayoutLocalServiceUtil.getDefaultPlid(controlPanelGroup.getGroupId(), true);
+						long controlPanelPlid = PortalUtil.getControlPanelPlid(themeDisplay.getCompanyId());
 
 						PortletURL portletURL = liferayPortletResponse.createLiferayPortletURL(controlPanelPlid, PortletKeys.MY_WORKFLOW_TASKS, PortletRequest.RENDER_PHASE);
 

portal-web/docroot/html/taglib/ui/ddm-template-selector/page.jsp
@@ -22,9 +22,9 @@ String icon = GetterUtil.getString((String)request.getAttribute("liferay-ui:ddm-
 String message = (String)request.getAttribute("liferay-ui:ddm-template-selector:message");
 String refreshURL = (String)request.getAttribute("liferay-ui:ddm-template-selector:refreshURL");
 
-Group controlPanelGroup = GroupLocalServiceUtil.getGroup(themeDisplay.getCompanyId(), GroupConstants.CONTROL_PANEL);
+long controlPanelPlid = PortalUtil.getControlPanelPlid(themeDisplay.getCompanyId());
 
-LiferayPortletURL liferayPortletURL = PortletURLFactoryUtil.create(request, PortletKeys.PORTLET_DISPLAY_TEMPLATES, LayoutLocalServiceUtil.getDefaultPlid(controlPanelGroup.getGroupId(), true), PortletRequest.RENDER_PHASE);
+LiferayPortletURL liferayPortletURL = PortletURLFactoryUtil.create(request, PortletKeys.PORTLET_DISPLAY_TEMPLATES, controlPanelPlid, PortletRequest.RENDER_PHASE);
 
 liferayPortletURL.setDoAsGroupId(scopeGroupId);
 liferayPortletURL.setParameter("struts_action", "/dynamic_data_mapping/view_template");

portal-web/docroot/html/taglib/ui/input_asset_links/page.jsp
@@ -51,9 +51,9 @@ else {
 	}
 }
 
-Group controlPanelGroup = GroupLocalServiceUtil.getGroup(themeDisplay.getCompanyId(), GroupConstants.CONTROL_PANEL);
+long controlPanelPlid = PortalUtil.getControlPanelPlid(themeDisplay.getCompanyId());
 
-PortletURL assetBrowserURL = PortletURLFactoryUtil.create(request, PortletKeys.ASSET_BROWSER, LayoutLocalServiceUtil.getDefaultPlid(controlPanelGroup.getGroupId(), true), PortletRequest.RENDER_PHASE);
+PortletURL assetBrowserURL = PortletURLFactoryUtil.create(request, PortletKeys.ASSET_BROWSER, controlPanelPlid, PortletRequest.RENDER_PHASE);
 
 assetBrowserURL.setWindowState(LiferayWindowState.POP_UP);
 assetBrowserURL.setPortletMode(PortletMode.VIEW);

liferay/liferay-central-test-2

portal-web/docroot/html/portlet/polls_display/view.jsp
@@ -112,9 +112,7 @@ boolean showIconsActions = themeDisplay.isSignedIn() && (hasConfigurationPermiss
 <c:if test="<%= hasViewPermission && showIconsActions %>">
 
 	<%
-	Group controlPanelGroup = GroupLocalServiceUtil.getGroup(themeDisplay.getCompanyId(), GroupConstants.CONTROL_PANEL);
-
-	long controlPanelPlid = LayoutLocalServiceUtil.getDefaultPlid(controlPanelGroup.getGroupId(), true);
+	long controlPanelPlid = PortalUtil.getControlPanelPlid(company.getCompanyId());
 
 	PortletURL redirectURL = liferayPortletResponse.createRenderURL();
 

portal-web/docroot/html/portlet/portlet_configuration/edit_permissions.jsp
@@ -128,9 +128,7 @@ renderPortletURL.setParameter("resourceGroupId", String.valueOf(resourceGroupId)
 renderPortletURL.setParameter("resourcePrimKey", resourcePrimKey);
 renderPortletURL.setParameter("roleTypes", roleTypesParam);
 
-Group controlPanelGroup = GroupLocalServiceUtil.getGroup(company.getCompanyId(), GroupConstants.CONTROL_PANEL);
-
-long controlPanelPlid = LayoutLocalServiceUtil.getDefaultPlid(controlPanelGroup.getGroupId(), true);
+long controlPanelPlid = PortalUtil.getControlPanelPlid(company.getCompanyId());
 
 PortletURLImpl definePermissionsURL = new PortletURLImpl(request, PortletKeys.ROLES_ADMIN, controlPanelPlid, PortletRequest.RENDER_PHASE);
 

portal-web/docroot/html/portlet/staging_bar/view_layout_revision_details.jsp
@@ -92,9 +92,7 @@ else {
 					handler: function(event) {
 
 						<%
-						Group controlPanelGroup = GroupLocalServiceUtil.getGroup(themeDisplay.getCompanyId(), GroupConstants.CONTROL_PANEL);
-
-						long controlPanelPlid = LayoutLocalServiceUtil.getDefaultPlid(controlPanelGroup.getGroupId(), true);
+						long controlPanelPlid = PortalUtil.getControlPanelPlid(company.getCompanyId());
 
 						PortletURL portletURL = liferayPortletResponse.createLiferayPortletURL(controlPanelPlid, PortletKeys.MY_WORKFLOW_TASKS, PortletRequest.RENDER_PHASE);
 

portal-web/docroot/html/taglib/ui/ddm-template-selector/page.jsp
@@ -22,9 +22,9 @@ String icon = GetterUtil.getString((String)request.getAttribute("liferay-ui:ddm-
 String message = (String)request.getAttribute("liferay-ui:ddm-template-selector:message");
 String refreshURL = (String)request.getAttribute("liferay-ui:ddm-template-selector:refreshURL");
 
-Group controlPanelGroup = GroupLocalServiceUtil.getGroup(themeDisplay.getCompanyId(), GroupConstants.CONTROL_PANEL);
+long controlPanelPlid = PortalUtil.getControlPanelPlid(company.getCompanyId());
 
-LiferayPortletURL liferayPortletURL = PortletURLFactoryUtil.create(request, PortletKeys.PORTLET_DISPLAY_TEMPLATES, LayoutLocalServiceUtil.getDefaultPlid(controlPanelGroup.getGroupId(), true), PortletRequest.RENDER_PHASE);
+LiferayPortletURL liferayPortletURL = PortletURLFactoryUtil.create(request, PortletKeys.PORTLET_DISPLAY_TEMPLATES, controlPanelPlid, PortletRequest.RENDER_PHASE);
 
 liferayPortletURL.setDoAsGroupId(scopeGroupId);
 liferayPortletURL.setParameter("struts_action", "/dynamic_data_mapping/view_template");

portal-web/docroot/html/taglib/ui/input_asset_links/page.jsp
@@ -51,9 +51,9 @@ else {
 	}
 }
 
-Group controlPanelGroup = GroupLocalServiceUtil.getGroup(themeDisplay.getCompanyId(), GroupConstants.CONTROL_PANEL);
+long controlPanelPlid = PortalUtil.getControlPanelPlid(company.getCompanyId());
 
-PortletURL assetBrowserURL = PortletURLFactoryUtil.create(request, PortletKeys.ASSET_BROWSER, LayoutLocalServiceUtil.getDefaultPlid(controlPanelGroup.getGroupId(), true), PortletRequest.RENDER_PHASE);
+PortletURL assetBrowserURL = PortletURLFactoryUtil.create(request, PortletKeys.ASSET_BROWSER, controlPanelPlid, PortletRequest.RENDER_PHASE);
 
 assetBrowserURL.setWindowState(LiferayWindowState.POP_UP);
 assetBrowserURL.setPortletMode(PortletMode.VIEW);