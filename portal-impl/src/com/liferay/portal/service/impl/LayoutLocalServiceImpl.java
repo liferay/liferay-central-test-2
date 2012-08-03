d92c1fa9a03bdd630ff06debaea0b403f2b1774f
Hugo Huijser <hugo.huijser@liferay.com>
2012-08-03 10:19:51 -0700

    LPS-29054 Rollback LPS-28436 (reverse-merged from commit 4a257c9f9c1b7293d1ada78dac4fe68ad852395a)

--- portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
@@ -1136,9 +1136,7 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 
 		int count = layoutSet.getPageCount();
 
-		if (PropsValues.USER_GROUPS_COPY_LAYOUTS_TO_USER_PERSONAL_SITE &&
-			group.isUser()) {
-
+		if (group.isUser()) {
 			List<UserGroup> userGroups = userPersistence.getUserGroups(
 				group.getClassPK());
 
@@ -1239,9 +1237,7 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 			return true;
 		}
 
-		if (PropsValues.USER_GROUPS_COPY_LAYOUTS_TO_USER_PERSONAL_SITE &&
-			group.isUser()) {
-
+		if (group.isUser()) {
 			List<UserGroup> userGroups = userPersistence.getUserGroups(
 				group.getClassPK());
 
