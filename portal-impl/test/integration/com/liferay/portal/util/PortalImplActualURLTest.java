dbac9935302b9430febca10fa30396be4c89e310
Brian Chan <brian.chan@liferay.com>
2012-08-06 11:13:49 -0700

    LPS-28949 Simplify test

--- portal-impl/test/integration/com/liferay/portal/util/PortalImplActualURLTest.java
+++ portal-impl/test/integration/com/liferay/portal/util/PortalImplActualURLTest.java
@@ -17,7 +17,6 @@ package com.liferay.portal.util;
 import com.liferay.portal.NoSuchLayoutException;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
-import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.GroupConstants;
 import com.liferay.portal.model.Layout;
@@ -25,19 +24,16 @@ import com.liferay.portal.model.LayoutConstants;
 import com.liferay.portal.model.LayoutTypePortlet;
 import com.liferay.portal.model.LayoutTypePortletConstants;
 import com.liferay.portal.model.UserGroup;
-import com.liferay.portal.model.UserGroupConstants;
 import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
 import com.liferay.portal.service.LayoutServiceUtil;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.service.ServiceTestUtil;
 import com.liferay.portal.service.UserGroupLocalServiceUtil;
-import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.test.EnvironmentExecutionTestListener;
 import com.liferay.portal.test.ExecutionTestListeners;
 import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
 import com.liferay.portal.webdav.methods.Method;
-import com.liferay.portlet.journal.model.JournalArticle;
 import com.liferay.portlet.journal.service.JournalArticleLocalServiceUtil;
 
 import java.util.HashMap;
@@ -58,223 +54,137 @@ import org.springframework.mock.web.MockHttpServletRequest;
 public class PortalImplActualURLTest {
 
 	@Test
-	public void testPortalImplWithArticleFriendlyURL() throws Exception {
-		setupEnvironmentForArticleFriendlyURLTest();
+	public void testChildLayoutFriendlyURL() throws Exception {
+		ServiceContext serviceContext = ServiceTestUtil.getServiceContext();
 
-		Map<String, Object> requestContext = new HashMap<String, Object>();
-		Map<String, String[]> params = new HashMap<String, String[]>();
+		UserGroup userGroup = UserGroupLocalServiceUtil.addUserGroup(
+			TestPropsValues.getUserId(), TestPropsValues.getCompanyId(),
+			"Test " + ServiceTestUtil.nextInt(), StringPool.BLANK);
 
-		String requestURI = "/";
+		Group group = userGroup.getGroup();
 
-		MockHttpServletRequest mockHttpServletRequest =
-			new MockHttpServletRequest(Method.GET, requestURI);
+		Layout homeLayout = LayoutLocalServiceUtil.addLayout(
+			serviceContext.getUserId(), group.getGroupId(), true,
+			LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, "Home", StringPool.BLANK,
+			StringPool.BLANK, LayoutConstants.TYPE_PORTLET, false,
+			StringPool.BLANK, serviceContext);
 
-		requestContext.put("request", mockHttpServletRequest);
+		LayoutLocalServiceUtil.addLayout(
+			serviceContext.getUserId(), group.getGroupId(), true,
+			homeLayout.getLayoutId(), "Child Layout", StringPool.BLANK,
+			StringPool.BLANK, LayoutConstants.TYPE_PORTLET, false,
+			StringPool.BLANK, serviceContext);
 
 		String actualURL = PortalUtil.getActualURL(
-			_testSite.getGroupId(), false, Portal.PATH_MAIN,
-			WORKING_ARTICLE_TITLE_FRIENDLYURL, params, requestContext);
+			userGroup.getGroup().getGroupId(), true, Portal.PATH_MAIN,
+			"/~/" + userGroup.getUserGroupId() + "/child-layout",
+			new HashMap<String, String[]>(), getRequestContext());
 
-		Assert.assertFalse(actualURL == null);
-
-		boolean nonExistingLayoutFound = false;
+		Assert.assertNotNull(actualURL);
 
 		try {
 			PortalUtil.getActualURL(
-				_testSite.getGroupId(), false, Portal.PATH_MAIN,
-				NOT_WORKING_ARTICLE_TITLE_FRIENDLYURL, params, requestContext);
+				userGroup.getGroup().getGroupId(), true, Portal.PATH_MAIN,
+				"/~/" + userGroup.getUserGroupId() +
+					"/non-existing-child-layout",
+				new HashMap<String, String[]>(), getRequestContext());
+
+			Assert.fail();
 		}
-		catch (NoSuchLayoutException e) {
-			nonExistingLayoutFound = true;
+		catch (NoSuchLayoutException nsle) {
 		}
 
-		Assert.assertTrue(nonExistingLayoutFound);
-
-		JournalArticleLocalServiceUtil.deleteArticle(
-			_testSite.getGroupId(), _article.getArticleId(),
-			ServiceTestUtil.getServiceContext());
-
-		LayoutLocalServiceUtil.deleteLayout(_homePage);
-		LayoutLocalServiceUtil.deleteLayout(_testPage);
-		GroupLocalServiceUtil.deleteGroup(_testSite);
+		UserGroupLocalServiceUtil.deleteUserGroup(userGroup);
 	}
 
 	@Test
-	public void testPortalImplWithVirtualLayoutFriendlyURL() throws Exception {
-		setupEnvironmentForVirtualLayoutFriendlyURLTest();
-
-		Map<String, Object> requestContext = new HashMap<String, Object>();
-		Map<String, String[]> params = new HashMap<String, String[]>();
-
-		String requestURI = "/";
-
-		MockHttpServletRequest mockHttpServletRequest =
-			new MockHttpServletRequest(Method.GET, requestURI);
-
-		requestContext.put("request", mockHttpServletRequest);
-
-		String actualURL = PortalUtil.getActualURL(
-			_userGroup.getGroup().getGroupId(), true, Portal.PATH_MAIN,
-			_existingVirtualLayoutFriendlyURL, params, requestContext);
-
-		Assert.assertFalse(actualURL == null);
-
-		boolean nonExistingLayoutFound = false;
-
-		try {
-			PortalUtil.getActualURL(
-				_userGroup.getGroup().getGroupId(), true, Portal.PATH_MAIN,
-				_nonExistingVirtualLayoutFriendlyURL, params, requestContext);
-		}
-		catch (NoSuchLayoutException e) {
-			nonExistingLayoutFound = true;
-		}
-
-		Assert.assertTrue(nonExistingLayoutFound);
-
-		LayoutLocalServiceUtil.deleteLayout(_virtualHomeSub);
-		LayoutLocalServiceUtil.deleteLayout(_virtualHome);
-	}
-
-	protected void setupEnvironmentForArticleFriendlyURLTest()
-		throws Exception {
-
+	public void testJournalArticleFriendlyURL() throws Exception {
 		ServiceContext serviceContext = ServiceTestUtil.getServiceContext();
 
-		String siteName = "Test" + ServiceTestUtil.nextInt();
-		String siteFriendlyURL = StringPool.SLASH + siteName.toLowerCase();
+		Group group = GroupLocalServiceUtil.addGroup(
+			TestPropsValues.getUserId(), GroupConstants.DEFAULT_PARENT_GROUP_ID,
+			StringPool.BLANK, 0, "Test " + ServiceTestUtil.nextInt(),
+			StringPool.BLANK, GroupConstants.TYPE_SITE_OPEN, StringPool.BLANK,
+			true, true, serviceContext);
 
-		_testSite = GroupLocalServiceUtil.addGroup(
-			serviceContext.getUserId(), GroupConstants.DEFAULT_PARENT_GROUP_ID,
-			null, 0, siteName, StringPool.BLANK, GroupConstants.TYPE_SITE_OPEN,
-			siteFriendlyURL, true, true, ServiceTestUtil.getServiceContext());
+		LayoutLocalServiceUtil.addLayout(
+			TestPropsValues.getUserId(), group.getGroupId(), false,
+			LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, "Home", StringPool.BLANK,
+			StringPool.BLANK, LayoutConstants.TYPE_PORTLET, false,
+			StringPool.BLANK, serviceContext);
 
-		_homePage = LayoutLocalServiceUtil.addLayout(
-			serviceContext.getUserId(), _testSite.getGroupId(), false,
-			LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, "home", "Home",
-			StringPool.BLANK, LayoutConstants.TYPE_PORTLET, false, "/home",
-			ServiceTestUtil.getServiceContext());
+		Layout layout = LayoutLocalServiceUtil.addLayout(
+			TestPropsValues.getUserId(), group.getGroupId(), false,
+			LayoutConstants.DEFAULT_PARENT_LAYOUT_ID,
+			"Test " + ServiceTestUtil.nextInt(), StringPool.BLANK,
+			StringPool.BLANK, LayoutConstants.TYPE_PORTLET, false,
+			StringPool.BLANK, serviceContext);
 
-		_testPage = LayoutLocalServiceUtil.addLayout(
-			serviceContext.getUserId(), _testSite.getGroupId(), false,
-			LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, "testpage", "TestPage",
-			StringPool.BLANK, LayoutConstants.TYPE_PORTLET, false, "/testpage",
-			ServiceTestUtil.getServiceContext());
+		LayoutTypePortlet layoutTypePortlet =
+			(LayoutTypePortlet)layout.getLayoutType();
 
-		String portletId = PortletKeys.ASSET_PUBLISHER;
+		String portletId = layoutTypePortlet.addPortletId(
+			TestPropsValues.getUserId(), PortletKeys.ASSET_PUBLISHER,
+			"column-1", 0);
 
-		LayoutTypePortlet layoutTypePortlet = LayoutTypePortletFactoryUtil
-			.create(_testPage);
+		layoutTypePortlet.setTypeSettingsProperty(
+			LayoutTypePortletConstants.DEFAULT_ASSET_PUBLISHER_PORTLET_ID,
+			portletId);
 
-		String columnId = "column-1";
-		int columnPos = 0;
-
-		portletId = layoutTypePortlet.addPortletId(
-			serviceContext.getUserId(), portletId, columnId, columnPos);
-
-		layoutTypePortlet.resetModes();
-		layoutTypePortlet.resetStates();
-
-		_testPage = LayoutServiceUtil.updateLayout(
-			_testPage.getGroupId(), _testPage.isPrivateLayout(),
-			_testPage.getLayoutId(), _testPage.getTypeSettings());
+		layout = LayoutServiceUtil.updateLayout(
+			layout.getGroupId(), layout.isPrivateLayout(), layout.getLayoutId(),
+			layout.getTypeSettings());
 
 		Map<Locale, String> titleMap = new HashMap<Locale, String>();
 
-		Locale englishLocale = new Locale("en", "US");
+		titleMap.put(Locale.US, "Test Journal Article");
 
-		titleMap.put(englishLocale, "ExistingArticle");
-
-		Map<Locale, String> descriptionMap = new HashMap<Locale, String>();
-
-		StringBundler sb = new StringBundler();
+		StringBundler sb = new StringBundler(6);
 
 		sb.append("<?xml version=\"1.0\"?><root available-locales=");
 		sb.append("\"en_US\" default-locale=\"en_US\">");
 		sb.append("<static-content language-id=\"en_US\"><![CDATA[<p>");
-		sb.append("This is the test content in English");
+		sb.append("This test content is in English.");
 		sb.append("</p>]]>");
 		sb.append("</static-content></root>");
 
-		_article = JournalArticleLocalServiceUtil.addArticle(
-			serviceContext.getUserId(), _testSite.getGroupId(), 0, 0, 0,
-			StringPool.BLANK, true, 1, titleMap, descriptionMap, sb.toString(),
-			"general", null, null, _testPage.getUuid(), 1, 1, 1965, 0, 0, 0, 0,
-			0, 0, 0, true, 0, 0, 0, 0, 0, true, false, false, null, null, null,
-			"/existingarticle", serviceContext);
+		JournalArticleLocalServiceUtil.addArticle(
+			TestPropsValues.getUserId(), group.getGroupId(), 0, 0, 0,
+			StringPool.BLANK, true, 1, titleMap, new HashMap<Locale, String>(),
+			sb.toString(), "general", null, null, layout.getUuid(), 1, 1, 1965,
+			0, 0, 0, 0, 0, 0, 0, true, 0, 0, 0, 0, 0, true, false, false, null,
+			null, null, StringPool.BLANK, serviceContext);
 
-		UnicodeProperties typeSettingsProperties =
-			_testPage.getTypeSettingsProperties();
+		String actualURL = PortalUtil.getActualURL(
+			group.getGroupId(), false, Portal.PATH_MAIN,
+			"/-/test-journal-article", new HashMap<String, String[]>(),
+			getRequestContext());
 
-		typeSettingsProperties.setProperty(
-			LayoutTypePortletConstants.DEFAULT_ASSET_PUBLISHER_PORTLET_ID,
-			portletId);
+		Assert.assertNotNull(actualURL);
 
-		_testPage.setTypeSettingsProperties(typeSettingsProperties);
+		try {
+			PortalUtil.getActualURL(
+				group.getGroupId(), false, Portal.PATH_MAIN,
+				"/-/non-existing-test-journal-article",
+				new HashMap<String, String[]>(), getRequestContext());
 
-		LayoutLocalServiceUtil.updateLayout(_testPage);
+			Assert.fail();
+		}
+		catch (NoSuchLayoutException nsle) {
+		}
+
+		GroupLocalServiceUtil.deleteGroup(group);
 	}
 
-	protected void setupEnvironmentForVirtualLayoutFriendlyURLTest()
-			throws Exception {
+	protected Map<String, Object> getRequestContext() {
+		Map<String, Object> requestContext = new HashMap<String, Object>();
 
-		ServiceContext serviceContext = ServiceTestUtil.getServiceContext();
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest(Method.GET, "/");
 
-		long userGroupId = ServiceTestUtil.nextLong();
+		requestContext.put("request", mockHttpServletRequest);
 
-		_userGroup = UserGroupLocalServiceUtil.createUserGroup(userGroupId);
-
-		_userGroup.setAddedByLDAPImport(false);
-		_userGroup.setCompanyId(serviceContext.getCompanyId());
-		_userGroup.setDescription("Test User Group");
-		_userGroup.setExpandoBridgeAttributes(
-			ServiceTestUtil.getServiceContext());
-		_userGroup.setName("TestGroup_" + userGroupId);
-		_userGroup.setNew(true);
-		_userGroup.setParentUserGroupId(
-			UserGroupConstants.DEFAULT_PARENT_USER_GROUP_ID);
-
-		UserGroupLocalServiceUtil.addUserGroup(_userGroup);
-
-		GroupLocalServiceUtil.addGroup(
-			serviceContext.getUserId(), GroupConstants.DEFAULT_PARENT_GROUP_ID,
-			UserGroup.class.getName(), _userGroup.getUserGroupId(),
-			String.valueOf(userGroupId), null, 0, null, false, true, null);
-
-		UserLocalServiceUtil.addUserGroupUsers(
-			userGroupId, new long[] {serviceContext.getUserId()});
-
-		_virtualHome = LayoutLocalServiceUtil.addLayout(
-			serviceContext.getUserId(), _userGroup.getGroup().getGroupId(),
-			true, LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, "home", "Home",
-			StringPool.BLANK, LayoutConstants.TYPE_PORTLET, false, "/home",
-			ServiceTestUtil.getServiceContext());
-
-		_virtualHomeSub = LayoutLocalServiceUtil.addLayout(
-			serviceContext.getUserId(), _userGroup.getGroup().getGroupId(),
-			true, _virtualHome.getLayoutId(), "homesub", "HomeSub",
-			StringPool.BLANK, LayoutConstants.TYPE_PORTLET, false, "/homesub",
-			ServiceTestUtil.getServiceContext());
-
-		_existingVirtualLayoutFriendlyURL = "/~/" + userGroupId + "/homesub";
-		_nonExistingVirtualLayoutFriendlyURL =
-			"/~/" + userGroupId + "/homesubbb";
+		return requestContext;
 	}
-
-	private static final String NOT_WORKING_ARTICLE_TITLE_FRIENDLYURL =
-		"/-/nonexistingarticle";
-
-	private static final String WORKING_ARTICLE_TITLE_FRIENDLYURL =
-		"/-/existingarticle";
-
-	private JournalArticle _article;
-	private String _existingVirtualLayoutFriendlyURL;
-	private Layout _homePage;
-	private String _nonExistingVirtualLayoutFriendlyURL;
-	private Layout _testPage;
-	private Group _testSite;
-	private UserGroup _userGroup;
-	private Layout _virtualHome;
-	private Layout _virtualHomeSub;
 
 }
