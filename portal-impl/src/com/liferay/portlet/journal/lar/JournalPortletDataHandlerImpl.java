29e4cae16728eef763cc21c418fbb838b5a0d1d5
Brian Chan <brian.chan@liferay.com>
2012-08-06 17:11:36 -0700

    LPS-29072 Source formatting

--- portal-impl/src/com/liferay/portlet/journal/lar/JournalPortletDataHandlerImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/lar/JournalPortletDataHandlerImpl.java
@@ -681,8 +681,6 @@ public class JournalPortletDataHandlerImpl extends BasePortletDataHandler {
 			"article-resource-uuid");
 
 		if (portletDataContext.isDataStrategyMirror()) {
-			serviceContext.setAttribute("urlTitle", article.getUrlTitle());
-
 			JournalArticleResource articleResource =
 				JournalArticleResourceUtil.fetchByUUID_G(
 					articleResourceUuid, portletDataContext.getScopeGroupId());
@@ -698,6 +696,8 @@ public class JournalPortletDataHandlerImpl extends BasePortletDataHandler {
 			}
 
 			serviceContext.setUuid(articleResourceUuid);
+
+			serviceContext.setAttribute("urlTitle", article.getUrlTitle());
 
 			JournalArticle existingArticle = null;
 
