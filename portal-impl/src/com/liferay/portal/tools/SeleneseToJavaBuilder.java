54eff77f79a5e991733fa3d6e3452b9778dd7f2e
Michael Hashimoto <michael.hashimoto@liferay.com>
2012-08-08 16:59:48 -0700

    LPS-28186

--- portal-impl/src/com/liferay/portal/tools/SeleneseToJavaBuilder.java
+++ portal-impl/src/com/liferay/portal/tools/SeleneseToJavaBuilder.java
@@ -891,14 +891,12 @@ public class SeleneseToJavaBuilder {
 				}
 
 				sb.append(");");
-
 			}
 			else if (param1.equals("clickAndWait")) {
 				sb.append("selenium.click(RuntimeVariables.replace(\"");
 				sb.append(param2);
 				sb.append("\"));");
 				sb.append("selenium.waitForPageToLoad(\"30000\");");
-
 			}
 			else if (param1.equals("clickAtAndWait") ||
 					 param1.equals("keyDownAndWait") ||
@@ -917,7 +915,6 @@ public class SeleneseToJavaBuilder {
 				sb.append(param3);
 				sb.append("\"));");
 				sb.append("selenium.waitForPageToLoad(\"30000\");");
-
 			}
 			else if (param1.equals("close") || param1.equals("goBack") ||
 					 param1.equals("refresh") ||
@@ -952,7 +949,6 @@ public class SeleneseToJavaBuilder {
 				sb.append(text);
 				sb.append("();");
 				sb.append("selenium.waitForPageToLoad(\"30000\");");
-
 			}
 			else if (param1.equals("gotoIf")) {
 				String conditional = StringUtil.replace(
