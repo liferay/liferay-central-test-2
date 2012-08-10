7d64ae5bfa00c5931bb78e3072c52e50ec87214a
Ken Duenwald <ken.duenwald@liferay.com>
2012-08-10 14:50:04 -0700

    LPS-29187

---
+++ portal-web/test/com/liferay/portalweb/portal/dbupgrade/sampledata611/social/relation/SRl_ConfirmTest.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portalweb.portal.dbupgrade.sampledata611.social.relation;
+
+import com.liferay.portalweb.portal.BaseTestCase;
+import com.liferay.portalweb.portal.util.RuntimeVariables;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class SRl_ConfirmTest extends BaseTestCase {
+	public void testSRl_Confirm() throws Exception {
+		selenium.open("/web/socialrelationsn1/home/");
+
+		for (int second = 0;; second++) {
+			if (second >= 90) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isVisible("link=Requests Test Page")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.click(RuntimeVariables.replace("link=Requests Test Page"));
+		selenium.waitForPageToLoad("30000");
+		assertEquals(RuntimeVariables.replace("Confirm"),
+			selenium.getText("//div[2]/div/ul/li/a/span"));
+		selenium.clickAt("//div[2]/div/ul/li/a/span",
+			RuntimeVariables.replace("Confirm"));
+		selenium.waitForPageToLoad("30000");
+		assertFalse(selenium.isTextPresent("Confirm"));
+	}
+}
