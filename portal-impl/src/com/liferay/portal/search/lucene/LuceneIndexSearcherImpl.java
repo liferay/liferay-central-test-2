80781bbc685f58e618a110f7ff0adc224fc77027
Zsolt Berentey <zsolt.berentey@liferay.com>
2012-08-15 11:24:04 +0200

    LPS-28728 Source formatting

--- portal-impl/src/com/liferay/portal/search/lucene/LuceneIndexSearcherImpl.java
+++ portal-impl/src/com/liferay/portal/search/lucene/LuceneIndexSearcherImpl.java
@@ -568,11 +568,7 @@ public class LuceneIndexSearcherImpl implements IndexSearcher {
 
 			QueryConfig queryConfig = query.getQueryConfig();
 
-			for (int i = start; i < end; i++) {
-				if (i >= PropsValues.INDEX_SEARCH_LIMIT) {
-					break;
-				}
-
+			for (int i = start; i < start + subsetTotal; i++) {
 				int docId = hitDocs.getDocId(i);
 
 				org.apache.lucene.document.Document document =
@@ -592,9 +588,6 @@ public class LuceneIndexSearcherImpl implements IndexSearcher {
 						document, query, Field.DESCRIPTION, locale, queryTerms);
 					getSnippet(
 						document, query, Field.TITLE, locale, queryTerms);
-				}
-				else {
-					queryTerms = getQueryTerms(query);
 				}
 
 				subsetDocument.addText(Field.SNIPPET, subsetSnippet);
@@ -621,6 +614,10 @@ public class LuceneIndexSearcherImpl implements IndexSearcher {
 					catch (Exception e) {
 					}
 				}
+			}
+
+			if (!queryConfig.isHighlightEnabled()) {
+				queryTerms = getQueryTerms(query);
 			}
 
 			hits.setStart(startTime);
