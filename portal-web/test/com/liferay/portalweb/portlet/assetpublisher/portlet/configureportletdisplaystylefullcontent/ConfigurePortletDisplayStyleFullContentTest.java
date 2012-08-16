152854a3d5161f3c8a19de36522971075ce64245
Albert Lee <albert.lee@liferay.com>
2012-08-16 12:05:52 -0700

    Test Fixes - portlet/assetpublisher/portlet/configureportletdisplaystylefullcontent

--- portal-web/test/com/liferay/portalweb/portlet/assetpublisher/portlet/configureportletdisplaystylefullcontent/ConfigurePortletDisplayStyleFullContentTest.java
+++ portal-web/test/com/liferay/portalweb/portlet/assetpublisher/portlet/configureportletdisplaystylefullcontent/ConfigurePortletDisplayStyleFullContentTest.java
@@ -24,23 +24,6 @@ public class ConfigurePortletDisplayStyleFullContentTest extends BaseTestCase {
 	public void testConfigurePortletDisplayStyleFullContent()
 		throws Exception {
 		selenium.open("/web/guest/home/");
-
-		for (int second = 0;; second++) {
-			if (second >= 90) {
-				fail("timeout");
-			}
-
-			try {
-				if (selenium.isVisible("link=Asset Publisher Test Page")) {
-					break;
-				}
-			}
-			catch (Exception e) {
-			}
-
-			Thread.sleep(1000);
-		}
-
 		selenium.clickAt("link=Asset Publisher Test Page",
 			RuntimeVariables.replace("Asset Publisher Test Page"));
 		selenium.waitForPageToLoad("30000");
