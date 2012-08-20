169e33f4a4c00aab3ae85376d2f715a5f50bc79b
shuyangzhou <shuyang.zhou@liferay.com>
2012-08-20 17:39:24 +0800

    LPS-29369 Remove wrong used LIFERAY_SHARED_

--- portal-service/src/com/liferay/portal/kernel/util/WebKeys.java
+++ portal-service/src/com/liferay/portal/kernel/util/WebKeys.java
@@ -73,11 +73,11 @@ public interface WebKeys {
 
 	public static final String OUTPUT_DATA = "LIFERAY_SHARED_OUTPUT_DATA";
 
-	public static final String PAGE_BODY_BOTTOM = "LIFERAY_SHARED_PAGE_BODY_BOTTOM";
+	public static final String PAGE_BODY_BOTTOM = "PAGE_BODY_BOTTOM";
 
-	public static final String PAGE_BODY_TOP = "LIFERAY_SHARED_PAGE_BODY_TOP";
+	public static final String PAGE_BODY_TOP = "PAGE_BODY_TOP";
 
-	public static final String PAGE_BOTTOM = "LIFERAY_SHARED_PAGE_BOTTOM";
+	public static final String PAGE_BOTTOM = "PAGE_BOTTOM";
 
 	public static final String PAGE_DESCRIPTION = "LIFERAY_SHARED_PAGE_DESCRIPTION";
 
@@ -87,7 +87,7 @@ public interface WebKeys {
 
 	public static final String PAGE_TITLE = "LIFERAY_SHARED_PAGE_TITLE";
 
-	public static final String PAGE_TOP = "LIFERAY_SHARED_PAGE_TOP";
+	public static final String PAGE_TOP = "PAGE_TOP";
 
 	public static final String PARALLEL_RENDERING_MERGE_LOCK = "PARALLEL_RENDERING_MERGE_LOCK";
 
