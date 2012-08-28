eaccd9d4422f6cef247383abec9be091c4e996b9
Sergio González <sergio.gonzalez@liferay.com>
2012-08-28 10:54:33 +0200

    LPS-25278 - Change description to TEXTAREA (as it was intended in LPS-2063), set max width in px and upgrade

---
+++ portal-impl/src/com/liferay/portal/upgrade/v6_2_0/UpgradeBlogs.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.v6_2_0;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.upgrade.util.UpgradeTable;
+import com.liferay.portal.kernel.upgrade.util.UpgradeTableFactoryUtil;
+import com.liferay.portal.upgrade.v6_2_0.util.BlogsEntryTable;
+
+/**
+ * @author Sergio González
+ */
+public class UpgradeBlogs extends UpgradeProcess {
+
+	protected void doUpgrade() throws Exception {
+		try {
+			runSQL("alter_column_type BlogsEntry description STRING null");
+		}
+		catch (Exception e) {
+			UpgradeTable upgradeTable = UpgradeTableFactoryUtil.getUpgradeTable(
+				BlogsEntryTable.TABLE_NAME, BlogsEntryTable.TABLE_COLUMNS);
+
+			upgradeTable.setCreateSQL(BlogsEntryTable.TABLE_SQL_CREATE);
+			upgradeTable.setIndexesSQL(BlogsEntryTable.TABLE_SQL_ADD_INDEXES);
+
+			upgradeTable.updateTable();
+		}
+	}
+
+}
