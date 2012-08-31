dd1c44cbdf60acaf5fd2b78ee031198756e7c062
Brian Chan <brian.chan@liferay.com>
2012-08-31 10:00:43 -0700

    Regenerate

--- portal-impl/src/com/liferay/portal/upgrade/v6_2_0/util/DDMTemplateTable.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_2_0/util/DDMTemplateTable.java
@@ -53,6 +53,7 @@ public class DDMTemplateTable {
 		"create index IX_2E1BAFD9 on DDMTemplate (classNameId, classPK, type_, mode_)",
 		"create index IX_32F83D16 on DDMTemplate (classPK)",
 		"create index IX_DB24DDDD on DDMTemplate (groupId)",
+		"create index IX_BD9A4A91 on DDMTemplate (groupId, classNameId)",
 		"create index IX_824ADC72 on DDMTemplate (groupId, classNameId, classPK)",
 		"create unique index IX_233D3B8 on DDMTemplate (groupId, templateKey)",
 		"create index IX_33BEF579 on DDMTemplate (language)",
