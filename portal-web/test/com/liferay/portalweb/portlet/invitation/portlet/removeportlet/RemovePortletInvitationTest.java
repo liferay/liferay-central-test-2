67da4193e28f71ac6b9fbcd962b2317f87727219
Lawrence Lee <lawrence.lee@liferay.com>
2012-09-04 11:19:41 -0700

    Test Fixes - portlet/invitation

--- portal-web/test/com/liferay/portalweb/portlet/invitation/portlet/removeportlet/RemovePortletInvitationTest.java
+++ portal-web/test/com/liferay/portalweb/portlet/invitation/portlet/removeportlet/RemovePortletInvitationTest.java
@@ -41,11 +41,26 @@ public class RemovePortletInvitationTest extends BaseTestCase {
 		}
 
 		selenium.clickAt("link=Invitation Test Page",
-			RuntimeVariables.replace(""));
+			RuntimeVariables.replace("Invitation Test Page"));
 		selenium.waitForPageToLoad("30000");
 		selenium.click("//img[@alt='Remove']");
-		assertTrue(selenium.getConfirmation()
-						   .matches("^Are you sure you want to remove this component[\\s\\S]$"));
+
+		for (int second = 0;; second++) {
+			if (second >= 90) {
+				fail("timeout");
+			}
+
+			try {
+				if ("Are you sure you want to remove this component?".equals(
+							selenium.getConfirmation())) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
 
 		for (int second = 0;; second++) {
 			if (second >= 90) {
