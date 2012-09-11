94255376942c1e08de97de8e6179068bc513076a
Juan Fernández <juan.fernandez@liferay.com>
2012-09-11 10:36:23 +0200

    LPS-29677 Autogenerated

--- portal-service/src/com/liferay/portal/model/Team.java
+++ portal-service/src/com/liferay/portal/model/Team.java
@@ -14,6 +14,8 @@
 
 package com.liferay.portal.model;
 
+import com.liferay.portal.kernel.util.Accessor;
+
 /**
  * The extended model interface for the Team service. Represents a row in the &quot;Team&quot; database table, with each column mapped to a property of this class.
  *
@@ -29,6 +31,12 @@ public interface Team extends TeamModel, PersistedModel {
 	 *
 	 * Never modify this interface directly. Add methods to {@link com.liferay.portal.model.impl.TeamImpl} and rerun ServiceBuilder to automatically copy the method declarations to this interface.
 	 */
+	public static final Accessor<Team, Long> TEAM_ID_ACCESSOR = new Accessor<Team, Long>() {
+			public Long get(Team team) {
+				return team.getTeamId();
+			}
+		};
+
 	public com.liferay.portal.model.Role getRole()
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException;
