2e1acb195d539bb86101aae3bd1f90cd62f14511
Jeffrey Yang <jeffrey.yang@liferay.com>
2012-09-13 16:11:11 -0700

    Test Fixes - plugins/analogclock

--- portal-web/test/com/liferay/portalweb/plugins/analogclock/AddPortletTest.java
+++ portal-web/test/com/liferay/portalweb/plugins/analogclock/AddPortletTest.java
@@ -25,14 +25,27 @@ public class AddPortletTest extends BaseTestCase {
 		selenium.selectWindow("null");
 		selenium.selectFrame("relative=top");
 		selenium.open("/web/guest/home/");
-		selenium.waitForVisible("link=Analog Clock Test Page");
-		selenium.click(RuntimeVariables.replace("link=Analog Clock Test Page"));
+		selenium.clickAt("link=Analog Clock Test Page",
+			RuntimeVariables.replace("Analog Clock Test Page"));
 		selenium.waitForPageToLoad("30000");
+		selenium.clickAt("//div[@id='dockbar']",
+			RuntimeVariables.replace("Dockbar"));
+		selenium.waitForElementPresent(
+			"//script[contains(@src,'/aui/aui-editable/aui-editable-min.js')]");
+		assertEquals(RuntimeVariables.replace("Add"),
+			selenium.getText("//li[@id='_145_addContent']/a/span"));
+		selenium.mouseOver("//li[@id='_145_addContent']/a/span");
+		selenium.waitForVisible("//a[@id='_145_addApplication']");
 		assertTrue(selenium.isPartialText("//a[@id='_145_addApplication']",
 				"More"));
 		selenium.clickAt("//a[@id='_145_addApplication']",
 			RuntimeVariables.replace("More"));
-		selenium.waitForElementPresent("//div[@title='Analog Clock']/p/a");
+		selenium.waitForElementPresent(
+			"//script[contains(@src,'/aui/aui-live-search/aui-live-search-min.js')]");
+		selenium.waitForVisible("//input[@id='layout_configuration_content']");
+		selenium.sendKeys("//input[@id='layout_configuration_content']",
+			RuntimeVariables.replace("a"));
+		selenium.waitForVisible("//div[@title='Analog Clock']/p/a");
 		selenium.clickAt("//div[@title='Analog Clock']/p/a",
 			RuntimeVariables.replace("Add"));
 		selenium.waitForVisible("//section");
