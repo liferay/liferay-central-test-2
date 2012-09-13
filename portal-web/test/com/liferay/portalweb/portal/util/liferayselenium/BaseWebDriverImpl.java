fdb271ea90054dab02d04f02f5d4b9ae1c53d888
Michael Hashimoto <michael.hashimoto@liferay.com>
2012-09-13 11:09:33 -0700

    LPS-29885

--- portal-web/test/com/liferay/portalweb/portal/util/liferayselenium/BaseWebDriverImpl.java
+++ portal-web/test/com/liferay/portalweb/portal/util/liferayselenium/BaseWebDriverImpl.java
@@ -179,23 +179,23 @@ public abstract class BaseWebDriverImpl
 	}
 
 	public void waitForElementPresent(String locator) throws Exception {
-		int timeout =
-			TestPropsValues.TIMEOUT_EXPLICIT_WAIT /
-				TestPropsValues.TIMEOUT_IMPLICIT_WAIT;
+		WebDriver.Options options = manage();
 
-		for (int second = 0;; second++) {
-			if (second >= timeout) {
-				BaseTestCase.fail(
+		Timeouts timeouts = options.timeouts();
+
+		timeouts.implicitlyWait(
+			TestPropsValues.TIMEOUT_EXPLICIT_WAIT, TimeUnit.SECONDS);
+
+		try {
+			WebElement webElement = getWebElement(locator);
+		}
+		catch (Exception e) {
+			BaseTestCase.fail(
 					"Timeout: unable to find the locator \"" + locator + "\"");
-			}
-
-			try {
-				if (isElementPresent(locator)) {
-					break;
-				}
-			}
-			catch (Exception e) {
-			}
+		}
+		finally {
+			timeouts.implicitlyWait(
+				TestPropsValues.TIMEOUT_IMPLICIT_WAIT, TimeUnit.SECONDS);
 		}
 	}
 
