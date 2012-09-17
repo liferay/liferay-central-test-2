3252405a4c955b41ae4ebf9ba1ab31a68faf99a5
Brian Chan <brian.chan@liferay.com>
2012-09-17 13:57:07 -0700

    LPS-29792 Source formatting

--- portal-impl/src/com/liferay/portal/server/capabilities/JBossServerCapabilities.java
+++ portal-impl/src/com/liferay/portal/server/capabilities/JBossServerCapabilities.java
@@ -34,8 +34,8 @@ public class JBossServerCapabilities implements ServerCapabilities {
 	protected void determineSupportsHotDeploy(ServletContext servletContext)
 		throws Exception {
 
-		DeepNamedValueScanner deepNamedValueScanner =
-			new DeepNamedValueScanner("scanEnabled", true);
+		DeepNamedValueScanner deepNamedValueScanner = new DeepNamedValueScanner(
+			"scanEnabled", true);
 
 		deepNamedValueScanner.setExcludedClassNames(
 			"ChainedInterceptorFactory", "TagAttributeInfo", ".jandex.",
