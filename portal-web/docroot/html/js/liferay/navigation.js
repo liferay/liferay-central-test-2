d4e30015572ea1e1f6983614139e20d8c71c692a
Byran Zaugg <byran.zaugg@liferay.com>
2012-09-20 12:25:58 -0700

    LPS-30081 - Clicking the gear icon when adding a page, does nothing on the first click

--- portal-web/docroot/html/js/liferay/navigation.js
+++ portal-web/docroot/html/js/liferay/navigation.js
@@ -426,7 +426,7 @@ AUI.add(
 
 								var action = 'show';
 
-								if (toolItem.StateInteraction.get('active')) {
+								if (!toolItem.StateInteraction.get('active')) {
 									action = 'hide';
 								}
 
