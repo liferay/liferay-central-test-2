e0f34ec4f2ce827898fdf346b5df5604ba3ea9c2
Hugo Huijser <hugo.huijser@liferay.com>
2012-09-20 10:30:53 -0700

    LPS-26006 Generated

--- portal-service/src/com/liferay/portal/service/UserLocalService.java
+++ portal-service/src/com/liferay/portal/service/UserLocalService.java
@@ -1693,7 +1693,9 @@ public interface UserLocalService extends BaseLocalService,
 			com.liferay.portal.kernel.exception.SystemException;
 
 	/**
-	* Returns <code>true</code> if the user's password is expiring soon.
+	* Returns <code>true</code> if the password policy is configured to warn
+	* the user that his password is expiring and the remaining time until
+	* expiration is equal or less than the configured warning time.
 	*
 	* @param user the user
 	* @return <code>true</code> if the user's password is expiring soon;
