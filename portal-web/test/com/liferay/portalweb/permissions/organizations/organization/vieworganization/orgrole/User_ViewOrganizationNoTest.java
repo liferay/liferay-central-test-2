25230d6c09a9ebef11f6c691c3eb31f9f85d7b67
Albert Lee <albert.lee@liferay.com>
2012-09-19 17:43:36 -0700

    Test Fixes - permissions/organizations

--- portal-web/test/com/liferay/portalweb/permissions/organizations/organization/vieworganization/orgrole/User_ViewOrganizationNoTest.java
+++ portal-web/test/com/liferay/portalweb/permissions/organizations/organization/vieworganization/orgrole/User_ViewOrganizationNoTest.java
@@ -25,6 +25,10 @@ public class User_ViewOrganizationNoTest extends BaseTestCase {
 		selenium.selectWindow("null");
 		selenium.selectFrame("relative=top");
 		selenium.open("/web/guest/home/");
+		selenium.clickAt("//div[@id='dockbar']",
+			RuntimeVariables.replace("Dockbar"));
+		selenium.waitForElementPresent(
+			"//script[contains(@src,'/liferay/dockbar_underlay.js')]");
 		assertEquals(RuntimeVariables.replace("Go to"),
 			selenium.getText("//li[@id='_145_mySites']/a/span"));
 		selenium.mouseOver("//li[@id='_145_mySites']/a/span");
