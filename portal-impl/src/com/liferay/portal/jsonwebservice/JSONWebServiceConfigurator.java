4fdb0700ab3371661210afb4d317f01abb890c1e
Sergio González <sergio.gonzalez@liferay.com>
2012-09-21 10:06:08 +0200

    LPS-30059 Source formatting

--- portal-impl/src/com/liferay/portal/jsonwebservice/JSONWebServiceConfigurator.java
+++ portal-impl/src/com/liferay/portal/jsonwebservice/JSONWebServiceConfigurator.java
@@ -373,6 +373,7 @@ public class JSONWebServiceConfigurator extends ClassFinder {
 				for (String excludedMethodName : _excludedMethodNames) {
 					if (excludedMethodName.equals(methodName)) {
 						registerMethod = false;
+
 						break;
 					}
 				}
