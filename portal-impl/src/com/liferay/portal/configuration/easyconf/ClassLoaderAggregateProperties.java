ed6154e49020abc008fef17a0ab52f90aae5cfae
Raymond Auge <raymond.auge@liferay.com>
2012-09-27 10:10:18 -0400

    LPS-29904 Regression issue with portal-ext.properties loading order (order should be reversed)

--- portal-impl/src/com/liferay/portal/configuration/easyconf/ClassLoaderAggregateProperties.java
+++ portal-impl/src/com/liferay/portal/configuration/easyconf/ClassLoaderAggregateProperties.java
@@ -23,6 +23,7 @@ import com.germinus.easyconf.JndiURL;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.security.lang.PortalSecurityManagerThreadLocal;
 
 import java.net.URL;
@@ -213,6 +214,8 @@ public class ClassLoaderAggregateProperties extends AggregatedProperties {
 
 		String[] fileNames = tempCompositeConfiguration.getStringArray(
 			Conventions.INCLUDE_PROPERTY);
+
+		ArrayUtil.reverse(fileNames);
 
 		for (String fileName : fileNames) {
 			URL url = null;
