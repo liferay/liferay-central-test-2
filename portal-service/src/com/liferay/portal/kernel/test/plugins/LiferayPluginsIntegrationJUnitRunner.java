735f4d90ea5916a3eaab6dac3ccf2617b9a2ba7d
Brian Chan <brian.chan@liferay.com>
2012-10-08 09:38:40 -0700

    LPS-30348 Source formatting

--- portal-service/src/com/liferay/portal/kernel/test/plugins/LiferayPluginsIntegrationJUnitRunner.java
+++ portal-service/src/com/liferay/portal/kernel/test/plugins/LiferayPluginsIntegrationJUnitRunner.java
@@ -33,8 +33,8 @@ public class LiferayPluginsIntegrationJUnitRunner
 	@Override
 	public void initApplicationContext() {
 
-		// It is not necessary to init application context in plugins because
-		// a portal instance must be up and running
+		// It is not necessary to initialize the application context because a
+		// portal instance must be up and running
 
 	}
 
