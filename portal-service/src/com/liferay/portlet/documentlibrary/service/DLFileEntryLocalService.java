hhuijser/liferay-portal

LPS-30374 Regenerate

portal-service/src/com/liferay/portlet/documentlibrary/service/DLFileEntryLocalService.java
@@ -380,6 +380,11 @@ public interface DLFileEntryLocalService extends BaseLocalService,
 		throws com.liferay.portal.kernel.exception.SystemException;
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> fetchFileEntryByName(
+		long folderId, java.lang.String name)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> getExtraSettingsFileEntries(
 		int start, int end)
 		throws com.liferay.portal.kernel.exception.SystemException;

portal-service/src/com/liferay/portlet/documentlibrary/service/DLFileEntryLocalServiceUtil.java
@@ -454,6 +454,12 @@ public class DLFileEntryLocalServiceUtil {
 		return getService().fetchFileEntryByName(groupId, folderId, name);
 	}
 
+	public static java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> fetchFileEntryByName(
+		long folderId, java.lang.String name)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getService().fetchFileEntryByName(folderId, name);
+	}
+
 	public static java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> getExtraSettingsFileEntries(
 		int start, int end)
 		throws com.liferay.portal.kernel.exception.SystemException {

portal-service/src/com/liferay/portlet/documentlibrary/service/DLFileEntryLocalServiceWrapper.java
@@ -449,6 +449,12 @@ public class DLFileEntryLocalServiceWrapper implements DLFileEntryLocalService,
 			name);
 	}
 
+	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> fetchFileEntryByName(
+		long folderId, java.lang.String name)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return _dlFileEntryLocalService.fetchFileEntryByName(folderId, name);
+	}
+
 	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> getExtraSettingsFileEntries(
 		int start, int end)
 		throws com.liferay.portal.kernel.exception.SystemException {

liferay/liferay-central-test-2

portal-service/src/com/liferay/portlet/documentlibrary/service/DLFileEntryLocalService.java
@@ -434,6 +434,11 @@ public interface DLFileEntryLocalService extends BaseLocalService,
 		throws com.liferay.portal.kernel.exception.SystemException;
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> getFileEntries(
+		long folderId, java.lang.String name)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> getFileEntriesByMimeType(
 		java.lang.String mimeType)
 		throws com.liferay.portal.kernel.exception.SystemException;

portal-service/src/com/liferay/portlet/documentlibrary/service/DLFileEntryLocalServiceUtil.java
@@ -530,6 +530,12 @@ public class DLFileEntryLocalServiceUtil {
 		return getService().getFileEntries(groupId, folderId, start, end, obc);
 	}
 
+	public static java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> getFileEntries(
+		long folderId, java.lang.String name)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getService().getFileEntries(folderId, name);
+	}
+
 	public static java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> getFileEntriesByMimeType(
 		java.lang.String mimeType)
 		throws com.liferay.portal.kernel.exception.SystemException {

portal-service/src/com/liferay/portlet/documentlibrary/service/DLFileEntryLocalServiceWrapper.java
@@ -523,6 +523,12 @@ public class DLFileEntryLocalServiceWrapper implements DLFileEntryLocalService,
 			start, end, obc);
 	}
 
+	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> getFileEntries(
+		long folderId, java.lang.String name)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return _dlFileEntryLocalService.getFileEntries(folderId, name);
+	}
+
 	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> getFileEntriesByMimeType(
 		java.lang.String mimeType)
 		throws com.liferay.portal.kernel.exception.SystemException {