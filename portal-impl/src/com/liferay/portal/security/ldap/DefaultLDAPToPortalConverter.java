0f8acf7f568494363cd2537324eb000ac84cdc0f
Brian Chan <brian.chan@liferay.com>
2012-10-17 14:11:46 -0700

    LPS-30516 Forgot import

--- portal-impl/src/com/liferay/portal/security/ldap/DefaultLDAPToPortalConverter.java
+++ portal-impl/src/com/liferay/portal/security/ldap/DefaultLDAPToPortalConverter.java
@@ -20,6 +20,7 @@ import com.liferay.portal.kernel.ldap.LDAPUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.CalendarFactoryUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.StringPool;
