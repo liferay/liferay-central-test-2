189f61e96623ce1e2c3e0c2b656db8aa3266a3c0
Hugo Huijser <hugo.huijser@liferay.com>
2012-10-22 15:06:30 -0700

    LPS-30473 Unused import

--- portal-impl/src/com/liferay/portlet/wiki/engines/mediawiki/LiferayDataHandler.java
+++ portal-impl/src/com/liferay/portlet/wiki/engines/mediawiki/LiferayDataHandler.java
@@ -14,7 +14,6 @@
 
 package com.liferay.portlet.wiki.engines.mediawiki;
 
-import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
