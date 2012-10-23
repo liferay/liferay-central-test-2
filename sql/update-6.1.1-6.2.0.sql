35f4348873fe93d03978c80aca8726278b7d9677
Marcellus Tavares <marcellus.tavares@liferay.com>
2012-10-23 12:14:44 -0300

    LPS-30610 Renamed detail/list to form/display templates - sql

--- sql/update-6.1.1-6.2.0.sql
+++ sql/update-6.1.1-6.2.0.sql
@@ -236,6 +236,9 @@ alter table DDMStructure add parentStructureId LONG;
 
 alter table DDMTemplate add cacheable BOOLEAN;
 
+update DDMTemplate set type_ = 'display' where type_ = 'list';
+update DDMTemplate set type_ = 'form' where type_ = 'detail';
+
 alter table DLFileEntry add manualCheckInRequired BOOLEAN;
 
 alter table DLFileRank add active_ BOOLEAN;
