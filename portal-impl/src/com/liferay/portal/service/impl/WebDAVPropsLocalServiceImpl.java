b6451b7c105a511654acdb8cc3bf9247544dd34e
shuyangzhou <shuyang.zhou@liferay.com>
2012-10-23 10:54:06 +0800

    LPS-30620 WebDAVPropsLocalServiceImpl is doing write in a read-only transaction

--- portal-impl/src/com/liferay/portal/service/impl/WebDAVPropsLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/WebDAVPropsLocalServiceImpl.java
@@ -63,7 +63,7 @@ public class WebDAVPropsLocalServiceImpl
 			webDavProps.setClassNameId(classNameId);
 			webDavProps.setClassPK(classPK);
 
-			webDAVPropsPersistence.update(webDavProps);
+			webDAVPropsLocalService.updateWebDAVProps(webDavProps);
 		}
 
 		return webDavProps;
