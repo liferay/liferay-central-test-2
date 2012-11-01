80ec8a1e587132aa784e92c560025a395cd8a811
Brian Chan <brian.chan@liferay.com>
2012-11-01 00:21:37 -0700

    LPS-30792

--- portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
+++ portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
@@ -194,7 +194,8 @@ public class HookHotDeployListener
 		"application.startup.events", "auth.failure", "auth.max.failures",
 		"auth.token.impl", "auth.pipeline.post", "auth.pipeline.pre",
 		"auto.login.hooks", "captcha.check.portal.create_account",
-		"captcha.engine.impl", "company.settings.form.authentication",
+		"captcha.engine.impl", "company.default.locale",
+		"company.default.time.zone", "company.settings.form.authentication",
 		"company.settings.form.configuration",
 		"company.settings.form.identification",
 		"company.settings.form.miscellaneous",
@@ -2595,6 +2596,7 @@ public class HookHotDeployListener
 	};
 
 	private static final String[] _PROPS_VALUES_STRING = {
+		"company.default.locale", "company.default.time.zone",
 		"default.landing.page.path",
 		"passwords.passwordpolicytoolkit.generator",
 		"passwords.passwordpolicytoolkit.static",
