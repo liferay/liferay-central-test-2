3f328a0c58c8ec140e1eb2f4761ab5db544c6a49
Sergio González <sergio.gonzalez@liferay.com>
2012-11-02 11:13:28 +0100

    LPS-29498 Moving an organization will move the site associated to it unless the parents don't match (which means the user moved the site after creating it)

--- portal-impl/src/com/liferay/portal/service/impl/OrganizationLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/OrganizationLocalServiceImpl.java
@@ -1610,11 +1610,31 @@ public class OrganizationLocalServiceImpl
 
 		Group group = organization.getGroup();
 
-		if (!oldName.equals(name)) {
+		boolean moveSiteHierarchy = checkMoveSiteHierarchy(
+			oldParentOrganizationId, group.getParentGroupId());
+
+		long parentGroupId = group.getParentGroupId();
+
+		if (moveSiteHierarchy) {
+			if (parentOrganizationId !=
+				OrganizationConstants.DEFAULT_PARENT_ORGANIZATION_ID) {
+
+				Organization parentOrganization =
+					organizationPersistence.fetchByPrimaryKey(
+						parentOrganizationId);
+
+				parentGroupId = parentOrganization.getGroupId();
+			}
+			else {
+				parentGroupId = GroupConstants.DEFAULT_PARENT_GROUP_ID;
+			}
+		}
+
+		if (!oldName.equals(name) || moveSiteHierarchy) {
 			groupLocalService.updateGroup(
-				group.getGroupId(), group.getParentGroupId(), name,
-				group.getDescription(), group.getType(), group.getFriendlyURL(),
-				group.isActive(), null);
+				group.getGroupId(), parentGroupId, name, group.getDescription(),
+				group.getType(), group.getFriendlyURL(), group.isActive(),
+				null);
 		}
 
 		if (group.isSite() != site) {
@@ -1664,6 +1684,31 @@ public class OrganizationLocalServiceImpl
 				addSuborganizations(allSuborganizations, suborganizations);
 			}
 		}
+	}
+
+	protected boolean checkMoveSiteHierarchy(
+			long parentOrganizationId, long parentGroupId)
+		throws PortalException, SystemException {
+
+		if ((parentOrganizationId ==
+				OrganizationConstants.DEFAULT_PARENT_ORGANIZATION_ID) &&
+			(parentGroupId == GroupConstants.DEFAULT_PARENT_GROUP_ID)) {
+
+			return true;
+		}
+
+		if (parentOrganizationId !=
+				OrganizationConstants.DEFAULT_PARENT_ORGANIZATION_ID) {
+
+			Organization parentOrganization =
+				organizationPersistence.fetchByPrimaryKey(parentOrganizationId);
+
+			if (parentOrganization.getGroupId() == parentGroupId) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	protected long getParentOrganizationId(
