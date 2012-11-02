e2cc77d89577f572559cddd3f40b6b906e28d427
Brian Chiu <brian.chiu@liferay.com>
2012-11-02 09:38:52 -0700

    Test Fixes - asset/assetpublisher

--- portal-web/test/com/liferay/portalweb/asset/assetpublisher/portlet/configureportletapenableratings/ConfigurePortletAPEnableRatingsTest.java
+++ portal-web/test/com/liferay/portalweb/asset/assetpublisher/portlet/configureportletapenableratings/ConfigurePortletAPEnableRatingsTest.java
@@ -22,45 +22,66 @@ import com.liferay.portalweb.portal.util.RuntimeVariables;
  */
 public class ConfigurePortletAPEnableRatingsTest extends BaseTestCase {
 	public void testConfigurePortletAPEnableRatings() throws Exception {
-		selenium.selectWindow("null");
-		selenium.selectFrame("relative=top");
-		selenium.open("/web/guest/home/");
-		selenium.clickAt("link=Asset Publisher Test Page",
-			RuntimeVariables.replace("Asset Publisher Test Page"));
-		selenium.waitForPageToLoad("30000");
-		Thread.sleep(5000);
-		assertEquals(RuntimeVariables.replace("Options"),
-			selenium.getText("//span[@title='Options']/ul/li/strong/a"));
-		selenium.clickAt("//span[@title='Options']/ul/li/strong/a",
-			RuntimeVariables.replace("Options"));
-		selenium.waitForVisible(
-			"//div[@class='lfr-component lfr-menu-list']/ul/li/a[contains(.,'Configuration')]");
-		assertEquals(RuntimeVariables.replace("Configuration"),
-			selenium.getText(
-				"//div[@class='lfr-component lfr-menu-list']/ul/li/a[contains(.,'Configuration')]"));
-		selenium.clickAt("//div[@class='lfr-component lfr-menu-list']/ul/li/a[contains(.,'Configuration')]",
-			RuntimeVariables.replace("Configuration"));
-		selenium.waitForElementPresent(
-			"//iframe[contains(@id,'configurationIframeDialog')]");
-		selenium.selectFrame(
-			"//iframe[contains(@id,'configurationIframeDialog')]");
-		selenium.waitForElementPresent(
-			"//script[contains(@src,'/liferay/navigation_interaction.js')]");
-		selenium.waitForVisible("//input[@id='_86_enableRatingsCheckbox']");
-		assertFalse(selenium.isChecked(
-				"//input[@id='_86_enableRatingsCheckbox']"));
-		selenium.clickAt("//input[@id='_86_enableRatingsCheckbox']",
-			RuntimeVariables.replace("Enable Ratings"));
-		assertTrue(selenium.isChecked(
-				"//input[@id='_86_enableRatingsCheckbox']"));
-		selenium.clickAt("//input[@value='Save']",
-			RuntimeVariables.replace("Save"));
-		selenium.waitForPageToLoad("30000");
-		assertEquals(RuntimeVariables.replace(
-				"You have successfully updated the setup."),
-			selenium.getText("//div[@class='portlet-msg-success']"));
-		assertTrue(selenium.isChecked(
-				"//input[@id='_86_enableRatingsCheckbox']"));
-		selenium.selectFrame("relative=top");
+		int label = 1;
+
+		while (label >= 1) {
+			switch (label) {
+			case 1:
+				selenium.selectWindow("null");
+				selenium.selectFrame("relative=top");
+				selenium.open("/web/guest/home/");
+				selenium.clickAt("link=Asset Publisher Test Page",
+					RuntimeVariables.replace("Asset Publisher Test Page"));
+				selenium.waitForPageToLoad("30000");
+				Thread.sleep(5000);
+				assertEquals(RuntimeVariables.replace("Options"),
+					selenium.getText("//span[@title='Options']/ul/li/strong/a"));
+				selenium.clickAt("//span[@title='Options']/ul/li/strong/a",
+					RuntimeVariables.replace("Options"));
+				selenium.waitForVisible(
+					"//div[@class='lfr-component lfr-menu-list']/ul/li/a[contains(.,'Configuration')]");
+				assertEquals(RuntimeVariables.replace("Configuration"),
+					selenium.getText(
+						"//div[@class='lfr-component lfr-menu-list']/ul/li/a[contains(.,'Configuration')]"));
+				selenium.clickAt("//div[@class='lfr-component lfr-menu-list']/ul/li/a[contains(.,'Configuration')]",
+					RuntimeVariables.replace("Configuration"));
+				selenium.waitForElementPresent(
+					"//iframe[contains(@id,'configurationIframeDialog')]");
+				selenium.selectFrame(
+					"//iframe[contains(@id,'configurationIframeDialog')]");
+				selenium.waitForElementPresent(
+					"//script[contains(@src,'/liferay/navigation_interaction.js')]");
+				selenium.waitForVisible(
+					"//input[@id='_86_enableRatingsCheckbox']");
+
+				boolean ratingsDisabled = selenium.isChecked(
+						"//input[@id='_86_enableRatingsCheckbox']");
+
+				if (ratingsDisabled) {
+					label = 2;
+
+					continue;
+				}
+
+				selenium.clickAt("//input[@id='_86_enableRatingsCheckbox']",
+					RuntimeVariables.replace("Enable Ratings"));
+
+			case 2:
+				assertTrue(selenium.isChecked(
+						"//input[@id='_86_enableRatingsCheckbox']"));
+				selenium.clickAt("//input[@value='Save']",
+					RuntimeVariables.replace("Save"));
+				selenium.waitForPageToLoad("30000");
+				assertEquals(RuntimeVariables.replace(
+						"You have successfully updated the setup."),
+					selenium.getText("//div[@class='portlet-msg-success']"));
+				assertTrue(selenium.isChecked(
+						"//input[@id='_86_enableRatingsCheckbox']"));
+				selenium.selectFrame("relative=top");
+
+			case 100:
+				label = -1;
+			}
+		}
 	}
 }
