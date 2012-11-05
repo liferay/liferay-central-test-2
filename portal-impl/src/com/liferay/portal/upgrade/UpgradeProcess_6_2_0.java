d3c5cb89c42a84a1eb6cb2e8136951dca9b2d71c
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2012-11-05 10:39:09 +0100

    LPS-30204 Upgrade process

--- portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_6_2_0.java
+++ portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_6_2_0.java
@@ -22,6 +22,7 @@ import com.liferay.portal.upgrade.v6_2_0.UpgradeDynamicDataListDisplay;
 import com.liferay.portal.upgrade.v6_2_0.UpgradeDynamicDataMapping;
 import com.liferay.portal.upgrade.v6_2_0.UpgradeSchema;
 import com.liferay.portal.upgrade.v6_2_0.UpgradeUser;
+import com.liferay.portal.upgrade.v6_2_0.UpgradeWikiAttachments;
 
 /**
  * @author Raymond Augé
@@ -42,6 +43,7 @@ public class UpgradeProcess_6_2_0 extends UpgradeProcess {
 		upgrade(UpgradeDynamicDataMapping.class);
 		upgrade(UpgradeDynamicDataListDisplay.class);
 		upgrade(UpgradeUser.class);
+		upgrade(UpgradeWikiAttachments.class);
 	}
 
 }
