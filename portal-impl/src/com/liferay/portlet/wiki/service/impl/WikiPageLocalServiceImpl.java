c16c590abe6e327ab8e43b6d1df528ef5dbf17f0
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2012-11-06 11:35:06 +0100

    LPS-30856 Wiki portlet breaks when FrontPage is moved to Recycle Bin

--- portal-impl/src/com/liferay/portlet/wiki/service/impl/WikiPageLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/wiki/service/impl/WikiPageLocalServiceImpl.java
@@ -1156,6 +1156,15 @@ public class WikiPageLocalServiceImpl extends WikiPageLocalServiceBaseImpl {
 
 		wikiPagePersistence.update(page);
 
+		List<WikiPage> pageVersions = wikiPagePersistence.findByR_N_H(
+			page.getResourcePrimKey(), page.getNodeId(), false);
+
+		for (WikiPage pageVersion : pageVersions) {
+			pageVersion.setTitle(title);
+
+			wikiPagePersistence.update(pageVersion);
+		}
+
 		WikiPageResource pageResource =
 			wikiPageResourcePersistence.fetchByPrimaryKey(
 				page.getResourcePrimKey());
@@ -1190,6 +1199,15 @@ public class WikiPageLocalServiceImpl extends WikiPageLocalServiceBaseImpl {
 		page.setTitle(title);
 
 		wikiPagePersistence.update(page);
+
+		List<WikiPage> pageVersions = wikiPagePersistence.findByR_N_H(
+			page.getResourcePrimKey(), page.getNodeId(), false);
+
+		for (WikiPage pageVersion : pageVersions) {
+			pageVersion.setTitle(title);
+
+			wikiPagePersistence.update(pageVersion);
+		}
 
 		WikiPageResource pageResource =
 			wikiPageResourcePersistence.fetchByPrimaryKey(
