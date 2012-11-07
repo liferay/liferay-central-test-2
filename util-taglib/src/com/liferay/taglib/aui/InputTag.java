4b8b3730b70792b80d60cda0c414698f2ed11d07
Bruno Basto <bruno.basto@liferay.com>
2012-11-07 17:01:18 -0300

    LPS-30681 Allowing input taglibs to be rendered outside a form taglib

--- util-taglib/src/com/liferay/taglib/aui/InputTag.java
+++ util-taglib/src/com/liferay/taglib/aui/InputTag.java
@@ -269,9 +269,12 @@ public class InputTag extends BaseInputTag {
 			(Map<String, List<ValidatorTag>>)request.getAttribute(
 				"aui:form:validatorTagsMap");
 
-		List<ValidatorTag> validatorTags = ListUtil.fromMapValues(_validators);
+		if (validatorTagsMap != null) {
+			List<ValidatorTag> validatorTags = ListUtil.fromMapValues(
+				_validators);
 
-		validatorTagsMap.put(_inputName, validatorTags);
+			validatorTagsMap.put(_inputName, validatorTags);
+		}
 	}
 
 	private static final boolean _CLEAN_UP_SET_ATTRIBUTES = true;
