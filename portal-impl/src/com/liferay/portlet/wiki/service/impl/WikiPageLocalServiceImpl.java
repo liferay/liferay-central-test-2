baace958ed723ad78657da17fcdb1866ed79d639
Hugo Huijser <hugo.huijser@liferay.com>
2012-11-12 09:53:07 -0800

    LPS-27619 Fix accidental changed logic on previous commit

--- portal-impl/src/com/liferay/portlet/wiki/service/impl/WikiPageLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/wiki/service/impl/WikiPageLocalServiceImpl.java
@@ -1761,7 +1761,7 @@ public class WikiPageLocalServiceImpl extends WikiPageLocalServiceBaseImpl {
 		String rootPortletId = serviceContext.getRootPortletId();
 
 		if (Validator.isNull(rootPortletId) ||
-			rootPortletId.equals(PortletKeys.WIKI_DISPLAY)) {
+			!rootPortletId.equals(PortletKeys.WIKI_DISPLAY)) {
 
 			preferences = ServiceContextUtil.getPortletPreferences(
 				serviceContext);
