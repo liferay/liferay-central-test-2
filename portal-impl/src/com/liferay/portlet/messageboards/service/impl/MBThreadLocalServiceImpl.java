juliocamarero/liferay-portal

LPS-28164 Apply QueryDefinition pattern to MBThread

portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBThreadLocalServiceImpl.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portlet.messageboards.service.impl;
 
+import com.liferay.portal.kernel.dao.orm.QueryDefinition;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.increment.BufferedIncrement;
@@ -256,144 +257,232 @@ public class MBThreadLocalServiceImpl extends MBThreadLocalServiceBaseImpl {
 		}
 	}
 
+	/**
+	 * @deprecated {@link #getGroupThreads(long, QueryDefinition)}
+	 */
 	public List<MBThread> getGroupThreads(
 			long groupId, int status, int start, int end)
 		throws SystemException {
 
-		if (status == WorkflowConstants.STATUS_ANY) {
-			return mbThreadPersistence.findByG_NotC(
-				groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID, start,
-				end);
+		QueryDefinition queryDefinition = new QueryDefinition(
+			status, start, end, null);
+
+		return getGroupThreads(groupId, queryDefinition);
+	}
+
+	public List<MBThread> getGroupThreads(
+			long groupId, long userId, boolean subscribed,
+			boolean includeAnonymous, QueryDefinition queryDefinition)
+		throws PortalException, SystemException {
+
+		if (userId <= 0) {
+			return getGroupThreads(groupId, queryDefinition);
 		}
 		else {
-			return mbThreadPersistence.findByG_NotC_S(
-				groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID, status,
-				start, end);
+			if (subscribed) {
+				return mbThreadFinder.findByS_G_U_C_S(
+					groupId, userId, null, queryDefinition);
+			}
+			else {
+				if (includeAnonymous) {
+					return mbThreadFinder.findByG_U_C_S(
+						groupId, userId, null, queryDefinition);
+				}
+				else {
+					return mbThreadFinder.findByG_U_C_A_S(
+						groupId, userId, null, false, queryDefinition);
+				}
+			}
 		}
 	}
 
+	public List<MBThread> getGroupThreads(
+			long groupId, long userId, boolean subscribed,
+			QueryDefinition queryDefinition)
+		throws PortalException, SystemException {
+
+		return getGroupThreads(
+			groupId, userId, subscribed, true, queryDefinition);
+	}
+
+	/**
+	 * @deprecated {@link #getGroupThreads(
+	 * 				long, long, boolean, boolean, QueryDefinition)}
+	 */
 	public List<MBThread> getGroupThreads(
 			long groupId, long userId, int status, boolean subscribed,
 			boolean includeAnonymous, int start, int end)
 		throws PortalException, SystemException {
 
-		if (userId <= 0) {
-			if (status == WorkflowConstants.STATUS_ANY) {
-				return mbThreadPersistence.findByG_NotC(
-					groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID, start,
-					end);
-			}
-			else {
-				return mbThreadPersistence.findByG_NotC_S(
-					groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID, status,
-					start, end);
-			}
-		}
-		else {
-			if (subscribed) {
-				return mbThreadFinder.findByS_G_U_C_S(
-					groupId, userId, null, status, start, end);
-			}
-			else {
-				List<Long> threadIds = null;
+		QueryDefinition queryDefinition = new QueryDefinition(
+			status, start, end, null);
 
-				if (includeAnonymous) {
-					threadIds = mbMessageFinder.findByG_U_C_S(
-						groupId, userId, null, status, start, end);
-				}
-				else {
-					threadIds = mbMessageFinder.findByG_U_C_A_S(
-						groupId, userId, null, false, status, start, end);
-				}
-
-				List<MBThread> threads = new ArrayList<MBThread>(
-					threadIds.size());
-
-				for (long threadId : threadIds) {
-					MBThread thread = mbThreadPersistence.findByPrimaryKey(
-						threadId);
-
-					threads.add(thread);
-				}
-
-				return threads;
-			}
-		}
+		return getGroupThreads(
+			groupId, userId, subscribed, includeAnonymous, queryDefinition);
 	}
 
+	/**
+	 * @deprecated {@link #getGroupThreads(
+	 * 				long, long, boolean, QueryDefinition)}
+	 */
 	public List<MBThread> getGroupThreads(
 			long groupId, long userId, int status, boolean subscribed,
 			int start, int end)
 		throws PortalException, SystemException {
 
-		return getGroupThreads(
-			groupId, userId, status, subscribed, true, start, end);
+		QueryDefinition queryDefinition = new QueryDefinition(
+			status, start, end, null);
+
+		return getGroupThreads(groupId, userId, subscribed, queryDefinition);
 	}
 
+	/**
+	 * @deprecated {@link #getGroupThreads(
+	 * 				long, long, QueryDefinition)}
+	 */
 	public List<MBThread> getGroupThreads(
 			long groupId, long userId, int status, int start, int end)
 		throws PortalException, SystemException {
 
-		return getGroupThreads(groupId, userId, status, false, start, end);
+		QueryDefinition queryDefinition = new QueryDefinition(
+			status, start, end, null);
+
+		return getGroupThreads(groupId, userId, false, queryDefinition);
 	}
 
-	public int getGroupThreadsCount(long groupId, int status)
+	public List<MBThread> getGroupThreads(
+			long groupId, long userId, QueryDefinition queryDefinition)
+		throws PortalException, SystemException {
+
+		return getGroupThreads(groupId, userId, false, queryDefinition);
+	}
+
+	public List<MBThread> getGroupThreads(
+			long groupId, QueryDefinition queryDefinition)
 		throws SystemException {
 
-		if (status == WorkflowConstants.STATUS_ANY) {
-			return mbThreadPersistence.countByG_NotC(
-				groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID);
+		if (queryDefinition.isExcludeStatus()) {
+			return mbThreadPersistence.findByG_NeC_NeS(
+				groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID,
+				queryDefinition.getStatus(), queryDefinition.getStart(),
+				queryDefinition.getEnd());
 		}
 		else {
-			return mbThreadPersistence.countByG_NotC_S(
-				groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID, status);
+			return mbThreadPersistence.findByG_NeC_S(
+				groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID,
+				queryDefinition.getStatus(), queryDefinition.getStart(),
+				queryDefinition.getEnd());
 		}
 	}
 
+	/**
+	 * @deprecated {@link #getGroupThreadsCount(
+	 * 				long, QueryDefinition)}
+	 */
+	public int getGroupThreadsCount(long groupId, int status)
+			throws SystemException {
+
+		QueryDefinition queryDefinition = new QueryDefinition(status);
+
+		return getGroupThreadsCount(groupId, queryDefinition);
+	}
+
+	public int getGroupThreadsCount(
+			long groupId, long userId, boolean subscribed,
+			boolean includeAnonymous, QueryDefinition queryDefinition)
+		throws SystemException {
+
+		if (userId <= 0) {
+			return getGroupThreadsCount(groupId, queryDefinition);
+		}
+		else {
+			if (subscribed) {
+				return mbThreadFinder.countByS_G_U_C_S(
+					groupId, userId, null, queryDefinition);
+			}
+			else {
+				if (includeAnonymous) {
+					return mbThreadFinder.countByG_U_C_S(
+						groupId, userId, null, queryDefinition);
+				}
+				else {
+					return mbThreadFinder.countByG_U_C_A_S(
+						groupId, userId, null, false, queryDefinition);
+				}
+			}
+		}
+	}
+
+	public int getGroupThreadsCount(
+			long groupId, long userId, boolean subscribed,
+			QueryDefinition queryDefinition)
+		throws SystemException {
+
+		return getGroupThreadsCount(
+			groupId, userId, subscribed, true, queryDefinition);
+	}
+
+	/**
+	 * @deprecated {@link #getGroupThreadsCount(
+	 * 				long, long, QueryDefinition)}
+	 */
 	public int getGroupThreadsCount(long groupId, long userId, int status)
 		throws SystemException {
 
-		return getGroupThreadsCount(groupId, userId, status, false);
+		QueryDefinition queryDefinition = new QueryDefinition(status);
+
+		return getGroupThreadsCount(groupId, userId, false, queryDefinition);
 	}
 
+	/**
+	 * @deprecated {@link #getGroupThreadsCount(
+	 * 				long, long, boolean, QueryDefinition)}
+	 */
 	public int getGroupThreadsCount(
 			long groupId, long userId, int status, boolean subscribed)
 		throws SystemException {
 
-		return getGroupThreadsCount(groupId, userId, status, subscribed, true);
+		QueryDefinition queryDefinition = new QueryDefinition(status);
+
+		return getGroupThreadsCount(
+			groupId, userId, subscribed, true, queryDefinition);
 	}
 
+	/**
+	 * @deprecated {@link #getGroupThreadsCount(
+	 * 				long, long, boolean, boolean, QueryDefinition)}
+	 */
 	public int getGroupThreadsCount(
 			long groupId, long userId, int status, boolean subscribed,
 			boolean includeAnonymous)
 		throws SystemException {
 
-		if (userId <= 0) {
-			if (status == WorkflowConstants.STATUS_ANY) {
-				return mbThreadPersistence.countByG_NotC(
-					groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID);
-			}
-			else {
-				return mbThreadPersistence.countByG_NotC_S(
-					groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID,
-					status);
-			}
+		QueryDefinition queryDefinition = new QueryDefinition(status);
+
+		return getGroupThreadsCount(
+			groupId, userId, subscribed, includeAnonymous, queryDefinition);
+	}
+
+	public int getGroupThreadsCount(
+			long groupId, long userId, QueryDefinition queryDefinition)
+		throws SystemException {
+
+		return getGroupThreadsCount(groupId, userId, false, queryDefinition);
+	}
+
+	public int getGroupThreadsCount(
+			long groupId, QueryDefinition queryDefinition)
+		throws SystemException {
+
+		if (queryDefinition.isExcludeStatus()) {
+			return mbThreadPersistence.countByG_NeC_NeS(
+				groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID,
+				queryDefinition.getStatus());
 		}
 		else {
-			if (subscribed) {
-				return mbThreadFinder.countByS_G_U_C_S(
-					groupId, userId, null, status);
-			}
-			else {
-				if (includeAnonymous) {
-					return mbMessageFinder.countByG_U_C_S(
-						groupId, userId, null, status);
-				}
-				else {
-					return mbMessageFinder.countByG_U_C_A_S(
-						groupId, userId, null, false, status);
-				}
-			}
+			return mbThreadPersistence.countByG_NeC_S(
+				groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID,
+				queryDefinition.getStatus());
 		}
 	}
 

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBThreadLocalServiceImpl.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portlet.messageboards.service.impl;
 
+import com.liferay.portal.kernel.dao.orm.QueryDefinition;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.increment.BufferedIncrement;
@@ -262,144 +263,232 @@ public class MBThreadLocalServiceImpl extends MBThreadLocalServiceBaseImpl {
 		}
 	}
 
+	/**
+	 * @deprecated {@link #getGroupThreads(long, QueryDefinition)}
+	 */
 	public List<MBThread> getGroupThreads(
 			long groupId, int status, int start, int end)
 		throws SystemException {
 
-		if (status == WorkflowConstants.STATUS_ANY) {
-			return mbThreadPersistence.findByG_NotC(
-				groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID, start,
-				end);
+		QueryDefinition queryDefinition = new QueryDefinition(
+			status, start, end, null);
+
+		return getGroupThreads(groupId, queryDefinition);
+	}
+
+	public List<MBThread> getGroupThreads(
+			long groupId, long userId, boolean subscribed,
+			boolean includeAnonymous, QueryDefinition queryDefinition)
+		throws PortalException, SystemException {
+
+		if (userId <= 0) {
+			return getGroupThreads(groupId, queryDefinition);
 		}
 		else {
-			return mbThreadPersistence.findByG_NotC_S(
-				groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID, status,
-				start, end);
+			if (subscribed) {
+				return mbThreadFinder.findByS_G_U_C(
+					groupId, userId, null, queryDefinition);
+			}
+			else {
+				if (includeAnonymous) {
+					return mbThreadFinder.findByG_U_C(
+						groupId, userId, null, queryDefinition);
+				}
+				else {
+					return mbThreadFinder.findByG_U_C_A(
+						groupId, userId, null, false, queryDefinition);
+				}
+			}
 		}
 	}
 
+	public List<MBThread> getGroupThreads(
+			long groupId, long userId, boolean subscribed,
+			QueryDefinition queryDefinition)
+		throws PortalException, SystemException {
+
+		return getGroupThreads(
+			groupId, userId, subscribed, true, queryDefinition);
+	}
+
+	/**
+	 * @deprecated {@link #getGroupThreads(
+	 * 				long, long, boolean, boolean, QueryDefinition)}
+	 */
 	public List<MBThread> getGroupThreads(
 			long groupId, long userId, int status, boolean subscribed,
 			boolean includeAnonymous, int start, int end)
 		throws PortalException, SystemException {
 
-		if (userId <= 0) {
-			if (status == WorkflowConstants.STATUS_ANY) {
-				return mbThreadPersistence.findByG_NotC(
-					groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID, start,
-					end);
-			}
-			else {
-				return mbThreadPersistence.findByG_NotC_S(
-					groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID, status,
-					start, end);
-			}
-		}
-		else {
-			if (subscribed) {
-				return mbThreadFinder.findByS_G_U_C_S(
-					groupId, userId, null, status, start, end);
-			}
-			else {
-				List<Long> threadIds = null;
+		QueryDefinition queryDefinition = new QueryDefinition(
+			status, start, end, null);
 
-				if (includeAnonymous) {
-					threadIds = mbMessageFinder.findByG_U_C_S(
-						groupId, userId, null, status, start, end);
-				}
-				else {
-					threadIds = mbMessageFinder.findByG_U_C_A_S(
-						groupId, userId, null, false, status, start, end);
-				}
-
-				List<MBThread> threads = new ArrayList<MBThread>(
-					threadIds.size());
-
-				for (long threadId : threadIds) {
-					MBThread thread = mbThreadPersistence.findByPrimaryKey(
-						threadId);
-
-					threads.add(thread);
-				}
-
-				return threads;
-			}
-		}
+		return getGroupThreads(
+			groupId, userId, subscribed, includeAnonymous, queryDefinition);
 	}
 
+	/**
+	 * @deprecated {@link #getGroupThreads(
+	 * 				long, long, boolean, QueryDefinition)}
+	 */
 	public List<MBThread> getGroupThreads(
 			long groupId, long userId, int status, boolean subscribed,
 			int start, int end)
 		throws PortalException, SystemException {
 
-		return getGroupThreads(
-			groupId, userId, status, subscribed, true, start, end);
+		QueryDefinition queryDefinition = new QueryDefinition(
+			status, start, end, null);
+
+		return getGroupThreads(groupId, userId, subscribed, queryDefinition);
 	}
 
+	/**
+	 * @deprecated {@link #getGroupThreads(
+	 * 				long, long, QueryDefinition)}
+	 */
 	public List<MBThread> getGroupThreads(
 			long groupId, long userId, int status, int start, int end)
 		throws PortalException, SystemException {
 
-		return getGroupThreads(groupId, userId, status, false, start, end);
+		QueryDefinition queryDefinition = new QueryDefinition(
+			status, start, end, null);
+
+		return getGroupThreads(groupId, userId, false, queryDefinition);
 	}
 
-	public int getGroupThreadsCount(long groupId, int status)
+	public List<MBThread> getGroupThreads(
+			long groupId, long userId, QueryDefinition queryDefinition)
+		throws PortalException, SystemException {
+
+		return getGroupThreads(groupId, userId, false, queryDefinition);
+	}
+
+	public List<MBThread> getGroupThreads(
+			long groupId, QueryDefinition queryDefinition)
 		throws SystemException {
 
-		if (status == WorkflowConstants.STATUS_ANY) {
-			return mbThreadPersistence.countByG_NotC(
-				groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID);
+		if (queryDefinition.isExcludeStatus()) {
+			return mbThreadPersistence.findByG_NotC_NotS(
+				groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID,
+				queryDefinition.getStatus(), queryDefinition.getStart(),
+				queryDefinition.getEnd());
 		}
 		else {
-			return mbThreadPersistence.countByG_NotC_S(
-				groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID, status);
+			return mbThreadPersistence.findByG_NotC_S(
+				groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID,
+				queryDefinition.getStatus(), queryDefinition.getStart(),
+				queryDefinition.getEnd());
 		}
 	}
 
+	/**
+	 * @deprecated {@link #getGroupThreadsCount(
+	 * 				long, QueryDefinition)}
+	 */
+	public int getGroupThreadsCount(long groupId, int status)
+			throws SystemException {
+
+		QueryDefinition queryDefinition = new QueryDefinition(status);
+
+		return getGroupThreadsCount(groupId, queryDefinition);
+	}
+
+	public int getGroupThreadsCount(
+			long groupId, long userId, boolean subscribed,
+			boolean includeAnonymous, QueryDefinition queryDefinition)
+		throws SystemException {
+
+		if (userId <= 0) {
+			return getGroupThreadsCount(groupId, queryDefinition);
+		}
+		else {
+			if (subscribed) {
+				return mbThreadFinder.countByS_G_U_C(
+					groupId, userId, null, queryDefinition);
+			}
+			else {
+				if (includeAnonymous) {
+					return mbThreadFinder.countByG_U_C(
+						groupId, userId, null, queryDefinition);
+				}
+				else {
+					return mbThreadFinder.countByG_U_C_A(
+						groupId, userId, null, false, queryDefinition);
+				}
+			}
+		}
+	}
+
+	public int getGroupThreadsCount(
+			long groupId, long userId, boolean subscribed,
+			QueryDefinition queryDefinition)
+		throws SystemException {
+
+		return getGroupThreadsCount(
+			groupId, userId, subscribed, true, queryDefinition);
+	}
+
+	/**
+	 * @deprecated {@link #getGroupThreadsCount(
+	 * 				long, long, QueryDefinition)}
+	 */
 	public int getGroupThreadsCount(long groupId, long userId, int status)
 		throws SystemException {
 
-		return getGroupThreadsCount(groupId, userId, status, false);
+		QueryDefinition queryDefinition = new QueryDefinition(status);
+
+		return getGroupThreadsCount(groupId, userId, false, queryDefinition);
 	}
 
+	/**
+	 * @deprecated {@link #getGroupThreadsCount(
+	 * 				long, long, boolean, QueryDefinition)}
+	 */
 	public int getGroupThreadsCount(
 			long groupId, long userId, int status, boolean subscribed)
 		throws SystemException {
 
-		return getGroupThreadsCount(groupId, userId, status, subscribed, true);
+		QueryDefinition queryDefinition = new QueryDefinition(status);
+
+		return getGroupThreadsCount(
+			groupId, userId, subscribed, true, queryDefinition);
 	}
 
+	/**
+	 * @deprecated {@link #getGroupThreadsCount(
+	 * 				long, long, boolean, boolean, QueryDefinition)}
+	 */
 	public int getGroupThreadsCount(
 			long groupId, long userId, int status, boolean subscribed,
 			boolean includeAnonymous)
 		throws SystemException {
 
-		if (userId <= 0) {
-			if (status == WorkflowConstants.STATUS_ANY) {
-				return mbThreadPersistence.countByG_NotC(
-					groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID);
-			}
-			else {
-				return mbThreadPersistence.countByG_NotC_S(
-					groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID,
-					status);
-			}
+		QueryDefinition queryDefinition = new QueryDefinition(status);
+
+		return getGroupThreadsCount(
+			groupId, userId, subscribed, includeAnonymous, queryDefinition);
+	}
+
+	public int getGroupThreadsCount(
+			long groupId, long userId, QueryDefinition queryDefinition)
+		throws SystemException {
+
+		return getGroupThreadsCount(groupId, userId, false, queryDefinition);
+	}
+
+	public int getGroupThreadsCount(
+			long groupId, QueryDefinition queryDefinition)
+		throws SystemException {
+
+		if (queryDefinition.isExcludeStatus()) {
+			return mbThreadPersistence.countByG_NotC_NotS(
+				groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID,
+				queryDefinition.getStatus());
 		}
 		else {
-			if (subscribed) {
-				return mbThreadFinder.countByS_G_U_C_S(
-					groupId, userId, null, status);
-			}
-			else {
-				if (includeAnonymous) {
-					return mbMessageFinder.countByG_U_C_S(
-						groupId, userId, null, status);
-				}
-				else {
-					return mbMessageFinder.countByG_U_C_A_S(
-						groupId, userId, null, false, status);
-				}
-			}
+			return mbThreadPersistence.countByG_NotC_S(
+				groupId, MBCategoryConstants.DISCUSSION_CATEGORY_ID,
+				queryDefinition.getStatus());
 		}
 	}
 