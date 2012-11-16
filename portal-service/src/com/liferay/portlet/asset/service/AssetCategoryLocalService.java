juliocamarero/liferay-portal

LPS-30301 Autogenerated

portal-service/src/com/liferay/portlet/asset/service/AssetCategoryLocalService.java
@@ -305,6 +305,12 @@ public interface AssetCategoryLocalService extends BaseLocalService,
 			com.liferay.portal.kernel.exception.SystemException;
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public com.liferay.portlet.asset.model.AssetCategory getCategory(
+		java.lang.String categoryUuid, long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public long[] getCategoryIds(java.lang.String className, long classPK)
 		throws com.liferay.portal.kernel.exception.SystemException;
 

portal-service/src/com/liferay/portlet/asset/service/AssetCategoryLocalServiceUtil.java
@@ -349,6 +349,13 @@ public class AssetCategoryLocalServiceUtil {
 		return getService().getCategory(categoryId);
 	}
 
+	public static com.liferay.portlet.asset.model.AssetCategory getCategory(
+		java.lang.String categoryUuid, long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return getService().getCategory(categoryUuid, groupId);
+	}
+
 	public static long[] getCategoryIds(java.lang.String className, long classPK)
 		throws com.liferay.portal.kernel.exception.SystemException {
 		return getService().getCategoryIds(className, classPK);

portal-service/src/com/liferay/portlet/asset/service/AssetCategoryLocalServiceWrapper.java
@@ -345,6 +345,13 @@ public class AssetCategoryLocalServiceWrapper
 		return _assetCategoryLocalService.getCategory(categoryId);
 	}
 
+	public com.liferay.portlet.asset.model.AssetCategory getCategory(
+		java.lang.String categoryUuid, long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return _assetCategoryLocalService.getCategory(categoryUuid, groupId);
+	}
+
 	public long[] getCategoryIds(java.lang.String className, long classPK)
 		throws com.liferay.portal.kernel.exception.SystemException {
 		return _assetCategoryLocalService.getCategoryIds(className, classPK);

liferay/liferay-central-test-2

portal-service/src/com/liferay/portlet/asset/service/AssetCategoryLocalService.java
@@ -305,6 +305,12 @@ public interface AssetCategoryLocalService extends BaseLocalService,
 			com.liferay.portal.kernel.exception.SystemException;
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public com.liferay.portlet.asset.model.AssetCategory getCategory(
+		java.lang.String uuid, long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public long[] getCategoryIds(java.lang.String className, long classPK)
 		throws com.liferay.portal.kernel.exception.SystemException;
 

portal-service/src/com/liferay/portlet/asset/service/AssetCategoryLocalServiceUtil.java
@@ -349,6 +349,13 @@ public class AssetCategoryLocalServiceUtil {
 		return getService().getCategory(categoryId);
 	}
 
+	public static com.liferay.portlet.asset.model.AssetCategory getCategory(
+		java.lang.String uuid, long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return getService().getCategory(uuid, groupId);
+	}
+
 	public static long[] getCategoryIds(java.lang.String className, long classPK)
 		throws com.liferay.portal.kernel.exception.SystemException {
 		return getService().getCategoryIds(className, classPK);

portal-service/src/com/liferay/portlet/asset/service/AssetCategoryLocalServiceWrapper.java
@@ -345,6 +345,13 @@ public class AssetCategoryLocalServiceWrapper
 		return _assetCategoryLocalService.getCategory(categoryId);
 	}
 
+	public com.liferay.portlet.asset.model.AssetCategory getCategory(
+		java.lang.String uuid, long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return _assetCategoryLocalService.getCategory(uuid, groupId);
+	}
+
 	public long[] getCategoryIds(java.lang.String className, long classPK)
 		throws com.liferay.portal.kernel.exception.SystemException {
 		return _assetCategoryLocalService.getCategoryIds(className, classPK);