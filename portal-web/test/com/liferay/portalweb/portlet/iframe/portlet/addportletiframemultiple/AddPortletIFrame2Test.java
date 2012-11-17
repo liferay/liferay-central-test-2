bc5bcdc4a584bd1a385605ca7ffa541fd60c06ad
Brian Chiu <brian.chiu@liferay.com>
2012-11-16 18:08:29 -0800

    Test Fixes - portlet/iframe

--- portal-web/test/com/liferay/portalweb/portlet/iframe/portlet/addportletiframemultiple/AddPortletIFrame2Test.java
+++ portal-web/test/com/liferay/portalweb/portlet/iframe/portlet/addportletiframemultiple/AddPortletIFrame2Test.java
@@ -48,9 +48,9 @@ public class AddPortletIFrame2Test extends BaseTestCase {
 		selenium.waitForVisible("//li[@title='IFrame']/p/a");
 		selenium.clickAt("//li[@title='IFrame']/p/a",
 			RuntimeVariables.replace("Add"));
-		selenium.waitForVisible("//section");
-		assertTrue(selenium.isVisible("//section"));
-		selenium.waitForVisible("//div[2]/div/section");
-		assertTrue(selenium.isVisible("//div[2]/div/section"));
+		selenium.waitForVisible("//div[1]/section");
+		assertTrue(selenium.isVisible("//div[1]/section"));
+		selenium.waitForVisible("//div[2]/section");
+		assertTrue(selenium.isVisible("//div[2]/section"));
 	}
 }
