9599bc445b4cad4b9d1d2a78ed4baf1dbde4f718
Miguel Pastor <miguel.pastor@liferay.com>
2012-11-18 16:58:20 -0500

    LPS-31076 Add a mechanism to conditionally load c.l.p.k.u.ServiceLoader services

--- portal-service/src/com/liferay/portal/kernel/util/ServiceLoader.java
+++ portal-service/src/com/liferay/portal/kernel/util/ServiceLoader.java
@@ -31,18 +31,35 @@ import java.util.List;
 
 /**
  * @author Brian Wing Shun Chan
+ * @author Miguel Pastor
+ * @author Raymond Augé
  */
 public class ServiceLoader {
 
 	public static <S> List<S> load(Class<S> clazz) throws Exception {
+		return load(clazz, _serviceLoaderCondition);
+	}
+
+	public static <S> List<S> load(
+			Class<S> clazz, ServiceLoaderCondition serviceLoaderCondition)
+		throws Exception {
+
 		Thread currentThread = Thread.currentThread();
 
 		ClassLoader classLoader = currentThread.getContextClassLoader();
 
-		return load(classLoader, clazz);
+		return load(classLoader, clazz, serviceLoaderCondition);
 	}
 
 	public static <S> List<S> load(ClassLoader classLoader, Class<S> clazz)
+		throws Exception {
+
+		return load(classLoader, clazz, _serviceLoaderCondition);
+	}
+
+	public static <S> List<S> load(
+			ClassLoader classLoader, Class<S> clazz,
+			ServiceLoaderCondition serviceLoaderCondition)
 		throws Exception {
 
 		Enumeration<URL> enu = classLoader.getResources(
@@ -52,6 +69,10 @@ public class ServiceLoader {
 
 		while (enu.hasMoreElements()) {
 			URL url = enu.nextElement();
+
+			if (!serviceLoaderCondition.shouldLoad(url)) {
+				continue;
+			}
 
 			try {
 				_load(services, classLoader, clazz, url);
@@ -112,5 +133,12 @@ public class ServiceLoader {
 	}
 
 	private static Log _log = LogFactoryUtil.getLog(ServiceLoader.class);
+
+	private static ServiceLoaderCondition _serviceLoaderCondition =
+		new ServiceLoaderCondition() {
+			public boolean shouldLoad(URL url) {
+				return true;
+			}
+		};
 
 }
