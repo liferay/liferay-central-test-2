f80fc23b1ef32eae900b051018998997e47eb38e
Eduardo Garcia <eduardo.garcia@liferay.com>
2012-11-22 11:10:35 +0100

    LPS-29796 Upgrade Process: change portlet preference rssFormat to rssFeedType in Asset Publisher

--- portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_6_2_0.java
+++ portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_6_2_0.java
@@ -16,6 +16,7 @@ package com.liferay.portal.upgrade;
 
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.ReleaseInfo;
+import com.liferay.portal.upgrade.v6_2_0.UpgradeAssetPublisher;
 import com.liferay.portal.upgrade.v6_2_0.UpgradeBlogs;
 import com.liferay.portal.upgrade.v6_2_0.UpgradeCustomizablePortlets;
 import com.liferay.portal.upgrade.v6_2_0.UpgradeDynamicDataListDisplay;
@@ -38,6 +39,7 @@ public class UpgradeProcess_6_2_0 extends UpgradeProcess {
 	@Override
 	protected void doUpgrade() throws Exception {
 		upgrade(UpgradeSchema.class);
+		upgrade(UpgradeAssetPublisher.class);
 		upgrade(UpgradeBlogs.class);
 		upgrade(UpgradeCustomizablePortlets.class);
 		upgrade(UpgradeDynamicDataMapping.class);
