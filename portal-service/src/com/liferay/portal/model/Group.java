juliocamarero/liferay-portal

LPS-31111 Autogenerated

portal-service/src/com/liferay/portal/model/Group.java
@@ -58,6 +58,11 @@ public interface Group extends GroupModel, PersistedModel {
 
 	public com.liferay.portal.model.Group getLiveGroup();
 
+	public com.liferay.portal.kernel.util.UnicodeProperties getLiveParentTypeSettingsProperties();
+
+	public java.lang.String getLiveParentTypeSettingsProperty(
+		java.lang.String key);
+
 	public long getOrganizationId();
 
 	public com.liferay.portal.model.Group getParentGroup()

portal-service/src/com/liferay/portal/model/GroupWrapper.java
@@ -591,6 +591,15 @@ public class GroupWrapper implements Group, ModelWrapper<Group> {
 		return _group.getLiveGroup();
 	}
 
+	public com.liferay.portal.kernel.util.UnicodeProperties getLiveParentTypeSettingsProperties() {
+		return _group.getLiveParentTypeSettingsProperties();
+	}
+
+	public java.lang.String getLiveParentTypeSettingsProperty(
+		java.lang.String key) {
+		return _group.getLiveParentTypeSettingsProperty(key);
+	}
+
 	public long getOrganizationId() {
 		return _group.getOrganizationId();
 	}

liferay/liferay-central-test-2

portal-service/src/com/liferay/portal/model/Group.java
@@ -58,11 +58,16 @@ public interface Group extends GroupModel, PersistedModel {
 
 	public com.liferay.portal.model.Group getLiveGroup();
 
+	public java.lang.String getLiveParentTypeSettingsProperty(
+		java.lang.String key);
+
 	public long getOrganizationId();
 
 	public com.liferay.portal.model.Group getParentGroup()
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException;
+
+	public com.liferay.portal.kernel.util.UnicodeProperties getParentLiveGroupTypeSettingsProperties();
 
 	public java.lang.String getPathFriendlyURL(boolean privateLayout,
 		com.liferay.portal.theme.ThemeDisplay themeDisplay);

portal-service/src/com/liferay/portal/model/GroupWrapper.java
@@ -591,6 +591,11 @@ public class GroupWrapper implements Group, ModelWrapper<Group> {
 		return _group.getLiveGroup();
 	}
 
+	public java.lang.String getLiveParentTypeSettingsProperty(
+		java.lang.String key) {
+		return _group.getLiveParentTypeSettingsProperty(key);
+	}
+
 	public long getOrganizationId() {
 		return _group.getOrganizationId();
 	}
@@ -599,6 +604,10 @@ public class GroupWrapper implements Group, ModelWrapper<Group> {
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException {
 		return _group.getParentGroup();
+	}
+
+	public com.liferay.portal.kernel.util.UnicodeProperties getParentLiveGroupTypeSettingsProperties() {
+		return _group.getParentLiveGroupTypeSettingsProperties();
 	}
 
 	public java.lang.String getPathFriendlyURL(boolean privateLayout,