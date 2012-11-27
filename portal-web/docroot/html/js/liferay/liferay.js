1b99077b2f7507e0fb37a81bdce503a33ea6d2b7
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2012-11-27 14:39:42 -0800

    LPS-31121 - Source formatting

--- portal-web/docroot/html/js/liferay/liferay.js
+++ portal-web/docroot/html/js/liferay/liferay.js
@@ -20,134 +20,177 @@ Liferay = window.Liferay || {};
 		true
 	);
 
-	var ServiceUtil = {
-		invoke: function(payload, ioConfig) {
-			var instance = this;
-
-			if (ioConfig) {
-				if (!owns(ioConfig, 'cache') && REGEX_METHOD_GET.test(ioConfig.method)) {
-					ioConfig.cache = false;
-				}
-
-				if (Liferay.PropsValues.NTLM_AUTH_ENABLED && Liferay.Browser.isIe()) {
-					ioConfig.method = 'GET';
-				}
-			}
-
-			return A.io.request(
-				themeDisplay.getPathContext() + '/api/jsonws/invoke',
-				A.merge(
-					{
-						data: {
-							cmd: A.JSON.stringify(payload),
-							p_auth: Liferay.authToken
-						},
-						dataType: 'json'
-					},
-					ioConfig
-				)
-			);
-		},
-
-		parseInvokeArgs: function(args) {
-			var instance = this;
-
-			var payload = args[0];
-
-			var ioConfig = instance.parseIOConfig(args);
-
-			if (Lang.isString(payload)) {
-				payload = instance.parseStringPayload(args);
-			}
-
-			return [ payload, ioConfig ];
-		},
-
-		parseIOConfig: function(args) {
-			var instance = this;
-
-			var payload = args[0];
-
-			var ioConfig = payload.io;
-
-			payload.io = undefined;
-
-			if (!ioConfig) {
-				var callbacks = [];
-				var callbackIndex = 0;
-
-				while (args[callbackIndex++]) {
-					var callback = args[callbackIndex];
-
-					if (Lang.isFunction(callback)) {
-						callbacks.push(callback);
-					}
-				}
-
-				var callbackSuccess = callbacks[0];
-				var callbackException = callbacks[1];
-
-				ioConfig = {
-					on: {
-						success: function(event) {
-							var responseData = this.get('responseData');
-
-							if (responseData && !owns(responseData, 'exception')) {
-								if (callbackSuccess) {
-									callbackSuccess.call(this, responseData);
-								}
-							}
-							else if (callbackException) {
-								var exception = responseData ? responseData.exception : 'The server returned an empty response';
-
-								callbackException.call(this, exception, responseData);
-							}
-						}
-					}
-				};
-			}
-
-			return ioConfig;
-		},
-
-		parseStringPayload: function(args) {
-			var instance = this;
-
-			var params = {};
-			var payload = {};
-
-			if (!Lang.isFunction(args[1])) {
-				params = args[1];
-			}
-
-			payload[args[0]] = params;
-
-			return payload;
-		}
-	};
+	/**
+	 * OPTIONS
+	 *
+	 * Required
+	 * service {string|object}: Either the service name, or an object with the keys as the service to call, and the value as the service configuration object.
+	 *
+	 * Optional
+	 * data {object|node|string}: The data to send to the service. If the object passed is the ID of a form or a form element, the form fields will be serialized and used as the data.
+	 * successCallback {function}: A function to execute when the server returns a response. It receives a JSON object as it's first parameter.
+	 * exceptionCallback {function}: A function to execute when the response from the server contains a service exception. It receives a the exception message as it's first parameter.
+	 */
 
 	var Service = function() {
 		var instance = this;
 
-		var args = ServiceUtil.parseInvokeArgs(arguments);
+		var args = Service.parseInvokeArgs(arguments);
 
-		ServiceUtil.invoke.apply(ServiceUtil, args);
+		Service.invoke.apply(Service, args);
 	};
 
-	Service.bind = function() {
-		var instance = this;
+	var URL_BASE = themeDisplay.getPathContext() + '/api/jsonws/';
 
-		var args = A.Array(arguments, 0, true);
+	Service.URL_BASE = URL_BASE;
 
-		args.unshift(Liferay.Service, Liferay);
+	Service.URL_INVOKE = URL_BASE + 'invoke';
 
-		return A.bind.apply(A, args);
-	};
+	A.mix(
+		Service,
+		{
+			bind: function() {
+				var instance = this;
+
+				var args = A.Array(arguments, 0, true);
+
+				args.unshift(Liferay.Service, Liferay);
+
+				return A.bind.apply(A, args);
+			},
+
+			invoke: function(payload, ioConfig) {
+				var instance = this;
+
+				return A.io.request(
+					Service.URL_INVOKE,
+					A.merge(
+						{
+							data: {
+								cmd: A.JSON.stringify(payload),
+								p_auth: Liferay.authToken
+							},
+							dataType: 'json'
+						},
+						ioConfig
+					)
+				);
+			},
+
+			parseInvokeArgs: function(args) {
+				var instance = this;
+
+				args = A.Array(args, 0, true);
+
+				var payload = args[0];
+
+				var ioConfig = instance.parseIOConfig(args);
+
+				if (Lang.isString(payload)) {
+					payload = instance.parseStringPayload(args);
+				}
+
+				return [payload, ioConfig];
+			},
+
+			parseIOConfig: function(args) {
+				var instance = this;
+
+				var payload = args[0];
+
+				var ioConfig = payload.io || {};
+
+				delete payload.io;
+
+				var ioData = args[1];
+
+				if (ioData && !Lang.isFunction(ioData) && (ioData.nodeType || ioData._node)) {
+					args.splice(1, 1);
+
+					var formConfig = A.namespace.call(ioConfig, 'form');
+
+					formConfig.id = ioData._node || ioData;
+				}
+
+				var callbackConfig = A.namespace.call(ioConfig, 'on');
+
+				if (!callbackConfig.success) {
+					var callbacks = A.Array.filter(
+						args,
+						function(item, index, collection) {
+							return index > 0 && Lang.isFunction(item);
+						}
+					);
+
+					var successCallback = callbacks[0];
+					var	exceptionCallback = callbacks[1];
+
+					callbackConfig.success = function(event) {
+						var responseData = this.get('responseData');
+
+						if ((!responseData || responseData.exception) && exceptionCallback) {
+							var exception = responseData ? responseData.exception : 'The server returned an empty response';
+
+							exceptionCallback.call(this, exception, responseData);
+						}
+						else {
+							successCallback.call(this, responseData);
+						}
+					};
+				}
+
+				var method = ioConfig.method;
+
+				var argLength = args.length;
+
+				var lastArg = args[argLength - 1];
+
+				if (argLength >= 1 && Lang.isObject(lastArg, true)) {
+					method = lastArg.method || null;
+				}
+
+				if (!owns(ioConfig, 'cache') && REGEX_METHOD_GET.test(method)) {
+					ioConfig.cache = false;
+				}
+
+				if (Liferay.PropsValues.NTLM_AUTH_ENABLED && Liferay.Browser.isIe()) {
+					method = 'GET';
+				}
+
+				if (method) {
+					ioConfig.method = method;
+				}
+
+				return ioConfig;
+			},
+
+			parseStringPayload: function(args) {
+				var instance = this;
+
+				var params = {};
+				var payload = {};
+
+				if (!Lang.isFunction(args[1])) {
+					params = args[1];
+				}
+
+				payload[args[0]] = params;
+
+				return payload;
+			}
+		}
+	);
 
 	A.each(
-		['get', 'del', 'post', 'put', 'update'],
+		['get', 'delete', 'post', 'put', 'update'],
 		function(item, index, collection) {
-			Service[item] = A.rbind(
+			var methodName = item;
+
+			if (item === 'delete') {
+				methodName = 'del';
+			}
+
+			Service[methodName] = A.rbind(
 				'Service',
 				Liferay,
 				{
@@ -191,4 +234,4 @@ Liferay = window.Liferay || {};
 	Liferay.Template = {
 		PORTLET: '<div class="portlet"><div class="portlet-topper"><div class="portlet-title"></div></div><div class="portlet-content"></div><div class="forbidden-action"></div></div>'
 	};
-}(AUI(), Liferay));
+})(AUI(), Liferay);
