dd557b29e66bd81185d77fe9e8e54f9407d7e119
Eduardo Garcia <eduardo.garcia@liferay.com>
2012-12-03 19:24:43 +0100

    LPS-31303 Upgrade Process: change portlet preference rssFormat to rssFeedType in Blogs

--- portal-impl/src/com/liferay/portal/upgrade/v6_2_0/UpgradeBlogs.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_2_0/UpgradeBlogs.java
@@ -14,15 +14,22 @@
 
 package com.liferay.portal.upgrade.v6_2_0;
 
-import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.upgrade.BaseUpgradePortletPreferences;
 import com.liferay.portal.kernel.upgrade.util.UpgradeTable;
 import com.liferay.portal.kernel.upgrade.util.UpgradeTableFactoryUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.upgrade.v6_2_0.util.BlogsEntryTable;
+import com.liferay.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.util.RSSUtil;
+
+import javax.portlet.PortletPreferences;
 
 /**
  * @author Sergio González
+ * @author Eduardo Garcia
  */
-public class UpgradeBlogs extends UpgradeProcess {
+public class UpgradeBlogs extends BaseUpgradePortletPreferences {
 
 	@Override
 	protected void doUpgrade() throws Exception {
@@ -38,6 +45,41 @@ public class UpgradeBlogs extends UpgradeProcess {
 
 			upgradeTable.updateTable();
 		}
+
+		updatePortletPreferences();
+	}
+
+	@Override
+	protected String[] getPortletIds() {
+		return new String[] {"33"};
+	}
+
+	@Override
+	protected String upgradePreferences(
+			long companyId, long ownerId, int ownerType, long plid,
+			String portletId, String xml)
+		throws Exception {
+
+		PortletPreferences portletPreferences =
+			PortletPreferencesFactoryUtil.fromXML(
+				companyId, ownerId, ownerType, plid, portletId, xml);
+
+		String rssFormat = GetterUtil.getString(
+			portletPreferences.getValue("rssFormat", null));
+
+		if (Validator.isNotNull(rssFormat)) {
+			String rssFormatType = RSSUtil.getFormatType(rssFormat);
+			double rssFormatVersion = RSSUtil.getFormatVersion(rssFormat);
+
+			String rssFeedType = RSSUtil.getFeedType(
+				rssFormatType, rssFormatVersion);
+
+			portletPreferences.setValue("rssFeedType", rssFeedType);
+		}
+
+		portletPreferences.reset("rssFormat");
+
+		return PortletPreferencesFactoryUtil.toXML(portletPreferences);
 	}
 
 }
