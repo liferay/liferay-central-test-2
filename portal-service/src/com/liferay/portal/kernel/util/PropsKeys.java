brianchandotcom/liferay-portal

LPS-30413 Source formatting

portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -1615,19 +1615,19 @@ public interface PropsKeys {
 
 	public static final String OPEN_ID_AUTH_ENABLED = "open.id.auth.enabled";
 
-	public static final String OPEN_ID_AX_TYPE_EMAIL = "open.id.ax.type.email";
+	public static final String OPEN_ID_PROVIDERS = "open.id.providers";
 
-	public static final String OPEN_ID_AX_TYPE_FIRST_NAME = "open.id.ax.type.first.name";
+	public static final String OPEN_ID_SCHEMA = "open.id.schema";
 
-	public static final String OPEN_ID_AX_TYPE_FULL_NAME = "open.id.ax.type.full.name";
+	public static final String OPEN_ID_TYPE_EMAIL = "open.id.type.email";
 
-	public static final String OPEN_ID_AX_TYPE_LAST_NAME = "open.id.ax.type.last.name";
+	public static final String OPEN_ID_TYPE_FIRST_NAME = "open.id.type.first.name";
 
-	public static final String OPEN_ID_AX_TYPES = "open.id.ax.types";
+	public static final String OPEN_ID_TYPE_FULL_NAME = "open.id.type.full.name";
 
-	public static final String OPEN_ID_HOST = "open.id.host";
+	public static final String OPEN_ID_TYPE_LAST_NAME = "open.id.type.last.name";
 
-	public static final String OPEN_ID_HOST_TYPES = "open.id.host.types";
+	public static final String OPEN_ID_URL = "open.id.url";
 
 	public static final String OPEN_SSO_AUTH_ENABLED = "open.sso.auth.enabled";
 

liferay/liferay-central-test-2

portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -1615,19 +1615,19 @@ public interface PropsKeys {
 
 	public static final String OPEN_ID_AUTH_ENABLED = "open.id.auth.enabled";
 
+	public static final String OPEN_ID_AX_SCHEMA = "open.id.ax.schema";
+
 	public static final String OPEN_ID_AX_TYPE_EMAIL = "open.id.ax.type.email";
 
-	public static final String OPEN_ID_AX_TYPE_FIRST_NAME = "open.id.ax.type.first.name";
+	public static final String OPEN_ID_AX_TYPE_FIRST_NAME = "open.id.ax.type.firstname";
 
-	public static final String OPEN_ID_AX_TYPE_FULL_NAME = "open.id.ax.type.full.name";
+	public static final String OPEN_ID_AX_TYPE_FULL_NAME = "open.id.ax.type.fullname";
 
-	public static final String OPEN_ID_AX_TYPE_LAST_NAME = "open.id.ax.type.last.name";
+	public static final String OPEN_ID_AX_TYPE_LAST_NAME = "open.id.ax.type.lastname";
 
-	public static final String OPEN_ID_AX_TYPES = "open.id.ax.types";
+	public static final String OPEN_ID_PROVIDERS = "open.id.providers";
 
-	public static final String OPEN_ID_HOST = "open.id.host";
-
-	public static final String OPEN_ID_HOST_TYPES = "open.id.host.types";
+	public static final String OPEN_ID_URL = "open.id.url";
 
 	public static final String OPEN_SSO_AUTH_ENABLED = "open.sso.auth.enabled";
 