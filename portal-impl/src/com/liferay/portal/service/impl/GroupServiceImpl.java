6f40e3512d781d875b1624328c0d713506e03067
Brian Chan <brian.chan@liferay.com>
2012-12-04 21:58:56 -0800

    LPS-31354 Remove Javadoc for Jim to readd

--- portal-impl/src/com/liferay/portal/service/impl/GroupServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupServiceImpl.java
@@ -561,15 +561,6 @@ public class GroupServiceImpl extends GroupServiceBaseImpl {
 		return getUserPlaces(getGuestOrUserId(), classNames, false, max);
 	}
 
-	/**
-	* Returns the count of the guest or current user's group &quot;places&quot;
-	* associated with the group entity class names, including the control panel
-	* group if the user is permitted to view the control panel.
-	*
-	* @return the count of user's group &quot;places&quot;
-	* @throws PortalException if a portal exception occurred
-	* @throws SystemException if a system exception occurred
-	**/
 	public int getUserPlacesCount() throws PortalException, SystemException {
 		List<Group> userPlaces = getUserPlaces(
 			getGuestOrUserId(), null, true, QueryUtil.ALL_POS);
