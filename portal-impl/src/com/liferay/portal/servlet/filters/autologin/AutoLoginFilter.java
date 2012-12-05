ebfcf32bc1e3626386cc37646e8179f82a016969
Mate Thurzo <mate.thurzo@liferay.com>
2012-12-04 16:06:36 -0800

    LPS-31363 Logging refactoring in AutoLoginFilter

--- portal-impl/src/com/liferay/portal/servlet/filters/autologin/AutoLoginFilter.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/autologin/AutoLoginFilter.java
@@ -20,6 +20,7 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.servlet.ProtectedServletRequest;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.InstancePool;
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.User;
@@ -219,23 +220,24 @@ public class AutoLoginFilter extends BasePortalFilter {
 					}
 				}
 				catch (Exception e) {
-					if (_log.isWarnEnabled()) {
-						_log.warn(e, e);
-					}
-
 					String currentURL = PortalUtil.getCurrentURL(request);
+
+					StringBundler sb = new StringBundler(4);
+
+					sb.append("Current URL ");
+					sb.append(currentURL);
+					sb.append(" generates exception: ");
+					sb.append(e.getMessage());
 
 					if (currentURL.endsWith(_PATH_CHAT_LATEST)) {
 						if (_log.isWarnEnabled()) {
-							_log.warn(
-								"Current URL " + currentURL +
-									" generates exception: " + e.getMessage());
+							_log.warn(sb.toString());
 						}
 					}
 					else {
-						_log.error(
-							"Current URL " + currentURL +
-								" generates exception: " + e.getMessage());
+						if (_log.isErrorEnabled()) {
+							_log.error(sb.toString());
+						}
 					}
 				}
 			}
