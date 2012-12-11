dc399c857c6ce38d1a1a4e172a89dfa2e3ca6b61
Brian Chan <brian.chan@liferay.com>
2012-12-11 14:10:57 -0800

    LPS-30580 Source formatting

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -5467,9 +5467,8 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		final Indexer indexer = IndexerRegistryUtil.nullSafeGetIndexer(
 			User.class);
 
-		long companyId = user.getCompanyId();
-
-		Callable<Void> callable = new PortalCallable<Void>(companyId) {
+		Callable<Void> callable = new PortalCallable<Void>(
+			user.getCompanyId()) {
 
 			@Override
 			protected Void doCall() throws Exception {
