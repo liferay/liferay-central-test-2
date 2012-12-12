93e8fcab1e78f47466314b086d8fcb5c757064b5
Juan Fernández <juan.fernandez@liferay.com>
2012-12-12 19:42:44 +0100

    LPS-30967 Upgrade JournalTemplates to DDMTemplates
    Conflicts:
    	portal-impl/src/com/liferay/portal/upgrade/v6_2_0/UpgradeJournal.java

--- portal-impl/src/com/liferay/portal/upgrade/v6_2_0/UpgradeJournal.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_2_0/UpgradeJournal.java
@@ -21,7 +21,9 @@ import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.upgrade.v6_2_0.util.JournalFeedTable;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.PropsValues;
+import com.liferay.portlet.dynamicdatamapping.model.DDMStructure;
 import com.liferay.portlet.dynamicdatamapping.model.DDMStructureConstants;
+import com.liferay.portlet.dynamicdatamapping.model.DDMTemplateConstants;
 import com.liferay.portlet.journal.model.JournalArticle;
 
 import java.sql.Connection;
@@ -47,8 +49,8 @@ public class UpgradeJournal extends RenameUpgradePortletPreferences {
 	protected long addDDMStructure(
 			String uuid_, long groupId, long companyId, long userId,
 			String userName, Date createDate, Date modifiedDate,
-			String parentStructureId, String name, String description,
-			String xsd)
+			String structureKey, String parentStructureId, String name,
+			String description, String xsd)
 		throws Exception {
 
 		Connection con = null;
@@ -80,13 +82,12 @@ public class UpgradeJournal extends RenameUpgradePortletPreferences {
 			long classNameId = PortalUtil.getClassNameId(
 				JournalArticle.class.getName());
 
-			String structureKey = String.valueOf(increment());
 			String storageType = PropsValues.JOURNAL_ARTICLE_STORAGE_TYPE;
 
 			int type_ = DDMStructureConstants.TYPE_DEFAULT;
 
 			ps.setString(1, uuid_);
-			ps.setLong(2, increment());
+			ps.setLong(2, newStructureId);
 			ps.setLong(3, groupId);
 			ps.setLong(4, companyId);
 			ps.setLong(5, userId);
@@ -111,6 +112,69 @@ public class UpgradeJournal extends RenameUpgradePortletPreferences {
 		return newStructureId;
 	}
 
+	protected long addDDMTemplate(
+			String uuid_, long groupId, long companyId, long userId,
+			String userName, Date createDate, Date modifiedDate, long classPK,
+			String templateKey, String name, String description, String type,
+			String mode, String language, String script, boolean cacheable,
+			boolean smallImage, long smallImageId, String smallImageURL)
+		throws Exception {
+
+		Connection con = null;
+		PreparedStatement ps = null;
+
+		long newTemplateId = increment();
+
+		try {
+			con = DataAccess.getUpgradeOptimizedConnection();
+
+			StringBundler sb = new StringBundler(5);
+
+			sb.append("insert into DDMTemplate(uuid_, templateId, groupId, ");
+			sb.append("companyId, userId, userName, createDate, modifiedDate,");
+			sb.append("classNameId, classPK , templateKey, name, description,");
+			sb.append("type_, mode_, language, script, cacheable, smallImage,");
+			sb.append("smallImageId, smallImageURL) values (?, ?, ?, ?, ?, ?,");
+			sb.append("?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+			String sql = sb.toString();
+
+			ps = con.prepareStatement(sql);
+
+			long classNameId = PortalUtil.getClassNameId(
+				DDMStructure.class.getName());
+
+			ps.setString(1, uuid_);
+			ps.setLong(2, newTemplateId);
+			ps.setLong(3, groupId);
+			ps.setLong(4, companyId);
+			ps.setLong(5, userId);
+			ps.setString(6, userName);
+			ps.setDate(7, createDate);
+			ps.setDate(8, modifiedDate);
+			ps.setLong(9, classNameId);
+			ps.setLong(10, classPK);
+			ps.setString(11, templateKey);
+			ps.setString(12, name);
+			ps.setString(13, description);
+			ps.setString(14, type);
+			ps.setString(15, mode);
+			ps.setString(16, language);
+			ps.setString(17, script);
+			ps.setBoolean(18, cacheable);
+			ps.setBoolean(19, smallImage);
+			ps.setLong(20, smallImageId);
+			ps.setString(21, smallImageURL);
+
+			ps.executeUpdate();
+		}
+		finally {
+			DataAccess.cleanUp(con, ps);
+		}
+
+		return newTemplateId;
+	}
+
 	@Override
 	protected void doUpgrade() throws Exception {
 		try {
@@ -126,7 +190,7 @@ public class UpgradeJournal extends RenameUpgradePortletPreferences {
 		}
 
 		updateJournalStructures();
-
+		updateJournalTemplates();
 		updatePortletPreferences();
 	}
 
@@ -158,28 +222,30 @@ public class UpgradeJournal extends RenameUpgradePortletPreferences {
 
 			while (rs.next()) {
 				String uuid_ = rs.getString("uuid_");
-				long id_ = rs.getLong("id_");
 				long groupId = rs.getLong("groupId");
 				long companyId = rs.getLong("companyId");
 				long userId = rs.getLong("userId");
 				String userName = rs.getString("userName");
 				Date createDate = rs.getDate("createDate");
 				Date modifiedDate = rs.getDate("modifiedDate");
+				String structureKey = rs.getString("structureId");
 				String parentStructureId = rs.getString("parentStructureId");
 				String name = rs.getString("name");
 				String description = rs.getString("description");
 				String xsd = rs.getString("xsd");
 
-				if (!_structureIdsMap.containsKey(id_)) {
+				if (!_structureKeyStructureIdMap.containsKey(structureKey)) {
 					newStructureId = addDDMStructure(
 						uuid_, groupId, companyId, userId, userName, createDate,
-						modifiedDate, parentStructureId, name, description,
-						xsd);
+						modifiedDate, structureKey, parentStructureId, name,
+						description, xsd);
 
-					_structureIdsMap.put(id_, newStructureId);
+					_structureKeyStructureIdMap.put(
+						structureKey, newStructureId);
 				}
 				else {
-					newStructureId = _structureIdsMap.get(id_);
+					newStructureId = _structureKeyStructureIdMap.get(
+						structureKey);
 				}
 			}
 		}
@@ -204,13 +270,13 @@ public class UpgradeJournal extends RenameUpgradePortletPreferences {
 
 			while (rs.next()) {
 				String uuid_ = rs.getString("uuid_");
-				long id_ = rs.getLong("id_");
 				long groupId = rs.getLong("groupId");
 				long companyId = rs.getLong("companyId");
 				long userId = rs.getLong("userId");
 				String userName = rs.getString("userName");
 				Date createDate = rs.getDate("createDate");
 				Date modifiedDate = rs.getDate("modifiedDate");
+				String ddmStructureKey = rs.getString("structureId");
 				String parentStructureId = rs.getString("parentStructureId");
 				String name = rs.getString("name");
 				String description = rs.getString("description");
@@ -218,9 +284,64 @@ public class UpgradeJournal extends RenameUpgradePortletPreferences {
 
 				long newStructureId = addDDMStructure(
 					uuid_, groupId, companyId, userId, userName, createDate,
-					modifiedDate, parentStructureId, name, description, xsd);
+					modifiedDate, ddmStructureKey, parentStructureId, name,
+					description, xsd);
 
-				_structureIdsMap.put(id_, newStructureId);
+				_structureKeyStructureIdMap.put(
+					ddmStructureKey, newStructureId);
+			}
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+	}
+
+	protected void updateJournalTemplates() throws Exception {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = DataAccess.getUpgradeOptimizedConnection();
+
+			ps = con.prepareStatement("select * from JournalTemplate");
+
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				String uuid_ = rs.getString("uuid_");
+				long id_ = rs.getLong("id_");
+				long groupId = rs.getLong("groupId");
+				long companyId = rs.getLong("companyId");
+				long userId = rs.getLong("userId");
+				String userName = rs.getString("userName");
+				Date createDate = rs.getDate("createDate");
+				Date modifiedDate = rs.getDate("modifiedDate");
+				String templateKey = rs.getString("templateId");
+				String structureKey = rs.getString("structureId");
+				String name = rs.getString("name");
+				String description = rs.getString("description");
+				String language = rs.getString("langType");
+				String script = rs.getString("xsl");
+				boolean cacheable = rs.getBoolean("cacheable");
+				boolean smallImage = rs.getBoolean("smallImage");
+				long smallImageId = rs.getLong("smallImageId");
+				String smallImageURL = rs.getString("smallImageURL");
+
+				long classPK = 0;
+
+				if (Validator.isNotNull(structureKey)) {
+					classPK = _structureKeyStructureIdMap.get(structureKey);
+				}
+
+				long newTemplateId = addDDMTemplate(
+					uuid_, groupId, companyId, userId, userName, createDate,
+					modifiedDate, classPK, templateKey, name, description,
+					DDMTemplateConstants.TEMPLATE_TYPE_DISPLAY,
+					DDMTemplateConstants.TEMPLATE_MODE_CREATE, language, script,
+					cacheable, smallImage, smallImageId, smallImageURL);
+
+				templateIdsMap.put(id_, newTemplateId);
 			}
 		}
 		finally {
@@ -230,6 +351,8 @@ public class UpgradeJournal extends RenameUpgradePortletPreferences {
 
 	private Map<String, String> _preferenceNamesMap =
 		new HashMap<String, String>();
-	private Map<Long, Long> _structureIdsMap = new HashMap<Long, Long>();
+	private Map<String, Long> _structureKeyStructureIdMap =
+		new HashMap<String, Long>();
+	private Map<Long, Long> templateIdsMap = new HashMap<Long, Long>();
 
 }
