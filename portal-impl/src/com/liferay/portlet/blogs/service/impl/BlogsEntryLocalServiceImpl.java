706d664dcd959e87da68094c1aded606ee11e1ab
Jim Hinkey <james.hinkey@liferay.com>
2012-12-13 15:49:28 -0800

    BlogsEntryLocalServiceImpl - Edit javadoc

--- portal-impl/src/com/liferay/portlet/blogs/service/impl/BlogsEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/blogs/service/impl/BlogsEntryLocalServiceImpl.java
@@ -87,6 +87,8 @@ import net.htmlparser.jericho.Source;
 import net.htmlparser.jericho.StartTag;
 
 /**
+ * The implementation of the blogs entry local service.
+ *
  * @author Brian Wing Shun Chan
  * @author Wilson S. Man
  * @author Raymond Augé
@@ -783,13 +785,15 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 	}
 
 	/**
-	 * Moves the blog entry to the recycle bin.
-	 * Social activity counters for this entry gets disabled.
-	 * 
-	 * @param userId the ID of the user deleting the blog entry
-	 * @param entry the blog entry to be deleted
-	 * @return the deleted blog entry
-	 * @throws PortalExcpetion if a user with the primary key could not be found
+	 * Moves the blogs entry to the recycle bin. Social activity counters for
+	 * this entry get disabled.
+	 *
+	 * @param  userId the primary key of the user moving the blogs entry
+	 * @param  entry the blogs entry to be moved
+	 * @return the moved blogs entry
+	 * @throws PortalException if a user with the primary key could not be found
+	 *         or if the blogs entry owner's social activity counter could not
+	 *         be updated
 	 * @throws SystemException if a system exception occurred
 	 */
 	public BlogsEntry moveEntryToTrash(long userId, BlogsEntry entry)
@@ -835,12 +839,14 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 	}
 
 	/**
-	 * Moves the blog entry with the ID to the recycle bin.
-	 * 
-	 * @param userId the ID of the user deleting the blog entry
-	 * @param entryId the ID of the blog entry to be deleted
-	 * @return the deleted blog entry
-	 * @throws PortalExcpetion if a user with the primary key could not be found
+	 * Moves the blogs entry with the ID to the recycle bin.
+	 *
+	 * @param  userId the primary key of the user moving the blogs entry
+	 * @param  entryId the primary key of the blogs entry to be moved
+	 * @return the moved blogs entry
+	 * @throws PortalException if a user or blogs entry with the primary key
+	 *         could not be found or if the blogs entry owner's social activity
+	 *         counter could not be updated
 	 * @throws SystemException if a system exception occurred
 	 */
 	public BlogsEntry moveEntryToTrash(long userId, long entryId)
@@ -852,12 +858,14 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 	}
 
 	/**
-	 * Restores the blog entry with the ID from the recycle bin.
-	 * Social activity counters for this entry gets activated.
-	 * 
-	 * @param userId the ID of the user restoring the blog entry
-	 * @param entryId the ID of the blog entry to be restored
-	 * @throws PortalExcpetion if a user with the primary key could not be found
+	 * Restores the blogs entry with the ID from the recycle bin. Social
+	 * activity counters for this entry get activated.
+	 *
+	 * @param  userId the primary key of the user restoring the blogs entry
+	 * @param  entryId the primary key of the blogs entry to be restored
+	 * @throws PortalException if a user or blogs entry with the primary key
+	 *         could not be found or if the blogs entry owner's social activity
+	 *         counter could not be updated
 	 * @throws SystemException if a system exception occurred
 	 */
 	public void restoreEntryFromTrash(long userId, long entryId)
