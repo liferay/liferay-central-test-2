51fd1edd794e427d71f061d58740b82c3189045a
Kenneth Chang <kenneth.chang@liferay.com>
2012-12-14 09:28:56 -0800

    LPS-31713 Perform post filter check on wiki search results

--- portal-impl/src/com/liferay/portlet/wiki/util/WikiPageIndexer.java
+++ portal-impl/src/com/liferay/portlet/wiki/util/WikiPageIndexer.java
@@ -36,12 +36,15 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.util.PortletKeys;
 import com.liferay.portlet.wiki.asset.WikiPageAssetRendererFactory;
 import com.liferay.portlet.wiki.model.WikiNode;
 import com.liferay.portlet.wiki.model.WikiPage;
 import com.liferay.portlet.wiki.service.WikiNodeServiceUtil;
 import com.liferay.portlet.wiki.service.WikiPageLocalServiceUtil;
+import com.liferay.portlet.wiki.service.permission.WikiPagePermission;
 import com.liferay.portlet.wiki.service.persistence.WikiNodeActionableDynamicQuery;
 import com.liferay.portlet.wiki.service.persistence.WikiPageActionableDynamicQuery;
 
@@ -64,6 +67,7 @@ public class WikiPageIndexer extends BaseIndexer {
 	public static final String PORTLET_ID = PortletKeys.WIKI;
 
 	public WikiPageIndexer() {
+		setFilterSearch(true);
 		setPermissionAware(true);
 	}
 
@@ -73,6 +77,18 @@ public class WikiPageIndexer extends BaseIndexer {
 
 	public String getPortletId() {
 		return PORTLET_ID;
+	}
+
+	@Override
+	public boolean hasPermission(
+			PermissionChecker permissionChecker, String entryClassName,
+			long entryClassPK, String actionId)
+		throws Exception {
+
+		WikiPage page = WikiPageLocalServiceUtil.getPage(entryClassPK);
+
+		return WikiPagePermission.contains(
+			permissionChecker, page, ActionKeys.VIEW);
 	}
 
 	@Override
