b6cb73d2d889ab29c27536375247df471ee528dd
Jonathan McCann <jonathan.mccann@liferay.com>
2012-12-17 11:07:50 -0800

    LPS-31660 LDAP import fails without email address

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -691,7 +691,7 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		Date now = new Date();
 
 		if (Validator.isNotNull(emailAddress)) {
-			emailAddress = emailAddress.trim().toLowerCase();
+			emailAddress = StringUtil.lowerCase(StringUtil.trim(emailAddress));
 		}
 
 		if (PrefsPropsUtil.getBoolean(
@@ -3406,11 +3406,11 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 			return;
 		}
 
-		emailAddress = emailAddress.trim().toLowerCase();
-
 		if (Validator.isNull(emailAddress)) {
 			throw new UserEmailAddressException();
 		}
+
+		emailAddress = emailAddress.trim().toLowerCase();
 
 		User user = userPersistence.findByC_EA(companyId, emailAddress);
 
@@ -4790,7 +4790,6 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 			user.getCompanyId());
 		String password = oldPassword;
 		screenName = getScreenName(screenName);
-		emailAddress = emailAddress.trim().toLowerCase();
 		openId = openId.trim();
 		String oldFullName = user.getFullName();
 		aimSn = aimSn.trim().toLowerCase();
@@ -4803,6 +4802,10 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		twitterSn = twitterSn.trim().toLowerCase();
 		ymSn = ymSn.trim().toLowerCase();
 		Date now = new Date();
+
+		if (Validator.isNotNull(emailAddress)) {
+			emailAddress = StringUtil.lowerCase(StringUtil.trim(emailAddress));
+		}
 
 		EmailAddressGenerator emailAddressGenerator =
 			EmailAddressGeneratorFactory.getInstance();
