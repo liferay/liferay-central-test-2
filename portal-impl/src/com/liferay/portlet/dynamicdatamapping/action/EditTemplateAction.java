02388faa79d80e76633cac441011270d361d2eea
Juan Fernández <juan.fernandez@liferay.com>
2012-12-18 11:57:36 +0100

    LPS-31608 Template Upload script function does not work

--- portal-impl/src/com/liferay/portlet/dynamicdatamapping/action/EditTemplateAction.java
+++ portal-impl/src/com/liferay/portlet/dynamicdatamapping/action/EditTemplateAction.java
@@ -14,9 +14,12 @@
 
 package com.liferay.portlet.dynamicdatamapping.action;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.upload.UploadPortletRequest;
 import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -41,6 +44,8 @@ import com.liferay.portlet.dynamicdatamapping.service.DDMTemplateServiceUtil;
 
 import java.io.File;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Locale;
 import java.util.Map;
 
@@ -205,6 +210,27 @@ public class EditTemplateAction extends PortletAction {
 		return portletURL.toString();
 	}
 
+	protected String getScript(UploadPortletRequest uploadPortletRequest) {
+		String script = null;
+
+		try {
+			InputStream is = uploadPortletRequest.getFileAsStream("script");
+
+			if (is != null) {
+				script = new String(FileUtil.getBytes(is));
+
+				is.close();
+			}
+		}
+		catch (IOException ioe) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(ioe, ioe);
+			}
+		}
+
+		return script;
+	}
+
 	protected DDMTemplate updateTemplate(ActionRequest actionRequest)
 		throws Exception {
 
@@ -227,7 +253,7 @@ public class EditTemplateAction extends PortletAction {
 			uploadPortletRequest, "language",
 			DDMTemplateConstants.LANG_TYPE_VM);
 
-		String script = ParamUtil.getString(uploadPortletRequest, "script");
+		String script = getScript(uploadPortletRequest);
 		String scriptContent = ParamUtil.getString(
 			uploadPortletRequest, "scriptContent");
 
@@ -286,4 +312,5 @@ public class EditTemplateAction extends PortletAction {
 		return template;
 	}
 
+	private static Log _log = LogFactoryUtil.getLog(EditTemplateAction.class);
 }
