5d381033b437b3c6fbfe7a5ba5cf3427c8214d84
Brian Chiu <brian.chiu@liferay.com>
2013-01-02 15:00:50 -0800

    Test Fixes - controlpanel/calendar

--- portal-web/test/com/liferay/portalweb/portal/controlpanel/calendar/AddNullTitleEventTest.java
+++ portal-web/test/com/liferay/portalweb/portal/controlpanel/calendar/AddNullTitleEventTest.java
@@ -27,6 +27,8 @@ public class AddNullTitleEventTest extends BaseTestCase {
 		selenium.open("/web/guest/home/");
 		selenium.clickAt("//div[@id='dockbar']",
 			RuntimeVariables.replace("Dockbar"));
+		selenium.waitForElementPresent(
+			"//script[contains(@src,'/aui/aui-editable/aui-editable-min.js')]	");
 		assertEquals(RuntimeVariables.replace("Go to"),
 			selenium.getText("//li[@id='_145_mySites']/a/span"));
 		selenium.mouseOver("//li[@id='_145_mySites']/a/span");
