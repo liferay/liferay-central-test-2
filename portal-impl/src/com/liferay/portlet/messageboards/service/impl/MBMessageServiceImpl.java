3db6e819b31c22d8af0980982aead28c06db6e49
Hugo Huijser <hugo.huijser@liferay.com>
2013-01-02 16:12:16 -0800

    LPS-31870 Only replace messagebody parts when message is BBCode

--- portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBMessageServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBMessageServiceImpl.java
@@ -767,15 +767,13 @@ public class MBMessageServiceImpl extends MBMessageServiceBaseImpl {
 			else if (displayStyle.equals(RSSUtil.DISPLAY_STYLE_TITLE)) {
 				value = StringPool.BLANK;
 			}
-			else {
-				if (message.isFormatBBCode()) {
-					value = BBCodeTranslatorUtil.getHTML(message.getBody());
-				}
-				else {
-					value = message.getBody();
-				}
+			else if (message.isFormatBBCode()) {
+				value = BBCodeTranslatorUtil.getHTML(message.getBody());
 
 				value = MBUtil.replaceMessageBodyPaths(themeDisplay, value);
+			}
+			else {
+				value = message.getBody();
 			}
 
 			syndContent.setValue(value);
