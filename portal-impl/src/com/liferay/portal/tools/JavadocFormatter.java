40d78bd6599362ea08f5dd476feff28e659a02b4
Jim Hinkey <james.hinkey@liferay.com>
2013-01-03 14:59:12 -0500

    LPS-32067 fix Javadoc formatter for test classes

--- portal-impl/src/com/liferay/portal/tools/JavadocFormatter.java
+++ portal-impl/src/com/liferay/portal/tools/JavadocFormatter.java
@@ -768,6 +768,22 @@ public class JavadocFormatter {
 		int pos = fileName.indexOf("src/");
 
 		if (pos == -1) {
+			pos = fileName.indexOf("test/integration/");
+
+			if (pos != -1) {
+				pos = fileName.indexOf("integration/", pos);
+			}
+		}
+
+		if (pos == -1) {
+			pos = fileName.indexOf("test/unit/");
+
+			if (pos != -1) {
+				pos = fileName.indexOf("unit/", pos);
+			}
+		}
+
+		if (pos == -1) {
 			pos = fileName.indexOf("test/");
 		}
 
