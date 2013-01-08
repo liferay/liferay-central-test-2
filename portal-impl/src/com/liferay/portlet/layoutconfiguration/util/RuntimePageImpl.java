3a3abe06e7ae2aec8ab1a867c479d29ac653a39b
Brian Chan <brian.chan@liferay.com>
2013-01-08 11:32:39 -0800

    LPS-28363 Source formatting

--- portal-impl/src/com/liferay/portlet/layoutconfiguration/util/RuntimePageImpl.java
+++ portal-impl/src/com/liferay/portlet/layoutconfiguration/util/RuntimePageImpl.java
@@ -234,25 +234,23 @@ public class RuntimePageImpl implements RuntimePage {
 			TemplateResource templateResource, boolean processTemplate)
 		throws Exception {
 
+		ClassLoader pluginClassLoader = null;
+
 		LayoutTemplate layoutTemplate = getLayoutTemplate(
 			templateResource.getTemplateId());
-
-		ServletContext pluginServletContext = null;
 
 		if (layoutTemplate != null) {
 			String pluginServletContextName = GetterUtil.getString(
 				layoutTemplate.getServletContextName());
 
-			pluginServletContext = ServletContextPool.get(
+			ServletContext pluginServletContext = ServletContextPool.get(
 				pluginServletContextName);
-		}
 
-		ClassLoader pluginClassLoader = null;
-
-		if (pluginServletContext != null) {
-			pluginClassLoader =
-				(ClassLoader)pluginServletContext.getAttribute(
-					PluginContextListener.PLUGIN_CLASS_LOADER);
+			if (pluginServletContext != null) {
+				pluginClassLoader =
+					(ClassLoader)pluginServletContext.getAttribute(
+						PluginContextListener.PLUGIN_CLASS_LOADER);
+			}
 		}
 
 		ClassLoader contextClassLoader =
