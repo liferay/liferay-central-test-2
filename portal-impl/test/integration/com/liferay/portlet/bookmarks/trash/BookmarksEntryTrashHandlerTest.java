8d52dc66e9ed368c1895ee4ff1fe336d2e7f5055
ealonso <eudaldo.alonso@liferay.com>
2013-01-08 17:08:41 +0100

    LPS-28159 Test

---
+++ portal-impl/test/integration/com/liferay/portlet/bookmarks/trash/BookmarksEntryTrashHandlerTest.java
@@ -0,0 +1,215 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package integration.com.liferay.portlet.bookmarks.trash;
+
+import com.liferay.portal.kernel.test.ExecutionTestListeners;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.model.BaseModel;
+import com.liferay.portal.model.ClassedModel;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.WorkflowedModel;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceTestUtil;
+import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
+import com.liferay.portal.test.MainServletExecutionTestListener;
+import com.liferay.portal.util.TestPropsValues;
+import com.liferay.portlet.bookmarks.model.BookmarksEntry;
+import com.liferay.portlet.bookmarks.model.BookmarksFolder;
+import com.liferay.portlet.bookmarks.model.BookmarksFolderConstants;
+import com.liferay.portlet.bookmarks.service.BookmarksEntryLocalServiceUtil;
+import com.liferay.portlet.bookmarks.service.BookmarksEntryServiceUtil;
+import com.liferay.portlet.bookmarks.service.BookmarksFolderLocalServiceUtil;
+import com.liferay.portlet.bookmarks.service.BookmarksFolderServiceUtil;
+import com.liferay.portlet.trash.BaseTrashHandlerTestCase;
+
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@ExecutionTestListeners(listeners = {MainServletExecutionTestListener.class})
+@RunWith(LiferayIntegrationJUnitTestRunner.class)
+public class BookmarksEntryTrashHandlerTest extends BaseTrashHandlerTestCase {
+
+	@Override
+	public void testTrashAndDeleteDraft() throws Exception {
+		Assert.assertTrue("This test does not apply", true);
+	}
+
+	@Override
+	public void testTrashAndRestoreDraft() throws Exception {
+		Assert.assertTrue("This test does not apply", true);
+	}
+
+	@Override
+	public void testTrashDuplicate() throws Exception {
+		Assert.assertTrue("This test does not apply", true);
+	}
+
+	@Override
+	public void testTrashVersionAndDelete() throws Exception {
+		Assert.assertTrue("This test does not apply", true);
+	}
+
+	@Override
+	public void testTrashVersionAndRestore() throws Exception {
+		Assert.assertTrue("This test does not apply", true);
+	}
+
+	@Override
+	protected BaseModel<?> addBaseModelWithWorkflow(
+			BaseModel<?> parentBaseModel, boolean approved,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		BookmarksFolder folder = (BookmarksFolder)parentBaseModel;
+
+		String name = getSearchKeywords();
+
+		String url = "http://www.liferay.com";
+
+		String description = "Content: Enterprise. Open Source.";
+
+		serviceContext = (ServiceContext)serviceContext.clone();
+
+		serviceContext.setWorkflowAction(WorkflowConstants.ACTION_SAVE_DRAFT);
+
+		if (approved) {
+			serviceContext.setWorkflowAction(WorkflowConstants.ACTION_PUBLISH);
+		}
+
+		return BookmarksEntryLocalServiceUtil.addEntry(
+			folder.getUserId(), folder.getGroupId(), folder.getFolderId(), name,
+			url, description, serviceContext);
+	}
+
+	@Override
+	protected BaseModel<?> getBaseModel(long primaryKey) throws Exception {
+		return BookmarksEntryLocalServiceUtil.getEntry(primaryKey);
+	}
+
+	@Override
+	protected Class<?> getBaseModelClass() {
+		return BookmarksEntry.class;
+	}
+
+	@Override
+	protected String getBaseModelName(ClassedModel classedModel) {
+		BookmarksEntry entry = (BookmarksEntry)classedModel;
+
+		return entry.getName();
+	}
+
+	@Override
+	protected int getBaseModelsNotInTrashCount(BaseModel<?> parentBaseModel)
+		throws Exception {
+
+		BookmarksFolder folder = (BookmarksFolder)parentBaseModel;
+
+		return BookmarksEntryServiceUtil.getEntriesCount(
+			folder.getGroupId(), folder.getFolderId());
+	}
+
+	@Override
+	protected BaseModel<?> getParentBaseModel(
+			Group group, ServiceContext serviceContext)
+		throws Exception {
+
+		return BookmarksFolderLocalServiceUtil.addFolder(
+			TestPropsValues.getUserId(),
+			BookmarksFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			ServiceTestUtil.randomString(), StringPool.BLANK, serviceContext);
+	}
+
+	@Override
+	protected Class<?> getParentBaseModelClass() {
+		return BookmarksFolder.class;
+	}
+
+	@Override
+	protected String getSearchKeywords() {
+		return "Title";
+	}
+
+	@Override
+	protected String getUniqueTitle(BaseModel<?> baseModel) {
+		return null;
+	}
+
+	@Override
+	protected WorkflowedModel getWorkflowedModel(ClassedModel baseModel)
+		throws Exception {
+
+		BookmarksEntry entry = (BookmarksEntry)baseModel;
+
+		return entry;
+	}
+
+	@Override
+	protected boolean isInTrashContainer(ClassedModel classedModel)
+		throws Exception {
+
+		BookmarksEntry entry = (BookmarksEntry)baseModel;
+
+		return entry.isInTrashContainer();
+	}
+
+	@Override
+	protected BaseModel<?> moveBaseModelFromTrash(
+			ClassedModel classedModel, Group group,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		BaseModel<?> parentBaseModel = getParentBaseModel(
+			group, serviceContext);
+
+		BookmarksEntryServiceUtil.moveEntryFromTrash(
+				(Long) classedModel.getPrimaryKeyObj(),
+				(Long) parentBaseModel.getPrimaryKeyObj());
+
+		return parentBaseModel;
+	}
+
+	@Override
+	protected void moveBaseModelToTrash(long primaryKey) throws Exception {
+		BookmarksEntryServiceUtil.moveEntryToTrash(primaryKey);
+	}
+
+	@Override
+	protected void moveParentBaseModelToTrash(long primaryKey)
+			throws Exception {
+
+		BookmarksFolderServiceUtil.moveFolderToTrash(primaryKey);
+	}
+
+	@Override
+	protected BaseModel<?> updateBaseModel(
+			long primaryKey, ServiceContext serviceContext)
+		throws Exception {
+
+		BookmarksEntry entry = BookmarksEntryLocalServiceUtil.getEntry(
+				primaryKey);
+
+		String description = "Content: Enterprise. Open Source. For Life.";
+
+		return BookmarksEntryLocalServiceUtil.updateEntry(
+			entry.getUserId(), primaryKey, entry.getGroupId(),
+			entry.getFolderId(), entry.getName(), entry.getUrl(), description,
+			serviceContext);
+	}
+
+}
