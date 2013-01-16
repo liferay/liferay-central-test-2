b42d9c74c4fcfd3a7d5eb13d5aa514676be568e7
Brian Chiu <brian.chiu@liferay.com>
2013-01-15 17:11:47 -0800

    Test Fixes - plugins/weather

--- portal-web/test/com/liferay/portalweb/plugins/weather/EditPreferencesTest.java
+++ portal-web/test/com/liferay/portalweb/plugins/weather/EditPreferencesTest.java
@@ -25,29 +25,48 @@ public class EditPreferencesTest extends BaseTestCase {
 		selenium.selectWindow("null");
 		selenium.selectFrame("relative=top");
 		selenium.open("/web/guest/home/");
-		selenium.waitForVisible("link=Weather Test Page");
 		selenium.click(RuntimeVariables.replace("link=Weather Test Page"));
 		selenium.waitForPageToLoad("30000");
-		Thread.sleep(5000);
+		Thread.sleep(1000);
 		assertEquals(RuntimeVariables.replace("Options"),
-			selenium.getText("//strong/a"));
-		selenium.clickAt("//strong/a", RuntimeVariables.replace("Options"));
+			selenium.getText("//span[@title='Options']/ul/li/strong/a"));
+		selenium.clickAt("//span[@title='Options']/ul/li/strong/a",
+			RuntimeVariables.replace("Options"));
 		selenium.waitForVisible(
-			"//div[@class='lfr-component lfr-menu-list']/ul/li[3]/a");
-		selenium.clickAt("//div[@class='lfr-component lfr-menu-list']/ul/li[3]/a",
+			"//div[@class='lfr-component lfr-menu-list']/ul/li/a[contains(.,'Configuration')]");
+		assertEquals(RuntimeVariables.replace("Configuration"),
+			selenium.getText(
+				"//div[@class='lfr-component lfr-menu-list']/ul/li/a[contains(.,'Configuration')]"));
+		selenium.clickAt("//div[@class='lfr-component lfr-menu-list']/ul/li/a[contains(.,'Configuration')]",
 			RuntimeVariables.replace("Configuration"));
-		selenium.waitForPageToLoad("30000");
-		selenium.type("//textarea[@name='_1_WAR_weatherportlet_zips']",
+		selenium.waitForVisible(
+			"//iframe[contains(@id,'weatherportlet_configurationIframeDialog')]");
+		selenium.selectFrame(
+			"//iframe[contains(@id,'weatherportlet_configurationIframeDialog')]");
+		selenium.waitForElementPresent(
+			"//script[contains(@src,'/liferay/navigation_interaction.js')]");
+		selenium.waitForVisible("//input[@id='_86_apiKey']");
+		selenium.type("//input[@id='_86_apiKey']",
+			RuntimeVariables.replace("9d72d0f809003954131601"));
+		selenium.type("//textarea[@id='_86_zips']",
 			RuntimeVariables.replace("Diamond Bar, CA"));
 		selenium.clickAt("//input[@value='Save']",
 			RuntimeVariables.replace("Save"));
 		selenium.waitForPageToLoad("30000");
 		assertEquals(RuntimeVariables.replace(
-				"You have successfully updated your preferences."),
+				"You have successfully updated the setup."),
 			selenium.getText("//div[@class='portlet-msg-success']"));
-		selenium.clickAt("link=Return to Full Page",
-			RuntimeVariables.replace("Return to Full Page"));
+		selenium.selectFrame("relative=top");
+		assertEquals(RuntimeVariables.replace("Diamond Bar, CA"),
+			selenium.getText("//td[1]/a"));
+		assertTrue(selenium.isVisible("//td[2]/span"));
+		assertTrue(selenium.isVisible("//td[3]/img"));
+		selenium.open("/web/guest/home/");
+		selenium.click(RuntimeVariables.replace("link=Weather Test Page"));
 		selenium.waitForPageToLoad("30000");
-		assertTrue(selenium.isElementPresent("link=Diamond Bar, CA"));
+		assertEquals(RuntimeVariables.replace("Diamond Bar, CA"),
+			selenium.getText("//td[1]/a"));
+		assertTrue(selenium.isVisible("//td[2]/span"));
+		assertTrue(selenium.isVisible("//td[3]/img"));
 	}
 }
