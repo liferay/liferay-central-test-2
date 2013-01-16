da66559966c7b9b829a09b6fe950afa32455377d
Michael Han <michael.han@liferay.com>
2013-01-16 17:29:35 -0600

    LPS-32367 adding ability to trigger dictionary reindex from control panel

--- portal-impl/src/com/liferay/portlet/admin/action/EditServerAction.java
+++ portal-impl/src/com/liferay/portlet/admin/action/EditServerAction.java
@@ -187,6 +187,9 @@ public class EditServerAction extends PortletAction {
 		else if (cmd.equals("reindex")) {
 			reindex(actionRequest);
 		}
+		else if (cmd.equals("reindexDictionaries")) {
+			reindexDictionaries(actionRequest);
+		}
 		else if (cmd.equals("runScript")) {
 			runScript(portletConfig, actionRequest, actionResponse);
 		}
@@ -448,6 +451,16 @@ public class EditServerAction extends PortletAction {
 
 			submitClusterIndexLoadingSyncJob(
 				searchWriterDestinations, companyIds);
+		}
+	}
+
+	protected void reindexDictionaries(ActionRequest actionRequest)
+		throws Exception {
+
+		long[] companyIds = PortalInstances.getCompanyIds();
+
+		for (long companyId : companyIds) {
+			SearchEngineUtil.indexDictionaries(companyId);
 		}
 	}
 
