dc15cd1ff8e734182634c2bf21b238586b7f4ee2
ealonso <eudaldo.alonso@liferay.com>
2013-01-21 16:36:26 +0100

    LPS-32414 Add Integration test for search in attachments and comments

---
+++ portal-impl/test/integration/com/liferay/portal/search/lucene/BaseSearchTestCase.java
@@ -0,0 +1,219 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.lucene;
+
+import com.liferay.portal.kernel.dao.orm.FinderCacheUtil;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.kernel.workflow.WorkflowThreadLocal;
+import com.liferay.portal.model.BaseModel;
+import com.liferay.portal.model.ClassedModel;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceTestUtil;
+import com.liferay.portal.util.TestPropsValues;
+import com.liferay.portlet.messageboards.model.MBMessage;
+import com.liferay.portlet.messageboards.service.MBMessageLocalServiceUtil;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public abstract class BaseSearchTestCase {
+
+	@Before
+	public void setUp() throws Exception {
+		FinderCacheUtil.clearCache();
+
+		group = ServiceTestUtil.addGroup();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		GroupLocalServiceUtil.deleteGroup(group);
+	}
+
+	@Test
+	@Transactional
+	public void testSearchAttachments() throws Exception {
+		searchAttachments();
+	}
+
+	@Test
+	@Transactional
+	public void testSearchComments() throws Exception {
+		searchComments();
+	}
+
+	protected void addAttachment(ClassedModel classedModel) throws Exception {
+	}
+
+	protected BaseModel<?> addBaseModel(
+			BaseModel<?> parentBaseModel, boolean approved,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		boolean workflowEnabled = WorkflowThreadLocal.isEnabled();
+
+		try {
+			WorkflowThreadLocal.setEnabled(true);
+
+			BaseModel<?> baseModel = addBaseModelWithWorkflow(
+				parentBaseModel, approved, serviceContext);
+
+			return baseModel;
+		}
+		finally {
+			WorkflowThreadLocal.setEnabled(workflowEnabled);
+		}
+	}
+
+	protected abstract BaseModel<?> addBaseModelWithWorkflow(
+			BaseModel<?> parentBaseModel, boolean approved,
+			ServiceContext serviceContext)
+		throws Exception;
+
+	protected void addComment(
+			ClassedModel classedModel, String body,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		User user = TestPropsValues.getUser();
+
+		List<MBMessage> messages = MBMessageLocalServiceUtil.getMessages(
+			getBaseModelClassName(), getBaseModelClassPK(classedModel),
+			WorkflowConstants.STATUS_ANY);
+
+		MBMessage message = messages.get(0);
+
+		MBMessageLocalServiceUtil.addDiscussionMessage(
+			user.getUserId(), user.getFullName(),
+			serviceContext.getScopeGroupId(), getBaseModelClassName(),
+			getBaseModelClassPK(classedModel), message.getThreadId(),
+			message.getMessageId(), message.getSubject(), body, serviceContext);
+	}
+
+	protected abstract Class<?> getBaseModelClass();
+
+	protected String getBaseModelClassName() {
+		Class<?> clazz = getBaseModelClass();
+
+		return clazz.getName();
+	}
+
+	protected Long getBaseModelClassPK(ClassedModel classedModel) {
+		return (Long)classedModel.getPrimaryKeyObj();
+	}
+
+	protected BaseModel<?> getParentBaseModel(
+			Group group, ServiceContext serviceContext)
+		throws Exception {
+
+		return group;
+	}
+
+	protected abstract String getSearchKeywords();
+
+	protected void searchAttachments() throws Exception {
+		ServiceContext serviceContext = ServiceTestUtil.getServiceContext();
+
+		serviceContext.setScopeGroupId(group.getGroupId());
+
+		SearchContext searchContext = ServiceTestUtil.getSearchContext();
+
+		searchContext.setIncludeAttachments(true);
+
+		BaseModel<?> parentBaseModel = getParentBaseModel(
+			group, serviceContext);
+
+		int initialBaseModelsSearchCount = searchBaseModelsCount(
+			getBaseModelClass(), group.getGroupId(), searchContext);
+
+		baseModel = addBaseModel(parentBaseModel, true, serviceContext);
+
+		Assert.assertEquals(
+			initialBaseModelsSearchCount + 1,
+			searchBaseModelsCount(
+				getBaseModelClass(), group.getGroupId(), searchContext));
+
+		addAttachment(baseModel);
+
+		Assert.assertEquals(
+			initialBaseModelsSearchCount + 2,
+			searchBaseModelsCount(
+				getBaseModelClass(), group.getGroupId(), searchContext));
+	}
+
+	protected int searchBaseModelsCount(
+			Class<?> clazz, long groupId, SearchContext searchContext)
+		throws Exception {
+
+		Thread.sleep(2000 * TestPropsValues.JUNIT_DELAY_FACTOR);
+
+		Indexer indexer = IndexerRegistryUtil.getIndexer(clazz);
+
+		searchContext.setGroupIds(new long[]{groupId});
+
+		Hits results = indexer.search(searchContext);
+
+		return results.getLength();
+	}
+
+	protected void searchComments() throws Exception {
+		ServiceContext serviceContext = ServiceTestUtil.getServiceContext();
+
+		serviceContext.setScopeGroupId(group.getGroupId());
+
+		SearchContext searchContext = ServiceTestUtil.getSearchContext();
+
+		searchContext.setIncludeDiscussions(true);
+
+		BaseModel<?> parentBaseModel = getParentBaseModel(
+			group, serviceContext);
+
+		int initialBaseModelsSearchCount = searchBaseModelsCount(
+			getBaseModelClass(), group.getGroupId(), searchContext);
+
+		baseModel = addBaseModel(parentBaseModel, true, serviceContext);
+
+		Assert.assertEquals(
+			initialBaseModelsSearchCount + 1,
+			searchBaseModelsCount(
+				getBaseModelClass(), group.getGroupId(), searchContext));
+
+		addComment(baseModel, getSearchKeywords(), serviceContext);
+
+		Assert.assertEquals(
+			initialBaseModelsSearchCount + 2,
+			searchBaseModelsCount(
+				getBaseModelClass(), group.getGroupId(), searchContext));
+	}
+
+	protected BaseModel<?> baseModel;
+	protected Group group;
+
+}
---
+++ portal-impl/test/integration/com/liferay/portlet/wiki/search/WikiPageSearchTest.java
@@ -0,0 +1,129 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.wiki.search;
+
+import com.liferay.portal.kernel.test.ExecutionTestListeners;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.model.BaseModel;
+import com.liferay.portal.model.ClassedModel;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.search.lucene.BaseSearchTestCase;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceTestUtil;
+import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
+import com.liferay.portal.test.MainServletExecutionTestListener;
+import com.liferay.portal.util.TestPropsValues;
+import com.liferay.portlet.wiki.asset.WikiPageAssetRenderer;
+import com.liferay.portlet.wiki.model.WikiPage;
+import com.liferay.portlet.wiki.service.WikiNodeLocalServiceUtil;
+import com.liferay.portlet.wiki.service.WikiPageLocalServiceUtil;
+
+import java.io.File;
+
+import org.junit.runner.RunWith;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@ExecutionTestListeners(listeners = {MainServletExecutionTestListener.class})
+@RunWith(LiferayIntegrationJUnitTestRunner.class)
+public class WikiPageSearchTest extends BaseSearchTestCase {
+
+	@Override
+	protected void addAttachment(ClassedModel classedModel) throws Exception {
+		WikiPage page = (WikiPage)classedModel;
+
+		Class<?> clazz = getClass();
+
+		byte[] fileBytes = FileUtil.getBytes(
+			clazz.getResourceAsStream("dependencies/OSX_Test.docx"));
+
+		File file = null;
+
+		if ((fileBytes != null) && (fileBytes.length > 0)) {
+			file = FileUtil.createTempFile(fileBytes);
+		}
+
+		String fileName = ServiceTestUtil.randomString() + ".txt";
+
+		WikiPageLocalServiceUtil.addPageAttachment(
+			TestPropsValues.getUserId(), page.getNodeId(), page.getTitle(),
+			fileName, file);
+	}
+
+	@Override
+	protected BaseModel<?> addBaseModelWithWorkflow(
+			BaseModel<?> parentBaseModel, boolean approved,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		serviceContext = (ServiceContext)serviceContext.clone();
+
+		serviceContext.setWorkflowAction(WorkflowConstants.ACTION_SAVE_DRAFT);
+
+		String title = getSearchKeywords();
+
+		title += ServiceTestUtil.randomString(
+			_PAGE_TITLE_MAX_LENGTH - title.length());
+
+		WikiPage page = WikiPageLocalServiceUtil.addPage(
+			TestPropsValues.getUserId(),
+			(Long)parentBaseModel.getPrimaryKeyObj(), title,
+			ServiceTestUtil.randomString(), ServiceTestUtil.randomString(),
+			true, serviceContext);
+
+		page = WikiPageLocalServiceUtil.updateStatus(
+			TestPropsValues.getUserId(), page.getResourcePrimKey(),
+			WorkflowConstants.STATUS_APPROVED, serviceContext);
+
+		return page;
+	}
+
+	@Override
+	protected Class<?> getBaseModelClass() {
+		return WikiPage.class;
+	}
+
+	@Override
+	protected Long getBaseModelClassPK(ClassedModel classedModel) {
+		return WikiPageAssetRenderer.getClassPK((WikiPage)classedModel);
+	}
+
+	@Override
+	protected BaseModel<?> getParentBaseModel(
+			Group group, ServiceContext serviceContext)
+		throws Exception {
+
+		serviceContext = (ServiceContext)serviceContext.clone();
+
+		serviceContext.setWorkflowAction(WorkflowConstants.STATUS_APPROVED);
+
+		return WikiNodeLocalServiceUtil.addNode(
+			TestPropsValues.getUserId(),
+			ServiceTestUtil.randomString(_NODE_NAME_MAX_LENGTH),
+			ServiceTestUtil.randomString(), serviceContext);
+	}
+
+	@Override
+	protected String getSearchKeywords() {
+		return "Title";
+	}
+
+	private static final int _NODE_NAME_MAX_LENGTH = 75;
+
+	private static final int _PAGE_TITLE_MAX_LENGTH = 255;
+
+}
