dec06daf92bc3356c63f7da1b4b9450a8ea547ba
ealonso <eudaldo.alonso@liferay.com>
2013-01-22 13:42:45 +0100

    LPS-32143 Create new object to search results

---
+++ portal-service/src/com/liferay/portal/kernel/search/SearchResult.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.search;
+
+import com.liferay.portlet.messageboards.model.MBMessage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class SearchResult {
+
+	public SearchResult(String className, long classPK) {
+		_className = className;
+		_classPK = classPK;
+
+		_messages = new ArrayList<MBMessage>();
+	}
+
+	public SearchResult(String className, long classPK, MBMessage message) {
+		_className = className;
+		_classPK = classPK;
+
+		_messages = new ArrayList<MBMessage>();
+
+		_messages.add(message);
+	}
+
+	public void addMessage(MBMessage message) {
+		_messages.add(message);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (!(obj instanceof SearchResult)) {
+			return false;
+		}
+
+		SearchResult searchResult = (SearchResult)obj;
+
+		return
+			((_classPK == searchResult.getClassPK()) &&
+				_className.equals(searchResult.getClassName()));
+	}
+
+	public String getClassName() {
+		return _className;
+	}
+
+	public long getClassPK() {
+		return _classPK;
+	}
+
+	public List<MBMessage> getMessages() {
+		return _messages;
+	}
+
+	public void setClassName(String className) {
+		_className = className;
+	}
+
+	public void setClassPK(long classPK) {
+		_classPK = classPK;
+	}
+
+	public void setMessages(List<MBMessage> messages) {
+		_messages = messages;
+	}
+
+	private String _className;
+	private long _classPK;
+	private List<MBMessage> _messages;
+
+}
