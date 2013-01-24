dcabfd36c331f5ff1c68d84b45e5e8cdf6ed0165
Iliyan Peychev <iliyan.peychev@liferay.com>
2013-01-24 10:37:35 +0100

    LPS-30352 - Fix passing autosave param on saving data explicitly

--- portal-web/docroot/html/js/editor/ckeditor_diffs/main.js
+++ portal-web/docroot/html/js/editor/ckeditor_diffs/main.js
@@ -46,7 +46,7 @@ AUI.add(
 							editor.on('blur', instance._onEditorBlur, instance),
 							editor.on('focus', instance._onEditorFocus, instance),
 							editor.on('restoreContent', instance._restoreContent, instance),
-							editor.on('saveContent', instance.save, instance, false)
+							editor.on('saveContent', instance._saveContent, instance)
 						];
 
 						instance.after('destroy', instance._destructor, instance);
@@ -144,6 +144,12 @@ AUI.add(
 						if (instance.isContentDirty()) {
 							instance.save();
 						}
+					},
+
+					_saveContent: function() {
+						var instance = this;
+
+						instance.save();
 					},
 
 					_updateNoticePosition: function() {
