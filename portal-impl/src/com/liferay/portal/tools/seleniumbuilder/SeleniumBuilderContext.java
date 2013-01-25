c26d712818e40e02db40eb11c2ba3aba7eb0d509
Michael Hashimoto <michael.hashimoto@liferay.com>
2013-01-24 19:05:36 -0800

    LPS-32400

--- portal-impl/src/com/liferay/portal/tools/seleniumbuilder/SeleniumBuilderContext.java
+++ portal-impl/src/com/liferay/portal/tools/seleniumbuilder/SeleniumBuilderContext.java
@@ -30,7 +30,14 @@ public class SeleniumBuilderContext {
 	public SeleniumBuilderContext(String basedir) throws Exception {
 		_basedir = basedir;
 
-		_fileNameSet = _initFileNameSet(basedir);
+		_fileNameSet = _initFileNameSet();
+
+		_fileNameSetAction = _initFileNameSetAction();
+		_fileNameSetFunction = _initFileNameSetFunction();
+		_fileNameSetMacro = _initFileNameSetMacro();
+		_fileNameSetPath = _initFileNameSetPath();
+		_fileNameSetTestCase = _initFileNameSetTestCase();
+		_fileNameSetTestSuite = _initFileNameSetTestSuite();
 	}
 
 	public String getBasedir() {
@@ -41,12 +48,36 @@ public class SeleniumBuilderContext {
 		return _fileNameSet;
 	}
 
-	private Set<String> _initFileNameSet(String basedir) throws Exception {
+	public Set<String> getFileNameSetAction() {
+		return _fileNameSetAction;
+	}
+
+	public Set<String> getFileNameSetFunction() {
+		return _fileNameSetFunction;
+	}
+
+	public Set<String> getFileNameSetMacro() {
+		return _fileNameSetMacro;
+	}
+
+	public Set<String> getFileNameSetPath() {
+		return _fileNameSetPath;
+	}
+
+	public Set<String> getFileNameSetTestCase() {
+		return _fileNameSetTestCase;
+	}
+
+	public Set<String> getFileNameSetTestSuite() {
+		return _fileNameSetTestSuite;
+	}
+
+	private Set<String> _initFileNameSet() throws Exception {
 		Set<String> treeSet = new TreeSet<String>();
 
 		DirectoryScanner directoryScanner = new DirectoryScanner();
 
-		directoryScanner.setBasedir(basedir);
+		directoryScanner.setBasedir(_basedir);
 		directoryScanner.setIncludes(
 			new String[] {
 				"**\\portalweb\\**\\*.action", "**\\portalweb\\**\\*.function",
@@ -66,6 +97,42 @@ public class SeleniumBuilderContext {
 		return treeSet;
 	}
 
+	private Set<String> _initFileNameSetAction() {
+		return _initFileNameSetType(".action");
+	}
+
+	private Set<String> _initFileNameSetFunction() {
+		return _initFileNameSetType(".function");
+	}
+
+	private Set<String> _initFileNameSetMacro() {
+		return _initFileNameSetType(".macro");
+	}
+
+	private Set<String> _initFileNameSetPath() {
+		return _initFileNameSetType(".path");
+	}
+
+	private Set<String> _initFileNameSetTestCase() {
+		return _initFileNameSetType(".testcase");
+	}
+
+	private Set<String> _initFileNameSetTestSuite() {
+		return _initFileNameSetType(".testsuite");
+	}
+
+	private Set<String> _initFileNameSetType(String suffix) {
+		Set<String> treeSet = new TreeSet<String>();
+
+		for (String fileName : _fileNameSet) {
+			if (fileName.endsWith(suffix)) {
+				treeSet.add(fileName);
+			}
+		}
+
+		return treeSet;
+	}
+
 	private String _normalizeFileName(String fileName) {
 		return StringUtil.replace(
 			fileName, StringPool.BACK_SLASH, StringPool.SLASH);
@@ -73,5 +140,11 @@ public class SeleniumBuilderContext {
 
 	private String _basedir;
 	private Set<String> _fileNameSet;
+	private Set<String> _fileNameSetAction;
+	private Set<String> _fileNameSetFunction;
+	private Set<String> _fileNameSetMacro;
+	private Set<String> _fileNameSetPath;
+	private Set<String> _fileNameSetTestCase;
+	private Set<String> _fileNameSetTestSuite;
 
 }
