924b6e054789097747a4ff81bdd88536895dfdef
Ken Duenwald <ken.duenwald@liferay.com>
2013-01-28 11:08:53 -0800

    SOS-1409

--- portal-web/test/functional/com/liferay/portalweb/socialofficesite/home/announcement/AnnouncementTestPlan.java
+++ portal-web/test/functional/com/liferay/portalweb/socialofficesite/home/announcement/AnnouncementTestPlan.java
@@ -15,6 +15,19 @@
 package com.liferay.portalweb.socialofficesite.home.announcement;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
+import com.liferay.portalweb.socialofficesite.home.announcement.addannouncementsentrygeneralsite.AddAnnouncementsEntryGeneralSiteTests;
+import com.liferay.portalweb.socialofficesite.home.announcement.deleteannouncementsentrygeneralsite.DeleteAnnouncementsEntryGeneralSiteTests;
+import com.liferay.portalweb.socialofficesite.home.announcement.editannouncementsentrygeneralsite.EditAnnouncementsEntryGeneralSiteTests;
+import com.liferay.portalweb.socialofficesite.home.announcement.markasreadannouncementsentrygeneralsite.MarkAsReadAnnouncementsEntryGeneralSiteTests;
+import com.liferay.portalweb.socialofficesite.home.announcement.showannouncementsentrygeneralsite.ShowAnnouncementsEntryGeneralSiteTests;
+import com.liferay.portalweb.socialofficesite.home.announcement.sousaddannouncementsentrysitesite.SOUs_AddAnnouncementsEntrySiteSiteTests;
+import com.liferay.portalweb.socialofficesite.home.announcement.sousdeleteannouncementsentrysitesite.SOUs_DeleteAnnouncementsEntrySiteSiteTests;
+import com.liferay.portalweb.socialofficesite.home.announcement.souseditannouncementsentrysitesite.SOUs_EditAnnouncementsEntrySiteSiteTests;
+import com.liferay.portalweb.socialofficesite.home.announcement.sousmarkasreadannouncementsentrysitesite.SOUs_MarkAsReadAnnouncementsEntrySiteSiteTests;
+import com.liferay.portalweb.socialofficesite.home.announcement.sousshowannouncementsentrysitesite.SOUs_ShowAnnouncementsEntrySiteSiteTests;
+import com.liferay.portalweb.socialofficesite.home.announcement.sousviewannouncementsentrysitesite.SOUs_ViewAnnouncementsEntrySiteSiteTests;
+import com.liferay.portalweb.socialofficesite.home.announcement.viewannouncementsentrygeneralsite.ViewAnnouncementsEntryGeneralSiteTests;
+import com.liferay.portalweb.socialofficesite.home.announcement.viewannouncementsentrynotifications.ViewAnnouncementsEntryNotificationsTests;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -26,6 +39,21 @@ public class AnnouncementTestPlan extends BaseTestSuite {
 
 	public static Test suite() {
 		TestSuite testSuite = new TestSuite();
+
+		testSuite.addTest(AddAnnouncementsEntryGeneralSiteTests.suite());
+		testSuite.addTest(DeleteAnnouncementsEntryGeneralSiteTests.suite());
+		testSuite.addTest(EditAnnouncementsEntryGeneralSiteTests.suite());
+		testSuite.addTest(MarkAsReadAnnouncementsEntryGeneralSiteTests.suite());
+		testSuite.addTest(ShowAnnouncementsEntryGeneralSiteTests.suite());
+		testSuite.addTest(SOUs_AddAnnouncementsEntrySiteSiteTests.suite());
+		testSuite.addTest(SOUs_DeleteAnnouncementsEntrySiteSiteTests.suite());
+		testSuite.addTest(SOUs_EditAnnouncementsEntrySiteSiteTests.suite());
+		testSuite.addTest(
+			SOUs_MarkAsReadAnnouncementsEntrySiteSiteTests.suite());
+		testSuite.addTest(SOUs_ShowAnnouncementsEntrySiteSiteTests.suite());
+		testSuite.addTest(SOUs_ViewAnnouncementsEntrySiteSiteTests.suite());
+		testSuite.addTest(ViewAnnouncementsEntryGeneralSiteTests.suite());
+		testSuite.addTest(ViewAnnouncementsEntryNotificationsTests.suite());
 
 		return testSuite;
 	}
