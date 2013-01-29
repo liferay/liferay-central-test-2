531ff3150b0c4e1d078997c9650e09a7a951eaa5
Hugo Huijser <hugo.huijser@liferay.com>
2013-01-29 15:09:16 -0800

    LPS-32371 Source formatting

--- portal-impl/src/com/liferay/portlet/wiki/service/impl/WikiPageServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/wiki/service/impl/WikiPageServiceImpl.java
@@ -300,16 +300,11 @@ public class WikiPageServiceImpl extends WikiPageServiceBaseImpl {
 
 		WikiNode node = wikiNodePersistence.findByPrimaryKey(nodeId);
 
-		long companyId = node.getCompanyId();
-		String name = node.getName();
-		String description = node.getDescription();
 		List<WikiPage> pages = getNodePages(nodeId, max);
-		boolean diff = false;
-		Locale locale = null;
 
 		return exportToRSS(
-			companyId, name, description, type, version, displayStyle, feedURL,
-			entryURL, pages, diff, locale);
+			node.getCompanyId(), node.getName(), node.getDescription(), type,
+			version, displayStyle, feedURL, entryURL, pages, false, null);
 	}
 
 	public List<WikiPage> getOrphans(long groupId, long nodeId)
@@ -438,14 +433,12 @@ public class WikiPageServiceImpl extends WikiPageServiceBaseImpl {
 		WikiPagePermission.check(
 			getPermissionChecker(), nodeId, title, ActionKeys.VIEW);
 
-		String description = title;
 		List<WikiPage> pages = wikiPageLocalService.getPages(
 			nodeId, title, 0, max, new PageCreateDateComparator(true));
-		boolean diff = true;
 
 		return exportToRSS(
-			companyId, title, description, type, version, displayStyle, feedURL,
-			entryURL, pages, diff, locale);
+			companyId, title, title, type, version, displayStyle, feedURL,
+			entryURL, pages, true, locale);
 	}
 
 	public List<WikiPage> getRecentChanges(
