703eab06cfd455537c9b66df27cdf557a3a674fa
sherryre <sherry.yang@liferay.com>
2013-01-29 17:48:51 -0800

    LPS-32399 Editor makeups are displayed in the RSS feed of the Wiki portlet

--- portal-impl/src/com/liferay/portlet/wiki/service/impl/WikiPageServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/wiki/service/impl/WikiPageServiceImpl.java
@@ -737,12 +737,16 @@ public class WikiPageServiceImpl extends WikiPageServiceBaseImpl {
 			template.put("languageUtil", LanguageUtil.getLanguage());
 			template.put("locale", locale);
 
-			String sourceContent = WikiUtil.processContent(
-				latestPage.getContent());
-			String targetContent = WikiUtil.processContent(page.getContent());
+			String sourceContent = StringPool.BLANK;
+			String targetContent = StringPool.BLANK;
 
-			sourceContent = HtmlUtil.escape(sourceContent);
-			targetContent = HtmlUtil.escape(targetContent);
+			if (latestPage != null) {
+				sourceContent = WikiUtil.convert(latestPage, null, null, null);
+			}
+
+			if (page != null) {
+				targetContent = WikiUtil.convert(page, null, null, null);
+			}
 
 			List<DiffResult>[] diffResults = DiffUtil.diff(
 				new UnsyncStringReader(sourceContent),
