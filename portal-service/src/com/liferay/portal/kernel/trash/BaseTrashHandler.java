d1b1f8e07c4e83828fed3bc10c8fecfdea45be96
Jim Hinkey <james.hinkey@liferay.com>
2013-02-04 10:17:59 -0500

    LPS-31950 remove unneeded Javadoc - no changes

--- portal-service/src/com/liferay/portal/kernel/trash/BaseTrashHandler.java
+++ portal-service/src/com/liferay/portal/kernel/trash/BaseTrashHandler.java
@@ -42,23 +42,6 @@ import javax.portlet.PortletRequest;
  */
 public abstract class BaseTrashHandler implements TrashHandler {
 
-	/**
-	 * Checks if a duplicate trash entry already exists in the destination
-	 * container.
-	 *
-	 * <p>
-	 * This method is used to check for duplicates when a trash entry is being
-	 * restored or moved out of the Recycle Bin.
-	 * </p>
-	 *
-	 * @param  trashEntry the trash entry to check
-	 * @param  containerModelId the primary key of the destination (e.g. folder)
-	 * @param  newName the new name to be assigned to the trash entry
-	 *         (optionally <code>null</code> to forego renaming the trash entry)
-	 * @throws PortalException if a duplicate trash entry already existed in the
-	 *         destination container
-	 * @throws SystemException if a system exception occurred
-	 */
 	public void checkDuplicateTrashEntry(
 			TrashEntry trashEntry, long containerModelId, String newName)
 		throws PortalException, SystemException {
@@ -82,15 +65,6 @@ public abstract class BaseTrashHandler implements TrashHandler {
 		deleteTrashEntries(new long[] {classPK}, checkPermission);
 	}
 
-	/**
-	 * Returns the container model with the primary key.
-	 *
-	 * @param  containerModelId the primary key of the container model
-	 * @return the container model with the primary key
-	 * @throws PortalException if a container model with the primary key could
-	 *         not be found
-	 * @throws SystemException if a system exception occurred
-	 */
 	public ContainerModel getContainerModel(long containerModelId)
 		throws PortalException, SystemException {
 
@@ -105,37 +79,6 @@ public abstract class BaseTrashHandler implements TrashHandler {
 		return StringPool.BLANK;
 	}
 
-	/**
-	 * Returns a range of all the container models that are children of the
-	 * parent container model identified by the container model ID. These
-	 * container models must be able to contain the entity identified by the
-	 * primary key.
-	 *
-	 * <p>
-	 * This method checks for the view permission when retrieving the container
-	 * models.
-	 * </p>
-	 *
-	 * <p>
-	 * Useful when paginating results. Returns a maximum of <code>end -
-	 * start</code> instances. The <code>start</code> and <code>end</code>
-	 * values are not primary keys but, rather, indexes in the result set. Thus,
-	 * <code>0</code> refers to the first result in the set. Setting both
-	 * <code>start</code> and <code>end</code> to {@link
-	 * com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS} will return the full
-	 * result set.
-	 * </p>
-	 *
-	 * @param  classPK the primary key of an entity the container models must be
-	 *         able to contain
-	 * @param  containerModelId the primary key of the parent container model
-	 * @param  start the lower bound of the range of results
-	 * @param  end the upper bound of the range of results (not inclusive)
-	 * @return the range of matching container models
-	 * @throws PortalException if a trash entry with the primary key could not
-	 *         be found
-	 * @throws SystemException if a system exception occurred
-	 */
 	public List<ContainerModel> getContainerModels(
 			long classPK, long containerModelId, int start, int end)
 		throws PortalException, SystemException {
@@ -143,24 +86,6 @@ public abstract class BaseTrashHandler implements TrashHandler {
 		return Collections.emptyList();
 	}
 
-	/**
-	 * Returns the number of container models that are children of the parent
-	 * container model identified by the container model ID. These container
-	 * models must be able to contain the entity identified by the primary key.
-	 *
-	 * <p>
-	 * This method checks for the view permission when counting the container
-	 * models.
-	 * </p>
-	 *
-	 * @param  classPK the primary key of an entity the container models must be
-	 *         able to contain
-	 * @param  containerModelId the primary key of the parent container model
-	 * @return the number of matching container models
-	 * @throws PortalException if a trash entry with the primary key could not
-	 *         be found
-	 * @throws SystemException if a system exception occurred
-	 */
 	public int getContainerModelsCount(long classPK, long containerModelId)
 		throws PortalException, SystemException {
 
@@ -171,53 +96,24 @@ public abstract class BaseTrashHandler implements TrashHandler {
 		return "deleted-in-x";
 	}
 
-	/**
-	 * @throws PortalException
-	 * @throws SystemException if a system exception occurred
-	 */
 	public ContainerModel getParentContainerModel(long classPK)
 		throws PortalException, SystemException {
 
 		return null;
 	}
 
-	/**
-	 * @throws PortalException
-	 * @throws SystemException if a system exception occurred
-	 */
 	public List<ContainerModel> getParentContainerModels(long classPK)
 		throws PortalException, SystemException {
 
 		return Collections.emptyList();
 	}
 
-	/**
-	 * Returns the link to the location to which the trash entry was restored.
-	 *
-	 * @param  portletRequest the portlet request
-	 * @param  classPK the primary key of the restored trash entry
-	 * @return the restore link
-	 * @throws PortalException if a trash entry with the primary key could not
-	 *         be found
-	 * @throws SystemException if a system exception occurred
-	 */
 	public String getRestoreLink(PortletRequest portletRequest, long classPK)
 		throws PortalException, SystemException {
 
 		return StringPool.BLANK;
 	}
 
-	/**
-	 * Returns the message describing the location to which the trash entry was
-	 * restored.
-	 *
-	 * @param  portletRequest the portlet request
-	 * @param  classPK the primary key of the restored trash entry
-	 * @return the restore message
-	 * @throws PortalException if a trash entry with the primary key could not
-	 *         be found
-	 * @throws SystemException if a system exception occurred
-	 */
 	public String getRestoreMessage(PortletRequest portletRequest, long classPK)
 		throws PortalException, SystemException {
 
@@ -236,20 +132,12 @@ public abstract class BaseTrashHandler implements TrashHandler {
 		return StringPool.BLANK;
 	}
 
-	/**
-	 * @throws PortalException
-	 * @throws SystemException if a system exception occurred
-	 */
 	public int getTrashContainedModelsCount(long classPK)
 		throws PortalException, SystemException {
 
 		return 0;
 	}
 
-	/**
-	 * @throws PortalException
-	 * @throws SystemException if a system exception occurred
-	 */
 	public List<TrashRenderer> getTrashContainedModelTrashRenderers(
 			long classPK, int start, int end)
 		throws PortalException, SystemException {
@@ -261,20 +149,12 @@ public abstract class BaseTrashHandler implements TrashHandler {
 		return StringPool.BLANK;
 	}
 
-	/**
-	 * @throws PortalException
-	 * @throws SystemException if a system exception occurred
-	 */
 	public int getTrashContainerModelsCount(long classPK)
 		throws PortalException, SystemException {
 
 		return 0;
 	}
 
-	/**
-	 * @throws PortalException
-	 * @throws SystemException if a system exception occurred
-	 */
 	public List<TrashRenderer> getTrashContainerModelTrashRenderers(
 			long classPK, int start, int end)
 		throws PortalException, SystemException {
@@ -333,10 +213,6 @@ public abstract class BaseTrashHandler implements TrashHandler {
 		return true;
 	}
 
-	/**
-	 * @throws PortalException
-	 * @throws SystemException if a system exception occurred
-	 */
 	public boolean isInTrashContainer(long classPK)
 		throws PortalException, SystemException {
 
@@ -347,33 +223,12 @@ public abstract class BaseTrashHandler implements TrashHandler {
 		return false;
 	}
 
-	/**
-	 * Returns <code>true</code> if the trash entry can be restored to its
-	 * original location.
-	 *
-	 * <p>
-	 * This method usually returns <code>false</code> if the container (e.g.
-	 * folder) of the trash entry is no longer available (e.g. moved to the
-	 * Recycle Bin or deleted).
-	 * </p>
-	 *
-	 * @param  classPK the primary key of the trash entry
-	 * @return <code>true</code> if the trash entry can be restored to its
-	 *         original location; <code>false</code> otherwise
-	 * @throws PortalException if a trash entry with the primary key could not
-	 *         be found
-	 * @throws SystemException if a system exception occurred
-	 */
 	public boolean isRestorable(long classPK)
 		throws PortalException, SystemException {
 
 		return true;
 	}
 
-	/**
-	 * @throws PortalException
-	 * @throws SystemException
-	 */
 	public void moveEntry(
 			long classPK, long containerModelId, ServiceContext serviceContext)
 		throws PortalException, SystemException {
@@ -403,17 +258,6 @@ public abstract class BaseTrashHandler implements TrashHandler {
 		restoreTrashEntries(new long[] {classPK});
 	}
 
-	/**
-	 * Updates the title of the trash entry with the primary key. This method is
-	 * called by {@link com.liferay.portlet.trash.action.EditEntryAction} before
-	 * restoring the trash entry via its restore rename action.
-	 *
-	 * @param  classPK the primary key of the trash entry
-	 * @param  title the title to be assigned
-	 * @throws PortalException if a trash entry with the primary key could not
-	 *         be found
-	 * @throws SystemException if a system exception occurred
-	 */
 	public void updateTitle(long classPK, String title)
 		throws PortalException, SystemException {
 	}
