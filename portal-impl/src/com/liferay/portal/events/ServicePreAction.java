ee4aea54267dca9ceea1588fe26a4fbf5b1f2c97
Brian Chan <brian.chan@liferay.com>
2013-02-05 15:18:32 -0800

    LPS-32745 Sort

--- portal-impl/src/com/liferay/portal/events/ServicePreAction.java
+++ portal-impl/src/com/liferay/portal/events/ServicePreAction.java
@@ -821,8 +821,8 @@ public class ServicePreAction extends Action {
 		themeDisplay.setPlid(plid);
 		themeDisplay.setPpid(ppid);
 		themeDisplay.setLayoutTypePortlet(layoutTypePortlet);
-		themeDisplay.setSiteGroupId(siteGroupId);
 		themeDisplay.setScopeGroupId(scopeGroupId);
+		themeDisplay.setSiteGroupId(siteGroupId);
 		themeDisplay.setSignedIn(signedIn);
 		themeDisplay.setPermissionChecker(permissionChecker);
 		themeDisplay.setLocale(locale);
