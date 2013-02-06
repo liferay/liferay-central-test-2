cce1f52e997c98b51671aec53744be29011d0da1
Brian Chiu <brian.chiu@liferay.com>
2013-02-05 16:25:59 -0800

    Test Fixes - portal/controlpanel/blogs

--- portal-web/test/functional/com/liferay/portalweb/portal/controlpanel/blogs/entry/addblogsentryckeditorcp/AddBlogsEntryCKEditorCPTest.java
+++ portal-web/test/functional/com/liferay/portalweb/portal/controlpanel/blogs/entry/addblogsentryckeditorcp/AddBlogsEntryCKEditorCPTest.java
@@ -42,7 +42,8 @@ public class AddBlogsEntryCKEditorCPTest extends BaseTestCase {
 		selenium.waitForPageToLoad("30000");
 		selenium.type("//input[@id='_161_title']",
 			RuntimeVariables.replace("Blogs Entry Title"));
-		selenium.waitForVisible("//a[contains(@class,'cke_button_disabled')]");
+		selenium.waitForVisible(
+			"//a[contains(@class,'cke_button cke_button__cut') and contains(@class,'cke_button_disabled')]");
 		selenium.waitForVisible("//iframe[contains(@title,'Rich Text Editor')]");
 		selenium.typeFrame("//iframe[contains(@title,'Rich Text Editor')]",
 			RuntimeVariables.replace("CK Editor Content"));
