5235ba7261a9d7fc61fbd9f996bbfb3f28957496
Brian Chan <brian.chan@liferay.com>
2013-02-06 00:00:50 -0800

    LPS-32790 Ran "ant build-upgrade-table"

--- portal-impl/src/com/liferay/portal/upgrade/v6_2_0/util/DLFileRankTable.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_2_0/util/DLFileRankTable.java
@@ -17,7 +17,7 @@ package com.liferay.portal.upgrade.v6_2_0.util;
 import java.sql.Types;
 
 /**
- * @author	  Mate Thurzo
+ * @author	  Brian Wing Shun Chan
  * @generated
  */
 public class DLFileRankTable {
@@ -25,16 +25,16 @@ public class DLFileRankTable {
 	public static final String TABLE_NAME = "DLFileRank";
 
 	public static final Object[][] TABLE_COLUMNS = {
-			{"uuid_", Types.VARCHAR},
-			{"fileRankId", Types.BIGINT},
-			{"groupId", Types.BIGINT},
-			{"companyId", Types.BIGINT},
-			{"userId", Types.BIGINT},
-			{"userName", Types.VARCHAR},
-			{"createDate", Types.TIMESTAMP},
-			{"modifiedDate", Types.TIMESTAMP},
-			{"fileEntryId", Types.BIGINT},
-			{"active_", Types.BOOLEAN}
+		{"uuid_", Types.VARCHAR},
+		{"fileRankId", Types.BIGINT},
+		{"groupId", Types.BIGINT},
+		{"companyId", Types.BIGINT},
+		{"userId", Types.BIGINT},
+		{"userName", Types.VARCHAR},
+		{"createDate", Types.TIMESTAMP},
+		{"modifiedDate", Types.TIMESTAMP},
+		{"fileEntryId", Types.BIGINT},
+		{"active_", Types.BOOLEAN}
 	};
 
 	public static final String TABLE_SQL_CREATE = "create table DLFileRank (uuid_ VARCHAR(75) null,fileRankId LONG not null primary key,groupId LONG,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,fileEntryId LONG,active_ BOOLEAN)";
@@ -42,14 +42,14 @@ public class DLFileRankTable {
 	public static final String TABLE_SQL_DROP = "drop table DLFileRank";
 
 	public static final String[] TABLE_SQL_ADD_INDEXES = {
-			"create unique index IX_38F0315 on DLFileRank (companyId, userId, fileEntryId)",
-			"create index IX_A65A1F8B on DLFileRank (fileEntryId)",
-			"create index IX_BAFB116E on DLFileRank (groupId, userId)",
-			"create index IX_4E96195B on DLFileRank (groupId, userId, active_)",
-			"create index IX_EED06670 on DLFileRank (userId)",
-			"create index IX_B8BB7CBE on DLFileRank (uuid_)",
-			"create index IX_B3D1C4AA on DLFileRank (uuid_, companyId)",
-			"create unique index IX_6D776DAC on DLFileRank (uuid_, groupId)"
+		"create unique index IX_38F0315 on DLFileRank (companyId, userId, fileEntryId)",
+		"create index IX_A65A1F8B on DLFileRank (fileEntryId)",
+		"create index IX_BAFB116E on DLFileRank (groupId, userId)",
+		"create index IX_4E96195B on DLFileRank (groupId, userId, active_)",
+		"create index IX_EED06670 on DLFileRank (userId)",
+		"create index IX_B8BB7CBE on DLFileRank (uuid_)",
+		"create index IX_B3D1C4AA on DLFileRank (uuid_, companyId)",
+		"create unique index IX_6D776DAC on DLFileRank (uuid_, groupId)"
 	};
 
 }
