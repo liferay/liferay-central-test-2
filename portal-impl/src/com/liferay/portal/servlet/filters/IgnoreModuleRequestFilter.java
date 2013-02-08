brianchandotcom/liferay-portal

LPS-32743 Modify the required filters in order to support the new kind of applications we are deploying into the OSGi container

portal-impl/src/com/liferay/portal/servlet/filters/IgnoreModuleRequestFilter.java
@@ -1,0 +1,66 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.servlet.filters;
+
+import com.liferay.portal.util.Portal;
+import com.liferay.portal.util.PortalUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * This filter checks if the request is targeted toward a module, and if so it
+ * ignores the request. This is because the module itself may later invoke the
+ * filter. A use case includes when the filter must operate on resources of
+ * the target module context, these will be unreachable from the portal's
+ * context.
+ *
+ * @author Raymond Augé
+ */
+public abstract class IgnoreModuleRequestFilter extends BasePortalFilter {
+
+	@Override
+	public boolean isFilterEnabled(
+		HttpServletRequest request, HttpServletResponse response) {
+
+		if (isModuleRequest(request)) {
+			return false;
+		}
+
+		return super.isFilterEnabled(request, response);
+	}
+
+	protected boolean isModuleRequest(HttpServletRequest request) {
+		String contextPath = request.getContextPath();
+		String requestURI = request.getRequestURI();
+		String resourcePath = requestURI;
+
+		int pos = requestURI.indexOf(contextPath);
+
+		if (pos == 0) {
+			resourcePath = resourcePath.substring(contextPath.length());
+		}
+
+		if (resourcePath.startsWith(_MODULE_PATH)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private static final String _MODULE_PATH =
+		PortalUtil.getPathContext().concat(Portal.MODULE_MAPPING);
+
+}

portal-impl/src/com/liferay/portal/servlet/filters/virtualhost/VirtualHostFilter.java
@@ -29,6 +29,7 @@ import com.liferay.portal.model.impl.LayoutImpl;
 import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.servlet.I18nServlet;
 import com.liferay.portal.servlet.filters.BasePortalFilter;
+import com.liferay.portal.util.Portal;
 import com.liferay.portal.util.PortalInstances;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.PropsValues;
@@ -81,6 +82,7 @@ public class VirtualHostFilter extends BasePortalFilter {
 		friendlyURL = friendlyURL.toLowerCase();
 
 		if (PortalInstances.isVirtualHostsIgnorePath(friendlyURL) ||
+			friendlyURL.startsWith(Portal.MODULE_MAPPING) ||
 			friendlyURL.startsWith(_PRIVATE_GROUP_SERVLET_MAPPING_SLASH) ||
 			friendlyURL.startsWith(_PRIVATE_USER_SERVLET_MAPPING_SLASH) ||
 			friendlyURL.startsWith(_PUBLIC_GROUP_SERVLET_MAPPING_SLASH)) {

portal-service/src/com/liferay/portal/util/Portal.java
@@ -72,6 +72,8 @@ public interface Portal {
 
 	public static final String FRIENDLY_URL_SEPARATOR = "/-/";
 
+	public static final String MODULE_MAPPING = "/o/";
+
 	public static final String PATH_IMAGE = "/image";
 
 	public static final String PATH_MAIN = "/c";

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portal/servlet/filters/IgnoreModuleRequestFilter.java
@@ -1,0 +1,66 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.servlet.filters;
+
+import com.liferay.portal.util.Portal;
+import com.liferay.portal.util.PortalUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * This filter checks if the request is targeted toward a module, and if so it
+ * ignores the request. This is because the module itself may later invoke the
+ * filter. A use case includes when the filter must operate on resources of
+ * the target module context, these will be unreachable from the portal's
+ * context.
+ *
+ * @author Raymond Augé
+ */
+public abstract class IgnoreModuleRequestFilter extends BasePortalFilter {
+
+	@Override
+	public boolean isFilterEnabled(
+		HttpServletRequest request, HttpServletResponse response) {
+
+		if (isModuleRequest(request)) {
+			return false;
+		}
+
+		return super.isFilterEnabled(request, response);
+	}
+
+	protected boolean isModuleRequest(HttpServletRequest request) {
+		String contextPath = request.getContextPath();
+		String requestURI = request.getRequestURI();
+		String resourcePath = requestURI;
+
+		int pos = requestURI.indexOf(contextPath);
+
+		if (pos == 0) {
+			resourcePath = resourcePath.substring(contextPath.length());
+		}
+
+		if (resourcePath.startsWith(_MODULE_PATH)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private static final String _MODULE_PATH =
+		PortalUtil.getPathContext().concat(Portal.PATH_MODULE);
+
+}

portal-impl/src/com/liferay/portal/servlet/filters/virtualhost/VirtualHostFilter.java
@@ -29,6 +29,7 @@ import com.liferay.portal.model.impl.LayoutImpl;
 import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.servlet.I18nServlet;
 import com.liferay.portal.servlet.filters.BasePortalFilter;
+import com.liferay.portal.util.Portal;
 import com.liferay.portal.util.PortalInstances;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.PropsValues;
@@ -81,6 +82,7 @@ public class VirtualHostFilter extends BasePortalFilter {
 		friendlyURL = friendlyURL.toLowerCase();
 
 		if (PortalInstances.isVirtualHostsIgnorePath(friendlyURL) ||
+			friendlyURL.startsWith(Portal.PATH_MODULE) ||
 			friendlyURL.startsWith(_PRIVATE_GROUP_SERVLET_MAPPING_SLASH) ||
 			friendlyURL.startsWith(_PRIVATE_USER_SERVLET_MAPPING_SLASH) ||
 			friendlyURL.startsWith(_PUBLIC_GROUP_SERVLET_MAPPING_SLASH)) {

portal-service/src/com/liferay/portal/util/Portal.java
@@ -76,6 +76,8 @@ public interface Portal {
 
 	public static final String PATH_MAIN = "/c";
 
+	public static final String PATH_MODULE = "/o/";
+
 	public static final String PATH_PORTAL_LAYOUT = "/portal/layout";
 
 	public static final String PORTAL_REALM = "PortalRealm";