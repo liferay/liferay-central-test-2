910c1d2834b5f471e1b0a93dca77dd9f44d32688
Jim Hinkey <james.hinkey@liferay.com>
2013-02-12 09:46:42 -0500

    LPS-30153 Javadoc add missing param comment

--- portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
@@ -1246,7 +1246,7 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 	/**
 	 * Returns all the layouts without resource permissions
 	 *
-	 * @param
+	 * @param  roleId the primary key of the role
 	 * @return all the layouts without resource permissions
 	 * @throws SystemException if a system exception occurred
 	 */
