cde8ca47d2c66fb693dcf19e6ac9567c8bb7f513
Miguel Pastor <miguel.pastor@liferay.com>
2013-02-15 10:36:38 +0100

    LPS-32742 Refactor the resource servlet to use a constants instead the hardcoded string

--- osgi/modules/http-service/src/com/liferay/httpservice/servlet/ResourceServlet.java
+++ osgi/modules/http-service/src/com/liferay/httpservice/servlet/ResourceServlet.java
@@ -14,6 +14,7 @@
 
 package com.liferay.httpservice.servlet;
 
+import com.liferay.httpservice.HttpServicePropsKeys;
 import com.liferay.portal.kernel.servlet.HttpHeaders;
 import com.liferay.portal.kernel.servlet.ServletResponseUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
@@ -46,12 +47,13 @@ public class ResourceServlet extends WebServerServlet {
 	public void init(ServletConfig servletConfig) {
 		_bundleServletConfig = (BundleServletConfig)servletConfig;
 
+		_alias = GetterUtil.getString(
+			_bundleServletConfig.getInitParameter(HttpServicePropsKeys.ALIAS));
+
 		_httpContext = _bundleServletConfig.getHttpContext();
 
-		_alias = GetterUtil.getString(
-			_bundleServletConfig.getInitParameter("alias"));
 		_name = GetterUtil.getString(
-			_bundleServletConfig.getInitParameter("name"));
+			_bundleServletConfig.getInitParameter(HttpServicePropsKeys.NAME));
 	}
 
 	@Override
