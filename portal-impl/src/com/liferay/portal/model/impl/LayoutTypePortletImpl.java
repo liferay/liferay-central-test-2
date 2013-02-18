30291e207e79186235e7a96954ef94fd556e5205
Eduardo Garcia <eduardo.garcia@liferay.com>
2013-02-18 13:08:15 +0100

    LPS-31497 A Site Admin cannot edit the Layout Template of a page created by site templates

--- portal-impl/src/com/liferay/portal/model/impl/LayoutTypePortletImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/LayoutTypePortletImpl.java
@@ -332,16 +332,8 @@ public class LayoutTypePortletImpl
 	}
 
 	public String getLayoutTemplateId() {
-		String layoutTemplateId = StringPool.BLANK;
-
-		if (hasLayoutSetPrototypeLayout()) {
-			layoutTemplateId = getLayoutSetPrototypeLayoutProperty(
-				LayoutTypePortletConstants.LAYOUT_TEMPLATE_ID);
-		}
-		else {
-			layoutTemplateId = getTypeSettingsProperty(
-				LayoutTypePortletConstants.LAYOUT_TEMPLATE_ID);
-		}
+		String layoutTemplateId = getTypeSettingsProperty(
+			LayoutTypePortletConstants.LAYOUT_TEMPLATE_ID);
 
 		if (Validator.isNull(layoutTemplateId)) {
 			layoutTemplateId = StringPool.BLANK;
