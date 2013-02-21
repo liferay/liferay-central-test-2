f1ba7e5b0ac48df3ab6afb3f4b3709ad807171b6
Mate Thurzo <mate.thurzo@liferay.com>
2013-02-21 11:32:24 +0100

    LPS-33090 Adding MDR related skeleton staged model data handlers

---
+++ portal-impl/src/com/liferay/portlet/mobiledevicerules/lar/MDRActionStagedModelDataHandler.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.mobiledevicerules.lar;
+
+import com.liferay.portal.kernel.lar.BaseStagedModelDataHandler;
+import com.liferay.portal.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portlet.mobiledevicerules.model.MDRAction;
+
+/**
+ * @author Mate Thurzo
+ */
+public class MDRActionStagedModelDataHandler
+	extends BaseStagedModelDataHandler<MDRAction> {
+
+	@Override
+	public String getClassName() {
+		return MDRAction.class.getName();
+	}
+
+	@Override
+	protected void doExportStagedModel(
+			PortletDataContext portletDataContext, Element[] elements,
+			MDRAction action)
+		throws Exception {
+
+		return;
+	}
+
+	@Override
+	protected void doImportStagedModel(
+			PortletDataContext portletDataContext, Element element, String path,
+			MDRAction action)
+		throws Exception {
+
+		return;
+	}
+
+}
