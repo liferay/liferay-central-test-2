juliocamarero/liferay-portal

LPS-31965 Autogenerated

portal-service/src/com/liferay/portlet/asset/service/AssetCategoryLocalService.java
@@ -291,6 +291,11 @@ public interface AssetCategoryLocalService extends BaseLocalService,
 		throws com.liferay.portal.kernel.exception.SystemException;
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<java.lang.Long> getAllChildCategories(
+		long parentCategoryId)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public java.util.List<com.liferay.portlet.asset.model.AssetCategory> getCategories()
 		throws com.liferay.portal.kernel.exception.SystemException;
 

portal-service/src/com/liferay/portlet/asset/service/AssetCategoryLocalServiceUtil.java
@@ -334,6 +334,12 @@ public class AssetCategoryLocalServiceUtil {
 		return getService().fetchCategory(categoryId);
 	}
 
+	public static java.util.List<java.lang.Long> getAllChildCategories(
+		long parentCategoryId)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getService().getAllChildCategories(parentCategoryId);
+	}
+
 	public static java.util.List<com.liferay.portlet.asset.model.AssetCategory> getCategories()
 		throws com.liferay.portal.kernel.exception.SystemException {
 		return getService().getCategories();

portal-service/src/com/liferay/portlet/asset/service/AssetCategoryLocalServiceWrapper.java
@@ -330,6 +330,12 @@ public class AssetCategoryLocalServiceWrapper
 		return _assetCategoryLocalService.fetchCategory(categoryId);
 	}
 
+	public java.util.List<java.lang.Long> getAllChildCategories(
+		long parentCategoryId)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return _assetCategoryLocalService.getAllChildCategories(parentCategoryId);
+	}
+
 	public java.util.List<com.liferay.portlet.asset.model.AssetCategory> getCategories()
 		throws com.liferay.portal.kernel.exception.SystemException {
 		return _assetCategoryLocalService.getCategories();

liferay/liferay-central-test-2

portal-service/src/com/liferay/portlet/asset/service/AssetCategoryLocalService.java
@@ -354,6 +354,11 @@ public interface AssetCategoryLocalService extends BaseLocalService,
 		throws com.liferay.portal.kernel.exception.SystemException;
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<java.lang.Long> getSubcategoryIds(
+		long parentCategoryId)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public java.util.List<com.liferay.portlet.asset.model.AssetCategory> getVocabularyCategories(
 		long vocabularyId, int start, int end,
 		com.liferay.portal.kernel.util.OrderByComparator obc)

portal-service/src/com/liferay/portlet/asset/service/AssetCategoryLocalServiceUtil.java
@@ -411,6 +411,12 @@ public class AssetCategoryLocalServiceUtil {
 		return getService().getEntryCategories(entryId);
 	}
 
+	public static java.util.List<java.lang.Long> getSubcategoryIds(
+		long parentCategoryId)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getService().getSubcategoryIds(parentCategoryId);
+	}
+
 	public static java.util.List<com.liferay.portlet.asset.model.AssetCategory> getVocabularyCategories(
 		long vocabularyId, int start, int end,
 		com.liferay.portal.kernel.util.OrderByComparator obc)

portal-service/src/com/liferay/portlet/asset/service/AssetCategoryLocalServiceWrapper.java
@@ -407,6 +407,12 @@ public class AssetCategoryLocalServiceWrapper
 		return _assetCategoryLocalService.getEntryCategories(entryId);
 	}
 
+	public java.util.List<java.lang.Long> getSubcategoryIds(
+		long parentCategoryId)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return _assetCategoryLocalService.getSubcategoryIds(parentCategoryId);
+	}
+
 	public java.util.List<com.liferay.portlet.asset.model.AssetCategory> getVocabularyCategories(
 		long vocabularyId, int start, int end,
 		com.liferay.portal.kernel.util.OrderByComparator obc)