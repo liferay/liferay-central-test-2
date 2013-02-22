64f0b7df20c24fc1b67a551c94b942d9f0d48438
Ken Duenwald <ken.duenwald@liferay.com>
2013-02-21 17:08:23 -0800

    SOS-1452

--- portal-web/test/functional/com/liferay/portalweb/socialoffice/users/teams/TeamsTestPlan.java
+++ portal-web/test/functional/com/liferay/portalweb/socialoffice/users/teams/TeamsTestPlan.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -12,14 +12,10 @@
  * details.
  */
 
-package com.liferay.portalweb.socialoffice.users;
+package com.liferay.portalweb.socialoffice.users.teams;
 
 import com.liferay.portalweb.portal.BaseTestSuite;
-import com.liferay.portalweb.socialoffice.users.organizations.OrganizationsTestPlan;
-import com.liferay.portalweb.socialoffice.users.sites.SitesTestPlan;
-import com.liferay.portalweb.socialoffice.users.teams.TeamsTestPlan;
-import com.liferay.portalweb.socialoffice.users.user.UserTestPlan;
-import com.liferay.portalweb.socialoffice.users.usergroups.UsergroupTestPlan;
+import com.liferay.portalweb.socialoffice.users.teams.addteamsite.AddTeamSiteTests;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -27,16 +23,12 @@ import junit.framework.TestSuite;
 /**
  * @author Brian Wing Shun Chan
  */
-public class UsersTestPlan extends BaseTestSuite {
+public class TeamsTestPlan extends BaseTestSuite {
 
 	public static Test suite() {
 		TestSuite testSuite = new TestSuite();
 
-		testSuite.addTest(OrganizationsTestPlan.suite());
-		testSuite.addTest(SitesTestPlan.suite());
-		testSuite.addTest(TeamsTestPlan.suite());
-		testSuite.addTest(UserTestPlan.suite());
-		testSuite.addTest(UsergroupTestPlan.suite());
+		testSuite.addTest(AddTeamSiteTests.suite());
 
 		return testSuite;
 	}
