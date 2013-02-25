f2f260855f082036ae16a8a78a46625529c4ac19
ealonso <eudaldo.alonso@liferay.com>
2013-02-25 13:10:19 +0100

    LPS-31965 Add new field classTypeIds to SearchContext

--- portal-service/src/com/liferay/portal/kernel/search/SearchContext.java
+++ portal-service/src/com/liferay/portal/kernel/search/SearchContext.java
@@ -75,6 +75,10 @@ public class SearchContext implements Serializable {
 		return _categoryIds;
 	}
 
+	public long[] getClassTypeIds() {
+		return _classTypeIds;
+	}
+
 	public long getCompanyId() {
 		return _companyId;
 	}
@@ -225,6 +229,10 @@ public class SearchContext implements Serializable {
 		_categoryIds = categoryIds;
 	}
 
+	public void setClassTypeIds(long[] classTypeIds) {
+		_classTypeIds = classTypeIds;
+	}
+
 	public void setCompanyId(long companyId) {
 		_companyId = companyId;
 	}
@@ -330,6 +338,7 @@ public class SearchContext implements Serializable {
 	private Map<String, Serializable> _attributes;
 	private BooleanClause[] _booleanClauses;
 	private long[] _categoryIds;
+	private long[] _classTypeIds;
 	private long _companyId;
 	private int _end = QueryUtil.ALL_POS;
 	private String[] _entryClassNames;
