sergiogonzalez/liferay-portal

LPS-29962 Integration tests for JournalFolderFinder and JournalArticleFinder

portal-impl/test/integration/com/liferay/portlet/journal/service/persistence/JournalFolderFinderTest.java
@@ -1,0 +1,193 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.journal.service.persistence;
+
+import com.liferay.portal.kernel.dao.orm.QueryDefinition;
+import com.liferay.portal.kernel.test.ExecutionTestListeners;
+import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.service.ServiceTestUtil;
+import com.liferay.portal.spring.hibernate.LastSessionRecorderUtil;
+import com.liferay.portal.test.EnvironmentExecutionTestListener;
+import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
+import com.liferay.portal.test.TransactionalExecutionTestListener;
+import com.liferay.portal.util.TestPropsValues;
+import com.liferay.portlet.asset.service.AssetEntryLocalServiceUtil;
+import com.liferay.portlet.journal.model.JournalArticle;
+import com.liferay.portlet.journal.model.JournalFolder;
+import com.liferay.portlet.journal.service.JournalArticleLocalServiceUtil;
+import com.liferay.portlet.journal.util.JournalTestUtil;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Zsolt Berentey
+ */
+@ExecutionTestListeners(
+	listeners = {
+		EnvironmentExecutionTestListener.class,
+		TransactionalExecutionTestListener.class
+	})
+@RunWith(LiferayIntegrationJUnitTestRunner.class)
+@Transactional
+public class JournalFolderFinderTest {
+
+	@Before
+	public void setUp() throws Exception {
+		_group = ServiceTestUtil.addGroup();
+
+		_folderA = JournalTestUtil.addFolder(_group.getGroupId(), "Folder A");
+
+		_folderB = JournalTestUtil.addFolder(
+			_group.getGroupId(), _folderA.getFolderId(), "Folder B");
+
+		JournalTestUtil.addArticle(
+			_group.getGroupId(), _folderA.getFolderId(), "Article 1",
+			StringPool.BLANK);
+
+		JournalArticle article = JournalTestUtil.addArticle(
+			_group.getGroupId(), _folderA.getFolderId(), "Article 2",
+			StringPool.BLANK);
+
+		JournalArticleLocalServiceUtil.moveArticleToTrash(
+			TestPropsValues.getUserId(), article);
+	}
+
+	@Test
+	public void testCountF_A_ByG_F() throws Exception {
+		QueryDefinition queryDefinition = new QueryDefinition();
+
+		queryDefinition.setStatus(WorkflowConstants.STATUS_ANY);
+
+		Assert.assertEquals(
+			3,
+			JournalFolderFinderUtil.countF_A_ByG_F(
+				_group.getGroupId(), _folderA.getFolderId(), queryDefinition));
+
+		queryDefinition.setStatus(WorkflowConstants.STATUS_IN_TRASH);
+
+		Assert.assertEquals(
+			2,
+			JournalFolderFinderUtil.countF_A_ByG_F(
+				_group.getGroupId(), _folderA.getFolderId(), queryDefinition));
+
+		queryDefinition.setStatus(WorkflowConstants.STATUS_IN_TRASH, true);
+
+		Assert.assertEquals(
+			2,
+			JournalFolderFinderUtil.countF_A_ByG_F(
+				_group.getGroupId(), _folderA.getFolderId(), queryDefinition));
+	}
+
+	@Test
+	public void testFindF_A_ByG_F() throws Exception {
+		QueryDefinition queryDefinition = new QueryDefinition();
+
+		queryDefinition.setStatus(WorkflowConstants.STATUS_ANY);
+
+		List<Object> list = JournalFolderFinderUtil.findF_A_ByG_F(
+			_group.getGroupId(), _folderA.getFolderId(), queryDefinition);
+
+		Assert.assertEquals(3, list.size());
+
+		for (Object object : list) {
+			if (object instanceof JournalFolder) {
+				JournalFolder folder = (JournalFolder)object;
+
+				Assert.assertEquals("Folder B", folder.getName());
+			}
+			else if (object instanceof JournalArticle) {
+				JournalArticle article = (JournalArticle)object;
+
+				String title = article.getTitleCurrentValue();
+
+				if (!title.equals("Article 1") && !title.equals("Article 2")) {
+					Assert.fail(title);
+				}
+			}
+		}
+
+		queryDefinition.setStatus(WorkflowConstants.STATUS_IN_TRASH);
+
+		list = JournalFolderFinderUtil.findF_A_ByG_F(
+			_group.getGroupId(), _folderA.getFolderId(), queryDefinition);
+
+		Assert.assertEquals(2, list.size());
+
+		for (Object object : list) {
+			if (object instanceof JournalFolder) {
+				JournalFolder folder = (JournalFolder)object;
+
+				Assert.assertEquals("Folder B", folder.getName());
+			}
+			else if (object instanceof JournalArticle) {
+				JournalArticle article = (JournalArticle)object;
+
+				Assert.assertEquals(
+					"Article 2", article.getTitleCurrentValue());
+			}
+		}
+
+		queryDefinition.setStatus(WorkflowConstants.STATUS_IN_TRASH, true);
+
+		list = JournalFolderFinderUtil.findF_A_ByG_F(
+			_group.getGroupId(), _folderA.getFolderId(), queryDefinition);
+
+		Assert.assertEquals(2, list.size());
+
+		for (Object object : list) {
+			if (object instanceof JournalFolder) {
+				JournalFolder folder = (JournalFolder)object;
+
+				Assert.assertEquals("Folder B", folder.getName());
+			}
+			else if (object instanceof JournalArticle) {
+				JournalArticle article = (JournalArticle)object;
+
+				Assert.assertEquals(
+					"Article 1", article.getTitleCurrentValue());
+			}
+		}
+	}
+
+	@Test
+	public void testFindF_ByNoAssets() throws Exception {
+		AssetEntryLocalServiceUtil.deleteEntry(
+			JournalFolder.class.getName(), _folderB.getFolderId());
+
+		LastSessionRecorderUtil.syncLastSessionState();
+
+		List<JournalFolder> folders =
+			JournalFolderFinderUtil.findF_ByNoAssets();
+
+		Assert.assertEquals(1, folders.size());
+
+		JournalFolder folder = folders.get(0);
+
+		Assert.assertEquals(_folderB.getFolderId(), folder.getFolderId());
+	}
+
+	private JournalFolder _folderA;
+	private JournalFolder _folderB;
+	private Group _group;
+
+}

portal-impl/test/integration/com/liferay/portlet/journal/util/JournalTestUtil.java
@@ -157,16 +157,18 @@ public class JournalTestUtil {
 	}
 
 	public static JournalArticle addArticleWithXMLContent(
-			long groupId, String xml, String structureId, String templateId)
+			long groupId, long folderId, long classNameId, String xml,
+			String structureId, String templateId)
 		throws Exception {
 
 		return addArticleWithXMLContent(
-			groupId, xml, structureId, templateId, LocaleUtil.getDefault());
+			groupId, folderId, classNameId, xml, structureId, templateId,
+			LocaleUtil.getDefault());
 	}
 
 	public static JournalArticle addArticleWithXMLContent(
-			long groupId, String xml, String structureId, String templateId,
-			Locale defaultLocale)
+			long groupId, long folderId, long classNameId, String xml,
+			String structureId, String templateId, Locale defaultLocale)
 		throws Exception {
 
 		Map<Locale, String> titleMap = new HashMap<Locale, String>();
@@ -180,10 +182,21 @@ public class JournalTestUtil {
 		serviceContext.setScopeGroupId(TestPropsValues.getGroupId());
 
 		return JournalArticleLocalServiceUtil.addArticle(
-			TestPropsValues.getUserId(), groupId, 0, 0, 0, StringPool.BLANK,
-			true, 0, titleMap, null, xml, "general", structureId, templateId,
-			null, 1, 1, 1965, 0, 0, 0, 0, 0, 0, 0, true, 0, 0, 0, 0, 0, true,
-			true, false, null, null, null, null, serviceContext);
+			TestPropsValues.getUserId(), groupId, folderId, classNameId, 0,
+			StringPool.BLANK, true, 0, titleMap, null, xml, "general",
+			structureId, templateId, null, 1, 1, 1965, 0, 0, 0, 0, 0, 0, 0,
+			true, 0, 0, 0, 0, 0, true, true, false, null, null, null, null,
+			serviceContext);
+	}
+
+	public static JournalArticle addArticleWithXMLContent(
+			long groupId, String xml, String structureId, String templateId)
+		throws Exception {
+
+		return addArticleWithXMLContent(
+			groupId, JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			JournalArticleConstants.CLASSNAME_ID_DEFAULT, xml, structureId,
+			templateId, LocaleUtil.getDefault());
 	}
 
 	public static JournalArticle addArticleWithXMLContent(
@@ -191,8 +204,10 @@ public class JournalTestUtil {
 		throws Exception {
 
 		return addArticleWithXMLContent(
-			TestPropsValues.getGroupId(), xml, structureId, templateId,
-			LocaleUtil.getDefault());
+			TestPropsValues.getGroupId(),
+			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			JournalArticleConstants.CLASSNAME_ID_DEFAULT, xml, structureId,
+			templateId, LocaleUtil.getDefault());
 	}
 
 	public static JournalArticle addArticleWithXMLContent(
@@ -201,8 +216,10 @@ public class JournalTestUtil {
 		throws Exception {
 
 		return addArticleWithXMLContent(
-			TestPropsValues.getGroupId(), xml, structureId, templateId,
-			defaultLocale);
+			TestPropsValues.getGroupId(),
+			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			JournalArticleConstants.CLASSNAME_ID_DEFAULT, xml, structureId,
+			templateId, defaultLocale);
 	}
 
 	public static DDMStructure addDDMStructure() throws Exception {

liferay/liferay-central-test-2

portal-impl/test/integration/com/liferay/portlet/journal/service/persistence/JournalFolderFinderTest.java
@@ -1,0 +1,193 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.journal.service.persistence;
+
+import com.liferay.portal.kernel.dao.orm.QueryDefinition;
+import com.liferay.portal.kernel.test.ExecutionTestListeners;
+import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.spring.hibernate.LastSessionRecorderUtil;
+import com.liferay.portal.test.EnvironmentExecutionTestListener;
+import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
+import com.liferay.portal.test.TransactionalExecutionTestListener;
+import com.liferay.portal.util.GroupTestUtil;
+import com.liferay.portal.util.TestPropsValues;
+import com.liferay.portlet.asset.service.AssetEntryLocalServiceUtil;
+import com.liferay.portlet.journal.model.JournalArticle;
+import com.liferay.portlet.journal.model.JournalFolder;
+import com.liferay.portlet.journal.service.JournalArticleLocalServiceUtil;
+import com.liferay.portlet.journal.util.JournalTestUtil;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Zsolt Berentey
+ */
+@ExecutionTestListeners(
+	listeners = {
+		EnvironmentExecutionTestListener.class,
+		TransactionalExecutionTestListener.class
+	})
+@RunWith(LiferayIntegrationJUnitTestRunner.class)
+@Transactional
+public class JournalFolderFinderTest {
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		_folderA = JournalTestUtil.addFolder(_group.getGroupId(), "Folder A");
+
+		_folderB = JournalTestUtil.addFolder(
+			_group.getGroupId(), _folderA.getFolderId(), "Folder B");
+
+		JournalTestUtil.addArticle(
+			_group.getGroupId(), _folderA.getFolderId(), "Article 1",
+			StringPool.BLANK);
+
+		JournalArticle article = JournalTestUtil.addArticle(
+			_group.getGroupId(), _folderA.getFolderId(), "Article 2",
+			StringPool.BLANK);
+
+		JournalArticleLocalServiceUtil.moveArticleToTrash(
+			TestPropsValues.getUserId(), article);
+	}
+
+	@Test
+	public void testCountF_A_ByG_F() throws Exception {
+		QueryDefinition queryDefinition = new QueryDefinition();
+
+		queryDefinition.setStatus(WorkflowConstants.STATUS_ANY);
+
+		Assert.assertEquals(
+			3,
+			JournalFolderFinderUtil.countF_A_ByG_F(
+				_group.getGroupId(), _folderA.getFolderId(), queryDefinition));
+
+		queryDefinition.setStatus(WorkflowConstants.STATUS_IN_TRASH);
+
+		Assert.assertEquals(
+			2,
+			JournalFolderFinderUtil.countF_A_ByG_F(
+				_group.getGroupId(), _folderA.getFolderId(), queryDefinition));
+
+		queryDefinition.setStatus(WorkflowConstants.STATUS_IN_TRASH, true);
+
+		Assert.assertEquals(
+			2,
+			JournalFolderFinderUtil.countF_A_ByG_F(
+				_group.getGroupId(), _folderA.getFolderId(), queryDefinition));
+	}
+
+	@Test
+	public void testFindF_A_ByG_F() throws Exception {
+		QueryDefinition queryDefinition = new QueryDefinition();
+
+		queryDefinition.setStatus(WorkflowConstants.STATUS_ANY);
+
+		List<Object> results = JournalFolderFinderUtil.findF_A_ByG_F(
+			_group.getGroupId(), _folderA.getFolderId(), queryDefinition);
+
+		Assert.assertEquals(3, results.size());
+
+		for (Object result : results) {
+			if (result instanceof JournalFolder) {
+				JournalFolder folder = (JournalFolder)result;
+
+				Assert.assertEquals("Folder B", folder.getName());
+			}
+			else if (result instanceof JournalArticle) {
+				JournalArticle article = (JournalArticle)result;
+
+				String title = article.getTitleCurrentValue();
+
+				if (!title.equals("Article 1") && !title.equals("Article 2")) {
+					Assert.fail(title);
+				}
+			}
+		}
+
+		queryDefinition.setStatus(WorkflowConstants.STATUS_IN_TRASH);
+
+		results = JournalFolderFinderUtil.findF_A_ByG_F(
+			_group.getGroupId(), _folderA.getFolderId(), queryDefinition);
+
+		Assert.assertEquals(2, results.size());
+
+		for (Object result : results) {
+			if (result instanceof JournalFolder) {
+				JournalFolder folder = (JournalFolder)result;
+
+				Assert.assertEquals("Folder B", folder.getName());
+			}
+			else if (result instanceof JournalArticle) {
+				JournalArticle article = (JournalArticle)result;
+
+				Assert.assertEquals(
+					"Article 2", article.getTitleCurrentValue());
+			}
+		}
+
+		queryDefinition.setStatus(WorkflowConstants.STATUS_IN_TRASH, true);
+
+		results = JournalFolderFinderUtil.findF_A_ByG_F(
+			_group.getGroupId(), _folderA.getFolderId(), queryDefinition);
+
+		Assert.assertEquals(2, results.size());
+
+		for (Object result : results) {
+			if (result instanceof JournalFolder) {
+				JournalFolder folder = (JournalFolder)result;
+
+				Assert.assertEquals("Folder B", folder.getName());
+			}
+			else if (result instanceof JournalArticle) {
+				JournalArticle article = (JournalArticle)result;
+
+				Assert.assertEquals(
+					"Article 1", article.getTitleCurrentValue());
+			}
+		}
+	}
+
+	@Test
+	public void testFindF_ByNoAssets() throws Exception {
+		AssetEntryLocalServiceUtil.deleteEntry(
+			JournalFolder.class.getName(), _folderB.getFolderId());
+
+		LastSessionRecorderUtil.syncLastSessionState();
+
+		List<JournalFolder> folders =
+			JournalFolderFinderUtil.findF_ByNoAssets();
+
+		Assert.assertEquals(1, folders.size());
+
+		JournalFolder folder = folders.get(0);
+
+		Assert.assertEquals(_folderB.getFolderId(), folder.getFolderId());
+	}
+
+	private JournalFolder _folderA;
+	private JournalFolder _folderB;
+	private Group _group;
+
+}

portal-impl/test/integration/com/liferay/portlet/journal/util/JournalTestUtil.java
@@ -157,18 +157,18 @@ public class JournalTestUtil {
 	}
 
 	public static JournalArticle addArticleWithXMLContent(
-			long groupId, String xml, String ddmStructureKey,
-			String ddmTemplateKey)
+			long groupId, long folderId, long classNameId, String xml,
+			String ddmStructureKey, String ddmTemplateKey)
 		throws Exception {
 
 		return addArticleWithXMLContent(
-			groupId, xml, ddmStructureKey, ddmTemplateKey,
-			LocaleUtil.getDefault());
+			groupId, folderId, classNameId, xml, ddmStructureKey,
+			ddmTemplateKey, LocaleUtil.getDefault());
 	}
 
 	public static JournalArticle addArticleWithXMLContent(
-			long groupId, String xml, String ddmStructureKey,
-			String ddmTemplateKey, Locale defaultLocale)
+			long groupId, long folderId, long classNameId, String xml,
+			String ddmStructureKey, String ddmTemplateKey, Locale defaultLocale)
 		throws Exception {
 
 		Map<Locale, String> titleMap = new HashMap<Locale, String>();
@@ -182,10 +182,22 @@ public class JournalTestUtil {
 		serviceContext.setScopeGroupId(TestPropsValues.getGroupId());
 
 		return JournalArticleLocalServiceUtil.addArticle(
-			TestPropsValues.getUserId(), groupId, 0, 0, 0, StringPool.BLANK,
-			true, 0, titleMap, null, xml, "general", ddmStructureKey,
-			ddmTemplateKey, null, 1, 1, 1965, 0, 0, 0, 0, 0, 0, 0, true, 0, 0,
-			0, 0, 0, true, true, false, null, null, null, null, serviceContext);
+			TestPropsValues.getUserId(), groupId, folderId, classNameId, 0,
+			StringPool.BLANK, true, 0, titleMap, null, xml, "general",
+			ddmStructureKey, ddmTemplateKey, null, 1, 1, 1965, 0, 0, 0, 0, 0, 0,
+			0, true, 0, 0, 0, 0, 0, true, true, false, null, null, null, null,
+			serviceContext);
+	}
+
+	public static JournalArticle addArticleWithXMLContent(
+			long groupId, String xml, String ddmStructureKey,
+			String ddmTemplateKey)
+		throws Exception {
+
+		return addArticleWithXMLContent(
+			groupId, JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			JournalArticleConstants.CLASSNAME_ID_DEFAULT, xml, ddmStructureKey,
+			ddmTemplateKey, LocaleUtil.getDefault());
 	}
 
 	public static JournalArticle addArticleWithXMLContent(
@@ -193,8 +205,10 @@ public class JournalTestUtil {
 		throws Exception {
 
 		return addArticleWithXMLContent(
-			TestPropsValues.getGroupId(), xml, ddmStructureKey, ddmTemplateKey,
-			LocaleUtil.getDefault());
+			TestPropsValues.getGroupId(),
+			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			JournalArticleConstants.CLASSNAME_ID_DEFAULT, xml, ddmStructureKey,
+			ddmTemplateKey, LocaleUtil.getDefault());
 	}
 
 	public static JournalArticle addArticleWithXMLContent(
@@ -203,8 +217,10 @@ public class JournalTestUtil {
 		throws Exception {
 
 		return addArticleWithXMLContent(
-			TestPropsValues.getGroupId(), xml, ddmStructureKey, ddmTemplateKey,
-			defaultLocale);
+			TestPropsValues.getGroupId(),
+			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			JournalArticleConstants.CLASSNAME_ID_DEFAULT, xml, ddmStructureKey,
+			ddmTemplateKey, defaultLocale);
 	}
 
 	public static DDMStructure addDDMStructure() throws Exception {