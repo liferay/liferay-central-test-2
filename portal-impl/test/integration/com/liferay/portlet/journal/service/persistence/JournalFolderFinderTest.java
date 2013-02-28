769d906aeb74a93c913330b3feb7512266cc8dc6
ealonso <eudaldo.alonso@liferay.com>
2013-02-28 09:11:09 +0100

    LPS-29962 Fix test

--- portal-impl/test/integration/com/liferay/portlet/journal/service/persistence/JournalFolderFinderTest.java
+++ portal-impl/test/integration/com/liferay/portlet/journal/service/persistence/JournalFolderFinderTest.java
@@ -85,7 +85,7 @@ public class JournalFolderFinderTest {
 		queryDefinition.setStatus(WorkflowConstants.STATUS_IN_TRASH);
 
 		Assert.assertEquals(
-			2,
+			1,
 			JournalFolderFinderUtil.countF_A_ByG_F(
 				_group.getGroupId(), _folder1.getFolderId(), queryDefinition));
 
@@ -130,7 +130,7 @@ public class JournalFolderFinderTest {
 		results = JournalFolderFinderUtil.findF_A_ByG_F(
 			_group.getGroupId(), _folder1.getFolderId(), queryDefinition);
 
-		Assert.assertEquals(2, results.size());
+		Assert.assertEquals(1, results.size());
 
 		for (Object result : results) {
 			if (result instanceof JournalFolder) {
