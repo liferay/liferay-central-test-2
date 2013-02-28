3626b93916a9049d1ba579c5307e329a81fe2558
Brian Chan <brian.chan@liferay.com>
2013-02-27 18:37:50 -0800

    LPS-26127 SF

--- portal-impl/src/com/liferay/portal/servlet/filters/virtualhost/VirtualHostFilter.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/virtualhost/VirtualHostFilter.java
@@ -248,7 +248,7 @@ public class VirtualHostFilter extends BasePortalFilter {
 					layoutSet.getGroupId());
 
 				if (group.isGuest() && friendlyURL.equals(StringPool.SLASH) &&
-						!layoutSet.isPrivateLayout()) {
+					!layoutSet.isPrivateLayout()) {
 
 					String homeURL = PortalUtil.getRelativeHomeURL(request);
 
