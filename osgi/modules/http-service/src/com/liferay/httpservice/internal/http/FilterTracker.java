3138f9997f24d81ef65c6ea12aa4892b79d6ecc0
Miguel Pastor <miguel.pastor@liferay.com>
2013-03-05 09:55:56 +0100

    LPS-32742 Implementation of the trackers for servlets and filters

--- osgi/modules/http-service/src/com/liferay/httpservice/internal/http/FilterTracker.java
+++ osgi/modules/http-service/src/com/liferay/httpservice/internal/http/FilterTracker.java
@@ -14,9 +14,22 @@
 
 package com.liferay.httpservice.internal.http;
 
+import com.liferay.httpservice.HttpServicePropsKeys.Action;
+import com.liferay.httpservice.HttpServicePropsKeys;
+import com.liferay.httpservice.internal.servlet.BundleServletContext;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import java.util.Hashtable;
+import java.util.Map;
+
 import javax.servlet.Filter;
 
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpContext;
 import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
@@ -26,18 +39,94 @@ import org.osgi.util.tracker.ServiceTrackerCustomizer;
 public class FilterTracker implements ServiceTrackerCustomizer<Filter, Filter> {
 
 	public FilterTracker(HttpSupport httpSupport) {
+		_httpSupport = httpSupport;
 	}
 
 	public Filter addingService(ServiceReference<Filter> serviceReference) {
-		return null;
+		BundleContext bundleContext = _httpSupport.getBundleContext();
+
+		Filter filter = bundleContext.getService(serviceReference);
+
+		return doAction(serviceReference, filter, Action.ADDING);
 	}
 
 	public void modifiedService(
 		ServiceReference<Filter> serviceReference, Filter filter) {
+
+		doAction(serviceReference, filter, Action.MODIFIED);
 	}
 
 	public void removedService(
 		ServiceReference<Filter> serviceReference, Filter filter) {
+
+		doAction(serviceReference, filter, Action.REMOVED);
 	}
+
+	protected Filter doAction(
+		ServiceReference<Filter> serviceReference, Filter filter,
+		Action action) {
+
+		String pattern = GetterUtil.getString(
+			serviceReference.getProperty("pattern"));
+
+		Map<String, String> initParameters = new Hashtable<String, String>();
+
+		if (action != Action.REMOVED) {
+			for (String key : serviceReference.getPropertyKeys()) {
+				if (key.startsWith(HttpServicePropsKeys.INIT_PREFIX)) {
+					String value = GetterUtil.getString(
+						serviceReference.getProperty(key));
+
+					initParameters.put(
+						key.substring(
+							HttpServicePropsKeys.INIT_PREFIX.length()), value);
+				}
+			}
+
+			int serviceRanking = GetterUtil.getInteger(
+				serviceReference.getProperty(
+					HttpServicePropsKeys.SERVICE_RANKING));
+
+			initParameters.put(
+				HttpServicePropsKeys.SERVICE_RANKING,
+				String.valueOf(serviceRanking));
+		}
+
+		Bundle bundle = serviceReference.getBundle();
+
+		try {
+			BundleServletContext bundleServletContext =
+				_httpSupport.getBundleServletContext(bundle);
+
+			if (action != Action.ADDING) {
+				bundleServletContext.unregisterFilter(pattern);
+			}
+
+			if (action != Action.REMOVED) {
+				String contextId = GetterUtil.getString(
+					serviceReference.getProperty(
+						HttpServicePropsKeys.CONTEXT_ID));
+
+				HttpContext httpContext = _httpSupport.getHttpContext(
+					contextId);
+
+				if (httpContext == null) {
+					httpContext = bundleServletContext.getHttpContext();
+				}
+
+				bundleServletContext.registerFilter(
+					pattern, filter, initParameters, httpContext);
+			}
+		}
+		catch (Exception e) {
+			_log.error(e);
+		}
+
+		return filter;
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(FilterTracker.class);
+
+	private HttpSupport _httpSupport;
 
 }
--- osgi/modules/http-service/src/com/liferay/httpservice/internal/http/ServletTracker.java
+++ osgi/modules/http-service/src/com/liferay/httpservice/internal/http/ServletTracker.java
@@ -14,9 +14,23 @@
 
 package com.liferay.httpservice.internal.http;
 
+import com.liferay.httpservice.HttpServicePropsKeys.Action;
+import com.liferay.httpservice.HttpServicePropsKeys;
+import com.liferay.httpservice.internal.servlet.BundleServletContext;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.Hashtable;
+import java.util.Map;
+
 import javax.servlet.Servlet;
 
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpContext;
 import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
@@ -27,18 +41,90 @@ public class ServletTracker
 	implements ServiceTrackerCustomizer<Servlet, Servlet> {
 
 	public ServletTracker(HttpSupport httpSupport) {
+		_httpSupport = httpSupport;
 	}
 
 	public Servlet addingService(ServiceReference<Servlet> serviceReference) {
-		return null;
+		BundleContext bundleContext = _httpSupport.getBundleContext();
+
+		Servlet servlet = bundleContext.getService(serviceReference);
+
+		return doAction(serviceReference, servlet, Action.ADDING);
 	}
 
 	public void modifiedService(
 		ServiceReference<Servlet> serviceReference, Servlet servlet) {
+
+		doAction(serviceReference, servlet, Action.MODIFIED);
 	}
 
 	public void removedService(
 		ServiceReference<Servlet> serviceReference, Servlet servlet) {
+
+		doAction(serviceReference, servlet, Action.REMOVED);
 	}
+
+	protected Servlet doAction(
+		ServiceReference<Servlet> serviceReference, Servlet servlet,
+		Action action) {
+
+		String alias = GetterUtil.getString(
+			serviceReference.getProperty(HttpServicePropsKeys.ALIAS));
+
+		if (Validator.isNull(alias)) {
+			return servlet;
+		}
+
+		Map<String, String> initParameters = new Hashtable<String, String>();
+
+		if (action != Action.REMOVED) {
+			for (String key : serviceReference.getPropertyKeys()) {
+				if (key.startsWith(HttpServicePropsKeys.INIT_PREFIX)) {
+					String value = GetterUtil.getString(
+						serviceReference.getProperty(key));
+
+					initParameters.put(
+						key.substring(
+							HttpServicePropsKeys.INIT_PREFIX.length()), value);
+				}
+			}
+		}
+
+		Bundle bundle = serviceReference.getBundle();
+
+		try {
+			BundleServletContext bundleServletContext =
+				_httpSupport.getBundleServletContext(bundle);
+
+			if (action != Action.ADDING) {
+				bundleServletContext.unregisterServlet(alias);
+			}
+
+			if (action != Action.REMOVED) {
+				String contextId = GetterUtil.getString(
+					serviceReference.getProperty(
+						HttpServicePropsKeys.CONTEXT_ID));
+
+				HttpContext httpContext = _httpSupport.getHttpContext(
+					contextId);
+
+				if (httpContext == null) {
+					httpContext = bundleServletContext.getHttpContext();
+				}
+
+				bundleServletContext.registerServlet(
+					alias, servlet, initParameters, httpContext);
+			}
+		}
+		catch (Exception e) {
+			_log.error(e);
+		}
+
+		return servlet;
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(ServletTracker.class);
+
+	private HttpSupport _httpSupport;
 
 }
