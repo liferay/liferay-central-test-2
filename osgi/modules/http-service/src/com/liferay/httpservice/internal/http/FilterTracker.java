f2211ef13bb134366502aae8ee4cb65ebbfcd942
Brian Chan <brian.chan@liferay.com>
2013-03-05 16:02:35 -0800

    LPS-32742 1.) Move common logic for FilterTracker and ServletTracker into BaseServiceTrackerCustomizer 2.) Remove logic from BundleServletContext because I think it's fundamentally wrong. This is similar to the bug I fixed in WebXMLDefinitionLoader. Each "filter" and "servlet" is guaranteed by the servlet spec to have only one instance, but each instance (by filter name and servlet alias) can have multiple mappings. We should not map filters by mappings and servlets by aliases. Please fix *Tracker to both be consistent with either mappings or names (aliases). I already fixed WebXMLDefinitionLoader to ensure that there is only one instance per name, mapped to multiple mappings vs multiple instances to multiple mappings.

--- osgi/modules/http-service/src/com/liferay/httpservice/internal/http/FilterTracker.java
+++ osgi/modules/http-service/src/com/liferay/httpservice/internal/http/FilterTracker.java
@@ -15,17 +15,12 @@
 package com.liferay.httpservice.internal.http;
 
 import com.liferay.httpservice.internal.servlet.BundleServletContext;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.Filter;
 
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.http.HttpContext;
 
@@ -37,88 +32,32 @@ public class FilterTracker extends
 	BaseServiceTrackerCustomizer<Filter, Filter> {
 
 	public FilterTracker(HttpSupport httpSupport) {
-		_httpSupport = httpSupport;
+		super(httpSupport);
 	}
 
-	public Filter addingService(ServiceReference<Filter> serviceReference) {
-		BundleContext bundleContext = _httpSupport.getBundleContext();
-
-		Filter filter = bundleContext.getService(serviceReference);
-
-		return doAction(serviceReference, filter, ACTION_ADDING);
-	}
-
-	public void modifiedService(
-		ServiceReference<Filter> serviceReference, Filter filter) {
-
-		doAction(serviceReference, filter, ACTION_MODIFIED);
-	}
-
-	public void removedService(
-		ServiceReference<Filter> serviceReference, Filter filter) {
-
-		doAction(serviceReference, filter, ACTION_REMOVED);
-	}
-
-	protected Filter doAction(
-		ServiceReference<Filter> serviceReference, Filter filter, int action) {
+	@Override
+	protected void registerService(
+			BundleServletContext bundleServletContext,
+			ServiceReference<Filter> serviceReference, Filter filter,
+			Map<String, String> initParameters, HttpContext httpContext)
+		throws Exception {
 
 		String pattern = GetterUtil.getString(
 			serviceReference.getProperty("pattern"));
 
-		Map<String, String> initParameters = new HashMap<String, String>();
-
-		if (action != ACTION_REMOVED) {
-			for (String key : serviceReference.getPropertyKeys()) {
-				if (key.startsWith("init.")) {
-					String value = GetterUtil.getString(
-						serviceReference.getProperty(key));
-
-					initParameters.put(key.substring(5), value);
-				}
-			}
-
-			int serviceRanking = GetterUtil.getInteger(
-				serviceReference.getProperty("service.ranking"));
-
-			initParameters.put(
-				"service.ranking", String.valueOf(serviceRanking));
-		}
-
-		Bundle bundle = serviceReference.getBundle();
-
-		try {
-			BundleServletContext bundleServletContext =
-				_httpSupport.getBundleServletContext(bundle);
-
-			if (action != ACTION_ADDING) {
-				bundleServletContext.unregisterFilter(pattern);
-			}
-
-			if (action != ACTION_REMOVED) {
-				String contextId = GetterUtil.getString(
-					serviceReference.getProperty("contextId"));
-
-				HttpContext httpContext = _httpSupport.getHttpContext(
-					contextId);
-
-				if (httpContext == null) {
-					httpContext = bundleServletContext.getHttpContext();
-				}
-
-				bundleServletContext.registerFilter(
-					pattern, filter, initParameters, httpContext);
-			}
-		}
-		catch (Exception e) {
-			_log.error(e);
-		}
-
-		return filter;
+		bundleServletContext.registerFilter(
+			pattern, filter, initParameters, httpContext);
 	}
 
-	private static Log _log = LogFactoryUtil.getLog(FilterTracker.class);
+	@Override
+	protected void unregisterService(
+		BundleServletContext bundleServletContext,
+		ServiceReference<Filter> serviceReference) {
 
-	private HttpSupport _httpSupport;
+		String pattern = GetterUtil.getString(
+			serviceReference.getProperty("pattern"));
+
+		bundleServletContext.unregisterFilter(pattern);
+	}
 
 }
--- osgi/modules/http-service/src/com/liferay/httpservice/internal/http/ServletTracker.java
+++ osgi/modules/http-service/src/com/liferay/httpservice/internal/http/ServletTracker.java
@@ -15,18 +15,12 @@
 package com.liferay.httpservice.internal.http;
 
 import com.liferay.httpservice.internal.servlet.BundleServletContext;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.Validator;
 
-import java.util.Hashtable;
 import java.util.Map;
 
 import javax.servlet.Servlet;
 
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.http.HttpContext;
 
@@ -38,87 +32,32 @@ public class ServletTracker
 	extends BaseServiceTrackerCustomizer<Servlet, Servlet> {
 
 	public ServletTracker(HttpSupport httpSupport) {
-		_httpSupport = httpSupport;
+		super(httpSupport);
 	}
 
-	public Servlet addingService(ServiceReference<Servlet> serviceReference) {
-		BundleContext bundleContext = _httpSupport.getBundleContext();
-
-		Servlet servlet = bundleContext.getService(serviceReference);
-
-		return doAction(serviceReference, servlet, ACTION_ADDING);
-	}
-
-	public void modifiedService(
-		ServiceReference<Servlet> serviceReference, Servlet servlet) {
-
-		doAction(serviceReference, servlet, ACTION_MODIFIED);
-	}
-
-	public void removedService(
-		ServiceReference<Servlet> serviceReference, Servlet servlet) {
-
-		doAction(serviceReference, servlet, ACTION_REMOVED);
-	}
-
-	protected Servlet doAction(
-		ServiceReference<Servlet> serviceReference, Servlet servlet,
-		int action) {
+	@Override
+	protected void registerService(
+			BundleServletContext bundleServletContext,
+			ServiceReference<Servlet> serviceReference, Servlet servlet,
+			Map<String, String> initParameters, HttpContext httpContext)
+		throws Exception {
 
 		String alias = GetterUtil.getString(
 			serviceReference.getProperty("alias"));
 
-		if (Validator.isNull(alias)) {
-			return servlet;
-		}
-
-		Map<String, String> initParameters = new Hashtable<String, String>();
-
-		if (action != ACTION_REMOVED) {
-			for (String key : serviceReference.getPropertyKeys()) {
-				if (key.startsWith("init.")) {
-					String value = GetterUtil.getString(
-						serviceReference.getProperty(key));
-
-					initParameters.put(key.substring(5), value);
-				}
-			}
-		}
-
-		Bundle bundle = serviceReference.getBundle();
-
-		try {
-			BundleServletContext bundleServletContext =
-				_httpSupport.getBundleServletContext(bundle);
-
-			if (action != ACTION_ADDING) {
-				bundleServletContext.unregisterServlet(alias);
-			}
-
-			if (action != ACTION_REMOVED) {
-				String contextId = GetterUtil.getString(
-					serviceReference.getProperty("contextId"));
-
-				HttpContext httpContext = _httpSupport.getHttpContext(
-					contextId);
-
-				if (httpContext == null) {
-					httpContext = bundleServletContext.getHttpContext();
-				}
-
-				bundleServletContext.registerServlet(
-					alias, servlet, initParameters, httpContext);
-			}
-		}
-		catch (Exception e) {
-			_log.error(e);
-		}
-
-		return servlet;
+		bundleServletContext.registerServlet(
+			alias, servlet, initParameters, httpContext);
 	}
 
-	private static Log _log = LogFactoryUtil.getLog(ServletTracker.class);
+	@Override
+	protected void unregisterService(
+		BundleServletContext bundleServletContext,
+		ServiceReference<Servlet> serviceReference) {
 
-	private HttpSupport _httpSupport;
+		String alias = GetterUtil.getString(
+			serviceReference.getProperty("alias"));
+
+		bundleServletContext.unregisterServlet(alias);
+	}
 
 }
