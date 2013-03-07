sergiogonzalez/liferay-portal

LPS-33079 Frontend related files refactor

portal-web/docroot/html/portlet/sites_admin/select_site.jsp
@@ -127,7 +127,7 @@ if (selUser != null) {
 			<%
 			String rowHREF = null;
 
-			if (MembershipPolicyUtil.isMembershipAllowed(group, selUser)) {
+			if (SiteMembershipPolicyUtil.isMembershipAllowed(selUser.getGroupId(), group.getGroupId())) {
 				StringBundler sb = new StringBundler(10);
 
 				sb.append("javascript:opener.");

liferay/liferay-central-test-2

portal-web/docroot/html/portlet/sites_admin/select_site.jsp
@@ -127,7 +127,7 @@ if (selUser != null) {
 			<%
 			String rowHREF = null;
 
-			if (MembershipPolicyUtil.isMembershipAllowed(group, selUser)) {
+			if (SiteMembershipPolicyUtil.isMembershipAllowed(selUser.getUserId(), group.getGroupId())) {
 				StringBundler sb = new StringBundler(10);
 
 				sb.append("javascript:opener.");