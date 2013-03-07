aeabfc932da09ee0cf8ac50118ef8d4d49625de0
Miguel Pastor <miguel.pastor@liferay.com>
2013-03-07 10:47:39 +0100

    LPS-32742 Include the new behaviour added to the ExtHttpService. We are allowing to register multiple servlet mappings under the same servlet instance (as Servlet specs dictates). This is an extension to the HttpService (which is worth to extend since we are trying to implement the WAB specs). We store a map where multiple alias maps to the unique servlet instance in order to do an efficient lookup when doing the dispatching (which will be pushed as soon as possible)

--- osgi/modules/http-service/src/com/liferay/httpservice/internal/servlet/BundleServletContext.java
+++ osgi/modules/http-service/src/com/liferay/httpservice/internal/servlet/BundleServletContext.java
@@ -14,6 +14,7 @@
 
 package com.liferay.httpservice.internal.servlet;
 
+import com.liferay.httpservice.servlet.BundleServletConfig;
 import com.liferay.portal.apache.bridges.struts.LiferayServletContext;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -27,19 +28,24 @@ import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.kernel.xml.SAXReaderUtil;
 import com.liferay.portal.util.PortalUtil;
 
+import java.util.Arrays;
 import java.util.Dictionary;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.servlet.Filter;
 import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.ServletRequestAttributeListener;
 import javax.servlet.ServletRequestListener;
 
 import org.osgi.framework.Bundle;
 import org.osgi.service.http.HttpContext;
+import org.osgi.service.http.NamespaceException;
 
 /**
  * @author Raymond Augé
@@ -125,6 +131,7 @@ public class BundleServletContext extends LiferayServletContext {
 		super(servletContext);
 
 		_bundle = bundle;
+		_servletContextName = servletContextName;
 	}
 
 	public void close() {
@@ -142,6 +149,10 @@ public class BundleServletContext extends LiferayServletContext {
 		return null;
 	}
 
+	public String getServletContextName() {
+		return _servletContextName;
+	}
+
 	public List<ServletRequestAttributeListener>
 		getServletRequestAttributeListeners() {
 
@@ -156,16 +167,48 @@ public class BundleServletContext extends LiferayServletContext {
 	}
 
 	public void registerFilter(
-		String filterMapping, Filter filter, Map<String, String> initParameters,
-		HttpContext httpContext) {
+		String filterMapping, Filter filter,
+		Dictionary<String, String> initParameters, HttpContext httpContext) {
 	}
 
 	public void registerServlet(
-		String alias, Servlet servlet, Map<String, String> initParameters,
-		HttpContext httpContext) {
+			String servletName, List<String> aliases, Servlet servlet,
+			Dictionary<String, String> initParameters, HttpContext httpContext)
+		throws NamespaceException, ServletException {
+
+		_validate(servlet, aliases, httpContext);
+
+		Thread currentThread = Thread.currentThread();
+
+		ClassLoader contextClassLoader = currentThread.getContextClassLoader();
+
+		try {
+			currentThread.setContextClassLoader(getClassLoader());
+
+			ServletConfig servletConfig = new BundleServletConfig(
+				this, servletName, initParameters, httpContext);
+
+			servlet.init(servletConfig);
+
+			_registerServlet(servlet, aliases);
+		}
+		finally {
+			currentThread.setContextClassLoader(contextClassLoader);
+		}
+	}
+
+	public void registerServlet(
+			String alias, Servlet servlet,
+			Dictionary<String, String> initParameters, HttpContext httpContext)
+		throws NamespaceException, ServletException {
+
+		List<String> aliases = Arrays.asList(alias);
+
+		registerServlet(alias, aliases, servlet, initParameters, httpContext);
 	}
 
 	public void setServletContextName(String servletContextName) {
+		_servletContextName = servletContextName;
 	}
 
 	public void unregisterFilter(String filterMapping) {
@@ -174,8 +217,61 @@ public class BundleServletContext extends LiferayServletContext {
 	public void unregisterServlet(String alias) {
 	}
 
+	private void _registerServlet(Servlet servlet, List<String> aliases) {
+		for (String alias : aliases) {
+			_servletsMap.put(alias, servlet);
+
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					"Registered servlet at " + getContextPath().concat(alias));
+			}
+		}
+	}
+
+	private void _validate(
+			Servlet servlet, List<String> aliases, HttpContext httpContext)
+		throws NamespaceException {
+
+		for (String alias : aliases) {
+			_validate(alias);
+		}
+
+		if (servlet == null) {
+			throw new IllegalArgumentException("Servlet must not be null");
+		}
+
+		if (_servletsMap.containsValue(servlet)) {
+			throw new IllegalArgumentException("Servlet is already registered");
+		}
+
+		if (httpContext == null) {
+			throw new IllegalArgumentException("HttpContext cannot be null");
+		}
+	}
+
+	private void _validate(String alias) throws NamespaceException {
+		if (Validator.isNull(alias)) {
+			throw new IllegalArgumentException("Empty aliases are not allowed");
+		}
+
+		if (!alias.startsWith(StringPool.SLASH) ||
+			(alias.endsWith(StringPool.SLASH) &&
+			 !alias.equals(StringPool.SLASH))) {
+
+			throw new IllegalArgumentException(
+				"Alias must start with / but must not end with it");
+		}
+
+		if (_servletsMap.containsKey(alias)) {
+			throw new NamespaceException("Alias " + alias + " already exists");
+		}
+	}
+
 	private static Log _log = LogFactoryUtil.getLog(BundleServletContext.class);
 
 	private Bundle _bundle;
+	private String _servletContextName;
+	private Map<String, Servlet> _servletsMap =
+		new ConcurrentHashMap<String, Servlet>();
 
 }
