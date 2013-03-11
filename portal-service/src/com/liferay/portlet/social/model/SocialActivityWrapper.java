juliocamarero/liferay-portal

LPS-33093 Generated

portal-service/src/com/liferay/portlet/social/model/SocialActivityWrapper.java
@@ -515,6 +515,15 @@ public class SocialActivityWrapper implements SocialActivity,
 		return _socialActivity.getAssetEntry();
 	}
 
+	public java.lang.String getExtraDataProperty(java.lang.String property)
+		throws com.liferay.portal.kernel.json.JSONException {
+		return _socialActivity.getExtraDataProperty(property);
+	}
+
+	public boolean isClassName(java.lang.String className) {
+		return _socialActivity.isClassName(className);
+	}
+
 	public void setAssetEntry(
 		com.liferay.portlet.asset.model.AssetEntry assetEntry) {
 		_socialActivity.setAssetEntry(assetEntry);

liferay/liferay-central-test-2

portal-service/src/com/liferay/portlet/social/model/SocialActivityWrapper.java
@@ -515,6 +515,15 @@ public class SocialActivityWrapper implements SocialActivity,
 		return _socialActivity.getAssetEntry();
 	}
 
+	public java.lang.String getExtraDataValue(java.lang.String key)
+		throws com.liferay.portal.kernel.json.JSONException {
+		return _socialActivity.getExtraDataValue(key);
+	}
+
+	public boolean isClassName(java.lang.String className) {
+		return _socialActivity.isClassName(className);
+	}
+
 	public void setAssetEntry(
 		com.liferay.portlet.asset.model.AssetEntry assetEntry) {
 		_socialActivity.setAssetEntry(assetEntry);