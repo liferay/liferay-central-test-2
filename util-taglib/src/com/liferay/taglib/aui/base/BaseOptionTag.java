24a3fc5ba2220577d68c3f445ad03a3eb1042ed1
Sergio González <sergio.gonzalez@liferay.com>
2013-03-11 09:53:22 +0100

    LPS-33145 Set the default value to false, as it was before. It was accidentally changed in ticket LPS-28841

--- util-taglib/src/com/liferay/taglib/aui/base/BaseOptionTag.java
+++ util-taglib/src/com/liferay/taglib/aui/base/BaseOptionTag.java
@@ -121,7 +121,7 @@ public class BaseOptionTag extends com.liferay.taglib.util.IncludeTag {
 		_label = null;
 		_selected = false;
 		_style = null;
-		_useModelValue = false;
+		_useModelValue = true;
 		_value = null;
 	}
 
@@ -161,7 +161,7 @@ public class BaseOptionTag extends com.liferay.taglib.util.IncludeTag {
 	private java.lang.Object _label = null;
 	private boolean _selected = false;
 	private java.lang.String _style = null;
-	private boolean _useModelValue = false;
+	private boolean _useModelValue = true;
 	private java.lang.Object _value = null;
 
 }
