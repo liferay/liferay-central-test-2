869f411bf332976ab217486df1fe32175999a6f3
KocsisDaniel <daniel.kocsis@liferay.com>
2013-03-12 16:34:21 +0100

    LPS-33087 Adding new staged model data handlers skeletons

---
+++ portal-impl/src/com/liferay/portlet/messageboards/lar/MBThreadFlagStagedModelDataHandler.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.messageboards.lar;
+
+import com.liferay.portal.kernel.lar.BaseStagedModelDataHandler;
+import com.liferay.portal.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portlet.messageboards.model.MBThreadFlag;
+
+/**
+ * @author Daniel Kocsis
+ */
+public class MBThreadFlagStagedModelDataHandler
+	extends BaseStagedModelDataHandler<MBThreadFlag> {
+
+	@Override
+	public String getClassName() {
+		return MBThreadFlag.class.getName();
+	}
+
+	@Override
+	protected void doExportStagedModel(
+			PortletDataContext portletDataContext, Element[] elements,
+			MBThreadFlag threadFlag)
+		throws Exception {
+
+		return;
+	}
+
+	@Override
+	protected void doImportStagedModel(
+			PortletDataContext portletDataContext, Element element, String path,
+			MBThreadFlag threadFlag)
+		throws Exception {
+
+		return;
+	}
+
+}
