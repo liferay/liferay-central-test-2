abdb590eca87db120fd0112af34980618b89766b
Raymond Auge <raymond.auge@liferay.com>
2013-03-13 16:19:26 -0400

    LPS-33700 PACL - centralize PortalMessageBusPermission checking

--- portal-service/src/com/liferay/portal/kernel/security/pacl/permission/PortalMessageBusPermission.java
+++ portal-service/src/com/liferay/portal/kernel/security/pacl/permission/PortalMessageBusPermission.java
@@ -14,12 +14,29 @@
 
 package com.liferay.portal.kernel.security.pacl.permission;
 
+import com.liferay.portal.kernel.security.pacl.PACLConstants;
+
 import java.security.BasicPermission;
+import java.security.Permission;
 
 /**
  * @author Brian Wing Shun Chan
  */
 public class PortalMessageBusPermission extends BasicPermission {
+
+	public static void checkPermission(String destinationName) {
+		SecurityManager securityManager = System.getSecurityManager();
+
+		if (securityManager == null) {
+			return;
+		}
+
+		Permission permission = new PortalMessageBusPermission(
+			PACLConstants.PORTAL_MESSAGE_BUS_PERMISSION_SEND,
+			destinationName);
+
+		securityManager.checkPermission(permission);
+	}
 
 	public PortalMessageBusPermission(String name, String destinationName) {
 		super(name);
