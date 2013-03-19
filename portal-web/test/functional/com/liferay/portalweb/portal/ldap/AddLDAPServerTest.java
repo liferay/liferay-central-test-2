c525104ad4c5f21edea8b7c8af08a7ca79a12ac2
Kiyoshi Lee <kiyoshi.lee@liferay.com>
2013-03-19 15:17:47 -0700

    LPS-34018

--- portal-web/test/functional/com/liferay/portalweb/portal/ldap/AddLDAPServerTest.java
+++ portal-web/test/functional/com/liferay/portalweb/portal/ldap/AddLDAPServerTest.java
@@ -57,7 +57,7 @@ public class AddLDAPServerTest extends BaseTestCase {
 				"//input[@name='_130_defaultLdap' and @value='apache']"));
 		selenium.click("//input[@value='Reset Values']");
 		selenium.type("//input[@id='_130_ldap.base.provider.url.0']",
-			RuntimeVariables.replace("ldap://[$VM_HOST$]:10389"));
+			RuntimeVariables.replace("ldap://localhost:10389"));
 		selenium.type("//input[@id='_130_ldap.base.dn.0']",
 			RuntimeVariables.replace("dc=example,dc=com"));
 		selenium.type("//input[@id='_130_ldap.security.principal.0']",
