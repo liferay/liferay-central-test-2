e9a57c93446124c631973b235e6907962d839a59
Raymond Auge <raymond.auge@liferay.com>
2013-03-20 16:43:09 -0400

    LPS-33899 PACL - auto proxy finder and persistence when PACL is enabled

--- portal-impl/src/com/liferay/portal/spring/aop/ServiceBeanAutoProxyCreator.java
+++ portal-impl/src/com/liferay/portal/spring/aop/ServiceBeanAutoProxyCreator.java
@@ -15,6 +15,7 @@
 package com.liferay.portal.spring.aop;
 
 import com.liferay.portal.kernel.spring.aop.Skip;
+import com.liferay.portal.security.lang.SecurityManagerUtil;
 
 import org.aopalliance.intercept.MethodInterceptor;
 
@@ -77,7 +78,11 @@ public class ServiceBeanAutoProxyCreator
 
 		Object[] advices = DO_NOT_PROXY;
 
-		if (beanName.endsWith(_SERVICE_SUFFIX)) {
+		if ((!SecurityManagerUtil.isPACLDisabled() &&
+			 (beanName.endsWith(_FINDER_SUFFIX) ||
+			  beanName.endsWith(_PERSISTENCE_SUFFIX))) ||
+			beanName.endsWith(_SERVICE_SUFFIX)) {
+
 			advices = super.getAdvicesAndAdvisorsForBean(
 				beanClass, beanName, targetSource);
 
@@ -89,6 +94,8 @@ public class ServiceBeanAutoProxyCreator
 		return advices;
 	}
 
+	private static final String _FINDER_SUFFIX = "Finder";
+	private static final String _PERSISTENCE_SUFFIX = "Persistence";
 	private static final String _SERVICE_SUFFIX = "Service";
 
 	private MethodInterceptor _methodInterceptor;
