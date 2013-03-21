8a082a1d08f96a885c942bae8b56c45aba5cc1cd
Raymond Auge <raymond.auge@liferay.com>
2013-03-21 14:58:19 -0400

    LPS-33894 PACL - inject pacl into ServiceBeanAopProxy

--- portal-impl/src/com/liferay/portal/spring/aop/ServiceBeanAopProxy.java
+++ portal-impl/src/com/liferay/portal/spring/aop/ServiceBeanAopProxy.java
@@ -125,7 +125,11 @@ public class ServiceBeanAopProxy implements AopProxy, InvocationHandler {
 		Class<?>[] proxiedInterfaces = AopProxyUtils.completeProxiedInterfaces(
 			_advisedSupport);
 
-		return ProxyUtil.newProxyInstance(classLoader, proxiedInterfaces, this);
+		InvocationHandler invocationHandler = _pacl.getInvocationHandler(
+			this, _advisedSupport);
+
+		return ProxyUtil.newProxyInstance(
+			classLoader, proxiedInterfaces, invocationHandler);
 	}
 
 	public Object invoke(Object proxy, Method method, Object[] arguments)
@@ -236,11 +240,31 @@ public class ServiceBeanAopProxy implements AopProxy, InvocationHandler {
 
 	private static Log _log = LogFactoryUtil.getLog(ServiceBeanAopProxy.class);
 
+	private static PACL _pacl = new NoPACL();
+
 	private AdvisedSupport _advisedSupport;
 	private AdvisorChainFactory _advisorChainFactory;
 	private final List<MethodInterceptor> _classLevelMethodInterceptors;
 	private final List<MethodInterceptor> _fullMethodInterceptors;
 	private boolean _mergeSpringMethodInterceptors;
 	private ServiceBeanAopCacheManager _serviceBeanAopCacheManager;
+
+	public static interface PACL {
+
+		public InvocationHandler getInvocationHandler(
+			InvocationHandler invocationHandler, AdvisedSupport advisedSupport);
+
+	}
+
+	private static class NoPACL implements PACL {
+
+		public InvocationHandler getInvocationHandler(
+			InvocationHandler invocationHandler,
+			AdvisedSupport advisedSupport) {
+
+			return invocationHandler;
+		}
+
+	}
 
 }
