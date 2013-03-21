5202c20f73c31076e52ffae073e09c9ee88a2708
Albert Lee <albert.lee@liferay.com>
2013-03-21 07:44:36 -0700

    Test Fixes - properties/messageboards/anonymousposting

--- portal-web/test/functional/com/liferay/portalweb/properties/messageboards/anonymousposting/viewmbanonymouspostingno/ViewMBAnonymousPostingNoTest.java
+++ portal-web/test/functional/com/liferay/portalweb/properties/messageboards/anonymousposting/viewmbanonymouspostingno/ViewMBAnonymousPostingNoTest.java
@@ -31,8 +31,7 @@ public class ViewMBAnonymousPostingNoTest extends BaseTestCase {
 		selenium.clickAt("//input[@value='Post New Thread']",
 			RuntimeVariables.replace("Post New Thread"));
 		selenium.waitForPageToLoad("30000");
-		selenium.waitForElementPresent(
-			"//iframe[contains(@title,'Rich Text Editor')]");
+		selenium.waitForVisible("//iframe[contains(@title,'Rich Text Editor')]");
 		assertFalse(selenium.isTextPresent("Anonymous"));
 		assertFalse(selenium.isTextPresent(
 				"If this is checked, then this message will be posted anonymously."));
