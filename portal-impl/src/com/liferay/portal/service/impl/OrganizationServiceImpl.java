16469edc0f35601040e9c3eb04a791fbff100e75
Brian Chan <brian.chan@liferay.com>
2013-03-21 15:07:45 -0700

    LPS-33915 Ran "ant format-javadoc -Dlimit=OrganizationServiceImpl"

--- portal-impl/src/com/liferay/portal/service/impl/OrganizationServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/OrganizationServiceImpl.java
@@ -87,32 +87,32 @@ public class OrganizationServiceImpl extends OrganizationServiceBaseImpl {
 	 * including its resources, metadata, and internal data structures.
 	 * </p>
 	 *
-	 * @param  parentOrganizationId the primary key of the organization's parent
-	 *         organization
-	 * @param  name the organization's name
-	 * @param  type the organization's type
-	 * @param  recursable whether the permissions of the organization are to be
-	 *         inherited by its suborganizations
-	 * @param  regionId the primary key of the organization's region
-	 * @param  countryId the primary key of the organization's country
-	 * @param  statusId the organization's workflow status
-	 * @param  comments the comments about the organization
-	 * @param  site whether the organization is to be associated with a main
-	 *         site
-	 * @param  addresses the organization's addresses
-	 * @param  emailAddresses the organization's email addresses
-	 * @param  orgLabors the organization's hours of operation
-	 * @param  phones the organization's phone numbers
-	 * @param  websites the organization's websites
-	 * @param  serviceContext the service context to be applied (optionally
-	 *         <code>null</code>). Can set asset category IDs, asset tag names,
-	 *         and expando bridge attributes for the organization.
-	 * @return the organization
-	 * @throws PortalException if a parent organization with the primary key
-	 *         could not be found, if the organization's information was
-	 *         invalid, or if the user did not have permission to add the
-	 *         organization
-	 * @throws SystemException if a system exception occurred
+	 * @param      parentOrganizationId the primary key of the organization's
+	 *             parent organization
+	 * @param      name the organization's name
+	 * @param      type the organization's type
+	 * @param      recursable whether the permissions of the organization are to
+	 *             be inherited by its suborganizations
+	 * @param      regionId the primary key of the organization's region
+	 * @param      countryId the primary key of the organization's country
+	 * @param      statusId the organization's workflow status
+	 * @param      comments the comments about the organization
+	 * @param      site whether the organization is to be associated with a main
+	 *             site
+	 * @param      addresses the organization's addresses
+	 * @param      emailAddresses the organization's email addresses
+	 * @param      orgLabors the organization's hours of operation
+	 * @param      phones the organization's phone numbers
+	 * @param      websites the organization's websites
+	 * @param      serviceContext the service context to be applied (optionally
+	 *             <code>null</code>). Can set asset category IDs, asset tag
+	 *             names, and expando bridge attributes for the organization.
+	 * @return     the organization
+	 * @throws     PortalException if a parent organization with the primary key
+	 *             could not be found, if the organization's information was
+	 *             invalid, or if the user did not have permission to add the
+	 *             organization
+	 * @throws     SystemException if a system exception occurred
 	 * @deprecated As of 6.2.0, replaced by {@link #addOrganization(long,
 	 *             String, String, long, long, int, String, boolean,
 	 *             java.util.List, java.util.List, java.util.List,
@@ -141,26 +141,27 @@ public class OrganizationServiceImpl extends OrganizationServiceBaseImpl {
 	 * including its resources, metadata, and internal data structures.
 	 * </p>
 	 *
-	 * @param  parentOrganizationId the primary key of the organization's parent
-	 *         organization
-	 * @param  name the organization's name
-	 * @param  type the organization's type
-	 * @param  recursable whether the permissions of the organization are to be
-	 *         inherited by its suborganizations
-	 * @param  regionId the primary key of the organization's region
-	 * @param  countryId the primary key of the organization's country
-	 * @param  statusId the organization's workflow status
-	 * @param  comments the comments about the organization
-	 * @param  site whether the organization is to be associated with a main
-	 *         site
-	 * @param  serviceContext the service context to be applied (optionally
-	 *         <code>null</code>). Can set asset category IDs, asset tag names,
-	 *         and expando bridge attributes for the organization.
-	 * @return the organization
-	 * @throws PortalException if the parent organization with the primary key
-	 *         could not be found, if the organization information was invalid,
-	 *         or if the user did not have permission to add the organization
-	 * @throws SystemException if a system exception occurred
+	 * @param      parentOrganizationId the primary key of the organization's
+	 *             parent organization
+	 * @param      name the organization's name
+	 * @param      type the organization's type
+	 * @param      recursable whether the permissions of the organization are to
+	 *             be inherited by its suborganizations
+	 * @param      regionId the primary key of the organization's region
+	 * @param      countryId the primary key of the organization's country
+	 * @param      statusId the organization's workflow status
+	 * @param      comments the comments about the organization
+	 * @param      site whether the organization is to be associated with a main
+	 *             site
+	 * @param      serviceContext the service context to be applied (optionally
+	 *             <code>null</code>). Can set asset category IDs, asset tag
+	 *             names, and expando bridge attributes for the organization.
+	 * @return     the organization
+	 * @throws     PortalException if the parent organization with the primary
+	 *             key could not be found, if the organization information was
+	 *             invalid, or if the user did not have permission to add the
+	 *             organization
+	 * @throws     SystemException if a system exception occurred
 	 * @deprecated As of 6.2.0, replaced by {@link #addOrganization(long,
 	 *             String, String, long, long, int, String, boolean,
 	 *             ServiceContext)}
@@ -615,34 +616,34 @@ public class OrganizationServiceImpl extends OrganizationServiceBaseImpl {
 	/**
 	 * Updates the organization with additional parameters.
 	 *
-	 * @param  organizationId the primary key of the organization
-	 * @param  parentOrganizationId the primary key of the organization's parent
-	 *         organization
-	 * @param  name the organization's name
-	 * @param  type the organization's type
-	 * @param  recursable whether the permissions of the organization are to be
-	 *         inherited by its suborganizations
-	 * @param  regionId the primary key of the organization's region
-	 * @param  countryId the primary key of the organization's country
-	 * @param  statusId the organization's workflow status
-	 * @param  comments the comments about the organization
-	 * @param  site whether the organization is to be associated with a main
-	 *         site
-	 * @param  addresses the organization's addresses
-	 * @param  emailAddresses the organization's email addresses
-	 * @param  orgLabors the organization's hours of operation
-	 * @param  phones the organization's phone numbers
-	 * @param  websites the organization's websites
-	 * @param  serviceContext the service context to be applied (optionally
-	 *         <code>null</code>). Can set asset category IDs and asset tag
-	 *         names for the organization, and merge expando bridge attributes
-	 *         for the organization.
-	 * @return the organization
-	 * @throws PortalException if an organization or parent organization with
-	 *         the primary key could not be found, if the user did not have
-	 *         permission to update the organization information, or if the new
-	 *         information was invalid
-	 * @throws SystemException if a system exception occurred
+	 * @param      organizationId the primary key of the organization
+	 * @param      parentOrganizationId the primary key of the organization's
+	 *             parent organization
+	 * @param      name the organization's name
+	 * @param      type the organization's type
+	 * @param      recursable whether the permissions of the organization are to
+	 *             be inherited by its suborganizations
+	 * @param      regionId the primary key of the organization's region
+	 * @param      countryId the primary key of the organization's country
+	 * @param      statusId the organization's workflow status
+	 * @param      comments the comments about the organization
+	 * @param      site whether the organization is to be associated with a main
+	 *             site
+	 * @param      addresses the organization's addresses
+	 * @param      emailAddresses the organization's email addresses
+	 * @param      orgLabors the organization's hours of operation
+	 * @param      phones the organization's phone numbers
+	 * @param      websites the organization's websites
+	 * @param      serviceContext the service context to be applied (optionally
+	 *             <code>null</code>). Can set asset category IDs and asset tag
+	 *             names for the organization, and merge expando bridge
+	 *             attributes for the organization.
+	 * @return     the organization
+	 * @throws     PortalException if an organization or parent organization
+	 *             with the primary key could not be found, if the user did not
+	 *             have permission to update the organization information, or if
+	 *             the new information was invalid
+	 * @throws     SystemException if a system exception occurred
 	 * @deprecated As of 6.2.0, replaced by {@link #updateOrganization(long,
 	 *             long, String, String, long, long, int, String, boolean,
 	 *             java.util.List, java.util.List, java.util.List,
@@ -666,29 +667,29 @@ public class OrganizationServiceImpl extends OrganizationServiceBaseImpl {
 	/**
 	 * Updates the organization.
 	 *
-	 * @param  organizationId the primary key of the organization
-	 * @param  parentOrganizationId the primary key of the organization's parent
-	 *         organization
-	 * @param  name the organization's name
-	 * @param  type the organization's type
-	 * @param  recursable whether permissions of the organization are to be
-	 *         inherited by its suborganizations
-	 * @param  regionId the primary key of the organization's region
-	 * @param  countryId the primary key of the organization's country
-	 * @param  statusId the organization's workflow status
-	 * @param  comments the comments about the organization
-	 * @param  site whether the organization is to be associated with a main
-	 *         site
-	 * @param  serviceContext the service context to be applied (optionally
-	 *         <code>null</code>). Can set asset category IDs and asset tag
-	 *         names for the organization, and merge expando bridge attributes
-	 *         for the organization.
-	 * @return the organization
-	 * @throws PortalException if an organization or parent organization with
-	 *         the primary key could not be found, if the user did not have
-	 *         permission to update the organization, or if the new information
-	 *         was invalid
-	 * @throws SystemException if a system exception occurred
+	 * @param      organizationId the primary key of the organization
+	 * @param      parentOrganizationId the primary key of the organization's
+	 *             parent organization
+	 * @param      name the organization's name
+	 * @param      type the organization's type
+	 * @param      recursable whether permissions of the organization are to be
+	 *             inherited by its suborganizations
+	 * @param      regionId the primary key of the organization's region
+	 * @param      countryId the primary key of the organization's country
+	 * @param      statusId the organization's workflow status
+	 * @param      comments the comments about the organization
+	 * @param      site whether the organization is to be associated with a main
+	 *             site
+	 * @param      serviceContext the service context to be applied (optionally
+	 *             <code>null</code>). Can set asset category IDs and asset tag
+	 *             names for the organization, and merge expando bridge
+	 *             attributes for the organization.
+	 * @return     the organization
+	 * @throws     PortalException if an organization or parent organization
+	 *             with the primary key could not be found, if the user did not
+	 *             have permission to update the organization, or if the new
+	 *             information was invalid
+	 * @throws     SystemException if a system exception occurred
 	 * @deprecated As of 6.2.0, replaced by {@link #updateOrganization(long,
 	 *             long, String, String, long, long, int, String, boolean,
 	 *             ServiceContext)}
