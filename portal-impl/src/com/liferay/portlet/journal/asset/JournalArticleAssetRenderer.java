7fdf6cdec441d998bde8709b770be16bf3cc8aa3
ealonso <eudaldo.alonso@liferay.com>
2013-03-25 16:05:10 +0100

    LPS-31753 Display Page for web content does not work in Asset Publisher

--- portal-impl/src/com/liferay/portlet/journal/asset/JournalArticleAssetRenderer.java
+++ portal-impl/src/com/liferay/portlet/journal/asset/JournalArticleAssetRenderer.java
@@ -177,32 +177,31 @@ public class JournalArticleAssetRenderer
 
 		Layout layout = themeDisplay.getLayout();
 
-		if (Validator.isNotNull(_article.getLayoutUuid())) {
-			String portletId = (String)liferayPortletRequest.getAttribute(
-				WebKeys.PORTLET_ID);
+		String portletId = (String)liferayPortletRequest.getAttribute(
+			WebKeys.PORTLET_ID);
 
-			PortletPreferences portletSetup =
-				PortletPreferencesFactoryUtil.getLayoutPortletSetup(
-					layout, portletId);
+		PortletPreferences portletSetup =
+			PortletPreferencesFactoryUtil.getLayoutPortletSetup(
+				layout, portletId);
 
-			String linkToLayoutUuid = GetterUtil.getString(
-				portletSetup.getValue("portletSetupLinkToLayoutUuid", null));
+		String linkToLayoutUuid = GetterUtil.getString(
+			portletSetup.getValue("portletSetupLinkToLayoutUuid", null));
 
-			if (linkToLayoutUuid.equals(_article.getLayoutUuid())) {
-				Group group = themeDisplay.getScopeGroup();
+		if (Validator.isNull(linkToLayoutUuid) &&
+			Validator.isNotNull(_article.getLayoutUuid())) {
 
-				if (group.getGroupId() != _article.getGroupId()) {
-					group = GroupLocalServiceUtil.getGroup(
-						_article.getGroupId());
-				}
+			Group group = themeDisplay.getScopeGroup();
 
-				String groupFriendlyURL = PortalUtil.getGroupFriendlyURL(
-					group, false, themeDisplay);
-
-				return groupFriendlyURL.concat(
-					JournalArticleConstants.CANONICAL_URL_SEPARATOR).concat(
-						_article.getUrlTitle());
+			if (group.getGroupId() != _article.getGroupId()) {
+				group = GroupLocalServiceUtil.getGroup(_article.getGroupId());
 			}
+
+			String groupFriendlyURL = PortalUtil.getGroupFriendlyURL(
+				group, false, themeDisplay);
+
+			return groupFriendlyURL.concat(
+				JournalArticleConstants.CANONICAL_URL_SEPARATOR).concat(
+					_article.getUrlTitle());
 		}
 
 		List<Long> hitLayoutIds =
