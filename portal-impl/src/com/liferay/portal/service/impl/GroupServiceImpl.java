eb754158ec0a39ec0ada27cffa1b27a85b3b9b33
Jose Jiménez <jose.jimenez@liferay.com>
2013-04-01 10:59:46 +0200

    LPS-34035 Check that the remote user belongs to the same company as the remote group on Remote Staging validations

--- portal-impl/src/com/liferay/portal/service/impl/GroupServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupServiceImpl.java
@@ -14,12 +14,14 @@
 
 package com.liferay.portal.service.impl;
 
+import com.liferay.portal.NoSuchGroupException;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.UniqueList;
 import com.liferay.portal.model.Group;
@@ -182,6 +184,26 @@ public class GroupServiceImpl extends GroupServiceBaseImpl {
 			getPermissionChecker(), roleId, ActionKeys.UPDATE);
 
 		groupLocalService.addRoleGroups(roleId, groupIds);
+	}
+
+	public void checkRemoteStagingGroup(long groupId)
+		throws PortalException, SystemException {
+
+		Group group = getGroup(groupId);
+
+		PermissionChecker permissionChecker = getPermissionChecker();
+
+		if (group.getCompanyId() != permissionChecker.getCompanyId()) {
+			StringBundler sb = new StringBundler(5);
+
+			sb.append("No Group exists with the key {groupId=");
+			sb.append(groupId);
+			sb.append("} in the Company {companyId=");
+			sb.append(permissionChecker.getCompanyId());
+			sb.append("}");
+
+			throw new NoSuchGroupException(sb.toString());
+		}
 	}
 
 	/**
--- portal-impl/src/com/liferay/portal/staging/StagingImpl.java
+++ portal-impl/src/com/liferay/portal/staging/StagingImpl.java
@@ -252,7 +252,8 @@ public class StagingImpl implements Staging {
 		// Ping remote host and verify that the group exists
 
 		try {
-			GroupServiceHttp.getGroup(httpPrincipal, remoteGroupId);
+			GroupServiceHttp.checkRemoteStagingGroup(
+				httpPrincipal, remoteGroupId);
 		}
 		catch (NoSuchGroupException nsge) {
 			RemoteExportException ree = new RemoteExportException(
@@ -2282,7 +2283,8 @@ public class StagingImpl implements Staging {
 		// Ping remote host and verify that the group exists
 
 		try {
-			GroupServiceHttp.getGroup(httpPrincipal, remoteGroupId);
+			GroupServiceHttp.checkRemoteStagingGroup(
+				httpPrincipal, remoteGroupId);
 		}
 		catch (NoSuchGroupException nsge) {
 			RemoteExportException ree = new RemoteExportException(
