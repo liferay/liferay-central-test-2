871d7834ff8929e5664c350b6312c6af1b5089ab
Jose Jiménez <jose.jimenez@liferay.com>
2013-04-01 15:29:25 +0200

    LPS-34035 Javadoc

--- portal-impl/src/com/liferay/portal/service/impl/GroupServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupServiceImpl.java
@@ -185,6 +185,15 @@ public class GroupServiceImpl extends GroupServiceBaseImpl {
 		groupLocalService.addRoleGroups(roleId, groupIds);
 	}
 
+	/**
+	 * Checks if the group is valid for Remote Staging.
+	 *
+	 * @param  groupId the primary key of the group
+	 * @throws PortalException if the user did not have permission to view the
+	 *         group or if the group belongs to a company different to the
+	 *         users's company
+	 * @throws SystemException if a system exception occurred
+	 */
 	public void checkRemoteStagingGroup(long groupId)
 		throws PortalException, SystemException {
 
