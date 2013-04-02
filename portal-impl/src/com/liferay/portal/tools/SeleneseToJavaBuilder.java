d3913ee993c67c1b0282519b4106c4ef9cb63aa2
Michael Hashimoto <michael.hashimoto@liferay.com>
2013-04-02 15:00:34 -0700

    LPS-34144

--- portal-impl/src/com/liferay/portal/tools/SeleneseToJavaBuilder.java
+++ portal-impl/src/com/liferay/portal/tools/SeleneseToJavaBuilder.java
@@ -890,7 +890,8 @@ public class SeleneseToJavaBuilder {
 					 param1.equals("makeVisible") ||
 					 param1.equals("mouseDown") || param1.equals("mouseMove") ||
 					 param1.equals("mouseOver") || param1.equals("mouseUp") ||
-					 param1.equals("open") || param1.equals("selectFrame") ||
+					 param1.equals("open") || param1.equals("runScript") ||
+					 param1.equals("selectFrame") ||
 					 param1.equals("selectPopUp") ||
 					 param1.equals("selectWindow") ||
 					 param1.equals("setTimeout") ||
