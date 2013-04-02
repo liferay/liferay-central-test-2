sergiogonzalez/liferay-portal

LPS-33928 Validate that a group cannot be its own parent

portal-impl/src/com/liferay/portlet/sites/action/EditGroupAction.java
@@ -17,6 +17,7 @@ package com.liferay.portlet.sites.action;
 import com.liferay.portal.DuplicateGroupException;
 import com.liferay.portal.GroupFriendlyURLException;
 import com.liferay.portal.GroupNameException;
+import com.liferay.portal.GroupParentGroupIdException;
 import com.liferay.portal.LayoutSetVirtualHostException;
 import com.liferay.portal.LocaleException;
 import com.liferay.portal.NoSuchGroupException;
@@ -158,6 +159,7 @@ public class EditGroupAction extends PortletAction {
 					 e instanceof DuplicateGroupException ||
 					 e instanceof GroupFriendlyURLException ||
 					 e instanceof GroupNameException ||
+					 e instanceof GroupParentGroupIdException ||
 					 e instanceof LayoutSetVirtualHostException ||
 					 e instanceof LocaleException ||
 					 e instanceof RemoteExportException ||

portal-web/docroot/html/portlet/sites_admin/init.jsp
@@ -20,6 +20,7 @@
 page import="com.liferay.portal.DuplicateTeamException" %><%@
 page import="com.liferay.portal.GroupFriendlyURLException" %><%@
 page import="com.liferay.portal.GroupNameException" %><%@
+page import="com.liferay.portal.GroupParentGroupIdException" %><%@
 page import="com.liferay.portal.LayoutSetVirtualHostException" %><%@
 page import="com.liferay.portal.LocaleException" %><%@
 page import="com.liferay.portal.MembershipRequestCommentsException" %><%@

portal-web/docroot/html/portlet/sites_admin/site/details.jsp
@@ -73,6 +73,7 @@ if (showPrototypes && (group != null)) {
 
 <liferay-ui:error exception="<%= DuplicateGroupException.class %>" message="please-enter-a-unique-name" />
 <liferay-ui:error exception="<%= GroupNameException.class %>" message="please-enter-a-valid-name" />
+<liferay-ui:error exception="<%= GroupParentGroupIdException.class %>" message="the-site-cannot-be-its-own-parent-site" />
 
 <liferay-ui:error exception="<%= RequiredGroupException.class %>">
 

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portlet/sites/action/EditGroupAction.java
@@ -17,6 +17,7 @@ package com.liferay.portlet.sites.action;
 import com.liferay.portal.DuplicateGroupException;
 import com.liferay.portal.GroupFriendlyURLException;
 import com.liferay.portal.GroupNameException;
+import com.liferay.portal.GroupParentException;
 import com.liferay.portal.LayoutSetVirtualHostException;
 import com.liferay.portal.LocaleException;
 import com.liferay.portal.NoSuchGroupException;
@@ -158,6 +159,7 @@ public class EditGroupAction extends PortletAction {
 					 e instanceof DuplicateGroupException ||
 					 e instanceof GroupFriendlyURLException ||
 					 e instanceof GroupNameException ||
+					 e instanceof GroupParentException ||
 					 e instanceof LayoutSetVirtualHostException ||
 					 e instanceof LocaleException ||
 					 e instanceof RemoteExportException ||

portal-web/docroot/html/portlet/sites_admin/init.jsp
@@ -20,6 +20,7 @@
 page import="com.liferay.portal.DuplicateTeamException" %><%@
 page import="com.liferay.portal.GroupFriendlyURLException" %><%@
 page import="com.liferay.portal.GroupNameException" %><%@
+page import="com.liferay.portal.GroupParentException" %><%@
 page import="com.liferay.portal.LayoutSetVirtualHostException" %><%@
 page import="com.liferay.portal.LocaleException" %><%@
 page import="com.liferay.portal.MembershipRequestCommentsException" %><%@

portal-web/docroot/html/portlet/sites_admin/site/details.jsp
@@ -73,6 +73,7 @@ if (showPrototypes && (group != null)) {
 
 <liferay-ui:error exception="<%= DuplicateGroupException.class %>" message="please-enter-a-unique-name" />
 <liferay-ui:error exception="<%= GroupNameException.class %>" message="please-enter-a-valid-name" />
+<liferay-ui:error exception="<%= GroupParentException.class %>" message="the-site-cannot-be-its-own-parent-site" />
 
 <liferay-ui:error exception="<%= RequiredGroupException.class %>">
 