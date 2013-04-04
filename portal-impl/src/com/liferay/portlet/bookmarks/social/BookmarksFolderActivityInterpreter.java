juliocamarero/liferay-portal

LPS-33682 Add separate interpreters for folders

portal-impl/src/com/liferay/portlet/bookmarks/social/BookmarksFolderActivityInterpreter.java
@@ -1,0 +1,95 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.bookmarks.social;
+
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.bookmarks.model.BookmarksFolder;
+import com.liferay.portlet.bookmarks.service.BookmarksFolderLocalServiceUtil;
+import com.liferay.portlet.bookmarks.service.permission.BookmarksFolderPermission;
+import com.liferay.portlet.social.model.BaseSocialActivityInterpreter;
+import com.liferay.portlet.social.model.SocialActivity;
+import com.liferay.portlet.social.model.SocialActivityConstants;
+
+/**
+ * @author Zsolt Berentey
+ */
+public class BookmarksFolderActivityInterpreter
+	extends BaseSocialActivityInterpreter {
+
+	public String[] getClassNames() {
+		return _CLASS_NAMES;
+	}
+
+	@Override
+	protected String getPath(SocialActivity activity) throws Exception {
+		return "/bookmarks/find_folder?folderId=";
+	}
+
+	@Override
+	protected String getTitle(
+			SocialActivity activity, ThemeDisplay themeDisplay)
+		throws Exception {
+
+		BookmarksFolder folder = BookmarksFolderLocalServiceUtil.getFolder(
+			activity.getClassPK());
+
+		return folder.getName();
+	}
+
+	@Override
+	protected String getTitlePattern(String groupName, SocialActivity activity)
+		throws Exception {
+
+		int activityType = activity.getType();
+
+		if (activityType == SocialActivityConstants.TYPE_MOVE_TO_TRASH) {
+			if (Validator.isNull(groupName)) {
+				return "activity-bookmarks-folder-move-to-trash";
+			}
+			else {
+				return "activity-bookmarks-folder-move-to-trash-in";
+			}
+		}
+		else if (activityType ==
+					SocialActivityConstants.TYPE_RESTORE_FROM_TRASH) {
+
+			if (Validator.isNull(groupName)) {
+				return "activity-bookmarks-folder-restore-from-trash";
+			}
+			else {
+				return "activity-bookmarks-folder-restore-from-trash-in";
+			}
+		}
+
+		return null;
+	}
+
+	@Override
+	protected boolean hasPermissions(
+			PermissionChecker permissionChecker, SocialActivity activity,
+			String actionId, ThemeDisplay themeDisplay)
+		throws Exception {
+
+		return BookmarksFolderPermission.contains(
+			permissionChecker, activity.getGroupId(), activity.getClassPK(),
+			actionId);
+	}
+
+	private static final String[] _CLASS_NAMES =
+		{BookmarksFolder.class.getName()};
+
+}

portal-impl/src/com/liferay/portlet/documentlibrary/social/DLFolderActivityInterpreter.java
@@ -1,0 +1,121 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.documentlibrary.social;
+
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.documentlibrary.model.DLFolder;
+import com.liferay.portlet.documentlibrary.service.DLAppLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.service.permission.DLFolderPermission;
+import com.liferay.portlet.social.model.BaseSocialActivityInterpreter;
+import com.liferay.portlet.social.model.SocialActivity;
+import com.liferay.portlet.social.model.SocialActivityConstants;
+import com.liferay.portlet.trash.util.TrashUtil;
+
+/**
+ * @author Zsolt Berentey
+ */
+public class DLFolderActivityInterpreter extends BaseSocialActivityInterpreter {
+
+	public String[] getClassNames() {
+		return _CLASS_NAMES;
+	}
+
+	protected String getFolderLink(
+		FileEntry fileEntry, ThemeDisplay themeDisplay) {
+
+		StringBundler sb = new StringBundler(6);
+
+		sb.append(themeDisplay.getPortalURL());
+		sb.append(themeDisplay.getPathMain());
+		sb.append("/document_library/find_folder?groupId=");
+		sb.append(fileEntry.getRepositoryId());
+		sb.append("&folderId=");
+		sb.append(fileEntry.getFolderId());
+
+		return sb.toString();
+	}
+
+	@Override
+	protected String getPath(SocialActivity activity) throws Exception {
+		return "/document_library/find_folder?folderId=";
+	}
+
+	@Override
+	protected String getTitle(
+			SocialActivity activity, ThemeDisplay themeDisplay)
+		throws Exception {
+
+		Folder folder = DLAppLocalServiceUtil.getFolder(activity.getClassPK());
+
+		if (folder.getModel() instanceof DLFolder) {
+			DLFolder dlFolder = (DLFolder)folder.getModel();
+
+			if (dlFolder.isInTrash()) {
+				return TrashUtil.getOriginalTitle(folder.getName());
+			}
+		}
+
+		return folder.getName();
+	}
+
+	@Override
+	protected String getTitlePattern(String groupName, SocialActivity activity)
+		throws Exception {
+
+		int activityType = activity.getType();
+
+		if (activityType == SocialActivityConstants.TYPE_MOVE_TO_TRASH) {
+			if (Validator.isNull(groupName)) {
+				return "activity-document-library-folder-move-to-trash";
+			}
+			else {
+				return "activity-document-library-folder-move-to-trash-in";
+			}
+		}
+		else if (activityType ==
+					SocialActivityConstants.TYPE_RESTORE_FROM_TRASH) {
+
+			if (Validator.isNull(groupName)) {
+				return "activity-document-library-folder-restore-from-trash";
+			}
+			else {
+				return
+					"activity-document-library-folder-restore-from-trash-" +
+						"in";
+			}
+		}
+
+		return null;
+	}
+
+	@Override
+	protected boolean hasPermissions(
+			PermissionChecker permissionChecker, SocialActivity activity,
+			String actionId, ThemeDisplay themeDisplay)
+		throws Exception {
+
+		return DLFolderPermission.contains(
+			permissionChecker, activity.getGroupId(), activity.getClassPK(),
+			actionId);
+	}
+
+	private static final String[] _CLASS_NAMES = {DLFolder.class.getName()};
+
+}

portal-impl/src/com/liferay/portlet/journal/social/JournalFolderActivityInterpreter.java
@@ -1,0 +1,100 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.journal.social;
+
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.journal.model.JournalFolder;
+import com.liferay.portlet.journal.service.JournalFolderLocalServiceUtil;
+import com.liferay.portlet.journal.service.permission.JournalFolderPermission;
+import com.liferay.portlet.social.model.BaseSocialActivityInterpreter;
+import com.liferay.portlet.social.model.SocialActivity;
+import com.liferay.portlet.social.model.SocialActivityConstants;
+import com.liferay.portlet.trash.util.TrashUtil;
+
+/**
+ * @author Zsolt Berentey
+ */
+public class JournalFolderActivityInterpreter
+	extends BaseSocialActivityInterpreter {
+
+	public String[] getClassNames() {
+		return _CLASS_NAMES;
+	}
+
+	@Override
+	protected String getPath(SocialActivity activity) throws Exception {
+		return "/journal/find_folder?folderId=";
+	}
+
+	@Override
+	protected String getTitle(
+			SocialActivity activity, ThemeDisplay themeDisplay)
+		throws Exception {
+
+		JournalFolder folder = JournalFolderLocalServiceUtil.getFolder(
+			activity.getClassPK());
+
+		if (folder.isInTrash()) {
+			return TrashUtil.getOriginalTitle(folder.getName());
+		}
+
+		return folder.getName();
+	}
+
+	@Override
+	protected String getTitlePattern(String groupName, SocialActivity activity)
+		throws Exception {
+
+		int activityType = activity.getType();
+
+		if (activityType == SocialActivityConstants.TYPE_MOVE_TO_TRASH) {
+			if (Validator.isNull(groupName)) {
+				return "activity-journal-folder-move-to-trash";
+			}
+			else {
+				return "activity-journal-folder-move-to-trash-in";
+			}
+		}
+		else if (activityType ==
+					SocialActivityConstants.TYPE_RESTORE_FROM_TRASH) {
+
+			if (Validator.isNull(groupName)) {
+				return "activity-journal-folder-restore-from-trash";
+			}
+			else {
+				return "activity-journal-folder-restore-from-trash-in";
+			}
+		}
+
+		return null;
+	}
+
+	@Override
+	protected boolean hasPermissions(
+			PermissionChecker permissionChecker, SocialActivity activity,
+			String actionId, ThemeDisplay themeDisplay)
+		throws Exception {
+
+		return JournalFolderPermission.contains(
+			permissionChecker, activity.getGroupId(), activity.getClassPK(),
+			actionId);
+	}
+
+	private static final String[] _CLASS_NAMES =
+		{JournalFolder.class.getName()};
+
+}

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portlet/bookmarks/social/BookmarksFolderActivityInterpreter.java
@@ -1,0 +1,96 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.bookmarks.social;
+
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.bookmarks.model.BookmarksFolder;
+import com.liferay.portlet.bookmarks.service.BookmarksFolderLocalServiceUtil;
+import com.liferay.portlet.bookmarks.service.permission.BookmarksFolderPermission;
+import com.liferay.portlet.social.model.BaseSocialActivityInterpreter;
+import com.liferay.portlet.social.model.SocialActivity;
+import com.liferay.portlet.social.model.SocialActivityConstants;
+
+/**
+ * @author Zsolt Berentey
+ */
+public class BookmarksFolderActivityInterpreter
+	extends BaseSocialActivityInterpreter {
+
+	public String[] getClassNames() {
+		return _CLASS_NAMES;
+	}
+
+	@Override
+	protected String getEntryTitle(
+			SocialActivity activity, ServiceContext serviceContext)
+		throws Exception {
+
+		BookmarksFolder folder = BookmarksFolderLocalServiceUtil.getFolder(
+			activity.getClassPK());
+
+		return folder.getName();
+	}
+
+	@Override
+	protected String getPath(SocialActivity activity) {
+		return "/bookmarks/find_folder?folderId=";
+	}
+
+	@Override
+	protected String getTitlePattern(String groupName, SocialActivity activity)
+		throws Exception {
+
+		int activityType = activity.getType();
+
+		if (activityType == SocialActivityConstants.TYPE_MOVE_TO_TRASH) {
+			if (Validator.isNull(groupName)) {
+				return "activity-bookmarks-folder-move-to-trash";
+			}
+			else {
+				return "activity-bookmarks-folder-move-to-trash-in";
+			}
+		}
+		else if (activityType ==
+					SocialActivityConstants.TYPE_RESTORE_FROM_TRASH) {
+
+			if (Validator.isNull(groupName)) {
+				return "activity-bookmarks-folder-restore-from-trash";
+			}
+			else {
+				return "activity-bookmarks-folder-restore-from-trash-in";
+			}
+		}
+
+		return null;
+	}
+
+	@Override
+	protected boolean hasPermissions(
+			PermissionChecker permissionChecker, SocialActivity activity,
+			String actionId, ServiceContext serviceContext)
+		throws Exception {
+
+		return BookmarksFolderPermission.contains(
+			permissionChecker, activity.getGroupId(), activity.getClassPK(),
+			actionId);
+	}
+
+	private static final String[] _CLASS_NAMES =
+		{BookmarksFolder.class.getName()};
+
+}

portal-impl/src/com/liferay/portlet/documentlibrary/social/DLFolderActivityInterpreter.java
@@ -1,0 +1,122 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.documentlibrary.social;
+
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.documentlibrary.model.DLFolder;
+import com.liferay.portlet.documentlibrary.service.DLAppLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.service.permission.DLFolderPermission;
+import com.liferay.portlet.social.model.BaseSocialActivityInterpreter;
+import com.liferay.portlet.social.model.SocialActivity;
+import com.liferay.portlet.social.model.SocialActivityConstants;
+import com.liferay.portlet.trash.util.TrashUtil;
+
+/**
+ * @author Zsolt Berentey
+ */
+public class DLFolderActivityInterpreter extends BaseSocialActivityInterpreter {
+
+	public String[] getClassNames() {
+		return _CLASS_NAMES;
+	}
+
+	@Override
+	protected String getEntryTitle(
+			SocialActivity activity, ServiceContext serviceContext)
+		throws Exception {
+
+		Folder folder = DLAppLocalServiceUtil.getFolder(activity.getClassPK());
+
+		if (folder.getModel() instanceof DLFolder) {
+			DLFolder dlFolder = (DLFolder)folder.getModel();
+
+			if (dlFolder.isInTrash()) {
+				return TrashUtil.getOriginalTitle(folder.getName());
+			}
+		}
+
+		return folder.getName();
+	}
+
+	protected String getFolderLink(
+		FileEntry fileEntry, ThemeDisplay themeDisplay) {
+
+		StringBundler sb = new StringBundler(6);
+
+		sb.append(themeDisplay.getPortalURL());
+		sb.append(themeDisplay.getPathMain());
+		sb.append("/document_library/find_folder?groupId=");
+		sb.append(fileEntry.getRepositoryId());
+		sb.append("&folderId=");
+		sb.append(fileEntry.getFolderId());
+
+		return sb.toString();
+	}
+
+	@Override
+	protected String getPath(SocialActivity activity) {
+		return "/document_library/find_folder?folderId=";
+	}
+
+	@Override
+	protected String getTitlePattern(String groupName, SocialActivity activity)
+		throws Exception {
+
+		int activityType = activity.getType();
+
+		if (activityType == SocialActivityConstants.TYPE_MOVE_TO_TRASH) {
+			if (Validator.isNull(groupName)) {
+				return "activity-document-library-folder-move-to-trash";
+			}
+			else {
+				return "activity-document-library-folder-move-to-trash-in";
+			}
+		}
+		else if (activityType ==
+					SocialActivityConstants.TYPE_RESTORE_FROM_TRASH) {
+
+			if (Validator.isNull(groupName)) {
+				return "activity-document-library-folder-restore-from-trash";
+			}
+			else {
+				return
+					"activity-document-library-folder-restore-from-trash-" +
+						"in";
+			}
+		}
+
+		return null;
+	}
+
+	@Override
+	protected boolean hasPermissions(
+			PermissionChecker permissionChecker, SocialActivity activity,
+			String actionId, ServiceContext serviceContext)
+		throws Exception {
+
+		return DLFolderPermission.contains(
+			permissionChecker, activity.getGroupId(), activity.getClassPK(),
+			actionId);
+	}
+
+	private static final String[] _CLASS_NAMES = {DLFolder.class.getName()};
+
+}

portal-impl/src/com/liferay/portlet/journal/social/JournalFolderActivityInterpreter.java
@@ -1,0 +1,100 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.journal.social;
+
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portlet.journal.model.JournalFolder;
+import com.liferay.portlet.journal.service.JournalFolderLocalServiceUtil;
+import com.liferay.portlet.journal.service.permission.JournalFolderPermission;
+import com.liferay.portlet.social.model.BaseSocialActivityInterpreter;
+import com.liferay.portlet.social.model.SocialActivity;
+import com.liferay.portlet.social.model.SocialActivityConstants;
+import com.liferay.portlet.trash.util.TrashUtil;
+
+/**
+ * @author Zsolt Berentey
+ */
+public class JournalFolderActivityInterpreter
+	extends BaseSocialActivityInterpreter {
+
+	public String[] getClassNames() {
+		return _CLASS_NAMES;
+	}
+
+	@Override
+	protected String getEntryTitle(
+			SocialActivity activity, ServiceContext serviceContext)
+		throws Exception {
+
+		JournalFolder folder = JournalFolderLocalServiceUtil.getFolder(
+			activity.getClassPK());
+
+		if (folder.isInTrash()) {
+			return TrashUtil.getOriginalTitle(folder.getName());
+		}
+
+		return folder.getName();
+	}
+
+	@Override
+	protected String getPath(SocialActivity activity) {
+		return "/journal/find_folder?folderId=";
+	}
+
+	@Override
+	protected String getTitlePattern(String groupName, SocialActivity activity)
+		throws Exception {
+
+		int activityType = activity.getType();
+
+		if (activityType == SocialActivityConstants.TYPE_MOVE_TO_TRASH) {
+			if (Validator.isNull(groupName)) {
+				return "activity-journal-folder-move-to-trash";
+			}
+			else {
+				return "activity-journal-folder-move-to-trash-in";
+			}
+		}
+		else if (activityType ==
+					SocialActivityConstants.TYPE_RESTORE_FROM_TRASH) {
+
+			if (Validator.isNull(groupName)) {
+				return "activity-journal-folder-restore-from-trash";
+			}
+			else {
+				return "activity-journal-folder-restore-from-trash-in";
+			}
+		}
+
+		return null;
+	}
+
+	@Override
+	protected boolean hasPermissions(
+			PermissionChecker permissionChecker, SocialActivity activity,
+			String actionId, ServiceContext serviceContext)
+		throws Exception {
+
+		return JournalFolderPermission.contains(
+			permissionChecker, activity.getGroupId(), activity.getClassPK(),
+			actionId);
+	}
+
+	private static final String[] _CLASS_NAMES =
+		{JournalFolder.class.getName()};
+
+}