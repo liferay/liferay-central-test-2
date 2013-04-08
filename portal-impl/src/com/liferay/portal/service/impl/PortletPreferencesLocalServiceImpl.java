073d32d36b1d40a8a89d080fb7de33820497c1f6
shuyangzhou <shuyang.zhou@liferay.com>
2013-04-08 08:53:41 -0700

    LPS-34161 Add ThreadLocal cache for PortletPreferencesLocalServiceImpl.getPortletPreferences()

--- portal-impl/src/com/liferay/portal/service/impl/PortletPreferencesLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/PortletPreferencesLocalServiceImpl.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.service.impl;
 
+import com.liferay.portal.kernel.cache.ThreadLocalCachable;
 import com.liferay.portal.kernel.concurrent.LockRegistry;
 import com.liferay.portal.kernel.dao.db.DB;
 import com.liferay.portal.kernel.dao.db.DBFactoryUtil;
@@ -155,6 +156,7 @@ public class PortletPreferencesLocalServiceImpl
 			ownerId, ownerType, plid, portletId);
 	}
 
+	@ThreadLocalCachable
 	public List<PortletPreferences> getPortletPreferences(
 			long companyId, long groupId, long ownerId, int ownerType,
 			String portletId, boolean privateLayout)
