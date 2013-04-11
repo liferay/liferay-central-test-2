juliocamarero/liferay-portal

LPS-32869 Autogenerated

portal-service/src/com/liferay/portlet/documentlibrary/service/persistence/DLFileEntryFinder.java
@@ -54,6 +54,10 @@ public interface DLFileEntryFinder {
 		int start, int end)
 		throws com.liferay.portal.kernel.exception.SystemException;
 
+	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> findByStructureIds(
+		long[] structureIds, int start, int end)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
 	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> findByMisversioned()
 		throws com.liferay.portal.kernel.exception.SystemException;
 

portal-service/src/com/liferay/portlet/documentlibrary/service/persistence/DLFileEntryFinderUtil.java
@@ -75,6 +75,12 @@ public class DLFileEntryFinderUtil {
 		return getFinder().findByExtraSettings(start, end);
 	}
 
+	public static java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> findByStructureIds(
+		long[] structureIds, int start, int end)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getFinder().findByStructureIds(structureIds, start, end);
+	}
+
 	public static java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> findByMisversioned()
 		throws com.liferay.portal.kernel.exception.SystemException {
 		return getFinder().findByMisversioned();

liferay/liferay-central-test-2

portal-service/src/com/liferay/portlet/documentlibrary/service/persistence/DLFileEntryFinder.java
@@ -50,6 +50,10 @@ public interface DLFileEntryFinder {
 		throws com.liferay.portal.kernel.exception.SystemException,
 			com.liferay.portlet.documentlibrary.NoSuchFileEntryException;
 
+	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> findByDDMStructureIds(
+		long[] ddmStructureIds, int start, int end)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
 	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> findByExtraSettings(
 		int start, int end)
 		throws com.liferay.portal.kernel.exception.SystemException;

portal-service/src/com/liferay/portlet/documentlibrary/service/persistence/DLFileEntryFinderUtil.java
@@ -69,6 +69,12 @@ public class DLFileEntryFinderUtil {
 		return getFinder().findByAnyImageId(imageId);
 	}
 
+	public static java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> findByDDMStructureIds(
+		long[] ddmStructureIds, int start, int end)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getFinder().findByDDMStructureIds(ddmStructureIds, start, end);
+	}
+
 	public static java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntry> findByExtraSettings(
 		int start, int end)
 		throws com.liferay.portal.kernel.exception.SystemException {