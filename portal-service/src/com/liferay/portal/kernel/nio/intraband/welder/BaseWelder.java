d2456325b261864fc5c91b5e32c64fecab548f43
Brian Chan <brian.chan@liferay.com>
2013-04-11 12:41:34 -0700

    LPS-30235 SF

--- portal-service/src/com/liferay/portal/kernel/nio/intraband/welder/BaseWelder.java
+++ portal-service/src/com/liferay/portal/kernel/nio/intraband/welder/BaseWelder.java
@@ -27,11 +27,12 @@ public abstract class BaseWelder implements Welder {
 
 	public BaseWelder() {
 
-		// This assignment has to stay in constructor. Because we need to tell
-		// the difference between a constructor created welder and
+		// This assignment has to stay in the constructor because we need to
+		// differentiate between a constructor created welder and a
 		// deserialization created welder. Only the constructor created welder
-		// needs a force set to server. The deserialization created welder
-		// remains what the original creator set it to be.
+		// needs a forcibly set the server value to true. The deserialization
+		// created welder has the server value set to the value set in the
+		// original welder.
 
 		server = true;
 	}
@@ -39,7 +40,7 @@ public abstract class BaseWelder implements Welder {
 	public synchronized void destroy() throws IOException {
 		if (state != State.WELDED) {
 			throw new IllegalStateException(
-				"Can not destroy a welder at state " + state);
+				"Unable to destroy a welder with state " + state);
 		}
 
 		registrationReference.cancelRegistration();
@@ -54,7 +55,7 @@ public abstract class BaseWelder implements Welder {
 
 		if (state != State.CREATED) {
 			throw new IllegalStateException(
-				"Can not weld a welder at state " + state);
+				"Unable to weld a welder with state " + state);
 		}
 
 		if (server) {
