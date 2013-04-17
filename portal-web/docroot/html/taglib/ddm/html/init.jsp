bb15c96f5f4347e687332bacd25ef14725199586
Julio Camarero <julio.camarero@liferay.com>
2013-04-17 18:30:18 +0200

    LPS-33739 autogenerated

--- portal-web/docroot/html/taglib/ddm/html/init.jsp
+++ portal-web/docroot/html/taglib/ddm/html/init.jsp
@@ -34,6 +34,7 @@ if ((dynamicAttributes != null) && !dynamicAttributes.isEmpty()) {
 
 long classNameId = GetterUtil.getLong(String.valueOf(request.getAttribute("ddm:html:classNameId")));
 long classPK = GetterUtil.getLong(String.valueOf(request.getAttribute("ddm:html:classPK")));
+java.lang.String fieldName = GetterUtil.getString((java.lang.String)request.getAttribute("ddm:html:fieldName"));
 com.liferay.portlet.dynamicdatamapping.storage.Fields fields = (com.liferay.portlet.dynamicdatamapping.storage.Fields)request.getAttribute("ddm:html:fields");
 java.lang.String fieldsNamespace = GetterUtil.getString((java.lang.String)request.getAttribute("ddm:html:fieldsNamespace"));
 java.lang.String mode = GetterUtil.getString((java.lang.String)request.getAttribute("ddm:html:mode"));
@@ -43,6 +44,7 @@ java.util.Locale requestedLocale = (java.util.Locale)request.getAttribute("ddm:h
 
 _updateOptions(_options, "classNameId", classNameId);
 _updateOptions(_options, "classPK", classPK);
+_updateOptions(_options, "fieldName", fieldName);
 _updateOptions(_options, "fields", fields);
 _updateOptions(_options, "fieldsNamespace", fieldsNamespace);
 _updateOptions(_options, "mode", mode);
--- util-taglib/src/com/liferay/taglib/ddm/base/BaseHTMLTag.java
+++ util-taglib/src/com/liferay/taglib/ddm/base/BaseHTMLTag.java
@@ -38,6 +38,10 @@ public class BaseHTMLTag extends com.liferay.taglib.util.IncludeTag {
 		return _classPK;
 	}
 
+	public java.lang.String getFieldName() {
+		return _fieldName;
+	}
+
 	public com.liferay.portlet.dynamicdatamapping.storage.Fields getFields() {
 		return _fields;
 	}
@@ -72,6 +76,12 @@ public class BaseHTMLTag extends com.liferay.taglib.util.IncludeTag {
 		_classPK = classPK;
 
 		setScopedAttribute("classPK", classPK);
+	}
+
+	public void setFieldName(java.lang.String fieldName) {
+		_fieldName = fieldName;
+
+		setScopedAttribute("fieldName", fieldName);
 	}
 
 	public void setFields(com.liferay.portlet.dynamicdatamapping.storage.Fields fields) {
@@ -114,6 +124,7 @@ public class BaseHTMLTag extends com.liferay.taglib.util.IncludeTag {
 	protected void cleanUp() {
 		_classNameId = 0;
 		_classPK = 0;
+		_fieldName = null;
 		_fields = null;
 		_fieldsNamespace = null;
 		_mode = null;
@@ -136,6 +147,7 @@ public class BaseHTMLTag extends com.liferay.taglib.util.IncludeTag {
 	protected void setAttributes(HttpServletRequest request) {
 		setNamespacedAttribute(request, "classNameId", _classNameId);
 		setNamespacedAttribute(request, "classPK", _classPK);
+		setNamespacedAttribute(request, "fieldName", _fieldName);
 		setNamespacedAttribute(request, "fields", _fields);
 		setNamespacedAttribute(request, "fieldsNamespace", _fieldsNamespace);
 		setNamespacedAttribute(request, "mode", _mode);
@@ -154,6 +166,7 @@ public class BaseHTMLTag extends com.liferay.taglib.util.IncludeTag {
 
 	private long _classNameId = 0;
 	private long _classPK = 0;
+	private java.lang.String _fieldName = null;
 	private com.liferay.portlet.dynamicdatamapping.storage.Fields _fields = null;
 	private java.lang.String _fieldsNamespace = null;
 	private java.lang.String _mode = null;
