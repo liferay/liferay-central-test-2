c666f05e114e98416d6b4da17a8dacf1b61ebae6
shuyangzhou <shuyang.zhou@liferay.com>
2013-04-19 17:32:00 -0700

    LPS-30240 Mailbox utility

--- portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -955,6 +955,10 @@ public interface PropsKeys {
 
 	public static final String INDEX_WITH_THREAD = "index.with.thread";
 
+	public static final String INTRABAND_MAILBOX_REAPER_THREAD_ENABLED = "intraband.mailbox.reaper.thread.enabled";
+
+	public static final String INTRABAND_MAILBOX_STORAGE_LIFE = "intraband.mailbox.storage.life";
+
 	public static final String INTRABAND_WELDER_IMPL = "intraband.welder.impl";
 
 	public static final String INTRABAND_WELDER_SOCKET_BUFFER_SIZE = "intraband.welder.socket.buffer.size";
