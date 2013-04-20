5881d0df43f38e60aff28f1e1093f6a8bfe4d0a4
Michael Hashimoto <michael.hashimoto@liferay.com>
2013-04-19 17:01:07 -0700

    LPS-34448

--- portal-impl/test/integration/com/liferay/portal/tools/seleniumbuilder/SeleniumBuilderTest.java
+++ portal-impl/test/integration/com/liferay/portal/tools/seleniumbuilder/SeleniumBuilderTest.java
@@ -771,7 +771,7 @@ public class SeleniumBuilderTest {
 	public void testMacroConditionElement1002_1() throws Exception {
 		test(
 			"MacroConditionElement1002_1.macro",
-			"Error 1002: Invalid fail element in " + _DIR_NAME +
+			"Error 1002: Invalid var element in " + _DIR_NAME +
 				"/MacroConditionElement1002_1.macro:5");
 	}
 
@@ -779,7 +779,7 @@ public class SeleniumBuilderTest {
 	public void testMacroConditionElement1002_2() throws Exception {
 		test(
 			"MacroConditionElement1002_2.macro",
-			"Error 1002: Invalid fail element in " + _DIR_NAME +
+			"Error 1002: Invalid var element in " + _DIR_NAME +
 				"/MacroConditionElement1002_2.macro:5");
 	}
 
@@ -1117,7 +1117,7 @@ public class SeleniumBuilderTest {
 	public void testMacroExecuteElement1002_1() throws Exception {
 		test(
 			"MacroExecuteElement1002_1.macro",
-			"Error 1002: Invalid var element in " + _DIR_NAME +
+			"Error 1002: Invalid fail element in " + _DIR_NAME +
 				"/MacroExecuteElement1002_1.macro:4");
 	}
 
@@ -1125,7 +1125,7 @@ public class SeleniumBuilderTest {
 	public void testMacroExecuteElement1002_2() throws Exception {
 		test(
 			"MacroExecuteElement1002_2.macro",
-			"Error 1002: Invalid var element in " + _DIR_NAME +
+			"Error 1002: Invalid fail element in " + _DIR_NAME +
 				"/MacroExecuteElement1002_2.macro:4");
 	}
 
