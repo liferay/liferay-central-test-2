cda0749adf063e3981bb1042e7a6267eaada4e89
KocsisDaniel <daniel.kocsis@liferay.com>
2013-04-26 11:13:14 +0200

    LPS-33680 Refactor AssetLinkExportImportTest

--- portal-impl/test/integration/com/liferay/portlet/asset/lar/AssetLinkExportImportTest.java
+++ portal-impl/test/integration/com/liferay/portlet/asset/lar/AssetLinkExportImportTest.java
@@ -14,17 +14,12 @@
 
 package com.liferay.portlet.asset.lar;
 
-import com.liferay.counter.service.CounterLocalServiceUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.lar.PortletDataHandlerKeys;
-import com.liferay.portal.kernel.lar.UserIdStrategy;
 import com.liferay.portal.kernel.test.ExecutionTestListeners;
 import com.liferay.portal.kernel.transaction.Transactional;
-import com.liferay.portal.kernel.util.Constants;
-import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.lar.BasePortletExportImportTestCase;
-import com.liferay.portal.lar.PortletImporter;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
 import com.liferay.portal.service.ServiceTestUtil;
 import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
@@ -39,14 +34,19 @@ import com.liferay.portlet.asset.model.AssetLink;
 import com.liferay.portlet.asset.service.AssetEntryLocalServiceUtil;
 import com.liferay.portlet.asset.service.AssetLinkLocalServiceUtil;
 import com.liferay.portlet.blogs.model.BlogsEntry;
+import com.liferay.portlet.blogs.service.BlogsEntryLocalServiceUtil;
 import com.liferay.portlet.blogs.util.BlogsTestUtil;
 import com.liferay.portlet.bookmarks.model.BookmarksEntry;
+import com.liferay.portlet.bookmarks.service.BookmarksEntryLocalServiceUtil;
 import com.liferay.portlet.bookmarks.util.BookmarksTestUtil;
 import com.liferay.portlet.journal.model.JournalArticle;
+import com.liferay.portlet.journal.model.JournalArticleResource;
+import com.liferay.portlet.journal.service.JournalArticleLocalServiceUtil;
+import com.liferay.portlet.journal.service.persistence.JournalArticleResourceUtil;
 import com.liferay.portlet.journal.util.JournalTestUtil;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -68,74 +68,43 @@ public class AssetLinkExportImportTest extends BasePortletExportImportTestCase {
 
 	@Test
 	public void testExportImportBlogWithLinks() throws Exception {
-
 		long groupId = group.getGroupId();
 
-		BlogsEntry mainBlogEntry = BlogsTestUtil.addEntry(
-			TestPropsValues.getUserId(), group.getGroupId(),
+		BlogsEntry blogsEntry = BlogsTestUtil.addEntry(
+			TestPropsValues.getUserId(), groupId,
 			ServiceTestUtil.randomString(), true);
 
-		String mainBlogEntryUuid = mainBlogEntry.getUuid();
-
-		AssetEntry mainAssetEntry = AssetEntryLocalServiceUtil.getEntry(
-			group.getGroupId(), mainBlogEntryUuid);
-
-		BlogsEntry relatedBlogEntry1 = BlogsTestUtil.addEntry(
-			TestPropsValues.getUserId(), group.getGroupId(),
+		BlogsEntry relatedBlogsEntry1 = BlogsTestUtil.addEntry(
+			TestPropsValues.getUserId(), groupId,
 			ServiceTestUtil.randomString(), true);
 
-		BlogsEntry relatedBlogEntry2 = BlogsTestUtil.addEntry(
-			TestPropsValues.getUserId(), group.getGroupId(),
+		BlogsEntry relatedBlogsEntry2 = BlogsTestUtil.addEntry(
+			TestPropsValues.getUserId(), groupId,
 			ServiceTestUtil.randomString(), true);
 
-		List<AssetLink> assetLinks = new ArrayList<AssetLink>();
+		addAssetLink(
+			groupId, blogsEntry.getUuid(), relatedBlogsEntry1.getUuid(), 1);
 
-		AssetLink assetLink1 = createAssetLink(
-			groupId, mainBlogEntryUuid, relatedBlogEntry1.getUuid(), 1);
+		addAssetLink(
+			groupId, blogsEntry.getUuid(), relatedBlogsEntry2.getUuid(), 2);
 
-		assetLinks.add(assetLink1);
+		doExportImportPortlet(PortletKeys.BLOGS);
 
-		AssetLink assetLink2 = createAssetLink(
-			groupId, mainBlogEntryUuid, relatedBlogEntry2.getUuid(), 2);
+		BlogsEntry importedBlogsEntry =
+			BlogsEntryLocalServiceUtil.getBlogsEntryByUuidAndGroupId(
+				blogsEntry.getUuid(), importedGroup.getGroupId());
 
-		assetLinks.add(assetLink2);
+		Assert.assertNotNull(importedBlogsEntry);
 
-		long numberOfLinksOfAssetEntry =
-			AssetLinkLocalServiceUtil.getDirectLinks(
-				mainAssetEntry.getEntryId()).size();
-
-		doExport(PortletKeys.BLOGS);
-
-		doImport(PortletKeys.BLOGS);
-
-		AssetEntry importMainAssetEntry = AssetEntryLocalServiceUtil.getEntry(
-			importedGroup.getGroupId(), mainBlogEntryUuid);
-
-		List<AssetLink> importedLinks =
-			AssetLinkLocalServiceUtil.getDirectLinks(
-				importMainAssetEntry.getEntryId());
-
-		long numberOfLinksAfterImport = importedLinks.size();
-
-		Assert.assertEquals(
-			numberOfLinksAfterImport, numberOfLinksOfAssetEntry);
-
-		_checkImportedLinks(assetLinks, importedGroup.getGroupId());
-
+		validateImportedLinks(blogsEntry.getUuid());
 	}
 
 	@Test
 	public void testExportImportBookmarksWithLinks() throws Exception {
-
 		long groupId = group.getGroupId();
 
-		BookmarksEntry mainBookmarkEntry = BookmarksTestUtil.addEntry(
+		BookmarksEntry bookmarksEntry = BookmarksTestUtil.addEntry(
 			groupId, true);
-
-		String mainBookmarkEntryUuid = mainBookmarkEntry.getUuid();
-
-		AssetEntry mainAssetEntry = AssetEntryLocalServiceUtil.getEntry(
-			group.getGroupId(), mainBookmarkEntryUuid);
 
 		BookmarksEntry relatedBookmarkEntry1 = BookmarksTestUtil.addEntry(
 			groupId, true);
@@ -143,208 +112,72 @@ public class AssetLinkExportImportTest extends BasePortletExportImportTestCase {
 		BookmarksEntry relatedBookmarkEntry2 = BookmarksTestUtil.addEntry(
 			groupId, true);
 
-		List<AssetLink> assetLinks = new ArrayList<AssetLink>();
+		addAssetLink(
+			groupId, bookmarksEntry.getUuid(), relatedBookmarkEntry1.getUuid(),
+			1);
 
-		AssetLink assetLink1 = createAssetLink(
-			groupId, mainBookmarkEntryUuid, relatedBookmarkEntry1.getUuid(), 1);
+		addAssetLink(
+			groupId, bookmarksEntry.getUuid(), relatedBookmarkEntry2.getUuid(),
+			2);
 
-		assetLinks.add(assetLink1);
+		doExportImportPortlet(PortletKeys.BOOKMARKS);
 
-		AssetLink assetLink2 = createAssetLink(
-			groupId, mainBookmarkEntryUuid, relatedBookmarkEntry2.getUuid(), 2);
+		BookmarksEntry importedBookmarksEntry =
+			BookmarksEntryLocalServiceUtil.getBookmarksEntryByUuidAndGroupId(
+				bookmarksEntry.getUuid(), importedGroup.getGroupId());
 
-		assetLinks.add(assetLink2);
+		Assert.assertNotNull(importedBookmarksEntry);
 
-		long numberOfLinksBeforeImport =
-			AssetLinkLocalServiceUtil.getDirectLinks(
-				mainAssetEntry.getEntryId()).size();
-
-		doExport(PortletKeys.BOOKMARKS);
-
-		doImport(PortletKeys.BOOKMARKS);
-
-		AssetEntry importMainAssetEntry = AssetEntryLocalServiceUtil.getEntry(
-			importedGroup.getGroupId(), mainBookmarkEntryUuid);
-
-		List<AssetLink> importedLinks =
-				AssetLinkLocalServiceUtil.getDirectLinks(
-					importMainAssetEntry.getEntryId());
-
-		long numberOfLinksAfterImport = importedLinks.size();
-
-		Assert.assertEquals(
-			numberOfLinksAfterImport, numberOfLinksBeforeImport);
-
-		_checkImportedLinks(assetLinks, importedGroup.getGroupId());
+		validateImportedLinks(bookmarksEntry.getUuid());
 	}
 
 	@Test
 	public void testExportImportJournalWithLinks() throws Exception {
-
 		long groupId = group.getGroupId();
 
-		JournalArticle mainJournalEntry = JournalTestUtil.addArticle(
-			group.getGroupId(),
-			"mainJournalEntryTest"+ServiceTestUtil.randomString(),
+		JournalArticle article = JournalTestUtil.addArticle(
+			groupId, ServiceTestUtil.randomString(),
 			ServiceTestUtil.randomString());
 
-		String mainJournalEntryUuid = mainJournalEntry.getArticleResourceUuid();
-
-		AssetEntry mainAssetEntry = AssetEntryLocalServiceUtil.getEntry(
-			group.getGroupId(), mainJournalEntryUuid);
-
-		JournalArticle relatedArticleEntry1 = JournalTestUtil.addArticle(
-			group.getGroupId(), ServiceTestUtil.randomString(),
+		JournalArticle relatedArticle1 = JournalTestUtil.addArticle(
+			groupId, ServiceTestUtil.randomString(),
 			ServiceTestUtil.randomString());
 
-		JournalArticle relatedArticleEntry2 = JournalTestUtil.addArticle(
-			group.getGroupId(), ServiceTestUtil.randomString(),
+		JournalArticle relatedArticle2 = JournalTestUtil.addArticle(
+			groupId, ServiceTestUtil.randomString(),
 			ServiceTestUtil.randomString());
 
-		List<AssetLink> assetLinks = new ArrayList<AssetLink>();
+		addAssetLink(
+			groupId, article.getArticleResourceUuid(),
+			relatedArticle1.getArticleResourceUuid(), 1);
 
-		AssetLink assetLink1 = createAssetLink(
-			groupId, mainJournalEntryUuid,
-			relatedArticleEntry1.getArticleResourceUuid(), 1);
+		addAssetLink(
+			groupId, article.getArticleResourceUuid(),
+			relatedArticle2.getArticleResourceUuid(), 2);
 
-		assetLinks.add(assetLink1);
+		doExportImportPortlet(PortletKeys.JOURNAL);
 
-		AssetLink assetLink2 = createAssetLink(
-			groupId, mainJournalEntryUuid,
-			relatedArticleEntry2.getArticleResourceUuid(), 2);
+		JournalArticleResource importedArticleResource =
+			JournalArticleResourceUtil.fetchByUUID_G(
+				article.getArticleResourceUuid(), importedGroup.getGroupId());
 
-		assetLinks.add(assetLink2);
+		JournalArticle importedArticle =
+			JournalArticleLocalServiceUtil.getLatestArticle(
+				importedArticleResource.getResourcePrimKey(),
+				article.getStatus(), false);
 
-		List<AssetLink> exportAssetLinks = AssetLinkLocalServiceUtil.getLinks(
-			mainAssetEntry.getEntryId());
+		Assert.assertNotNull(importedArticle);
 
-		doExport(PortletKeys.JOURNAL);
-
-		doImport(PortletKeys.JOURNAL);
-
-		AssetEntry mainAssetEntryImported = AssetEntryLocalServiceUtil.getEntry(
-			importedGroup.getGroupId(), mainJournalEntryUuid);
-
-		List<AssetLink> importAssetLinks = AssetLinkLocalServiceUtil.getLinks(
-			mainAssetEntryImported.getEntryId());
-
-		Assert.assertEquals(exportAssetLinks.size(), importAssetLinks.size());
-
-		_checkImportedLinks(assetLinks, importedGroup.getGroupId());
+		validateImportedLinks(article.getArticleResourceUuid());
 	}
 
-	protected Map<String, String[]> getBaseParameterMap() throws Exception {
-		Map<String, String[]> parameterMap = new HashMap<String, String[]>();
-
-		parameterMap.put(
-			PortletDataHandlerKeys.CATEGORIES,
-			new String[] {Boolean.TRUE.toString()});
-		parameterMap.put(
-			PortletDataHandlerKeys.PORTLET_DATA_ALL,
-			new String[] {Boolean.TRUE.toString()});
-		parameterMap.put(
-			PortletDataHandlerKeys.PERMISSIONS,
-			new String[] {Boolean.TRUE.toString()});
-		parameterMap.put(
-			PortletDataHandlerKeys.PORTLET_DATA_CONTROL_DEFAULT,
-			new String[] {Boolean.FALSE.toString()});
-		parameterMap.put(
-			PortletDataHandlerKeys.PORTLET_METADATA_ALL,
-			new String[] {Boolean.TRUE.toString()});
-
-		return parameterMap;
-	}
-
-	protected Map<String, String[]> getExportParameterMap(String portlet)
-		throws Exception {
-
-		Map<String, String[]> parameterMap = getBaseParameterMap();
-
-		parameterMap.put(Constants.CMD, new String[] {Constants.EXPORT});
-		parameterMap.put(
-			PortletDataHandlerKeys.PORTLET_DATA + StringPool.UNDERLINE +
-				portlet,
-			new String[] {Boolean.TRUE.toString()});
-
-		parameterMap.put(
-			PortletDataHandlerKeys.PORTLET_METADATA_ALL,
-			new String[] {Boolean.TRUE.toString()});
-
-		parameterMap.put(
-			"_journal_embedded-assets", new String[] {Boolean.TRUE.toString()});
-
-		parameterMap.put(
-			"_journal_web-content", new String[] {Boolean.TRUE.toString()});
-
-		parameterMap.put("range", new String[] {"fromLastPublishDate"});
-
-		return parameterMap;
-	}
-
-	protected Map<String, String[]> getImportParameterMap(
-			long plid, String portletResource)
-		throws Exception {
-
-		Map<String, String[]> parameterMap = getBaseParameterMap();
-
-		parameterMap.put(Constants.CMD, new String[] {Constants.IMPORT});
-		parameterMap.put(
-			PortletDataHandlerKeys.DATA_STRATEGY,
-			new String[] {PortletDataHandlerKeys.DATA_STRATEGY_MIRROR});
-		parameterMap.put(
-			PortletDataHandlerKeys.DELETE_PORTLET_DATA,
-			new String[] {Boolean.FALSE.toString()});
-		parameterMap.put(
-			PortletDataHandlerKeys.PORTLET_DATA,
-			new String[] {Boolean.TRUE.toString()});
-		parameterMap.put(
-			PortletDataHandlerKeys.USER_ID_STRATEGY,
-			new String[] {UserIdStrategy.CURRENT_USER_ID});
-
-		parameterMap.put("plid", new String[] {String.valueOf(plid)});
-
-		parameterMap.put("portletResource", new String[] {portletResource});
-
-		parameterMap.put(
-			"_journal_embedded-assets", new String[] {Boolean.TRUE.toString()});
-
-		parameterMap.put(
-			"_journal_web-content", new String[] {Boolean.TRUE.toString()});
-		parameterMap.put(
-			"_journal_version-history", new String[] {Boolean.TRUE.toString()});
-
-		parameterMap.put("range", new String[] {"fromLastPublishDate"});
-
-		return parameterMap;
-	}
-
-	private void _checkImportedLinks(
-			List<AssetLink> assetLinks, long importedGroupId)
+	protected AssetLink addAssetLink(
+			long groupId, String sourceEntryUUID, String targetEntryUUID,
+			int weight)
 		throws PortalException, SystemException {
 
-		for (AssetLink originLink : assetLinks) {
-			AssetEntry sourceAssetEntry = AssetEntryLocalServiceUtil.getEntry(
-				originLink.getEntryId1());
-
-			AssetEntry targetAssetEntry = AssetEntryLocalServiceUtil.getEntry(
-				originLink.getEntryId2());
-
-			AssetLink importedLink = AssetLinkLocalServiceUtil.getLink(
-				importedGroupId, sourceAssetEntry.getClassUuid(),
-				targetAssetEntry.getClassUuid(), originLink.getType());
-
-			Assert.assertEquals(originLink.getType(), importedLink.getType());
-			Assert.assertEquals(
-				originLink.getWeight(), importedLink.getWeight());
-		}
-	}
-
-	private AssetLink createAssetLink(
-			long groupId, String originEntryUUID, String targetEntryUUID,
-			int weight) throws PortalException, SystemException {
-
 		AssetEntry originAssetEntry = AssetEntryLocalServiceUtil.getEntry(
-			groupId, originEntryUUID);
+			groupId, sourceEntryUUID);
 
 		AssetEntry targetAssetEntry = AssetEntryLocalServiceUtil.getEntry(
 			groupId, targetEntryUUID);
@@ -354,26 +187,102 @@ public class AssetLinkExportImportTest extends BasePortletExportImportTestCase {
 			targetAssetEntry.getEntryId(), 0, weight);
 	}
 
-	private void doExport(String portlet) throws Exception {
-		Map<String, String[]> parameterMap = getExportParameterMap(portlet);
-
+	protected void doExportImportPortlet(String portletId) throws Exception {
 		larFile = LayoutLocalServiceUtil.exportPortletInfoAsFile(
-			layout.getPlid(), group.getGroupId(), portlet, parameterMap, null,
-			null);
-	}
+			layout.getPlid(), layout.getGroupId(), portletId, getParameterMap(),
+			null, null);
 
-	private void doImport(String portlet) throws Exception {
 		importedGroup = GroupTestUtil.addGroup();
 
 		importedLayout = LayoutTestUtil.addLayout(
 			importedGroup.getGroupId(), ServiceTestUtil.randomString());
 
-		Map<String, String[]> parameterMap = getImportParameterMap(
-			importedLayout.getPlid(), portlet);
-
 		LayoutLocalServiceUtil.importPortletInfo(
 			TestPropsValues.getUserId(), importedLayout.getPlid(),
-			importedGroup.getGroupId(), portlet, parameterMap, larFile);
+			importedGroup.getGroupId(), portletId, getParameterMap(), larFile);
+
+		importedLayout = LayoutLocalServiceUtil.fetchLayoutByUuidAndGroupId(
+			layout.getUuid(), importedGroup.getGroupId(),
+			layout.isPrivateLayout());
+	}
+
+	protected Map<String, String[]> getParameterMap() {
+		Map<String, String[]> parameterMap =
+			new LinkedHashMap<String, String[]>();
+
+		parameterMap.put(
+			PortletDataHandlerKeys.DATA_STRATEGY,
+			new String[] {
+				PortletDataHandlerKeys.DATA_STRATEGY_MIRROR_OVERWRITE});
+		parameterMap.put(
+			PortletDataHandlerKeys.IGNORE_LAST_PUBLISH_DATE,
+			new String[] {Boolean.TRUE.toString()});
+		parameterMap.put(
+			PortletDataHandlerKeys.PORTLET_DATA,
+			new String[] {Boolean.TRUE.toString()});
+		parameterMap.put(
+			PortletDataHandlerKeys.PORTLET_DATA_ALL,
+			new String[] {Boolean.TRUE.toString()});
+
+		return parameterMap;
+	}
+
+	protected void validateImportedLinks(String uuid)
+		throws PortalException, SystemException {
+
+		AssetEntry originalAssetEntry = AssetEntryLocalServiceUtil.getEntry(
+			group.getGroupId(), uuid);
+
+		List<AssetLink> assetLinks = AssetLinkLocalServiceUtil.getLinks(
+			originalAssetEntry.getEntryId());
+
+		AssetEntry importedAssetEntry =  AssetEntryLocalServiceUtil.getEntry(
+			importedGroup.getGroupId(), uuid);
+
+		List<AssetLink> importedAssetLinks = AssetLinkLocalServiceUtil.getLinks(
+			importedAssetEntry.getEntryId());
+
+		Assert.assertEquals(assetLinks.size(), importedAssetLinks.size());
+
+		for (AssetLink assetLink : assetLinks) {
+			AssetEntry sourceAssetEntry = AssetEntryLocalServiceUtil.getEntry(
+				assetLink.getEntryId1());
+
+			AssetEntry targetAssetEntry = AssetEntryLocalServiceUtil.getEntry(
+				assetLink.getEntryId2());
+
+			Iterator<AssetLink> iterator = importedAssetLinks.iterator();
+
+			while (iterator.hasNext()) {
+				AssetLink importedLink = iterator.next();
+
+				AssetEntry importedLinkSourceAssetEntry =
+					AssetEntryLocalServiceUtil.getEntry(
+						importedLink.getEntryId1());
+
+				AssetEntry importedLinkTargetAssetEntry =
+					AssetEntryLocalServiceUtil.getEntry(
+						importedLink.getEntryId2());
+
+				if (!sourceAssetEntry.getClassUuid().equals(
+						importedLinkSourceAssetEntry.getClassUuid())) {
+
+					continue;
+				}
+
+				if (!targetAssetEntry.getClassUuid().equals(
+						importedLinkTargetAssetEntry.getClassUuid())) {
+
+					continue;
+				}
+
+				iterator.remove();
+
+				break;
+			}
+		}
+
+		Assert.assertEquals(0, importedAssetLinks.size());
 	}
 
 }
