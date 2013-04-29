e65cb74da3f7ec79302cd5ddbf7a8ec36bcad456
Ken Duenwald <ken.duenwald@liferay.com>
2013-04-29 16:22:38 -0700

    SOS-1555

--- portal-web/test/functional/com/liferay/portalweb/socialofficesite/forums/mbthread/postnewmbcategorythreadmessagesite/ViewMBCategoryThreadMessageSiteTest.java
+++ portal-web/test/functional/com/liferay/portalweb/socialofficesite/forums/mbthread/postnewmbcategorythreadmessagesite/ViewMBCategoryThreadMessageSiteTest.java
@@ -22,67 +22,152 @@ import com.liferay.portalweb.portal.util.RuntimeVariables;
  */
 public class ViewMBCategoryThreadMessageSiteTest extends BaseTestCase {
 	public void testViewMBCategoryThreadMessageSite() throws Exception {
-		selenium.selectWindow("null");
-		selenium.selectFrame("relative=top");
-		selenium.open("/user/joebloggs/so/dashboard");
-		assertEquals(RuntimeVariables.replace("Sites"),
-			selenium.getText("//div[@id='so-sidebar']/h3"));
-		assertTrue(selenium.isVisible("//input[@class='search-input']"));
-		selenium.type("//input[@class='search-input']",
-			RuntimeVariables.replace("Open"));
-		Thread.sleep(1000);
-		assertEquals(RuntimeVariables.replace("Open Site Name"),
-			selenium.getText(
-				"//li[contains(@class, 'social-office-enabled')]/span[2]/a"));
-		selenium.clickAt("//li[contains(@class, 'social-office-enabled')]/span[2]/a",
-			RuntimeVariables.replace("Open Site Name"));
-		selenium.waitForPageToLoad("30000");
-		assertEquals(RuntimeVariables.replace("Forums"),
-			selenium.getText("//nav/ul/li[contains(.,'Forums')]/a/span"));
-		selenium.clickAt("//nav/ul/li[contains(.,'Forums')]/a/span",
-			RuntimeVariables.replace("Forums"));
-		selenium.waitForPageToLoad("30000");
-		assertEquals(RuntimeVariables.replace("MB Category Name"),
-			selenium.getText("//td[1]/a/strong"));
-		assertEquals(RuntimeVariables.replace("0"),
-			selenium.getText("//td[2]/a"));
-		assertEquals(RuntimeVariables.replace("1"),
-			selenium.getText("//td[3]/a"));
-		assertEquals(RuntimeVariables.replace("1"),
-			selenium.getText("//td[4]/a"));
-		selenium.clickAt("//td[1]/a/strong",
-			RuntimeVariables.replace("MB Category Name"));
-		selenium.waitForPageToLoad("30000");
-		assertEquals(RuntimeVariables.replace(
-				"MB Category Thread Message Subject"),
-			selenium.getText("//td[1]/a"));
-		assertEquals(RuntimeVariables.replace(""), selenium.getText("//td[2]/a"));
-		assertEquals(RuntimeVariables.replace("Joe Bloggs"),
-			selenium.getText("//td[3]/a"));
-		assertEquals(RuntimeVariables.replace("1"),
-			selenium.getText("//td[4]/a"));
-		assertTrue(selenium.isVisible("//td[5]/a"));
-		assertTrue(selenium.isPartialText("//td[6]/a", "By: Joe Bloggs"));
-		assertEquals(RuntimeVariables.replace("Actions"),
-			selenium.getText("//td[7]/span/ul/li/strong/a/span"));
-		selenium.clickAt("//td[1]/a",
-			RuntimeVariables.replace("MB Category Thread Message Subject"));
-		selenium.waitForPageToLoad("30000");
-		assertEquals(RuntimeVariables.replace(
-				"MB Category Thread Message Subject"),
-			selenium.getText("//h1[@class='header-title']"));
-		assertEquals(RuntimeVariables.replace("\u00ab Back to MB Category Name"),
-			selenium.getText("//a[@id='_19_TabsBack']"));
-		assertEquals(RuntimeVariables.replace("Threads [ Previous | Next ]"),
-			selenium.getText("//div[@class='thread-navigation']"));
-		assertEquals(RuntimeVariables.replace("Joe Bloggs"),
-			selenium.getText("//span[@class='user-name']"));
-		assertEquals(RuntimeVariables.replace(
-				"MB Category Thread Message Subject"),
-			selenium.getText("//div[@class='subject']/a"));
-		assertEquals(RuntimeVariables.replace("MB Category Thread Message Body"),
-			selenium.getText("//div[@class='thread-body']"));
-		assertEquals(RuntimeVariables.replace("0 (0 Votes)"),
-			selenium.getText("//div[contains(@id,'ratingThumbContent')]/div"));
+		int label = 1;
+
+		while (label >= 1) {
+			switch (label) {
+			case 1:
+				selenium.selectWindow("null");
+				selenium.selectFrame("relative=top");
+				selenium.open("/user/joebloggs/so/dashboard");
+				assertEquals(RuntimeVariables.replace("Sites"),
+					selenium.getText("//div[@id='so-sidebar']/h3"));
+				assertTrue(selenium.isVisible("//input[@class='search-input']"));
+				selenium.type("//input[@class='search-input']",
+					RuntimeVariables.replace("Open"));
+				Thread.sleep(1000);
+				assertEquals(RuntimeVariables.replace("Open Site Name"),
+					selenium.getText(
+						"//li[contains(@class, 'social-office-enabled')]/span[2]/a"));
+				selenium.clickAt("//li[contains(@class, 'social-office-enabled')]/span[2]/a",
+					RuntimeVariables.replace("Open Site Name"));
+				selenium.waitForPageToLoad("30000");
+				assertEquals(RuntimeVariables.replace("Forums"),
+					selenium.getText("//nav/ul/li[contains(.,'Forums')]/a/span"));
+				selenium.clickAt("//nav/ul/li[contains(.,'Forums')]/a/span",
+					RuntimeVariables.replace("Forums"));
+				selenium.waitForPageToLoad("30000");
+
+				boolean portletTitleVisible = selenium.isVisible(
+						"xPath=(//span[@class='portlet-title-default'])[contains(.,'Breadcrumb')]");
+
+				if (portletTitleVisible) {
+					label = 2;
+
+					continue;
+				}
+
+				selenium.clickAt("//li[@id='_145_toggleControls']",
+					RuntimeVariables.replace("Edit Controls"));
+
+			case 2:
+				assertEquals(RuntimeVariables.replace("Breadcrumb"),
+					selenium.getText(
+						"xPath=(//span[@class='portlet-title-default'])[contains(.,'Breadcrumb')]"));
+				assertEquals(RuntimeVariables.replace("Message Boards"),
+					selenium.getText(
+						"xPath=(//span[@class='portlet-title-default'])[contains(.,'Message Boards')]"));
+				assertEquals(RuntimeVariables.replace("Liferay"),
+					selenium.getText(
+						"xPath=(//ul[contains(@class,'breadcrumbs')]/li)[1]"));
+				assertEquals(RuntimeVariables.replace("Open Site Name"),
+					selenium.getText(
+						"xPath=(//ul[contains(@class,'breadcrumbs')]/li)[2]"));
+				assertEquals(RuntimeVariables.replace("Forums"),
+					selenium.getText(
+						"xPath=(//ul[contains(@class,'breadcrumbs')]/li)[3]"));
+				assertEquals(RuntimeVariables.replace("MB Category Name"),
+					selenium.getText("//td[1]/a/strong"));
+				assertEquals(RuntimeVariables.replace("0"),
+					selenium.getText("//td[2]/a"));
+				assertEquals(RuntimeVariables.replace("1"),
+					selenium.getText("//td[3]/a"));
+				assertEquals(RuntimeVariables.replace("1"),
+					selenium.getText("//td[4]/a"));
+				selenium.clickAt("//td[1]/a/strong",
+					RuntimeVariables.replace("MB Category Name"));
+				selenium.waitForPageToLoad("30000");
+				assertEquals(RuntimeVariables.replace("Breadcrumb"),
+					selenium.getText(
+						"xPath=(//span[@class='portlet-title-default'])[contains(.,'Breadcrumb')]"));
+				assertEquals(RuntimeVariables.replace("Message Boards"),
+					selenium.getText(
+						"xPath=(//span[@class='portlet-title-default'])[contains(.,'Message Boards')]"));
+				assertEquals(RuntimeVariables.replace("Liferay"),
+					selenium.getText(
+						"xPath=(//ul[contains(@class,'breadcrumbs')]/li)[1]"));
+				assertEquals(RuntimeVariables.replace("Open Site Name"),
+					selenium.getText(
+						"xPath=(//ul[contains(@class,'breadcrumbs')]/li)[2]"));
+				assertEquals(RuntimeVariables.replace("Forums"),
+					selenium.getText(
+						"xPath=(//ul[contains(@class,'breadcrumbs')]/li)[3]"));
+				assertEquals(RuntimeVariables.replace("MB Category Name"),
+					selenium.getText(
+						"xPath=(//ul[contains(@class,'breadcrumbs')]/li)[4]"));
+				assertEquals(RuntimeVariables.replace(
+						"MB Category Thread Message Subject"),
+					selenium.getText("//td[1]/a"));
+				assertEquals(RuntimeVariables.replace(""),
+					selenium.getText("//td[2]/a"));
+				assertEquals(RuntimeVariables.replace("Joe Bloggs"),
+					selenium.getText("//td[3]/a"));
+				assertEquals(RuntimeVariables.replace("1"),
+					selenium.getText("//td[4]/a"));
+				assertTrue(selenium.isVisible("//td[5]/a"));
+				assertTrue(selenium.isPartialText("//td[6]/a", "By: Joe Bloggs"));
+				assertEquals(RuntimeVariables.replace("Actions"),
+					selenium.getText("//td[7]/span/ul/li/strong/a/span"));
+				selenium.clickAt("//td[1]/a",
+					RuntimeVariables.replace(
+						"MB Category Thread Message Subject"));
+				selenium.waitForPageToLoad("30000");
+				assertEquals(RuntimeVariables.replace("Breadcrumb"),
+					selenium.getText(
+						"xPath=(//span[@class='portlet-title-default'])[contains(.,'Breadcrumb')]"));
+				assertEquals(RuntimeVariables.replace("Message Boards"),
+					selenium.getText(
+						"xPath=(//span[@class='portlet-title-default'])[contains(.,'Message Boards')]"));
+				assertEquals(RuntimeVariables.replace("Liferay"),
+					selenium.getText(
+						"xPath=(//ul[contains(@class,'breadcrumbs')]/li)[1]"));
+				assertEquals(RuntimeVariables.replace("Open Site Name"),
+					selenium.getText(
+						"xPath=(//ul[contains(@class,'breadcrumbs')]/li)[2]"));
+				assertEquals(RuntimeVariables.replace("Forums"),
+					selenium.getText(
+						"xPath=(//ul[contains(@class,'breadcrumbs')]/li)[3]"));
+				assertEquals(RuntimeVariables.replace("MB Category Name"),
+					selenium.getText(
+						"xPath=(//ul[contains(@class,'breadcrumbs')]/li)[4]"));
+				assertEquals(RuntimeVariables.replace(
+						"MB Category Thread Message Subject"),
+					selenium.getText(
+						"xPath=(//ul[contains(@class,'breadcrumbs')]/li)[5]"));
+				assertEquals(RuntimeVariables.replace(
+						"MB Category Thread Message Subject"),
+					selenium.getText("//h1[@class='header-title']"));
+				assertEquals(RuntimeVariables.replace(
+						"\u00ab Back to MB Category Name"),
+					selenium.getText("//a[@id='_19_TabsBack']"));
+				assertEquals(RuntimeVariables.replace(
+						"Threads [ Previous | Next ]"),
+					selenium.getText("//div[@class='thread-navigation']"));
+				assertEquals(RuntimeVariables.replace("Joe Bloggs"),
+					selenium.getText("//span[@class='user-name']"));
+				assertEquals(RuntimeVariables.replace(
+						"MB Category Thread Message Subject"),
+					selenium.getText("//div[@class='subject']/a"));
+				assertEquals(RuntimeVariables.replace(
+						"MB Category Thread Message Body"),
+					selenium.getText("//div[@class='thread-body']"));
+				assertEquals(RuntimeVariables.replace("0 (0 Votes)"),
+					selenium.getText(
+						"//div[contains(@id,'ratingThumbContent')]/div"));
+
+			case 100:
+				label = -1;
+			}
+		}
 	}
 }
