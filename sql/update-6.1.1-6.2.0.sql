860085d18665f5dbca88fbe593b49f8a04d3fe3a
Eduardo Garcia <eduardo.garcia@liferay.com>
2013-05-10 10:42:24 -0700

    LPS-34896 Upgrade Process

--- sql/update-6.1.1-6.2.0.sql
+++ sql/update-6.1.1-6.2.0.sql
@@ -334,6 +334,7 @@ alter table Group_ add uuid_ VARCHAR(75) null;
 alter table Group_ add treePath STRING null;
 
 update Group_ set site = FALSE where name = 'Control Panel';
+update Group_ set site = TRUE where friendlyURL = '/global';
 
 drop table Groups_Permissions;
 
