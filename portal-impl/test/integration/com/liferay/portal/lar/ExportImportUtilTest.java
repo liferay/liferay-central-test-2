zberentey/liferay-portal

LPS-34705 Fix ExportImportUtilTest

portal-impl/test/integration/com/liferay/portal/lar/ExportImportUtilTest.java
@@ -30,6 +30,7 @@ import com.liferay.portal.kernel.zip.ZipReader;
 import com.liferay.portal.kernel.zip.ZipWriter;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Layout;
+import com.liferay.portal.model.StagedModel;
 import com.liferay.portal.repository.liferayrepository.model.LiferayFileEntry;
 import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.ServiceTestUtil;
@@ -44,6 +45,7 @@ import com.liferay.portlet.documentlibrary.model.DLFileEntry;
 import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
 import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalServiceUtil;
 import com.liferay.portlet.documentlibrary.util.DLAppTestUtil;
+import com.liferay.portlet.journal.util.JournalTestUtil;
 
 import java.io.File;
 import java.io.InputStream;
@@ -51,7 +53,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -90,6 +91,10 @@ public class ExportImportUtilTest {
 			ServiceTestUtil.randomString() + ".txt",
 			ServiceTestUtil.randomString(), true);
 
+		_parentStagedModel = JournalTestUtil.addArticle(
+			_stagingGroup.getGroupId(), ServiceTestUtil.randomString(),
+			ServiceTestUtil.randomString());
+
 		LiferayFileEntry liferayFileEntry = (LiferayFileEntry)_fileEntry;
 
 		DLFileEntry dlFileEntry = liferayFileEntry.getDLFileEntry();
@@ -102,7 +107,7 @@ public class ExportImportUtilTest {
 
 		_portletDataContextExport = new PortletDataContextImpl(
 			_stagingGroup.getCompanyId(), _stagingGroup.getGroupId(),
-			new HashMap<String, String[]>(), new HashSet<String>(),
+			new HashMap<String, String[]>(),
 			new Date(System.currentTimeMillis() - Time.HOUR), new Date(),
 			testReaderWriter);
 
@@ -112,7 +117,7 @@ public class ExportImportUtilTest {
 
 		_portletDataContextImport = new PortletDataContextImpl(
 			_stagingGroup.getCompanyId(), _stagingGroup.getGroupId(),
-			new HashMap<String, String[]>(), new HashSet<String>(),
+			new HashMap<String, String[]>(),
 			new CurrentUserIdStrategy(TestPropsValues.getUser()),
 			testReaderWriter);
 
@@ -139,7 +144,8 @@ public class ExportImportUtilTest {
 		List<String> urls = getURLs(content);
 
 		content = ExportImportUtil.exportContentReferences(
-			_portletDataContextExport, rootElement.element("entry"), content);
+			_portletDataContextExport, _parentStagedModel,
+			rootElement.element("entry"), content);
 
 		for (String url : urls) {
 			Assert.assertFalse(content.contains(url));
@@ -171,7 +177,8 @@ public class ExportImportUtilTest {
 			getContent("layout_references.txt"), _fileEntry);
 
 		content = ExportImportUtil.exportContentReferences(
-			_portletDataContextExport, rootElement.element("entry"), content);
+			_portletDataContextExport, _parentStagedModel,
+			rootElement.element("entry"), content);
 
 		Assert.assertFalse(
 			content.contains(PortalUtil.getPathFriendlyURLPrivateGroup()));
@@ -196,7 +203,8 @@ public class ExportImportUtilTest {
 			getContent("layout_links.txt"), _fileEntry);
 
 		content = ExportImportUtil.exportContentReferences(
-			_portletDataContextExport, rootElement.element("entry"), content);
+			_portletDataContextExport, _parentStagedModel,
+			rootElement.element("entry"), content);
 
 		StringBundler sb = new StringBundler(5);
 
@@ -230,7 +238,8 @@ public class ExportImportUtilTest {
 			getContent("dl_references.txt"), _fileEntry);
 
 		content = ExportImportUtil.exportContentReferences(
-			_portletDataContextExport, entryElement, content);
+			_portletDataContextExport, _parentStagedModel, entryElement,
+			content);
 		content = ExportImportUtil.importContentReferences(
 			_portletDataContextImport, entryElement, content);
 
@@ -248,7 +257,8 @@ public class ExportImportUtilTest {
 			getContent("layout_references.txt"), _fileEntry);
 
 		content = ExportImportUtil.exportContentReferences(
-			_portletDataContextExport, entryElement, content);
+			_portletDataContextExport, _parentStagedModel, entryElement,
+			content);
 		content = ExportImportUtil.importContentReferences(
 			_portletDataContextExport, entryElement, content);
 
@@ -278,10 +288,12 @@ public class ExportImportUtilTest {
 			getContent("layout_links.txt"), _fileEntry);
 
 		content = ExportImportUtil.exportContentReferences(
-			_portletDataContextExport, entryElement, content);
+			_portletDataContextExport, _parentStagedModel, entryElement,
+			content);
 
 		String importedContent = ExportImportUtil.exportContentReferences(
-			_portletDataContextExport, entryElement, content);
+			_portletDataContextExport, _parentStagedModel, entryElement,
+			content);
 
 		Assert.assertEquals(importedContent, content);
 	}
@@ -338,6 +350,7 @@ public class ExportImportUtilTest {
 
 	private FileEntry _fileEntry;
 	private Group _liveGroup;
+	private StagedModel _parentStagedModel;
 	private PortletDataContext _portletDataContextExport;
 	private PortletDataContext _portletDataContextImport;
 	private Group _stagingGroup;

liferay/liferay-central-test-2

portal-impl/test/integration/com/liferay/portal/lar/ExportImportUtilTest.java
@@ -30,6 +30,7 @@ import com.liferay.portal.kernel.zip.ZipReader;
 import com.liferay.portal.kernel.zip.ZipWriter;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Layout;
+import com.liferay.portal.model.StagedModel;
 import com.liferay.portal.repository.liferayrepository.model.LiferayFileEntry;
 import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.ServiceTestUtil;
@@ -44,6 +45,7 @@ import com.liferay.portlet.documentlibrary.model.DLFileEntry;
 import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
 import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalServiceUtil;
 import com.liferay.portlet.documentlibrary.util.DLAppTestUtil;
+import com.liferay.portlet.journal.util.JournalTestUtil;
 
 import java.io.File;
 import java.io.InputStream;
@@ -51,7 +53,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -90,6 +91,10 @@ public class ExportImportUtilTest {
 			ServiceTestUtil.randomString() + ".txt",
 			ServiceTestUtil.randomString(), true);
 
+		_referrerStagedModel = JournalTestUtil.addArticle(
+			_stagingGroup.getGroupId(), ServiceTestUtil.randomString(),
+			ServiceTestUtil.randomString());
+
 		LiferayFileEntry liferayFileEntry = (LiferayFileEntry)_fileEntry;
 
 		DLFileEntry dlFileEntry = liferayFileEntry.getDLFileEntry();
@@ -102,7 +107,7 @@ public class ExportImportUtilTest {
 
 		_portletDataContextExport = new PortletDataContextImpl(
 			_stagingGroup.getCompanyId(), _stagingGroup.getGroupId(),
-			new HashMap<String, String[]>(), new HashSet<String>(),
+			new HashMap<String, String[]>(),
 			new Date(System.currentTimeMillis() - Time.HOUR), new Date(),
 			testReaderWriter);
 
@@ -112,7 +117,7 @@ public class ExportImportUtilTest {
 
 		_portletDataContextImport = new PortletDataContextImpl(
 			_stagingGroup.getCompanyId(), _stagingGroup.getGroupId(),
-			new HashMap<String, String[]>(), new HashSet<String>(),
+			new HashMap<String, String[]>(),
 			new CurrentUserIdStrategy(TestPropsValues.getUser()),
 			testReaderWriter);
 
@@ -139,7 +144,8 @@ public class ExportImportUtilTest {
 		List<String> urls = getURLs(content);
 
 		content = ExportImportUtil.exportContentReferences(
-			_portletDataContextExport, rootElement.element("entry"), content);
+			_portletDataContextExport, _referrerStagedModel,
+			rootElement.element("entry"), content);
 
 		for (String url : urls) {
 			Assert.assertFalse(content.contains(url));
@@ -171,7 +177,8 @@ public class ExportImportUtilTest {
 			getContent("layout_references.txt"), _fileEntry);
 
 		content = ExportImportUtil.exportContentReferences(
-			_portletDataContextExport, rootElement.element("entry"), content);
+			_portletDataContextExport, _referrerStagedModel,
+			rootElement.element("entry"), content);
 
 		Assert.assertFalse(
 			content.contains(PortalUtil.getPathFriendlyURLPrivateGroup()));
@@ -196,7 +203,8 @@ public class ExportImportUtilTest {
 			getContent("layout_links.txt"), _fileEntry);
 
 		content = ExportImportUtil.exportContentReferences(
-			_portletDataContextExport, rootElement.element("entry"), content);
+			_portletDataContextExport, _referrerStagedModel,
+			rootElement.element("entry"), content);
 
 		StringBundler sb = new StringBundler(5);
 
@@ -230,7 +238,8 @@ public class ExportImportUtilTest {
 			getContent("dl_references.txt"), _fileEntry);
 
 		content = ExportImportUtil.exportContentReferences(
-			_portletDataContextExport, entryElement, content);
+			_portletDataContextExport, _referrerStagedModel, entryElement,
+			content);
 		content = ExportImportUtil.importContentReferences(
 			_portletDataContextImport, entryElement, content);
 
@@ -248,7 +257,8 @@ public class ExportImportUtilTest {
 			getContent("layout_references.txt"), _fileEntry);
 
 		content = ExportImportUtil.exportContentReferences(
-			_portletDataContextExport, entryElement, content);
+			_portletDataContextExport, _referrerStagedModel, entryElement,
+			content);
 		content = ExportImportUtil.importContentReferences(
 			_portletDataContextExport, entryElement, content);
 
@@ -278,10 +288,12 @@ public class ExportImportUtilTest {
 			getContent("layout_links.txt"), _fileEntry);
 
 		content = ExportImportUtil.exportContentReferences(
-			_portletDataContextExport, entryElement, content);
+			_portletDataContextExport, _referrerStagedModel, entryElement,
+			content);
 
 		String importedContent = ExportImportUtil.exportContentReferences(
-			_portletDataContextExport, entryElement, content);
+			_portletDataContextExport, _referrerStagedModel, entryElement,
+			content);
 
 		Assert.assertEquals(importedContent, content);
 	}
@@ -340,6 +352,7 @@ public class ExportImportUtilTest {
 	private Group _liveGroup;
 	private PortletDataContext _portletDataContextExport;
 	private PortletDataContext _portletDataContextImport;
+	private StagedModel _referrerStagedModel;
 	private Group _stagingGroup;
 
 	private class TestReaderWriter implements ZipReader, ZipWriter {