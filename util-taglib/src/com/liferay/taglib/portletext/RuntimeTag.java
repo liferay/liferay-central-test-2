928f76deb3e339dc336e1fbc7b5f7307229b3eac
Jonathan Lee <Jonathan.Lee@liferay.com>
2013-05-10 16:43:31 -0700

    LPS-35061 Add the logic to write head/footer css and js onto the page if the runtime portlet is run on  the first time.

--- util-taglib/src/com/liferay/taglib/portletext/RuntimeTag.java
+++ util-taglib/src/com/liferay/taglib/portletext/RuntimeTag.java
@@ -14,6 +14,9 @@
 
 package com.liferay.taglib.portletext;
 
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.PortletContainerUtil;
@@ -22,6 +25,9 @@ import com.liferay.portal.kernel.portlet.PortletParameterUtil;
 import com.liferay.portal.kernel.portlet.RestrictPortletServletRequest;
 import com.liferay.portal.kernel.servlet.DynamicServletRequest;
 import com.liferay.portal.kernel.servlet.PipingServletResponse;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.HttpUtil;
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.LayoutTypePortlet;
@@ -29,8 +35,14 @@ import com.liferay.portal.model.Portlet;
 import com.liferay.portal.service.PortletLocalServiceUtil;
 import com.liferay.portal.service.PortletPreferencesLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.PortletKeys;
 import com.liferay.portlet.PortletPreferencesFactoryUtil;
+
+import java.io.PrintWriter;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -99,12 +111,18 @@ public class RuntimeTag extends TagSupport {
 				}
 			}
 
+			Layout layout = themeDisplay.getLayout();
+
 			Portlet portlet = getPortlet(
 				themeDisplay.getCompanyId(), portletId);
 
-			if (PortletPreferencesLocalServiceUtil.getPortletPreferencesCount(
+			JSONObject jsonObject = null;
+
+			if ((PortletPreferencesLocalServiceUtil.getPortletPreferencesCount(
 					PortletKeys.PREFS_OWNER_TYPE_LAYOUT, themeDisplay.getPlid(),
-					portletId) < 1) {
+					portletId) < 1) ||
+				layout.isTypeControlPanel() ||
+				layout.isTypePanel()) {
 
 				PortletPreferencesFactoryUtil.getPortletSetup(
 					request, portletId, defaultPreferences);
@@ -116,9 +134,65 @@ public class RuntimeTag extends TagSupport {
 					portletLayoutListener.onAddToLayout(
 						portletId, themeDisplay.getPlid());
 				}
+
+				jsonObject = JSONFactoryUtil.createJSONObject();
+
+				populatePortletJSONObject(
+					restrictPortletServletRequest, StringPool.BLANK, portlet,
+					jsonObject);
+			}
+
+			if (jsonObject != null) {
+				PrintWriter writer = response.getWriter();
+
+				JSONArray jsonArray = jsonObject.getJSONArray("headerCssPaths");
+
+				for (int i = 0; i < jsonArray.length(); i++) {
+					String value = jsonArray.getString(i);
+
+					writer.print("<link href=\"");
+					writer.print(HtmlUtil.escape(value));
+					writer.println(
+						"\" rel=\"stylesheet\" type=\"text/css\" />");
+				}
+
+				jsonArray = jsonObject.getJSONArray("headerJavaScriptPaths");
+
+				for (int i = 0; i < jsonArray.length(); i++) {
+					String value = jsonArray.getString(i);
+
+					writer.print("<script src=\"");
+					writer.print(HtmlUtil.escape(value));
+					writer.println("\" type=\"text/javascript\"></script>");
+				}
 			}
 
 			PortletContainerUtil.render(request, response, portlet);
+
+			if (jsonObject != null) {
+				PrintWriter writer = response.getWriter();
+
+				JSONArray jsonArray = jsonObject.getJSONArray("footerCssPaths");
+
+				for (int i = 0; i < jsonArray.length(); i++) {
+					String value = jsonArray.getString(i);
+
+					writer.print("<link href=\"");
+					writer.print(HtmlUtil.escape(value));
+					writer.println(
+						"\" rel=\"stylesheet\" type=\"text/css\" />");
+				}
+
+				jsonArray = jsonObject.getJSONArray("footerJavaScriptPaths");
+
+				for (int i = 0; i < jsonArray.length(); i++) {
+					String value = jsonArray.getString(i);
+
+					writer.print("<script src=\"");
+					writer.print(HtmlUtil.escape(value));
+					writer.println("\" type=\"text/javascript\"></script>");
+				}
+			}
 		}
 		finally {
 			restrictPortletServletRequest.mergeSharedAttributes();
@@ -185,6 +259,199 @@ public class RuntimeTag extends TagSupport {
 		portlet.setStatic(true);
 
 		return portlet;
+	}
+
+	protected static String getRootPortletId(Portlet portlet) {
+
+		// Workaround for portlet.getRootPortletId() because that does not
+		// return the proper root portlet ID for OpenSocial and WSRP portlets
+
+		Portlet rootPortlet = portlet.getRootPortlet();
+
+		return rootPortlet.getPortletId();
+	}
+
+	protected static void populatePortletJSONObject(
+			HttpServletRequest request, String portletHTML, Portlet portlet,
+			JSONObject jsonObject)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		LayoutTypePortlet layoutTypePortlet =
+			themeDisplay.getLayoutTypePortlet();
+
+		jsonObject.put("portletHTML", portletHTML);
+		jsonObject.put("refresh", !portlet.isAjaxable());
+
+		Set<String> footerCssSet = new LinkedHashSet<String>();
+		Set<String> footerJavaScriptSet = new LinkedHashSet<String>();
+		Set<String> headerCssSet = new LinkedHashSet<String>();
+		Set<String> headerJavaScriptSet = new LinkedHashSet<String>();
+
+		boolean portletOnLayout = false;
+
+		String rootPortletId = getRootPortletId(portlet);
+		String portletId = portlet.getPortletId();
+
+		for (Portlet layoutPortlet : layoutTypePortlet.getAllPortlets()) {
+
+			// Check to see if an instance of this portlet is already in the
+			// layout, but ignore the portlet that was just added
+
+			String layoutPortletRootPortletId = getRootPortletId(layoutPortlet);
+
+			if (rootPortletId.equals(layoutPortletRootPortletId) &&
+				!portletId.equals(layoutPortlet.getPortletId())) {
+
+				portletOnLayout = true;
+
+				break;
+			}
+		}
+
+		if (!portletOnLayout && portlet.isAjaxable()) {
+			Portlet rootPortlet = portlet.getRootPortlet();
+
+			for (String footerPortalCss : portlet.getFooterPortalCss()) {
+				if (!HttpUtil.hasProtocol(footerPortalCss)) {
+					footerPortalCss =
+						PortalUtil.getPathContext() + footerPortalCss;
+
+					footerPortalCss = PortalUtil.getStaticResourceURL(
+						request, footerPortalCss, rootPortlet.getTimestamp());
+				}
+
+				footerCssSet.add(footerPortalCss);
+			}
+
+			for (String footerPortalJavaScript :
+					portlet.getFooterPortalJavaScript()) {
+
+				if (!HttpUtil.hasProtocol(footerPortalJavaScript)) {
+					footerPortalJavaScript =
+						PortalUtil.getPathContext() + footerPortalJavaScript;
+
+					footerPortalJavaScript = PortalUtil.getStaticResourceURL(
+						request, footerPortalJavaScript,
+						rootPortlet.getTimestamp());
+				}
+
+				footerJavaScriptSet.add(footerPortalJavaScript);
+			}
+
+			for (String footerPortletCss : portlet.getFooterPortletCss()) {
+				if (!HttpUtil.hasProtocol(footerPortletCss)) {
+					footerPortletCss =
+						portlet.getStaticResourcePath() + footerPortletCss;
+
+					footerPortletCss = PortalUtil.getStaticResourceURL(
+						request, footerPortletCss, rootPortlet.getTimestamp());
+				}
+
+				footerCssSet.add(footerPortletCss);
+			}
+
+			for (String footerPortletJavaScript :
+					portlet.getFooterPortletJavaScript()) {
+
+				if (!HttpUtil.hasProtocol(footerPortletJavaScript)) {
+					footerPortletJavaScript =
+						portlet.getStaticResourcePath() +
+							footerPortletJavaScript;
+
+					footerPortletJavaScript = PortalUtil.getStaticResourceURL(
+						request, footerPortletJavaScript,
+						rootPortlet.getTimestamp());
+				}
+
+				footerJavaScriptSet.add(footerPortletJavaScript);
+			}
+
+			for (String headerPortalCss : portlet.getHeaderPortalCss()) {
+				if (!HttpUtil.hasProtocol(headerPortalCss)) {
+					headerPortalCss =
+						PortalUtil.getPathContext() + headerPortalCss;
+
+					headerPortalCss = PortalUtil.getStaticResourceURL(
+						request, headerPortalCss, rootPortlet.getTimestamp());
+				}
+
+				headerCssSet.add(headerPortalCss);
+			}
+
+			for (String headerPortalJavaScript :
+					portlet.getHeaderPortalJavaScript()) {
+
+				if (!HttpUtil.hasProtocol(headerPortalJavaScript)) {
+					headerPortalJavaScript =
+						PortalUtil.getPathContext() + headerPortalJavaScript;
+
+					headerPortalJavaScript = PortalUtil.getStaticResourceURL(
+						request, headerPortalJavaScript,
+						rootPortlet.getTimestamp());
+				}
+
+				headerJavaScriptSet.add(headerPortalJavaScript);
+			}
+
+			for (String headerPortletCss : portlet.getHeaderPortletCss()) {
+				if (!HttpUtil.hasProtocol(headerPortletCss)) {
+					headerPortletCss =
+						portlet.getStaticResourcePath() + headerPortletCss;
+
+					headerPortletCss = PortalUtil.getStaticResourceURL(
+						request, headerPortletCss, rootPortlet.getTimestamp());
+				}
+
+				headerCssSet.add(headerPortletCss);
+			}
+
+			for (String headerPortletJavaScript :
+					portlet.getHeaderPortletJavaScript()) {
+
+				if (!HttpUtil.hasProtocol(headerPortletJavaScript)) {
+					headerPortletJavaScript =
+						portlet.getStaticResourcePath() +
+							headerPortletJavaScript;
+
+					headerPortletJavaScript = PortalUtil.getStaticResourceURL(
+						request, headerPortletJavaScript,
+						rootPortlet.getTimestamp());
+				}
+
+				headerJavaScriptSet.add(headerPortletJavaScript);
+			}
+		}
+
+		String footerCssPaths = JSONFactoryUtil.serialize(
+			footerCssSet.toArray(new String[footerCssSet.size()]));
+
+		jsonObject.put(
+			"footerCssPaths", JSONFactoryUtil.createJSONArray(footerCssPaths));
+
+		String footerJavaScriptPaths = JSONFactoryUtil.serialize(
+			footerJavaScriptSet.toArray(
+				new String[footerJavaScriptSet.size()]));
+
+		jsonObject.put(
+			"footerJavaScriptPaths",
+			JSONFactoryUtil.createJSONArray(footerJavaScriptPaths));
+
+		String headerCssPaths = JSONFactoryUtil.serialize(
+			headerCssSet.toArray(new String[headerCssSet.size()]));
+
+		jsonObject.put(
+			"headerCssPaths", JSONFactoryUtil.createJSONArray(headerCssPaths));
+
+		String headerJavaScriptPaths = JSONFactoryUtil.serialize(
+			headerJavaScriptSet.toArray(
+				new String[headerJavaScriptSet.size()]));
+
+		jsonObject.put(
+			"headerJavaScriptPaths",
+			JSONFactoryUtil.createJSONArray(headerJavaScriptPaths));
 	}
 
 	private static Log _log = LogFactoryUtil.getLog(RuntimeTag.class);
