6b2ecdf0dafbbcc7850381438faaa8b038b21797
Chas Austin <charles.austin@liferay.com>
2013-05-14 09:15:03 -0700

    Prepare 6.2.0 CE M6 release

--- portal-service/src/com/liferay/portal/kernel/util/ReleaseInfo.java
+++ portal-service/src/com/liferay/portal/kernel/util/ReleaseInfo.java
@@ -140,7 +140,7 @@ public class ReleaseInfo {
 
 	private static final String _CODE_NAME = "Newton";
 
-	private static final String _DATE = "April 25, 2013";
+	private static final String _DATE = "May 24, 2013";
 
 	private static final String _NAME = "Liferay Portal Community Edition";
 
@@ -156,7 +156,7 @@ public class ReleaseInfo {
 
 	private static final String _VERSION = "6.2.0";
 
-	private static final String _VERSION_DISPLAY_NAME = "6.2.0 CE M5";
+	private static final String _VERSION_DISPLAY_NAME = "6.2.0 CE M6";
 
 	private static String _releaseInfo;
 	private static String _serverInfo;
