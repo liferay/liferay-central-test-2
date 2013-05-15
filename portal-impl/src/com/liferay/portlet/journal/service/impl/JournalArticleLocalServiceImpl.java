66208b97786891ca23b9e093c4b8e8a6c413ccf1
Zsolt Balogh <zsolt.balogh@liferay.com>
2013-05-14 17:55:43 -0700

    LPS-34991 Article's display date is null when it's a structure default values article

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -2404,7 +2404,8 @@ public class JournalArticleLocalServiceImpl
 
 			Date expirationDate = article.getExpirationDate();
 
-			if (article.getDisplayDate().before(now) &&
+			if (((article.getDisplayDate() == null) ||
+				 article.getDisplayDate().before(now)) &&
 				((expirationDate == null) || expirationDate.after(now))) {
 
 				return article;
