juliocamarero/liferay-portal

LPS-35221 Autogenerated

portal-service/src/com/liferay/portal/service/LayoutLocalService.java
@@ -1076,13 +1076,14 @@ public interface LayoutLocalService extends BaseLocalService,
 	*
 	* @param plid the primary key of the layout
 	* @param friendlyURL the friendly URL to be assigned
+	* @param locale the locale of the friendly URL
 	* @return the updated layout
 	* @throws PortalException if a group or layout with the primary key could
 	not be found
 	* @throws SystemException if a system exception occurred
 	*/
 	public com.liferay.portal.model.Layout updateFriendlyURL(long plid,
-		java.lang.String friendlyURL)
+		java.lang.String friendlyURL, java.util.Locale locale)
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException;
 

portal-service/src/com/liferay/portal/service/LayoutLocalServiceUtil.java
@@ -1218,16 +1218,17 @@ public class LayoutLocalServiceUtil {
 	*
 	* @param plid the primary key of the layout
 	* @param friendlyURL the friendly URL to be assigned
+	* @param locale the locale of the friendly URL
 	* @return the updated layout
 	* @throws PortalException if a group or layout with the primary key could
 	not be found
 	* @throws SystemException if a system exception occurred
 	*/
 	public static com.liferay.portal.model.Layout updateFriendlyURL(long plid,
-		java.lang.String friendlyURL)
+		java.lang.String friendlyURL, java.util.Locale locale)
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException {
-		return getService().updateFriendlyURL(plid, friendlyURL);
+		return getService().updateFriendlyURL(plid, friendlyURL, locale);
 	}
 
 	public static com.liferay.portal.model.Layout updateLayout(long groupId,

liferay/liferay-central-test-2

portal-service/src/com/liferay/portal/service/LayoutLocalService.java
@@ -1082,7 +1082,7 @@ public interface LayoutLocalService extends BaseLocalService,
 	* @throws SystemException if a system exception occurred
 	*/
 	public com.liferay.portal.model.Layout updateFriendlyURL(long plid,
-		java.lang.String friendlyURL)
+		java.lang.String friendlyURL, java.lang.String languageId)
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException;
 

portal-service/src/com/liferay/portal/service/LayoutLocalServiceUtil.java
@@ -1224,10 +1224,10 @@ public class LayoutLocalServiceUtil {
 	* @throws SystemException if a system exception occurred
 	*/
 	public static com.liferay.portal.model.Layout updateFriendlyURL(long plid,
-		java.lang.String friendlyURL)
+		java.lang.String friendlyURL, java.lang.String languageId)
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException {
-		return getService().updateFriendlyURL(plid, friendlyURL);
+		return getService().updateFriendlyURL(plid, friendlyURL, languageId);
 	}
 
 	public static com.liferay.portal.model.Layout updateLayout(long groupId,