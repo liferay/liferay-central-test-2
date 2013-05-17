a0f5403fb2754561b8308828f994a21822161f34
CalvinKeum <Calvin.Keum@liferay.com>
2013-05-17 09:57:07 -0700

    LPS-35416 Reindex the baseModelClass based on the model being updated

--- util-bridges/src/com/liferay/util/bridges/alloy/BaseAlloyControllerImpl.java
+++ util-bridges/src/com/liferay/util/bridges/alloy/BaseAlloyControllerImpl.java
@@ -190,6 +190,15 @@ public abstract class BaseAlloyControllerImpl implements AlloyController {
 
 			indexer.reindex(baseModel);
 		}
+		else {
+			Indexer baseModelIndexer = 
+				IndexerRegistryUtil.nullSafeGetIndexer(
+					baseModel.getModelClass());
+
+			if (baseModelIndexer != null) {
+				baseModelIndexer.reindex(baseModel);
+			}
+		}	
 	}
 
 	protected void addSuccessMessage() {
