1befa7d53bece457add03aa908a00be75a8db924
Brian Chan <brian.chan@liferay.com>
2013-05-23 15:52:56 -0700

    LPS-30343 SF

--- util-java/src/com/liferay/util/resiliency/spi/provider/SPIClassPathContextListener.java
+++ util-java/src/com/liferay/util/resiliency/spi/provider/SPIClassPathContextListener.java
@@ -41,7 +41,7 @@ import javax.servlet.ServletContextListener;
  */
 public class SPIClassPathContextListener implements ServletContextListener {
 
-	public static volatile String SPI_CLASSPATH = StringPool.BLANK;
+	public static volatile String SPI_CLASS_PATH = StringPool.BLANK;
 
 	@Override
 	public void contextDestroyed(ServletContextEvent servletContextEvent) {
@@ -54,28 +54,25 @@ public class SPIClassPathContextListener implements ServletContextListener {
 
 	@Override
 	public void contextInitialized(ServletContextEvent servletContextEvent) {
-
-		// Generate classpath
-
 		ServletContext servletContext = servletContextEvent.getServletContext();
 
 		String contextPath = servletContext.getRealPath(StringPool.BLANK);
-		String embeddedLibsDir = servletContext.getInitParameter(
-			"embeddedLibsDir");
+		String embeddedLibDirName = servletContext.getInitParameter(
+			"embeddedLibDir");
 
-		File embeddedLibsFolder = new File(contextPath, embeddedLibsDir);
+		File embeddedLibDir = new File(contextPath, embeddedLibDirName);
 
-		if (!embeddedLibsFolder.exists() || !embeddedLibsFolder.isDirectory()) {
+		if (!embeddedLibDir.exists() || !embeddedLibDir.isDirectory()) {
 			_log.error(
-				"Can not find embedded libs folder : " +
-					embeddedLibsFolder.getAbsolutePath());
+				"Unable to find embedded lib directory " +
+					embeddedLibDir.getAbsolutePath());
 
 			return;
 		}
 
 		List<File> jarFiles = new ArrayList<File>();
 
-		for (File file : embeddedLibsFolder.listFiles()) {
+		for (File file : embeddedLibDir.listFiles()) {
 			String fileName = file.getName();
 
 			if (fileName.endsWith(".jar")) {
@@ -91,7 +88,7 @@ public class SPIClassPathContextListener implements ServletContextListener {
 		}
 
 		if (_log.isDebugEnabled()) {
-			_log.debug("Generated embedded libs classpath : " + sb.toString());
+			_log.debug("Embedded lib class path " + sb.toString());
 		}
 
 		sb.append(contextPath);
@@ -100,13 +97,11 @@ public class SPIClassPathContextListener implements ServletContextListener {
 
 		sb.append(ClassPathUtil.getGlobalClassPath());
 
-		SPI_CLASSPATH = sb.toString();
+		SPI_CLASS_PATH = sb.toString();
 
 		if (_log.isDebugEnabled()) {
-			_log.debug("Generated SPI classpath : " + SPI_CLASSPATH);
+			_log.debug("SPI class path " + SPI_CLASS_PATH);
 		}
-
-		// Generate SPIProvider instance
 
 		String spiProviderClassName = servletContext.getInitParameter(
 			"spiProviderClassName");
@@ -127,9 +122,9 @@ public class SPIClassPathContextListener implements ServletContextListener {
 
 			if (!result) {
 				_log.error(
-					"Unable to register generated SPIProvider " + spiProvider +
-						". A SPIProvider already registered in this " +
-							"ServletContext " + servletContext);
+					"Duplicate SPI provider " + spiProvider +
+						" is already registered in servlet context " +
+							servletContext.getContextPath());
 			}
 			else {
 				MPIHelperUtil.registerSPIProvider(spiProvider);
@@ -137,45 +132,17 @@ public class SPIClassPathContextListener implements ServletContextListener {
 		}
 		catch (Exception e) {
 			_log.error(
-				"Unable to create SPIProvide with name " + spiProviderClassName,
+				"Unable to create SPI provider with name " +
+					spiProviderClassName,
 				e);
 		}
 	}
 
-	/**
-	 * This is an important hack to support process family tree.
-	 * It allows a SPI to create its own SPI instances.
-	 * It may seem naturally that SPI should able to create new SPI, as long as
-	 * it has the spi provider web.
-	 *
-	 * But there is a tricky classloading issue.
-	 * Let's call the original MPI's global classpath mcp1, the original spi
-	 * provider web's private classpath scp1.
-	 *
-	 * Because SPI1 is forked from spi provider web, so its global classpath is
-	 * mcp1 + scp1. In order to let SPI1 forks its own SPI, it needs the same
-	 * spi provider web whose private classpath is still scp1.
-	 *
-	 * However the new SPI2 forked from SPI1 will automatically load the spi
-	 * provider web classes from global classpath mcp1 + scp1, not the priavte
-	 * scp1.
-	 * So later on if the class in scp1 refers other private class like those in
-	 * util-java.jar or do a cast to a real private Class type in spi provider
-	 * web. We will see class cast failure.
-	 *
-	 * This method allow you to directly load class from the given
-	 * ClassLoader(the plugin ClassLoader), to bypass the parent ClassLoader(the
-	 * global ClassLoader). Because ClassLoader will automatically cache up the
-	 * loaded classes, after we load them once, they will always be used.
-	 */
 	protected static Class<?> loadClassDirectly(
 			ClassLoader classLoader, String className)
 		throws Exception {
 
 		synchronized (classLoader) {
-
-			// Load from cache first
-
 			Method findLoadedClassMethod = ReflectionUtil.getDeclaredMethod(
 				ClassLoader.class, "findLoadedClass", String.class);
 
@@ -183,9 +150,6 @@ public class SPIClassPathContextListener implements ServletContextListener {
 				classLoader, className);
 
 			if (clazz == null) {
-
-				// Miss cache, skip parent, directly load from given ClassLoader
-
 				Method findClassMethod = ReflectionUtil.getDeclaredMethod(
 					ClassLoader.class, "findClass", String.class);
 
