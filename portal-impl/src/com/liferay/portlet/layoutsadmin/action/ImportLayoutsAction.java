76394fb272a3f1261996f0bdc18caf631f62bd94
Brian Chan <brian.chan@liferay.com>
2013-05-23 17:18:31 -0700

    LPS-35476 3 or less use +, more than 3, use SB

--- portal-impl/src/com/liferay/portlet/layoutsadmin/action/ImportLayoutsAction.java
+++ portal-impl/src/com/liferay/portlet/layoutsadmin/action/ImportLayoutsAction.java
@@ -448,13 +448,10 @@ public class ImportLayoutsAction extends EditFileEntryAction {
 			else if (e instanceof MissingReferenceException) {
 				MissingReferenceException mre = (MissingReferenceException)e;
 
-				StringBundler sb = new StringBundler(4);
-
-				sb.append("there-are-missing-references-that-could-not-be-");;
-				sb.append("found-in-the-current-site.-please-import-another-");
-				sb.append("lar-file-containing-the-following-elements");
-
-				errorMessage = themeDisplay.translate(sb.toString());
+				errorMessage = themeDisplay.translate(
+					"there-are-missing-references-that-could-not-be-found-" +
+						"in-the-current-site.-please-import-another-lar-file-" +
+							"containing-the-following-elements");
 
 				errorMessageJSONArray = JSONFactoryUtil.createJSONArray();
 
