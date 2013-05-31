5152168d0f271e8d5e6df4109243f807834d2adc
Zsolt Berentey <zsolt.berentey@liferay.com>
2013-05-31 15:33:17 +0200

    LPS-35667 Regenerate *ExportActionableDynamicQuery

--- portal-service/src/com/liferay/portlet/dynamicdatamapping/service/persistence/DDMTemplateExportActionableDynamicQuery.java
+++ portal-service/src/com/liferay/portlet/dynamicdatamapping/service/persistence/DDMTemplateExportActionableDynamicQuery.java
@@ -17,7 +17,10 @@ package com.liferay.portlet.dynamicdatamapping.service.persistence;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.lar.ManifestSummary;
 import com.liferay.portal.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.lar.StagedModelDataHandler;
+import com.liferay.portal.kernel.lar.StagedModelDataHandlerRegistryUtil;
 import com.liferay.portal.kernel.lar.StagedModelDataHandlerUtil;
 
 import com.liferay.portlet.dynamicdatamapping.model.DDMTemplate;
@@ -36,8 +39,25 @@ public class DDMTemplateExportActionableDynamicQuery
 	}
 
 	@Override
+	public long performCount() throws PortalException, SystemException {
+		long count = super.performCount();
+
+		ManifestSummary manifestSummary = _portletDataContext.getManifestSummary();
+
+		manifestSummary.addModelCount(getManifestSummaryKey(), count);
+
+		return count;
+	}
+
+	@Override
 	protected void addCriteria(DynamicQuery dynamicQuery) {
 		_portletDataContext.addDateRangeCriteria(dynamicQuery, "modifiedDate");
+	}
+
+	protected String getManifestSummaryKey() {
+		StagedModelDataHandler<?> stagedModelDataHandler = StagedModelDataHandlerRegistryUtil.getStagedModelDataHandler(DDMTemplate.class.getName());
+
+		return stagedModelDataHandler.getManifestSummaryKey(null);
 	}
 
 	@Override
