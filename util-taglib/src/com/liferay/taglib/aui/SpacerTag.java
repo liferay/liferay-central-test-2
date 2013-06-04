11db4a07d2afafac67fa9d0583a37fadb57f5724
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2013-06-04 12:25:21 -0300

    LPS-35934 Create spacer taglib to allow servlet 2.1 spec for trim support

---
+++ util-taglib/src/com/liferay/taglib/aui/SpacerTag.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.taglib.aui;
+
+import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.taglib.aui.base.BaseSpacerTag;
+
+import javax.servlet.jsp.JspException;
+
+/**
+ * @author Eduardo Lundgren
+ */
+public class SpacerTag extends BaseSpacerTag {
+
+	@Override
+	public int doStartTag() throws JspException {
+		try {
+			pageContext.getOut().print(CharPool.SPACE);
+		}
+		catch (Exception e) {
+			throw new JspException(e);
+		}
+
+		return super.doStartTag();
+	}
+
+}
