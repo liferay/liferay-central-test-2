bc0cbea365c1ce057750894646680853b129dff2
Brian Chan <brian.chan@liferay.com>
2013-06-05 09:03:59 -0700

    LPS-33547 SF

--- portal-impl/src/com/liferay/portal/lar/LayoutImporter.java
+++ portal-impl/src/com/liferay/portal/lar/LayoutImporter.java
@@ -367,7 +367,7 @@ public class LayoutImporter {
 			}
 
 			if (existingLayoutPrototype == null) {
-				List<Layout> layoutsWithPreviousPrototype =
+				List<Layout> layouts =
 					LayoutLocalServiceUtil.getLayoutsByLayoutPrototypeUuid(
 						layoutPrototype.getUuid());
 
@@ -376,7 +376,7 @@ public class LayoutImporter {
 				LayoutPrototypeLocalServiceUtil.updateLayoutPrototype(
 					layoutPrototype);
 
-				for (Layout layout : layoutsWithPreviousPrototype) {
+				for (Layout layout : layouts) {
 					layout.setLayoutPrototypeUuid(layoutPrototypeUuid);
 
 					LayoutLocalServiceUtil.updateLayout(layout);
