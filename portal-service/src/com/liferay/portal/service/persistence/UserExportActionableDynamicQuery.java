dc10cf3d7367927d9d706a6c9d604a4bce4c7123
Brian Chan <brian.chan@liferay.com>
2013-06-06 13:41:59 -0700

    LPS-35643 Regen

--- portal-service/src/com/liferay/portal/service/persistence/UserExportActionableDynamicQuery.java
+++ portal-service/src/com/liferay/portal/service/persistence/UserExportActionableDynamicQuery.java
@@ -17,7 +17,10 @@ package com.liferay.portal.service.persistence;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.lar.ManifestSummary;
 import com.liferay.portal.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.lar.StagedModelDataHandler;
+import com.liferay.portal.kernel.lar.StagedModelDataHandlerRegistryUtil;
 import com.liferay.portal.kernel.lar.StagedModelDataHandlerUtil;
 import com.liferay.portal.model.User;
 
@@ -32,8 +35,25 @@ public class UserExportActionableDynamicQuery extends UserActionableDynamicQuery
 	}
 
 	@Override
+	public long performCount() throws PortalException, SystemException {
+		long count = super.performCount();
+
+		ManifestSummary manifestSummary = _portletDataContext.getManifestSummary();
+
+		manifestSummary.addModelCount(getManifestSummaryKey(), count);
+
+		return count;
+	}
+
+	@Override
 	protected void addCriteria(DynamicQuery dynamicQuery) {
 		_portletDataContext.addDateRangeCriteria(dynamicQuery, "modifiedDate");
+	}
+
+	protected String getManifestSummaryKey() {
+		StagedModelDataHandler<?> stagedModelDataHandler = StagedModelDataHandlerRegistryUtil.getStagedModelDataHandler(User.class.getName());
+
+		return stagedModelDataHandler.getManifestSummaryKey(null);
 	}
 
 	@Override
