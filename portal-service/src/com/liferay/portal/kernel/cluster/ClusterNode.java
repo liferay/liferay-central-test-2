e0f905e8682a77509f770a44c124e764ca3885a7
Brian Chan <brian.chan@liferay.com>
2013-06-10 13:32:11 -0700

    SF

--- portal-service/src/com/liferay/portal/kernel/cluster/ClusterNode.java
+++ portal-service/src/com/liferay/portal/kernel/cluster/ClusterNode.java
@@ -43,19 +43,19 @@ public class ClusterNode implements Comparable<ClusterNode>, Serializable {
 	public int compareTo(ClusterNode clusterNode) {
 		InetAddress inetAddress = clusterNode._inetAddress;
 
-		String ipAddress = inetAddress.getHostAddress();
+		String hostAddress = _inetAddress.getHostAddress();
 
-		String curIpAddress = _inetAddress.getHostAddress();
+		int value = hostAddress.compareTo(inetAddress.getHostAddress());
 
-		int value = curIpAddress.compareTo(ipAddress);
+		if (value != 0) {
+			return value;
+		}
 
-		if (value == 0) {
-			if (_port > clusterNode._port) {
-				value = 1;
-			}
-			else if (_port < clusterNode._port) {
-				value = -1;
-			}
+		if (_port > clusterNode._port) {
+			value = 1;
+		}
+		else if (_port < clusterNode._port) {
+			value = -1;
 		}
 
 		return value;
