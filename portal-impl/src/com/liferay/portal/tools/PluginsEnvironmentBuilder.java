3309a99984f657a2aeb57be09aff8eb5f111f590
Ivica Cardic <ivica.cardic@liferay.com>
2013-06-10 16:50:26 +0200

    LPS-35685 When creating shared plugin portal-compat-shared.jar is not included in .gitignore file

--- portal-impl/src/com/liferay/portal/tools/PluginsEnvironmentBuilder.java
+++ portal-impl/src/com/liferay/portal/tools/PluginsEnvironmentBuilder.java
@@ -104,7 +104,8 @@ public class PluginsEnvironmentBuilder {
 			}
 
 			if (osgiProject || sharedProject) {
-				setupJarProject(dirName, fileName, dependencyJars);
+				setupJarProject(
+					dirName, fileName, dependencyJars, sharedProject);
 			}
 		}
 	}
@@ -229,7 +230,8 @@ public class PluginsEnvironmentBuilder {
 	}
 
 	protected void setupJarProject(
-			String dirName, String fileName, List<String> dependencyJars)
+			String dirName, String fileName, List<String> dependencyJars,
+			boolean sharedProject)
 		throws Exception {
 
 		File buildFile = new File(dirName + "/" + fileName);
@@ -241,6 +243,14 @@ public class PluginsEnvironmentBuilder {
 		writeEclipseFiles(libDir, projectDir, dependencyJars);
 
 		List<String> importSharedJars = getImportSharedJars(projectDir);
+
+		if (sharedProject) {
+			if (!importSharedJars.contains("portal-compat-shared.jar")) {
+				importSharedJars = new ArrayList<String>(importSharedJars);
+
+				importSharedJars.add("portal-compat-shared.jar");
+			}
+		}
 
 		File gitignoreFile = new File(
 			projectDir.getCanonicalPath() + "/.gitignore");
