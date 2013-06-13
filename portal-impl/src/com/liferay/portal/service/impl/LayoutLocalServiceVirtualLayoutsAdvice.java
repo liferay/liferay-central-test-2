db33a08d13ce14a85b305a4901e6be8197686cda
Tamas Molnar <tamas.molnar@liferay.com>
2013-06-13 16:41:56 +0200

    LPS-36192 Simplify the logic

--- portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceVirtualLayoutsAdvice.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceVirtualLayoutsAdvice.java
@@ -274,15 +274,6 @@ public class LayoutLocalServiceVirtualLayoutsAdvice
 			MergeLayoutPrototypesThreadLocal.setInProgress(true);
 			WorkflowThreadLocal.setEnabled(false);
 
-			int count = LayoutLocalServiceUtil.getLayoutsCount(
-				group, privateLayout);
-
-			if (count == 0) {
-				SitesUtil.mergeLayoutSetPrototypeLayouts(group, layoutSet);
-
-				return;
-			}
-
 			SitesUtil.mergeLayoutSetPrototypeLayouts(group, layoutSet);
 		}
 		catch (Exception e) {
