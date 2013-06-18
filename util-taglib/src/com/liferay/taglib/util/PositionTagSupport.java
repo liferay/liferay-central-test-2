a4941b3e5d178ad6af445e2260bbe03ea553df98
Brian Chan <brian.chan@liferay.com>
2013-06-17 17:56:39 -0700

    LPS-35322 SF

--- util-taglib/src/com/liferay/taglib/util/PositionTagSupport.java
+++ util-taglib/src/com/liferay/taglib/util/PositionTagSupport.java
@@ -78,7 +78,7 @@ public class PositionTagSupport extends BaseBodyTagSupport implements BodyTag {
 			ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
 				WebKeys.THEME_DISPLAY);
 
-			if (themeDisplay.isIsolated() || themeDisplay.isStateExclusive()) {
+			if (themeDisplay.isStateExclusive() || themeDisplay.isIsolated()) {
 				position = _POSITION_INLINE;
 			}
 			else {
