8b19a7336b402908b04fb5bba226f099346c2898
Hugo Huijser <hugo.huijser@liferay.com>
2013-06-18 10:34:19 -0700

    LPS-36303 Add check to SourceFormatter

--- portal-impl/src/com/liferay/portal/tools/sourceformatter/JavaSourceProcessor.java
+++ portal-impl/src/com/liferay/portal/tools/sourceformatter/JavaSourceProcessor.java
@@ -243,6 +243,46 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 		}
 	}
 
+	protected void checkAnnotationForMethod(
+		JavaTerm javaTerm, String annotation, String requiredMethodNameRegex,
+		int requiredMethodType, String fileName) {
+
+		String methodContent = javaTerm.getContent();
+		String methodName = javaTerm.getName();
+
+		Pattern pattern = Pattern.compile(requiredMethodNameRegex);
+
+		Matcher matcher = pattern.matcher(methodName);
+
+		if (methodContent.contains(
+				StringPool.TAB + StringPool.AT + annotation + "\n") ||
+			methodContent.contains(
+				StringPool.TAB + StringPool.AT + annotation +
+					StringPool.OPEN_PARENTHESIS)) {
+
+			if (!matcher.find()) {
+				processErrorMessage(
+					fileName,
+					"LPS-36303: Incorrect method name: " + methodName + " " +
+						fileName);
+			}
+			else if (javaTerm.getType() != requiredMethodType) {
+				processErrorMessage(
+					fileName,
+					"LPS-36303: Incorrect method type for " + methodName + " " +
+						fileName);
+			}
+		}
+		else if (matcher.find() &&
+				 !methodContent.contains(StringPool.TAB + "@Override")) {
+
+			processErrorMessage(
+				fileName,
+				"Annotation @" + annotation + " required for " + methodName +
+					" " + fileName);
+		}
+	}
+
 	protected String checkIfClause(
 			String ifClause, String fileName, int lineCount)
 		throws IOException {
@@ -357,6 +397,29 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 		}
 
 		return ifClause;
+	}
+
+	protected void checkTestAnnotations(JavaTerm javaTerm, String fileName) {
+		int methodType = javaTerm.getType();
+
+		if ((methodType != TYPE_METHOD_PUBLIC) &&
+			(methodType != TYPE_METHOD_PUBLIC_STATIC)) {
+
+			return;
+		}
+
+		checkAnnotationForMethod(
+			javaTerm, "After", "^.*tearDown\\z", TYPE_METHOD_PUBLIC, fileName);
+		checkAnnotationForMethod(
+			javaTerm, "AfterClass", "^.*tearDownClass\\z",
+			TYPE_METHOD_PUBLIC_STATIC, fileName);
+		checkAnnotationForMethod(
+			javaTerm, "Before", "^.*setUp\\z", TYPE_METHOD_PUBLIC, fileName);
+		checkAnnotationForMethod(
+			javaTerm, "BeforeClass", "^.*setUpClass\\z",
+			TYPE_METHOD_PUBLIC_STATIC, fileName);
+		checkAnnotationForMethod(
+			javaTerm, "Test", "^.*test", TYPE_METHOD_PUBLIC, fileName);
 	}
 
 	protected void checkUnprocessedExceptions(
@@ -1546,7 +1609,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 
 			newContent = sortJavaTerms(fileName, content, javaTerms);
 
-			newContent = sortAnnotations(newContent, javaTerms);
+			newContent = formatAnnotations(fileName, newContent, javaTerms);
 		}
 
 		return newContent;
@@ -2371,13 +2434,20 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 		return false;
 	}
 
-	protected String sortAnnotations(
-		String content, Set<JavaTerm> javaTerms) throws IOException {
+	protected String formatAnnotations(
+			String fileName, String content, Set<JavaTerm> javaTerms)
+		throws IOException {
 
 		Iterator<JavaTerm> itr = javaTerms.iterator();
 
 		while (itr.hasNext()) {
 			JavaTerm javaTerm = itr.next();
+
+			if (fileName.contains("/test/") &&
+				!fileName.endsWith("TestBean.java")) {
+
+				checkTestAnnotations(javaTerm, fileName);
+			}
 
 			for (;;) {
 				String javaTermContent = javaTerm.getContent();
