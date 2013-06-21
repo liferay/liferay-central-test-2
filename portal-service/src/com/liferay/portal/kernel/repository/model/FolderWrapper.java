fb83c9ca3224cb124d6a878d4abd93b99610e075
kylestiemann <kyle.stiemann@liferay.com>
2013-06-21 15:58:39 -0400

    LPS-34351 As a Liferay plugin developer, I would like to have the ability to wrap Folder and FileEntry objects. Removed @Override annotation for compliance with Java 1.5

--- portal-service/src/com/liferay/portal/kernel/repository/model/FolderWrapper.java
+++ portal-service/src/com/liferay/portal/kernel/repository/model/FolderWrapper.java
@@ -33,238 +33,191 @@ public class FolderWrapper implements Folder, ModelWrapper<Folder> {
 		this._folder = folder;
 	}
 
-	@Override
 	public List<Long> getAncestorFolderIds() throws PortalException,
 			SystemException {
 		return _folder.getAncestorFolderIds();
 	}
 
-	@Override
 	public List<Folder> getAncestors() throws PortalException, SystemException {
 		return _folder.getAncestors();
 	}
 
-	@Override
 	public Map<String, Serializable> getAttributes() {
 		return _folder.getAttributes();
 	}
 
-	@Override
 	public long getCompanyId() {
 		return _folder.getCompanyId();
 	}
 
-	@Override
 	public void setCompanyId(long companyId) {
 		_folder.setCompanyId(companyId);
 	}
 
-	@Override
 	public Date getCreateDate() {
 		return _folder.getCreateDate();
 	}
 
-	@Override
 	public void setCreateDate(Date date) {
 		_folder.setCreateDate(date);
 	}
 
-	@Override
 	public String getDescription() {
 		return _folder.getDescription();
 	}
 
-	@Override
 	public ExpandoBridge getExpandoBridge() {
 		return _folder.getExpandoBridge();
 	}
 
-	@Override
 	public long getFolderId() {
 		return _folder.getFolderId();
 	}
 
-	@Override
 	public long getGroupId() {
 		return _folder.getGroupId();
 	}
 
-	@Override
 	public void setGroupId(long groupId) {
 		_folder.setGroupId(groupId);
 	}
 
-	@Override
 	public Date getLastPostDate() {
 		return _folder.getLastPostDate();
 	}
 
-	@Override
 	public Object getModel() {
 		return _folder.getModel();
 	}
 
-	@Override
 	public Class<?> getModelClass() {
 		return Folder.class;
 	}
 
-	@Override
 	public String getModelClassName() {
 		return Folder.class.getName();
 	}
 
-	@Override
 	public Date getModifiedDate() {
 		return _folder.getModifiedDate();
 	}
 
-	@Override
 	public void setModifiedDate(Date date) {
 		_folder.setModifiedDate(date);
 	}
 
-	@Override
 	public String getName() {
 		return _folder.getName();
 	}
 
-	@Override
 	public Folder getParentFolder() throws PortalException, SystemException {
 		return _folder.getParentFolder();
 	}
 
-	@Override
 	public long getParentFolderId() {
 		return _folder.getParentFolderId();
 	}
 
-	@Override
 	public long getPrimaryKey() {
 		return _folder.getPrimaryKey();
 	}
 
-	@Override
 	public Serializable getPrimaryKeyObj() {
 		return _folder.getPrimaryKeyObj();
 	}
 
-	@Override
 	public void setPrimaryKeyObj(Serializable primaryKeyObj) {
 		_folder.setPrimaryKeyObj(primaryKeyObj);
 	}
 
-	@Override
 	public long getRepositoryId() {
 		return _folder.getRepositoryId();
 	}
 
-	@Override
 	public long getUserId() {
 		return _folder.getUserId();
 	}
 
-	@Override
 	public void setUserId(long userId) {
 		_folder.setUserId(userId);
 	}
 
-	@Override
 	public String getUserName() {
 		return _folder.getUserName();
 	}
 
-	@Override
 	public void setUserName(String userName) {
 		_folder.setUserName(userName);
 	}
 
-	@Override
 	public String getUserUuid() throws SystemException {
 		return _folder.getUserUuid();
 	}
 
-	@Override
 	public void setUserUuid(String userUuid) {
 		_folder.setUserUuid(userUuid);
 	}
 
-	@Override
 	public String getUuid() {
 		return _folder.getUuid();
 	}
 
-	@Override
 	public void setUuid(String uuid) {
 		_folder.setUuid(uuid);
 	}
 
-	@Override
 	public Folder getWrappedModel() {
 		return _folder;
 	}
 
-	@Override
 	public boolean hasInheritableLock() {
 		return _folder.hasInheritableLock();
 	}
 
-	@Override
 	public boolean hasLock() {
 		return _folder.hasLock();
 	}
 
-	@Override
 	public boolean isDefaultRepository() {
 		return _folder.isDefaultRepository();
 	}
 
-	@Override
 	public boolean isEscapedModel() {
 		return _folder.isEscapedModel();
 	}
 
-	@Override
 	public boolean isLocked() {
 		return _folder.isLocked();
 	}
 
-	@Override
 	public boolean isMountPoint() {
 		return _folder.isMountPoint();
 	}
 
-	@Override
 	public boolean isRoot() {
 		return _folder.isRoot();
 	}
 
-	@Override
 	public boolean isSupportsLocking() {
 		return _folder.isSupportsLocking();
 	}
 
-	@Override
 	public boolean isSupportsMetadata() {
 		return _folder.isSupportsMetadata();
 	}
 
-	@Override
 	public boolean isSupportsMultipleUpload() {
 		return _folder.isSupportsMultipleUpload();
 	}
 
-	@Override
 	public boolean isSupportsShortcuts() {
 		return _folder.isSupportsShortcuts();
 	}
 
-	@Override
 	public boolean isSupportsSocial() {
 		return _folder.isSupportsSocial();
 	}
 
-	@Override
 	public boolean isSupportsSubscribing() {
 		return _folder.isSupportsSubscribing();
 	}
@@ -274,7 +227,6 @@ public class FolderWrapper implements Folder, ModelWrapper<Folder> {
 		return new FolderWrapper((Folder) _folder.clone());
 	}
 
-	@Override
 	public boolean containsPermission(PermissionChecker permissionChecker,
 			String actionId) throws PortalException, SystemException {
 		return _folder.containsPermission(permissionChecker, actionId);
@@ -291,14 +243,17 @@ public class FolderWrapper implements Folder, ModelWrapper<Folder> {
 		return _folder.hashCode();
 	}
 
-	@Override
 	public Folder toEscapedModel() {
 		return new FolderWrapper(_folder.toEscapedModel());
 	}
 
-	@Override
 	public Folder toUnescapedModel() {
 		return new FolderWrapper(_folder.toUnescapedModel());
+	}
+	
+	@Override
+	public String toString() {
+		return _folder.toString();
 	}
 
 	private Folder _folder;
