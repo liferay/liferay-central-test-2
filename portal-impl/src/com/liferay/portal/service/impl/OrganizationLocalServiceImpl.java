e04ad498e66fd68fd87113c8d493595ace163e7e
KocsisDaniel <daniel.kocsis@liferay.com>
2013-06-21 09:56:37 +0200

    LPS-36706 Save the organization uuid into the asset

--- portal-impl/src/com/liferay/portal/service/impl/OrganizationLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/OrganizationLocalServiceImpl.java
@@ -1629,9 +1629,9 @@ public class OrganizationLocalServiceImpl
 		assetEntryLocalService.updateEntry(
 			userId, companyGroup.getGroupId(), null, null,
 			Organization.class.getName(), organization.getOrganizationId(),
-			null, 0, assetCategoryIds, assetTagNames, false, null, null, null,
-			null, organization.getName(), StringPool.BLANK, null, null, null, 0,
-			0, null, false);
+			organization.getUuid(), 0, assetCategoryIds, assetTagNames, false,
+			null, null, null, null, organization.getName(), StringPool.BLANK,
+			null, null, null, 0, 0, null, false);
 	}
 
 	/**
