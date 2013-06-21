923ec204c509d61a87fd2407536ff36e44116c96
Igor Spasic <igor.spasic@liferay.com>
2013-06-21 09:46:05 -0400

    LPS-36459 configuration constructor for wrapping actions on regular objects

--- portal-impl/src/com/liferay/portal/jsonwebservice/JSONWebServiceActionConfig.java
+++ portal-impl/src/com/liferay/portal/jsonwebservice/JSONWebServiceActionConfig.java
@@ -31,6 +31,24 @@ public class JSONWebServiceActionConfig
 	JSONWebServiceActionMapping {
 
 	public JSONWebServiceActionConfig(
+		String contextPath, Object actionObject, Class<?> actionClass,
+		Method actionMethod, String path, String method) {
+
+		this(contextPath, actionClass, actionMethod, path, method);
+
+		_actionObject = actionObject;
+
+		try {
+			Method realActionMethod = actionObject.getClass().getMethod(actionMethod.getName(), actionMethod.getParameterTypes());
+
+			_actionMethod = realActionMethod;
+		}
+		catch (NoSuchMethodException e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+
+	public JSONWebServiceActionConfig(
 		String contextPath, Class<?> actionClass, Method actionMethod,
 		String path, String method) {
 
@@ -159,6 +177,7 @@ public class JSONWebServiceActionConfig
 
 	private Class<?> _actionClass;
 	private Method _actionMethod;
+	private Object _actionObject;
 	private String _contextPath;
 	private String _fullPath;
 	private String _method;
