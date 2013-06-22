c9c6d46674c2da00ad9fcca7317603a19942c51f
Brian Chan <brian.chan@liferay.com>
2013-06-21 21:01:48 -0700

    LPS-29232 Remove unused var

--- portal-service/src/com/liferay/portal/kernel/util/Validator.java
+++ portal-service/src/com/liferay/portal/kernel/util/Validator.java
@@ -1340,8 +1340,6 @@ public class Validator {
 		'_', '`', '{', '|', '}', '~'
 	};
 
-	private static final String _LOCALHOST = "localhost";
-
 	private static final String _VARIABLE_TERM_BEGIN = "[$";
 
 	private static final String _VARIABLE_TERM_END = "$]";
