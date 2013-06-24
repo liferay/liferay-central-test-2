edd354efe681ed75740226674567d48fceaff16b
Nate Cavanaugh <nate@shift22.com>
2013-06-24 13:51:26 -0700

    LPS-35900 - Source formatting

--- portal-web/docroot/html/js/liferay/language.js
+++ portal-web/docroot/html/js/liferay/language.js
@@ -1,48 +1,54 @@
 ;(function(A, Liferay) {
+	var Language = {};
+
+	Language.get = function(key){
+		return key;
+	};
+
 	A.use(
 		'io-base',
 		function(A) {
-			Liferay.Language = {
-				get: A.cached(
-					function(key, extraParams) {
-						var instance = this;
+			Language.get = A.cached(
+				function(key, extraParams) {
+					var instance = this;
 
-						var url = themeDisplay.getPathContext() + '/language/' + themeDisplay.getLanguageId() + '/' + key + '/';
+					var url = themeDisplay.getPathContext() + '/language/' + themeDisplay.getLanguageId() + '/' + key + '/';
 
-						if (extraParams) {
-							if (typeof extraParams == 'string') {
-								url += extraParams;
-							}
-							else if (Liferay.Util.isArray(extraParams)) {
-								url += extraParams.join('/');
-							}
+					if (extraParams) {
+						if (typeof extraParams == 'string') {
+							url += extraParams;
 						}
-
-						var authUrl = url;
-
-						var authToken = Liferay.authToken;
-
-						if (authToken) {
-							authUrl = Liferay.Util.addParams('p_auth=' + authToken, url);
+						else if (Liferay.Util.isArray(extraParams)) {
+							url += extraParams.join('/');
 						}
-
-						A.io(
-							authUrl,
-							{
-								on: {
-									complete: function(i, o) {
-										value = o.responseText;
-									}
-								},
-								sync: true,
-								type: 'GET'
-							}
-						);
-
-						return value;
 					}
-				)
-			};
+
+					var authUrl = url;
+
+					var authToken = Liferay.authToken;
+
+					if (authToken) {
+						authUrl = Liferay.Util.addParams('p_auth=' + authToken, url);
+					}
+
+					A.io(
+						authUrl,
+						{
+							on: {
+								complete: function(i, o) {
+									value = o.responseText;
+								}
+							},
+							sync: true,
+							type: 'GET'
+						}
+					);
+
+					return value;
+				}
+			);
 		}
 	);
+
+	Liferay.Language = Language;
 })(AUI(), Liferay);
