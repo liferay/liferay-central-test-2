fc4b421a7a617e87e87ed017c01efc3fae987f8a
Jonathan Lee <Jonathan.Lee@liferay.com>
2013-06-26 14:28:42 -0700

    LPS-36673 Create UserNotificationManger to manage UserNotificationHandler

---
+++ portal-service/src/com/liferay/portal/kernel/notifications/UserNotificationManager.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+
+package com.liferay.portal.kernel.notifications;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.model.UserNotificationEvent;
+import com.liferay.portal.service.ServiceContext;
+
+import java.util.Map;
+
+/**
+ * @author Jonathan Lee
+ */
+public interface UserNotificationManager {
+
+	public void addUserNotificationHandler(
+		UserNotificationHandler userNotificationHandler);
+
+	public void deleteUserNotificationHandler(
+		UserNotificationHandler userNotificationHandler);
+
+	public Map<String, Map<String, UserNotificationHandler>>
+		getUserNotificationHandlers();
+
+	public UserNotificationFeedEntry interpret(
+			String selector, UserNotificationEvent userNotificationEvent,
+			ServiceContext serviceContext)
+		throws PortalException;
+
+}
---
+++ portal-service/src/com/liferay/portal/kernel/notifications/UserNotificationManagerUtil.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.notifications;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.pacl.permission.PortalRuntimePermission;
+import com.liferay.portal.model.UserNotificationEvent;
+import com.liferay.portal.service.ServiceContext;
+
+import java.util.Map;
+
+/**
+ * @author Jonathan Lee
+ */
+public class UserNotificationManagerUtil {
+
+	public static void addUserNotificationHandler(
+		UserNotificationHandler userNotificationHandler) {
+
+		getUserNotificationManager().addUserNotificationHandler(
+			userNotificationHandler);
+	}
+
+	public static void deleteUserNotificationHandler(
+		UserNotificationHandler userNotificationHandler) {
+
+		getUserNotificationManager().deleteUserNotificationHandler(
+			userNotificationHandler);
+	}
+
+	public static Map<String, Map<String, UserNotificationHandler>>
+		getUserNotificationHandlers() {
+
+		return getUserNotificationManager().getUserNotificationHandlers();
+	}
+
+	public static UserNotificationFeedEntry interpret(
+			String selector, UserNotificationEvent userNotificationEvent,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		return getUserNotificationManager().interpret(
+			selector, userNotificationEvent, serviceContext);
+	}
+
+	public static UserNotificationManager getUserNotificationManager() {
+		PortalRuntimePermission.checkGetBeanProperty(
+			UserNotificationManagerUtil.class);
+
+		return _userNotificationManager;
+	}
+
+	public void setUserNotificationManager (
+		UserNotificationManager userNotificationManager) {
+
+		PortalRuntimePermission.checkSetBeanProperty(getClass());
+
+		_userNotificationManager = userNotificationManager;
+	}
+
+	private static UserNotificationManager _userNotificationManager;
+
+}
