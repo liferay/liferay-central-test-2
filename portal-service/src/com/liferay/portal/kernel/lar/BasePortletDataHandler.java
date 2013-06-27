45e7127359f4d7a8d6539f55bab9de4af41cb0c5
Brian Chan <brian.chan@liferay.com>
2013-06-27 06:48:50 -0700

    LPS-36527 Remove unthrown exception

--- portal-service/src/com/liferay/portal/kernel/lar/BasePortletDataHandler.java
+++ portal-service/src/com/liferay/portal/kernel/lar/BasePortletDataHandler.java
@@ -228,8 +228,7 @@ public abstract class BasePortletDataHandler implements PortletDataHandler {
 
 	@Override
 	public PortletDataHandlerControl[] getImportConfigurationControls(
-			Portlet portlet, ManifestSummary manifestSummary)
-		throws PortletDataException {
+		Portlet portlet, ManifestSummary manifestSummary) {
 
 		if (Validator.isNull(portlet.getConfigurationActionClass())) {
 			return null;
