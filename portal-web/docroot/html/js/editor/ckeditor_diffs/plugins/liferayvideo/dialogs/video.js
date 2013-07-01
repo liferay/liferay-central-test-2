3046ddf34cfcd6efa26a2013975d897d013fe50a
Iliyan Peychev <iliyan.peychev@liferay.com>
2013-07-01 13:50:45 +0200

    LPS-37304 - Source formatting

--- portal-web/docroot/html/js/editor/ckeditor_diffs/plugins/liferayvideo/dialogs/video.js
+++ portal-web/docroot/html/js/editor/ckeditor_diffs/plugins/liferayvideo/dialogs/video.js
@@ -1,282 +1,284 @@
-CKEDITOR.dialog.add( 'liferayvideo',
-		function (editor) {
-			var lang = editor.lang.liferayvideo;
+CKEDITOR.dialog.add(
+	'liferayvideo',
+	function(editor) {
+		var TPL_SCRIPT_PREFIX = 'AUI().use(' +
+								'	\'aui-base\',\'aui-video\',' +
+								'	function(A) {' +
+								'		new A.Video(' +
+								'			{';
 
-			function commitValue(videoNode, extraStyles) {
-				var instance = this;
+		var TPL_SCRIPT = 		'				ogvUrl: \'{ogvUrl}\',' +
+								'				url: \'{url}\',' +
+								'				poster: \'{poster}\',' +
+								'				boundingBox: \'#{videoBoxId}\',' +
+								'				height: {height},' +
+								'				width: {width}';
 
-				var value=instance.getValue();
-				var id = instance.id;
+		var TPL_SCRIPT_SUFFIX = '			}' +
+								'		).render();' +
+								'	}' +
+								');';
 
-				var videoDivNode =  videoNode.getChild(0);
-				var scriptNode = videoNode.getChild(1);
+		var lang = editor.lang.liferayvideo;
 
-				var scripttpl = null;
-				var textScript = null;
+		function commitValue(videoNode, extraStyles) {
+			var instance = this;
 
-				var videoUrl = videoNode.getAttribute('data-video-url');
-				var videoOgvUrl = videoNode.getAttribute('data-video-ogv-url');
-				var videoPoster = videoNode.getAttribute('data-poster');
-				var videoId = videoDivNode.getAttribute('id');
-				var videoHeight = videoNode.getAttribute('data-height');
-				var videoWidth = videoNode.getAttribute('data-width');
+			var value = instance.getValue();
+			var id = instance.id;
 
-				var TPL_SCRIPT_PREFIX = 'AUI().use(' +
-										'	\'aui-base\',\'aui-video\',' +
-										'  	function(A) {' +
-										'	   	 new A.Video(' +
-										'		    {'; 
-				var TPL_SCRIPT = 		'        		ogvUrl: \'{ogvUrl}\',' +
-										'        		url: \'{url}\',' +
-										'				poster: \'{poster}\',' +
-										'	       		boundingBox: \'#{videoBoxId}\',' +
-										'				height: {height},' +
-										'				width: {width}';
+			var videoDivNode = videoNode.getChild(0);
+			var scriptNode = videoNode.getChild(1);
 
-				var TPL_SCRIPT_SUFFIX = '	     	}' +
-										'		).render();' +
-										'	}' +
-										');'; 
+			var scriptTPL = null;
+			var textScript = null;
 
-				if (!value && id ==='id') {
-					value = generateId();
+			var videoHeight = videoNode.getAttribute('data-height');
+			var videoId = videoDivNode.getAttribute('id');
+			var videoOgvUrl = videoNode.getAttribute('data-video-ogv-url');
+			var videoPoster = videoNode.getAttribute('data-poster');
+			var videoUrl = videoNode.getAttribute('data-video-url');
+			var videoWidth = videoNode.getAttribute('data-width');
+
+			if (!value && id === 'id') {
+				value = generateId();
+			}
+
+			if (id === 'poster') {
+				videoNode.setAttribute('data-document-url', value);
+
+				var urlChar = '?';
+
+				if (value.indexOf(urlChar) >= 0) {
+					urlChar = '&';
 				}
 
-				if (id ==='poster') {
-				    	videoNode.setAttribute('data-document-url', value);
+				videoUrl = value + urlChar + 'videoPreview=1&type=mp4';
+				videoNode.setAttribute('data-video-url', videoUrl);
 
-				        var urlChar = '?';				        
+				videoOgvUrl = value + urlChar + 'videoPreview=1&type=ogv';
+				videoNode.setAttribute('data-video-ogv-url', videoOgvUrl);
 
-				        if (value.indexOf('?') >=0) {
-				          urlChar = '&';
-				        }
+				value = value + urlChar + 'videoThumbnail=1';
 
-				        videoUrl = value + urlChar + 'videoPreview=1&type=mp4';
-				        videoNode.setAttribute('data-video-url', videoUrl);
+				videoNode.setAttribute('data-poster', value);
 
-				        videoOgvUrl = value + urlChar + 'videoPreview=1&type=ogv';
-				        videoNode.setAttribute('data-video-ogv-url', videoOgvUrl);
+				scriptTPL = new CKEDITOR.template(TPL_SCRIPT);
 
-				        value = value + urlChar + 'videoThumbnail=1';
+				textScript = scriptTPL.output(
+					{
+						height: videoHeight,
+						ogvUrl: videoOgvUrl,
+						poster: value,
+						url: videoUrl,
+						videoBoxId: videoId,
+						width: videoWidth
+					}
+				);
 
-						videoNode.setAttribute('data-poster', value);
+				scriptNode.setText(TPL_SCRIPT_PREFIX + textScript + TPL_SCRIPT_SUFFIX);
+			}
 
-						scripttpl = new CKEDITOR.template(TPL_SCRIPT);
-						textScript = scripttpl.output(
-															{
-																ogvUrl: videoOgvUrl,
-																url: videoUrl,
-																poster: value,
-																videoBoxId: videoId,
-																height: videoHeight,
-																width: videoWidth
-															}
-														);
+			if (value) {
+				if (id === 'poster') {
+					extraStyles.backgroundImage = 'url(' + value + ')';
+				}
+				else if (id === 'height') {
+					extraStyles.width = value + 'px';
+
+					videoNode.setAttribute('data-height', value);
+
+					if (scriptNode && scriptNode.getText()) {
+						scriptTPL = new CKEDITOR.template(TPL_SCRIPT);
+
+						textScript = scriptTPL.output(
+							{
+								height: value,
+								ogvUrl: videoOgvUrl,
+								poster: videoPoster,
+								url: videoUrl,
+								videoBoxId: videoId,
+								width: videoWidth
+							}
+						);
+
 						scriptNode.setText(TPL_SCRIPT_PREFIX + textScript + TPL_SCRIPT_SUFFIX);
+					}
 				}
+				else if (id === 'width') {
+					extraStyles.height = value + 'px';
 
-				if (value) {
-					if (id === 'poster') {
-						extraStyles.backgroundImage = 'url(' + value + ')';
-					}
-					else if (id === 'height') {
+					videoNode.setAttribute('data-width', value);
 
-						extraStyles.width = value + 'px';
-						videoNode.setAttribute('data-height', value);
+					if (scriptNode && scriptNode.getText()) {
+						scriptTPL = new CKEDITOR.template(TPL_SCRIPT);
 
-						if (scriptNode && scriptNode.getText()) {
-							scripttpl = new CKEDITOR.template(TPL_SCRIPT);
+						textScript = scriptTPL.output(
+							{
+								height: videoHeight,
+								ogvUrl: videoOgvUrl,
+								poster: videoPoster,
+								url: videoUrl,
+								videoBoxId: videoId,
+								width: value
+							}
+						);
 
-							textScript = scripttpl.output(
-																{
-																	ogvUrl: videoOgvUrl,
-																	url: videoUrl,
-																	poster: videoPoster,
-																	videoBoxId: videoId,
-																	height: value,
-																	width: videoWidth
-																}
-															);
-							scriptNode.setText(TPL_SCRIPT_PREFIX + textScript + TPL_SCRIPT_SUFFIX);
-						}
-					}
-					else if (id === 'width') {
-
-						extraStyles.height = value + 'px';
-						videoNode.setAttribute('data-width', value);
-
-						if (scriptNode && scriptNode.getText()) {
-							scripttpl = new CKEDITOR.template(TPL_SCRIPT);
-
-							textScript = scripttpl.output(
-																{ 
-																	ogvUrl: videoOgvUrl,
-																	url: videoUrl,
-																	poster: videoPoster,
-																	videoBoxId: videoId,
-																	height: videoHeight,
-																	width: value 
-																}
-															);
-							scriptNode.setText(TPL_SCRIPT_PREFIX + textScript + TPL_SCRIPT_SUFFIX);
-						}
+						scriptNode.setText(TPL_SCRIPT_PREFIX + textScript + TPL_SCRIPT_SUFFIX);
 					}
 				}
 			}
+		}
 
-			function loadValue(videoNode) {
-				var instance = this;
-				var id = instance.id;
+		function generateId() {
+			return 'video' + new Date().getTime();
+		}
 
-				if (videoNode) {
-					if (id === 'id') {
-							instance.setValue(videoNode.getChild(0).getAttribute('id'));
-					}
-					else if (id === 'poster') {
-							instance.setValue(videoNode.getAttribute('data-document-url'));
-					}
-					else if (id === 'height') {
-							instance.setValue(videoNode.getAttribute('data-height'));
-					}
-					else if (id === 'width') {
-							instance.setValue(videoNode.getAttribute('data-width'));
-					}
+		function loadValue(videoNode) {
+			var instance = this;
+			var id = instance.id;
+
+			if (videoNode) {
+				if (id === 'id') {
+					instance.setValue(videoNode.getChild(0).getAttribute('id'));
 				}
-				else {
-					if (id === 'id') {
-						instance.setValue(generateId());
-					}
+				else if (id === 'poster') {
+					instance.setValue(videoNode.getAttribute('data-document-url'));
+				}
+				else if (id === 'height') {
+					instance.setValue(videoNode.getAttribute('data-height'));
+				}
+				else if (id === 'width') {
+					instance.setValue(videoNode.getAttribute('data-width'));
 				}
 			}
-
-			function generateId() {
-				return 'video' + new Date().getTime();
+			else {
+				if (id === 'id') {
+					instance.setValue(generateId());
+				}
 			}
+		}
 
-			return {
-					contents :
-							[{
-								elements :
-								[
+		return {
+				contents: [
+					{
+						elements:
+						[
+							{
+								children: [
 									{
-										children: [
-													{
-														commit: commitValue,
-														id: 'poster',
-														label: lang.poster,
-														setup: loadValue,
-														type: 'text'
-													},
-													{
-														filebrowser:
-														{
-															action: 'Browse',
-															target: 'info:poster',
-															url: editor.config.filebrowserBrowseUrl + '&Type=Video'
-														},
-														hidden: 'true',
-														id: 'browse',
-														label: editor.lang.common.browseServer,
-														style: 'display:inline-block;margin-top:10px;',
-														type: 'button'
-													}],
-										type: 'hbox',
-										widths: [ '', '100px']
+										commit: commitValue,
+										id: 'poster',
+										label: lang.poster,
+										setup: loadValue,
+										type: 'text'
 									},
 									{
-										children: [
-													{
-														commit: commitValue,
-														'default': 400,
-														id: 'width',
-														label: editor.lang.common.width,
-														setup: loadValue,
-														type: 'text',
-														validate: CKEDITOR.dialog.validate.notEmpty(lang.widthRequired)
-													},
-													{
-														commit: commitValue,
-														'default': 300,
-														id: 'height',
-														label: editor.lang.common.height,
-														setup: loadValue,
-														type: 'text',
-														validate: CKEDITOR.dialog.validate.notEmpty(lang.heightRequired)
-													},
-													{
-														commit: commitValue,
-														id: 'id',
-														label: 'Id',
-														setup: loadValue,
-														type: 'text'
-													}],
-										type : 'hbox',
-										widths: [ '33%', '33%', '33%']
+										filebrowser:
+										{
+											action: 'Browse',
+											target: 'info:poster',
+											url: editor.config.filebrowserBrowseUrl + '&Type=Video'
+										},
+										hidden: 'true',
+										id: 'browse',
+										label: editor.lang.common.browseServer,
+										style: 'display:inline-block;margin-top:10px;',
+										type: 'button'
 									}
 								],
-								id : 'info'
-							}]
-					,
+								type: 'hbox',
+								widths: [ '', '100px']
+							},
+							{
+								children: [
+									{
+										commit: commitValue,
+										'default': 400,
+										id: 'width',
+										label: editor.lang.common.width,
+										setup: loadValue,
+										type: 'text',
+										validate: CKEDITOR.dialog.validate.notEmpty(lang.widthRequired)
+									},
+									{
+										commit: commitValue,
+										'default': 300,
+										id: 'height',
+										label: editor.lang.common.height,
+										setup: loadValue,
+										type: 'text',
+										validate: CKEDITOR.dialog.validate.notEmpty(lang.heightRequired)
+									},
+									{
+										commit: commitValue,
+										id: 'id',
+										label: 'Id',
+										setup: loadValue,
+										type: 'text'
+									}
+								],
+								type: 'hbox',
+								widths: [ '33%', '33%', '33%']
+							}
+						],
+						id: 'info'
+					}
+				],
+				onShow: function() {
+					var instance = this;
 
-					minWidth : 400,
+					instance.fakeImage = null;
+					instance.videoNode = null;
 
-					minHeight : 200,
+					var fakeImage = instance.getSelectedElement();
 
-					onShow : function() {
-						var instance = this;
+					if (fakeImage && fakeImage.data('cke-real-element-type') && fakeImage.data('cke-real-element-type') === 'liferayvideo') {
+							instance.fakeImage = fakeImage;
+							var videoNode = editor.restoreRealElement(fakeImage);
+							instance.videoNode = videoNode;
+							instance.setupContent( videoNode);
+					}
+					else {
+						instance.setupContent(null);
+					}
+				},
+				onOk: function() {
+					var instance = this;
 
-						instance.fakeImage = null;
-						instance.videoNode = null;
+					var STR_DIV = 'div';
 
-						var fakeImage = instance.getSelectedElement();
+					var tmpid = generateId();
+					var extraStyles = {};
 
-						if (fakeImage && fakeImage.data('cke-real-element-type') && 
-							fakeImage.data('cke-real-element-type') === 'liferayvideo') {
+					var divNode = editor.document.createElement(STR_DIV);
+					divNode.setAttribute('class', 'liferayckevideo video-container');
 
-								instance.fakeImage = fakeImage;
-								var videoNode = editor.restoreRealElement(fakeImage);
-								instance.videoNode = videoNode;
-								instance.setupContent( videoNode);
-						}
-						else {
-							instance.setupContent( null);
-						}
-					},
+					var boundingBoxTmp = editor.document.createElement(STR_DIV);
+					boundingBoxTmp.setAttribute('id', tmpid);
 
-					onOk : function() {
-						var instance = this;
+					var scriptTmp = editor.document.createElement('script');
+					scriptTmp.setAttribute('type', 'text/javascript');
 
-						var STR_DIV = 'div';
+					divNode.append(boundingBoxTmp);
+					divNode.append(scriptTmp);
 
-						var tmpid = generateId();
-						var extraStyles = {};
+					instance.commitContent(divNode, extraStyles);
 
-						var divNode = editor.document.createElement(STR_DIV);
-						divNode.setAttribute('class', 'liferayckevideo video-container');
+					var newFakeImage = editor.createFakeElement(divNode, 'liferay_cke_video', 'liferayvideo', false);
+					newFakeImage.setStyles(extraStyles);
 
-						var boundingBoxTmp = editor.document.createElement(STR_DIV);
-						boundingBoxTmp.setAttribute('id', tmpid);
-
-						var scriptTmp = editor.document.createElement('script');
-						scriptTmp.setAttribute('type', 'text/javascript');
-
-						divNode.append(boundingBoxTmp);
-						divNode.append(scriptTmp);			
-
-						instance.commitContent(divNode, extraStyles);	
-
-						var newFakeImage = editor.createFakeElement(divNode, 'liferay_cke_video', 'liferayvideo', false);
-						newFakeImage.setStyles(extraStyles);
-
-						if (instance.fakeImage) {
-							newFakeImage.replace(instance.fakeImage);
-							editor.getSelection().selectElement(newFakeImage);
-						}
-						else {
-							editor.insertElement(newFakeImage);
-						}
-					},
-					title : lang.dialogTitle				
-			};
-		}
+					if (instance.fakeImage) {
+						newFakeImage.replace(instance.fakeImage);
+						editor.getSelection().selectElement(newFakeImage);
+					}
+					else {
+						editor.insertElement(newFakeImage);
+					}
+				},
+				minHeight: 200,
+				minWidth: 400,
+				title: lang.dialogTitle
+		};
+	}
 );
