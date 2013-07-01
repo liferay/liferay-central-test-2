75cf2bb72e2c89ff83aec1bda14bfd6c0e6d2ddf
akosthurzo <akos.thurzo@liferay.com>
2013-07-01 16:03:20 +0200

    LPS-37335 Make sure the WorkflowDefinitionLink is tied to live group

--- portal-impl/src/com/liferay/portal/service/impl/WorkflowDefinitionLinkLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/WorkflowDefinitionLinkLocalServiceImpl.java
@@ -60,6 +60,8 @@ public class WorkflowDefinitionLinkLocalServiceImpl
 		WorkflowDefinitionLink workflowDefinitionLink =
 			workflowDefinitionLinkPersistence.create(workflowDefinitionLinkId);
 
+		groupId = StagingUtil.getLiveGroupId(groupId);
+
 		workflowDefinitionLink.setCreateDate(now);
 		workflowDefinitionLink.setModifiedDate(now);
 		workflowDefinitionLink.setUserId(userId);
@@ -290,6 +292,8 @@ public class WorkflowDefinitionLinkLocalServiceImpl
 		User user = userPersistence.findByPrimaryKey(userId);
 		long classNameId = PortalUtil.getClassNameId(className);
 		Date now = new Date();
+
+		groupId = StagingUtil.getLiveGroupId(groupId);
 
 		WorkflowDefinitionLink workflowDefinitionLink =
 			workflowDefinitionLinkPersistence.fetchByG_C_C_C_T(
