8518eb4b2929e2042304df2f51f055d7844569d9
Sergio González <sergio.gonzalez@liferay.com>
2013-07-01 12:22:55 +0200

    LPS-37303 Upgrade process (all existing groups shouldn't have any restriction at all)

--- sql/update-6.1.1-6.2.0.sql
+++ sql/update-6.1.1-6.2.0.sql
@@ -358,12 +358,14 @@ update ExpandoRow set modifiedDate = CURRENT_TIMESTAMP;
 alter table Group_ add uuid_ VARCHAR(75) null;
 alter table Group_ add treePath STRING null;
 alter table Group_ add manualMembership BOOLEAN;
+alter table Group_ add membershipRestriction INTEGER;
 
 COMMIT_TRANSACTION;
 
 update Group_ set site = FALSE where name = 'Control Panel';
 update Group_ set site = TRUE where friendlyURL = '/global';
 update Group_ set manualMembership = TRUE;
+update Group_ set membershipRestriction = 0;
 
 drop table Groups_Permissions;
 
