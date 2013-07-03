ef9a435185e045083443769f261d92e49aef540d
Brian Chan <brian.chan@liferay.com>
2013-07-03 14:38:55 -0700

    LPS-37044 SF

--- portal-service/src/com/liferay/portal/kernel/lar/BasePortletDataHandler.java
+++ portal-service/src/com/liferay/portal/kernel/lar/BasePortletDataHandler.java
@@ -14,7 +14,6 @@
 
 package com.liferay.portal.kernel.lar;
 
-import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
