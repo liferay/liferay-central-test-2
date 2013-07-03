zberentey/liferay-portal

LPS-37252 Generated

portal-service/src/com/liferay/portal/model/Group.java
@@ -109,6 +109,8 @@ public interface Group extends GroupModel, PersistedModel {
 
 	public boolean hasPublicLayouts();
 
+	public boolean hasRemoteOrLocalStagingGroup();
+
 	public boolean hasStagingGroup();
 
 	/**

portal-service/src/com/liferay/portal/model/GroupWrapper.java
@@ -63,6 +63,7 @@ public class GroupWrapper implements Group, ModelWrapper<Group> {
 		attributes.put("friendlyURL", getFriendlyURL());
 		attributes.put("site", getSite());
 		attributes.put("active", getActive());
+		attributes.put("remoteStagingGroupCount", getRemoteStagingGroupCount());
 
 		return attributes;
 	}
@@ -163,6 +164,13 @@ public class GroupWrapper implements Group, ModelWrapper<Group> {
 
 		if (active != null) {
 			setActive(active);
+		}
+
+		Integer remoteStagingGroupCount = (Integer)attributes.get(
+				"remoteStagingGroupCount");
+
+		if (remoteStagingGroupCount != null) {
+			setRemoteStagingGroupCount(remoteStagingGroupCount);
 		}
 	}
 
@@ -563,6 +571,26 @@ public class GroupWrapper implements Group, ModelWrapper<Group> {
 		_group.setActive(active);
 	}
 
+	/**
+	* Returns the remote staging group count of this group.
+	*
+	* @return the remote staging group count of this group
+	*/
+	@Override
+	public int getRemoteStagingGroupCount() {
+		return _group.getRemoteStagingGroupCount();
+	}
+
+	/**
+	* Sets the remote staging group count of this group.
+	*
+	* @param remoteStagingGroupCount the remote staging group count of this group
+	*/
+	@Override
+	public void setRemoteStagingGroupCount(int remoteStagingGroupCount) {
+		_group.setRemoteStagingGroupCount(remoteStagingGroupCount);
+	}
+
 	@Override
 	public boolean isNew() {
 		return _group.isNew();
@@ -832,6 +860,11 @@ public class GroupWrapper implements Group, ModelWrapper<Group> {
 	@Override
 	public boolean hasPublicLayouts() {
 		return _group.hasPublicLayouts();
+	}
+
+	@Override
+	public boolean hasRemoteOrLocalStagingGroup() {
+		return _group.hasRemoteOrLocalStagingGroup();
 	}
 
 	@Override

liferay/liferay-central-test-2

portal-service/src/com/liferay/portal/model/Group.java
@@ -109,6 +109,8 @@ public interface Group extends GroupModel, PersistedModel {
 
 	public boolean hasPublicLayouts();
 
+	public boolean hasLocalOrRemoteStagingGroup();
+
 	public boolean hasStagingGroup();
 
 	/**

portal-service/src/com/liferay/portal/model/GroupWrapper.java
@@ -64,6 +64,7 @@ public class GroupWrapper implements Group, ModelWrapper<Group> {
 		attributes.put("membershipRestriction", getMembershipRestriction());
 		attributes.put("friendlyURL", getFriendlyURL());
 		attributes.put("site", getSite());
+		attributes.put("remoteStagingGroupCount", getRemoteStagingGroupCount());
 		attributes.put("active", getActive());
 
 		return attributes;
@@ -172,6 +173,13 @@ public class GroupWrapper implements Group, ModelWrapper<Group> {
 
 		if (site != null) {
 			setSite(site);
+		}
+
+		Integer remoteStagingGroupCount = (Integer)attributes.get(
+				"remoteStagingGroupCount");
+
+		if (remoteStagingGroupCount != null) {
+			setRemoteStagingGroupCount(remoteStagingGroupCount);
 		}
 
 		Boolean active = (Boolean)attributes.get("active");
@@ -599,6 +607,26 @@ public class GroupWrapper implements Group, ModelWrapper<Group> {
 	}
 
 	/**
+	* Returns the remote staging group count of this group.
+	*
+	* @return the remote staging group count of this group
+	*/
+	@Override
+	public int getRemoteStagingGroupCount() {
+		return _group.getRemoteStagingGroupCount();
+	}
+
+	/**
+	* Sets the remote staging group count of this group.
+	*
+	* @param remoteStagingGroupCount the remote staging group count of this group
+	*/
+	@Override
+	public void setRemoteStagingGroupCount(int remoteStagingGroupCount) {
+		_group.setRemoteStagingGroupCount(remoteStagingGroupCount);
+	}
+
+	/**
 	* Returns the active of this group.
 	*
 	* @return the active of this group
@@ -897,6 +925,11 @@ public class GroupWrapper implements Group, ModelWrapper<Group> {
 	@Override
 	public boolean hasPublicLayouts() {
 		return _group.hasPublicLayouts();
+	}
+
+	@Override
+	public boolean hasLocalOrRemoteStagingGroup() {
+		return _group.hasLocalOrRemoteStagingGroup();
 	}
 
 	@Override