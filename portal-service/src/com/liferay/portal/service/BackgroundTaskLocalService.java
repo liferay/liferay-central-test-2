brianchandotcom/liferay-portal

LPS-37409 autogenerated

portal-service/src/com/liferay/portal/service/BackgroundTaskLocalService.java
@@ -318,6 +318,9 @@ public interface BackgroundTaskLocalService extends BaseLocalService,
 		java.lang.String taskExecutorClassName)
 		throws com.liferay.portal.kernel.exception.SystemException;
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.lang.String fetchBackgroundTaskStatus(long backgroundTaskId);
+
 	public void resumeBackgroundTask(long backgroundTaskId)
 		throws com.liferay.portal.kernel.exception.SystemException;
 

portal-service/src/com/liferay/portal/service/BackgroundTaskLocalServiceUtil.java
@@ -381,6 +381,11 @@ public class BackgroundTaskLocalServiceUtil {
 				   .getBackgroundTasksCount(groupId, name, taskExecutorClassName);
 	}
 
+	public static java.lang.String fetchBackgroundTaskStatus(
+		long backgroundTaskId) {
+		return getService().fetchBackgroundTaskStatus(backgroundTaskId);
+	}
+
 	public static void resumeBackgroundTask(long backgroundTaskId)
 		throws com.liferay.portal.kernel.exception.SystemException {
 		getService().resumeBackgroundTask(backgroundTaskId);

portal-service/src/com/liferay/portal/service/BackgroundTaskLocalServiceWrapper.java
@@ -399,6 +399,11 @@ public class BackgroundTaskLocalServiceWrapper
 	}
 
 	@Override
+	public java.lang.String fetchBackgroundTaskStatus(long backgroundTaskId) {
+		return _backgroundTaskLocalService.fetchBackgroundTaskStatus(backgroundTaskId);
+	}
+
+	@Override
 	public void resumeBackgroundTask(long backgroundTaskId)
 		throws com.liferay.portal.kernel.exception.SystemException {
 		_backgroundTaskLocalService.resumeBackgroundTask(backgroundTaskId);

portal-service/src/com/liferay/portal/service/BackgroundTaskService.java
@@ -1,0 +1,63 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.service;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
+import com.liferay.portal.kernel.transaction.Isolation;
+import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.security.ac.AccessControlled;
+
+/**
+ * Provides the remote service interface for BackgroundTask. Methods of this
+ * service are expected to have security checks based on the propagated JAAS
+ * credentials because this service can be accessed remotely.
+ *
+ * @author Brian Wing Shun Chan
+ * @see BackgroundTaskServiceUtil
+ * @see com.liferay.portal.service.base.BackgroundTaskServiceBaseImpl
+ * @see com.liferay.portal.service.impl.BackgroundTaskServiceImpl
+ * @generated
+ */
+@AccessControlled
+@JSONWebService
+@Transactional(isolation = Isolation.PORTAL, rollbackFor =  {
+	PortalException.class, SystemException.class})
+public interface BackgroundTaskService extends BaseService {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify or reference this interface directly. Always use {@link BackgroundTaskServiceUtil} to access the background task remote service. Add custom service methods to {@link com.liferay.portal.service.impl.BackgroundTaskServiceImpl} and rerun ServiceBuilder to automatically copy the method declarations to this interface.
+	 */
+
+	/**
+	* Returns the Spring bean ID for this bean.
+	*
+	* @return the Spring bean ID for this bean
+	*/
+	public java.lang.String getBeanIdentifier();
+
+	/**
+	* Sets the Spring bean ID for this bean.
+	*
+	* @param beanIdentifier the Spring bean ID for this bean
+	*/
+	public void setBeanIdentifier(java.lang.String beanIdentifier);
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.lang.String fetchBackgroundTaskStatus(long backgroundTaskId);
+}

portal-service/src/com/liferay/portal/service/BackgroundTaskServiceUtil.java
@@ -1,0 +1,82 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.service;
+
+import com.liferay.portal.kernel.bean.PortalBeanLocatorUtil;
+import com.liferay.portal.kernel.util.ReferenceRegistry;
+
+/**
+ * Provides the remote service utility for BackgroundTask. This utility wraps
+ * {@link com.liferay.portal.service.impl.BackgroundTaskServiceImpl} and is the
+ * primary access point for service operations in application layer code running
+ * on a remote server. Methods of this service are expected to have security
+ * checks based on the propagated JAAS credentials because this service can be
+ * accessed remotely.
+ *
+ * @author Brian Wing Shun Chan
+ * @see BackgroundTaskService
+ * @see com.liferay.portal.service.base.BackgroundTaskServiceBaseImpl
+ * @see com.liferay.portal.service.impl.BackgroundTaskServiceImpl
+ * @generated
+ */
+public class BackgroundTaskServiceUtil {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this class directly. Add custom service methods to {@link com.liferay.portal.service.impl.BackgroundTaskServiceImpl} and rerun ServiceBuilder to regenerate this class.
+	 */
+
+	/**
+	* Returns the Spring bean ID for this bean.
+	*
+	* @return the Spring bean ID for this bean
+	*/
+	public static java.lang.String getBeanIdentifier() {
+		return getService().getBeanIdentifier();
+	}
+
+	/**
+	* Sets the Spring bean ID for this bean.
+	*
+	* @param beanIdentifier the Spring bean ID for this bean
+	*/
+	public static void setBeanIdentifier(java.lang.String beanIdentifier) {
+		getService().setBeanIdentifier(beanIdentifier);
+	}
+
+	public static java.lang.String fetchBackgroundTaskStatus(
+		long backgroundTaskId) {
+		return getService().fetchBackgroundTaskStatus(backgroundTaskId);
+	}
+
+	public static BackgroundTaskService getService() {
+		if (_service == null) {
+			_service = (BackgroundTaskService)PortalBeanLocatorUtil.locate(BackgroundTaskService.class.getName());
+
+			ReferenceRegistry.registerReference(BackgroundTaskServiceUtil.class,
+				"_service");
+		}
+
+		return _service;
+	}
+
+	/**
+	 * @deprecated As of 6.2.0
+	 */
+	public void setService(BackgroundTaskService service) {
+	}
+
+	private static BackgroundTaskService _service;
+}

portal-service/src/com/liferay/portal/service/BackgroundTaskServiceWrapper.java
@@ -1,0 +1,82 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.service;
+
+/**
+ * Provides a wrapper for {@link BackgroundTaskService}.
+ *
+ * @author Brian Wing Shun Chan
+ * @see BackgroundTaskService
+ * @generated
+ */
+public class BackgroundTaskServiceWrapper implements BackgroundTaskService,
+	ServiceWrapper<BackgroundTaskService> {
+	public BackgroundTaskServiceWrapper(
+		BackgroundTaskService backgroundTaskService) {
+		_backgroundTaskService = backgroundTaskService;
+	}
+
+	/**
+	* Returns the Spring bean ID for this bean.
+	*
+	* @return the Spring bean ID for this bean
+	*/
+	@Override
+	public java.lang.String getBeanIdentifier() {
+		return _backgroundTaskService.getBeanIdentifier();
+	}
+
+	/**
+	* Sets the Spring bean ID for this bean.
+	*
+	* @param beanIdentifier the Spring bean ID for this bean
+	*/
+	@Override
+	public void setBeanIdentifier(java.lang.String beanIdentifier) {
+		_backgroundTaskService.setBeanIdentifier(beanIdentifier);
+	}
+
+	@Override
+	public java.lang.String fetchBackgroundTaskStatus(long backgroundTaskId) {
+		return _backgroundTaskService.fetchBackgroundTaskStatus(backgroundTaskId);
+	}
+
+	/**
+	 * @deprecated As of 6.1.0, replaced by {@link #getWrappedService}
+	 */
+	public BackgroundTaskService getWrappedBackgroundTaskService() {
+		return _backgroundTaskService;
+	}
+
+	/**
+	 * @deprecated As of 6.1.0, replaced by {@link #setWrappedService}
+	 */
+	public void setWrappedBackgroundTaskService(
+		BackgroundTaskService backgroundTaskService) {
+		_backgroundTaskService = backgroundTaskService;
+	}
+
+	@Override
+	public BackgroundTaskService getWrappedService() {
+		return _backgroundTaskService;
+	}
+
+	@Override
+	public void setWrappedService(BackgroundTaskService backgroundTaskService) {
+		_backgroundTaskService = backgroundTaskService;
+	}
+
+	private BackgroundTaskService _backgroundTaskService;
+}

liferay/liferay-central-test-2

portal-service/src/com/liferay/portal/service/BackgroundTaskLocalService.java
@@ -318,6 +318,9 @@ public interface BackgroundTaskLocalService extends BaseLocalService,
 		java.lang.String taskExecutorClassName)
 		throws com.liferay.portal.kernel.exception.SystemException;
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.lang.String getBackgroundTaskStatusJSON(long backgroundTaskId);
+
 	public void resumeBackgroundTask(long backgroundTaskId)
 		throws com.liferay.portal.kernel.exception.SystemException;
 

portal-service/src/com/liferay/portal/service/BackgroundTaskLocalServiceUtil.java
@@ -381,6 +381,11 @@ public class BackgroundTaskLocalServiceUtil {
 				   .getBackgroundTasksCount(groupId, name, taskExecutorClassName);
 	}
 
+	public static java.lang.String fetchBackgroundTaskStatus(
+		long backgroundTaskId) {
+		return getService().getBackgroundTaskStatusJSON(backgroundTaskId);
+	}
+
 	public static void resumeBackgroundTask(long backgroundTaskId)
 		throws com.liferay.portal.kernel.exception.SystemException {
 		getService().resumeBackgroundTask(backgroundTaskId);

portal-service/src/com/liferay/portal/service/BackgroundTaskLocalServiceWrapper.java
@@ -399,6 +399,11 @@ public class BackgroundTaskLocalServiceWrapper
 	}
 
 	@Override
+	public java.lang.String getBackgroundTaskStatusJSON(long backgroundTaskId) {
+		return _backgroundTaskLocalService.getBackgroundTaskStatusJSON(backgroundTaskId);
+	}
+
+	@Override
 	public void resumeBackgroundTask(long backgroundTaskId)
 		throws com.liferay.portal.kernel.exception.SystemException {
 		_backgroundTaskLocalService.resumeBackgroundTask(backgroundTaskId);

portal-service/src/com/liferay/portal/service/BackgroundTaskService.java
@@ -1,0 +1,63 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.service;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
+import com.liferay.portal.kernel.transaction.Isolation;
+import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.security.ac.AccessControlled;
+
+/**
+ * Provides the remote service interface for BackgroundTask. Methods of this
+ * service are expected to have security checks based on the propagated JAAS
+ * credentials because this service can be accessed remotely.
+ *
+ * @author Brian Wing Shun Chan
+ * @see BackgroundTaskServiceUtil
+ * @see com.liferay.portal.service.base.BackgroundTaskServiceBaseImpl
+ * @see com.liferay.portal.service.impl.BackgroundTaskServiceImpl
+ * @generated
+ */
+@AccessControlled
+@JSONWebService
+@Transactional(isolation = Isolation.PORTAL, rollbackFor =  {
+	PortalException.class, SystemException.class})
+public interface BackgroundTaskService extends BaseService {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify or reference this interface directly. Always use {@link BackgroundTaskServiceUtil} to access the background task remote service. Add custom service methods to {@link com.liferay.portal.service.impl.BackgroundTaskServiceImpl} and rerun ServiceBuilder to automatically copy the method declarations to this interface.
+	 */
+
+	/**
+	* Returns the Spring bean ID for this bean.
+	*
+	* @return the Spring bean ID for this bean
+	*/
+	public java.lang.String getBeanIdentifier();
+
+	/**
+	* Sets the Spring bean ID for this bean.
+	*
+	* @param beanIdentifier the Spring bean ID for this bean
+	*/
+	public void setBeanIdentifier(java.lang.String beanIdentifier);
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.lang.String getBackgroundTaskStatusJSON(long backgroundTaskId);
+}

portal-service/src/com/liferay/portal/service/BackgroundTaskServiceUtil.java
@@ -1,0 +1,82 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.service;
+
+import com.liferay.portal.kernel.bean.PortalBeanLocatorUtil;
+import com.liferay.portal.kernel.util.ReferenceRegistry;
+
+/**
+ * Provides the remote service utility for BackgroundTask. This utility wraps
+ * {@link com.liferay.portal.service.impl.BackgroundTaskServiceImpl} and is the
+ * primary access point for service operations in application layer code running
+ * on a remote server. Methods of this service are expected to have security
+ * checks based on the propagated JAAS credentials because this service can be
+ * accessed remotely.
+ *
+ * @author Brian Wing Shun Chan
+ * @see BackgroundTaskService
+ * @see com.liferay.portal.service.base.BackgroundTaskServiceBaseImpl
+ * @see com.liferay.portal.service.impl.BackgroundTaskServiceImpl
+ * @generated
+ */
+public class BackgroundTaskServiceUtil {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this class directly. Add custom service methods to {@link com.liferay.portal.service.impl.BackgroundTaskServiceImpl} and rerun ServiceBuilder to regenerate this class.
+	 */
+
+	/**
+	* Returns the Spring bean ID for this bean.
+	*
+	* @return the Spring bean ID for this bean
+	*/
+	public static java.lang.String getBeanIdentifier() {
+		return getService().getBeanIdentifier();
+	}
+
+	/**
+	* Sets the Spring bean ID for this bean.
+	*
+	* @param beanIdentifier the Spring bean ID for this bean
+	*/
+	public static void setBeanIdentifier(java.lang.String beanIdentifier) {
+		getService().setBeanIdentifier(beanIdentifier);
+	}
+
+	public static java.lang.String fetchBackgroundTaskStatus(
+		long backgroundTaskId) {
+		return getService().getBackgroundTaskStatusJSON(backgroundTaskId);
+	}
+
+	public static BackgroundTaskService getService() {
+		if (_service == null) {
+			_service = (BackgroundTaskService)PortalBeanLocatorUtil.locate(BackgroundTaskService.class.getName());
+
+			ReferenceRegistry.registerReference(BackgroundTaskServiceUtil.class,
+				"_service");
+		}
+
+		return _service;
+	}
+
+	/**
+	 * @deprecated As of 6.2.0
+	 */
+	public void setService(BackgroundTaskService service) {
+	}
+
+	private static BackgroundTaskService _service;
+}

portal-service/src/com/liferay/portal/service/BackgroundTaskServiceWrapper.java
@@ -1,0 +1,82 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.service;
+
+/**
+ * Provides a wrapper for {@link BackgroundTaskService}.
+ *
+ * @author Brian Wing Shun Chan
+ * @see BackgroundTaskService
+ * @generated
+ */
+public class BackgroundTaskServiceWrapper implements BackgroundTaskService,
+	ServiceWrapper<BackgroundTaskService> {
+	public BackgroundTaskServiceWrapper(
+		BackgroundTaskService backgroundTaskService) {
+		_backgroundTaskService = backgroundTaskService;
+	}
+
+	/**
+	* Returns the Spring bean ID for this bean.
+	*
+	* @return the Spring bean ID for this bean
+	*/
+	@Override
+	public java.lang.String getBeanIdentifier() {
+		return _backgroundTaskService.getBeanIdentifier();
+	}
+
+	/**
+	* Sets the Spring bean ID for this bean.
+	*
+	* @param beanIdentifier the Spring bean ID for this bean
+	*/
+	@Override
+	public void setBeanIdentifier(java.lang.String beanIdentifier) {
+		_backgroundTaskService.setBeanIdentifier(beanIdentifier);
+	}
+
+	@Override
+	public java.lang.String getBackgroundTaskStatusJSON(long backgroundTaskId) {
+		return _backgroundTaskService.getBackgroundTaskStatusJSON(backgroundTaskId);
+	}
+
+	/**
+	 * @deprecated As of 6.1.0, replaced by {@link #getWrappedService}
+	 */
+	public BackgroundTaskService getWrappedBackgroundTaskService() {
+		return _backgroundTaskService;
+	}
+
+	/**
+	 * @deprecated As of 6.1.0, replaced by {@link #setWrappedService}
+	 */
+	public void setWrappedBackgroundTaskService(
+		BackgroundTaskService backgroundTaskService) {
+		_backgroundTaskService = backgroundTaskService;
+	}
+
+	@Override
+	public BackgroundTaskService getWrappedService() {
+		return _backgroundTaskService;
+	}
+
+	@Override
+	public void setWrappedService(BackgroundTaskService backgroundTaskService) {
+		_backgroundTaskService = backgroundTaskService;
+	}
+
+	private BackgroundTaskService _backgroundTaskService;
+}