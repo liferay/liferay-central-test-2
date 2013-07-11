35e1621cc59f4679a36afaae2f55a7bb316d62bc
Zsolt Berentey <zsolt.berentey@liferay.com>
2013-07-11 11:24:14 +0200

    LPS-37677 Try to reliably get the first element node even if strip filter is turned off

--- portal-web/docroot/html/js/liferay/portlet.js
+++ portal-web/docroot/html/js/liferay/portlet.js
@@ -266,7 +266,7 @@
 				portletBound.plug(A.Plugin.ParseContent);
 
 				portletBound.setContent(html);
-				portletBound = portletBound.get('firstChild');
+				portletBound = portletBound.one('> *');
 
 				var id = portletBound.attr('id');
 
