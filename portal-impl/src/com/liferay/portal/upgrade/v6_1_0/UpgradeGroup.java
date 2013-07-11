e3d7b225da08c2bb592f5b048fdbcbe25076e9d4
Jose Jiménez <jose.jimenez@liferay.com>
2013-07-11 16:58:07 +0200

    LPS-36026 classNameId must be retrieved from default shardName

--- portal-impl/src/com/liferay/portal/upgrade/v6_1_0/UpgradeGroup.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_1_0/UpgradeGroup.java
@@ -15,11 +15,14 @@
 package com.liferay.portal.upgrade.v6_1_0;
 
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.dao.shard.ShardUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Organization;
 import com.liferay.portal.upgrade.v6_1_0.util.GroupTable;
+import com.liferay.portal.util.PropsValues;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -52,7 +55,12 @@ public class UpgradeGroup extends UpgradeProcess {
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 
+		String currentShardName = null;
+
 		try {
+			currentShardName = ShardUtil.setTargetSource(
+				PropsValues.SHARD_DEFAULT_NAME);
+
 			con = DataAccess.getUpgradeOptimizedConnection();
 
 			ps = con.prepareStatement(
@@ -69,6 +77,10 @@ public class UpgradeGroup extends UpgradeProcess {
 			return 0;
 		}
 		finally {
+			if (Validator.isNotNull(currentShardName)) {
+				ShardUtil.setTargetSource(currentShardName);
+			}
+
 			DataAccess.cleanUp(con, ps, rs);
 		}
 	}
