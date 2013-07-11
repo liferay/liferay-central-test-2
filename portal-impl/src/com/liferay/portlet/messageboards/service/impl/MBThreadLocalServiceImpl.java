b4585bc3fe2ffd9e66200e8ecc78f82f70fb56d3
ealonso <eudaldo.alonso@liferay.com>
2013-07-11 13:33:17 +0200

    LPS-37627 After moving a Message Board post to the Recycle Bin it is still visible in My Workflow Tasks

--- portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBThreadLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBThreadLocalServiceImpl.java
@@ -744,6 +744,14 @@ public class MBThreadLocalServiceImpl extends MBThreadLocalServiceBaseImpl {
 			return thread;
 		}
 
+		int oldStatus = thread.getStatus();
+
+		if (oldStatus == WorkflowConstants.STATUS_PENDING) {
+			thread.setStatus(WorkflowConstants.STATUS_DRAFT);
+
+			mbThreadPersistence.update(thread);
+		}
+
 		return updateStatus(
 			userId, thread.getThreadId(), WorkflowConstants.STATUS_IN_TRASH,
 			WorkflowConstants.STATUS_ANY);
@@ -916,15 +924,6 @@ public class MBThreadLocalServiceImpl extends MBThreadLocalServiceBaseImpl {
 			Date now = new Date();
 
 			int oldStatus = thread.getStatus();
-
-			if (oldStatus == WorkflowConstants.STATUS_PENDING) {
-				MBMessage rootMessage = mbMessageLocalService.getMBMessage(
-					thread.getRootMessageId());
-
-				rootMessage.setStatus(WorkflowConstants.STATUS_DRAFT);
-
-				mbMessagePersistence.update(rootMessage);
-			}
 
 			thread.setModifiedDate(now);
 			thread.setStatus(status);
