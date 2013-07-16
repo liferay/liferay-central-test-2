02c8df3d3ae662f34fa71067e482abda0b316ffb
Eduardo Garcia <eduardo.garcia@liferay.com>
2013-07-16 15:52:54 +0200

    LPS-37813 Remove portlet specific export import logic from BaseExportImportTestCase

--- portal-impl/test/integration/com/liferay/portal/lar/BaseExportImportTestCase.java
+++ portal-impl/test/integration/com/liferay/portal/lar/BaseExportImportTestCase.java
@@ -29,23 +29,14 @@ import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.service.ServiceTestUtil;
 import com.liferay.portal.util.GroupTestUtil;
 import com.liferay.portal.util.LayoutTestUtil;
-import com.liferay.portal.util.TestPropsValues;
-import com.liferay.portlet.asset.model.AssetEntry;
-import com.liferay.portlet.asset.model.AssetLink;
-import com.liferay.portlet.asset.service.AssetEntryLocalServiceUtil;
-import com.liferay.portlet.asset.service.AssetLinkLocalServiceUtil;
 
 import java.io.File;
 
-import java.util.Iterator;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
 
 import org.powermock.api.mockito.PowerMockito;
 
@@ -53,14 +44,6 @@ import org.powermock.api.mockito.PowerMockito;
  * @author Eduardo Garcia
  */
 public class BaseExportImportTestCase extends PowerMockito {
-
-	public String getNamespace() {
-		return null;
-	}
-
-	public String getPortletId() {
-		return null;
-	}
 
 	@Before
 	public void setUp() throws Exception {
@@ -99,51 +82,6 @@ public class BaseExportImportTestCase extends PowerMockito {
 		}
 	}
 
-	@Test
-	public void testExportImportAssetLinks() throws Exception {
-		StagedModel stagedModel = addStagedModel(group.getGroupId());
-
-		StagedModel relatedStagedModel1 = addStagedModel(group.getGroupId());
-		StagedModel relatedStagedModel2 = addStagedModel(group.getGroupId());
-
-		addAssetLink(
-			group.getGroupId(), getStagedModelUuid(stagedModel),
-			getStagedModelUuid(relatedStagedModel1), 1);
-		addAssetLink(
-			group.getGroupId(), getStagedModelUuid(stagedModel),
-			getStagedModelUuid(relatedStagedModel2), 2);
-
-		doExportImportPortlet(getPortletId());
-
-		StagedModel importedStagedModel = getStagedModel(
-			getStagedModelUuid(stagedModel), importedGroup.getGroupId());
-
-		Assert.assertNotNull(importedStagedModel);
-
-		validateImportedLinks(getStagedModelUuid(stagedModel));
-	}
-
-	protected AssetLink addAssetLink(
-			long groupId, String sourceStagedModelUuid,
-			String targetStagedModelUuid, int weight)
-		throws PortalException, SystemException {
-
-		AssetEntry originAssetEntry = AssetEntryLocalServiceUtil.getEntry(
-			groupId, sourceStagedModelUuid);
-		AssetEntry targetAssetEntry = AssetEntryLocalServiceUtil.getEntry(
-			groupId, targetStagedModelUuid);
-
-		return AssetLinkLocalServiceUtil.addLink(
-			TestPropsValues.getUserId(), originAssetEntry.getEntryId(),
-			targetAssetEntry.getEntryId(), 0, weight);
-	}
-
-	protected void addParameter(
-		Map<String, String[]> parameterMap, String name, boolean value) {
-
-		addParameter(parameterMap, getNamespace(), name, value);
-	}
-
 	protected void addParameter(
 		Map<String, String[]> parameterMap, String name, String value) {
 
@@ -164,20 +102,6 @@ public class BaseExportImportTestCase extends PowerMockito {
 
 	protected StagedModel addStagedModel(long groupId) throws Exception {
 		return null;
-	}
-
-	protected void doExportImportPortlet(String portletId) throws Exception {
-		larFile = LayoutLocalServiceUtil.exportPortletInfoAsFile(
-			layout.getPlid(), layout.getGroupId(), portletId,
-			getExportParameterMap(), null, null);
-
-		importedLayout = LayoutTestUtil.addLayout(
-			importedGroup.getGroupId(), ServiceTestUtil.randomString());
-
-		LayoutLocalServiceUtil.importPortletInfo(
-			TestPropsValues.getUserId(), importedLayout.getPlid(),
-			importedGroup.getGroupId(), portletId, getImportParameterMap(),
-			larFile);
 	}
 
 	protected Map<String, String[]> getExportParameterMap() throws Exception {
@@ -242,69 +166,6 @@ public class BaseExportImportTestCase extends PowerMockito {
 		throws PortalException, SystemException {
 
 		return stagedModel.getUuid();
-	}
-
-	protected void validateImportedLinks(String uuid)
-		throws PortalException, SystemException {
-
-		AssetEntry originalAssetEntry = AssetEntryLocalServiceUtil.getEntry(
-			group.getGroupId(), uuid);
-
-		List<AssetLink> originalAssetLinks = AssetLinkLocalServiceUtil.getLinks(
-			originalAssetEntry.getEntryId());
-
-		AssetEntry importedAssetEntry =  AssetEntryLocalServiceUtil.getEntry(
-			importedGroup.getGroupId(), uuid);
-
-		List<AssetLink> importedAssetLinks = AssetLinkLocalServiceUtil.getLinks(
-			importedAssetEntry.getEntryId());
-
-		Assert.assertEquals(
-			originalAssetLinks.size(), importedAssetLinks.size());
-
-		for (AssetLink originalLink : originalAssetLinks) {
-			AssetEntry sourceAssetEntry = AssetEntryLocalServiceUtil.getEntry(
-				originalLink.getEntryId1());
-
-			AssetEntry targetAssetEntry = AssetEntryLocalServiceUtil.getEntry(
-				originalLink.getEntryId2());
-
-			Iterator<AssetLink> iterator = importedAssetLinks.iterator();
-
-			while (iterator.hasNext()) {
-				AssetLink importedLink = iterator.next();
-
-				AssetEntry importedLinkSourceAssetEntry =
-					AssetEntryLocalServiceUtil.getEntry(
-						importedLink.getEntryId1());
-				AssetEntry importedLinkTargetAssetEntry =
-					AssetEntryLocalServiceUtil.getEntry(
-						importedLink.getEntryId2());
-
-				if (!sourceAssetEntry.getClassUuid().equals(
-						importedLinkSourceAssetEntry.getClassUuid())) {
-
-					continue;
-				}
-
-				if (!targetAssetEntry.getClassUuid().equals(
-						importedLinkTargetAssetEntry.getClassUuid())) {
-
-					continue;
-				}
-
-				Assert.assertEquals(
-					originalLink.getWeight(), importedLink.getWeight());
-				Assert.assertEquals(
-					originalLink.getType(), importedLink.getType());
-
-				iterator.remove();
-
-				break;
-			}
-		}
-
-		Assert.assertEquals(0, importedAssetLinks.size());
 	}
 
 	protected Group group;
