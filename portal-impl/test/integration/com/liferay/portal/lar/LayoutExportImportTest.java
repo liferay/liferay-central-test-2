febcd7c0f016a6fdedada4ee088fa2801e64b80d
Eduardo Garcia <eduardo.garcia@liferay.com>
2013-07-16 16:58:54 +0200

    LPS-37813 Add test for exporting and importing pages

--- portal-impl/test/integration/com/liferay/portal/lar/LayoutExportImportTest.java
+++ portal-impl/test/integration/com/liferay/portal/lar/LayoutExportImportTest.java
@@ -16,10 +16,17 @@ package com.liferay.portal.lar;
 
 import com.liferay.portal.kernel.test.ExecutionTestListeners;
 import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.model.Layout;
+import com.liferay.portal.service.LayoutLocalServiceUtil;
+import com.liferay.portal.service.ServiceTestUtil;
 import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
 import com.liferay.portal.test.MainServletExecutionTestListener;
 import com.liferay.portal.test.TransactionalCallbackAwareExecutionTestListener;
+import com.liferay.portal.util.LayoutTestUtil;
+import com.liferay.portal.util.TestPropsValues;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -33,4 +40,48 @@ import org.junit.runner.RunWith;
 @RunWith(LiferayIntegrationJUnitTestRunner.class)
 @Transactional
 public class LayoutExportImportTest extends BaseExportImportTestCase {
+
+	@Test
+	public void testExportImportLayouts() throws Exception {
+		LayoutTestUtil.addLayout(
+			group.getGroupId(), ServiceTestUtil.randomString());
+
+		long[] layoutIds = new long[0];
+
+		doExportImportLayout(layoutIds);
+
+		Assert.assertEquals(
+			LayoutLocalServiceUtil.getLayoutsCount(group, false),
+			LayoutLocalServiceUtil.getLayoutsCount(importedGroup, false));
+	}
+
+	@Test
+	public void testExportImportSelectedLayouts() throws Exception {
+		Layout layout2 = LayoutTestUtil.addLayout(
+			group.getGroupId(), ServiceTestUtil.randomString());
+
+		long[] layoutIds = new long[] {layout2.getLayoutId()};
+
+		doExportImportLayout(layoutIds);
+
+		Assert.assertEquals(
+			layoutIds.length,
+			LayoutLocalServiceUtil.getLayoutsCount(importedGroup, false));
+
+		importedLayout = LayoutLocalServiceUtil.fetchLayoutByUuidAndGroupId(
+			layout2.getUuid(), importedGroup.getGroupId(), false);
+
+		Assert.assertNotNull(importedLayout);
+	}
+
+	protected void doExportImportLayout(long[] layoutIds) throws Exception {
+		larFile = LayoutLocalServiceUtil.exportLayoutsAsFile(
+			group.getGroupId(), false, layoutIds, getExportParameterMap(), null,
+			null);
+
+		LayoutLocalServiceUtil.importLayouts(
+			TestPropsValues.getUserId(), importedGroup.getGroupId(), false,
+			getImportParameterMap(), larFile);
+	}
+
 }
