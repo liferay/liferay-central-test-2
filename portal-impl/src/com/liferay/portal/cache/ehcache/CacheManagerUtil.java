c5afbba47feb3423fe345295cb0a9b49ac93d4c7
shuyangzhou <shuyang.zhou@liferay.com>
2013-07-17 15:15:23 -0700

    LPS-37582 Workaround for Sun JDK6 bug http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6522773

--- portal-impl/src/com/liferay/portal/cache/ehcache/CacheManagerUtil.java
+++ portal-impl/src/com/liferay/portal/cache/ehcache/CacheManagerUtil.java
@@ -14,12 +14,17 @@
 
 package com.liferay.portal.cache.ehcache;
 
+import com.liferay.portal.kernel.util.JavaDetector;
 import com.liferay.portal.kernel.util.ReflectionUtil;
 import com.liferay.portal.util.PropsValues;
 
 import java.lang.reflect.Field;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.RunnableScheduledFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.config.Configuration;
@@ -37,8 +42,37 @@ public class CacheManagerUtil {
 				(ScheduledThreadPoolExecutor)_statisticsExecutorField.get(
 					cacheManager);
 
+			BlockingQueue<Runnable> blockingQueue = null;
+
+			// This odd logic is a workaround for
+			// http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6522773
+
+			if (JavaDetector.isJDK6()) {
+				blockingQueue = (BlockingQueue<Runnable>)_workQueueField.get(
+					scheduledThreadPoolExecutor);;
+
+				_workQueueField.set(
+					scheduledThreadPoolExecutor,
+					new DelayQueue<RunnableScheduledFuture>() {
+
+						@Override
+						public int remainingCapacity() {
+							return 0;
+						}
+
+					});
+			}
+
 			scheduledThreadPoolExecutor.setCorePoolSize(
 				PropsValues.EHCACHE_CACHE_MANAGER_STATISTICS_THREAD_POOL_SIZE);
+
+			if (JavaDetector.isJDK6()) {
+				while (
+					scheduledThreadPoolExecutor.getPoolSize() >
+						PropsValues.EHCACHE_CACHE_MANAGER_STATISTICS_THREAD_POOL_SIZE);
+
+				_workQueueField.set(scheduledThreadPoolExecutor, blockingQueue);
+			}
 		}
 		catch (Exception e) {
 			throw new RuntimeException(e);
@@ -48,11 +82,17 @@ public class CacheManagerUtil {
 	}
 
 	private static Field _statisticsExecutorField;
+	private static Field _workQueueField;
 
 	static {
 		try {
 			_statisticsExecutorField = ReflectionUtil.getDeclaredField(
 				CacheManager.class, "statisticsExecutor");
+
+			if (JavaDetector.isJDK6()) {
+				_workQueueField = ReflectionUtil.getDeclaredField(
+					ThreadPoolExecutor.class, "workQueue");
+			}
 		}
 		catch (Exception e) {
 			throw new ExceptionInInitializerError(e);
