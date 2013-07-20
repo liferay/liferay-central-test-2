0e4496c5e33cbfb20656decad4d9e33ff40540e3
Brian Chan <brian.chan@liferay.com>
2013-07-19 17:04:24 -0700

    Prepare 6.2.0 CE SF

--- portal-service/src/com/liferay/portal/kernel/util/ReleaseInfo.java
+++ portal-service/src/com/liferay/portal/kernel/util/ReleaseInfo.java
@@ -156,7 +156,7 @@ public class ReleaseInfo {
 
 	private static final String _VERSION = "6.2.0";
 
-	private static final String _VERSION_DISPLAY_NAME = "6.2.0 CE ALPHA1";
+	private static final String _VERSION_DISPLAY_NAME = "6.2.0 CE A1";
 
 	private static String _releaseInfo;
 	private static String _serverInfo;
