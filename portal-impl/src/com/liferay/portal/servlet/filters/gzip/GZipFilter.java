ead6b318492f2ce93da6445b96aad589403647f4
Brian Chan <brian.chan@liferay.com>
2013-07-23 09:17:28 -0700

    LPS-37990 SF

--- portal-impl/src/com/liferay/portal/servlet/filters/gzip/GZipFilter.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/gzip/GZipFilter.java
@@ -38,8 +38,8 @@ public class GZipFilter extends BasePortalFilter {
 
 	public GZipFilter() {
 
-		// The compression filter will work on JBoss, JOnAS, OC4J, and
-		// Tomcat, but may break on other servers
+		// The compression filter will work on JBoss, JOnAS, OC4J, and Tomcat,
+		// but may break on other servers
 
 		if (super.isFilterEnabled()) {
 			if (ServerDetector.isJBoss() || ServerDetector.isJOnAS() ||
