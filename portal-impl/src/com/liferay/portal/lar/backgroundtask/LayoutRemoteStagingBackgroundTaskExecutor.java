605bcecfa6ada5b70311b8ce5eaca2101a9a4ca6
Julio Camarero <julio.camarero@liferay.com>
2013-07-23 15:54:32 +0200

    LPS-37434 copy/paste from LayoutStagingBackgroundTaskExecutor (no changes)

---
+++ portal-impl/src/com/liferay/portal/lar/backgroundtask/LayoutRemoteStagingBackgroundTaskExecutor.java
@@ -0,0 +1,129 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.lar.backgroundtask;
+
+import com.liferay.portal.kernel.backgroundtask.BackgroundTaskConstants;
+import com.liferay.portal.kernel.backgroundtask.BackgroundTaskResult;
+import com.liferay.portal.kernel.backgroundtask.BackgroundTaskStatus;
+import com.liferay.portal.kernel.backgroundtask.BackgroundTaskStatusRegistryUtil;
+import com.liferay.portal.kernel.backgroundtask.BaseBackgroundTaskExecutor;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.lar.MissingReference;
+import com.liferay.portal.kernel.lar.MissingReferences;
+import com.liferay.portal.kernel.staging.StagingUtil;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.model.BackgroundTask;
+import com.liferay.portal.service.LayoutLocalServiceUtil;
+
+import java.io.File;
+import java.io.Serializable;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * @author Julio Camarero
+ */
+public class LayoutRemoteStagingBackgroundTaskExecutor
+	extends BaseBackgroundTaskExecutor {
+
+	public LayoutRemoteStagingBackgroundTaskExecutor() {
+		setBackgroundTaskStatusMessageTranslator(
+			new DefaultExportImportBackgroundTaskStatusMessageTranslator());
+
+		setSerial(true);
+	}
+
+	@Override
+	public BackgroundTaskResult execute(BackgroundTask backgroundTask)
+		throws Exception {
+
+		Map<String, Serializable> taskContextMap =
+			backgroundTask.getTaskContextMap();
+
+		long userId = MapUtil.getLong(taskContextMap, "userId");
+		long targetGroupId = MapUtil.getLong(taskContextMap, "targetGroupId");
+
+		StagingUtil.lockGroup(userId, targetGroupId);
+
+		long sourceGroupId = MapUtil.getLong(taskContextMap, "sourceGroupId");
+		boolean privateLayout = MapUtil.getBoolean(
+			taskContextMap, "privateLayout");
+		long[] layoutIds = GetterUtil.getLongValues(
+			taskContextMap.get("layoutIds"));
+		Map<String, String[]> parameterMap =
+			(Map<String, String[]>)taskContextMap.get("parameterMap");
+		Date startDate = (Date)taskContextMap.get("startDate");
+		Date endDate = (Date)taskContextMap.get("endDate");
+
+		BackgroundTaskStatus backgroundTaskStatus =
+			BackgroundTaskStatusRegistryUtil.getBackgroundTaskStatus(
+				backgroundTask.getBackgroundTaskId());
+
+		backgroundTaskStatus.clearAttributes();
+
+		File file = null;
+		MissingReferences missingReferences = null;
+
+		try {
+			file = LayoutLocalServiceUtil.exportLayoutsAsFile(
+				sourceGroupId, privateLayout, layoutIds, parameterMap,
+				startDate, endDate);
+
+			missingReferences =
+				LayoutLocalServiceUtil.validateImportLayoutsFile(
+					userId, targetGroupId, privateLayout, parameterMap,
+					file);
+
+			LayoutLocalServiceUtil.importLayouts(
+				userId, targetGroupId, privateLayout, parameterMap, file);
+		}
+		finally {
+			FileUtil.delete(file);
+
+			StagingUtil.unlockGroup(targetGroupId);
+		}
+
+		BackgroundTaskResult backgroundTaskResult = new BackgroundTaskResult(
+			BackgroundTaskConstants.STATUS_SUCCESSFUL);
+
+		Map<String, MissingReference> weakMissingReferences =
+			missingReferences.getWeakMissingReferences();
+
+		if ((weakMissingReferences != null) &&
+			!weakMissingReferences.isEmpty()) {
+
+			JSONArray jsonArray = StagingUtil.getWarningMessagesJSONArray(
+				getLocale(backgroundTask), weakMissingReferences,
+				backgroundTask.getTaskContextMap());
+
+			backgroundTaskResult.setStatusMessage(jsonArray.toString());
+		}
+
+		return backgroundTaskResult;
+	}
+
+	@Override
+	public String handleException(BackgroundTask backgroundTask, Exception e) {
+		JSONObject jsonObject = StagingUtil.getExceptionMessagesJSONObject(
+			getLocale(backgroundTask), e, backgroundTask.getTaskContextMap());
+
+		return jsonObject.toString();
+	}
+
+}
