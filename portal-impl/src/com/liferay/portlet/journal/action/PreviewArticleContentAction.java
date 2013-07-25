0b4066b4d2ae0aa3c0e39f42651fd6abfe2dcc7a
Julio Camarero <julio.camarero@liferay.com>
2013-07-25 15:17:43 +0200

    LPS-37814 SF

--- portal-impl/src/com/liferay/portlet/journal/action/PreviewArticleContentAction.java
+++ portal-impl/src/com/liferay/portlet/journal/action/PreviewArticleContentAction.java
@@ -15,9 +15,16 @@
 package com.liferay.portlet.journal.action;
 
 import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.security.auth.PrincipalException;
 import com.liferay.portal.struts.PortletAction;
+import com.liferay.portal.util.PortalUtil;
 import com.liferay.portlet.journal.NoSuchArticleException;
+import com.liferay.portlet.journal.model.JournalArticle;
+import com.liferay.portlet.journal.model.JournalArticleConstants;
+import com.liferay.portlet.journal.service.JournalArticleServiceUtil;
+import com.liferay.portlet.journal.util.JournalUtil;
 
 import javax.portlet.PortletConfig;
 import javax.portlet.PortletRequest;
