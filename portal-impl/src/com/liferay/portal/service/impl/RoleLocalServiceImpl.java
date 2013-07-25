mhan810/liferay-portal

LPS-35256 Send system event when deleting a model (part 1)

portal-impl/src/com/liferay/portal/service/impl/RoleLocalServiceImpl.java
@@ -30,6 +30,7 @@ import com.liferay.portal.kernel.lar.ExportImportThreadLocal;
 import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.IndexerRegistryUtil;
 import com.liferay.portal.kernel.spring.aop.Skip;
+import com.liferay.portal.kernel.systemevents.SystemEvent;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.Transactional;
 import com.liferay.portal.kernel.util.CharPool;
@@ -49,6 +50,7 @@ import com.liferay.portal.model.ResourceTypePermission;
 import com.liferay.portal.model.Role;
 import com.liferay.portal.model.RoleConstants;
 import com.liferay.portal.model.Shard;
+import com.liferay.portal.model.SystemEventConstants;
 import com.liferay.portal.model.Team;
 import com.liferay.portal.model.User;
 import com.liferay.portal.security.auth.CompanyThreadLocal;
@@ -416,7 +418,7 @@ public class RoleLocalServiceImpl extends RoleLocalServiceBaseImpl {
 
 		Role role = rolePersistence.findByPrimaryKey(roleId);
 
-		return deleteRole(role);
+		return roleLocalService.deleteRole(role);
 	}
 
 	/**
@@ -429,6 +431,9 @@ public class RoleLocalServiceImpl extends RoleLocalServiceBaseImpl {
 	 * @throws SystemException if a system exception occurred
 	 */
 	@Override
+	@SystemEvent(
+		childAction = SystemEventConstants.ACTION_SKIP,
+		type = SystemEventConstants.TYPE_DELETE)
 	public Role deleteRole(Role role) throws PortalException, SystemException {
 		if (PortalUtil.isSystemRole(role.getName()) &&
 			!CompanyThreadLocal.isDeleteInProcess()) {

portal-impl/src/com/liferay/portlet/journal/service/impl/JournalFolderLocalServiceImpl.java
@@ -21,12 +21,14 @@ import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.systemevents.SystemEvent;
 import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.model.ResourceConstants;
+import com.liferay.portal.model.SystemEventConstants;
 import com.liferay.portal.model.User;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.util.PropsValues;
@@ -101,6 +103,9 @@ public class JournalFolderLocalServiceImpl
 
 	@Indexable(type = IndexableType.DELETE)
 	@Override
+	@SystemEvent(
+		childAction = SystemEventConstants.ACTION_SKIP, sendEvent = false,
+		type = SystemEventConstants.TYPE_DELETE)
 	public JournalFolder deleteFolder(JournalFolder folder)
 		throws PortalException, SystemException {
 
@@ -109,6 +114,9 @@ public class JournalFolderLocalServiceImpl
 
 	@Indexable(type = IndexableType.DELETE)
 	@Override
+	@SystemEvent(
+		childAction = SystemEventConstants.ACTION_SKIP, sendEvent = false,
+		type = SystemEventConstants.TYPE_DELETE)
 	public JournalFolder deleteFolder(
 			JournalFolder folder, boolean includeTrashedEntries)
 		throws PortalException, SystemException {
@@ -164,7 +172,7 @@ public class JournalFolderLocalServiceImpl
 		JournalFolder folder = journalFolderPersistence.findByPrimaryKey(
 			folderId);
 
-		return deleteFolder(folder, true);
+		return journalFolderLocalService.deleteFolder(folder, true);
 	}
 
 	@Indexable(type = IndexableType.DELETE)
@@ -176,7 +184,8 @@ public class JournalFolderLocalServiceImpl
 		JournalFolder folder = journalFolderPersistence.findByPrimaryKey(
 			folderId);
 
-		return deleteFolder(folder, includeTrashedEntries);
+		return journalFolderLocalService.deleteFolder(
+			folder, includeTrashedEntries);
 	}
 
 	@Override

portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBBanLocalServiceImpl.java
@@ -18,6 +18,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.systemevents.SystemEvent;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.Transactional;
 import com.liferay.portal.model.SystemEventConstants;
@@ -84,7 +85,7 @@ public class MBBanLocalServiceImpl extends MBBanLocalServiceBaseImpl {
 	public void deleteBan(long banId) throws PortalException, SystemException {
 		MBBan ban = mbBanPersistence.findByPrimaryKey(banId);
 
-		deleteBan(ban);
+		mbBanLocalService.deleteBan(ban);
 	}
 
 	@Override
@@ -96,19 +97,16 @@ public class MBBanLocalServiceImpl extends MBBanLocalServiceBaseImpl {
 		try {
 			MBBan ban = mbBanPersistence.findByG_B(groupId, banUserId);
 
-			deleteBan(ban);
+			mbBanLocalService.deleteBan(ban);
 		}
 		catch (NoSuchBanException nsbe) {
 		}
 	}
 
 	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
 	public void deleteBan(MBBan ban) throws PortalException, SystemException {
 		mbBanPersistence.remove(ban);
-
-		systemEventLocalService.addSystemEvent(
-			ban.getGroupId(), MBBan.class.getName(), ban.getBanId(),
-			ban.getUuid(), SystemEventConstants.TYPE_DELETE);
 	}
 
 	@Override

portal-impl/src/com/liferay/portlet/mobiledevicerules/service/impl/MDRRuleGroupInstanceLocalServiceImpl.java
@@ -17,6 +17,7 @@ package com.liferay.portlet.mobiledevicerules.service.impl;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.systemevents.SystemEvent;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.model.SystemEventConstants;
 import com.liferay.portal.model.User;
@@ -101,7 +102,8 @@ public class MDRRuleGroupInstanceLocalServiceImpl
 			mdrRuleGroupInstancePersistence.findByGroupId(groupId);
 
 		for (MDRRuleGroupInstance ruleGroupInstance : ruleGroupInstances) {
-			deleteRuleGroupInstance(ruleGroupInstance);
+			mdrRuleGroupInstanceLocalService.deleteRuleGroupInstance(
+				ruleGroupInstance);
 		}
 	}
 
@@ -113,24 +115,20 @@ public class MDRRuleGroupInstanceLocalServiceImpl
 			mdrRuleGroupInstancePersistence.fetchByPrimaryKey(
 				ruleGroupInstanceId);
 
-		deleteRuleGroupInstance(ruleGroupInstance);
+		mdrRuleGroupInstanceLocalService.deleteRuleGroupInstance(
+			ruleGroupInstance);
 	}
 
 	@Override
+	@SystemEvent(
+		childAction = SystemEventConstants.ACTION_SKIP,
+		type = SystemEventConstants.TYPE_DELETE)
 	public void deleteRuleGroupInstance(MDRRuleGroupInstance ruleGroupInstance)
 		throws PortalException, SystemException {
 
 		// Rule group instance
 
 		mdrRuleGroupInstancePersistence.remove(ruleGroupInstance);
-
-		// System event
-
-		systemEventLocalService.addSystemEvent(
-			ruleGroupInstance.getGroupId(),
-			MDRRuleGroupInstance.class.getName(),
-			ruleGroupInstance.getRuleGroupInstanceId(),
-			ruleGroupInstance.getUuid(), SystemEventConstants.TYPE_DELETE);
 
 		// Rule actions
 
@@ -146,7 +144,8 @@ public class MDRRuleGroupInstanceLocalServiceImpl
 			mdrRuleGroupInstancePersistence.findByRuleGroupId(ruleGroupId);
 
 		for (MDRRuleGroupInstance ruleGroupInstance : ruleGroupInstances) {
-			deleteRuleGroupInstance(ruleGroupInstance);
+			mdrRuleGroupInstanceLocalService.deleteRuleGroupInstance(
+				ruleGroupInstance);
 		}
 	}
 

portal-impl/src/com/liferay/portlet/mobiledevicerules/service/impl/MDRRuleLocalServiceImpl.java
@@ -16,6 +16,7 @@ package com.liferay.portlet.mobiledevicerules.service.impl;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.systemevents.SystemEvent;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.model.SystemEventConstants;
 import com.liferay.portal.model.User;
@@ -119,19 +120,16 @@ public class MDRRuleLocalServiceImpl extends MDRRuleLocalServiceBaseImpl {
 		MDRRule rule = mdrRulePersistence.fetchByPrimaryKey(ruleId);
 
 		if (rule != null) {
-			deleteRule(rule);
+			mdrRuleLocalService.deleteRule(rule);
 		}
 	}
 
 	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
 	public void deleteRule(MDRRule rule)
 		throws PortalException, SystemException {
 
 		mdrRulePersistence.remove(rule);
-
-		systemEventLocalService.addSystemEvent(
-			rule.getGroupId(), MDRRule.class.getName(), rule.getRuleId(),
-			rule.getUuid(), SystemEventConstants.TYPE_DELETE);
 
 		MDRRuleGroup ruleGroup = mdrRuleGroupPersistence.fetchByPrimaryKey(
 			rule.getRuleGroupId());
@@ -150,7 +148,7 @@ public class MDRRuleLocalServiceImpl extends MDRRuleLocalServiceBaseImpl {
 		List<MDRRule> rules = mdrRulePersistence.findByRuleGroupId(ruleGroupId);
 
 		for (MDRRule rule : rules) {
-			deleteRule(rule);
+			mdrRuleLocalService.deleteRule(rule);
 		}
 	}
 

portal-impl/src/com/liferay/portlet/polls/service/impl/PollsQuestionLocalServiceImpl.java
@@ -16,6 +16,7 @@ package com.liferay.portlet.polls.service.impl;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.systemevents.SystemEvent;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.ResourceConstants;
@@ -173,10 +174,13 @@ public class PollsQuestionLocalServiceImpl
 		PollsQuestion question = pollsQuestionPersistence.findByPrimaryKey(
 			questionId);
 
-		deleteQuestion(question);
+		pollsQuestionLocalService.deleteQuestion(question);
 	}
 
 	@Override
+	@SystemEvent(
+		childAction = SystemEventConstants.ACTION_SKIP,
+		type = SystemEventConstants.TYPE_DELETE)
 	public void deleteQuestion(PollsQuestion question)
 		throws PortalException, SystemException {
 
@@ -189,13 +193,6 @@ public class PollsQuestionLocalServiceImpl
 		resourceLocalService.deleteResource(
 			question.getCompanyId(), PollsQuestion.class.getName(),
 			ResourceConstants.SCOPE_INDIVIDUAL, question.getQuestionId());
-
-		// System event
-
-		systemEventLocalService.addSystemEvent(
-			question.getGroupId(), PollsQuestion.class.getName(),
-			question.getQuestionId(), question.getUuid(),
-			SystemEventConstants.TYPE_DELETE);
 
 		// Choices
 
@@ -213,7 +210,7 @@ public class PollsQuestionLocalServiceImpl
 		for (PollsQuestion question :
 				pollsQuestionPersistence.findByGroupId(groupId)) {
 
-			deleteQuestion(question);
+			pollsQuestionLocalService.deleteQuestion(question);
 		}
 	}
 

portal-impl/src/com/liferay/portlet/wiki/service/impl/WikiPageLocalServiceImpl.java
@@ -23,6 +23,7 @@ import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.sanitizer.SanitizerUtil;
 import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.systemevents.SystemEvent;
 import com.liferay.portal.kernel.util.CalendarFactoryUtil;
 import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.FileUtil;
@@ -45,6 +46,7 @@ import com.liferay.portal.kernel.workflow.WorkflowHandlerRegistryUtil;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.LayoutConstants;
 import com.liferay.portal.model.ResourceConstants;
+import com.liferay.portal.model.SystemEventConstants;
 import com.liferay.portal.model.User;
 import com.liferay.portal.portletfilerepository.PortletFileRepositoryUtil;
 import com.liferay.portal.service.ServiceContext;
@@ -448,7 +450,7 @@ public class WikiPageLocalServiceImpl extends WikiPageLocalServiceBaseImpl {
 			nodeId, title, true, 0, 1);
 
 		if (!pages.isEmpty()) {
-			deletePage(pages.get(0));
+			wikiPageLocalService.deletePage(pages.get(0));
 		}
 	}
 
@@ -464,6 +466,9 @@ public class WikiPageLocalServiceImpl extends WikiPageLocalServiceBaseImpl {
 	}
 
 	@Override
+	@SystemEvent(
+		childAction = SystemEventConstants.ACTION_SKIP,
+		type = SystemEventConstants.TYPE_DELETE)
 	public void deletePage(WikiPage page)
 		throws PortalException, SystemException {
 

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portal/service/impl/RoleLocalServiceImpl.java
@@ -30,6 +30,7 @@ import com.liferay.portal.kernel.lar.ExportImportThreadLocal;
 import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.IndexerRegistryUtil;
 import com.liferay.portal.kernel.spring.aop.Skip;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.Transactional;
 import com.liferay.portal.kernel.util.CharPool;
@@ -49,6 +50,7 @@ import com.liferay.portal.model.ResourceTypePermission;
 import com.liferay.portal.model.Role;
 import com.liferay.portal.model.RoleConstants;
 import com.liferay.portal.model.Shard;
+import com.liferay.portal.model.SystemEventConstants;
 import com.liferay.portal.model.Team;
 import com.liferay.portal.model.User;
 import com.liferay.portal.security.auth.CompanyThreadLocal;
@@ -416,7 +418,7 @@ public class RoleLocalServiceImpl extends RoleLocalServiceBaseImpl {
 
 		Role role = rolePersistence.findByPrimaryKey(roleId);
 
-		return deleteRole(role);
+		return roleLocalService.deleteRole(role);
 	}
 
 	/**
@@ -429,6 +431,9 @@ public class RoleLocalServiceImpl extends RoleLocalServiceBaseImpl {
 	 * @throws SystemException if a system exception occurred
 	 */
 	@Override
+	@SystemEvent(
+		action = SystemEventConstants.ACTION_SKIP,
+		type = SystemEventConstants.TYPE_DELETE)
 	public Role deleteRole(Role role) throws PortalException, SystemException {
 		if (PortalUtil.isSystemRole(role.getName()) &&
 			!CompanyThreadLocal.isDeleteInProcess()) {

portal-impl/src/com/liferay/portlet/journal/service/impl/JournalFolderLocalServiceImpl.java
@@ -23,11 +23,13 @@ import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.model.ResourceConstants;
+import com.liferay.portal.model.SystemEventConstants;
 import com.liferay.portal.model.User;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.util.PropsValues;
@@ -102,6 +104,9 @@ public class JournalFolderLocalServiceImpl
 
 	@Indexable(type = IndexableType.DELETE)
 	@Override
+	@SystemEvent(
+		action = SystemEventConstants.ACTION_SKIP, send = false,
+		type = SystemEventConstants.TYPE_DELETE)
 	public JournalFolder deleteFolder(JournalFolder folder)
 		throws PortalException, SystemException {
 
@@ -110,6 +115,9 @@ public class JournalFolderLocalServiceImpl
 
 	@Indexable(type = IndexableType.DELETE)
 	@Override
+	@SystemEvent(
+		action = SystemEventConstants.ACTION_SKIP, send = false,
+		type = SystemEventConstants.TYPE_DELETE)
 	public JournalFolder deleteFolder(
 			JournalFolder folder, boolean includeTrashedEntries)
 		throws PortalException, SystemException {
@@ -165,7 +173,7 @@ public class JournalFolderLocalServiceImpl
 		JournalFolder folder = journalFolderPersistence.findByPrimaryKey(
 			folderId);
 
-		return deleteFolder(folder, true);
+		return journalFolderLocalService.deleteFolder(folder, true);
 	}
 
 	@Indexable(type = IndexableType.DELETE)
@@ -177,7 +185,8 @@ public class JournalFolderLocalServiceImpl
 		JournalFolder folder = journalFolderPersistence.findByPrimaryKey(
 			folderId);
 
-		return deleteFolder(folder, includeTrashedEntries);
+		return journalFolderLocalService.deleteFolder(
+			folder, includeTrashedEntries);
 	}
 
 	@Override

portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBBanLocalServiceImpl.java
@@ -18,6 +18,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.Transactional;
 import com.liferay.portal.model.SystemEventConstants;
@@ -83,7 +84,7 @@ public class MBBanLocalServiceImpl extends MBBanLocalServiceBaseImpl {
 	public void deleteBan(long banId) throws PortalException, SystemException {
 		MBBan ban = mbBanPersistence.findByPrimaryKey(banId);
 
-		deleteBan(ban);
+		mbBanLocalService.deleteBan(ban);
 	}
 
 	@Override
@@ -95,18 +96,14 @@ public class MBBanLocalServiceImpl extends MBBanLocalServiceBaseImpl {
 		MBBan ban = mbBanPersistence.fetchByG_B(groupId, banUserId);
 
 		if (ban != null) {
-			deleteBan(ban);
+			mbBanLocalService.deleteBan(ban);
 		}
 	}
 
 	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
 	public void deleteBan(MBBan ban) throws PortalException, SystemException {
 		mbBanPersistence.remove(ban);
-
-		systemEventLocalService.addSystemEvent(
-			0, ban.getGroupId(), MBBan.class.getName(), ban.getBanId(),
-			ban.getUuid(), null, SystemEventConstants.TYPE_DELETE, null);
-
 	}
 
 	@Override

portal-impl/src/com/liferay/portlet/mobiledevicerules/service/impl/MDRRuleGroupInstanceLocalServiceImpl.java
@@ -17,6 +17,7 @@ package com.liferay.portlet.mobiledevicerules.service.impl;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.model.SystemEventConstants;
 import com.liferay.portal.model.User;
@@ -101,7 +102,8 @@ public class MDRRuleGroupInstanceLocalServiceImpl
 			mdrRuleGroupInstancePersistence.findByGroupId(groupId);
 
 		for (MDRRuleGroupInstance ruleGroupInstance : ruleGroupInstances) {
-			deleteRuleGroupInstance(ruleGroupInstance);
+			mdrRuleGroupInstanceLocalService.deleteRuleGroupInstance(
+				ruleGroupInstance);
 		}
 	}
 
@@ -113,25 +115,20 @@ public class MDRRuleGroupInstanceLocalServiceImpl
 			mdrRuleGroupInstancePersistence.fetchByPrimaryKey(
 				ruleGroupInstanceId);
 
-		deleteRuleGroupInstance(ruleGroupInstance);
+		mdrRuleGroupInstanceLocalService.deleteRuleGroupInstance(
+			ruleGroupInstance);
 	}
 
 	@Override
+	@SystemEvent(
+		action = SystemEventConstants.ACTION_SKIP,
+		type = SystemEventConstants.TYPE_DELETE)
 	public void deleteRuleGroupInstance(MDRRuleGroupInstance ruleGroupInstance)
 		throws PortalException, SystemException {
 
 		// Rule group instance
 
 		mdrRuleGroupInstancePersistence.remove(ruleGroupInstance);
-
-		// System event
-
-		systemEventLocalService.addSystemEvent(
-			0, ruleGroupInstance.getGroupId(),
-			MDRRuleGroupInstance.class.getName(),
-			ruleGroupInstance.getRuleGroupInstanceId(),
-			ruleGroupInstance.getUuid(), null, SystemEventConstants.TYPE_DELETE,
-			null);
 
 		// Rule actions
 
@@ -147,7 +144,8 @@ public class MDRRuleGroupInstanceLocalServiceImpl
 			mdrRuleGroupInstancePersistence.findByRuleGroupId(ruleGroupId);
 
 		for (MDRRuleGroupInstance ruleGroupInstance : ruleGroupInstances) {
-			deleteRuleGroupInstance(ruleGroupInstance);
+			mdrRuleGroupInstanceLocalService.deleteRuleGroupInstance(
+				ruleGroupInstance);
 		}
 	}
 

portal-impl/src/com/liferay/portlet/mobiledevicerules/service/impl/MDRRuleLocalServiceImpl.java
@@ -16,6 +16,7 @@ package com.liferay.portlet.mobiledevicerules.service.impl;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.model.SystemEventConstants;
 import com.liferay.portal.model.User;
@@ -119,19 +120,16 @@ public class MDRRuleLocalServiceImpl extends MDRRuleLocalServiceBaseImpl {
 		MDRRule rule = mdrRulePersistence.fetchByPrimaryKey(ruleId);
 
 		if (rule != null) {
-			deleteRule(rule);
+			mdrRuleLocalService.deleteRule(rule);
 		}
 	}
 
 	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
 	public void deleteRule(MDRRule rule)
 		throws PortalException, SystemException {
 
 		mdrRulePersistence.remove(rule);
-
-		systemEventLocalService.addSystemEvent(
-			0, rule.getGroupId(), MDRRule.class.getName(), rule.getRuleId(),
-			rule.getUuid(), null, SystemEventConstants.TYPE_DELETE, null);
 
 		MDRRuleGroup ruleGroup = mdrRuleGroupPersistence.fetchByPrimaryKey(
 			rule.getRuleGroupId());
@@ -150,7 +148,7 @@ public class MDRRuleLocalServiceImpl extends MDRRuleLocalServiceBaseImpl {
 		List<MDRRule> rules = mdrRulePersistence.findByRuleGroupId(ruleGroupId);
 
 		for (MDRRule rule : rules) {
-			deleteRule(rule);
+			mdrRuleLocalService.deleteRule(rule);
 		}
 	}
 

portal-impl/src/com/liferay/portlet/polls/service/impl/PollsQuestionLocalServiceImpl.java
@@ -16,6 +16,7 @@ package com.liferay.portlet.polls.service.impl;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.ResourceConstants;
@@ -173,10 +174,13 @@ public class PollsQuestionLocalServiceImpl
 		PollsQuestion question = pollsQuestionPersistence.findByPrimaryKey(
 			questionId);
 
-		deleteQuestion(question);
+		pollsQuestionLocalService.deleteQuestion(question);
 	}
 
 	@Override
+	@SystemEvent(
+		action = SystemEventConstants.ACTION_SKIP,
+		type = SystemEventConstants.TYPE_DELETE)
 	public void deleteQuestion(PollsQuestion question)
 		throws PortalException, SystemException {
 
@@ -189,13 +193,6 @@ public class PollsQuestionLocalServiceImpl
 		resourceLocalService.deleteResource(
 			question.getCompanyId(), PollsQuestion.class.getName(),
 			ResourceConstants.SCOPE_INDIVIDUAL, question.getQuestionId());
-
-		// System event
-
-		systemEventLocalService.addSystemEvent(
-			0, question.getGroupId(), PollsQuestion.class.getName(),
-			question.getQuestionId(), question.getUuid(), null,
-			SystemEventConstants.TYPE_DELETE, null);
 
 		// Choices
 
@@ -213,7 +210,7 @@ public class PollsQuestionLocalServiceImpl
 		for (PollsQuestion question :
 				pollsQuestionPersistence.findByGroupId(groupId)) {
 
-			deleteQuestion(question);
+			pollsQuestionLocalService.deleteQuestion(question);
 		}
 	}
 

portal-impl/src/com/liferay/portlet/wiki/service/impl/WikiPageLocalServiceImpl.java
@@ -23,6 +23,7 @@ import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.sanitizer.SanitizerUtil;
 import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.util.CalendarFactoryUtil;
 import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.FileUtil;
@@ -44,6 +45,7 @@ import com.liferay.portal.kernel.workflow.WorkflowHandlerRegistryUtil;
 import com.liferay.portal.kernel.workflow.WorkflowThreadLocal;
 import com.liferay.portal.model.LayoutConstants;
 import com.liferay.portal.model.ResourceConstants;
+import com.liferay.portal.model.SystemEventConstants;
 import com.liferay.portal.model.User;
 import com.liferay.portal.portletfilerepository.PortletFileRepositoryUtil;
 import com.liferay.portal.service.ServiceContext;
@@ -442,7 +444,7 @@ public class WikiPageLocalServiceImpl extends WikiPageLocalServiceBaseImpl {
 			nodeId, title, true, 0, 1);
 
 		if (!pages.isEmpty()) {
-			deletePage(pages.get(0));
+			wikiPageLocalService.deletePage(pages.get(0));
 		}
 	}
 
@@ -458,6 +460,9 @@ public class WikiPageLocalServiceImpl extends WikiPageLocalServiceBaseImpl {
 	}
 
 	@Override
+	@SystemEvent(
+		action = SystemEventConstants.ACTION_SKIP,
+		type = SystemEventConstants.TYPE_DELETE)
 	public void deletePage(WikiPage page)
 		throws PortalException, SystemException {
 