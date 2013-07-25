d6cc47587aafa48943a42fcbb19d5f91545849cd
Brian Chan <brian.chan@liferay.com>
2013-07-24 20:28:43 -0700

    LPS-30036 Sort

--- portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
@@ -420,11 +420,11 @@ public class VerifyProperties extends VerifyProcess {
 				"configuration.jsp"
 		},
 		new String[] {
-			"field.editable.com.liferay.portal.model.User.screenName",
+			"field.editable.com.liferay.portal.model.User.emailAddress",
 			"field.editable.user.types"
 		},
 		new String[] {
-			"field.editable.com.liferay.portal.model.User.emailAddress",
+			"field.editable.com.liferay.portal.model.User.screenName",
 			"field.editable.user.types"
 		},
 		new String[] {
