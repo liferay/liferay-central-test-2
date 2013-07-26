bea607af4add733caf4f10dd1afe6f8041e9a983
Daniel Sanz <daniel.sanz@liferay.com>
2013-07-26 09:06:20 +0200

    LPS-38201

--- portal-impl/src/com/liferay/portlet/dynamicdatamapping/util/DDMIndexerImpl.java
+++ portal-impl/src/com/liferay/portlet/dynamicdatamapping/util/DDMIndexerImpl.java
@@ -71,16 +71,28 @@ public class DDMIndexerImpl implements DDMIndexer {
 						document.addDate(name, (Date[])value);
 					}
 					else if (value instanceof Double) {
-						document.addKeyword(name, (Double)value);
+						document.addNumber(name, (Double)value);
 					}
 					else if (value instanceof Double[]) {
-						document.addKeyword(name, (Double[])value);
+						document.addNumber(name, (Double[])value);
 					}
 					else if (value instanceof Integer) {
-						document.addKeyword(name, (Integer)value);
+						document.addNumber(name, (Integer)value);
 					}
 					else if (value instanceof Integer[]) {
-						document.addKeyword(name, (Integer[])value);
+						document.addNumber(name, (Integer[])value);
+					}
+					else if (value instanceof Long) {
+						document.addNumber(name, (Long)value);
+					}
+					else if (value instanceof Long[]) {
+						document.addNumber(name, (Long[])value);
+					}
+					else if (value instanceof Float) {
+						document.addNumber(name, (Float)value);
+					}
+					else if (value instanceof Float[]) {
+						document.addNumber(name, (Float[])value);
 					}
 					else if (value instanceof Object[]) {
 						String[] valuesString = ArrayUtil.toStringArray(
