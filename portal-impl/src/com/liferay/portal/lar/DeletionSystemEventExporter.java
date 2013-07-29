0679eb4f29ccc13a5fe38a4d0d360512bc7ee8f6
Brian Chan <brian.chan@liferay.com>
2013-07-29 13:08:25 -0700

    LPS-36216 Sort

--- portal-impl/src/com/liferay/portal/lar/DeletionSystemEventExporter.java
+++ portal-impl/src/com/liferay/portal/lar/DeletionSystemEventExporter.java
@@ -165,6 +165,8 @@ public class DeletionSystemEventExporter {
 			"class-name",
 			PortalUtil.getClassName(systemEvent.getClassNameId()));
 		deletionSystemEventElement.addAttribute(
+			"extra-data", systemEvent.getExtraData());
+		deletionSystemEventElement.addAttribute(
 			"group-id", String.valueOf(systemEvent.getGroupId()));
 
 		if (systemEvent.getReferrerClassNameId() > 0) {
@@ -175,9 +177,6 @@ public class DeletionSystemEventExporter {
 
 		deletionSystemEventElement.addAttribute(
 			"uuid", systemEvent.getClassUuid());
-
-		deletionSystemEventElement.addAttribute(
-			"extra-data", systemEvent.getExtraData());
 
 		ManifestSummary manifestSummary =
 			portletDataContext.getManifestSummary();
