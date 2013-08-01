69b26b6ddef84d77cb65ebe0c25260cf3af596f9
shuyangzhou <shuyang.zhou@liferay.com>
2013-08-01 15:29:08 -0700

    LPS-38220 Fix test.

--- portal-impl/test/integration/com/liferay/portlet/service/persistence/GroupFinderTest.java
+++ portal-impl/test/integration/com/liferay/portlet/service/persistence/GroupFinderTest.java
@@ -158,9 +158,9 @@ public class GroupFinderTest {
 
 		return GroupFinderUtil.findByC_C_PG_N_D(
 			TestPropsValues.getCompanyId(), classNameIds,
-			GroupConstants.ANY_PARENT_GROUP_ID, StringPool.NOT_EQUAL,
-			new String[] {null}, new String[] {null}, new String[] {null},
-			groupParams, true, QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
+			GroupConstants.ANY_PARENT_GROUP_ID, new String[] {null},
+			new String[] {null}, new String[] {null}, groupParams, true,
+			QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
 	}
 
 	private static ResourceAction _arbitraryResourceAction;
