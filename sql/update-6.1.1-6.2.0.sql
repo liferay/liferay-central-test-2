a70135e6cef4268d56cc5dbe99a89ea49c69022e
Jorge Ferrer <jorge.ferrer@liferay.com>
2013-08-02 13:48:45 +0200

    LPS-38450 Deprecate Software Catalog

--- sql/update-6.1.1-6.2.0.sql
+++ sql/update-6.1.1-6.2.0.sql
@@ -503,6 +503,7 @@ alter table PollsVote add uuid_ VARCHAR(75) null;
 alter table PollsVote add groupId LONG;
 
 update Portlet set active_ = FALSE where portletId = "62";
+update Portlet set active_ = FALSE where portletId = "98";
 update Portlet set active_ = FALSE where portletId = "173";
 
 alter table RepositoryEntry add companyId LONG;
