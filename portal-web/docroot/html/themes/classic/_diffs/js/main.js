c6a0dc68fe25b647c19051d2adae83548d3f6267
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2013-08-05 15:49:43 -0700

    LPS-38297 - SF: Rather than recreating the FormValidator only when in exclusive, making it so that the modal moves the one on the page if it exists, and loads via IO if it isn't

--- portal-web/docroot/html/themes/classic/_diffs/js/main.js
+++ portal-web/docroot/html/themes/classic/_diffs/js/main.js
@@ -2,16 +2,71 @@ AUI().ready(
 	'aui-io-request', 'aui-modal', 'aui-parse-content', 'liferay-hudcrumbs', 'liferay-navigation-interaction', 'liferay-portlet-url',
 	function(A) {
 		var navigation = A.one('#navigation');
-		var signIn = A.one('li.sign-in a');
-		var siteBreadcrumbs = A.one('#breadcrumbs');
 
 		if (navigation) {
 			navigation.plug(Liferay.NavigationInteraction);
 		}
 
+		var siteBreadcrumbs = A.one('#breadcrumbs');
+
 		if (siteBreadcrumbs) {
 			siteBreadcrumbs.plug(A.Hudcrumbs);
 		}
+
+		var signIn = A.one('li.sign-in a');
+
+		var signInPortletBody;
+
+		var signInPortlet = A.one('#p_p_id_58_');
+
+		if (signInPortlet) {
+			signInPortletBody = signInPortlet.one('.portlet-body');
+		}
+
+		var modal;
+
+		var getModal = function() {
+			if (!modal) {
+				modal = new A.Modal(
+					{
+						after: {
+							visibleChange: function(event) {
+								if (!event.newVal && signInPortletBody) {
+									var oldParent = signInPortletBody.getData('oldParent');
+
+									if (oldParent) {
+										oldParent.append(signInPortletBody);
+									}
+								}
+							}
+						},
+						centered: true,
+						constrain: true,
+						headerContent: '<h3>' + Liferay.Language.get('sign-in') + '</h3>',
+						modal: true,
+						zIndex: 400
+					}
+				).render();
+
+				var bodyNode = modal.bodyNode;
+
+				bodyNode.plug(A.Plugin.ParseContent);
+			}
+
+			return modal;
+		};
+
+		var setModalContent = function(content) {
+			var modal = getModal();
+
+			modal.show();
+
+			var bodyNode = modal.bodyNode;
+
+			bodyNode.setContent(content);
+
+			modal.align();
+		};
 
 		if (signIn) {
 			signIn.on(
@@ -19,52 +74,41 @@ AUI().ready(
 				function(event) {
 					event.preventDefault();
 
-					var signInURL = event.currentTarget.attr('href');
-
-					var modalSignInURL = Liferay.PortletURL.createURL(signInURL);
-
-					modalSignInURL.setWindowState('exclusive');
-
 					var redirectPage = function() {
 						A.config.win.location.href = signInURL;
 					};
 
-					A.io.request(
-						modalSignInURL.toString(),
-						{
-							on: {
-								failure: redirectPage,
-								success: function(event, id, obj) {
-									var responseData = this.get('responseData');
+					if (signInPortletBody) {
+						signInPortletBody.setData('oldParent', signInPortletBody.ancestor());
 
-									var modal;
+						setModalContent(signInPortletBody);
+					}
+					else {
+						var signInURL = event.currentTarget.attr('href');
 
-									if (responseData) {
-										modal = new A.Modal(
-											{
-												centered: true,
-												constrain: true,
-												headerContent: '<h3>' + Liferay.Language.get('sign-in') + '</h3>',
-												modal: true,
-												zIndex: 400
-											}
-										).render();
+						var modalSignInURL = Liferay.PortletURL.createURL(signInURL);
 
-										var bodyNode = modal.get('contentBox').one('.modal-body');
+						modalSignInURL.setWindowState('exclusive');
 
-										bodyNode.plug(A.Plugin.ParseContent);
+						A.io.request(
+							modalSignInURL.toString(),
+							{
+								on: {
+									failure: redirectPage,
+									success: function(event, id, obj) {
+										var responseData = this.get('responseData');
 
-										bodyNode.setContent(responseData);
-
-										modal.align();
-									}
-									else {
-										redirectPage();
+										if (responseData) {
+											setModalContent(responseData);
+										}
+										else {
+											redirectPage();
+										}
 									}
 								}
 							}
-						}
-					);
+						);
+					}
 				}
 			);
 		}
