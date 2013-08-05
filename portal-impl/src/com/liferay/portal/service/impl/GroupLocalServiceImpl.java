9e7f74504c078219d702f7ae0a2676c10a6e8b69
Brian Chan <brian.chan@liferay.com>
2013-08-05 16:27:22 -0700

    LPS-38220 Fix false vs true

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -4104,7 +4104,7 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 
 	protected boolean isUseComplexSQL(long[] classNameIds) {
 		if ((classNameIds == null) || (classNameIds.length == 0)) {
-			return false;
+			return true;
 		}
 
 		if (_complexSQLClassNameIds == null) {
@@ -4126,11 +4126,11 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 
 		for (long classNameId : classNameIds) {
 			if (ArrayUtil.contains(_complexSQLClassNameIds, classNameId)) {
-				return false;
+				return true;
 			}
 		}
 
-		return true;
+		return false;
 	}
 
 	protected boolean matches(String s, String[] keywords) {
