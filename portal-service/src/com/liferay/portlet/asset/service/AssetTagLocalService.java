juliocamarero/liferay-portal

LPS-33802 Autogenerated

portal-service/src/com/liferay/portlet/asset/service/AssetTagLocalService.java
@@ -382,6 +382,10 @@ public interface AssetTagLocalService extends BaseLocalService,
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException;
 
+	public void deleteTags(long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException;
+
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public java.util.List<com.liferay.portlet.asset.model.AssetTag> getEntryTags(
 		long entryId)

portal-service/src/com/liferay/portlet/asset/service/AssetTagLocalServiceUtil.java
@@ -456,6 +456,12 @@ public class AssetTagLocalServiceUtil {
 		getService().deleteTag(tagId);
 	}
 
+	public static void deleteTags(long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		getService().deleteTags(groupId);
+	}
+
 	public static java.util.List<com.liferay.portlet.asset.model.AssetTag> getEntryTags(
 		long entryId)
 		throws com.liferay.portal.kernel.exception.SystemException {

portal-service/src/com/liferay/portlet/asset/service/AssetTagLocalServiceWrapper.java
@@ -485,6 +485,13 @@ public class AssetTagLocalServiceWrapper implements AssetTagLocalService,
 	}
 
 	@Override
+	public void deleteTags(long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		_assetTagLocalService.deleteTags(groupId);
+	}
+
+	@Override
 	public java.util.List<com.liferay.portlet.asset.model.AssetTag> getEntryTags(
 		long entryId)
 		throws com.liferay.portal.kernel.exception.SystemException {

liferay/liferay-central-test-2

portal-service/src/com/liferay/portlet/asset/service/AssetTagLocalService.java
@@ -382,6 +382,10 @@ public interface AssetTagLocalService extends BaseLocalService,
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException;
 
+	public void deleteGroupTags(long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException;
+
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public java.util.List<com.liferay.portlet.asset.model.AssetTag> getEntryTags(
 		long entryId)

portal-service/src/com/liferay/portlet/asset/service/AssetTagLocalServiceUtil.java
@@ -456,6 +456,12 @@ public class AssetTagLocalServiceUtil {
 		getService().deleteTag(tagId);
 	}
 
+	public static void deleteGroupTags(long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		getService().deleteGroupTags(groupId);
+	}
+
 	public static java.util.List<com.liferay.portlet.asset.model.AssetTag> getEntryTags(
 		long entryId)
 		throws com.liferay.portal.kernel.exception.SystemException {

portal-service/src/com/liferay/portlet/asset/service/AssetTagLocalServiceWrapper.java
@@ -485,6 +485,13 @@ public class AssetTagLocalServiceWrapper implements AssetTagLocalService,
 	}
 
 	@Override
+	public void deleteGroupTags(long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		_assetTagLocalService.deleteGroupTags(groupId);
+	}
+
+	@Override
 	public java.util.List<com.liferay.portlet.asset.model.AssetTag> getEntryTags(
 		long entryId)
 		throws com.liferay.portal.kernel.exception.SystemException {