42d8a1eb9b6406ad81c5909459bbbb1630731187
Manuel de la Peña <manuel.delapenya@liferay.com>
2013-08-07 09:30:27 +0200

    LPS-38589 Revert as NPE in SeleniumBuilderContextTest (so it hasn't been tested)

--- portal-impl/test/integration/com/liferay/portal/tools/seleniumbuilder/SeleniumBuilderContextTest.java
+++ portal-impl/test/integration/com/liferay/portal/tools/seleniumbuilder/SeleniumBuilderContextTest.java
@@ -611,11 +611,11 @@ public class SeleniumBuilderContextTest {
 		}
 	}
 
-	private static final String _BASE_DIR =
-		"./portal-impl/test/integration/com/liferay/portal/tools" +
-			"/seleniumbuilder/dependencies/context";
+	private static final String _BASE_DIR = "./portal-web/test/functional/";
 
-	private static final String _DIR_NAME = "/..";
+	private static final String _DIR_NAME =
+		"/../../../portal-impl/test/integration/com/liferay/portal/tools/" +
+			"seleniumbuilder/dependencies";
 
 	private static Log _log = LogFactoryUtil.getLog(
 		SeleniumBuilderContextTest.class);
