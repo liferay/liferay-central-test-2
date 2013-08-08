ccbefa0b4ae33962281ec34fa159c0eca6cc3237
Kwang <kwang.lee@liferay.com>
2013-08-08 15:10:01 -0700

    LPS-38684

--- portal-web/test/functional/com/liferay/portalweb/selenium/AssertTextTestCase.java
+++ portal-web/test/functional/com/liferay/portalweb/selenium/AssertTextTestCase.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.portalweb.selenium;
 
 import com.liferay.portalweb.portal.BaseTestCase;
