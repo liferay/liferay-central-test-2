e4bbae6acee37a8d4b1c71737f62df77c9e3c30b
Brian Chan <brian.chan@liferay.com>
2013-08-08 13:33:29 -0700

    LPS-38689 Consolidate to use LocaleUtil.*

--- portal-service/src/com/liferay/portal/kernel/util/LocaleUtil.java
+++ portal-service/src/com/liferay/portal/kernel/util/LocaleUtil.java
@@ -34,11 +34,55 @@ public class LocaleUtil {
 
 	public static final Locale BRAZIL = new Locale("pt", "BR");
 
+	public static final Locale CANADA = Locale.CANADA;
+
+	public static final Locale CANADA_FRENCH = Locale.CANADA_FRENCH;
+
+	public static final Locale CHINA = Locale.CHINA;
+
+	public static final Locale CHINESE = Locale.CHINESE;
+
+	public static final Locale ENGLISH = Locale.ENGLISH;
+
+	public static final Locale FRANCE = Locale.FRANCE;
+
+	public static final Locale FRENCH = Locale.FRENCH;
+
+	public static final Locale GERMAN = Locale.GERMAN;
+
+	public static final Locale GERMANY = Locale.GERMANY;
+
 	public static final Locale HUNGARY = new Locale("hu", "HU");
+
+	public static final Locale ITALIAN = Locale.ITALIAN;
+
+	public static final Locale ITALY = Locale.ITALY;
+
+	public static final Locale JAPAN = Locale.JAPAN;
+
+	public static final Locale JAPANESE = Locale.JAPANESE;
+
+	public static final Locale KOREA = Locale.KOREA;
+
+	public static final Locale KOREAN = Locale.KOREAN;
 
 	public static final Locale PORTUGAL = new Locale("pt", "PT");
 
+	public static final Locale PRC = Locale.PRC;
+
+	public static final Locale ROOT = Locale.ROOT;
+
+	public static final Locale SIMPLIFIED_CHINESE = Locale.SIMPLIFIED_CHINESE;
+
 	public static final Locale SPAIN = new Locale("es", "ES");
+
+	public static final Locale TAIWAN = Locale.TAIWAN;
+
+	public static final Locale TRADITIONAL_CHINESE = Locale.TRADITIONAL_CHINESE;
+
+	public static final Locale UK = Locale.UK;
+
+	public static final Locale US = Locale.US;
 
 	public static boolean equals(Locale locale1, Locale locale2) {
 		return getInstance()._equals(locale1, locale2);
