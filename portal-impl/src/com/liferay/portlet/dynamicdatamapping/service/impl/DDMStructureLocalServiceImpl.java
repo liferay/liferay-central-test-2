35fc503744609d2410c3b90f400f54c7c3772ff8
Brian Chan <brian.chan@liferay.com>
2013-08-09 09:40:52 -0700

    Revert "LPS-38435 Javadocs"
    This reverts commit eafe442c7a7e3df9ba0fcdafaad78fc193f18e7f.

--- portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/impl/DDMStructureLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/impl/DDMStructureLocalServiceImpl.java
@@ -1024,30 +1024,6 @@ public class DDMStructureLocalServiceImpl
 		return ddmStructurePersistence.findByG_C(groupIds, classNameId);
 	}
 
-	/**
-	 * Returns a range of all the structures matching the class name ID and
-	 * belonging to the groups.
-	 *
-	 * <p>
-	 * Useful when paginating results. Returns a maximum of <code>end -
-	 * start</code> instances. <code>start</code> and <code>end</code> are not
-	 * primary keys, they are indexes in the result set. Thus, <code>0</code>
-	 * refers to the first result in the set. Setting both <code>start</code>
-	 * and <code>end</code> to {@link
-	 * com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS} will return the full
-	 * result set.
-	 * </p>
-	 *
-	 * @param  groupIds the primary keys of the groups
-	 * @param  classNameId the primary key of the class name for the structure's
-	 *         related model
-	 * @param  start the lower bound of the range of structures to return
-	 * @param  end the upper bound of the range of structures to return (not
-	 *         inclusive)
-	 * @return the structures matching the class name ID and belonging to the
-	 *         groups
-	 * @throws SystemException if a system exception occurred
-	 */
 	@Override
 	public List<DDMStructure> getStructures(
 			long[] groupIds, long classNameId, int start, int end)
@@ -1085,16 +1061,6 @@ public class DDMStructureLocalServiceImpl
 		return ddmStructurePersistence.countByG_C(groupId, classNameId);
 	}
 
-	/**
-	 * Returns the number of structures matching the class name ID and belonging
-	 * to the groups.
-	 *
-	 * @param  groupIds the primary keys of the groups
-	 * @param  classNameId the primary key of the class name for the structure's
-	 *         related model
-	 * @return the number of matching structures
-	 * @throws SystemException if a system exception occurred
-	 */
 	@Override
 	public int getStructuresCount(long[] groupIds, long classNameId)
 		throws SystemException {
