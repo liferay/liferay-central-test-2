cc4eb806af0994728ee6d5796449127b27e95395
Manuel de la Peña <manuel.delapenya@liferay.com>
2013-08-13 12:53:02 +0200

    LPS-36313 Use proper locale when creating a JournalArticle

--- portal-impl/test/integration/com/liferay/portlet/journal/service/JournalArticleScheduledTest.java
+++ portal-impl/test/integration/com/liferay/portlet/journal/service/JournalArticleScheduledTest.java
@@ -87,11 +87,12 @@ public class JournalArticleScheduledTest {
 
 		Map<Locale, String> titleMap = new HashMap<Locale, String>();
 
-		titleMap.put(Locale.getDefault(), ServiceTestUtil.randomString());
+		titleMap.put(LocaleUtil.getDefault(), ServiceTestUtil.randomString());
 
 		Map<Locale, String> descriptionMap = new HashMap<Locale, String>();
 
-		descriptionMap.put(Locale.getDefault(), ServiceTestUtil.randomString());
+		descriptionMap.put(
+			LocaleUtil.getDefault(), ServiceTestUtil.randomString());
 
 		Calendar displayDateCalendar = new GregorianCalendar();
 
