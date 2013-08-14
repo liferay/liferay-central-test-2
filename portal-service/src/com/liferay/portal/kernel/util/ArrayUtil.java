4af603541fae69ae048872138923dee7bfe07f23
Roberto Díaz <roberto.diaz@liferay.com>
2013-08-14 09:18:35 +0200

    LPS-35645 Create new Arrayuil method based in Apache Commons Collections isEmpty method to Null-safe check if the specified collection is empty. (see http://commons.apache.org/proper/commons-collections//javadocs/api-3.2.1/org/apache/commons/collections/CollectionUtils.html#isEmpty(java.util.Collection))

--- portal-service/src/com/liferay/portal/kernel/util/ArrayUtil.java
+++ portal-service/src/com/liferay/portal/kernel/util/ArrayUtil.java
@@ -1038,6 +1038,42 @@ public class ArrayUtil {
 		}
 	}
 
+	public static boolean isEmpty(boolean[] array) {
+		return ((array == null) || (array.length == 0));
+	}
+
+	public static boolean isEmpty(byte[] array) {
+		return ((array == null) || (array.length == 0));
+	}
+
+	public static boolean isEmpty(char[] array) {
+		return ((array == null) || (array.length == 0));
+	}
+
+	public static boolean isEmpty(double[] array) {
+		return ((array == null) || (array.length == 0));
+	}
+
+	public static boolean isEmpty(float[] array) {
+		return ((array == null) || (array.length == 0));
+	}
+
+	public static boolean isEmpty(int[] array) {
+		return ((array == null) || (array.length == 0));
+	}
+
+	public static boolean isEmpty(long[] array) {
+		return ((array == null) || (array.length == 0));
+	}
+
+	public static boolean isEmpty(Object[] array) {
+		return ((array == null) || (array.length == 0));
+	}
+
+	public static boolean isEmpty(short[] array) {
+		return ((array == null) || (array.length == 0));
+	}
+
 	public static boolean[] remove(boolean[] array, boolean value) {
 		List<Boolean> list = new ArrayList<Boolean>();
 
