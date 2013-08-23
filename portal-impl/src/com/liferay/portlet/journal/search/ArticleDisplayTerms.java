d9eacb02638910a90766de8d23768475a4911338
Julio Camarero <julio.camarero@liferay.com>
2013-08-23 12:06:04 +0200

    LPS-39268 from the scope the structure belongs to, you can see who is using that structure (or template)

--- portal-impl/src/com/liferay/portlet/journal/search/ArticleDisplayTerms.java
+++ portal-impl/src/com/liferay/portlet/journal/search/ArticleDisplayTerms.java
@@ -15,11 +15,18 @@
 package com.liferay.portlet.journal.search;
 
 import com.liferay.portal.kernel.dao.search.DisplayTerms;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.WebKeys;
+import com.liferay.portlet.dynamicdatamapping.model.DDMStructure;
+import com.liferay.portlet.dynamicdatamapping.model.DDMTemplate;
+import com.liferay.portlet.dynamicdatamapping.service.DDMStructureLocalServiceUtil;
+import com.liferay.portlet.dynamicdatamapping.service.DDMTemplateLocalServiceUtil;
+import com.liferay.portlet.journal.model.JournalArticle;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -183,14 +190,38 @@ public class ArticleDisplayTerms extends DisplayTerms {
 				(ThemeDisplay)portletRequest.getAttribute(
 					WebKeys.THEME_DISPLAY);
 
-			if ((themeDisplay.getScopeGroupId() ==
-					themeDisplay.getCompanyGroupId()) &&
-				((Validator.isNotNull(structureId) &&
-					!structureId.equals("0")) ||
-				(Validator.isNotNull(templateId) &&
-					!templateId.equals("0")))) {
+			if (Validator.isNotNull(structureId) && !structureId.equals("0")) {
+				DDMStructure ddmStructure = null;
 
-				return 0;
+				try {
+					ddmStructure = DDMStructureLocalServiceUtil.fetchStructure(
+						themeDisplay.getSiteGroupId(),
+						PortalUtil.getClassNameId(JournalArticle.class),
+						structureId);
+				}
+				catch (SystemException se) {
+				}
+
+				if (ddmStructure != null) {
+					return 0;
+				}
+			}
+
+			if (Validator.isNotNull(templateId) && !templateId.equals("0")) {
+				DDMTemplate ddmTemplate = null;
+
+				try {
+					ddmTemplate = DDMTemplateLocalServiceUtil.fetchTemplate(
+						themeDisplay.getSiteGroupId(),
+						PortalUtil.getClassNameId(JournalArticle.class),
+						templateId);
+				}
+				catch (SystemException se) {
+				}
+
+				if (ddmTemplate != null) {
+					return 0;
+				}
 			}
 
 			groupId = themeDisplay.getScopeGroupId();
