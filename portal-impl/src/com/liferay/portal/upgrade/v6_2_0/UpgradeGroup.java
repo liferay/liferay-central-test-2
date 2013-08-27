aebd91c0520d55c1e43b5366422a7370486ae29d
Shinn Lok <shinn.lok@liferay.com>
2013-08-27 12:05:10 -0700

    LPS-36480 Upgrade for change made in LPS-35062

--- portal-impl/src/com/liferay/portal/upgrade/v6_2_0/UpgradeGroup.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_2_0/UpgradeGroup.java
@@ -15,7 +15,9 @@
 package com.liferay.portal.upgrade.v6_2_0;
 
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.model.Company;
 import com.liferay.portal.upgrade.v6_2_0.util.GroupTable;
+import com.liferay.portal.util.PortalUtil;
 
 import java.sql.SQLException;
 
@@ -35,6 +37,11 @@ public class UpgradeGroup extends UpgradeProcess {
 				GroupTable.TABLE_NAME, GroupTable.TABLE_COLUMNS,
 				GroupTable.TABLE_SQL_CREATE, GroupTable.TABLE_SQL_ADD_INDEXES);
 		}
+
+		long classNameId = PortalUtil.getClassNameId(Company.class.getName());
+
+		runSQL(
+			"update Group_ set site = TRUE where classNameId = " + classNameId);
 	}
 
 }
