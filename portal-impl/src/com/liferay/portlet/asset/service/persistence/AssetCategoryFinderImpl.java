9c34a4004ab59888635760c4f97f26bd97ff6e58
Brian Chan <brian.chan@liferay.com>
2013-08-27 10:01:23 -0700

    LPS-38619 Add missing @Override

--- portal-impl/src/com/liferay/portlet/asset/service/persistence/AssetCategoryFinderImpl.java
+++ portal-impl/src/com/liferay/portlet/asset/service/persistence/AssetCategoryFinderImpl.java
@@ -121,6 +121,7 @@ public class AssetCategoryFinderImpl
 		}
 	}
 
+	@Override
 	public int countByG_N_V(long groupId, String name, long vocabularyId)
 		throws SystemException {
 
@@ -173,12 +174,14 @@ public class AssetCategoryFinderImpl
 		}
 	}
 
+	@Override
 	public int filterCountByG_N_V(long groupId, String name, long vocabularyId)
 		throws SystemException {
 
 		return doCountByG_N_V(groupId, name, vocabularyId, true);
 	}
 
+	@Override
 	public List<AssetCategory> filterFindByG_N_V(
 			long groupId, String name, long vocabularyId, int start, int end,
 			OrderByComparator obc)
@@ -291,6 +294,7 @@ public class AssetCategoryFinderImpl
 		throw new NoSuchCategoryException(sb.toString());
 	}
 
+	@Override
 	public List<AssetCategory> findByG_N_V(
 			long groupId, String name, long vocabularyId, int start, int end,
 			OrderByComparator obc)
