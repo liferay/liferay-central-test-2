8da83456396c4c07de8d36eaf0c30b95cbd6661b
Chas Austin <charles.austin@liferay.com>
2013-08-28 10:58:19 -0700

    Prepare 6.2.0 CE B3 release

--- portal-service/src/com/liferay/portal/kernel/util/ReleaseInfo.java
+++ portal-service/src/com/liferay/portal/kernel/util/ReleaseInfo.java
@@ -140,7 +140,7 @@ public class ReleaseInfo {
 
 	private static final String _CODE_NAME = "Newton";
 
-	private static final String _DATE = "September 2, 2013";
+	private static final String _DATE = "September 6, 2013";
 
 	private static final String _NAME = "Liferay Portal Community Edition";
 
@@ -156,7 +156,7 @@ public class ReleaseInfo {
 
 	private static final String _VERSION = "6.2.0";
 
-	private static final String _VERSION_DISPLAY_NAME = "6.2.0 CE B2";
+	private static final String _VERSION_DISPLAY_NAME = "6.2.0 CE B3";
 
 	private static String _releaseInfo;
 	private static String _serverInfo;
