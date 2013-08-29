juliocamarero/liferay-portal

LPS-38525 Autogenerated

portal-service/src/com/liferay/portal/model/Group.java
@@ -123,6 +123,8 @@ public interface Group extends GroupModel, PersistedModel {
 
 	public boolean isCompany();
 
+	public boolean isCompanyStaging();
+
 	public boolean isControlPanel();
 
 	public boolean isGuest();

portal-service/src/com/liferay/portal/model/GroupWrapper.java
@@ -957,6 +957,11 @@ public class GroupWrapper implements Group, ModelWrapper<Group> {
 	}
 
 	@Override
+	public boolean isCompanyStaging() {
+		return _group.isCompanyStaging();
+	}
+
+	@Override
 	public boolean isControlPanel() {
 		return _group.isControlPanel();
 	}

liferay/liferay-central-test-2

portal-service/src/com/liferay/portal/model/Group.java
@@ -123,6 +123,8 @@ public interface Group extends GroupModel, PersistedModel {
 
 	public boolean isCompany();
 
+	public boolean isCompanyStagingGroup();
+
 	public boolean isControlPanel();
 
 	public boolean isGuest();

portal-service/src/com/liferay/portal/model/GroupWrapper.java
@@ -957,6 +957,11 @@ public class GroupWrapper implements Group, ModelWrapper<Group> {
 	}
 
 	@Override
+	public boolean isCompanyStagingGroup() {
+		return _group.isCompanyStagingGroup();
+	}
+
+	@Override
 	public boolean isControlPanel() {
 		return _group.isControlPanel();
 	}