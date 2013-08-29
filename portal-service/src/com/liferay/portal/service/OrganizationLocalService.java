brianchandotcom/liferay-portal

LPS-39448 Regenerate

portal-service/src/com/liferay/portal/service/OrganizationLocalService.java
@@ -649,6 +649,10 @@ public interface OrganizationLocalService extends BaseLocalService,
 		long companyId, java.lang.String name)
 		throws com.liferay.portal.kernel.exception.SystemException;
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portal.model.Organization> getNoAssetEntries()
+		throws com.liferay.portal.kernel.exception.SystemException;
+
 	/**
 	* Returns the organization with the name.
 	*

portal-service/src/com/liferay/portal/service/OrganizationLocalServiceUtil.java
@@ -757,6 +757,11 @@ public class OrganizationLocalServiceUtil {
 		return getService().fetchOrganization(companyId, name);
 	}
 
+	public static java.util.List<com.liferay.portal.model.Organization> getNoAssetEntries()
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getService().getNoAssetEntries();
+	}
+
 	/**
 	* Returns the organization with the name.
 	*

portal-service/src/com/liferay/portal/service/OrganizationLocalServiceWrapper.java
@@ -810,6 +810,12 @@ public class OrganizationLocalServiceWrapper implements OrganizationLocalService
 		return _organizationLocalService.fetchOrganization(companyId, name);
 	}
 
+	@Override
+	public java.util.List<com.liferay.portal.model.Organization> getNoAssetEntries()
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return _organizationLocalService.getNoAssetEntries();
+	}
+
 	/**
 	* Returns the organization with the name.
 	*

liferay/liferay-central-test-2

portal-service/src/com/liferay/portal/service/OrganizationLocalService.java
@@ -649,6 +649,10 @@ public interface OrganizationLocalService extends BaseLocalService,
 		long companyId, java.lang.String name)
 		throws com.liferay.portal.kernel.exception.SystemException;
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portal.model.Organization> getNoAssetOrganizations()
+		throws com.liferay.portal.kernel.exception.SystemException;
+
 	/**
 	* Returns the organization with the name.
 	*

portal-service/src/com/liferay/portal/service/OrganizationLocalServiceUtil.java
@@ -757,6 +757,11 @@ public class OrganizationLocalServiceUtil {
 		return getService().fetchOrganization(companyId, name);
 	}
 
+	public static java.util.List<com.liferay.portal.model.Organization> getNoAssetOrganizations()
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getService().getNoAssetOrganizations();
+	}
+
 	/**
 	* Returns the organization with the name.
 	*

portal-service/src/com/liferay/portal/service/OrganizationLocalServiceWrapper.java
@@ -810,6 +810,12 @@ public class OrganizationLocalServiceWrapper implements OrganizationLocalService
 		return _organizationLocalService.fetchOrganization(companyId, name);
 	}
 
+	@Override
+	public java.util.List<com.liferay.portal.model.Organization> getNoAssetOrganizations()
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return _organizationLocalService.getNoAssetOrganizations();
+	}
+
 	/**
 	* Returns the organization with the name.
 	*