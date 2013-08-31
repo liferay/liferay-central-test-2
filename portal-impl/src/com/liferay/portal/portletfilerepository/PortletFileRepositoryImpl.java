7b1c3f942dc5a5cde69f90354f603485ad7649e9
Brian Chan <brian.chan@liferay.com>
2013-08-31 14:19:25 -0700

    LPS-38386 Remove unused

--- portal-impl/src/com/liferay/portal/portletfilerepository/PortletFileRepositoryImpl.java
+++ portal-impl/src/com/liferay/portal/portletfilerepository/PortletFileRepositoryImpl.java
@@ -17,8 +17,6 @@ package com.liferay.portal.portletfilerepository;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.FileVersion;
 import com.liferay.portal.kernel.repository.model.Folder;
@@ -557,8 +555,5 @@ public class PortletFileRepositoryImpl implements PortletFileRepository {
 			return fileEntries;
 		}
 	}
-
-	private static Log _log = LogFactoryUtil.getLog(
-		PortletFileRepositoryImpl.class);
 
 }
