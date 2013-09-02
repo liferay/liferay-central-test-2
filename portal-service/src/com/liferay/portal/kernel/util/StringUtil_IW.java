85567640a1ad1c6f6e5c812602cd960c7ba55855
Hugo Huijser <hugo.huijser@liferay.com>
2013-09-02 11:29:53 +0800

    LPS-39567 Generated

--- portal-service/src/com/liferay/portal/kernel/util/StringUtil_IW.java
+++ portal-service/src/com/liferay/portal/kernel/util/StringUtil_IW.java
@@ -165,6 +165,14 @@ public class StringUtil_IW {
 		return StringUtil.insert(s, insert, offset);
 	}
 
+	public boolean isLowerCase(java.lang.String s) {
+		return StringUtil.isLowerCase(s);
+	}
+
+	public boolean isUpperCase(java.lang.String s) {
+		return StringUtil.isUpperCase(s);
+	}
+
 	public int lastIndexOfAny(java.lang.String s, char[] chars) {
 		return StringUtil.lastIndexOfAny(s, chars);
 	}
