5cb66f7a842f957bceb9b0a5f9a4a0c0e99f0897
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2013-09-04 17:46:25 +0200

    LPS-38315 Fix UpdateEmailAddressAction

--- portal-impl/src/com/liferay/portal/action/UpdateEmailAddressAction.java
+++ portal-impl/src/com/liferay/portal/action/UpdateEmailAddressAction.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.security.auth.AuthTokenUtil;
 import com.liferay.portal.security.auth.PrincipalException;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.service.ServiceContextFactory;
@@ -88,6 +89,9 @@ public class UpdateEmailAddressAction extends Action {
 
 	protected void updateEmailAddress(HttpServletRequest request)
 		throws Exception {
+
+		AuthTokenUtil.checkCSRFToken(
+			request, UpdateEmailAddressAction.class.getName());
 
 		long userId = PortalUtil.getUserId(request);
 		String password = AdminUtil.getUpdateUserPassword(request, userId);
