f03238953599a7b1b297e6b75d02e1a866ac2242
Zsolt Berentey <zsolt.berentey@liferay.com>
2013-09-05 16:56:13 +0200

    LPS-39632 Override the addDeletionSystemEvent method for Wiki since we need the page id instead of the resource primery key

--- portal-impl/src/com/liferay/portlet/wiki/trash/WikiPageTrashHandler.java
+++ portal-impl/src/com/liferay/portlet/wiki/trash/WikiPageTrashHandler.java
@@ -25,6 +25,7 @@ import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.model.ContainerModel;
 import com.liferay.portal.model.LayoutConstants;
+import com.liferay.portal.model.SystemEvent;
 import com.liferay.portal.portletfilerepository.PortletFileRepositoryUtil;
 import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.security.permission.PermissionChecker;
@@ -58,6 +59,19 @@ import javax.portlet.PortletURL;
  * @author Eudaldo Alonso
  */
 public class WikiPageTrashHandler extends BaseTrashHandler {
+
+	@Override
+	public SystemEvent addDeletionSystemEvent(
+			long userId, long groupId, long classPK, String classUuid,
+			String referrerClassName)
+		throws PortalException, SystemException {
+
+		WikiPage page = WikiPageLocalServiceUtil.getLatestPage(
+			classPK, WorkflowConstants.STATUS_ANY, false);
+
+		return super.addDeletionSystemEvent(
+			userId, groupId, page.getPageId(), classUuid, referrerClassName);
+	}
 
 	@Override
 	public void checkDuplicateTrashEntry(
