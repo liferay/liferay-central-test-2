9bca56dd1f15cc2b227e6a26f1747a0d547a357e
Catherine Lui <catherine.lui@liferay.com>
2013-09-05 12:35:10 -0700

    LPS-39686

--- portal-web/test/functional/com/liferay/portalweb/portal/util/SeleniumUtil.java
+++ portal-web/test/functional/com/liferay/portalweb/portal/util/SeleniumUtil.java
@@ -23,6 +23,7 @@ import com.liferay.portalweb.portal.util.liferayselenium.FirefoxWebDriverImpl;
 import com.liferay.portalweb.portal.util.liferayselenium.InternetExplorerWebDriverImpl;
 import com.liferay.portalweb.portal.util.liferayselenium.LiferaySelenium;
 import com.liferay.portalweb.portal.util.liferayselenium.LoggerHandler;
+import com.liferay.portalweb.portal.util.liferayselenium.SafariWebDriverImpl;
 
 import com.thoughtworks.selenium.Selenium;
 
@@ -133,6 +134,15 @@ public class SeleniumUtil extends TestPropsValues {
 				else {
 					_selenium = new InternetExplorerWebDriverImpl(
 						projectDir, PORTAL_URL);
+				}
+			}
+			else if (BROWSER_TYPE.equals("*safari")) {
+				if (SELENIUM_LOGGER_ENABLED) {
+					_selenium = _wrapWithLoggerHandler(
+						new SafariWebDriverImpl(projectDir, PORTAL_URL));
+				}
+				else {
+					_selenium = new SafariWebDriverImpl(projectDir, PORTAL_URL);
 				}
 			}
 			else {
