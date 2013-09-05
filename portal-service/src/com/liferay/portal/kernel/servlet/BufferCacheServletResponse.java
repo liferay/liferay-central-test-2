0db7ab937afce14ea275cc34fd391dbe35059249
Hugo Huijser <hugo.huijser@liferay.com>
2013-09-05 08:56:49 +0800

    LPS-39221 Source formatting

--- portal-service/src/com/liferay/portal/kernel/servlet/BufferCacheServletResponse.java
+++ portal-service/src/com/liferay/portal/kernel/servlet/BufferCacheServletResponse.java
@@ -310,10 +310,8 @@ public class BufferCacheServletResponse extends MetaInfoCacheServletResponse {
 
 	@Override
 	public void setBufferSize(int bufferSize) {
-		if (isCommitted()) {
-			if (!ServerDetector.isWebLogic()) {
-				throw new IllegalStateException("Set buffer size after commit");
-			}
+		if (isCommitted() && !ServerDetector.isWebLogic()) {
+			throw new IllegalStateException("Set buffer size after commit");
 		}
 
 		// Buffered response cannot accept buffer size because it has an
