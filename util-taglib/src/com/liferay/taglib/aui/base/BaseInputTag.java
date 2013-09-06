2246783ffc2b264edbbaef66d7a1a42609e7f905
Michael Han <michael.han@liferay.com>
2013-09-06 08:49:00 -0500

    LPS-39722 build-taglibs

--- util-taglib/src/com/liferay/taglib/aui/base/BaseInputTag.java
+++ util-taglib/src/com/liferay/taglib/aui/base/BaseInputTag.java
@@ -129,8 +129,16 @@ public class BaseInputTag extends com.liferay.taglib.util.IncludeTag {
 		return _last;
 	}
 
+	public int getMax() {
+		return _max;
+	}
+
 	public java.lang.Class<?> getModel() {
 		return _model;
+	}
+
+	public int getMin() {
+		return _min;
 	}
 
 	public boolean getMultiple() {
@@ -333,10 +341,22 @@ public class BaseInputTag extends com.liferay.taglib.util.IncludeTag {
 		setScopedAttribute("last", last);
 	}
 
+	public void setMax(int max) {
+		_max = max;
+
+		setScopedAttribute("max", max);
+	}
+
 	public void setModel(java.lang.Class<?> model) {
 		_model = model;
 
 		setScopedAttribute("model", model);
+	}
+
+	public void setMin(int min) {
+		_min = min;
+
+		setScopedAttribute("min", min);
 	}
 
 	public void setMultiple(boolean multiple) {
@@ -449,7 +469,9 @@ public class BaseInputTag extends com.liferay.taglib.util.IncludeTag {
 		_label = null;
 		_languageId = null;
 		_last = false;
+		_max = 0;
 		_model = null;
+		_min = 0;
 		_multiple = false;
 		_name = null;
 		_onChange = null;
@@ -497,7 +519,9 @@ public class BaseInputTag extends com.liferay.taglib.util.IncludeTag {
 		setNamespacedAttribute(request, "label", _label);
 		setNamespacedAttribute(request, "languageId", _languageId);
 		setNamespacedAttribute(request, "last", _last);
+		setNamespacedAttribute(request, "max", _max);
 		setNamespacedAttribute(request, "model", _model);
+		setNamespacedAttribute(request, "min", _min);
 		setNamespacedAttribute(request, "multiple", _multiple);
 		setNamespacedAttribute(request, "name", _name);
 		setNamespacedAttribute(request, "onChange", _onChange);
@@ -543,7 +567,9 @@ public class BaseInputTag extends com.liferay.taglib.util.IncludeTag {
 	private java.lang.String _label = null;
 	private java.lang.String _languageId = null;
 	private boolean _last = false;
+	private int _max = 0;
 	private java.lang.Class<?> _model = null;
+	private int _min = 0;
 	private boolean _multiple = false;
 	private java.lang.String _name = null;
 	private java.lang.String _onChange = null;
