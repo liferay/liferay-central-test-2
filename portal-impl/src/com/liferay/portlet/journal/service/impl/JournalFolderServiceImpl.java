ab7bb61248a822ea722e9d72ab11fdb0842d59f5
Sergio González <sergio.gonzalez@liferay.com>
2013-09-09 17:46:27 +0200

    LPS-36379 Remove unnecessary exception

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalFolderServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalFolderServiceImpl.java
@@ -144,7 +144,7 @@ public class JournalFolderServiceImpl extends JournalFolderServiceBaseImpl {
 	public List<Object> getFoldersAndArticles(
 			long groupId, long folderId, int status, int start, int end,
 			OrderByComparator obc)
-		throws PortalException, SystemException {
+		throws SystemException {
 
 		QueryDefinition queryDefinition = new QueryDefinition(
 			status, start, end, obc);
@@ -157,7 +157,7 @@ public class JournalFolderServiceImpl extends JournalFolderServiceBaseImpl {
 	public List<Object> getFoldersAndArticles(
 			long groupId, long folderId, int start, int end,
 			OrderByComparator obc)
-		throws PortalException, SystemException {
+		throws SystemException {
 
 		return getFoldersAndArticles(
 			groupId, folderId, WorkflowConstants.STATUS_ANY, start, end, obc);
