7bb42ca5344bb604bd7429e45f028826ed9812b9
KocsisDaniel <daniel.kocsis@liferay.com>
2013-09-10 13:44:22 +0200

    LPS-25621 Add convenience methods to handle permissions export-import

--- portal-service/src/com/liferay/portal/kernel/lar/PortletDataContext.java
+++ portal-service/src/com/liferay/portal/kernel/lar/PortletDataContext.java
@@ -139,6 +139,9 @@ public interface PortletDataContext extends Serializable {
 	public void addPermissions(
 		String resourceName, long resourcePK, List<KeyValuePair> permissions);
 
+	public void addPortalPermissions()
+		throws PortalException, SystemException;
+
 	public void addPortletPermissions(String resourceName)
 		throws PortalException, SystemException;
 
@@ -422,6 +425,12 @@ public interface PortletDataContext extends Serializable {
 
 	public void importPermissions(
 			String resourceName, long resourcePK, long newResourcePK)
+		throws PortalException, SystemException;
+
+	public void importPortalPermissions()
+		throws PortalException, SystemException;
+
+	public void importPortletPermissions(String resourceName)
 		throws PortalException, SystemException;
 
 	public void importRatingsEntries(
