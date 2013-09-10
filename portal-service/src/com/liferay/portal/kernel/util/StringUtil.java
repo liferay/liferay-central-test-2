c564bd9cf5a513b2109c67f47a3a0c241b7dbb22
shuyangzhou <shuyang.zhou@liferay.com>
2013-09-10 10:17:34 -0700

    LPS-39780 Avoid unnecessary temp String creation

--- portal-service/src/com/liferay/portal/kernel/util/StringUtil.java
+++ portal-service/src/com/liferay/portal/kernel/util/StringUtil.java
@@ -387,10 +387,39 @@ public class StringUtil {
 	}
 
 	public static boolean equalsIgnoreCase(String s1, String s2) {
-		s1 = toUpperCase(s1);
-		s2 = toUpperCase(s2);
+		if (s1 == s2) {
+			return true;
+		}
 
-		return s1.equals(s2);
+		if ((s1 == null) || (s2 == null)) {
+			return false;
+		}
+
+		if (s1.length() != s2.length()) {
+			return false;
+		}
+
+		for (int i = 0; i < s1.length(); i++) {
+			char c1 = s1.charAt(i);
+
+			char c2 = s2.charAt(i);
+
+			if (c1 == c2) {
+				continue;
+			}
+
+			if ((c1 > 127) || (c2 > 127)) {
+				return s1.equalsIgnoreCase(s2);
+			}
+
+			int delta = c1 - c2;
+
+			if ((delta != 32) && (delta != -32)) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 	/**
