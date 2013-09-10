e6a47686abbb999fbf736c1239c4f7224abd1389
Laszlo Csontos <laszlo.csontos@liferay.com>
2013-09-10 14:15:27 +0200

    LPS-40214 Javadocs
    Conflicts:
    	portal-impl/src/META-INF/javadocs-all.xml
    	portal-impl/src/META-INF/javadocs-rt.xml

--- portal-impl/src/com/liferay/portal/service/impl/RoleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/RoleLocalServiceImpl.java
@@ -734,6 +734,16 @@ public class RoleLocalServiceImpl extends RoleLocalServiceBaseImpl {
 		return rolePersistence.findByCompanyId(companyId);
 	}
 
+	/**
+	 * Returns all the roles in the company which belong to either of the given
+	 * types.
+	 *
+	 * @param  companyId the primary key of the company
+	 * @param  types the role types (optionally <code>null</code>)
+	 * @return the roles in the company which belong to either of the given
+	 *         types
+	 * @throws SystemException if a system exception occurred
+	 */
 	@Override
 	public List<Role> getRoles(long companyId, int[] types)
 		throws SystemException {
