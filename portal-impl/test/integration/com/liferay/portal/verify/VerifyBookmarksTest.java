a0bfe488f814e258f8ae92c2723c71f35d5b580b
ealonso <eudaldo.alonso@liferay.com>
2013-09-11 13:13:32 +0200

    LPS-22629 Test - Add new tests to check the verify process when we have an entity in trash with a not existing folder

--- portal-impl/test/integration/com/liferay/portal/verify/VerifyBookmarksTest.java
+++ portal-impl/test/integration/com/liferay/portal/verify/VerifyBookmarksTest.java
@@ -15,9 +15,21 @@
 package com.liferay.portal.verify;
 
 import com.liferay.portal.kernel.test.ExecutionTestListeners;
+import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceTestUtil;
 import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
 import com.liferay.portal.test.MainServletExecutionTestListener;
+import com.liferay.portal.util.GroupTestUtil;
+import com.liferay.portal.util.TestPropsValues;
+import com.liferay.portlet.bookmarks.model.BookmarksEntry;
+import com.liferay.portlet.bookmarks.model.BookmarksFolder;
+import com.liferay.portlet.bookmarks.service.BookmarksEntryLocalServiceUtil;
+import com.liferay.portlet.bookmarks.service.BookmarksFolderLocalServiceUtil;
+import com.liferay.portlet.bookmarks.util.BookmarksTestUtil;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -26,6 +38,49 @@ import org.junit.runner.RunWith;
 @ExecutionTestListeners(listeners = {MainServletExecutionTestListener.class})
 @RunWith(LiferayIntegrationJUnitTestRunner.class)
 public class VerifyBookmarksTest extends BaseVerifyTestCase {
+
+	@Test
+	@Transactional
+	public void testTreePathWithBookmarksEntryInTrash() throws Exception {
+		Group group = GroupTestUtil.addGroup();
+
+		ServiceContext serviceContext = ServiceTestUtil.getServiceContext(
+			group.getGroupId());
+
+		BookmarksFolder parentFolder = BookmarksTestUtil.addFolder(
+			group.getGroupId(), "parentFolder");
+
+		BookmarksEntry entry = BookmarksTestUtil.addEntry(
+			parentFolder.getFolderId(), true, serviceContext);
+
+		BookmarksEntryLocalServiceUtil.moveEntryToTrash(
+			TestPropsValues.getUserId(), entry.getEntryId());
+
+		BookmarksFolderLocalServiceUtil.deleteFolder(
+			parentFolder.getFolderId(), false);
+
+		doVerify();
+	}
+
+	@Test
+	@Transactional
+	public void testTreePathWithBookmarksFolderInTrash() throws Exception {
+		Group group = GroupTestUtil.addGroup();
+
+		BookmarksFolder parentFolder = BookmarksTestUtil.addFolder(
+			group.getGroupId(), "parentFolder");
+
+		BookmarksFolder childFolder = BookmarksTestUtil.addFolder(
+			group.getGroupId(), parentFolder.getFolderId(), "childFolder");
+
+		BookmarksFolderLocalServiceUtil.moveFolderToTrash(
+			TestPropsValues.getUserId(), childFolder.getFolderId());
+
+		BookmarksFolderLocalServiceUtil.deleteFolder(
+			parentFolder.getFolderId(), false);
+
+		doVerify();
+	}
 
 	@Override
 	protected VerifyProcess getVerifyProcess() {
