4f72c626609c964952dd70350588c17d78b26d4e
ealonso <eudaldo.alonso@liferay.com>
2013-09-11 13:13:32 +0200

    LPS-22629 Test - Add new tests to check the verify process when we have an entity in trash with a not existing folder

--- portal-impl/test/integration/com/liferay/portal/verify/VerifyDocumentLibraryTest.java
+++ portal-impl/test/integration/com/liferay/portal/verify/VerifyDocumentLibraryTest.java
@@ -14,10 +14,21 @@
 
 package com.liferay.portal.verify;
 
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.test.ExecutionTestListeners;
+import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.model.Group;
 import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
 import com.liferay.portal.test.MainServletExecutionTestListener;
+import com.liferay.portal.util.GroupTestUtil;
+import com.liferay.portlet.documentlibrary.model.DLFileShortcut;
+import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
+import com.liferay.portlet.documentlibrary.service.DLAppServiceUtil;
+import com.liferay.portlet.documentlibrary.service.DLFolderLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.util.DLAppTestUtil;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -26,6 +37,70 @@ import org.junit.runner.RunWith;
 @ExecutionTestListeners(listeners = {MainServletExecutionTestListener.class})
 @RunWith(LiferayIntegrationJUnitTestRunner.class)
 public class VerifyDocumentLibraryTest extends BaseVerifyTestCase {
+
+	@Test
+	@Transactional
+	public void testTreePathWithDLFileEntryInTrash() throws Exception {
+		Group group = GroupTestUtil.addGroup();
+
+		Folder parentFolder = DLAppTestUtil.addFolder(
+			group.getGroupId(), DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			"parentFolder");
+
+		FileEntry fileEntry = DLAppTestUtil.addFileEntry(
+			group.getGroupId(), parentFolder.getFolderId(), false, "fileName");
+
+		DLAppServiceUtil.moveFileEntryToTrash(fileEntry.getFileEntryId());
+
+		DLFolderLocalServiceUtil.deleteFolder(
+			parentFolder.getFolderId(), false);
+
+		doVerify();
+	}
+
+	@Test
+	@Transactional
+	public void testTreePathWithDLFileShortcutInTrash() throws Exception {
+		Group group = GroupTestUtil.addGroup();
+
+		Folder parentFolder = DLAppTestUtil.addFolder(
+			group.getGroupId(), DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			"parentFolder");
+
+		FileEntry fileEntry = DLAppTestUtil.addFileEntry(
+			group.getGroupId(), parentFolder.getFolderId(), false, "fileName");
+
+		DLFileShortcut dlFileShortcut = DLAppTestUtil.addDLFileShortcut(
+			fileEntry, group.getGroupId(), parentFolder.getFolderId());
+
+		DLAppServiceUtil.moveFileShortcutToTrash(
+			dlFileShortcut.getFileShortcutId());
+
+		DLFolderLocalServiceUtil.deleteFolder(
+			parentFolder.getFolderId(), false);
+
+		doVerify();
+	}
+
+	@Test
+	@Transactional
+	public void testTreePathWithDLFolderInTrash() throws Exception {
+		Group group = GroupTestUtil.addGroup();
+
+		Folder parentFolder = DLAppTestUtil.addFolder(
+			group.getGroupId(), DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			"parentFolder");
+
+		Folder childFolder = DLAppTestUtil.addFolder(
+			group.getGroupId(), parentFolder.getFolderId(), "parentFolder");
+
+		DLAppServiceUtil.moveFolderToTrash(childFolder.getFolderId());
+
+		DLFolderLocalServiceUtil.deleteFolder(
+			parentFolder.getFolderId(), false);
+
+		doVerify();
+	}
 
 	@Override
 	protected VerifyProcess getVerifyProcess() {
--- portal-impl/test/integration/com/liferay/portal/verify/VerifyJournalTest.java
+++ portal-impl/test/integration/com/liferay/portal/verify/VerifyJournalTest.java
@@ -15,9 +15,19 @@
 package com.liferay.portal.verify;
 
 import com.liferay.portal.kernel.test.ExecutionTestListeners;
+import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.model.Group;
 import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
 import com.liferay.portal.test.MainServletExecutionTestListener;
+import com.liferay.portal.util.GroupTestUtil;
+import com.liferay.portal.util.TestPropsValues;
+import com.liferay.portlet.journal.model.JournalArticle;
+import com.liferay.portlet.journal.model.JournalFolder;
+import com.liferay.portlet.journal.service.JournalArticleLocalServiceUtil;
+import com.liferay.portlet.journal.service.JournalFolderLocalServiceUtil;
+import com.liferay.portlet.journal.util.JournalTestUtil;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -26,6 +36,47 @@ import org.junit.runner.RunWith;
 @ExecutionTestListeners(listeners = {MainServletExecutionTestListener.class})
 @RunWith(LiferayIntegrationJUnitTestRunner.class)
 public class VerifyJournalTest extends BaseVerifyTestCase {
+
+	@Test
+	@Transactional
+	public void testTreePathWithJournalArticleInTrash() throws Exception {
+		Group group = GroupTestUtil.addGroup();
+
+		JournalFolder parentFolder = JournalTestUtil.addFolder(
+			group.getGroupId(), "parentFolder");
+
+		JournalArticle article = JournalTestUtil.addArticle(
+			group.getGroupId(), parentFolder.getFolderId(), "title", "content");
+
+		JournalArticleLocalServiceUtil.moveArticleToTrash(
+			TestPropsValues.getUserId(), group.getGroupId(),
+			article.getArticleId());
+
+		JournalFolderLocalServiceUtil.deleteFolder(
+			parentFolder.getFolderId(), false);
+
+		doVerify();
+	}
+
+	@Test
+	@Transactional
+	public void testTreePathWithJournalFolderInTrash() throws Exception {
+		Group group = GroupTestUtil.addGroup();
+
+		JournalFolder parentFolder = JournalTestUtil.addFolder(
+			group.getGroupId(), "parentFolder");
+
+		JournalFolder childFolder = JournalTestUtil.addFolder(
+			group.getGroupId(), parentFolder.getFolderId(), "childFolder");
+
+		JournalFolderLocalServiceUtil.moveFolderToTrash(
+			TestPropsValues.getUserId(), childFolder.getFolderId());
+
+		JournalFolderLocalServiceUtil.deleteFolder(
+			parentFolder.getFolderId(), false);
+
+		doVerify();
+	}
 
 	@Override
 	protected VerifyProcess getVerifyProcess() {
