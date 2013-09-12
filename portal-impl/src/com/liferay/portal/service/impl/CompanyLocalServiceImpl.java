ebec7a9a491604bdb62cc38dbafde0ab1ba2e21f
Mika Koivisto <mika.koivisto@liferay.com>
2013-09-12 10:50:59 -0700

    LPS-35742 Delete company impl with ActionableDynamicQuery

--- portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
@@ -22,6 +22,10 @@ import com.liferay.portal.LocaleException;
 import com.liferay.portal.NoSuchShardException;
 import com.liferay.portal.NoSuchVirtualHostException;
 import com.liferay.portal.RequiredCompanyException;
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.Property;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.language.LanguageUtil;
@@ -45,21 +49,38 @@ import com.liferay.portal.kernel.util.TimeZoneUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.liveusers.LiveUsers;
 import com.liferay.portal.model.Account;
 import com.liferay.portal.model.Company;
 import com.liferay.portal.model.CompanyConstants;
 import com.liferay.portal.model.Contact;
 import com.liferay.portal.model.ContactConstants;
 import com.liferay.portal.model.Group;
+import com.liferay.portal.model.GroupConstants;
+import com.liferay.portal.model.LayoutPrototype;
 import com.liferay.portal.model.LayoutSetPrototype;
+import com.liferay.portal.model.Organization;
+import com.liferay.portal.model.OrganizationConstants;
+import com.liferay.portal.model.PasswordPolicy;
+import com.liferay.portal.model.PortalPreferences;
+import com.liferay.portal.model.Portlet;
 import com.liferay.portal.model.Role;
 import com.liferay.portal.model.RoleConstants;
+import com.liferay.portal.model.Shard;
 import com.liferay.portal.model.User;
 import com.liferay.portal.model.VirtualHost;
 import com.liferay.portal.security.auth.CompanyThreadLocal;
 import com.liferay.portal.service.base.CompanyLocalServiceBaseImpl;
+import com.liferay.portal.service.persistence.GroupActionableDynamicQuery;
+import com.liferay.portal.service.persistence.LayoutPrototypeActionableDynamicQuery;
+import com.liferay.portal.service.persistence.LayoutSetPrototypeActionableDynamicQuery;
+import com.liferay.portal.service.persistence.OrganizationActionableDynamicQuery;
+import com.liferay.portal.service.persistence.RoleActionableDynamicQuery;
+import com.liferay.portal.service.persistence.UserActionableDynamicQuery;
 import com.liferay.portal.util.Portal;
 import com.liferay.portal.util.PortalInstances;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portal.util.PortletKeys;
 import com.liferay.portal.util.PrefsPropsUtil;
 import com.liferay.portal.util.PropsUtil;
 import com.liferay.portal.util.PropsValues;
@@ -1185,6 +1206,26 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		return company;
 	}
 
+	protected void deleteGroup(Group group)
+		throws PortalException, SystemException {
+
+		RecursiveDeleteGroupActionableDynamicQuery
+			recursiveDeleteGroupActionableDynamicQuery =
+				new RecursiveDeleteGroupActionableDynamicQuery();
+
+		recursiveDeleteGroupActionableDynamicQuery.setCompanyId(
+			group.getCompanyId());
+
+		recursiveDeleteGroupActionableDynamicQuery.setParentGroupId(
+			group.getGroupId());
+
+		recursiveDeleteGroupActionableDynamicQuery.performActions();
+
+		groupLocalService.deleteGroup(group);
+
+		LiveUsers.deleteGroup(group.getCompanyId(), group.getGroupId());
+	}
+
 	protected Company doDeleteCompany(long companyId)
 		throws PortalException, SystemException {
 
@@ -1193,6 +1234,183 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		company.setActive(false);
 
 		companyPersistence.update(company);
+
+		// Users
+
+		ActionableDynamicQuery userActionableDynamicQuery =
+			new UserActionableDynamicQuery() {
+
+			@Override
+			protected void performAction(Object object)
+				throws PortalException, SystemException {
+
+				User user = (User)object;
+
+				if (!user.isDefaultUser()) {
+					userLocalService.deleteUser(user.getUserId());
+				}
+			}
+		};
+
+		userActionableDynamicQuery.setCompanyId(companyId);
+
+		userActionableDynamicQuery.performActions();
+
+		// Organizations
+
+		RecursiveDeleteOrganizationActionableDynamicQuery
+			recursiveDeleteOrganizationActionableDynamicQuery =
+				new RecursiveDeleteOrganizationActionableDynamicQuery();
+
+		recursiveDeleteOrganizationActionableDynamicQuery.setCompanyId(
+			companyId);
+
+		recursiveDeleteOrganizationActionableDynamicQuery.performActions();
+
+		// Sites
+
+		RecursiveDeleteGroupActionableDynamicQuery
+			recursiveDeleteGroupActionableDynamicQuery =
+				new RecursiveDeleteGroupActionableDynamicQuery();
+
+		recursiveDeleteGroupActionableDynamicQuery.setCompanyId(companyId);
+
+		recursiveDeleteGroupActionableDynamicQuery.performActions();
+
+		// System Groups
+
+		String[] systemGroups = PortalUtil.getSystemGroups();
+
+		for (String groupName : systemGroups) {
+			Group group = groupLocalService.getGroup(companyId, groupName);
+
+			deleteGroup(group);
+		}
+
+		// Company Group
+
+		Group companyGroup = groupLocalService.getCompanyGroup(companyId);
+
+		deleteGroup(companyGroup);
+
+		// LayoutPrototype
+
+		ActionableDynamicQuery layoutPrototypeActionableDynamicQuery =
+			new LayoutPrototypeActionableDynamicQuery() {
+
+			@Override
+			protected void performAction(Object object)
+				throws PortalException, SystemException {
+
+				LayoutPrototype layoutPrototype = (LayoutPrototype)object;
+
+				layoutPrototypeLocalService.deleteLayoutPrototype(
+					layoutPrototype);
+			}
+		};
+
+		layoutPrototypeActionableDynamicQuery.setCompanyId(companyId);
+
+		layoutPrototypeActionableDynamicQuery.performActions();
+
+		ActionableDynamicQuery layoutSetPrototypeActionableDynamicQuery =
+			new LayoutSetPrototypeActionableDynamicQuery() {
+
+			@Override
+			protected void performAction(Object object)
+				throws PortalException, SystemException {
+
+				LayoutSetPrototype layoutSetPrototype =
+					(LayoutSetPrototype)object;
+
+				layoutSetPrototypeLocalService.deleteLayoutSetPrototype(
+					layoutSetPrototype);
+			}
+		};
+
+		layoutSetPrototypeActionableDynamicQuery.setCompanyId(companyId);
+
+		layoutSetPrototypeActionableDynamicQuery.performActions();
+
+		// Default user
+
+		User defaultUser = userLocalService.getDefaultUser(companyId);
+
+		userLocalService.deleteUser(defaultUser);
+
+		// Roles
+
+		ActionableDynamicQuery roleActionableDynamicQuery =
+			new RoleActionableDynamicQuery() {
+
+			@Override
+			protected void performAction(Object object)
+				throws PortalException, SystemException {
+
+				Role role = (Role)object;
+
+				roleLocalService.deleteRole(role);
+			}
+		};
+
+		roleActionableDynamicQuery.setCompanyId(companyId);
+
+		roleActionableDynamicQuery.performActions();
+
+		// Password Policy
+
+		passwordPolicyLocalService.deleteNondefaultPasswordPolicies(companyId);
+
+		PasswordPolicy defaultPasswordPolicy =
+			passwordPolicyLocalService.getDefaultPasswordPolicy(companyId);
+
+		passwordPolicyLocalService.deletePasswordPolicy(defaultPasswordPolicy);
+
+		// Portlet
+
+		List<Portlet> portlets = portletPersistence.findByCompanyId(companyId);
+
+		for (Portlet portlet : portlets) {
+			portletLocalService.deletePortlet(portlet.getId());
+		}
+
+		portletLocalService.removeCompanyPortletsPool(companyId);
+
+		// Portal Preferences
+
+		PortalPreferences portalPreferences =
+			portalPreferencesPersistence.findByO_O(
+				companyId, PortletKeys.PREFS_OWNER_TYPE_COMPANY);
+
+		portalPreferencesLocalService.deletePortalPreferences(
+			portalPreferences);
+
+		// Virtual Host
+
+		VirtualHost companyVirtualHost =
+			virtualHostLocalService.fetchVirtualHost(companyId, 0);
+
+		virtualHostLocalService.deleteVirtualHost(companyVirtualHost);
+
+		// Account
+
+		accountLocalService.deleteAccount(company.getAccountId());
+
+		// Shard
+
+		Shard shard =
+			shardLocalService.getShard(
+				Company.class.getName(), company.getCompanyId());
+
+		shardLocalService.deleteShard(shard);
+
+		// Company
+
+		company = companyPersistence.remove(companyId);
+
+		// Portal instance
+
+		PortalInstances.removeCompany(companyId);
 
 		return company;
 	}
@@ -1335,6 +1553,99 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 				throw le;
 			}
 		}
+	}
+
+	protected class RecursiveDeleteGroupActionableDynamicQuery
+		extends GroupActionableDynamicQuery {
+
+		public RecursiveDeleteGroupActionableDynamicQuery()
+			throws SystemException {
+
+			super();
+		}
+
+		public void setParentGroupId(long parentGroupId) {
+			_parentGroupId = parentGroupId;
+		}
+
+		@Override
+		protected void addCriteria(DynamicQuery dynamicQuery) {
+			Property parentGroupIdProperty = PropertyFactoryUtil.forName(
+				"parentGroupId");
+
+			dynamicQuery.add(parentGroupIdProperty.eq(_parentGroupId));
+
+			Property siteProperty = PropertyFactoryUtil.forName("site");
+
+			dynamicQuery.add(siteProperty.eq(Boolean.TRUE));
+		}
+
+		@Override
+		protected void performAction(Object object)
+			throws PortalException, SystemException {
+
+			Group group = (Group)object;
+
+			if (!PortalUtil.isSystemGroup(group.getName()) &&
+				!group.isCompany()) {
+
+				deleteGroup(group);
+			}
+		}
+
+		private long _parentGroupId = GroupConstants.DEFAULT_PARENT_GROUP_ID;
+	}
+
+	protected class RecursiveDeleteOrganizationActionableDynamicQuery
+		extends OrganizationActionableDynamicQuery {
+
+		public RecursiveDeleteOrganizationActionableDynamicQuery()
+			throws SystemException {
+
+			super();
+		}
+
+		public void setParentOrganizationId(long parentOrganizationId) {
+			_parentOrganizationId = parentOrganizationId;
+		}
+
+		@Override
+		protected void addCriteria(DynamicQuery dynamicQuery) {
+			Property property = PropertyFactoryUtil.forName(
+				"parentOrganizationId");
+
+			dynamicQuery.add(property.eq(_parentOrganizationId));
+		}
+
+		protected void deleteOrganization(Organization organization)
+			throws PortalException, SystemException {
+
+			RecursiveDeleteOrganizationActionableDynamicQuery
+			recursiveDeleteOrganizationActionableDynamicQuery =
+				new RecursiveDeleteOrganizationActionableDynamicQuery();
+
+			recursiveDeleteOrganizationActionableDynamicQuery.setCompanyId(
+				organization.getCompanyId());
+
+			recursiveDeleteOrganizationActionableDynamicQuery.
+				setParentOrganizationId(organization.getOrganizationId());
+
+			recursiveDeleteOrganizationActionableDynamicQuery.performActions();
+
+			organizationLocalService.deleteOrganization(organization);
+		}
+
+		@Override
+		protected void performAction(Object object)
+			throws PortalException, SystemException {
+
+			Organization organization = (Organization)object;
+
+			deleteOrganization(organization);
+		}
+
+		private long _parentOrganizationId =
+			OrganizationConstants.DEFAULT_PARENT_ORGANIZATION_ID;
 	}
 
 	private static final String _DEFAULT_VIRTUAL_HOST = "localhost";
