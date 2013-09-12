f0a29d8104a5cf79d7e17f22bc7657c13787ae1f
Sergio González <sergio.gonzalez@liferay.com>
2013-09-12 18:14:54 +0200

    LPS-39901 Revert changes of LPS-30188 and rollback of LPS-34920

--- portal-impl/src/com/liferay/portal/verify/VerifyResourcePermissions.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyResourcePermissions.java
@@ -18,7 +18,6 @@ import com.liferay.portal.NoSuchResourcePermissionException;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.model.Contact;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.LayoutSetBranch;
@@ -72,10 +71,6 @@ public class VerifyResourcePermissions extends VerifyProcess {
 
 	@Override
 	protected void doVerify() throws Exception {
-		for (String[] portletAndActionId : _PORTLET_ACTION_IDS) {
-			verifyActionIds(portletAndActionId[0], portletAndActionId[1]);
-		}
-
 		long[] companyIds = PortalInstances.getCompanyIdsBySQL();
 
 		for (long companyId : companyIds) {
@@ -87,31 +82,6 @@ public class VerifyResourcePermissions extends VerifyProcess {
 			}
 
 			verifyLayout(role);
-		}
-	}
-
-	protected void verifyActionIds(String portlet, String actionId)
-		throws Exception {
-
-		Connection con = null;
-		PreparedStatement ps = null;
-
-		try {
-			con = DataAccess.getUpgradeOptimizedConnection();
-
-			ps = con.prepareStatement(
-				"update ResourcePermission set actionIds = ? where name = ? " +
-					"and roleId in (select roleId from Role_ where name = ?) " +
-						"and primKey != '0'");
-
-			ps.setLong(1, GetterUtil.getLong(actionId));
-			ps.setString(2, portlet);
-			ps.setString(3, "Site Member");
-
-			ps.executeUpdate();
-		}
-		finally {
-			DataAccess.cleanUp(con, ps);
 		}
 	}
 
@@ -297,15 +267,6 @@ public class VerifyResourcePermissions extends VerifyProcess {
 		new String[] {
 			WikiPage.class.getName(), "WikiPage", "resourcePrimKey"
 		}
-	};
-
-	private static final String[][] _PORTLET_ACTION_IDS = new String[][] {
-		new String[] {
-			"com.liferay.portlet.bookmarks", "17"
-		},
-		new String[] {
-			"com.liferay.portlet.documentlibrary", "513"
-		},
 	};
 
 	private static Log _log = LogFactoryUtil.getLog(
