danielkocsis/liferay-portal

LPS-39399 Add method to trash handlers to get the parent entity

portal-service/src/com/liferay/portal/kernel/trash/TrashHandler.java
@@ -18,6 +18,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.model.ClassedModel;
 import com.liferay.portal.model.ContainerModel;
+import com.liferay.portal.model.TrashedModel;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portlet.trash.model.TrashEntry;
@@ -139,6 +140,9 @@ public interface TrashHandler {
 	 * @throws SystemException if a system exception occurred
 	 */
 	public ContainerModel getContainerModel(long containerModelId)
+		throws PortalException, SystemException;
+
+	public ContainerModel getContainerModel(TrashedModel trashedModel)
 		throws PortalException, SystemException;
 
 	/**

liferay/liferay-central-test-2

portal-service/src/com/liferay/portal/kernel/trash/TrashHandler.java
@@ -19,6 +19,7 @@ import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.model.ClassedModel;
 import com.liferay.portal.model.ContainerModel;
 import com.liferay.portal.model.SystemEvent;
+import com.liferay.portal.model.TrashedModel;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portlet.trash.model.TrashEntry;
@@ -245,6 +246,9 @@ public interface TrashHandler {
 	 * @throws SystemException if a system exception occurred
 	 */
 	public ContainerModel getParentContainerModel(long classPK)
+		throws PortalException, SystemException;
+
+	public ContainerModel getParentContainerModel(TrashedModel trashedModel)
 		throws PortalException, SystemException;
 
 	/**