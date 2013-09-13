johnnyhowey/liferay-portal

LPS-40152 Regen

portal-service/src/com/liferay/portal/service/UserNotificationDeliveryLocalService.java
@@ -267,4 +267,8 @@ public interface UserNotificationDeliveryLocalService extends BaseLocalService,
 		int notificationType, int deliveryType, boolean deliver)
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException;
+
+	public com.liferay.portal.model.UserNotificationDelivery updateUserNotificationDelivery(
+		long userNotificationDeliveryId, boolean isDeliver)
+		throws com.liferay.portal.kernel.exception.SystemException;
 }

portal-service/src/com/liferay/portal/service/UserNotificationDeliveryLocalServiceUtil.java
@@ -317,6 +317,14 @@ public class UserNotificationDeliveryLocalServiceUtil {
 			notificationType, deliveryType, deliver);
 	}
 
+	public static com.liferay.portal.model.UserNotificationDelivery updateUserNotificationDelivery(
+		long userNotificationDeliveryId, boolean isDeliver)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getService()
+				   .updateUserNotificationDelivery(userNotificationDeliveryId,
+			isDeliver);
+	}
+
 	public static UserNotificationDeliveryLocalService getService() {
 		if (_service == null) {
 			_service = (UserNotificationDeliveryLocalService)PortalBeanLocatorUtil.locate(UserNotificationDeliveryLocalService.class.getName());

portal-service/src/com/liferay/portal/service/UserNotificationDeliveryLocalServiceWrapper.java
@@ -324,6 +324,14 @@ public class UserNotificationDeliveryLocalServiceWrapper
 			portletId, classNameId, notificationType, deliveryType, deliver);
 	}
 
+	@Override
+	public com.liferay.portal.model.UserNotificationDelivery updateUserNotificationDelivery(
+		long userNotificationDeliveryId, boolean isDeliver)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return _userNotificationDeliveryLocalService.updateUserNotificationDelivery(userNotificationDeliveryId,
+			isDeliver);
+	}
+
 	/**
 	 * @deprecated As of 6.1.0, replaced by {@link #getWrappedService}
 	 */

liferay/liferay-central-test-2

portal-service/src/com/liferay/portal/service/UserNotificationDeliveryLocalService.java
@@ -267,4 +267,8 @@ public interface UserNotificationDeliveryLocalService extends BaseLocalService,
 		int notificationType, int deliveryType, boolean deliver)
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException;
+
+	public com.liferay.portal.model.UserNotificationDelivery updateUserNotificationDelivery(
+		long userNotificationDeliveryId, boolean deliver)
+		throws com.liferay.portal.kernel.exception.SystemException;
 }

portal-service/src/com/liferay/portal/service/UserNotificationDeliveryLocalServiceUtil.java
@@ -317,6 +317,14 @@ public class UserNotificationDeliveryLocalServiceUtil {
 			notificationType, deliveryType, deliver);
 	}
 
+	public static com.liferay.portal.model.UserNotificationDelivery updateUserNotificationDelivery(
+		long userNotificationDeliveryId, boolean deliver)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getService()
+				   .updateUserNotificationDelivery(userNotificationDeliveryId,
+			deliver);
+	}
+
 	public static UserNotificationDeliveryLocalService getService() {
 		if (_service == null) {
 			_service = (UserNotificationDeliveryLocalService)PortalBeanLocatorUtil.locate(UserNotificationDeliveryLocalService.class.getName());

portal-service/src/com/liferay/portal/service/UserNotificationDeliveryLocalServiceWrapper.java
@@ -324,6 +324,14 @@ public class UserNotificationDeliveryLocalServiceWrapper
 			portletId, classNameId, notificationType, deliveryType, deliver);
 	}
 
+	@Override
+	public com.liferay.portal.model.UserNotificationDelivery updateUserNotificationDelivery(
+		long userNotificationDeliveryId, boolean deliver)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return _userNotificationDeliveryLocalService.updateUserNotificationDelivery(userNotificationDeliveryId,
+			deliver);
+	}
+
 	/**
 	 * @deprecated As of 6.1.0, replaced by {@link #getWrappedService}
 	 */