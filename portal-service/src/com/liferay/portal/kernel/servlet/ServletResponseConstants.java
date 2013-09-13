9e3eebc7fe296779bf901ddba78fc5017c87541a
Juan G <juan.gonzalez@liferay.com>
2013-09-13 08:50:59 +0200

    LPS-39474 Run format-source

--- portal-service/src/com/liferay/portal/kernel/servlet/ServletResponseConstants.java
+++ portal-service/src/com/liferay/portal/kernel/servlet/ServletResponseConstants.java
@@ -19,9 +19,11 @@ package com.liferay.portal.kernel.servlet;
  */
 public interface ServletResponseConstants {
 
-	public static final int SC_DUPLICATE_FILE_EXCEPTION = 490;
+	public static final int SC_AUDIO_PREVIEW_DISABLED_EXCEPTION = 211; 
 
 	public static final int SC_FILE_CUSTOM_EXCEPTION = 499;
+
+	public static final int SC_DUPLICATE_FILE_EXCEPTION = 490;
 
 	public static final int SC_FILE_EXTENSION_EXCEPTION = 491;
 
@@ -31,5 +33,4 @@ public interface ServletResponseConstants {
 
 	public static final int SC_VIDEO_PREVIEW_DISABLED_EXCEPTION = 210;
 
-	public static final int SC_AUDIO_PREVIEW_DISABLED_EXCEPTION = 211;
 }
