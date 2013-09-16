8d760bbf282edaebb5fee72d29e38c18d624550f
NorbertKocsis <norbert.kocsis@liferay.com>
2013-09-16 12:51:50 +0200

    LPS-40224 Add workflowDefinitions only if overrideFileEntryTypes is true

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFolderLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFolderLocalServiceImpl.java
@@ -871,12 +871,19 @@ public class DLFolderLocalServiceImpl extends DLFolderLocalServiceBaseImpl {
 			}
 
 			for (long fileEntryTypeId : fileEntryTypeIds) {
-				String workflowDefinition = ParamUtil.getString(
-					serviceContext, "workflowDefinition" + fileEntryTypeId);
+				if (overrideFileEntryTypes) {
+					String workflowDefinition = ParamUtil.getString(
+						serviceContext, "workflowDefinition" + fileEntryTypeId);
 
-				workflowDefinitionOVPs.add(
-					new ObjectValuePair<Long, String>(
-						fileEntryTypeId, workflowDefinition));
+					workflowDefinitionOVPs.add(
+						new ObjectValuePair<Long, String>(
+							fileEntryTypeId, workflowDefinition));
+				}
+				else {
+					workflowDefinitionOVPs.add(
+						new ObjectValuePair<Long, String>(
+							fileEntryTypeId, StringPool.BLANK));
+				}
 			}
 
 			workflowDefinitionLinkLocalService.updateWorkflowDefinitionLinks(
