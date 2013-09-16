d83585b934127eaedc27cdd83d5b0f23e7753780
Laszlo Csontos <laszlo.csontos@liferay.com>
2013-09-16 13:42:36 +0200

    LPS-40223 Javadocs

--- portal-impl/src/com/liferay/portal/service/impl/RoleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/RoleLocalServiceImpl.java
@@ -782,6 +782,15 @@ public class RoleLocalServiceImpl extends RoleLocalServiceBaseImpl {
 		return rolePersistence.findByC_C_C(companyId, classNameId, teamId);
 	}
 
+	/**
+	 * Returns a team to team role mapping for the group
+	 *
+	 * @param  groupId the primary key of the group
+	 * @return team to team role mapping for the group
+	 * @throws PortalException if a role could not be found for one of the teams
+	 *         within the given group
+	 * @throws SystemException if a system exception occurred
+	 */
 	@Override
 	public Map<Team, Role> getTeamRoleMap(long groupId)
 		throws PortalException, SystemException {
@@ -789,6 +798,15 @@ public class RoleLocalServiceImpl extends RoleLocalServiceBaseImpl {
 		return getTeamRoleMap(groupId, null);
 	}
 
+	/**
+	 * Returns the team roles in the group
+	 *
+	 * @param  groupId the primary key of the group
+	 * @return the team roles in the given groupId
+	 * @throws PortalException if a role could not be found for one of the teams
+	 *         within the given group
+	 * @throws SystemException if a system exception occurred
+	 */
 	@Override
 	public List<Role> getTeamRoles(long groupId)
 		throws PortalException, SystemException {
@@ -796,6 +814,16 @@ public class RoleLocalServiceImpl extends RoleLocalServiceBaseImpl {
 		return getTeamRoles(groupId, null);
 	}
 
+	/**
+	 * Returns the team roles in the group
+	 *
+	 * @param  groupId the primary key of the group
+	 * @param  skipRoleIds array of roleIds to be skipped (optional)
+	 * @return the team roles in the given groupId
+	 * @throws PortalException if a role could not be found for one of the teams
+	 *         within the given group
+	 * @throws SystemException if a system exception occurred
+	 */
 	@Override
 	public List<Role> getTeamRoles(long groupId, long[] skipRoleIds)
 		throws PortalException, SystemException {
