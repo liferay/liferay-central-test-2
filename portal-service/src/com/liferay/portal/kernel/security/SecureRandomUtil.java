48f50267e08938ef280897f6e39e1f4031fa16fd
Samuel Kong <samuel.kong@liferay.com>
2013-09-18 22:58:39 +0800

    LPS-40319 Add randomize methods that use SecureRandom

--- portal-service/src/com/liferay/portal/kernel/security/SecureRandomUtil.java
+++ portal-service/src/com/liferay/portal/kernel/security/SecureRandomUtil.java
@@ -19,6 +19,7 @@ import com.liferay.portal.kernel.util.GetterUtil;
 
 import java.security.SecureRandom;
 
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -102,6 +103,75 @@ public class SecureRandomUtil {
 		}
 
 		return _reload();
+	}
+
+	public static void randomize(char[] array) {
+		int length = array.length;
+
+		for (int i = 0; i < length - 1; i++) {
+			int x = nextInt(length);
+			char y = array[i];
+
+			array[i] = array[i + x];
+			array[i + x] = y;
+
+			length--;
+		}
+	}
+
+	public static void randomize(int[] array) {
+		int length = array.length;
+
+		for (int i = 0; i < length - 1; i++) {
+			int x = nextInt(length);
+			int y = array[i];
+
+			array[i] = array[i + x];
+			array[i + x] = y;
+
+			length--;
+		}
+	}
+
+	public static void randomize(List<Object> list) {
+		int size = list.size();
+
+		for (int i = 0; i <= size; i++) {
+			Object obj = list.get(i);
+
+			int j = nextInt(size);
+
+			list.set(i, list.get(i + j));
+			list.set(i + j, obj);
+
+			size--;
+		}
+	}
+
+	public static void randomize(Object[] array) {
+		int length = array.length;
+
+		for (int i = 0; i < length - 1; i++) {
+			int x = nextInt(length);
+			Object y = array[i];
+
+			array[i] = array[i + x];
+			array[i + x] = y;
+
+			length--;
+		}
+	}
+
+	public static String randomize(String s) {
+		if (s == null) {
+			return null;
+		}
+
+		char[] array = s.toCharArray();
+
+		randomize(array);
+
+		return new String(array);
 	}
 
 	private static long _reload() {
