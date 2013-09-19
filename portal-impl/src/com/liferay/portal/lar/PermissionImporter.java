47a9507bd1cc0d6ac68e95656def511eb9907842
Mate Thurzo <mate.thurzo@liferay.com>
2013-09-19 13:32:15 +0200

    LPS-40289 Layout can be null

--- portal-impl/src/com/liferay/portal/lar/PermissionImporter.java
+++ portal-impl/src/com/liferay/portal/lar/PermissionImporter.java
@@ -159,7 +159,7 @@ public class PermissionImporter {
 
 		Element permissionsElement = portletElement.element("permissions");
 
-		if (permissionsElement != null) {
+		if ((permissionsElement != null) && (layout != null)) {
 			String resourceName = PortletConstants.getRootPortletId(portletId);
 
 			String resourcePrimKey = PortletPermissionUtil.getPrimaryKey(
