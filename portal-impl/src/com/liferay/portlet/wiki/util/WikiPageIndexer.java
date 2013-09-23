28a047436e0713f43af95b4290115b8672a42664
ealonso <eudaldo.alonso@liferay.com>
2013-09-23 17:00:37 +0200

    LPS-39077 Wiki - Remove code from indexers

--- portal-impl/src/com/liferay/portlet/wiki/util/WikiPageIndexer.java
+++ portal-impl/src/com/liferay/portlet/wiki/util/WikiPageIndexer.java
@@ -36,14 +36,12 @@ import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.PortletKeys;
 import com.liferay.portlet.documentlibrary.model.DLFileEntry;
 import com.liferay.portlet.messageboards.model.MBMessage;
-import com.liferay.portlet.wiki.asset.WikiPageAssetRendererFactory;
 import com.liferay.portlet.wiki.model.WikiNode;
 import com.liferay.portlet.wiki.model.WikiPage;
 import com.liferay.portlet.wiki.service.WikiNodeServiceUtil;
@@ -222,18 +220,6 @@ public class WikiPageIndexer extends BaseIndexer {
 
 		document.addKeyword(Field.NODE_ID, page.getNodeId());
 		document.addText(Field.TITLE, page.getTitle());
-
-		if (!page.isInTrash() && page.isInTrashContainer()) {
-			addTrashFields(
-				document, WikiNode.class.getName(), page.getNodeId(), null,
-				null, WikiPageAssetRendererFactory.TYPE);
-
-			document.addKeyword(
-				Field.ROOT_ENTRY_CLASS_NAME, WikiNode.class.getName());
-			document.addKeyword(Field.ROOT_ENTRY_CLASS_PK, page.getNodeId());
-			document.addKeyword(
-				Field.STATUS, WorkflowConstants.STATUS_IN_TRASH);
-		}
 
 		return document;
 	}
