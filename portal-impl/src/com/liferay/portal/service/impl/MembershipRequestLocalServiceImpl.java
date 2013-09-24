laszlocsontos/liferay-portal

LPS-40223 Apply to places which contain teams' role lookup redundantely

portal-impl/src/com/liferay/portal/service/impl/MembershipRequestLocalServiceImpl.java
@@ -28,7 +28,6 @@ import com.liferay.portal.model.Resource;
 import com.liferay.portal.model.ResourceConstants;
 import com.liferay.portal.model.Role;
 import com.liferay.portal.model.RoleConstants;
-import com.liferay.portal.model.Team;
 import com.liferay.portal.model.User;
 import com.liferay.portal.model.UserGroupRole;
 import com.liferay.portal.security.permission.ActionKeys;
@@ -40,6 +39,7 @@ import com.liferay.portal.util.ResourcePermissionUtil;
 import com.liferay.portal.util.SubscriptionSender;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
@@ -208,16 +208,9 @@ public class MembershipRequestLocalServiceImpl
 		List<Role> roles = ResourceActionsUtil.getRoles(
 			group.getCompanyId(), group, modelResource, null);
 
-		List<Team> teams = teamLocalService.getGroupTeams(groupId);
+		Collection<Role> teamRoles = roleLocalService.getTeamRoles(groupId);
 
-		if (teams != null) {
-			for (Team team : teams) {
-				Role role = roleLocalService.getTeamRole(
-					team.getCompanyId(), team.getTeamId());
-
-				roles.add(role);
-			}
-		}
+		roles.addAll(teamRoles);
 
 		Resource resource = resourceLocalService.getResource(
 			group.getCompanyId(), modelResource,

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portal/service/impl/MembershipRequestLocalServiceImpl.java
@@ -29,7 +29,6 @@ import com.liferay.portal.model.Resource;
 import com.liferay.portal.model.ResourceConstants;
 import com.liferay.portal.model.Role;
 import com.liferay.portal.model.RoleConstants;
-import com.liferay.portal.model.Team;
 import com.liferay.portal.model.User;
 import com.liferay.portal.model.UserGroupRole;
 import com.liferay.portal.security.permission.ActionKeys;
@@ -210,16 +209,9 @@ public class MembershipRequestLocalServiceImpl
 			ResourceActionsUtil.getRoles(
 				group.getCompanyId(), group, modelResource, null));
 
-		List<Team> teams = teamLocalService.getGroupTeams(groupId);
+		List<Role> teamRoles = roleLocalService.getTeamRoles(groupId);
 
-		if (teams != null) {
-			for (Team team : teams) {
-				Role role = roleLocalService.getTeamRole(
-					team.getCompanyId(), team.getTeamId());
-
-				roles.add(role);
-			}
-		}
+		roles.addAll(teamRoles);
 
 		Resource resource = resourceLocalService.getResource(
 			group.getCompanyId(), modelResource,