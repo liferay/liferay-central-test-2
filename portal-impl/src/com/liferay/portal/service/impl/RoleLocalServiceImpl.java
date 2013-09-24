laszlocsontos/liferay-portal

LPS-40223 Move logic to the service layer

portal-impl/src/com/liferay/portal/service/impl/RoleLocalServiceImpl.java
@@ -33,10 +33,12 @@ import com.liferay.portal.kernel.spring.aop.Skip;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.Company;
@@ -66,6 +68,7 @@ import com.liferay.portlet.usersadmin.util.UsersAdminUtil;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
@@ -73,6 +76,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Provides the local service for accessing, adding, checking, deleting, and
@@ -771,6 +775,29 @@ public class RoleLocalServiceImpl extends RoleLocalServiceBaseImpl {
 		return rolePersistence.findByC_C_C(companyId, classNameId, teamId);
 	}
 
+	@Override
+	public Map<Team, Role> getTeamRoleMap(long groupId)
+		throws PortalException, SystemException {
+
+		return getTeamRoleMap(groupId, null);
+	}
+
+	@Override
+	public Collection<Role> getTeamRoles(long groupId)
+		throws PortalException, SystemException {
+
+		return getTeamRoles(groupId, null);
+	}
+
+	@Override
+	public Collection<Role> getTeamRoles(long groupId, long[] skipRoleIds)
+		throws PortalException, SystemException {
+
+		Map<Team, Role> roles = getTeamRoleMap(groupId, null);
+
+		return roles.values();
+	}
+
 	/**
 	 * Returns all the roles of the type.
 	 *
@@ -1421,6 +1448,45 @@ public class RoleLocalServiceImpl extends RoleLocalServiceBaseImpl {
 			PortletKeys.MY_ACCOUNT, PortletKeys.MY_PAGES,
 			PortletKeys.MY_WORKFLOW_INSTANCES, PortletKeys.MY_WORKFLOW_TASKS
 		};
+	}
+
+	protected Map<Team, Role> getTeamRoleMap(long groupId, long[] skipRoleIds)
+		throws PortalException, SystemException {
+
+		Group group = groupLocalService.getGroup(groupId);
+
+		List<Team> teams = null;
+
+		if (group.isLayout()) {
+			teams = teamLocalService.getGroupTeams(group.getParentGroupId());
+		}
+		else {
+			teams = teamLocalService.getGroupTeams(groupId);
+		}
+
+		if ((teams == null) || teams.isEmpty()) {
+			return Collections.emptyMap();
+		}
+
+		Set<Long> roleIds = null;
+
+		if (ArrayUtil.isNotEmpty(skipRoleIds)) {
+			roleIds = SetUtil.fromArray(skipRoleIds);
+		}
+
+		Map<Team, Role> teamRoleMap = new LinkedHashMap<Team, Role>();
+
+		for (Team team : teams) {
+			Role role = getTeamRole(team.getCompanyId(), team.getTeamId());
+
+			if ((roleIds != null) && roleIds.contains(role.getRoleId())) {
+				continue;
+			}
+
+			teamRoleMap.put(team, role);
+		}
+
+		return teamRoleMap;
 	}
 
 	protected void initPersonalControlPanelPortletsPermissions(Role role)

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portal/service/impl/RoleLocalServiceImpl.java
@@ -35,8 +35,10 @@ import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.Transactional;
 import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.Company;
@@ -66,6 +68,7 @@ import com.liferay.portlet.usersadmin.util.UsersAdminUtil;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
@@ -73,6 +76,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Provides the local service for accessing, adding, checking, deleting, and
@@ -778,6 +782,31 @@ public class RoleLocalServiceImpl extends RoleLocalServiceBaseImpl {
 		return rolePersistence.findByC_C_C(companyId, classNameId, teamId);
 	}
 
+	@Override
+	public Map<Team, Role> getTeamRoleMap(long groupId)
+		throws PortalException, SystemException {
+
+		return getTeamRoleMap(groupId, null);
+	}
+
+	@Override
+	public List<Role> getTeamRoles(long groupId)
+		throws PortalException, SystemException {
+
+		return getTeamRoles(groupId, null);
+	}
+
+	@Override
+	public List<Role> getTeamRoles(long groupId, long[] skipRoleIds)
+		throws PortalException, SystemException {
+
+		Map<Team, Role> teamRoleMap = getTeamRoleMap(groupId, skipRoleIds);
+
+		Collection<Role> roles = teamRoleMap.values();
+
+		return ListUtil.fromCollection(roles);
+	}
+
 	/**
 	 * Returns all the roles of the type.
 	 *
@@ -1428,6 +1457,38 @@ public class RoleLocalServiceImpl extends RoleLocalServiceBaseImpl {
 			PortletKeys.MY_ACCOUNT, PortletKeys.MY_PAGES,
 			PortletKeys.MY_WORKFLOW_INSTANCES, PortletKeys.MY_WORKFLOW_TASKS
 		};
+	}
+
+	protected Map<Team, Role> getTeamRoleMap(long groupId, long[] skipRoleIds)
+		throws PortalException, SystemException {
+
+		Group group = groupLocalService.getGroup(groupId);
+
+		if (group.isLayout()) {
+			group = group.getParentGroup();
+		}
+
+		List<Team> teams = teamLocalService.getGroupTeams(group.getGroupId());
+
+		if (teams.isEmpty()) {
+			return Collections.emptyMap();
+		}
+
+		Set<Long> roleIds = SetUtil.fromArray(skipRoleIds);
+
+		Map<Team, Role> teamRoleMap = new LinkedHashMap<Team, Role>();
+
+		for (Team team : teams) {
+			Role role = getTeamRole(team.getCompanyId(), team.getTeamId());
+
+			if (roleIds.contains(role.getRoleId())) {
+				continue;
+			}
+
+			teamRoleMap.put(team, role);
+		}
+
+		return teamRoleMap;
 	}
 
 	protected void initPersonalControlPanelPortletsPermissions(Role role)