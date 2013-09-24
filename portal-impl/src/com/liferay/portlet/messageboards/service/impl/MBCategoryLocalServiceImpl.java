49f008a4b7997104439ca42bed0ddda197540d93
Brian Chan <brian.chan@liferay.com>
2013-09-23 19:38:23 -0700

    LPS-39077 Consistency with MB

--- portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBCategoryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBCategoryLocalServiceImpl.java
@@ -580,7 +580,7 @@ public class MBCategoryLocalServiceImpl extends MBCategoryLocalServiceBaseImpl {
 			List<Object> categoriesAndThreads = getCategoriesAndThreads(
 				category.getGroupId(), categoryId);
 
-			restoreDependentFromTrash(
+			restoreDependentsFromTrash(
 				user, categoriesAndThreads, trashEntry.getEntryId());
 		}
 
@@ -635,7 +635,7 @@ public class MBCategoryLocalServiceImpl extends MBCategoryLocalServiceBaseImpl {
 		List<Object> categoriesAndThreads = getCategoriesAndThreads(
 			category.getGroupId(), categoryId);
 
-		restoreDependentFromTrash(
+		restoreDependentsFromTrash(
 			user, categoriesAndThreads, trashEntry.getEntryId());
 
 		// Trash
@@ -963,7 +963,7 @@ public class MBCategoryLocalServiceImpl extends MBCategoryLocalServiceBaseImpl {
 		}
 	}
 
-	protected void restoreDependentFromTrash(
+	protected void restoreDependentsFromTrash(
 			User user, List<Object> categoriesAndThreads, long trashEntryId)
 		throws PortalException, SystemException {
 
@@ -1044,7 +1044,7 @@ public class MBCategoryLocalServiceImpl extends MBCategoryLocalServiceBaseImpl {
 
 				// Categories and threads
 
-				restoreDependentFromTrash(
+				restoreDependentsFromTrash(
 					user,
 					getCategoriesAndThreads(
 						category.getGroupId(), category.getCategoryId()),
