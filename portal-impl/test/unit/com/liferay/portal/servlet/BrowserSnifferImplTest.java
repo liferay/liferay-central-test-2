2d9727838b5af3b7ef34e45d0392857f05de4dfd
Iliyan Peychev <iliyan.peychev@liferay.com>
2013-09-26 18:11:55 +0200

    LPS-40591 - Add isAndroid function in BrowserSniffer (tests)
    Conflicts:
    	portal-impl/src/com/liferay/portal/servlet/BrowserSnifferImpl.java

--- portal-impl/test/unit/com/liferay/portal/servlet/BrowserSnifferImplTest.java
+++ portal-impl/test/unit/com/liferay/portal/servlet/BrowserSnifferImplTest.java
@@ -15,6 +15,7 @@
 package com.liferay.portal.servlet;
 
 import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
+import com.liferay.portal.kernel.servlet.HttpHeaders;
 import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.StringUtil;
 
@@ -24,10 +25,66 @@ import java.io.InputStreamReader;
 import org.junit.Assert;
 import org.junit.Test;
 
+import org.springframework.mock.web.MockHttpServletRequest;
+
 /**
  * @author Shuyang Zhou
  */
 public class BrowserSnifferImplTest {
+
+	@Test
+	public void testIsAndroid() throws IOException {
+
+		UnsyncBufferedReader unsyncBufferedReader = new UnsyncBufferedReader(
+			new InputStreamReader(getClass().getResourceAsStream(
+				"dependencies/user_agents.csv")));
+
+		String line = null;
+
+		boolean androidSection = false;
+
+		BrowserSnifferImpl browserSnifferImpl = new BrowserSnifferImpl();
+
+		while ((line = unsyncBufferedReader.readLine()) != null) {
+			line = line.trim();
+
+			if (line.isEmpty()) {
+				continue;
+			}
+
+			if (line.contains("Android Webkit Browser")) {
+				androidSection = true;
+
+				continue;
+			}
+
+			if (androidSection && (line.charAt(0) == CharPool.POUND)) {
+				break;
+			}
+
+			if (androidSection) {
+				MockHttpServletRequest mockHttpServletRequest =
+					new MockHttpServletRequest();
+
+				mockHttpServletRequest.addHeader(HttpHeaders.USER_AGENT, line);
+
+				Assert.assertTrue(
+					browserSnifferImpl.isAndroid(mockHttpServletRequest));
+			}
+		}
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		mockHttpServletRequest.addHeader(
+			HttpHeaders.USER_AGENT,
+			"Safari 6, 6.0, 536.26, mozilla/5.0 (ipad; cpu os 6_0 like mac os" +
+				" x) applewebkit/536.26 (khtml, like gecko) version/6.0" +
+				" mobile/10a5355d safari/8536.25");
+
+		Assert.assertFalse(
+			browserSnifferImpl.isAndroid(mockHttpServletRequest));
+	}
 
 	@Test
 	public void testParseVersion() throws IOException {
