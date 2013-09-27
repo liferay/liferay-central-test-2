874fcf69a76943b08f51bd9791805e7769ee9387
Levente Hudák <levente.hudak@liferay.com>
2013-09-27 11:58:38 +0200

    LPS-40285 Creating a remote service

--- portal-impl/src/com/liferay/portal/service/impl/BackgroundTaskServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/BackgroundTaskServiceImpl.java
@@ -14,12 +14,22 @@
 
 package com.liferay.portal.service.impl;
 
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.service.base.BackgroundTaskServiceBaseImpl;
 
 /**
  * @author Michael C. Han
  */
 public class BackgroundTaskServiceImpl extends BackgroundTaskServiceBaseImpl {
+
+	@Override
+	public int getBackgroundTasksCount(
+			long groupId, String taskExecutorClassName, String completed)
+		throws SystemException {
+
+		return backgroundTaskLocalService.getBackgroundTasksCount(
+			groupId, taskExecutorClassName, completed);
+	}
 
 	@Override
 	public String getBackgroundTaskStatusJSON(long backgroundTaskId) {
