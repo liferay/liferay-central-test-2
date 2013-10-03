c650e4f0b9000d9b68e2a3bd5852db56f570b7b0
Igor Spasic <igor.spasic@liferay.com>
2013-10-03 14:46:06 +0200

    LPS-40858 remove DummyModule as not needed anymore (we support JBoss EAP 5.2 from now)

--- portal-impl/src/com/liferay/portal/spring/context/PortalContextLoaderListener.java
+++ portal-impl/src/com/liferay/portal/spring/context/PortalContextLoaderListener.java
@@ -46,7 +46,6 @@ import com.liferay.portal.kernel.util.ReflectionUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.SystemProperties;
 import com.liferay.portal.kernel.webcache.WebCachePoolUtil;
-import com.liferay.portal.module.framework.DummyModuleFramework;
 import com.liferay.portal.module.framework.ModuleFrameworkUtilAdapter;
 import com.liferay.portal.security.lang.SecurityManagerUtil;
 import com.liferay.portal.security.permission.PermissionCacheUtil;
@@ -204,13 +203,7 @@ public class PortalContextLoaderListener extends ContextLoaderListener {
 			ModuleFrameworkUtilAdapter.startFramework();
 		}
 		catch (Exception e) {
-			_log.error(
-				"Unable to load the module framework because of a bug in the " +
-					"application server. Support for OSGi plugins is disabled.",
-				e);
-
-			ModuleFrameworkUtilAdapter.setModuleFramework(
-				new DummyModuleFramework());
+			throw new RuntimeException(e);
 		}
 
 		SecurityManagerUtil.applySmartStrategy();
