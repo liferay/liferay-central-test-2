fa45291d8fe5b210d0bf015c5a03f9847169c34e
Jose A. Jiménez <jose.jimenez@liferay.com>
2013-10-03 20:25:36 +0200

    LPS-40788 Support for filtering DDLRecordSets in finder - Autogenerated

--- portal-service/src/com/liferay/portlet/dynamicdatalists/service/persistence/DDLRecordSetFinder.java
+++ portal-service/src/com/liferay/portlet/dynamicdatalists/service/persistence/DDLRecordSetFinder.java
@@ -27,6 +27,35 @@ public interface DDLRecordSetFinder {
 		boolean andOperator)
 		throws com.liferay.portal.kernel.exception.SystemException;
 
+	public int filterCountByKeywords(long companyId, long groupId,
+		java.lang.String keywords, int scope)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	public int filterCountByC_G_N_D_S(long companyId, long groupId,
+		java.lang.String name, java.lang.String description, int scope,
+		boolean andOperator)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	public java.util.List<com.liferay.portlet.dynamicdatalists.model.DDLRecordSet> filterFindByKeywords(
+		long companyId, long groupId, java.lang.String keywords, int scope,
+		int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator orderByComparator)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	public java.util.List<com.liferay.portlet.dynamicdatalists.model.DDLRecordSet> filterFindByC_G_N_D_S(
+		long companyId, long groupId, java.lang.String name,
+		java.lang.String description, int scope, boolean andOperator,
+		int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator orderByComparator)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	public java.util.List<com.liferay.portlet.dynamicdatalists.model.DDLRecordSet> filterFindByC_G_N_D_S(
+		long companyId, long groupId, java.lang.String[] names,
+		java.lang.String[] descriptions, int scope, boolean andOperator,
+		int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator orderByComparator)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
 	public java.util.List<com.liferay.portlet.dynamicdatalists.model.DDLRecordSet> findByKeywords(
 		long companyId, long groupId, java.lang.String keywords, int scope,
 		int start, int end,
