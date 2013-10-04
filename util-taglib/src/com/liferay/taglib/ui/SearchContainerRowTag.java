77e16c1b390610d107678d67d64b8d484de49d18
Roberto Díaz <roberto.diaz@liferay.com>
2013-10-04 13:29:37 +0200

    LPS-40794 Keep Ray's SF

--- util-taglib/src/com/liferay/taglib/ui/SearchContainerRowTag.java
+++ util-taglib/src/com/liferay/taglib/ui/SearchContainerRowTag.java
@@ -73,15 +73,8 @@ public class SearchContainerRowTag<R>
 		if (!_headerNamesAssigned && (_headerNames != null) &&
 			!_headerNames.isEmpty()) {
 
-			SearchContainerTag<R> searchContainerTag =
-				(SearchContainerTag<R>)findAncestorWithClass(
-					this, SearchContainerTag.class);
-
-			SearchContainer<R> searchContainer =
-				searchContainerTag.getSearchContainer();
-
-			searchContainer.setHeaderNames(_headerNames);
-			searchContainer.setOrderableHeaders(_orderableHeaders);
+			_searchContainer.setHeaderNames(_headerNames);
+			_searchContainer.setOrderableHeaders(_orderableHeaders);
 
 			_headerNamesAssigned = true;
 		}
@@ -140,13 +133,12 @@ public class SearchContainerRowTag<R>
 				"Requires liferay-ui:search-container-results");
 		}
 
-		SearchContainer<R> searchContainer =
-			searchContainerTag.getSearchContainer();
+		_searchContainer = searchContainerTag.getSearchContainer();
 
-		searchContainer.setClassName(_className);
+		_searchContainer.setClassName(_className);
 
-		_resultRows = searchContainer.getResultRows();
-		_results = searchContainer.getResults();
+		_resultRows = _searchContainer.getResultRows();
+		_results = _searchContainer.getResults();
 
 		if ((_results != null) && !_results.isEmpty()) {
 			processRow();
@@ -339,6 +331,7 @@ public class SearchContainerRowTag<R>
 	private String _rowIdProperty;
 	private int _rowIndex;
 	private String _rowVar = DEFAULT_ROW_VAR;
+	private SearchContainer<R> _searchContainer;
 	private boolean _stringKey;
 
 }
