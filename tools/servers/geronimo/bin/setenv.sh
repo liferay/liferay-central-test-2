e76ec03a8104b0ba9a7975407f164d710b60de41
Raymond Auge <raymond.auge@liferay.com>
2013-10-04 20:37:22 -0400

    LPS-39137 geronimo

--- tools/servers/geronimo/bin/setenv.sh
+++ tools/servers/geronimo/bin/setenv.sh
@@ -1,1 +1,1 @@
-GERONIMO_OPTS="$GERONIMO_OPTS -Dfile.encoding=UTF8 -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=512m"
+GERONIMO_OPTS="$GERONIMO_OPTS -Dfile.encoding=UTF8 @java.security.manager.option@ -Djava.security.policy==@app.server.geronimo.dir@/etc/server.policy -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=512m"
