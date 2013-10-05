c669a4e3016a350b8e1414f860f9bc47f585bb17
Raymond Auge <raymond.auge@liferay.com>
2013-10-04 20:37:22 -0400

    LPS-39137 geronimo

--- tools/servers/geronimo/bin/setenv.bat
+++ tools/servers/geronimo/bin/setenv.bat
@@ -1,1 +1,1 @@
-set "GERONIMO_OPTS=%GERONIMO_OPTS% -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT -DXorg.apache.geronimo.deployment.LenientMFCP=true -Xmx1024m -XX:MaxPermSize=512m"
+set "GERONIMO_OPTS=%GERONIMO_OPTS% -Dfile.encoding=UTF8 @java.security.manager.option@ -Djava.security.policy==@app.server.geronimo.dir@/etc/server.policy -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT -DXorg.apache.geronimo.deployment.LenientMFCP=true -Xmx1024m -XX:MaxPermSize=512m"
