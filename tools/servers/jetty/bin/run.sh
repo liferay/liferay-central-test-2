f6a475c7afe31418d307ab4ebc0a22c040dc1230
Brian Chan <brian.chan@liferay.com>
2013-10-05 17:14:37 -0700

    LPS-39137 Sort

--- tools/servers/jetty/bin/run.sh
+++ tools/servers/jetty/bin/run.sh
@@ -6,6 +6,6 @@ then
 	exit
 fi
 
-export JAVA_OPTS="-Dfile.encoding=UTF8 ${java.security.manager.option} -Djetty.home=${app.server.jetty.dir} -Djava.security.policy==${app.server.jetty.dir}/lib/policy/server.policy -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
+export JAVA_OPTS="-Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true ${java.security.manager.option} -Djava.security.policy==${app.server.jetty.dir}/lib/policy/server.policy -Djetty.home=${app.server.jetty.dir} -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
 
 $JAVA_HOME/bin/java $JAVA_OPTS -jar ../start.jar
