a213a87419854bbea8c48e07c5b2aa7a122d82d6
Mika Koivisto <mika.koivisto@liferay.com>
2013-10-09 16:51:10 -0700

    LPS-41081 Use MainServletExecutionTestListener so that classNames are initialized correctly and not rolledback by transactional test

--- portal-impl/test/integration/com/liferay/portlet/wiki/attachments/WikiAttachmentsTest.java
+++ portal-impl/test/integration/com/liferay/portlet/wiki/attachments/WikiAttachmentsTest.java
@@ -21,8 +21,8 @@ import com.liferay.portal.kernel.transaction.Transactional;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.ServiceTestUtil;
-import com.liferay.portal.test.EnvironmentExecutionTestListener;
 import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
+import com.liferay.portal.test.MainServletExecutionTestListener;
 import com.liferay.portal.test.TransactionalExecutionTestListener;
 import com.liferay.portal.util.GroupTestUtil;
 import com.liferay.portal.util.TestPropsValues;
@@ -51,7 +51,7 @@ import org.junit.runner.RunWith;
  */
 @ExecutionTestListeners(
 	listeners = {
-		EnvironmentExecutionTestListener.class,
+		MainServletExecutionTestListener.class,
 		TransactionalExecutionTestListener.class
 	})
 @RunWith(LiferayIntegrationJUnitTestRunner.class)
