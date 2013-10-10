5031dacc780643b5621334d59b1dbe287106dd5e
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2013-10-10 22:24:19 +0200

    LPS-41142 Fix SaveAction

--- portal-impl/src/com/liferay/portlet/quicknote/action/SaveAction.java
+++ portal-impl/src/com/liferay/portlet/quicknote/action/SaveAction.java
@@ -16,12 +16,14 @@ package com.liferay.portlet.quicknote.action;
 
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.security.auth.PrincipalException;
 import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.service.permission.PortletPermissionUtil;
 import com.liferay.portal.struts.JSONAction;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.WebKeys;
 import com.liferay.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portlet.StrictPortletPreferencesImpl;
 
 import javax.portlet.PortletPreferences;
 
@@ -52,7 +54,12 @@ public class SaveAction extends JSONAction {
 			portletId, ActionKeys.CONFIGURATION);
 
 		PortletPreferences portletPreferences =
-			PortletPreferencesFactoryUtil.getPortletSetup(request, portletId);
+			PortletPreferencesFactoryUtil.getStrictPortletSetup(
+				themeDisplay.getLayout(), portletId);
+
+		if (portletPreferences instanceof StrictPortletPreferencesImpl) {
+			throw new PrincipalException();
+		}
 
 		String color = ParamUtil.getString(request, "color");
 		String data = ParamUtil.getString(request, "data");
