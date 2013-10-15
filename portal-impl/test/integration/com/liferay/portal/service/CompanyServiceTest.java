7b7821a86600168f9b2ddd06d05540c235db7aba
Mika Koivisto <mika.koivisto@liferay.com>
2013-10-15 13:36:47 -0700

    LPS-41201 Add tests for portal instance deletion

---
+++ portal-impl/test/integration/com/liferay/portal/service/CompanyServiceTest.java
@@ -0,0 +1,242 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.service;
+
+import com.liferay.portal.RequiredCompanyException;
+import com.liferay.portal.kernel.test.ExecutionTestListeners;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.model.Company;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.GroupConstants;
+import com.liferay.portal.model.LayoutSetPrototype;
+import com.liferay.portal.model.User;
+import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
+import com.liferay.portal.test.MainServletExecutionTestListener;
+import com.liferay.portal.util.GroupTestUtil;
+import com.liferay.portal.util.PortalInstances;
+import com.liferay.portal.util.PropsValues;
+import com.liferay.portal.util.UserTestUtil;
+import com.liferay.portlet.sites.util.SitesUtil;
+
+import java.io.File;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.core.io.FileSystemResourceLoader;
+import org.springframework.mock.web.MockServletContext;
+
+/**
+ * @author Mika Koivisto
+ */
+@ExecutionTestListeners(
+	listeners = {
+		MainServletExecutionTestListener.class
+	})
+@RunWith(LiferayIntegrationJUnitTestRunner.class)
+public class CompanyServiceTest {
+
+	@Before
+	public void setUp() {
+		mockServletContext = new MockServletContext(
+			getResourceBasePath(), new FileSystemResourceLoader());
+	}
+
+	@Test
+	public void testAddAndDeleteCompany() throws Exception {
+		Company company = addCompany();
+
+		CompanyLocalServiceUtil.deleteCompany(company.getCompanyId());
+
+		for (String webId : PortalInstances.getWebIds()) {
+			Assert.assertNotEquals("test.com", webId);
+		}
+	}
+
+	@Test
+	public void testAddAndDeleteCompanyWithCompanyGroupStaging()
+		throws Exception {
+
+		Company company = addCompany();
+
+		long companyId = company.getCompanyId();
+
+		long userId = UserLocalServiceUtil.getDefaultUserId(companyId);
+
+		Group companyGroup = company.getGroup();
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setAttribute("staging", Boolean.TRUE);
+
+		Group companyStagingGroup = GroupLocalServiceUtil.addGroup(
+			userId, GroupConstants.DEFAULT_PARENT_GROUP_ID,
+			companyGroup.getClassName(), companyGroup.getClassPK(),
+			companyGroup.getGroupId(), companyGroup.getDescriptiveName(),
+			companyGroup.getDescription(), companyGroup.getType(),
+			companyGroup.isManualMembership(),
+			companyGroup.getMembershipRestriction(),
+			companyGroup.getFriendlyURL(), false, companyGroup.isActive(),
+			serviceContext);
+
+		CompanyLocalServiceUtil.deleteCompany(company.getCompanyId());
+
+		companyGroup = GroupLocalServiceUtil.fetchGroup(
+			companyGroup.getGroupId());
+
+		Assert.assertNull(companyGroup);
+
+		companyStagingGroup = GroupLocalServiceUtil.fetchGroup(
+			companyStagingGroup.getGroupId());
+
+		Assert.assertNull(companyStagingGroup);
+	}
+
+	@Test
+	public void testAddandDeleteCompanyWithLayoutSetPrototype()
+		throws Exception {
+
+		Company company = addCompany();
+
+		long companyId = company.getCompanyId();
+
+		long userId = UserLocalServiceUtil.getDefaultUserId(companyId);
+
+		LayoutSetPrototype layoutSetPrototype = addLayoutSetPrototype(
+			companyId, userId, ServiceTestUtil.randomString());
+
+		Group group = GroupTestUtil.addGroup(
+			companyId, userId, GroupConstants.DEFAULT_PARENT_GROUP_ID,
+			ServiceTestUtil.randomString(), ServiceTestUtil.randomString());
+
+		SitesUtil.updateLayoutSetPrototypesLinks(
+			group, layoutSetPrototype.getLayoutSetPrototypeId(), 0, true,
+			false);
+
+		addUser(
+			companyId, userId, group.getGroupId(),
+			getServiceContext(companyId));
+
+		CompanyLocalServiceUtil.deleteCompany(companyId);
+
+		layoutSetPrototype =
+			LayoutSetPrototypeLocalServiceUtil.fetchLayoutSetPrototype(
+				layoutSetPrototype.getLayoutSetPrototypeId());
+
+		Assert.assertNull(layoutSetPrototype);
+	}
+
+	@Test
+	public void testAddandDeleteCompanyWithSubGroup() throws Exception {
+		Company company = addCompany();
+
+		long companyId = company.getCompanyId();
+
+		long userId = UserLocalServiceUtil.getDefaultUserId(companyId);
+
+		Group parentGroup = GroupTestUtil.addGroup(
+			companyId, userId, GroupConstants.DEFAULT_PARENT_GROUP_ID,
+			ServiceTestUtil.randomString(), ServiceTestUtil.randomString());
+
+		Group subGroup = GroupTestUtil.addGroup(
+			companyId, userId, parentGroup.getGroupId(),
+			ServiceTestUtil.randomString(), ServiceTestUtil.randomString());
+
+		addUser(
+			companyId, userId, subGroup.getGroupId(),
+			getServiceContext(companyId));
+
+		CompanyLocalServiceUtil.deleteCompany(company.getCompanyId());
+
+		parentGroup = GroupLocalServiceUtil.fetchGroup(
+			parentGroup.getGroupId());
+
+		Assert.assertNull(parentGroup);
+
+		subGroup = GroupLocalServiceUtil.fetchGroup(subGroup.getGroupId());
+
+		Assert.assertNull(subGroup);
+	}
+
+	@Test(expected = RequiredCompanyException.class)
+	public void testDeleteDefaultCompany() throws Exception {
+		long companyId = PortalInstances.getDefaultCompanyId();
+
+		CompanyLocalServiceUtil.deleteCompany(companyId);
+	}
+
+	protected Company addCompany() throws Exception {
+		Company company = CompanyLocalServiceUtil.addCompany(
+			"test.com", "test.com", "test.com", PropsValues.SHARD_DEFAULT_NAME,
+			false, 0, true);
+
+		PortalInstances.initCompany(mockServletContext, "test.com");
+
+		return company;
+	}
+
+	protected LayoutSetPrototype addLayoutSetPrototype(
+			long companyId, long userId, String name)
+		throws Exception {
+
+		Map<Locale, String> nameMap = new HashMap<Locale, String>();
+
+		nameMap.put(LocaleUtil.getDefault(), name);
+
+		LayoutSetPrototype layoutSetPrototype =
+			LayoutSetPrototypeLocalServiceUtil.addLayoutSetPrototype(
+				userId, companyId, nameMap, StringPool.BLANK, true, true,
+				getServiceContext(companyId));
+
+		return layoutSetPrototype;
+	}
+
+	protected User addUser(
+			long companyId, long userId, long groupId,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		return UserTestUtil.addUser(
+			companyId, userId, ServiceTestUtil.randomString(), false,
+			LocaleUtil.getDefault(), "ServiceTestSuite", "ServiceTestSuite",
+			new long[] {groupId}, serviceContext);
+	}
+
+	protected String getResourceBasePath() {
+		File file = new File("portal-web/docroot");
+
+		return "file:" + file.getAbsolutePath();
+	}
+
+	protected ServiceContext getServiceContext(long companyId) {
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setAddGroupPermissions(true);
+		serviceContext.setAddGuestPermissions(true);
+		serviceContext.setCompanyId(companyId);
+
+		return serviceContext;
+	}
+
+	private MockServletContext mockServletContext;
+
+}
