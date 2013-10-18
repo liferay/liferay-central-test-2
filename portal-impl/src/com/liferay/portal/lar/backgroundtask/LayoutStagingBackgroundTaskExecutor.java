16c1ab2e61266ab38b8444ddaf8161420c021c6c
Edward C. Han <edward.han@liferay.com>
2013-10-18 14:14:51 +0200

    LPS-40533 - Transactions

--- portal-impl/src/com/liferay/portal/lar/backgroundtask/LayoutStagingBackgroundTaskExecutor.java
+++ portal-impl/src/com/liferay/portal/lar/backgroundtask/LayoutStagingBackgroundTaskExecutor.java
@@ -15,9 +15,11 @@
 package com.liferay.portal.lar.backgroundtask;
 
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskResult;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.lar.MissingReferences;
 import com.liferay.portal.kernel.lar.PortletDataHandlerKeys;
 import com.liferay.portal.kernel.staging.StagingUtil;
+import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.MapUtil;
@@ -28,12 +30,17 @@ import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
 import com.liferay.portal.service.LayoutSetBranchLocalServiceUtil;
 import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.spring.transaction.TransactionAttributeBuilder;
+import com.liferay.portal.spring.transaction.TransactionalCallableUtil;
 
 import java.io.File;
 import java.io.Serializable;
 
 import java.util.Date;
 import java.util.Map;
+import java.util.concurrent.Callable;
+
+import org.springframework.transaction.interceptor.TransactionAttribute;
 
 /**
  * @author Julio Camarero
@@ -54,65 +61,44 @@ public class LayoutStagingBackgroundTaskExecutor
 		StagingUtil.lockGroup(userId, targetGroupId);
 
 		long sourceGroupId = MapUtil.getLong(taskContextMap, "sourceGroupId");
-		boolean privateLayout = MapUtil.getBoolean(
-			taskContextMap, "privateLayout");
-		long[] layoutIds = GetterUtil.getLongValues(
-			taskContextMap.get("layoutIds"));
-		Map<String, String[]> parameterMap =
-			(Map<String, String[]>)taskContextMap.get("parameterMap");
-		Date startDate = (Date)taskContextMap.get("startDate");
-		Date endDate = (Date)taskContextMap.get("endDate");
+		boolean initialImport = MapUtil.getBoolean(
+			taskContextMap, "initialImport");
+		ServiceContext serviceContext = (ServiceContext)taskContextMap.get(
+			"serviceContext");
+
+		Group sourceGroup = GroupLocalServiceUtil.getGroup(sourceGroupId);
 
 		clearBackgroundTaskStatus(backgroundTask);
 
-		File file = null;
 		MissingReferences missingReferences = null;
 
+		boolean failed = false;
+
 		try {
-			Date lastPublishDate = endDate;
-
-			if (lastPublishDate == null) {
-				lastPublishDate = new Date();
-			}
-
-			file = LayoutLocalServiceUtil.exportLayoutsAsFile(
-				sourceGroupId, privateLayout, layoutIds, parameterMap,
-				startDate, endDate);
-
-			backgroundTask = markBackgroundTask(backgroundTask, "exported");
+			Callable layoutStagingCallable = new LayoutStagingCallable(
+				backgroundTask, sourceGroupId, targetGroupId, taskContextMap,
+				userId);
 
 			missingReferences =
-				LayoutLocalServiceUtil.validateImportLayoutsFile(
-					userId, targetGroupId, privateLayout, parameterMap, file);
+				(MissingReferences)TransactionalCallableUtil.call(
+					_transactionAttribute, layoutStagingCallable);
+		}
+		catch (Throwable t) {
+			failed = true;
 
-			backgroundTask = markBackgroundTask(backgroundTask, "validated");
-
-			LayoutLocalServiceUtil.importLayouts(
-				userId, targetGroupId, privateLayout, parameterMap, file);
-
-			initLayoutSetBranches(userId, sourceGroupId, targetGroupId);
-
-			boolean updateLastPublishDate = MapUtil.getBoolean(
-				parameterMap, PortletDataHandlerKeys.UPDATE_LAST_PUBLISH_DATE);
-
-			if (updateLastPublishDate) {
-				Group sourceGroup = GroupLocalServiceUtil.getGroup(
-					sourceGroupId);
-
-				if (!sourceGroup.hasStagingGroup()) {
-					StagingUtil.updateLastPublishDate(
-						sourceGroupId, privateLayout, lastPublishDate);
-				}
-				else {
-					StagingUtil.updateLastPublishDate(
-						targetGroupId, privateLayout, lastPublishDate);
-				}
+			if (t instanceof Exception) {
+				throw (Exception)t;
+			}
+			else {
+				throw new SystemException(t);
 			}
 		}
 		finally {
-			FileUtil.delete(file);
-
 			StagingUtil.unlockGroup(targetGroupId);
+
+			if (failed && initialImport) {
+				StagingUtil.disableStaging(sourceGroup, serviceContext);
+			}
 		}
 
 		return processMissingReferences(backgroundTask, missingReferences);
@@ -148,6 +134,97 @@ public class LayoutStagingBackgroundTaskExecutor
 		StagingUtil.checkDefaultLayoutSetBranches(
 			userId, sourceGroup, branchingPublic, branchingPrivate, false,
 			serviceContext);
+	}
+
+	private TransactionAttribute _transactionAttribute =
+		TransactionAttributeBuilder.build(
+			Propagation.REQUIRED, new Class<?>[]{Exception.class});
+
+	private class LayoutStagingCallable implements Callable {
+
+		private LayoutStagingCallable(
+			BackgroundTask backgroundTask, long sourceGroupId,
+			long targetGroupId, Map<String, Serializable> taskContextMap,
+			long userId) {
+
+			_backgroundTask = backgroundTask;
+			_sourceGroupId = sourceGroupId;
+			_targetGroupId = targetGroupId;
+			_taskContextMap = taskContextMap;
+			_userId = userId;
+		}
+
+		public Object call() throws Exception {
+			File file = null;
+			MissingReferences missingReferences = null;
+
+			try {
+				boolean privateLayout = MapUtil.getBoolean(
+					_taskContextMap, "privateLayout");
+				long[] layoutIds = GetterUtil.getLongValues(
+					_taskContextMap.get("layoutIds"));
+				Map<String, String[]> parameterMap =
+					(Map<String, String[]>)_taskContextMap.get("parameterMap");
+				Date startDate = (Date)_taskContextMap.get("startDate");
+				Date endDate = (Date)_taskContextMap.get("endDate");
+
+				Date lastPublishDate = endDate;
+
+				if (lastPublishDate == null) {
+					lastPublishDate = new Date();
+				}
+
+				file = LayoutLocalServiceUtil.exportLayoutsAsFile(
+					_sourceGroupId, privateLayout, layoutIds, parameterMap,
+					startDate, endDate);
+
+				_backgroundTask = markBackgroundTask(
+					_backgroundTask, "exported");
+
+				missingReferences =
+					LayoutLocalServiceUtil.validateImportLayoutsFile(
+						_userId, _targetGroupId, privateLayout, parameterMap,
+						file);
+
+				_backgroundTask = markBackgroundTask(
+					_backgroundTask, "validated");
+
+				LayoutLocalServiceUtil.importLayouts(
+					_userId, _targetGroupId, privateLayout, parameterMap, file);
+
+				initLayoutSetBranches(_userId, _sourceGroupId, _targetGroupId);
+
+				boolean updateLastPublishDate = MapUtil.getBoolean(
+					parameterMap,
+					PortletDataHandlerKeys.UPDATE_LAST_PUBLISH_DATE);
+
+				if (updateLastPublishDate) {
+					Group sourceGroup = GroupLocalServiceUtil.getGroup(
+						_sourceGroupId);
+
+					if (!sourceGroup.hasStagingGroup()) {
+						StagingUtil.updateLastPublishDate(
+							_sourceGroupId, privateLayout, lastPublishDate);
+					}
+					else {
+						StagingUtil.updateLastPublishDate(
+							_targetGroupId, privateLayout, lastPublishDate);
+					}
+				}
+			}
+			finally {
+				FileUtil.delete(file);
+			}
+
+			return missingReferences;
+		}
+
+		private BackgroundTask _backgroundTask;
+		private long _sourceGroupId;
+		private long _targetGroupId;
+		private Map<String, Serializable> _taskContextMap;
+		private long _userId;
+
 	}
 
 }
