726cb7d8c9d13f99abd551ab829f97e7f9991dfd
shuyangzhou <shuyang.zhou@liferay.com>
2013-10-21 17:49:30 -0700

    LPS-41514 LiferayIntegrationJUnitTestRunner should not create new thread to run test just for reseting thread locals.

--- portal-impl/test/integration/com/liferay/portal/test/LiferayIntegrationJUnitTestRunner.java
+++ portal-impl/test/integration/com/liferay/portal/test/LiferayIntegrationJUnitTestRunner.java
@@ -15,7 +15,11 @@
 package com.liferay.portal.test;
 
 import com.liferay.portal.kernel.test.AbstractIntegrationJUnitTestRunner;
+import com.liferay.portal.kernel.util.ReflectionUtil;
 import com.liferay.portal.util.InitUtil;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.model.InitializationError;
@@ -24,6 +28,7 @@ import org.junit.runners.model.Statement;
 /**
  * @author Miguel Pastor
  * @author Carlos Sierra
+ * @author Shuyang Zhou
  */
 public class LiferayIntegrationJUnitTestRunner
 	extends AbstractIntegrationJUnitTestRunner {
@@ -42,41 +47,62 @@ public class LiferayIntegrationJUnitTestRunner
 	}
 
 	@Override
-	protected Statement classBlock(final RunNotifier notifier) {
+	protected Statement classBlock(RunNotifier notifier) {
+		final Statement classBlock = super.classBlock(notifier);
+
 		return new Statement() {
 
 			@Override
 			public void evaluate() throws Throwable {
-				Thread thread = new Thread() {
+				Thread currentThread = Thread.currentThread();
 
-					@Override
-					public void run() {
-						try {
-							Statement classBlock =
-								LiferayIntegrationJUnitTestRunner.super.
-									classBlock(notifier);
+				Object threadLocals = _threadLocalsField.get(currentThread);
 
-							classBlock.evaluate();
-						}
-						catch (Throwable throwable) {
-							_throwable = throwable;
-						}
-					}
+				_threadLocalsField.set(currentThread, null);
 
-				};
+				Object inheritableThreadLocals =
+					_inheritableThreadLocalsField.get(currentThread);
 
-				thread.start();
+				_inheritableThreadLocalsField.set(
+					currentThread,
+					_createInheritedMapMethod.invoke(
+						null, inheritableThreadLocals));
 
-				thread.join();
-
-				if (_throwable != null) {
-					throw _throwable;
+				try {
+					classBlock.evaluate();
+				}
+				finally {
+					_threadLocalsField.set(currentThread, threadLocals);
+					_inheritableThreadLocalsField.set(
+						currentThread, inheritableThreadLocals);
 				}
 			}
 
-			private Throwable _throwable;
-
 		};
+	}
+
+	private static final Method _createInheritedMapMethod;
+	private static final Field _inheritableThreadLocalsField;
+	private static final Class<?> _threadLocalMapClass;
+	private static final Field _threadLocalsField;
+
+	static {
+		try {
+			_threadLocalMapClass = Class.forName(
+				ThreadLocal.class.getName().concat("$ThreadLocalMap"));
+
+			_createInheritedMapMethod = ReflectionUtil.getDeclaredMethod(
+				ThreadLocal.class, "createInheritedMap", _threadLocalMapClass);
+
+			_threadLocalsField = ReflectionUtil.getDeclaredField(
+				Thread.class, "threadLocals");
+
+			_inheritableThreadLocalsField = ReflectionUtil.getDeclaredField(
+				Thread.class, "inheritableThreadLocals");
+		}
+		catch (Exception e) {
+			throw new ExceptionInInitializerError(e);
+		}
 	}
 
 }
