47b6218841922fd9bb7bd8f509baddcdf7563626
Juan G <juan.gonzalez@liferay.com>
2013-10-22 22:52:59 +0200

    LPS-39361 Fix deleting previews

--- portal-service/src/com/liferay/portlet/documentlibrary/util/DLPreviewableProcessor.java
+++ portal-service/src/com/liferay/portlet/documentlibrary/util/DLPreviewableProcessor.java
@@ -355,12 +355,15 @@ public abstract class DLPreviewableProcessor implements DLProcessor {
 	protected void deletePreviews(
 		long companyId, long groupId, long fileEntryId, long fileVersionId) {
 
-		try {
-			DLStoreUtil.deleteDirectory(
-				companyId, REPOSITORY_ID,
-				getPathSegment(groupId, fileEntryId, fileVersionId, true));
-		}
-		catch (Exception e) {
+		for (String type : getPreviewTypes()) {
+			String path = getPreviewFilePath(
+				groupId, fileEntryId, fileVersionId, type);
+
+			try {
+				DLStoreUtil.deleteDirectory(companyId, REPOSITORY_ID, path);
+			}
+			catch (Exception e) {
+			}
 		}
 	}
 
@@ -697,6 +700,19 @@ public abstract class DLPreviewableProcessor implements DLProcessor {
 	protected String getPreviewFilePath(
 		FileVersion fileVersion, int index, String type) {
 
+		return getPreviewFilePath(
+			fileVersion.getGroupId(), fileVersion.getFileEntryId(),
+			fileVersion.getFileVersionId(), index, getPreviewType());
+	}
+
+	protected String getPreviewFilePath(FileVersion fileVersion, String type) {
+		return getPreviewFilePath(fileVersion, 0, type);
+	}
+
+	protected String getPreviewFilePath(
+			long groupId, long fileEntryId, long fileVersionId, int index,
+			String type) {
+
 		StringBundler sb = null;
 
 		if (index > 0) {
@@ -706,7 +722,7 @@ public abstract class DLPreviewableProcessor implements DLProcessor {
 			sb = new StringBundler(3);
 		}
 
-		sb.append(getPathSegment(fileVersion, true));
+		sb.append(getPathSegment(groupId, fileEntryId, fileVersionId, true));
 
 		if (index > 0) {
 			sb.append(StringPool.SLASH);
@@ -719,8 +735,10 @@ public abstract class DLPreviewableProcessor implements DLProcessor {
 		return sb.toString();
 	}
 
-	protected String getPreviewFilePath(FileVersion fileVersion, String type) {
-		return getPreviewFilePath(fileVersion, 0, type);
+	protected String getPreviewFilePath(
+		long groupId, long fileEntryId, long fileVersionId, String type) {
+
+		return getPreviewFilePath(groupId, fileEntryId, fileVersionId, 0, type);
 	}
 
 	protected File getPreviewTempFile(String id) {
