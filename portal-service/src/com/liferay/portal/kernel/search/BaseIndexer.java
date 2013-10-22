5f57df30b985156bed694b4ce159ce0bd3c6b8b1
Sergio González <sergio.gonzalez@liferay.com>
2013-10-22 11:59:50 +0200

    LPS-40779 Return the sortField only when it's a number and otherwise call the preexisting method

--- portal-service/src/com/liferay/portal/kernel/search/BaseIndexer.java
+++ portal-service/src/com/liferay/portal/kernel/search/BaseIndexer.java
@@ -311,11 +311,13 @@ public abstract class BaseIndexer implements Indexer {
 
 	@Override
 	public String getSortField(String orderByCol, int sortType) {
-		if (sortType == Sort.STRING_TYPE) {
-			return getSortField(orderByCol);
+		if ((sortType == Sort.DOUBLE_TYPE) || (sortType == Sort.FLOAT_TYPE) ||
+			(sortType == Sort.INT_TYPE) || (sortType == Sort.LONG_TYPE)) {
+
+			return DocumentImpl.getSortableFieldName(orderByCol);
 		}
 
-		return DocumentImpl.getSortableFieldName(orderByCol);
+		return getSortField(orderByCol);
 	}
 
 	@Override
