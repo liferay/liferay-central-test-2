050ed008fbc00e2fa1f9fda1192325b63ea9d6b7
Dustin Ryerson <dustin.ryerson@liferay.com>
2013-10-23 15:30:38 -0700

    LPS-41473 Missed a rename to roleType

--- portal-impl/src/com/liferay/portal/lar/PortletDataContextImpl.java
+++ portal-impl/src/com/liferay/portal/lar/PortletDataContextImpl.java
@@ -460,9 +460,9 @@ public class PortletDataContextImpl implements PortletDataContext {
 		for (Role role : roles) {
 			String roleName = role.getName();
 
-			int type = role.getType();
+			int roleType = role.getType();
 
-			if ((type == RoleConstants.TYPE_PROVIDER) && role.isTeam()) {
+			if ((roleType == RoleConstants.TYPE_PROVIDER) && role.isTeam()) {
 				Team team = TeamLocalServiceUtil.getTeam(role.getClassPK());
 
 				roleName = PermissionExporter.ROLE_TEAM_PREFIX + team.getName();
