be41ff0a25a294857596bbae94265c9e49a3a529
Sergio González <sergio.gonzalez@liferay.com>
2013-10-23 13:42:23 +0200

    LPS-41530 Do not allow having empty lists for current languages for site and portal

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -3485,7 +3485,7 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 		String newLanguageIds = typeSettingsProperties.getProperty(
 			PropsKeys.LOCALES);
 
-		if (newLanguageIds != null) {
+		if (Validator.isNotNull(newLanguageIds)) {
 			String oldLanguageIds = oldTypeSettingsProperties.getProperty(
 				PropsKeys.LOCALES, StringPool.BLANK);
 
