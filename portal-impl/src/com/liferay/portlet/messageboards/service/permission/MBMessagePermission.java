4690669bce795af5fa98ff8e0a245fe11506932e
Kenneth Chang <Kenneth.Chang@liferay.com>
2013-10-25 17:39:45 -0700

    LPS-41713 Prioritize hasBan check

--- portal-impl/src/com/liferay/portlet/messageboards/service/permission/MBMessagePermission.java
+++ portal-impl/src/com/liferay/portlet/messageboards/service/permission/MBMessagePermission.java
@@ -71,6 +71,12 @@ public class MBMessagePermission {
 			String actionId)
 		throws PortalException, SystemException {
 
+		if (MBBanLocalServiceUtil.hasBan(
+				message.getGroupId(), permissionChecker.getUserId())) {
+
+			return false;
+		}
+
 		Boolean hasPermission = StagingPermissionUtil.hasPermission(
 			permissionChecker, message.getGroupId(), MBMessage.class.getName(),
 			message.getMessageId(), PortletKeys.MESSAGE_BOARDS, actionId);
