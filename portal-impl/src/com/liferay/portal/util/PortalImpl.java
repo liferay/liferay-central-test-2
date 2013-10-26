1ea0b16865809d3dbf016a30b34808b98e7b2d44
Julio Camarero <julio.camarero@liferay.com>
2013-10-26 16:48:53 -0700

    LPS-41717 this randomKey is used for Ids in several places but it has non valid js characters

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -869,7 +869,8 @@ public class PortalImpl implements Portal {
 			sb.append(StringPool.UNDERLINE);
 			sb.append(request.getAttribute(WebKeys.RENDER_PORTLET_COLUMN_POS));
 
-			return sb.toString();
+			return StringUtil.replace(
+				sb.toString(), StringPool.DASH, StringPool.UNDERLINE);
 		}
 	}
 
