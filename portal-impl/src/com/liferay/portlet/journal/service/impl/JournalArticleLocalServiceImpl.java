72b68cdb9b92c6f7ced97ec43e64fd0b30831690
Zsolt Berentey <zsolt.berentey@liferay.com>
2013-10-28 14:18:08 +0100

    LPS-41537 Fix wrong class name for the group subscription when sending notifications

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -6314,7 +6314,7 @@ public class JournalArticleLocalServiceImpl
 		}
 
 		subscriptionSender.addPersistedSubscribers(
-			JournalFolder.class.getName(), article.getGroupId());
+			JournalArticle.class.getName(), article.getGroupId());
 
 		subscriptionSender.flushNotificationsAsync();
 	}
