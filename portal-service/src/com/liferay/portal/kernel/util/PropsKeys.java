b7a75fcf8b5b12114cb44db8036c635a00533427
Igor Spasic <igor.spasic@liferay.com>
2013-10-30 14:02:00 +0100

    LPS-41843 added included/excluded lists for JSONWebServices

--- portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -1231,6 +1231,10 @@ public interface PropsKeys {
 
 	public static final String JSONWS_WEB_SERVICE_INVALID_HTTP_METHODS = "jsonws.web.service.invalid.http.methods";
 
+	public static final String JSONWS_WEB_SERVICE_PATHS_EXCLUDED = "jsonws.web.service.paths.excluded";
+
+	public static final String JSONWS_WEB_SERVICE_PATHS_INCLUDED = "jsonws.web.service.paths.included";
+
 	public static final String JSONWS_WEB_SERVICE_STRICT_HTTP_METHOD = "jsonws.web.service.strict.http.method";
 
 	public static final String JSP_WRITER_BUFFER_SIZE = "jsp.writer.buffer.size";
