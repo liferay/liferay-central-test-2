917674611660b2c6f5df37c16af4f186e26f5769
Brian Chan <brian.chan@liferay.com>
2013-10-30 13:52:38 -0700

    LPS-41759 Weird, but consistent. See other comparators in those same dirs.

--- portal-service/src/com/liferay/portlet/documentlibrary/util/comparator/FolderFolderIdComparator.java
+++ portal-service/src/com/liferay/portlet/documentlibrary/util/comparator/FolderFolderIdComparator.java
@@ -12,34 +12,34 @@
  * details.
  */
 
-package com.liferay.portlet.journal.util.comparator;
+package com.liferay.portlet.documentlibrary.util.comparator;
 
 import com.liferay.portal.kernel.util.OrderByComparator;
-import com.liferay.portlet.journal.model.JournalFolder;
+import com.liferay.portlet.documentlibrary.model.DLFolder;
 
 /**
  * @author Shinn Lok
  */
-public class FolderIdComparator extends OrderByComparator {
+public class FolderFolderIdComparator extends OrderByComparator {
 
-	public static final String ORDER_BY_ASC = "JournalFolder.folderId ASC";
+	public static final String ORDER_BY_ASC = "DLFolder.folderId ASC";
 
-	public static final String ORDER_BY_DESC = "JournalFolder.folderId DESC";
+	public static final String ORDER_BY_DESC = "DLFolder.folderId DESC";
 
 	public static final String[] ORDER_BY_FIELDS = {"folderId"};
 
-	public FolderIdComparator() {
+	public FolderFolderIdComparator() {
 		this(false);
 	}
 
-	public FolderIdComparator(boolean ascending) {
+	public FolderFolderIdComparator(boolean ascending) {
 		_ascending = ascending;
 	}
 
 	@Override
 	public int compare(Object obj1, Object obj2) {
-		JournalFolder folder1 = (JournalFolder)obj1;
-		JournalFolder folder2 = (JournalFolder)obj2;
+		DLFolder folder1 = (DLFolder)obj1;
+		DLFolder folder2 = (DLFolder)obj2;
 
 		long folderId1 = folder1.getFolderId();
 		long folderId2 = folder2.getFolderId();
