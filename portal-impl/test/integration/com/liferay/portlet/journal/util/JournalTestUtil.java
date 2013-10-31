c51307c2a5e148884c4f12fa192073d118720a62
ealonso <eudaldo.alonso@liferay.com>
2013-10-31 08:35:27 +0100

    LPS-41820 Fix test

--- portal-impl/test/integration/com/liferay/portlet/journal/util/JournalTestUtil.java
+++ portal-impl/test/integration/com/liferay/portlet/journal/util/JournalTestUtil.java
@@ -674,11 +674,12 @@ public class JournalTestUtil {
 		return JournalArticleLocalServiceUtil.updateArticle(
 			article.getUserId(), article.getGroupId(), article.getFolderId(),
 			article.getArticleId(), article.getVersion(), titleMap,
-			article.getDescriptionMap(), content, article.getType(),
-			article.getStructureId(), article.getTemplateId(),
-			article.getLayoutUuid(), displayDateMonth, displayDateDay,
-			displayDateYear, displayDateHour, displayDateMinute, 0, 0, 0, 0, 0,
-			true, 0, 0, 0, 0, 0, true, article.getIndexable(),
+			article.getDescriptionMap(),
+			createLocalizedContent(content, LocaleUtil.getSiteDefault()),
+			article.getType(), article.getStructureId(),
+			article.getTemplateId(), article.getLayoutUuid(), displayDateMonth,
+			displayDateDay, displayDateYear, displayDateHour, displayDateMinute,
+			0, 0, 0, 0, 0, true, 0, 0, 0, 0, 0, true, article.getIndexable(),
 			article.isSmallImage(), article.getSmallImageURL(), null, null,
 			null, serviceContext);
 	}
