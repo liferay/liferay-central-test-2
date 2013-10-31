4275759a83b0a9ae1e6849aa5760a06d9ca3f38f
Sergio González <sergio.gonzalez@liferay.com>
2013-10-31 12:10:54 +0100

    LPS-41900 Make it camelcase insensitive to avoid db setting issues

--- portal-impl/src/com/liferay/portal/upgrade/v6_2_0/UpgradeDynamicDataMapping.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_2_0/UpgradeDynamicDataMapping.java
@@ -256,7 +256,7 @@ public class UpgradeDynamicDataMapping extends UpgradeProcess {
 				"acceptFiles", "displayChildLabelAsValue", "fieldCssClass"
 			});
 
-		if (structureKey.equals("TikaRawMetadata")) {
+		if (StringUtil.equalsIgnoreCase(structureKey, "TIKARAWMETADATA")) {
 			element.addAttribute("indexType", "text");
 		}
 
