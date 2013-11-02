5190dc762dad6fa0777778168ff19c9f759adf09
Brian Chan <brian.chan@liferay.com>
2013-11-02 08:21:53 -0700

    LPS-41972

--- portal-service/src/com/liferay/portal/kernel/util/ReleaseInfo.java
+++ portal-service/src/com/liferay/portal/kernel/util/ReleaseInfo.java
@@ -87,6 +87,8 @@ public class ReleaseInfo {
 
 	public static final int RELEASE_6_2_10_BUILD_NUMBER = 6210;
 
+	public static final int RELEASE_7_0_0_BUILD_NUMBER = 7000;
+
 	public static final Date getBuildDate() {
 		DateFormat df = DateFormat.getDateInstance(DateFormat.LONG);
 
@@ -136,13 +138,13 @@ public class ReleaseInfo {
 		return _VERSION;
 	}
 
-	private static final String _BUILD = "6200";
+	private static final String _BUILD = "7000";
 
 	private static final int _BUILD_NUMBER = GetterUtil.getInteger(_BUILD);
 
-	private static final String _CODE_NAME = "Newton";
+	private static final String _CODE_NAME = "Wilberforce";
 
-	private static final String _DATE = "November 1, 2013";
+	private static final String _DATE = "May 1, 2014";
 
 	private static final String _NAME = "Liferay Portal Community Edition";
 
@@ -156,9 +158,9 @@ public class ReleaseInfo {
 
 	private static final String _VENDOR = "Liferay, Inc.";
 
-	private static final String _VERSION = "6.2.0";
+	private static final String _VERSION = "7.0.0";
 
-	private static final String _VERSION_DISPLAY_NAME = "6.2.0 CE GA1";
+	private static final String _VERSION_DISPLAY_NAME = "7.0.0 CE M1";
 
 	private static String _releaseInfo;
 	private static String _serverInfo;
