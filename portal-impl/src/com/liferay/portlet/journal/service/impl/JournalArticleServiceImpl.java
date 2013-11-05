71f258c9034b5fa22beed978ada43afe5eaa96d1
Julio Camarero <julio.camarero@liferay.com>
2013-11-05 15:23:52 -0800

    LPS-42064 SF

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleServiceImpl.java
@@ -35,7 +35,6 @@ import java.io.File;
 import java.io.Serializable;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
