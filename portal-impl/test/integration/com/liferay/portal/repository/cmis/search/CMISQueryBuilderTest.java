6a4781cfe4e3043eaa9e2c5dc0452fa42a782955
Manuel de la Peña <manuel.delapenya@liferay.com>
2013-11-08 11:39:16 +0100

    LPS-42036 Reset used services in a protected to method

--- portal-impl/test/integration/com/liferay/portal/repository/cmis/search/CMISQueryBuilderTest.java
+++ portal-impl/test/integration/com/liferay/portal/repository/cmis/search/CMISQueryBuilderTest.java
@@ -107,17 +107,7 @@ public class CMISQueryBuilderTest extends PowerMockito {
 
 	@After
 	public void tearDown() {
-		for (Class<?> serviceUtilClass : _serviceUtilClasses) {
-			try {
-				Field field = serviceUtilClass.getDeclaredField("_service");
-
-				field.setAccessible(true);
-
-				field.set(serviceUtilClass, null);
-			}
-			catch (Exception e) {
-			}
-		}
+		resetServices();
 
 		PortalBeanLocatorUtil.setBeanLocator(_beanLocator);
 	}
@@ -438,6 +428,20 @@ public class CMISQueryBuilderTest extends PowerMockito {
 		);
 
 		return service;
+	}
+
+	protected void resetServices() {
+		for (Class<?> serviceUtilClass : _serviceUtilClasses) {
+			try {
+				Field field = serviceUtilClass.getDeclaredField("_service");
+
+				field.setAccessible(true);
+
+				field.set(serviceUtilClass, null);
+			}
+			catch (Exception e) {
+			}
+		}
 	}
 
 	private static final String _QUERY_POSTFIX = ") ORDER BY HITS DESC";
