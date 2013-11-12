1619c0e9ca7bcde64004d1c779f934a3550be7be
Sergio González <sergio.gonzalez@liferay.com>
2013-11-12 17:11:21 +0100

    LPS-42222 If the group is a user personal site and we are not copying the UserGroup layouts to the user personal site we should do the process in order to obtain the VirtualLayouts and do a merge if it proceeds.

--- portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceVirtualLayoutsAdvice.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceVirtualLayoutsAdvice.java
@@ -73,12 +73,6 @@ public class LayoutLocalServiceVirtualLayoutsAdvice
 
 		Class<?>[] parameterTypes = method.getParameterTypes();
 
-		if (MergeLayoutPrototypesThreadLocal.isMergeComplete(
-				method, arguments)) {
-
-			return methodInvocation.proceed();
-		}
-
 		boolean workflowEnabled = WorkflowThreadLocal.isEnabled();
 
 		if (methodName.equals("getLayout") &&
@@ -87,13 +81,18 @@ public class LayoutLocalServiceVirtualLayoutsAdvice
 
 			Layout layout = (Layout)methodInvocation.proceed();
 
+			Group group = layout.getGroup();
+
+			if (isMergeComplete(method, arguments, group)) {
+				return layout;
+			}
+
 			if (Validator.isNull(layout.getLayoutPrototypeUuid()) &&
 				Validator.isNull(layout.getSourcePrototypeLayoutUuid())) {
 
 				return layout;
 			}
 
-			Group group = layout.getGroup();
 			LayoutSet layoutSet = layout.getLayoutSet();
 
 			try {
@@ -119,12 +118,17 @@ public class LayoutLocalServiceVirtualLayoutsAdvice
 				  Arrays.equals(parameterTypes, _TYPES_L_B_L_B_I_I))) {
 
 			long groupId = (Long)arguments[0];
+
+			Group group = GroupLocalServiceUtil.getGroup(groupId);
+
+			if (isMergeComplete(method, arguments, group)) {
+				return methodInvocation.proceed();
+			}
+
 			boolean privateLayout = (Boolean)arguments[1];
 			long parentLayoutId = (Long)arguments[2];
 
 			try {
-				Group group = GroupLocalServiceUtil.getGroup(groupId);
-
 				LayoutSet layoutSet = LayoutSetLocalServiceUtil.getLayoutSet(
 					groupId, privateLayout);
 
@@ -259,6 +263,21 @@ public class LayoutLocalServiceVirtualLayoutsAdvice
 		dynamicQuery.add(sourcePrototypeLayoutUuidProperty.isNotNull());
 
 		return LayoutLocalServiceUtil.dynamicQuery(dynamicQuery);
+	}
+
+	protected boolean isMergeComplete(
+		Method method, Object[] arguments, Group group) {
+
+		if (MergeLayoutPrototypesThreadLocal.isMergeComplete(
+				method, arguments) &&
+			(!group.isUser() ||
+			 PropsValues.USER_GROUPS_COPY_LAYOUTS_TO_USER_PERSONAL_SITE)) {
+
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 	protected void mergeLayoutSetPrototypeLayouts(
