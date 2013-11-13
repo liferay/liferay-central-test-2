0cc542464d88bed0e2fb3256b1fef33cc36d5983
Marc Lundgren <marc.lundgren@liferay.com>
2013-11-13 12:40:09 -0800

    LPS-42275  A User is unable to paste any content into CKEDITOR of the clipboard contains an anchor without href property

--- portal-web/docroot/html/js/editor/ckeditor_diffs/plugins/bbcode/bbcode_data_processor.js
+++ portal-web/docroot/html/js/editor/ckeditor_diffs/plugins/bbcode/bbcode_data_processor.js
@@ -530,7 +530,7 @@
 		},
 
 		_handleLink: function(element, listTagsIn, listTagsOut) {
-			var hrefAttribute = element.getAttribute('href');
+			var hrefAttribute = element.getAttribute('href') || '';
 
 			var decodedLink = decodeURIComponent(hrefAttribute);
 
