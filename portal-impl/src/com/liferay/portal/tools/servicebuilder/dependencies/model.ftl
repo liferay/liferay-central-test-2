brianchandotcom/liferay-portal

LPS-42022 method to know if an element is the recycle bin explicitely or just because belongs to another which was sent there

portal-impl/src/com/liferay/portal/tools/servicebuilder/dependencies/model.ftl
@@ -392,6 +392,9 @@ public interface ${entity.name}Model extends
 		 */
 		@Override
 		public boolean isInTrashContainer();
+
+		@Override
+		public boolean isInTrashExplicitely() throws SystemException;
 	</#if>
 
 	<#if entity.isWorkflowEnabled()>

portal-impl/src/com/liferay/portal/tools/servicebuilder/dependencies/model_clp.ftl
@@ -567,6 +567,21 @@ public class ${entity.name}Clp extends BaseModelImpl<${entity.name}> implements 
 
 			return false;
 		}
+
+		@Override
+		public boolean isInTrashExplicitely() throws SystemException {
+			if (!isInTrash()) {
+				return false;
+			}
+
+			TrashEntry trashEntry = TrashEntryLocalServiceUtil.fetchEntry(getModelClassName(), getTrashEntryClassPK());
+
+			if (trashEntry != null) {
+				return true;
+			}
+
+			return false;
+		}
 	</#if>
 
 	<#if entity.isWorkflowEnabled()>

portal-impl/src/com/liferay/portal/tools/servicebuilder/dependencies/model_impl.ftl
@@ -828,6 +828,21 @@ public class ${entity.name}ModelImpl extends BaseModelImpl<${entity.name}> imple
 
 			return false;
 		}
+
+		@Override
+		public boolean isInTrashExplicitely() throws SystemException {
+			if (!isInTrash()) {
+				return false;
+			}
+
+			TrashEntry trashEntry = TrashEntryLocalServiceUtil.fetchEntry(getModelClassName(), getTrashEntryClassPK());
+
+			if (trashEntry != null) {
+				return true;
+			}
+
+			return false;
+		}
 	</#if>
 
 	<#if entity.isWorkflowEnabled()>

portal-service/src/com/liferay/portal/model/TrashedModel.java
@@ -36,4 +36,6 @@ public interface TrashedModel {
 
 	public boolean isInTrashContainer();
 
+	public boolean isInTrashExplicitely() throws SystemException;
+
 }

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portal/tools/servicebuilder/dependencies/model.ftl
@@ -392,6 +392,9 @@ public interface ${entity.name}Model extends
 		 */
 		@Override
 		public boolean isInTrashContainer();
+
+		@Override
+		public boolean isInTrashExplicitly() throws SystemException;
 	</#if>
 
 	<#if entity.isWorkflowEnabled()>

portal-impl/src/com/liferay/portal/tools/servicebuilder/dependencies/model_clp.ftl
@@ -567,6 +567,21 @@ public class ${entity.name}Clp extends BaseModelImpl<${entity.name}> implements 
 
 			return false;
 		}
+
+		@Override
+		public boolean isInTrashExplicitly() throws SystemException {
+			if (!isInTrash()) {
+				return false;
+			}
+
+			TrashEntry trashEntry = TrashEntryLocalServiceUtil.fetchEntry(getModelClassName(), getTrashEntryClassPK());
+
+			if (trashEntry != null) {
+				return true;
+			}
+
+			return false;
+		}
 	</#if>
 
 	<#if entity.isWorkflowEnabled()>

portal-impl/src/com/liferay/portal/tools/servicebuilder/dependencies/model_impl.ftl
@@ -828,6 +828,21 @@ public class ${entity.name}ModelImpl extends BaseModelImpl<${entity.name}> imple
 
 			return false;
 		}
+
+		@Override
+		public boolean isInTrashExplicitly() throws SystemException {
+			if (!isInTrash()) {
+				return false;
+			}
+
+			TrashEntry trashEntry = TrashEntryLocalServiceUtil.fetchEntry(getModelClassName(), getTrashEntryClassPK());
+
+			if (trashEntry != null) {
+				return true;
+			}
+
+			return false;
+		}
 	</#if>
 
 	<#if entity.isWorkflowEnabled()>

portal-service/src/com/liferay/portal/model/TrashedModel.java
@@ -36,4 +36,6 @@ public interface TrashedModel {
 
 	public boolean isInTrashContainer();
 
+	public boolean isInTrashExplicitly() throws SystemException;
+
 }