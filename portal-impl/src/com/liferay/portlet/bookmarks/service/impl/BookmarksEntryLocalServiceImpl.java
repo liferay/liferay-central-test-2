brianchandotcom/liferay-portal

LPS-42022 fix tests. this was broken when refactoring the recycle bin pattern for better performance.

portal-impl/src/com/liferay/portlet/bookmarks/service/impl/BookmarksEntryLocalServiceImpl.java
@@ -159,7 +159,7 @@ public class BookmarksEntryLocalServiceImpl
 			groupId, folderId);
 
 		for (BookmarksEntry entry : entries) {
-			if (includeTrashedEntries || !entry.isInTrash()) {
+			if (includeTrashedEntries || !entry.isInTrashExplicitely()) {
 				bookmarksEntryLocalService.deleteEntry(entry);
 			}
 		}

portal-impl/src/com/liferay/portlet/bookmarks/service/impl/BookmarksFolderLocalServiceImpl.java
@@ -129,7 +129,7 @@ public class BookmarksFolderLocalServiceImpl
 			folder.getGroupId(), folder.getFolderId());
 
 		for (BookmarksFolder curFolder : folders) {
-			if (includeTrashedEntries || !curFolder.isInTrash()) {
+			if (includeTrashedEntries || !curFolder.isInTrashExplicitely()) {
 				deleteFolder(curFolder);
 			}
 		}

portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileShortcutLocalServiceImpl.java
@@ -231,7 +231,7 @@ public class DLFileShortcutLocalServiceImpl
 			dlFileShortcutPersistence.findByG_F(groupId, folderId);
 
 		for (DLFileShortcut fileShortcut : fileShortcuts) {
-			if (includeTrashedEntries || !fileShortcut.isInTrash()) {
+			if (includeTrashedEntries || !fileShortcut.isInTrashExplicitely()) {
 				deleteFileShortcut(fileShortcut);
 			}
 		}

portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFolderLocalServiceImpl.java
@@ -225,7 +225,7 @@ public class DLFolderLocalServiceImpl extends DLFolderLocalServiceBaseImpl {
 			dlFolder.getGroupId(), dlFolder.getFolderId());
 
 		for (DLFolder curDLFolder : dlFolders) {
-			if (includeTrashedEntries || !curDLFolder.isInTrash()) {
+			if (includeTrashedEntries || !curDLFolder.isInTrashExplicitely()) {
 				dlFolderLocalService.deleteFolder(
 					curDLFolder, includeTrashedEntries);
 			}

portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -1209,7 +1209,7 @@ public class JournalArticleLocalServiceImpl
 					articleResources.add(articleResource);
 				}
 
-				if (includeTrashedEntries || !article.isInTrash()) {
+				if (includeTrashedEntries || !article.isInTrashExplicitely()) {
 					journalArticleLocalService.deleteArticle(
 						article, null, null);
 				}

portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBCategoryLocalServiceImpl.java
@@ -237,7 +237,7 @@ public class MBCategoryLocalServiceImpl extends MBCategoryLocalServiceBaseImpl {
 			category.getGroupId(), category.getCategoryId());
 
 		for (MBCategory curCategory : categories) {
-			if (includeTrashedEntries || !curCategory.isInTrash()) {
+			if (includeTrashedEntries || !curCategory.isInTrashExplicitely()) {
 				deleteCategory(curCategory, includeTrashedEntries);
 			}
 		}

portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBThreadLocalServiceImpl.java
@@ -286,7 +286,7 @@ public class MBThreadLocalServiceImpl extends MBThreadLocalServiceBaseImpl {
 			groupId, categoryId);
 
 		for (MBThread thread : threads) {
-			if (includeTrashedEntries || !thread.isInTrash()) {
+			if (includeTrashedEntries || !thread.isInTrashExplicitely()) {
 				mbThreadLocalService.deleteThread(thread);
 			}
 		}

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portlet/bookmarks/service/impl/BookmarksEntryLocalServiceImpl.java
@@ -159,7 +159,7 @@ public class BookmarksEntryLocalServiceImpl
 			groupId, folderId);
 
 		for (BookmarksEntry entry : entries) {
-			if (includeTrashedEntries || !entry.isInTrash()) {
+			if (includeTrashedEntries || !entry.isInTrashExplicitly()) {
 				bookmarksEntryLocalService.deleteEntry(entry);
 			}
 		}

portal-impl/src/com/liferay/portlet/bookmarks/service/impl/BookmarksFolderLocalServiceImpl.java
@@ -129,7 +129,7 @@ public class BookmarksFolderLocalServiceImpl
 			folder.getGroupId(), folder.getFolderId());
 
 		for (BookmarksFolder curFolder : folders) {
-			if (includeTrashedEntries || !curFolder.isInTrash()) {
+			if (includeTrashedEntries || !curFolder.isInTrashExplicitly()) {
 				deleteFolder(curFolder);
 			}
 		}

portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileShortcutLocalServiceImpl.java
@@ -231,7 +231,7 @@ public class DLFileShortcutLocalServiceImpl
 			dlFileShortcutPersistence.findByG_F(groupId, folderId);
 
 		for (DLFileShortcut fileShortcut : fileShortcuts) {
-			if (includeTrashedEntries || !fileShortcut.isInTrash()) {
+			if (includeTrashedEntries || !fileShortcut.isInTrashExplicitly()) {
 				deleteFileShortcut(fileShortcut);
 			}
 		}

portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFolderLocalServiceImpl.java
@@ -225,7 +225,7 @@ public class DLFolderLocalServiceImpl extends DLFolderLocalServiceBaseImpl {
 			dlFolder.getGroupId(), dlFolder.getFolderId());
 
 		for (DLFolder curDLFolder : dlFolders) {
-			if (includeTrashedEntries || !curDLFolder.isInTrash()) {
+			if (includeTrashedEntries || !curDLFolder.isInTrashExplicitly()) {
 				dlFolderLocalService.deleteFolder(
 					curDLFolder, includeTrashedEntries);
 			}

portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -1209,7 +1209,7 @@ public class JournalArticleLocalServiceImpl
 					articleResources.add(articleResource);
 				}
 
-				if (includeTrashedEntries || !article.isInTrash()) {
+				if (includeTrashedEntries || !article.isInTrashExplicitly()) {
 					journalArticleLocalService.deleteArticle(
 						article, null, null);
 				}

portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBCategoryLocalServiceImpl.java
@@ -237,7 +237,7 @@ public class MBCategoryLocalServiceImpl extends MBCategoryLocalServiceBaseImpl {
 			category.getGroupId(), category.getCategoryId());
 
 		for (MBCategory curCategory : categories) {
-			if (includeTrashedEntries || !curCategory.isInTrash()) {
+			if (includeTrashedEntries || !curCategory.isInTrashExplicitly()) {
 				deleteCategory(curCategory, includeTrashedEntries);
 			}
 		}

portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBThreadLocalServiceImpl.java
@@ -286,7 +286,7 @@ public class MBThreadLocalServiceImpl extends MBThreadLocalServiceBaseImpl {
 			groupId, categoryId);
 
 		for (MBThread thread : threads) {
-			if (includeTrashedEntries || !thread.isInTrash()) {
+			if (includeTrashedEntries || !thread.isInTrashExplicitly()) {
 				mbThreadLocalService.deleteThread(thread);
 			}
 		}