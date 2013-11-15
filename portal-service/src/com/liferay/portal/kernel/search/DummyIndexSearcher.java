f99d92cfd568ce85c770fb9efe084c216bb3f438
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2013-11-15 10:40:38 +0100

    LPS-42401 Return empty objects instead of null

--- portal-service/src/com/liferay/portal/kernel/search/DummyIndexSearcher.java
+++ portal-service/src/com/liferay/portal/kernel/search/DummyIndexSearcher.java
@@ -14,17 +14,21 @@
 
 package com.liferay.portal.kernel.search;
 
+import com.liferay.portal.kernel.util.StringPool;
+
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 /**
  * @author Marcellus Tavares
+ * @author Carlos Sierra Andrés
  */
 public class DummyIndexSearcher implements IndexSearcher {
 
 	@Override
 	public Hits search(SearchContext searchContext, Query query) {
-		return null;
+		return _emptyHits();
 	}
 
 	@Override
@@ -32,26 +36,49 @@ public class DummyIndexSearcher implements IndexSearcher {
 		String searchEngineId, long companyId, Query query, Sort[] sort,
 		int start, int end) {
 
-		return null;
+		return _emptyHits();
 	}
 
 	@Override
 	public String spellCheckKeywords(SearchContext searchContext) {
-		return null;
+		return StringPool.BLANK;
 	}
 
 	@Override
 	public Map<String, List<String>> spellCheckKeywords(
 		SearchContext searchContext, int max) {
 
-		return null;
+		return _emptyMap;
 	}
 
 	@Override
 	public String[] suggestKeywordQueries(
 		SearchContext searchContext, int max) {
 
-		return null;
+		return _emptyStrings;
 	}
+
+	private Hits _emptyHits() {
+		Hits dummyHits = new HitsImpl();
+
+		dummyHits.setCollatedSpellCheckResult(StringPool.BLANK);
+		dummyHits.setDocs(new Document[0]);
+		dummyHits.setLength(0);
+		dummyHits.setQuery(new StringQueryImpl(StringPool.BLANK));
+		dummyHits.setQuerySuggestions(_emptyStrings);
+		dummyHits.setQueryTerms(_emptyStrings);
+		dummyHits.setLength(0);
+		dummyHits.setScores(_emptyFloats);
+		dummyHits.setSearchTime(0);
+		dummyHits.setSnippets(_emptyStrings);
+		dummyHits.setSpellCheckResults(_emptyMap);
+		dummyHits.setStart(0);
+
+		return dummyHits;
+	}
+
+	private static float[] _emptyFloats = new float[0];
+	private static Map<String, List<String>> _emptyMap = Collections.emptyMap();
+	private static String[] _emptyStrings = new String[0];
 
 }
