b6d3c69ddac56b9ad88017dc7fdec28003976c04
KocsisDaniel <daniel.kocsis@liferay.com>
2013-11-21 13:15:50 +0100

    LPS-42288 Do not add all new portlets to the target layout from the source, but only the ones where the setup was imported as well

--- portal-impl/src/com/liferay/portlet/layoutsadmin/lar/LayoutStagedModelDataHandler.java
+++ portal-impl/src/com/liferay/portlet/layoutsadmin/lar/LayoutStagedModelDataHandler.java
@@ -21,6 +21,7 @@ import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.lar.BaseStagedModelDataHandler;
+import com.liferay.portal.kernel.lar.ExportImportHelperUtil;
 import com.liferay.portal.kernel.lar.ExportImportPathUtil;
 import com.liferay.portal.kernel.lar.PortletDataContext;
 import com.liferay.portal.kernel.lar.PortletDataHandlerKeys;
@@ -33,6 +34,7 @@ import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.StringBundler;
@@ -75,6 +77,7 @@ import com.liferay.portlet.sites.util.SitesUtil;
 
 import java.io.IOException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -458,7 +461,7 @@ public class LayoutStagedModelDataHandler
 		if (layout.isTypeArticle()) {
 			importJournalArticle(portletDataContext, layout);
 
-			updateTypeSettings(importedLayout, layout);
+			updateTypeSettings(portletDataContext, importedLayout, layout);
 		}
 		else if (layout.isTypePortlet() &&
 				 Validator.isNotNull(layout.getTypeSettings()) &&
@@ -474,7 +477,7 @@ public class LayoutStagedModelDataHandler
 				newLayoutsMap);
 		}
 		else {
-			updateTypeSettings(importedLayout, layout);
+			updateTypeSettings(portletDataContext, importedLayout, layout);
 		}
 
 		importedLayout.setHidden(layout.isHidden());
@@ -865,7 +868,7 @@ public class LayoutStagedModelDataHandler
 			"linked-to-layout-uuid");
 
 		if (linkToLayoutId <= 0) {
-			updateTypeSettings(importedLayout, layout);
+			updateTypeSettings(portletDataContext, importedLayout, layout);
 
 			return;
 		}
@@ -909,7 +912,7 @@ public class LayoutStagedModelDataHandler
 			}
 		}
 
-		updateTypeSettings(importedLayout, layout);
+		updateTypeSettings(portletDataContext, importedLayout, layout);
 	}
 
 	protected void importTheme(
@@ -1106,7 +1109,32 @@ public class LayoutStagedModelDataHandler
 		}
 	}
 
-	protected void updateTypeSettings(Layout importedLayout, Layout layout)
+	protected void removePortletFromLayoutType(
+		String portletId, LayoutTypePortlet layoutType) {
+
+		List<String> columns = new ArrayList<String>();
+
+		LayoutTemplate layoutTemplate = layoutType.getLayoutTemplate();
+
+		columns.addAll(layoutTemplate.getColumns());
+
+		String nestedColumnIds = layoutType.getTypeSettingsProperty(
+			LayoutTypePortletConstants.NESTED_COLUMN_IDS);
+
+		columns.addAll(ListUtil.fromArray(StringUtil.split(nestedColumnIds)));
+
+		for (String columnId : columns) {
+			String columnValue = layoutType.getTypeSettingsProperty(columnId);
+
+			columnValue = StringUtil.removeFromList(columnValue, portletId);
+
+			layoutType.setTypeSettingsProperty(columnId, columnValue);
+		}
+	}
+
+	protected void updateTypeSettings(
+			PortletDataContext portletDataContext, Layout importedLayout,
+			Layout layout)
 		throws PortalException, SystemException {
 
 		long groupId = layout.getGroupId();
@@ -1123,7 +1151,34 @@ public class LayoutStagedModelDataHandler
 			LayoutTypePortlet layoutType =
 				(LayoutTypePortlet)layout.getLayoutType();
 
+			// Remove portlets with unchecked setup from the target layout
+
+			List<String> sourcePortletIds = layoutType.getPortletIds();
+
+			for (String portletId : sourcePortletIds) {
+				boolean importPortletSetup = false;
+
+				try {
+					boolean[] importControls =
+						ExportImportHelperUtil.getImportPortletControls(
+							portletDataContext.getCompanyId(), portletId,
+							portletDataContext.getParameterMap(), null);
+
+					importPortletSetup = importControls[2];
+				}
+				catch (Exception e) {
+				}
+
+				if (!importPortletSetup &&
+					!importedPortletIds.contains(portletId)) {
+
+					removePortletFromLayoutType(portletId, layoutType);
+				}
+			}
+
 			importedPortletIds.removeAll(layoutType.getPortletIds());
+
+			// Delete already removed portlet instances
 
 			if (!importedPortletIds.isEmpty()) {
 				PortletLocalServiceUtil.deletePortlets(
@@ -1133,7 +1188,8 @@ public class LayoutStagedModelDataHandler
 					importedLayout.getPlid());
 			}
 
-			importedLayout.setTypeSettings(layout.getTypeSettings());
+			importedLayout.setTypeSettingsProperties(
+				layoutType.getTypeSettingsProperties());
 		}
 		finally {
 			layout.setGroupId(groupId);
