a461a6873bc96d1bc8e6e304fcab97b9cd8f0065
Laszlo Csontos <laszlo.csontos@liferay.com>
2013-11-25 15:26:03 +0100

    LPS-39528 Extend testcase to take virtual layouts into account as well

--- portal-impl/test/integration/com/liferay/portal/struts/FindActionTest.java
+++ portal-impl/test/integration/com/liferay/portal/struts/FindActionTest.java
@@ -16,9 +16,11 @@ package com.liferay.portal.struts;
 
 import com.liferay.portal.NoSuchLayoutException;
 import com.liferay.portal.kernel.test.ExecutionTestListeners;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.PortletConstants;
+import com.liferay.portal.model.impl.VirtualLayout;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.service.ServiceTestUtil;
@@ -36,9 +38,13 @@ import com.liferay.portlet.blogs.util.BlogsTestUtil;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockHttpServletRequest;
 
 /**
  * @author Julio Camarero
@@ -55,7 +61,7 @@ public class FindActionTest {
 
 	@Test
 	public void testGetPlidAndPortletIdViewInContext() throws Exception {
-		addLayouts(true);
+		addLayouts(true, false);
 
 		Object[] plidAndPorltetId = FindAction.getPlidAndPortletId(
 			getThemeDisplay(), _blogsEntry.getGroupId(), _assetLayout.getPlid(),
@@ -69,7 +75,7 @@ public class FindActionTest {
 	public void testGetPlidAndPortletIdWhenPortletDoesNotExist()
 		throws Exception {
 
-		addLayouts(false);
+		addLayouts(false, false);
 
 		try {
 			FindAction.getPlidAndPortletId(
@@ -82,7 +88,39 @@ public class FindActionTest {
 		}
 	}
 
-	protected void addLayouts(boolean portletExists) throws Exception {
+	@Test
+	public void testSetTargetGroupWithDifferentGroup() throws Exception {
+		addLayouts(true, true);
+
+		HttpServletRequest request = getHttpServletRequest();
+
+		FindAction.setTargetGroup(
+			request, _blogsEntry.getGroupId(), _blogLayout.getPlid());
+
+		Layout layout = (Layout)request.getAttribute(WebKeys.LAYOUT);
+
+		Assert.assertTrue(layout instanceof VirtualLayout);
+		Assert.assertNotEquals(_group.getGroupId(), layout.getGroupId());
+	}
+
+	@Test
+	public void testSetTargetGroupWithSameGroup() throws Exception {
+		addLayouts(true, false);
+
+		HttpServletRequest request = getHttpServletRequest();
+
+		FindAction.setTargetGroup(
+			request, _blogsEntry.getGroupId(), _blogLayout.getPlid());
+
+		Layout layout = (Layout)request.getAttribute(WebKeys.LAYOUT);
+
+		Assert.assertNull(layout);
+	}
+
+	protected void addLayouts(
+			boolean portletExists, boolean blogEntryWithAlternateGroup)
+		throws Exception {
+
 		_group = GroupTestUtil.addGroup();
 
 		_blogLayout = LayoutTestUtil.addLayout(_group.getGroupId(), "Blog");
@@ -104,8 +142,24 @@ public class FindActionTest {
 			TestPropsValues.getUserId(), _assetLayout, _assetPublisherPortletId,
 			"column-1", preferenceMap);
 
+		Group blogEntryGroup = _group;
+
+		if (blogEntryWithAlternateGroup) {
+			blogEntryGroup = GroupTestUtil.addGroup();
+		}
+
 		_blogsEntry = BlogsTestUtil.addEntry(
-			TestPropsValues.getUserId(), _group, true);
+			TestPropsValues.getUserId(), blogEntryGroup, true);
+	}
+
+	protected HttpServletRequest getHttpServletRequest() throws Exception {
+		HttpServletRequest request = new MockHttpServletRequest();
+
+		ThemeDisplay themeDisplay = getThemeDisplay();
+
+		request.setAttribute(WebKeys.THEME_DISPLAY, themeDisplay);
+
+		return request;
 	}
 
 	protected ThemeDisplay getThemeDisplay() throws Exception {
