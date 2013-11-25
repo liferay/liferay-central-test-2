ce72454fcfe16fa64fa821cf50542889a7bceba6
Eduardo Garcia <eduardo.garcia@liferay.com>
2013-11-25 12:45:16 +0100

    LPS-42402 Move common lastPublishDate logic to util method

--- portal-impl/src/com/liferay/portal/staging/StagingImpl.java
+++ portal-impl/src/com/liferay/portal/staging/StagingImpl.java
@@ -1770,6 +1770,10 @@ public class StagingImpl implements Staging {
 			long groupId, boolean privateLayout, Date lastPublishDate)
 		throws Exception {
 
+		if (lastPublishDate == null) {
+			lastPublishDate = new Date();
+		}
+
 		LayoutSet layoutSet = LayoutSetLocalServiceUtil.getLayoutSet(
 			groupId, privateLayout);
 
