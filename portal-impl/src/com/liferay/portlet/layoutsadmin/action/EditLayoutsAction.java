58cacc1552d83c6ee6a1fd0aa6179e348b6bfa19
ealonso <eudaldo.alonso@liferay.com>
2013-11-28 17:10:35 +0100

    LPS-39185 Uses the default value when the property is not set

--- portal-impl/src/com/liferay/portlet/layoutsadmin/action/EditLayoutsAction.java
+++ portal-impl/src/com/liferay/portlet/layoutsadmin/action/EditLayoutsAction.java
@@ -767,18 +767,12 @@ public class EditLayoutsAction extends PortletAction {
 		for (String key : themeSettings.keySet()) {
 			ThemeSetting themeSetting = themeSettings.get(key);
 
-			String value = null;
+			String property =
+				device + "ThemeSettingsProperties--" + key +
+					StringPool.DOUBLE_DASH;
 
-			if (!themeId.equals(deviceThemeId)) {
-				value = themeSetting.getValue();
-			}
-			else {
-				String property =
-					device + "ThemeSettingsProperties--" + key +
-						StringPool.DOUBLE_DASH;
-
-				value = ParamUtil.getString(actionRequest, property);
-			}
+			String value = ParamUtil.getString(
+				actionRequest, property, themeSetting.getValue());
 
 			if (!Validator.equals(
 					value, layoutSet.getThemeSetting(key, device))) {
