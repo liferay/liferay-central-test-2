892c663a023f573e99db192f9d4e7969679376e9
Julio Camarero <julio.camarero@liferay.com>
2013-11-29 16:27:13 +0100

    LPS-42645 remove methods because we can use the parent one (the only difference is the forward on error but it is ok now too)

--- portal-impl/src/com/liferay/portlet/portalsettings/action/EditCompanyLogoAction.java
+++ portal-impl/src/com/liferay/portlet/portalsettings/action/EditCompanyLogoAction.java
@@ -14,23 +14,9 @@
 
 package com.liferay.portlet.portalsettings.action;
 
-import com.liferay.portal.ImageTypeException;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.repository.model.FileEntry;
-import com.liferay.portal.kernel.servlet.SessionErrors;
-import com.liferay.portal.kernel.servlet.SessionMessages;
-import com.liferay.portal.kernel.upload.UploadException;
-import com.liferay.portal.kernel.util.Constants;
-import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.security.auth.PrincipalException;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.WebKeys;
-import com.liferay.portlet.documentlibrary.FileSizeException;
-import com.liferay.portlet.documentlibrary.NoSuchFileException;
 
-import javax.portlet.ActionRequest;
-import javax.portlet.ActionResponse;
 import javax.portlet.PortletConfig;
 import javax.portlet.PortletRequest;
 import javax.portlet.RenderRequest;
@@ -44,53 +30,6 @@ import org.apache.struts.action.ActionMapping;
  * @author Brian Wing Shun Chan
  */
 public class EditCompanyLogoAction extends EditLogoAction {
-
-	@Override
-	public void processAction(
-			ActionMapping actionMapping, ActionForm actionForm,
-			PortletConfig portletConfig, ActionRequest actionRequest,
-			ActionResponse actionResponse)
-		throws Exception {
-
-		try {
-			String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
-
-			if (cmd.equals(Constants.ADD_TEMP)) {
-				addTempImageFile(actionRequest);
-			}
-			else {
-				FileEntry fileEntry = saveTempImageFile(actionRequest);
-
-				SessionMessages.add(actionRequest, "imageUploaded", fileEntry);
-
-				sendRedirect(actionRequest, actionResponse);
-			}
-		}
-		catch (Exception e) {
-			if (e instanceof PrincipalException) {
-				SessionErrors.add(actionRequest, e.getClass());
-
-				setForward(actionRequest, "portlet.portal_settings.error");
-			}
-			else if (e instanceof FileSizeException ||
-					 e instanceof ImageTypeException) {
-
-				JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
-
-				jsonObject.putException(e);
-
-				writeJSON(actionRequest, actionResponse, jsonObject);
-			}
-			else if (e instanceof NoSuchFileException ||
-					 e instanceof UploadException) {
-
-				SessionErrors.add(actionRequest, e.getClass());
-			}
-			else {
-				throw e;
-			}
-		}
-	}
 
 	@Override
 	public ActionForward render(
