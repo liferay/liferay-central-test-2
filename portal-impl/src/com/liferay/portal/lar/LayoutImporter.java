01d1f2b9ca19b8af8aee746cdb3afbe83320f047
Brian Chan <brian.chan@liferay.com>
2013-12-04 09:54:17 -0800

    LPS-42288 SF

--- portal-impl/src/com/liferay/portal/lar/LayoutImporter.java
+++ portal-impl/src/com/liferay/portal/lar/LayoutImporter.java
@@ -274,7 +274,6 @@ public class LayoutImporter {
 				companyId, groupId, parameterMap, strategy, zipReader);
 
 		portletDataContext.setManifestSummary(manifestSummary);
-
 		portletDataContext.setPortetDataContextListener(
 			new PortletDataContextListenerImpl(portletDataContext));
 
