8fc2349bc7bd9f77d791b512b21b1bc3467a3e94
Eduardo Garcia <eduardo.garcia@liferay.com>
2013-12-04 17:35:19 +0100

    LPS-42389 Reload rule group instance panel after selecting a device family

---
+++ portal-impl/src/com/liferay/portlet/layoutsadmin/action/MobileDeviceRulesAction.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.layoutsadmin.action;
+
+import com.liferay.portal.NoSuchGroupException;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.security.auth.PrincipalException;
+import com.liferay.portal.struts.PortletAction;
+import com.liferay.portlet.sites.action.ActionUtil;
+
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletContext;
+import javax.portlet.PortletRequestDispatcher;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ * @author Eduardo Garcia
+ */
+public class MobileDeviceRulesAction extends PortletAction {
+
+	@Override
+	public ActionForward render(
+			ActionMapping actionMapping, ActionForm actionForm,
+			PortletConfig portletConfig, RenderRequest renderRequest,
+			RenderResponse renderResponse)
+		throws Exception {
+
+		try {
+			ActionUtil.getGroup(renderRequest);
+		}
+		catch (Exception e) {
+			if (e instanceof NoSuchGroupException ||
+				e instanceof PrincipalException) {
+
+				SessionErrors.add(renderRequest, e.getClass());
+
+				return actionMapping.findForward("portlet.layouts_admin.error");
+			}
+			else {
+				throw e;
+			}
+		}
+
+		return actionMapping.findForward("portlet.layouts_admin.edit_layouts");
+	}
+
+	@Override
+	public void serveResource(
+			ActionMapping actionMapping, ActionForm actionForm,
+			PortletConfig portletConfig, ResourceRequest resourceRequest,
+			ResourceResponse resourceResponse)
+		throws Exception {
+
+		PortletContext portletContext = portletConfig.getPortletContext();
+
+		PortletRequestDispatcher portletRequestDispatcher =
+			portletContext.getRequestDispatcher(
+				"/html/portlet/layouts_admin/layout/" +
+					"mobile_device_rules_rule_group_instances.jsp");
+
+		portletRequestDispatcher.include(resourceRequest, resourceResponse);
+	}
+
+}
