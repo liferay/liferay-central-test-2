c34b2534aeaeb2af0a161fc4bcae2f52db089739
Brian Chan <brian.chan@liferay.com>
2013-12-04 22:24:25 -0800

    LPS-42645 Ran "ant format-javadoc -Dlimit=UserLocalServiceImpl etc on every file

--- portal-impl/src/com/liferay/portal/service/impl/OrganizationServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/OrganizationServiceImpl.java
@@ -846,36 +846,37 @@ public class OrganizationServiceImpl extends OrganizationServiceBaseImpl {
 	/**
 	 * Updates the organization with additional parameters.
 	 *
-	 * @param  organizationId the primary key of the organization
-	 * @param  parentOrganizationId the primary key of the organization's parent
-	 *         organization
-	 * @param  name the organization's name
-	 * @param  type the organization's type
-	 * @param  regionId the primary key of the organization's region
-	 * @param  countryId the primary key of the organization's country
-	 * @param  statusId the organization's workflow status
-	 * @param  comments the comments about the organization
-	 * @param  site whether the organization is to be associated with a main
-	 *         site
-	 * @param  addresses the organization's addresses
-	 * @param  emailAddresses the organization's email addresses
-	 * @param  orgLabors the organization's hours of operation
-	 * @param  phones the organization's phone numbers
-	 * @param  websites the organization's websites
-	 * @param  serviceContext the service context to be applied (optionally
-	 *         <code>null</code>). Can set asset category IDs and asset tag
-	 *         names for the organization, and merge expando bridge attributes
-	 *         for the organization.
-	 * @return the organization
-	 * @throws PortalException if an organization or parent organization with
-	 *         the primary key could not be found, if the user did not have
-	 *         permission to update the organization information, or if the new
-	 *         information was invalid
-	 * @throws SystemException if a system exception occurred
-	 * @deprecated As of 7.0.0, replaced by {@link #updateOrganization(
-	 *         long, long, String, String, long, long, int, String, boolean,
-	 *         java.util.List, java.util.List, java.util.List, java.util.List,
-	 *         java.util.List, boolean, byte[], ServiceContext)}
+	 * @param      organizationId the primary key of the organization
+	 * @param      parentOrganizationId the primary key of the organization's
+	 *             parent organization
+	 * @param      name the organization's name
+	 * @param      type the organization's type
+	 * @param      regionId the primary key of the organization's region
+	 * @param      countryId the primary key of the organization's country
+	 * @param      statusId the organization's workflow status
+	 * @param      comments the comments about the organization
+	 * @param      site whether the organization is to be associated with a main
+	 *             site
+	 * @param      addresses the organization's addresses
+	 * @param      emailAddresses the organization's email addresses
+	 * @param      orgLabors the organization's hours of operation
+	 * @param      phones the organization's phone numbers
+	 * @param      websites the organization's websites
+	 * @param      serviceContext the service context to be applied (optionally
+	 *             <code>null</code>). Can set asset category IDs and asset tag
+	 *             names for the organization, and merge expando bridge
+	 *             attributes for the organization.
+	 * @return     the organization
+	 * @throws     PortalException if an organization or parent organization
+	 *             with the primary key could not be found, if the user did not
+	 *             have permission to update the organization information, or if
+	 *             the new information was invalid
+	 * @throws     SystemException if a system exception occurred
+	 * @deprecated As of 7.0.0, replaced by {@link #updateOrganization( long,
+	 *             long, String, String, long, long, int, String, boolean,
+	 *             java.util.List, java.util.List, java.util.List,
+	 *             java.util.List, java.util.List, boolean, byte[],
+	 *             ServiceContext)}
 	 */
 	@Override
 	public Organization updateOrganization(
