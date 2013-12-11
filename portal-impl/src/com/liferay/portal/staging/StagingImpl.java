fa86ff1ab41e292b822c78dedb52f427becb33a7
Tamas Molnar <tamas.molnar@liferay.com>
2013-12-11 18:14:59 +0100

    LPS-41868 Page Revision Workflow Task and Staging causes pages previously added to be in the Review Status

--- portal-impl/src/com/liferay/portal/staging/StagingImpl.java
+++ portal-impl/src/com/liferay/portal/staging/StagingImpl.java
@@ -2002,6 +2002,8 @@ public class StagingImpl implements Staging {
 			groupName);
 
 		try {
+			serviceContext.setWorkflowAction(WorkflowConstants.STATUS_APPROVED);
+
 			LayoutSetBranch layoutSetBranch =
 				LayoutSetBranchLocalServiceUtil.addLayoutSetBranch(
 					userId, groupId, privateLayout,
