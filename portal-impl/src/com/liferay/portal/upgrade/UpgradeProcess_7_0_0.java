614e72dc83c9a6d0de42c0b81cc9f76529d11b73
Shinn Lok <shinn.lok@liferay.com>
2013-12-16 11:18:55 -0800

    LPS-42901 Wrong UpgradeSchema

--- portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_7_0_0.java
+++ portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_7_0_0.java
@@ -16,7 +16,7 @@ package com.liferay.portal.upgrade;
 
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.ReleaseInfo;
-import com.liferay.portal.upgrade.v6_2_0.UpgradeSchema;
+import com.liferay.portal.upgrade.v7_0_0.UpgradeSchema;
 
 /**
  * @author Julio Camarero
