f1977429471026859056f1df7af83449715943bf
shuyangzhou <shuyang.zhou@liferay.com>
2014-01-02 22:58:17 -0800

    LPS-42599 Jsqlparser + regex is too heavy, switch to maunal parsing.

--- portal-impl/src/com/liferay/portal/dao/orm/hibernate/SQLQueryTableNamesUtil.java
+++ portal-impl/src/com/liferay/portal/dao/orm/hibernate/SQLQueryTableNamesUtil.java
@@ -16,21 +16,11 @@ package com.liferay.portal.dao.orm.hibernate;
 
 import com.liferay.portal.kernel.cache.PortalCache;
 import com.liferay.portal.kernel.cache.SingleVMPoolUtil;
-import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.portal.kernel.util.StringUtil;
 
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import net.sf.jsqlparser.JSQLParserException;
-import net.sf.jsqlparser.parser.CCJSqlParserManager;
-import net.sf.jsqlparser.parser.JSqlParser;
-import net.sf.jsqlparser.statement.Statement;
-import net.sf.jsqlparser.statement.select.Select;
-import net.sf.jsqlparser.util.TablesNamesFinder;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Shuyang Zhou
@@ -44,98 +34,80 @@ public class SQLQueryTableNamesUtil {
 			return tableNames;
 		}
 
-		Statement statement = null;
+		String lowerCaseSQL = StringUtil.toLowerCase(sql);
 
-		try {
-			statement = _jSqlParser.parse(
-				new UnsyncStringReader(_escapeSQL(sql)));
+		Set<String> tableNameSet = new HashSet<String>();
+
+		// Find table name from "from" clause
+
+		int index = 0;
+
+		while ((index = lowerCaseSQL.indexOf(" from ", index)) != -1) {
+			index += 6;
+
+			int[] indexes = findWord(lowerCaseSQL, index);
+
+			if (indexes != null) {
+				tableNameSet.add(sql.substring(indexes[0], indexes[1]));
+			}
 		}
-		catch (JSQLParserException jsqlpe) {
-			_log.error("Unable to parse SQL: " + sql, jsqlpe);
+
+		// Find table name from "join" clause
+
+		index = 0;
+
+		while ((index = lowerCaseSQL.indexOf(" join ", index)) != -1) {
+			index += 6;
+
+			int[] indexes = findWord(lowerCaseSQL, index);
+
+			if (indexes != null) {
+				tableNameSet.add(sql.substring(indexes[0], indexes[1]));
+			}
 		}
 
-		if ((statement == null) || !(statement instanceof Select)) {
-			tableNames = new String[0];
-
-			_portalCache.put(sql, tableNames);
-
-			return tableNames;
-		}
-
-		TablesNamesFinder tablesNamesFinder = new TablesNamesFinder();
-
-		List<String> tableNameList = tablesNamesFinder.getTableList(
-			(Select)statement);
-
-		tableNames = tableNameList.toArray(new String[tableNameList.size()]);
+		tableNames = tableNameSet.toArray(new String[tableNameSet.size()]);
 
 		_portalCache.put(sql, tableNames);
 
 		return tableNames;
 	}
 
-	private static String _escapeSQL(String sql) {
-		sql = sql.trim();
+	private static int[] findWord(String sql, int index) {
+		int start = -1;
+		int end = sql.length();
 
-		if ((sql.length() > 1) &&
-			(sql.charAt(0) == CharPool.OPEN_PARENTHESIS)) {
+		for (int i = index; i < sql.length(); i++) {
+			char c = sql.charAt(i);
 
-			int closeIndex = sql.lastIndexOf(CharPool.CLOSE_PARENTHESIS);
+			if (c == CharPool.OPEN_PARENTHESIS) {
 
-			int openCount = 1;
+				// Subquery in clause, no need to parse table name.
 
-			for (int i = 1; i < closeIndex; i++) {
-				if (sql.charAt(i) == CharPool.OPEN_PARENTHESIS) {
-					openCount++;
-				}
+				break;
+			}
+			else if ((c == CharPool.SPACE) ||
+					 (c == CharPool.CLOSE_PARENTHESIS)) {
 
-				if (sql.charAt(i) == CharPool.CLOSE_PARENTHESIS) {
-					openCount--;
+				if (start != -1) {
+					end = i;
 
-					if (openCount == 0) {
-						break;
-					}
+					break;
 				}
 			}
-
-			if (openCount == 1) {
-				String body = sql.substring(1, closeIndex);
-
-				if (closeIndex < (sql.length() - 1)) {
-					String tail = sql.substring(closeIndex + 1);
-
-					sql = body.concat(tail);
-				}
-				else {
-					sql = body;
-				}
+			else if (start == -1) {
+				start = i;
 			}
 		}
 
-		Matcher matcher = _tableNamePattern.matcher(sql);
+		if (start == -1) {
+			return null;
+		}
 
-		sql = matcher.replaceAll("$1");
-
-		matcher = _modPattern.matcher(sql);
-
-		sql = matcher.replaceAll("1");
-
-		matcher = _valuePattern.matcher(sql);
-
-		return matcher.replaceAll("value_");
+		return new int[] {start, end};
 	}
 
-	private static Log _log = LogFactoryUtil.getLog(
-		SQLQueryTableNamesUtil.class);
-
-	private static JSqlParser _jSqlParser = new CCJSqlParserManager();
-	private static Pattern _modPattern = Pattern.compile(
-		"MOD\\((.+?),(.+?)\\)", Pattern.CASE_INSENSITIVE);
 	private static PortalCache<String, String[]> _portalCache =
 		SingleVMPoolUtil.getCache(SQLQueryTableNamesUtil.class.getName());
-	private static Pattern _tableNamePattern = Pattern.compile(
-		"\\{(\\w+\\.\\*)\\}", Pattern.CASE_INSENSITIVE);
-	private static Pattern _valuePattern = Pattern.compile(
-		"\\bvalue\\b", Pattern.CASE_INSENSITIVE);
 
 }
---
+++ portal-impl/test/unit/com/liferay/portal/dao/orm/hibernate/SQLQueryTableNamesUtilTest.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.dao.orm.hibernate;
+
+import com.liferay.portal.cache.SingleVMPoolImpl;
+import com.liferay.portal.cache.memory.MemoryPortalCacheManager;
+import com.liferay.portal.kernel.cache.SingleVMPoolUtil;
+import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
+import com.liferay.portal.kernel.test.CodeCoverageAssertor;
+import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.io.InputStreamReader;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+/**
+ * @author Shuyang Zhou
+ */
+public class SQLQueryTableNamesUtilTest {
+
+	@ClassRule
+	public static CodeCoverageAssertor codeCoverageAssertor =
+		new CodeCoverageAssertor();
+
+	@Before
+	public void setUp() {
+		MemoryPortalCacheManager<String, String> memoryPortalCacheManager =
+			new MemoryPortalCacheManager<String, String>();
+
+		memoryPortalCacheManager.afterPropertiesSet();
+
+		SingleVMPoolImpl singleVMPoolImpl = new SingleVMPoolImpl();
+
+		singleVMPoolImpl.setPortalCacheManager(memoryPortalCacheManager);
+
+		SingleVMPoolUtil singleVMPoolUtil = new SingleVMPoolUtil();
+
+		singleVMPoolUtil.setSingleVMPool(singleVMPoolImpl);
+	}
+
+	@Test
+	public void testGetTableNames() throws Exception {
+		UnsyncBufferedReader unsyncBufferedReader =
+			new UnsyncBufferedReader(
+				new InputStreamReader(
+					SQLQueryTableNamesUtilTest.class.getResourceAsStream(
+						"dependencies/sql.txt")));
+
+		String line = null;
+
+		while ((line = unsyncBufferedReader.readLine()) != null) {
+			int index = line.indexOf(CharPool.POUND);
+
+			if (index != -1) {
+				String sql = line.substring(0, index);
+
+				String[] expectedTableNames = StringUtil.split(
+					line.substring(index + 1), CharPool.COMMA);
+
+				String[] actualTableNames =
+					SQLQueryTableNamesUtil.getTableNames(sql);
+
+				Arrays.sort(actualTableNames);
+
+				Assert.assertArrayEquals(
+					"For sql " + sql, expectedTableNames, actualTableNames);
+
+				// Try again to hit the cache
+
+				actualTableNames = SQLQueryTableNamesUtil.getTableNames(sql);
+
+				Arrays.sort(actualTableNames);
+
+				Assert.assertArrayEquals(
+					"For sql " + sql, expectedTableNames, actualTableNames);
+			}
+		}
+
+		unsyncBufferedReader.close();
+	}
+
+}
