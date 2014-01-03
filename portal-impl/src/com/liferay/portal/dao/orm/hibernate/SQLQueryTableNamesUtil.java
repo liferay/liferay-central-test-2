0a44176cd5ab735db73d486e62596f114a787046
Brian Chan <brian.chan@liferay.com>
2014-01-03 15:18:15 -0800

    LPS-42599 SF

--- portal-impl/src/com/liferay/portal/dao/orm/hibernate/SQLQueryTableNamesUtil.java
+++ portal-impl/src/com/liferay/portal/dao/orm/hibernate/SQLQueryTableNamesUtil.java
@@ -82,8 +82,8 @@ public class SQLQueryTableNamesUtil {
 
 			if (c == CharPool.OPEN_PARENTHESIS) {
 
-				// There is a subquery in current clause, no need to parse for
-				// table name
+				// There is a subquery in the current clause, so no need to
+				// parse for a table name
 
 				break;
 			}
