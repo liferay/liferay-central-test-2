221151c23fb7277fbe5771f01cee8ec9e0f77d30
Brian Chan <brian.chan@liferay.com>
2014-01-06 13:26:04 -0800

    LPS-43287 Readd method, since it may be used by plugins

--- portal-impl/src/com/liferay/portal/events/EventsProcessorUtil.java
+++ portal-impl/src/com/liferay/portal/events/EventsProcessorUtil.java
@@ -73,6 +73,10 @@ public class EventsProcessorUtil {
 		_instance.registerEvent(key, event);
 	}
 
+	public static void setEventsProcessor(EventsProcessor eventsProcessor) {
+		_instance = eventsProcessor;
+	}
+
 	public static void unregisterEvent(String key, Object event) {
 		_instance.unregisterEvent(key, event);
 	}
