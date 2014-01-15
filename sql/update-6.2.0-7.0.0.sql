913645e50cdfed9ac4f487f7b0fa5a74dcb81bd2
Daniel Kocsis <daniel.kocsis@liferay.com>
2014-01-15 16:42:30 +0100

    LPS-43621 Add table creation to the update script

--- sql/update-6.2.0-7.0.0.sql
+++ sql/update-6.2.0-7.0.0.sql
@@ -1,3 +1,17 @@
+create table ExportImportConfiguration (
+	exportImportConfigurationId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	name VARCHAR(75) null,
+	description STRING null,
+	type_ INTEGER,
+	settings TEXT null
+);
+
 alter table Layout drop column iconImage;
 
 alter table LayoutRevision drop column iconImage;
