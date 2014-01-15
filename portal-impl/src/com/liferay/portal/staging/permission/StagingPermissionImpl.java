7bf32221fd3aaa1a388bf9bcf273eb9dcc8873af
Brian Chan <brian.chan@liferay.com>
2014-01-15 14:48:57 -0800

    LPS-43074 SF

--- portal-impl/src/com/liferay/portal/staging/permission/StagingPermissionImpl.java
+++ portal-impl/src/com/liferay/portal/staging/permission/StagingPermissionImpl.java
@@ -71,11 +71,11 @@ public class StagingPermissionImpl implements StagingPermission {
 			long classPK, String portletId, String actionId)
 		throws Exception {
 
-		if (!actionId.equals(ActionKeys.VIEW) &&
-			!actionId.equals(ActionKeys.DELETE) &&
+		if (!actionId.equals(ActionKeys.ADD_TO_PAGE) &&
 			!actionId.equals(ActionKeys.CONFIGURATION) &&
 			!actionId.equals(ActionKeys.CUSTOMIZE) &&
-			!actionId.equals(ActionKeys.ADD_TO_PAGE) &&
+			!actionId.equals(ActionKeys.DELETE) &&
+			!actionId.equals(ActionKeys.VIEW) &&
 			group.hasLocalOrRemoteStagingGroup() &&
 			(Validator.isNull(portletId) || group.isStagedPortlet(portletId))) {
 
