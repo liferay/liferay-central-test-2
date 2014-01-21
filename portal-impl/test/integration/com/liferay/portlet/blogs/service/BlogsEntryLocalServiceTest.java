8539ef1e6f70ebd7dcf8001f2679b4f70c7d8888
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-01-21 13:30:47 +0100

    LPS-43495 SF

--- portal-impl/test/integration/com/liferay/portlet/blogs/service/BlogsEntryLocalServiceTest.java
+++ portal-impl/test/integration/com/liferay/portlet/blogs/service/BlogsEntryLocalServiceTest.java
@@ -44,6 +44,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 @ExecutionTestListeners(
 	listeners = {
 		MainServletExecutionTestListener.class,
