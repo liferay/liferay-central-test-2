44b9a8c5ac2cc5810b5b6a81d681962bd825b6ca
Chema Balsas <jose.balsas@liferay.com>
2014-01-23 12:09:52 +0100

    Revert "LPS-43250 Position modal resize handles based on language direction"
    This reverts several commits:
    966dc19d612290e24bef012ac2041cc8e53346e3.
    b44b1f35b1db2152c2bbc8bdc692b6bc4adcc075.
    af39478be621fad39fe12ef4a3c4d0f8c408e484.

--- portal-web/docroot/html/js/liferay/util_window.js
+++ portal-web/docroot/html/js/liferay/util_window.js
@@ -41,25 +41,13 @@ AUI.add(
 			}
 		);
 
-		var MAP_DIR = Liferay.Language.direction;
-
-		var DIR = (MAP_DIR && MAP_DIR[themeDisplay.getLanguageId()]) || 'ltr';
-
-		var RESIZE_HANDLES = {
-			ltr: 'br',
-			rtl: 'bl'
-		};
-
 		A.mix(
 			Window,
 			{
 				DEFAULTS: {
 					centered: true,
-					headerContent: '&nbsp;',
 					modal: true,
-					resizable: {
-						handles: RESIZE_HANDLES[DIR]
-					},
+					headerContent: '&nbsp;',
 					visible: true,
 					zIndex: Liferay.zIndex.WINDOW
 				},
