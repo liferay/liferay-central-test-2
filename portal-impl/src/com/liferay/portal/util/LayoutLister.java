b7977ebf35f0ab3e95d6a21edee7791c139cd460
shuyangzhou <shuyang.zhou@liferay.com>
2014-01-27 17:58:54 -0800

    LPS-42295 Revert, this can be done in a cleaner way.

--- portal-impl/src/com/liferay/portal/util/LayoutLister.java
+++ portal-impl/src/com/liferay/portal/util/LayoutLister.java
@@ -14,94 +14,23 @@
 
 package com.liferay.portal.util;
 
-import com.liferay.portal.kernel.cache.Lifecycle;
-import com.liferay.portal.kernel.cache.ThreadLocalCache;
-import com.liferay.portal.kernel.cache.ThreadLocalCacheManager;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.StringBundler;
-import com.liferay.portal.kernel.util.StringPool;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.LayoutConstants;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
 
 import java.util.ArrayList;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
-
-import org.apache.commons.lang.time.StopWatch;
 
 /**
  * @author Brian Wing Shun Chan
- * @author László Csontos
  */
 public class LayoutLister {
 
 	public LayoutView getLayoutView(
-			long groupId, boolean privateLayout, String rootNodeName,
-			Locale locale)
-		throws PortalException, SystemException {
-
-		StopWatch stopWatch = null;
-
-		if (_log.isDebugEnabled()) {
-			stopWatch = new StopWatch();
-
-			stopWatch.start();
-		}
-
-		ThreadLocalCache<LayoutView> threadLocalCache =
-			ThreadLocalCacheManager.getThreadLocalCache(
-				Lifecycle.REQUEST, LayoutLister.class.getName());
-
-		String key = getKey(groupId, privateLayout, rootNodeName, locale);
-
-		LayoutView layoutView = threadLocalCache.get(key);
-
-		if (layoutView == null) {
-			layoutView = doGetLayoutView(
-				groupId, privateLayout, rootNodeName, locale);
-
-			threadLocalCache.put(key, layoutView);
-		}
-
-		if (_log.isDebugEnabled()) {
-			stopWatch.stop();
-
-			_log.debug("Runtime: " + stopWatch.getTime());
-		}
-
-		return layoutView;
-	}
-
-	protected void createList(Map<Long, Deque<Layout>> layoutsBag) {
-		Deque<LayoutNode> layoutNodeStack = new LinkedList<LayoutNode>();
-
-		pushLayouts(
-			layoutNodeStack, layoutsBag,
-			LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, 1, _nodeId);
-
-		while (!layoutNodeStack.isEmpty()) {
-			LayoutNode layoutNode = layoutNodeStack.pop();
-
-			visitLayout(layoutNode);
-
-			Layout layout = layoutNode.getLayout();
-
-			pushLayouts(
-				layoutNodeStack, layoutsBag, layout.getLayoutId(),
-				layoutNode.getDepth() + 1, _nodeId);
-		}
-	}
-
-	protected LayoutView doGetLayoutView(
 			long groupId, boolean privateLayout, String rootNodeName,
 			Locale locale)
 		throws PortalException, SystemException {
@@ -111,193 +40,75 @@ public class LayoutLister {
 
 		_list = new ArrayList<String>();
 
-		StringBundler sb = new StringBundler(5);
-
-		sb.append("1|0|0|");
-		sb.append(LayoutConstants.DEFAULT_PLID);
-		sb.append("|");
-		sb.append(rootNodeName);
-		sb.append("|0");
-
-		_list.add(sb.toString());
+		_list.add(
+			"1|0|0|" + LayoutConstants.DEFAULT_PLID + "|" + rootNodeName +
+				"|0");
 
 		List<Layout> layouts = LayoutLocalServiceUtil.getLayouts(
 			groupId, privateLayout);
 
-		Map<Long, Deque<Layout>> layoutsBag = getLayoutsBag(layouts);
-
-		createList(layoutsBag);
+		_createList(
+			layouts, LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, _nodeId, 0);
 
 		return new LayoutView(_list, _depth);
 	}
 
-	protected String getKey(
-		long groupId, boolean privateLayout, String rootNodeName,
-		Locale locale) {
+	private void _createList(
+			List<Layout> layouts, long parentLayoutId, int parentId, int depth)
+		throws PortalException, SystemException {
 
-		StringBundler sb = new StringBundler(7);
-
-		sb.append(StringUtil.toHexString(groupId));
-		sb.append(StringPool.POUND);
-		sb.append(privateLayout);
-		sb.append(StringPool.POUND);
-		sb.append(rootNodeName);
-		sb.append(StringPool.POUND);
-
-		if (locale != null) {
-			sb.append(locale.toString());
-		}
-		else {
-			sb.append(StringPool.NULL);
-		}
-
-		return sb.toString();
-	}
-
-	protected Map<Long, Deque<Layout>> getLayoutsBag(List<Layout> layouts) {
-		Map<Long, Deque<Layout>> layoutsBag =
-			new HashMap<Long, Deque<Layout>>();
+		List<Layout> matchedLayouts = new ArrayList<Layout>();
 
 		for (Layout layout : layouts) {
-			long parentLayoutId = layout.getParentLayoutId();
+			if (layout.getParentLayoutId() == parentLayoutId) {
+				matchedLayouts.add(layout);
+			}
+		}
 
-			Deque<Layout> matchedLayouts = layoutsBag.get(parentLayoutId);
+		for (int i = 0; i < matchedLayouts.size(); i++) {
+			Layout layout = matchedLayouts.get(i);
 
-			if (matchedLayouts == null) {
-				matchedLayouts = new LinkedList<Layout>();
+			if (i == 0) {
+				depth++;
 
-				layoutsBag.put(parentLayoutId, matchedLayouts);
+				if (depth > _depth) {
+					_depth = depth;
+				}
 			}
 
-			matchedLayouts.offer(layout);
-		}
+			StringBundler sb = new StringBundler(13);
 
-		return layoutsBag;
-	}
+			sb.append(++_nodeId);
+			sb.append("|");
+			sb.append(parentId);
+			sb.append("|");
 
-	protected void pushLayouts(
-		Deque<LayoutNode> layoutNodeStack, Map<Long, Deque<Layout>> layoutsBag,
-		long parentLayoutId, int depth, int parentId) {
+			if ((i + 1) == matchedLayouts.size()) {
+				sb.append("1");
+			}
+			else {
+				sb.append("0");
+			}
 
-		Deque<Layout> matchedLayouts = layoutsBag.get(parentLayoutId);
+			sb.append("|");
+			sb.append(layout.getPlid());
+			sb.append("|");
+			sb.append(layout.getName(_locale));
+			sb.append("|");
+			//sb.append("9");
+			sb.append("11");
+			sb.append("|");
+			sb.append(depth);
 
-		if ((matchedLayouts == null) || matchedLayouts.isEmpty()) {
-			return;
-		}
+			_list.add(sb.toString());
 
-		int siblingCount = matchedLayouts.size();
-
-		int index = siblingCount;
-
-		Layout layout = null;
-
-		while ((layout = matchedLayouts.pollLast()) != null) {
-			LayoutNode layoutNode = new LayoutNode(
-				layout, --index, siblingCount, depth, parentId);
-
-			layoutNodeStack.push(layoutNode);
-		}
-
-		if (depth > _depth) {
-			_depth = depth;
+			_createList(layouts, layout.getLayoutId(), _nodeId, depth);
 		}
 	}
-
-	protected void visitLayout(LayoutNode layoutNode) {
-		StringBundler sb = new StringBundler(13);
-
-		sb.append(++_nodeId);
-		sb.append("|");
-		sb.append(layoutNode.getParentId());
-		sb.append("|");
-
-		if ((layoutNode.getIndex() + 1) == layoutNode.getSiblingCount()) {
-			sb.append("1");
-		}
-		else {
-			sb.append("0");
-		}
-
-		Layout layout = layoutNode.getLayout();
-
-		sb.append("|");
-		sb.append(layout.getPlid());
-		sb.append("|");
-		sb.append(layout.getName(_locale));
-		sb.append("|");
-		sb.append("11");
-		sb.append("|");
-		sb.append(layoutNode.getDepth());
-
-		_list.add(sb.toString());
-	}
-
-	private static Log _log = LogFactoryUtil.getLog(LayoutLister.class);
 
 	private int _depth;
 	private List<String> _list;
 	private Locale _locale;
 	private int _nodeId;
-
-	private class LayoutNode {
-
-		public LayoutNode(
-			Layout layout, int index, int siblingCount, int depth,
-			int parentId) {
-
-			_depth = depth;
-			_index = index;
-			_layout = layout;
-			_parentId = parentId;
-			_siblingCount = siblingCount;
-		}
-
-		public int getDepth() {
-			return _depth;
-		}
-
-		public int getIndex() {
-			return _index;
-		}
-
-		public Layout getLayout() {
-			return _layout;
-		}
-
-		public int getParentId() {
-			return _parentId;
-		}
-
-		public int getSiblingCount() {
-			return _siblingCount;
-		}
-
-		public void setDepth(int depth) {
-			_depth = depth;
-		}
-
-		public void setIndex(int index) {
-			_index = index;
-		}
-
-		public void setLayout(Layout layout) {
-			_layout = layout;
-		}
-
-		public void setParentId(int parentId) {
-			_parentId = parentId;
-		}
-
-		public void setSiblingCount(int siblingCount) {
-			_siblingCount = siblingCount;
-		}
-
-		private int _depth;
-		private int _index;
-		private Layout _layout;
-		private int _parentId;
-		private int _siblingCount;
-
-	}
 
 }
