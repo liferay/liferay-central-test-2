44a45dcff7827fc6513e0c36acacfdd143d436c2
Daniel Kocsis <daniel.kocsis@liferay.com>
2014-01-30 16:23:11 +0100

    LPS-43842 Add MBDiscussionStagedModelDataHandler

---
+++ portal-impl/src/com/liferay/portlet/messageboards/lar/MBDiscussionStagedModelDataHandler.java
@@ -0,0 +1,140 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.messageboards.lar;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.lar.BaseStagedModelDataHandler;
+import com.liferay.portal.kernel.lar.ExportImportPathUtil;
+import com.liferay.portal.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portal.model.Layout;
+import com.liferay.portal.util.PropsValues;
+import com.liferay.portlet.asset.model.AssetEntry;
+import com.liferay.portlet.asset.service.AssetEntryLocalServiceUtil;
+import com.liferay.portlet.messageboards.model.MBDiscussion;
+import com.liferay.portlet.messageboards.model.MBMessage;
+import com.liferay.portlet.messageboards.model.MBThread;
+import com.liferay.portlet.messageboards.service.MBDiscussionLocalServiceUtil;
+import com.liferay.portlet.messageboards.service.MBMessageLocalServiceUtil;
+
+import java.util.Map;
+
+/**
+ * @author Daniel Kocsis
+ */
+public class MBDiscussionStagedModelDataHandler
+	extends BaseStagedModelDataHandler<MBDiscussion> {
+
+	public static final String[] CLASS_NAMES = {MBDiscussion.class.getName()};
+
+	@Override
+	public void deleteStagedModel(
+			String uuid, long groupId, String className, String extraData)
+		throws PortalException, SystemException {
+
+		MBDiscussion discussion =
+			MBDiscussionLocalServiceUtil.fetchMBDiscussionByUuidAndGroupId(
+				uuid, groupId);
+
+		if (discussion != null) {
+			MBDiscussionLocalServiceUtil.deleteMBDiscussion(discussion);
+		}
+	}
+
+	@Override
+	public String[] getClassNames() {
+		return CLASS_NAMES;
+	}
+
+	@Override
+	public String getDisplayName(MBDiscussion discussion) {
+		try {
+			AssetEntry assetEntry = AssetEntryLocalServiceUtil.getEntry(
+				discussion.getClassName(), discussion.getClassPK());
+
+			return assetEntry.getTitleCurrentValue();
+		}
+		catch (Exception e) {
+			return discussion.getUuid();
+		}
+	}
+
+	@Override
+	protected void doExportStagedModel(
+			PortletDataContext portletDataContext, MBDiscussion discussion)
+		throws Exception {
+
+		Element discussionElement = portletDataContext.getExportDataElement(
+			discussion);
+
+		portletDataContext.addClassedModel(
+			discussionElement, ExportImportPathUtil.getModelPath(discussion),
+			discussion);
+	}
+
+	@Override
+	protected void doImportStagedModel(
+			PortletDataContext portletDataContext, MBDiscussion discussion)
+		throws Exception {
+
+		long userId = portletDataContext.getUserId(discussion.getUserUuid());
+
+		String className = discussion.getClassName();
+
+		Map<Long, Long> relatedEntityIds =
+			(Map<Long, Long>)portletDataContext.getNewPrimaryKeysMap(className);
+
+		long newClassPK = MapUtil.getLong(
+			relatedEntityIds, discussion.getClassPK(), discussion.getClassPK());
+
+		MBDiscussion existingDiscussion =
+			MBDiscussionLocalServiceUtil.fetchDiscussion(
+				discussion.getClassName(), newClassPK);
+
+		if ((existingDiscussion == null) &&
+			className.equals(Layout.class.getName()) &&
+			PropsValues.LAYOUT_COMMENTS_ENABLED) {
+
+			MBMessage rootMessage =
+				MBMessageLocalServiceUtil.addDiscussionMessage(
+					userId, discussion.getUserName(),
+					portletDataContext.getScopeGroupId(),
+					Layout.class.getName(), newClassPK,
+					WorkflowConstants.ACTION_PUBLISH);
+
+			existingDiscussion =
+				MBDiscussionLocalServiceUtil.getThreadDiscussion(
+					rootMessage.getThreadId());
+		}
+
+		Map<Long, Long> discussionIds =
+			(Map<Long, Long>)portletDataContext.getNewPrimaryKeysMap(
+				MBDiscussion.class);
+
+		discussionIds.put(
+			discussion.getDiscussionId(), existingDiscussion.getDiscussionId());
+
+		Map<Long, Long> threadIds =
+			(Map<Long, Long>)portletDataContext.getNewPrimaryKeysMap(
+				MBThread.class);
+
+		threadIds.put(
+			discussion.getThreadId(), existingDiscussion.getThreadId());
+	}
+
+}
