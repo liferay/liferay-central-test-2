81e32a5b19d26660db28eb60800e69ba208c8999
Hugo Huijser <hugo.huijser@liferay.com>
2014-01-31 21:30:27 +0800

    LPS-43806 Source formatting

--- portal-impl/src/com/liferay/portal/editor/fckeditor/receiver/impl/AttachmentCommandReceiver.java
+++ portal-impl/src/com/liferay/portal/editor/fckeditor/receiver/impl/AttachmentCommandReceiver.java
@@ -148,6 +148,6 @@ public class AttachmentCommandReceiver extends BaseCommandReceiver {
 	}
 
 	private static Log _log = LogFactoryUtil.getLog(
-			AttachmentCommandReceiver.class);
+		AttachmentCommandReceiver.class);
 
 }
