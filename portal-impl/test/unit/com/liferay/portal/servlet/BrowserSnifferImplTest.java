0695cfebe2eb3ff1435de8065735e0a7fae6a486
Alberto Chaparro <alberto.chaparro@liferay.com>
2014-01-31 12:10:19 +0100

    LPS-43909 JUnit Tests

--- portal-impl/test/unit/com/liferay/portal/servlet/BrowserSnifferImplTest.java
+++ portal-impl/test/unit/com/liferay/portal/servlet/BrowserSnifferImplTest.java
@@ -83,6 +83,53 @@ public class BrowserSnifferImplTest {
 	}
 
 	@Test
+	public void testIsIe() throws IOException {
+		UnsyncBufferedReader unsyncBufferedReader =
+			getResourceAsUnsyncBufferedReader("dependencies/user_agents.csv");
+
+		boolean ie = false;
+		String line = null;
+
+		while ((line = unsyncBufferedReader.readLine()) != null) {
+			line = line.trim();
+
+			if (line.isEmpty()) {
+				continue;
+			}
+
+			if (line.contains("## IE")) {
+				ie = true;
+
+				continue;
+			}
+
+			if (ie && (line.charAt(0) == CharPool.POUND)) {
+				break;
+			}
+
+			if (ie) {
+				MockHttpServletRequest mockHttpServletRequest =
+					new MockHttpServletRequest();
+
+				mockHttpServletRequest.addHeader(HttpHeaders.USER_AGENT, line);
+
+				Assert.assertTrue(
+					_browserSnifferImpl.isIe(mockHttpServletRequest));
+			}
+		}
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		mockHttpServletRequest.addHeader(
+			HttpHeaders.USER_AGENT,
+			"Opera 12 var1, 12.14, 9.80, opera/9.80 (windows nt 6.0)" +
+				" presto/2.12.388 version/12.14");
+
+		Assert.assertFalse(_browserSnifferImpl.isIe(mockHttpServletRequest));
+	}
+
+	@Test
 	public void testIsMobile() throws IOException {
 		UnsyncBufferedReader unsyncBufferedReader =
 			getResourceAsUnsyncBufferedReader("dependencies/user_agents.csv");
