22e6f08abd115672a242b3d2c999107d31b8fd6b
Brian Chan <brian.chan@liferay.com>
2014-02-03 11:09:39 -0800

    LPS-43705 add Julio's logic

--- portal-service/src/com/liferay/portal/kernel/servlet/MultiLayoutMessages.java
+++ portal-service/src/com/liferay/portal/kernel/servlet/MultiLayoutMessages.java
@@ -15,6 +15,7 @@
 package com.liferay.portal.kernel.servlet;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -27,6 +28,8 @@ import javax.portlet.PortletSession;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import com.liferay.portal.util.PortalUtil;
+
 /**
  * @author Brian Wing Shun Chan
  * @author Shuyang Zhou
@@ -35,65 +38,154 @@ public class MultiLayoutMessages {
 
 	public static void add(PortletRequest portletRequest, Class<?> clazz) {
 		SessionMessages.add(portletRequest, clazz.getName());
+		
+		HttpServletRequest request = PortalUtil.getHttpServletRequest(
+			portletRequest);
+
+		SessionMessages.add(request, clazz.getName());
 	}
 
 	public static void add(
 		PortletRequest portletRequest, Class<?> clazz, Object value) {
 
 		SessionMessages.add(portletRequest, clazz.getName(), value);
+
+		HttpServletRequest request = PortalUtil.getHttpServletRequest(
+			portletRequest);
+
+		SessionMessages.add(request, clazz.getName(), value);
 	}
 
 	public static void add(PortletRequest portletRequest, String key) {
 		SessionMessages.add(portletRequest, key);
+
+		HttpServletRequest request = PortalUtil.getHttpServletRequest(
+			portletRequest);
+
+		SessionMessages.add(request, key);
 	}
 
 	public static void add(
 		PortletRequest portletRequest, String key, Object value) {
 
 		SessionMessages.add(portletRequest, key, value);
+
+		HttpServletRequest request = PortalUtil.getHttpServletRequest(
+			portletRequest);
+
+		SessionMessages.add(request, key, value);
 	}
 
 	public static void clear(PortletRequest portletRequest) {
 		SessionMessages.clear(portletRequest);
+
+		HttpServletRequest request = PortalUtil.getHttpServletRequest(
+			portletRequest);
+
+		SessionMessages.clear(request);
 	}
 
 	public static boolean contains(
 		PortletRequest portletRequest, Class<?> clazz) {
 
-		return SessionMessages.contains(
-			portletRequest, clazz.getName());
+		HttpServletRequest request = PortalUtil.getHttpServletRequest(
+			portletRequest);
+
+		if (SessionMessages.contains(portletRequest, clazz.getName()) ||
+			SessionMessages.contains(request, clazz.getName())) {
+				
+			return true;
+		}
+		
+		return false;
 	}
 
 	public static boolean contains(PortletRequest portletRequest, String key) {
-		return SessionMessages.contains(portletRequest, key);
+		HttpServletRequest request = PortalUtil.getHttpServletRequest(
+			portletRequest);
+
+		if (SessionMessages.contains(portletRequest, key) ||
+			SessionMessages.contains(request, key)) {
+				
+			return true;
+		}
+		
+		return false;
 	}
 
 	public static Object get(PortletRequest portletRequest, Class<?> clazz) {
-		return SessionMessages.get(portletRequest, clazz.getName());
+		Object value = SessionMessages.get(portletRequest, clazz.getName());
+		
+		if (value != null) {
+			return value;
+		}
+
+		HttpServletRequest request = PortalUtil.getHttpServletRequest(
+			portletRequest);
+		
+		return SessionMessages.get(request, clazz.getName());
 	}
 
 	public static Object get(PortletRequest portletRequest, String key) {
-		return SessionMessages.get(portletRequest, key);
+		Object value = SessionMessages.get(portletRequest, key);
+
+		if (value != null) {
+			return value;
+		}
+
+		HttpServletRequest request = PortalUtil.getHttpServletRequest(
+			portletRequest);
+		
+		return SessionMessages.get(request, key);
 	}
 
 	public static boolean isEmpty(PortletRequest portletRequest) {
-		return SessionMessages.isEmpty(portletRequest);
+		HttpServletRequest request = PortalUtil.getHttpServletRequest(
+			portletRequest);
+
+		if (SessionMessages.isEmpty(portletRequest) &&
+			SessionMessages.isEmpty(request)) {
+				
+			return true;
+		}
+		
+		return false;
 	}
 
 	public static Iterator<String> iterator(PortletRequest portletRequest) {
-		return SessionMessages.iterator(portletRequest);
+		Set<String> set = keySet(portletRequest);
+		
+		return set.iterator();
 	}
 
 	public static Set<String> keySet(PortletRequest portletRequest) {
-		return SessionMessages.keySet(portletRequest);
+		Set<String> set = new HashSet<String>();
+
+		set.addAll(SessionMessages.keySet(portletRequest));
+
+		HttpServletRequest request = PortalUtil.getHttpServletRequest(
+			portletRequest);
+
+		set.addAll(SessionMessages.keySet(request));
+
+		return Collections.unmodifiableSet(set);
 	}
 
 	public static void print(PortletRequest portletRequest) {
 		SessionMessages.print(portletRequest);
+
+		HttpServletRequest request = PortalUtil.getHttpServletRequest(
+			portletRequest);
+			
+		SessionMessages.print(request);
 	}
 
 	public static int size(PortletRequest portletRequest) {
-		return SessionMessages.size(portletRequest);
+		HttpServletRequest request = PortalUtil.getHttpServletRequest(
+			portletRequest);
+
+		return SessionMessages.size(portletRequest) +
+			SessionMessages.size(request);
 	}
 
 }
