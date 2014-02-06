d12ea449912fb468ef9fbf875ef1e78897040cf5
Máté Thurzó <mate.thurzo@liferay.com>
2014-02-06 15:23:30 +0100

    LPS-44049 Updating StagedModelTypes for RepositoryModels where needed. Portal uses DL*Constants.getClassName() in most cases when deling with DL ie.: Asset/Comments/Ratings frameworks

--- portal-impl/src/com/liferay/portal/repository/liferayrepository/model/LiferayFileEntry.java
+++ portal-impl/src/com/liferay/portal/repository/liferayrepository/model/LiferayFileEntry.java
@@ -28,6 +28,7 @@ import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.Lock;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portlet.documentlibrary.model.DLFileEntry;
+import com.liferay.portlet.documentlibrary.model.DLFileEntryConstants;
 import com.liferay.portlet.documentlibrary.model.DLFileVersion;
 import com.liferay.portlet.documentlibrary.service.DLFileVersionLocalServiceUtil;
 import com.liferay.portlet.documentlibrary.service.permission.DLFileEntryPermission;
@@ -318,7 +319,7 @@ public class LiferayFileEntry extends LiferayModel implements FileEntry {
 
 	@Override
 	public StagedModelType getStagedModelType() {
-		return new StagedModelType(FileEntry.class);
+		return new StagedModelType(DLFileEntryConstants.getClassName());
 	}
 
 	@Override
