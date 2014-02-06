f69aaaa4ed7bc49d2f627a283197f3010e1bda81
Mika Koivisto <mika.koivisto@liferay.com>
2014-02-05 16:03:12 -0800

    LPS-42551 SF

--- portal-service/src/com/liferay/portal/kernel/repository/cmis/search/CMISContainsValueExpression.java
+++ portal-service/src/com/liferay/portal/kernel/repository/cmis/search/CMISContainsValueExpression.java
@@ -30,16 +30,17 @@ public class CMISContainsValueExpression implements CMISCriterion {
 	@Override
 	public String toQueryFragment() {
 		boolean multipleTerms = _value.contains(StringPool.SPACE);
-		StringBundler sb = new StringBundler( 1 + (multipleTerms?4:0) );
 
-		if ( _value.contains(StringPool.SPACE) ) {
+		StringBundler sb = new StringBundler(1 + (multipleTerms ? 4 : 0));
+
+		if (_value.contains(StringPool.SPACE)) {
 			sb.append(StringPool.BACK_SLASH);
-		sb.append(StringPool.APOSTROPHE);
+			sb.append(StringPool.APOSTROPHE);
 		}
 
 		sb.append(_value);
 
-		if ( _value.contains(StringPool.SPACE) ) {
+		if (_value.contains(StringPool.SPACE)) {
 			sb.append(StringPool.BACK_SLASH);
 			sb.append(StringPool.APOSTROPHE);
 		}
