ea64f76124fd7b29a728e643beab9be29f998062
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2014-02-10 10:39:11 +0100

    LPS-43933 no logic changes - Copy from blogs

---
+++ portal-impl/src/com/liferay/portlet/wiki/notifications/WikiUserNotificationHandler.java
@@ -0,0 +1,155 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.blogs.notifications;
+
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.notifications.BaseUserNotificationHandler;
+import com.liferay.portal.kernel.notifications.UserNotificationDefinition;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.User;
+import com.liferay.portal.model.UserNotificationEvent;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.UserNotificationEventLocalServiceUtil;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portlet.PortletURLFactoryUtil;
+import com.liferay.portlet.blogs.model.BlogsEntry;
+import com.liferay.portlet.blogs.service.BlogsEntryLocalServiceUtil;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+import javax.portlet.WindowState;
+
+/**
+ * @author Sergio González
+ */
+public class BlogsUserNotificationHandler extends BaseUserNotificationHandler {
+
+	public BlogsUserNotificationHandler() {
+		setPortletId(PortletKeys.BLOGS);
+	}
+
+	@Override
+	protected String getBody(
+		UserNotificationEvent userNotificationEvent,
+		ServiceContext serviceContext)
+		throws Exception {
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+			userNotificationEvent.getPayload());
+
+		long classPK = jsonObject.getLong("classPK");
+
+		BlogsEntry entry = BlogsEntryLocalServiceUtil.fetchBlogsEntry(classPK);
+
+		if (entry == null) {
+			UserNotificationEventLocalServiceUtil.deleteUserNotificationEvent(
+				userNotificationEvent.getUserNotificationEventId());
+
+			return null;
+		}
+
+		int notificationType = jsonObject.getInt("notificationType");
+
+		String title = StringPool.BLANK;
+
+		if (notificationType ==
+			UserNotificationDefinition.NOTIFICATION_TYPE_ADD_ENTRY) {
+
+			title = "x-wrote-a-new-blog-entry";
+		}
+		else if (notificationType ==
+			UserNotificationDefinition.NOTIFICATION_TYPE_UPDATE_ENTRY) {
+
+			title = "x-updated-a-blog-entry";
+		}
+
+		StringBundler sb = new StringBundler(5);
+
+		sb.append("<div class=\"title\">");
+		sb.append(
+			serviceContext.translate(
+				title,
+				HtmlUtil.escape(
+					PortalUtil.getUserName(
+						entry.getUserId(), StringPool.BLANK))));
+		sb.append("</div><div class=\"body\">");
+		sb.append(HtmlUtil.escape(StringUtil.shorten(entry.getTitle(), 50)));
+		sb.append("</div>");
+
+		return sb.toString();
+	}
+
+	@Override
+	protected String getLink(
+		UserNotificationEvent userNotificationEvent,
+		ServiceContext serviceContext)
+		throws Exception {
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+			userNotificationEvent.getPayload());
+
+		long classPK = jsonObject.getLong("classPK");
+
+		BlogsEntry entry = BlogsEntryLocalServiceUtil.fetchBlogsEntry(classPK);
+
+		if (entry == null) {
+			return null;
+		}
+
+		ThemeDisplay themeDisplay = serviceContext.getThemeDisplay();
+
+		User user = themeDisplay.getUser();
+
+		Group group = user.getGroup();
+
+		long portletPlid = PortalUtil.getPlidFromPortletId(
+			group.getGroupId(), true, PortletKeys.BLOGS);
+
+		PortletURL portletURL = null;
+
+		if (portletPlid != 0) {
+			portletURL = PortletURLFactoryUtil.create(
+				serviceContext.getLiferayPortletRequest(), PortletKeys.BLOGS,
+				portletPlid, PortletRequest.RENDER_PHASE);
+
+			portletURL.setParameter("struts_action", "/blogs/view_entry");
+			portletURL.setParameter(
+				"entryId", String.valueOf(entry.getEntryId()));
+		}
+		else {
+			LiferayPortletResponse liferayPortletResponse =
+				serviceContext.getLiferayPortletResponse();
+
+			portletURL = liferayPortletResponse.createRenderURL(
+				PortletKeys.BLOGS);
+
+			portletURL.setParameter("struts_action", "/blogs/view_entry");
+			portletURL.setParameter(
+				"entryId", String.valueOf(entry.getEntryId()));
+			portletURL.setWindowState(WindowState.MAXIMIZED);
+		}
+
+		return portletURL.toString();
+	}
+
+}
