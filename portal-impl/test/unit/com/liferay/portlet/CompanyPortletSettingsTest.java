brianchandotcom/liferay-portal

LPS-43574 Unit tests of PortletSettings subclasses
(Designed with and reviewed by @mdelapenya)

portal-impl/test/unit/com/liferay/portlet/CompanyPortletSettingsTest.java
@@ -1,0 +1,64 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet;
+
+import java.util.Properties;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Jorge Ferrer
+ */
+public class CompanyPortletSettingsTest extends BasePortletSettingsTestCase {
+
+	@Before
+	public void setUp() throws Exception {
+		portletSettings = new CompanyPortletSettings(getSettings());
+	}
+
+	@Test
+	public void testGetDefaultFromProperties() {
+		setPortalProperties(TEST_PROPERTY_NAME, TEST_PROPERTY_VALUE);
+
+		String value = portletSettings.getValue(TEST_PROPERTY_NAME, null);
+
+		Assert.assertEquals(TEST_PROPERTY_VALUE, value);
+	}
+
+	@Test
+	public void testSetValueWithProperty() {
+		setPortalProperties(TEST_PROPERTY_NAME, TEST_PROPERTY_VALUE);
+
+		portletSettings.setValue(TEST_PROPERTY_NAME, "newValue");
+
+		String value = portletSettings.getValue(TEST_PROPERTY_NAME, null);
+
+		Assert.assertEquals("newValue", value);
+	}
+
+	protected void setPortalProperties(String name, String value) {
+		Properties properties = new Properties();
+
+		properties.setProperty(name, value);
+
+		CompanyPortletSettings companyPortletSettings =
+			(CompanyPortletSettings)portletSettings;
+
+		companyPortletSettings.setPortalProperties(properties);
+	}
+
+}

portal-impl/test/unit/com/liferay/portlet/InstancePortletSettingsTest.java
@@ -1,0 +1,58 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet;
+
+import javax.portlet.PortletPreferences;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Jorge Ferrer
+ */
+public class InstancePortletSettingsTest extends SitePortletSettingsTest {
+
+	@Before
+	public void setUp() throws Exception {
+		portletSettings = new InstancePortletSettings(getSettings());
+	}
+
+	@Test
+	public void testGetDefaultFromSiteDefaults() throws Exception {
+		setSiteDefaults(_TEST_SITE_SETTING_NAME, _TEST_SITE_SETTING_VALUE);
+
+		String value = portletSettings.getValue(_TEST_SITE_SETTING_NAME, null);
+
+		Assert.assertEquals(_TEST_SITE_SETTING_VALUE, value);
+	}
+
+	protected void setSiteDefaults(String name, String value) throws Exception {
+		PortletPreferences siteDefaultSettings = new PortletPreferencesImpl();
+
+		siteDefaultSettings.setValue(name, value);
+
+		InstancePortletSettings instancePortletSettings =
+			(InstancePortletSettings)portletSettings;
+
+		instancePortletSettings.setSiteDefaultSettings(siteDefaultSettings);
+	}
+
+	private static final String _TEST_SITE_SETTING_NAME = "testSiteSettingName";
+
+	private static final String _TEST_SITE_SETTING_VALUE =
+		"testSiteSettingValue";
+
+}

portal-impl/test/unit/com/liferay/portlet/SitePortletSettingsTest.java
@@ -1,0 +1,64 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet;
+
+import javax.portlet.PortletPreferences;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Jorge Ferrer
+ */
+public class SitePortletSettingsTest extends CompanyPortletSettingsTest {
+
+	@Before
+	public void setUp() throws Exception {
+		portletSettings = new SitePortletSettings(getSettings());
+	}
+
+	@Test
+	public void testGetDefaultFromCompanyDefaults() throws Exception {
+		setCompanyDefaults(
+			_TEST_COMPANY_SETTING_NAME, _TEST_COMPANY_SETTING_VALUE);
+
+		String value = portletSettings.getValue(
+			_TEST_COMPANY_SETTING_NAME, null);
+
+		Assert.assertEquals(_TEST_COMPANY_SETTING_VALUE, value);
+	}
+
+	protected void setCompanyDefaults(String name, String value)
+		throws Exception {
+
+		PortletPreferences companyDefaultSettings =
+			new PortletPreferencesImpl();
+
+		companyDefaultSettings.setValue(name, value);
+
+		SitePortletSettings sitePortletSettings =
+			(SitePortletSettings)portletSettings;
+
+		sitePortletSettings.setCompanyDefaultSettings(companyDefaultSettings);
+	}
+
+	private static final String _TEST_COMPANY_SETTING_NAME =
+		"testSiteSettingName";
+
+	private static final String _TEST_COMPANY_SETTING_VALUE =
+		"testSiteSettingValue";
+
+}

liferay/liferay-central-test-2

portal-impl/test/unit/com/liferay/portlet/CompanyPortletSettingsTest.java
@@ -1,0 +1,64 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet;
+
+import java.util.Properties;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Jorge Ferrer
+ */
+public class CompanyPortletSettingsTest extends BasePortletSettingsTestCase {
+
+	@Before
+	public void setUp() throws Exception {
+		portletSettings = new CompanyPortletSettings(getSettings());
+	}
+
+	@Test
+	public void testGetDefaultFromProperties() {
+		setPortalProperties(TEST_PROPERTY_NAME, TEST_PROPERTY_VALUE);
+
+		String value = portletSettings.getValue(TEST_PROPERTY_NAME, null);
+
+		Assert.assertEquals(TEST_PROPERTY_VALUE, value);
+	}
+
+	@Test
+	public void testSetValueWithProperty() {
+		setPortalProperties(TEST_PROPERTY_NAME, TEST_PROPERTY_VALUE);
+
+		portletSettings.setValue(TEST_PROPERTY_NAME, "newValue");
+
+		String value = portletSettings.getValue(TEST_PROPERTY_NAME, null);
+
+		Assert.assertEquals("newValue", value);
+	}
+
+	protected void setPortalProperties(String name, String value) {
+		Properties properties = new Properties();
+
+		properties.setProperty(name, value);
+
+		CompanyPortletSettings companyPortletSettings =
+			(CompanyPortletSettings)portletSettings;
+
+		companyPortletSettings.setPortalDefaults(properties);
+	}
+
+}

portal-impl/test/unit/com/liferay/portlet/InstancePortletSettingsTest.java
@@ -1,0 +1,58 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet;
+
+import javax.portlet.PortletPreferences;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Jorge Ferrer
+ */
+public class InstancePortletSettingsTest extends SitePortletSettingsTest {
+
+	@Before
+	public void setUp() throws Exception {
+		portletSettings = new InstancePortletSettings(getSettings());
+	}
+
+	@Test
+	public void testGetDefaultFromSiteDefaults() throws Exception {
+		setSiteDefaults(_TEST_SITE_SETTING_NAME, _TEST_SITE_SETTING_VALUE);
+
+		String value = portletSettings.getValue(_TEST_SITE_SETTING_NAME, null);
+
+		Assert.assertEquals(_TEST_SITE_SETTING_VALUE, value);
+	}
+
+	protected void setSiteDefaults(String name, String value) throws Exception {
+		PortletPreferences siteDefaultSettings = new PortletPreferencesImpl();
+
+		siteDefaultSettings.setValue(name, value);
+
+		InstancePortletSettings instancePortletSettings =
+			(InstancePortletSettings)portletSettings;
+
+		instancePortletSettings.setSiteDefaults(siteDefaultSettings);
+	}
+
+	private static final String _TEST_SITE_SETTING_NAME = "testSiteSettingName";
+
+	private static final String _TEST_SITE_SETTING_VALUE =
+		"testSiteSettingValue";
+
+}

portal-impl/test/unit/com/liferay/portlet/SitePortletSettingsTest.java
@@ -1,0 +1,64 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet;
+
+import javax.portlet.PortletPreferences;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Jorge Ferrer
+ */
+public class SitePortletSettingsTest extends CompanyPortletSettingsTest {
+
+	@Before
+	public void setUp() throws Exception {
+		portletSettings = new SitePortletSettings(getSettings());
+	}
+
+	@Test
+	public void testGetDefaultFromCompanyDefaults() throws Exception {
+		setCompanyDefaults(
+			_TEST_COMPANY_SETTING_NAME, _TEST_COMPANY_SETTING_VALUE);
+
+		String value = portletSettings.getValue(
+			_TEST_COMPANY_SETTING_NAME, null);
+
+		Assert.assertEquals(_TEST_COMPANY_SETTING_VALUE, value);
+	}
+
+	protected void setCompanyDefaults(String name, String value)
+		throws Exception {
+
+		PortletPreferences companyDefaultSettings =
+			new PortletPreferencesImpl();
+
+		companyDefaultSettings.setValue(name, value);
+
+		SitePortletSettings sitePortletSettings =
+			(SitePortletSettings)portletSettings;
+
+		sitePortletSettings.setCompanyDefaults(companyDefaultSettings);
+	}
+
+	private static final String _TEST_COMPANY_SETTING_NAME =
+		"testSiteSettingName";
+
+	private static final String _TEST_COMPANY_SETTING_VALUE =
+		"testSiteSettingValue";
+
+}