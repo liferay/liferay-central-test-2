ce3d665b212e30dbc088e84dc0f9abdd5e596c88
Michael Hashimoto <michael.hashimoto@liferay.com>
2014-02-11 11:27:00 -0800

    LPS-43329 - Change "\\" into "/" for compatibility within linux

--- portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/LiferaySeleniumHelper.java
+++ portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/LiferaySeleniumHelper.java
@@ -680,8 +680,8 @@ public class LiferaySeleniumHelper {
 		}
 
 		File file = new File(
-			liferaySelenium.getProjectDir() + "portal-web\\test-results\\" +
-				"functional\\" + _screenshotFileName + "\\" +
+			liferaySelenium.getProjectDir() + "portal-web/test-results/" +
+				"functional/" + _screenshotFileName + "/" +
 				_screenshotFileName + _screenshotCount + ".jpg");
 
 		file.mkdirs();
