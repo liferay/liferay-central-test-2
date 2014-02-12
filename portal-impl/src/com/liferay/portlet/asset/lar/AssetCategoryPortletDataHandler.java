efc8fc0bbf3c2de6bc2f5dbaa1817498351187d7
Máté Thurzó <mate.thurzo@liferay.com>
2014-02-12 11:31:36 +0100

    LPS-42517 Apply changes to the new portlet data handler

--- portal-impl/src/com/liferay/portlet/asset/lar/AssetCategoryPortletDataHandler.java
+++ portal-impl/src/com/liferay/portlet/asset/lar/AssetCategoryPortletDataHandler.java
@@ -14,9 +14,21 @@
 
 package com.liferay.portlet.asset.lar;
 
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.lar.BasePortletDataHandler;
 import com.liferay.portal.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.lar.StagedModelDataHandlerUtil;
+import com.liferay.portal.kernel.lar.StagedModelType;
 import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portlet.asset.model.AssetCategory;
+import com.liferay.portlet.asset.model.AssetVocabulary;
+import com.liferay.portlet.asset.service.AssetVocabularyLocalServiceUtil;
+import com.liferay.portlet.asset.service.persistence.AssetCategoryExportActionableDynamicQuery;
+import com.liferay.portlet.asset.service.persistence.AssetVocabularyExportActionableDynamicQuery;
+
+import java.util.List;
 
 import javax.portlet.PortletPreferences;
 
@@ -28,6 +40,28 @@ public class AssetCategoryPortletDataHandler extends BasePortletDataHandler {
 	public static final String NAMESPACE = "asset_category";
 
 	public AssetCategoryPortletDataHandler() {
+		setDeletionSystemEventStagedModelTypes(
+			new StagedModelType(AssetCategory.class),
+			new StagedModelType(AssetVocabulary.class));
+		setPublishToLiveByDefault(true);
+	}
+
+	@Override
+	protected PortletPreferences doDeleteData(
+			PortletDataContext portletDataContext, String portletId,
+			PortletPreferences portletPreferences)
+		throws Exception {
+
+		if (portletDataContext.addPrimaryKey(
+				AssetCategoryPortletDataHandler.class, "deleteData")) {
+
+			return portletPreferences;
+		}
+
+		AssetVocabularyLocalServiceUtil.deleteVocabularies(
+			portletDataContext.getScopeGroupId());
+
+		return portletPreferences;
 	}
 
 	@Override
@@ -36,29 +70,22 @@ public class AssetCategoryPortletDataHandler extends BasePortletDataHandler {
 			PortletPreferences portletPreferences)
 		throws Exception {
 
-		if (exportPortletDataAll || exportCategories || companyGroup) {
-			if (_log.isDebugEnabled()) {
-				_log.debug("Export categories");
-			}
+		Element rootElement = addExportDataRootElement(portletDataContext);
 
-			List<AssetVocabulary> assetVocabularies =
-				AssetVocabularyLocalServiceUtil.getGroupVocabularies(
-					portletDataContext.getGroupId());
+		rootElement.addAttribute(
+			"group-id", String.valueOf(portletDataContext.getScopeGroupId()));
 
-			for (AssetVocabulary assetVocabulary : assetVocabularies) {
-				StagedModelDataHandlerUtil.exportStagedModel(
-					portletDataContext, assetVocabulary);
-			}
+		ActionableDynamicQuery categoryActionableDynamicQuery =
+			getCategoryActionableDynamicQuery(portletDataContext);
 
-			List<AssetCategory> assetCategories =
-				AssetCategoryUtil.findByGroupId(
-					portletDataContext.getGroupId());
+		categoryActionableDynamicQuery.performActions();
 
-			for (AssetCategory assetCategory : assetCategories) {
-				StagedModelDataHandlerUtil.exportStagedModel(
-					portletDataContext, assetCategory);
-			}
-		}
+		ActionableDynamicQuery vocabularyActionableDynamicQuery =
+			getVocabularyActionableDynamicQuery(portletDataContext);
+
+		vocabularyActionableDynamicQuery.performActions();
+
+		return getExportDataRootElementString(rootElement);
 	}
 
 	@Override
@@ -67,26 +94,80 @@ public class AssetCategoryPortletDataHandler extends BasePortletDataHandler {
 			PortletPreferences portletPreferences, String data)
 		throws Exception {
 
-		Element assetVocabulariesElement =
-			portletDataContext.getImportDataGroupElement(AssetVocabulary.class);
-
-		List<Element> assetVocabularyElements =
-			assetVocabulariesElement.elements();
-
-		for (Element assetVocabularyElement : assetVocabularyElements) {
-			StagedModelDataHandlerUtil.importStagedModel(
-				portletDataContext, assetVocabularyElement);
-		}
-
-		Element assetCategoriesElement =
+		Element categoriesElement =
 			portletDataContext.getImportDataGroupElement(AssetCategory.class);
 
-		List<Element> assetCategoryElements = assetCategoriesElement.elements();
+		List<Element> categoryElements = categoriesElement.elements();
 
-		for (Element assetCategoryElement : assetCategoryElements) {
+		for (Element categoryElement : categoryElements) {
 			StagedModelDataHandlerUtil.importStagedModel(
-				portletDataContext, assetCategoryElement);
+				portletDataContext, categoryElement);
 		}
+
+		Element vocabulariesElement =
+			portletDataContext.getImportDataGroupElement(AssetVocabulary.class);
+
+		List<Element> vocabularyElements = vocabulariesElement.elements();
+
+		for (Element vocabularyElement : vocabularyElements) {
+			StagedModelDataHandlerUtil.importStagedModel(
+				portletDataContext, vocabularyElement);
+		}
+
+		return null;
+	}
+
+	@Override
+	protected void doPrepareManifestSummary(
+			PortletDataContext portletDataContext,
+			PortletPreferences portletPreferences)
+		throws Exception {
+
+		ActionableDynamicQuery categoryActionableDynamicQuery =
+			getCategoryActionableDynamicQuery(portletDataContext);
+
+		categoryActionableDynamicQuery.performCount();
+
+		ActionableDynamicQuery vocabularyActionableDynamicQuery =
+			getVocabularyActionableDynamicQuery(portletDataContext);
+
+		vocabularyActionableDynamicQuery.performCount();
+	}
+
+	protected ActionableDynamicQuery getCategoryActionableDynamicQuery(
+			final PortletDataContext portletDataContext)
+		throws SystemException {
+
+		return new AssetCategoryExportActionableDynamicQuery(
+			portletDataContext) {
+
+			@Override
+			protected void addCriteria(DynamicQuery dynamicQuery) {
+
+				// Omit date range filter by override
+
+				return;
+			}
+
+		};
+	}
+
+	protected ActionableDynamicQuery getVocabularyActionableDynamicQuery(
+			final PortletDataContext portletDataContext)
+		throws SystemException {
+
+		return new AssetVocabularyExportActionableDynamicQuery(
+			portletDataContext) {
+
+			@Override
+			protected void addCriteria(DynamicQuery dynamicQuery) {
+
+				// Omit date range filter by override
+
+				return;
+			}
+
+		};
 	}
 
 }
