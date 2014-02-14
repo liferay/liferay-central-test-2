e688a41a01c48ac75fdebe22f0f413fe0c7bfaa5
Carlos Sierra <carlos.sierra@liferay.com>
2014-02-14 12:47:10 +0100

    LPS-44198 A JavaScriptResourceFilter

---
+++ portal-service/src/com/liferay/portal/kernel/util/JavascriptPortletResourceFilter.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.util;
+
+import com.liferay.portal.theme.ThemeDisplay;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public class JavascriptPortletResourceFilter
+	implements PredicateFilter<String> {
+
+	public JavascriptPortletResourceFilter(ThemeDisplay themeDisplay) {
+		_themeDisplay = themeDisplay;
+	}
+
+	@Override
+	public boolean filter(String resource) {
+		return !_themeDisplay.isIncludedJs(resource);
+	}
+
+	private ThemeDisplay _themeDisplay;
+
+}
