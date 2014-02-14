7350956c8a44c6e39dc613bb91376cc7bae0e806
Brian Chan <brian.chan@liferay.com>
2014-02-13 17:49:30 -0800

    LPS-44255 Missed a spot

--- portal-service/src/com/liferay/portal/kernel/portlet/PortletResponseUtil.java
+++ portal-service/src/com/liferay/portal/kernel/portlet/PortletResponseUtil.java
@@ -155,8 +155,8 @@ public class PortletResponseUtil {
 		throws IOException {
 
 		sendFile(
-			portletRequest, mimeResponse, fileName, inputStream, 0, contentType,
-			null);
+			portletRequest, mimeResponse, fileName, inputStream, contentLength,
+			contentType, null);
 	}
 
 	public static void sendFile(
