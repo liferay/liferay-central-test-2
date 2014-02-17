9b65f2fce8a6916720d919268dce02d2a50e3f61
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-02-17 10:39:23 +0100

    LPS-44302 SF: correct indentation

--- portal-impl/test/unit/com/liferay/portal/util/HtmlImplTest.java
+++ portal-impl/test/unit/com/liferay/portal/util/HtmlImplTest.java
@@ -23,8 +23,9 @@ public class HtmlImplTest {
 		Assert.assertEquals("&lt;script&gt;", _htmlImpl.escape("<script>"));
 		Assert.assertEquals("&amp;", _htmlImpl.escape("&"));
 		Assert.assertEquals("You &amp; Me", _htmlImpl.escape("You & Me"));
-		Assert.assertEquals("&lt;span class=&#034;test&#034;&gt;Test&lt;/span&gt;",
-				_htmlImpl.escape("<span class=\"test\">Test</span>"));
+		Assert.assertEquals(
+			"&lt;span class=&#034;test&#034;&gt;Test&lt;/span&gt;",
+			_htmlImpl.escape("<span class=\"test\">Test</span>"));
 		Assert.assertEquals(
 			"I&#039;m quoting: &#034;this is a quote&#034;",
 			_htmlImpl.escape("I'm quoting: \"this is a quote\""));
@@ -36,15 +37,17 @@ public class HtmlImplTest {
 			"whitespace removal",
 			_htmlImpl.extractText("   whitespace \n   <br/> removal   "));
 		Assert.assertEquals(
-				"script removal",
-				_htmlImpl.extractText("script <script>   test   </script> removal"));
+			"script removal",
+			_htmlImpl.extractText(
+				"script <script>   test   </script> removal"));
 		Assert.assertEquals(
 			"HTML attribute removal",
 			_htmlImpl.extractText(
 				"<h1>HTML</h1> <i>attribute</i> <strong>removal</strong>"));
 		Assert.assertEquals(
-				"onclick removal",
-				_htmlImpl.extractText("<div onclick=\"honk()\">onclick removal</div>"));
+			"onclick removal",
+			_htmlImpl.extractText(
+				"<div onclick=\"honk()\">onclick removal</div>"));
 	}
 
 	@Test
@@ -57,28 +60,32 @@ public class HtmlImplTest {
 	@Test
 	public void testStripBetween() {
 		Assert.assertNull(_htmlImpl.stripBetween(null, "test"));
-		Assert.assertEquals("test--test", _htmlImpl.stripBetween(
+		Assert.assertEquals(
+			"test--test",
+			_htmlImpl.stripBetween(
 				"test-<honk>thiswillbestripped</honk>-test", "honk"));
 		Assert.assertEquals(
 			"test--test",
 			_htmlImpl.stripBetween(
 				"test-<honk attribute=\"value\">thiswillbestripped</honk>-test",
 				"honk"));
-		Assert.assertEquals("test-test-test",
-				_htmlImpl.stripBetween("test-test-test", "test"));
 		Assert.assertEquals(
-			"works across  lines", _htmlImpl.stripBetween(
-			"works across <honk>\r\n a number of </honk> lines", "honk"));
-		Assert.assertEquals("test-test-test",
-				_htmlImpl.stripBetween("test-test-test", "test"));
+			"test-test-test", _htmlImpl.stripBetween("test-test-test", "test"));
+		Assert.assertEquals(
+			"works across  lines",
+			_htmlImpl.stripBetween(
+				"works across <honk>\r\n a number of </honk> lines", "honk"));
+		Assert.assertEquals(
+			"test-test-test", _htmlImpl.stripBetween("test-test-test", "test"));
 		Assert.assertEquals(
 			"multiple occurrences, multiple indeed",
 			_htmlImpl.stripBetween(
 				"multiple <a>many</a>occurrences, multiple <a>HONK</a>indeed",
 				"a"));
-		Assert.assertEquals("self-closing <test/> is unhandled",
-				_htmlImpl.stripBetween("self-closing <test/> is unhandled",
-						"test"));
+		Assert.assertEquals(
+			"self-closing <test/> is unhandled",
+			_htmlImpl.stripBetween(
+				"self-closing <test/> is unhandled", "test"));
 		Assert.assertEquals(
 			"self-closing <test /> is unhandled",
 			_htmlImpl.stripBetween(
@@ -91,12 +98,16 @@ public class HtmlImplTest {
 		Assert.assertNull(_htmlImpl.stripComments(null));
 		Assert.assertEquals("", _htmlImpl.stripComments("<!-- bla -->"));
 		Assert.assertEquals("", _htmlImpl.stripComments("<!---->"));
-		Assert.assertEquals("test",
-				_htmlImpl.stripComments("te<!--  bla -->s<!-- bla bla -->t"));
+		Assert.assertEquals(
+			"test",
+			_htmlImpl.stripComments("te<!--  bla -->s<!-- bla bla -->t"));
 		Assert.assertEquals("test", _htmlImpl.stripComments("te<!-- \n -->st"));
-		Assert.assertEquals("test", _htmlImpl.stripComments("<!--  bla -->test"));
-		Assert.assertEquals("test", _htmlImpl.stripComments("test<!--  bla -->"));
-		Assert.assertEquals("test", _htmlImpl.stripComments("te<!-- --><!-- -->st"));
+		Assert.assertEquals(
+			"test", _htmlImpl.stripComments("<!--  bla -->test"));
+		Assert.assertEquals(
+			"test", _htmlImpl.stripComments("test<!--  bla -->"));
+		Assert.assertEquals(
+			"test", _htmlImpl.stripComments("te<!-- --><!-- -->st"));
 	}
 
 	private HtmlImpl _htmlImpl = new HtmlImpl();
