ae04a836a127bbf479e429cd6c2bd1141d3f0dcf
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-02-17 17:46:21 +0100

    LPS-44187 Creates a new class for quick access entry

---
+++ portal-service/src/com/liferay/portal/kernel/servlet/taglib/ui/QuickAccessEntry.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.servlet.taglib.ui;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class QuickAccessEntry {
+
+	public static final String ATTRIBUTE_NAME = "QUICK_ACCESS";
+
+	public String getData() {
+		return _data;
+	}
+
+	public String getLabel() {
+		return _label;
+	}
+
+	public String getURL() {
+		return _url;
+	}
+
+	public void setData(String data) {
+		_data = data;
+	}
+
+	public void setLabel(String label) {
+		_label = label;
+	}
+
+	public void setURL(String url) {
+		_url = url;
+	}
+
+	private String _data;
+	private String _label;
+	private String _url;
+
+}
