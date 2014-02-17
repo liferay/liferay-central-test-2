65b7dd68675cec2ddd6e951f09d19bbfc7a311b8
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-02-17 11:05:34 +0100

    LPS-44302 SF: order methods

--- portal-impl/test/unit/com/liferay/portal/util/HtmlImplTest.java
+++ portal-impl/test/unit/com/liferay/portal/util/HtmlImplTest.java
@@ -23,18 +23,13 @@ import org.junit.Test;
 public class HtmlImplTest {
 
 	@Test
-	public void testEscapeHtmlEncodingLessThan() {
-		Assert.assertEquals("&lt;", _htmlImpl.escape("<"));
+	public void testEscapeBlank() {
+		_assertUnchangedEscape("");
 	}
 
 	@Test
-	public void testEscapeHtmlEncodingGreaterThan() {
-		Assert.assertEquals("&gt;", _htmlImpl.escape(">"));
-	}
-
-	@Test
-	public void testEscapeHtmlEncodingScriptTag() {
-		Assert.assertEquals("&lt;script&gt;", _htmlImpl.escape("<script>"));
+	public void testEscapeCaseSensitive() {
+		_assertUnchangedEscape("CAPITAL lowercase Text");
 	}
 
 	@Test
@@ -55,6 +50,16 @@ public class HtmlImplTest {
 	}
 
 	@Test
+	public void testEscapeHtmlEncodingGreaterThan() {
+		Assert.assertEquals("&gt;", _htmlImpl.escape(">"));
+	}
+
+	@Test
+	public void testEscapeHtmlEncodingLessThan() {
+		Assert.assertEquals("&lt;", _htmlImpl.escape("<"));
+	}
+
+	@Test
 	public void testEscapeHtmlEncodingQuotes() {
 		Assert.assertEquals(
 			"I&#039;m quoting: &#034;this is a quote&#034;",
@@ -62,28 +67,8 @@ public class HtmlImplTest {
 	}
 
 	@Test
-	public void testEscapeBlank() {
-		_assertUnchangedEscape("");
-	}
-
-	@Test
-	public void testEscapeWhitespace() {
-		_assertUnchangedEscape(" ");
-	}
-
-	@Test
-	public void testEscapeNull() {
-		Assert.assertNull(_htmlImpl.escape(null));
-	}
-
-	@Test
-	public void testEscapeText() {
-		_assertUnchangedEscape("text");
-	}
-
-	@Test
-	public void testEscapeCaseSensitive() {
-		_assertUnchangedEscape("CAPITAL lowercase Text");
+	public void testEscapeHtmlEncodingScriptTag() {
+		Assert.assertEquals("&lt;script&gt;", _htmlImpl.escape("<script>"));
 	}
 
 	@Test
@@ -92,8 +77,23 @@ public class HtmlImplTest {
 	}
 
 	@Test
+	public void testEscapeNull() {
+		Assert.assertNull(_htmlImpl.escape(null));
+	}
+
+	@Test
 	public void testEscapeSemiColon() {
 		_assertUnchangedEscape(";");
+	}
+
+	@Test
+	public void testEscapeText() {
+		_assertUnchangedEscape("text");
+	}
+
+	@Test
+	public void testEscapeWhitespace() {
+		_assertUnchangedEscape(" ");
 	}
 
 	@Test
@@ -123,8 +123,9 @@ public class HtmlImplTest {
 	}
 
 	@Test
-	public void testStripBetweenNull() {
-		Assert.assertNull(_htmlImpl.stripBetween(null, "test"));
+	public void testStripBetween() {
+		Assert.assertEquals(
+			"test-test-test", _htmlImpl.stripBetween("test-test-test", "test"));
 	}
 
 	@Test
@@ -133,6 +134,14 @@ public class HtmlImplTest {
 			"test--test",
 			_htmlImpl.stripBetween(
 				"test-<honk>thiswillbestripped</honk>-test", "honk"));
+	}
+
+	@Test
+	public void testStripBetweenHtmlElementAcrossLines() {
+		Assert.assertEquals(
+			"works across  lines",
+			_htmlImpl.stripBetween(
+				"works across <honk>\r\n a number of </honk> lines", "honk"));
 	}
 
 	@Test
@@ -145,26 +154,17 @@ public class HtmlImplTest {
 	}
 
 	@Test
-	public void testStripBetween() {
-		Assert.assertEquals(
-			"test-test-test", _htmlImpl.stripBetween("test-test-test", "test"));
-	}
-
-	@Test
-	public void testStripBetweenHtmlElementAcrossLines() {
-		Assert.assertEquals(
-			"works across  lines",
-			_htmlImpl.stripBetween(
-				"works across <honk>\r\n a number of </honk> lines", "honk"));
-	}
-
-	@Test
 	public void testStripBetweenMultipleOcurrencesOfHtmlElement() {
 		Assert.assertEquals(
 			"multiple occurrences, multiple indeed",
 			_htmlImpl.stripBetween(
 				"multiple <a>many</a>occurrences, multiple <a>HONK</a>indeed",
 				"a"));
+	}
+
+	@Test
+	public void testStripBetweenNull() {
+		Assert.assertNull(_htmlImpl.stripBetween(null, "test"));
 	}
 
 	@Test
@@ -184,13 +184,25 @@ public class HtmlImplTest {
 	}
 
 	@Test
-	public void testStripNullComments() {
-		Assert.assertNull(_htmlImpl.stripComments(null));
+	public void testStripComments() {
+		Assert.assertEquals("", _htmlImpl.stripComments("<!-- bla -->"));
 	}
 
 	@Test
-	public void testStripComments() {
-		Assert.assertEquals("", _htmlImpl.stripComments("<!-- bla -->"));
+	public void testStripCommentsAccrossLines() {
+		Assert.assertEquals("test", _htmlImpl.stripComments("te<!-- \n -->st"));
+	}
+
+	@Test
+	public void testStripCommentsAfter() {
+		Assert.assertEquals(
+			"test", _htmlImpl.stripComments("test<!--  bla -->"));
+	}
+
+	@Test
+	public void testStripCommentsBefore() {
+		Assert.assertEquals(
+			"test", _htmlImpl.stripComments("<!--  bla -->test"));
 	}
 
 	@Test
@@ -206,26 +218,14 @@ public class HtmlImplTest {
 	}
 
 	@Test
-	public void testStripCommentsAccrossLines() {
-		Assert.assertEquals("test", _htmlImpl.stripComments("te<!-- \n -->st"));
-	}
-
-	@Test
-	public void testStripCommentsBefore() {
-		Assert.assertEquals(
-			"test", _htmlImpl.stripComments("<!--  bla -->test"));
-	}
-
-	@Test
-	public void testStripCommentsAfter() {
-		Assert.assertEquals(
-			"test", _htmlImpl.stripComments("test<!--  bla -->"));
-	}
-
-	@Test
 	public void testStripMultipleEmptyComments() {
 		Assert.assertEquals(
 			"test", _htmlImpl.stripComments("te<!-- --><!-- -->st"));
+	}
+
+	@Test
+	public void testStripNullComments() {
+		Assert.assertNull(_htmlImpl.stripComments(null));
 	}
 
 	private void _assertUnchangedEscape(String input) {
