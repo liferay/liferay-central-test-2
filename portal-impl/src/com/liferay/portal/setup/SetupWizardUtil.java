14c30261fcce4a5596e04b5aed795ef6a2f8bd39
Brian Chan <brian.chan@liferay.com>
2014-02-18 15:46:17 -0800

    LPS-43045 SF

--- portal-impl/src/com/liferay/portal/setup/SetupWizardUtil.java
+++ portal-impl/src/com/liferay/portal/setup/SetupWizardUtil.java
@@ -464,8 +464,7 @@ public class SetupWizardUtil {
 
 				if (testUser != null) {
 					UserLocalServiceUtil.updateStatus(
-						testUser.getUserId(),
-						WorkflowConstants.STATUS_INACTIVE,
+						testUser.getUserId(), WorkflowConstants.STATUS_INACTIVE,
 						new ServiceContext());
 				}
 			}
