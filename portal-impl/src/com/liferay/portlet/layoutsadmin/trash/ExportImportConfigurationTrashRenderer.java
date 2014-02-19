d7d12ff316414e1032c5b4c75beb23fb47c9128f
Levente Hudak <levente.hudak@liferay.com>
2014-02-19 16:44:45 +0100

    LPS-43978 Adding trash handler and trash renderer classes

---
+++ portal-impl/src/com/liferay/portlet/layoutsadmin/trash/ExportImportConfigurationTrashRenderer.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.layoutsadmin.trash;
+
+import com.liferay.portal.kernel.trash.BaseTrashRenderer;
+import com.liferay.portal.model.ExportImportConfiguration;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portlet.trash.util.TrashUtil;
+
+import java.util.Locale;
+
+/**
+ * @author Levente Hudák
+ */
+public class ExportImportConfigurationTrashRenderer extends BaseTrashRenderer {
+
+	public static final String TYPE = "export_import_configuration";
+
+	public ExportImportConfigurationTrashRenderer(
+		ExportImportConfiguration exportImportConfiguration) {
+
+		_exportImportConfiguration = exportImportConfiguration;
+	}
+
+	@Override
+	public String getClassName() {
+		return ExportImportConfiguration.class.getName();
+	}
+
+	@Override
+	public long getClassPK() {
+		return _exportImportConfiguration.getPrimaryKey();
+	}
+
+	@Override
+	public String getIconPath(ThemeDisplay themeDisplay) {
+		return themeDisplay.getPathThemeImages() + "/common/configuration.png";
+	}
+
+	@Override
+	public String getPortletId() {
+		return PortletKeys.LAYOUTS_ADMIN;
+	}
+
+	@Override
+	public String getSummary(Locale locale) {
+		return HtmlUtil.stripHtml(_exportImportConfiguration.getDescription());
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		if (!_exportImportConfiguration.isInTrash()) {
+			return _exportImportConfiguration.getName();
+		}
+
+		return TrashUtil.getOriginalTitle(_exportImportConfiguration.getName());
+	}
+
+	@Override
+	public String getType() {
+		return TYPE;
+	}
+
+	private ExportImportConfiguration _exportImportConfiguration;
+
+}
