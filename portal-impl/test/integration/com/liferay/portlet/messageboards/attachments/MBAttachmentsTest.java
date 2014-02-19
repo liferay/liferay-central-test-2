36928a998b1d7a8b8bd7624bebb51d16206840d2
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-02-19 12:26:20 +0100

    LPS-44390 Forgot removing unused import

--- portal-impl/test/integration/com/liferay/portlet/messageboards/attachments/MBAttachmentsTest.java
+++ portal-impl/test/integration/com/liferay/portlet/messageboards/attachments/MBAttachmentsTest.java
@@ -23,7 +23,6 @@ import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.User;
 import com.liferay.portal.portletfilerepository.PortletFileRepositoryUtil;
-import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.service.ServiceTestUtil;
 import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
