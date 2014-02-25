66f3c04ae40330764355eeff781240d22c4a6b65
Brian Chan <brian.chan@liferay.com>
2014-02-25 11:09:31 -0800

    LPS-44471 SF

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -3401,8 +3401,8 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 				Group group = layout.getGroup();
 
 				if (!layout.isPrivateLayout() && !group.isUser()) {
-					verifyEmailAddressURL += "&p_l_id=" +
-						serviceContext.getPlid();
+					verifyEmailAddressURL +=
+						"&p_l_id=" + serviceContext.getPlid();
 				}
 			}
 		}
