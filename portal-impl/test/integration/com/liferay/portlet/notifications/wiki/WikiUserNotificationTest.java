415bafa50a629c7e260262b01bffcd502779e425
Roberto Díaz <roberto.diaz@liferay.com>
2014-02-25 12:33:13 +0100

    LPS-44515 no logic changes - Create a new test class for wiki notifications (Copy and paste from BlogsUserNotificationTest)

---
+++ portal-impl/test/integration/com/liferay/portlet/notifications/wiki/WikiUserNotificationTest.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.blogs.notifications;
+
+import com.liferay.portal.kernel.test.ExecutionTestListeners;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.model.BaseModel;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceTestUtil;
+import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
+import com.liferay.portal.test.MainServletExecutionTestListener;
+import com.liferay.portal.test.Sync;
+import com.liferay.portal.test.SynchronousDestinationExecutionTestListener;
+import com.liferay.portal.util.BaseUserNotificationTestCase;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portlet.blogs.model.BlogsEntry;
+import com.liferay.portlet.blogs.service.BlogsEntryLocalServiceUtil;
+import com.liferay.portlet.blogs.util.BlogsTestUtil;
+
+import org.junit.runner.RunWith;
+
+/**
+ * @author Roberto Díaz
+ * @author Sergio González
+ */
+@ExecutionTestListeners(
+	listeners = {
+		MainServletExecutionTestListener.class,
+		SynchronousDestinationExecutionTestListener.class
+	})
+@RunWith(LiferayIntegrationJUnitTestRunner.class)
+@Sync
+public class BlogsUserNotificationTest extends BaseUserNotificationTestCase {
+
+	protected BaseModel addBaseModel() throws Exception {
+		ServiceContext serviceContext = ServiceTestUtil.getServiceContext(
+			group.getGroupId());
+
+		serviceContext.setCommand(Constants.ADD);
+		serviceContext.setLayoutFullURL("http://localhost");
+		serviceContext.setScopeGroupId(group.getGroupId());
+
+		return BlogsTestUtil.addEntry(
+			user.getUserId(), ServiceTestUtil.randomString(), true,
+			serviceContext);
+	}
+
+	@Override
+	protected void addSubscription() throws Exception {
+		BlogsEntryLocalServiceUtil.subscribe(
+			user.getUserId(), group.getGroupId());
+	}
+
+	@Override
+	protected String getPortletId() {
+		return PortletKeys.BLOGS;
+	}
+
+	protected void updateBaseModel(BaseModel baseModel) throws Exception {
+		BlogsEntry blogsEntry = (BlogsEntry)baseModel;
+
+		ServiceContext serviceContext = ServiceTestUtil.getServiceContext();
+
+		serviceContext.setCommand(Constants.UPDATE);
+		serviceContext.setLayoutFullURL("http://localhost");
+		serviceContext.setScopeGroupId(group.getGroupId());
+
+		BlogsEntryLocalServiceUtil.updateEntry(
+			blogsEntry.getUserId(), blogsEntry.getEntryId(),
+			ServiceTestUtil.randomString(), blogsEntry.getDescription(),
+			blogsEntry.getContent(), 1, 1, 2012, 12, 00, true, true,
+			new String[0], blogsEntry.getSmallImage(),
+			blogsEntry.getSmallImageURL(), StringPool.BLANK, null,
+			serviceContext);
+	}
+
+}
