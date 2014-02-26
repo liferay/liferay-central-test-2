7e17f00267b9e7fbb3c7e81970f82a87d4283b74
Roberto Díaz <roberto.diaz@liferay.com>
2014-02-26 08:37:48 +0100

    LPS-44553 Set the persistedSubscribers of the entity in order to check the view permissions of this entity before sending the email.

--- portal-impl/src/com/liferay/portlet/blogs/service/impl/BlogsEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/blogs/service/impl/BlogsEntryLocalServiceImpl.java
@@ -1486,6 +1486,9 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 		subscriptionSender.addPersistedSubscribers(
 			BlogsEntry.class.getName(), entry.getGroupId());
 
+		subscriptionSender.addPersistedSubscribers(
+			BlogsEntry.class.getName(), entry.getEntryId());
+
 		subscriptionSender.flushNotificationsAsync();
 	}
 
