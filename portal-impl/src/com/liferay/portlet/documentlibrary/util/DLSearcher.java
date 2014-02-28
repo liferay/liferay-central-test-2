064b52d6ea97d497875eada0f679587511ac9f0b
IstvanD <istvan.dezsi@liferay.com>
2014-02-28 09:09:30 +0100

    LPS-44657 Adapting DLSearcher to generalized searching of models

--- portal-impl/src/com/liferay/portlet/documentlibrary/util/DLSearcher.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/util/DLSearcher.java
@@ -15,17 +15,9 @@
 package com.liferay.portlet.documentlibrary.util;
 
 import com.liferay.portal.kernel.search.BaseSearcher;
-import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Indexer;
-import com.liferay.portal.kernel.search.IndexerPostProcessor;
-import com.liferay.portal.kernel.search.SearchContext;
-import com.liferay.portal.kernel.search.Summary;
 import com.liferay.portlet.documentlibrary.model.DLFileEntry;
 import com.liferay.portlet.documentlibrary.model.DLFolder;
-
-import java.util.Locale;
-
-import javax.portlet.PortletURL;
 
 /**
  * @author Julio Camarero
@@ -41,70 +33,11 @@ public class DLSearcher extends BaseSearcher {
 		return new DLSearcher();
 	}
 
-	public DLSearcher() {
+	protected DLSearcher() {
+		super(CLASS_NAMES);
+
 		setFilterSearch(true);
 		setPermissionAware(true);
-	}
-
-	@Override
-	public String[] getClassNames() {
-		return CLASS_NAMES;
-	}
-
-	@Override
-	public IndexerPostProcessor[] getIndexerPostProcessors() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public String getPortletId() {
-		return null;
-	}
-
-	@Override
-	public void registerIndexerPostProcessor(
-		IndexerPostProcessor indexerPostProcessor) {
-
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	protected void doDelete(Object obj) throws Exception {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	protected Document doGetDocument(Object obj) throws Exception {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	protected Summary doGetSummary(
-			Document document, Locale locale, String snippet,
-			PortletURL portletURL)
-		throws Exception {
-
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	protected void doReindex(Object obj) throws Exception {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	protected void doReindex(String className, long classPK) throws Exception {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	protected void doReindex(String[] ids) throws Exception {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	protected String getPortletId(SearchContext searchContext) {
-		return null;
 	}
 
 }
