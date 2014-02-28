a137ac420aaad3be9e81a075640e46763ba7fc13
IstvanD <istvan.dezsi@liferay.com>
2014-02-28 11:26:36 +0100

    LPS-44428 Test - Adding AssetCategorySearchTest

---
+++ portal-impl/test/integration/com/liferay/portlet/asset/search/AssetCategorySearchTest.java
@@ -0,0 +1,138 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.asset.search;
+
+import com.liferay.portal.kernel.test.ExecutionTestListeners;
+import com.liferay.portal.model.BaseModel;
+import com.liferay.portal.search.BaseSearchTestCase;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
+import com.liferay.portal.test.MainServletExecutionTestListener;
+import com.liferay.portal.test.Sync;
+import com.liferay.portal.test.SynchronousDestinationExecutionTestListener;
+import com.liferay.portlet.asset.model.AssetCategory;
+import com.liferay.portlet.asset.service.AssetCategoryServiceUtil;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Istvan Andras Dezsi
+ */
+@ExecutionTestListeners(
+	listeners = {
+		MainServletExecutionTestListener.class,
+		SynchronousDestinationExecutionTestListener.class
+	})
+@RunWith(LiferayIntegrationJUnitTestRunner.class)
+@Sync
+public class AssetCategorySearchTest extends BaseSearchTestCase {
+
+	@Ignore()
+	@Override
+	@Test
+	public void testParentBaseModelUserPermissions() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testSearchAttachments() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testSearchByDDMStructureField() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testSearchByKeywordsInsideParentBaseModel() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testSearchExpireAllVersions() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testSearchExpireLatestVersion() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testSearchMyEntries() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testSearchRecentEntries() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testSearchStatus() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testSearchVersions() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testSearchWithinDDMStructure() throws Exception {
+	}
+
+	@Override
+	protected BaseModel<?> addBaseModelWithWorkflow(
+			BaseModel<?> parentBaseModel, boolean approved, String keywords,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		AssetCategory assetCategory =
+						AssetCategoryServiceUtil.addCategory(
+							keywords, 0, serviceContext);
+
+		return assetCategory;
+	}
+
+	@Override
+	protected Class<?> getBaseModelClass() {
+		return AssetCategory.class;
+	}
+
+	@Override
+	protected String getSearchKeywords() {
+		return "Title";
+	}
+
+	@Override
+	protected void moveBaseModelToTrash(long primaryKey) throws Exception {
+		AssetCategoryServiceUtil.deleteCategory(primaryKey);
+	}
+
+}
