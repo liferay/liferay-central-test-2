59d11dbf09eb5e6ba10b92ca972a863f00014482
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2014-02-28 13:03:51 +0100

    LPS-44695 Upgrade process. In the future we will add more portlet to the map and the upgrade will upgrade them all.

--- portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_7_0_0.java
+++ portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_7_0_0.java
@@ -16,6 +16,7 @@ package com.liferay.portal.upgrade;
 
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.ReleaseInfo;
+import com.liferay.portal.upgrade.v7_0_0.UpgradePortletSettings;
 import com.liferay.portal.upgrade.v7_0_0.UpgradeRepositoryEntry;
 import com.liferay.portal.upgrade.v7_0_0.UpgradeSchema;
 import com.liferay.portal.upgrade.v7_0_0.UpgradeShopping;
@@ -34,6 +35,7 @@ public class UpgradeProcess_7_0_0 extends UpgradeProcess {
 	@Override
 	protected void doUpgrade() throws Exception {
 		upgrade(UpgradeSchema.class);
+		upgrade(UpgradePortletSettings.class);
 		upgrade(UpgradeRepositoryEntry.class);
 		upgrade(UpgradeShopping.class);
 		upgrade(UpgradeSubscription.class);
---
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradePortletSettings.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.v7_0_0;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portlet.messageboards.model.MBConstants;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Sergio González
+ */
+public class UpgradePortletSettings extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		for (Map.Entry<String, String> serviceNameMapEntry :
+				_serviceNameMap.entrySet()) {
+
+			String portletId = serviceNameMapEntry.getKey();
+			String serviceName = serviceNameMapEntry.getValue();
+
+			upgradePortletSettings(portletId, serviceName);
+		}
+	}
+
+	protected void upgradePortletSettings(String portletId, String serviceName)
+		throws Exception {
+
+		StringBundler sb = new StringBundler(6);
+
+		sb.append("update PortletPreferences portletId = '");
+		sb.append(serviceName);
+		sb.append("' where ownerType = ");
+		sb.append(PortletKeys.PREFS_OWNER_TYPE_GROUP);
+		sb.append(" and portletId like '");
+		sb.append(portletId);
+		sb.append("'");
+
+		runSQL(sb.toString());
+	}
+
+	private static Map<String, String> _serviceNameMap =
+		new HashMap<String, String>();
+
+	static {
+		_serviceNameMap.put("19", MBConstants.SERVICE_NAME);
+	}
+
+}
