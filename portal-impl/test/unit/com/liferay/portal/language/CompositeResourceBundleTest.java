3c43bfd0109024892dd56a264e088e14b04105f4
Carlos Sierra <carlos.sierra@liferay.com>
2014-03-03 12:18:59 +0100

    LPS-44342 Added test for CompositeResourceBundle

---
+++ portal-impl/test/unit/com/liferay/portal/language/CompositeResourceBundleTest.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.language;
+
+import java.util.HashSet;
+import java.util.ListResourceBundle;
+import java.util.ResourceBundle;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public class CompositeResourceBundleTest {
+
+	@Test
+	public void testGetKeys() {
+		ResourceBundle resourceBundleA = _createResourceBundle(
+			"keyA", "valueA");
+		ResourceBundle resourceBundleB = _createResourceBundle(
+			"keyB", "valueB");
+
+		CompositeResourceBundle compositeResourceBundle =
+			new CompositeResourceBundle(resourceBundleA, resourceBundleB);
+
+		Assert.assertEquals(
+			"valueA", compositeResourceBundle.getString("keyA"));
+		Assert.assertEquals(
+			"valueB", compositeResourceBundle.getString("keyB"));
+
+		Assert.assertEquals(
+			new HashSet<String>(){{
+				add("keyA");
+				add("keyB");
+			}},
+			compositeResourceBundle.keySet());
+
+	}
+
+	private ResourceBundle _createResourceBundle(
+		final String ... keysAndValues) {
+
+		if ((keysAndValues.length % 2) != 0) {
+			throw new RuntimeException(
+				"You must provide values for all the keys");
+		}
+
+		return new ListResourceBundle() {
+
+			@Override
+			protected Object[][] getContents() {
+				Object[][] contents = new Object[keysAndValues.length / 2][];
+
+				for (int i = 0; i < contents.length; i++) {
+					contents[i] = new Object[] {
+						keysAndValues[i/2], keysAndValues[(i/2) + 1]};
+				}
+
+				return contents;
+			}
+		};
+	}
+
+}
