1d36a75c4e2dfefc3914eba30e285b3b620bba3f
Carlos Sierra <carlos.sierra@liferay.com>
2014-03-03 11:35:12 +0100

    LPS-44342 New CompositeResourceBundle
    Returns the first non null key from the lists of bundles

---
+++ portal-impl/src/com/liferay/portal/language/CompositeResourceBundle.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.language;
+
+import edu.emory.mathcs.backport.java.util.Collections;
+
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public class CompositeResourceBundle extends ResourceBundle {
+
+	private final HashSet<String> _keySet;
+
+	public CompositeResourceBundle(ResourceBundle ... delegateResourceBundles) {
+
+		_delegateResourceBundles = delegateResourceBundles;
+		_keySet = new HashSet<String>();
+
+		for (ResourceBundle delegateResourceBundle : _delegateResourceBundles) {
+			try {
+				Set<String> delegateKeySet = delegateResourceBundle.keySet();
+
+				_keySet.addAll(delegateKeySet);
+			} catch (Exception e) {
+				continue;
+			}
+		}
+
+	}
+
+	@Override
+	protected Set<String> handleKeySet() {
+		return _keySet;
+	}
+
+	@Override
+	public Enumeration<String> getKeys() {
+		return Collections.enumeration(keySet());
+	}
+
+	@Override
+	protected Object handleGetObject(String key) {
+		for (ResourceBundle delegateResourceBundle : _delegateResourceBundles) {
+			Object object = null;
+
+			try {
+				object = delegateResourceBundle.getObject(key);
+			}
+			catch (MissingResourceException e) {
+				continue;
+			}
+
+			if (object != null) {
+				return object;
+			}
+		}
+
+		throw new MissingResourceException(
+			"Could not find resource", CompositeResourceBundle.class.getName(),
+			key);
+	}
+
+	private ResourceBundle[] _delegateResourceBundles;
+
+}
