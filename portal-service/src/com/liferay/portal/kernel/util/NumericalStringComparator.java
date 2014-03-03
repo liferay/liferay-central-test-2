4b0dd6fe25d1477dc59705cc6575e4aeb438b6d9
Hugo Huijser <hugo.huijser@liferay.com>
2014-03-03 20:54:20 +0800

    LPS-44723 Move logic to new class NaturalOrderStringComparator

--- portal-service/src/com/liferay/portal/kernel/util/NumericalStringComparator.java
+++ portal-service/src/com/liferay/portal/kernel/util/NumericalStringComparator.java
@@ -14,117 +14,17 @@
 
 package com.liferay.portal.kernel.util;
 
-import java.io.Serializable;
-
-import java.util.Comparator;
-
 /**
  * @author Hugo Huijser
  */
-public class NumericalStringComparator
-	implements Comparator<String>, Serializable {
+public class NumericalStringComparator extends NaturalOrderStringComparator {
 
 	public NumericalStringComparator() {
-		this(true, false);
+		super(true, false);
 	}
 
 	public NumericalStringComparator(boolean ascending, boolean caseSensitive) {
-		_ascending = ascending;
-		_caseSensitive = caseSensitive;
+		super(ascending, caseSensitive);
 	}
-
-	@Override
-	public int compare(String s1, String s2) {
-		if (s1 == null) {
-			s1 = StringPool.BLANK;
-		}
-
-		if (s2 == null) {
-			s2 = StringPool.BLANK;
-		}
-
-		int value = 0;
-
-		int i1 = 0;
-		int i2 = 0;
-
-		int length1 = s1.length();
-		int length2 = s2.length();
-
-		while ((i1 < length1) && (i2 < length2)) {
-			char c1 = s1.charAt(i1);
-			char c2 = s2.charAt(i2);
-
-			if (Validator.isDigit(c1) && Validator.isDigit(c2)) {
-				String leadingDigitsAsString1 = StringUtil.extractLeadingDigits(
-					s1.substring(i1));
-				String leadingDigitsAsString2 = StringUtil.extractLeadingDigits(
-					s2.substring(i2));
-
-				int leadingNumber1 = GetterUtil.getInteger(
-					leadingDigitsAsString1);
-				int leadingNumber2 = GetterUtil.getInteger(
-					leadingDigitsAsString2);
-
-				if (leadingNumber1 != leadingNumber2) {
-					value = leadingNumber1 - leadingNumber2;
-
-					break;
-				}
-
-				i1 += leadingDigitsAsString1.length();
-				i2 += leadingDigitsAsString2.length();
-
-				continue;
-			}
-
-			if (c1 == c2) {
-				i1++;
-				i2++;
-
-				continue;
-			}
-
-			if (_caseSensitive) {
-				value = c1 - c2;
-
-				break;
-			}
-			else {
-				char c1UpperCase = Character.toUpperCase(c1);
-				char c2UpperCase = Character.toUpperCase(c2);
-
-				if (c1UpperCase == c2UpperCase) {
-					i1++;
-					i2++;
-
-					continue;
-				}
-
-				value = c1UpperCase - c2UpperCase;
-
-				break;
-			}
-		}
-
-		if ((value == 0) && (length1 != length2)) {
-			if ((length1 == i1) && (length2 == i2)) {
-				value = length2 - length1;
-			}
-			else {
-				value = length1 - length2;
-			}
-		}
-
-		if (_ascending) {
-			return value;
-		}
-		else {
-			return -value;
-		}
-	}
-
-	private boolean _ascending;
-	private boolean _caseSensitive;
 
 }
