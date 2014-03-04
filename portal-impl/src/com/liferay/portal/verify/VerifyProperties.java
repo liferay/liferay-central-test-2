3af7cf474932ee6b0f781c4975e30f930791ddc9
Brian Chan <brian.chan@liferay.com>
2014-03-04 09:53:54 -0800

    LPS-44401 Add obsolete portal.properties to VerifyProperties

--- portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
@@ -271,6 +271,8 @@ public class VerifyProperties extends VerifyProcess {
 		"dynamic.data.lists.template.language.parser[vm]",
 		"dynamic.data.lists.template.language.parser[xsl]",
 		"dynamic.data.mapping.template.language.types",
+		"editor.wysiwyg.portal-web.docroot.html.portlet.blogs.configuration." +
+			"jsp", 
 		"editor.wysiwyg.portal-web.docroot.html.portlet.login.configuration." +
 			"jsp",
 		"ehcache.statistics.enabled", "index.filter.search.limit",
