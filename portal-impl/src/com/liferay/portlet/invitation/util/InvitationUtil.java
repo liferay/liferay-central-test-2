35ab172b69c4733ede05929beef1761d4ceea9a4
Chema Balsas <jose.balsas@liferay.com>
2014-03-06 10:24:48 +0100

    LPS-44825 Add email definitions terms map

--- portal-impl/src/com/liferay/portlet/invitation/util/InvitationUtil.java
+++ portal-impl/src/com/liferay/portlet/invitation/util/InvitationUtil.java
@@ -14,19 +14,59 @@
 
 package com.liferay.portlet.invitation.util;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.Company;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.PropsUtil;
+import com.liferay.portal.util.WebKeys;
 import com.liferay.util.ContentUtil;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.portlet.PortletPreferences;
+import javax.portlet.PortletRequest;
 
 /**
  * @author Brian Wing Shun Chan
  */
 public class InvitationUtil {
+
+	public static Map<String, String> getEmailDefinitionTerms(
+			PortletRequest portletRequest)
+		throws PortalException, SystemException {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		Map<String, String> definitionTerms = new HashMap<String, String>();
+
+		definitionTerms.put(
+			"[$FROM_ADDRESS$]",
+			LanguageUtil.get(
+				themeDisplay.getLocale(), "the-address-of-the-email-sender"));
+		definitionTerms.put(
+			"[$FROM_NAME$]",
+			LanguageUtil.get(
+				themeDisplay.getLocale(), "the-name-of-the-email-sender"));
+		definitionTerms.put(
+			"[$PAGE_URL$]",
+			PortalUtil.getLayoutFullURL(
+				themeDisplay.getLayout(), themeDisplay));
+
+		Company company = themeDisplay.getCompany();
+
+		definitionTerms.put("[$PORTAL_URL$]", company.getVirtualHostname());
+
+		return definitionTerms;
+	}
 
 	public static String getEmailMessageBody(PortletPreferences preferences) {
 		String emailMessageBody = preferences.getValue(
