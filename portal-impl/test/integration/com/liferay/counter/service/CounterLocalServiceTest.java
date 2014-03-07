0a21cefbaf4ff00db2a007bf485c41dd25ef02f5
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2014-03-07 10:16:10 +0100

    LPS-44901 When multiple process are executing within the testing infrastructure they are creating a few module framework runtimes. We need to explicitly close the framework under these situations because we could easily fall into deadlock situations

--- portal-impl/test/integration/com/liferay/counter/service/CounterLocalServiceTest.java
+++ portal-impl/test/integration/com/liferay/counter/service/CounterLocalServiceTest.java
@@ -21,7 +21,6 @@ import com.liferay.portal.kernel.process.ProcessCallable;
 import com.liferay.portal.kernel.process.ProcessException;
 import com.liferay.portal.kernel.process.ProcessExecutor;
 import com.liferay.portal.kernel.scheduler.SchedulerEngineHelperUtil;
-import com.liferay.portal.kernel.scheduler.SchedulerException;
 import com.liferay.portal.kernel.test.ExecutionTestListeners;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.test.EnvironmentExecutionTestListener;
@@ -143,9 +142,11 @@ public class CounterLocalServiceTest {
 			finally {
 				try {
 					SchedulerEngineHelperUtil.shutdown();
+
+					InitUtil.stopModuleFramework();
 				}
-				catch (SchedulerException se) {
-					throw new ProcessException(se);
+				catch (Exception e) {
+					throw new ProcessException(e);
 				}
 			}
 
