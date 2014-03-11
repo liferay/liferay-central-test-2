ece4c0359a445f221bbdb919ff78ec7f6b65eb24
Brian Chan <brian.chan@liferay.com>
2014-03-11 16:33:06 -0700

    LPS-44013 SF

--- portal-impl/test/integration/com/liferay/portal/service/PortletPreferencesLocalServiceTest.java
+++ portal-impl/test/integration/com/liferay/portal/service/PortletPreferencesLocalServiceTest.java
@@ -433,8 +433,7 @@ public class PortletPreferencesLocalServiceTest {
 
 		javax.portlet.PortletPreferences jxPortletPreferences =
 			PortletPreferencesLocalServiceUtil.fetchPreferences(
-				_layout.getCompanyId(),
-				PortletKeys.PREFS_OWNER_ID_DEFAULT,
+				_layout.getCompanyId(), PortletKeys.PREFS_OWNER_ID_DEFAULT,
 				PortletKeys.PREFS_OWNER_TYPE_LAYOUT, _layout.getPlid(),
 				_portlet.getPortletId());
 
