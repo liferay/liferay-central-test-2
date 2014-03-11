6a4a41b496d831c6e32d7aaefab743cebeabf002
Marcellus Tavares <marcellus.tavares@liferay.com>
2014-03-11 12:04:11 -0300

    LPS-45003 Unable to import lar file on a site that has the default locale modified before export

--- portal-service/src/com/liferay/portal/kernel/messaging/BaseAsyncDestination.java
+++ portal-service/src/com/liferay/portal/kernel/messaging/BaseAsyncDestination.java
@@ -22,6 +22,8 @@ import com.liferay.portal.kernel.executor.PortalExecutorManagerUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.proxy.MessageValuesThreadLocal;
+import com.liferay.portal.kernel.util.GroupThreadLocal;
+import com.liferay.portal.kernel.util.LocaleThreadLocal;
 import com.liferay.portal.kernel.util.NamedThreadFactory;
 import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
 import com.liferay.portal.kernel.util.Validator;
@@ -33,6 +35,7 @@ import com.liferay.portal.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.security.permission.PermissionThreadLocal;
 import com.liferay.portal.service.UserLocalServiceUtil;
 
+import java.util.Locale;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -222,6 +225,14 @@ public abstract class BaseAsyncDestination extends BaseDestination {
 			message.put("companyId", CompanyThreadLocal.getCompanyId());
 		}
 
+		if (!message.contains("defaultLocale")) {
+			message.put("defaultLocale", LocaleThreadLocal.getDefaultLocale());
+		}
+
+		if (!message.contains("groupId")) {
+			message.put("groupId", GroupThreadLocal.getGroupId());
+		}
+
 		if (!message.contains("permissionChecker")) {
 			message.put(
 				"permissionChecker",
@@ -236,6 +247,17 @@ public abstract class BaseAsyncDestination extends BaseDestination {
 			message.put(
 				"principalPassword", PrincipalThreadLocal.getPassword());
 		}
+
+		if (!message.contains("siteDefaultLocale")) {
+			message.put(
+				"siteDefaultLocale", LocaleThreadLocal.getSiteDefaultLocale());
+		}
+
+		if (!message.contains("themeDisplayLocale")) {
+			message.put(
+				"themeDisplayLocale",
+				LocaleThreadLocal.getThemeDisplayLocale());
+		}
 	}
 
 	protected void populateThreadLocalsFromMessage(Message message) {
@@ -243,6 +265,18 @@ public abstract class BaseAsyncDestination extends BaseDestination {
 
 		if (companyId > 0) {
 			CompanyThreadLocal.setCompanyId(companyId);
+		}
+
+		Locale defaultLocale = (Locale)message.get("defaultLocale");
+
+		if (defaultLocale != null) {
+			LocaleThreadLocal.setDefaultLocale(defaultLocale);
+		}
+
+		long groupId = message.getLong("groupId");
+
+		if (groupId > 0) {
+			GroupThreadLocal.setGroupId(groupId);
 		}
 
 		PermissionChecker permissionChecker = (PermissionChecker)message.get(
@@ -282,6 +316,18 @@ public abstract class BaseAsyncDestination extends BaseDestination {
 		if (clusterForwardMessage != null) {
 			MessageValuesThreadLocal.setValue(
 				ClusterLink.CLUSTER_FORWARD_MESSAGE, clusterForwardMessage);
+		}
+
+		Locale siteDefaultLocale = (Locale)message.get("siteDefaultLocale");
+
+		if (siteDefaultLocale != null) {
+			LocaleThreadLocal.setSiteDefaultLocale(siteDefaultLocale);
+		}
+
+		Locale themeDisplayLocale = (Locale)message.get("themeDisplayLocale");
+
+		if (themeDisplayLocale != null) {
+			LocaleThreadLocal.setThemeDisplayLocale(themeDisplayLocale);
 		}
 	}
 
