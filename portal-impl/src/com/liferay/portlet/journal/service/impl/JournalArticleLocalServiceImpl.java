6c49d78450db8bc9f146185890a32399ec54dea5
Brian Chan <brian.chan@liferay.com>
2014-03-13 11:56:43 -0700

    LPS-33427 Jim, please readd

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -4374,17 +4374,6 @@ public class JournalArticleLocalServiceImpl
 		return searchJournalArticles(searchContext);
 	}
 
-	/**
-	 * Subscribe the user to changes in elements that belongs to specified
-	 * structure.
-	 *
-	 * @param  groupId the primary key of the folder's group
-	 * @param  userId the primary key of the user to be subscribed
-	 * @param ddmStructureId the primary key of the structure to subscribe to
-	 * @throws PortalException if the user, group os structure could not be
-	 * 		   found, or if subscribing was not permissible
-	 * @throws SystemException if a system exception occurred
-	 */
 	@Override
 	public void subscribeStructure(
 			long groupId, long userId, long ddmStructureId)
@@ -4398,17 +4387,6 @@ public class JournalArticleLocalServiceImpl
 			userId, groupId, className, classPK);
 	}
 
-	/**
-	 * Unsubscribe the user from changes in elements that belongs to specified
-	 * structure.
-	 *
-	 * @param  groupId the primary key of the folder's group
-	 * @param  userId the primary key of the user to be subscribed
-	 * @param ddmStructureId the primary key of the structure to subscribe to
-	 * @throws PortalException if the user, group os structure could not be
-	 * 		   found, or if subscribing was not permissible
-	 * @throws SystemException if a system exception occurred
-	 */
 	@Override
 	public void unsubscribeStructure(
 			long groupId, long userId, long ddmStructureId)
