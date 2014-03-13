6639cd211c5844f4e3660436b34edfd96c0c8886
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-03-13 12:16:26 +0100

    LPS-43298 We should reindex always in Blog, not only when the state are approve or in trash

--- portal-impl/src/com/liferay/portlet/blogs/util/BlogsIndexer.java
+++ portal-impl/src/com/liferay/portlet/blogs/util/BlogsIndexer.java
@@ -141,10 +141,6 @@ public class BlogsIndexer extends BaseIndexer {
 	protected void doReindex(Object obj) throws Exception {
 		BlogsEntry entry = (BlogsEntry)obj;
 
-		if (!entry.isApproved() && !entry.isInTrash()) {
-			return;
-		}
-
 		Document document = getDocument(entry);
 
 		SearchEngineUtil.updateDocument(
