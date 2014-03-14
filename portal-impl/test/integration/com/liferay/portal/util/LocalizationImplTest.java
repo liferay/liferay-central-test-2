7f995bde537563d1c0ccf0d35678a07a439a9d77
Julio Camarero <julio.camarero@liferay.com>
2014-03-14 08:49:38 +0100

    LPS-44999 tests for localizations

--- portal-impl/test/integration/com/liferay/portal/util/LocalizationImplTest.java
+++ portal-impl/test/integration/com/liferay/portal/util/LocalizationImplTest.java
@@ -134,6 +134,45 @@ public class LocalizationImplTest {
 	}
 
 	@Test
+	public void testLocalizationsXMLDefaultValue() {
+		String xml = StringPool.BLANK;
+
+		xml = LocalizationUtil.updateLocalization(
+			xml, "greeting", _englishHello, _englishLanguageId,
+			_englishLanguageId);
+		xml = LocalizationUtil.updateLocalization(
+			xml, "greeting", _germanHello, _germanLanguageId,
+			_englishLanguageId);
+
+		String defaultValue = "default value";
+
+		Assert.assertEquals(
+			defaultValue,
+			LocalizationUtil.getLocalization(
+				xml, _spanishLanguageId, false, defaultValue));
+		Assert.assertEquals(
+			_englishHello,
+			LocalizationUtil.getLocalization(
+				xml, _spanishLanguageId, true, defaultValue));
+	}
+
+	@Test
+	public void testLocalizationsXMLUseDefault() {
+		String xml = StringPool.BLANK;
+
+		xml = LocalizationUtil.updateLocalization(
+			xml, "greeting", _englishHello, _englishLanguageId,
+			_englishLanguageId);
+		xml = LocalizationUtil.updateLocalization(
+			xml, "greeting", _germanHello, _germanLanguageId,
+			_englishLanguageId);
+
+		Assert.assertEquals(
+			_englishHello,
+			LocalizationUtil.getLocalization(xml, _spanishLanguageId, true));
+	}
+
+	@Test
 	public void testLongTranslationText() {
 		StringBundler sb = new StringBundler();
 
@@ -250,6 +289,8 @@ public class LocalizationImplTest {
 	private String _germanHello = "Hallo Welt";
 	private String _germanLanguageId = LocaleUtil.toLanguageId(
 		LocaleUtil.GERMANY);
+	private String _spanishLanguageId = LocaleUtil.toLanguageId(
+		LocaleUtil.SPAIN);
 	private String _xml;
 
 }
