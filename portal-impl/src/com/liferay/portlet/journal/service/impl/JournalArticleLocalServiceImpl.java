a4f1928743ebd1a3102b9b4d9ff97e82b17797b2
akosthurzo <akos.thurzo@liferay.com>
2014-03-17 15:33:43 +0100

    LPS-45164 Update author when the content is updated

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -4744,8 +4744,6 @@ public class JournalArticleLocalServiceImpl
 			article.setResourcePrimKey(latestArticle.getResourcePrimKey());
 			article.setGroupId(latestArticle.getGroupId());
 			article.setCompanyId(latestArticle.getCompanyId());
-			article.setUserId(user.getUserId());
-			article.setUserName(user.getFullName());
 			article.setCreateDate(serviceContext.getModifiedDate(now));
 			article.setClassNameId(latestArticle.getClassNameId());
 			article.setClassPK(latestArticle.getClassPK());
@@ -4792,6 +4790,8 @@ public class JournalArticleLocalServiceImpl
 		article.setReviewDate(reviewDate);
 		article.setIndexable(indexable);
 		article.setSmallImage(smallImage);
+		article.setUserId(user.getUserId());
+		article.setUserName(user.getFullName());
 
 		if (smallImage) {
 			if ((smallImageFile != null) && (smallImageBytes != null)) {
