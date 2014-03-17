04ace9afd6613435312c22677c8a1fb4530f6cb5
Michael Hashimoto <michael.hashimoto@liferay.com>
2014-03-17 12:32:38 -0700

    LPS-45177 - Update 'InitUtil' methods within 'SeleniumBuilder'

--- portal-impl/src/com/liferay/portal/tools/seleniumbuilder/SeleniumBuilder.java
+++ portal-impl/src/com/liferay/portal/tools/seleniumbuilder/SeleniumBuilder.java
@@ -35,9 +35,11 @@ import java.util.TreeSet;
 public class SeleniumBuilder {
 
 	public static void main(String[] args) throws Exception {
-		InitUtil.initWithSpring();
+		InitUtil.initWithSpringAndModuleFramework();
 
 		new SeleniumBuilder(args);
+
+		InitUtil.stopModuleFramework();
 	}
 
 	/**
