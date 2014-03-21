60e348b6f33f425519b88bfa2012d9ce628578fc
shuyangzhou <shuyang.zhou@liferay.com>
2014-03-21 07:26:38 -0700

    LPS-45309 To make the logic consistent, MemoryPortalCacheManager should also return a copied set of the CacheManagerListeners

--- portal-impl/src/com/liferay/portal/cache/memory/MemoryPortalCacheManager.java
+++ portal-impl/src/com/liferay/portal/cache/memory/MemoryPortalCacheManager.java
@@ -22,6 +22,7 @@ import java.io.Serializable;
 
 import java.net.URL;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -96,7 +97,7 @@ public class MemoryPortalCacheManager<K extends Serializable, V>
 
 	@Override
 	public Set<CacheManagerListener> getCacheManagerListeners() {
-		return _cacheManagerListeners;
+		return new HashSet<CacheManagerListener>(_cacheManagerListeners);
 	}
 
 	@Override
