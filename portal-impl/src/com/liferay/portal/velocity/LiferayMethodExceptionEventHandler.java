26d6aea917e3ccc142242ce5f4ee484efb540d70
Peter Shin <peter.shin@liferay.com>
2014-03-24 14:36:52 -0500

    LPS-45394

--- portal-impl/src/com/liferay/portal/velocity/LiferayMethodExceptionEventHandler.java
+++ portal-impl/src/com/liferay/portal/velocity/LiferayMethodExceptionEventHandler.java
@@ -16,11 +16,19 @@ package com.liferay.portal.velocity;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.ProxyUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.velocity.app.event.MethodExceptionEventHandler;
 
 /**
  * @author Raymond Augé
+ * @author Peter Shin
  */
 public class LiferayMethodExceptionEventHandler
 	implements MethodExceptionEventHandler {
@@ -31,9 +39,47 @@ public class LiferayMethodExceptionEventHandler
 			Exception e)
 		throws Exception {
 
-		_log.error(e, e);
+		StringBundler sb = new StringBundler(9);
+
+		sb.append("Unable to execute method ");
+		sb.append(method);
+		sb.append(StringPool.SPACE);
+		sb.append(StringPool.OPEN_CURLY_BRACE);
+		sb.append("exception=");
+		sb.append(e);
+		sb.append(StringPool.COMMA_AND_SPACE);
+		sb.append(getClassMessage(clazz));
+		sb.append(StringPool.CLOSE_CURLY_BRACE);
+
+		_log.error(sb.toString(), e);
 
 		return null;
+	}
+
+	protected String getClassMessage(Class<?> clazz) {
+		if (clazz == null) {
+			return "class=null";
+		}
+
+		if (!ProxyUtil.isProxyClass(clazz)) {
+			return "className=".concat(clazz.getName());
+		}
+
+		Class<?>[] interfaceClasses = clazz.getInterfaces();
+
+		if (interfaceClasses == null) {
+			return "className=".concat(clazz.getName());
+		}
+
+		List<String> proxyInterfaceClassNames = new ArrayList<String>();
+
+		for (Class<?> interfaceClass : interfaceClasses) {
+			proxyInterfaceClassNames.add(interfaceClass.getName());
+		}
+
+		return "proxyInterfaceClassNames=".concat(
+			StringUtil.merge(
+				proxyInterfaceClassNames, StringPool.COMMA_AND_SPACE));
 	}
 
 	private static Log _log = LogFactoryUtil.getLog(
