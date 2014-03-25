5fb0ddfa8e0fbeb8121ed425fbebe27135b5a379
Roberto Díaz <roberto.diaz@liferay.com>
2014-03-25 14:49:22 +0100

    LPS-45418 On portlet instance deletion, delete the subscriptions of this instance.

--- portal-impl/src/com/liferay/portlet/assetpublisher/AssetPublisherPortletLayoutListener.java
+++ portal-impl/src/com/liferay/portlet/assetpublisher/AssetPublisherPortletLayoutListener.java
@@ -18,6 +18,8 @@ import com.liferay.portal.kernel.portlet.PortletLayoutListener;
 import com.liferay.portal.kernel.portlet.PortletLayoutListenerException;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
+import com.liferay.portal.service.SubscriptionLocalServiceUtil;
+import com.liferay.portlet.assetpublisher.util.AssetPublisherUtil;
 import com.liferay.portlet.journal.service.JournalArticleLocalServiceUtil;
 
 /**
@@ -43,6 +45,11 @@ public class AssetPublisherPortletLayoutListener
 
 			JournalArticleLocalServiceUtil.deleteLayoutArticleReferences(
 				layout.getGroupId(), layout.getUuid());
+
+			SubscriptionLocalServiceUtil.deleteSubscriptions(
+				layout.getCompanyId(),
+				com.liferay.portal.model.PortletPreferences.class.getName(),
+				AssetPublisherUtil.getSubscriptionClassPK(plid, portletId));
 		}
 		catch (Exception e) {
 			throw new PortletLayoutListenerException(e);
