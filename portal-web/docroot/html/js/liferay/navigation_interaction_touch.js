7e3ba6e93983997ebe134ebacd92212b07cc9d6e
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2014-03-25 08:25:05 -0700

    LPS-42322 On mobile devices, child page dropdowns cannot be collapsed

--- portal-web/docroot/html/js/liferay/navigation_interaction_touch.js
+++ portal-web/docroot/html/js/liferay/navigation_interaction_touch.js
@@ -1,14 +1,38 @@
 AUI.add(
 	'liferay-navigation-interaction-touch',
 	function(A) {
+		var STR_OPEN = 'open';
+
 		A.mix(
 			Liferay.NavigationInteraction.prototype,
 			{
+				_handleShowNavigationMenu: function(menuNew, menuOld, event) {
+					var instance = this;
+
+					var mapHover = instance.MAP_HOVER;
+
+					mapHover.menu = menuNew;
+
+					var menuOpen = menuNew.hasClass(STR_OPEN);
+
+					if (menuOpen) {
+						Liferay.fire('hideNavigationMenu', mapHover);
+					}
+					else {
+						Liferay.fire('showNavigationMenu', mapHover);
+
+						if ((menuOld && menuOld.hasClass(STR_OPEN)) && (menuOld != menuNew)) {
+							menuOld.removeClass(STR_OPEN);
+							menuOld.removeClass('hover');
+						}
+					}
+				},
+
 				_initChildMenuHandlers: function(navigation) {
 					var instance = this;
 
 					if (navigation) {
-						navigation.delegate('click', instance._onTouchClick, '> li > a', instance);
+						navigation.delegate(['click', 'touchstart'], instance._onTouchClick, '> li > a', instance);
 					}
 				},
 
@@ -17,13 +41,15 @@ AUI.add(
 				_onTouchClick: function(event) {
 					var instance = this;
 
+					var target = event.target;
+
 					var menuNew = event.currentTarget.ancestor(instance._directChildLi);
 
-					if (menuNew.one('.child-menu') && !menuNew.hasClass('hover')) {
+					if (menuNew.one('.child-menu') && target.ancestor('.lfr-nav-child-toggle', true, '.lfr-nav-item')) {
 						event.preventDefault();
-					}
 
-					instance._handleShowNavigationMenu(menuNew, instance.MAP_HOVER.menu, event);
+						instance._handleShowNavigationMenu(menuNew, instance.MAP_HOVER.menu, event);
+					}
 				}
 			},
 			true
