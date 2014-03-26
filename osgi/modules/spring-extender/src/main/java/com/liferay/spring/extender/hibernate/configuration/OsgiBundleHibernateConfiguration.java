9a173cb48da7e2597d03ee865b4e641e531dd363
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2014-03-26 12:52:07 +0100

    LPS-45495 Custom Hibernate session factory configurations in order be able to make new applications to work within the OSGi container

---
+++ osgi/modules/spring-extender/src/main/java/com/liferay/spring/extender/hibernate/configuration/OsgiBundleHibernateConfiguration.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.spring.extender.hibernate.configuration;
+
+import com.liferay.portal.spring.hibernate.PortletHibernateConfiguration;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * @author Miguel Pastor
+ */
+public class OsgiBundleHibernateConfiguration
+	extends PortletHibernateConfiguration implements ApplicationContextAware {
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext)
+		throws BeansException {
+
+		_classLoader = applicationContext.getClassLoader();
+	}
+
+	@Override
+	protected ClassLoader getConfigurationClassLoader() {
+		return _classLoader;
+	}
+
+	@Override
+	protected String[] getConfigurationResources() {
+		return new String[] {"META-INF/module-hbm.xml"};
+	}
+
+	private ClassLoader _classLoader;
+
+}
---
+++ osgi/modules/spring-extender/src/main/java/com/liferay/spring/extender/hibernate/session/OsgiBundleSessionFactory.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.spring.extender.hibernate.session;
+
+import com.liferay.portal.dao.orm.hibernate.PortletSessionFactoryImpl;
+import com.liferay.spring.extender.classloader.BundleResolverClassLoader;
+import com.liferay.spring.extender.hibernate.configuration.OsgiBundleHibernateConfiguration;
+
+import javax.sql.DataSource;
+
+import org.eclipse.gemini.blueprint.context.BundleContextAware;
+
+import org.hibernate.SessionFactory;
+
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author Miguel Pastor
+ */
+public class OsgiBundleSessionFactory extends PortletSessionFactoryImpl
+	implements BundleContextAware {
+
+	@Override
+	public ClassLoader getSessionFactoryClassLoader() {
+		return _classLoader;
+	}
+
+	@Override
+	public void setBundleContext(BundleContext bundleContext) {
+		_classLoader = new BundleResolverClassLoader(
+			bundleContext.getBundle(), null);
+	}
+
+	@Override
+	protected SessionFactory createSessionFactory(DataSource dataSource) {
+		OsgiBundleHibernateConfiguration osgiBundleHibernateConfiguration =
+			new OsgiBundleHibernateConfiguration();
+
+		osgiBundleHibernateConfiguration.setDataSource(dataSource);
+
+		SessionFactory sessionFactory;
+
+		try {
+			sessionFactory =
+				osgiBundleHibernateConfiguration.buildSessionFactory();
+		}
+		catch (Exception e) {
+			return null;
+		}
+
+		return sessionFactory;
+	}
+
+	private ClassLoader _classLoader;
+
+}
