848a9fa8ca226bae3c92e462d90b9f52b7ad47bc
Michael Han <michael.han@liferay.com>
2014-04-03 05:55:37 -0500

    LPS-45663 update user groups to perform searchCount against search index

--- portal-impl/src/com/liferay/portal/service/impl/UserGroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserGroupLocalServiceImpl.java
@@ -18,6 +18,7 @@ import com.liferay.portal.DuplicateUserGroupException;
 import com.liferay.portal.NoSuchUserGroupException;
 import com.liferay.portal.RequiredUserGroupException;
 import com.liferay.portal.UserGroupNameException;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.lar.PortletDataHandlerKeys;
@@ -698,7 +699,43 @@ public class UserGroupLocalServiceImpl extends UserGroupLocalServiceBaseImpl {
 			LinkedHashMap<String, Object> params)
 		throws SystemException {
 
-		return userGroupFinder.countByKeywords(companyId, keywords, params);
+		if (!PropsValues.USER_GROUPS_INDEXER_ENABLED ||
+			!PropsValues.USER_GROUPS_SEARCH_WITH_INDEX) {
+
+			return userGroupFinder.countByKeywords(companyId, keywords, params);
+		}
+
+		String name = null;
+		String description = null;
+		boolean andOperator = false;
+
+		if (Validator.isNotNull(keywords)) {
+			name = keywords;
+			description = keywords;
+		}
+		else {
+			andOperator = true;
+		}
+
+		if (params != null) {
+			params.put("keywords", keywords);
+		}
+
+		try {
+			Indexer indexer = IndexerRegistryUtil.nullSafeGetIndexer(
+				UserGroup.class);
+
+			SearchContext searchContext = buildSearchContext(
+				companyId, name, description, params, andOperator,
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
+
+			Hits hits = indexer.search(searchContext);
+
+			return hits.getLength();
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
 	}
 
 	/**
@@ -723,8 +760,28 @@ public class UserGroupLocalServiceImpl extends UserGroupLocalServiceBaseImpl {
 			LinkedHashMap<String, Object> params, boolean andOperator)
 		throws SystemException {
 
-		return userGroupFinder.countByC_N_D(
-			companyId, name, description, params, andOperator);
+		if (!PropsValues.USER_GROUPS_INDEXER_ENABLED ||
+			!PropsValues.USER_GROUPS_SEARCH_WITH_INDEX) {
+
+			return userGroupFinder.countByC_N_D(
+				companyId, name, description, params, andOperator);
+		}
+
+		try {
+			Indexer indexer = IndexerRegistryUtil.nullSafeGetIndexer(
+				UserGroup.class);
+
+			SearchContext searchContext = buildSearchContext(
+				companyId, name, description, params, true, QueryUtil.ALL_POS,
+				QueryUtil.ALL_POS, null);
+
+			Hits hits = indexer.search(searchContext);
+
+			return hits.getLength();
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
 	}
 
 	@Override
@@ -947,12 +1004,10 @@ public class UserGroupLocalServiceImpl extends UserGroupLocalServiceBaseImpl {
 			}
 		}
 
-		QueryConfig queryConfig = new QueryConfig();
+		QueryConfig queryConfig = searchContext.getQueryConfig();
 
 		queryConfig.setHighlightEnabled(false);
 		queryConfig.setScoreEnabled(false);
-
-		searchContext.setQueryConfig(queryConfig);
 
 		if (sort != null) {
 			searchContext.setSorts(sort);
