e0100697ce67a6f7a4d489c5accc9bfdc6ea9844
Jorge Ferrer <jorge.ferrer@liferay.com>
2014-04-04 10:37:06 +0200

    LPS-43591 Revert previous changes to Action class

--- portal-impl/src/com/liferay/portlet/sites/action/EditGroupAssignmentsAction.java
+++ portal-impl/src/com/liferay/portlet/sites/action/EditGroupAssignmentsAction.java
@@ -261,22 +261,8 @@ public class EditGroupAssignmentsAction extends PortletAction {
 		long[] removeRoleIds = StringUtil.split(
 			ParamUtil.getString(actionRequest, "removeRoleIds"), 0L);
 
-		if (addRoleIds.length > 0) {
-			if (!UserLocalServiceUtil.hasGroupUser(groupId, user.getUserId())) {
-				ServiceContext serviceContext =
-					ServiceContextFactory.getInstance(actionRequest);
-
-				long[] userIds = new long[] {user.getUserId()};
-
-				UserServiceUtil.addGroupUsers(groupId, userIds, serviceContext);
-
-				LiveUsers.joinGroup(user.getCompanyId(), groupId, userIds);
-			}
-
-			UserGroupRoleServiceUtil.addUserGroupRoles(
-				user.getUserId(), groupId, addRoleIds);
-		}
-
+		UserGroupRoleServiceUtil.addUserGroupRoles(
+			user.getUserId(), groupId, addRoleIds);
 		UserGroupRoleServiceUtil.deleteUserGroupRoles(
 			user.getUserId(), groupId, removeRoleIds);
 	}
