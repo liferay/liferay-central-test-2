8b28ce2752cc83c98f326ee76f65f30b8288c9df
Carlos Sierra <carlos.sierra@liferay.com>
2014-04-07 10:29:43 +0200

    LPS-45262 Use containsKey before trying to retrieve key

--- portal-impl/src/com/liferay/portal/util/LocalizationImpl.java
+++ portal-impl/src/com/liferay/portal/util/LocalizationImpl.java
@@ -1100,14 +1100,16 @@ public class LocalizationImpl implements Localization {
 
 		String value = null;
 
-		try {
-			value = resourceBundle.getString(key);
+		if (resourceBundle.containsKey(key)) {
+			try {
+				value = resourceBundle.getString(key);
 
-			value = new String(
-				value.getBytes(StringPool.ISO_8859_1), StringPool.UTF8);
-		}
+				value = new String(
+					value.getBytes(StringPool.ISO_8859_1), StringPool.UTF8);
+			}
 
-		catch (Exception e) {
+			catch (Exception e) {
+			}
 		}
 
 		if (Validator.isNotNull(value)) {
