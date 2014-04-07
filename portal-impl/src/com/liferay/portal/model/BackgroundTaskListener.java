b0bdc8ed27cb3840990f282dc4def6432ade9c0e
Levente Hudak <levente.hudak@liferay.com>
2014-04-07 11:14:12 +0200

    LPS-45449 Adding a model listener

---
+++ portal-impl/src/com/liferay/portal/model/BackgroundTaskListener.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.model;
+
+import com.liferay.portal.ModelListenerException;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.service.ExportImportConfigurationLocalServiceUtil;
+
+import java.io.Serializable;
+
+import java.util.Map;
+
+/**
+ * @author Levente Hudák
+ */
+public class BackgroundTaskListener extends BaseModelListener<BackgroundTask> {
+
+	@Override
+	public void onBeforeRemove(BackgroundTask backgroundTask)
+		throws ModelListenerException {
+
+		Map<String, Serializable> taskContextMap =
+			backgroundTask.getTaskContextMap();
+
+		long exportImportConfigurationId = MapUtil.getLong(
+			taskContextMap, "exportImportConfigurationId");
+
+		if (exportImportConfigurationId > 0) {
+			try {
+				ExportImportConfiguration exportImportConfiguration =
+					ExportImportConfigurationLocalServiceUtil.
+						getExportImportConfiguration(
+							exportImportConfigurationId);
+
+				if (exportImportConfiguration.getStatus() ==
+						WorkflowConstants.STATUS_DRAFT) {
+
+					ExportImportConfigurationLocalServiceUtil.
+						deleteExportImportConfiguration(
+							exportImportConfiguration);
+				}
+			}
+			catch (Exception e) {
+				throw new ModelListenerException(e);
+			}
+		}
+	}
+
+}
