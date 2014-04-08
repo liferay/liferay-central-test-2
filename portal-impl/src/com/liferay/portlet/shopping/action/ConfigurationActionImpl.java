bd65c03350c33b219f05970834d5f40844bbcce1
Chema Balsas <jose.balsas@liferay.com>
2014-04-08 12:47:09 +0200

    LPS-45011 Don't override default values

--- portal-impl/src/com/liferay/portlet/shopping/action/ConfigurationActionImpl.java
+++ portal-impl/src/com/liferay/portlet/shopping/action/ConfigurationActionImpl.java
@@ -19,8 +19,10 @@ import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.settings.Settings;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.WebKeys;
+import com.liferay.portlet.shopping.ShoppingSettings;
 
 import java.text.NumberFormat;
 import java.text.ParseException;
@@ -28,11 +30,45 @@ import java.text.ParseException;
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.PortletConfig;
+import javax.portlet.PortletRequest;
 
 /**
  * @author Brian Wing Shun Chan
  */
 public class ConfigurationActionImpl extends SettingsConfigurationAction {
+
+	@Override
+	public void postProcess(
+		long companyId, PortletRequest portletRequest, Settings settings) {
+
+		ShoppingSettings shoppingSettings = new ShoppingSettings(settings);
+
+		removeDefaultValue(
+			portletRequest, settings, "emailFromAddress",
+			shoppingSettings.getEmailFromAddress());
+		removeDefaultValue(
+			portletRequest, settings, "emailFromName",
+			shoppingSettings.getEmailFromName());
+
+		String languageId = LocaleUtil.toLanguageId(
+			LocaleUtil.getSiteDefault());
+
+		removeDefaultValue(
+			portletRequest, settings,
+			"emailOrderConfirmationBody_" + languageId,
+			shoppingSettings.getEmailOrderConfirmationBody());
+		removeDefaultValue(
+			portletRequest, settings,
+			"emailOrderConfirmationSubject_" + languageId,
+			shoppingSettings.getEmailOrderConfirmationSubject());
+		removeDefaultValue(
+			portletRequest, settings, "emailOrderShippingBody_" + languageId,
+			shoppingSettings.getEmailOrderShippingBody());
+		removeDefaultValue(
+			portletRequest, settings,
+			"emailOrderShippingSubject_" + languageId,
+			shoppingSettings.getEmailOrderShippingSubject());
+	}
 
 	@Override
 	public void processAction(
