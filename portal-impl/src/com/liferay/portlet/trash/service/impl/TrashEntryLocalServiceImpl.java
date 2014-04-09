38d7cebb8dca71a746fce0975b2ceb7cc6b8a221
shuyangzhou <shuyang.zhou@liferay.com>
2014-04-09 15:21:13 -0700

    LPS-45839 Apply customized transaction boundary to TrashEntryLocalServiceImpl.checkEntries()

--- portal-impl/src/com/liferay/portlet/trash/service/impl/TrashEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/trash/service/impl/TrashEntryLocalServiceImpl.java
@@ -26,6 +26,8 @@ import com.liferay.portal.kernel.search.IndexerRegistryUtil;
 import com.liferay.portal.kernel.search.QueryConfig;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.kernel.transaction.TransactionAttribute;
 import com.liferay.portal.kernel.trash.TrashHandler;
 import com.liferay.portal.kernel.trash.TrashHandlerRegistryUtil;
 import com.liferay.portal.kernel.util.ObjectValuePair;
@@ -129,6 +131,11 @@ public class TrashEntryLocalServiceImpl extends TrashEntryLocalServiceBaseImpl {
 	public void checkEntries() throws PortalException, SystemException {
 		ActionableDynamicQuery actionableDynamicQuery =
 			new GroupActionableDynamicQuery() {
+
+			@Override
+			protected TransactionAttribute getTransactionAttribute() {
+				return _transactionAttribute;
+			}
 
 			@Override
 			protected void performAction(Object object)
@@ -435,5 +442,10 @@ public class TrashEntryLocalServiceImpl extends TrashEntryLocalServiceBaseImpl {
 
 		return calendar.getTime();
 	}
+
+	private static final TransactionAttribute _transactionAttribute =
+		new TransactionAttribute.Builder().propagation(
+			Propagation.REQUIRES_NEW).rollbackForClasses(
+				PortalException.class, SystemException.class).build();
 
 }
