jonathanmccann/liferay-portal

LPS-45189 Create hidden input labels from the name when the label is omitted

portal-web/docroot/html/taglib/aui/input/page.jsp
@@ -36,13 +36,18 @@
 	<div class="<%= controlGroupCssClass %>">
 </c:if>
 
-<c:if test='<%= !type.equals("assetCategories") && !type.equals("hidden") && Validator.isNotNull(label) %>'>
-	<label <%= labelTag %>>
-		<c:if test='<%= !choiceField && !inlineLabel.equals("right") %>'>
-				<%= labelContent %>
-			</label>
-		</c:if>
-</c:if>
+<c:choose>
+	<c:when test='<%= !type.equals("assetCategories") && !type.equals("hidden") && Validator.isNotNull(label) %>'>
+		<label <%= labelTag %>>
+			<c:if test='<%= !choiceField && !inlineLabel.equals("right") %>'>
+					<%= labelContent %>
+				</label>
+			</c:if>
+	</c:when>
+	<c:when test='<%= !type.equals("hidden") && !type.equals("image") %>'>
+		<label class="hide-accessible" for="<%= forLabel %>"><liferay-ui:message key='<%= Validator.isNotNull(hideAccessibleLabel) ? hideAccessibleLabel : name %>' /></label>
+	</c:when>
+</c:choose>
 
 <c:if test="<%= Validator.isNotNull(prefix) || Validator.isNotNull(suffix) %>">
 	<div class="<%= addOnCssClass %>">

liferay/liferay-central-test-2

portal-web/docroot/html/taglib/aui/input/page.jsp
@@ -36,13 +36,18 @@
 	<div class="<%= controlGroupCssClass %>">
 </c:if>
 
-<c:if test='<%= !type.equals("assetCategories") && !type.equals("hidden") && Validator.isNotNull(label) %>'>
-	<label <%= labelTag %>>
-		<c:if test='<%= !choiceField && !inlineLabel.equals("right") %>'>
-				<%= labelContent %>
-			</label>
-		</c:if>
-</c:if>
+<c:choose>
+	<c:when test='<%= !type.equals("assetCategories") && !type.equals("hidden") && Validator.isNotNull(label) && !hideLabel %>'>
+		<label <%= labelTag %>>
+			<c:if test='<%= !choiceField && !inlineLabel.equals("right") %>'>
+					<%= labelContent %>
+				</label>
+			</c:if>
+	</c:when>
+	<c:when test='<%= !type.equals("hidden") && !type.equals("image") %>'>
+		<label class="hide-accessible" for="<%= forLabel %>"><liferay-ui:message key='<%= Validator.isNotNull(label) ? label : name %>' /></label>
+	</c:when>
+</c:choose>
 
 <c:if test="<%= Validator.isNotNull(prefix) || Validator.isNotNull(suffix) %>">
 	<div class="<%= addOnCssClass %>">
@@ -236,7 +241,7 @@
 	</div>
 </c:if>
 
-<c:if test='<%= !type.equals("assetCategories") && !type.equals("hidden") && Validator.isNotNull(label) %>'>
+<c:if test='<%= !type.equals("assetCategories") && !type.equals("hidden") && Validator.isNotNull(label) && !hideLabel %>'>
 	<c:if test='<%= choiceField || inlineLabel.equals("right") %>'>
 			<%= labelContent %>
 		</label>