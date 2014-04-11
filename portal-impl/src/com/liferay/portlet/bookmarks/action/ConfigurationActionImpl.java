8db5efe9b43da1496a61a75045664fa826d077ab
Iván Zaera Avellón <ivan.zaera@liferay.com>
2014-04-11 09:23:14 +0200

    LPS-45815 Change ConfigurationActionImpl to support multi value properties and validate email values

--- portal-impl/src/com/liferay/portlet/bookmarks/action/ConfigurationActionImpl.java
+++ portal-impl/src/com/liferay/portlet/bookmarks/action/ConfigurationActionImpl.java
@@ -14,9 +14,14 @@
 
 package com.liferay.portlet.bookmarks.action;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.portlet.SettingsConfigurationAction;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.settings.Settings;
+import com.liferay.portlet.bookmarks.BookmarksSettings;
 import com.liferay.portlet.bookmarks.model.BookmarksFolderConstants;
 import com.liferay.portlet.bookmarks.service.BookmarksFolderLocalServiceUtil;
 import com.liferay.portlet.documentlibrary.NoSuchFolderException;
@@ -36,9 +41,36 @@ public class ConfigurationActionImpl extends SettingsConfigurationAction {
 			ActionResponse actionResponse)
 		throws Exception {
 
+		validateEmail(actionRequest, "emailMessageAdded");
+		validateEmail(actionRequest, "emailMessageUpdated");
+		validateEmailFrom(actionRequest);
 		validateRootFolder(actionRequest);
 
+		updateEntryColumns(actionRequest);
+		updateFolderColumns(actionRequest);
+
 		super.processAction(portletConfig, actionRequest, actionResponse);
+	}
+
+	@Override
+	protected Settings getSettings(ActionRequest actionRequest)
+		throws PortalException, SystemException {
+
+		return new BookmarksSettings(super.getSettings(actionRequest));
+	}
+
+	protected void updateEntryColumns(ActionRequest actionRequest) {
+		String entryColumns = getParameter(actionRequest, "entryColumns");
+
+		setPreference(
+			actionRequest, "entryColumns", StringUtil.split(entryColumns));
+	}
+
+	protected void updateFolderColumns(ActionRequest actionRequest) {
+		String folderColumns = getParameter(actionRequest, "folderColumns");
+
+		setPreference(
+			actionRequest, "folderColumns", StringUtil.split(folderColumns));
 	}
 
 	protected void validateRootFolder(ActionRequest actionRequest)
