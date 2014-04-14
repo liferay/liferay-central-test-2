2a7d7453db846c681887633f9057d0d5ecbedb87
shuyangzhou <shuyang.zhou@liferay.com>
2014-04-14 08:21:22 -0700

    LPS-45936 Simplify

--- portal-service/src/com/liferay/portal/security/auth/CompanyThreadLocal.java
+++ portal-service/src/com/liferay/portal/security/auth/CompanyThreadLocal.java
@@ -51,20 +51,16 @@ public class CompanyThreadLocal {
 			_log.debug("setCompanyId " + companyId);
 		}
 
-		Long currentCompanyId = _companyId.get();
-
 		if (companyId > 0) {
 			try {
-				_companyId.set(companyId);
-
 				Company company = CompanyLocalServiceUtil.getCompany(companyId);
+
+				_companyId.set(companyId);
 
 				LocaleThreadLocal.setDefaultLocale(company.getLocale());
 				TimeZoneThreadLocal.setDefaultTimeZone(company.getTimeZone());
 			}
 			catch (Exception e) {
-				_companyId.set(currentCompanyId);
-
 				_log.error(e, e);
 			}
 		}
