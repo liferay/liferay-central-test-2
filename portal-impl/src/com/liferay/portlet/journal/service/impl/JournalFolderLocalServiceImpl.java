juliocamarero/liferay-portal

LPS-45615 Folders can be restricted to allow only certain structures

portal-impl/src/com/liferay/portlet/journal/service/impl/JournalFolderLocalServiceImpl.java
@@ -27,6 +27,9 @@ import com.liferay.portal.kernel.search.IndexerRegistryUtil;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.SortedArrayList;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.TreeModelFinder;
 import com.liferay.portal.kernel.util.TreePathUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
@@ -39,8 +42,10 @@ import com.liferay.portal.util.PropsValues;
 import com.liferay.portlet.asset.model.AssetEntry;
 import com.liferay.portlet.asset.model.AssetLinkConstants;
 import com.liferay.portlet.asset.util.AssetUtil;
+import com.liferay.portlet.dynamicdatamapping.model.DDMStructure;
 import com.liferay.portlet.journal.DuplicateFolderNameException;
 import com.liferay.portlet.journal.FolderNameException;
+import com.liferay.portlet.journal.NoSuchFolderException;
 import com.liferay.portlet.journal.model.JournalArticle;
 import com.liferay.portlet.journal.model.JournalFolder;
 import com.liferay.portlet.journal.model.JournalFolderConstants;
@@ -52,6 +57,7 @@ import com.liferay.portlet.trash.model.TrashVersion;
 import com.liferay.portlet.trash.util.TrashUtil;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -252,6 +258,33 @@ public class JournalFolderLocalServiceImpl
 		throws PortalException, SystemException {
 
 		return journalFolderPersistence.findByPrimaryKey(folderId);
+	}
+
+	@Override
+	public List<DDMStructure> getFolderDDMStructures(
+			long[] groupIds, long folderId, boolean inherited)
+		throws PortalException, SystemException {
+
+		if (!inherited) {
+			return journalFolderPersistence.getDDMStructures(folderId);
+		}
+
+		List<DDMStructure> ddmStructures = null;
+
+		folderId = getStructurePrimaryFolderId(folderId);
+
+		if (folderId != JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
+			ddmStructures = journalFolderPersistence.getDDMStructures(folderId);
+		}
+		else {
+			long classNameId = classNameLocalService.getClassNameId(
+				JournalArticle.class);
+
+			ddmStructures = ddmStructurePersistence.findByG_C(
+				groupIds, classNameId);
+		}
+
+		return ddmStructures;
 	}
 
 	@Override
@@ -658,7 +691,16 @@ public class JournalFolderLocalServiceImpl
 			ServiceContext serviceContext)
 		throws PortalException, SystemException {
 
+		List<Long> ddmStructureIds = getLongList(
+			serviceContext, "ddmStructuresSearchContainerPrimaryKeys");
+		boolean overrideDDMStructures = ParamUtil.getBoolean(
+			serviceContext, "overrideDDMStructures");
+
 		// Merge folders
+
+		if (!overrideDDMStructures) {
+			ddmStructureIds = Collections.emptyList();
+		}
 
 		JournalFolder folder = journalFolderPersistence.findByPrimaryKey(
 			folderId);
@@ -680,9 +722,16 @@ public class JournalFolderLocalServiceImpl
 		folder.setTreePath(folder.buildTreePath());
 		folder.setName(name);
 		folder.setDescription(description);
+		folder.setOverrideDDMStructures(overrideDDMStructures);
 		folder.setExpandoBridgeAttributes(serviceContext);
 
 		journalFolderPersistence.update(folder);
+
+		// DDMStructures
+
+		if (ddmStructureIds != null) {
+			updateFolderDDMStructures(folder, ddmStructureIds);
+		}
 
 		// Asset
 
@@ -692,6 +741,33 @@ public class JournalFolderLocalServiceImpl
 			serviceContext.getAssetLinkEntryIds());
 
 		return folder;
+	}
+
+	@Override
+	public void updateFolderDDMStructures(
+			JournalFolder folder, List<Long> ddmStructureIds)
+		throws SystemException {
+
+		List<Long> originalDDMStructureIds = getStructureIds(
+			journalFolderPersistence.getDDMStructures(folder.getFolderId()));
+
+		if (ddmStructureIds.equals(originalDDMStructureIds)) {
+			return;
+		}
+
+		for (Long ddmStructureId : ddmStructureIds) {
+			if (!originalDDMStructureIds.contains(ddmStructureId)) {
+				journalFolderPersistence.addDDMStructure(
+					folder.getFolderId(), ddmStructureId);
+			}
+		}
+
+		for (Long originalDDMStructureId : originalDDMStructureIds) {
+			if (!ddmStructureIds.contains(originalDDMStructureId)) {
+				journalFolderPersistence.removeDDMStructure(
+					folder.getFolderId(), originalDDMStructureId);
+			}
+		}
 	}
 
 	@Override
@@ -729,6 +805,26 @@ public class JournalFolderLocalServiceImpl
 		indexer.reindex(folder);
 
 		return folder;
+	}
+
+	protected SortedArrayList<Long> getLongList(
+		ServiceContext serviceContext, String name) {
+
+		String value = ParamUtil.getString(serviceContext, name);
+
+		if (value == null) {
+			return new SortedArrayList<Long>();
+		}
+
+		long[] longArray = StringUtil.split(value, 0L);
+
+		SortedArrayList<Long> longList = new SortedArrayList<Long>();
+
+		for (long longValue : longArray) {
+			longList.add(longValue);
+		}
+
+		return longList;
 	}
 
 	protected long getParentFolderId(JournalFolder folder, long parentFolderId)
@@ -779,6 +875,33 @@ public class JournalFolderLocalServiceImpl
 		}
 
 		return parentFolderId;
+	}
+
+	protected List<Long> getStructureIds(List<DDMStructure> structures) {
+		List<Long> structureIds = new SortedArrayList<Long>();
+
+		for (DDMStructure structure : structures) {
+			structureIds.add(structure.getStructureId());
+		}
+
+		return structureIds;
+	}
+
+	protected long getStructurePrimaryFolderId(long folderId)
+		throws NoSuchFolderException, SystemException {
+
+		while (folderId != JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
+			JournalFolder folder = journalFolderPersistence.findByPrimaryKey(
+				folderId);
+
+			if (folder.isOverrideDDMStructures()) {
+				break;
+			}
+
+			folderId = folder.getParentFolderId();
+		}
+
+		return folderId;
 	}
 
 	protected void mergeFolders(JournalFolder fromFolder, long toFolderId)

portal-web/docroot/html/portlet/journal/edit_folder.jsp
@@ -26,11 +26,21 @@ long folderId = BeanParamUtil.getLong(folder, request, "folderId");
 long parentFolderId = BeanParamUtil.getLong(folder, request, "parentFolderId", JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID);
 
 boolean mergeWithParentFolderDisabled = ParamUtil.getBoolean(request, "mergeWithParentFolderDisabled");
+
+boolean rootFolder = ParamUtil.getBoolean(request, "rootFolder");
 %>
 
 <portlet:actionURL var="editFolderURL">
 	<portlet:param name="struts_action" value="/journal/edit_folder" />
 </portlet:actionURL>
+
+<liferay-util:buffer var="removeDDMStructureIcon">
+	<liferay-ui:icon
+		image="unlink"
+		label="<%= true %>"
+		message="remove"
+	/>
+</liferay-util:buffer>
 
 <aui:form action="<%= editFolderURL %>" method="post" name="fm" onSubmit='<%= "event.preventDefault(); " + renderResponse.getNamespace() + "saveFolder();" %>'>
 	<aui:input name="<%= Constants.CMD %>" type="hidden" />
@@ -129,6 +139,63 @@ boolean mergeWithParentFolderDisabled = ParamUtil.getBoolean(request, "mergeWith
 			/>
 		</liferay-ui:custom-attributes-available>
 
+		<c:if test="<%= rootFolder || (folder != null) %>">
+
+			<%
+			List<DDMStructure> ddmStructures = JournalFolderLocalServiceUtil.getFolderDDMStructures(PortalUtil.getCurrentAndAncestorSiteGroupIds(scopeGroupId), folderId, false);
+
+			String headerNames = "name,null";
+			%>
+
+			<aui:field-wrapper helpMessage='<%= rootFolder ? "" : "structure-restrictions-help" %>' label='<%= rootFolder ? "" : "structure-restrictions" %>'>
+				<c:if test="<%= !rootFolder %>">
+					<aui:input checked="<%= !folder.isOverrideDDMStructures() %>" id="useDDMStructures" label="use-structure-restrictions-of-the-parent-folder" name="overrideDDMStructures" type="radio" value="<%= false %>" />
+
+					<aui:input checked="<%= folder.isOverrideDDMStructures() %>" id="overrideDDMStructures" label="define-specific-structure-restrictions-for-this-folder" name="overrideDDMStructures" type="radio" value="<%= true %>" />
+				</c:if>
+
+				<div id="<portlet:namespace />overrideParentSettings">
+					<c:if test="<%= !rootFolder %>">
+						<liferay-ui:search-container
+							headerNames="<%= headerNames %>"
+							total="<%= ddmStructures.size() %>"
+						>
+							<liferay-ui:search-container-results
+								results="<%= ddmStructures %>"
+							/>
+
+							<liferay-ui:search-container-row
+								className="com.liferay.portlet.dynamicdatamapping.model.DDMStructure"
+								escapedModel="<%= true %>"
+								keyProperty="structureId"
+								modelVar="ddmStructure"
+							>
+								<liferay-ui:search-container-column-text
+									name="name"
+									value="<%= ddmStructure.getName(locale) %>"
+								/>
+
+								<liferay-ui:search-container-column-text>
+									<a class="modify-link" data-rowId="<%= ddmStructure.getStructureId() %>" href="javascript:;"><%= removeDDMStructureIcon %></a>
+								</liferay-ui:search-container-column-text>
+							</liferay-ui:search-container-row>
+
+							<liferay-ui:search-iterator paginate="<%= false %>" />
+						</liferay-ui:search-container>
+
+						<liferay-ui:icon
+							cssClass="modify-link select-structure"
+							iconCssClass="icon-search"
+							label="<%= true %>"
+							linkCssClass="btn"
+							message="choose-structure"
+							url='<%= "javascript:" + renderResponse.getNamespace() + "openDDMStructureSelector();" %>'
+						/>
+					</c:if>
+				</div>
+			</aui:field-wrapper>
+		</c:if>
+
 		<c:if test="<%= folder == null %>">
 			<aui:field-wrapper label="permissions">
 				<liferay-ui:input-permissions
@@ -151,6 +218,64 @@ boolean mergeWithParentFolderDisabled = ParamUtil.getBoolean(request, "mergeWith
 
 		submitForm(document.<portlet:namespace />fm);
 	}
+
+	function <portlet:namespace />openDDMStructureSelector() {
+		Liferay.Util.openDDMPortlet(
+			{
+				basePortletURL: '<%= PortletURLFactoryUtil.create(request, PortletKeys.DYNAMIC_DATA_MAPPING, themeDisplay.getPlid(), PortletRequest.RENDER_PHASE) %>',
+				dialog: {
+					destroyOnHide: true
+				},
+				eventName: '<portlet:namespace />selectStructure',
+				groupId: <%= scopeGroupId %>,
+				refererPortletName: '<%= PortletKeys.JOURNAL_CONTENT %>',
+				showGlobalScope: true,
+				struts_action: '/dynamic_data_mapping/select_structure',
+				title: '<%= UnicodeLanguageUtil.get(pageContext, "structures") %>'
+			},
+			function(event) {
+				<portlet:namespace />selectStructure(event.ddmstructureid, event.name);
+			}
+		);
+	}
+
+	Liferay.provide(
+		window,
+		'<portlet:namespace />selectStructure',
+		function(ddmStructureId, ddmStructureName) {
+			var A = AUI();
+
+			var searchContainer = Liferay.SearchContainer.get('<portlet:namespace />ddmStructuresSearchContainer');
+
+			var ddmStructureLink = '<a class="modify-link" data-rowId="' + ddmStructureId + '" href="javascript:;"><%= UnicodeFormatter.toString(removeDDMStructureIcon) %></a>';
+
+			searchContainer.addRow([ddmStructureName, ddmStructureLink], ddmStructureId);
+
+			searchContainer.updateDataStore();
+		},
+		['liferay-search-container']
+	);
+
+	Liferay.Util.toggleRadio('<portlet:namespace />overrideDDMStructures', '<portlet:namespace />overrideParentSettings', '');
+	Liferay.Util.toggleRadio('<portlet:namespace />useDDMStructures', '', '<portlet:namespace />overrideParentSettings');
+</aui:script>
+
+<aui:script use="liferay-search-container">
+	var searchContainer = Liferay.SearchContainer.get('<portlet:namespace />ddmStructuresSearchContainer');
+
+	searchContainer.get('contentBox').delegate(
+		'click',
+		function(event) {
+			var A = AUI();
+
+			var link = event.currentTarget;
+
+			var tr = link.ancestor('tr');
+
+			searchContainer.deleteRow(tr, link.getAttribute('data-rowId'));
+		},
+		'.modify-link'
+	);
 </aui:script>
 
 <%

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portlet/journal/service/impl/JournalFolderLocalServiceImpl.java
@@ -39,8 +39,10 @@ import com.liferay.portal.util.PropsValues;
 import com.liferay.portlet.asset.model.AssetEntry;
 import com.liferay.portlet.asset.model.AssetLinkConstants;
 import com.liferay.portlet.asset.util.AssetUtil;
+import com.liferay.portlet.dynamicdatamapping.model.DDMStructure;
 import com.liferay.portlet.journal.DuplicateFolderNameException;
 import com.liferay.portlet.journal.FolderNameException;
+import com.liferay.portlet.journal.NoSuchFolderException;
 import com.liferay.portlet.journal.model.JournalArticle;
 import com.liferay.portlet.journal.model.JournalFolder;
 import com.liferay.portlet.journal.model.JournalFolderConstants;
@@ -52,6 +54,7 @@ import com.liferay.portlet.trash.model.TrashVersion;
 import com.liferay.portlet.trash.util.TrashUtil;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -397,6 +400,24 @@ public class JournalFolderLocalServiceImpl
 	}
 
 	@Override
+	public long getOverridedDDMStructuresFolderId(long folderId)
+		throws NoSuchFolderException, SystemException {
+
+		while (folderId != JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
+			JournalFolder folder = journalFolderPersistence.findByPrimaryKey(
+				folderId);
+
+			if (folder.isOverrideDDMStructures()) {
+				break;
+			}
+
+			folderId = folder.getParentFolderId();
+		}
+
+		return folderId;
+	}
+
+	@Override
 	public void getSubfolderIds(
 			List<Long> folderIds, long groupId, long folderId)
 		throws SystemException {
@@ -658,7 +679,25 @@ public class JournalFolderLocalServiceImpl
 			ServiceContext serviceContext)
 		throws PortalException, SystemException {
 
+		return updateFolder(
+			userId, folderId, parentFolderId, name, description, null, false,
+			mergeWithParentFolder, serviceContext);
+	}
+
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public JournalFolder updateFolder(
+			long userId, long folderId, long parentFolderId, String name,
+			String description, List<Long> ddmStructureIds,
+			boolean overrideDDMStructures, boolean mergeWithParentFolder,
+			ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
 		// Merge folders
+
+		if (!overrideDDMStructures) {
+			ddmStructureIds = Collections.emptyList();
+		}
 
 		JournalFolder folder = journalFolderPersistence.findByPrimaryKey(
 			folderId);
@@ -680,9 +719,16 @@ public class JournalFolderLocalServiceImpl
 		folder.setTreePath(folder.buildTreePath());
 		folder.setName(name);
 		folder.setDescription(description);
+		folder.setOverrideDDMStructures(overrideDDMStructures);
 		folder.setExpandoBridgeAttributes(serviceContext);
 
 		journalFolderPersistence.update(folder);
+
+		// DDMStructures
+
+		if (ddmStructureIds != null) {
+			updateFolderDDMStructures(folder, ddmStructureIds);
+		}
 
 		// Asset
 
@@ -692,6 +738,33 @@ public class JournalFolderLocalServiceImpl
 			serviceContext.getAssetLinkEntryIds());
 
 		return folder;
+	}
+
+	@Override
+	public void updateFolderDDMStructures(
+			JournalFolder folder, List<Long> ddmStructureIds)
+		throws SystemException {
+
+		List<Long> originalDDMStructureIds = getDDMStructureIds(
+			journalFolderPersistence.getDDMStructures(folder.getFolderId()));
+
+		if (ddmStructureIds.equals(originalDDMStructureIds)) {
+			return;
+		}
+
+		for (Long ddmStructureId : ddmStructureIds) {
+			if (!originalDDMStructureIds.contains(ddmStructureId)) {
+				journalFolderPersistence.addDDMStructure(
+					folder.getFolderId(), ddmStructureId);
+			}
+		}
+
+		for (Long originalDDMStructureId : originalDDMStructureIds) {
+			if (!ddmStructureIds.contains(originalDDMStructureId)) {
+				journalFolderPersistence.removeDDMStructure(
+					folder.getFolderId(), originalDDMStructureId);
+			}
+		}
 	}
 
 	@Override
@@ -729,6 +802,16 @@ public class JournalFolderLocalServiceImpl
 		indexer.reindex(folder);
 
 		return folder;
+	}
+
+	protected List<Long> getDDMStructureIds(List<DDMStructure> ddmStructures) {
+		List<Long> ddmStructureIds = new ArrayList<Long>();
+
+		for (DDMStructure ddmStructure : ddmStructures) {
+			ddmStructureIds.add(ddmStructure.getStructureId());
+		}
+
+		return ddmStructureIds;
 	}
 
 	protected long getParentFolderId(JournalFolder folder, long parentFolderId)

portal-web/docroot/html/portlet/journal/edit_folder.jsp
@@ -26,11 +26,21 @@ long folderId = BeanParamUtil.getLong(folder, request, "folderId");
 long parentFolderId = BeanParamUtil.getLong(folder, request, "parentFolderId", JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID);
 
 boolean mergeWithParentFolderDisabled = ParamUtil.getBoolean(request, "mergeWithParentFolderDisabled");
+
+boolean rootFolder = ParamUtil.getBoolean(request, "rootFolder");
 %>
 
 <portlet:actionURL var="editFolderURL">
 	<portlet:param name="struts_action" value="/journal/edit_folder" />
 </portlet:actionURL>
+
+<liferay-util:buffer var="removeDDMStructureIcon">
+	<liferay-ui:icon
+		image="unlink"
+		label="<%= true %>"
+		message="remove"
+	/>
+</liferay-util:buffer>
 
 <aui:form action="<%= editFolderURL %>" method="post" name="fm" onSubmit='<%= "event.preventDefault(); " + renderResponse.getNamespace() + "saveFolder();" %>'>
 	<aui:input name="<%= Constants.CMD %>" type="hidden" />
@@ -129,6 +139,63 @@ boolean mergeWithParentFolderDisabled = ParamUtil.getBoolean(request, "mergeWith
 			/>
 		</liferay-ui:custom-attributes-available>
 
+		<c:if test="<%= rootFolder || (folder != null) %>">
+
+			<%
+			List<DDMStructure> ddmStructures = DDMStructureLocalServiceUtil.getJournalFolderStructures(PortalUtil.getCurrentAndAncestorSiteGroupIds(scopeGroupId), folderId, false);
+
+			String headerNames = "name,null";
+			%>
+
+			<aui:field-wrapper helpMessage='<%= rootFolder ? "" : "structure-restrictions-help" %>' label='<%= rootFolder ? "" : "structure-restrictions" %>'>
+				<c:if test="<%= !rootFolder %>">
+					<aui:input checked="<%= !folder.isOverrideDDMStructures() %>" id="useDDMStructures" label="use-structure-restrictions-of-the-parent-folder" name="overrideDDMStructures" type="radio" value="<%= false %>" />
+
+					<aui:input checked="<%= folder.isOverrideDDMStructures() %>" id="overrideDDMStructures" label="define-specific-structure-restrictions-for-this-folder" name="overrideDDMStructures" type="radio" value="<%= true %>" />
+				</c:if>
+
+				<div id="<portlet:namespace />overrideParentSettings">
+					<c:if test="<%= !rootFolder %>">
+						<liferay-ui:search-container
+							headerNames="<%= headerNames %>"
+							total="<%= ddmStructures.size() %>"
+						>
+							<liferay-ui:search-container-results
+								results="<%= ddmStructures %>"
+							/>
+
+							<liferay-ui:search-container-row
+								className="com.liferay.portlet.dynamicdatamapping.model.DDMStructure"
+								escapedModel="<%= true %>"
+								keyProperty="structureId"
+								modelVar="ddmStructure"
+							>
+								<liferay-ui:search-container-column-text
+									name="name"
+									value="<%= ddmStructure.getName(locale) %>"
+								/>
+
+								<liferay-ui:search-container-column-text>
+									<a class="modify-link" data-rowId="<%= ddmStructure.getStructureId() %>" href="javascript:;"><%= removeDDMStructureIcon %></a>
+								</liferay-ui:search-container-column-text>
+							</liferay-ui:search-container-row>
+
+							<liferay-ui:search-iterator paginate="<%= false %>" />
+						</liferay-ui:search-container>
+
+						<liferay-ui:icon
+							cssClass="modify-link select-structure"
+							iconCssClass="icon-search"
+							label="<%= true %>"
+							linkCssClass="btn"
+							message="choose-structure"
+							url='<%= "javascript:" + renderResponse.getNamespace() + "openDDMStructureSelector();" %>'
+						/>
+					</c:if>
+				</div>
+			</aui:field-wrapper>
+		</c:if>
+
 		<c:if test="<%= folder == null %>">
 			<aui:field-wrapper label="permissions">
 				<liferay-ui:input-permissions
@@ -151,6 +218,64 @@ boolean mergeWithParentFolderDisabled = ParamUtil.getBoolean(request, "mergeWith
 
 		submitForm(document.<portlet:namespace />fm);
 	}
+
+	function <portlet:namespace />openDDMStructureSelector() {
+		Liferay.Util.openDDMPortlet(
+			{
+				basePortletURL: '<%= PortletURLFactoryUtil.create(request, PortletKeys.DYNAMIC_DATA_MAPPING, themeDisplay.getPlid(), PortletRequest.RENDER_PHASE) %>',
+				dialog: {
+					destroyOnHide: true
+				},
+				eventName: '<portlet:namespace />selectStructure',
+				groupId: <%= scopeGroupId %>,
+				refererPortletName: '<%= PortletKeys.JOURNAL_CONTENT %>',
+				showGlobalScope: true,
+				struts_action: '/dynamic_data_mapping/select_structure',
+				title: '<%= UnicodeLanguageUtil.get(pageContext, "structures") %>'
+			},
+			function(event) {
+				<portlet:namespace />selectStructure(event.ddmstructureid, event.name);
+			}
+		);
+	}
+
+	Liferay.provide(
+		window,
+		'<portlet:namespace />selectStructure',
+		function(ddmStructureId, ddmStructureName) {
+			var A = AUI();
+
+			var searchContainer = Liferay.SearchContainer.get('<portlet:namespace />ddmStructuresSearchContainer');
+
+			var ddmStructureLink = '<a class="modify-link" data-rowId="' + ddmStructureId + '" href="javascript:;"><%= UnicodeFormatter.toString(removeDDMStructureIcon) %></a>';
+
+			searchContainer.addRow([ddmStructureName, ddmStructureLink], ddmStructureId);
+
+			searchContainer.updateDataStore();
+		},
+		['liferay-search-container']
+	);
+
+	Liferay.Util.toggleRadio('<portlet:namespace />overrideDDMStructures', '<portlet:namespace />overrideParentSettings', '');
+	Liferay.Util.toggleRadio('<portlet:namespace />useDDMStructures', '', '<portlet:namespace />overrideParentSettings');
+</aui:script>
+
+<aui:script use="liferay-search-container">
+	var searchContainer = Liferay.SearchContainer.get('<portlet:namespace />ddmStructuresSearchContainer');
+
+	searchContainer.get('contentBox').delegate(
+		'click',
+		function(event) {
+			var A = AUI();
+
+			var link = event.currentTarget;
+
+			var tr = link.ancestor('tr');
+
+			searchContainer.deleteRow(tr, link.getAttribute('data-rowId'));
+		},
+		'.modify-link'
+	);
 </aui:script>
 
 <%