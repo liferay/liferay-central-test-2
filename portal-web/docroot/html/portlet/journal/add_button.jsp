juliocamarero/liferay-portal

LPS-45615 Show only the allowed ddmStructureIds for the actual folder

portal-web/docroot/html/portlet/journal/add_button.jsp
@@ -21,7 +21,13 @@ JournalFolder folder = (JournalFolder)request.getAttribute("view.jsp-folder");
 
 long folderId = GetterUtil.getLong((String)request.getAttribute("view.jsp-folderId"));
 
-List<DDMStructure> ddmStructures = DDMStructureServiceUtil.getStructures(PortalUtil.getCurrentAndAncestorSiteGroupIds(scopeGroupId), PortalUtil.getClassNameId(JournalArticle.class));
+boolean inherited = true;
+
+if (folder != null) {
+	inherited = !folder.isOverrideDDMStructures();
+}
+
+List<DDMStructure> ddmStructures = JournalFolderLocalServiceUtil.getFolderDDMStructures(PortalUtil.getCurrentAndAncestorSiteGroupIds(scopeGroupId), folderId, inherited);
 %>
 
 <aui:nav-item dropdown="<%= true %>" id="addButtonContainer" label="add">

liferay/liferay-central-test-2

portal-web/docroot/html/portlet/journal/add_button.jsp
@@ -21,7 +21,13 @@ JournalFolder folder = (JournalFolder)request.getAttribute("view.jsp-folder");
 
 long folderId = GetterUtil.getLong((String)request.getAttribute("view.jsp-folderId"));
 
-List<DDMStructure> ddmStructures = DDMStructureServiceUtil.getStructures(PortalUtil.getCurrentAndAncestorSiteGroupIds(scopeGroupId), PortalUtil.getClassNameId(JournalArticle.class));
+boolean inherited = true;
+
+if (folder != null) {
+	inherited = !folder.isOverrideDDMStructures();
+}
+
+List<DDMStructure> ddmStructures = DDMStructureServiceUtil.getJournalFolderStructures(PortalUtil.getCurrentAndAncestorSiteGroupIds(scopeGroupId), folderId, inherited);
 %>
 
 <aui:nav-item dropdown="<%= true %>" id="addButtonContainer" label="add">