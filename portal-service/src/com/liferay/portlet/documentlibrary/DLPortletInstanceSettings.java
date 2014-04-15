c2bd8f7515b91ecc6e0bbc47430dc7118c9572f4
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2014-04-15 10:10:00 +0200

    LPS-45983 Copy from DLSettings - no logic changes

---
+++ portal-service/src/com/liferay/portlet/documentlibrary/DLPortletInstanceSettings.java
@@ -0,0 +1,228 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.documentlibrary;
+
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.settings.BaseServiceSettings;
+import com.liferay.portal.settings.FallbackKeys;
+import com.liferay.portal.settings.LocalizedValuesMap;
+import com.liferay.portal.settings.Settings;
+import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
+import com.liferay.portlet.documentlibrary.util.DLUtil;
+
+import java.util.Set;
+
+/**
+ * @author Sergio González
+ */
+public class DLPortletInstanceSettings extends BaseServiceSettings {
+
+	public DLPortletInstanceSettings(Settings settings) {
+		super(settings, _fallbackKeys);
+	}
+
+	public long getDefaultFolderId() {
+		return typedSettings.getLongValue(
+			"rootFolderId", DLFolderConstants.DEFAULT_PARENT_FOLDER_ID);
+	}
+
+	public String getDisplayViews() {
+		return typedSettings.getValue("displayViews");
+	}
+
+	public LocalizedValuesMap getEmailFileEntryAddedBody() {
+		return typedSettings.getLocalizedValuesMap("emailFileEntryAddedBody");
+	}
+
+	public String getEmailFileEntryAddedBodyXml() {
+		LocalizedValuesMap emailFileEntryAddedBody =
+			getEmailFileEntryAddedBody();
+
+		return emailFileEntryAddedBody.getLocalizationXml();
+	}
+
+	public boolean getEmailFileEntryAddedEnabled() {
+		return typedSettings.getBooleanValue("emailFileEntryAddedEnabled");
+	}
+
+	public LocalizedValuesMap getEmailFileEntryAddedSubject() {
+		return typedSettings.getLocalizedValuesMap(
+			"emailFileEntryAddedSubject");
+	}
+
+	public String getEmailFileEntryAddedSubjectXml() {
+		LocalizedValuesMap emailFileEntryAddedSubject =
+			getEmailFileEntryAddedSubject();
+
+		return emailFileEntryAddedSubject.getLocalizationXml();
+	}
+
+	public LocalizedValuesMap getEmailFileEntryUpdatedBody() {
+		return typedSettings.getLocalizedValuesMap("emailFileEntryUpdatedBody");
+	}
+
+	public String getEmailFileEntryUpdatedBodyXml() {
+		LocalizedValuesMap emailFileEntryUpdatedBody =
+			getEmailFileEntryUpdatedBody();
+
+		return emailFileEntryUpdatedBody.getLocalizationXml();
+	}
+
+	public boolean getEmailFileEntryUpdatedEnabled() {
+		return typedSettings.getBooleanValue("emailFileEntryUpdatedEnabled");
+	}
+
+	public LocalizedValuesMap getEmailFileEntryUpdatedSubject() {
+		return typedSettings.getLocalizedValuesMap(
+			"emailFileEntryUpdatedSubject");
+	}
+
+	public String getEmailFileEntryUpdatedSubjectXml() {
+		LocalizedValuesMap emailFileEntryUpdatedSubject =
+			getEmailFileEntryUpdatedSubject();
+
+		return emailFileEntryUpdatedSubject.getLocalizationXml();
+	}
+
+	public String getEmailFromAddress() {
+		return typedSettings.getValue("emailFromAddress");
+	}
+
+	public String getEmailFromName() {
+		return typedSettings.getValue("emailFromName");
+	}
+
+	public boolean getEnableCommentRatings() {
+		return typedSettings.getBooleanValue("enableCommentRatings");
+	}
+
+	public boolean getEnableRatings() {
+		return typedSettings.getBooleanValue("enableRatings");
+	}
+
+	public boolean getEnableRelatedAssets() {
+		return typedSettings.getBooleanValue("enableRelatedAssets");
+	}
+
+	public int getEntriesPerPage() {
+		return typedSettings.getIntegerValue("entriesPerPage");
+	}
+
+	public String getEntryColumns() {
+		return typedSettings.getValue("entryColumns");
+	}
+
+	public int getFileEntriesPerPage() {
+		return typedSettings.getIntegerValue("fileEntriesPerPage");
+	}
+
+	public String getFileEntryColumns() {
+		return typedSettings.getValue("fileEntryColumns");
+	}
+
+	public String getFolderColumns() {
+		return typedSettings.getValue("folderColumns");
+	}
+
+	public int getFoldersPerPage() {
+		return typedSettings.getIntegerValue("foldersPerPage");
+	}
+
+	public String[] getMediaGalleryMimeTypes() {
+		return typedSettings.getValues(
+			"mimeTypes", _defaultMediaGalleryMimeTypes);
+	}
+
+	public long getRootFolderId() {
+		return typedSettings.getLongValue(
+			"rootFolderId", DLFolderConstants.DEFAULT_PARENT_FOLDER_ID);
+	}
+
+	public boolean getShowActions() {
+		return typedSettings.getBooleanValue("showActions");
+	}
+
+	public boolean getShowFolderMenu() {
+		return typedSettings.getBooleanValue("showFolderMenu");
+	}
+
+	public boolean getShowFoldersSearch() {
+		return typedSettings.getBooleanValue("showFoldersSearch");
+	}
+
+	public boolean getShowSubfolders() {
+		return typedSettings.getBooleanValue("showSubfolders");
+	}
+
+	public boolean getShowTabs() {
+		return typedSettings.getBooleanValue("showTabs");
+	}
+
+	private static String[] _defaultMediaGalleryMimeTypes;
+	private static FallbackKeys _fallbackKeys = new FallbackKeys();
+
+	static {
+		_fallbackKeys.add("displayViews", PropsKeys.DL_DISPLAY_VIEWS);
+		_fallbackKeys.add(
+			"emailFileEntryAddedBody",
+			PropsKeys.DL_EMAIL_FILE_ENTRY_ADDED_BODY);
+		_fallbackKeys.add(
+			"emailFileEntryAddedEnabled",
+			PropsKeys.DL_EMAIL_FILE_ENTRY_ADDED_ENABLED);
+		_fallbackKeys.add(
+			"emailFileEntryAddedSubject",
+			PropsKeys.DL_EMAIL_FILE_ENTRY_ADDED_SUBJECT);
+		_fallbackKeys.add(
+			"emailFileEntryUpdatedBody",
+			PropsKeys.DL_EMAIL_FILE_ENTRY_UPDATED_BODY);
+		_fallbackKeys.add(
+			"emailFileEntryUpdatedEnabled",
+			PropsKeys.DL_EMAIL_FILE_ENTRY_UPDATED_ENABLED);
+		_fallbackKeys.add(
+			"emailFileEntryUpdatedSubject",
+			PropsKeys.DL_EMAIL_FILE_ENTRY_UPDATED_SUBJECT);
+		_fallbackKeys.add(
+			"emailFromAddress", PropsKeys.DL_EMAIL_FROM_ADDRESS,
+			PropsKeys.ADMIN_EMAIL_FROM_ADDRESS);
+		_fallbackKeys.add(
+			"emailFromName", PropsKeys.DL_EMAIL_FROM_NAME,
+			PropsKeys.ADMIN_EMAIL_FROM_NAME);
+		_fallbackKeys.add(
+			"enableCommentRatings", PropsKeys.DL_COMMENT_RATINGS_ENABLED);
+		_fallbackKeys.add("enableRatings", PropsKeys.DL_RATINGS_ENABLED);
+		_fallbackKeys.add(
+			"enableRelatedAssets", PropsKeys.DL_RELATED_ASSETS_ENABLED);
+		_fallbackKeys.add(
+			"entriesPerPage", PropsKeys.SEARCH_CONTAINER_PAGE_DEFAULT_DELTA);
+		_fallbackKeys.add("entryColumns", PropsKeys.DL_ENTRY_COLUMNS);
+		_fallbackKeys.add("fileEntryColumns", PropsKeys.DL_FILE_ENTRY_COLUMNS);
+		_fallbackKeys.add("folderColumns", PropsKeys.DL_FOLDER_COLUMNS);
+		_fallbackKeys.add(
+			"foldersPerPage", PropsKeys.SEARCH_CONTAINER_PAGE_DEFAULT_DELTA);
+		_fallbackKeys.add(
+			"fileEntriesPerPage",
+			PropsKeys.SEARCH_CONTAINER_PAGE_DEFAULT_DELTA);
+		_fallbackKeys.add(
+			"showFoldersSearch", PropsKeys.DL_FOLDERS_SEARCH_VISIBLE);
+		_fallbackKeys.add(
+			"showSubfolders", PropsKeys.DL_FOLDERS_SEARCH_VISIBLE);
+
+		Set<String> allMimeTypes = DLUtil.getAllMediaGalleryMimeTypes();
+
+		_defaultMediaGalleryMimeTypes = allMimeTypes.toArray(
+			new String[allMimeTypes.size()]);
+	}
+
+}
