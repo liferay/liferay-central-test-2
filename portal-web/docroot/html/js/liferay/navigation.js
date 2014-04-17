f429f0a0633a35342dcd5b96ba11a0ce506b96d6
michael.bradford@liferay.com <michael.bradford@liferay.com>
2014-04-17 15:02:48 -0700

    LPS-42246 - Briefly unable to remove pages after clicking between two pages multiple times

--- portal-web/docroot/html/js/liferay/navigation.js
+++ portal-web/docroot/html/js/liferay/navigation.js
@@ -272,12 +272,6 @@ AUI.add(
 						}
 					},
 
-					_hoverNavItem: function(event) {
-						var instance = this;
-
-						event.currentTarget.toggleClass('lfr-nav-hover', (event.type == 'mouseenter'));
-					},
-
 					_makeDeletable: function() {
 						var instance = this;
 
@@ -303,8 +297,6 @@ AUI.add(
 								A.bind('_handleKeyDown', instance),
 								navItemSelector
 							);
-
-							navBlock.delegate(['mouseenter', 'mouseleave'], instance._hoverNavItem, '.lfr-nav-deletable', instance);
 
 							instance._deleteButton(navItems);
 						}
