8be0753ce854c3aaf9dbeee744ef4f5bdf8f1762
Julio Camarero <julio.camarero@liferay.com>
2014-04-21 12:21:54 +0200

    LPS-42403 fix letter

--- util-taglib/src/com/liferay/taglib/ui/util/InputAssetLinksDisplayContext.java
+++ util-taglib/src/com/liferay/taglib/ui/util/InputAssetLinksDisplayContext.java
@@ -57,7 +57,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.PageContext;
 
 /**
- * @author Jos� Manuel Navarro
+ * @author José Manuel Navarro
  */
 public class InputAssetLinksDisplayContext {
 
