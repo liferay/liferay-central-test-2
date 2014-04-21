fa53f0003d546c2a646a41d43da14aa82b9cc12f
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-04-21 12:13:59 +0200

    Revert "LPS-45766 added bean analyzer as Flexjson transformer to collect information about serializable properties"
    This reverts commit 6b7cd2df5fea2ebd7ba8ff7ccd8c8d3a85b8b203.

--- portal-impl/src/com/liferay/portal/json/transformer/FlexjsonObjectJSONTransformer.java
+++ portal-impl/src/com/liferay/portal/json/transformer/FlexjsonObjectJSONTransformer.java
@@ -44,15 +44,7 @@ public class FlexjsonObjectJSONTransformer
 			(List<PathExpression>)BeanUtil.getDeclaredProperty(
 				getContext(), "pathExpressions");
 
-		String path = getPath();
-
-		addIncludesAndExcludesForType(type, pathExpressions, path);
-
-		super.transform(object);
-	}
-
-	protected void addIncludesAndExcludesForType(
-			Class<?> type, List<PathExpression> pathExpressions, String path) {
+		String path = _getPath();
 
 		String[] excludes = JSONIncludesManagerUtil.lookupExcludes(type);
 
@@ -61,27 +53,8 @@ public class FlexjsonObjectJSONTransformer
 		String[] includes = JSONIncludesManagerUtil.lookupIncludes(type);
 
 		_include(pathExpressions, path, includes);
-	}
 
-	protected String getPath() {
-		JSONContext jsonContext = getContext();
-
-		Path path = jsonContext.getPath();
-
-		List<String> paths = path.getPath();
-
-		if (paths.isEmpty()) {
-			return StringPool.BLANK;
-		}
-
-		StringBundler sb = new StringBundler(paths.size() * 2);
-
-		for (String curPath : paths) {
-			sb.append(curPath);
-			sb.append(CharPool.PERIOD);
-		}
-
-		return sb.toString();
+		super.transform(object);
 	}
 
 	private void _exclude(
@@ -108,6 +81,27 @@ public class FlexjsonObjectJSONTransformer
 
 			pathExpressions.add(pathExpression);
 		}
+	}
+
+	private String _getPath() {
+		JSONContext jsonContext = getContext();
+
+		Path path = jsonContext.getPath();
+
+		List<String> paths = path.getPath();
+
+		if (paths.isEmpty()) {
+			return StringPool.BLANK;
+		}
+
+		StringBundler sb = new StringBundler(paths.size() * 2);
+
+		for (String curPath : paths) {
+			sb.append(curPath);
+			sb.append(CharPool.PERIOD);
+		}
+
+		return sb.toString();
 	}
 
 	private void _include(
