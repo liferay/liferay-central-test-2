f6471aecbdeb7e7bc15a6e6f03560cc242c03d44
Brian Chan <brian.chan@liferay.com>
2014-04-22 10:27:09 -0700

    LPS-46133 SF

--- portal-impl/test/integration/com/liferay/portal/service/BackgroundTaskLocalServiceTest.java
+++ portal-impl/test/integration/com/liferay/portal/service/BackgroundTaskLocalServiceTest.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.portal.service;
 
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskConstants;
