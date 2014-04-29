2b811c5058662c25f2012d7d9e57b56400ce225a
Marcellus Tavares <marcellus.tavares@liferay.com>
2014-04-29 14:10:39 -0300

    LPS-46263 SF

--- portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/impl/DDMTemplateLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/impl/DDMTemplateLocalServiceImpl.java
@@ -1355,36 +1355,12 @@ public class DDMTemplateLocalServiceImpl
 		DDMTemplate template = ddmTemplateLocalService.getDDMTemplate(
 			templateId);
 
-		File smallImageFile = copySmallImage(template);
+		File smallImageFile = getSmallImageFile(template);
 
 		return updateTemplate(
 			templateId, classPK, nameMap, descriptionMap, type, mode, language,
 			script, cacheable, template.isSmallImage(),
 			template.getSmallImageURL(), smallImageFile, serviceContext);
-	}
-
-	protected File copySmallImage(DDMTemplate template) throws SystemException {
-		File smallImageFile = null;
-
-		if (template.isSmallImage() &&
-			Validator.isNull(template.getSmallImageURL())) {
-
-			Image smallImage = ImageUtil.fetchByPrimaryKey(
-				template.getSmallImageId());
-
-			if (smallImage != null) {
-				smallImageFile = FileUtil.createTempFile(smallImage.getType());
-
-				try {
-					FileUtil.write(smallImageFile, smallImage.getTextObj());
-				}
-				catch (IOException ioe) {
-					_log.error(ioe, ioe);
-				}
-			}
-		}
-
-		return smallImageFile;
 	}
 
 	protected DDMTemplate copyTemplate(
@@ -1393,7 +1369,7 @@ public class DDMTemplateLocalServiceImpl
 			ServiceContext serviceContext)
 		throws PortalException, SystemException {
 
-		File smallImageFile = copySmallImage(template);
+		File smallImageFile = getSmallImageFile(template);
 
 		return addTemplate(
 			userId, template.getGroupId(), template.getClassNameId(), classPK,
@@ -1419,6 +1395,32 @@ public class DDMTemplateLocalServiceImpl
 		}
 
 		return script;
+	}
+
+	protected File getSmallImageFile(DDMTemplate template)
+		throws SystemException {
+
+		File smallImageFile = null;
+
+		if (template.isSmallImage() &&
+			Validator.isNull(template.getSmallImageURL())) {
+
+			Image smallImage = ImageUtil.fetchByPrimaryKey(
+				template.getSmallImageId());
+
+			if (smallImage != null) {
+				smallImageFile = FileUtil.createTempFile(smallImage.getType());
+
+				try {
+					FileUtil.write(smallImageFile, smallImage.getTextObj());
+				}
+				catch (IOException ioe) {
+					_log.error(ioe, ioe);
+				}
+			}
+		}
+
+		return smallImageFile;
 	}
 
 	protected void saveImages(
