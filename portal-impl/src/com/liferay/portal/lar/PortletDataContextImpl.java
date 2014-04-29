b0678521681d63116b70c08474b2988c4bdc58de
Máté Thurzó <mate.thurzo@liferay.com>
2014-04-29 13:45:19 +0200

    LPS-45854 Replace attribute addition, to avoid overriding attribute value

--- portal-impl/src/com/liferay/portal/lar/PortletDataContextImpl.java
+++ portal-impl/src/com/liferay/portal/lar/PortletDataContextImpl.java
@@ -543,11 +543,11 @@ public class PortletDataContextImpl implements PortletDataContext {
 		String referenceKey = getReferenceKey(classedModel);
 
 		if (missing) {
-			referenceElement.addAttribute("missing", Boolean.TRUE.toString());
-
 			if (_references.contains(referenceKey)) {
 				return referenceElement;
 			}
+
+			referenceElement.addAttribute("missing", Boolean.TRUE.toString());
 
 			if (!_missingReferences.contains(referenceKey)) {
 				_missingReferences.add(referenceKey);
