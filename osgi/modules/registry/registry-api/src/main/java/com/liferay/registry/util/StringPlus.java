5dc8908b8e96cf26c59f10bc2dc6e36c416f8def
Raymond Auge <raymond.auge@liferay.com>
2014-04-30 10:59:29 -0400

    LPS-46425 As a developer I'd like to have a helper utility to work with OSGi specified String+ service properties

---
+++ osgi/modules/registry/registry-api/src/main/java/com/liferay/registry/util/StringPlus.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.registry.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Raymond Augé
+ */
+public class StringPlus {
+
+	@SuppressWarnings("unchecked")
+	public static List<String> asList(Object object) {
+		if (String.class.isInstance(object)) {
+			return Collections.singletonList((String)object);
+		}
+		else if (String[].class.isInstance(object)) {
+			return Arrays.asList((String[])object);
+		}
+		else if (Collection.class.isInstance(object)) {
+			Collection<?> collection = (Collection<?>)object;
+
+			if (!collection.isEmpty() &&
+				String.class.isInstance(collection.iterator().next())) {
+
+				return new ArrayList<String>((Collection<String>)object);
+			}
+		}
+
+		return Collections.emptyList();
+	}
+
+}
