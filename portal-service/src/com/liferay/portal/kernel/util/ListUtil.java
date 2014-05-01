131179635a4bdcadaf7639b4861b20f15d24a83c
Hugo Huijser <hugo.huijser@liferay.com>
2014-05-01 15:56:12 -0700

    Revert "LPS-46445 Deprecate ListUtil.fromArray"
    This reverts commit f11c0a02f6d89122b82fe2e9beec1552da2eb14c.

--- portal-service/src/com/liferay/portal/kernel/util/ListUtil.java
+++ portal-service/src/com/liferay/portal/kernel/util/ListUtil.java
@@ -108,10 +108,6 @@ public class ListUtil {
 			inputList, new ArrayList<T>(inputList.size()), predicateFilter);
 	}
 
-	/**
-	 * @deprecated As of 7.0.0, see LPS-46445
-	 */
-	@Deprecated
 	public static <E> List<E> fromArray(E[] array) {
 		if (ArrayUtil.isEmpty(array)) {
 			return new ArrayList<E>();
