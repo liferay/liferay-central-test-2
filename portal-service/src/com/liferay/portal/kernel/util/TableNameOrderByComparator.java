07f0276da3c17d8e32a04298a9b7a1e3ccba6260
JM <jose.navarro@liferay.com>
2014-05-05 11:24:07 +0200

    LPS-43952 Added new decorator class to prepend table name in OrderBy clauses. Added corresponding tests

---
+++ portal-service/src/com/liferay/portal/kernel/util/TableNameOrderByComparator.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.util;
+
+/**
+ * @author José Manuel Navarro
+ */
+public class TableNameOrderByComparator extends OrderByComparator {
+
+	public TableNameOrderByComparator(
+		OrderByComparator decoratedComparator, String tableName) {
+
+		super();
+
+		_decoratedComparator = decoratedComparator;
+
+		setTableName(tableName);
+	}
+
+	@Override
+	public int compare(Object obj1, Object obj2) {
+		return _decoratedComparator.compare(obj1, obj2);
+	}
+
+	public OrderByComparator getDecoratedComparator() {
+		return _decoratedComparator;
+	}
+
+	@Override
+	public String getOrderBy() {
+		String originalOrderBy = _decoratedComparator.getOrderBy();
+
+		if (_tableName == null) {
+			return originalOrderBy;
+		}
+
+		String[] sortedFields = StringUtil.split(originalOrderBy);
+
+		StringBundler finalOrderBy = new StringBundler(
+			(3 * sortedFields.length) - 1);
+
+		for (int i = 0; i < sortedFields.length; ++i) {
+			String field = sortedFields[i];
+
+			if (field.indexOf(CharPool.PERIOD) == -1) {
+				finalOrderBy.append(_tableName);
+			}
+
+			finalOrderBy.append(StringUtil.trim(field));
+
+			if (i < (sortedFields.length - 1)) {
+				finalOrderBy.append(ORDER_BY_SEPARATOR);
+			}
+		}
+
+		return finalOrderBy.toString();
+	}
+
+	@Override
+	public String[] getOrderByConditionFields() {
+		return _decoratedComparator.getOrderByConditionFields();
+	}
+
+	@Override
+	public Object[] getOrderByConditionValues(Object obj) {
+		return _decoratedComparator.getOrderByConditionValues(obj);
+	}
+
+	@Override
+	public String[] getOrderByFields() {
+		return _decoratedComparator.getOrderByFields();
+	}
+
+	@Override
+	public boolean isAscending() {
+		return _decoratedComparator.isAscending();
+	}
+
+	@Override
+	public boolean isAscending(String field) {
+		return _decoratedComparator.isAscending(field);
+	}
+
+	public void setTableName(String tableName) {
+		if ((tableName != null) && (tableName.length() > 0)) {
+			_tableName = tableName + CharPool.PERIOD;
+		}
+		else {
+			_tableName = null;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return _decoratedComparator.toString();
+	}
+
+	private static final String ORDER_BY_SEPARATOR = ", ";
+
+	private OrderByComparator _decoratedComparator;
+	private String _tableName;
+
+}
---
+++ portal-service/test/unit/com/liferay/portal/kernel/util/TableNameOrderByComparatorTest.java
@@ -0,0 +1,126 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author José Manuel Navarro
+ */
+public class TableNameOrderByComparatorTest {
+
+	@Test
+	public void testOrderByWithBlankTableNameReturnsUndecoratedTableName() {
+		_tableNameOrderByComparator = new TableNameOrderByComparator(
+			new TestableOrderByComparator("field1, field2"), "");
+
+		String actualOrderBy = _tableNameOrderByComparator.getOrderBy();
+		String expectedOrderBy = "field1, field2";
+
+		Assert.assertEquals(expectedOrderBy, actualOrderBy);
+	}
+
+	@Test
+	public void testOrderByWithMultipleFieldsReturnsDecoratedTableName() {
+		_tableNameOrderByComparator = new TableNameOrderByComparator(
+			new TestableOrderByComparator("field1, field2"), "table");
+
+		String actualOrderBy = _tableNameOrderByComparator.getOrderBy();
+		String expectedOrderBy = "table.field1, table.field2";
+
+		Assert.assertEquals(expectedOrderBy, actualOrderBy);
+	}
+
+	@Test
+	public void testOrderByWithMultipleTableNameReturnsOriginalTableName() {
+		_tableNameOrderByComparator = new TableNameOrderByComparator(
+			new TestableOrderByComparator("table1.field1, field2"), "table2");
+
+		String actualOrderBy = _tableNameOrderByComparator.getOrderBy();
+		String expectedOrderBy = "table1.field1, table2.field2";
+
+		Assert.assertEquals(expectedOrderBy, actualOrderBy);
+	}
+
+	@Test
+	public void testOrderByWithNullTableNameReturnsUndecoratedTableName() {
+		_tableNameOrderByComparator = new TableNameOrderByComparator(
+			new TestableOrderByComparator("field1, field2"), null);
+
+		String actualOrderBy = _tableNameOrderByComparator.getOrderBy();
+		String expectedOrderBy = "field1, field2";
+
+		Assert.assertEquals(expectedOrderBy, actualOrderBy);
+	}
+
+	@Test
+	public void testOrderByWithSingleFieldReturnsDecoratedTableName() {
+		_tableNameOrderByComparator = new TableNameOrderByComparator(
+			new TestableOrderByComparator("field"), "table");
+
+		String actualOrderBy = _tableNameOrderByComparator.getOrderBy();
+		String expectedOrderBy = "table.field";
+
+		Assert.assertEquals(expectedOrderBy, actualOrderBy);
+	}
+
+	@Test
+	public void testOrderByWithSingleTableNameReturnsOriginalTableName() {
+		_tableNameOrderByComparator = new TableNameOrderByComparator(
+			new TestableOrderByComparator("table1.field1"), "table2");
+
+		String actualOrderBy = _tableNameOrderByComparator.getOrderBy();
+		String expectedOrderBy = "table1.field1";
+
+		Assert.assertEquals(expectedOrderBy, actualOrderBy);
+	}
+
+	@Test
+	public void testOrderByWithSortDirectionReturnsDecoratedTableName() {
+		_tableNameOrderByComparator = new TableNameOrderByComparator(
+			new TestableOrderByComparator("field ASC"), "table");
+
+		String actualOrderBy = _tableNameOrderByComparator.getOrderBy();
+		String expectedOrderBy = "table.field ASC";
+
+		Assert.assertEquals(expectedOrderBy, actualOrderBy);
+	}
+
+	private TableNameOrderByComparator _tableNameOrderByComparator;
+
+	private class TestableOrderByComparator extends OrderByComparator {
+
+		public TestableOrderByComparator(String orderByClause) {
+			super();
+
+			_orderByClause = orderByClause;
+		}
+
+		@Override
+		public int compare(Object obj1, Object obj2) {
+			return 0;
+		}
+
+		@Override
+		public String getOrderBy() {
+			return _orderByClause;
+		}
+
+		private String _orderByClause;
+
+	}
+
+}
