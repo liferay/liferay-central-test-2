97eca0a031aa9159c3f18723e9baa36fd46b2cc8
Eduardo Garcia <eduardo.garcia@liferay.com>
2014-05-05 12:34:21 +0200

    LPS-45069 Support ADT in Language Portlet - Handler

---
+++ portal-impl/src/com/liferay/portlet/language/template/LanguagePortletDisplayTemplateHandler.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.language.template;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portletdisplaytemplate.BasePortletDisplayTemplateHandler;
+import com.liferay.portal.kernel.template.TemplateVariableGroup;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portlet.portletdisplaytemplate.util.PortletDisplayTemplateConstants;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Eduardo Garcia
+ */
+public class LanguagePortletDisplayTemplateHandler
+	extends BasePortletDisplayTemplateHandler {
+
+	@Override
+	public String getClassName() {
+		return Locale.class.getName();
+	}
+
+	@Override
+	public String getName(Locale locale) {
+		String portletTitle = PortalUtil.getPortletTitle(
+			PortletKeys.LANGUAGE, locale);
+
+		return portletTitle.concat(StringPool.SPACE).concat(
+			LanguageUtil.get(locale, "template"));
+	}
+
+	@Override
+	public String getResourceName() {
+		return PortletKeys.LANGUAGE;
+	}
+
+	@Override
+	public Map<String, TemplateVariableGroup> getTemplateVariableGroups(
+			long classPK, String language, Locale locale)
+		throws Exception {
+
+		Map<String, TemplateVariableGroup> templateVariableGroups =
+			super.getTemplateVariableGroups(classPK, language, locale);
+
+		TemplateVariableGroup templateVariableGroup =
+			templateVariableGroups.get("fields");
+
+		templateVariableGroup.empty();
+
+		templateVariableGroup.addCollectionVariable(
+			"languages", List.class, PortletDisplayTemplateConstants.ENTRIES,
+			"language", Locale.class, "curLanguage", "displayName");
+
+		String[] restrictedVariables = getRestrictedVariables(language);
+
+		TemplateVariableGroup documentServicesTemplateVariableGroup =
+			new TemplateVariableGroup("document-services", restrictedVariables);
+
+		documentServicesTemplateVariableGroup.setAutocompleteEnabled(false);
+
+		templateVariableGroups.put(
+			documentServicesTemplateVariableGroup.getLabel(),
+			documentServicesTemplateVariableGroup);
+
+		return templateVariableGroups;
+	}
+
+}
