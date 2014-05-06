5a749db123937cd16293675e9e1b577af2827689
akosthurzo <akos.thurzo@liferay.com>
2014-05-06 13:56:39 +0200

    LPS-46579 return in FileEntrySMDH as well when the repository got imported

--- portal-impl/src/com/liferay/portlet/documentlibrary/lar/FileEntryStagedModelDataHandler.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/lar/FileEntryStagedModelDataHandler.java
@@ -246,6 +246,8 @@ public class FileEntryStagedModelDataHandler
 			StagedModelDataHandlerUtil.importReferenceStagedModel(
 				portletDataContext, fileEntry, Repository.class,
 				fileEntry.getRepositoryId());
+
+			return;
 		}
 
 		if (fileEntry.getFolderId() !=
