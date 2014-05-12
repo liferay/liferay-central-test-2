7edc852445804dcabcbab3d57acad51507f868c9
Máté Thurzó <mate.thurzo@liferay.com>
2014-05-12 13:34:29 +0200

    LPS-46289 Keeping the _references variable up to date, when serializing a new entity

--- portal-impl/src/com/liferay/portal/lar/PortletDataContextImpl.java
+++ portal-impl/src/com/liferay/portal/lar/PortletDataContextImpl.java
@@ -242,19 +242,18 @@ public class PortletDataContextImpl implements PortletDataContext {
 			auditedModel.setUserUuid(auditedModel.getUserUuid());
 		}
 
-		if (!isResourceMain(classedModel)) {
-			addZipEntry(path, classedModel);
+		if (isResourceMain(classedModel)) {
+			long classPK = ExportImportClassedModelUtil.getClassPK(
+				classedModel);
 
-			return;
+			addAssetLinks(clazz, classPK);
+			addAssetTags(clazz, classPK);
+			addExpando(element, path, classedModel, clazz);
+			addLocks(clazz, String.valueOf(classPK));
+			addPermissions(clazz, classPK);
 		}
 
-		long classPK = ExportImportClassedModelUtil.getClassPK(classedModel);
-
-		addAssetLinks(clazz, classPK);
-		addAssetTags(clazz, classPK);
-		addExpando(element, path, classedModel, clazz);
-		addLocks(clazz, String.valueOf(classPK));
-		addPermissions(clazz, classPK);
+		_references.add(getReferenceKey(classedModel));
 
 		addZipEntry(path, classedModel);
 	}
