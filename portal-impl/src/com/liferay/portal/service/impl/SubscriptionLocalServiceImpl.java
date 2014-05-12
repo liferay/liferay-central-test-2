6ef714e6312d36fc680bb51203e8a8b1e391714f
adolfo.perez <adolfo.perez@liferay.com>
2014-05-12 12:20:55 +0200

    LPS-29741 ignore invalid unsubscriptions

--- portal-impl/src/com/liferay/portal/service/impl/SubscriptionLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/SubscriptionLocalServiceImpl.java
@@ -218,10 +218,12 @@ public class SubscriptionLocalServiceImpl
 		User user = userPersistence.findByPrimaryKey(userId);
 		long classNameId = classNameLocalService.getClassNameId(className);
 
-		Subscription subscription = subscriptionPersistence.findByC_U_C_C(
+		Subscription subscription = subscriptionPersistence.fetchByC_U_C_C(
 			user.getCompanyId(), userId, classNameId, classPK);
 
-		deleteSubscription(subscription);
+		if (subscription != null) {
+			deleteSubscription(subscription);
+		}
 	}
 
 	/**
