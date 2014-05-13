4032f05ee3ce697e9e86d2cdf20371d39de38a23
adolfo.perez <adolfo.perez@liferay.com>
2014-05-13 15:11:38 +0200

    LPS-46748 add general classes and interfaces for class types

---
+++ portal-service/src/com/liferay/portlet/asset/model/ClassType.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.asset.model;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+
+import java.util.List;
+
+/**
+ * @author Adolfo Pérez
+ */
+public interface ClassType {
+
+	public ClassTypeField getClassTypeField(String fieldName)
+		throws PortalException, SystemException;
+
+	public List<ClassTypeField> getClassTypeFields()
+		throws PortalException, SystemException;
+
+	public List<ClassTypeField> getClassTypeFields(int start, int end)
+		throws PortalException, SystemException;
+
+	public int getClassTypeFieldsCount()
+		throws PortalException, SystemException;
+
+	public long getClassTypeId();
+
+	public String getName();
+
+}
