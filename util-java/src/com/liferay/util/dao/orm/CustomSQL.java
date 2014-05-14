494ec3ff599c005b71d83196e17fd0fe4471bf55
Brian Chan <brian.chan@liferay.com>
2014-05-13 17:15:37 -0700

    LPS-46679 SF

--- util-java/src/com/liferay/util/dao/orm/CustomSQL.java
+++ util-java/src/com/liferay/util/dao/orm/CustomSQL.java
@@ -46,7 +46,6 @@ import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
