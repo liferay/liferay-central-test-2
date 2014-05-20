70129378ac4ad9f003ce557f34e85002f6bdf85b
JM <jose.navarro@liferay.com>
2014-05-20 10:45:15 +0200

    LPS-22792 Upgrade process

--- portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeAsset.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeAsset.java
@@ -14,10 +14,26 @@
 
 package com.liferay.portal.upgrade.v7_0_0;
 
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.upgrade.v7_0_0.util.AssetEntryTable;
+import com.liferay.portlet.asset.model.AssetCategoryConstants;
+import com.liferay.portlet.asset.util.AssetVocabularySettingsHelper;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Gergely Mathe
@@ -26,6 +42,40 @@ public class UpgradeAsset extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
+		doUpgradeAssetEntry();
+		doUpgradeAllAssetVocabularies();
+	}
+
+	protected void doUpgradeAllAssetVocabularies() throws Exception {
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet result = null;
+
+		try {
+			connection = DataAccess.getUpgradeOptimizedConnection();
+
+			statement = connection.prepareStatement(
+				"SELECT vocabularyId, settings_ FROM AssetVocabulary");
+
+			result = statement.executeQuery();
+
+			while (result.next()) {
+				Long key = result.getLong("vocabularyId");
+				String settings = result.getString("settings_");
+
+				String newSettings = doUpgradeVocabularySettings(settings);
+
+				runSQL(
+					"UPDATE AssetVocabulary SET settings_ = '" + newSettings +
+						"' WHERE vocabularyId = " + key);
+			}
+		}
+		finally {
+			DataAccess.cleanUp(connection, statement, result);
+		}
+	}
+
+	protected void doUpgradeAssetEntry() throws Exception {
 		try {
 			runSQL("alter_column_type AssetEntry description TEXT null");
 			runSQL("alter_column_type AssetEntry summary TEXT null");
@@ -36,6 +86,40 @@ public class UpgradeAsset extends UpgradeProcess {
 				AssetEntryTable.TABLE_SQL_CREATE,
 				AssetEntryTable.TABLE_SQL_ADD_INDEXES);
 		}
+	}
+
+	protected String doUpgradeVocabularySettings(String settings) {
+		UnicodeProperties oldProperties = new UnicodeProperties(true);
+		oldProperties.fastLoad(settings);
+
+		AssetVocabularySettingsHelper newProperties =
+			new AssetVocabularySettingsHelper();
+
+		newProperties.setMultiValued(
+			GetterUtil.getBoolean(
+				oldProperties.getProperty("multiValued"), true));
+
+		long[] classNameIds = StringUtil.split(
+			oldProperties.getProperty("selectedClassNameIds"), 0L);
+
+		long[] requiredClassNameIds = StringUtil.split(
+			oldProperties.getProperty("requiredClassNameIds"), 0L);
+
+		long[] classTypeIds = new long[classNameIds.length];
+
+		Arrays.fill(classTypeIds, AssetCategoryConstants.ALL_CLASS_TYPE_IDS);
+
+		boolean[] requireds = new boolean[classNameIds.length];
+
+		for (int i = 0; i < classNameIds.length; i++) {
+			requireds[i] = ArrayUtil.contains(
+				requiredClassNameIds, classNameIds[i]);
+		}
+
+		newProperties.setClassNameAndTypeIds(
+			classNameIds, classTypeIds, requireds);
+
+		return newProperties.toString();
 	}
 
 }
---
+++ portal-impl/test/unit/com/liferay/portal/upgrade/v7_0_0/UpgradeAssetVocabularyTest.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.v7_0_0;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author José Manuel Navarro
+ */
+public class UpgradeAssetVocabularyTest {
+
+	@Test
+	public void testUpgradeWithEmptySettings() {
+		String oldSettings = "multiValued=false\nselectedClassNameIds=0\n";
+		String expectedSettings =
+			"multiValued=false\nselectedClassNameIds=0:-1\n";
+
+		_testUpgrade(oldSettings, expectedSettings);
+	}
+
+	@Test
+	public void testUpgradeWithMultipleRequiredSettings() {
+		String oldSettings =
+			"multiValued=true\nrequiredClassNameIds=10005\n" +
+				"selectedClassNameIds=10007,10005,10109\n";
+		String expectedSettings =
+			"multiValued=true\nrequiredClassNameIds=10005:-1\n" +
+				"selectedClassNameIds=10007:-1,10005:-1,10109:-1\n";
+
+		_testUpgrade(oldSettings, expectedSettings);
+	}
+
+	@Test
+	public void testUpgradeWithMultiValuedSettings() {
+		String oldSettings =
+			"multiValued=true\nselectedClassNameIds=10007,10005\n";
+		String expectedSettings =
+			"multiValued=true\nselectedClassNameIds=10007:-1,10005:-1\n";
+
+		_testUpgrade(oldSettings, expectedSettings);
+	}
+
+	@Test
+	public void testUpgradeWithoutRequiredSettings() {
+		String oldSettings = "multiValued=false\nselectedClassNameIds=10007\n";
+		String expectedSettings =
+			"multiValued=false\nselectedClassNameIds=10007:-1\n";
+
+		_testUpgrade(oldSettings, expectedSettings);
+	}
+
+	@Test
+	public void testUpgradeWithRequiredSettings() {
+		String oldSettings =
+			"multiValued=false\nrequiredClassNameIds=10007\n" +
+				"selectedClassNameIds=10007\n";
+		String expectedSettings =
+			"multiValued=false\nrequiredClassNameIds=10007:-1\n" +
+				"selectedClassNameIds=10007:-1\n";
+
+		_testUpgrade(oldSettings, expectedSettings);
+	}
+
+	private void _testUpgrade(String oldSettings, String expectedSettings) {
+		UpgradeAsset upgradeAsset = new UpgradeAsset();
+
+		String upgradedSettings = upgradeAsset.doUpgradeVocabularySettings(
+			oldSettings);
+
+		Assert.assertEquals(expectedSettings, upgradedSettings);
+	}
+
+}
