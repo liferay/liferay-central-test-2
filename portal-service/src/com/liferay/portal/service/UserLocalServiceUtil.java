brianchandotcom/liferay-portal

LPS-46132 autogenerated

portal-service/src/com/liferay/portal/service/UserLocalServiceUtil.java
@@ -2296,6 +2296,35 @@ public class UserLocalServiceUtil {
 		return getService().getUserIdByScreenName(companyId, screenName);
 	}
 
+	public static java.util.List<com.liferay.portal.model.User> getUsersByGroups(
+		java.lang.String keywords, long userId, long[] groupIds, int start,
+		int end)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return getService()
+				   .getUsersByGroups(keywords, userId, groupIds, start, end);
+	}
+
+	public static java.util.List<com.liferay.portal.model.User> getUsersBySocialRelationsTypes(
+		java.lang.String keywords, long userId, int[] types, long[] groupIds,
+		int start, int end)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return getService()
+				   .getUsersBySocialRelationsTypes(keywords, userId, types,
+			groupIds, start, end);
+	}
+
+	public static java.util.List<com.liferay.portal.model.User> getUsersBySocialRelationsTypesGroups(
+		java.lang.String keywords, long userId, int[] types, long[] groupIds,
+		int start, int end)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return getService()
+				   .getUsersBySocialRelationsTypesGroups(keywords, userId,
+			types, groupIds, start, end);
+	}
+
 	/**
 	* Returns <code>true</code> if the password policy has been assigned to the
 	* user.

portal-service/src/com/liferay/portal/service/UserLocalServiceWrapper.java
@@ -2430,6 +2430,36 @@ public class UserLocalServiceWrapper implements UserLocalService,
 		return _userLocalService.getUserIdByScreenName(companyId, screenName);
 	}
 
+	@Override
+	public java.util.List<com.liferay.portal.model.User> getUsersByGroups(
+		java.lang.String keywords, long userId, long[] groupIds, int start,
+		int end)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return _userLocalService.getUsersByGroups(keywords, userId, groupIds,
+			start, end);
+	}
+
+	@Override
+	public java.util.List<com.liferay.portal.model.User> getUsersBySocialRelationsTypes(
+		java.lang.String keywords, long userId, int[] types, long[] groupIds,
+		int start, int end)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return _userLocalService.getUsersBySocialRelationsTypes(keywords,
+			userId, types, groupIds, start, end);
+	}
+
+	@Override
+	public java.util.List<com.liferay.portal.model.User> getUsersBySocialRelationsTypesGroups(
+		java.lang.String keywords, long userId, int[] types, long[] groupIds,
+		int start, int end)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return _userLocalService.getUsersBySocialRelationsTypesGroups(keywords,
+			userId, types, groupIds, start, end);
+	}
+
 	/**
 	* Returns <code>true</code> if the password policy has been assigned to the
 	* user.

liferay/liferay-central-test-2

portal-service/src/com/liferay/portal/service/UserLocalServiceUtil.java
@@ -2624,6 +2624,29 @@ public class UserLocalServiceUtil {
 			screenName, emailAddress, status, params, andSearch);
 	}
 
+	public static java.util.List<com.liferay.portal.model.User> searchSocial(
+		long companyId, java.lang.String keywords, long[] groupIds, int start,
+		int end) throws com.liferay.portal.kernel.exception.SystemException {
+		return getService()
+				   .searchSocial(companyId, keywords, groupIds, start, end);
+	}
+
+	public static java.util.List<com.liferay.portal.model.User> searchSocial(
+		java.lang.String keywords, long userId, int[] types, int start, int end)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return getService().searchSocial(keywords, userId, types, start, end);
+	}
+
+	public static java.util.List<com.liferay.portal.model.User> searchSocial(
+		java.lang.String keywords, long userId, int[] types, long[] groupIds,
+		int start, int end)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return getService()
+				   .searchSocial(keywords, userId, types, groupIds, start, end);
+	}
+
 	public static com.liferay.portal.kernel.search.BaseModelSearchResult<com.liferay.portal.model.User> searchUsers(
 		long companyId, java.lang.String keywords, int status,
 		java.util.LinkedHashMap<java.lang.String, java.lang.Object> params,

portal-service/src/com/liferay/portal/service/UserLocalServiceWrapper.java
@@ -2766,6 +2766,33 @@ public class UserLocalServiceWrapper implements UserLocalService,
 	}
 
 	@Override
+	public java.util.List<com.liferay.portal.model.User> searchSocial(
+		long companyId, java.lang.String keywords, long[] groupIds, int start,
+		int end) throws com.liferay.portal.kernel.exception.SystemException {
+		return _userLocalService.searchSocial(companyId, keywords, groupIds,
+			start, end);
+	}
+
+	@Override
+	public java.util.List<com.liferay.portal.model.User> searchSocial(
+		java.lang.String keywords, long userId, int[] types, int start, int end)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return _userLocalService.searchSocial(keywords, userId, types, start,
+			end);
+	}
+
+	@Override
+	public java.util.List<com.liferay.portal.model.User> searchSocial(
+		java.lang.String keywords, long userId, int[] types, long[] groupIds,
+		int start, int end)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return _userLocalService.searchSocial(keywords, userId, types,
+			groupIds, start, end);
+	}
+
+	@Override
 	public com.liferay.portal.kernel.search.BaseModelSearchResult<com.liferay.portal.model.User> searchUsers(
 		long companyId, java.lang.String keywords, int status,
 		java.util.LinkedHashMap<java.lang.String, java.lang.Object> params,