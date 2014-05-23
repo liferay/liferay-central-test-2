0ee48110b55ffe65e12754077776508ab8d53c94
Eduardo Garcia <eduardo.garcia@liferay.com>
2014-05-23 12:46:03 +0200

    LPS-46142 Append -btn for every cssClass

--- util-taglib/src/com/liferay/taglib/aui/NavTag.java
+++ util-taglib/src/com/liferay/taglib/aui/NavTag.java
@@ -17,6 +17,7 @@ package com.liferay.taglib.aui;
 import com.liferay.portal.kernel.dao.search.DisplayTerms;
 import com.liferay.portal.kernel.dao.search.SearchContainer;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
@@ -60,9 +61,11 @@ public class NavTag extends BaseNavTag implements BodyTag {
 			String cssClass = getCssClass();
 
 			if (Validator.isNotNull(cssClass)) {
-				sb.append(StringPool.SPACE);
-				sb.append(cssClass);
-				sb.append("-btn");
+				for (String s : StringUtil.split(cssClass, CharPool.SPACE)) {
+					sb.append(StringPool.SPACE);
+					sb.append(s);
+					sb.append("-btn");
+				}
 			}
 
 			if (_hasSearchResults()) {
