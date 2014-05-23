brianchandotcom/liferay-portal

LPS-22792 autogenerated

portal-service/src/com/liferay/portlet/asset/model/AssetVocabulary.java
@@ -37,7 +37,20 @@ public interface AssetVocabulary extends AssetVocabularyModel, PersistedModel {
 	public java.util.List<com.liferay.portlet.asset.model.AssetCategory> getCategories()
 		throws com.liferay.portal.kernel.exception.SystemException;
 
+	public long[] getRequiredClassNameIds();
+
+	public long[] getSelectedClassNameIds();
+
 	public com.liferay.portal.kernel.util.UnicodeProperties getSettingsProperties();
+
+	public boolean hasMoreThanOneCategorySelected(long[] categoryIds)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	public boolean isAssociatedToAsset(long classNameId);
+
+	public boolean isMissingRequiredCategory(long classNameId,
+		long[] categoryIds)
+		throws com.liferay.portal.kernel.exception.SystemException;
 
 	public boolean isMultiValued();
 

portal-service/src/com/liferay/portlet/asset/model/AssetVocabularyWrapper.java
@@ -804,8 +804,37 @@ public class AssetVocabularyWrapper implements AssetVocabulary,
 	}
 
 	@Override
+	public long[] getRequiredClassNameIds() {
+		return _assetVocabulary.getRequiredClassNameIds();
+	}
+
+	@Override
+	public long[] getSelectedClassNameIds() {
+		return _assetVocabulary.getSelectedClassNameIds();
+	}
+
+	@Override
 	public com.liferay.portal.kernel.util.UnicodeProperties getSettingsProperties() {
 		return _assetVocabulary.getSettingsProperties();
+	}
+
+	@Override
+	public boolean hasMoreThanOneCategorySelected(long[] categoryIds)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return _assetVocabulary.hasMoreThanOneCategorySelected(categoryIds);
+	}
+
+	@Override
+	public boolean isAssociatedToAsset(long classNameId) {
+		return _assetVocabulary.isAssociatedToAsset(classNameId);
+	}
+
+	@Override
+	public boolean isMissingRequiredCategory(long classNameId,
+		long[] categoryIds)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return _assetVocabulary.isMissingRequiredCategory(classNameId,
+			categoryIds);
 	}
 
 	@Override

liferay/liferay-central-test-2

portal-service/src/com/liferay/portlet/asset/model/AssetVocabulary.java
@@ -37,7 +37,20 @@ public interface AssetVocabulary extends AssetVocabularyModel, PersistedModel {
 	public java.util.List<com.liferay.portlet.asset.model.AssetCategory> getCategories()
 		throws com.liferay.portal.kernel.exception.SystemException;
 
+	public long[] getRequiredClassNameIds();
+
+	public long[] getSelectedClassNameIds();
+
 	public com.liferay.portal.kernel.util.UnicodeProperties getSettingsProperties();
+
+	public boolean hasMoreThanOneCategorySelected(long[] categoryIds)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	public boolean isAssociatedToAssetRendererFactory(long classNameId);
+
+	public boolean isMissingRequiredCategory(long classNameId,
+		long[] categoryIds)
+		throws com.liferay.portal.kernel.exception.SystemException;
 
 	public boolean isMultiValued();
 

portal-service/src/com/liferay/portlet/asset/model/AssetVocabularyWrapper.java
@@ -804,8 +804,37 @@ public class AssetVocabularyWrapper implements AssetVocabulary,
 	}
 
 	@Override
+	public long[] getRequiredClassNameIds() {
+		return _assetVocabulary.getRequiredClassNameIds();
+	}
+
+	@Override
+	public long[] getSelectedClassNameIds() {
+		return _assetVocabulary.getSelectedClassNameIds();
+	}
+
+	@Override
 	public com.liferay.portal.kernel.util.UnicodeProperties getSettingsProperties() {
 		return _assetVocabulary.getSettingsProperties();
+	}
+
+	@Override
+	public boolean hasMoreThanOneCategorySelected(long[] categoryIds)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return _assetVocabulary.hasMoreThanOneCategorySelected(categoryIds);
+	}
+
+	@Override
+	public boolean isAssociatedToAssetRendererFactory(long classNameId) {
+		return _assetVocabulary.isAssociatedToAssetRendererFactory(classNameId);
+	}
+
+	@Override
+	public boolean isMissingRequiredCategory(long classNameId,
+		long[] categoryIds)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return _assetVocabulary.isMissingRequiredCategory(classNameId,
+			categoryIds);
 	}
 
 	@Override