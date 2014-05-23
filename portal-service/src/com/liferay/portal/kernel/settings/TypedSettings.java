b0f6a9ffad6f7cb43fd54828f80016ea12662ff8
Brian Chan <brian.chan@liferay.com>
2014-05-23 15:59:06 -0700

    LPS-46267 Auto SF

--- portal-service/src/com/liferay/portal/kernel/settings/TypedSettings.java
+++ portal-service/src/com/liferay/portal/kernel/settings/TypedSettings.java
@@ -36,8 +36,7 @@ public class TypedSettings {
 	}
 
 	public TypedSettings(
-		Settings settings, Locale defaultLocale,
-		Locale... availableLocales) {
+		Settings settings, Locale defaultLocale, Locale... availableLocales) {
 
 		_settings = settings;
 		_defaultLocale = defaultLocale;
@@ -52,10 +51,6 @@ public class TypedSettings {
 		String value = getValue(key, null);
 
 		return GetterUtil.getBoolean(value, defaultValue);
-	}
-
-	public Settings getWrappedSettings() {
-		return _settings;
 	}
 
 	public double getDoubleValue(String key) {
@@ -136,6 +131,10 @@ public class TypedSettings {
 		return _settings.getValues(key, defaultValue);
 	}
 
+	public Settings getWrappedSettings() {
+		return _settings;
+	}
+
 	public void reset(String key) {
 		_settings.reset(key);
 	}
@@ -161,7 +160,7 @@ public class TypedSettings {
 	}
 
 	private Locale[] _availableLocales;
-	private Settings _settings;
 	private Locale _defaultLocale;
+	private Settings _settings;
 
 }
