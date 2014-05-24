cfccf7225473ac4abfcb2757a8629edf6c4cbf80
Raymond Auge <raymond.auge@liferay.com>
2014-05-24 19:54:24 -0400

    Revert "LPS-47048 SF"
    This reverts commit 2ac470b587a7af55a196c0b6b7fe979dcd77e290.

--- modules/portal/portal-portlet-tracker/src/com/liferay/portal/portlet/tracker/internal/PortletTracker.java
+++ modules/portal/portal-portlet-tracker/src/com/liferay/portal/portlet/tracker/internal/PortletTracker.java
@@ -18,6 +18,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.plugin.PluginPackage;
 import com.liferay.portal.kernel.portlet.LiferayWindowState;
 import com.liferay.portal.kernel.portlet.PortletBag;
 import com.liferay.portal.kernel.portlet.PortletBagPool;
@@ -29,6 +30,7 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.Company;
 import com.liferay.portal.model.CompanyConstants;
+import com.liferay.portal.model.PortletApp;
 import com.liferay.portal.model.PortletCategory;
 import com.liferay.portal.model.PortletConstants;
 import com.liferay.portal.model.PortletInfo;
@@ -98,7 +100,7 @@ public class PortletTracker
 		}
 
 		if (_log.isInfoEnabled()) {
-			_log.info("Adding " + serviceReference);
+			_log.info("Adding portlet " + portletId);
 		}
 
 		try {
@@ -424,8 +426,8 @@ public class PortletTracker
 				try {
 					defaultPreferences = StringUtil.read(url.openStream());
 				}
-				catch (IOException ioe) {
-					_log.error(ioe, ioe);
+				catch (IOException e) {
+					_log.error(e, e);
 				}
 			}
 		}
