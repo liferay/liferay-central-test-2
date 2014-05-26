bd6dae6b6eb53e6135e716f0d17331060b6e04c1
Brian Chan <brian.chan@liferay.com>
2014-05-26 12:20:03 -0700

    LPS-47161 Use longer package, and let's use the "internal" package convention to signify that something is private

--- modules/portal/portal-log-bridge/src/com/liferay/portal/log/bridge/internal/LogBridge.java
+++ modules/portal/portal-log-bridge/src/com/liferay/portal/log/bridge/internal/LogBridge.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.logbridge;
+package com.liferay.portal.log.bridge.internal;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
