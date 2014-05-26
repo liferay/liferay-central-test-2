1331e8f87d4ac4794e311d328798c12c5913d185
Marcellus Tavares <marcellus.tavares@liferay.com>
2014-05-26 15:10:14 -0300

    LPS-47173 Tests

---
+++ portal-service/test/unit/com/liferay/portal/kernel/util/MathUtilTest.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Marcellus Tavares
+ */
+public class MathUtilTest {
+
+	@Test
+	public void testSumWithDoubleValues() {
+		double expected = 1.1 + 2.2 + 3.3 + 5.5 + 8.8;
+
+		Assert.assertEquals(
+			expected, MathUtil.sum(1.1, 2.2, 3.3, 5.5, 8.8), 0.01);
+	}
+
+	@Test
+	public void testSumWithLongValues() {
+		long expected = 1 + 2 + 3 + 5 + 8;
+
+		Assert.assertEquals(expected, MathUtil.sum(1L, 2L, 3L, 5L, 8L));
+	}
+
+}
