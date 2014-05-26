dc2ec9eb7566d31367875d04af46c699c7ce2c08
Brian Chan <brian.chan@liferay.com>
2014-05-26 12:19:01 -0700

    LPS-47161 Add @Override

--- modules/portal/portal-log-bridge/src/com/liferay/logbridge/LogBridge.java
+++ modules/portal/portal-log-bridge/src/com/liferay/logbridge/LogBridge.java
@@ -37,6 +37,7 @@ public class LogBridge
 	implements BundleActivator, LogListener,
 			   ServiceTrackerCustomizer<LogReaderService, LogReaderService> {
 
+	@Override
 	public LogReaderService addingService(
 		ServiceReference<LogReaderService> serviceReference) {
 
@@ -48,6 +49,7 @@ public class LogBridge
 		return logReaderService;
 	}
 
+	@Override
 	public void logged(LogEntry logEntry) {
 		int level = logEntry.getLevel();
 
@@ -80,11 +82,13 @@ public class LogBridge
 		}
 	}
 
+	@Override
 	public void modifiedService(
 		ServiceReference<LogReaderService> serviceReference,
 		LogReaderService logReaderService) {
 	}
 
+	@Override
 	public void removedService(
 		ServiceReference<LogReaderService> serviceReference,
 		LogReaderService logReaderService) {
@@ -92,6 +96,7 @@ public class LogBridge
 		logReaderService.removeLogListener(this);
 	}
 
+	@Override
 	public void start(BundleContext bundleContext) throws Exception {
 		_bundleContext = bundleContext;
 
@@ -102,6 +107,7 @@ public class LogBridge
 		_serviceTracker.open();
 	}
 
+	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
 		_bundleContext = null;
 
