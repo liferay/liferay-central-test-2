343c7499bc8153d23730af4a7c6ba7a58871fa4b
Iván Zaera Avellón <ivan.zaera@liferay.com>
2014-05-28 12:25:54 +0200

    LPS-46270 Adapt to changes in LPS-46267

--- portal-impl/src/com/liferay/portlet/blogs/BlogsPortletInstanceSettings.java
+++ portal-impl/src/com/liferay/portlet/blogs/BlogsPortletInstanceSettings.java
@@ -14,48 +14,84 @@
 
 package com.liferay.portlet.blogs;
 
-import com.liferay.portal.kernel.settings.BaseServiceSettings;
 import com.liferay.portal.kernel.settings.FallbackKeys;
 import com.liferay.portal.kernel.settings.Settings;
+import com.liferay.portal.kernel.settings.TypedSettings;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.util.PortalUtil;
 
 /**
  * @author Iván Zaera
  */
-public class BlogsPortletInstanceSettings extends BaseServiceSettings {
+public class BlogsPortletInstanceSettings {
+
+	public static final String[] MULTI_VALUED_KEYS = {};
+
+	public static FallbackKeys getFallbackKeys() {
+		FallbackKeys fallbackKeys = new FallbackKeys();
+
+		fallbackKeys.add(
+			"enableComments", PropsKeys.BLOGS_ENTRY_COMMENTS_ENABLED);
+		fallbackKeys.add(
+			"enableCommentRatings", PropsKeys.BLOGS_COMMENT_RATINGS_ENABLED);
+		fallbackKeys.add("enableFlags", PropsKeys.BLOGS_FLAGS_ENABLED);
+		fallbackKeys.add("enableRatings", PropsKeys.BLOGS_RATINGS_ENABLED);
+		fallbackKeys.add(
+			"enableRelatedAssets", PropsKeys.BLOGS_RELATED_ASSETS_ENABLED);
+		fallbackKeys.add("enableRss", PropsKeys.BLOGS_RSS_ENABLED);
+		fallbackKeys.add(
+			"enableSocialBookmarks", PropsKeys.BLOGS_SOCIAL_BOOKMARKS_ENABLED);
+		fallbackKeys.add("displayStyle", PropsKeys.BLOGS_DISPLAY_STYLE);
+		fallbackKeys.add(
+			"pageDelta", PropsKeys.SEARCH_CONTAINER_PAGE_DEFAULT_DELTA);
+		fallbackKeys.add(
+			"rssDelta", PropsKeys.SEARCH_CONTAINER_PAGE_DEFAULT_DELTA);
+		fallbackKeys.add(
+			"rssDisplayStyle", PropsKeys.RSS_FEED_DISPLAY_STYLE_DEFAULT);
+		fallbackKeys.add("rssFeedType", PropsKeys.RSS_FEED_TYPE_DEFAULT);
+		fallbackKeys.add(
+			"socialBookmarksDisplayPosition",
+			PropsKeys.BLOGS_SOCIAL_BOOKMARKS_DISPLAY_POSITION);
+		fallbackKeys.add(
+			"socialBookmarksDisplayStyle",
+			PropsKeys.BLOGS_SOCIAL_BOOKMARKS_DISPLAY_STYLE);
+		fallbackKeys.add(
+			"socialBookmarksTypes", PropsKeys.SOCIAL_BOOKMARK_TYPES);
+
+		return fallbackKeys;
+	}
 
 	public BlogsPortletInstanceSettings(Settings settings) {
-		super(settings, _fallbackKeys);
+		_typedSettings = new TypedSettings(settings);
 	}
 
 	public String getDisplayStyle() {
-		return typedSettings.getValue("displayStyle");
+		return _typedSettings.getValue("displayStyle");
 	}
 
 	public long getDisplayStyleGroupId(long defaultDisplayStyleGroupId) {
-		return typedSettings.getLongValue(
+		return _typedSettings.getLongValue(
 			"displayStyleGroupId", defaultDisplayStyleGroupId);
 	}
 
 	public boolean getEnableCommentRatings() {
-		return typedSettings.getBooleanValue("enableCommentRatings");
+		return _typedSettings.getBooleanValue("enableCommentRatings");
 	}
 
 	public boolean getEnableComments() {
-		return typedSettings.getBooleanValue("enableComments");
+		return _typedSettings.getBooleanValue("enableComments");
 	}
 
 	public boolean getEnableFlags() {
-		return typedSettings.getBooleanValue("enableFlags");
+		return _typedSettings.getBooleanValue("enableFlags");
 	}
 
 	public boolean getEnableRatings() {
-		return typedSettings.getBooleanValue("enableRatings");
+		return _typedSettings.getBooleanValue("enableRatings");
 	}
 
 	public boolean getEnableRelatedAssets() {
-		return typedSettings.getBooleanValue("enableRelatedAssets");
+		return _typedSettings.getBooleanValue("enableRelatedAssets");
 	}
 
 	public boolean getEnableRSS() {
@@ -63,71 +99,41 @@ public class BlogsPortletInstanceSettings extends BaseServiceSettings {
 			return false;
 		}
 
-		return typedSettings.getBooleanValue("enableRss");
+		return _typedSettings.getBooleanValue("enableRss");
 	}
 
 	public boolean getEnableSocialBookmarks() {
-		return typedSettings.getBooleanValue("enableSocialBookmarks");
+		return _typedSettings.getBooleanValue("enableSocialBookmarks");
 	}
 
 	public int getPageDelta() {
-		return typedSettings.getIntegerValue("pageDelta");
+		return _typedSettings.getIntegerValue("pageDelta");
 	}
 
 	public int getRssDelta() {
-		return typedSettings.getIntegerValue("rssDelta");
+		return _typedSettings.getIntegerValue("rssDelta");
 	}
 
 	public String getRssDisplayStyle() {
-		return typedSettings.getValue("rssDisplayStyle");
+		return _typedSettings.getValue("rssDisplayStyle");
 	}
 
 	public String getRssFeedType() {
-		return typedSettings.getValue("rssFeedType");
+		return _typedSettings.getValue("rssFeedType");
 	}
 
 	public String getSocialBookmarksDisplayPosition() {
-		return typedSettings.getValue("socialBookmarksDisplayPosition");
+		return _typedSettings.getValue("socialBookmarksDisplayPosition");
 	}
 
 	public String getSocialBookmarksDisplayStyle() {
-		return typedSettings.getValue("socialBookmarksDisplayStyle");
+		return _typedSettings.getValue("socialBookmarksDisplayStyle");
 	}
 
 	public String getSocialBookmarksTypes() {
-		return typedSettings.getValue("socialBookmarksTypes");
+		return _typedSettings.getValue("socialBookmarksTypes");
 	}
 
-	private static FallbackKeys _fallbackKeys = new FallbackKeys();
-
-	static {
-		_fallbackKeys.add(
-			"enableComments", PropsKeys.BLOGS_ENTRY_COMMENTS_ENABLED);
-		_fallbackKeys.add(
-			"enableCommentRatings", PropsKeys.BLOGS_COMMENT_RATINGS_ENABLED);
-		_fallbackKeys.add("enableFlags", PropsKeys.BLOGS_FLAGS_ENABLED);
-		_fallbackKeys.add("enableRatings", PropsKeys.BLOGS_RATINGS_ENABLED);
-		_fallbackKeys.add(
-			"enableRelatedAssets", PropsKeys.BLOGS_RELATED_ASSETS_ENABLED);
-		_fallbackKeys.add("enableRss", PropsKeys.BLOGS_RSS_ENABLED);
-		_fallbackKeys.add(
-			"enableSocialBookmarks", PropsKeys.BLOGS_SOCIAL_BOOKMARKS_ENABLED);
-		_fallbackKeys.add("displayStyle", PropsKeys.BLOGS_DISPLAY_STYLE);
-		_fallbackKeys.add(
-			"pageDelta", PropsKeys.SEARCH_CONTAINER_PAGE_DEFAULT_DELTA);
-		_fallbackKeys.add(
-			"rssDelta", PropsKeys.SEARCH_CONTAINER_PAGE_DEFAULT_DELTA);
-		_fallbackKeys.add(
-			"rssDisplayStyle", PropsKeys.RSS_FEED_DISPLAY_STYLE_DEFAULT);
-		_fallbackKeys.add("rssFeedType", PropsKeys.RSS_FEED_TYPE_DEFAULT);
-		_fallbackKeys.add(
-			"socialBookmarksDisplayPosition",
-			PropsKeys.BLOGS_SOCIAL_BOOKMARKS_DISPLAY_POSITION);
-		_fallbackKeys.add(
-			"socialBookmarksDisplayStyle",
-			PropsKeys.BLOGS_SOCIAL_BOOKMARKS_DISPLAY_STYLE);
-		_fallbackKeys.add(
-			"socialBookmarksTypes", PropsKeys.SOCIAL_BOOKMARK_TYPES);
-	}
+	private TypedSettings _typedSettings;
 
 }
