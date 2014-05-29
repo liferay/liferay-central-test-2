0103f5ef4054dceeeae9f58a92041e9946636e91
Daniel Kocsis <daniel.kocsis@liferay.com>
2014-05-29 10:06:39 +0200

    LPS-45350 Add new status message translators to satisfy the different needs of publish/export processes

---
+++ portal-impl/src/com/liferay/portal/lar/backgroundtask/PortletStagingBackgroundTaskStatusMessageTranslator.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.lar.backgroundtask;
+
+import com.liferay.portal.kernel.backgroundtask.BackgroundTaskStatus;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LongWrapper;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.HashMap;
+
+/**
+ * @author Daniel Kocsis
+ */
+public class PortletStagingBackgroundTaskStatusMessageTranslator
+	extends DefaultExportImportBackgroundTaskStatusMessageTranslator {
+
+	@Override
+	protected synchronized void translatePortletMessage(
+		BackgroundTaskStatus backgroundTaskStatus, Message message) {
+
+		String phase = GetterUtil.getString(
+			backgroundTaskStatus.getAttribute("phase"));
+
+		if (Validator.isNull(phase)) {
+			clearBackgroundTaskStatus(backgroundTaskStatus);
+
+			phase = Constants.EXPORT;
+		}
+		else {
+			phase = Constants.IMPORT;
+		}
+
+		backgroundTaskStatus.setAttribute("phase", phase);
+
+		if (phase.equals(Constants.EXPORT)) {
+			long portletModelAdditionCountersTotal = GetterUtil.getLong(
+				message.get("portletModelAdditionCountersTotal"));
+
+			backgroundTaskStatus.setAttribute(
+				"allModelAdditionCountersTotal",
+				portletModelAdditionCountersTotal);
+		}
+		else {
+			backgroundTaskStatus.setAttribute(
+				"allPortletModelAdditionCounters",
+				new HashMap<String, LongWrapper>());
+			backgroundTaskStatus.setAttribute(
+				"currentPortletModelAdditionCounters",
+				new HashMap<String, LongWrapper>());
+
+			long allModelAdditionCountersTotal = GetterUtil.getLong(
+				backgroundTaskStatus.getAttribute(
+					"allModelAdditionCountersTotal"));
+			long currentModelAdditionCountersTotal = GetterUtil.getLong(
+				backgroundTaskStatus.getAttribute(
+					"currentModelAdditionCountersTotal"));
+
+			backgroundTaskStatus.setAttribute(
+				"allModelAdditionCountersTotal",
+				allModelAdditionCountersTotal +
+					currentModelAdditionCountersTotal);
+		}
+
+		super.translatePortletMessage(backgroundTaskStatus, message);
+	}
+
+}

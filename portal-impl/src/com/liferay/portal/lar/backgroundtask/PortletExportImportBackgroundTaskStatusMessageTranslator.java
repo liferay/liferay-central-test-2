brianchandotcom/liferay-portal

LPS-45350 Add new status message translators to satisfy the different needs of publish/export processes

portal-impl/src/com/liferay/portal/lar/backgroundtask/PortletExportImportBackgroundTaskStatusMessageTranslator.java
@@ -1,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.lar.backgroundtask;
+
+import com.liferay.portal.kernel.backgroundtask.BackgroundTaskStatus;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+/**
+ * @author Daniel Kocsis
+ */
+public class PortletExportImportBackgroundTaskStatusMessageTranslator
+	extends DefaultExportImportBackgroundTaskStatusMessageTranslator {
+
+	@Override
+	protected synchronized void translatePortletMessage(
+		BackgroundTaskStatus backgroundTaskStatus, Message message) {
+
+		clearBackgroundTaskStatus(backgroundTaskStatus);
+
+		long modelAdditionByPortlet = GetterUtil.getLong(
+			message.get("modelAdditionsByPortlet"));
+
+		backgroundTaskStatus.setAttribute(
+			"allModelAdditionCountersTotal", modelAdditionByPortlet);
+
+		super.translatePortletMessage(backgroundTaskStatus, message);
+	}
+
+}

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portal/lar/backgroundtask/PortletExportImportBackgroundTaskStatusMessageTranslator.java
@@ -1,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.lar.backgroundtask;
+
+import com.liferay.portal.kernel.backgroundtask.BackgroundTaskStatus;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+/**
+ * @author Daniel Kocsis
+ */
+public class PortletExportImportBackgroundTaskStatusMessageTranslator
+	extends DefaultExportImportBackgroundTaskStatusMessageTranslator {
+
+	@Override
+	protected synchronized void translatePortletMessage(
+		BackgroundTaskStatus backgroundTaskStatus, Message message) {
+
+		clearBackgroundTaskStatus(backgroundTaskStatus);
+
+		long portletModelAdditionCountersTotal = GetterUtil.getLong(
+			message.get("portletModelAdditionCountersTotal"));
+
+		backgroundTaskStatus.setAttribute(
+			"allModelAdditionCountersTotal", portletModelAdditionCountersTotal);
+
+		super.translatePortletMessage(backgroundTaskStatus, message);
+	}
+
+}