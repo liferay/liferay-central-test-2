cb04ef73d1df82e113e28d68568ea048ac74888b
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-05-31 01:03:31 +0200

    LPS-47315 Creates a new search container column for users

---
+++ portal-service/src/com/liferay/portal/kernel/dao/search/UserSearchEntry.java
@@ -0,0 +1,116 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.dao.search;
+
+import com.liferay.portal.kernel.bean.BeanPropertiesUtil;
+import com.liferay.portal.kernel.servlet.DirectRequestDispatcherFactoryUtil;
+import com.liferay.portal.kernel.servlet.PipingServletResponse;
+
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class UserSearchEntry extends TextSearchEntry {
+
+	@Override
+	public Object clone() {
+		UserSearchEntry jspSearchEntry = new UserSearchEntry();
+
+		BeanPropertiesUtil.copyProperties(this, jspSearchEntry);
+
+		return jspSearchEntry;
+	}
+
+	public Date getDate() {
+		return _date;
+	}
+
+	public HttpServletRequest getRequest() {
+		return _request;
+	}
+
+	public HttpServletResponse getResponse() {
+		return _response;
+	}
+
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	public long getUserId() {
+		return _userId;
+	}
+
+	@Override
+	public void print(PageContext pageContext) throws Exception {
+		if (_request == null) {
+			_request = (HttpServletRequest)pageContext.getRequest();
+		}
+
+		_request.setAttribute(
+			"liferay-ui:search-container-column-user:date", _date);
+		_request.setAttribute(
+			"liferay-ui:search-container-column-user:userId", _userId);
+
+		if (_servletContext != null) {
+			RequestDispatcher requestDispatcher =
+				DirectRequestDispatcherFactoryUtil.getRequestDispatcher(
+					_servletContext, _PAGE);
+
+			requestDispatcher.include(
+				_request, new PipingServletResponse(pageContext));
+		}
+		else {
+			pageContext.include(_PAGE);
+		}
+	}
+
+	public void setDate(Date date) {
+		_date = date;
+	}
+
+	public void setRequest(HttpServletRequest request) {
+		_request = request;
+	}
+
+	public void setResponse(HttpServletResponse response) {
+		_response = response;
+	}
+
+	public void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
+	}
+
+	public void setUserId(long userId) {
+		_userId = userId;
+	}
+
+	private static final String _PAGE =
+		"/html/taglib/ui/search_container/user.jsp";
+
+	private Date _date;
+	private HttpServletRequest _request;
+	private HttpServletResponse _response;
+	private ServletContext _servletContext;
+	private long _userId;
+
+}
---
+++ portal-web/docroot/html/taglib/ui/search_container/user.jsp
@@ -0,0 +1,46 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/html/taglib/ui/search_container/init.jsp" %>
+
+<%
+long userId = GetterUtil.getLong(request.getAttribute("liferay-ui:search-container-column-user:userId"));
+Date date = GetterUtil.getDate(request.getAttribute("liferay-ui:search-container-column-user:date"), DateFormatFactoryUtil.getDate(locale), null);
+
+User user2 = UserLocalServiceUtil.fetchUser(userId);
+%>
+
+<c:if test="<%= user2 != null %>">
+	<div class="user-info">
+		<div class="user-avatar">
+			<img alt="<%= HtmlUtil.escapeAttribute(user2.getFullName()) %>" class="avatar img-circle" src="<%= HtmlUtil.escape(user2.getPortraitURL(themeDisplay)) %>" />
+		</div>
+
+		<div class="user-details">
+			<div class="row <%= (date == null) ? "line" : StringPool.BLANK %>">
+				<span class="span12 user-name"><%= HtmlUtil.escapeAttribute(user2.getFullName()) %></span>
+			</div>
+
+			<c:if test="<%= date != null %>">
+				<div class="row">
+					<span class="span12 date-info">
+						<liferay-ui:message arguments="<%= LanguageUtil.getTimeDescription(pageContext, System.currentTimeMillis() - date.getTime(), true) %>" key="x-ago" translateArguments="<%= false %>" />
+					</span>
+				</div>
+			</c:if>
+		</div>
+	</div>
+</c:if>
