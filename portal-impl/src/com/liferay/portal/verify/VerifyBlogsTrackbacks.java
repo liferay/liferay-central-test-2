marcellustavares/liferay-portal

LPS-46436 Linkbacks: Drop every last reference to MBMessage
https://issues.liferay.com/browse/LPS-46436

portal-impl/src/com/liferay/portal/verify/VerifyBlogsTrackbacks.java
@@ -14,9 +14,14 @@
 
 package com.liferay.portal.verify;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portal.util.Portal;
 import com.liferay.portlet.blogs.linkback.LinkbackConsumer;
 import com.liferay.portlet.blogs.model.BlogsEntry;
 import com.liferay.portlet.blogs.service.BlogsEntryLocalServiceUtil;
@@ -61,11 +66,44 @@ public class VerifyBlogsTrackbacks extends VerifyProcess {
 						threadId, WorkflowConstants.STATUS_APPROVED);
 
 				for (MBMessage message : messages) {
-					_linkbackConsumer.verifyPost(entry, message);
+					_verifyPost(entry, message);
 				}
 			}
 			catch (Exception e) {
 				_log.error(e, e);
+			}
+		}
+	}
+
+	private void _verifyPost(BlogsEntry entry, MBMessage message)
+		throws PortalException, SystemException {
+
+		long messageId = message.getMessageId();
+		String entryURL =
+			Portal.FRIENDLY_URL_SEPARATOR + "blogs/" + entry.getUrlTitle();
+		String body = message.getBody();
+		String url = null;
+
+		int start = body.indexOf("[url=");
+
+		if (start > -1) {
+			start += "[url=".length();
+
+			int end = body.indexOf("]", start);
+
+			if (end > -1) {
+				url = body.substring(start, end);
+			}
+		}
+
+		if (Validator.isNotNull(url)) {
+			long companyId = message.getCompanyId();
+			long userId = message.getUserId();
+			long defaultUserId = UserLocalServiceUtil.getDefaultUserId(
+				companyId);
+
+			if (userId == defaultUserId) {
+				_linkbackConsumer.verifyTrackback(messageId, url, entryURL);
 			}
 		}
 	}

portal-impl/src/com/liferay/portlet/blogs/linkback/LinkbackConsumer.java
@@ -14,11 +14,6 @@
 
 package com.liferay.portlet.blogs.linkback;
 
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portlet.blogs.model.BlogsEntry;
-import com.liferay.portlet.messageboards.model.MBMessage;
-
 /**
  * @author André de Oliveira
  */
@@ -28,7 +23,6 @@ public interface LinkbackConsumer {
 
 	public void verifyNewTrackbacks();
 
-	public void verifyPost(BlogsEntry entry, MBMessage message)
-		throws PortalException, SystemException;
+	public void verifyTrackback(long messageId, String url, String entryURL);
 
 }

portal-impl/src/com/liferay/portlet/blogs/linkback/LinkbackConsumerImpl.java
@@ -16,17 +16,10 @@ package com.liferay.portlet.blogs.linkback;
 
 import com.liferay.portal.comments.CommentsImpl;
 import com.liferay.portal.kernel.comments.Comments;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.Tuple;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.service.UserLocalServiceUtil;
-import com.liferay.portal.util.Portal;
-import com.liferay.portlet.blogs.model.BlogsEntry;
-import com.liferay.portlet.messageboards.model.MBMessage;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -64,45 +57,12 @@ public class LinkbackConsumerImpl implements LinkbackConsumer {
 			String url = (String)tuple.getObject(1);
 			String entryUrl = (String)tuple.getObject(2);
 
-			_verifyTrackback(messageId, url, entryUrl);
+			verifyTrackback(messageId, url, entryUrl);
 		}
 	}
 
 	@Override
-	public void verifyPost(BlogsEntry entry, MBMessage message)
-		throws PortalException, SystemException {
-
-		long messageId = message.getMessageId();
-		String entryURL =
-			Portal.FRIENDLY_URL_SEPARATOR + "blogs/" + entry.getUrlTitle();
-		String body = message.getBody();
-		String url = null;
-
-		int start = body.indexOf("[url=");
-
-		if (start > -1) {
-			start += "[url=".length();
-
-			int end = body.indexOf("]", start);
-
-			if (end > -1) {
-				url = body.substring(start, end);
-			}
-		}
-
-		if (Validator.isNotNull(url)) {
-			long companyId = message.getCompanyId();
-			long userId = message.getUserId();
-			long defaultUserId = UserLocalServiceUtil.getDefaultUserId(
-				companyId);
-
-			if (userId == defaultUserId) {
-				_verifyTrackback(messageId, url, entryURL);
-			}
-		}
-	}
-
-	private void _verifyTrackback(long messageId, String url, String entryURL) {
+	public void verifyTrackback(long messageId, String url, String entryURL) {
 		try {
 			String result = HttpUtil.URLtoString(url);
 

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portal/verify/VerifyBlogsTrackbacks.java
@@ -14,9 +14,14 @@
 
 package com.liferay.portal.verify;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portal.util.Portal;
 import com.liferay.portlet.blogs.linkback.LinkbackConsumer;
 import com.liferay.portlet.blogs.linkback.LinkbackConsumerUtil;
 import com.liferay.portlet.blogs.model.BlogsEntry;
@@ -61,11 +66,44 @@ public class VerifyBlogsTrackbacks extends VerifyProcess {
 						threadId, WorkflowConstants.STATUS_APPROVED);
 
 				for (MBMessage message : messages) {
-					_linkbackConsumer.verifyPost(entry, message);
+					_verifyPost(entry, message);
 				}
 			}
 			catch (Exception e) {
 				_log.error(e, e);
+			}
+		}
+	}
+
+	private void _verifyPost(BlogsEntry entry, MBMessage message)
+		throws PortalException, SystemException {
+
+		String entryURL =
+			Portal.FRIENDLY_URL_SEPARATOR + "blogs/" + entry.getUrlTitle();
+		String body = message.getBody();
+		String url = null;
+
+		int start = body.indexOf("[url=");
+
+		if (start > -1) {
+			start += "[url=".length();
+
+			int end = body.indexOf("]", start);
+
+			if (end > -1) {
+				url = body.substring(start, end);
+			}
+		}
+
+		if (Validator.isNotNull(url)) {
+			long companyId = message.getCompanyId();
+			long userId = message.getUserId();
+			long defaultUserId = UserLocalServiceUtil.getDefaultUserId(
+				companyId);
+
+			if (userId == defaultUserId) {
+				long commentId = message.getMessageId();
+				_linkbackConsumer.verifyTrackback(commentId, url, entryURL);
 			}
 		}
 	}

portal-impl/src/com/liferay/portlet/blogs/linkback/LinkbackConsumer.java
@@ -14,11 +14,6 @@
 
 package com.liferay.portlet.blogs.linkback;
 
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portlet.blogs.model.BlogsEntry;
-import com.liferay.portlet.messageboards.model.MBMessage;
-
 /**
  * @author André de Oliveira
  */
@@ -28,7 +23,6 @@ public interface LinkbackConsumer {
 
 	public void verifyNewTrackbacks();
 
-	public void verifyPost(BlogsEntry entry, MBMessage message)
-		throws PortalException, SystemException;
+	public void verifyTrackback(long commentId, String url, String entryURL);
 
 }

portal-impl/src/com/liferay/portlet/blogs/linkback/LinkbackConsumerImpl.java
@@ -16,17 +16,10 @@ package com.liferay.portlet.blogs.linkback;
 
 import com.liferay.portal.comments.CommentsImpl;
 import com.liferay.portal.kernel.comments.Comments;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.Tuple;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.service.UserLocalServiceUtil;
-import com.liferay.portal.util.Portal;
-import com.liferay.portlet.blogs.model.BlogsEntry;
-import com.liferay.portlet.messageboards.model.MBMessage;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -64,45 +57,12 @@ public class LinkbackConsumerImpl implements LinkbackConsumer {
 			String url = (String)tuple.getObject(1);
 			String entryUrl = (String)tuple.getObject(2);
 
-			_verifyTrackback(commentId, url, entryUrl);
+			verifyTrackback(commentId, url, entryUrl);
 		}
 	}
 
 	@Override
-	public void verifyPost(BlogsEntry entry, MBMessage message)
-		throws PortalException, SystemException {
-
-		long messageId = message.getMessageId();
-		String entryURL =
-			Portal.FRIENDLY_URL_SEPARATOR + "blogs/" + entry.getUrlTitle();
-		String body = message.getBody();
-		String url = null;
-
-		int start = body.indexOf("[url=");
-
-		if (start > -1) {
-			start += "[url=".length();
-
-			int end = body.indexOf("]", start);
-
-			if (end > -1) {
-				url = body.substring(start, end);
-			}
-		}
-
-		if (Validator.isNotNull(url)) {
-			long companyId = message.getCompanyId();
-			long userId = message.getUserId();
-			long defaultUserId = UserLocalServiceUtil.getDefaultUserId(
-				companyId);
-
-			if (userId == defaultUserId) {
-				_verifyTrackback(messageId, url, entryURL);
-			}
-		}
-	}
-
-	private void _verifyTrackback(long commentId, String url, String entryURL) {
+	public void verifyTrackback(long commentId, String url, String entryURL) {
 		try {
 			String result = HttpUtil.URLtoString(url);
 