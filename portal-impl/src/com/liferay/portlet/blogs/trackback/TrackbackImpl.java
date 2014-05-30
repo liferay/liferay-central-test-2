marcellustavares/liferay-portal

LPS-46436 Linkbacks: stop using a static class for singleton purposes
https://issues.liferay.com/browse/LPS-46436

portal-impl/src/com/liferay/portlet/blogs/trackback/TrackbackImpl.java
@@ -26,6 +26,7 @@ import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.Portal;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portlet.blogs.model.BlogsEntry;
+import com.liferay.portlet.blogs.util.LinkbackConsumerStaticSingleton;
 import com.liferay.portlet.blogs.util.LinkbackConsumerUtil;
 
 /**
@@ -59,7 +60,10 @@ public class TrackbackImpl implements Trackback {
 
 		String entryURL = buildEntryURL(entry, themeDisplay);
 
-		LinkbackConsumerUtil.addNewTrackback(messageId, url, entryURL);
+		LinkbackConsumerUtil linkbackConsumer =
+			LinkbackConsumerStaticSingleton.getInstance();
+
+		linkbackConsumer.addNewTrackback(messageId, url, entryURL);
 	}
 
 	protected TrackbackImpl(Comments comments) {

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portlet/blogs/trackback/TrackbackImpl.java
@@ -26,6 +26,7 @@ import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.Portal;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portlet.blogs.model.BlogsEntry;
+import com.liferay.portlet.blogs.util.LinkbackConsumer;
 import com.liferay.portlet.blogs.util.LinkbackConsumerUtil;
 
 /**
@@ -59,7 +60,10 @@ public class TrackbackImpl implements Trackback {
 
 		String entryURL = buildEntryURL(entry, themeDisplay);
 
-		LinkbackConsumerUtil.addNewTrackback(commentId, url, entryURL);
+		LinkbackConsumer linkbackConsumer =
+			LinkbackConsumerUtil.getLinkbackConsumer();
+
+		linkbackConsumer.addNewTrackback(commentId, url, entryURL);
 	}
 
 	protected TrackbackImpl(Comments comments) {