47c24c8d2aaf3d4a2fef853edb569185189bbfcf
Miguel Angelo <miguel.angelo@liferay.com>
2014-05-30 16:40:41 -0300

    LPS-45702 Test suite for running all tests

---
+++ portal-impl/test/unit/com/liferay/portlet/dynamicdatamapping/DDMTestSuite.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.dynamicdatamapping;
+
+import com.liferay.portlet.dynamicdatamapping.model.impl.DDMStructureImplTest;
+import com.liferay.portlet.dynamicdatamapping.util.DDMFormXSDSerializerTest;
+import com.liferay.portlet.dynamicdatamapping.util.DDMXMLImplTest;
+import com.liferay.portlet.dynamicdatamapping.util.DDMXSDImplTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author Miguel Angelo Caldas Gallindo
+ */
+@RunWith(Suite.class)
+@SuiteClasses( {
+	DDMFormXSDSerializerTest.class, DDMStructureImplTest.class,
+	DDMXMLImplTest.class, DDMXSDImplTest.class
+})
+public class DDMTestSuite {
+
+}
