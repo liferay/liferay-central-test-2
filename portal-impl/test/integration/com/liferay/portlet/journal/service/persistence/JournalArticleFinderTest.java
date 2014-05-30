8dcafb7c0c8a6820ae897e1e85b80dad90528a43
shuyangzhou <shuyang.zhou@liferay.com>
2014-05-30 14:23:45 -0700

    LPS-47071 Fix JournalArticleFinderTest

--- portal-impl/test/integration/com/liferay/portlet/journal/service/persistence/JournalArticleFinderTest.java
+++ portal-impl/test/integration/com/liferay/portlet/journal/service/persistence/JournalArticleFinderTest.java
@@ -20,8 +20,8 @@ import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.service.GroupLocalServiceUtil;
-import com.liferay.portal.test.EnvironmentExecutionTestListener;
 import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
+import com.liferay.portal.test.MainServletExecutionTestListener;
 import com.liferay.portal.test.Sync;
 import com.liferay.portal.test.SynchronousDestinationExecutionTestListener;
 import com.liferay.portal.test.TransactionalTestRule;
@@ -65,7 +65,7 @@ import org.junit.runner.RunWith;
  */
 @ExecutionTestListeners(
 	listeners = {
-		EnvironmentExecutionTestListener.class,
+		MainServletExecutionTestListener.class,
 		SynchronousDestinationExecutionTestListener.class
 	})
 @RunWith(LiferayIntegrationJUnitTestRunner.class)
