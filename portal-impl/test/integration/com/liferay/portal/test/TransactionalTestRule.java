b407580c050268ca13a5cd1c5038320d3316051f
shuyangzhou <shuyang.zhou@liferay.com>
2014-05-29 22:36:17 -0700

    LPS-47071 Introduce TransactionalTestRule

---
+++ portal-impl/test/integration/com/liferay/portal/test/TransactionalTestRule.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.test;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.kernel.transaction.TransactionAttribute;
+import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
+
+import java.util.concurrent.Callable;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+/**
+ * @author Shuyang Zhou
+ */
+public class TransactionalTestRule implements TestRule {
+
+	public static final TransactionAttribute SUPPORTS_TRANSACTION_ATTRIBUTE;
+
+	static {
+		TransactionAttribute.Builder builder =
+			new TransactionAttribute.Builder();
+
+		builder.propagation(Propagation.SUPPORTS);
+		builder.rollbackForClasses(
+			PortalException.class, SystemException.class);
+
+		SUPPORTS_TRANSACTION_ATTRIBUTE = builder.build();
+	}
+
+	@Override
+	public Statement apply(final Statement statement, Description description) {
+
+		return new Statement() {
+
+			@Override
+			public void evaluate() throws Throwable {
+				TransactionInvokerUtil.invoke(
+					getTransactionAttribute(), new Callable<Void>() {
+
+					@Override
+					public Void call() throws Exception {
+						try {
+							statement.evaluate();
+						}
+						catch (Throwable t) {
+							throw new Exception(t);
+						}
+
+						return null;
+					}
+				});
+			}
+
+		};
+	}
+
+	public TransactionAttribute getTransactionAttribute() {
+		return SUPPORTS_TRANSACTION_ATTRIBUTE;
+	}
+
+}
