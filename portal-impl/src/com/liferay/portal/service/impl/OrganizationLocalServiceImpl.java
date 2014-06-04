d900ba66ad05e4037af668f46bd865d54ebb4f84
Cody Hoag <cody.hoag@liferay.com>
2014-06-04 13:32:56 -0400

    LRDOCS-1090 Edit Javadoc for consistency and added new method description

--- portal-impl/src/com/liferay/portal/service/impl/OrganizationLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/OrganizationLocalServiceImpl.java
@@ -499,6 +499,14 @@ public class OrganizationLocalServiceImpl
 		return organization;
 	}
 
+	/**
+	 * Returns the organization with the name.
+	 *
+	 * @param  companyId the primary key of the organization's company
+	 * @param  name the organization's name
+	 * @return the organization with the name, or <code>null</code> if no
+	 *         organization could be found
+	 */
 	@Override
 	public Organization fetchOrganization(long companyId, String name) {
 		return organizationPersistence.fetchByC_N(companyId, name);
@@ -873,14 +881,14 @@ public class OrganizationLocalServiceImpl
 
 	/**
 	 * Returns all the organizations associated with the user. If
-	 * includeAdministrative is <code>true</code>, the result includes those
-	 * organizations that are not directly associated to the user but he is an
-	 * administrator or an owner of the organization.
+	 * <code>includeAdministrative</code> is <code>true</code>, the result
+	 * includes those organizations that are indirectly associated to the user
+	 * because he is an administrator or owner of the organization.
 	 *
 	 * @param  userId the primary key of the user
-	 * @param  includeAdministrative whether to includes organizations that are
+	 * @param  includeAdministrative whether to include organizations that are
 	 *         indirectly associated to the user because he is an administrator
-	 *         or an owner of the organization
+	 *         or owner of the organization
 	 * @return the organizations associated with the user
 	 * @throws PortalException if a user with the primary key could not be found
 	 */
@@ -1020,7 +1028,7 @@ public class OrganizationLocalServiceImpl
 	}
 
 	/**
-	 * Rebuilds the organizations tree.
+	 * Rebuilds the organization's tree.
 	 *
 	 * <p>
 	 * Only call this method if the tree has become stale through operations
