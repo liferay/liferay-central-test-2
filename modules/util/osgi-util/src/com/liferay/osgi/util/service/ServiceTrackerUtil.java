a1dd87c3e0bf81744ae4ed29522efe8bf168c107
Brian Chan <brian.chan@liferay.com>
2014-06-06 15:23:55 -0700

    LPS-47340 Rename

--- modules/util/osgi-util/src/com/liferay/osgi/util/service/ServiceTrackerUtil.java
+++ modules/util/osgi-util/src/com/liferay/osgi/util/service/ServiceTrackerUtil.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.osgi.util.service;
+package com.liferay.osgi.util.service;
 
 import com.liferay.portal.kernel.util.ProxyUtil;
 
--- modules/util/osgi-util/src/com/liferay/osgi/util/service/ServiceUnavailableException.java
+++ modules/util/osgi-util/src/com/liferay/osgi/util/service/ServiceUnavailableException.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.osgi.util.service;
+package com.liferay.osgi.util.service;
 
 /**
  * @author Carlos Sierra Andrés
