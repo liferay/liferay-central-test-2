43cdb1e8ba0429f9242346c8fb62fe5b9282fc89
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-06-08 11:54:31 +0200

    LPS-44575 Adding test runner/listener stuff, as these tests couldn't be executed alone

--- portal-impl/test/integration/com/liferay/portal/util/PortalImplLayoutFullURLTest.java
+++ portal-impl/test/integration/com/liferay/portal/util/PortalImplLayoutFullURLTest.java
@@ -14,17 +14,28 @@
 
 package com.liferay.portal.util;
 
+import com.liferay.portal.kernel.test.ExecutionTestListeners;
 import com.liferay.portal.model.LayoutSet;
 import com.liferay.portal.service.VirtualHostLocalServiceUtil;
+import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
+import com.liferay.portal.test.MainServletExecutionTestListener;
+import com.liferay.portal.test.ResetDatabaseExecutionTestListener;
 import com.liferay.portal.theme.ThemeDisplay;
 
 import java.net.URL;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * @author Akos Thurzo
  */
+@ExecutionTestListeners(
+	listeners = {
+		MainServletExecutionTestListener.class,
+		ResetDatabaseExecutionTestListener.class
+	})
+@RunWith(LiferayIntegrationJUnitTestRunner.class)
 public class PortalImplLayoutFullURLTest extends PortalImplBaseURLTestCase {
 
 	@Test
