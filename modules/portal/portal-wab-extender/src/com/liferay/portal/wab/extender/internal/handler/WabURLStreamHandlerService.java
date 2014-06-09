d4c486b1f7b13acdc4b1be5cdcda509a0d1795a5
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2014-06-09 08:36:41 +0200

    LPS-47496 Simple stream handler service

---
+++ modules/portal/portal-wab-extender/src/com/liferay/portal/wab/extender/internal/handler/WabURLStreamHandlerService.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.wab.extender.internal.handler;
+
+import java.io.IOException;
+
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.url.AbstractURLStreamHandlerService;
+
+/**
+ * @author Miguel Pastor
+ * @author Raymond Augé
+ */
+public class WabURLStreamHandlerService
+	extends AbstractURLStreamHandlerService {
+
+	public WabURLStreamHandlerService(
+		BundleContext bundleContext, ClassLoader classLoader) {
+
+		_bundleContext = bundleContext;
+		_classLoader = classLoader;
+	}
+
+	@Override
+	public URLConnection openConnection(URL url) throws IOException {
+		return null;
+	}
+
+	private BundleContext _bundleContext;
+	private ClassLoader _classLoader;
+
+}
