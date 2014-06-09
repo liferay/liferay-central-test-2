966ddd4f616e212bc6fba4c1a4469460aa75b9ae
Roberto Díaz <roberto.diaz@liferay.com>
2014-06-09 11:47:01 +0200

    LPS-47433 Reformat Bookmarks Subscription tests

--- portal-impl/test/integration/com/liferay/portlet/bookmarks/service/BookmarksSubscriptionRootContainerModelTest.java
+++ portal-impl/test/integration/com/liferay/portlet/bookmarks/service/BookmarksSubscriptionRootContainerModelTest.java
@@ -14,33 +14,23 @@
 
 package com.liferay.portlet.bookmarks.service;
 
-import com.liferay.portal.kernel.settings.ModifiableSettings;
-import com.liferay.portal.kernel.settings.Settings;
-import com.liferay.portal.kernel.settings.SettingsFactoryUtil;
 import com.liferay.portal.kernel.test.ExecutionTestListeners;
-import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
 import com.liferay.portal.test.MainServletExecutionTestListener;
 import com.liferay.portal.test.Sync;
 import com.liferay.portal.test.SynchronousMailExecutionTestListener;
-import com.liferay.portal.util.BaseSubscriptionTestCase;
-import com.liferay.portal.util.PortletKeys;
+import com.liferay.portal.util.BaseSubscriptionRootContainerModelTestCase;
 import com.liferay.portal.util.test.RandomTestUtil;
 import com.liferay.portal.util.test.ServiceContextTestUtil;
 import com.liferay.portal.util.test.TestPropsValues;
 import com.liferay.portlet.bookmarks.model.BookmarksEntry;
 import com.liferay.portlet.bookmarks.model.BookmarksFolder;
-import com.liferay.portlet.bookmarks.util.BookmarksConstants;
 import com.liferay.portlet.bookmarks.util.test.BookmarksTestUtil;
 
-import org.junit.Ignore;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
- * @author Sergio González
  * @author Roberto Díaz
  */
 @ExecutionTestListeners(
@@ -50,19 +40,8 @@ import org.junit.runner.RunWith;
 	})
 @RunWith(LiferayIntegrationJUnitTestRunner.class)
 @Sync
-public class BookmarksSubscriptionTest extends BaseSubscriptionTestCase {
-
-	@Ignore
-	@Override
-	@Test
-	public void testSubscriptionClassType() {
-	}
-
-	@Ignore
-	@Override
-	@Test
-	public void testSubscriptionDefaultClassType() {
-	}
+public class BookmarksSubscriptionRootContainerModelTest
+	extends BaseSubscriptionRootContainerModelTestCase {
 
 	@Override
 	protected long addBaseModel(long containerModelId) throws Exception {
@@ -87,66 +66,11 @@ public class BookmarksSubscriptionTest extends BaseSubscriptionTestCase {
 	}
 
 	@Override
-	protected void addSubscriptionBaseModel(long baseModelId) throws Exception {
-		BookmarksEntryLocalServiceUtil.subscribeEntry(
-			TestPropsValues.getUserId(), baseModelId);
-	}
-
-	@Override
 	protected void addSubscriptionContainerModel(long containerModelId)
 		throws Exception {
 
 		BookmarksFolderLocalServiceUtil.subscribeFolder(
 			TestPropsValues.getUserId(), group.getGroupId(), containerModelId);
-	}
-
-	@Override
-	protected String getPortletId() {
-		return PortletKeys.BOOKMARKS;
-	}
-
-	@Override
-	protected String getSubscriptionBodyPreferenceName() throws Exception {
-		return "emailEntryAddedBody";
-	}
-
-	@Override
-	protected void setAddBaseModelSubscriptionBodyPreferences()
-		throws Exception {
-
-		Settings settings = SettingsFactoryUtil.getGroupServiceSettings(
-			group.getGroupId(), BookmarksConstants.SERVICE_NAME);
-
-		ModifiableSettings modifiableSettings =
-			settings.getModifiableSettings();
-
-		String germanSubscriptionBodyPreferencesKey =
-			LocalizationUtil.getPreferencesKey(
-				getSubscriptionBodyPreferenceName(),
-				LocaleUtil.toLanguageId(LocaleUtil.GERMANY));
-
-		modifiableSettings.setValue(
-			germanSubscriptionBodyPreferencesKey, GERMAN_BODY);
-
-		String spanishSubscriptionBodyPreferencesKey =
-			LocalizationUtil.getPreferencesKey(
-				getSubscriptionBodyPreferenceName(),
-				LocaleUtil.toLanguageId(LocaleUtil.SPAIN));
-
-		modifiableSettings.setValue(
-			spanishSubscriptionBodyPreferencesKey, SPANISH_BODY);
-
-		modifiableSettings.store();
-	}
-
-	@Override
-	protected long updateEntry(long baseModelId) throws Exception {
-		BookmarksEntry entry = BookmarksEntryLocalServiceUtil.getEntry(
-			baseModelId);
-
-		entry = BookmarksTestUtil.updateEntry(entry);
-
-		return entry.getEntryId();
 	}
 
 }
