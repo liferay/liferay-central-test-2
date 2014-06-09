555a601c11f521285b1654eae471eca98fe88861
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2014-06-09 10:52:40 +0200

    LPS-47496 Wire all the pieces in the activator by publishing a couple of services

--- modules/portal/portal-wab-extender/src/com/liferay/portal/wab/extender/internal/WabExtenderActivator.java
+++ modules/portal/portal-wab-extender/src/com/liferay/portal/wab/extender/internal/WabExtenderActivator.java
@@ -14,8 +14,19 @@
 
 package com.liferay.portal.wab.extender.internal;
 
+import com.liferay.portal.wab.extender.internal.artifact.WarArtifactTransformer;
+import com.liferay.portal.wab.extender.internal.handler.WabURLStreamHandlerService;
+
+import java.util.Hashtable;
+
+import org.apache.felix.fileinstall.ArtifactUrlTransformer;
+
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.url.URLConstants;
+import org.osgi.service.url.URLStreamHandlerService;
 
 /**
  * @author Miguel Pastor
@@ -24,9 +35,41 @@ import org.osgi.framework.BundleContext;
 public class WabExtenderActivator implements BundleActivator {
 
 	public void start(BundleContext bundleContext) throws Exception {
+		_registerStreamHandler(bundleContext);
+
+		_registerArtifactTransformer(bundleContext);
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
+		_serviceRegistration.unregister();
+
+		_serviceRegistration = null;
 	}
+
+	private void _registerArtifactTransformer(BundleContext bundleContext) {
+		_serviceRegistration = bundleContext.registerService(
+			ArtifactUrlTransformer.class, new WarArtifactTransformer(), null);
+	}
+
+	private void _registerStreamHandler(BundleContext bundleContext) {
+		Bundle bundle = bundleContext.getBundle(0);
+
+		Class clazz = bundle.getClass();
+
+		ClassLoader classLoader = clazz.getClassLoader();
+
+		Hashtable<String, Object> properties = new Hashtable<String, Object>();
+
+		properties.put(
+			URLConstants.URL_HANDLER_PROTOCOL, new String[] {"webbundle"});
+
+		bundleContext.registerService(
+			URLStreamHandlerService.class.getName(),
+			new WabURLStreamHandlerService(bundleContext, classLoader),
+			properties
+		);
+	}
+
+	private ServiceRegistration<ArtifactUrlTransformer> _serviceRegistration;
 
 }
