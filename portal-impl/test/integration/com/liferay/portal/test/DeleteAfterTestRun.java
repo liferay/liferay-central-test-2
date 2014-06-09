72b34a571f3bb9cc0fb05889a8056efe960c8b98
Brian Chan <brian.chan@liferay.com>
2014-06-08 21:02:41 -0700

    LPS-47491 Rename to DeleteAfterTestRun

--- portal-impl/test/integration/com/liferay/portal/test/DeleteAfterTestRun.java
+++ portal-impl/test/integration/com/liferay/portal/test/DeleteAfterTestRun.java
@@ -26,5 +26,5 @@ import java.lang.annotation.Target;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-public @interface AutoRemove {
+public @interface DeleteAfterTestRun {
 }
