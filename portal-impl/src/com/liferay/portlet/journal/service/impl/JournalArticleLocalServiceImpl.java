6bcc9a96284e55fbcc518bb0a20a8253d7790553
Pablo Pinheiro <pablo.carvalho@liferay.com>
2014-06-10 15:03:24 -0300

    LPS-47559 Removed call to updateXSDFieldMetadata from JournalArticleLocalServiceImpl

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -103,7 +103,7 @@ import com.liferay.portlet.dynamicdatamapping.model.DDMForm;
 import com.liferay.portlet.dynamicdatamapping.model.DDMFormField;
 import com.liferay.portlet.dynamicdatamapping.model.DDMStructure;
 import com.liferay.portlet.dynamicdatamapping.model.DDMTemplate;
-import com.liferay.portlet.dynamicdatamapping.storage.FieldConstants;
+import com.liferay.portlet.dynamicdatamapping.model.LocalizedValue;
 import com.liferay.portlet.dynamicdatamapping.storage.Fields;
 import com.liferay.portlet.dynamicdatamapping.util.DDMUtil;
 import com.liferay.portlet.dynamicdatamapping.util.DDMXMLUtil;
@@ -5767,6 +5767,37 @@ public class JournalArticleLocalServiceImpl
 		newArticle.setContent(contentDocument.formattedString());
 	}
 
+	protected Map<String, String> createFieldsValuesMap(String content) {
+		try {
+			Map<String, String> fieldsValuesMap = new HashMap<String, String>();
+
+			Document document = SAXReaderUtil.read(content);
+
+			Element rootElement = document.getRootElement();
+
+			List<Element> elements = rootElement.elements();
+
+			for (Element element : elements) {
+				String fieldName = element.attributeValue(
+					"name", StringPool.BLANK);
+
+				List<Element> dynamicContentElements = element.elements(
+					"dynamic-content");
+
+				for (Element dynamicContentElement : dynamicContentElements) {
+					String value = dynamicContentElement.getText();
+
+					fieldsValuesMap.put(fieldName, value);
+				}
+			}
+
+			return fieldsValuesMap;
+		}
+		catch (DocumentException de) {
+			throw new SystemException(de);
+		}
+	}
+
 	protected void format(
 			User user, long groupId, String articleId, double version,
 			boolean incrementVersion, Element root, Map<String, byte[]> images)
@@ -6517,36 +6548,43 @@ public class JournalArticleLocalServiceImpl
 			serviceContext, workflowContext);
 	}
 
+	protected void updateDDMFormFieldPredefinedValue(
+		DDMFormField ddmFormField, String ddmFormFieldValue) {
+
+		LocalizedValue predefinedValue = ddmFormField.getPredefinedValue();
+
+		for (Locale locale : predefinedValue.getAvailableLocales()) {
+			predefinedValue.addValue(locale, ddmFormFieldValue);
+		}
+	}
+
 	protected void updateDDMStructurePredefinedValues(
 			long ddmStructureId, String content, ServiceContext serviceContext)
 		throws PortalException {
 
-		try {
-			Document document = SAXReaderUtil.read(content);
+		Map<String, String> fieldsValuesMap = createFieldsValuesMap(content);
 
-			Element rootElement = document.getRootElement();
+		DDMStructure ddmStructure = ddmStructureLocalService.fetchDDMStructure(
+			ddmStructureId);
 
-			List<Element> elements = rootElement.elements();
+		DDMForm ddmForm = ddmStructure.getDDMForm();
 
-			for (Element element : elements) {
-				String fieldName = element.attributeValue(
-					"name", StringPool.BLANK);
+		Map<String, DDMFormField> ddmFormFieldsMap =
+			ddmForm.getDDMFormFieldsMap(true);
 
-				List<Element> dynamicContentElements = element.elements(
-					"dynamic-content");
+		for (Map.Entry<String, String> fieldValue :
+				fieldsValuesMap.entrySet()) {
 
-				for (Element dynamicContentElement : dynamicContentElements) {
-					String value = dynamicContentElement.getText();
+			String ddmFormFieldName = fieldValue.getKey();
+			String ddmFormFieldValue = fieldValue.getValue();
 
-					ddmStructureLocalService.updateXSDFieldMetadata(
-						ddmStructureId, fieldName,
-						FieldConstants.PREDEFINED_VALUE, value, serviceContext);
-				}
-			}
+			updateDDMFormFieldPredefinedValue(
+				ddmFormFieldsMap.get(ddmFormFieldName), ddmFormFieldValue);
 		}
-		catch (DocumentException de) {
-			throw new SystemException(de);
-		}
+
+		ddmStructure.updateDDMForm(ddmForm);
+
+		ddmStructureLocalService.updateDDMStructure(ddmStructure);
 	}
 
 	protected void updatePreviousApprovedArticle(JournalArticle article)
