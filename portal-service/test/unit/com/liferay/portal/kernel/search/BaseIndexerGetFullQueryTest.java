marcellustavares/liferay-portal

LPS-47303 Drop every last explicit class reference (MBMessage included)
https://issues.liferay.com/browse/LPS-47303

portal-service/test/unit/com/liferay/portal/kernel/search/BaseIndexerGetFullQueryTest.java
@@ -20,13 +20,16 @@ import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.util.Props;
 import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portlet.documentlibrary.model.DLFileEntry;
+import com.liferay.portlet.documentlibrary.util.DLFileEntryIndexer;
 import com.liferay.portlet.messageboards.model.MBMessage;
+import com.liferay.portlet.messageboards.util.MBMessageIndexer;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceTracker;
 import com.liferay.registry.ServiceTrackerCustomizer;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
 import javax.portlet.PortletRequest;
@@ -56,7 +59,7 @@ import org.powermock.modules.junit4.PowerMockRunner;
 public class BaseIndexerGetFullQueryTest extends PowerMockito {
 
 	@Before
-	public void setUp() {
+	public void setUp() throws Exception {
 		setUpBooleanQueryFactory();
 		setUpJSONFactory();
 		setUpProps();
@@ -176,7 +179,7 @@ public class BaseIndexerGetFullQueryTest extends PowerMockito {
 		PropsUtil.setProps(Mockito.mock(Props.class));
 	}
 
-	protected void setUpRegistries() {
+	protected void setUpRegistries() throws Exception {
 		Registry registry = mock(Registry.class);
 
 		when(
@@ -205,6 +208,15 @@ public class BaseIndexerGetFullQueryTest extends PowerMockito {
 		RegistryUtil.setRegistry(registry);
 
 		mockStatic(IndexerRegistryUtil.class, Mockito.CALLS_REAL_METHODS);
+
+		List<Indexer> indexers = Arrays.<Indexer>asList(
+			new DLFileEntryIndexer(), new MBMessageIndexer());
+
+		PowerMockito.when(
+			IndexerRegistryUtil.class, "getIndexers"
+		).thenReturn(
+			indexers
+		);
 	}
 
 	protected void setUpSearchEngine() {

liferay/liferay-central-test-2

portal-service/test/unit/com/liferay/portal/kernel/search/BaseIndexerGetFullQueryTest.java
@@ -21,13 +21,16 @@ import com.liferay.portal.kernel.util.Props;
 import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.util.test.RandomTestUtil;
 import com.liferay.portlet.documentlibrary.model.DLFileEntry;
+import com.liferay.portlet.documentlibrary.util.DLFileEntryIndexer;
 import com.liferay.portlet.messageboards.model.MBMessage;
+import com.liferay.portlet.messageboards.util.MBMessageIndexer;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceTracker;
 import com.liferay.registry.ServiceTrackerCustomizer;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
 import javax.portlet.PortletRequest;
@@ -57,7 +60,7 @@ import org.powermock.modules.junit4.PowerMockRunner;
 public class BaseIndexerGetFullQueryTest extends PowerMockito {
 
 	@Before
-	public void setUp() {
+	public void setUp() throws Exception {
 		setUpBooleanQueryFactoryUtil();
 		setUpJSONFactoryUtil();
 		setUpPropsUtil();
@@ -172,7 +175,7 @@ public class BaseIndexerGetFullQueryTest extends PowerMockito {
 		PropsUtil.setProps(props);
 	}
 
-	protected void setUpRegistryUtil() {
+	protected void setUpRegistryUtil() throws Exception {
 		Registry registry = mock(Registry.class);
 
 		when(
@@ -201,6 +204,15 @@ public class BaseIndexerGetFullQueryTest extends PowerMockito {
 		RegistryUtil.setRegistry(registry);
 
 		mockStatic(IndexerRegistryUtil.class, Mockito.CALLS_REAL_METHODS);
+
+		List<Indexer> indexers = Arrays.<Indexer>asList(
+			new DLFileEntryIndexer(), new MBMessageIndexer());
+
+		PowerMockito.when(
+			IndexerRegistryUtil.class, "getIndexers"
+		).thenReturn(
+			indexers
+		);
 	}
 
 	protected void setUpSearchEngineUtil() {