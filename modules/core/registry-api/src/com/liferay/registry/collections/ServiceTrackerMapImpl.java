a31d19b926d44ba9cecb9a43a1f39246f06c51cd
Carlos Sierra <carlos.sierra@liferay.com>
2014-06-11 08:13:53 +0200

    LPS-47465 Added empty default implementation
    This class will contain the common logic for maintaining the mapping
    with the services.

---
+++ modules/core/registry-api/src/com/liferay/registry/collections/ServiceTrackerMapImpl.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.registry.collections;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public class ServiceTrackerMapImpl<K, R> implements ServiceTrackerMap<K, R> {
+
+	public ServiceTrackerMapImpl() {
+
+	}
+
+	@Override
+	public void close() {
+		
+	}
+
+	@Override
+	public R getService(K key) {
+		return null;
+	}
+
+	@Override
+	public void open() {
+		
+	}
+
+}
