ea6360c48c82fa8a5136b87e67587bed8385647c
Carlos Sierra <carlos.sierra@liferay.com>
2014-06-11 17:10:02 +0200

    LPS-47465 Convenience method to rebuild the collection of services from the ordered set.

--- modules/core/registry-api/src/com/liferay/registry/collections/MultipleItemsBucketFactory.java
+++ modules/core/registry-api/src/com/liferay/registry/collections/MultipleItemsBucketFactory.java
@@ -14,9 +14,12 @@
 
 package com.liferay.registry.collections;
 
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceReference;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
@@ -69,6 +72,26 @@ public class MultipleItemsBucketFactory<S>
 		@Override
 		public synchronized void store(ServiceReference<S> serviceReference) {
 
+		}
+
+		private void _rebuildList() {
+			Registry registry = RegistryUtil.getRegistry();
+
+			try {
+				_services = new ArrayList<S>(_orderedSet.size());
+
+				for (ServiceReference<S> serviceReference : _orderedSet) {
+					_services.add(registry.getService(serviceReference));
+				}
+
+				_services = Collections.unmodifiableList(_services);
+			}
+			catch (IllegalStateException e) {
+				//BundleContext seems to be no longer usable
+				_orderedSet.clear();
+
+				_services = new ArrayList<S>();
+			}
 		}
 
 		private final Set<ServiceReference<S>> _orderedSet;
