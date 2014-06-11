37c21e83ff741f732573135d891e6121892bccba
Marcellus Tavares <marcellus.tavares@liferay.com>
2014-06-11 10:42:54 -0300

    LPS-47544 SF (no logic changes)

---
+++ portal-impl/test/unit/com/liferay/portlet/dynamicdatamapping/util/BaseDDMFormSerializer.java
@@ -0,0 +1,166 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.dynamicdatamapping.util;
+
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portlet.dynamicdatamapping.BaseDDMTest;
+import com.liferay.portlet.dynamicdatamapping.model.DDMForm;
+import com.liferay.portlet.dynamicdatamapping.model.DDMFormField;
+import com.liferay.portlet.dynamicdatamapping.model.DDMFormFieldOptions;
+import com.liferay.portlet.dynamicdatamapping.model.LocalizedValue;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * @author Pablo Carvalho
+ */
+public class BaseDDMFormSerializer extends BaseDDMTest {
+
+	protected List<Locale> createAvailableLocales() {
+		List<Locale> availableLocales = new ArrayList<Locale>();
+
+		availableLocales.add(LocaleUtil.BRAZIL);
+		availableLocales.add(LocaleUtil.US);
+
+		return availableLocales;
+	}
+
+	protected DDMForm createDDMForm() {
+		DDMForm ddmForm = new DDMForm();
+
+		ddmForm.setAvailableLocales(createAvailableLocales());
+		ddmForm.setDDMFormFields(createDDMFormFields());
+		ddmForm.setDefaultLocale(LocaleUtil.US);
+
+		return ddmForm;
+	}
+
+	protected DDMFormFieldOptions createDDMFormFieldOptions() {
+		DDMFormFieldOptions ddmFormFieldOptions = new DDMFormFieldOptions();
+
+		ddmFormFieldOptions.addOption("Value 1");
+
+		ddmFormFieldOptions.addOptionLabel(
+			"Value 1", LocaleUtil.BRAZIL, "Opcao 1");
+		ddmFormFieldOptions.addOptionLabel(
+			"Value 1", LocaleUtil.US, "Option 1");
+
+		ddmFormFieldOptions.addOption("Value 2");
+
+		ddmFormFieldOptions.addOptionLabel(
+			"Value 2", LocaleUtil.BRAZIL, "Opcao 2");
+		ddmFormFieldOptions.addOptionLabel(
+			"Value 2", LocaleUtil.US, "Option 2");
+
+		return ddmFormFieldOptions;
+	}
+
+	protected List<DDMFormField> createDDMFormFields() {
+		List<DDMFormField> ddmFormFields = new ArrayList<DDMFormField>();
+
+		ddmFormFields.add(
+			createNestedDDMFormFields("ParentField", "ChildField"));
+		ddmFormFields.add(createRadioDDMFormField("BooleanField"));
+		ddmFormFields.add(createSelectDDMFormField("SelectField"));
+		ddmFormFields.add(createTextDDMFormField("TextField"));
+
+		return ddmFormFields;
+	}
+
+	protected DDMFormField createNestedDDMFormFields(
+		String parentName, String childName) {
+
+		DDMFormField parentDDMFormField = createTextDDMFormField(parentName);
+
+		List<DDMFormField> nestedDDMFormFields = new ArrayList<DDMFormField>();
+
+		nestedDDMFormFields.add(createSelectDDMFormField(childName));
+
+		parentDDMFormField.setNestedDDMFormFields(nestedDDMFormFields);
+
+		return parentDDMFormField;
+	}
+
+	protected DDMFormField createRadioDDMFormField(String name) {
+		DDMFormField radioDDMFormField = new DDMFormField(name, "radio");
+
+		radioDDMFormField.setDataType("string");
+		radioDDMFormField.setDDMFormFieldOptions(createDDMFormFieldOptions());
+		radioDDMFormField.setRequired(true);
+
+		return radioDDMFormField;
+	}
+
+	protected DDMFormField createSelectDDMFormField(String name) {
+		DDMFormField selectDDMFormField = new DDMFormField(name, "select");
+
+		selectDDMFormField.setDataType("string");
+		selectDDMFormField.setIndexType("");
+		selectDDMFormField.setMultiple(true);
+
+		DDMFormFieldOptions ddmFormFieldOptions = createDDMFormFieldOptions();
+
+		selectDDMFormField.setDDMFormFieldOptions(ddmFormFieldOptions);
+
+		return selectDDMFormField;
+	}
+
+	protected DDMFormField createTextDDMFormField(String name) {
+		DDMFormField textDDMFormField = new DDMFormField(name, "text");
+
+		textDDMFormField.setDataType("string");
+		textDDMFormField.setIndexType("keyword");
+		textDDMFormField.setLabel(createTextDDMFormFieldLabel());
+		textDDMFormField.setPredefinedValue(
+			createTextDDMFormFieldPredefinedValue());
+		textDDMFormField.setRepeatable(true);
+
+		return textDDMFormField;
+	}
+
+	protected LocalizedValue createTextDDMFormFieldLabel() {
+		LocalizedValue label = new LocalizedValue();
+
+		label.addValue(LocaleUtil.BRAZIL, "Texto");
+		label.addValue(LocaleUtil.US, "Text");
+
+		return label;
+	}
+
+	protected LocalizedValue createTextDDMFormFieldPredefinedValue() {
+		LocalizedValue predefinedValue = new LocalizedValue();
+
+		predefinedValue.addValue(LocaleUtil.BRAZIL, "Exemplo");
+		predefinedValue.addValue(LocaleUtil.US, "Example");
+
+		return predefinedValue;
+	}
+
+	protected String readXML(String fileName) throws IOException {
+		Class<?> clazz = getClass();
+
+		InputStream inputStream = clazz.getResourceAsStream(
+			"dependencies/" + fileName);
+
+		return StringUtil.read(inputStream);
+	}
+
+}
--- portal-impl/test/unit/com/liferay/portlet/dynamicdatamapping/util/DDMFormXSDSerializerTest.java
+++ portal-impl/test/unit/com/liferay/portlet/dynamicdatamapping/util/DDMFormXSDSerializerTest.java
@@ -14,21 +14,10 @@
 
 package com.liferay.portlet.dynamicdatamapping.util;
 
-import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portlet.dynamicdatamapping.BaseDDMTest;
 import com.liferay.portlet.dynamicdatamapping.model.DDMForm;
-import com.liferay.portlet.dynamicdatamapping.model.DDMFormField;
-import com.liferay.portlet.dynamicdatamapping.model.DDMFormFieldOptions;
-import com.liferay.portlet.dynamicdatamapping.model.LocalizedValue;
 import com.liferay.portlet.dynamicdatamapping.util.test.DDMStructureTestUtil;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 import org.junit.Assert;
@@ -41,7 +30,7 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
  * @author Pablo Carvalho
  */
 @PrepareForTest({DDMFormXSDSerializerUtil.class, StringUtil.class})
-public class DDMFormXSDSerializerTest extends BaseDDMTest {
+public class DDMFormXSDSerializerTest extends BaseDDMFormSerializer {
 
 	@Before
 	@Override
@@ -59,135 +48,6 @@ public class DDMFormXSDSerializerTest extends BaseDDMTest {
 		String xsd = DDMFormXSDSerializerUtil.serialize(ddmForm);
 
 		testXSDMatchesExpected(xsd);
-	}
-
-	protected List<Locale> createAvailableLocales() {
-		List<Locale> availableLocales = new ArrayList<Locale>();
-
-		availableLocales.add(LocaleUtil.BRAZIL);
-		availableLocales.add(LocaleUtil.US);
-
-		return availableLocales;
-	}
-
-	protected DDMForm createDDMForm() {
-		DDMForm ddmForm = new DDMForm();
-
-		ddmForm.setAvailableLocales(createAvailableLocales());
-		ddmForm.setDDMFormFields(createDDMFormFields());
-		ddmForm.setDefaultLocale(LocaleUtil.US);
-
-		return ddmForm;
-	}
-
-	protected DDMFormFieldOptions createDDMFormFieldOptions() {
-		DDMFormFieldOptions ddmFormFieldOptions = new DDMFormFieldOptions();
-
-		ddmFormFieldOptions.addOption("Value 1");
-
-		ddmFormFieldOptions.addOptionLabel(
-			"Value 1", LocaleUtil.BRAZIL, "Opcao 1");
-		ddmFormFieldOptions.addOptionLabel(
-			"Value 1", LocaleUtil.US, "Option 1");
-
-		ddmFormFieldOptions.addOption("Value 2");
-
-		ddmFormFieldOptions.addOptionLabel(
-			"Value 2", LocaleUtil.BRAZIL, "Opcao 2");
-		ddmFormFieldOptions.addOptionLabel(
-			"Value 2", LocaleUtil.US, "Option 2");
-
-		return ddmFormFieldOptions;
-	}
-
-	protected List<DDMFormField> createDDMFormFields() {
-		List<DDMFormField> ddmFormFields = new ArrayList<DDMFormField>();
-
-		ddmFormFields.add(
-			createNestedDDMFormFields("ParentField", "ChildField"));
-		ddmFormFields.add(createRadioDDMFormField("BooleanField"));
-		ddmFormFields.add(createSelectDDMFormField("SelectField"));
-		ddmFormFields.add(createTextDDMFormField("TextField"));
-
-		return ddmFormFields;
-	}
-
-	protected DDMFormField createNestedDDMFormFields(
-		String parentName, String childName) {
-
-		DDMFormField parentDDMFormField = createTextDDMFormField(parentName);
-
-		List<DDMFormField> nestedDDMFormFields = new ArrayList<DDMFormField>();
-
-		nestedDDMFormFields.add(createSelectDDMFormField(childName));
-
-		parentDDMFormField.setNestedDDMFormFields(nestedDDMFormFields);
-
-		return parentDDMFormField;
-	}
-
-	protected DDMFormField createRadioDDMFormField(String name) {
-		DDMFormField radioDDMFormField = new DDMFormField(name, "radio");
-
-		radioDDMFormField.setDataType("string");
-		radioDDMFormField.setDDMFormFieldOptions(createDDMFormFieldOptions());
-		radioDDMFormField.setRequired(true);
-
-		return radioDDMFormField;
-	}
-
-	protected DDMFormField createSelectDDMFormField(String name) {
-		DDMFormField selectDDMFormField = new DDMFormField(name, "select");
-
-		selectDDMFormField.setDataType("string");
-		selectDDMFormField.setIndexType("");
-		selectDDMFormField.setMultiple(true);
-
-		DDMFormFieldOptions ddmFormFieldOptions = createDDMFormFieldOptions();
-
-		selectDDMFormField.setDDMFormFieldOptions(ddmFormFieldOptions);
-
-		return selectDDMFormField;
-	}
-
-	protected DDMFormField createTextDDMFormField(String name) {
-		DDMFormField textDDMFormField = new DDMFormField(name, "text");
-
-		textDDMFormField.setDataType("string");
-		textDDMFormField.setIndexType("keyword");
-		textDDMFormField.setLabel(createTextDDMFormFieldLabel());
-		textDDMFormField.setPredefinedValue(
-			createTextDDMFormFieldPredefinedValue());
-		textDDMFormField.setRepeatable(true);
-
-		return textDDMFormField;
-	}
-
-	protected LocalizedValue createTextDDMFormFieldLabel() {
-		LocalizedValue label = new LocalizedValue();
-
-		label.addValue(LocaleUtil.BRAZIL, "Texto");
-		label.addValue(LocaleUtil.US, "Text");
-
-		return label;
-	}
-
-	protected LocalizedValue createTextDDMFormFieldPredefinedValue() {
-		LocalizedValue predefinedValue = new LocalizedValue();
-
-		predefinedValue.addValue(LocaleUtil.BRAZIL, "Exemplo");
-		predefinedValue.addValue(LocaleUtil.US, "Example");
-
-		return predefinedValue;
-	}
-
-	protected String readXML(String fileName) throws IOException {
-		Class<?> clazz = getClass();
-
-		InputStream inputStream = clazz.getResourceAsStream(
-			"dependencies/" + fileName);
-
-		return StringUtil.read(inputStream);
 	}
 
 	protected void setUpDDMFormToXSDSerializer() {
