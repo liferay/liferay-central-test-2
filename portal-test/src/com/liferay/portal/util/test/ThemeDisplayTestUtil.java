a4ae2d0f1ed9a5f13a805b3056615b0492e0925d
Roberto Díaz <roberto.diaz@liferay.com>
2014-06-16 12:32:41 +0200

    Revert "LPS-47389 Sort"
    This reverts commit c90b76d664e4ba4c0c92beda92bf7bc2de30bcc6.

--- portal-test/src/com/liferay/portal/util/test/ThemeDisplayTestUtil.java
+++ portal-test/src/com/liferay/portal/util/test/ThemeDisplayTestUtil.java
@@ -40,8 +40,6 @@ public class ThemeDisplayTestUtil {
 	public static ThemeDisplay getThemeDisplay(long companyId)
 		throws PortalException, SystemException {
 
-		ThemeDisplay themeDisplay = new ThemeDisplay();
-
 		HttpServletRequest request = new MockHttpServletRequest();
 
 		request.setAttribute(WebKeys.COMPANY_ID, companyId);
@@ -49,8 +47,9 @@ public class ThemeDisplayTestUtil {
 			WebKeys.CURRENT_URL, "http://localhost:80/web/guest/home");
 		request.setAttribute(WebKeys.USER, TestPropsValues.getUser());
 
-		themeDisplay.setRequest(request);
+		ThemeDisplay themeDisplay = new ThemeDisplay();
 
+		themeDisplay.setRequest(request);
 		themeDisplay.setSecure(false);
 		themeDisplay.setServerPort(8080);
 		themeDisplay.setWidget(false);
