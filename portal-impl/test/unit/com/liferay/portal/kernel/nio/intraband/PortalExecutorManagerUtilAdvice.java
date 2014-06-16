bb2cdf6c9ec37bd64122a46365aa4039208a1fdd
shuyangzhou <shuyang.zhou@liferay.com>
2014-06-16 16:57:21 -0700

    LPS-46902 Unit test

--- portal-impl/test/unit/com/liferay/portal/kernel/nio/intraband/PortalExecutorManagerUtilAdvice.java
+++ portal-impl/test/unit/com/liferay/portal/kernel/nio/intraband/PortalExecutorManagerUtilAdvice.java
@@ -27,7 +27,7 @@ public class PortalExecutorManagerUtilAdvice {
 
 	@Around(
 		"execution(* com.liferay.portal.kernel.executor." +
-			"PortalExecutorManagerUtil.getPortalExecutor(String))")
+			"PortalExecutorManagerUtil.getPortalExecutor(..))")
 	public ThreadPoolExecutor getPortalExecutor() {
 		return new ThreadPoolExecutor(0, 1) {
 
