d538142b87e605deb36ffb484c4129cfded86079
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2014-06-17 11:08:48 -0700

    LPS-47709 iFrames in an iPad are displayed off to the left half out of view

--- portal-web/docroot/html/js/liferay/util_window.js
+++ portal-web/docroot/html/js/liferay/util_window.js
@@ -164,7 +164,13 @@ AUI.add(
 										modalUtil.Window._name = id;
 									}
 
-									modal.iframe.node.focus();
+									var iframeNode = modal.iframe.node;
+
+									iframeNode.focus();
+
+									if (A.UA.ios) {
+										iframeNode.attr('scrolling', 'no');
+									}
 								}
 							}
 						)
