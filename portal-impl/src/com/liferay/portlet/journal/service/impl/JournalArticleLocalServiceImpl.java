d60e35fe1e7583984c9c4da507c1f62295268837
Julio Camarero <julio.camarero@liferay.com>
2014-06-18 14:32:28 +0200

    LPS-46804 Support for setting a publish date in the past

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -5009,12 +5009,6 @@ public class JournalArticleLocalServiceImpl
 
 					Date publishDate = dateInterval[0];
 
-					if ((oldStatus != WorkflowConstants.STATUS_APPROVED) &&
-						publishDate.before(now)) {
-
-						publishDate = now;
-					}
-
 					Date expirationDate = dateInterval[1];
 
 					if (neverExpire) {
