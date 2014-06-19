98ed7715584649ba58deab206f4ed132cbe95f20
Daniel Kocsis <daniel.kocsis@liferay.com>
2014-06-19 17:42:55 +0200

    LPS-47090 Use the saved dates when possible and try to calculate otherwise

--- portal-service/src/com/liferay/portal/kernel/lar/ExportImportDateUtil.java
+++ portal-service/src/com/liferay/portal/kernel/lar/ExportImportDateUtil.java
@@ -94,6 +94,13 @@ public class ExportImportDateUtil {
 
 		Map<String, Serializable> settingsMap = configuration.getSettingsMap();
 
+		Date endDate = (Date)settingsMap.get("endDate");
+		Date startDate = (Date)settingsMap.get("startDate");
+
+		if ((endDate != null) && (startDate != null)) {
+			return new DateRange(startDate, endDate);
+		}
+
 		Map<String, String[]> parameterMap =
 			(Map<String, String[]>)settingsMap.get("parameterMap");
 
