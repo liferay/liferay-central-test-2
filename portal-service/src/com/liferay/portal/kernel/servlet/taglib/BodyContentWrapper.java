f72cacbb3b84407f1228e5548e06a2447f2b6e17
Brian Chan <brian.chan@liferay.com>
2014-06-19 22:18:45 -0700

    LPS-47682 SF

--- portal-service/src/com/liferay/portal/kernel/servlet/taglib/BodyContentWrapper.java
+++ portal-service/src/com/liferay/portal/kernel/servlet/taglib/BodyContentWrapper.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.portal.kernel.servlet.taglib;
 
 import com.liferay.portal.kernel.util.StringBundler;
