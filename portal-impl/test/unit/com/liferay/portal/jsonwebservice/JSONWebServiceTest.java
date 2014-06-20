8b8880cf5229c74c698522f381680c3409135268
Igor Spasić <igor.spasic@liferay.com>
2014-06-20 10:25:00 +0200

    LPS-45197 fixed one and added new testcase

--- portal-impl/test/unit/com/liferay/portal/jsonwebservice/JSONWebServiceTest.java
+++ portal-impl/test/unit/com/liferay/portal/jsonwebservice/JSONWebServiceTest.java
@@ -171,7 +171,7 @@ public class JSONWebServiceTest extends BaseJSONWebServiceTestCase {
 
 			Assert.fail();
 		}
-		catch (IllegalArgumentException iae) {
+		catch (Exception e) {
 		}
 
 		mockHttpServletRequest = createHttpRequest(
@@ -215,6 +215,19 @@ public class JSONWebServiceTest extends BaseJSONWebServiceTestCase {
 		}
 		catch (Exception e) {
 		}
+
+		mockHttpServletRequest = createHttpRequest("/foo/use2");
+
+		mockHttpServletRequest.setParameter(
+			"fooData:" + FooDataImpl.class.getName(),
+			"{height: 121, name:'Felix', value:'!!!'}");
+
+		jsonWebServiceAction = lookupJSONWebServiceAction(
+			mockHttpServletRequest);
+
+		Assert.assertEquals(
+			"using #2: h=121/id=-1/n=Felix/v=!!!",
+			jsonWebServiceAction.invoke());
 	}
 
 	@Test
