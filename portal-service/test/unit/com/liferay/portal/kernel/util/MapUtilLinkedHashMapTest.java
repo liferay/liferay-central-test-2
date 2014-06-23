0876087f58674fb692e2539834095450614d4c18
Máté Thurzó <mate.thurzo@liferay.com>
2014-06-23 11:38:13 +0200

    LPS-47094 Source formatting - method names and assertion ordering

--- portal-service/test/unit/com/liferay/portal/kernel/util/MapUtilLinkedHashMapTest.java
+++ portal-service/test/unit/com/liferay/portal/kernel/util/MapUtilLinkedHashMapTest.java
@@ -25,115 +25,120 @@ import org.junit.Test;
 public class MapUtilLinkedHashMapTest {
 
 	@Test
-	public void testToLinkedHashMapBoolean() throws Exception {
+	public void testCustomDelimiter() throws Exception {
+		Map<String, String> map = MapUtil.toLinkedHashMap(
+			new String[] {"one,1"}, ",");
+
+		Assert.assertTrue(map.size() == 1);
+
+		Assert.assertTrue(map.containsKey("one"));
+		Assert.assertTrue(map.containsValue("1"));
+	}
+
+	@Test
+	public void testDefaultDelimiter() throws Exception {
+		Map<String, String> map = MapUtil.toLinkedHashMap(
+			new String[] {"one:1"});
+
+		Assert.assertTrue(map.size() == 1);
+
+		Assert.assertTrue(map.containsKey("one"));
+		Assert.assertTrue(map.containsValue("1"));
+	}
+
+	@Test
+	public void testTypeBoolean() throws Exception {
 		Map<String, Object> map = MapUtil.toLinkedHashMap(
 			new String[] {"one:true:boolean"});
 
-		Assert.assertFalse(map.isEmpty());
 		Assert.assertTrue(map.size() == 1);
-		Assert.assertTrue(map.containsValue(true));
+
 		Assert.assertTrue(map.containsKey("one"));
+		Assert.assertTrue(map.containsValue(true));
+
 		Assert.assertTrue(map.get("one") instanceof Boolean);
 	}
 
 	@Test
-	public void testToLinkedHashMapDefaultDelimiter() throws Exception {
-		Map<String, String> map = MapUtil.toLinkedHashMap(
-			new String[] {"one:1"});
-
-		Assert.assertFalse(map.isEmpty());
-		Assert.assertTrue(map.size() == 1);
-		Assert.assertTrue(map.containsValue("1"));
-		Assert.assertTrue(map.containsKey("one"));
-	}
-
-	@Test
-	public void testToLinkedHashMapDouble() throws Exception {
+	public void testTypeByte() throws Exception {
 		Map<String, Object> map = MapUtil.toLinkedHashMap(
-			new String[] {"one:1.0:double"});
+			new String[] {"one:1:" + Byte.class.getName()});
 
-		Assert.assertFalse(map.isEmpty());
 		Assert.assertTrue(map.size() == 1);
-		Assert.assertTrue(map.containsValue(1.0d));
+
 		Assert.assertTrue(map.containsKey("one"));
-		Assert.assertTrue(map.get("one") instanceof Double);
-	}
-
-	@Test
-	public void testToLinkedHashMapInteger() throws Exception {
-		Map<String, Object> map = MapUtil.toLinkedHashMap(
-			new String[] {"one:1:int"});
-
-		Assert.assertFalse(map.isEmpty());
-		Assert.assertTrue(map.size() == 1);
-		Assert.assertTrue(map.containsValue(1));
-		Assert.assertTrue(map.containsKey("one"));
-		Assert.assertTrue(map.get("one") instanceof Integer);
-	}
-
-	@Test
-	public void testToLinkedHashMapLong() throws Exception {
-		Map<String, Object> map = MapUtil.toLinkedHashMap(
-			new String[] {"one:1:long"});
-
-		Assert.assertFalse(map.isEmpty());
-		Assert.assertTrue(map.size() == 1);
-		Assert.assertTrue(map.containsValue(1l));
-		Assert.assertTrue(map.containsKey("one"));
-		Assert.assertTrue(map.get("one") instanceof Long);
-	}
-
-	@Test
-	public void testToLinkedHashMapOneLength() throws Exception {
-		Map<String, String> map = MapUtil.toLinkedHashMap(
-			new String[] {"one,1"}, ",");
-
-		Assert.assertFalse(map.isEmpty());
-		Assert.assertTrue(map.size() == 1);
-		Assert.assertTrue(map.containsValue("1"));
-		Assert.assertTrue(map.containsKey("one"));
-	}
-
-	@Test
-	public void testToLinkedHashMapOtherType() throws Exception {
-		Map<String, Object> map = MapUtil.toLinkedHashMap(
-			new String[] {"one:1:java.lang.Byte"});
-
-		Assert.assertFalse(map.isEmpty());
-		Assert.assertTrue(map.size() == 1);
 		Assert.assertTrue(map.containsValue((byte)1));
-		Assert.assertTrue(map.containsKey("one"));
+
 		Assert.assertTrue(map.get("one") instanceof Byte);
 	}
 
 	@Test
-	public void testToLinkedHashMapShort() throws Exception {
+	public void testTypeDouble() throws Exception {
+		Map<String, Object> map = MapUtil.toLinkedHashMap(
+			new String[] {"one:1.0:double"});
+
+		Assert.assertTrue(map.size() == 1);
+
+		Assert.assertTrue(map.containsKey("one"));
+		Assert.assertTrue(map.containsValue(1.0d));
+
+		Assert.assertTrue(map.get("one") instanceof Double);
+	}
+
+	@Test
+	public void testTypeInteger() throws Exception {
+		Map<String, Object> map = MapUtil.toLinkedHashMap(
+			new String[] {"one:1:int"});
+
+		Assert.assertTrue(map.size() == 1);
+
+		Assert.assertTrue(map.containsKey("one"));
+		Assert.assertTrue(map.containsValue(1));
+
+		Assert.assertTrue(map.get("one") instanceof Integer);
+	}
+
+	@Test
+	public void testTypeLong() throws Exception {
+		Map<String, Object> map = MapUtil.toLinkedHashMap(
+			new String[] {"one:1:long"});
+
+		Assert.assertTrue(map.size() == 1);
+
+		Assert.assertTrue(map.containsKey("one"));
+		Assert.assertTrue(map.containsValue(1l));
+
+		Assert.assertTrue(map.get("one") instanceof Long);
+	}
+
+	@Test
+	public void testTypeShort() throws Exception {
 		Map<String, Object> map = MapUtil.toLinkedHashMap(
 			new String[] {"one:1:short"});
 
-		Assert.assertFalse(map.isEmpty());
 		Assert.assertTrue(map.size() == 1);
-		Assert.assertTrue(map.containsValue((short)1));
+
 		Assert.assertTrue(map.containsKey("one"));
+		Assert.assertTrue(map.containsValue((short)1));
+
 		Assert.assertTrue(map.get("one") instanceof Short);
 	}
 
 	@Test
-	public void testToLinkedHashMapString() throws Exception {
-		String type = String.class.getName();
-
+	public void testTypeString() throws Exception {
 		Map<String, Object> map = MapUtil.toLinkedHashMap(
-			new String[] {"one:X:" + type});
+			new String[] {"one:X:" + String.class.getName()});
 
-		Assert.assertFalse(map.isEmpty());
 		Assert.assertTrue(map.size() == 1);
-		Assert.assertTrue(map.containsValue("X"));
+
 		Assert.assertTrue(map.containsKey("one"));
+		Assert.assertTrue(map.containsValue("X"));
+
 		Assert.assertTrue(map.get("one") instanceof String);
 	}
 
 	@Test
-	public void testToLinkedHashMapZeroLength() throws Exception {
+	public void testZeroLength() throws Exception {
 		Map<String, String> map = MapUtil.toLinkedHashMap(new String[] {});
 
 		Assert.assertTrue(map.isEmpty());
