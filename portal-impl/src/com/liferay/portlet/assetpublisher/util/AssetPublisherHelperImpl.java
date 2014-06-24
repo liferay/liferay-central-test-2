f768d50e111c0a2bdeb2ccb1046f73db48b6d5a2
Brian Chan <brian.chan@liferay.com>
2014-06-24 10:25:29 -0700

    LPS-47632 SF

--- portal-impl/src/com/liferay/portlet/assetpublisher/util/AssetPublisherHelperImpl.java
+++ portal-impl/src/com/liferay/portlet/assetpublisher/util/AssetPublisherHelperImpl.java
@@ -20,7 +20,6 @@ import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.theme.ThemeDisplay;
-import com.liferay.portal.util.PortalUtil;
 import com.liferay.portlet.PortletURLUtil;
 import com.liferay.portlet.asset.model.AssetEntry;
 import com.liferay.portlet.asset.model.AssetRenderer;
