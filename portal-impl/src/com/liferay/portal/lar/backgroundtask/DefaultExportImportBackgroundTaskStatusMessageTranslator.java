brianchandotcom/liferay-portal

LPS-45350 SF - while it's not necessary, it's immediately visible like this

portal-impl/src/com/liferay/portal/lar/backgroundtask/DefaultExportImportBackgroundTaskStatusMessageTranslator.java
@@ -55,16 +55,17 @@ public class DefaultExportImportBackgroundTaskStatusMessageTranslator
 
 		backgroundTaskStatus.clearAttributes();
 
-		backgroundTaskStatus.setAttribute("allModelAdditionCountersCurrent", 0);
-		backgroundTaskStatus.setAttribute("allModelAdditionCountersTotal", 0);
+		backgroundTaskStatus.setAttribute(
+			"allModelAdditionCountersCurrent", 0L);
+		backgroundTaskStatus.setAttribute("allModelAdditionCountersTotal", 0L);
 		backgroundTaskStatus.setAttribute(
 			"modelAdditionsByPortletsCurrent",
 			new HashMap<String, LongWrapper>());
 		backgroundTaskStatus.setAttribute(
 			"modelAdditionsByPortletsTotal",
 			new HashMap<String, LongWrapper>());
-		backgroundTaskStatus.setAttribute("portletsNumberCurrent", 0);
-		backgroundTaskStatus.setAttribute("portletsNumberTotal", 0);
+		backgroundTaskStatus.setAttribute("portletsNumberCurrent", 0L);
+		backgroundTaskStatus.setAttribute("portletsNumberTotal", 0L);
 	}
 
 	protected long getTotal(Map<String, LongWrapper> modelCounters) {

liferay/liferay-central-test-2

Conflicts:
	portal-impl/src/com/liferay/portal/lar/backgroundtask/DefaultExportImportBackgroundTaskStatusMessageTranslator.java

portal-impl/src/com/liferay/portal/lar/backgroundtask/DefaultExportImportBackgroundTaskStatusMessageTranslator.java
@@ -55,14 +55,14 @@ public class DefaultExportImportBackgroundTaskStatusMessageTranslator
 
 		backgroundTaskStatus.clearAttributes();
 
-		backgroundTaskStatus.setAttribute("allModelAdditionCountersTotal", 0);
-		backgroundTaskStatus.setAttribute("allPortletAdditionCounter", 0);
+		backgroundTaskStatus.setAttribute("allModelAdditionCountersTotal", 0L);
+		backgroundTaskStatus.setAttribute("allPortletAdditionCounter", 0L);
 		backgroundTaskStatus.setAttribute(
 			"allPortletModelAdditionCounters",
 			new HashMap<String, LongWrapper>());
 		backgroundTaskStatus.setAttribute(
-			"currentModelAdditionCountersTotal", 0);
-		backgroundTaskStatus.setAttribute("currentPortletAdditionCounter", 0);
+			"currentModelAdditionCountersTotal", 0L);
+		backgroundTaskStatus.setAttribute("currentPortletAdditionCounter", 0L);
 		backgroundTaskStatus.setAttribute(
 			"currentPortletModelAdditionCounters",
 			new HashMap<String, LongWrapper>());