d81eea18dfc545ab8bd1d324dfa0394125821d78
shuyangzhou <shuyang.zhou@liferay.com>
2014-06-26 14:55:49 -0700

    LPS-48056  Refactor OrderByComparatorFactoryUtil to be a plain utility class

--- portal-service/src/com/liferay/portal/kernel/util/OrderByComparatorFactoryUtil.java
+++ portal-service/src/com/liferay/portal/kernel/util/OrderByComparatorFactoryUtil.java
@@ -14,7 +14,11 @@
 
 package com.liferay.portal.kernel.util;
 
-import com.liferay.portal.kernel.security.pacl.permission.PortalRuntimePermission;
+import com.liferay.portal.kernel.bean.BeanPropertiesUtil;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Brian Wing Shun Chan
@@ -24,24 +28,173 @@ public class OrderByComparatorFactoryUtil {
 	public static OrderByComparator create(
 		String tableName, Object... columns) {
 
-		return getOrderByComparatorFactory().create(tableName, columns);
+		if ((columns.length == 0) || ((columns.length % 2) != 0)) {
+			throw new IllegalArgumentException(
+				"Columns length is not an even number");
+		}
+
+		return new DefaultOrderByComparator(tableName, columns);
 	}
 
-	public static OrderByComparatorFactory getOrderByComparatorFactory() {
-		PortalRuntimePermission.checkGetBeanProperty(
-			OrderByComparatorFactoryUtil.class);
+	protected static class DefaultOrderByComparator extends OrderByComparator {
 
-		return _orderByComparatorFactory;
+		@Override
+		public int compare(Object object1, Object object2) {
+			for (int i = 0; i < _columns.length; i += 2) {
+				String columnName = String.valueOf(_columns[i]);
+				boolean columnAscending = Boolean.valueOf(
+					String.valueOf(_columns[i + 1]));
+
+				Object columnInstance = null;
+
+				Class<?> columnClass = BeanPropertiesUtil.getObjectTypeSilent(
+					object1, columnName);
+
+				if (columnClass.isPrimitive()) {
+					columnInstance = _primitiveObjects.get(columnClass);
+				}
+				else {
+					try {
+						columnInstance = columnClass.newInstance();
+					}
+					catch (Exception e) {
+					}
+				}
+
+				Object columnValue1 = BeanPropertiesUtil.getObjectSilent(
+					object1, columnName);
+				Object columnValue2 = BeanPropertiesUtil.getObjectSilent(
+					object2, columnName);
+
+				if (columnInstance instanceof Date) {
+					Date columnValueDate1 = (Date)columnValue1;
+					Date columnValueDate2 = (Date)columnValue2;
+
+					int value = DateUtil.compareTo(
+						columnValueDate1, columnValueDate2);
+
+					if (value == 0) {
+						continue;
+					}
+
+					if (columnAscending) {
+						return value;
+					}
+					else {
+						return -value;
+					}
+				}
+				else if (columnInstance instanceof Comparable<?>) {
+					Comparable<Object> columnValueComparable1 =
+						(Comparable<Object>)columnValue1;
+					Comparable<Object> columnValueComparable2 =
+						(Comparable<Object>)columnValue2;
+
+					int value = columnValueComparable1.compareTo(
+						columnValueComparable2);
+
+					if (value == 0) {
+						continue;
+					}
+
+					if (columnAscending) {
+						return value;
+					}
+					else {
+						return -value;
+					}
+				}
+			}
+
+			return 0;
+		}
+
+		@Override
+		public String getOrderBy() {
+			StringBundler sb = new StringBundler(5 * _columns.length - 1);
+
+			for (int i = 0; i < _columns.length; i += 2) {
+				if (i != 0) {
+					sb.append(StringPool.COMMA);
+				}
+
+				sb.append(_tableName);
+				sb.append(StringPool.PERIOD);
+
+				String columnName = String.valueOf(_columns[i]);
+				boolean columnAscending = Boolean.valueOf(
+					String.valueOf(_columns[i + 1]));
+
+				sb.append(columnName);
+
+				if (columnAscending) {
+					sb.append(_ORDER_BY_ASC);
+				}
+				else {
+					sb.append(_ORDER_BY_DESC);
+				}
+			}
+
+			return sb.toString();
+		}
+
+		@Override
+		public boolean isAscending(String field) {
+			String orderBy = getOrderBy();
+
+			if (orderBy == null) {
+				return false;
+			}
+
+			int x = orderBy.indexOf(
+				StringPool.PERIOD + field + StringPool.SPACE);
+
+			if (x == -1) {
+				return false;
+			}
+
+			int y = orderBy.indexOf(_ORDER_BY_ASC, x);
+
+			if (y == -1) {
+				return false;
+			}
+
+			int z = orderBy.indexOf(_ORDER_BY_DESC, x);
+
+			if ((z >= 0) && (z < y)) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+
+		private DefaultOrderByComparator(String tableName, Object... columns) {
+			_tableName = tableName;
+			_columns = columns;
+		}
+
+		private static final String _ORDER_BY_ASC = " ASC";
+
+		private static final String _ORDER_BY_DESC = " DESC";
+
+		private static Map<Class<?>, Object> _primitiveObjects =
+			new HashMap<Class<?>, Object>();
+
+		static {
+			_primitiveObjects.put(boolean.class, new Boolean(true));
+			_primitiveObjects.put(byte.class, new Byte("0"));
+			_primitiveObjects.put(char.class, new Character('0'));
+			_primitiveObjects.put(double.class, new Double(0));
+			_primitiveObjects.put(float.class, new Float(0));
+			_primitiveObjects.put(int.class, new Integer(0));
+			_primitiveObjects.put(long.class, new Long(0));
+			_primitiveObjects.put(short.class, new Short("0"));
+		}
+
+		private Object[] _columns;
+		private String _tableName;
+
 	}
-
-	public void setOrderByComparatorFactory(
-		OrderByComparatorFactory orderByComparatorFactory) {
-
-		PortalRuntimePermission.checkSetBeanProperty(getClass());
-
-		_orderByComparatorFactory = orderByComparatorFactory;
-	}
-
-	private static OrderByComparatorFactory _orderByComparatorFactory;
 
 }
