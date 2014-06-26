f32af46f01813f90001b5d9a6872be35f9d962ea
Raymond Auge <raymond.auge@liferay.com>
2014-06-26 17:29:24 -0400

    LPS-48050 add some utility adapter classes to the API

---
+++ modules/core/registry-api/src/com/liferay/registry/util/UMDictionaryMap.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.registry.util;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Raymond Augé
+ */
+public class UMDictionaryMap <K, V> implements Map<K, V> {
+
+	public UMDictionaryMap(Dictionary<K, V> dictionary) {
+		Map<K, V> map = new HashMap<K, V>();
+
+		if (dictionary != null) {
+			for (Enumeration<K> em = dictionary.keys(); em.hasMoreElements();) {
+				K key = em.nextElement();
+
+				map.put(key, dictionary.get(key));
+			}
+		}
+
+		_map = Collections.unmodifiableMap(map);
+	}
+
+	@Override
+	public int size() {
+		return _map.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return _map.isEmpty();
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		return _map.containsKey(key);
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		return _map.containsValue(value);
+	}
+
+	@Override
+	public V get(Object key) {
+		return _map.get(key);
+	}
+
+	@Override
+	public V put(K key, V value) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public V remove(Object key) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void putAll(Map<? extends K, ? extends V> m) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void clear() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Set<K> keySet() {
+		return _map.keySet();
+	}
+
+	@Override
+	public Collection<V> values() {
+		return _map.values();
+	}
+
+	@Override
+	public Set<java.util.Map.Entry<K, V>> entrySet() {
+		return _map.entrySet();
+	}
+
+	private final Map<K, V> _map;
+
+}
---
+++ modules/core/registry-api/src/com/liferay/registry/util/UMMapDictionary.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.registry.util;
+
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Map;
+
+/**
+ * @author Raymond Augé
+ */
+public class UMMapDictionary <K, V> extends Dictionary<K, V> {
+
+	public UMMapDictionary(Map<K, V> map) {
+		if (map == null) {
+			_map = Collections.emptyMap();
+		}
+		else {
+			_map = Collections.unmodifiableMap(map);
+		}
+
+		_keys = Collections.enumeration(_map.keySet());
+		_elements = Collections.enumeration(_map.values());
+	}
+
+	@Override
+	public int size() {
+		return _map.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return _map.isEmpty();
+	}
+
+	@Override
+	public Enumeration<K> keys() {
+		return _keys;
+	}
+
+	@Override
+	public Enumeration<V> elements() {
+		return _elements;
+	}
+
+	@Override
+	public V get(Object key) {
+		return _map.get(key);
+	}
+
+	@Override
+	public V put(K key, V value) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public V remove(Object key) {
+		throw new UnsupportedOperationException();
+	}
+
+	private final Enumeration<K> _keys;
+	private final Enumeration<V> _elements;
+	private final Map<K, V> _map;
+
+}
