ed69947d61dc1d48b8a126fa496a8a158405de2e
Daniel Kocsis <daniel.kocsis@liferay.com>
2014-06-26 17:26:16 +0200

    LPS-48039 Since the original method this case tested doesn't exist anymore in LayoutExporter it doesn't make sense to have a test method for a method exists only in the same test class

--- portal-impl/test/integration/com/liferay/portal/lar/LayoutExportImportTest.java
+++ portal-impl/test/integration/com/liferay/portal/lar/LayoutExportImportTest.java
@@ -16,45 +16,28 @@ package com.liferay.portal.lar;
 
 import com.liferay.portal.LARTypeException;
 import com.liferay.portal.LocaleException;
-import com.liferay.portal.kernel.lar.PortletDataHandler;
-import com.liferay.portal.kernel.lar.PortletDataHandlerControl;
 import com.liferay.portal.kernel.lar.PortletDataHandlerKeys;
-import com.liferay.portal.kernel.staging.StagingConstants;
 import com.liferay.portal.kernel.test.ExecutionTestListeners;
-import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.model.Company;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.LayoutPrototype;
 import com.liferay.portal.model.LayoutSetPrototype;
-import com.liferay.portal.model.LayoutTypePortlet;
-import com.liferay.portal.model.Portlet;
 import com.liferay.portal.service.CompanyLocalServiceUtil;
-import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
-import com.liferay.portal.service.PortletLocalServiceUtil;
-import com.liferay.portal.service.ServiceContext;
-import com.liferay.portal.service.ServiceContextThreadLocal;
-import com.liferay.portal.service.StagingLocalServiceUtil;
 import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
 import com.liferay.portal.test.MainServletExecutionTestListener;
 import com.liferay.portal.test.ResetDatabaseExecutionTestListener;
 import com.liferay.portal.test.Sync;
 import com.liferay.portal.test.SynchronousDestinationExecutionTestListener;
-import com.liferay.portal.util.PortletKeys;
 import com.liferay.portal.util.test.GroupTestUtil;
 import com.liferay.portal.util.test.LayoutTestUtil;
 import com.liferay.portal.util.test.RandomTestUtil;
-import com.liferay.portal.util.test.ServiceContextTestUtil;
 import com.liferay.portal.util.test.TestPropsValues;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Set;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -431,80 +414,6 @@ public class LayoutExportImportTest extends BaseExportImportTestCase {
 		exportImportLayouts(layoutIds, getImportParameterMap());
 	}
 
-	@Test
-	public void testGetPortletDataHandlerPortlets() throws Exception {
-		LayoutTestUtil.addPortletToLayout(layout, PortletKeys.BOOKMARKS);
-		LayoutTestUtil.addPortletToLayout(layout, PortletKeys.LOGIN);
-
-		ServiceContext serviceContext =
-			ServiceContextTestUtil.getServiceContext(group.getGroupId());
-
-		serviceContext.setAttribute(
-			StagingConstants.STAGED_PORTLET + PortletKeys.BOOKMARKS,
-			Boolean.TRUE);
-
-		ServiceContextThreadLocal.pushServiceContext(serviceContext);
-
-		StagingLocalServiceUtil.enableLocalStaging(
-			TestPropsValues.getUserId(), group, false, false, serviceContext);
-
-		Group stagingGroup = group.getStagingGroup();
-
-		Layout stagingLayout = LayoutLocalServiceUtil.getLayoutByUuidAndGroupId(
-			layout.getUuid(), stagingGroup.getGroupId(),
-			layout.isPrivateLayout());
-
-		List<Layout> layouts = new ArrayList<Layout>();
-
-		layouts.add(stagingLayout);
-
-		List<Portlet> portlets = getPortletDataHandlerPortlets(
-			group.getGroupId(), layouts);
-
-		Assert.assertEquals(2, portlets.size());
-
-		for (Portlet portlet : portlets) {
-			String portletId = portlet.getPortletId();
-
-			if (!portletId.equals(PortletKeys.BOOKMARKS) &&
-				!portletId.equals(PortletKeys.LOGIN)) {
-
-				Assert.fail();
-			}
-		}
-
-		ServiceContextThreadLocal.popServiceContext();
-	}
-
-	protected void addPortlet(
-			List<Portlet> portlets, Set<String> rootPortletIds, Portlet portlet,
-			long groupId, long plid, boolean privateLayout)
-		throws Exception {
-
-		if ((portlet == null) ||
-			rootPortletIds.contains(portlet.getRootPortletId())) {
-
-			return;
-		}
-
-		PortletDataHandler portletDataHandler =
-			portlet.getPortletDataHandlerInstance();
-
-		if (portletDataHandler == null) {
-			return;
-		}
-
-		PortletDataHandlerControl[] portletDataHandlerControls =
-			portletDataHandler.getExportConfigurationControls(
-				portlet.getCompanyId(), groupId, portlet, plid, privateLayout);
-
-		if (ArrayUtil.isNotEmpty(portletDataHandlerControls)) {
-			rootPortletIds.add(portlet.getRootPortletId());
-
-			portlets.add(portlet);
-		}
-	}
-
 	protected void exportImportLayouts(
 			long[] layoutIds, Map<String, String[]> parameterMap)
 		throws Exception {
@@ -516,45 +425,6 @@ public class LayoutExportImportTest extends BaseExportImportTestCase {
 		LayoutLocalServiceUtil.importLayouts(
 			TestPropsValues.getUserId(), importedGroup.getGroupId(), false,
 			parameterMap, larFile);
-	}
-
-	protected List<Portlet> getPortletDataHandlerPortlets(
-			long groupId, List<Layout> layouts)
-		throws Exception {
-
-		List<Portlet> portlets = new ArrayList<Portlet>();
-		Set<String> rootPortletIds = new HashSet<String>();
-
-		for (Layout layout : layouts) {
-			if (!layout.isTypePortlet()) {
-				continue;
-			}
-
-			LayoutTypePortlet layoutTypePortlet =
-				(LayoutTypePortlet)layout.getLayoutType();
-
-			for (String portletId : layoutTypePortlet.getPortletIds()) {
-				Portlet portlet = PortletLocalServiceUtil.getPortletById(
-					layout.getCompanyId(), portletId);
-
-				addPortlet(
-					portlets, rootPortletIds, portlet, layout.getGroupId(),
-					layout.getPlid(), layout.getPrivateLayout());
-			}
-		}
-
-		Group group = GroupLocalServiceUtil.getGroup(groupId);
-
-		List<Portlet> dataSiteLevelPortlets =
-			LayoutExporter.getDataSiteLevelPortlets(group.getCompanyId());
-
-		for (Portlet dataSiteLevelPortlet : dataSiteLevelPortlets) {
-			addPortlet(
-				portlets, rootPortletIds, dataSiteLevelPortlet, groupId, 0,
-				false);
-		}
-
-		return portlets;
 	}
 
 	protected void testAvailableLocales(
