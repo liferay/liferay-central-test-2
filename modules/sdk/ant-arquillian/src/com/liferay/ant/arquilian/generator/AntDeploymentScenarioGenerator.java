bb671d1a1b46afd868f8bc836c65f13978259d16
Carlos Sierra <carlos.sierra@liferay.com>
2014-06-30 14:44:23 +0200

    LPS-48114 Create a DeploymentScenarioGenerator
    This arquillian extension will eliminate the need to implement the
    method:
    
    ```
    @Deployment
    public static void getDeployment() {
    
    }
    ```
    
    when the artifacts are going to be built using SDK.

---
+++ modules/sdk/ant-arquillian/src/com/liferay/ant/arquilian/generator/AntDeploymentScenarioGenerator.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ant.arquilian.generator;
+
+import org.jboss.arquillian.container.spi.client.deployment.DeploymentDescription;
+import org.jboss.arquillian.container.test.impl.client.deployment.AnnotationDeploymentScenarioGenerator;
+import org.jboss.arquillian.test.spi.TestClass;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public class AntDeploymentScenarioGenerator
+	extends AnnotationDeploymentScenarioGenerator {
+
+	@Override
+	public List<DeploymentDescription> generate(TestClass testClass) {
+	
+	}
+
+}
