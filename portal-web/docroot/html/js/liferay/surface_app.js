4512af2d50f90ef2a58d5ac026c81c178bf26c2f
Marc Lundgren <marc.lundgren@liferay.com>
2014-06-30 12:46:08 -0700

    LPS-47486 -  Safari only - SPA will continuously reload the page

--- portal-web/docroot/html/js/liferay/surface_app.js
+++ portal-web/docroot/html/js/liferay/surface_app.js
@@ -3,131 +3,136 @@ AUI.add(
 	function(A) {
 		var Surface = Liferay.Surface;
 
-		Surface.app = new A.SurfaceApp(
-			{
-				basePath: Surface.getBasePath(),
-				linkSelector: 'a:not(.portlet-icon-back):not([data-navigation]):not([data-resource-href]):not([target="_blank"])',
-				on: {
-					endNavigate: function(event) {
-						var instance = this;
+		Liferay.once(
+			'domready',
+			function() {
+				Surface.app = new A.SurfaceApp(
+					{
+						basePath: Surface.getBasePath(),
+						linkSelector: 'a:not(.portlet-icon-back):not([data-navigation]):not([data-resource-href]):not([target="_blank"])',
+						on: {
+							endNavigate: function(event) {
+								var instance = this;
 
-						var error = event.error;
+								var error = event.error;
 
-						if (!error && Surface.isActionURL(event.path)) {
-							var redirect = Surface.getRedirect(event.path);
+								if (!error && Surface.isActionURL(event.path)) {
+									var redirect = Surface.getRedirect(event.path);
 
-							Surface.sendRedirect(redirect, instance.get('title'));
-						}
+									Surface.sendRedirect(redirect, instance.get('title'));
+								}
 
-						Liferay.fire(
-							'surfaceEndNavigate',
-							{
-								app: Surface.app,
-								error: error,
-								path: event.path
+								Liferay.fire(
+									'surfaceEndNavigate',
+									{
+										app: Surface.app,
+										error: error,
+										path: event.path
+									}
+								);
+
+								A.getBody().removeClass('lfr-surface-loading');
+							},
+
+							startNavigate: function(event) {
+								var instance = this;
+
+								if (Surface.isActionURL(event.path)) {
+									event.path = Surface.isolatePortletURLRedirect(event.path);
+								}
+
+								Liferay.fire(
+									'surfaceStartNavigate',
+									{
+										app: Surface.app,
+										path: event.path
+									}
+								);
+
+								A.getBody().addClass('lfr-surface-loading');
 							}
-						);
-
-						A.getBody().removeClass('lfr-surface-loading');
-					},
-
-					startNavigate: function(event) {
-						var instance = this;
-
-						if (Surface.isActionURL(event.path)) {
-							event.path = Surface.isolatePortletURLRedirect(event.path);
 						}
-
-						Liferay.fire(
-							'surfaceStartNavigate',
-							{
-								app: Surface.app,
-								path: event.path
-							}
-						);
-
-						A.getBody().addClass('lfr-surface-loading');
 					}
-				}
-			}
-		);
+				);
 
-		Surface.app.addScreenRoutes(
-			[
-				{
-					path: function(url) {
-						return url.search(Surface.getPatternPortletURL(Liferay.PortletURL.ACTION_PHASE)) > -1;
-					},
-					screen: Surface.ActionURLScreen
-				},
-				{
-					path: Surface.getPatternFriendlyURL(),
-					screen: Surface.RenderURLScreen
-				},
-				{
-					path: function(url) {
-						return url.search(Surface.getPatternPortletURL(Liferay.PortletURL.RENDER_PHASE)) > -1;
-					},
-					screen: Surface.RenderURLScreen
-				}
-			]
-		);
-
-		Surface.app.addSurfaces(Surface.getSurfaceIds());
-
-		Liferay.on(
-			'closePortlet',
-			function(event) {
-				var portletId = event.portletId;
-
-				var surfaceId = Surface.getPortletBoundaryId(portletId);
-
-				delete Surface.app.surfaces[surfaceId];
-			}
-		);
-
-		Liferay.on(
-			'portletReady',
-			function(event) {
-				var portletId = event.portletId;
-
-				var surfaceId = Surface.getPortletBoundaryId(portletId);
-
-				if (Surface.isAllowedPortletId(portletId) && !Surface.app.surfaces[surfaceId]) {
-					Surface.app.addSurfaces(surfaceId);
-				}
-			}
-		);
-
-		Liferay.on(
-			'surfaceScreenLoad',
-			function(event) {
-				Surface.resetAllPortlets();
-			}
-		);
-
-		Liferay.on(
-			'surfaceEndNavigate',
-			function(event) {
-				var activeScreen = Surface.app.activeScreen;
-
-				var dataChannel = activeScreen.get('dataChannel');
-
-				if (dataChannel.clearScreensCache) {
-					A.each(
-						Surface.app.screens,
-						function(value, key) {
-							if (value !== activeScreen) {
-								value.clearCache();
-							}
+				Surface.app.addScreenRoutes(
+					[
+						{
+							path: function(url) {
+								return url.search(Surface.getPatternPortletURL(Liferay.PortletURL.ACTION_PHASE)) > -1;
+							},
+							screen: Surface.ActionURLScreen
+						},
+						{
+							path: Surface.getPatternFriendlyURL(),
+							screen: Surface.RenderURLScreen
+						},
+						{
+							path: function(url) {
+								return url.search(Surface.getPatternPortletURL(Liferay.PortletURL.RENDER_PHASE)) > -1;
+							},
+							screen: Surface.RenderURLScreen
 						}
-					);
-				}
+					]
+				);
+
+				Surface.app.addSurfaces(Surface.getSurfaceIds());
+
+				Liferay.on(
+					'closePortlet',
+					function(event) {
+						var portletId = event.portletId;
+
+						var surfaceId = Surface.getPortletBoundaryId(portletId);
+
+						delete Surface.app.surfaces[surfaceId];
+					}
+				);
+
+				Liferay.on(
+					'portletReady',
+					function(event) {
+						var portletId = event.portletId;
+
+						var surfaceId = Surface.getPortletBoundaryId(portletId);
+
+						if (Surface.isAllowedPortletId(portletId) && !Surface.app.surfaces[surfaceId]) {
+							Surface.app.addSurfaces(surfaceId);
+						}
+					}
+				);
+
+				Liferay.on(
+					'surfaceScreenLoad',
+					function(event) {
+						Surface.resetAllPortlets();
+					}
+				);
+
+				Liferay.on(
+					'surfaceEndNavigate',
+					function(event) {
+						var activeScreen = Surface.app.activeScreen;
+
+						var dataChannel = activeScreen.get('dataChannel');
+
+						if (dataChannel.clearScreensCache) {
+							A.each(
+								Surface.app.screens,
+								function(value, key) {
+									if (value !== activeScreen) {
+										value.clearCache();
+									}
+								}
+							);
+						}
+					}
+				);
 			}
 		);
 	},
 	'',
 	{
-		requires: ['liferay-surface']
+		requires: ['event-base', 'liferay-surface']
 	}
 );
