f4105108b8938ece214499cbc772efd9b17074e6
André de Oliveira <andre.oliveira@liferay.com>
2014-07-02 17:39:44 -0300

    LPS-48247 Catch functional test Spring problems at integration test time
    https://issues.liferay.com/browse/LPS-48247

---
+++ portal-impl/test/integration/com/liferay/portal/util/InitUtilTest.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.util;
+
+import com.liferay.portal.kernel.util.PropsKeys;
+
+import org.junit.Test;
+
+/**
+ * @author André de Oliveira
+ */
+public class InitUtilTest {
+
+	@Test
+	public void testSeleniumBaseTestCaseSpringConfigs() {
+		String springConfigs = PropsUtil.get(PropsKeys.SPRING_CONFIGS);
+
+		try {
+			String seleniumBaseTestCaseSpringConfigs =
+				"META-INF/management-spring.xml,META-INF/util-spring.xml";
+
+			PropsUtil.set(
+				PropsKeys.SPRING_CONFIGS, seleniumBaseTestCaseSpringConfigs);
+
+			InitUtil.initWithSpringAndModuleFramework();
+		}
+		finally {
+			PropsUtil.set(PropsKeys.SPRING_CONFIGS, springConfigs);
+		}
+	}
+
+}
