95e15cbcdca4dc3f04ecd9bfcfddf9ec8a683684
Julio Camarero <julio.camarero@liferay.com>
2014-07-04 14:52:36 +0200

    LPS-47920 related assets shouldn't remember parameters in main view either

---
+++ portal-impl/src/com/liferay/portlet/relatedassets/action/ViewAction.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.relatedassets.action;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import javax.portlet.PortletConfig;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Brian Wing Shun Chan
+ * @author Raymond Augé
+ */
+public class ViewAction extends com.liferay.portlet.assetpublisher.action.ViewAction {
+
+	@Override
+	public ActionForward render(
+			ActionMapping actionMapping, ActionForm actionForm,
+			PortletConfig portletConfig, RenderRequest renderRequest,
+			RenderResponse renderResponse)
+		throws Exception {
+
+		renderResponse.setProperty(
+			"clear-request-parameters", Boolean.TRUE.toString());
+
+		return super.render(
+			actionMapping, actionForm, portletConfig, renderRequest,
+			renderResponse);
+	}
+
+}
