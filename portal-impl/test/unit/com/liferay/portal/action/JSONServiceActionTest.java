6dc4e473a2ad9adb8b5eafee8cdecd05f5a40e17
Brian Chan <brian.chan@liferay.com>
2014-07-09 15:30:45 -0700

    Revert "LPS-48085 Adding tests"
    This reverts commit 89f06aa7fe0424be1b7139dc8e34708eeebc0378.

--- portal-impl/test/unit/com/liferay/portal/action/JSONServiceActionTest.java
+++ portal-impl/test/unit/com/liferay/portal/action/JSONServiceActionTest.java
@@ -16,7 +16,6 @@ package com.liferay.portal.action;
 
 import com.liferay.portal.json.JSONFactoryImpl;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.service.GroupServiceUtil;
 import com.liferay.portlet.messageboards.service.MBMessageServiceUtil;
 
 import java.lang.reflect.Method;
@@ -80,39 +79,6 @@ public class JSONServiceActionTest extends TestCase {
 		assertEquals(
 			"{class=com.liferay.portal.kernel.dao.orm.EntityCacheUtil}",
 			value.toString());
-	}
-
-	@Test
-	public void testGetArgumentWithArrayValue() throws Exception {
-		JSONServiceAction jsonServiceAction = new JSONServiceAction();
-
-		String[] parameters = {
-			"roleId", "groupIds"
-		};
-
-		Object[] methodAndParameterTypes =
-			jsonServiceAction.getMethodAndParameterTypes(
-				GroupServiceUtil.class, "addRoleGroups", parameters,
-				new String[0]);
-
-		Method method = (Method)methodAndParameterTypes[0];
-		Type[] parameterTypes = (Type[])methodAndParameterTypes[1];
-
-		MockHttpServletRequest mockHttpServletRequest =
-			new MockHttpServletRequest();
-
-		mockHttpServletRequest.setParameter("roleId", "11111");
-		mockHttpServletRequest.setParameter("groupIds", "11111,22222,33333");
-
-		Object value = jsonServiceAction.getArgValue(
-			mockHttpServletRequest, GroupServiceUtil.class, method.getName(),
-			parameters[1], parameterTypes[1]);
-
-		assertTrue(value.getClass().isArray());
-
-		long[] arrayValue = (long[])value;
-
-		assertEquals(3, arrayValue.length);
 	}
 
 }
