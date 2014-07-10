4b96b7754d4e942545eb25cd4f8a35b114c40a39
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2014-07-10 11:44:51 +0200

    LPS-48130 Do the upgrade for Document Library to Documents and Media Admin and from Bookmarks to Bookmarks Admin

---
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeAdminPortlets.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.v7_0_0;
+
+import com.liferay.portal.kernel.upgrade.BaseUpgradeAdminPortlets;
+import com.liferay.portal.util.PortletKeys;
+
+/**
+ * @author Sergio González
+ */
+public class UpgradeAdminPortlets extends BaseUpgradeAdminPortlets {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		updateAccessInControlPanelPermission(
+			PortletKeys.DOCUMENT_LIBRARY, PortletKeys.DOCUMENT_LIBRARY_ADMIN);
+
+		updateAccessInControlPanelPermission(
+			PortletKeys.BOOKMARKS, PortletKeys.BOOKMARKS_ADMIN);
+	}
+
+}
