249cb1622684f3ac7b144ebf7848156e925fe929
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-07-14 12:00:45 +0200

    LPS-48487 Adds new methods to filter by mimeTypes

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLAppServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLAppServiceImpl.java
@@ -994,6 +994,17 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 			folderId, mimeTypes, QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
 	}
 
+	@Override
+	public List<FileEntry> getFileEntries(
+			long repositoryId, long folderId, String[] mimeTypes, int start,
+			int end)
+		throws PortalException {
+
+		Repository repository = getRepository(repositoryId);
+
+		return repository.getFileEntries(folderId, mimeTypes, start, end, null);
+	}
+
 	/**
 	 * Returns a range of all the file entries and shortcuts in the folder.
 	 *
@@ -1101,6 +1112,16 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 		Repository repository = getRepository(repositoryId);
 
 		return repository.getFileEntriesCount(folderId, fileEntryTypeId);
+	}
+
+	@Override
+	public int getFileEntriesCount(
+			long repositoryId, long folderId, String[] mimeTypes)
+		throws PortalException {
+
+		Repository repository = getRepository(repositoryId);
+
+		return repository.getFileEntriesCount(folderId, mimeTypes);
 	}
 
 	/**
