7b45f0d9480784b45e7a1f89b4f0f8010cee109e
willnewbury <william.newbury@liferay.com>
2014-07-14 16:02:57 -0700

    LPS-48529 Remove dead code

--- portal-service/src/com/liferay/portal/kernel/search/BaseBooleanQueryImpl.java
+++ portal-service/src/com/liferay/portal/kernel/search/BaseBooleanQueryImpl.java
@@ -20,10 +20,8 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 /**
  * @author Brian Wing Shun Chan
@@ -71,77 +69,6 @@ public abstract class BaseBooleanQueryImpl
 				addTerm(field, value);
 			}
 		}
-	}
-
-	protected String getTermFieldRemainderValues(
-		String field, String values, List<String> valuesList, String pattern,
-		String replacement) {
-
-		if (Validator.isNull(values)) {
-			return values;
-		}
-
-		if (Validator.isNull(pattern) || Validator.isNull(replacement)) {
-			return values;
-		}
-
-		if (Validator.isNotNull(field)) {
-			field += ":";
-		}
-		else {
-			field = StringPool.BLANK;
-		}
-
-		while (values.matches(pattern)) {
-			String value = values.replaceAll(pattern, replacement);
-
-			valuesList.add(value);
-
-			String duplicate =
-				"(?i)\\s*" + Pattern.quote(field + value) + "\\s*";
-
-			values = values.replaceAll(duplicate, StringPool.SPACE);
-			values = values.trim();
-		}
-
-		return values;
-	}
-
-	protected Map<String, List<String>> getTermFieldsValuesMap(
-		String[] fields, String values) {
-
-		Map<String, List<String>> termFieldsValuesMap =
-			new HashMap<String, List<String>>();
-
-		for (String field : fields) {
-			List<String> valuesList = new ArrayList<String>();
-
-			values = getTermFieldRemainderValues(
-				field, values, valuesList,
-				"(?i)^.*" + field + ":([\"\'])(.+?)(\\1).*$", "$1$2$3");
-
-			values = getTermFieldRemainderValues(
-				field, values, valuesList,
-				"(?i)^.*" + field + ":([^\\s\"']*).*$", "$1");
-
-			termFieldsValuesMap.put(field, valuesList);
-		}
-
-		values = values.trim();
-
-		List<String> valuesList = new ArrayList<String>();
-
-		if (Validator.isNotNull(values)) {
-			values = getTermFieldRemainderValues(
-				null, values, valuesList,
-				"^[^\"\']*([\"\'])(.+?)(\\1)[^\"\']*$", "$1$2$3");
-
-			valuesList.add(values);
-		}
-
-		termFieldsValuesMap.put("no_field", valuesList);
-
-		return termFieldsValuesMap;
 	}
 
 	protected String[] parseKeywords(String values) {
