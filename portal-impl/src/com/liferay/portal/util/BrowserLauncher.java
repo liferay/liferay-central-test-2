e17186566ead97953bf3326b06743cf92add9f1d
Brian Chan <brian.chan@liferay.com>
2014-07-14 15:46:54 -0700

    LPS-48525 Sort

--- portal-impl/src/com/liferay/portal/util/BrowserLauncher.java
+++ portal-impl/src/com/liferay/portal/util/BrowserLauncher.java
@@ -107,7 +107,7 @@ public class BrowserLauncher implements Runnable {
 	}
 
 	private static final String[] _BROWSERS = {
-		"xdg-open", "firefox", "mozilla", "konqueror", "opera"
+		"firefox", "mozilla", "konqueror", "opera", "xdg-open"
 	};
 
 }
