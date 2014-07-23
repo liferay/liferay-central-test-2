157d7a20e1d8c5a6cafb6f54f79a2835e110bb42
Marcellus Tavares <marcellus.tavares@liferay.com>
2014-07-23 18:25:06 -0300

    LPS-48786 SearchContainerColumnStatus does not work for plugins

--- util-taglib/src/com/liferay/taglib/ui/SearchContainerColumnStatusTag.java
+++ util-taglib/src/com/liferay/taglib/ui/SearchContainerColumnStatusTag.java
@@ -17,8 +17,10 @@ package com.liferay.taglib.ui;
 import com.liferay.portal.kernel.bean.BeanPropertiesUtil;
 import com.liferay.portal.kernel.dao.search.ResultRow;
 import com.liferay.portal.kernel.dao.search.SearchEntry;
+import com.liferay.portal.kernel.servlet.ServletContextPool;
 import com.liferay.portal.kernel.util.ServerDetector;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.util.PortalUtil;
 import com.liferay.taglib.search.StatusSearchEntry;
 
 import java.util.Date;
@@ -27,6 +29,7 @@ import java.util.Map;
 
 import javax.portlet.PortletURL;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.JspException;
@@ -81,8 +84,11 @@ public class SearchContainerColumnStatusTag<R>
 				(HttpServletRequest)pageContext.getRequest());
 			statusSearchEntry.setResponse(
 				(HttpServletResponse)pageContext.getResponse());
-			statusSearchEntry.setServletContext(
-				pageContext.getServletContext());
+
+			ServletContext servletContext = ServletContextPool.get(
+				PortalUtil.getServletContextName());
+
+			statusSearchEntry.setServletContext(servletContext);
 			statusSearchEntry.setStatus(_status);
 			statusSearchEntry.setStatusByUserId(_statusByUserId);
 			statusSearchEntry.setStatusDate(_statusDate);
