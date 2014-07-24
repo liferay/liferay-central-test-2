43323abb3dad4f8f10a846f1f77d50be3c874429
adolfo.perez <adolfo.perez@liferay.com>
2014-07-24 22:02:10 +0200

    LPS-48728 add skeleton plugin for external repositories

---
+++ portal-impl/src/com/liferay/portal/repository/external/ExternalRepositoryCreator.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.repository.external;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.repository.LocalRepository;
+import com.liferay.portal.kernel.repository.Repository;
+import com.liferay.portal.kernel.repository.registry.RepositoryCreator;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class ExternalRepositoryCreator implements RepositoryCreator {
+
+	@Override
+	public LocalRepository createLocalRepository(long repositoryId)
+		throws PortalException {
+
+		throw new UnsupportedOperationException("not implemented");
+	}
+
+	@Override
+	public Repository createRepository(long repositoryId)
+		throws PortalException {
+
+		throw new UnsupportedOperationException("not implemented");
+	}
+
+}
