6525ae6ab92c240adbdf30f798482935d1609ab7
Brian Chan <brian.chan@liferay.com>
2014-07-25 15:18:56 -0700

    LPS-48617 SF

--- portal-impl/src/com/liferay/portlet/journalcontentsearch/util/ContentHits.java
+++ portal-impl/src/com/liferay/portlet/journalcontentsearch/util/ContentHits.java
@@ -43,7 +43,9 @@ public class ContentHits {
 		List<Document> docs = new ArrayList<Document>();
 		List<Float> scores = new ArrayList<Float>();
 
-		for (int i = 0; i < hits.getDocs().length; i++) {
+		Document[] docsArray = hits.getDocs();
+
+		for (int i = 0; i < docsArray.length; i++) {
 			Document doc = hits.doc(i);
 
 			String articleId = doc.get(Field.ARTICLE_ID);
