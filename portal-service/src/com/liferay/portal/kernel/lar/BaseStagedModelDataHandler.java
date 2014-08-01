cb021c1f25fb8906b9722eda84bd9d7d3aa69032
Máté Thurzó <mate.thurzo@liferay.com>
2014-08-01 13:02:55 +0200

    LPS-49013 SF

--- portal-service/src/com/liferay/portal/kernel/lar/BaseStagedModelDataHandler.java
+++ portal-service/src/com/liferay/portal/kernel/lar/BaseStagedModelDataHandler.java
@@ -18,7 +18,6 @@ import com.liferay.portal.NoSuchModelException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.trash.TrashHandler;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.MapUtil;
