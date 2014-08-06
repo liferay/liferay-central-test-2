2c08c0e331a6e9868433f7d9d01df37983106084
Máté Thurzó <mate.thurzo@liferay.com>
2014-08-06 18:19:09 +0200

    LPS-46754 Adding base converter to provide a helper for marshalling based on the implementing class' preference for fields

---
+++ portal-service/src/com/liferay/portal/kernel/lar/xstream/BaseXStreamConverter.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.lar.xstream;
+
+import com.liferay.portal.kernel.bean.BeanPropertiesUtil;
+
+import java.util.List;
+
+/**
+ * @author Akos Thurzo
+ */
+public abstract class BaseXStreamConverter implements XStreamConverter {
+
+	@Override
+	public abstract boolean canConvert(Class<?> clazz);
+
+	@Override
+	public void marshal(
+			Object object, XStreamHierarchicalStreamWriter writer,
+			XStreamMarshallingContext marshallingContext)
+		throws Exception {
+
+		for (String field : getFields()) {
+			writer.startNode(field);
+
+			Object value = BeanPropertiesUtil.getObject(object, field);
+
+			if (value != null) {
+				marshallingContext.convertAnother(value);
+			}
+
+			writer.endNode();
+		}
+	}
+
+	@Override
+	public abstract Object unmarshal(
+			XStreamHierarchicalStreamReader reader,
+			XStreamUnmarshallingContext unmarshallingContext)
+		throws Exception;
+
+	protected abstract List<String> getFields();
+
+}
