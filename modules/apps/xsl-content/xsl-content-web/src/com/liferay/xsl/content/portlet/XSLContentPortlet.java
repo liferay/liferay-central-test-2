5a6c936d4b2eeab6d193990634ffc5a9e8549518
Raymond Auge <raymond.auge@liferay.com>
2014-08-06 17:27:10 -0700

    LPS-49175 convert to an MVCPortlet component portlet

---
+++ modules/apps/xsl-content/xsl-content-web/src/com/liferay/xsl/content/portlet/XSLContentPortlet.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.xsl.content.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.css-class-wrapper=portlet-xsl-content",
+		"com.liferay.portlet.instanceable=true",
+		"com.liferay.portlet.layout-cacheable=true",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.struts-path=xsl_content",
+		"javax.portlet.name=102",
+		"javax.portlet.display-name=XSL Content",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.config-template=/configuration.jsp",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.resource-bundle=com.liferay.portlet.StrutsResourceBundle",
+		"javax.portlet.security-role-ref=guest",
+		"javax.portlet.security-role-ref=power-user",
+		"javax.portlet.security-role-ref=user",
+		"javax.portlet.supported-public-render-parameter=tags"
+	},
+	service = Portlet.class
+)
+public class XSLContentPortlet extends MVCPortlet {
+}
