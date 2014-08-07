e56646f2d82aabb218cd252937506a5a999f44e6
Brian Chan <brian.chan@liferay.com>
2014-08-07 10:38:09 -0700

    LPS-48604 remove

--- modules/apps/analytics/analytics-impl/sql/indexes.sql
+++
@@ -1,5 +0,0 @@
-create index IX_C02F5433 on Analytics_AnalyticsEvent (companyId, createDate);
-create index IX_237E2264 on Analytics_AnalyticsEvent (createDate, className, classPK, referrerClassName, referrerClassPK, type_);
-create index IX_C4F5EF74 on Analytics_AnalyticsEvent (createDate, className, classPK, type_);
-create index IX_B8A22F4D on Analytics_AnalyticsEvent (createDate, elementKey, type_);
-create index IX_8FFC623D on Analytics_AnalyticsEvent (createDate, referrerClassName, referrerClassPK, elementKey, type_);
