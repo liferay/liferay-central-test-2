138488cd0a8f264e25e3924d5ac224222b761ba1
Brian Chan <brian.chan@liferay.com>
2014-08-10 15:22:49 -0700

    LPS-49175 package

--- modules/apps/xsl-content/xsl-content-web/src/com/liferay/xsl/content/portlet/XSLContentPortlet.java
+++ modules/apps/xsl-content/xsl-content-web/src/com/liferay/xsl/content/portlet/XSLContentPortlet.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.xsl.content.portlet;
 
 import com.liferay.util.bridges.mvc.MVCPortlet;
