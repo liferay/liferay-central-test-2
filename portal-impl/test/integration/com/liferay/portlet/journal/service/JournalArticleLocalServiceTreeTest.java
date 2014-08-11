296df45446f670a16066eaba2625280567a31f49
Dale Shan <dale.shan@liferay.com>
2014-08-11 10:33:07 +0800

    LPS-48189 Tests for Journal

--- portal-impl/test/integration/com/liferay/portlet/journal/service/JournalArticleLocalServiceTreeTest.java
+++ portal-impl/test/integration/com/liferay/portlet/journal/service/JournalArticleLocalServiceTreeTest.java
@@ -15,18 +15,24 @@
 package com.liferay.portlet.journal.service;
 
 import com.liferay.portal.kernel.test.ExecutionTestListeners;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.test.DeleteAfterTestRun;
 import com.liferay.portal.test.listeners.MainServletExecutionTestListener;
 import com.liferay.portal.test.runners.LiferayIntegrationJUnitTestRunner;
+import com.liferay.portal.util.test.GroupTestUtil;
 import com.liferay.portal.util.test.RandomTestUtil;
+import com.liferay.portal.util.test.ServiceContextTestUtil;
 import com.liferay.portal.util.test.TestPropsValues;
 import com.liferay.portlet.journal.model.JournalArticle;
 import com.liferay.portlet.journal.model.JournalFolder;
+import com.liferay.portlet.journal.model.JournalFolderConstants;
 import com.liferay.portlet.journal.util.test.JournalTestUtil;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -39,11 +45,80 @@ import org.testng.Assert;
 @RunWith(LiferayIntegrationJUnitTestRunner.class)
 public class JournalArticleLocalServiceTreeTest {
 
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test
+	public void testJournalArticleTreePathWhenMovingSubfolderWithArticle()
+		throws Exception {
+
+		JournalFolder folderA = JournalTestUtil.addFolder(
+			_group.getGroupId(),
+			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID, "Folder A");
+
+		JournalFolder folderAA = JournalTestUtil.addFolder(
+			_group.getGroupId(), folderA.getFolderId(), "Folder AA");
+
+		JournalArticle article = JournalTestUtil.addArticle(
+			_group.getGroupId(), folderAA.getFolderId());
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId());
+
+		JournalFolderLocalServiceUtil.moveFolder(
+			folderAA.getFolderId(),
+			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID, serviceContext);
+
+		article = JournalArticleLocalServiceUtil.getArticle(
+			_group.getGroupId(), article.getArticleId());
+
+		Assert.assertEquals(article.buildTreePath(), article.getTreePath());
+	}
+
+	@Test
+	public void testJournalFolderTreePathWhenMovingFolderWithSubfolder()
+		throws Exception {
+
+		List<JournalFolder> folders = new ArrayList<JournalFolder>();
+
+		JournalFolder folderA = JournalTestUtil.addFolder(
+			_group.getGroupId(),
+			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID, "Folder A");
+
+		folders.add(folderA);
+
+		JournalFolder folderAA = JournalTestUtil.addFolder(
+			_group.getGroupId(), folderA.getFolderId(), "Folder AA");
+
+		folders.add(folderAA);
+
+		JournalFolder folderAAA = JournalTestUtil.addFolder(
+			_group.getGroupId(), folderAA.getFolderId(), "Folder AAA");
+
+		folders.add(folderAAA);
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId());
+
+		JournalFolderLocalServiceUtil.moveFolder(
+			folderAA.getFolderId(),
+			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID, serviceContext);
+
+		for (JournalFolder curFolder : folders) {
+			JournalFolder folder = JournalFolderLocalServiceUtil.getFolder(
+				curFolder.getFolderId());
+
+			Assert.assertEquals(folder.buildTreePath(), folder.getTreePath());
+		}
+	}
+
 	@Test
 	public void testRebuildTree() throws Exception {
-		createTree();
+		List<JournalArticle> articles = createTree();
 
-		for (JournalArticle article : _articles) {
+		for (JournalArticle article : articles) {
 			article.setTreePath(null);
 
 			JournalArticleLocalServiceUtil.updateJournalArticle(article);
@@ -52,7 +127,7 @@ public class JournalArticleLocalServiceTreeTest {
 		JournalArticleLocalServiceUtil.rebuildTree(
 			TestPropsValues.getCompanyId());
 
-		for (JournalArticle article : _articles) {
+		for (JournalArticle article : articles) {
 			article = JournalArticleLocalServiceUtil.getArticle(
 				article.getId());
 
@@ -60,27 +135,27 @@ public class JournalArticleLocalServiceTreeTest {
 		}
 	}
 
-	protected void createTree() throws Exception {
+	protected List<JournalArticle> createTree() throws Exception {
+		List<JournalArticle> articles = new ArrayList<JournalArticle>();
+
 		JournalArticle articleA = JournalTestUtil.addArticle(
-			TestPropsValues.getGroupId(), "Article A",
-			RandomTestUtil.randomString());
+			_group.getGroupId(), "Article A", RandomTestUtil.randomString());
 
-		_articles.add(articleA);
+		articles.add(articleA);
 
-		_folder = JournalTestUtil.addFolder(
-			TestPropsValues.getGroupId(), "Folder A");
+		JournalFolder folder = JournalTestUtil.addFolder(
+			_group.getGroupId(), "Folder A");
 
 		JournalArticle articleAA = JournalTestUtil.addArticle(
-			TestPropsValues.getGroupId(), _folder.getFolderId(), "Article AA",
+			_group.getGroupId(), folder.getFolderId(), "Article AA",
 			RandomTestUtil.randomString());
 
-		_articles.add(articleAA);
+		articles.add(articleAA);
+
+		return articles;
 	}
 
 	@DeleteAfterTestRun
-	private List<JournalArticle> _articles = new ArrayList<JournalArticle>();
-
-	@DeleteAfterTestRun
-	private JournalFolder _folder;
+	private Group _group;
 
 }
