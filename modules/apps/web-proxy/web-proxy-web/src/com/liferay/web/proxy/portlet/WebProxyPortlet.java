f07bb140bc2d9c02ba7af27d3ee4f719263dce45
Raymond Auge <raymond.auge@liferay.com>
2014-08-11 13:21:16 -0700

    LPS-49260 migrate portletbridge servlet

--- modules/apps/web-proxy/web-proxy-web/src/com/liferay/web/proxy/portlet/WebProxyPortlet.java
+++ modules/apps/web-proxy/web-proxy-web/src/com/liferay/web/proxy/portlet/WebProxyPortlet.java
@@ -23,13 +23,16 @@ import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.struts.StrutsUtil;
-import com.liferay.portal.util.PortalUtil;
 import com.liferay.portlet.RenderResponseImpl;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 import javax.portlet.Portlet;
+import javax.portlet.PortletConfig;
 import javax.portlet.PortletContext;
 import javax.portlet.PortletException;
 import javax.portlet.PortletPreferences;
@@ -37,9 +40,18 @@ import javax.portlet.PortletRequestDispatcher;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
+import javax.servlet.Servlet;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
 
 import org.portletbridge.portlet.PortletBridgePortlet;
+import org.portletbridge.portlet.PortletBridgeServlet;
 
 /**
  * @author Brian Wing Shun Chan
@@ -79,6 +91,13 @@ import org.portletbridge.portlet.PortletBridgePortlet;
 public class WebProxyPortlet extends PortletBridgePortlet {
 
 	@Override
+	public void destroy() {
+		_serviceRegistration.unregister();
+
+		super.destroy();
+	}
+
+	@Override
 	public void doView(
 			RenderRequest renderRequest, RenderResponse renderResponse)
 		throws IOException, PortletException {
@@ -115,7 +134,7 @@ public class WebProxyPortlet extends PortletBridgePortlet {
 			String output = bufferCacheServletResponse.getString();
 
 			output = StringUtil.replace(
-				output, "//pbhs/", PortalUtil.getPathContext() + "/pbhs/");
+				output, "//pbhs/", renderRequest.getContextPath() + "/pbhs/");
 
 			bufferCacheServletResponse.setString(output);
 		}
@@ -125,6 +144,8 @@ public class WebProxyPortlet extends PortletBridgePortlet {
 	public void init() {
 		try {
 			super.init();
+
+			doInit();
 
 			_enabled = true;
 		}
@@ -140,6 +161,51 @@ public class WebProxyPortlet extends PortletBridgePortlet {
 					"serializer.jar and xalan.jar files are copied to the " +
 						"JDK's endorsed directory");
 		}
+	}
+
+	@Activate
+	protected void activate(ComponentContext componentContext) {
+		_componentContext = componentContext;
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_componentContext = null;
+	}
+
+	protected void doInit() {
+		PortletConfig portletConfig = getPortletConfig();
+		PortletContext portletContext = getPortletContext();
+
+		Dictionary<String, Object> properties = new Hashtable<String, Object>();
+
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_SELECT,
+			portletContext.getPortletContextName());
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_NAME,
+			"Portlet Bridge Servlet");
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_PATTERN, "/pbhs/*");
+		properties.put(
+			"servlet.init.mementoSessionKey",
+			portletConfig.getInitParameter("mementoSessionKey"));
+		properties.put(
+			"servlet.init.cssRegex",
+			portletConfig.getInitParameter("cssRegex"));
+		properties.put(
+			"servlet.init.jsRegex", portletConfig.getInitParameter("jsRegex"));
+		properties.put(
+			"servlet.init.ignoreRequestHeaders",
+			"accept-encoding,connection,keep-alive");
+		properties.put(
+			"servlet.init.ignorePostToGetRequestHeaders",
+			"content-type,content-length");
+
+		BundleContext bundleContext = _componentContext.getBundleContext();
+
+		_serviceRegistration = bundleContext.registerService(
+			Servlet.class, new PortletBridgeServlet(), properties);
 	}
 
 	protected void printError(RenderResponse renderResponse)
@@ -159,6 +225,8 @@ public class WebProxyPortlet extends PortletBridgePortlet {
 
 	private static Log _log = LogFactoryUtil.getLog(WebProxyPortlet.class);
 
+	private ComponentContext _componentContext;
 	private boolean _enabled;
+	private ServiceRegistration<?> _serviceRegistration;
 
 }
