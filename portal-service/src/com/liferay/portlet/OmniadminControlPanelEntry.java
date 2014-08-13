c98cec3b8a877937b34ed94ecad71a4e66944d08
Jim Hinkey <james.hinkey@liferay.com>
2014-08-13 17:01:34 -0400

    LRDOCS-981 Edit OmniadminControlPanelEntry Javadoc

--- portal-service/src/com/liferay/portlet/OmniadminControlPanelEntry.java
+++ portal-service/src/com/liferay/portlet/OmniadminControlPanelEntry.java
@@ -19,21 +19,27 @@ import com.liferay.portal.model.Portlet;
 import com.liferay.portal.security.permission.PermissionChecker;
 
 /**
- * Represents the Omni Administrator control panel entry, which is used as a
- * {@link ControlPanelEntry} for portlets that is only visible/accessible to the
- * Omni Administrator and declared in <code>liferay-portlet.xml</code>.
+ * Represents the omni administrator control panel entry for portlets that shall only
+ * be visible and accessible to the omni administrator. In a portlet's
+ * <code>liferay-portlet.xml</code> file, this class can be specified as the
+ * value for the
+ * <a href="http://docs.liferay.com/portal/6.2/definitions/liferay-portlet-app_6_2_0.dtd.html#control-panel-entry-class" >
+ * control-panel-entry-class
+ * </a>
+ * element.
  *
  * @author Jorge Ferrer
  */
 public class OmniadminControlPanelEntry extends BaseControlPanelEntry {
 
 	/**
-	 * Returns <code>true</code> if the current user is an Omni Administrator.
+	 * Returns <code>true</code> if the current user is an omni administrator.
 	 *
-	 * @param  permissionChecker the permission checker referencing a user
+	 * @param  permissionChecker the permission checker referencing the current
+	 *         user
 	 * @param  group the group
 	 * @param  portlet the portlet being checked
-	 * @return <code>true</code> if the current user is an Omni Administrator;
+	 * @return <code>true</code> if the current user is an omni administrator;
 	 *         <code>false</code> otherwise
 	 * @throws Exception if an exception occurred
 	 */
