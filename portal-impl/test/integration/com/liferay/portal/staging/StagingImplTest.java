1e02e530944e5eec619dfc5ddfcf6bd2500f1bb7
Daniel Kocsis <daniel.kocsis@liferay.com>
2014-08-13 11:32:43 +0200

    LPS-48745 Add test case

--- portal-impl/test/integration/com/liferay/portal/staging/StagingImplTest.java
+++ portal-impl/test/integration/com/liferay/portal/staging/StagingImplTest.java
@@ -17,11 +17,17 @@ package com.liferay.portal.staging;
 import com.liferay.portal.kernel.lar.PortletDataHandlerKeys;
 import com.liferay.portal.kernel.staging.StagingUtil;
 import com.liferay.portal.kernel.test.ExecutionTestListeners;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.model.Group;
+import com.liferay.portal.model.LayoutSetBranch;
+import com.liferay.portal.model.LayoutSetBranchConstants;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
+import com.liferay.portal.service.LayoutSetBranchLocalServiceUtil;
 import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceContextThreadLocal;
 import com.liferay.portal.service.StagingLocalServiceUtil;
 import com.liferay.portal.test.DeleteAfterTestRun;
 import com.liferay.portal.test.Sync;
@@ -52,6 +58,7 @@ import org.junit.runner.RunWith;
 
 /**
  * @author Julio Camarero
+ * @author Daniel Kocsis
  */
 @ExecutionTestListeners(listeners = {
 	MainServletExecutionTestListener.class,
@@ -67,13 +74,35 @@ public class StagingImplTest {
 	}
 
 	@Test
+	public void testLocalStaging() throws Exception {
+		enableLocalStaging(false);
+	}
+
+	@Test
 	public void testLocalStagingCategories() throws Exception {
-		enableLocalStaging(false, true);
+		enableLocalStagingWithContent(false, true, false);
 	}
 
 	@Test
 	public void testLocalStagingJournal() throws Exception {
-		enableLocalStaging(true, false);
+		enableLocalStagingWithContent(true, false, false);
+	}
+
+	@Test
+	public void testLocalStagingWithPageVersioning() throws Exception {
+		enableLocalStaging(true);
+	}
+
+	@Test
+	public void testLocalStagingWithPageVersioningCategories()
+		throws Exception {
+
+		enableLocalStagingWithContent(false, true, true);
+	}
+
+	@Test
+	public void testLocalStagingWithPageVersioningJournal() throws Exception {
+		enableLocalStagingWithContent(true, false, true);
 	}
 
 	protected AssetCategory addAssetCategory(
@@ -102,15 +131,83 @@ public class StagingImplTest {
 			assetVocabulary.getVocabularyId(), new String[0], serviceContext);
 	}
 
+	protected void enableLocalStaging(boolean branching) throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId());
+
+		Map<String, String[]> parameters = StagingUtil.getStagingParameters();
+
+		for (String parameterName : parameters.keySet()) {
+			serviceContext.setAttribute(
+				parameterName, parameters.get(parameterName)[0]);
+		}
+
+		if (branching) {
+			serviceContext.setSignedIn(true);
+		}
+
+		ServiceContextThreadLocal.pushServiceContext(serviceContext);
+
+		enableLocalStaging(branching, serviceContext);
+
+		ServiceContextThreadLocal.popServiceContext();
+
+		if (!branching) {
+			return;
+		}
+
+		UnicodeProperties typeSettingsProperties =
+			_group.getTypeSettingsProperties();
+
+		Assert.assertTrue(
+			GetterUtil.getBoolean(
+				typeSettingsProperties.getProperty("branchingPrivate")));
+		Assert.assertTrue(
+			GetterUtil.getBoolean(
+				typeSettingsProperties.getProperty("branchingPublic")));
+
+		Group stagingGroup = _group.getStagingGroup();
+
+		LayoutSetBranch layoutSetBranch =
+			LayoutSetBranchLocalServiceUtil.fetchLayoutSetBranch(
+				stagingGroup.getGroupId(), false,
+				LayoutSetBranchConstants.MASTER_BRANCH_NAME);
+
+		Assert.assertNotNull(layoutSetBranch);
+
+		layoutSetBranch = LayoutSetBranchLocalServiceUtil.fetchLayoutSetBranch(
+			stagingGroup.getGroupId(), true,
+			LayoutSetBranchConstants.MASTER_BRANCH_NAME);
+
+		Assert.assertNotNull(layoutSetBranch);
+	}
+
 	protected void enableLocalStaging(
-			boolean stageJournal, boolean stageCategories)
+			boolean branching, ServiceContext serviceContext)
+		throws Exception {
+
+		int initialPagesCount = LayoutLocalServiceUtil.getLayoutsCount(
+			_group, false);
+
+		StagingLocalServiceUtil.enableLocalStaging(
+			TestPropsValues.getUserId(), _group, branching, branching,
+			serviceContext);
+
+		Group stagingGroup = _group.getStagingGroup();
+
+		Assert.assertNotNull(stagingGroup);
+
+		Assert.assertEquals(
+			initialPagesCount,
+			LayoutLocalServiceUtil.getLayoutsCount(stagingGroup, false));
+	}
+
+	protected void enableLocalStagingWithContent(
+			boolean stageJournal, boolean stageCategories, boolean branching)
 		throws Exception {
 
 		LayoutTestUtil.addLayout(_group.getGroupId(), "Page1");
 		LayoutTestUtil.addLayout(_group.getGroupId(), "Page2");
-
-		int initialPagesCount = LayoutLocalServiceUtil.getLayoutsCount(
-			_group, false);
 
 		// Create content
 
@@ -155,18 +252,9 @@ public class StagingImplTest {
 				parameterName, parameters.get(parameterName)[0]);
 		}
 
-		// Enable staging
-
-		StagingLocalServiceUtil.enableLocalStaging(
-			TestPropsValues.getUserId(), _group, false, false, serviceContext);
+		enableLocalStaging(branching, serviceContext);
 
 		Group stagingGroup = _group.getStagingGroup();
-
-		Assert.assertNotNull(stagingGroup);
-
-		Assert.assertEquals(
-			initialPagesCount,
-			LayoutLocalServiceUtil.getLayoutsCount(stagingGroup, false));
 
 		// Update content in staging
 
