1f2dc7baf88c27a22e2d35773fc5244390e05062
Michael Hashimoto <michael.hashimoto@liferay.com>
2014-08-18 14:18:32 -0700

    LPS-48556 - Add Tests for DIFFERENCE, PRODUCT, and QUOTIENT functions in 'MathUtilTest'

--- portal-service/test/unit/com/liferay/portal/kernel/util/MathUtilTest.java
+++ portal-service/test/unit/com/liferay/portal/kernel/util/MathUtilTest.java
@@ -23,6 +23,42 @@ import org.junit.Test;
 public class MathUtilTest {
 
 	@Test
+	public void testDifferenceWithIntegerValues() {
+		int expected = 8 - 2;
+
+		Assert.assertEquals(expected, MathUtil.difference(8, 2));
+	}
+
+	@Test
+	public void testProductWithDoubleValues() {
+		double expected = 1.1 * 2.2 * 3.3 * 5.5 * 8.8;
+
+		Assert.assertEquals(
+			expected, MathUtil.product(1.1, 2.2, 3.3, 5.5, 8.8), 0.01);
+	}
+
+	@Test
+	public void testProductWithIntegerValues() {
+		int expected = 1 * 2 * 3 * 5 * 8;
+
+		Assert.assertEquals(expected, MathUtil.product(1, 2, 3, 5, 8));
+	}
+
+	@Test
+	public void testProductWithLongValues() {
+		long expected = 1 * 2 * 3 * 5 * 8;
+
+		Assert.assertEquals(expected, MathUtil.product(1L, 2L, 3L, 5L, 8L));
+	}
+
+	@Test
+	public void testQuotientWithIntegerValues() {
+		int expected = 8 / 2;
+
+		Assert.assertEquals(expected, MathUtil.quotient(8, 2));
+	}
+
+	@Test
 	public void testSumWithDoubleValues() {
 		double expected = 1.1 + 2.2 + 3.3 + 5.5 + 8.8;
 
