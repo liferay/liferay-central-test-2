95cd7069a075a77696d769920f2727eb0877205b
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2014-08-18 14:01:12 -0700

    LPS-48454 - Deprecating instead of removing

--- portal-web/docroot/html/js/liferay/deprecated.js
+++ portal-web/docroot/html/js/liferay/deprecated.js
@@ -2,6 +2,41 @@
 
 Liferay.namespace('Util');
 
+Liferay.Util.actsAsAspect = function(object) {
+	object.yield = null;
+	object.rv = {};
+
+	object.before = function(method, f) {
+		var original = eval('this.' + method);
+
+		this[method] = function() {
+			f.apply(this, arguments);
+
+			return original.apply(this, arguments);
+		};
+	};
+
+	object.after = function(method, f) {
+		var original = eval('this.' + method);
+
+		this[method] = function() {
+			this.rv[method] = original.apply(this, arguments);
+
+			return f.apply(this, arguments);
+		};
+	};
+
+	object.around = function(method, f) {
+		var original = eval('this.' + method);
+
+		this[method] = function() {
+			this.yield = original;
+
+			return f.apply(this, arguments);
+		};
+	};
+};
+
 Liferay.Util.clamp = function(value, min, max) {
 	return Math.min(Math.max(value, min), max);
-}
+};
