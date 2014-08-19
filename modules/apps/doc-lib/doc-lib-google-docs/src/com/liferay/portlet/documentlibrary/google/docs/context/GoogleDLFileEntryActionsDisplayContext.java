060df6e5eeda9b4cf6ea29c017c1dbb5e1b3f862
Iván Zaera Avellón <ivan.zaera@liferay.com>
2014-08-19 09:30:05 +0200

    LPS-49147 Add an empty implementation of the DLFileEntryActionsDisplayContext for Google Documents. I will add more methods in future commits until the whole Google Thing (TM) is supported.

---
+++ modules/apps/doc-lib/doc-lib-google-docs/src/com/liferay/portlet/documentlibrary/google/docs/context/GoogleDLFileEntryActionsDisplayContext.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.documentlibrary.google.docs.context;
+
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.FileVersion;
+import com.liferay.portlet.documentlibrary.context.BaseDLFileEntryActionsDisplayContext;
+import com.liferay.portlet.documentlibrary.context.DLFileEntryActionsDisplayContext;
+
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Iván Zaera
+ */
+public class GoogleDLFileEntryActionsDisplayContext
+	extends BaseDLFileEntryActionsDisplayContext {
+
+	public GoogleDLFileEntryActionsDisplayContext(
+		DLFileEntryActionsDisplayContext parentDLDisplayContext,
+		HttpServletRequest request, HttpServletResponse response,
+		FileEntry fileEntry, FileVersion fileVersion) {
+
+		super(
+			_UUID, parentDLDisplayContext, request, response, fileEntry,
+			fileVersion);
+	}
+
+	private static final UUID _UUID = UUID.fromString(
+		"7B61EA79-83AE-4FFD-A77A-1D47E06EBBE9");
+
+}
