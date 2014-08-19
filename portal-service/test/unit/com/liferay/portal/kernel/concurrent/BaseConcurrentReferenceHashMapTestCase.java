488faee2b55f96db8d3e614f3b573d715213f197
shuyangzhou <shuyang.zhou@liferay.com>
2014-08-19 15:20:05 -0700

    LPS-49479 Unit test

---
+++ portal-service/test/unit/com/liferay/portal/kernel/concurrent/BaseConcurrentReferenceHashMapTestCase.java
@@ -0,0 +1,105 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.concurrent;
+
+import com.liferay.portal.kernel.memory.FinalizeAction;
+import com.liferay.portal.kernel.memory.FinalizeManager;
+import com.liferay.portal.kernel.test.GCUtil;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+
+import java.lang.ref.Reference;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentMap;
+
+import org.junit.Assert;
+
+/**
+ * @author Shuyang Zhou
+ */
+public class BaseConcurrentReferenceHashMapTestCase {
+
+	protected Map<String, Object> createDataMap() {
+		Map<String, Object> map = new HashMap<String, Object>();
+
+		map.put("testKey", new Object());
+		map.put("testKey2", new Object());
+
+		return map;
+	}
+
+	protected void doTestAutoRemove(
+			ConcurrentMap<String, Object> concurrentMap, boolean fullGC)
+		throws Exception {
+
+		Map<Reference<?>, FinalizeAction> finalizeActions =
+			(Map<Reference<?>, FinalizeAction>)ReflectionTestUtil.getFieldValue(
+				FinalizeManager.class, "_finalizeActions");
+
+		String testKey = new String("testKey");
+		Object testValue = new Object();
+		Object testValue2 = new Object();
+
+		Assert.assertFalse(concurrentMap.containsKey(testKey));
+		Assert.assertTrue(finalizeActions.isEmpty());
+		Assert.assertNull(concurrentMap.put(testKey, testValue));
+		Assert.assertTrue(concurrentMap.containsKey(testKey));
+		Assert.assertSame(testValue, concurrentMap.get(testKey));
+		Assert.assertTrue(concurrentMap.containsValue(testValue));
+		Assert.assertEquals(1, finalizeActions.size());
+		Assert.assertSame(testValue, concurrentMap.put(testKey, testValue2));
+		Assert.assertTrue(concurrentMap.containsKey(testKey));
+		Assert.assertSame(testValue2, concurrentMap.get(testKey));
+		Assert.assertFalse(concurrentMap.containsValue(testValue));
+		Assert.assertTrue(concurrentMap.containsValue(testValue2));
+		Assert.assertEquals(1, finalizeActions.size());
+
+		Set<String> keySet = concurrentMap.keySet();
+
+		Iterator<String> keyIterator = keySet.iterator();
+
+		Assert.assertSame(testKey, keyIterator.next());
+
+		Collection<Object> values = concurrentMap.values();
+
+		Iterator<Object> valueIterator = values.iterator();
+
+		Assert.assertSame(testValue2, valueIterator.next());
+		Assert.assertSame(
+			testValue2, concurrentMap.replace(testKey, new Object()));
+		Assert.assertEquals(1, finalizeActions.size());
+		Assert.assertEquals(1, concurrentMap.size());
+
+		testKey = null;
+
+		if (fullGC) {
+			GCUtil.fullGC();
+		}
+		else {
+			GCUtil.gc();
+		}
+
+		ReflectionTestUtil.invoke(
+			FinalizeManager.class, "_pollingCleanup", new Class<?>[0]);
+
+		Assert.assertTrue(finalizeActions.isEmpty());
+		Assert.assertTrue(concurrentMap.isEmpty());
+	}
+
+}
---
+++ portal-service/test/unit/com/liferay/portal/kernel/concurrent/ConcurrentReferenceKeyHashMapTest.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.concurrent;
+
+import com.liferay.portal.kernel.memory.FinalizeManager;
+import com.liferay.portal.kernel.test.CodeCoverageAssertor;
+import com.liferay.portal.kernel.test.NewClassLoaderJUnitTestRunner;
+import com.liferay.portal.kernel.util.StringPool;
+
+import java.lang.ref.Reference;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Shuyang Zhou
+ */
+@RunWith(NewClassLoaderJUnitTestRunner.class)
+public class ConcurrentReferenceKeyHashMapTest
+	extends BaseConcurrentReferenceHashMapTestCase {
+
+	@ClassRule
+	public static CodeCoverageAssertor codeCoverageAssertor =
+		new CodeCoverageAssertor();
+
+	@Test
+	public void testAutoRemove() throws Exception {
+		System.setProperty(
+			FinalizeManager.class.getName() + ".thread.enabled",
+			StringPool.FALSE);
+
+		doTestAutoRemove(
+			new ConcurrentReferenceKeyHashMap<String, Object>(
+				FinalizeManager.SOFT_REFERENCE_FACTORY),
+			true);
+
+		doTestAutoRemove(
+			new ConcurrentReferenceKeyHashMap<String, Object>(
+				FinalizeManager.WEAK_REFERENCE_FACTORY),
+			false);
+	}
+
+	@Test
+	public void testConstructor() {
+		ConcurrentMap<Reference<String>, Object> innerConcurrentMap =
+			new ConcurrentHashMap<Reference<String>, Object>();
+
+		ConcurrentReferenceKeyHashMap<String, Object>
+			concurrentReferenceKeyHashMap =
+				new ConcurrentReferenceKeyHashMap<String, Object>(
+					innerConcurrentMap, FinalizeManager.WEAK_REFERENCE_FACTORY);
+
+		Assert.assertSame(
+			innerConcurrentMap,
+			concurrentReferenceKeyHashMap.innerConcurrentMap);
+
+		Map<String, Object> dataMap = createDataMap();
+
+		concurrentReferenceKeyHashMap =
+			new ConcurrentReferenceKeyHashMap<String, Object>(
+				dataMap, FinalizeManager.WEAK_REFERENCE_FACTORY);
+
+		Assert.assertEquals(dataMap, concurrentReferenceKeyHashMap);
+
+		new ConcurrentReferenceKeyHashMap<String, Object>(
+			10, FinalizeManager.WEAK_REFERENCE_FACTORY);
+		new ConcurrentReferenceKeyHashMap<String, Object>(
+			10, 0.75F, 4, FinalizeManager.WEAK_REFERENCE_FACTORY);
+	}
+
+}
