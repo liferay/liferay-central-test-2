1cd5365c75521e60ca1e0da0745534ff4f31c4e7
peterfellwock <peter.fellwock@liferay.com>
2014-08-20 12:48:30 -0700

    LPS-49192 Removing unused properties.

--- portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -67,12 +67,6 @@ public interface PropsKeys {
 
 	public static final String AIM_PASSWORD = "aim.password";
 
-	public static final String AMAZON_ACCESS_KEY_ID = "amazon.access.key.id";
-
-	public static final String AMAZON_ASSOCIATE_TAG = "amazon.associate.tag";
-
-	public static final String AMAZON_SECRET_ACCESS_KEY = "amazon.secret.access.key";
-
 	public static final String ANNOUNCEMENTS_EMAIL_BODY = "announcements.email.body";
 
 	public static final String ANNOUNCEMENTS_EMAIL_FROM_ADDRESS = "announcements.email.from.address";
