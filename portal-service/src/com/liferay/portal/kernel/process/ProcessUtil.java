f0b0ab53666766a96dbc14d53d79773d5e19a13b
shuyangzhou <shuyang.zhou@liferay.com>
2014-08-21 15:42:58 -0700

    LPS-49517 Fix wrong class name used

--- portal-service/src/com/liferay/portal/kernel/process/ProcessUtil.java
+++ portal-service/src/com/liferay/portal/kernel/process/ProcessUtil.java
@@ -124,7 +124,7 @@ public class ProcessUtil {
 					0, Integer.MAX_VALUE, 60, TimeUnit.SECONDS, true,
 					Integer.MAX_VALUE, new AbortPolicy(),
 					new NamedThreadFactory(
-						ProcessExecutor.class.getName(), Thread.MIN_PRIORITY,
+						ProcessUtil.class.getName(), Thread.MIN_PRIORITY,
 						PortalClassLoaderUtil.getClassLoader()),
 					new ThreadPoolHandlerAdapter());
 			}
