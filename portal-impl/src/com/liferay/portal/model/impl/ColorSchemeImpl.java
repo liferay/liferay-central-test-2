788a9de2e0c41d7b57dbe066f61cfb3befc4bb90
willnewbury <william.newbury@liferay.com>
2014-08-21 16:07:36 -0700

    LPS-49294 Part 1: Modify or create constructors to ensure that all final variables are initialized

--- portal-impl/src/com/liferay/portal/model/impl/ColorSchemeImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/ColorSchemeImpl.java
@@ -33,10 +33,11 @@ import java.util.Properties;
 public class ColorSchemeImpl implements ColorScheme {
 
 	public ColorSchemeImpl() {
+		this(null, null, null);
 	}
 
 	public ColorSchemeImpl(String colorSchemeId) {
-		_colorSchemeId = colorSchemeId;
+		this(colorSchemeId, null, null);
 	}
 
 	public ColorSchemeImpl(String colorSchemeId, String name, String cssClass) {
@@ -201,7 +202,7 @@ public class ColorSchemeImpl implements ColorScheme {
 
 	private static Log _log = LogFactoryUtil.getLog(ColorSchemeImpl.class);
 
-	private String _colorSchemeId;
+	private final String _colorSchemeId;
 	private String _colorSchemeImagesPath =
 		"${images-path}/color_schemes/${css-class}";
 	private String _cssClass;
