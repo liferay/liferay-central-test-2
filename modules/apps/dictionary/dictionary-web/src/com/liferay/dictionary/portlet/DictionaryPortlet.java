1e7a1be183ed4eaffc1d7ac10753708a8e4f82bd
Brian Chan <brian.chan@liferay.com>
2014-08-21 11:45:37 -0700

    LPS-49170 SF

--- modules/apps/dictionary/dictionary-web/src/com/liferay/dictionary/portlet/DictionaryPortlet.java
+++ modules/apps/dictionary/dictionary-web/src/com/liferay/dictionary/portlet/DictionaryPortlet.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.dictionary.portlet;
 
 import com.liferay.dictionary.upgrade.DictionaryUpgrade;
