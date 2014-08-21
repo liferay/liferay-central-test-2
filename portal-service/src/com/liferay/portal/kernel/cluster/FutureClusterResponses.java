e787a79757bb9c7aa027edf78fc4de3ced582468
shuyangzhou <shuyang.zhou@liferay.com>
2014-08-20 20:12:16 -0700

    LPS-49517 Apply to FutureClusterResponses

--- portal-service/src/com/liferay/portal/kernel/cluster/FutureClusterResponses.java
+++ portal-service/src/com/liferay/portal/kernel/cluster/FutureClusterResponses.java
@@ -14,24 +14,33 @@
 
 package com.liferay.portal.kernel.cluster;
 
+import com.liferay.portal.kernel.concurrent.DefaultNoticeableFuture;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Future;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Tina Tian
  */
-public class FutureClusterResponses implements Future<ClusterNodeResponses> {
+public class FutureClusterResponses
+	extends DefaultNoticeableFuture<ClusterNodeResponses> {
 
 	public FutureClusterResponses(List<Address> addresses) {
 		_clusterNodeResponses = new ClusterNodeResponses();
-		_countDownLatch = new CountDownLatch(addresses.size());
+
+		int size = addresses.size();
+
+		if (size == 0) {
+			set(_clusterNodeResponses);
+		}
+
+		_counter = new AtomicInteger(size);
 		_expectedReplyAddress = new HashSet<Address>(addresses);
 	}
 
@@ -40,22 +49,9 @@ public class FutureClusterResponses implements Future<ClusterNodeResponses> {
 
 		_clusterNodeResponses.addClusterResponse(clusterNodeResponse);
 
-		_countDownLatch.countDown();
-	}
-
-	public void addExpectedReplyAddress(Address address) {
-		_expectedReplyAddress.add(address);
-	}
-
-	@Override
-	public boolean cancel(boolean mayInterruptIfRunning) {
-		if (_cancelled || isDone()) {
-			return false;
+		if (_counter.decrementAndGet() == 0) {
+			set(_clusterNodeResponses);
 		}
-
-		_cancelled = true;
-
-		return true;
 	}
 
 	public boolean expectsReply(Address address) {
@@ -64,28 +60,25 @@ public class FutureClusterResponses implements Future<ClusterNodeResponses> {
 
 	@Override
 	public ClusterNodeResponses get() throws InterruptedException {
-		if (_cancelled) {
-			throw new CancellationException();
+		try {
+			return super.get();
 		}
-
-		_countDownLatch.await();
-
-		return _clusterNodeResponses;
+		catch (ExecutionException ee) {
+			throw new AssertionError(
+				"Not possible to throw ExecutionException", ee);
+		}
 	}
 
 	@Override
-	public ClusterNodeResponses get(long timeout, TimeUnit timeUnit)
+	public ClusterNodeResponses get(long timeout, TimeUnit unit)
 		throws InterruptedException, TimeoutException {
 
-		if (_cancelled) {
-			throw new CancellationException();
+		try {
+			return super.get(timeout, unit);
 		}
-
-		if (_countDownLatch.await(timeout, timeUnit)) {
-			return _clusterNodeResponses;
-		}
-		else {
-			throw new TimeoutException();
+		catch (ExecutionException ee) {
+			throw new AssertionError(
+				"Not possible to throw ExecutionException", ee);
 		}
 	}
 
@@ -93,24 +86,8 @@ public class FutureClusterResponses implements Future<ClusterNodeResponses> {
 		return _clusterNodeResponses.getClusterResponses();
 	}
 
-	@Override
-	public boolean isCancelled() {
-		return _cancelled;
-	}
-
-	@Override
-	public boolean isDone() {
-		if ((_countDownLatch.getCount() == 0) || _cancelled) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-
-	private boolean _cancelled;
 	private ClusterNodeResponses _clusterNodeResponses;
-	private CountDownLatch _countDownLatch;
+	private AtomicInteger _counter;
 	private Set<Address> _expectedReplyAddress;
 
 }
