80be84243bb148b05fe4e79d8b40244f15194e54
shuyangzhou <shuyang.zhou@liferay.com>
2014-08-20 20:06:52 -0700

    LPS-49517 Create NoticeableFutureConvertor

---
+++ portal-service/src/com/liferay/portal/kernel/concurrent/NoticeableFutureConvertor.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.concurrent;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+/**
+ * @author Shuyang Zhou
+ */
+public abstract class NoticeableFutureConvertor<T, V>
+	extends DefaultNoticeableFuture<T> {
+
+	public NoticeableFutureConvertor(NoticeableFuture<V> noticeableFuture) {
+		noticeableFuture.addFutureListener(
+			new FutureListener<V>() {
+
+				@Override
+				public void complete(Future<V> future) {
+					try {
+						NoticeableFutureConvertor.this.set(
+							convert(future.get()));
+					}
+					catch (Throwable t) {
+						if (t instanceof ExecutionException) {
+							t = t.getCause();
+						}
+
+						NoticeableFutureConvertor.this.setException(t);
+					}
+				}
+
+			});
+	}
+
+	protected abstract T convert(V v) throws Throwable;
+
+}
