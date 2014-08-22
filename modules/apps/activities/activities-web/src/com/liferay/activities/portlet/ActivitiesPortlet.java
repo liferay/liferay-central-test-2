a67b1c5f7e9c238a65f3b120693a664a3b644d00
Brian Chan <brian.chan@liferay.com>
2014-08-21 19:22:55 -0700

    LPS-49191 match WebProxyPortlet

--- modules/apps/activities/activities-web/src/com/liferay/activities/portlet/ActivitiesPortlet.java
+++ modules/apps/activities/activities-web/src/com/liferay/activities/portlet/ActivitiesPortlet.java
@@ -50,10 +50,6 @@ import org.osgi.service.component.annotations.Reference;
 )
 public class ActivitiesPortlet extends MVCPortlet {
 
-	/**
-	 * Force upgrades to register before the portlet is registered to prevent
-	 * race conditions.
-	 */
 	@Reference(unbind = "-")
 	protected void setActivitiesUpgrade(ActivitiesUpgrade activitiesUpgrade) {
 	}
