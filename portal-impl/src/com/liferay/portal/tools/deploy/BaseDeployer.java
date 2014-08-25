94400c257fb6fa80c9399495b5f4475df395f16b
Tibor Lipusz <tibor.lipusz@liferay.com>
2014-08-25 16:23:48 +0200

    LPS-13222 web.xml in is missing from ext-web/WEB-INF when auto.deploy.unpack.war=false

--- portal-impl/src/com/liferay/portal/tools/deploy/BaseDeployer.java
+++ portal-impl/src/com/liferay/portal/tools/deploy/BaseDeployer.java
@@ -775,7 +775,7 @@ public class BaseDeployer implements AutoDeployer, Deployer {
 				SystemProperties.get(SystemProperties.TMP_DIR) +
 					File.separator + Time.getTimestamp());
 
-			excludes += "**/WEB-INF/web.xml";
+			excludes += "/WEB-INF/web.xml";
 
 			WarTask.war(srcFile, tempDir, excludes, webXml);
 
