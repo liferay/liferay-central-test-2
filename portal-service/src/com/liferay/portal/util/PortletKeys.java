51339793393c7f2596feed01b9d41348ab39d80f
peterfellwock <peter.fellwock@liferay.com>
2014-08-26 11:35:25 -0700

    LPS-49193 Removed key from PortletKeys.

--- portal-service/src/com/liferay/portal/util/PortletKeys.java
+++ portal-service/src/com/liferay/portal/util/PortletKeys.java
@@ -65,8 +65,6 @@ public class PortletKeys {
 
 	public static final String CONTROL_PANEL_MENU = "160";
 
-	public static final String CURRENCY_CONVERTER = "16";
-
 	public static final String DICTIONARY = "23";
 
 	public static final String DIRECTORY = "11";
