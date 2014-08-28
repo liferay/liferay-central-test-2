3e1e048cada34d5e9b4ee79b51d9cddeb824c14a
Brian Chan <brian.chan@liferay.com>
2014-08-27 21:56:55 -0700

    LPS-49684 SF

--- modules/portal/portal-search-elasticsearch/src/com/liferay/portal/search/elasticsearch/facet/ModifiedFacetProcessor.java
+++ modules/portal/portal-search-elasticsearch/src/com/liferay/portal/search/elasticsearch/facet/ModifiedFacetProcessor.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.portal.search.elasticsearch.facet;
 
 import org.osgi.service.component.annotations.Component;
