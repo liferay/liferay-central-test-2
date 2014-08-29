6018a2310fd63c56e3d92de64f50c279a13b9bbf
Brian Chan <brian.chan@liferay.com>
2014-08-29 08:59:02 -0700

    LPS-49738 SF

--- portal-impl/src/com/liferay/portlet/journal/action/ActionUtil.java
+++ portal-impl/src/com/liferay/portlet/journal/action/ActionUtil.java
@@ -438,7 +438,8 @@ public class ActionUtil {
 	}
 
 	protected static String getElementInstanceId(
-		String content, String fieldName, int index) throws Exception {
+			String content, String fieldName, int index)
+		throws Exception {
 
 		Document document = SAXReaderUtil.read(content);
 
