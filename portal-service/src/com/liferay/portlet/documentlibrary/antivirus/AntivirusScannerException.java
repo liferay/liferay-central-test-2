346f21d91a139ee473089a54c190518422147a90
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2014-08-29 15:14:15 +0200

    LPS-49495 SF

--- portal-service/src/com/liferay/portlet/documentlibrary/antivirus/AntivirusScannerException.java
+++ portal-service/src/com/liferay/portlet/documentlibrary/antivirus/AntivirusScannerException.java
@@ -27,8 +27,8 @@ public class AntivirusScannerException extends PortalException {
 	public static final int VIRUS_DETECTED = 2;
 
 	/**
-	 * @deprecated As of 7.0.0, replace by {@link
-	 *             #AntivirusScannerException(int)}
+	 * @deprecated As of 7.0.0, replace by {@link #AntivirusScannerException(
+	 *             int)}
 	 */
 	@Deprecated
 	public AntivirusScannerException() {
@@ -40,8 +40,8 @@ public class AntivirusScannerException extends PortalException {
 	}
 
 	/**
-	 * @deprecated As of 7.0.0, replace by {@link
-	 *             #AntivirusScannerException(String,int)}
+	 * @deprecated As of 7.0.0, replace by {@link #AntivirusScannerException(
+	 *             String, int)}
 	 */
 	@Deprecated
 	public AntivirusScannerException(String msg) {
