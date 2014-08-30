sergiogonzalez/liferay-portal

LPS-48728 use plugins to create repositories

portal-impl/src/com/liferay/portal/repository/registry/DefaultRepositoryRegistry.java
@@ -1,0 +1,85 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.repository.registry;
+
+import com.liferay.portal.kernel.repository.capabilities.Capability;
+import com.liferay.portal.kernel.repository.registry.CapabilityRegistry;
+import com.liferay.portal.kernel.repository.registry.RepositoryCreator;
+import com.liferay.portal.kernel.repository.registry.RepositoryCreatorRegistry;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class DefaultRepositoryRegistry
+	implements CapabilityRegistry, RepositoryConfiguration,
+		RepositoryCreatorRegistry {
+
+	public DefaultRepositoryRegistry() {
+		_publicCapabilities = new HashSet<Class<? extends Capability>>();
+		_supportedCapabilities =
+			new HashMap<Class<? extends Capability>, Capability>();
+	}
+
+	@Override
+	public <S extends Capability, T extends S> void addPrivateCapability(
+		Class<S> capabilityClass, T capability) {
+
+		_supportedCapabilities.put(capabilityClass, capability);
+	}
+
+	@Override
+	public <S extends Capability, T extends S> void addPublicCapability(
+		Class<S> capabilityClass, T capability) {
+
+		_publicCapabilities.add(capabilityClass);
+		_supportedCapabilities.put(capabilityClass, capability);
+	}
+
+	@Override
+	public Set<Class<? extends Capability>> getPublicCapabilities() {
+		return _publicCapabilities;
+	}
+
+	@Override
+	public RepositoryCreator getRepositoryCreator() {
+		return _repositoryCreator;
+	}
+
+	@Override
+	public Map<Class<? extends Capability>, Capability>
+		getSupportedCapabilities() {
+
+		return _supportedCapabilities;
+	}
+
+	@Override
+	public void setRepositoryCreator(RepositoryCreator repositoryCreator) {
+		if (_repositoryCreator != null) {
+			throw new IllegalStateException("repository creator already set");
+		}
+
+		_repositoryCreator = repositoryCreator;
+	}
+
+	private Set<Class<? extends Capability>> _publicCapabilities;
+	private RepositoryCreator _repositoryCreator;
+	private Map<Class<? extends Capability>, Capability> _supportedCapabilities;
+
+}

portal-impl/src/com/liferay/portal/repository/registry/RepositoryConfiguration.java
@@ -12,18 +12,24 @@
  * details.
  */
 
-package com.liferay.portal.kernel.repository.registry;
+package com.liferay.portal.repository.registry;
+
+import com.liferay.portal.kernel.repository.capabilities.Capability;
+import com.liferay.portal.kernel.repository.registry.RepositoryCreator;
+
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author Adolfo Pérez
  */
-public interface RepositoryRegistryPlugin {
+public interface RepositoryConfiguration {
 
-	public long getClassNameId();
+	public Set<Class<? extends Capability>> getPublicCapabilities();
 
-	public void registerCapabilities(CapabilityRegistry capabilityRegistry);
+	public RepositoryCreator getRepositoryCreator();
 
-	public void registerRepositoryCreator(
-		RepositoryCreatorRegistry repositoryCreatorRegistry);
+	public Map<Class<? extends Capability>, Capability>
+		getSupportedCapabilities();
 
 }

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portal/repository/registry/DefaultRepositoryRegistry.java
@@ -1,0 +1,88 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.repository.registry;
+
+import com.liferay.portal.kernel.repository.capabilities.Capability;
+import com.liferay.portal.kernel.repository.registry.CapabilityRegistry;
+import com.liferay.portal.kernel.repository.registry.RepositoryCreator;
+import com.liferay.portal.kernel.repository.registry.RepositoryCreatorRegistry;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class DefaultRepositoryRegistry
+	implements CapabilityRegistry, RepositoryConfiguration,
+		RepositoryCreatorRegistry {
+
+	public DefaultRepositoryRegistry() {
+		_exportedCapabilities = new HashSet<Class<? extends Capability>>();
+
+		_supportedCapabilities =
+			new HashMap<Class<? extends Capability>, Capability>();
+	}
+
+	@Override
+	public <S extends Capability, T extends S> void addExportedCapability(
+		Class<S> capabilityClass, T capability) {
+
+		_exportedCapabilities.add(capabilityClass);
+
+		addSupportedCapability(capabilityClass, capability);
+	}
+
+	@Override
+	public <S extends Capability, T extends S> void addSupportedCapability(
+		Class<S> capabilityClass, T capability) {
+
+		_supportedCapabilities.put(capabilityClass, capability);
+	}
+
+	@Override
+	public Set<Class<? extends Capability>> getExportedCapabilities() {
+		return _exportedCapabilities;
+	}
+
+	@Override
+	public RepositoryCreator getRepositoryCreator() {
+		return _repositoryCreator;
+	}
+
+	@Override
+	public Map<Class<? extends Capability>, Capability>
+		getSupportedCapabilities() {
+
+		return _supportedCapabilities;
+	}
+
+	@Override
+	public void setRepositoryCreator(RepositoryCreator repositoryCreator) {
+		if (_repositoryCreator != null) {
+			throw new IllegalStateException(
+				"Repository creator is already set");
+		}
+
+		_repositoryCreator = repositoryCreator;
+	}
+
+	private Set<Class<? extends Capability>> _exportedCapabilities;
+	private RepositoryCreator _repositoryCreator;
+	private Map<Class<? extends Capability>, Capability> _supportedCapabilities;
+
+}

portal-impl/src/com/liferay/portal/repository/registry/RepositoryConfiguration.java
@@ -12,18 +12,24 @@
  * details.
  */
 
-package com.liferay.portal.kernel.repository.registry;
+package com.liferay.portal.repository.registry;
+
+import com.liferay.portal.kernel.repository.capabilities.Capability;
+import com.liferay.portal.kernel.repository.registry.RepositoryCreator;
+
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author Adolfo Pérez
  */
-public interface RepositoryRegistryPlugin {
+public interface RepositoryConfiguration {
 
-	public String getClassName();
+	public Set<Class<? extends Capability>> getExportedCapabilities();
 
-	public void registerCapabilities(CapabilityRegistry capabilityRegistry);
+	public RepositoryCreator getRepositoryCreator();
 
-	public void registerRepositoryCreator(
-		RepositoryCreatorRegistry repositoryCreatorRegistry);
+	public Map<Class<? extends Capability>, Capability>
+		getSupportedCapabilities();
 
 }