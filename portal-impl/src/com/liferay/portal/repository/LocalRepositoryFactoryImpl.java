sergiogonzalez/liferay-portal

LPS-48728 wire capabilities in the registry plugins

portal-impl/src/com/liferay/portal/repository/LocalRepositoryFactoryImpl.java
@@ -51,8 +51,8 @@ public class LocalRepositoryFactoryImpl
 			repositoryCreator.createLocalRepository(repositoryId);
 
 		return new CapabilityLocalRepository(
-			localRepository, getExternalSupportedCapabilities(),
-			getExternalExportedCapabilityClasses());
+			localRepository, repositoryConfiguration.getSupportedCapabilities(),
+			repositoryConfiguration.getPublicCapabilities());
 	}
 
 	@Override
@@ -80,8 +80,8 @@ public class LocalRepositoryFactoryImpl
 			repositoryCreator.createLocalRepository(repositoryId);
 
 		return new CapabilityLocalRepository(
-			localRepository, getInternalSupportedCapabilities(),
-			getInternalExportedCapabilityClasses());
+			localRepository, repositoryConfiguration.getSupportedCapabilities(),
+			repositoryConfiguration.getPublicCapabilities());
 	}
 
 	@Override

portal-impl/src/com/liferay/portal/repository/liferayrepository/LiferayRepositoryRegistryPlugin.java
@@ -15,10 +15,12 @@
 package com.liferay.portal.repository.liferayrepository;
 
 import com.liferay.portal.kernel.bean.BeanReference;
+import com.liferay.portal.kernel.repository.capabilities.TrashCapability;
 import com.liferay.portal.kernel.repository.registry.BaseRepositoryRegistryPlugin;
 import com.liferay.portal.kernel.repository.registry.CapabilityRegistry;
 import com.liferay.portal.kernel.repository.registry.RepositoryCreator;
 import com.liferay.portal.kernel.repository.registry.RepositoryCreatorRegistry;
+import com.liferay.portal.repository.capabilities.LiferayTrashCapability;
 import com.liferay.portal.service.ClassNameLocalService;
 
 /**
@@ -38,6 +40,8 @@ public class LiferayRepositoryRegistryPlugin
 
 	@Override
 	public void registerCapabilities(CapabilityRegistry capabilityRegistry) {
+		capabilityRegistry.addPublicCapability(
+			TrashCapability.class, new LiferayTrashCapability());
 	}
 
 	@Override

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portal/repository/LocalRepositoryFactoryImpl.java
@@ -51,8 +51,8 @@ public class LocalRepositoryFactoryImpl
 			repositoryCreator.createLocalRepository(repositoryId);
 
 		return new CapabilityLocalRepository(
-			localRepository, getExternalSupportedCapabilities(),
-			getExternalExportedCapabilityClasses());
+			localRepository, repositoryConfiguration.getSupportedCapabilities(),
+			repositoryConfiguration.getExportedCapabilities());
 	}
 
 	@Override
@@ -80,8 +80,8 @@ public class LocalRepositoryFactoryImpl
 			repositoryCreator.createLocalRepository(repositoryId);
 
 		return new CapabilityLocalRepository(
-			localRepository, getInternalSupportedCapabilities(),
-			getInternalExportedCapabilityClasses());
+			localRepository, repositoryConfiguration.getSupportedCapabilities(),
+			repositoryConfiguration.getExportedCapabilities());
 	}
 
 	@Override

portal-impl/src/com/liferay/portal/repository/liferayrepository/LiferayRepositoryRegistryPlugin.java
@@ -14,10 +14,12 @@
 
 package com.liferay.portal.repository.liferayrepository;
 
+import com.liferay.portal.kernel.repository.capabilities.TrashCapability;
 import com.liferay.portal.kernel.repository.registry.BaseRepositoryRegistryPlugin;
 import com.liferay.portal.kernel.repository.registry.CapabilityRegistry;
 import com.liferay.portal.kernel.repository.registry.RepositoryCreator;
 import com.liferay.portal.kernel.repository.registry.RepositoryCreatorRegistry;
+import com.liferay.portal.repository.capabilities.LiferayTrashCapability;
 
 /**
  * @author Adolfo Pérez
@@ -32,6 +34,8 @@ public class LiferayRepositoryRegistryPlugin
 
 	@Override
 	public void registerCapabilities(CapabilityRegistry capabilityRegistry) {
+		capabilityRegistry.addExportedCapability(
+			TrashCapability.class, new LiferayTrashCapability());
 	}
 
 	@Override