04657c14682b19a5fce2ab6c16f0dd5c708bad4b
peterfellwock <peter.fellwock@liferay.com>
2014-09-02 14:04:42 -0700

    LPS-49631 Clean up Portlet class.

--- modules/apps/iframe/iframe-web/src/com/liferay/iframe/portlet/IFramePortlet.java
+++ modules/apps/iframe/iframe-web/src/com/liferay/iframe/portlet/IFramePortlet.java
@@ -15,15 +15,12 @@
 package com.liferay.iframe.portlet;
 
 import com.liferay.iframe.upgrade.IFrameUpgrade;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import javax.portlet.Portlet;
-import javax.portlet.PortletConfig;
-import javax.portlet.PortletException;
 
 /**
  * @author Brian Wing Shun Chan
@@ -32,33 +29,31 @@ import javax.portlet.PortletException;
 */
 
 @Component(
-immediate = true,
-property = {
-		
-	"com.liferay.portlet.configuration-action-class=com.liferay.portlet.iframe.action.ConfigurationActionImpl",
-	"com.liferay.portlet.css-class-wrapper=portlet-iframe",
-	"com.liferay.portlet.display-category=category.sample", 
-	"com.liferay.portlet.header-portlet-css=/css/main.css",
-	"com.liferay.portlet.icon=/default.png",
-	"com.liferay.portlet.instanceable=true",
-	"com.liferay.portlet.preferences-owned-by-group=true",
-	"com.liferay.portlet.private-request-attributes=false",
-	"com.liferay.portlet.private-session-attributes=false",
-	"com.liferay.portlet.render-weight=50",
-	"com.liferay.portlet.struts-path=iframe",
-	"com.liferay.portlet.use-default-template=false",
-	"javax.portlet.portlet.display-name=IFrame",
-	"javax.portlet.portlet.expiration-cache=0",
-	"javax.portlet.init-param.config-template=/configuration.jsp",
-	"javax.portlet.init-param.template-path=/",
-	"javax.portlet.init-param.view-template=/view.jsp",
-	"javax.portlet.portlet.portlet-class=com.liferay.portlet.iframe.IFramePortlet",
-	"javax.portlet.portlet.resource-bundle=com.liferay.portlet.StrutsResourceBundle",
-	"javax.portlet.security-role-ref=power-user,user",
-	"javax.portlet.supports.mime-type=text/html"
-	
-},
-service = Portlet.class
+	immediate = true,
+	property = {
+		"com.liferay.portlet.css-class-wrapper=portlet-iframe",
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.icon=/default.png",
+		"com.liferay.portlet.instanceable=true",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.struts-path=iframe",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=IFrame",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.config-template=/configuration.jsp",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.display-name=IFrame",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.preferences=classpath:/META-INF/portlet-preferences/default-portlet-preferences.xml",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
 )
 
 public class IFramePortlet extends MVCPortlet {
@@ -68,60 +63,7 @@ public class IFramePortlet extends MVCPortlet {
 	*/
 	@Reference(unbind = "-")
 	protected void setIFrameUpgrade(
-			IFrameUpgrade iFrameUpgrade) {
-	}
-
-	public static final String DEFAULT_EDIT_ACTION = "/iframe/edit";
-
-	public static final String DEFAULT_VIEW_ACTION = "/iframe/view";
-
-	@Override
-	public void init(PortletConfig portletConfig) throws PortletException {
-		super.init(portletConfig);
-
-		if (Validator.isNull(editAction)) {
-			editAction = DEFAULT_EDIT_ACTION;
-		}
-
-		if (Validator.isNull(viewAction)) {
-			viewAction = DEFAULT_VIEW_ACTION;
-		}
+		IFrameUpgrade iFrameUpgrade) {
 	}
 
 }
-
-/**
-
-
-<!-- IFrame -->
-
-		<action path="/iframe/proxy" forward="/portlet/iframe/proxy.jsp" />
-
-		<action path="/iframe/view" type="com.liferay.portlet.iframe.action.ViewAction">
-			<forward name="portlet.iframe.view" path="portlet.iframe.view" />
-		</action>
-		
-		
-		
-			<!-- IFrame -->
-
-	<definition name="portlet.iframe" extends="portlet" />
-
-	<definition name="portlet.iframe.view" extends="portlet.iframe">
-		<put name="portlet_content" value="/portlet/iframe/view.jsp" />
-	</definition>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-**/
