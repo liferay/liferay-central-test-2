0bdc1dc7d84ce814be22e3b70b208119f46c59b1
Jorge Díaz <jorge.diaz@liferay.com>
2014-09-03 20:18:50 +0200

    LPS-47852 Remove unnecessary validations after LPS-49619 changes

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
@@ -2234,8 +2234,7 @@ public class DLFileEntryLocalServiceImpl
 
 		validateFile(
 			dlFileEntry.getGroupId(), newFolderId, dlFileEntry.getFileEntryId(),
-			dlFileEntry.getFileName(), dlFileEntry.getExtension(),
-			dlFileEntry.getTitle());
+			dlFileEntry.getFileName(), dlFileEntry.getTitle());
 
 		if (DLStoreUtil.hasFile(
 				user.getCompanyId(),
@@ -2543,7 +2542,7 @@ public class DLFileEntryLocalServiceImpl
 
 	protected void validateFile(
 			long groupId, long folderId, long fileEntryId, String fileName,
-			String extension, String title)
+			String title)
 		throws PortalException {
 
 		DLFolder dlFolder = dlFolderPersistence.fetchByG_P_N(
@@ -2560,21 +2559,6 @@ public class DLFileEntryLocalServiceImpl
 			(dlFileEntry.getFileEntryId() != fileEntryId)) {
 
 			throw new DuplicateFileException(title);
-		}
-
-		String periodAndExtension = StringPool.PERIOD.concat(extension);
-
-		if (!title.endsWith(periodAndExtension)) {
-			title += periodAndExtension;
-
-			dlFileEntry = dlFileEntryPersistence.fetchByG_F_T(
-				groupId, folderId, title);
-
-			if ((dlFileEntry != null) &&
-				(dlFileEntry.getFileEntryId() != fileEntryId)) {
-
-				throw new DuplicateFileException(title);
-			}
 		}
 
 		dlFileEntry = dlFileEntryPersistence.fetchByG_F_FN(
@@ -2612,8 +2596,7 @@ public class DLFileEntryLocalServiceImpl
 
 		DLStoreUtil.validate(title, false);
 
-		validateFile(
-			groupId, folderId, fileEntryId, fileName, extension, title);
+		validateFile(groupId, folderId, fileEntryId, fileName, title);
 	}
 
 	protected void validateFileEntryTypeId(
