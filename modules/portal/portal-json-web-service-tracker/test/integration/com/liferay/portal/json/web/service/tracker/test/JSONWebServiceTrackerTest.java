4c6020ec92049dd808efdd71c6aada976da452ce
Carlos Sierra <carlos.sierra@liferay.com>
2014-09-03 11:48:18 +0200

    LPS-49783 Added test

---
+++ modules/portal/portal-json-web-service-tracker/test/integration/com/liferay/portal/json/web/service/tracker/test/JSONWebServiceTrackerTest.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.json.web.service.tracker.test;
+
+import com.liferay.arquillian.deploymentscenario.annotations.BndFile;
+import com.liferay.portal.kernel.util.StringUtil;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Random;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+@BndFile("test/integration/test-bnd.bnd")
+@RunAsClient
+@RunWith(Arquillian.class)
+public class JSONWebServiceTrackerTest {
+
+	@Test
+	public void testWebServiceContextAppearsInTheSummary() throws IOException {
+		URL wsUrl = new URL(context, "/api/jsonws");
+
+		String body = StringUtil.read(wsUrl.openStream());
+
+		Assert.assertTrue(body.contains("testws"));
+	}
+
+	@Test
+	public void testWebServiceInvocation()
+		throws IOException, URISyntaxException {
+
+		Random random = new Random();
+
+		int a = random.nextInt(50);
+		int b = random.nextInt(50);
+
+		final URL wsUrl = new URL(
+			context, "/api/jsonws/testws.testweb/sum/a/"+a+"/b/"+b);
+
+		CloseableHttpClient httpClient =
+			HttpClients.custom().setDefaultCredentialsProvider(
+				new BasicCredentialsProvider() { {
+					setCredentials(
+						new AuthScope(wsUrl.getHost(), wsUrl.getPort()),
+						new UsernamePasswordCredentials(
+							"test@liferay.com", "test")
+					);
+				}}
+			).build();
+
+		HttpGet httpGet = new HttpGet(wsUrl.toURI());
+
+		CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
+
+		InputStream content = httpResponse.getEntity().getContent();
+
+		String contentString = StringUtil.read(content);
+
+		Assert.assertEquals(a+b, Integer.parseInt(contentString));
+	}
+
+	@ArquillianResource
+	URL context;
+
+}
