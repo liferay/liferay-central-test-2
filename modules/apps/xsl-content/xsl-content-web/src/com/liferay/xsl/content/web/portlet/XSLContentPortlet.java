53e3cd84206925308a675cd93f814c29563ed212
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2014-09-03 17:15:41 +0200

    LPS-49451 Limit ADD_TO_PAGE permission to administrator only

--- modules/apps/xsl-content/xsl-content-web/src/com/liferay/xsl/content/web/portlet/XSLContentPortlet.java
+++ modules/apps/xsl-content/xsl-content-web/src/com/liferay/xsl/content/web/portlet/XSLContentPortlet.java
@@ -43,7 +43,7 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=guest,power-user,user",
+		"javax.portlet.security-role-ref=administrator",
 		"javax.portlet.supported-public-render-parameter=tags"
 	},
 	service = Portlet.class
