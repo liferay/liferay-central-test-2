171b7c4965bdda6cb933abb745a276c714a248d2
peterfellwock <peter.fellwock@liferay.com>
2014-09-03 09:43:56 -0700

    LPS-49751 New Portlet.

---
+++ modules/apps/loan-calculator/loan-calculator-web/src/com/liferay/loan/calculator/portlet/LoanCalculatorPortlet.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.loan.calculator.portlet;
+
+import com.liferay.loan.calculator.upgrade.LoanCalculatorUpgrade;
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Peter Fellwock
+*/
+
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.css-class-wrapper=portlet-loan-calculator",
+		"com.liferay.portlet.display-category=category.finance",
+		"com.liferay.portlet.icon=/icons/loan_calculator.png",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.remoteable=true",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.struts-path=loan_calculator",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Loan Calculator",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+
+public class LoanCalculatorPortlet extends MVCPortlet {
+	/**
+	* Force upgrades to register before the portlet is registered to prevent
+	* race conditions.
+	*/
+	@Reference(unbind = "-")
+	protected void setLoanCalculatorUpgrade(
+		LoanCalculatorUpgrade loanCalculatorUpgrade) {
+	}
+
+}
