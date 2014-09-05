34c82fbb9a98d142a730c9cd9bfdc66fe73cfd74
Raymond Auge <raymond.auge@liferay.com>
2014-09-05 14:05:02 -0400

    LPS-49631 file doesn't exist

--- modules/apps/iframe/iframe-web/src/com/liferay/iframe/web/portlet/IFramePortlet.java
+++ modules/apps/iframe/iframe-web/src/com/liferay/iframe/web/portlet/IFramePortlet.java
@@ -46,7 +46,6 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.css-class-wrapper=portlet-iframe",
 		"com.liferay.portlet.display-category=category.sample",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
-		"com.liferay.portlet.icon=/default.png",
 		"com.liferay.portlet.instanceable=true",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
