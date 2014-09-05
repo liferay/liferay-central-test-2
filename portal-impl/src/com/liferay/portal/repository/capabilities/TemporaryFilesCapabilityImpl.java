76a85ac6dfb767c928a622c087957fde3091c888
Iván Zaera Avellón <ivan.zaera@liferay.com>
2014-09-05 14:43:37 +0200

    LPS-49778 Add a new type of repository which is based on LiferayRepository but adding TemporaryFilesCapability too.

---
+++ portal-impl/src/com/liferay/portal/repository/capabilities/TemporaryFilesCapabilityImpl.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.repository.capabilities;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.repository.LocalRepository;
+import com.liferay.portal.kernel.repository.capabilities.TemporaryFilesCapability;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+
+import java.io.InputStream;
+
+import java.util.List;
+
+/**
+ * @author Iván Zaera
+ */
+public class TemporaryFilesCapabilityImpl implements TemporaryFilesCapability {
+
+	public TemporaryFilesCapabilityImpl(LocalRepository repository) {
+		_repository = repository;
+	}
+
+	@Override
+	public FileEntry addTemporaryFile(
+			long userId, String folderName, String fileName, String mimeType,
+			InputStream inputStream)
+		throws PortalException {
+
+		return null;
+	}
+
+	@Override
+	public void deleteExpiredTemporaryFiles() throws PortalException {
+	}
+
+	@Override
+	public void deleteTemporaryFile(
+			long userId, String folderName, String fileName)
+		throws PortalException {
+	}
+
+	@Override
+	public FileEntry getTemporaryFile(
+			long userId, String folderName, String fileName)
+		throws PortalException {
+
+		return null;
+	}
+
+	@Override
+	public List<FileEntry> getTemporaryFiles(long userId, String folderName)
+		throws PortalException {
+
+		return null;
+	}
+
+	@Override
+	public long getTemporaryFilesTimeoutMilliseconds() {
+		return 0;
+	}
+
+	@Override
+	public void setTemporaryFilesTimeoutMilliseconds(
+		long temporaryFilesTimeoutMilliseconds) {
+	}
+
+	private LocalRepository _repository;
+
+}
---
+++ portal-impl/src/com/liferay/portal/repository/temporaryrepository/TemporaryRepositoryDefiner.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.repository.temporaryrepository;
+
+import com.liferay.portal.kernel.repository.DocumentRepository;
+import com.liferay.portal.kernel.repository.LocalRepository;
+import com.liferay.portal.kernel.repository.RepositoryFactory;
+import com.liferay.portal.kernel.repository.capabilities.TemporaryFilesCapability;
+import com.liferay.portal.kernel.repository.registry.BaseRepositoryDefiner;
+import com.liferay.portal.kernel.repository.registry.CapabilityRegistry;
+import com.liferay.portal.kernel.repository.registry.RepositoryEventRegistry;
+import com.liferay.portal.kernel.repository.registry.RepositoryFactoryRegistry;
+import com.liferay.portal.repository.capabilities.TemporaryFilesCapabilityImpl;
+import com.liferay.portal.repository.liferayrepository.LiferayRepositoryFactory;
+
+/**
+ * @author Iván Zaera
+ */
+public class TemporaryRepositoryDefiner extends BaseRepositoryDefiner {
+
+	@Override
+	public String getClassName() {
+		return TemporaryRepository.class.getName();
+	}
+
+	@Override
+	public boolean isExternalRepository() {
+		return false;
+	}
+
+	@Override
+	public void registerCapabilities(CapabilityRegistry capabilityRegistry) {
+		DocumentRepository documentRepository =
+			capabilityRegistry.getDocumentRepository();
+
+		if (documentRepository instanceof LocalRepository) {
+			capabilityRegistry.addExportedCapability(
+				TemporaryFilesCapability.class,
+				new TemporaryFilesCapabilityImpl(
+					(LocalRepository)documentRepository));
+		}
+	}
+
+	@Override
+	public void registerRepositoryEventListeners(
+		RepositoryEventRegistry repositoryEventRegistry) {
+	}
+
+	@Override
+	public void registerRepositoryFactory(
+		RepositoryFactoryRegistry repositoryFactoryRegistry) {
+
+		repositoryFactoryRegistry.setRepositoryFactory(
+			_liferayRepositoryFactory);
+	}
+
+	public void setLiferayRepositoryFactory(
+		LiferayRepositoryFactory liferayRepositoryFactory) {
+
+		_liferayRepositoryFactory = liferayRepositoryFactory;
+	}
+
+	private RepositoryFactory _liferayRepositoryFactory;
+
+}
