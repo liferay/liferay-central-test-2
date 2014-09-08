f2a4e08300cd1229863c70ba6555046029ef2d9b
Brian Chan <brian.chan@liferay.com>
2014-09-08 10:36:16 -0700

    LPS-49607 Sort according to service.xml

--- portal-impl/src/com/liferay/portlet/layoutsadmin/lar/LayoutStagedModelDataHandler.java
+++ portal-impl/src/com/liferay/portlet/layoutsadmin/lar/LayoutStagedModelDataHandler.java
@@ -538,11 +538,12 @@ public class LayoutStagedModelDataHandler
 			_log.debug(sb.toString());
 		}
 
+		importedLayout.setCompanyId(portletDataContext.getCompanyId());
+
 		if (layout.getLayoutPrototypeUuid() != null) {
 			importedLayout.setModifiedDate(new Date());
 		}
 
-		importedLayout.setCompanyId(portletDataContext.getCompanyId());
 		importedLayout.setParentLayoutId(parentLayoutId);
 		importedLayout.setName(layout.getName());
 		importedLayout.setTitle(layout.getTitle());
