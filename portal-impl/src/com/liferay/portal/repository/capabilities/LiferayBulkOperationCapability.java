26aa79a8a7d637d7ee525b9d31c1da2e1928ce83
adolfo.perez <adolfo.perez@liferay.com>
2014-09-10 15:03:07 +0200

    LPS-48895 implementation of bulk capability

--- portal-impl/src/com/liferay/portal/repository/capabilities/LiferayBulkOperationCapability.java
+++ portal-impl/src/com/liferay/portal/repository/capabilities/LiferayBulkOperationCapability.java
@@ -14,20 +14,125 @@
 
 package com.liferay.portal.repository.capabilities;
 
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.repository.capabilities.BulkOperationCapability;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.repository.model.RepositoryModelOperation;
+import com.liferay.portal.repository.liferayrepository.model.LiferayFileEntry;
+import com.liferay.portal.repository.liferayrepository.model.LiferayFolder;
+import com.liferay.portlet.documentlibrary.model.DLFileEntry;
+import com.liferay.portlet.documentlibrary.model.DLFolder;
+import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.service.DLFolderLocalServiceUtil;
 
 /**
  * @author Adolfo Pérez
  */
 public class LiferayBulkOperationCapability implements BulkOperationCapability {
 
+	public LiferayBulkOperationCapability(long repositoryId) {
+		_repositoryId = repositoryId;
+	}
+
 	@Override
 	public void execute(RepositoryModelOperation repositoryModelOperation)
 		throws PortalException {
 
-		throw new UnsupportedOperationException("Not implemented");
+		executeOnAllFileEntries(repositoryModelOperation);
+		executeOnAllFolders(repositoryModelOperation);
+	}
+
+	protected void executeOnAllFileEntries(
+			RepositoryModelOperation repositoryModelOperation)
+		throws PortalException {
+
+		ActionableDynamicQuery actionableDynamicQuery =
+			DLFileEntryLocalServiceUtil.getActionableDynamicQuery();
+
+		actionableDynamicQuery.setAddCriteriaMethod(
+			new RepositoryModelAddCriteriaMethod());
+
+		actionableDynamicQuery.setPerformActionMethod(
+			new FileEntryPerformActionMethod(repositoryModelOperation));
+
+		actionableDynamicQuery.performActions();
+	}
+
+	protected void executeOnAllFolders(
+			RepositoryModelOperation repositoryModelOperation)
+		throws PortalException {
+
+		ActionableDynamicQuery actionableDynamicQuery =
+			DLFolderLocalServiceUtil.getActionableDynamicQuery();
+
+		actionableDynamicQuery.setAddCriteriaMethod(
+			new RepositoryModelAddCriteriaMethod());
+
+		actionableDynamicQuery.setPerformActionMethod(
+			new FolderPerformActionMethod(repositoryModelOperation));
+
+		actionableDynamicQuery.performActions();
+	}
+
+	private long _repositoryId;
+
+	private static class FileEntryPerformActionMethod
+		implements ActionableDynamicQuery.PerformActionMethod {
+
+		public FileEntryPerformActionMethod(
+			RepositoryModelOperation repositoryModelOperation) {
+
+			_repositoryModelOperation = repositoryModelOperation;
+		}
+
+		@Override
+		public void performAction(Object object) throws PortalException {
+			DLFileEntry dlFileEntry = (DLFileEntry)object;
+
+			FileEntry fileEntry = new LiferayFileEntry(dlFileEntry);
+
+			fileEntry.execute(_repositoryModelOperation);
+		}
+
+		private RepositoryModelOperation _repositoryModelOperation;
+
+	}
+
+	private static class FolderPerformActionMethod
+		implements ActionableDynamicQuery.PerformActionMethod {
+
+		public FolderPerformActionMethod(
+			RepositoryModelOperation repositoryModelOperation) {
+
+			_repositoryModelOperation = repositoryModelOperation;
+		}
+
+		@Override
+		public void performAction(Object object) throws PortalException {
+			DLFolder dlFolder = (DLFolder)object;
+
+			Folder folder = new LiferayFolder(dlFolder);
+
+			folder.execute(_repositoryModelOperation);
+		}
+
+		private final RepositoryModelOperation _repositoryModelOperation;
+
+	}
+
+	private class RepositoryModelAddCriteriaMethod
+		implements ActionableDynamicQuery.AddCriteriaMethod {
+
+		@Override
+		public void addCriteria(DynamicQuery dynamicQuery) {
+			dynamicQuery.add(
+				RestrictionsFactoryUtil.eq("repositoryId", _repositoryId));
+		}
+
 	}
 
 }
