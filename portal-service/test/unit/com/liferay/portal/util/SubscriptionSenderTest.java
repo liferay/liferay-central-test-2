fe364af3f7960c571e3fc72fb3eeb45bd2640eb1
Cristina González Castellano <cristina.gonzalez@liferay.com>
2014-09-10 13:25:13 +0200

    LPS-49600 Fix Test

--- portal-service/test/unit/com/liferay/portal/util/SubscriptionSenderTest.java
+++ portal-service/test/unit/com/liferay/portal/util/SubscriptionSenderTest.java
@@ -18,6 +18,8 @@ import com.liferay.portal.kernel.bean.BeanLocator;
 import com.liferay.portal.kernel.bean.PortalBeanLocatorUtil;
 import com.liferay.portal.kernel.util.Props;
 import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.uuid.PortalUUID;
+import com.liferay.portal.kernel.uuid.PortalUUIDUtil;
 import com.liferay.portal.model.Company;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.service.CompanyLocalService;
@@ -92,6 +94,12 @@ public class SubscriptionSenderTest extends PowerMockito {
 
 		PortalBeanLocatorUtil.setBeanLocator(_beanLocator);
 
+		PortalUUIDUtil portalUUIDUtil = new PortalUUIDUtil();
+
+		PortalUUID portalUUID = mock(PortalUUID.class);
+
+		portalUUIDUtil.setPortalUUID(portalUUID);
+
 		PortalUtil portalUtil = new PortalUtil();
 
 		Portal portal = mock(Portal.class);
@@ -126,6 +134,8 @@ public class SubscriptionSenderTest extends PowerMockito {
 
 		subscriptionSender.setGroupId(100);
 
+		subscriptionSender.setMailId("test-mail-id");
+
 		subscriptionSender.initialize();
 
 		String portalURL = String.valueOf(
@@ -137,6 +147,8 @@ public class SubscriptionSenderTest extends PowerMockito {
 	@Test
 	public void testGetPortalURLWithoutGroupId() throws Exception {
 		SubscriptionSender subscriptionSender = new SubscriptionSender();
+
+		subscriptionSender.setMailId("test-mail-id");
 
 		subscriptionSender.initialize();
 
@@ -155,6 +167,8 @@ public class SubscriptionSenderTest extends PowerMockito {
 		SubscriptionSender subscriptionSender = new SubscriptionSender();
 
 		subscriptionSender.setServiceContext(serviceContext);
+
+		subscriptionSender.setMailId("test-mail-id");
 
 		subscriptionSender.initialize();
 
