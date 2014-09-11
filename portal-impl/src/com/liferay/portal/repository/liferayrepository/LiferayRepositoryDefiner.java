df54f2d2f71d354f0c597e130b0dad3eba6a1f84
adolfo.perez <adolfo.perez@liferay.com>
2014-09-11 09:56:53 +0200

    LPS-48895 wire in the BulkOperationCapability

--- portal-impl/src/com/liferay/portal/repository/liferayrepository/LiferayRepositoryDefiner.java
+++ portal-impl/src/com/liferay/portal/repository/liferayrepository/LiferayRepositoryDefiner.java
@@ -14,13 +14,16 @@
 
 package com.liferay.portal.repository.liferayrepository;
 
+import com.liferay.portal.kernel.repository.DocumentRepository;
 import com.liferay.portal.kernel.repository.RepositoryFactory;
+import com.liferay.portal.kernel.repository.capabilities.BulkOperationCapability;
 import com.liferay.portal.kernel.repository.capabilities.SyncCapability;
 import com.liferay.portal.kernel.repository.capabilities.TrashCapability;
 import com.liferay.portal.kernel.repository.registry.BaseRepositoryDefiner;
 import com.liferay.portal.kernel.repository.registry.CapabilityRegistry;
 import com.liferay.portal.kernel.repository.registry.RepositoryEventRegistry;
 import com.liferay.portal.kernel.repository.registry.RepositoryFactoryRegistry;
+import com.liferay.portal.repository.capabilities.LiferayBulkOperationCapability;
 import com.liferay.portal.repository.capabilities.LiferaySyncCapability;
 import com.liferay.portal.repository.capabilities.LiferayTrashCapability;
 
@@ -43,6 +46,16 @@ public class LiferayRepositoryDefiner extends BaseRepositoryDefiner {
 	public void registerCapabilities(CapabilityRegistry capabilityRegistry) {
 		capabilityRegistry.addExportedCapability(
 			TrashCapability.class, _liferayTrashCapability);
+
+		DocumentRepository documentRepository =
+			capabilityRegistry.getDocumentRepository();
+
+		BulkOperationCapability bulkOperationCapability =
+			new LiferayBulkOperationCapability(
+				documentRepository.getRepositoryId());
+
+		capabilityRegistry.addExportedCapability(
+			BulkOperationCapability.class, bulkOperationCapability);
 
 		capabilityRegistry.addSupportedCapability(
 			SyncCapability.class, _liferaySyncCapability);
