55362e2d6df8533e04dcfaa8a7940d9f11a76e1b
adolfo.perez <adolfo.perez@liferay.com>
2014-09-11 09:57:55 +0200

    LPS-48895 call the sync capability explicitly
    We could have attached the call to the `destroy()` capability method to
    the (Delete, LocalRepository) event, but in general, we don't want bulk
    operations to be executed that way.
    
    Potentially, more than one capability could execute a bulk operation as
    part of any event, leading to terrible performance (each bulk execution
    would retrieve the whole repository from db page by page). To solve this
    problem, we are planning to allow repositories to register, along even
    listeners, repository operations to execute when an event is
    triggered. This way, the repository would be able to combine all
    operations into one and traverse the repository elements only once.
    
    Also, we will probably make bulk operations asynchronous, as they could
    potentially take some time to finish.

--- portal-impl/src/com/liferay/portal/repository/capabilities/CapabilityLocalRepository.java
+++ portal-impl/src/com/liferay/portal/repository/capabilities/CapabilityLocalRepository.java
@@ -16,6 +16,7 @@ package com.liferay.portal.repository.capabilities;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.repository.LocalRepository;
+import com.liferay.portal.kernel.repository.capabilities.SyncCapability;
 import com.liferay.portal.kernel.repository.event.RepositoryEventTrigger;
 import com.liferay.portal.kernel.repository.event.RepositoryEventType;
 import com.liferay.portal.kernel.repository.model.FileEntry;
@@ -107,6 +108,13 @@ public class CapabilityLocalRepository
 		_repositoryEventTrigger.trigger(
 			RepositoryEventType.Delete.class, LocalRepository.class,
 			localRepository);
+
+		SyncCapability syncCapability = getInternalCapability(
+			SyncCapability.class);
+
+		if (syncCapability != null) {
+			syncCapability.destroy(this);
+		}
 
 		localRepository.deleteAll();
 	}
