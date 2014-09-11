eb1aff867fcce299af902f5dc91c59d34d4f3eea
peterfellwock <peter.fellwock@liferay.com>
2014-09-11 13:46:30 -0700

    LPS-50036 Portlet Class

---
+++ modules/apps/network/network-web/src/com/liferay/network/web/portlet/NetworkPortlet.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.network.web.portlet;
+
+import com.liferay.network.web.upgrade.NetworkUpgrade;
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+* @author Peter Fellwock
+*/
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.css-class-wrapper=portlet-network",
+		"com.liferay.portlet.display-category=category.tools",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=0",
+		"com.liferay.portlet.remoteable=true",
+		"com.liferay.portlet.struts-path=network",
+		"javax.portlet.display-name=Network",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class NetworkPortlet extends MVCPortlet {
+
+	@Reference(unbind = "-")
+	protected void setNetworkUpgrade(NetworkUpgrade networkUpgrade) {
+	}
+
+}
