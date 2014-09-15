liferay/liferay-central-test-2

commit 86c98f82dd2c760f5e792b90caae5db5dd29b1c2

LPS-48834  URLs for links on portlets are pointing to the virtual host instead of the url the portal is being accessed through

portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -7955,11 +7955,22 @@ public class PortalImpl implements Portal {
 
 		boolean useGroupVirtualHostName = false;
 
-		if (canonicalURL ||
-			!StringUtil.equalsIgnoreCase(
-				themeDisplay.getServerName(), _LOCALHOST)) {
-
+		if (canonicalURL) {
 			useGroupVirtualHostName = true;
+		}
+
+		for (String virtualHost : PropsValues.VIRTUAL_HOSTS_VALID_HOSTS) {
+			if (StringUtil.equalsIgnoreCase(
+					themeDisplay.getServerName(), virtualHost) ||
+				StringUtil.wildcardMatches(
+					themeDisplay.getServerName(), virtualHost,
+					CharPool.QUESTION, CharPool.STAR,
+					CharPool.PERCENT, false)) {
+
+				useGroupVirtualHostName = false;
+
+				break;
+			}
 		}
 
 		long refererPlid = themeDisplay.getRefererPlid();

commit efbe8b198a7ff19d1f33e650cf5c8debe8416a2d

LPS-48834 (Temporary fix) Keep original logic that was broken by 86c98f82dd2c760f5e792b90caae5db5dd29b1c2 and its review
As it affects ee-6.2.x and master, this fix turns back original logic for getGroupFriendlyURL method, because the rollback is pretty difficult as too many commits were introduced after offending ones

portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -5931,8 +5931,14 @@ public class PortalImpl implements Portal {
 
 	@Override
 	public String getValidPortalDomain(long companyId, String domain) {
-		if (isValidPortalDomain(companyId, domain)) {
-			return domain;
+		for (String virtualHost : PropsValues.VIRTUAL_HOSTS_VALID_HOSTS) {
+			if (StringUtil.equalsIgnoreCase(domain, virtualHost) ||
+				StringUtil.wildcardMatches(
+					domain, virtualHost, CharPool.QUESTION, CharPool.STAR,
+					CharPool.PERCENT, false)) {
+
+				return domain;
+			}
 		}
 
 		if (_log.isWarnEnabled()) {