daf029b34e9f3b7526056f14fd2416dedff8b843
Iván Zaera Avellón <ivan.zaera@liferay.com>
2014-09-16 15:56:00 +0200

    LPS-49873 Remove FileVersionMetadataHelper applying the same pattern I have applied to DLFileEntryImpl before (adding more utility methods).

--- portal-impl/src/com/liferay/portlet/documentlibrary/model/impl/DLFileVersionImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/model/impl/DLFileVersionImpl.java
@@ -20,16 +20,22 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portlet.documentlibrary.model.DLFileEntry;
+import com.liferay.portlet.documentlibrary.model.DLFileEntryType;
 import com.liferay.portlet.documentlibrary.model.DLFolder;
 import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
 import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.service.DLFileEntryTypeLocalServiceUtil;
 import com.liferay.portlet.documentlibrary.service.DLFolderLocalServiceUtil;
 import com.liferay.portlet.documentlibrary.util.DLUtil;
+import com.liferay.portlet.dynamicdatamapping.model.DDMStructure;
 import com.liferay.portlet.expando.model.ExpandoBridge;
 import com.liferay.portlet.expando.util.ExpandoBridgeFactoryUtil;
 
 import java.io.IOException;
 import java.io.InputStream;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Jorge Ferrer
@@ -38,6 +44,29 @@ import java.io.InputStream;
 public class DLFileVersionImpl extends DLFileVersionBaseImpl {
 
 	public DLFileVersionImpl() {
+	}
+
+	@Override
+	public List<DDMStructure> getDDMStructures() throws PortalException {
+		List<DDMStructure> ddmStructures = Collections.emptyList();
+
+		long fileEntryTypeId = getFileEntryTypeId();
+
+		if (fileEntryTypeId != -1) {
+			DLFileEntryType dlFileEntryType =
+				DLFileEntryTypeLocalServiceUtil.getFileEntryType(
+					fileEntryTypeId);
+
+			ddmStructures = dlFileEntryType.getDDMStructures();
+		}
+
+		return ddmStructures;
+	}
+
+	@Override
+	public DLFileEntryType getDLFileEntryType() throws PortalException {
+		return DLFileEntryTypeLocalServiceUtil.getFileEntryType(
+			getFileEntryTypeId());
 	}
 
 	@Override
