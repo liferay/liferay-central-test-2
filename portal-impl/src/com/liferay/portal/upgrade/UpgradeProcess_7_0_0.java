a38c20ec4b312c0a0c0d88cda83f1b464ce5edc3
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-09-16 09:08:29 +0200

    LPS-50107 Upgrade preferences

--- portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_7_0_0.java
+++ portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_7_0_0.java
@@ -35,6 +35,7 @@ import com.liferay.portal.upgrade.v7_0_0.UpgradeSchema;
 import com.liferay.portal.upgrade.v7_0_0.UpgradeShopping;
 import com.liferay.portal.upgrade.v7_0_0.UpgradeShoppingPreferences;
 import com.liferay.portal.upgrade.v7_0_0.UpgradeSubscription;
+import com.liferay.portal.upgrade.v7_0_0.UpgradeWebContentDisplayPreferences;
 import com.liferay.portal.upgrade.v7_0_0.UpgradeWiki;
 
 /**
@@ -67,6 +68,7 @@ public class UpgradeProcess_7_0_0 extends UpgradeProcess {
 		upgrade(UpgradeShopping.class);
 		upgrade(UpgradeShoppingPreferences.class);
 		upgrade(UpgradeSubscription.class);
+		upgrade(UpgradeWebContentDisplayPreferences.class);
 		upgrade(UpgradeWiki.class);
 
 		// This must be the last upgrade process. Otherwise, upgrades based on
