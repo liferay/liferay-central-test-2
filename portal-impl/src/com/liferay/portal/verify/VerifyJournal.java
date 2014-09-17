205b85ac820a50a9fd4853f5e051dc54dc396ab7
Bryan Engler <bryan.engler@liferay.com>
2014-09-17 10:12:51 -0700

    LPS-49844 Filter by document_library type in query

--- portal-impl/src/com/liferay/portal/verify/VerifyJournal.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyJournal.java
@@ -154,7 +154,8 @@ public class VerifyJournal extends VerifyProcess {
 			con = DataAccess.getUpgradeOptimizedConnection();
 
 			ps = con.prepareStatement(
-				"select id_ from JournalArticle where structureId != ''");
+				"select id_ from JournalArticle where structureId != '' and " +
+					"content like '%document_library%'");
 
 			rs = ps.executeQuery();
 
