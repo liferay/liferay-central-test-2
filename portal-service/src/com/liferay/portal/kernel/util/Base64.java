8ba79a0bc54649907ecce0753f947b40caa9b3c8
shuyangzhou <shuyang.zhou@liferay.com>
2014-09-17 14:57:11 -0700

    LPS-50049 No need to flush before close

--- portal-service/src/com/liferay/portal/kernel/util/Base64.java
+++ portal-service/src/com/liferay/portal/kernel/util/Base64.java
@@ -97,12 +97,8 @@ public class Base64 {
 		UnsyncByteArrayOutputStream ubaos = new UnsyncByteArrayOutputStream(
 			32000);
 
-		try {
-			ObjectOutputStream os = new ObjectOutputStream(ubaos);
-
-			os.flush();
+		try (ObjectOutputStream os = new ObjectOutputStream(ubaos)) {
 			os.writeObject(o);
-			os.flush();
 		}
 		catch (Exception e) {
 			_log.error(e, e);
