cdb353bec6a417bca44bad968c5c67775352c7bb
Brian Chan <brian.chan@liferay.com>
2014-09-18 15:47:55 -0700

    LPS-49815 add warning to VerifyProperties

--- portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
@@ -291,8 +291,9 @@ public class VerifyProperties extends VerifyProcess {
 		"editor.wysiwyg.portal-web.docroot.html.portlet.portal_settings." +
 			"email_notifications.jsp",
 		"ehcache.statistics.enabled", "index.filter.search.limit",
-		"javax.persistence.validation.mode", "jbi.workflow.url",
-		"json.deserializer.strict.mode",
+		"invitation.email.max.recipients", "invitation.email.message.body",
+		"invitation.email.message.subject", "javax.persistence.validation.mode",
+		"jbi.workflow.url", "json.deserializer.strict.mode",
 		"journal.template.language.parser[css]",
 		"journal.template.language.parser[ftl]",
 		"journal.template.language.parser[vm]",
