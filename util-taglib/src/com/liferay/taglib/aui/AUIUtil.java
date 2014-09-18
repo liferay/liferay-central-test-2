eac9c04dbff50e7194c3622fabf959304e03cc18
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2014-09-17 17:09:47 -0700

    LPS-50059 - Source formatting

--- util-taglib/src/com/liferay/taglib/aui/AUIUtil.java
+++ util-taglib/src/com/liferay/taglib/aui/AUIUtil.java
@@ -163,14 +163,12 @@ public class AUIUtil {
 		}
 
 		if (baseType.equals("checkbox") || baseType.equals("radio")) {
-			sb.append("class=\"");
-			sb.append(baseType);
-
 			if (inlineField) {
-				sb.append(" inline");
+				sb.append("class=\"");
+				sb.append(baseType);
+				sb.append("-inline");
+				sb.append("\" ");
 			}
-
-			sb.append("\" ");
 		}
 		else {
 			sb.append("class=\"control-label\" ");
