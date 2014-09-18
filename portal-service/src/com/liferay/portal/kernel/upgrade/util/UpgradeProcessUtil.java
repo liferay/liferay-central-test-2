03555cd610724d62d1d3fefb4d3e67142974ebcf
IstvanD <istvan.dezsi@liferay.com>
2014-09-18 10:16:08 +0200

    LPS-48770 Translation of Basic Document is missing after upgrading to 6.2.10

--- portal-service/src/com/liferay/portal/kernel/upgrade/util/UpgradeProcessUtil.java
+++ portal-service/src/com/liferay/portal/kernel/upgrade/util/UpgradeProcessUtil.java
@@ -23,6 +23,7 @@ import com.liferay.portal.kernel.search.SearchEngineUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeException;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.StringPool;
@@ -75,7 +76,7 @@ public class UpgradeProcessUtil {
 				return languageId;
 			}
 			else {
-				return StringPool.BLANK;
+				return LocaleUtil.toLanguageId(LocaleUtil.US);
 			}
 		}
 		finally {
