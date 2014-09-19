6e53c647271a7241d5ea30da05d4ab42974b148a
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2014-09-19 15:02:22 +0200

    LPS-50185 Move to documentlibrary package instead of documentlibraryadmin

--- portal-impl/src/com/liferay/portlet/documentlibrary/messaging/TemporaryFilesMessageListener.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/messaging/TemporaryFilesMessageListener.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portlet.documentlibraryadmin.messaging;
+package com.liferay.portlet.documentlibrary.messaging;
 
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
