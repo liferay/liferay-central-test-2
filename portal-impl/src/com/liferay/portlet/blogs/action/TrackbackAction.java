9f541fe91933ab1debfd82d798a1b195510e8b5e
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2014-09-19 13:27:20 +0200

    LPS-49176 Do not disclose IP of the provided host, can expose local network addresses

--- portal-impl/src/com/liferay/portlet/blogs/action/TrackbackAction.java
+++ portal-impl/src/com/liferay/portlet/blogs/action/TrackbackAction.java
@@ -218,8 +218,7 @@ public class TrackbackAction extends PortletAction {
 
 		if (!remoteIP.equals(trackbackIP)) {
 			throw new TrackbackValidationException(
-				"Remote IP does not match the trackback URL's IP " +
-					trackbackIP);
+				"Remote IP does not match the trackback URL's IP");
 		}
 	}
 
