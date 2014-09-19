cb2f3f57e89b83ad5dba592615a9decf1c55f8d7
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2014-09-19 11:37:40 -0700

    LPS-49952 Generate random id in init-ext

--- util-taglib/src/com/liferay/taglib/aui/AudioTag.java
+++ util-taglib/src/com/liferay/taglib/aui/AudioTag.java
@@ -14,11 +14,7 @@
 
 package com.liferay.taglib.aui;
 
-import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.taglib.aui.base.BaseAudioTag;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Eduardo Lundgren
@@ -27,20 +23,4 @@ import javax.servlet.http.HttpServletRequest;
  * @author Julio Camarero
  */
 public class AudioTag extends BaseAudioTag {
-
-	@Override
-
-	protected void setAttributes(HttpServletRequest request) {
-		super.setAttributes(request);
-
-		String id = getId();
-
-		if (Validator.isNull(id)) {
-			id = StringUtil.randomId();
-		}
-
-		setId(id);
-
-		setNamespacedAttribute(request, "id", id);
-	}
 }
