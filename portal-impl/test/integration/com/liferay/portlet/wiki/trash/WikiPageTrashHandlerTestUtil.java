cc21357a7f7746480fcd15b2fa07fc2668b96d0b
Roberto Díaz <roberto.diaz@liferay.com>
2014-09-19 10:23:58 +0200

    LPS-49773 [COPY-PASTE- NO CHANGES] Create a new Util class to include common methods (Based on WPTHT)

---
+++ portal-impl/test/integration/com/liferay/portlet/wiki/trash/WikiPageTrashHandlerTestUtil.java
@@ -0,0 +1,262 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.wiki.trash;
+
+import com.liferay.portal.kernel.test.ExecutionTestListeners;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.model.BaseModel;
+import com.liferay.portal.model.ClassedModel;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.test.Sync;
+import com.liferay.portal.test.SynchronousDestinationExecutionTestListener;
+import com.liferay.portal.test.listeners.MainServletExecutionTestListener;
+import com.liferay.portal.test.runners.LiferayIntegrationJUnitTestRunner;
+import com.liferay.portal.util.test.RandomTestUtil;
+import com.liferay.portal.util.test.TestPropsValues;
+import com.liferay.portlet.trash.BaseTrashHandlerTestCase;
+import com.liferay.portlet.trash.util.TrashUtil;
+import com.liferay.portlet.wiki.asset.WikiPageAssetRenderer;
+import com.liferay.portlet.wiki.model.WikiNode;
+import com.liferay.portlet.wiki.model.WikiPage;
+import com.liferay.portlet.wiki.service.WikiNodeLocalServiceUtil;
+import com.liferay.portlet.wiki.service.WikiPageLocalServiceUtil;
+import com.liferay.portlet.wiki.util.test.WikiTestUtil;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Eudaldo Alonso
+ * @author Roberto Díaz
+ */
+@ExecutionTestListeners(listeners = {
+	MainServletExecutionTestListener.class,
+	SynchronousDestinationExecutionTestListener.class
+})
+@RunWith(LiferayIntegrationJUnitTestRunner.class)
+@Sync
+public class WikiPageTrashHandlerTest extends BaseTrashHandlerTestCase {
+
+	@Ignore()
+	@Override
+	@Test
+	public void testTrashAndDeleteDraft() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testTrashAndRestoreDraft() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testTrashGrandparentBaseModelAndRestoreParentModel()
+		throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testTrashIsRestorableBaseModel() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testTrashIsRestorableBaseModelWithParent1() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testTrashIsRestorableBaseModelWithParent2() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testTrashIsRestorableBaseModelWithParent3() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testTrashIsRestorableBaseModelWithParent4() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testTrashMoveBaseModel() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testTrashMyBaseModel() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testTrashRecentBaseModel() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testTrashVersionParentBaseModel() throws Exception {
+	}
+
+	@Ignore()
+	@Override
+	@Test
+	public void testTrashVersionParentBaseModelAndRestore() throws Exception {
+	}
+
+	@Override
+	protected BaseModel<?> addBaseModelWithWorkflow(
+			BaseModel<?> parentBaseModel, boolean approved,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		serviceContext = (ServiceContext)serviceContext.clone();
+
+		serviceContext.setWorkflowAction(WorkflowConstants.ACTION_SAVE_DRAFT);
+
+		return WikiTestUtil.addPage(
+			TestPropsValues.getUserId(), serviceContext.getScopeGroupId(),
+			(Long)parentBaseModel.getPrimaryKeyObj(), getSearchKeywords(),
+			approved);
+	}
+
+	@Override
+	protected Long getAssetClassPK(ClassedModel classedModel) {
+		return WikiPageAssetRenderer.getClassPK((WikiPage)classedModel);
+	}
+
+	@Override
+	protected BaseModel<?> getBaseModel(long primaryKey) throws Exception {
+		return WikiPageLocalServiceUtil.getPageByPageId(primaryKey);
+	}
+
+	@Override
+	protected Class<?> getBaseModelClass() {
+		return WikiPage.class;
+	}
+
+	@Override
+	protected String getBaseModelName(ClassedModel classedModel) {
+		WikiPage page = (WikiPage)classedModel;
+
+		return page.getTitle();
+	}
+
+	@Override
+	protected int getNotInTrashBaseModelsCount(BaseModel<?> parentBaseModel)
+		throws Exception {
+
+		return WikiPageLocalServiceUtil.getPagesCount(
+			(Long)parentBaseModel.getPrimaryKeyObj(), true,
+			WorkflowConstants.STATUS_ANY);
+	}
+
+	@Override
+	protected BaseModel<?> getParentBaseModel(
+			Group group, ServiceContext serviceContext)
+		throws Exception {
+
+		serviceContext = (ServiceContext)serviceContext.clone();
+
+		serviceContext.setWorkflowAction(WorkflowConstants.STATUS_APPROVED);
+
+		return WikiNodeLocalServiceUtil.addNode(
+			TestPropsValues.getUserId(),
+			RandomTestUtil.randomString(_NODE_NAME_MAX_LENGTH),
+			RandomTestUtil.randomString(), serviceContext);
+	}
+
+	@Override
+	protected Class<?> getParentBaseModelClass() {
+		return WikiNode.class;
+	}
+
+	@Override
+	protected String getSearchKeywords() {
+		return _PAGE_TITLE;
+	}
+
+	@Override
+	protected long getTrashEntryClassPK(ClassedModel classedModel) {
+		WikiPage page = (WikiPage)classedModel;
+
+		return page.getResourcePrimKey();
+	}
+
+	@Override
+	protected String getUniqueTitle(BaseModel<?> baseModel) {
+		WikiPage page = (WikiPage)baseModel;
+
+		String title = page.getTitle();
+
+		return TrashUtil.getOriginalTitle(title);
+	}
+
+	@Override
+	protected boolean isBaseModelMoveableFromTrash() {
+		return false;
+	}
+
+	@Override
+	protected void moveBaseModelToTrash(long primaryKey) throws Exception {
+		WikiPage page = WikiPageLocalServiceUtil.getPageByPageId(primaryKey);
+
+		WikiPageLocalServiceUtil.movePageToTrash(
+			TestPropsValues.getUserId(), page.getNodeId(), page.getTitle());
+	}
+
+	@Override
+	protected void moveParentBaseModelToTrash(long primaryKey)
+		throws Exception {
+
+		WikiNodeLocalServiceUtil.moveNodeToTrash(
+			TestPropsValues.getUserId(), primaryKey);
+	}
+
+	@Override
+	protected BaseModel<?> updateBaseModel(
+			long primaryKey, ServiceContext serviceContext)
+		throws Exception {
+
+		WikiPage page = WikiPageLocalServiceUtil.getPageByPageId(primaryKey);
+
+		serviceContext = (ServiceContext)serviceContext.clone();
+
+		return WikiPageLocalServiceUtil.updatePage(
+			TestPropsValues.getUserId(), page.getNodeId(), page.getTitle(),
+			page.getVersion(), RandomTestUtil.randomString(),
+			RandomTestUtil.randomString(), false, page.getFormat(),
+			page.getParentTitle(), page.getRedirectTitle(), serviceContext);
+	}
+
+	private static final int _NODE_NAME_MAX_LENGTH = 75;
+
+	private static final String _PAGE_TITLE = RandomTestUtil.randomString(255);
+
+}
