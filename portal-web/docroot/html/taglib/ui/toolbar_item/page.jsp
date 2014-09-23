465e13640f59456b625acb63cf6b58e6b157c2f1
Iván Zaera Avellón <ivan.zaera@liferay.com>
2014-09-23 12:42:45 +0200

    LPS-49277 Create a tag lib to render ToolbarItem instances

---
+++ portal-web/docroot/html/taglib/ui/toolbar_item/page.jsp
@@ -0,0 +1,61 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/html/taglib/ui/toolbar_item/init.jsp" %>
+
+<%
+ToolbarItem toolbarItem = (ToolbarItem)request.getAttribute("liferay-ui:toolbar_item:toolbarItem");
+String var = (String)request.getAttribute("liferay-ui:toolbar_item:var");
+%>
+
+<c:choose>
+	<c:when test="<%= toolbarItem instanceof JavascriptToolbarItem %>">
+
+		<%
+		JavascriptToolbarItem javascriptToolbarItem = (JavascriptToolbarItem)toolbarItem;
+		%>
+
+		<%= var %>.push(
+			{
+				icon: '<%= javascriptToolbarItem.getIcon() %>',
+				label: '<%= javascriptToolbarItem.getLabel() %>',
+				on: {
+					click: function(event) {
+						<%= javascriptToolbarItem.getOnClick() %>
+					}
+				}
+			}
+		);
+	</c:when>
+	<c:when test="<%= toolbarItem instanceof URLToolbarItem %>">
+
+		<%
+		URLToolbarItem urlToolbarItem = (URLToolbarItem)toolbarItem;
+		%>
+
+		<%= var %>.push(
+			{
+				icon: '<%= urlToolbarItem.getIcon() %>',
+				label: '<%= urlToolbarItem.getLabel() %>',
+				on: {
+					click: function(event) {
+						window.open('<%= urlToolbarItem.getURL() %>', '<%= urlToolbarItem.getTarget() %>');
+					}
+				}
+			}
+		);
+	</c:when>
+</c:choose>
---
+++ util-taglib/src/com/liferay/taglib/ui/ToolbarItemTag.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.taglib.ui;
+
+import com.liferay.portal.kernel.servlet.taglib.ui.ToolbarItem;
+import com.liferay.taglib.util.IncludeTag;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Ivan Zaera
+ */
+public class ToolbarItemTag extends IncludeTag {
+
+	@Override
+	public int doStartTag() {
+		return EVAL_BODY_INCLUDE;
+	}
+
+	public void setToolbarItem(ToolbarItem toolbarItem) {
+		_toolbarItem = toolbarItem;
+	}
+
+	public void setVar(String var) {
+		_var = var;
+	}
+
+	@Override
+	protected void cleanUp() {
+		_toolbarItem = null;
+	}
+
+	@Override
+	protected String getPage() {
+		return _PAGE;
+	}
+
+	@Override
+	protected boolean isCleanUpSetAttributes() {
+		return _CLEAN_UP_SET_ATTRIBUTES;
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest request) {
+		request.setAttribute(
+			"liferay-ui:toolbar_item:toolbarItem", _toolbarItem);
+		request.setAttribute("liferay-ui:toolbar_item:var", _var);
+	}
+
+	private static final boolean _CLEAN_UP_SET_ATTRIBUTES = true;
+
+	private static final String _PAGE = "/html/taglib/ui/toolbar_item/page.jsp";
+
+	private ToolbarItem _toolbarItem;
+	private String _var;
+
+}
