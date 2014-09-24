ca6317c71579fd1961d7cf15107d8f457a703ca4
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2014-09-24 16:28:20 +0200

    LPS-47464 SF

--- portal-impl/test/integration/com/liferay/portal/service/permission/BasePermissionTestCase.java
+++ portal-impl/test/integration/com/liferay/portal/service/permission/BasePermissionTestCase.java
@@ -92,8 +92,9 @@ public abstract class BasePermissionTestCase {
 			ResourceConstants.SCOPE_GROUP, String.valueOf(group.getGroupId()),
 			ActionKeys.VIEW);
 
-		RoleTestUtil.removeResourcePermission(RoleConstants.GUEST,
-			getResourceName(), ResourceConstants.SCOPE_INDIVIDUAL,
+		RoleTestUtil.removeResourcePermission(
+			RoleConstants.GUEST, getResourceName(),
+			ResourceConstants.SCOPE_INDIVIDUAL,
 			String.valueOf(group.getGroupId()), ActionKeys.VIEW);
 	}
 
