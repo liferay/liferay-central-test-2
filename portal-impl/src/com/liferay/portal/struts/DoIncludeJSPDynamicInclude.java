cf91beee60931d47fe0a460e11a5f13bffb723f3
Raymond Auge <raymond.auge@liferay.com>
2014-09-25 13:54:27 -0400

    LPS-50365 re-implement as a dynamic include

---
+++ portal-impl/src/com/liferay/portal/struts/DoIncludeJSPDynamicInclude.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.portal.struts;
+
+import com.liferay.kernel.servlet.taglib.DynamicInclude;
+import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.theme.ThemeDisplay;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.taglib.tiles.ComponentConstants;
+import org.apache.struts.tiles.ComponentContext;
+
+/**
+ * @author Raymond Augé
+ */
+@OSGiBeanProperties(
+	property = {"key=com.liferay.taglib.util.ThemeUtil#doIncludeJSP"}
+)
+public class DoIncludeJSPDynamicInclude implements DynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest request, HttpServletResponse response)
+		throws IOException {
+
+		ComponentContext componentContext =
+			(ComponentContext)request.getAttribute(
+				ComponentConstants.COMPONENT_CONTEXT);
+
+		if (componentContext == null) {
+			return;
+		}
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		String tilesTitle = (String)componentContext.getAttribute("title");
+		String tilesContent = (String)componentContext.getAttribute("content");
+		boolean tilesSelectable = GetterUtil.getBoolean(
+			(String)componentContext.getAttribute("selectable"));
+
+		themeDisplay.setTilesTitle(tilesTitle);
+		themeDisplay.setTilesContent(tilesContent);
+		themeDisplay.setTilesSelectable(tilesSelectable);
+	}
+
+}
