f317f0f91d406ec6ab43cac02d78e43b0be284d4
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2014-09-26 16:00:55 +0200

    LPS-50319 Rebuild - no change

--- portal-impl/src/com/liferay/portlet/announcements/service/persistence/impl/AnnouncementsEntryPersistenceImpl.java
+++ portal-impl/src/com/liferay/portlet/announcements/service/persistence/impl/AnnouncementsEntryPersistenceImpl.java
@@ -25,8 +25,14 @@ import com.liferay.portal.kernel.dao.orm.QueryPos;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.dao.orm.SQLQuery;
 import com.liferay.portal.kernel.dao.orm.Session;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.sanitizer.Sanitizer;
+import com.liferay.portal.kernel.sanitizer.SanitizerException;
+import com.liferay.portal.kernel.sanitizer.SanitizerUtil;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.kernel.util.StringBundler;
@@ -34,6 +40,7 @@ import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.uuid.PortalUUIDUtil;
 import com.liferay.portal.model.CacheModel;
+import com.liferay.portal.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.security.permission.InlineSQLHelperUtil;
 import com.liferay.portal.service.persistence.impl.BasePersistenceImpl;
 
@@ -4913,6 +4920,31 @@ public class AnnouncementsEntryPersistenceImpl extends BasePersistenceImpl<Annou
 			String uuid = PortalUUIDUtil.generate();
 
 			announcementsEntry.setUuid(uuid);
+		}
+
+		long userId = GetterUtil.getLong(PrincipalThreadLocal.getName());
+
+		if (userId > 0) {
+			long companyId = announcementsEntry.getCompanyId();
+
+			long groupId = 0;
+
+			long entryId = 0;
+
+			if (!isNew) {
+				entryId = announcementsEntry.getPrimaryKey();
+			}
+
+			try {
+				announcementsEntry.setContent(SanitizerUtil.sanitize(
+						companyId, groupId, userId,
+						com.liferay.portlet.announcements.model.AnnouncementsEntry.class.getName(),
+						entryId, ContentTypes.TEXT_HTML, Sanitizer.MODE_ALL,
+						announcementsEntry.getContent(), null));
+			}
+			catch (SanitizerException se) {
+				throw new SystemException(se);
+			}
 		}
 
 		Session session = null;
