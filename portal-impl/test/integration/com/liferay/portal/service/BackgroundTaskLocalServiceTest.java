mdelapenya/liferay-portal

LPS-45869 Change test, as taskContext is a Map now

portal-impl/test/integration/com/liferay/portal/service/BackgroundTaskLocalServiceTest.java
@@ -184,9 +184,7 @@ public class BackgroundTaskLocalServiceTest {
 
 		Map<String, Serializable> taskContextMap = getTaskContextMap();
 
-		String taskContext = JSONFactoryUtil.serialize(taskContextMap);
-
-		backgroundTaskImpl.setTaskContext(taskContext);
+		backgroundTaskImpl.setTaskContext(taskContextMap);
 
 		BackgroundTask backgroundTask =
 			BackgroundTaskLocalServiceUtil.addBackgroundTask(

liferay/liferay-central-test-2

portal-impl/test/integration/com/liferay/portal/service/BackgroundTaskLocalServiceTest.java
@@ -15,7 +15,6 @@
 package com.liferay.portal.service;
 
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskConstants;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.messaging.Destination;
 import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.MessageBus;
@@ -184,9 +183,7 @@ public class BackgroundTaskLocalServiceTest {
 
 		Map<String, Serializable> taskContextMap = getTaskContextMap();
 
-		String taskContext = JSONFactoryUtil.serialize(taskContextMap);
-
-		backgroundTaskImpl.setTaskContext(taskContext);
+		backgroundTaskImpl.setTaskContextMap(taskContextMap);
 
 		BackgroundTask backgroundTask =
 			BackgroundTaskLocalServiceUtil.addBackgroundTask(