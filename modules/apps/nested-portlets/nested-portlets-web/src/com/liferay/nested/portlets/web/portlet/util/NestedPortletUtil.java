cbbbfa18522ff08e0d3b795673881883ae58f0e2
peterfellwock <peter.fellwock@liferay.com>
2014-10-02 15:51:07 -0700

    LPS-50418 Centralize functionality

--- modules/apps/nested-portlets/nested-portlets-web/src/com/liferay/nested/portlets/web/portlet/util/NestedPortletUtil.java
+++ modules/apps/nested-portlets/nested-portlets-web/src/com/liferay/nested/portlets/web/portlet/util/NestedPortletUtil.java
@@ -12,27 +12,27 @@ import javax.portlet.RenderResponse;
  * @author Peter Fellwock
  */
 public class NestedPortletUtil {
-	
-	public static void main(String[] args) {
-	}
-	
-	public static void defineObjects(PortletConfig portletConfig, RenderResponse renderResponse, RenderRequest renderRequest){
-		
+
+	public static void defineObjects(
+		PortletConfig portletConfig, RenderResponse renderResponse,
+		RenderRequest renderRequest) {
+
 		RenderRequestImpl renderRequestImpl = (RenderRequestImpl)renderRequest;
 
 		renderRequestImpl.defineObjects(portletConfig, renderResponse);
 	}
 
 	public static String getLayoutTemplateId(PortletPreferences preferences) {
-		
 		String layoutTemplateIdDefault = preferences.getValue(
 			"nested.portlets.layout.template.default", StringPool.BLANK);
-		
+
 		String LayoutTemplateId = preferences.getValue(
 			"layoutTemplateId", layoutTemplateIdDefault);
-		
+
 		return LayoutTemplateId;
-		
+	}
+
+	public static void main(String[] args) {
 	}
 
 }
