4e1f3d4269a2905a7838e46c11ce15c2e3a73515
peterfellwock <peter.fellwock@liferay.com>
2014-10-02 14:02:59 -0700

    LPS-50418 Using util for when jsps imports dont work

---
+++ modules/apps/nested-portlets/nested-portlets-web/src/com/liferay/nested/portlets/web/portlet/util/NestedPortletUtil.java
@@ -0,0 +1,38 @@
+package com.liferay.nested.portlets.web.portlet.util;
+
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portlet.RenderRequestImpl;
+
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletPreferences;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Peter Fellwock
+ */
+public class NestedPortletUtil {
+	
+	public static void main(String[] args) {
+	}
+	
+	public static void defineObjects(PortletConfig portletConfig, RenderResponse renderResponse, RenderRequest renderRequest){
+		
+		RenderRequestImpl renderRequestImpl = (RenderRequestImpl)renderRequest;
+
+		renderRequestImpl.defineObjects(portletConfig, renderResponse);
+	}
+
+	public static String getLayoutTemplateId(PortletPreferences preferences) {
+		
+		String layoutTemplateIdDefault = preferences.getValue(
+			"nested.portlets.layout.template.default", StringPool.BLANK);
+		
+		String LayoutTemplateId = preferences.getValue(
+			"layoutTemplateId", layoutTemplateIdDefault);
+		
+		return LayoutTemplateId;
+		
+	}
+
+}
