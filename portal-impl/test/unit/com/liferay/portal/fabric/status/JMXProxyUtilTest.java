1fa4b1c7750a6564fe94fc871ad8768b27a4f8e0
Cristina González Castellano <cristina.gonzalez@liferay.com>
2014-10-06 09:25:50 +0200

    LPS-50564 Auto SF

--- portal-impl/test/unit/com/liferay/portal/fabric/status/JMXProxyUtilTest.java
+++ portal-impl/test/unit/com/liferay/portal/fabric/status/JMXProxyUtilTest.java
@@ -339,7 +339,6 @@ public class JMXProxyUtilTest {
 			public boolean equals(Object object) {
 				return super.equals(object);
 			}
-
 		}
 
 		Assert.assertFalse(
@@ -360,7 +359,6 @@ public class JMXProxyUtilTest {
 			public int hashCode() {
 				return super.hashCode();
 			}
-
 		}
 
 		Assert.assertFalse(
@@ -381,7 +379,6 @@ public class JMXProxyUtilTest {
 			public String toString() {
 				return super.toString();
 			}
-
 		}
 
 		Assert.assertFalse(
@@ -400,7 +397,6 @@ public class JMXProxyUtilTest {
 			@SuppressWarnings("unused")
 			public void method2() {
 			}
-
 		}
 
 		Assert.assertTrue(
