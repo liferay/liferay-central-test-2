fa96ea86f3daf69fe36ca785d9aa22fd48870890
Bruno Basto <bruno.basto@liferay.com>
2014-10-06 19:25:04 -0300

    LPS-48873 We need the file title in the front-end

--- portal-impl/src/com/liferay/portlet/journal/util/JournalConverterImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/util/JournalConverterImpl.java
@@ -377,6 +377,7 @@ public class JournalConverterImpl implements JournalConverter {
 			JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
 
 			jsonObject.put("groupId", fileEntry.getGroupId());
+			jsonObject.put("title", fileEntry.getTitle());
 			jsonObject.put("uuid", fileEntry.getUuid());
 			jsonObject.put("version", fileEntry.getVersion());
 
