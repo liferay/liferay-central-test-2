c789e0804695f1c701e37d8003cad001a49dad32
Roberto Díaz <roberto.diaz@liferay.com>
2014-10-08 17:39:23 +0200

    LPS-50642 Make the test clearer avoiding nested loops

--- portal-impl/test/integration/com/liferay/portlet/documentlibrary/service/DLAppServiceTest.java
+++ portal-impl/test/integration/com/liferay/portlet/documentlibrary/service/DLAppServiceTest.java
@@ -232,46 +232,25 @@ public class DLAppServiceTest extends BaseDLAppTestCase {
 
 			_fileEntryIds = new long[_users.length];
 
-			for (int i = 0; i < 2; i++) {
-				for (int j = 0; j < doAsUserThreads.length; j++) {
-					if (i == 0) {
-						doAsUserThreads[j] = new AddFileEntryThread(
-							_users[j].getUserId(), j);
-					}
-					else {
-						doAsUserThreads[j] = new GetFileEntryThread(
-							_users[j].getUserId(), j);
-					}
-				}
+			int successCount = 0;
 
-				for (DoAsUserThread doAsUserThread : doAsUserThreads) {
-					doAsUserThread.start();
-				}
-
-				for (DoAsUserThread doAsUserThread : doAsUserThreads) {
-					doAsUserThread.join();
-				}
-
-				int successCount = 0;
-
-				for (DoAsUserThread doAsUserThread : doAsUserThreads) {
-					if (doAsUserThread.isSuccess()) {
-						successCount++;
-					}
-				}
-
-				String message =
-					"Only " + successCount + " out of " + _users.length;
-
-				if (i == 0) {
-					message += " threads added file entries successfully";
-				}
-				else {
-					message += " threads retrieved file entries successfully";
-				}
-
-				Assert.assertTrue(message, successCount == _users.length);
+			for (int i = 0; i < doAsUserThreads.length; i++) {
+				doAsUserThreads[i] = new AddFileEntryThread(
+					_users[i].getUserId(), i);
 			}
+
+			successCount = runUserThreads(doAsUserThreads);
+
+			Assert.assertTrue(successCount == _users.length);
+
+			for (int i = 0; i < doAsUserThreads.length; i++) {
+				doAsUserThreads[i] = new GetFileEntryThread(
+					_users[i].getUserId(), i);
+			}
+
+			successCount = runUserThreads(doAsUserThreads);
+
+			Assert.assertTrue(successCount == _users.length);
 		}
 
 		@Test
@@ -1090,6 +1069,28 @@ public class DLAppServiceTest extends BaseDLAppTestCase {
 			});
 
 		return counter;
+	}
+
+	protected static int runUserThreads(DoAsUserThread[] doAsUserThreads)
+		throws Exception {
+
+		for (DoAsUserThread doAsUserThread : doAsUserThreads) {
+			doAsUserThread.start();
+		}
+
+		for (DoAsUserThread doAsUserThread : doAsUserThreads) {
+			doAsUserThread.join();
+		}
+
+		int successCount = 0;
+
+		for (DoAsUserThread doAsUserThread : doAsUserThreads) {
+			if (doAsUserThread.isSuccess()) {
+				successCount++;
+			}
+		}
+
+		return successCount;
 	}
 
 	protected static void search(
