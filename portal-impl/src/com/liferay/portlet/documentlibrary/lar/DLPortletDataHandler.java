7c3796de9d8445bd966c36ba4e44c9422be7af1b
Máté Thurzó <mate.thurzo@liferay.com>
2014-10-10 17:13:25 +0200

    LPS-50554 Consider the proper statuses for file entries, when querying them up during the import

--- portal-impl/src/com/liferay/portlet/documentlibrary/lar/DLPortletDataHandler.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/lar/DLPortletDataHandler.java
@@ -17,7 +17,9 @@ package com.liferay.portlet.documentlibrary.lar;
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.Disjunction;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.ExportActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.ProjectionFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.Property;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.QueryDefinition;
@@ -29,6 +31,8 @@ import com.liferay.portal.kernel.lar.ManifestSummary;
 import com.liferay.portal.kernel.lar.PortletDataContext;
 import com.liferay.portal.kernel.lar.PortletDataHandlerBoolean;
 import com.liferay.portal.kernel.lar.PortletDataHandlerControl;
+import com.liferay.portal.kernel.lar.StagedModelDataHandler;
+import com.liferay.portal.kernel.lar.StagedModelDataHandlerRegistryUtil;
 import com.liferay.portal.kernel.lar.StagedModelDataHandlerUtil;
 import com.liferay.portal.kernel.lar.StagedModelType;
 import com.liferay.portal.kernel.lar.xstream.XStreamAliasRegistryUtil;
@@ -39,6 +43,7 @@ import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.kernel.xml.Element;
@@ -57,6 +62,7 @@ import com.liferay.portlet.documentlibrary.model.DLFileEntryConstants;
 import com.liferay.portlet.documentlibrary.model.DLFileEntryType;
 import com.liferay.portlet.documentlibrary.model.DLFileRank;
 import com.liferay.portlet.documentlibrary.model.DLFileShortcut;
+import com.liferay.portlet.documentlibrary.model.DLFileVersion;
 import com.liferay.portlet.documentlibrary.model.DLFolder;
 import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
 import com.liferay.portlet.documentlibrary.model.impl.DLFileEntryImpl;
@@ -484,6 +490,40 @@ public class DLPortletDataHandler extends BasePortletDataHandler {
 
 					dynamicQuery.add(
 						property.eq(portletDataContext.getScopeGroupId()));
+
+					DynamicQuery fileVersionDynamicQuery =
+						DynamicQueryFactoryUtil.forClass(
+							DLFileVersion.class, "fileVersion",
+							PortalClassLoaderUtil.getClassLoader());
+
+					fileVersionDynamicQuery.setProjection(
+						ProjectionFactoryUtil.property("fileEntryId"));
+
+					Property statusProperty = PropertyFactoryUtil.forName(
+						"status");
+
+					StagedModelDataHandler<?> stagedModelDataHandler =
+						StagedModelDataHandlerRegistryUtil.
+							getStagedModelDataHandler(
+								DLFileEntry.class.getName());
+
+					fileVersionDynamicQuery.add(
+						statusProperty.in(
+							stagedModelDataHandler.getExportableStatuses()));
+
+					fileVersionDynamicQuery.add(
+						RestrictionsFactoryUtil.eqProperty(
+							"fileVersion.version", "version"));
+
+					fileVersionDynamicQuery.add(
+						RestrictionsFactoryUtil.eqProperty(
+							"fileVersion.fileEntryId", "fileEntryId"));
+
+					Property fileEntryIdProperty = PropertyFactoryUtil.forName(
+						"fileEntryId");
+
+					dynamicQuery.add(
+						fileEntryIdProperty.in(fileVersionDynamicQuery));
 				}
 
 			});
