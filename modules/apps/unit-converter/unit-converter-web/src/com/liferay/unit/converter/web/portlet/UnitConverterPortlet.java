c3b8fd72153aad35b4111c317c14f2a05b45b060
peterfellwock <peter.fellwock@liferay.com>
2014-10-14 15:52:42 -0700

    LPS-50848 New Portlet Class

---
+++ modules/apps/unit-converter/unit-converter-web/src/com/liferay/unit/converter/web/portlet/UnitConverterPortlet.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.unit.converter.web.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.unit.converter.web.upgrade.UnitConverterUpgrade;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+* @author Raymond Augé
+* @author Peter Fellwock
+*/
+@Component(
+	property = {
+		"com.liferay.portlet.css-class-wrapper=portlet-unit-converter",
+		"com.liferay.portlet.display-category=category.tools",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.remoteable=true",
+		"com.liferay.portlet.struts-path=unit_converter",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Unit Converter",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class UnitConverterPortlet extends MVCPortlet {
+
+	@Reference(unbind = "-")
+	protected void setUnitConverterUpgrade(
+		UnitConverterUpgrade unitConvertersUpgrade) {
+	}
+
+}
