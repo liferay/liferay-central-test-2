8cbee41e6ce176cfae42a1ca72e261712f0b9b05
Brian Chan <brian.chan@liferay.com>
2014-10-14 16:24:01 -0700

    LPS-50834 SF

--- portal-impl/test/unit/com/liferay/portlet/dynamicdatamapping/util/DDMFormValuesToFieldsConverterTest.java
+++ portal-impl/test/unit/com/liferay/portlet/dynamicdatamapping/util/DDMFormValuesToFieldsConverterTest.java
@@ -97,7 +97,7 @@ public class DDMFormValuesToFieldsConverterTest extends BaseDDMTestCase {
 		Class<?> clazz = value.getClass();
 
 		Assert.assertEquals(true, clazz.isAssignableFrom(Boolean.class));
-		Assert.assertEquals(true, (Boolean)value);
+		Assert.assertEquals(true, value);
 
 		Field fieldsDisplayField = fields.get(DDMImpl.FIELDS_DISPLAY_NAME);
 
