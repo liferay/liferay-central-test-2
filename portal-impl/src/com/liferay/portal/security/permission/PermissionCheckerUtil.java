9da6cae5cc16674524ccc76e871fef134954a475
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2014-10-16 22:01:25 +0200

    LPS-50938 Define the resource permission checkers as beans and use the registry to track the new abstractions

--- portal-impl/src/com/liferay/portal/security/permission/PermissionCheckerUtil.java
+++ portal-impl/src/com/liferay/portal/security/permission/PermissionCheckerUtil.java
@@ -17,32 +17,11 @@ package com.liferay.portal.security.permission;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.model.User;
 import com.liferay.portal.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.util.PropsValues;
-import com.liferay.portlet.blogs.model.BlogsEntry;
-import com.liferay.portlet.blogs.service.permission.BlogsPermission;
-import com.liferay.portlet.bookmarks.model.BookmarksEntry;
-import com.liferay.portlet.bookmarks.model.BookmarksFolder;
-import com.liferay.portlet.bookmarks.service.permission.BookmarksEntryPermission;
-import com.liferay.portlet.bookmarks.service.permission.BookmarksPermission;
-import com.liferay.portlet.documentlibrary.model.DLFileEntry;
-import com.liferay.portlet.documentlibrary.service.permission.DLFileEntryPermission;
-import com.liferay.portlet.documentlibrary.service.permission.DLPermission;
-import com.liferay.portlet.journal.model.JournalArticle;
-import com.liferay.portlet.journal.model.JournalFolder;
-import com.liferay.portlet.journal.service.permission.JournalArticlePermission;
-import com.liferay.portlet.journal.service.permission.JournalPermission;
-import com.liferay.portlet.messageboards.model.MBCategory;
-import com.liferay.portlet.messageboards.model.MBMessage;
-import com.liferay.portlet.messageboards.model.MBThread;
-import com.liferay.portlet.messageboards.service.permission.MBMessagePermission;
-import com.liferay.portlet.messageboards.service.permission.MBPermission;
-import com.liferay.portlet.wiki.model.WikiNode;
-import com.liferay.portlet.wiki.model.WikiPage;
-import com.liferay.portlet.wiki.service.permission.WikiNodePermission;
-import com.liferay.portlet.wiki.service.permission.WikiPagePermission;
+import com.liferay.registry.collections.ServiceTrackerCollections;
+import com.liferay.registry.collections.ServiceTrackerMap;
 
 /**
  * @author Brian Wing Shun Chan
@@ -54,51 +33,14 @@ public class PermissionCheckerUtil {
 			String actionId)
 		throws PortalException {
 
-		if (className.equals(BlogsEntry.class.getName())) {
-			return BlogsPermission.contains(
-				permissionChecker, classPK, actionId);
-		}
-		else if (className.equals(BookmarksEntry.class.getName())) {
-			return BookmarksEntryPermission.contains(
-				permissionChecker, classPK, actionId);
-		}
-		else if (className.equals(BookmarksFolder.class.getName())) {
-			return BookmarksPermission.contains(
-				permissionChecker, classPK, actionId);
-		}
-		else if (className.equals(DLFileEntry.class.getName())) {
-			return DLFileEntryPermission.contains(
-				permissionChecker, classPK, actionId);
-		}
-		else if (className.equals(Folder.class.getName())) {
-			return DLPermission.contains(permissionChecker, classPK, actionId);
-		}
-		else if (className.equals(JournalArticle.class.getName())) {
-			return JournalArticlePermission.contains(
-				permissionChecker, classPK, actionId);
-		}
-		else if (className.equals(JournalFolder.class.getName())) {
-			return JournalPermission.contains(
-				permissionChecker, classPK, actionId);
-		}
-		else if (className.equals(MBCategory.class.getName())) {
-			return MBPermission.contains(permissionChecker, classPK, actionId);
-		}
-		else if (className.equals(MBMessage.class.getName())) {
-			return MBMessagePermission.contains(
-				permissionChecker, classPK, actionId);
-		}
-		else if (className.equals(MBThread.class.getName())) {
-			return MBMessagePermission.contains(
-				permissionChecker, classPK, actionId);
-		}
-		else if (className.equals(WikiNode.class.getName())) {
-			return WikiNodePermission.contains(
-				permissionChecker, classPK, actionId);
-		}
-		else if (className.equals(WikiPage.class.getName())) {
-			return WikiPagePermission.contains(
-				permissionChecker, classPK, actionId);
+		ResourcePermissionChecker resourcePermissionChecker =
+			_serviceTrackerMap.getService(className);
+
+		Boolean resource = resourcePermissionChecker.checkResource(
+			permissionChecker, classPK, actionId);
+
+		if (resource != null) {
+			resource.booleanValue();
 		}
 
 		return null;
@@ -139,5 +81,9 @@ public class PermissionCheckerUtil {
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		PermissionCheckerUtil.class);
+
+	private static final ServiceTrackerMap<String, ResourcePermissionChecker>
+		_serviceTrackerMap = ServiceTrackerCollections.singleValueMap(
+			ResourcePermissionChecker.class, "model.class.name");
 
 }
