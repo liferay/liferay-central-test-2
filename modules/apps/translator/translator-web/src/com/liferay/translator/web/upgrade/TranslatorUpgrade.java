aa15f24ec12c0a3b805c3d2c91a6a6315d41e0ab
peterfellwock <peter.fellwock@liferay.com>
2014-10-16 15:39:43 -0700

    LPS-50900 Migrate Portlet ID

---
+++ modules/apps/translator/translator-web/src/com/liferay/translator/web/upgrade/TranslatorUpgrade.java
@@ -0,0 +1,60 @@
+package com.liferay.translator.web.upgrade;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.portal.upgrade.util.UpgradePortletId;
+
+import java.util.Collections;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Raymond Augé
+ * @author Peter Fellwock
+ */
+@Component(
+	immediate = true, service = TranslatorUpgrade.class
+)
+public class TranslatorUpgrade {
+
+	@Reference(unbind = "-")
+	protected void setReleaseLocalService(
+		ReleaseLocalService releaseLocalService) {
+
+		_releaseLocalService = releaseLocalService;
+	}
+
+	@Reference(target = "(original.bean=true)", unbind = "-")
+	protected void setServletContext(ServletContext servletContext) {
+	}
+
+	@Activate
+	protected void upgrade() throws PortalException {
+		UpgradePortletId upgradePortletId = new UpgradePortletId() {
+
+			@Override
+			protected String[][] getRenamePortletIdsArray() {
+				return new String[][] {
+					new String[] {
+						"26",
+						"com_liferay_translator_web_portlet_TranslatorPortlet"
+					}
+				};
+			}
+
+		};
+
+		_releaseLocalService.updateRelease(
+			"com.liferay.translator.web",
+			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 0,
+			false);
+	}
+
+	private ReleaseLocalService _releaseLocalService;
+
+}
