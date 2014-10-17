32b9fecdd722215ee90852c86b2757c95b112108
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2014-10-16 17:53:30 -0700

    LPS-50945 Create _.sub method to replace A.Lang.sub

--- portal-web/docroot/html/js/lodash/util.js
+++ portal-web/docroot/html/js/lodash/util.js
@@ -1,3 +1,14 @@
 ;(function(_) {
-
+	_.mixin(
+		{
+			sub: function(string, data) {
+				return string.replace ? string.replace(
+					/\{\s*([^|}]+?)\s*(?:\|([^}]*))?\s*\}/g,
+					function (match, key) {
+						return _.isUndefined(data[key]) ? match : data[key];
+					}
+				) : string;
+			}
+		}
+	);
 })(_);
