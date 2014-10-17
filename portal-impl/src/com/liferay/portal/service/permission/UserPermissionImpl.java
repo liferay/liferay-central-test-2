689cf3171ee6a56651f6823cca5e0d5f56259395
John Zhao <john.zhao@liferay.com>
2014-10-17 14:46:33 +0800

    LPS-50953 Company admin can be viewed by other uses.

--- portal-impl/src/com/liferay/portal/service/permission/UserPermissionImpl.java
+++ portal-impl/src/com/liferay/portal/service/permission/UserPermissionImpl.java
@@ -128,7 +128,8 @@ public class UserPermissionImpl
 			if (userId != ResourceConstants.PRIMKEY_DNE) {
 				user = UserLocalServiceUtil.getUserById(userId);
 
-				if ((actionId.equals(ActionKeys.DELETE) ||
+				if ((actionId.equals(ActionKeys.VIEW) ||
+					 actionId.equals(ActionKeys.DELETE) ||
 					 actionId.equals(ActionKeys.IMPERSONATE) ||
 					 actionId.equals(ActionKeys.PERMISSIONS) ||
 					 actionId.equals(ActionKeys.UPDATE)) &&
