f835187b64e33bde4825ca65ca90281bd1429188
Roberto Díaz <roberto.diaz@liferay.com>
2014-10-20 17:27:21 +0200

    LPS-50998 Move JournalSearcher to the right package

--- portal-impl/src/com/liferay/portlet/journal/util/JournalSearcher.java
+++ portal-impl/src/com/liferay/portlet/journal/util/JournalSearcher.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portlet.documentlibrary.util;
+package com.liferay.portlet.journal.util;
 
 import com.liferay.portal.kernel.search.BaseSearcher;
 import com.liferay.portal.kernel.search.Field;
