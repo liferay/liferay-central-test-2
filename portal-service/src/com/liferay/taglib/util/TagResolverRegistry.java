c3497664b31daefab2be67761d1c00654e1ce6e3
Carlos Sierra <carlos.sierra@liferay.com>
2014-10-20 14:55:54 +0200

    LPS-47664 Added registry for resolvers

---
+++ portal-service/src/com/liferay/taglib/util/TagResolverRegistry.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.taglib.util;
+
+import com.liferay.registry.collections.ServiceTrackerCollections;
+import com.liferay.registry.collections.ServiceTrackerMap;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public class TagResolverRegistry {
+
+	public static TagKeyResolver getTagIdResolver(String target) {
+		return _instance._tagResolvers.getService(target);
+	}
+
+	private TagResolverRegistry() {
+		_tagResolvers.open();
+	}
+
+	private static final TagResolverRegistry _instance =
+		new TagResolverRegistry();
+
+	private final ServiceTrackerMap<String, TagKeyResolver>
+		_tagResolvers = ServiceTrackerCollections.singleValueMap(
+			TagKeyResolver.class, "target");
+
+}
