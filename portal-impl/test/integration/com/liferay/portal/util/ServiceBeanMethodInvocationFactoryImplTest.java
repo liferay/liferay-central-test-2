f8897037af1469a9ed97127f9f18bb0856becb8c
Brian Chan <brian.chan@liferay.com>
2014-10-19 17:21:14 -0700

    Revert "LPS-49294 Break lines > 80 chars"
    This reverts commit 76da10546ff6d55ef5221e073db518d9bba6807f.

--- portal-impl/test/integration/com/liferay/portal/util/ServiceBeanMethodInvocationFactoryImplTest.java
+++ portal-impl/test/integration/com/liferay/portal/util/ServiceBeanMethodInvocationFactoryImplTest.java
@@ -118,7 +118,6 @@ public class ServiceBeanMethodInvocationFactoryImplTest {
 	}
 
 	@DeleteAfterTestRun
-	private final Set<EmailAddress> _emailAddresses =
-		new HashSet<EmailAddress>();
+	private final Set<EmailAddress> _emailAddresses = new HashSet<EmailAddress>();
 
 }
