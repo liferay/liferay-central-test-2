293e0bbffc61863fd2020a2dbd9f735779a35412
Hugo Huijser <hugo.huijser@liferay.com>
2014-10-22 14:54:43 +0800

    LPS-49294 Logic not working when field type has annotations

--- portal-impl/src/com/liferay/portal/tools/sourceformatter/JavaSourceProcessor.java
+++ portal-impl/src/com/liferay/portal/tools/sourceformatter/JavaSourceProcessor.java
@@ -1999,9 +1999,20 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 
 		String[] lines = StringUtil.splitLines(content);
 
-		String line = lines[javaField.getLineNumber() - 1];
+		String line = null;
+		int lineNumber = javaField.getLineNumber() - 1;
 
-		lines[javaField.getLineNumber() - 1] = StringUtil.replace(
+		while (true) {
+			line = lines[lineNumber];
+
+			if (line.contains(oldFieldType)) {
+				break;
+			}
+
+			lineNumber++;
+		}
+
+		lines[lineNumber] = StringUtil.replace(
 			line, oldFieldType, newFieldType);
 
 		StringBundler sb = new StringBundler(2 * lines.length);
@@ -2013,9 +2024,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 
 		sb.setIndex(sb.index() - 1);
 
-		content = sb.toString();
-
-		return content;
+		return sb.toString();
 	}
 
 	protected String getCombinedLinesContent(
