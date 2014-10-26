peterborkuti/liferay-portal

LPS-50578 Changing language is impossible on pages like terms_of_use

portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
@@ -94,6 +94,7 @@ import com.liferay.portal.security.auth.EmailAddressGenerator;
 import com.liferay.portal.security.auth.EmailAddressValidator;
 import com.liferay.portal.security.auth.FullNameGenerator;
 import com.liferay.portal.security.auth.FullNameValidator;
+import com.liferay.portal.security.auth.InterruptedPortletRequestWhitelistUtil;
 import com.liferay.portal.security.auth.ScreenNameGenerator;
 import com.liferay.portal.security.auth.ScreenNameValidator;
 import com.liferay.portal.security.lang.DoPrivilegedBean;
@@ -2249,6 +2250,21 @@ public class HookHotDeployListener
 		}
 
 		if (containsKey(
+				portalProperties, INTERRUPTED_PORTLET_REQUEST_WHITELIST)) {
+
+		InterruptedPortletRequestWhitelistUtil.
+			resetPortletInvocationWhitelist();
+		}
+
+		if (containsKey(
+				portalProperties,
+				INTERRUPTED_PORTLET_REQUEST_WHITELIST_ACTIONS)) {
+
+			InterruptedPortletRequestWhitelistUtil.
+				resetPortletInvocationWhitelistActions();
+		}
+
+		if (containsKey(
 				portalProperties,
 				PORTLET_ADD_DEFAULT_RESOURCE_CHECK_WHITELIST)) {
 
@@ -2393,6 +2409,8 @@ public class HookHotDeployListener
 		"company.settings.form.identification",
 		"company.settings.form.miscellaneous", "company.settings.form.social",
 		"convert.processes", "dockbar.add.portlets", "journal.article.form.add",
+		"interrupted.portlet.request.whitelist",
+		"interrupted.portlet.request.whitelist.actions",
 		"journal.article.form.translate", "journal.article.form.update",
 		"layout.form.add", "layout.form.update", "layout.set.form.update",
 		"layout.static.portlets.all", "login.form.navigation.post",

portal-impl/src/com/liferay/portal/security/auth/InterruptedPortletRequestWhitelistImpl.java
@@ -1,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.auth;
+
+import com.liferay.portal.kernel.security.pacl.DoPrivileged;
+import com.liferay.portal.util.PropsValues;
+
+/**
+ * @author Peter Borkuti
+ */
+@DoPrivileged
+public class InterruptedPortletRequestWhitelistImpl
+	extends AbstractPortletRequestWhitelist {
+
+	@Override
+	public String[] getWhitelistActionsPropsValues() {
+		return PropsValues.INTERRUPTED_PORTLET_REQUEST_WHITELIST_ACTIONS;
+	}
+
+	@Override
+	public String[] getWhitelistPropsValues() {
+		return PropsValues.INTERRUPTED_PORTLET_REQUEST_WHITELIST;
+	}
+
+}

portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -826,6 +826,10 @@ public class PropsValues {
 
 	public static final boolean INDEX_WITH_THREAD = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.INDEX_WITH_THREAD));
 
+	public static String[] INTERRUPTED_PORTLET_REQUEST_WHITELIST = PropsUtil.getArray(PropsKeys.INTERRUPTED_PORTLET_REQUEST_WHITELIST);
+
+	public static String[] INTERRUPTED_PORTLET_REQUEST_WHITELIST_ACTIONS = PropsUtil.getArray(PropsKeys.INTERRUPTED_PORTLET_REQUEST_WHITELIST_ACTIONS);
+
 	public static final String INTRABAND_PROXY_DUMP_CLASSES_DIR = PropsUtil.get(PropsKeys.INTRABAND_PROXY_DUMP_CLASSES_DIR);
 
 	public static final boolean INTRABAND_PROXY_DUMP_CLASSES_ENABLED = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.INTRABAND_PROXY_DUMP_CLASSES_ENABLED));

portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -1103,6 +1103,10 @@ public interface PropsKeys {
 
 	public static final String INDEX_WITH_THREAD = "index.with.thread";
 
+	public static final String INTERRUPTED_PORTLET_REQUEST_WHITELIST = "interrupted.portlet.request.whitelist";
+
+	public static final String INTERRUPTED_PORTLET_REQUEST_WHITELIST_ACTIONS = "interrupted.portlet.request.whitelist.actions";
+
 	public static final String INTRABAND_IMPL = "intraband.impl";
 
 	public static final String INTRABAND_MAILBOX_REAPER_THREAD_ENABLED = "intraband.mailbox.reaper.thread.enabled";

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
@@ -94,6 +94,7 @@ import com.liferay.portal.security.auth.EmailAddressGenerator;
 import com.liferay.portal.security.auth.EmailAddressValidator;
 import com.liferay.portal.security.auth.FullNameGenerator;
 import com.liferay.portal.security.auth.FullNameValidator;
+import com.liferay.portal.security.auth.InterruptedPortletRequestWhitelistUtil;
 import com.liferay.portal.security.auth.ScreenNameGenerator;
 import com.liferay.portal.security.auth.ScreenNameValidator;
 import com.liferay.portal.security.lang.DoPrivilegedBean;
@@ -2262,6 +2263,21 @@ public class HookHotDeployListener
 			AuthTokenWhitelistUtil.resetPortletInvocationWhitelistActions();
 		}
 
+		if (containsKey(
+				portalProperties, PORTLET_INTERRUPTED_REQUEST_WHITELIST)) {
+
+			InterruptedPortletRequestWhitelistUtil.
+				resetPortletInvocationWhitelist();
+		}
+
+		if (containsKey(
+				portalProperties,
+				PORTLET_INTERRUPTED_REQUEST_WHITELIST_ACTIONS)) {
+
+			InterruptedPortletRequestWhitelistUtil.
+				resetPortletInvocationWhitelistActions();
+		}
+
 		CacheUtil.clearCache();
 
 		JavaScriptBundleUtil.clearCache();
@@ -2400,6 +2416,8 @@ public class HookHotDeployListener
 		"organizations.form.add.main", "organizations.form.add.miscellaneous",
 		"portlet.add.default.resource.check.whitelist",
 		"portlet.add.default.resource.check.whitelist.actions",
+		"portlet.interrupted.request.whitelist",
+		"portlet.interrupted.request.whitelist.actions",
 		"session.phishing.protected.attributes", "sites.form.add.advanced",
 		"sites.form.add.main", "sites.form.add.miscellaneous",
 		"sites.form.add.seo", "sites.form.update.advanced",

portal-impl/src/com/liferay/portal/security/auth/InterruptedPortletRequestWhitelistImpl.java
@@ -1,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.auth;
+
+import com.liferay.portal.kernel.security.pacl.DoPrivileged;
+import com.liferay.portal.util.PropsValues;
+
+/**
+ * @author Peter Borkuti
+ */
+@DoPrivileged
+public class InterruptedPortletRequestWhitelistImpl
+	extends AbstractPortletRequestWhitelist {
+
+	@Override
+	public String[] getWhitelistActionsPropsValues() {
+		return PropsValues.PORTLET_INTERRUPTED_REQUEST_WHITELIST_ACTIONS;
+	}
+
+	@Override
+	public String[] getWhitelistPropsValues() {
+		return PropsValues.PORTLET_INTERRUPTED_REQUEST_WHITELIST;
+	}
+
+}

portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -1492,6 +1492,10 @@ public class PropsValues {
 
 	public static final String[] PORTLET_FILTERS_SYSTEM = PropsUtil.getArray(PropsKeys.PORTLET_FILTERS_SYSTEM);
 
+	public static String[] PORTLET_INTERRUPTED_REQUEST_WHITELIST = PropsUtil.getArray(PropsKeys.PORTLET_INTERRUPTED_REQUEST_WHITELIST);
+
+	public static String[] PORTLET_INTERRUPTED_REQUEST_WHITELIST_ACTIONS = PropsUtil.getArray(PropsKeys.PORTLET_INTERRUPTED_REQUEST_WHITELIST_ACTIONS);
+
 	public static final String PORTLET_PUBLIC_RENDER_PARAMETER_DISTRIBUTION = PropsUtil.get(PropsKeys.PORTLET_PUBLIC_RENDER_PARAMETER_DISTRIBUTION);
 
 	public static final boolean PORTLET_PUBLIC_RENDER_PARAMETER_DISTRIBUTION_LAYOUT = StringUtil.equalsIgnoreCase(PORTLET_PUBLIC_RENDER_PARAMETER_DISTRIBUTION, "layout");

portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -2087,6 +2087,10 @@ public interface PropsKeys {
 
 	public static final String PORTLET_FILTERS_SYSTEM = "portlet.filters.system";
 
+	public static final String PORTLET_INTERRUPTED_REQUEST_WHITELIST = "portlet.interrupted.request.whitelist";
+
+	public static final String PORTLET_INTERRUPTED_REQUEST_WHITELIST_ACTIONS = "portlet.interrupted.request.whitelist.actions";
+
 	public static final String PORTLET_PUBLIC_RENDER_PARAMETER_DISTRIBUTION = "portlet.public.render.parameter.distribution";
 
 	public static final String PORTLET_RESOURCE_ID_BANNED_PATHS_REGEXP = "portlet.resource.id.banned.paths.regexp";