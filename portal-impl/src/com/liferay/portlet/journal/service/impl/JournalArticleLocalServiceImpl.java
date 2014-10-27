0e3e4f383cc3109d9d51c19de0460f63bc3dc511
Brian Chan <brian.chan@liferay.com>
2014-10-27 12:02:10 -0700

    LPS-50671 ran ant format-javadoc -Dlimit="JournalArticleLocalServiceImpl"

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -3791,9 +3791,9 @@ public class JournalArticleLocalServiceImpl
 	 * article ID, title, description, and content, a DDM structure key
 	 * parameter, a DDM template key parameter, and an AND operator switch. It
 	 * is preferable to use the indexed version {@link #search(long, long, List,
-	 * long, String, String, String, String, int, String, String,
-	 * LinkedHashMap, boolean, int, int, Sort)} instead of this method wherever
-	 * possible for performance reasons.
+	 * long, String, String, String, String, int, String, String, LinkedHashMap,
+	 * boolean, int, int, Sort)} instead of this method wherever possible for
+	 * performance reasons.
 	 *
 	 * <p>
 	 * Useful when paginating results. Returns a maximum of <code>end -
@@ -4108,8 +4108,8 @@ public class JournalArticleLocalServiceImpl
 
 	/**
 	 * @deprecated As of 7.0.0, replaced by {@link #search(long, long, List,
-	 *             long, String, String, String, String, int, String,
-	 *             String, LinkedHashMap, boolean, int, int, Sort)}
+	 *             long, String, String, String, String, int, String, String,
+	 *             LinkedHashMap, boolean, int, int, Sort)}
 	 */
 	@Deprecated
 	@Override
@@ -5748,20 +5748,19 @@ public class JournalArticleLocalServiceImpl
 	 * Updates the web content articles matching the group, class name ID, and
 	 * DDM template key, replacing the DDM template key with a new one.
 	 *
-	 * @param groupId the primary key of the web content article's group
-	 * @param classNameId the primary key of the DDMStructure class if the web
-	 *        content article is related to a DDM structure, the primary key of
-	 *        the class name associated with the article, or {@link
-	 *        JournalArticleConstants#CLASSNAME_ID_DEFAULT} otherwise
-	 * @param oldDDMTemplateKey the primary key of the web content article's old
-	 *        DDM template
-	 * @param newDDMTemplateKey the primary key of the web content article's new
-	 *        DDM template
-	 *
-	 *
+	 * @param      groupId the primary key of the web content article's group
+	 * @param      classNameId the primary key of the DDMStructure class if the
+	 *             web content article is related to a DDM structure, the
+	 *             primary key of the class name associated with the article, or
+	 *             {@link JournalArticleConstants#CLASSNAME_ID_DEFAULT}
+	 *             otherwise
+	 * @param      oldDDMTemplateKey the primary key of the web content
+	 *             article's old DDM template
+	 * @param      newDDMTemplateKey the primary key of the web content
+	 *             article's new DDM template
 	 * @deprecated As of 7.0.0, replaced by {@link #updateDDMTemplateKey}
-	 *
 	 */
+	@Deprecated
 	@Override
 	public void updateTemplateId(
 		long groupId, long classNameId, String oldDDMTemplateKey,
