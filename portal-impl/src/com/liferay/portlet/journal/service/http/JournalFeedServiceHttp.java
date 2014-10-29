juliocamarero/liferay-portal

LPS-50671 Autogenerated

portal-impl/src/com/liferay/portlet/journal/service/http/JournalFeedServiceHttp.java
@@ -59,8 +59,8 @@ public class JournalFeedServiceHttp {
 		HttpPrincipal httpPrincipal, long groupId, java.lang.String feedId,
 		boolean autoFeedId, java.lang.String name,
 		java.lang.String description, java.lang.String type,
-		java.lang.String structureId, java.lang.String templateId,
-		java.lang.String rendererTemplateId, int delta,
+		java.lang.String ddmStructureKey, java.lang.String ddmTemplateKey,
+		java.lang.String ddmRendererTemplateKey, int delta,
 		java.lang.String orderByCol, java.lang.String orderByType,
 		java.lang.String targetLayoutFriendlyUrl,
 		java.lang.String targetPortletId, java.lang.String contentField,
@@ -72,10 +72,11 @@ public class JournalFeedServiceHttp {
 					"addFeed", _addFeedParameterTypes0);
 
 			MethodHandler methodHandler = new MethodHandler(methodKey, groupId,
-					feedId, autoFeedId, name, description, type, structureId,
-					templateId, rendererTemplateId, delta, orderByCol,
-					orderByType, targetLayoutFriendlyUrl, targetPortletId,
-					contentField, feedType, feedVersion, serviceContext);
+					feedId, autoFeedId, name, description, type,
+					ddmStructureKey, ddmTemplateKey, ddmRendererTemplateKey,
+					delta, orderByCol, orderByType, targetLayoutFriendlyUrl,
+					targetPortletId, contentField, feedType, feedVersion,
+					serviceContext);
 
 			Object returnObj = null;
 
@@ -278,9 +279,10 @@ public class JournalFeedServiceHttp {
 	public static com.liferay.portlet.journal.model.JournalFeed updateFeed(
 		HttpPrincipal httpPrincipal, long groupId, java.lang.String feedId,
 		java.lang.String name, java.lang.String description,
-		java.lang.String type, java.lang.String structureId,
-		java.lang.String templateId, java.lang.String rendererTemplateId,
-		int delta, java.lang.String orderByCol, java.lang.String orderByType,
+		java.lang.String type, java.lang.String ddmStructureKey,
+		java.lang.String ddmTemplateKey,
+		java.lang.String ddmRendererTemplateKey, int delta,
+		java.lang.String orderByCol, java.lang.String orderByType,
 		java.lang.String targetLayoutFriendlyUrl,
 		java.lang.String targetPortletId, java.lang.String contentField,
 		java.lang.String feedType, double feedVersion,
@@ -291,10 +293,10 @@ public class JournalFeedServiceHttp {
 					"updateFeed", _updateFeedParameterTypes7);
 
 			MethodHandler methodHandler = new MethodHandler(methodKey, groupId,
-					feedId, name, description, type, structureId, templateId,
-					rendererTemplateId, delta, orderByCol, orderByType,
-					targetLayoutFriendlyUrl, targetPortletId, contentField,
-					feedType, feedVersion, serviceContext);
+					feedId, name, description, type, ddmStructureKey,
+					ddmTemplateKey, ddmRendererTemplateKey, delta, orderByCol,
+					orderByType, targetLayoutFriendlyUrl, targetPortletId,
+					contentField, feedType, feedVersion, serviceContext);
 
 			Object returnObj = null;
 

portal-service/src/com/liferay/portlet/journal/service/JournalFeedLocalService.java
@@ -51,9 +51,10 @@ public interface JournalFeedLocalService extends BaseLocalService,
 	public com.liferay.portlet.journal.model.JournalFeed addFeed(long userId,
 		long groupId, java.lang.String feedId, boolean autoFeedId,
 		java.lang.String name, java.lang.String description,
-		java.lang.String type, java.lang.String structureId,
-		java.lang.String templateId, java.lang.String rendererTemplateId,
-		int delta, java.lang.String orderByCol, java.lang.String orderByType,
+		java.lang.String type, java.lang.String ddmStructureKey,
+		java.lang.String ddmTemplateKey,
+		java.lang.String ddmRendererTemplateKey, int delta,
+		java.lang.String orderByCol, java.lang.String orderByType,
 		java.lang.String targetLayoutFriendlyUrl,
 		java.lang.String targetPortletId, java.lang.String contentField,
 		java.lang.String feedFormat, double feedVersion,
@@ -351,8 +352,8 @@ public interface JournalFeedLocalService extends BaseLocalService,
 	public com.liferay.portlet.journal.model.JournalFeed updateFeed(
 		long groupId, java.lang.String feedId, java.lang.String name,
 		java.lang.String description, java.lang.String type,
-		java.lang.String structureId, java.lang.String templateId,
-		java.lang.String rendererTemplateId, int delta,
+		java.lang.String ddmStructureKey, java.lang.String ddmTemplateKey,
+		java.lang.String ddmRendererTemplateKey, int delta,
 		java.lang.String orderByCol, java.lang.String orderByType,
 		java.lang.String targetLayoutFriendlyUrl,
 		java.lang.String targetPortletId, java.lang.String contentField,

portal-service/src/com/liferay/portlet/journal/service/JournalFeedService.java
@@ -51,8 +51,8 @@ public interface JournalFeedService extends BaseService {
 	public com.liferay.portlet.journal.model.JournalFeed addFeed(long groupId,
 		java.lang.String feedId, boolean autoFeedId, java.lang.String name,
 		java.lang.String description, java.lang.String type,
-		java.lang.String structureId, java.lang.String templateId,
-		java.lang.String rendererTemplateId, int delta,
+		java.lang.String ddmStructureKey, java.lang.String ddmTemplateKey,
+		java.lang.String ddmRendererTemplateKey, int delta,
 		java.lang.String orderByCol, java.lang.String orderByType,
 		java.lang.String targetLayoutFriendlyUrl,
 		java.lang.String targetPortletId, java.lang.String contentField,
@@ -109,8 +109,8 @@ public interface JournalFeedService extends BaseService {
 	public com.liferay.portlet.journal.model.JournalFeed updateFeed(
 		long groupId, java.lang.String feedId, java.lang.String name,
 		java.lang.String description, java.lang.String type,
-		java.lang.String structureId, java.lang.String templateId,
-		java.lang.String rendererTemplateId, int delta,
+		java.lang.String ddmStructureKey, java.lang.String ddmTemplateKey,
+		java.lang.String ddmRendererTemplateKey, int delta,
 		java.lang.String orderByCol, java.lang.String orderByType,
 		java.lang.String targetLayoutFriendlyUrl,
 		java.lang.String targetPortletId, java.lang.String contentField,

portal-service/src/com/liferay/portlet/journal/service/JournalFeedServiceWrapper.java
@@ -36,8 +36,8 @@ public class JournalFeedServiceWrapper implements JournalFeedService,
 	public com.liferay.portlet.journal.model.JournalFeed addFeed(long groupId,
 		java.lang.String feedId, boolean autoFeedId, java.lang.String name,
 		java.lang.String description, java.lang.String type,
-		java.lang.String structureId, java.lang.String templateId,
-		java.lang.String rendererTemplateId, int delta,
+		java.lang.String ddmStructureKey, java.lang.String ddmTemplateKey,
+		java.lang.String ddmRendererTemplateKey, int delta,
 		java.lang.String orderByCol, java.lang.String orderByType,
 		java.lang.String targetLayoutFriendlyUrl,
 		java.lang.String targetPortletId, java.lang.String contentField,
@@ -45,9 +45,10 @@ public class JournalFeedServiceWrapper implements JournalFeedService,
 		com.liferay.portal.service.ServiceContext serviceContext)
 		throws com.liferay.portal.kernel.exception.PortalException {
 		return _journalFeedService.addFeed(groupId, feedId, autoFeedId, name,
-			description, type, structureId, templateId, rendererTemplateId,
-			delta, orderByCol, orderByType, targetLayoutFriendlyUrl,
-			targetPortletId, contentField, feedType, feedVersion, serviceContext);
+			description, type, ddmStructureKey, ddmTemplateKey,
+			ddmRendererTemplateKey, delta, orderByCol, orderByType,
+			targetLayoutFriendlyUrl, targetPortletId, contentField, feedType,
+			feedVersion, serviceContext);
 	}
 
 	@Override
@@ -119,8 +120,8 @@ public class JournalFeedServiceWrapper implements JournalFeedService,
 	public com.liferay.portlet.journal.model.JournalFeed updateFeed(
 		long groupId, java.lang.String feedId, java.lang.String name,
 		java.lang.String description, java.lang.String type,
-		java.lang.String structureId, java.lang.String templateId,
-		java.lang.String rendererTemplateId, int delta,
+		java.lang.String ddmStructureKey, java.lang.String ddmTemplateKey,
+		java.lang.String ddmRendererTemplateKey, int delta,
 		java.lang.String orderByCol, java.lang.String orderByType,
 		java.lang.String targetLayoutFriendlyUrl,
 		java.lang.String targetPortletId, java.lang.String contentField,
@@ -128,9 +129,10 @@ public class JournalFeedServiceWrapper implements JournalFeedService,
 		com.liferay.portal.service.ServiceContext serviceContext)
 		throws com.liferay.portal.kernel.exception.PortalException {
 		return _journalFeedService.updateFeed(groupId, feedId, name,
-			description, type, structureId, templateId, rendererTemplateId,
-			delta, orderByCol, orderByType, targetLayoutFriendlyUrl,
-			targetPortletId, contentField, feedType, feedVersion, serviceContext);
+			description, type, ddmStructureKey, ddmTemplateKey,
+			ddmRendererTemplateKey, delta, orderByCol, orderByType,
+			targetLayoutFriendlyUrl, targetPortletId, contentField, feedType,
+			feedVersion, serviceContext);
 	}
 
 	/**

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portlet/journal/service/http/JournalFeedServiceHttp.java
@@ -58,9 +58,10 @@ public class JournalFeedServiceHttp {
 	public static com.liferay.portlet.journal.model.JournalFeed addFeed(
 		HttpPrincipal httpPrincipal, long groupId, java.lang.String feedId,
 		boolean autoFeedId, java.lang.String name,
-		java.lang.String description, java.lang.String structureId,
-		java.lang.String templateId, java.lang.String rendererTemplateId,
-		int delta, java.lang.String orderByCol, java.lang.String orderByType,
+		java.lang.String description, java.lang.String ddmStructureKey,
+		java.lang.String ddmTemplateKey,
+		java.lang.String ddmRendererTemplateKey, int delta,
+		java.lang.String orderByCol, java.lang.String orderByType,
 		java.lang.String targetLayoutFriendlyUrl,
 		java.lang.String targetPortletId, java.lang.String contentField,
 		java.lang.String feedType, double feedVersion,
@@ -71,8 +72,8 @@ public class JournalFeedServiceHttp {
 					"addFeed", _addFeedParameterTypes0);
 
 			MethodHandler methodHandler = new MethodHandler(methodKey, groupId,
-					feedId, autoFeedId, name, description, structureId,
-					templateId, rendererTemplateId, delta, orderByCol,
+					feedId, autoFeedId, name, description, ddmStructureKey,
+					ddmTemplateKey, ddmRendererTemplateKey, delta, orderByCol,
 					orderByType, targetLayoutFriendlyUrl, targetPortletId,
 					contentField, feedType, feedVersion, serviceContext);
 
@@ -277,8 +278,8 @@ public class JournalFeedServiceHttp {
 	public static com.liferay.portlet.journal.model.JournalFeed updateFeed(
 		HttpPrincipal httpPrincipal, long groupId, java.lang.String feedId,
 		java.lang.String name, java.lang.String description,
-		java.lang.String structureId, java.lang.String templateId,
-		java.lang.String rendererTemplateId, int delta,
+		java.lang.String ddmStructureKey, java.lang.String ddmTemplateKey,
+		java.lang.String ddmRendererTemplateKey, int delta,
 		java.lang.String orderByCol, java.lang.String orderByType,
 		java.lang.String targetLayoutFriendlyUrl,
 		java.lang.String targetPortletId, java.lang.String contentField,
@@ -290,8 +291,8 @@ public class JournalFeedServiceHttp {
 					"updateFeed", _updateFeedParameterTypes7);
 
 			MethodHandler methodHandler = new MethodHandler(methodKey, groupId,
-					feedId, name, description, structureId, templateId,
-					rendererTemplateId, delta, orderByCol, orderByType,
+					feedId, name, description, ddmStructureKey, ddmTemplateKey,
+					ddmRendererTemplateKey, delta, orderByCol, orderByType,
 					targetLayoutFriendlyUrl, targetPortletId, contentField,
 					feedType, feedVersion, serviceContext);
 

portal-service/src/com/liferay/portlet/journal/service/JournalFeedLocalService.java
@@ -51,8 +51,8 @@ public interface JournalFeedLocalService extends BaseLocalService,
 	public com.liferay.portlet.journal.model.JournalFeed addFeed(long userId,
 		long groupId, java.lang.String feedId, boolean autoFeedId,
 		java.lang.String name, java.lang.String description,
-		java.lang.String structureId, java.lang.String templateId,
-		java.lang.String rendererTemplateId, int delta,
+		java.lang.String ddmStructureKey, java.lang.String ddmTemplateKey,
+		java.lang.String ddmRendererTemplateKey, int delta,
 		java.lang.String orderByCol, java.lang.String orderByType,
 		java.lang.String targetLayoutFriendlyUrl,
 		java.lang.String targetPortletId, java.lang.String contentField,
@@ -350,9 +350,10 @@ public interface JournalFeedLocalService extends BaseLocalService,
 
 	public com.liferay.portlet.journal.model.JournalFeed updateFeed(
 		long groupId, java.lang.String feedId, java.lang.String name,
-		java.lang.String description, java.lang.String structureId,
-		java.lang.String templateId, java.lang.String rendererTemplateId,
-		int delta, java.lang.String orderByCol, java.lang.String orderByType,
+		java.lang.String description, java.lang.String ddmStructureKey,
+		java.lang.String ddmTemplateKey,
+		java.lang.String ddmRendererTemplateKey, int delta,
+		java.lang.String orderByCol, java.lang.String orderByType,
 		java.lang.String targetLayoutFriendlyUrl,
 		java.lang.String targetPortletId, java.lang.String contentField,
 		java.lang.String feedFormat, double feedVersion,

portal-service/src/com/liferay/portlet/journal/service/JournalFeedService.java
@@ -50,9 +50,10 @@ public interface JournalFeedService extends BaseService {
 	 */
 	public com.liferay.portlet.journal.model.JournalFeed addFeed(long groupId,
 		java.lang.String feedId, boolean autoFeedId, java.lang.String name,
-		java.lang.String description, java.lang.String structureId,
-		java.lang.String templateId, java.lang.String rendererTemplateId,
-		int delta, java.lang.String orderByCol, java.lang.String orderByType,
+		java.lang.String description, java.lang.String ddmStructureKey,
+		java.lang.String ddmTemplateKey,
+		java.lang.String ddmRendererTemplateKey, int delta,
+		java.lang.String orderByCol, java.lang.String orderByType,
 		java.lang.String targetLayoutFriendlyUrl,
 		java.lang.String targetPortletId, java.lang.String contentField,
 		java.lang.String feedType, double feedVersion,
@@ -107,9 +108,10 @@ public interface JournalFeedService extends BaseService {
 
 	public com.liferay.portlet.journal.model.JournalFeed updateFeed(
 		long groupId, java.lang.String feedId, java.lang.String name,
-		java.lang.String description, java.lang.String structureId,
-		java.lang.String templateId, java.lang.String rendererTemplateId,
-		int delta, java.lang.String orderByCol, java.lang.String orderByType,
+		java.lang.String description, java.lang.String ddmStructureKey,
+		java.lang.String ddmTemplateKey,
+		java.lang.String ddmRendererTemplateKey, int delta,
+		java.lang.String orderByCol, java.lang.String orderByType,
 		java.lang.String targetLayoutFriendlyUrl,
 		java.lang.String targetPortletId, java.lang.String contentField,
 		java.lang.String feedType, double feedVersion,

portal-service/src/com/liferay/portlet/journal/service/JournalFeedServiceWrapper.java
@@ -35,18 +35,20 @@ public class JournalFeedServiceWrapper implements JournalFeedService,
 	@Override
 	public com.liferay.portlet.journal.model.JournalFeed addFeed(long groupId,
 		java.lang.String feedId, boolean autoFeedId, java.lang.String name,
-		java.lang.String description, java.lang.String structureId,
-		java.lang.String templateId, java.lang.String rendererTemplateId,
-		int delta, java.lang.String orderByCol, java.lang.String orderByType,
+		java.lang.String description, java.lang.String ddmStructureKey,
+		java.lang.String ddmTemplateKey,
+		java.lang.String ddmRendererTemplateKey, int delta,
+		java.lang.String orderByCol, java.lang.String orderByType,
 		java.lang.String targetLayoutFriendlyUrl,
 		java.lang.String targetPortletId, java.lang.String contentField,
 		java.lang.String feedType, double feedVersion,
 		com.liferay.portal.service.ServiceContext serviceContext)
 		throws com.liferay.portal.kernel.exception.PortalException {
 		return _journalFeedService.addFeed(groupId, feedId, autoFeedId, name,
-			description, structureId, templateId, rendererTemplateId, delta,
-			orderByCol, orderByType, targetLayoutFriendlyUrl, targetPortletId,
-			contentField, feedType, feedVersion, serviceContext);
+			description, ddmStructureKey, ddmTemplateKey,
+			ddmRendererTemplateKey, delta, orderByCol, orderByType,
+			targetLayoutFriendlyUrl, targetPortletId, contentField, feedType,
+			feedVersion, serviceContext);
 	}
 
 	@Override
@@ -117,18 +119,20 @@ public class JournalFeedServiceWrapper implements JournalFeedService,
 	@Override
 	public com.liferay.portlet.journal.model.JournalFeed updateFeed(
 		long groupId, java.lang.String feedId, java.lang.String name,
-		java.lang.String description, java.lang.String structureId,
-		java.lang.String templateId, java.lang.String rendererTemplateId,
-		int delta, java.lang.String orderByCol, java.lang.String orderByType,
+		java.lang.String description, java.lang.String ddmStructureKey,
+		java.lang.String ddmTemplateKey,
+		java.lang.String ddmRendererTemplateKey, int delta,
+		java.lang.String orderByCol, java.lang.String orderByType,
 		java.lang.String targetLayoutFriendlyUrl,
 		java.lang.String targetPortletId, java.lang.String contentField,
 		java.lang.String feedType, double feedVersion,
 		com.liferay.portal.service.ServiceContext serviceContext)
 		throws com.liferay.portal.kernel.exception.PortalException {
 		return _journalFeedService.updateFeed(groupId, feedId, name,
-			description, structureId, templateId, rendererTemplateId, delta,
-			orderByCol, orderByType, targetLayoutFriendlyUrl, targetPortletId,
-			contentField, feedType, feedVersion, serviceContext);
+			description, ddmStructureKey, ddmTemplateKey,
+			ddmRendererTemplateKey, delta, orderByCol, orderByType,
+			targetLayoutFriendlyUrl, targetPortletId, contentField, feedType,
+			feedVersion, serviceContext);
 	}
 
 	/**