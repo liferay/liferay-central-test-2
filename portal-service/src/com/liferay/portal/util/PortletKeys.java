10c4c9427e68a68aaa8285939870e3a86e55b2e5
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-10-30 12:17:59 +0100

    LPS-51249 Creates new portlet only for test

--- portal-service/src/com/liferay/portal/util/PortletKeys.java
+++ portal-service/src/com/liferay/portal/util/PortletKeys.java
@@ -250,6 +250,8 @@ public class PortletKeys {
 	@Deprecated
 	public static final String TAGS_ENTRIES_NAVIGATION = ASSET_TAGS_NAVIGATION;
 
+	public static final String TEST = "com_liferay_test_portlet_TestPortlet";
+
 	public static final String TRANSLATOR = "26";
 
 	public static final String TRASH = "182";
