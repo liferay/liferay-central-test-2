cf421f6be6ab784c3d82c23e8fa13ebc3307333b
shuyangzhou <shuyang.zhou@liferay.com>
2014-10-31 12:50:56 -0700

    LPS-51290 BaseAssetSearchTestCase's poor expiration date generation maybe too close to current time causing JournalArticle validation to fail

--- portal-impl/test/integration/com/liferay/portlet/asset/service/persistence/BaseAssetSearchTestCase.java
+++ portal-impl/test/integration/com/liferay/portlet/asset/service/persistence/BaseAssetSearchTestCase.java
@@ -17,10 +17,12 @@ package com.liferay.portlet.asset.service.persistence;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.search.Hits;
 import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.security.RandomUtil;
 import com.liferay.portal.kernel.test.ExecutionTestListeners;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.DateFormatFactoryUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.model.BaseModel;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.service.ServiceContext;
@@ -922,8 +924,7 @@ public abstract class BaseAssetSearchTestCase {
 
 		for (int i = 0; i < size; i++) {
 			Date date = new Date(
-				startDate.getTime() +
-				(long)(Math.random() * 60 * 60 * 24 * 365));
+				startDate.getTime() + (RandomUtil.nextInt(365) + 1) * Time.DAY);
 
 			Calendar calendar = new GregorianCalendar();
 
