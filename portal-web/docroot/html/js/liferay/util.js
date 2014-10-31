d12655f98a4ba004ff72390dd72da5b0e7d83643
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2014-10-31 11:30:53 -0700

    LPS-51317 Move util_list_fields.js methods to util.js

--- portal-web/docroot/html/js/liferay/util.js
+++ portal-web/docroot/html/js/liferay/util.js
@@ -405,6 +405,114 @@
 			return (instance.getWindowWidth() < Liferay.BREAKPOINTS.TABLET);
 		},
 
+		listChecked: function(form, name) {
+			var buffer = [];
+
+			form = A.one(form);
+
+			if (form) {
+				var selector = 'input[type=checkbox]';
+
+				if (name) {
+					selector += '[name='+ name +']';
+				}
+
+				form.all(selector).each(
+					function(item, index) {
+						var val = item.val();
+
+						if (val && item.get('checked')) {
+							buffer.push(val);
+						}
+					}
+				);
+			}
+
+			return buffer.join();
+		},
+
+		listCheckedExcept: function(form, except, name) {
+			var buffer = [];
+
+			form = A.one(form);
+
+			if (form) {
+				var selector = 'input[type=checkbox]';
+
+				if (name) {
+					selector += '[name='+ name +']';
+				}
+
+				form.all(selector).each(
+					function(item, index) {
+						var val = item.val();
+
+						if (val && item.get('name') != except && item.get('checked') && !item.get('disabled')) {
+							buffer.push(val);
+						}
+					}
+				);
+			}
+
+			return buffer.join();
+		},
+
+		listSelect: function(box, delimeter) {
+			var buffer = [];
+
+			var selectList = '';
+
+			if (box != null) {
+				var select = A.one(box);
+
+				if (select) {
+					var options = select.all('option');
+
+					options.each(
+						function(item, index) {
+							var val = item.val();
+
+							if (val) {
+								buffer.push(val);
+							}
+						}
+					);
+				}
+
+				if (buffer[0] != '.none') {
+					selectList = buffer.join(delimeter || ',');
+				}
+			}
+
+			return selectList;
+		},
+
+		listUncheckedExcept: function(form, except, name) {
+			var buffer = [];
+
+			form = A.one(form);
+
+			if (form) {
+				var selector = 'input[type=checkbox]';
+
+				if (name) {
+					selector += '[name='+ name +']';
+				}
+
+				form.all(selector).each(
+					function(item, index) {
+						var val = item.val();
+
+						if (val && item.get('name') != except && !item.get('checked') && !item.get('disabled')) {
+							buffer.push(val);
+						}
+					}
+				);
+			}
+
+			return buffer.join();
+		},
+
 		ns: function(namespace, obj) {
 			var instance = this;
 
