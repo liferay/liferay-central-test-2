252acf79a9ffa47e60561929d2568826f64e73a8
adolfo.perez <adolfo.perez@liferay.com>
2014-11-03 12:29:07 +0100

    LPS-50752 make startup action and eager component
    Make sure it's executed as soon as possible by using the `immediate`
    attribute.

--- modules/apps/social/social-networking-web/src/com/liferay/socialnetworking/hook/events/StartupAction.java
+++ modules/apps/social/social-networking-web/src/com/liferay/socialnetworking/hook/events/StartupAction.java
@@ -17,17 +17,27 @@ package com.liferay.socialnetworking.hook.events;
 import com.liferay.portal.kernel.events.ActionException;
 import com.liferay.portal.kernel.events.SimpleAction;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.model.Company;
 import com.liferay.portal.model.User;
+import com.liferay.portal.security.auth.CompanyThreadLocal;
+import com.liferay.portal.service.CompanyLocalService;
 import com.liferay.portlet.expando.DuplicateColumnNameException;
 import com.liferay.portlet.expando.DuplicateTableNameException;
 import com.liferay.portlet.expando.model.ExpandoColumnConstants;
 import com.liferay.portlet.expando.model.ExpandoTable;
-import com.liferay.portlet.expando.service.ExpandoColumnLocalServiceUtil;
-import com.liferay.portlet.expando.service.ExpandoTableLocalServiceUtil;
+import com.liferay.portlet.expando.service.ExpandoColumnLocalService;
+import com.liferay.portlet.expando.service.ExpandoTableLocalService;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
  */
+@Component(immediate = true)
 public class StartupAction extends SimpleAction {
 
 	@Override
@@ -40,28 +50,74 @@ public class StartupAction extends SimpleAction {
 		}
 	}
 
+	@Reference(unbind = "-")
+	public void setCompanyLocalService(
+		CompanyLocalService companyLocalService) {
+
+		_companyLocalService = companyLocalService;
+	}
+
 	protected void doRun(long companyId) throws Exception {
 		setupExpando(companyId);
+	}
+
+	/**
+	 * See {@link HookHotDeployListener#initEvent}
+	 */
+	@Activate
+	protected void run() throws ActionException {
+		Long companyId = CompanyThreadLocal.getCompanyId();
+
+		try {
+			List<Company> companys = _companyLocalService.getCompanies();
+
+			for (Company company : companys) {
+				CompanyThreadLocal.setCompanyId(company.getCompanyId());
+
+				run(new String[]{String.valueOf(company.getCompanyId())});
+			}
+		}
+		finally {
+			CompanyThreadLocal.setCompanyId(companyId);
+		}
+	}
+
+	@Reference(unbind = "-")
+	protected void setExpandoColumnLocalService(
+		ExpandoColumnLocalService expandoColumnLocalService) {
+
+		_expandoColumnLocalService = expandoColumnLocalService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setExpandoTableLocalService(
+		ExpandoTableLocalService expandoTableLocalService) {
+
+		_expandoTableLocalService = expandoTableLocalService;
 	}
 
 	protected void setupExpando(long companyId) throws Exception {
 		ExpandoTable table = null;
 
 		try {
-			table = ExpandoTableLocalServiceUtil.addTable(
+			table = _expandoTableLocalService.addTable(
 				companyId, User.class.getName(), "SN");
 		}
 		catch (DuplicateTableNameException dtne) {
-			table = ExpandoTableLocalServiceUtil.getTable(
+			table = _expandoTableLocalService.getTable(
 				companyId, User.class.getName(), "SN");
 		}
 
 		try {
-			ExpandoColumnLocalServiceUtil.addColumn(
+			_expandoColumnLocalService.addColumn(
 				table.getTableId(), "aboutMe", ExpandoColumnConstants.STRING);
 		}
 		catch (DuplicateColumnNameException dcne) {
 		}
 	}
+
+	private CompanyLocalService _companyLocalService;
+	private ExpandoColumnLocalService _expandoColumnLocalService;
+	private ExpandoTableLocalService _expandoTableLocalService;
 
 }
