caf9713de33e3a55890d3bf4ed6c16cdf6a6021d
adolfo.perez <adolfo.perez@liferay.com>
2014-11-03 13:43:29 +0100

    LPS-50752 make upgrade process an OSGi component

---
+++ modules/apps/social/social-networking-web/src/com/liferay/socialnetworking/hook/upgrade/SocialNetworkingWebUpgrade.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.socialnetworking.hook.upgrade;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.socialnetworking.hook.upgrade.v1_0_0.UpgradeNamespace;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import org.springframework.context.ApplicationContext;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(
+	immediate = true, service = SocialNetworkingWebUpgrade.class
+)
+public class SocialNetworkingWebUpgrade {
+
+	@Reference(
+		target =
+			"(org.springframework.context.service.name=" +
+				"com.liferay.socialnetworking)",
+		unbind = "-"
+	)
+	protected void setApplicationContext(
+		ApplicationContext applicationContext) {
+	}
+
+	@Reference(unbind = "-")
+	protected void setReleaseLocalService(
+		ReleaseLocalService releaseLocalService) {
+
+		_releaseLocalService = releaseLocalService;
+	}
+
+	@Activate
+	protected void upgrade() throws PortalException {
+		List<UpgradeProcess> upgradeProcesses =
+			Collections.<UpgradeProcess>singletonList(new UpgradeNamespace());
+
+		_releaseLocalService.updateRelease(
+			"social-networking-portlet", upgradeProcesses, 100, 0, false);
+	}
+
+	private ReleaseLocalService _releaseLocalService;
+
+}
