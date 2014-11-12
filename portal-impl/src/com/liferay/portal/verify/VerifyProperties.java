695ea219a060c9d8b7e1dd382be8967a13eac0e0
Jonathan McCann <jonathan.mccann@liferay.com>
2014-11-12 15:15:55 -0800

    LPS-50325 Source Formatting

--- portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
@@ -351,20 +351,16 @@ public class VerifyProperties extends VerifyProcess {
 			"xml.doctype.declaration.allowed",
 			"xml.doctype.declaration.allowed", "com.liferay.xsl.content.web"
 		},
-
 		new String[] {
 			"xml.external.general.entities.allowed",
 			"xml.external.general.entities.allowed",
 			"com.liferay.xsl.content.web"
 		},
-
 		new String[] {
 			"xml.external.parameter.entities.allowed",
 			"xml.external.parameter.entities.allowed",
 			"com.liferay.xsl.content.web"
-
 		},
-
 		new String[] {
 			"xsl.secure.processing.enabled", "xsl.secure.processing.enabled",
 			"com.liferay.xsl.content.web"
