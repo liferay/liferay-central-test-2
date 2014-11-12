433115fba244281d3673dc3e48ac01ef28ffb508
Brian Chan <brian.chan@liferay.com>
2014-11-12 11:11:52 -0800

    LPS-51358 full package not needed

--- portal-impl/src/com/liferay/portal/repository/liferayrepository/LiferayRepository.java
+++ portal-impl/src/com/liferay/portal/repository/liferayrepository/LiferayRepository.java
@@ -804,8 +804,8 @@ public class LiferayRepository
 
 	/**
 	 * @deprecated As of 7.0.0, replaced by {@link #updateFileEntry(long, long,
-	 *             String, String, String, String, String, boolean,
-	 *             java.io.File, com.liferay.portal.service.ServiceContext)}
+	 *             String, String, String, String, String, boolean, File,
+	 *             ServiceContext)}
 	 */
 	@Deprecated
 	@Override
@@ -823,8 +823,7 @@ public class LiferayRepository
 	/**
 	 * @deprecated As of 7.0.0, replaced by {@link #updateFileEntry(long, long,
 	 *             String, String, String, String, String, boolean,
-	 *             java.io.InputStream, long,
-	 *             com.liferay.portal.service.ServiceContext)}
+	 *             InputStream, long, ServiceContext)}
 	 */
 	@Deprecated
 	@Override
