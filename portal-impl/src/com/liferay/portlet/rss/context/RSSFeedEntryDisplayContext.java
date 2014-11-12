fcdf824d64500df826d48e59b7bf12ec137461d1
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-11-12 17:13:23 +0100

    LPS-46984 Moves code to RSSFeedEntryDisplayContext

--- portal-impl/src/com/liferay/portlet/rss/context/RSSFeedEntryDisplayContext.java
+++ portal-impl/src/com/liferay/portlet/rss/context/RSSFeedEntryDisplayContext.java
@@ -19,6 +19,7 @@ import com.liferay.portal.kernel.sanitizer.SanitizerException;
 import com.liferay.portal.kernel.sanitizer.SanitizerUtil;
 import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -28,15 +29,71 @@ import com.liferay.portal.util.WebKeys;
 import com.liferay.util.RSSUtil;
 
 import com.sun.syndication.feed.synd.SyndContent;
+import com.sun.syndication.feed.synd.SyndEnclosure;
+import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author Eudaldo Alonso
  */
 public class RSSFeedEntryDisplayContext {
+
+	public RSSFeedEntryDisplayContext(
+		SyndEntry entry, HttpServletRequest request, RSSFeed rssFeed) {
+
+		String entryLink = entry.getLink();
+
+		if (Validator.isNotNull(entryLink) && !HttpUtil.hasDomain(entryLink)) {
+			entryLink = rssFeed.getBaseURL() + entryLink;
+		}
+
+		List<SyndEnclosure> enclosures =
+			(List<SyndEnclosure>)entry.getEnclosures();
+
+		String enclosureLink = StringPool.BLANK;
+		String enclosureLinkTitle = entry.getTitle();
+
+		for (SyndEnclosure enclosure : enclosures) {
+			if (Validator.isNotNull(enclosure.getUrl())) {
+				enclosureLink = enclosure.getUrl();
+
+				int pos = enclosureLink.lastIndexOf(StringPool.FORWARD_SLASH);
+
+				if (pos > -1) {
+					enclosureLinkTitle = enclosureLink.substring(pos + 1);
+				}
+				else {
+					enclosureLinkTitle = enclosureLink;
+				}
+
+				break;
+			}
+		}
+
+		_entry = entry;
+		_entryLink = entryLink;
+		_enclosureLink = enclosureLink;
+		_enclosureLinkTitle = enclosureLinkTitle;
+		_request = request;
+		_rssFeed = rssFeed;
+	}
+
+	public String getEnclosureLink() {
+		return _enclosureLink;
+	}
+
+	public String getEnclosureLinkTitle() {
+		return _enclosureLinkTitle;
+	}
+
+	public String getEntryLink() {
+		return _entryLink;
+	}
 
 	public String getSanitizedContent() {
 		ThemeDisplay themeDisplay = (ThemeDisplay) _request.getAttribute(
@@ -101,5 +158,12 @@ public class RSSFeedEntryDisplayContext {
 
 		return contents;
 	}
+
+	private final String _enclosureLink;
+	private final String _enclosureLinkTitle;
+	private final SyndEntry _entry;
+	private final String _entryLink;
+	private final HttpServletRequest _request;
+	private final RSSFeed _rssFeed;
 
 }
