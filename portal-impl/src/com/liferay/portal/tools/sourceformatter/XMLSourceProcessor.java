e52d15f9a1f2b6cd659cb4dd503b4dc35e4db2be
Hugo Huijser <hugo.huijser@liferay.com>
2014-11-12 14:08:24 +0800

    LPS-51315 Add check to SourceFormatter

--- portal-impl/src/com/liferay/portal/tools/sourceformatter/XMLSourceProcessor.java
+++ portal-impl/src/com/liferay/portal/tools/sourceformatter/XMLSourceProcessor.java
@@ -16,6 +16,7 @@ package com.liferay.portal.tools.sourceformatter;
 
 import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
 import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
+import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.PropertiesUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.StringBundler;
@@ -202,6 +203,9 @@ public class XMLSourceProcessor extends BaseSourceProcessor {
 
 		if (fileName.contains("/build") && !fileName.contains("/tools/")) {
 			newContent = formatAntXML(fileName, newContent);
+		}
+		else if (fileName.contains("/custom-sql/")) {
+			formatCustomSQLXML(fileName, newContent);
 		}
 		else if (fileName.endsWith("structures.xml")) {
 			newContent = formatDDLStructuresXML(newContent);
@@ -527,6 +531,25 @@ public class XMLSourceProcessor extends BaseSourceProcessor {
 		}
 
 		return newContent;
+	}
+
+	protected void formatCustomSQLXML(String fileName, String content) {
+		Matcher matcher = _whereNotInSQLPattern.matcher(content);
+
+		if (!matcher.find()) {
+			return;
+		}
+
+		int x = content.lastIndexOf("<sql id=", matcher.start());
+
+		int y = content.indexOf(CharPool.QUOTE, x);
+
+		int z = content.indexOf(CharPool.QUOTE, y + 1);
+
+		processErrorMessage(
+			fileName,
+				"LPS-51315 Avoid using WHERE ... NOT IN: " + fileName + " " +
+					content.substring(y + 1, z));
 	}
 
 	protected String formatDDLStructuresXML(String content) throws Exception {
@@ -1214,6 +1237,8 @@ public class XMLSourceProcessor extends BaseSourceProcessor {
 			"(?:(?:\\n){1,}+|\\</execute\\>)");
 	private Pattern _poshiWholeTagPattern = Pattern.compile("<[^\\>^/]*\\/>");
 	private String _tablesContent;
+	private Pattern _whereNotInSQLPattern = Pattern.compile(
+		"WHERE[ \t\n]+\\(*[a-zA-z0-9.]+ NOT IN");
 	private List<String> _xmlExclusions;
 
 	private class FinderElementComparator implements Comparator<Element> {
