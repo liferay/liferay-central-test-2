18d29300ba9f3c984c380e27ffc2a7a95e41b6d2
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2014-11-13 15:08:12 -0800

    LPS-50406 - Converting this one over to jQuery since we're already in the process and don't want this one to get forgotten

--- portal-web/docroot/html/js/liferay/util.js
+++ portal-web/docroot/html/js/liferay/util.js
@@ -1595,6 +1595,10 @@
 		Util,
 		'toggleDisabled',
 		function(button, state) {
+			if (button.jquery) {
+				button = button.get();
+			}
+
 			if (!A.instanceOf(button, A.NodeList)) {
 				button = A.all(button);
 			}
