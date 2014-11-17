990df2f30f963a734df9291bb31587c724170701
Shinn Lok <shinn.lok@liferay.com>
2014-11-17 11:37:19 -0800

    sync-engine - Clear uiEvent if nothing was uploaded

--- modules/util/sync-engine/src/com/liferay/sync/engine/documentlibrary/handler/UpdateFileEntryHandler.java
+++ modules/util/sync-engine/src/com/liferay/sync/engine/documentlibrary/handler/UpdateFileEntryHandler.java
@@ -73,6 +73,9 @@ public class UpdateFileEntryHandler extends BaseSyncDLObjectHandler {
 		if (getParameterValue("filePath") != null) {
 			localSyncFile.setUiEvent(SyncFile.UI_EVENT_UPLOADED);
 		}
+		else {
+			localSyncFile.setUiEvent(SyncFile.UI_EVENT_DEFAULT);
+		}
 
 		localSyncFile.setVersion(remoteSyncFile.getVersion());
 
