ce3eb7106ce85f71cd2ff4194ccccc3561fed782
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2014-11-18 10:02:01 +0100

    LPS-51563 Shortcuts cannot be checked out/in from the actions toolbar as well as they cannot be checked out from the action button in the shortcut. All the actions from actions toolbar or the actions button apply to the shortcut itself, and not to the document it's pointing at

--- portal-impl/src/com/liferay/portlet/documentlibrary/action/EditEntryAction.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/action/EditEntryAction.java
@@ -41,7 +41,6 @@ import com.liferay.portlet.documentlibrary.SourceFileNameException;
 import com.liferay.portlet.documentlibrary.model.DLFileEntry;
 import com.liferay.portlet.documentlibrary.model.DLFileShortcut;
 import com.liferay.portlet.documentlibrary.model.DLFolder;
-import com.liferay.portlet.documentlibrary.service.DLAppLocalServiceUtil;
 import com.liferay.portlet.documentlibrary.service.DLAppServiceUtil;
 import com.liferay.portlet.trash.util.TrashUtil;
 
@@ -195,16 +194,6 @@ public class EditEntryAction extends PortletAction {
 		for (long fileEntryId : fileEntryIds) {
 			DLAppServiceUtil.cancelCheckOut(fileEntryId);
 		}
-
-		long[] fileShortcutIds = StringUtil.split(
-			ParamUtil.getString(actionRequest, "fileShortcutIds"), 0L);
-
-		for (long fileShortcutId : fileShortcutIds) {
-			DLFileShortcut fileShortcut = DLAppLocalServiceUtil.getFileShortcut(
-				fileShortcutId);
-
-			DLAppServiceUtil.cancelCheckOut(fileShortcut.getToFileEntryId());
-		}
 	}
 
 	protected void checkInEntries(ActionRequest actionRequest)
@@ -220,18 +209,6 @@ public class EditEntryAction extends PortletAction {
 			DLAppServiceUtil.checkInFileEntry(
 				fileEntryId, false, StringPool.BLANK, serviceContext);
 		}
-
-		long[] fileShortcutIds = StringUtil.split(
-			ParamUtil.getString(actionRequest, "fileShortcutIds"), 0L);
-
-		for (long fileShortcutId : fileShortcutIds) {
-			DLFileShortcut fileShortcut = DLAppLocalServiceUtil.getFileShortcut(
-				fileShortcutId);
-
-			DLAppServiceUtil.checkInFileEntry(
-				fileShortcut.getToFileEntryId(), false, StringPool.BLANK,
-				serviceContext);
-		}
 	}
 
 	protected void checkOutEntries(ActionRequest actionRequest)
@@ -245,17 +222,6 @@ public class EditEntryAction extends PortletAction {
 
 		for (long fileEntryId : fileEntryIds) {
 			DLAppServiceUtil.checkOutFileEntry(fileEntryId, serviceContext);
-		}
-
-		long[] fileShortcutIds = StringUtil.split(
-			ParamUtil.getString(actionRequest, "fileShortcutIds"), 0L);
-
-		for (long fileShortcutId : fileShortcutIds) {
-			DLFileShortcut fileShortcut = DLAppLocalServiceUtil.getFileShortcut(
-				fileShortcutId);
-
-			DLAppServiceUtil.checkOutFileEntry(
-				fileShortcut.getToFileEntryId(), serviceContext);
 		}
 	}
 
