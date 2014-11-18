jonathanmccann/liferay-portal

LPS-51175 Separate tracking groups, organizations, roles and user groups from search containers as this was truncating results when updating users. Handle adding and removing these separatly to avoid a situations like LPS-46353.

portal-web/docroot/html/portlet/users_admin/user/sites.jsp
@@ -36,6 +36,9 @@ currentURLObj.setParameter("historyKey", renderResponse.getNamespace() + "sites"
 	/>
 </liferay-util:buffer>
 
+<aui:input name="addSiteGroupIds" type="hidden" />
+<aui:input name="deleteSiteGroupIds" type="hidden" />
+
 <h3><liferay-ui:message key="sites" /></h3>
 
 <liferay-ui:search-container
@@ -93,6 +96,9 @@ currentURLObj.setParameter("historyKey", renderResponse.getNamespace() + "sites"
 	/>
 
 	<aui:script use="liferay-search-container">
+		var <portlet:namespace />addSiteGroupIds = [];
+		var <portlet:namespace />deleteSiteGroupIds = [];
+
 		var Util = Liferay.Util;
 
 		var handleOnSelect = A.one('#<portlet:namespace />selectSiteLink').on(
@@ -126,6 +132,19 @@ currentURLObj.setParameter("historyKey", renderResponse.getNamespace() + "sites"
 
 						searchContainer.addRow(rowColumns, event.groupid);
 						searchContainer.updateDataStore();
+
+						<portlet:namespace />addSiteGroupIds.push(event.groupid);
+
+						for (var i = 0; i < <portlet:namespace />deleteSiteGroupIds.length; i++) {
+							if (<portlet:namespace />deleteSiteGroupIds[i] == event.groupid) {
+								<portlet:namespace />deleteSiteGroupIds.splice(i, 1);
+
+								break;
+							}
+						}
+
+						document.<portlet:namespace />fm.<portlet:namespace />addSiteGroupIds.value = <portlet:namespace />addSiteGroupIds.join(',');
+						document.<portlet:namespace />fm.<portlet:namespace />deleteSiteGroupIds.value = <portlet:namespace />deleteSiteGroupIds.join(',');
 					}
 				);
 			}
@@ -152,6 +171,19 @@ currentURLObj.setParameter("historyKey", renderResponse.getNamespace() + "sites"
 				}
 
 				searchContainer.deleteRow(tr, rowId);
+
+				for (var i = 0; i < <portlet:namespace />addSiteGroupIds.length; i++) {
+					if (<portlet:namespace />addSiteGroupIds[i] == rowId) {
+						<portlet:namespace />addSiteGroupIds.splice(i, 1);
+
+						break;
+					}
+				}
+
+				<portlet:namespace />deleteSiteGroupIds.push(rowId);
+
+				document.<portlet:namespace />fm.<portlet:namespace />addSiteGroupIds.value = <portlet:namespace />addSiteGroupIds.join(',');
+				document.<portlet:namespace />fm.<portlet:namespace />deleteSiteGroupIds.value = <portlet:namespace />deleteSiteGroupIds.join(',');
 			},
 			'.modify-link'
 		);

liferay/liferay-central-test-2

Conflicts:
	portal-impl/src/com/liferay/portlet/usersadmin/util/UsersAdminImpl.java
	portal-service/src/com/liferay/portlet/usersadmin/util/UsersAdmin.java
	portal-service/src/com/liferay/portlet/usersadmin/util/UsersAdminUtil.java

portal-web/docroot/html/portlet/users_admin/user/sites.jsp
@@ -36,6 +36,9 @@ currentURLObj.setParameter("historyKey", renderResponse.getNamespace() + "sites"
 	/>
 </liferay-util:buffer>
 
+<aui:input name="addGroupIds" type="hidden" />
+<aui:input name="deleteGroupIds" type="hidden" />
+
 <h3><liferay-ui:message key="sites" /></h3>
 
 <liferay-ui:search-container
@@ -94,6 +97,9 @@ currentURLObj.setParameter("historyKey", renderResponse.getNamespace() + "sites"
 	/>
 
 	<aui:script use="liferay-search-container">
+		var <portlet:namespace />addGroupIds = [];
+		var <portlet:namespace />deleteGroupIds = [];
+
 		var Util = Liferay.Util;
 
 		var handleOnSelect = A.one('#<portlet:namespace />selectSiteLink').on(
@@ -127,6 +133,19 @@ currentURLObj.setParameter("historyKey", renderResponse.getNamespace() + "sites"
 
 						searchContainer.addRow(rowColumns, event.groupid);
 						searchContainer.updateDataStore();
+
+						<portlet:namespace />addGroupIds.push(event.groupid);
+
+						for (var i = 0; i < <portlet:namespace />deleteGroupIds.length; i++) {
+							if (<portlet:namespace />deleteGroupIds[i] == event.groupid) {
+								<portlet:namespace />deleteGroupIds.splice(i, 1);
+
+								break;
+							}
+						}
+
+						document.<portlet:namespace />fm.<portlet:namespace />addGroupIds.value = <portlet:namespace />addGroupIds.join(',');
+						document.<portlet:namespace />fm.<portlet:namespace />deleteGroupIds.value = <portlet:namespace />deleteGroupIds.join(',');
 					}
 				);
 			}
@@ -153,6 +172,19 @@ currentURLObj.setParameter("historyKey", renderResponse.getNamespace() + "sites"
 				}
 
 				searchContainer.deleteRow(tr, rowId);
+
+				for (var i = 0; i < <portlet:namespace />addGroupIds.length; i++) {
+					if (<portlet:namespace />addGroupIds[i] == rowId) {
+						<portlet:namespace />addGroupIds.splice(i, 1);
+
+						break;
+					}
+				}
+
+				<portlet:namespace />deleteGroupIds.push(rowId);
+
+				document.<portlet:namespace />fm.<portlet:namespace />addGroupIds.value = <portlet:namespace />addGroupIds.join(',');
+				document.<portlet:namespace />fm.<portlet:namespace />deleteGroupIds.value = <portlet:namespace />deleteGroupIds.join(',');
 			},
 			'.modify-link'
 		);