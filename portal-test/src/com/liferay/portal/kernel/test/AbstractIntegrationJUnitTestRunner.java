bf17f6f39297ba0138919734d4d25c510554f6d9
shuyangzhou <shuyang.zhou@liferay.com>
2014-11-24 10:19:49 -0800

    LPS-51777 Drop off the ExecutionTestListener infrastructure to make sure all future test infrastructure are implemented by TestRules

--- portal-test/src/com/liferay/portal/kernel/test/AbstractIntegrationJUnitTestRunner.java
+++ portal-test/src/com/liferay/portal/kernel/test/AbstractIntegrationJUnitTestRunner.java
@@ -16,9 +16,7 @@ package com.liferay.portal.kernel.test;
 
 import org.junit.runner.manipulation.Sorter;
 import org.junit.runners.BlockJUnit4ClassRunner;
-import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.InitializationError;
-import org.junit.runners.model.Statement;
 
 /**
  * @author Miguel Pastor
@@ -37,62 +35,9 @@ public abstract class AbstractIntegrationJUnitTestRunner
 			System.setProperty("external-properties", "portal-test.properties");
 		}
 
-		_testContextHandler = new TestContextHandler(clazz);
-
 		sort(new Sorter(new DescriptionComparator()));
 	}
 
 	public abstract void initApplicationContext();
-
-	@Override
-	protected Statement withAfterClasses(Statement statement) {
-		Statement withAfterClassesStatement = super.withAfterClasses(statement);
-
-		return new RunAfterTestClassesCallback(
-			withAfterClassesStatement, _testContextHandler);
-	}
-
-	/**
-	 * @deprecated As of 6.2.0
-	 */
-	@Deprecated
-	@Override
-	protected Statement withAfters(
-		FrameworkMethod frameworkMethod, Object instance, Statement statement) {
-
-		Statement withAftersStatement = super.withAfters(
-			frameworkMethod, instance, statement);
-
-		return new RunAfterTestMethodCallback(
-			instance, frameworkMethod.getMethod(), withAftersStatement,
-			_testContextHandler);
-	}
-
-	@Override
-	protected Statement withBeforeClasses(Statement statement) {
-		Statement withBeforeClassesStatement = super.withBeforeClasses(
-			statement);
-
-		return new RunBeforeTestClassesCallback(
-			withBeforeClassesStatement, _testContextHandler);
-	}
-
-	/**
-	 * @deprecated As of 6.2.0
-	 */
-	@Deprecated
-	@Override
-	protected Statement withBefores(
-		FrameworkMethod frameworkMethod, Object instance, Statement statement) {
-
-		Statement withBeforesStatement = super.withBefores(
-			frameworkMethod, instance, statement);
-
-		return new RunBeforeTestMethodCallback(
-			instance, frameworkMethod.getMethod(), withBeforesStatement,
-			_testContextHandler);
-	}
-
-	private final TestContextHandler _testContextHandler;
 
 }
