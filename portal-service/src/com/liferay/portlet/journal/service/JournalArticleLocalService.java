brianchandotcom/liferay-portal

LPS-51384 regen svcs

portal-service/src/com/liferay/portlet/journal/service/JournalArticleLocalService.java
@@ -1420,6 +1420,9 @@ public interface JournalArticleLocalService extends BaseLocalService,
 	*/
 	public java.lang.String getBeanIdentifier();
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portlet.journal.model.JournalArticle> getByNoPermissions();
+
 	/**
 	* Returns an ordered range of all the web content articles matching the
 	* company and workflow status.
@@ -1799,6 +1802,9 @@ public interface JournalArticleLocalService extends BaseLocalService,
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public double getLatestVersion(long groupId, java.lang.String articleId,
 		int status) throws com.liferay.portal.kernel.exception.PortalException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portlet.journal.model.JournalArticle> getNoAssetArticles();
 
 	/**
 	* Returns the number of web content articles that are not recycled.

portal-service/src/com/liferay/portlet/journal/service/JournalArticleLocalServiceUtil.java
@@ -1581,6 +1581,10 @@ public class JournalArticleLocalServiceUtil {
 		return getService().getBeanIdentifier();
 	}
 
+	public static java.util.List<com.liferay.portlet.journal.model.JournalArticle> getByNoPermissions() {
+		return getService().getByNoPermissions();
+	}
+
 	/**
 	* Returns an ordered range of all the web content articles matching the
 	* company and workflow status.
@@ -1991,6 +1995,10 @@ public class JournalArticleLocalServiceUtil {
 		java.lang.String articleId, int status)
 		throws com.liferay.portal.kernel.exception.PortalException {
 		return getService().getLatestVersion(groupId, articleId, status);
+	}
+
+	public static java.util.List<com.liferay.portlet.journal.model.JournalArticle> getNoAssetArticles() {
+		return getService().getNoAssetArticles();
 	}
 
 	/**

portal-service/src/com/liferay/portlet/journal/service/JournalArticleLocalServiceWrapper.java
@@ -1650,6 +1650,11 @@ public class JournalArticleLocalServiceWrapper
 		return _journalArticleLocalService.getBeanIdentifier();
 	}
 
+	@Override
+	public java.util.List<com.liferay.portlet.journal.model.JournalArticle> getByNoPermissions() {
+		return _journalArticleLocalService.getByNoPermissions();
+	}
+
 	/**
 	* Returns an ordered range of all the web content articles matching the
 	* company and workflow status.
@@ -2088,6 +2093,11 @@ public class JournalArticleLocalServiceWrapper
 		int status) throws com.liferay.portal.kernel.exception.PortalException {
 		return _journalArticleLocalService.getLatestVersion(groupId, articleId,
 			status);
+	}
+
+	@Override
+	public java.util.List<com.liferay.portlet.journal.model.JournalArticle> getNoAssetArticles() {
+		return _journalArticleLocalService.getNoAssetArticles();
 	}
 
 	/**

liferay/liferay-central-test-2

portal-service/src/com/liferay/portlet/journal/service/JournalArticleLocalService.java
@@ -1800,6 +1800,12 @@ public interface JournalArticleLocalService extends BaseLocalService,
 	public double getLatestVersion(long groupId, java.lang.String articleId,
 		int status) throws com.liferay.portal.kernel.exception.PortalException;
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portlet.journal.model.JournalArticle> getNoAssetArticles();
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portlet.journal.model.JournalArticle> getNoPermissionArticles();
+
 	/**
 	* Returns the number of web content articles that are not recycled.
 	*

portal-service/src/com/liferay/portlet/journal/service/JournalArticleLocalServiceUtil.java
@@ -1993,6 +1993,14 @@ public class JournalArticleLocalServiceUtil {
 		return getService().getLatestVersion(groupId, articleId, status);
 	}
 
+	public static java.util.List<com.liferay.portlet.journal.model.JournalArticle> getNoAssetArticles() {
+		return getService().getNoAssetArticles();
+	}
+
+	public static java.util.List<com.liferay.portlet.journal.model.JournalArticle> getNoPermissionArticles() {
+		return getService().getNoPermissionArticles();
+	}
+
 	/**
 	* Returns the number of web content articles that are not recycled.
 	*

portal-service/src/com/liferay/portlet/journal/service/JournalArticleLocalServiceWrapper.java
@@ -2090,6 +2090,16 @@ public class JournalArticleLocalServiceWrapper
 			status);
 	}
 
+	@Override
+	public java.util.List<com.liferay.portlet.journal.model.JournalArticle> getNoAssetArticles() {
+		return _journalArticleLocalService.getNoAssetArticles();
+	}
+
+	@Override
+	public java.util.List<com.liferay.portlet.journal.model.JournalArticle> getNoPermissionArticles() {
+		return _journalArticleLocalService.getNoPermissionArticles();
+	}
+
 	/**
 	* Returns the number of web content articles that are not recycled.
 	*