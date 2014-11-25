18c312e24c169ae633d543826fbdeabbe0bd2664
Máté Thurzó <mate.thurzo@liferay.com>
2014-11-25 16:57:24 +0100

    LPS-51672 Add test for this

---
+++ portal-impl/test/integration/com/liferay/portal/lar/PortletDataContextZipWriterTest.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.lar;
+
+import com.liferay.portal.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.lar.PortletDataContextFactoryUtil;
+import com.liferay.portal.kernel.zip.ZipWriter;
+import com.liferay.portal.test.LiferayIntegrationTestRule;
+import com.liferay.portal.util.test.RandomTestUtil;
+import com.liferay.portal.util.test.TestPropsValues;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+import org.powermock.api.mockito.PowerMockito;
+
+/**
+ * @author Mate Thurzo
+ */
+public class PortletDataContextZipWriterTest extends PowerMockito {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_zipWriter = mock(ZipWriter.class);
+
+		_portletDataContext =
+			PortletDataContextFactoryUtil.createExportPortletDataContext(
+				TestPropsValues.getCompanyId(), TestPropsValues.getGroupId(),
+				null, null, null, _zipWriter);
+	}
+
+	@Test
+	public void testMultipleByteArrayAddition() throws Exception {
+		byte[] bytes = {Byte.MIN_VALUE, Byte.MAX_VALUE};
+
+		_portletDataContext.addZipEntry(_PATH, bytes);
+
+		Mockito.verify(_zipWriter, Mockito.times(1)).addEntry(_PATH, bytes);
+
+		_portletDataContext.addZipEntry(_PATH, bytes);
+
+		//Verify that it's not called again
+
+		Mockito.verify(_zipWriter, Mockito.times(1)).addEntry(_PATH, bytes);
+	}
+
+	@Test
+	public void testMultipleInputStreamAddition() throws Exception {
+		byte[] bytes = {Byte.MIN_VALUE, Byte.MAX_VALUE};
+
+		InputStream is = new ByteArrayInputStream(bytes);
+
+		_portletDataContext.addZipEntry(_PATH, is);
+
+		Mockito.verify(_zipWriter, Mockito.times(1)).addEntry(_PATH, is);
+
+		_portletDataContext.addZipEntry(_PATH, is);
+
+		//Verify that it's not called again
+
+		Mockito.verify(_zipWriter, Mockito.times(1)).addEntry(_PATH, is);
+	}
+
+	@Test
+	public void testMultipleStringAddition() throws Exception {
+		String string = RandomTestUtil.randomString();
+
+		_portletDataContext.addZipEntry(_PATH, string);
+
+		Mockito.verify(_zipWriter, Mockito.times(1)).addEntry(_PATH, string);
+
+		_portletDataContext.addZipEntry(_PATH, string);
+
+		//Verify that it's not called again
+
+		Mockito.verify(_zipWriter, Mockito.times(1)).addEntry(_PATH, string);
+	}
+
+	private static final String _PATH = "/test.xml";
+
+	private PortletDataContext _portletDataContext;
+	private ZipWriter _zipWriter;
+
+}
