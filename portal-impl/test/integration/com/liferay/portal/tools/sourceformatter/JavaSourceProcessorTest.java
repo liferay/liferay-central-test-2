1c66168772d7ef38af5a7b7d94169d0748e52d80
Cristina González Castellano <cristina.gonzalez@liferay.com>
2014-11-26 17:04:16 +0100

    LPS-51849 Test that reproduce the bug

--- portal-impl/test/integration/com/liferay/portal/tools/sourceformatter/JavaSourceProcessorTest.java
+++ portal-impl/test/integration/com/liferay/portal/tools/sourceformatter/JavaSourceProcessorTest.java
@@ -148,6 +148,11 @@ public class JavaSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
+	public void testParametersWithAnnotation() throws Throwable {
+		test("JavaClassMethodParameterAnnotated.testjava");
+	}
+
+	@Test
 	public void testSecureRandomNumberGeneration() throws Exception {
 		test(
 			"SecureRandomNumberGeneration.testjava",
