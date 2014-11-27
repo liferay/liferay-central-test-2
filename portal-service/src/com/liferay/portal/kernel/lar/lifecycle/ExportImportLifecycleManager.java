4cffae7ebd1e9227d85207e6c5361c5a2483ad5e
Daniel Kocsis <daniel.kocsis@liferay.com>
2014-11-27 14:51:02 +0100

    LPS-51795 Add basic manager class to fire lifecycle events

---
+++ portal-service/src/com/liferay/portal/kernel/lar/lifecycle/ExportImportLifecycleManager.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.lar.lifecycle;
+
+import com.liferay.portal.kernel.messaging.DestinationNames;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageBusUtil;
+
+import java.io.Serializable;
+
+/**
+ * @author Daniel Kocsis
+ */
+public class ExportImportLifecycleManager {
+
+	public static void fireExportImportLifecycleEvent(
+		int code, Serializable... arguments) {
+
+		Message message = new Message();
+
+		ExportImportLifecycleEvent exportImportLifecycleEvent =
+			ExportImportLifecycleEventFactoryUtil.create(code, arguments);
+
+		message.put("event", exportImportLifecycleEvent);
+
+		MessageBusUtil.sendMessage(
+			DestinationNames.EXPORT_IMPORT_LIFECYCLE_EVENT_SYNC, message);
+		MessageBusUtil.sendMessage(
+			DestinationNames.EXPORT_IMPORT_LIFECYCLE_EVENT_ASYNC, message);
+	}
+
+}
