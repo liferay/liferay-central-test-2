danielkocsis/liferay-portal

LPS-51795 Add implementation and factory to create lifecycle events

portal-impl/src/com/liferay/portal/lar/lifecycle/ExportImportLifecycleEventFactoryImpl.java
@@ -1,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.lar.lifecycle;
+
+import com.liferay.portal.kernel.lar.lifecycle.ExportImportLifecycleEvent;
+import com.liferay.portal.kernel.lar.lifecycle.ExportImportLifecycleEventFactory;
+
+import java.io.Serializable;
+
+/**
+ * @author Daniel Kocsis
+ */
+public class ExportImportLifecycleEventFactoryImpl
+	implements ExportImportLifecycleEventFactory {
+
+	@Override
+	public ExportImportLifecycleEvent createExportImportLifecycleEvent(
+		int eventCode, Serializable... attributes) {
+
+		ExportImportLifecycleEvent exportImportLifecycleEvent =
+			new ExportImportLifecycleEventImpl();
+
+		exportImportLifecycleEvent.setEventCode(eventCode);
+
+		for (Serializable attribute : attributes) {
+			exportImportLifecycleEvent.addEventAttribute(attribute);
+		}
+
+		return exportImportLifecycleEvent;
+	}
+
+}

portal-service/src/com/liferay/portal/kernel/lar/lifecycle/ExportImportLifecycleEventFactory.java
@@ -1,0 +1,27 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.lar.lifecycle;
+
+import java.io.Serializable;
+
+/**
+ * @author Daniel Kocsis
+ */
+public interface ExportImportLifecycleEventFactory {
+
+	public ExportImportLifecycleEvent createExportImportLifecycleEvent(
+		int eventCode, Serializable... attributes);
+
+}

portal-service/src/com/liferay/portal/kernel/lar/lifecycle/ExportImportLifecycleEventFactoryUtil.java
@@ -1,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.lar.lifecycle;
+
+import com.liferay.portal.kernel.security.pacl.permission.PortalRuntimePermission;
+
+import java.io.Serializable;
+
+/**
+ * @author Daniel Kocsis
+ */
+public class ExportImportLifecycleEventFactoryUtil {
+
+	public static ExportImportLifecycleEvent createLifecycleEvent(
+		int eventCode, Serializable... attributes) {
+
+		return getExportImportLifecycleEventFactory().
+			createExportImportLifecycleEvent(eventCode, attributes);
+	}
+
+	public static ExportImportLifecycleEventFactory
+		getExportImportLifecycleEventFactory() {
+
+		PortalRuntimePermission.checkGetBeanProperty(
+			ExportImportLifecycleEventFactoryUtil.class);
+
+		return _exportImportLifecycleEventFactory;
+	}
+
+	public void setExportImportLifecycleEventFactory(
+		ExportImportLifecycleEventFactory exportImportLifecycleEventFactory) {
+
+		PortalRuntimePermission.checkSetBeanProperty(getClass());
+
+		_exportImportLifecycleEventFactory = exportImportLifecycleEventFactory;
+	}
+
+	private static ExportImportLifecycleEventFactory
+		_exportImportLifecycleEventFactory;
+
+}

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portal/lar/lifecycle/ExportImportLifecycleEventFactoryImpl.java
@@ -1,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.lar.lifecycle;
+
+import com.liferay.portal.kernel.lar.lifecycle.ExportImportLifecycleEvent;
+import com.liferay.portal.kernel.lar.lifecycle.ExportImportLifecycleEventFactory;
+
+import java.io.Serializable;
+
+/**
+ * @author Daniel Kocsis
+ */
+public class ExportImportLifecycleEventFactoryImpl
+	implements ExportImportLifecycleEventFactory {
+
+	@Override
+	public ExportImportLifecycleEvent create(
+		int code, Serializable... attributes) {
+
+		ExportImportLifecycleEvent exportImportLifecycleEvent =
+			new ExportImportLifecycleEventImpl();
+
+		exportImportLifecycleEvent.setCode(code);
+
+		for (Serializable attribute : attributes) {
+			exportImportLifecycleEvent.addAttribute(attribute);
+		}
+
+		return exportImportLifecycleEvent;
+	}
+
+}

portal-service/src/com/liferay/portal/kernel/lar/lifecycle/ExportImportLifecycleEventFactory.java
@@ -1,0 +1,27 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.lar.lifecycle;
+
+import java.io.Serializable;
+
+/**
+ * @author Daniel Kocsis
+ */
+public interface ExportImportLifecycleEventFactory {
+
+	public ExportImportLifecycleEvent create(
+		int code, Serializable... attributes);
+
+}

portal-service/src/com/liferay/portal/kernel/lar/lifecycle/ExportImportLifecycleEventFactoryUtil.java
@@ -1,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.lar.lifecycle;
+
+import com.liferay.portal.kernel.security.pacl.permission.PortalRuntimePermission;
+
+import java.io.Serializable;
+
+/**
+ * @author Daniel Kocsis
+ */
+public class ExportImportLifecycleEventFactoryUtil {
+
+	public static ExportImportLifecycleEvent create(
+		int code, Serializable... attributes) {
+
+		return getExportImportLifecycleEventFactory().create(code, attributes);
+	}
+
+	public static ExportImportLifecycleEventFactory
+		getExportImportLifecycleEventFactory() {
+
+		PortalRuntimePermission.checkGetBeanProperty(
+			ExportImportLifecycleEventFactoryUtil.class);
+
+		return _exportImportLifecycleEventFactory;
+	}
+
+	public void setExportImportLifecycleEventFactory(
+		ExportImportLifecycleEventFactory exportImportLifecycleEventFactory) {
+
+		PortalRuntimePermission.checkSetBeanProperty(getClass());
+
+		_exportImportLifecycleEventFactory = exportImportLifecycleEventFactory;
+	}
+
+	private static ExportImportLifecycleEventFactory
+		_exportImportLifecycleEventFactory;
+
+}