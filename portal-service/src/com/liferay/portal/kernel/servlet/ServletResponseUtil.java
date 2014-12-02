25c920754f4bd3dedb21f3f56a7f6f3f010c30ec
Jonathan McCann <jonathan.mccann@liferay.com>
2014-12-02 15:24:12 -0800

    LPS-16196 Partial revert of LPS-44255 in order to put the correct (long) content length into the response header

--- portal-service/src/com/liferay/portal/kernel/servlet/ServletResponseUtil.java
+++ portal-service/src/com/liferay/portal/kernel/servlet/ServletResponseUtil.java
@@ -563,7 +563,8 @@ public class ServletResponseUtil {
 		}
 
 		if (contentLength > 0) {
-			response.setContentLength((int)contentLength);
+			response.setHeader(
+				HttpHeaders.CONTENT_LENGTH, String.valueOf(contentLength));
 		}
 
 		response.flushBuffer();
