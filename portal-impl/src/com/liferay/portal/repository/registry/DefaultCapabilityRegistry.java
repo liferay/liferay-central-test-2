4890678e47c2352d0c9e427fb8f2910e4ea750d5
adolfo.perez <adolfo.perez@liferay.com>
2014-12-04 15:44:14 +0100

    LPS-51940 create class to manage capabilities

---
+++ portal-impl/src/com/liferay/portal/repository/registry/DefaultCapabilityRegistry.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.repository.registry;
+
+import com.liferay.portal.kernel.repository.DocumentRepository;
+import com.liferay.portal.kernel.repository.capabilities.BaseCapabilityProvider;
+import com.liferay.portal.kernel.repository.capabilities.Capability;
+import com.liferay.portal.kernel.repository.capabilities.CapabilityProvider;
+import com.liferay.portal.kernel.repository.registry.CapabilityRegistry;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class DefaultCapabilityRegistry
+	extends BaseCapabilityProvider
+	implements CapabilityRegistry, CapabilityProvider {
+
+	public DefaultCapabilityRegistry(DocumentRepository documentRepository) {
+		_documentRepository = documentRepository;
+	}
+
+	@Override
+	public <S extends Capability, T extends S> void addExportedCapability(
+		Class<S> capabilityClass, T capability) {
+
+		super.addExportedCapability(capabilityClass, capability);
+	}
+
+	@Override
+	public <S extends Capability, T extends S> void addSupportedCapability(
+		Class<S> capabilityClass, T capability) {
+
+		super.addSupportedCapability(capabilityClass, capability);
+	}
+
+	@Override
+	public DocumentRepository getDocumentRepository() {
+		return _documentRepository;
+	}
+
+	@Override
+	protected String getProviderKey() {
+		return String.valueOf(_documentRepository.getRepositoryId());
+	}
+
+	private final DocumentRepository _documentRepository;
+
+}
