d818a540937bb1c449d196e81c240b3a4cfb2e12
Hugo Huijser <hugo.huijser@liferay.com>
2014-12-05 11:51:06 -0800

    SourceFormatter - Add tests

--- portal-impl/test/integration/com/liferay/portal/tools/sourceformatter/JavaSourceProcessorTest.java
+++ portal-impl/test/integration/com/liferay/portal/tools/sourceformatter/JavaSourceProcessorTest.java
@@ -85,10 +85,10 @@ public class JavaSourceProcessorTest extends BaseSourceProcessorTestCase {
 				"line break:", "line break:", "line break:", "line break:",
 				"line break:", "line break:", "line break:", "line break:",
 				"line break:", "line break:", "line break:", "line break:",
-				"line break:", "line break:"
+				"line break:", "line break:", "line break:", "line break:"
 			},
 			new Integer[] {
-				23, 27, 31, 38, 42, 45, 48, 52, 55, 60, 66, 70, 74, 77
+				23, 27, 31, 38, 42, 45, 48, 52, 55, 60, 66, 70, 76, 84, 87, 94
 			});
 	}
 
