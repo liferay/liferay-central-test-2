0983ba93da65554e42b832c0b3a8b9f5c233d78c
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-12-10 12:28:34 +0100

    LPS-52093 Add new configuration class

---
+++ modules/apps/asset/categories-navigation-web/src/com/liferay/asset/categories/navigation/web/portlet/action/CategoriesNavigationConfigurationAction.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.navigation.web.portlet.action;
+
+import com.liferay.portal.kernel.portlet.ConfigurationAction;
+import com.liferay.portal.kernel.portlet.DefaultConfigurationAction;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=com_liferay_asset_categories_navigation_web_portlet_CategoriesNavigationPortlet"
+	},
+	service = ConfigurationAction.class
+)
+public class CategoriesNavigationConfigurationAction
+	extends DefaultConfigurationAction {
+}
