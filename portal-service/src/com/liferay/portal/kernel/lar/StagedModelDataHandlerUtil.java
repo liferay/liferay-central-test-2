6bba034daa91c72e790dc12fcec5c9dc82a14549
shuyangzhou <shuyang.zhou@liferay.com>
2014-12-16 09:37:51 -0800

    LPS-52219 Force sync last session state after each staged model importing to keep hibernate 1st level cache size under control

--- portal-service/src/com/liferay/portal/kernel/lar/StagedModelDataHandlerUtil.java
+++ portal-service/src/com/liferay/portal/kernel/lar/StagedModelDataHandlerUtil.java
@@ -17,6 +17,7 @@ package com.liferay.portal.kernel.lar;
 import aQute.bnd.annotation.ProviderType;
 
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.spring.orm.LastSessionRecorderHelperUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
@@ -295,6 +296,8 @@ public class StagedModelDataHandlerUtil {
 
 		stagedModelDataHandler.importStagedModel(
 			portletDataContext, stagedModel);
+
+		LastSessionRecorderHelperUtil.syncLastSessionState();
 	}
 
 	private static StagedModel _getReferenceStagedModel(
