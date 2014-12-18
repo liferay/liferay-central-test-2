5ae8a36e32218dd91a5efc7c520fd20ad08e10e4
John Zhao <john.zhao@liferay.com>
2014-12-18 15:02:51 +0800

    LPS-52196 Add method 'removeUndefinedLocaleValue' to guarantee not losing field for translated contents.

--- portal-web/docroot/html/js/liferay/ddm_form.js
+++ portal-web/docroot/html/js/liferay/ddm_form.js
@@ -449,6 +449,26 @@ AUI.add(
 						instance.get('container').remove(true);
 					},
 
+					removeUndefinedLocaleValue: function() {
+						var instance = this;
+
+						var parent = instance.get('parent');
+
+						var translationManager = parent.get('translationManager');
+
+						var availableLocales = translationManager.get('availableLocales');
+
+						var localizationMap = instance.get('localizationMap');
+
+						for (var i = 0; i < availableLocales.length; i++) {
+							var value = localizationMap[availableLocales[i]];
+
+							if (Lang.isUndefined(value)) {
+								localizationMap[availableLocales[i]] = instance.getValue();
+							}
+						}
+					},
+
 					renderRepeatableUI: function() {
 						var instance = this;
 
@@ -573,6 +593,8 @@ AUI.add(
 
 						if (dataType) {
 							instance.updateLocalizationMap(instance.get('displayLocale'));
+
+							instance.removeUndefinedLocaleValue();
 
 							fieldJSON.value = instance.get('localizationMap');
 						}
