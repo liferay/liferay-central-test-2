b0951cb218f43a1a8f9650bd0e176efc9a4f4267
Cristina González Castellano <cristina.gonzalez@liferay.com>
2014-12-18 16:47:13 -0800

    LPS-52290 Execute ANT directly, to avoid JDK settings problems.

--- modules/sdk/ant-arquillian/src/com/liferay/ant/arquillian/WebArchiveBuilder.java
+++ modules/sdk/ant-arquillian/src/com/liferay/ant/arquillian/WebArchiveBuilder.java
@@ -14,12 +14,13 @@
 
 package com.liferay.ant.arquillian;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.InputStreamReader;
+import org.apache.log4j.Logger;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.ProjectHelper;
 import org.apache.tools.ant.listener.Log4jListener;
-
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 
@@ -32,12 +33,31 @@ public class WebArchiveBuilder {
 	public static WebArchive build() {
 		File tempDir = new File(System.getProperty("java.io.tmpdir"));
 
+		try {
+			ProcessBuilder processBuilder = new ProcessBuilder(
+				"ant", "direct-deploy",
+				"-Dapp.server.deploy.dir=" + tempDir.getAbsolutePath() ,
+				"-Dauto.deploy.unpack.war=false");
+
+			Process process = processBuilder.start();
+
+			process.waitFor();
+
+			BufferedReader reader = new BufferedReader(
+				new InputStreamReader(process.getInputStream()));
+
+			String line = reader.readLine();
+			while (line != null) {
+				_logger.debug(line);
+
+				line = reader.readLine();
+			}
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
 		Project project = _getProject();
-
-		project.setProperty("app.server.deploy.dir", tempDir.getAbsolutePath());
-		project.setProperty("auto.deploy.unpack.war", "false");
-
-		project.executeTarget("direct-deploy");
 
 		File warFile = new File(
 			tempDir.getAbsolutePath(),
@@ -65,5 +85,7 @@ public class WebArchiveBuilder {
 
 		return project;
 	}
+
+	private static Logger _logger = Logger.getLogger(WebArchiveBuilder.class);
 
 }
