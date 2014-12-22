2ced356b12e15daaac66463c5ef0f22829e4b715
Matthew Tambara <matthew.tambara@liferay.com>
2014-12-22 09:57:50 -0800

    LPS-52291 Merge setUp(), testAddRelationWithBiType(), and testAddRelationWithUniType() into setUpClass()

--- portal-impl/test/integration/com/liferay/portlet/social/service/SocialRelationLocalServiceTest.java
+++ portal-impl/test/integration/com/liferay/portlet/social/service/SocialRelationLocalServiceTest.java
@@ -31,7 +31,7 @@ import com.liferay.portlet.social.model.SocialRelationConstants;
 import java.util.List;
 
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.FixMethodOrder;
 import org.junit.Rule;
@@ -50,17 +50,16 @@ public class SocialRelationLocalServiceTest {
 		new AggregateTestRule(
 			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE);
 
-	@Before
-	public void setUp() throws Exception {
+	@BeforeClass
+	public static void setUpClass() throws Exception {
 		for (String screenNamePrefix : new String[] {"dlc", "fra"}) {
 			for (int i = 1; i <= 9; i++) {
 				UserTestUtil.addUser(screenNamePrefix + i, false, null);
 			}
 		}
-	}
 
-	@Test
-	public void testAddRelationWithBiType() throws Exception {
+		// Bi Type
+
 		User dlc1User = UserLocalServiceUtil.getUserByScreenName(
 			TestPropsValues.getCompanyId(), "dlc1");
 
@@ -147,10 +146,9 @@ public class SocialRelationLocalServiceTest {
 		SocialRelationLocalServiceUtil.addRelation(
 			dlc1User.getUserId(), dlc2User.getUserId(),
 			SocialRelationConstants.TYPE_BI_ROMANTIC_PARTNER);
-	}
 
-	@Test
-	public void testAddRelationWithUniType() throws Exception {
+		// Uni Type
+
 		User fra1User = UserLocalServiceUtil.getUserByScreenName(
 			TestPropsValues.getCompanyId(), "fra1");
 
