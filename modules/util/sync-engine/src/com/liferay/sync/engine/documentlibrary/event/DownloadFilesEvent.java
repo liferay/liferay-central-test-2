shinnlok/liferay-portal

sync-engine - Apply batch download

modules/util/sync-engine/src/com/liferay/sync/engine/documentlibrary/event/DownloadFilesEvent.java
@@ -1,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sync.engine.documentlibrary.event;
+
+import com.liferay.sync.engine.documentlibrary.handler.DownloadFilesHandler;
+import com.liferay.sync.engine.documentlibrary.handler.Handler;
+import com.liferay.sync.engine.model.SyncAccount;
+import com.liferay.sync.engine.service.SyncAccountService;
+import com.liferay.sync.engine.session.Session;
+import com.liferay.sync.engine.session.SessionManager;
+
+import java.util.Map;
+
+/**
+ * @author Shinn Lok
+ */
+public class DownloadFilesEvent extends BaseEvent {
+
+	public DownloadFilesEvent(
+		long syncAccountId, Map<String, Object> parameters) {
+
+		super(syncAccountId, _URL_PATH, parameters);
+
+		_handler = new DownloadFilesHandler(this);
+	}
+
+	@Override
+	public Handler<Void> getHandler() {
+		return _handler;
+	}
+
+	@Override
+	protected void processRequest() throws Exception {
+		SyncAccount syncAccount = SyncAccountService.fetchSyncAccount(
+			getSyncAccountId());
+
+		Session session = SessionManager.getSession(getSyncAccountId());
+
+		session.executePost(
+			syncAccount.getUrl() + _URL_PATH, getParameters(), _handler, true);
+	}
+
+	private static final String _URL_PATH = "/sync-web/download/zip";
+
+	private final Handler<Void> _handler;
+
+}

modules/util/sync-engine/src/com/liferay/sync/engine/documentlibrary/handler/DownloadFilesHandler.java
@@ -1,0 +1,141 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sync.engine.documentlibrary.handler;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.liferay.sync.engine.documentlibrary.event.Event;
+import com.liferay.sync.engine.model.SyncFile;
+import com.liferay.sync.engine.session.Session;
+import com.liferay.sync.engine.session.SessionManager;
+import com.liferay.sync.engine.util.StreamUtil;
+
+import java.io.InputStream;
+
+import java.nio.file.Paths;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.apache.commons.io.input.CountingInputStream;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+* @author Shinn Lok
+*/
+public class DownloadFilesHandler extends BaseHandler {
+
+	public DownloadFilesHandler(Event event) {
+		super(event);
+	}
+
+	@Override
+	protected void doHandleResponse(HttpResponse httpResponse)
+		throws Exception {
+
+		final Session session = SessionManager.getSession(getSyncAccountId());
+
+		Header tokenHeader = httpResponse.getFirstHeader("Sync-JWT");
+
+		if (tokenHeader != null) {
+			session.setToken(tokenHeader.getValue());
+		}
+
+		Map<String, DownloadFileHandler> handlers =
+			(Map<String, DownloadFileHandler>)getParameterValue("handlers");
+
+		InputStream inputStream = null;
+
+		try {
+			HttpEntity httpEntity = httpResponse.getEntity();
+
+			inputStream = new CountingInputStream(httpEntity.getContent()) {
+
+				@Override
+				protected synchronized void afterRead(int n) {
+					session.incrementDownloadedBytes(n);
+
+					super.afterRead(n);
+				}
+
+			};
+
+			ZipInputStream zipInputStream = new ZipInputStream(inputStream);
+
+			ZipEntry zipEntry = null;
+
+			while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+				String zipEntryName = zipEntry.getName();
+
+				if (zipEntryName.equals("manifest.json")) {
+					ObjectMapper objectMapper = new ObjectMapper();
+
+					JsonNode rootJsonNode = objectMapper.readTree(
+						zipInputStream);
+
+					Iterator<Map.Entry<String, JsonNode>> fields =
+						rootJsonNode.fields();
+
+					while (fields.hasNext()) {
+						Map.Entry<String, JsonNode> field = fields.next();
+
+						Handler handler = handlers.get(field.getKey());
+
+						JsonNode fieldValue = field.getValue();
+
+						String exception = handler.getException(
+							fieldValue.textValue());
+
+						handler.handlePortalException(exception);
+					}
+
+					break;
+				}
+
+				DownloadFileHandler downloadFileHandler = handlers.get(
+					zipEntryName);
+
+				SyncFile syncFile =
+					(SyncFile)downloadFileHandler.getParameterValue("syncFile");
+
+				if (downloadFileHandler.isUnsynced(syncFile)) {
+					continue;
+				}
+
+				downloadFileHandler.copyFile(
+					syncFile, Paths.get(syncFile.getFilePathName()),
+					zipInputStream);
+			}
+		}
+		catch (Exception e) {
+			_logger.debug(e.getMessage(), e);
+		}
+		finally {
+			StreamUtil.cleanUp(inputStream);
+		}
+	}
+
+	private static final Logger _logger = LoggerFactory.getLogger(
+		DownloadFilesHandler.class);
+
+}

liferay/liferay-central-test-2

modules/util/sync-engine/src/com/liferay/sync/engine/documentlibrary/event/DownloadFilesEvent.java
@@ -1,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sync.engine.documentlibrary.event;
+
+import com.liferay.sync.engine.documentlibrary.handler.DownloadFilesHandler;
+import com.liferay.sync.engine.documentlibrary.handler.Handler;
+import com.liferay.sync.engine.model.SyncAccount;
+import com.liferay.sync.engine.service.SyncAccountService;
+import com.liferay.sync.engine.session.Session;
+import com.liferay.sync.engine.session.SessionManager;
+
+import java.util.Map;
+
+/**
+ * @author Shinn Lok
+ */
+public class DownloadFilesEvent extends BaseEvent {
+
+	public DownloadFilesEvent(
+		long syncAccountId, Map<String, Object> parameters) {
+
+		super(syncAccountId, _URL_PATH, parameters);
+
+		_handler = new DownloadFilesHandler(this);
+	}
+
+	@Override
+	public Handler<Void> getHandler() {
+		return _handler;
+	}
+
+	@Override
+	protected void processRequest() throws Exception {
+		SyncAccount syncAccount = SyncAccountService.fetchSyncAccount(
+			getSyncAccountId());
+
+		Session session = SessionManager.getSession(getSyncAccountId());
+
+		session.executeAsynchronousPost(
+			syncAccount.getUrl() + _URL_PATH, getParameters(), _handler, true);
+	}
+
+	private static final String _URL_PATH = "/sync-web/download/zip";
+
+	private final Handler<Void> _handler;
+
+}

modules/util/sync-engine/src/com/liferay/sync/engine/documentlibrary/handler/DownloadFilesHandler.java
@@ -1,0 +1,141 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sync.engine.documentlibrary.handler;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.liferay.sync.engine.documentlibrary.event.Event;
+import com.liferay.sync.engine.model.SyncFile;
+import com.liferay.sync.engine.session.Session;
+import com.liferay.sync.engine.session.SessionManager;
+import com.liferay.sync.engine.util.StreamUtil;
+
+import java.io.InputStream;
+
+import java.nio.file.Paths;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.apache.commons.io.input.CountingInputStream;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+* @author Shinn Lok
+*/
+public class DownloadFilesHandler extends BaseHandler {
+
+	public DownloadFilesHandler(Event event) {
+		super(event);
+	}
+
+	@Override
+	protected void doHandleResponse(HttpResponse httpResponse)
+		throws Exception {
+
+		final Session session = SessionManager.getSession(getSyncAccountId());
+
+		Header tokenHeader = httpResponse.getFirstHeader("Sync-JWT");
+
+		if (tokenHeader != null) {
+			session.setToken(tokenHeader.getValue());
+		}
+
+		Map<String, DownloadFileHandler> handlers =
+			(Map<String, DownloadFileHandler>)getParameterValue("handlers");
+
+		InputStream inputStream = null;
+
+		try {
+			HttpEntity httpEntity = httpResponse.getEntity();
+
+			inputStream = new CountingInputStream(httpEntity.getContent()) {
+
+				@Override
+				protected synchronized void afterRead(int n) {
+					session.incrementDownloadedBytes(n);
+
+					super.afterRead(n);
+				}
+
+			};
+
+			ZipInputStream zipInputStream = new ZipInputStream(inputStream);
+
+			ZipEntry zipEntry = null;
+
+			while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+				String zipEntryName = zipEntry.getName();
+
+				if (zipEntryName.equals("errors.json")) {
+					ObjectMapper objectMapper = new ObjectMapper();
+
+					JsonNode rootJsonNode = objectMapper.readTree(
+						zipInputStream);
+
+					Iterator<Map.Entry<String, JsonNode>> fields =
+						rootJsonNode.fields();
+
+					while (fields.hasNext()) {
+						Map.Entry<String, JsonNode> field = fields.next();
+
+						Handler handler = handlers.get(field.getKey());
+
+						JsonNode fieldValue = field.getValue();
+
+						String exception = handler.getException(
+							fieldValue.textValue());
+
+						handler.handlePortalException(exception);
+					}
+
+					break;
+				}
+
+				DownloadFileHandler downloadFileHandler = handlers.get(
+					zipEntryName);
+
+				SyncFile syncFile =
+					(SyncFile)downloadFileHandler.getParameterValue("syncFile");
+
+				if (downloadFileHandler.isUnsynced(syncFile)) {
+					continue;
+				}
+
+				downloadFileHandler.copyFile(
+					syncFile, Paths.get(syncFile.getFilePathName()),
+					zipInputStream);
+			}
+		}
+		catch (Exception e) {
+			_logger.debug(e.getMessage(), e);
+		}
+		finally {
+			StreamUtil.cleanUp(inputStream);
+		}
+	}
+
+	private static final Logger _logger = LoggerFactory.getLogger(
+		DownloadFilesHandler.class);
+
+}