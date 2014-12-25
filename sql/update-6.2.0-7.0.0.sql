1f63ea3e83e4ea868a6bb4552635f299cc52aa31
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-12-26 00:16:44 +0100

    LPS-51903 Upgrade process

--- sql/update-6.2.0-7.0.0.sql
+++ sql/update-6.2.0-7.0.0.sql
@@ -73,6 +73,10 @@ create table ExportImportConfiguration (
 	statusDate DATE null
 );
 
+alter table Group_ add groupKey STRING;
+
+update Group_ set groupKey = name;
+
 alter table Group_ add inheritContent BOOLEAN;
 
 alter table JournalFolder add restrictionType INTEGER;
