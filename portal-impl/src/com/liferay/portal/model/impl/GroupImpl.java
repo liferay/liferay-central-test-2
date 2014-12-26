7bf32f76c772717994d707eef120f88d7925e277
Brian Chan <brian.chan@liferay.com>
2014-12-26 15:07:35 -0800

    LPS-51903 Move

--- portal-impl/src/com/liferay/portal/model/impl/GroupImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/GroupImpl.java
@@ -200,6 +200,14 @@ public class GroupImpl extends GroupBaseImpl {
 		else if (isControlPanel()) {
 			name = LanguageUtil.get(locale, "control-panel");
 		}
+		else if (isGuest()) {
+			Company company = CompanyLocalServiceUtil.getCompany(
+				getCompanyId());
+
+			Account account = company.getAccount();
+
+			name = account.getName();
+		}
 		else if (isLayout()) {
 			Layout layout = LayoutLocalServiceUtil.getLayout(getClassPK());
 
@@ -246,14 +254,6 @@ public class GroupImpl extends GroupBaseImpl {
 		}
 		else if (isUserPersonalSite()) {
 			name = LanguageUtil.get(locale, "user-personal-site");
-		}
-		else if (isGuest()) {
-			Company company = CompanyLocalServiceUtil.getCompany(
-				getCompanyId());
-
-			Account account = company.getAccount();
-
-			name = account.getName();
 		}
 
 		if (curGroup.isStaged() && !curGroup.isStagedRemotely() &&
