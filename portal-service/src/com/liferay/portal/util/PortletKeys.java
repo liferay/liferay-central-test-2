b4bc27919143f5b9cbb24c58708b1ea58534087d
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-12-29 23:45:22 +0100

    LPS-52368 Porlet Key

--- portal-service/src/com/liferay/portal/util/PortletKeys.java
+++ portal-service/src/com/liferay/portal/util/PortletKeys.java
@@ -224,8 +224,6 @@ public class PortletKeys {
 
 	public static final String STOCKS = "12";
 
-	public static final String TAGS_ADMIN = "99";
-
 	public static final String TAGS_COMPILER = "103";
 
 	public static final String TRANSLATOR = "26";
