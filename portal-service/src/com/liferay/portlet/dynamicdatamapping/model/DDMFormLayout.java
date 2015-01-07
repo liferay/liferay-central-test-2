394921e7698df3227ed5b8c7ffe2bbee99acee0c
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-01-07 15:26:03 -0300

    LPS-52490 Add DDMFormLayout model

---
+++ portal-service/src/com/liferay/portlet/dynamicdatamapping/model/DDMFormLayout.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.dynamicdatamapping.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Marcellus Tavares
+ */
+public class DDMFormLayout {
+
+	public DDMFormLayout() {
+	}
+
+	public DDMFormLayout(DDMFormLayout ddmFormLayout) {
+		for (DDMFormLayoutRow ddmFormLayoutRow :
+				ddmFormLayout._ddmFormLayoutRows) {
+
+			addDDMFormLayoutRow(new DDMFormLayoutRow(ddmFormLayoutRow));
+		}
+	}
+
+	public void addDDMFormLayoutRow(DDMFormLayoutRow ddmFormLayoutRow) {
+		_ddmFormLayoutRows.add(ddmFormLayoutRow);
+	}
+
+	public DDMFormLayoutRow getDDMFormLayoutRow(int index) {
+		return _ddmFormLayoutRows.get(index);
+	}
+
+	public List<DDMFormLayoutRow> getDDMFormLayoutRows() {
+		return _ddmFormLayoutRows;
+	}
+
+	public void setDDMFormLayoutRows(List<DDMFormLayoutRow> ddmFormLayoutRows) {
+		_ddmFormLayoutRows = ddmFormLayoutRows;
+	}
+
+	private List<DDMFormLayoutRow> _ddmFormLayoutRows =
+		new ArrayList<DDMFormLayoutRow>();
+
+}
---
+++ portal-service/src/com/liferay/portlet/dynamicdatamapping/model/DDMFormLayoutColumn.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.dynamicdatamapping.model;
+
+/**
+ * @author Marcellus Tavares
+ */
+public class DDMFormLayoutColumn {
+
+	public DDMFormLayoutColumn(DDMFormLayoutColumn ddmFormLayoutColumn) {
+		this(ddmFormLayoutColumn._ddmFormFieldName, ddmFormLayoutColumn._size);
+	}
+
+	public DDMFormLayoutColumn(String ddmFormFieldName, int size) {
+		_ddmFormFieldName = ddmFormFieldName;
+		_size = size;
+	}
+
+	public String getDDMFormFieldName() {
+		return _ddmFormFieldName;
+	}
+
+	public int getSize() {
+		return _size;
+	}
+
+	private final String _ddmFormFieldName;
+	private final int _size;
+
+}
