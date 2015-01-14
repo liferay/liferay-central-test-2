0e353c1a5d2593f4a7e65c53ab588698d2f87783
Brian Chan <brian.chan@liferay.com>
2015-01-14 14:40:57 -0800

    LPS-52620 SF

--- modules/test/poshi-runner/src/com/liferay/poshi/runner/selenium/MobileDriverToSeleniumBridge.java
+++ modules/test/poshi-runner/src/com/liferay/poshi/runner/selenium/MobileDriverToSeleniumBridge.java
@@ -972,27 +972,30 @@ public class MobileDriverToSeleniumBridge
 	}
 
 	protected void tap(String locator) {
-		int elementPositionCenterX = WebDriverHelper.getElementPositionCenterX(
-			this, locator);
-
-		int elementPositionCenterY = WebDriverHelper.getElementPositionCenterY(
-			this, locator);
-
-		int viewportPositionTop = WebDriverHelper.getScrollOffsetY(this);
-
-		int screenPositionX = elementPositionCenterX * 3 / 2;
-
-		int screenPositionY =
-			(elementPositionCenterY - viewportPositionTop) * 3 / 2 + 116;
-
 		try {
 			Runtime runtime = Runtime.getRuntime();
 
 			StringBuilder sb = new StringBuilder(4);
 
 			sb.append("adb -s emulator-5554 shell /data/local/tap.sh ");
+
+			int elementPositionCenterX =
+				WebDriverHelper.getElementPositionCenterX(this, locator);
+
+			int screenPositionX = elementPositionCenterX * 3 / 2;
+
 			sb.append(screenPositionX);
+
 			sb.append(" ");
+
+			int elementPositionCenterY =
+				WebDriverHelper.getElementPositionCenterY(this, locator);
+
+			int viewportPositionTop = WebDriverHelper.getScrollOffsetY(this);
+
+			int screenPositionY =
+				(elementPositionCenterY - viewportPositionTop) * 3 / 2 + 116;
+
 			sb.append(screenPositionY);
 
 			runtime.exec(sb.toString());
