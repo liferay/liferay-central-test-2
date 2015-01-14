856ff2e24114650cb1d828ea8c729fd7842f77e0
Jim Hinkey <james.hinkey@liferay.com>
2015-01-14 18:00:56 -0500

    LPS-52695 Autogenerated (ant format-javadoc)

--- portal-impl/src/com/liferay/portal/lar/PortletDataContextListenerImpl.java
+++ portal-impl/src/com/liferay/portal/lar/PortletDataContextListenerImpl.java
@@ -20,8 +20,9 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 
 /**
- * @author Raymond Augé
- * @deprecated As of 7.0.0, see {@link com.liferay.portal.kernel.lar.lifecycle.ExportImportLifecycleEvent}
+ * @author     Raymond Augé
+ * @deprecated As of 7.0.0, see {@link
+ *             com.liferay.portal.kernel.lar.lifecycle.ExportImportLifecycleEvent}
  */
 @Deprecated
 public class PortletDataContextListenerImpl
