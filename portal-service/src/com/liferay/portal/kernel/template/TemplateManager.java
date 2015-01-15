97ace77fd69a30ba46f9d74adc301a5184121c96
peterfellwock <peter.fellwock@liferay.com>
2015-01-14 22:32:15 -0800

    LPS-52139 Move tagLib and static class functionality to TemplateManager

--- portal-service/src/com/liferay/portal/kernel/template/TemplateManager.java
+++ portal-service/src/com/liferay/portal/kernel/template/TemplateManager.java
@@ -14,16 +14,62 @@
 
 package com.liferay.portal.kernel.template;
 
+import java.io.Writer;
+
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author Tina Tian
+ * @author Raymond Augé
  */
 public interface TemplateManager {
+
+	public void addStaticClassSupport(
+		Map<String, Object> contextObjects, String variableName,
+		Class<?> variableClass);
+
+	public void addStaticClassSupport(
+		Template template, String variableName, Class<?> variableClass);
+
+	public void addTaglibApplication(
+		Map<String, Object> contextObjects, String applicationName,
+		ServletContext servletContext);
+
+	public void addTaglibApplication(
+		Template template, String applicationName,
+		ServletContext servletContext);
+
+	public void addTaglibFactory(
+		Template template, String taglibLiferayHash,
+		ServletContext servletContext);
+
+	public void addTaglibFactory(
+		Map<String, Object> contextObjects, String taglibLiferayHash,
+		ServletContext servletContext);
+
+	public void addTaglibRequest(
+		Map<String, Object> contextObjects, String applicationName,
+		HttpServletRequest request, HttpServletResponse response);
+
+	public void addTaglibRequest(
+		Template template, String applicationName,
+		HttpServletRequest request, HttpServletResponse response);
+
+	public void addTaglibTheme(
+		Template template, String string, HttpServletRequest request,
+		HttpServletResponse response, Writer writer);
 
 	public void destroy();
 
 	public void destroy(ClassLoader classLoader);
 
 	public String getName();
+
+	public String[] getRestrictedVariables();
 
 	public Template getTemplate(
 		TemplateResource templateResource, boolean restricted);
