47d1a187f7211791fab8112a59d4eff372c58f88
hai.yu <8219754@liferay.com>
2015-01-19 15:16:36 +0800

    LPS-52772 When deine permission for role in Control Panel->Role, it should not update the detailed entry's permission(IndividualScopePermissions)

--- portal-impl/src/com/liferay/portal/service/impl/ResourceBlockLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/ResourceBlockLocalServiceImpl.java
@@ -737,11 +737,6 @@ public class ResourceBlockLocalServiceImpl
 			updateCompanyScopeResourceTypePermissions(
 				companyId, name, roleId, actionIdsLong, operator);
 
-		List<ResourceBlock> resourceBlocks = resourceBlockPersistence.findByC_N(
-			companyId, name);
-
-		updatePermissions(resourceBlocks, roleId, actionIdsLong, operator);
-
 		PermissionCacheUtil.clearCache();
 	}
 
@@ -753,11 +748,6 @@ public class ResourceBlockLocalServiceImpl
 		resourceTypePermissionLocalService.
 			updateGroupScopeResourceTypePermissions(
 				companyId, groupId, name, roleId, actionIdsLong, operator);
-
-		List<ResourceBlock> resourceBlocks =
-			resourceBlockPersistence.findByC_G_N(companyId, groupId, name);
-
-		updatePermissions(resourceBlocks, roleId, actionIdsLong, operator);
 
 		PermissionCacheUtil.clearCache();
 	}
