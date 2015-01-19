8552cbba3ac1d9c6f90690870ac7bbc20fdeba7a
Máté Thurzó <mate.thurzo@liferay.com>
2015-01-19 15:57:09 +0100

    LPS-52822 Add adapter builders for theme and group - as the adapters already exist: StagedTheme and StagedGroup

---
+++ portal-impl/src/com/liferay/portal/model/adapter/builder/StagedThemeModelAdapterBuilder.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.model.adapter.builder;
+
+import com.liferay.portal.model.Theme;
+import com.liferay.portlet.layoutsadmin.lar.StagedTheme;
+import com.liferay.portlet.layoutsadmin.lar.StagedThemeImpl;
+
+/**
+ * @author Mate Thurzo
+ */
+public class StagedThemeModelAdapterBuilder
+	implements ModelAdapterBuilder<Theme, StagedTheme> {
+
+	@Override
+	public StagedTheme build(Theme theme) {
+		return new StagedThemeImpl(theme);
+	}
+
+}
