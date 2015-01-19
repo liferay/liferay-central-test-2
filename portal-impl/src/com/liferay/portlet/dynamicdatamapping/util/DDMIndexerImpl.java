6dfa9c74deaa1d9ece2ccc57f60ee283eee2f827
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-01-19 19:01:41 -0300

    LPS-52125 Leave DDMIndexerImpl untouched for now, since we'll refactor soon.

--- portal-impl/src/com/liferay/portlet/dynamicdatamapping/util/DDMIndexerImpl.java
+++ portal-impl/src/com/liferay/portlet/dynamicdatamapping/util/DDMIndexerImpl.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portlet.dynamicdatamapping.util;
 
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
@@ -21,16 +22,25 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.FastDateFormatFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portlet.dynamicdatamapping.model.DDMStructure;
-import com.liferay.portlet.dynamicdatamapping.storage.DDMFormFieldValue;
 import com.liferay.portlet.dynamicdatamapping.storage.DDMFormValues;
+import com.liferay.portlet.dynamicdatamapping.storage.Field;
+import com.liferay.portlet.dynamicdatamapping.storage.Fields;
 
+import java.io.Serializable;
+
+import java.text.Format;
+
+import java.util.Date;
 import java.util.Locale;
 
 /**
@@ -48,12 +58,12 @@ public class DDMIndexerImpl implements DDMIndexer {
 
 		Locale[] locales = LanguageUtil.getAvailableLocales(groupId);
 
-		for (DDMFormFieldValue ddmFormFieldValue :
-			ddmFormValues.getDDMFormFieldValues()) {
+		Fields fields = toFields(ddmStructure, ddmFormValues);
 
+		for (Field field : fields) {
 			try {
 				String indexType = ddmStructure.getFieldProperty(
-					ddmFormFieldValue.getName(), "indexType");
+					field.getName(), "indexType");
 
 				if (Validator.isNull(indexType)) {
 					continue;
@@ -61,41 +71,89 @@ public class DDMIndexerImpl implements DDMIndexer {
 
 				for (Locale locale : locales) {
 					String name = encodeName(
-						ddmStructure.getStructureId(),
-						ddmFormFieldValue.getName(), locale);
+						ddmStructure.getStructureId(), field.getName(), locale);
 
-					String valueString = ddmFormFieldValue.getValue().getString(
-						locale);
+					Serializable value = field.getValue(locale);
 
-					String type = ddmStructure.getDDMFormField(
-						ddmFormFieldValue.getName()).getType();
-
-					if (type.equals(DDMImpl.TYPE_RADIO) ||
-						type.equals(DDMImpl.TYPE_SELECT)) {
-
-						JSONArray jsonArray = JSONFactoryUtil.createJSONArray(
-							valueString);
-
-						String[] stringArray = ArrayUtil.toStringArray(
-							jsonArray);
+					if (value instanceof Boolean) {
+						document.addKeyword(name, (Boolean)value);
+					}
+					else if (value instanceof Boolean[]) {
+						document.addKeyword(name, (Boolean[])value);
+					}
+					else if (value instanceof Date) {
+						document.addDate(name, (Date)value);
+					}
+					else if (value instanceof Date[]) {
+						document.addDate(name, (Date[])value);
+					}
+					else if (value instanceof Double) {
+						document.addNumber(name, (Double)value);
+					}
+					else if (value instanceof Double[]) {
+						document.addNumber(name, (Double[])value);
+					}
+					else if (value instanceof Integer) {
+						document.addNumber(name, (Integer)value);
+					}
+					else if (value instanceof Integer[]) {
+						document.addNumber(name, (Integer[])value);
+					}
+					else if (value instanceof Long) {
+						document.addNumber(name, (Long)value);
+					}
+					else if (value instanceof Long[]) {
+						document.addNumber(name, (Long[])value);
+					}
+					else if (value instanceof Float) {
+						document.addNumber(name, (Float)value);
+					}
+					else if (value instanceof Float[]) {
+						document.addNumber(name, (Float[])value);
+					}
+					else if (value instanceof Object[]) {
+						String[] valuesString = ArrayUtil.toStringArray(
+							(Object[])value);
 
 						if (indexType.equals("keyword")) {
-							document.addKeyword(name, stringArray);
+							document.addKeyword(name, valuesString);
 						}
 						else {
-							document.addText(name, stringArray);
+							document.addText(name, valuesString);
 						}
 					}
 					else {
-						if (type.equals(DDMImpl.TYPE_DDM_TEXT_HTML)) {
-							valueString = HtmlUtil.extractText(valueString);
-						}
+						String valueString = String.valueOf(value);
 
-						if (indexType.equals("keyword")) {
-							document.addKeyword(name, valueString);
+						String type = field.getType();
+
+						if (type.equals(DDMImpl.TYPE_RADIO) ||
+							type.equals(DDMImpl.TYPE_SELECT)) {
+
+							JSONArray jsonArray =
+								JSONFactoryUtil.createJSONArray(valueString);
+
+							String[] stringArray = ArrayUtil.toStringArray(
+								jsonArray);
+
+							if (indexType.equals("keyword")) {
+								document.addKeyword(name, stringArray);
+							}
+							else {
+								document.addText(name, stringArray);
+							}
 						}
 						else {
-							document.addText(name, valueString);
+							if (type.equals(DDMImpl.TYPE_DDM_TEXT_HTML)) {
+								valueString = HtmlUtil.extractText(valueString);
+							}
+
+							if (indexType.equals("keyword")) {
+								document.addKeyword(name, valueString);
+							}
+							else {
+								document.addText(name, valueString);
+							}
 						}
 					}
 				}
@@ -137,43 +195,76 @@ public class DDMIndexerImpl implements DDMIndexer {
 	public String extractIndexableAttributes(
 		DDMStructure ddmStructure, DDMFormValues ddmFormValues, Locale locale) {
 
+		Format dateFormat = FastDateFormatFactoryUtil.getSimpleDateFormat(
+			PropsUtil.get(PropsKeys.INDEX_DATE_FORMAT_PATTERN));
+
 		StringBundler sb = new StringBundler();
 
-		for (DDMFormFieldValue ddmFormFieldValue :
-			ddmFormValues.getDDMFormFieldValues()) {
+		Fields fields = toFields(ddmStructure, ddmFormValues);
 
+		for (Field field : fields) {
 			try {
 				String indexType = ddmStructure.getFieldProperty(
-					ddmFormFieldValue.getName(), "indexType");
+					field.getName(), "indexType");
 
 				if (Validator.isNull(indexType)) {
 					continue;
 				}
 
-				String valueString = ddmFormFieldValue.getValue().getString(
-					locale);
+				Serializable value = field.getValue(locale);
 
-				String type = ddmStructure.getDDMFormField(
-					ddmFormFieldValue.getName()).getType();
+				if ((value instanceof Boolean) || (value instanceof Double) ||
+					(value instanceof Integer) || (value instanceof Long) ||
+					(value instanceof Float)) {
 
-				if (type.equals(DDMImpl.TYPE_RADIO) ||
-					type.equals(DDMImpl.TYPE_SELECT)) {
-
-					JSONArray jsonArray = JSONFactoryUtil.createJSONArray(
-						valueString);
-
-					String[] stringArray = ArrayUtil.toStringArray(jsonArray);
-
-					sb.append(stringArray);
+					sb.append(value);
 					sb.append(StringPool.SPACE);
 				}
-				else {
-					if (type.equals(DDMImpl.TYPE_DDM_TEXT_HTML)) {
-						valueString = HtmlUtil.extractText(valueString);
-					}
-
-					sb.append(valueString);
+				else if (value instanceof Date) {
+					sb.append(dateFormat.format(value));
 					sb.append(StringPool.SPACE);
+				}
+				else if (value instanceof Date[]) {
+					Date[] dates = (Date[])value;
+
+					for (Date date : dates) {
+						sb.append(dateFormat.format(date));
+						sb.append(StringPool.SPACE);
+					}
+				}
+				else if (value instanceof Object[]) {
+					Object[] values = (Object[])value;
+
+					for (Object object : values) {
+						sb.append(object);
+						sb.append(StringPool.SPACE);
+					}
+				}
+				else {
+					String valueString = String.valueOf(value);
+
+					String type = field.getType();
+
+					if (type.equals(DDMImpl.TYPE_RADIO) ||
+						type.equals(DDMImpl.TYPE_SELECT)) {
+
+						JSONArray jsonArray = JSONFactoryUtil.createJSONArray(
+							valueString);
+
+						String[] stringArray = ArrayUtil.toStringArray(
+							jsonArray);
+
+						sb.append(stringArray);
+						sb.append(StringPool.SPACE);
+					}
+					else {
+						if (type.equals(DDMImpl.TYPE_DDM_TEXT_HTML)) {
+							valueString = HtmlUtil.extractText(valueString);
+						}
+
+						sb.append(valueString);
+						sb.append(StringPool.SPACE);
+					}
 				}
 			}
 			catch (Exception e) {
@@ -184,6 +275,20 @@ public class DDMIndexerImpl implements DDMIndexer {
 		}
 
 		return sb.toString();
+	}
+
+	protected Fields toFields(
+		DDMStructure ddmStructure, DDMFormValues ddmFormValues) {
+
+		try {
+			return DDMFormValuesToFieldsConverterUtil.convert(
+				ddmStructure, ddmFormValues);
+		}
+		catch (PortalException pe) {
+			_log.error("Unable to convert DDMFormValues to Fields", pe);
+		}
+
+		return new Fields();
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(DDMIndexerImpl.class);
