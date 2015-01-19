fe5d776e47a6ded15ece532e3e0fa530b7711801
Máté Thurzó <mate.thurzo@liferay.com>
2015-01-19 15:57:37 +0100

    LPS-52822 Utility class to adapt easily

---
+++ portal-service/src/com/liferay/portal/model/adapter/ModelAdapterUtil.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.model.adapter;
+
+import com.liferay.portal.model.adapter.builder.ModelAdapterBuilder;
+import com.liferay.portal.model.adapter.builder.ModelAdapterBuilderLocator;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceTracker;
+
+/**
+ * @author Mate Thurzo
+ */
+public class ModelAdapterUtil {
+
+	public static <T, V> V adapt(
+		T adapteeModel, Class<T> adapteeModelClass,
+		Class<V> adaptedModelClass) {
+
+		return doAdapt(adapteeModel, adapteeModelClass, adaptedModelClass);
+	}
+
+	public static <T, V> V adapt(T adapteeModel, Class<V> adaptedModelClass) {
+		Class<T> adapteeModelClass = (Class<T>)adapteeModel.getClass();
+
+		return doAdapt(adapteeModel, adapteeModelClass, adaptedModelClass);
+	}
+
+	protected static <T, V> V doAdapt(
+		T adapteeModel, Class<T> adapteeModelClass,
+		Class<V> adaptedModelClass) {
+
+		ModelAdapterBuilderLocator modelAdapterBuilderLocator =
+			_modelAdapterBuilderLocatorServiceTracker.getService();
+
+		if (modelAdapterBuilderLocator == null) {
+			return null;
+		}
+
+		ModelAdapterBuilder<T, V> modelAdapterBuilder =
+			(ModelAdapterBuilder<T, V>)modelAdapterBuilderLocator.locate(
+				adapteeModelClass, adaptedModelClass);
+
+		return modelAdapterBuilder.build(adapteeModel);
+	}
+
+	private static final
+		ServiceTracker<ModelAdapterBuilderLocator, ModelAdapterBuilderLocator>
+			_modelAdapterBuilderLocatorServiceTracker =
+				RegistryUtil.getRegistry().trackServices(
+					ModelAdapterBuilderLocator.class);
+
+	static {
+		_modelAdapterBuilderLocatorServiceTracker.open();
+	}
+
+}
