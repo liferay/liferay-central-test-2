8b906d3f68ee4f380785e6afd8a4491a2c38a22e
Jonathan McCann <jonathan.mccann@liferay.com>
2015-01-20 10:36:08 -0800

    LPS-52910 Related Assets do not display in Nested Portlets Portlet

--- modules/apps/nested-portlets/nested-portlets-web/src/com/liferay/nested/portlets/web/portlet/NestedPortletsPortlet.java
+++ modules/apps/nested-portlets/nested-portlets-web/src/com/liferay/nested/portlets/web/portlet/NestedPortletsPortlet.java
@@ -71,7 +71,7 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
 		"com.liferay.portlet.private-session-attributes=false",
-		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.render-weight=1",
 		"com.liferay.portlet.single-page-application=false",
 		"com.liferay.portlet.struts-path=nested_portlets",
 		"com.liferay.portlet.use-default-template=true",
