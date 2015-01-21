5ad1745e80617f1e7ad6d586001aba415e405d23
arbetts <andrew.betts@liferay.com>
2015-01-21 11:23:22 -0800

    Revert "LPS-52223 Do proper delegation to reduce duplication"
    This reverts commit fe10e1466fae374bc461ca8786c16459ba5b0373.

--- portal-impl/src/com/liferay/portal/notifications/ChannelHubManagerImpl.java
+++ portal-impl/src/com/liferay/portal/notifications/ChannelHubManagerImpl.java
@@ -68,7 +68,7 @@ public class ChannelHubManagerImpl implements ChannelHubManager {
 		}
 
 		MethodHandler methodHandler = new MethodHandler(
-			_confirmDeliveryMethodKey, companyId, userId,
+			_confirmDeliveriesMethodKey, companyId, userId,
 			notificationEventUuids, archive);
 
 		ClusterRequest clusterRequest = ClusterRequest.createMulticastRequest(
@@ -97,9 +97,28 @@ public class ChannelHubManagerImpl implements ChannelHubManager {
 			boolean archive)
 		throws ChannelException {
 
-		confirmDelivery(
-			companyId, userId, Collections.singleton(notificationEventUuid),
+		ChannelHub channelHub = getChannelHub(companyId);
+
+		channelHub.confirmDelivery(userId, notificationEventUuid, archive);
+
+		if (!ClusterInvokeThreadLocal.isEnabled()) {
+			return;
+		}
+
+		MethodHandler methodHandler = new MethodHandler(
+			_confirmDeliveryMethodKey, companyId, userId, notificationEventUuid,
 			archive);
+
+		ClusterRequest clusterRequest = ClusterRequest.createMulticastRequest(
+			methodHandler, true);
+
+		try {
+			ClusterExecutorUtil.execute(clusterRequest);
+		}
+		catch (Exception e) {
+			throw new ChannelException(
+				"Unable to confirm delivery of event across cluster", e);
+		}
 	}
 
 	@Override
@@ -408,10 +427,14 @@ public class ChannelHubManagerImpl implements ChannelHubManager {
 		channelHub.unregisterChannelListener(userId, channelListener);
 	}
 
-	private static final MethodKey _confirmDeliveryMethodKey =
+	private static final MethodKey _confirmDeliveriesMethodKey =
 		new MethodKey(
 			ChannelHubManagerUtil.class, "confirmDelivery", long.class,
 			long.class, Collection.class, boolean.class);
+	private static final MethodKey _confirmDeliveryMethodKey =
+		new MethodKey(
+			ChannelHubManagerUtil.class, "confirmDelivery", long.class,
+			long.class, String.class, boolean.class);
 	private static final MethodKey _destroyChannelMethodKey =
 		new MethodKey(
 			ChannelHubManagerUtil.class, "destroyChannel", long.class,
