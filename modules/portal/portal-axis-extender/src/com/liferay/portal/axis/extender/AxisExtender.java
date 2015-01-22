d97603397319d701e27764021a72b9196d497dd7
Brian Chan <brian.chan@liferay.com>
2015-01-21 21:19:16 -0800

    LPS-52678 Rename to simplify

--- modules/portal/portal-axis-extender/src/com/liferay/portal/axis/extender/AxisExtender.java
+++ modules/portal/portal-axis-extender/src/com/liferay/portal/axis/extender/AxisExtender.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.axis.legacy.web.service;
+package com.liferay.portal.axis.extender;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
