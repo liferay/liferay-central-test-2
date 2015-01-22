61ba9a3b3f8a9db95f06924098cbcd3e797c9b14
Brian Chan <brian.chan@liferay.com>
2015-01-21 16:28:04 -0800

    LPS-52139 obvious

--- modules/portal/portal-template-freemarker/src/com/liferay/portal/template/freemarker/FreemarkerTemplateResourceLoader.java
+++ modules/portal/portal-template-freemarker/src/com/liferay/portal/template/freemarker/FreemarkerTemplateResourceLoader.java
@@ -86,12 +86,10 @@ public class FreemarkerTemplateResourceLoader implements TemplateResourceLoader{
 
 	@Reference(unbind = "-")
 	protected void setMultiVMPoolUtil(MultiVMPoolUtil multiVMPoolUtil) {
-		// This reference ensures that MultipVMPoolUtil is ready for use.
 	}
 
 	@Reference(unbind = "-")
 	protected void setSingleVMPoolUtil(SingleVMPoolUtil singleVMPoolUtil) {
-		// This reference ensures that SingleVMPoolUtil is ready for use.
 	}
 
 	private static volatile DefaultTemplateResourceLoader
