f644313d59d2f7de3ddddae234075f2502542a2d
Brian Chan <brian.chan@liferay.com>
2015-01-22 20:56:47 -0800

    LPS-50604 Match bookmarks search

--- modules/apps/wiki/wiki-service/src/com/liferay/wiki/search/WikiOpenSearchImpl.java
+++ modules/apps/wiki/wiki-service/src/com/liferay/wiki/search/WikiOpenSearchImpl.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.wiki.opensearch;
+package com.liferay.wiki.search;
 
 import com.liferay.portal.kernel.search.HitsOpenSearchImpl;
 import com.liferay.portal.kernel.search.Indexer;
