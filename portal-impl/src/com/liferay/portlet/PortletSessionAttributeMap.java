abef66fa72ea1a7aef357462ec6c6a1282c122cf
Brian Chan <brian.chan@liferay.com>
2015-01-25 15:16:24 -0800

    LPS-52962 not used

--- portal-impl/src/com/liferay/portlet/PortletSessionAttributeMap.java
+++ portal-impl/src/com/liferay/portlet/PortletSessionAttributeMap.java
@@ -26,7 +26,6 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 
 import javax.servlet.http.HttpSession;
