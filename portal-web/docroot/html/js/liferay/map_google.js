3fa2cda1c2f40221f79f1476dbba62f1e10d0d43
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2015-01-26 13:31:44 +0100

    LPS-51997 apply lastest changes to GoogleMaps, so InfoWindow does not hide marker

--- portal-web/docroot/html/js/liferay/map_google.js
+++ portal-web/docroot/html/js/liferay/map_google.js
@@ -41,7 +41,7 @@ AUI.add(
 
 						instance._dialog.setOptions(cfg);
 
-						instance._dialog.open(instance.get('map'));
+						instance._dialog.open(instance.get('map'), cfg.marker);
 					}
 				}
 			}
@@ -168,7 +168,28 @@ AUI.add(
 						return style;
 					},
 
-					_wrapNativeFeature: function(feature) {
+					_wrapNativeFeature: function(event) {
+						var instance = this;
+
+						var feature = (event.getGeometry) ? event : event.feature;
+
+						feature.getMarker = function() {
+							if (!feature._marker) {
+								var marker = new google.maps.Marker(
+									{
+										icon: feature.getProperty('icon'),
+										map: instance.get('map'),
+										opacity: 0,
+										position: feature.getGeometry().get('location'),
+										zIndex: -1
+									}
+								);
+
+								feature._marker = marker;
+							}
+							return feature._marker;
+						};
+
 						return feature;
 					}
 				}
