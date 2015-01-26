daledotshan/liferay-portal

LPS-52733 - Special characters are not allowed in folder names for Web Content

portal-impl/src/com/liferay/portlet/journal/util/JournalValidatorImpl.java
@@ -1,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.journal.util;
+
+import com.liferay.portal.kernel.util.UnicodeFormatter;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.util.PropsValues;
+import com.liferay.portlet.journal.FolderNameException;
+
+/**
+ * @author David Zhang
+ */
+public final class JournalValidatorImpl implements JournalValidator {
+
+	@Override
+	public boolean isValidName(String name) {
+		if (Validator.isNull(name)) {
+			return false;
+		}
+
+		for (String blacklistChar : PropsValues.JOURNAL_CHAR_BLACKLIST) {
+			if (name.contains(blacklistChar)) {
+				return false;
+			}
+		}
+
+		for (String blacklistLastChar :
+				PropsValues.JOURNAL_CHAR_LAST_BLACKLIST) {
+
+			if (blacklistLastChar.startsWith(_UNICODE_PREFIX)) {
+				blacklistLastChar = UnicodeFormatter.parseString(
+					blacklistLastChar);
+			}
+
+			if (name.endsWith(blacklistLastChar)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	@Override
+	public void validateFoldorName(String folderName)
+		throws FolderNameException {
+
+		if (!isValidName(folderName)) {
+			throw new FolderNameException(folderName);
+		}
+	}
+
+	private static final String _UNICODE_PREFIX = "\\u";
+
+}

portal-service/src/com/liferay/portlet/journal/util/JournalValidator.java
@@ -1,0 +1,29 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.journal.util;
+
+import com.liferay.portlet.journal.FolderNameException;
+
+/**
+ * @author David Zhang
+ */
+public interface JournalValidator {
+
+	public boolean isValidName(String name);
+
+	public void validateFoldorName(String folderName)
+		throws FolderNameException;
+
+}

portal-service/src/com/liferay/portlet/journal/util/JournalValidatorUtil.java
@@ -1,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.journal.util;
+
+import com.liferay.portal.kernel.security.pacl.permission.PortalRuntimePermission;
+import com.liferay.portlet.journal.FolderNameException;
+
+/**
+ * @author David Zhang
+ */
+public class JournalValidatorUtil {
+
+	public static JournalValidator getJournalValidator() {
+		PortalRuntimePermission.checkGetBeanProperty(
+			JournalValidatorUtil.class);
+
+		return _journalValidator;
+	}
+
+	public static boolean isValidName(String name) {
+		return getJournalValidator().isValidName(name);
+	}
+
+	public static final void validateFolderName(String folderName)
+		throws FolderNameException {
+
+		getJournalValidator().validateFoldorName(folderName);
+	}
+
+	public void setJournalValidator(JournalValidator journalValidator) {
+		PortalRuntimePermission.checkSetBeanProperty(getClass());
+
+		_journalValidator = journalValidator;
+	}
+
+	private static JournalValidator _journalValidator;
+
+}

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portlet/journal/util/JournalValidatorImpl.java
@@ -1,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.journal.util;
+
+import com.liferay.portal.kernel.util.UnicodeFormatter;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.util.PropsValues;
+import com.liferay.portlet.journal.FolderNameException;
+
+/**
+ * @author David Zhang
+ */
+public final class JournalValidatorImpl implements JournalValidator {
+
+	@Override
+	public boolean isValidName(String name) {
+		if (Validator.isNull(name)) {
+			return false;
+		}
+
+		for (String blacklistChar : PropsValues.JOURNAL_CHAR_BLACKLIST) {
+			if (name.contains(blacklistChar)) {
+				return false;
+			}
+		}
+
+		for (String blacklistLastChar :
+				PropsValues.JOURNAL_CHAR_LAST_BLACKLIST) {
+
+			if (blacklistLastChar.startsWith(_UNICODE_PREFIX)) {
+				blacklistLastChar = UnicodeFormatter.parseString(
+					blacklistLastChar);
+			}
+
+			if (name.endsWith(blacklistLastChar)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	@Override
+	public void validateFolderName(String folderName)
+		throws FolderNameException {
+
+		if (!isValidName(folderName)) {
+			throw new FolderNameException(folderName);
+		}
+	}
+
+	private static final String _UNICODE_PREFIX = "\\u";
+
+}

portal-service/src/com/liferay/portlet/journal/util/JournalValidator.java
@@ -1,0 +1,29 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.journal.util;
+
+import com.liferay.portlet.journal.FolderNameException;
+
+/**
+ * @author David Zhang
+ */
+public interface JournalValidator {
+
+	public boolean isValidName(String name);
+
+	public void validateFolderName(String folderName)
+		throws FolderNameException;
+
+}

portal-service/src/com/liferay/portlet/journal/util/JournalValidatorUtil.java
@@ -1,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.journal.util;
+
+import com.liferay.portal.kernel.security.pacl.permission.PortalRuntimePermission;
+import com.liferay.portlet.journal.FolderNameException;
+
+/**
+ * @author David Zhang
+ */
+public class JournalValidatorUtil {
+
+	public static JournalValidator getJournalValidator() {
+		PortalRuntimePermission.checkGetBeanProperty(
+			JournalValidatorUtil.class);
+
+		return _journalValidator;
+	}
+
+	public static boolean isValidName(String name) {
+		return getJournalValidator().isValidName(name);
+	}
+
+	public static final void validateFolderName(String folderName)
+		throws FolderNameException {
+
+		getJournalValidator().validateFolderName(folderName);
+	}
+
+	public void setJournalValidator(JournalValidator journalValidator) {
+		PortalRuntimePermission.checkSetBeanProperty(getClass());
+
+		_journalValidator = journalValidator;
+	}
+
+	private static JournalValidator _journalValidator;
+
+}