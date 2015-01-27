d0df805b0520c0a09a95cc1059ce50dc85bdec66
Gregory Amerson <gregory.amerson@liferay.com>
2015-01-27 10:35:42 +0800

    LPS-52961 add basic cmdline operation

--- modules/util/java-sass-compiler/src/com/liferay/tools/jsc/SassCompiler.java
+++ modules/util/java-sass-compiler/src/com/liferay/tools/jsc/SassCompiler.java
@@ -14,6 +14,7 @@
 
 package com.liferay.tools.jsc;
 
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.tools.jsc.libsass.SassLibrary;
 import com.liferay.tools.jsc.libsass.SassLibrary.Sass_Context;
 import com.liferay.tools.jsc.libsass.SassLibrary.Sass_File_Context;
@@ -21,11 +22,42 @@ import com.liferay.tools.jsc.libsass.SassLibrary.Sass_Options;
 import com.sun.jna.Pointer;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 /**
  * @author Gregory Amerson
  */
 public class SassCompiler {
+
+	public static void main(String[] args) {
+		try {
+			new SassCompiler(args);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public SassCompiler() {
+	}
+
+	public SassCompiler(String[] args) throws Exception {
+		final SassCompiler compiler = new SassCompiler();
+
+		for (String arg : args) {
+			final File file = new File(arg);
+
+			if (isValidFile(file)) {
+				final String output = compiler.compileFile(arg, "", "");
+
+				final File outputFile = getOutputFile(file);
+				write(outputFile, output);
+			}
+		}
+	}
 
 	public String compileFile(
 			String inputFile, String includePath, String imgPath)
@@ -87,6 +119,27 @@ public class SassCompiler {
 			catch (Throwable t) {
 				throw new SassCompilationException(t);
 			}
+		}
+	}
+
+	private File getOutputFile(File file) {
+		return new File(file.getParentFile(), getOutputFileName(file));
+	}
+
+	private String getOutputFileName(File file) {
+		return file.getName().replaceAll("scss$", "css");
+	}
+
+	private boolean isValidFile(File file) {
+		return file != null && file.exists() &&
+			file.getName().endsWith(".scss");
+	}
+
+	private void write(File outputFile, String output) throws IOException {
+		try (Writer writer = new OutputStreamWriter(
+			new FileOutputStream(outputFile, false), StringPool.UTF8)) {
+
+			writer.write(output);
 		}
 	}
 
