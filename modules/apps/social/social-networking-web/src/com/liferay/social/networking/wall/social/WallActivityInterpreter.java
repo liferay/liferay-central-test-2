1ea526573530c4753ed0a91b9bac7b81527e3903
Brian Chan <brian.chan@liferay.com>
2015-01-27 15:44:42 -0800

    LPS-50752 SF

--- modules/apps/social/social-networking-web/src/com/liferay/social/networking/wall/social/WallActivityInterpreter.java
+++ modules/apps/social/social-networking-web/src/com/liferay/social/networking/wall/social/WallActivityInterpreter.java
@@ -37,7 +37,7 @@ import org.osgi.service.component.annotations.Reference;
  * @author Zsolt Berentey
  */
 @Component(
-	property = { "javax.portlet.name=" + WallPortlet.JAVAX_PORTLET_NAME },
+	property = {"javax.portlet.name=" + WallPortlet.JAVAX_PORTLET_NAME},
 	service = SocialActivityInterpreter.class
 )
 public class WallActivityInterpreter extends BaseSocialActivityInterpreter {
