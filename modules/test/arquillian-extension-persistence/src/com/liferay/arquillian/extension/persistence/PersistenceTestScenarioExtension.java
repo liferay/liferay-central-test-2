4c3ff2c4a45a7cfacc70c849229e569bf4daa1be
Cristina González Castellano <cristina.gonzalez@liferay.com>
2015-01-28 16:09:15 +0100

    LPS-50675 register common classes into Arquillian for persistence

--- modules/test/arquillian-extension-persistence/src/com/liferay/arquillian/extension/persistence/PersistenceTestScenarioExtension.java
+++ modules/test/arquillian-extension-persistence/src/com/liferay/arquillian/extension/persistence/PersistenceTestScenarioExtension.java
@@ -19,6 +19,8 @@ import com.liferay.arquillian.extension.internal.descriptor.SpringDescriptorImpl
 import com.liferay.arquillian.extension.persistence.internal.descriptor.PersistenceTestSpringDescriptorImpl;
 import com.liferay.arquillian.extension.persistence.internal.instanceproducer.ExtensionInstanceProducer;
 import com.liferay.arquillian.extension.persistence.internal.observer.PersistenceTestObserver;
+import com.liferay.portal.test.util.InitPersistenceTest;
+import com.liferay.portal.test.util.InitPersistenceTestImpl;
 
 import org.jboss.arquillian.core.spi.LoadableExtension;
 
@@ -35,6 +37,8 @@ public class PersistenceTestScenarioExtension implements LoadableExtension {
 		builder.override(
 			SpringDescriptor.class, SpringDescriptorImpl.class,
 			PersistenceTestSpringDescriptorImpl.class);
+		builder.service(
+			InitPersistenceTest.class, InitPersistenceTestImpl.class);
 	}
 
 }
