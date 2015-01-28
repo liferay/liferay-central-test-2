e2e6ff3169f29ea5d9aa11c99c7d6aaceb1c40d6
Brian Chan <brian.chan@liferay.com>
2015-01-28 14:08:24 -0800

    LPS-52983 SF

--- modules/apps/asset/asset-browser-web/src/com/liferay/asset/browser/web/portlet/AssetBrowserPortlet.java
+++ modules/apps/asset/asset-browser-web/src/com/liferay/asset/browser/web/portlet/AssetBrowserPortlet.java
@@ -40,7 +40,7 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.resource-bundle=content.Language",
 		"javax.portlet.security-role-ref=guest,power-user,user",
 		"javax.portlet.supports.mime-type=text/html"
-		},
+	},
 	service = Portlet.class
 )
 public class AssetBrowserPortlet extends MVCPortlet {
