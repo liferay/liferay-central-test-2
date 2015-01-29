d5a45dff0c60e52feaf0be151d9fa8518279883a
Zoltán Takács <zoltan.takacs@liferay.com>
2015-01-29 17:40:26 +0100

    LPS-53052 Add utility method for getting the portal JSP name

--- portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
+++ portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
@@ -748,6 +748,19 @@ public class HookHotDeployListener
 		return new File(filePath);
 	}
 
+	protected String getPortalJspName(String customJsp, String customJspDir) {
+		if (Validator.isNull(customJsp) || Validator.isNull(customJspDir)) {
+			return null;
+		}
+
+		int customJspDirIndex = customJsp.indexOf(customJspDir);
+
+		String portalJspName = customJsp.substring(
+			customJspDirIndex + customJspDir.length());
+
+		return portalJspName;
+	}
+
 	protected Map<Object, ServiceRegistration<?>> getServiceRegistrations(
 		String servletContextName) {
 
