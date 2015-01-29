d132e5eda36bd6a4496358fa58d189baccedbd29
adolfo.perez <adolfo.perez@liferay.com>
2015-01-29 17:18:02 +0100

    LPS-50752 update portlet keys

--- modules/apps/social/social-networking-api/src/com/liferay/social/networking/util/SocialNetworkingPortletKeys.java
+++ modules/apps/social/social-networking-api/src/com/liferay/social/networking/util/SocialNetworkingPortletKeys.java
@@ -20,29 +20,29 @@ package com.liferay.social.networking.util;
 public class SocialNetworkingPortletKeys {
 
 	public static final String FRIENDS =
-		"com_liferay_social_networking_friends_portlet_FriendsPortlet";
+		"com_liferay_social_networking_web_friends_portlet_FriendsPortlet";
 
 	public static final String FRIENDS_ACTIVITIES =
-		"com_liferay_social_networking_friendsactivities_portlet_" +
+		"com_liferay_social_networking_web_friendsactivities_portlet_" +
 			"FriendsActivitiesPortlet";
 
 	public static final String MAP =
-		"com_liferay_social_networking_map_portlet_MapPortlet";
+		"com_liferay_social_networking_web_map_portlet_MapPortlet";
 
 	public static final String MEETUPS =
-		"com_liferay_social_networking_meetups_portlet_MeetupsPortlet";
+		"com_liferay_social_networking_web_meetups_portlet_MeetupsPortlet";
 
 	public static final String MEMBERS =
-		"com_liferay_social_networking_members_portlet_MembersPortlet";
+		"com_liferay_social_networking_web_members_portlet_MembersPortlet";
 
 	public static final String MEMBERS_ACTIVITIES =
-		"com_liferay_social_networking_membersactivities_portlet_" +
+		"com_liferay_social_networking_web_membersactivities_portlet_" +
 			"MembersActivitiesPortlet";
 
 	public static final String SUMMARY =
-		"com_liferay_social_networking_summary_portlet_SummaryPortlet";
+		"com_liferay_social_networking_web_summary_portlet_SummaryPortlet";
 
 	public static final String WALL =
-		"com_liferay_social_networking_wall_portlet_WallPortlet";
+		"com_liferay_social_networking_web_wall_portlet_WallPortlet";
 
 }
