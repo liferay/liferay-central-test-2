7ebeb70145795c8844eb43e18f2ae009c29162e6
Laszlo Csontos <laszlo.csontos@liferay.com>
2015-01-30 13:49:36 +0100

    LPS-53052 This is the common pattern to append separator tokens (newline in this case)

--- portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
+++ portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
@@ -1001,16 +1001,21 @@ public class HookHotDeployListener
 			customJspDir, customJspGlobal, customJsps);
 
 		if (_log.isDebugEnabled()) {
-			StringBundler sb = new StringBundler(customJsps.size() * 2 + 1);
+			StringBundler sb = new StringBundler(customJsps.size() * 2);
 
 			sb.append("Custom JSP files:\n");
 
+			int i = 0;
+
 			for (String customJsp : customJsps) {
 				sb.append(customJsp);
-				sb.append(StringPool.NEW_LINE);
-			}
 
-			sb.setIndex(sb.index() - 1);
+				if ((i + 1) < customJsps.size()) {
+					sb.append(StringPool.NEW_LINE);
+				}
+
+				i++;
+			}
 
 			_log.debug(sb.toString());
 		}
@@ -2437,11 +2442,13 @@ public class HookHotDeployListener
 			Log log = SanitizerLogWrapper.allowCRLF(_log);
 
 			StringBundler sb = new StringBundler(
-				collidingCustomJsps.size() * 4 + 3);
+				collidingCustomJsps.size() * 4 + 2);
 
 			sb.append("Colliding JSP files in ");
 			sb.append(servletContextName);
 			sb.append(StringPool.NEW_LINE);
+
+			int i = 0;
 
 			for (Map.Entry<String, String> entry :
 					collidingCustomJsps.entrySet()) {
@@ -2449,10 +2456,13 @@ public class HookHotDeployListener
 				sb.append((String)entry.getKey());
 				sb.append(" with ");
 				sb.append((String)entry.getValue());
-				sb.append(StringPool.NEW_LINE);
-			}
 
-			sb.setIndex(sb.index() - 1);
+				if ((i + 1) < collidingCustomJsps.size()) {
+					sb.append(StringPool.NEW_LINE);
+				}
+
+				i++;
+			}
 
 			log.debug(sb.toString());
 		}
