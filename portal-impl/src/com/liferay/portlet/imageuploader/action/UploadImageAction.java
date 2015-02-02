e5ff5377cf528fc1130f5d621d6f96ba0a521180
Mika Koivisto <mika.koivisto@liferay.com>
2015-02-02 15:46:37 +0200

    LPS-52121 Don't let user upload non images files in the first place

--- portal-impl/src/com/liferay/portlet/imageuploader/action/UploadImageAction.java
+++ portal-impl/src/com/liferay/portlet/imageuploader/action/UploadImageAction.java
@@ -30,6 +30,7 @@ import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.upload.UploadException;
 import com.liferay.portal.kernel.upload.UploadPortletRequest;
 import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
@@ -184,11 +185,21 @@ public class UploadImageAction extends PortletAction {
 
 		String contentType = uploadPortletRequest.getContentType("fileName");
 
+		String fileName = uploadPortletRequest.getFileName("fileName");
+
+		File file = uploadPortletRequest.getFile("fileName");
+
+		String mimeType = MimeTypesUtil.getContentType(file, fileName);
+
+		if (!StringUtil.equalsIgnoreCase(
+				ContentTypes.APPLICATION_OCTET_STREAM, mimeType)) {
+
+			contentType = mimeType;
+		}
+
 		if (!MimeTypesUtil.isWebImage(contentType)) {
 			throw new ImageTypeException();
 		}
-
-		String fileName = uploadPortletRequest.getFileName("fileName");
 
 		try {
 			TempFileEntryUtil.deleteTempFileEntry(
@@ -198,18 +209,9 @@ public class UploadImageAction extends PortletAction {
 		catch (Exception e) {
 		}
 
-		InputStream inputStream = null;
-
-		try {
-			inputStream = uploadPortletRequest.getFileAsStream("fileName");
-
-			return TempFileEntryUtil.addTempFileEntry(
-				themeDisplay.getScopeGroupId(), themeDisplay.getUserId(),
-				getTempImageFolderName(), fileName, inputStream, contentType);
-		}
-		finally {
-			StreamUtil.cleanUp(inputStream);
-		}
+		return TempFileEntryUtil.addTempFileEntry(
+			themeDisplay.getScopeGroupId(), themeDisplay.getUserId(),
+			getTempImageFolderName(), fileName, file, contentType);
 	}
 
 	protected FileEntry getTempImageFileEntry(PortletRequest portletRequest)
