70c34d0db5bd21d3c3d5bab7cc7d9c803aa9df7e
Tibor Lipusz <tibor.lipusz@liferay.com>
2015-02-02 10:58:19 +0100

    LPS-53194 Update JavaDoc for methods merge & getUuid in ServiceContext

--- portal-service/src/com/liferay/portal/service/ServiceContext.java
+++ portal-service/src/com/liferay/portal/service/ServiceContext.java
@@ -711,6 +711,9 @@ public class ServiceContext implements Cloneable, Serializable {
 
 	/**
 	 * Returns the UUID of this service context's current entity.
+	 *<p>
+	 * Note: to ensure it is never accidentally used by two entities, the UUID
+	 * is always reset to <code>null</code> upon invoking this method.
 	 *
 	 * @return the UUID of this service context's current entity
 	 */
@@ -865,6 +868,13 @@ public class ServiceContext implements Cloneable, Serializable {
 		return _signedIn;
 	}
 
+	/**
+	 * Merges all not <code>null</code> or zero attributes and fields
+	 * (except for the request) of the parameter service context with this
+	 * service context object.
+	 *
+	 * @param serviceContext the servic context object to be merged
+	 */
 	public void merge(ServiceContext serviceContext) {
 		setAddGroupPermissions(serviceContext.isAddGroupPermissions());
 		setAddGuestPermissions(serviceContext.isAddGuestPermissions());
