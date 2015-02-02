73f1a77f3c3cceaa4a3e3af3b2abb8d55984fd72
shuyangzhou <shuyang.zhou@liferay.com>
2015-02-01 17:52:12 -0800

    LPS-53221 Move BaseUserNotificationTestCase to proper package

--- portal-test-internal/src/com/liferay/portlet/notifications/BaseUserNotificationTestCase.java
+++ portal-test-internal/src/com/liferay/portlet/notifications/BaseUserNotificationTestCase.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.util;
+package com.liferay.portlet.notifications;
 
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
