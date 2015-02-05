f0aa1e9714193feff11bac6101fd97ba3c057aad
Carlos Sierra <carlos.sierra@liferay.com>
2015-02-05 11:04:42 +0100

    LPS-53183 Change module name and packages

---
+++ modules/core/osgi-service-tracker-map/src/com/liferay/osgi/service/tracker/map/ServiceReferenceServiceTuple.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.osgi.service.tracker.map;
+
+import org.osgi.framework.ServiceReference;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public class ServiceReferenceServiceTuple<SR, TS>
+	implements Comparable<ServiceReferenceServiceTuple<SR, TS>> {
+
+	public ServiceReferenceServiceTuple(
+		ServiceReference<SR> serviceReference, TS service) {
+
+		_serviceReference = serviceReference;
+		_service = service;
+	}
+
+	@Override
+	public int compareTo(
+		ServiceReferenceServiceTuple<SR, TS> serviceReferenceServiceTuple) {
+
+		return _serviceReference.compareTo(
+			serviceReferenceServiceTuple.getServiceReference());
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+
+		if (!(obj instanceof ServiceReferenceServiceTuple)) {
+			return false;
+		}
+
+		ServiceReferenceServiceTuple<SR, TS> serviceReferenceServiceTuple =
+			(ServiceReferenceServiceTuple<SR, TS>)obj;
+
+		return _serviceReference.equals(
+			serviceReferenceServiceTuple.getServiceReference());
+	}
+
+	public TS getService() {
+		return _service;
+	}
+
+	public ServiceReference<SR> getServiceReference() {
+		return _serviceReference;
+	}
+
+	@Override
+	public int hashCode() {
+		return _serviceReference.hashCode();
+	}
+
+	private final TS _service;
+	private final ServiceReference<SR> _serviceReference;
+
+}
---
+++ modules/core/osgi-service-tracker-map/src/com/liferay/osgi/service/tracker/map/internal/DefaultServiceTrackerCustomizer.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.osgi.service.tracker.map.internal;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public class DefaultServiceTrackerCustomizer<S>
+	implements ServiceTrackerCustomizer<S, S> {
+
+	public DefaultServiceTrackerCustomizer(BundleContext bundleContext) {
+		this.bundleContext = bundleContext;
+	}
+
+	@Override
+	public S addingService(ServiceReference<S> serviceReference) {
+		return bundleContext.getService(serviceReference);
+	}
+
+	@Override
+	public void modifiedService(
+		ServiceReference<S> serviceReference, S service) {
+
+		removedService(serviceReference, service);
+	}
+
+	@Override
+	public void removedService(
+		ServiceReference<S> serviceReference, S service) {
+
+		bundleContext.ungetService(serviceReference);
+	}
+
+	private final BundleContext bundleContext;
+
+}
