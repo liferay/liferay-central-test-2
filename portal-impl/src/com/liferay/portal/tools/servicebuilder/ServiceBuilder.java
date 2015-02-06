3914746cbae6bc8e2a16a20a773f78ef8ff19772
Jim Hinkey <james.hinkey@liferay.com>
2015-02-06 10:28:07 -0500

    LPS-52134 Do exact comparison with template name.

--- portal-impl/src/com/liferay/portal/tools/servicebuilder/ServiceBuilder.java
+++ portal-impl/src/com/liferay/portal/tools/servicebuilder/ServiceBuilder.java
@@ -3769,7 +3769,7 @@ public class ServiceBuilder {
 			sb.append(tag.getName());
 			sb.append(" ");
 
-			if (_tplName.endsWith("service_soap.ftl")) {
+			if (_tpl.equals(_tplServiceSoap)) {
 				if (tagValue.startsWith(
 						"com.liferay.portal.kernel.exception.PortalException"))
 				{
@@ -5043,8 +5043,8 @@ public class ServiceBuilder {
 
 	private String _processTemplate(String name, Map<String, Object> context)
 		throws Exception {
-		
-		_tplName = name;
+
+		_tpl = name;
 
 		return StringUtil.strip(FreeMarkerUtil.process(name, context), '\r');
 	}
@@ -5240,6 +5240,7 @@ public class ServiceBuilder {
 	private String _targetEntityName;
 	private String _testDir;
 	private String _testOutputPath;
+	private String _tpl;
 	private String _tplActionableDynamicQuery =
 		_TPL_ROOT + "actionable_dynamic_query.ftl";
 	private String _tplBadAliasNames = _TPL_ROOT + "bad_alias_names.txt";
@@ -5267,7 +5268,6 @@ public class ServiceBuilder {
 	private String _tplModelImpl = _TPL_ROOT + "model_impl.ftl";
 	private String _tplModelSoap = _TPL_ROOT + "model_soap.ftl";
 	private String _tplModelWrapper = _TPL_ROOT + "model_wrapper.ftl";
-	private String _tplName;
 	private String _tplPersistence = _TPL_ROOT + "persistence.ftl";
 	private String _tplPersistenceImpl = _TPL_ROOT + "persistence_impl.ftl";
 	private String _tplPersistenceTest = _TPL_ROOT + "persistence_test.ftl";
