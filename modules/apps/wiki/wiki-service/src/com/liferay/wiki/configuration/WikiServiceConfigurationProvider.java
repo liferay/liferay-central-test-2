0aac9f95630f2eae631f5649fcb554e002213243
Iván Zaera Avellón <ivan.zaera@liferay.com>
2015-02-06 10:27:37 +0100

    LPS-53138 Create an OSGi component to be able to inject the Config Admin bean as a reference and also, be able to get it from a static method from non-OSGi lifecycled parts of the code. Also remove the old facilities to deal with module configuration which are no longer needed.

---
+++ modules/apps/wiki/wiki-service/src/com/liferay/wiki/configuration/WikiServiceConfigurationProvider.java
@@ -0,0 +1,140 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wiki.configuration;
+
+import aQute.bnd.annotation.metatype.Configurable;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+
+/**
+ * @author Ivan Zaera
+ */
+@Component(
+	configurationPid = "com.liferay.wiki.service", immediate = true,
+	service = WikiServiceConfiguration.class
+)
+public class WikiServiceConfigurationProvider
+	implements WikiServiceConfiguration {
+
+	public static WikiServiceConfiguration getWikiServiceConfiguration() {
+		if (_wikiServiceConfigurationProvider == null) {
+			throw new IllegalStateException(
+				"WikiServiceConfiguration is not yet available");
+		}
+
+		return _wikiServiceConfigurationProvider;
+	}
+
+	public String defaultFormat() {
+		return _wikiServiceConfiguration.defaultFormat();
+	}
+
+	public String displayTemplatesConfig() {
+		return _wikiServiceConfiguration.displayTemplatesConfig();
+	}
+
+	public String emailFromAddress() {
+		return _wikiServiceConfiguration.emailFromAddress();
+	}
+
+	public String emailFromName() {
+		return _wikiServiceConfiguration.emailFromName();
+	}
+
+	public String emailPageAddedBody() {
+		return _wikiServiceConfiguration.emailPageAddedBody();
+	}
+
+	public boolean emailPageAddedEnabled() {
+		return _wikiServiceConfiguration.emailPageAddedEnabled();
+	}
+
+	public String emailPageAddedSubject() {
+		return _wikiServiceConfiguration.emailPageAddedSubject();
+	}
+
+	public String emailPageUpdatedBody() {
+		return _wikiServiceConfiguration.emailPageUpdatedBody();
+	}
+
+	public boolean emailPageUpdatedEnabled() {
+		return _wikiServiceConfiguration.emailPageUpdatedEnabled();
+	}
+
+	public String emailPageUpdatedSubject() {
+		return _wikiServiceConfiguration.emailPageUpdatedSubject();
+	}
+
+	public String frontPageName() {
+		return _wikiServiceConfiguration.frontPageName();
+	}
+
+	public String initialNodeName() {
+		return _wikiServiceConfiguration.initialNodeName();
+	}
+
+	public boolean pageCommentsEnabled() {
+		return _wikiServiceConfiguration.pageCommentsEnabled();
+	}
+
+	public boolean pageMinorEditAddSocialActivity() {
+		return _wikiServiceConfiguration.pageMinorEditAddSocialActivity();
+	}
+
+	public boolean pageMinorEditSendEmail() {
+		return _wikiServiceConfiguration.pageMinorEditSendEmail();
+	}
+
+	public String pageTitlesRegexp() {
+		return _wikiServiceConfiguration.pageTitlesRegexp();
+	}
+
+	public String pageTitlesRemoveRegexp() {
+		return _wikiServiceConfiguration.pageTitlesRemoveRegexp();
+	}
+
+	public String[] parsersCreoleSupportedProtocols() {
+		return _wikiServiceConfiguration.parsersCreoleSupportedProtocols();
+	}
+
+	public int rssAbstractLength() {
+		return _wikiServiceConfiguration.rssAbstractLength();
+	}
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		_wikiServiceConfiguration = Configurable.createConfigurable(
+			WikiServiceConfiguration.class, properties);
+
+		_wikiServiceConfigurationProvider = this;
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_wikiServiceConfigurationProvider = null;
+	}
+
+	private static WikiServiceConfigurationProvider
+		_wikiServiceConfigurationProvider;
+
+	private volatile WikiServiceConfiguration _wikiServiceConfiguration;
+
+}
