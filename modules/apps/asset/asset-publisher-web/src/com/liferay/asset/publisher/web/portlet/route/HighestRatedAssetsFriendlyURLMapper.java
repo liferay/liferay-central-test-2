08003c97d01e3432fe6dfa2d90eded9394d921dc
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-02-09 11:02:59 +0100

    LPS-52768 Register FriendlyURL Mapper

---
+++ modules/apps/asset/asset-publisher-web/src/com/liferay/asset/publisher/web/portlet/route/HighestRatedAssetsFriendlyURLMapper.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.publisher.web.portlet.route;
+
+import com.liferay.asset.publisher.web.constants.AssetPublisherPortletKeys;
+import com.liferay.portal.kernel.portlet.FriendlyURLMapper;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + AssetPublisherPortletKeys.HIGHEST_RATED_ASSETS
+	},
+	service = FriendlyURLMapper.class
+)
+public class HighestRatedAssetsFriendlyURLMapper
+	extends AssetPublisherFriendlyURLMapper {
+}
