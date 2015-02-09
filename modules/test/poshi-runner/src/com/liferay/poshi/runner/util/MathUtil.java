6e0ed1c19817ae82e561d6c24ed6784c496e236b
Karen Dang <karen.dang@liferay.com>
2015-02-09 09:54:39 -0800

    LPS-53493 - Add MathUtil

---
+++ modules/test/poshi-runner/src/com/liferay/poshi/runner/util/MathUtil.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.poshi.runner.util;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class MathUtil {
+
+	public static int difference(Integer...values) {
+		int difference = values[0];
+
+		for (int i = 1; i < values.length; i++) {
+			difference -= values[i];
+		}
+
+		return difference;
+	}
+
+	public static boolean isEven(int x) {
+		if ((x % 2) == 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public static boolean isOdd(int x) {
+		return !isEven(x);
+	}
+
+	public static int product(Integer... values) {
+		int product = 1;
+
+		for (int value : values) {
+			product *= value;
+		}
+
+		return product;
+	}
+
+	public static int quotient(Integer value1, Integer value2) {
+		return value1 / value2;
+	}
+
+	public static int sum(Integer... values) {
+		int sum = 0;
+
+		for (int value : values) {
+			sum += value;
+		}
+
+		return sum;
+	}
+
+}
