4f8460272d04f228549268d7c667bb972da94cfc
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-02-09 12:00:09 -0300

    LPS-53379 Add Text DDMFormFieldType

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-text/src/com/liferay/dynamicdatamapping/type/TextDDMFormFieldRenderer.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamicdatamapping.type;
+
+import com.liferay.portal.kernel.template.TemplateResource;
+import com.liferay.portal.kernel.template.URLTemplateResource;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portlet.dynamicdatamapping.registry.BaseDDMFormFieldRenderer;
+import com.liferay.portlet.dynamicdatamapping.registry.DDMFormFieldRenderer;
+
+import java.net.URL;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marcellus Tavares
+ */
+@Component(
+	immediate = true, property = {"templatePath=/META-INF/text.soy"},
+	service = {
+		TextDDMFormFieldRenderer.class, DDMFormFieldRenderer.class
+	}
+)
+public class TextDDMFormFieldRenderer extends BaseDDMFormFieldRenderer {
+
+	@Activate
+	protected void activate(Map<String, Object> properties) {
+		String templatePath = MapUtil.getString(properties, "templatePath");
+
+		TemplateResource templateResource = getTemplateResource(templatePath);
+
+		this.templateNamespace = "ddm.text";
+		this.templateResource = templateResource;
+	}
+
+	protected TemplateResource getTemplateResource(String templatePath) {
+		Class<?> clazz = getClass();
+
+		ClassLoader classLoader = clazz.getClassLoader();
+
+		URL templateURL = classLoader.getResource(templatePath);
+
+		return new URLTemplateResource(templateURL.getPath(), templateURL);
+	}
+
+}
---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-text/src/com/liferay/dynamicdatamapping/type/TextDDMFormFieldType.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamicdatamapping.type;
+
+import com.liferay.portlet.dynamicdatamapping.registry.DDMFormFieldRenderer;
+import com.liferay.portlet.dynamicdatamapping.registry.DDMFormFieldType;
+import com.liferay.portlet.dynamicdatamapping.registry.DDMFormFieldValueAccessor;
+import com.liferay.portlet.dynamicdatamapping.registry.DDMFormFieldValueRendererAccessor;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marcellus Tavares
+ */
+@Component(immediate = true, service = DDMFormFieldType.class)
+public class TextDDMFormFieldType implements DDMFormFieldType {
+
+	@Override
+	public DDMFormFieldRenderer getDDMFormFieldRenderer() {
+		return _ddmFormFieldRenderer;
+	}
+
+	@Override
+	public DDMFormFieldValueAccessor<String> getDDMFormFieldValueAccessor(
+		Locale locale) {
+
+		return new TextDDMFormFieldValueAccessor(locale);
+	}
+
+	@Override
+	public DDMFormFieldValueRendererAccessor
+		getDDMFormFieldValueRendererAccessor(Locale locale) {
+
+		return new TextDDMFormFieldValueRendererAccessor(
+			getDDMFormFieldValueAccessor(locale));
+	}
+
+	@Override
+	public String getName() {
+		return "text";
+	}
+
+	@Reference(service = TextDDMFormFieldRenderer.class, unbind = "-")
+	protected void setDDMFormFieldRenderer(
+		DDMFormFieldRenderer ddmFormFieldRenderer) {
+
+		_ddmFormFieldRenderer = ddmFormFieldRenderer;
+	}
+
+	private DDMFormFieldRenderer _ddmFormFieldRenderer;
+
+}
