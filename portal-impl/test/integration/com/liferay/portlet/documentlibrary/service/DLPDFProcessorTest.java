2d10505700ef69286402c05fab1a58446a93e034
adolfo.perez <adolfo.perez@liferay.com>
2015-02-11 18:18:42 +0100

    LPS-53515 test processors are cleaned up on updateAndCheckIn

--- portal-impl/test/integration/com/liferay/portlet/documentlibrary/service/DLPDFProcessorTest.java
+++ portal-impl/test/integration/com/liferay/portlet/documentlibrary/service/DLPDFProcessorTest.java
@@ -135,6 +135,32 @@ public class DLPDFProcessorTest {
 	}
 
 	@Test
+	public void testShouldCleanUpProcessorsOnUpdateAndCheckIn()
+		throws Exception {
+
+		AtomicBoolean cleanedUp = registerCleanUpDLProcessor();
+
+		FileEntry fileEntry = DLAppServiceUtil.addFileEntry(
+			_serviceContext.getScopeGroupId(),
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			StringUtil.randomString() + ".pdf", ContentTypes.APPLICATION_PDF,
+			StringUtil.randomString(), StringUtil.randomString(),
+			StringUtil.randomString(), _PDF_DATA.getBytes(), _serviceContext);
+
+		byte[] bytes = _PDF_DATA.getBytes();
+
+		InputStream inputStream = new ByteArrayInputStream(bytes);
+
+		DLAppServiceUtil.updateFileEntryAndCheckIn(
+			fileEntry.getFileEntryId(), StringUtil.randomString() + ".pdf",
+			ContentTypes.APPLICATION_PDF, StringUtil.randomString(),
+			StringUtil.randomString(), StringUtil.randomString(), true,
+			inputStream, bytes.length, _serviceContext);
+
+		Assert.assertTrue(cleanedUp.get());
+	}
+
+	@Test
 	public void testShouldCopyPreviousPreviewOnCheckIn() throws Exception {
 		AtomicInteger count = registerDLPDFProcessorMessageListener(
 			EventType.COPY_PREVIOUS);
