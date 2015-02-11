8e700d728aa5ee0dd1354f1a2fc00b67c2032eeb
adolfo.perez <adolfo.perez@liferay.com>
2015-02-11 18:23:34 +0100

    LPS-53515 ant format-source

--- portal-impl/test/integration/com/liferay/portlet/documentlibrary/service/DLPDFProcessorTest.java
+++ portal-impl/test/integration/com/liferay/portlet/documentlibrary/service/DLPDFProcessorTest.java
@@ -80,26 +80,6 @@ public class DLPDFProcessorTest {
 	}
 
 	@Test
-	public void testShouldCleanUpProcessorsOnUpdate() throws Exception {
-		AtomicBoolean cleanedUp = registerCleanUpDLProcessor();
-
-		FileEntry fileEntry = DLAppServiceUtil.addFileEntry(
-			_serviceContext.getScopeGroupId(),
-			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
-			StringUtil.randomString() + ".pdf", ContentTypes.APPLICATION_PDF,
-			StringUtil.randomString(), StringUtil.randomString(),
-			StringUtil.randomString(), _PDF_DATA.getBytes(), _serviceContext);
-
-		DLAppServiceUtil.updateFileEntry(
-			fileEntry.getFileEntryId(), StringUtil.randomString() + ".pdf",
-			ContentTypes.APPLICATION_PDF, StringUtil.randomString(),
-			StringUtil.randomString(), StringUtil.randomString(), true,
-			_PDF_DATA.getBytes(), _serviceContext);
-
-		Assert.assertTrue(cleanedUp.get());
-	}
-
-	@Test
 	public void testShouldCleanUpProcessorsOnCancelCheckOut() throws Exception {
 		AtomicBoolean cleanedUp = registerCleanUpDLProcessor();
 
@@ -130,6 +110,26 @@ public class DLPDFProcessorTest {
 			StringUtil.randomString(), _PDF_DATA.getBytes(), _serviceContext);
 
 		DLAppServiceUtil.deleteFileEntry(fileEntry.getFileEntryId());
+
+		Assert.assertTrue(cleanedUp.get());
+	}
+
+	@Test
+	public void testShouldCleanUpProcessorsOnUpdate() throws Exception {
+		AtomicBoolean cleanedUp = registerCleanUpDLProcessor();
+
+		FileEntry fileEntry = DLAppServiceUtil.addFileEntry(
+			_serviceContext.getScopeGroupId(),
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			StringUtil.randomString() + ".pdf", ContentTypes.APPLICATION_PDF,
+			StringUtil.randomString(), StringUtil.randomString(),
+			StringUtil.randomString(), _PDF_DATA.getBytes(), _serviceContext);
+
+		DLAppServiceUtil.updateFileEntry(
+			fileEntry.getFileEntryId(), StringUtil.randomString() + ".pdf",
+			ContentTypes.APPLICATION_PDF, StringUtil.randomString(),
+			StringUtil.randomString(), StringUtil.randomString(), true,
+			_PDF_DATA.getBytes(), _serviceContext);
 
 		Assert.assertTrue(cleanedUp.get());
 	}
