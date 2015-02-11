a2a7374097bf649a74d22466973c04df5c4e1452
Preston-Crary <preston.crary@liferay.com>
2015-02-11 09:31:55 -0800

    LPS-53485 Improve API hiding bitwise operations

--- portal-impl/src/com/liferay/portal/model/impl/ResourcePermissionImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/ResourcePermissionImpl.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.model.impl;
 
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.model.ResourceAction;
 import com.liferay.portal.service.ResourceActionLocalServiceUtil;
 
@@ -89,6 +90,15 @@ import com.liferay.portal.service.ResourceActionLocalServiceUtil;
 public class ResourcePermissionImpl extends ResourcePermissionBaseImpl {
 
 	@Override
+	public void addResourceAction(String actionId) throws PortalException {
+		ResourceAction resourceAction =
+			ResourceActionLocalServiceUtil.getResourceAction(
+				getName(), actionId);
+
+		setActionIds(getActionIds() | resourceAction.getBitwiseValue());
+	}
+
+	@Override
 	public boolean hasActionId(String actionId) {
 		ResourceAction resourceAction =
 			ResourceActionLocalServiceUtil.fetchResourceAction(
@@ -104,6 +114,15 @@ public class ResourcePermissionImpl extends ResourcePermissionBaseImpl {
 		}
 
 		return false;
+	}
+
+	@Override
+	public void removeResourceAction(String actionId) throws PortalException {
+		ResourceAction resourceAction =
+			ResourceActionLocalServiceUtil.getResourceAction(
+				getName(), actionId);
+
+		setActionIds(getActionIds() & (~resourceAction.getBitwiseValue()));
 	}
 
 }
