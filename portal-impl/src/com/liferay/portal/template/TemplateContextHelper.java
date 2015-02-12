7c294de180c140a1be5df6f5634dca8a9c1d884f
shuyangzhou <shuyang.zhou@liferay.com>
2015-02-11 16:45:27 -0800

    LPS-50479 Wrong bean locating

--- portal-impl/src/com/liferay/portal/template/TemplateContextHelper.java
+++ portal-impl/src/com/liferay/portal/template/TemplateContextHelper.java
@@ -50,7 +50,7 @@ import com.liferay.portal.kernel.util.StringUtil_IW;
 import com.liferay.portal.kernel.util.TimeZoneUtil_IW;
 import com.liferay.portal.kernel.util.UnicodeFormatter_IW;
 import com.liferay.portal.kernel.util.Validator_IW;
-import com.liferay.portal.kernel.xml.SAXReader;
+import com.liferay.portal.kernel.xml.SAXReaderUtil;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.Theme;
 import com.liferay.portal.service.GroupLocalService;
@@ -650,7 +650,7 @@ public class TemplateContextHelper {
 			try {
 				variables.put(
 					"saxReaderUtil",
-					utilLocator.findUtil(SAXReader.class.getName()));
+					utilLocator.findUtil(SAXReaderUtil.class.getName()));
 			}
 			catch (SecurityException se) {
 				_log.error(se, se);
