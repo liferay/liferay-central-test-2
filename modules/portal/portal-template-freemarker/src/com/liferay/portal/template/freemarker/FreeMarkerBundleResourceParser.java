0e1a971fa700f80ac6d022bce213a85f11f5971b
Brian Chan <brian.chan@liferay.com>
2015-02-13 11:49:17 -0800

    LPS-52795 SF

--- modules/portal/portal-template-freemarker/src/com/liferay/portal/template/freemarker/FreeMarkerBundleResourceParser.java
+++ modules/portal/portal-template-freemarker/src/com/liferay/portal/template/freemarker/FreeMarkerBundleResourceParser.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.portal.template.freemarker;
 
 import com.liferay.portal.template.ClassLoaderResourceParser;
