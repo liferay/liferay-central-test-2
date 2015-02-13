08f7c38e9526bd5108b31feeb3ee1f7c67c79b9a
shuyangzhou <shuyang.zhou@liferay.com>
2015-02-13 09:07:18 -0800

    LPS-52768 Remove unused field added by 20064da1ddb611ee2a08554e029d10233fec6998

--- portal-impl/src/com/liferay/portlet/assetpublisher/context/AssetPublisherDisplayContext.java
+++ portal-impl/src/com/liferay/portlet/assetpublisher/context/AssetPublisherDisplayContext.java
@@ -1043,7 +1043,6 @@ public class AssetPublisherDisplayContext {
 	private Boolean _enableRatings;
 	private Boolean _enableRelatedAssets;
 	private Boolean _enableRSS;
-	private final boolean _enableSetAsDefualtAssetPublisher;
 	private Boolean _enableSocialBookmarks;
 	private Boolean _enableTagBasedNavigation;
 	private Boolean _excludeZeroViewCount;
