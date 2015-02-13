bb96aa85721ca12e19ec6669e77d830e7bbdb4bd
Raymond Auge <raymond.auge@liferay.com>
2015-02-12 23:45:32 -0500

    LPS-52795 fix test

--- modules/portal/portal-template-freemarker/test/unit/com/liferay/portal/template/freemarker/FreeMarkerTemplateTest.java
+++ modules/portal/portal-template-freemarker/test/unit/com/liferay/portal/template/freemarker/FreeMarkerTemplateTest.java
@@ -16,6 +16,11 @@ package com.liferay.portal.template.freemarker;
 
 import aQute.bnd.annotation.metatype.Configurable;
 
+import com.liferay.portal.cache.MultiVMPoolImpl;
+import com.liferay.portal.cache.SingleVMPoolImpl;
+import com.liferay.portal.cache.memory.MemoryPortalCacheManager;
+import com.liferay.portal.kernel.cache.MultiVMPoolUtil;
+import com.liferay.portal.kernel.cache.SingleVMPoolUtil;
 import com.liferay.portal.kernel.io.unsync.UnsyncStringWriter;
 import com.liferay.portal.kernel.template.StringTemplateResource;
 import com.liferay.portal.kernel.template.Template;
@@ -24,7 +29,8 @@ import com.liferay.portal.kernel.template.TemplateResource;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.template.TemplateContextHelper;
 import com.liferay.portal.template.freemarker.configuration.FreemarkerEngineConfiguration;
-import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.registry.BasicRegistryImpl;
+import com.liferay.registry.RegistryUtil;
 
 import freemarker.cache.TemplateCache;
 
@@ -36,6 +42,7 @@ import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
 import java.io.Reader;
+import java.io.Serializable;
 import java.io.StringReader;
 
 import java.util.Collections;
@@ -47,8 +54,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Rule;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -56,10 +62,29 @@ import org.junit.Test;
  */
 public class FreeMarkerTemplateTest {
 
-	@ClassRule
-	@Rule
-	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
-		new LiferayIntegrationTestRule();
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		RegistryUtil.setRegistry(new BasicRegistryImpl());
+
+		MultiVMPoolUtil multiVMPoolUtil = new MultiVMPoolUtil();
+		MultiVMPoolImpl multiVMPoolImpl = new MultiVMPoolImpl();
+
+		multiVMPoolImpl.setPortalCacheManager(
+			MemoryPortalCacheManager.
+				<Serializable, Serializable>createMemoryPortalCacheManager(
+					"multi.vm.pool"));
+
+		multiVMPoolUtil.setMultiVMPool(multiVMPoolImpl);
+
+		SingleVMPoolUtil singleVMPoolUtil = new SingleVMPoolUtil();
+		SingleVMPoolImpl singleVMPoolImpl = new SingleVMPoolImpl();
+
+		singleVMPoolImpl.setPortalCacheManager(
+			MemoryPortalCacheManager.createMemoryPortalCacheManager(
+				"single.vm.pool"));
+
+		singleVMPoolUtil.setSingleVMPool(singleVMPoolImpl);
+	}
 
 	@Before
 	public void setUp() throws Exception {
