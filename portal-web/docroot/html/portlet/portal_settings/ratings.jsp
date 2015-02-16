sergiogonzalez/liferay-portal

LPS-52630 Create a convenience method to retrieve the property name

portal-web/docroot/html/portlet/portal_settings/ratings.jsp
@@ -54,7 +54,7 @@ PortletPreferences companyPortletPreferences = PrefsPropsUtil.getPreferences(com
 			</c:if>
 
 			<%
-			String propertyKey = className + StringPool.UNDERLINE + "RatingsType";
+			String propertyKey = RatingsDataTransformerUtil.getPropertyName(className);
 
 			RatingsType defaultRatingsType = PortletRatingsDefinitionUtil.getDefaultRatingsType(portletId, className);
 

liferay/liferay-central-test-2

portal-web/docroot/html/portlet/portal_settings/ratings.jsp
@@ -54,7 +54,7 @@ PortletPreferences companyPortletPreferences = PrefsPropsUtil.getPreferences(com
 			</c:if>
 
 			<%
-			String propertyKey = className + StringPool.UNDERLINE + "RatingsType";
+			String propertyKey = RatingsDataTransformerUtil.getPropertyKey(className);
 
 			RatingsType defaultRatingsType = PortletRatingsDefinitionUtil.getDefaultRatingsType(portletId, className);
 