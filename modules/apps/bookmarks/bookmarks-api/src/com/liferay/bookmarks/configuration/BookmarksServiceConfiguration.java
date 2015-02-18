0e7cada6e9cec472a7c3d5e25b02a1ab0f41c0d6
adolfo.perez <adolfo.perez@liferay.com>
2015-02-18 11:54:14 +0100

    LPS-53571 move properties into service configuration
    Note that for `entriesPerPage` and `foldersPerPage` I'm hardcoding the
    value of `search.container.page.default.delta`. This is a temporal hack
    so that everything works as expected.
    
    In LPS-53401 an new LocationVariableResolver will be added to the
    settings framework to reference portal properties from the service
    configuration. Once that gets merged, I'll update the service
    configuration to reference the correct portal property.

---
+++ modules/apps/bookmarks/bookmarks-api/src/com/liferay/bookmarks/configuration/BookmarksServiceConfiguration.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.bookmarks.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Meta.OCD(
+	id = "com.liferay.bookmarks.configuration.BookmarksServiceConfiguration"
+)
+public interface BookmarksServiceConfiguration {
+
+	@Meta.AD(
+		deflt = "${resource:com/liferay/bookmarks/dependencies/" +
+			"email_entry_added_body.tmpl}",
+		required = false
+	)
+	public String emailEntryAddedBody();
+
+	@Meta.AD(deflt = "true", required = false)
+	public boolean emailEntryAddedEnabled();
+
+	@Meta.AD(
+		deflt = "${resource:com/liferay/bookmarks/dependencies/" +
+			"email_entry_added_subject.tmpl}",
+		required = false
+	)
+	public String emailEntryAddedSubject();
+
+	@Meta.AD(
+		deflt = "${resource:com/liferay/bookmarks/dependencies/ +" +
+			"email_entry_updated_body.tmpl}",
+		required = false
+	)
+	public String emailEntryUpdatedBody();
+
+	@Meta.AD(deflt = "true", required = false)
+	public boolean emailEntryUpdatedEnabled();
+
+	@Meta.AD(deflt = "", required = false)
+	public String emailFromAddress();
+
+	@Meta.AD(deflt = "", required = false)
+	public String emailFromName();
+
+	@Meta.AD(deflt = "20", required = false)
+	public int entriesPerPage();
+
+	@Meta.AD(deflt = "name|url|visits|modified-date|action", required = false)
+	public String[] entryColumns();
+
+	@Meta.AD(
+		deflt = "folder|num-of-folders|num-of-entries|action", required = false
+	)
+	public String[] folderColumns();
+
+	@Meta.AD(deflt = "20", required = false)
+	public int foldersPerPage();
+
+	@Meta.AD(deflt = "true", required = false)
+	public boolean foldersSearchVisible();
+
+	@Meta.AD(deflt = "true", required = false)
+	public boolean relatedAssetsEnabled();
+
+	@Meta.AD(deflt = "true", required = false)
+	public boolean subfoldersVisible();
+
+}
---
+++ modules/apps/bookmarks/bookmarks-api/src/com/liferay/bookmarks/configuration/BookmarksServiceConfigurationImpl.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.bookmarks.configuration;
+
+import aQute.bnd.annotation.metatype.Configurable;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(
+	configurationPid = "com.liferay.bookmarks.configuration.BookmarksServiceConfiguration",
+	immediate = true, service = BookmarksServiceConfiguration.class
+)
+public class BookmarksServiceConfigurationImpl
+	implements BookmarksServiceConfiguration {
+
+	@Override
+	public String emailEntryAddedBody() {
+		return _bookmarksServiceConfiguration.emailEntryAddedBody();
+	}
+
+	@Override
+	public boolean emailEntryAddedEnabled() {
+		return _bookmarksServiceConfiguration.emailEntryAddedEnabled();
+	}
+
+	@Override
+	public String emailEntryAddedSubject() {
+		return _bookmarksServiceConfiguration.emailEntryAddedSubject();
+	}
+
+	@Override
+	public String emailEntryUpdatedBody() {
+		return _bookmarksServiceConfiguration.emailEntryUpdatedBody();
+	}
+
+	@Override
+	public boolean emailEntryUpdatedEnabled() {
+		return _bookmarksServiceConfiguration.emailEntryUpdatedEnabled();
+	}
+
+	@Override
+	public String emailFromAddress() {
+		return _bookmarksServiceConfiguration.emailFromAddress();
+	}
+
+	@Override
+	public String emailFromName() {
+		return _bookmarksServiceConfiguration.emailFromName();
+	}
+
+	@Override
+	public int entriesPerPage() {
+		return _bookmarksServiceConfiguration.entriesPerPage();
+	}
+
+	@Override
+	public String[] entryColumns() {
+		return _bookmarksServiceConfiguration.entryColumns();
+	}
+
+	@Override
+	public String[] folderColumns() {
+		return _bookmarksServiceConfiguration.folderColumns();
+	}
+
+	@Override
+	public int foldersPerPage() {
+		return _bookmarksServiceConfiguration.foldersPerPage();
+	}
+
+	@Override
+	public boolean foldersSearchVisible() {
+		return _bookmarksServiceConfiguration.foldersSearchVisible();
+	}
+
+	@Override
+	public boolean relatedAssetsEnabled() {
+		return _bookmarksServiceConfiguration.relatedAssetsEnabled();
+	}
+
+	@Override
+	public boolean subfoldersVisible() {
+		return _bookmarksServiceConfiguration.subfoldersVisible();
+	}
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		_bookmarksServiceConfiguration = Configurable.createConfigurable(
+			BookmarksServiceConfiguration.class, properties);
+	}
+
+	private volatile BookmarksServiceConfiguration
+		_bookmarksServiceConfiguration;
+
+}
