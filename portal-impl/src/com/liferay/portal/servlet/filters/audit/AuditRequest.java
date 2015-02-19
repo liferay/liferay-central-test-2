1801e36e81a930262b6fa83c67eced32c23f8ee7
iobrist <isaac.obrist@liferay.com>
2015-02-18 18:08:38 -0800

    LPS-53693 - Adds wrapper to check for X-FORWARDED-FOR header in a given request If this header != null this is the new ClientIP

---
+++ portal-impl/src/com/liferay/portal/servlet/filters/audit/AuditRequest.java
@@ -0,0 +1,26 @@
+package com.liferay.portal.servlet.filters.audit;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+public class AuditRequest extends HttpServletRequestWrapper {
+
+	public AuditRequest(HttpServletRequest request){
+		super(request);
+		_request = request;
+	}
+
+	@Override
+	public String getRemoteAddr(){
+
+		String ip = _request.getHeader("X-FORWARDED-FOR");
+
+		if (ip != null) {
+			return ip;
+		}
+
+		return super.getRemoteAddr();
+	}
+
+	private HttpServletRequest _request;
+}
