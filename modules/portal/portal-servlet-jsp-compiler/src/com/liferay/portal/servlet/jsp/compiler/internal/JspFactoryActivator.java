1314dcd0286e1a41970880c3e972ff637a0a941c
Raymond Auge <raymond.auge@liferay.com>
2015-02-19 15:32:54 -0500

    LPS-53673 Portal fails to start with setup wizard enabled

---
+++ modules/portal/portal-servlet-jsp-compiler/src/com/liferay/portal/servlet/jsp/compiler/internal/JspFactoryActivator.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.servlet.jsp.compiler.internal;
+
+import com.liferay.taglib.servlet.JspFactorySwapper;
+
+import javax.servlet.jsp.JspFactory;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author Raymond Augé
+ */
+public class JspFactoryActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		JspFactory.setDefaultFactory(
+			new org.apache.jasper.runtime.JspFactoryImpl());
+
+		JspFactorySwapper.swap();
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		JspFactory.setDefaultFactory(null);
+	}
+
+}
