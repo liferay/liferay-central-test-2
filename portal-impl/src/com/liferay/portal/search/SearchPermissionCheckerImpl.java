e2d98edb57ba6b4eb7adc4f48d4c134e7ec5b3f4
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-02-20 13:54:31 +0100

    LPS-53730 Check if the groupIds is not empty to prevent NullPointerException

--- portal-impl/src/com/liferay/portal/search/SearchPermissionCheckerImpl.java
+++ portal-impl/src/com/liferay/portal/search/SearchPermissionCheckerImpl.java
@@ -428,13 +428,17 @@ public class SearchPermissionCheckerImpl implements SearchPermissionChecker {
 				}
 			}
 
-			for (long groupId : groupIds) {
-				if (ResourcePermissionLocalServiceUtil.hasResourcePermission(
-						companyId, className, ResourceConstants.SCOPE_GROUP,
-						String.valueOf(groupId), role.getRoleId(),
-						ActionKeys.VIEW)) {
+			if (ArrayUtil.isNotEmpty(groupIds)) {
+				for (long groupId : groupIds) {
+					if (ResourcePermissionLocalServiceUtil.
+							hasResourcePermission(
+								companyId, className,
+								ResourceConstants.SCOPE_GROUP,
+								String.valueOf(groupId), role.getRoleId(),
+								ActionKeys.VIEW)) {
 
-					groupsQuery.addTerm(Field.GROUP_ID, groupId);
+						groupsQuery.addTerm(Field.GROUP_ID, groupId);
+					}
 				}
 			}
 
