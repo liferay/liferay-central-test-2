de5170b5ca823c3ed3512245c42318cbd0876d18
Iván Zaera Avellón <ivan.zaera@liferay.com>
2015-02-20 12:03:22 +0100

    LPS-53463 Add a component per module to grab references to configuration and settings components. This is used to serve as a bridge between OSGi lifecycle and internal module classes which are not instantiated inside the OSGi lifecyle. By referencing an internal component, inside the same module, when we need a configuration or settings object, we make sure that no situations will arise where the configuration has not yet been loaded for example.
    Of course, this is a temporal situation until all classes are "lifecycled" by the OSGi container.

---
+++ modules/apps/wiki/wiki-service/src/com/liferay/wiki/service/settings/WikiServiceSettingsProvider.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wiki.service.settings;
+
+import com.liferay.portal.kernel.settings.SettingsProvider;
+import com.liferay.wiki.configuration.WikiConfiguration;
+import com.liferay.wiki.settings.WikiSettings;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Iván Zaera
+ */
+@Component(
+	immediate = true
+)
+public class WikiServiceSettingsProvider {
+
+	public static WikiServiceSettingsProvider getWikiServiceSettingsProvider() {
+		return _wikiServiceSettingsProvider;
+	}
+
+	public WikiConfiguration getWikiConfiguration() {
+		return _wikiConfiguration;
+	}
+
+	public SettingsProvider<WikiSettings> getWikiSettingsProvider() {
+		return _settingsProvider;
+	}
+
+	@Activate
+	protected void activate() {
+		_wikiServiceSettingsProvider = this;
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_wikiServiceSettingsProvider = null;
+	}
+
+	@Reference(
+		target = "(class.name=com.liferay.wiki.settings.WikiSettings)"
+	)
+	protected void setSettingsProvider(
+		SettingsProvider<WikiSettings> settingsProvider) {
+
+		_settingsProvider = settingsProvider;
+	}
+
+	@Reference
+	protected void setWikiConfiguration(WikiConfiguration wikiConfiguration) {
+		_wikiConfiguration = wikiConfiguration;
+	}
+
+	protected void unsetSettingsProvider(
+		SettingsProvider<WikiSettings> settingsProvider) {
+
+		_settingsProvider = null;
+	}
+
+	protected void unsetWikiConfiguration(WikiConfiguration wikiConfiguration) {
+		_wikiConfiguration = null;
+	}
+
+	private static WikiServiceSettingsProvider _wikiServiceSettingsProvider;
+
+	private SettingsProvider<WikiSettings> _settingsProvider;
+	private WikiConfiguration _wikiConfiguration;
+
+}
