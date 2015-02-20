d6671a20c09c780f881e6f3683bd72584879df1e
peterfellwock <peter.fellwock@liferay.com>
2015-02-19 23:39:32 -0800

    LPS-53601 Screen Name Validator Integration point test

---
+++ portal-impl/test/integration/com/liferay/portal/security/auth/ScreenNameValidatorFactoryTest.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.auth;
+
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.MainServletTestRule;
+import com.liferay.portal.test.rule.SyntheticBundleRule;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Peter Fellwock
+ */
+public class ScreenNameValidatorFactoryTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE,
+			new SyntheticBundleRule("bundle.screennamevalidatorfactory"));
+
+	@Test
+	public void testValidate() {
+		ScreenNameValidator screenNameValidator =
+			ScreenNameValidatorFactory.getInstance();
+
+		Assert.assertTrue(screenNameValidator.validate(1, "lftest"));
+
+		Assert.assertFalse(screenNameValidator.validate(2, "lftest"));
+
+		Assert.assertFalse(screenNameValidator.validate(1, "bob"));
+	}
+
+}
