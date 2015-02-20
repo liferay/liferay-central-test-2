ea5aeb79e5f0ff1d53f91f93f5a681e8605a9929
peterfellwock <peter.fellwock@liferay.com>
2015-02-19 23:39:14 -0800

    LPS-53601 Screen Name Generator Integration point test

---
+++ portal-impl/test/integration/com/liferay/portal/security/auth/ScreenNameGeneratorFactoryTest.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.auth;
+
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.MainServletTestRule;
+import com.liferay.portal.test.rule.SyntheticBundleRule;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Peter Fellwock
+ */
+public class ScreenNameGeneratorFactoryTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE,
+			new SyntheticBundleRule("bundle.screennamegeneratorfactory"));
+
+	@Test
+	public void testGenerate() {
+		try {
+			ScreenNameGenerator screenNameGenerator =
+				ScreenNameGeneratorFactory.getInstance();
+
+			Assert.assertEquals(
+				"1-1", screenNameGenerator.generate(
+					1, 1, "test@screenamegeneratorfactorytest.com"));
+		}
+		catch (Exception e) {
+			Assert.fail();
+		}
+	}
+
+}
