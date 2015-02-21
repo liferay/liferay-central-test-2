f0006f43de542d40c276d28c92b31b99eafea6e8
Raymond Auge <raymond.auge@liferay.com>
2015-02-21 12:28:11 -0500

    LPS-53601 SF

--- portal-impl/test/integration/com/liferay/portal/security/auth/bundle/fullnamevalidatorfactory/TestFullNameValidator.java
+++ portal-impl/test/integration/com/liferay/portal/security/auth/bundle/fullnamevalidatorfactory/TestFullNameValidator.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.portal.security.auth.bundle.fullnamevalidatorfactory;
 
 import com.liferay.portal.security.auth.FullNameValidator;
