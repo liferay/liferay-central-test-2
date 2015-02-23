28f5f4821fd31bd9a9165c58c1ed020aaf50188d
Michael Hashimoto <michael.hashimoto@liferay.com>
2015-02-23 10:33:14 -0800

    LRQA-11740 - Have 'build-test.xml' write the 'test.class.command.name' as a property to be used in the middle of a test

--- portal-web/test/functional/com/liferay/portalweb/util/TestPropsValues.java
+++ portal-web/test/functional/com/liferay/portalweb/util/TestPropsValues.java
@@ -133,6 +133,9 @@ public class TestPropsValues
 	public static final String TEST_BASE_DIR_NAME = TestPropsUtil.get(
 		"test.basedir");
 
+	public static final String TEST_CLASS_COMMAND_NAME = TestPropsUtil.get(
+		"test.class.command.name");
+
 	public static final boolean TEST_DATABASE_MINIMAL = GetterUtil.getBoolean(
 		TestPropsUtil.get("test.database.minimal"));
 
