1a329ae98981b2809d554b11979c59a3a807d11c
Michael Hashimoto <michael.hashimoto@liferay.com>
2015-02-23 12:40:39 -0800

    LRQA-11740 - Add 'test.class' whenever you run 'AntCommands' for PoshiRunner

--- modules/test/poshi-runner/src/com/liferay/poshi/runner/util/AntCommands.java
+++ modules/test/poshi-runner/src/com/liferay/poshi/runner/util/AntCommands.java
@@ -61,7 +61,8 @@ public class AntCommands implements Callable<Void> {
 			sb.append(_fileName);
 			sb.append(" ");
 			sb.append(_target);
-			sb.append(" -Dtest.ant.launched.by.selenium=true");
+			sb.append(" -Dtest.ant.launched.by.selenium=true -Dtest.class=");
+			sb.append(PropsValues.TEST_CLASS_COMMAND_NAME);
 		}
 
 		Process process = runtime.exec(sb.toString());
