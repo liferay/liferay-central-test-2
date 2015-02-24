7bfb0f2b7d6b10fc9fafb4adb46281358fc93802
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-02-24 09:44:42 +0100

    LPS-53683 upgrade

--- modules/apps/search/search-web/src/com/liferay/search/web/portlet/SearchPortlet.java
+++ modules/apps/search/search-web/src/com/liferay/search/web/portlet/SearchPortlet.java
@@ -15,10 +15,12 @@
 package com.liferay.search.web.portlet;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.search.web.upgrade.SearchWebUpgrade;
 
 import javax.portlet.Portlet;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Eudaldo Alonso
@@ -47,4 +49,9 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class SearchPortlet extends MVCPortlet {
+
+	@Reference(unbind = "-")
+	protected void setSearchWebUpgrade(SearchWebUpgrade searchWebUpgrade) {
+	}
+
 }
