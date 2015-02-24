c71cd4e5b073dbdcb06a5d234f4490e161bb8801
Jose A. Jimenez <jose.jimenez@liferay.com>
2015-02-24 19:03:06 +0100

    LPS-53821 Moving descriptors configuration inside the DocumentSelectorPortlet class

--- modules/apps/document-selector/document-selector-web/src/com/liferay/documentselector/web/portlet/DocumentSelectorPortlet.java
+++ modules/apps/document-selector/document-selector-web/src/com/liferay/documentselector/web/portlet/DocumentSelectorPortlet.java
@@ -16,9 +16,33 @@ package com.liferay.documentselector.web.portlet;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
 /**
  * @author Jose A. Jimenez
  */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.css-class-wrapper=portlet-document-selector",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.use-default-template=false",
+		"com.liferay.portlet.system=true",
+		"javax.portlet.portlet-name=200",
+		"javax.portlet.display-name=Document Selector",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=guest,power-user,user",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
 public class DocumentSelectorPortlet extends MVCPortlet {
 
 }
