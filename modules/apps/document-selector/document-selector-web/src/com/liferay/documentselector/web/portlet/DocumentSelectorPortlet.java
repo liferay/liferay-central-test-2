8dc832455c5b42f5c8ae0a111aa2e2224302befe
Jose A. Jimenez <jose.jimenez@liferay.com>
2015-02-25 09:11:18 +0100

    LPS-53821 Move the logic from the Action to the new MVC Portlet class, no logic changes

--- modules/apps/document-selector/document-selector-web/src/com/liferay/documentselector/web/portlet/DocumentSelectorPortlet.java
+++ modules/apps/document-selector/document-selector-web/src/com/liferay/documentselector/web/portlet/DocumentSelectorPortlet.java
@@ -14,9 +14,58 @@
 
 package com.liferay.documentselector.web.portlet;
 
+import com.liferay.documentselector.web.portlet.action.ActionForm;
+import com.liferay.documentselector.web.portlet.action.ActionForward;
+import com.liferay.documentselector.web.portlet.action.ActionMapping;
+import com.liferay.documentselector.web.util.DocumentSelectorUtil;
+import com.liferay.portal.NoSuchRepositoryEntryException;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.upload.LiferayFileItemException;
+import com.liferay.portal.kernel.upload.UploadException;
+import com.liferay.portal.kernel.upload.UploadPortletRequest;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.MimeTypesUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StreamUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.security.auth.PrincipalException;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceContextFactory;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portal.util.WebKeys;
+import com.liferay.portlet.asset.AssetCategoryException;
+import com.liferay.portlet.asset.AssetTagException;
+import com.liferay.portlet.documentlibrary.DuplicateFileException;
+import com.liferay.portlet.documentlibrary.DuplicateFolderNameException;
+import com.liferay.portlet.documentlibrary.FileExtensionException;
+import com.liferay.portlet.documentlibrary.FileMimeTypeException;
+import com.liferay.portlet.documentlibrary.FileNameException;
+import com.liferay.portlet.documentlibrary.FileSizeException;
+import com.liferay.portlet.documentlibrary.InvalidFileVersionException;
+import com.liferay.portlet.documentlibrary.NoSuchFileEntryException;
+import com.liferay.portlet.documentlibrary.NoSuchFileVersionException;
+import com.liferay.portlet.documentlibrary.NoSuchFolderException;
+import com.liferay.portlet.documentlibrary.SourceFileNameException;
+import com.liferay.portlet.documentlibrary.action.ActionUtil;
+import com.liferay.portlet.documentlibrary.antivirus.AntivirusScannerException;
+import com.liferay.portlet.documentlibrary.model.DLFileEntry;
+import com.liferay.portlet.documentlibrary.service.DLAppServiceUtil;
+import com.liferay.portlet.dynamicdatamapping.StorageFieldRequiredException;
 
+import java.io.InputStream;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.Portlet;
+import javax.portlet.PortletConfig;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -44,5 +93,214 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class DocumentSelectorPortlet extends MVCPortlet {
+
+	@Override
+	public void processAction(
+			ActionMapping actionMapping, ActionForm actionForm,
+			PortletConfig portletConfig, ActionRequest actionRequest,
+			ActionResponse actionResponse)
+		throws Exception {
+
+		try {
+			UploadException uploadException =
+				(UploadException)actionRequest.getAttribute(
+					WebKeys.UPLOAD_EXCEPTION);
+
+			if (uploadException != null) {
+				if (uploadException.isExceededLiferayFileItemSizeLimit()) {
+					throw new LiferayFileItemException();
+				}
+				else if (uploadException.isExceededSizeLimit()) {
+					throw new FileSizeException(uploadException.getCause());
+				}
+
+				throw new PortalException(uploadException.getCause());
+			}
+			else {
+				addFileEntry(actionRequest);
+			}
+
+			String redirect = PortalUtil.escapeRedirect(
+				ParamUtil.getString(actionRequest, "redirect"));
+
+			if (Validator.isNotNull(redirect)) {
+				actionResponse.sendRedirect(redirect);
+			}
+		}
+		catch (Exception e) {
+			handleUploadException(actionRequest, actionResponse, e);
+		}
+	}
+
+	@Override
+	public ActionForward render(
+			ActionMapping actionMapping, ActionForm actionForm,
+			PortletConfig portletConfig, RenderRequest renderRequest,
+			RenderResponse renderResponse)
+		throws Exception {
+
+		try {
+			ActionUtil.getFileEntry(renderRequest);
+		}
+		catch (Exception e) {
+			if (e instanceof NoSuchFileEntryException ||
+				e instanceof NoSuchFileVersionException ||
+				e instanceof NoSuchRepositoryEntryException ||
+				e instanceof PrincipalException) {
+
+				SessionErrors.add(renderRequest, e.getClass());
+
+				return actionMapping.findForward(
+					"portlet.document_selector.error");
+			}
+			else {
+				throw e;
+			}
+		}
+
+		String forward = "portlet.document_selector.add_file_entry";
+
+		return actionMapping.findForward(getForward(renderRequest, forward));
+	}
+
+	protected FileEntry addFileEntry(ActionRequest actionRequest)
+		throws Exception {
+
+		UploadPortletRequest uploadPortletRequest =
+			PortalUtil.getUploadPortletRequest(actionRequest);
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		long repositoryId = ParamUtil.getLong(
+			uploadPortletRequest, "repositoryId");
+		long folderId = ParamUtil.getLong(uploadPortletRequest, "folderId");
+		String sourceFileName = uploadPortletRequest.getFileName("file");
+		String title = ParamUtil.getString(uploadPortletRequest, "title");
+		String description = ParamUtil.getString(
+			uploadPortletRequest, "description");
+		String changeLog = ParamUtil.getString(
+			uploadPortletRequest, "changeLog");
+
+		if (folderId > 0) {
+			Folder folder = DLAppServiceUtil.getFolder(folderId);
+
+			if (folder.getGroupId() != themeDisplay.getScopeGroupId()) {
+				throw new NoSuchFolderException("{folderId=" + folderId + "}");
+			}
+		}
+
+		InputStream inputStream = null;
+
+		try {
+			String contentType = uploadPortletRequest.getContentType("file");
+
+			long size = uploadPortletRequest.getSize("file");
+
+			if (size == 0) {
+				contentType = MimeTypesUtil.getContentType(title);
+			}
+
+			if (size > 0) {
+				HttpServletRequest request = PortalUtil.getHttpServletRequest(
+					actionRequest);
+
+				String[] mimeTypes = DocumentSelectorUtil.getMimeTypes(request);
+
+				if (ArrayUtil.isNotEmpty(mimeTypes) &&
+					!ArrayUtil.contains(mimeTypes, contentType)) {
+
+					throw new FileMimeTypeException(contentType);
+				}
+			}
+
+			inputStream = uploadPortletRequest.getFileAsStream("file");
+
+			ServiceContext serviceContext = ServiceContextFactory.getInstance(
+				DLFileEntry.class.getName(), uploadPortletRequest);
+
+			FileEntry fileEntry = DLAppServiceUtil.addFileEntry(
+				repositoryId, folderId, sourceFileName, contentType, title,
+				description, changeLog, inputStream, size, serviceContext);
+
+			return fileEntry;
+		}
+		catch (Exception e) {
+			UploadException uploadException =
+				(UploadException)actionRequest.getAttribute(
+					WebKeys.UPLOAD_EXCEPTION);
+
+			if (uploadException != null) {
+				if (uploadException.isExceededLiferayFileItemSizeLimit()) {
+					throw new LiferayFileItemException();
+				}
+				else if (uploadException.isExceededSizeLimit()) {
+					throw new FileSizeException(uploadException.getCause());
+				}
+			}
+
+			throw e;
+		}
+		finally {
+			StreamUtil.cleanUp(inputStream);
+		}
+	}
+
+	protected void handleUploadException(
+			ActionRequest actionRequest, ActionResponse actionResponse,
+			Exception e)
+		throws Exception {
+
+		if (e instanceof AssetCategoryException ||
+			e instanceof AssetTagException) {
+
+			SessionErrors.add(actionRequest, e.getClass(), e);
+		}
+		else if (e instanceof AntivirusScannerException ||
+				 e instanceof DuplicateFileException ||
+				 e instanceof DuplicateFolderNameException ||
+				 e instanceof FileExtensionException ||
+				 e instanceof FileMimeTypeException ||
+				 e instanceof FileNameException ||
+				 e instanceof FileSizeException ||
+				 e instanceof LiferayFileItemException ||
+				 e instanceof NoSuchFolderException ||
+				 e instanceof SourceFileNameException ||
+				 e instanceof StorageFieldRequiredException) {
+
+			UploadException uploadException =
+				(UploadException)actionRequest.getAttribute(
+					WebKeys.UPLOAD_EXCEPTION);
+
+			if (uploadException != null) {
+				String uploadExceptionRedirect = ParamUtil.getString(
+					actionRequest, "uploadExceptionRedirect");
+
+				actionResponse.sendRedirect(uploadExceptionRedirect);
+
+				SessionErrors.add(actionRequest, e.getClass());
+
+				return;
+			}
+
+			if (e instanceof AntivirusScannerException) {
+				SessionErrors.add(actionRequest, e.getClass(), e);
+			}
+			else {
+				SessionErrors.add(actionRequest, e.getClass());
+			}
+		}
+		else if (e instanceof InvalidFileVersionException ||
+				 e instanceof NoSuchFileEntryException ||
+				 e instanceof PrincipalException) {
+
+			SessionErrors.add(actionRequest, e.getClass());
+
+			setForward(actionRequest, "portlet.document_library.error");
+		}
+		else {
+			throw e;
+		}
+	}
 
 }
