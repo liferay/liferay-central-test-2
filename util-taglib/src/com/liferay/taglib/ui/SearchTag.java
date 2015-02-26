e49e5224df446a773c685e31ec2cccc74db4e7d9
Julio Camarero <julio.camarero@liferay.com>
2015-02-26 11:28:23 +0100

    LPS-53683 taglib only includes one page

--- util-taglib/src/com/liferay/taglib/ui/SearchTag.java
+++ util-taglib/src/com/liferay/taglib/ui/SearchTag.java
@@ -16,35 +16,16 @@ package com.liferay.taglib.ui;
 
 import com.liferay.taglib.util.IncludeTag;
 
-import javax.servlet.jsp.JspWriter;
-
 /**
  * @author Brian Wing Shun Chan
  */
 public class SearchTag extends IncludeTag {
 
 	@Override
-	protected String getEndPage() {
-		return _END_PAGE;
+	protected String getPage() {
+		return _PAGE;
 	}
 
-	@Override
-	protected String getStartPage() {
-		return _START_PAGE;
-	}
-
-	@Override
-	protected int processEndTag() throws Exception {
-		JspWriter jspWriter = pageContext.getOut();
-
-		jspWriter.write("</form>");
-
-		return EVAL_PAGE;
-	}
-
-	private static final String _END_PAGE = "/html/taglib/ui/search/end.jsp";
-
-	private static final String _START_PAGE =
-		"/html/taglib/ui/search/start.jsp";
+	private static final String _PAGE = "/html/taglib/ui/search/page.jsp";
 
 }
