3b2000491818dc65f9b1615d94ee4d4b012b5f1f
Brian Chan <brian.chan@liferay.com>
2015-02-26 21:01:01 -0800

    Revert "LPS-53436 SF"
    This reverts commit 7d2fed33af20e6a5d93dbd90eda69673960e1db4.

--- portal-impl/src/com/liferay/portal/staging/StagingImpl.java
+++ portal-impl/src/com/liferay/portal/staging/StagingImpl.java
@@ -269,8 +269,7 @@ public class StagingImpl implements Staging {
 			String portletId)
 		throws PortalException {
 
-		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
-			WebKeys.THEME_DISPLAY);
+		long userId = PortalUtil.getUserId(portletRequest);
 
 		Map<String, String[]> parameterMap = getStagingParameters(
 			portletRequest);
@@ -279,13 +278,15 @@ public class StagingImpl implements Staging {
 			portletRequest, sourceGroupId, false, sourcePlid, portletId,
 			ExportImportDateUtil.RANGE_FROM_LAST_PUBLISH_DATE);
 
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
 		Map<String, Serializable> settingsMap =
 			ExportImportConfigurationSettingsMapFactory.buildSettingsMap(
-				themeDisplay.getUserId(), sourceGroupId, sourcePlid,
-				targetGroupId, targetPlid, portletId, parameterMap,
-				Constants.PUBLISH_TO_LIVE, dateRange.getStartDate(),
-				dateRange.getEndDate(), themeDisplay.getLocale(),
-				themeDisplay.getTimeZone());
+				userId, sourceGroupId, sourcePlid, targetGroupId, targetPlid,
+				portletId, parameterMap, Constants.PUBLISH_TO_LIVE,
+				dateRange.getStartDate(), dateRange.getEndDate(),
+				themeDisplay.getLocale(), themeDisplay.getTimeZone());
 
 		ServiceContext serviceContext = new ServiceContext();
 
