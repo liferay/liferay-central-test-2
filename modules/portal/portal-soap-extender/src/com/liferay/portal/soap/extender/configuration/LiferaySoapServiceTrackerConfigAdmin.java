df10358900b947026a736cfe14d586795caf4393
Carlos Sierra <carlos.sierra@liferay.com>
2015-03-02 15:02:36 +0100

    LPS-52679 Component to manage Extender configuration and dependencies
    This component leverages on apache dependency manager to create another
    component (LiferaySoapServiceTRacker), POJO based, and provide it with
    certain dependencies (using the constructor) and describe dependencies
    to other concrete services instances (the extensions).
    
    The POJO component is not started by dependency manager until all
    dependencies are satisfied.

---
+++ modules/portal/portal-soap-extender/src/com/liferay/portal/soap/extender/configuration/LiferaySoapServiceTrackerConfigAdmin.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.soap.extender.configuration;
+
+import aQute.bnd.annotation.metatype.Configurable;
+
+import com.liferay.portal.soap.extender.LiferaySoapServiceTracker;
+
+import org.apache.felix.dm.DependencyManager;
+import org.apache.felix.dm.ServiceDependency;
+
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+@Component(
+	configurationPid = "com.liferay.portal.soap.extender",
+	configurationPolicy = ConfigurationPolicy.OPTIONAL, immediate = true
+)
+public class LiferaySoapServiceTrackerConfigAdmin {
+
+	@Activate
+	protected void activate(final ComponentContext componentContext) {
+		DependencyManager dependencyManager = new DependencyManager(
+			componentContext.getBundleContext());
+
+		_liferaySoapServiceTrackerConfiguration =
+			Configurable.createConfigurable(
+				LiferaySoapServiceTrackerConfiguration.class,
+				componentContext.getProperties());
+
+		_component = dependencyManager.createComponent();
+
+		ExtensionManager extensionManager = new ExtensionManager();
+
+		LiferaySoapServiceTracker liferaySoapServiceTracker =
+			new LiferaySoapServiceTracker(
+				componentContext.getBundleContext(),
+				_liferaySoapServiceTrackerConfiguration.contextPath(),
+				extensionManager);
+
+		_component.setImplementation(liferaySoapServiceTracker);
+
+		String[] extensions =
+			_liferaySoapServiceTrackerConfiguration.extensions();
+
+		if (extensions != null) {
+			for (String extension : extensions) {
+				ServiceDependency serviceDependency =
+					dependencyManager.createServiceDependency();
+
+				serviceDependency.setCallbacks(
+					extensionManager, "addExtension", "removeExtension");
+				serviceDependency.setService(
+					Object.class, _createExtensionFilter(extension));
+				serviceDependency.setRequired(true);
+
+				_component.add(serviceDependency);
+			}
+		}
+
+		dependencyManager.add(_component);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_component.stop();
+	}
+
+	@Modified
+	protected void modified() {
+	}
+
+	private String _createExtensionFilter(String extensionName) {
+		return "(&(cxf.extension=true)(extension.name="+ extensionName +
+			")(extension.class=*))";
+	}
+
+	private org.apache.felix.dm.Component _component;
+	private LiferaySoapServiceTrackerConfiguration
+		_liferaySoapServiceTrackerConfiguration;
+
+}
