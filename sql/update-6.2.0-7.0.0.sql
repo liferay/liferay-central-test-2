86ee41b9b54a050c20084d9cd472e1c31f36a5dc
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-03-03 12:11:39 +0100

    LPS-53916 Same order as service.xml

--- sql/update-6.2.0-7.0.0.sql
+++ sql/update-6.2.0-7.0.0.sql
@@ -7,9 +7,9 @@ update AssetEntry set listable = TRUE;
 drop table AssetTagProperty;
 
 alter table BlogsEntry add subtitle STRING null;
-alter table BlogsEntry add coverImageCaption String null;
 alter table BlogsEntry add coverImageFileEntryId LONG;
 alter table BlogsEntry add coverImageURL STRING null;
+alter table BlogsEntry add coverImageCaption String null;
 alter table BlogsEntry add smallImageFileEntryId LONG;
 
 alter table DDMStructure add version VARCHAR(75) null;
