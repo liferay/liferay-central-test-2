17fea079072756b40437e6eaf8a7cc21a82515f7
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-03-05 15:32:06 +0100

    LPS-53988 Test the functionality of the stubbed extension point

---
+++ portal-impl/test/integration/com/liferay/portal/security/auth/AuthTokenUtilTest.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.auth;
+
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.MainServletTestRule;
+import com.liferay.portal.test.rule.SyntheticBundleRule;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class AuthTokenUtilTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE,
+			new SyntheticBundleRule("bundle.authtokenutil"));
+
+	@Test
+	public void testGetToken() {
+		Assert.assertEquals(
+			"TEST_TOKEN", AuthTokenUtil.getToken(new MockHttpServletRequest()));
+	}
+
+	@Test
+	public void testGetTokenByPlidAndPortletId() {
+		Assert.assertEquals(
+			"TEST_TOKEN_BY_PLID_AND_PORTLETID",
+			AuthTokenUtil.getToken(new MockHttpServletRequest(), 0L,
+				"portletId"));
+	}
+
+	@Test
+	public void testIsValidPortletInvocationToken() {
+		Assert.assertTrue(
+			AuthTokenUtil.isValidPortletInvocationToken(
+				new MockHttpServletRequest(), 0L, "portletId", "struts action",
+				"VALID_PORTLET_INVOCATION_TOKEN"));
+
+		Assert.assertFalse(
+			AuthTokenUtil.isValidPortletInvocationToken(
+				new MockHttpServletRequest(), 0L, "portletId", "struts action",
+				"foo"));
+	}
+
+}
