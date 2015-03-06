8e551d7eaeb49d585c335bb4371b4096e22f5844
Karen Dang <karen.dang@liferay.com>
2015-03-05 18:12:32 -0800

    LRQA-14048 - Unit tests for PoshiRunnerContext

---
+++ modules/test/poshi-runner/test/unit/com/liferay/poshi/runner/PoshiRunnerContextTest.java
@@ -0,0 +1,172 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.poshi.runner;
+
+import java.io.File;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.dom4j.Element;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Karen Dang
+ * @author Michael Hashimoto
+ */
+public class PoshiRunnerContextTest extends TestCase {
+
+	@Test
+	public void testGetActionCaseElements() {
+		List<Element> elements = PoshiRunnerContext.getActionCaseElements(
+			"Action1#test2");
+
+		for (Element element : elements) {
+			Assert.assertEquals(
+				"getActionCaseElements is failing", "/test2",
+				element.attributeValue("locator1"));
+		}
+	}
+
+	@Test
+	public void testGetActionCommandElement() {
+		Element element = PoshiRunnerContext.getActionCommandElement(
+			"Action1#test1");
+
+		Assert.assertEquals(
+			"getActionCommandElement is failing", "test1",
+			element.attributeValue("name"));
+	}
+
+	@Test
+	public void testGetActionLocatorCount() {
+		int locatorCount = PoshiRunnerContext.getActionLocatorCount(
+			"Action1#type");
+
+		Assert.assertEquals(
+			"getActionLocatorCount is failing", 1, locatorCount);
+	}
+
+	@Test
+	public void testGetActionRootElement() {
+		Element rootElement = PoshiRunnerContext.getActionRootElement(
+			"Action1");
+
+		Assert.assertEquals(
+			"getActionRootElement is failing", "definition",
+			rootElement.getName());
+	}
+
+	@Test
+	public void testGetFilePath() throws Exception {
+		String actualFilePath = PoshiRunnerContext.getFilePath(
+			"Action2.action");
+
+		String baseDir = PoshiRunnerGetterUtil.getCanonicalPath(
+			"test/unit/com/liferay/poshi/runner/");
+
+		File file = new File(baseDir + "/dependencies/Action2.action");
+
+		String expectedFilePath = file.getCanonicalPath();
+
+		Assert.assertEquals(
+			"getFilePath is failing", expectedFilePath, actualFilePath);
+	}
+
+	@Test
+	public void testGetFunctionCommandElement() throws Exception {
+		Element element = PoshiRunnerContext.getFunctionCommandElement(
+			"Click#clickAt");
+
+		Assert.assertEquals(
+			"getFunctionCommandElement is failing", "clickAt",
+			element.attributeValue("name"));
+	}
+
+	@Test
+	public void testGetFunctionLocatorCount() {
+		int locatorCount = PoshiRunnerContext.getFunctionLocatorCount("Click");
+
+		Assert.assertEquals(
+			"getFunctionLocatorCount is failing", 1, locatorCount);
+	}
+
+	@Test
+	public void testGetFunctionRootElement() {
+		Element element = PoshiRunnerContext.getFunctionRootElement("Click");
+
+		Assert.assertEquals(
+			"getFunctionRootElement is failing", "definition",
+			element.getName());
+	}
+
+	@Test
+	public void testGetMacroCommandElement() {
+		Element element = PoshiRunnerContext.getMacroCommandElement(
+			"Macro#test");
+
+		Assert.assertEquals(
+			"getMacroCommandElement is failing", "test",
+			element.attributeValue("name"));
+	}
+
+	@Test
+	public void testGetPathLocator() {
+		String locator = PoshiRunnerContext.getPathLocator(
+			"Action1#TEST_TITLE");
+
+		Assert.assertEquals(
+			"getPathLocator is failing", "//input[@class='Title']", locator);
+
+		locator = PoshiRunnerContext.getPathLocator("Action1#TEST_CONTENT");
+
+		Assert.assertEquals(
+			"getPathLocator is failing", "//input[@class='Content']", locator);
+	}
+
+	@Test
+	public void testGetSeleniumParameterCount() {
+		int count = PoshiRunnerContext.getSeleniumParameterCount("clickAt");
+
+		Assert.assertEquals("getSeleniumParameterCount is failing", 2, count);
+
+		count = PoshiRunnerContext.getSeleniumParameterCount("click");
+
+		Assert.assertEquals("getSeleniumParameterCount is failing", 1, count);
+	}
+
+	@Test
+	public void testGetTestcaseCommandElement() {
+		Element element = PoshiRunnerContext.getTestcaseCommandElement(
+			"Testcase#test");
+
+		Assert.assertEquals(
+			"getTestcaseCommandElement is failing", "test",
+			element.attributeValue("name"));
+	}
+
+	@Test
+	public void testGetTestcaseRootElement() {
+		Element element = PoshiRunnerContext.getTestcaseRootElement("Testcase");
+
+		Assert.assertEquals(
+			"getTestcaseRootElement is failing", "definition",
+			element.getName());
+	}
+
+}
