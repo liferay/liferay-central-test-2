86a4c2a2f8f911dd0dfffee90eb5f9c70d3c0894
John Zhao <john.zhao@liferay.com>
2015-03-06 14:10:13 +0800

    LPS-53812 The fix in LPS-33747 is not compatible now. When a thread message is not in a category, the category is null. The category.isRoot() checking cannot really help getting correct groupId.

--- portal-impl/src/com/liferay/portlet/messageboards/pop/MessageListenerImpl.java
+++ portal-impl/src/com/liferay/portlet/messageboards/pop/MessageListenerImpl.java
@@ -164,22 +164,14 @@ public class MessageListenerImpl implements MessageListener {
 				categoryId);
 
 			if (category == null) {
-				groupId = categoryId;
 				categoryId = MBCategoryConstants.DEFAULT_PARENT_CATEGORY_ID;
+
+				if (parentMessage != null) {
+					groupId = parentMessage.getGroupId();
+				}
 			}
 			else {
 				groupId = category.getGroupId();
-
-				if (category.isRoot()) {
-					long messageId = MBUtil.getMessageId(messageIdString);
-
-					MBMessage threadMessage =
-						MBMessageLocalServiceUtil.fetchMBMessage(messageId);
-
-					if (threadMessage != null) {
-						groupId = threadMessage.getGroupId();
-					}
-				}
 			}
 
 			if (_log.isDebugEnabled()) {
