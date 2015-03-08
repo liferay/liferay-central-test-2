d87c766642fe10fce010144325a99096fc2517de
shuyangzhou <shuyang.zhou@liferay.com>
2015-03-07 17:35:16 -0800

    LPS-54094 Add defense for concurrent ResourceAction populating

--- portal-impl/src/com/liferay/portal/service/impl/ResourceActionLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/ResourceActionLocalServiceImpl.java
@@ -41,6 +41,29 @@ public class ResourceActionLocalServiceImpl
 	extends ResourceActionLocalServiceBaseImpl {
 
 	@Override
+	public ResourceAction addResourceAction(
+		String name, String actionId, long bitwiseValue) {
+
+		ResourceAction resourceAction = resourceActionPersistence.fetchByN_A(
+			name, actionId);
+
+		if (resourceAction == null) {
+			long resourceActionId = counterLocalService.increment(
+				ResourceAction.class.getName());
+
+			resourceAction = resourceActionPersistence.create(resourceActionId);
+
+			resourceAction.setName(name);
+			resourceAction.setActionId(actionId);
+			resourceAction.setBitwiseValue(bitwiseValue);
+
+			resourceActionPersistence.update(resourceAction);
+		}
+
+		return resourceAction;
+	}
+
+	@Override
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public void checkResourceActions() {
 		List<ResourceAction> resourceActions =
@@ -102,17 +125,16 @@ public class ResourceActionLocalServiceImpl
 					bitwiseValue = lastBitwiseValue;
 				}
 
-				long resourceActionId = counterLocalService.increment(
-					ResourceAction.class.getName());
-
-				resourceAction = resourceActionPersistence.create(
-					resourceActionId);
-
-				resourceAction.setName(name);
-				resourceAction.setActionId(actionId);
-				resourceAction.setBitwiseValue(bitwiseValue);
-
-				resourceActionPersistence.update(resourceAction);
+				try {
+					resourceAction =
+						resourceActionLocalService.addResourceAction(
+							name, actionId, bitwiseValue);
+				}
+				catch (Throwable t) {
+					resourceAction =
+						resourceActionLocalService.addResourceAction(
+							name, actionId, bitwiseValue);
+				}
 
 				if (newResourceActions == null) {
 					newResourceActions = new ArrayList<>();
