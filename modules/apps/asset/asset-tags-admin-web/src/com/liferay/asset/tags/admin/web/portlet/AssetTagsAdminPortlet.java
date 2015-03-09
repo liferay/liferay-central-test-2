677090e8ed40e12b6683f6adedf4252a13539ef5
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-03-09 16:58:26 +0100

    LPS-52368 SF

--- modules/apps/asset/asset-tags-admin-web/src/com/liferay/asset/tags/admin/web/portlet/AssetTagsAdminPortlet.java
+++ modules/apps/asset/asset-tags-admin-web/src/com/liferay/asset/tags/admin/web/portlet/AssetTagsAdminPortlet.java
@@ -65,7 +65,7 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.resource-bundle=content.Language",
 		"javax.portlet.security-role-ref=administrator",
 		"javax.portlet.supports.mime-type=text/html"
-		},
+	},
 	service = Portlet.class
 )
 public class AssetTagsAdminPortlet extends MVCPortlet {
