eb3eb5c8bbd3566fb1cd069da5f422e3fc3d105b
Raymond Auge <raymond.auge@liferay.com>
2015-03-09 12:45:00 -0400

    LPS-54184 FilterExceptionAdaptor

---
+++ modules/portal/portal-wab-extender/src/com/liferay/portal/wab/extender/internal/adaptor/FilterExceptionAdaptor.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.wab.extender.internal.adaptor;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * @author Raymond Augé
+ */
+public class FilterExceptionAdaptor implements Filter {
+
+	public FilterExceptionAdaptor(Filter filter) {
+		_filter = filter;
+	}
+
+	@Override
+	public void destroy() {
+		_filter.destroy();
+	}
+
+	@Override
+	public void doFilter(
+			ServletRequest servletRequest, ServletResponse servletResponse,
+			FilterChain filterChain)
+		throws IOException, ServletException {
+
+		_filter.doFilter(servletRequest, servletResponse, filterChain);
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		try {
+			_filter.init(filterConfig);
+		}
+		catch (Exception e) {
+			_exception = e;
+		}
+	}
+
+	public Exception getException() {
+		return _exception;
+	}
+
+	private final Filter _filter;
+	private Exception _exception;
+
+}
