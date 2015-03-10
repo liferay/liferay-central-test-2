e1b185c7cf06b9efbc6d84f2536c21081880c21e
iobrist <isaac.obrist@liferay.com>
2015-03-10 14:42:13 -0700

    LPS-54082 - Added public static inner class to replace _type for RequiredGroupException, and depricate old

--- portal-service/src/com/liferay/portal/RequiredGroupException.java
+++ portal-service/src/com/liferay/portal/RequiredGroupException.java
@@ -18,23 +18,79 @@ import com.liferay.portal.kernel.exception.PortalException;
 
 /**
  * @author Brian Wing Shun Chan
+ * @author Isaac Obrist
  */
 public class RequiredGroupException extends PortalException {
 
+	@Deprecated
 	public static final int CURRENT_GROUP = 3;
 
+	@Deprecated
 	public static final int PARENT_GROUP = 2;
 
+	@Deprecated
 	public static final int SYSTEM_GROUP = 1;
 
-	public RequiredGroupException(String message, int type) {
+	/**
+	 * @deprecated As of 7.0.0, replaced by the inner classes
+	 */
+	@Deprecated
+	public int getType() {
+		return _type;
+	}
+
+	public static class MustNotDeleteCurrentSite extends RequiredGroupException{
+
+		public MustNotDeleteCurrentSite(long groupId){
+			super(
+				String.format(
+					"Site %s cannot be deleted because you are currently accessing it.",
+					groupId),
+				CURRENT_GROUP);
+
+			this.groupId = groupId;
+		}
+
+		public long groupId;
+
+	}
+
+	public static class MustNotDeleteSiteThatHasChild extends RequiredGroupException{
+
+		public MustNotDeleteSiteThatHasChild(long groupId){
+			super(
+				String.format(
+					"Site %s cannot be deleted because it has child sites",
+					groupId),
+				PARENT_GROUP);
+
+			this.groupId = groupId;
+		}
+
+		public long groupId;
+
+	}
+
+	public static class MustNotDeleteSystemGroup extends RequiredGroupException{
+
+		public MustNotDeleteSystemGroup(long groupId){
+			super(
+				String.format(
+					"The site %s is required by the system, and cannot be deleted",
+					groupId),
+				SYSTEM_GROUP);
+
+			this.groupId = groupId;
+		}
+
+		public long groupId;
+
+	}
+
+	private RequiredGroupException(String message, int type){
 		super(message);
 
 		_type = type;
-	}
-
-	public int getType() {
-		return _type;
 	}
 
 	private final int _type;
