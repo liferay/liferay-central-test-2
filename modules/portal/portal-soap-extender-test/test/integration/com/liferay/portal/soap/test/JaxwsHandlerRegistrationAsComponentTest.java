4f91e277e121b593fbb84de1efec72d6a923444b
Brian Chan <brian.chan@liferay.com>
2015-03-10 16:12:54 -0700

    LPS-52679 Simplify

--- modules/portal/portal-soap-extender-test/test/integration/com/liferay/portal/soap/test/JaxwsHandlerRegistrationAsComponentTest.java
+++ modules/portal/portal-soap-extender-test/test/integration/com/liferay/portal/soap/test/JaxwsHandlerRegistrationAsComponentTest.java
@@ -16,14 +16,9 @@ package com.liferay.portal.soap.test;
 
 import com.liferay.portal.soap.sample.service.Greeter;
 
-import java.io.IOException;
-
 import java.net.URL;
 
-import javax.xml.bind.JAXBException;
 import javax.xml.namespace.QName;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.soap.SOAPException;
 import javax.xml.ws.Service;
 
 import org.arquillian.liferay.deploymentscenario.annotations.BndFile;
@@ -45,10 +40,7 @@ import org.junit.runner.RunWith;
 public class JaxwsHandlerRegistrationAsComponentTest {
 
 	@Test
-	public void testHandlerIsRegistered()
-		throws JAXBException, IOException, SOAPException,
-			ParserConfigurationException {
-
+	public void testHandlerIsRegistered() throws Exception {
 		URL url = new URL(_url, "/o/soap/greeter?wsdl");
 
 		QName qName = new QName(
