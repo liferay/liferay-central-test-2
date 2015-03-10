56c476563eb7b3be7deb3796f25a2a1c91f3216b
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-03-10 17:42:47 +0100

    LPS-54214 Return the trashEntry moved

--- portal-impl/src/com/liferay/portlet/trash/service/impl/TrashEntryServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/trash/service/impl/TrashEntryServiceImpl.java
@@ -303,7 +303,7 @@ public class TrashEntryServiceImpl extends TrashEntryServiceBaseImpl {
 	 *         location, or if a portal exception occurred
 	 */
 	@Override
-	public void moveEntry(
+	public TrashEntry moveEntry(
 			String className, long classPK, long destinationContainerModelId,
 			ServiceContext serviceContext)
 		throws PortalException {
@@ -351,6 +351,8 @@ public class TrashEntryServiceImpl extends TrashEntryServiceBaseImpl {
 
 		trashHandler.moveTrashEntry(
 			getUserId(), classPK, destinationContainerModelId, serviceContext);
+
+		return trashEntry;
 	}
 
 	@Override
