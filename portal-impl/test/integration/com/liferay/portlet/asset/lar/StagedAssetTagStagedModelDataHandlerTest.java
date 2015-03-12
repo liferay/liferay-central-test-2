44a566c503eb2a18dc74e7a8f3c8461a0a83d4a7
Daniel Kocsis <daniel.kocsis@liferay.com>
2015-03-12 15:35:44 +0100

    LPS-54045 Add test

---
+++ portal-impl/test/integration/com/liferay/portlet/asset/lar/StagedAssetTagStagedModelDataHandlerTest.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.asset.lar;
+
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.TransactionalTestRule;
+import com.liferay.portal.lar.test.BaseStagedModelDataHandlerTestCase;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.StagedModel;
+import com.liferay.portal.model.adapter.ModelAdapterUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.MainServletTestRule;
+import com.liferay.portlet.asset.adapter.StagedAssetTag;
+import com.liferay.portlet.asset.model.AssetTag;
+import com.liferay.portlet.asset.service.AssetTagLocalServiceUtil;
+import com.liferay.portlet.asset.util.test.AssetTestUtil;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+
+/**
+ * @author Daniel Kocsis
+ */
+public class StagedAssetTagStagedModelDataHandlerTest
+	extends BaseStagedModelDataHandlerTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE,
+			TransactionalTestRule.INSTANCE);
+
+	@Override
+	protected StagedModel addStagedModel(
+			Group group, Map<String,
+			List<StagedModel>> dependentStagedModelsMap)
+		throws Exception {
+
+		AssetTag tag = AssetTestUtil.addTag(group.getGroupId());
+
+		return ModelAdapterUtil.adapt(
+			tag, AssetTag.class, StagedAssetTag.class);
+	}
+
+	@Override
+	protected StagedModel getStagedModel(String uuid, Group group) {
+		try {
+			AssetTag tag = AssetTagLocalServiceUtil.getTag(
+				group.getGroupId(), uuid);
+
+			return ModelAdapterUtil.adapt(
+				tag, AssetTag.class, StagedAssetTag.class);
+		}
+		catch (Exception e) {
+			return null;
+		}
+	}
+
+	@Override
+	protected Class<? extends StagedModel> getStagedModelClass() {
+		return StagedAssetTag.class;
+	}
+
+}
