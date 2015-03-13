aee05cce7801c3f4c533037f91624d8186b0de51
Zsolt Oláh <zsolt.olah@liferay.com>
2015-03-13 12:00:23 +0100

    LPS-53825 Friendly URL beginning with the same string as proxy path is filtered wrong

--- portal-impl/src/com/liferay/portal/servlet/filters/virtualhost/VirtualHostFilter.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/virtualhost/VirtualHostFilter.java
@@ -161,16 +161,30 @@ public class VirtualHostFilter extends BasePortalFilter {
 
 		long companyId = PortalInstances.getCompanyId(request);
 
+		String proxyPath = PortalUtil.getPathProxy();
+
 		String contextPath = PortalUtil.getPathContext();
 
 		String originalFriendlyURL = request.getRequestURI();
 
 		String friendlyURL = originalFriendlyURL;
 
-		if (Validator.isNotNull(contextPath) &&
-			friendlyURL.contains(contextPath)) {
+		friendlyURL = StringUtil.replace(
+			friendlyURL, StringPool.DOUBLE_SLASH, StringPool.SLASH);
 
-			friendlyURL = friendlyURL.substring(contextPath.length());
+		if (!friendlyURL.equals(StringPool.SLASH) &&
+				Validator.isNotNull(contextPath)) {
+
+			if (Validator.isNotNull(proxyPath) &&
+				contextPath.startsWith(proxyPath)) {
+
+				contextPath = contextPath.substring(proxyPath.length());
+			}
+
+			if (friendlyURL.startsWith(contextPath)) {
+
+				friendlyURL = friendlyURL.substring(contextPath.length());
+			}
 		}
 
 		int pos = friendlyURL.indexOf(StringPool.SEMICOLON);
@@ -178,9 +192,6 @@ public class VirtualHostFilter extends BasePortalFilter {
 		if (pos != -1) {
 			friendlyURL = friendlyURL.substring(0, pos);
 		}
-
-		friendlyURL = StringUtil.replace(
-			friendlyURL, StringPool.DOUBLE_SLASH, StringPool.SLASH);
 
 		String i18nLanguageId = null;
 
