18c167ad6bfd608371b9c8470b1813d2d24c62b0
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2015-03-16 14:48:02 +0100

    LPS-54286 Proper upgrade versions for the Unit Converter app

--- modules/apps/unit-converter/unit-converter-web/src/com/liferay/unit/converter/web/upgrade/UnitConverterWebUpgrade.java
+++ modules/apps/unit-converter/unit-converter-web/src/com/liferay/unit/converter/web/upgrade/UnitConverterWebUpgrade.java
@@ -64,7 +64,7 @@ public class UnitConverterWebUpgrade {
 
 		_releaseLocalService.updateRelease(
 			"com.liferay.unit.converter.web",
-			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 0,
+			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 1,
 			false);
 	}
 
