151a85bcfff096d102e1927cba4649e06abd5a41
Preston-Crary <preston.crary@liferay.com>
2015-03-16 13:59:18 -0700

    LPS-53563 Test

--- portal-impl/test/integration/com/liferay/portal/verify/VerifyPermissionTest.java
+++ portal-impl/test/integration/com/liferay/portal/verify/VerifyPermissionTest.java
@@ -14,16 +14,49 @@
 
 package com.liferay.portal.verify;
 
+import com.liferay.portal.events.TestServicePreAction;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.model.Company;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.Layout;
+import com.liferay.portal.model.LayoutConstants;
+import com.liferay.portal.model.Portlet;
+import com.liferay.portal.model.ResourceConstants;
+import com.liferay.portal.model.ResourcePermission;
+import com.liferay.portal.model.Role;
+import com.liferay.portal.model.RoleConstants;
+import com.liferay.portal.model.User;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.service.CompanyLocalServiceUtil;
+import com.liferay.portal.service.LayoutLocalServiceUtil;
+import com.liferay.portal.service.PortletLocalServiceUtil;
+import com.liferay.portal.service.ResourcePermissionLocalServiceUtil;
+import com.liferay.portal.service.RoleLocalServiceUtil;
+import com.liferay.portal.service.permission.PortletPermissionUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.MainServletTestRule;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portal.util.PortletKeys;
 import com.liferay.portal.verify.test.BaseVerifyProcessTestCase;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
+import org.junit.Test;
+
+import org.springframework.mock.web.MockHttpServletRequest;
 
 /**
  * @author Manuel de la Peña
+ * @author Preston Crary
  */
 public class VerifyPermissionTest extends BaseVerifyProcessTestCase {
 
@@ -33,9 +66,103 @@ public class VerifyPermissionTest extends BaseVerifyProcessTestCase {
 		new AggregateTestRule(
 			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE);
 
+	@Test
+	public void testFixUserDefaultRolePermissions() throws Exception {
+		Layout layout = addPersonalLayoutWithDefaultResource();
+
+		Role powerUserRole = RoleLocalServiceUtil.getRole(
+			TestPropsValues.getCompanyId(), RoleConstants.POWER_USER);
+		Role userRole = RoleLocalServiceUtil.getRole(
+			TestPropsValues.getCompanyId(), RoleConstants.USER);
+
+		String primKey = PortletPermissionUtil.getPrimaryKey(
+			layout.getPlid(), PortletKeys.DOCKBAR);
+
+		ResourcePermission resourcePermission =
+			ResourcePermissionLocalServiceUtil.fetchResourcePermission(
+				TestPropsValues.getCompanyId(), PortletKeys.DOCKBAR,
+				ResourceConstants.SCOPE_INDIVIDUAL, primKey,
+				powerUserRole.getRoleId());
+
+		Assert.assertNull(resourcePermission);
+
+		resourcePermission =
+			ResourcePermissionLocalServiceUtil.getResourcePermission(
+				TestPropsValues.getCompanyId(), PortletKeys.DOCKBAR,
+				ResourceConstants.SCOPE_INDIVIDUAL, primKey,
+				userRole.getRoleId());
+
+		Assert.assertTrue(resourcePermission.hasActionId(ActionKeys.VIEW));
+
+		resourcePermission.setRoleId(powerUserRole.getRoleId());
+
+		ResourcePermissionLocalServiceUtil.updateResourcePermission(
+			resourcePermission);
+
+		doVerify();
+
+		resourcePermission =
+			ResourcePermissionLocalServiceUtil.fetchResourcePermission(
+				TestPropsValues.getCompanyId(), PortletKeys.DOCKBAR,
+				ResourceConstants.SCOPE_INDIVIDUAL, primKey,
+				powerUserRole.getRoleId());
+
+		Assert.assertNull(resourcePermission);
+
+		resourcePermission =
+			ResourcePermissionLocalServiceUtil.getResourcePermission(
+				TestPropsValues.getCompanyId(), PortletKeys.DOCKBAR,
+				ResourceConstants.SCOPE_INDIVIDUAL, primKey,
+				userRole.getRoleId());
+
+		Assert.assertTrue(resourcePermission.hasActionId(ActionKeys.VIEW));
+	}
+
+	protected Layout addPersonalLayoutWithDefaultResource() throws Exception {
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		User user = UserTestUtil.addUser();
+
+		themeDisplay.setUser(user);
+
+		Company company = CompanyLocalServiceUtil.getCompany(
+			user.getCompanyId());
+
+		themeDisplay.setCompany(company);
+
+		TestServicePreAction.INSTANCE.addDefaultUserPublicLayouts(user);
+
+		Group userGroup = user.getGroup();
+
+		List<Layout> layouts = LayoutLocalServiceUtil.getLayouts(
+			userGroup.getGroupId(), true,
+			LayoutConstants.DEFAULT_PARENT_LAYOUT_ID);
+
+		if (layouts.isEmpty()) {
+			layouts = LayoutLocalServiceUtil.getLayouts(
+				userGroup.getGroupId(), false,
+				LayoutConstants.DEFAULT_PARENT_LAYOUT_ID);
+		}
+
+		Layout layout = layouts.get(0);
+
+		themeDisplay.setLayout(layout);
+
+		_request.setAttribute(WebKeys.THEME_DISPLAY, themeDisplay);
+
+		Portlet portlet = PortletLocalServiceUtil.getPortletById(
+			user.getCompanyId(), PortletKeys.DOCKBAR);
+
+		PortalUtil.addPortletDefaultResource(_request, portlet);
+
+		return layout;
+	}
+
 	@Override
 	protected VerifyProcess getVerifyProcess() {
 		return new VerifyPermission();
 	}
+
+	private final HttpServletRequest _request = new MockHttpServletRequest();
 
 }
---
+++ portal-test-internal/src/com/liferay/portal/events/TestServicePreAction.java
@@ -0,0 +1,166 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.events;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.model.Layout;
+import com.liferay.portal.model.User;
+import com.liferay.portal.security.permission.PermissionChecker;
+
+import java.io.File;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Preston Crary
+ */
+public class TestServicePreAction extends ServicePreAction {
+
+	public static TestServicePreAction INSTANCE = new TestServicePreAction();
+
+	@Override
+	public void addDefaultLayoutsByLAR(
+			long userId, long groupId, boolean privateLayout, File larFile)
+		throws PortalException {
+
+		super.addDefaultLayoutsByLAR(userId, groupId, privateLayout, larFile);
+	}
+
+	@Override
+	public void addDefaultUserPrivateLayoutByProperties(
+			long userId, long groupId)
+		throws PortalException {
+
+		super.addDefaultUserPublicLayoutByProperties(userId, groupId);
+	}
+
+	@Override
+	public void addDefaultUserPrivateLayouts(User user) throws PortalException {
+		super.addDefaultUserPrivateLayouts(user);
+	}
+
+	@Override
+	public void addDefaultUserPublicLayoutByProperties(
+			long userId, long groupId)
+		throws PortalException {
+
+		super.addDefaultUserPublicLayoutByProperties(userId, groupId);
+	}
+
+	@Override
+	public void addDefaultUserPublicLayouts(User user) throws PortalException {
+		super.addDefaultUserPublicLayouts(user);
+	}
+
+	@Override
+	public void deleteDefaultUserPrivateLayouts(User user)
+		throws PortalException {
+
+		super.deleteDefaultUserPrivateLayouts(user);
+	}
+
+	@Override
+	public void deleteDefaultUserPublicLayouts(User user)
+		throws PortalException {
+
+		super.deleteDefaultUserPublicLayouts(user);
+	}
+
+	@Override
+	public Object[] getDefaultLayout(
+			HttpServletRequest request, User user, boolean signedIn)
+		throws PortalException {
+
+		return super.getDefaultLayout(request, user, signedIn);
+	}
+
+	@Override
+	public Object[] getViewableLayouts(
+			HttpServletRequest request, User user,
+			PermissionChecker permissionChecker, Layout layout,
+			List<Layout> layouts, long doAsGroupId, String controlPanelCategory)
+		throws PortalException {
+
+		return super.getViewableLayouts(
+			request, user, permissionChecker, layout, layouts, doAsGroupId,
+			controlPanelCategory);
+	}
+
+	@Override
+	public boolean hasAccessPermission(
+			PermissionChecker permissionChecker, Layout layout,
+			long doAsGroupId, String controlPanelCategory,
+			boolean checkViewableGroup)
+		throws PortalException {
+
+		return super.hasAccessPermission(
+			permissionChecker, layout, doAsGroupId, controlPanelCategory,
+			checkViewableGroup);
+	}
+
+	@Override
+	public void initImportLARFiles() {
+		super.initImportLARFiles();
+	}
+
+	@Override
+	public boolean isLoginRequest(HttpServletRequest request) {
+		return super.isLoginRequest(request);
+	}
+
+	@Override
+	public List<Layout> mergeAdditionalLayouts(
+			HttpServletRequest request, User user,
+			PermissionChecker permissionChecker, Layout layout,
+			List<Layout> layouts, long doAsGroupId, String controlPanelCategory)
+		throws PortalException {
+
+		return super.mergeAdditionalLayouts(
+			request, user, permissionChecker, layout, layouts, doAsGroupId,
+			controlPanelCategory);
+	}
+
+	@Override
+	public void processControlPanelRedirects(
+			HttpServletRequest request, HttpServletResponse response)
+		throws Exception {
+
+		super.processControlPanelRedirects(request, response);
+	}
+
+	@Override
+	public void rememberVisitedGroupIds(
+		HttpServletRequest request, long currentGroupId) {
+
+		super.rememberVisitedGroupIds(request, currentGroupId);
+	}
+
+	@Override
+	public void servicePre(
+			HttpServletRequest request, HttpServletResponse response)
+		throws Exception {
+
+		super.servicePre(request, response);
+	}
+
+	@Override
+	public void updateUserLayouts(User user) throws Exception {
+		super.updateUserLayouts(user);
+	}
+
+}
