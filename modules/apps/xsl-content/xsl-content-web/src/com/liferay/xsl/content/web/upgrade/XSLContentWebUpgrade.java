cc8fdd4395f41eac9e0a5a8fc751686366b4e07a
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2015-03-16 14:49:19 +0100

    LPS-54286 Proper upgrade versions for the XSL Content app

--- modules/apps/xsl-content/xsl-content-web/src/com/liferay/xsl/content/web/upgrade/XSLContentWebUpgrade.java
+++ modules/apps/xsl-content/xsl-content-web/src/com/liferay/xsl/content/web/upgrade/XSLContentWebUpgrade.java
@@ -62,7 +62,7 @@ public class XSLContentWebUpgrade {
 
 		_releaseLocalService.updateRelease(
 			"com.liferay.xsl.content.web",
-			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 0,
+			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 1,
 			false);
 	}
 
