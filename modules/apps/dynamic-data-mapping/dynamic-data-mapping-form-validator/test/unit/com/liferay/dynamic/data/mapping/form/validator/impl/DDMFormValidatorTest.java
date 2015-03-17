77b63f6a9fa0ae93bb5e34d9f8ba287ca421f4ba
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-03-17 14:49:30 -0300

    LPS-53919 Unit tests

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-validator/test/unit/com/liferay/dynamic/data/mapping/form/validator/impl/DDMFormValidatorTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-validator/test/unit/com/liferay/dynamic/data/mapping/form/validator/impl/DDMFormValidatorTest.java
@@ -14,6 +14,8 @@
 
 package com.liferay.dynamic.data.mapping.form.validator.impl;
 
+import static org.mockito.Mockito.when;
+
 import com.liferay.dynamic.data.mapping.form.validator.DDMFormValidationException;
 import com.liferay.dynamic.data.mapping.form.validator.DDMFormValidator;
 import com.liferay.portal.bean.BeanPropertiesImpl;
@@ -24,13 +26,20 @@ import com.liferay.portlet.dynamicdatamapping.model.DDMFormField;
 import com.liferay.portlet.dynamicdatamapping.model.DDMFormFieldOptions;
 import com.liferay.portlet.dynamicdatamapping.model.DDMFormFieldType;
 import com.liferay.portlet.dynamicdatamapping.model.LocalizedValue;
+import com.liferay.portlet.dynamicdatamapping.registry.DDMFormFieldTypeRegistry;
+import com.liferay.portlet.dynamicdatamapping.registry.DDMFormFieldTypeRegistryUtil;
 import com.liferay.portlet.dynamicdatamapping.util.test.DDMFormTestUtil;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Locale;
 import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 /**
  * @author Marcellus Tavares
@@ -39,7 +48,10 @@ public class DDMFormValidatorTest {
 
 	@Before
 	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+
 		setUpBeanPropertiesUtil();
+		setUpDDMFormFieldTypeRegistryUtil();
 	}
 
 	@Test(expected = DDMFormValidationException.class)
@@ -50,6 +62,25 @@ public class DDMFormValidatorTest {
 
 		ddmForm.setAvailableLocales(createAvailableLocales(LocaleUtil.BRAZIL));
 		ddmForm.setDefaultLocale(LocaleUtil.US);
+
+		_ddmFormValidator.validate(ddmForm);
+	}
+
+	@Test(expected = DDMFormValidationException.class)
+	public void testValidationWithDuplicateFieldName() throws Exception {
+		DDMForm ddmForm = DDMFormTestUtil.createDDMForm(
+			createAvailableLocales(LocaleUtil.US), LocaleUtil.US);
+
+		ddmForm.addDDMFormField(
+			new DDMFormField("Name1", DDMFormFieldType.TEXT));
+
+		DDMFormField name2DDMFormField = new DDMFormField(
+			"Name2", DDMFormFieldType.TEXT);
+
+		name2DDMFormField.addNestedDDMFormField(
+			new DDMFormField("Name1", DDMFormFieldType.TEXT));
+
+		ddmForm.addDDMFormField(name2DDMFormField);
 
 		_ddmFormValidator.validate(ddmForm);
 	}
@@ -76,18 +107,6 @@ public class DDMFormValidatorTest {
 
 		DDMFormField ddmFormField = new DDMFormField(
 			"*", DDMFormFieldType.TEXT);
-
-		ddmForm.addDDMFormField(ddmFormField);
-
-		_ddmFormValidator.validate(ddmForm);
-	}
-
-	@Test(expected = DDMFormValidationException.class)
-	public void testValidationWithInvalidFieldType() throws Exception {
-		DDMForm ddmForm = DDMFormTestUtil.createDDMForm(
-			createAvailableLocales(LocaleUtil.US), LocaleUtil.US);
-
-		DDMFormField ddmFormField = new DDMFormField("Text", null);
 
 		ddmForm.addDDMFormField(ddmFormField);
 
@@ -124,6 +143,45 @@ public class DDMFormValidatorTest {
 		DDMForm ddmForm = new DDMForm();
 
 		ddmForm.setDefaultLocale(null);
+
+		_ddmFormValidator.validate(ddmForm);
+	}
+
+	@Test
+	public void testValidationWithRegisteredTextFieldType() throws Exception {
+		DDMForm ddmForm = DDMFormTestUtil.createDDMForm(
+			createAvailableLocales(LocaleUtil.US), LocaleUtil.US);
+
+		DDMFormField ddmFormField = new DDMFormField(
+			"Text", DDMFormFieldType.TEXT);
+
+		ddmForm.addDDMFormField(ddmFormField);
+
+		_ddmFormValidator.validate(ddmForm);
+	}
+
+	@Test(expected = DDMFormValidationException.class)
+	public void testValidationWithUnregisteredFieldType() throws Exception {
+		DDMForm ddmForm = DDMFormTestUtil.createDDMForm(
+			createAvailableLocales(LocaleUtil.US), LocaleUtil.US);
+
+		DDMFormField ddmFormField = new DDMFormField(
+			"Name", "Unregistered Type");
+
+		ddmForm.addDDMFormField(ddmFormField);
+
+		_ddmFormValidator.validate(ddmForm);
+	}
+
+	@Test
+	public void testValidationWithValidFieldName() throws Exception {
+		DDMForm ddmForm = DDMFormTestUtil.createDDMForm(
+			createAvailableLocales(LocaleUtil.US), LocaleUtil.US);
+
+		DDMFormField ddmFormField = new DDMFormField(
+			"valid_name", DDMFormFieldType.TEXT);
+
+		ddmForm.addDDMFormField(ddmFormField);
 
 		_ddmFormValidator.validate(ddmForm);
 	}
@@ -221,6 +279,26 @@ public class DDMFormValidatorTest {
 
 		beanPropertiesUtil.setBeanProperties(new BeanPropertiesImpl());
 	}
+
+	protected void setUpDDMFormFieldTypeRegistryUtil() {
+		when(
+			_ddmFormFieldTypeRegistry.getDDMFormFieldTypeNames()
+		).thenReturn(
+			new HashSet<String>(
+				Arrays.asList(
+					new String[] {
+						DDMFormFieldType.TEXT, DDMFormFieldType.SELECT}))
+		);
+
+		DDMFormFieldTypeRegistryUtil ddmFormFieldTypeRegistryUtil =
+			new DDMFormFieldTypeRegistryUtil();
+
+		ddmFormFieldTypeRegistryUtil.setDDMFormFieldTypeRegistry(
+			_ddmFormFieldTypeRegistry);
+	}
+
+	@Mock
+	private DDMFormFieldTypeRegistry _ddmFormFieldTypeRegistry;
 
 	private final DDMFormValidator _ddmFormValidator =
 		new DDMFormValidatorImpl();
