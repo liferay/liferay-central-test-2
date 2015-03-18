dfee7fdd234b8dfa37dbc082b154a28db377c4ff
adolfo.perez <adolfo.perez@liferay.com>
2015-03-18 15:54:13 +0100

    LPS-53640 Make all portlets visible in user panel

---
+++ modules/apps/productivity-center/productivity-center-web/src/com/liferay/productivity/center/layout/UserPersonalPanelLayoutTypeAccessPolicy.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.productivity.center.layout;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.model.DefaultLayoutTypeAccessPolicyImpl;
+import com.liferay.portal.model.Layout;
+import com.liferay.portal.model.LayoutTypeAccessPolicy;
+import com.liferay.portal.model.Portlet;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(
+	immediate = true, property = {"layout.type=user_personal_panel"},
+	service = LayoutTypeAccessPolicy.class
+)
+public class UserPersonalPanelLayoutTypeAccessPolicy
+	extends DefaultLayoutTypeAccessPolicyImpl {
+
+	@Override
+	public void checkAccessAllowedToPortlet(
+			HttpServletRequest request, Layout layout, Portlet portlet)
+		throws PortalException {
+	}
+
+}
