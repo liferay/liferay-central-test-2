2ac6523c263d3680d0189e893eb6b43ec2a3cabe
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-03-18 11:12:15 +0100

    LPS-54317 Add a test portlet as an OSGi component, that will be used to test action command extension point

---
+++ portal-impl/test/integration/com/liferay/portal/kernel/portlet/bridges/mvc/bundle/actioncommandcache/TestActionCommandPortlet.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.portlet.bridges.mvc.bundle.actioncommandcache;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Manuel de la Peña
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + TestActionCommandPortlet.PORTLET_NAME,
+		"javax.portlet.init-param.action.package.prefix=" +
+			TestActionCommandPortlet.ACTION_PACKAGE_PREFIX
+	},
+	service = Portlet.class
+)
+public class TestActionCommandPortlet extends MVCPortlet {
+
+	public static final String ACTION_PACKAGE_PREFIX =
+		"com.liferay.portal.kernel.portlet.bridges.mvc.bundle." +
+			"actioncommandcache";
+
+	public static final String PORTLET_NAME =
+		"TEST_ACTION_COMMAND_PORTLET_NAME";
+
+}
