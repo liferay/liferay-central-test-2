7c747d6e60f474dbe72b7e2eb8ccc3c2785b4f72
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-03-18 11:18:03 +0100

    LPS-54317 Add a test for ActionCommand OSGi extension point
    The test will verify that the added portlet will process the added ActionCommand on action phase if present on the actionRequest.

---
+++ portal-impl/test/integration/com/liferay/portal/kernel/portlet/bridges/mvc/ActionCommandCacheTest.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.portlet.bridges.mvc;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.bundle.actioncommandcache.TestActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.bundle.actioncommandcache.TestActionCommandPortlet;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.MainServletTestRule;
+import com.liferay.portal.test.rule.SyntheticBundleRule;
+import com.liferay.registry.Filter;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceTracker;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.GenericPortlet;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.springframework.mock.web.portlet.MockActionRequest;
+import org.springframework.mock.web.portlet.MockActionResponse;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class ActionCommandCacheTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE,
+			new SyntheticBundleRule("bundle.actioncommandcache"));
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		Registry registry = RegistryUtil.getRegistry();
+
+		Filter portletFilter = registry.getFilter(
+			"(&(objectClass=javax.portlet.Portlet)(javax.portlet.name=" +
+				TestActionCommandPortlet.PORTLET_NAME + "))");
+
+		_portletServiceTracker = registry.trackServices(portletFilter);
+
+		_portletServiceTracker.open();
+
+		_portlet = _portletServiceTracker.getService();
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		_portletServiceTracker.close();
+	}
+
+	@Test
+	public void testProcessAction() throws Exception {
+		MockActionRequest actionRequest = new MockActionRequest();
+
+		actionRequest.addParameter(
+			ActionRequest.ACTION_NAME,
+			TestActionCommand.TEST_ACTION_COMMAND_NAME);
+
+		_portlet.processAction(actionRequest, new MockActionResponse());
+
+		Assert.assertNotNull(actionRequest.getAttribute("TEST_ACTION_COMMAND"));
+		Assert.assertEquals(
+			"TEST_ACTION_COMMAND",
+			actionRequest.getAttribute("TEST_ACTION_COMMAND"));
+	}
+
+	private static GenericPortlet _portlet;
+	private static ServiceTracker<GenericPortlet, GenericPortlet>
+		_portletServiceTracker;
+
+}
