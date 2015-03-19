53eda1d954d506d2eca18d054729e8427869d1c3
shuyangzhou <shuyang.zhou@liferay.com>
2015-03-19 15:34:04 -0700

    LPS-54402 When the real stuff is more handy, why mock at all.

--- portal-impl/test/unit/com/liferay/portal/servlet/filters/strip/StripFilterTest.java
+++ portal-impl/test/unit/com/liferay/portal/servlet/filters/strip/StripFilterTest.java
@@ -30,22 +30,23 @@ import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import org.mockito.Mockito;
-
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
  * @author Shuyang Zhou
  * @author Miguel Pastor
  */
-@PrepareForTest({CacheKeyGeneratorUtil.class})
-@RunWith(PowerMockRunner.class)
-public class StripFilterTest extends PowerMockito {
+public class StripFilterTest {
+
+	@BeforeClass
+	public static void setUpClass() {
+		CacheKeyGeneratorUtil cacheKeyGeneratorUtil =
+			new CacheKeyGeneratorUtil();
+
+		cacheKeyGeneratorUtil.setDefaultCacheKeyGenerator(
+			new HashCodeCacheKeyGenerator());
+	}
 
 	@Test
 	public void testHasMarker() {
@@ -91,8 +92,6 @@ public class StripFilterTest extends PowerMockito {
 	@Test
 	public void testProcessCSS() throws Exception {
 		StripFilter stripFilter = new StripFilter();
-
-		_mockCacheGenerationUtil();
 
 		// Missing close tag
 
@@ -171,15 +170,11 @@ public class StripFilterTest extends PowerMockito {
 			"style type=\"text/css\">" + minifiedCode + "</style> ",
 			stringWriter.toString());
 		Assert.assertEquals(code.length() + 34, charBuffer.position());
-
-		verifyStatic(Mockito.times(3));
 	}
 
 	@Test
 	public void testProcessJavaScript() throws Exception {
 		StripFilter stripFilter = new StripFilter();
-
-		_mockCacheGenerationUtil();
 
 		// Missing close tag
 
@@ -295,8 +290,6 @@ public class StripFilterTest extends PowerMockito {
 		}
 
 		Assert.assertEquals(code.length() + 20, charBuffer.position());
-
-		verifyStatic(Mockito.times(5));
 	}
 
 	@Test
@@ -469,17 +462,6 @@ public class StripFilterTest extends PowerMockito {
 
 		Assert.assertEquals(" ", stringWriter.toString());
 		Assert.assertEquals(4, charBuffer.position());
-	}
-
-	private void _mockCacheGenerationUtil() {
-		mockStatic(CacheKeyGeneratorUtil.class);
-
-		when(
-			CacheKeyGeneratorUtil.getCacheKeyGenerator(
-				StripFilter.class.getName())
-		).thenReturn(
-			new HashCodeCacheKeyGenerator()
-		);
 	}
 
 }
