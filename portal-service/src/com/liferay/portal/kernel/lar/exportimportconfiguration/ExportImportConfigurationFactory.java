7b82b5bef127bf443d284f286e6354a1718bd7b4
Daniel Kocsis <daniel.kocsis@liferay.com>
2015-03-19 11:21:01 +0100

    LPS-54340 Add default parmeters to always export/import deletions and additions

--- portal-service/src/com/liferay/portal/kernel/lar/exportimportconfiguration/ExportImportConfigurationFactory.java
+++ portal-service/src/com/liferay/portal/kernel/lar/exportimportconfiguration/ExportImportConfigurationFactory.java
@@ -149,6 +149,9 @@ public class ExportImportConfigurationFactory {
 		Map<String, String[]> parameterMap) {
 
 		parameterMap.put(
+			PortletDataHandlerKeys.DELETIONS,
+			new String[] {Boolean.TRUE.toString()});
+		parameterMap.put(
 			PortletDataHandlerKeys.IGNORE_LAST_PUBLISH_DATE,
 			new String[] {Boolean.FALSE.toString()});
 		parameterMap.put(
@@ -156,6 +159,12 @@ public class ExportImportConfigurationFactory {
 			new String[] {Boolean.TRUE.toString()});
 		parameterMap.put(
 			PortletDataHandlerKeys.LOGO,
+			new String[] {Boolean.TRUE.toString()});
+		parameterMap.put(
+			PortletDataHandlerKeys.PORTLET_DATA,
+			new String[] {Boolean.TRUE.toString()});
+		parameterMap.put(
+			PortletDataHandlerKeys.PORTLET_DATA_ALL,
 			new String[] {Boolean.TRUE.toString()});
 		parameterMap.put(
 			ExportImportDateUtil.RANGE,
