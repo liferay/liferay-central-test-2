58c6f0bbdee0f9f5021984e56e3089d082efe7e7
joanhyokim <joan.kim@liferay.com>
2015-03-20 10:13:47 -0700

    LPS-54169 Update StartupAction

--- modules/apps/marketplace/marketplace-web/docroot/WEB-INF/src/com/liferay/marketplace/web/events/StartupAction.java
+++ modules/apps/marketplace/marketplace-web/docroot/WEB-INF/src/com/liferay/marketplace/web/events/StartupAction.java
@@ -17,17 +17,30 @@ package com.liferay.marketplace.web.events;
 import com.liferay.portal.kernel.events.ActionException;
 import com.liferay.portal.kernel.events.SimpleAction;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.model.Company;
 import com.liferay.portal.model.User;
+import com.liferay.portal.security.auth.CompanyThreadLocal;
+import com.liferay.portal.service.CompanyLocalService;
 import com.liferay.portlet.expando.DuplicateColumnNameException;
 import com.liferay.portlet.expando.DuplicateTableNameException;
 import com.liferay.portlet.expando.model.ExpandoColumnConstants;
 import com.liferay.portlet.expando.model.ExpandoTable;
-import com.liferay.portlet.expando.service.ExpandoColumnLocalServiceUtil;
-import com.liferay.portlet.expando.service.ExpandoTableLocalServiceUtil;
+import com.liferay.portlet.expando.service.ExpandoColumnLocalService;
+import com.liferay.portlet.expando.service.ExpandoTableLocalService;
+
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Peter Shin
+ * @author Joan Kim
  */
+@Component(immediate = true)
 public class StartupAction extends SimpleAction {
 
 	@Override
@@ -40,24 +53,75 @@ public class StartupAction extends SimpleAction {
 		}
 	}
 
+	@Reference(unbind = "-")
+	public void setCompanyLocalService(
+		CompanyLocalService companyLocalService) {
+
+		_companyLocalService = companyLocalService;
+	}
+
 	protected void doRun(long companyId) throws Exception {
+		setupExpando(companyId);
+	}
+
+	@Activate
+	protected void run() throws ActionException {
+		Long companyId = CompanyThreadLocal.getCompanyId();
+
+		try {
+			List<Company> companys = _companyLocalService.getCompanies();
+
+			for (Company company : companys) {
+				CompanyThreadLocal.setCompanyId(company.getCompanyId());
+
+				run(new String[] {String.valueOf(company.getCompanyId())});
+			}
+		}
+		finally {
+			CompanyThreadLocal.setCompanyId(companyId);
+		}
+	}
+
+	@Reference(unbind = "-")
+	protected void setExpandoColumnLocalService(
+		ExpandoColumnLocalService expandoColumnLocalService) {
+
+		_expandoColumnLocalService = expandoColumnLocalService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setExpandoTableLocalService(
+		ExpandoTableLocalService expandoTableLocalService) {
+
+		_expandoTableLocalService = expandoTableLocalService;
+	}
+
+	@Reference(target = "(original.bean=true)", unbind = "-")
+	protected void setServletContext(ServletContext servletContext) {
+	}
+
+	protected void setupExpando(long companyId) throws Exception {
 		ExpandoTable table = null;
 
 		try {
-			table = ExpandoTableLocalServiceUtil.addTable(
+			table = _expandoTableLocalService.addTable(
 				companyId, User.class.getName(), "MP");
 		}
 		catch (DuplicateTableNameException dtne) {
-			table = ExpandoTableLocalServiceUtil.getTable(
+			table = _expandoTableLocalService.getTable(
 				companyId, User.class.getName(), "MP");
 		}
 
 		try {
-			ExpandoColumnLocalServiceUtil.addColumn(
-				table.getTableId(), "clientId", ExpandoColumnConstants.STRING);
+			_expandoColumnLocalService.addColumn(
+				table.getTableId(), "clientID", ExpandoColumnConstants.STRING);
 		}
 		catch (DuplicateColumnNameException dcne) {
 		}
 	}
+
+	private CompanyLocalService _companyLocalService;
+	private ExpandoColumnLocalService _expandoColumnLocalService;
+	private ExpandoTableLocalService _expandoTableLocalService;
 
 }
