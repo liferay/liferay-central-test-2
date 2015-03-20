5b3a45c7a1bef24526e2cdc53908c8c947ac8b65
joanhyokim <joan.kim@liferay.com>
2015-03-20 10:13:01 -0700

    LPS-54169 Move StartupAction

--- modules/apps/marketplace/marketplace-web/docroot/WEB-INF/src/com/liferay/marketplace/web/events/StartupAction.java
+++ modules/apps/marketplace/marketplace-web/docroot/WEB-INF/src/com/liferay/marketplace/web/events/StartupAction.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.marketplace.hook.events;
+package com.liferay.marketplace.web.events;
 
 import com.liferay.portal.kernel.events.ActionException;
 import com.liferay.portal.kernel.events.SimpleAction;
