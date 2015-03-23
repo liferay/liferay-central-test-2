5d6927779891d6a00e98eb23582c933fbc56aebe
peterfellwock <peter.fellwock@liferay.com>
2015-03-23 14:01:33 -0700

    LPS-54480 - Filter requires servletContext from bundle to Aggregate js

--- portal-impl/src/com/liferay/portal/servlet/filters/aggregate/AggregateFilter.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/aggregate/AggregateFilter.java
@@ -35,6 +35,7 @@ import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.web.PortalWebResourcesUtil;
 import com.liferay.portal.minifier.MinifierUtil;
 import com.liferay.portal.servlet.filters.IgnoreModuleRequestFilter;
 import com.liferay.portal.servlet.filters.dynamiccss.DynamicCSSUtil;
@@ -260,7 +261,10 @@ public class AggregateFilter extends IgnoreModuleRequestFilter {
 		String bundleDirName = PropsUtil.get(
 			PropsKeys.JAVASCRIPT_BUNDLE_DIR, new Filter(bundleId));
 
-		URL bundleDirURL = _servletContext.getResource(bundleDirName);
+		ServletContext resourceServeletContext =
+				PortalWebResourcesUtil.getServletContext();
+
+		URL bundleDirURL = resourceServeletContext.getResource(bundleDirName);
 
 		if (bundleDirURL == null) {
 			return null;
@@ -276,7 +280,7 @@ public class AggregateFilter extends IgnoreModuleRequestFilter {
 			boolean staleCache = false;
 
 			for (String fileName : fileNames) {
-				URL resourceURL = _servletContext.getResource(
+				URL resourceURL = resourceServeletContext.getResource(
 					bundleDirName.concat(StringPool.SLASH).concat(fileName));
 
 				if (resourceURL == null) {
@@ -312,7 +316,8 @@ public class AggregateFilter extends IgnoreModuleRequestFilter {
 		}
 		else {
 			content = aggregateJavaScript(
-				new ServletPaths(_servletContext, bundleDirName), fileNames);
+				new ServletPaths(resourceServeletContext, bundleDirName),
+				fileNames);
 		}
 
 		response.setContentType(ContentTypes.TEXT_JAVASCRIPT);
@@ -512,6 +517,20 @@ public class AggregateFilter extends IgnoreModuleRequestFilter {
 
 		return getJavaScriptContent(resourceURL.toString(), content);
 	}
+
+	@Override
+	protected boolean isModuleRequest(HttpServletRequest request) {
+		String requestURI = request.getRequestURI();
+
+		String frontEndContextPath = PortalWebResourcesUtil.getContextPath();
+
+		if(requestURI.startsWith(frontEndContextPath)){
+			return false;
+		}
+
+		return super.isModuleRequest(request);
+	}
+
 
 	@Override
 	protected void processFilter(
