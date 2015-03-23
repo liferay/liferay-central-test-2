rotty3000/liferay-portal

LPS-53578 Encapsulate preferencesIds logic

portal-service/src/com/liferay/portlet/PortletPreferencesFactory.java
@@ -103,6 +103,10 @@ public interface PortletPreferencesFactory {
 			boolean modeEditGuest)
 		throws PortalException;
 
+	public PortletPreferencesIds getPortletPreferencesIds(
+		long companyId, long siteGroupId, long plid, String portletId,
+		String scope);
+
 	public PortletPreferences getPortletSetup(
 			HttpServletRequest request, String portletId)
 		throws PortalException;

portal-service/src/com/liferay/portlet/PortletPreferencesFactoryUtil.java
@@ -173,6 +173,14 @@ public class PortletPreferencesFactoryUtil {
 			scopeGroupId, userId, layout, portletId, modeEditGuest);
 	}
 
+	public static PortletPreferencesIds getPortletPreferencesIds(
+		long companyId, long siteGroupId, long plid, String portletId,
+		String scope) {
+
+		return getPortletPreferencesFactory().getPortletPreferencesIds(
+			companyId, siteGroupId, plid, portletId, scope);
+	}
+
 	public static PortletPreferences getPortletSetup(
 			HttpServletRequest request, String portletId)
 		throws PortalException {

liferay/liferay-central-test-2

portal-service/src/com/liferay/portlet/PortletPreferencesFactory.java
@@ -103,6 +103,10 @@ public interface PortletPreferencesFactory {
 			boolean modeEditGuest)
 		throws PortalException;
 
+	public PortletPreferencesIds getPortletPreferencesIds(
+		long companyId, long siteGroupId, long plid, String portletId,
+		String settingsScope);
+
 	public PortletPreferences getPortletSetup(
 			HttpServletRequest request, String portletId)
 		throws PortalException;

portal-service/src/com/liferay/portlet/PortletPreferencesFactoryUtil.java
@@ -173,6 +173,14 @@ public class PortletPreferencesFactoryUtil {
 			scopeGroupId, userId, layout, portletId, modeEditGuest);
 	}
 
+	public static PortletPreferencesIds getPortletPreferencesIds(
+		long companyId, long siteGroupId, long plid, String portletId,
+		String settingsScope) {
+
+		return getPortletPreferencesFactory().getPortletPreferencesIds(
+			companyId, siteGroupId, plid, portletId, settingsScope);
+	}
+
 	public static PortletPreferences getPortletSetup(
 			HttpServletRequest request, String portletId)
 		throws PortalException {