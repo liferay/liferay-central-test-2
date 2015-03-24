7e2b22b36aaab48e58d5271f1e460e5257c3e843
IstvanD <istvan.dezsi@liferay.com>
2015-03-24 12:59:24 +0100

    LPS-53267 Input_localized field doesn't gets initialized with the users language ID

--- portal-web/docroot/html/js/liferay/input_localized.js
+++ portal-web/docroot/html/js/liferay/input_localized.js
@@ -557,7 +557,7 @@ AUI.add(
 
 					var currentTarget = event.currentTarget;
 
-					InputLocalized._initializeInputLocalized(event, currentTarget);
+					InputLocalized._initializeInputLocalized(event, currentTarget, userLanguageId);
 				},
 
 				_registerConfiguration: function(id, config) {
