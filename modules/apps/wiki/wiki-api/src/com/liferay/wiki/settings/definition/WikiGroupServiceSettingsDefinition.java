c80cbb8c7386f4f70b0a65a9e6df168a7d64133c
Iván Zaera Avellón <ivan.zaera@liferay.com>
2015-03-24 16:32:52 +0100

    LPS-54497 Generics

--- modules/apps/wiki/wiki-api/src/com/liferay/wiki/settings/definition/WikiGroupServiceSettingsDefinition.java
+++ modules/apps/wiki/wiki-api/src/com/liferay/wiki/settings/definition/WikiGroupServiceSettingsDefinition.java
@@ -25,10 +25,12 @@ import org.osgi.service.component.annotations.Component;
  * @author Iván Zaera
  */
 @Component(immediate = true)
-public class WikiGroupServiceSettingsDefinition implements SettingsDefinition {
+public class WikiGroupServiceSettingsDefinition
+	implements SettingsDefinition<WikiGroupServiceSettings,
+		WikiGroupServiceConfiguration> {
 
 	@Override
-	public Class<?> getConfigurationBeanClass() {
+	public Class<WikiGroupServiceConfiguration> getConfigurationBeanClass() {
 		return WikiGroupServiceConfiguration.class;
 	}
 
