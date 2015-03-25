0ff73939a6d11e3f66419a25ceedf2aa2e144c5d
Michael Han <michael.han@liferay.com>
2015-03-25 07:24:10 -0500

    LPS-54436 add new test rule to initialize PortalExecutorManager

--- portal-test/src/com/liferay/portal/kernel/test/rule/AggregateTestRule.java
+++ portal-test/src/com/liferay/portal/kernel/test/rule/AggregateTestRule.java
@@ -61,6 +61,7 @@ public class AggregateTestRule implements TestRule {
 	private static final String[] _ORDERED_RULE_CLASS_NAMES = new String[] {
 		HeapDumpTestRule.class.getName(), CodeCoverageAssertor.class.getName(),
 		NewEnvTestRule.class.getName(),
+		"com.liferay.portal.test.rule.PortalExecutorManagerTestRule",
 		"com.liferay.portal.test.rule.LiferayIntegrationTestRule",
 		"com.liferay.portal.test.rule.MainServletTestRule",
 		"com.liferay.portal.test.rule.PersistenceTestRule",
