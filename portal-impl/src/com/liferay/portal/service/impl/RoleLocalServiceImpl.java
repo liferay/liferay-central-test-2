jonathanmccann/liferay-portal

LPS-53865 Use UserPermissionCheckerBag to avoid complex RoleFinder join

portal-impl/src/com/liferay/portal/service/impl/RoleLocalServiceImpl.java
@@ -58,6 +58,7 @@ import com.liferay.portal.model.User;
 import com.liferay.portal.security.auth.CompanyThreadLocal;
 import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.security.permission.PermissionCacheUtil;
+import com.liferay.portal.security.permission.UserPermissionCheckerBag;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.service.base.RoleLocalServiceBaseImpl;
 import com.liferay.portal.util.PortalUtil;
@@ -1019,27 +1020,34 @@ public class RoleLocalServiceImpl extends RoleLocalServiceBaseImpl {
 				return true;
 			}
 
-			ThreadLocalCache<Integer> threadLocalCache =
+			ThreadLocalCache<Boolean> threadLocalCache =
 				ThreadLocalCacheManager.getThreadLocalCache(
 					Lifecycle.REQUEST, RoleLocalServiceImpl.class.getName());
 
 			String key = String.valueOf(role.getRoleId()).concat(
 				String.valueOf(userId));
 
-			Integer value = threadLocalCache.get(key);
+			Boolean value = threadLocalCache.get(key);
 
-			if (value == null) {
-				value = roleFinder.countByR_U(role.getRoleId(), userId);
-
-				threadLocalCache.put(key, value);
+			if (value != null) {
+				return value;
 			}
 
-			if (value > 0) {
-				return true;
+			UserPermissionCheckerBag userPermissionCheckerBag =
+				PermissionCacheUtil.getUserBag(userId);
+
+			if (userPermissionCheckerBag != null) {
+				value = userPermissionCheckerBag.hasUserRole(role);
 			}
 			else {
-				return false;
+				int count = roleFinder.countByR_U(role.getRoleId(), userId);
+
+				value = (count > 0);
 			}
+
+			threadLocalCache.put(key, value);
+
+			return value;
 		}
 		else {
 			return userPersistence.containsRole(userId, role.getRoleId());

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portal/service/impl/RoleLocalServiceImpl.java
@@ -58,6 +58,7 @@ import com.liferay.portal.model.User;
 import com.liferay.portal.security.auth.CompanyThreadLocal;
 import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.security.permission.PermissionCacheUtil;
+import com.liferay.portal.security.permission.UserPermissionCheckerBag;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.service.base.RoleLocalServiceBaseImpl;
 import com.liferay.portal.util.PortalUtil;
@@ -1016,27 +1017,34 @@ public class RoleLocalServiceImpl extends RoleLocalServiceBaseImpl {
 				return true;
 			}
 
-			ThreadLocalCache<Integer> threadLocalCache =
+			ThreadLocalCache<Boolean> threadLocalCache =
 				ThreadLocalCacheManager.getThreadLocalCache(
 					Lifecycle.REQUEST, RoleLocalServiceImpl.class.getName());
 
 			String key = String.valueOf(role.getRoleId()).concat(
 				String.valueOf(userId));
 
-			Integer value = threadLocalCache.get(key);
+			Boolean value = threadLocalCache.get(key);
 
-			if (value == null) {
-				value = roleFinder.countByR_U(role.getRoleId(), userId);
-
-				threadLocalCache.put(key, value);
+			if (value != null) {
+				return value;
 			}
 
-			if (value > 0) {
-				return true;
+			UserPermissionCheckerBag userPermissionCheckerBag =
+				PermissionCacheUtil.getUserBag(userId);
+
+			if (userPermissionCheckerBag != null) {
+				value = userPermissionCheckerBag.hasRole(role);
 			}
 			else {
-				return false;
+				int count = roleFinder.countByR_U(role.getRoleId(), userId);
+
+				value = (count > 0);
 			}
+
+			threadLocalCache.put(key, value);
+
+			return value;
 		}
 		else {
 			return userPersistence.containsRole(userId, role.getRoleId());