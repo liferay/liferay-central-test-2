7f4ae8855034ea31f0861f27feb7bf67b000331e
Iván Zaera Avellón <ivan.zaera@liferay.com>
2015-03-25 11:01:21 +0100

    LPS-52381 Make attachment browser work from Admin->Content too

--- portal-web/docroot/html/js/editor/ckeditor_diffs/plugins/lfrpopup/plugin.js
+++ portal-web/docroot/html/js/editor/ckeditor_diffs/plugins/lfrpopup/plugin.js
@@ -4,7 +4,7 @@ AUI().use(
 	'querystring-parse',
 	function(A) {
 		CKEDITOR.editor.prototype.popup = function(url, width, height, options) {
-            var params = A.QueryString.parse(url);
+            var params = A.QueryString.parse(url.split("?")[1]);
 
             if (params.p_p_id) {
                 url = url.replace("CKEditorFuncNum=", "_" + params.p_p_id + "_CKEditorFuncNum=");
