d54ccafc48009bf5574f513ec9bcd0986241bc59
Tamas Molnar <tamas.molnar@liferay.com>
2015-03-26 15:02:22 +0100

    LPS-54559 IFrame portlet - preference name changes aren't handled by the upgradeprocess

--- portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_6_1_0.java
+++ portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_6_1_0.java
@@ -25,6 +25,7 @@ import com.liferay.portal.upgrade.v6_1_0.UpgradeCountry;
 import com.liferay.portal.upgrade.v6_1_0.UpgradeDocumentLibrary;
 import com.liferay.portal.upgrade.v6_1_0.UpgradeExpando;
 import com.liferay.portal.upgrade.v6_1_0.UpgradeGroup;
+import com.liferay.portal.upgrade.v6_1_0.UpgradeIFrame;
 import com.liferay.portal.upgrade.v6_1_0.UpgradeImageGallery;
 import com.liferay.portal.upgrade.v6_1_0.UpgradeJournal;
 import com.liferay.portal.upgrade.v6_1_0.UpgradeLayout;
@@ -66,6 +67,7 @@ public class UpgradeProcess_6_1_0 extends UpgradeProcess {
 		upgrade(UpgradeExpando.class);
 		upgrade(UpgradeGroup.class);
 		upgrade(UpgradeImageGallery.class);
+		upgrade(UpgradeIFrame.class);
 		upgrade(UpgradeJournal.class);
 		upgrade(UpgradeLayout.class);
 		upgrade(UpgradeLock.class);
