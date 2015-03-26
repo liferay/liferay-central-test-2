e754b0316dd20e4f7eaf798b636b3a233bf558eb
Michael Hashimoto <michael.hashimoto@liferay.com>
2015-03-26 10:08:22 -0700

    LRQA-13571 - Fix compilation error, because I started throwing regular Exceptions

--- modules/test/poshi-runner/src/com/liferay/poshi/runner/PoshiRunner.java
+++ modules/test/poshi-runner/src/com/liferay/poshi/runner/PoshiRunner.java
@@ -37,7 +37,7 @@ import org.junit.runners.Parameterized.Parameters;
 public class PoshiRunner {
 
 	@Parameters(name = "{0}")
-	public static List<String> getList() throws PoshiRunnerException {
+	public static List<String> getList() throws Exception {
 		List<String> classCommandNames = new ArrayList<>();
 
 		String testName = PropsValues.TEST_NAME;
@@ -62,7 +62,7 @@ public class PoshiRunner {
 		return classCommandNames;
 	}
 
-	public PoshiRunner(String classCommandName) throws PoshiRunnerException {
+	public PoshiRunner(String classCommandName) throws Exception {
 		LoggerUtil.startLogger();
 
 		SeleniumUtil.startSelenium();
@@ -87,7 +87,7 @@ public class PoshiRunner {
 	}
 
 	private void _runClassCommandName(String classCommandName)
-		throws PoshiRunnerException {
+		throws Exception {
 
 		Element rootElement = PoshiRunnerContext.getTestcaseRootElement(
 			_testClassName);
@@ -116,15 +116,15 @@ public class PoshiRunner {
 		}
 	}
 
-	private void _runCommand() throws PoshiRunnerException {
+	private void _runCommand() throws Exception {
 		_runClassCommandName(_testClassCommandName);
 	}
 
-	private void _runSetUp() throws PoshiRunnerException {
+	private void _runSetUp() throws Exception {
 		_runClassCommandName(_testClassName + "#set-up");
 	}
 
-	private void _runTearDown() throws PoshiRunnerException {
+	private void _runTearDown() throws Exception {
 		try {
 			_runClassCommandName(_testClassName + "#tear-down");
 		}
