c661d582f22b7e0086b1535ea200eb58b3379a90
Michael Hashimoto <michael.hashimoto@liferay.com>
2015-03-26 10:10:32 -0700

    LRQA-13571 - Add a class called 'LoggerElement' that will be used to represent HTML elements within the logger

---
+++ modules/test/poshi-runner/src/com/liferay/poshi/runner/logger/LoggerElement.java
@@ -0,0 +1,127 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.poshi.runner.logger;
+
+import com.liferay.poshi.runner.util.Validator;
+
+import java.text.SimpleDateFormat;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Michael Hashimoto
+ */
+public class LoggerElement {
+
+	public LoggerElement() {
+		Date date = new Date();
+
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+			"yyyyMMddHHmmssSSS");
+
+		_id = simpleDateFormat.format(date);
+	}
+
+	public LoggerElement(String id) {
+		_id = id;
+	}
+
+	public void addChildLoggerElement(LoggerElement childLoggerElement) {
+		_childLoggerElements.add(childLoggerElement);
+	}
+
+	public String getClass() {
+		return _class;
+	}
+
+	public String getID() {
+		return _id;
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public String getText() {
+		return _text;
+	}
+
+	public void setClass(String class) {
+		_class = class;
+	}
+
+	public void setID(String id) {
+		_id = id;
+	}
+
+	public void setName(String name) {
+		_name = name;
+	}
+
+	public void setText(String text) {
+		_text = text;
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("<");
+		sb.append(_name);
+		sb.append(" id=\"");
+		sb.append(_id);
+		sb.append("\"");
+
+		if (Validator.isNotNull(_class)) {
+			sb.append(" class=\"");
+			sb.append(_class);
+			sb.append("\"");
+		}
+
+		boolean hasChildren = _childLoggerElements.size() > 0;
+		boolean hasText = Validator.isNotNull(_text);
+
+		if (hasChildren || hasText) {
+			sb.append(">\n");
+
+			if (hasText) {
+				sb.append(_text);
+			}
+
+			if (hasChildren) {
+				for (LoggerElement childLoggerElement : _childLoggerElements) {
+					sb.append(childLoggerElement.toString());
+				}
+			}
+
+			sb.append("</");
+			sb.append(_name);
+			sb.append(">\n");
+		}
+		else {
+			sb.append(" />\n");
+		}
+
+		return sb.toString();
+	}
+
+	private final List<LoggerElement> _childLoggerElements = new ArrayList<>();
+	private String _class = "";
+	private String _id;
+	private String _name = "div";
+	private String _text = "";
+
+}
