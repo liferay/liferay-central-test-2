67d704074d2cd497abb715c0e14c01a306eb3e3f
Michael Han <michael.han@liferay.com>
2015-03-27 10:10:17 -0500

    LPS-54427 remove static initializer for address detection

--- modules/portal/portal-cluster/src/com/liferay/portal/cluster/internal/jgroups/JGroupsClusterChannel.java
+++ modules/portal/portal-cluster/src/com/liferay/portal/cluster/internal/jgroups/JGroupsClusterChannel.java
@@ -20,17 +20,11 @@ import com.liferay.portal.kernel.cluster.Address;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.CharPool;
-import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.SocketUtil;
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.util.PropsValues;
 
-import java.io.IOException;
 import java.io.Serializable;
 
 import java.net.InetAddress;
-import java.net.NetworkInterface;
 
 import org.jgroups.JChannel;
 import org.jgroups.protocols.TP;
@@ -165,86 +159,8 @@ public class JGroupsClusterChannel implements ClusterChannel {
 		}
 	}
 
-	private static void _initBindAddress() {
-		String autodetectAddress = PropsValues.CLUSTER_LINK_AUTODETECT_ADDRESS;
-
-		if (Validator.isNull(autodetectAddress)) {
-			return;
-		}
-
-		String host = autodetectAddress;
-		int port = 80;
-
-		int index = autodetectAddress.indexOf(CharPool.COLON);
-
-		if (index != -1) {
-			host = autodetectAddress.substring(0, index);
-			port = GetterUtil.getInteger(
-				autodetectAddress.substring(index + 1), port);
-		}
-
-		if (_log.isInfoEnabled()) {
-			_log.info(
-				"Autodetecting JGroups outgoing IP address and interface for " +
-					host + ":" + port);
-		}
-
-		try {
-			SocketUtil.BindInfo bindInfo = SocketUtil.getBindInfo(host, port);
-
-			InetAddress inetAddress = bindInfo.getInetAddress();
-
-			NetworkInterface networkInterface = bindInfo.getNetworkInterface();
-
-			System.setProperty(
-				"jgroups.bind_addr", inetAddress.getHostAddress());
-			System.setProperty(
-				"jgroups.bind_interface", networkInterface.getName());
-
-			if (_log.isInfoEnabled()) {
-				_log.info(
-					"Setting JGroups outgoing IP address to " +
-						inetAddress.getHostAddress() + " and interface to " +
-							networkInterface.getName());
-			}
-		}
-		catch (IOException e) {
-			if (_log.isErrorEnabled()) {
-				_log.error("Unable to detect bind address for jgroups", e);
-			}
-		}
-	}
-
-	private static void _initSystemProperties() {
-		for (String systemProperty :
-				PropsValues.CLUSTER_LINK_CHANNEL_SYSTEM_PROPERTIES) {
-
-			int index = systemProperty.indexOf(CharPool.COLON);
-
-			if (index == -1) {
-				continue;
-			}
-
-			String key = systemProperty.substring(0, index);
-			String value = systemProperty.substring(index + 1);
-
-			System.setProperty(key, value);
-
-			if (_log.isDebugEnabled()) {
-				_log.debug(
-					"Setting system property {key=" + key + ", value=" + value +
-						"}");
-			}
-		}
-	}
-
 	private static final Log _log = LogFactoryUtil.getLog(
 		JGroupsClusterChannel.class);
-
-	static {
-		_initSystemProperties();
-		_initBindAddress();
-	}
 
 	private final String _clusterName;
 	private final JChannel _jChannel;
--- modules/portal/portal-cluster/src/com/liferay/portal/cluster/internal/jgroups/JGroupsClusterChannelFactory.java
+++ modules/portal/portal-cluster/src/com/liferay/portal/cluster/internal/jgroups/JGroupsClusterChannelFactory.java
@@ -17,15 +17,38 @@ package com.liferay.portal.cluster.internal.jgroups;
 import com.liferay.portal.cluster.ClusterChannel;
 import com.liferay.portal.cluster.ClusterChannelFactory;
 import com.liferay.portal.cluster.ClusterReceiver;
+import com.liferay.portal.cluster.internal.constants.ClusterPropsKeys;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Props;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.SocketUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 
+import java.io.IOException;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Tina Tian
  */
 @Component(
-	configurationPid = "com.liferay.portal.cluster.configuration.ClusterLinkConfiguration",
-	immediate = true, service = ClusterChannelFactory.class
+	immediate = true,
+	property = {
+		"autodetect.address=www.google.com:80"
+	},
+	service = ClusterChannelFactory.class
 )
 public class JGroupsClusterChannelFactory implements ClusterChannelFactory {
 
@@ -37,5 +60,113 @@ public class JGroupsClusterChannelFactory implements ClusterChannelFactory {
 		return new JGroupsClusterChannel(
 			channelProperties, clusterName, clusterReceiver);
 	}
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		String channelSystemProperties = GetterUtil.getString(
+			properties.get(ClusterPropsKeys.CHANNEL_SYSTEM_PROPERTIES));
+
+		if (Validator.isNull(channelSystemProperties)) {
+			channelSystemProperties = GetterUtil.getString(
+				_props.get(PropsKeys.CLUSTER_LINK_CHANNEL_SYSTEM_PROPERTIES));
+		}
+
+		initSystemProperties(channelSystemProperties);
+
+		String autodetectAddress = GetterUtil.getString(
+			properties.get(ClusterPropsKeys.AUTO_DETECT_ADDRESS));
+
+		if (Validator.isNull(autodetectAddress)) {
+			autodetectAddress = GetterUtil.getString(
+				_props.get(PropsKeys.CLUSTER_LINK_AUTODETECT_ADDRESS));
+		}
+
+		initBindAddress(autodetectAddress);
+	}
+
+	protected void initBindAddress(String autodetectAddress) {
+		if (Validator.isNull(autodetectAddress)) {
+			return;
+		}
+
+		String host = autodetectAddress;
+		int port = 80;
+
+		int index = autodetectAddress.indexOf(CharPool.COLON);
+
+		if (index != -1) {
+			host = autodetectAddress.substring(0, index);
+			port = GetterUtil.getInteger(
+				autodetectAddress.substring(index + 1), port);
+		}
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				"Autodetecting JGroups outgoing IP address and interface for " +
+					host + ":" + port);
+		}
+
+		try {
+			SocketUtil.BindInfo bindInfo = SocketUtil.getBindInfo(host, port);
+
+			InetAddress inetAddress = bindInfo.getInetAddress();
+
+			NetworkInterface networkInterface = bindInfo.getNetworkInterface();
+
+			System.setProperty(
+				"jgroups.bind_addr", inetAddress.getHostAddress());
+			System.setProperty(
+				"jgroups.bind_interface", networkInterface.getName());
+
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					"Setting JGroups outgoing IP address to " +
+						inetAddress.getHostAddress() + " and interface to " +
+							networkInterface.getName());
+			}
+		}
+		catch (IOException e) {
+			if (_log.isErrorEnabled()) {
+				_log.error("Unable to detect bind address for jgroups", e);
+			}
+		}
+	}
+
+	protected void initSystemProperties(String channelSystemProperties) {
+		String[] channelSystemPropertiesArray = StringUtil.split(
+			channelSystemProperties);
+
+		for (String channelSystemProperty : channelSystemPropertiesArray) {
+			int index = channelSystemProperty.indexOf(CharPool.COLON);
+
+			if (index == -1) {
+				continue;
+			}
+
+			String key = channelSystemProperty.substring(0, index);
+			String value = channelSystemProperty.substring(index + 1);
+
+			System.setProperty(key, value);
+
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					"Setting system property {key=" + key + ", value=" + value +
+						"}");
+			}
+		}
+	}
+
+	@Reference(unbind = "-")
+	protected void setProps(Props props) {
+		_props = props;
+	}
+
+	private static final String _AUTO_DETECT_ADDRESS = "autodetect.address";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		JGroupsClusterChannelFactory.class);
+
+	private Props _props;
 
 }
