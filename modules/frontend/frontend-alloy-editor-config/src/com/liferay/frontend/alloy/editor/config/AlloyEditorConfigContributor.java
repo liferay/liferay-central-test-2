a72cc8fb03efa956bf6d804105fc756982f7aaf8
Brian Chan <brian.chan@liferay.com>
2015-03-26 20:32:24 -0700

    LPS-52978 rename. Ray, Nate, and I have already been splitting stuff up into frontend in another pull. So it makes sense to put it here

--- modules/frontend/frontend-alloy-editor-config/src/com/liferay/frontend/alloy/editor/config/AlloyEditorConfigContributor.java
+++ modules/frontend/frontend-alloy-editor-config/src/com/liferay/frontend/alloy/editor/config/AlloyEditorConfigContributor.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.editor.alloyeditor.config;
+package com.liferay.frontend.alloy.editor.config;
 
 import com.liferay.portal.kernel.editor.config.EditorConfigContributor;
 import com.liferay.portal.kernel.json.JSONArray;
