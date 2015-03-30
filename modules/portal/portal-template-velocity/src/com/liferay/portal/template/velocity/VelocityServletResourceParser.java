936444af1fa7540fd7b228371637654e1bccd725
peterfellwock <peter.fellwock@liferay.com>
2015-03-30 14:32:10 -0700

    LPS-54505 - Use PortalWebResourceUtil methods to find servlet context resources

--- modules/portal/portal-template-velocity/src/com/liferay/portal/template/velocity/VelocityServletResourceParser.java
+++ modules/portal/portal-template-velocity/src/com/liferay/portal/template/velocity/VelocityServletResourceParser.java
@@ -16,6 +16,8 @@ package com.liferay.portal.template.velocity;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.PortalWebResourceConstants;
+import com.liferay.portal.kernel.servlet.PortalWebResourcesUtil;
 import com.liferay.portal.kernel.servlet.ServletContextPool;
 import com.liferay.portal.kernel.template.TemplateConstants;
 import com.liferay.portal.template.URLResourceParser;
@@ -69,13 +71,18 @@ public class VelocityServletResourceParser extends URLResourceParser {
 
 		URL url = servletContext.getResource(name);
 
+		if (url == null) {
+			url = PortalWebResourcesUtil.getServletContextResource(name);
+		}
+
 		if ((url == null) && name.endsWith("/init_custom.vm")) {
 			if (_log.isWarnEnabled()) {
 				_log.warn("The template " + name + " should be created");
 			}
 
-			ServletContext portalServletContext = ServletContextPool.get(
-				PortalUtil.getServletContextName());
+			ServletContext portalServletContext =
+				PortalWebResourcesUtil.getServletContext(
+					PortalWebResourceConstants.RESOURCE_TYPE_CSS);
 
 			url = portalServletContext.getResource(
 				"/html/themes/_unstyled/templates/init_custom.vm");
