29e1c8f70c4517b829f25b61d0526c36b0554a72
peterfellwock <peter.fellwock@liferay.com>
2015-03-30 11:15:32 -0700

    LPS-53802 AtomCollectionAdapterRegistryUtil Integration point test

---
+++ portal-impl/test/integration/com/liferay/portal/kernel/atom/AtomCollectionAdapterRegistryUtilTest.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.atom;
+
+import com.liferay.portal.kernel.atom.bundle.atomcollectionadapterregistryutil.TestAtomCollectionAdapter;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.MainServletTestRule;
+import com.liferay.portal.test.rule.SyntheticBundleRule;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Peter Fellwock
+ */
+public class AtomCollectionAdapterRegistryUtilTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE,
+			new SyntheticBundleRule(
+				"bundle.atomcollectionadapterregistryutil"));
+
+	@Test
+	public void testGetAtomCollectionAdapter() {
+		AtomCollectionAdapter<?> atomCollectionAdapter =
+			AtomCollectionAdapterRegistryUtil.getAtomCollectionAdapter(
+				TestAtomCollectionAdapter.COLLECTION_NAME);
+
+		Class<? extends AtomCollectionAdapter> clazz =
+			atomCollectionAdapter.getClass();
+
+		String className = clazz.getName();
+
+		Assert.assertEquals(
+			TestAtomCollectionAdapter.class.getName(), className);
+	}
+
+	@Test
+	public void testGetAtomCollectionAdapters() {
+		boolean found = false;
+
+		List<AtomCollectionAdapter<?>> atomCollectionAdapterList =
+			AtomCollectionAdapterRegistryUtil.getAtomCollectionAdapters();
+
+		for (AtomCollectionAdapter<?> atomCollectionAdapter :
+				atomCollectionAdapterList) {
+
+			String collectionName = atomCollectionAdapter.getCollectionName();
+
+			if (TestAtomCollectionAdapter.COLLECTION_NAME.equals(
+					collectionName)) {
+
+				found = true;
+			}
+		}
+
+		Assert.assertTrue(found);
+	}
+
+}
