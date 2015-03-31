9b901e1a7d7fcbd9ec7c60796b85b11d8bf6d931
Pablo Pinheiro <pablo.carvalho@liferay.com>
2015-03-31 17:05:35 -0300

    LPS-54677 Added ExpressionFactoryRegistry

---
+++ portal-impl/src/com/liferay/portal/kernel/expression/ExpressionFactoryRegistryImpl.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.expression;
+
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+
+/**
+ * @author Pablo Carvalho
+ */
+public class ExpressionFactoryRegistryImpl
+	implements ExpressionFactoryRegistry {
+
+	@Override
+	public ExpressionFactory getExpressionFactory() {
+		Registry registry = RegistryUtil.getRegistry();
+
+		return registry.getService(ExpressionFactory.class);
+	}
+
+}
