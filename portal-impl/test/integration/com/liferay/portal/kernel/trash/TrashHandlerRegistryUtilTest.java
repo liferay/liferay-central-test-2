de8bc0424c66f33abe5accc7f67d47bfff87bea2
peterfellwock <peter.fellwock@liferay.com>
2015-03-31 14:43:18 -0700

    LPS-53802 TrashHandlerRegistryUtil Integration point test

---
+++ portal-impl/test/integration/com/liferay/portal/kernel/trash/TrashHandlerRegistryUtilTest.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.trash;
+
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.trash.bundle.trashhandlerregistryutil.TestTrashHandler;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.MainServletTestRule;
+import com.liferay.portal.test.rule.SyntheticBundleRule;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Peter Fellwock
+ */
+public class TrashHandlerRegistryUtilTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE,
+			new SyntheticBundleRule("bundle.trashhandlerregistryutil"));
+
+	@Test
+	public void testGetTrashHandler() {
+		String testClassName = TestTrashHandler.class.getName();
+
+		TrashHandler trashHandler = TrashHandlerRegistryUtil.getTrashHandler(
+			testClassName);
+
+		Class clazz = trashHandler.getClass();
+
+		String className = clazz.getName();
+
+		Assert.assertEquals(testClassName, className);
+	}
+
+	@Test
+	public void testGetTrashHandlers() {
+		boolean found = false;
+
+		String testClassName = TestTrashHandler.class.getName();
+
+		List<TrashHandler> trashHandlers =
+			TrashHandlerRegistryUtil.getTrashHandlers();
+
+		for (TrashHandler trashHandler : trashHandlers) {
+			Class clazz = trashHandler.getClass();
+
+			String className = clazz.getName();
+
+			if (testClassName.equals(className)) {
+				found = true;
+			}
+		}
+
+		Assert.assertTrue(found);
+	}
+
+}
---
+++ portal-impl/test/integration/com/liferay/portal/kernel/trash/bundle/trashhandlerregistryutil/TestTrashHandler.java
@@ -0,0 +1,317 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.trash.bundle.trashhandlerregistryutil;
+
+import com.liferay.portal.kernel.trash.TrashHandler;
+import com.liferay.portal.kernel.trash.TrashRenderer;
+import com.liferay.portal.model.ContainerModel;
+import com.liferay.portal.model.SystemEvent;
+import com.liferay.portal.model.TrashedModel;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portlet.trash.model.TrashEntry;
+
+import java.util.List;
+import java.util.Locale;
+
+import javax.portlet.PortletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Peter Fellwock
+ */
+@Component(
+	immediate = true,
+	property = {"service.ranking:Integer=" + Integer.MAX_VALUE}
+)
+public class TestTrashHandler implements TrashHandler {
+
+	@Override
+	public SystemEvent addDeletionSystemEvent(
+		long userId, long groupId, long classPK, String classUuid,
+		String referrerClassName) {
+
+		return null;
+	}
+
+	@Override
+	public void checkDuplicateEntry(
+		long classPK, long containerModelId, String newName) {
+	}
+
+	@Override
+	public void checkDuplicateTrashEntry(
+		TrashEntry trashEntry, long containerModelId, String newName) {
+	}
+
+	@Override
+	public void checkRestorableEntry(
+		long classPK, long containerModelId, String newName) {
+	}
+
+	@Override
+	public void checkRestorableEntry(
+		TrashEntry trashEntry, long containerModelId, String newName) {
+	}
+
+	@Override
+	public void deleteTrashEntry(long classPK) {
+	}
+
+	@Override
+	public String getClassName() {
+		return TestTrashHandler.class.getName();
+	}
+
+	@Override
+	public ContainerModel getContainerModel(long containerModelId) {
+		return null;
+	}
+
+	@Override
+	public String getContainerModelClassName() {
+		return null;
+	}
+
+	@Override
+	public String getContainerModelClassName(long classPK) {
+		return null;
+	}
+
+	@Override
+	public String getContainerModelName() {
+		return null;
+	}
+
+	@Override
+	public String getContainerModelName(long classPK) {
+		return null;
+	}
+
+	@Override
+	public List<ContainerModel> getContainerModels(
+		long classPK, long containerModelId, int start, int end) {
+
+		return null;
+	}
+
+	@Override
+	public int getContainerModelsCount(long classPK, long containerModelId) {
+		return 0;
+	}
+
+	@Override
+	public String getDeleteMessage() {
+		return null;
+	}
+
+	@Override
+	public long getDestinationContainerModelId(
+		long classPK, long destinationContainerModelId) {
+
+		return 0;
+	}
+
+	@Override
+	public ContainerModel getParentContainerModel(long classPK) {
+		return null;
+	}
+
+	@Override
+	public ContainerModel getParentContainerModel(TrashedModel trashedModel) {
+		return null;
+	}
+
+	@Override
+	public List<ContainerModel> getParentContainerModels(long classPK) {
+		return null;
+	}
+
+	@Override
+	public String getRestoreContainedModelLink(
+		PortletRequest portletRequest, long classPK) {
+
+		return null;
+	}
+
+	@Override
+	public String getRestoreContainerModelLink(
+		PortletRequest portletRequest, long classPK) {
+
+		return null;
+	}
+
+	@Override
+	public String getRestoreMessage(
+		PortletRequest portletRequest, long classPK) {
+
+		return null;
+	}
+
+	@Override
+	public String getRootContainerModelClassName() {
+		return null;
+	}
+
+	@Override
+	public long getRootContainerModelId(long classPK) {
+		return 0;
+	}
+
+	@Override
+	public String getRootContainerModelName() {
+		return null;
+	}
+
+	@Override
+	public List<ContainerModel> getRootContainerModels(long groupId) {
+		return null;
+	}
+
+	@Override
+	public int getRootContainerModelsCount(long groupId) {
+		return 0;
+	}
+
+	@Override
+	public String getRootContainerModelTitle(
+		long containerModelId, Locale locale) {
+
+		return null;
+	}
+
+	@Override
+	public String getSubcontainerModelName() {
+		return null;
+	}
+
+	@Override
+	public String getSystemEventClassName() {
+		return null;
+	}
+
+	@Override
+	public String getTrashContainedModelName() {
+		return null;
+	}
+
+	@Override
+	public int getTrashContainedModelsCount(long classPK) {
+		return 0;
+	}
+
+	@Override
+	public List<TrashRenderer> getTrashContainedModelTrashRenderers(
+		long classPK, int start, int end) {
+
+		return null;
+	}
+
+	@Override
+	public String getTrashContainerModelName() {
+		return null;
+	}
+
+	@Override
+	public int getTrashContainerModelsCount(long classPK) {
+		return 0;
+	}
+
+	@Override
+	public List<TrashRenderer> getTrashContainerModelTrashRenderers(
+		long classPK, int start, int end) {
+
+		return null;
+	}
+
+	@Override
+	public TrashEntry getTrashEntry(long classPK) {
+		return null;
+	}
+
+	@Override
+	public TrashRenderer getTrashRenderer(long classPK) {
+		return null;
+	}
+
+	@Override
+	public boolean hasTrashPermission(
+		PermissionChecker permissionChecker, long groupId, long classPK,
+		String trashActionId) {
+
+		return false;
+	}
+
+	@Override
+	public boolean isContainerModel() {
+		return false;
+	}
+
+	@Override
+	public boolean isDeletable() {
+		return false;
+	}
+
+	@Override
+	public boolean isInTrash(long classPK) {
+		return false;
+	}
+
+	@Override
+	public boolean isInTrashContainer(long classPK) {
+		return false;
+	}
+
+	@Override
+	public boolean isMovable() {
+		return false;
+	}
+
+	@Override
+	public boolean isRestorable(long classPK) {
+		return false;
+	}
+
+	@Override
+	public boolean isRootContainerModelMovable() {
+		return false;
+	}
+
+	@Override
+	public void moveEntry(
+		long userId, long classPK, long containerModelId,
+		ServiceContext serviceContext) {
+	}
+
+	@Override
+	public void moveTrashEntry(
+		long userId, long classPK, long containerModelId,
+		ServiceContext serviceContext) {
+	}
+
+	@Override
+	public void restoreRelatedTrashEntry(String className, long classPK) {
+	}
+
+	@Override
+	public void restoreTrashEntry(long userId, long classPK) {
+	}
+
+	@Override
+	public void updateTitle(long classPK, String title) {
+	}
+
+}
