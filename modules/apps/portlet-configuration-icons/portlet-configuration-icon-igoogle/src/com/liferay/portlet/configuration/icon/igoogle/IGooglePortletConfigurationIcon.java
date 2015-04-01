juliocamarero/liferay-portal

LPS-54620 Creates new modules for share icons

modules/apps/portlet-configuration-icons/portlet-configuration-icon-igoogle-web/src/com/liferay/portlet/configuration/icon/igoogle/web/IGooglePortletConfigurationIcon.java
@@ -1,0 +1,105 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.configuration.icon.igoogle.web;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.configuration.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.PortletConfigurationIcon;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.model.Portlet;
+import com.liferay.portal.theme.PortletDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.PortletPreferencesFactoryUtil;
+
+import javax.portlet.PortletPreferences;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true, service = PortletConfigurationIcon.class
+)
+public class IGooglePortletConfigurationIcon
+	extends BasePortletConfigurationIcon {
+
+	@Override
+	public String getCssClass() {
+		PortletDisplay portletDisplay = _themeDisplay.getPortletDisplay();
+
+		return portletDisplay.getNamespace() + "expose-as-widget";
+	}
+
+	@Override
+	public String getIconCssClass() {
+		return "icon-plus-sign";
+	}
+
+	@Override
+	public String getMessage() {
+		return "add-to-igoogle";
+	}
+
+	@Override
+	public String getURL() {
+		try {
+			HttpServletRequest request = _themeDisplay.getRequest();
+
+			Portlet portlet = (Portlet)request.getAttribute(
+				WebKeys.RENDER_PORTLET);
+
+			return "http://fusion.google.com/add?source=atgs&moduleurl=" +
+				PortalUtil.getGoogleGadgetURL(portlet, _themeDisplay);
+		}
+		catch (PortalException pe) {
+			return StringPool.BLANK;
+		}
+	}
+
+	@Override
+	public double getWeight() {
+		return 3.0;
+	}
+
+	@Override
+	public boolean isShow() {
+		PortletDisplay portletDisplay = _themeDisplay.getPortletDisplay();
+
+		PortletPreferences portletSetup =
+			PortletPreferencesFactoryUtil.getStrictLayoutPortletSetup(
+				_themeDisplay.getLayout(), portletDisplay.getId());
+
+		boolean lfrIgoogleShowAddAppLink = GetterUtil.getBoolean(
+			portletSetup.getValue(
+				"lfrIgoogleShowAddAppLink", StringPool.BLANK));
+
+		if (lfrIgoogleShowAddAppLink) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean showLabel() {
+		return true;
+	}
+
+}

modules/apps/portlet-configuration-icons/portlet-configuration-icon-netvibes-web/src/com/liferay/portlet/configuration/icon/netvibes/web/NetvibesPortletConfigurationIcon.java
@@ -1,0 +1,119 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.configuration.icon.netvibes.web;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.configuration.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.PortletConfigurationIcon;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.model.Portlet;
+import com.liferay.portal.theme.PortletDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portlet.PortletURLFactoryUtil;
+
+import javax.portlet.PortletPreferences;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true, service = PortletConfigurationIcon.class
+)
+public class NetvibesPortletConfigurationIcon
+	extends BasePortletConfigurationIcon {
+
+	@Override
+	public String getIconCssClass() {
+		return "icon-plus-sign";
+	}
+
+	@Override
+	public String getMessage() {
+		return "add-to-netvibes";
+	}
+
+	@Override
+	public String getMethod() {
+		return "get";
+	}
+
+	@Override
+	public String getURL() {
+		try {
+			HttpServletRequest request = _themeDisplay.getRequest();
+
+			Portlet portlet = (Portlet)request.getAttribute(
+				WebKeys.RENDER_PORTLET);
+
+			PortletURL basePortletURL = PortletURLFactoryUtil.create(
+				request, PortletKeys.PORTLET_SHARING, _themeDisplay.getPlid(),
+				PortletRequest.RESOURCE_PHASE);
+
+			StringBundler sb = new StringBundler(5);
+
+			sb.append("javascript:Liferay.PortletSharing.showNetvibesInfo('");
+			sb.append(PortalUtil.getNetvibesURL(portlet, _themeDisplay));
+			sb.append("', '");
+			sb.append(basePortletURL);
+			sb.append("');");
+
+			return sb.toString();
+		}
+		catch (PortalException pe) {
+			return StringPool.BLANK;
+		}
+	}
+
+	@Override
+	public double getWeight() {
+		return 2.0;
+	}
+
+	@Override
+	public boolean isShow() {
+		PortletDisplay portletDisplay = _themeDisplay.getPortletDisplay();
+
+		PortletPreferences portletSetup =
+			PortletPreferencesFactoryUtil.getStrictLayoutPortletSetup(
+				_themeDisplay.getLayout(), portletDisplay.getId());
+
+		boolean lfrNetvibesShowAddAppLink = GetterUtil.getBoolean(
+			portletSetup.getValue(
+				"lfrNetvibesShowAddAppLink", StringPool.BLANK));
+
+		if (lfrNetvibesShowAddAppLink) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean showLabel() {
+		return true;
+	}
+
+}

modules/apps/portlet-configuration-icons/portlet-configuration-icon-widget-web/src/com/liferay/portlet/configuration/icon/widget/web/WidgetPortletConfigurationIcon.java
@@ -1,0 +1,115 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.configuration.icon.widget.web;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.configuration.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.PortletConfigurationIcon;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.model.Portlet;
+import com.liferay.portal.theme.PortletDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portal.util.PropsValues;
+import com.liferay.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portlet.PortletURLFactoryUtil;
+
+import javax.portlet.PortletPreferences;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true, service = PortletConfigurationIcon.class
+)
+public class WidgetPortletConfigurationIcon
+	extends BasePortletConfigurationIcon {
+
+	@Override
+	public String getIconCssClass() {
+		return "icon-plus-sign";
+	}
+
+	@Override
+	public String getMessage() {
+		return "add-to-any-website";
+	}
+
+	@Override
+	public String getURL() {
+		try {
+			HttpServletRequest request = _themeDisplay.getRequest();
+
+			Portlet portlet = (Portlet)request.getAttribute(
+				WebKeys.RENDER_PORTLET);
+
+			PortletURL basePortletURL = PortletURLFactoryUtil.create(
+				request, PortletKeys.PORTLET_SHARING, _themeDisplay.getPlid(),
+				PortletRequest.RESOURCE_PHASE);
+
+			StringBundler sb = new StringBundler();
+
+			sb.append("javascript:Liferay.PortletSharing.showWidgetInfo('");
+			sb.append(PortalUtil.getWidgetURL(portlet, _themeDisplay));
+			sb.append("', '");
+			sb.append(basePortletURL);
+			sb.append("');");
+
+			return sb.toString();
+		}
+		catch (PortalException pe) {
+			return StringPool.BLANK;
+		}
+	}
+
+	@Override
+	public double getWeight() {
+		return 5.0;
+	}
+
+	@Override
+	public boolean isShow() {
+		PortletDisplay portletDisplay = _themeDisplay.getPortletDisplay();
+
+		PortletPreferences portletSetup =
+			PortletPreferencesFactoryUtil.getStrictLayoutPortletSetup(
+				_themeDisplay.getLayout(), portletDisplay.getId());
+
+		boolean widgetShowAddAppLink = GetterUtil.getBoolean(
+			portletSetup.getValue("lfrWidgetShowAddAppLink", null),
+			PropsValues.THEME_PORTLET_SHARING_DEFAULT);
+
+		if (widgetShowAddAppLink) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean showLabel() {
+		return true;
+	}
+
+}

liferay/liferay-central-test-2

modules/apps/portlet-configuration-icons/portlet-configuration-icon-igoogle/src/com/liferay/portlet/configuration/icon/igoogle/IGooglePortletConfigurationIcon.java
@@ -1,0 +1,101 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.configuration.icon.igoogle;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.configuration.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.PortletConfigurationIcon;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.model.Portlet;
+import com.liferay.portal.theme.PortletDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.PortletPreferencesFactoryUtil;
+
+import javax.portlet.PortletPreferences;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true, service = PortletConfigurationIcon.class
+)
+public class IGooglePortletConfigurationIcon
+	extends BasePortletConfigurationIcon {
+
+	@Override
+	public String getCssClass() {
+		PortletDisplay portletDisplay = _themeDisplay.getPortletDisplay();
+
+		return portletDisplay.getNamespace() + "expose-as-widget";
+	}
+
+	@Override
+	public String getIconCssClass() {
+		return "icon-plus-sign";
+	}
+
+	@Override
+	public String getMessage() {
+		return "add-to-igoogle";
+	}
+
+	@Override
+	public String getURL() {
+		try {
+			Portlet portlet = (Portlet)_request.getAttribute(
+				WebKeys.RENDER_PORTLET);
+
+			return "http://fusion.google.com/add?source=atgs&moduleurl=" +
+				PortalUtil.getGoogleGadgetURL(portlet, _themeDisplay);
+		}
+		catch (PortalException pe) {
+			return StringPool.BLANK;
+		}
+	}
+
+	@Override
+	public double getWeight() {
+		return 3.0;
+	}
+
+	@Override
+	public boolean isShow() {
+		PortletDisplay portletDisplay = _themeDisplay.getPortletDisplay();
+
+		PortletPreferences portletSetup =
+			PortletPreferencesFactoryUtil.getStrictLayoutPortletSetup(
+				_themeDisplay.getLayout(), portletDisplay.getId());
+
+		boolean lfrIgoogleShowAddAppLink = GetterUtil.getBoolean(
+			portletSetup.getValue(
+				"lfrIgoogleShowAddAppLink", StringPool.BLANK));
+
+		if (lfrIgoogleShowAddAppLink) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean showLabel() {
+		return true;
+	}
+
+}

modules/apps/portlet-configuration-icons/portlet-configuration-icon-netvibes/src/com/liferay/portlet/configuration/icon/netvibes/NetvibesPortletConfigurationIcon.java
@@ -1,0 +1,115 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.configuration.icon.netvibes;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.configuration.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.PortletConfigurationIcon;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.model.Portlet;
+import com.liferay.portal.theme.PortletDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portlet.PortletURLFactoryUtil;
+
+import javax.portlet.PortletPreferences;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true, service = PortletConfigurationIcon.class
+)
+public class NetvibesPortletConfigurationIcon
+	extends BasePortletConfigurationIcon {
+
+	@Override
+	public String getIconCssClass() {
+		return "icon-plus-sign";
+	}
+
+	@Override
+	public String getMessage() {
+		return "add-to-netvibes";
+	}
+
+	@Override
+	public String getMethod() {
+		return "get";
+	}
+
+	@Override
+	public String getURL() {
+		try {
+			Portlet portlet = (Portlet)_request.getAttribute(
+				WebKeys.RENDER_PORTLET);
+
+			PortletURL basePortletURL = PortletURLFactoryUtil.create(
+				_request, PortletKeys.PORTLET_SHARING, _themeDisplay.getPlid(),
+				PortletRequest.RESOURCE_PHASE);
+
+			StringBundler sb = new StringBundler(5);
+
+			sb.append("javascript:Liferay.PortletSharing.showNetvibesInfo('");
+			sb.append(PortalUtil.getNetvibesURL(portlet, _themeDisplay));
+			sb.append("', '");
+			sb.append(basePortletURL);
+			sb.append("');");
+
+			return sb.toString();
+		}
+		catch (PortalException pe) {
+			return StringPool.BLANK;
+		}
+	}
+
+	@Override
+	public double getWeight() {
+		return 2.0;
+	}
+
+	@Override
+	public boolean isShow() {
+		PortletDisplay portletDisplay = _themeDisplay.getPortletDisplay();
+
+		PortletPreferences portletSetup =
+			PortletPreferencesFactoryUtil.getStrictLayoutPortletSetup(
+				_themeDisplay.getLayout(), portletDisplay.getId());
+
+		boolean lfrNetvibesShowAddAppLink = GetterUtil.getBoolean(
+			portletSetup.getValue(
+				"lfrNetvibesShowAddAppLink", StringPool.BLANK));
+
+		if (lfrNetvibesShowAddAppLink) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean showLabel() {
+		return true;
+	}
+
+}

modules/apps/portlet-configuration-icons/portlet-configuration-icon-widget/src/com/liferay/portlet/configuration/icon/widget/WidgetPortletConfigurationIcon.java
@@ -1,0 +1,111 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.configuration.icon.widget;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.configuration.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.PortletConfigurationIcon;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.model.Portlet;
+import com.liferay.portal.theme.PortletDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portal.util.PropsValues;
+import com.liferay.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portlet.PortletURLFactoryUtil;
+
+import javax.portlet.PortletPreferences;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true, service = PortletConfigurationIcon.class
+)
+public class WidgetPortletConfigurationIcon
+	extends BasePortletConfigurationIcon {
+
+	@Override
+	public String getIconCssClass() {
+		return "icon-plus-sign";
+	}
+
+	@Override
+	public String getMessage() {
+		return "add-to-any-website";
+	}
+
+	@Override
+	public String getURL() {
+		try {
+			Portlet portlet = (Portlet)_request.getAttribute(
+				WebKeys.RENDER_PORTLET);
+
+			PortletURL basePortletURL = PortletURLFactoryUtil.create(
+				_request, PortletKeys.PORTLET_SHARING, _themeDisplay.getPlid(),
+				PortletRequest.RESOURCE_PHASE);
+
+			StringBundler sb = new StringBundler();
+
+			sb.append("javascript:Liferay.PortletSharing.showWidgetInfo('");
+			sb.append(PortalUtil.getWidgetURL(portlet, _themeDisplay));
+			sb.append("', '");
+			sb.append(basePortletURL);
+			sb.append("');");
+
+			return sb.toString();
+		}
+		catch (PortalException pe) {
+			return StringPool.BLANK;
+		}
+	}
+
+	@Override
+	public double getWeight() {
+		return 5.0;
+	}
+
+	@Override
+	public boolean isShow() {
+		PortletDisplay portletDisplay = _themeDisplay.getPortletDisplay();
+
+		PortletPreferences portletSetup =
+			PortletPreferencesFactoryUtil.getStrictLayoutPortletSetup(
+				_themeDisplay.getLayout(), portletDisplay.getId());
+
+		boolean widgetShowAddAppLink = GetterUtil.getBoolean(
+			portletSetup.getValue("lfrWidgetShowAddAppLink", null),
+			PropsValues.THEME_PORTLET_SHARING_DEFAULT);
+
+		if (widgetShowAddAppLink) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean showLabel() {
+		return true;
+	}
+
+}