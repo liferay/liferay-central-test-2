07bcf097147f8fbb9fe86ba91a20d3d26c914d03
kenjiheigel <kenji.heigel@liferay.com>
2015-04-01 15:33:27 -0700

    LRQA-14720 Add new MobileDriver class for iOS testing in poshi runner

---
+++ modules/test/poshi-runner/src/com/liferay/poshi/runner/selenium/IOSMobileDriverImpl.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.poshi.runner.selenium;
+
+import io.appium.java_client.ios.IOSDriver;
+
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * @author Kenji Heigel
+ */
+public class IOSMobileDriverImpl extends BaseMobileDriverImpl {
+
+	public IOSMobileDriverImpl(String projectDirName, String browserURL) {
+		super(
+			projectDirName, browserURL,
+			new IOSDriver(_url, _desiredCapabilities));
+	}
+
+	private static final DesiredCapabilities _desiredCapabilities;
+	private static final URL _url;
+
+	static {
+		_desiredCapabilities = DesiredCapabilities.android();
+
+		_desiredCapabilities.setCapability("browserName", "Safari");
+		_desiredCapabilities.setCapability("deviceName", "iPhone 5s");
+		_desiredCapabilities.setCapability("platformName", "iOS");
+		_desiredCapabilities.setCapability("platformVersion", "8.2");
+
+		URL url = null;
+
+		try {
+			url = new URL("http://0.0.0.0:4723/wd/hub/");
+		}
+		catch (Exception e) {
+		}
+
+		_url = url;
+	}
+
+}
