75acf3b3c52a057e926d6bc874ba38e694552b8e
Julio Camarero <julio.camarero@liferay.com>
2015-04-01 18:31:15 +0200

    LPS-53627 create components to encapsulate user tools and content metadata

---
+++ modules/apps/journal/journal-content-web/src/com/liferay/journal/content/web/entries/RatingsContentMetadataAssetAddonEntry.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.content.web.entries;
+
+import com.liferay.journal.content.web.util.ContentMetadataAssetAddonEntry;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.servlet.taglib.ui.BaseAssetAddonEntry;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Julio Camarero
+ */
+@Component(
+	immediate = true, service = ContentMetadataAssetAddonEntry.class
+)
+public class RatingsContentMetadataAssetAddonEntry
+	extends BaseAssetAddonEntry implements ContentMetadataAssetAddonEntry {
+
+	@Override
+	public String getIcon() {
+		return "star-half-full";
+	}
+
+	@Override
+	public String getKey() {
+		return "enableRatings";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return LanguageUtil.get(locale, "ratings");
+	}
+
+	@Override
+	public Double getWeight() {
+		return 2.0;
+	}
+
+}
