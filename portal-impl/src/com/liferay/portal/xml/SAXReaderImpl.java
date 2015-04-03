379745f307392262b74b2ed6cdfc6ad21e6898a4
Raymond Auge <raymond.auge@liferay.com>
2015-04-03 12:36:38 -0400

    LPS-54735 isolate the initialization of the sax parser instance from SAXReaderImpl

--- portal-impl/src/com/liferay/portal/xml/SAXReaderImpl.java
+++ portal-impl/src/com/liferay/portal/xml/SAXReaderImpl.java
@@ -14,8 +14,6 @@
 
 package com.liferay.portal.xml;
 
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.security.pacl.DoPrivileged;
 import com.liferay.portal.kernel.xml.Attribute;
 import com.liferay.portal.kernel.xml.Document;
@@ -33,7 +31,6 @@ import com.liferay.portal.kernel.xml.XPath;
 import com.liferay.portal.security.xml.SecureXMLFactoryProvider;
 import com.liferay.portal.security.xml.SecureXMLFactoryProviderImpl;
 import com.liferay.portal.util.ClassLoaderUtil;
-import com.liferay.portal.util.EntityResolver;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.util.xml.XMLSafeReader;
 
@@ -547,42 +544,12 @@ public class SAXReaderImpl implements SAXReader {
 	}
 
 	protected org.dom4j.io.SAXReader getSAXReader(boolean validate) {
-		org.dom4j.io.SAXReader reader = null;
-
 		if (!PropsValues.XML_VALIDATION_ENABLED) {
 			validate = false;
 		}
 
-		try {
-			reader = new org.dom4j.io.SAXReader(
-				_secureXMLFactoryProvider.newXMLReader(), validate);
-
-			reader.setEntityResolver(new EntityResolver());
-			reader.setFeature(_FEATURES_DYNAMIC, validate);
-			reader.setFeature(_FEATURES_VALIDATION, validate);
-			reader.setFeature(_FEATURES_VALIDATION_SCHEMA, validate);
-			reader.setFeature(
-				_FEATURES_VALIDATION_SCHEMA_FULL_CHECKING, validate);
-
-			if (!_secure) {
-				reader.setFeature(_FEATURES_DISALLOW_DOCTYPE_DECL, false);
-				reader.setFeature(_FEATURES_LOAD_DTD_GRAMMAR, validate);
-				reader.setFeature(_FEATURES_LOAD_EXTERNAL_DTD, validate);
-			}
-		}
-		catch (Exception e) {
-			if (_log.isWarnEnabled()) {
-				_log.warn(
-					"XSD validation is disabled because " + e.getMessage());
-			}
-
-			reader = new org.dom4j.io.SAXReader(
-				_secureXMLFactoryProvider.newXMLReader(), false);
-
-			reader.setEntityResolver(new EntityResolver());
-		}
-
-		return reader;
+		return SAXReaderFactory.getSAXReader(
+			_secureXMLFactoryProvider.newXMLReader(), validate, _secure);
 	}
 
 	protected org.dom4j.io.SAXReader getSAXReader(XMLSchema xmlSchema) {
@@ -592,56 +559,10 @@ public class SAXReaderImpl implements SAXReader {
 			validate = false;
 		}
 
-		org.dom4j.io.SAXReader saxReader = getSAXReader(validate);
-
-		if ((xmlSchema == null) || (validate == false)) {
-			return saxReader;
-		}
-
-		try {
-			saxReader.setProperty(
-				_PROPERTY_SCHEMA_LANGUAGE, xmlSchema.getSchemaLanguage());
-			saxReader.setProperty(
-				_PROPERTY_SCHEMA_SOURCE, xmlSchema.getSchemaSource());
-		}
-		catch (Exception e) {
-			if (_log.isWarnEnabled()) {
-				_log.warn(
-					"XSD validation is disabled because " + e.getMessage());
-			}
-		}
-
-		return saxReader;
+		return SAXReaderFactory.getSAXReader(
+			_secureXMLFactoryProvider.newXMLReader(), xmlSchema, validate,
+			_secure);
 	}
-
-	private static final String _FEATURES_DISALLOW_DOCTYPE_DECL =
-		"http://apache.org/xml/features/disallow-doctype-decl";
-
-	private static final String _FEATURES_DYNAMIC =
-		"http://apache.org/xml/features/validation/dynamic";
-
-	private static final String _FEATURES_LOAD_DTD_GRAMMAR =
-		"http://apache.org/xml/features/nonvalidating/load-dtd-grammar";
-
-	private static final String _FEATURES_LOAD_EXTERNAL_DTD =
-		"http://apache.org/xml/features/nonvalidating/load-external-dtd";
-
-	private static final String _FEATURES_VALIDATION =
-		"http://xml.org/sax/features/validation";
-
-	private static final String _FEATURES_VALIDATION_SCHEMA =
-		"http://apache.org/xml/features/validation/schema";
-
-	private static final String _FEATURES_VALIDATION_SCHEMA_FULL_CHECKING =
-		"http://apache.org/xml/features/validation/schema-full-checking";
-
-	private static final String _PROPERTY_SCHEMA_LANGUAGE =
-		"http://java.sun.com/xml/jaxp/properties/schemaLanguage";
-
-	private static final String _PROPERTY_SCHEMA_SOURCE =
-		"http://java.sun.com/xml/jaxp/properties/schemaSource";
-
-	private static final Log _log = LogFactoryUtil.getLog(SAXReaderImpl.class);
 
 	private final DocumentFactory _documentFactory =
 		DocumentFactory.getInstance();
