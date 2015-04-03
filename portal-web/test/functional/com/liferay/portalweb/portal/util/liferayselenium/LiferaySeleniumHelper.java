f392f7a2dd53b76d292d70260db1e624823e7fa1
Brian Chiu <brian.chiu@liferay.com>
2015-04-03 16:48:28 -0700

    LRQA-14770 SF

--- portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/LiferaySeleniumHelper.java
+++ portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/LiferaySeleniumHelper.java
@@ -1043,13 +1043,11 @@ public class LiferaySeleniumHelper {
 
 		// LPS-54680
 
-		if (line.matches(
-				".*The web application \\[\\] appears to have started a " +
-					"thread named.*")) {
+		if (line.contains(
+				"The web application [/reports-portlet] appears to have " +
+					"started a thread named [C3P0PooledConnectionPool")) {
 
-			if (line.contains("[/reports-portlet]")) {
-				return true;
-			}
+			return true;
 		}
 
 		// LRQA-14442, temporary workaround until Kiyoshi Lee fixes it
