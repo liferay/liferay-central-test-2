efb7f60b1643c9423c0d60a476258f4b4e183985
kenjiheigel <kenji.heigel@liferay.com>
2015-04-03 15:08:18 -0700

    LRQA-14476 Make android.home property available to selenium in selenium builder

--- portal-web/test/functional/com/liferay/portalweb/util/TestPropsValues.java
+++ portal-web/test/functional/com/liferay/portalweb/util/TestPropsValues.java
@@ -23,6 +23,8 @@ import com.liferay.portal.kernel.util.StringUtil;
 public class TestPropsValues
 	extends com.liferay.portal.kernel.test.util.TestPropsValues {
 
+	public static final String ANDROID_HOME = TestPropsUtil.get("android.home");
+
 	public static final String BROWSER_COMMANDS_DIR_NAME = TestPropsUtil.get(
 		"browser.commands.dir");
 
