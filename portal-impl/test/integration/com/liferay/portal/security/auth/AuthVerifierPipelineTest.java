9147c343daba6c049120af41c366b954a146d041
peterfellwock <peter.fellwock@liferay.com>
2015-04-08 16:09:27 -0700

    LPS-53802 AuthVerifierPipelineTest Integration point test

---
+++ portal-impl/test/integration/com/liferay/portal/security/auth/AuthVerifierPipelineTest.java
@@ -0,0 +1,113 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.auth;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.servlet.HttpMethods;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.MainServletTestRule;
+import com.liferay.portal.test.rule.SyntheticBundleRule;
+import com.liferay.portal.util.WebKeys;
+import com.liferay.portal.util.test.AtomicState;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockServletContext;
+
+/**
+ * @author Peter Fellwock
+ */
+public class AuthVerifierPipelineTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE,
+			new SyntheticBundleRule("bundle.authverifierpipeline"));
+
+	@BeforeClass
+	public static void setUpClass() {
+		_atomicState = new AtomicState();
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		_atomicState.close();
+	}
+
+	@Test
+	public void testVerifyRequest() {
+		_atomicState.reset();
+
+		try {
+			AccessControlContext accessControlContext =
+				new AccessControlContext();
+
+			MockHttpServletRequest mockHttpServletRequest = createHttpRequest(
+				"/foo/hello");
+
+			mockHttpServletRequest.setAttribute(
+				WebKeys.COMPANY_ID, new Long(TestPropsValues.getCompanyId()));
+
+			accessControlContext.setRequest(mockHttpServletRequest);
+
+			AuthVerifierPipeline.verifyRequest(accessControlContext);
+		}
+		catch (PortalException e) {
+			Assert.fail();
+		}
+	}
+
+	protected MockHttpServletRequest createHttpRequest(String pathInfo) {
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequestTest();
+
+		mockHttpServletRequest.setMethod(HttpMethods.GET);
+		mockHttpServletRequest.setPathInfo(pathInfo);
+
+		return mockHttpServletRequest;
+	}
+
+	private static AtomicState _atomicState;
+
+	private class MockHttpServletRequestTest extends MockHttpServletRequest {
+
+		public MockHttpServletRequestTest() {
+			_mockServletContext = new MockServletContext() {};
+
+			_mockServletContext.setContextPath(StringPool.BLANK);
+			_mockServletContext.setServletContextName(StringPool.BLANK);
+		}
+
+		@Override
+		public MockServletContext getServletContext() {
+			return _mockServletContext;
+		}
+
+		private final MockServletContext _mockServletContext;
+
+	}
+
+}
---
+++ portal-impl/test/integration/com/liferay/portal/security/auth/bundle/authverifierpipeline/TestAuthVerifier.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.auth.bundle.authverifierpipeline;
+
+import com.liferay.portal.kernel.util.StackTraceUtil;
+import com.liferay.portal.security.auth.AccessControlContext;
+import com.liferay.portal.security.auth.AuthVerifier;
+import com.liferay.portal.security.auth.AuthVerifierResult;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Peter Fellwock
+ */
+@Component(
+	immediate = true,
+	property = {
+		"service.ranking:Integer=" + Integer.MAX_VALUE,
+		"urls.includes=/TestAuthVerifier/*,/TestAuthVerifierTest/*"
+	}
+)
+public class TestAuthVerifier implements AuthVerifier {
+
+	@Override
+	public String getAuthType() {
+		return HttpServletRequest.BASIC_AUTH;
+	}
+
+	@Override
+	public AuthVerifierResult verify(
+		AccessControlContext accessControlContext, Properties properties) {
+
+		AuthVerifierResult authVerifierResult = new AuthVerifierResult();
+		authVerifierResult.setPassword("best_password_ever");
+		authVerifierResult.setState(AuthVerifierResult.State.SUCCESS);
+		authVerifierResult.setUserId(1);
+
+		Map<String, Object> settings = new HashMap<>();
+
+		settings.put("auth.type", HttpServletRequest.BASIC_AUTH);
+
+		authVerifierResult.setSettings(settings);
+
+		_atomicReference.set(StackTraceUtil.getCallerKey());
+
+		return authVerifierResult;
+	}
+
+	@Reference(target = "(test=AtomicState)")
+	protected void getAtomicReference(AtomicReference<String> atomicReference) {
+		_atomicReference = atomicReference;
+	}
+
+	private AtomicReference<String> _atomicReference;
+
+}
