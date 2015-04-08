7118ceb35ec8934cc6dd6960214389ee4392b09a
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-04-08 18:31:56 +0200

    LPS-54424 Format source

--- modules/apps/document-library/document-library-cmis-repository-test/test/integration/com/liferay/document/library/repository/cmis/search/test/CMISQueryBuilderTest.java
+++ modules/apps/document-library/document-library-cmis-repository-test/test/integration/com/liferay/document/library/repository/cmis/search/test/CMISQueryBuilderTest.java
@@ -44,6 +44,7 @@ import org.apache.chemistry.opencmis.commons.enums.CapabilityQuery;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -53,6 +54,11 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Arquillian.class)
 public class CMISQueryBuilderTest {
+
+	@ClassRule
+	@Rule
+	public static final DeleteAfterTestRunTestRule deleteAfterTestRule =
+		new DeleteAfterTestRunTestRule();
 
 	@Before
 	public void setUp() throws Exception {
@@ -439,10 +445,6 @@ public class CMISQueryBuilderTest {
 			"(cmis:name LIKE 'test%.jpg' OR cmis:createdBy LIKE 'test%.jpg')",
 			cmisQuery);
 	}
-
-	@Rule
-	public final DeleteAfterTestRunTestRule deleteAfterTestRule =
-		new DeleteAfterTestRunTestRule();
 
 	protected void assertQueryEquals(String where, String query) {
 		Assert.assertEquals(_QUERY_PREFIX + where + _QUERY_POSTFIX, query);
