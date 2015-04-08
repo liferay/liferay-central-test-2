15d5bbb99964af8dec56a6cec4a957de5452bda5
adolfo.perez <adolfo.perez@liferay.com>
2015-04-08 17:15:53 +0200

    LPS-54713 Remove duplication by using delegate in FindStrutsAction

--- portal-impl/src/com/liferay/portal/struts/FindStrutsAction.java
+++ portal-impl/src/com/liferay/portal/struts/FindStrutsAction.java
@@ -14,38 +14,9 @@
 
 package com.liferay.portal.struts;
 
-import com.liferay.portal.NoSuchLayoutException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.struts.BaseStrutsAction;
-import com.liferay.portal.kernel.util.ArrayUtil;
-import com.liferay.portal.kernel.util.HttpUtil;
-import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.StringBundler;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.portal.model.Group;
-import com.liferay.portal.model.Layout;
-import com.liferay.portal.model.LayoutConstants;
-import com.liferay.portal.model.LayoutTypePortlet;
-import com.liferay.portal.model.PortletConstants;
-import com.liferay.portal.model.impl.VirtualLayout;
-import com.liferay.portal.security.auth.PrincipalException;
-import com.liferay.portal.security.permission.ActionKeys;
-import com.liferay.portal.security.permission.PermissionChecker;
-import com.liferay.portal.service.GroupLocalServiceUtil;
-import com.liferay.portal.service.LayoutLocalServiceUtil;
-import com.liferay.portal.service.permission.LayoutPermissionUtil;
-import com.liferay.portal.theme.ThemeDisplay;
-import com.liferay.portal.util.PortalUtil;
-import com.liferay.portlet.PortletURLFactoryUtil;
-import com.liferay.portlet.sites.util.SitesUtil;
 
-import javax.portlet.PortletMode;
-import javax.portlet.PortletRequest;
 import javax.portlet.PortletURL;
-import javax.portlet.WindowState;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -55,11 +26,49 @@ import javax.servlet.http.HttpServletResponse;
 public abstract class FindStrutsAction extends BaseStrutsAction {
 
 	public FindStrutsAction() {
-		_portletIds = initPortletIds();
+		_findActionDelegate = new BaseFindActionDelegate() {
 
-		if (ArrayUtil.isEmpty(_portletIds)) {
-			throw new RuntimeException("Portlet IDs cannot be null or empty");
-		}
+			@Override
+			public long getGroupId(long primaryKey) throws Exception {
+				return FindStrutsAction.this.getGroupId(primaryKey);
+			}
+
+			@Override
+			public String getPrimaryKeyParameterName() {
+				return FindStrutsAction.this.getPrimaryKeyParameterName();
+			}
+
+			@Override
+			public String getStrutsAction(
+				HttpServletRequest request, String portletId) {
+
+				return FindStrutsAction.this.getStrutsAction(
+					request, portletId);
+			}
+
+			@Override
+			public String[] initPortletIds() {
+				return FindStrutsAction.this.initPortletIds();
+			}
+
+			@Override
+			public PortletURL processPortletURL(
+					HttpServletRequest request, PortletURL portletURL)
+				throws Exception {
+
+				return FindStrutsAction.this.processPortletURL(
+					request, portletURL);
+			}
+
+			@Override
+			public void setPrimaryKeyParameter(
+				PortletURL portletURL, long primaryKey) throws Exception {
+
+				FindStrutsAction.this.setPrimaryKeyParameter(
+					portletURL, primaryKey);
+			}
+
+		};
 	}
 
 	@Override
@@ -67,243 +76,9 @@ public abstract class FindStrutsAction extends BaseStrutsAction {
 			HttpServletRequest request, HttpServletResponse response)
 		throws Exception {
 
-		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		try {
-			long primaryKey = ParamUtil.getLong(
-				request, getPrimaryKeyParameterName());
-
-			long groupId = ParamUtil.getLong(
-				request, "groupId", themeDisplay.getScopeGroupId());
-
-			if (primaryKey > 0) {
-				try {
-					long overrideGroupId = getGroupId(primaryKey);
-
-					if (overrideGroupId > 0) {
-						groupId = overrideGroupId;
-					}
-				}
-				catch (Exception e) {
-					if (_log.isDebugEnabled()) {
-						_log.debug(e, e);
-					}
-				}
-			}
-
-			Object[] plidAndPortletId = getPlidAndPortletId(
-				themeDisplay, groupId, themeDisplay.getPlid(), _portletIds);
-
-			long plid = (Long)plidAndPortletId[0];
-
-			Layout layout = setTargetLayout(request, groupId, plid);
-
-			LayoutPermissionUtil.check(
-				themeDisplay.getPermissionChecker(), layout, true,
-				ActionKeys.VIEW);
-
-			String portletId = (String)plidAndPortletId[1];
-
-			PortletURL portletURL = PortletURLFactoryUtil.create(
-				request, portletId, plid, PortletRequest.RENDER_PHASE);
-
-			portletURL.setParameter(
-				"struts_action", getStrutsAction(request, portletId));
-
-			boolean inheritRedirect = ParamUtil.getBoolean(
-				request, "inheritRedirect");
-
-			String redirect = null;
-
-			if (inheritRedirect) {
-				String noSuchEntryRedirect = ParamUtil.getString(
-					request, "noSuchEntryRedirect");
-
-				redirect = HttpUtil.getParameter(
-					noSuchEntryRedirect, "redirect", false);
-
-				redirect = HttpUtil.decodeURL(redirect);
-			}
-			else {
-				redirect = ParamUtil.getString(request, "redirect");
-			}
-
-			if (Validator.isNotNull(redirect)) {
-				portletURL.setParameter("redirect", redirect);
-			}
-
-			setPrimaryKeyParameter(portletURL, primaryKey);
-
-			portletURL.setPortletMode(PortletMode.VIEW);
-			portletURL.setWindowState(WindowState.NORMAL);
-
-			portletURL = processPortletURL(request, portletURL);
-
-			response.sendRedirect(portletURL.toString());
-
-			return null;
-		}
-		catch (Exception e) {
-			String noSuchEntryRedirect = ParamUtil.getString(
-				request, "noSuchEntryRedirect");
-
-			noSuchEntryRedirect = PortalUtil.escapeRedirect(
-				noSuchEntryRedirect);
-
-			if (Validator.isNotNull(noSuchEntryRedirect) &&
-				(e instanceof NoSuchLayoutException ||
-				 e instanceof PrincipalException)) {
-
-				response.sendRedirect(noSuchEntryRedirect);
-			}
-			else {
-				PortalUtil.sendError(e, request, response);
-			}
-
-			return null;
-		}
-	}
-
-	protected static Object[] fetchPlidAndPortletId(
-			PermissionChecker permissionChecker, long groupId,
-			String[] portletIds)
-		throws Exception {
-
-		for (String portletId : portletIds) {
-			long plid = PortalUtil.getPlidFromPortletId(groupId, portletId);
-
-			if (plid == LayoutConstants.DEFAULT_PLID) {
-				continue;
-			}
-
-			Layout layout = LayoutLocalServiceUtil.getLayout(plid);
-
-			if (!LayoutPermissionUtil.contains(
-					permissionChecker, layout, ActionKeys.VIEW)) {
-
-				continue;
-			}
-
-			LayoutTypePortlet layoutTypePortlet =
-				(LayoutTypePortlet)layout.getLayoutType();
-
-			portletId = getPortletId(layoutTypePortlet, portletId);
-
-			return new Object[] {plid, portletId};
-		}
+		_findActionDelegate.execute(request, response);
 
 		return null;
-	}
-
-	protected static Object[] getPlidAndPortletId(
-			ThemeDisplay themeDisplay, long groupId, long plid,
-			String[] portletIds)
-		throws Exception {
-
-		if ((plid != LayoutConstants.DEFAULT_PLID) &&
-			(groupId == themeDisplay.getScopeGroupId())) {
-
-			try {
-				Layout layout = LayoutLocalServiceUtil.getLayout(plid);
-
-				LayoutTypePortlet layoutTypePortlet =
-					(LayoutTypePortlet)layout.getLayoutType();
-
-				for (String portletId : portletIds) {
-					if (!layoutTypePortlet.hasPortletId(portletId, false) ||
-						!LayoutPermissionUtil.contains(
-							themeDisplay.getPermissionChecker(), layout,
-							ActionKeys.VIEW)) {
-
-						continue;
-					}
-
-					portletId = getPortletId(layoutTypePortlet, portletId);
-
-					return new Object[] {plid, portletId};
-				}
-			}
-			catch (NoSuchLayoutException nsle) {
-			}
-		}
-
-		Object[] plidAndPortletId = fetchPlidAndPortletId(
-			themeDisplay.getPermissionChecker(), groupId, portletIds);
-
-		if ((plidAndPortletId == null) &&
-			SitesUtil.isUserGroupLayoutSetViewable(
-				themeDisplay.getPermissionChecker(),
-				themeDisplay.getScopeGroup())) {
-
-			plidAndPortletId = fetchPlidAndPortletId(
-				themeDisplay.getPermissionChecker(),
-				themeDisplay.getScopeGroupId(), portletIds);
-		}
-
-		if (plidAndPortletId != null) {
-			return plidAndPortletId;
-		}
-
-		StringBundler sb = new StringBundler(portletIds.length * 2 + 5);
-
-		sb.append("{groupId=");
-		sb.append(groupId);
-		sb.append(", plid=");
-		sb.append(plid);
-
-		for (String portletId : portletIds) {
-			sb.append(", portletId=");
-			sb.append(portletId);
-		}
-
-		sb.append("}");
-
-		throw new NoSuchLayoutException(sb.toString());
-	}
-
-	protected static String getPortletId(
-		LayoutTypePortlet layoutTypePortlet, String portletId) {
-
-		for (String curPortletId : layoutTypePortlet.getPortletIds()) {
-			String curRootPortletId = PortletConstants.getRootPortletId(
-				curPortletId);
-
-			if (portletId.equals(curRootPortletId)) {
-				return curPortletId;
-			}
-		}
-
-		return portletId;
-	}
-
-	protected static Layout setTargetLayout(
-			HttpServletRequest request, long groupId, long plid)
-		throws Exception {
-
-		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		PermissionChecker permissionChecker =
-			themeDisplay.getPermissionChecker();
-
-		Group group = GroupLocalServiceUtil.getGroup(groupId);
-		Layout layout = LayoutLocalServiceUtil.getLayout(plid);
-
-		if ((groupId == layout.getGroupId()) ||
-			(group.getParentGroupId() == layout.getGroupId()) ||
-			(layout.isPrivateLayout() &&
-			 !SitesUtil.isUserGroupLayoutSetViewable(
-				 permissionChecker, layout.getGroup()))) {
-
-			return layout;
-		}
-
-		layout = new VirtualLayout(layout, group);
-
-		request.setAttribute(WebKeys.LAYOUT, layout);
-
-		return layout;
 	}
 
 	protected abstract long getGroupId(long primaryKey) throws Exception;
@@ -330,9 +105,6 @@ public abstract class FindStrutsAction extends BaseStrutsAction {
 			getPrimaryKeyParameterName(), String.valueOf(primaryKey));
 	}
 
-	private static final Log _log = LogFactoryUtil.getLog(
-		FindStrutsAction.class);
-
-	private final String[] _portletIds;
+	private final FindActionDelegate _findActionDelegate;
 
 }
