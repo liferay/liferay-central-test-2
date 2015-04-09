0efb16e3c87b39029b7fc90297ddb2943909126d
Carlos Sierra <carlos.sierra@liferay.com>
2015-04-09 15:36:48 +0200

    LPS-54842 Initializes JAX-RS infrastructure and cleans it up

---
+++ modules/portal/portal-cxf-jaxrs-common/src/com/liferay/portal/cxf/jaxrs/common/activator/SetUpActivator.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.cxf.jaxrs.common.activator;
+
+import java.util.Collection;
+
+import javax.ws.rs.ext.RuntimeDelegate;
+
+import org.apache.cxf.Bus;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.wiring.BundleWiring;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public class SetUpActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+		Bundle bundle = bundleContext.getBundle();
+
+		BundleWiring bundleWiring = bundle.adapt(BundleWiring.class);
+
+		ClassLoader bundleClassLoader = bundleWiring.getClassLoader();
+
+		Thread thread = Thread.currentThread();
+
+		ClassLoader contextClassLoader = thread.getContextClassLoader();
+
+		thread.setContextClassLoader(bundleClassLoader);
+
+		//INIT CACHE inside RuntimeDelegate so it is never looked up again
+		RuntimeDelegate.getInstance();
+
+		thread.setContextClassLoader(contextClassLoader);
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+		Collection<ServiceReference<Bus>> serviceReferences =
+			bundleContext.getServiceReferences(Bus.class, null);
+
+		//Cleans up the cached instance so a new bundle can take our place
+
+		for (ServiceReference<Bus> serviceReference : serviceReferences) {
+			Bus bus = bundleContext.getService(serviceReference);
+
+			bus.setProperty("jaxrs.shared.server.factory", null);
+		}
+	}
+
+}
