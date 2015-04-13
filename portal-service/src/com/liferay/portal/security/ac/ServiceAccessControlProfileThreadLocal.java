1125f9a16246c82b53f59fb5773e026f3ecc207d
Mika Koivisto <mika.koivisto@liferay.com>
2015-04-13 13:06:07 +0300

    LPS-54043 Add public API

---
+++ portal-service/src/com/liferay/portal/security/ac/ServiceAccessControlProfileThreadLocal.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.ac;
+
+import com.liferay.portal.kernel.util.AutoResetThreadLocal;
+
+import java.util.List;
+
+/**
+ * @author Mika Koivisto
+ */
+public class ServiceAccessControlProfileThreadLocal {
+
+	public static List<String>
+		getActiveServiceAccessControlProfileNames() {
+
+		return _activeServiceAccessControlProfileNames.get();
+	}
+
+	public static void setActiveServiceAccessControlProfileNames(
+		List<String> activeServiceAccessControlProfileNames) {
+
+		_activeServiceAccessControlProfileNames.set(
+			activeServiceAccessControlProfileNames);
+	}
+
+	private static final ThreadLocal<List<String>>
+		_activeServiceAccessControlProfileNames = new AutoResetThreadLocal<>(
+			AutoResetThreadLocal.class +
+			"._activeServiceAccessControlProfileNames");
+
+}
