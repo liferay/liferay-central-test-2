91e757617a458a1409d043b36280b5fc8e9ac7ea
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-04-13 10:55:55 +0200

    LPS-54918 Apply changes

--- portal-impl/src/com/liferay/portlet/layoutsetprototypes/LayoutSetPrototypePortlet.java
+++ portal-impl/src/com/liferay/portlet/layoutsetprototypes/LayoutSetPrototypePortlet.java
@@ -14,14 +14,45 @@
 
 package com.liferay.portlet.layoutsetprototypes;
 
+import com.liferay.portal.NoSuchLayoutSetPrototypeException;
+import com.liferay.portal.RequiredLayoutSetPrototypeException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.servlet.MultiSessionMessages;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.model.LayoutSetPrototype;
+import com.liferay.portal.security.auth.PrincipalException;
+import com.liferay.portal.service.LayoutSetPrototypeServiceUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceContextFactory;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portlet.sites.util.SitesUtil;
+
+import java.io.IOException;
+
+import java.util.Locale;
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 /**
  * @author Eudaldo Alonso
  */
 public class LayoutSetPrototypePortlet extends MVCPortlet {
 
-	public void deleteLayoutSetPrototypes(ActionRequest actionRequest)
+	public void deleteLayoutSetPrototypes(
+			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
 
 		long[] layoutSetPrototypeIds = StringUtil.split(
@@ -33,120 +64,8 @@ public class LayoutSetPrototypePortlet extends MVCPortlet {
 		}
 	}
 
-	@Override
-	public void processAction(
-			ActionMapping actionMapping, ActionForm actionForm,
-			PortletConfig portletConfig, ActionRequest actionRequest,
-			ActionResponse actionResponse)
-		throws Exception {
-
-		String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
-
-		String redirect = ParamUtil.getString(actionRequest, "redirect");
-
-		try {
-			if (cmd.equals(Constants.ADD) || cmd.equals(Constants.UPDATE)) {
-				LayoutSetPrototype layoutSetPrototype =
-					updateLayoutSetPrototype(actionRequest);
-
-				ThemeDisplay themeDisplay =
-					(ThemeDisplay)actionRequest.getAttribute(
-						WebKeys.THEME_DISPLAY);
-
-				ThemeDisplay siteThemeDisplay =
-					(ThemeDisplay)themeDisplay.clone();
-
-				siteThemeDisplay.setScopeGroupId(
-					layoutSetPrototype.getGroupId());
-
-				PortletURL siteAdministrationURL =
-					PortalUtil.getSiteAdministrationURL(
-						actionResponse, siteThemeDisplay,
-						PortletKeys.SITE_TEMPLATE_SETTINGS);
-
-				String controlPanelURL = HttpUtil.setParameter(
-					themeDisplay.getURLControlPanel(), "p_p_id",
-					PortletKeys.LAYOUT_SET_PROTOTYPE);
-
-				controlPanelURL = HttpUtil.setParameter(
-					controlPanelURL, "controlPanelCategory",
-					themeDisplay.getControlPanelCategory());
-
-				siteAdministrationURL.setParameter("redirect", controlPanelURL);
-
-				redirect = siteAdministrationURL.toString();
-
-				if (cmd.equals(Constants.ADD)) {
-					hideDefaultSuccessMessage(actionRequest);
-
-					MultiSessionMessages.add(
-						actionRequest,
-						PortletKeys.SITE_TEMPLATE_SETTINGS +
-							"requestProcessed");
-				}
-			}
-			else if (cmd.equals(Constants.DELETE)) {
-				deleteLayoutSetPrototypes(actionRequest);
-			}
-			else if (cmd.equals("reset_merge_fail_count")) {
-				resetMergeFailCount(actionRequest);
-			}
-
-			sendRedirect(actionRequest, actionResponse, redirect);
-		}
-		catch (Exception e) {
-			if (e instanceof PrincipalException) {
-				SessionErrors.add(actionRequest, e.getClass());
-
-				setForward(
-					actionRequest, "portlet.layout_set_prototypes.error");
-			}
-			else if (e instanceof RequiredLayoutSetPrototypeException) {
-				SessionErrors.add(actionRequest, e.getClass());
-
-				redirect = PortalUtil.escapeRedirect(redirect);
-
-				if (Validator.isNotNull(redirect)) {
-					actionResponse.sendRedirect(redirect);
-				}
-			}
-			else {
-				throw e;
-			}
-		}
-	}
-
-	@Override
-	public ActionForward render(
-			ActionMapping actionMapping, ActionForm actionForm,
-			PortletConfig portletConfig, RenderRequest renderRequest,
-			RenderResponse renderResponse)
-		throws Exception {
-
-		try {
-			ActionUtil.getLayoutSetPrototype(renderRequest);
-		}
-		catch (Exception e) {
-			if (e instanceof NoSuchLayoutSetPrototypeException ||
-				e instanceof PrincipalException) {
-
-				SessionErrors.add(renderRequest, e.getClass());
-
-				return actionMapping.findForward(
-					"portlet.layout_set_prototypes.error");
-			}
-			else {
-				throw e;
-			}
-		}
-
-		return actionMapping.findForward(
-			getForward(
-				renderRequest,
-				"portlet.layout_set_prototypes.edit_layout_set_prototype"));
-	}
-
-	public void resetMergeFailCount(ActionRequest actionRequest)
+	public void resetMergeFailCount(
+			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
 
 		long layoutSetPrototypeId = ParamUtil.getLong(
@@ -159,8 +78,8 @@ public class LayoutSetPrototypePortlet extends MVCPortlet {
 		SitesUtil.setMergeFailCount(layoutSetPrototype, 0);
 	}
 
-	public LayoutSetPrototype updateLayoutSetPrototype(
-			ActionRequest actionRequest)
+	public void updateLayoutSetPrototype(
+			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
 
 		long layoutSetPrototypeId = ParamUtil.getLong(
@@ -187,6 +106,10 @@ public class LayoutSetPrototypePortlet extends MVCPortlet {
 				LayoutSetPrototypeServiceUtil.addLayoutSetPrototype(
 					nameMap, descriptionMap, active, layoutsUpdateable,
 					serviceContext);
+
+			MultiSessionMessages.add(
+				actionRequest,
+				PortletKeys.SITE_TEMPLATE_SETTINGS + "requestProcessed");
 		}
 		else {
 
@@ -209,16 +132,53 @@ public class LayoutSetPrototypePortlet extends MVCPortlet {
 		settingsProperties.setProperty(
 			"customJspServletContextName", customJspServletContextName);
 
-		return LayoutSetPrototypeServiceUtil.updateLayoutSetPrototype(
-			layoutSetPrototype.getLayoutSetPrototypeId(),
-			settingsProperties.toString());
+		layoutSetPrototype =
+			LayoutSetPrototypeServiceUtil.updateLayoutSetPrototype(
+				layoutSetPrototype.getLayoutSetPrototypeId(),
+				settingsProperties.toString());
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		ThemeDisplay siteThemeDisplay = (ThemeDisplay)themeDisplay.clone();
+
+		siteThemeDisplay.setScopeGroupId(layoutSetPrototype.getGroupId());
+
+		PortletURL siteAdministrationURL = PortalUtil.getSiteAdministrationURL(
+			actionResponse, siteThemeDisplay,
+			PortletKeys.SITE_TEMPLATE_SETTINGS);
+
+		actionRequest.setAttribute(
+			WebKeys.REDIRECT, siteAdministrationURL.toString());
+
+		sendRedirect(actionRequest, actionResponse);
 	}
 
 	@Override
-	protected boolean isCheckMethodOnProcessAction() {
-		return _CHECK_METHOD_ON_PROCESS_ACTION;
+	protected void doDispatch(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		if (SessionErrors.contains(
+				renderRequest, PrincipalException.class.getName())) {
+
+			include("/error.jsp", renderRequest, renderResponse);
+		}
+		else {
+			super.doDispatch(renderRequest, renderResponse);
+		}
 	}
 
-	private static final boolean _CHECK_METHOD_ON_PROCESS_ACTION = false;
+	@Override
+	protected boolean isSessionErrorException(Throwable cause) {
+		if (cause instanceof NoSuchLayoutSetPrototypeException ||
+			cause instanceof PrincipalException ||
+			cause instanceof RequiredLayoutSetPrototypeException) {
+
+			return true;
+		}
+
+		return false;
+	}
 
 }
