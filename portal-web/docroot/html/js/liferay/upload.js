db5922db6c6df341a208c7be3735a732bfde9604
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2015-04-13 13:45:28 -0700

    LPS-54890 - We can reverse this check instead of having to add a third condition

--- portal-web/docroot/html/js/liferay/upload.js
+++ portal-web/docroot/html/js/liferay/upload.js
@@ -403,7 +403,7 @@ AUI.add(
 					_formatTempFiles: function(fileNames) {
 						var instance = this;
 
-						if (fileNames && fileNames.length && Lang.isArray(fileNames)) {
+						if (Lang.isArray(fileNames) && fileNames.length) {
 							var fileListContent = instance._fileListContent;
 
 							instance._pendingFileInfo.show();
