130a1073012e23de3834827d211403ae81c9b502
Carlos Sierra <carlos.sierra@liferay.com>
2015-04-13 14:54:37 +0200

    LPS-54842 Test module

---
+++ modules/portal/portal-rest-extender-test/src/com/liferay/portal/rest/extender/test/activator/config/ConfigAdminActivator.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.rest.extender.test.activator.config;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public class ConfigAdminActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		ServiceReference<ConfigurationAdmin> serviceReference =
+			context.getServiceReference(ConfigurationAdmin.class);
+
+		ConfigurationAdmin configurationAdmin = context.getService(
+			serviceReference);
+
+		try {
+			_cxfConfiguration = configurationAdmin.createFactoryConfiguration(
+				"com.liferay.portal.cxf.common.configuration." +
+					"CXFEndpointPublisherConfiguration", null);
+
+			Dictionary<String, Object> properties = new Hashtable<>();
+
+			properties.put("contextPath", "/rest-test");
+
+			_cxfConfiguration.update(properties);
+
+			_restConfiguration = configurationAdmin.createFactoryConfiguration(
+				"com.liferay.portal.rest.extender.configuration." +
+					"RestExtenderConfiguration", null);
+
+			properties = new Hashtable<>();
+
+			properties.put(
+				"jaxRsApplicationFilterStrings",
+				new String[] {"(jaxrs.application=true)"});
+			properties.put("contextPaths", new String[] {"/rest-test"});
+
+			_restConfiguration.update(properties);
+		}
+		finally {
+			context.ungetService(serviceReference);
+		}
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		_restConfiguration.delete();
+
+		_cxfConfiguration.delete();
+	}
+
+	private Configuration _cxfConfiguration;
+	private Configuration _restConfiguration;
+
+}
---
+++ modules/portal/portal-rest-extender-test/test/integration/com/liferay/portal/soap/extender/test/JaxrsComponentRegistrationTest.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.soap.extender.test;
+
+import java.net.URL;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+@RunAsClient
+@RunWith(Arquillian.class)
+public class JaxrsComponentRegistrationTest {
+
+	@Test
+	public void testIsRegistered() throws Exception {
+		Thread.sleep(2000L);
+
+		URL url = new URL(_url, "/o/rest-test/testApp/sayHello");
+
+		System.out.println(url.getContent());
+	}
+
+	@ArquillianResource
+	private URL _url;
+
+}
