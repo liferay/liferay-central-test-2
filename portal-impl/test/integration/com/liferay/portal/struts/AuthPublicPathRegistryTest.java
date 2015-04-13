26d596e33d73e5a38e219f95a1be79a87d5192b0
peterfellwock <peter.fellwock@liferay.com>
2015-04-13 16:57:13 -0700

    LPS-53802 AuthPublicPathRegistryTest Integration point test

---
+++ portal-impl/test/integration/com/liferay/portal/struts/AuthPublicPathRegistryTest.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.struts;
+
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.struts.bundle.authpublicpathregistry.TestAuthPublicPath;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.MainServletTestRule;
+import com.liferay.portal.test.rule.SyntheticBundleRule;
+import com.liferay.portal.util.test.AtomicState;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Peter Fellwock
+ */
+public class AuthPublicPathRegistryTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE,
+			new SyntheticBundleRule("bundle.authpublicpathregistry"));
+
+	@Test
+	public void testContains() {
+		TestAuthPublicPath testAuthPublicPath = new TestAuthPublicPath();
+
+		Assert.assertTrue(
+			AuthPublicPathRegistry.contains(testAuthPublicPath.path()));
+
+		Assert.assertFalse(
+			AuthPublicPathRegistry.contains("/unregistered/unknown/path/"));
+	}
+
+	private static final AtomicState _atomicState;
+
+}
