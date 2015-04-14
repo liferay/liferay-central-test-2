bce7817faaee10e3a1f4c84abd20813b774705a1
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-04-14 13:25:43 -0300

    LPS-54931 Add implementation for the checkbox DDMFormFieldType

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-checkbox/src/com/liferay/dynamic/data/mapping/type/CheckboxDDMFormFieldRenderer.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.type;
+
+import com.liferay.portal.kernel.template.Template;
+import com.liferay.portal.kernel.template.TemplateConstants;
+import com.liferay.portal.kernel.template.TemplateResource;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portlet.dynamicdatamapping.model.DDMFormField;
+import com.liferay.portlet.dynamicdatamapping.model.LocalizedValue;
+import com.liferay.portlet.dynamicdatamapping.registry.BaseDDMFormFieldRenderer;
+import com.liferay.portlet.dynamicdatamapping.registry.DDMFormFieldRenderer;
+import com.liferay.portlet.dynamicdatamapping.render.DDMFormFieldRenderingContext;
+
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Renato Rego
+ */
+@Component(
+	immediate = true,
+	property = {"templatePath=/META-INF/resources/checkbox.soy"},
+	service = {
+		CheckboxDDMFormFieldRenderer.class, DDMFormFieldRenderer.class
+	}
+)
+public class CheckboxDDMFormFieldRenderer extends BaseDDMFormFieldRenderer {
+
+	@Override
+	public String getTemplateLanguage() {
+		return TemplateConstants.LANG_TYPE_SOY;
+	}
+
+	@Override
+	public String getTemplateNamespace() {
+		return "ddm.checkbox";
+	}
+
+	@Override
+	public TemplateResource getTemplateResource() {
+		return _templateResource;
+	}
+
+	@Activate
+	protected void activate(Map<String, Object> properties) {
+		String templatePath = MapUtil.getString(properties, "templatePath");
+
+		_templateResource = getTemplateResource(templatePath);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_templateResource = null;
+	}
+
+	protected String getStatus(String value, String predefinedValue) {
+		String status = StringPool.BLANK;
+
+		if (Validator.equals(value, "true")) {
+			status = "checked";
+		}
+		else if (Validator.equals(predefinedValue, "true")) {
+			status = "checked";
+		}
+
+		return status;
+	}
+
+	@Override
+	protected void populateOptionalContext(
+		Template template, DDMFormField ddmFormField,
+		DDMFormFieldRenderingContext ddmFormFieldRenderingContext) {
+
+		Locale locale = ddmFormFieldRenderingContext.getLocale();
+
+		LocalizedValue predefinedValue = ddmFormField.getPredefinedValue();
+
+		String status = getStatus(
+			ddmFormFieldRenderingContext.getValue(),
+			predefinedValue.getString(locale));
+
+		template.put("status", status);
+	}
+
+	private TemplateResource _templateResource;
+
+}
---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-checkbox/src/com/liferay/dynamic/data/mapping/type/CheckboxDDMFormFieldValueRendererAccessor.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.type;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portlet.dynamicdatamapping.registry.DDMFormFieldValueAccessor;
+import com.liferay.portlet.dynamicdatamapping.registry.DDMFormFieldValueRendererAccessor;
+import com.liferay.portlet.dynamicdatamapping.storage.DDMFormFieldValue;
+
+/**
+ * @author Renato Rego
+ */
+public class CheckboxDDMFormFieldValueRendererAccessor
+	extends DDMFormFieldValueRendererAccessor {
+
+	public CheckboxDDMFormFieldValueRendererAccessor(
+		DDMFormFieldValueAccessor<Boolean> ddmFormFieldValueAccessor) {
+
+		_ddmFormFieldValueAccessor = ddmFormFieldValueAccessor;
+	}
+
+	@Override
+	public String get(DDMFormFieldValue ddmFormFieldValue) {
+		Boolean valueBoolean = _ddmFormFieldValueAccessor.get(
+			ddmFormFieldValue);
+
+		if (valueBoolean == Boolean.TRUE) {
+			return LanguageUtil.get(
+				_ddmFormFieldValueAccessor.getLocale(), "yes");
+		}
+		else {
+			return LanguageUtil.get(
+				_ddmFormFieldValueAccessor.getLocale(), "no");
+		}
+	}
+
+	private final DDMFormFieldValueAccessor<Boolean> _ddmFormFieldValueAccessor;
+
+}
