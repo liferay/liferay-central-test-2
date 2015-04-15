b2b21c2bc83b8b781d1a77f225deb2008320b5af
Brian Chan <brian.chan@liferay.com>
2015-04-15 13:28:22 -0700

    LPS-54919 SF

--- modules/apps/layout/layout-set-prototype-web/src/com/liferay/layout/set/prototype/web/portlet/LayoutSetPrototypeViewPortletProvider.java
+++ modules/apps/layout/layout-set-prototype-web/src/com/liferay/layout/set/prototype/web/portlet/LayoutSetPrototypeViewPortletProvider.java
@@ -24,9 +24,7 @@ import org.osgi.service.component.annotations.Component;
  */
 @Component(
 	immediate = true,
-	property = {
-		"model.class.name=com.liferay.portal.model.LayoutSetPrototype"
-	},
+	property = {"model.class.name=com.liferay.portal.model.LayoutSetPrototype"},
 	service = ViewPortletProvider.class
 )
 public class LayoutSetPrototypeViewPortletProvider
