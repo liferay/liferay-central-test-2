71b91a25f6b55cd4228d9c3ec5dd59942ff4f304
adolfo.perez <adolfo.perez@liferay.com>
2015-04-16 10:32:46 +0200

    LPS-54920 Add portlet to display the panel
    This is required for the site admin and user personal panel to look
    exactly the same, as site admin CSS require this to be inside a portlet.

---
+++ modules/apps/productivity-center/productivity-center-web/src/com/liferay/productivity/center/portlet/ProductivityCenterPortlet.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.productivity.center.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.productivity.center.constants.ProductivityCenterPortletKeys;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.hidden",
+		"com.liferay.portlet.instanceable=false",
+		"javax.portlet.init-param.view-template=/portlet/view.jsp",
+		"javax.portlet.name=" + ProductivityCenterPortletKeys.PRODUCTIVITY_CENTER,
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
+public class ProductivityCenterPortlet extends MVCPortlet {
+}
