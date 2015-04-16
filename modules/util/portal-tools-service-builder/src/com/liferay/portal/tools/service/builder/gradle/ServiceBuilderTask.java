1dd1250d79659ece7d3110c35458269d0d871556
Raymond Auge <raymond.auge@liferay.com>
2015-04-16 08:56:44 -0400

    LPS-52594 Gradle

---
+++ modules/util/portal-tools-service-builder/src/com/liferay/portal/tools/service/builder/gradle/ServiceBuilderTask.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.tools.service.builder.gradle;
+
+import com.liferay.portal.tools.service.builder.ServiceBuilder;
+import com.liferay.portal.tools.service.builder.ServiceBuilderBean;
+import com.liferay.portal.tools.service.builder.ServiceBuilderInvoker;
+
+import java.util.Set;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.GradleException;
+import org.gradle.api.Project;
+import org.gradle.api.plugins.ExtensionContainer;
+import org.gradle.api.plugins.ExtraPropertiesExtension;
+import org.gradle.api.tasks.TaskAction;
+
+/**
+ * @author Raymond Augé
+ */
+public class ServiceBuilderTask extends DefaultTask {
+
+	@TaskAction
+	public void buildService() {
+		Project project = getProject();
+
+		ExtensionContainer extensionContainer = project.getExtensions();
+
+		ServiceBuilderBean serviceBuilderBean = extensionContainer.findByType(
+			ServiceBuilderBean.class);
+
+		if (serviceBuilderBean == null) {
+			serviceBuilderBean = new ServiceBuilderBean();
+		}
+
+		try {
+			ServiceBuilder serviceBuilder = ServiceBuilderInvoker.invoke(
+				serviceBuilderBean);
+
+			Set<String> modifiedFiles = serviceBuilder.getModifiedFiles();
+
+			ExtraPropertiesExtension extraProperties =
+				extensionContainer.getExtraProperties();
+
+			extraProperties.set(
+				ServiceBuilder.MODIFIED_FILES_ATTRIBUTE, modifiedFiles);
+		}
+		catch (Exception e) {
+			throw new GradleException(e.getMessage(), e);
+		}
+	}
+
+}
