157b0d10b12ea90106a8dacf7593a2a8e25f4bb5
michael.bradford@liferay.com <michael.bradford@liferay.com>
2015-04-16 12:01:57 -0700

    LPS-54505 - create CssPortalWebResources

--- portal-service/src/com/liferay/portal/kernel/servlet/PortalWebResourceConstants.java
+++ portal-service/src/com/liferay/portal/kernel/servlet/PortalWebResourceConstants.java
@@ -19,6 +19,8 @@ package com.liferay.portal.kernel.servlet;
  */
 public class PortalWebResourceConstants {
 
+	public static final String RESOURCE_TYPE_CSS = "css";
+
 	public static final String RESOURCE_TYPE_EDITORS = "editors";
 
 	public static final String RESOURCE_TYPE_JS = "js";
