90894e7c693bbf41d3a0722807cc7603aaacbb3d
Brian Chan <brian.chan@liferay.com>
2015-04-16 14:29:16 -0700

    LPS-54984 SF

--- portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
@@ -3432,7 +3432,7 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 
 	/**
 	 * @deprecated As of 7.0.0, with no direct replacement
-	 * @throws PortalException 
+	 * @throws PortalException
 	 */
 	@Deprecated
 	@Override
