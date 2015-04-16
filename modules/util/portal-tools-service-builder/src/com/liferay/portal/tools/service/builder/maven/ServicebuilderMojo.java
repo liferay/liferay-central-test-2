36fadbf21fda7004dcd282f2964032317a2f4a65
Raymond Auge <raymond.auge@liferay.com>
2015-04-16 08:40:00 -0400

    LPS-52594 Maven

---
+++ modules/util/portal-tools-service-builder/src/com/liferay/portal/tools/service/builder/maven/ServicebuilderMojo.java
@@ -0,0 +1,179 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.tools.service.builder.maven;
+
+import com.liferay.portal.tools.service.builder.ServiceBuilder;
+import com.liferay.portal.tools.service.builder.ServiceBuilderBean;
+import com.liferay.portal.tools.service.builder.ServiceBuilderInvoker;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * @author Raymond Augé
+ */
+public class ServicebuilderMojo extends AbstractMojo {
+
+	public ServicebuilderMojo() {
+		_serviceBuilderBean = new ServiceBuilderBean();
+	}
+
+	@Override
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		try {
+			ServiceBuilder serviceBuilder = ServiceBuilderInvoker.invoke(
+				_serviceBuilderBean);
+
+			Set<String> modifiedFiles = serviceBuilder.getModifiedFiles();
+
+			Map pluginContext = getPluginContext();
+
+			pluginContext.put("service.modified.files", modifiedFiles);
+		}
+		catch (Exception e) {
+			throw new MojoExecutionException(e.getMessage(), e);
+		}
+	}
+
+	public void setApiDir(String apiDir) {
+		_serviceBuilderBean.setApiDir(apiDir);
+	}
+
+	public void setAutoImportDefaultReferences(
+		boolean autoImportDefaultReferences) {
+
+		_serviceBuilderBean.setAutoImportDefaultReferences(
+			autoImportDefaultReferences);
+	}
+
+	public void setAutoNamespaceTables(boolean autoNamespaceTables) {
+		_serviceBuilderBean.setAutoNamespaceTables(autoNamespaceTables);
+	}
+
+	public void setBeanLocatorUtil(String beanLocatorUtil) {
+		_serviceBuilderBean.setBeanLocatorUtil(beanLocatorUtil);
+	}
+
+	public void setBuildNumber(long buildNumber) {
+		_serviceBuilderBean.setBuildNumber(buildNumber);
+	}
+
+	public void setBuildNumberIncrement(boolean buildNumberIncrement) {
+		_serviceBuilderBean.setBuildNumberIncrement(buildNumberIncrement);
+	}
+
+	public void setHbmFileName(String hbmFileName) {
+		_serviceBuilderBean.setHbmFileName(hbmFileName);
+	}
+
+	public void setImplDir(String implDir) {
+		_serviceBuilderBean.setImplDir(implDir);
+	}
+
+	public void setInputFileName(String inputFileName) {
+		_serviceBuilderBean.setInputFileName(inputFileName);
+	}
+
+	public void setMergeModelHintsConfigs(String mergeModelHintsConfigs) {
+		_serviceBuilderBean.setMergeModelHintsConfigs(mergeModelHintsConfigs);
+	}
+
+	public void setMergeReadOnlyPrefixes(String mergeReadOnlyPrefixes) {
+		_serviceBuilderBean.setMergeReadOnlyPrefixes(mergeReadOnlyPrefixes);
+	}
+
+	public void setMergeResourceActionsConfigs(
+		String mergeResourceActionsConfigs) {
+
+		_serviceBuilderBean.setMergeResourceActionsConfigs(
+			mergeResourceActionsConfigs);
+	}
+
+	public void setModelHintsConfigs(String modelHintsConfigs) {
+		_serviceBuilderBean.setModelHintsConfigs(modelHintsConfigs);
+	}
+
+	public void setModelHintsFileName(String modelHintsFileName) {
+		_serviceBuilderBean.setModelHintsFileName(modelHintsFileName);
+	}
+
+	public void setOsgiModule(boolean osgiModule) {
+		_serviceBuilderBean.setOsgiModule(osgiModule);
+	}
+
+	public void setPluginName(String pluginName) {
+		_serviceBuilderBean.setPluginName(pluginName);
+	}
+
+	public void setPropsUtil(String propsUtil) {
+		_serviceBuilderBean.setPropsUtil(propsUtil);
+	}
+
+	public void setReadOnlyPrefixes(String readOnlyPrefixes) {
+		_serviceBuilderBean.setReadOnlyPrefixes(readOnlyPrefixes);
+	}
+
+	public void setRemotingFileName(String remotingFileName) {
+		_serviceBuilderBean.setRemotingFileName(remotingFileName);
+	}
+
+	public void setResourceActionsConfigs(String resourceActionsConfigs) {
+		_serviceBuilderBean.setResourceActionsConfigs(resourceActionsConfigs);
+	}
+
+	public void setResourcesDir(String resourcesDir) {
+		_serviceBuilderBean.setResourcesDir(resourcesDir);
+	}
+
+	public void setSpringFileName(String springFileName) {
+		_serviceBuilderBean.setSpringFileName(springFileName);
+	}
+
+	public void setSpringNamespaces(String springNamespaces) {
+		_serviceBuilderBean.setSpringNamespaces(springNamespaces);
+	}
+
+	public void setSqlDir(String sqlDir) {
+		_serviceBuilderBean.setSqlDir(sqlDir);
+	}
+
+	public void setSqlFileName(String sqlFileName) {
+		_serviceBuilderBean.setSqlFileName(sqlFileName);
+	}
+
+	public void setSqlIndexesFileName(String sqlIndexesFileName) {
+		_serviceBuilderBean.setSqlIndexesFileName(sqlIndexesFileName);
+	}
+
+	public void setSqlSequencesFileName(String sqlSequencesFileName) {
+		_serviceBuilderBean.setSqlSequencesFileName(sqlSequencesFileName);
+	}
+
+	public void setTargetEntityName(String targetEntityName) {
+		_serviceBuilderBean.setTargetEntityName(targetEntityName);
+	}
+
+	public void setTestDir(String testDir) {
+		_serviceBuilderBean.setTestDir(testDir);
+	}
+
+	private final ServiceBuilderBean _serviceBuilderBean;
+
+}
