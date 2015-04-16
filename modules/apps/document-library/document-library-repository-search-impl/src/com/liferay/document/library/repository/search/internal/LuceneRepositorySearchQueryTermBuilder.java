764bd9d313d25696b3481749d7d5d3c2191f3b70
Michael Han <michael.han@liferay.com>
2015-04-15 21:28:05 -0500

    LPS-54989 move packages

--- modules/apps/document-library/document-library-repository-search-impl/src/com/liferay/document/library/repository/search/internal/LuceneRepositorySearchQueryTermBuilder.java
+++ modules/apps/document-library/document-library-repository-search-impl/src/com/liferay/document/library/repository/search/internal/LuceneRepositorySearchQueryTermBuilder.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.search.lucene.repository;
+package com.liferay.document.library.repository.search.internal;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
