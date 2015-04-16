7d2a801ce113db747a0a5b47b1320e79c66aef62
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-04-16 19:52:56 -0300

    LPS-55044 Autogenerated

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-checkbox/src/META-INF/resources/checkbox.soy.js
@@ -0,0 +1,9 @@
+// This file was automatically generated from checkbox.soy.
+// Please don't edit this file by hand.
+
+if (typeof ddm == 'undefined') { var ddm = {}; }
+
+
+ddm.checkbox = function(opt_data, opt_ignored) {
+return '\t<div class="form-group field-wrapper form-inline" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '"><label for="' + soy.$$escapeHtml(opt_data.name) + '"><input class="field" dir="' + soy.$$escapeHtml(opt_data.dir) + '" id="' + soy.$$escapeHtml(opt_data.name) + '" name="' + soy.$$escapeHtml(opt_data.name) + '" type="checkbox" ' + soy.$$escapeHtml(opt_data.status) + ' value="true" />&nbsp;' + soy.$$escapeHtml(opt_data.label) + '</label>' + soy.$$escapeHtml(opt_data.childElementsHTML) + '</div>';
+};
---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-text/src/META-INF/resources/text.soy.js
@@ -0,0 +1,9 @@
+// This file was automatically generated from text.soy.
+// Please don't edit this file by hand.
+
+if (typeof ddm == 'undefined') { var ddm = {}; }
+
+
+ddm.text = function(opt_data, opt_ignored) {
+return '\t<div class="form-group field-wrapper"><label class="control-fieldLabel" for="' + soy.$$escapeHtml(opt_data.name) + '">' + soy.$$escapeHtml(opt_data.label) + '</label><input class="field form-control" dir="' + soy.$$escapeHtml(opt_data.dir) + '" id="' + soy.$$escapeHtml(opt_data.name) + '" name="' + soy.$$escapeHtml(opt_data.name) + '" type="text" value="' + soy.$$escapeHtml(opt_data.value) + '">' + soy.$$escapeHtml(opt_data.childElementsHTML) + '</div>';
+};
