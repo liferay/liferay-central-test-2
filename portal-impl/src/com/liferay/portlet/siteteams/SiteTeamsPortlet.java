7c49d312be25208f1d07940a4a646edae316d3cb
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-04-16 16:40:15 +0200

    LPS-55053 Copy to site teams portlet, no logic changes

--- portal-impl/src/com/liferay/portlet/siteteams/SiteTeamsPortlet.java
+++ portal-impl/src/com/liferay/portlet/siteteams/SiteTeamsPortlet.java
@@ -44,6 +44,73 @@ public class SiteTeamsPortlet extends MVCPortlet {
 		TeamServiceUtil.deleteTeam(teamId);
 	}
 
+	@Override
+	public void processAction(
+			ActionMapping actionMapping, ActionForm actionForm,
+			PortletConfig portletConfig, ActionRequest actionRequest,
+			ActionResponse actionResponse)
+		throws Exception {
+
+		String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
+
+		try {
+			if (cmd.equals("team_user_groups")) {
+				updateTeamUserGroups(actionRequest);
+			}
+			else if (cmd.equals("team_users")) {
+				updateTeamUsers(actionRequest);
+			}
+
+			if (Validator.isNotNull(cmd)) {
+				String redirect = ParamUtil.getString(
+					actionRequest, "assignmentsRedirect");
+
+				sendRedirect(actionRequest, actionResponse, redirect);
+			}
+		}
+		catch (Exception e) {
+			if (e instanceof NoSuchTeamException ||
+				e instanceof PrincipalException) {
+
+				SessionErrors.add(actionRequest, e.getClass());
+
+				setForward(actionRequest, "portlet.sites_admin.error");
+			}
+			else {
+				throw e;
+			}
+		}
+	}
+
+	@Override
+	public ActionForward render(
+			ActionMapping actionMapping, ActionForm actionForm,
+			PortletConfig portletConfig, RenderRequest renderRequest,
+			RenderResponse renderResponse)
+		throws Exception {
+
+		try {
+			ActionUtil.getTeam(renderRequest);
+		}
+		catch (Exception e) {
+			if (e instanceof NoSuchGroupException ||
+				e instanceof NoSuchTeamException ||
+				e instanceof PrincipalException) {
+
+				SessionErrors.add(renderRequest, e.getClass());
+
+				return actionMapping.findForward("portlet.sites_admin.error");
+			}
+			else {
+				throw e;
+			}
+		}
+
+		return actionMapping.findForward(
+			getForward(
+				renderRequest, "portlet.sites_admin.edit_team_assignments"));
+	}
+
 	public void editTeam(
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
@@ -95,6 +162,34 @@ public class SiteTeamsPortlet extends MVCPortlet {
 		}
 
 		return false;
+	}
+
+	protected void updateTeamUserGroups(ActionRequest actionRequest)
+		throws Exception {
+
+		long teamId = ParamUtil.getLong(actionRequest, "teamId");
+
+		long[] addUserGroupIds = StringUtil.split(
+			ParamUtil.getString(actionRequest, "addUserGroupIds"), 0L);
+		long[] removeUserGroupIds = StringUtil.split(
+			ParamUtil.getString(actionRequest, "removeUserGroupIds"), 0L);
+
+		UserGroupServiceUtil.addTeamUserGroups(teamId, addUserGroupIds);
+		UserGroupServiceUtil.unsetTeamUserGroups(teamId, removeUserGroupIds);
+	}
+
+	protected void updateTeamUsers(ActionRequest actionRequest)
+		throws Exception {
+
+		long teamId = ParamUtil.getLong(actionRequest, "teamId");
+
+		long[] addUserIds = StringUtil.split(
+			ParamUtil.getString(actionRequest, "addUserIds"), 0L);
+		long[] removeUserIds = StringUtil.split(
+			ParamUtil.getString(actionRequest, "removeUserIds"), 0L);
+
+		UserServiceUtil.addTeamUsers(teamId, addUserIds);
+		UserServiceUtil.unsetTeamUsers(teamId, removeUserIds);
 	}
 
 }
