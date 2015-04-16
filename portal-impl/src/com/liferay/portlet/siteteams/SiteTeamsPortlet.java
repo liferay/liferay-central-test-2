50dbea3eace415c0b0dcdf0ed35516904c7a6a6c
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-04-16 16:21:43 +0200

    LPS-55053 Make it works as MVCPortlet

--- portal-impl/src/com/liferay/portlet/siteteams/SiteTeamsPortlet.java
+++ portal-impl/src/com/liferay/portlet/siteteams/SiteTeamsPortlet.java
@@ -15,115 +15,39 @@
 package com.liferay.portlet.siteteams;
 
 import com.liferay.portal.DuplicateTeamException;
-import com.liferay.portal.NoSuchGroupException;
 import com.liferay.portal.NoSuchTeamException;
 import com.liferay.portal.TeamNameException;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.servlet.SessionErrors;
-import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.security.auth.PrincipalException;
 import com.liferay.portal.service.TeamServiceUtil;
-import com.liferay.portal.struts.PortletAction;
-import com.liferay.portal.util.PortalUtil;
-import com.liferay.portlet.sites.action.ActionUtil;
+
+import java.io.IOException;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
-import javax.portlet.PortletConfig;
+import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
-
-import org.apache.struts.action.ActionForm;
-import org.apache.struts.action.ActionForward;
-import org.apache.struts.action.ActionMapping;
 
 /**
  * @author Brian Wing Shun Chan
  */
-public class SiteTeamsPortlet extends PortletAction {
+public class SiteTeamsPortlet extends MVCPortlet {
 
-	public void deleteTeam(ActionRequest actionRequest) throws Exception {
+	public void deleteTeam(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
 		long teamId = ParamUtil.getLong(actionRequest, "teamId");
 
 		TeamServiceUtil.deleteTeam(teamId);
 	}
 
-	@Override
-	public void processAction(
-			ActionMapping actionMapping, ActionForm actionForm,
-			PortletConfig portletConfig, ActionRequest actionRequest,
-			ActionResponse actionResponse)
+	public void editTeam(
+			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
 
-		String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
-
-		try {
-			if (cmd.equals(Constants.ADD) || cmd.equals(Constants.UPDATE)) {
-				updateTeam(actionRequest);
-			}
-			else if (cmd.equals(Constants.DELETE)) {
-				deleteTeam(actionRequest);
-			}
-
-			sendRedirect(actionRequest, actionResponse);
-		}
-		catch (Exception e) {
-			if (e instanceof PrincipalException) {
-				SessionErrors.add(actionRequest, e.getClass());
-
-				setForward(actionRequest, "portlet.sites_admin.error");
-			}
-			else if (e instanceof DuplicateTeamException ||
-					 e instanceof NoSuchTeamException ||
-					 e instanceof TeamNameException) {
-
-				SessionErrors.add(actionRequest, e.getClass());
-
-				if (cmd.equals(Constants.DELETE)) {
-					String redirect = PortalUtil.escapeRedirect(
-						ParamUtil.getString(actionRequest, "redirect"));
-
-					if (Validator.isNotNull(redirect)) {
-						actionResponse.sendRedirect(redirect);
-					}
-				}
-			}
-			else {
-				throw e;
-			}
-		}
-	}
-
-	@Override
-	public ActionForward render(
-			ActionMapping actionMapping, ActionForm actionForm,
-			PortletConfig portletConfig, RenderRequest renderRequest,
-			RenderResponse renderResponse)
-		throws Exception {
-
-		try {
-			ActionUtil.getTeam(renderRequest);
-		}
-		catch (Exception e) {
-			if (e instanceof NoSuchGroupException ||
-				e instanceof NoSuchTeamException ||
-				e instanceof PrincipalException) {
-
-				SessionErrors.add(renderRequest, e.getClass());
-
-				return actionMapping.findForward("portlet.sites_admin.error");
-			}
-			else {
-				throw e;
-			}
-		}
-
-		return actionMapping.findForward(
-			getForward(renderRequest, "portlet.sites_admin.edit_team"));
-	}
-
-	public void updateTeam(ActionRequest actionRequest) throws Exception {
 		long teamId = ParamUtil.getLong(actionRequest, "teamId");
 
 		long groupId = ParamUtil.getLong(actionRequest, "groupId");
@@ -142,6 +66,35 @@ public class SiteTeamsPortlet extends PortletAction {
 
 			TeamServiceUtil.updateTeam(teamId, name, description);
 		}
+	}
+
+	@Override
+	protected void doDispatch(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		if (SessionErrors.contains(
+				renderRequest, PrincipalException.class.getName())) {
+
+			include("/error.jsp", renderRequest, renderResponse);
+		}
+		else {
+			super.doDispatch(renderRequest, renderResponse);
+		}
+	}
+
+	@Override
+	protected boolean isSessionErrorException(Throwable cause) {
+		if (cause instanceof DuplicateTeamException ||
+			cause instanceof NoSuchTeamException ||
+			cause instanceof PrincipalException ||
+			cause instanceof TeamNameException ||
+			super.isSessionErrorException(cause)) {
+
+			return true;
+		}
+
+		return false;
 	}
 
 }
