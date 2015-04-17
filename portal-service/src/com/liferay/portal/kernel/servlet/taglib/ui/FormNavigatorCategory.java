c320da639a9af50c8313b481ae7ee7bb1d5b145b
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-04-17 11:01:26 +0200

    LPS-54903 Javadoc for the interfaces

--- portal-service/src/com/liferay/portal/kernel/servlet/taglib/ui/FormNavigatorCategory.java
+++ portal-service/src/com/liferay/portal/kernel/servlet/taglib/ui/FormNavigatorCategory.java
@@ -17,14 +17,49 @@ package com.liferay.portal.kernel.servlet.taglib.ui;
 import java.util.Locale;
 
 /**
+ * Provides an interface defining categories that will be used by a specific
+ * <code>liferay-ui:form-navigator</code> taglib instance to render a new
+ * category. Form navigator categories includes form navigator entries, defined
+ * by {@link FormNavigatorEntry} implementations.
+ *
+ * <p>
+ * Implementations must be registered in the OSGI Registry. The order of the
+ * form navigator categories is determined by the service ranking.
+ * </p>
+ *
  * @author Sergio González
  */
 public interface FormNavigatorCategory {
 
+	/**
+	 * Defines the form navigator ID where the form navigator category will be
+	 * included. This ID must match the ID attribute of the
+	 * <code>liferay-ui:form-navigator</code> taglib where this form navigator
+	 * category is to be included.
+	 *
+	 * @return the form navigator ID where the form navigator category will be
+	 *         included.
+	 */
 	public String getFormNavigatorId();
 
+	/**
+	 * Defines a key for the form navigator category. This key needs to be
+	 * unique in the scope of a form navigator ID.
+	 *
+	 * This key will be referred by the form navigator entries to be added to
+	 * this form navigator category.
+	 *
+	 * @return the key of the form navigator category.
+	 */
 	public String getKey();
 
+	/**
+	 * Defines the label that will be displayed in the user interface when the
+	 * form navigator category is included in the form navigator.
+	 *
+	 * @param  locale the locale that the label should be retrieved for
+	 * @return the label of the form navigator category.
+	 */
 	public String getLabel(Locale locale);
 
 }
--- portal-service/src/com/liferay/portal/kernel/servlet/taglib/ui/FormNavigatorEntry.java
+++ portal-service/src/com/liferay/portal/kernel/servlet/taglib/ui/FormNavigatorEntry.java
@@ -24,20 +24,75 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
+ * Provides an interface defining entries that will be used by a specific
+ * <code>liferay-ui:form-navigator</code> taglib instance to render a new
+ * section. Form navigator entries are included within form navigator
+ * categories, defined by {@link FormNavigatorCategory} implementations.
+ *
+ * <p>
+ * Implementations must be registered in the OSGI Registry. The order of the
+ * form navigator entries inside a category is determined by the service
+ * ranking.
+ * </p>
+ *
  * @author Sergio González
  */
 public interface FormNavigatorEntry<T> {
 
+	/**
+	 * Defines the category key where the form navigator entry will be included.
+	 *
+	 * @return the category key where the form navigator entry will be included.
+	 */
 	public String getCategoryKey();
 
+	/**
+	 * Defines the form navigator ID where the form navigator entry will be
+	 * included. This ID must match the ID attribute of the
+	 * <code>liferay-ui:form-navigator</code> taglib where this form navigator
+	 * entry is to be included.
+	 *
+	 * @return the form navigator ID where the form navigator entry will be
+	 *         included.
+	 */
 	public String getFormNavigatorId();
 
+	/**
+	 * Defines a key for the form navigator entry. This key needs to be unique
+	 * in the scope of a category key and form navigator ID.
+	 *
+	 * @return the key of the form navigator entry.
+	 */
 	public String getKey();
 
+	/**
+	 * Defines the label that will be displayed in the user interface when the
+	 * form navigator entry is included in the form navigator.
+	 *
+	 * @param  locale the locale that the label should be retrieved for
+	 * @return the label of the form navigator entry.
+	 */
 	public String getLabel(Locale locale);
 
+	/**
+	 * Defines the label that will be displayed in the user interface when the
+	 * form navigator entry is included in the form navigator.
+	 *
+	 * @param  user the user viewing the form navigator entry
+	 * @param  formModelBean the bean edited by the form navigator
+	 * @return whether the form navigator entry should be displayed or not.
+	 */
 	public boolean isVisible(User user, T formModelBean);
 
+	/**
+	 * Renders the HTML that needs to be displayed when the form navigator entry
+	 * is displayed.
+	 *
+	 * @param request the request with which the form navigator entry is
+	 *        rendered
+	 * @param response the response with which the form navigator entry is
+	 *        rendered
+	 */
 	public void render(HttpServletRequest request, HttpServletResponse response)
 		throws IOException;
 
