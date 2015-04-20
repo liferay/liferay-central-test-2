b61de41e696f8984fea0e004b576ec109bfa9fc2
Iliyan Peychev <iliyan.peychev@liferay.com>
2015-04-20 13:30:15 +0200

    LPS-55018 Regenerate bbcode parser

--- modules/frontend/frontend-js-web/src/META-INF/resources/html/js/editor/ckeditor_diffs/plugins/bbcode/bbcode_parser.js
+++ modules/frontend/frontend-js-web/src/META-INF/resources/html/js/editor/ckeditor_diffs/plugins/bbcode/bbcode_parser.js
@@ -271,6 +271,8 @@
 
 	Liferay.BBCodeParser = Parser;
 })();;(function() {
+	var A = AUI();
+
 	var BBCodeUtil = Liferay.BBCodeUtil;
 	var CKTools = CKEDITOR.tools;
 
@@ -378,11 +380,15 @@
 
 	var STR_NEW_LINE = '\n';
 
+	var STR_TAG_A_CLOSE = '</a>';
+
 	var STR_TAG_ATTR_CLOSE = '">';
 
 	var STR_TAG_ATTR_HREF_OPEN = '<a href="';
 
-	var STR_TAG_A_CLOSE = '</a>';
+	var STR_TAG_DIV_CLOSE = '</div>';
+
+	var STR_TAG_DIV_STYLE_OPEN = '<div style="';
 
 	var STR_TAG_END_CLOSE = '>';
 
@@ -397,8 +403,6 @@
 	var STR_TAG_SPAN_CLOSE = '</span>';
 
 	var STR_TAG_SPAN_STYLE_OPEN = '<span style="';
-
-	var STR_TAG_DIV_STYLE_OPEN = '<div style="';
 
 	var STR_TAG_URL = 'url';
 
@@ -486,7 +490,6 @@
 				if (token.type == TOKEN_DATA) {
 					result.push(token.value);
 				}
-
 			}
 			while ((token.type != TOKEN_TAG_END) && (token.value != toTagName));
 
@@ -624,6 +627,8 @@
 			var indent = token.attribute;
 
 			instance._result.push(STR_TAG_DIV_STYLE_OPEN, 'margin-left: ', indent, 'px;', STR_TAG_ATTR_CLOSE);
+
+			instance._stack.push(STR_TAG_DIV_CLOSE);
 		},
 
 		_handleList: function(token) {
