ff139c129d19fa15ebd030d830daec484dbd5022
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2015-04-22 01:05:13 +0200

    LPS-54811 Wrong method

--- modules/apps/wiki/wiki-service/src/com/liferay/wiki/service/impl/WikiNodeLocalServiceImpl.java
+++ modules/apps/wiki/wiki-service/src/com/liferay/wiki/service/impl/WikiNodeLocalServiceImpl.java
@@ -519,7 +519,7 @@ public class WikiNodeLocalServiceImpl extends WikiNodeLocalServiceBaseImpl {
 	protected void moveDependentsToTrash(long nodeId, long trashEntryId)
 		throws PortalException {
 
-		List<WikiPage> pages = wikiPagePersistence.findByNodeId(nodeId);
+		List<WikiPage> pages = wikiPagePersistence.findByN_H(nodeId);
 
 		for (WikiPage page : pages) {
 			wikiPageLocalService.moveDependentToTrash(page, trashEntryId);
