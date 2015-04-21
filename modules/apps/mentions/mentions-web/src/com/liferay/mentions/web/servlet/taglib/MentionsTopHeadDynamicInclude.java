4e521025f077d7ae17217ae02271b7e26be75332
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-04-21 15:49:18 +0200

    LPS-55092 Include css

---
+++ modules/apps/mentions/mentions-web/src/com/liferay/mentions/web/servlet/taglib/MentionsTopHeadDynamicInclude.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mentions.web.servlet.taglib;
+
+import com.liferay.portal.kernel.servlet.taglib.BaseDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.theme.ThemeDisplay;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Sergio González
+ */
+@Component(immediate = true, service = DynamicInclude.class)
+public class MentionsTopHeadDynamicInclude extends BaseDynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest request, HttpServletResponse response,
+			String key)
+		throws IOException {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		PrintWriter printWriter = response.getWriter();
+
+		StringBundler sb = new StringBundler();
+
+		sb.append("<link href=\"");
+		sb.append(themeDisplay.getPortalURL());
+		sb.append(themeDisplay.getPathContext());
+		sb.append("/o/comliferaymentionsweb/css/mentions.css");
+		sb.append("\" rel=\"stylesheet\" type = \"text/css\" />");
+
+		printWriter.println(sb.toString());
+	}
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register("/html/common/themes/top_head-ext.jsp");
+	}
+
+}
