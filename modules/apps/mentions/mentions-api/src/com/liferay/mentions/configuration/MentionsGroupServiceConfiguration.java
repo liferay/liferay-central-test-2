a4fe956b7b868b2cc64586f7e05034e186f2e209
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-04-21 10:11:45 +0200

    LPS-55092 Move properties to settings configuration

---
+++ modules/apps/mentions/mentions-api/src/com/liferay/mentions/configuration/MentionsGroupServiceConfiguration.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mentions.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.kernel.settings.LocalizedValuesMap;
+
+/**
+ * @author Sergio González
+ */
+@Meta.OCD(
+	id = "com.liferay.mentions.configuration.MentionsGroupServiceConfiguration"
+)
+public interface MentionsGroupServiceConfiguration {
+
+	@Meta.AD(
+		deflt = "${resource:com/liferay/mentions/configuration/dependencies/asset_entry_mention_email_body.tmpl}",
+		required = false
+	)
+	public LocalizedValuesMap assetEntryMentionEmailBody();
+
+	@Meta.AD(
+		deflt = "${resource:com/liferay/mentions/configuration/dependencies/asset_entry_mention_email_subject.tmpl}",
+		required = false
+	)
+	public LocalizedValuesMap assetEntryMentionEmailSubject();
+
+	@Meta.AD(
+		deflt = "${resource:com/liferay/mentions/configuration/dependencies/comment_mention_email_body.tmpl}",
+		required = false
+	)
+	public LocalizedValuesMap commentMentionEmailBody();
+
+	@Meta.AD(
+		deflt = "${resource:com/liferay/mentions/configuration/dependencies/comment_mention_email_subject.tmpl}",
+		required = false
+	)
+	public LocalizedValuesMap commentMentionEmailSubject();
+
+}
