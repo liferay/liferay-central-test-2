66cd07a50064a6284a491f43d096dc1913af9c37
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-04-22 18:23:57 -0300

    LPS-55044 ran ant build-soy

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-text/src/META-INF/resources/text.soy.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-text/src/META-INF/resources/text.soy.js
@@ -5,5 +5,5 @@ if (typeof ddm == 'undefined') { var ddm = {}; }
 
 
 ddm.text = function(opt_data, opt_ignored) {
-return '\t<div class="form-group field-wrapper"><label class="control-fieldLabel" for="' + soy.$$escapeHtml(opt_data.name) + '">' + soy.$$escapeHtml(opt_data.label) + '</label><input class="field form-control" dir="' + soy.$$escapeHtml(opt_data.dir) + '" id="' + soy.$$escapeHtml(opt_data.name) + '" name="' + soy.$$escapeHtml(opt_data.name) + '" type="text" value="' + soy.$$escapeHtml(opt_data.value) + '">' + soy.$$escapeHtml(opt_data.childElementsHTML) + '</div>';
+return '\t<div class="form-group field-wrapper" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '"><label class="control-fieldLabel" for="' + soy.$$escapeHtml(opt_data.name) + '">' + soy.$$escapeHtml(opt_data.label) + '</label><input class="field form-control" dir="' + soy.$$escapeHtml(opt_data.dir) + '" id="' + soy.$$escapeHtml(opt_data.name) + '" name="' + soy.$$escapeHtml(opt_data.name) + '" type="text" value="' + soy.$$escapeHtml(opt_data.value) + '">' + soy.$$filterNoAutoescape(opt_data.childElementsHTML) + '</div>';
 };
