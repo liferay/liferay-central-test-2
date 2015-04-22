6d4dcf81e3dbe2d62d35f3b5169975ae5bf41a4a
philip.jones <philip.jones@liferay.com>
2015-04-22 16:40:07 -0700

    LPS-53802 LayoutTypeControllerTrackerTest Integration point test

---
+++ portal-impl/test/integration/com/liferay/portal/util/bundle/layouttypecontrollertracker/TestLayoutTypeController.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.util.bundle.layouttypecontrollertracker;
+
+import com.liferay.portal.model.Layout;
+import com.liferay.portal.model.LayoutTypeController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Phil Jones
+ */
+@Component(
+		immediate = true,
+		property = {"layout.type=testLayoutTypeController" + Integer.MAX_VALUE,
+				    "service.ranking:Integer="}
+	)
+public class TestLayoutTypeController implements LayoutTypeController {
+
+	@Override
+	public String[] getConfigurationActionDelete() {
+		
+		return null;
+	}
+
+	@Override
+	public String[] getConfigurationActionUpdate() {
+		
+		return null;
+	}
+
+	@Override
+	public String getURL() {
+		return null;
+	}
+
+	@Override
+	public String includeEditContent(
+		HttpServletRequest request,	HttpServletResponse response, 
+			Layout layout) {
+
+		return null;
+	}
+
+	@Override
+	public boolean includeLayoutContent(HttpServletRequest request,
+			HttpServletResponse response, Layout layout) {
+
+		return false;
+	}
+
+	@Override
+	public boolean isFirstPageable() {
+		return false;
+	}
+
+	@Override
+	public boolean isParentable() {
+		return false;
+	}
+
+	@Override
+	public boolean isSitemapable() {
+		return false;
+	}
+
+	@Override
+	public boolean isURLFriendliable() {
+		return false;
+	}
+
+	@Override
+	public boolean matches(HttpServletRequest request, String friendlyURL,
+			Layout layout) {
+
+		return false;
+	}
+
+}
