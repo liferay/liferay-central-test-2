e0db957fe5b65da978354acea9c5c36b166ae1a8
Cristina González Castellano <cristina.gonzalez@liferay.com>
2015-04-22 14:08:18 -0700

    LPS-55159 Add copyright and author

--- modules/test/cobertura-agent/src/com/liferay/cobertura/agent/instrumentation/SwitchHolder.java
+++ modules/test/cobertura-agent/src/com/liferay/cobertura/agent/instrumentation/SwitchHolder.java
@@ -1,26 +1,22 @@
-/*
- * Cobertura - http://cobertura.sourceforge.net/
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
  *
- * Copyright (C) 2006 Jiri Mares
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
  *
- * Cobertura is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published
- * by the Free Software Foundation; either version 2 of the License,
- * or (at your option) any later version.
- *
- * Cobertura is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Cobertura; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
- * USA
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  */
 
 package com.liferay.cobertura.agent.instrumentation;
 
+/**
+ * @author Cristina González
+ */
 public class SwitchHolder extends JumpHolder {
    protected int branch;
 
