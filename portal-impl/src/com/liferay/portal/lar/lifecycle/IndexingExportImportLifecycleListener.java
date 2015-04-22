eb0194c8d4412c3d253b8d964a0dfc705f0721b3
Daniel Kocsis <daniel.kocsis@liferay.com>
2015-04-22 15:08:54 +0200

    LPS-55034 Source formatting

--- portal-impl/src/com/liferay/portal/lar/lifecycle/IndexingExportImportLifecycleListener.java
+++ portal-impl/src/com/liferay/portal/lar/lifecycle/IndexingExportImportLifecycleListener.java
@@ -14,9 +14,15 @@
 
 package com.liferay.portal.lar.lifecycle;
 
+import static com.liferay.portal.kernel.lar.lifecycle.ExportImportLifecycleConstants.EVENT_LAYOUT_IMPORT_SUCCEEDED;
+import static com.liferay.portal.kernel.lar.lifecycle.ExportImportLifecycleConstants.EVENT_PORTLET_IMPORT_SUCCEEDED;
+import static com.liferay.portal.kernel.lar.lifecycle.ExportImportLifecycleConstants.PROCESS_FLAG_LAYOUT_IMPORT_IN_PROCESS;
+import static com.liferay.portal.kernel.lar.lifecycle.ExportImportLifecycleConstants.PROCESS_FLAG_LAYOUT_STAGING_IN_PROCESS;
+import static com.liferay.portal.kernel.lar.lifecycle.ExportImportLifecycleConstants.PROCESS_FLAG_PORTLET_IMPORT_IN_PROCESS;
+import static com.liferay.portal.kernel.lar.lifecycle.ExportImportLifecycleConstants.PROCESS_FLAG_PORTLET_STAGING_IN_PROCESS;
+
 import com.liferay.portal.kernel.lar.PortletDataContext;
 import com.liferay.portal.kernel.lar.PortletDataHandlerKeys;
-import com.liferay.portal.kernel.lar.lifecycle.ExportImportLifecycleConstants;
 import com.liferay.portal.kernel.lar.lifecycle.ExportImportLifecycleEvent;
 import com.liferay.portal.kernel.lar.lifecycle.ExportImportLifecycleListener;
 import com.liferay.portal.kernel.log.Log;
@@ -50,16 +56,17 @@ public class IndexingExportImportLifecycleListener
 		throws Exception {
 
 		if (((exportImportLifecycleEvent.getCode() !=
-				ExportImportLifecycleConstants.EVENT_LAYOUT_IMPORT_SUCCEEDED) ||
-			 !(exportImportLifecycleEvent.getProcessFlag() ==
-				 ExportImportLifecycleConstants.
-					 PROCESS_FLAG_LAYOUT_IMPORT_IN_PROCESS)) &&
+				EVENT_LAYOUT_IMPORT_SUCCEEDED) ||
+			 ((exportImportLifecycleEvent.getProcessFlag() !=
+				 PROCESS_FLAG_LAYOUT_IMPORT_IN_PROCESS) &&
+			  (exportImportLifecycleEvent.getProcessFlag() !=
+				  PROCESS_FLAG_LAYOUT_STAGING_IN_PROCESS))) &&
 			((exportImportLifecycleEvent.getCode() !=
-				ExportImportLifecycleConstants.
-					EVENT_PORTLET_IMPORT_SUCCEEDED) ||
-			 !(exportImportLifecycleEvent.getProcessFlag() ==
-				 ExportImportLifecycleConstants.
-					 PROCESS_FLAG_PORTLET_IMPORT_IN_PROCESS))) {
+				EVENT_PORTLET_IMPORT_SUCCEEDED) ||
+			 ((exportImportLifecycleEvent.getProcessFlag() !=
+				 PROCESS_FLAG_PORTLET_IMPORT_IN_PROCESS) &&
+			  (exportImportLifecycleEvent.getProcessFlag() !=
+				  PROCESS_FLAG_PORTLET_STAGING_IN_PROCESS)))) {
 
 			return;
 		}
