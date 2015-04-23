58e8cba311cbeb2fbdfd33b79b336352eff0f8cb
shuyangzhou <shuyang.zhou@liferay.com>
2015-04-23 11:33:38 -0700

    LPS-55159 This apparently is a cobertura bug, doing equals logic without consulting super.equals() first.

--- modules/test/cobertura-agent/src/com/liferay/cobertura/agent/coveragedata/TouchCollector.java
+++ modules/test/cobertura-agent/src/com/liferay/cobertura/agent/coveragedata/TouchCollector.java
@@ -116,7 +116,8 @@ public class TouchCollector {
 		public boolean equals(Object obj) {
 			JumpTouchData jumpTouchData = (JumpTouchData)obj;
 
-			if ((_branch == jumpTouchData._branch) &&
+			if (super.equals(jumpTouchData) &&
+				(_branch == jumpTouchData._branch) &&
 				(_branchNumber == jumpTouchData._branchNumber)) {
 
 				return true;
@@ -201,7 +202,8 @@ public class TouchCollector {
 		public boolean equals(Object obj) {
 			SwitchTouchData switchTouchData = (SwitchTouchData)obj;
 
-			if ((_branch == switchTouchData._branch) &&
+			if (super.equals(switchTouchData) &&
+				(_branch == switchTouchData._branch) &&
 				(_switchNumber == switchTouchData._switchNumber)) {
 
 				return true;
