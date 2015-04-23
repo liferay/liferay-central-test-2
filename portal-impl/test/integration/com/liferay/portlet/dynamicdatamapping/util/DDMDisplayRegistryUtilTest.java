9cbc9f12708ce20ffc9fd6283302bb16f06e76fc
peterfellwock <peter.fellwock@liferay.com>
2015-04-23 16:44:07 -0700

    LPS-53802 DDMDisplayRegistryUtilTest Integration point test

---
+++ portal-impl/test/integration/com/liferay/portlet/dynamicdatamapping/util/DDMDisplayRegistryUtilTest.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.dynamicdatamapping.util;
+
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.MainServletTestRule;
+import com.liferay.portal.test.rule.SyntheticBundleRule;
+import com.liferay.portlet.dynamicdatamapping.util.bundle.ddmdisplayregistryutil.TestDDMDisplay;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Peter Fellwock
+ */
+public class DDMDisplayRegistryUtilTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE,
+			new SyntheticBundleRule("bundle.ddmdisplayregistryutil"));
+
+	@Test
+	public void testGetDDMDisplay() {
+		String testClassName = TestDDMDisplay.class.getName();
+
+		DDMDisplay dDMDisplay = DDMDisplayRegistryUtil.getDDMDisplay(
+			testClassName);
+
+		Class<?> clazz = dDMDisplay.getClass();
+
+		Assert.assertEquals(testClassName, clazz.getName());
+	}
+
+	@Test
+	public void testGetDDMDisplays() {
+		String testClassName = TestDDMDisplay.class.getName();
+
+		List<DDMDisplay> dDMDisplays = DDMDisplayRegistryUtil.getDDMDisplays();
+
+		for (DDMDisplay dDMDisplay : dDMDisplays) {
+			Class<?> clazz = dDMDisplay.getClass();
+
+			if (testClassName.equals(clazz.getName())) {
+				return;
+			}
+		}
+
+		Assert.fail();
+	}
+
+	@Test
+	public void testGetPortletIds() {
+		String testClassName = TestDDMDisplay.class.getName();
+
+		String[] portletIds = DDMDisplayRegistryUtil.getPortletIds();
+
+		for (String portletId : portletIds) {
+			if (testClassName.equals(portletId)) {
+				return;
+			}
+		}
+
+		Assert.fail();
+	}
+
+}
---
+++ portal-impl/test/integration/com/liferay/portlet/dynamicdatamapping/util/bundle/ddmdisplayregistryutil/TestDDMDisplay.java
@@ -0,0 +1,188 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.dynamicdatamapping.util.bundle.ddmdisplayregistryutil;
+
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.dynamicdatamapping.model.DDMStructure;
+import com.liferay.portlet.dynamicdatamapping.model.DDMTemplate;
+import com.liferay.portlet.dynamicdatamapping.util.DDMDisplay;
+import com.liferay.portlet.dynamicdatamapping.util.DDMPermissionHandler;
+
+import java.util.Locale;
+import java.util.Set;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Peter Fellwock
+ */
+@Component(
+	immediate = true,
+	property = {"service.ranking:Integer=" + Integer.MAX_VALUE}
+)
+public class TestDDMDisplay implements DDMDisplay {
+
+	@Override
+	public String getAvailableFields() {
+		return null;
+	}
+
+	@Override
+	public DDMPermissionHandler getDDMPermissionHandler() {
+		return null;
+	}
+
+	@Override
+	public String getEditStructureDefaultValuesURL(
+		LiferayPortletRequest liferayPortletRequest,
+		LiferayPortletResponse liferayPortletResponse, DDMStructure structure,
+		String redirectURL, String backURL) {
+
+		return null;
+	}
+
+	@Override
+	public String getEditTemplateBackURL(
+		LiferayPortletRequest liferayPortletRequest,
+		LiferayPortletResponse liferayPortletResponse, long classNameId,
+		long classPK, String portletResource) {
+
+		return null;
+	}
+
+	@Override
+	public String getEditTemplateTitle(
+		DDMStructure structure, DDMTemplate template, Locale locale) {
+
+		return null;
+	}
+
+	@Override
+	public String getEditTemplateTitle(long classNameId, Locale locale) {
+		return null;
+	}
+
+	@Override
+	public String getPortletId() {
+		return TestDDMDisplay.class.getName();
+	}
+
+	@Override
+	public String getStorageType() {
+		return null;
+	}
+
+	@Override
+	public String getStructureName(Locale locale) {
+		return null;
+	}
+
+	@Override
+	public String getStructureType() {
+		return null;
+	}
+
+	@Override
+	public long[] getTemplateClassNameIds(long classNameId) {
+		return null;
+	}
+
+	@Override
+	public long[] getTemplateClassPKs(
+		long companyId, long classNameId, long classPK) {
+
+		return null;
+	}
+
+	@Override
+	public long[] getTemplateGroupIds(
+		ThemeDisplay themeDisplay, boolean includeAncestorTemplates) {
+
+		return null;
+	}
+
+	@Override
+	public long getTemplateHandlerClassNameId(
+		DDMTemplate template, long classNameId) {
+
+		return 0;
+	}
+
+	@Override
+	public Set<String> getTemplateLanguageTypes() {
+		return null;
+	}
+
+	@Override
+	public String getTemplateMode() {
+		return null;
+	}
+
+	@Override
+	public String getTemplateType() {
+		return null;
+	}
+
+	@Override
+	public String getTemplateType(DDMTemplate template, Locale locale) {
+		return null;
+	}
+
+	@Override
+	public String getViewTemplatesBackURL(
+		LiferayPortletRequest liferayPortletRequest,
+		LiferayPortletResponse liferayPortletResponse, long classPK) {
+
+		return null;
+	}
+
+	@Override
+	public Set<String> getViewTemplatesExcludedColumnNames() {
+		return null;
+	}
+
+	@Override
+	public String getViewTemplatesTitle(
+		DDMStructure structure, boolean controlPanel, boolean search,
+		Locale locale) {
+
+		return null;
+	}
+
+	@Override
+	public String getViewTemplatesTitle(
+		DDMStructure structure, boolean controlPanel, Locale locale) {
+
+		return null;
+	}
+
+	@Override
+	public String getViewTemplatesTitle(DDMStructure structure, Locale locale) {
+		return null;
+	}
+
+	@Override
+	public boolean isShowAddStructureButton() {
+		return false;
+	}
+
+	@Override
+	public boolean isShowStructureSelector() {
+		return false;
+	}
+
+}
