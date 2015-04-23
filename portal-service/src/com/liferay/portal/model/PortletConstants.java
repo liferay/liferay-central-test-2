8fb51dc6f5d549e60757db1e58be6e1419c504c9
Jim Hinkey <james.hinkey@liferay.com>
2015-04-23 12:54:27 -0400

    LPS-53947 Edit Javadoc

--- portal-service/src/com/liferay/portal/model/PortletConstants.java
+++ portal-service/src/com/liferay/portal/model/PortletConstants.java
@@ -104,8 +104,8 @@ public class PortletConstants {
 	 *
 	 * @param  portletId the portlet ID
 	 * @param  userId the user ID
-	 * @param  instanceId an instance ID. If null the method will try to obtain
-	 *         the instanceId form the portletId.
+	 * @param  instanceId an instance ID. If <code>null</code>, an instance ID
+	 *         is derived from the portlet ID.
 	 * @return the properly assembled portlet ID
 	 */
 	public static String assemblePortletId(
