9d150d65ef9276f1d65aaf9d0d9120b00800863e
tinatian <tina.tian@liferay.com>
2015-04-24 16:15:03 +0800

    LPS-55178 Remove unused code

--- portal-impl/src/com/liferay/portal/spring/hibernate/PortalHibernateConfiguration.java
+++ portal-impl/src/com/liferay/portal/spring/hibernate/PortalHibernateConfiguration.java
@@ -22,7 +22,6 @@ import com.liferay.portal.kernel.dao.db.DBFactoryUtil;
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.resiliency.spi.SPIUtil;
 import com.liferay.portal.kernel.util.Converter;
 import com.liferay.portal.kernel.util.PreloadClassLoader;
 import com.liferay.portal.kernel.util.PropsKeys;
@@ -144,15 +143,6 @@ public class PortalHibernateConfiguration extends LocalSessionFactoryBean {
 			}
 
 			Properties properties = PropsUtil.getProperties();
-
-			if (SPIUtil.isSPI()) {
-				properties.put(
-					"hibernate.cache.use_query_cache",
-					Boolean.FALSE.toString());
-				properties.put(
-					"hibernate.cache.use_second_level_cache",
-					Boolean.FALSE.toString());
-			}
 
 			configuration.setProperties(properties);
 
--- portal-service/src/com/liferay/portal/kernel/cache/PortalCacheManagerNames.java
+++ portal-service/src/com/liferay/portal/kernel/cache/PortalCacheManagerNames.java
@@ -19,8 +19,6 @@ package com.liferay.portal.kernel.cache;
  */
 public interface PortalCacheManagerNames {
 
-	public static final String HIBERNATE = "HIBERNATE_PORTAL_CACHE_MANAGER";
-
 	public static final String MULTI_VM = "MULTI_VM_PORTAL_CACHE_MANAGER";
 
 	public static final String SINGLE_VM = "SINGLE_VM_PORTAL_CACHE_MANAGER";
