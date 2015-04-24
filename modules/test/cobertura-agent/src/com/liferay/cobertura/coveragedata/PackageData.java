a4252866afc467eaa9ab9e4bda7ef4c0b00c6836
shuyangzhou <shuyang.zhou@liferay.com>
2015-04-24 10:34:02 -0700

    LPS-55159 Manual SF

--- modules/test/cobertura-agent/src/com/liferay/cobertura/coveragedata/PackageData.java
+++ modules/test/cobertura-agent/src/com/liferay/cobertura/coveragedata/PackageData.java
@@ -1,10 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.cobertura.coveragedata;
 
 import net.sourceforge.cobertura.coveragedata.ClassData;
+
+/**
+ * @author Shuyang Zhou
+ */
 public class PackageData extends CoverageDataContainer {
 
 	public PackageData(String name) {
-		this.name = name;
+		_name = name;
 	}
 
 	public ClassData addClassData(ClassData classData) {
@@ -30,7 +48,7 @@ public class PackageData extends CoverageDataContainer {
 
 		PackageData packageData = (PackageData)obj;
 
-		if (name.equals(packageData.name) && super.equals(packageData)) {
+		if (_name.equals(packageData._name) && super.equals(packageData)) {
 			return true;
 		}
 
@@ -39,11 +57,11 @@ public class PackageData extends CoverageDataContainer {
 
 	@Override
 	public int hashCode() {
-		return name.hashCode();
+		return _name.hashCode();
 	}
 
-	private static final long serialVersionUID = 7;
+	private static final long serialVersionUID = 1;
 
-	private final String name;
+	private final String _name;
 
 }
