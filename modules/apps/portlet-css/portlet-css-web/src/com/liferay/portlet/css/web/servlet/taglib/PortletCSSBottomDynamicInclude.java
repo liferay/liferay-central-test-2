954a91e33140c71a94366c28d799fc14d5b8cd9b
Brian Chan <brian.chan@liferay.com>
2015-04-24 14:30:55 -0700

    LPS-54619 Better package

--- modules/apps/portlet-css/portlet-css-web/src/com/liferay/portlet/css/web/servlet/taglib/PortletCSSBottomDynamicInclude.java
+++ modules/apps/portlet-css/portlet-css-web/src/com/liferay/portlet/css/web/servlet/taglib/PortletCSSBottomDynamicInclude.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portlet.css.web.taglib;
+package com.liferay.portlet.css.web.servlet.taglib;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
