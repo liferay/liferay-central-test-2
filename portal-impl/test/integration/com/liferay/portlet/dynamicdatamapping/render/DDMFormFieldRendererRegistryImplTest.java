e06e4d5b9a25f1acb11c0fe31954421d4e36f52e
philip.jones <philip.jones@liferay.com>
2015-04-27 13:12:48 -0700

    LPS-53802 DDMFormFieldRendererRegistryImplTest Integration point test

---
+++ portal-impl/test/integration/com/liferay/portlet/dynamicdatamapping/render/DDMFormFieldRendererRegistryImplTest.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.dynamicdatamapping.render;
+
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.MainServletTestRule;
+import com.liferay.portal.test.rule.SyntheticBundleRule;
+import com.liferay.portlet.dynamicdatamapping.render.bundle.ddmformfieldrendererregistryimpl.TestDDMFormFieldRenderer;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Philip Jones
+ */
+public class DDMFormFieldRendererRegistryImplTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE,
+			new SyntheticBundleRule("bundle.ddmformfieldrendererregistryimpl"));
+
+	@Test
+	public void testGetDDMFormFieldRenderer() {
+		DDMFormFieldRendererRegistry ddmFormFieldRendererRegistry =
+			DDMFormFieldRendererRegistryUtil.getDDMFormFieldRendererRegistry();
+
+		DDMFormFieldRenderer ddmFormFieldRenderer =
+			ddmFormFieldRendererRegistry.getDDMFormFieldRenderer(
+				TestDDMFormFieldRenderer.SUPPORTED_DDM_FIELD_TYPE);
+
+		Class<?> clazz = ddmFormFieldRenderer.getClass();
+
+		Assert.assertEquals(TestDDMFormFieldRenderer.class.getName(), clazz.
+			getName());
+	}
+
+}
---
+++ portal-impl/test/integration/com/liferay/portlet/dynamicdatamapping/render/bundle/ddmformfieldrendererregistryimpl/TestDDMFormFieldRenderer.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.dynamicdatamapping.render.bundle.ddmformfieldrendererregistryimpl;
+
+import com.liferay.portlet.dynamicdatamapping.model.DDMFormField;
+import com.liferay.portlet.dynamicdatamapping.render.DDMFormFieldRenderer;
+import com.liferay.portlet.dynamicdatamapping.render.DDMFormFieldRenderingContext;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Philip Jones
+ */
+@Component(
+	immediate = true,
+	property = {
+		"layout.type=testDDMFormFieldRenderer",
+		"service.ranking:Integer=" + Integer.MAX_VALUE
+	}
+)
+public class TestDDMFormFieldRenderer implements DDMFormFieldRenderer {
+
+	public static final String SUPPORTED_DDM_FIELD_TYPE = "text";
+
+	@Override
+	public String[] getSupportedDDMFormFieldTypes() {
+		List<String> supportedDDMFormFieldTypes = Arrays.asList(
+			SUPPORTED_DDM_FIELD_TYPE);
+
+		return (String[])supportedDDMFormFieldTypes.toArray();
+	}
+
+	@Override
+	public String render(DDMFormField ddmFormField, DDMFormFieldRenderingContext
+		ddmFormFieldRenderingContext) {
+
+		return null;
+	}
+
+}
