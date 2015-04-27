1a76edc271e40a37158a273ddf55aa536ea03676
Brian Chan <brian.chan@liferay.com>
2015-04-27 14:00:23 -0700

    LPS-52679 abstract it out

--- modules/portal/portal-soap-extender/src/com/liferay/portal/soap/extender/internal/DefaultSoapDescriptorBuilder.java
+++ modules/portal/portal-soap-extender/src/com/liferay/portal/soap/extender/internal/DefaultSoapDescriptorBuilder.java
@@ -30,63 +30,75 @@ public class DefaultSoapDescriptorBuilder implements SoapDescriptorBuilder {
 
 	@Override
 	public SoapDescriptor buildSoapDescriptor(
-		final Object service, final Map<String, Object> properties) {
+		Object service, Map<String, Object> properties) {
 
-		return new SoapDescriptor() {
-
-			@Override
-			public QName getEndpointName() {
-				Object soapEndpointName = properties.get("soap.endpoint.name");
-
-				if ((soapEndpointName != null) &&
-					soapEndpointName instanceof QName) {
-
-					QName endpointName = (QName)soapEndpointName;
-
-					return endpointName;
-				}
-
-				return null;
-			}
-
-			@Override
-			public String getPublicationAddress() {
-				Object soapAddress = properties.get("soap.address");
-
-				if (soapAddress == null) {
-					Class<?> clazz = service.getClass();
-
-					return "/" + clazz.getSimpleName();
-				}
-
-				return soapAddress.toString();
-			}
-
-			@Override
-			public Class<?> getServiceClass() {
-				Object soapServiceClass = properties.get("soap.service.class");
-
-				if ((soapServiceClass != null) &&
-					soapServiceClass instanceof Class<?>) {
-
-					return (Class<?>)soapServiceClass;
-				}
-
-				return null;
-			}
-
-			@Override
-			public String getWsdlLocation() {
-				Object soapWsdlLocation = properties.get("soap.wsdl.location");
-
-				if (soapWsdlLocation != null) {
-					return soapWsdlLocation.toString();
-				}
-
-				return null;
-			}
-
-		};
+		return new DefaultSoapDescriptor(service, properties);
 	}
+
+	private class DefaultSoapDescriptor implements SoapDescriptor {
+
+		public DefaultSoapDescriptor(
+			Object service, Map<String, Object> properties) {
+
+			_service = service;
+			_properties = properties;
+		}
+
+		@Override
+		public QName getEndpointName() {
+			Object soapEndpointName = _properties.get("soap.endpoint.name");
+
+			if ((soapEndpointName != null) &&
+				soapEndpointName instanceof QName) {
+
+				QName endpointName = (QName)soapEndpointName;
+
+				return endpointName;
+			}
+
+			return null;
+		}
+
+		@Override
+		public String getPublicationAddress() {
+			Object soapAddress = _properties.get("soap.address");
+
+			if (soapAddress == null) {
+				Class<?> clazz = _service.getClass();
+
+				return "/" + clazz.getSimpleName();
+			}
+
+			return soapAddress.toString();
+		}
+
+		@Override
+		public Class<?> getServiceClass() {
+			Object soapServiceClass = _properties.get("soap.service.class");
+
+			if ((soapServiceClass != null) &&
+				soapServiceClass instanceof Class<?>) {
+
+				return (Class<?>)soapServiceClass;
+			}
+
+			return null;
+		}
+
+		@Override
+		public String getWsdlLocation() {
+			Object soapWsdlLocation = _properties.get("soap.wsdl.location");
+
+			if (soapWsdlLocation != null) {
+				return soapWsdlLocation.toString();
+			}
+
+			return null;
+		}
+
+		private final Map<String, Object> _properties;
+		private final Object _service;
+
+	};
 
 }
