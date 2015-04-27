0a32442f4317081a53e9fb9357547594b12e57dc
Máté Thurzó <mate.thurzo@liferay.com>
2015-04-27 17:35:46 +0200

    LPS-54978 Add the lifecycle listener for clearing journal cache after finishing import

---
+++ modules/apps/journal/journal-web/src/com/liferay/journal/web/lar/lifecycle/JournalCacheExportImportLifecycleListener.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.web.lar.lifecycle;
+
+import com.liferay.journal.web.constants.JournalPortletKeys;
+import com.liferay.portal.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.lar.lifecycle.ExportImportLifecycleListener;
+import com.liferay.portal.lar.lifecycle.BaseExportImportLifecycleListener;
+import com.liferay.portlet.journal.util.JournalContentUtil;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Mate Thurzo
+ */
+@Component(service = ExportImportLifecycleListener.class)
+public class JournalCacheExportImportLifecycleListener
+	extends BaseExportImportLifecycleListener {
+
+	@Override
+	public boolean isParallel() {
+		return false;
+	}
+
+	protected void clearCache() {
+		JournalContentUtil.clearCache();
+	}
+
+	@Override
+	protected void onLayoutImportProcessFinished(
+			PortletDataContext portletDataContext)
+		throws Exception {
+
+		clearCache();
+	}
+
+	@Override
+	protected void onPortletImportProcessFinished(
+			PortletDataContext portletDataContext)
+		throws Exception {
+
+		String rootPortletId = portletDataContext.getRootPortletId();
+
+		if (!rootPortletId.equals(JournalPortletKeys.JOURNAL)) {
+			return;
+		}
+
+		clearCache();
+	}
+
+}
