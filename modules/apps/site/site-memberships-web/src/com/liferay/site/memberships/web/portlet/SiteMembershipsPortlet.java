6c87c090e2431d07fa3f3f0b4ff1b423ec3215f0
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-04-28 09:46:57 +0200

    LPS-55168 Register portlet

--- modules/apps/site/site-memberships-web/src/com/liferay/site/memberships/web/portlet/SiteMembershipsPortlet.java
+++ modules/apps/site/site-memberships-web/src/com/liferay/site/memberships/web/portlet/SiteMembershipsPortlet.java
@@ -50,13 +50,40 @@ import java.util.Set;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.portlet.Portlet;
 import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
+import org.osgi.service.component.annotations.Component;
+
 /**
  * @author Brian Wing Shun Chan
  */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.control-panel-entry-category=site_administration.users",
+		"com.liferay.portlet.control-panel-entry-weight=1.0",
+		"com.liferay.portlet.css-class-wrapper=portlet-communities",
+		"com.liferay.portlet.icon=/icons/site_memberships_admin.png",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.system=true",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Site Memberships Admin",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
 public class SiteMembershipsPortlet extends MVCPortlet {
 
 	public void editGroupOrganizations(
