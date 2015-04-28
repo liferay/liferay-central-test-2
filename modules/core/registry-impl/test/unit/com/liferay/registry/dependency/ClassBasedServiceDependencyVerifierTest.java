fae476c906a51e8551cc762b825dd5c9a6f71163
Michael Han <michael.han@liferay.com>
2015-04-28 11:00:37 -0500

    LPS-55266 add test

---
+++ modules/core/registry-impl/test/unit/com/liferay/registry/dependency/ClassBasedServiceDependencyVerifierTest.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.registry.dependency;
+
+import com.liferay.registry.BasicRegistryImpl;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceReference;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Michael C. Han
+ */
+public class ClassBasedServiceDependencyVerifierTest {
+
+	@Before
+	public void setUp() {
+		RegistryUtil.setRegistry(null);
+		RegistryUtil.setRegistry(new BasicRegistryImpl());
+	}
+
+	@Test
+	public void testVerifyImplementations() {
+		Registry registry = RegistryUtil.getRegistry();
+		registry.registerService(TestInstance1.class, new TestInstance1());
+		registry.registerService(TestInterface2.class, new TestInstance2());
+
+		ServiceReference serviceReference = registry.getServiceReference(
+			TestInstance1.class);
+
+		//test interfaces
+		ClassBasedServiceDependencyVerifier
+			classBasedServiceDependencyVerifier =
+				new ClassBasedServiceDependencyVerifier(TestInstance1.class);
+
+		Assert.assertTrue(
+			classBasedServiceDependencyVerifier.verify(serviceReference));
+
+		//test interfaces
+		ClassBasedServiceDependencyVerifier
+			classBasedServiceDependencyVerifier2 =
+				new ClassBasedServiceDependencyVerifier(TestInterface1.class);
+
+		Assert.assertTrue(
+			classBasedServiceDependencyVerifier2.verify(serviceReference));
+	}
+
+	@Test
+	public void testVerifyInterfaces() {
+		Registry registry = RegistryUtil.getRegistry();
+		registry.registerService(TestInterface1.class, new TestInstance1());
+		registry.registerService(TestInterface2.class, new TestInstance2());
+
+		ServiceReference serviceReference = registry.getServiceReference(
+			TestInterface1.class);
+		ServiceReference serviceReference2 = registry.getServiceReference(
+			TestInterface2.class);
+		ServiceReference serviceReference3 = registry.getServiceReference(
+			TestInterface2.class);
+
+		//test interfaces
+		ClassBasedServiceDependencyVerifier
+			classBasedServiceDependencyVerifier =
+				new ClassBasedServiceDependencyVerifier(TestInstance1.class);
+
+		Assert.assertTrue(
+			classBasedServiceDependencyVerifier.verify(serviceReference));
+
+		Assert.assertFalse(
+			classBasedServiceDependencyVerifier.verify(serviceReference2));
+
+		ClassBasedServiceDependencyVerifier
+			classBasedServiceDependencyVerifier2 =
+				new ClassBasedServiceDependencyVerifier(TestInstance2.class);
+
+		Assert.assertTrue(
+			classBasedServiceDependencyVerifier2.verify(serviceReference2));
+
+		ClassBasedServiceDependencyVerifier
+			classBasedServiceDependencyVerifier3 =
+				new ClassBasedServiceDependencyVerifier(TestInstance3.class);
+
+		Assert.assertFalse(
+			classBasedServiceDependencyVerifier3.verify(serviceReference3));
+	}
+
+	private class TestInstance1 implements TestInterface1 {
+	}
+
+	private class TestInstance2 implements TestInterface2 {
+	}
+
+	private class TestInstance3 implements TestInterface1, TestInterface2 {
+	}
+
+	private interface TestInterface1 {
+	}
+
+	private interface TestInterface2 {
+	}
+
+}
---
+++ modules/core/registry-impl/test/unit/com/liferay/registry/dependency/ServiceDependencyManagerTest.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.registry.dependency;
+
+import com.liferay.registry.BasicRegistryImpl;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Michael C. Han
+ */
+public class ServiceDependencyManagerTest {
+
+	@Before
+	public void setUp() {
+		RegistryUtil.setRegistry(null);
+		RegistryUtil.setRegistry(new BasicRegistryImpl());
+	}
+
+	@Test
+	public void testDependenciesFulfilled() {
+		Registry registry = RegistryUtil.getRegistry();
+		registry.registerService(TestInterface1.class, new TestInstance1());
+		registry.registerService(TestInterface2.class, new TestInstance2());
+
+		ServiceDependencyManager serviceDependencyManager =
+			new ServiceDependencyManager();
+
+		final Set<Boolean> completion = new HashSet<>();
+
+		serviceDependencyManager.addServiceDependencyListener(
+			new ServiceDependencyListener() {
+
+			@Override
+			public void dependenciesFulfilled() {
+				completion.add(true);
+			}
+
+			@Override
+			public void destroy() {
+			}
+		});
+
+		serviceDependencyManager.registerDependencies(TestInterface2.class);
+	}
+
+	@Test
+	public void testNoDependencies() {
+		Registry registry = RegistryUtil.getRegistry();
+		registry.registerService(TestInterface1.class, new TestInstance1());
+
+		ServiceDependencyManager serviceDependencyManager =
+			new ServiceDependencyManager();
+
+		serviceDependencyManager.addServiceDependencyListener(
+			new ServiceDependencyListener() {
+
+				@Override
+				public void dependenciesFulfilled() {
+					Assert.fail();
+				}
+
+				@Override
+				public void destroy() {
+				}
+			});
+
+		serviceDependencyManager.registerDependencies(TestInterface2.class);
+	}
+
+	private class TestInstance1 implements TestInterface1 {
+	}
+
+	private class TestInstance2 implements TestInterface2 {
+	}
+
+	private interface TestInterface1 {
+	}
+
+	private interface TestInterface2 {
+	}
+
+}
