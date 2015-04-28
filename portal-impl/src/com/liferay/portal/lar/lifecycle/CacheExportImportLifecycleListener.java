47b3752ec17aa224059977ea92c8b30527cfbd2b
Máté Thurzó <mate.thurzo@liferay.com>
2015-04-28 14:59:54 +0200

    LPS-55254 Add Cache export import lifecycle listener

---
+++ portal-impl/src/com/liferay/portal/lar/lifecycle/CacheExportImportLifecycleListener.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.lar.lifecycle;
+
+import com.liferay.portal.kernel.lar.PortletDataContext;
+import com.liferay.portal.security.permission.PermissionCacheUtil;
+import com.liferay.portal.servlet.filters.cache.CacheUtil;
+
+/**
+ * @author Mate Thurzo
+ */
+public class CacheExportImportLifecycleListener
+	extends BaseExportImportLifecycleListener {
+
+	@Override
+	public boolean isParallel() {
+		return false;
+	}
+
+	protected void clearCache() {
+		CacheUtil.clearCache();
+		PermissionCacheUtil.clearCache();
+	}
+
+	@Override
+	protected void onLayoutImportProcessFinished(
+			PortletDataContext portletDataContext)
+		throws Exception {
+
+		clearCache();
+	}
+
+	@Override
+	protected void onPortletImportProcessFinished(
+			PortletDataContext portletDataContext)
+		throws Exception {
+
+		clearCache();
+	}
+
+}
