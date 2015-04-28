36a8c7c814caf3b86f8fc01a04afc8e56bb6ea31
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-04-28 09:46:56 +0200

    LPS-55168 PortletKeys

--- portal-service/src/com/liferay/portal/util/PortletKeys.java
+++ portal-service/src/com/liferay/portal/util/PortletKeys.java
@@ -167,8 +167,6 @@ public class PortletKeys {
 
 	public static final String SITE_MEMBERS_DIRECTORY = "187";
 
-	public static final String SITE_MEMBERSHIPS_ADMIN = "174";
-
 	public static final String SITE_SETTINGS = "165";
 
 	public static final String SITES_ADMIN = "134";
