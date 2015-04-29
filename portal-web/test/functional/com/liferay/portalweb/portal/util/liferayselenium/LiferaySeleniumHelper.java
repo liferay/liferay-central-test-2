3c3d08195dbf2bddb64f40aaa663eaf1ceb0e92b
Brian Chiu <brian.chiu@liferay.com>
2015-04-29 16:17:34 -0700

    LRQA-15314 Ignore LPS-55154 until it is fixed

--- portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/LiferaySeleniumHelper.java
+++ portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/LiferaySeleniumHelper.java
@@ -1060,6 +1060,15 @@ public class LiferaySeleniumHelper {
 			return true;
 		}
 
+		// LPS-55154, temporary workaround until Shuyang Zhou fixes it
+
+		if (line.contains(
+				"Unable to process message " +
+					"{destinationName=liferay/hot_deploy")) {
+
+			return true;
+		}
+
 		// LRQA-14442, temporary workaround until Kiyoshi Lee fixes it
 
 		if (line.contains("Framework Event Dispatcher: Equinox Container:")) {
