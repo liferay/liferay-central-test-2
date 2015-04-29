34ee957a3fcf5482091e88cc4be920466dc13336
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2015-04-29 15:09:01 -0700

    LPS-55126 Gradle plugin

---
+++ modules/sdk/gradle-plugins-xml-formatter/src/com/liferay/gradle/plugins/xml/formatter/XMLFormatterPlugin.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.gradle.plugins.xml.formatter;
+
+import java.util.Set;
+
+import org.gradle.api.Action;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.ConfigurationContainer;
+import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.artifacts.ResolvableDependencies;
+import org.gradle.api.artifacts.dsl.DependencyHandler;
+import org.gradle.api.tasks.TaskContainer;
+
+/**
+ * @author Andrea Di Giorgi
+ */
+public class XMLFormatterPlugin implements Plugin<Project> {
+
+	public static final String CONFIGURATION_NAME = "xmlFormatter";
+
+	@Override
+	public void apply(Project project) {
+		addXMLFormatterConfiguration(project);
+
+		addFormatXMLTask(project);
+	}
+
+	protected Task addFormatXMLTask(Project project) {
+		TaskContainer taskContainer = project.getTasks();
+
+		Task task = taskContainer.create("formatXML", XMLFormatterTask.class);
+
+		task.setDescription("Runs Liferay XML Formatter to format files.");
+
+		return task;
+	}
+
+	protected Configuration addXMLFormatterConfiguration(
+		final Project project) {
+
+		ConfigurationContainer configurationContainer =
+			project.getConfigurations();
+
+		final Configuration configuration = configurationContainer.create(
+			CONFIGURATION_NAME);
+
+		configuration.setDescription(
+			"Configures Liferay XML Formatter for this project.");
+		configuration.setVisible(false);
+
+		ResolvableDependencies resolvableDependencies =
+			configuration.getIncoming();
+
+		resolvableDependencies.beforeResolve(
+			new Action<ResolvableDependencies>() {
+
+				@Override
+				public void execute(
+					ResolvableDependencies resolvableDependencies) {
+
+					Set<Dependency> dependencies =
+						configuration.getDependencies();
+
+					if (dependencies.isEmpty()) {
+						addXMLFormatterDependencies(project);
+					}
+				}
+
+			});
+
+		return configuration;
+	}
+
+	protected void addXMLFormatterDependencies(Project project) {
+		DependencyHandler dependencyHandler = project.getDependencies();
+
+		dependencyHandler.add(
+			CONFIGURATION_NAME,
+			"com.liferay:com.liferay.xml.formatter:latest.release");
+	}
+
+}
---
+++ modules/sdk/gradle-plugins-xml-formatter/src/com/liferay/gradle/plugins/xml/formatter/XMLFormatterTask.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.gradle.plugins.xml.formatter;
+
+import groovy.lang.Closure;
+
+import java.io.File;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.gradle.api.Project;
+import org.gradle.api.artifacts.ConfigurationContainer;
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.file.FileTree;
+import org.gradle.api.tasks.SourceTask;
+import org.gradle.api.tasks.TaskAction;
+import org.gradle.process.JavaExecSpec;
+
+/**
+ * @author Andrea Di Giorgi
+ */
+public class XMLFormatterTask extends SourceTask {
+
+	@TaskAction
+	public void formatXML() {
+		FileTree fileTree = getSource();
+
+		for (File file : fileTree) {
+			formatXML(file);
+		}
+	}
+
+	public boolean isStripComments() {
+		return _stripComments;
+	}
+
+	public void setStripComments(boolean stripComments) {
+		_stripComments = stripComments;
+	}
+
+	protected void formatXML(final File file) {
+		final Project project = getProject();
+
+		project.javaexec(
+			new Closure<Void>(null) {
+
+				@SuppressWarnings("unused")
+				public void doCall(JavaExecSpec javaExecSpec) {
+					javaExecSpec.setClasspath(getClasspath());
+					javaExecSpec.setMain(
+						"com.liferay.xml.formatter.XMLFormatter");
+					javaExecSpec.setSystemProperties(getSystemProperties(file));
+					javaExecSpec.setWorkingDir(project.getProjectDir());
+				}
+
+			});
+	}
+
+	protected FileCollection getClasspath() {
+		Project project = getProject();
+
+		ConfigurationContainer configurationContainer =
+			project.getConfigurations();
+
+		return configurationContainer.getByName(
+			XMLFormatterPlugin.CONFIGURATION_NAME);
+	}
+
+	protected Map<String, Object> getSystemProperties(File file) {
+		Map<String, Object> systemProperties = new HashMap<>();
+
+		systemProperties.put("xml.formatter.file", file.getAbsolutePath());
+		systemProperties.put("xml.formatter.strip.comments", isStripComments());
+
+		return systemProperties;
+	}
+
+	private boolean _stripComments;
+
+}
