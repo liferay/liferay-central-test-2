f839aa03becc7f319fe4b3738c1e0a892528ecf4
Michael Han <michael.han@liferay.com>
2015-04-28 23:06:17 -0500

    LPS-55189 move to module

--- modules/portal/portal-messaging/test/unit/com/liferay/portal/messaging/internal/sender/DefaultSynchronousMessageSenderTest.java
+++ modules/portal/portal-messaging/test/unit/com/liferay/portal/messaging/internal/sender/DefaultSynchronousMessageSenderTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.kernel.messaging.sender;
+package com.liferay.portal.messaging.internal.sender;
 
 import com.liferay.portal.dao.orm.common.EntityCacheImpl;
 import com.liferay.portal.dao.orm.common.FinderCacheImpl;
