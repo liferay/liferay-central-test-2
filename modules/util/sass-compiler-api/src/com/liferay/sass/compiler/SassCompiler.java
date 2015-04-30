784fc9042c63b5e19f5a423460277287ae895d4c
Brian Chan <brian.chan@liferay.com>
2015-04-30 09:48:59 -0700

    LPS-55292 SF

--- modules/util/sass-compiler-api/src/com/liferay/sass/compiler/SassCompiler.java
+++ modules/util/sass-compiler-api/src/com/liferay/sass/compiler/SassCompiler.java
@@ -18,6 +18,7 @@ package com.liferay.sass.compiler;
  * @author David Truong
  */
 public interface SassCompiler {
+
 	public String compileFile(
 			String inputFileName, String includeDirName, String imgDirName)
 		throws SassCompilerException;
