4cc2411200ce1881a3a87f30dd3fcaff76dffae5
Preston-Crary <preston.crary@liferay.com>
2015-04-29 17:55:43 -0700

    LPS-54269 Add tests for other stores

---
+++ portal-impl/test/integration/com/liferay/portlet/documentlibrary/store/JCRStoreTest.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.documentlibrary.store;
+
+import com.liferay.portal.jcr.JCRFactoryUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.MainServletTestRule;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+
+/**
+ * @author Preston Crary
+ */
+public class JCRStoreTest extends BaseStoreTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE);
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		JCRFactoryUtil.prepare();
+		JCRFactoryUtil.initialize();
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		JCRFactoryUtil.shutdown();
+	}
+
+	@Override
+	protected Store getStore() {
+		return new JCRStore();
+	}
+
+}
