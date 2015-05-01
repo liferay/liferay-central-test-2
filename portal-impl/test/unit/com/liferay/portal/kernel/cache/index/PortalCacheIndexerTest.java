289bc4b2e87a878736cb955100efbeeb3913b92b
Preston-Crary <preston.crary@liferay.com>
2015-05-01 14:20:07 -0700

    LPS-54887 Full test coverage for PortalCacheIndexer

---
+++ portal-impl/test/unit/com/liferay/portal/kernel/cache/index/PortalCacheIndexerTest.java
@@ -0,0 +1,373 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.cache.index;
+
+import com.liferay.portal.cache.MockPortalCacheManager;
+import com.liferay.portal.cache.memory.MemoryPortalCache;
+import com.liferay.portal.kernel.cache.CacheListener;
+import com.liferay.portal.kernel.cache.PortalCache;
+import com.liferay.portal.kernel.concurrent.test.MappedMethodNameCallableInvocationHandler;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.rule.CodeCoverageAssertor;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.ProxyUtil;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentMap;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+/**
+ * @author Preston Crary
+ */
+public class PortalCacheIndexerTest {
+
+	@ClassRule
+	public static final CodeCoverageAssertor codeCoverageAssertor =
+		CodeCoverageAssertor.INSTANCE;
+
+	@Before
+	public void setUp() throws Exception {
+		_portalCache = new MemoryPortalCache<>(
+			new MockPortalCacheManager<TestIndexedCacheKey, String>(
+				RandomTestUtil.randomString()),
+			RandomTestUtil.randomString(), 10);
+
+		_portalCacheIndexer = new PortalCacheIndexer<>(_portalCache);
+
+		_cacheListener = ReflectionTestUtil.getFieldValue(
+			_portalCache, "aggregatedCacheListener");
+
+		ConcurrentMap<Long, TestIndexedCacheKey> map =
+			ReflectionTestUtil.getFieldValue(
+				_portalCacheIndexer, "_indexedCacheKeys");
+
+		_mappedMethodNameCallableInvocationHandler =
+			new MappedMethodNameCallableInvocationHandler(map, true);
+
+		Object proxy = ProxyUtil.newProxyInstance(
+			ClassLoader.getSystemClassLoader(),
+			new Class<?>[] {ConcurrentMap.class},
+			_mappedMethodNameCallableInvocationHandler);
+
+		ReflectionTestUtil.setFieldValue(
+			_portalCacheIndexer, "_indexedCacheKeys", proxy);
+	}
+
+	@Test
+	public void testAddIndexedCacheKeyConcurrentPutDifferentKeys() {
+		Callable callable = new Callable() {
+
+			@Override
+			public Object call() {
+				_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+
+				return null;
+			}
+
+		};
+
+		_mappedMethodNameCallableInvocationHandler.putBeforeCallable(
+			"putIfAbsent", callable);
+
+		_portalCache.put(_INDEX_1_KEY_2, _VALUE);
+
+		assertIndexCacheSynchronization();
+	}
+
+	@Test
+	public void testAddIndexedCacheKeyConcurrentPutRemove() {
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+
+		Callable beforePutIfAbsentCallable = new Callable() {
+
+			@Override
+			public Object call() {
+				_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+
+				return null;
+			}
+
+		};
+
+		_mappedMethodNameCallableInvocationHandler.putBeforeCallable(
+			"putIfAbsent", beforePutIfAbsentCallable);
+
+		Callable beforeReplaceCallable = new Callable() {
+
+			@Override
+			public Object call() {
+				_portalCache.remove(_INDEX_1_KEY_1);
+
+				return null;
+			}
+
+		};
+
+		_mappedMethodNameCallableInvocationHandler.putBeforeCallable(
+			"replace", beforeReplaceCallable);
+
+		_portalCache.put(_INDEX_1_KEY_2, _VALUE);
+
+		assertIndexCacheSynchronization();
+	}
+
+	@Test
+	public void testAddIndexedCacheKeyPutSameKey() {
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+
+		assertIndexCacheSynchronization();
+	}
+
+	@Test
+	public void testAddIndexedCacheKeyWithDifferentIndex() {
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+		_portalCache.put(_INDEX_2_KEY_3, _VALUE);
+
+		assertIndexCacheSynchronization();
+	}
+
+	@Test
+	public void testAddIndexedCacheKeyWithSameIndex() {
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+		_portalCache.put(_INDEX_1_KEY_2, _VALUE);
+
+		assertIndexCacheSynchronization();
+	}
+
+	@Test
+	public void testDispose() {
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+
+		_portalCache.unregisterCacheListeners();
+
+		Set<TestIndexedCacheKey> keys = _portalCacheIndexer.getIndexedCacheKeys(
+			_INDEX_1_KEY_1.getIndex());
+
+		Assert.assertTrue(keys.isEmpty());
+	}
+
+	@Test
+	public void testGetIndexedCacheKeysWithIndexKey() {
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+
+		Set<TestIndexedCacheKey> set = _portalCacheIndexer.getIndexedCacheKeys(
+			_INDEX_1_KEY_1.getIndex());
+
+		set.clear();
+
+		assertIndexCacheSynchronization();
+	}
+
+	@Test
+	public void testGetIndexedCacheKeysWithoutIndexKey() {
+		_portalCacheIndexer.getIndexedCacheKeys(_INDEX_1_KEY_1.getIndex());
+
+		assertIndexCacheSynchronization();
+	}
+
+	@Test
+	public void testInit() {
+		_portalCache = new MemoryPortalCache<>(
+			new MockPortalCacheManager<TestIndexedCacheKey, String>(
+				RandomTestUtil.randomString()),
+			RandomTestUtil.randomString(), 10);
+
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+
+		_portalCacheIndexer = new PortalCacheIndexer<>(_portalCache);
+
+		assertIndexCacheSynchronization();
+	}
+
+	@Test
+	public void testNotifyEntryEvicted() {
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+
+		_portalCache.remove(_INDEX_1_KEY_1);
+		_cacheListener.notifyEntryEvicted(
+			_portalCache, _INDEX_1_KEY_1, _VALUE, 0);
+
+		assertIndexCacheSynchronization();
+	}
+
+	@Test
+	public void testNotifyEntryExpired() {
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+
+		_portalCache.remove(_INDEX_1_KEY_1);
+		_cacheListener.notifyEntryExpired(
+			_portalCache, _INDEX_1_KEY_1, _VALUE, 0);
+
+		assertIndexCacheSynchronization();
+	}
+
+	@Test
+	public void testNotifyEntryRemoved() {
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+
+		_portalCache.remove(_INDEX_1_KEY_1);
+
+		assertIndexCacheSynchronization();
+	}
+
+	@Test
+	public void testNotifyEntryUpdated() {
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+
+		assertIndexCacheSynchronization();
+	}
+
+	@Test
+	public void testNotifyRemoveAll() {
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+		_portalCache.put(_INDEX_1_KEY_2, _VALUE);
+		_portalCache.put(_INDEX_2_KEY_3, _VALUE);
+
+		_portalCache.removeAll();
+
+		assertIndexCacheSynchronization();
+	}
+
+	@Test
+	public void testRemoveIndexedCacheKeyConcurrentPut() {
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+
+		Callable callable = new Callable() {
+
+			@Override
+			public Object call() throws Exception {
+				_portalCache.put(_INDEX_1_KEY_2, _VALUE);
+
+				return null;
+			}
+
+		};
+
+		_mappedMethodNameCallableInvocationHandler.putBeforeCallable(
+			"remove", callable);
+
+		_portalCache.remove(_INDEX_1_KEY_1);
+
+		assertIndexCacheSynchronization();
+	}
+
+	@Test
+	public void testRemoveIndexedCacheKeyConcurrentRemove() {
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+		_portalCache.put(_INDEX_1_KEY_2, _VALUE);
+
+		Callable callable = new Callable() {
+
+			@Override
+			public Object call() throws Exception {
+				_portalCache.remove(_INDEX_1_KEY_1);
+
+				return null;
+			}
+
+		};
+
+		_mappedMethodNameCallableInvocationHandler.putBeforeCallable(
+			"replace", callable);
+
+		_portalCache.remove(_INDEX_1_KEY_2);
+
+		assertIndexCacheSynchronization();
+	}
+
+	@Test
+	public void testRemoveIndexedCacheKeysWithIndex() {
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+
+		_portalCacheIndexer.removeIndexedCacheKeys(_INDEX_1_KEY_1.getIndex());
+
+		assertIndexCacheSynchronization();
+	}
+
+	@Test
+	public void testRemoveIndexedCacheKeysWithoutIndex() {
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+
+		_portalCacheIndexer.removeIndexedCacheKeys(_INDEX_2_KEY_3.getIndex());
+
+		assertIndexCacheSynchronization();
+	}
+
+	@Test
+	public void testRemoveIndexedCacheKeyWithKey() {
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+		_portalCache.put(_INDEX_1_KEY_2, _VALUE);
+
+		_portalCache.remove(_INDEX_1_KEY_1);
+
+		assertIndexCacheSynchronization();
+	}
+
+	@Test
+	public void testRemoveIndexedCacheKeyWithoutKey() {
+		_portalCache.put(_INDEX_1_KEY_1, _VALUE);
+
+		_portalCache.remove(_INDEX_1_KEY_2);
+
+		assertIndexCacheSynchronization();
+	}
+
+	protected void assertIndexCacheSynchronization() {
+		Set<TestIndexedCacheKey> keys = new HashSet<>(_portalCache.getKeys());
+
+		Set<Long> indexes = new HashSet<>();
+
+		for (TestIndexedCacheKey key : keys) {
+			indexes.add(key.getIndex());
+		}
+
+		Set<TestIndexedCacheKey> indexedCacheKeys = new HashSet<>();
+
+		for (Long index : indexes) {
+			indexedCacheKeys.addAll(
+				_portalCacheIndexer.getIndexedCacheKeys(index));
+		}
+
+		Assert.assertEquals(keys, indexedCacheKeys);
+	}
+
+	private static final TestIndexedCacheKey _INDEX_1_KEY_1 =
+		new TestIndexedCacheKey(1L, 1L);
+
+	private static final TestIndexedCacheKey _INDEX_1_KEY_2 =
+		new TestIndexedCacheKey(1L, 2L);
+
+	private static final TestIndexedCacheKey _INDEX_2_KEY_3 =
+		new TestIndexedCacheKey(2L, 3L);
+
+	private static final String _VALUE = "VALUE";
+
+	private CacheListener<TestIndexedCacheKey, String> _cacheListener;
+	private MappedMethodNameCallableInvocationHandler
+		_mappedMethodNameCallableInvocationHandler;
+	private PortalCache<TestIndexedCacheKey, String> _portalCache;
+	private PortalCacheIndexer<Long, TestIndexedCacheKey, String>
+		_portalCacheIndexer;
+
+}
---
+++ portal-impl/test/unit/com/liferay/portal/kernel/cache/index/TestIndexedCacheKey.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.cache.index;
+
+import com.liferay.portal.kernel.util.HashUtil;
+
+/**
+ * @author Preston Crary
+ */
+public class TestIndexedCacheKey implements IndexedCacheKey<Long> {
+
+	public TestIndexedCacheKey(long indexedLong, long nonIndexedLong) {
+		_indexedLong = indexedLong;
+		_nonIndexedLong = nonIndexedLong;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		TestIndexedCacheKey testIndexedCacheKey = (TestIndexedCacheKey)obj;
+
+		if ((testIndexedCacheKey._indexedLong == _indexedLong) &&
+			(testIndexedCacheKey._nonIndexedLong == _nonIndexedLong)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public Long getIndex() {
+		return _indexedLong;
+	}
+
+	@Override
+	public int hashCode() {
+		int hashCode = HashUtil.hash(0, _indexedLong);
+
+		return HashUtil.hash(hashCode, _nonIndexedLong);
+	}
+
+	private final long _indexedLong;
+	private final long _nonIndexedLong;
+
+}
