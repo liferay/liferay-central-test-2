9befdc622f1863c4ec56bd90a984154fa3135c0b
Juergen Kappler <jurgen.kappler@liferay.com>
2015-05-04 20:54:00 +0200

    LPS-55354 Create portlet instance configuration classes for the module

---
+++ modules/apps/iframe/iframe-web/src/com/liferay/iframe/web/configuration/IFramePortletInstanceConfiguration.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.iframe.web.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+/**
+ * @author Juergen Kappler
+ */
+@Meta.OCD(
+	id = "com.liferay.iframe.web.configuration.IFramePortletInstanceConfiguration"
+)
+public interface IFramePortletInstanceConfiguration {
+
+	@Meta.AD(deflt = "", required = false)
+	public String alt();
+
+	@Meta.AD(deflt = "false", required = false)
+	public boolean auth();
+
+	@Meta.AD(deflt = "", required = false)
+	public String authType();
+
+	@Meta.AD(deflt = "", required = false)
+	public String basicPassword();
+
+	@Meta.AD(deflt = "", required = false)
+	public String basicUserName();
+
+	@Meta.AD(deflt = "0", required = false)
+	public String border();
+
+	@Meta.AD(deflt = "#000000", required = false)
+	public String bordercolor();
+
+	@Meta.AD(deflt = "", required = false)
+	public String formMethod();
+
+	@Meta.AD(deflt = "", required = false)
+	public String formPassword();
+
+	@Meta.AD(deflt = "", required = false)
+	public String formUserName();
+
+	@Meta.AD(deflt = "0", required = false)
+	public String frameborder();
+
+	@Meta.AD(deflt = "600", required = false)
+	public String heightMaximized();
+
+	@Meta.AD(deflt = "600", required = false)
+	public String heightNormal();
+
+	@Meta.AD(deflt = "", required = false)
+	public String hiddenVariables();
+
+	@Meta.AD(deflt = "0", required = false)
+	public String hspace();
+
+	@Meta.AD(deflt = "", required = false)
+	public String longdesc();
+
+	@Meta.AD(deflt = "", required = false)
+	public String password();
+
+	@Meta.AD(deflt = "", required = false)
+	public String passwordField();
+
+	@Meta.AD(deflt = "", required = false)
+	public boolean relative();
+
+	@Meta.AD(deflt = "true", required = false)
+	public boolean resizeAutomatically();
+
+	@Meta.AD(deflt = "auto", required = false)
+	public String scrolling();
+
+	@Meta.AD(deflt = "", required = false)
+	public String src();
+
+	@Meta.AD(deflt = "", required = false)
+	public String title();
+
+	@Meta.AD(deflt = "", required = false)
+	public String userName();
+
+	@Meta.AD(deflt = "", required = false)
+	public String userNameField();
+
+	@Meta.AD(deflt = "0", required = false)
+	public String vspace();
+
+	@Meta.AD(deflt = "100%", required = false)
+	public String width();
+
+}
