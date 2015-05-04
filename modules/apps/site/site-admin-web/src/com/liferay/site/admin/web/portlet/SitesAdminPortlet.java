b7b2943e8932b091a4c8b2fc4931c29a06e61c11
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-05-04 12:15:39 +0200

    LPS-55267 Register portlet

--- modules/apps/site/site-admin-web/src/com/liferay/site/admin/web/portlet/SitesAdminPortlet.java
+++ modules/apps/site/site-admin-web/src/com/liferay/site/admin/web/portlet/SitesAdminPortlet.java
@@ -96,17 +96,45 @@ import java.util.concurrent.Callable;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.portlet.Portlet;
 import javax.portlet.PortletException;
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletURL;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
+import org.osgi.service.component.annotations.Component;
+
 import org.springframework.transaction.interceptor.TransactionAttribute;
 
 /**
  * @author Eudaldo Alonso
  */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.control-panel-entry-category=sites",
+		"com.liferay.portlet.control-panel-entry-weight=1.0",
+		"com.liferay.portlet.css-class-wrapper=portlet-sites-admin",
+		"com.liferay.portlet.display-category=category.hidden",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.icon=/icons/sites_admin.png",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Sites Admin",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
 public class SitesAdminPortlet extends MVCPortlet {
 
 	public void activate(
