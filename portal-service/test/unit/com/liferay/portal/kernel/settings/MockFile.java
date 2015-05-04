a649e5ad7b51ddb3f057425904fc13ce0a869220
Laszlo Csontos <laszlo.csontos@liferay.com>
2015-05-04 16:50:45 +0200

    LPS-54558 Fix MockFile

--- portal-service/test/unit/com/liferay/portal/kernel/settings/MockFile.java
+++ portal-service/test/unit/com/liferay/portal/kernel/settings/MockFile.java
@@ -241,6 +241,10 @@ public class MockFile implements com.liferay.portal.kernel.util.File {
 	}
 
 	@Override
+	public void mkdirs(File file) throws IOException {
+	}
+
+	@Override
 	public void mkdirs(String pathName) {
 	}
 
