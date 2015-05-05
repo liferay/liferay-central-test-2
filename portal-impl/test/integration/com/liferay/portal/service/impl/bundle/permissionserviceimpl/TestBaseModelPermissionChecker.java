6813b2a6dcb230cf282e75680f9dfa42c6016372
philip.jones <philip.jones@liferay.com>
2015-05-05 14:59:26 -0700

    LPS-53802-PermissionServiceImplTest Integration point test

---
+++ portal-impl/test/integration/com/liferay/portal/service/impl/bundle/permissionserviceimpl/TestBaseModelPermissionChecker.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.service.impl.bundle.permissionserviceimpl;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.StackTraceUtil;
+import com.liferay.portal.security.permission.BaseModelPermissionChecker;
+import com.liferay.portal.security.permission.PermissionChecker;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Philip Jones
+ */
+@Component(
+		immediate = true,
+		property = {
+			"model.class.name=PermissionServiceImplTest",
+			"service.ranking:Integer=" + Integer.MAX_VALUE
+		}
+	)
+public class TestBaseModelPermissionChecker
+	implements BaseModelPermissionChecker {
+
+	@Override
+	public void checkBaseModel(PermissionChecker permissionChecker, long
+		groupId, long primaryKey, String actionId) throws PortalException {
+
+		_atomicReference.set(StackTraceUtil.getCallerKey());
+
+		return;
+	}
+
+	@Reference(target = "(test=AtomicState)")
+	protected void getAtomicReference(AtomicReference<String> atomicReference) {
+		_atomicReference = atomicReference;
+	}
+
+	private AtomicReference<String> _atomicReference;
+
+}
