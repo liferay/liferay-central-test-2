fb7e76d2f0107b8d2dc043a06c142523fb0be626
Brian Chan <brian.chan@liferay.com>
2015-05-05 15:02:28 -0700

    LPS-54842 Readd

--- modules/portal/portal-rest-extender-test/src/com/liferay/portal/rest/extender/test/service/Greeter.java
+++ modules/portal/portal-rest-extender-test/src/com/liferay/portal/rest/extender/test/service/Greeter.java
@@ -21,6 +21,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
 @Path("/testApp")
 public class Greeter extends Application {
 
