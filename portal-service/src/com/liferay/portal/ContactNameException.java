a5b049a773b3fcf8ebbb473a01d1c1d6a8260c8e
drewbrokke <drew.brokke@liferay.com>
2015-05-05 09:58:10 -0700

    LPS-55364 Create ContactNameException class

---
+++ portal-service/src/com/liferay/portal/ContactNameException.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Drew Brokke
+ */
+public class ContactNameException extends PortalException {
+
+	/**
+	 * @deprecated As of 7.0.0, replaced by the inner classes
+	 */
+	@Deprecated
+	public ContactNameException() {
+	}
+
+	/**
+	 * @deprecated As of 7.0.0, replaced by the inner classes
+	 */
+	@Deprecated
+	public ContactNameException(String msg) {
+		super(msg);
+	}
+
+	/**
+	 * @deprecated As of 7.0.0, replaced by the inner classes
+	 */
+	@Deprecated
+	public ContactNameException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+	/**
+	 * @deprecated As of 7.0.0, replaced by the inner classes
+	 */
+	@Deprecated
+	public ContactNameException(Throwable cause) {
+		super(cause);
+	}
+
+}
