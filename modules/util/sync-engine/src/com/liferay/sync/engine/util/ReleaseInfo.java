1ca0fdc7281b968e3df74250abc87c439383cebc
Shinn Lok <shinn.lok@liferay.com>
2015-05-06 16:44:25 -0700

    SYNC-1281 Prep for next release

--- modules/util/sync-engine/src/com/liferay/sync/engine/util/ReleaseInfo.java
+++ modules/util/sync-engine/src/com/liferay/sync/engine/util/ReleaseInfo.java
@@ -50,11 +50,11 @@ public class ReleaseInfo {
 		return true;
 	}
 
-	private static final String _BUILD = "3007";
+	private static final String _BUILD = "3008";
 
 	private static final int _BUILD_NUMBER = Integer.parseInt(_BUILD);
 
-	private static final String _VERSION = "3.0.7";
+	private static final String _VERSION = "3.0.8";
 
 	private static final Pattern _pattern = Pattern.compile(
 		"(?:[0-9]+\\.){3}([0-9]+)");
