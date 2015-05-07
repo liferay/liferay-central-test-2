4f6d606fd389c3e0badb75688030d07cfae57364
Juergen Kappler <jurgen.kappler@liferay.com>
2015-05-07 11:07:45 +0200

    LPS-55419 Create configuration class for the module based on portlet.properties

---
+++ modules/apps/site-navigation/site-navigation-menu-web/src/com/liferay/site/navigation/menu/web/configuration/NavigationMenuWebConfiguration.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.navigation.menu.web.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+/**
+ * @author Juergen Kappler
+ */
+@Meta.OCD(
+	id = "com.liferay.site.navigation.menu.web.configuration.NavigationMenuWebConfiguration"
+)
+public interface NavigationMenuWebConfiguration {
+
+	@Meta.AD(deflt = "dots", required = false)
+	public String defaultBulletStyle();
+
+	@Meta.AD(deflt = "relative-with-breadcrumb", required = false)
+	public String defaultDisplayStyle();
+
+	@Meta.AD(
+		deflt = "relative-with-breadcrumb,from-level-2-with-title,from-level-1-with-title,from-level-1,from-level-1-to-all-sublevels,from-level-0",
+		required = false
+	)
+	public String[] displayStyleOptions();
+
+}
