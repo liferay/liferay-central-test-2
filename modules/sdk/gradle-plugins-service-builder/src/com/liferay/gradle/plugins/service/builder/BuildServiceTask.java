52cb5183b5443bd57271acad229979f9b6df49ca
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2015-05-07 12:29:46 -0700

    LPS-51081 Service Builder requires relative paths

--- modules/sdk/gradle-plugins-service-builder/src/com/liferay/gradle/plugins/service/builder/BuildServiceTask.java
+++ modules/sdk/gradle-plugins-service-builder/src/com/liferay/gradle/plugins/service/builder/BuildServiceTask.java
@@ -14,7 +14,6 @@
 
 package com.liferay.gradle.plugins.service.builder;
 
-import com.liferay.gradle.util.FileUtil;
 import com.liferay.gradle.util.StringUtil;
 import com.liferay.portal.tools.service.builder.ServiceBuilderArgs;
 
@@ -58,17 +57,15 @@ public class BuildServiceTask extends JavaExec {
 		super.exec();
 	}
 
-	public File getApiDir() {
-		Project project = getProject();
-
-		return project.file(_serviceBuilderArgs.getApiDirName());
+	public String getApiDirName() {
+		return _serviceBuilderArgs.getApiDirName();
 	}
 
 	@Override
 	public List<String> getArgs() {
 		List<String> args = new ArrayList<>();
 
-		args.add("service.api.dir=" + FileUtil.getAbsolutePath(getApiDir()));
+		args.add("service.api.dir=" + getApiDirName());
 		args.add(
 			"service.auto.import.default.references=" +
 				isAutoImportDefaultReferences());
@@ -76,42 +73,34 @@ public class BuildServiceTask extends JavaExec {
 		args.add("service.bean.locator.util=" + getBeanLocatorUtil());
 		args.add("service.build.number.increment=" + isBuildNumberIncrement());
 		args.add("service.build.number=" + getBuildNumber());
-		args.add("service.hbm.file=" + FileUtil.getAbsolutePath(getHbmFile()));
-		args.add("service.impl.dir=" + FileUtil.getAbsolutePath(getImplDir()));
-		args.add(
-			"service.input.file=" + FileUtil.getAbsolutePath(getInputFile()));
+		args.add("service.hbm.file=" + getHbmFileName());
+		args.add("service.impl.dir=" + getImplDirName());
+		args.add("service.input.file=" + getInputFileName());
 		args.add(
 			"service.model.hints.configs=" +
 				StringUtil.merge(getModelHintsConfigs(), ","));
-		args.add(
-			"service.model.hints.file=" +
-				FileUtil.getAbsolutePath(getModelHintsFile()));
+		args.add("service.model.hints.file=" + getModelHintsFileName());
 		args.add("service.osgi.module=" + isOsgiModule());
 		args.add("service.plugin.name=" + getPluginName());
 		args.add("service.props.util=" + getPropsUtil());
 		args.add(
 			"service.read.only.prefixes=" +
 				StringUtil.merge(getReadOnlyPrefixes(), ","));
-		args.add(
-			"service.remoting.file=" +
-				FileUtil.getAbsolutePath(getRemotingFile()));
+		args.add("service.remoting.file=" + getRemotingFileName());
 		args.add(
 			"service.resource.actions.configs=" +
 				StringUtil.merge(getResourceActionsConfigs(), ","));
-		args.add(
-			"service.resources.dir=" +
-				FileUtil.getAbsolutePath(getResourcesDir()));
-		args.add(
-			"service.spring.file=" + FileUtil.getAbsolutePath(getSpringFile()));
+		args.add("service.resources.dir=" + getResourcesDirName());
+		args.add("service.spring.file=" + getSpringFileName());
 		args.add(
 			"service.spring.namespaces=" +
 				StringUtil.merge(getSpringNamespaces(), ","));
-		args.add("service.sql.dir=" + FileUtil.getAbsolutePath(getSqlDir()));
+		args.add("service.sql.dir=" + getSqlDirName());
 		args.add("service.sql.file=" + getSqlFileName());
 		args.add("service.sql.indexes.file=" + getSqlIndexesFileName());
 		args.add("service.sql.sequences.file=" + getSqlSequencesFileName());
 		args.add("service.target.entity.name=" + getTargetEntityName());
-		args.add("service.test.dir=" + FileUtil.getAbsolutePath(getTestDir()));
+		args.add("service.test.dir=" + getTestDirName());
 
 		return args;
 	}
@@ -135,22 +124,16 @@ public class BuildServiceTask extends JavaExec {
 			ServiceBuilderPlugin.CONFIGURATION_NAME);
 	}
 
-	public File getHbmFile() {
-		Project project = getProject();
-
-		return project.file(_serviceBuilderArgs.getHbmFileName());
+	public String getHbmFileName() {
+		return _serviceBuilderArgs.getHbmFileName();
 	}
 
-	public File getImplDir() {
-		Project project = getProject();
-
-		return project.file(_serviceBuilderArgs.getImplDirName());
+	public String getImplDirName() {
+		return _serviceBuilderArgs.getImplDirName();
 	}
 
-	public File getInputFile() {
-		Project project = getProject();
-
-		return project.file(_serviceBuilderArgs.getInputFileName());
+	public String getInputFileName() {
+		return _serviceBuilderArgs.getInputFileName();
 	}
 
 	@Override
@@ -162,10 +145,8 @@ public class BuildServiceTask extends JavaExec {
 		return _serviceBuilderArgs.getModelHintsConfigs();
 	}
 
-	public File getModelHintsFile() {
-		Project project = getProject();
-
-		return project.file(_serviceBuilderArgs.getModelHintsFileName());
+	public String getModelHintsFileName() {
+		return _serviceBuilderArgs.getModelHintsFileName();
 	}
 
 	public String getPluginName() {
@@ -180,36 +161,28 @@ public class BuildServiceTask extends JavaExec {
 		return _serviceBuilderArgs.getReadOnlyPrefixes();
 	}
 
-	public File getRemotingFile() {
-		Project project = getProject();
-
-		return project.file(_serviceBuilderArgs.getRemotingFileName());
+	public String getRemotingFileName() {
+		return _serviceBuilderArgs.getRemotingFileName();
 	}
 
 	public String[] getResourceActionsConfigs() {
 		return _serviceBuilderArgs.getResourceActionsConfigs();
 	}
 
-	public File getResourcesDir() {
-		Project project = getProject();
-
-		return project.file(_serviceBuilderArgs.getResourcesDirName());
+	public String getResourcesDirName() {
+		return _serviceBuilderArgs.getResourcesDirName();
 	}
 
-	public File getSpringFile() {
-		Project project = getProject();
-
-		return project.file(_serviceBuilderArgs.getSpringFileName());
+	public String getSpringFileName() {
+		return _serviceBuilderArgs.getSpringFileName();
 	}
 
 	public String[] getSpringNamespaces() {
 		return _serviceBuilderArgs.getSpringNamespaces();
 	}
 
-	public File getSqlDir() {
-		Project project = getProject();
-
-		return project.file(_serviceBuilderArgs.getSqlDirName());
+	public String getSqlDirName() {
+		return _serviceBuilderArgs.getSqlDirName();
 	}
 
 	public String getSqlFileName() {
@@ -228,10 +201,8 @@ public class BuildServiceTask extends JavaExec {
 		return _serviceBuilderArgs.getTargetEntityName();
 	}
 
-	public File getTestDir() {
-		Project project = getProject();
-
-		return project.file(_serviceBuilderArgs.getTestDirName());
+	public String getTestDirName() {
+		return _serviceBuilderArgs.getTestDirName();
 	}
 
 	@Override
