47e2bf35bce6e76849698f40dc563443d1e60237
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2015-05-08 11:29:52 -0700

    LPS-55175 - Source formatting

--- modules/frontend/frontend-js-web/src/META-INF/resources/html/js/liferay/item_viewer.js
+++ modules/frontend/frontend-js-web/src/META-INF/resources/html/js/liferay/item_viewer.js
@@ -3,25 +3,27 @@ AUI.add(
 	function(A) {
 		var Lang = A.Lang;
 
-		var CSS_FOOTER_BUTTONS = A.getClassName('image', 'viewer', 'footer', 'buttons');
+		var CSS_IMAGE_VIEWER = A.getClassName('image', 'viewer');
 
-		var CSS_FOOTER_CONTROL = A.getClassName('image', 'viewer', 'footer', 'control');
+		var CSS_IMAGE_VIEWER_BASE = A.getClassName(CSS_IMAGE_VIEWER, 'base');
 
-		var CSS_FOOTER_CONTROL_LEFT = A.getClassName('image', 'viewer', 'footer', 'control', 'left');
+		var CSS_IMAGE_VIEWER_FOOTER = A.getClassName(CSS_IMAGE_VIEWER, 'footer');
 
-		var CSS_FOOTER_CONTROL_LEFT_BASE = A.getClassName('image', 'viewer', 'base', 'control', 'left');
+		var CSS_FOOTER_CONTROL = A.getClassName(CSS_IMAGE_VIEWER_FOOTER, 'control');
 
-		var CSS_FOOTER_CONTROL_RIGHT = A.getClassName('image', 'viewer', 'footer', 'control', 'right');
+		var CSS_FOOTER_CONTROL_LEFT = A.getClassName(CSS_IMAGE_VIEWER_FOOTER, 'control', 'left');
 
-		var CSS_FOOTER_CONTROL_RIGHT_BASE = A.getClassName('image', 'viewer', 'base', 'control', 'right');
+		var CSS_FOOTER_CONTROL_LEFT_BASE = A.getClassName(CSS_IMAGE_VIEWER_BASE, 'control', 'left');
 
-		var CSS_IMAGE_CONTAINER = A.getClassName('image', 'viewer', 'base', 'image', 'container');
+		var CSS_FOOTER_CONTROL_RIGHT = A.getClassName(CSS_IMAGE_VIEWER_FOOTER, 'control', 'right');
 
-		var CSS_IMAGE_CURRENT_CONTAINER = A.getClassName('image', 'viewer', 'base', 'current', 'image');
+		var CSS_FOOTER_CONTROL_RIGHT_BASE = A.getClassName(CSS_IMAGE_VIEWER_BASE, 'control', 'right');
 
-		var CSS_IMAGE_INFO = A.getClassName('image', 'viewer', 'base', 'image', 'info');
+		var CSS_IMAGE_CONTAINER = A.getClassName(CSS_IMAGE_VIEWER_BASE, 'image', 'container');
 
-		var CSS_LOADING_ICON = A.getClassName('image', 'viewer', 'base', 'loading', 'icon');
+		var CSS_IMAGE_INFO = A.getClassName(CSS_IMAGE_VIEWER_BASE, 'image', 'info');
+
+		var CSS_LOADING_ICON = A.getClassName(CSS_IMAGE_VIEWER_BASE, 'loading', 'icon');
 
 		var STR_RENDER_CONTROLS = 'renderControls';
 
@@ -68,11 +70,18 @@ AUI.add(
 				NS: 'lfr-item-viewer',
 
 				prototype: {
-					TPL_CONTROL_LEFT: '<a href="#" class="' + CSS_FOOTER_CONTROL + ' ' + CSS_FOOTER_CONTROL_LEFT_BASE + ' ' + CSS_FOOTER_CONTROL_LEFT + '"><span class="glyphicon glyphicon-chevron-left"></span></a>',
+					TPL_CONTROL_LEFT: '<a href="#" class="' + CSS_FOOTER_CONTROL + ' ' + CSS_FOOTER_CONTROL_LEFT_BASE + ' ' + CSS_FOOTER_CONTROL_LEFT + '">' +
+						'<span class="glyphicon glyphicon-chevron-left"></span>' +
+					'</a>',
 
-					TPL_CONTROL_RIGHT: '<a href="#" class="' + CSS_FOOTER_CONTROL + ' ' + CSS_FOOTER_CONTROL_RIGHT_BASE + ' ' + CSS_FOOTER_CONTROL_RIGHT + '"><span class="glyphicon glyphicon-chevron-right"></span></a>',
+					TPL_CONTROL_RIGHT: '<a href="#" class="' + CSS_FOOTER_CONTROL + ' ' + CSS_FOOTER_CONTROL_RIGHT_BASE + ' ' + CSS_FOOTER_CONTROL_RIGHT + '">' +
+						'<span class="glyphicon glyphicon-chevron-right"></span>' +
+					'</a>',
 
-					TPL_IMAGE_CONTAINER: '<div class="' + CSS_IMAGE_CONTAINER + '"> <div class="' + CSS_IMAGE_INFO + '"></div><span class="glyphicon glyphicon-time ' + CSS_LOADING_ICON + '"></span></div>',
+					TPL_IMAGE_CONTAINER: '<div class="' + CSS_IMAGE_CONTAINER + '">' +
+						'<div class="' + CSS_IMAGE_INFO + '"></div>' +
+						'<span class="glyphicon glyphicon-time ' + CSS_LOADING_ICON + '"></span>' +
+					'</div>',
 
 					initializer: function() {
 						var instance = this;
@@ -98,16 +107,24 @@ AUI.add(
 
 						instance.setStdModContent('footer', container);
 
-						instance._captionEl = A.Node.create(instance.TPL_CAPTION);
-						instance._captionEl.selectable();
-						container.append(instance._captionEl);
+						var captionEl = A.Node.create(instance.TPL_CAPTION);
+
+						captionEl.selectable();
+
+						container.append(captionEl);
+
+						instance._captionEl = captionEl;
 
 						if (instance.get(STR_RENDER_CONTROLS)) {
 							container.append(instance.get('controlPrevious'));
 
-							instance._infoEl = A.Node.create(instance.TPL_INFO);
-							instance._infoEl.selectable();
-							container.append(instance._infoEl);
+							var infoEl = A.Node.create(instance.TPL_INFO);
+
+							infoEl.selectable();
+
+							container.append(infoEl);
+
+							instance._infoEl = infoEl;
 
 							container.append(instance.get('controlNext'));
 						}
