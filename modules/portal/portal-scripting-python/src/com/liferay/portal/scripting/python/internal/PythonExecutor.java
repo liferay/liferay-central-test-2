a83c1262402bb6b9fcf4e87b9421caeec1d2eddb
Michael Han <michael.han@liferay.com>
2015-05-11 14:28:32 -0500

    LPS-55501 move to module

--- modules/portal/portal-scripting-python/src/com/liferay/portal/scripting/python/internal/PythonExecutor.java
+++ modules/portal/portal-scripting-python/src/com/liferay/portal/scripting/python/internal/PythonExecutor.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.scripting.python;
+package com.liferay.portal.scripting.python.internal;
 
 import com.liferay.portal.kernel.cache.PortalCache;
 import com.liferay.portal.kernel.cache.SingleVMPoolUtil;
