a4472a73b37f93b9709f08d2313d6291bf9331af
Brian Chan <brian.chan@liferay.com>
2015-05-11 22:16:24 -0700

    LPS-55454 generics

--- modules/apps/wiki/wiki-web/docroot/WEB-INF/src/com/liferay/wiki/web/settings/internal/WikiPortletInstanceConfigurationBeanDeclaration.java
+++ modules/apps/wiki/wiki-web/docroot/WEB-INF/src/com/liferay/wiki/web/settings/internal/WikiPortletInstanceConfigurationBeanDeclaration.java
@@ -27,7 +27,7 @@ public class WikiPortletInstanceConfigurationBeanDeclaration
 	implements ConfigurationBeanDeclaration {
 
 	@Override
-	public Class getConfigurationBeanClass() {
+	public Class<?> getConfigurationBeanClass() {
 		return WikiPortletInstanceConfiguration.class;
 	}
 
