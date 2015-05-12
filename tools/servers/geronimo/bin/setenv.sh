cb044b170a5fafcb194efc52af54680203447732
Igor Spasić <igor.spasic@liferay.com>
2015-05-12 11:25:00 +0200

    LRQA-15486 removed geronimo under tools/servers

--- tools/servers/geronimo/bin/setenv.sh
+++
@@ -1,1 +0,0 @@
-GERONIMO_OPTS="$GERONIMO_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true @java.security.config@ -Duser.timezone=GMT -DXorg.apache.geronimo.deployment.LenientMFCP=true -Xmx1024m -XX:MaxPermSize=512m"
