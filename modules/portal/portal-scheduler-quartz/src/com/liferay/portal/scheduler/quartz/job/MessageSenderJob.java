da21c0d2f24e5bc9ef3e5380ec6cae6dcba5c445
Edward Han <edward.han@liferay.com>
2015-05-13 01:03:18 +1000

    LPS-55643 - Extracting Quartz-specific MessageSenderJob

--- modules/portal/portal-scheduler-quartz/src/com/liferay/portal/scheduler/quartz/job/MessageSenderJob.java
+++ modules/portal/portal-scheduler-quartz/src/com/liferay/portal/scheduler/quartz/job/MessageSenderJob.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.scheduler.job;
+package com.liferay.portal.scheduler.quartz.job;
 
 import com.liferay.portal.kernel.cluster.ClusterExecutorUtil;
 import com.liferay.portal.kernel.cluster.ClusterRequest;
@@ -26,9 +26,11 @@ import com.liferay.portal.kernel.scheduler.JobStateSerializeUtil;
 import com.liferay.portal.kernel.scheduler.SchedulerEngine;
 import com.liferay.portal.kernel.scheduler.SchedulerEngineHelperUtil;
 import com.liferay.portal.kernel.scheduler.StorageType;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.MethodHandler;
 import com.liferay.portal.kernel.util.MethodKey;
-import com.liferay.portal.util.PropsValues;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
 
 import java.util.Map;
 
@@ -91,7 +93,8 @@ public class MessageSenderJob implements Job {
 		if (jobExecutionContext.getNextFireTime() == null) {
 			message.put(SchedulerEngine.DISABLE, true);
 
-			if (PropsValues.CLUSTER_LINK_ENABLED &&
+			if (GetterUtil.getBoolean(
+					PropsUtil.get(PropsKeys.CLUSTER_LINK_ENABLED)) &&
 				(storageType == StorageType.MEMORY_CLUSTERED)) {
 
 				notifyClusterMember(jobKey, storageType);
