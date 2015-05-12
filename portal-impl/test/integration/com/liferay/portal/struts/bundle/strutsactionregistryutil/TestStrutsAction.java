2ab00a9e9c58b5f89a7e83378606bab0f5490aae
Brian Chan <brian.chan@liferay.com>
2015-05-12 13:22:20 -0700

    LPS-53802 SF

--- portal-impl/test/integration/com/liferay/portal/struts/bundle/strutsactionregistryutil/TestStrutsAction.java
+++ portal-impl/test/integration/com/liferay/portal/struts/bundle/strutsactionregistryutil/TestStrutsAction.java
@@ -31,8 +31,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	immediate = true,
 	property = {
-		"path=TestStrutsAction",
-		"service.ranking:Integer=" + Integer.MAX_VALUE
+		"path=TestStrutsAction", "service.ranking:Integer=" + Integer.MAX_VALUE
 	}
 )
 public class TestStrutsAction implements StrutsAction {
