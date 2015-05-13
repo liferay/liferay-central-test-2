40c107788190aa6da51747ae6070dfb699c62f69
tinatian <tina.tian@liferay.com>
2015-05-13 19:40:59 +0800

    LPS-55479 Add osgi bindings.

---
+++ portal-impl/src/com/liferay/portal/cache/ehcache/SingleVMEhcachePortalCacheManager.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.cache.ehcache;
+
+import com.liferay.portal.kernel.cache.PortalCacheManager;
+import com.liferay.portal.kernel.cache.PortalCacheManagerNames;
+import com.liferay.portal.kernel.cache.PortalCacheManagerTypes;
+import com.liferay.portal.kernel.util.Props;
+import com.liferay.portal.kernel.util.PropsKeys;
+
+import java.io.Serializable;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Tina Tian
+ */
+@Component(
+	immediate = true,
+	property = {
+		"portal.cache.manager.name=" + PortalCacheManagerNames.SINGLE_VM,
+		"portal.cache.manager.type=" + PortalCacheManagerTypes.EHCACHE
+	},
+	service = PortalCacheManager.class
+)
+public class SingleVMEhcachePortalCacheManager<K extends Serializable, V>
+	extends EhcachePortalCacheManager<K, V> {
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		setConfigFile(props.get(PropsKeys.EHCACHE_SINGLE_VM_CONFIG_LOCATION));
+		setDefaultConfigFile("/ehcache/liferay-single-vm.xml");
+		setName(PortalCacheManagerNames.SINGLE_VM);
+
+		initialize();
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		destroy();
+	}
+
+	@Reference(unbind = "-")
+	protected void setProps(Props props) {
+		this.props = props;
+	}
+
+}
