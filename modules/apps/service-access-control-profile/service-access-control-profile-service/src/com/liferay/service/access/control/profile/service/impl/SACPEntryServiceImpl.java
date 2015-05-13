3e2c5c541af7110f51d8144f26dc5f2db9e0996e
Brian Chan <brian.chan@liferay.com>
2015-05-13 15:51:16 -0700

    LPS-54042 SF

--- modules/apps/service-access-control-profile/service-access-control-profile-service/src/com/liferay/service/access/control/profile/service/impl/SACPEntryServiceImpl.java
+++ modules/apps/service-access-control-profile/service-access-control-profile-service/src/com/liferay/service/access/control/profile/service/impl/SACPEntryServiceImpl.java
@@ -31,18 +31,7 @@ import java.util.Locale;
 import java.util.Map;
 
 /**
- * The implementation of the s a c p entry remote service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.service.access.control.profile.service.SACPEntryService} interface.
- *
- * <p>
- * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
- * </p>
- *
  * @author Brian Wing Shun Chan
- * @see SACPEntryServiceBaseImpl
- * @see com.liferay.service.access.control.profile.service.SACPEntryServiceUtil
  */
 @ProviderType
 public class SACPEntryServiceImpl extends SACPEntryServiceBaseImpl {
