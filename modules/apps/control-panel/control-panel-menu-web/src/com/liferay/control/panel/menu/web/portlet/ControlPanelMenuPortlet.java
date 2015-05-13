bb5bf585f8c533e529a74339e331f343a8b68519
Adolfo Pérez <adolfo.perez@liferay.com>
2015-05-13 15:26:30 +0200

    LPS-55428 Use default template

--- modules/apps/control-panel/control-panel-menu-web/src/com/liferay/control/panel/menu/web/portlet/ControlPanelMenuPortlet.java
+++ modules/apps/control-panel/control-panel-menu-web/src/com/liferay/control/panel/menu/web/portlet/ControlPanelMenuPortlet.java
@@ -33,7 +33,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.show-portlet-access-denied=false",
 		"com.liferay.portlet.show-portlet-inactive=false",
 		"com.liferay.portlet.system=true",
-		"com.liferay.portlet.use-default-template=false",
+		"com.liferay.portlet.use-default-template=true",
 		"javax.portlet.display-name=Control Panel Menu",
 		"javax.portlet.expiration-cache=0",
 		"javax.portlet.init-param.template-path=/",
