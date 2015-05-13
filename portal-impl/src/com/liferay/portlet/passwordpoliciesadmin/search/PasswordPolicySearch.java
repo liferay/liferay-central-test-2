peterfellwock/liferay-portal

LPS-54323 Move OrderByComparator method to PasswordPolicySearch

modules/apps/password-policies-admin/password-policies-admin-web/src/com/liferay/password/policies/admin/web/search/PasswordPolicySearch.java
@@ -22,10 +22,11 @@ import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.PasswordPolicy;
+import com.liferay.portal.util.comparator.PasswordPolicyDescriptionComparator;
+import com.liferay.portal.util.comparator.PasswordPolicyNameComparator;
 import com.liferay.portal.util.PortletKeys;
 import com.liferay.portlet.PortalPreferences;
 import com.liferay.portlet.PortletPreferencesFactoryUtil;
-import com.liferay.portlet.passwordpoliciesadmin.util.PasswordPoliciesAdminUtil;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -98,7 +99,7 @@ public class PasswordPolicySearch extends SearchContainer<PasswordPolicy> {
 			}
 
 			OrderByComparator<PasswordPolicy> orderByComparator =
-				PasswordPoliciesAdminUtil.getPasswordPolicyOrderByComparator(
+				getPasswordPolicyOrderByComparator(
 					orderByCol, orderByType);
 
 			setOrderableHeaders(orderableHeaders);
@@ -109,6 +110,31 @@ public class PasswordPolicySearch extends SearchContainer<PasswordPolicy> {
 		catch (Exception e) {
 			_log.error(e);
 		}
+	}
+
+	private OrderByComparator<PasswordPolicy> getPasswordPolicyOrderByComparator(
+		String orderByCol, String orderByType) {
+
+		boolean orderByAsc = false;
+
+		if (orderByType.equals("asc")) {
+			orderByAsc = true;
+		}
+
+		OrderByComparator<PasswordPolicy> orderByComparator = null;
+
+		if (orderByCol.equals("name")) {
+			orderByComparator = new PasswordPolicyNameComparator(orderByAsc);
+		}
+		else if (orderByCol.equals("description")) {
+			orderByComparator = new PasswordPolicyDescriptionComparator(
+				orderByAsc);
+		}
+		else {
+			orderByComparator = new PasswordPolicyNameComparator(orderByAsc);
+		}
+
+		return orderByComparator;
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portlet/passwordpoliciesadmin/search/PasswordPolicySearch.java
@@ -22,9 +22,10 @@ import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.PasswordPolicy;
 import com.liferay.portal.util.PortletKeys;
+import com.liferay.portal.util.comparator.PasswordPolicyDescriptionComparator;
+import com.liferay.portal.util.comparator.PasswordPolicyNameComparator;
 import com.liferay.portlet.PortalPreferences;
 import com.liferay.portlet.PortletPreferencesFactoryUtil;
-import com.liferay.portlet.passwordpoliciesadmin.util.PasswordPoliciesAdminUtil;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -97,8 +98,7 @@ public class PasswordPolicySearch extends SearchContainer<PasswordPolicy> {
 			}
 
 			OrderByComparator<PasswordPolicy> orderByComparator =
-				PasswordPoliciesAdminUtil.getPasswordPolicyOrderByComparator(
-					orderByCol, orderByType);
+				_getOrderByComparator(orderByCol, orderByType);
 
 			setOrderableHeaders(orderableHeaders);
 			setOrderByCol(orderByCol);
@@ -108,6 +108,31 @@ public class PasswordPolicySearch extends SearchContainer<PasswordPolicy> {
 		catch (Exception e) {
 			_log.error(e);
 		}
+	}
+
+	private OrderByComparator<PasswordPolicy> _getOrderByComparator(
+		String orderByCol, String orderByType) {
+
+		boolean orderByAsc = false;
+
+		if (orderByType.equals("asc")) {
+			orderByAsc = true;
+		}
+
+		OrderByComparator<PasswordPolicy> orderByComparator = null;
+
+		if (orderByCol.equals("name")) {
+			orderByComparator = new PasswordPolicyNameComparator(orderByAsc);
+		}
+		else if (orderByCol.equals("description")) {
+			orderByComparator = new PasswordPolicyDescriptionComparator(
+				orderByAsc);
+		}
+		else {
+			orderByComparator = new PasswordPolicyNameComparator(orderByAsc);
+		}
+
+		return orderByComparator;
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(