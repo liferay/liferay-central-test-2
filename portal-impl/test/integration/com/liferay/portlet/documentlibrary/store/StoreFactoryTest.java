f6231cb049b014a2deb59bf3826bb2130d3ba66b
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-05-13 08:52:36 +0200

    LPS-55555 Add a test for the new OSGi extension point for Store

---
+++ portal-impl/test/integration/com/liferay/portlet/documentlibrary/store/StoreFactoryTest.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.documentlibrary.store;
+
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.MainServletTestRule;
+import com.liferay.portal.test.rule.SyntheticBundleRule;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class StoreFactoryTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE,
+			new SyntheticBundleRule("bundle.storefactory"));
+
+	@Test
+	public void testGetInstance() throws Exception {
+		Store testStore = StoreFactory.getInstance("test");
+
+		Assert.assertNotNull(testStore);
+
+		String[] fileNames = testStore.getFileNames(0, 0);
+
+		Assert.assertEquals(1, fileNames.length);
+		Assert.assertEquals("TestStore", fileNames[0]);
+	}
+
+}
