4cd9c2b26056eea70a5913a1792dd22b5b401c67
Leonardo Barros <leonardo.barros@liferay.com>
2015-05-14 08:56:34 -0300

    LPS-54927 Add getUserId to BaseRequestHelper

--- portal-service/src/com/liferay/portal/kernel/display/context/util/BaseRequestHelper.java
+++ portal-service/src/com/liferay/portal/kernel/display/context/util/BaseRequestHelper.java
@@ -254,6 +254,16 @@ public abstract class BaseRequestHelper {
 
 		return _user;
 	}
+	
+	public long getUserId() {
+		if (_user == null) {
+			ThemeDisplay themeDisplay = getThemeDisplay();
+
+			_user = themeDisplay.getUser();
+		}
+		
+		return _user.getUserId();
+	}
 
 	private Company _company;
 	private Long _companyId;
