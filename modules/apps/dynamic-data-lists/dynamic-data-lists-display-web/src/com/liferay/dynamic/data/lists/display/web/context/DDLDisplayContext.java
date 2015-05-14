d558887e0c1fc552fab208b2d3bf75414d69652a
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-05-14 18:42:12 -0300

    LPS-54703 Add initial portlet implementation

---
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-display-web/src/com/liferay/dynamic/data/lists/display/web/context/DDLDisplayContext.java
@@ -0,0 +1,336 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.display.web.context;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.PrefsParamUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.model.Layout;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.service.permission.PortletPermissionUtil;
+import com.liferay.portal.theme.PortletDisplay;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portal.util.WebKeys;
+import com.liferay.portlet.dynamicdatalists.model.DDLRecord;
+import com.liferay.portlet.dynamicdatalists.model.DDLRecordSet;
+import com.liferay.portlet.dynamicdatalists.service.DDLRecordSetLocalServiceUtil;
+import com.liferay.portlet.dynamicdatalists.service.permission.DDLRecordSetPermission;
+import com.liferay.portlet.dynamicdatalists.util.DDLUtil;
+import com.liferay.portlet.dynamicdatamapping.model.DDMFormField;
+import com.liferay.portlet.dynamicdatamapping.model.DDMTemplate;
+import com.liferay.portlet.dynamicdatamapping.registry.DDMFormFieldType;
+import com.liferay.portlet.dynamicdatamapping.registry.DDMFormFieldTypeRegistryUtil;
+import com.liferay.portlet.dynamicdatamapping.registry.DDMFormFieldValueRendererAccessor;
+import com.liferay.portlet.dynamicdatamapping.service.DDMTemplateLocalServiceUtil;
+import com.liferay.portlet.dynamicdatamapping.service.permission.DDMPermission;
+import com.liferay.portlet.dynamicdatamapping.service.permission.DDMTemplatePermission;
+import com.liferay.portlet.dynamicdatamapping.storage.DDMFormFieldValue;
+import com.liferay.portlet.dynamicdatamapping.util.DDMDisplay;
+import com.liferay.portlet.dynamicdatamapping.util.DDMDisplayRegistryUtil;
+import com.liferay.portlet.dynamicdatamapping.util.DDMPermissionHandler;
+
+import java.util.List;
+import java.util.Locale;
+
+import javax.portlet.PortletPreferences;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Marcellus Tavares
+ */
+public class DDLDisplayContext {
+
+	public DDLDisplayContext(
+		RenderRequest renderRequest, RenderResponse renderResponse) {
+
+		_renderRequest = renderRequest;
+		_renderResponse = renderResponse;
+
+		_portletPreferences = renderRequest.getPreferences();
+
+		String portletId = PortalUtil.getPortletId(renderRequest);
+
+		if (portletId.equals(PortletKeys.PORTLET_CONFIGURATION)) {
+			return;
+		}
+
+		DDLRecordSet recordSet = getRecordSet();
+
+		if ((recordSet == null) || !hasViewPermission()) {
+			_renderRequest.setAttribute(
+				WebKeys.PORTLET_CONFIGURATOR_VISIBILITY, Boolean.TRUE);
+		}
+	}
+
+	public long getDisplayDDMTemplateId() {
+		return PrefsParamUtil.getLong(
+			_portletPreferences, _renderRequest, "displayDDMTemplateId");
+	}
+
+	public String getEditTemplateTitle() throws PortalException {
+		DDMDisplay ddmDisplay = getDDMDisplay();
+
+		return ddmDisplay.getEditTemplateTitle(
+			_recordSet.getDDMStructure(), fetchDisplayDDMTemplate(),
+			getLocale());
+	}
+
+	public DDLRecordSet getRecordSet() {
+		if (_recordSet != null) {
+			return _recordSet;
+		}
+
+		_recordSet = (DDLRecordSet)_renderRequest.getAttribute(
+			WebKeys.DYNAMIC_DATA_LISTS_RECORD_SET);
+
+		if (_recordSet != null) {
+			return _recordSet;
+		}
+
+		_recordSet = DDLRecordSetLocalServiceUtil.fetchDDLRecordSet(
+			getRecordSetId());
+
+		return _recordSet;
+	}
+
+	public long getRecordSetId() {
+		return PrefsParamUtil.getLong(
+			_portletPreferences, _renderRequest, "recordSetId");
+	}
+
+	public String getTemplateContent() throws Exception {
+		return DDLUtil.getTemplateContent(
+			getDisplayDDMTemplateId(), _recordSet, getThemeDisplay(),
+			_renderRequest, _renderResponse);
+	}
+
+	public boolean isEditable() {
+		return PrefsParamUtil.getBoolean(
+			_portletPreferences, _renderRequest, "editable");
+	}
+
+	public boolean isShowAddDisplayDDMTemplateIcon() {
+		if (_hasAddDisplayDDMTemplatePermission != null) {
+			return _hasAddDisplayDDMTemplatePermission;
+		}
+
+		_hasAddDisplayDDMTemplatePermission = Boolean.FALSE;
+
+		DDLRecordSet recordSet = getRecordSet();
+
+		if (recordSet == null) {
+			return _hasAddDisplayDDMTemplatePermission;
+		}
+
+		DDMPermissionHandler ddmPermissionHandler = getDDMPermissionHandler();
+
+		_hasAddDisplayDDMTemplatePermission = DDMPermission.contains(
+			getPermissionChecker(), getScopeGroupId(),
+			ddmPermissionHandler.getResourceName(getStructureTypeClassNameId()),
+			ddmPermissionHandler.getAddTemplateActionId());
+
+		return _hasAddDisplayDDMTemplatePermission;
+	}
+
+	public boolean isShowConfigurationIcon() throws PortalException {
+		if (_showConfigurationIcon != null) {
+			return _showConfigurationIcon;
+		}
+
+		ThemeDisplay themeDisplay = getThemeDisplay();
+
+		_showConfigurationIcon = PortletPermissionUtil.contains(
+			themeDisplay.getPermissionChecker(), themeDisplay.getLayout(),
+			getPortletId(), ActionKeys.CONFIGURATION);
+
+		return _showConfigurationIcon;
+	}
+
+	public boolean isShowEditDisplayDDMTemplateIcon() throws PortalException {
+		if (_hasEditDisplayDDMTemplatePermission != null) {
+			return _hasEditDisplayDDMTemplatePermission;
+		}
+
+		_hasEditDisplayDDMTemplatePermission = Boolean.FALSE;
+
+		if (getDisplayDDMTemplateId() == 0) {
+			return _hasEditDisplayDDMTemplatePermission;
+		}
+
+		_hasEditDisplayDDMTemplatePermission = DDMTemplatePermission.contains(
+			getPermissionChecker(), getScopeGroupId(),
+			getDisplayDDMTemplateId(), PortletKeys.DYNAMIC_DATA_LISTS,
+			ActionKeys.UPDATE);
+
+		return _hasEditDisplayDDMTemplatePermission;
+	}
+
+	public boolean isShowIconsActions() throws PortalException {
+		if (_hasShowIconsActionPermission != null) {
+			return _hasShowIconsActionPermission;
+		}
+
+		_hasShowIconsActionPermission = Boolean.FALSE;
+
+		ThemeDisplay themeDisplay = getThemeDisplay();
+
+		if (!themeDisplay.isSignedIn()) {
+			return _hasShowIconsActionPermission;
+		}
+
+		Layout layout = themeDisplay.getLayout();
+
+		if (layout.isLayoutPrototypeLinkActive()) {
+			return _hasShowIconsActionPermission;
+		}
+
+		if (isShowConfigurationIcon() || isShowAddDisplayDDMTemplateIcon() ||
+			isShowEditDisplayDDMTemplateIcon()) {
+
+			_hasShowIconsActionPermission = Boolean.TRUE;
+		}
+
+		return _hasShowIconsActionPermission;
+	}
+
+	public String renderRecordField(
+			DDLRecord record, DDMFormField ddmFormField, Locale locale)
+		throws PortalException {
+
+		List<DDMFormFieldValue> ddmFormFieldValues =
+			record.getDDMFormFieldValues(ddmFormField.getName());
+
+		DDMFormFieldValueRendererAccessor
+			ddmFormFieldValueRendererAccessor =
+				getDDMFormFieldValueRendererAccessor(ddmFormField, locale);
+
+		return ListUtil.toString(
+			ddmFormFieldValues, ddmFormFieldValueRendererAccessor,
+			StringPool.COMMA_AND_SPACE);
+	}
+
+	protected DDMTemplate fetchDisplayDDMTemplate() {
+		if (_displayDDMTemplate != null) {
+			return _displayDDMTemplate;
+		}
+
+		_displayDDMTemplate = DDMTemplateLocalServiceUtil.fetchDDMTemplate(
+			getDisplayDDMTemplateId());
+
+		return _displayDDMTemplate;
+	}
+
+	protected DDMDisplay getDDMDisplay() {
+		return DDMDisplayRegistryUtil.getDDMDisplay(
+			PortletKeys.DYNAMIC_DATA_LISTS);
+	}
+
+	protected DDMFormFieldValueRendererAccessor
+		getDDMFormFieldValueRendererAccessor(
+			DDMFormField ddmFormField, Locale locale) {
+
+		DDMFormFieldType ddmFormFieldType =
+			DDMFormFieldTypeRegistryUtil.getDDMFormFieldType(
+				ddmFormField.getType());
+
+		return ddmFormFieldType.getDDMFormFieldValueRendererAccessor(locale);
+	}
+
+	protected DDMPermissionHandler getDDMPermissionHandler() {
+		DDMDisplay ddmDisplay = getDDMDisplay();
+
+		return ddmDisplay.getDDMPermissionHandler();
+	}
+
+	protected Layout getLayout() {
+		ThemeDisplay themeDisplay = getThemeDisplay();
+
+		return themeDisplay.getLayout();
+	}
+
+	protected Locale getLocale() {
+		ThemeDisplay themeDisplay = getThemeDisplay();
+
+		return themeDisplay.getLocale();
+	}
+
+	protected PermissionChecker getPermissionChecker() {
+		ThemeDisplay themeDisplay = getThemeDisplay();
+
+		return themeDisplay.getPermissionChecker();
+	}
+
+	protected String getPortletId() {
+		ThemeDisplay themeDisplay = getThemeDisplay();
+
+		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+		return portletDisplay.getId();
+	}
+
+	protected long getScopeGroupId() {
+		ThemeDisplay themeDisplay = getThemeDisplay();
+
+		return themeDisplay.getScopeGroupId();
+	}
+
+	protected long getStructureTypeClassNameId() {
+		DDMDisplay ddmDisplay = getDDMDisplay();
+
+		return PortalUtil.getClassNameId(ddmDisplay.getStructureType());
+	}
+
+	protected ThemeDisplay getThemeDisplay() {
+		ThemeDisplay themeDisplay = (ThemeDisplay)_renderRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		return themeDisplay;
+	}
+
+	protected boolean hasViewPermission() {
+		if (_hasViewPermission != null) {
+			return _hasViewPermission;
+		}
+
+		_hasViewPermission = true;
+
+		if (_recordSet != null) {
+			ThemeDisplay themeDisplay = getThemeDisplay();
+
+			_hasViewPermission = DDLRecordSetPermission.contains(
+				themeDisplay.getPermissionChecker(), _recordSet,
+				ActionKeys.VIEW);
+		}
+
+		return _hasViewPermission;
+	}
+
+	private DDMTemplate _displayDDMTemplate;
+	private Boolean _hasAddDisplayDDMTemplatePermission;
+	private Boolean _hasEditDisplayDDMTemplatePermission;
+	private Boolean _hasShowIconsActionPermission;
+	private Boolean _hasViewPermission;
+	private final PortletPreferences _portletPreferences;
+	private DDLRecordSet _recordSet;
+	private final RenderRequest _renderRequest;
+	private final RenderResponse _renderResponse;
+	private Boolean _showConfigurationIcon;
+
+}
---
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-display-web/src/com/liferay/dynamic/data/lists/display/web/portlet/DDLDisplayPortlet.java
@@ -0,0 +1,169 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.display.web.portlet;
+
+import com.liferay.dynamic.data.lists.display.web.constants.DDLDisplayPortletKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.PrefsParamUtil;
+import com.liferay.portal.security.auth.PrincipalException;
+import com.liferay.portal.util.WebKeys;
+import com.liferay.portlet.dynamicdatalists.NoSuchRecordSetException;
+import com.liferay.portlet.dynamicdatalists.model.DDLRecordSet;
+import com.liferay.portlet.dynamicdatalists.service.DDLRecordSetService;
+import com.liferay.util.log4j.Log4JUtil;
+
+import java.io.IOException;
+
+import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marcellus Tavares
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.css-class-wrapper=dynamic-data-lists-display",
+		"com.liferay.portlet.display-category=category.collaboration",
+		"com.liferay.portlet.instanceable=true",
+		"com.liferay.portlet.layout-cacheable=true",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.scopeable=true",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Dynamic Data Lists Display",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.config-template=/configuration.jsp",
+		"javax.portlet.init-param.copy-request-parameters=true",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + DDLDisplayPortletKeys.DDL_DISPLAY,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=guest,power-user,user",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
+public class DDLDisplayPortlet extends MVCPortlet {
+
+	@Override
+	public void render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		try {
+			setRenderRequestAttributes(renderRequest, renderResponse);
+		}
+		catch (Exception e) {
+			if (isSessionErrorException(e)) {
+				if (_log.isWarnEnabled()) {
+					_log.warn(e, e);
+				}
+
+				hideDefaultErrorMessage(renderRequest);
+
+				SessionErrors.add(renderRequest, e.getClass());
+			}
+			else {
+				throw new PortletException(e);
+			}
+		}
+
+		super.render(renderRequest, renderResponse);
+	}
+
+	@Activate
+	protected void activate() {
+		Class<? extends MVCPortlet> clazz = getClass();
+
+		initLogger(clazz.getClassLoader());
+	}
+
+	@Override
+	protected void doDispatch(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		if (SessionErrors.contains(
+				renderRequest, NoSuchRecordSetException.class.getName()) ||
+			SessionErrors.contains(
+				renderRequest, PrincipalException.class.getName())) {
+
+			include(templatePath + "error.jsp", renderRequest, renderResponse);
+		}
+		else {
+			super.doDispatch(renderRequest, renderResponse);
+		}
+	}
+
+	protected void initLogger(ClassLoader classLoader) {
+		Log4JUtil.configureLog4J(
+			classLoader.getResource("META-INF/portal-log4j.xml"));
+	}
+
+	@Override
+	protected boolean isSessionErrorException(Throwable cause) {
+		if (cause instanceof NoSuchRecordSetException ||
+			cause instanceof PrincipalException) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Reference
+	protected void setDDLRecordSetService(
+		DDLRecordSetService ddlRecordSetService) {
+
+		_ddlRecordSetService = ddlRecordSetService;
+	}
+
+	protected void setRenderRequestAttributes(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortalException {
+
+		long recordSetId = PrefsParamUtil.getLong(
+			renderRequest.getPreferences(), renderRequest, "recordSetId");
+
+		DDLRecordSet recordSet = null;
+
+		if (recordSetId > 0) {
+			recordSet = _ddlRecordSetService.getRecordSet(recordSetId);
+
+			renderRequest.setAttribute(
+				WebKeys.DYNAMIC_DATA_LISTS_RECORD_SET, recordSet);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DDLDisplayPortlet.class);
+
+	private DDLRecordSetService _ddlRecordSetService;
+
+}
