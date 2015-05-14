1c80aec067378479d5b99ea5e03373d598579681
Cristina González Castellano <cristina.gonzalez@liferay.com>
2015-05-14 09:56:16 +0200

    LPS-55433 You can't share the same package between two bundles, so the test classes should have another package

--- modules/apps/document-library/document-library-test/test/integration/com/liferay/document/library/events/test/DLFileVersionTest.java
+++ modules/apps/document-library/document-library-test/test/integration/com/liferay/document/library/events/test/DLFileVersionTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.document.library.events;
+package com.liferay.document.library.events.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.document.library.events.AddDefaultDocumentLibraryStructuresAction;
