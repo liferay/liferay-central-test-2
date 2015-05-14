d89573cdc3bade4e423d80fdca57b8d3c0683304
Jose A. Jimenez <jose.jimenez@liferay.com>
2015-05-14 18:46:07 +0200

    LPS-55225 tests - articles are searcheable for admins when were fully expired

--- portal-impl/test/integration/com/liferay/portlet/journal/service/JournalArticleIndexVersionsTest.java
+++ portal-impl/test/integration/com/liferay/portlet/journal/service/JournalArticleIndexVersionsTest.java
@@ -131,6 +131,8 @@ public class JournalArticleIndexVersionsTest {
 	public void testExpireAllArticleVersions() throws Exception {
 		long initialSearchCount = searchCount(true);
 
+		long initialNonHeadSearchCount = searchCount(false);
+
 		JournalArticle article = JournalTestUtil.addArticle(
 			_group.getGroupId(),
 			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID);
@@ -146,6 +148,8 @@ public class JournalArticleIndexVersionsTest {
 		JournalTestUtil.expireArticle(_group.getGroupId(), updateArticle);
 
 		Assert.assertEquals(initialSearchCount, searchCount(true));
+
+		Assert.assertEquals(initialNonHeadSearchCount + 1, searchCount(false));
 	}
 
 	@Test
