19b392629a7c0f7287569c4d5790dca705339e84
Steven Smith <steven.smith@liferay.com>
2015-05-15 13:43:28 -0700

    LPS-55628 - VerifyProcess to remove the orphaned AssetEntries and AssetLinks

--- portal-impl/src/com/liferay/portal/verify/VerifyAsset.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyAsset.java
@@ -15,7 +15,12 @@
 package com.liferay.portal.verify;
 
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.util.PortalUtil;
 import com.liferay.portlet.asset.service.AssetCategoryLocalServiceUtil;
+import com.liferay.portlet.asset.service.AssetEntryLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.model.DLFileEntry;
+import com.liferay.portlet.documentlibrary.model.DLFileEntryConstants;
+import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalServiceUtil;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -29,6 +34,7 @@ public class VerifyAsset extends VerifyProcess {
 	@Override
 	protected void doVerify() throws Exception {
 		rebuildTree();
+		removeOrphanedEntries();
 	}
 
 	protected void rebuildTree() throws Exception {
@@ -51,6 +57,44 @@ public class VerifyAsset extends VerifyProcess {
 				AssetCategoryLocalServiceUtil.rebuildTree(groupId, true);
 			}
 		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+	}
+
+	protected void removeOrphanedEntries() throws Exception {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			long classNameId = PortalUtil.getClassNameId(
+				DLFileEntryConstants.getClassName());
+
+			con = DataAccess.getUpgradeOptimizedConnection();
+
+			ps = con.prepareStatement(
+				"select entryId, classPK from AssetEntry where " +
+					"classNameId =" + classNameId + " and classPK not in" +
+					"(select fileVersionId from DLFileVersion);");
+
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				long classPK = rs.getLong("classPK");
+				long entryId = rs.getLong("entryId");
+
+				DLFileEntry dlFileEntry =
+					DLFileEntryLocalServiceUtil.fetchDLFileEntry(classPK);
+
+				if (dlFileEntry != null) {
+					continue;
+				}
+
+				AssetEntryLocalServiceUtil.deleteAssetEntry(entryId);
+			}
+		}
+
 		finally {
 			DataAccess.cleanUp(con, ps, rs);
 		}
