f6c50fa5fcc927b7709e07811987a9880fed747e
shuyangzhou <shuyang.zhou@liferay.com>
2015-05-17 13:59:28 -0700

    LPS-55691 Fix thread safety issues. Project's build listeners can be concurrently modified.

--- modules/util/ant-logger/src/com/liferay/ant/logger/LiferayLoggerTask.java
+++ modules/util/ant-logger/src/com/liferay/ant/logger/LiferayLoggerTask.java
@@ -14,7 +14,10 @@
 
 package com.liferay.ant.logger;
 
+import java.lang.reflect.Field;
+
 import org.apache.tools.ant.BuildEvent;
+import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.BuildListener;
 import org.apache.tools.ant.DefaultLogger;
 import org.apache.tools.ant.Project;
@@ -22,19 +25,45 @@ import org.apache.tools.ant.Task;
 
 /**
  * @author William Newbury
+ * @author Shuyang Zhou
  */
 public class LiferayLoggerTask extends Task {
 
 	@Override
-	public void execute() {
+	public void execute() throws BuildException {
 		Project currentProject = getProject();
 
-		for (BuildListener buildListener : currentProject.getBuildListeners()) {
-			if (buildListener.getClass() == DefaultLogger.class) {
-				currentProject.removeBuildListener(buildListener);
-				currentProject.addBuildListener(
-					new LiferayBuildLogger(buildListener));
+		try {
+			synchronized(_listenersLockField.get(currentProject)) {
+				for (BuildListener buildListener :
+						currentProject.getBuildListeners()) {
+
+					if (buildListener.getClass() == DefaultLogger.class) {
+						currentProject.removeBuildListener(buildListener);
+						currentProject.addBuildListener(
+							new LiferayBuildLogger(buildListener));
+					}
+				}
 			}
+		}
+		catch (IllegalAccessException iae) {
+			throw new BuildException(
+				"Unable to access listenersLock field of " + currentProject,
+				iae);
+		}
+	}
+
+	private static final Field _listenersLockField;
+
+	static {
+		try {
+			_listenersLockField = Project.class.getDeclaredField(
+				"listenersLock");
+
+			_listenersLockField.setAccessible(true);
+		}
+		catch (ReflectiveOperationException roe) {
+			throw new ExceptionInInitializerError(roe);
 		}
 	}
 
