5341f69e0a6c28967ae5602be9bb2fef5930c5f9
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-05-18 16:54:11 -0300

    LPS-54703 Add Upgrade Process

---
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-display-web/src/com/liferay/dynamic/data/lists/display/web/upgrade/DDLDisplayWebUpgrade.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.display.web.upgrade;
+
+import com.liferay.dynamic.data.lists.display.web.constants.DDLDisplayPortletKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.portal.upgrade.util.UpgradePortletId;
+
+import java.util.Collections;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marcellus Tavares
+ */
+@Component(immediate = true, service = DDLDisplayWebUpgrade.class)
+public class DDLDisplayWebUpgrade {
+
+	@Reference(unbind = "-")
+	protected void setReleaseLocalService(
+		ReleaseLocalService releaseLocalService) {
+
+		_releaseLocalService = releaseLocalService;
+	}
+
+	@Reference(target = "(original.bean=*)", unbind = "-")
+	protected void setServletContext(ServletContext servletContext) {
+	}
+
+	@Activate
+	protected void upgrade() throws PortalException {
+		UpgradePortletId upgradePortletId = new UpgradePortletId() {
+
+			@Override
+			protected String[][] getRenamePortletIdsArray() {
+				return new String[][] {
+					new String[] {
+						"169", DDLDisplayPortletKeys.DDL_DISPLAY
+					}
+				};
+			}
+
+		};
+
+		_releaseLocalService.updateRelease(
+			"com.liferay.dynamic.data.lists.display.web",
+			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 0,
+			false);
+	}
+
+	private ReleaseLocalService _releaseLocalService;
+
+}
