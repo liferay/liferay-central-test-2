2c4c8f30eec053304031def8f9713bc7ff030696
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-05-18 17:02:27 -0300

    LPS-54703 SF

--- modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/com/liferay/dynamic/data/lists/form/web/portlet/DDLFormPortlet.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/com/liferay/dynamic/data/lists/form/web/portlet/DDLFormPortlet.java
@@ -65,7 +65,7 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.render-weight=50",
 		"com.liferay.portlet.scopeable=true",
 		"com.liferay.portlet.use-default-template=true",
-		"javax.portlet.display-name=Dynamic Data Lists Form Display",
+		"javax.portlet.display-name=Dynamic Data Lists Form",
 		"javax.portlet.expiration-cache=0",
 		"javax.portlet.init-param.config-template=/configuration.jsp",
 		"javax.portlet.init-param.copy-request-parameters=true",
