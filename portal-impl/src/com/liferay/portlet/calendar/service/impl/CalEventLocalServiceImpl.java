9ca1927a8b448fe63e9de564bbf7d7bf15bbdf6a
Jonathan McCann <jonathan.mccann@liferay.com>
2015-05-18 10:49:01 -0700

    LPS-55675 Source Formatting

--- portal-impl/src/com/liferay/portlet/calendar/service/impl/CalEventLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/calendar/service/impl/CalEventLocalServiceImpl.java
@@ -1755,7 +1755,7 @@ public class CalEventLocalServiceImpl extends CalEventLocalServiceBaseImpl {
 			sb.append(", startDateMonth=");
 			sb.append(startDateMonth);
 			sb.append(", startDateDay=");
-			sb.append(startDateMonth);
+			sb.append(startDateDay);
 			sb.append(", startDateYear=");
 			sb.append(startDateYear);
 			sb.append(StringPool.CLOSE_BRACKET);
