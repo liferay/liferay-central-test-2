3f966c03434f5c009c76601e0037889fab018b4e
Raymond Auge <raymond.auge@liferay.com>
2015-05-19 17:03:04 -0400

    LPS-53802 SF

--- portal-impl/test/integration/com/liferay/portal/kernel/template/TemplateMangerUtilTest.java
+++ portal-impl/test/integration/com/liferay/portal/kernel/template/TemplateMangerUtilTest.java
@@ -14,14 +14,6 @@
 
 package com.liferay.portal.kernel.template;
 
-import java.util.Map;
-import java.util.Set;
-
-import org.junit.Assert;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
-
 import com.liferay.portal.kernel.template.bundle.templatemanagerutil.TestTemplate;
 import com.liferay.portal.kernel.template.bundle.templatemanagerutil.TestTemplateManager;
 import com.liferay.portal.kernel.template.bundle.templatemanagerutil.TestTemplateResource;
@@ -30,11 +22,18 @@ import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.MainServletTestRule;
 import com.liferay.portal.test.rule.SyntheticBundleRule;
 
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
 /**
  * @author Philip Jones
  */
 public class TemplateMangerUtilTest {
-
 
 	@ClassRule
 	@Rule
@@ -43,80 +42,73 @@ public class TemplateMangerUtilTest {
 			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE,
 			new SyntheticBundleRule("bundle.templatemanagerutil"));
 
-	
 	@Test
 	public void testGetTemplate1() throws TemplateException {
-	
 		TestTemplateResource testTemplateResource = new TestTemplateResource();
-		
+
 		Template template = TemplateManagerUtil.getTemplate(
-			TestTemplateManager.TEST_TEMPLATE_MANAGER_NAME, 
+			TestTemplateManager.TEST_TEMPLATE_MANAGER_NAME,
 			testTemplateResource, false);
-			
+
 		Class clazz = template.getClass();
-			
+
 		Assert.assertEquals(TestTemplate.class.getName(), clazz.getName());
 	}
 
 	@Test
-	public void testGetTemplate2() throws TemplateException{
-		
+	public void testGetTemplate2() throws TemplateException {
 		TestTemplateResource testTemplateResource = new TestTemplateResource();
-	
+
 		Template template = TemplateManagerUtil.getTemplate(
-			TestTemplateManager.TEST_TEMPLATE_MANAGER_NAME,	
+			TestTemplateManager.TEST_TEMPLATE_MANAGER_NAME,
 			testTemplateResource, null, false);
-		
+
 		Class clazz = template.getClass();
-		
+
 		Assert.assertEquals(TestTemplate.class.getName(), clazz.getName());
 	}
 
 	@Test
 	public void testGetTemplateManager() {
-		
-		TemplateManager templateManager = 
+		TemplateManager templateManager =
 			TemplateManagerUtil.getTemplateManager(
 				TestTemplateManager.TEST_TEMPLATE_MANAGER_NAME);
-		
+
 		Class clazz = templateManager.getClass();
-		
+
 		Assert.assertEquals(
 			TestTemplateManager.class.getName(), clazz.getName());
 	}
-	
+
 	@Test
 	public void testGetTemplateManagerName() {
-		
-		Set<String> templateManagerNames = 
+		Set<String> templateManagerNames =
 			TemplateManagerUtil.getTemplateManagerNames();
-		
+
 		Assert.assertTrue(
 			templateManagerNames.contains(
 				TestTemplateManager.TEST_TEMPLATE_MANAGER_NAME));
 	}
-	
+
 	@Test
 	public void testGetTemplateManagers() {
-		
-		Map<String, TemplateManager> templateManagers = 
+		Map<String, TemplateManager> templateManagers =
 			TemplateManagerUtil.getTemplateManagers();
-		
-		TemplateManager templateManager = 
-			templateManagers.get(
-				TestTemplateManager.TEST_TEMPLATE_MANAGER_NAME);
-		
+
+		TemplateManager templateManager = templateManagers.get(
+			TestTemplateManager.TEST_TEMPLATE_MANAGER_NAME);
+
 		Class clazz = templateManager.getClass();
-		
+
 		Assert.assertEquals(
 			TestTemplateManager.class.getName(), clazz.getName());
 	}
 
 	@Test
 	public void testHasTemplateManager() {
-		
 		Assert.assertTrue(
 			TemplateManagerUtil.hasTemplateManager(
 				TestTemplateManager.TEST_TEMPLATE_MANAGER_NAME));
 	}
+
 }
