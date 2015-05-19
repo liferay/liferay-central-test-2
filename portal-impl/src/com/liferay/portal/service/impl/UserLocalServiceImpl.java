57444de112acf412f19a42e89e1a8f382fe4e805
Vilmos Papp <vilmos.papp@liferay.com>
2015-05-19 15:50:48 +0200

    LPS-55706 Reset failed login counts on successful login

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -1188,7 +1188,7 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 	 * @throws PortalException if a portal exception occurred
 	 */
 	@Override
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	@Transactional(propagation = Propagation.SUPPORTS)
 	public long authenticateForBasic(
 			long companyId, String authType, String login, String password)
 		throws PortalException {
@@ -1231,6 +1231,8 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 			password, userPassword);
 
 		if (userPassword.equals(password) || userPassword.equals(encPassword)) {
+			resetFailedLoginAttempts(user);
+
 			return user.getUserId();
 		}
 
@@ -1259,7 +1261,7 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 	 * @throws PortalException if a portal exception occurred
 	 */
 	@Override
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	@Transactional(propagation = Propagation.SUPPORTS)
 	public long authenticateForDigest(
 			long companyId, String username, String realm, String nonce,
 			String method, String uri, String response)
@@ -1310,6 +1312,8 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 				Digester.MD5, ha1, nonce, ha2);
 
 			if (response.equals(curResponse)) {
+				resetFailedLoginAttempts(user);
+
 				return user.getUserId();
 			}
 		}
@@ -4674,9 +4678,8 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		user.setLoginIP(loginIP);
 		user.setLastLoginDate(lastLoginDate);
 		user.setLastLoginIP(lastLoginIP);
-		user.setFailedLoginAttempts(0);
 
-		userPersistence.update(user);
+		resetFailedLoginAttempts(user);
 
 		return user;
 	}
@@ -5901,6 +5904,9 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 			authResult = handleAuthenticationFailure(
 				login, authType, user, headerMap, parameterMap);
 		}
+		else {
+			resetFailedLoginAttempts(user);
+		}
 
 		// PLACEHOLDER 02
 
@@ -6200,6 +6206,12 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		};
 
 		TransactionCommitCallbackRegistryUtil.registerCallback(callable);
+	}
+
+	protected void resetFailedLoginAttempts(User user) {
+		user.setFailedLoginAttempts(0);
+
+		userPersistence.update(user);
 	}
 
 	protected BaseModelSearchResult<User> searchUsers(
