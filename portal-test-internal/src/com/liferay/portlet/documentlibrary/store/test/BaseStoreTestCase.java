f92c8869a06fa03f739c58fc1cb6f2181069ae79
Brian Chan <brian.chan@liferay.com>
2015-05-20 15:04:49 -0700

    LPS-54269 as used

--- portal-test-internal/src/com/liferay/portlet/documentlibrary/store/test/BaseStoreTestCase.java
+++ portal-test-internal/src/com/liferay/portlet/documentlibrary/store/test/BaseStoreTestCase.java
@@ -345,12 +345,16 @@ public abstract class BaseStoreTestCase {
 		String subdirectory = directory + "/" + RandomTestUtil.randomString();
 
 		String fileName1 = directory + "/" + RandomTestUtil.randomString();
+
+		store.addFile(companyId, repositoryId, fileName1, _DATA_VERSION_1);
+
 		String fileName2 = subdirectory + "/" + RandomTestUtil.randomString();
+
+		store.addFile(companyId, repositoryId, fileName2, _DATA_VERSION_1);
+
 		String fileName3 =
 			RandomTestUtil.randomString() + "/" + RandomTestUtil.randomString();
 
-		store.addFile(companyId, repositoryId, fileName1, _DATA_VERSION_1);
-		store.addFile(companyId, repositoryId, fileName2, _DATA_VERSION_1);
 		store.addFile(companyId, repositoryId, fileName3, _DATA_VERSION_1);
 
 		String[] fileNames = store.getFileNames(
