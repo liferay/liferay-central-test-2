c03eb104a1d73cd1535fb744704944d6dc5144a6
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-05-20 17:04:21 +0200

    LPS-55825 Define new interface that will transform Config object based on the information of the taglib, context information, and the EditorOptions object

---
+++ portal-service/src/com/liferay/portal/kernel/editor/configuration/EditorConfigTransformer.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.editor.configuration;
+
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.theme.ThemeDisplay;
+
+import java.util.Map;
+
+/**
+ * @author Sergio González
+ */
+public interface EditorConfigTransformer {
+
+	public void transform(
+		EditorOptions editorOptions,
+		Map<String, Object> inputEditorTaglibAttributes,
+		ThemeDisplay themeDisplay,
+		LiferayPortletResponse liferayPortletResponse,
+		JSONObject configJSONObject);
+
+}
