8c9cb84e423fd60c2f3fecc674f3f84534c6ced9
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2015-05-21 14:46:19 -0700

    LPS-54611 Maven

---
+++ modules/util/portal-tools-upgrade-table-builder/src/com/liferay/portal/tools/upgrade/table/builder/maven/UpgradeTableBuilderMojo.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.tools.upgrade.table.builder.maven;
+
+import com.liferay.portal.tools.upgrade.table.builder.UpgradeTableBuilderArgs;
+import com.liferay.portal.tools.upgrade.table.builder.UpgradeTableBuilderInvoker;
+
+import java.io.File;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * @author Andrea Di Giorgi
+ */
+public class UpgradeTableBuilderMojo extends AbstractMojo {
+
+	@Override
+	public void execute() throws MojoExecutionException {
+		try {
+			UpgradeTableBuilderInvoker.invoke(
+				baseDir, _upgradeTableBuilderArgs);
+		}
+		catch (Exception e) {
+			throw new MojoExecutionException(e.getMessage(), e);
+		}
+	}
+
+	public void setBaseDirName(String baseDirName) {
+		_upgradeTableBuilderArgs.setBaseDirName(baseDirName);
+	}
+
+	public void setOsgiModule(boolean osgiModule) {
+		_upgradeTableBuilderArgs.setOsgiModule(osgiModule);
+	}
+
+	public void setUpgradeTableDirName(String upgradeTableDirName) {
+		_upgradeTableBuilderArgs.setUpgradeTableDirName(upgradeTableDirName);
+	}
+
+	/**
+	 * @parameter default-value="${project.basedir}
+	 * @readonly
+	 */
+	protected File baseDir;
+
+	private final UpgradeTableBuilderArgs _upgradeTableBuilderArgs =
+		new UpgradeTableBuilderArgs();
+
+}
