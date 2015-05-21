50fa1534ba90fbe029430973045deefb5cb95e70
Juergen Kappler <jurgen.kappler@liferay.com>
2015-05-21 10:45:01 +0200

    LPS-55704 RSS: Add the display context to the palette

--- modules/apps/rss/rss-web/src/com/liferay/rss/web/portlet/template/RSSPortletDisplayTemplateHandler.java
+++ modules/apps/rss/rss-web/src/com/liferay/rss/web/portlet/template/RSSPortletDisplayTemplateHandler.java
@@ -23,6 +23,7 @@ import com.liferay.portal.util.PortalUtil;
 import com.liferay.portlet.portletdisplaytemplate.util.PortletDisplayTemplateConstants;
 import com.liferay.rss.web.configuration.RSSWebConfigurationValues;
 import com.liferay.rss.web.constants.RSSPortletKeys;
+import com.liferay.rss.web.display.context.RSSDisplayContext;
 import com.liferay.rss.web.util.RSSFeed;
 
 import java.util.List;
@@ -76,6 +77,10 @@ public class RSSPortletDisplayTemplateHandler
 			templateVariableGroups.get("fields");
 
 		templateVariableGroup.empty();
+
+		templateVariableGroup.addVariable(
+			"rss-display-context", RSSDisplayContext.class,
+			"rssDisplayContext");
 
 		templateVariableGroup.addCollectionVariable(
 			"rss-feeds", List.class, PortletDisplayTemplateConstants.ENTRIES,
