976932b7f0d95b28de7be2314e678f198f21a087
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-05-22 11:08:37 -0300

    LPS-55497 Rename

--- portal-service/src/com/liferay/portal/util/PortletKeys.java
+++ portal-service/src/com/liferay/portal/util/PortletKeys.java
@@ -135,7 +135,7 @@ public class PortletKeys {
 
 	public static final String PORTLET_CONFIGURATION = "86";
 
-	public static final String PORTLET_DISPLAY_TEMPLATES =
+	public static final String PORTLET_DISPLAY_TEMPLATE =
 		"com_liferay_dynamic_data_mapping_web_portlet_" +
 			"PortletDisplayTemplatePortlet";
 
