dc0388bc550eaa05f2be6fe97e364b6239ef813f
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-05-25 10:05:38 +0200

    LPS-55832 Copy MobileDeviceRulesAction to portlet, no logic changes.

--- portal-impl/src/com/liferay/portlet/layoutsadmin/LayoutsAdminPortlet.java
+++ portal-impl/src/com/liferay/portlet/layoutsadmin/LayoutsAdminPortlet.java
@@ -20,4 +20,48 @@ import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
  * @author Eudaldo Alonso
  */
 public class LayoutsAdminPortlet extends MVCPortlet {
+
+	@Override
+	public ActionForward render(
+			ActionMapping actionMapping, ActionForm actionForm,
+			PortletConfig portletConfig, RenderRequest renderRequest,
+			RenderResponse renderResponse)
+		throws Exception {
+
+		try {
+			ActionUtil.getGroup(renderRequest);
+		}
+		catch (Exception e) {
+			if (e instanceof NoSuchGroupException ||
+				e instanceof PrincipalException) {
+
+				SessionErrors.add(renderRequest, e.getClass());
+
+				return actionMapping.findForward("portlet.layouts_admin.error");
+			}
+			else {
+				throw e;
+			}
+		}
+
+		return actionMapping.findForward("portlet.layouts_admin.edit_layouts");
+	}
+
+	@Override
+	public void serveResource(
+			ActionMapping actionMapping, ActionForm actionForm,
+			PortletConfig portletConfig, ResourceRequest resourceRequest,
+			ResourceResponse resourceResponse)
+		throws Exception {
+
+		PortletContext portletContext = portletConfig.getPortletContext();
+
+		PortletRequestDispatcher portletRequestDispatcher =
+			portletContext.getRequestDispatcher(
+				"/html/portlet/layouts_admin/layout/" +
+					"mobile_device_rules_rule_group_instances.jsp");
+
+		portletRequestDispatcher.include(resourceRequest, resourceResponse);
+	}
+
 }
