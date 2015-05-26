204ab792311e7037f6120a222a452089980c303d
drewbrokke <drew.brokke@liferay.com>
2015-05-26 14:48:54 -0700

    LPS-55841 Revert "LPS-55841 Includes css file"
    This reverts commit 723613c3f39a8fa3df83edeec8afdb84033c9dbd.

--- modules/apps/password-generator/password-generator-web/src/com/liferay/password/generator/web/portlet/PasswordGeneratorPortlet.java
+++ modules/apps/password-generator/password-generator-web/src/com/liferay/password/generator/web/portlet/PasswordGeneratorPortlet.java
@@ -31,7 +31,6 @@ import org.osgi.service.component.annotations.Reference;
 	property = {
 		"com.liferay.portlet.css-class-wrapper=portlet-password-generator",
 		"com.liferay.portlet.display-category=category.tools",
-		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
 		"com.liferay.portlet.private-session-attributes=false",
