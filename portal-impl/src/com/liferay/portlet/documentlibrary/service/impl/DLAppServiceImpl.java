2e2f51c02b27bb419d1bb0b43f3bd736a9ad8784
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-05-26 14:08:52 +0200

    LPS-55859 Javadoc

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLAppServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLAppServiceImpl.java
@@ -856,7 +856,7 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 	}
 
 	/**
-	 * Returns a range of all the file entries in the folder.
+	 * Returns an ordered range of all the file entries in the folder.
 	 *
 	 * <p>
 	 * Useful when paginating results. Returns a maximum of <code>end -
@@ -872,7 +872,7 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 	 * @param  folderId the primary key of the file entry's folder
 	 * @param  start the lower bound of the range of results
 	 * @param  end the upper bound of the range of results (not inclusive)
-	 * @return the range of file entries in the folder
+	 * @return the range of the file entries in the folder ordered by name
 	 * @throws PortalException if the folder could not be found
 	 */
 	@Override
@@ -939,15 +939,15 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 	}
 
 	/**
-	 * Returns a range of all the file entries with the file entry type in the
-	 * folder.
+	 * Returns an ordered range of all the file entries with the file entry type
+	 * in the folder.
 	 *
 	 * @param  repositoryId the primary key of the file entry's repository
 	 * @param  folderId the primary key of the file entry's folder
 	 * @param  fileEntryTypeId the primary key of the file entry type
 	 * @param  start the lower bound of the range of results
 	 * @param  end the upper bound of the range of results (not inclusive)
-	 * @return the file entries in the folder
+	 * @return the range of the file entries in the folder ordered by name
 	 * @throws PortalException if the folder could not be found
 	 */
 	@Override
@@ -1319,8 +1319,8 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 	}
 
 	/**
-	 * Returns a range of all the immediate subfolders of the parent folder,
-	 * optionally including mount folders for third-party repositories.
+	 * Returns an ordered range of all the immediate subfolders of the parent
+	 * folder, optionally including mount folders for third-party repositories.
 	 *
 	 * <p>
 	 * Useful when paginating results. Returns a maximum of <code>end -
@@ -1338,7 +1338,8 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 	 *         third-party repositories
 	 * @param  start the lower bound of the range of results
 	 * @param  end the upper bound of the range of results (not inclusive)
-	 * @return the range of immediate subfolders of the parent folder
+	 * @return the range of immediate subfolders of the parent folder ordered by
+	 *         name
 	 * @throws PortalException if the parent folder could not be found
 	 */
 	@Override
@@ -1431,7 +1432,8 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 	}
 
 	/**
-	 * Returns a range of all the immediate subfolders of the parent folder.
+	 * Returns an ordered range of all the immediate subfolders of the parent
+	 * folder.
 	 *
 	 * <p>
 	 * Useful when paginating results. Returns a maximum of <code>end -
@@ -1447,7 +1449,8 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 	 * @param  parentFolderId the primary key of the folder's parent folder
 	 * @param  start the lower bound of the range of results
 	 * @param  end the upper bound of the range of results (not inclusive)
-	 * @return the range of immediate subfolders of the parent folder
+	 * @return the range of immediate subfolders of the parent folder ordered by
+	 *         name
 	 * @throws PortalException if the parent folder could not be found
 	 */
 	@Override
@@ -1496,8 +1499,8 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 	}
 
 	/**
-	 * Returns a range of all the immediate subfolders, file entries, and file
-	 * shortcuts in the parent folder.
+	 * Returns an ordered range of all the immediate subfolders, file entries,
+	 * and file shortcuts in the parent folder.
 	 *
 	 * <p>
 	 * Useful when paginating results. Returns a maximum of <code>end -
@@ -1517,8 +1520,7 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 	 * @param  start the lower bound of the range of results
 	 * @param  end the upper bound of the range of results (not inclusive)
 	 * @return the range of immediate subfolders, file entries, and file
-	 *         shortcuts in the parent folder ordered by comparator
-	 *         <code>obc</code>
+	 *         shortcuts in the parent folder ordered by name
 	 * @throws PortalException if the parent folder could not be found
 	 */
 	@Override
@@ -1942,9 +1944,9 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 	}
 
 	/**
-	 * Returns a range of all the immediate subfolders of the parent folder that
-	 * are used for mounting third-party repositories. This method is only
-	 * supported by the Liferay repository.
+	 * Returns an ordered range of all the immediate subfolders of the parent
+	 * folder that are used for mounting third-party repositories. This method
+	 * is only supported by the Liferay repository.
 	 *
 	 * <p>
 	 * Useful when paginating results. Returns a maximum of <code>end -
@@ -1961,7 +1963,7 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 	 * @param  start the lower bound of the range of results
 	 * @param  end the upper bound of the range of results (not inclusive)
 	 * @return the range of immediate subfolders of the parent folder that are
-	 *         used for mounting third-party repositories
+	 *         used for mounting third-party repositories ordered by name
 	 * @throws PortalException if the repository or parent folder could not be
 	 *         found
 	 */
