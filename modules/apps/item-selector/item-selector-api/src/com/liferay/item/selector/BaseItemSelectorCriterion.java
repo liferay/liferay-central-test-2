robertoDiaz/liferay-portal

LPS-55533 Fix detected issue. DesiredReturnTypes are not been used properly.

modules/apps/item-selector/item-selector-api/src/com/liferay/item/selector/BaseItemSelectorCriterion.java
@@ -35,18 +35,23 @@ public abstract class BaseItemSelectorCriterion
 	}
 
 	@Override
-	public void setDesiredReturnTypes(Class<?>... desiredReturnTypes) {
-		List<Class<?>> desiredReturnTypesSet = new ArrayList<>();
+	public void setDesiredReturnTypes(Class<?> ... desiredReturnType) {
+		List<Class<?>> desiredReturnTypesList = new ArrayList<>();
 
-		Collections.addAll(desiredReturnTypesSet, desiredReturnTypes);
+		Collections.addAll(desiredReturnTypesList, desiredReturnType);
 
-		if (!_availableReturnTypes.containsAll(desiredReturnTypesSet)) {
+		if (!_availableReturnTypes.containsAll(desiredReturnTypesList)) {
 			throw new IllegalArgumentException(
 				"Desired return types must be a subset of available return " +
 					"types");
 		}
 
-		_desiredReturnTypes = desiredReturnTypesSet;
+		_desiredReturnTypes = desiredReturnTypesList;
+	}
+
+	@Override
+	public void setDesiredReturnTypes(List<Class<?>> desiredReturnTypes) {
+		_desiredReturnTypes = desiredReturnTypes;
 	}
 
 	protected static List<Class<?>> getInmutableList(Class<?>... classes) {

modules/apps/item-selector/item-selector-api/src/com/liferay/item/selector/ItemSelectorCriterion.java
@@ -25,6 +25,8 @@ public interface ItemSelectorCriterion {
 
 	public List<Class<?>> getDesiredReturnTypes();
 
-	public void setDesiredReturnTypes(Class<?>... desiredReturnTypes);
+	public void setDesiredReturnTypes(Class<?> ... desiredReturnTypes);
+
+	public void setDesiredReturnTypes(List<Class<?>> desiredReturnTypes);
 
 }

liferay/liferay-central-test-2

modules/apps/item-selector/item-selector-api/src/com/liferay/item/selector/BaseItemSelectorCriterion.java
@@ -16,6 +16,7 @@ package com.liferay.item.selector;
 
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 /**
@@ -35,18 +36,14 @@ public abstract class BaseItemSelectorCriterion
 	}
 
 	@Override
-	public void setDesiredReturnTypes(Class<?>... desiredReturnTypes) {
-		Set<Class<?>> desiredReturnTypesSet = new HashSet<>();
-
-		Collections.addAll(desiredReturnTypesSet, desiredReturnTypes);
-
-		if (!_availableReturnTypes.containsAll(desiredReturnTypesSet)) {
+	public void setDesiredReturnTypes(Set<Class<?>> desiredReturnTypes) {
+		if (!_availableReturnTypes.containsAll(desiredReturnTypes)) {
 			throw new IllegalArgumentException(
 				"Desired return types must be a subset of available return " +
 					"types");
 		}
 
-		_desiredReturnTypes = desiredReturnTypesSet;
+		_desiredReturnTypes = desiredReturnTypes;
 	}
 
 	protected static Set<Class<?>> getInmutableSet(Class<?>... classes) {

modules/apps/item-selector/item-selector-api/src/com/liferay/item/selector/ItemSelectorCriterion.java
@@ -25,6 +25,6 @@ public interface ItemSelectorCriterion {
 
 	public Set<Class<?>> getDesiredReturnTypes();
 
-	public void setDesiredReturnTypes(Class<?>... desiredReturnTypes);
+	public void setDesiredReturnTypes(Set<Class<?>> desiredReturnTypes);
 
 }