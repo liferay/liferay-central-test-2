a1671fb0731b7fbef22092085be581f84202b7a2
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-05-28 15:00:07 -0300

    LPS-54704 Add Upgrade Process

---
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/com/liferay/dynamic/data/lists/upgrade/DDLServiceUpgrade.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.upgrade;
+
+import com.liferay.dynamic.data.lists.upgrade.v1_0_0.DDLClassNamesUpgradeProcess;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.service.ReleaseLocalService;
+
+import java.util.Collections;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import org.springframework.context.ApplicationContext;
+
+/**
+ * @author Marcellus Tavares
+ */
+@Component(immediate = true, service = DDLServiceUpgrade.class)
+public class DDLServiceUpgrade {
+
+	@Reference(
+		target =
+			"(org.springframework.context.service.name=" +
+				"com.liferay.dynamic.data.lists.service)",
+		unbind = "-"
+	)
+	protected void setApplicationContext(
+		ApplicationContext applicationContext) {
+	}
+
+	@Reference(unbind = "-")
+	protected void setReleaseLocalService(
+		ReleaseLocalService releaseLocalService) {
+
+		_releaseLocalService = releaseLocalService;
+	}
+
+	@Activate
+	protected void upgrade() throws PortalException {
+		UpgradeProcess upgradeProcess = new DDLClassNamesUpgradeProcess();
+
+		_releaseLocalService.updateRelease(
+			"com.liferay.dynamic.data.lists.service",
+			Collections.<UpgradeProcess>singletonList(upgradeProcess), 1, 0,
+			false);
+	}
+
+	private ReleaseLocalService _releaseLocalService;
+
+}
---
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/com/liferay/dynamic/data/lists/upgrade/v1_0_0/DDLClassNamesUpgradeProcess.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.upgrade.v1_0_0;
+
+import com.liferay.dynamic.data.lists.model.DDLRecord;
+import com.liferay.dynamic.data.lists.model.DDLRecordSet;
+import com.liferay.dynamic.data.lists.model.DDLRecordVersion;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.upgrade.util.classname.ClassNameDependency;
+import com.liferay.portal.upgrade.util.classname.ClassNameDependencyUpgrader;
+import com.liferay.portal.upgrade.util.classname.dependency.ResourcePermissionClassNameDependency;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Marcellus Tavares
+ */
+public class DDLClassNamesUpgradeProcess extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		List<ClassNameDependency> classNameDependencies =
+			Collections.singletonList(
+				(ClassNameDependency)(
+					new ResourcePermissionClassNameDependency()));
+
+		upgradeDDLClassName(classNameDependencies);
+		upgradeDDLRecordClassName(classNameDependencies);
+		upgradeDDLRecordSetClassName(classNameDependencies);
+		upgradeDDLRecordSetClassName(classNameDependencies);
+	}
+
+	protected void upgradeDDLClassName(
+		List<ClassNameDependency> classNameDependencies) {
+
+		ClassNameDependencyUpgrader classNameDependencyUpgrader =
+			new ClassNameDependencyUpgrader(
+				"com.liferay.portlet.dynamicdatalists",
+				"com.liferay.dynamic.data.lists", classNameDependencies);
+
+		classNameDependencyUpgrader.upgrade();
+	}
+
+	protected void upgradeDDLRecordClassName(
+		List<ClassNameDependency> classNameDependencies) {
+
+		ClassNameDependencyUpgrader classNameDependencyUpgrader =
+			new ClassNameDependencyUpgrader(
+				"com.liferay.portlet.dynamicdatalists.model.DDLRecord",
+				DDLRecord.class.getName(), classNameDependencies);
+
+		classNameDependencyUpgrader.upgrade();
+	}
+
+	protected void upgradeDDLRecordSetClassName(
+		List<ClassNameDependency> classNameDependencies) {
+
+		ClassNameDependencyUpgrader classNameDependencyUpgrader =
+			new ClassNameDependencyUpgrader(
+				"com.liferay.portlet.dynamicdatalists.model.DDLRecordSet",
+				DDLRecordSet.class.getName(), classNameDependencies);
+
+		classNameDependencyUpgrader.upgrade();
+	}
+
+	protected void upgradeDDLRecordVersionClassName(
+		List<ClassNameDependency> classNameDependencies) {
+
+		ClassNameDependencyUpgrader classNameDependencyUpgrader =
+			new ClassNameDependencyUpgrader(
+				"com.liferay.portlet.dynamicdatalists.model.DDLRecordVersion",
+				DDLRecordVersion.class.getName(), classNameDependencies);
+
+		classNameDependencyUpgrader.upgrade();
+	}
+
+}
