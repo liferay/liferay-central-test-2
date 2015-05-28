86dcec4ec1e5aec4adbc9a23de708853027fd2d1
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-05-28 12:36:22 -0300

    LPS-54704 Regenerate dynamic-data-lists-service resources

---
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/META-INF/sql/tables.sql
@@ -0,0 +1,51 @@
+create table DDLRecord (
+	uuid_ VARCHAR(75) null,
+	recordId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	versionUserId LONG,
+	versionUserName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	DDMStorageId LONG,
+	recordSetId LONG,
+	version VARCHAR(75) null,
+	displayIndex INTEGER
+);
+
+create table DDLRecordSet (
+	uuid_ VARCHAR(75) null,
+	recordSetId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	DDMStructureId LONG,
+	recordSetKey VARCHAR(75) null,
+	name STRING null,
+	description STRING null,
+	minDisplayRows INTEGER,
+	scope INTEGER
+);
+
+create table DDLRecordVersion (
+	recordVersionId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	DDMStorageId LONG,
+	recordSetId LONG,
+	recordId LONG,
+	version VARCHAR(75) null,
+	displayIndex INTEGER,
+	status INTEGER,
+	statusByUserId LONG,
+	statusByUserName VARCHAR(75) null,
+	statusDate DATE null
+);
