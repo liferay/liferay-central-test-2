525a087c20133876d26e349d6a5666c4877c78e7
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2015-05-28 21:53:23 +0200

    LPS-55585 Remove verifier package, AuthVerifier is already in the class name

--- modules/portal-security/portal-security-auth/src/com/liferay/portal/security/auth/impl/PortalSessionAuthVerifier.java
+++ modules/portal-security/portal-security-auth/src/com/liferay/portal/security/auth/impl/PortalSessionAuthVerifier.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.security.auth.impl.verifier;
+package com.liferay.portal.security.auth.impl;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
