7d6f08d1138a2db96ddc90096abfc6559232f2de
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-05-28 16:04:44 -0300

    LPS-54704 Remove remaining references from portal

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -104,18 +104,6 @@ create unique index IX_717B9BA2 on Country (a3);
 create index IX_25D734CD on Country (active_);
 create unique index IX_19DA007B on Country (name);
 
-create index IX_6A6C1C85 on DDLRecord (companyId);
-create index IX_AAC564D3 on DDLRecord (recordSetId, userId);
-create index IX_384AB6F7 on DDLRecord (uuid_, companyId);
-create unique index IX_B4328F39 on DDLRecord (uuid_, groupId);
-
-create unique index IX_56DAB121 on DDLRecordSet (groupId, recordSetKey);
-create index IX_5938C39F on DDLRecordSet (uuid_, companyId);
-create unique index IX_270BA5E1 on DDLRecordSet (uuid_, groupId);
-
-create index IX_762ADC7 on DDLRecordVersion (recordId, status);
-create unique index IX_C79E347 on DDLRecordVersion (recordId, version);
-
 create index IX_E3BAF436 on DDMContent (companyId);
 create index IX_50BF1038 on DDMContent (groupId);
 create index IX_3A9C0626 on DDMContent (uuid_, companyId);
--- sql/portal-tables.sql
+++ sql/portal-tables.sql
@@ -384,58 +384,6 @@ create table CyrusVirtual (
 	userId VARCHAR(75) not null
 );
 
-create table DDLRecord (
-	uuid_ VARCHAR(75) null,
-	recordId LONG not null primary key,
-	groupId LONG,
-	companyId LONG,
-	userId LONG,
-	userName VARCHAR(75) null,
-	versionUserId LONG,
-	versionUserName VARCHAR(75) null,
-	createDate DATE null,
-	modifiedDate DATE null,
-	DDMStorageId LONG,
-	recordSetId LONG,
-	version VARCHAR(75) null,
-	displayIndex INTEGER
-);
-
-create table DDLRecordSet (
-	uuid_ VARCHAR(75) null,
-	recordSetId LONG not null primary key,
-	groupId LONG,
-	companyId LONG,
-	userId LONG,
-	userName VARCHAR(75) null,
-	createDate DATE null,
-	modifiedDate DATE null,
-	DDMStructureId LONG,
-	recordSetKey VARCHAR(75) null,
-	name STRING null,
-	description STRING null,
-	minDisplayRows INTEGER,
-	scope INTEGER
-);
-
-create table DDLRecordVersion (
-	recordVersionId LONG not null primary key,
-	groupId LONG,
-	companyId LONG,
-	userId LONG,
-	userName VARCHAR(75) null,
-	createDate DATE null,
-	DDMStorageId LONG,
-	recordSetId LONG,
-	recordId LONG,
-	version VARCHAR(75) null,
-	displayIndex INTEGER,
-	status INTEGER,
-	statusByUserId LONG,
-	statusByUserName VARCHAR(75) null,
-	statusDate DATE null
-);
-
 create table DDMContent (
 	uuid_ VARCHAR(75) null,
 	contentId LONG not null primary key,
