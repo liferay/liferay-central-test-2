liferay/liferay-central-test-2

commit cc75255f5a248ef829e8d9209b58042714df0559

LPS-51081 sort

modules/sdk/gradle-plugins/src/com/liferay/gradle/plugins/tasks/InitGradleTask.java
@@ -144,7 +144,7 @@ public class InitGradleTask extends DefaultTask {
 				contents.add(
 					wrapDependency(
 						JavaPlugin.COMPILE_CONFIGURATION_NAME, group, name,
-						version, transitive, optional));
+						optional, transitive, version));
 			}
 		}
 
@@ -237,7 +237,7 @@ public class InitGradleTask extends DefaultTask {
 				contents.add(
 					wrapDependency(
 						ProvidedBasePlugin.getPROVIDED_CONFIGURATION_NAME(),
-						group, name, version, false));
+						group, name, false, version));
 			}
 		}
 
@@ -279,7 +279,7 @@ public class InitGradleTask extends DefaultTask {
 
 					contents.add(
 						wrapDependency(
-							configurationName, group, name, version, false));
+							configurationName, group, name, false, version));
 				}
 			}
 		}
@@ -311,7 +311,7 @@ public class InitGradleTask extends DefaultTask {
 				contents.add(
 					wrapDependency(
 						JavaPlugin.TEST_COMPILE_CONFIGURATION_NAME, group, name,
-						version, true));
+						true, version));
 			}
 		}
 
@@ -501,16 +501,8 @@ public class InitGradleTask extends DefaultTask {
 	}
 
 	protected String wrapDependency(
-		String configurationName, String group, String name, String version,
-		boolean transitive) {
-
-		return wrapDependency(
-			configurationName, group, name, version, transitive, false);
-	}
-
-	protected String wrapDependency(
-		String configurationName, String group, String name, String version,
-		boolean transitive, boolean optional) {
+		String configurationName, String group, String name, boolean optional,
+		boolean transitive, String version) {
 
 		StringBuilder sb = new StringBuilder();
 
@@ -520,20 +512,29 @@ public class InitGradleTask extends DefaultTask {
 		sb.append(group);
 		sb.append("\", name: \"");
 		sb.append(name);
-		sb.append("\", version: \"");
-		sb.append(version);
-		sb.append('\"');
-
-		if (!transitive) {
-			sb.append(", transitive: false");
-		}
 
 		if (optional) {
 			sb.append(", ");
 			sb.append(OptionalBasePlugin.getOPTIONAL_IDENTIFIER());
 		}
 
+		if (!transitive) {
+			sb.append(", transitive: false");
+		}
+
+		sb.append("\", version: \"");
+		sb.append(version);
+		sb.append('\"');
+
 		return sb.toString();
+	}
+
+	protected String wrapDependency(
+		String configurationName, String group, String name, boolean transitive,
+		String version) {
+
+		return wrapDependency(
+			configurationName, group, name, false, transitive, version);
 	}
 
 	protected String wrapProjectDependency(

commit 0397b87f5d3ea16e5d8532438e0f4e36195381fd

LPS-51081 Fix issue caused by cc75255f5a248ef829e8d9209b58042714df0559

modules/sdk/gradle-plugins/src/com/liferay/gradle/plugins/tasks/InitGradleTask.java
@@ -513,6 +513,7 @@ public class InitGradleTask extends DefaultTask {
 		sb.append(group);
 		sb.append("\", name: \"");
 		sb.append(name);
+		sb.append('\"');
 
 		if (optional) {
 			sb.append(", ");
@@ -523,7 +524,7 @@ public class InitGradleTask extends DefaultTask {
 			sb.append(", transitive: false");
 		}
 
-		sb.append("\", version: \"");
+		sb.append(", version: \"");
 		sb.append(version);
 		sb.append('\"');
 