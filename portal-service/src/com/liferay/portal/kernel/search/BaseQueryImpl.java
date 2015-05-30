mhan810/liferay-portal

LPS-55944 add preFilter to a query

portal-service/src/com/liferay/portal/kernel/search/BaseQueryImpl.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.kernel.search;
 
+import com.liferay.portal.kernel.search.filter.Filter;
 import com.liferay.portal.kernel.search.query.QueryVisitor;
 
 /**
@@ -29,6 +30,11 @@ public abstract class BaseQueryImpl implements Query {
 	@Override
 	public float getBoost() {
 		return _boost;
+	}
+
+	@Override
+	public Filter getPreFilter() {
+		return _preFilter;
 	}
 
 	@Override
@@ -58,11 +64,17 @@ public abstract class BaseQueryImpl implements Query {
 	}
 
 	@Override
+	public void setPreFilter(Filter preFilter) {
+		_preFilter = preFilter;
+	}
+
+	@Override
 	public void setQueryConfig(QueryConfig queryConfig) {
 		_queryConfig = queryConfig;
 	}
 
 	private float _boost = BOOST_DEFAULT;
+	private Filter _preFilter;
 	private QueryConfig _queryConfig;
 
 }

portal-service/src/com/liferay/portal/kernel/search/Query.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.kernel.search;
 
+import com.liferay.portal.kernel.search.filter.Filter;
 import com.liferay.portal.kernel.search.query.QueryVisitor;
 
 import java.io.Serializable;
@@ -29,6 +30,8 @@ public interface Query extends Serializable {
 
 	public float getBoost();
 
+	public Filter getPreFilter();
+
 	public QueryConfig getQueryConfig();
 
 	public Object getWrappedQuery();
@@ -36,6 +39,8 @@ public interface Query extends Serializable {
 	public boolean isDefaultBoost();
 
 	public void setBoost(float boost);
+
+	public void setPreFilter(Filter filter);
 
 	public void setQueryConfig(QueryConfig queryConfig);
 

liferay/liferay-central-test-2

portal-service/src/com/liferay/portal/kernel/search/BaseQueryImpl.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.kernel.search;
 
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
 import com.liferay.portal.kernel.search.query.QueryVisitor;
 
 /**
@@ -29,6 +30,11 @@ public abstract class BaseQueryImpl implements Query {
 	@Override
 	public float getBoost() {
 		return _boost;
+	}
+
+	@Override
+	public BooleanFilter getPreFilter() {
+		return _preFilter;
 	}
 
 	@Override
@@ -58,11 +64,17 @@ public abstract class BaseQueryImpl implements Query {
 	}
 
 	@Override
+	public void setPreFilter(BooleanFilter preFilter) {
+		_preFilter = preFilter;
+	}
+
+	@Override
 	public void setQueryConfig(QueryConfig queryConfig) {
 		_queryConfig = queryConfig;
 	}
 
 	private float _boost = BOOST_DEFAULT;
+	private BooleanFilter _preFilter;
 	private QueryConfig _queryConfig;
 
 }

portal-service/src/com/liferay/portal/kernel/search/Query.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.kernel.search;
 
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
 import com.liferay.portal.kernel.search.query.QueryVisitor;
 
 import java.io.Serializable;
@@ -29,6 +30,8 @@ public interface Query extends Serializable {
 
 	public float getBoost();
 
+	public BooleanFilter getPreFilter();
+
 	public QueryConfig getQueryConfig();
 
 	public Object getWrappedQuery();
@@ -36,6 +39,8 @@ public interface Query extends Serializable {
 	public boolean isDefaultBoost();
 
 	public void setBoost(float boost);
+
+	public void setPreFilter(BooleanFilter preFilter);
 
 	public void setQueryConfig(QueryConfig queryConfig);
 