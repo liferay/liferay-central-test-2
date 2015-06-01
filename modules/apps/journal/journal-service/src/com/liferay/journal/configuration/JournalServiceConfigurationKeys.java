fcfc081d8da78bca94ba146e5837ce6995a700e6
Eduardo Garcia <eduardo.garcia@liferay.com>
2015-06-02 00:13:28 +0200

    LPS-56383 Properties: update portlet properties

--- modules/apps/journal/journal-service/src/com/liferay/journal/configuration/JournalServiceConfigurationKeys.java
+++ modules/apps/journal/journal-service/src/com/liferay/journal/configuration/JournalServiceConfigurationKeys.java
@@ -19,6 +19,62 @@ package com.liferay.journal.configuration;
  */
 public class JournalServiceConfigurationKeys {
 
+	public static final String CHAR_BLACKLIST = "char.blacklist";
+
+	public static final String EMAIL_ARTICLE_ADDED_BODY = "email.article.added.body";
+
+	public static final String EMAIL_ARTICLE_ADDED_ENABLED = "email.article.added.enabled";
+
+	public static final String EMAIL_ARTICLE_ADDED_SUBJECT = "email.article.added.subject";
+
+	public static final String EMAIL_ARTICLE_APPROVAL_DENIED_BODY = "email.article.approval.denied.body";
+
+	public static final String EMAIL_ARTICLE_APPROVAL_DENIED_ENABLED = "email.article.approval.denied.enabled";
+
+	public static final String EMAIL_ARTICLE_APPROVAL_DENIED_SUBJECT = "email.article.approval.denied.subject";
+
+	public static final String EMAIL_ARTICLE_APPROVAL_GRANTED_BODY = "email.article.approval.granted.body";
+
+	public static final String EMAIL_ARTICLE_APPROVAL_GRANTED_ENABLED = "email.article.approval.granted.enabled";
+
+	public static final String EMAIL_ARTICLE_APPROVAL_GRANTED_SUBJECT = "email.article.approval.granted.subject";
+
+	public static final String EMAIL_ARTICLE_APPROVAL_REQUESTED_BODY = "email.article.approval.requested.body";
+
+	public static final String EMAIL_ARTICLE_APPROVAL_REQUESTED_ENABLED = "email.article.approval.requested.enabled";
+
+	public static final String EMAIL_ARTICLE_APPROVAL_REQUESTED_SUBJECT = "email.article.approval.requested.subject";
+
+	public static final String EMAIL_ARTICLE_MOVED_FROM_FOLDER_BODY = "email.article.moved.from.folder.body";
+
+	public static final String EMAIL_ARTICLE_MOVED_FROM_FOLDER_ENABLED = "email.article.moved.from.folder.enabled";
+
+	public static final String EMAIL_ARTICLE_MOVED_FROM_FOLDER_SUBJECT = "email.article.moved.from.folder.subject";
+
+	public static final String EMAIL_ARTICLE_MOVED_TO_FOLDER_BODY = "email.article.moved.to.folder.body";
+
+	public static final String EMAIL_ARTICLE_MOVED_TO_FOLDER_ENABLED = "email.article.moved.to.folder.enabled";
+
+	public static final String EMAIL_ARTICLE_MOVED_TO_FOLDER_SUBJECT = "email.article.moved.to.folder.subject";
+
+	public static final String EMAIL_ARTICLE_REVIEW_BODY = "email.article.review.body";
+
+	public static final String EMAIL_ARTICLE_REVIEW_ENABLED = "email.article.review.enabled";
+
+	public static final String EMAIL_ARTICLE_REVIEW_SUBJECT = "email.article.review.subject";
+
+	public static final String EMAIL_ARTICLE_UPDATED_BODY = "email.article.updated.body";
+
+	public static final String EMAIL_ARTICLE_UPDATED_ENABLED = "email.article.updated.enabled";
+
+	public static final String EMAIL_ARTICLE_UPDATED_SUBJECT = "email.article.updated.subject";
+
+	public static final String EMAIL_FROM_ADDRESS = "email.from.address";
+
+	public static final String EMAIL_FROM_NAME = "email.from.name";
+
+	public static final String ERROR_TEMPLATE = "error.template";
+
 	public static final String JOURNAL_ARTICLE_CHECK_INTERVAL = "journal.article.check.interval";
 
 	public static final String JOURNAL_ARTICLE_COMMENTS_ENABLED = "journal.article.comments.enabled";
@@ -39,72 +95,16 @@ public class JournalServiceConfigurationKeys {
 
 	public static final String JOURNAL_ARTICLE_VIEW_PERMISSION_CHECK_ENABLED = "journal.article.view.permission.check.enabled";
 
-	public static final String JOURNAL_CHAR_BLACKLIST = "journal.char.blacklist";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_ADDED_BODY = "journal.email.article.added.body";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_ADDED_ENABLED = "journal.email.article.added.enabled";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_ADDED_SUBJECT = "journal.email.article.added.subject";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_APPROVAL_DENIED_BODY = "journal.email.article.approval.denied.body";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_APPROVAL_DENIED_ENABLED = "journal.email.article.approval.denied.enabled";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_APPROVAL_DENIED_SUBJECT = "journal.email.article.approval.denied.subject";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_APPROVAL_GRANTED_BODY = "journal.email.article.approval.granted.body";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_APPROVAL_GRANTED_ENABLED = "journal.email.article.approval.granted.enabled";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_APPROVAL_GRANTED_SUBJECT = "journal.email.article.approval.granted.subject";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_APPROVAL_REQUESTED_BODY = "journal.email.article.approval.requested.body";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_APPROVAL_REQUESTED_ENABLED = "journal.email.article.approval.requested.enabled";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_APPROVAL_REQUESTED_SUBJECT = "journal.email.article.approval.requested.subject";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_MOVED_FROM_FOLDER_BODY = "journal.email.article.moved.from.folder.body";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_MOVED_FROM_FOLDER_ENABLED = "journal.email.article.moved.from.folder.enabled";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_MOVED_FROM_FOLDER_SUBJECT = "journal.email.article.moved.from.folder.subject";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_MOVED_TO_FOLDER_BODY = "journal.email.article.moved.to.folder.body";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_MOVED_TO_FOLDER_ENABLED = "journal.email.article.moved.to.folder.enabled";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_MOVED_TO_FOLDER_SUBJECT = "journal.email.article.moved.to.folder.subject";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_REVIEW_BODY = "journal.email.article.review.body";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_REVIEW_ENABLED = "journal.email.article.review.enabled";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_REVIEW_SUBJECT = "journal.email.article.review.subject";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_UPDATED_BODY = "journal.email.article.updated.body";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_UPDATED_ENABLED = "journal.email.article.updated.enabled";
-
-	public static final String JOURNAL_EMAIL_ARTICLE_UPDATED_SUBJECT = "journal.email.article.updated.subject";
-
-	public static final String JOURNAL_EMAIL_FROM_ADDRESS = "journal.email.from.address";
-
-	public static final String JOURNAL_EMAIL_FROM_NAME = "journal.email.from.name";
-
-	public static final String JOURNAL_ERROR_TEMPLATE = "journal.error.template";
-
 	public static final String JOURNAL_FOLDER_ICON_CHECK_COUNT = "journal.folder.icon.check.count";
 
-	public static final String JOURNAL_LAR_CREATION_STRATEGY = "journal.lar.creation.strategy";
+	public static final String LAR_CREATION_STRATEGY = "lar.creation.strategy";
 
-	public static final String JOURNAL_SYNC_CONTENT_SEARCH_ON_STARTUP = "journal.sync.content.search.on.startup";
-
-	public static final String JOURNAL_TRANSFORMER_LISTENER = "journal.transformer.listener";
+	public static final String SYNC_CONTENT_SEARCH_ON_STARTUP = "sync.content.search.on.startup";
 
 	public static final String TERMS_OF_USE_JOURNAL_ARTICLE_GROUP_ID = "terms.of.use.journal.article.group.id";
 
 	public static final String TERMS_OF_USE_JOURNAL_ARTICLE_ID = "terms.of.use.journal.article.id";
+
+	public static final String TRANSFORMER_LISTENER = "transformer.listener";
 
 }
--- modules/apps/journal/journal-service/src/com/liferay/journal/configuration/JournalServiceConfigurationValues.java
+++ modules/apps/journal/journal-service/src/com/liferay/journal/configuration/JournalServiceConfigurationValues.java
@@ -21,68 +21,86 @@ import com.liferay.portal.kernel.util.GetterUtil;
  */
 public class JournalServiceConfigurationValues {
 
-	public static final int JOURNAL_ARTICLE_CHECK_INTERVAL = GetterUtil.getInteger(PropsUtil.get(PropsKeys.JOURNAL_ARTICLE_CHECK_INTERVAL));
+	public static final String[] CHAR_BLACKLIST = JournalServiceConfigurationUtil.getArray(JournalServiceConfigurationKeys.CHAR_BLACKLIST);
 
-	public static final boolean JOURNAL_ARTICLE_COMMENTS_ENABLED = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.JOURNAL_ARTICLE_COMMENTS_ENABLED));
+	public static final String EMAIL_ARTICLE_ADDED_BODY = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_ADDED_BODY);
 
-	public static final String[] JOURNAL_ARTICLE_CUSTOM_TOKENS = PropsUtil.getArray(PropsKeys.JOURNAL_ARTICLE_CUSTOM_TOKENS);
+	public static final boolean EMAIL_ARTICLE_ADDED_ENABLED = GetterUtil.getBoolean(JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_ADDED_ENABLED));
 
-	public static final boolean JOURNAL_ARTICLE_EXPIRE_ALL_VERSIONS = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.JOURNAL_ARTICLE_EXPIRE_ALL_VERSIONS));
+	public static final String EMAIL_ARTICLE_ADDED_SUBJECT = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_ADDED_SUBJECT);
 
-	public static boolean JOURNAL_ARTICLE_INDEX_ALL_VERSIONS = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.JOURNAL_ARTICLE_INDEX_ALL_VERSIONS));
+	public static final String EMAIL_ARTICLE_APPROVAL_DENIED_BODY = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_APPROVAL_DENIED_BODY);
 
-	public static boolean JOURNAL_ARTICLE_DATABASE_KEYWORD_SEARCH_CONTENT = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.JOURNAL_ARTICLE_DATABASE_KEYWORD_SEARCH_CONTENT));
+	public static final boolean EMAIL_ARTICLE_APPROVAL_DENIED_ENABLED = GetterUtil.getBoolean(JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_APPROVAL_DENIED_ENABLED));
 
-	public static final String JOURNAL_ARTICLE_STORAGE_TYPE = PropsUtil.get(PropsKeys.JOURNAL_ARTICLE_STORAGE_TYPE);
+	public static final String EMAIL_ARTICLE_APPROVAL_DENIED_SUBJECT = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_APPROVAL_DENIED_SUBJECT);
 
-	public static final String JOURNAL_ARTICLE_TOKEN_PAGE_BREAK = PropsUtil.get(PropsKeys.JOURNAL_ARTICLE_TOKEN_PAGE_BREAK);
+	public static final String EMAIL_ARTICLE_APPROVAL_GRANTED_BODY = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_APPROVAL_GRANTED_BODY);
 
-	public static final boolean JOURNAL_ARTICLE_VIEW_PERMISSION_CHECK_ENABLED = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.JOURNAL_ARTICLE_VIEW_PERMISSION_CHECK_ENABLED));
+	public static final boolean EMAIL_ARTICLE_APPROVAL_GRANTED_ENABLED = GetterUtil.getBoolean(JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_APPROVAL_GRANTED_ENABLED));
 
-	public static final String[] JOURNAL_CHAR_BLACKLIST = PropsUtil.getArray(PropsKeys.JOURNAL_CHAR_BLACKLIST);
+	public static final String EMAIL_ARTICLE_APPROVAL_GRANTED_SUBJECT = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_APPROVAL_GRANTED_SUBJECT);
 
-	public static final String JOURNAL_EMAIL_ARTICLE_ADDED_BODY = PropsUtil.get(PropsKeys.JOURNAL_EMAIL_ARTICLE_ADDED_BODY);
+	public static final String EMAIL_ARTICLE_APPROVAL_REQUESTED_BODY = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_APPROVAL_REQUESTED_BODY);
 
-	public static final String JOURNAL_EMAIL_ARTICLE_ADDED_SUBJECT = PropsUtil.get(PropsKeys.JOURNAL_EMAIL_ARTICLE_ADDED_SUBJECT);
+	public static final boolean EMAIL_ARTICLE_APPROVAL_REQUESTED_ENABLED = GetterUtil.getBoolean(JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_APPROVAL_REQUESTED_ENABLED));
 
-	public static final String JOURNAL_EMAIL_ARTICLE_APPROVAL_DENIED_BODY = PropsUtil.get(PropsKeys.JOURNAL_EMAIL_ARTICLE_APPROVAL_DENIED_BODY);
+	public static final String EMAIL_ARTICLE_APPROVAL_REQUESTED_SUBJECT = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_APPROVAL_REQUESTED_SUBJECT);
 
-	public static final String JOURNAL_EMAIL_ARTICLE_APPROVAL_DENIED_SUBJECT = PropsUtil.get(PropsKeys.JOURNAL_EMAIL_ARTICLE_APPROVAL_DENIED_SUBJECT);
+	public static final String EMAIL_ARTICLE_MOVED_FROM_FOLDER_BODY = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_MOVED_FROM_FOLDER_BODY);
 
-	public static final String JOURNAL_EMAIL_ARTICLE_APPROVAL_GRANTED_BODY = PropsUtil.get(PropsKeys.JOURNAL_EMAIL_ARTICLE_APPROVAL_GRANTED_BODY);
+	public static final boolean EMAIL_ARTICLE_MOVED_FROM_FOLDER_ENABLED = GetterUtil.getBoolean(JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_MOVED_FROM_FOLDER_ENABLED));
 
-	public static final String JOURNAL_EMAIL_ARTICLE_APPROVAL_GRANTED_SUBJECT = PropsUtil.get(PropsKeys.JOURNAL_EMAIL_ARTICLE_APPROVAL_GRANTED_SUBJECT);
+	public static final String EMAIL_ARTICLE_MOVED_FROM_FOLDER_SUBJECT = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_MOVED_FROM_FOLDER_SUBJECT);
 
-	public static final String JOURNAL_EMAIL_ARTICLE_APPROVAL_REQUESTED_BODY = PropsUtil.get(PropsKeys.JOURNAL_EMAIL_ARTICLE_APPROVAL_REQUESTED_BODY);
+	public static final String EMAIL_ARTICLE_MOVED_TO_FOLDER_BODY = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_MOVED_TO_FOLDER_BODY);
 
-	public static final String JOURNAL_EMAIL_ARTICLE_APPROVAL_REQUESTED_SUBJECT = PropsUtil.get(PropsKeys.JOURNAL_EMAIL_ARTICLE_APPROVAL_REQUESTED_SUBJECT);
+	public static final boolean EMAIL_ARTICLE_MOVED_TO_FOLDER_ENABLED = GetterUtil.getBoolean(JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_MOVED_TO_FOLDER_ENABLED));
 
-	public static final String JOURNAL_EMAIL_ARTICLE_MOVED_FROM_FOLDER_BODY = PropsUtil.get(PropsKeys.JOURNAL_EMAIL_ARTICLE_MOVED_FROM_FOLDER_BODY);
+	public static final String EMAIL_ARTICLE_MOVED_TO_FOLDER_SUBJECT = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_MOVED_TO_FOLDER_SUBJECT);
 
-	public static final String JOURNAL_EMAIL_ARTICLE_MOVED_FROM_FOLDER_SUBJECT = PropsUtil.get(PropsKeys.JOURNAL_EMAIL_ARTICLE_MOVED_FROM_FOLDER_SUBJECT);
+	public static final String EMAIL_ARTICLE_REVIEW_BODY = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_REVIEW_BODY);
 
-	public static final String JOURNAL_EMAIL_ARTICLE_MOVED_TO_FOLDER_BODY = PropsUtil.get(PropsKeys.JOURNAL_EMAIL_ARTICLE_MOVED_TO_FOLDER_BODY);
+	public static final boolean EMAIL_ARTICLE_REVIEW_ENABLED = GetterUtil.getBoolean(JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_REVIEW_ENABLED));
 
-	public static final String JOURNAL_EMAIL_ARTICLE_MOVED_TO_FOLDER_SUBJECT = PropsUtil.get(PropsKeys.JOURNAL_EMAIL_ARTICLE_MOVED_TO_FOLDER_SUBJECT);
+	public static final String EMAIL_ARTICLE_REVIEW_SUBJECT = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_REVIEW_SUBJECT);
 
-	public static final String JOURNAL_EMAIL_ARTICLE_REVIEW_BODY = PropsUtil.get(PropsKeys.JOURNAL_EMAIL_ARTICLE_REVIEW_BODY);
+	public static final String EMAIL_ARTICLE_UPDATED_BODY = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_UPDATED_BODY);
 
-	public static final String JOURNAL_EMAIL_ARTICLE_REVIEW_SUBJECT = PropsUtil.get(PropsKeys.JOURNAL_EMAIL_ARTICLE_REVIEW_SUBJECT);
+	public static final boolean EMAIL_ARTICLE_UPDATED_ENABLED = GetterUtil.getBoolean(JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_UPDATED_ENABLED));
 
-	public static final String JOURNAL_EMAIL_ARTICLE_UPDATED_BODY = PropsUtil.get(PropsKeys.JOURNAL_EMAIL_ARTICLE_UPDATED_BODY);
+	public static final String EMAIL_ARTICLE_UPDATED_SUBJECT = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_ARTICLE_UPDATED_SUBJECT);
 
-	public static final String JOURNAL_EMAIL_ARTICLE_UPDATED_SUBJECT = PropsUtil.get(PropsKeys.JOURNAL_EMAIL_ARTICLE_UPDATED_SUBJECT);
+	public static final String EMAIL_FROM_ADDRESS = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_FROM_ADDRESS);
 
-	public static final String JOURNAL_EMAIL_FROM_ADDRESS = PropsUtil.get(PropsKeys.JOURNAL_EMAIL_FROM_ADDRESS);
+	public static final String EMAIL_FROM_NAME = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.EMAIL_FROM_NAME);
 
-	public static final String JOURNAL_EMAIL_FROM_NAME = PropsUtil.get(PropsKeys.JOURNAL_EMAIL_FROM_NAME);
+	public static final int JOURNAL_ARTICLE_CHECK_INTERVAL = GetterUtil.getInteger(JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.JOURNAL_ARTICLE_CHECK_INTERVAL));
 
-	public static final boolean JOURNAL_FOLDER_ICON_CHECK_COUNT = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.JOURNAL_FOLDER_ICON_CHECK_COUNT));
+	public static final boolean JOURNAL_ARTICLE_COMMENTS_ENABLED = GetterUtil.getBoolean(JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.JOURNAL_ARTICLE_COMMENTS_ENABLED));
 
-	public static final String JOURNAL_LAR_CREATION_STRATEGY = PropsUtil.get(PropsKeys.JOURNAL_LAR_CREATION_STRATEGY);
+	public static final String[] JOURNAL_ARTICLE_CUSTOM_TOKENS = JournalServiceConfigurationUtil.getArray(JournalServiceConfigurationKeys.JOURNAL_ARTICLE_CUSTOM_TOKENS);
 
-	public static final long TERMS_OF_USE_JOURNAL_ARTICLE_GROUP_ID = GetterUtil.getLong(PropsUtil.get(PropsKeys.TERMS_OF_USE_JOURNAL_ARTICLE_GROUP_ID));
+	public static boolean JOURNAL_ARTICLE_DATABASE_KEYWORD_SEARCH_CONTENT = GetterUtil.getBoolean(JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.JOURNAL_ARTICLE_DATABASE_KEYWORD_SEARCH_CONTENT));
 
-	public static final String TERMS_OF_USE_JOURNAL_ARTICLE_ID = PropsUtil.get(PropsKeys.TERMS_OF_USE_JOURNAL_ARTICLE_ID);
+	public static final boolean JOURNAL_ARTICLE_EXPIRE_ALL_VERSIONS = GetterUtil.getBoolean(JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.JOURNAL_ARTICLE_EXPIRE_ALL_VERSIONS));
+
+	public static boolean JOURNAL_ARTICLE_INDEX_ALL_VERSIONS = GetterUtil.getBoolean(JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.JOURNAL_ARTICLE_INDEX_ALL_VERSIONS));
+
+	public static final String JOURNAL_ARTICLE_STORAGE_TYPE = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.JOURNAL_ARTICLE_STORAGE_TYPE);
+
+	public static final String JOURNAL_ARTICLE_TOKEN_PAGE_BREAK = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.JOURNAL_ARTICLE_TOKEN_PAGE_BREAK);
+
+	public static final boolean JOURNAL_ARTICLE_VIEW_PERMISSION_CHECK_ENABLED = GetterUtil.getBoolean(JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.JOURNAL_ARTICLE_VIEW_PERMISSION_CHECK_ENABLED));
+
+	public static final boolean JOURNAL_FOLDER_ICON_CHECK_COUNT = GetterUtil.getBoolean(JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.JOURNAL_FOLDER_ICON_CHECK_COUNT));
+
+	public static final String LAR_CREATION_STRATEGY = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.LAR_CREATION_STRATEGY);
+
+	public static final boolean SYNC_CONTENT_SEARCH_ON_STARTUP = GetterUtil.getBoolean(JournalServiceConfigurationKeys.SYNC_CONTENT_SEARCH_ON_STARTUP);
+
+	public static final long TERMS_OF_USE_JOURNAL_ARTICLE_GROUP_ID = GetterUtil.getLong(JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.TERMS_OF_USE_JOURNAL_ARTICLE_GROUP_ID));
+
+	public static final String TERMS_OF_USE_JOURNAL_ARTICLE_ID = JournalServiceConfigurationUtil.get(JournalServiceConfigurationKeys.TERMS_OF_USE_JOURNAL_ARTICLE_ID);
 
 }
