05c982ef321eded7e3e35980dd7cfd967a984e6d
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-06-01 17:14:49 -0300

    LPS-55933 Rename

--- modules/apps/workflow/workflow-instance-web/src/com/liferay/workflow/instance/web/portlet/MyWorkflowInstancePortlet.java
+++ modules/apps/workflow/workflow-instance-web/src/com/liferay/workflow/instance/web/portlet/MyWorkflowInstancePortlet.java
@@ -35,7 +35,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.private-session-attributes=false",
 		"com.liferay.portlet.render-weight=50",
 		"com.liferay.portlet.use-default-template=true",
-		"javax.portlet.display-name=My Workflow Instances",
+		"javax.portlet.display-name=My Submissions",
 		"javax.portlet.expiration-cache=0",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
