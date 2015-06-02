dad6a2dd1aec8fba599f977abdf94d8045b66c05
drewbrokke <drew.brokke@liferay.com>
2015-06-02 16:22:09 -0700

    LPS-55718 Uses MVC methods and exception handling

--- portal-impl/src/com/liferay/portlet/rolesadmin/RolesAdminPortlet.java
+++ portal-impl/src/com/liferay/portlet/rolesadmin/RolesAdminPortlet.java
@@ -14,6 +14,32 @@
 
 package com.liferay.portlet.rolesadmin;
 
+import com.liferay.portal.DuplicateRoleException;
+import com.liferay.portal.NoSuchRoleException;
+import com.liferay.portal.RequiredRoleException;
+import com.liferay.portal.RoleNameException;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.model.Role;
+import com.liferay.portal.model.RoleConstants;
+import com.liferay.portal.security.auth.PrincipalException;
+import com.liferay.portal.service.RoleServiceUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceContextFactory;
+
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import java.util.Locale;
+import java.util.Map;
+
 /**
  * @author Brian Wing Shun Chan
  * @author Drew Brokke
@@ -21,97 +47,19 @@ package com.liferay.portlet.rolesadmin;
 
 public class RolesAdminPortlet extends MVCPortlet {
 
-	public void deleteRole(ActionRequest actionRequest) throws Exception {
+	public void deleteRole(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
 		long roleId = ParamUtil.getLong(actionRequest, "roleId");
 
 		RoleServiceUtil.deleteRole(roleId);
 	}
 
-	@Override
-	public void processAction(
-			ActionMapping actionMapping, ActionForm actionForm,
-			PortletConfig portletConfig, ActionRequest actionRequest,
-			ActionResponse actionResponse)
+	public Role updateRole(
+			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
 
-		String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
-
-		try {
-			Role role = null;
-
-			if (cmd.equals(Constants.ADD) || cmd.equals(Constants.UPDATE)) {
-				role = updateRole(actionRequest);
-			}
-			else if (cmd.equals(Constants.DELETE)) {
-				deleteRole(actionRequest);
-			}
-
-			String redirect = ParamUtil.getString(actionRequest, "redirect");
-
-			if (role != null) {
-				redirect = HttpUtil.setParameter(
-					redirect, actionResponse.getNamespace() + "roleId",
-					role.getRoleId());
-			}
-
-			sendRedirect(actionRequest, actionResponse, redirect);
-		}
-		catch (Exception e) {
-			if (e instanceof PrincipalException) {
-				SessionErrors.add(actionRequest, e.getClass());
-
-				setForward(actionRequest, "portlet.roles_admin.error");
-			}
-			else if (e instanceof DuplicateRoleException ||
-					 e instanceof NoSuchRoleException ||
-					 e instanceof RequiredRoleException ||
-					 e instanceof RoleNameException) {
-
-				SessionErrors.add(actionRequest, e.getClass());
-
-				if (cmd.equals(Constants.DELETE)) {
-					String redirect = PortalUtil.escapeRedirect(
-						ParamUtil.getString(actionRequest, "redirect"));
-
-					if (Validator.isNotNull(redirect)) {
-						actionResponse.sendRedirect(redirect);
-					}
-				}
-			}
-			else {
-				throw e;
-			}
-		}
-	}
-
-	@Override
-	public ActionForward render(
-			ActionMapping actionMapping, ActionForm actionForm,
-			PortletConfig portletConfig, RenderRequest renderRequest,
-			RenderResponse renderResponse)
-		throws Exception {
-
-		try {
-			ActionUtil.getRole(renderRequest);
-		}
-		catch (Exception e) {
-			if (e instanceof NoSuchRoleException ||
-				e instanceof PrincipalException) {
-
-				SessionErrors.add(renderRequest, e.getClass());
-
-				return actionMapping.findForward("portlet.roles_admin.error");
-			}
-			else {
-				throw e;
-			}
-		}
-
-		return actionMapping.findForward(
-			getForward(renderRequest, "portlet.roles_admin.edit_role"));
-	}
-
-	public Role updateRole(ActionRequest actionRequest) throws Exception {
 		long roleId = ParamUtil.getLong(actionRequest, "roleId");
 
 		String name = ParamUtil.getString(actionRequest, "name");
@@ -141,6 +89,47 @@ public class RolesAdminPortlet extends MVCPortlet {
 				roleId, name, titleMap, descriptionMap, subtype,
 				serviceContext);
 		}
+	}
+
+	@Override
+	protected void doDispatch(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		if (SessionErrors.contains(
+			renderRequest, PrincipalException.class.getName()) ||
+			SessionErrors.contains(
+				renderRequest, NoSuchRoleException.class.getName())) {
+
+			include(
+				"/html/portlet/roles_admin/error.jsp", renderRequest,
+				renderResponse);
+		}
+		else if (SessionErrors.contains(
+					renderRequest,
+					RequiredRoleException.class.getName())) {
+
+			include(
+				"/html/portlet/roles_admin/view.jsp", renderRequest,
+				renderResponse);
+		}
+		else {
+			super.doDispatch(renderRequest, renderResponse);
+		}
+	}
+
+	@Override
+	protected boolean isSessionErrorException(Throwable cause) {
+		if (cause instanceof DuplicateRoleException ||
+			cause instanceof NoSuchRoleException ||
+			cause instanceof PrincipalException ||
+			cause instanceof RequiredRoleException ||
+			cause instanceof RoleNameException) {
+
+			return true;
+		}
+
+		return false;
 	}
 
 }
