d8186d4dc88ff7d4aa7a8f4811d9970f0406c73f
shuyangzhou <shuyang.zhou@liferay.com>
2015-06-01 19:22:38 -0700

    LPS-56053 Create an unit test to scan for missing build scripts

---
+++ portal-service/test/unit/com/liferay/portal/modules/ModulesBuildScriptsTest.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.modules;
+
+import java.io.IOException;
+
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Shuyang Zhou
+ */
+public class ModulesBuildScriptsTest {
+
+	@Test
+	public void testScanBuildScripts() throws IOException {
+		Files.walkFileTree(
+			Paths.get("modules"),
+			new SimpleFileVisitor<Path>() {
+
+				@Override
+				public FileVisitResult preVisitDirectory(
+					Path dirPath, BasicFileAttributes basicFileAttributes) {
+
+					Path buildXmlPath = dirPath.resolve("build.xml");
+
+					if (Files.notExists(buildXmlPath)) {
+						Assert.fail("Missing " + buildXmlPath);
+					}
+
+					if (Files.exists(dirPath.resolve("bnd.bnd"))) {
+						Path buildGradlePath = dirPath.resolve("build.gradle");
+
+						if (Files.notExists(buildGradlePath)) {
+							Assert.fail("Missing " + buildGradlePath);
+						}
+
+						return FileVisitResult.SKIP_SUBTREE;
+					}
+
+					return FileVisitResult.CONTINUE;
+				}
+
+			});
+	}
+
+}
