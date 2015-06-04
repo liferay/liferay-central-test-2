83b7890bedd8c2df6de05e5a53aa4354ad4c1b4f
Ryan Park <ryan.park@liferay.com>
2015-06-04 09:16:00 -0700

    LPS-56041 Copy microblogs-portlet web classes and JSPs

---
+++ modules/apps/microblogs/microblogs-web/docroot/WEB-INF/src/com/liferay/microblogs/microblogs/notifications/MicroblogsUserNotificationHandler.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This file is part of Liferay Social Office. Liferay Social Office is free
+ * software: you can redistribute it and/or modify it under the terms of the GNU
+ * Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Liferay Social Office is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Liferay Social Office. If not, see http://www.gnu.org/licenses/agpl-3.0.html.
+ */
+
+package com.liferay.microblogs.microblogs.notifications;
+
+import com.liferay.microblogs.model.MicroblogsEntry;
+import com.liferay.microblogs.model.MicroblogsEntryConstants;
+import com.liferay.microblogs.service.MicroblogsEntryLocalServiceUtil;
+import com.liferay.microblogs.util.PortletKeys;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.notifications.BaseModelUserNotificationHandler;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.asset.model.AssetRenderer;
+
+/**
+ * @author Jonathan Lee
+ */
+public class MicroblogsUserNotificationHandler
+	extends BaseModelUserNotificationHandler {
+
+	public MicroblogsUserNotificationHandler() {
+		setPortletId(PortletKeys.MICROBLOGS);
+	}
+
+	@Override
+	protected String getTitle(
+		JSONObject jsonObject, AssetRenderer assetRenderer,
+		ServiceContext serviceContext) {
+
+		MicroblogsEntry microblogsEntry =
+			MicroblogsEntryLocalServiceUtil.fetchMicroblogsEntry(
+				assetRenderer.getClassPK());
+
+		String title = StringPool.BLANK;
+
+		String userFullName = HtmlUtil.escape(
+			PortalUtil.getUserName(
+				microblogsEntry.getUserId(), StringPool.BLANK));
+
+		int notificationType = jsonObject.getInt("notificationType");
+
+		if (notificationType ==
+				MicroblogsEntryConstants.NOTIFICATION_TYPE_REPLY) {
+
+			title = serviceContext.translate(
+				"x-commented-on-your-post", userFullName);
+		}
+		else if (notificationType ==
+					MicroblogsEntryConstants.
+						NOTIFICATION_TYPE_REPLY_TO_REPLIED) {
+
+			long parentMicroblogsEntryUserId =
+				microblogsEntry.fetchParentMicroblogsEntryUserId();
+
+			User user = UserLocalServiceUtil.fetchUser(
+				parentMicroblogsEntryUserId);
+
+			if (user != null) {
+				title = serviceContext.translate(
+					"x-also-commented-on-x's-post", userFullName,
+					user.getFullName());
+			}
+		}
+		else if (notificationType ==
+					MicroblogsEntryConstants.
+						NOTIFICATION_TYPE_REPLY_TO_TAGGED) {
+
+			title = serviceContext.translate(
+				"x-commented-on-a-post-you-are-tagged-in", userFullName);
+		}
+		else if (notificationType ==
+					MicroblogsEntryConstants.NOTIFICATION_TYPE_TAG) {
+
+			title = serviceContext.translate(
+				"x-tagged-you-in-a-post", userFullName);
+		}
+
+		return title;
+	}
+
+}
---
+++ modules/apps/microblogs/microblogs-web/docroot/status_update/view.jsp
@@ -0,0 +1,73 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This file is part of Liferay Social Office. Liferay Social Office is free
+ * software: you can redistribute it and/or modify it under the terms of the GNU
+ * Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Liferay Social Office is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Liferay Social Office. If not, see http://www.gnu.org/licenses/agpl-3.0.html.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+Group group = themeDisplay.getScopeGroup();
+%>
+
+<c:if test="<%= MicroblogsPermission.contains(permissionChecker, scopeGroupId, ActionKeys.ADD_ENTRY) && (group.isUser() && !layout.isPublicLayout()) %>">
+	<liferay-util:include page="/microblogs/edit_microblogs_entry.jsp" servletContext="<%= application %>" />
+</c:if>
+
+<c:if test="<%= showStatus %>">
+	<div class="microblogs-container microblogs-status-container">
+
+		<%
+		List<MicroblogsEntry> microblogsEntries = null;
+
+		long microblogsEntryUserId = themeDisplay.getUserId();
+
+		if (group.isUser()) {
+			microblogsEntryUserId = group.getClassPK();
+		}
+
+		if (microblogsEntryUserId == themeDisplay.getUserId()) {
+			microblogsEntries = MicroblogsEntryLocalServiceUtil.getUserMicroblogsEntries(microblogsEntryUserId, 0, 0, 1);
+		}
+		else {
+			microblogsEntries = MicroblogsEntryServiceUtil.getUserMicroblogsEntries(microblogsEntryUserId, 0, 0, 1);
+		}
+
+		request.setAttribute(WebKeys.MICROBLOGS_ENTRIES, microblogsEntries);
+
+		PortletURL portletURL = renderResponse.createRenderURL();
+
+		portletURL.setParameter("mvcPath", "/status_update/view.jsp");
+		portletURL.setWindowState(WindowState.NORMAL);
+
+		request.setAttribute(WebKeys.MICROBLOGS_ENTRIES_URL, portletURL);
+		%>
+
+		<liferay-util:include page="/microblogs/view_microblogs_entries.jsp" servletContext="<%= application %>" />
+	</div>
+</c:if>
+
+<aui:script use="aui-base">
+	AUI().ready(
+		function() {
+			Liferay.Microblogs.init(
+				{
+					microblogsEntriesURL: '<portlet:renderURL windowState="<%= LiferayWindowState.EXCLUSIVE.toString() %>"><portlet:param name="mvcPath" value="/status_update/view.jsp" /></portlet:renderURL>'
+				}
+			);
+		}
+	);
+</aui:script>
