1725356dd7c712855e5b121f2c06a880dbf13a99
Ryan Park <ryan.park@liferay.com>
2015-06-04 09:16:00 -0700

    LPS-56041 Copy microblogs-portlet web classes and JSPs

---
+++ modules/apps/microblogs/microblogs-web/docroot/WEB-INF/src/com/liferay/microblogs/microblogs/asset/MicroblogsEntryAssetRenderer.java
@@ -0,0 +1,172 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This file is part of Liferay Social Office. Liferay Social Office is free
+ * software: you can redistribute it and/or modify it under the terms of the GNU
+ * Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Liferay Social Office is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Liferay Social Office. If not, see http://www.gnu.org/licenses/agpl-3.0.html.
+ */
+
+package com.liferay.microblogs.microblogs.asset;
+
+import com.liferay.microblogs.model.MicroblogsEntry;
+import com.liferay.microblogs.service.permission.MicroblogsEntryPermission;
+import com.liferay.microblogs.util.WebKeys;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.User;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.PortletURLFactoryUtil;
+import com.liferay.portlet.asset.model.BaseAssetRenderer;
+
+import java.util.Locale;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+import javax.portlet.PortletURL;
+
+/**
+ * @author Matthew Kong
+ */
+public class MicroblogsEntryAssetRenderer extends BaseAssetRenderer {
+
+	public MicroblogsEntryAssetRenderer(MicroblogsEntry entry) {
+		_entry = entry;
+	}
+
+	@Override
+	public String getClassName() {
+		return MicroblogsEntry.class.getName();
+	}
+
+	@Override
+	public long getClassPK() {
+		return _entry.getMicroblogsEntryId();
+	}
+
+	@Override
+	public long getGroupId() {
+		try {
+			Group group = GroupLocalServiceUtil.getCompanyGroup(
+				_entry.getCompanyId());
+
+			return group.getGroupId();
+		}
+		catch (Exception e) {
+		}
+
+		return 0;
+	}
+
+	@Override
+	public String getSummary(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		return _entry.getContent();
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		return _entry.getContent();
+	}
+
+	@Override
+	public String getURLViewInContext(
+		LiferayPortletRequest liferayPortletRequest,
+		LiferayPortletResponse liferayPortletResponse,
+		String noSuchEntryRedirect) {
+
+		try {
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)liferayPortletRequest.getAttribute(
+					WebKeys.THEME_DISPLAY);
+
+			User user = themeDisplay.getUser();
+
+			long portletPlid = PortalUtil.getPlidFromPortletId(
+				user.getGroupId(), true, "1_WAR_microblogsportlet");
+
+			PortletURL portletURL = PortletURLFactoryUtil.create(
+				liferayPortletRequest, "1_WAR_microblogsportlet", portletPlid,
+				PortletRequest.RENDER_PHASE);
+
+			portletURL.setParameter("mvcPath", "/microblogs/view.jsp");
+
+			long microblogsEntryId = _entry.getMicroblogsEntryId();
+
+			if (_entry.getParentMicroblogsEntryId() > 0) {
+				microblogsEntryId =_entry.getParentMicroblogsEntryId();
+			}
+
+			portletURL.setParameter(
+				"parentMicroblogsEntryId", String.valueOf(microblogsEntryId));
+
+			return portletURL.toString();
+		}
+		catch (Exception e) {
+		}
+
+		return null;
+	}
+
+	@Override
+	public long getUserId() {
+		return _entry.getUserId();
+	}
+
+	@Override
+	public String getUserName() {
+		return _entry.getUserName();
+	}
+
+	@Override
+	public String getUuid() {
+		return null;
+	}
+
+	@Override
+	public boolean hasViewPermission(PermissionChecker permissionChecker) {
+		try {
+			return MicroblogsEntryPermission.contains(
+				permissionChecker, _entry, ActionKeys.VIEW);
+		}
+		catch (Exception e) {
+		}
+
+		return false;
+	}
+
+	@Override
+	public String render(
+			PortletRequest portletRequest, PortletResponse portletResponse,
+			String template)
+		throws Exception {
+
+		if (template.equals(TEMPLATE_ABSTRACT) ||
+			template.equals(TEMPLATE_FULL_CONTENT)) {
+
+			portletRequest.setAttribute(WebKeys.MICROBLOGS_ENTRY, _entry);
+
+			return "/microblogs/asset/" + template + ".jsp";
+		}
+		else {
+			return null;
+		}
+	}
+
+	private MicroblogsEntry _entry;
+
+}
---
+++ modules/apps/microblogs/microblogs-web/docroot/WEB-INF/src/com/liferay/microblogs/microblogs/social/MicroblogsActivityInterpreter.java
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This file is part of Liferay Social Office. Liferay Social Office is free
+ * software: you can redistribute it and/or modify it under the terms of the GNU
+ * Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Liferay Social Office is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Liferay Social Office. If not, see http://www.gnu.org/licenses/agpl-3.0.html.
+ */
+
+package com.liferay.microblogs.microblogs.social;
+
+import com.liferay.microblogs.model.MicroblogsEntry;
+import com.liferay.microblogs.model.MicroblogsEntryConstants;
+import com.liferay.microblogs.service.MicroblogsEntryLocalServiceUtil;
+import com.liferay.microblogs.service.permission.MicroblogsEntryPermission;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portlet.social.model.BaseSocialActivityInterpreter;
+import com.liferay.portlet.social.model.SocialActivity;
+
+/**
+ * @author Jonathan Lee
+ */
+public class MicroblogsActivityInterpreter
+	extends BaseSocialActivityInterpreter {
+
+	@Override
+	public String[] getClassNames() {
+		return _CLASS_NAMES;
+	}
+
+	@Override
+	protected String getBody(
+		SocialActivity activity, ServiceContext serviceContext) {
+
+		return getUserName(activity.getUserId(), serviceContext);
+	}
+
+	@Override
+	protected String getLink(
+		SocialActivity activity, ServiceContext serviceContext) {
+
+		return StringPool.BLANK;
+	}
+
+	@Override
+	protected String getTitle(
+			SocialActivity activity, ServiceContext serviceContext)
+		throws Exception {
+
+		StringBundler sb = new StringBundler(5);
+
+		MicroblogsEntry microblogsEntry =
+			MicroblogsEntryLocalServiceUtil.getMicroblogsEntry(
+				activity.getClassPK());
+
+		String receiverUserName = getUserName(
+			activity.getReceiverUserId(), serviceContext);
+
+		if (activity.getReceiverUserId() > 0) {
+			if (microblogsEntry.getType() ==
+					MicroblogsEntryConstants.TYPE_REPLY) {
+
+				sb.append("@");
+				sb.append(receiverUserName);
+				sb.append(": ");
+			}
+			else if (microblogsEntry.getType() ==
+						MicroblogsEntryConstants.TYPE_REPOST) {
+
+				sb.append(serviceContext.translate("reposted-from"));
+				sb.append(" ");
+				sb.append(receiverUserName);
+				sb.append(": ");
+			}
+		}
+
+		sb.append(HtmlUtil.escape(microblogsEntry.getContent()));
+
+		return sb.toString();
+	}
+
+	@Override
+	protected boolean hasPermissions(
+			PermissionChecker permissionChecker, SocialActivity activity,
+			String actionId, ServiceContext serviceContext)
+		throws Exception {
+
+		MicroblogsEntry microblogsEntry =
+			MicroblogsEntryLocalServiceUtil.getMicroblogsEntry(
+				activity.getClassPK());
+
+		return MicroblogsEntryPermission.contains(
+			permissionChecker, microblogsEntry, ActionKeys.VIEW);
+	}
+
+	private static final String[] _CLASS_NAMES =
+		{MicroblogsEntry.class.getName()};
+
+}
