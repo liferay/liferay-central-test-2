fd19d81b3472893824150e5771634a3db35789e4
Roberto Díaz <roberto.diaz@liferay.com>
2015-06-05 08:29:42 +0200

    LPS-53821 Copy dl-item-selector-api to item selector folder [no logic changes]

---
+++ modules/apps/item-selector/document-library-item-selector-api/src/com/liferay/document/library/item/selector/DLItemSelectorCriterion.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.item.selector;
+
+import com.liferay.item.selector.BaseItemSelectorCriterion;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+
+import java.net.URL;
+
+import java.util.Set;
+
+/**
+ * @author Roberto Díaz
+ */
+public class DLItemSelectorCriterion extends BaseItemSelectorCriterion {
+
+	public DLItemSelectorCriterion() {
+		super(_AVAILABLE_RETURN_TYPES);
+	}
+
+	public DLItemSelectorCriterion(
+		long folderId, long repositoryId, String[] mimeTypes) {
+
+		super(_AVAILABLE_RETURN_TYPES);
+
+		_folderId = folderId;
+		_repositoryId = repositoryId;
+		_mimeTypes = mimeTypes;
+	}
+
+	public long getFolderId() {
+		return _folderId;
+	}
+
+	public String[] getMimeTypes() {
+		return _mimeTypes;
+	}
+
+	public long getRepositoryId() {
+		return _repositoryId;
+	}
+
+	public void setFolderId(long folderId) {
+		_folderId = folderId;
+	}
+
+	public void setMimeTypes(String[] mimeTypes) {
+		_mimeTypes = mimeTypes;
+	}
+
+	public void setRepositoryId(long repositoryId) {
+		_repositoryId = repositoryId;
+	}
+
+	private static final Set<Class<?>> _AVAILABLE_RETURN_TYPES =
+		getInmutableSet(FileEntry.class, URL.class);
+
+	private long _folderId;
+	private String[] _mimeTypes;
+	private long _repositoryId;
+
+}
