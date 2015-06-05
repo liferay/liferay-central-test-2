f9bd16a0dab38791d92795873d0745a935d06a55
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-06-05 18:10:07 -0300

    LPS-55538 Initial implementation of DDMForm annotation processor

---
+++ portal-service/src/com/liferay/portlet/dynamicdatamapping/registry/DDMFormFactory.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.dynamicdatamapping.registry;
+
+import com.liferay.portlet.dynamicdatamapping.model.DDMForm;
+import com.liferay.portlet.dynamicdatamapping.model.DDMFormField;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+/**
+ * @author Marcellus Tavares
+ */
+public class DDMFormFactory {
+
+	public static DDMForm create(Class<?> clazz) {
+		if (!clazz.isAnnotationPresent(_DDM_FORM_ANNOTATION)) {
+			throw new IllegalArgumentException(
+				"The provided type is not supported.");
+		}
+
+		DDMForm ddmForm = new DDMForm();
+
+		addDDMFormFields(clazz, ddmForm);
+
+		return ddmForm;
+	}
+
+	protected static void addDDMFormFields(Class<?> clazz, DDMForm ddmForm) {
+		for (Class<?> interfaceClass : clazz.getInterfaces()) {
+			addDDMFormFields(interfaceClass, ddmForm);
+		}
+
+		for (Method method : clazz.getDeclaredMethods()) {
+			if (!method.isAnnotationPresent(_DDM_FORM_FIELD_ANNOTATION)) {
+				continue;
+			}
+
+			DDMFormField ddmFormField = createDDMFormField(method);
+
+			ddmForm.addDDMFormField(ddmFormField);
+		}
+	}
+
+	protected static DDMFormField createDDMFormField(Method method) {
+		DDMFormFactoryHelper ddmFormFactoryHelper = new DDMFormFactoryHelper(
+			method);
+
+		String name = ddmFormFactoryHelper.getDDMFormFieldName();
+		String type = ddmFormFactoryHelper.getDDMFormFieldType();
+
+		DDMFormField ddmFormField = new DDMFormField(name, type);
+
+		ddmFormField.setDataType(
+			ddmFormFactoryHelper.getDDMFormFieldDataType());
+		ddmFormField.setLocalizable(
+			ddmFormFactoryHelper.isDDMFormFieldLocalizable(method));
+
+		return ddmFormField;
+	}
+
+	private static final Class<? extends Annotation> _DDM_FORM_ANNOTATION =
+		com.liferay.portlet.dynamicdatamapping.registry.annotations.
+			DDMForm.class;
+
+	private static final Class<? extends Annotation>
+		_DDM_FORM_FIELD_ANNOTATION =
+			com.liferay.portlet.dynamicdatamapping.registry.annotations.
+				DDMFormField.class;
+
+}
---
+++ portal-service/src/com/liferay/portlet/dynamicdatamapping/registry/DDMFormFactoryHelper.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.dynamicdatamapping.registry;
+
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portlet.dynamicdatamapping.model.LocalizedValue;
+import com.liferay.portlet.dynamicdatamapping.registry.annotations.DDMFormField;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author Marcellus Tavares
+ */
+public class DDMFormFactoryHelper {
+
+	public DDMFormFactoryHelper(Method method) {
+		_method = method;
+		_ddmFormField = method.getAnnotation(DDMFormField.class);
+	}
+
+	public String getDDMFormFieldName() {
+		if (Validator.isNotNull(_ddmFormField.name())) {
+			return _ddmFormField.name();
+		}
+
+		return _method.getName();
+	}
+
+	public String getDDMFormFieldType() {
+		if (Validator.isNotNull(_ddmFormField.type())) {
+			return _ddmFormField.type();
+		}
+
+		Class<?> returnType = _method.getReturnType();
+
+		if (returnType.isAssignableFrom(boolean.class) ||
+			returnType.isAssignableFrom(Boolean.class)) {
+
+			return "checkbox";
+		}
+
+		return "text";
+	}
+
+	public boolean isDDMFormFieldLocalizable(Method method) {
+		Class<?> returnType = _method.getReturnType();
+
+		if (returnType.isAssignableFrom(LocalizedValue.class)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	protected String getDDMFormFieldDataType() {
+		if (Validator.isNotNull(_ddmFormField.dataType())) {
+			return _ddmFormField.dataType();
+		}
+
+		Class<?> returnType = _method.getReturnType();
+
+		if (returnType.isAssignableFrom(boolean.class) ||
+			returnType.isAssignableFrom(Boolean.class)) {
+
+			return "boolean";
+		}
+
+		return "string";
+	}
+
+	private final DDMFormField _ddmFormField;
+	private final Method _method;
+
+}
