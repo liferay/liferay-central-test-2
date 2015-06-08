6d98bc26eda368a7deec0abaa563060d15c6993a
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-06-08 20:04:54 -0300

    LPS-56073 Add Form Admin portlet

--- modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/com/liferay/dynamic/data/lists/form/web/constants/DDLFormPortletKeys.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/com/liferay/dynamic/data/lists/form/web/constants/DDLFormPortletKeys.java
@@ -22,4 +22,7 @@ public class DDLFormPortletKeys {
 	public static final String DYNAMIC_DATA_LISTS_FORM =
 		"com_liferay_dynamic_data_lists_form_web_portlet_DDLFormPortlet";
 
+	public static final String DYNAMIC_DATA_LISTS_FORM_ADMIN =
+		"com_liferay_dynamic_data_lists_form_web_portlet_DDLFormAdminPortlet";
+
 }
---
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/com/liferay/dynamic/data/lists/form/web/portlet/DDLFormAdminPortlet.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.form.web.portlet;
+
+import com.liferay.dynamic.data.lists.form.web.constants.DDLFormPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Bruno Basto
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.control-panel-entry-category=site_administration.content",
+		"com.liferay.portlet.control-panel-entry-weight=8.0",
+		"com.liferay.portlet.css-class-wrapper=portlet-forms",
+		"com.liferay.portlet.display-category=category.hidden",
+		"com.liferay.portlet.footer-portlet-javascript=/admin/js/modules.js",
+		"com.liferay.portlet.header-portlet-css=/admin/css/main.css",
+		"com.liferay.portlet.instanceable=false",
+		"com.liferay.portlet.layout-cacheable=true",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Forms", "javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/admin/",
+		"javax.portlet.init-param.view-template=/admin/view.jsp",
+		"javax.portlet.name=" + DDLFormPortletKeys.DYNAMIC_DATA_LISTS_FORM_ADMIN,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
+public class DDLFormAdminPortlet extends MVCPortlet {
+}
