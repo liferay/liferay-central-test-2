5a663aada86681eaee61d84af69a56bd61722fe6
Michael Han <michael.han@liferay.com>
2015-06-08 11:27:18 -0500

    LPS-52240 add partialUpdate support to interface

--- portal-service/src/com/liferay/portal/kernel/search/SearchEngineUtil.java
+++ portal-service/src/com/liferay/portal/kernel/search/SearchEngineUtil.java
@@ -556,6 +556,64 @@ public class SearchEngineUtil {
 		return _indexReadOnly;
 	}
 
+	public static void partialUpdateDocument(
+			String searchEngineId, long companyId, Document document,
+			boolean commitImmediately)
+		throws SearchException {
+
+		if (isIndexReadOnly()) {
+			return;
+		}
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Document " + document.toString());
+		}
+
+		SearchEngine searchEngine = getSearchEngine(searchEngineId);
+
+		IndexWriter indexWriter = searchEngine.getIndexWriter();
+
+		_searchPermissionChecker.addPermissionFields(companyId, document);
+
+		SearchContext searchContext = new SearchContext();
+
+		searchContext.setCommitImmediately(commitImmediately);
+		searchContext.setCompanyId(companyId);
+		searchContext.setSearchEngineId(searchEngineId);
+
+		indexWriter.partialUpdateDocument(searchContext, document);
+	}
+
+	public static void partialUpdateDocuments(
+			String searchEngineId, long companyId,
+			Collection<Document> documents, boolean commitImmediately)
+		throws SearchException {
+
+		if (isIndexReadOnly() || (documents == null) || documents.isEmpty()) {
+			return;
+		}
+
+		SearchEngine searchEngine = getSearchEngine(searchEngineId);
+
+		IndexWriter indexWriter = searchEngine.getIndexWriter();
+
+		for (Document document : documents) {
+			if (_log.isDebugEnabled()) {
+				_log.debug("Document " + document.toString());
+			}
+
+			_searchPermissionChecker.addPermissionFields(companyId, document);
+		}
+
+		SearchContext searchContext = new SearchContext();
+
+		searchContext.setCommitImmediately(commitImmediately);
+		searchContext.setCompanyId(companyId);
+		searchContext.setSearchEngineId(searchEngineId);
+
+		indexWriter.partialUpdateDocuments(searchContext, documents);
+	}
+
 	public synchronized static void removeBackup(
 			long companyId, String backupName)
 		throws SearchException {
