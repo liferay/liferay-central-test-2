49b3b7a695566e38b6db50bf14b6dafe1ffe34fa
Bryan Engler <bryan.engler@liferay.com>
2015-06-08 11:53:15 -0700

    LPS-56129 Match the permission checking logic in AnnouncementsEntryServiceImpl

--- portal-service/src/com/liferay/portlet/announcements/util/AnnouncementsUtil.java
+++ portal-service/src/com/liferay/portlet/announcements/util/AnnouncementsUtil.java
@@ -219,6 +219,10 @@ public class AnnouncementsUtil {
 
 				if (GroupPermissionUtil.contains(
 						themeDisplay.getPermissionChecker(), team.getGroupId(),
+						ActionKeys.MANAGE_ANNOUNCEMENTS) ||
+					RolePermissionUtil.contains(
+						themeDisplay.getPermissionChecker(),
+						themeDisplay.getScopeGroupId(), role.getRoleId(),
 						ActionKeys.MANAGE_ANNOUNCEMENTS)) {
 
 					filteredRoles.add(role);
