ec91b322773ea3d564f66f382dc672749795d1b5
Adolfo Pérez <adolfo.perez@liferay.com>
2015-06-09 11:32:11 +0200

    LPS-56211 Move blogs base action code into new base handler

---
+++ portal-impl/src/com/liferay/portlet/blogs/action/BaseBlogsImageSelectorUploadHandler.java
@@ -0,0 +1,139 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.blogs.action;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.imageselector.BaseImageSelectorUploadHandler;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.servlet.ServletResponseConstants;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.JSONResponseUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.security.auth.PrincipalException;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.security.permission.ResourcePermissionCheckerUtil;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PrefsPropsUtil;
+import com.liferay.portlet.blogs.CoverImageNameException;
+import com.liferay.portlet.blogs.CoverImageSizeException;
+import com.liferay.portlet.blogs.service.permission.BlogsPermission;
+import com.liferay.portlet.documentlibrary.FileNameException;
+import com.liferay.portlet.documentlibrary.antivirus.AntivirusScannerException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+/**
+ * @author Sergio González
+ * @author Adolfo Pérez
+ */
+public abstract class BaseBlogsImageSelectorUploadHandler
+	extends BaseImageSelectorUploadHandler {
+
+	@Override
+	public void checkPermission(
+			long groupId, PermissionChecker permissionChecker)
+		throws PortalException {
+
+		boolean containsResourcePermission =
+			ResourcePermissionCheckerUtil.containsResourcePermission(
+				permissionChecker, BlogsPermission.RESOURCE_NAME, groupId,
+				ActionKeys.ADD_ENTRY);
+
+		if (!containsResourcePermission) {
+			throw new PrincipalException();
+		}
+	}
+
+	@Override
+	public void validateFile(String fileName, String contentType, long size)
+		throws PortalException {
+
+		String extension = FileUtil.getExtension(fileName);
+
+		String[] imageExtensions = PrefsPropsUtil.getStringArray(
+			PropsKeys.BLOGS_IMAGE_EXTENSIONS, StringPool.COMMA);
+
+		for (String imageExtension : imageExtensions) {
+			if (StringPool.STAR.equals(imageExtension) ||
+				imageExtension.equals(StringPool.PERIOD + extension)) {
+
+				return;
+			}
+		}
+
+		throw new CoverImageNameException(
+			"Invalid cover image for file name " + fileName);
+	}
+
+	protected abstract long getMaxFileSize();
+
+	@Override
+	protected void handleUploadException(
+			ActionRequest actionRequest, ActionResponse actionResponse,
+			Exception e, JSONObject jsonObject)
+		throws Exception {
+
+		jsonObject.put("success", Boolean.FALSE);
+
+		if (e instanceof AntivirusScannerException ||
+			e instanceof CoverImageNameException ||
+			e instanceof CoverImageSizeException ||
+			e instanceof FileNameException) {
+
+			String errorMessage = StringPool.BLANK;
+			int errorType = 0;
+
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
+
+			if (e instanceof AntivirusScannerException) {
+				errorType =
+					ServletResponseConstants.SC_FILE_ANTIVIRUS_EXCEPTION;
+				AntivirusScannerException ase = (AntivirusScannerException)e;
+
+				errorMessage = themeDisplay.translate(ase.getMessageKey());
+			}
+			else if (e instanceof CoverImageNameException) {
+				errorType =
+					ServletResponseConstants.SC_FILE_EXTENSION_EXCEPTION;
+			}
+			else if (e instanceof CoverImageSizeException) {
+				errorType = ServletResponseConstants.SC_FILE_SIZE_EXCEPTION;
+			}
+			else if (e instanceof FileNameException) {
+				errorType = ServletResponseConstants.SC_FILE_NAME_EXCEPTION;
+			}
+
+			JSONObject errorJSONObject = JSONFactoryUtil.createJSONObject();
+
+			errorJSONObject.put("errorType", errorType);
+			errorJSONObject.put("message", errorMessage);
+
+			jsonObject.put("error", errorJSONObject);
+
+			JSONResponseUtil.writeJSON(
+				actionRequest, actionResponse, jsonObject);
+		}
+		else {
+			throw e;
+		}
+	}
+
+}
--- portal-impl/src/com/liferay/portlet/blogs/action/BaseImageSelectorAction.java
+++ portal-impl/src/com/liferay/portlet/blogs/action/BaseImageSelectorAction.java
@@ -15,24 +15,8 @@
 package com.liferay.portlet.blogs.action;
 
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.servlet.ServletResponseConstants;
-import com.liferay.portal.kernel.util.FileUtil;
-import com.liferay.portal.kernel.util.PropsKeys;
-import com.liferay.portal.kernel.util.StringPool;
-import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.portal.security.auth.PrincipalException;
-import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.security.permission.PermissionChecker;
-import com.liferay.portal.security.permission.ResourcePermissionCheckerUtil;
-import com.liferay.portal.theme.ThemeDisplay;
-import com.liferay.portal.util.PrefsPropsUtil;
-import com.liferay.portlet.blogs.CoverImageNameException;
-import com.liferay.portlet.blogs.CoverImageSizeException;
-import com.liferay.portlet.blogs.service.permission.BlogsPermission;
-import com.liferay.portlet.documentlibrary.FileNameException;
-import com.liferay.portlet.documentlibrary.antivirus.AntivirusScannerException;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -48,35 +32,14 @@ public abstract class BaseImageSelectorAction
 			long groupId, PermissionChecker permissionChecker)
 		throws PortalException {
 
-		boolean containsResourcePermission =
-			ResourcePermissionCheckerUtil.containsResourcePermission(
-				permissionChecker, BlogsPermission.RESOURCE_NAME, groupId,
-				ActionKeys.ADD_ENTRY);
-
-		if (!containsResourcePermission) {
-			throw new PrincipalException();
-		}
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void validateFile(String fileName, String contentType, long size)
 		throws PortalException {
 
-		String extension = FileUtil.getExtension(fileName);
-
-		String[] imageExtensions = PrefsPropsUtil.getStringArray(
-			PropsKeys.BLOGS_IMAGE_EXTENSIONS, StringPool.COMMA);
-
-		for (String imageExtension : imageExtensions) {
-			if (StringPool.STAR.equals(imageExtension) ||
-				imageExtension.equals(StringPool.PERIOD + extension)) {
-
-				return;
-			}
-		}
-
-		throw new CoverImageNameException(
-			"Invalid cover image for file name " + fileName);
+		throw new UnsupportedOperationException();
 	}
 
 	protected abstract long getMaxFileSize();
@@ -87,49 +50,7 @@ public abstract class BaseImageSelectorAction
 			Exception e, JSONObject jsonObject)
 		throws Exception {
 
-		jsonObject.put("success", Boolean.FALSE);
-
-		if (e instanceof AntivirusScannerException ||
-			e instanceof CoverImageNameException ||
-			e instanceof CoverImageSizeException ||
-			e instanceof FileNameException) {
-
-			String errorMessage = StringPool.BLANK;
-			int errorType = 0;
-
-			ThemeDisplay themeDisplay =
-				(ThemeDisplay)actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
-
-			if (e instanceof AntivirusScannerException) {
-				errorType =
-					ServletResponseConstants.SC_FILE_ANTIVIRUS_EXCEPTION;
-				AntivirusScannerException ase = (AntivirusScannerException)e;
-
-				errorMessage = themeDisplay.translate(ase.getMessageKey());
-			}
-			else if (e instanceof CoverImageNameException) {
-				errorType =
-					ServletResponseConstants.SC_FILE_EXTENSION_EXCEPTION;
-			}
-			else if (e instanceof CoverImageSizeException) {
-				errorType = ServletResponseConstants.SC_FILE_SIZE_EXCEPTION;
-			}
-			else if (e instanceof FileNameException) {
-				errorType = ServletResponseConstants.SC_FILE_NAME_EXCEPTION;
-			}
-
-			JSONObject errorJSONObject = JSONFactoryUtil.createJSONObject();
-
-			errorJSONObject.put("errorType", errorType);
-			errorJSONObject.put("message", errorMessage);
-
-			jsonObject.put("error", errorJSONObject);
-
-			writeJSON(actionRequest, actionResponse, jsonObject);
-		}
-		else {
-			throw e;
-		}
+		throw new UnsupportedOperationException();
 	}
 
 }
