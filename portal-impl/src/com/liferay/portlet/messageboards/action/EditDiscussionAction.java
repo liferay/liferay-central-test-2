162b5ee8e758effc2f7c9328a4583fd53f7b7d4f
Adolfo Pérez <adolfo.perez@liferay.com>
2015-06-09 17:11:15 +0200

    LPS-56228 Move code from portlet action into struts action

--- portal-impl/src/com/liferay/portlet/messageboards/action/EditDiscussionAction.java
+++ portal-impl/src/com/liferay/portlet/messageboards/action/EditDiscussionAction.java
@@ -14,30 +14,7 @@
 
 package com.liferay.portlet.messageboards.action;
 
-import com.liferay.portal.kernel.comment.Comment;
-import com.liferay.portal.kernel.comment.CommentManagerUtil;
-import com.liferay.portal.kernel.comment.DiscussionPermission;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.util.Constants;
-import com.liferay.portal.kernel.util.Function;
-import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.kernel.workflow.WorkflowConstants;
-import com.liferay.portal.model.User;
-import com.liferay.portal.security.auth.PrincipalException;
-import com.liferay.portal.security.auth.PrincipalThreadLocal;
-import com.liferay.portal.service.ServiceContext;
-import com.liferay.portal.service.ServiceContextFunction;
-import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.struts.PortletAction;
-import com.liferay.portal.theme.ThemeDisplay;
-import com.liferay.portal.util.PortalUtil;
-import com.liferay.portal.util.WebKeys;
-import com.liferay.portlet.messageboards.DiscussionMaxCommentsException;
-import com.liferay.portlet.messageboards.MessageBodyException;
-import com.liferay.portlet.messageboards.NoSuchMessageException;
-import com.liferay.portlet.messageboards.RequiredMessageException;
 
 import java.io.IOException;
 
@@ -64,53 +41,7 @@ public class EditDiscussionAction extends PortletAction {
 			ActionResponse actionResponse)
 		throws Exception {
 
-		String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
-
-		try {
-			String redirect = PortalUtil.escapeRedirect(
-				ParamUtil.getString(actionRequest, "redirect"));
-
-			if (cmd.equals(Constants.ADD) || cmd.equals(Constants.UPDATE)) {
-				long commentId = updateComment(actionRequest);
-
-				boolean ajax = ParamUtil.getBoolean(actionRequest, "ajax");
-
-				if (ajax) {
-					String randomNamespace = ParamUtil.getString(
-						actionRequest, "randomNamespace");
-
-					JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
-
-					jsonObject.put("commentId", commentId);
-					jsonObject.put("randomNamespace", randomNamespace);
-
-					writeJSON(actionRequest, actionResponse, jsonObject);
-
-					return;
-				}
-			}
-			else if (cmd.equals(Constants.DELETE)) {
-				deleteComment(actionRequest);
-			}
-			else if (cmd.equals(Constants.SUBSCRIBE_TO_COMMENTS)) {
-				subscribeToComments(actionRequest, true);
-			}
-			else if (cmd.equals(Constants.UNSUBSCRIBE_FROM_COMMENTS)) {
-				subscribeToComments(actionRequest, false);
-			}
-
-			sendRedirect(actionRequest, actionResponse, redirect);
-		}
-		catch (DiscussionMaxCommentsException | MessageBodyException |
-				NoSuchMessageException | PrincipalException |
-				RequiredMessageException e) {
-
-				JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
-
-				jsonObject.putException(e);
-
-				writeJSON(actionRequest, actionResponse, jsonObject);
-		}
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -124,18 +55,7 @@ public class EditDiscussionAction extends PortletAction {
 	}
 
 	protected void deleteComment(ActionRequest actionRequest) throws Exception {
-		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		long commentId = ParamUtil.getLong(actionRequest, "commentId");
-
-		DiscussionPermission discussionPermission =
-			CommentManagerUtil.getDiscussionPermission(
-				themeDisplay.getPermissionChecker());
-
-		discussionPermission.checkDeletePermission(commentId);
-
-		CommentManagerUtil.deleteComment(commentId);
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -147,114 +67,11 @@ public class EditDiscussionAction extends PortletAction {
 			ActionRequest actionRequest, boolean subscribe)
 		throws Exception {
 
-		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		String className = ParamUtil.getString(actionRequest, "className");
-		long classPK = ParamUtil.getLong(actionRequest, "classPK");
-
-		if (subscribe) {
-			CommentManagerUtil.subscribeDiscussion(
-				themeDisplay.getUserId(), themeDisplay.getScopeGroupId(),
-				className, classPK);
-		}
-		else {
-			CommentManagerUtil.unsubscribeDiscussion(
-				themeDisplay.getUserId(), className, classPK);
-		}
+		throw new UnsupportedOperationException();
 	}
 
 	protected long updateComment(ActionRequest actionRequest) throws Exception {
-		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		long commentId = ParamUtil.getLong(actionRequest, "commentId");
-
-		String className = ParamUtil.getString(actionRequest, "className");
-		long classPK = ParamUtil.getLong(actionRequest, "classPK");
-		long parentCommentId = ParamUtil.getLong(
-			actionRequest, "parentCommentId");
-		String subject = ParamUtil.getString(actionRequest, "subject");
-		String body = ParamUtil.getString(actionRequest, "body");
-
-		Function<String, ServiceContext> serviceContextFunction =
-			new ServiceContextFunction(actionRequest);
-
-		DiscussionPermission discussionPermission =
-			CommentManagerUtil.getDiscussionPermission(
-				themeDisplay.getPermissionChecker());
-
-		if (commentId <= 0) {
-
-			// Add message
-
-			User user = null;
-
-			if (themeDisplay.isSignedIn()) {
-				user = themeDisplay.getUser();
-			}
-			else {
-				String emailAddress = ParamUtil.getString(
-					actionRequest, "emailAddress");
-
-				user = UserLocalServiceUtil.fetchUserByEmailAddress(
-					themeDisplay.getCompanyId(), emailAddress);
-
-				if ((user == null) ||
-					(user.getStatus() != WorkflowConstants.STATUS_INCOMPLETE)) {
-
-					return 0;
-				}
-			}
-
-			String name = PrincipalThreadLocal.getName();
-
-			PrincipalThreadLocal.setName(user.getUserId());
-
-			try {
-				discussionPermission.checkAddPermission(
-					themeDisplay.getCompanyId(), themeDisplay.getScopeGroupId(),
-					className, classPK);
-
-				commentId = CommentManagerUtil.addComment(
-					user.getUserId(), className, classPK, user.getFullName(),
-					parentCommentId, subject, body, serviceContextFunction);
-			}
-			finally {
-				PrincipalThreadLocal.setName(name);
-			}
-		}
-		else {
-
-			// Update message
-
-			if (Validator.isNull(className) || (classPK == 0)) {
-				Comment comment = CommentManagerUtil.fetchComment(commentId);
-
-				if (comment != null) {
-					className = comment.getClassName();
-					classPK = comment.getClassPK();
-				}
-			}
-
-			discussionPermission.checkUpdatePermission(commentId);
-
-			commentId = CommentManagerUtil.updateComment(
-				themeDisplay.getUserId(), className, classPK, commentId,
-				subject, body, serviceContextFunction);
-		}
-
-		// Subscription
-
-		boolean subscribe = ParamUtil.getBoolean(actionRequest, "subscribe");
-
-		if (subscribe) {
-			CommentManagerUtil.subscribeDiscussion(
-				themeDisplay.getUserId(), themeDisplay.getScopeGroupId(),
-				className, classPK);
-		}
-
-		return commentId;
+		throw new UnsupportedOperationException();
 	}
 
 	private static final boolean _CHECK_METHOD_ON_PROCESS_ACTION = false;
