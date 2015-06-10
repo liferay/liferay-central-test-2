ec587ff6674912bf5f860370573f9d1db98933cc
Bruno Basto <bruno.basto@liferay.com>
2015-06-10 19:04:33 -0300

    LPS-56073 Adding portlet icon

--- modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/com/liferay/dynamic/data/lists/form/web/portlet/DDLFormAdminPortlet.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/com/liferay/dynamic/data/lists/form/web/portlet/DDLFormAdminPortlet.java
@@ -33,6 +33,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.display-category=category.hidden",
 		"com.liferay.portlet.footer-portlet-javascript=/admin/js/modules.js",
 		"com.liferay.portlet.header-portlet-css=/admin/css/main.css",
+		"com.liferay.portlet.icon=/admin/icons/form.png",
 		"com.liferay.portlet.instanceable=false",
 		"com.liferay.portlet.layout-cacheable=true",
 		"com.liferay.portlet.preferences-owned-by-group=true",
