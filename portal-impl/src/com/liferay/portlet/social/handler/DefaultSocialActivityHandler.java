bdec8fc9ac483a2a075c245b6c8604d40d6b49b6
Adolfo Pérez <adolfo.perez@liferay.com>
2015-06-11 16:47:49 +0200

    LPS-56285 Create skeleton classes from Social Activities handlers

---
+++ portal-impl/src/com/liferay/portlet/social/handler/DefaultSocialActivityHandler.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.social.handler;
+
+import com.liferay.portal.kernel.bean.BeanReference;
+import com.liferay.portlet.social.service.SocialActivityLocalService;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class DefaultSocialActivityHandler extends BaseSocialActivityHandler {
+
+	@Override
+	protected SocialActivityLocalService getSocialActivityLocalService() {
+		return _socialActivityLocalService;
+	}
+
+	@BeanReference(type = SocialActivityLocalService.class)
+	protected SocialActivityLocalService _socialActivityLocalService;
+
+}
