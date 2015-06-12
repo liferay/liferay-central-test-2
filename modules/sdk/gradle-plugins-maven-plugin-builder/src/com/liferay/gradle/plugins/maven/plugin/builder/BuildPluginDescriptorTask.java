3998cf73e25be02418cc1a4bcb860d2492b91d9f
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2015-06-12 14:39:51 -0700

    LPS-51081 Move OSDetector to "gradle-util"

--- modules/sdk/gradle-plugins-maven-plugin-builder/src/com/liferay/gradle/plugins/maven/plugin/builder/BuildPluginDescriptorTask.java
+++ modules/sdk/gradle-plugins-maven-plugin-builder/src/com/liferay/gradle/plugins/maven/plugin/builder/BuildPluginDescriptorTask.java
@@ -14,8 +14,8 @@
 
 package com.liferay.gradle.plugins.maven.plugin.builder;
 
-import com.liferay.gradle.plugins.maven.plugin.builder.util.OSDetector;
 import com.liferay.gradle.plugins.maven.plugin.builder.util.XMLUtil;
+import com.liferay.gradle.util.OSDetector;
 
 import com.thoughtworks.qdox.JavaDocBuilder;
 import com.thoughtworks.qdox.model.BeanProperty;
