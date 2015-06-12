d4f78c80f75d713e1a5f9736183e3c4b2a0712f6
Michael Han <michael.han@liferay.com>
2015-06-12 08:48:15 -0500

    LPS-56273 move to module

--- modules/portal/portal-scheduler/src/com/liferay/portal/scheduler/internal/SchedulerEntryRegistry.java
+++ modules/portal/portal-scheduler/src/com/liferay/portal/scheduler/internal/SchedulerEntryRegistry.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.deploy.hot;
+package com.liferay.portal.scheduler.internal;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
