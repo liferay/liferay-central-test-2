c1f8173b1ea76c140ed650097c2f35a0085d8710
Cristina González Castellano <cristina.gonzalez@liferay.com>
2015-06-12 10:35:17 +0200

    LPS-55878 Ignore tests

--- modules/apps/wiki/wiki-test/test/integration/com/liferay/wiki/trash/test/WikiNodeTrashHandlerTest.java
+++ modules/apps/wiki/wiki-test/test/integration/com/liferay/wiki/trash/test/WikiNodeTrashHandlerTest.java
@@ -188,6 +188,13 @@ public class WikiNodeTrashHandlerTest extends BaseTrashHandlerTestCase {
 	@Ignore
 	@Override
 	@Test
+	public void testTrashGrandparentBaseModelAndRestoreParentModelIsVisible()
+		throws Exception {
+	}
+
+	@Ignore
+	@Override
+	@Test
 	public void testTrashGrandparentBaseModelIsNotVisible() throws Exception {
 	}
 
