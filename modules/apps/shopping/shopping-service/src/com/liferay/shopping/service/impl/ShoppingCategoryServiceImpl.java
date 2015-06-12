6747ecad89620b1e8e7d0aaf4ff2cfece56342c7
peterfellwock <peter.fellwock@liferay.com>
2015-06-12 12:55:31 -0700

    LPS-55355 (SERVICE) Migrate and correct Paths: Java Files

---
+++ modules/apps/shopping/shopping-service/src/com/liferay/shopping/service/impl/ShoppingCategoryServiceImpl.java
@@ -0,0 +1,152 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.shopping.service.impl;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.shopping.model.ShoppingCategory;
+import com.liferay.shopping.service.base.ShoppingCategoryServiceBaseImpl;
+import com.liferay.shopping.service.permission.ShoppingCategoryPermission;
+
+import java.util.List;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class ShoppingCategoryServiceImpl
+	extends ShoppingCategoryServiceBaseImpl {
+
+	@Override
+	public void afterPropertiesSet() {
+		Bundle bundle = FrameworkUtil.getBundle(getClass());
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		_serviceTracker = new ServiceTracker<>(
+			bundleContext, ShoppingCategoryPermission.class, null);
+
+		_serviceTracker.open();
+	}
+
+	@Override
+	public void destroy() {
+		_serviceTracker.close();
+	}
+
+	@Override
+	public ShoppingCategory addCategory(
+			long parentCategoryId, String name, String description,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		getShoppingCategoryPermission().check(
+			getPermissionChecker(), serviceContext.getScopeGroupId(),
+			parentCategoryId, ActionKeys.ADD_CATEGORY);
+
+		return shoppingCategoryLocalService.addCategory(
+			getUserId(), parentCategoryId, name, description, serviceContext);
+	}
+
+	@Override
+	public void deleteCategory(long categoryId) throws PortalException {
+		ShoppingCategory category = shoppingCategoryLocalService.getCategory(
+			categoryId);
+
+		getShoppingCategoryPermission().check(
+			getPermissionChecker(), category, ActionKeys.DELETE);
+
+		shoppingCategoryLocalService.deleteCategory(categoryId);
+	}
+
+	@Override
+	public List<ShoppingCategory> getCategories(long groupId) {
+		return shoppingCategoryPersistence.filterFindByGroupId(groupId);
+	}
+
+	@Override
+	public List<ShoppingCategory> getCategories(
+		long groupId, long parentCategoryId, int start, int end) {
+
+		return shoppingCategoryPersistence.filterFindByG_P(
+			groupId, parentCategoryId, start, end);
+	}
+
+	@Override
+	public int getCategoriesCount(long groupId, long parentCategoryId) {
+		return shoppingCategoryPersistence.filterCountByG_P(
+			groupId, parentCategoryId);
+	}
+
+	@Override
+	public ShoppingCategory getCategory(long categoryId)
+		throws PortalException {
+
+		ShoppingCategory category = shoppingCategoryLocalService.getCategory(
+			categoryId);
+
+		getShoppingCategoryPermission().check(
+			getPermissionChecker(), category, ActionKeys.VIEW);
+
+		return category;
+	}
+
+	@Override
+	public void getSubcategoryIds(
+		List<Long> categoryIds, long groupId, long categoryId) {
+
+		List<ShoppingCategory> categories =
+			shoppingCategoryPersistence.filterFindByG_P(groupId, categoryId);
+
+		for (ShoppingCategory category : categories) {
+			categoryIds.add(category.getCategoryId());
+
+			getSubcategoryIds(
+				categoryIds, category.getGroupId(), category.getCategoryId());
+		}
+	}
+
+	@Override
+	public ShoppingCategory updateCategory(
+			long categoryId, long parentCategoryId, String name,
+			String description, boolean mergeWithParentCategory,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		ShoppingCategory category = shoppingCategoryLocalService.getCategory(
+			categoryId);
+
+		getShoppingCategoryPermission().check(
+			getPermissionChecker(), category, ActionKeys.UPDATE);
+
+		return shoppingCategoryLocalService.updateCategory(
+			categoryId, parentCategoryId, name, description,
+			mergeWithParentCategory, serviceContext);
+	}
+
+	private ShoppingCategoryPermission getShoppingCategoryPermission() {
+		return _serviceTracker.getService();
+	}
+
+	private ServiceTracker
+		<ShoppingCategoryPermission, ShoppingCategoryPermission>
+			_serviceTracker;
+
+}
