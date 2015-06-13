6b1faf4bacaa2f6b37b02fec5db3339d32f3c65a
Eduardo Garcia <eduardo.garcia@liferay.com>
2015-06-13 22:18:07 +0200

    LPS-55754 Avoid test packages conflicts with service packages

---
+++ modules/apps/journal/journal-test/test/integration/com/liferay/journal/lar/test/JournalArticleStagedModelDataHandlerTest.java
@@ -0,0 +1,366 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.lar.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.journal.test.util.JournalTestUtil;
+import com.liferay.portal.kernel.lar.StagedModelDataHandlerUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
+import com.liferay.portal.kernel.test.rule.TransactionalTestRule;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.lar.test.BaseWorkflowedStagedModelDataHandlerTestCase;
+import com.liferay.portal.model.Company;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.StagedModel;
+import com.liferay.portal.service.CompanyLocalServiceUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portlet.asset.model.AssetEntry;
+import com.liferay.portlet.asset.service.AssetEntryLocalServiceUtil;
+import com.liferay.portlet.dynamicdatamapping.model.DDMStructure;
+import com.liferay.portlet.dynamicdatamapping.model.DDMTemplate;
+import com.liferay.portlet.dynamicdatamapping.service.DDMStructureLocalServiceUtil;
+import com.liferay.portlet.dynamicdatamapping.service.DDMTemplateLocalServiceUtil;
+import com.liferay.portlet.dynamicdatamapping.util.test.DDMStructureTestUtil;
+import com.liferay.portlet.dynamicdatamapping.util.test.DDMTemplateTestUtil;
+import com.liferay.portlet.journal.model.JournalArticle;
+import com.liferay.portlet.journal.model.JournalArticleConstants;
+import com.liferay.portlet.journal.model.JournalArticleResource;
+import com.liferay.portlet.journal.model.JournalFolder;
+import com.liferay.portlet.journal.service.JournalArticleLocalServiceUtil;
+import com.liferay.portlet.journal.service.JournalArticleResourceLocalServiceUtil;
+import com.liferay.portlet.journal.service.JournalFolderLocalServiceUtil;
+import com.liferay.portlet.journal.service.persistence.JournalArticleResourceUtil;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Daniel Kocsis
+ */
+@RunWith(Arquillian.class)
+@Sync
+public class JournalArticleStagedModelDataHandlerTest
+	extends BaseWorkflowedStagedModelDataHandlerTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			SynchronousDestinationTestRule.INSTANCE,
+			TransactionalTestRule.INSTANCE);
+
+	@Test
+	public void testCompanyScopeDependencies() throws Exception {
+		initExport();
+
+		Map<String, List<StagedModel>> dependentStagedModelsMap =
+			addCompanyDependencies();
+
+		StagedModel stagedModel = addStagedModel(
+			stagingGroup, dependentStagedModelsMap);
+
+		StagedModelDataHandlerUtil.exportStagedModel(
+			portletDataContext, stagedModel);
+
+		initImport();
+
+		StagedModel exportedStagedModel = readExportedStagedModel(stagedModel);
+
+		Assert.assertNotNull(exportedStagedModel);
+
+		StagedModelDataHandlerUtil.importStagedModel(
+			portletDataContext, exportedStagedModel);
+
+		validateCompanyDependenciesImport(dependentStagedModelsMap, liveGroup);
+	}
+
+	protected Map<String, List<StagedModel>> addCompanyDependencies()
+		throws Exception {
+
+		Map<String, List<StagedModel>> dependentStagedModelsMap =
+			new HashMap<>();
+
+		Company company = CompanyLocalServiceUtil.fetchCompany(
+			stagingGroup.getCompanyId());
+
+		Group companyGroup = company.getGroup();
+
+		DDMStructure ddmStructure = DDMStructureTestUtil.addStructure(
+			companyGroup.getGroupId(), JournalArticle.class.getName());
+
+		addDependentStagedModel(
+			dependentStagedModelsMap, DDMStructure.class, ddmStructure);
+
+		DDMTemplate ddmTemplate = DDMTemplateTestUtil.addTemplate(
+			companyGroup.getGroupId(), ddmStructure.getStructureId());
+
+		addDependentStagedModel(
+			dependentStagedModelsMap, DDMTemplate.class, ddmTemplate);
+
+		JournalFolder folder = JournalTestUtil.addFolder(
+			stagingGroup.getGroupId(), RandomTestUtil.randomString());
+
+		addDependentStagedModel(
+			dependentStagedModelsMap, JournalFolder.class, folder);
+
+		return dependentStagedModelsMap;
+	}
+
+	@Override
+	protected Map<String, List<StagedModel>> addDependentStagedModelsMap(
+			Group group)
+		throws Exception {
+
+		Map<String, List<StagedModel>> dependentStagedModelsMap =
+			new LinkedHashMap<>();
+
+		DDMStructure ddmStructure = DDMStructureTestUtil.addStructure(
+			group.getGroupId(), JournalArticle.class.getName());
+
+		DDMTemplate ddmTemplate = DDMTemplateTestUtil.addTemplate(
+			group.getGroupId(), ddmStructure.getStructureId());
+
+		addDependentStagedModel(
+			dependentStagedModelsMap, DDMTemplate.class, ddmTemplate);
+
+		addDependentStagedModel(
+			dependentStagedModelsMap, DDMStructure.class, ddmStructure);
+
+		JournalFolder folder = JournalTestUtil.addFolder(
+			group.getGroupId(), RandomTestUtil.randomString());
+
+		addDependentStagedModel(
+			dependentStagedModelsMap, JournalFolder.class, folder);
+
+		return dependentStagedModelsMap;
+	}
+
+	@Override
+	protected StagedModel addStagedModel(
+			Group group,
+			Map<String, List<StagedModel>> dependentStagedModelsMap)
+		throws Exception {
+
+		List<StagedModel> folderDependentStagedModels =
+			dependentStagedModelsMap.get(JournalFolder.class.getSimpleName());
+
+		JournalFolder folder = (JournalFolder)folderDependentStagedModels.get(
+			0);
+
+		List<StagedModel> ddmStructureDependentStagedModels =
+			dependentStagedModelsMap.get(DDMStructure.class.getSimpleName());
+
+		DDMStructure ddmStructure =
+			(DDMStructure)ddmStructureDependentStagedModels.get(0);
+
+		List<StagedModel> ddmTemplateDependentStagedModels =
+			dependentStagedModelsMap.get(DDMTemplate.class.getSimpleName());
+
+		DDMTemplate ddmTemplate =
+			(DDMTemplate)ddmTemplateDependentStagedModels.get(0);
+
+		return JournalTestUtil.addArticleWithXMLContent(
+			group.getGroupId(), folder.getFolderId(),
+			JournalArticleConstants.CLASSNAME_ID_DEFAULT,
+			DDMStructureTestUtil.getSampleStructuredContent(),
+			ddmStructure.getStructureKey(), ddmTemplate.getTemplateKey());
+	}
+
+	@Override
+	protected List<StagedModel> addWorkflowedStagedModels(Group group)
+		throws Exception {
+
+		List<StagedModel> stagedModels = new ArrayList<>();
+
+		stagedModels.add(
+			JournalTestUtil.addArticleWithWorkflow(group.getGroupId(), true));
+
+		stagedModels.add(
+			JournalTestUtil.addArticleWithWorkflow(group.getGroupId(), false));
+
+		JournalArticle expiredArticle = JournalTestUtil.addArticleWithWorkflow(
+			group.getGroupId(), true);
+
+		expiredArticle = JournalArticleLocalServiceUtil.expireArticle(
+			TestPropsValues.getUserId(), group.getGroupId(),
+			expiredArticle.getArticleId(), expiredArticle.getVersion(),
+			expiredArticle.getUrlTitle(),
+			ServiceContextTestUtil.getServiceContext());
+
+		stagedModels.add(expiredArticle);
+
+		return stagedModels;
+	}
+
+	@Override
+	protected AssetEntry fetchAssetEntry(StagedModel stagedModel, Group group)
+		throws Exception {
+
+		JournalArticle article = (JournalArticle)stagedModel;
+
+		JournalArticleResource articleResource =
+			JournalArticleResourceLocalServiceUtil.getArticleResource(
+				article.getResourcePrimKey());
+
+		return AssetEntryLocalServiceUtil.fetchEntry(
+			group.getGroupId(), articleResource.getUuid());
+	}
+
+	@Override
+	protected StagedModel getStagedModel(String uuid, Group group) {
+		try {
+			return JournalArticleLocalServiceUtil.
+				getJournalArticleByUuidAndGroupId(uuid, group.getGroupId());
+		}
+		catch (Exception e) {
+			return null;
+		}
+	}
+
+	@Override
+	protected Class<? extends StagedModel> getStagedModelClass() {
+		return JournalArticle.class;
+	}
+
+	@Override
+	protected boolean isCommentableStagedModel() {
+		return true;
+	}
+
+	protected void validateCompanyDependenciesImport(
+			Map<String, List<StagedModel>> dependentStagedModelsMap,
+			Group group)
+		throws Exception {
+
+		List<StagedModel> ddmStructureDependentStagedModels =
+			dependentStagedModelsMap.get(DDMStructure.class.getSimpleName());
+
+		Assert.assertEquals(1, ddmStructureDependentStagedModels.size());
+
+		DDMStructure ddmStructure =
+			(DDMStructure)ddmStructureDependentStagedModels.get(0);
+
+		Assert.assertNull(
+			"Company DDM structure dependency should not be imported",
+			DDMStructureLocalServiceUtil.fetchDDMStructureByUuidAndGroupId(
+				ddmStructure.getUuid(), group.getGroupId()));
+
+		List<StagedModel> ddmTemplateDependentStagedModels =
+			dependentStagedModelsMap.get(DDMTemplate.class.getSimpleName());
+
+		Assert.assertEquals(1, ddmTemplateDependentStagedModels.size());
+
+		DDMTemplate ddmTemplate =
+			(DDMTemplate)ddmTemplateDependentStagedModels.get(0);
+
+		Assert.assertNull(
+			"Company DDM template dependency should not be imported",
+			DDMTemplateLocalServiceUtil.fetchDDMTemplateByUuidAndGroupId(
+				ddmTemplate.getUuid(), group.getGroupId()));
+
+		List<StagedModel> folderDependentStagedModels =
+			dependentStagedModelsMap.get(JournalFolder.class.getSimpleName());
+
+		Assert.assertEquals(1, folderDependentStagedModels.size());
+
+		JournalFolder folder = (JournalFolder)folderDependentStagedModels.get(
+			0);
+
+		JournalFolderLocalServiceUtil.getJournalFolderByUuidAndGroupId(
+			folder.getUuid(), group.getGroupId());
+	}
+
+	@Override
+	protected void validateImport(
+			Map<String, List<StagedModel>> dependentStagedModelsMap,
+			Group group)
+		throws Exception {
+
+		List<StagedModel> ddmStructureDependentStagedModels =
+			dependentStagedModelsMap.get(DDMStructure.class.getSimpleName());
+
+		Assert.assertEquals(1, ddmStructureDependentStagedModels.size());
+
+		DDMStructure ddmStructure =
+			(DDMStructure)ddmStructureDependentStagedModels.get(0);
+
+		DDMStructureLocalServiceUtil.getDDMStructureByUuidAndGroupId(
+			ddmStructure.getUuid(), group.getGroupId());
+
+		List<StagedModel> ddmTemplateDependentStagedModels =
+			dependentStagedModelsMap.get(DDMTemplate.class.getSimpleName());
+
+		Assert.assertEquals(1, ddmTemplateDependentStagedModels.size());
+
+		DDMTemplate ddmTemplate =
+			(DDMTemplate)ddmTemplateDependentStagedModels.get(0);
+
+		DDMTemplateLocalServiceUtil.getDDMTemplateByUuidAndGroupId(
+			ddmTemplate.getUuid(), group.getGroupId());
+
+		List<StagedModel> folderDependentStagedModels =
+			dependentStagedModelsMap.get(JournalFolder.class.getSimpleName());
+
+		Assert.assertEquals(1, folderDependentStagedModels.size());
+
+		JournalFolder folder = (JournalFolder)folderDependentStagedModels.get(
+			0);
+
+		JournalFolderLocalServiceUtil.getJournalFolderByUuidAndGroupId(
+			folder.getUuid(), group.getGroupId());
+	}
+
+	@Override
+	protected void validateImport(
+			StagedModel stagedModel, StagedModelAssets stagedModelAssets,
+			Map<String, List<StagedModel>> dependentStagedModelsMap,
+			Group group)
+		throws Exception {
+
+		JournalArticle article = (JournalArticle)stagedModel;
+
+		JournalArticleResource articleResource =
+			JournalArticleResourceUtil.fetchByUUID_G(
+				article.getArticleResourceUuid(), group.getGroupId());
+
+		Assert.assertNotNull(articleResource);
+
+		JournalArticle importedArticle =
+			JournalArticleLocalServiceUtil.getLatestArticle(
+				articleResource.getResourcePrimKey(), article.getStatus(),
+				false);
+
+		validateAssets(importedArticle, stagedModelAssets, group);
+
+		validateComments(article, importedArticle, group);
+
+		validateImport(dependentStagedModelsMap, group);
+	}
+
+}
---
+++ modules/apps/journal/journal-test/test/integration/com/liferay/journal/lar/test/JournalExportImportTest.java
@@ -0,0 +1,342 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.lar.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.journal.test.util.JournalTestUtil;
+import com.liferay.journal.web.lar.JournalPortletDataHandler;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.lar.PortletDataHandlerKeys;
+import com.liferay.portal.kernel.lar.UserIdStrategy;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.lar.test.BasePortletExportImportTestCase;
+import com.liferay.portal.model.Company;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.StagedModel;
+import com.liferay.portal.service.CompanyLocalServiceUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.test.ServiceTestUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portlet.asset.model.AssetEntry;
+import com.liferay.portlet.asset.service.AssetEntryLocalServiceUtil;
+import com.liferay.portlet.dynamicdatamapping.model.DDMStructure;
+import com.liferay.portlet.dynamicdatamapping.model.DDMTemplate;
+import com.liferay.portlet.dynamicdatamapping.service.DDMStructureLocalServiceUtil;
+import com.liferay.portlet.dynamicdatamapping.service.DDMTemplateLocalServiceUtil;
+import com.liferay.portlet.dynamicdatamapping.util.test.DDMStructureTestUtil;
+import com.liferay.portlet.dynamicdatamapping.util.test.DDMTemplateTestUtil;
+import com.liferay.portlet.journal.model.JournalArticle;
+import com.liferay.portlet.journal.model.JournalArticleConstants;
+import com.liferay.portlet.journal.model.JournalFolderConstants;
+import com.liferay.portlet.journal.service.JournalArticleLocalServiceUtil;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Juan Fernández
+ */
+@RunWith(Arquillian.class)
+@Sync
+public class JournalExportImportTest extends BasePortletExportImportTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			SynchronousDestinationTestRule.INSTANCE);
+
+	@Override
+	public String getNamespace() {
+		return JournalPortletDataHandler.NAMESPACE;
+	}
+
+	@Override
+	public String getPortletId() {
+		return PortletKeys.JOURNAL;
+	}
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		ServiceTestUtil.setUser(TestPropsValues.getUser());
+	}
+
+	@Test
+	public void testExportImportCompanyScopeStructuredJournalArticle()
+		throws Exception {
+
+		exportImportJournalArticle(true);
+	}
+
+	@Test
+	public void testExportImportJournalArticleVersions() throws Exception {
+		JournalArticle article = (JournalArticle)addStagedModel(
+			group.getGroupId());
+
+		exportImportPortlet(PortletKeys.JOURNAL);
+
+		JournalArticle liveArticle =
+			JournalArticleLocalServiceUtil.fetchJournalArticleByUuidAndGroupId(
+				article.getUuid(), importedGroup.getGroupId());
+
+		Assert.assertNotNull(liveArticle);
+
+		JournalTestUtil.updateArticle(article, RandomTestUtil.randomString());
+
+		int articlesCount = JournalArticleLocalServiceUtil.getArticlesCount(
+			group.getGroupId(), article.getArticleId());
+
+		Assert.assertEquals(2, articlesCount);
+
+		exportImportPortlet(PortletKeys.JOURNAL);
+
+		articlesCount = JournalArticleLocalServiceUtil.getArticlesCount(
+			importedGroup.getGroupId(), liveArticle.getArticleId());
+
+		Assert.assertEquals(2, articlesCount);
+	}
+
+	@Test
+	public void testExportImportStructuredJournalArticle() throws Exception {
+		exportImportJournalArticle(false);
+	}
+
+	@Override
+	protected StagedModel addStagedModel(long groupId) throws Exception {
+		return JournalTestUtil.addArticle(
+			groupId, RandomTestUtil.randomString(),
+			RandomTestUtil.randomString());
+	}
+
+	@Override
+	protected StagedModel addStagedModel(long groupId, Date createdDate)
+		throws Exception {
+
+		String title = RandomTestUtil.randomString();
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(groupId);
+
+		serviceContext.setCommand(Constants.ADD);
+		serviceContext.setCreateDate(createdDate);
+		serviceContext.setLayoutFullURL("http://localhost");
+		serviceContext.setModifiedDate(createdDate);
+
+		return JournalTestUtil.addArticle(
+			groupId, JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			JournalArticleConstants.CLASSNAME_ID_DEFAULT, title, title,
+			RandomTestUtil.randomString(), LocaleUtil.getSiteDefault(), false,
+			false, serviceContext);
+	}
+
+	@Override
+	protected void deleteStagedModel(StagedModel stagedModel) throws Exception {
+		JournalArticleLocalServiceUtil.deleteArticle(
+			(JournalArticle)stagedModel);
+	}
+
+	protected void exportImportJournalArticle(boolean companyScopeDependencies)
+		throws Exception {
+
+		JournalArticle article = null;
+		DDMStructure ddmStructure = null;
+		DDMTemplate ddmTemplate = null;
+
+		long groupId = group.getGroupId();
+
+		Company company = CompanyLocalServiceUtil.fetchCompany(
+			group.getCompanyId());
+
+		Group companyGroup = company.getGroup();
+
+		if (companyScopeDependencies) {
+			groupId = companyGroup.getGroupId();
+		}
+
+		ddmStructure = DDMStructureTestUtil.addStructure(
+			groupId, JournalArticle.class.getName());
+
+		ddmTemplate = DDMTemplateTestUtil.addTemplate(
+			groupId, ddmStructure.getStructureId());
+
+		String content = DDMStructureTestUtil.getSampleStructuredContent();
+
+		article = JournalTestUtil.addArticleWithXMLContent(
+			group.getGroupId(), content, ddmStructure.getStructureKey(),
+			ddmTemplate.getTemplateKey());
+
+		exportImportPortlet(PortletKeys.JOURNAL);
+
+		int articlesCount = JournalArticleLocalServiceUtil.getArticlesCount(
+			importedGroup.getGroupId());
+
+		Assert.assertEquals(1, articlesCount);
+
+		JournalArticle groupArticle =
+			JournalArticleLocalServiceUtil.fetchJournalArticleByUuidAndGroupId(
+				article.getUuid(), importedGroup.getGroupId());
+
+		Assert.assertNotNull(groupArticle);
+
+		groupId = importedGroup.getGroupId();
+
+		if (companyScopeDependencies) {
+			DDMStructure importedDDMStructure =
+				DDMStructureLocalServiceUtil.fetchDDMStructureByUuidAndGroupId(
+					ddmStructure.getUuid(), groupId);
+
+			Assert.assertNull(importedDDMStructure);
+
+			DDMTemplate importedDDMTemplate =
+				DDMTemplateLocalServiceUtil.fetchDDMTemplateByUuidAndGroupId(
+					ddmTemplate.getUuid(), groupId);
+
+			Assert.assertNull(importedDDMTemplate);
+
+			groupId = companyGroup.getGroupId();
+		}
+
+		DDMStructure dependentDDMStructure =
+			DDMStructureLocalServiceUtil.fetchDDMStructureByUuidAndGroupId(
+				ddmStructure.getUuid(), groupId);
+
+		Assert.assertNotNull(dependentDDMStructure);
+
+		DDMTemplate dependentDDMTemplate =
+			DDMTemplateLocalServiceUtil.fetchDDMTemplateByUuidAndGroupId(
+				ddmTemplate.getUuid(), groupId);
+
+		Assert.assertNotNull(dependentDDMTemplate);
+		Assert.assertEquals(
+			article.getDDMStructureKey(),
+			dependentDDMStructure.getStructureKey());
+		Assert.assertEquals(
+			article.getDDMTemplateKey(), dependentDDMTemplate.getTemplateKey());
+		Assert.assertEquals(
+			dependentDDMTemplate.getClassPK(),
+			dependentDDMStructure.getStructureId());
+	}
+
+	@Override
+	protected AssetEntry getAssetEntry(StagedModel stagedModel)
+		throws PortalException {
+
+		JournalArticle article = (JournalArticle)stagedModel;
+
+		return AssetEntryLocalServiceUtil.getEntry(
+			article.getGroupId(), article.getArticleResourceUuid());
+	}
+
+	protected Map<String, String[]> getBaseParameterMap(long groupId, long plid)
+		throws Exception {
+
+		Map<String, String[]> parameterMap = new HashMap<>();
+
+		parameterMap.put(
+			PortletDataHandlerKeys.PERMISSIONS,
+			new String[] {Boolean.TRUE.toString()});
+		parameterMap.put(
+			PortletDataHandlerKeys.PORTLET_DATA_CONTROL_DEFAULT,
+			new String[] {Boolean.FALSE.toString()});
+
+		addParameter(parameterMap, "doAsGroupId", String.valueOf(groupId));
+		addParameter(parameterMap, "feeds", true);
+		addParameter(parameterMap, "groupId", String.valueOf(groupId));
+		addParameter(
+			parameterMap, "permissionsAssignedToRoles",
+			Boolean.TRUE.toString());
+		addParameter(parameterMap, "plid", String.valueOf(plid));
+		addParameter(parameterMap, "portletResource", PortletKeys.JOURNAL);
+		addParameter(parameterMap, "referenced-content", true);
+		addParameter(parameterMap, "structures", true);
+		addParameter(parameterMap, "version-history", true);
+		addParameter(parameterMap, "web-content", true);
+
+		return parameterMap;
+	}
+
+	@Override
+	protected Map<String, String[]> getExportParameterMap() throws Exception {
+		Map<String, String[]> parameterMap = super.getExportParameterMap();
+
+		MapUtil.merge(
+			parameterMap,
+			getBaseParameterMap(group.getGroupId(), layout.getPlid()));
+
+		parameterMap.put(
+			PortletDataHandlerKeys.PORTLET_DATA + StringPool.UNDERLINE +
+				PortletKeys.JOURNAL,
+			new String[] {Boolean.TRUE.toString()});
+
+		return parameterMap;
+	}
+
+	@Override
+	protected Map<String, String[]> getImportParameterMap() throws Exception {
+		Map<String, String[]> parameterMap = super.getImportParameterMap();
+
+		MapUtil.merge(
+			parameterMap,
+			getBaseParameterMap(
+				importedGroup.getGroupId(), importedLayout.getPlid()));
+
+		parameterMap.put(
+			PortletDataHandlerKeys.DATA_STRATEGY,
+			new String[] {PortletDataHandlerKeys.DATA_STRATEGY_MIRROR});
+		parameterMap.put(
+			PortletDataHandlerKeys.DELETE_PORTLET_DATA,
+			new String[] {Boolean.FALSE.toString()});
+		parameterMap.put(
+			PortletDataHandlerKeys.USER_ID_STRATEGY,
+			new String[] {UserIdStrategy.CURRENT_USER_ID});
+
+		return parameterMap;
+	}
+
+	@Override
+	protected StagedModel getStagedModel(String uuid, long groupId) {
+		return
+			JournalArticleLocalServiceUtil.fetchJournalArticleByUuidAndGroupId(
+				uuid, groupId);
+	}
+
+	@Override
+	protected void testExportImportDisplayStyle(long groupId, String scopeType)
+		throws Exception {
+	}
+
+}
