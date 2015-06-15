a8b12fed2cd183e25fe7c4164f27133ddc95ccbc
drewbrokke <drew.brokke@liferay.com>
2015-06-15 12:49:58 -0700

    LPS-56199 registers portlet

--- modules/apps/expando/expando-web/src/com/liferay/expando/web/portlet/ExpandoPortlet.java
+++ modules/apps/expando/expando-web/src/com/liferay/expando/web/portlet/ExpandoPortlet.java
@@ -49,15 +49,45 @@ import java.util.List;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.portlet.Portlet;
 import javax.portlet.PortletException;
 import javax.portlet.PortletRequest;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
+import org.osgi.service.component.annotations.Component;
+
 /**
  * @author Raymond Augé
  * @author Drew Brokke
  */
+
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.icon=/icons/expando.png",
+		"com.liferay.portlet.control-panel-entry-category=configuration",
+		"com.liferay.portlet.control-panel-entry-weight=2.0",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.use-default-template=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.css-class-wrapper=portlet-expando",
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.system=true",
+		"com.liferay.portlet.display-category=category.hidden",
+		"javax.portlet.portlet-name=com_liferay_expando_web_portlet_ExpandoPortlet",
+		"javax.portlet.display-name=Custom Fields",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.portlet-mode=text/html",
+		"javax.portlet.resource-bundle=content.Language"
+	},
+	service = Portlet.class
+)
 public class ExpandoPortlet extends MVCPortlet {
 
 	public void addExpando(
