eeb34dc42d845cb65a3ef8bd567bed0b1a9f7039
Brian Chan <brian.chan@liferay.com>
2015-06-15 15:13:24 -0700

    LPS-47199 SF

--- modules/apps/journal/journal-test/test/integration/com/liferay/journal/service/persistence/test/JournalArticleFinderTest.java
+++ modules/apps/journal/journal-test/test/integration/com/liferay/journal/service/persistence/test/JournalArticleFinderTest.java
@@ -419,6 +419,7 @@ public class JournalArticleFinderTest {
 
 		QueryDefinition<JournalArticle> queryDefinition = new QueryDefinition<>(
 			WorkflowConstants.STATUS_ANY);
+
 		queryDefinition.setUserId(TestPropsValues.getUserId());
 
 		testQueryByG_C(
