a1d4a6c938d1116fa11dcf7b2a199051143165a2
Cristina González Castellano <cristina.gonzalez@liferay.com>
2015-06-15 11:38:07 +0200

    LPS-55878 Add Interface WhenIsAssetableParentModel

--- modules/apps/bookmarks/bookmarks-test/test/integration/com/liferay/bookmarks/trash/test/BookmarksFolderTrashHandlerTest.java
+++ modules/apps/bookmarks/bookmarks-test/test/integration/com/liferay/bookmarks/trash/test/BookmarksFolderTrashHandlerTest.java
@@ -35,6 +35,7 @@ import com.liferay.portal.service.test.ServiceTestUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portlet.trash.test.BaseTrashHandlerTestCase;
 import com.liferay.portlet.trash.test.WhenIsAssetableBaseModel;
+import com.liferay.portlet.trash.test.WhenIsAssetableParentModel;
 import com.liferay.portlet.trash.test.WhenIsIndexableBaseModel;
 
 import org.junit.After;
@@ -52,7 +53,8 @@ import org.junit.runner.RunWith;
 @Sync
 public class BookmarksFolderTrashHandlerTest
 	extends BaseTrashHandlerTestCase
-	implements WhenIsAssetableBaseModel, WhenIsIndexableBaseModel {
+	implements WhenIsAssetableBaseModel, WhenIsAssetableParentModel,
+	WhenIsIndexableBaseModel {
 
 	@ClassRule
 	@Rule
--- modules/apps/wiki/wiki-test/test/integration/com/liferay/wiki/trash/test/WikiPageTrashHandlerTest.java
+++ modules/apps/wiki/wiki-test/test/integration/com/liferay/wiki/trash/test/WikiPageTrashHandlerTest.java
@@ -28,6 +28,7 @@ import com.liferay.portal.service.test.ServiceTestUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portlet.trash.test.BaseTrashHandlerTestCase;
 import com.liferay.portlet.trash.test.WhenIsAssetableBaseModel;
+import com.liferay.portlet.trash.test.WhenIsAssetableParentModel;
 import com.liferay.portlet.trash.test.WhenIsIndexableBaseModel;
 import com.liferay.wiki.asset.WikiPageAssetRenderer;
 import com.liferay.wiki.model.WikiNode;
@@ -50,7 +51,8 @@ import org.junit.runner.RunWith;
 @Sync
 public class WikiPageTrashHandlerTest
 	extends BaseTrashHandlerTestCase
-	implements WhenIsAssetableBaseModel, WhenIsIndexableBaseModel {
+	implements WhenIsAssetableBaseModel, WhenIsAssetableParentModel,
+	WhenIsIndexableBaseModel {
 
 	@ClassRule
 	@Rule
