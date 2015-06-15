909f85a71a23f51004148770a310510351a841a3
Bruno Basto <bruno.basto@liferay.com>
2015-06-15 14:32:25 -0300

    LPS-56382 Regenerate JavaScript renderers

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-select/src/META-INF/resources/select.soy.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-select/src/META-INF/resources/select.soy.js
@@ -5,13 +5,13 @@ if (typeof ddm == 'undefined') { var ddm = {}; }
 
 
 ddm.select = function(opt_data, opt_ignored) {
-var output = '\t<div class="form-group field-wrapper" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '"><div class="form-group input-select-wrapper"><label class="control-label" for="' + soy.$$escapeHtml(opt_data.name) + '">' + soy.$$escapeHtml(opt_data.label) + '</label><select id="' + soy.$$escapeHtml(opt_data.name) + '" class="form-control" dir="' + soy.$$escapeHtml(opt_data.dir) + '" name="' + soy.$$escapeHtml(opt_data.name) + '" ' + soy.$$escapeHtml(opt_data.multiple) + '>';
-var optionList18 = opt_data.options;
-var optionListLen18 = optionList18.length;
-for (var optionIndex18 = 0; optionIndex18 < optionListLen18; optionIndex18++) {
-	var optionData18 = optionList18[optionIndex18];
-	output += '<option dir="' + soy.$$escapeHtml(opt_data.dir) + '" ' + soy.$$escapeHtml(optionData18.status) + ' value="' + soy.$$escapeHtml(optionData18.value) + '">' + soy.$$escapeHtml(optionData18.label) + '</option>';
-}
-output += '</select>' + soy.$$filterNoAutoescape(opt_data.childElementsHTML) + '</div></div>';
-return output;
+  var output = '\t<div class="form-group field-wrapper' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + '" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '"><div class="form-group input-select-wrapper"><label class="control-label" for="' + soy.$$escapeHtml(opt_data.name) + '">' + soy.$$escapeHtml(opt_data.label) + '</label><select id="' + soy.$$escapeHtml(opt_data.name) + '" class="form-control" dir="' + soy.$$escapeHtml(opt_data.dir) + '" name="' + soy.$$escapeHtml(opt_data.name) + '" ' + soy.$$escapeHtml(opt_data.multiple) + '>';
+  var optionList20 = opt_data.options;
+  var optionListLen20 = optionList20.length;
+  for (var optionIndex20 = 0; optionIndex20 < optionListLen20; optionIndex20++) {
+    var optionData20 = optionList20[optionIndex20];
+    output += '<option dir="' + soy.$$escapeHtml(opt_data.dir) + '" ' + soy.$$escapeHtml(optionData20.status) + ' value="' + soy.$$escapeHtml(optionData20.value) + '">' + soy.$$escapeHtml(optionData20.label) + '</option>';
+  }
+  output += '</select>' + soy.$$filterNoAutoescape(opt_data.childElementsHTML) + '</div></div>';
+  return output;
 };
