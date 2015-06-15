74abe47d7fd8df6ae4a0074cef06537827ab5b8f
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2015-06-15 12:12:19 -0700

    LPS-54970 Ant

---
+++ modules/util/css-builder/src/com/liferay/css/builder/ant/CSSBuilderTask.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.css.builder.ant;
+
+import com.liferay.css.builder.CSSBuilderArgs;
+import com.liferay.css.builder.CSSBuilderInvoker;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.Task;
+
+/**
+ * @author Andrea Di Giorgi
+ */
+public class CSSBuilderTask extends Task {
+
+	@Override
+	public void execute() throws BuildException {
+		try {
+			Project project = getProject();
+
+			CSSBuilderInvoker.invoke(project.getBaseDir(), _cssBuilderArgs);
+		}
+		catch (Exception e) {
+			throw new BuildException(e);
+		}
+	}
+
+	public void setDirNames(String dirNames) {
+		_cssBuilderArgs.setDirNames(dirNames);
+	}
+
+	public void setDocrootDirName(String docrootDirName) {
+		_cssBuilderArgs.setDocrootDirName(docrootDirName);
+	}
+
+	public void setPortalCommonDirName(String portalCommonDirName) {
+		_cssBuilderArgs.setPortalCommonDirName(portalCommonDirName);
+	}
+
+	public void setRtlExcludedPathRegexps(String rtlExcludedPathRegexps) {
+		_cssBuilderArgs.setRtlExcludedPathRegexps(rtlExcludedPathRegexps);
+	}
+
+	public void setSassCompilerClassName(String sassCompilerClassName) {
+		_cssBuilderArgs.setSassCompilerClassName(sassCompilerClassName);
+	}
+
+	private final CSSBuilderArgs _cssBuilderArgs = new CSSBuilderArgs();
+
+}
