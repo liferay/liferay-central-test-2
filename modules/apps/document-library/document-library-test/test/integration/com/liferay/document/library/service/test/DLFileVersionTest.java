12616c9d63e6e9458f7d6af42aed2115a2ff28fd
Brian Chan <brian.chan@liferay.com>
2015-06-17 11:09:47 -0700

    LPS-55433 SF

--- modules/apps/document-library/document-library-test/test/integration/com/liferay/document/library/service/test/DLFileVersionTest.java
+++ modules/apps/document-library/document-library-test/test/integration/com/liferay/document/library/service/test/DLFileVersionTest.java
@@ -155,6 +155,7 @@ public class DLFileVersionTest {
 		ExpandoTableLocalServiceUtil.deleteTable(expandoTable);
 
 		PermissionThreadLocal.setPermissionChecker(_originalPermissionChecker);
+
 		PrincipalThreadLocal.setName(_originalName);
 
 		RoleTestUtil.removeResourcePermission(
