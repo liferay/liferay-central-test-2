7bc5adb79fbc953731515dcb010fb9f7b21bd4c4
Brian Chan <brian.chan@liferay.com>
2015-06-17 11:17:52 -0700

    LPS-56271 SF

--- portal-impl/test/integration/com/liferay/portal/service/ResourceBlockLocalServiceTest.java
+++ portal-impl/test/integration/com/liferay/portal/service/ResourceBlockLocalServiceTest.java
@@ -84,17 +84,20 @@ public class ResourceBlockLocalServiceTest {
 				expectedType = ExpectedType.PREFIX
 			),
 			@ExpectedLog(
-				dbType = DB.TYPE_MYSQL, expectedLog =
+				dbType = DB.TYPE_MYSQL,
+				expectedLog =
 					"Deadlock found when trying to get lock; try restarting " +
 						"transaction",
 				expectedType = ExpectedType.EXACT
 			),
 			@ExpectedLog(
-				dbType = DB.TYPE_MYSQL, expectedLog = "Duplicate entry ",
+				dbType = DB.TYPE_MYSQL,
+				expectedLog = "Duplicate entry ",
 				expectedType = ExpectedType.PREFIX
 			),
 			@ExpectedLog(
-				dbType = DB.TYPE_POSTGRESQL, expectedLog =
+				dbType = DB.TYPE_POSTGRESQL,
+				expectedLog =
 					"ERROR: duplicate key value violates unique constraint ",
 				expectedType = ExpectedType.PREFIX
 			)
@@ -184,7 +187,8 @@ public class ResourceBlockLocalServiceTest {
 				expectedType = ExpectedType.PREFIX
 			),
 			@ExpectedLog(
-				dbType = DB.TYPE_MYSQL, expectedLog =
+				dbType = DB.TYPE_MYSQL,
+				expectedLog =
 					"Deadlock found when trying to get lock; try restarting " +
 						"transaction",
 				expectedType = ExpectedType.EXACT
@@ -194,7 +198,8 @@ public class ResourceBlockLocalServiceTest {
 				expectedType = ExpectedType.PREFIX
 			),
 			@ExpectedLog(
-				dbType = DB.TYPE_POSTGRESQL, expectedLog =
+				dbType = DB.TYPE_POSTGRESQL,
+				expectedLog =
 					"ERROR: duplicate key value violates unique constraint ",
 				expectedType = ExpectedType.PREFIX
 			)
