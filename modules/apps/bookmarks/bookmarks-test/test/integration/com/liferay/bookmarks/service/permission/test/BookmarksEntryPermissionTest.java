1cac3f949e280ad8f931a7badd3218068a5247aa
Brian Chan <brian.chan@liferay.com>
2015-06-18 09:47:25 -0700

    LPS-53961 regen

--- modules/apps/bookmarks/bookmarks-test/test/integration/com/liferay/bookmarks/service/permission/test/BookmarksEntryPermissionTest.java
+++ modules/apps/bookmarks/bookmarks-test/test/integration/com/liferay/bookmarks/service/permission/test/BookmarksEntryPermissionTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.bookmarks.permission;
+package com.liferay.bookmarks.service.permission.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.bookmarks.model.BookmarksEntry;
