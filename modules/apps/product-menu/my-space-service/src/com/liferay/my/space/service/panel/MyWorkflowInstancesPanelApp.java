3d6288cf1f409448ab5da987efa7b8da7480edaa
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-06-18 13:46:32 +0200

    LPS-56341 My Space Service: Rename to the new name

--- modules/apps/product-menu/my-space-service/src/com/liferay/my/space/service/panel/MyWorkflowInstancesPanelApp.java
+++ modules/apps/product-menu/my-space-service/src/com/liferay/my/space/service/panel/MyWorkflowInstancesPanelApp.java
@@ -35,7 +35,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	immediate = true,
 	property = {
-		"panel.category.key=" + PanelCategoryKeys.MY,
+		"panel.category.key=" + PanelCategoryKeys.PRODUCTIVITY_CENTER,
 		"service.ranking:Integer=400"
 	},
 	service = PanelApp.class
