2a95cdab0625f6a0a1b313b209c3e931b73dab0d
peterfellwock <peter.fellwock@liferay.com>
2015-06-18 12:37:17 -0700

    LPS-55355 ConfigurationBeanSetting should not blindly cast to String[], class cast exception

--- portal-service/src/com/liferay/portal/kernel/settings/ConfigurationBeanSettings.java
+++ portal-service/src/com/liferay/portal/kernel/settings/ConfigurationBeanSettings.java
@@ -15,6 +15,7 @@
 package com.liferay.portal.kernel.settings;
 
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.GetterUtil;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -66,8 +67,9 @@ public class ConfigurationBeanSettings extends BaseSettings
 		if (object == null) {
 			return null;
 		}
-
-		return (String[])object;
+		
+		return GetterUtil.getStringValues(object);
+		
 	}
 
 	private Object _getProperty(String key) {
