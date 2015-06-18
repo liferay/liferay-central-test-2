56812e5a2c54ddeb79cc6b94ed8ff74172a7465d
tinatian <tina.tian@liferay.com>
2015-06-18 10:35:15 +0800

    LPS-56476 Move memory portal cache manager to bundle

---
+++ modules/portal/portal-cache-memory/src/com/liferay/portal/cache/memory/internal/SingleVMMemoryPortalCacheManager.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.cache.memory.internal;
+
+import com.liferay.portal.kernel.cache.PortalCacheManager;
+import com.liferay.portal.kernel.cache.PortalCacheManagerNames;
+import com.liferay.portal.kernel.cache.PortalCacheManagerTypes;
+import com.liferay.portal.kernel.cache.configurator.PortalCacheConfiguratorSettings;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.Props;
+
+import java.io.Serializable;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Tina Tian
+ */
+@Component(
+	immediate = true,
+	property = {
+		PortalCacheManager.PORTAL_CACHE_MANAGER_NAME + "=" + PortalCacheManagerNames.SINGLE_VM,
+		PortalCacheManager.PORTAL_CACHE_MANAGER_TYPE + "=" + PortalCacheManagerTypes.MEMORY
+	},
+	service = PortalCacheManager.class
+)
+public class SingleVMMemoryPortalCacheManager<K extends Serializable, V>
+	extends MemoryPortalCacheManager<K, V> {
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		setName(PortalCacheManagerNames.SINGLE_VM);
+
+		initialize();
+
+		if (_log.isDebugEnabled()) {
+			_log.debug(
+				"Activated " + PortalCacheManagerNames.SINGLE_VM + " " +
+					PortalCacheManagerTypes.MEMORY);
+		}
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		destroy();
+	}
+
+	@Reference(unbind = "-")
+	protected void setProps(Props props) {
+		this.props = props;
+	}
+
+	protected void unsetPortalCacheConfiguratorSettings(
+		PortalCacheConfiguratorSettings portalCacheConfiguratorSettings) {
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		SingleVMMemoryPortalCacheManager.class);
+
+}
