mjbradford89/liferay-portal

LPS-55166 - refactor PortalWebResourceUtil so Aggregate and CSS filters can retrieve correct servlet context

portal-impl/src/com/liferay/portal/servlet/filters/dynamiccss/DynamicCSSFilter.java
@@ -20,7 +20,6 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.servlet.BufferCacheServletResponse;
 import com.liferay.portal.kernel.servlet.HttpHeaders;
-import com.liferay.portal.kernel.servlet.PortalWebResourceConstants;
 import com.liferay.portal.kernel.servlet.PortalWebResourcesUtil;
 import com.liferay.portal.kernel.servlet.ServletResponseUtil;
 import com.liferay.portal.kernel.util.ContentTypes;
@@ -114,16 +113,19 @@ public class DynamicCSSFilter extends IgnoreModuleRequestFilter {
 		boolean bundleResource = false;
 
 		if (resourceURL == null) {
-			resourceURL = PortalWebResourcesUtil.getServletContextResource(
-				requestPath);
+			ServletContext portalWebContext =
+				PortalWebResourcesUtil.getServletContextByResource(requestPath);
+
+			if (portalWebContext != null) {
+				resourceURL = PortalWebResourcesUtil.getResource(
+					portalWebContext, requestPath);
+			}
 
 			if (resourceURL == null) {
 				return null;
 			}
 
-			currentThreadServletContext =
-				PortalWebResourcesUtil.getServletContext(
-					PortalWebResourceConstants.RESOURCE_TYPE_CSS);
+			currentThreadServletContext = portalWebContext;
 
 			bundleResource = true;
 		}

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portal/servlet/filters/dynamiccss/DynamicCSSFilter.java
@@ -20,7 +20,6 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.servlet.BufferCacheServletResponse;
 import com.liferay.portal.kernel.servlet.HttpHeaders;
-import com.liferay.portal.kernel.servlet.PortalWebResourceConstants;
 import com.liferay.portal.kernel.servlet.PortalWebResourcesUtil;
 import com.liferay.portal.kernel.servlet.ServletResponseUtil;
 import com.liferay.portal.kernel.util.ContentTypes;
@@ -105,16 +104,20 @@ public class DynamicCSSFilter extends IgnoreModuleRequestFilter {
 		URL resourceURL = _servletContext.getResource(requestPath);
 
 		if (resourceURL == null) {
-			resourceURL = PortalWebResourcesUtil.getServletContextResource(
-				requestPath);
+			ServletContext portalWebContext =
+				PortalWebResourcesUtil.getServletContextByResource(requestPath);
+
+			if (portalWebContext != null) {
+				resourceURL = PortalWebResourcesUtil.getResource(
+					portalWebContext, requestPath);
+			}
 
 			if (resourceURL == null) {
 				return null;
 			}
 
 			bundleResource = true;
-			servletContext = PortalWebResourcesUtil.getServletContext(
-				PortalWebResourceConstants.RESOURCE_TYPE_CSS);
+			servletContext = portalWebContext;
 		}
 
 		String cacheCommonFileName = getCacheFileName(request);