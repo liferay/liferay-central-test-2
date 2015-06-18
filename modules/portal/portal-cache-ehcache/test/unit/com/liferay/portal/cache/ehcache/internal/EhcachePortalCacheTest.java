dd5d8f1b80f45b23e40651b9fb71f7b87f905856
tinatian <tina.tian@liferay.com>
2015-06-18 11:50:16 +0800

    LPS-56476 Fix test

--- modules/portal/portal-cache-ehcache/test/unit/com/liferay/portal/cache/ehcache/internal/EhcachePortalCacheTest.java
+++ modules/portal/portal-cache-ehcache/test/unit/com/liferay/portal/cache/ehcache/internal/EhcachePortalCacheTest.java
@@ -19,6 +19,7 @@ import com.liferay.portal.cache.test.TestCacheReplicator;
 import com.liferay.portal.cache.test.TestPortalCacheManager;
 import com.liferay.portal.kernel.cache.CacheListenerScope;
 import com.liferay.portal.kernel.cache.PortalCacheHelperUtil;
+import com.liferay.portal.kernel.cache.PortalCacheManager;
 import com.liferay.portal.kernel.test.rule.CodeCoverageAssertor;
 
 import java.util.List;
@@ -71,10 +72,12 @@ public class EhcachePortalCacheTest {
 
 		Cache cache = _cacheManager.getCache(_PORTAL_CACHE_NAME);
 
-		_ehcachePortalCache = new EhcachePortalCache<String, String>(
-			new TestPortalCacheManager<String, String>(
-				_PORTAL_CACHE_MANAGER_NAME),
-			cache);
+		PortalCacheManager<String, String> portalCacheManager =
+			TestPortalCacheManager.createTestPortalCacheManager(
+				_PORTAL_CACHE_NAME);
+
+		_ehcachePortalCache = new EhcachePortalCache<>(
+			portalCacheManager, cache);
 
 		_ehcachePortalCache.put(_KEY_1, _VALUE_1);
 
