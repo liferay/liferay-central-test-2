f33d52bb342468781705f7983ec3223ce84dda01
Michael Han <michael.han@liferay.com>
2015-06-19 08:15:03 -0500

    LPS-55505 move to module

--- modules/portal/portal-scripting-ruby/src/com/liferay/portal/scripting/ruby/internal/RubyExecutor.java
+++ modules/portal/portal-scripting-ruby/src/com/liferay/portal/scripting/ruby/internal/RubyExecutor.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.scripting.ruby;
+package com.liferay.portal.scripting.ruby.internal;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
