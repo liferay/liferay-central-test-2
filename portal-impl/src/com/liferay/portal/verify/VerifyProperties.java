7bcbc689713d0785dcf2e9a40a731f8d118f9dd3
Zoltán Takács <zoltan.takacs@liferay.com>
2015-06-19 11:32:10 +0200

    LPS-52824 Log that "editor.ckeditor.version" is obsolete

--- portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
@@ -1248,7 +1248,7 @@ public class VerifyProperties extends VerifyProcess {
 		"dynamic.data.mapping.structure.private.field.repeatable[" +
 			"_fieldsDisplay]",
 		"dynamic.data.mapping.template.language.types",
-		"editor.inline.editing.enabled",
+		"editor.ckeditor.version", "editor.inline.editing.enabled",
 		"editor.wysiwyg.portal-web.docroot.html.portlet.asset_publisher." +
 			"configuration.jsp",
 		"editor.wysiwyg.portal-web.docroot.html.portlet.blogs.configuration." +
