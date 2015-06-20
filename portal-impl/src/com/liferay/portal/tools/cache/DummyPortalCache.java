85e501aef57e411146af70fa373e45ff61584f0d
shuyangzhou <shuyang.zhou@liferay.com>
2015-06-20 14:52:13 -0700

    LPS-56476 Dummy PortalCache impl

---
+++ portal-impl/src/com/liferay/portal/tools/cache/DummyPortalCache.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.tools.cache;
+
+import com.liferay.portal.kernel.cache.AbstractPortalCache;
+import com.liferay.portal.kernel.cache.PortalCacheManager;
+
+import java.io.Serializable;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Shuyang Zhou
+ */
+public class DummyPortalCache<K extends Serializable, V>
+	extends AbstractPortalCache<K, V> {
+
+	public DummyPortalCache(
+		PortalCacheManager<K, V> portalCacheManager, String portalCacheName) {
+
+		super(portalCacheManager);
+
+		_portalCacheName = portalCacheName;
+	}
+
+	@Override
+	public List<K> getKeys() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public String getName() {
+		return _portalCacheName;
+	}
+
+	@Override
+	public void removeAll() {
+	}
+
+	@Override
+	protected V doGet(K key) {
+		return null;
+	}
+
+	@Override
+	protected void doPut(K key, V value, int timeToLive) {
+	}
+
+	@Override
+	protected V doPutIfAbsent(K key, V value, int timeToLive) {
+		return null;
+	}
+
+	@Override
+	protected void doRemove(K key) {
+	}
+
+	@Override
+	protected boolean doRemove(K key, V value) {
+		return false;
+	}
+
+	@Override
+	protected V doReplace(K key, V value, int timeToLive) {
+		return null;
+	}
+
+	@Override
+	protected boolean doReplace(K key, V oldValue, V newValue, int timeToLive) {
+		return true;
+	}
+
+	private final String _portalCacheName;
+
+}
---
+++ portal-impl/src/com/liferay/portal/tools/cache/DummyPortalCacheManager.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.tools.cache;
+
+import com.liferay.portal.kernel.cache.AbstractPortalCacheManager;
+import com.liferay.portal.kernel.cache.PortalCache;
+import com.liferay.portal.kernel.cache.PortalCacheManagerTypes;
+import com.liferay.portal.kernel.cache.configuration.PortalCacheConfiguration;
+import com.liferay.portal.kernel.cache.configuration.PortalCacheManagerConfiguration;
+
+import java.io.Serializable;
+
+import java.net.URL;
+
+/**
+ * @author Shuyang Zhou
+ */
+public class DummyPortalCacheManager<K extends Serializable, V>
+	extends AbstractPortalCacheManager<K, V> {
+
+	public DummyPortalCacheManager(String portalCacheManagerName) {
+		setName(portalCacheManagerName);
+
+		initialize();
+	}
+
+	@Override
+	public void reconfigureCaches(URL configurationURL) {
+	}
+
+	@Override
+	protected PortalCache<K, V> createPortalCache(
+		PortalCacheConfiguration portalCacheConfiguration) {
+
+		return new DummyPortalCache<>(
+			this, portalCacheConfiguration.getPortalCacheName());
+	}
+
+	@Override
+	protected void doClearAll() {
+	}
+
+	@Override
+	protected void doDestroy() {
+	}
+
+	@Override
+	protected void doRemoveCache(String portalCacheName) {
+	}
+
+	@Override
+	protected PortalCacheManagerConfiguration
+		getPortalCacheManagerConfiguration() {
+
+		return new PortalCacheManagerConfiguration(
+			null,
+			new PortalCacheConfiguration(
+				PortalCacheConfiguration.DEFAULT_PORTAL_CACHE_NAME, null, null),
+			null);
+	}
+
+	@Override
+	protected String getType() {
+		return PortalCacheManagerTypes.TEST;
+	}
+
+	@Override
+	protected void initPortalCacheManager() {
+	}
+
+}
