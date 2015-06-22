3151a5a92755d6fa236c984421eefa7a8eccdfef
Matthew Tambara <matthew.tambara@liferay.com>
2015-06-22 10:05:49 -0700

    LPS-56567 Add SynchronousDestinationTestRule

--- modules/apps/document-library/document-library-test/test/integration/com/liferay/document/library/service/test/DLFileEntryTypeServiceTest.java
+++ modules/apps/document-library/document-library-test/test/integration/com/liferay/document/library/service/test/DLFileEntryTypeServiceTest.java
@@ -22,6 +22,8 @@ import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
@@ -70,12 +72,15 @@ import org.junit.runner.RunWith;
  * @author Alexander Chow
  */
 @RunWith(Arquillian.class)
+@Sync
 public class DLFileEntryTypeServiceTest {
 
 	@ClassRule
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
-		new LiferayIntegrationTestRule();
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			SynchronousDestinationTestRule.INSTANCE);
 
 	@Before
 	public void setUp() throws Exception {
