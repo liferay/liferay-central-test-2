1daa8de8c5a8cf68f0b4f4b39c1c6d9270bcde5c
Eduardo Garcia <eduardo.garcia@liferay.com>
2015-06-22 18:27:45 +0200

    LPS-56383 Prevent NPE in scheduler
    The portlet must be available in the Portlet table. Applying same solution as in com.liferay.asset.publisher.layout.prototype.action.AddLayoutPrototypeAction.

--- modules/apps/journal/journal-web/src/com/liferay/journal/web/messaging/CheckArticleMessageListener.java
+++ modules/apps/journal/journal-web/src/com/liferay/journal/web/messaging/CheckArticleMessageListener.java
@@ -17,12 +17,14 @@ package com.liferay.journal.web.messaging;
 import com.liferay.journal.service.JournalArticleLocalServiceUtil;
 import com.liferay.journal.web.configuration.JournalWebConfigurationValues;
 import com.liferay.journal.web.constants.JournalPortletKeys;
-import com.liferay.journal.web.portlet.JournalPortlet;
 import com.liferay.portal.kernel.messaging.BaseSchedulerEntryMessageListener;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.scheduler.SchedulerEntry;
 import com.liferay.portal.kernel.scheduler.TimeUnit;
 import com.liferay.portal.kernel.scheduler.TriggerType;
+import com.liferay.portal.model.Portlet;
+
+import javax.servlet.ServletContext;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -53,8 +55,14 @@ public class CheckArticleMessageListener
 		JournalArticleLocalServiceUtil.checkArticles();
 	}
 
-	@Reference
-	protected void setJournalPortlet(JournalPortlet journalPortlet) {
+	@Reference(
+		target = "(javax.portlet.name=" + JournalPortletKeys.JOURNAL + ")"
+	)
+	protected void setPortlet(Portlet portlet) {
+	}
+
+	@Reference(target = "(original.bean=*)", unbind = "-")
+	protected void setServletContext(ServletContext servletContext) {
 	}
 
 }
