90bc4cfccbdc9a5c5afad754e44b7289494935e2
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-06-23 15:31:43 +0200

    LPS-56451 Javadoc for MVCActionCommand

--- portal-service/src/com/liferay/portal/kernel/portlet/bridges/mvc/MVCActionCommand.java
+++ portal-service/src/com/liferay/portal/kernel/portlet/bridges/mvc/MVCActionCommand.java
@@ -19,6 +19,68 @@ import javax.portlet.ActionResponse;
 import javax.portlet.PortletException;
 
 /**
+ * Provides an interface to allow the portlet to process a particular action
+ * request. This interface can only be used when the portlet is based on
+ * {@link MVCPortlet}.
+ *
+ * <p>
+ * The action command that will be invoked is determined based on two different
+ * factors:
+ * </p>
+ *
+ * <ul>
+ * <li>
+ * The portlet name that the action url is referring to.
+ * </li>
+ * <li>
+ * The parameter value <code>ActionRequest.ACTION_NAME</code> of the action url.
+ * </li>
+ *
+ * <p>
+ * Implementations of this interface must be OSGi components that are registered
+ * in the OSGi Registry with the following properties:
+ * </p>
+ *
+ * <ul>
+ * <li>
+ * <code>javax.portlet.name</code>: The portlet name associated to this action
+ * command.
+ * </li>
+ * <li>
+ * <code>mvc.command.name</code>: the command name that will match the parameter
+ * value <code>ActionRequest.ACTION_NAME</code>. This name cannot contain any
+ * comma (<code>,</code>).
+ * </li>
+ *
+ * <p>
+ * The method <code>processAction</code> in {@link MVCPortlet}
+ * will search in the OSGi Registry the action command that will match both the
+ * portlet name and the parameter value <code>ActionRequest.ACTION_NAME</code>
+ * with the properties <code>javax.portlet.name</code> and
+ * <code>mvc.command.name</code>.
+ * </p>
+ *
+ * <p>
+ * In general, only one action command will be executed per portlet action url.
+ * However, if the parameter value <code>ActionRequest.ACTION_NAME</code> is a
+ * comma separated list of multiple names, {@link MVCPortlet} will find the
+ * action commands that matches and they will all be invoked sequentially based
+ * on the order they were specified in the parameter value
+ * <code>ActionRequest.ACTION_NAME</code>.
+ * </p>
+ *
+ * <p>
+ * Every time there is more than one action command registered for the same
+ * portlet name and with the same command name, only the one with the they
+ * highest service ranking will be used.
+ * </p>
+ *
+ * <p>
+ * {@link BaseMVCActionCommand} is an abstract class that implements this
+ * interface and can be used by extending it by third party developers to
+ * simplify the use of action commands.
+ * </p>
+ *
  * @author Michael C. Han
  */
 public interface MVCActionCommand extends MVCCommand {
@@ -34,6 +96,15 @@ public interface MVCActionCommand extends MVCCommand {
 
 	};
 
+	/**
+	 * Invoked by {@link MVCPortlet} to allow the portlet to process an action
+	 * request.
+	 *
+	 * @param actionRequest the action request
+	 * @param actionResponse the action response
+	 * @return <code>true</code> if there was an error during the process of the
+	 *         action request; <code>false</code> otherwise.
+	 */
 	public boolean processAction(
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws PortletException;
