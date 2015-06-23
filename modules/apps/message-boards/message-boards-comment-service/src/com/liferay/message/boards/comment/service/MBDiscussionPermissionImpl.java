c99034a565a42433024cdb54810e632f249ad438
Adolfo Pérez <adolfo.perez@liferay.com>
2015-06-23 12:10:00 +0200

    LPS-56481 Rename package

--- modules/apps/message-boards/message-boards-comment-service/src/com/liferay/message/boards/comment/service/MBDiscussionPermissionImpl.java
+++ modules/apps/message-boards/message-boards-comment-service/src/com/liferay/message/boards/comment/service/MBDiscussionPermissionImpl.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.message.boards.comment.impl;
+package com.liferay.message.boards.comment.service;
 
 import com.liferay.portal.kernel.comment.BaseDiscussionPermission;
 import com.liferay.portal.kernel.exception.PortalException;
