a879042f0bf9a3df84036e231ffe705643b9a654
Brian Chan <brian.chan@liferay.com>
2015-06-23 08:36:01 -0700

    LPS-56383 SF

--- modules/apps/journal/journal-service/src/com/liferay/journal/configuration/JournalServiceConfigurationKeys.java
+++ modules/apps/journal/journal-service/src/com/liferay/journal/configuration/JournalServiceConfigurationKeys.java
@@ -21,53 +21,77 @@ public class JournalServiceConfigurationKeys {
 
 	public static final String CHAR_BLACKLIST = "char.blacklist";
 
-	public static final String EMAIL_ARTICLE_ADDED_BODY = "email.article.added.body";
+	public static final String EMAIL_ARTICLE_ADDED_BODY =
+		"email.article.added.body";
 
-	public static final String EMAIL_ARTICLE_ADDED_ENABLED = "email.article.added.enabled";
+	public static final String EMAIL_ARTICLE_ADDED_ENABLED =
+		"email.article.added.enabled";
 
-	public static final String EMAIL_ARTICLE_ADDED_SUBJECT = "email.article.added.subject";
+	public static final String EMAIL_ARTICLE_ADDED_SUBJECT =
+		"email.article.added.subject";
 
-	public static final String EMAIL_ARTICLE_APPROVAL_DENIED_BODY = "email.article.approval.denied.body";
+	public static final String EMAIL_ARTICLE_APPROVAL_DENIED_BODY =
+		"email.article.approval.denied.body";
 
-	public static final String EMAIL_ARTICLE_APPROVAL_DENIED_ENABLED = "email.article.approval.denied.enabled";
+	public static final String EMAIL_ARTICLE_APPROVAL_DENIED_ENABLED =
+		"email.article.approval.denied.enabled";
 
-	public static final String EMAIL_ARTICLE_APPROVAL_DENIED_SUBJECT = "email.article.approval.denied.subject";
+	public static final String EMAIL_ARTICLE_APPROVAL_DENIED_SUBJECT =
+		"email.article.approval.denied.subject";
 
-	public static final String EMAIL_ARTICLE_APPROVAL_GRANTED_BODY = "email.article.approval.granted.body";
+	public static final String EMAIL_ARTICLE_APPROVAL_GRANTED_BODY =
+		"email.article.approval.granted.body";
 
-	public static final String EMAIL_ARTICLE_APPROVAL_GRANTED_ENABLED = "email.article.approval.granted.enabled";
+	public static final String EMAIL_ARTICLE_APPROVAL_GRANTED_ENABLED =
+		"email.article.approval.granted.enabled";
 
-	public static final String EMAIL_ARTICLE_APPROVAL_GRANTED_SUBJECT = "email.article.approval.granted.subject";
+	public static final String EMAIL_ARTICLE_APPROVAL_GRANTED_SUBJECT =
+		"email.article.approval.granted.subject";
 
-	public static final String EMAIL_ARTICLE_APPROVAL_REQUESTED_BODY = "email.article.approval.requested.body";
+	public static final String EMAIL_ARTICLE_APPROVAL_REQUESTED_BODY =
+		"email.article.approval.requested.body";
 
-	public static final String EMAIL_ARTICLE_APPROVAL_REQUESTED_ENABLED = "email.article.approval.requested.enabled";
+	public static final String EMAIL_ARTICLE_APPROVAL_REQUESTED_ENABLED =
+		"email.article.approval.requested.enabled";
 
-	public static final String EMAIL_ARTICLE_APPROVAL_REQUESTED_SUBJECT = "email.article.approval.requested.subject";
+	public static final String EMAIL_ARTICLE_APPROVAL_REQUESTED_SUBJECT =
+		"email.article.approval.requested.subject";
 
-	public static final String EMAIL_ARTICLE_MOVED_FROM_FOLDER_BODY = "email.article.moved.from.folder.body";
+	public static final String EMAIL_ARTICLE_MOVED_FROM_FOLDER_BODY =
+		"email.article.moved.from.folder.body";
 
-	public static final String EMAIL_ARTICLE_MOVED_FROM_FOLDER_ENABLED = "email.article.moved.from.folder.enabled";
+	public static final String EMAIL_ARTICLE_MOVED_FROM_FOLDER_ENABLED =
+		"email.article.moved.from.folder.enabled";
 
-	public static final String EMAIL_ARTICLE_MOVED_FROM_FOLDER_SUBJECT = "email.article.moved.from.folder.subject";
+	public static final String EMAIL_ARTICLE_MOVED_FROM_FOLDER_SUBJECT =
+		"email.article.moved.from.folder.subject";
 
-	public static final String EMAIL_ARTICLE_MOVED_TO_FOLDER_BODY = "email.article.moved.to.folder.body";
+	public static final String EMAIL_ARTICLE_MOVED_TO_FOLDER_BODY =
+		"email.article.moved.to.folder.body";
 
-	public static final String EMAIL_ARTICLE_MOVED_TO_FOLDER_ENABLED = "email.article.moved.to.folder.enabled";
+	public static final String EMAIL_ARTICLE_MOVED_TO_FOLDER_ENABLED =
+		"email.article.moved.to.folder.enabled";
 
-	public static final String EMAIL_ARTICLE_MOVED_TO_FOLDER_SUBJECT = "email.article.moved.to.folder.subject";
+	public static final String EMAIL_ARTICLE_MOVED_TO_FOLDER_SUBJECT =
+		"email.article.moved.to.folder.subject";
 
-	public static final String EMAIL_ARTICLE_REVIEW_BODY = "email.article.review.body";
+	public static final String EMAIL_ARTICLE_REVIEW_BODY =
+		"email.article.review.body";
 
-	public static final String EMAIL_ARTICLE_REVIEW_ENABLED = "email.article.review.enabled";
+	public static final String EMAIL_ARTICLE_REVIEW_ENABLED =
+		"email.article.review.enabled";
 
-	public static final String EMAIL_ARTICLE_REVIEW_SUBJECT = "email.article.review.subject";
+	public static final String EMAIL_ARTICLE_REVIEW_SUBJECT =
+		"email.article.review.subject";
 
-	public static final String EMAIL_ARTICLE_UPDATED_BODY = "email.article.updated.body";
+	public static final String EMAIL_ARTICLE_UPDATED_BODY =
+		"email.article.updated.body";
 
-	public static final String EMAIL_ARTICLE_UPDATED_ENABLED = "email.article.updated.enabled";
+	public static final String EMAIL_ARTICLE_UPDATED_ENABLED =
+		"email.article.updated.enabled";
 
-	public static final String EMAIL_ARTICLE_UPDATED_SUBJECT = "email.article.updated.subject";
+	public static final String EMAIL_ARTICLE_UPDATED_SUBJECT =
+		"email.article.updated.subject";
 
 	public static final String EMAIL_FROM_ADDRESS = "email.from.address";
 
@@ -75,35 +99,49 @@ public class JournalServiceConfigurationKeys {
 
 	public static final String ERROR_TEMPLATE = "error.template";
 
-	public static final String JOURNAL_ARTICLE_CHECK_INTERVAL = "journal.article.check.interval";
+	public static final String JOURNAL_ARTICLE_CHECK_INTERVAL =
+		"journal.article.check.interval";
 
-	public static final String JOURNAL_ARTICLE_COMMENTS_ENABLED = "journal.article.comments.enabled";
+	public static final String JOURNAL_ARTICLE_COMMENTS_ENABLED =
+		"journal.article.comments.enabled";
 
-	public static final String JOURNAL_ARTICLE_CUSTOM_TOKEN_VALUE = "journal.article.custom.token.value";
+	public static final String JOURNAL_ARTICLE_CUSTOM_TOKEN_VALUE =
+		"journal.article.custom.token.value";
 
-	public static final String JOURNAL_ARTICLE_CUSTOM_TOKENS = "journal.article.custom.tokens";
+	public static final String JOURNAL_ARTICLE_CUSTOM_TOKENS =
+		"journal.article.custom.tokens";
 
-	public static final String JOURNAL_ARTICLE_DATABASE_KEYWORD_SEARCH_CONTENT = "journal.article.database.keyword.search.content";
+	public static final String JOURNAL_ARTICLE_DATABASE_KEYWORD_SEARCH_CONTENT =
+		"journal.article.database.keyword.search.content";
 
-	public static final String JOURNAL_ARTICLE_EXPIRE_ALL_VERSIONS = "journal.article.expire.all.versions";
+	public static final String JOURNAL_ARTICLE_EXPIRE_ALL_VERSIONS =
+		"journal.article.expire.all.versions";
 
-	public static final String JOURNAL_ARTICLE_INDEX_ALL_VERSIONS = "journal.articles.index.all.versions";
+	public static final String JOURNAL_ARTICLE_INDEX_ALL_VERSIONS =
+		"journal.articles.index.all.versions";
 
-	public static final String JOURNAL_ARTICLE_STORAGE_TYPE = "journal.article.storage.type";
+	public static final String JOURNAL_ARTICLE_STORAGE_TYPE =
+		"journal.article.storage.type";
 
-	public static final String JOURNAL_ARTICLE_TOKEN_PAGE_BREAK = "journal.article.token.page.break";
+	public static final String JOURNAL_ARTICLE_TOKEN_PAGE_BREAK =
+		"journal.article.token.page.break";
 
-	public static final String JOURNAL_ARTICLE_VIEW_PERMISSION_CHECK_ENABLED = "journal.article.view.permission.check.enabled";
+	public static final String JOURNAL_ARTICLE_VIEW_PERMISSION_CHECK_ENABLED =
+		"journal.article.view.permission.check.enabled";
 
-	public static final String JOURNAL_FOLDER_ICON_CHECK_COUNT = "journal.folder.icon.check.count";
+	public static final String JOURNAL_FOLDER_ICON_CHECK_COUNT =
+		"journal.folder.icon.check.count";
 
 	public static final String LAR_CREATION_STRATEGY = "lar.creation.strategy";
 
-	public static final String SYNC_CONTENT_SEARCH_ON_STARTUP = "sync.content.search.on.startup";
+	public static final String SYNC_CONTENT_SEARCH_ON_STARTUP =
+		"sync.content.search.on.startup";
 
-	public static final String TERMS_OF_USE_JOURNAL_ARTICLE_GROUP_ID = "terms.of.use.journal.article.group.id";
+	public static final String TERMS_OF_USE_JOURNAL_ARTICLE_GROUP_ID =
+		"terms.of.use.journal.article.group.id";
 
-	public static final String TERMS_OF_USE_JOURNAL_ARTICLE_ID = "terms.of.use.journal.article.id";
+	public static final String TERMS_OF_USE_JOURNAL_ARTICLE_ID =
+		"terms.of.use.journal.article.id";
 
 	public static final String TRANSFORMER_LISTENER = "transformer.listener";
 
