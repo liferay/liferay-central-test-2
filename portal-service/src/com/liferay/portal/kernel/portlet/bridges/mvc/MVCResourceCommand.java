aaed83de12ecda81fbf549d92a91131b0a53a195
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-06-23 15:48:16 +0200

    LPS-56451 Javadoc for ResourceCommand

--- portal-service/src/com/liferay/portal/kernel/portlet/bridges/mvc/MVCResourceCommand.java
+++ portal-service/src/com/liferay/portal/kernel/portlet/bridges/mvc/MVCResourceCommand.java
@@ -19,21 +19,21 @@ import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
 
 /**
- * Provides an interface to allow the portlet to process a particular action
- * request. This interface can only be used when the portlet is based on
+ * Provides an interface to allow the portlet to serve a resource. This
+ * interface can only be used when the portlet is based on
  * {@link MVCPortlet}.
  *
  * <p>
- * The action command that will be invoked is determined based on two different
- * factors:
+ * The resource command that will be invoked is determined based on two
+ * different factors:
  * </p>
  *
  * <ul>
  * <li>
- * The portlet name that the action url is referring to.
+ * The portlet name that the resource url is referring to.
  * </li>
  * <li>
- * The parameter value <code>ActionRequest.ACTION_NAME</code> of the action url.
+ * The <code>ResourceID</code> of the resource request.
  * </li>
  *
  * <p>
@@ -43,42 +43,40 @@ import javax.portlet.ResourceResponse;
  *
  * <ul>
  * <li>
- * <code>javax.portlet.name</code>: The portlet name associated to this action
+ * <code>javax.portlet.name</code>: The portlet name associated to this resource
  * command.
  * </li>
  * <li>
- * <code>mvc.command.name</code>: the command name that will match the parameter
- * value <code>ActionRequest.ACTION_NAME</code>. This name cannot contain any
+ * <code>mvc.command.name</code>: the command name that will match the
+ * ResourceID of the resource request. This name cannot contain any
  * comma (<code>,</code>).
  * </li>
  *
  * <p>
- * The method <code>processAction</code> in {@link MVCPortlet}
- * will search in the OSGi Registry the action command that will match both the
- * portlet name and the parameter value <code>ActionRequest.ACTION_NAME</code>
- * with the properties <code>javax.portlet.name</code> and
- * <code>mvc.command.name</code>.
+ * The method <code>serveResource</code> in {@link MVCPortlet}
+ * will search in the OSGi Registry the resource command that will match both
+ * the portlet name and the ResourceID of the resource request with the
+ * properties <code>javax.portlet.name</code> and <code>mvc.command.name</code>.
  * </p>
  *
  * <p>
- * In general, only one action command will be executed per portlet action url.
- * However, if the parameter value <code>ActionRequest.ACTION_NAME</code> is a
- * comma separated list of multiple names, {@link MVCPortlet} will find the
- * action commands that matches and they will all be invoked sequentially based
- * on the order they were specified in the parameter value
- * <code>ActionRequest.ACTION_NAME</code>.
+ * In general, only one resource command will be executed per portlet resource
+ * url. However, if the ResourceID of the resource request is a comma separated
+ * list of multiple names, {@link MVCPortlet} will find the resource commands
+ * that matches and they will all be invoked sequentially based
+ * on the order they were specified in the resourceID of the resource request.
  * </p>
  *
  * <p>
- * Every time there is more than one action command registered for the same
+ * Every time there is more than one resource command registered for the same
  * portlet name and with the same command name, only the one with the they
  * highest service ranking will be used.
  * </p>
  *
  * <p>
- * {@link BaseMVCActionCommand} is an abstract class that implements this
+ * {@link BaseMVCResourceCommand} is an abstract class that implements this
  * interface and can be used by extending it by third party developers to
- * simplify the use of action commands.
+ * simplify the use of resource commands.
  * </p>
  *
  * @author Sergio González
@@ -97,6 +95,14 @@ public interface MVCResourceCommand extends MVCCommand {
 
 	};
 
+	/**
+	 * Invoked by {@link MVCPortlet} to allow the portlet to serve a resource.
+	 *
+	 * @param resourceRequest the resource request
+	 * @param resourceResponse the resource response
+	 * @return <code>true</code> if there was an error during serving the
+	 *         resource; <code>false</code> otherwise.
+	 */
 	public boolean serveResource(
 			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
 		throws PortletException;
