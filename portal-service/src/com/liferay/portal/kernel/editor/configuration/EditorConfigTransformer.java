6164f76620491230b3aa6cb437391390ea390353
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-06-23 12:56:25 +0200

    LPS-56451 Javadoc for EditorConfigTransformer

--- portal-service/src/com/liferay/portal/kernel/editor/configuration/EditorConfigTransformer.java
+++ portal-service/src/com/liferay/portal/kernel/editor/configuration/EditorConfigTransformer.java
@@ -21,10 +21,52 @@ import com.liferay.portal.theme.ThemeDisplay;
 import java.util.Map;
 
 /**
+ * Provides an interface that is responsible for adapting the high level options
+ * provided by {@link EditorOptionsContributor} to specific configuration JSON
+ * object (as used by {@link EditorConfigContributor}).
+ *
+ * <p>
+ * In general, each editor should create its own Editor Config Transformer
+ * because the configuration JSON object that will need to be created is usually
+ * different based on the editor.
+ * </p>
+ *
+ * <p>
+ * Implementations of this class must be OSGi components that are registered in
+ * the OSGi Registry.
+ * </p>
+ *
+ * <p>
+ * The Editor Config Transformers will be targeted to specific editors based on
+ * the <code>editor.name</code> OSGi property.
+ * </p>
+ *
+ * <p>
+ * In case there's more than one editor config transformer for a particular
+ * editor, the one with the higher service rank will be used.
+ * </p>
+ *
  * @author Sergio González
  */
 public interface EditorConfigTransformer {
 
+	/**
+	 * Transforms the editor options in configuration that the editor can handle
+	 * by populating the configuration JSON object.
+	 *
+	 * @param editorOptions the EditorOptions object containing the
+	 *        options set by previous {@link EditorOptionsContributor}
+	 *        modules
+	 * @param inputEditorTaglibAttributes the attributes specified to the input
+	 *        taglib tag that renders the editor
+	 * @param themeDisplay the theme display
+	 * @param liferayPortletResponse the Liferay portlet response (optionally
+	 *        <code>null</code>). Only use the response to generate portlet
+	 *        URLs.
+	 * @param configJSONObject the JSON object containing the entire
+	 *        configuration set by previous {@link EditorConfigContributor}
+	 *        modules
+	 */
 	public void transform(
 		EditorOptions editorOptions,
 		Map<String, Object> inputEditorTaglibAttributes,
