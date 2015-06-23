146caded4fc222b2eeabda6f1106413467fe505d
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-06-23 12:43:37 +0200

    LPS-56451 Javadoc for EditorOptionsContributor

--- portal-service/src/com/liferay/portal/kernel/editor/configuration/EditorOptionsContributor.java
+++ portal-service/src/com/liferay/portal/kernel/editor/configuration/EditorOptionsContributor.java
@@ -20,8 +20,22 @@ import com.liferay.portal.theme.ThemeDisplay;
 import java.util.Map;
 
 /**
- * Provides an interface responsible for setting the configuration of the
- * editor. Editor options can be set using {@link EditorOptionsContributor}.
+ * Provides an interface responsible for setting the options of the
+ * editor. Editor configuration can be set using
+ * {@link EditorConfigContributor}.
+ *
+ * <p>
+ * This interface is intended to easily configure options at a higher level than
+ * using {@link EditorConfigContributor}. The former requires knowledge about
+ * the editor internals, while the latter is more versatile in terms of
+ * customization.
+ * </p>
+ *
+ * <p>
+ * The options specified through this interface are not tight to any particular
+ * editor implementation. These high level options will be transformed to a low
+ * level configuration object by {@link EditorConfigTransformer}.
+ * </p>
  *
  * <p>
  * Implementations of this class must be OSGi components that are registered in
@@ -29,34 +43,33 @@ import java.util.Map;
  * </p>
  *
  * <p>
- * The configuration can be targeted for specific editors, based on three
- * different criteria: portlet name, editor config key, and editor name.
+ * The options can be targeted for specific editors, based on three different
+ * criteria: portlet name, editor config key, and editor name.
  * These criteria can be defined as OSGi properties with the following names:
  * </p>
  *
  * <ul>
  * <li>
  * <code>javax.portlet.name</code>: The portlet name. If specified, the
- * configuration populated in the JSON object is applied to every editor used
- * in that portlet.
+ * options are applied to every editor used in that portlet.
  * </li>
  * <li>
  * <code>editor.config.key</code>: The key used to identify the editor (the
  * <code>input-editor</code> taglib tag's <code>configKey</code> attribute
- * value). If specified, the configuration populated in the JSON object is
- * applied to every editor with the specified <code>configKey</code>.
+ * value). If specified, the options are applied to every editor with the
+ * specified <code>configKey</code>.
  * </li>
  * <li>
  * <code>editor.name</code>: The name of the editor (the
  * <code>input-editor</code> taglib tag's <code>editorName</code> attribute
  * value: <code>ckeditor</code>, <code>ckeditor_bbcode</code>,
- * <code>alloyeditor</code>, etc.). If specified, the configuration populated in
- * the JSON object is applied to every editor with that name.
+ * <code>alloyeditor</code>, etc.). If specified, the options are applied to
+ * every editor with that name.
  * </li>
  * </ul>
  *
  * <p>
- * In case there's more than one configuration, they're prioritized by the
+ * In case there's more than one options contributor, they're prioritized by the
  * following criteria combinations (the first combination getting the highest
  * priority):
  * </p>
@@ -89,13 +102,34 @@ import java.util.Map;
  * </ol>
  *
  * <p>
- * If there are multiple configurations having the same criteria elements,
+ * If there are multiple contributors having the same criteria elements,
  * prioritization between them is based on service rank.
  * </p>
  *
  * @author Sergio González
- */public interface EditorOptionsContributor {
+ */
+public interface EditorOptionsContributor {
 
+	/**
+	 * Updates the original EditorOptions object to some new options.
+	 * It can even update or delete the original options, or any other
+	 * options introduced by any other {@link EditorOptionsContributor}.
+	 *
+	 * <p>
+	 * The editor options object contains the options that will be transformed
+	 * by the editor config transformer to a Configuration JSON object.
+	 * </p>
+	 *
+	 * @param editorOptions the original EditorOptions object containing the
+	 *        options set by previous {@link EditorOptionsContributor}
+	 *        modules
+	 * @param inputEditorTaglibAttributes the attributes specified to the input
+	 *        taglib tag that renders the editor
+	 * @param themeDisplay the theme display
+	 * @param liferayPortletResponse the Liferay portlet response (optionally
+	 *        <code>null</code>). Only use the response to generate portlet
+	 *        URLs.
+	 */
 	public void populateEditorOptions(
 		EditorOptions editorOptions,
 		Map<String, Object> inputEditorTaglibAttributes,
