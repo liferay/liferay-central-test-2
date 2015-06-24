9b53adc36e82f1044c270b586d3749ed0db8d869
tomwang2011 <tong.wang@liferay.com>
2015-06-24 13:46:28 -0700

    LPS-56280 SearchResultUtilDLFileEntryTest

--- portal-impl/test/unit/com/liferay/portal/kernel/search/SearchResultUtilDLFileEntryTest.java
+++ portal-impl/test/unit/com/liferay/portal/kernel/search/SearchResultUtilDLFileEntryTest.java
@@ -15,7 +15,10 @@
 package com.liferay.portal.kernel.search;
 
 import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.test.CaptureHandler;
+import com.liferay.portal.kernel.test.JDKLoggerTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.search.test.BaseSearchResultUtilTestCase;
 import com.liferay.portal.search.test.SearchTestUtil;
 import com.liferay.portlet.asset.AssetRendererFactoryRegistryUtil;
@@ -28,6 +31,8 @@ import java.lang.reflect.Method;
 
 import java.util.List;
 import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
 
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletResponse;
@@ -288,36 +293,54 @@ public class SearchResultUtilDLFileEntryTest
 
 		document.add(new Field(Field.SNIPPET, snippet));
 
-		SearchResult searchResult = assertOneSearchResult(document);
+		try (CaptureHandler captureHandler =
+				JDKLoggerTestUtil.configureJDKLogger(
+					SearchResultUtil.class.getName(), Level.WARNING)) {
 
-		Assert.assertEquals(
-			SearchTestUtil.ATTACHMENT_OWNER_CLASS_NAME,
-			searchResult.getClassName());
-		Assert.assertEquals(
-			SearchTestUtil.ATTACHMENT_OWNER_CLASS_PK,
-			searchResult.getClassPK());
-		Assert.assertNull(searchResult.getSummary());
+			SearchResult searchResult = assertOneSearchResult(document);
 
-		verifyStatic();
+			List<LogRecord> logRecords = captureHandler.getLogRecords();
 
-		IndexerRegistryUtil.getIndexer(_DL_FILE_ENTRY_CLASS_NAME);
+			Assert.assertEquals(1, logRecords.size());
 
-		Mockito.verify(
-			indexer
-		).getSummary(
-			document, snippet, null, null
-		);
+			LogRecord logRecord = logRecords.get(0);
 
-		assertEmptyFileEntryRelatedSearchResults(searchResult);
+			long entryClassPK = GetterUtil.getLong(
+				document.get(Field.ENTRY_CLASS_PK));
 
-		Mockito.verify(
-			dlAppLocalService
-		).getFileEntry(
-			SearchTestUtil.ENTRY_CLASS_PK
-		);
+			Assert.assertEquals(
+				"Search index is stale and contains entry {" + entryClassPK +
+					"}", logRecord.getMessage());
 
-		assertEmptyCommentRelatedSearchResults(searchResult);
-		assertEmptyVersions(searchResult);
+			Assert.assertEquals(
+				SearchTestUtil.ATTACHMENT_OWNER_CLASS_NAME,
+				searchResult.getClassName());
+			Assert.assertEquals(
+				SearchTestUtil.ATTACHMENT_OWNER_CLASS_PK,
+				searchResult.getClassPK());
+			Assert.assertNull(searchResult.getSummary());
+
+			verifyStatic();
+
+			IndexerRegistryUtil.getIndexer(_DL_FILE_ENTRY_CLASS_NAME);
+
+			Mockito.verify(
+				indexer
+			).getSummary(
+				document, snippet, null, null
+			);
+
+			assertEmptyFileEntryRelatedSearchResults(searchResult);
+
+			Mockito.verify(
+				dlAppLocalService
+			).getFileEntry(
+				SearchTestUtil.ENTRY_CLASS_PK
+			);
+
+			assertEmptyCommentRelatedSearchResults(searchResult);
+			assertEmptyVersions(searchResult);
+		}
 	}
 
 	private static final String _DL_FILE_ENTRY_CLASS_NAME =
