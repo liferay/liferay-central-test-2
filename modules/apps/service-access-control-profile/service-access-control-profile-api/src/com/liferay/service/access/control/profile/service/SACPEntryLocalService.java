mikakoivisto/liferay-portal

LPS-55931 Regenerate services

modules/apps/service-access-control-profile/service-access-control-profile-api/src/com/liferay/service/access/control/profile/service/SACPEntryLocalService.java
@@ -60,10 +60,12 @@ public interface SACPEntryLocalService extends BaseLocalService,
 
 	public com.liferay.service.access.control.profile.model.SACPEntry addSACPEntry(
 		long userId, java.lang.String allowedServiceSignatures,
-		java.lang.String name,
+		boolean defaultProfile, java.lang.String name,
 		java.util.Map<java.util.Locale, java.lang.String> titleMap,
 		com.liferay.portal.service.ServiceContext serviceContext)
 		throws PortalException;
+
+	public void checkDefaultProfile(long companyId) throws PortalException;
 
 	/**
 	* Creates a new s a c p entry with the primary key. Does not add the s a c p entry to the database.

modules/apps/service-access-control-profile/service-access-control-profile-api/src/com/liferay/service/access/control/profile/service/SACPEntryLocalServiceUtil.java
@@ -56,13 +56,18 @@ public class SACPEntryLocalServiceUtil {
 
 	public static com.liferay.service.access.control.profile.model.SACPEntry addSACPEntry(
 		long userId, java.lang.String allowedServiceSignatures,
-		java.lang.String name,
+		boolean defaultProfile, java.lang.String name,
 		java.util.Map<java.util.Locale, java.lang.String> titleMap,
 		com.liferay.portal.service.ServiceContext serviceContext)
 		throws com.liferay.portal.kernel.exception.PortalException {
 		return getService()
-				   .addSACPEntry(userId, allowedServiceSignatures, name,
-			titleMap, serviceContext);
+				   .addSACPEntry(userId, allowedServiceSignatures,
+			defaultProfile, name, titleMap, serviceContext);
+	}
+
+	public static void checkDefaultProfile(long companyId)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		getService().checkDefaultProfile(companyId);
 	}
 
 	/**

modules/apps/service-access-control-profile/service-access-control-profile-api/src/com/liferay/service/access/control/profile/service/SACPEntryLocalServiceWrapper.java
@@ -48,12 +48,19 @@ public class SACPEntryLocalServiceWrapper implements SACPEntryLocalService,
 	@Override
 	public com.liferay.service.access.control.profile.model.SACPEntry addSACPEntry(
 		long userId, java.lang.String allowedServiceSignatures,
-		java.lang.String name,
+		boolean defaultProfile, java.lang.String name,
 		java.util.Map<java.util.Locale, java.lang.String> titleMap,
 		com.liferay.portal.service.ServiceContext serviceContext)
 		throws com.liferay.portal.kernel.exception.PortalException {
 		return _sacpEntryLocalService.addSACPEntry(userId,
-			allowedServiceSignatures, name, titleMap, serviceContext);
+			allowedServiceSignatures, defaultProfile, name, titleMap,
+			serviceContext);
+	}
+
+	@Override
+	public void checkDefaultProfile(long companyId)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		_sacpEntryLocalService.checkDefaultProfile(companyId);
 	}
 
 	/**

modules/apps/service-access-control-profile/service-access-control-profile-service/src/META-INF/sql/tables.sql
@@ -7,6 +7,7 @@ create table SACPEntry (
 	createDate DATE null,
 	modifiedDate DATE null,
 	allowedServiceSignatures STRING null,
+	defaultProfile BOOLEAN,
 	name VARCHAR(75) null,
 	title STRING null
 );

modules/apps/service-access-control-profile/service-access-control-profile-service/src/com/liferay/service/access/control/profile/service/persistence/impl/SACPEntryPersistenceImpl.java
@@ -3468,6 +3468,7 @@ public class SACPEntryPersistenceImpl extends BasePersistenceImpl<SACPEntry>
 		sacpEntryImpl.setCreateDate(sacpEntry.getCreateDate());
 		sacpEntryImpl.setModifiedDate(sacpEntry.getModifiedDate());
 		sacpEntryImpl.setAllowedServiceSignatures(sacpEntry.getAllowedServiceSignatures());
+		sacpEntryImpl.setDefaultProfile(sacpEntry.isDefaultProfile());
 		sacpEntryImpl.setName(sacpEntry.getName());
 		sacpEntryImpl.setTitle(sacpEntry.getTitle());
 

liferay/liferay-central-test-2

modules/apps/service-access-control-profile/service-access-control-profile-api/src/com/liferay/service/access/control/profile/service/SACPEntryLocalService.java
@@ -60,10 +60,12 @@ public interface SACPEntryLocalService extends BaseLocalService,
 
 	public com.liferay.service.access.control.profile.model.SACPEntry addSACPEntry(
 		long userId, java.lang.String allowedServiceSignatures,
-		java.lang.String name,
+		boolean defaultSACPEntry, java.lang.String name,
 		java.util.Map<java.util.Locale, java.lang.String> titleMap,
 		com.liferay.portal.service.ServiceContext serviceContext)
 		throws PortalException;
+
+	public void checkDefaultSACPEntry(long companyId) throws PortalException;
 
 	/**
 	* Creates a new s a c p entry with the primary key. Does not add the s a c p entry to the database.

modules/apps/service-access-control-profile/service-access-control-profile-api/src/com/liferay/service/access/control/profile/service/SACPEntryLocalServiceUtil.java
@@ -56,13 +56,18 @@ public class SACPEntryLocalServiceUtil {
 
 	public static com.liferay.service.access.control.profile.model.SACPEntry addSACPEntry(
 		long userId, java.lang.String allowedServiceSignatures,
-		java.lang.String name,
+		boolean defaultSACPEntry, java.lang.String name,
 		java.util.Map<java.util.Locale, java.lang.String> titleMap,
 		com.liferay.portal.service.ServiceContext serviceContext)
 		throws com.liferay.portal.kernel.exception.PortalException {
 		return getService()
-				   .addSACPEntry(userId, allowedServiceSignatures, name,
-			titleMap, serviceContext);
+				   .addSACPEntry(userId, allowedServiceSignatures,
+			defaultSACPEntry, name, titleMap, serviceContext);
+	}
+
+	public static void checkDefaultSACPEntry(long companyId)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		getService().checkDefaultSACPEntry(companyId);
 	}
 
 	/**

modules/apps/service-access-control-profile/service-access-control-profile-api/src/com/liferay/service/access/control/profile/service/SACPEntryLocalServiceWrapper.java
@@ -48,12 +48,19 @@ public class SACPEntryLocalServiceWrapper implements SACPEntryLocalService,
 	@Override
 	public com.liferay.service.access.control.profile.model.SACPEntry addSACPEntry(
 		long userId, java.lang.String allowedServiceSignatures,
-		java.lang.String name,
+		boolean defaultSACPEntry, java.lang.String name,
 		java.util.Map<java.util.Locale, java.lang.String> titleMap,
 		com.liferay.portal.service.ServiceContext serviceContext)
 		throws com.liferay.portal.kernel.exception.PortalException {
 		return _sacpEntryLocalService.addSACPEntry(userId,
-			allowedServiceSignatures, name, titleMap, serviceContext);
+			allowedServiceSignatures, defaultSACPEntry, name, titleMap,
+			serviceContext);
+	}
+
+	@Override
+	public void checkDefaultSACPEntry(long companyId)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		_sacpEntryLocalService.checkDefaultSACPEntry(companyId);
 	}
 
 	/**

modules/apps/service-access-control-profile/service-access-control-profile-service/src/META-INF/sql/tables.sql
@@ -7,6 +7,7 @@ create table SACPEntry (
 	createDate DATE null,
 	modifiedDate DATE null,
 	allowedServiceSignatures STRING null,
+	defaultSACPEntry BOOLEAN,
 	name VARCHAR(75) null,
 	title STRING null
 );

modules/apps/service-access-control-profile/service-access-control-profile-service/src/com/liferay/service/access/control/profile/service/persistence/impl/SACPEntryPersistenceImpl.java
@@ -3468,6 +3468,7 @@ public class SACPEntryPersistenceImpl extends BasePersistenceImpl<SACPEntry>
 		sacpEntryImpl.setCreateDate(sacpEntry.getCreateDate());
 		sacpEntryImpl.setModifiedDate(sacpEntry.getModifiedDate());
 		sacpEntryImpl.setAllowedServiceSignatures(sacpEntry.getAllowedServiceSignatures());
+		sacpEntryImpl.setDefaultSACPEntry(sacpEntry.isDefaultSACPEntry());
 		sacpEntryImpl.setName(sacpEntry.getName());
 		sacpEntryImpl.setTitle(sacpEntry.getTitle());
 