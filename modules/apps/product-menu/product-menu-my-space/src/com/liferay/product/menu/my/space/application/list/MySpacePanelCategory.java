40964fd45ce37c17cb548642f9057d453ee938e7
Brian Chan <brian.chan@liferay.com>
2015-06-24 20:00:07 -0700

    LPS-56341 -service suffix not needed? This is not like bookmarks-service

---
+++ modules/apps/product-menu/product-menu-my-space/src/com/liferay/product/menu/my/space/application/list/MySpacePanelCategory.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.product.menu.my.space.application.list;
+
+import com.liferay.application.list.BasePanelCategory;
+import com.liferay.application.list.PanelCategory;
+import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.portal.kernel.util.StringPool;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(
+	immediate = true,
+	property = {"panel.category.key=" + PanelCategoryKeys.ROOT},
+	service = PanelCategory.class
+)
+public class MySpacePanelCategory extends BasePanelCategory {
+
+	@Override
+	public String getIconCssClass() {
+		return StringPool.BLANK;
+	}
+
+	@Override
+	public String getKey() {
+		return PanelCategoryKeys.MY_SPACE;
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return StringPool.BLANK;
+	}
+
+}
