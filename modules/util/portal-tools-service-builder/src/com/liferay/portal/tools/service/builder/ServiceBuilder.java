4785d4d9129f8c0a1ac92053fa67ac6ca6cc6434
shuyangzhou <shuyang.zhou@liferay.com>
2015-06-26 16:47:39 -0700

    LPS-56674 Inconsistent "Map" type mapping breaks Oracle. Caused by LPS-45869 ce29cc63f48657c9666b89c59eaf838f909937df, when creating table definition, we unconditional translate "Map" to "Text", however on generating XYZModelImpl.TABLE_COLUMNS, we conditionally translate "Map" to "CLOB" or "VARCHAR" base on its size. This inconsistency is causing Oracle to break on "ORA-00932: inconsistent datatypes: expected - got CLOB"LPS-56674 Inconsistent "Map" type mapping breaks Oracle. Caused by LPS-45869 ce29cc63f48657c9666b89c59eaf838f909937df, when creating table definition, we unconditional translate "Map" to "Text", however on generating XYZModelImpl.TABLE_COLUMNS, we conditionally translate "Map" to "CLOB" or "VARCHAR" base on its size. This inconsistency is causing Oracle to break with "ORA-00932: inconsistent datatypes: expected - got CLOB". Since we already released with tables generated unconditionally using "Text", to avoid upgrade issue, we sync the table column type for "Map" to be unconditionally "CLOB" to match table definition.

--- modules/util/portal-tools-service-builder/src/com/liferay/portal/tools/service/builder/ServiceBuilder.java
+++ modules/util/portal-tools-service-builder/src/com/liferay/portal/tools/service/builder/ServiceBuilder.java
@@ -1616,7 +1616,10 @@ public class ServiceBuilder {
 		else if (type.equals("Date")) {
 			return "TIMESTAMP";
 		}
-		else if (type.equals("Map") || type.equals("String")) {
+		else if (type.equals("Map")) {
+			return "CLOB";
+		}
+		else if (type.equals("String")) {
 			Map<String, String> hints = ModelHintsUtil.getHints(model, field);
 
 			if (hints != null) {
