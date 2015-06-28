fe7f572f0a6be0acf0385b90ad80280d71a3aaeb
Alberto Chaparro <alberto.chaparro@liferay.com>
2015-06-28 23:37:41 +0200

    LPS-51213 Keep backward compatibility for DL fields using image gallery links in DDMStructures

--- portal-impl/src/com/liferay/portlet/journal/util/JournalConverterImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/util/JournalConverterImpl.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portlet.journal.util;
 
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
@@ -55,6 +56,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Marcellus Tavares
@@ -349,29 +352,57 @@ public class JournalConverterImpl implements JournalConverter {
 		return StringUtil.merge(languageIds);
 	}
 
+	protected FileEntry getDLFileEntryFromDLURL(String url)
+		throws PortalException {
+
+		int x = url.indexOf("/documents/");
+		int y = url.indexOf(StringPool.QUESTION);
+
+		if (y == -1) {
+			y = url.length();
+		}
+
+		url = url.substring(x, y);
+
+		String[] parts = StringUtil.split(url, CharPool.SLASH);
+
+		long groupId = GetterUtil.getLong(parts[2]);
+
+		return DLAppLocalServiceUtil.getFileEntryByUuidAndGroupId(
+			parts[5], groupId);
+	}
+
+	protected FileEntry getDLFileEntryFromIGURL(String url)
+		throws PortalException {
+
+		Matcher m = _imageGalleryURLPattern.matcher(url);
+
+		if (!m.find()) {
+			return null;
+		}
+
+		long groupId = GetterUtil.getLong(m.group(2));
+
+		return DLAppLocalServiceUtil.getFileEntryByUuidAndGroupId(
+			m.group(1), groupId);
+	}
+
 	protected Serializable getDocumentLibraryValue(String url) {
 		try {
-			int x = url.indexOf("/documents/");
+			FileEntry fileEntry = null;
 
-			if (x == -1) {
+			if (url.contains("/documents/")) {
+				fileEntry = getDLFileEntryFromDLURL(url);
+			}
+			else {
+				if (url.contains("/image/image_gallery?")) {
+					fileEntry = getDLFileEntryFromIGURL(url);
+				}
+			}
+
+			if (fileEntry == null) {
 				return StringPool.BLANK;
 			}
-
-			int y = url.indexOf(StringPool.QUESTION);
-
-			if (y == -1) {
-				y = url.length();
-			}
-
-			url = url.substring(x, y);
-
-			String[] parts = StringUtil.split(url, CharPool.SLASH);
-
-			long groupId = GetterUtil.getLong(parts[2]);
-
-			FileEntry fileEntry =
-				DLAppLocalServiceUtil.getFileEntryByUuidAndGroupId(
-					parts[5], groupId);
 
 			JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
 
@@ -1007,6 +1038,8 @@ public class JournalConverterImpl implements JournalConverter {
 	private final Map<String, String> _ddmDataTypes;
 	private final Map<String, String> _ddmMetadataAttributes;
 	private final Map<String, String> _ddmTypesToJournalTypes;
+	private final Pattern _imageGalleryURLPattern = Pattern.compile(
+		"uuid=([^&]+)&groupId=([^&]+)");
 	private final Map<String, String> _journalTypesToDDMTypes;
 
 }
