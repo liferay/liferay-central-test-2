06ce296a95fe3ef891d2dc2dcad64070ea44fe9d
Matthew Tambara <matthew.tambara@liferay.com>
2015-06-29 16:03:38 -0700

    LPS-56674 Oracle needs to check for nulls differently.

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileShortcutLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileShortcutLocalServiceImpl.java
@@ -15,9 +15,11 @@
 package com.liferay.portlet.documentlibrary.service.impl;
 
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.Criterion;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
 import com.liferay.portal.kernel.dao.orm.Property;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
@@ -302,6 +304,10 @@ public class DLFileShortcutLocalServiceImpl
 	public void setTreePaths(final long folderId, final String treePath)
 		throws PortalException {
 
+		if (treePath == null) {
+			throw new IllegalArgumentException("Cannot set null treePath");
+		}
+
 		ActionableDynamicQuery actionableDynamicQuery =
 			getActionableDynamicQuery();
 
@@ -318,7 +324,11 @@ public class DLFileShortcutLocalServiceImpl
 					Property treePathProperty = PropertyFactoryUtil.forName(
 						"treePath");
 
-					dynamicQuery.add(treePathProperty.ne(treePath));
+					Criterion criterion = RestrictionsFactoryUtil.or(
+						treePathProperty.isNull(),
+						treePathProperty.ne(treePath));
+
+					dynamicQuery.add(criterion);
 				}
 
 			});
