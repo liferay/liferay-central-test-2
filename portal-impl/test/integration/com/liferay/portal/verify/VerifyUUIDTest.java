8a549bf870d13b3f3075cf754beef88743a436ef
shuyangzhou <shuyang.zhou@liferay.com>
2015-06-29 16:07:30 -0700

    LPS-56674 Sort

--- portal-impl/test/integration/com/liferay/portal/verify/VerifyUUIDTest.java
+++ portal-impl/test/integration/com/liferay/portal/verify/VerifyUUIDTest.java
@@ -57,15 +57,15 @@ public class VerifyUUIDTest extends BaseVerifyProcessTestCase {
 				expectedType = ExpectedType.EXACT
 			),
 			@ExpectedLog(
-				dbType = DB.TYPE_POSTGRESQL, expectedLog =
-					"Unable to process runnable: ERROR: column \"unknown\" " +
-						"does not exist",
-				expectedType = ExpectedType.PREFIX
-			),
-			@ExpectedLog(
 				dbType = DB.TYPE_ORACLE, expectedLog =
 					"Unable to process runnable: ORA-00904: \"UNKNOWN\":" +
 						" invalid identifier",
+				expectedType = ExpectedType.PREFIX
+			),
+			@ExpectedLog(
+				dbType = DB.TYPE_POSTGRESQL, expectedLog =
+					"Unable to process runnable: ERROR: column \"unknown\" " +
+						"does not exist",
 				expectedType = ExpectedType.PREFIX
 			)
 		},
@@ -97,15 +97,15 @@ public class VerifyUUIDTest extends BaseVerifyProcessTestCase {
 				expectedType = ExpectedType.PREFIX
 			),
 			@ExpectedLog(
-				dbType = DB.TYPE_POSTGRESQL, expectedLog =
-					"Unable to process runnable: ERROR: relation " +
-						"\"unknown\" does not exist",
-				expectedType = ExpectedType.PREFIX
-			),
-			@ExpectedLog(
 				dbType = DB.TYPE_ORACLE, expectedLog =
 					"Unable to process runnable: ORA-00942: table or view" +
 						" does not exist",
+				expectedType = ExpectedType.PREFIX
+			),
+			@ExpectedLog(
+				dbType = DB.TYPE_POSTGRESQL, expectedLog =
+					"Unable to process runnable: ERROR: relation " +
+						"\"unknown\" does not exist",
 				expectedType = ExpectedType.PREFIX
 			)
 		},
@@ -148,15 +148,15 @@ public class VerifyUUIDTest extends BaseVerifyProcessTestCase {
 				expectedType = ExpectedType.PREFIX
 			),
 			@ExpectedLog(
-				dbType = DB.TYPE_POSTGRESQL, expectedLog =
-					"Unable to process runnable: ERROR: relation \"unknown\" " +
-						"does not exist",
-				expectedType = ExpectedType.PREFIX
-			),
-			@ExpectedLog(
 				dbType = DB.TYPE_ORACLE, expectedLog =
 					"Unable to process runnable: ORA-00942: table or view" +
 						" does not exist",
+				expectedType = ExpectedType.PREFIX
+			),
+			@ExpectedLog(
+				dbType = DB.TYPE_POSTGRESQL, expectedLog =
+					"Unable to process runnable: ERROR: relation \"unknown\" " +
+						"does not exist",
 				expectedType = ExpectedType.PREFIX
 			)
 		},
