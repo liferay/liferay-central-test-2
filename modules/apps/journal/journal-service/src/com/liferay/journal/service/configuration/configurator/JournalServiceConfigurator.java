8f5622f92b1a6891b78099d44987c8fc43cfbc3a
Eduardo Garcia <eduardo.garcia@liferay.com>
2015-06-29 17:09:43 +0200

    LPS-56383 Invert dependencies: JournalUpgradeService should depend on JournalServiceConfigurator

--- modules/apps/journal/journal-service/src/com/liferay/journal/service/configuration/configurator/JournalServiceConfigurator.java
+++ modules/apps/journal/journal-service/src/com/liferay/journal/service/configuration/configurator/JournalServiceConfigurator.java
@@ -14,10 +14,11 @@
 
 package com.liferay.journal.service.configuration.configurator;
 
-import com.liferay.journal.upgrade.JournalServiceUpgrade;
 import com.liferay.portal.service.configuration.ServiceComponentConfiguration;
 import com.liferay.portal.service.configuration.configurator.ServiceConfigurator;
 import com.liferay.portal.spring.extender.loader.ModuleResourceLoader;
+
+import javax.servlet.ServletContext;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
@@ -56,15 +57,14 @@ public class JournalServiceConfigurator {
 	}
 
 	@Reference(unbind = "-")
-	protected void setJournalServiceUpgrade(
-		JournalServiceUpgrade journalServiceUpgrade) {
-	}
-
-	@Reference(unbind = "-")
 	protected void setServiceConfigurator(
 		ServiceConfigurator serviceConfigurator) {
 
 		_serviceConfigurator = serviceConfigurator;
+	}
+
+	@Reference(target = "(original.bean=*)", unbind = "-")
+	protected void setServletContext(ServletContext servletContext) {
 	}
 
 	private ServiceConfigurator _serviceConfigurator;
