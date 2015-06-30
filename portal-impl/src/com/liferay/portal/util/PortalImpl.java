191f2f9b6d10681936c92ec649c548805a939f04
Iván Zaera Avellón <ivan.zaera@liferay.com>
2015-06-30 12:04:27 +0200

    LPS-56244 Change struts_action to mvcRenderCommandName

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -5564,18 +5564,20 @@ public class PortalImpl implements Portal {
 		}
 
 		String path = GetterUtil.getString(request.getPathInfo());
-		String strutsAction = getStrutsAction(request);
+		String mvcRenderCommandName = ParamUtil.getString(
+			request, "mvcRenderCommandName");
 		String actionName = getPortletParam(request, "actionName");
 
 		boolean alwaysAllowDoAsUser = false;
 
 		if (path.equals("/portal/session_click") ||
-			strutsAction.equals("/document_library/edit_file_entry") ||
-			strutsAction.equals("/document_library_display/edit_file_entry") ||
-			strutsAction.equals("/image_gallery_display/edit_file_entry") ||
-			strutsAction.equals("/image_gallery_display/edit_image") ||
+			mvcRenderCommandName.equals("/document_library/edit_file_entry") ||
+			mvcRenderCommandName.equals(
+				"/document_library_display/edit_file_entry") ||
+			mvcRenderCommandName.equals(
+				"/image_gallery_display/edit_file_entry") ||
 			actionName.equals("addFile") ||
-			isAlwaysAllowDoAsUser(path, strutsAction, actionName)) {
+			isAlwaysAllowDoAsUser(path, mvcRenderCommandName, actionName)) {
 
 			try {
 				alwaysAllowDoAsUser = isAlwaysAllowDoAsUser(request);
