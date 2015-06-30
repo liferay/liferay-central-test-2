57474dcf469b063d1ed357858a4a43db2d58f978
Dustin Ryerson <dustin.ryerson@liferay.com>
2015-06-30 16:57:25 -0700

    LPS-56727 SF

--- portal-service/src/com/liferay/portal/kernel/util/SystemProperties.java
+++ portal-service/src/com/liferay/portal/kernel/util/SystemProperties.java
@@ -75,9 +75,8 @@ public class SystemProperties {
 	public static Properties getProperties(
 		String prefix, boolean removePrefix) {
 
-		Properties properties = getProperties();
-
-		return PropertiesUtil.getProperties(properties, prefix, removePrefix);
+		return PropertiesUtil.getProperties(
+			getProperties(), prefix, removePrefix);
 	}
 
 	public static void reload() {
