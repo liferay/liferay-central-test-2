a6e2ae9bbb1ff5534976fc671203c7018f892f70
Brian Chan <brian.chan@liferay.com>
2015-06-30 10:20:36 -0700

    LPS-56683 SF

--- portal-test-internal/src/com/liferay/portal/lar/test/BaseExportImportTestCase.java
+++ portal-test-internal/src/com/liferay/portal/lar/test/BaseExportImportTestCase.java
@@ -19,7 +19,6 @@ import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.util.DateUtil;
 import com.liferay.portal.kernel.util.FileUtil;
-import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.StagedModel;
