9584ae0b0ca03a60987c965b25b1ab01809d84fb
Julio Camarero <julio.camarero@liferay.com>
2015-07-01 14:22:09 +0200

    LPS-56773 rename methods to be more accurate of what they expect

--- modules/apps/search/search-web/src/com/liferay/search/web/facet/AssetEntriesSearchFacet.java
+++ modules/apps/search/search-web/src/com/liferay/search/web/facet/AssetEntriesSearchFacet.java
@@ -50,7 +50,7 @@ public class AssetEntriesSearchFacet extends BaseJSPSearchFacet {
 	}
 
 	@Override
-	public String getConfigurationView() {
+	public String getConfigurationJspPath() {
 		return _JSP_CONFIGURATION_PATH;
 	}
 
@@ -84,7 +84,7 @@ public class AssetEntriesSearchFacet extends BaseJSPSearchFacet {
 	}
 
 	@Override
-	public String getDisplayView() {
+	public String getDisplayJspPath() {
 		return _JSP_VIEW_PATH;
 	}
 
