624769758fb7916791e4b7e9b9d59bb864811940
Roberto Díaz <roberto.diaz@liferay.com>
2015-07-01 08:52:28 +0200

    LPS-56709 Allow search in portlet file repository

--- portal-service/src/com/liferay/portal/portletfilerepository/PortletFileRepositoryUtil.java
+++ portal-service/src/com/liferay/portal/portletfilerepository/PortletFileRepositoryUtil.java
@@ -19,6 +19,8 @@ import aQute.bnd.annotation.ProviderType;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.security.pacl.permission.PortalRuntimePermission;
 import com.liferay.portal.kernel.util.ObjectValuePair;
 import com.liferay.portal.kernel.util.OrderByComparator;
@@ -310,6 +312,14 @@ public class PortletFileRepositoryUtil {
 
 		getPortletFileRepository().restorePortletFileEntryFromTrash(
 			groupId, userId, folderId, fileName);
+	}
+
+	public static Hits searchPortletFileEntries(
+			long groupId, SearchContext searchContext)
+		throws PortalException {
+
+		return getPortletFileRepository().searchPortletFileEntries(
+			groupId, searchContext);
 	}
 
 	public void setPortletFileRepository(
