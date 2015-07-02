5d0829b44a6aa1811dfb8a55eb144d89c1624a2f
Carlos Sierra <carlos.sierra@liferay.com>
2015-07-02 11:28:13 +0200

    LPS-56271 Do not make caller know the callee

--- modules/portal/portal-store-db/src/com/liferay/portal/store/db/DBStoreRegistrator.java
+++ modules/portal/portal-store-db/src/com/liferay/portal/store/db/DBStoreRegistrator.java
@@ -17,11 +17,18 @@ package com.liferay.portal.store.db;
 import com.liferay.portal.kernel.bean.PortalBeanLocatorUtil;
 import com.liferay.portal.kernel.dao.db.DB;
 import com.liferay.portal.kernel.dao.db.DBFactoryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.kernel.transaction.Transactional;
 import com.liferay.portal.kernel.util.ClassLoaderUtil;
 import com.liferay.portal.kernel.util.ProxyUtil;
 import com.liferay.portal.spring.aop.MethodInterceptorInvocationHandler;
+import com.liferay.portlet.documentlibrary.store.BaseStore;
 import com.liferay.portlet.documentlibrary.store.Store;
 import com.liferay.portlet.documentlibrary.store.TempFileMethodInterceptor;
+
+import java.io.File;
+import java.io.InputStream;
 
 import java.util.Arrays;
 import java.util.Hashtable;
@@ -73,8 +80,6 @@ public class DBStoreRegistrator {
 			return store;
 		}
 
-		DBStore dbStore = (DBStore)store;
-
 		ClassLoader classLoader = ClassLoaderUtil.getPortalClassLoader();
 
 		MethodInterceptor transactionAdviceMethodInterceptor =
@@ -88,15 +93,191 @@ public class DBStoreRegistrator {
 			transactionAdviceMethodInterceptor, tempFileMethodInterceptor);
 
 		store = (Store)ProxyUtil.newProxyInstance(
-			classLoader, new Class<?>[]{Store.class},
+			classLoader, new Class<?>[] {Store.class},
 			new MethodInterceptorInvocationHandler(store, methodInterceptors));
 
-		dbStore.setStoreProxy(store);
-
-		return dbStore;
+		return new ProxyDelegatorDBStore(store);
 	}
 
 	private Store _dbStore;
 	private ServiceRegistration<Store> _storeServiceRegistration;
+
+	/**
+	 * This class must only delegate the methods that DBStore overrides
+	 */
+	private static class ProxyDelegatorDBStore extends BaseStore {
+
+		public ProxyDelegatorDBStore(Store store) {
+			_store = store;
+		}
+
+		@Override
+		public void addDirectory(
+			long companyId, long repositoryId, String dirName) {
+
+			_store.addDirectory(companyId, repositoryId, dirName);
+		}
+
+		@Override
+		public void addFile(
+				long companyId, long repositoryId, String fileName,
+				byte[] bytes)
+			throws PortalException {
+
+			_store.addFile(companyId, repositoryId, fileName, bytes);
+		}
+
+		@Override
+		public void addFile(
+				long companyId, long repositoryId, String fileName, File file)
+			throws PortalException {
+
+			_store.addFile(companyId, repositoryId, fileName, file);
+		}
+
+		@Override
+		public void addFile(
+				long companyId, long repositoryId, String fileName,
+				InputStream inputStream)
+			throws PortalException {
+
+			_store.addFile(companyId, repositoryId, fileName, inputStream);
+		}
+
+		@Override
+		public void checkRoot(long companyId) {
+			_store.checkRoot(companyId);
+		}
+
+		@Override
+		public void deleteDirectory(
+			long companyId, long repositoryId, String dirName) {
+
+			_store.deleteDirectory(companyId, repositoryId, dirName);
+		}
+
+		@Override
+		public void deleteFile(
+			long companyId, long repositoryId, String fileName) {
+
+			_store.deleteFile(companyId, repositoryId, fileName);
+		}
+
+		@Override
+		public void deleteFile(
+			long companyId, long repositoryId, String fileName,
+			String versionLabel) {
+
+			_store.deleteFile(companyId, repositoryId, fileName, versionLabel);
+		}
+
+		@Override
+		@Transactional(propagation = Propagation.REQUIRED)
+		public InputStream getFileAsStream(
+				long companyId, long repositoryId, String fileName)
+			throws PortalException {
+
+			return _store.getFileAsStream(companyId, repositoryId, fileName);
+		}
+
+		@Override
+		@Transactional(propagation = Propagation.REQUIRED)
+		public InputStream getFileAsStream(
+				long companyId, long repositoryId, String fileName,
+				String versionLabel)
+			throws PortalException {
+
+			return _store.getFileAsStream(
+				companyId, repositoryId, fileName, versionLabel);
+		}
+
+		@Override
+		public String[] getFileNames(long companyId, long repositoryId) {
+			return _store.getFileNames(companyId, repositoryId);
+		}
+
+		@Override
+		public String[] getFileNames(
+			long companyId, long repositoryId, String dirName) {
+
+			return _store.getFileNames(companyId, repositoryId, dirName);
+		}
+
+		@Override
+		public long getFileSize(
+				long companyId, long repositoryId, String fileName)
+			throws PortalException {
+
+			return _store.getFileSize(companyId, repositoryId, fileName);
+		}
+
+		@Override
+		public boolean hasDirectory(
+			long companyId, long repositoryId, String dirName) {
+
+			return _store.hasDirectory(companyId, repositoryId, dirName);
+		}
+
+		@Override
+		public boolean hasFile(
+			long companyId, long repositoryId, String fileName,
+			String versionLabel) {
+
+			return _store.hasFile(
+				companyId, repositoryId, fileName, versionLabel);
+		}
+
+		@Override
+		public void updateFile(
+				long companyId, long repositoryId, long newRepositoryId,
+				String fileName)
+			throws PortalException {
+
+			_store.updateFile(
+				companyId, repositoryId, newRepositoryId, fileName);
+		}
+
+		@Override
+		public void updateFile(
+				long companyId, long repositoryId, String fileName,
+				String newFileName)
+			throws PortalException {
+
+			_store.updateFile(companyId, repositoryId, fileName, newFileName);
+		}
+
+		@Override
+		public void updateFile(
+				long companyId, long repositoryId, String fileName,
+				String versionLabel, byte[] bytes)
+			throws PortalException {
+
+			_store.updateFile(
+				companyId, repositoryId, fileName, versionLabel, bytes);
+		}
+
+		@Override
+		public void updateFile(
+				long companyId, long repositoryId, String fileName,
+				String versionLabel, File file)
+			throws PortalException {
+
+			_store.updateFile(
+				companyId, repositoryId, fileName, versionLabel, file);
+		}
+
+		@Override
+		public void updateFile(
+				long companyId, long repositoryId, String fileName,
+				String versionLabel, InputStream inputStream)
+			throws PortalException {
+
+			_store.updateFile(
+				companyId, repositoryId, fileName, versionLabel, inputStream);
+		}
+
+		private final Store _store;
+
+	}
 
 }
