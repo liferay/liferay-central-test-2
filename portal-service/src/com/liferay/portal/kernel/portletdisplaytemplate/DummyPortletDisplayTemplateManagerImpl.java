6eb7aaeb4a944532bd1afcf33d7c8b37ce3166ec
Leonardo Barros <leonardo.barros@liferay.com>
2015-07-02 12:23:25 -0300

    LPS-56789 Create dummy implementation

---
+++ portal-service/src/com/liferay/portal/kernel/portletdisplaytemplate/DummyPortletDisplayTemplateManagerImpl.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.portletdisplaytemplate;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.liferay.portal.kernel.template.TemplateHandler;
+import com.liferay.portal.kernel.template.TemplateVariableGroup;
+import com.liferay.portlet.dynamicdatamapping.model.DDMTemplate;
+
+/**
+ * @author Leonardo Barros
+ */
+public class DummyPortletDisplayTemplateManagerImpl 
+	implements PortletDisplayTemplateManager {
+
+	@Override
+	public DDMTemplate getDDMTemplate(long groupId, long classNameId,
+			String displayStyle, boolean useDefault) {
+		return null;
+	}
+
+	@Override
+	public long getDDMTemplateGroupId(long groupId) {
+		return 0;
+	}
+
+	@Override
+	public List<TemplateHandler> getPortletDisplayTemplateHandlers() {
+		return null;
+	}
+
+	@Override
+	public Map<String, TemplateVariableGroup> getTemplateVariableGroups(
+			String language) {
+		return null;
+	}
+
+	@Override
+	public String renderDDMTemplate(HttpServletRequest request,
+			HttpServletResponse response, long templateId, List<?> entries,
+			Map<String, Object> contextObjects) throws Exception {
+		return null;
+	}
+
+}
--- portal-service/src/com/liferay/portal/kernel/portletdisplaytemplate/PortletDisplayTemplateManagerUtil.java
+++ portal-service/src/com/liferay/portal/kernel/portletdisplaytemplate/PortletDisplayTemplateManagerUtil.java
@@ -82,7 +82,18 @@ public class PortletDisplayTemplateManagerUtil {
 	private static PortletDisplayTemplateManager
 		_getPortletDisplayTemplateManager() {
 
-		return _instance._serviceTracker.getService();
+		PortletDisplayTemplateManager manager = 
+			_instance._serviceTracker.getService();
+		
+		if(manager != null) {
+			return manager;
+		}
+		else {
+			if(_dummyImpl == null) {
+				_dummyImpl = new DummyPortletDisplayTemplateManagerImpl();
+			}
+			return _dummyImpl;
+		}
 	}
 
 	private PortletDisplayTemplateManagerUtil() {
@@ -96,6 +107,8 @@ public class PortletDisplayTemplateManagerUtil {
 
 	private static final PortletDisplayTemplateManagerUtil _instance =
 		new PortletDisplayTemplateManagerUtil();
+	
+	private static DummyPortletDisplayTemplateManagerImpl _dummyImpl;
 
 	private final
 		ServiceTracker<PortletDisplayTemplateManager,
