2cc69a30f0ce4a995e3d8618f1a8fae65dd94841
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-07-06 13:53:36 +0200

    LPS-56703 Public paths

---
+++ modules/apps/blogs/blogs-web/src/com/liferay/blogs/web/blogs/portlet/path/FindEntryAuthPublicPath.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.blogs.web.blogs.portlet.path;
+
+import com.liferay.portal.kernel.struts.path.AuthPublicPath;
+import com.liferay.portal.kernel.struts.path.BaseAuthPublicPath;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+
+/**
+ * @author Sergio González
+ */
+@Component(
+	immediate = true,
+	property = BaseAuthPublicPath.AUTH_PUBLIC_PATH + "=/blogs/trackback",
+	service = AuthPublicPath.class
+)
+public class FindEntryAuthPublicPath extends BaseAuthPublicPath {
+
+	@Activate
+	protected void activate(Map<String, String> properties) {
+		updatePath(properties);
+	}
+
+	@Modified
+	protected void modified(Map<String, String> properties) {
+		updatePath(properties);
+	}
+
+}
