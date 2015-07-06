jonathanmccann/liferay-portal

LPS-52763 Unable to change order of assets in Asset Publisher with expired content

portal-service/src/com/liferay/portlet/asset/model/AssetRendererFactory.java
@@ -50,6 +50,10 @@ public interface AssetRendererFactory {
 	public AssetRenderer getAssetRenderer(long classPK, int type)
 		throws PortalException;
 
+	public AssetRenderer getAssetRenderer(
+			long classPK, int type, boolean manualConfiguration)
+		throws PortalException;
+
 	public AssetRenderer getAssetRenderer(long groupId, String urlTitle)
 		throws PortalException;
 

portal-service/src/com/liferay/portlet/asset/model/BaseAssetRendererFactory.java
@@ -66,6 +66,14 @@ public abstract class BaseAssetRendererFactory implements AssetRendererFactory {
 	}
 
 	@Override
+	public AssetRenderer getAssetRenderer(
+			long classPK, int type, boolean manualConfiguration)
+		throws PortalException {
+
+		return getAssetRenderer(classPK, type);
+	}
+
+	@Override
 	@SuppressWarnings("unused")
 	public AssetRenderer getAssetRenderer(long groupId, String urlTitle)
 		throws PortalException {

liferay/liferay-central-test-2

portal-service/src/com/liferay/portlet/asset/model/AssetRendererFactory.java
@@ -50,6 +50,10 @@ public interface AssetRendererFactory {
 	public AssetRenderer getAssetRenderer(long classPK, int type)
 		throws PortalException;
 
+	public AssetRenderer getAssetRenderer(
+			long classPK, int type, boolean includeNonVisibleAssets)
+		throws PortalException;
+
 	public AssetRenderer getAssetRenderer(long groupId, String urlTitle)
 		throws PortalException;
 

portal-service/src/com/liferay/portlet/asset/model/BaseAssetRendererFactory.java
@@ -66,6 +66,14 @@ public abstract class BaseAssetRendererFactory implements AssetRendererFactory {
 	}
 
 	@Override
+	public AssetRenderer getAssetRenderer(
+			long classPK, int type, boolean includeNonVisibleAssets)
+		throws PortalException {
+
+		return getAssetRenderer(classPK, type);
+	}
+
+	@Override
 	@SuppressWarnings("unused")
 	public AssetRenderer getAssetRenderer(long groupId, String urlTitle)
 		throws PortalException {