0abdfc22c0c4ecb95c69e05c5bf35280b2ea70e2
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-07-06 18:32:42 +0200

    LPS-56703 Move Language key to module and remove them from portal

--- modules/apps/blogs/blogs-layout-prototype/src/com/liferay/blogs/layout/prototype/action/AddLayoutPrototypeAction.java
+++ modules/apps/blogs/blogs-layout-prototype/src/com/liferay/blogs/layout/prototype/action/AddLayoutPrototypeAction.java
@@ -15,6 +15,8 @@
 package com.liferay.blogs.layout.prototype.action;
 
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.model.Company;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.LayoutPrototype;
@@ -25,6 +27,7 @@ import com.liferay.portal.util.DefaultLayoutPrototypesUtil;
 import com.liferay.portal.util.PortletKeys;
 
 import java.util.List;
+import java.util.ResourceBundle;
 
 import javax.servlet.ServletContext;
 
@@ -61,10 +64,15 @@ public class AddLayoutPrototypeAction {
 			List<LayoutPrototype> layoutPrototypes)
 		throws Exception {
 
+		ResourceBundle resourceBundle = ResourceBundle.getBundle(
+			"content.Language", LocaleUtil.getDefault());
+
 		Layout layout = DefaultLayoutPrototypesUtil.addLayoutPrototype(
-			companyId, defaultUserId, "layout-prototype-blog-title",
-			"layout-prototype-blog-description", "2_columns_iii",
-			layoutPrototypes);
+			companyId, defaultUserId,
+			LanguageUtil.get(resourceBundle, "layout-prototype-blog-title"),
+			LanguageUtil.get(
+				resourceBundle, "layout-prototype-blog-description"),
+			"2_columns_iii", layoutPrototypes);
 
 		if (layout == null) {
 			return;
