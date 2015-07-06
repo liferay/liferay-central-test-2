ba67f39f3e4172c0f3d9fd584469df4d7cf11fb2
Pavel Savinov <pavel.savinov@liferay.com>
2015-07-06 16:11:05 +0200

    LPS-55966 update JournalContentSearch on layout copy

--- portal-impl/src/com/liferay/portlet/sites/action/ActionUtil.java
+++ portal-impl/src/com/liferay/portlet/sites/action/ActionUtil.java
@@ -14,14 +14,17 @@
 
 package com.liferay.portlet.sites.action;
 
+import com.liferay.portal.kernel.portlet.PortletLayoutListener;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.LayoutTypePortlet;
+import com.liferay.portal.model.Portlet;
 import com.liferay.portal.model.PortletPreferencesIds;
 import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.LayoutServiceUtil;
+import com.liferay.portal.service.PortletLocalServiceUtil;
 import com.liferay.portal.service.PortletPreferencesLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.PortalUtil;
@@ -105,6 +108,17 @@ public class ActionUtil {
 				themeDisplay.getUserId(), sourceLayout, targetLayout,
 				sourcePreferences, targetPreferences, sourcePortletId,
 				themeDisplay.getLanguageId());
+
+			Portlet sourcePortlet = PortletLocalServiceUtil.getPortletById(
+				sourceLayout.getCompanyId(), sourcePortletId);
+
+			PortletLayoutListener sourcePortletLayoutListener =
+				sourcePortlet.getPortletLayoutListenerInstance();
+
+			if (sourcePortletLayoutListener != null) {
+				sourcePortletLayoutListener.onAddToLayout(
+					sourcePortletId, targetLayout.getPlid());
+			}
 		}
 	}
 
