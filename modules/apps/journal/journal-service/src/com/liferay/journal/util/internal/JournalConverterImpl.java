df03176374ac9b42317efe95e4adc54eaec3632e
Leonardo Barros <leonardo.barros@liferay.com>
2015-07-06 09:47:24 -0300

    LPS-56848 Move to internal package

--- modules/apps/journal/journal-service/src/com/liferay/journal/util/internal/JournalConverterImpl.java
+++ modules/apps/journal/journal-service/src/com/liferay/journal/util/internal/JournalConverterImpl.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.journal.util.impl;
+package com.liferay.journal.util.internal;
 
 import com.liferay.journal.util.JournalConverter;
 import com.liferay.portal.kernel.exception.PortalException;
