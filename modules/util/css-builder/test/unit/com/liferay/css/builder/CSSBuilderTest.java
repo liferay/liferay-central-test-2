3f603fffb9e8d7f5e2cbdcb70ae0e386552475d8
Raymond Auge <raymond.auge@liferay.com>
2015-07-06 22:25:59 -0400

    LPS-55156 typo removed use of param

--- modules/util/css-builder/test/unit/com/liferay/css/builder/CSSBuilderTest.java
+++ modules/util/css-builder/test/unit/com/liferay/css/builder/CSSBuilderTest.java
@@ -102,7 +102,7 @@ public class CSSBuilderTest {
 	private void _testSassToCssBuilder(String compiler) throws Exception {
 		CSSBuilder cssBuilder = new CSSBuilder(
 			_docrootDirName, "../../frontend/frontend-common-css/src/META-INF" +
-				"/resources/html/css/common", new String[0], "jni");
+				"/resources/html/css/common", new String[0], compiler);
 
 		cssBuilder.execute(Arrays.asList(new String[] {"/css"}));
 
