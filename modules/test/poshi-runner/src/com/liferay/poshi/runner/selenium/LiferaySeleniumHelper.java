1bae8fac7a3676d37d3d4933029e8969333044dd
Brian Chiu <brian.chiu@liferay.com>
2015-07-07 13:49:11 -0700

    LRQA-16609 Remove from poshi runner also

--- modules/test/poshi-runner/src/com/liferay/poshi/runner/selenium/LiferaySeleniumHelper.java
+++ modules/test/poshi-runner/src/com/liferay/poshi/runner/selenium/LiferaySeleniumHelper.java
@@ -1078,14 +1078,6 @@ public class LiferaySeleniumHelper {
 			return true;
 		}
 
-		// LPS-55835, temporary workaround while Brian Wulbern investigates it
-
-		if (line.matches(
-				"Current URL.*add_panel generates exception:[\\s\\S]*")) {
-
-			return true;
-		}
-
 		// LRQA-14442, temporary workaround until Kiyoshi Lee fixes it
 
 		if (line.contains("Framework Event Dispatcher: Equinox Container:")) {
