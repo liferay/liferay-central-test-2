8244ad3056957e34f83a4656984d9f9bbd659b6a
NorbertKocsis <norbert.kocsis@liferay.com>
2015-07-07 12:18:05 +0200

    LPS-56888 Remove permission only if present on form and not selected

--- portal-impl/src/com/liferay/portlet/rolesadmin/RolesAdminPortlet.java
+++ portal-impl/src/com/liferay/portlet/rolesadmin/RolesAdminPortlet.java
@@ -270,6 +270,8 @@ public class RolesAdminPortlet extends MVCPortlet {
 
 		String[] selectedTargets = StringUtil.split(
 			ParamUtil.getString(actionRequest, "selectedTargets"));
+		String[] unselectedTargets = StringUtil.split(
+			ParamUtil.getString(actionRequest, "unselectedTargets"));
 
 		for (Map.Entry<String, List<String>> entry :
 				resourceActionsMap.entrySet()) {
@@ -284,6 +286,12 @@ public class RolesAdminPortlet extends MVCPortlet {
 				String target = selResource + actionId;
 
 				boolean selected = ArrayUtil.contains(selectedTargets, target);
+
+				if (!selected &&
+					!ArrayUtil.contains(unselectedTargets, target)) {
+
+					continue;
+				}
 
 				String[] groupIds = StringUtil.split(
 					ParamUtil.getString(actionRequest, "groupIds" + target));
