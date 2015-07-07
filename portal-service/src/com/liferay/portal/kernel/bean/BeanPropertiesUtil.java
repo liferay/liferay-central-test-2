2d12dd8456e0f54cdaed3b4012cefa699ead5151
Leonardo Barros <leonardo.barros@liferay.com>
2015-07-07 10:36:20 -0300

    LPS-56908 Add a method for making a deep copy of an object

--- portal-service/src/com/liferay/portal/kernel/bean/BeanPropertiesUtil.java
+++ portal-service/src/com/liferay/portal/kernel/bean/BeanPropertiesUtil.java
@@ -16,6 +16,23 @@ package com.liferay.portal.kernel.bean;
 
 import com.liferay.portal.kernel.security.pacl.permission.PortalRuntimePermission;
 
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -37,6 +54,64 @@ public class BeanPropertiesUtil {
 		Object source, Object target, String[] ignoreProperties) {
 
 		getBeanProperties().copyProperties(source, target, ignoreProperties);
+	}
+
+	public static <T> T deepCopyProperties(Class<T> toClazz, Object from)
+		throws Exception {
+
+		T to = toClazz.newInstance();
+
+		BeanInfo fromBean = Introspector.getBeanInfo(from.getClass());
+		BeanInfo toBean = Introspector.getBeanInfo(toClazz);
+
+		PropertyDescriptor[] fromProperties = fromBean.getPropertyDescriptors();
+		PropertyDescriptor[] toProperties = toBean.getPropertyDescriptors();
+
+		for (PropertyDescriptor toProperty : toProperties) {
+			Class<?> toPropertyType = toProperty.getPropertyType();
+
+			for (PropertyDescriptor fromProperty : fromProperties) {
+				if (!toProperty.getName().equals("class") &&
+					toProperty.getName().equals(fromProperty.getName())) {
+
+					Class<?> fromPropertyType = fromProperty.getPropertyType();
+
+					if ((toPropertyType != null) &&
+						(fromPropertyType != null) &&
+						(toProperty.getWriteMethod() != null) &&
+						(fromProperty.getReadMethod() != null)) {
+
+						if (isCollectionType(
+								toPropertyType, fromPropertyType)) {
+
+							copyCollection(fromProperty, toProperty, from, to);
+						}
+						else if (isMapType(toPropertyType, fromPropertyType)) {
+							copyMap(fromProperty, toProperty, from, to);
+						}
+						else if (isSimpleCopy(
+									toPropertyType, fromPropertyType)) {
+
+							toProperty.getWriteMethod().invoke(
+								to, fromProperty.getReadMethod().invoke(from));
+						}
+						else {
+							Object value =
+								fromProperty.getReadMethod().invoke(from);
+
+							Object newObject = deepCopyProperties(
+								toPropertyType, value);
+
+							toProperty.getWriteMethod().invoke(to, newObject);
+						}
+					}
+
+					break;
+				}
+			}
+		}
+
+		return to;
 	}
 
 	public static BeanProperties getBeanProperties() {
@@ -257,6 +332,124 @@ public class BeanPropertiesUtil {
 		PortalRuntimePermission.checkSetBeanProperty(getClass());
 
 		_beanProperties = beanProperties;
+	}
+
+	private static Collection<Object> copyCollection(
+			Class<?> toClazz, Collection<?> from)
+		throws Exception {
+
+		Collection<Object> to = null;
+
+		if (from == null) {
+			return null;
+		}
+
+		if (from instanceof Set<?>) {
+			to = new LinkedHashSet<>();
+		}
+		else if (from instanceof List<?>) {
+			to = new ArrayList<>();
+		}
+
+		if (!from.isEmpty()) {
+			for (Object obj : from) {
+				if (isSimpleCopy(toClazz, obj.getClass())) {
+					to.add(obj);
+				}
+				else {
+					to.add(deepCopyProperties(toClazz, obj));
+				}
+			}
+		}
+
+		return to;
+	}
+
+	private static void copyCollection(
+			PropertyDescriptor fromProperty, PropertyDescriptor toProperty,
+			Object from, Object to)
+		throws Exception {
+
+		Object value = fromProperty.getReadMethod().invoke(from);
+
+		if (value != null) {
+			Method toMethod = toProperty.getWriteMethod();
+
+			Type[] types = toMethod.getGenericParameterTypes();
+
+			ParameterizedType pType = (ParameterizedType)types[0];
+
+			Class<?> clazz = (Class<?>)pType.getActualTypeArguments()[0];
+
+			toProperty.getWriteMethod().invoke(
+				to, copyCollection(clazz, (Collection<?>)value));
+		}
+	}
+
+	private static void copyMap(
+			PropertyDescriptor fromProperty, PropertyDescriptor toProperty,
+			Object from, Object to)
+		throws Exception {
+
+		Object value = fromProperty.getReadMethod().invoke(from);
+
+		if (value != null) {
+			Method toMethod = toProperty.getWriteMethod();
+
+			Type[] types = toMethod.getGenericParameterTypes();
+
+			ParameterizedType pType = (ParameterizedType)types[0];
+
+			Class<?> keyClazz = (Class<?>)pType.getActualTypeArguments()[0];
+			Class<?> valueClazz = (Class<?>)pType.getActualTypeArguments()[1];
+
+			Map<?, ?> fromMap = (Map<?, ?>)value;
+			Map<Object, Object> toMap = new HashMap<>();
+
+			for (Object key : fromMap.keySet()) {
+				Object toKey = null;
+				Object toValue = null;
+
+				Object mapValue = fromMap.get(key);
+
+				if (isSimpleCopy(keyClazz, key.getClass())) {
+					toKey = key;
+				}
+				else {
+					toKey = deepCopyProperties(keyClazz, key);
+				}
+
+				if (isSimpleCopy(valueClazz, mapValue.getClass())) {
+					toValue = mapValue;
+				}
+				else {
+					toValue = deepCopyProperties(valueClazz, mapValue);
+				}
+
+				toMap.put(toKey, toValue);
+			}
+
+			toProperty.getWriteMethod().invoke(to, toMap);
+		}
+	}
+
+	private static boolean isCollectionType(
+		Class<?> toClazz, Class<?> fromClazz) {
+
+		return ((toClazz.equals(Set.class) || toClazz.equals(List.class)) &&
+			(fromClazz.equals(Set.class) || fromClazz.equals(List.class)));
+	}
+
+	private static boolean isMapType(Class<?> toClazz, Class<?> fromClazz) {
+		return toClazz.equals(Map.class) && fromClazz.equals(Map.class);
+	}
+
+	private static boolean isSimpleCopy(Class<?> toClazz, Class<?> fromClazz) {
+		return toClazz.isAssignableFrom(fromClazz) &&
+			(fromClazz.isPrimitive() || fromClazz.equals(String.class) ||
+			fromClazz.equals(Long.class) || fromClazz.equals(Integer.class) ||
+			fromClazz.equals(Double.class) || fromClazz.equals(Float.class) ||
+			fromClazz.equals(Boolean.class) || fromClazz.equals(Locale.class));
 	}
 
 	private static BeanProperties _beanProperties;
