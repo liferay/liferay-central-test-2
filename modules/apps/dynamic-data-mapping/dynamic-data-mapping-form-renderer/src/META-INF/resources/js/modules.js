85fd967140ae0171d16edf9bfd41545b5b0e138b
Bruno Basto <bruno.basto@liferay.com>
2015-07-07 22:04:40 -0300

    LPS-56601 Cleaning up modules

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/META-INF/resources/js/modules.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/META-INF/resources/js/modules.js
@@ -6,12 +6,18 @@
 					base: '/o/ddm-type-checkbox/',
 					modules: {
 						'liferay-ddm-form-field-checkbox': {
+							condition: {
+								trigger: 'liferay-ddm-form-renderer'
+							},
 							path: 'checkbox_field.js',
 							requires: [
 								'liferay-ddm-form-renderer-field'
 							]
 						},
 						'liferay-ddm-form-field-checkbox-template': {
+							condition: {
+								trigger: 'liferay-ddm-form-renderer'
+							},
 							path: 'checkbox.soy.js',
 							requires: [
 								'soyutils'
@@ -24,12 +30,18 @@
 					base: '/o/ddm-type-options/',
 					modules: {
 						'liferay-ddm-form-field-options': {
+							condition: {
+								trigger: 'liferay-ddm-form-renderer'
+							},
 							path: 'options_field.js',
 							requires: [
 								'liferay-ddm-form-renderer-field'
 							]
 						},
 						'liferay-ddm-form-field-options-template': {
+							condition: {
+								trigger: 'liferay-ddm-form-renderer'
+							},
 							path: 'options.soy.js',
 							requires: [
 								'soyutils'
@@ -42,12 +54,18 @@
 					base: '/o/ddm-type-radio/',
 					modules: {
 						'liferay-ddm-form-field-radio': {
+							condition: {
+								trigger: 'liferay-ddm-form-renderer'
+							},
 							path: 'radio_field.js',
 							requires: [
 								'liferay-ddm-form-renderer-field'
 							]
 						},
 						'liferay-ddm-form-field-radio-template': {
+							condition: {
+								trigger: 'liferay-ddm-form-renderer'
+							},
 							path: 'radio.soy.js',
 							requires: [
 								'soyutils'
@@ -60,12 +78,18 @@
 					base: '/o/ddm-type-select/',
 					modules: {
 						'liferay-ddm-form-field-select': {
+							condition: {
+								trigger: 'liferay-ddm-form-renderer'
+							},
 							path: 'select_field.js',
 							requires: [
 								'liferay-ddm-form-renderer-field'
 							]
 						},
 						'liferay-ddm-form-field-select-template': {
+							condition: {
+								trigger: 'liferay-ddm-form-renderer'
+							},
 							path: 'select.soy.js',
 							requires: [
 								'soyutils'
@@ -78,6 +102,9 @@
 					base: '/o/ddm-type-text/',
 					modules: {
 						'liferay-ddm-form-field-text-template': {
+							condition: {
+								trigger: 'liferay-ddm-form-renderer'
+							},
 							path: 'text.soy.js',
 							requires: [
 								'soyutils'
@@ -89,8 +116,34 @@
 				'form': {
 					base: '/o/ddm-form-renderer/js/',
 					modules: {
-						'liferay-ddm-form-field': {
+						'liferay-ddm-form-renderer': {
+							path: 'form.js',
+							requires: [
+								'aui-component',
+								'aui-tabview',
+								'liferay-ddm-form-renderer-field-types',
+								'liferay-ddm-form-renderer-util'
+							]
+						},
+						'liferay-ddm-form-renderer-definition': {
+							path: 'form_definition_support.js',
+							requires: [
+								'liferay-ddm-form-renderer-field-types',
+								'liferay-ddm-form-renderer-util'
+							]
+						},
+						'liferay-ddm-form-renderer-field': {
 							path: 'field.js',
+							requires: [
+								'aui-datatype',
+								'aui-node',
+								'liferay-ddm-form-renderer',
+								'liferay-ddm-form-renderer-field-types',
+								'liferay-ddm-form-renderer-util'
+							]
+						},
+						'liferay-ddm-form-renderer-field-repetition': {
+							path: 'field_repetition_support.js',
 							requires: [
 								'aui-datatype',
 								'liferay-ddm-form-renderer-field-types',
@@ -102,28 +155,12 @@
 							requires: [
 								'array-extras',
 								'aui-form-builder-field-type',
-								'liferay-ddm-form-field-checkbox-template',
-								'liferay-ddm-form-field-radio-template',
-								'liferay-ddm-form-field-select-template',
-								'liferay-ddm-form-field-text-template',
 								'liferay-ddm-form-renderer-util'
 							]
 						},
-						'liferay-ddm-form-renderer': {
-							path: 'form.js',
+						'liferay-ddm-form-renderer-nested-fields': {
+							path: 'nested_fields_support.js',
 							requires: [
-								'array-extras',
-								'aui-tabview',
-								'liferay-ddm-form-field',
-								'liferay-ddm-form-field-checkbox',
-								'liferay-ddm-form-field-checkbox-template',
-								'liferay-ddm-form-field-options',
-								'liferay-ddm-form-field-options-template',
-								'liferay-ddm-form-field-radio',
-								'liferay-ddm-form-field-radio-template',
-								'liferay-ddm-form-field-select',
-								'liferay-ddm-form-field-select-template',
-								'liferay-ddm-form-field-text-template',
 								'liferay-ddm-form-renderer-field-types',
 								'liferay-ddm-form-renderer-util'
 							]
@@ -131,7 +168,8 @@
 						'liferay-ddm-form-renderer-util': {
 							path: 'util.js',
 							requires: [
-								'array-extras'
+								'liferay-ddm-form-renderer-field-types',
+								'queue'
 							]
 						}
 					},
