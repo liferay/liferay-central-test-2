15f6a96fcf6c960deea48daf33f3d7c2f83daeee
Bruno Basto <bruno.basto@liferay.com>
2015-07-07 21:59:19 -0300

    LPS-56601 Moving logic to support nested fields to a separated class

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/META-INF/resources/js/nested_fields_support.js
@@ -0,0 +1,151 @@
+AUI.add(
+	'liferay-ddm-form-renderer-nested-fields',
+	function(A) {
+		var _ = AUI._;
+
+		var NestedFieldsSupport = function() {
+		};
+
+		NestedFieldsSupport.ATTRS = {
+			fields: {
+				setter: '_setFields',
+				value: []
+			}
+		};
+
+		NestedFieldsSupport.prototype = {
+			initializer: function() {
+				var instance = this;
+
+				if (!instance._eventHandlers) {
+					instance._eventHandlers = [];
+				}
+
+				instance._eventHandlers.push(
+					instance.after('containerChange', instance._afterContainerChange)
+				);
+			},
+
+			destructor: function() {
+				var instance = this;
+
+				_.invoke(instance.get('fields'), 'destroy');
+			},
+
+			appendChild: function(field) {
+				var instance = this;
+
+				instance.insert(0, field);
+			},
+
+			eachField: function(fn) {
+				var instance = this;
+
+				var queue = new A.Queue(instance);
+
+				var addToQueue = function(item) {
+					queue.add(item);
+				};
+
+				while (queue.size() > 0) {
+					var field = queue.next();
+
+					fn.call(instance, field);
+
+					field.get('fields').forEach(addToQueue);
+				}
+			},
+
+			eachParent: function(fn) {
+				var instance = this;
+
+				var parent = instance.get('parent');
+
+				while (parent !== undefined) {
+					fn.call(instance, parent);
+
+					parent = parent.get('parent');
+				}
+			},
+
+			getField: function(name) {
+				var instance = this;
+
+				var field;
+
+				instance.eachField(
+					function(item) {
+						if (item.get('name') === name) {
+							field = item;
+						}
+					}
+				);
+
+				return field;
+			},
+
+			getRoot: function() {
+				var instance = this;
+
+				var root;
+
+				instance.eachParent(
+					function(parent) {
+						root = parent;
+					}
+				);
+
+				return root;
+			},
+
+			indexOf: function(field) {
+				var instance = this;
+
+				return _.indexOf(instance.get('fields'), field);
+			},
+
+			insert: function(index, field) {
+				var instance = this;
+
+				instance.removeChild(field);
+
+				var fields = instance.get('fields');
+
+				fields.splice(index, 0, field);
+
+				instance.set('fields', fields);
+			},
+
+			removeChild: function(field) {
+				var instance = this;
+
+				var fields = instance.get('fields');
+
+				var index = instance.indexOf(field);
+
+				if (index > -1) {
+					fields.splice(index, 1);
+				}
+
+				instance.set('fields', fields);
+			},
+
+			_afterContainerChange: function(event) {
+				var instance = this;
+
+				_.each(
+					instance.get('fields'),
+					function(item) {
+						event.newVal.append(item.get('container'));
+					}
+				);
+			}
+		};
+
+		Liferay.namespace('DDM.Renderer').NestedFieldsSupport = NestedFieldsSupport;
+	},
+	'',
+	{
+		requires: ['liferay-ddm-form-renderer-field-types', 'liferay-ddm-form-renderer-util']
+	}
+);
