dbadbc2232909bc8228698040505393db1160ae4
Adolfo Pérez <adolfo.perez@liferay.com>
2015-07-08 15:06:07 +0200

    LPS-56946 Add friendly-url-mapping to wf portlets

--- modules/apps/workflow/workflow-instance-web/src/com/liferay/workflow/instance/web/portlet/MyWorkflowInstancePortlet.java
+++ modules/apps/workflow/workflow-instance-web/src/com/liferay/workflow/instance/web/portlet/MyWorkflowInstancePortlet.java
@@ -29,6 +29,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.control-panel-entry-category=my",
 		"com.liferay.portlet.control-panel-entry-weight=4.0",
 		"com.liferay.portlet.display-category=category.hidden",
+                "com.liferay.portlet.friendly-url-mapping=my_workflow_instances",
 		"com.liferay.portlet.icon=/icons/my_workflow_instance.png",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
