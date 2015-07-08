445d959443831182062b31d09531352ab1c7433c
Bruno Basto <bruno.basto@liferay.com>
2015-07-07 21:58:40 -0300

    LPS-56601 Moving repetition logic to a separated class

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/META-INF/resources/js/field_repetition_support.js
@@ -0,0 +1,162 @@
+AUI.add(
+	'liferay-ddm-form-renderer-field-repetition',
+	function(A) {
+		var _ = AUI._;
+		var Renderer = Liferay.DDM.Renderer;
+
+		var FieldTypes = Renderer.FieldTypes;
+		var Util = Renderer.Util;
+
+		var SELECTOR_REPEAT_BUTTONS = '.lfr-ddm-form-field-repeatable-add-button, .lfr-ddm-form-field-repeatable-delete-button';
+
+		var TPL_REPEATABLE_ADD = '<a class="icon-plus-sign lfr-ddm-form-field-repeatable-add-button" href="javascript:;"></a>';
+
+		var TPL_REPEATABLE_DELETE = '<a class="hide icon-minus-sign lfr-ddm-form-field-repeatable-delete-button" href="javascript:;"></a>';
+
+		var TPL_REPEATABLE_TOOLBAR = '<div class="lfr-ddm-form-field-repeatable-toolbar">' + TPL_REPEATABLE_DELETE + TPL_REPEATABLE_ADD + '</div>';
+
+		var FieldRepetitionSupport = function() {
+		};
+
+		FieldRepetitionSupport.ATTRS = {
+			repeatable: {
+				setter: A.DataType.Boolean.parse,
+				value: false
+			},
+
+			repeatedIndex: {
+				value: 0
+			}
+		};
+
+		FieldRepetitionSupport.prototype = {
+			initializer: function() {
+				var instance = this;
+
+				if (instance.get('repeatable')) {
+					instance._eventHandlers.push(
+						instance.after(instance.renderRepeatable, instance, 'render'),
+						instance.after('repeatedIndexChange', instance._afterRepeatableIndexChange),
+						instance.get('container').delegate('click', instance._handleToolbarClick, SELECTOR_REPEAT_BUTTONS, instance)
+					);
+
+					instance.renderRepeatable();
+				}
+			},
+
+			getRepeatedSiblings: function() {
+				var instance = this;
+
+				var parent = instance.get('parent');
+
+				return _.filter(
+					parent.get('fields'),
+					function(item) {
+						return item.get('name') === instance.get('name');
+					}
+				);
+			},
+
+			remove: function() {
+				var instance = this;
+
+				instance.destroy();
+
+				_.each(
+					instance.getRepeatedSiblings(),
+					A.bind('_syncRepeatableField', instance)
+				);
+			},
+
+			renderRepeatable: function() {
+				var instance = this;
+
+				instance.renderRepeatableUI();
+				instance.syncRepeatablelUI();
+			},
+
+			renderRepeatableUI: function() {
+				var instance = this;
+
+				var container = instance.get('container');
+
+				container.append(TPL_REPEATABLE_TOOLBAR);
+			},
+
+			repeat: function() {
+				var instance = this;
+
+				var type = instance.get('type');
+
+				var parent = instance.get('parent');
+
+				var fieldClass = Util.getFieldClass(type);
+
+				var field = new fieldClass(
+					{
+						label: instance.get('label'),
+						name: instance.get('name'),
+						parent: parent,
+						portletNamespace: instance.get('portletNamespace'),
+						repeatable: instance.get('repeatable'),
+						type: type
+					}
+				);
+
+				parent.insert(parent.indexOf(instance), field);
+
+				instance.get('container').insert(field.get('container'), 'after');
+
+				_.each(
+					instance.getRepeatedSiblings(),
+					A.bind('_syncRepeatableField', instance)
+				);
+
+				return field;
+			},
+
+			syncRepeatablelUI: function() {
+				var instance = this;
+
+				var container = instance.get('container');
+
+				container.one('.lfr-ddm-form-field-repeatable-delete-button').toggle(instance.get('repeatedIndex') > 0);
+			},
+
+			_afterRepeatableIndexChange: function() {
+				var instance = this;
+
+				instance.render();
+			},
+
+			_handleToolbarClick: function(event) {
+				var instance = this;
+
+				var currentTarget = event.currentTarget;
+
+				if (currentTarget.hasClass('lfr-ddm-form-field-repeatable-delete-button')) {
+					instance.remove();
+				}
+				else {
+					instance.repeat();
+				}
+
+				event.stopPropagation();
+			},
+
+			_syncRepeatableField: function(field) {
+				var instance = this;
+
+				var repeatedSiblings = instance.getRepeatedSiblings();
+
+				field.set('repeatedIndex', _.indexOf(repeatedSiblings, field));
+			}
+		};
+
+		Liferay.namespace('DDM.Renderer').FieldRepetitionSupport = FieldRepetitionSupport;
+	},
+	'',
+	{
+		requires: ['liferay-ddm-form-renderer-field-types', 'liferay-ddm-form-renderer-util']
+	}
+);
