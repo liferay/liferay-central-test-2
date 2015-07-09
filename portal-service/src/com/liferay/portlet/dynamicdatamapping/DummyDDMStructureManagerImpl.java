6983999da95b2114cdb2e6a8711263825185c00b
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-07-09 20:29:25 -0300

    LPS-56995 Create dummy impl

---
+++ portal-service/src/com/liferay/portlet/dynamicdatamapping/DummyDDMStructureManagerImpl.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.dynamicdatamapping;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.service.ServiceContext;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Marcellus Tavares
+ */
+public class DummyDDMStructureManagerImpl implements DDMStructureManager {
+
+	@Override
+	public DDMStructure addStructure(
+			long userId, long groupId, String parentStructureKey,
+			long classNameId, String structureKey, Map<Locale, String> nameMap,
+			Map<Locale, String> descriptionMap, DDMForm ddmForm,
+			DDMFormLayout ddmFormLayout, String storageType, int type,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		return null;
+	}
+
+	@Override
+	public void deleteStructure(long structureId) throws PortalException {
+	}
+
+	@Override
+	public DDMStructure fetchStructure(
+		long groupId, long classNameId, String structureKey) {
+
+		return null;
+	}
+
+	@Override
+	public DDMStructure fetchStructureByUuidAndGroupId(
+		String uuid, long groupId) {
+
+		return null;
+	}
+
+	@Override
+	public List<DDMStructure> getClassStructures(
+		long companyId, long classNameId, int start, int end) {
+
+		return null;
+	}
+
+	@Override
+	public DDMStructure getStructure(long structureId) throws PortalException {
+		return null;
+	}
+
+	@Override
+	public DDMStructure getStructure(
+			long groupId, long classNameId, String structureKey)
+		throws PortalException {
+
+		return null;
+	}
+
+	@Override
+	public DDMStructure getStructureByUuidAndGroupId(String uuid, long groupId)
+		throws PortalException {
+
+		return null;
+	}
+
+	@Override
+	public DDMStructure updateStructure(
+			long userId, long structureId, long parentStructureId,
+			Map<Locale, String> nameMap, Map<Locale, String> descriptionMap,
+			DDMForm ddmForm, DDMFormLayout ddmFormLayout,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		return null;
+	}
+
+}
