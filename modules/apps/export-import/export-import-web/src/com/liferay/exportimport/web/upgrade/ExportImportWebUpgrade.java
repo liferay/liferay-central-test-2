022662d1292b61eac4a48846f04849a1c850d3e8
Daniel Kocsis <daniel.kocsis@liferay.com>
2015-07-09 16:25:41 +0200

    LPS-57048 Web upgrade

---
+++ modules/apps/export-import/export-import-web/src/com/liferay/exportimport/web/upgrade/ExportImportWebUpgrade.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.exportimport.web.upgrade;
+
+import com.liferay.exportimport.web.upgrade.v1_0_0.UpgradeExportImportPortletId;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.service.ReleaseLocalService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Daniel Kocsis
+ */
+@Component(immediate = true, service = ExportImportWebUpgrade.class)
+public class ExportImportWebUpgrade {
+
+	@Reference(unbind = "-")
+	protected void setReleaseLocalService(
+		ReleaseLocalService releaseLocalService) {
+
+		_releaseLocalService = releaseLocalService;
+	}
+
+	@Reference(target = "(original.bean=*)", unbind = "-")
+	protected void setServletContext(ServletContext servletContext) {
+	}
+
+	@Activate
+	protected void upgrade() throws PortalException {
+		List<UpgradeProcess> upgradeProcesses = new ArrayList<>();
+
+		upgradeProcesses.add(new UpgradeExportImportPortletId());
+
+		_releaseLocalService.updateRelease(
+			"com.liferay.exportimport.web", upgradeProcesses, 1, 1, false);
+	}
+
+	private ReleaseLocalService _releaseLocalService;
+
+}
