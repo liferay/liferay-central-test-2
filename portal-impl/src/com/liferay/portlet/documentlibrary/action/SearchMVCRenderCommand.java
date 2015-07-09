5799799b47422ce977a7164bbb55cd9d00ab9273
Brian Chan <brian.chan@liferay.com>
2015-07-09 15:22:43 -0700

    LPS-56244 SF

--- portal-impl/src/com/liferay/portlet/documentlibrary/action/SearchMVCRenderCommand.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/action/SearchMVCRenderCommand.java
@@ -18,7 +18,6 @@ import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
 import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
 import com.liferay.portal.util.PortletKeys;
 
-import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
