55c9ab5f442a42f5e5ce2fe29569fbfc1dc1e60c
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-07-10 09:36:36 +0200

    LPS-57052 Register portlet

---
+++ modules/apps/control-menu/control-menu-web/src/com/liferay/control/menu/web/portlet/ControlMenuPortlet.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.control.menu.web.portlet;
+
+import com.liferay.control.menu.web.constants.ControlMenuPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.hidden",
+		"com.liferay.portlet.header-portlet-javascript=/js/modules.js",
+		"com.liferay.portlet.instanceable=false",
+		"com.liferay.portlet.use-default-template=false",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + ControlMenuPortletKeys.CONTROL_MENU,
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
+public class ControlMenuPortlet extends MVCPortlet {
+}
