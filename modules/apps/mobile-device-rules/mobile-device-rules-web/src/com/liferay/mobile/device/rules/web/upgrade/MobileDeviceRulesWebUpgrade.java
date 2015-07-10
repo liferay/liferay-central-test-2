7d286a6be6e7591fdd15640e545a4124324512f8
Máté Thurzó <mate.thurzo@liferay.com>
2015-07-10 16:45:58 +0200

    LPS-57210 Web upgrade

---
+++ modules/apps/mobile-device-rules/mobile-device-rules-web/src/com/liferay/mobile/device/rules/web/upgrade/MobileDeviceRulesWebUpgrade.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mobile.device.rules.web.upgrade;
+
+import com.liferay.mobile.device.rules.web.upgrade.v1_0_0.UpgradeMobileDeviceRulesPortletId;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.service.ReleaseLocalService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Mate Thurzo
+ */
+@Component(immediate = true, service = MobileDeviceRulesWebUpgrade.class)
+public class MobileDeviceRulesWebUpgrade {
+
+	@Reference(unbind = "-")
+	protected void setReleaseLocalService(
+		ReleaseLocalService releaseLocalService) {
+
+		_releaseLocalService = releaseLocalService;
+	}
+
+	@Reference(target = "(original.bean=*)", unbind = "-")
+	protected void setServletContext(ServletContext servletContext) {
+	}
+
+	@Activate
+	protected void upgrade() throws PortalException {
+		List<UpgradeProcess> upgradeProcesses = new ArrayList<>();
+
+		upgradeProcesses.add(new UpgradeMobileDeviceRulesPortletId());
+
+		_releaseLocalService.updateRelease(
+			"com.liferay.mobile.device.rules.web", upgradeProcesses, 1, 1,
+			false);
+	}
+
+	private ReleaseLocalService _releaseLocalService;
+
+}
