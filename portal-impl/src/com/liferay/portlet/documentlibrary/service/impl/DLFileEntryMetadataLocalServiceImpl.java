0e02e6d0ff0b18983a99868629e25dd0beace037
Rafael Praxedes <rafael.praxedes@liferay.com>
2015-07-10 10:37:11 -0300

    LPS-57001 SF

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryMetadataLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryMetadataLocalServiceImpl.java
@@ -36,6 +36,28 @@ public class DLFileEntryMetadataLocalServiceImpl
 	extends DLFileEntryMetadataLocalServiceBaseImpl {
 
 	@Override
+	public void deleteFileEntryMetadata(DLFileEntryMetadata fileEntryMetadata)
+		throws PortalException {
+
+		// FileEntry metadata
+
+		dlFileEntryMetadataPersistence.remove(fileEntryMetadata);
+
+		// Dynamic data mapping storage
+
+		StorageEngineManagerUtil.deleteByClass(
+			fileEntryMetadata.getDDMStorageId());
+
+		// Dynamic data mapping structure link
+
+		long classNameId = classNameLocalService.getClassNameId(
+			DLFileEntryMetadata.class);
+
+		ddmStructureLinkLocalService.deleteStructureLinks(
+			classNameId, fileEntryMetadata.getFileEntryMetadataId());
+	}
+
+	@Override
 	public void deleteFileEntryMetadata(long fileEntryId)
 		throws PortalException {
 
@@ -164,29 +186,6 @@ public class DLFileEntryMetadataLocalServiceImpl
 		updateFileEntryMetadata(
 			fileEntryType.getCompanyId(), ddmStructures, fileEntryId,
 			fileVersionId, ddmFormValuesMap, serviceContext);
-	}
-
-	@Override
-	public void deleteFileEntryMetadata(
-			DLFileEntryMetadata fileEntryMetadata)
-		throws PortalException {
-
-		// FileEntry metadata
-
-		dlFileEntryMetadataPersistence.remove(fileEntryMetadata);
-
-		// Dynamic data mapping storage
-
-		StorageEngineManagerUtil.deleteByClass(
-			fileEntryMetadata.getDDMStorageId());
-
-		// Dynamic data mapping structure link
-
-		long classNameId = classNameLocalService.getClassNameId(
-			DLFileEntryMetadata.class);
-
-		ddmStructureLinkLocalService.deleteStructureLinks(
-			classNameId, fileEntryMetadata.getFileEntryMetadataId());
 	}
 
 	protected void updateFileEntryMetadata(
