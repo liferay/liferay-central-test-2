19e0c8681b7c69343eaa4c7459bbff332c062dd8
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-07-10 13:43:19 +0200

    LPS-57052 Removes no needed code

--- portal-impl/src/com/liferay/portlet/dockbar/action/ViewAction.java
+++ portal-impl/src/com/liferay/portlet/dockbar/action/ViewAction.java
@@ -17,12 +17,8 @@ package com.liferay.portlet.dockbar.action;
 import com.liferay.portal.struts.PortletAction;
 
 import javax.portlet.PortletConfig;
-import javax.portlet.PortletContext;
-import javax.portlet.PortletRequestDispatcher;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
-import javax.portlet.ResourceRequest;
-import javax.portlet.ResourceResponse;
 
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
@@ -41,22 +37,6 @@ public class ViewAction extends PortletAction {
 		throws Exception {
 
 		return actionMapping.findForward("portlet.dockbar.view");
-	}
-
-	@Override
-	public void serveResource(
-			ActionMapping actionMapping, ActionForm actionForm,
-			PortletConfig portletConfig, ResourceRequest resourceRequest,
-			ResourceResponse resourceResponse)
-		throws Exception {
-
-		PortletContext portletContext = portletConfig.getPortletContext();
-
-		PortletRequestDispatcher portletRequestDispatcher =
-			portletContext.getRequestDispatcher(
-				"/html/portlet/dockbar/view_resources.jsp");
-
-		portletRequestDispatcher.include(resourceRequest, resourceResponse);
 	}
 
 }
