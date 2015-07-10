3d21aad2c8adc564c40877a6b8d70e35681afdfd
Michael Han <michael.han@liferay.com>
2015-07-09 20:20:59 -0500

    LPS-56971 add factory for creating field specific queries

---
+++ modules/portal/portal-search/src/com/liferay/portal/search/internal/query/FieldQueryFactoryImpl.java
@@ -0,0 +1,154 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.internal.query;
+
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.Query;
+import com.liferay.portal.kernel.search.generic.BooleanQueryImpl;
+import com.liferay.portal.kernel.search.generic.MatchQuery;
+import com.liferay.portal.kernel.search.generic.QueryTermImpl;
+import com.liferay.portal.kernel.search.generic.WildcardQueryImpl;
+import com.liferay.portal.kernel.search.query.FieldQueryFactory;
+import com.liferay.portal.kernel.search.query.QueryPreProcessConfiguration;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.search.analysis.KeywordTokenizer;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+
+/**
+ * @author Michael C. Han
+ */
+@Component(immediate = true, service = FieldQueryFactory.class)
+public class FieldQueryFactoryImpl implements FieldQueryFactory {
+
+	@Override
+	public Query createQuery(
+		String field, String value, boolean like, boolean splitKeywords) {
+
+		if (!splitKeywords) {
+			splitKeywords = _keywordTokenizer.requiresTokenization(value);
+		}
+
+		if (splitKeywords) {
+			List<String> tokens = _keywordTokenizer.tokenize(value);
+
+			if (tokens.size() == 1) {
+				return createQuery(field, tokens.get(0), like, false);
+			}
+
+			BooleanQueryImpl booleanQuery = new BooleanQueryImpl();
+
+			for (String token : tokens) {
+				Query query = doCreateQuery(field, token, like);
+
+				booleanQuery.add(query, BooleanClauseOccur.SHOULD);
+			}
+
+			return booleanQuery;
+		}
+
+		return doCreateQuery(field, value, like);
+	}
+
+	protected Query doCreateQuery(String field, String value, boolean like) {
+		boolean isSubstringSearchAlways = false;
+
+		if (_queryPreProcessConfiguration != null) {
+			isSubstringSearchAlways =
+				_queryPreProcessConfiguration.isSubstringSearchAlways(field);
+		}
+
+		boolean phrase = false;
+
+		if (value.startsWith(StringPool.QUOTE) &&
+			value.endsWith(StringPool.QUOTE)) {
+
+			phrase = true;
+		}
+
+		Query query = null;
+
+		if (!phrase && (like || isSubstringSearchAlways)) {
+			value = StringUtil.replace(
+				value, StringPool.PERCENT, StringPool.BLANK);
+
+			if (isSubstringSearchAlways) {
+				if (value.length() == 0) {
+					value = StringPool.STAR;
+				}
+				else {
+					value = StringUtil.toLowerCase(value);
+
+					value = StringPool.STAR + value + StringPool.STAR;
+				}
+			}
+
+			query = new WildcardQueryImpl(new QueryTermImpl(field, value));
+		}
+		else {
+			MatchQuery matchQuery = new MatchQuery(field, value);
+
+			if (value.startsWith(StringPool.QUOTE) &&
+				value.endsWith(StringPool.QUOTE)) {
+
+				value = value.substring(0, value.length() - 2);
+
+				if (value.endsWith(StringPool.STAR)) {
+					matchQuery.setType(MatchQuery.Type.PHRASE_PREFIX);
+				}
+				else {
+					matchQuery.setType(MatchQuery.Type.PHRASE);
+				}
+			}
+
+			query = matchQuery;
+		}
+
+		return query;
+	}
+
+	@Reference(unbind = "-")
+	protected void setKeywordTokenizer(KeywordTokenizer keywordTokenizer) {
+		_keywordTokenizer = keywordTokenizer;
+	}
+
+	@Reference(
+		cardinality = ReferenceCardinality.OPTIONAL,
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY
+	)
+	protected void setQueryPreProcessConfiguration(
+		QueryPreProcessConfiguration queryPreProcessConfiguration) {
+
+		_queryPreProcessConfiguration = queryPreProcessConfiguration;
+	}
+
+	protected void unsetQueryPreProcessConfiguration(
+		QueryPreProcessConfiguration queryPreProcessConfiguration) {
+
+		_queryPreProcessConfiguration = null;
+	}
+
+	private KeywordTokenizer _keywordTokenizer;
+	private QueryPreProcessConfiguration _queryPreProcessConfiguration;
+
+}
---
+++ portal-service/src/com/liferay/portal/kernel/search/query/FieldQueryFactoryUtil.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.search.query;
+
+import com.liferay.portal.kernel.search.Query;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceReference;
+import com.liferay.registry.ServiceTracker;
+import com.liferay.registry.ServiceTrackerCustomizer;
+
+/**
+ * @author Michael C. Han
+ */
+public class FieldQueryFactoryUtil {
+
+	public static Query createQuery(
+		String field, String value, boolean like, boolean splitKeywords) {
+
+		if (_instance._fieldQueryFactory == null) {
+			throw new IllegalStateException("Uninitialized FieldQueryFactory");
+		}
+
+		return _instance._fieldQueryFactory.createQuery(
+			field, value, like, splitKeywords);
+	}
+
+	private FieldQueryFactoryUtil() {
+		Registry registry = RegistryUtil.getRegistry();
+
+		_serviceTracker = registry.trackServices(
+			FieldQueryFactory.class,
+			new FieldQueryFactoryServiceTrackerCustomizer());
+
+		_serviceTracker.open();
+	}
+
+	private static final FieldQueryFactoryUtil _instance =
+		new FieldQueryFactoryUtil();
+
+	private FieldQueryFactory _fieldQueryFactory;
+	private final ServiceTracker<FieldQueryFactory, FieldQueryFactory>
+		_serviceTracker;
+
+	private class FieldQueryFactoryServiceTrackerCustomizer
+		implements ServiceTrackerCustomizer
+			<FieldQueryFactory, FieldQueryFactory> {
+
+		@Override
+		public FieldQueryFactory addingService(
+			ServiceReference<FieldQueryFactory> serviceReference) {
+
+			Registry registry = RegistryUtil.getRegistry();
+
+			_fieldQueryFactory = registry.getService(serviceReference);
+
+			return _fieldQueryFactory;
+		}
+
+		@Override
+		public void modifiedService(
+			ServiceReference<FieldQueryFactory> serviceReference,
+			FieldQueryFactory fieldQueryFactory) {
+		}
+
+		@Override
+		public void removedService(
+			ServiceReference<FieldQueryFactory> serviceReference,
+			FieldQueryFactory fieldQueryFactory) {
+
+			Registry registry = RegistryUtil.getRegistry();
+
+			registry.ungetService(serviceReference);
+
+			_fieldQueryFactory = null;
+		}
+
+	}
+
+}
