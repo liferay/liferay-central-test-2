89195cd7d20f0cdb9312c9f1ca72de77565c3d07
Adolfo Pérez <adolfo.perez@liferay.com>
2015-07-13 18:07:53 +0200

    LPS-56889 Move portlet config into annotation

---
+++ modules/apps/comments/comments-page-comments-web/src/com/liferay/page/comments/web/portlet/PageCommentsPortlet.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.page.comments.web.portlet;
+
+import com.liferay.page.comments.web.constants.PageCommentsPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.css-class-wrapper=portlet-page-comments",
+		"com.liferay.portlet.display-category=category.community",
+		"com.liferay.portlet.icon=/icons/page_comments.png",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.use-default-template=false",
+		"javax.portlet.display-name=Page Comments",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + PageCommentsPortletKeys.PAGE_COMMENTS,
+		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
+public class PageCommentsPortlet extends MVCPortlet {
+}
