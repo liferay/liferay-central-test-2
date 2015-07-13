1e1db9f6a5fe3c973566d60d97b0f3be0948a31f
Brian Chan <brian.chan@liferay.com>
2015-07-13 12:25:22 -0700

    LPS-56863 still modular, but don't duplicate code

--- modules/apps/portal-instances/portal-instances-web/src/com/liferay/portal/instances/web/permission/OmniadminControlPanelEntry.java
+++ modules/apps/portal-instances/portal-instances-web/src/com/liferay/portal/instances/web/permission/OmniadminControlPanelEntry.java
@@ -15,47 +15,17 @@
 package com.liferay.portal.instances.web.permission;
 
 import com.liferay.portal.instances.web.constants.PortalInstancesPortletKeys;
-import com.liferay.portal.model.Group;
-import com.liferay.portal.model.Portlet;
-import com.liferay.portal.security.permission.PermissionChecker;
-import com.liferay.portlet.BaseControlPanelEntry;
 import com.liferay.portlet.ControlPanelEntry;
 
 import org.osgi.service.component.annotations.Component;
 
 /**
- * Represents the omni administrator control panel entry for portlets that shall
- * only be visible and accessible to the omni administrator. In a portlet's
- * <code>liferay-portlet.xml</code> file, this class can be specified as the
- * value for the <a
- * href="http://docs.liferay.com/portal/6.2/definitions/liferay-portlet-app_6_2_0.dtd.html#control-panel-entry-class"
- * > control-panel-entry-class </a> element.
- *
- * @author Jorge Ferrer
+ * @author Pei-Jung Lan
  */
 @Component(
 	property = {"javax.portlet.name=" + PortalInstancesPortletKeys.PORTAL_INSTANCES},
 	service = ControlPanelEntry.class
 )
-public class OmniadminControlPanelEntry extends BaseControlPanelEntry {
-
-	/**
-	 * Returns <code>true</code> if the current user is an omni administrator.
-	 *
-	 * @param  permissionChecker the permission checker referencing the current
-	 *         user
-	 * @param  group the group
-	 * @param  portlet the portlet being checked
-	 * @return <code>true</code> if the current user is an omni administrator;
-	 *         <code>false</code> otherwise
-	 * @throws Exception if an exception occurred
-	 */
-	@Override
-	public boolean hasAccessPermission(
-			PermissionChecker permissionChecker, Group group, Portlet portlet)
-		throws Exception {
-
-		return permissionChecker.isOmniadmin();
-	}
-
+public class OmniadminControlPanelEntry
+	extends com.liferay.portlet.OmniadminControlPanelEntry {
 }
