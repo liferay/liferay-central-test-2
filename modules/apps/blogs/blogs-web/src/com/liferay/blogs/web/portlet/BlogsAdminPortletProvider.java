8bdbf51103d501a43ac53a3d64ebc47c578b3d41
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-07-13 10:40:49 +0200

    LPS-56703 Rename

--- modules/apps/blogs/blogs-web/src/com/liferay/blogs/web/portlet/BlogsAdminPortletProvider.java
+++ modules/apps/blogs/blogs-web/src/com/liferay/blogs/web/portlet/BlogsAdminPortletProvider.java
@@ -33,7 +33,7 @@ import org.osgi.service.component.annotations.Component;
 		ViewPortletProvider.class
 	}
 )
-public class BlogsAdminAdminPortletProvider
+public class BlogsAdminPortletProvider
 	extends BasePortletProvider
 	implements EditPortletProvider, ManagePortletProvider, ViewPortletProvider {
 
