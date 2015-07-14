1729f3785a10520b16a3e3ebbc941e76fa38be56
Bruno Basto <bruno.basto@liferay.com>
2015-07-14 18:17:32 -0300

    LPS-57106 Regen

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-radio/src/META-INF/resources/radio.soy.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-radio/src/META-INF/resources/radio.soy.js
@@ -5,13 +5,13 @@ if (typeof ddm == 'undefined') { var ddm = {}; }
 
 
 ddm.radio = function(opt_data, opt_ignored) {
-var output = '\t<div class="form-group field-wrapper' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + '" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '"><span class="control-label">' + soy.$$escapeHtml(opt_data.label) + '</span>';
-var optionList10 = opt_data.options;
-var optionListLen10 = optionList10.length;
-for (var optionIndex10 = 0; optionIndex10 < optionListLen10; optionIndex10++) {
-	var optionData10 = optionList10[optionIndex10];
-	output += '<div class="radio"><label for="' + soy.$$escapeHtml(optionData10.value) + '"><input class="field" dir="' + soy.$$escapeHtml(opt_data.dir) + '" id="' + soy.$$escapeHtml(optionData10.value) + '" name="' + soy.$$escapeHtml(opt_data.name) + '" ' + soy.$$escapeHtml(optionData10.status) + ' type="radio" value="' + soy.$$escapeHtml(optionData10.value) + '" />' + soy.$$escapeHtml(optionData10.label) + '</label></div>';
-}
-output += soy.$$filterNoAutoescape(opt_data.childElementsHTML) + '</div>';
-return output;
+  var output = '\t<div class="form-group field-wrapper' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + '" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '"><span class="control-label">' + soy.$$escapeHtml(opt_data.label) + ((opt_data.required) ? '<b>&nbsp;*</b>' : '') + '</span>';
+  var optionList13 = opt_data.options;
+  var optionListLen13 = optionList13.length;
+  for (var optionIndex13 = 0; optionIndex13 < optionListLen13; optionIndex13++) {
+    var optionData13 = optionList13[optionIndex13];
+    output += '<div class="radio"><label for="' + soy.$$escapeHtml(optionData13.value) + '"><input class="field" dir="' + soy.$$escapeHtml(opt_data.dir) + '" id="' + soy.$$escapeHtml(optionData13.value) + '" name="' + soy.$$escapeHtml(opt_data.name) + '" ' + soy.$$escapeHtml(optionData13.status) + ' type="radio" value="' + soy.$$escapeHtml(optionData13.value) + '" />' + soy.$$escapeHtml(optionData13.label) + '</label></div>';
+  }
+  output += soy.$$filterNoAutoescape(opt_data.childElementsHTML) + '</div>';
+  return output;
 };
