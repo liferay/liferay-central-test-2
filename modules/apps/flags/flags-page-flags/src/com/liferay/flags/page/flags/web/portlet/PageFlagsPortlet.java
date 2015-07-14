78a0d8864ea187c2b16d33109ef510273718c293
Adolfo Pérez <adolfo.perez@liferay.com>
2015-07-14 18:04:03 +0200

    LPS-56893 Add resource bundle to portlet

--- modules/apps/flags/flags-page-flags/src/com/liferay/flags/page/flags/web/portlet/PageFlagsPortlet.java
+++ modules/apps/flags/flags-page-flags/src/com/liferay/flags/page/flags/web/portlet/PageFlagsPortlet.java
@@ -41,6 +41,7 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + PageFlagsPortletKeys.PAGE_FLAGS,
+		"javax.portlet.resource-bundle=content.Language",
 		"javax.portlet.security-role-ref=guest,power-user,user",
 		"javax.portlet.supports.mime-type=text/html"
 	},
