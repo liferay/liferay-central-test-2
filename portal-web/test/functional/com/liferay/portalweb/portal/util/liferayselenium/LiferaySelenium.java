400b6635cf48a6b0bfd168a7dd79f3fc0f5d7993
Brian Chiu <brian.chiu@liferay.com>
2015-07-14 14:30:49 -0700

    LRQA-16707 Unneeded because isEditable is already part of Selenium interface

--- portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/LiferaySelenium.java
+++ portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/LiferaySelenium.java
@@ -148,8 +148,6 @@ public interface LiferaySelenium extends Selenium {
 
 	public boolean isConfirmation(String pattern);
 
-	public boolean isEditable(String locator);
-
 	public boolean isElementNotPresent(String locator);
 
 	public boolean isElementPresentAfterWait(String locator) throws Exception;
