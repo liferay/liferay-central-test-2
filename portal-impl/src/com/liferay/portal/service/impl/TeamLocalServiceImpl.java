48df34b56c1c17651dae26650e878415a646445a
Máté Thurzó <mate.thurzo@liferay.com>
2015-07-14 10:45:44 +0200

    LPS-56988 SF

--- portal-impl/src/com/liferay/portal/service/impl/TeamLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/TeamLocalServiceImpl.java
@@ -39,8 +39,8 @@ public class TeamLocalServiceImpl extends TeamLocalServiceBaseImpl {
 
 	/**
 	 * @throws     PortalException
-	 * @deprecated As of 7.0.0, replaced by {@link
-	 *             #addTeam(long,long,String,String,ServiceContext)}
+	 * @deprecated As of 7.0.0, replaced by {@link #addTeam(long, long, String,
+	 *             String, ServiceContext)}
 	 */
 	@Deprecated
 	@Override
