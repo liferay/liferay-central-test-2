60a421f0ef550e8d52749f9cb734b648fe8c60e9
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-07-14 19:12:53 -0300

    LPS-57124 Unit test

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-checkbox/test/unit/com/liferay/dynamic/data/mapping/type/checkbox/CheckboxDDMFormFieldTypeSettingsTest.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.type.checkbox;
+
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portlet.dynamicdatamapping.model.DDMForm;
+import com.liferay.portlet.dynamicdatamapping.model.DDMFormField;
+import com.liferay.portlet.dynamicdatamapping.registry.DDMFormFactory;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mock;
+
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ * @author Marcellus Tavares
+ */
+@RunWith(PowerMockRunner.class)
+public class CheckboxDDMFormFieldTypeSettingsTest extends PowerMockito {
+
+	@Before
+	public void setUp() {
+		setUpLanguageUtil();
+	}
+
+	@Test
+	public void testCreateCheckboxDDMFormFieldTypeSettingsDDMForm() {
+		DDMForm ddmForm = DDMFormFactory.create(
+			CheckboxDDMFormFieldTypeSettings.class);
+
+		Map<String, DDMFormField> ddmFormFieldsMap =
+			ddmForm.getDDMFormFieldsMap(false);
+
+		DDMFormField predefinedValueDDMFormField = ddmFormFieldsMap.get(
+			"predefinedValue");
+
+		Assert.assertNotNull(predefinedValueDDMFormField);
+		Assert.assertEquals(
+			"boolean", predefinedValueDDMFormField.getDataType());
+		Assert.assertEquals("checkbox", predefinedValueDDMFormField.getType());
+		Assert.assertEquals(true, predefinedValueDDMFormField.isLocalizable());
+	}
+
+	protected void setUpLanguageUtil() {
+		Set<Locale> availableLocales = SetUtil.fromArray(
+			new Locale[] {LocaleUtil.US});
+
+		when(
+			_language.getAvailableLocales()
+		).thenReturn(
+			availableLocales
+		);
+
+		LanguageUtil languageUtil = new LanguageUtil();
+
+		languageUtil.setLanguage(_language);
+	}
+
+	@Mock
+	protected Language _language;
+
+}
