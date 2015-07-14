d1fb129361cc67b78c3a92b64a4d9b8425c36867
Brian Chan <brian.chan@liferay.com>
2015-07-14 15:06:02 -0700

    LPS-56889 Forgot to modularize language

--- modules/apps/comments/comments-page-comments-web/src/com/liferay/comments/page/comments/web/portlet/PageCommentsPortlet.java
+++ modules/apps/comments/comments-page-comments-web/src/com/liferay/comments/page/comments/web/portlet/PageCommentsPortlet.java
@@ -42,6 +42,7 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + PageCommentsPortletKeys.PAGE_COMMENTS,
+		"javax.portlet.resource-bundle=content.Language",
 		"javax.portlet.security-role-ref=power-user,user",
 		"javax.portlet.supports.mime-type=text/html"
 	},
