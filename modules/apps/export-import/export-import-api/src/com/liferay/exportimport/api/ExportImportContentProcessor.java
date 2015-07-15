cb4f7c938980e903f0af4a61511f3ee1009d9cdf
Gergely Mathe <gergely.mathe@liferay.com>
2015-07-15 16:08:20 +0200

    LPS-57247 Add service interface for ExportImportContentProcessor

---
+++ modules/apps/export-import/export-import-api/src/com/liferay/exportimport/api/ExportImportContentProcessor.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.exportimport.api;
+
+import com.liferay.portal.model.StagedModel;
+import com.liferay.portlet.exportimport.lar.PortletDataContext;
+
+/**
+ * @author Gergely Mathe
+ */
+public interface ExportImportContentProcessor {
+
+	public String replaceExportContentReferences(
+			PortletDataContext portletDataContext,
+			StagedModel entityStagedModel, String content,
+			boolean exportReferencedContent, boolean escapeContent)
+		throws Exception;
+
+	public String replaceImportContentReferences(
+			PortletDataContext portletDataContext,
+			StagedModel entityStagedModel, String content)
+		throws Exception;
+
+}
