0c0bcbc5a806e1f91e91e6c7de9f91bda974ccfa
Victor Ware <victor.ware@liferay.com>
2015-07-16 14:18:37 -0700

    LRQA-16871 Rename "typeCKEditor" to "typeAlloyEditor"

--- portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/WebDriverHelper.java
+++ portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/WebDriverHelper.java
@@ -528,7 +528,7 @@ public class WebDriverHelper {
 		webElement.sendKeys(value);
 	}
 
-	public static void typeCKEditor(
+	public static void typeAlloyEditor(
 		WebDriver webDriver, String locator, String value) {
 
 		WebElement webElement = getWebElement(webDriver, locator);
