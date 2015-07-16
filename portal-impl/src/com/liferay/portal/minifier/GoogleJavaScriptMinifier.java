ccf99400627e6a106c0772d34cec71c684786c04
michael.bradford@liferay.com <michael.bradford@liferay.com>
2015-07-16 11:46:27 -0700

    LPS-57169 - Set GoogleJavaScriptMinifier to ES5

--- portal-impl/src/com/liferay/portal/minifier/GoogleJavaScriptMinifier.java
+++ portal-impl/src/com/liferay/portal/minifier/GoogleJavaScriptMinifier.java
@@ -18,6 +18,7 @@ import com.google.javascript.jscomp.BasicErrorManager;
 import com.google.javascript.jscomp.CheckLevel;
 import com.google.javascript.jscomp.Compiler;
 import com.google.javascript.jscomp.CompilerOptions;
+import com.google.javascript.jscomp.CompilerOptions.LanguageMode;
 import com.google.javascript.jscomp.DiagnosticGroups;
 import com.google.javascript.jscomp.JSError;
 import com.google.javascript.jscomp.MessageFormatter;
@@ -43,6 +44,8 @@ public class GoogleJavaScriptMinifier implements JavaScriptMinifier {
 		SourceFile sourceFile = SourceFile.fromCode(resourceName, content);
 
 		CompilerOptions compilerOptions = new CompilerOptions();
+
+		compilerOptions.setLanguageIn(LanguageMode.ECMASCRIPT5);
 
 		compilerOptions.setWarningLevel(
 			DiagnosticGroups.NON_STANDARD_JSDOC, CheckLevel.OFF);
