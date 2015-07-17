0c2749d41c7d36343e37a0175c7b1e042bc5a802
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-07-17 13:51:29 +0200

    LPS-56703 Move document library admin portlet to OSGi module and apply definitions

---
+++ modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/portlet/DLAdminPortlet.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.util.PortletKeys;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Sergio González
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.control-panel-entry-category=site_administration.content",
+		"com.liferay.portlet.control-panel-entry-weight=3.0",
+		"com.liferay.portlet.css-class-wrapper=portlet-document-library",
+		"com.liferay.portlet.display-category=category.hidden",
+		"com.liferay.portlet.footer-portlet-javascript=/o/ddm-web/js/main.js",
+		"com.liferay.portlet.footer-portlet-javascript=/o/ddm-web/js/custom_fields.js",
+		"com.liferay.portlet.footer-portlet-javascript=/html/portlet/document_library/js/main.js",
+		"com.liferay.portlet.footer-portlet-javascript=/html/portlet/document_library/js/upload.js",
+		"com.liferay.portlet.header-portlet-css=/html/portlet/document_library/css/main.css",
+		"com.liferay.portlet.header-portlet-css=/o/ddm-web/css/main.css",
+		"com.liferay.portlet.icon=/document_library/icons/document_library.png",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.preferences-unique-per-layout=false",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.scopeable=true",
+		"com.liferay.portlet.use-default-template=true",
+		"com.liferay.portlet.webdav-storage-token=document_library",
+		"javax.portlet.display-name=Documents and Media Admin",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/document_library_admin/view.jsp",
+		"javax.portlet.name=" + PortletKeys.DOCUMENT_LIBRARY_ADMIN,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
+public class DLAdminPortlet extends MVCPortlet {
+}
