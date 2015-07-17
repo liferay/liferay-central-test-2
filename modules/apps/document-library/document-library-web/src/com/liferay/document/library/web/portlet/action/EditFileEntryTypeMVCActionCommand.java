09583750b3f4b610b176bab587d00d12be532851
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-07-17 14:46:45 +0200

    LPS-56703 Move Command classes to OSGi module and convert them to OSGi components

---
+++ modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/portlet/action/EditFileEntryTypeMVCActionCommand.java
@@ -0,0 +1,235 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.portlet.action;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.servlet.SessionMessages;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.security.auth.PrincipalException;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceContextFactory;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portal.util.WebKeys;
+import com.liferay.portlet.documentlibrary.DuplicateFileEntryTypeException;
+import com.liferay.portlet.documentlibrary.NoSuchFileEntryTypeException;
+import com.liferay.portlet.documentlibrary.NoSuchMetadataSetException;
+import com.liferay.portlet.documentlibrary.model.DLFileEntryType;
+import com.liferay.portlet.documentlibrary.service.DLAppServiceUtil;
+import com.liferay.portlet.documentlibrary.service.DLFileEntryTypeServiceUtil;
+import com.liferay.portlet.dynamicdatamapping.DDMStructureManagerUtil;
+import com.liferay.portlet.dynamicdatamapping.NoSuchStructureException;
+import com.liferay.portlet.dynamicdatamapping.RequiredStructureException;
+import com.liferay.portlet.dynamicdatamapping.StructureDefinitionException;
+import com.liferay.portlet.dynamicdatamapping.StructureDuplicateElementException;
+import com.liferay.portlet.dynamicdatamapping.StructureNameException;
+import com.liferay.portlet.dynamicdatamapping.model.DDMForm;
+
+import java.util.Locale;
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Alexander Chow
+ * @author Sergio González
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + PortletKeys.DOCUMENT_LIBRARY,
+		"javax.portlet.name=" + PortletKeys.DOCUMENT_LIBRARY_ADMIN,
+		"javax.portlet.name=" + PortletKeys.DOCUMENT_LIBRARY_DISPLAY,
+		"javax.portlet.name=" + PortletKeys.MEDIA_GALLERY_DISPLAY,
+		"mvc.command.name=/document_library/edit_file_entry_type"
+	},
+	service = MVCActionCommand.class
+)
+public class EditFileEntryTypeMVCActionCommand extends BaseMVCActionCommand {
+
+	protected void deleteFileEntryType(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long fileEntryTypeId = ParamUtil.getLong(
+			actionRequest, "fileEntryTypeId");
+
+		DLFileEntryTypeServiceUtil.deleteFileEntryType(fileEntryTypeId);
+	}
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
+
+		try {
+			if (cmd.equals(Constants.ADD) || cmd.equals(Constants.UPDATE)) {
+				updateFileEntryType(actionRequest, actionResponse);
+			}
+			else if (cmd.equals(Constants.DELETE)) {
+				deleteFileEntryType(actionRequest, actionResponse);
+			}
+			else if (cmd.equals(Constants.SUBSCRIBE)) {
+				subscribeFileEntryType(actionRequest);
+			}
+			else if (cmd.equals(Constants.UNSUBSCRIBE)) {
+				unsubscribeFileEntryType(actionRequest);
+			}
+
+			if (SessionErrors.isEmpty(actionRequest)) {
+				SessionMessages.add(
+					actionRequest,
+					PortalUtil.getPortletId(actionRequest) +
+						SessionMessages.KEY_SUFFIX_REFRESH_PORTLET,
+					PortletKeys.DOCUMENT_LIBRARY);
+
+				String redirect = PortalUtil.escapeRedirect(
+					ParamUtil.getString(actionRequest, "redirect"));
+
+				if (Validator.isNotNull(redirect)) {
+					sendRedirect(actionRequest, actionResponse, redirect);
+				}
+			}
+		}
+		catch (Exception e) {
+			if (e instanceof DuplicateFileEntryTypeException ||
+				e instanceof NoSuchMetadataSetException ||
+				e instanceof StructureDefinitionException ||
+				e instanceof StructureDuplicateElementException ||
+				e instanceof StructureNameException) {
+
+				SessionErrors.add(actionRequest, e.getClass());
+			}
+			else if (e instanceof NoSuchFileEntryTypeException ||
+					 e instanceof NoSuchStructureException ||
+					 e instanceof PrincipalException) {
+
+				SessionErrors.add(actionRequest, e.getClass());
+
+				actionResponse.setRenderParameter(
+					"mvcPath", "/html/portlet/document_library/error.jsp");
+			}
+			else if (e instanceof RequiredStructureException) {
+				SessionErrors.add(actionRequest, e.getClass());
+			}
+			else {
+				throw e;
+			}
+		}
+	}
+
+	protected long[] getLongArray(PortletRequest portletRequest, String name) {
+		String value = portletRequest.getParameter(name);
+
+		if (value == null) {
+			return null;
+		}
+
+		return StringUtil.split(GetterUtil.getString(value), 0L);
+	}
+
+	protected void subscribeFileEntryType(ActionRequest actionRequest)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		long fileEntryTypeId = ParamUtil.getLong(
+			actionRequest, "fileEntryTypeId");
+
+		DLAppServiceUtil.subscribeFileEntryType(
+			themeDisplay.getScopeGroupId(), fileEntryTypeId);
+	}
+
+	protected void unsubscribeFileEntryType(ActionRequest actionRequest)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		long fileEntryTypeId = ParamUtil.getLong(
+			actionRequest, "fileEntryTypeId");
+
+		DLAppServiceUtil.unsubscribeFileEntryType(
+			themeDisplay.getScopeGroupId(), fileEntryTypeId);
+	}
+
+	protected void updateFileEntryType(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		long fileEntryTypeId = ParamUtil.getLong(
+			actionRequest, "fileEntryTypeId");
+
+		Map<Locale, String> nameMap = LocalizationUtil.getLocalizationMap(
+			actionRequest, "name");
+		Map<Locale, String> descriptionMap =
+			LocalizationUtil.getLocalizationMap(actionRequest, "description");
+
+		long[] ddmStructureIds = getLongArray(
+			actionRequest, "ddmStructuresSearchContainerPrimaryKeys");
+
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(
+			DLFileEntryType.class.getName(), actionRequest);
+
+		DDMForm ddmForm = DDMStructureManagerUtil.getDDMForm(actionRequest);
+
+		serviceContext.setAttribute("ddmForm", ddmForm);
+
+		if (fileEntryTypeId <= 0) {
+
+			// Add file entry type
+
+			long groupId = themeDisplay.getScopeGroupId();
+
+			Group scopeGroup = GroupLocalServiceUtil.getGroup(groupId);
+
+			if (scopeGroup.isLayout()) {
+				groupId = scopeGroup.getParentGroupId();
+			}
+
+			DLFileEntryTypeServiceUtil.addFileEntryType(
+				groupId, null, nameMap, descriptionMap, ddmStructureIds,
+				serviceContext);
+		}
+		else {
+
+			// Update file entry type
+
+			DLFileEntryTypeServiceUtil.updateFileEntryType(
+				fileEntryTypeId, nameMap, descriptionMap, ddmStructureIds,
+				serviceContext);
+		}
+	}
+
+}
---
+++ modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/portlet/action/EditFileShortcutMVCActionCommand.java
@@ -0,0 +1,146 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.portlet.action;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.repository.model.FileShortcut;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.model.TrashedModel;
+import com.liferay.portal.security.auth.PrincipalException;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceContextFactory;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portlet.documentlibrary.FileShortcutPermissionException;
+import com.liferay.portlet.documentlibrary.NoSuchFileEntryException;
+import com.liferay.portlet.documentlibrary.NoSuchFileShortcutException;
+import com.liferay.portlet.documentlibrary.model.DLFileShortcutConstants;
+import com.liferay.portlet.documentlibrary.service.DLAppServiceUtil;
+import com.liferay.portlet.trash.util.TrashUtil;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Brian Wing Shun Chan
+ * @author Levente Hudák
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + PortletKeys.DOCUMENT_LIBRARY,
+		"javax.portlet.name=" + PortletKeys.DOCUMENT_LIBRARY_ADMIN,
+		"javax.portlet.name=" + PortletKeys.DOCUMENT_LIBRARY_DISPLAY,
+		"javax.portlet.name=" + PortletKeys.MEDIA_GALLERY_DISPLAY,
+		"mvc.command.name=/document_library/edit_file_shortcut"
+	},
+	service = MVCActionCommand.class
+)
+public class EditFileShortcutMVCActionCommand extends BaseMVCActionCommand {
+
+	protected void deleteFileShortcut(
+			ActionRequest actionRequest, boolean moveToTrash)
+		throws Exception {
+
+		long fileShortcutId = ParamUtil.getLong(
+			actionRequest, "fileShortcutId");
+
+		if (moveToTrash) {
+			FileShortcut fileShortcut =
+				DLAppServiceUtil.moveFileShortcutToTrash(fileShortcutId);
+
+			if (fileShortcut.getModel() instanceof TrashedModel) {
+				TrashUtil.addTrashSessionMessages(
+					actionRequest, (TrashedModel)fileShortcut.getModel());
+			}
+
+			hideDefaultSuccessMessage(actionRequest);
+		}
+		else {
+			DLAppServiceUtil.deleteFileShortcut(fileShortcutId);
+		}
+	}
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
+
+		try {
+			if (cmd.equals(Constants.ADD) || cmd.equals(Constants.UPDATE)) {
+				updateFileShortcut(actionRequest);
+			}
+			else if (cmd.equals(Constants.DELETE)) {
+				deleteFileShortcut(actionRequest, false);
+			}
+			else if (cmd.equals(Constants.MOVE_TO_TRASH)) {
+				deleteFileShortcut(actionRequest, true);
+			}
+		}
+		catch (Exception e) {
+			if (e instanceof NoSuchFileShortcutException ||
+				e instanceof PrincipalException) {
+
+				SessionErrors.add(actionRequest, e.getClass());
+
+				actionResponse.setRenderParameter(
+					"mvcPath", "/html/portlet.document_library/error.jsp");
+			}
+			else if (e instanceof FileShortcutPermissionException ||
+					 e instanceof NoSuchFileEntryException) {
+
+				SessionErrors.add(actionRequest, e.getClass());
+			}
+			else {
+				throw e;
+			}
+		}
+	}
+
+	protected void updateFileShortcut(ActionRequest actionRequest)
+		throws Exception {
+
+		long fileShortcutId = ParamUtil.getLong(
+			actionRequest, "fileShortcutId");
+
+		long repositoryId = ParamUtil.getLong(actionRequest, "repositoryId");
+		long folderId = ParamUtil.getLong(actionRequest, "folderId");
+		long toFileEntryId = ParamUtil.getLong(actionRequest, "toFileEntryId");
+
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(
+			DLFileShortcutConstants.getClassName(), actionRequest);
+
+		if (fileShortcutId <= 0) {
+
+			// Add file shortcut
+
+			DLAppServiceUtil.addFileShortcut(
+				repositoryId, folderId, toFileEntryId, serviceContext);
+		}
+		else {
+
+			// Update file shortcut
+
+			DLAppServiceUtil.updateFileShortcut(
+				fileShortcutId, folderId, toFileEntryId, serviceContext);
+		}
+	}
+
+}
