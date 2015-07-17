110ea90ed3246b21bce9850ba12b286c771a5e6a
Pei-Jung Lan <pei-jung.lan@liferay.com>
2015-07-17 15:37:39 -0700

    LPS-55591 Move custom attributes classes

--- modules/apps/users-admin/users-admin-web/src/com/liferay/users/admin/web/custom/attributes/OrganizationCustomAttributesDisplay.java
+++ modules/apps/users-admin/users-admin-web/src/com/liferay/users/admin/web/custom/attributes/OrganizationCustomAttributesDisplay.java
@@ -12,25 +12,32 @@
  * details.
  */
 
-package com.liferay.portlet.usersadmin;
+package com.liferay.users.admin.web.custom.attributes;
 
-import com.liferay.portal.model.Layout;
+import com.liferay.portal.model.Organization;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portlet.expando.model.BaseCustomAttributesDisplay;
 
 /**
  * @author Jorge Ferrer
  */
-public class LayoutCustomAttributesDisplay extends BaseCustomAttributesDisplay {
+public class OrganizationCustomAttributesDisplay
+	extends BaseCustomAttributesDisplay {
 
 	@Override
 	public String getClassName() {
-		return Layout.class.getName();
+		return Organization.class.getName();
+	}
+
+	@Override
+	public String getIconCssClass() {
+		return "icon-globe";
 	}
 
 	@Override
 	public String getIconPath(ThemeDisplay themeDisplay) {
-		return themeDisplay.getPathThemeImages() + "/common/page.png";
+		return
+			themeDisplay.getPathThemeImages() + "/common/organization_icon.png";
 	}
 
 }
