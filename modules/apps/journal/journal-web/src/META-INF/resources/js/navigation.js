df08766c39bccf5aaf78bf472c3c7b44defd3682
Carlos Lancha <carlos.lancha@liferay.com>
2015-07-17 09:57:14 +0200

    LPS-56932 Fixed merge order

--- modules/apps/journal/journal-web/src/META-INF/resources/js/navigation.js
+++ modules/apps/journal/journal-web/src/META-INF/resources/js/navigation.js
@@ -39,7 +39,6 @@ AUI.add(
 						var portletContainerId = instance.ns('journalContainer');
 
 						var selectConfig = A.merge(
-							config.select,
 							{
 								checkBoxesId: checkBoxesId,
 								displayStyle: displayStyle,
@@ -48,13 +47,13 @@ AUI.add(
 								namespace: namespace,
 								portletContainerId: portletContainerId,
 								selector: 'entry-selector'
-							}
+							},
+							config.select
 						);
 
 						instance._appViewSelect = new Liferay.AppViewSelect(selectConfig);
 
 						var moveConfig = A.merge(
-							config.move,
 							{
 								displayStyleCSSClass: displayStyleCSSClass,
 								draggableCSSClass: '.entry-link',
@@ -69,7 +68,8 @@ AUI.add(
 										instance.ns('folderIds')
 									]
 								}
-							}
+							},
+							config.move
 						);
 
 						instance._appViewMove = new Liferay.AppViewMove(moveConfig);
