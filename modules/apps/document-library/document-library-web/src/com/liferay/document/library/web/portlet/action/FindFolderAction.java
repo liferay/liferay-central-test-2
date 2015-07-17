ea3bbe115e438932546e270f4a26504953fe84ca
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-07-17 14:46:45 +0200

    LPS-56703 Move Command classes to OSGi module and convert them to OSGi components

---
+++ modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/portlet/action/FindFolderAction.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.portlet.action;
+
+import com.liferay.portal.kernel.struts.BaseStrutsAction;
+import com.liferay.portal.kernel.struts.StrutsAction;
+import com.liferay.portal.struts.FindActionHelper;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+@Component(
+	immediate = true,
+	property = {
+		"path=/document_library/find_folder",
+		"path=/image_gallery_display/find_folder"
+	},
+	service = StrutsAction.class
+)
+public class FindFolderAction extends BaseStrutsAction {
+
+	@Override
+	public String execute(
+			HttpServletRequest request, HttpServletResponse response)
+		throws Exception {
+
+		_findActionHelper.execute(request, response);
+
+		return null;
+	}
+
+	private final FindActionHelper _findActionHelper =
+		new DLFolderFindActionHelper();
+
+}
