f82544febab50cecd6842d1d0a1dc819f4ba9ff4
akosthurzo <akos.thurzo@liferay.com>
2015-07-17 16:34:41 +0200

    LPS-57211 Add PortletDataHandler

---
+++ modules/apps/site/site-teams-web/src/com/liferay/site/teams/web/lar/SiteTeamsPortletDataHandler.java
@@ -0,0 +1,113 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.teams.web.lar;
+
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portal.model.Team;
+import com.liferay.portal.service.TeamLocalServiceUtil;
+import com.liferay.portlet.exportimport.lar.BasePortletDataHandler;
+import com.liferay.portlet.exportimport.lar.PortletDataContext;
+import com.liferay.portlet.exportimport.lar.PortletDataHandler;
+import com.liferay.portlet.exportimport.lar.StagedModelDataHandlerUtil;
+import com.liferay.site.teams.web.constants.SiteTeamsPortletKeys;
+
+import java.util.List;
+
+import javax.portlet.PortletPreferences;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Akos Thurzo
+ */
+@Component(
+	property = {"javax.portlet.name=" + SiteTeamsPortletKeys.SITE_TEAMS_ADMIN},
+	service = PortletDataHandler.class
+)
+public class SiteTeamsPortletDataHandler extends BasePortletDataHandler {
+
+	public static final String NAMESPACE = "team";
+
+	@Override
+	protected PortletPreferences doDeleteData(
+			PortletDataContext portletDataContext, String portletId,
+			PortletPreferences portletPreferences)
+		throws Exception {
+
+		if (portletDataContext.addPrimaryKey(
+				SiteTeamsPortletDataHandler.class, "deleteData")) {
+
+			return portletPreferences;
+		}
+
+		TeamLocalServiceUtil.deleteTeams(portletDataContext.getScopeGroupId());
+
+		return portletPreferences;
+	}
+
+	@Override
+	protected String doExportData(
+			final PortletDataContext portletDataContext, String portletId,
+			PortletPreferences portletPreferences)
+		throws Exception {
+
+		Element rootElement = addExportDataRootElement(portletDataContext);
+
+		rootElement.addAttribute(
+			"group-id", String.valueOf(portletDataContext.getScopeGroupId()));
+
+		ActionableDynamicQuery teamActionableDynamicQuery =
+			TeamLocalServiceUtil.getExportActionableDynamicQuery(
+				portletDataContext);
+
+		teamActionableDynamicQuery.performActions();
+
+		return getExportDataRootElementString(rootElement);
+	}
+
+	@Override
+	protected PortletPreferences doImportData(
+			PortletDataContext portletDataContext, String portletId,
+			PortletPreferences portletPreferences, String data)
+		throws Exception {
+
+		Element teamsElement = portletDataContext.getImportDataGroupElement(
+			Team.class);
+
+		List<Element> teamElements = teamsElement.elements();
+
+		for (Element teamElement : teamElements) {
+			StagedModelDataHandlerUtil.importStagedModel(
+				portletDataContext, teamElement);
+		}
+
+		return portletPreferences;
+	}
+
+	@Override
+	protected void doPrepareManifestSummary(
+			PortletDataContext portletDataContext,
+			PortletPreferences portletPreferences)
+		throws Exception {
+
+		ActionableDynamicQuery teamActionableDynamicQuery =
+			TeamLocalServiceUtil.getExportActionableDynamicQuery(
+				portletDataContext);
+
+		teamActionableDynamicQuery.performCount();
+	}
+
+}
