6f5e6b3d7483aab32ecb95d533c04780f872a011
Michael Han <michael.han@liferay.com>
2015-07-17 21:06:09 -0500

    LPS-57227 move to module

--- modules/portal/portal-search/src/com/liferay/portal/search/internal/SearchPermissionCheckerImpl.java
+++ modules/portal/portal-search/src/com/liferay/portal/search/internal/SearchPermissionCheckerImpl.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.search;
+package com.liferay.portal.search.internal;
 
 import com.liferay.portal.NoSuchResourceException;
 import com.liferay.portal.kernel.log.Log;
