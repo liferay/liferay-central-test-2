b90701b8dae92ccba0cce3a62a1cde907ecb5363
Michael Han <michael.han@liferay.com>
2015-07-17 21:17:01 -0500

    LPS-57224 move to module

--- modules/apps/document-library/document-library-repository-search/src/com/liferay/document/library/repository/search/internal/RepositorySearchQueryBuilderImpl.java
+++ modules/apps/document-library/document-library-repository-search/src/com/liferay/document/library/repository/search/internal/RepositorySearchQueryBuilderImpl.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.repository.search;
+package com.liferay.document.library.repository.search.internal;
 
 import com.liferay.portal.kernel.repository.search.RepositorySearchQueryBuilder;
 import com.liferay.portal.kernel.repository.search.RepositorySearchQueryTermBuilder;
