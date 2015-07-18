a339a6f0e93b02f68352b9a60164e6c7610cef91
Máté Thurzó <mate.thurzo@liferay.com>
2015-07-18 14:22:22 +0200

    LPS-53727 Bookmarks upgrade

---
+++ modules/apps/bookmarks/bookmarks-service/src/com/liferay/bookmarks/upgrade/v1_0_0/UpgradeBookmarksLastPublishDate.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.bookmarks.upgrade.v1_0_0;
+
+import com.liferay.bookmarks.constants.BookmarksPortletKeys;
+import com.liferay.portal.upgrade.v7_0_0.UpgradeLastPublishDate;
+
+/**
+ * @author Mate Thurzo
+ */
+public class UpgradeBookmarksLastPublishDate extends UpgradeLastPublishDate {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		runSQL("alter table BookmarksEntry add lastPublishDate DATE null");
+
+		super.updateLastPublishDates(
+			BookmarksPortletKeys.BOOKMARKS, "BookmarksEntry");
+
+		runSQL("alter table BookmarksFolder add lastPublishDate DATE null");
+
+		super.updateLastPublishDates(
+			BookmarksPortletKeys.BOOKMARKS, "BookmarksFolder");
+	}
+
+}
