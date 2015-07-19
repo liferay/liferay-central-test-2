40d42ee778a394fdfe76667967c19b5b12ea538b
Michael Han <michael.han@liferay.com>
2015-07-19 13:25:42 -0500

    LPS-57221 add osgi bindings

---
+++ modules/portal/portal-search/src/com/liferay/portal/search/configuration/QueryPreProcessConfiguration.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+/**
+ * @author Michael C. Han
+ */
+@Meta.OCD(
+	id = "com.liferay.portal.search.configuration.QueryPreProcessConfiguration"
+)
+public interface QueryPreProcessConfiguration {
+
+	@Meta.AD(
+		deflt = "assetCategoryTitles?(_.+)?|assetTagNames|emailAddress|firstName|lastName|license|middleName|path|screenName|tag|treePath|userName",
+		required = false
+	)
+	public String[] fieldNamePatterns();
+
+}
