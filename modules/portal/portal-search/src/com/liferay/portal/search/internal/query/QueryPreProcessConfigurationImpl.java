bc2af0bd6ed6a64ad80e5490d07e4ec25b477fc7
Michael Han <michael.han@liferay.com>
2015-07-19 13:17:21 -0500

    LPS-57221 move to module

--- modules/portal/portal-search/src/com/liferay/portal/search/internal/query/QueryPreProcessConfigurationImpl.java
+++ modules/portal/portal-search/src/com/liferay/portal/search/internal/query/QueryPreProcessConfigurationImpl.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.search.query;
+package com.liferay.portal.search.internal.query;
 
 import com.liferay.portal.kernel.search.query.QueryPreProcessConfiguration;
 
