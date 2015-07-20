647b6fca37b37eacdb9d027f5f7a69946fb1e8b0
Adolfo Pérez <adolfo.perez@liferay.com>
2015-07-20 09:35:53 +0200

    LPS-56047 Make FindThreadAction a StrutsAction

---
+++ portal-impl/src/com/liferay/portlet/messageboards/action/FindThreadActionHelper.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.messageboards.action;
+
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.struts.BaseFindActionHelper;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portlet.messageboards.model.MBThread;
+import com.liferay.portlet.messageboards.service.MBThreadLocalServiceUtil;
+
+import javax.portlet.PortletURL;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class FindThreadActionHelper extends BaseFindActionHelper {
+
+	@Override
+	public long getGroupId(long primaryKey) throws Exception {
+		MBThread thread = MBThreadLocalServiceUtil.getThread(primaryKey);
+
+		return thread.getGroupId();
+	}
+
+	@Override
+	public String getPrimaryKeyParameterName() {
+		return "threadId";
+	}
+
+	@Override
+	public String[] initPortletIds() {
+		return new String[] {
+			PortletKeys.MESSAGE_BOARDS, PortletKeys.MESSAGE_BOARDS_ADMIN
+		};
+	}
+
+	@Override
+	public PortletURL processPortletURL(
+			HttpServletRequest request, PortletURL portletURL)
+		throws Exception {
+
+		long threadId = ParamUtil.getLong(
+			request, getPrimaryKeyParameterName());
+
+		MBThread thread = MBThreadLocalServiceUtil.getThread(threadId);
+
+		portletURL.setParameter(
+			"messageId", String.valueOf(thread.getRootMessageId()));
+
+		return portletURL;
+	}
+
+	@Override
+	public void setPrimaryKeyParameter(PortletURL portletURL, long primaryKey)
+		throws Exception {
+	}
+
+	@Override
+	protected void addRequiredParameters(
+			HttpServletRequest request, String portletId,
+			PortletURL portletURL) {
+
+		portletURL.setParameter(
+			"mvcRenderCommandName", "message_boards/view_message");
+	}
+
+}
