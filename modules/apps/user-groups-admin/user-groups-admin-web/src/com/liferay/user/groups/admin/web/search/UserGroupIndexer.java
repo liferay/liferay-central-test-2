3071c63dc509f1112de4ddfc3e10f86707150847
Brian Chan <brian.chan@liferay.com>
2015-07-20 13:44:45 -0700

    LPS-55583 wrong dir

--- modules/apps/user-groups-admin/user-groups-admin-web/src/com/liferay/user/groups/admin/web/search/UserGroupIndexer.java
+++ modules/apps/user-groups-admin/user-groups-admin-web/src/com/liferay/user/groups/admin/web/search/UserGroupIndexer.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.user.groups.admin.web.util;
+package com.liferay.user.groups.admin.web.search;
 
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.exception.PortalException;
