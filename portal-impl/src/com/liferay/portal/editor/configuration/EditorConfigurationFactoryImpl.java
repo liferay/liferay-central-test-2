160957ccdba44e3d8ee271bbac484d1c02d57e99
Adolfo Pérez <adolfo.perez@liferay.com>
2015-07-20 17:20:59 +0200

    LPS-56228 ant format-source

--- portal-impl/src/com/liferay/portal/editor/configuration/EditorConfigurationFactoryImpl.java
+++ portal-impl/src/com/liferay/portal/editor/configuration/EditorConfigurationFactoryImpl.java
@@ -55,8 +55,8 @@ public class EditorConfigurationFactoryImpl
 
 		if (editorConfigTransformer != null) {
 			editorConfigTransformer.transform(
-				editorOptions, inputEditorTaglibAttributes, configJSONObject, themeDisplay,
-				requestBackedPortletURLFactory);
+				editorOptions, inputEditorTaglibAttributes, configJSONObject,
+				themeDisplay, requestBackedPortletURLFactory);
 		}
 
 		return new EditorConfigurationImpl(configJSONObject, editorOptions);
