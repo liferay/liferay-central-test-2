99094d28915f9e4499dbc1c12c49555c76a7d26f
Rafael Praxedes <rafael.praxedes@liferay.com>
2015-07-20 12:05:30 -0300

    LPS-57117 extract DL integration test that has references to TestOrderHelper

--- modules/apps/document-library/document-library-test/test/integration/com/liferay/document/library/search/test/DLFileEntrySearchTest.java
+++ modules/apps/document-library/document-library-test/test/integration/com/liferay/document/library/search/test/DLFileEntrySearchTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portlet.documentlibrary.search;
+package com.liferay.document.library.search.test;
 
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.repository.model.FileEntry;
