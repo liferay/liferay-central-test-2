006c94b285d9d3d11b8100e9b75b9ae45c951531
Adolfo Pérez <adolfo.perez@liferay.com>
2015-07-20 09:23:56 +0200

    LPS-56047 Make FindMessageAction a StrutsAction

---
+++ portal-impl/src/com/liferay/portlet/messageboards/action/FindMessageActionHelper.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.messageboards.action;
+
+import com.liferay.portal.struts.BaseFindActionHelper;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portlet.messageboards.model.MBMessage;
+import com.liferay.portlet.messageboards.service.MBMessageLocalServiceUtil;
+
+import javax.portlet.PortletURL;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class FindMessageActionHelper extends BaseFindActionHelper {
+
+	@Override
+	public long getGroupId(long primaryKey) throws Exception {
+		MBMessage message = MBMessageLocalServiceUtil.getMessage(primaryKey);
+
+		return message.getGroupId();
+	}
+
+	@Override
+	public String getPrimaryKeyParameterName() {
+		return "messageId";
+	}
+
+	@Override
+	public String[] initPortletIds() {
+
+		// Order is important. See LPS-23770.
+
+		return new String[] {
+			PortletKeys.MESSAGE_BOARDS_ADMIN, PortletKeys.MESSAGE_BOARDS
+		};
+	}
+
+	@Override
+	public PortletURL processPortletURL(
+			HttpServletRequest request, PortletURL portletURL)
+		throws Exception {
+
+		return portletURL;
+	}
+
+	@Override
+	public void setPrimaryKeyParameter(
+			PortletURL portletURL, long primaryKey)
+		throws Exception {
+
+		portletURL.setParameter(
+			getPrimaryKeyParameterName(), String.valueOf(primaryKey));
+	}
+
+	@Override
+	protected void addRequiredParameters(
+		HttpServletRequest request, String portletId, PortletURL portletURL) {
+
+		portletURL.setParameter(
+			"mvcRenderCommandName", "/message_boards/view_message");
+	}
+
+}
