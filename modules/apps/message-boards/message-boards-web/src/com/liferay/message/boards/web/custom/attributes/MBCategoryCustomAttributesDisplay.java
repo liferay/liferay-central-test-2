5e3f062883a7a50e07d376024c77ef9914574dff
Adolfo Pérez <adolfo.perez@liferay.com>
2015-07-21 17:17:32 +0200

    LPS-56048 Move CustomAttributesDisplay into module

--- modules/apps/message-boards/message-boards-web/src/com/liferay/message/boards/web/custom/attributes/MBCategoryCustomAttributesDisplay.java
+++ modules/apps/message-boards/message-boards-web/src/com/liferay/message/boards/web/custom/attributes/MBCategoryCustomAttributesDisplay.java
@@ -12,26 +12,35 @@
  * details.
  */
 
-package com.liferay.portlet.messageboards;
+package com.liferay.message.boards.web.custom.attributes;
 
 import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortletKeys;
 import com.liferay.portlet.expando.model.BaseCustomAttributesDisplay;
-import com.liferay.portlet.messageboards.model.MBMessage;
+import com.liferay.portlet.expando.model.CustomAttributesDisplay;
+import com.liferay.portlet.messageboards.model.MBCategory;
+
+import org.osgi.service.component.annotations.Component;
 
 /**
  * @author Jorge Ferrer
  */
-public class MBMessageCustomAttributesDisplay
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + PortletKeys.MESSAGE_BOARDS},
+	service = CustomAttributesDisplay.class
+)
+public class MBCategoryCustomAttributesDisplay
 	extends BaseCustomAttributesDisplay {
 
 	@Override
 	public String getClassName() {
-		return MBMessage.class.getName();
+		return MBCategory.class.getName();
 	}
 
 	@Override
 	public String getIconPath(ThemeDisplay themeDisplay) {
-		return themeDisplay.getPathThemeImages() + "/common/conversation.png";
+		return themeDisplay.getPathThemeImages() + "/common/folder.png";
 	}
 
 }
