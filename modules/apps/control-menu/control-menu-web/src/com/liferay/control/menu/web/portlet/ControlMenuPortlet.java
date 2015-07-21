4f0ecfb8700f325d6dc620cbdad7c0195a0d1cdb
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-07-21 09:50:01 +0200

    LPS-57286 Moves java

--- modules/apps/control-menu/control-menu-web/src/com/liferay/control/menu/web/portlet/ControlMenuPortlet.java
+++ modules/apps/control-menu/control-menu-web/src/com/liferay/control/menu/web/portlet/ControlMenuPortlet.java
@@ -15,10 +15,26 @@
 package com.liferay.control.menu.web.portlet;
 
 import com.liferay.control.menu.web.constants.ControlMenuPortletKeys;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.Portlet;
+import javax.portlet.PortletConfig;
 
+import com.liferay.portal.kernel.servlet.MultiSessionMessages;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.model.LayoutTypePortlet;
+import com.liferay.portal.security.auth.PrincipalException;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.service.permission.LayoutPermissionUtil;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portal.util.WebKeys;
+import com.liferay.portlet.sites.util.SitesUtil;
 import org.osgi.service.component.annotations.Component;
 
 /**
@@ -40,4 +56,58 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class ControlMenuPortlet extends MVCPortlet {
+
+	@Override
+	public void processAction(
+			ActionMapping actionMapping, ActionForm actionForm,
+			PortletConfig portletConfig, ActionRequest actionRequest,
+			ActionResponse actionResponse)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
+
+		try {
+			if (cmd.equals("reset_customized_view")) {
+				if (!LayoutPermissionUtil.contains(
+						themeDisplay.getPermissionChecker(),
+						themeDisplay.getLayout(), ActionKeys.CUSTOMIZE)) {
+
+					throw new PrincipalException();
+				}
+
+				LayoutTypePortlet layoutTypePortlet =
+					themeDisplay.getLayoutTypePortlet();
+
+				if ((layoutTypePortlet != null) &&
+					layoutTypePortlet.isCustomizable() &&
+					layoutTypePortlet.isCustomizedView()) {
+
+					layoutTypePortlet.resetUserPreferences();
+				}
+			}
+			else if (cmd.equals("reset_prototype")) {
+				SitesUtil.resetPrototype(themeDisplay.getLayout());
+			}
+
+			MultiSessionMessages.add(
+				actionRequest,
+				PortalUtil.getPortletId(actionRequest) + "requestProcessed");
+
+			sendRedirect(actionRequest, actionResponse);
+		}
+		catch (Exception e) {
+			if (e instanceof SystemException) {
+				SessionErrors.add(actionRequest, e.getClass(), e);
+
+				sendRedirect(actionRequest, actionResponse);
+			}
+			else {
+				throw e;
+			}
+		}
+	}
+
 }
