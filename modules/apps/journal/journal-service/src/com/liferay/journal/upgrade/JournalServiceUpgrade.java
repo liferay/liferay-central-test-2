7b42d141f7c7edc76f9854d0eea043b03da90038
Máté Thurzó <mate.thurzo@liferay.com>
2015-07-21 11:35:11 +0200

    LPS-57276 Add upgrade process

--- modules/apps/journal/journal-service/src/com/liferay/journal/upgrade/JournalServiceUpgrade.java
+++ modules/apps/journal/journal-service/src/com/liferay/journal/upgrade/JournalServiceUpgrade.java
@@ -16,6 +16,7 @@ package com.liferay.journal.upgrade;
 
 import com.liferay.journal.service.configuration.configurator.JournalServiceConfigurator;
 import com.liferay.journal.upgrade.v1_0_0.UpgradeClassNames;
+import com.liferay.journal.upgrade.v1_0_0.UpgradeLastPublishDate;
 import com.liferay.portal.kernel.dao.db.DB;
 import com.liferay.portal.kernel.dao.db.DBFactoryUtil;
 import com.liferay.portal.kernel.log.Log;
@@ -67,6 +68,8 @@ public class JournalServiceUpgrade {
 		List<UpgradeProcess> upgradeProcesses = new ArrayList<>();
 
 		upgradeProcesses.add(new UpgradeClassNames());
+
+		upgradeProcesses.add(new UpgradeLastPublishDate());
 
 		_releaseLocalService.updateRelease(
 			"com.liferay.journal.service", upgradeProcesses, 1, 1, false);
---
+++ modules/apps/journal/journal-service/src/com/liferay/journal/upgrade/v1_0_0/UpgradeLastPublishDate.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.upgrade.v1_0_0;
+
+import com.liferay.journal.constants.JournalPortletKeys;
+
+/**
+ * @author Mate Thurzo
+ */
+public class UpgradeLastPublishDate
+	extends com.liferay.portal.upgrade.v7_0_0.UpgradeLastPublishDate {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		runSQL("alter table JournalArticle add lastPublishDate DATE null");
+
+		super.updateLastPublishDates(
+			JournalPortletKeys.JOURNAL, "JournalArticle");
+
+		runSQL("alter table JournalFeed add lastPublishDate DATE null");
+
+		super.updateLastPublishDates(JournalPortletKeys.JOURNAL, "JournalFeed");
+
+		runSQL("alter table JournalFolder add lastPublishDate DATE null");
+
+		super.updateLastPublishDates(
+			JournalPortletKeys.JOURNAL, "JournalFolder");
+	}
+
+}
