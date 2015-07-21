414d06b7468d3699a258c9916688bee0c3d5977e
Adolfo Pérez <adolfo.perez@liferay.com>
2015-07-21 12:25:58 +0200

    LPS-56048 Create portlets and move basic properties into annotation

---
+++ modules/apps/message-boards/message-boards-web/src/com/liferay/message/boards/web/portlet/MessageBoardsPortlet.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.message.boards.web.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.util.PortletKeys;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.css-class-wrapper=portlet-message-boards",
+		"com.liferay.portlet.display-category=category.collaboration",
+		"com.liferay.portlet.facebook-integration=fbml",
+		"com.liferay.portlet.footer-portlet-javascript=/html/portlet/message_boards/js/main.js",
+		"com.liferay.portlet.header-portlet-css=/html/portlet/message_boards/css/main.css",
+		"com.liferay.portlet.icon=/html/icons/message_boards.png",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.preferences-unique-per-layout=false",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.scopeable=true",
+		"com.liferay.portlet.struts-path=message_boards",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Message Boards",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/message_boards/view",
+		"javax.portlet.name=" + PortletKeys.MESSAGE_BOARDS,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.supported-public-render-parameter=tag",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
+public class MessageBoardsPortlet extends MVCPortlet {
+}
