61740a66765b1fb725a504e0a2f602062ddda20a
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-07-22 11:07:12 +0200

    LPS-56703 Move PortletRatingsDefinition to document-library-web module

--- modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/ratings/definition/DLPortletRatingsDefinition.java
+++ modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/ratings/definition/DLPortletRatingsDefinition.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.document.library.ratings.definition;
+package com.liferay.document.library.web.ratings.definition;
 
 import com.liferay.portal.util.PortletKeys;
 import com.liferay.portlet.ratings.RatingsType;
