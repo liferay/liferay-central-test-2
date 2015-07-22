e4c0d91c2be98abc13a7a9584bfe1b1c9aa3c2e6
Michael Han <michael.han@liferay.com>
2015-07-22 17:47:51 -0500

    LPS-56530 tokenize if multiple words not in phrase

--- modules/portal/portal-search/src/com/liferay/portal/search/internal/analysis/SimpleKeywordTokenizer.java
+++ modules/portal/portal-search/src/com/liferay/portal/search/internal/analysis/SimpleKeywordTokenizer.java
@@ -38,7 +38,10 @@ public class SimpleKeywordTokenizer implements KeywordTokenizer {
 		if (!((keyword.indexOf(CharPool.QUOTE) == 0) &&
 			(keyword.lastIndexOf(CharPool.QUOTE) == (keyword.length() -1)))) {
 
-			if ((start > -1) && (end > start)) {
+			if (((start > -1) && (end > start)) ||
+				((start == -1) && (end == -1) &&
+				 (keyword.indexOf(CharPool.SPACE) != -1))) {
+
 				return true;
 			}
 		}
