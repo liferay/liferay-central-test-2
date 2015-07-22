a2b8832324bf227471bcc550f2cc345a7a3b9e57
Máté Thurzó <mate.thurzo@liferay.com>
2015-07-22 12:15:23 +0200

    LPS-57307 Add upgrade process for Calendar

---
+++ modules/apps/calendar/calendar-service/src/com/liferay/calendar/upgrade/v1_0_0/UpgradeLastPublishDate.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.calendar.upgrade.v1_0_0;
+
+import com.liferay.calendar.constants.CalendarPortletKeys;
+
+/**
+ * @author Mate Thurzo
+ */
+public class UpgradeLastPublishDate
+	extends com.liferay.portal.upgrade.v7_0_0.UpgradeLastPublishDate {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		runSQL("alter table Calendar add lastPublishDate DATE null");
+
+		super.updateLastPublishDates(CalendarPortletKeys.CALENDAR, "Calendar");
+
+		runSQL("alter table CalendarBooking add lastPublishDate DATE null");
+
+		super.updateLastPublishDates(
+			CalendarPortletKeys.CALENDAR, "CalendarBooking");
+
+		runSQL(
+			"alter table CalendarNotificationTemplate add lastPublishDate " +
+				"DATE null");
+
+		super.updateLastPublishDates(
+			CalendarPortletKeys.CALENDAR, "CalendarNotificationTemplate");
+
+		runSQL("alter table CalendarResource add lastPublishDate DATE null");
+
+		super.updateLastPublishDates(
+			CalendarPortletKeys.CALENDAR, "CalendarResource");
+	}
+
+}
