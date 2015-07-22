36ce7cfb129df1a6d227e2b2ffff8838b8952a85
Brian Chan <brian.chan@liferay.com>
2015-07-22 16:35:10 -0700

    LRQA-16956 SF

--- modules/test/poshi-runner/src/com/liferay/poshi/runner/logger/LoggerElement.java
+++ modules/test/poshi-runner/src/com/liferay/poshi/runner/logger/LoggerElement.java
@@ -40,12 +40,7 @@ public class LoggerElement {
 		long time = System.currentTimeMillis();
 
 		while (true) {
-			StringBuilder sb = new StringBuilder();
-
-			sb.append("id");
-			sb.append(simpleDateFormat.format(new Date(time++)));
-
-			String id = sb.toString();
+			String id = "id" + simpleDateFormat.format(new Date(time++));
 
 			if (!_usedIds.contains(id)) {
 				_usedIds.add(id);
