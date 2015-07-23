208630f32b13e76351956900623572f950251c94
Eric Yan <eric.yan@liferay.com>
2015-07-23 09:00:42 -0700

    LPS-57125 Revert "LPS-16283"
    This reverts commit d990ecb9383b7a187be9c656d0da4f9ae4ea1621.

--- portal-impl/src/com/liferay/portal/struts/PortalRequestProcessor.java
+++ portal-impl/src/com/liferay/portal/struts/PortalRequestProcessor.java
@@ -147,15 +147,6 @@ public class PortalRequestProcessor extends TilesRequestProcessor {
 			HttpServletRequest request, HttpServletResponse response)
 		throws IOException, ServletException {
 
-		HttpSession session = request.getSession();
-
-		Boolean basicAuthEnabled = (Boolean)session.getAttribute(
-			WebKeys.BASIC_AUTH_ENABLED);
-
-		if (basicAuthEnabled != null) {
-			session.removeAttribute(WebKeys.BASIC_AUTH_ENABLED);
-		}
-
 		String path = super.processPath(request, response);
 
 		ActionMapping actionMapping =
@@ -163,9 +154,7 @@ public class PortalRequestProcessor extends TilesRequestProcessor {
 
 		Action action = StrutsActionRegistryUtil.getAction(path);
 
-		if (((basicAuthEnabled != null) && basicAuthEnabled.booleanValue()) ||
-			((actionMapping == null) && (action == null))) {
-
+		if ((actionMapping == null) && (action == null)) {
 			String lastPath = getLastPath(request);
 
 			if (_log.isDebugEnabled()) {
