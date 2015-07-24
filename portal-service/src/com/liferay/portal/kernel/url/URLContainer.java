4ec12fea6ed9c3c907eef02d19e669f9c6fac367
Cody Hoag <cody.hoag@liferay.com>
2015-07-23 21:03:03 -0400

    LRDOCS-1809 Readd Javadoc

--- portal-service/src/com/liferay/portal/kernel/url/URLContainer.java
+++ portal-service/src/com/liferay/portal/kernel/url/URLContainer.java
@@ -23,8 +23,35 @@ import java.util.Set;
  */
 public interface URLContainer {
 
+	/**
+	 * Finds the resource with the given name. A resource is some data that can
+	 * be accessed in a way that is independent of the location or storage.
+	 *
+	 * <p> The name of a resource is a '<tt>/</tt>'-separated path name that
+	 * identifies the resource.
+	 *
+	 * @param   name The resource name
+	 * @return  A <tt>URL</tt> object for reading the resource, or <tt>null</tt>
+	 *          if the resource could not be found or the invoker doesn't have
+	 *          adequate  privileges to get the resource.
+	 */
 	public URL getResource(String name);
 
+	/**
+	 * Returns a directory-like listing of all the paths to resources within the
+	 * container whose longest sub-path matches the supplied path argument.
+	 * <p>
+	 * Paths indicating sub-directory paths end with a '<tt>/</tt>'.
+	 * <p>
+	 * A path can be passed to the {{@link #getResource(String)} method to
+	 * return a resource URL.
+	 *
+	 * @param   name The resource name
+	 * @return  A set of {@link String <tt>String</tt>} objects representing
+	 *          individual resources in the container. If no resources could be
+	 *          found, the set will be empty. Resources that the invoker doesn't
+	 *          have access to will not be included.
+	 */
 	public Set<String> getResources(String path);
 
 }
