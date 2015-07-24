b33c82c706451604f64656589f0d7e4de375f894
Roberto Díaz <roberto.diaz@liferay.com>
2015-07-24 13:48:34 +0200

    LPS-56892 Move ViewAction to MVC

---
+++ portal-impl/src/com/liferay/portlet/socialactivity/action/ViewMVCRenderCommand.java
@@ -0,0 +1,131 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.socialactivity.action;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.security.auth.PrincipalException;
+import com.liferay.portal.security.permission.comparator.ModelResourceComparator;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portal.util.WebKeys;
+import com.liferay.portlet.social.model.SocialActivityDefinition;
+import com.liferay.portlet.social.model.SocialActivitySetting;
+import com.liferay.portlet.social.service.SocialActivitySettingServiceUtil;
+import com.liferay.portlet.social.util.SocialConfigurationUtil;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Roberto Díaz
+ */
+@OSGiBeanProperties(
+	property = {
+		"javax.portlet.name=" + PortletKeys.SOCIAL_ACTIVITY,
+		"javax.portlet.name=" + PortletKeys.SOCIAL_ACTIVITY,
+		"mvc.command.name=/social_activity/view"
+	},
+	service = MVCRenderCommand.class
+)
+public class ViewMVCRenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)renderRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		try {
+			renderRequest.setAttribute(
+				WebKeys.SOCIAL_ACTIVITY_SETTINGS_MAP,
+				getActivitySettingsMap(themeDisplay));
+		}
+		catch (Exception e) {
+			if (e instanceof PrincipalException) {
+				SessionErrors.add(renderRequest, e.getClass());
+
+				return actionMapping.findForward(
+					"portlet.social_activity.error");
+			}
+			else {
+				throw e;
+			}
+		}
+
+		return actionMapping.findForward("portlet.social_activity.view");
+	}
+
+	protected Map<String, Boolean> getActivitySettingsMap(
+			ThemeDisplay themeDisplay)
+		throws Exception {
+
+		Map<String, Boolean> activitySettingsMap = new LinkedHashMap<>();
+
+		List<SocialActivitySetting> activitySettings =
+			SocialActivitySettingServiceUtil.getActivitySettings(
+				themeDisplay.getSiteGroupIdOrLiveGroupId());
+
+		String[] modelNames = SocialConfigurationUtil.getActivityModelNames();
+
+		Comparator<String> comparator = new ModelResourceComparator(
+			themeDisplay.getLocale());
+
+		Arrays.sort(modelNames, comparator);
+
+		for (String modelName : modelNames) {
+			List<SocialActivityDefinition> activityDefinitions =
+				SocialActivitySettingServiceUtil.getActivityDefinitions(
+					themeDisplay.getScopeGroupId(), modelName);
+
+			for (SocialActivityDefinition activityDefinition :
+					activityDefinitions) {
+
+				if (activityDefinition.isCountersEnabled()) {
+					activitySettingsMap.put(modelName, false);
+
+					break;
+				}
+			}
+		}
+
+		for (SocialActivitySetting activitySetting : activitySettings) {
+			String name = activitySetting.getName();
+
+			if (name.equals("enabled") &&
+				activitySettingsMap.containsKey(
+					activitySetting.getClassName())) {
+
+				activitySettingsMap.put(
+					activitySetting.getClassName(),
+					GetterUtil.getBoolean(activitySetting.getValue()));
+			}
+		}
+
+		return activitySettingsMap;
+	}
+
+}
