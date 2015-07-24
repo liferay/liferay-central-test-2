d7cd32ff856badccf16056114520a9085dd01d84
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-07-24 08:31:37 +0200

    LPS-57450 Auto

--- portal-service/src/com/liferay/portal/model/GroupWrapper.java
+++ portal-service/src/com/liferay/portal/model/GroupWrapper.java
@@ -1037,11 +1037,6 @@ public class GroupWrapper implements Group, ModelWrapper<Group> {
 	}
 
 	@Override
-	public boolean isUserPersonalPanel() {
-		return _group.isUserPersonalPanel();
-	}
-
-	@Override
 	public boolean isUserPersonalSite() {
 		return _group.isUserPersonalSite();
 	}
