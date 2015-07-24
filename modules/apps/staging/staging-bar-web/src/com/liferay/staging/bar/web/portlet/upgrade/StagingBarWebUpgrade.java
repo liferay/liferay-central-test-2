fdf8bd5e5e7da6968bf82addcf16ae7304a5d87b
Levente Hudak <levente.hudak@liferay.com>
2015-07-24 15:11:26 +0200

    LPS-56992 Upgrade process for the Staging Bar portlet

---
+++ modules/apps/staging/staging-bar-web/src/com/liferay/staging/bar/web/portlet/upgrade/StagingBarWebUpgrade.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.staging.bar.web.portlet.upgrade;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.staging.bar.web.portlet.upgrade.v1_0_0.UpgradeStagingBarPortletId;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Levente Hudák
+ */
+@Component(immediate = true, service = StagingBarWebUpgrade.class)
+public class StagingBarWebUpgrade {
+
+	@Reference(unbind = "-")
+	protected void setReleaseLocalService(
+		ReleaseLocalService releaseLocalService) {
+
+		_releaseLocalService = releaseLocalService;
+	}
+
+	@Reference(target = "(original.bean=*)", unbind = "-")
+	protected void setServletContext(ServletContext servletContext) {
+	}
+
+	@Activate
+	protected void upgrade() throws PortalException {
+		List<UpgradeProcess> upgradeProcesses = new ArrayList<>();
+
+		upgradeProcesses.add(new UpgradeStagingBarPortletId());
+
+		_releaseLocalService.updateRelease(
+			"com.liferay.staging.bar.web", upgradeProcesses, 1, 1, false);
+	}
+
+	private ReleaseLocalService _releaseLocalService;
+
+}
