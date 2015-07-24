3049d0f83e8acab49ab71eb2d3584a66f978ae81
Levente Hudak <levente.hudak@liferay.com>
2015-07-24 15:11:26 +0200

    LPS-56992 Upgrade process for the Staging Bar portlet

---
+++ modules/apps/staging/staging-bar-web/src/com/liferay/staging/bar/web/portlet/upgrade/v1_0_0/UpgradeStagingBarPortletId.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.staging.bar.web.portlet.upgrade.v1_0_0;
+
+import com.liferay.portal.upgrade.util.UpgradePortletId;
+import com.liferay.staging.bar.web.portlet.constants.StagingBarPortletKeys;
+
+/**
+ * @author Levente Hudák
+ */
+public class UpgradeStagingBarPortletId extends UpgradePortletId {
+
+	@Override
+	protected String[][] getRenamePortletIdsArray() {
+		return new String[][] {
+			new String[] {"170", StagingBarPortletKeys.STAGING_BAR}
+		};
+	}
+
+}
