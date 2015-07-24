06c2da3d609f75d2ac8fe4d1dc34a0a096196e6c
Brian Chan <brian.chan@liferay.com>
2015-07-24 09:29:25 -0700

    LPS-57327 SF

--- portal-impl/src/com/liferay/portal/service/impl/ResourceLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/ResourceLocalServiceImpl.java
@@ -500,7 +500,7 @@ public class ResourceLocalServiceImpl extends ResourceLocalServiceBaseImpl {
 	 * @param  name the resource's name, which should be a portlet ID if the
 	 *         resource is a portlet or the resource's class name otherwise
 	 * @param  primKey the primary key of the resource instance
-	 * @param  modelPermissions the permissions to be applied
+	 * @param  modelPermissions the model permissions to be applied
 	 * @throws PortalException if a portal exception occurred
 	 */
 	@Override
@@ -547,7 +547,7 @@ public class ResourceLocalServiceImpl extends ResourceLocalServiceBaseImpl {
 	 * @param  name the resource's name, which should be a portlet ID if the
 	 *         resource is a portlet or the resource's class name otherwise
 	 * @param  primKey the primary key string of the resource instance
-	 * @param  modelPermissions the permissions to be applied
+	 * @param  modelPermissions the model permissions to be applied
 	 * @throws PortalException if a portal exception occurred
 	 */
 	@Override
--- portal-impl/src/com/liferay/portlet/asset/service/impl/AssetTagLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/asset/service/impl/AssetTagLocalServiceImpl.java
@@ -139,7 +139,7 @@ public class AssetTagLocalServiceImpl extends AssetTagLocalServiceBaseImpl {
 	 * Adds resources for the asset tag using the group and guest permissions.
 	 *
 	 * @param  tag the asset tag for which to add resources
-	 * @param  modelPermissions the permissions to be applied
+	 * @param  modelPermissions the model permissions to be applied
 	 * @throws PortalException if resources could not be added for the asset tag
 	 *         or if a portal exception occurred
 	 */
