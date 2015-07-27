365012df365afa50e5add3ffdec844d96c091694
Daniel Kocsis <daniel.kocsis@liferay.com>
2015-07-27 11:15:15 +0200

    LPS-57375 Move staging related classes to the new bundle

--- modules/apps/users-admin/users-admin-service/src/com/liferay/users/admin/lar/OrganizationStagedModelDataHandler.java
+++ modules/apps/users-admin/users-admin-service/src/com/liferay/users/admin/lar/OrganizationStagedModelDataHandler.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portlet.usersadmin.lar;
+package com.liferay.users.admin.lar;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.util.MapUtil;
