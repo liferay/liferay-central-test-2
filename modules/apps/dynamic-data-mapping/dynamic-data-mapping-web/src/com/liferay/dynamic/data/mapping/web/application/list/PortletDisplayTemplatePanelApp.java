7f3249dd8cd38cb00b8004241cc6a5948f61fdae
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-07-27 18:19:08 +0200

    LPS-57286 Creates new category to publishing tools

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/com/liferay/dynamic/data/mapping/web/application/list/PortletDisplayTemplatePanelApp.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/com/liferay/dynamic/data/mapping/web/application/list/PortletDisplayTemplatePanelApp.java
@@ -30,7 +30,7 @@ import org.osgi.service.component.annotations.Reference;
 	immediate = true,
 	property = {
 		"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_CONFIGURATION,
-		"service.ranking:Integer=300"
+		"service.ranking:Integer=200"
 	},
 	service = PanelApp.class
 )
