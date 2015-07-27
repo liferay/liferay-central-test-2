be07e70ab6973145ceeea3d218bab8fd045ba90d
Roberto Díaz <roberto.diaz@liferay.com>
2015-07-27 15:35:32 +0200

    LPS-57357 Move MVC actions

--- modules/apps/social/social-activity-web/src/com/liferay/social/activity/web/portlet/action/EditActivitySettingsMVCActionCommand.java
+++ modules/apps/social/social-activity-web/src/com/liferay/social/activity/web/portlet/action/EditActivitySettingsMVCActionCommand.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portlet.socialactivity.action;
+package com.liferay.social.activity.web.portlet.action;
 
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
