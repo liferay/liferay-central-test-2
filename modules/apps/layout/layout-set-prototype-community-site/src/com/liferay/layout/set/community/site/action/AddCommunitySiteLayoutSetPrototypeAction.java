5d52718bb974bdc6a7080a18fad9916a97438780
Adolfo Pérez <adolfo.perez@liferay.com>
2015-07-28 13:23:09 +0200

    LPS-57463 Run action on activation

--- modules/apps/layout/layout-set-prototype-community-site/src/com/liferay/layout/set/community/site/action/AddCommunitySiteLayoutSetPrototypeAction.java
+++ modules/apps/layout/layout-set-prototype-community-site/src/com/liferay/layout/set/community/site/action/AddCommunitySiteLayoutSetPrototypeAction.java
@@ -20,9 +20,12 @@ import com.liferay.portal.kernel.events.SimpleAction;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.model.Company;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.LayoutSet;
 import com.liferay.portal.model.LayoutSetPrototype;
+import com.liferay.portal.security.auth.CompanyThreadLocal;
+import com.liferay.portal.service.CompanyLocalService;
 import com.liferay.portal.service.LayoutSetPrototypeLocalService;
 import com.liferay.portal.service.UserLocalService;
 import com.liferay.portal.util.DefaultLayoutPrototypesUtil;
@@ -32,6 +35,7 @@ import com.liferay.portal.util.PortletKeys;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -50,6 +54,24 @@ public class AddCommunitySiteLayoutSetPrototypeAction extends SimpleAction {
 		}
 		catch (Exception e) {
 			throw new ActionException(e);
+		}
+	}
+
+	@Activate
+	protected void activate() throws ActionException {
+		Long companyId = CompanyThreadLocal.getCompanyId();
+
+		try {
+			List<Company> companies = _companyLocalService.getCompanies();
+
+			for (Company company : companies) {
+				CompanyThreadLocal.setCompanyId(company.getCompanyId());
+
+				run(new String[] {String.valueOf(company.getCompanyId())});
+			}
+		}
+		finally {
+			CompanyThreadLocal.setCompanyId(companyId);
 		}
 	}
 
@@ -104,6 +126,13 @@ public class AddCommunitySiteLayoutSetPrototypeAction extends SimpleAction {
 	}
 
 	@Reference(unbind = "-")
+	protected void setCompanyLocalService(
+		CompanyLocalService companyLocalService) {
+
+		_companyLocalService = companyLocalService;
+	}
+
+	@Reference(unbind = "-")
 	protected void setLayoutSetPrototypeLocalService(
 		LayoutSetPrototypeLocalService layoutSetPrototypeLocalService) {
 
@@ -115,6 +144,7 @@ public class AddCommunitySiteLayoutSetPrototypeAction extends SimpleAction {
 		_userLocalService = userLocalService;
 	}
 
+	private CompanyLocalService _companyLocalService;
 	private LayoutSetPrototypeLocalService _layoutSetPrototypeLocalService;
 	private UserLocalService _userLocalService;
 
