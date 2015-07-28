cbe4e4d5fd873d7e7e3667def2ff557f2904bd7d
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-07-28 15:13:02 +0200

    LPS-56245 Move notifications tests to document library test module

--- modules/apps/document-library/document-library-test/test/integration/com/liferay/document/library/notifications/test/DocumentLibraryUserNotificationTest.java
+++ modules/apps/document-library/document-library-test/test/integration/com/liferay/document/library/notifications/test/DocumentLibraryUserNotificationTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portlet.documentlibrary.notifications;
+package com.liferay.document.library.notifications.test;
 
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.Folder;
