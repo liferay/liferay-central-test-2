brianchandotcom/liferay-portal

LPS-47199 avoid naming conflict by renaming to OWNER_USER_ID

util-java/test/unit/com/liferay/util/dao/orm/CustomSQLTest.java
@@ -269,7 +269,7 @@ public class CustomSQLTest {
 	}
 
 	private static final String _SQL =
-		"([$USER_ID$] [$USER_ID_AND_OR_CONNECTOR$] [$STATUS$]) ";
+		"([$OWNER_USER_ID$] [$USER_ID_AND_OR_CONNECTOR$] [$STATUS$]) ";
 
 	private static final String _TABLE_NAME = "TestModel";
 

liferay/liferay-central-test-2

util-java/test/unit/com/liferay/util/dao/orm/CustomSQLTest.java
@@ -269,7 +269,7 @@ public class CustomSQLTest {
 	}
 
 	private static final String _SQL =
-		"([$USER_ID$] [$USER_ID_AND_OR_CONNECTOR$] [$STATUS$]) ";
+		"([$OWNER_USER_ID$] [$OWNER_USER_ID_AND_OR_CONNECTOR$] [$STATUS$]) ";
 
 	private static final String _TABLE_NAME = "TestModel";
 