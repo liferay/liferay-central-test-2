shuyangzhou/liferay-portal

LPS-56674 Add Oracle database ExpectedLogs for VerifyUUIDTest

portal-impl/test/integration/com/liferay/portal/verify/VerifyUUIDTest.java
@@ -61,6 +61,12 @@ public class VerifyUUIDTest extends BaseVerifyProcessTestCase {
 					"Unable to process runnable: ERROR: column \"unknown\" " +
 						"does not exist_",
 				expectedType = ExpectedType.PREFIX
+			),
+			@ExpectedLog(
+				dbType = DB.TYPE_ORACLE, expectedLog =
+					"Unable to process runnable: ORA-00904: \"UNKNOWN\":" +
+						" invalid identifier_",
+				expectedType = ExpectedType.PREFIX
 			)
 		},
 		level = "ERROR", loggerClass = ThrowableAwareRunnable.class
@@ -94,6 +100,12 @@ public class VerifyUUIDTest extends BaseVerifyProcessTestCase {
 				dbType = DB.TYPE_POSTGRESQL, expectedLog =
 					"Unable to process runnable: ERROR: relation " +
 						"\"unknown\" does not exist_",
+				expectedType = ExpectedType.PREFIX
+			),
+			@ExpectedLog(
+				dbType = DB.TYPE_ORACLE, expectedLog =
+					"Unable to process runnable: ORA-00942: table or view" +
+						" does not exist_",
 				expectedType = ExpectedType.PREFIX
 			)
 		},
@@ -139,6 +151,12 @@ public class VerifyUUIDTest extends BaseVerifyProcessTestCase {
 				dbType = DB.TYPE_POSTGRESQL, expectedLog =
 					"Unable to process runnable: ERROR: relation \"unknown\" " +
 						"does not exist_",
+				expectedType = ExpectedType.PREFIX
+			),
+			@ExpectedLog(
+				dbType = DB.TYPE_ORACLE, expectedLog =
+					"Unable to process runnable: ORA-00942: table or view" +
+						" does not exist_",
 				expectedType = ExpectedType.PREFIX
 			)
 		},

liferay/liferay-central-test-2

portal-impl/test/integration/com/liferay/portal/verify/VerifyUUIDTest.java
@@ -61,6 +61,12 @@ public class VerifyUUIDTest extends BaseVerifyProcessTestCase {
 					"Unable to process runnable: ERROR: column \"unknown\" " +
 						"does not exist",
 				expectedType = ExpectedType.PREFIX
+			),
+			@ExpectedLog(
+				dbType = DB.TYPE_ORACLE, expectedLog =
+					"Unable to process runnable: ORA-00904: \"UNKNOWN\":" +
+						" invalid identifier",
+				expectedType = ExpectedType.PREFIX
 			)
 		},
 		level = "ERROR", loggerClass = ThrowableAwareRunnable.class
@@ -94,6 +100,12 @@ public class VerifyUUIDTest extends BaseVerifyProcessTestCase {
 				dbType = DB.TYPE_POSTGRESQL, expectedLog =
 					"Unable to process runnable: ERROR: relation " +
 						"\"unknown\" does not exist",
+				expectedType = ExpectedType.PREFIX
+			),
+			@ExpectedLog(
+				dbType = DB.TYPE_ORACLE, expectedLog =
+					"Unable to process runnable: ORA-00942: table or view" +
+						" does not exist",
 				expectedType = ExpectedType.PREFIX
 			)
 		},
@@ -139,6 +151,12 @@ public class VerifyUUIDTest extends BaseVerifyProcessTestCase {
 				dbType = DB.TYPE_POSTGRESQL, expectedLog =
 					"Unable to process runnable: ERROR: relation \"unknown\" " +
 						"does not exist",
+				expectedType = ExpectedType.PREFIX
+			),
+			@ExpectedLog(
+				dbType = DB.TYPE_ORACLE, expectedLog =
+					"Unable to process runnable: ORA-00942: table or view" +
+						" does not exist",
 				expectedType = ExpectedType.PREFIX
 			)
 		},