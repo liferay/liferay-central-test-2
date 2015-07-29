6ab5b3608e6fd90ed9be06b3428a5734b122eda8
Jonathan McCann <jonathan.mccann@liferay.com>
2015-07-29 07:25:32 -0700

    LPS-57318 Source Formatting

--- portal-service/test/unit/com/liferay/portal/kernel/cal/RecurrenceSerializerTest.java
+++ portal-service/test/unit/com/liferay/portal/kernel/cal/RecurrenceSerializerTest.java
@@ -27,44 +27,23 @@ import org.junit.Test;
  */
 public class RecurrenceSerializerTest extends RecurrenceSerializer {
 
-	public Recurrence getRecurrence(Calendar calendar, int recurrenceType) {
-		Recurrence recurrence = new Recurrence(
-			calendar, new Duration(1, 0, 0, 0), recurrenceType);
-
-		return recurrence;
-	}
-
 	@Test
 	public void testDailyRecurrence() throws Exception {
-		Calendar recurrenceCalendar = new GregorianCalendar(
-			2010, 0, 2, 3, 4, 5);
-
 		Recurrence recurrence = getRecurrence(
-			recurrenceCalendar, Recurrence.DAILY);
-
-		recurrence.setWeekStart(Calendar.SUNDAY);
-
-		recurrence.setInterval(1);
+			_RECURRENCE_CALENDAR, Recurrence.DAILY);
 
 		Assert.assertEquals("0 4 3 1/1 * ? *", toCronText(recurrence));
 	}
 
 	@Test
 	public void testMonthlyRecurrence() throws Exception {
-		Calendar recurrenceCalendar = new GregorianCalendar(
-			2010, 0, 2, 3, 4, 5);
-
 		Recurrence recurrence = getRecurrence(
-			recurrenceCalendar, Recurrence.MONTHLY);
-
-		recurrence.setWeekStart(Calendar.SUNDAY);
-
-		recurrence.setInterval(1);
+			_RECURRENCE_CALENDAR, Recurrence.MONTHLY);
 
 		recurrence.setByMonthDay(new int[] {15});
 
 		Assert.assertEquals("0 4 3 15 1/1 ? *", toCronText(recurrence));
-		
+
 		recurrence.setInterval(3);
 
 		Assert.assertEquals("0 4 3 15 1/3 ? *", toCronText(recurrence));
@@ -72,22 +51,16 @@ public class RecurrenceSerializerTest extends RecurrenceSerializer {
 
 	@Test
 	public void testNoRecurrence() throws Exception {
-		Calendar recurrenceCalendar = new GregorianCalendar(
-			2010, 0, 2, 3, 4, 5);
-
 		Recurrence recurrence = getRecurrence(
-			recurrenceCalendar, Recurrence.NO_RECURRENCE);
+			_RECURRENCE_CALENDAR, Recurrence.NO_RECURRENCE);
 
 		Assert.assertEquals("0 4 3 2 1 ? 2010", toCronText(recurrence));
 	}
 
 	@Test
 	public void testWeeklyRecurrence() throws Exception {
-		Calendar recurrenceCalendar = new GregorianCalendar(
-			2010, 0, 2, 3, 4, 5);
-
 		Recurrence recurrence = getRecurrence(
-			recurrenceCalendar, Recurrence.WEEKLY);
+			_RECURRENCE_CALENDAR, Recurrence.WEEKLY);
 
 		List<DayAndPosition> dayPos = new ArrayList<>();
 
@@ -100,15 +73,18 @@ public class RecurrenceSerializerTest extends RecurrenceSerializer {
 
 	@Test
 	public void testYearlyRecurrence() throws Exception {
-		Calendar recurrenceCalendar = new GregorianCalendar(
-			2010, 0, 2, 3, 4, 5);
-
 		Recurrence recurrence = getRecurrence(
-			recurrenceCalendar, Recurrence.YEARLY);
-
-		recurrence.setInterval(1);
+			_RECURRENCE_CALENDAR, Recurrence.YEARLY);
 
 		Assert.assertEquals("0 4 3 ? 1 ? 2010/1", toCronText(recurrence));
 	}
+
+	protected Recurrence getRecurrence(Calendar calendar, int recurrenceType) {
+		return new Recurrence(
+			calendar, new Duration(1, 0, 0, 0), recurrenceType);
+	}
+
+	private static final Calendar _RECURRENCE_CALENDAR = new GregorianCalendar(
+		2010, 0, 2, 3, 4, 5);
 
 }
