sergiogonzalez/liferay-portal

LPS-55518 Move ckeditor config to OSGI modules

modules/frontend/frontend-editors-web/src/com/liferay/frontend/editors/web/editor/config/BaseCKEditorConfigContributor.java
@@ -1,0 +1,117 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editors.web.editor.config;
+
+import com.liferay.portal.kernel.editor.config.BaseEditorConfigContributor;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.theme.ThemeDisplay;
+
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Ambrin Chaudhary
+ */
+
+public class BaseCKEditorConfigContributor extends BaseEditorConfigContributor {
+    @Override
+    public void populateConfigJSONObject(
+            JSONObject jsonObject,
+            Map<String, Object> inputEditorTaglibAttributes,
+            ThemeDisplay themeDisplay,
+            LiferayPortletResponse liferayPortletResponse) {
+
+        jsonObject.put("allowedContent", Boolean.TRUE);
+
+        String cssClasses = (String)inputEditorTaglibAttributes.get(
+                "liferay-ui:input-editor:cssClasses");
+
+        jsonObject.put("bodyClass", "html-editor " +
+                HtmlUtil.escape(cssClasses));
+
+        JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+        String cssPath = HtmlUtil.escape(themeDisplay.getPathThemeCss());
+
+        jsonArray.put(cssPath + "/aui.css");
+        jsonArray.put(cssPath + "/main.css");
+
+        jsonObject.put("contentsCss", jsonArray);
+
+        String contentsLanguageId = (String)inputEditorTaglibAttributes.get(
+                "liferay-ui:input-editor:contentsLanguageId");
+
+        Locale contentsLocale = LocaleUtil.fromLanguageId(contentsLanguageId);
+
+        String contentsLanguageDir = LanguageUtil.get(
+                contentsLocale, "lang.dir");
+
+        contentsLanguageId = LocaleUtil.toLanguageId(contentsLocale);
+
+        jsonObject.put("contentsLangDirection",
+                HtmlUtil.escapeJS(contentsLanguageDir));
+
+        jsonObject.put("contentsLanguage",
+                contentsLanguageId.replace("iw_", "he_"));
+
+        jsonObject.put("height", 265);
+
+        String languageId = LocaleUtil.toLanguageId(themeDisplay.getLocale());
+
+        Locale locale = LocaleUtil.fromLanguageId(languageId);
+
+        languageId = LocaleUtil.toLanguageId(locale);
+
+        jsonObject.put("language", languageId.replace("iw_", "he_"));
+
+        boolean resizable = GetterUtil.getBoolean(
+                (String) inputEditorTaglibAttributes.get(
+                        "liferay-ui:input-editor:resizable"));
+
+        if (resizable) {
+            jsonObject.put("resize_dir", "vertical");
+        }
+
+        jsonObject.put("resize_enabled", resizable);
+
+        liferayPortletResponse.getHttpServletResponse()
+                .setContentType(ContentTypes.TEXT_JAVASCRIPT);
+    }
+
+    @Override
+    public void populateOptionsJSONObject(
+            JSONObject jsonObject,
+            Map<String, Object> inputEditorTaglibAttributes,
+            ThemeDisplay themeDisplay,
+            LiferayPortletResponse liferayPortletResponse) {
+
+    }
+
+    protected boolean isShowSource(
+            Map<String, Object> inputEditorTaglibAttributes) {
+
+        return GetterUtil.getBoolean(
+                inputEditorTaglibAttributes.get(
+                        "liferay-ui:input-editor:showSource"));
+    }
+}

modules/frontend/frontend-editors-web/src/com/liferay/frontend/editors/web/editor/config/CKEditorBBCodeConfigContributor.java
@@ -1,0 +1,213 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNES
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editors.web.editor.config;
+
+import com.liferay.portal.kernel.editor.config.BaseEditorConfigContributor;
+import com.liferay.portal.kernel.editor.config.EditorConfigContributor;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.parsers.bbcode.BBCodeTranslatorUtil;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.util.*;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.messageboards.model.MBThreadConstants;
+import org.osgi.service.component.annotations.Component;
+
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Ambrin Chaudhary
+ */
+@Component(
+        property = {"editor.name=ckeditor_bbcode"},
+        service = EditorConfigContributor.class
+)
+
+public class CKEditorBBCodeConfigContributor
+    extends BaseCKEditorConfigContributor {
+
+    @Override
+    public void populateConfigJSONObject(
+            JSONObject jsonObject,
+            Map<String, Object> inputEditorTaglibAttributes,
+            ThemeDisplay themeDisplay,
+            LiferayPortletResponse liferayPortletResponse) {
+
+        super.populateConfigJSONObject(
+                jsonObject, inputEditorTaglibAttributes, themeDisplay,
+                liferayPortletResponse);
+
+        jsonObject.put("allowedContent", Boolean.TRUE);
+
+        jsonObject.put("enterMode", _CKEDITOR_ENTER_BR);
+
+        jsonObject.put("extraPlugins", "a11yhelpbtn,bbcode,wikilink");
+
+        jsonObject.put("filebrowserBrowseUrl", "");
+
+        jsonObject.put("filebrowserImageBrowseLinkUrl", "");
+
+        jsonObject.put("filebrowserImageBrowseUrl", "");
+
+        jsonObject.put("filebrowserImageUploadUrl", "");
+
+        jsonObject.put("filebrowserUploadUrl", "");
+
+        jsonObject.put("fontSize_sizes",
+            "10/10px;12/12px;16/16px;18/18px;24/24px;32/32px;48/48px");
+
+        jsonObject.put("format_tags", "p;pre");
+
+        jsonObject.put("imagesPath",
+            HtmlUtil.escape(themeDisplay.getPathThemeImages()) +
+            "/message_boards/");
+
+        jsonObject.put("newThreadURL", MBThreadConstants.NEW_THREAD_URL);
+
+        String removePlugins ="bidi,div,elementspath,flash,forms,indentblock," +
+            "keystrokes,link,maximize,newpage,pagebreak,preview,print," +
+            "save,showblocks,templates,video";
+
+        jsonObject.put("removePlugins", removePlugins);
+
+        String smileyEmoticons = StringUtil.merge(
+            BBCodeTranslatorUtil.getEmoticonDescriptions(), "','");
+
+        jsonObject.put("smiley_descriptions",
+            toJSONArray("['" + smileyEmoticons + "']"));
+
+        String smileyImages = StringUtil.merge(
+            BBCodeTranslatorUtil.getEmoticonFiles(), "','");
+
+        jsonObject.put("smiley_images",
+            toJSONArray("['" + smileyImages + "']"));
+
+        jsonObject.put("smiley_path",
+                HtmlUtil.escape(themeDisplay.getPathThemeImages()) +
+                "/emoticons/");
+
+        String smileySymbols = StringUtil.merge(
+            BBCodeTranslatorUtil.getEmoticonSymbols(), "','");
+
+        jsonObject.put("smiley_symbols",
+            toJSONArray("['" + smileySymbols + "']"));
+
+        jsonObject.put("toolbar_bbcode",
+            getToolbarsBBCodeJSONArray(inputEditorTaglibAttributes));
+
+        jsonObject.put("toolbar_phone",
+            getToolbarsPhoneJSONArray(inputEditorTaglibAttributes));
+
+        jsonObject.put("toolbar_tablet",
+            getToolbarsTabletJSONArray(inputEditorTaglibAttributes));
+    }
+
+    @Override
+    public void populateOptionsJSONObject(
+            JSONObject jsonObject,
+            Map<String, Object> inputEditorTaglibAttributes,
+            ThemeDisplay themeDisplay,
+            LiferayPortletResponse liferayPortletResponse) {
+
+    }
+
+    protected JSONArray getToolbarsBBCodeJSONArray(
+            Map<String, Object> inputEditorTaglibAttributes) {
+        JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+        jsonArray.put(toJSONArray(
+            "['Bold', 'Italic', 'Underline', 'Strike']"));
+
+        jsonArray.put(toJSONArray("['TextColor']"));
+
+        jsonArray.put(toJSONArray(
+            "['JustifyLeft', 'JustifyCenter'," +
+            "'JustifyRight', 'JustifyBlock']"));
+
+        jsonArray.put(toJSONArray(
+            "['NumberedList', 'BulletedList', '-', 'Outdent', " +
+            " 'Indent', '-', 'Blockquote', '-', 'Code']"));
+
+        jsonArray.put("/");
+
+        jsonArray.put(toJSONArray("['Format', 'Font', 'FontSize']"));
+
+        jsonArray.put(toJSONArray("['Link', 'Unlink']"));
+
+        jsonArray.put(toJSONArray("['Image', '-', 'Smiley']"));
+
+        jsonArray.put("/");
+
+        jsonArray.put(toJSONArray("['Cut', 'Copy', 'Paste', '-', " +
+            "'SelectAll', '-', 'Undo', 'Redo']"));
+
+        if (isShowSource(inputEditorTaglibAttributes)) {
+            jsonArray.put(toJSONArray("['Source']"));
+        }
+
+        jsonArray.put(toJSONArray("['A11YBtn']"));
+
+
+        return jsonArray;
+    }
+
+    protected JSONArray getToolbarsPhoneJSONArray(
+            Map<String, Object> inputEditorTaglibAttributes) {
+        JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+        jsonArray.put(toJSONArray("['Bold', 'Italic', 'Underline']"));
+
+        jsonArray.put(toJSONArray("['NumberedList', 'BulletedList']"));
+
+        jsonArray.put(toJSONArray("['Link', 'Unlink']"));
+
+        jsonArray.put(toJSONArray("['Image']"));
+
+        if (isShowSource(inputEditorTaglibAttributes)) {
+            jsonArray.put(toJSONArray("['Source']"));
+        }
+
+        return jsonArray;
+    }
+
+    protected JSONArray getToolbarsTabletJSONArray(
+            Map<String, Object> inputEditorTaglibAttributes) {
+        JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+        jsonArray.put(toJSONArray("['Bold', 'Italic', 'Underline', 'Strike']"));
+
+        jsonArray.put(toJSONArray("['JustifyLeft', 'JustifyCenter', " +
+            "'JustifyRight', 'JustifyBlock']"));
+
+        jsonArray.put(toJSONArray("['NumberedList', 'BulletedList']"));
+
+        jsonArray.put(toJSONArray("['Styles', 'FontSize']"));
+
+        jsonArray.put(toJSONArray("['Link', 'Unlink']"));
+
+        jsonArray.put(toJSONArray("['Image']"));
+
+        if (isShowSource(inputEditorTaglibAttributes)) {
+            jsonArray.put(toJSONArray("['Source']"));
+        }
+
+        return jsonArray;
+    }
+
+    private static Integer _CKEDITOR_ENTER_BR = 2;
+}

liferay/liferay-central-test-2

modules/frontend/frontend-editors-web/src/com/liferay/frontend/editors/web/editor/configuration/BaseCKEditorConfigContributor.java
@@ -1,0 +1,115 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editors.web.editor.configuration;
+
+import com.liferay.portal.kernel.editor.configuration.BaseEditorConfigContributor;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.theme.ThemeDisplay;
+
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Ambrin Chaudhary
+ */
+
+public class BaseCKEditorConfigContributor extends BaseEditorConfigContributor {
+	@Override
+	public void populateConfigJSONObject(
+		JSONObject jsonObject, Map<String, Object> inputEditorTaglibAttributes,
+		ThemeDisplay themeDisplay,
+		LiferayPortletResponse liferayPortletResponse) {
+
+		jsonObject.put("allowedContent", Boolean.TRUE);
+
+		String cssClasses = (String)inputEditorTaglibAttributes.get(
+			"liferay-ui:input-editor:cssClasses");
+
+		jsonObject.put("bodyClass", "html-editor " +
+			HtmlUtil.escape(cssClasses));
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		String cssPath = HtmlUtil.escape(themeDisplay.getPathThemeCss());
+
+		jsonArray.put(cssPath + "/aui.css");
+		jsonArray.put(cssPath + "/main.css");
+
+		jsonObject.put("contentsCss", jsonArray);
+
+		String contentsLanguageId = (String)inputEditorTaglibAttributes.get(
+			"liferay-ui:input-editor:contentsLanguageId");
+
+		Locale contentsLocale = LocaleUtil.fromLanguageId(contentsLanguageId);
+
+		String contentsLanguageDir = LanguageUtil.get(
+			contentsLocale, "lang.dir");
+
+		contentsLanguageId = LocaleUtil.toLanguageId(contentsLocale);
+
+		jsonObject.put("contentsLangDirection",
+			HtmlUtil.escapeJS(contentsLanguageDir));
+
+		jsonObject.put("contentsLanguage",
+			contentsLanguageId.replace("iw_", "he_"));
+
+		jsonObject.put("height", 265);
+
+		String languageId = LocaleUtil.toLanguageId(themeDisplay.getLocale());
+
+		Locale locale = LocaleUtil.fromLanguageId(languageId);
+
+		languageId = LocaleUtil.toLanguageId(locale);
+
+		jsonObject.put("language", languageId.replace("iw_", "he_"));
+
+		boolean resizable = GetterUtil.getBoolean(
+				(String)inputEditorTaglibAttributes.get(
+					"liferay-ui:input-editor:resizable"));
+
+		if (resizable) {
+			jsonObject.put("resize_dir", "vertical");
+		}
+
+		jsonObject.put("resize_enabled", resizable);
+
+		liferayPortletResponse.getHttpServletResponse()
+			.setContentType(ContentTypes.TEXT_JAVASCRIPT);
+	}
+
+	@Override
+	public void populateOptionsJSONObject(
+		JSONObject jsonObject, Map<String, Object> inputEditorTaglibAttributes,
+		ThemeDisplay themeDisplay,
+		LiferayPortletResponse liferayPortletResponse) {
+	}
+
+	protected boolean isShowSource(
+		Map<String, Object> inputEditorTaglibAttributes) {
+
+		return GetterUtil.getBoolean(
+				inputEditorTaglibAttributes.get(
+					"liferay-ui:input-editor:showSource"));
+	}
+
+}

modules/frontend/frontend-editors-web/src/com/liferay/frontend/editors/web/editor/configuration/CKEditorBBCodeConfigContributor.java
@@ -1,0 +1,213 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNES
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editors.web.editor.configuration;
+
+import com.liferay.portal.kernel.editor.configuration.EditorConfigContributor;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.parsers.bbcode.BBCodeTranslatorUtil;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.util.*;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.messageboards.model.MBThreadConstants;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Ambrin Chaudhary
+ */
+@Component(
+		property = {"editor.name=ckeditor_bbcode"},
+		service = EditorConfigContributor.class
+)
+
+public class CKEditorBBCodeConfigContributor
+	extends BaseCKEditorConfigContributor {
+
+	@Override
+	public void populateConfigJSONObject(
+		JSONObject jsonObject, Map<String, Object> inputEditorTaglibAttributes,
+		ThemeDisplay themeDisplay,
+		LiferayPortletResponse liferayPortletResponse) {
+
+		super.populateConfigJSONObject(
+				jsonObject, inputEditorTaglibAttributes, themeDisplay,
+				liferayPortletResponse);
+
+		jsonObject.put("allowedContent", Boolean.TRUE);
+
+		jsonObject.put("enterMode", _CKEDITOR_ENTER_BR);
+
+		jsonObject.put(
+			"extraPlugins", "a11yhelpbtn,bbcode,imageselector,wikilink");
+
+		jsonObject.put("filebrowserBrowseUrl", "");
+
+		jsonObject.put("filebrowserImageBrowseLinkUrl", "");
+
+		jsonObject.put("filebrowserImageBrowseUrl", "");
+
+		jsonObject.put("filebrowserImageUploadUrl", "");
+
+		jsonObject.put("filebrowserUploadUrl", "");
+
+		jsonObject.put("fontSize_sizes",
+			"10/10px;12/12px;16/16px;18/18px;24/24px;32/32px;48/48px");
+
+		jsonObject.put("format_tags", "p;pre");
+
+		jsonObject.put("imagesPath",
+			HtmlUtil.escape(themeDisplay.getPathThemeImages()) +
+			"/message_boards/");
+
+		jsonObject.put("newThreadURL", MBThreadConstants.NEW_THREAD_URL);
+
+		String removePlugins ="bidi,div,elementspath,flash,forms,indentblock," +
+			"keystrokes,link,maximize,newpage,pagebreak,preview,print," +
+			"save,showblocks,templates,video";
+
+		jsonObject.put("removePlugins", removePlugins);
+
+		String smileyEmoticons = StringUtil.merge(
+			BBCodeTranslatorUtil.getEmoticonDescriptions(), "','");
+
+		jsonObject.put("smiley_descriptions",
+			toJSONArray("['" + smileyEmoticons + "']"));
+
+		String smileyImages = StringUtil.merge(
+			BBCodeTranslatorUtil.getEmoticonFiles(), "','");
+
+		jsonObject.put("smiley_images",
+			toJSONArray("['" + smileyImages + "']"));
+
+		jsonObject.put("smiley_path",
+				HtmlUtil.escape(themeDisplay.getPathThemeImages()) +
+				"/emoticons/");
+
+		String smileySymbols = StringUtil.merge(
+			BBCodeTranslatorUtil.getEmoticonSymbols(), "','");
+
+		jsonObject.put("smiley_symbols",
+			toJSONArray("['" + smileySymbols + "']"));
+
+		jsonObject.put("toolbar_bbcode",
+			getToolbarsBBCodeJSONArray(inputEditorTaglibAttributes));
+
+		jsonObject.put("toolbar_phone",
+			getToolbarsPhoneJSONArray(inputEditorTaglibAttributes));
+
+		jsonObject.put("toolbar_tablet",
+			getToolbarsTabletJSONArray(inputEditorTaglibAttributes));
+	}
+
+	@Override
+	public void populateOptionsJSONObject(
+		JSONObject jsonObject, Map<String, Object> inputEditorTaglibAttributes,
+		ThemeDisplay themeDisplay,
+		LiferayPortletResponse liferayPortletResponse) {
+
+		jsonObject.put("useCustomDataProcessor", Boolean.TRUE);
+	}
+
+	protected JSONArray getToolbarsBBCodeJSONArray(
+		Map<String, Object> inputEditorTaglibAttributes) {
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		jsonArray.put(toJSONArray("['Bold', 'Italic', 'Underline', 'Strike']"));
+
+		jsonArray.put(toJSONArray("['TextColor']"));
+
+		jsonArray.put(toJSONArray(
+			"['JustifyLeft', 'JustifyCenter'," +
+			"'JustifyRight', 'JustifyBlock']"));
+
+		jsonArray.put(toJSONArray(
+			"['NumberedList', 'BulletedList', '-', 'Outdent', " +
+			" 'Indent', '-', 'Blockquote', '-', 'Code']"));
+
+		jsonArray.put("/");
+
+		jsonArray.put(toJSONArray("['Format', 'Font', 'FontSize']"));
+
+		jsonArray.put(toJSONArray("['Link', 'Unlink']"));
+
+		jsonArray.put(toJSONArray("['ImageSelector', '-', 'Smiley']"));
+
+		jsonArray.put("/");
+
+		jsonArray.put(toJSONArray("['Cut', 'Copy', 'Paste', '-', " +
+			"'SelectAll', '-', 'Undo', 'Redo']"));
+
+		if (isShowSource(inputEditorTaglibAttributes)) {
+			jsonArray.put(toJSONArray("['Source']"));
+		}
+
+		jsonArray.put(toJSONArray("['A11YBtn']"));
+
+		return jsonArray;
+	}
+
+	protected JSONArray getToolbarsPhoneJSONArray(
+		Map<String, Object> inputEditorTaglibAttributes) {
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		jsonArray.put(toJSONArray("['Bold', 'Italic', 'Underline']"));
+
+		jsonArray.put(toJSONArray("['NumberedList', 'BulletedList']"));
+
+		jsonArray.put(toJSONArray("['Link', 'Unlink']"));
+
+		jsonArray.put(toJSONArray("['ImageSelector']"));
+
+		if (isShowSource(inputEditorTaglibAttributes)) {
+			jsonArray.put(toJSONArray("['Source']"));
+		}
+
+		return jsonArray;
+	}
+
+	protected JSONArray getToolbarsTabletJSONArray(
+		Map<String, Object> inputEditorTaglibAttributes) {
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		jsonArray.put(toJSONArray("['Bold', 'Italic', 'Underline', 'Strike']"));
+
+		jsonArray.put(toJSONArray("['JustifyLeft', 'JustifyCenter', " +
+			"'JustifyRight', 'JustifyBlock']"));
+
+		jsonArray.put(toJSONArray("['NumberedList', 'BulletedList']"));
+
+		jsonArray.put(toJSONArray("['Styles', 'FontSize']"));
+
+		jsonArray.put(toJSONArray("['Link', 'Unlink']"));
+
+		jsonArray.put(toJSONArray("['ImageSelector']"));
+
+		if (isShowSource(inputEditorTaglibAttributes)) {
+			jsonArray.put(toJSONArray("['Source']"));
+		}
+
+		return jsonArray;
+	}
+
+	private static final Integer _CKEDITOR_ENTER_BR = 2;
+
+}