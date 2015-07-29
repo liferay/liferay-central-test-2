79ce1d85c9db93b938c812ce6b32b067ac262c18
Roberto Díaz <roberto.diaz@liferay.com>
2015-07-29 11:04:07 +0200

    LPS-57354 Create new class and helper class [Copy and paste]

---
+++ modules/apps/blogs/blogs-web/src/com/liferay/blogs/web/portlet/action/UploadItemSelectorImageMVCActionCommand.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.blogs.web.portlet.action;
+
+import com.liferay.blogs.web.constants.BlogsPortletKeys;
+import com.liferay.blogs.web.image.selector.EditorImageSelectorUploadHandler;
+import com.liferay.portal.kernel.image.selector.ImageSelectorUploadHandler;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Roberto Díaz
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + BlogsPortletKeys.BLOGS,
+		"javax.portlet.name=" + BlogsPortletKeys.BLOGS_ADMIN,
+		"mvc.command.name=/blogs/upload_editor_image"
+	},
+	service = MVCActionCommand.class
+)
+public class UploadEditorImageMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		_imageSelectorUploadHandler.uploadSelectedImage(
+			actionRequest, actionResponse);
+	}
+
+	private final ImageSelectorUploadHandler _imageSelectorUploadHandler =
+		new EditorImageSelectorUploadHandler();
+
+}
