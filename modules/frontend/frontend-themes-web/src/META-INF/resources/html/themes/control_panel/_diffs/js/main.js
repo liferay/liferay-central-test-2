87cb4898c3ef6bf160296c481b29e3600c55d44e
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2015-07-30 11:07:52 -0700

    LPS-57450 - Moving the JS to main.js, going to try to find a way to not even need this on the theme layer

--- modules/frontend/frontend-themes-web/src/META-INF/resources/html/themes/control_panel/_diffs/js/main.js
+++ modules/frontend/frontend-themes-web/src/META-INF/resources/html/themes/control_panel/_diffs/js/main.js
@@ -1,2 +1,18 @@
 Liferay.Util.portletTitleEdit = function() {
 };
+
+if (themeDisplay.isSignedIn()) {
+	AUI.$(
+		function() {
+			AUI.$('#sidenavContainerId').sideNavigation(
+				{
+					gutter: '0',
+					toggler: '#sidenavToggleId',
+					type: 'fixed-push',
+					typeMobile: 'fixed',
+					width: '320px'
+				}
+			);
+		}
+	);
+}
