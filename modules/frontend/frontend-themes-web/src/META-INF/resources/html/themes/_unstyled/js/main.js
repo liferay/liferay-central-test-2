fd9d81da8d89e49dcbf4c68d42ddf09183458609
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2015-07-30 11:11:29 -0700

    LPS-57450 - Reverting last commit, effect is too jarring, but leaving in the history so we know it needs to be fixed.
    This reverts commit 3be24fd9e4ab10fd24270c862986ad9f79eb4750.

--- modules/frontend/frontend-themes-web/src/META-INF/resources/html/themes/_unstyled/js/main.js
+++ modules/frontend/frontend-themes-web/src/META-INF/resources/html/themes/_unstyled/js/main.js
@@ -33,19 +33,3 @@ Liferay.on(
 	function() {
 	}
 );
-
-if (themeDisplay.isSignedIn()) {
-	AUI.$(
-		function() {
-			AUI.$('#sidenavContainerId').sideNavigation(
-				{
-					gutter: '0',
-					toggler: '#sidenavToggleId',
-					type: 'fixed-push',
-					typeMobile: 'fixed',
-					width: '320px'
-				}
-			);
-		}
-	);
-}
--- modules/frontend/frontend-themes-web/src/META-INF/resources/html/themes/classic/_diffs/js/main.js
+++ modules/frontend/frontend-themes-web/src/META-INF/resources/html/themes/classic/_diffs/js/main.js
@@ -13,17 +13,5 @@ AUI().ready(
 		if (signIn && signIn.getData('redirect') !== 'true') {
 			signIn.plug(Liferay.SignInModal);
 		}
-
-		if (themeDisplay.isSignedIn()) {
-			AUI.$('#sidenavContainerId').sideNavigation(
-				{
-					gutter: '0',
-					toggler: '#sidenavToggleId',
-					type: 'fixed-push',
-					typeMobile: 'fixed',
-					width: '320px'
-				}
-			);
-		}
 	}
 );
