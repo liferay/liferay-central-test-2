4a07389b5052c05193a5320f542af1be83d55b59
Máté Thurzó <mate.thurzo@liferay.com>
2015-07-30 16:06:05 +0200

    LPS-57519 Cleanup MDR from core

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -313,24 +313,6 @@ create unique index IX_33781904 on MBThreadFlag (userId, threadId);
 create index IX_DCE308C5 on MBThreadFlag (uuid_, companyId);
 create unique index IX_FEB0FC87 on MBThreadFlag (uuid_, groupId);
 
-create index IX_FD90786C on MDRAction (ruleGroupInstanceId);
-create index IX_C58A516B on MDRAction (uuid_, companyId);
-create unique index IX_75BE36AD on MDRAction (uuid_, groupId);
-
-create index IX_4F4293F1 on MDRRule (ruleGroupId);
-create index IX_7DEA8DF1 on MDRRule (uuid_, companyId);
-create unique index IX_F3EFDCB3 on MDRRule (uuid_, groupId);
-
-create index IX_5849891C on MDRRuleGroup (groupId);
-create index IX_CC14DC2 on MDRRuleGroup (uuid_, companyId);
-create unique index IX_46665CC4 on MDRRuleGroup (uuid_, groupId);
-
-create unique index IX_808A0036 on MDRRuleGroupInstance (classNameId, classPK, ruleGroupId);
-create index IX_22DAB85C on MDRRuleGroupInstance (groupId, classNameId, classPK);
-create index IX_BF3E642B on MDRRuleGroupInstance (ruleGroupId);
-create index IX_25C9D1F7 on MDRRuleGroupInstance (uuid_, companyId);
-create unique index IX_9CBC6A39 on MDRRuleGroupInstance (uuid_, groupId);
-
 create index IX_C28C72EC on MembershipRequest (groupId, statusId);
 create index IX_35AA8FA6 on MembershipRequest (groupId, userId, statusId);
 create index IX_66D70879 on MembershipRequest (userId);
