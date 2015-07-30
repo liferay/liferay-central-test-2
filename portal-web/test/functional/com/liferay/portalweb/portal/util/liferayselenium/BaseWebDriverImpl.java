445b7ce5b99216574296b6a37d0ef05bb63281e4
Karen Dang <karen.dang@liferay.com>
2015-07-30 15:03:26 -0700

    LRQA-17072 Create a new method to check if image is present and change sikuliClick to only click on the image if found to avoild NullPointExceptions

--- portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/BaseWebDriverImpl.java
+++ portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/BaseWebDriverImpl.java
@@ -529,6 +529,11 @@ public abstract class BaseWebDriverImpl
 	}
 
 	@Override
+	public boolean isSikuliImagePresent(String image) throws Exception {
+		return LiferaySeleniumHelper.isSikuliImagePresent(this, image);
+	}
+
+	@Override
 	public boolean isTCatEnabled() {
 		return LiferaySeleniumHelper.isTCatEnabled();
 	}
