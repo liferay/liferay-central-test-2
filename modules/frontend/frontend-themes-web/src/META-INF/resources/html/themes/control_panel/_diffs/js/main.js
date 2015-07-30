58ed9a9d30698c4a82f05f95cb4298dede5598b2
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2015-07-30 11:11:29 -0700

    LPS-57450 - Reverting last commit, effect is too jarring, but leaving in the history so we know it needs to be fixed.
    This reverts commit 3be24fd9e4ab10fd24270c862986ad9f79eb4750.

--- modules/frontend/frontend-themes-web/src/META-INF/resources/html/themes/control_panel/_diffs/js/main.js
+++ modules/frontend/frontend-themes-web/src/META-INF/resources/html/themes/control_panel/_diffs/js/main.js
@@ -1,18 +1,2 @@
 Liferay.Util.portletTitleEdit = function() {
 };
-
-if (themeDisplay.isSignedIn()) {
-	AUI.$(
-		function() {
-			AUI.$('#sidenavContainerId').sideNavigation(
-				{
-					gutter: '0',
-					toggler: '#sidenavToggleId',
-					type: 'fixed-push',
-					typeMobile: 'fixed',
-					width: '320px'
-				}
-			);
-		}
-	);
-}
