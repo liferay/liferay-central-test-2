fc5033a9c5fcf2a02e1fe03a5aeb3a9bca5090cf
Rafael Praxedes <rafael.praxedes@liferay.com>
2015-07-30 11:11:34 -0300

    LPS-57255 Extract DDM actionKeys

--- portal-service/src/com/liferay/portal/security/permission/ActionKeys.java
+++ portal-service/src/com/liferay/portal/security/permission/ActionKeys.java
@@ -113,15 +113,11 @@ public class ActionKeys {
 
 	public static final String ADD_SHORTCUT = "ADD_SHORTCUT";
 
-	public static final String ADD_STRUCTURE = "ADD_STRUCTURE";
-
 	public static final String ADD_SUBCATEGORY = "ADD_SUBCATEGORY";
 
 	public static final String ADD_SUBFOLDER = "ADD_SUBFOLDER";
 
 	public static final String ADD_TAG = "ADD_TAG";
-
-	public static final String ADD_TEMPLATE = "ADD_TEMPLATE";
 
 	public static final String ADD_TO_PAGE = "ADD_TO_PAGE";
 
