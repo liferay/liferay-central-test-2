3252eb76a12859f162c86efe84bc0c00b42fde69
Raymond Auge <raymond.auge@liferay.com>
2015-07-30 18:59:01 -0400

    LPS-55355 fix upgrade

--- modules/apps/shopping/shopping-service/src/com/liferay/shopping/upgrade/ShoppingServiceUpgrade.java
+++ modules/apps/shopping/shopping-service/src/com/liferay/shopping/upgrade/ShoppingServiceUpgrade.java
@@ -23,11 +23,11 @@ import com.liferay.shopping.upgrade.v1_0_0.UpgradePortletId;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.ServletContext;
+
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-
-import org.springframework.context.ApplicationContext;
 
 /**
  * @author Peter Fellwock
@@ -35,21 +35,15 @@ import org.springframework.context.ApplicationContext;
 @Component(immediate = true, service = ShoppingServiceUpgrade.class)
 public class ShoppingServiceUpgrade {
 
-	@Reference(
-		target =
-			"(org.springframework.context.service.name=" +
-				"com.liferay.shopping.service)",
-		unbind = "-"
-	)
-	protected void setApplicationContext(
-		ApplicationContext applicationContext) {
-	}
-
 	@Reference(unbind = "-")
 	protected void setReleaseLocalService(
 		ReleaseLocalService releaseLocalService) {
 
 		_releaseLocalService = releaseLocalService;
+	}
+
+	@Reference(target = "(original.bean=*)", unbind = "-")
+	protected void setServletContext(ServletContext servletContext) {
 	}
 
 	@Activate
