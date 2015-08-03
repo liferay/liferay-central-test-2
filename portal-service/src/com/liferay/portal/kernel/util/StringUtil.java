f7a6a49376083e75e326839166fdb1fc62c356ae
Cody Hoag <cody.hoag@liferay.com>
2015-08-03 13:57:57 -0400

    LPS-56817 Readd Javadoc

--- portal-service/src/com/liferay/portal/kernel/util/StringUtil.java
+++ portal-service/src/com/liferay/portal/kernel/util/StringUtil.java
@@ -3887,6 +3887,28 @@ public class StringUtil {
 		return s;
 	}
 
+	/**
+	 * Returns the original string without an appended parenthetical suffix.
+	 *
+	 * <p>
+	 * Example:
+	 * <p>
+	 *
+	 * <p>
+	 * <pre>
+	 * <code>
+	 * stripParentheticalSuffix("file") returns "file"
+	 * stripParentheticalSuffix("file (0)") returns "file"
+	 * stripParentheticalSuffix("file (0 0)") returns "file"
+	 * stripParentheticalSuffix("file(0)") returns "file(0)"
+	 * </code>
+	 * </pre>
+	 * </p>
+	 *
+	 * @param  s the original string
+	 * @return a string representing the string <code>s</code> without the
+	 *         parentheticalSuffix
+	 */
 	public static String stripParentheticalSuffix(String s) {
 		int x = s.lastIndexOf(StringPool.OPEN_PARENTHESIS);
 		int y = s.lastIndexOf(StringPool.CLOSE_PARENTHESIS);
