sergiogonzalez/liferay-portal

LPS-56048 Make all module tests pass

modules/apps/message-boards/message-boards-test/test/integration/com/liferay/message/boards/social/test/BaseMBSocialActivityInterpreterTestCase.java
@@ -1,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.message.boards.social.test;
+
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portlet.social.model.SocialActivityInterpreter;
+import com.liferay.portlet.social.test.BaseSocialActivityInterpreterTestCase;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+
+import java.util.Collection;
+
+/**
+ * @author Adolfo Pérez
+ */
+public abstract class BaseMBSocialActivityInterpreterTestCase
+	extends BaseSocialActivityInterpreterTestCase {
+
+	@Override
+	protected SocialActivityInterpreter getActivityInterpreter() {
+		try {
+			Registry registry = RegistryUtil.getRegistry();
+
+			Collection<SocialActivityInterpreter> socialActivityInterpreters =
+				registry.getServices(
+					SocialActivityInterpreter.class,
+					"(javax.portlet.name=" + PortletKeys.MESSAGE_BOARDS + ")");
+
+			for (SocialActivityInterpreter socialActivityInterpreter :
+					socialActivityInterpreters) {
+
+				if (ArrayUtil.contains(
+						socialActivityInterpreter.getClassNames(),
+						getClassName())) {
+
+					return socialActivityInterpreter;
+				}
+			}
+
+			throw new IllegalStateException(
+				"No activity exception found for class " + getClassName());
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected abstract String getClassName();
+
+}

liferay/liferay-central-test-2

modules/apps/message-boards/message-boards-test/test/integration/com/liferay/message/boards/social/test/BaseMBSocialActivityInterpreterTestCase.java
@@ -1,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.message.boards.social.test;
+
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portlet.social.model.SocialActivityInterpreter;
+import com.liferay.portlet.social.test.BaseSocialActivityInterpreterTestCase;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+
+import java.util.Collection;
+
+/**
+ * @author Adolfo Pérez
+ */
+public abstract class BaseMBSocialActivityInterpreterTestCase
+	extends BaseSocialActivityInterpreterTestCase {
+
+	@Override
+	protected SocialActivityInterpreter getActivityInterpreter() {
+		try {
+			Registry registry = RegistryUtil.getRegistry();
+
+			Collection<SocialActivityInterpreter> socialActivityInterpreters =
+				registry.getServices(
+					SocialActivityInterpreter.class,
+					"(javax.portlet.name=" + PortletKeys.MESSAGE_BOARDS + ")");
+
+			for (SocialActivityInterpreter socialActivityInterpreter :
+					socialActivityInterpreters) {
+
+				if (ArrayUtil.contains(
+						socialActivityInterpreter.getClassNames(),
+						getClassName())) {
+
+					return socialActivityInterpreter;
+				}
+			}
+
+			throw new IllegalStateException(
+				"No activity interpreter found for class " + getClassName());
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected abstract String getClassName();
+
+}