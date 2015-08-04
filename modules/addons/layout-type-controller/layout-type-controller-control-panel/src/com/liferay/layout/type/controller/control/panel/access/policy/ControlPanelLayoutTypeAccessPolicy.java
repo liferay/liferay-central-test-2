3ec02384e1aa129722bc89b342d35788142c824f
Juergen Kappler <jurgen.kappler@liferay.com>
2015-08-04 12:44:42 +0200

    LPS-57392 Move java to addon: layout-type-controller-control-panel, change reference and super class

--- modules/addons/layout-type-controller/layout-type-controller-control-panel/src/com/liferay/layout/type/controller/control/panel/access/policy/ControlPanelLayoutTypeAccessPolicy.java
+++ modules/addons/layout-type-controller/layout-type-controller-control-panel/src/com/liferay/layout/type/controller/control/panel/access/policy/ControlPanelLayoutTypeAccessPolicy.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.control.panel.menu.web.layout;
+package com.liferay.layout.type.controller.control.panel.access.policy;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.util.WebKeys;
