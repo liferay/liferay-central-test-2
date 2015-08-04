a29fd878ea0086fedbb330eb0c987617fef41d22
Iván Zaera Avellón <ivan.zaera@liferay.com>
2015-08-04 16:14:36 +0200

    LPS-57402 Create upgrade process

---
+++ modules/apps/social/social-group-statistics-web/src/com/liferay/social/group/statistics/web/upgrade/GroupStatisticsWebUpgrade.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.social.group.statistics.web.upgrade;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.social.group.statistics.web.upgrade.v1_0_0.UpgradePortletId;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Iván Zaera
+ */
+@Component(immediate = true, service = GroupStatisticsWebUpgrade.class)
+public class GroupStatisticsWebUpgrade {
+
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
+	protected void setModuleServiceLifecycle(
+		ModuleServiceLifecycle moduleServiceLifecycle) {
+	}
+
+	@Reference(unbind = "-")
+	protected void setReleaseLocalService(
+		ReleaseLocalService releaseLocalService) {
+
+		_releaseLocalService = releaseLocalService;
+	}
+
+	@Activate
+	protected void upgrade() throws PortalException {
+		List<UpgradeProcess> upgradeProcesses = new ArrayList<>();
+
+		upgradeProcesses.add(new UpgradePortletId());
+
+		_releaseLocalService.updateRelease(
+			"com.liferay.social.group.statistics.web", upgradeProcesses, 1, 1,
+			false);
+	}
+
+	private ReleaseLocalService _releaseLocalService;
+
+}
