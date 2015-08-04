cc0e84d5a206eae4e64a5686ab0dd821e0ed1874
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2015-08-04 16:10:01 -0700

    LPS-51081 Porting liferay/liferay-plugins@43e020fd91046b8cf928c26a68423aa52072287f from Ant to Gradle

--- modules/sdk/gradle-plugins-js-transpiler/src/com/liferay/gradle/plugins/js/transpiler/JSTranspilerExtension.java
+++ modules/sdk/gradle-plugins-js-transpiler/src/com/liferay/gradle/plugins/js/transpiler/JSTranspilerExtension.java
@@ -43,6 +43,6 @@ public class JSTranspilerExtension {
 	}
 
 	private Object _babelVersion = "5.6.14";
-	private Object _lfrAmdLoaderVersion = "1.3.4";
+	private Object _lfrAmdLoaderVersion = "1.3.5";
 
 }
