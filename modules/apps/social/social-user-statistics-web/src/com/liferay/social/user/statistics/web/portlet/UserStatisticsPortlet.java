1563b97d2a1bad0d78ddb707b1be4f00b452c201
Iván Zaera Avellón <ivan.zaera@liferay.com>
2015-08-05 13:08:13 +0200

    LPS-57672 Move portlets from core to module

---
+++ modules/apps/social/social-user-statistics-web/src/com/liferay/social/user/statistics/web/portlet/UserStatisticsPortlet.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.social.user.statistics.web.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.social.user.statistics.web.constants.UserStatisticsPortletKeys;
+import org.osgi.service.component.annotations.Component;
+
+import javax.portlet.Portlet;
+
+/**
+ * @author Iván Zaera
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.css-class-wrapper=portlet-user-statistics",
+		"com.liferay.portlet.display-category=category.social",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=User Statistics",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + UserStatisticsPortletKeys.USER_STATISTICS,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=guest,power-user,user",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
+public class UserStatisticsPortlet extends MVCPortlet {
+
+
+
+}
--- portal-service/src/com/liferay/portal/util/PortletKeys.java
+++ portal-service/src/com/liferay/portal/util/PortletKeys.java
@@ -155,8 +155,6 @@ public class PortletKeys {
 	public static final String USER_GROUPS_ADMIN =
 		"com_liferay_user_groups_admin_web_portlet_UserGroupsAdminPortlet";
 
-	public static final String USER_STATISTICS = "180";
-
 	public static final String USERS_ADMIN = "125";
 
 	public static final String WORKFLOW_DEFINITION =
