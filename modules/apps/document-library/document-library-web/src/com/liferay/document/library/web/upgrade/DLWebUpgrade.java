75244b2c7e2ab5bd9d9b5c25666a58bf800e2b47
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-08-05 19:43:29 +0200

    LPS-56245 Create DL Upgrades

---
+++ modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/upgrade/DLWebUpgrade.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.upgrade;
+
+import com.liferay.document.library.web.upgrade.v1_0_0.UpgradePortletId;
+import com.liferay.document.library.web.upgrade.v1_0_0.UpgradePortletSettings;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
+import com.liferay.portal.kernel.settings.SettingsFactory;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.service.ReleaseLocalService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Sergio González
+ */
+@Component(immediate = true, service = DLWebUpgrade.class)
+public class DLWebUpgrade {
+
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
+	protected void setModuleServiceLifecycle(
+		ModuleServiceLifecycle moduleServiceLifecycle) {
+	}
+
+	@Reference(unbind = "-")
+	protected void setReleaseLocalService(
+		ReleaseLocalService releaseLocalService) {
+
+		_releaseLocalService = releaseLocalService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setSettingsFactory(SettingsFactory settingsFactory) {
+		_settingsFactory = settingsFactory;
+	}
+
+	@Activate
+	protected void upgrade() throws PortalException {
+		List<UpgradeProcess> upgradeProcesses = new ArrayList<>();
+
+		upgradeProcesses.add(new UpgradePortletId());
+
+		upgradeProcesses.add(new UpgradePortletSettings(_settingsFactory));
+
+		_releaseLocalService.updateRelease(
+			"com.liferay.document.library.web", upgradeProcesses, 1, 1, false);
+	}
+
+	private ReleaseLocalService _releaseLocalService;
+	private SettingsFactory _settingsFactory;
+
+}
