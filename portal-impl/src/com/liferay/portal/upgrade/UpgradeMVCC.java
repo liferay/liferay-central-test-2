4b95c826b91a10b2039e845200244f907d920527
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2015-08-05 16:14:53 +0200

    LPS-57681 Refactor the MVCC upgrade so we can reuse it

--- portal-impl/src/com/liferay/portal/upgrade/UpgradeMVCC.java
+++ portal-impl/src/com/liferay/portal/upgrade/UpgradeMVCC.java
@@ -37,6 +37,46 @@ import java.util.List;
  */
 public class UpgradeMVCC extends UpgradeProcess {
 
+	public void upgradeMVCC(DatabaseMetaData databaseMetaData, String tableName)
+		throws Exception {
+
+		String table = normalizeName(tableName, databaseMetaData);
+
+		ResultSet tableResultSet = databaseMetaData.getTables(
+			null, null, table, null);
+
+		try {
+			if (!tableResultSet.next()) {
+				_log.error("Table " + table + " does not exist");
+
+				return;
+			}
+
+			ResultSet columnResultSet = databaseMetaData.getColumns(
+				null, null, table,
+				normalizeName("mvccVersion", databaseMetaData));
+
+			try {
+				if (columnResultSet.next()) {
+					return;
+				}
+
+				runSQL(
+					"alter table " + table + " add mvccVersion LONG default 0");
+
+				if (_log.isDebugEnabled()) {
+					_log.debug("Added column mvccVersion to table " + table);
+				}
+			}
+			finally {
+				DataAccess.cleanUp(columnResultSet);
+			}
+		}
+		finally {
+			DataAccess.cleanUp(tableResultSet);
+		}
+	}
+
 	@Override
 	protected void doUpgrade() throws Exception {
 		Connection connection = null;
@@ -95,43 +135,7 @@ public class UpgradeMVCC extends UpgradeProcess {
 			DatabaseMetaData databaseMetaData, Element classElement)
 		throws Exception {
 
-		String table = classElement.attributeValue("table");
-
-		table = normalizeName(table, databaseMetaData);
-
-		ResultSet tableResultSet = databaseMetaData.getTables(
-			null, null, table, null);
-
-		try {
-			if (!tableResultSet.next()) {
-				_log.error("Table " + table + " does not exist");
-
-				return;
-			}
-
-			ResultSet columnResultSet = databaseMetaData.getColumns(
-				null, null, table,
-				normalizeName("mvccVersion", databaseMetaData));
-
-			try {
-				if (columnResultSet.next()) {
-					return;
-				}
-
-				runSQL(
-					"alter table " + table + " add mvccVersion LONG default 0");
-
-				if (_log.isDebugEnabled()) {
-					_log.debug("Added column mvccVersion to table " + table);
-				}
-			}
-			finally {
-				DataAccess.cleanUp(columnResultSet);
-			}
-		}
-		finally {
-			DataAccess.cleanUp(tableResultSet);
-		}
+		upgradeMVCC(databaseMetaData, classElement.attributeValue("table"));
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(UpgradeMVCC.class);
