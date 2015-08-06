876221bdde720bf2cbafbbd20b233e9f93e75c06
Raymond Auge <raymond.auge@liferay.com>
2015-08-06 17:56:32 -0400

    LPS-55355 fix upgrades

--- modules/apps/shopping/shopping-service/src/com/liferay/shopping/upgrade/ShoppingServiceUpgrade.java
+++ modules/apps/shopping/shopping-service/src/com/liferay/shopping/upgrade/ShoppingServiceUpgrade.java
@@ -15,6 +15,7 @@
 package com.liferay.shopping.upgrade;
 
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.service.ReleaseLocalService;
 import com.liferay.shopping.upgrade.v1_0_0.UpgradeClassNames;
@@ -22,8 +23,6 @@ import com.liferay.shopping.upgrade.v1_0_0.UpgradePortletId;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.servlet.ServletContext;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -35,15 +34,16 @@ import org.osgi.service.component.annotations.Reference;
 @Component(immediate = true, service = ShoppingServiceUpgrade.class)
 public class ShoppingServiceUpgrade {
 
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
+	protected void setModuleServiceLifecycle(
+		ModuleServiceLifecycle moduleServiceLifecycle) {
+	}
+
 	@Reference(unbind = "-")
 	protected void setReleaseLocalService(
 		ReleaseLocalService releaseLocalService) {
 
 		_releaseLocalService = releaseLocalService;
-	}
-
-	@Reference(target = "(original.bean=*)", unbind = "-")
-	protected void setServletContext(ServletContext servletContext) {
 	}
 
 	@Activate
