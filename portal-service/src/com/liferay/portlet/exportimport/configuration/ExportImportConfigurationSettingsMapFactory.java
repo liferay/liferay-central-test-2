c7105012ae24269ea779baaf7c176a452f82d35e
Cody Hoag <cody.hoag@liferay.com>
2015-08-06 15:16:21 -0400

    LPS-57332 Edit Javadoc

--- portal-service/src/com/liferay/portlet/exportimport/configuration/ExportImportConfigurationSettingsMapFactory.java
+++ portal-service/src/com/liferay/portlet/exportimport/configuration/ExportImportConfigurationSettingsMapFactory.java
@@ -40,10 +40,9 @@ import java.util.TimeZone;
 import javax.portlet.PortletRequest;
 
 /**
- * This factory class provides a convenient way to build a settings map for an
- * {@link com.liferay.portlet.exportimport.model.ExportImportConfiguration}
- * which can be used to kick-off and control an export, import or staging
- * process.
+ * Provides a way to build a settings map for an {@link
+ * com.liferay.portlet.exportimport.model.ExportImportConfiguration}, which can
+ * be used to start and control an export, import, or staging process.
  *
  * @author Daniel Kocsis
  * @author Akos Thurzo
