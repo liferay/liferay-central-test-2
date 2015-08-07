0ef9d154a60211d70541af868fc7c82a4ddbf432
Julio Camarero <julio.camarero@liferay.com>
2015-08-07 13:23:17 +0200

    LPS-57676 filter panels by permissions

--- modules/apps/product-menu/product-menu-site-administration/src/com/liferay/product/menu/site/administration/application/list/SiteAdministrationPanelCategory.java
+++ modules/apps/product-menu/product-menu-site-administration/src/com/liferay/product/menu/site/administration/application/list/SiteAdministrationPanelCategory.java
@@ -17,7 +17,12 @@ package com.liferay.product.menu.site.administration.application.list;
 import com.liferay.application.list.BaseJSPPanelCategory;
 import com.liferay.application.list.PanelCategory;
 import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.service.permission.GroupPermissionUtil;
 
 import java.util.Locale;
 
@@ -62,6 +67,21 @@ public class SiteAdministrationPanelCategory extends BaseJSPPanelCategory {
 	@Override
 	public String getParentCategoryKey() {
 		return PanelCategoryKeys.SITES;
+	}
+
+	@Override
+	public boolean hasAccessPermission(
+			PermissionChecker permissionChecker, Group group)
+		throws PortalException {
+
+		if (GroupPermissionUtil.contains(
+				permissionChecker, group,
+				ActionKeys.VIEW_SITE_ADMINISTRATION)) {
+
+			return true;
+		}
+
+		return false;
 	}
 
 	@Override
