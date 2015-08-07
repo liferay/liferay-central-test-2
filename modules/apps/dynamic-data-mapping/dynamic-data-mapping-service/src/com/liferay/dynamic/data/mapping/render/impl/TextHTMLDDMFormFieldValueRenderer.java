d5a1222aef28780d8dfc515586e97d7f9932e086
Bejond Shao <bejond.shao@liferay.com>
2015-08-07 17:22:04 +0800

    LPS-55749 amend last commit to fix the preview issue

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/com/liferay/dynamic/data/mapping/render/impl/TextHTMLDDMFormFieldValueRenderer.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/com/liferay/dynamic/data/mapping/render/impl/TextHTMLDDMFormFieldValueRenderer.java
@@ -56,7 +56,8 @@ public class TextHTMLDDMFormFieldValueRenderer
 	}
 
 	private static final String _HTML =
-		"<a href=\"\" onclick=\"Liferay.DDLUtil.openPreviewDialog(" +
+		"<a href=\"javascript:;\" onclick=\"" +
+			"Liferay.DDLUtil.openPreviewDialog(" +
 			"'[$DDM_FORM_FIELD_VALUE$]');\">([$PREVIEW$])</a>";
 
 }
