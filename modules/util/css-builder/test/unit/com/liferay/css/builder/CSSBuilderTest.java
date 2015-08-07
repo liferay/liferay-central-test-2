69228d70f6185a8cf0a6b71cf31a6c1a1504b802
michael.bradford@liferay.com <michael.bradford@liferay.com>
2015-08-07 14:46:19 -0700

    LPS-55156 - update paths to common css, src to tmp

--- modules/util/css-builder/test/unit/com/liferay/css/builder/CSSBuilderTest.java
+++ modules/util/css-builder/test/unit/com/liferay/css/builder/CSSBuilderTest.java
@@ -102,7 +102,7 @@ public class CSSBuilderTest {
 	private void _testSassToCssBuilder(String compiler) throws Exception {
 		CSSBuilder cssBuilder = new CSSBuilder(
 			_docrootDirName,
-			"../../frontend/frontend-common-css/src/META-INF/resources/html" +
+			"../../frontend/frontend-common-css/tmp/META-INF/resources/html" +
 				"/css/common",
 			new String[0], compiler);
 
