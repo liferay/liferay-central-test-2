d7f638951f44b2a7d7d62aad846162b2120e12d6
Máté Thurzó <mate.thurzo@liferay.com>
2015-08-07 16:45:13 +0200

    LPS-56496 Add User Groups Admin's portlet preferences processor

---
+++ modules/apps/user-groups-admin/user-groups-admin-web/src/com/liferay/user/groups/admin/web/exportimport/portlet/preferences/processor/UserGroupsAdminExportImportPreferencesProcessor.java
@@ -0,0 +1,103 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.user.groups.admin.web.exportimport.portlet.preferences.processor;
+
+import com.liferay.exportimport.portlet.preferences.processor.Capability;
+import com.liferay.exportimport.portlet.preferences.processor.ExportImportPortletPreferencesProcessor;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portlet.exportimport.lar.PortletDataContext;
+import com.liferay.portlet.exportimport.lar.PortletDataException;
+import com.liferay.user.groups.admin.web.constants.UserGroupsAdminPortletKeys;
+
+import java.util.List;
+
+import javax.portlet.PortletPreferences;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Mate Thurzo
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + UserGroupsAdminPortletKeys.USER_GROUPS_ADMIN
+	},
+	service = ExportImportPortletPreferencesProcessor.class
+)
+public class UserGroupsAdminExportImportPreferencesProcessor
+	implements ExportImportPortletPreferencesProcessor {
+
+	@Override
+	public List<Capability> getExportCapabilities() {
+		return ListUtil.toList(
+			new Capability[] {
+				_userGroupsAdminPortletDisplayTemplateExportCapability
+			});
+	}
+
+	@Override
+	public List<Capability> getImportCapabilities() {
+		return ListUtil.toList(
+			new Capability[] {
+				_userGroupsAdminPortletDisplayTemplateImportCapability
+			});
+	}
+
+	@Override
+	public PortletPreferences processExportPortletPreferences(
+			PortletDataContext portletDataContext,
+			PortletPreferences portletPreferences)
+		throws PortletDataException {
+
+		return null;
+	}
+
+	@Override
+	public PortletPreferences processImportPortletPreferences(
+			PortletDataContext portletDataContext,
+			PortletPreferences portletPreferences)
+		throws PortletDataException {
+
+		return null;
+	}
+
+	@Reference(unbind = "-")
+	protected void
+		setUserGroupsAdminPortletDisplayTemplateExportCapability(
+			UserGroupsAdminPortletDisplayTemplateExportCapability
+				userGroupsAdminPortletDisplayTemplateExportCapability) {
+
+		_userGroupsAdminPortletDisplayTemplateExportCapability =
+			userGroupsAdminPortletDisplayTemplateExportCapability;
+	}
+
+	@Reference(unbind = "-")
+	protected void
+		setUserGroupsAdminPortletDisplayTemplateImportCapability(
+			UserGroupsAdminPortletDisplayTemplateImportCapability
+				userGroupsAdminPortletDisplayTemplateImportCapability) {
+
+		_userGroupsAdminPortletDisplayTemplateImportCapability =
+			userGroupsAdminPortletDisplayTemplateImportCapability;
+	}
+
+	private UserGroupsAdminPortletDisplayTemplateExportCapability
+		_userGroupsAdminPortletDisplayTemplateExportCapability;
+	private UserGroupsAdminPortletDisplayTemplateImportCapability
+		_userGroupsAdminPortletDisplayTemplateImportCapability;
+
+}
