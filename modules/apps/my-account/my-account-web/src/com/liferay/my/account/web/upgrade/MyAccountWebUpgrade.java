d9b68577e6cbe72073a6c04d0d9ebf05f803da8e
Pei-Jung Lan <pei-jung.lan@liferay.com>
2015-08-07 10:41:07 -0700

    LPS-55591 MyAccount - add portlet upgrade component

---
+++ modules/apps/my-account/my-account-web/src/com/liferay/my/account/web/upgrade/MyAccountWebUpgrade.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.my.account.web.upgrade;
+
+import com.liferay.my.account.web.constants.MyAccountPortletKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.portal.upgrade.util.UpgradePortletId;
+
+import java.util.Collections;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pei-Jung Lan
+ */
+@Component(immediate = true, service = MyAccountWebUpgrade.class)
+public class MyAccountWebUpgrade {
+
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
+	protected void setModuleServiceLifecycle(
+		ModuleServiceLifecycle moduleServiceLifecycle) {
+	}
+
+	@Reference(unbind = "-")
+	protected void setReleaseLocalService(
+		ReleaseLocalService releaseLocalService) {
+
+		_releaseLocalService = releaseLocalService;
+	}
+
+	@Activate
+	protected void upgrade() throws PortalException {
+		UpgradePortletId upgradePortletId = new UpgradePortletId() {
+
+			@Override
+			protected String[][] getRenamePortletIdsArray() {
+				return new String[][] {
+					new String[] {
+						"2", MyAccountPortletKeys.MY_ACCOUNT
+					}
+				};
+			}
+
+		};
+
+		_releaseLocalService.updateRelease(
+			"com.liferay.my.account.web",
+			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 1,
+			false);
+	}
+
+	private ReleaseLocalService _releaseLocalService;
+
+}
