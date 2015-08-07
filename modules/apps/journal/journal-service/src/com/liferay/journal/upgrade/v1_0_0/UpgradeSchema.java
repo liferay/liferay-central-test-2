0c75e8c3373ab4b8a636dc284b54b9b50b800290
Brian Chan <brian.chan@liferay.com>
2015-08-07 09:07:47 -0700

    LPS-57730 See UpgradePolls and UpgradeWiki

--- modules/apps/journal/journal-service/src/com/liferay/journal/upgrade/v1_0_0/UpgradeSchema.java
+++ modules/apps/journal/journal-service/src/com/liferay/journal/upgrade/v1_0_0/UpgradeSchema.java
@@ -15,6 +15,7 @@
 package com.liferay.journal.upgrade.v1_0_0;
 
 import com.liferay.journal.upgrade.v1_0_0.util.JournalArticleTable;
+import com.liferay.journal.upgrade.v1_0_0.util.JournalFeedTable;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.upgrade.UpgradeMVCC;
@@ -39,31 +40,37 @@ public class UpgradeSchema extends UpgradeProcess {
 			runSQL(
 				"alter_column_name JournalArticle structureId " +
 					"DDMStructureKey VARCHAR(75) null");
-
 			runSQL(
 				"alter_column_name JournalArticle templateId DDMTemplateKey " +
 					"VARCHAR(75) null");
-
 			runSQL("alter_column_type JournalArticle description TEXT null");
 
 			runSQL(
 				"alter_column_name JournalFeed structureId DDMStructureKey " +
 					"TEXT null");
-
 			runSQL(
 				"alter_column_name JournalFeed templateId DDMTemplateKey " +
 					"TEXT null");
-
 			runSQL(
 				"alter_column_name JournalFeed rendererTemplateId " +
 					"DDMRendererTemplateKey TEXT null");
 		}
 		catch (SQLException sqle) {
+
+			// JournalArticle
+
 			upgradeTable(
 				JournalArticleTable.TABLE_NAME,
 				JournalArticleTable.TABLE_COLUMNS,
 				JournalArticleTable.TABLE_SQL_CREATE,
 				JournalArticleTable.TABLE_SQL_ADD_INDEXES);
+
+			// JournalFeed
+
+			upgradeTable(
+				JournalFeedTable.TABLE_NAME, JournalFeedTable.TABLE_COLUMNS,
+				JournalFeedTable.TABLE_SQL_CREATE,
+				JournalFeedTable.TABLE_SQL_ADD_INDEXES);
 		}
 	}
 
