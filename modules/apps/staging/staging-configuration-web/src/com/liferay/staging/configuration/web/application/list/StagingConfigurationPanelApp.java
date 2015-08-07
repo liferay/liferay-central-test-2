a849f9e04cd29775d07665c2f8ea6964ca64784a
Levente Hudak <levente.hudak@liferay.com>
2015-08-07 13:26:31 +0200

    LPS-57736 Creating the staging product menu

---
+++ modules/apps/staging/staging-configuration-web/src/com/liferay/staging/configuration/web/application/list/StagingConfigurationPanelApp.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.staging.configuration.web.application.list;
+
+import com.liferay.application.list.BaseControlPanelEntryPanelApp;
+import com.liferay.application.list.PanelApp;
+import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.portal.service.PortletLocalService;
+import com.liferay.staging.configuration.web.portlet.constants.StagingConfigurationPortletKeys;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Levente Hudák
+ */
+@Component(
+	immediate = true,
+	property = {
+		"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_PUBLISHING_TOOLS,
+		"service.ranking:Integer=100"
+	},
+	service = PanelApp.class
+)
+public class StagingConfigurationPanelApp
+	extends BaseControlPanelEntryPanelApp {
+
+	@Override
+	public String getParentCategoryKey() {
+		return PanelCategoryKeys.SITE_ADMINISTRATION_PUBLISHING_TOOLS;
+	}
+
+	@Override
+	public String getPortletId() {
+		return StagingConfigurationPortletKeys.STAGING_CONFIGURATION;
+	}
+
+	@Reference(unbind = "-")
+	protected void setPortletLocalService(
+		PortletLocalService portletLocalService) {
+
+		_portletLocalService = portletLocalService;
+	}
+
+}
