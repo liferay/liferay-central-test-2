sergiogonzalez/liferay-portal

LPS-57357 Move some properties to new Configuration API

modules/apps/social/social-activity-api/src/com/liferay/social/activity/configuration/settings/internal/SocialActivityServiceSettingsIdMapping.java
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.social.activity.configuration.settings.internal;
+
+import com.liferay.portal.kernel.settings.definition.SettingsIdMapping;
+import com.liferay.portlet.social.model.SocialActivityConstants;
+import com.liferay.social.activity.configuration.SocialActivityGroupServiceConfiguration;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Roberto Díaz
+ */
+@Component
+public class SocialActivityServiceSettingsIdMapping
+	implements SettingsIdMapping {
+
+	@Override
+	public Class<?> getConfigurationBeanClass() {
+		return SocialActivityGroupServiceConfiguration.class;
+	}
+
+	@Override
+	public String getSettingsId() {
+		return SocialActivityConstants.SERVICE_NAME;
+	}
+
+}

modules/apps/social/social-activity-web/src/com/liferay/social/activity/web/util/SocialActivityUtil.java
@@ -1,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.social.activity.web.util;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.settings.CompanyServiceSettingsLocator;
+import com.liferay.portal.kernel.settings.SettingsFactory;
+import com.liferay.portal.kernel.settings.SettingsFactoryUtil;
+import com.liferay.portlet.social.model.SocialActivityConstants;
+import com.liferay.social.activity.configuration.SocialActivityGroupServiceConfiguration;
+
+/**
+ * @author Roberto Díaz
+ */
+public class SocialActivityUtil {
+
+	public static SocialActivityGroupServiceConfiguration
+		getSocialActivityGroupServiceConfiguration(
+			long companyId)
+		throws PortalException {
+
+		SettingsFactory settingsFactory =
+			SettingsFactoryUtil.getSettingsFactory();
+
+		return settingsFactory.getSettings(
+			SocialActivityGroupServiceConfiguration.class,
+			new CompanyServiceSettingsLocator(
+				companyId, SocialActivityConstants.SERVICE_NAME));
+	}
+
+}

liferay/liferay-central-test-2

modules/apps/social/social-activity-api/src/com/liferay/social/activity/configuration/settings/internal/SocialActivityServiceSettingsIdMapping.java
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.social.activity.configuration.settings.internal;
+
+import com.liferay.portal.kernel.settings.definition.SettingsIdMapping;
+import com.liferay.portlet.social.constants.SocialConstants;
+import com.liferay.social.activity.configuration.SocialActivityGroupServiceConfiguration;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Roberto Díaz
+ */
+@Component
+public class SocialActivityServiceSettingsIdMapping
+	implements SettingsIdMapping {
+
+	@Override
+	public Class<?> getConfigurationBeanClass() {
+		return SocialActivityGroupServiceConfiguration.class;
+	}
+
+	@Override
+	public String getSettingsId() {
+		return SocialConstants.SERVICE_NAME;
+	}
+
+}

modules/apps/social/social-activity-web/src/com/liferay/social/activity/web/util/SocialActivityUtil.java
@@ -1,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.social.activity.web.util;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.settings.CompanyServiceSettingsLocator;
+import com.liferay.portal.kernel.settings.SettingsFactory;
+import com.liferay.portal.kernel.settings.SettingsFactoryUtil;
+import com.liferay.portlet.social.constants.SocialConstants;
+import com.liferay.social.activity.configuration.SocialActivityGroupServiceConfiguration;
+
+/**
+ * @author Roberto Díaz
+ */
+public class SocialActivityUtil {
+
+	public static SocialActivityGroupServiceConfiguration
+		getSocialActivityGroupServiceConfiguration(
+			long companyId)
+		throws PortalException {
+
+		SettingsFactory settingsFactory =
+			SettingsFactoryUtil.getSettingsFactory();
+
+		return settingsFactory.getSettings(
+			SocialActivityGroupServiceConfiguration.class,
+			new CompanyServiceSettingsLocator(
+				companyId, SocialConstants.SERVICE_NAME));
+	}
+
+}