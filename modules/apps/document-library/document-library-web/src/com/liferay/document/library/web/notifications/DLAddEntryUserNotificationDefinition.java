sergiogonzalez/liferay-portal

LPS-56703 User notifications

modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/notifications/DLAddEntryUserNotificationDefinition.java
@@ -1,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.notifications;
+
+import com.liferay.document.library.web.constants.DLPortletKeys;
+import com.liferay.portal.kernel.notifications.UserNotificationDefinition;
+import com.liferay.portal.kernel.notifications.UserNotificationDeliveryType;
+import com.liferay.portal.model.UserNotificationDeliveryConstants;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Sergio González
+ */
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY},
+	service = UserNotificationDefinition.class
+)
+public class DLAddEntryUserNotificationDefinition
+	extends UserNotificationDefinition {
+
+	public DLAddEntryUserNotificationDefinition() {
+		super(
+			DLPortletKeys.DOCUMENT_LIBRARY, 0,
+			UserNotificationDefinition.NOTIFICATION_TYPE_ADD_ENTRY,
+			"receive-a-notification-when-someone-adds-a-new-document-in-a-" +
+				"folder-you-are-subscribed-to");
+
+		addUserNotificationDeliveryType(
+			new UserNotificationDeliveryType(
+				"email", UserNotificationDeliveryConstants.TYPE_EMAIL, true,
+				true));
+		addUserNotificationDeliveryType(
+			new UserNotificationDeliveryType(
+				"website", UserNotificationDeliveryConstants.TYPE_WEBSITE, true,
+				true));
+	}
+
+}

modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/notifications/DLUpdateEntryUserNotificationDefinition.java
@@ -1,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.notifications;
+
+import com.liferay.document.library.web.constants.DLPortletKeys;
+import com.liferay.portal.kernel.notifications.UserNotificationDefinition;
+import com.liferay.portal.kernel.notifications.UserNotificationDeliveryType;
+import com.liferay.portal.model.UserNotificationDeliveryConstants;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Sergio González
+ */
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY},
+	service = UserNotificationDefinition.class
+)
+public class DLUpdateEntryUserNotificationDefinition
+	extends UserNotificationDefinition {
+
+	public DLUpdateEntryUserNotificationDefinition() {
+		super(
+			DLPortletKeys.DOCUMENT_LIBRARY, 0,
+			UserNotificationDefinition.NOTIFICATION_TYPE_UPDATE_ENTRY,
+			"receive-a-notification-when-someone-updates-a-document-you-are-" +
+				"subscribed-to");
+
+		addUserNotificationDeliveryType(
+			new UserNotificationDeliveryType(
+				"email", UserNotificationDeliveryConstants.TYPE_EMAIL, true,
+				true));
+		addUserNotificationDeliveryType(
+			new UserNotificationDeliveryType(
+				"website", UserNotificationDeliveryConstants.TYPE_WEBSITE, true,
+				true));
+	}
+
+}

modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/notifications/DLUserNotificationHandler.java
@@ -12,19 +12,27 @@
  * details.
  */
 
-package com.liferay.portlet.documentlibrary.notifications;
+package com.liferay.document.library.web.notifications;
 
+import com.liferay.document.library.web.constants.DLPortletKeys;
 import com.liferay.portal.kernel.notifications.BaseModelUserNotificationHandler;
-import com.liferay.portal.util.PortletKeys;
+import com.liferay.portal.kernel.notifications.UserNotificationHandler;
+
+import org.osgi.service.component.annotations.Component;
 
 /**
  * @author Roberto Díaz
  */
-public class DocumentLibraryUserNotificationHandler
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY},
+	service = UserNotificationHandler.class
+)
+public class DLUserNotificationHandler
 	extends BaseModelUserNotificationHandler {
 
-	public DocumentLibraryUserNotificationHandler() {
-		setPortletId(PortletKeys.DOCUMENT_LIBRARY);
+	public DLUserNotificationHandler() {
+		setPortletId(DLPortletKeys.DOCUMENT_LIBRARY);
 	}
 
 }

liferay/liferay-central-test-2

modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/notifications/DLAddEntryUserNotificationDefinition.java
@@ -1,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.notifications;
+
+import com.liferay.portal.kernel.notifications.UserNotificationDefinition;
+import com.liferay.portal.kernel.notifications.UserNotificationDeliveryType;
+import com.liferay.portal.model.UserNotificationDeliveryConstants;
+import com.liferay.portal.util.PortletKeys;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Sergio González
+ */
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + PortletKeys.DOCUMENT_LIBRARY},
+	service = UserNotificationDefinition.class
+)
+public class DLAddEntryUserNotificationDefinition
+	extends UserNotificationDefinition {
+
+	public DLAddEntryUserNotificationDefinition() {
+		super(
+			PortletKeys.DOCUMENT_LIBRARY, 0,
+			UserNotificationDefinition.NOTIFICATION_TYPE_ADD_ENTRY,
+			"receive-a-notification-when-someone-adds-a-new-document-in-a-" +
+				"folder-you-are-subscribed-to");
+
+		addUserNotificationDeliveryType(
+			new UserNotificationDeliveryType(
+				"email", UserNotificationDeliveryConstants.TYPE_EMAIL, true,
+				true));
+		addUserNotificationDeliveryType(
+			new UserNotificationDeliveryType(
+				"website", UserNotificationDeliveryConstants.TYPE_WEBSITE, true,
+				true));
+	}
+
+}

modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/notifications/DLUpdateEntryUserNotificationDefinition.java
@@ -1,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.notifications;
+
+import com.liferay.portal.kernel.notifications.UserNotificationDefinition;
+import com.liferay.portal.kernel.notifications.UserNotificationDeliveryType;
+import com.liferay.portal.model.UserNotificationDeliveryConstants;
+import com.liferay.portal.util.PortletKeys;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Sergio González
+ */
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + PortletKeys.DOCUMENT_LIBRARY},
+	service = UserNotificationDefinition.class
+)
+public class DLUpdateEntryUserNotificationDefinition
+	extends UserNotificationDefinition {
+
+	public DLUpdateEntryUserNotificationDefinition() {
+		super(
+			PortletKeys.DOCUMENT_LIBRARY, 0,
+			UserNotificationDefinition.NOTIFICATION_TYPE_UPDATE_ENTRY,
+			"receive-a-notification-when-someone-updates-a-document-you-are-" +
+				"subscribed-to");
+
+		addUserNotificationDeliveryType(
+			new UserNotificationDeliveryType(
+				"email", UserNotificationDeliveryConstants.TYPE_EMAIL, true,
+				true));
+		addUserNotificationDeliveryType(
+			new UserNotificationDeliveryType(
+				"website", UserNotificationDeliveryConstants.TYPE_WEBSITE, true,
+				true));
+	}
+
+}

modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/notifications/DLUserNotificationHandler.java
@@ -12,18 +12,26 @@
  * details.
  */
 
-package com.liferay.portlet.documentlibrary.notifications;
+package com.liferay.document.library.web.notifications;
 
 import com.liferay.portal.kernel.notifications.BaseModelUserNotificationHandler;
+import com.liferay.portal.kernel.notifications.UserNotificationHandler;
 import com.liferay.portal.util.PortletKeys;
+
+import org.osgi.service.component.annotations.Component;
 
 /**
  * @author Roberto Díaz
  */
-public class DocumentLibraryUserNotificationHandler
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + PortletKeys.DOCUMENT_LIBRARY},
+	service = UserNotificationHandler.class
+)
+public class DLUserNotificationHandler
 	extends BaseModelUserNotificationHandler {
 
-	public DocumentLibraryUserNotificationHandler() {
+	public DLUserNotificationHandler() {
 		setPortletId(PortletKeys.DOCUMENT_LIBRARY);
 	}
 
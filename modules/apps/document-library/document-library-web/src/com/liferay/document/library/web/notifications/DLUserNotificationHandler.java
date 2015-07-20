b7a75c917415800bc2f2c73fa9dbcd14daf0cf37
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-07-20 09:37:25 +0200

    LPS-56703 User notifications

--- modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/notifications/DLUserNotificationHandler.java
+++ modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/notifications/DLUserNotificationHandler.java
@@ -12,18 +12,26 @@
  * details.
  */
 
-package com.liferay.portlet.documentlibrary.notifications;
+package com.liferay.document.library.web.notifications;
 
 import com.liferay.portal.kernel.notifications.BaseModelUserNotificationHandler;
+import com.liferay.portal.kernel.notifications.UserNotificationHandler;
 import com.liferay.portal.util.PortletKeys;
+
+import org.osgi.service.component.annotations.Component;
 
 /**
  * @author Roberto Díaz
  */
-public class DocumentLibraryUserNotificationHandler
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + PortletKeys.DOCUMENT_LIBRARY},
+	service = UserNotificationHandler.class
+)
+public class DLUserNotificationHandler
 	extends BaseModelUserNotificationHandler {
 
-	public DocumentLibraryUserNotificationHandler() {
+	public DLUserNotificationHandler() {
 		setPortletId(PortletKeys.DOCUMENT_LIBRARY);
 	}
 
