e0c47814392c1930088bc35d459785bfe0f5be26
Jorge Díaz <jorge.diaz@liferay.com>
2015-08-11 20:02:01 +0200

    LPS-46553 Fix log message

--- modules/apps/document-library/document-library-service/src/com/liferay/document/library/verify/DLServiceVerifyProcess.java
+++ modules/apps/document-library/document-library-service/src/com/liferay/document/library/verify/DLServiceVerifyProcess.java
@@ -360,7 +360,7 @@ public class DLServiceVerifyProcess extends VerifyProcess {
 						catch (Exception e) {
 							if (_log.isWarnEnabled()) {
 								_log.warn(
-									"Unable to rename duplicate title for " +
+									"Unable to rename invalid title for " +
 										"file entry " +
 											dlFileEntry.getFileEntryId(),
 									e);
