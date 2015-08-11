eacf2adfbf8f16d6c470975084400bfc1b201d29
Julio Camarero <julio.camarero@liferay.com>
2015-08-11 09:55:43 +0200

    LPS-57746 open the personal apps in the scope of the user group

--- modules/apps/layout/layout-admin-web/src/com/liferay/layout/admin/web/application/list/MyPagesPanelApp.java
+++ modules/apps/layout/layout-admin-web/src/com/liferay/layout/admin/web/application/list/MyPagesPanelApp.java
@@ -18,10 +18,15 @@ import com.liferay.application.list.PanelApp;
 import com.liferay.application.list.constants.PanelCategoryKeys;
 import com.liferay.layout.admin.web.constants.LayoutAdminPortletKeys;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.User;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.service.PortletLocalService;
+import com.liferay.portal.util.PortalUtil;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -59,11 +64,30 @@ public class MyPagesPanelApp extends GroupPagesPanelApp {
 		return super.hasAccessPermission(permissionChecker, user.getGroup());
 	}
 
+	@Override
+	protected Group getGroup(HttpServletRequest request) {
+		Group group = null;
+
+		try {
+			User user = PortalUtil.getUser(request);
+
+			return user.getGroup();
+		}
+		catch (PortalException pe) {
+			_log.error(pe);
+		}
+
+		return group;
+	}
+
 	@Reference(unbind = "-")
 	protected void setPortletLocalService(
 		PortletLocalService portletLocalService) {
 
 		_portletLocalService = portletLocalService;
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		MyPagesPanelApp.class);
 
 }
