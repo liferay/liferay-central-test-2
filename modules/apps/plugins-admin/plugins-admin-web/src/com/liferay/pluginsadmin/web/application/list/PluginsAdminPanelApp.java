ac130ab10702e0ec7d8a23f34b9058ef8f60d6a4
peterfellwock <peter.fellwock@liferay.com>
2015-08-11 16:01:23 -0700

    LPS-57159 Add link to Configuration Apps list

---
+++ modules/apps/plugins-admin/plugins-admin-web/src/com/liferay/pluginsadmin/web/application/list/PluginsAdminPanelApp.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.pluginsadmin.web.application.list;
+
+import com.liferay.application.list.BaseControlPanelEntryPanelApp;
+import com.liferay.application.list.PanelApp;
+import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.pluginsadmin.web.constants.PluginsAdminPortletKeys;
+import com.liferay.portal.service.PortletLocalService;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Peter Fellwock
+ */
+@Component(
+	immediate = true,
+	property = {
+		"panel.category.key=" + PanelCategoryKeys.CONTROL_PANEL_APPS,
+		"service.ranking:Integer=400"
+	},
+	service = PanelApp.class
+)
+public class PluginsAdminPanelApp extends BaseControlPanelEntryPanelApp {
+
+	@Override
+	public String getParentCategoryKey() {
+		return PanelCategoryKeys.CONTROL_PANEL_APPS;
+	}
+
+	@Override
+	public String getPortletId() {
+		return PluginsAdminPortletKeys.PLUGINS_ADMIN;
+	}
+
+	@Reference(unbind = "-")
+	protected void setPortletLocalService(
+		PortletLocalService portletLocalService) {
+
+		_portletLocalService = portletLocalService;
+	}
+
+}
