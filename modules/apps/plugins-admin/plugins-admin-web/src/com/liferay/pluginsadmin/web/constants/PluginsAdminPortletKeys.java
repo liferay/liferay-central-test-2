5fdb4e15d925e134ec79231d65a906a26ed2e88f
peterfellwock <peter.fellwock@liferay.com>
2015-08-11 16:01:23 -0700

    LPS-57159 Migrate constant

---
+++ modules/apps/plugins-admin/plugins-admin-web/src/com/liferay/pluginsadmin/web/constants/PluginsAdminPortletKeys.java
@@ -0,0 +1,8 @@
+package com.liferay.pluginsadmin.web.constants;
+
+public class PluginsAdminPortletKeys {
+	
+	public static final String PLUGINS_ADMIN = 
+		"com_liferay_pluginsadmin_web_portlet_PluginsAdminPortlet";
+
+}
