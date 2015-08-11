e89995aadb4217a7fe74b9e5315a87d4ef64b657
peterfellwock <peter.fellwock@liferay.com>
2015-08-11 16:01:23 -0700

    LPS-57159 Migrate and update Java Portlet

---
+++ modules/apps/plugins-admin/plugins-admin-web/src/com/liferay/pluginsadmin/web/portlet/PluginsAdminPortlet.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.pluginsadmin.web.portlet;
+
+import com.liferay.pluginsadmin.web.constants.PluginsAdminPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Peter Fellwock
+ */
+
+@Component(
+		immediate = true,
+		property = {
+			"com.liferay.portlet.control-panel-entry-category=apps",
+			"com.liferay.portlet.control-panel-entry-weight=4.0",
+			"com.liferay.portlet.css-class-wrapper=portlet-users-admin",
+			"com.liferay.portlet.display-category=category.hidden",
+			"com.liferay.portlet.icon=/icons/plugins_admin.png",
+			"com.liferay.portlet.preferences-owned-by-group=true",
+			"com.liferay.portlet.private-request-attributes=false",
+			"com.liferay.portlet.private-session-attributes=false",
+			"com.liferay.portlet.render-weight=50",
+			"com.liferay.portlet.struts-path=plugins_admin",
+			"com.liferay.portlet.use-default-template=true",
+			"javax.portlet.display-name=Plugins Configuration",
+			"javax.portlet.expiration-cache=0",
+			"javax.portlet.init-param.template-path=/",
+			"javax.portlet.name=" + PluginsAdminPortletKeys.PLUGINS_ADMIN,
+			"javax.portlet.resource-bundle=content.Language",
+			"javax.portlet.security-role-ref=administrator",
+			"javax.portlet.supports.mime-type=text/html"
+		},
+		service = Portlet.class
+	)
+public class PluginsAdminPortlet extends MVCPortlet {
+}
