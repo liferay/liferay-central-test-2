f73ddad234122c24b192c287499aa4d9d328d653
peterfellwock <peter.fellwock@liferay.com>
2015-08-12 13:14:20 -0700

    LPS-57626 Add Java MVC Renderers

---
+++ portal-impl/src/com/liferay/portlet/login/action/ForgotPasswordMVCRenderCommand.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.login.action;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
+import com.liferay.portal.model.Company;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortletKeys;
+import com.liferay.portal.util.WebKeys;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+
+/**
+ * @author Peter Fellwock
+ */
+@OSGiBeanProperties(
+	property = {
+		"javax.portlet.name=" + PortletKeys.FAST_LOGIN,
+		"javax.portlet.name=" + PortletKeys.LOGIN,
+		"mvc.command.name=/login/forgot_password"
+	}
+)
+public class ForgotPasswordMVCRenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(
+		RenderRequest renderRequest, RenderResponse renderResponse) {
+
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)renderRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		Company company = themeDisplay.getCompany();
+
+		if (!company.isSendPassword() && !company.isSendPasswordResetLink()) {
+			return "/html/portlet/login/login.jsp";
+		}
+
+		renderResponse.setTitle(themeDisplay.translate("forgot-password"));
+
+		return "/html/portlet/login/forgot_password.jsp";
+	}
+
+}
