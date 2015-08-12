b8b9ae79410319139a91984cd7bf02ecd055896a
Brian Chan <brian.chan@liferay.com>
2015-08-11 22:02:57 -0700

    LPS-57159 Keep this here for now, see PortletKeys#NESTED_PORTLETS so we know which ones we need to remove.

--- portal-service/src/com/liferay/portal/util/PortletKeys.java
+++ portal-service/src/com/liferay/portal/util/PortletKeys.java
@@ -101,6 +101,9 @@ public class PortletKeys {
 	public static final String NESTED_PORTLETS =
 		"com_liferay_nested_portlets_web_portlet_NestedPortletsPortlet";
 
+	public static final String PLUGINS_ADMIN =
+		"com_liferay_plugins_admin_web_portlet_PluginsAdminPortlet";
+
 	public static final String PORTAL = "90";
 
 	public static final String PORTAL_SETTINGS = "130";
