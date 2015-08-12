3149ba9534abeacb4f269f6822b01ff69fe0e28f
Brian Chan <brian.chan@liferay.com>
2015-08-12 14:36:35 -0700

    LPS-57626 SF

--- portal-impl/src/com/liferay/portlet/login/action/CreateAnonymousAccountMVCActionCommand.java
+++ portal-impl/src/com/liferay/portlet/login/action/CreateAnonymousAccountMVCActionCommand.java
@@ -52,8 +52,6 @@ import com.liferay.portal.util.PropsValues;
 import com.liferay.portal.util.WebKeys;
 import com.liferay.portlet.PortletURLFactoryUtil;
 
-import java.io.IOException;
-
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.PortletConfig;
