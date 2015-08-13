7b05d8852de652a48fe2a2ecfc325fe04839e1ea
Iván Zaera Avellón <ivan.zaera@liferay.com>
2015-08-13 12:18:36 +0200

    LPS-57581 Message Boards - checkboxes are missing in Admin View

---
+++ modules/apps/message-boards/message-boards-web/src/com/liferay/message/boards/web/portlet/action/BaseViewMVCRenderCommand.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.message.boards.web.portlet.action;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.security.auth.PrincipalException;
+import com.liferay.portlet.messageboards.BannedUserException;
+import com.liferay.portlet.messageboards.NoSuchCategoryException;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Iván Zaera
+ */
+public abstract class BaseViewMVCRenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		try {
+			ActionUtil.getCategory(renderRequest);
+		}
+		catch (BannedUserException | NoSuchCategoryException |
+				PrincipalException e) {
+
+			SessionErrors.add(renderRequest, e.getClass());
+
+			return "/message_boards/error.jsp";
+		}
+		catch (RuntimeException re) {
+			throw re;
+		}
+		catch (Exception e) {
+			throw new PortletException(e);
+		}
+
+		return _jspPath;
+	}
+
+	protected BaseViewMVCRenderCommand(String jspPath) {
+		_jspPath = jspPath;
+	}
+
+	private final String _jspPath;
+
+}
