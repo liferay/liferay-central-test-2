7714a179dc29ab8f0c713d01ee3d84a42ad07ae6
Brian Chan <brian.chan@liferay.com>
2015-08-14 11:04:01 -0700

    LPS-57796 move

--- modules/portal/portal-portlet-container-test/test/integration/com/liferay/portal/portlet/container/test/RequestTest.java
+++ modules/portal/portal-portlet-container-test/test/integration/com/liferay/portal/portlet/container/test/RequestTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portlet.container.test;
+package com.liferay.portal.portlet.container.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
