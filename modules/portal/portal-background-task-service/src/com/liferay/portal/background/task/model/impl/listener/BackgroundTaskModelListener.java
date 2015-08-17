f8f3a5f56ef2f35c95fad3ec1ab85cb91fa4a865
Brian Chan <brian.chan@liferay.com>
2015-08-16 22:11:46 -0700

    LPS-57740 Match GroupModelListener in modules

--- modules/portal/portal-background-task-service/src/com/liferay/portal/background/task/model/impl/listener/BackgroundTaskModelListener.java
+++ modules/portal/portal-background-task-service/src/com/liferay/portal/background/task/model/impl/listener/BackgroundTaskModelListener.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.background.task.model.impl;
+package com.liferay.portal.background.task.model.listener;
 
 import com.liferay.portal.ModelListenerException;
 import com.liferay.portal.background.task.model.BackgroundTask;
