f85a48f00729b2ac09a9bd618636946011c2f97c
Michael Han <michael.han@liferay.com>
2015-08-18 03:30:00 -0500

    LPS-57980 move to module

--- modules/portal/portal-background-task-service/src/com/liferay/portal/background/task/internal/BackgroundTaskClusterMasterTokenTransitionListener.java
+++ modules/portal/portal-background-task-service/src/com/liferay/portal/background/task/internal/BackgroundTaskClusterMasterTokenTransitionListener.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.backgroundtask;
+package com.liferay.portal.background.task.internal;
 
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskManagerUtil;
 import com.liferay.portal.kernel.cluster.BaseClusterMasterTokenTransitionListener;
