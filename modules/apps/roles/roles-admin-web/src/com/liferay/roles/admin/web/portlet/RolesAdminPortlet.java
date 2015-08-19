ea2b36846a695f81111aa0631d350caa4e7e1b96
drewbrokke <drew.brokke@liferay.com>
2015-08-19 12:43:58 -0700

    LPS-57849 adds missing CSS in Roles Admin and User Groups Admin portlet registration

--- modules/apps/roles/roles-admin-web/src/com/liferay/roles/admin/web/portlet/RolesAdminPortlet.java
+++ modules/apps/roles/roles-admin-web/src/com/liferay/roles/admin/web/portlet/RolesAdminPortlet.java
@@ -87,7 +87,7 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.control-panel-entry-weight=3.0",
 		"com.liferay.portlet.css-class-wrapper=portlet-users-admin",
 		"com.liferay.portlet.display-category=category.hidden",
-		"com.liferay.portlet.footer-portlet-javascript=/js/main.js",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.icon=/icons/roles_admin.png",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
