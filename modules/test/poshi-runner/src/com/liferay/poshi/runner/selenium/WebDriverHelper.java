61acb138c21dbf89e390a04cbc9612c24c073880
Michael Hashimoto <michael.hashimoto@liferay.com>
2015-08-18 18:43:16 -0700

    LRQA-16928 Update 'assertJavaScriptErrors' to throw 'PoshiRunnerWarningException's so that they can be caught

--- modules/test/poshi-runner/src/com/liferay/poshi/runner/selenium/WebDriverHelper.java
+++ modules/test/poshi-runner/src/com/liferay/poshi/runner/selenium/WebDriverHelper.java
@@ -14,12 +14,14 @@
 
 package com.liferay.poshi.runner.selenium;
 
+import com.liferay.poshi.runner.exception.PoshiRunnerWarningException;
 import com.liferay.poshi.runner.util.CharPool;
 import com.liferay.poshi.runner.util.GetterUtil;
 import com.liferay.poshi.runner.util.HtmlUtil;
 import com.liferay.poshi.runner.util.PropsValues;
 import com.liferay.poshi.runner.util.Validator;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
@@ -113,11 +115,11 @@ public class WebDriverHelper {
 		List<JavaScriptError> javaScriptErrors = JavaScriptError.readErrors(
 			wrappedWebDriver);
 
+		List<Exception> exceptions = new ArrayList<>();
+
 		if (!javaScriptErrors.isEmpty()) {
 			for (JavaScriptError javaScriptError : javaScriptErrors) {
 				String javaScriptErrorValue = javaScriptError.toString();
-
-				System.out.println("JS_ERROR: " + javaScriptErrorValue);
 
 				if (Validator.isNotNull(ignoreJavaScriptError) &&
 					javaScriptErrorValue.contains(ignoreJavaScriptError)) {
@@ -131,12 +133,21 @@ public class WebDriverHelper {
 					continue;
 				}
 
-				Exception exception = new Exception(javaScriptErrorValue);
+				StringBuilder sb = new StringBuilder();
 
-				LiferaySeleniumHelper.addToJavaScriptExceptions(exception);
+				sb.append("JAVA_SCRIPT_ERROR: ");
+				sb.append(javaScriptErrorValue);
 
-				throw exception;
+				System.out.println(sb.toString());
+
+				exceptions.add(new PoshiRunnerWarningException(sb.toString()));
 			}
+		}
+
+		if (!exceptions.isEmpty()) {
+			LiferaySeleniumHelper.addToJavaScriptExceptions(exceptions);
+
+			throw exceptions.get(0);
 		}
 	}
 
