787193adcf5d4356a5c2664c9dab4a9101bbe5fb
Cristina González Castellano <cristina.gonzalez@liferay.com>
2015-08-20 09:33:09 +0200

    LPS-58017 Add test for BaseRatingsTestCase in portal-impl

---
+++ portal-impl/test/integration/com/liferay/portal/ratings/UserRatingsTest.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.ratings;
+
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.model.BaseModel;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.MainServletTestRule;
+import com.liferay.portlet.ratings.test.BaseRatingsTestCase;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+
+/**
+ * @author Cristina González
+ */
+public class UserRatingsTest extends BaseRatingsTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE);
+
+	@Override
+	protected BaseModel<?> addBaseModel(
+			BaseModel<?> parentBaseModel, ServiceContext serviceContext)
+		throws Exception {
+
+		return UserTestUtil.addUser(group.getGroupId());
+	}
+
+	@Override
+	protected BaseModel<?> deleteBaseModel(
+			BaseModel<?> baseModel, ServiceContext serviceContext)
+		throws Exception {
+
+		return UserLocalServiceUtil.deleteUser((User)baseModel);
+	}
+
+	@Override
+	protected Class<?> getBaseModelClass() {
+		return User.class;
+	}
+
+}
---
+++ portal-impl/test/integration/com/liferay/portlet/blogs/ratings/DLFileEntryRatingsTest.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.blogs.ratings;
+
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.model.BaseModel;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.MainServletTestRule;
+import com.liferay.portlet.documentlibrary.model.DLFileEntry;
+import com.liferay.portlet.documentlibrary.model.DLFolder;
+import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.util.test.DLTestUtil;
+import com.liferay.portlet.ratings.test.BaseRatingsTestCase;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+
+/**
+ * @author Cristina González
+ */
+public class DLFileEntryRatingsTest extends BaseRatingsTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE);
+
+	@Override
+	protected BaseModel<?> addBaseModel(
+			BaseModel<?> parentBaseModel, ServiceContext serviceContext)
+		throws Exception {
+
+		DLFolder folder = (DLFolder)parentBaseModel;
+
+		return DLTestUtil.addDLFileEntry(folder.getFolderId());
+	}
+
+	@Override
+	protected BaseModel<?> deleteBaseModel(
+			BaseModel<?> baseModel, ServiceContext serviceContext)
+		throws Exception {
+
+		return DLFileEntryLocalServiceUtil.deleteFileEntry(
+			(DLFileEntry)baseModel);
+	}
+
+	@Override
+	protected Class<?> getBaseModelClass() {
+		return DLFileEntry.class;
+	}
+
+	@Override
+	protected BaseModel<?> getParentBaseModel(
+			Group group, ServiceContext serviceContext)
+		throws Exception {
+
+		return DLTestUtil.addDLFolder(group.getGroupId());
+	}
+
+}
