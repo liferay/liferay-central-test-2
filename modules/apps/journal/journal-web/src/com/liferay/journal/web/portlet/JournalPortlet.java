6122c26c8b1a870b0e575b08a7ca01d75056d175
Adolfo Pérez <adolfo.perez@liferay.com>
2015-08-20 17:51:29 +0200

    LPS-57733 Remove redundant configuration from portlets
    All these portlets already have a PanelApp, so we need to remove these
    property declarations to avoid duplicate entries in the product menu.

--- modules/apps/journal/journal-web/src/com/liferay/journal/web/portlet/JournalPortlet.java
+++ modules/apps/journal/journal-web/src/com/liferay/journal/web/portlet/JournalPortlet.java
@@ -142,8 +142,6 @@ import org.osgi.service.component.annotations.Reference;
 	immediate = true,
 	property = {
 		"com.liferay.portlet.add-default-resource=true",
-		"com.liferay.portlet.control-panel-entry-category=site_administration.content",
-		"com.liferay.portlet.control-panel-entry-weight=2.0",
 		"com.liferay.portlet.css-class-wrapper=portlet-journal",
 		"com.liferay.portlet.display-category=category.hidden",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
