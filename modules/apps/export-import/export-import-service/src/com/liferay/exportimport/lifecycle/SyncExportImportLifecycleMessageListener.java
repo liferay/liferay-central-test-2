c6a5124c932314d866d5ca5e5fcbd6c867acb72b
Michael Han <michael.han@liferay.com>
2015-08-20 04:14:23 -0500

    LPS-58043 depend upon destination being registered

--- modules/apps/export-import/export-import-service/src/com/liferay/exportimport/lifecycle/SyncExportImportLifecycleMessageListener.java
+++ modules/apps/export-import/export-import-service/src/com/liferay/exportimport/lifecycle/SyncExportImportLifecycleMessageListener.java
@@ -14,9 +14,10 @@
 
 package com.liferay.exportimport.lifecycle;
 
+import com.liferay.portal.kernel.messaging.Destination;
+import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageListener;
-import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portlet.exportimport.lifecycle.ExportImportLifecycleEventListenerRegistryUtil;
 import com.liferay.portlet.exportimport.lifecycle.ExportImportLifecycleListener;
 
@@ -30,7 +31,7 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(
 	immediate = true,
-	property = {"destination.name=liferay/export_import_lifecycle_event_sync"},
+	property = {"destination.name=" + DestinationNames.EXPORT_IMPORT_LIFECYCLE_EVENT_SYNC},
 	service = MessageListener.class
 )
 public class SyncExportImportLifecycleMessageListener
@@ -44,9 +45,11 @@ public class SyncExportImportLifecycleMessageListener
 			getSyncExportImportLifecycleListeners();
 	}
 
-	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
-	protected void setModuleServiceLifecycle(
-		ModuleServiceLifecycle moduleServiceLifecycle) {
+	@Reference(
+		target = "(destination.name=" + DestinationNames.EXPORT_IMPORT_LIFECYCLE_EVENT_SYNC + ")",
+		unbind = "-"
+	)
+	protected void setDestination(Destination destination) {
 	}
 
 }
