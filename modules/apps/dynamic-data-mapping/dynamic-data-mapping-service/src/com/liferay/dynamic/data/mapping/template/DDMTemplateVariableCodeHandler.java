98e3f3511206c1a94110ffadcc30ef450f286762
Brian Chan <brian.chan@liferay.com>
2015-08-22 14:49:59 -0700

    LPS-51615 SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/com/liferay/dynamic/data/mapping/template/DDMTemplateVariableCodeHandler.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/com/liferay/dynamic/data/mapping/template/DDMTemplateVariableCodeHandler.java
@@ -22,7 +22,6 @@ import com.liferay.portal.kernel.template.TemplateManagerUtil;
 import com.liferay.portal.kernel.template.TemplateResource;
 import com.liferay.portal.kernel.template.TemplateVariableCodeHandler;
 import com.liferay.portal.kernel.template.TemplateVariableDefinition;
-import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 
