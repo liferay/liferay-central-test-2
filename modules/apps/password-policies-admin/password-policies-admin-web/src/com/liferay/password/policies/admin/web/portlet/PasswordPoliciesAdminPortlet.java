66b3b734d5cc1b807add97a1c8b7ab4169a24f24
drewbrokke <drew.brokke@liferay.com>
2015-08-24 12:38:59 -0700

    LPS-58115 links Password Policy resource and model permissions to the PasswordPoliciesAdmin portlet

--- modules/apps/password-policies-admin/password-policies-admin-web/src/com/liferay/password/policies/admin/web/portlet/PasswordPoliciesAdminPortlet.java
+++ modules/apps/password-policies-admin/password-policies-admin-web/src/com/liferay/password/policies/admin/web/portlet/PasswordPoliciesAdminPortlet.java
@@ -56,6 +56,7 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.control-panel-entry-category=users",
 		"com.liferay.portlet.control-panel-entry-weight=4.0",
 		"com.liferay.portlet.css-class-wrapper=portlet-users-admin",
+		"com.liferay.portlet.display-category=category.hidden",
 		"com.liferay.portlet.icon=/icons/password_policies_admin.png",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
