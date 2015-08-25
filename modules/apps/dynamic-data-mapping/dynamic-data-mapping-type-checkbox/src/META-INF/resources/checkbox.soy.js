84fc6bdad913795e032987c58fb7e2a05c15d9f8
Bruno Basto <bruno.basto@liferay.com>
2015-08-25 19:58:25 -0300

    LPS-58114 Regen

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-checkbox/src/META-INF/resources/checkbox.soy.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-checkbox/src/META-INF/resources/checkbox.soy.js
@@ -5,5 +5,5 @@ if (typeof ddm == 'undefined') { var ddm = {}; }
 
 
 ddm.checkbox = function(opt_data, opt_ignored) {
-return '\t<div class="form-group form-inline' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + '" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '"><label class="control-label" for="' + soy.$$escapeHtml(opt_data.name) + '"><input class="field" dir="' + soy.$$escapeHtml(opt_data.dir) + '" id="' + soy.$$escapeHtml(opt_data.name) + '" name="' + soy.$$escapeHtml(opt_data.name) + '" type="checkbox" ' + soy.$$escapeHtml(opt_data.status) + ' value="true" />&nbsp;' + soy.$$escapeHtml(opt_data.label) + ((opt_data.required) ? '<b>&nbsp;*</b>' : '') + '</label>' + soy.$$filterNoAutoescape(opt_data.childElementsHTML) + '</div>';
+return '\t<div class="form-group form-inline' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + '" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '"><label class="control-label" for="' + soy.$$escapeHtml(opt_data.name) + '"><input class="field" dir="' + soy.$$escapeHtml(opt_data.dir) + '" id="' + soy.$$escapeHtml(opt_data.name) + '" name="' + soy.$$escapeHtml(opt_data.name) + '" type="checkbox" ' + soy.$$escapeHtml(opt_data.status) + ' value="true" />' + soy.$$escapeHtml(opt_data.label) + ((opt_data.required) ? '<b>*</b>' : '') + '</label>' + soy.$$filterNoAutoescape(opt_data.childElementsHTML) + '</div>';
 };
