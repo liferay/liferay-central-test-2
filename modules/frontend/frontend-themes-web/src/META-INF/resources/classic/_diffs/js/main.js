5360369c763490fab791c19f89be1d8241248d2d
Juergen Kappler <jurgen.kappler@liferay.com>
2015-08-25 12:34:29 +0200

    LPS-58016 Remove code since now it's used in the default ADT

--- modules/frontend/frontend-themes-web/src/META-INF/resources/classic/_diffs/js/main.js
+++ modules/frontend/frontend-themes-web/src/META-INF/resources/classic/_diffs/js/main.js
@@ -1,13 +1,6 @@
 AUI().ready(
-	'liferay-navigation-interaction',
 	'liferay-sign-in-modal',
 	function(A) {
-		var navigation = A.one('#navigation');
-
-		if (navigation) {
-			navigation.plug(Liferay.NavigationInteraction);
-		}
-
 		var signIn = A.one('li.sign-in a');
 
 		if (signIn && signIn.getData('redirect') !== 'true') {
