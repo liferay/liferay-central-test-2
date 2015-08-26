fa619012a48319457d033a4ed1b9c5daa19bc48a
Miguel Angelo <miguel.angelo@liferay.com>
2015-08-26 11:46:30 -0300

    LPS-58615 Test Solr impl for Stats
    https://issues.liferay.com/browse/LPS-58615

---
+++ modules/portal/portal-search-solr/test/unit/com/liferay/portal/search/solr/connection/TestSolrClientManager.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.solr.connection;
+
+import aQute.bnd.annotation.metatype.Configurable;
+
+import com.liferay.portal.search.solr.configuration.SolrConfiguration;
+import com.liferay.portal.search.solr.internal.connection.ReplicatedSolrClientFactory;
+import com.liferay.portal.search.solr.internal.http.BasicAuthPoolingHttpClientFactory;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+import org.mockito.Mockito;
+
+import org.osgi.service.component.ComponentContext;
+
+/**
+ * @author Miguel Angelo Caldas Gallindo
+ */
+public class TestSolrClientManager extends SolrClientManager {
+
+	public TestSolrClientManager(
+			HashMap<String, Object> configurationProperties)
+		throws Exception {
+
+		BasicAuthPoolingHttpClientFactory httpClientFactory =
+			new BasicAuthPoolingHttpClientFactory() {
+
+				{
+					activate(Collections.<String, Object>emptyMap());
+				}
+			};
+
+		SolrConfiguration solrConfiguration = Configurable.createConfigurable(
+			SolrConfiguration.class, configurationProperties);
+
+		Map<String, Object> properties = new HashMap<>();
+
+		properties.put("type", solrConfiguration.authenticationMode());
+
+		setHttpClientFactory(httpClientFactory, properties);
+
+		properties.put("type", solrConfiguration.clientType());
+
+		setSolrClientFactory(new ReplicatedSolrClientFactory(), properties);
+
+		ComponentContext componentContext = Mockito.mock(
+			ComponentContext.class);
+
+		Mockito.when(
+			componentContext.getProperties()
+		).thenReturn(
+			new Hashtable<String, Object>(configurationProperties)
+		);
+
+		activate(componentContext);
+	}
+
+}
---
+++ modules/portal/portal-search-solr/test/unit/com/liferay/portal/search/solr/internal/stats/StatisticsTest.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.solr.internal.stats;
+
+import com.liferay.portal.search.solr.internal.SolrIndexingFixture;
+import com.liferay.portal.search.unit.test.util.IndexingFixture;
+import com.liferay.portal.search.unit.test.util.stats.BaseStatisticsTest;
+
+import org.junit.Test;
+
+/**
+ * @author Miguel Angelo Caldas Gallindo
+ */
+public class StatisticsTest extends BaseStatisticsTest {
+
+	@Override
+	@Test
+	public void testGetStats() throws Exception {
+		super.testGetStats();
+	}
+
+	@Override
+	protected IndexingFixture createIndexingFixture() throws Exception {
+		return new SolrIndexingFixture();
+	}
+
+}
