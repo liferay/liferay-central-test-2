d986fb24221e1fa74cb87bd85a0e7159a2e81da7
philip.jones <philip.jones@liferay.com>
2015-08-26 10:17:53 -0700

    LPS-55725 move MonitoringPanelApp into module

--- modules/apps/monitoring/monitoring-web/src/com/liferay/monitoring/web/application/list/MonitoringPanelApp.java
+++ modules/apps/monitoring/monitoring-web/src/com/liferay/monitoring/web/application/list/MonitoringPanelApp.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.product.navigation.control.panel.application.list;
+package com.liferay.monitoring.web.application.list;
 
 import com.liferay.application.list.BaseControlPanelEntryPanelApp;
 import com.liferay.application.list.PanelApp;
