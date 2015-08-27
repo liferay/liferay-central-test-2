c61c66fbc47becf85c3d820acf75e734e6752938
Julio Camarero <julio.camarero@liferay.com>
2015-08-27 13:07:31 +0200

    LPS-58194 Control Panel Menu: portlet class, constants and portlet provider

---
+++ modules/apps/product-navigation/product-navigation-control-panel-menu-web/src/com/liferay/product/navigation/control/panel/menu/web/portlet/ProductNavigationControlPanelMenuViewPortletProvider.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.product.navigation.control.panel.menu.web.portlet;
+
+import com.liferay.portal.kernel.portlet.BasePortletProvider;
+import com.liferay.portal.kernel.portlet.ViewPortletProvider;
+import com.liferay.portlet.admin.util.PortalControlPanelMenuApplicationType;
+import com.liferay.product.navigation.control.panel.menu.web.constants.ProductNavigationControlPanelMenuPortletKeys;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	property = {
+		"model.class.name=" + PortalControlPanelMenuApplicationType.ControlPanelMenu.CLASS_NAME
+	},
+	service = ViewPortletProvider.class
+)
+public class ProductNavigationControlPanelMenuViewPortletProvider
+	extends BasePortletProvider implements ViewPortletProvider {
+
+	@Override
+	public String getPortletId() {
+		return ProductNavigationControlPanelMenuPortletKeys.CONTROL_PANEL_MENU;
+	}
+
+}
