d69dc03313d6fac9e11ab4e61383aa5fd5b5fa96
Gergely Mathe <gergely.mathe@liferay.com>
2015-08-27 12:35:59 +0200

    LPS-58195 Create implementation for Journal

---
+++ modules/apps/journal/journal-service/src/com/liferay/journal/service/permission/JournalArticlePermissionUpdateHandler.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.service.permission;
+
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.service.JournalArticleLocalService;
+import com.liferay.portal.security.permission.PermissionUpdateHandler;
+
+import java.util.Date;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Gergely Mathe
+ */
+@Component(
+	property = {"model.class.name=com.liferay.journal.model.JournalArticle"},
+	service = PermissionUpdateHandler.class
+)
+public class JournalArticlePermissionUpdateHandler
+	implements PermissionUpdateHandler {
+
+	@Override
+	public void updatePermission(String primKey) {
+		JournalArticle article = _journalArticleLocalService.fetchLatestArticle(
+			Long.valueOf(primKey));
+
+		article.setModifiedDate(new Date());
+
+		_journalArticleLocalService.updateJournalArticle(article);
+	}
+
+	@Reference
+	protected void setJournalArticleLocalService(
+		JournalArticleLocalService journalArticleLocalService) {
+
+		_journalArticleLocalService = journalArticleLocalService;
+	}
+
+	private JournalArticleLocalService _journalArticleLocalService;
+
+}
---
+++ modules/apps/journal/journal-service/src/com/liferay/journal/service/permission/JournalFeedPermissionUpdateHandler.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.service.permission;
+
+import com.liferay.journal.model.JournalFeed;
+import com.liferay.journal.service.JournalFeedLocalService;
+import com.liferay.portal.security.permission.PermissionUpdateHandler;
+
+import java.util.Date;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Gergely Mathe
+ */
+@Component(
+	property = {"model.class.name=com.liferay.journal.model.JournalFeed"},
+	service = PermissionUpdateHandler.class
+)
+public class JournalFeedPermissionUpdateHandler
+	implements PermissionUpdateHandler {
+
+	@Override
+	public void updatePermission(String primKey) {
+		JournalFeed feed = _journalFeedLocalService.fetchJournalFeed(
+			Long.valueOf(primKey));
+
+		feed.setModifiedDate(new Date());
+
+		_journalFeedLocalService.updateJournalFeed(feed);
+	}
+
+	@Reference
+	protected void setJournalFeedLocalService(
+		JournalFeedLocalService journalFeedLocalService) {
+
+		_journalFeedLocalService = journalFeedLocalService;
+	}
+
+	private JournalFeedLocalService _journalFeedLocalService;
+
+}
