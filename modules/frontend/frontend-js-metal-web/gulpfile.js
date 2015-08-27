c34b4110c095938c3bac93f4f2c062cf082821c2
Chema Balsas <jose.balsas@liferay.com>
2015-08-27 16:27:09 +0200

    LPS-57308 SF

--- modules/frontend/frontend-js-metal-web/gulpfile.js
+++ modules/frontend/frontend-js-metal-web/gulpfile.js
@@ -6,23 +6,34 @@ var metal = require('gulp-metal');
 var path = require('path');
 var runSequence = require('run-sequence');
 
-gulp.task('clean-build', function(callback) {
-	del(['build'], callback);
-});
+gulp.task(
+	'clean-build',
+	function(callback) {
+		del(['build'], callback);
+	}
+);
 
-gulp.task('copy-resources', function() {
-	return gulp.src(path.join('build', 'amd-jquery', '**/*'))
-		.pipe(gulp.dest(path.join('.js-cache', 'META-INF', 'resources')));
-});
+gulp.task(
+	'copy-resources',
+	function() {
+		return gulp.src(path.join('build', 'amd-jquery', '**/*'))
+			.pipe(gulp.dest(path.join('.js-cache', 'META-INF', 'resources')));
+	}
+);
 
-gulp.task('default', function(callback) {
-	runSequence('build:amd:jquery', 'copy-resources', 'clean-build', callback);
-});
+gulp.task(
+	'default',
+	function(callback) {
+		runSequence('build:amd:jquery', 'copy-resources', 'clean-build', callback);
+	}
+);
 
-metal.registerTasks({
-	buildSrc: 'bower_components/crystal-*/src/**/*.js',
-	bundleFileName: 'crystal.js',
-	cssSrc: 'bower_components/crystal-*/src/**/*.css',
-	globalName: 'crystal',
-	scssSrc: 'bower_components/crystal-*/src/**/*.scss'
-});
+metal.registerTasks(
+	{
+		buildSrc: 'bower_components/crystal-*/src/**/*.js',
+		bundleFileName: 'crystal.js',
+		cssSrc: 'bower_components/crystal-*/src/**/*.css',
+		globalName: 'crystal',
+		scssSrc: 'bower_components/crystal-*/src/**/*.scss'
+	}
+);
