csierra/liferay-portal

LPS-57922 Fix module definitions

modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-text/src/META-INF/resources/modules.js
@@ -1,0 +1,25 @@
+;(function() {
+	var PATH_DDM_TYPE_TEXT = Liferay.ThemeDisplay.getPathContext() + '/o/ddm-type-text';
+
+	AUI().applyConfig(
+		{
+			groups: {
+				'field-text': {
+					base: PATH_DDM_TYPE_CHECKBOX + '/',
+					modules: {
+						'liferay-ddm-form-field-text-template': {
+							condition: {
+								trigger: 'liferay-ddm-form-renderer'
+							},
+							path: 'text.soy.js',
+							requires: [
+								'soyutils'
+							]
+						}
+					},
+					root: PATH_DDM_TYPE_CHECKBOX + '/'
+				}
+			}
+		}
+	);
+})();

liferay/liferay-central-test-2

modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-text/src/META-INF/resources/modules.js
@@ -1,0 +1,25 @@
+;(function() {
+	var PATH_DDM_TYPE_TEXT = Liferay.ThemeDisplay.getPathContext() + '/o/ddm-type-text';
+
+	AUI().applyConfig(
+		{
+			groups: {
+				'field-text': {
+					base: PATH_DDM_TYPE_TEXT + '/',
+					modules: {
+						'liferay-ddm-form-field-text-template': {
+							condition: {
+								trigger: 'liferay-ddm-form-renderer'
+							},
+							path: 'text.soy.js',
+							requires: [
+								'soyutils'
+							]
+						}
+					},
+					root: PATH_DDM_TYPE_TEXT + '/'
+				}
+			}
+		}
+	);
+})();