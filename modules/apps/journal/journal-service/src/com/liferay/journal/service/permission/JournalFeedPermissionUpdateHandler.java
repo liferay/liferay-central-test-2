3904e5f92a5079b0ac8c2e005adcb7c328dd47ae
Brian Chan <brian.chan@liferay.com>
2015-08-31 11:06:04 -0700

    LPS-58195 Use GetterUtil

--- modules/apps/journal/journal-service/src/com/liferay/journal/service/permission/JournalFeedPermissionUpdateHandler.java
+++ modules/apps/journal/journal-service/src/com/liferay/journal/service/permission/JournalFeedPermissionUpdateHandler.java
@@ -16,6 +16,7 @@ package com.liferay.journal.service.permission;
 
 import com.liferay.journal.model.JournalFeed;
 import com.liferay.journal.service.JournalFeedLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.security.permission.PermissionUpdateHandler;
 
 import java.util.Date;
@@ -36,7 +37,7 @@ public class JournalFeedPermissionUpdateHandler
 	@Override
 	public void updatedPermission(String primKey) {
 		JournalFeed feed = _journalFeedLocalService.fetchJournalFeed(
-			Long.valueOf(primKey));
+			GetterUtil.getLong(primKey));
 
 		feed.setModifiedDate(new Date());
 
