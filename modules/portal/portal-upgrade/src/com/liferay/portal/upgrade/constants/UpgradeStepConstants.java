86f95c25f49767ea0b7ad5758f8ed6d213b4ad63
Carlos Sierra <carlos.sierra@liferay.com>
2015-08-31 11:17:54 +0200

    LPS-57681 new UpgradeStep interface for smooth transition and constants

---
+++ modules/portal/portal-upgrade/src/com/liferay/portal/upgrade/constants/UpgradeStepConstants.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.constants;
+
+/**
+ * @author Miguel Pastor
+ * @author Carlos Sierra Andrés
+ */
+public interface UpgradeStepConstants {
+
+	public String ALL_DATABASES = "ALL";
+
+	public String APPLICATION_NAME = "application.name";
+
+	public String DATABASE = "database";
+
+	public String DATABASES_ALL_PROPERTY = DATABASE + "=" + ALL_DATABASES;
+
+	public String FROM = "from";
+
+	public String TO = "to";
+
+}
