2c2e1ce25ae8898154ddf2e1b469cfd0963e63bd
Shinn Lok <shinn.lok@liferay.com>
2015-09-01 15:33:41 -0700

    LPS-58294 Test

---
+++ portal-impl/test/unit/com/liferay/portal/dao/db/OracleDBTest.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.dao.db;
+
+import com.liferay.portal.kernel.dao.db.DB;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Shinn Lok
+ */
+public class OracleDBTest extends BaseDBTestCase {
+
+	@Test
+	public void testRewordAlterColumnType() throws IOException {
+		Assert.assertEquals(
+			"alter table DLFolder modify name VARCHAR2(255 CHAR);\n",
+			buildSQL("alter_column_type DLFolder name VARCHAR(255) null;"));
+	}
+
+	@Override
+	protected DB getDB() {
+		return OracleDB.getInstance();
+	}
+
+}
