26fd751c291d593ee52aac2aed93b5c0aa7ab491
Brian Chan <brian.chan@liferay.com>
2015-08-31 19:22:21 -0700

    LPS-58281 SF

--- modules/apps/marketplace/marketplace-service/src/com/liferay/marketplace/service/permission/MarketplacePermission.java
+++ modules/apps/marketplace/marketplace-service/src/com/liferay/marketplace/service/permission/MarketplacePermission.java
@@ -15,10 +15,8 @@
 package com.liferay.marketplace.service.permission;
 
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.model.User;
 import com.liferay.portal.security.auth.PrincipalException;
 import com.liferay.portal.security.permission.PermissionChecker;
-import com.liferay.portal.service.UserLocalServiceUtil;
 
 /**
  * @author Ryan Park
