d4af11b0705ae0e8d5a6ba0f91da8c3591857cb3
Roberto Díaz <roberto.diaz@liferay.com>
2015-09-01 17:58:47 +0200

    LPS-58307 Avoid NPE

--- modules/apps/item-selector/item-selector-web/src/com/liferay/item/selector/web/util/ItemSelectorCriterionSerializer.java
+++ modules/apps/item-selector/item-selector-web/src/com/liferay/item/selector/web/util/ItemSelectorCriterionSerializer.java
@@ -193,7 +193,9 @@ public class ItemSelectorCriterionSerializer<T extends ItemSelectorCriterion> {
 			List<ItemSelectorReturnType> itemSelectorReturnTypes =
 				_itemSelectorReturnTypes.get(desiredItemSelectorReturnTypeName);
 
-			if (itemSelectorReturnTypes.isEmpty()) {
+			if ((itemSelectorReturnTypes == null) ||
+				itemSelectorReturnTypes.isEmpty()) {
+
 				if (_log.isWarnEnabled()) {
 					_log.warn(
 						"No return types are registered for " +
