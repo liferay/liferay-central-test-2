866422c014650c1d2418b241f51f4805dec56fcb
Turcsán Csaba <csaba.turcsan@liferay.com>
2015-09-02 15:43:46 +0200

    LPS-47445 Using getAboslutePath()  and toLowerCase()

--- util-java/test/unit/com/liferay/util/resiliency/spi/provider/SPIClassPathContextListenerTest.java
+++ util-java/test/unit/com/liferay/util/resiliency/spi/provider/SPIClassPathContextListenerTest.java
@@ -33,6 +33,7 @@ import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.ProxyUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
 
 import java.io.File;
 import java.io.IOException;
@@ -472,6 +473,8 @@ public class SPIClassPathContextListenerTest {
 
 		deleteFile(new File(_CONTEXT_PATH, dirName));
 
+		File file = new File(_CONTEXT_PATH, dirName);
+
 		SPIClassPathContextListener spiClassPathContextListener =
 			new SPIClassPathContextListener();
 
@@ -483,13 +486,11 @@ public class SPIClassPathContextListenerTest {
 		}
 		catch (RuntimeException re) {
 			Assert.assertEquals(
-				"Unable to find directory " + _CONTEXT_PATH + dirName,
+				"Unable to find directory " + file.getAbsolutePath(),
 				re.getMessage());
 		}
 
 		// Not a directory
-
-		File file = new File(_CONTEXT_PATH, dirName);
 
 		file.deleteOnExit();
 
@@ -503,7 +504,7 @@ public class SPIClassPathContextListenerTest {
 		}
 		catch (RuntimeException re) {
 			Assert.assertEquals(
-				"Unable to find directory " + _CONTEXT_PATH + dirName,
+				"Unable to find directory " + file.getAbsolutePath(),
 				re.getMessage());
 		}
 		finally {
@@ -511,8 +512,8 @@ public class SPIClassPathContextListenerTest {
 		}
 	}
 
-	private static final String _CONTEXT_PATH = System.getProperty(
-		"java.io.tmpdir");
+	private static final String _CONTEXT_PATH = StringUtil.toLowerCase(
+		System.getProperty("java.io.tmpdir"));
 
 	private static final String _EMBEDDED_LIB_DIR_NAME = "/embeddedLib";
 
