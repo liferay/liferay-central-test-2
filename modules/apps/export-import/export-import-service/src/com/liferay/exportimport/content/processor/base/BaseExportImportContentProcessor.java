6f11c357535a5321204d6cb9c57587eee0feb6a2
Máté Thurzó <mate.thurzo@liferay.com>
2015-09-03 14:20:20 +0200

    LPS-57248 Fix after rebase

--- modules/apps/export-import/export-import-service/src/com/liferay/exportimport/content/processor/base/BaseExportImportContentProcessor.java
+++ modules/apps/export-import/export-import-service/src/com/liferay/exportimport/content/processor/base/BaseExportImportContentProcessor.java
@@ -78,7 +78,8 @@ public class BaseExportImportContentProcessor
 		content = replaceExportDLReferences(
 			portletDataContext, stagedModel, content, exportReferencedContent);
 
-		content = replaceExportLayoutReferences(portletDataContext, content);
+		content = replaceExportLayoutReferences(
+			portletDataContext, stagedModel, content);
 
 		content = replaceExportLinksToLayouts(
 			portletDataContext, stagedModel, content);
@@ -449,7 +450,8 @@ public class BaseExportImportContentProcessor
 	}
 
 	protected String replaceExportLayoutReferences(
-			PortletDataContext portletDataContext, String content)
+			PortletDataContext portletDataContext, StagedModel stagedModel,
+			String content)
 		throws Exception {
 
 		Group group = GroupLocalServiceUtil.getGroup(
@@ -641,13 +643,13 @@ public class BaseExportImportContentProcessor
 				}
 
 				Element entityElement = portletDataContext.getExportDataElement(
-					entityStagedModel);
+					stagedModel);
 
 				Layout layout = LayoutLocalServiceUtil.fetchLayoutByFriendlyURL(
 					group.getGroupId(), privateLayout, url);
 
 				portletDataContext.addReferenceElement(
-					entityStagedModel, entityElement, layout,
+					stagedModel, entityElement, layout,
 					PortletDataContext.REFERENCE_TYPE_DEPENDENCY, true);
 			}
 			finally {
