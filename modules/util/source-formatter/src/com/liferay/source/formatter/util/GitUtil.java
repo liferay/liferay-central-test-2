73374c23a3cb20ed322d56d3c753b76867f42580
Hugo Huijser <hugo.huijser@liferay.com>
2015-09-04 10:15:00 +0800

    LPS-58330 Extract logic out to separate methods

--- modules/util/source-formatter/src/com/liferay/source/formatter/util/GitUtil.java
+++ modules/util/source-formatter/src/com/liferay/source/formatter/util/GitUtil.java
@@ -20,6 +20,7 @@ import com.liferay.source.formatter.BaseSourceProcessor;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,7 +30,53 @@ import java.util.List;
  */
 public class GitUtil {
 
-	public static List<String> getLocalChangesFileNames(String baseDirName) 
+	public static List<String> getLocalChangesFileNames(String baseDirName)
+		throws Exception {
+
+		List<String> localChangesFileNames = new ArrayList<>();
+
+		int gitLevel = getGitLevel(baseDirName);
+
+		if (gitLevel == -1) {
+			return localChangesFileNames;
+		}
+
+		InputStream gitCommandInputStream = getGitCommandInputStream(
+			"git add . --dry-run");
+
+		if (gitCommandInputStream == null) {
+			return localChangesFileNames;
+		}
+
+		String output = StringUtil.read(gitCommandInputStream);
+
+		for (String line : StringUtil.splitLines(output)) {
+			if (!line.startsWith("add '") ||
+				(StringUtil.count(line, StringPool.SLASH) < gitLevel)) {
+
+				continue;
+			}
+
+			String fileName = getFileName(
+				line.substring(5, line.length() - 1), gitLevel);
+
+			localChangesFileNames.add(fileName);
+		}
+
+		return localChangesFileNames;
+	}
+
+	protected static String getFileName(String fileName, int gitLevel) {
+		for (int i = 0; i < gitLevel; i++) {
+			int x = fileName.indexOf(StringPool.SLASH);
+
+			fileName = fileName.substring(x + 1);
+		}
+
+		return fileName;
+	}
+
+	protected static InputStream getGitCommandInputStream(String gitCommand)
 		throws Exception {
 
 		Runtime runtime = Runtime.getRuntime();
@@ -37,7 +84,7 @@ public class GitUtil {
 		Process process = null;
 
 		try {
-			process = runtime.exec("git add . --dry-run");
+			process = runtime.exec(gitCommand);
 		}
 		catch (IOException ioe) {
 			String errorMessage = ioe.getMessage();
@@ -47,56 +94,30 @@ public class GitUtil {
 					"Add Git to your PATH system variable before executing " +
 						"'ant format-source-local-changes'.");
 
-				System.exit(0);
+				return null;
 			}
 
 			throw ioe;
 		}
 
-		String content = StringUtil.read(process.getInputStream());
+		return process.getInputStream();
+	}
 
-		int gitLevel = -1;
-
+	protected static int getGitLevel(String baseDirName) {
 		for (int i = 0; i < BaseSourceProcessor.PORTAL_MAX_DIR_LEVEL; i++) {
 			File file = new File(baseDirName + ".git");
 
 			if (file.exists()) {
-				gitLevel = i;
-
-				break;
+				return i;
 			}
 
 			baseDirName = "../" + baseDirName;
 		}
 
-		if (gitLevel == -1) {
-			System.out.println(
-				"Cannot retrieve files because .git directory is missing.");
+		System.out.println(
+			"Cannot retrieve files because .git directory is missing.");
 
-			System.exit(1);
-		}
-
-		List<String> localChangesFileNames = new ArrayList<>();
-
-		for (String line : StringUtil.splitLines(content)) {
-			if (!line.startsWith("add '") ||
-				(StringUtil.count(line, StringPool.SLASH) < gitLevel)) {
-
-				continue;
-			}
-
-			line = line.substring(5, line.length() - 1);
-
-			for (int i = 0; i < gitLevel; i++) {
-				int x = line.indexOf(StringPool.SLASH);
-
-				line = line.substring(x + 1);
-			}
-
-			localChangesFileNames.add(line);
-		}
-
-		return localChangesFileNames;
+		return -1;
 	}
 
 }
