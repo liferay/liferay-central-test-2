5690c277bdc5b83c622ace35b39aa432eca1d5e5
Roberto Díaz <roberto.diaz@liferay.com>
2015-09-07 09:45:55 +0200

    LPS-58242 Cover image should save its original image in blogs repo and store the cropped image in a hidden folder

--- portal-impl/src/com/liferay/portlet/blogs/service/impl/BlogsEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/blogs/service/impl/BlogsEntryLocalServiceImpl.java
@@ -25,6 +25,7 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.notifications.UserNotificationDefinition;
 import com.liferay.portal.kernel.portlet.PortletProvider;
 import com.liferay.portal.kernel.portlet.PortletProviderUtil;
+import com.liferay.portal.kernel.repository.capabilities.TemporaryFileEntriesCapability;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.search.Indexable;
@@ -74,6 +75,7 @@ import com.liferay.portal.util.PropsValues;
 import com.liferay.portal.util.SubscriptionSender;
 import com.liferay.portlet.asset.model.AssetEntry;
 import com.liferay.portlet.asset.model.AssetLinkConstants;
+import com.liferay.portlet.blogs.BlogsEntryAttachmentFileEntryHelper;
 import com.liferay.portlet.blogs.BlogsGroupServiceSettings;
 import com.liferay.portlet.blogs.EntryContentException;
 import com.liferay.portlet.blogs.EntryDisplayDateException;
@@ -129,20 +131,7 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 	public Folder addAttachmentsFolder(long userId, long groupId)
 		throws PortalException {
 
-		ServiceContext serviceContext = new ServiceContext();
-
-		serviceContext.setAddGroupPermissions(true);
-		serviceContext.setAddGuestPermissions(true);
-
-		Repository repository = PortletFileRepositoryUtil.addPortletRepository(
-			groupId, BlogsConstants.SERVICE_NAME, serviceContext);
-
-		Folder folder = PortletFileRepositoryUtil.addPortletFolder(
-			userId, repository.getRepositoryId(),
-			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
-			BlogsConstants.SERVICE_NAME, serviceContext);
-
-		return folder;
+		return doAddFolder(userId, groupId, BlogsConstants.SERVICE_NAME);
 	}
 
 	@Override
@@ -1632,51 +1621,51 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 			ImageSelector coverImageImageSelector)
 		throws PortalException {
 
-		long coverImageId = 0;
+		FileEntry fileEntry = PortletFileRepositoryUtil.getPortletFileEntry(
+			coverImageImageSelector.getImageId());
 
-		byte[] bytes = null;
+		BlogsEntryAttachmentFileEntryHelper
+			blogsEntryAttachmentFileEntryHelper =
+				new BlogsEntryAttachmentFileEntryHelper();
 
-		try {
-			bytes = coverImageImageSelector.getCroppedImageBytes();
-		}
-		catch (IOException ioe) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(
-					"Unable to get cropped image from image selector image " +
-						coverImageImageSelector.getImageId());
-			}
-		}
+		if (fileEntry.isRepositoryCapabilityProvided(
+				TemporaryFileEntriesCapability.class)) {
 
-		if (bytes == null) {
-			return 0;
+			Folder folder = addAttachmentsFolder(userId, groupId);
+
+			blogsEntryAttachmentFileEntryHelper.
+				addBlogsEntryAttachmentFileEntry(
+					groupId, userId, entryId, folder, fileEntry.getTitle(),
+					fileEntry.getMimeType(), fileEntry.getContentStream());
 		}
 
 		File file = null;
 
 		try {
+			byte[] bytes = coverImageImageSelector.getCroppedImageBytes();
+
+			if (bytes == null) {
+				return 0;
+			}
+
 			file = FileUtil.createTempFile(bytes);
 
-			String title = coverImageImageSelector.getTitle();
-			String mimeType = coverImageImageSelector.getMimeType();
+			String title = fileEntry.getTitle();
 
 			if (Validator.isNull(title)) {
 				title =
 					StringUtil.randomString() + "_tempCroppedImage_" + entryId;
 			}
 
-			ServiceContext serviceContext = new ServiceContext();
+			Folder folder = addCoverImageFolder(userId, groupId);
 
-			serviceContext.setAddGroupPermissions(true);
-			serviceContext.setAddGuestPermissions(true);
+			FileEntry coverImageFileEntry =
+				blogsEntryAttachmentFileEntryHelper.
+					addBlogsEntryAttachmentFileEntry(
+						groupId, userId, entryId, folder, title,
+						fileEntry.getMimeType(), file);
 
-			Folder folder = addAttachmentsFolder(userId, groupId);
-
-			FileEntry fileEntry = PortletFileRepositoryUtil.addPortletFileEntry(
-				groupId, userId, BlogsEntry.class.getName(), entryId,
-				BlogsConstants.SERVICE_NAME, folder.getFolderId(), file, title,
-				mimeType, false);
-
-			coverImageId = fileEntry.getFileEntryId();
+			return coverImageFileEntry.getFileEntryId();
 		}
 		catch (IOException ioe) {
 			if (_log.isDebugEnabled()) {
@@ -1689,7 +1678,13 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 			FileUtil.delete(file);
 		}
 
-		return coverImageId;
+		return 0;
+	}
+
+	protected Folder addCoverImageFolder(long userId, long groupId)
+		throws PortalException {
+
+		return doAddFolder(userId, groupId, BlogsConstants.BLOGS_COVER_IMAGE);
 	}
 
 	protected void addDiscussion(BlogsEntry entry, long userId, long groupId)
@@ -1707,17 +1702,16 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 			String title, InputStream is)
 		throws PortalException {
 
-		ServiceContext serviceContext = new ServiceContext();
-
-		serviceContext.setAddGroupPermissions(true);
-		serviceContext.setAddGuestPermissions(true);
+		BlogsEntryAttachmentFileEntryHelper
+			blogsEntryAttachmentFileEntryHelper =
+				new BlogsEntryAttachmentFileEntryHelper();
 
 		Folder folder = addAttachmentsFolder(userId, groupId);
 
-		FileEntry fileEntry = PortletFileRepositoryUtil.addPortletFileEntry(
-			groupId, userId, BlogsEntry.class.getName(), entryId,
-			BlogsConstants.SERVICE_NAME, folder.getFolderId(), is, title,
-			mimeType, false);
+		FileEntry fileEntry =
+			blogsEntryAttachmentFileEntryHelper.
+				addBlogsEntryAttachmentFileEntry(
+					groupId, userId, entryId, folder, title, mimeType, is);
 
 		return fileEntry.getFileEntryId();
 	}
@@ -1725,6 +1719,25 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 	protected void deleteDiscussion(BlogsEntry entry) throws PortalException {
 		commentManager.deleteDiscussion(
 			BlogsEntry.class.getName(), entry.getEntryId());
+	}
+
+	protected Folder doAddFolder(long userId, long groupId, String folderName)
+		throws PortalException {
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setAddGroupPermissions(true);
+		serviceContext.setAddGuestPermissions(true);
+
+		Repository repository = PortletFileRepositoryUtil.addPortletRepository(
+			groupId, BlogsConstants.SERVICE_NAME, serviceContext);
+
+		Folder folder = PortletFileRepositoryUtil.addPortletFolder(
+			userId, repository.getRepositoryId(),
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID, folderName,
+			serviceContext);
+
+		return folder;
 	}
 
 	protected String getEntryURL(
