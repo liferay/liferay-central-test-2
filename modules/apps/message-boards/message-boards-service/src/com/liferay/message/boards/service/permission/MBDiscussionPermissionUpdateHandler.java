00e7ef073b84bd7f0dc49aa0dcab905bffa32394
Gergely Mathe <gergely.mathe@liferay.com>
2015-09-08 10:07:20 +0200

    LPS-58458 Implement permission update handlers for Message Boards

---
+++ modules/apps/message-boards/message-boards-service/src/com/liferay/message/boards/service/permission/MBDiscussionPermissionUpdateHandler.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.message.boards.service.permission;
+
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.security.permission.PermissionUpdateHandler;
+import com.liferay.portlet.messageboards.model.MBDiscussion;
+import com.liferay.portlet.messageboards.service.MBDiscussionLocalService;
+
+import java.util.Date;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Gergely Mathe
+ */
+@Component(
+	property = {"model.class.name=com.liferay.portlet.messageboards.model.MBDiscussion"},
+	service = PermissionUpdateHandler.class
+)
+public class MBDiscussionPermissionUpdateHandler
+	implements PermissionUpdateHandler {
+
+	@Override
+	public void updatedPermission(String primKey) {
+		MBDiscussion mbDiscussion = _mbDiscussionLocalService.fetchMBDiscussion(
+			GetterUtil.getLong(primKey));
+
+		if (mbDiscussion == null) {
+			return;
+		}
+
+		mbDiscussion.setModifiedDate(new Date());
+
+		_mbDiscussionLocalService.updateMBDiscussion(mbDiscussion);
+	}
+
+	@Reference
+	protected void setMBDiscussionLocalService(
+		MBDiscussionLocalService mbDiscussionLocalService) {
+
+		_mbDiscussionLocalService = mbDiscussionLocalService;
+	}
+
+	private MBDiscussionLocalService _mbDiscussionLocalService;
+
+}
