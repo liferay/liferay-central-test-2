c59a94a3bbafb396b158774c1ff728130cbc17b2
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-09-08 15:05:32 +0200

    LPS-58459 Adds css for management bar

--- modules/apps/asset/asset-categories-admin-web/src/com/liferay/asset/categories/admin/web/portlet/AssetCategoryAdminPortlet.java
+++ modules/apps/asset/asset-categories-admin-web/src/com/liferay/asset/categories/admin/web/portlet/AssetCategoryAdminPortlet.java
@@ -62,6 +62,7 @@ import org.osgi.service.component.annotations.Reference;
 	property = {
 		"com.liferay.portlet.css-class-wrapper=portlet-asset-category-admin",
 		"com.liferay.portlet.display-category=category.hidden",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.icon=/icons/asset_category_admin.png",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
