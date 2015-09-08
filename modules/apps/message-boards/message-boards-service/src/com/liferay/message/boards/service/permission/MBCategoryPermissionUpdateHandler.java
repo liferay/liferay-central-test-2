dae65e78ce6725b8016ceb1aebe14ab0654b090f
Gergely Mathe <gergely.mathe@liferay.com>
2015-09-08 10:07:20 +0200

    LPS-58458 Implement permission update handlers for Message Boards

---
+++ modules/apps/message-boards/message-boards-service/src/com/liferay/message/boards/service/permission/MBCategoryPermissionUpdateHandler.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.message.boards.service.permission;
+
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.security.permission.PermissionUpdateHandler;
+import com.liferay.portlet.messageboards.model.MBCategory;
+import com.liferay.portlet.messageboards.service.MBCategoryLocalService;
+
+import java.util.Date;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Gergely Mathe
+ */
+@Component(
+	property = {"model.class.name=com.liferay.portlet.messageboards.model.MBCategory"},
+	service = PermissionUpdateHandler.class
+)
+public class MBCategoryPermissionUpdateHandler
+	implements PermissionUpdateHandler {
+
+	@Override
+	public void updatedPermission(String primKey) {
+		MBCategory mbCategory = _mbCategoryLocalService.fetchMBCategory(
+			GetterUtil.getLong(primKey));
+
+		if (mbCategory == null) {
+			return;
+		}
+
+		mbCategory.setModifiedDate(new Date());
+
+		_mbCategoryLocalService.updateMBCategory(mbCategory);
+	}
+
+	@Reference
+	protected void setMBCategoryLocalService(
+		MBCategoryLocalService mbCategoryLocalService) {
+
+		_mbCategoryLocalService = mbCategoryLocalService;
+	}
+
+	private MBCategoryLocalService _mbCategoryLocalService;
+
+}
---
+++ modules/apps/message-boards/message-boards-service/src/com/liferay/message/boards/service/permission/MBMessagePermissionUpdateHandler.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.message.boards.service.permission;
+
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.security.permission.PermissionUpdateHandler;
+import com.liferay.portlet.messageboards.model.MBMessage;
+import com.liferay.portlet.messageboards.service.MBMessageLocalService;
+
+import java.util.Date;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Gergely Mathe
+ */
+@Component(
+	property = {"model.class.name=com.liferay.portlet.messageboards.model.MBMessage"},
+	service = PermissionUpdateHandler.class
+)
+public class MBMessagePermissionUpdateHandler
+	implements PermissionUpdateHandler {
+
+	@Override
+	public void updatedPermission(String primKey) {
+		MBMessage mbMessage = _mbMessageLocalService.fetchMBMessage(
+			GetterUtil.getLong(primKey));
+
+		if (mbMessage == null) {
+			return;
+		}
+
+		mbMessage.setModifiedDate(new Date());
+
+		_mbMessageLocalService.updateMBMessage(mbMessage);
+	}
+
+	@Reference
+	protected void setMBMessageLocalService(
+		MBMessageLocalService mbMessageLocalService) {
+
+		_mbMessageLocalService = mbMessageLocalService;
+	}
+
+	private MBMessageLocalService _mbMessageLocalService;
+
+}
