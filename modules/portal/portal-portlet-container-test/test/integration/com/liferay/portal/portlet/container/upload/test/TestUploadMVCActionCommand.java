6a7dd33edae6c7f8b43599edec240ebe56815311
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-09-09 13:21:48 +0200

    LPS-58561 Add tests to upload a file using an http client

---
+++ modules/portal/portal-portlet-container-test/test/integration/com/liferay/portal/portlet/container/upload/test/TestUploadMVCActionCommand.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.portlet.container.upload.test;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.upload.UploadHandler;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.junit.Assert;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Manuel de la Peña
+ */
+@Component(immediate = true, service = MVCActionCommand.class)
+public class TestUploadMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		_uploadHandler.upload(actionRequest, actionResponse);
+
+		Assert.assertNull(actionRequest.getAttribute(WebKeys.UPLOAD_EXCEPTION));
+	}
+
+	private final UploadHandler _uploadHandler = new TestUploadHandler();
+
+}
