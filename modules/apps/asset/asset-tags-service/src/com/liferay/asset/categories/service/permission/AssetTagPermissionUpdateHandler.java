fa1fb0efb754282b46bc17c79b5c8471da0ad7d2
Gergely Mathe <gergely.mathe@liferay.com>
2015-09-09 11:05:29 +0200

    LPS-58574 Implement permission update handlers for Asset Tags & Categories

---
+++ modules/apps/asset/asset-tags-service/src/com/liferay/asset/categories/service/permission/AssetTagPermissionUpdateHandler.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.service.permission;
+
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.security.permission.PermissionUpdateHandler;
+import com.liferay.portlet.asset.model.AssetTag;
+import com.liferay.portlet.asset.service.AssetTagLocalService;
+
+import java.util.Date;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Gergely Mathe
+ */
+@Component(
+	property = {"model.class.name=com.liferay.portlet.asset.model.AssetTag"},
+	service = PermissionUpdateHandler.class
+)
+public class AssetTagPermissionUpdateHandler
+	implements PermissionUpdateHandler {
+
+	@Override
+	public void updatedPermission(String primKey) {
+		AssetTag assetTag = _assetTagLocalService.fetchAssetTag(
+			GetterUtil.getLong(primKey));
+
+		if (assetTag == null) {
+			return;
+		}
+
+		assetTag.setModifiedDate(new Date());
+
+		_assetTagLocalService.updateAssetTag(assetTag);
+	}
+
+	@Reference
+	protected void setAssetTagLocalService(
+		AssetTagLocalService assetTagLocalService) {
+
+		_assetTagLocalService = assetTagLocalService;
+	}
+
+	private AssetTagLocalService _assetTagLocalService;
+
+}
