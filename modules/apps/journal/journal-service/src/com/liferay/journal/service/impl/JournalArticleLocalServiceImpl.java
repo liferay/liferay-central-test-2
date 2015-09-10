78d75caa5d79a52f57f5183afd070b7e69b83d77
Julio Camarero <julio.camarero@liferay.com>
2015-09-10 15:17:44 +0200

    LPS-58412 use the priority from the service context

--- modules/apps/journal/journal-service/src/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -437,13 +437,11 @@ public class JournalArticleLocalServiceImpl
 
 		// Asset
 
-		Double priority = GetterUtil.getDouble(
-			serviceContext.getAttribute("priority"));
-
 		updateAsset(
 			userId, article, serviceContext.getAssetCategoryIds(),
 			serviceContext.getAssetTagNames(),
-			serviceContext.getAssetLinkEntryIds(), priority);
+			serviceContext.getAssetLinkEntryIds(),
+			serviceContext.getAssetPriority());
 
 		// Comment
 
@@ -516,7 +514,7 @@ public class JournalArticleLocalServiceImpl
 	 * @param  serviceContext the service context to be applied. Can set the
 	 *         UUID, creation date, modification date, expando bridge
 	 *         attributes, guest permissions, group permissions, asset category
-	 *         IDs, asset tag names, asset link entry IDs, URL title, and
+	 *         IDs, asset tag names, asset link entry IDs, asset priority, URL title, and
 	 *         workflow actions for the web content article. Can also set
 	 *         whether to add the default guest and group permissions.
 	 * @return the web content article
@@ -883,7 +881,7 @@ public class JournalArticleLocalServiceImpl
 
 		updateAsset(
 			userId, newArticle, assetCategoryIds, assetTagNames,
-			assetLinkEntryIds, null);
+			assetLinkEntryIds, oldAssetEntry.getPriority());
 
 		// Dynamic data mapping
 
@@ -4928,7 +4926,7 @@ public class JournalArticleLocalServiceImpl
 	 *         article's display page
 	 * @param  serviceContext the service context to be applied. Can set the
 	 *         modification date, expando bridge attributes, asset category IDs,
-	 *         asset tag names, asset link entry IDs, workflow actions, URL
+	 *         asset tag names, asset link entry IDs, asset priority, workflow actions, URL
 	 *         title, and can set whether to add the default command update for
 	 *         the web content article. With respect to social activities, by
 	 *         setting the service context's command to {@link
@@ -5116,7 +5114,7 @@ public class JournalArticleLocalServiceImpl
 	 *         <code>null</code>)
 	 * @param  serviceContext the service context to be applied. Can set the
 	 *         modification date, expando bridge attributes, asset category IDs,
-	 *         asset tag names, asset link entry IDs, workflow actions, URL
+	 *         asset tag names, asset link entry IDs, asset priority, workflow actions, URL
 	 *         title , and can set whether to add the default command update for
 	 *         the web content article. With respect to social activities, by
 	 *         setting the service context's command to {@link
@@ -5305,13 +5303,11 @@ public class JournalArticleLocalServiceImpl
 
 		// Asset
 
-		Double priority = GetterUtil.getDouble(
-			serviceContext.getAttribute("priority"));
-
 		updateAsset(
 			userId, article, serviceContext.getAssetCategoryIds(),
 			serviceContext.getAssetTagNames(),
-			serviceContext.getAssetLinkEntryIds(), priority);
+			serviceContext.getAssetLinkEntryIds(),
+			serviceContext.getAssetPriority());
 
 		// Dynamic data mapping
 
@@ -5381,7 +5377,7 @@ public class JournalArticleLocalServiceImpl
 	 *         boolean, String, File, Map, String, ServiceContext)} description.
 	 * @param  serviceContext the service context to be applied. Can set the
 	 *         modification date, expando bridge attributes, asset category IDs,
-	 *         asset tag names, asset link entry IDs, workflow actions, URL
+	 *         asset tag names, asset link entry IDs, asset priority, workflow actions, URL
 	 *         title, and can set whether to add the default command update for
 	 *         the web content article. With respect to social activities, by
 	 *         setting the service context's command to {@link
