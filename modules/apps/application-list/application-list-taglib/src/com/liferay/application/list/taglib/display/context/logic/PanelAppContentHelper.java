893c7e8a3ea4b82367b85b362610b87a2ade74a3
shuyangzhou <shuyang.zhou@liferay.com>
2015-09-10 11:08:59 -0700

    LPS-58357 LPS-57261 Undo change to PanelAppContentHelper in 4a06868d982a001247dbd5112d58fac835612f87

--- modules/apps/application-list/application-list-taglib/src/com/liferay/application/list/taglib/display/context/logic/PanelAppContentHelper.java
+++ modules/apps/application-list/application-list-taglib/src/com/liferay/application/list/taglib/display/context/logic/PanelAppContentHelper.java
@@ -15,6 +15,7 @@
 package com.liferay.application.list.taglib.display.context.logic;
 
 import com.liferay.portal.kernel.template.StringTemplateResource;
+import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -25,7 +26,7 @@ import com.liferay.portal.model.Theme;
 import com.liferay.portal.service.LayoutTemplateLocalServiceUtil;
 import com.liferay.portal.service.PortletLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
-import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.PortletRequestImpl;
 
 import java.io.Writer;
 
@@ -73,8 +74,15 @@ public class PanelAppContentHelper {
 		if (Validator.isNotNull(velocityTemplateId) &&
 			Validator.isNotNull(content)) {
 
-			HttpServletRequest request = PortalUtil.getOriginalServletRequest(
-				_request);
+			HttpServletRequest request = _request;
+
+			PortletRequestImpl portletRequestImpl =
+				(PortletRequestImpl)_request.getAttribute(
+					JavaConstants.JAVAX_PORTLET_REQUEST);
+
+			if (portletRequestImpl != null) {
+				request = portletRequestImpl.getOriginalHttpServletRequest();
+			}
 
 			StringBundler sb = RuntimePageUtil.getProcessedTemplate(
 				request, _response, getPortletId(),
