932c144f1cdc0168f7b94e2f57e84197f14b9437
Rafael Praxedes <rafael.praxedes@liferay.com>
2015-09-10 11:26:01 -0300

    LPS-58460 Add needed OrderByComparators for DDLRecordSet

---
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/com/liferay/dynamic/data/lists/form/web/util/DDLFormPortletUtil.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.form.web.util;
+
+import com.liferay.dynamic.data.lists.model.DDLRecordSet;
+import com.liferay.dynamic.data.lists.util.comparator.DDLRecordSetCreateDateComparator;
+import com.liferay.dynamic.data.lists.util.comparator.DDLRecordSetModifiedDateComparator;
+import com.liferay.dynamic.data.lists.util.comparator.DDLRecordSetNameComparator;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+/**
+ * @author Rafael Praxedes
+ */
+public class DDLFormPortletUtil {
+
+	public static OrderByComparator<DDLRecordSet> getDDLRecordSetOrderByComparator(
+		String orderByCol, String orderByType) {
+
+		boolean orderByAsc = false;
+
+		if (orderByType.equals("asc")) {
+			orderByAsc = true;
+		}
+
+		OrderByComparator<DDLRecordSet> orderByComparator = null;
+
+		if (orderByCol.equals("create-date")) {
+			orderByComparator = new DDLRecordSetCreateDateComparator(orderByAsc);
+		}
+		else if (orderByCol.equals("modified-date")) {
+			orderByComparator = new DDLRecordSetModifiedDateComparator(orderByAsc);
+		}
+		else if (orderByCol.equals("name")) {
+			orderByComparator = new DDLRecordSetNameComparator(orderByAsc);
+		}
+
+		return orderByComparator;
+	}
+
+}
