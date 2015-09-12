3a297fcd8abc6eb4ec65f4aa339d2775492d7905
shuyangzhou <shuyang.zhou@liferay.com>
2015-09-12 09:40:44 -0700

    LPS-58589 Make ServiceBuilder to keep the String type columns' length info inside the generated generic indexes sql file, in order to provide context info to BaseDB and all its sub-classes to do proper index length restriction.

--- portal-service/src/com/liferay/portal/kernel/dao/db/IndexMetadata.java
+++ portal-service/src/com/liferay/portal/kernel/dao/db/IndexMetadata.java
@@ -44,36 +44,7 @@ public class IndexMetadata extends Index implements Comparable<IndexMetadata> {
 
 		_columnNames = columnNames;
 
-		StringBundler sb = new StringBundler(8 + columnNames.length * 2);
-
-		if (unique) {
-			sb.append("create unique ");
-		}
-		else {
-			sb.append("create ");
-		}
-
-		sb.append("index ");
-		sb.append(indexName);
-		sb.append(" on ");
-		sb.append(tableName);
-
-		sb.append(StringPool.SPACE);
-		sb.append(StringPool.OPEN_PARENTHESIS);
-
-		for (String columnName : columnNames) {
-			sb.append(columnName);
-			sb.append(StringPool.COMMA_AND_SPACE);
-		}
-
-		sb.setIndex(sb.index() - 1);
-
-		sb.append(StringPool.CLOSE_PARENTHESIS);
-		sb.append(StringPool.SEMICOLON);
-
-		_createSQL = sb.toString();
-
-		sb.setIndex(0);
+		StringBundler sb = new StringBundler(5);
 
 		sb.append("drop index ");
 		sb.append(indexName);
@@ -86,10 +57,10 @@ public class IndexMetadata extends Index implements Comparable<IndexMetadata> {
 
 	@Override
 	public int compareTo(IndexMetadata indexMetadata) {
-		String columnNames = StringUtil.merge(_columnNames);
+		String columnNames = StringUtil.merge(getColumnNames());
 
 		String indexMetadataColumnNames = StringUtil.merge(
-			indexMetadata._columnNames);
+			indexMetadata.getColumnNames());
 
 		return columnNames.compareTo(indexMetadataColumnNames);
 	}
@@ -107,7 +78,7 @@ public class IndexMetadata extends Index implements Comparable<IndexMetadata> {
 		IndexMetadata indexMetadata = (IndexMetadata)obj;
 
 		if (Validator.equals(getTableName(), indexMetadata.getTableName()) &&
-			Arrays.equals(getColumnNames(), indexMetadata.getColumnNames())) {
+			Arrays.equals(_columnNames, indexMetadata._columnNames)) {
 
 			return true;
 		}
@@ -116,11 +87,61 @@ public class IndexMetadata extends Index implements Comparable<IndexMetadata> {
 	}
 
 	public String[] getColumnNames() {
-		return _columnNames;
+		String[] columnNames = _columnNames.clone();
+
+		for (int i = 0; i < columnNames.length; i++) {
+			int index = columnNames[i].indexOf("[$COLUMN_LENGTH:");
+
+			if (index > 0) {
+				columnNames[i] = columnNames[i].substring(0, index);
+			}
+		}
+
+		return columnNames;
 	}
 
-	public String getCreateSQL() {
-		return _createSQL;
+	public String getCreateSQL(int[] lengths) {
+		int sbSize = 8 + _columnNames.length * 2;
+
+		if (lengths != null) {
+			sbSize += _columnNames.length * 3;
+		}
+
+		StringBundler sb = new StringBundler(sbSize);
+
+		if (isUnique()) {
+			sb.append("create unique ");
+		}
+		else {
+			sb.append("create ");
+		}
+
+		sb.append("index ");
+		sb.append(getIndexName());
+		sb.append(" on ");
+		sb.append(getTableName());
+
+		sb.append(StringPool.SPACE);
+		sb.append(StringPool.OPEN_PARENTHESIS);
+
+		for (int i = 0; i < _columnNames.length; i++) {
+			sb.append(_columnNames[i]);
+
+			if ((lengths != null) && (lengths[i] > 0)) {
+				sb.append("[$COLUMN_LENGTH:");
+				sb.append(lengths[i]);
+				sb.append("$]");
+			}
+
+			sb.append(StringPool.COMMA_AND_SPACE);
+		}
+
+		sb.setIndex(sb.index() - 1);
+
+		sb.append(StringPool.CLOSE_PARENTHESIS);
+		sb.append(StringPool.SEMICOLON);
+
+		return sb.toString();
 	}
 
 	public String getDropSQL() {
@@ -167,11 +188,10 @@ public class IndexMetadata extends Index implements Comparable<IndexMetadata> {
 
 	@Override
 	public String toString() {
-		return _createSQL;
+		return getCreateSQL(null);
 	}
 
 	private final String[] _columnNames;
-	private final String _createSQL;
 	private final String _dropSQL;
 
 }
