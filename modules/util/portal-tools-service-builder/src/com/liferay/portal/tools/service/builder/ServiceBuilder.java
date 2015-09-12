liferay/liferay-central-test-2

commit 1bfe17a3f65fc6052c955457d7862d8555bde7c4

LPS-53828 Consolidate logic

modules/util/portal-tools-service-builder/src/com/liferay/portal/tools/service/builder/ServiceBuilder.java
@@ -1188,6 +1188,17 @@ public class ServiceBuilder {
 		return mappingEntitiesPKList;
 	}
 
+	public int getMaxLength(String model, String field) {
+		Map<String, String> hints = ModelHintsUtil.getHints(model, field);
+
+		if (hints == null) {
+			return _DEFAULT_COLUMN_MAX_LENGTH;
+		}
+
+		return GetterUtil.getInteger(
+			hints.get("max-length"), _DEFAULT_COLUMN_MAX_LENGTH);
+	}
+
 	public Set<String> getModifiedFileNames() {
 		return _modifiedFileNames;
 	}
@@ -1387,14 +1398,10 @@ public class ServiceBuilder {
 			return "CLOB";
 		}
 		else if (type.equals("String")) {
-			Map<String, String> hints = ModelHintsUtil.getHints(model, field);
+			int maxLength = getMaxLength(model, field);
 
-			if (hints != null) {
-				int maxLength = GetterUtil.getInteger(hints.get("max-length"));
-
-				if (maxLength == 2000000) {
-					return "CLOB";
-				}
+			if (maxLength == 2000000) {
+				return "CLOB";
 			}
 
 			return "VARCHAR";
@@ -3912,15 +3919,9 @@ public class ServiceBuilder {
 					sb.append("TEXT");
 				}
 				else if (colType.equals("String")) {
-					Map<String, String> hints = ModelHintsUtil.getHints(
-						_packagePath + ".model." + entity.getName(), colName);
-
-					int maxLength = _DEFAULT_COLUMN_MAX_LENGTH;
-
-					if (hints != null) {
-						maxLength = GetterUtil.getInteger(
-							hints.get("max-length"), maxLength);
-					}
+					int maxLength = getMaxLength(
+						_packagePath + ".model." + entity.getName(),
+						entity.getName());
 
 					if (col.isLocalized()) {
 						maxLength = 4000;
@@ -4035,15 +4036,8 @@ public class ServiceBuilder {
 				sb.append("TEXT");
 			}
 			else if (colType.equals("String")) {
-				Map<String, String> hints = ModelHintsUtil.getHints(
+				int maxLength = getMaxLength(
 					_packagePath + ".model." + entity.getName(), colName);
-
-				int maxLength = _DEFAULT_COLUMN_MAX_LENGTH;
-
-				if (hints != null) {
-					maxLength = GetterUtil.getInteger(
-						hints.get("max-length"), maxLength);
-				}
 
 				if (col.isLocalized() && (maxLength < 4000)) {
 					maxLength = 4000;

commit f3ae3338012e0202d4aa1eb6e6dcfb509568881f

LPS-53828 Wrong column name, caused by 1bfe17a3f65fc6052c955457d7862d8555bde7c4

modules/util/portal-tools-service-builder/src/com/liferay/portal/tools/service/builder/ServiceBuilder.java
@@ -3892,6 +3892,7 @@ public class ServiceBuilder {
 			for (int i = 0; i < pkList.size(); i++) {
 				EntityColumn col = pkList.get(i);
 
+				String colName = col.getName();
 				String colType = col.getType();
 
 				sb.append("\t");
@@ -3919,8 +3920,7 @@ public class ServiceBuilder {
 				}
 				else if (colType.equals("String")) {
 					int maxLength = getMaxLength(
-						_packagePath + ".model." + entity.getName(),
-						entity.getName());
+						_packagePath + ".model." + entity.getName(), colName);
 
 					if (col.isLocalized()) {
 						maxLength = 4000;