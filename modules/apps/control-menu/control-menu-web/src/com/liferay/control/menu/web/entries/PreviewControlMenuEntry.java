a93671a2c1266e5c5ab169106c40e4eaec72ab45
Julio Camarero <julio.camarero@liferay.com>
2015-09-14 13:28:01 +0200

    LPS-58563 extract device preview

---
+++ modules/apps/control-menu/control-menu-web/src/com/liferay/control/menu/web/entries/PreviewControlMenuEntry.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.control.menu.web.entries;
+
+import com.liferay.control.menu.BaseJSPControlMenuEntry;
+import com.liferay.control.menu.ControlMenuEntry;
+import com.liferay.control.menu.constants.ControlMenuCategoryKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.model.Layout;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.service.permission.GroupPermissionUtil;
+import com.liferay.portal.service.permission.LayoutPermissionUtil;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.WebKeys;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ *
+ * @author Julio Camarero
+ */
+@Component(
+	immediate = true,
+	property = {
+		"control.menu.category.key=" + ControlMenuCategoryKeys.USER,
+		"service.ranking:Integer=100"
+	},
+	service = ControlMenuEntry.class
+)
+public class PreviewControlMenuEntry
+	extends BaseJSPControlMenuEntry implements ControlMenuEntry {
+
+	@Override
+	public String getJspPath() {
+		return "/entries/preview.jsp";
+	}
+
+	@Override
+	public boolean hasAccessPermission(HttpServletRequest request)
+		throws PortalException {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		Layout layout = themeDisplay.getLayout();
+
+		if (layout.isTypeControlPanel()) {
+			return false;
+		}
+
+		if (!(hasUpdateLayoutPermission(themeDisplay) ||
+			  hasPreviewInDevicePermission(themeDisplay))) {
+
+			return false;
+		}
+
+		return super.hasAccessPermission(request);
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.control.menu.web)", unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+	protected boolean hasPreviewInDevicePermission(ThemeDisplay themeDisplay)
+		throws PortalException {
+
+		return GroupPermissionUtil.contains(
+			themeDisplay.getPermissionChecker(), themeDisplay.getScopeGroup(),
+			ActionKeys.PREVIEW_IN_DEVICE);
+	}
+
+	protected boolean hasUpdateLayoutPermission(ThemeDisplay themeDisplay)
+		throws PortalException {
+
+		return LayoutPermissionUtil.contains(
+			themeDisplay.getPermissionChecker(), themeDisplay.getLayout(),
+			ActionKeys.UPDATE);
+	}
+
+}
