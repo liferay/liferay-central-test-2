77795f0fbe3617fc6fb681d33eb6d131e318211e
Julio Camarero <julio.camarero@liferay.com>
2015-09-14 10:56:05 +0200

    LPS-58563 Extract add content button

---
+++ modules/apps/control-menu/control-menu-web/src/META-INF/resources/entries/add_content.jsp
@@ -0,0 +1,41 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<portlet:renderURL var="addURL" windowState="<%= LiferayWindowState.EXCLUSIVE.toString() %>">
+	<portlet:param name="mvcPath" value="/add_panel.jsp" />
+	<portlet:param name="stateMaximized" value="<%= String.valueOf(themeDisplay.isStateMaximized()) %>" />
+	<portlet:param name="viewEntries" value="<%= Boolean.TRUE.toString() %>" />
+</portlet:renderURL>
+
+<%
+Map<String, Object> data = new HashMap<String, Object>();
+
+data.put("panelURL", addURL);
+%>
+
+<li>
+	<liferay-ui:icon
+		data="<%= data %>"
+		iconCssClass="icon-plus icon-monospaced"
+		id="addPanel"
+		label="<%= false %>"
+		linkCssClass="control-menu-icon"
+		message="add"
+		url="javascript:;"
+	/>
+</li>
---
+++ modules/apps/control-menu/control-menu-web/src/com/liferay/control/menu/web/entries/AddContentControlMenuEntry.java
@@ -0,0 +1,139 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.control.menu.web.entries;
+
+import com.liferay.control.menu.BaseJSPControlMenuEntry;
+import com.liferay.control.menu.ControlMenuEntry;
+import com.liferay.control.menu.constants.ControlMenuCategoryKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.model.Layout;
+import com.liferay.portal.model.LayoutConstants;
+import com.liferay.portal.model.LayoutTypePortlet;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.service.permission.GroupPermissionUtil;
+import com.liferay.portal.service.permission.LayoutPermissionUtil;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.WebKeys;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ *
+ * @author Julio Camarero
+ */
+@Component(
+	immediate = true,
+	property = {
+		"control.menu.category.key=" + ControlMenuCategoryKeys.TOOLS,
+		"service.ranking:Integer=100"
+	},
+	service = ControlMenuEntry.class
+)
+public class AddContentControlMenuEntry
+	extends BaseJSPControlMenuEntry implements ControlMenuEntry {
+
+	@Override
+	public String getJspPath() {
+		return "/entries/add_content.jsp";
+	}
+
+	@Override
+	public boolean hasAccessPermission(HttpServletRequest request)
+		throws PortalException {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		Layout layout = themeDisplay.getLayout();
+
+		if (layout.isTypeControlPanel()) {
+			return false;
+		}
+
+		if (!(hasAddLayoutPermission(themeDisplay) ||
+			  hasUpdateLayoutPermission(themeDisplay) ||
+			  hasCustomizePermission(themeDisplay))) {
+
+			return false;
+		}
+
+		return super.hasAccessPermission(request);
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.control.menu.web)", unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+	protected boolean hasAddLayoutPermission(ThemeDisplay themeDisplay)
+		throws PortalException {
+
+		Layout layout = themeDisplay.getLayout();
+
+		if (layout.getParentLayoutId() ==
+				LayoutConstants.DEFAULT_PARENT_LAYOUT_ID) {
+
+			return GroupPermissionUtil.contains(
+				themeDisplay.getPermissionChecker(), layout.getGroup(),
+				ActionKeys.ADD_LAYOUT);
+		}
+
+		return LayoutPermissionUtil.contains(
+			themeDisplay.getPermissionChecker(), layout, ActionKeys.ADD_LAYOUT);
+	}
+
+	protected boolean hasCustomizePermission(ThemeDisplay themeDisplay)
+		throws PortalException {
+
+		Layout layout = themeDisplay.getLayout();
+		LayoutTypePortlet layoutTypePortlet =
+			themeDisplay.getLayoutTypePortlet();
+
+		if (!layout.isTypePortlet() || (layoutTypePortlet == null)) {
+			return false;
+		}
+
+		if (!layoutTypePortlet.isCustomizable() ||
+			!layoutTypePortlet.isCustomizedView()) {
+
+			return false;
+		}
+
+		if (LayoutPermissionUtil.contains(
+				themeDisplay.getPermissionChecker(), layout,
+				ActionKeys.CUSTOMIZE)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	protected boolean hasUpdateLayoutPermission(ThemeDisplay themeDisplay)
+		throws PortalException {
+
+		return LayoutPermissionUtil.contains(
+			themeDisplay.getPermissionChecker(), themeDisplay.getLayout(),
+			ActionKeys.UPDATE);
+	}
+
+}
