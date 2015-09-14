4655ed305f66fad8328b612980e572f1ad9e4e14
Matthew Tambara <matthew.tambara@liferay.com>
2015-09-14 14:12:46 -0700

    LPS-58278 Concurrent writes are no longer a problem, remove branching paths

--- portal-impl/src/com/liferay/portal/service/impl/PortalPreferencesLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/PortalPreferencesLocalServiceImpl.java
@@ -14,14 +14,9 @@
 
 package com.liferay.portal.service.impl;
 
-import com.liferay.portal.kernel.concurrent.LockRegistry;
-import com.liferay.portal.kernel.dao.db.DB;
-import com.liferay.portal.kernel.dao.db.DBFactoryUtil;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.StringBundler;
-import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.PortalPreferences;
 import com.liferay.portal.model.PortletConstants;
@@ -30,8 +25,6 @@ import com.liferay.portlet.PortalPreferencesImpl;
 import com.liferay.portlet.PortalPreferencesWrapper;
 import com.liferay.portlet.PortalPreferencesWrapperCacheUtil;
 import com.liferay.portlet.PortletPreferencesFactoryUtil;
-
-import java.util.concurrent.locks.Lock;
 
 import javax.portlet.PortletPreferences;
 
@@ -104,36 +97,7 @@ public class PortalPreferencesLocalServiceImpl
 	public PortletPreferences getPreferences(
 		long ownerId, int ownerType, String defaultPreferences) {
 
-		DB db = DBFactoryUtil.getDB();
-
-		String dbType = db.getType();
-
-		if (!dbType.equals(DB.TYPE_HYPERSONIC)) {
-			return doGetPreferences(ownerId, ownerType, defaultPreferences);
-		}
-
-		StringBundler sb = new StringBundler(4);
-
-		sb.append(ownerId);
-		sb.append(StringPool.POUND);
-		sb.append(ownerType);
-		sb.append(StringPool.POUND);
-
-		String groupName = getClass().getName();
-		String key = sb.toString();
-
-		Lock lock = LockRegistry.allocateLock(groupName, key);
-
-		lock.lock();
-
-		try {
-			return doGetPreferences(ownerId, ownerType, defaultPreferences);
-		}
-		finally {
-			lock.unlock();
-
-			LockRegistry.freeLock(groupName, key);
-		}
+		return doGetPreferences(ownerId, ownerType, defaultPreferences);
 	}
 
 	/**
