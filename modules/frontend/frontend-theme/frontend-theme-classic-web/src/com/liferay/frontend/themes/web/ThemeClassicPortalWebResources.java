acbe6c97f346843eef0f41c606a5a33f4458c1e5
michael.bradford@liferay.com <michael.bradford@liferay.com>
2015-09-15 15:38:19 -0700

    LPS-58116 - rename com.liferay.frontend.themes.* packages to com.liferay.frontend.theme.*

--- modules/frontend/frontend-theme/frontend-theme-classic-web/src/com/liferay/frontend/themes/web/ThemeClassicPortalWebResources.java
+++ modules/frontend/frontend-theme/frontend-theme-classic-web/src/com/liferay/frontend/themes/web/ThemeClassicPortalWebResources.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.frontend.themes.classic.web;
+package com.liferay.frontend.theme.classic.web;
 
 import com.liferay.portal.kernel.servlet.PortalWebResourceConstants;
 import com.liferay.portal.kernel.servlet.PortalWebResources;
@@ -57,7 +57,7 @@ public class ThemeClassicPortalWebResources implements PortalWebResources {
 	}
 
 	@Reference(
-		target = "(osgi.web.symbolicname=com.liferay.frontend.themes.classic.web)"
+		target = "(osgi.web.symbolicname=com.liferay.frontend.theme.classic.web)"
 	)
 	protected void setServletContext(ServletContext servletContext) {
 		_servletContext = servletContext;
