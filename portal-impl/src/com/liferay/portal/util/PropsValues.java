20a02ad7172019cc0e2ac7f542fe225f099315a5
Matthew Tambara <matthew.tambara@liferay.com>
2015-09-15 16:50:28 -0700

    LPS-57725 Add advice to retry methods

--- portal-impl/src/com/liferay/portal/util/PropsValues.java
+++ portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -1354,6 +1354,8 @@ public class PropsValues {
 
 	public static final String[] REST_PROXY_URL_PREFIXES_ALLOWED = PropsUtil.getArray(PropsKeys.REST_PROXY_URL_PREFIXES_ALLOWED);
 
+	public static final String RETRY_ADVICE_DEFAULT_RETRIES =  PropsUtil.get(PropsKeys.RETRY_ADVICE_DEFAULT_RETRIES);
+
 	public static final String ROBOTS_TXT_WITH_SITEMAP = PropsUtil.get(PropsKeys.ROBOTS_TXT_WITH_SITEMAP);
 
 	public static final String ROBOTS_TXT_WITHOUT_SITEMAP = PropsUtil.get(PropsKeys.ROBOTS_TXT_WITHOUT_SITEMAP);
