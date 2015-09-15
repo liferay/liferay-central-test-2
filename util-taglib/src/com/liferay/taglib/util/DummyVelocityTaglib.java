juliocamarero/liferay-portal

LPS-54585 Update VelocityTag to accept new changes in RuntimeTag

util-taglib/src/com/liferay/taglib/util/DummyVelocityTaglib.java
@@ -482,12 +482,27 @@ public class DummyVelocityTaglib implements VelocityTaglib {
 	}
 
 	@Override
+	public void runtime(
+			String portletProviderClassName,
+			PortletProvider.Action portletProviderAction,
+			String portletInstanceName)
+		throws Exception {
+	}
+
+	@Override
 	public void runtime(String portletName, String queryString) {
 	}
 
 	@Override
 	public void runtime(
 		String portletName, String queryString, String defaultPreferences) {
+	}
+
+	@Override
+	public void runtime(
+			String portletName, String portletInstanceName, String queryString,
+			String defaultPreferences)
+		throws Exception {
 	}
 
 	@Override

util-taglib/src/com/liferay/taglib/util/VelocityTaglib.java
@@ -323,11 +323,22 @@ public interface VelocityTaglib {
 			PortletProvider.Action portletProviderAction)
 		throws Exception;
 
+	public void runtime(
+			String portletProviderClassName,
+			PortletProvider.Action portletProviderAction,
+			String portletInstanceName)
+		throws Exception;
+
 	public void runtime(String portletName, String queryString)
 		throws Exception;
 
 	public void runtime(
 			String portletName, String queryString, String defaultPreferences)
+		throws Exception;
+
+	public void runtime(
+			String portletName, String portletInstanceName, String queryString,
+			String defaultPreferences)
 		throws Exception;
 
 	public void search() throws Exception;

liferay/liferay-central-test-2

util-taglib/src/com/liferay/taglib/util/DummyVelocityTaglib.java
@@ -482,12 +482,27 @@ public class DummyVelocityTaglib implements VelocityTaglib {
 	}
 
 	@Override
+	public void runtime(
+			String portletProviderClassName,
+			PortletProvider.Action portletProviderAction,
+			String portletInstanceId)
+		throws Exception {
+	}
+
+	@Override
 	public void runtime(String portletName, String queryString) {
 	}
 
 	@Override
 	public void runtime(
 		String portletName, String queryString, String defaultPreferences) {
+	}
+
+	@Override
+	public void runtime(
+			String portletName, String portletInstanceId, String queryString,
+			String defaultPreferences)
+		throws Exception {
 	}
 
 	@Override

util-taglib/src/com/liferay/taglib/util/VelocityTaglib.java
@@ -323,11 +323,22 @@ public interface VelocityTaglib {
 			PortletProvider.Action portletProviderAction)
 		throws Exception;
 
+	public void runtime(
+			String portletProviderClassName,
+			PortletProvider.Action portletProviderAction,
+			String portletInstanceId)
+		throws Exception;
+
 	public void runtime(String portletName, String queryString)
 		throws Exception;
 
 	public void runtime(
 			String portletName, String queryString, String defaultPreferences)
+		throws Exception;
+
+	public void runtime(
+			String portletName, String portletInstanceId, String queryString,
+			String defaultPreferences)
 		throws Exception;
 
 	public void search() throws Exception;