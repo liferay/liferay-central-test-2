a57fc67bbfc9473ab002dc2f5d7240489e7a7f79
Michael Han <michael.han@liferay.com>
2015-09-14 22:07:51 -0500

    LPS-58479 migrate to module

--- modules/portal/portal-ldap/src/com/liferay/portal/ldap/messaging/UserImportMessageListener.java
+++ modules/portal/portal-ldap/src/com/liferay/portal/ldap/messaging/UserImportMessageListener.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portlet.admin.messaging;
+package com.liferay.portal.ldap.messaging;
 
 import com.liferay.portal.kernel.messaging.BaseMessageListener;
 import com.liferay.portal.kernel.messaging.Message;
