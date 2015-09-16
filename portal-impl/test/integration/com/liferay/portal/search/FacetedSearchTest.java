5ee0f0e859571661049c74515c83eeb35d5f3883
arbetts <andrew.betts@liferay.com>
2015-09-16 11:43:11 -0700

    LPS-58506 add test for tokenizing assetTagNames

---
+++ portal-impl/test/integration/com/liferay/portal/search/FacetedSearchTest.java
@@ -0,0 +1,128 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search;
+
+import com.liferay.portal.kernel.search.FacetedSearcher;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.facet.Facet;
+import com.liferay.portal.kernel.search.facet.MultiValueFacet;
+import com.liferay.portal.kernel.search.facet.collector.FacetCollector;
+import com.liferay.portal.kernel.search.facet.collector.TermCollector;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.SearchContextTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.workflow.WorkflowThreadLocal;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.MainServletTestRule;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Andrew Betts
+ */
+@Sync
+public class FacetedSearchTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(), MainServletTestRule.INSTANCE,
+			SynchronousDestinationTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		WorkflowThreadLocal.setEnabled(false);
+
+		_group = GroupTestUtil.addGroup();
+
+		addUserAsset(_group.getGroupId(), _TAG_NAME);
+	}
+
+	@Test
+	public void testAssetTagNamesFacet() throws Exception {
+		SearchContext searchContext = SearchContextTestUtil.getSearchContext(
+			_group.getGroupId());
+
+		searchContext.setKeywords(_TAG_NAME);
+
+		MultiValueFacet multiValueFacet = new MultiValueFacet(searchContext);
+
+		multiValueFacet.setFieldName(Field.ASSET_TAG_NAMES);
+		multiValueFacet.setStatic(false);
+
+		searchContext.addFacet(multiValueFacet);
+
+		Indexer facetedSearcher = FacetedSearcher.getInstance();
+
+		Hits hits = facetedSearcher.search(searchContext);
+
+		Assert.assertNotEquals(0, hits.getLength());
+
+		Map<String, Facet> facets = searchContext.getFacets();
+
+		Facet assetTagNameFacet = facets.get(Field.ASSET_TAG_NAMES);
+
+		FacetCollector facetCollector = assetTagNameFacet.getFacetCollector();
+
+		List<TermCollector> termCollectors = facetCollector.getTermCollectors();
+
+		Assert.assertEquals(1, termCollectors.size());
+
+		TermCollector termCollector = termCollectors.get(0);
+
+		Assert.assertEquals(_TAG_NAME, termCollector.getTerm());
+		Assert.assertEquals(1, termCollector.getFrequency());
+	}
+
+	protected void addUserAsset(long groupId, String assetTagName)
+		throws Exception {
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				groupId, TestPropsValues.getUserId());
+
+		serviceContext.setAssetTagNames(new String[] {assetTagName});
+
+		User user = UserTestUtil.addUser();
+
+		UserTestUtil.updateUser(user, serviceContext);
+	}
+
+	private static final String _TAG_NAME = "enterprise. open-source for life";
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+}
