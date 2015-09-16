740afd2b46bac8044617ab790e729630104ab0c9
tina.tian <tina.tian@liferay.com>
2015-09-16 14:50:28 +0800

    LPS-58479 Need to register LDAPConfigurationSettingsUtil as a service

--- modules/portal/portal-ldap/src/com/liferay/portal/ldap/settings/LDAPConfigurationSettingsUtil.java
+++ modules/portal/portal-ldap/src/com/liferay/portal/ldap/settings/LDAPConfigurationSettingsUtil.java
@@ -26,7 +26,7 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Michael C. Han
  */
-@Component(immediate = true)
+@Component(immediate = true, service = LDAPConfigurationSettingsUtil.class)
 public class LDAPConfigurationSettingsUtil {
 
 	public LDAPConfiguration getLDAPConfiguration() {
