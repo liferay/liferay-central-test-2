195838a3483e7e1fd120f0f881b321532a2a2b14
Brian Chan <brian.chan@liferay.com>
2015-09-16 15:40:50 -0700

    LPS-56346 SF

--- portal-service/test/unit/com/liferay/portal/kernel/servlet/MetaInfoCacheServletResponseTest.java
+++ portal-service/test/unit/com/liferay/portal/kernel/servlet/MetaInfoCacheServletResponseTest.java
@@ -24,6 +24,7 @@ import com.liferay.portal.kernel.util.StringPool;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Serializable;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
