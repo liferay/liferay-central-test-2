7c7b331298536baf36e182887ac20fd612ee2949
Julio Camarero <julio.camarero@liferay.com>
2015-09-18 14:17:37 +0200

    LPS-58563 create registries for keeping track of categories and entries

---
+++ modules/apps/control-menu/control-menu-api/src/com/liferay/control/menu/util/ControlMenuEntryRegistry.java
@@ -0,0 +1,130 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.control.menu.util;
+
+import com.liferay.control.menu.ControlMenuCategory;
+import com.liferay.control.menu.ControlMenuEntry;
+import com.liferay.osgi.service.tracker.map.PropertyServiceReferenceComparator;
+import com.liferay.osgi.service.tracker.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.map.ServiceTrackerMapFactory;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.PredicateFilter;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Julio Camarero
+ */
+@Component(immediate = true, service = ControlMenuEntryRegistry.class)
+public class ControlMenuEntryRegistry {
+
+	public List<ControlMenuEntry> getControlMenuEntries(
+		ControlMenuCategory controlMenuCategory) {
+
+		List<ControlMenuEntry> controlMenuEntries =
+			_serviceTrackerMap.getService(controlMenuCategory.getKey());
+
+		if (controlMenuEntries == null) {
+			return Collections.emptyList();
+		}
+
+		return controlMenuEntries;
+	}
+
+	public List<ControlMenuEntry> getControlMenuEntries(
+		ControlMenuCategory controlMenuCategory,
+		final HttpServletRequest request) {
+
+		List<ControlMenuEntry> controlMenuEntries = getControlMenuEntries(
+			controlMenuCategory);
+
+		if (controlMenuEntries.isEmpty()) {
+			return controlMenuEntries;
+		}
+
+		return ListUtil.filter(
+			controlMenuEntries,
+			new PredicateFilter<ControlMenuEntry>() {
+
+				@Override
+				public boolean filter(ControlMenuEntry controlMenuEntry) {
+					try {
+						return controlMenuEntry.hasAccessPermission(request);
+					}
+					catch (PortalException e) {
+						_log.error(e);
+					}
+
+					return false;
+				}
+
+			});
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext)
+		throws InvalidSyntaxException {
+
+		_serviceTrackerMap = ServiceTrackerMapFactory.multiValueMap(
+			bundleContext, ControlMenuEntry.class,
+			"(control.menu.category.key=*)",
+			new ControlMenuEntryServiceReferenceMapper(),
+			new ServiceRankingPropertyServiceReferenceComparator());
+
+		_serviceTrackerMap.open();
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ControlMenuEntryRegistry.class);
+
+	private ServiceTrackerMap<String, List<ControlMenuEntry>>
+		_serviceTrackerMap;
+
+	private static class ServiceRankingPropertyServiceReferenceComparator
+		extends PropertyServiceReferenceComparator<ControlMenuEntry> {
+
+		public ServiceRankingPropertyServiceReferenceComparator() {
+			super("service.ranking");
+		}
+
+		@Override
+		public int compare(
+			ServiceReference<ControlMenuEntry> serviceReference1,
+			ServiceReference<ControlMenuEntry> serviceReference2) {
+
+			return -(super.compare(serviceReference1, serviceReference2));
+		}
+
+	}
+
+}
---
+++ modules/apps/control-menu/control-menu-api/src/com/liferay/control/menu/util/ControlMenuEntryServiceReferenceMapper.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.control.menu.util;
+
+import com.liferay.control.menu.ControlMenuEntry;
+import com.liferay.osgi.service.tracker.map.ServiceReferenceMapper;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import org.osgi.framework.ServiceReference;
+
+/**
+ * @author Julio Camarero
+ */
+public class ControlMenuEntryServiceReferenceMapper
+	implements ServiceReferenceMapper<String, ControlMenuEntry> {
+
+	@Override
+	public void map(
+		ServiceReference<ControlMenuEntry> serviceReference,
+		Emitter<String> emitter) {
+
+		String controlMenuCategoryKey = (String)serviceReference.getProperty(
+			"control.menu.category.key");
+
+		if (Validator.isNull(controlMenuCategoryKey)) {
+			_log.error(
+				"Unable to register control menu entry because of missing " +
+					"service property \"control.menu.category.key\"");
+		}
+		else {
+			emitter.emit(controlMenuCategoryKey);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ControlMenuEntryServiceReferenceMapper.class);
+
+}
