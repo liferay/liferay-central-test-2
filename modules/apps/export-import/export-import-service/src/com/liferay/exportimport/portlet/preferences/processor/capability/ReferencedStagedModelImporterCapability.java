eebbc949f698f2c29ee38e2a8ee11c1bbfe1ab7c
Máté Thurzó <mate.thurzo@liferay.com>
2015-09-18 10:22:23 +0200

    LPS-58734 Implment a capabiltiy for automatic referenced staged model importing

---
+++ modules/apps/export-import/export-import-service/src/com/liferay/exportimport/portlet/preferences/processor/capability/ReferencedStagedModelImporterCapability.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.exportimport.portlet.preferences.processor.capability;
+
+import com.liferay.exportimport.portlet.preferences.processor.Capability;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portlet.exportimport.lar.PortletDataContext;
+import com.liferay.portlet.exportimport.lar.PortletDataException;
+import com.liferay.portlet.exportimport.lar.StagedModelDataHandlerUtil;
+
+import java.util.List;
+
+import javax.portlet.PortletPreferences;
+
+/**
+ * @author Mate Thurzo
+ */
+public class ReferencedStagedModelImporterCapability implements Capability {
+
+	@Override
+	public PortletPreferences process(
+			PortletDataContext portletDataContext,
+			PortletPreferences portletPreferences)
+		throws PortletDataException {
+
+		Element importDataRootElement =
+			portletDataContext.getImportDataRootElement();
+
+		Element referencesElement = importDataRootElement.element("references");
+
+		if (referencesElement == null) {
+			return portletPreferences;
+		}
+
+		List<Element> referenceElements = referencesElement.elements();
+
+		for (Element referenceElement : referenceElements) {
+			String className = referenceElement.attributeValue("class-name");
+			long classPK = GetterUtil.getLong(
+				referenceElement.attributeValue("class-pk"));
+
+			StagedModelDataHandlerUtil.importReferenceStagedModel(
+				portletDataContext, className, classPK);
+		}
+
+		return portletPreferences;
+	}
+
+}
