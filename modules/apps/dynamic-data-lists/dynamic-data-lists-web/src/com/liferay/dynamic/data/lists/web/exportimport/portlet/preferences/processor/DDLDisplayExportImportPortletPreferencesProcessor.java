cd0301ae3f144a4df8cf029c1ab646ce45f96b4c
Máté Thurzó <mate.thurzo@liferay.com>
2015-09-21 10:20:16 +0200

    LPS-58790 Create ExportImportPortletPreferencesProcessor for ddl display and move over related code (no changes just move)

---
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-web/src/com/liferay/dynamic/data/lists/web/exportimport/portlet/preferences/processor/DDLDisplayExportImportPortletPreferencesProcessor.java
@@ -0,0 +1,137 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.web.exportimport.portlet.preferences.processor;
+
+import com.liferay.dynamic.data.lists.constants.DDLPortletKeys;
+import com.liferay.dynamic.data.lists.model.DDLRecordSet;
+import com.liferay.dynamic.data.lists.service.DDLRecordSetLocalServiceUtil;
+import com.liferay.dynamic.data.lists.service.permission.DDLPermission;
+import com.liferay.exportimport.portlet.preferences.processor.Capability;
+import com.liferay.exportimport.portlet.preferences.processor.ExportImportPortletPreferencesProcessor;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portlet.exportimport.lar.PortletDataContext;
+import com.liferay.portlet.exportimport.lar.PortletDataException;
+import com.liferay.portlet.exportimport.lar.StagedModelDataHandlerUtil;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.portlet.PortletPreferences;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Mate Thurzo
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + DDLPortletKeys.DYNAMIC_DATA_LISTS_DISPLAY
+	},
+	service = ExportImportPortletPreferencesProcessor.class
+)
+public class DDLDisplayExportImportPortletPreferencesProcessor
+	implements ExportImportPortletPreferencesProcessor {
+
+	@Override
+	public List<Capability> getExportCapabilities() {
+		return null;
+	}
+
+	@Override
+	public List<Capability> getImportCapabilities() {
+		return null;
+	}
+
+	@Override
+	public PortletPreferences processExportPortletPreferences(
+			PortletDataContext portletDataContext,
+			PortletPreferences portletPreferences)
+		throws PortletDataException {
+
+		portletDataContext.addPortletPermissions(DDLPermission.RESOURCE_NAME);
+
+		long recordSetId = GetterUtil.getLong(
+			portletPreferences.getValue("recordSetId", null), 0);
+
+		if (recordSetId == 0) {
+			if (_log.isDebugEnabled()) {
+				_log.debug("Unable to get record set with ID " + portletId);
+			}
+
+			return portletPreferences;
+		}
+
+		DDLRecordSet recordSet = DDLRecordSetLocalServiceUtil.fetchRecordSet(
+			recordSetId);
+
+		if (recordSet == null) {
+			return portletPreferences;
+		}
+
+		StagedModelDataHandlerUtil.exportReferenceStagedModel(
+			portletDataContext, portletId, recordSet);
+
+		return portletPreferences;
+	}
+
+	@Override
+	public PortletPreferences processImportPortletPreferences(
+			PortletDataContext portletDataContext,
+			PortletPreferences portletPreferences)
+		throws PortletDataException {
+
+		portletDataContext.importPortletPermissions(
+			DDLPermission.RESOURCE_NAME);
+
+		StagedModelDataHandlerUtil.importReferenceStagedModels(
+			portletDataContext, DDLRecordSet.class);
+
+		long importedRecordSetId = GetterUtil.getLong(
+			portletPreferences.getValue("recordSetId", null));
+		long importedDisplayDDMTemplateId = GetterUtil.getLong(
+			portletPreferences.getValue("displayDDMTemplateId", null));
+		long importedFormDDMTemplateId = GetterUtil.getLong(
+			portletPreferences.getValue("formDDMTemplateId", null));
+
+		Map<Long, Long> recordSetIds =
+			(Map<Long, Long>)portletDataContext.getNewPrimaryKeysMap(
+				DDLRecordSet.class);
+
+		long recordSetId = MapUtil.getLong(
+			recordSetIds, importedRecordSetId, importedRecordSetId);
+
+		Map<Long, Long> templateIds =
+			(Map<Long, Long>)portletDataContext.getNewPrimaryKeysMap(
+				DDMTemplate.class);
+
+		long displayDDMTemplateId = MapUtil.getLong(
+			templateIds, importedDisplayDDMTemplateId,
+			importedDisplayDDMTemplateId);
+
+		long formDDMTemplateId = MapUtil.getLong(
+			templateIds, importedFormDDMTemplateId, importedFormDDMTemplateId);
+
+		portletPreferences.setValue("recordSetId", String.valueOf(recordSetId));
+		portletPreferences.setValue(
+			"displayDDMTemplateId", String.valueOf(displayDDMTemplateId));
+		portletPreferences.setValue(
+			"formDDMTemplateId", String.valueOf(formDDMTemplateId));
+
+		return portletPreferences;
+	}
+
+}
