aac0d40a9d416a2a76b7ac5ebc3b98f013f66a86
Máté Thurzó <mate.thurzo@liferay.com>
2015-09-21 09:39:44 +0200

    LPS-58764 Check for empty or null capability list

--- modules/apps/export-import/export-import-service/src/com/liferay/exportimport/controller/PortletImportController.java
+++ modules/apps/export-import/export-import-service/src/com/liferay/exportimport/controller/PortletImportController.java
@@ -40,6 +40,7 @@ import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.IndexerRegistryUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.ReleaseInfo;
@@ -926,9 +927,13 @@ public class PortletImportController implements ImportController {
 							exportImportPortletPreferencesProcessor.
 								getImportCapabilities();
 
-						for (Capability importCapability : importCapabilities) {
-							importCapability.process(
-								portletDataContext, jxPortletPreferences);
+						if (ListUtil.isNotEmpty(importCapabilities)) {
+							for (Capability importCapability :
+									importCapabilities) {
+
+								importCapability.process(
+									portletDataContext, jxPortletPreferences);
+							}
 						}
 
 						exportImportPortletPreferencesProcessor.
