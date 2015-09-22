386889cb726fa29da215729e7518ea9437d2aee4
Adolfo Pérez <adolfo.perez@liferay.com>
2015-09-22 16:21:52 +0200

    LPS-53819 Simplify test fixtures with util class

--- modules/portal-store/portal-store-file-system-test/test/integration/com/liferay/portal/store/file/system/test/AdvancedFileSystemStoreTest.java
+++ modules/portal-store/portal-store-file-system-test/test/integration/com/liferay/portal/store/file/system/test/AdvancedFileSystemStoreTest.java
@@ -15,22 +15,15 @@
 package com.liferay.portal.store.file.system.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
-import com.liferay.portal.module.framework.ModuleFrameworkUtilAdapter;
+import com.liferay.portal.module.framework.test.ModuleFrameworkTestUtil;
 import com.liferay.portal.service.test.ServiceTestUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portlet.documentlibrary.store.StoreWrapper;
 import com.liferay.portlet.documentlibrary.store.test.BaseStoreTestCase;
-import com.liferay.registry.Registry;
-import com.liferay.registry.RegistryUtil;
-import com.liferay.registry.ServiceReference;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -53,34 +46,15 @@ public class AdvancedFileSystemStoreTest extends BaseStoreTestCase {
 	public static void setUpClass() throws Exception {
 		ServiceTestUtil.setUser(TestPropsValues.getUser());
 
-		Registry registry = RegistryUtil.getRegistry();
+		_bundleIds = ModuleFrameworkTestUtil.getBundleIds(
+			StoreWrapper.class, "(store.type="+ _STORE_TYPE +")");
 
-		Collection<ServiceReference<StoreWrapper>>
-			serviceReferences = registry.getServiceReferences(
-				StoreWrapper.class, "(store.type="+ _STORE_TYPE +")");
-
-		for (ServiceReference<StoreWrapper> serviceReference :
-				serviceReferences) {
-
-			Long bundleId = (Long)serviceReference.getProperty(
-				"service.bundleid");
-
-			_bundleIds.add(bundleId);
-
-			ModuleFrameworkUtilAdapter.stopBundle(bundleId);
-		}
+		ModuleFrameworkTestUtil.stopBundles(_bundleIds);
 	}
 
 	@AfterClass
 	public static void tearDownClass() {
-		for (long bundleId : _bundleIds) {
-			try {
-				ModuleFrameworkUtilAdapter.startBundle(bundleId);
-			}
-			catch (Exception e) {
-				_log.error("Unable to start bundle " + bundleId, e);
-			}
-		}
+		ModuleFrameworkTestUtil.startBundles(_bundleIds);
 	}
 
 	@Override
@@ -91,9 +65,6 @@ public class AdvancedFileSystemStoreTest extends BaseStoreTestCase {
 	private static final String _STORE_TYPE =
 		"com.liferay.portal.store.file.system.AdvancedFileSystemStore";
 
-	private static final Log _log = LogFactoryUtil.getLog(
-		AdvancedFileSystemStoreTest.class);
-
-	private static final List<Long> _bundleIds = new ArrayList<>();
+	private static Collection<Long> _bundleIds;
 
 }
---
+++ portal-test-internal/src/com/liferay/portal/module/framework/test/ModuleFrameworkTestUtil.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.module.framework.test;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.module.framework.ModuleFrameworkUtilAdapter;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceReference;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class ModuleFrameworkTestUtil {
+
+	public static <T> Collection<Long> getBundleIds(
+			Class<T> clazz, String filter)
+		throws Exception {
+
+		Registry registry = RegistryUtil.getRegistry();
+
+		Collection<Long> bundleIds = new ArrayList<>();
+
+		Collection<ServiceReference<T>>
+			serviceReferences = registry.getServiceReferences(clazz, filter);
+
+		for (ServiceReference<T> serviceReference : serviceReferences) {
+			Long bundleId = (Long)serviceReference.getProperty(
+				"service.bundleid");
+
+			bundleIds.add(bundleId);
+		}
+
+		return bundleIds;
+	}
+
+	public static void startBundles(Iterable<Long> bundleIds) {
+		for (long bundleId : bundleIds) {
+			try {
+				ModuleFrameworkUtilAdapter.startBundle(bundleId);
+			}
+			catch (Exception e) {
+				_log.error("Unable to start bundle " + bundleId, e);
+			}
+		}
+	}
+
+	public static void stopBundles(Iterable<Long> bundleIds)
+		throws PortalException {
+
+		for (long bundleId : bundleIds) {
+			ModuleFrameworkUtilAdapter.stopBundle(bundleId);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ModuleFrameworkTestUtil.class);
+
+}
