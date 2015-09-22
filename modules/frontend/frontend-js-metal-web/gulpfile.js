92a929436209b79117c315d0023f6c5ee8a61bfa
Chema Balsas <jose.balsas@liferay.com>
2015-09-22 09:02:18 +0200

    LPS-58792 Update build to add soyutils implicit dependency

--- modules/frontend/frontend-js-metal-web/gulpfile.js
+++ modules/frontend/frontend-js-metal-web/gulpfile.js
@@ -1,8 +1,20 @@
 'use strict';
 
 var gulp = require('gulp');
+var path = require('path');
+var replace = require('gulp-replace');
 
-gulp.task('default', ['build:amd:jquery']);
+var soyFolder = path.join('bower_components', 'metal', 'src', 'soy');
+var soyImport = "'use strict';\n\nimport soyutils from 'module:soyutils';"
+var soyRegex = /'use strict';(\s*import\s+soyutils\s+from\s+'module:soyutils';)?/
+
+gulp.task('addSoyImport', function(done) {
+    return gulp.src(path.join(soyFolder, 'SoyComponent.js'))
+		.pipe(replace(soyRegex, soyImport))
+		.pipe(gulp.dest(soyFolder));
+});
+
+gulp.task('default', ['addSoyImport', 'build:amd:jquery']);
 
 var gulpMetal = require('gulp-metal');
 
