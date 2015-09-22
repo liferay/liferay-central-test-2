45beae8fba08aabc79122f708232871ab7980c64
Máté Thurzó <mate.thurzo@liferay.com>
2015-09-22 13:30:58 +0200

    LPS-58826 Apply changes to fit the new environment

--- modules/apps/journal/journal-content-web/src/com/liferay/journal/content/web/exportimport/portlet/preferences/processor/JournalContentExportImportPortletPreferencesProcessor.java
+++ modules/apps/journal/journal-content-web/src/com/liferay/journal/content/web/exportimport/portlet/preferences/processor/JournalContentExportImportPortletPreferencesProcessor.java
@@ -14,6 +14,7 @@
 
 package com.liferay.journal.content.web.exportimport.portlet.preferences.processor;
 
+import com.liferay.dynamic.data.mapping.service.DDMTemplateLocalServiceUtil;
 import com.liferay.exportimport.portlet.preferences.processor.Capability;
 import com.liferay.exportimport.portlet.preferences.processor.ExportImportPortletPreferencesProcessor;
 import com.liferay.journal.content.web.constants.JournalContentPortletKeys;
@@ -21,6 +22,9 @@ import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.service.JournalArticleLocalServiceUtil;
 import com.liferay.journal.service.JournalContentSearchLocalServiceUtil;
 import com.liferay.journal.service.permission.JournalPermission;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.StringPool;
@@ -30,6 +34,8 @@ import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
 import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.dynamicdatamapping.DDMStructure;
+import com.liferay.portlet.dynamicdatamapping.DDMTemplate;
 import com.liferay.portlet.exportimport.lar.PortletDataContext;
 import com.liferay.portlet.exportimport.lar.PortletDataException;
 import com.liferay.portlet.exportimport.lar.StagedModelDataHandlerUtil;
@@ -38,6 +44,7 @@ import java.util.List;
 import java.util.Map;
 
 import javax.portlet.PortletPreferences;
+import javax.portlet.ReadOnlyException;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -70,8 +77,16 @@ public class JournalContentExportImportPortletPreferencesProcessor
 			PortletPreferences portletPreferences)
 		throws PortletDataException {
 
-		portletDataContext.addPortletPermissions(
-			JournalPermission.RESOURCE_NAME);
+		String portletId = portletDataContext.getPortletId();
+
+		try {
+			portletDataContext.addPortletPermissions(
+				JournalPermission.RESOURCE_NAME);
+		}
+		catch (PortalException pe) {
+			throw new PortletDataException(
+				"Unable to export portlet permissions", pe);
+		}
 
 		String articleId = portletPreferences.getValue("articleId", null);
 
@@ -157,8 +172,14 @@ public class JournalContentExportImportPortletPreferencesProcessor
 			PortletPreferences portletPreferences)
 		throws PortletDataException {
 
-		portletDataContext.importPortletPermissions(
-			JournalPermission.RESOURCE_NAME);
+		try {
+			portletDataContext.importPortletPermissions(
+				JournalPermission.RESOURCE_NAME);
+		}
+		catch (PortalException pe) {
+			throw new PortletDataException(
+				"Unable to import portlet permissions", pe);
+		}
 
 		long previousScopeGroupId = portletDataContext.getScopeGroupId();
 
@@ -184,49 +205,66 @@ public class JournalContentExportImportPortletPreferencesProcessor
 
 		String articleId = portletPreferences.getValue("articleId", null);
 
-		if (Validator.isNotNull(articleId)) {
-			Map<String, String> articleIds =
-				(Map<String, String>)portletDataContext.getNewPrimaryKeysMap(
-					JournalArticle.class + ".articleId");
+		try {
+			if (Validator.isNotNull(articleId)) {
+				Map<String, String> articleIds =
+					(Map<String, String>)
+						portletDataContext.getNewPrimaryKeysMap(
+							JournalArticle.class + ".articleId");
 
-			articleId = MapUtil.getString(articleIds, articleId, articleId);
+				articleId = MapUtil.getString(articleIds, articleId, articleId);
 
-			portletPreferences.setValue("articleId", articleId);
+				portletPreferences.setValue("articleId", articleId);
 
-			portletPreferences.setValue("groupId", String.valueOf(groupId));
+				portletPreferences.setValue("groupId", String.valueOf(groupId));
 
-			Layout layout = LayoutLocalServiceUtil.getLayout(
-				portletDataContext.getPlid());
+				Layout layout = LayoutLocalServiceUtil.fetchLayout(
+					portletDataContext.getPlid());
 
-			JournalContentSearchLocalServiceUtil.updateContentSearch(
-				layout.getGroupId(), layout.isPrivateLayout(),
-				layout.getLayoutId(), portletId, articleId, true);
+				JournalContentSearchLocalServiceUtil.updateContentSearch(
+					layout.getGroupId(), layout.isPrivateLayout(),
+					layout.getLayoutId(), portletDataContext.getPortletId(),
+					articleId, true);
+			}
+			else {
+				portletPreferences.setValue("groupId", StringPool.BLANK);
+				portletPreferences.setValue("articleId", StringPool.BLANK);
+			}
+
+			String ddmTemplateKey = portletPreferences.getValue(
+				"ddmTemplateKey", null);
+
+			if (Validator.isNotNull(ddmTemplateKey)) {
+				Map<String, String> ddmTemplateKeys =
+					(Map<String, String>)
+						portletDataContext.getNewPrimaryKeysMap(
+							DDMTemplate.class + ".ddmTemplateKey");
+
+				ddmTemplateKey = MapUtil.getString(
+					ddmTemplateKeys, ddmTemplateKey, ddmTemplateKey);
+
+				portletPreferences.setValue("ddmTemplateKey", ddmTemplateKey);
+			}
+			else {
+				portletPreferences.setValue("ddmTemplateKey", StringPool.BLANK);
+			}
 		}
-		else {
-			portletPreferences.setValue("groupId", StringPool.BLANK);
-			portletPreferences.setValue("articleId", StringPool.BLANK);
+		catch (PortalException pe) {
+			throw new PortletDataException(
+				"Unable to update journal content search data during import",
+				pe);
 		}
-
-		String ddmTemplateKey = portletPreferences.getValue(
-			"ddmTemplateKey", null);
-
-		if (Validator.isNotNull(ddmTemplateKey)) {
-			Map<String, String> ddmTemplateKeys =
-				(Map<String, String>)portletDataContext.getNewPrimaryKeysMap(
-					DDMTemplate.class + ".ddmTemplateKey");
-
-			ddmTemplateKey = MapUtil.getString(
-				ddmTemplateKeys, ddmTemplateKey, ddmTemplateKey);
-
-			portletPreferences.setValue("ddmTemplateKey", ddmTemplateKey);
-		}
-		else {
-			portletPreferences.setValue("ddmTemplateKey", StringPool.BLANK);
+		catch (ReadOnlyException roe) {
+			throw new PortletDataException(
+				"Unable to update portlet preferences during import", roe);
 		}
 
 		portletDataContext.setScopeGroupId(previousScopeGroupId);
 
 		return portletPreferences;
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		JournalContentExportImportPortletPreferencesProcessor.class);
 
 }
