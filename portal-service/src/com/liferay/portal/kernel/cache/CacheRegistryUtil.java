f69da9ea788abd8323e2e7170a02c7817ee93ef8
tina.tian <tina.tian@liferay.com>
2015-09-22 09:42:39 +0800

    LPS-58820 Modify CacheRegistryUtil to use osgi service registry mechianism

--- portal-service/src/com/liferay/portal/kernel/cache/CacheRegistryUtil.java
+++ portal-service/src/com/liferay/portal/kernel/cache/CacheRegistryUtil.java
@@ -14,7 +14,16 @@
 
 package com.liferay.portal.kernel.cache;
 
-import com.liferay.portal.kernel.security.pacl.permission.PortalRuntimePermission;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceReference;
+import com.liferay.registry.ServiceTracker;
+import com.liferay.registry.ServiceTrackerCustomizer;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Brian Wing Shun Chan
@@ -22,41 +31,117 @@ import com.liferay.portal.kernel.security.pacl.permission.PortalRuntimePermissio
 public class CacheRegistryUtil {
 
 	public static void clear() {
-		getCacheRegistry().clear();
+		_instance._clear();
 	}
 
 	public static void clear(String name) {
-		getCacheRegistry().clear(name);
-	}
-
-	public static CacheRegistry getCacheRegistry() {
-		PortalRuntimePermission.checkGetBeanProperty(CacheRegistryUtil.class);
-
-		return _cacheRegistry;
+		_instance._clear(name);
 	}
 
 	public static boolean isActive() {
-		return getCacheRegistry().isActive();
-	}
-
-	public static void register(CacheRegistryItem cacheRegistryItem) {
-		getCacheRegistry().register(cacheRegistryItem);
+		return _instance._isActive();
 	}
 
 	public static void setActive(boolean active) {
-		getCacheRegistry().setActive(active);
+		_instance._setActive(active);
 	}
 
-	public static void setCacheRegistry(CacheRegistry cacheRegistry) {
-		PortalRuntimePermission.checkSetBeanProperty(CacheRegistryUtil.class);
+	private CacheRegistryUtil() {
+		Registry registry = RegistryUtil.getRegistry();
 
-		_cacheRegistry = cacheRegistry;
+		_serviceTracker = registry.trackServices(
+			CacheRegistryItem.class,
+			new CacheRegistryItemServiceTrackerCustomizer());
+
+		_serviceTracker.open();
 	}
 
-	public static void unregister(String name) {
-		getCacheRegistry().unregister(name);
+	private void _clear() {
+		for (Map.Entry<String, CacheRegistryItem> entry :
+				_cacheRegistryItems.entrySet()) {
+
+			CacheRegistryItem cacheRegistryItem = entry.getValue();
+
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					"Invalidating " + cacheRegistryItem.getRegistryName());
+			}
+
+			cacheRegistryItem.invalidate();
+		}
 	}
 
-	private static CacheRegistry _cacheRegistry;
+	private void _clear(String name) {
+		CacheRegistryItem cacheRegistryItem = _cacheRegistryItems.get(name);
+
+		if (cacheRegistryItem != null) {
+			if (_log.isDebugEnabled()) {
+				_log.debug("Invalidating " + name);
+			}
+
+			cacheRegistryItem.invalidate();
+		}
+		else {
+			_log.error("No cache registry found with name " + name);
+		}
+	}
+
+	private boolean _isActive() {
+		return _active;
+	}
+
+	private void _setActive(boolean active) {
+		_active = active;
+
+		if (!active) {
+			_clear();
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CacheRegistryUtil.class);
+
+	private static final CacheRegistryUtil _instance = new CacheRegistryUtil();
+
+	private volatile boolean _active = true;
+	private final Map<String, CacheRegistryItem> _cacheRegistryItems =
+		new ConcurrentHashMap<>();
+	private final ServiceTracker<CacheRegistryItem, CacheRegistryItem>
+		_serviceTracker;
+
+	private class CacheRegistryItemServiceTrackerCustomizer
+		implements ServiceTrackerCustomizer<CacheRegistryItem, CacheRegistryItem> {
+
+		@Override
+		public CacheRegistryItem addingService(
+			ServiceReference<CacheRegistryItem> serviceReference) {
+
+			Registry registry = RegistryUtil.getRegistry();
+
+			CacheRegistryItem cacheRegistryItem = registry.getService(
+				serviceReference);
+
+			_cacheRegistryItems.put(
+				cacheRegistryItem.getRegistryName(), cacheRegistryItem);
+
+			return cacheRegistryItem;
+		}
+
+		@Override
+		public void modifiedService(
+			ServiceReference<CacheRegistryItem> serviceReference,
+			CacheRegistryItem cacheRegistryItem) {
+		}
+
+		@Override
+		public void removedService(
+			ServiceReference<CacheRegistryItem> serviceReference,
+			CacheRegistryItem cacheRegistryItem) {
+
+			_cacheRegistryItems.put(
+				cacheRegistryItem.getRegistryName(), cacheRegistryItem);
+		}
+
+	}
 
 }
