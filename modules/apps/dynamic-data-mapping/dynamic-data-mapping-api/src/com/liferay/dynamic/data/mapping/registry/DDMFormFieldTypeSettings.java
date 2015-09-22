04f76968464f263dcda609748396f584f7872584
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-09-22 13:02:04 -0300

    LPS-58829 Apply

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/com/liferay/dynamic/data/mapping/registry/DDMFormFieldTypeSettings.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/com/liferay/dynamic/data/mapping/registry/DDMFormFieldTypeSettings.java
@@ -23,17 +23,17 @@ import com.liferay.dynamic.data.mapping.registry.annotations.DDMFormField;
 @DDMForm(localization = "content/Language")
 public interface DDMFormFieldTypeSettings {
 
-	@DDMFormField(visibilityExpression = "false")
+	@DDMFormField(required = true, visibilityExpression = "false")
 	public String dataType();
 
 	@DDMFormField(
 		label = "%name",
 		properties = {"setting.category=basic", "setting.weight=3"},
-		validationExpression = "!name.equals(\"\")"
+		required = true
 	)
 	public String name();
 
-	@DDMFormField(visibilityExpression = "false")
+	@DDMFormField(required = true, visibilityExpression = "false")
 	public String type();
 
 }
