ce7ed0a09a29bac6c2d57d15bba165ba8cec0c0d
Carlos Sierra <carlos.sierra@liferay.com>
2015-09-22 16:58:19 +0200

    LPS-58834 Add parameter to the factory
    Adding only to the method from which it is going to be used so the
    interface does not get cluttered.
    
    We should subtitute the factory with a builder pattern.

--- modules/core/osgi-service-tracker-map/src/com/liferay/osgi/service/tracker/map/ServiceTrackerMapFactory.java
+++ modules/core/osgi-service-tracker-map/src/com/liferay/osgi/service/tracker/map/ServiceTrackerMapFactory.java
@@ -40,7 +40,7 @@ public class ServiceTrackerMapFactory {
 			bundleContext, clazz, "(" + propertyKey + "=*)",
 			new PropertyServiceReferenceMapper<String, S>(propertyKey),
 			new DefaultServiceTrackerCustomizer<S>(bundleContext),
-			new MultiValueServiceTrackerBucketFactory<S, S>());
+			new MultiValueServiceTrackerBucketFactory<S, S>(), null);
 	}
 
 	public static <K, S> ServiceTrackerMap<K, List<S>> multiValueMap(
@@ -51,7 +51,7 @@ public class ServiceTrackerMapFactory {
 		return new ServiceTrackerMapImpl<>(
 			bundleContext, clazz, filterString, serviceReferenceMapper,
 			new DefaultServiceTrackerCustomizer<S>(bundleContext),
-			new MultiValueServiceTrackerBucketFactory<S, S>());
+			new MultiValueServiceTrackerBucketFactory<S, S>(), null);
 	}
 
 	public static <K, S> ServiceTrackerMap<K, List<S>> multiValueMap(
@@ -63,7 +63,7 @@ public class ServiceTrackerMapFactory {
 		return new ServiceTrackerMapImpl<>(
 			bundleContext, clazz, filterString, serviceReferenceMapper,
 			new DefaultServiceTrackerCustomizer<S>(bundleContext),
-			new MultiValueServiceTrackerBucketFactory<S, S>(comparator));
+			new MultiValueServiceTrackerBucketFactory<S, S>(comparator), null);
 	}
 
 	public static <K, SR, S> ServiceTrackerMap<K, List<S>> multiValueMap(
@@ -75,7 +75,7 @@ public class ServiceTrackerMapFactory {
 		return new ServiceTrackerMapImpl<>(
 			bundleContext, clazz, filterString, serviceReferenceMapper,
 			serviceTrackerCustomizer,
-			new MultiValueServiceTrackerBucketFactory<SR, S>());
+			new MultiValueServiceTrackerBucketFactory<SR, S>(), null);
 	}
 
 	public static <K, SR, S> ServiceTrackerMap<K, List<S>> multiValueMap(
@@ -88,7 +88,22 @@ public class ServiceTrackerMapFactory {
 		return new ServiceTrackerMapImpl<>(
 			bundleContext, clazz, filterString, serviceReferenceMapper,
 			serviceTrackerCustomizer,
-			new MultiValueServiceTrackerBucketFactory<SR, S>(comparator));
+			new MultiValueServiceTrackerBucketFactory<SR, S>(comparator), null);
+	}
+
+	public static <K, SR, S> ServiceTrackerMap<K, List<S>> multiValueMap(
+			BundleContext bundleContext, Class<SR> clazz, String filterString,
+			ServiceReferenceMapper<K, ? super SR> serviceReferenceMapper,
+			ServiceTrackerCustomizer<SR, S> serviceTrackerCustomizer,
+			Comparator<ServiceReference<SR>> comparator,
+			ServiceTrackerMapListener<K, S, List<S>> serviceTrackerMapListener)
+		throws InvalidSyntaxException {
+
+		return new ServiceTrackerMapImpl<>(
+			bundleContext, clazz, filterString, serviceReferenceMapper,
+			serviceTrackerCustomizer,
+			new MultiValueServiceTrackerBucketFactory<SR, S>(comparator),
+			serviceTrackerMapListener);
 	}
 
 	public static <SR, S> ServiceTrackerMap<String, List<S>> multiValueMap(
@@ -100,7 +115,7 @@ public class ServiceTrackerMapFactory {
 			bundleContext, clazz, "(" + propertyKey + "=*)",
 			new PropertyServiceReferenceMapper<String, SR>(propertyKey),
 			serviceTrackerCustomizer,
-			new MultiValueServiceTrackerBucketFactory<SR, S>());
+			new MultiValueServiceTrackerBucketFactory<SR, S>(), null);
 	}
 
 	public static <S> ServiceTrackerMap<String, S> singleValueMap(
@@ -111,7 +126,7 @@ public class ServiceTrackerMapFactory {
 			bundleContext, clazz, "(" + propertyKey + "=*)",
 			new PropertyServiceReferenceMapper<String, S>(propertyKey),
 			new DefaultServiceTrackerCustomizer<S>(bundleContext),
-			new SingleValueServiceTrackerBucketFactory<S, S>());
+			new SingleValueServiceTrackerBucketFactory<S, S>(), null);
 	}
 
 	public static <K, S> ServiceTrackerMap<K, S> singleValueMap(
@@ -122,7 +137,7 @@ public class ServiceTrackerMapFactory {
 		return new ServiceTrackerMapImpl<>(
 			bundleContext, clazz, filterString, serviceReferenceMapper,
 			new DefaultServiceTrackerCustomizer<S>(bundleContext),
-			new SingleValueServiceTrackerBucketFactory<S, S>());
+			new SingleValueServiceTrackerBucketFactory<S, S>(), null);
 	}
 
 	public static <K, S> ServiceTrackerMap<K, S> singleValueMap(
@@ -134,7 +149,7 @@ public class ServiceTrackerMapFactory {
 		return new ServiceTrackerMapImpl<>(
 			bundleContext, clazz, filterString, serviceReferenceMapper,
 			new DefaultServiceTrackerCustomizer<S>(bundleContext),
-			new SingleValueServiceTrackerBucketFactory<S, S>(comparator));
+			new SingleValueServiceTrackerBucketFactory<S, S>(comparator), null);
 	}
 
 	public static <K, SR, S> ServiceTrackerMap<K, S> singleValueMap(
@@ -146,7 +161,7 @@ public class ServiceTrackerMapFactory {
 		return new ServiceTrackerMapImpl<>(
 			bundleContext, clazz, filterString, serviceReferenceMapper,
 			serviceTrackerCustomizer,
-			new SingleValueServiceTrackerBucketFactory<SR, S>());
+			new SingleValueServiceTrackerBucketFactory<SR, S>(), null);
 	}
 
 	public static <K, SR, S> ServiceTrackerMap<K, S> singleValueMap(
@@ -159,7 +174,8 @@ public class ServiceTrackerMapFactory {
 		return new ServiceTrackerMapImpl<>(
 			bundleContext, clazz, filterString, serviceReferenceMapper,
 			serviceTrackerCustomizer,
-			new SingleValueServiceTrackerBucketFactory<SR, S>(comparator));
+			new SingleValueServiceTrackerBucketFactory<SR, S>(comparator),
+			null);
 	}
 
 	public static <SR, S> ServiceTrackerMap<String, S> singleValueMap(
@@ -171,7 +187,7 @@ public class ServiceTrackerMapFactory {
 			bundleContext, clazz, "(" + propertyKey + "=*)",
 			new PropertyServiceReferenceMapper<String, SR>(propertyKey),
 			serviceTrackerCustomizer,
-			new SingleValueServiceTrackerBucketFactory<SR, S>());
+			new SingleValueServiceTrackerBucketFactory<SR, S>(), null);
 	}
 
 }
