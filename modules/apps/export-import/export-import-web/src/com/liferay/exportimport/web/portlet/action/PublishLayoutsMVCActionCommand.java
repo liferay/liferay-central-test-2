a81d8935fa7308898569e61456ed8f3aa31fef6f
Levente Hudak <levente.hudak@liferay.com>
2015-09-22 17:53:04 +0200

    LPS-58731 Selecting Date Range at Export/Import starts the export process

--- modules/apps/export-import/export-import-web/src/com/liferay/exportimport/web/portlet/action/PublishLayoutsMVCActionCommand.java
+++ modules/apps/export-import/export-import-web/src/com/liferay/exportimport/web/portlet/action/PublishLayoutsMVCActionCommand.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.lock.DuplicateLockException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -29,8 +30,12 @@ import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.security.auth.AuthException;
 import com.liferay.portal.security.auth.PrincipalException;
 import com.liferay.portal.security.auth.RemoteAuthException;
+import com.liferay.portal.util.PortalUtil;
 import com.liferay.portlet.exportimport.RemoteExportException;
+import com.liferay.portlet.exportimport.configuration.ExportImportConfigurationParameterMapFactory;
 import com.liferay.portlet.exportimport.staging.StagingUtil;
+
+import java.util.Map;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -57,44 +62,55 @@ public class PublishLayoutsMVCActionCommand extends BaseMVCActionCommand {
 
 		String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
 
+		if (Validator.isNull(cmd)) {
+			String portletId = PortalUtil.getPortletId(actionRequest);
+
+			Map<String, String[]> parameterMap =
+				ExportImportConfigurationParameterMapFactory.buildParameterMap(
+					actionRequest);
+
+			SessionMessages.add(
+				actionRequest, portletId + "parameterMap", parameterMap);
+
+			return;
+		}
+
 		String redirect = ParamUtil.getString(actionRequest, "redirect");
 
 		try {
-			if (Validator.isNotNull(cmd)) {
-				if (cmd.equals("copy_from_live")) {
-					StagingUtil.copyFromLive(actionRequest);
-				}
-				else if (cmd.equals(Constants.PUBLISH_TO_LIVE)) {
-					hideDefaultSuccessMessage(actionRequest);
-
-					StagingUtil.publishToLive(actionRequest);
-				}
-				else if (cmd.equals(Constants.PUBLISH_TO_REMOTE)) {
-					hideDefaultSuccessMessage(actionRequest);
-
-					StagingUtil.publishToRemote(actionRequest);
-				}
-				else if (cmd.equals("schedule_copy_from_live")) {
-					StagingUtil.scheduleCopyFromLive(actionRequest);
-				}
-				else if (cmd.equals("schedule_publish_to_live")) {
-					StagingUtil.schedulePublishToLive(actionRequest);
-				}
-				else if (cmd.equals("schedule_publish_to_remote")) {
-					StagingUtil.schedulePublishToRemote(actionRequest);
-				}
-				else if (cmd.equals("unschedule_copy_from_live")) {
-					StagingUtil.unscheduleCopyFromLive(actionRequest);
-				}
-				else if (cmd.equals("unschedule_publish_to_live")) {
-					StagingUtil.unschedulePublishToLive(actionRequest);
-				}
-				else if (cmd.equals("unschedule_publish_to_remote")) {
-					StagingUtil.unschedulePublishToRemote(actionRequest);
-				}
-
-				sendRedirect(actionRequest, actionResponse, redirect);
+			if (cmd.equals("copy_from_live")) {
+				StagingUtil.copyFromLive(actionRequest);
 			}
+			else if (cmd.equals(Constants.PUBLISH_TO_LIVE)) {
+				hideDefaultSuccessMessage(actionRequest);
+
+				StagingUtil.publishToLive(actionRequest);
+			}
+			else if (cmd.equals(Constants.PUBLISH_TO_REMOTE)) {
+				hideDefaultSuccessMessage(actionRequest);
+
+				StagingUtil.publishToRemote(actionRequest);
+			}
+			else if (cmd.equals("schedule_copy_from_live")) {
+				StagingUtil.scheduleCopyFromLive(actionRequest);
+			}
+			else if (cmd.equals("schedule_publish_to_live")) {
+				StagingUtil.schedulePublishToLive(actionRequest);
+			}
+			else if (cmd.equals("schedule_publish_to_remote")) {
+				StagingUtil.schedulePublishToRemote(actionRequest);
+			}
+			else if (cmd.equals("unschedule_copy_from_live")) {
+				StagingUtil.unscheduleCopyFromLive(actionRequest);
+			}
+			else if (cmd.equals("unschedule_publish_to_live")) {
+				StagingUtil.unschedulePublishToLive(actionRequest);
+			}
+			else if (cmd.equals("unschedule_publish_to_remote")) {
+				StagingUtil.unschedulePublishToRemote(actionRequest);
+			}
+
+			sendRedirect(actionRequest, actionResponse, redirect);
 		}
 		catch (Exception e) {
 			if (e instanceof PrincipalException) {
