143776923e0c688467af5c6e7580b8d466a9c9e1
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-09-22 14:29:49 +0200

    LPS-58828

---
+++ modules/apps/blogs/blogs-web/src/com/liferay/blogs/web/portlet/configuration/icon/PermissionsPortletConfigurationIcon.java
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.blogs.web.portlet.configuration.icon;
+
+import com.liferay.blogs.web.constants.BlogsPortletKeys;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.model.User;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.theme.PortletDisplay;
+import com.liferay.portlet.blogs.service.permission.BlogsPermission;
+import com.liferay.taglib.security.PermissionsURLTag;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Sergio González
+ */
+public class PermissionsPortletConfigurationIcon
+	extends BasePortletConfigurationIcon {
+
+	public PermissionsPortletConfigurationIcon(HttpServletRequest request) {
+		super(request);
+	}
+
+	@Override
+	public String getImage() {
+		return "../aui/lock";
+	}
+
+	@Override
+	public String getMessage() {
+		return "permissions";
+	}
+
+	@Override
+	public String getURL() {
+		String url = StringPool.BLANK;
+
+		try {
+			url = PermissionsURLTag.doTag(
+				StringPool.BLANK, "com.liferay.portlet.blogs",
+				themeDisplay.getScopeGroupName(), null,
+				String.valueOf(themeDisplay.getScopeGroupId()),
+				LiferayWindowState.POP_UP.toString(), null,
+				themeDisplay.getRequest());
+		}
+		catch (Exception e) {
+		}
+
+		return url;
+	}
+
+	@Override
+	public boolean isShow() {
+		User user = themeDisplay.getUser();
+
+		if (user.isDefaultUser()) {
+			return false;
+		}
+
+		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+		String rootPortletId = portletDisplay.getRootPortletId();
+
+		if (!rootPortletId.equals(BlogsPortletKeys.BLOGS_ADMIN)) {
+			return false;
+		}
+
+		PermissionChecker permissionChecker =
+			themeDisplay.getPermissionChecker();
+
+		try {
+			if (!BlogsPermission.contains(
+					permissionChecker, themeDisplay.getScopeGroupId(),
+					ActionKeys.PERMISSIONS)) {
+
+				return false;
+			}
+		}
+		catch (Exception e) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean isToolTip() {
+		return false;
+	}
+
+	@Override
+	public boolean isUseDialog() {
+		return true;
+	}
+
+}
