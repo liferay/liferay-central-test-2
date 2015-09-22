973491df223775504fc6c0563588eb5fc21fec33
shuyangzhou <shuyang.zhou@liferay.com>
2015-09-22 11:22:55 -0700

    LPS-58449 Preserve the exception on bad input

--- portal-impl/src/com/liferay/portal/service/impl/PortletLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/PortletLocalServiceImpl.java
@@ -1371,7 +1371,7 @@ public class PortletLocalServiceImpl extends PortletLocalServiceBaseImpl {
 				schedulerEntryImpl.setTrigger(
 					TriggerFactoryUtil.createTrigger(
 						eventListenerClass, eventListenerClass,
-						GetterUtil.getInteger(intervalString),
+						GetterUtil.getIntegerStrict(intervalString),
 						TimeUnit.valueOf(timeUnitString)));
 			}
 
