b370b2f25ece05daf39a15e283a027182b687343
Leslie Wong <leslie.wong@liferay.com>
2015-09-23 17:07:18 -0700

    LRQA-17722 Record status in command log before summary log

--- modules/test/poshi-runner/src/com/liferay/poshi/runner/PoshiRunnerExecutor.java
+++ modules/test/poshi-runner/src/com/liferay/poshi/runner/PoshiRunnerExecutor.java
@@ -380,10 +380,10 @@ public class PoshiRunnerExecutor {
 
 				PoshiRunnerStackTraceUtil.setCurrentElement(executeElement);
 
+				CommandLoggerHandler.failCommand(_functionExecuteElement);
+
 				SummaryLoggerHandler.failSummary(
 					_functionExecuteElement, t.getMessage());
-
-				CommandLoggerHandler.failCommand(_functionExecuteElement);
 
 				throw t;
 			}
@@ -395,15 +395,15 @@ public class PoshiRunnerExecutor {
 
 		if (_functionExecuteElement == executeElement) {
 			if (_functionWarningMessage != null) {
+				CommandLoggerHandler.warnCommand(_functionExecuteElement);
+
 				SummaryLoggerHandler.warnSummary(
 					_functionExecuteElement, _functionWarningMessage);
-
-				CommandLoggerHandler.warnCommand(_functionExecuteElement);
 			}
 			else {
-				SummaryLoggerHandler.passSummary(executeElement);
-
 				CommandLoggerHandler.passCommand(executeElement);
+
+				SummaryLoggerHandler.passSummary(executeElement);
 			}
 
 			_functionExecuteElement = null;
