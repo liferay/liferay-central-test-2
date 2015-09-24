26a0138a455c5d629fdcfbf48409e864b27e1b77
Brian Chan <brian.chan@liferay.com>
2015-09-24 10:44:54 -0700

    LPS-58159 Hugo, can you enforce this

--- modules/apps/bookmarks/bookmarks-service/src/com/liferay/bookmarks/service/persistence/impl/BookmarksEntryFinderImpl.java
+++ modules/apps/bookmarks/bookmarks-service/src/com/liferay/bookmarks/service/persistence/impl/BookmarksEntryFinderImpl.java
@@ -28,8 +28,7 @@ import java.util.List;
  * @author Brian Wing Shun Chan
  */
 public class BookmarksEntryFinderImpl
-	extends BookmarksEntryFinderBaseImpl
-	implements BookmarksEntryFinder {
+	extends BookmarksEntryFinderBaseImpl implements BookmarksEntryFinder {
 
 	public static final String FIND_BY_NO_ASSETS =
 		BookmarksEntryFinder.class.getName() + ".findByNoAssets";
