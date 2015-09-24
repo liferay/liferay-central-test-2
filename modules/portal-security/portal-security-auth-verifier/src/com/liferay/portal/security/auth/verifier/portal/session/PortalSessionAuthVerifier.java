f3eab15d4e9e345d3fea0f21ea4e85e6dcdeb559
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2015-09-24 20:44:15 +0200

    LPS-58639 Register PortalSessionAuthVerifier based on Configuration objects

--- modules/portal-security/portal-security-auth-verifier/src/com/liferay/portal/security/auth/verifier/portal/session/PortalSessionAuthVerifier.java
+++ modules/portal-security/portal-security-auth-verifier/src/com/liferay/portal/security/auth/verifier/portal/session/PortalSessionAuthVerifier.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.security.auth.verifier;
+package com.liferay.portal.security.auth.verifier.portal.session;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
