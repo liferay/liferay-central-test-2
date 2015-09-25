fbb84086f19d230d6bf996584f58dc9897b42d12
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-09-25 13:00:58 +0200

    LPS-58895 Moves info message to diff html taglib

--- util-taglib/src/com/liferay/taglib/ui/DiffHtmlTag.java
+++ util-taglib/src/com/liferay/taglib/ui/DiffHtmlTag.java
@@ -27,9 +27,14 @@ public class DiffHtmlTag extends IncludeTag {
 		_diffHtmlResults = diffHtmlResults;
 	}
 
+	public void setInfoMessage(String infoMessage) {
+		_infoMessage = infoMessage;
+	}
+
 	@Override
 	protected void cleanUp() {
 		_diffHtmlResults = null;
+		_infoMessage = null;
 	}
 
 	@Override
@@ -41,10 +46,12 @@ public class DiffHtmlTag extends IncludeTag {
 	protected void setAttributes(HttpServletRequest request) {
 		request.setAttribute(
 			"liferay-ui:diff-html:diffHtmlResults", _diffHtmlResults);
+		request.setAttribute("liferay-ui:diff-html:infoMessage", _infoMessage);
 	}
 
 	private static final String _PAGE = "/html/taglib/ui/diff_html/page.jsp";
 
 	private String _diffHtmlResults;
+	private String _infoMessage;
 
 }
