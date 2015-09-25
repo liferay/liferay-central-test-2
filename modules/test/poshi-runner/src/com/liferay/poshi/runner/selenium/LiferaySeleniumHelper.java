23f4f32247e668d19953efa3695a99314e544768
Michael Hashimoto <michael.hashimoto@liferay.com>
2015-09-25 06:40:45 -0700

    LRQA-17722 Fix compilation error

--- modules/test/poshi-runner/src/com/liferay/poshi/runner/selenium/LiferaySeleniumHelper.java
+++ modules/test/poshi-runner/src/com/liferay/poshi/runner/selenium/LiferaySeleniumHelper.java
@@ -41,6 +41,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
