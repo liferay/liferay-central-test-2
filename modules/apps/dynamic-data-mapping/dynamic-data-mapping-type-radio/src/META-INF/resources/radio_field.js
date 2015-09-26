8089f15c6d444357366a27850e66d0d050405b53
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-09-26 16:12:51 -0300

    LPS-55599 Update radio field.js

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-radio/src/META-INF/resources/radio_field.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-radio/src/META-INF/resources/radio_field.js
@@ -1,9 +1,15 @@
 AUI.add(
 	'liferay-ddm-form-field-radio',
 	function(A) {
+		var Lang = A.Lang;
+
 		var RadioField = A.Component.create(
 			{
 				ATTRS: {
+					inline: {
+						value: true
+					},
+
 					options: {
 						validator: Array.isArray,
 						value: []
@@ -19,12 +25,30 @@ AUI.add(
 				NAME: 'liferay-ddm-form-field-radio',
 
 				prototype: {
+					getInputNode: function() {
+						var instance = this;
+
+						var container = instance.get('container');
+
+						var radiosNodeList = container.all(instance.getInputSelector());
+
+						var inputNode = radiosNodeList.item(0);
+
+						var checkedNodeList = radiosNodeList.filter(':checked');
+
+						if (checkedNodeList.size()) {
+							inputNode = checkedNodeList.item(0);
+						}
+
+						return inputNode;
+					},
+
 					getOptions: function() {
 						var instance = this;
 
 						var value = instance.get('value');
 
-						if (instance.get('localizable')) {
+						if (Lang.isObject(value)) {
 							value = value[instance.get('locale')];
 						}
 
@@ -46,9 +70,20 @@ AUI.add(
 						return A.merge(
 							RadioField.superclass.getTemplateContext.apply(instance, arguments),
 							{
+								inline: instance.get('inline'),
 								options: instance.getOptions()
 							}
 						);
+					},
+
+					_renderErrorMessage: function() {
+						var instance = this;
+
+						var container = instance.get('container');
+
+						RadioField.superclass._renderErrorMessage.apply(instance, arguments);
+
+						container.all('.validation-message').appendTo(container);
 					}
 				}
 			}
