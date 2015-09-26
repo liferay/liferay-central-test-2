brianchandotcom/liferay-portal

LPS-58504 Add portlet decorators option to Look & Feel to replace show borders
look feel

modules/apps/portlet-css/portlet-css-web/src/META-INF/resources/view.jsp
@@ -96,9 +96,22 @@ String portletResource = ParamUtil.getString(request, "portletResource");
 
 							</aui:select>
 
-							<aui:select label="show-borders" name="show-borders" showEmptyOption="<%= true %>">
-								<aui:option label="yes" value="true" />
-								<aui:option label="no" value="false" />
+							<aui:select label="portlet-decorators" name="portlet-decorator">
+
+								<%
+								Theme selTheme = layout.getTheme();
+
+								List<PortletDecorator> portletDecorators = selTheme.getPortletDecorators();
+
+								for (PortletDecorator portletDecorator : portletDecorators) {
+								%>
+
+									<aui:option label="<%= portletDecorator.getName() %>" selected="<%= portletDecorator.isDefaultPd() %>" value="<%= portletDecorator.getPortletDecoratorId() %>" />
+
+								<%
+								}
+								%>
+
 							</aui:select>
 
 							<span class="alert alert-info form-hint hide" id="border-note">

liferay/liferay-central-test-2

modules/apps/portlet-css/portlet-css-web/src/META-INF/resources/view.jsp
@@ -96,9 +96,22 @@ String portletResource = ParamUtil.getString(request, "portletResource");
 
 							</aui:select>
 
-							<aui:select label="show-borders" name="show-borders" showEmptyOption="<%= true %>">
-								<aui:option label="yes" value="true" />
-								<aui:option label="no" value="false" />
+							<aui:select label="portlet-decorators" name="portlet-decorator">
+
+								<%
+								Theme selTheme = layout.getTheme();
+
+								List<PortletDecorator> portletDecorators = selTheme.getPortletDecorators();
+
+								for (PortletDecorator portletDecorator : portletDecorators) {
+								%>
+
+									<aui:option label="<%= portletDecorator.getName() %>" selected="<%= portletDecorator.isDefaultPortletDecorator() %>" value="<%= portletDecorator.getPortletDecoratorId() %>" />
+
+								<%
+								}
+								%>
+
 							</aui:select>
 
 							<span class="alert alert-info form-hint hide" id="border-note">