e68ca613e5d6b3f0ca44a01417d4ac209ee320ea
Juergen Kappler <jurgen.kappler@liferay.com>
2015-09-28 14:22:31 +0200

    LPS-58597 Remove full page displayable attribute in favour of application types

--- modules/portal/portal-portlet-tracker/src/com/liferay/portal/portlet/tracker/internal/PortletPropertyValidator.java
+++ modules/portal/portal-portlet-tracker/src/com/liferay/portal/portlet/tracker/internal/PortletPropertyValidator.java
@@ -70,7 +70,6 @@ public class PortletPropertyValidator {
 			"com.liferay.portlet.footer-portal-javascript",
 			"com.liferay.portlet.footer-portlet-css",
 			"com.liferay.portlet.footer-portlet-javascript",
-			"com.liferay.portlet.full-page-displayable",
 			"com.liferay.portlet.header-portal-css",
 			"com.liferay.portlet.header-portal-javascript",
 			"com.liferay.portlet.header-portlet-css",
