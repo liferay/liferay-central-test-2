885393ebd0c86db57fbd1a815e00a10d828aadad
Julio Camarero <julio.camarero@liferay.com>
2015-09-28 10:42:22 +0200

    LPS-55229 create class to avoid using arrays

--- portal-impl/src/com/liferay/portal/servlet/I18nServlet.java
+++ portal-impl/src/com/liferay/portal/servlet/I18nServlet.java
@@ -20,6 +20,7 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
@@ -82,27 +83,24 @@ public class I18nServlet extends HttpServlet {
 		throws IOException, ServletException {
 
 		try {
-			String[] i18nData = getI18nData(request);
+			I18nData i18nData = getI18nData(request);
 
 			if ((i18nData == null) ||
-				!PortalUtil.isValidResourceId(i18nData[2])) {
+				!PortalUtil.isValidResourceId(i18nData.getPath())) {
 
 				PortalUtil.sendError(
 					HttpServletResponse.SC_NOT_FOUND,
 					new NoSuchLayoutException(), request, response);
 			}
 			else {
-				String i18nLanguageId = i18nData[0];
-				String i18nPath = i18nData[1];
-				String redirect = i18nData[2];
-				String i18nLanguageCode = i18nData[3];
-
 				request.setAttribute(
-					WebKeys.I18N_LANGUAGE_CODE, i18nLanguageCode);
-				request.setAttribute(WebKeys.I18N_LANGUAGE_ID, i18nLanguageId);
-				request.setAttribute(WebKeys.I18N_PATH, i18nPath);
+					WebKeys.I18N_LANGUAGE_CODE, i18nData.getLanguageCode());
+				request.setAttribute(
+					WebKeys.I18N_LANGUAGE_ID, i18nData.getLanguageId());
+				request.setAttribute(WebKeys.I18N_PATH, i18nData.getI18nPath());
 
-				Locale locale = LocaleUtil.fromLanguageId(i18nLanguageId);
+				Locale locale = LocaleUtil.fromLanguageId(
+					i18nData.getLanguageId());
 
 				HttpSession session = request.getSession();
 
@@ -113,7 +111,7 @@ public class I18nServlet extends HttpServlet {
 				ServletContext servletContext = getServletContext();
 
 				RequestDispatcher requestDispatcher =
-					servletContext.getRequestDispatcher(redirect);
+					servletContext.getRequestDispatcher(i18nData.getPath());
 
 				requestDispatcher.forward(request, response);
 			}
@@ -127,7 +125,7 @@ public class I18nServlet extends HttpServlet {
 		}
 	}
 
-	protected String[] getI18nData(HttpServletRequest request) {
+	protected I18nData getI18nData(HttpServletRequest request) {
 		String path = GetterUtil.getString(request.getPathInfo());
 
 		if (Validator.isNull(path)) {
@@ -173,9 +171,85 @@ public class I18nServlet extends HttpServlet {
 			_log.debug("Redirect " + redirect);
 		}
 
-		return new String[] {
-			i18nLanguageId, i18nPath, redirect, locale.getLanguage()
-		};
+		return new I18nData(
+			i18nPath, locale.getLanguage(), i18nLanguageId, redirect);
+	}
+
+	protected I18nData getI18nData(Locale locale) {
+		String languageId = LocaleUtil.toLanguageId(locale);
+
+		return new I18nData(
+			StringPool.SLASH + languageId, locale.getLanguage(), languageId,
+			StringPool.SLASH);
+	}
+
+	protected class I18nData {
+
+		public I18nData(
+			String i18nPath, String languageCode, String languageId,
+			String path) {
+
+			_i18nPath = i18nPath;
+			_languageCode = languageCode;
+			_languageId = languageId;
+			_path = path;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+
+			if (!(obj instanceof I18nData)) {
+				return false;
+			}
+
+			I18nData i18nData = (I18nData)obj;
+
+			if (Validator.equals(getI18nPath(), i18nData.getI18nPath()) &&
+				Validator.equals(
+					getLanguageCode(), i18nData.getLanguageCode()) &&
+				Validator.equals(getLanguageId(), i18nData.getLanguageId()) &&
+				Validator.equals(getPath(), i18nData.getPath())) {
+
+				return true;
+			}
+
+			return false;
+		}
+
+		public String getI18nPath() {
+			return _i18nPath;
+		}
+
+		public String getLanguageCode() {
+			return _languageCode;
+		}
+
+		public String getLanguageId() {
+			return _languageId;
+		}
+
+		public String getPath() {
+			return _path;
+		}
+
+		@Override
+		public int hashCode() {
+			int hash = HashUtil.hash(0, getI18nPath());
+
+			hash = HashUtil.hash(hash, getLanguageCode());
+			hash = HashUtil.hash(hash, getLanguageId());
+
+			return HashUtil.hash(hash, getPath());
+		}
+
+		private final String _i18nPath;
+		private final String _languageCode;
+		private final String _languageId;
+		private final String _path;
+
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(I18nServlet.class);
