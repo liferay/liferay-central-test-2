9549eee903c55699408d62b2ade9754566646641
Fernando Souza <fernando.souza@liferay.com>
2015-09-29 10:52:03 -0300

    LPS-59017 Fix visual to options

--- modules/frontend/frontend-js-web/src/META-INF/resources/liferay/auto_fields.js
+++ modules/frontend/frontend-js-web/src/META-INF/resources/liferay/auto_fields.js
@@ -13,11 +13,11 @@ AUI.add(
 			'success-field'
 		];
 
-		var TPL_ADD_BUTTON = '<button class="add-row btn btn-content btn-icon-only toolbar-first toolbar-item" title="" type="button">' +
+		var TPL_ADD_BUTTON = '<button class="add-row btn btn-default btn-icon-only toolbar-first toolbar-item" title="" type="button">' +
 				'<span class="btn-icon icon icon-plus"></span>' +
 			'</button>';
 
-		var TPL_DELETE_BUTTON = '<button class="btn btn-content btn-icon-only delete-row toolbar-item toolbar-last" title="" type="button">' +
+		var TPL_DELETE_BUTTON = '<button class="btn btn-default btn-icon-only delete-row toolbar-item toolbar-last" title="" type="button">' +
 				'<span class="btn-icon icon icon-minus"></span>' +
 			'</button>';
 
