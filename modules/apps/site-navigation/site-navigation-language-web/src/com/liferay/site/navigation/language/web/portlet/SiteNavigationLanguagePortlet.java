ab22584733a35cf768be787f1130379f76a3ddff
Juergen Kappler <jurgen.kappler@liferay.com>
2015-09-29 16:41:21 +0200

    LPS-59046 In Site Navigation Language Web, reference Portlet Display Template and inject it in the request

--- modules/apps/site-navigation/site-navigation-language-web/src/com/liferay/site/navigation/language/web/portlet/SiteNavigationLanguagePortlet.java
+++ modules/apps/site-navigation/site-navigation-language-web/src/com/liferay/site/navigation/language/web/portlet/SiteNavigationLanguagePortlet.java
@@ -15,9 +15,16 @@
 package com.liferay.site.navigation.language.web.portlet;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portlet.display.template.PortletDisplayTemplate;
 import com.liferay.site.navigation.language.web.upgrade.SiteNavigationLanguageWebUpgrade;
 
+import java.io.IOException;
+
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -50,9 +57,29 @@ import org.osgi.service.component.annotations.Reference;
 )
 public class SiteNavigationLanguagePortlet extends MVCPortlet {
 
+	@Override
+	protected void doDispatch(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		renderRequest.setAttribute(
+			WebKeys.PORTLET_DISPLAY_TEMPLATE, _portletDisplayTemplate);
+
+		super.doDispatch(renderRequest, renderResponse);
+	}
+
+	@Reference
+	protected void setPortletDisplayTemplate(
+		PortletDisplayTemplate portletDisplayTemplate) {
+
+		_portletDisplayTemplate = portletDisplayTemplate;
+	}
+
 	@Reference(unbind = "-")
 	protected void setSiteNavigationLanguageWebUpgrade(
 		SiteNavigationLanguageWebUpgrade siteNavigationLanguageWebUpgrade) {
 	}
+
+	private PortletDisplayTemplate _portletDisplayTemplate;
 
 }
