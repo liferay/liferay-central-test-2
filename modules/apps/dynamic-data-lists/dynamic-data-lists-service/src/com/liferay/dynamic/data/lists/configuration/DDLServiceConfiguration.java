5cdd216d0a62fa8a622fb1e23930359adcc5935f
Lino <lino.alves@liferay.com>
2015-09-30 18:33:36 -0300

    LPS-55879 - Add some DDL properties to Configuration Admin

---
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/com/liferay/dynamic/data/lists/configuration/DDLServiceConfiguration.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+/**
+ * @author Lino Alves
+ */
+@Meta.OCD(
+	id = "com.liferay.dynamic.data.lists.configuration.DDLServiceConfiguration",
+	name = "Dynamic Data Lists Service Configuration"
+)
+public interface DDLServiceConfiguration {
+
+	@Meta.AD(deflt = "list", required = false)
+	public String defaultDisplayView();
+	
+	@Meta.AD(
+			deflt = "descriptive | list",
+			required = false
+			)
+	public String[] supportedDisplayView();
+
+	@Meta.AD(deflt = "json", required = false)
+	public String storageType();
+	
+}
