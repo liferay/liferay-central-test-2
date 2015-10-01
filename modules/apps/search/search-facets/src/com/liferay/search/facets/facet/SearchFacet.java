6d79d27ea3f734d784e5da0c3254a02d88f79973
Brian Chan <brian.chan@liferay.com>
2015-10-01 16:14:05 -0700

    LPS-57070 Move

--- modules/apps/search/search-facets/src/com/liferay/search/facets/facet/SearchFacet.java
+++ modules/apps/search/search-facets/src/com/liferay/search/facets/facet/SearchFacet.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.search.facets.api.facet;
+package com.liferay.search.facets.facet;
 
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.search.SearchContext;
