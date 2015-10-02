c0ceea4c5bb177e6e6fbade5bfd23133a28fb00f
Lino <lino.alves@liferay.com>
2015-10-02 17:06:46 -0300

    LPS-55879 - Add some DDLForm properties to configuration admin

---
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/com/liferay/dynamic/data/lists/form/web/configuration/definition/DDLFormWebConfigurationPidMapping.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.form.web.configuration.definition;
+
+import com.liferay.dynamic.data.lists.form.web.configuration.DDLFormWebConfiguration;
+import com.liferay.dynamic.data.lists.form.web.constants.DDLFormConstants;
+import com.liferay.portal.kernel.settings.definition.ConfigurationPidMapping;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Lino Alves
+ */
+@Component
+public class DDLFormWebConfigurationPidMapping
+	implements ConfigurationPidMapping {
+
+	@Override
+	public Class<?> getConfigurationBeanClass() {
+		return DDLFormWebConfiguration.class;
+	}
+
+	@Override
+	public String getConfigurationPid() {
+		return DDLFormConstants.SERVICE_NAME;
+	}
+
+}
