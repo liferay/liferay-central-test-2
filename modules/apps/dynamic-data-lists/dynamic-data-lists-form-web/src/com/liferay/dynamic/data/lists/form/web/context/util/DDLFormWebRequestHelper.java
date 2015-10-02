d806d373d985c1c9e5b35d02f81412189d5142f8
Lino <lino.alves@liferay.com>
2015-10-02 17:16:57 -0300

    LPS-55879 - using new DDLFormWebConfiguration

---
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/com/liferay/dynamic/data/lists/form/web/context/util/DDLFormWebRequestHelper.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.form.web.context.util;
+
+import com.liferay.dynamic.data.lists.form.web.configuration.DDLFormWebConfiguration;
+import com.liferay.dynamic.data.lists.form.web.constants.DDLFormConstants;
+import com.liferay.portal.kernel.display.context.util.BaseRequestHelper;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.module.configuration.ConfigurationFactoryUtil;
+import com.liferay.portal.kernel.settings.GroupServiceSettingsLocator;
+import com.liferay.portal.kernel.settings.ParameterMapSettingsLocator;
+import com.liferay.portal.kernel.util.Validator;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Lino Alves
+ */
+public class DDLFormWebRequestHelper extends BaseRequestHelper {
+
+	public DDLFormWebRequestHelper(HttpServletRequest request) {
+		super(request);
+	}
+
+	public DDLFormWebConfiguration getDDLFormWebConfiguration() {
+		try {
+			if (_ddlFormWebConfiguration == null) {
+				if (Validator.isNotNull(getPortletResource())) {
+					HttpServletRequest request = getRequest();
+
+					_ddlFormWebConfiguration =
+						ConfigurationFactoryUtil.getConfiguration(
+							DDLFormWebConfiguration.class,
+						new ParameterMapSettingsLocator(
+							request.getParameterMap(),
+							new GroupServiceSettingsLocator(
+								getSiteGroupId(),
+								DDLFormConstants.SERVICE_NAME)));
+				}
+				else {
+					_ddlFormWebConfiguration =
+						ConfigurationFactoryUtil.getConfiguration(
+							DDLFormWebConfiguration.class,
+							new GroupServiceSettingsLocator(
+								getSiteGroupId(),
+								DDLFormConstants.SERVICE_NAME));
+				}
+			}
+
+			return _ddlFormWebConfiguration;
+		}
+		catch (PortalException pe) {
+			throw new SystemException(pe);
+		}
+	}
+
+	private DDLFormWebConfiguration _ddlFormWebConfiguration;
+
+}
