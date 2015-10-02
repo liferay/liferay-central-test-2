ff74457c1d842d1635360bbffbf1bad53af70450
Iván Zaera Avellón <ivan.zaera@liferay.com>
2015-10-02 12:20:31 +0200

    LPS-58680 Implement the ExtendedMetaTypeService based on Liferay's runtime annotations
    This is a temporal hack to be able to continue with configuration efforts
    until we finally integrate BND 3 into the build.
    
    Once we achieve that, we can simply remove this project and begin using
    the Equinox based implementation.

---
+++ modules/apps/configuration-admin/configuration-admin-annotations/src/com/liferay/configuration/admin/annotations/AnnotationsExtendedMetaTypeService.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.configuration.admin.annotations;
+
+import com.liferay.configuration.admin.api.ExtendedMetaTypeInformation;
+import com.liferay.configuration.admin.api.ExtendedMetaTypeService;
+
+import org.osgi.framework.Bundle;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.metatype.MetaTypeService;
+
+/**
+ * @author Iván Zaera
+ */
+@Component(service = ExtendedMetaTypeService.class)
+public class AnnotationsExtendedMetaTypeService
+	implements ExtendedMetaTypeService {
+
+	@Override
+	public ExtendedMetaTypeInformation getMetaTypeInformation(Bundle bundle) {
+		return new AnnotationsExtendedMetaTypeInformation(
+			bundle, _metaTypeService.getMetaTypeInformation(bundle));
+	}
+
+	@Reference
+	protected void setMetaTypeService(MetaTypeService metaTypeService) {
+		_metaTypeService = metaTypeService;
+	}
+
+	private MetaTypeService _metaTypeService;
+
+}
