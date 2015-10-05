0973ee035738c48a6368aac1f3d8dc6d8854109c
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2015-10-05 23:21:34 +0200

    LPS-59214 Metatype Configuration: allow to configure the hosts that will be able to invoke this remote services

---
+++ modules/portal/portal-http-tunnel-extender/src/com/liferay/portal/http/tunnel/configuration/HttpTunnelExtenderConfiguration.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.http.tunnel.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+/**
+ * @author Miguel Pastor
+ */
+@Meta.OCD(
+	id = "com.liferay.portal.http.tunnel.configuration.HttpTunnelExtenderConfiguration"
+)
+public interface HttpTunnelExtenderConfiguration {
+
+	@Meta.AD(deflt = "255.255.255.255", required = false)
+	public String[] hostsAllowed();
+
+}
