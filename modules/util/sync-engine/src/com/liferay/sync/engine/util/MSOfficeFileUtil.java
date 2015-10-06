9d35f5b0d724b43b15b3e4c8b3f2d5cae87a12b6
Shinn Lok <shinn.lok@liferay.com>
2015-10-06 10:40:53 -0700

    SYNC-1442 Extension check is cheaper than directory check

--- modules/util/sync-engine/src/com/liferay/sync/engine/util/MSOfficeFileUtil.java
+++ modules/util/sync-engine/src/com/liferay/sync/engine/util/MSOfficeFileUtil.java
@@ -53,17 +53,15 @@ public class MSOfficeFileUtil {
 	}
 
 	public static boolean isExcelFile(Path filePath) {
-		if (Files.isDirectory(filePath)) {
-			return false;
-		}
-
 		String extension = FilenameUtils.getExtension(filePath.toString());
 
 		if (extension == null) {
 			return false;
 		}
 
-		if (_excelExtensions.contains(extension.toLowerCase())) {
+		if (_excelExtensions.contains(extension.toLowerCase()) &&
+			!Files.isDirectory(filePath)) {
+
 			return true;
 		}
 
@@ -71,10 +69,6 @@ public class MSOfficeFileUtil {
 	}
 
 	public static boolean isLegacyExcelFile(Path filePath) {
-		if (Files.isDirectory(filePath)) {
-			return false;
-		}
-
 		String extension = FilenameUtils.getExtension(filePath.toString());
 
 		if (extension == null) {
@@ -83,7 +77,7 @@ public class MSOfficeFileUtil {
 
 		extension = extension.toLowerCase();
 
-		if (extension.equals("xls")) {
+		if (extension.equals("xls") && !Files.isDirectory(filePath)) {
 			return true;
 		}
 
@@ -91,14 +85,11 @@ public class MSOfficeFileUtil {
 	}
 
 	public static boolean isTempCreatedFile(Path filePath) {
-		if (Files.isDirectory(filePath)) {
-			return false;
-		}
-
 		String fileName = String.valueOf(filePath.getFileName());
 
-		if (fileName.startsWith("~$") ||
-			(fileName.startsWith("~") && fileName.endsWith(".tmp"))) {
+		if ((fileName.startsWith("~$") ||
+			 (fileName.startsWith("~") && fileName.endsWith(".tmp"))) &&
+			!Files.isDirectory(filePath)) {
 
 			return true;
 		}
