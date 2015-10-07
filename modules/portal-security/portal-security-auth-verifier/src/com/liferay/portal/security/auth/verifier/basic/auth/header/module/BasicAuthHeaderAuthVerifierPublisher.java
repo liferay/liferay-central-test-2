70f94c93eca34a0b6f0dbd076c1f2ede9a6d1bc2
Brian Chan <brian.chan@liferay.com>
2015-10-07 15:30:26 -0700

    LPS-58639 Rename, I generally use the word Base* to mean Abstract*

--- modules/portal-security/portal-security-auth-verifier/src/com/liferay/portal/security/auth/verifier/basic/auth/header/module/BasicAuthHeaderAuthVerifierPublisher.java
+++ modules/portal-security/portal-security-auth-verifier/src/com/liferay/portal/security/auth/verifier/basic/auth/header/module/BasicAuthHeaderAuthVerifierPublisher.java
@@ -16,7 +16,7 @@ package com.liferay.portal.security.auth.verifier.basic.auth.header.module;
 
 import com.liferay.portal.kernel.security.auth.verifier.AuthVerifier;
 import com.liferay.portal.security.auth.verifier.basic.auth.header.BasicAuthHeaderAuthVerifier;
-import com.liferay.portal.security.auth.verifier.module.AbstractAuthVerifierPublisher;
+import com.liferay.portal.security.auth.verifier.module.BaseAuthVerifierPublisher;
 
 import java.util.Map;
 
@@ -35,7 +35,7 @@ import org.osgi.service.component.annotations.Modified;
 	configurationPolicy = ConfigurationPolicy.REQUIRE
 )
 public class BasicAuthHeaderAuthVerifierPublisher
-	extends AbstractAuthVerifierPublisher {
+	extends BaseAuthVerifierPublisher {
 
 	@Activate
 	@Override
--- modules/portal-security/portal-security-auth-verifier/src/com/liferay/portal/security/auth/verifier/portal/session/module/PortalSessionAuthVerifierPublisher.java
+++ modules/portal-security/portal-security-auth-verifier/src/com/liferay/portal/security/auth/verifier/portal/session/module/PortalSessionAuthVerifierPublisher.java
@@ -15,7 +15,7 @@
 package com.liferay.portal.security.auth.verifier.portal.session.module;
 
 import com.liferay.portal.kernel.security.auth.verifier.AuthVerifier;
-import com.liferay.portal.security.auth.verifier.module.AbstractAuthVerifierPublisher;
+import com.liferay.portal.security.auth.verifier.module.BaseAuthVerifierPublisher;
 import com.liferay.portal.security.auth.verifier.portal.session.PortalSessionAuthVerifier;
 
 import java.util.Map;
@@ -35,7 +35,7 @@ import org.osgi.service.component.annotations.Modified;
 	configurationPolicy = ConfigurationPolicy.REQUIRE
 )
 public class PortalSessionAuthVerifierPublisher
-	extends AbstractAuthVerifierPublisher {
+	extends BaseAuthVerifierPublisher {
 
 	@Activate
 	@Override
