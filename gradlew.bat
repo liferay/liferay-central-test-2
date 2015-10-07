1a322acdf5c4ccd4fabd7106785ec30698f0ce09
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2015-10-07 14:48:02 -0700

    LRQA-18118 Regenerate Gradle wrapper

--- gradlew.bat
+++ gradlew.bat
@@ -73,7 +73,7 @@ set DEFAULT_JVM_OPTS=%DEFAULT_JVM_OPTS% -Dgradle.user.home=%APP_HOME%\.gradle
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 @rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain --init-script=%APP_HOME%\modules\init.gradle %CMD_LINE_ARGS%
 
 :end
 @rem End local scope for the variables with windows NT shell
