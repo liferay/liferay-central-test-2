e0e015f0002032cb88514954bca1e98704eff224
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2015-10-08 09:44:20 +0200

    LPS-59292 Apply the new pattern to DDM Service

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/com/liferay/dynamic/data/mapping/upgrade/DDMServiceUpgrade.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/com/liferay/dynamic/data/mapping/upgrade/DDMServiceUpgrade.java
@@ -18,58 +18,38 @@ import com.liferay.dynamic.data.mapping.upgrade.v1_0_0.UpgradeClassNames;
 import com.liferay.dynamic.data.mapping.upgrade.v1_0_0.UpgradeDynamicDataMapping;
 import com.liferay.dynamic.data.mapping.upgrade.v1_0_0.UpgradeLastPublishDate;
 import com.liferay.dynamic.data.mapping.upgrade.v1_0_0.UpgradeSchema;
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
-import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.portal.kernel.upgrade.UpgradeStep;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Marcellus Tavares
  */
-@Component(immediate = true, service = DDMServiceUpgrade.class)
-public class DDMServiceUpgrade {
+@Component(immediate = true)
+public class DDMServiceUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"com.liferay.dynamic.data.mapping.service", "0.0.1", "1.0.0",
+			Arrays.<UpgradeStep>asList(
+				new UpgradeSchema(), new UpgradeClassNames(),
+				new UpgradeDynamicDataMapping(), new UpgradeLastPublishDate()));
+	}
 
 	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
 	protected void setModuleServiceLifecycle(
 		ModuleServiceLifecycle moduleServiceLifecycle) {
 	}
 
-	@Reference(unbind = "-")
-	protected void setReleaseLocalService(
-		ReleaseLocalService releaseLocalService) {
-
-		_releaseLocalService = releaseLocalService;
-	}
-
-	@Activate
-	protected void upgrade() throws PortalException {
-		List<UpgradeProcess> upgradeProcesses = new ArrayList<>();
-
-		upgradeProcesses.add(new UpgradeSchema());
-
-		upgradeProcesses.add(new UpgradeClassNames());
-		upgradeProcesses.add(new UpgradeDynamicDataMapping());
-		upgradeProcesses.add(new UpgradeLastPublishDate());
-
-		for (UpgradeProcess upgradeProcess : upgradeProcesses) {
-			if (_log.isDebugEnabled()) {
-				_log.debug("Upgrade process " + upgradeProcess);
-			}
-		}
-	}
-
 	private static final Log _log = LogFactoryUtil.getLog(
 		DDMServiceUpgrade.class);
-
-	private ReleaseLocalService _releaseLocalService;
 
 }
