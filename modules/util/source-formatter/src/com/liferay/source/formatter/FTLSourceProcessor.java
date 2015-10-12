fdbc3a715f1e7ab5fb82f4d65f4fce124e720e0a
Hugo Huijser <hugo.huijser@liferay.com>
2015-10-12 10:54:23 +0800

    LPS-58988 Add formatting rules for .ftl files

--- modules/util/source-formatter/src/com/liferay/source/formatter/FTLSourceProcessor.java
+++ modules/util/source-formatter/src/com/liferay/source/formatter/FTLSourceProcessor.java
@@ -14,9 +14,17 @@
 
 package com.liferay.source.formatter;
 
+import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.portal.kernel.util.StringPool;
+
 import java.io.File;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.tools.ToolsUtil;
 
 /**
  * @author Hugo Huijser
@@ -33,6 +41,83 @@ public class FTLSourceProcessor extends BaseSourceProcessor {
 			File file, String fileName, String absolutePath, String content)
 		throws Exception {
 
+		Matcher matcher = _singleParameterTag.matcher(content);
+
+		while (matcher.find()) {
+			String match = matcher.group();
+
+			String replacement = match;
+
+			String group1 = matcher.group(1);
+			String group2 = matcher.group(2);
+
+			if (group2 != null) {
+				replacement = StringUtil.replaceFirst(
+					replacement, group1 + StringPool.SPACE, group1);
+			}
+
+			String group3 = matcher.group(3);
+
+			if (group3.startsWith(StringPool.SPACE)) {
+				replacement = StringUtil.replaceLast(
+					replacement, group3, group3.substring(1));
+			}
+
+			content = StringUtil.replace(content, match, replacement);
+		}
+
+		matcher = _multiParameterTag.matcher(content);
+
+		while (matcher.find()) {
+			String match = matcher.group();
+
+			if (match.contains("><")) {
+				continue;
+			}
+
+			String strippedMatch = stripQuotes(match, CharPool.QUOTE);
+
+			if (StringUtil.count(strippedMatch, StringPool.EQUAL) <= 1) {
+				continue;
+			}
+
+			String replacement = match;
+
+			String tabs = matcher.group(1);
+
+			int x = -1;
+
+			while (true) {
+				x = replacement.indexOf(
+					StringPool.EQUAL, x + tabs.length() + 2);
+
+				if (x == -1) {
+					break;
+				}
+
+				if (ToolsUtil.isInsideQuotes(replacement, x)) {
+					continue;
+				}
+
+				int y = replacement.lastIndexOf(StringPool.SPACE, x);
+
+				if (y == -1) {
+					break;
+				}
+
+				replacement =
+					replacement.substring(0, y) + StringPool.NEW_LINE + tabs +
+						StringPool.TAB + replacement.substring(y + 1);
+			}
+
+			if (!match.equals(replacement)) {
+				replacement = StringUtil.replaceLast(
+					replacement, "/>", StringPool.NEW_LINE + tabs + "/>");
+
+				content = StringUtil.replace(content, match, replacement);
+			}
+		}
+
 		return trimContent(content, false);
 	}
 
@@ -47,5 +132,9 @@ public class FTLSourceProcessor extends BaseSourceProcessor {
 	}
 
 	private static final String[] _INCLUDES = new String[] {"**/*.ftl"};
+
+	private Pattern _multiParameterTag = Pattern.compile("\n(\t*)<@.+=.+=.+/>");
+	private Pattern _singleParameterTag = Pattern.compile(
+		"(<@[\\w\\.]+ \\w+)( )?=([^=]+?)/>");
 
 }
