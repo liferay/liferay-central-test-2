1d36704ff71341b55c808c0c98538227a69dc31f
Juergen Kappler <jurgen.kappler@liferay.com>
2015-10-13 16:43:57 +0200

    LPS-59409 Apply the new upgrade pattern to Asset Tags Admin Web

--- modules/apps/asset/asset-tags-admin-web/src/com/liferay/asset/tags/admin/web/upgrade/AssetTagsAdminWebUpgrade.java
+++ modules/apps/asset/asset-tags-admin-web/src/com/liferay/asset/tags/admin/web/upgrade/AssetTagsAdminWebUpgrade.java
@@ -15,57 +15,39 @@
 package com.liferay.asset.tags.admin.web.upgrade;
 
 import com.liferay.asset.tags.admin.web.constants.AssetTagsAdminPortletKeys;
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
-import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.portal.upgrade.util.UpgradePortletId;
 
-import java.util.Collections;
-
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Eudaldo Alonso
  */
-@Component(immediate = true, service = AssetTagsAdminWebUpgrade.class)
-public class AssetTagsAdminWebUpgrade {
+@Component(immediate = true)
+public class AssetTagsAdminWebUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"com.liferay.asset.tags.admin.web", "0.0.1", "1.0.0",
+			new UpgradePortletId() {
+				@Override
+				protected String[][] getRenamePortletIdsArray() {
+					return new String[][] {
+						new String[] {
+							"99", AssetTagsAdminPortletKeys.ASSET_TAGS_ADMIN
+						}
+					};
+				}
+
+			});
+	}
 
 	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
 	protected void setModuleServiceLifecycle(
 		ModuleServiceLifecycle moduleServiceLifecycle) {
 	}
-
-	@Reference(unbind = "-")
-	protected void setReleaseLocalService(
-		ReleaseLocalService releaseLocalService) {
-
-		_releaseLocalService = releaseLocalService;
-	}
-
-	@Activate
-	protected void upgrade() throws PortalException {
-		UpgradePortletId upgradePortletId = new UpgradePortletId() {
-
-			@Override
-			protected String[][] getRenamePortletIdsArray() {
-				return new String[][] {
-					new String[] {
-						"99", AssetTagsAdminPortletKeys.ASSET_TAGS_ADMIN
-					}
-				};
-			}
-
-		};
-
-		_releaseLocalService.updateRelease(
-			"com.liferay.asset.tags.admin.web",
-			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 1,
-			false);
-	}
-
-	private ReleaseLocalService _releaseLocalService;
 
 }
