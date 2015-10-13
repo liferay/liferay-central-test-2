0c4dc21bb403658056189ce8e1ed31752a46ed4f
Brian Chan <brian.chan@liferay.com>
2015-10-13 15:46:28 -0700

    LPS-59237 SF

--- modules/apps/layout/layout-admin-web/src/com/liferay/layout/admin/web/application/list/MyPagesPanelApp.java
+++ modules/apps/layout/layout-admin-web/src/com/liferay/layout/admin/web/application/list/MyPagesPanelApp.java
@@ -18,8 +18,6 @@ import com.liferay.application.list.PanelApp;
 import com.liferay.application.list.constants.PanelCategoryKeys;
 import com.liferay.layout.admin.web.constants.LayoutAdminPortletKeys;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Portlet;
 import com.liferay.portal.model.User;
