46a5d41769de451363dbf23264e7f30c8d47aef7
Juergen Kappler <jurgen.kappler@liferay.com>
2015-10-13 16:50:47 +0200

    LPS-59409 Apply the new upgrade pattern to Asset Tags Navigation Web

--- modules/apps/asset/asset-tags-navigation-web/src/com/liferay/asset/tags/navigation/web/upgrade/AssetTagsNavigationWebUpgrade.java
+++ modules/apps/asset/asset-tags-navigation-web/src/com/liferay/asset/tags/navigation/web/upgrade/AssetTagsNavigationWebUpgrade.java
@@ -15,61 +15,45 @@
 package com.liferay.asset.tags.navigation.web.upgrade;
 
 import com.liferay.asset.tags.navigation.web.constants.AssetTagsNavigationPortletKeys;
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
-import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.portal.upgrade.util.UpgradePortletId;
 
-import java.util.Collections;
-
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Eudaldo Alonso
  */
-@Component(immediate = true, service = AssetTagsNavigationWebUpgrade.class)
-public class AssetTagsNavigationWebUpgrade {
+@Component(immediate = true)
+public class AssetTagsNavigationWebUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"com.liferay.asset.tags.navigation.web", "0.0.1", "1.0.0",
+			new UpgradePortletId() {
+				@Override
+				protected String[][] getRenamePortletIdsArray() {
+					return new String[][] {
+						new String[] {
+							"141",
+							AssetTagsNavigationPortletKeys.
+								ASSET_TAGS_NAVIGATION
+						},
+						new String[] {
+							"148",
+							AssetTagsNavigationPortletKeys.ASSET_TAGS_CLOUD
+						}
+					};
+				}
+
+			});
+	}
 
 	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
 	protected void setModuleServiceLifecycle(
 		ModuleServiceLifecycle moduleServiceLifecycle) {
 	}
-
-	@Reference(unbind = "-")
-	protected void setReleaseLocalService(
-		ReleaseLocalService releaseLocalService) {
-
-		_releaseLocalService = releaseLocalService;
-	}
-
-	@Activate
-	protected void upgrade() throws PortalException {
-		UpgradePortletId upgradePortletId = new UpgradePortletId() {
-
-			@Override
-			protected String[][] getRenamePortletIdsArray() {
-				return new String[][] {
-					new String[] {
-						"141",
-						AssetTagsNavigationPortletKeys.ASSET_TAGS_NAVIGATION
-					},
-					new String[] {
-						"148", AssetTagsNavigationPortletKeys.ASSET_TAGS_CLOUD
-					}
-				};
-			}
-
-		};
-
-		_releaseLocalService.updateRelease(
-			"com.liferay.asset.tags.navigation.web",
-			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 1,
-			false);
-	}
-
-	private ReleaseLocalService _releaseLocalService;
 
 }
