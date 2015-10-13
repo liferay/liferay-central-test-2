e32a92dfbb59c5e30dcd1e278e36ee666f135704
Juergen Kappler <jurgen.kappler@liferay.com>
2015-10-13 17:18:51 +0200

    LPS-59409 Apply the new upgrade pattern to Nested Portlets Web

--- modules/apps/nested-portlets/nested-portlets-web/src/com/liferay/nested/portlets/web/upgrade/NestedPortletWebUpgrade.java
+++ modules/apps/nested-portlets/nested-portlets-web/src/com/liferay/nested/portlets/web/upgrade/NestedPortletWebUpgrade.java
@@ -15,15 +15,10 @@
 package com.liferay.nested.portlets.web.upgrade;
 
 import com.liferay.nested.portlets.web.constants.NestedPortletsPortletKeys;
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
-import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.portal.upgrade.util.UpgradePortletId;
 
-import java.util.Collections;
-
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -31,42 +26,29 @@ import org.osgi.service.component.annotations.Reference;
  * @author Raymond Augé
  * @author Peter Fellwock
  */
-@Component(immediate = true, service = NestedPortletWebUpgrade.class)
-public class NestedPortletWebUpgrade {
+@Component(immediate = true)
+public class NestedPortletWebUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"com.liferay.nested.portlets.web", "0.0.1", "1.0.0",
+			new UpgradePortletId() {
+				@Override
+				protected String[][] getRenamePortletIdsArray() {
+					return new String[][] {
+						new String[] {
+							"118", NestedPortletsPortletKeys.NESTED_PORTLETS
+						}
+					};
+				}
+
+			});
+	}
 
 	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
 	protected void setModuleServiceLifecycle(
 		ModuleServiceLifecycle moduleServiceLifecycle) {
 	}
-
-	@Reference(unbind = "-")
-	protected void setReleaseLocalService(
-		ReleaseLocalService releaseLocalService) {
-
-		_releaseLocalService = releaseLocalService;
-	}
-
-	@Activate
-	protected void upgrade() throws PortalException {
-		UpgradePortletId upgradePortletId = new UpgradePortletId() {
-
-			@Override
-			protected String[][] getRenamePortletIdsArray() {
-				return new String[][] {
-					new String[] {
-						"118", NestedPortletsPortletKeys.NESTED_PORTLETS
-					}
-				};
-			}
-
-		};
-
-		_releaseLocalService.updateRelease(
-			"com.liferay.nested.portlets.web",
-			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 1,
-			false);
-	}
-
-	private ReleaseLocalService _releaseLocalService;
 
 }
