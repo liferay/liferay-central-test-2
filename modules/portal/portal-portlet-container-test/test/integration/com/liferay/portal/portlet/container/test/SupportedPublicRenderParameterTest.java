721d9e7ef3ce1e57c4e445a4acda0b60faca3bdc
Raymond Auge <raymond.auge@liferay.com>
2015-10-14 14:01:10 -0400

    LPS-59479 tests

---
+++ modules/portal/portal-portlet-container-test/test/integration/com/liferay/portal/portlet/container/test/SupportedPublicRenderParameterTest.java
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.portlet.container.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.TransactionalTestRule;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.model.Portlet;
+import com.liferay.portal.model.PublicRenderParameter;
+import com.liferay.portal.service.PortletLocalServiceUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Raymond Augé
+ */
+@RunWith(Arquillian.class)
+public class SupportedPublicRenderParameterTest
+	extends BasePortletContainerTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(), TransactionalTestRule.INSTANCE);
+
+	@Test
+	public void testPortalProvidedPRP() throws Exception {
+		String prpName = "categoryId";
+
+		properties.put(
+			"javax.portlet.supported-public-render-parameter",
+			prpName);
+
+		setUpPortlet(testPortlet, properties, TEST_PORTLET_ID);
+
+		Portlet portlet = PortletLocalServiceUtil.getPortletById(
+			TestPropsValues.getCompanyId(), TEST_PORTLET_ID);
+
+		Assert.assertFalse(portlet.isUndeployedPortlet());
+
+		Set<PublicRenderParameter> publicRenderParameters =
+			portlet.getPublicRenderParameters();
+
+		Assert.assertNotNull(publicRenderParameters);
+
+		boolean found = false;
+
+		for (PublicRenderParameter publicRenderParameter :
+				publicRenderParameters) {
+
+			if (prpName.equals(publicRenderParameter.getIdentifier())) {
+				found = true;
+			}
+		}
+
+		Assert.assertTrue(found);
+	}
+
+	@Test
+	public void testPortletProvidedPRP() throws Exception {
+		String prpName = "myprp";
+
+		properties.put(
+			"javax.portlet.supported-public-render-parameter",
+			prpName + ";http://some.uri.tld/space");
+
+		setUpPortlet(testPortlet, properties, TEST_PORTLET_ID);
+
+		Portlet portlet = PortletLocalServiceUtil.getPortletById(
+			TestPropsValues.getCompanyId(), TEST_PORTLET_ID);
+
+		Assert.assertFalse(portlet.isUndeployedPortlet());
+
+		Set<PublicRenderParameter> publicRenderParameters =
+			portlet.getPublicRenderParameters();
+
+		Assert.assertNotNull(publicRenderParameters);
+
+		boolean found = false;
+
+		for (PublicRenderParameter publicRenderParameter :
+				publicRenderParameters) {
+
+			if (prpName.equals(publicRenderParameter.getIdentifier())) {
+				found = true;
+			}
+		}
+
+		Assert.assertTrue(found);
+	}
+}
