59fcf403e54d894f3136a491e48830a5860a18ff
Juergen Kappler <jurgen.kappler@liferay.com>
2015-10-14 11:55:36 +0200

    LPS-59409 Apply the new upgrade pattern to Site Membership Web

--- modules/apps/site/site-memberships-web/src/com/liferay/site/memberships/web/upgrade/SiteMembershipsWebUpgrade.java
+++ modules/apps/site/site-memberships-web/src/com/liferay/site/memberships/web/upgrade/SiteMembershipsWebUpgrade.java
@@ -14,58 +14,42 @@
 
 package com.liferay.site.memberships.web.upgrade;
 
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
-import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.portal.upgrade.util.UpgradePortletId;
 import com.liferay.site.memberships.web.constants.SiteMembershipsPortletKeys;
 
-import java.util.Collections;
-
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Eudaldo Alonso
  */
-@Component(immediate = true, service = SiteMembershipsWebUpgrade.class)
-public class SiteMembershipsWebUpgrade {
+@Component(immediate = true)
+public class SiteMembershipsWebUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"com.liferay.site.memberships.web", "0.0.1", "1.0.0",
+			new UpgradePortletId() {
+				@Override
+				protected String[][] getRenamePortletIdsArray() {
+					return new String[][] {
+						new String[] {
+							"174",
+							SiteMembershipsPortletKeys.
+								SITE_MEMBERSHIPS_ADMIN
+						}
+					};
+				}
+
+			});
+	}
 
 	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
 	protected void setModuleServiceLifecycle(
 		ModuleServiceLifecycle moduleServiceLifecycle) {
 	}
-
-	@Reference(unbind = "-")
-	protected void setReleaseLocalService(
-		ReleaseLocalService releaseLocalService) {
-
-		_releaseLocalService = releaseLocalService;
-	}
-
-	@Activate
-	protected void upgrade() throws PortalException {
-		UpgradePortletId upgradePortletId = new UpgradePortletId() {
-
-			@Override
-			protected String[][] getRenamePortletIdsArray() {
-				return new String[][] {
-					new String[] {
-						"174", SiteMembershipsPortletKeys.SITE_MEMBERSHIPS_ADMIN
-					}
-				};
-			}
-
-		};
-
-		_releaseLocalService.updateRelease(
-			"com.liferay.site.memberships.web",
-			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 1,
-			false);
-	}
-
-	private ReleaseLocalService _releaseLocalService;
 
 }
