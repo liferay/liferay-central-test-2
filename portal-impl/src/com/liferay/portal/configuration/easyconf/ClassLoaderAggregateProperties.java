3e00567cf40cbce32f0087a246403cc3397d256b
Tibor Lipusz <tibor.lipusz@liferay.com>
2015-10-14 12:30:36 +0200

    LPS-59348 Allow to load empty properties files in ClassLoaderAggregateProperties

--- portal-impl/src/com/liferay/portal/configuration/easyconf/ClassLoaderAggregateProperties.java
+++ portal-impl/src/com/liferay/portal/configuration/easyconf/ClassLoaderAggregateProperties.java
@@ -64,8 +64,13 @@ public class ClassLoaderAggregateProperties extends AggregatedProperties {
 		Configuration configuration = _addPropertiesSource(
 			fileName, url, _baseCompositeConfiguration);
 
-		if ((configuration != null) && !configuration.isEmpty()) {
+		if (configuration != null) {
 			_baseConfigurationLoaded = true;
+
+			if (configuration.isEmpty() && _log.isDebugEnabled()) {
+				_log.debug(
+					"Base configuration source " + fileName + " is empty");
+			}
 		}
 	}
 
