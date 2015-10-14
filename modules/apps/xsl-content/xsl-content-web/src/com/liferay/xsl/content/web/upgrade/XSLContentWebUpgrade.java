cde521bac8092785c146d80e856b1468efb298ff
Juergen Kappler <jurgen.kappler@liferay.com>
2015-10-14 12:18:16 +0200

    LPS-59409 Apply the new upgrade pattern to XSL Web

--- modules/apps/xsl-content/xsl-content-web/src/com/liferay/xsl/content/web/upgrade/XSLContentWebUpgrade.java
+++ modules/apps/xsl-content/xsl-content-web/src/com/liferay/xsl/content/web/upgrade/XSLContentWebUpgrade.java
@@ -14,56 +14,40 @@
 
 package com.liferay.xsl.content.web.upgrade;
 
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
-import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.portal.upgrade.util.UpgradePortletId;
 import com.liferay.xsl.content.web.constants.XSLContentPortletKeys;
 
-import java.util.Collections;
-
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Raymond Augé
  */
-@Component(immediate = true, service = XSLContentWebUpgrade.class)
-public class XSLContentWebUpgrade {
+@Component(immediate = true)
+public class XSLContentWebUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"com.liferay.xsl.content.web", "0.0.1", "1.0.0",
+			new UpgradePortletId() {
+				@Override
+				protected String[][] getRenamePortletIdsArray() {
+					return new String[][] {
+						new String[] {
+							"102", XSLContentPortletKeys.XSL_CONTENT
+						}
+					};
+				}
+
+			});
+	}
 
 	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
 	protected void setModuleServiceLifecycle(
 		ModuleServiceLifecycle moduleServiceLifecycle) {
 	}
-
-	@Reference(unbind = "-")
-	protected void setReleaseLocalService(
-		ReleaseLocalService releaseLocalService) {
-
-		_releaseLocalService = releaseLocalService;
-	}
-
-	@Activate
-	protected void upgrade() throws PortalException {
-		UpgradePortletId upgradePortletId = new UpgradePortletId() {
-
-			@Override
-			protected String[][] getRenamePortletIdsArray() {
-				return new String[][] {
-					new String[] {"102", XSLContentPortletKeys.XSL_CONTENT}
-				};
-			}
-
-		};
-
-		_releaseLocalService.updateRelease(
-			"com.liferay.xsl.content.web",
-			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 1,
-			false);
-	}
-
-	private ReleaseLocalService _releaseLocalService;
 
 }
