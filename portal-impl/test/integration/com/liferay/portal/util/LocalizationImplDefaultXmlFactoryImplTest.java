5118a410e552ebf0d65175d81659144e52c62b3d
Eric Yan <eric.yan@liferay.com>
2015-10-15 14:12:46 -0700

    LPS-59248 - Integration Test

---
+++ portal-impl/test/integration/com/liferay/portal/util/LocalizationImplDefaultXmlFactoryImplTest.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.util;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * @author Eric Yan
+ */
+public class LocalizationImplDefaultXmlFactoryImplTest
+	extends LocalizationImplTest {
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_inputFactoryProperty = System.getProperty(
+			XMLInputFactory.class.getName());
+		_outputFactoryProperty = System.getProperty(
+			XMLOutputFactory.class.getName());
+
+		System.setProperty(
+			XMLInputFactory.class.getName(),
+			"com.sun.xml.internal.stream.XMLInputFactoryImpl");
+		System.setProperty(
+			XMLOutputFactory.class.getName(),
+			"com.sun.xml.internal.stream.XMLOutputFactoryImpl");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		if (_inputFactoryProperty == null) {
+			System.clearProperty(XMLInputFactory.class.getName());
+		}
+		else {
+			System.setProperty(
+				XMLInputFactory.class.getName(), _inputFactoryProperty);
+		}
+
+		if (_outputFactoryProperty == null) {
+			System.clearProperty(XMLOutputFactory.class.getName());
+		}
+		else {
+			System.setProperty(
+				XMLOutputFactory.class.getName(), _outputFactoryProperty);
+		}
+	}
+
+	private String _inputFactoryProperty;
+	private String _outputFactoryProperty;
+
+}
