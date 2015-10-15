c5fc9c949e1457f89d0ce67d3e81ab5e08182ab7
Julio Camarero <julio.camarero@liferay.com>
2015-10-15 12:10:22 +0200

    LPS-53737 autogenerated

--- portal-service/src/com/liferay/portlet/asset/service/persistence/AssetTagFinder.java
+++ portal-service/src/com/liferay/portlet/asset/service/persistence/AssetTagFinder.java
@@ -22,25 +22,17 @@ import aQute.bnd.annotation.ProviderType;
  */
 @ProviderType
 public interface AssetTagFinder {
+	public java.util.List<com.liferay.portlet.asset.model.AssetTag> findByG_N_S_E(
+		long groupId, java.lang.String name, int startPeriod, int endPeriod,
+		int periodLength);
+
+	public int countByG_N(long groupId, java.lang.String name);
+
 	public int countByG_C_N(long groupId, long classNameId,
 		java.lang.String name);
-
-	public int filterCountByG_N(long groupId, java.lang.String name);
-
-	public int filterCountByG_C_N(long groupId, long classNameId,
-		java.lang.String name);
-
-	public java.util.List<com.liferay.portlet.asset.model.AssetTag> filterFindByG_C_N(
-		long groupId, long classNameId, java.lang.String name, int start,
-		int end,
-		com.liferay.portal.kernel.util.OrderByComparator<com.liferay.portlet.asset.model.AssetTag> obc);
 
 	public java.util.List<com.liferay.portlet.asset.model.AssetTag> findByG_C_N(
 		long groupId, long classNameId, java.lang.String name, int start,
 		int end,
 		com.liferay.portal.kernel.util.OrderByComparator<com.liferay.portlet.asset.model.AssetTag> obc);
-
-	public java.util.List<com.liferay.portlet.asset.model.AssetTag> findByG_N_S_E(
-		long groupId, java.lang.String name, int startPeriod, int endPeriod,
-		int periodLength);
 }
