8fb65668f06698d36ab0d003ff92af5546ea8958
Juergen Kappler <jurgen.kappler@liferay.com>
2015-10-15 11:24:45 +0200

    LPS-59409 Apply the new upgrade pattern to Site Navigation Breadcrumb Web

--- modules/apps/site-navigation/site-navigation-breadcrumb-web/src/com/liferay/site/navigation/breadcrumb/web/upgrade/SiteNavigationBreadcrumbWebUpgrade.java
+++ modules/apps/site-navigation/site-navigation-breadcrumb-web/src/com/liferay/site/navigation/breadcrumb/web/upgrade/SiteNavigationBreadcrumbWebUpgrade.java
@@ -14,51 +14,31 @@
 
 package com.liferay.site.navigation.breadcrumb.web.upgrade;
 
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
-import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.portal.upgrade.util.UpgradePortletId;
 import com.liferay.site.navigation.breadcrumb.web.upgrade.v1_0_0.UpgradePortletPreferences;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Eudaldo Alonso
  */
-@Component(immediate = true, service = SiteNavigationBreadcrumbWebUpgrade.class)
-public class SiteNavigationBreadcrumbWebUpgrade {
+@Component(immediate = true)
+public class SiteNavigationBreadcrumbWebUpgrade
+	implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"com.liferay.site.navigation.breadcrumb.web", "0.0.1", "1.0.0",
+			new UpgradePortletId(), new UpgradePortletPreferences());
+	}
 
 	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
 	protected void setModuleServiceLifecycle(
 		ModuleServiceLifecycle moduleServiceLifecycle) {
 	}
-
-	@Reference(unbind = "-")
-	protected void setReleaseLocalService(
-		ReleaseLocalService releaseLocalService) {
-
-		_releaseLocalService = releaseLocalService;
-	}
-
-	@Activate
-	protected void upgrade() throws PortalException {
-		List<UpgradeProcess> upgradeProcesses = new ArrayList<>();
-
-		upgradeProcesses.add(new UpgradePortletId());
-
-		upgradeProcesses.add(new UpgradePortletPreferences());
-
-		_releaseLocalService.updateRelease(
-			"com.liferay.site.navigation.breadcrumb.web", upgradeProcesses, 1,
-			1, false);
-	}
-
-	private ReleaseLocalService _releaseLocalService;
 
 }
