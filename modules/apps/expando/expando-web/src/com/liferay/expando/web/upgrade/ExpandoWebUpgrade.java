1030f3dd4e1361c1f8ff4e3af6673cd18ae5af84
drewbrokke <drew.brokke@liferay.com>
2015-10-15 10:13:30 -0700

    LPS-59413 applies new upgrade pattern to Expando

--- modules/apps/expando/expando-web/src/com/liferay/expando/web/upgrade/ExpandoWebUpgrade.java
+++ modules/apps/expando/expando-web/src/com/liferay/expando/web/upgrade/ExpandoWebUpgrade.java
@@ -14,16 +14,10 @@
 
 package com.liferay.expando.web.upgrade;
 
-import com.liferay.expando.web.constants.ExpandoPortletKeys;
-import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.expando.web.upgrade.v1_0_0.UpgradePortletId;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
-import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.service.ReleaseLocalService;
-import com.liferay.portal.upgrade.util.UpgradePortletId;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
-import java.util.Collections;
-
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -31,42 +25,19 @@ import org.osgi.service.component.annotations.Reference;
  * @author Eudaldo Alonso
  * @author Drew Brokke
  */
-@Component(immediate = true, service = ExpandoWebUpgrade.class)
-public class ExpandoWebUpgrade {
+@Component(immediate = true)
+public class ExpandoWebUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"com.liferay.expando.web", "0.0.1", "1.0.0",
+			new UpgradePortletId());
+	}
 
 	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
 	protected void setModuleServiceLifecycle(
 		ModuleServiceLifecycle moduleServiceLifecycle) {
 	}
-
-	@Reference(unbind = "-")
-	protected void setReleaseLocalService(
-		ReleaseLocalService releaseLocalService) {
-
-		_releaseLocalService = releaseLocalService;
-	}
-
-	@Activate
-	protected void upgrade() throws PortalException {
-		UpgradePortletId upgradePortletId = new UpgradePortletId() {
-
-			@Override
-			protected String[][] getRenamePortletIdsArray() {
-				return new String[][] {
-					new String[] {
-						"139", ExpandoPortletKeys.EXPANDO
-					}
-				};
-			}
-
-		};
-
-		_releaseLocalService.updateRelease(
-			"com.liferay.expando.web",
-			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 1,
-			false);
-	}
-
-	private ReleaseLocalService _releaseLocalService;
 
 }
