d2ed1aafc6afa1c5b9193f25bc5fa06c38db5457
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-10-16 13:48:19 +0200

    LPS-59547 Apply new upgrade pattern to Polls portlet

--- modules/apps/polls/polls-service/src/com/liferay/polls/upgrade/PollsServiceUpgrade.java
+++ modules/apps/polls/polls-service/src/com/liferay/polls/upgrade/PollsServiceUpgrade.java
@@ -16,58 +16,35 @@ package com.liferay.polls.upgrade;
 
 import com.liferay.polls.upgrade.v1_0_0.UpgradeClassNames;
 import com.liferay.polls.upgrade.v1_0_0.UpgradeLastPublishDate;
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
-import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.portal.upgrade.util.UpgradePortletId;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Miguel Pastor
  */
-@Component(immediate = true, service = PollsServiceUpgrade.class)
-public class PollsServiceUpgrade {
+@Component(immediate = true)
+public class PollsServiceUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"com.liferay.polls.service", "0.0.1", "1.0.0",
+			new UpgradePortletId(), new UpgradeClassNames(),
+			new UpgradeLastPublishDate());
+	}
 
 	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
 	protected void setModuleServiceLifecycle(
 		ModuleServiceLifecycle moduleServiceLifecycle) {
 	}
 
-	@Reference(unbind = "-")
-	protected void setReleaseLocalService(
-		ReleaseLocalService releaseLocalService) {
-
-		_releaseLocalService = releaseLocalService;
-	}
-
-	@Activate
-	protected void upgrade() throws PortalException {
-		List<UpgradeProcess> upgradeProcesses = new ArrayList<>();
-
-		upgradeProcesses.add(new UpgradePortletId());
-
-		upgradeProcesses.add(new UpgradeClassNames());
-		upgradeProcesses.add(new UpgradeLastPublishDate());
-
-		for (UpgradeProcess upgradeProcess : upgradeProcesses) {
-			if (_log.isDebugEnabled()) {
-				_log.debug("Upgrade process " + upgradeProcess);
-			}
-		}
-	}
-
 	private static final Log _log = LogFactoryUtil.getLog(
 		PollsServiceUpgrade.class);
-
-	private ReleaseLocalService _releaseLocalService;
 
 }
