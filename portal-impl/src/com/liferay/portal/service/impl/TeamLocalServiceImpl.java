ead97b38509b49fdaeb2b95c870189dce653eda1
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2015-10-16 22:59:47 +0200

    LPS-59562 Clear permissions cache when team assignment is changed

--- portal-impl/src/com/liferay/portal/service/impl/TeamLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/TeamLocalServiceImpl.java
@@ -26,6 +26,7 @@ import com.liferay.portal.model.Role;
 import com.liferay.portal.model.RoleConstants;
 import com.liferay.portal.model.Team;
 import com.liferay.portal.model.User;
+import com.liferay.portal.security.permission.PermissionCacheUtil;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.service.base.TeamLocalServiceBaseImpl;
 
@@ -94,6 +95,66 @@ public class TeamLocalServiceImpl extends TeamLocalServiceBaseImpl {
 	}
 
 	@Override
+	public void addUserGroupTeam(long userGroupId, long teamId) {
+		super.addUserGroupTeam(userGroupId, teamId);
+		PermissionCacheUtil.clearCache();
+	}
+
+	@Override
+	public void addUserGroupTeam(long userGroupId, Team team) {
+		super.addUserGroupTeam(userGroupId, team);
+		PermissionCacheUtil.clearCache();
+	}
+
+	@Override
+	public void addUserGroupTeams(long userGroupId, List<Team> Teams) {
+		super.addUserGroupTeams(userGroupId, Teams);
+		PermissionCacheUtil.clearCache();
+	}
+
+	@Override
+	public void addUserGroupTeams(long userGroupId, long[] teamIds) {
+		super.addUserGroupTeams(userGroupId, teamIds);
+		PermissionCacheUtil.clearCache();
+	}
+
+	@Override
+	public void addUserTeam(long userId, long teamId) {
+		super.addUserTeam(userId, teamId);
+		PermissionCacheUtil.clearCache(userId);
+	}
+
+	@Override
+	public void addUserTeam(long userId, Team team) {
+		super.addUserTeam(userId, team);
+		PermissionCacheUtil.clearCache(userId);
+	}
+
+	@Override
+	public void addUserTeams(long userId, List<Team> Teams) {
+		super.addUserTeams(userId, Teams);
+		PermissionCacheUtil.clearCache(userId);
+	}
+
+	@Override
+	public void addUserTeams(long userId, long[] teamIds) {
+		super.addUserTeams(userId, teamIds);
+		PermissionCacheUtil.clearCache(userId);
+	}
+
+	@Override
+	public void clearUserGroupTeams(long userGroupId) {
+		super.clearUserGroupTeams(userGroupId);
+		PermissionCacheUtil.clearCache();
+	}
+
+	@Override
+	public void clearUserTeams(long userId) {
+		super.clearUserTeams(userId);
+		PermissionCacheUtil.clearCache(userId);
+	}
+
+	@Override
 	public Team deleteTeam(long teamId) throws PortalException {
 		Team team = teamPersistence.findByPrimaryKey(teamId);
 
@@ -129,6 +190,54 @@ public class TeamLocalServiceImpl extends TeamLocalServiceBaseImpl {
 		for (Team team : teams) {
 			deleteTeam(team.getTeamId());
 		}
+	}
+
+	@Override
+	public void deleteUserGroupTeam(long userGroupId, long teamId) {
+		super.deleteUserGroupTeam(userGroupId, teamId);
+		PermissionCacheUtil.clearCache();
+	}
+
+	@Override
+	public void deleteUserGroupTeam(long userGroupId, Team team) {
+		super.deleteUserGroupTeam(userGroupId, team);
+		PermissionCacheUtil.clearCache();
+	}
+
+	@Override
+	public void deleteUserGroupTeams(long userGroupId, List<Team> Teams) {
+		super.deleteUserGroupTeams(userGroupId, Teams);
+		PermissionCacheUtil.clearCache();
+	}
+
+	@Override
+	public void deleteUserGroupTeams(long userGroupId, long[] teamIds) {
+		super.deleteUserGroupTeams(userGroupId, teamIds);
+		PermissionCacheUtil.clearCache();
+	}
+
+	@Override
+	public void deleteUserTeam(long userId, long teamId) {
+		super.deleteUserTeam(userId, teamId);
+		PermissionCacheUtil.clearCache(userId);
+	}
+
+	@Override
+	public void deleteUserTeam(long userId, Team team) {
+		super.deleteUserTeam(userId, team);
+		PermissionCacheUtil.clearCache(userId);
+	}
+
+	@Override
+	public void deleteUserTeams(long userId, List<Team> Teams) {
+		super.deleteUserTeams(userId, Teams);
+		PermissionCacheUtil.clearCache(userId);
+	}
+
+	@Override
+	public void deleteUserTeams(long userId, long[] teamIds) {
+		super.deleteUserTeams(userId, teamIds);
+		PermissionCacheUtil.clearCache(userId);
 	}
 
 	@Override
