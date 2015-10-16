16cfa97c750a517f76a244d094e9cbced32b189f
Pei-Jung Lan <pei-jung.lan@liferay.com>
2015-10-16 09:17:42 -0700

    LPS-59413 Apply new upgrade pattern to Portal Settings Web

--- modules/apps/portal-settings/portal-settings-web/src/com/liferay/portal/settings/web/upgrade/PortalSettingWebUpgrade.java
+++ modules/apps/portal-settings/portal-settings-web/src/com/liferay/portal/settings/web/upgrade/PortalSettingWebUpgrade.java
@@ -14,58 +14,29 @@
 
 package com.liferay.portal.settings.web.upgrade;
 
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
-import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.service.ReleaseLocalService;
-import com.liferay.portal.settings.web.constants.PortalSettingsPortletKeys;
-import com.liferay.portal.upgrade.util.UpgradePortletId;
+import com.liferay.portal.settings.web.upgrade.v1_0_0.UpgradePortletId;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
-import java.util.Collections;
-
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Philip Jones
  */
-@Component(immediate = true, service = PortalSettingWebUpgrade.class)
-public class PortalSettingWebUpgrade {
+@Component(immediate = true)
+public class PortalSettingWebUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"com.liferay.portal.settings.web", "0.0.1", "1.0.0",
+			new UpgradePortletId());
+	}
 
 	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
 	protected void setModuleServiceLifecycle(
 		ModuleServiceLifecycle moduleServiceLifecycle) {
 	}
-
-	@Reference(unbind = "-")
-	protected void setReleaseLocalService(
-		ReleaseLocalService releaseLocalService) {
-
-		_releaseLocalService = releaseLocalService;
-	}
-
-	@Activate
-	protected void upgrade() throws PortalException {
-		UpgradePortletId upgradePortletId = new UpgradePortletId() {
-
-			@Override
-			protected String[][] getRenamePortletIdsArray() {
-				return new String[][] {
-					new String[] {
-						"130", PortalSettingsPortletKeys.PORTAL_SETTINGS
-					}
-				};
-			}
-
-		};
-
-		_releaseLocalService.updateRelease(
-			"com.liferay.portal.settings.web",
-			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 1,
-			false);
-	}
-
-	private ReleaseLocalService _releaseLocalService;
 
 }
