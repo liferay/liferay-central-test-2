9ac66211d448a6b75c883bcb7c06ad25cd393317
Pei-Jung Lan <pei-jung.lan@liferay.com>
2015-10-16 12:28:21 -0700

    LPS-59413 Apply new upgrade pattern to Server Admin Web

--- modules/apps/server-admin/server-admin-web/src/com/liferay/server/admin/web/upgrade/ServerAdminWebUpgrade.java
+++ modules/apps/server-admin/server-admin-web/src/com/liferay/server/admin/web/upgrade/ServerAdminWebUpgrade.java
@@ -14,57 +14,30 @@
 
 package com.liferay.server.admin.web.upgrade;
 
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.service.ReleaseLocalService;
-import com.liferay.portal.upgrade.util.UpgradePortletId;
-import com.liferay.portal.util.PortletKeys;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.server.admin.web.upgrade.v1_0_0.UpgradePortletId;
 
-import java.util.Collections;
-
-import javax.servlet.ServletContext;
-
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Philip Jones
+ * @author Pei-Jung Lan
  */
-@Component(immediate = true, service = ServerAdminWebUpgrade.class)
-public class ServerAdminWebUpgrade {
+@Component(immediate = true)
+public class ServerAdminWebUpgrade implements UpgradeStepRegistrator {
 
-	@Reference(unbind = "-")
-	protected void setReleaseLocalService(
-		ReleaseLocalService releaseLocalService) {
-
-		_releaseLocalService = releaseLocalService;
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"com.liferay.server.admin.web", "0.0.1", "1.0.0",
+			new UpgradePortletId());
 	}
 
-	@Reference(target = "(original.bean=*)", unbind = "-")
-	protected void setServletContext(ServletContext servletContext) {
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
+	protected void setModuleServiceLifecycle(
+		ModuleServiceLifecycle moduleServiceLifecycle) {
 	}
-
-	@Activate
-	protected void upgrade() throws PortalException {
-		UpgradePortletId upgradePortletId = new UpgradePortletId() {
-
-			@Override
-			protected String[][] getRenamePortletIdsArray() {
-				return new String[][] {
-					new String[] {
-						"137", PortletKeys.SERVER_ADMIN
-					}
-				};
-			}
-		};
-
-		_releaseLocalService.updateRelease(
-			"com.liferay.server.admin.web",
-			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 1,
-			false);
-	}
-
-	private ReleaseLocalService _releaseLocalService;
 
 }
