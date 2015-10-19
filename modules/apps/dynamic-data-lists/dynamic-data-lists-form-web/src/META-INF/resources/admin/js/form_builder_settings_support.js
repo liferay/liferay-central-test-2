8e5582015417ce06229c0d0932af49c4d003e9c5
Bruno Basto <bruno.basto@liferay.com>
2015-10-19 17:50:00 -0300

    LPS-59604 Fields in Form Builder should trigger validation or visibility computation

--- modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/META-INF/resources/admin/js/form_builder_settings_support.js
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/META-INF/resources/admin/js/form_builder_settings_support.js
@@ -1,6 +1,8 @@
 AUI.add(
 	'liferay-ddl-form-builder-field-support',
 	function(A) {
+		var Lang = A.Lang;
+
 		var FieldTypes = Liferay.DDM.Renderer.FieldTypes;
 
 		var CSS_FIELD = A.getClassName('form', 'builder', 'field');
@@ -115,6 +117,10 @@ AUI.add(
 					}
 				);
 			},
+
+			syncVisibility: Lang.emptyFn,
+
+			validate: Lang.emptyFn,
 
 			validateSettings: function(callback) {
 				var instance = this;
