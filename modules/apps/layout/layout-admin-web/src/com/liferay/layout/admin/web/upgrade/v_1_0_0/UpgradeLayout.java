609fa84ba773c0562f16b3a6c523ef9a4c8ea0c2
Juergen Kappler <jurgen.kappler@liferay.com>
2015-10-19 16:49:39 +0200

    LPS-59566 Create upgrade process for the embedded layout URL

---
+++ modules/apps/layout/layout-admin-web/src/com/liferay/layout/admin/web/upgrade/v_1_0_0/UpgradeLayout.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.admin.web.upgrade.v_1_0_0;
+
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.LayoutConstants;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class UpgradeLayout extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = DataAccess.getUpgradeOptimizedConnection();
+
+			ps = con.prepareStatement(
+				"select plid, typeSettings from Layout where type_ = ?");
+
+			ps.setString(1, LayoutConstants.TYPE_EMBEDDED);
+
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				long plid = rs.getLong("plid");
+				String typeSettings = rs.getString("typeSettings");
+
+				updateLayout(plid, typeSettings);
+			}
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+	}
+
+	protected void updateLayout(long plid, String typeSettings)
+		throws Exception {
+
+		if (Validator.isNull(typeSettings)) {
+			return;
+		}
+
+		UnicodeProperties typeSettingsProperties = new UnicodeProperties(true);
+
+		typeSettingsProperties.load(typeSettings);
+
+		String url = typeSettingsProperties.getProperty("url");
+
+		typeSettingsProperties.setProperty("embeddedLayoutURL", url);
+
+		typeSettingsProperties.remove("url");
+
+		updateTypeSettings(plid, typeSettingsProperties.toString());
+	}
+
+	protected void updateTypeSettings(long plid, String typeSettings)
+		throws Exception {
+
+		Connection con = null;
+		PreparedStatement ps = null;
+
+		try {
+			con = DataAccess.getUpgradeOptimizedConnection();
+
+			ps = con.prepareStatement(
+				"update Layout set typeSettings = ? where plid = ?");
+
+			ps.setString(1, typeSettings);
+			ps.setLong(2, plid);
+
+			ps.executeUpdate();
+		}
+		finally {
+			DataAccess.cleanUp(con, ps);
+		}
+	}
+
+}
