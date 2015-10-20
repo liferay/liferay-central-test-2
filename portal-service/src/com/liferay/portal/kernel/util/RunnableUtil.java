2102c27aa86900064b58c0dea465eedcd30fd21c
shuyangzhou <shuyang.zhou@liferay.com>
2015-10-20 16:36:42 -0700

    LPS-58134 Fix RunnableUtil.runWithSwappedSystemOut() thread safety issue. System.out is a global resource, we have no control over who will use it or when it will be used. As a reasult, the previously implementation has a major thread safety issue, if any other thread prints into System.out while the current thread has swapped it, the end result is unpredictable. This fix is not complete either, due to there is no way to atomicly swap System.out field, to ensure no concurrent swapping, I have to synchronized on RunnableUtil.class, but that will only help if every swap is triggered by RunnableUtil.runWithSwappedSystemOut() if any other code in the system is doing something similar thing, we will see concurrent swap race condition again. Swapping System.out/System.err is very much anti-production, we should avoid doing this at all cost. At this moment I am merely doing damage control, later we should revisit this to eliminate this requirement from root completely.

--- portal-service/src/com/liferay/portal/kernel/util/RunnableUtil.java
+++ portal-service/src/com/liferay/portal/kernel/util/RunnableUtil.java
@@ -14,6 +14,9 @@
 
 package com.liferay.portal.kernel.util;
 
+import com.liferay.portal.kernel.io.unsync.UnsyncFilterOutputStream;
+
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
@@ -25,16 +28,68 @@ public class RunnableUtil {
 	public static void runWithSwappedSystemOut(
 		Runnable runnable, OutputStream outputStream) {
 
-		PrintStream printStream = System.out;
+		PrintStream printStream = null;
 
-		System.setOut(new PrintStream(outputStream));
+		synchronized (RunnableUtil.class) {
+			printStream = System.out;
+
+			System.setOut(
+				new PrintStream(
+					new SwappedOutputStream(
+						outputStream, printStream, Thread.currentThread())));
+		}
 
 		try {
 			runnable.run();
 		}
 		finally {
-			System.setOut(printStream);
+			synchronized (RunnableUtil.class) {
+				System.setOut(printStream);
+			}
 		}
+	}
+
+	private static class SwappedOutputStream extends UnsyncFilterOutputStream {
+
+		@Override
+		public void write(byte[] bytes, int offset, int length)
+			throws IOException {
+
+			Thread thread = Thread.currentThread();
+
+			if (thread == _thread) {
+				super.write(bytes, offset, length);
+			}
+			else {
+				_fallbackOutputStream.write(bytes, offset, length);
+			}
+		}
+
+		@Override
+		public void write(int b) throws IOException {
+			Thread thread = Thread.currentThread();
+
+			if (thread == _thread) {
+				super.write(b);
+			}
+			else {
+				_fallbackOutputStream.write(b);
+			}
+		}
+
+		private SwappedOutputStream(
+			OutputStream outputStream, OutputStream fallbackOutputStream,
+			Thread thread) {
+
+			super(outputStream);
+
+			_fallbackOutputStream = fallbackOutputStream;
+			_thread = thread;
+		}
+
+		private final OutputStream _fallbackOutputStream;
+		private final Thread _thread;
+
 	}
 
 }
