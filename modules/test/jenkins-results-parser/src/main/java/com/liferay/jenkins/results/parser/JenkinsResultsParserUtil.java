e1fad1496bba5b9fbc0b6e6710e6565aac3b4f0e
Brian Chan <brian.chan@liferay.com>
2015-10-21 15:24:44 -0700

    LRQA-18684

---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/JenkinsResultsParserUtil.java
@@ -0,0 +1,170 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jenkins.results.parser;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.net.URL;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * @author Peter Yoo
+ */
+public class JenkinsResultsParserUtil {
+
+	public static String fixJSON(String json) {
+		json = json.replaceAll("\t", "&#09;");
+		json = json.replaceAll("\\\"", "&#34; ");
+		json = json.replaceAll("'", "&#39;");
+		json = json.replaceAll("\\(", "&#40;");
+		json = json.replaceAll("\\)", "&#41;");
+		json = json.replaceAll("<", "&#60;");
+		json = json.replaceAll(">", "&#62;");
+		json = json.replaceAll("\\[", "&#91;");
+		json = json.replaceAll("\\\\", "&#92;");
+		json = json.replaceAll("\\]", "&#93;");
+		json = json.replaceAll("\\{", "&#123;");
+		json = json.replaceAll("\\}", "&#125;");
+		json = json.replaceAll("\n", "<br />");
+
+		return json;
+	}
+
+	public static String fixURL(String url) {
+		if (url.contains("(")) {
+			url = url.replace("(", "%28");
+		}
+
+		if (url.contains(")")) {
+			url = url.replace(")", "%29");
+		}
+
+		if (url.contains("[")) {
+			url = url.replace("[", "%5B");
+		}
+
+		if (url.contains("]")) {
+			url = url.replace("]", "%5D");
+		}
+
+		return url;
+	}
+
+	public static String getJobVariant(JSONObject jsonObject) throws Exception {
+		JSONArray actionsJSONArray = jsonObject.getJSONArray("actions");
+
+		for (int i = 0; i < actionsJSONArray.length(); i++) {
+			Object object = actionsJSONArray.get(i);
+
+			if (object.equals(org.json.JSONObject.NULL)) {
+				continue;
+			}
+
+			JSONObject actionsJSONObject = actionsJSONArray.getJSONObject(
+				i);
+
+			if (actionsJSONObject.has("parameters")) {
+				JSONArray parametersJSONArray =
+					actionsJSONObject.getJSONArray("parameters");
+
+				for (int j = 0; j < parametersJSONArray.length(); j++) {
+					JSONObject parametersJSONObject =
+						parametersJSONArray.getJSONObject(j);
+
+					if ("JOB_VARIANT".contains(
+							parametersJSONObject.getString("name"))) {
+
+						return parametersJSONObject.getString("value");
+					}
+				}
+			}
+		}
+
+		return "";
+	}
+
+	public static String getLocalURL(String remoteURL) {
+		Matcher matcher = _localURLPattern1.matcher(remoteURL);
+
+		if (matcher.find()) {
+			StringBuilder sb = new StringBuilder();
+
+			sb.append("http://test-");
+			sb.append(matcher.group(1));
+			sb.append("/");
+			sb.append(matcher.group(1));
+			sb.append("/");
+
+			return remoteURL.replaceAll(matcher.group(0), sb.toString());
+		}
+
+		matcher = _localURLPattern2.matcher(remoteURL);
+
+		if (matcher.find()) {
+			StringBuilder sb = new StringBuilder();
+
+			sb.append("http://");
+			sb.append(matcher.group(1));
+			sb.append("/");
+
+			return remoteURL.replaceAll(matcher.group(0), sb.toString());
+		}
+
+		return remoteURL;
+	}
+
+	public static JSONObject toJSONObject(String url) throws Exception {
+		return new JSONObject(toString(fixURL(url)));
+	}
+
+	public static String toString(String url) throws IOException {
+		StringBuilder sb = new StringBuilder();
+
+		URL urlObject = new URL(fixURL(url));
+
+		InputStreamReader inputStreamReader = new InputStreamReader(
+			urlObject.openStream());
+
+		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+		String line = null;
+
+		while ((line = bufferedReader.readLine()) != null) {
+			sb.append(line);
+			sb.append("\n");
+		}
+
+		bufferedReader.close();
+
+		return sb.toString();
+	}
+
+	private static final Pattern _localURLPattern1 = Pattern.compile(
+		"https://test.liferay.com/([0-9]+)/");
+	private static final Pattern _localURLPattern2 = Pattern.compile(
+		"https://(test-[0-9]+-[0-9]+).liferay.com/");
+
+}
