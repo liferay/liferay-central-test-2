9f9b8dd92ba4e57e49a061155f5ab9b93e5aca27
Hugo Huijser <hugo.huijser@liferay.com>
2015-10-21 11:49:36 +0800

    LPS-53819 Fix incorrect tabbing on multiline catch statements

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/JavaSourceProcessor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/JavaSourceProcessor.java
@@ -1336,9 +1336,9 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 				strippedQuotesLine, StringPool.OPEN_PARENTHESIS);
 
 			if ((previousLineLength > 0) &&
-				(line.endsWith("||") || line.endsWith("&&") ||
+				(line.endsWith("|") || line.endsWith("&&") ||
 				 line.endsWith(") {")) &&
-				(previousLine.endsWith("||") || previousLine.endsWith("&&")) &&
+				(previousLine.endsWith("|") || previousLine.endsWith("&&")) &&
 				(previousLineLength + trimmedLine.length() <
 					_MAX_LINE_LENGTH) &&
 				(openParenthesesCount <= closeParenthesesCount) &&
@@ -1498,7 +1498,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 
 					if (line.contains(StringPool.TAB + StringPool.SPACE) &&
 						!previousLine.endsWith("&&") &&
-						!previousLine.endsWith("||") &&
+						!previousLine.endsWith("|") &&
 						!previousLine.contains(StringPool.TAB + "((") &&
 						!previousLine.contains(
 							StringPool.TAB + StringPool.LESS_THAN) &&
@@ -1914,7 +1914,8 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 						fileName, "{:" + fileName + " " + lineCount);
 				}
 
-				if (trimmedLine.startsWith("if (") ||
+				if (trimmedLine.startsWith("catch (") ||
+					trimmedLine.startsWith("if (") ||
 					trimmedLine.startsWith("else if (") ||
 					trimmedLine.startsWith("while (") ||
 					Validator.isNotNull(ifClause)) {
