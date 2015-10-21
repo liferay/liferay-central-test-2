9190246c10ce1e348210f30fd3611703be6b8184
Bruno Basto <bruno.basto@liferay.com>
2015-10-21 18:15:03 -0300

    LPS-59706 Regen

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-validation/src/META-INF/resources/validation.soy.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-validation/src/META-INF/resources/validation.soy.js
@@ -10,19 +10,19 @@ return '\t<option ' + soy.$$escapeHtml(opt_data.option.status) + ' value="' + so
 
 
 ddm.validation = function(opt_data, opt_ignored) {
-var output = '\t<link href="/o/ddm-type-validation/validation.css" rel="stylesheet"></link><div class="form-group' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + ' lfr-ddm-form-field-validation" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '"><label class="control-label">' + soy.$$escapeHtml(opt_data.label) + ((opt_data.required) ? '<b>*</b>' : '') + '</label><div class="form-group"><label class="control-label" for="' + soy.$$escapeHtml(opt_data.name) + 'EnableValidation"><input class="enable-validation toggle-switch" ' + soy.$$escapeHtml(opt_data.enableValidationValue ? 'checked="checked"' : '') + ' id="' + soy.$$escapeHtml(opt_data.name) + 'EnableValidation" type="checkbox" value="true" /><span aria-hidden="true" class="toggle-switch-bar"><span class="toggle-switch-handle" data-label-off="' + soy.$$escapeHtml(opt_data.enableValidationMessage) + '" data-label-on="' + soy.$$escapeHtml(opt_data.enableValidationMessage) + '"></span></span></label></div><div class="' + soy.$$escapeHtml(opt_data.enableValidationValue ? '' : 'hide') + ' row"><div class="col-md-6"><select class="form-control types-select">';
-var optionList33 = opt_data.typesOptions;
+var output = '\t<link href="/o/ddm-type-validation/validation.css" rel="stylesheet"></link><div class="form-group' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + ' lfr-ddm-form-field-validation" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '"><div class="form-group"><label class="control-label" for="' + soy.$$escapeHtml(opt_data.name) + 'EnableValidation"><input class="enable-validation toggle-switch" ' + soy.$$escapeHtml(opt_data.enableValidationValue ? 'checked="checked"' : '') + ' id="' + soy.$$escapeHtml(opt_data.name) + 'EnableValidation" type="checkbox" value="true" /><span aria-hidden="true" class="toggle-switch-bar"><span class="toggle-switch-handle" data-label-off="' + soy.$$escapeHtml(opt_data.enableValidationMessage) + '" data-label-on="' + soy.$$escapeHtml(opt_data.disableValidationMessage) + '"></span></span></label></div><div class="' + soy.$$escapeHtml(opt_data.enableValidationValue ? '' : 'hide') + ' row"><div class="col-md-6"><select class="form-control types-select">';
+var optionList28 = opt_data.typesOptions;
+var optionListLen28 = optionList28.length;
+for (var optionIndex28 = 0; optionIndex28 < optionListLen28; optionIndex28++) {
+	var optionData28 = optionList28[optionIndex28];
+	output += ddm.validationOption(soy.$$augmentMap(opt_data, {option: optionData28}));
+}
+output += '</select></div><div class="col-md-6"><select class="form-control validations-select">';
+var optionList33 = opt_data.validationsOptions;
 var optionListLen33 = optionList33.length;
 for (var optionIndex33 = 0; optionIndex33 < optionListLen33; optionIndex33++) {
 	var optionData33 = optionList33[optionIndex33];
 	output += ddm.validationOption(soy.$$augmentMap(opt_data, {option: optionData33}));
-}
-output += '</select></div><div class="col-md-6"><select class="form-control validations-select">';
-var optionList38 = opt_data.validationsOptions;
-var optionListLen38 = optionList38.length;
-for (var optionIndex38 = 0; optionIndex38 < optionListLen38; optionIndex38++) {
-	var optionData38 = optionList38[optionIndex38];
-	output += ddm.validationOption(soy.$$augmentMap(opt_data, {option: optionData38}));
 }
 output += '</select></div></div><div class="' + soy.$$escapeHtml(opt_data.enableValidationValue ? '' : 'hide') + ' row"><div class="col-md-6"><input class="field form-control ' + soy.$$escapeHtml(opt_data.parameterMessagePlaceholder ? '' : ' hide') + ' parameter-input" placeholder="' + soy.$$escapeHtml(opt_data.parameterMessagePlaceholder) + '" type="text" value="' + soy.$$escapeHtml(opt_data.parameterValue) + '" /></div><div class="col-md-6"><input class="field form-control message-input" placeholder="' + soy.$$escapeHtml(opt_data.errorMessagePlaceholder) + '" type="text" value="' + soy.$$escapeHtml(opt_data.errorMessageValue) + '" /></div></div><input name="' + soy.$$escapeHtml(opt_data.name) + '" type="hidden" value="' + soy.$$escapeHtml(opt_data.value) + '" /></div>';
 return output;
