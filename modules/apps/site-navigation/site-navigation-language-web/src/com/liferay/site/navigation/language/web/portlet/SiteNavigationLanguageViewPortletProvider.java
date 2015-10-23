c765d0c98ef7400f9a521d92c731a5e66039cfbd
Hugo Huijser <hugo.huijser@liferay.com>
2015-10-23 08:51:10 +0800

    LPS-59341 Fix package path

--- modules/apps/site-navigation/site-navigation-language-web/src/com/liferay/site/navigation/language/web/portlet/SiteNavigationLanguageViewPortletProvider.java
+++ modules/apps/site-navigation/site-navigation-language-web/src/com/liferay/site/navigation/language/web/portlet/SiteNavigationLanguageViewPortletProvider.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.site.navigation.language.web.provider;
+package com.liferay.site.navigation.language.web.portlet;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.BasePortletProvider;
