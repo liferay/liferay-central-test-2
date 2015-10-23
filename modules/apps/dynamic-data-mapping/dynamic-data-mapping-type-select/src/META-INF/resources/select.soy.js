0d1ffd28ca3b601a2786ee78f7e1e72136e2ae88
Bruno Basto <bruno.basto@liferay.com>
2015-10-23 12:58:58 -0300

    LPS-59779 Regen

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-select/src/META-INF/resources/select.soy.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-select/src/META-INF/resources/select.soy.js
@@ -5,13 +5,13 @@ if (typeof ddm == 'undefined') { var ddm = {}; }
 
 
 ddm.select = function(opt_data, opt_ignored) {
-  var output = '\t<div class="form-group' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + '" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '"><div class="input-select-wrapper">' + ((opt_data.showLabel) ? '<label class="control-label" for="' + soy.$$escapeHtml(opt_data.name) + '">' + soy.$$escapeHtml(opt_data.label) + ((opt_data.required) ? '<b>*</b>' : '') + '</label>' : '') + '<select class="form-control" dir="' + soy.$$escapeHtml(opt_data.dir) + '" id="' + soy.$$escapeHtml(opt_data.name) + '" name="' + soy.$$escapeHtml(opt_data.name) + '" ' + soy.$$escapeHtml(opt_data.multiple) + '>';
-  var optionList27 = opt_data.options;
-  var optionListLen27 = optionList27.length;
-  for (var optionIndex27 = 0; optionIndex27 < optionListLen27; optionIndex27++) {
-    var optionData27 = optionList27[optionIndex27];
-    output += '<option dir="' + soy.$$escapeHtml(opt_data.dir) + '" ' + soy.$$escapeHtml(optionData27.status) + ' value="' + soy.$$escapeHtml(optionData27.value) + '">' + soy.$$escapeHtml(optionData27.label) + '</option>';
-  }
-  output += '</select>' + soy.$$filterNoAutoescape(opt_data.childElementsHTML) + '</div></div>';
-  return output;
+var output = '\t<div class="form-group' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + '" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '"><div class="input-select-wrapper">' + ((opt_data.showLabel) ? '<label class="control-label" for="' + soy.$$escapeHtml(opt_data.name) + '">' + soy.$$escapeHtml(opt_data.label) + ((opt_data.required) ? '<b>*</b>' : '') + '</label>' : '') + '<select class="form-control" dir="' + soy.$$escapeHtml(opt_data.dir) + '" id="' + soy.$$escapeHtml(opt_data.name) + '" name="' + soy.$$escapeHtml(opt_data.name) + '" ' + soy.$$escapeHtml(opt_data.multiple) + '>';
+var optionList27 = opt_data.options;
+var optionListLen27 = optionList27.length;
+for (var optionIndex27 = 0; optionIndex27 < optionListLen27; optionIndex27++) {
+	var optionData27 = optionList27[optionIndex27];
+	output += '<option dir="' + soy.$$escapeHtml(opt_data.dir) + '" ' + soy.$$escapeHtml(optionData27.status) + ' value="' + soy.$$escapeHtml(optionData27.value) + '">' + soy.$$escapeHtml(optionData27.label) + '</option>';
+}
+output += '</select>' + soy.$$filterNoAutoescape(opt_data.childElementsHTML) + '</div></div>';
+return output;
 };
