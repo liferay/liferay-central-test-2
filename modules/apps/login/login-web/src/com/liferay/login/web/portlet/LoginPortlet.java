5dba5b34b307b4cdcb9d37ee25f874b9af6f1917
peterfellwock <peter.fellwock@liferay.com>
2015-10-22 18:40:57 -0700

    LPS-57626 Convert Portlet OSGI Portlet

--- modules/apps/login/login-web/src/com/liferay/login/web/portlet/LoginPortlet.java
+++ modules/apps/login/login-web/src/com/liferay/login/web/portlet/LoginPortlet.java
@@ -12,12 +12,42 @@
  * details.
  */
 
-package com.liferay.portlet.login;
+package com.liferay.login.web.portlet;
 
+import com.liferay.login.web.constants.LoginPortletKeys;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
 
 /**
  * @author Peter Fellwock
  */
+@Component(
+		immediate = true,
+		property = {
+			"com.liferay.portlet.add-default-resource=true",
+			"com.liferay.portlet.css-class-wrapper=portlet-login",
+			"com.liferay.portlet.display-category=category.tools",
+			"com.liferay.portlet.icon=/icons/login.png",
+			"com.liferay.portlet.preferences-owned-by-group=true",
+			"com.liferay.portlet.private-request-attributes=false",
+			"com.liferay.portlet.private-session-attributes=false",
+			"com.liferay.portlet.render-weight=50",
+			"com.liferay.portlet.use-default-template=true",
+			"javax.portlet.display-name=Sign In",
+			"javax.portlet.expiration-cache=0",
+			"javax.portlet.init-param.add-process-action-success-action=false",
+			"javax.portlet.init-param.config-template=/configuration.jsp",
+			"javax.portlet.init-param.template-path=/",
+			"javax.portlet.init-param.view-template=/login.jsp",
+			"javax.portlet.name=" + LoginPortletKeys.LOGIN,
+			"javax.portlet.resource-bundle=content.Language",
+			"javax.portlet.security-role-ref=guest,power-user,user",
+			"javax.portlet.supports.mime-type=text/html"
+		},
+		service = Portlet.class
+	)
 public class LoginPortlet extends MVCPortlet {
 }
