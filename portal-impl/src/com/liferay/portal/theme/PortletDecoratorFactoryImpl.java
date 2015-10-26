brianchandotcom/liferay-portal

LPS-58504 Add Portlet Decorator classes

portal-impl/src/com/liferay/portal/util/PortletDecoratorFactoryImpl.java
@@ -1,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.util;
+
+import com.liferay.portal.kernel.util.PortletDecoratorFactory;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.model.PortletDecorator;
+import com.liferay.portal.model.impl.PortletDecoratorImpl;
+
+/**
+ * @author Eduardo Garcia
+ */
+public class PortletDecoratorFactoryImpl implements PortletDecoratorFactory {
+
+	@Override
+	public PortletDecorator getDefaultPortletDecorator() {
+		return new PortletDecoratorImpl(
+			getDefaultPortletDecoratorId(), StringPool.BLANK,
+			getDefaultPortletDecoratorCssClass());
+	}
+
+	@Override
+	public String getDefaultPortletDecoratorCssClass() {
+		return PropsValues.DEFAULT_PORTLET_DECORATOR_CSS_CLASS;
+	}
+
+	@Override
+	public String getDefaultPortletDecoratorId() {
+		return PropsValues.DEFAULT_PORTLET_DECORATOR_ID;
+	}
+
+	public PortletDecorator getPortletDecorator() {
+		return new PortletDecoratorImpl();
+	}
+
+	@Override
+	public PortletDecorator getPortletDecorator(String portletDecoratorId) {
+		return new PortletDecoratorImpl(portletDecoratorId);
+	}
+
+	@Override
+	public PortletDecorator getPortletDecorator(
+		String portletDecoratorId, String name, String cssClass) {
+
+		return new PortletDecoratorImpl(portletDecoratorId, name, cssClass);
+	}
+
+}

portal-service/src/com/liferay/portal/model/PortletDecorator.java
@@ -1,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.model;
+
+import com.liferay.portal.kernel.util.Accessor;
+
+import java.io.Serializable;
+
+/**
+ * @author Eduardo Garcia
+ */
+public interface PortletDecorator
+	extends Comparable<PortletDecorator>, Serializable {
+
+	public static final Accessor<PortletDecorator, String> NAME_ACCESSOR =
+		new Accessor<PortletDecorator, String>() {
+
+			@Override
+			public String get(PortletDecorator portletDecorator) {
+				return portletDecorator.getName();
+			}
+
+			@Override
+			public Class<String> getAttributeClass() {
+				return String.class;
+			}
+
+			@Override
+			public Class<PortletDecorator> getTypeClass() {
+				return PortletDecorator.class;
+			}
+
+		};
+
+	public String getCssClass();
+
+	public String getName();
+
+	public String getPortletDecoratorId();
+
+	public String getPortletDecoratorThumbnailPath();
+
+	public boolean isDefaultPd();
+
+	public void setCssClass(String cssClass);
+
+	public void setDefaultPd(boolean defaultPd);
+
+	public void setName(String name);
+
+	public void setPortletDecoratorThumbnailPath(
+		String portletDecoratorThumbnailPath);
+
+}

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portal/theme/PortletDecoratorFactoryImpl.java
@@ -1,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.theme;
+
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.model.PortletDecorator;
+import com.liferay.portal.model.impl.PortletDecoratorImpl;
+import com.liferay.portal.util.PropsValues;
+
+/**
+ * @author Eduardo Garcia
+ */
+public class PortletDecoratorFactoryImpl implements PortletDecoratorFactory {
+
+	@Override
+	public PortletDecorator getDefaultPortletDecorator() {
+		return new PortletDecoratorImpl(
+			getDefaultPortletDecoratorId(), StringPool.BLANK,
+			getDefaultPortletDecoratorCssClass());
+	}
+
+	@Override
+	public String getDefaultPortletDecoratorCssClass() {
+		return PropsValues.DEFAULT_PORTLET_DECORATOR_CSS_CLASS;
+	}
+
+	@Override
+	public String getDefaultPortletDecoratorId() {
+		return PropsValues.DEFAULT_PORTLET_DECORATOR_ID;
+	}
+
+	public PortletDecorator getPortletDecorator() {
+		return new PortletDecoratorImpl();
+	}
+
+	@Override
+	public PortletDecorator getPortletDecorator(String portletDecoratorId) {
+		return new PortletDecoratorImpl(portletDecoratorId);
+	}
+
+	@Override
+	public PortletDecorator getPortletDecorator(
+		String portletDecoratorId, String name, String cssClass) {
+
+		return new PortletDecoratorImpl(portletDecoratorId, name, cssClass);
+	}
+
+}

portal-service/src/com/liferay/portal/model/PortletDecorator.java
@@ -1,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.model;
+
+import com.liferay.portal.kernel.util.Accessor;
+
+import java.io.Serializable;
+
+/**
+ * @author Eduardo Garcia
+ */
+public interface PortletDecorator
+	extends Comparable<PortletDecorator>, Serializable {
+
+	public static final Accessor<PortletDecorator, String> NAME_ACCESSOR =
+		new Accessor<PortletDecorator, String>() {
+
+			@Override
+			public String get(PortletDecorator portletDecorator) {
+				return portletDecorator.getName();
+			}
+
+			@Override
+			public Class<String> getAttributeClass() {
+				return String.class;
+			}
+
+			@Override
+			public Class<PortletDecorator> getTypeClass() {
+				return PortletDecorator.class;
+			}
+
+		};
+
+	public String getCssClass();
+
+	public String getName();
+
+	public String getPortletDecoratorId();
+
+	public String getPortletDecoratorThumbnailPath();
+
+	public boolean isDefaultPortletDecorator();
+
+	public void setCssClass(String cssClass);
+
+	public void setDefaultPortletDecorator(boolean defaultPortletDecorator);
+
+	public void setName(String name);
+
+	public void setPortletDecoratorThumbnailPath(
+		String portletDecoratorThumbnailPath);
+
+}