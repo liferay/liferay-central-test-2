af963b3944833b109787f108d0709cca6aa9107f
Juergen Kappler <jurgen.kappler@liferay.com>
2015-10-28 17:29:29 +0100

    LPS-58808 Create PortletKeys for the different test cases of portlets

---
+++ portal-impl/test/integration/com/liferay/portlet/bundle/portletpreferencesfactoryimplgetpreferencesids/constants/TestPortletsPortletKeys.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.bundle.portletpreferencesfactoryimplgetpreferencesids.constants;
+
+/**
+ * @author Juergen Kappler
+ */
+public class TestPortletsPortletKeys {
+
+	public static final String TEST_COMPANY_PORTLET =
+		"com_liferay_test_TestCompanyPortlet";
+
+	public static final String TEST_GROUP_LAYOUT_PORTLET =
+		"com_liferay_test_TestGroupLayoutPortlet";
+
+	public static final String TEST_GROUP_PORTLET =
+		"com_liferay_test_TestGroupPortlet";
+
+	public static final String TEST_GROUP_RESTRICTED_PORTLET =
+		"com_liferay_test_TestGroupRestrictedPortlet";
+
+	public static final String TEST_USER_LAYOUT_PORTLET =
+		"com_liferay_test_TestUserLayoutPortlet";
+
+	public static final String TEST_USER_PORTLET =
+		"com_liferay_test_TestUserPortlet";
+
+}
