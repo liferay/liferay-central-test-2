c268d99d46503ab923e840106c7058e9d8bbdd9b
shuyangzhou <shuyang.zhou@liferay.com>
2015-10-28 09:30:53 -0700

    LPS-59941 Add unit test to make sure same issue won't happen again.

--- portal-service/test/unit/com/liferay/portal/modules/ModulesStructureTest.java
+++ portal-service/test/unit/com/liferay/portal/modules/ModulesStructureTest.java
@@ -23,6 +23,10 @@ import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -61,6 +65,47 @@ public class ModulesStructureTest {
 						}
 
 						return FileVisitResult.SKIP_SUBTREE;
+					}
+
+					return FileVisitResult.CONTINUE;
+				}
+
+			});
+	}
+
+	@Test
+	public void testScanLog4JConfigurationXML() throws IOException {
+		final Map<String, String> renameMap = new HashMap<>();
+
+		renameMap.put("src/META-INF/portal-log4j.xml", "module-log4j.xml");
+		renameMap.put(
+			"src/META-INF/portal-log4j-ext.xml", "module-log4j-ext.xml");
+		renameMap.put(
+			"src/main/resources/META-INF/portal-log4j.xml", "module-log4j.xml");
+		renameMap.put(
+			"src/main/resources/META-INF/portal-log4j-ext.xml",
+			"module-log4j-ext.xml");
+
+		Files.walkFileTree(
+			Paths.get("modules"),
+			new SimpleFileVisitor<Path>() {
+
+				@Override
+				public FileVisitResult preVisitDirectory(
+					Path dirPath, BasicFileAttributes basicFileAttributes) {
+
+					if (Files.exists(dirPath.resolve("bnd.bnd"))) {
+						for (Entry<String, String> entry :
+								renameMap.entrySet()) {
+
+							Path path = dirPath.resolve(entry.getKey());
+
+							if (Files.exists(path)) {
+								Assert.fail(
+									"Please rename " + path + " to " +
+										path.resolveSibling(entry.getValue()));
+							}
+						}
 					}
 
 					return FileVisitResult.CONTINUE;
