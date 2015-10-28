65e087d11c366cd09226b0f4d1f1aafeb957de52
Juergen Kappler <jurgen.kappler@liferay.com>
2015-10-28 17:41:16 +0100

    LPS-58808 Remove the unit tests that were moved

--- portal-impl/test/unit/com/liferay/portlet/PortletPreferencesFactoryImplGetPreferencesIdsTest.java
+++ portal-impl/test/unit/com/liferay/portlet/PortletPreferencesFactoryImplGetPreferencesIdsTest.java
@@ -17,7 +17,6 @@ package com.liferay.portlet;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.Portlet;
-import com.liferay.portal.model.PortletPreferencesIds;
 import com.liferay.portal.model.impl.LayoutImpl;
 import com.liferay.portal.model.impl.PortletImpl;
 import com.liferay.portal.security.auth.PrincipalException;
@@ -27,9 +26,7 @@ import com.liferay.portal.security.permission.PermissionThreadLocal;
 import com.liferay.portal.service.PortletLocalServiceUtil;
 import com.liferay.portal.service.permission.LayoutPermissionUtil;
 import com.liferay.portal.tools.ToolDependencies;
-import com.liferay.portal.util.PortletKeys;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -68,157 +65,6 @@ public class PortletPreferencesFactoryImplGetPreferencesIdsTest {
 		_layout.setCompanyId(RandomTestUtil.randomLong());
 		_layout.setPlid(RandomTestUtil.randomLong());
 		_layout.setPrivateLayout(true);
-	}
-
-	@Test
-	public void testPreferencesOwnedByCompany() throws Exception {
-		PowerMockito.mockStatic(PortletLocalServiceUtil.class);
-
-		Mockito.when(
-			PortletLocalServiceUtil.getPortletById(
-				_layout.getCompanyId(), _PORTLET_ID)
-		).thenReturn(
-			getCompanyPortlet()
-		);
-
-		long siteGroupId = _layout.getGroupId();
-		boolean modeEditGuest = false;
-
-		PortletPreferencesIds portletPreferencesIds =
-			PortletPreferencesFactoryUtil.getPortletPreferencesIds(
-				siteGroupId, _USER_ID, _layout, _PORTLET_ID, modeEditGuest);
-
-		Assert.assertEquals(
-			"The owner type should be of type company",
-			PortletKeys.PREFS_OWNER_TYPE_COMPANY,
-			portletPreferencesIds.getOwnerType());
-		Assert.assertEquals(
-			"The owner ID should be the ID of the company",
-			_layout.getCompanyId(), portletPreferencesIds.getOwnerId());
-		Assert.assertEquals(
-			"The PLID should not be a real value",
-			PortletKeys.PREFS_PLID_SHARED, portletPreferencesIds.getPlid());
-	}
-
-	@Test
-	public void testPreferencesOwnedByGroup() throws Exception {
-		PowerMockito.mockStatic(PortletLocalServiceUtil.class);
-
-		Mockito.when(
-			PortletLocalServiceUtil.getPortletById(
-				_layout.getCompanyId(), _PORTLET_ID)
-		).thenReturn(
-			getGroupPortlet()
-		);
-
-		long siteGroupId = _layout.getGroupId();
-		boolean modeEditGuest = false;
-
-		PortletPreferencesIds portletPreferencesIds =
-			PortletPreferencesFactoryUtil.getPortletPreferencesIds(
-				siteGroupId, _USER_ID, _layout, _PORTLET_ID, modeEditGuest);
-
-		Assert.assertEquals(
-			"The owner type should be of type group",
-			PortletKeys.PREFS_OWNER_TYPE_GROUP,
-			portletPreferencesIds.getOwnerType());
-		Assert.assertEquals(
-			"The owner ID should be the ID of the group", siteGroupId,
-			portletPreferencesIds.getOwnerId());
-		Assert.assertEquals(
-			"The PLID should not be a real value",
-			PortletKeys.PREFS_PLID_SHARED, portletPreferencesIds.getPlid());
-	}
-
-	@Test
-	public void testPreferencesOwnedByGroupLayout() throws Exception {
-		PowerMockito.mockStatic(PortletLocalServiceUtil.class);
-
-		Mockito.when(
-			PortletLocalServiceUtil.getPortletById(
-				_layout.getCompanyId(), _PORTLET_ID)
-		).thenReturn(
-			getGroupLayoutPortlet()
-		);
-
-		long siteGroupId = _layout.getGroupId();
-		boolean modeEditGuest = false;
-
-		PortletPreferencesIds portletPreferencesIds =
-			PortletPreferencesFactoryUtil.getPortletPreferencesIds(
-				siteGroupId, _USER_ID, _layout, _PORTLET_ID, modeEditGuest);
-
-		Assert.assertEquals(
-			"The owner type should be of type layout",
-			PortletKeys.PREFS_OWNER_TYPE_LAYOUT,
-			portletPreferencesIds.getOwnerType());
-		Assert.assertEquals(
-			"The owner ID should be the default value",
-			PortletKeys.PREFS_OWNER_ID_DEFAULT,
-			portletPreferencesIds.getOwnerId());
-		Assert.assertEquals(
-			"The PLID should be the PLID of the current layout",
-			_layout.getPlid(), portletPreferencesIds.getPlid());
-	}
-
-	@Test
-	public void testPreferencesOwnedByUser() throws Exception {
-		PowerMockito.mockStatic(PortletLocalServiceUtil.class);
-
-		Mockito.when(
-			PortletLocalServiceUtil.getPortletById(
-				_layout.getCompanyId(), _PORTLET_ID)
-		).thenReturn(
-			getUserPortlet()
-		);
-
-		long siteGroupId = _layout.getGroupId();
-		boolean modeEditGuest = false;
-
-		PortletPreferencesIds portletPreferencesIds =
-			PortletPreferencesFactoryUtil.getPortletPreferencesIds(
-				siteGroupId, _USER_ID, _layout, _PORTLET_ID, modeEditGuest);
-
-		Assert.assertEquals(
-			"The owner type should be of type user",
-			PortletKeys.PREFS_OWNER_TYPE_USER,
-			portletPreferencesIds.getOwnerType());
-		Assert.assertEquals(
-			"The owner ID should be the ID of the user who added it", _USER_ID,
-			portletPreferencesIds.getOwnerId());
-		Assert.assertEquals(
-			"The PLID should not be a real value",
-			PortletKeys.PREFS_PLID_SHARED, portletPreferencesIds.getPlid());
-	}
-
-	@Test
-	public void testPreferencesOwnedByUserLayout() throws Exception {
-		PowerMockito.mockStatic(PortletLocalServiceUtil.class);
-
-		Mockito.when(
-			PortletLocalServiceUtil.getPortletById(
-				_layout.getCompanyId(), _PORTLET_ID)
-		).thenReturn(
-			getUserLayoutPortlet()
-		);
-
-		long siteGroupId = _layout.getGroupId();
-		boolean modeEditGuest = false;
-
-		PortletPreferencesIds portletPreferencesIds =
-			PortletPreferencesFactoryUtil.getPortletPreferencesIds(
-				siteGroupId, _USER_ID, _layout, _PORTLET_ID, modeEditGuest);
-
-		Assert.assertEquals(
-			"The owner type should be of type user",
-			PortletKeys.PREFS_OWNER_TYPE_USER,
-			portletPreferencesIds.getOwnerType());
-		Assert.assertEquals(
-			"The owner ID should be the ID of the user who added it", _USER_ID,
-			portletPreferencesIds.getOwnerId());
-		Assert.assertEquals(
-			"The PLID should be the PLID of the current layout",
-			_layout.getPlid(), portletPreferencesIds.getPlid());
 	}
 
 	@Test(expected = PrincipalException.MustHavePermission.class)
@@ -300,81 +146,11 @@ public class PortletPreferencesFactoryImplGetPreferencesIdsTest {
 			siteGroupId, _USER_ID, _layout, _PORTLET_ID, modeEditGuest);
 	}
 
-	@Test
-	public void testPreferencesWithModeEditGuestInPublicLayoutWithPermission()
-		throws Exception {
-
-		_layout.setPrivateLayout(false);
-
-		PowerMockito.mockStatic(PortletLocalServiceUtil.class);
-
-		Mockito.when(
-			PortletLocalServiceUtil.getPortletById(
-				_layout.getCompanyId(), _PORTLET_ID)
-		).thenReturn(
-			getGroupPortlet()
-		);
-
-		PowerMockito.mockStatic(LayoutPermissionUtil.class);
-
-		Mockito.when(
-			LayoutPermissionUtil.contains(
-				Mockito.any(PermissionChecker.class), Mockito.eq(_layout),
-				Mockito.eq(ActionKeys.UPDATE))
-		).thenReturn(
-			true
-		);
-
-		long siteGroupId = _layout.getGroupId();
-		boolean modeEditGuest = true;
-
-		PortletPreferencesFactoryUtil.getPortletPreferencesIds(
-			siteGroupId, _USER_ID, _layout, _PORTLET_ID, modeEditGuest);
-	}
-
-	protected Portlet getCompanyPortlet() {
-		Portlet portlet = new PortletImpl();
-
-		portlet.setPreferencesCompanyWide(true);
-
-		return portlet;
-	}
-
-	protected Portlet getGroupLayoutPortlet() {
-		Portlet portlet = new PortletImpl();
-
-		portlet.setPreferencesCompanyWide(false);
-		portlet.setPreferencesOwnedByGroup(true);
-		portlet.setPreferencesUniquePerLayout(true);
-
-		return portlet;
-	}
-
 	protected Portlet getGroupPortlet() {
 		Portlet portlet = new PortletImpl();
 
 		portlet.setPreferencesCompanyWide(false);
 		portlet.setPreferencesOwnedByGroup(true);
-		portlet.setPreferencesUniquePerLayout(false);
-
-		return portlet;
-	}
-
-	protected Portlet getUserLayoutPortlet() {
-		Portlet portlet = new PortletImpl();
-
-		portlet.setPreferencesCompanyWide(false);
-		portlet.setPreferencesOwnedByGroup(false);
-		portlet.setPreferencesUniquePerLayout(true);
-
-		return portlet;
-	}
-
-	protected Portlet getUserPortlet() {
-		Portlet portlet = new PortletImpl();
-
-		portlet.setPreferencesCompanyWide(false);
-		portlet.setPreferencesOwnedByGroup(false);
 		portlet.setPreferencesUniquePerLayout(false);
 
 		return portlet;
