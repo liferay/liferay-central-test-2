90c57c84f18e96a211f2cbbd0c5cf946595dec60
Brian Chan <brian.chan@liferay.com>
2015-10-28 14:35:15 -0700

    LPS-59842 fix compile error

--- portal-impl/test/integration/com/liferay/portal/repository/registry/bundle/repositoryclassdefinitioncatalogimpl/TestExternalRepositoryDefiner.java
+++ portal-impl/test/integration/com/liferay/portal/repository/registry/bundle/repositoryclassdefinitioncatalogimpl/TestExternalRepositoryDefiner.java
@@ -49,7 +49,7 @@ public class TestExternalRepositoryDefiner implements RepositoryDefiner {
 
 	@Override
 	public RepositoryConfiguration getRepositoryConfiguration() {
-		return _EMPTY_CONFIGURATION;
+		return _repositoryConfiguration;
 	}
 
 	@Override
