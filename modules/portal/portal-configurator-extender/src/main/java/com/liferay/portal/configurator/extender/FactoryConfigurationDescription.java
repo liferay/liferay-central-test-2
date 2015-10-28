314175ab5db349b250c7dd445ee0c3ea7eabc84c
Carlos Sierra <carlos.sierra@liferay.com>
2015-10-28 12:00:45 +0100

    LPS-59342 Interfaces for the different configuration types available
    Configuration Admin supports two different types of configuration
    (Single and Factory). Implementors of these interfaces can instruct the
    extension which type the NamedConfiguration refers to.

---
+++ modules/portal/portal-configurator-extender/src/main/java/com/liferay/portal/configurator/extender/FactoryConfigurationDescription.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.configurator.extender;
+
+import com.liferay.portal.kernel.util.Supplier;
+
+import java.util.Dictionary;
+
+/**
+* @author Carlos Sierra Andrés
+*/
+public final class FactoryConfigurationDescription
+	extends ConfigurationDescription {
+
+	public FactoryConfigurationDescription(
+		String factoryPid, String pid,
+		Supplier<Dictionary<String, Object>> propertiesSupplier) {
+
+		_factoryPid = factoryPid;
+		_pid = pid;
+		_propertiesSupplier = propertiesSupplier;
+	}
+
+	public String getFactoryPid() {
+		return _factoryPid;
+	}
+
+	public String getPid() {
+		return _pid;
+	}
+
+	public Supplier<Dictionary<String, Object>> getPropertiesSupplier() {
+		return _propertiesSupplier;
+	}
+
+	@Override
+	public String toString() {
+		return "FactoryConfigurationDescription{" +
+			"factoryPid='" + _factoryPid + '\'' +
+			", pid='" + _pid + '\'' +
+			'}';
+	}
+
+	private final String _factoryPid;
+	private final String _pid;
+	private final Supplier<Dictionary<String, Object>> _propertiesSupplier;
+
+}
