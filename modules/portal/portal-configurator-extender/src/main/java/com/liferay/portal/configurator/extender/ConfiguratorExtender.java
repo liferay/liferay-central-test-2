c4b535cfc15e61aa43b3e39ef92cfbca0b059e83
Carlos Sierra <carlos.sierra@liferay.com>
2015-10-28 12:53:47 +0100

    LPS-59342 Extension mechanism

---
+++ modules/portal/portal-configurator-extender/src/main/java/com/liferay/portal/configurator/extender/ConfiguratorExtender.java
@@ -0,0 +1,221 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.configurator.extender;
+
+import com.liferay.portal.configurator.extender.internal.ConfiguratorExtension;
+
+import java.io.IOException;
+
+import java.net.URL;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.apache.felix.utils.extender.AbstractExtender;
+import org.apache.felix.utils.extender.Extension;
+import org.apache.felix.utils.log.Logger;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+
+/**
+ * @author Carlos Sierra Andrés
+ * @author Miguel Pastor
+ */
+@Component(immediate = true)
+public class ConfiguratorExtender extends AbstractExtender {
+
+	@Activate
+	protected void activate(BundleContext bundleContext) throws Exception {
+		setSynchronous(true);
+
+		_logger = new Logger(bundleContext);
+
+		start(bundleContext);
+	}
+
+	@Reference(
+		cardinality = ReferenceCardinality.MULTIPLE,
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
+		unbind = "removeConfigurationDescriptionFactory"
+	)
+	protected void addConfigurationDescriptionFactory(
+		ConfigurationDescriptionFactory configurationDescriptionFactory) {
+
+		_configurationDescriptionFactories.add(configurationDescriptionFactory);
+	}
+
+	@Reference(
+		cardinality = ReferenceCardinality.MULTIPLE,
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
+		unbind = "removeConfigurationURLLocator"
+	)
+	protected void addConfigurationURLLocator(
+		NamedConfigurationContentFactory namedConfigurationContentFactory) {
+
+		_namedConfigurationContentFactories.add(
+			namedConfigurationContentFactory);
+	}
+
+	@Deactivate
+	protected void deactivate(BundleContext bundleContext) throws Exception {
+		stop(bundleContext);
+	}
+
+	@Override
+	protected void debug(Bundle bundle, String s) {
+		_logger.log(Logger.LOG_DEBUG, "[" + bundle + "] " + s);
+	}
+
+	@Override
+	protected Extension doCreateExtension(Bundle bundle) throws Exception {
+		Collection<NamedConfigurationContent> configurationURLs =
+			new ArrayList<>();
+
+		for (NamedConfigurationContentFactory namedConfigurationContentFactory :
+				_namedConfigurationContentFactories) {
+
+			try {
+				configurationURLs.addAll(
+					namedConfigurationContentFactory.create(
+						new BundleStorageImpl(bundle)));
+			}
+			catch (Throwable t) {
+				_logger.log(
+					Logger.LOG_INFO, "ConfigurationURLLocator threw Exception",
+					t);
+			}
+		}
+
+		if (configurationURLs.isEmpty()) {
+			return null;
+		}
+
+		return new ConfiguratorExtension(
+			_configurationAdmin, _logger, bundle.getSymbolicName(),
+			configurationURLs, _configurationDescriptionFactories);
+	}
+
+	@Override
+	protected void error(String s, Throwable throwable) {
+		_logger.log(Logger.LOG_ERROR, s, throwable);
+	}
+
+	protected void removeConfigurationDescriptionFactory(
+		ConfigurationDescriptionFactory configurationDescriptionFactory) {
+
+		_configurationDescriptionFactories.remove(
+			configurationDescriptionFactory);
+	}
+
+	protected void removeConfigurationURLLocator(
+		NamedConfigurationContentFactory namedConfigurationContentFactory) {
+
+		_namedConfigurationContentFactories.remove(
+			namedConfigurationContentFactory);
+	}
+
+	@Reference
+	protected void setConfigurationAdmin(
+		ConfigurationAdmin configurationAdmin) {
+
+		_configurationAdmin = configurationAdmin;
+	}
+
+	@Override
+	protected void warn(Bundle bundle, String s, Throwable throwable) {
+		_logger.log(Logger.LOG_DEBUG, "[" + bundle + "] " + s);
+	}
+
+	private ConfigurationAdmin _configurationAdmin;
+	private final Collection<ConfigurationDescriptionFactory>
+		_configurationDescriptionFactories = new CopyOnWriteArrayList<>();
+	private Logger _logger;
+	private final Collection<NamedConfigurationContentFactory>
+		_namedConfigurationContentFactories = new CopyOnWriteArrayList<>();
+
+	private static class BundleStorageImpl implements BundleStorage {
+
+		public BundleStorageImpl(Bundle bundle) {
+			_bundle = bundle;
+		}
+
+		@Override
+		public Enumeration<URL> findEntries(String s, String s1, boolean b) {
+			return _bundle.findEntries(s, s1, b);
+		}
+
+		@Override
+		public long getBundleId() {
+			return _bundle.getBundleId();
+		}
+
+		@Override
+		public URL getEntry(String s) {
+			return _bundle.getEntry(s);
+		}
+
+		@Override
+		public Enumeration<String> getEntryPaths(String s) {
+			return _bundle.getEntryPaths(s);
+		}
+
+		@Override
+		public Dictionary<String, String> getHeaders() {
+			return _bundle.getHeaders();
+		}
+
+		public Dictionary<String, String> getHeaders(String s) {
+			return _bundle.getHeaders(s);
+		}
+
+		@Override
+		public String getLocation() {
+			return _bundle.getLocation();
+		}
+
+		@Override
+		public URL getResource(String s) {
+			return _bundle.getResource(s);
+		}
+
+		@Override
+		public Enumeration<URL> getResources(String s) throws IOException {
+			return _bundle.getResources(s);
+		}
+
+		@Override
+		public String getSymbolicName() {
+			return _bundle.getSymbolicName();
+		}
+
+		private final Bundle _bundle;
+
+	}
+
+}
---
+++ modules/portal/portal-configurator-extender/src/main/java/com/liferay/portal/configurator/extender/internal/ConfiguratorExtension.java
@@ -0,0 +1,201 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.configurator.extender.internal;
+
+import com.liferay.portal.configurator.extender.ConfigurationDescription;
+import com.liferay.portal.configurator.extender.ConfigurationDescriptionFactory;
+import com.liferay.portal.configurator.extender.FactoryConfigurationDescription;
+import com.liferay.portal.configurator.extender.NamedConfigurationContent;
+import com.liferay.portal.configurator.extender.SingleConfigurationDescription;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.Supplier;
+
+import java.io.IOException;
+
+import java.util.Collection;
+import java.util.Dictionary;
+
+import org.apache.felix.utils.extender.Extension;
+import org.apache.felix.utils.log.Logger;
+
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+
+/**
+* @author Carlos Sierra Andrés
+*/
+public class ConfiguratorExtension implements Extension {
+
+	public ConfiguratorExtension(
+		ConfigurationAdmin configurationAdmin, Logger logger, String namespace,
+		Collection<NamedConfigurationContent> configurationContents,
+		Collection<ConfigurationDescriptionFactory>
+			configurationDescriptionFactories) {
+
+		_configurationAdmin = configurationAdmin;
+		_logger = logger;
+		_namespace = namespace;
+		_configurationContents = configurationContents;
+		_configurationDescriptionFactories = configurationDescriptionFactories;
+	}
+
+	@Override
+	public void destroy() throws Exception {
+	}
+
+	@Override
+	public void start() throws Exception {
+		for (NamedConfigurationContent namedConfigurationContent :
+				_configurationContents) {
+
+			try {
+				_createConfiguration(namedConfigurationContent);
+			}
+			catch (IOException ioe) {
+				continue;
+			}
+		}
+	}
+
+	private void _createConfiguration(
+			NamedConfigurationContent namedConfigurationContent)
+		throws Exception {
+
+		for (ConfigurationDescriptionFactory
+			configurationDescriptionFactory :
+			_configurationDescriptionFactories) {
+
+			ConfigurationDescription configurationDescription =
+				configurationDescriptionFactory.create(
+					namedConfigurationContent);
+
+			if (configurationDescription == null) {
+				continue;
+			}
+
+			if (configurationDescription
+					instanceof SingleConfigurationDescription) {
+
+				process(
+					(SingleConfigurationDescription)configurationDescription);
+			}
+			else if (configurationDescription
+						instanceof FactoryConfigurationDescription) {
+
+				process(
+					(FactoryConfigurationDescription)configurationDescription);
+			}
+			else {
+				_logger.log(
+					Logger.LOG_ERROR,
+					configurationDescriptionFactory + " returned " +
+						"unsupported ConfigurationDescription " +
+							configurationDescription);
+			}
+		}
+	}
+
+	private boolean configurationExists(String filter)
+		throws InvalidSyntaxException, IOException {
+
+		Configuration[] configurations = _configurationAdmin.listConfigurations(
+			filter);
+
+		if (ArrayUtil.isNotEmpty(configurations)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private void process(FactoryConfigurationDescription description)
+		throws InvalidSyntaxException, IOException {
+
+		String factoryPid = description.getFactoryPid();
+		String pid = description.getPid();
+
+		String configuratorUrl = _namespace + "#" + pid;
+
+		if (configurationExists("(configurator.url=" + configuratorUrl + ")")) {
+			return;
+		}
+
+		Configuration configuration =
+			_configurationAdmin.createFactoryConfiguration(factoryPid, null);
+		Supplier<Dictionary<String, Object>> propertiesSupplier =
+			description.getPropertiesSupplier();
+
+		Dictionary<String, Object> properties;
+
+		try {
+			properties = propertiesSupplier.get();
+		}
+		catch (Throwable t) {
+			_logger.log(
+				Logger.LOG_WARNING,
+				"Supplier from description " + description + " threw " +
+					"Exception: ",
+				t);
+
+			return;
+		}
+
+		properties.put("configurator.url", configuratorUrl);
+
+		configuration.update(properties);
+	}
+
+	private void process(SingleConfigurationDescription description)
+		throws InvalidSyntaxException, IOException {
+
+		String pid = description.getPid();
+
+		if (configurationExists("(service.pid=" + pid + ")")) {
+			return;
+		}
+
+		Configuration configuration = _configurationAdmin.getConfiguration(
+			pid, null);
+
+		Supplier<Dictionary<String, Object>> propertiesSupplier =
+			description.getPropertiesSupplier();
+
+		Dictionary<String, Object> properties;
+
+		try {
+			properties = propertiesSupplier.get();
+		}
+		catch (Throwable t) {
+			_logger.log(
+				Logger.LOG_WARNING,
+				"Supplier from description " + description + " threw " +
+					"Exception: ",
+				t);
+
+			return;
+		}
+
+		configuration.update(properties);
+	}
+
+	private final ConfigurationAdmin _configurationAdmin;
+	private final Collection<NamedConfigurationContent> _configurationContents;
+	private final Collection<ConfigurationDescriptionFactory>
+		_configurationDescriptionFactories;
+	private final Logger _logger;
+	private final String _namespace;
+
+}
