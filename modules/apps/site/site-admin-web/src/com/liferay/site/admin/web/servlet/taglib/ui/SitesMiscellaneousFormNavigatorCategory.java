9fcf93719d7f55fcbf128ebc563fd4d8573fa808
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-10-28 11:02:43 +0100

    LPS-59925 Miscellaneous should be the last category (social should be placed before Miscellaneous)

--- modules/apps/site/site-admin-web/src/com/liferay/site/admin/web/servlet/taglib/ui/SitesMiscellaneousFormNavigatorCategory.java
+++ modules/apps/site/site-admin-web/src/com/liferay/site/admin/web/servlet/taglib/ui/SitesMiscellaneousFormNavigatorCategory.java
@@ -26,7 +26,7 @@ import org.osgi.service.component.annotations.Component;
  * @author Sergio González
  */
 @Component(
-	property = {"service.ranking:Integer=20"},
+	property = {"service.ranking:Integer=10"},
 	service = FormNavigatorCategory.class
 )
 public class SitesMiscellaneousFormNavigatorCategory
