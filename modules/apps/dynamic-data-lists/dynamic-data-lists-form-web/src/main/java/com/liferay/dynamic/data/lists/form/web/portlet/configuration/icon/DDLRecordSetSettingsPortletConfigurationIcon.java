b63c409f062383fff3958cf787c731f99973da8a
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-10-29 15:26:10 -0300

    LPS-59554 Add settings support for RecordSet

---
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/portlet/configuration/icon/DDLRecordSetSettingsPortletConfigurationIcon.java
@@ -0,0 +1,104 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.form.web.portlet.configuration.icon;
+
+import com.liferay.dynamic.data.lists.form.web.constants.DDLFormPortletKeys;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.theme.PortletDisplay;
+import com.liferay.portal.util.PortalUtil;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+import javax.portlet.WindowStateException;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Marcellus Tavares
+ */
+public class DDLRecordSetSettingsPortletConfigurationIcon
+	extends BasePortletConfigurationIcon {
+
+	public DDLRecordSetSettingsPortletConfigurationIcon(
+		HttpServletRequest request) {
+
+		super(request);
+	}
+
+	@Override
+	public String getMessage() {
+		return "settings";
+	}
+
+	@Override
+	public String getURL() {
+		PortletURL portletURL = PortalUtil.getControlPanelPortletURL(
+			request, DDLFormPortletKeys.DYNAMIC_DATA_LISTS_FORM_ADMIN, 0,
+			PortletRequest.RENDER_PHASE);
+
+		portletURL.setParameter("mvcPath", "/admin/record_set_settings.jsp");
+		portletURL.setParameter(
+			"recordSetId", String.valueOf(getRecordSetId()));
+
+		try {
+			portletURL.setWindowState(LiferayWindowState.POP_UP);
+		}
+		catch (WindowStateException wse) {
+		}
+
+		return portletURL.toString();
+	}
+
+	@Override
+	public boolean isShow() {
+		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+		String rootPortletId = portletDisplay.getRootPortletId();
+
+		if (!rootPortletId.equals(
+				DDLFormPortletKeys.DYNAMIC_DATA_LISTS_FORM_ADMIN)) {
+
+			return false;
+		}
+
+		long recordSetId = getRecordSetId();
+
+		if (recordSetId == 0) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean isToolTip() {
+		return false;
+	}
+
+	@Override
+	public boolean isUseDialog() {
+		return true;
+	}
+
+	protected long getRecordSetId() {
+		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+		return ParamUtil.getLong(
+			request, portletDisplay.getNamespace() + "recordSetId");
+	}
+
+}
