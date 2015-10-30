fc9aefc91cc659711921e060688c917a57aba109
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-10-30 09:48:50 -0300

    LPS-59952 Rename - consistency

--- modules/apps/workflow/workflow-instance-web/src/main/java/com/liferay/workflow/instance/web/application/list/MyWorkflowInstancePanelApp.java
+++ modules/apps/workflow/workflow-instance-web/src/main/java/com/liferay/workflow/instance/web/application/list/MyWorkflowInstancePanelApp.java
@@ -34,7 +34,7 @@ import org.osgi.service.component.annotations.Reference;
 	},
 	service = PanelApp.class
 )
-public class MyWorkflowInstancesPanelApp extends BaseControlPanelEntryPanelApp {
+public class MyWorkflowInstancePanelApp extends BaseControlPanelEntryPanelApp {
 
 	@Override
 	public String getPortletId() {
