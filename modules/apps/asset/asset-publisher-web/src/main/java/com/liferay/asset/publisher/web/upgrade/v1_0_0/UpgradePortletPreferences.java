74433efdde3e2d3bd27d7a89a06ad39896fb3f26
Rafael Praxedes <rafael.praxedes@liferay.com>
2015-10-30 18:50:32 -0300

    LPS-59291 Update upgrade to change the stored value for ddm date fields in AssetPublisher preferences, applying the pattern "yyyy-MM-dd" instead of "yyyyMMddHHmmss"

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/upgrade/v1_0_0/UpgradePortletPreferences.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/upgrade/v1_0_0/UpgradePortletPreferences.java
@@ -16,18 +16,33 @@ package com.liferay.asset.publisher.web.upgrade.v1_0_0;
 
 import com.liferay.asset.publisher.web.constants.AssetPublisherPortletKeys;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.upgrade.BaseUpgradePortletPreferences;
+import com.liferay.portal.kernel.upgrade.UpgradeException;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.DateFormatFactoryUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.xml.Document;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.kernel.xml.SAXReaderUtil;
+import com.liferay.portal.util.PortalUtil;
 import com.liferay.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portlet.documentlibrary.model.DLFileEntryType;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+
+import java.text.DateFormat;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.portlet.PortletPreferences;
 
@@ -97,6 +112,24 @@ public class UpgradePortletPreferences extends BaseUpgradePortletPreferences {
 			portletPreferences.setValues("assetEntryXml", assetEntryXmls);
 		}
 
+		String subtypeFieldsFilterEnabled = portletPreferences.getValue(
+			"subtypeFieldsFilterEnabled", Boolean.FALSE.toString());
+
+		if (subtypeFieldsFilterEnabled.equals(Boolean.TRUE.toString())) {
+			boolean journalFilterByFieldEnable = isFilterByFieldEnable(
+				portletPreferences, _JOURNAL_FILTER_BY_FIELD_ENABLED_KEY);
+
+			boolean dlFilterByFieldEnable = isFilterByFieldEnable(
+				portletPreferences, _DL_FILTER_BY_FIELD_ENABLED_KEY);
+
+			if (journalFilterByFieldEnable) {
+				upgradeJournalDateFieldValue(portletPreferences);
+			}
+			else if (dlFilterByFieldEnable) {
+				upgradeDLDateFieldsValues(portletPreferences);
+			}
+		}
+
 		return PortletPreferencesFactoryUtil.toXML(portletPreferences);
 	}
 
@@ -129,5 +162,202 @@ public class UpgradePortletPreferences extends BaseUpgradePortletPreferences {
 			assetEntryXmls[i] = document.formattedString(StringPool.BLANK);
 		}
 	}
+
+	private JSONObject findFieldByName(
+		JSONArray fields, String selectedFieldName) {
+
+		JSONObject result = null;
+
+		for (int i = 0; i < fields.length(); i++) {
+			JSONObject field = fields.getJSONObject(i);
+
+			String fieldName = field.getString("name");
+
+			if (fieldName.equals(selectedFieldName)) {
+				result = field;
+				break;
+			}
+
+			if (field.has("nestedFields")) {
+				JSONArray nestedFields = field.getJSONArray("nestedFields");
+
+				result = findFieldByName(nestedFields, selectedFieldName);
+
+				if (result != null) {
+					break;
+				}
+			}
+		}
+
+		return result;
+	}
+
+	private JSONObject getStructureJSON(long structureId) throws Exception {
+		JSONObject ddmStructureJSON = _structures.get(structureId);
+
+		if (ddmStructureJSON != null) {
+			return ddmStructureJSON;
+		}
+
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = DataAccess.getUpgradeOptimizedConnection();
+
+			ps = con.prepareStatement(
+					"select definition from DDMStructure " +
+						"where structureId = ?" );
+
+			ps.setLong(1, structureId);
+
+			rs = ps.executeQuery();
+
+			if (rs.next()) {
+				String definition = rs.getString("definition");
+
+				ddmStructureJSON = JSONFactoryUtil.createJSONObject(definition);
+
+				_structures.put(structureId, ddmStructureJSON);
+
+				return ddmStructureJSON;
+			}
+
+			throw new UpgradeException(
+				"Unable to find dynamic data mapping structure with ID " +
+					structureId);
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+	}
+
+	private boolean isDateField(
+		JSONObject ddmStructureJSON, String selectedFieldName) {
+
+		JSONArray fields = ddmStructureJSON.getJSONArray("fields");
+
+		JSONObject field = findFieldByName(fields, selectedFieldName);
+
+		if ((field != null) && field.getString("type").equals("ddm-date")) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean isFilterByFieldEnable(
+		PortletPreferences portletPreferences, String key) {
+
+		return GetterUtil.getBoolean(
+			portletPreferences.getValue(key, Boolean.FALSE.toString()));
+	}
+
+	private void transformDateFieldValue(PortletPreferences portletPreferences)
+		throws Exception {
+
+		String oldValue = GetterUtil.getString(
+			portletPreferences.getValue(_DDM_STRUCTURE_FIELD_VALUE, null));
+
+		if (Validator.isNotNull(oldValue)) {
+			Date dateValue = _oldDateFormat.parse(oldValue);
+
+			portletPreferences.setValue(
+				_DDM_STRUCTURE_FIELD_VALUE, _newDateFormat.format(dateValue));
+		}
+	}
+
+	private void upgradeDLDateFieldsValues(
+			PortletPreferences portletPreferences)
+		throws Exception {
+
+		long fileEntryTypeId = GetterUtil.getLong(
+			portletPreferences.getValue(_DL_CLASSTYPE, "0"));
+
+		if (fileEntryTypeId > 0) {
+			Connection con = null;
+			PreparedStatement ps = null;
+			ResultSet rs = null;
+
+			try {
+				long classNameId = PortalUtil.getClassNameId(
+					DLFileEntryType.class);
+
+				con = DataAccess.getUpgradeOptimizedConnection();
+
+				ps = con.prepareStatement(
+					"select structureId from DDMStructureLink " +
+					"where classNameId = ? and classPK = ?" );
+
+				ps.setLong(1, classNameId);
+				ps.setLong(2, fileEntryTypeId);
+
+				rs = ps.executeQuery();
+
+				String selectedFieldName = GetterUtil.getString(
+					portletPreferences.getValue(
+						_DDM_STRUCTURE_FIELD_NAME, null));
+
+				while (rs.next()) {
+					long structureId = rs.getLong("structureId");
+
+					JSONObject ddmStructureJSON = getStructureJSON(structureId);
+
+					if (isDateField(ddmStructureJSON, selectedFieldName)) {
+						transformDateFieldValue(portletPreferences);
+						break;
+					}
+				}
+			}
+			finally {
+				DataAccess.cleanUp(con, ps, rs);
+			}
+		}
+	}
+
+	private void upgradeJournalDateFieldValue(
+			PortletPreferences portletPreferences)
+		throws Exception {
+
+		long structureId = GetterUtil.getLong(
+			portletPreferences.getValue(_JOURNAL_CLASSTYPE, "0"));
+
+		if (structureId > 0) {
+			String selectedFieldName = GetterUtil.getString(
+				portletPreferences.getValue(_DDM_STRUCTURE_FIELD_NAME, null));
+
+			JSONObject ddmStructureJSON = getStructureJSON(structureId);
+
+			if (isDateField(ddmStructureJSON, selectedFieldName)) {
+				transformDateFieldValue(portletPreferences);
+			}
+		}
+	}
+
+	private static final String _DDM_STRUCTURE_FIELD_NAME =
+		"ddmStructureFieldName";
+
+	private static final String _DDM_STRUCTURE_FIELD_VALUE =
+		"ddmStructureFieldValue";
+
+	private static final String _DL_CLASSTYPE =
+		"anyClassTypeDLFileEntryAssetRendererFactory";
+
+	private static final String _DL_FILTER_BY_FIELD_ENABLED_KEY =
+		"subtypeFieldsFilterEnabledDLFileEntryAssetRendererFactory";
+
+	private static final String _JOURNAL_CLASSTYPE =
+		"anyClassTypeJournalArticleAssetRendererFactory";
+
+	private static final String _JOURNAL_FILTER_BY_FIELD_ENABLED_KEY =
+		"subtypeFieldsFilterEnabledJournalArticleAssetRendererFactory";
+
+	private static final Map<Long, JSONObject> _structures = new HashMap<>();
+
+	private final DateFormat _newDateFormat =
+		DateFormatFactoryUtil.getSimpleDateFormat("yyyy-MM-dd");
+	private final DateFormat _oldDateFormat =
+		DateFormatFactoryUtil.getSimpleDateFormat("yyyyMMddHHmmss");
 
 }
