920f257042ff37b76b48907c161e7f3147b3cb60
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-11-02 17:58:56 -0300

    LPS-60075 RecordSet setting was not being display on the Forms portlet

--- modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/portlet/configuration/icon/DDLRecordSetSettingsConfigurationIconFactory.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/portlet/configuration/icon/DDLRecordSetSettingsConfigurationIconFactory.java
@@ -14,6 +14,7 @@
 
 package com.liferay.dynamic.data.lists.form.web.portlet.configuration.icon;
 
+import com.liferay.dynamic.data.lists.form.web.constants.DDLFormPortletKeys;
 import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIconFactory;
 import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
 import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIconFactory;
@@ -25,7 +26,14 @@ import org.osgi.service.component.annotations.Component;
 /**
  * @author Marcellus Tavares
  */
-@Component(immediate = true, service = PortletConfigurationIconFactory.class)
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + DDLFormPortletKeys.DYNAMIC_DATA_LISTS_FORM_ADMIN,
+		"path=/admin/edit_record_set.jsp"
+	},
+	service = PortletConfigurationIconFactory.class
+)
 public class DDLRecordSetSettingsConfigurationIconFactory
 	extends BasePortletConfigurationIconFactory {
 
