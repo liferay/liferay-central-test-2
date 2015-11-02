c5c864a68b1db092319e16960967249e3bc1e1a7
Brian Chan <brian.chan@liferay.com>
2015-11-02 12:41:20 -0800

    LPS-60056 one more step

--- modules/apps/wiki/wiki-service/src/main/resources/com/liferay/wiki/upgrade/v1_0_0/dependencies/update.sql
+++ modules/apps/wiki/wiki-service/src/main/resources/com/liferay/wiki/upgrade/v1_0_0/dependencies/update.sql
@@ -1,3 +1,5 @@
 alter table WikiPageResource add groupId LONG;
 
+update WikiPageResource set groupId = (select max(groupId) from WikiPage where WikiPage.resourcePrimKey = WikiPageResource.resourcePrimKey);
+
 COMMIT_TRANSACTION;
