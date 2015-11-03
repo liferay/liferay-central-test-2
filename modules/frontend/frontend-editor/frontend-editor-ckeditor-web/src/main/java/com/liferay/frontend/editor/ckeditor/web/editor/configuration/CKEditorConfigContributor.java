4463b2d7cec2ea9312b398b599af04f290c9296f
Roberto Díaz <roberto.diaz@liferay.com>
2015-11-03 18:45:38 +0100

    LPS-59436 Use the plugin everywhere

--- modules/frontend/frontend-editor/frontend-editor-ckeditor-web/src/main/java/com/liferay/frontend/editor/ckeditor/web/editor/configuration/CKEditorConfigContributor.java
+++ modules/frontend/frontend-editor/frontend-editor-ckeditor-web/src/main/java/com/liferay/frontend/editor/ckeditor/web/editor/configuration/CKEditorConfigContributor.java
@@ -275,7 +275,7 @@ public class CKEditorConfigContributor extends BaseCKEditorConfigContributor {
 		String buttons = "['Table', '-', 'ImageSelector',";
 
 		if (XugglerUtil.isEnabled()) {
-			buttons +=" 'Audio', 'Video',";
+			buttons += " 'AudioSelector', 'VideoSelector',";
 		}
 
 		buttons += " 'Flash', '-', 'LiferayPageBreak', '-', " +
