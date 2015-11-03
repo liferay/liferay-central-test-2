65c8885a4112d07c1f7c90e24aad981ccaee6f61
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2015-11-03 09:31:16 +0100

    LPS-60101 Register the upgrades using the new approach

--- modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/upgrade/DDLServiceUpgrade.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/upgrade/DDLServiceUpgrade.java
@@ -17,56 +17,29 @@ package com.liferay.dynamic.data.lists.upgrade;
 import com.liferay.dynamic.data.lists.upgrade.v1_0_0.UpgradeClassNames;
 import com.liferay.dynamic.data.lists.upgrade.v1_0_0.UpgradeDynamicDataLists;
 import com.liferay.dynamic.data.lists.upgrade.v1_0_0.UpgradeLastPublishDate;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
-import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Marcellus Tavares
  */
-@Component(immediate = true, service = DDLServiceUpgrade.class)
-public class DDLServiceUpgrade {
+@Component(immediate = true)
+public class DDLServiceUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"com.liferay.dynamic.data.lists.service", "0.0.1", "1.0.0",
+			new UpgradeClassNames(), new UpgradeDynamicDataLists(),
+			new UpgradeLastPublishDate());
+	}
 
 	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
 	protected void setModuleServiceLifecycle(
 		ModuleServiceLifecycle moduleServiceLifecycle) {
 	}
-
-	@Reference(unbind = "-")
-	protected void setReleaseLocalService(
-		ReleaseLocalService releaseLocalService) {
-
-		_releaseLocalService = releaseLocalService;
-	}
-
-	@Activate
-	protected void upgrade() throws PortalException {
-		List<UpgradeProcess> upgradeProcesses = new ArrayList<>();
-
-		upgradeProcesses.add(new UpgradeClassNames());
-		upgradeProcesses.add(new UpgradeDynamicDataLists());
-		upgradeProcesses.add(new UpgradeLastPublishDate());
-
-		for (UpgradeProcess upgradeProcess : upgradeProcesses) {
-			if (_log.isDebugEnabled()) {
-				_log.debug("Upgrade process " + upgradeProcess);
-			}
-		}
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		DDLServiceUpgrade.class);
-
-	private ReleaseLocalService _releaseLocalService;
 
 }
