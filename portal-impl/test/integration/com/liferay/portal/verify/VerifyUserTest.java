303b6e190331b6518d03f63617aa8fdbc2eff724
Preston-Crary <preston.crary@liferay.com>
2015-11-04 14:46:38 -0800

    LPS-60091 Add Test

--- portal-impl/test/integration/com/liferay/portal/verify/VerifyUserTest.java
+++ portal-impl/test/integration/com/liferay/portal/verify/VerifyUserTest.java
@@ -14,15 +14,26 @@
 
 package com.liferay.portal.verify;
 
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.verify.test.BaseVerifyProcessTestCase;
 
+import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
+import org.junit.Test;
 
 /**
  * @author Manuel de la Peña
+ * @author Preston Crary
  */
 public class VerifyUserTest extends BaseVerifyProcessTestCase {
 
@@ -31,9 +42,33 @@ public class VerifyUserTest extends BaseVerifyProcessTestCase {
 	public static final AggregateTestRule aggregateTestRule =
 		new LiferayIntegrationTestRule();
 
+	@Test
+	public void testVerifyInactive() throws Exception {
+		_user = UserTestUtil.addUser();
+
+		UserLocalServiceUtil.updateStatus(
+			_user.getUserId(), WorkflowConstants.STATUS_INACTIVE,
+			new ServiceContext());
+
+		Group group = _user.getGroup();
+
+		group.setActive(true);
+
+		GroupLocalServiceUtil.updateGroup(group);
+
+		doVerify();
+
+		group = _user.getGroup();
+
+		Assert.assertFalse(group.isActive());
+	}
+
 	@Override
 	protected VerifyProcess getVerifyProcess() {
 		return new VerifyUser();
 	}
+
+	@DeleteAfterTestRun
+	private User _user;
 
 }
