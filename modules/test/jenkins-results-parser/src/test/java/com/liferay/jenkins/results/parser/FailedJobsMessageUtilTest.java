4ab3dfa77cfbec5c059fcd3090a5f31a21397b51
Peter Yoo <peter.yoo@liferay.com>
2015-11-04 09:13:42 -0800

    LRQA-18967 - Created FailedJobsMessageUtilTest class

---
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/FailedJobsMessageUtilTest.java
@@ -0,0 +1,20 @@
+package com.liferay.jenkins.results.parser;
+
+import java.io.File;
+import java.net.URL;
+
+public class FailedJobsMessageUtilTest extends BaseJenkinsResultsParserTestCase {
+
+	@Override
+	protected void downloadSample(File sampleDir, URL url) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected String getMessage(String urlString) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
