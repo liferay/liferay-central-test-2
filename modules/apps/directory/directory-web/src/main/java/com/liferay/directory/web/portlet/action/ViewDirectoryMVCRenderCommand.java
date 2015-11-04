fdd3dd0bce18a550b9fc7664191bbff9003d8449
peterfellwock <peter.fellwock@liferay.com>
2015-11-03 16:28:54 -0800

    LPS-51748 Fix and OSGi-fy MVCAction

--- modules/apps/directory/directory-web/src/main/java/com/liferay/directory/web/portlet/action/ViewDirectoryMVCRenderCommand.java
+++ modules/apps/directory/directory-web/src/main/java/com/liferay/directory/web/portlet/action/ViewDirectoryMVCRenderCommand.java
@@ -12,19 +12,21 @@
  * details.
  */
 
-package com.liferay.portlet.directory.action;
+package com.liferay.directory.web.portlet.action;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
-import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
 import com.liferay.portal.util.PortletKeys;
 
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
+import org.osgi.service.component.annotations.Component;
+
 /**
  * @author Peter Fellwock
  */
-@OSGiBeanProperties(
+@Component(
+	immediate = true,
 	property = {
 		"javax.portlet.name=" + PortletKeys.DIRECTORY,
 		"javax.portlet.name=" + PortletKeys.FRIENDS_DIRECTORY,
@@ -40,7 +42,7 @@ public class ViewDirectoryMVCRenderCommand implements MVCRenderCommand {
 	public String render(
 		RenderRequest renderRequest, RenderResponse renderResponse) {
 
-		return "/html/portlet/directory/view.jsp";
+		return "/view.jsp";
 	}
 
 }
