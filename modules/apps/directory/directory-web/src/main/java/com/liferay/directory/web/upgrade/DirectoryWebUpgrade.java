93761e99fca97e499e83573ba66cf155dc30a1fe
peterfellwock <peter.fellwock@liferay.com>
2015-11-03 16:29:31 -0800

    LPS-51748 Upgrade Portlet IDs

---
+++ modules/apps/directory/directory-web/src/main/java/com/liferay/directory/web/upgrade/DirectoryWebUpgrade.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.directory.web.upgrade;
+
+import com.liferay.directory.web.constants.DirectoryPortletKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.portal.upgrade.util.UpgradePortletId;
+
+import java.util.Collections;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Peter Fellwock
+ */
+@Component(immediate = true, service = DirectoryWebUpgrade.class)
+public class DirectoryWebUpgrade {
+
+	@Reference(unbind = "-")
+	protected void setReleaseLocalService(
+		ReleaseLocalService releaseLocalService) {
+
+		_releaseLocalService = releaseLocalService;
+	}
+
+	@Reference(target = "(original.bean=*)", unbind = "-")
+	protected void setServletContext(ServletContext servletContext) {
+	}
+
+	@Activate
+	protected void upgrade() throws PortalException {
+		UpgradePortletId upgradePortletId = new UpgradePortletId() {
+
+			@Override
+			protected String[][] getRenamePortletIdsArray() {
+				return new String[][] {
+					new String[] {
+						"11", DirectoryPortletKeys.DIRECTORY
+					},
+					new String[] {
+						"186", DirectoryPortletKeys.FRIENDS_DIRECTORY
+					},
+					new String[] {
+						"187", DirectoryPortletKeys.SITE_MEMBERS_DIRECTORY
+					},
+					new String[] {
+						"188", DirectoryPortletKeys.MY_SITES_DIRECTORY
+					}
+				};
+			}
+
+		};
+
+		_releaseLocalService.updateRelease(
+			"com.liferay.directory.web",
+			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 1,
+			false);
+	}
+
+	private ReleaseLocalService _releaseLocalService;
+
+}
