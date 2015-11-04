9d69ed92fea619ffdef72c8d1e5e8df50d98dcba
Leonardo Barros <leonardo.barros@liferay.com>
2015-11-04 09:02:10 -0300

    LPS-60133 Add REST data provider

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-data-provider-rest-impl/src/main/java/com/liferay/dynamic/data/mapping/data/provider/rest/DDMRESTDataProvider.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.data.provider.rest;
+
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProvider;
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderContext;
+import com.liferay.dynamic.data.mapping.data.provider.rest.DDMRESTDataProviderSettings.RESTSettings;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.util.KeyValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jodd.http.HttpRequest;
+import jodd.http.HttpResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marcellus Tavares
+ */
+@Component(immediate = true, property = "ddm.data.provider.name=rest")
+public class DDMRESTDataProvider implements DDMDataProvider {
+
+	@Override
+	public List<KeyValuePair> getData(
+			DDMDataProviderContext ddmDataProviderContext)
+		throws Exception {
+
+		RESTSettings restSettings = ddmDataProviderContext.getSettings(
+			RESTSettings.class);
+
+		HttpRequest httpRequest = HttpRequest.get(restSettings.url());
+
+		httpRequest.basicAuthentication(
+			restSettings.username(), restSettings.password());
+		httpRequest.query(ddmDataProviderContext.getParameters());
+
+		HttpResponse httpResponse = httpRequest.send();
+
+		JSONArray jsonArray = _jsonFactory.createJSONArray(httpResponse.body());
+
+		List<KeyValuePair> results = new ArrayList<>();
+
+		for (int i = 0; i < jsonArray.length(); i++) {
+			JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+			String key = jsonObject.getString(restSettings.key());
+			String value = jsonObject.getString(restSettings.value());
+
+			results.add(new KeyValuePair(key, value));
+		}
+
+		return results;
+	}
+
+	@Reference
+	protected void setJSONFactory(JSONFactory jsonFactory) {
+		_jsonFactory = jsonFactory;
+	}
+
+	private JSONFactory _jsonFactory;
+
+}
---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-data-provider-rest-impl/src/main/java/com/liferay/dynamic/data/mapping/data/provider/rest/DDMRESTDataProviderSettings.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.data.provider.rest;
+
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderSettings;
+import com.liferay.dynamic.data.mapping.registry.annotations.DDMForm;
+import com.liferay.dynamic.data.mapping.registry.annotations.DDMFormField;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marcellus Tavares
+ */
+@Component(immediate = true, property = "ddm.data.provider.name=rest")
+public class DDMRESTDataProviderSettings implements DDMDataProviderSettings {
+
+	@Override
+	public Class<?> getSettings() {
+		return RESTSettings.class;
+	}
+
+	@DDMForm
+	public interface RESTSettings {
+
+		@DDMFormField
+		public String key();
+
+		@DDMFormField
+		public String password();
+
+		@DDMFormField
+		public String url();
+
+		@DDMFormField
+		public String username();
+
+		@DDMFormField
+		public String value();
+
+	}
+
+}
