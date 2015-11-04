a1cf1231cef2cc0ee746752f16d0b008a9b73e87
shuyangzhou <shuyang.zhou@liferay.com>
2015-11-03 21:40:36 -0800

    LPS-60171 Properly unwrap InvocationTargetException

--- portal-test/src/com/liferay/portal/kernel/test/ReflectionTestUtil.java
+++ portal-test/src/com/liferay/portal/kernel/test/ReflectionTestUtil.java
@@ -18,6 +18,7 @@ import com.liferay.portal.kernel.util.ReflectionUtil;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import java.util.Arrays;
@@ -162,6 +163,9 @@ public class ReflectionTestUtil {
 		try {
 			return (T)method.invoke(null, parameters);
 		}
+		catch (InvocationTargetException ite) {
+			return ReflectionUtil.throwException(ite.getCause());
+		}
 		catch (Exception e) {
 			return ReflectionUtil.throwException(e);
 		}
@@ -177,6 +181,9 @@ public class ReflectionTestUtil {
 		try {
 			return (T)method.invoke(instance, parameters);
 		}
+		catch (InvocationTargetException ite) {
+			return ReflectionUtil.throwException(ite.getCause());
+		}
 		catch (Exception e) {
 			return ReflectionUtil.throwException(e);
 		}
@@ -191,6 +198,9 @@ public class ReflectionTestUtil {
 
 		try {
 			return (T)method.invoke(instance, parameters);
+		}
+		catch (InvocationTargetException ite) {
+			return ReflectionUtil.throwException(ite.getCause());
 		}
 		catch (Exception e) {
 			return ReflectionUtil.throwException(e);
