244606623804be65cdf2e5fe39e86c76ffcc3441
Peter Yoo <peter.yoo@liferay.com>
2015-11-04 09:29:23 -0800

    LRQA-18967 - Direct copy paste of some methods from UnstableMessageUtilTest

--- modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/FailedJobsMessageUtilTest.java
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/FailedJobsMessageUtilTest.java
@@ -2,15 +2,69 @@ package com.liferay.jenkins.results.parser;
 
 import java.io.File;
 import java.net.URL;
+import java.net.URLDecoder;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class FailedJobsMessageUtilTest extends BaseJenkinsResultsParserTestCase {
 
 	@Override
 	protected void downloadSample(File sampleDir, URL url) throws Exception {
-		// TODO Auto-generated method stub
+		downloadSampleURL(sampleDir, url, "/api/json");
+		downloadSampleURL(sampleDir, url, "/testReport/api/json");
 
+		downloadSampleAxisURLs(sampleDir, new File(sampleDir, "/api/json"));
 	}
 
+	protected void downloadSample(
+			String sampleKey, String buildNumber, String jobName,
+			String hostName)
+		throws Exception {
+
+		String urlString =
+			"https://${hostName}.liferay.com/job/${jobName}/${buildNumber}/";
+
+		urlString = replaceToken(urlString, "buildNumber", buildNumber);
+		urlString = replaceToken(urlString, "hostName", hostName);
+		urlString = replaceToken(urlString, "jobName", jobName);
+
+		URL url = createURL(urlString);
+
+		downloadSample(sampleKey, url);
+	}
+
+	protected void downloadSampleAxisURLs(File sampleDir, File jobJSONFile)
+		throws Exception {
+
+		JSONObject jobJSONObject = JenkinsResultsParserUtil.toJSONObject(
+			JenkinsResultsParserUtil.getLocalURL(toURLString(jobJSONFile)));
+
+		int number = jobJSONObject.getInt("number");
+
+		JSONArray runsJSONArray = jobJSONObject.getJSONArray("runs");
+
+		for (int i = 0; i < runsJSONArray.length(); i++) {
+			JSONObject runJSONObject = runsJSONArray.getJSONObject(i);
+
+			if (number != runJSONObject.getInt("number")) {
+				continue;
+			}
+
+			String runURLString = URLDecoder.decode(
+				runJSONObject.getString("url"), "UTF-8");
+
+			File runDir = new File(sampleDir, "run-" + i + "/" + number + "/");
+
+			downloadSampleURL(runDir, createURL(runURLString), "/api/json");
+			downloadSampleURL(
+				runDir, createURL(runURLString), "/testReport/api/json");
+
+			runJSONObject.put("url", toURLString(runDir));
+		}
+
+		write(jobJSONFile, jobJSONObject.toString(4));
+	}
 	@Override
 	protected String getMessage(String urlString) throws Exception {
 		// TODO Auto-generated method stub
