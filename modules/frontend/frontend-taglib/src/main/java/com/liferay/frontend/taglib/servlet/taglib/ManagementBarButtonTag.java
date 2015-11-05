df5fc239a038b934ee4c252960ae9926d08ede60
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-11-05 10:07:01 +0100

    LPS-60213 Creates new taglib to encapsulate management bar buttons

---
+++ modules/frontend/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/ManagementBarButtonTag.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.servlet.taglib;
+
+import com.liferay.frontend.taglib.servlet.ServletContextUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.taglib.util.IncludeTag;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class ManagementBarButtonTag extends IncludeTag {
+
+	@Override
+	public int doEndTag() throws JspException {
+		return super.doEndTag();
+	}
+
+	@Override
+	public int doStartTag() {
+		return EVAL_BODY_INCLUDE;
+	}
+
+	public void setActive(boolean active) {
+		_active = active;
+	}
+
+	public void setHref(String href) {
+		_href = href;
+	}
+
+	public void setIconCssClass(String iconCssClass) {
+		_iconCssClass = iconCssClass;
+	}
+
+	public void setId(String id) {
+		_id = id;
+	}
+
+	@Override
+	public void setPageContext(PageContext pageContext) {
+		super.setPageContext(pageContext);
+
+		servletContext = ServletContextUtil.getServletContext();
+	}
+
+	@Override
+	protected void cleanUp() {
+		_active = false;
+		_href = null;
+		_iconCssClass = StringPool.BLANK;
+		_id = StringPool.BLANK;
+	}
+
+	@Override
+	protected String getPage() {
+		return _PAGE;
+	}
+
+	@Override
+	protected boolean isCleanUpSetAttributes() {
+		return _CLEAN_UP_SET_ATTRIBUTES;
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest request) {
+		request.setAttribute(
+			"liferay-frontend:management-bar-button:active", _active);
+		request.setAttribute(
+			"liferay-frontend:management-bar-button:href", _href);
+		request.setAttribute(
+			"liferay-frontend:management-bar-button:iconCssClass",
+			_iconCssClass);
+		request.setAttribute("liferay-frontend:management-bar-button:id", _id);
+	}
+
+	private static final boolean _CLEAN_UP_SET_ATTRIBUTES = true;
+
+	private static final String _PAGE = "/management_bar_button/page.jsp";
+
+	private boolean _active;
+	private String _href;
+	private String _iconCssClass;
+	private String _id;
+
+}
