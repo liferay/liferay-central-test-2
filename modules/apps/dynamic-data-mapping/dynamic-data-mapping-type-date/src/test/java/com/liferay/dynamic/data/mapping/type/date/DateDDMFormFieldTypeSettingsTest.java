d7bebca6653d3b1be74379ed9aeede95aeae7d61
Bruno Basto <bruno.basto@liferay.com>
2015-11-06 16:28:42 -0300

    LPS-59900 Adding tests

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-date/src/test/java/com/liferay/dynamic/data/mapping/type/date/DateDDMFormFieldTypeSettingsTest.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.type.date;
+
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMFormField;
+import com.liferay.dynamic.data.mapping.registry.DDMFormFactory;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.SetUtil;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mock;
+
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ * @author Bruno Basto
+ */
+@RunWith(PowerMockRunner.class)
+public class DateDDMFormFieldTypeSettingsTest extends PowerMockito {
+
+	@Before
+	public void setUp() {
+		setUpLanguageUtil();
+	}
+
+	@Test
+	public void testCreateDateDDMFormFieldTypeSettingsDDMForm() {
+		DDMForm ddmForm = DDMFormFactory.create(
+			DateDDMFormFieldTypeSettings.class);
+
+		Map<String, DDMFormField> ddmFormFieldsMap =
+			ddmForm.getDDMFormFieldsMap(false);
+
+		DDMFormField maskDDMFormField = ddmFormFieldsMap.get("mask");
+
+		Assert.assertNotNull(maskDDMFormField);
+		Assert.assertEquals("string", maskDDMFormField.getDataType());
+		Assert.assertEquals("select", maskDDMFormField.getType());
+		Assert.assertEquals(false, maskDDMFormField.isLocalizable());
+
+		DDMFormField predefinedValueDDMFormField = ddmFormFieldsMap.get(
+			"predefinedValue");
+
+		Assert.assertNotNull(predefinedValueDDMFormField);
+		Assert.assertEquals(
+			"string", predefinedValueDDMFormField.getDataType());
+		Assert.assertEquals("date", predefinedValueDDMFormField.getType());
+		Assert.assertEquals(true, predefinedValueDDMFormField.isLocalizable());
+	}
+
+	protected void setUpLanguageUtil() {
+		Set<Locale> availableLocales = SetUtil.fromArray(
+			new Locale[] {LocaleUtil.US});
+
+		when(
+			language.getAvailableLocales()
+		).thenReturn(
+			availableLocales
+		);
+
+		LanguageUtil languageUtil = new LanguageUtil();
+
+		languageUtil.setLanguage(language);
+	}
+
+	@Mock
+	protected Language language;
+
+}
