fb63b6df61f66d100923671bf245477056f77a79
michael.bradford@liferay.com <michael.bradford@liferay.com>
2015-11-10 12:18:02 -0800

    LPS-60328 - update NavigationTag package

--- modules/apps/site-navigation/site-navigation-taglib/src/main/java/com/liferay/site/navigation/taglib/servlet/taglib/NavigationTag.java
+++ modules/apps/site-navigation/site-navigation-taglib/src/main/java/com/liferay/site/navigation/taglib/servlet/taglib/NavigationTag.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.taglib.ui;
+package com.liferay.site.navigation.taglib.servlet.taglib;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
