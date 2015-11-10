d21457d9f83dd7ee65c8850b4788b5f9ab98663b
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-11-10 17:26:59 +0100

    LPS-60604 Implement user vertical card

--- modules/frontend/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/UserVerticalCardTag.java
+++ modules/frontend/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/UserVerticalCardTag.java
@@ -14,73 +14,76 @@
 
 package com.liferay.frontend.taglib.servlet.taglib;
 
-import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.taglib.util.LexiconUtil;
 
 import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Eudaldo Alonso
  */
-public class UserVerticalCardTag extends CardTag {
+public class UserVerticalCardTag extends VerticalCardTag {
 
-	public void setBackgroundImage(boolean backgroundImage) {
-		_backgroundImage = backgroundImage;
-	}
-
-	public void setFooter(String footer) {
-		_footer = footer;
-	}
-
-	public void setHeader(String header) {
-		_header = header;
-	}
-
-	public void setStickerBottom(String stickerBottom) {
-		_stickerBottom = stickerBottom;
-	}
-
-	public void setSubtitle(String subtitle) {
-		_subtitle = HtmlUtil.unescape(subtitle);
-	}
-
-	public void setTitle(String title) {
-		_title = HtmlUtil.unescape(title);
+	public void setUserId(long userId) {
+		_userId = userId;
 	}
 
 	@Override
 	protected void cleanUp() {
-		_backgroundImage = true;
-		_footer = null;
-		_header = null;
-		_stickerBottom = null;
-		_subtitle = null;
-		_title = null;
+		_userId = 0;
 	}
 
 	@Override
 	protected String getPage() {
-		return "/card/vertical_card/page.jsp";
+		return "/card/user_vertical_card/page.jsp";
+	}
+
+	protected User getUser() {
+		return UserLocalServiceUtil.fetchUser(_userId);
+	}
+
+	@Override
+	protected boolean isCleanUpSetAttributes() {
+		return true;
 	}
 
 	@Override
 	protected void setAttributes(HttpServletRequest request) {
 		super.setAttributes(request);
 
+		User user = getUser();
+
 		request.setAttribute(
-			"liferay-frontend:card:backgroundImage", _backgroundImage);
-		request.setAttribute("liferay-frontend:card:footer", _footer);
-		request.setAttribute("liferay-frontend:card:header", _header);
-		request.setAttribute(
-			"liferay-frontend:card:stickerBottom", _stickerBottom);
-		request.setAttribute("liferay-frontend:card:subtitle", _subtitle);
-		request.setAttribute("liferay-frontend:card:title", _title);
+			"liferay-frontend:card:colorCssClass",
+			LexiconUtil.getUserColorCssClass(user));
+
+		if ((user != null) && (user.getPortraitId() > 0)) {
+			ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+			try {
+				request.setAttribute(
+					"liferay-frontend:card:portraitURL",
+					user.getPortraitURL(themeDisplay));
+			}
+			catch (PortalException pe) {
+			}
+		}
+
+		String initials = StringPool.BLANK;
+
+		if (user != null) {
+			initials = user.getInitials();
+		}
+
+		request.setAttribute("liferay-frontend:card:userInitials", initials);
 	}
 
-	private boolean _backgroundImage = true;
-	private String _footer;
-	private String _header;
-	private String _stickerBottom;
-	private String _subtitle;
-	private String _title;
+	private long _userId;
 
 }
