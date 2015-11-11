c9da2d2fdbdb403f2d681adf77c3830ebb20eb0e
Carlos Sierra <carlos.sierra@liferay.com>
2015-11-11 15:46:18 +0100

    LPS-60355 Create a DummyClassLoader and util to apply it
    Putting this class loader in the class loading path prevents
    BundleLoader#isRequestFromVM() from returning true.

---
+++ modules/portal/portal-wab-extender/src/main/java/com/liferay/portal/wab/extender/internal/adapter/DummyClassLoader.java
@@ -0,0 +1,131 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.wab.extender.internal.adapter;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.net.URL;
+
+import java.util.Enumeration;
+
+/**
+* This class exits with the sole purpose of altering the BundleLoader logic in
+* isRequestFromVM() so it returns false. It is essential that the methods are
+* implemented even though what they do is just calling the parent class.
+*
+* @author Carlos Sierra Andrés
+*/
+public class DummyClassLoader extends ClassLoader {
+
+	public DummyClassLoader(ClassLoader contextClassLoader) {
+		super(contextClassLoader);
+	}
+
+	@Override
+	public void clearAssertionStatus() {
+		super.clearAssertionStatus();
+	}
+
+	@Override
+	public URL getResource(String name) {
+		return super.getResource(name);
+	}
+
+	@Override
+	public InputStream getResourceAsStream(String name) {
+		return super.getResourceAsStream(name);
+	}
+
+	@Override
+	public Enumeration<URL> getResources(String name) throws IOException {
+		return super.getResources(name);
+	}
+
+	@Override
+	public Class<?> loadClass(String name) throws ClassNotFoundException {
+		return super.loadClass(name);
+	}
+
+	@Override
+	public void setClassAssertionStatus(String className, boolean enabled) {
+		super.setClassAssertionStatus(className, enabled);
+	}
+
+	@Override
+	public void setDefaultAssertionStatus(boolean enabled) {
+		super.setDefaultAssertionStatus(enabled);
+	}
+
+	@Override
+	public void setPackageAssertionStatus(String packageName, boolean enabled) {
+		super.setPackageAssertionStatus(packageName, enabled);
+	}
+
+	@Override
+	protected Package definePackage(
+			String name, String specTitle, String specVersion,
+			String specVendor, String implTitle, String implVersion,
+			String implVendor, URL sealBase)
+		throws IllegalArgumentException {
+
+		return super.definePackage(
+			name, specTitle, specVersion, specVendor, implTitle, implVersion,
+			implVendor, sealBase);
+	}
+
+	@Override
+	protected Class<?> findClass(String name) throws ClassNotFoundException {
+		return super.findClass(name);
+	}
+
+	@Override
+	protected String findLibrary(String libname) {
+		return super.findLibrary(libname);
+	}
+
+	@Override
+	protected URL findResource(String name) {
+		return super.findResource(name);
+	}
+
+	@Override
+	protected Enumeration<URL> findResources(String name) throws IOException {
+		return super.findResources(name);
+	}
+
+	@Override
+	protected Object getClassLoadingLock(String className) {
+		return super.getClassLoadingLock(className);
+	}
+
+	@Override
+	protected Package getPackage(String name) {
+		return super.getPackage(name);
+	}
+
+	@Override
+	protected Package[] getPackages() {
+		return super.getPackages();
+	}
+
+	@Override
+	protected Class<?> loadClass(String name, boolean resolve)
+		throws ClassNotFoundException {
+
+		return super.loadClass(name, resolve);
+	}
+
+}
---
+++ modules/portal/portal-wab-extender/src/main/java/com/liferay/portal/wab/extender/internal/adapter/TCCLUtil.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.wab.extender.internal.adapter;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public class TCCLUtil {
+
+	public static void wrapTCCL(Callable<Void> callable) throws Exception {
+		Thread thread = Thread.currentThread();
+
+		ClassLoader contextClassLoader = thread.getContextClassLoader();
+
+		//This classLoader is needed to disable Equinox' boot delegation
+		ClassLoader classLoader = new DummyClassLoader(contextClassLoader);
+
+		try {
+			thread.setContextClassLoader(classLoader);
+
+			callable.call();
+		}
+		finally {
+			thread.setContextClassLoader(contextClassLoader);
+		}
+	}
+
+}
