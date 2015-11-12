0fbb7bd0bb31e8aabfe70dc668eb662b72da17c5
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-11-12 09:46:45 -0300

    LPS-60393 Update date field type

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-date/src/main/java/com/liferay/dynamic/data/mapping/type/date/DateDDMFormFieldValueRenderer.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-date/src/main/java/com/liferay/dynamic/data/mapping/type/date/DateDDMFormFieldValueRenderer.java
@@ -14,8 +14,8 @@
 
 package com.liferay.dynamic.data.mapping.type.date;
 
+import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldValueRenderer;
 import com.liferay.dynamic.data.mapping.model.Value;
-import com.liferay.dynamic.data.mapping.registry.DDMFormFieldValueRenderer;
 import com.liferay.dynamic.data.mapping.storage.DDMFormFieldValue;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
