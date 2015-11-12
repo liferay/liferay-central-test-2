fa1f89e147920a4fa588830531a8637f622bf696
shuyangzhou <shuyang.zhou@liferay.com>
2015-11-12 13:55:25 -0800

    LPS-60473 Inline populateTableRows, no logic change.

--- portal-impl/src/com/liferay/portal/upgrade/util/Table.java
+++ portal-impl/src/com/liferay/portal/upgrade/util/Table.java
@@ -523,7 +523,9 @@ public class Table {
 					ps.addBatch();
 
 					if (count == _BATCH_SIZE) {
-						populateTableRows(ps, true);
+						ps.executeBatch();
+
+						ps.close();
 
 						count = 0;
 					}
@@ -532,13 +534,17 @@ public class Table {
 					}
 				}
 				else {
-					populateTableRows(ps, false);
+					ps.executeUpdate();
+
+					ps.close();
 				}
 			}
 
 			if (databaseMetaData.supportsBatchUpdates()) {
 				if (count != 0) {
-					populateTableRows(ps, true);
+					ps.executeBatch();
+
+					ps.close();
 				}
 			}
 		}
