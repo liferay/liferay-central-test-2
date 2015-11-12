ab272a8b15da8ea51a8a9db440af2a07e512ac0d
Brian Chan <brian.chan@liferay.com>
2015-11-12 10:27:15 -0800

    LPS-60403 Let's make things consistent

--- tools/servers/jetty/bin/run.bat
+++ tools/servers/jetty/bin/run.bat
@@ -2,7 +2,7 @@
 
 if "" == "%JAVA_HOME%" goto errorJavaHome
 
-set "JAVA_OPTS=-Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true @java.security.config@ -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
+set "JAVA_OPTS=-Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true @java.security.config@ -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=384m"
 
 "%JAVA_HOME%/bin/java" %JAVA_OPTS% -jar ../start.jar
 
--- tools/servers/jetty/bin/run.sh
+++ tools/servers/jetty/bin/run.sh
@@ -6,6 +6,6 @@ then
 	exit
 fi
 
-export JAVA_OPTS="-Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true @java.security.config@ -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
+export JAVA_OPTS="-Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true @java.security.config@ -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=384m"
 
 $JAVA_HOME/bin/java $JAVA_OPTS -jar ../start.jar
