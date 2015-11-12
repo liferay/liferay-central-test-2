2ee03231a8238cf263dc73d9c73a44ef1d2512c9
Julio Camarero <julio.camarero@liferay.com>
2015-11-12 12:42:40 +0100

    LPS-57564 delete code not needed anymore

--- util-taglib/src/com/liferay/taglib/ui/LayoutsTreeTag.java
+++ util-taglib/src/com/liferay/taglib/ui/LayoutsTreeTag.java
@@ -69,10 +69,6 @@ public class LayoutsTreeTag extends IncludeTag {
 		_linkTemplate = linkTemplate;
 	}
 
-	public void setPortletId(String portletId) {
-		_portletId = portletId;
-	}
-
 	public void setPortletURL(PortletURL portletURL) {
 		_portletURL = portletURL;
 	}
@@ -117,7 +113,6 @@ public class LayoutsTreeTag extends IncludeTag {
 		_expandFirstNode = true;
 		_groupId = 0;
 		_incomplete = true;
-		_portletId = null;
 		_linkTemplate = null;
 		_portletURL = null;
 		_privateLayout = false;
@@ -209,7 +204,6 @@ public class LayoutsTreeTag extends IncludeTag {
 			"liferay-ui:layouts-tree:linkTemplate",
 			String.valueOf(_linkTemplate));
 		request.setAttribute("liferay-ui:layouts-tree:modules", getModules());
-		request.setAttribute("liferay-ui:layouts-tree:portletId", _portletId);
 		request.setAttribute("liferay-ui:layouts-tree:portletURL", _portletURL);
 		request.setAttribute(
 			"liferay-ui:layouts-tree:privateLayout",
@@ -241,7 +235,6 @@ public class LayoutsTreeTag extends IncludeTag {
 	private long _groupId;
 	private boolean _incomplete = true;
 	private String _linkTemplate;
-	private String _portletId;
 	private PortletURL _portletURL;
 	private boolean _privateLayout;
 	private String _rootLinkTemplate;
