8cdf905064b840160050e71fdc6a559d2124c0c8
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-11-12 15:13:30 -0300

    LPS-60170 Regenerate services

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMDataProviderInstanceLocalServiceImpl.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.service.impl;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.dynamic.data.mapping.service.base.DDMDataProviderInstanceLocalServiceBaseImpl;
+
+/**
+ * The implementation of the d d m data provider instance local service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.dynamic.data.mapping.service.DDMDataProviderInstanceLocalService} interface.
+ *
+ * <p>
+ * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
+ * </p>
+ *
+ * @author Brian Wing Shun Chan
+ * @see DDMDataProviderInstanceLocalServiceBaseImpl
+ * @see com.liferay.dynamic.data.mapping.service.DDMDataProviderInstanceLocalServiceUtil
+ */
+@ProviderType
+public class DDMDataProviderInstanceLocalServiceImpl
+	extends DDMDataProviderInstanceLocalServiceBaseImpl {
+	/**
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never reference this class directly. Always use {@link com.liferay.dynamic.data.mapping.service.DDMDataProviderInstanceLocalServiceUtil} to access the d d m data provider instance local service.
+	 */
+}
