beefcccda14ccdd01d4490a738d4ec93a86160db
Leonardo Barros <leonardo.barros@liferay.com>
2015-11-12 14:17:08 -0300

    LPS-60170 Implement remote and local services

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMDataProviderInstanceLocalServiceImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMDataProviderInstanceLocalServiceImpl.java
@@ -14,30 +14,137 @@
 
 package com.liferay.dynamic.data.mapping.service.impl;
 
-import aQute.bnd.annotation.ProviderType;
-
+import com.liferay.dynamic.data.mapping.exception.DataProviderInstanceNameException;
+import com.liferay.dynamic.data.mapping.model.DDMDataProviderInstance;
 import com.liferay.dynamic.data.mapping.service.base.DDMDataProviderInstanceLocalServiceBaseImpl;
+import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
+import com.liferay.dynamic.data.mapping.validator.DDMFormValuesValidator;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.spring.extender.service.ServiceReference;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 /**
- * The implementation of the d d m data provider instance local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.dynamic.data.mapping.service.DDMDataProviderInstanceLocalService} interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
- * @author Brian Wing Shun Chan
- * @see DDMDataProviderInstanceLocalServiceBaseImpl
- * @see com.liferay.dynamic.data.mapping.service.DDMDataProviderInstanceLocalServiceUtil
+ * @author Leonardo Barros
  */
-@ProviderType
 public class DDMDataProviderInstanceLocalServiceImpl
 	extends DDMDataProviderInstanceLocalServiceBaseImpl {
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Always use {@link com.liferay.dynamic.data.mapping.service.DDMDataProviderInstanceLocalServiceUtil} to access the d d m data provider instance local service.
-	 */
+
+	@Override
+	public DDMDataProviderInstance addDataProviderInstance(
+			long userId, long groupId, Map<Locale, String> nameMap,
+			Map<Locale, String> descriptionMap, String definition, String type,
+			DDMFormValues ddmFormValues, ServiceContext serviceContext)
+		throws PortalException {
+
+		User user = userPersistence.findByPrimaryKey(userId);
+
+		ddmFormValuesValidator.validate(ddmFormValues);
+
+		validateName(nameMap);
+
+		long dataProviderInstanceId = counterLocalService.increment();
+
+		DDMDataProviderInstance ddmDataProviderInstance =
+			ddmDataProviderInstancePersistence.create(dataProviderInstanceId);
+
+		ddmDataProviderInstance.setUuid(serviceContext.getUuid());
+		ddmDataProviderInstance.setGroupId(groupId);
+		ddmDataProviderInstance.setCompanyId(user.getCompanyId());
+		ddmDataProviderInstance.setUserId(user.getUserId());
+		ddmDataProviderInstance.setUserName(user.getFullName());
+		ddmDataProviderInstance.setDefinition(definition);
+		ddmDataProviderInstance.setDescriptionMap(descriptionMap);
+		ddmDataProviderInstance.setNameMap(nameMap);
+		ddmDataProviderInstance.setType(type);
+
+		ddmDataProviderInstancePersistence.update(ddmDataProviderInstance);
+
+		return ddmDataProviderInstance;
+	}
+
+	@Override
+	public List<DDMDataProviderInstance> search(
+		long companyId, long[] groupIds, String keywords, int start, int end,
+		OrderByComparator<DDMDataProviderInstance> orderByComparator) {
+
+		return ddmDataProviderInstanceFinder.findByKeywords(
+			companyId, groupIds, keywords, start, end, orderByComparator);
+	}
+
+	@Override
+	public List<DDMDataProviderInstance> search(
+		long companyId, long[] groupIds, String name, String description,
+		boolean andOperator, int start, int end,
+		OrderByComparator<DDMDataProviderInstance> orderByComparator) {
+
+		return ddmDataProviderInstanceFinder.findByC_G_N_D(
+			companyId, groupIds, name, description, andOperator, start, end,
+			orderByComparator);
+	}
+
+	@Override
+	public int searchCount(long companyId, long[] groupIds, String keywords) {
+		return ddmDataProviderInstanceFinder.countByKeywords(
+			companyId, groupIds, keywords);
+	}
+
+	@Override
+	public int searchCount(
+		long companyId, long[] groupIds, String name, String description,
+		boolean andOperator) {
+
+		return ddmDataProviderInstanceFinder.countByC_G_N_D(
+			companyId, groupIds, name, description, andOperator);
+	}
+
+	@Override
+	public DDMDataProviderInstance updateDataProviderInstance(
+			long userId, long dataProviderInstanceId,
+			Map<Locale, String> nameMap, Map<Locale, String> descriptionMap,
+			String definition, DDMFormValues ddmFormValues,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		User user = userPersistence.findByPrimaryKey(userId);
+
+		ddmFormValuesValidator.validate(ddmFormValues);
+
+		validateName(nameMap);
+
+		DDMDataProviderInstance ddmDataProviderInstance =
+			ddmDataProviderInstancePersistence.findByPrimaryKey(
+				dataProviderInstanceId);
+
+		ddmDataProviderInstance.setUserId(user.getUserId());
+		ddmDataProviderInstance.setUserName(user.getFullName());
+		ddmDataProviderInstance.setDefinition(definition);
+		ddmDataProviderInstance.setDescriptionMap(descriptionMap);
+		ddmDataProviderInstance.setNameMap(nameMap);
+
+		ddmDataProviderInstancePersistence.update(ddmDataProviderInstance);
+
+		return ddmDataProviderInstance;
+	}
+
+	protected void validateName(Map<Locale, String> nameMap)
+		throws PortalException {
+
+		String name = nameMap.get(LocaleUtil.getSiteDefault());
+
+		if (Validator.isNull(name)) {
+			throw new DataProviderInstanceNameException();
+		}
+	}
+
+	@ServiceReference(type = DDMFormValuesValidator.class)
+	protected DDMFormValuesValidator ddmFormValuesValidator;
+
 }
