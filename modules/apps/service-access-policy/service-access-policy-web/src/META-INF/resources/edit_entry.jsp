mjbradford89/liferay-portal

LPS-55368 - Switch from dynamic select boxes to inputs w/ autocomplete.  Also add 'advanced' mode.

modules/apps/service-access-policy/service-access-policy-web/src/META-INF/resources/edit_entry.jsp
@@ -23,8 +23,18 @@ long sapEntryId = ParamUtil.getLong(request, "sapEntryId");
 
 SAPEntry sapEntry = null;
 
+String allowedServiceSignatures = StringPool.BLANK;
+
 if (sapEntryId > 0) {
 	sapEntry = SAPEntryServiceUtil.getSAPEntry(sapEntryId);
+
+	allowedServiceSignatures = sapEntry.getAllowedServiceSignatures();
+}
+
+String[] allowedServiceSignaturesArray = StringUtil.splitLines(allowedServiceSignatures);
+
+if (allowedServiceSignaturesArray.length == 0) {
+	allowedServiceSignaturesArray = new String[] { StringPool.BLANK };
 }
 
 boolean defaultSAPEntry = false;
@@ -32,9 +42,6 @@ boolean defaultSAPEntry = false;
 if (sapEntry != null) {
 	defaultSAPEntry = sapEntry.isDefaultSAPEntry();
 }
-
-int[] allowedServiceSignaturesIndexes = new int[] {0};
-
 %>
 
 <liferay-ui:header
@@ -72,184 +79,227 @@ int[] allowedServiceSignaturesIndexes = new int[] {0};
 
 	<aui:input name="title" required="<%= true %>" />
 
-	<portlet:resourceURL var="getServicesURL">
-		<portlet:param name="<%= ActionRequest.ACTION_NAME %>" value="getServices" />
-		<portlet:param name="mvcPath" value="<%= StringPool.SPACE %>" />
-	</portlet:resourceURL>
+	<aui:input cssClass="hide" helpMessage="allowed-service-signatures-help" name="allowedServiceSignatures" />
+
 	<portlet:resourceURL var="getMethodsURL">
 		<portlet:param name="<%= ActionRequest.ACTION_NAME %>" value="getMethods" />
 		<portlet:param name="mvcPath" value="<%= StringPool.SPACE %>" />
 	</portlet:resourceURL>
 
-	<div id="<portlet:namespace />allowedServiceSignatures">
+	<div id="<portlet:namespace />allowedServiceSignaturesFriendlyContentBox">
 
 		<%
-		for (int i = 0; i < allowedServiceSignaturesIndexes.length; i++) {
-			int allowedServiceIndex = allowedServiceSignaturesIndexes[i];
+		for (int i = 0; i < allowedServiceSignaturesArray.length; i++) {
+			String entry = allowedServiceSignaturesArray[i];
 
-			String packageName = ParamUtil.getString(request, "packageName" + allowedServiceIndex, "0");
-			String methodName = ParamUtil.getString(request, "methodName" + allowedServiceIndex, "All methods");
+			String[] split = StringUtil.split(entry, CharPool.POUND);
+
+			String serviceClass = StringPool.BLANK;
+			String methodName = StringPool.BLANK;
+
+			if (split.length > 0) {
+				serviceClass = GetterUtil.getString(split[0], StringPool.BLANK);
+
+				if (split.length > 1) {
+					methodName = GetterUtil.getString(split[1], StringPool.BLANK);
+				}
+			}
 		%>
 
 			<div class="lfr-form-row">
 				<div class="row-fields">
 					<aui:col md="6">
-						<aui:select id='<%= "serviceClass" + allowedServiceIndex %>' name="serviceClass"></aui:select>
+						<aui:input cssClass="service-class" data-service-class="<%= serviceClass %>" id='<%= "serviceClass" + i %>' name="serviceClass" type="text" value="<%= serviceClass %>" />
 					</aui:col>
 					<aui:col md="6">
-						<aui:select id='<%= "methodName" + allowedServiceIndex %>' name="methodName"></aui:select>
+						<aui:input cssClass="method-name" id='<%= "methodName" + i %>' name="methodName" type="text" value="<%= methodName %>" />
 					</aui:col>
 				</div>
 			</div>
-
-			<aui:script use="liferay-dynamic-select,io-base">
-				function getJSON(URL, callback, data) {
-					A.io(
-						URL,
-						{
-							arguments: {
-								callback: callback
-							},
-							data: data,
-							on: {
-								success: <portlet:namespace />processRequest
-							}
-						}
-					);
-				}
-
-				function getMethods(callback, key) {
-					var dataKey = key.split('/');
-
-					getJSON(
-						'<%= getMethodsURL %>',
-						callback,
-						{
-							<portlet:namespace />context: dataKey[0],
-							<portlet:namespace />serviceClass: dataKey[1]
-						}
-					);
-				}
-
-				function getServices(callback) {
-					getJSON('<%= getServicesURL %>', callback);
-				}
-
-				new Liferay.DynamicSelect(
-					[
-						{
-							select: '<portlet:namespace />serviceClass<%= allowedServiceIndex %>',
-							selectData: getServices,
-							selectDesc: 'serviceClass',
-							selectId: 'serviceClass',
-							selectSort: '<%= true %>',
-							selectVal: '<%= packageName %>'
-						},
-						{
-							select: '<portlet:namespace />methodName<%= allowedServiceIndex %>',
-							selectData: getMethods,
-							selectDesc: 'method',
-							selectId: 'method',
-							selectVal: '<%= methodName %>'
-						}
-					]
-				);
-			</aui:script>
 
 		<%
 		}
 		%>
 
-		<aui:input name="allowedServiceSignaturesIndexes" type="hidden" value="<%= StringUtil.merge(allowedServiceSignaturesIndexes) %>" />
 	</div>
 
-	<aui:script use="liferay-auto-fields,liferay-dynamic-select,io-base">
-		function getJSON(URL, callback, data) {
-			A.io(
-				URL,
-				{
-					arguments: {
-						callback: callback
-					},
-					data: data,
-					on: {
-						success: <portlet:namespace />processRequest
-					}
-				}
-			);
-		}
+	<aui:script use="autocomplete,autocomplete-filters,io-base,liferay-auto-fields">
+		var services = <%= JSONFactoryUtil.looseSerialize(JSONWebServiceActionsManagerUtil.getServiceNames()) %>;
 
-		function getMethods(callback, key) {
-			var dataKey = key.split('/');
-
-			getJSON(
-				'<%= getMethodsURL %>',
-				callback,
-				{
-					<portlet:namespace />context: dataKey[0],
-					<portlet:namespace />serviceClass: dataKey[1]
-				}
-			);
-		}
-
-		function getServices(callback) {
-			getJSON('<%= getServicesURL %>', callback);
-		}
-
-		var allowedServiceSignatures = new Liferay.AutoFields(
+		var autoFields = new Liferay.AutoFields(
 			{
-				contentBox: '#<portlet:namespace />allowedServiceSignatures',
-				fieldIndexes: '<portlet:namespace />allowedServiceSignaturesIndexes',
+				contentBox: '#<portlet:namespace />allowedServiceSignaturesFriendlyContentBox',
 				namespace: '<portlet:namespace />',
 				on: {
-					'clone': function(event) {
-						var guid = event.guid;
-						var row = event.row;
-
-						var dynamicSelects = row.one('select[data-componentType=dynamic_select]');
-
-						if (dynamicSelects) {
-							dynamicSelects.detach('change');
-						}
-
-						var dynamicSelect = new Liferay.DynamicSelect(
-							[
-								{
-									select: '<portlet:namespace />serviceClass' + guid,
-									selectData: getServices,
-									selectDesc: 'serviceClass',
-									selectId: 'serviceClass',
-									selectSort: '<%= true %>',
-									selectVal: '0'
-								},
-								{
-									select: '<portlet:namespace />methodName' + guid,
-									selectData: getMethods,
-									selectDesc: 'method',
-									selectId: 'method',
-									selectVal: 'All methods'
-								}
-							]
-						);
-
-						dynamicSelect._updateSelect(1, []);
-					}
+					clone: function(event) {
+						initAutoCompleteRow(event.row);
+					},
+					delete: updateAdvancedModeTextarea
 				}
 			}
 		).render();
-	</aui:script>
 
-	<aui:script>
-		function <portlet:namespace />processRequest(index, data, args) {
-			if (data.response) {
-				var response = JSON.parse(data.response);
+		var rows = A.all('#<portlet:namespace />allowedServiceSignaturesFriendlyContentBox .lfr-form-row');
 
-				args.callback(response);
+		A.each(
+			rows,
+			function(item, index) {
+				initAutoCompleteRow(item);
+			}
+		);
+
+		A.one('#<portlet:namespace />allowedServiceSignaturesFriendlyContentBox').delegate('blur', updateAdvancedModeTextarea, '.service-class, .method-name');
+		A.one('#<portlet:namespace />allowedServiceSignatures').on('blur', updateFriendlyModeInputs);
+
+		function updateAdvancedModeTextarea() {
+			var updatedInput = '';
+
+			A.all('#<portlet:namespace />allowedServiceSignaturesFriendlyContentBox .lfr-form-row:not(.hide)').each(
+				function(item, index) {
+					var serviceClass = item.one('.service-class').val();
+					var methodName = item.one('.method-name').val();
+
+					updatedInput += serviceClass;
+
+					if (methodName.length > 0) {
+						updatedInput += '#' + methodName;
+					}
+
+					updatedInput += '\n';
+				}
+			);
+
+			A.one('#<portlet:namespace />allowedServiceSignatures').val(updatedInput);
+		}
+
+		function updateFriendlyModeInputs() {
+			var advancedInput = A.one('#<portlet:namespace />allowedServiceSignatures').val();
+			var contentBox = A.one('#<portlet:namespace />allowedServiceSignaturesFriendlyContentBox');
+			var entries = advancedInput.split('\n');
+			var rows = contentBox.all('.lfr-form-row:not(.hide)');
+			var rowTemplate = rows.item(0);
+
+			for (var i = 0; i < entries.length; i++) {
+				var entry = entries[i];
+
+				if (entry.length) {
+					entry = entry.split('#');
+					var row = rows.item(i);
+
+					if (!row) {
+						row = rowTemplate.clone();
+
+						contentBox.append(row);
+
+						row.all('input').val('');
+
+						initAutoCompleteRow(row);
+					}
+
+					var serviceInput = row.one('.service-class');
+					var methodInput = row.one('.method-name');
+
+					serviceInput.val(entry[0]);
+					serviceInput.attr('data-service-class', entry[0]);
+
+					if (entry[1]) {
+						methodInput.val(entry[1]);
+					}
+				}
 			}
 		}
+
+		function initAutoCompleteRow(rowNode) {
+			var serviceInput = rowNode.one('.service-class');
+			var methodInput = rowNode.one('.method-name');
+
+			new A.AutoComplete(
+				{
+					inputNode: serviceInput,
+					source: services,
+					resultFilters: 'phraseMatch',
+					resultTextLocator: 'serviceClass',
+					on: {
+						select: function(event) {
+							var result = event.result.raw;
+
+							serviceInput.attr('data-service-class', result.serviceClass);
+							serviceInput.attr('data-context', result.context);
+						}
+					}
+				}
+			).render();
+
+			new A.AutoComplete(
+				{
+					inputNode: methodInput,
+					source: function(query, callback) {
+						var context = serviceInput.attr('data-context');
+						var serviceClass = serviceInput.attr('data-service-class');
+
+						if (context.length == 0) {
+							A.Array.each(services, function(item, index) {
+								if (item.serviceClass == serviceClass) {
+									context = item.context;
+
+									serviceInput.attr('data-context', context);
+								}
+							});
+						}
+
+						if (context.length && serviceClass.length) {
+							var methodsURL = '<%= getMethodsURL %>&<portlet:namespace />serviceClass=' + serviceClass + '&<portlet:namespace />context=' + context;
+
+							A.io.request(
+								methodsURL,
+								{
+									dataType: 'JSON',
+									method: 'GET',
+									on: {
+										complete: function(event, id, xhr) {
+											var responseData = {};
+
+											try {
+												responseData = A.JSON.parse(xhr.responseText);
+											}
+											catch (e) {
+											}
+
+											callback(responseData);
+										}
+									}
+								}
+							);
+						}
+					},
+					resultFilters: 'phraseMatch',
+					resultTextLocator: 'methodName'
+				}
+			).render();
+		}
+
+		Liferay.provide(
+			window,
+			'<portlet:namespace />toggleAdvancedMode',
+			function() {
+				var A = AUI();
+
+				var advancedButton = A.one('#<portlet:namespace />advancedMode');
+				var friendlyButton = A.one('#<portlet:namespace />friendlyMode');
+				var allowedServiceSignaturesTextarea = A.one('#<portlet:namespace />allowedServiceSignatures');
+				var allowedServiceSignaturesFriendlyContentBox = A.one('#<portlet:namespace />allowedServiceSignaturesFriendlyContentBox');
+
+				advancedButton.toggleClass('hide');
+				friendlyButton.toggleClass('hide');
+				allowedServiceSignaturesTextarea.toggleClass('hide');
+				allowedServiceSignaturesFriendlyContentBox.toggleClass('hide');
+			}
+		);
 	</aui:script>
 
 	<aui:button-row>
 		<aui:button type="submit" value="save" />
+		<aui:button id="advancedMode" onClick='<%= renderResponse.getNamespace() + "toggleAdvancedMode();" %>' value="Switch to Advanced mode" />
+		<aui:button cssClass="hide" id="friendlyMode" onClick='<%= renderResponse.getNamespace() + "toggleAdvancedMode();" %>' value="Switch to Friendly mode" />
 	</aui:button-row>
 </aui:form>

liferay/liferay-central-test-2

modules/apps/service-access-policy/service-access-policy-web/src/META-INF/resources/edit_entry.jsp
@@ -23,8 +23,18 @@ long sapEntryId = ParamUtil.getLong(request, "sapEntryId");
 
 SAPEntry sapEntry = null;
 
+String allowedServiceSignatures = StringPool.BLANK;
+
 if (sapEntryId > 0) {
 	sapEntry = SAPEntryServiceUtil.getSAPEntry(sapEntryId);
+
+	allowedServiceSignatures = sapEntry.getAllowedServiceSignatures();
+}
+
+String[] allowedServiceSignaturesArray = StringUtil.splitLines(allowedServiceSignatures);
+
+if (allowedServiceSignaturesArray.length == 0) {
+	allowedServiceSignaturesArray = new String[] { StringPool.BLANK };
 }
 
 boolean systemSAPEntry = false;
@@ -73,184 +83,231 @@ if (sapEntry != null) {
 
 	<aui:input name="title" required="<%= true %>" />
 
-	<portlet:resourceURL var="getServicesURL">
-		<portlet:param name="<%= ActionRequest.ACTION_NAME %>" value="getServices" />
-		<portlet:param name="mvcPath" value="<%= StringPool.SPACE %>" />
-	</portlet:resourceURL>
+	<aui:input cssClass="hide" helpMessage="allowed-service-signatures-help" name="allowedServiceSignatures" />
+
 	<portlet:resourceURL var="getMethodsURL">
 		<portlet:param name="<%= ActionRequest.ACTION_NAME %>" value="getMethods" />
 		<portlet:param name="mvcPath" value="<%= StringPool.SPACE %>" />
 	</portlet:resourceURL>
 
-	<div id="<portlet:namespace />allowedServiceSignatures">
+	<div id="<portlet:namespace />allowedServiceSignaturesFriendlyContentBox">
 
 		<%
-		for (int i = 0; i < allowedServiceSignaturesIndexes.length; i++) {
-			int allowedServiceIndex = allowedServiceSignaturesIndexes[i];
+		for (int i = 0; i < allowedServiceSignaturesArray.length; i++) {
+			String entry = allowedServiceSignaturesArray[i];
 
-			String packageName = ParamUtil.getString(request, "packageName" + allowedServiceIndex, "0");
-			String methodName = ParamUtil.getString(request, "methodName" + allowedServiceIndex, "All methods");
+			String[] split = StringUtil.split(entry, CharPool.POUND);
+
+			String serviceClass = StringPool.BLANK;
+			String methodName = StringPool.BLANK;
+
+			if (split.length > 0) {
+				serviceClass = GetterUtil.getString(split[0], StringPool.BLANK);
+
+				if (split.length > 1) {
+					methodName = GetterUtil.getString(split[1], StringPool.BLANK);
+				}
+			}
 		%>
 
 			<div class="lfr-form-row">
 				<div class="row-fields">
 					<aui:col md="6">
-						<aui:select id='<%= "serviceClass" + allowedServiceIndex %>' name="serviceClass"></aui:select>
+						<aui:input cssClass="service-class" data-service-class="<%= serviceClass %>" id='<%= "serviceClass" + i %>' name="serviceClass" type="text" value="<%= serviceClass %>" />
 					</aui:col>
 					<aui:col md="6">
-						<aui:select id='<%= "methodName" + allowedServiceIndex %>' name="methodName"></aui:select>
+						<aui:input cssClass="method-name" id='<%= "methodName" + i %>' name="methodName" type="text" value="<%= methodName %>" />
 					</aui:col>
 				</div>
 			</div>
-
-			<aui:script use="liferay-dynamic-select,io-base">
-				function getJSON(URL, callback, data) {
-					A.io(
-						URL,
-						{
-							arguments: {
-								callback: callback
-							},
-							data: data,
-							on: {
-								success: <portlet:namespace />processRequest
-							}
-						}
-					);
-				}
-
-				function getMethods(callback, key) {
-					var dataKey = key.split('/');
-
-					getJSON(
-						'<%= getMethodsURL %>',
-						callback,
-						{
-							<portlet:namespace />context: dataKey[0],
-							<portlet:namespace />serviceClass: dataKey[1]
-						}
-					);
-				}
-
-				function getServices(callback) {
-					getJSON('<%= getServicesURL %>', callback);
-				}
-
-				new Liferay.DynamicSelect(
-					[
-						{
-							select: '<portlet:namespace />serviceClass<%= allowedServiceIndex %>',
-							selectData: getServices,
-							selectDesc: 'serviceClass',
-							selectId: 'serviceClass',
-							selectSort: '<%= true %>',
-							selectVal: '<%= packageName %>'
-						},
-						{
-							select: '<portlet:namespace />methodName<%= allowedServiceIndex %>',
-							selectData: getMethods,
-							selectDesc: 'method',
-							selectId: 'method',
-							selectVal: '<%= methodName %>'
-						}
-					]
-				);
-			</aui:script>
 
 		<%
 		}
 		%>
 
-		<aui:input name="allowedServiceSignaturesIndexes" type="hidden" value="<%= StringUtil.merge(allowedServiceSignaturesIndexes) %>" />
 	</div>
 
-	<aui:script use="liferay-auto-fields,liferay-dynamic-select,io-base">
-		function getJSON(URL, callback, data) {
-			A.io(
-				URL,
-				{
-					arguments: {
-						callback: callback
-					},
-					data: data,
-					on: {
-						success: <portlet:namespace />processRequest
-					}
-				}
-			);
-		}
+	<aui:script use="autocomplete,autocomplete-filters,io-base,liferay-auto-fields">
+		var services = <%= JSONFactoryUtil.looseSerialize(JSONWebServiceActionsManagerUtil.getServiceNames()) %>;
 
-		function getMethods(callback, key) {
-			var dataKey = key.split('/');
-
-			getJSON(
-				'<%= getMethodsURL %>',
-				callback,
-				{
-					<portlet:namespace />context: dataKey[0],
-					<portlet:namespace />serviceClass: dataKey[1]
-				}
-			);
-		}
-
-		function getServices(callback) {
-			getJSON('<%= getServicesURL %>', callback);
-		}
-
-		var allowedServiceSignatures = new Liferay.AutoFields(
+		var autoFields = new Liferay.AutoFields(
 			{
-				contentBox: '#<portlet:namespace />allowedServiceSignatures',
-				fieldIndexes: '<portlet:namespace />allowedServiceSignaturesIndexes',
+				contentBox: '#<portlet:namespace />allowedServiceSignaturesFriendlyContentBox',
 				namespace: '<portlet:namespace />',
 				on: {
-					'clone': function(event) {
-						var guid = event.guid;
-						var row = event.row;
-
-						var dynamicSelects = row.one('select[data-componentType=dynamic_select]');
-
-						if (dynamicSelects) {
-							dynamicSelects.detach('change');
-						}
-
-						var dynamicSelect = new Liferay.DynamicSelect(
-							[
-								{
-									select: '<portlet:namespace />serviceClass' + guid,
-									selectData: getServices,
-									selectDesc: 'serviceClass',
-									selectId: 'serviceClass',
-									selectSort: '<%= true %>',
-									selectVal: '0'
-								},
-								{
-									select: '<portlet:namespace />methodName' + guid,
-									selectData: getMethods,
-									selectDesc: 'method',
-									selectId: 'method',
-									selectVal: 'All methods'
-								}
-							]
-						);
-
-						dynamicSelect._updateSelect(1, []);
-					}
+					clone: function(event) {
+						initAutoCompleteRow(event.row);
+					},
+					delete: updateAdvancedModeTextarea
 				}
 			}
 		).render();
-	</aui:script>
 
-	<aui:script>
-		function <portlet:namespace />processRequest(index, data, args) {
-			if (data.response) {
-				var response = JSON.parse(data.response);
+		var rows = A.all('#<portlet:namespace />allowedServiceSignaturesFriendlyContentBox .lfr-form-row');
 
-				args.callback(response);
+		var rowTemplate = rows.first().clone();
+
+		rowTemplate.all('input').val('');
+
+		A.each(
+			rows,
+			function(item, index) {
+				initAutoCompleteRow(item);
+			}
+		);
+
+		A.one('#<portlet:namespace />allowedServiceSignaturesFriendlyContentBox').delegate('blur', updateAdvancedModeTextarea, '.service-class, .method-name');
+		A.one('#<portlet:namespace />allowedServiceSignatures').on('blur', updateFriendlyModeInputs);
+
+		function updateAdvancedModeTextarea() {
+			var updatedInput = '';
+
+			A.all('#<portlet:namespace />allowedServiceSignaturesFriendlyContentBox .lfr-form-row:not(.hide)').each(
+				function(item, index) {
+					var serviceClass = item.one('.service-class').val();
+					var methodName = item.one('.method-name').val();
+
+					updatedInput += serviceClass;
+
+					if (methodName.length > 0) {
+						updatedInput += '#' + methodName;
+					}
+
+					updatedInput += '\n';
+				}
+			);
+
+			A.one('#<portlet:namespace />allowedServiceSignatures').val(updatedInput);
+		}
+
+		function updateFriendlyModeInputs() {
+			var advancedInput = A.one('#<portlet:namespace />allowedServiceSignatures').val();
+			var contentBox = A.one('#<portlet:namespace />allowedServiceSignaturesFriendlyContentBox');
+			var entries = advancedInput.split('\n');
+			var rows = contentBox.all('.lfr-form-row:not(.hide)');
+
+			rows.remove();
+
+			for (var i = 0; i < entries.length; i++) {
+				var entry = entries[i];
+				var row = rowTemplate.clone();
+
+				if (entry.length) {
+					var serviceInput = row.one('.service-class');
+					var methodInput = row.one('.method-name');
+
+					entry = entry.split('#');
+
+					contentBox.append(row);
+
+					initAutoCompleteRow(row);
+
+					serviceInput.val(entry[0]);
+					serviceInput.attr('data-service-class', entry[0]);
+
+					if (entry[1]) {
+						methodInput.val(entry[1]);
+					}
+				}
+				else {
+					contentBox.append(row);
+
+					initAutoCompleteRow(row);
+				}
 			}
 		}
+
+		function initAutoCompleteRow(rowNode) {
+			var serviceInput = rowNode.one('.service-class');
+			var methodInput = rowNode.one('.method-name');
+
+			new A.AutoComplete(
+				{
+					inputNode: serviceInput,
+					source: services,
+					resultFilters: 'phraseMatch',
+					resultTextLocator: 'serviceClass',
+					on: {
+						select: function(event) {
+							var result = event.result.raw;
+
+							serviceInput.attr('data-service-class', result.serviceClass);
+							serviceInput.attr('data-context', result.context);
+						}
+					}
+				}
+			).render();
+
+			new A.AutoComplete(
+				{
+					inputNode: methodInput,
+					source: function(query, callback) {
+						var context = serviceInput.attr('data-context');
+						var serviceClass = serviceInput.attr('data-service-class');
+
+						if (context.length == 0) {
+							A.Array.each(services, function(item, index) {
+								if (item.serviceClass == serviceClass) {
+									context = item.context;
+
+									serviceInput.attr('data-context', context);
+								}
+							});
+						}
+
+						if (context.length && serviceClass.length) {
+							var methodsURL = '<%= getMethodsURL %>&<portlet:namespace />serviceClass=' + serviceClass + '&<portlet:namespace />context=' + context;
+
+							A.io.request(
+								methodsURL,
+								{
+									dataType: 'JSON',
+									method: 'GET',
+									on: {
+										complete: function(event, id, xhr) {
+											var responseData = {};
+
+											try {
+												responseData = A.JSON.parse(xhr.responseText);
+											}
+											catch (e) {
+											}
+
+											callback(responseData);
+										}
+									}
+								}
+							);
+						}
+					},
+					resultFilters: 'phraseMatch',
+					resultTextLocator: 'methodName'
+				}
+			).render();
+		}
+
+		Liferay.provide(
+			window,
+			'<portlet:namespace />toggleAdvancedMode',
+			function() {
+				var A = AUI();
+
+				var advancedButton = A.one('#<portlet:namespace />advancedMode');
+				var friendlyButton = A.one('#<portlet:namespace />friendlyMode');
+				var allowedServiceSignaturesTextarea = A.one('#<portlet:namespace />allowedServiceSignatures');
+				var allowedServiceSignaturesFriendlyContentBox = A.one('#<portlet:namespace />allowedServiceSignaturesFriendlyContentBox');
+
+				advancedButton.toggleClass('hide');
+				friendlyButton.toggleClass('hide');
+				allowedServiceSignaturesTextarea.toggleClass('hide');
+				allowedServiceSignaturesFriendlyContentBox.toggleClass('hide');
+			}
+		);
 	</aui:script>
 
 	<aui:button-row>
 		<aui:button type="submit" value="save" />
+		<aui:button id="advancedMode" onClick='<%= renderResponse.getNamespace() + "toggleAdvancedMode();" %>' value="Switch to Advanced mode" />
+		<aui:button cssClass="hide" id="friendlyMode" onClick='<%= renderResponse.getNamespace() + "toggleAdvancedMode();" %>' value="Switch to Friendly mode" />
 	</aui:button-row>
 </aui:form>