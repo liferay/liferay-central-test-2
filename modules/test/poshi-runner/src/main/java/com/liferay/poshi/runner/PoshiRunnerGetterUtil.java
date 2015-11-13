fbe9223fa69ae610438ac05eed82108998456eb3
Kevin Yen <kevin.yen@liferay.com>
2015-11-13 12:02:22 -0800

    LRQA-15442 Add keyword "return" to Poshi validation

--- modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiRunnerGetterUtil.java
+++ modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiRunnerGetterUtil.java
@@ -390,9 +390,9 @@ public class PoshiRunnerGetterUtil {
 			"and", "body", "case", "command", "condition", "contains",
 			"default", "definition", "description", "echo", "else", "elseif",
 			"equals", "execute", "fail", "for", "if", "head", "html", "isset",
-			"not", "or", "property", "set-up", "table", "take-screenshot",
-			"task", "tbody", "td", "tear-down", "thead", "then", "title", "tr",
-			"var", "while"
+			"not", "or", "property", "return", "set-up", "table",
+			"take-screenshot", "task", "tbody", "td", "tear-down", "thead",
+			"then", "title", "tr", "var", "while"
 		});
 	private static final Pattern _tagPattern = Pattern.compile("<[a-z\\-]+");
 
