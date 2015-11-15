186d281aa01bddd09074d35c520e60e08d02dd01
shuyangzhou <shuyang.zhou@liferay.com>
2015-11-15 14:02:45 -0800

    LPS-60309 Apply AssumeTestRule

--- modules/portal-store/portal-store-s3-test/src/testIntegration/java/com/liferay/portal/store/s3/test/S3StoreTest.java
+++ modules/portal-store/portal-store-s3-test/src/testIntegration/java/com/liferay/portal/store/s3/test/S3StoreTest.java
@@ -16,6 +16,7 @@ package com.liferay.portal.store.s3.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.AssumeTestRule;
 import com.liferay.portal.kernel.test.rule.TransactionalTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
@@ -25,7 +26,6 @@ import com.liferay.portlet.documentlibrary.store.test.BaseStoreTestCase;
 
 import org.junit.Assert;
 import org.junit.Assume;
-import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -42,10 +42,10 @@ public class S3StoreTest extends BaseStoreTestCase {
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
 		new AggregateTestRule(
-			new LiferayIntegrationTestRule(), TransactionalTestRule.INSTANCE);
+			new AssumeTestRule("assume"), new LiferayIntegrationTestRule(),
+			TransactionalTestRule.INSTANCE);
 
-	@BeforeClass
-	public static void setUpClass() {
+	public static void assume() {
 		String dlStoreImpl = PropsUtil.get(PropsKeys.DL_STORE_IMPL);
 
 		String s3StoreClassName = "com.liferay.portal.store.s3.S3Store";
