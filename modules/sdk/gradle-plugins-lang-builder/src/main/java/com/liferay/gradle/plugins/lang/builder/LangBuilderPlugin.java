d6a1ddb3963ee5769a9254333a1e5eb7add2fb1c
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2015-11-16 14:56:31 -0800

    LPS-60548 Allow custom classpath

--- modules/sdk/gradle-plugins-lang-builder/src/main/java/com/liferay/gradle/plugins/lang/builder/LangBuilderPlugin.java
+++ modules/sdk/gradle-plugins-lang-builder/src/main/java/com/liferay/gradle/plugins/lang/builder/LangBuilderPlugin.java
@@ -20,6 +20,8 @@ import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.tasks.TaskContainer;
 
 /**
  * @author Andrea Di Giorgi
@@ -32,9 +34,12 @@ public class LangBuilderPlugin implements Plugin<Project> {
 
 	@Override
 	public void apply(Project project) {
-		addConfigurationLangBuilder(project);
+		Configuration langBuilderConfiguration = addConfigurationLangBuilder(
+			project);
 
 		addTaskBuildLang(project);
+
+		configureTasksBuildLang(project, langBuilderConfiguration);
 	}
 
 	protected Configuration addConfigurationLangBuilder(final Project project) {
@@ -73,6 +78,30 @@ public class LangBuilderPlugin implements Plugin<Project> {
 			"Runs Liferay Lang Builder to translate language property files.");
 
 		return buildLangTask;
+	}
+
+	protected void configureTaskBuildLangClasspath(
+		BuildLangTask buildLangTask, FileCollection fileCollection) {
+
+		buildLangTask.setClasspath(fileCollection);
+	}
+
+	protected void configureTasksBuildLang(
+		Project project, final Configuration langBuilderConfiguration) {
+
+		TaskContainer taskContainer = project.getTasks();
+
+		taskContainer.withType(
+			BuildLangTask.class,
+			new Action<BuildLangTask>() {
+
+				@Override
+				public void execute(BuildLangTask buildLangTask) {
+					configureTaskBuildLangClasspath(
+						buildLangTask, langBuilderConfiguration);
+				}
+
+			});
 	}
 
 }
