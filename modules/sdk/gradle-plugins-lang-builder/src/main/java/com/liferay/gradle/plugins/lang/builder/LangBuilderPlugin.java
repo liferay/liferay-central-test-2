400822c35acf4a010e0c850d14377e86f8ed3f2a
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2015-11-16 14:44:40 -0800

    LPS-60548 Not really used

--- modules/sdk/gradle-plugins-lang-builder/src/main/java/com/liferay/gradle/plugins/lang/builder/LangBuilderPlugin.java
+++ modules/sdk/gradle-plugins-lang-builder/src/main/java/com/liferay/gradle/plugins/lang/builder/LangBuilderPlugin.java
@@ -15,13 +15,11 @@
 package com.liferay.gradle.plugins.lang.builder;
 
 import com.liferay.gradle.util.GradleUtil;
-import com.liferay.gradle.util.Validator;
 
 import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.tasks.TaskContainer;
 
 /**
  * @author Andrea Di Giorgi
@@ -37,16 +35,6 @@ public class LangBuilderPlugin implements Plugin<Project> {
 		addLangBuilderConfiguration(project);
 
 		addBuildLangTask(project);
-
-		project.afterEvaluate(
-			new Action<Project>() {
-
-				@Override
-				public void execute(Project project) {
-					configureBuildLangTask(project);
-				}
-
-			});
 	}
 
 	protected BuildLangTask addBuildLangTask(Project project) {
@@ -85,39 +73,6 @@ public class LangBuilderPlugin implements Plugin<Project> {
 		GradleUtil.addDependency(
 			project, CONFIGURATION_NAME, "com.liferay",
 			"com.liferay.lang.builder", "latest.release");
-	}
-
-	protected void configureBuildLangTask(BuildLangTask buildLangTask) {
-		Project project = buildLangTask.getProject();
-
-		if (Validator.isNull(buildLangTask.getTranslateClientId()) &&
-			project.hasProperty("microsoftTranslatorClientId")) {
-
-			buildLangTask.setTranslateClientId(
-				(String)project.property("microsoftTranslatorClientId"));
-		}
-
-		if (Validator.isNull(buildLangTask.getTranslateClientSecret()) &&
-			project.hasProperty("microsoftTranslatorClientSecret")) {
-
-			buildLangTask.setTranslateClientSecret(
-				(String)project.property("microsoftTranslatorClientSecret"));
-		}
-	}
-
-	protected void configureBuildLangTask(Project project) {
-		TaskContainer taskContainer = project.getTasks();
-
-		taskContainer.withType(
-			BuildLangTask.class,
-			new Action<BuildLangTask>() {
-
-				@Override
-				public void execute(BuildLangTask buildLangTask) {
-					configureBuildLangTask(buildLangTask);
-				}
-
-			});
 	}
 
 }
