202b56161971c972602445f7a6a27cc4cd997473
philip.jones <philip.jones@liferay.com>
2015-11-17 10:23:12 -0800

    LPS-59180 Add portal-security-common

---
+++ modules/portal-security/portal-security-common/src/main/java/com/liferay/portal/security/common/action/BaseAuthenticationFormMVCActionCommand.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.common.action;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseFormMVCActionCommand;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.settings.CompanyServiceSettingsLocator;
+import com.liferay.portal.kernel.settings.ModifiableSettings;
+import com.liferay.portal.kernel.settings.Settings;
+import com.liferay.portal.kernel.settings.SettingsDescriptor;
+import com.liferay.portal.kernel.settings.SettingsException;
+import com.liferay.portal.kernel.settings.SettingsFactoryUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.security.auth.PrincipalException;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.security.permission.PermissionThreadLocal;
+import com.liferay.portal.theme.ThemeDisplay;
+
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.ValidatorException;
+
+/**
+ * @author Tomas Polesovsky
+ * @author Philip Jones
+ */
+public abstract class BaseAuthenticationFormMVCActionCommand
+	extends BaseFormMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		if (!hasPermissions(actionRequest, actionResponse, themeDisplay)) {
+			return;
+		}
+
+		storeSettings(
+			actionRequest, themeDisplay, getServiceName(), getShortNamespace());
+	}
+
+	protected abstract String getServiceName();
+
+	protected abstract String getShortNamespace();
+
+	protected boolean hasPermissions(
+		ActionRequest actionRequest, ActionResponse actionResponse,
+		ThemeDisplay themeDisplay) {
+
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		if (!permissionChecker.isCompanyAdmin(themeDisplay.getCompanyId())) {
+			SessionErrors.add(actionRequest, PrincipalException.class);
+
+			actionResponse.setRenderParameter("mvcPath", "/error.jsp");
+
+			return false;
+		}
+
+		return true;
+	}
+
+	protected void storeSettings(
+			ActionRequest actionRequest, ThemeDisplay themeDisplay,
+			String serviceName, String shortNamespace)
+		throws IOException, SettingsException, ValidatorException {
+
+		Settings settings = SettingsFactoryUtil.getSettings(
+			new CompanyServiceSettingsLocator(
+				themeDisplay.getCompanyId(), serviceName));
+
+		ModifiableSettings modifiableSettings =
+			settings.getModifiableSettings();
+
+		SettingsDescriptor settingsDescriptor =
+			SettingsFactoryUtil.getSettingsDescriptor(serviceName);
+
+		for (String name : settingsDescriptor.getAllKeys()) {
+			String value = ParamUtil.getString(
+				actionRequest, shortNamespace + name);
+			String oldValue = settings.getValue(name, null);
+
+			if (!value.equals(oldValue)) {
+				modifiableSettings.setValue(name, value);
+			}
+		}
+
+		modifiableSettings.store();
+	}
+
+}
