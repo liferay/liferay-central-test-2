9dfab3683598b5e73e9050de146ae33e3d009ccb
akosthurzo <akos.thurzo@liferay.com>
2015-11-17 10:00:45 +0100

    LPS-60801 Update StagedAssetLinkStagedModelDataHandler to use StagedModelRepository

--- modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/lar/StagedAssetLinkStagedModelDataHandler.java
+++ modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/lar/StagedAssetLinkStagedModelDataHandler.java
@@ -14,31 +14,18 @@
 
 package com.liferay.asset.categories.admin.web.lar;
 
-import com.liferay.portal.kernel.dao.orm.Criterion;
-import com.liferay.portal.kernel.dao.orm.DynamicQuery;
-import com.liferay.portal.kernel.dao.orm.OrderFactoryUtil;
-import com.liferay.portal.kernel.dao.orm.Projection;
-import com.liferay.portal.kernel.dao.orm.ProjectionFactoryUtil;
-import com.liferay.portal.kernel.dao.orm.Property;
-import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.util.ListUtil;
-import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.asset.exportimport.staged.model.repository.StagedAssetLinkStagedModelRepository;
+import com.liferay.exportimport.lar.BaseStagedModelDataHandler;
+import com.liferay.exportimport.staged.model.repository.StagedModelRepository;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.model.StagedModel;
-import com.liferay.portal.model.adapter.ModelAdapterUtil;
 import com.liferay.portlet.asset.model.AssetEntry;
-import com.liferay.portlet.asset.model.AssetLink;
 import com.liferay.portlet.asset.model.AssetRenderer;
 import com.liferay.portlet.asset.model.adapter.StagedAssetLink;
 import com.liferay.portlet.asset.service.AssetEntryLocalService;
-import com.liferay.portlet.asset.service.AssetLinkLocalService;
-import com.liferay.portlet.exportimport.lar.BaseStagedModelDataHandler;
 import com.liferay.portlet.exportimport.lar.ExportImportPathUtil;
 import com.liferay.portlet.exportimport.lar.PortletDataContext;
 import com.liferay.portlet.exportimport.lar.StagedModelDataHandler;
-
-import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -52,43 +39,6 @@ public class StagedAssetLinkStagedModelDataHandler
 
 	public static final String[] CLASS_NAMES =
 		{StagedAssetLink.class.getName()};
-
-	@Override
-	public void deleteStagedModel(StagedAssetLink stagedAssetLink)
-		throws PortalException {
-
-		_assetLinkLocalService.deleteAssetLink(stagedAssetLink);
-	}
-
-	@Override
-	public void deleteStagedModel(
-			String uuid, long groupId, String className, String extraData)
-		throws PortalException {
-
-		StagedAssetLink stagedAssetLink = fetchExistingAssetLink(
-			groupId, parseAssetEntry1Uuid(uuid), parseAssetEntry2Uuid(uuid));
-
-		if (stagedAssetLink != null) {
-			deleteStagedModel(stagedAssetLink);
-		}
-	}
-
-	@Override
-	public List<StagedAssetLink> fetchStagedModelsByUuidAndCompanyId(
-		String uuid, long companyId) {
-
-		DynamicQuery dynamicQuery = getAssetLinkDynamicQuery(
-			companyId, 0, parseAssetEntry1Uuid(uuid),
-			parseAssetEntry2Uuid(uuid));
-
-		dynamicQuery.addOrder(OrderFactoryUtil.desc("linkId"));
-
-		List<AssetLink> assetLinks = _assetLinkLocalService.dynamicQuery(
-			dynamicQuery);
-
-		return ModelAdapterUtil.adapt(
-			assetLinks, AssetLink.class, StagedAssetLink.class);
-	}
 
 	@Override
 	public String[] getClassNames() {
@@ -157,127 +107,28 @@ public class StagedAssetLinkStagedModelDataHandler
 			StagedAssetLink stagedAssetLink)
 		throws Exception {
 
-		long userId = portletDataContext.getUserId(
-			stagedAssetLink.getUserUuid());
-
-		StagedAssetLink existingStagedAssetLink = fetchExistingAssetLink(
-			portletDataContext.getScopeGroupId(),
-			stagedAssetLink.getEntry1Uuid(), stagedAssetLink.getEntry2Uuid());
+		StagedAssetLink existingStagedAssetLink =
+			_stagedAssetLinkStagedModelRepository.fetchExistingAssetLink(
+				portletDataContext.getScopeGroupId(),
+				stagedAssetLink.getEntry1Uuid(),
+				stagedAssetLink.getEntry2Uuid());
 
 		if (existingStagedAssetLink != null) {
-			_assetLinkLocalService.updateLink(
-				userId, existingStagedAssetLink.getEntryId1(),
-				existingStagedAssetLink.getEntryId2(),
-				stagedAssetLink.getType(), stagedAssetLink.getWeight());
+			_stagedAssetLinkStagedModelRepository.updateStagedModel(
+				portletDataContext, existingStagedAssetLink);
 
 			return;
 		}
 
-		AssetEntry assetEntry1 = _assetEntryLocalService.fetchEntry(
-			portletDataContext.getScopeGroupId(),
-			stagedAssetLink.getEntry1Uuid());
-		AssetEntry assetEntry2 = _assetEntryLocalService.fetchEntry(
-			portletDataContext.getScopeGroupId(),
-			stagedAssetLink.getEntry2Uuid());
-
-		if ((assetEntry1 == null) || (assetEntry2 == null)) {
-			return;
-		}
-
-		_assetLinkLocalService.addLink(
-			userId, assetEntry1.getEntryId(), assetEntry2.getEntryId(),
-			stagedAssetLink.getType(), stagedAssetLink.getWeight());
+		_stagedAssetLinkStagedModelRepository.addStagedModel(
+			portletDataContext, stagedAssetLink);
 	}
 
-	protected StagedAssetLink fetchExistingAssetLink(
-		long groupId, String assetEntry1Uuid, String assetEntry2Uuid) {
+	@Override
+	protected StagedModelRepository<StagedAssetLink>
+		getStagedModelRepository() {
 
-		DynamicQuery dynamicQuery = getAssetLinkDynamicQuery(
-			0, groupId, assetEntry1Uuid, assetEntry2Uuid);
-
-		List<AssetLink> assetLinks = _assetLinkLocalService.dynamicQuery(
-			dynamicQuery);
-
-		if (ListUtil.isNotEmpty(assetLinks)) {
-			return ModelAdapterUtil.adapt(
-				assetLinks.get(0), AssetLink.class, StagedAssetLink.class);
-		}
-
-		return null;
-	}
-
-	protected DynamicQuery getAssetLinkDynamicQuery(
-		long companyId, long groupId, String assetEntry1Uuid,
-		String assetEntry2Uuid) {
-
-		// Asset entry 1 dynamic query
-
-		Projection entryIdProjection = ProjectionFactoryUtil.property(
-			"entryId");
-
-		DynamicQuery assetEntry1DynamicQuery =
-			_assetEntryLocalService.dynamicQuery();
-
-		assetEntry1DynamicQuery.setProjection(entryIdProjection);
-
-		Property classUuidProperty = PropertyFactoryUtil.forName("classUuid");
-
-		assetEntry1DynamicQuery.add(classUuidProperty.eq(assetEntry1Uuid));
-
-		// Asset entry 2 dynamic query
-
-		DynamicQuery assetEntry2DynamicQuery =
-			_assetEntryLocalService.dynamicQuery();
-
-		assetEntry2DynamicQuery.setProjection(entryIdProjection);
-
-		assetEntry2DynamicQuery.add(classUuidProperty.eq(assetEntry2Uuid));
-
-		// Asset link dynamic query
-
-		DynamicQuery dynamicQuery = _assetLinkLocalService.dynamicQuery();
-
-		Property entryId1IdProperty = PropertyFactoryUtil.forName("entryId1");
-
-		dynamicQuery.add(entryId1IdProperty.eq(assetEntry1DynamicQuery));
-
-		Property entryId2IdProperty = PropertyFactoryUtil.forName("entryId2");
-
-		dynamicQuery.add(entryId2IdProperty.eq(assetEntry2DynamicQuery));
-
-		// Company ID
-
-		if (companyId > 0) {
-			Property companyIdProperty = PropertyFactoryUtil.forName(
-				"companyId");
-
-			Criterion companyIdCriterion = companyIdProperty.eq(companyId);
-
-			assetEntry1DynamicQuery.add(companyIdCriterion);
-			assetEntry2DynamicQuery.add(companyIdCriterion);
-			dynamicQuery.add(companyIdCriterion);
-		}
-
-		// Group ID
-
-		if (groupId > 0) {
-			Property groupIdProperty = PropertyFactoryUtil.forName("groupId");
-
-			Criterion groupIdCriterion = groupIdProperty.eq(groupId);
-
-			assetEntry1DynamicQuery.add(groupIdCriterion);
-			assetEntry2DynamicQuery.add(groupIdCriterion);
-		}
-
-		return dynamicQuery;
-	}
-
-	protected String parseAssetEntry1Uuid(String uuid) {
-		return uuid.substring(0, uuid.indexOf(StringPool.POUND));
-	}
-
-	protected String parseAssetEntry2Uuid(String uuid) {
-		return uuid.substring(uuid.indexOf(StringPool.POUND) + 1);
+		return _stagedAssetLinkStagedModelRepository;
 	}
 
 	@Reference(unbind = "-")
@@ -287,14 +138,23 @@ public class StagedAssetLinkStagedModelDataHandler
 		_assetEntryLocalService = assetEntryLocalService;
 	}
 
-	@Reference(unbind = "-")
-	protected void setAssetLinkLocalService(
-		AssetLinkLocalService assetLinkLocalService) {
+	@Reference(
+		target =
+			"(model.class.name=" +
+				"com.liferay.asset.exportimport.staged.model.repository." +
+					"StagedAssetLinkStagedModelRepository)",
+		unbind = "-"
+	)
+	protected void setStagedAssetLinkStagedModelRepository(
+		StagedAssetLinkStagedModelRepository
+			stagedAssetLinkStagedModelRepository) {
 
-		_assetLinkLocalService = assetLinkLocalService;
+		_stagedAssetLinkStagedModelRepository =
+			stagedAssetLinkStagedModelRepository;
 	}
 
 	private volatile AssetEntryLocalService _assetEntryLocalService;
-	private volatile AssetLinkLocalService _assetLinkLocalService;
+	private volatile StagedAssetLinkStagedModelRepository
+		_stagedAssetLinkStagedModelRepository;
 
 }
