5970b95ecca29c060dcf17d4a0f49d5ffec09038
Brian Chan <brian.chan@liferay.com>
2015-11-17 10:16:12 -0800

    LPS-60556 SF

--- modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/portlet/BlogsAggregatorPortlet.java
+++ modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/portlet/BlogsAggregatorPortlet.java
@@ -52,7 +52,8 @@ import org.osgi.service.component.annotations.Reference;
 public class BlogsAggregatorPortlet extends MVCPortlet {
 
 	@Reference(
-		target = "(&(release.bundle.symbolic.name=com.liferay.blogs.web)(release.schema.version=1.0.0))"
+		target = "(&(release.bundle.symbolic.name=com.liferay.blogs.web)(release.schema.version=1.0.0))",
+		unbind = "-"
 	)
 	protected void setRelease(Release release) {
 	}
