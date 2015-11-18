6b7e9f6ea13acfc06a8e7f82496bc290973e7c12
Brian Chan <brian.chan@liferay.com>
2015-11-18 10:21:01 -0800

    LPS-60613 SF

--- modules/apps/site/site-memberships-web/src/main/java/com/liferay/site/memberships/web/portlet/configuration/icon/AddSiteRolesToUsersPortletConfigurationIcon.java
+++ modules/apps/site/site-memberships-web/src/main/java/com/liferay/site/memberships/web/portlet/configuration/icon/AddSiteRolesToUsersPortletConfigurationIcon.java
@@ -71,7 +71,7 @@ public class AddSiteRolesToUsersPortletConfigurationIcon
 				themeDisplay.getPermissionChecker(),
 				themeDisplay.getScopeGroup(), ActionKeys.ASSIGN_USER_ROLES);
 		}
-		catch (PortalException e) {
+		catch (PortalException pe) {
 		}
 
 		return false;
