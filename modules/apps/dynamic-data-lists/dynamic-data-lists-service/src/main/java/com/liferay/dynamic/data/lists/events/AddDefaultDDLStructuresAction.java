0af707be63f19871671f909c360549738f692bbf
Rafael Praxedes <rafael.praxedes@liferay.com>
2015-11-19 18:09:39 -0300

    LPS-60655 Change scope of default DDL structures to global

--- modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/events/AddDefaultDDLStructuresAction.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/events/AddDefaultDDLStructuresAction.java
@@ -22,7 +22,6 @@ import com.liferay.portal.kernel.events.SimpleAction;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.model.Company;
 import com.liferay.portal.model.Group;
-import com.liferay.portal.model.GroupConstants;
 import com.liferay.portal.security.auth.CompanyThreadLocal;
 import com.liferay.portal.service.CompanyLocalService;
 import com.liferay.portal.service.GroupLocalService;
@@ -75,8 +74,10 @@ public class AddDefaultDDLStructuresAction extends SimpleAction {
 	protected void doRun(long companyId) throws Exception {
 		ServiceContext serviceContext = new ServiceContext();
 
-		Group group = _groupLocalService.getGroup(
-			companyId, GroupConstants.GUEST);
+		serviceContext.setAddGuestPermissions(true);
+		serviceContext.setAddGroupPermissions(true);
+
+		Group group = _groupLocalService.getCompanyGroup(companyId);
 
 		serviceContext.setScopeGroupId(group.getGroupId());
 
