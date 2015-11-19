2167bd4119a77d7f767b2dcf3e9c62a47cf9c81f
Bruno Basto <bruno.basto@liferay.com>
2015-11-19 15:04:30 -0300

    LPS-60590 Regenerate

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-select/src/main/resources/META-INF/resources/select.soy.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-select/src/main/resources/META-INF/resources/select.soy.js
@@ -5,12 +5,12 @@ if (typeof ddm == 'undefined') { var ddm = {}; }
 
 
 ddm.select = function(opt_data, opt_ignored) {
-var output = '\t<div class="form-group' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + '" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '"><div class="input-select-wrapper">' + ((opt_data.showLabel) ? '<label class="control-label" for="' + soy.$$escapeHtml(opt_data.name) + '">' + soy.$$escapeHtml(opt_data.label) + ((opt_data.required) ? '<b>*</b>' : '') + '</label>' : '') + '<select class="form-control" dir="' + soy.$$escapeHtml(opt_data.dir) + '" ' + ((opt_data.readOnly) ? 'disabled' : '') + ' id="' + soy.$$escapeHtml(opt_data.name) + '" name="' + soy.$$escapeHtml(opt_data.name) + '" ' + soy.$$escapeHtml(opt_data.multiple) + '><option dir="' + soy.$$escapeHtml(opt_data.dir) + '" selected value="">' + soy.$$escapeHtml(opt_data.strings.chooseAnOption) + '</option>';
-var optionList35 = opt_data.options;
-var optionListLen35 = optionList35.length;
-for (var optionIndex35 = 0; optionIndex35 < optionListLen35; optionIndex35++) {
-	var optionData35 = optionList35[optionIndex35];
-	output += '<option dir="' + soy.$$escapeHtml(opt_data.dir) + '" ' + soy.$$escapeHtml(optionData35.status) + ' value="' + soy.$$escapeHtml(optionData35.value) + '">' + soy.$$escapeHtml(optionData35.label) + '</option>';
+var output = '\t<div class="form-group' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + '" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '"><div class="input-select-wrapper">' + ((opt_data.showLabel) ? '<label class="control-label" for="' + soy.$$escapeHtml(opt_data.name) + '">' + soy.$$escapeHtml(opt_data.label) + ((opt_data.required) ? '<b>*</b>' : '') + '</label>' : '') + '<select class="form-control" dir="' + soy.$$escapeHtml(opt_data.dir) + '" ' + ((opt_data.readOnly) ? 'disabled' : '') + ' id="' + soy.$$escapeHtml(opt_data.name) + '" name="' + soy.$$escapeHtml(opt_data.name) + '" ' + soy.$$escapeHtml(opt_data.multiple) + '>' + ((! opt_data.readOnly) ? '<option dir="' + soy.$$escapeHtml(opt_data.dir) + '" selected value="">' + soy.$$escapeHtml(opt_data.strings.chooseAnOption) + '</option>' : '');
+var optionList38 = opt_data.options;
+var optionListLen38 = optionList38.length;
+for (var optionIndex38 = 0; optionIndex38 < optionListLen38; optionIndex38++) {
+	var optionData38 = optionList38[optionIndex38];
+	output += '<option dir="' + soy.$$escapeHtml(opt_data.dir) + '" ' + soy.$$escapeHtml(optionData38.status) + ' value="' + soy.$$escapeHtml(optionData38.value) + '">' + soy.$$escapeHtml(optionData38.label) + '</option>';
 }
 output += '</select>' + soy.$$filterNoAutoescape(opt_data.childElementsHTML) + '</div></div>';
 return output;
