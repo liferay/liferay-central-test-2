5b5715a476d035fd88e3b05df769970aa6caaf4a
Douglas Wong <douglas.wong@liferay.com>
2015-11-19 11:11:06 -0800

    LPS-60506 add "install remote app" functionality to header toolbar

---
+++ modules/apps/marketplace/marketplace-app-manager-web/src/main/java/com/liferay/marketplace/app/manager/web/portlet/configuration/icon/InstallFromURLPortletConfigurationIconFactory.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.marketplace.app.manager.web.portlet.configuration.icon;
+
+import com.liferay.marketplace.app.manager.web.constants.MarketplaceAppManagerPortletKeys;
+import com.liferay.portal.kernel.portlet.configuration.icon.BaseJSPPortletConfigurationIconFactory;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIconFactory;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Douglas Wong
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" +
+			MarketplaceAppManagerPortletKeys.MARKETPLACE_APP_MANAGER,
+		"path=/view.jsp"
+	},
+	service = PortletConfigurationIconFactory.class
+)
+public class InstallFromURLPortletConfigurationIconFactory
+	extends BaseJSPPortletConfigurationIconFactory {
+
+	@Override
+	public PortletConfigurationIcon create(HttpServletRequest request) {
+		return new InstallFromURLPortletConfigurationIcon(request);
+	}
+
+	@Override
+	public String getJspPath() {
+		return "/configuration/icon/install_from_url.jsp";
+	}
+
+	@Override
+	public double getWeight() {
+		return 102;
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.marketplace.app.manager.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+}
---
+++ modules/apps/marketplace/marketplace-app-manager-web/src/main/resources/META-INF/resources/configuration/icon/install_from_url.jsp
@@ -0,0 +1,44 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+PortletURL portletURL = renderResponse.createRenderURL();
+
+String taglibOnClick = renderResponse.getNamespace() + "openInstallFromURLView()";
+%>
+
+<liferay-ui:icon
+	message="install-from-url"
+	onClick="<%= taglibOnClick %>"
+	url="javascript:;"
+/>
+
+<aui:script>
+	function <portlet:namespace />openInstallFromURLView() {
+		Liferay.Util.openWindow(
+			{
+				dialog: {
+					destroyOnHide: true
+				},
+				id: '<portlet:namespace />openInstallFromURLView',
+				title: '<%= UnicodeLanguageUtil.get(request, "install-from-url") %>',
+				uri: '<liferay-portlet:renderURL windowState="<%= LiferayWindowState.POP_UP.toString() %>"><portlet:param name="mvcPath" value="/install_remote_app.jsp" /><portlet:param name="redirect" value="<%= portletURL.toString() %>" /></liferay-portlet:renderURL>'
+			}
+		);
+	}
+</aui:script>
