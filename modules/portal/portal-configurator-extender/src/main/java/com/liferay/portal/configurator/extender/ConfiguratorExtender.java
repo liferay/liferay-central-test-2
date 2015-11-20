88035f104fcfc3af7e306633b5a4ae4a83eac48f
Carlos Sierra <carlos.sierra@liferay.com>
2015-11-20 12:35:44 +0100

    LPS-59342 Do not log when no header is present

--- modules/portal/portal-configurator-extender/src/main/java/com/liferay/portal/configurator/extender/ConfiguratorExtender.java
+++ modules/portal/portal-configurator-extender/src/main/java/com/liferay/portal/configurator/extender/ConfiguratorExtender.java
@@ -24,6 +24,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Dictionary;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.apache.felix.utils.extender.AbstractExtender;
@@ -101,9 +102,13 @@ public class ConfiguratorExtender extends AbstractExtender {
 				_namedConfigurationContentFactories) {
 
 			try {
-				namedConfigurationContents.addAll(
+				List<NamedConfigurationContent> contents =
 					namedConfigurationContentFactory.create(
-						new BundleStorageImpl(bundle)));
+						new BundleStorageImpl(bundle));
+
+				if (contents != null) {
+					namedConfigurationContents.addAll(contents);
+				}
 			}
 			catch (Throwable t) {
 				_logger.log(Logger.LOG_INFO, t.getMessage(), t);
