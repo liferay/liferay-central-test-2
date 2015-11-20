4c7a467653cafa30ae60d5a82571714eb0534523
Peter Yoo <peter.yoo@liferay.com>
2015-11-20 14:46:39 -0800

    LRQA-19505 SF

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/load/balancer/LoadBalanceUtil.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/load/balancer/LoadBalanceUtil.java
@@ -39,44 +39,6 @@ import org.json.JSONObject;
  * @author Peter Yoo
  */
 public class LoadBalanceUtil {
-	
-	protected static String getHostNamePrefix(String baseInvocationUrl) {
-		Matcher matcher = _urlPattern.matcher(baseInvocationUrl);
-
-		if (!matcher.find()) {
-			return baseInvocationUrl;
-		}
-		
-		return matcher.group("hostNamePrefix");
-	}
-	
-	protected static void startParallelTasks(
-		List<String> hostNameList, String hostNamePrefix, int maxHostNames,
-		Project project, List<FutureTask<Integer>> taskList)
-			throws Exception {
-		
-		ExecutorService executor = Executors.newFixedThreadPool(maxHostNames);
-
-		for (int i = 1; i <= maxHostNames; i++) {
-			String hostName = hostNamePrefix + "-" + i;
-
-			hostNameList.add(hostName);
-
-			IdleSlaveCounterCallable callable =
-				new IdleSlaveCounterCallable(
-					project.getProperty(
-						"jenkins.local.url[" + hostName + "]") +
-							"/computer/api/json");
-
-			FutureTask<Integer> futureTask = new FutureTask<>(callable);
-
-			executor.execute(futureTask);
-
-			taskList.add(futureTask);
-		}
-
-		executor.shutdown();
-	}
 
 	public static String getMostAvailableMasterURL(Project project)
 		throws Exception {
@@ -97,17 +59,17 @@ public class LoadBalanceUtil {
 
 		waitForTurn(file, maxHostNames);
 
-		JenkinsResultsParserUtil.write(file, _HOSTNAME);
-		
+		JenkinsResultsParserUtil.write(file, myHostName);
+
 		try {
 			List<String> hostNameList = new ArrayList<>(maxHostNames);
 			List<FutureTask<Integer>> taskList = new ArrayList<>(maxHostNames);
-			
+
 			startParallelTasks(
 				hostNameList, hostNamePrefix, maxHostNames, project, taskList);
 
 			List<Integer> badIndicies = new ArrayList<>(taskList.size());
-			List<Integer> maxIndicies = new ArrayList<>(taskList.size());			
+			List<Integer> maxIndicies = new ArrayList<>(taskList.size());
 			int maxResult = 0;
 
 			for (int i = 0; i < taskList.size(); i++) {
@@ -138,15 +100,16 @@ public class LoadBalanceUtil {
 			int x = -1;
 
 			if (maxIndicies.size() > 0) {
-				x = maxIndicies.get(
-					getRandomValue(0, maxIndicies.size() - 1));
+				x = maxIndicies.get(getRandomValue(0, maxIndicies.size() - 1));
 			}
 			else {
 				while (true) {
 					x = getRandomValue(0, maxHostNames - 1);
+
 					if (badIndicies.contains(x)) {
 						continue;
 					}
+
 					break;
 				}
 			}
@@ -188,33 +151,14 @@ public class LoadBalanceUtil {
 		}
 	}
 
-	private static Project getProject() {
-		Project project = new Project();
+	protected static String getHostNamePrefix(String baseInvocationUrl) {
+		Matcher matcher = urlPattern.matcher(baseInvocationUrl);
 
-		project.setProperty("base.invocation.url", "http://test-1.liferay.com");
-		project.setProperty("jenkins.shared.dir", "mnt/mfs-ssd1-10.10/jenkins");
-		project.setProperty("jenkins.local.url[test-1-1]", "http://test-1-1");
-		project.setProperty("jenkins.local.url[test-1-2]", "http://test-1-2");
-		project.setProperty("jenkins.local.url[test-1-3]", "http://test-1-3");
-		project.setProperty("jenkins.local.url[test-1-4]", "http://test-1-4");
-		project.setProperty("jenkins.local.url[test-1-5]", "http://test-1-5");
-		project.setProperty("jenkins.local.url[test-1-6]", "http://test-1-6");
-		project.setProperty("jenkins.local.url[test-1-7]", "http://test-1-7");
-		project.setProperty("jenkins.local.url[test-1-8]", "http://test-1-8");
-		project.setProperty("jenkins.local.url[test-1-9]", "http://test-1-9");
-		project.setProperty("jenkins.local.url[test-1-10]", "http://test-1-10");
-		project.setProperty("jenkins.local.url[test-1-11]", "http://test-1-11");
-		project.setProperty("jenkins.local.url[test-1-12]", "http://test-1-12");
-		project.setProperty("jenkins.local.url[test-1-13]", "http://test-1-13");
-		project.setProperty("jenkins.local.url[test-1-14]", "http://test-1-14");
-		project.setProperty("jenkins.local.url[test-1-15]", "http://test-1-15");
-		project.setProperty("jenkins.local.url[test-1-16]", "http://test-1-16");
-		project.setProperty("jenkins.local.url[test-1-17]", "http://test-1-17");
-		project.setProperty("jenkins.local.url[test-1-18]", "http://test-1-18");
-		project.setProperty("jenkins.local.url[test-1-19]", "http://test-1-19");
-		project.setProperty("jenkins.local.url[test-1-20]", "http://test-1-20");
+		if (!matcher.find()) {
+			return baseInvocationUrl;
+		}
 
-		return project;
+		return matcher.group("hostNamePrefix");
 	}
 
 	protected static int getRandomValue(int start, int end) {
@@ -223,6 +167,33 @@ public class LoadBalanceUtil {
 		double randomDouble = Math.random();
 
 		return start + (int)Math.round(size * randomDouble);
+	}
+
+	protected static void startParallelTasks(
+		List<String> hostNameList, String hostNamePrefix, int maxHostNames,
+		Project project, List<FutureTask<Integer>> taskList) throws Exception {
+
+		ExecutorService executor = Executors.newFixedThreadPool(maxHostNames);
+
+		for (int i = 1; i <= maxHostNames; i++) {
+			String targetHostName = hostNamePrefix + "-" + i;
+
+			hostNameList.add(targetHostName);
+
+			IdleSlaveCounterCallable callable =
+				new IdleSlaveCounterCallable(
+					project.getProperty(
+						"jenkins.local.url[" + targetHostName + "]") +
+							"/computer/api/json");
+
+			FutureTask<Integer> futureTask = new FutureTask<>(callable);
+
+			executor.execute(futureTask);
+
+			taskList.add(futureTask);
+		}
+
+		executor.shutdown();
 	}
 
 	protected static void waitForTurn(File file, int maxHostNames)
@@ -258,42 +229,39 @@ public class LoadBalanceUtil {
 		}
 	}
 
-	protected static final String _HOSTNAME;
-
-	private static final long _MAX_AGE = 30 * 1000;
-
-	private static final long _MIN_RUN_INTERVAL = 15 * 1000;
-
-	protected static final Pattern _urlPattern = Pattern.compile(
+	protected static final String myHostName;
+	protected static final Pattern urlPattern = Pattern.compile(
 		"http://(?<hostNamePrefix>[\\S&&[^-]]+-\\d+).liferay.com");
 
 	static {
 		InetAddress inetAddress = null;
-		String hostName = null;
+		String inetHostName = null;
 
 		try {
 			inetAddress = InetAddress.getLocalHost();
-			hostName = inetAddress.getHostName();
+			inetHostName = inetAddress.getHostName();
 		}
 		catch (UnknownHostException uhe) {
-			hostName = "UNKNOWN";
+			inetHostName = "UNKNOWN";
 		}
 
-		_HOSTNAME = hostName;
+		myHostName = inetHostName;
 	}
 
-	protected static class IdleSlaveCounterCallable implements Callable<Integer> {
+	protected static class IdleSlaveCounterCallable
+		implements Callable<Integer> {
 
 		@Override
 		public Integer call() throws Exception {
 			JSONObject jsonObject = null;
 
 			try {
-				jsonObject = JenkinsResultsParserUtil.toJSONObject(_url, false);
-			} catch (Exception e) {
+				jsonObject = JenkinsResultsParserUtil.toJSONObject(url, false);
+			}
+			catch (Exception e) {
 				System.out.println(
 					"WARNING : Exception occurred while attempting to read: " +
-						_url);
+						url);
 				return -1;
 			}
 
@@ -312,12 +280,45 @@ public class LoadBalanceUtil {
 			return idle;
 		}
 
-		protected IdleSlaveCounterCallable(String url) {
-			_url = url;
+		protected IdleSlaveCounterCallable(String statusUrl) {
+			url = statusUrl;
 		}
 
-		protected final String _url;
+		protected final String url;
 
 	}
+
+	private static Project getProject() {
+		Project project = new Project();
+
+		project.setProperty("base.invocation.url", "http://test-1.liferay.com");
+		project.setProperty("jenkins.shared.dir", "mnt/mfs-ssd1-10.10/jenkins");
+		project.setProperty("jenkins.local.url[test-1-1]", "http://test-1-1");
+		project.setProperty("jenkins.local.url[test-1-2]", "http://test-1-2");
+		project.setProperty("jenkins.local.url[test-1-3]", "http://test-1-3");
+		project.setProperty("jenkins.local.url[test-1-4]", "http://test-1-4");
+		project.setProperty("jenkins.local.url[test-1-5]", "http://test-1-5");
+		project.setProperty("jenkins.local.url[test-1-6]", "http://test-1-6");
+		project.setProperty("jenkins.local.url[test-1-7]", "http://test-1-7");
+		project.setProperty("jenkins.local.url[test-1-8]", "http://test-1-8");
+		project.setProperty("jenkins.local.url[test-1-9]", "http://test-1-9");
+		project.setProperty("jenkins.local.url[test-1-10]", "http://test-1-10");
+		project.setProperty("jenkins.local.url[test-1-11]", "http://test-1-11");
+		project.setProperty("jenkins.local.url[test-1-12]", "http://test-1-12");
+		project.setProperty("jenkins.local.url[test-1-13]", "http://test-1-13");
+		project.setProperty("jenkins.local.url[test-1-14]", "http://test-1-14");
+		project.setProperty("jenkins.local.url[test-1-15]", "http://test-1-15");
+		project.setProperty("jenkins.local.url[test-1-16]", "http://test-1-16");
+		project.setProperty("jenkins.local.url[test-1-17]", "http://test-1-17");
+		project.setProperty("jenkins.local.url[test-1-18]", "http://test-1-18");
+		project.setProperty("jenkins.local.url[test-1-19]", "http://test-1-19");
+		project.setProperty("jenkins.local.url[test-1-20]", "http://test-1-20");
+
+		return project;
+	}
+
+	private static final long _MAX_AGE = 30 * 1000;
+
+	private static final long _MIN_RUN_INTERVAL = 15 * 1000;
 
 }
