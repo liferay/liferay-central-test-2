b1f9e60ee122b7aad63522d397bad5132ea1be8f
Enoch Chu <enoch.chu@liferay.com>
2015-11-20 14:53:21 -0800

    LPS-60505 Update UI for Upload and Install JAR, LPKG, or WAR

---
+++ modules/apps/marketplace/marketplace-app-manager-web/src/main/java/com/liferay/marketplace/app/manager/web/portlet/configuration/icon/UploadConfigurationIcon.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.marketplace.app.manager.web.portlet.configuration.icon;
+
+import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
+import com.liferay.portal.model.User;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Enoch Chu
+ */
+public class UploadConfigurationIcon extends BasePortletConfigurationIcon {
+
+	public UploadConfigurationIcon(HttpServletRequest request) {
+		super(request);
+	}
+
+	@Override
+	public String getMessage() {
+		return "upload";
+	}
+
+	@Override
+	public String getURL() {
+		return "javascript:;";
+	}
+
+	@Override
+	public boolean isShow() {
+		User user = themeDisplay.getUser();
+
+		if (user.isDefaultUser()) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean isToolTip() {
+		return false;
+	}
+
+}
---
+++ modules/apps/marketplace/marketplace-app-manager-web/src/main/java/com/liferay/marketplace/app/manager/web/portlet/configuration/icon/UploadConfigurationIconFactory.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.marketplace.app.manager.web.portlet.configuration.icon;
+
+import com.liferay.marketplace.app.manager.web.constants.MarketplaceAppManagerPortletKeys;
+import com.liferay.portal.kernel.portlet.configuration.icon.BaseJSPPortletConfigurationIconFactory;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIconFactory;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Enoch Chu
+ */
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + MarketplaceAppManagerPortletKeys.MARKETPLACE_APP_MANAGER},
+	service = PortletConfigurationIconFactory.class
+)
+public class UploadConfigurationIconFactory
+	extends BaseJSPPortletConfigurationIconFactory {
+
+	@Override
+	public PortletConfigurationIcon create(HttpServletRequest request) {
+		return new UploadConfigurationIcon(request);
+	}
+
+	@Override
+	public String getJspPath() {
+		return "/configuration/icon/install_from_file.jsp";
+	}
+
+	@Override
+	public double getWeight() {
+		return 102;
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.marketplace.app.manager.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+}
