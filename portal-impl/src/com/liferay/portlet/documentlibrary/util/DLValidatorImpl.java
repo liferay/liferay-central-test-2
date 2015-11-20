adaa7ec68a492c632bddfba4ff9a46ec29a4ad21
Brian Chan <brian.chan@liferay.com>
2015-11-20 13:50:05 -0800

    LPS-55676 SF

--- portal-impl/src/com/liferay/portlet/documentlibrary/util/DLValidatorImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/util/DLValidatorImpl.java
@@ -210,7 +210,7 @@ public final class DLValidatorImpl implements DLValidator {
 
 		if (!DLUtil.isValidVersion(versionLabel)) {
 			throw new InvalidFileVersionException(
-				"Invalid version label: " + versionLabel);
+				"Invalid version label " + versionLabel);
 		}
 	}
 
