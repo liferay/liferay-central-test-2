e5b351a7a181be6f2beaa1b1b388198e618b4b38
Peter Yoo <peter.yoo@liferay.com>
2015-11-20 14:47:36 -0800

    LRQA-19505 Created LoadBalanceUtilTest

---
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/load/balancer/LoadBalanceUtilTest.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jenkins.load.balancer;
+
+import java.io.File;
+import java.net.URL;
+
+import com.liferay.jenkins.results.parser.BaseJenkinsResultsParserTestCase;
+
+/**
+ * @author Peter Yoo
+ */
+public class LoadBalanceUtilTest extends BaseJenkinsResultsParserTestCase {
+
+	@Override
+	protected void downloadSample(File sampleDir, URL url) throws Exception {
+
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected String getMessage(String urlString) throws Exception {
+
+		// TODO Auto-generated method stub
+
+		return null;
+	}
+
+}
