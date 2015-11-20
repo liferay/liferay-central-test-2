5fc6b914873fa6db4fa7589d8c5ef2eb4236669c
Jim Hinkey <james.hinkey@liferay.com>
2015-11-20 11:42:55 -0500

    LPS-57844 Regenerate

--- portal-service/src/com/liferay/portal/service/GroupLocalService.java
+++ portal-service/src/com/liferay/portal/service/GroupLocalService.java
@@ -76,6 +76,7 @@ public interface GroupLocalService extends BaseLocalService,
 	<code>null</code>). Can set asset category IDs and asset tag
 	names for the group, and whether the group is for staging.
 	* @return the group
+	* @throws PortalException if a portal exception occurred
 	* @deprecated As of 6.2.0, replaced by {@link #addGroup(long, long, String,
 	long, long, Map, Map, int, boolean, int, String, boolean,
 	boolean, ServiceContext)}
@@ -106,6 +107,7 @@ public interface GroupLocalService extends BaseLocalService,
 	<code>null</code>). Can set asset category IDs and asset tag
 	names for the group, and whether the group is for staging.
 	* @return the group
+	* @throws PortalException if a portal exception occurred
 	* @deprecated As of 6.2.0, replaced by {@link #addGroup(long, long, String,
 	long, long, Map, Map, int, boolean, int, String, boolean,
 	boolean, ServiceContext)}
@@ -143,6 +145,7 @@ public interface GroupLocalService extends BaseLocalService,
 	<code>null</code>). Can set asset category IDs and asset tag
 	names for the group, and whether the group is for staging.
 	* @return the group
+	* @throws PortalException if a portal exception occured
 	* @deprecated As of 7.0.0, replaced by {@link #addGroup(long, long, String,
 	long, long, Map, Map, int, boolean, int, String, boolean,
 	boolean, ServiceContext)}
@@ -195,6 +198,7 @@ public interface GroupLocalService extends BaseLocalService,
 	<code>null</code>). Can set asset category IDs and asset tag
 	names for the group, and whether the group is for staging.
 	* @return the group
+	* @throws PortalException if a portal exception occurred
 	* @deprecated As of 6.2.0, replaced by {@link #addGroup(long, long, String,
 	long, long, Map, Map, int, boolean, int, String, boolean,
 	boolean, ServiceContext)}
@@ -250,6 +254,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* when a virtual host is added.
 	*
 	* @param companyId the primary key of the company
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public void checkCompanyGroup(long companyId) throws PortalException;
@@ -260,6 +265,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* and layouts.
 	*
 	* @param companyId the primary key of the company
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public void checkSystemGroups(long companyId) throws PortalException;
@@ -479,6 +485,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* @param companyId the primary key of the company
 	* @return the default user's personal site group, or <code>null</code> if a
 	matching group could not be found
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.Group fetchUserPersonalSiteGroup(
@@ -487,6 +494,14 @@ public interface GroupLocalService extends BaseLocalService,
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery getActionableDynamicQuery();
 
+	/**
+	* Returns all the active or inactive groups associated with the company.
+	*
+	* @param companyId the primary key of the company
+	* @param active whether to return only active groups, or only inactive
+	groups
+	* @return the active or inactive groups associated with the company
+	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public java.util.List<com.liferay.portal.model.Group> getActiveGroups(
 		long companyId, boolean active);
@@ -496,6 +511,7 @@ public interface GroupLocalService extends BaseLocalService,
 	*
 	* @param companyId the primary key of the company
 	* @return the group associated with the company
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.Group getCompanyGroup(long companyId)
@@ -538,6 +554,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* @param companyId the primary key of the company
 	* @param friendlyURL the group's friendlyURL
 	* @return the group with the friendly URL
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.Group getFriendlyURLGroup(long companyId,
@@ -549,6 +566,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* @param companyId the primary key of the company
 	* @param groupKey the group key
 	* @return the group with the group key
+	* @throws PortalException if a portal exception occurred
 	*/
 	@com.liferay.portal.kernel.spring.aop.Skip
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
@@ -650,6 +668,7 @@ public interface GroupLocalService extends BaseLocalService,
 	*
 	* @param groupIds the primary keys of the groups
 	* @return the groups with the primary keys
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public java.util.List<com.liferay.portal.model.Group> getGroups(
@@ -712,6 +731,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* @param companyId the primary key of the company
 	* @param plid the primary key of the layout
 	* @return the group associated with the layout
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.Group getLayoutGroup(long companyId,
@@ -723,6 +743,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* @param companyId the primary key of the company
 	* @param layoutPrototypeId the primary key of the layout prototype
 	* @return the group associated with the layout prototype
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.Group getLayoutPrototypeGroup(
@@ -734,6 +755,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* @param companyId the primary key of the company
 	* @param layoutSetPrototypeId the primary key of the layout set prototype
 	* @return the group associated with the layout set prototype
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.Group getLayoutSetPrototypeGroup(
@@ -865,6 +887,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* @param companyId the primary key of the company
 	* @param organizationId the primary key of the organization
 	* @return the group associated with the organization
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.Group getOrganizationGroup(long companyId,
@@ -922,6 +945,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* @param groupId the primary key of the group
 	* @return the group followed by all its parent groups ordered by closest
 	ancestor
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public java.util.List<com.liferay.portal.model.Group> getParentGroups(
@@ -962,6 +986,7 @@ public interface GroupLocalService extends BaseLocalService,
 	*
 	* @param liveGroupId the primary key of the live group
 	* @return the staging group
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.Group getStagingGroup(long liveGroupId)
@@ -973,6 +998,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* @param companyId the primary key of the company
 	* @param userId the primary key of the user
 	* @return the group directly associated with the user
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.Group getUserGroup(long companyId,
@@ -985,6 +1011,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* @param companyId the primary key of the company
 	* @param userGroupId the primary key of the user group
 	* @return the group associated with the user group
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.Group getUserGroupGroup(long companyId,
@@ -1028,6 +1055,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* @param inherit whether to include the user's inherited organization
 	groups and user groups
 	* @return the user's groups and immediate organization groups
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public java.util.List<com.liferay.portal.model.Group> getUserGroups(
@@ -1056,6 +1084,7 @@ public interface GroupLocalService extends BaseLocalService,
 	inclusive)
 	* @return the range of the user's groups and immediate organization groups
 	ordered by name
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public java.util.List<com.liferay.portal.model.Group> getUserGroups(
@@ -1083,6 +1112,7 @@ public interface GroupLocalService extends BaseLocalService,
 	*
 	* @param userGroups the user groups
 	* @return the groups associated with the user groups
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public java.util.List<com.liferay.portal.model.Group> getUserGroupsGroups(
@@ -1120,6 +1150,7 @@ public interface GroupLocalService extends BaseLocalService,
 	inclusive)
 	* @return the range of groups associated with the user's organization
 	groups
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public java.util.List<com.liferay.portal.model.Group> getUserOrganizationsGroups(
@@ -1130,6 +1161,7 @@ public interface GroupLocalService extends BaseLocalService,
 	*
 	* @param companyId the primary key of the company
 	* @return the default user's personal site group
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.Group getUserPersonalSiteGroup(
@@ -1220,6 +1252,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* @param companyId the primary key of the company
 	* @param groupKey the group key
 	* @return the group with the group key and associated company
+	* @throws PortalException if a portal exception occurred
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.Group loadGetGroup(long companyId,
@@ -1235,6 +1268,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* </p>
 	*
 	* @param companyId the primary key of the group's company
+	* @throws PortalException if a portal exception occurred
 	*/
 	public void rebuildTree(long companyId) throws PortalException;
 
@@ -2156,6 +2190,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* @param assetCategoryIds the primary keys of the asset categories
 	(optionally <code>null</code>)
 	* @param assetTagNames the asset tag names (optionally <code>null</code>)
+	* @throws PortalException if a portal exception occurred
 	*/
 	public void updateAsset(long userId, com.liferay.portal.model.Group group,
 		long[] assetCategoryIds, java.lang.String[] assetTagNames)
@@ -2168,6 +2203,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* @param friendlyURL the group's new friendlyURL (optionally
 	<code>null</code>)
 	* @return the group
+	* @throws PortalException if a portal exception occurred
 	*/
 	public com.liferay.portal.model.Group updateFriendlyURL(long groupId,
 		java.lang.String friendlyURL) throws PortalException;
@@ -2198,11 +2234,14 @@ public interface GroupLocalService extends BaseLocalService,
 	more information see {@link GroupConstants}.
 	* @param friendlyURL the group's new friendlyURL (optionally
 	<code>null</code>)
+	* @param inheritContent whether to inherit content from the parent
+	group
 	* @param active whether the group is active
 	* @param serviceContext the service context to be applied (optionally
 	<code>null</code>). Can set asset category IDs and asset tag
 	names for the group.
 	* @return the group
+	* @throws PortalException if a portal exception occurred
 	* @deprecated As of 7.0.0, replaced by {@link #updateGroup(long, long, Map,
 	Map, int, boolean, int, String, boolean, boolean,
 	ServiceContext)}
@@ -2232,6 +2271,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* @param typeSettings the group's new type settings (optionally
 	<code>null</code>)
 	* @return the group
+	* @throws PortalException if a portal exception occurred
 	*/
 	public com.liferay.portal.model.Group updateGroup(long groupId,
 		java.lang.String typeSettings) throws PortalException;
@@ -2242,6 +2282,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* @param groupId the primary key of the group
 	* @param site whether the group is to be associated with a main site
 	* @return the group
+	* @throws PortalException if a portal exception occurred
 	*/
 	public com.liferay.portal.model.Group updateSite(long groupId, boolean site)
 		throws PortalException;
