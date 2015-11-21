42dfb03da3d5738f7d1d727c89a171626c15f856
shuyangzhou <shuyang.zhou@liferay.com>
2015-11-21 14:31:39 -0800

    LPS-59298 Make it the invoker's responsibility to pass in proper type result.

--- portal-service/src/com/liferay/portal/kernel/cluster/ClusterNodeResponse.java
+++ portal-service/src/com/liferay/portal/kernel/cluster/ClusterNodeResponse.java
@@ -31,7 +31,7 @@ public class ClusterNodeResponse implements Serializable {
 	}
 
 	public static ClusterNodeResponse createResultClusterNodeResponse(
-		ClusterNode clusterNode, String uuid, Object result) {
+		ClusterNode clusterNode, String uuid, Serializable result) {
 
 		if ((result != null) && !(result instanceof Serializable)) {
 			return new ClusterNodeResponse(
@@ -39,8 +39,7 @@ public class ClusterNodeResponse implements Serializable {
 				new ClusterException("Return value is not serializable"));
 		}
 
-		return new ClusterNodeResponse(
-			clusterNode, uuid, (Serializable)result, null);
+		return new ClusterNodeResponse(clusterNode, uuid, result, null);
 	}
 
 	public ClusterNode getClusterNode() {
