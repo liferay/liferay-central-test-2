49e4a52cbad6b584211c3b184bd405afb97d11ea
Jorge Ferrer <jorge.ferrer@liferay.com>
2015-11-21 11:14:28 -0800

    LPS-60705 Remove database tables and indexes

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -401,30 +401,6 @@ create index IX_5EB4E2FB on Role_ (subtype[$COLUMN_LENGTH:75$]);
 create index IX_CBE204 on Role_ (type_, subtype[$COLUMN_LENGTH:75$]);
 create index IX_B9FF6043 on Role_ (uuid_[$COLUMN_LENGTH:75$], companyId);
 
-create index IX_630CC727 on SCFrameworkVersi_SCProductVers (companyId);
-create index IX_3BB93ECA on SCFrameworkVersi_SCProductVers (frameworkVersionId);
-create index IX_E8D33FF9 on SCFrameworkVersi_SCProductVers (productVersionId);
-
-create index IX_C98C0D78 on SCFrameworkVersion (companyId);
-create index IX_6E1764F on SCFrameworkVersion (groupId, active_);
-
-create index IX_5327BB79 on SCLicense (active_, recommended);
-
-create index IX_2EE8A074 on SCLicenses_SCProductEntries (companyId);
-create index IX_27006638 on SCLicenses_SCProductEntries (licenseId);
-create index IX_D7710A66 on SCLicenses_SCProductEntries (productEntryId);
-
-create index IX_5D25244F on SCProductEntry (companyId);
-create index IX_98E6A9CB on SCProductEntry (groupId, userId);
-create index IX_7311E812 on SCProductEntry (repoGroupId[$COLUMN_LENGTH:75$], repoArtifactId[$COLUMN_LENGTH:75$]);
-
-create index IX_AE8224CC on SCProductScreenshot (fullImageId);
-create index IX_DA913A55 on SCProductScreenshot (productEntryId, priority);
-create index IX_6C572DAC on SCProductScreenshot (thumbnailId);
-
-create index IX_7020130F on SCProductVersion (directDownloadURL[$COLUMN_LENGTH:2000$]);
-create index IX_8377A211 on SCProductVersion (productEntryId);
-
 create unique index IX_4F0315B8 on ServiceComponent (buildNamespace[$COLUMN_LENGTH:75$], buildNumber);
 
 create index IX_F542E9BC on SocialActivity (activitySetId);
