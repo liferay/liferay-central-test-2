8be30cbe7bf1de26f5dee6e5713fd8dece4bec05
Brian Chan <brian.chan@liferay.com>
2015-11-21 09:58:41 -0800

    LPS-59180 match BaseResourceBundlePublisher

--- modules/portal-security/portal-security-sso-facebook-connect/src/main/java/com/liferay/portal/security/sso/facebook/connect/language/PortalSettingsPortletResourceBundlePublisher.java
+++ modules/portal-security/portal-security-sso-facebook-connect/src/main/java/com/liferay/portal/security/sso/facebook/connect/language/PortalSettingsPortletResourceBundlePublisher.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.security.sso.facebook.connect.util;
+package com.liferay.portal.security.sso.facebook.connect.language;
 
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.language.BaseResourceBundlePublisher;
