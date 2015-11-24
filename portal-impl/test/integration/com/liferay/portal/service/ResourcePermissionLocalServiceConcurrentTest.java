70a62b97f227ab77a293b3ef46d9d53e4bc6c3f5
Matthew Tambara <matthew.tambara@liferay.com>
2015-11-24 11:42:56 -0800

    LPS-59190 Reenable Test

--- portal-impl/test/integration/com/liferay/portal/service/ResourcePermissionLocalServiceConcurrentTest.java
+++ portal-impl/test/integration/com/liferay/portal/service/ResourcePermissionLocalServiceConcurrentTest.java
@@ -56,7 +56,6 @@ import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.Before;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -68,9 +67,6 @@ import org.springframework.aop.framework.AdvisedSupport;
  * @author William Newbury
  * @author Shuyang Zhou
  */
-@Ignore(
-	"Ignored due to random hanging, Matthew Tambara will fix and reenable it"
-)
 public class ResourcePermissionLocalServiceConcurrentTest {
 
 	@ClassRule
