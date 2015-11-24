7aa6d1a915b86dc12d2da85cc7fe448dc13bc6dd
Leonardo Barros <leonardo.barros@liferay.com>
2015-11-24 11:27:51 -0300

    LPS-58995 Add default layout

--- modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/events/AddDefaultSharedPortletLayoutAction.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/events/AddDefaultSharedPortletLayoutAction.java
@@ -14,21 +14,21 @@
 
 package com.liferay.dynamic.data.lists.events;
 
-import com.liferay.dynamic.data.lists.model.DDLRecordSet;
-import com.liferay.dynamic.data.lists.service.DDLRecordSetLocalService;
-import com.liferay.dynamic.data.mapping.util.DefaultDDMStructureHelper;
 import com.liferay.portal.kernel.events.ActionException;
 import com.liferay.portal.kernel.events.SimpleAction;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.model.Company;
 import com.liferay.portal.model.Group;
+import com.liferay.portal.model.GroupConstants;
+import com.liferay.portal.model.LayoutConstants;
 import com.liferay.portal.security.auth.CompanyThreadLocal;
 import com.liferay.portal.service.CompanyLocalService;
 import com.liferay.portal.service.GroupLocalService;
+import com.liferay.portal.service.LayoutLocalService;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.service.UserLocalService;
-import com.liferay.portal.util.PortalUtil;
 
 import java.util.List;
 
@@ -37,10 +37,10 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
- * @author Michael C. Han
+ * @author Leonardo Barros
  */
 @Component(immediate = true)
-public class AddDefaultDDLStructuresAction extends SimpleAction {
+public class AddDefaultSharedPortletLayoutAction extends SimpleAction {
 
 	@Override
 	public void run(String[] ids) throws ActionException {
@@ -76,7 +76,8 @@ public class AddDefaultDDLStructuresAction extends SimpleAction {
 		serviceContext.setAddGuestPermissions(true);
 		serviceContext.setAddGroupPermissions(true);
 
-		Group group = _groupLocalService.getCompanyGroup(companyId);
+		Group group = _groupLocalService.getGroup(
+			companyId, GroupConstants.GUEST);
 
 		serviceContext.setScopeGroupId(group.getGroupId());
 
@@ -84,13 +85,11 @@ public class AddDefaultDDLStructuresAction extends SimpleAction {
 
 		serviceContext.setUserId(defaultUserId);
 
-		_defaultDDMStructureHelper.addDDMStructures(
-			defaultUserId, group.getGroupId(),
-			PortalUtil.getClassNameId(DDLRecordSet.class),
-			AddDefaultDDLStructuresAction.class.getClassLoader(),
-			"com/liferay/dynamic/data/lists/events/dependencies" +
-				"/default-dynamic-data-lists-structures.xml",
-			serviceContext);
+		_layoutLocalService.addLayout(
+			defaultUserId, group.getGroupId(), false,
+			LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, "shared",
+			StringPool.BLANK, StringPool.BLANK, "shared_portlet", true,
+			"/shared", serviceContext);
 	}
 
 	@Reference(unbind = "-")
@@ -101,20 +100,15 @@ public class AddDefaultDDLStructuresAction extends SimpleAction {
 	}
 
 	@Reference(unbind = "-")
-	protected void setDDLRecordSetLocalService(
-		DDLRecordSetLocalService ddlRecordSetLocalService) {
-	}
-
-	@Reference(unbind = "-")
-	protected void setDefaultDDMStructureHelper(
-		DefaultDDMStructureHelper defaultDDMStructureHelper) {
-
-		_defaultDDMStructureHelper = defaultDDMStructureHelper;
-	}
-
-	@Reference(unbind = "-")
 	protected void setGroupLocalService(GroupLocalService groupLocalService) {
 		_groupLocalService = groupLocalService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setLayoutLocalService(
+		LayoutLocalService layoutLocalService) {
+
+		_layoutLocalService = layoutLocalService;
 	}
 
 	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
@@ -128,8 +122,8 @@ public class AddDefaultDDLStructuresAction extends SimpleAction {
 	}
 
 	private volatile CompanyLocalService _companyLocalService;
-	private volatile DefaultDDMStructureHelper _defaultDDMStructureHelper;
 	private volatile GroupLocalService _groupLocalService;
+	private volatile LayoutLocalService _layoutLocalService;
 	private volatile UserLocalService _userLocalService;
 
 }
