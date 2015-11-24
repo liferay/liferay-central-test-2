990f612a2ec1bbb7cb3392d3d77e8d06100e7572
Ryan Park <ryan.park@liferay.com>
2015-11-24 10:35:54 -0800

    LPS-60750 Add ModuleGroupDisplay classes to assist generating front ends

---
+++ modules/apps/marketplace/marketplace-app-manager-web/src/main/java/com/liferay/marketplace/app/manager/web/util/ModuleGroupDisplay.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.marketplace.app.manager.web.util;
+
+import java.util.List;
+
+import org.osgi.framework.Bundle;
+
+/**
+ * @author Ryan Park
+ */
+public interface ModuleGroupDisplay extends Comparable<ModuleGroupDisplay> {
+
+	public static final String MODULE_GROUP_TITLE_UNCATEGORIZED =
+		"Uncategorized";
+
+	public void addBundle(Bundle bundle);
+
+	public List<Bundle> getBundles();
+
+	public String getDescription();
+
+	public int getState();
+
+	public String getTitle();
+
+	public String getVersion();
+
+}
