112b44c09d25fb206c7daa0fdcbdfe6b3138cd43
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-11-24 13:38:56 +0100

    LPS-60742 Removes custom css

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/portlet/LayoutAdminPortlet.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/portlet/LayoutAdminPortlet.java
@@ -125,7 +125,6 @@ import org.osgi.service.component.annotations.Reference;
 	property = {
 		"com.liferay.portlet.add-default-resource=true",
 		"com.liferay.portlet.css-class-wrapper=portlet-layouts-admin",
-		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.icon=/icons/default.png",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
