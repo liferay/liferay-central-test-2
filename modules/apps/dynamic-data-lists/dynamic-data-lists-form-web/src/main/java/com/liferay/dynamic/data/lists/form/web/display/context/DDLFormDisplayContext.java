52aa295f26feed8174c28ac78bf717646bc5357b
Leonardo Barros <leonardo.barros@liferay.com>
2015-11-25 08:40:50 -0300

    LPS-58995 Hide the select button when the form has been shared

--- modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/display/context/DDLFormDisplayContext.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/display/context/DDLFormDisplayContext.java
@@ -19,6 +19,7 @@ import com.liferay.dynamic.data.lists.model.DDLRecordSet;
 import com.liferay.dynamic.data.lists.service.DDLRecordSetLocalServiceUtil;
 import com.liferay.dynamic.data.lists.service.permission.DDLRecordSetPermission;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PrefsParamUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -85,6 +86,11 @@ public class DDLFormDisplayContext {
 			return _showConfigurationIcon;
 		}
 
+		if (isFormShared()) {
+			_showConfigurationIcon = false;
+			return _showConfigurationIcon;
+		}
+
 		ThemeDisplay themeDisplay = getThemeDisplay();
 
 		_showConfigurationIcon = PortletPermissionUtil.contains(
@@ -135,6 +141,10 @@ public class DDLFormDisplayContext {
 		}
 
 		return _hasViewPermission;
+	}
+
+	protected boolean isFormShared() {
+		return ParamUtil.get(_renderRequest, "shared", false);
 	}
 
 	private Boolean _hasViewPermission;
