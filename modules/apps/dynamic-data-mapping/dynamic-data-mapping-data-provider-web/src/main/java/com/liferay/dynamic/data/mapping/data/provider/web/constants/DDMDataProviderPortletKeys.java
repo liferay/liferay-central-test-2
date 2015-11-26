ada002012a3b15fc392fb60e72fc8e06e6c1dcc2
Brian Chan <brian.chan@liferay.com>
2015-11-26 13:40:23 -0800

    LPS-60299 SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-data-provider-web/src/main/java/com/liferay/dynamic/data/mapping/data/provider/web/constants/DDMDataProviderPortletKeys.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-data-provider-web/src/main/java/com/liferay/dynamic/data/mapping/data/provider/web/constants/DDMDataProviderPortletKeys.java
@@ -20,6 +20,7 @@ package com.liferay.dynamic.data.mapping.data.provider.web.constants;
 public class DDMDataProviderPortletKeys {
 
 	public static final String DYNAMIC_DATA_MAPPING_DATA_PROVIDER =
-		"com_liferay_dynamic_data_mapping_data_provider_web_portlet_DDMDataProviderPortlet";
+		"com_liferay_dynamic_data_mapping_data_provider_web_portlet_" +
+			"DDMDataProviderPortlet";
 
 }
