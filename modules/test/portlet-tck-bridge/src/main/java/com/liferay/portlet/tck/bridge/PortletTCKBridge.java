d4cba85a79046df289f609ae624809bbebfe8759
Brian Chan <brian.chan@liferay.com>
2015-11-27 14:16:18 -0800

    LPS-58575 Consistency

--- modules/test/portlet-tck-bridge/src/main/java/com/liferay/portlet/tck/bridge/PortletTCKBridge.java
+++ modules/test/portlet-tck-bridge/src/main/java/com/liferay/portlet/tck/bridge/PortletTCKBridge.java
@@ -25,6 +25,7 @@ import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.struts.StrutsActionRegistryUtil;
 import com.liferay.portlet.tck.bridge.configuration.PortletTCKBridgeConfiguration;
+import com.liferay.portlet.tck.bridge.struts.PortletTCKStrutsAction;
 
 import java.io.IOException;
 import java.io.OutputStream;
