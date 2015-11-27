dfd28b63d274b5c6dd5e45c6943ddc1cdc8ffcf6
Brian Chan <brian.chan@liferay.com>
2015-11-27 14:11:06 -0800

    LPS-58575 Consistency

--- modules/test/portlet-tck-bridge/src/main/java/com/liferay/portlet/tck/bridge/struts/PortletTCKStrutsAction.java
+++ modules/test/portlet-tck-bridge/src/main/java/com/liferay/portlet/tck/bridge/struts/PortletTCKStrutsAction.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portlet.tck.bridge;
+package com.liferay.portlet.tck.bridge.struts;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
