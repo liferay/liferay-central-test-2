8457479d93e22914e4d8cfe0d7d5d4c0a9feffb6
Chema Balsas <jose.balsas@liferay.com>
2015-11-29 19:07:18 +0100

    LPS-59825 Extract OpenStreet Map module

---
+++ modules/frontend/frontend-map/frontend-map-openstreet-web/src/main/resources/META-INF/resources/js/config.js
@@ -0,0 +1,24 @@
+;(function() {
+	var PATH_FRONTEND_MAP_OPENSTREET_WEB = Liferay.ThemeDisplay.getPathContext() + '/o/frontend-map-openstreet-web';
+
+	AUI().applyConfig(
+		{
+			groups: {
+				mapopenstreet: {
+					base: PATH_FRONTEND_MAP_OPENSTREET_WEB + '/js/',
+					modules: {
+						'liferay-map-openstreet': {
+							path: 'map.js',
+							requires: [
+								'jsonp',
+								'liferay-map-base',
+								'timers'
+							]
+						}
+					},
+					root: PATH_FRONTEND_MAP_OPENSTREET_WEB + '/js/'
+				}
+			}
+		}
+	);
+})();
