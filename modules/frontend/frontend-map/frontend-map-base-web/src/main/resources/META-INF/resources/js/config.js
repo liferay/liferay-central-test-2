d597b9b47ed8576321abe3e1b6cbb3c5732e4034
Chema Balsas <jose.balsas@liferay.com>
2015-11-29 19:01:54 +0100

    LPS-59825 Extract Base Map moduleo

---
+++ modules/frontend/frontend-map/frontend-map-base-web/src/main/resources/META-INF/resources/js/config.js
@@ -0,0 +1,22 @@
+;(function() {
+	var PATH_FRONTEND_MAP_BASE_WEB = Liferay.ThemeDisplay.getPathContext() + '/o/frontend-map-base-web';
+
+	AUI().applyConfig(
+		{
+			groups: {
+				mapbase: {
+					base: PATH_FRONTEND_MAP_BASE_WEB + '/js/',
+					modules: {
+						'liferay-map-base': {
+							path: 'map.js',
+							requires: [
+								'aui-base'
+							]
+						}
+					},
+					root: PATH_FRONTEND_MAP_BASE_WEB + '/js/'
+				}
+			}
+		}
+	);
+})();
