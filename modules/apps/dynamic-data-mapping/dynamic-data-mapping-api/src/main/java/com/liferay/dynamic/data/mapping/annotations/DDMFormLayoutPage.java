b4fe71cb5ce13ee773d4f0539f29d1dbc881ac22
Leonardo Barros <leonardo.barros@liferay.com>
2015-11-30 10:47:11 -0300

    LPS-60812 Create form's layout from annotations given

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/annotations/DDMFormLayoutPage.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/annotations/DDMFormLayoutPage.java
@@ -14,6 +14,8 @@
 
 package com.liferay.dynamic.data.mapping.annotations;
 
+import com.liferay.portal.kernel.util.StringPool;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -25,6 +27,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface DDMFormLayoutPage {
+
+	public String title() default StringPool.BLANK;
 
 	public DDMFormLayoutRow[] value() default {};
 
---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/util/DDMFormLayoutFactory.java
@@ -0,0 +1,123 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.util;
+
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayout;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayoutColumn;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayoutPage;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayoutRow;
+import com.liferay.dynamic.data.mapping.model.LocalizedValue;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+
+import java.lang.annotation.Annotation;
+
+/**
+ * @author Leonardo Barros
+ */
+public class DDMFormLayoutFactory {
+
+	public static com.liferay.dynamic.data.mapping.model.DDMFormLayout create(
+		Class<?> clazz) {
+
+		if (!clazz.isAnnotationPresent(_DDM_FORM_LAYOUT_ANNOTATION)) {
+			throw new IllegalArgumentException(
+				"Unsupported class " + clazz.getName());
+		}
+
+		return createDDMFormLayout(clazz);
+	}
+
+	private static com.liferay.dynamic.data.mapping.model.DDMFormLayout
+		createDDMFormLayout(Class<?> clazz) {
+
+		com.liferay.dynamic.data.mapping.model.DDMFormLayout ddmFormLayout =
+			new com.liferay.dynamic.data.mapping.model.DDMFormLayout();
+
+		DDMFormLayout ddmFormLayoutAnnotation =
+			(DDMFormLayout)clazz.getAnnotation(_DDM_FORM_LAYOUT_ANNOTATION);
+
+		DDMFormLayoutPage[] ddmFormLayoutPages =
+			ddmFormLayoutAnnotation.value();
+
+		for (DDMFormLayoutPage ddmFormLayoutPage : ddmFormLayoutPages) {
+			ddmFormLayout.addDDMFormLayoutPage(
+				createDDMFormLayoutPage(ddmFormLayoutPage));
+		}
+
+		return ddmFormLayout;
+	}
+
+	private static com.liferay.dynamic.data.mapping.model.DDMFormLayoutPage
+		createDDMFormLayoutPage(DDMFormLayoutPage ddmFormLayoutPageAnnotation) {
+
+		com.liferay.dynamic.data.mapping.model.DDMFormLayoutPage
+			ddmFormLayoutPage = new com.liferay.dynamic.data.mapping.model.DDMFormLayoutPage();
+
+		DDMFormLayoutRow[] ddmFormLayoutRows = ddmFormLayoutPageAnnotation.value();
+
+		LocalizedValue title = new LocalizedValue();
+		title.addString(
+			LocaleUtil.getDefault(), ddmFormLayoutPageAnnotation.title());
+
+		ddmFormLayoutPage.setTitle(title);
+
+		for (DDMFormLayoutRow ddmFormLayoutRow : ddmFormLayoutRows) {
+			ddmFormLayoutPage.addDDMFormLayoutRow(
+				createDDMFormLayoutRow(ddmFormLayoutRow));
+		}
+
+		return ddmFormLayoutPage;
+	}
+
+	private static com.liferay.dynamic.data.mapping.model.DDMFormLayoutRow
+		createDDMFormLayoutRow(DDMFormLayoutRow ddmFormLayoutRowAnnotation) {
+
+		com.liferay.dynamic.data.mapping.model.DDMFormLayoutRow
+			ddmFormLayoutRow = new com.liferay.dynamic.data.mapping.model.DDMFormLayoutRow();
+
+		DDMFormLayoutColumn[] ddmFormLayoutColumns =
+			ddmFormLayoutRowAnnotation.value();
+
+		for (DDMFormLayoutColumn ddmFormLayoutColumn : ddmFormLayoutColumns) {
+			ddmFormLayoutRow.addDDMFormLayoutColumn(
+				createDDMFormLayoutColumn(ddmFormLayoutColumn));
+		}
+
+		return ddmFormLayoutRow;
+	}
+
+	private static com.liferay.dynamic.data.mapping.model.DDMFormLayoutColumn
+		createDDMFormLayoutColumn(DDMFormLayoutColumn ddmFormLayoutColumnAnnotation) {
+
+		com.liferay.dynamic.data.mapping.model.DDMFormLayoutColumn
+			ddmFormLayoutColumn = new com.liferay.dynamic.data.mapping.model.DDMFormLayoutColumn();
+
+		String[] formFields = ddmFormLayoutColumnAnnotation.value();
+
+		ddmFormLayoutColumn.setDDMFormFieldNames(
+			ListUtil.fromArray(formFields));
+		ddmFormLayoutColumn.setSize(
+			com.liferay.dynamic.data.mapping.model.DDMFormLayoutColumn.FULL);
+
+		return ddmFormLayoutColumn;
+	}
+
+	private DDMFormLayoutFactory() {}
+
+	private static final Class<? extends Annotation>
+		_DDM_FORM_LAYOUT_ANNOTATION =
+			com.liferay.dynamic.data.mapping.annotations.DDMFormLayout.class;
+}
