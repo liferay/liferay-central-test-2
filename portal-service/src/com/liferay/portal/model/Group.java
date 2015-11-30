brianchandotcom/liferay-portal

LPS-60615 autogenerated
amend con autogenerated

portal-service/src/com/liferay/portal/model/Group.java
@@ -107,6 +107,9 @@ public interface Group extends GroupModel, PersistedModel, TreeModel {
 	public java.lang.String getLiveParentTypeSettingsProperty(
 		java.lang.String key);
 
+	public java.lang.String getLogo(
+		com.liferay.portal.theme.ThemeDisplay themeDisplay, boolean useDefault);
+
 	public long getOrganizationId();
 
 	public com.liferay.portal.model.Group getParentGroup()

portal-service/src/com/liferay/portal/model/GroupWrapper.java
@@ -552,6 +552,12 @@ public class GroupWrapper implements Group, ModelWrapper<Group> {
 		return _group.getLiveParentTypeSettingsProperty(key);
 	}
 
+	@Override
+	public java.lang.String getLogo(
+		com.liferay.portal.theme.ThemeDisplay themeDisplay, boolean useDefault) {
+		return _group.getLogo(themeDisplay, useDefault);
+	}
+
 	/**
 	* Returns the manual membership of this group.
 	*

liferay/liferay-central-test-2

portal-service/src/com/liferay/portal/model/Group.java
@@ -107,6 +107,9 @@ public interface Group extends GroupModel, PersistedModel, TreeModel {
 	public java.lang.String getLiveParentTypeSettingsProperty(
 		java.lang.String key);
 
+	public java.lang.String getLogoURL(
+		com.liferay.portal.theme.ThemeDisplay themeDisplay, boolean useDefault);
+
 	public long getOrganizationId();
 
 	public com.liferay.portal.model.Group getParentGroup()

portal-service/src/com/liferay/portal/model/GroupWrapper.java
@@ -552,6 +552,12 @@ public class GroupWrapper implements Group, ModelWrapper<Group> {
 		return _group.getLiveParentTypeSettingsProperty(key);
 	}
 
+	@Override
+	public java.lang.String getLogoURL(
+		com.liferay.portal.theme.ThemeDisplay themeDisplay, boolean useDefault) {
+		return _group.getLogoURL(themeDisplay, useDefault);
+	}
+
 	/**
 	* Returns the manual membership of this group.
 	*