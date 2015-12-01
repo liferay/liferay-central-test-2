64215e1e802947183206c5c1cd75fd76a7d29f71
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-12-01 13:35:27 +0100

    LPS-59594 Add entry point class to the tool, with tests for validation of arguments

---
+++ modules/util/portal-tools-shard-builder/src/test/java/com/liferay/portal/tools/shard/builder/MainTest.java
@@ -0,0 +1,149 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.tools.shard.builder;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import java.net.URL;
+
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class MainTest {
+
+	@Test
+	public void testValidateEmptyArguments() throws Exception {
+		try {
+			Main.main(new String[0]);
+
+			Assert.fail();
+		}
+		catch (IllegalArgumentException iae) {
+			Assert.assertEquals("Arguments cannot be null", iae.getMessage());
+		}
+	}
+
+	@Test(expected = FileNotFoundException.class)
+	public void testValidateNonExistingDatabaseFile() throws Exception {
+		String[] args = {
+			"foobar.properties", _DEFAULT_SCHEMA_NAME, _DEFAULT_COMPANY_ID,
+			"neverMindPath"
+		};
+
+		Main.main(args);
+	}
+
+	@Test(expected = FileNotFoundException.class)
+	public void testValidateNonExistingOutputFolder() throws Exception {
+		URL url = getClass().getResource("/mysql.properties");
+
+		String[] args = {
+			url.getFile(), _DEFAULT_SCHEMA_NAME, _DEFAULT_COMPANY_ID, "foo"
+		};
+
+		Main.main(args);
+	}
+
+	@Test
+	public void testValidateNonValidCompanyId() throws Exception {
+		URL url = getClass().getResource("/mysql.properties");
+
+		String[] args = {
+			url.getFile(), _DEFAULT_SCHEMA_NAME, "foo", "neverMindPath"
+		};
+
+		try {
+			Main.main(args);
+
+			Assert.fail();
+		}
+		catch (IllegalArgumentException iae) {
+			Assert.assertEquals(
+				"CompanyID is not a valid number", iae.getMessage());
+		}
+	}
+
+	@Test
+	public void testValidateNullArguments() throws Exception {
+		try {
+			Main.main(null);
+
+			Assert.fail();
+		}
+		catch (IllegalArgumentException iae) {
+			Assert.assertEquals("Arguments cannot be null", iae.getMessage());
+		}
+	}
+
+	@Test
+	public void testValidateReadOnlyOutputFolder() throws Exception {
+		URL url = getClass().getResource("/mysql.properties");
+
+		Assume.assumeNotNull(url);
+
+		File readOnlyFolder = temporaryFolder.newFolder();
+
+		readOnlyFolder.setReadable(false);
+		readOnlyFolder.setWritable(false);
+
+		String[] args = {
+			url.getFile(), _DEFAULT_SCHEMA_NAME, _DEFAULT_COMPANY_ID,
+			readOnlyFolder.getAbsolutePath()
+		};
+
+		try {
+			Main.main(args);
+
+			Assert.fail();
+		}
+		catch (IllegalArgumentException iae) {
+			Assert.assertEquals(
+				"Output directory is read-only", iae.getMessage());
+		}
+	}
+
+	@Test
+	public void testValidateWrongNumberOfArguments() throws Exception {
+		String expectedMessage =
+			"Only four argument can be passed: 1) database properties " +
+			"file, 2) name of the schema to export, 3) ID of the " +
+			"company to export, and 4) target folder where the " +
+			"inserts for a schema will be written";
+
+		try {
+			Main.main(new String[5]);
+
+			Assert.fail();
+		}
+		catch (IllegalArgumentException iae) {
+			Assert.assertEquals(expectedMessage, iae.getMessage());
+		}
+	}
+
+	@Rule
+	public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+	private static final String _DEFAULT_COMPANY_ID = "20156";
+
+	private static final String _DEFAULT_SCHEMA_NAME = "lportal";
+
+}
