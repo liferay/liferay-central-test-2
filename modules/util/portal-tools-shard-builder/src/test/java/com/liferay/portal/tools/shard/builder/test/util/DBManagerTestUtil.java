fd26795f861ed3ed7371790dce41fa83bc70c150
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-12-01 19:24:07 +0100

    LPS-59594 Add utility classes to manage SQL queries and connections

---
+++ modules/util/portal-tools-shard-builder/src/test/java/com/liferay/portal/tools/shard/builder/test/util/DBManagerTestUtil.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.tools.shard.builder.test.util;
+
+import com.liferay.portal.tools.shard.builder.internal.util.DBManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class DBManagerTestUtil {
+
+	public static int execute(DataSource dataSource, String sql, Object... args)
+		throws SQLException {
+
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = dataSource.getConnection();
+
+			ps = con.prepareStatement(sql);
+
+			for (int i = 0; i < args.length; i++) {
+				Object arg = args[i];
+
+				ps.setObject(i + 1, arg);
+			}
+
+			return ps.executeUpdate();
+		}
+		finally {
+			_dbManager.cleanUp(con, ps, rs);
+		}
+	}
+
+	public static boolean ping(DataSource dataSource, String sql)
+		throws SQLException {
+
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = dataSource.getConnection();
+
+			ps = con.prepareStatement(sql);
+
+			rs = ps.executeQuery();
+
+			return rs.next();
+		}
+		finally {
+			_dbManager.cleanUp(con, ps, rs);
+		}
+	}
+
+	private static final DBManager _dbManager = new DBManager();
+
+}
