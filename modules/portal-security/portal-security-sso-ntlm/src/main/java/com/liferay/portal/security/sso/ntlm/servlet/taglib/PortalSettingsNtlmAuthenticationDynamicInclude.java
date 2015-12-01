bd6459ee0f5fff265347677d9f6fb030a22edb47
philip.jones <philip.jones@liferay.com>
2015-11-30 23:26:34 -0800

    LPS-59178 Adapt ntlm.jsp to use NtlmConfiguration and serve using PortalSettingsNtlmAuthenticationDynamicInclude

---
+++ modules/portal-security/portal-security-sso-ntlm/src/main/java/com/liferay/portal/security/sso/ntlm/servlet/taglib/PortalSettingsNtlmAuthenticationDynamicInclude.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.sso.ntlm.servlet.taglib;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.taglib.BaseJSPDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Phililp Jones
+ */
+@Component(
+	immediate = true,
+	property = {"portal.settings.authentication.tabs.name=ntlm"},
+	service = DynamicInclude.class
+)
+public class PortalSettingsNtlmAuthenticationDynamicInclude
+	extends BaseJSPDynamicInclude {
+
+	protected String getJspPath() {
+		return "/com.liferay.portal.settings.web/ntlm.jsp";
+	}
+
+	protected Log getLog() {
+		return _log;
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.portal.security.sso.ntlm)",
+		unbind = "-"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		PortalSettingsNtlmAuthenticationDynamicInclude.class);
+
+}
