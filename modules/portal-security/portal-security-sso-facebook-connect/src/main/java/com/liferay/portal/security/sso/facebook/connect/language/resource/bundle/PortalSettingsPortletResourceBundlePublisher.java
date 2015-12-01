585503fd72978adbb32a88df5a9f57859f2cba44
Brian Chan <brian.chan@liferay.com>
2015-12-01 14:27:39 -0800

    LPS-59178 Move

--- modules/portal-security/portal-security-sso-facebook-connect/src/main/java/com/liferay/portal/security/sso/facebook/connect/language/resource/bundle/PortalSettingsPortletResourceBundlePublisher.java
+++ modules/portal-security/portal-security-sso-facebook-connect/src/main/java/com/liferay/portal/security/sso/facebook/connect/language/resource/bundle/PortalSettingsPortletResourceBundlePublisher.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.security.sso.facebook.connect.language;
+package com.liferay.portal.security.sso.facebook.connect.language.resource.bundle;
 
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.language.resource.bundle.BaseResourceBundlePublisher;
