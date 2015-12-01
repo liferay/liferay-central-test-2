aee940733843b2ee7f028e1e5999c51db1baa513
philip.jones <philip.jones@liferay.com>
2015-12-01 13:56:49 -0800

    LPS-59182 Adapt open_sso.jsp to use OpenSSOConfiguration and serve using PortalSettingsOpenSSOAuthenticationDynamicInclude

---
+++ modules/portal-security/portal-security-sso-opensso/src/main/java/com/liferay/portal/security/sso/opensso/servlet/taglib/PortalSettingsOpenSSOAuthenticationDynamicInclude.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.sso.opensso.servlet.taglib;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.taglib.BaseJSPDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Philip Jones
+ */
+@Component(
+	immediate = true,
+	property = {"portal.settings.authentication.tabs.name=open-sso"},
+	service = DynamicInclude.class
+)
+public class PortalSettingsOpenSSOAuthenticationDynamicInclude
+	extends BaseJSPDynamicInclude {
+
+	protected String getJspPath() {
+		return "/com.liferay.portal.settings.web/open_sso.jsp";
+	}
+
+	protected Log getLog() {
+		return _log;
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.portal.security.sso.opensso)",
+		unbind = "-"
+
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		PortalSettingsOpenSSOAuthenticationDynamicInclude.class);
+
+}
