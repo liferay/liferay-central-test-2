c32e907e6c623faccdc273303820fb0ce65f1180
Michael Han <michael.han@liferay.com>
2015-12-01 20:26:42 -0600

    LPS-59161 fix potential NPE in debug logging

--- modules/portal/portal-ldap/src/main/java/com/liferay/portal/ldap/internal/DefaultPortalLDAP.java
+++ modules/portal/portal-ldap/src/main/java/com/liferay/portal/ldap/internal/DefaultPortalLDAP.java
@@ -262,14 +262,21 @@ public class DefaultPortalLDAP implements PortalLDAP {
 				new String[mappedGroupAttributeIds.size()]));
 
 		if (_log.isDebugEnabled()) {
-			for (String attributeId : mappedGroupAttributeIds) {
-				Attribute attribute = attributes.get(attributeId);
+			if ((attributes == null) || (attributes.size() == 0)) {
+				_log.debug(
+					"No LDAP group attributes found for:: " +
+						fullDistinguishedName);
+			}
+			else {
+				for (String attributeId : mappedGroupAttributeIds) {
+					Attribute attribute = attributes.get(attributeId);
 
-				if (attribute == null) {
-					continue;
+					if (attribute == null) {
+						continue;
+					}
+
+					_log.debug("LDAP group attribute " + attribute.toString());
 				}
-
-				_log.debug("LDAP group attribute " + attribute.toString());
 			}
 		}
 
@@ -651,14 +658,21 @@ public class DefaultPortalLDAP implements PortalLDAP {
 			ldapContext, fullDistinguishedName, mappedUserAttributeIds);
 
 		if (_log.isDebugEnabled()) {
-			for (String attributeId : mappedUserAttributeIds) {
-				Attribute attribute = attributes.get(attributeId);
+			if ((attributes == null) || (attributes.size() == 0)) {
+				_log.debug(
+					"No LDAP user attributes found for:: " +
+						fullDistinguishedName);
+			}
+			else {
+				for (String attributeId : mappedUserAttributeIds) {
+					Attribute attribute = attributes.get(attributeId);
 
-				if (attribute == null) {
-					continue;
+					if (attribute == null) {
+						continue;
+					}
+
+					_log.debug("LDAP user attribute " + attribute.toString());
 				}
-
-				_log.debug("LDAP user attribute " + attribute.toString());
 			}
 		}
 
