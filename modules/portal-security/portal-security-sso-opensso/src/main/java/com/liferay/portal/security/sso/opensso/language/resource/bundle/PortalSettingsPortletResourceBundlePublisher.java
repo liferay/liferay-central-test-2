8b0a9b5f3bf918396b923f256defc364abf284c6
philip.jones <philip.jones@liferay.com>
2015-12-02 05:18:41 -0800

    LPS-59182 Register language keys to be available from Portal Settings portlet

---
+++ modules/portal-security/portal-security-sso-opensso/src/main/java/com/liferay/portal/security/sso/opensso/language/resource/bundle/PortalSettingsPortletResourceBundlePublisher.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.sso.opensso.language.resource.bundle;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.language.resource.bundle.BaseResourceBundlePublisher;
+import com.liferay.portal.settings.web.constants.PortalSettingsPortletKeys;
+
+import java.io.IOException;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Stian Sigvartsen
+ * @author Philip Jones
+ */
+@Component(immediate = true)
+public class PortalSettingsPortletResourceBundlePublisher
+	extends BaseResourceBundlePublisher {
+
+	@Activate
+	protected void doActivate(BundleContext bundleContext) throws IOException {
+		super.doActivate(bundleContext);
+	}
+
+	@Deactivate
+	protected void doDeactivate() {
+		super.doDeactivate();
+	}
+
+	protected String getPortletName() {
+		return PortalSettingsPortletKeys.PORTAL_SETTINGS;
+	}
+
+	@Modified
+	protected void modified(BundleContext bundleContext) throws IOException {
+		super.doModified(bundleContext);
+	}
+
+	@Reference(unbind = "-")
+	protected void setLanguageUtil(LanguageUtil languageUtil) {
+		super.setLanguageUtil(languageUtil);
+	}
+
+}
