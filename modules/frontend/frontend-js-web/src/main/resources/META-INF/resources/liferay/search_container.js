be34a606fb47a7905aae5857e1ef8173b346af3e
Chema Balsas <jose.balsas@liferay.com>
2015-12-02 10:12:52 +0100

    LPS-60834 Avoid unnecessary classes

--- modules/frontend/frontend-js-web/src/main/resources/META-INF/resources/liferay/search_container.js
+++ modules/frontend/frontend-js-web/src/main/resources/META-INF/resources/liferay/search_container.js
@@ -7,7 +7,7 @@ AUI.add(
 
 		var STR_BLANK = '';
 
-		var STR_CONTENT_BOX = 'contentBox';
+		var STR_BOUNDING_BOX = 'boundingBox';
 
 		var SearchContainer = A.Component.create(
 			{
@@ -63,7 +63,7 @@ AUI.add(
 
 						var id = instance.get('id');
 
-						var contentBox = instance.get(STR_CONTENT_BOX);
+						var boundingBox = instance.get(STR_BOUNDING_BOX);
 
 						instance._dataStore = A.one('#' + id + 'PrimaryKeys');
 						instance._emptyResultsMessage = A.one('#' + id + 'EmptyResultsMessage');
@@ -80,8 +80,8 @@ AUI.add(
 							}
 						}
 
-						instance._table = contentBox.one('table');
-						instance._parentContainer = contentBox.ancestor('.lfr-search-container');
+						instance._table = boundingBox.one('table');
+						instance._parentContainer = boundingBox.ancestor();
 
 						if (instance._table) {
 							instance._table.setAttribute('data-searchContainerId', id);
