76e85b13e26313e69552f243a5854f3af5ae43b6
Brian Chan <brian.chan@liferay.com>
2015-12-02 14:49:21 -0800

    LRQA-19732 SF

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/FailureMessageUtil.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/FailureMessageUtil.java
@@ -108,11 +108,11 @@ public class FailureMessageUtil {
 		}
 
 		for (Pattern pattern : _messagePatterns) {
-			Matcher sfMatcher = pattern.matcher(consoleOutput);
+			Matcher messageMatcher = pattern.matcher(consoleOutput);
 
-			if (sfMatcher.find()) {
+			if (messageMatcher.find()) {
 				sb.append("<pre>");
-				sb.append(sfMatcher.group("snippet"));
+				sb.append(messageMatcher.group("snippet"));
 				sb.append("</pre>");
 
 				return sb.toString();
@@ -169,14 +169,11 @@ public class FailureMessageUtil {
 	}
 
 	private static final Pattern[] _messagePatterns = new Pattern[] {
-
-		// SourceFormatter
-
 		Pattern.compile(
 			"(?<snippet>^\\s+\\[exec\\] " +
 				"com.liferay.source.formatter.SourceFormatterTest > " +
 				"testSourceFormatter FAILED.*?)^\\s+\\[exec\\]\\s+\\:",
-			Pattern.MULTILINE | Pattern.DOTALL)
+			Pattern.DOTALL | Pattern.MULTILINE)
 	};
 
 	private static final Pattern _pattern = Pattern.compile(
