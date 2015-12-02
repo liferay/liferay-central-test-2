4b29629f415e75a48d6bb278f2ed654a1bdbd084
Roberto Díaz <roberto.diaz@liferay.com>
2015-12-02 17:41:18 +0100

    LPS-60682 Change the object API and make it simpler

--- portal-service/src/com/liferay/portal/kernel/servlet/taglib/ui/ImageSelector.java
+++ portal-service/src/com/liferay/portal/kernel/servlet/taglib/ui/ImageSelector.java
@@ -14,136 +14,70 @@
 
 package com.liferay.portal.kernel.servlet.taglib.ui;
 
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.image.ImageBag;
-import com.liferay.portal.kernel.image.ImageToolUtil;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.repository.model.FileEntry;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.portletfilerepository.PortletFileRepositoryUtil;
-
-import java.awt.image.RenderedImage;
-
-import java.io.IOException;
+import com.liferay.portal.kernel.util.StringPool;
 
 /**
  * @author Sergio González
+ * @author Roberto Díaz
  */
 public class ImageSelector {
 
-	public ImageSelector(long imageId) {
-		_imageId = imageId;
-
-		_imageCropRegion = null;
-		_imageURL = null;
-	}
-
-	public ImageSelector(long imageId, String imageCropRegion) {
-		_imageId = imageId;
-		_imageCropRegion = imageCropRegion;
-
-		_imageURL = null;
+	public ImageSelector() {
+		this(
+			null, StringPool.BLANK, StringPool.BLANK, StringPool.BLANK,
+			StringPool.BLANK);
 	}
 
 	public ImageSelector(
-		long imageId, String imageURL, String imageCropRegion) {
+		byte[] imageBytes, String imageTitle, String imageMimeType,
+		String imageCropRegion) {
 
-		_imageId = imageId;
-		_imageURL = imageURL;
-		_imageCropRegion = imageCropRegion;
+		this(
+			imageBytes, imageTitle, imageMimeType, imageCropRegion,
+			StringPool.BLANK);
 	}
 
-	public ImageSelector(String imageURL, String imageCropRegion) {
-		_imageURL = imageURL;
-		_imageCropRegion = imageCropRegion;
-
-		_imageId = 0;
+	public ImageSelector(String coverImageURL) {
+		this(
+			null, StringPool.BLANK, StringPool.BLANK, StringPool.BLANK,
+			coverImageURL);
 	}
 
-	public byte[] getCroppedImageBytes() throws IOException, PortalException {
-		if (!isCroppedImage()) {
-			return null;
-		}
-
-		FileEntry fileEntry = PortletFileRepositoryUtil.getPortletFileEntry(
-			_imageId);
-
-		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
-			_imageCropRegion);
-
-		int height = jsonObject.getInt("height");
-		int width = jsonObject.getInt("width");
-		int x = jsonObject.getInt("x");
-		int y = jsonObject.getInt("y");
-
-		if ((x > 0) || (y > 0) || (width > 0) || (height > 0)) {
-			ImageBag imageBag = ImageToolUtil.read(
-				fileEntry.getContentStream());
-
-			RenderedImage renderedImage = imageBag.getRenderedImage();
-
-			renderedImage = ImageToolUtil.crop(
-				renderedImage, height, width, x, y);
-
-			return ImageToolUtil.getBytes(renderedImage, imageBag.getType());
-		}
-
-		return null;
+	public byte[] getImageBytes() {
+		return _imageBytes;
 	}
 
 	public String getImageCropRegion() {
 		return _imageCropRegion;
 	}
 
-	public long getImageId() {
-		return _imageId;
+	public String getImageMimeType() {
+		return _imageMimeType;
+	}
+
+	public String getImageTitle() {
+		return _imageTitle;
 	}
 
 	public String getImageURL() {
 		return _imageURL;
 	}
 
-	public String getMimeType() throws PortalException {
-		if (_imageId == 0) {
-			return null;
-		}
+	private ImageSelector(
+		byte[] imageBytes, String imageTitle, String imageMimeType,
+		String imageCropRegion, String imageURL) {
 
-		FileEntry fileEntry = PortletFileRepositoryUtil.getPortletFileEntry(
-			_imageId);
-
-		return fileEntry.getMimeType();
+		_imageBytes = imageBytes;
+		_imageTitle = imageTitle;
+		_imageMimeType = imageMimeType;
+		_imageCropRegion = imageCropRegion;
+		_imageURL = imageURL;
 	}
 
-	public String getTitle() throws PortalException {
-		if (_imageId == 0) {
-			return null;
-		}
-
-		FileEntry fileEntry = PortletFileRepositoryUtil.getPortletFileEntry(
-			_imageId);
-
-		return fileEntry.getTitle();
-	}
-
-	public boolean isCroppedImage() {
-		if ((_imageId == 0) || Validator.isNull(_imageCropRegion)) {
-			return false;
-		}
-
-		return true;
-	}
-
-	public boolean isRemoveSmallImage() {
-		if ((_imageId == 0) && Validator.isNull(_imageURL)) {
-			return true;
-		}
-
-		return false;
-	}
-
+	private final byte[] _imageBytes;
 	private final String _imageCropRegion;
-	private final long _imageId;
+	private final String _imageMimeType;
+	private final String _imageTitle;
 	private final String _imageURL;
 
 }
