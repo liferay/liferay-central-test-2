299a4acc8462ea7a64e684e5c675c9928d96ced3
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-12-02 12:43:46 +0100

    LPS-60980 Notification Portlets

---
+++ modules/apps/notifications/notifications-web/src/main/java/com/liferay/notifications/web/portlet/DockbarNotificationsPortlet.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.notifications.web.portlet;
+
+import com.liferay.notifications.web.constants.NotificationsPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Sergio González
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.css-class-wrapper=notifications-portlet",
+		"com.liferay.portlet.display-category=hidden",
+		"com.liferay.portlet.footer-portlet-javascript=/js/main.js",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Dockbar Notifications",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.add-process-action-success-action=false",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/dockbar_notifications/view.jsp",
+		"javax.portlet.name=" + NotificationsPortletKeys.DOCKBAR_NOTIFICATIONS,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator,guest,power-user,user",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
+public class DockbarNotificationsPortlet extends MVCPortlet {
+}
