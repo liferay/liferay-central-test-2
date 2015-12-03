72964708e4f021477fc53b0245ba0eb5b8d52193
Eduardo Garcia <eduardo.garcia@liferay.com>
2015-12-03 12:53:48 +0100

    LPS-60835 Add hidden panel category

--- modules/apps/application-list/application-list-api/src/main/java/com/liferay/application/list/constants/PanelCategoryKeys.java
+++ modules/apps/application-list/application-list-api/src/main/java/com/liferay/application/list/constants/PanelCategoryKeys.java
@@ -38,6 +38,8 @@ public interface PanelCategoryKeys {
 	public static final String CONTROL_PANEL_USERS =
 		PortletCategoryKeys.CONTROL_PANEL_USERS;
 
+	public static final String HIDDEN = "hidden";
+
 	public static final String ROOT = "root";
 
 	public static final String SITE_ADMINISTRATION = "site_administration";
