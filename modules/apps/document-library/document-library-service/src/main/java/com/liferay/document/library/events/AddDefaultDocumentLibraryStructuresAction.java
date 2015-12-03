4f0daa43a5663494040a0648dfb27a2fa7bec3c3
hai.yu <8219754@liferay.com>
2015-12-03 13:58:42 +0800

    LPS-60939 When create default data in the startup process, add availableLocales for ddmStructure (Document Types).

--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/events/AddDefaultDocumentLibraryStructuresAction.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/events/AddDefaultDocumentLibraryStructuresAction.java
@@ -32,8 +32,6 @@ import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.upgrade.util.UpgradeProcessUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.xml.Document;
@@ -149,10 +147,12 @@ public class AddDefaultDocumentLibraryStructuresAction extends SimpleAction {
 				groupId, dlFileEntryTypeKey);
 
 		if (dlFileEntryType == null) {
-			Map<Locale, String> localizationMap =
-				LocalizationUtil.getLocalizationMap(
-					LanguageUtil.getSupportedLocales(), LocaleUtil.getDefault(),
-					languageKey);
+			Map<Locale, String> localizationMap = new HashMap<>();
+
+			for (Locale curLocale : LanguageUtil.getAvailableLocales(groupId)) {
+				localizationMap.put(
+					curLocale, LanguageUtil.get(curLocale, languageKey));
+			}
 
 			_dlFileEntryTypeLocalService.addFileEntryType(
 				userId, groupId, dlFileEntryTypeKey, localizationMap,
