dc3f5206940e1428bdf77495df3a9523b2234bc3
Juergen Kappler <jurgen.kappler@liferay.com>
2015-12-04 09:05:39 +0100

    LPS-59825 Create GoogleMapProvider module, and views

---
+++ modules/frontend/frontend-map/frontend-map-google-web/src/main/java/com/liferay/frontend/map/google/web/GoogleMapProvider.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.map.google.web;
+
+import com.liferay.frontend.map.api.BaseJSPMapProvider;
+import com.liferay.frontend.map.api.MapProvider;
+import com.liferay.frontend.map.google.web.constants.GoogleMapConstants;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(immediate = true, service = MapProvider.class)
+public class GoogleMapProvider extends BaseJSPMapProvider {
+
+	@Override
+	public String getConfigurationJspPath() {
+		return "/configuration.jsp";
+	}
+
+	@Override
+	public String getHelpMessage() {
+		return "use-google-maps-as-the-maps-api-provider";
+	}
+
+	@Override
+	public String getJspPath() {
+		return "/view.jsp";
+	}
+
+	@Override
+	public String getKey() {
+		return GoogleMapConstants.GOOGLE_MAP_PROVIDER;
+	}
+
+	@Override
+	public String getLabel() {
+		return "google-maps";
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.frontend.map.google.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+}
