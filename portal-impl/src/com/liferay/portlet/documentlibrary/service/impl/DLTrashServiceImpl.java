bd44edb539ad6492b3dce48b80180f0e23128b4a
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-12-06 18:54:56 +0100

    LPS-60902 SF

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLTrashServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLTrashServiceImpl.java
@@ -33,18 +33,7 @@ import com.liferay.portlet.documentlibrary.service.permission.DLFileShortcutPerm
 import com.liferay.portlet.documentlibrary.service.permission.DLFolderPermission;
 
 /**
- * The implementation of the d l trash remote service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.portlet.documentlibrary.service.DLTrashService} interface.
- *
- * <p>
- * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
- * </p>
- *
- * @author Brian Wing Shun Chan
- * @see DLTrashServiceBaseImpl
- * @see com.liferay.portlet.documentlibrary.service.DLTrashServiceUtil
+ * @author Adolfo Pérez
  */
 @ProviderType
 public class DLTrashServiceImpl extends DLTrashServiceBaseImpl {
