akosthurzo/liferay-portal

LPS-60801 build-service

portal-service/src/com/liferay/portlet/asset/service/AssetLinkLocalService.java
@@ -136,6 +136,8 @@ public interface AssetLinkLocalService extends BaseLocalService,
 	*/
 	public void deleteLinks(long entryId1, long entryId2);
 
+	public void deleteLinksByGroupId(long groupId);
+
 	/**
 	* @throws PortalException
 	*/

portal-service/src/com/liferay/portlet/asset/service/AssetLinkLocalServiceUtil.java
@@ -146,6 +146,10 @@ public class AssetLinkLocalServiceUtil {
 		getService().deleteLinks(entryId1, entryId2);
 	}
 
+	public static void deleteLinksByGroupId(long groupId) {
+		getService().deleteLinksByGroupId(groupId);
+	}
+
 	/**
 	* @throws PortalException
 	*/

portal-service/src/com/liferay/portlet/asset/service/AssetLinkLocalServiceWrapper.java
@@ -147,6 +147,11 @@ public class AssetLinkLocalServiceWrapper implements AssetLinkLocalService,
 		_assetLinkLocalService.deleteLinks(entryId1, entryId2);
 	}
 
+	@Override
+	public void deleteLinksByGroupId(long groupId) {
+		_assetLinkLocalService.deleteLinksByGroupId(groupId);
+	}
+
 	/**
 	* @throws PortalException
 	*/

liferay/liferay-central-test-2

portal-service/src/com/liferay/portlet/asset/service/AssetLinkLocalService.java
@@ -136,6 +136,8 @@ public interface AssetLinkLocalService extends BaseLocalService,
 	*/
 	public void deleteLinks(long entryId1, long entryId2);
 
+	public void deleteLinksByAssetEntryGroupId(long groupId);
+
 	/**
 	* @throws PortalException
 	*/

portal-service/src/com/liferay/portlet/asset/service/AssetLinkLocalServiceUtil.java
@@ -146,6 +146,10 @@ public class AssetLinkLocalServiceUtil {
 		getService().deleteLinks(entryId1, entryId2);
 	}
 
+	public static void deleteLinksByAssetEntryGroupId(long groupId) {
+		getService().deleteLinksByAssetEntryGroupId(groupId);
+	}
+
 	/**
 	* @throws PortalException
 	*/

portal-service/src/com/liferay/portlet/asset/service/AssetLinkLocalServiceWrapper.java
@@ -147,6 +147,11 @@ public class AssetLinkLocalServiceWrapper implements AssetLinkLocalService,
 		_assetLinkLocalService.deleteLinks(entryId1, entryId2);
 	}
 
+	@Override
+	public void deleteLinksByAssetEntryGroupId(long groupId) {
+		_assetLinkLocalService.deleteLinksByAssetEntryGroupId(groupId);
+	}
+
 	/**
 	* @throws PortalException
 	*/