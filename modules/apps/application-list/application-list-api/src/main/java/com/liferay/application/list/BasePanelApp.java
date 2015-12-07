6b69284044ec4ff342573c5049ee88743f0c268d
Julio Camarero <julio.camarero@liferay.com>
2015-12-07 18:51:27 +0100

    LPS-61013 use latent group when needed

--- modules/apps/application-list/application-list-api/src/main/java/com/liferay/application/list/BasePanelApp.java
+++ modules/apps/application-list/application-list-api/src/main/java/com/liferay/application/list/BasePanelApp.java
@@ -14,17 +14,22 @@
 
 package com.liferay.application.list;
 
+import com.liferay.application.list.util.LatentGroupManagerUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Portlet;
 import com.liferay.portal.model.User;
 import com.liferay.portal.model.UserNotificationDeliveryConstants;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.service.UserNotificationEventLocalService;
+import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.PortalUtil;
+import com.liferay.portal.util.PortletCategoryKeys;
 import com.liferay.portlet.ControlPanelEntry;
 
 import java.io.IOException;
@@ -129,7 +134,28 @@ public abstract class BasePanelApp implements PanelApp {
 	}
 
 	protected Group getGroup(HttpServletRequest request) {
-		return null;
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		Group group = themeDisplay.getScopeGroup();
+
+		if (!group.isControlPanel()) {
+			return null;
+		}
+
+		Portlet portlet = getPortlet();
+
+		String controlPanelEntryCategory =
+			portlet.getControlPanelEntryCategory();
+
+		if (Validator.isNull(controlPanelEntryCategory) ||
+			!controlPanelEntryCategory.startsWith(
+				PortletCategoryKeys.SITE_ADMINISTRATION)) {
+
+			return null;
+		}
+
+		return LatentGroupManagerUtil.getLatentGroup(request.getSession());
 	}
 
 	protected void setUserNotificationEventLocalService(
