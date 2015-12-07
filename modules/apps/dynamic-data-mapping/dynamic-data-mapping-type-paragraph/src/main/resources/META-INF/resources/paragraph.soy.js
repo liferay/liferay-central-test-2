00a306b7a5859ad5ee749d532b4e478e49361755
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-12-07 12:29:46 -0300

    LPS-54236 Regenerate

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-paragraph/src/main/resources/META-INF/resources/paragraph.soy.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-paragraph/src/main/resources/META-INF/resources/paragraph.soy.js
@@ -5,5 +5,5 @@ if (typeof ddm == 'undefined') { var ddm = {}; }
 
 
 ddm.paragraph = function(opt_data, opt_ignored) {
-return '\t<div class="form-group form-inline' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + '" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '"><p dir="' + soy.$$escapeHtml(opt_data.dir) + '">' + soy.$$escapeHtml(opt_data.text) + '</p></div>';
+return '\t<div class="form-group form-inline' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + ' liferay-ddm-form-field-paragraph" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '"><p dir="' + soy.$$escapeHtml(opt_data.dir) + '">' + soy.$$escapeHtml(opt_data.text) + '</p></div>';
 };
