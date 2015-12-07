evannagayama/liferay-portal

LPS-60615 show the latent site UI panel even when the user is in the control panel

modules/apps/product-navigation/product-navigation-site-administration/src/main/java/com/liferay/product/navigation/site/administration/application/list/SiteAdministrationPanelCategory.java
@@ -19,14 +19,17 @@ import com.liferay.application.list.PanelCategory;
 import com.liferay.application.list.constants.PanelCategoryKeys;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.servlet.PortalSessionThreadLocal;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.service.permission.GroupPermissionUtil;
+import com.liferay.product.navigation.site.administration.util.LatentGroupManager;
 
 import java.util.Locale;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -75,7 +78,13 @@ public class SiteAdministrationPanelCategory extends BaseJSPPanelCategory {
 		throws PortalException {
 
 		if (group.isControlPanel()) {
-			return false;
+			HttpSession session = PortalSessionThreadLocal.getHttpSession();
+
+			group = LatentGroupManager.getLatentGroup(session);
+
+			if (group == null) {
+				return false;
+			}
 		}
 
 		if (GroupPermissionUtil.contains(

modules/apps/product-navigation/product-navigation-site-administration/src/main/java/com/liferay/product/navigation/site/administration/display/context/SiteAdministrationPanelCategoryDisplayContext.java
@@ -35,12 +35,16 @@ import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.portlet.exportimport.staging.StagingUtil;
+import com.liferay.product.navigation.site.administration.util.LatentGroupManager;
 
 import java.util.List;
 
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletResponse;
 import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author Julio Camarero
@@ -72,6 +76,19 @@ public class SiteAdministrationPanelCategoryDisplayContext {
 		}
 
 		_group = _themeDisplay.getScopeGroup();
+
+		HttpSession session = getSession();
+
+		Group latentGroup = LatentGroupManager.getLatentGroup(session);
+
+		if (_group.isControlPanel()) {
+			_group = latentGroup;
+		}
+		else if ((latentGroup != null) &&
+				 (_group.getGroupId() != latentGroup.getGroupId())) {
+
+			LatentGroupManager.setLatentGroup(session, _group);
+		}
 
 		return _group;
 	}
@@ -312,6 +329,13 @@ public class SiteAdministrationPanelCategoryDisplayContext {
 		}
 
 		return getGroupAdministrationURL(group);
+	}
+
+	protected HttpSession getSession() {
+		HttpServletRequest request = PortalUtil.getHttpServletRequest(
+			_portletRequest);
+
+		return request.getSession();
 	}
 
 	protected boolean hasStagingPermission() throws PortalException {

liferay/liferay-central-test-2

modules/apps/product-navigation/product-navigation-site-administration/src/main/java/com/liferay/product/navigation/site/administration/application/list/SiteAdministrationPanelCategory.java
@@ -19,14 +19,17 @@ import com.liferay.application.list.PanelCategory;
 import com.liferay.application.list.constants.PanelCategoryKeys;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.servlet.PortalSessionThreadLocal;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.service.permission.GroupPermissionUtil;
+import com.liferay.product.navigation.site.administration.util.LatentGroupManagerUtil;
 
 import java.util.Locale;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -75,7 +78,13 @@ public class SiteAdministrationPanelCategory extends BaseJSPPanelCategory {
 		throws PortalException {
 
 		if (group.isControlPanel()) {
-			return false;
+			HttpSession session = PortalSessionThreadLocal.getHttpSession();
+
+			group = LatentGroupManagerUtil.getLatentGroup(session);
+
+			if (group == null) {
+				return false;
+			}
 		}
 
 		if (GroupPermissionUtil.contains(

modules/apps/product-navigation/product-navigation-site-administration/src/main/java/com/liferay/product/navigation/site/administration/display/context/SiteAdministrationPanelCategoryDisplayContext.java
@@ -35,12 +35,16 @@ import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.portlet.exportimport.staging.StagingUtil;
+import com.liferay.product.navigation.site.administration.util.LatentGroupManagerUtil;
 
 import java.util.List;
 
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletResponse;
 import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author Julio Camarero
@@ -72,6 +76,19 @@ public class SiteAdministrationPanelCategoryDisplayContext {
 		}
 
 		_group = _themeDisplay.getScopeGroup();
+
+		HttpSession session = getSession();
+
+		Group latentGroup = LatentGroupManagerUtil.getLatentGroup(session);
+
+		if (_group.isControlPanel()) {
+			_group = latentGroup;
+		}
+		else if ((latentGroup != null) &&
+				 (_group.getGroupId() != latentGroup.getGroupId())) {
+
+			LatentGroupManagerUtil.setLatentGroup(session, _group);
+		}
 
 		return _group;
 	}
@@ -312,6 +329,13 @@ public class SiteAdministrationPanelCategoryDisplayContext {
 		}
 
 		return getGroupAdministrationURL(group);
+	}
+
+	protected HttpSession getSession() {
+		HttpServletRequest request = PortalUtil.getHttpServletRequest(
+			_portletRequest);
+
+		return request.getSession();
 	}
 
 	protected boolean hasStagingPermission() throws PortalException {