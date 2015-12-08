cb1087240aa5daffd5f2bb0d029ce57844db76ed
Máté Thurzó <mate.thurzo@liferay.com>
2015-12-08 15:28:50 +0100

    LPS-60901 Move page ratings portlet data handler to the service bundle

--- modules/apps/ratings/ratings-service/src/main/java/com/liferay/ratings/lar/PageRatingsPortletDataHandler.java
+++ modules/apps/ratings/ratings-service/src/main/java/com/liferay/ratings/lar/PageRatingsPortletDataHandler.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.ratings.page.ratings.web.lar;
+package com.liferay.ratings.lar;
 
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.exception.PortalException;
