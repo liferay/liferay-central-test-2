6255fdacdee8e845421dd6846fe1a73953339b9a
Máté Thurzó <mate.thurzo@liferay.com>
2015-12-08 15:21:39 +0100

    LPS-60901 Update references to constant class

--- modules/apps/ratings/ratings-page-ratings-web/src/main/java/com/liferay/ratings/page/ratings/web/upgrade/PageRatingsWebUpgrade.java
+++ modules/apps/ratings/ratings-page-ratings-web/src/main/java/com/liferay/ratings/page/ratings/web/upgrade/PageRatingsWebUpgrade.java
@@ -18,7 +18,7 @@ import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.portal.upgrade.util.UpgradePortletId;
-import com.liferay.ratings.page.ratings.web.constants.PageRatingsPortletKeys;
+import com.liferay.ratings.page.ratings.constants.PageRatingsPortletKeys;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
