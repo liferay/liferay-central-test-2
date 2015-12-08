aeaafb3c778292c85c835d71a6f599cffb67fc31
Mika Koivisto <mika.koivisto@liferay.com>
2015-12-08 17:16:36 +0200

    LPS-60358 SF

--- portal-service/src/com/liferay/portal/kernel/util/ProtectedClassLoaderObjectInputStream.java
+++ portal-service/src/com/liferay/portal/kernel/util/ProtectedClassLoaderObjectInputStream.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.portal.kernel.util;
 
 import com.liferay.portal.kernel.io.ProtectedObjectInputStream;
@@ -5,6 +19,10 @@ import com.liferay.portal.kernel.io.ProtectedObjectInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectStreamClass;
+
+/**
+ * @author Mika Koivisto
+ */
 public class ProtectedClassLoaderObjectInputStream
 	extends ProtectedObjectInputStream {
 
