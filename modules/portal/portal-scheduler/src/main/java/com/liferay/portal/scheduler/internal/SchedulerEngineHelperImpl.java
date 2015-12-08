3db55d1b02fcec93342bb6c02550f4bc3e4755b1
tina.tian <tina.tian@liferay.com>
2015-12-08 11:11:07 +0800

    LPS-60923 Rename property

--- modules/portal/portal-scheduler/src/main/java/com/liferay/portal/scheduler/internal/SchedulerEngineHelperImpl.java
+++ modules/portal/portal-scheduler/src/main/java/com/liferay/portal/scheduler/internal/SchedulerEngineHelperImpl.java
@@ -981,7 +981,7 @@ public class SchedulerEngineHelperImpl implements SchedulerEngineHelper {
 		_props = props;
 	}
 
-	@Reference(target = "(isProxy=true)", unbind = "-")
+	@Reference(target = "(scheduler.engine.is.proxy=true)", unbind = "-")
 	protected void setSchedulerEngine(SchedulerEngine schedulerEngine) {
 		_schedulerEngine = schedulerEngine;
 	}
