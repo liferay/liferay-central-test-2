510d57b703988c1eee938cab92ae227b4c08dbb5
Máté Thurzó <mate.thurzo@liferay.com>
2015-12-09 12:33:23 +0100

    LPS-60488 This is not necessary anymore

--- modules/apps/message-boards/message-boards-comment/src/main/java/com/liferay/message/boards/comment/MBCommentManagerImpl.java
+++ modules/apps/message-boards/message-boards-comment/src/main/java/com/liferay/message/boards/comment/MBCommentManagerImpl.java
@@ -29,14 +29,12 @@ import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.util.PortalUtil;
-import com.liferay.portlet.exportimport.xstream.XStreamAliasRegistryUtil;
 import com.liferay.portlet.messageboards.model.MBCategoryConstants;
 import com.liferay.portlet.messageboards.model.MBDiscussion;
 import com.liferay.portlet.messageboards.model.MBMessage;
 import com.liferay.portlet.messageboards.model.MBMessageDisplay;
 import com.liferay.portlet.messageboards.model.MBThread;
 import com.liferay.portlet.messageboards.model.MBTreeWalker;
-import com.liferay.portlet.messageboards.model.impl.MBMessageImpl;
 import com.liferay.portlet.messageboards.service.MBDiscussionLocalService;
 import com.liferay.portlet.messageboards.service.MBMessageLocalService;
 import com.liferay.portlet.messageboards.service.MBThreadLocalService;
@@ -50,9 +48,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 /**
@@ -337,16 +333,6 @@ public class MBCommentManagerImpl implements CommentManager {
 			serviceContext);
 
 		return message.getMessageId();
-	}
-
-	@Activate
-	protected void activate() {
-		XStreamAliasRegistryUtil.register(MBMessageImpl.class, "MBMessage");
-	}
-
-	@Deactivate
-	protected void deactivate() {
-		XStreamAliasRegistryUtil.unregister(MBMessageImpl.class, "MBMessage");
 	}
 
 	@Reference(unbind = "-")
