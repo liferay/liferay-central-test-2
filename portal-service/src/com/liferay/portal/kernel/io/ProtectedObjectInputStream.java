8510baa245ebb98afd12e31966594ffbd65a3c22
shuyangzhou <shuyang.zhou@liferay.com>
2015-12-09 14:43:54 -0800

    LPS-60358 Do strict class name equals check for better performance

--- portal-service/src/com/liferay/portal/kernel/io/ProtectedObjectInputStream.java
+++ portal-service/src/com/liferay/portal/kernel/io/ProtectedObjectInputStream.java
@@ -22,10 +22,9 @@ import java.io.InvalidClassException;
 import java.io.ObjectInputStream;
 import java.io.ObjectStreamClass;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Mika Koivisto
@@ -46,29 +45,7 @@ public class ProtectedObjectInputStream extends ObjectInputStream {
 	protected Class<?> resolveClass(ObjectStreamClass osc)
 		throws ClassNotFoundException, IOException {
 
-		for (Pattern restrictedClassPattern : _restrictedClassesPatterns) {
-			Matcher restrictedClassMatcher = restrictedClassPattern.matcher(
-				osc.getName());
-
-			if (restrictedClassMatcher.find()) {
-				throw new InvalidClassException(
-					"Trying to load restricted class: " + osc.getName());
-			}
-		}
-
-		boolean allowedClass = false;
-
-		for (Pattern allowedClassPattern : _allowedClassesPatterns) {
-			Matcher allowedClassMatcher = allowedClassPattern.matcher(
-				osc.getName());
-
-			if (allowedClassMatcher.find()) {
-				allowedClass = true;
-				break;
-			}
-		}
-
-		if (!allowedClass) {
+		if (_restrictedClassNames.contains(osc.getName())) {
 			throw new InvalidClassException(
 				"Trying to load restricted class: " + osc.getName());
 		}
@@ -76,37 +53,16 @@ public class ProtectedObjectInputStream extends ObjectInputStream {
 		return doResolveClass(osc);
 	}
 
-	private static final Pattern[] _allowedClassesPatterns;
-	private static final Pattern[] _restrictedClassesPatterns;
+	private static final Set<String> _restrictedClassNames;
 
 	static {
-		String[] allowedClassesRegex = StringUtil.split(
+		String[] restrictedClassNames = StringUtil.split(
 			System.getProperty(
 				ProtectedObjectInputStream.class.getName() +
-					".allowed.classes.regex"));
+					".restricted.class.names"));
 
-		List<Pattern> patterns = new ArrayList<>(allowedClassesRegex.length);
-
-		for (String allowedClassRegex : allowedClassesRegex) {
-			patterns.add(Pattern.compile(allowedClassRegex));
-		}
-
-		_allowedClassesPatterns = patterns.toArray(
-			new Pattern[allowedClassesRegex.length]);
-
-		String[] restrictedClassesRegex = StringUtil.split(
-			System.getProperty(
-				ProtectedObjectInputStream.class.getName() +
-					".restricted.classes.regex"));
-
-		patterns = new ArrayList<>(restrictedClassesRegex.length);
-
-		for (String restrictedClassRegex : restrictedClassesRegex) {
-			patterns.add(Pattern.compile(restrictedClassRegex));
-		}
-
-		_restrictedClassesPatterns = patterns.toArray(
-			new Pattern[restrictedClassesRegex.length]);
+		_restrictedClassNames = new HashSet<>(
+			Arrays.asList(restrictedClassNames));
 	}
 
 }
