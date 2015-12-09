58dfa815560cbe65b5a059b01d3d8c7b39c033d3
philip.jones <philip.jones@liferay.com>
2015-12-09 09:24:51 -0800

    LPS-61167 Fix PluginRepositoriesMessageListener

--- modules/apps/server-admin/server-admin-web/src/main/java/com/liferay/server/admin/web/messaging/PluginRepositoriesMessageListener.java
+++ modules/apps/server-admin/server-admin-web/src/main/java/com/liferay/server/admin/web/messaging/PluginRepositoriesMessageListener.java
@@ -14,21 +14,53 @@
 
 package com.liferay.server.admin.web.messaging;
 
-import com.liferay.portal.kernel.messaging.BaseMessageListener;
+import com.liferay.portal.kernel.messaging.BaseSchedulerEntryMessageListener;
+import com.liferay.portal.kernel.messaging.Destination;
 import com.liferay.portal.kernel.messaging.Message;
-import com.liferay.portal.kernel.messaging.MessageListener;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
+import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
+import com.liferay.portal.kernel.scheduler.TimeUnit;
+import com.liferay.portal.kernel.scheduler.TriggerFactory;
+import com.liferay.portal.kernel.scheduler.TriggerFactoryUtil;
 import com.liferay.portal.kernel.search.SearchEngineUtil;
 import com.liferay.portal.model.CompanyConstants;
 import com.liferay.portal.plugin.PluginPackageUtil;
 
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Shuyang Zhou
  * @author Philip Jones
  */
-@Component(immediate = true, service = MessageListener.class)
-public class PluginRepositoriesMessageListener extends BaseMessageListener {
+@Component(immediate = true, service = PluginRepositoriesMessageListener.class)
+public class PluginRepositoriesMessageListener
+	extends BaseSchedulerEntryMessageListener {
+
+	public static final String PLUGIN_REPOSITORIES =
+		"liferay/plugin_repositories";
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		schedulerEntryImpl.setTrigger(
+			TriggerFactoryUtil.createTrigger(
+				getEventListenerClass(), getEventListenerClass(), 1,
+				TimeUnit.DAY));
+
+		_schedulerEngineHelper.register(
+			this, schedulerEntryImpl, PLUGIN_REPOSITORIES);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_schedulerEngineHelper.unregister(this);
+	}
 
 	@Override
 	protected void doReceive(Message message) throws Exception {
@@ -36,5 +68,29 @@ public class PluginRepositoriesMessageListener extends BaseMessageListener {
 
 		PluginPackageUtil.reloadRepositories();
 	}
+
+	@Reference(
+		target = "(destination.name=" + PLUGIN_REPOSITORIES + ")", unbind = "-"
+	)
+	protected void setDestination(Destination destination) {
+	}
+
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
+	protected void setModuleServiceLifecycle(
+		ModuleServiceLifecycle moduleServiceLifecycle) {
+	}
+
+	@Reference(unbind = "-")
+	protected void setSchedulerEngineHelper(
+		SchedulerEngineHelper schedulerEngineHelper) {
+
+		_schedulerEngineHelper = schedulerEngineHelper;
+	}
+
+	@Reference(unbind = "-")
+	protected void setTriggerFactory(TriggerFactory triggerFactory) {
+	}
+
+	private volatile SchedulerEngineHelper _schedulerEngineHelper;
 
 }
