d2f5cf1d67bbc5f3ca990056b3acfef0a7ed7f00
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-12-09 13:20:55 +0100

    LPS-61128 Adds "dialog-footer" to ButtonRowTag if we are on a pop-up

--- util-taglib/src/com/liferay/taglib/aui/ButtonRowTag.java
+++ util-taglib/src/com/liferay/taglib/aui/ButtonRowTag.java
@@ -14,8 +14,12 @@
 
 package com.liferay.taglib.aui;
 
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.taglib.aui.base.BaseButtonRowTag;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspWriter;
 
 /**
@@ -37,6 +41,24 @@ public class ButtonRowTag extends BaseButtonRowTag {
 		jspWriter.write("</div>");
 
 		return EVAL_PAGE;
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest request) {
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		if (themeDisplay.isStatePopUp()) {
+			String cssClass = "dialog-footer";
+
+			if (getCssClass() != null) {
+				cssClass = cssClass + StringPool.SPACE + getCssClass();
+			}
+
+			setCssClass(cssClass);
+		}
+
+		super.setAttributes(request);
 	}
 
 	private static final boolean _CLEAN_UP_SET_ATTRIBUTES = true;
