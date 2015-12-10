8c1ccc028069409b008527df21568f6c64b4c34f
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2015-12-10 18:15:07 +0100

    LPS-61188 Add abstract base MVCRenderCommand to render local JSPs directly

---
+++ modules/portal/portal-ldap/src/main/java/com/liferay/portal/ldap/internal/portal/settings/web/portlet/action/PortalSettingsBaseMVCRenderCommand.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.ldap.internal.portal.settings.web.portlet.action;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.util.PortalUtil;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Tomas Polesovsky
+ */
+public abstract class PortalSettingsBaseMVCRenderCommand
+	implements MVCRenderCommand {
+
+	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		RequestDispatcher requestDispatcher =
+			servletContext.getRequestDispatcher(getJSPPath());
+
+		try {
+			HttpServletRequest httpServletRequest =
+				PortalUtil.getHttpServletRequest(renderRequest);
+			HttpServletResponse httpServletResponse =
+				PortalUtil.getHttpServletResponse(renderResponse);
+
+			requestDispatcher.include(httpServletRequest, httpServletResponse);
+		}
+		catch (Exception se) {
+			if (_log.isWarnEnabled()) {
+				_log.warn("Unable to include JSP " + getJSPPath(), se);
+			}
+
+			throw new PortletException(
+				"Unable to include JSP " + getJSPPath(), se);
+		}
+
+		return DONT_DISPATCH_PATH;
+	}
+
+	protected abstract String getJSPPath();
+
+	protected ServletContext servletContext;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		PortalSettingsBaseMVCRenderCommand.class);
+
+}
