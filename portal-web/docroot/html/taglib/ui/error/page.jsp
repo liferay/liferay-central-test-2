65a2a9088977eff835db58586192fdba5e9692fe
Carlos Lancha <carlos.lancha@liferay.com>
2015-12-10 16:16:09 +0100

    LPS-61140 Implement new alert component in error taglib

--- portal-web/docroot/html/taglib/ui/error/page.jsp
+++ portal-web/docroot/html/taglib/ui/error/page.jsp
@@ -21,44 +21,70 @@ String key = (String)request.getAttribute("liferay-ui:error:key");
 String message = (String)request.getAttribute("liferay-ui:error:message");
 boolean translateMessage = GetterUtil.getBoolean((String)request.getAttribute("liferay-ui:error:translateMessage"));
 String rowBreak = (String)request.getAttribute("liferay-ui:error:rowBreak");
+
+String bodyContentString = StringPool.BLANK;
+
+Object bodyContent = request.getAttribute("liferay-ui:error:bodyContent");
+
+if (bodyContent != null) {
+	bodyContentString = bodyContent.toString();
+}
 %>
 
 <c:choose>
 	<c:when test="<%= (key != null) && Validator.isNull(message) %>">
 		<c:if test="<%= SessionErrors.contains(portletRequest, key) %>">
-			</div>
+			<c:if test="<%= Validator.isNotNull(bodyContentString) %>">
+				<liferay-ui:alert
+					message="<%= HtmlUtil.escapeJS(bodyContentString) %>"
+					timeout="0"
+					title='<%= LanguageUtil.get(request, "danger") %>'
+					type="danger"
+				/>
 
-			<%= rowBreak %>
+				<%= rowBreak %>
+			</c:if>
 		</c:if>
 	</c:when>
 	<c:when test='<%= SessionErrors.contains(portletRequest, "warning") %>'>
-		<div class="alert alert-warning">
-			<c:choose>
-				<c:when test="<%= message != null %>">
-					<liferay-ui:message key="<%= message %>" localizeKey="<%= translateMessage %>" />
-				</c:when>
-				<c:otherwise>
-					<liferay-ui:message key='<%= (String)SessionErrors.get(portletRequest, "warning") %>' localizeKey="<%= translateMessage %>" />
-				</c:otherwise>
-			</c:choose>
-		</div>
+		<liferay-util:buffer var="alertMessage">
+			<c:when test="<%= message != null %>">
+				<liferay-ui:message key="<%= message %>" localizeKey="<%= translateMessage %>" />
+			</c:when>
+			<c:otherwise>
+				<liferay-ui:message key='<%= (String)SessionErrors.get(portletRequest, "warning") %>' localizeKey="<%= translateMessage %>" />
+			</c:otherwise>
+		</liferay-util:buffer>
+
+		<liferay-ui:alert
+			message="<%= alertMessage %>"
+			timeout="0"
+			title='<%= LanguageUtil.get(request, "warning") %>'
+			type="warning"
+		/>
 
 		<%= rowBreak %>
 	</c:when>
 	<c:when test="<%= key == null %>">
 		<c:if test="<%= !SessionErrors.isEmpty(portletRequest) %>">
-			<div class="alert alert-danger">
-				<liferay-ui:message key="your-request-failed-to-complete" />
-			</div>
+			<liferay-ui:alert
+				message='<%= LanguageUtil.get(request, "your-request-failed-to-complete") %>'
+				timeout="5000"
+				title='<%= LanguageUtil.get(request, "danger") %>'
+				type="danger"
+			/>
 
 			<%= rowBreak %>
 		</c:if>
 	</c:when>
 	<c:otherwise>
 		<c:if test="<%= SessionErrors.contains(portletRequest, key) %>">
-			<div class="alert alert-danger">
-				<liferay-ui:message key="<%= message %>" localizeKey="<%= translateMessage %>" />
-			</div>
+			<liferay-ui:alert
+				message="<%= translateMessage ? LanguageUtil.get(request, message) : message %>"
+				timeout="0"
+				title='<%= LanguageUtil.get(request, "danger") %>'
+				type="danger"
+			/>
 
 			<%= rowBreak %>
 		</c:if>
--- util-taglib/src/com/liferay/taglib/ui/ErrorTag.java
+++ util-taglib/src/com/liferay/taglib/ui/ErrorTag.java
@@ -19,116 +19,29 @@ import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.taglib.util.PortalIncludeUtil;
+import com.liferay.taglib.util.IncludeTag;
 
 import javax.portlet.PortletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.tagext.TagSupport;
+import javax.servlet.jsp.tagext.BodyTag;
 
 /**
  * @author Brian Wing Shun Chan
  */
-public class ErrorTag extends TagSupport {
-
-	@Override
-	public int doEndTag() throws JspException {
-		try {
-			HttpServletRequest request =
-				(HttpServletRequest)pageContext.getRequest();
-
-			PortletRequest portletRequest =
-				(PortletRequest)request.getAttribute(
-					JavaConstants.JAVAX_PORTLET_REQUEST);
-
-			boolean includeEndPage = false;
-
-			if (Validator.isNull(_key)) {
-				if (!SessionErrors.isEmpty(portletRequest)) {
-					includeEndPage = true;
-				}
-			}
-			else {
-				if (SessionErrors.contains(portletRequest, _key)) {
-					includeEndPage = true;
-				}
-			}
-
-			if (includeEndPage) {
-				PortalIncludeUtil.include(pageContext, getEndPage());
-
-				String errorMarkerKey = (String)request.getAttribute(
-					"liferay-ui:error-marker:key");
-				String errorMarkerValue = (String)request.getAttribute(
-					"liferay-ui:error-marker:value");
-
-				if (Validator.isNotNull(errorMarkerKey) &&
-					Validator.isNotNull(errorMarkerValue)) {
-
-					request.setAttribute(errorMarkerKey, errorMarkerValue);
-
-					Object exception = getException(portletRequest);
-
-					if (exception instanceof Exception) {
-						request.setAttribute(
-							"liferay-ui:error:exception", exception);
-					}
-
-					request.setAttribute(
-						"liferay-ui:error:focusField", _focusField);
-				}
-			}
-
-			return EVAL_PAGE;
-		}
-		catch (Exception e) {
-			throw new JspException(e);
-		}
-	}
+public class ErrorTag extends IncludeTag implements BodyTag {
 
 	@Override
 	public int doStartTag() throws JspException {
-		try {
-			HttpServletRequest request =
-				(HttpServletRequest)pageContext.getRequest();
+		setAttributeNamespace(_ATTRIBUTE_NAMESPACE);
 
-			PortletRequest portletRequest =
-				(PortletRequest)request.getAttribute(
-					JavaConstants.JAVAX_PORTLET_REQUEST);
-
-			request.setAttribute("liferay-ui:error:key", _key);
-			request.setAttribute("liferay-ui:error:message", _message);
-			request.setAttribute("liferay-ui:error:rowBreak", _rowBreak);
-			request.setAttribute(
-				"liferay-ui:error:translateMessage",
-				String.valueOf(_translateMessage));
-
-			if (Validator.isNotNull(_message)) {
-				return SKIP_BODY;
-			}
-
-			if (SessionErrors.contains(portletRequest, _key)) {
-				Object value = getException(portletRequest);
-
-				PortalIncludeUtil.include(pageContext, getStartPage());
-
-				if (value != null) {
-					pageContext.setAttribute("errorException", value);
-				}
-
-				return EVAL_BODY_INCLUDE;
-			}
-
-			return SKIP_BODY;
-		}
-		catch (Exception e) {
-			throw new JspException(e);
-		}
+		return super.doStartTag();
 	}
 
-	public void setEndPage(String endPage) {
-		_endPage = endPage;
+	@Override
+	protected int processStartTag() throws Exception {
+		return EVAL_BODY_BUFFERED;
 	}
 
 	public void setException(Class<?> exception) {
@@ -155,21 +68,20 @@ public class ErrorTag extends TagSupport {
 		_rowBreak = HtmlUtil.unescape(rowBreak);
 	}
 
-	public void setStartPage(String startPage) {
-		_startPage = startPage;
-	}
-
 	public void setTranslateMessage(boolean translateMessage) {
 		_translateMessage = translateMessage;
 	}
 
-	protected String getEndPage() {
-		if (Validator.isNull(_endPage)) {
-			return _END_PAGE;
-		}
-		else {
-			return _endPage;
-		}
+	@Override
+	protected void cleanUp() {
+		super.cleanUp();
+
+		_exception = null;
+		_focusField = null;
+		_key = null;
+		_message = null;
+		_rowBreak = StringPool.BLANK;
+		_translateMessage = true;
 	}
 
 	protected Object getException(PortletRequest portletRequest) {
@@ -185,26 +97,62 @@ public class ErrorTag extends TagSupport {
 		return value;
 	}
 
-	protected String getStartPage() {
-		if (Validator.isNull(_startPage)) {
-			return _START_PAGE;
-		}
-		else {
-			return _startPage;
+	@Override
+	protected String getPage() {
+		return _PAGE;
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest request) {
+		PortletRequest portletRequest = (PortletRequest)request.getAttribute(
+			JavaConstants.JAVAX_PORTLET_REQUEST);
+
+		request.setAttribute("liferay-ui:error:key", _key);
+		request.setAttribute("liferay-ui:error:message", _message);
+		request.setAttribute("liferay-ui:error:rowBreak", _rowBreak);
+		request.setAttribute(
+			"liferay-ui:error:translateMessage",
+			String.valueOf(_translateMessage));
+
+		if (SessionErrors.contains(portletRequest, _key)) {
+			Object value = getException(portletRequest);
+
+			if (value != null) {
+				pageContext.setAttribute("errorException", value);
+			}
+
+			String errorMarkerKey = (String)request.getAttribute(
+				"liferay-ui:error-marker:key");
+			String errorMarkerValue = (String)request.getAttribute(
+				"liferay-ui:error-marker:value");
+
+			if (Validator.isNotNull(errorMarkerKey) &&
+				Validator.isNotNull(errorMarkerValue)) {
+
+				request.setAttribute(errorMarkerKey, errorMarkerValue);
+
+				Object exception = getException(portletRequest);
+
+				if (exception instanceof Exception) {
+					request.setAttribute(
+						"liferay-ui:error:exception", exception);
+				}
+
+				request.setAttribute(
+					"liferay-ui:error:focusField", _focusField);
+			}
 		}
 	}
 
-	private static final String _END_PAGE = "/html/taglib/ui/error/end.jsp";
+	private static final String _ATTRIBUTE_NAMESPACE = "liferay-ui:error:";
 
-	private static final String _START_PAGE = "/html/taglib/ui/error/start.jsp";
+	private static final String _PAGE = "/html/taglib/ui/error/page.jsp";
 
-	private String _endPage;
 	private Class<?> _exception;
 	private String _focusField;
 	private String _key;
 	private String _message;
 	private String _rowBreak = StringPool.BLANK;
-	private String _startPage;
 	private boolean _translateMessage = true;
 
 }
