9efb3be744be68e33528e534e1149db7ff386117
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-12-10 21:00:23 -0300

    LPS-60963 Configuration icon shouldn't be displayed by the portlet placed on the shared layout, no matter whether the form is published or not

--- modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/display/context/DDLFormDisplayContext.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/display/context/DDLFormDisplayContext.java
@@ -95,7 +95,7 @@ public class DDLFormDisplayContext {
 			return _showConfigurationIcon;
 		}
 
-		if (isSharedURL() && isPublished() && isFormShared()) {
+		if (isSharedURL() && isFormShared()) {
 			_showConfigurationIcon = false;
 
 			return _showConfigurationIcon;
