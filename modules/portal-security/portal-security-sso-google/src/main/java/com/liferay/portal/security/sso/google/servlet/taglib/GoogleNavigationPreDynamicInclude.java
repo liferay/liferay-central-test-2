3686473fea69f26077febc94ed225ad9022aff0c
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-12-11 12:50:05 +0100

    LPS-61850 JSPs in the login portlet

---
+++ modules/portal-security/portal-security-sso-google/src/main/java/com/liferay/portal/security/sso/google/servlet/taglib/GoogleNavigationPreDynamicInclude.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.sso.google.servlet.taglib;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.taglib.BaseDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+import com.liferay.portal.kernel.util.PrefsPropsUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.theme.ThemeDisplay;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Sergio González
+ */
+@Component(immediate = true, service = DynamicInclude.class)
+public class GoogleNavigationPreDynamicInclude extends BaseDynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest request, HttpServletResponse response,
+			String key)
+		throws IOException {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		boolean googleAuthEnabled = PrefsPropsUtil.getBoolean(
+			themeDisplay.getCompanyId(), "google-auth-enabled", true);
+
+		if (!googleAuthEnabled) {
+			return;
+		}
+
+		String googleClientId = PrefsPropsUtil.getString(
+			themeDisplay.getCompanyId(), "google-client-id");
+
+		String googleClientSecret = PrefsPropsUtil.getString(
+			themeDisplay.getCompanyId(), "google-client-secret");
+
+		if (Validator.isNull(googleClientId) ||
+			Validator.isNull(googleClientSecret)) {
+
+			return;
+		}
+
+		RequestDispatcher requestDispatcher =
+			_servletContext.getRequestDispatcher(_JSP_PATH);
+
+		try {
+			requestDispatcher.include(request, response);
+		}
+		catch (ServletException se) {
+			_log.error("Unable to include JSP " + _JSP_PATH, se);
+
+			throw new IOException("Unable to include JSP " + _JSP_PATH, se);
+		}
+	}
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register(
+			"com.liferay.login.web#/navigation.jsp#pre");
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.portal.security.sso.google)",
+		unbind = "-"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
+	}
+
+	private static final String _JSP_PATH =
+		"/html/portlet/login/navigation/google.jsp";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		GoogleNavigationPreDynamicInclude.class);
+
+	private volatile ServletContext _servletContext;
+
+}
