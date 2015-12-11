12bf821ffc9644eb643415a4bd13b2bd0ba72b58
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-12-11 12:01:33 +0100

    LPS-61850 Run the action (annotation)

--- modules/portal-security/portal-security-sso-google/src/main/java/com/liferay/portal/security/sso/google/events/AddGoogleExpandoColumnsAction.java
+++ modules/portal-security/portal-security-sso-google/src/main/java/com/liferay/portal/security/sso/google/events/AddGoogleExpandoColumnsAction.java
@@ -16,9 +16,13 @@ package com.liferay.portal.security.sso.google.events;
 
 import com.liferay.portal.kernel.events.ActionException;
 import com.liferay.portal.kernel.events.SimpleAction;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.model.Company;
 import com.liferay.portal.model.User;
+import com.liferay.portal.security.auth.CompanyThreadLocal;
+import com.liferay.portal.service.CompanyLocalService;
 import com.liferay.portlet.expando.model.ExpandoColumn;
 import com.liferay.portlet.expando.model.ExpandoColumnConstants;
 import com.liferay.portlet.expando.model.ExpandoTable;
@@ -26,9 +30,16 @@ import com.liferay.portlet.expando.model.ExpandoTableConstants;
 import com.liferay.portlet.expando.service.ExpandoColumnLocalServiceUtil;
 import com.liferay.portlet.expando.service.ExpandoTableLocalServiceUtil;
 
+import java.util.List;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 /**
  * @author Sergio González
  */
+@Component(immediate = true)
 public class AddGoogleExpandoColumnsAction extends SimpleAction {
 
 	@Override
@@ -38,6 +49,24 @@ public class AddGoogleExpandoColumnsAction extends SimpleAction {
 		}
 		catch (Exception e) {
 			throw new ActionException(e);
+		}
+	}
+
+	@Activate
+	protected void activate() throws ActionException {
+		Long companyId = CompanyThreadLocal.getCompanyId();
+
+		try {
+			List<Company> companies = _companyLocalService.getCompanies();
+
+			for (Company company : companies) {
+				CompanyThreadLocal.setCompanyId(company.getCompanyId());
+
+				run(new String[] {String.valueOf(company.getCompanyId())});
+			}
+		}
+		finally {
+			CompanyThreadLocal.setCompanyId(companyId);
 		}
 	}
 
@@ -83,5 +112,19 @@ public class AddGoogleExpandoColumnsAction extends SimpleAction {
 		addExpandoColumn(expandoTable, "googleRefreshToken", properties);
 		addExpandoColumn(expandoTable, "googleUserId", properties);
 	}
+
+	@Reference(unbind = "-")
+	protected void setCompanyLocalService(
+		CompanyLocalService companyLocalService) {
+
+		_companyLocalService = companyLocalService;
+	}
+
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
+	protected void setModuleServiceLifecycle(
+		ModuleServiceLifecycle moduleServiceLifecycle) {
+	}
+
+	private volatile CompanyLocalService _companyLocalService;
 
 }
