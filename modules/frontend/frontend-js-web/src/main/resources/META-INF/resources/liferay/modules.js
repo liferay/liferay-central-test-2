54c2792980d59bcd73fbf88ab57b55b81dd3cf27
Carlos Lancha <carlos.lancha@liferay.com>
2015-12-11 15:19:29 +0100

    LPS-61238 Create sidebar panel js component

--- modules/frontend/frontend-js-web/src/main/resources/META-INF/resources/liferay/modules.js
+++ modules/frontend/frontend-js-web/src/main/resources/META-INF/resources/liferay/modules.js
@@ -725,6 +725,14 @@
 							'liferay-alert'
 						]
 					},
+					'liferay-sidebar-panel': {
+						path: 'sidebar_panel.js',
+						requires: [
+							'aui-base',
+							'aui-io-request',
+							'liferay-portlet-base'
+						]
+					},
 					'liferay-sign-in-modal': {
 						path: 'sign_in_modal.js',
 						requires: [
