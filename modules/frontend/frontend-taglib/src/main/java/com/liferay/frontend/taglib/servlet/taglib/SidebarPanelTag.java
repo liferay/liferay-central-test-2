8c726717239623926597e3dfd5bf9248dccec480
Carlos Lancha <carlos.lancha@liferay.com>
2015-12-11 15:20:02 +0100

    LPS-61238 Create sidebar panel taglib

---
+++ modules/frontend/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/SidebarPanelTag.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.servlet.taglib;
+
+import com.liferay.frontend.taglib.servlet.ServletContextUtil;
+import com.liferay.taglib.util.IncludeTag;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * @author Carlos Lancha
+ */
+public class SidebarPanelTag extends IncludeTag {
+
+	@Override
+	public int doEndTag() throws JspException {
+		setAttributeNamespace(_ATTRIBUTE_NAMESPACE);
+
+		setNamespacedAttribute(
+			request, "searchContainerId", _searchContainerId);
+		setNamespacedAttribute(request, "resourceUrl", _resourceUrl);
+
+		super.doEndTag();
+
+		return EVAL_PAGE;
+	}
+
+	@Override
+	public int doStartTag() throws JspException {
+		setAttributeNamespace(_ATTRIBUTE_NAMESPACE);
+
+		super.doStartTag();
+
+		return EVAL_BODY_INCLUDE;
+	}
+
+	@Override
+	public void setPageContext(PageContext pageContext) {
+		super.setPageContext(pageContext);
+
+		servletContext = ServletContextUtil.getServletContext();
+	}
+
+	public void setResourceUrl(String resourceUrl) {
+		_resourceUrl = resourceUrl;
+
+		setScopedAttribute("resourceUrl", _resourceUrl);
+	}
+
+	public void setSearchContainerId(String searchContainerId) {
+		_searchContainerId = searchContainerId;
+
+		setScopedAttribute("searchContainerId", _searchContainerId);
+	}
+
+	@Override
+	protected void cleanUp() {
+		super.cleanUp();
+
+		_resourceUrl = null;
+		_searchContainerId = null;
+	}
+
+	@Override
+	protected String getEndPage() {
+		return _END_PAGE;
+	}
+
+	@Override
+	protected String getStartPage() {
+		return _START_PAGE;
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest request) {
+	}
+
+	private static final String _ATTRIBUTE_NAMESPACE =
+		"liferay-frontend:sidebar-panel:";
+
+	private static final String _END_PAGE = "/sidebar_panel/end.jsp";
+
+	private static final String _START_PAGE = "/sidebar_panel/start.jsp";
+
+	private String _resourceUrl = null;
+	private String _searchContainerId = null;
+
+}
