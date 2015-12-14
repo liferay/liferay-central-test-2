rotty3000/liferay-portal

LPS-59894 Fill in generated services

portal-impl/src/com/liferay/portal/service/impl/RecentLayoutBranchLocalServiceImpl.java
@@ -16,28 +16,55 @@ package com.liferay.portal.service.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.portal.model.LayoutBranch;
+import com.liferay.portal.model.RecentLayoutBranch;
+import com.liferay.portal.model.User;
 import com.liferay.portal.service.base.RecentLayoutBranchLocalServiceBaseImpl;
 
 /**
- * The implementation of the recent layout branch local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.portal.service.RecentLayoutBranchLocalService} interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
  * @author Brian Wing Shun Chan
- * @see RecentLayoutBranchLocalServiceBaseImpl
- * @see com.liferay.portal.service.RecentLayoutBranchLocalServiceUtil
+ * @author Preston Crary
  */
 @ProviderType
 public class RecentLayoutBranchLocalServiceImpl
 	extends RecentLayoutBranchLocalServiceBaseImpl {
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Always use {@link com.liferay.portal.service.RecentLayoutBranchLocalServiceUtil} to access the recent layout branch local service.
-	 */
+
+	@Override
+	public RecentLayoutBranch addRecentLayoutBranch(
+		long companyId, long groupId, long userId, long layoutSetBranchId,
+		long plid) {
+
+		long recentLayoutBranchId = counterLocalService.increment();
+
+		RecentLayoutBranch RecentLayoutBranch =
+			recentLayoutBranchPersistence.create(recentLayoutBranchId);
+
+		RecentLayoutBranch.setCompanyId(companyId);
+		RecentLayoutBranch.setGroupId(groupId);
+		RecentLayoutBranch.setUserId(userId);
+		RecentLayoutBranch.setLayoutSetBranchId(layoutSetBranchId);
+		RecentLayoutBranch.setPlid(plid);
+
+		return recentLayoutBranchPersistence.update(RecentLayoutBranch);
+	}
+
+	@Override
+	public void deleteRecentLayoutBranches(LayoutBranch layoutBranch) {
+		recentLayoutBranchPersistence.removeByLayoutBranchId(
+			layoutBranch.getLayoutBranchId());
+	}
+
+	@Override
+	public void deleteRecentLayoutBranches(User user) {
+		recentLayoutBranchPersistence.removeByUserId(user.getUserId());
+	}
+
+	@Override
+	public RecentLayoutBranch fetchRecentLayoutBranch(
+		long userId, long layoutSetBranchId, long plid) {
+
+		return recentLayoutBranchPersistence.fetchByU_L_P(
+			userId, layoutSetBranchId, plid);
+	}
+
 }

portal-impl/src/com/liferay/portal/service/impl/RecentLayoutRevisionLocalServiceImpl.java
@@ -16,28 +16,55 @@ package com.liferay.portal.service.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.portal.model.LayoutRevision;
+import com.liferay.portal.model.RecentLayoutRevision;
+import com.liferay.portal.model.User;
 import com.liferay.portal.service.base.RecentLayoutRevisionLocalServiceBaseImpl;
 
 /**
- * The implementation of the recent layout revision local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.portal.service.RecentLayoutRevisionLocalService} interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
  * @author Brian Wing Shun Chan
- * @see RecentLayoutRevisionLocalServiceBaseImpl
- * @see com.liferay.portal.service.RecentLayoutRevisionLocalServiceUtil
+ * @author Preston Crary
  */
 @ProviderType
 public class RecentLayoutRevisionLocalServiceImpl
 	extends RecentLayoutRevisionLocalServiceBaseImpl {
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Always use {@link com.liferay.portal.service.RecentLayoutRevisionLocalServiceUtil} to access the recent layout revision local service.
-	 */
+
+	@Override
+	public RecentLayoutRevision addRecentLayoutRevision(
+		long companyId, long groupId, long userId, long layoutSetBranchId,
+		long plid) {
+
+		long recentLayoutRevisionId = counterLocalService.increment();
+
+		RecentLayoutRevision recentLayoutRevision =
+			recentLayoutRevisionPersistence.create(recentLayoutRevisionId);
+
+		recentLayoutRevision.setCompanyId(companyId);
+		recentLayoutRevision.setGroupId(groupId);
+		recentLayoutRevision.setUserId(userId);
+		recentLayoutRevision.setLayoutSetBranchId(layoutSetBranchId);
+		recentLayoutRevision.setPlid(plid);
+
+		return recentLayoutRevisionPersistence.update(recentLayoutRevision);
+	}
+
+	@Override
+	public void deleteRecentLayoutRevisions(LayoutRevision layoutRevision) {
+		recentLayoutRevisionPersistence.removeByLayoutRevisionId(
+			layoutRevision.getLayoutRevisionId());
+	}
+
+	@Override
+	public void deleteRecentLayoutRevisions(User user) {
+		recentLayoutRevisionPersistence.removeByUserId(user.getUserId());
+	}
+
+	@Override
+	public RecentLayoutRevision fetchRecentLayoutRevision(
+		long userId, long layoutSetBranchId, long plid) {
+
+		return recentLayoutRevisionPersistence.fetchByU_L_P(
+			userId, layoutSetBranchId, plid);
+	}
+
 }

portal-impl/src/com/liferay/portal/service/impl/RecentLayoutSetBranchLocalServiceImpl.java
@@ -16,28 +16,54 @@ package com.liferay.portal.service.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.portal.model.LayoutSetBranch;
+import com.liferay.portal.model.RecentLayoutSetBranch;
+import com.liferay.portal.model.User;
 import com.liferay.portal.service.base.RecentLayoutSetBranchLocalServiceBaseImpl;
 
 /**
- * The implementation of the recent layout set branch local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.portal.service.RecentLayoutSetBranchLocalService} interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
  * @author Brian Wing Shun Chan
- * @see RecentLayoutSetBranchLocalServiceBaseImpl
- * @see com.liferay.portal.service.RecentLayoutSetBranchLocalServiceUtil
+ * @author Preston Crary
  */
 @ProviderType
 public class RecentLayoutSetBranchLocalServiceImpl
 	extends RecentLayoutSetBranchLocalServiceBaseImpl {
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Always use {@link com.liferay.portal.service.RecentLayoutSetBranchLocalServiceUtil} to access the recent layout set branch local service.
-	 */
+
+	@Override
+	public RecentLayoutSetBranch addRecentLayoutSetBranch(
+		long companyId, long groupId, long userId, long layoutSetId,
+		long layoutSetBranchId) {
+
+		long recentLayoutSetBranchId = counterLocalService.increment();
+
+		RecentLayoutSetBranch recentLayoutSetBranch =
+			recentLayoutSetBranchPersistence.create(recentLayoutSetBranchId);
+
+		recentLayoutSetBranch.setCompanyId(companyId);
+		recentLayoutSetBranch.setGroupId(groupId);
+		recentLayoutSetBranch.setUserId(userId);
+		recentLayoutSetBranch.setLayoutSetId(layoutSetId);
+		recentLayoutSetBranch.setLayoutSetBranchId(layoutSetBranchId);
+
+		return recentLayoutSetBranchPersistence.update(recentLayoutSetBranch);
+	}
+
+	@Override
+	public void deleteRecentLayoutSetBranches(LayoutSetBranch layoutSetBranch) {
+		recentLayoutSetBranchPersistence.removeByLayoutSetBranchId(
+			layoutSetBranch.getLayoutSetBranchId());
+	}
+
+	@Override
+	public void deleteRecentLayoutSetBranches(User user) {
+		recentLayoutSetBranchPersistence.removeByUserId(user.getUserId());
+	}
+
+	@Override
+	public RecentLayoutSetBranch fetchRecentLayoutSetBranch(
+		long userId, long layoutSetId) {
+
+		return recentLayoutSetBranchPersistence.fetchByU_L(userId, layoutSetId);
+	}
+
 }

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portal/service/impl/RecentLayoutBranchLocalServiceImpl.java
@@ -16,12 +16,56 @@ package com.liferay.portal.service.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.portal.model.LayoutBranch;
+import com.liferay.portal.model.RecentLayoutBranch;
+import com.liferay.portal.model.User;
 import com.liferay.portal.service.base.RecentLayoutBranchLocalServiceBaseImpl;
 
 /**
  * @author Brian Wing Shun Chan
+ * @author Preston Crary
  */
 @ProviderType
 public class RecentLayoutBranchLocalServiceImpl
 	extends RecentLayoutBranchLocalServiceBaseImpl {
+
+	@Override
+	public RecentLayoutBranch addRecentLayoutBranch(
+		long companyId, long groupId, long userId, long layoutBranchId,
+		long layoutSetBranchId, long plid) {
+
+		long recentLayoutBranchId = counterLocalService.increment();
+
+		RecentLayoutBranch recentLayoutBranch =
+			recentLayoutBranchPersistence.create(recentLayoutBranchId);
+
+		recentLayoutBranch.setGroupId(groupId);
+		recentLayoutBranch.setCompanyId(companyId);
+		recentLayoutBranch.setUserId(userId);
+		recentLayoutBranch.setLayoutBranchId(layoutBranchId);
+		recentLayoutBranch.setLayoutSetBranchId(layoutSetBranchId);
+		recentLayoutBranch.setPlid(plid);
+
+		return recentLayoutBranchPersistence.update(recentLayoutBranch);
+	}
+
+	@Override
+	public void deleteRecentLayoutBranches(LayoutBranch layoutBranch) {
+		recentLayoutBranchPersistence.removeByLayoutBranchId(
+			layoutBranch.getLayoutBranchId());
+	}
+
+	@Override
+	public void deleteRecentLayoutBranches(User user) {
+		recentLayoutBranchPersistence.removeByUserId(user.getUserId());
+	}
+
+	@Override
+	public RecentLayoutBranch fetchRecentLayoutBranch(
+		long userId, long layoutSetBranchId, long plid) {
+
+		return recentLayoutBranchPersistence.fetchByU_L_P(
+			userId, layoutSetBranchId, plid);
+	}
+
 }

portal-impl/src/com/liferay/portal/service/impl/RecentLayoutRevisionLocalServiceImpl.java
@@ -16,12 +16,56 @@ package com.liferay.portal.service.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.portal.model.LayoutRevision;
+import com.liferay.portal.model.RecentLayoutRevision;
+import com.liferay.portal.model.User;
 import com.liferay.portal.service.base.RecentLayoutRevisionLocalServiceBaseImpl;
 
 /**
  * @author Brian Wing Shun Chan
+ * @author Preston Crary
  */
 @ProviderType
 public class RecentLayoutRevisionLocalServiceImpl
 	extends RecentLayoutRevisionLocalServiceBaseImpl {
+
+	@Override
+	public RecentLayoutRevision addRecentLayoutRevision(
+		long companyId, long groupId, long userId, long layoutRevisionId,
+		long layoutSetBranchId, long plid) {
+
+		long recentLayoutRevisionId = counterLocalService.increment();
+
+		RecentLayoutRevision recentLayoutRevision =
+			recentLayoutRevisionPersistence.create(recentLayoutRevisionId);
+
+		recentLayoutRevision.setGroupId(groupId);
+		recentLayoutRevision.setCompanyId(companyId);
+		recentLayoutRevision.setUserId(userId);
+		recentLayoutRevision.setLayoutRevisionId(layoutRevisionId);
+		recentLayoutRevision.setLayoutSetBranchId(layoutSetBranchId);
+		recentLayoutRevision.setPlid(plid);
+
+		return recentLayoutRevisionPersistence.update(recentLayoutRevision);
+	}
+
+	@Override
+	public void deleteRecentLayoutRevisions(LayoutRevision layoutRevision) {
+		recentLayoutRevisionPersistence.removeByLayoutRevisionId(
+			layoutRevision.getLayoutRevisionId());
+	}
+
+	@Override
+	public void deleteRecentLayoutRevisions(User user) {
+		recentLayoutRevisionPersistence.removeByUserId(user.getUserId());
+	}
+
+	@Override
+	public RecentLayoutRevision fetchRecentLayoutRevision(
+		long userId, long layoutSetBranchId, long plid) {
+
+		return recentLayoutRevisionPersistence.fetchByU_L_P(
+			userId, layoutSetBranchId, plid);
+	}
+
 }

portal-impl/src/com/liferay/portal/service/impl/RecentLayoutSetBranchLocalServiceImpl.java
@@ -16,12 +16,54 @@ package com.liferay.portal.service.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.portal.model.LayoutSetBranch;
+import com.liferay.portal.model.RecentLayoutSetBranch;
+import com.liferay.portal.model.User;
 import com.liferay.portal.service.base.RecentLayoutSetBranchLocalServiceBaseImpl;
 
 /**
  * @author Brian Wing Shun Chan
+ * @author Preston Crary
  */
 @ProviderType
 public class RecentLayoutSetBranchLocalServiceImpl
 	extends RecentLayoutSetBranchLocalServiceBaseImpl {
+
+	@Override
+	public RecentLayoutSetBranch addRecentLayoutSetBranch(
+		long companyId, long groupId, long userId, long layoutSetBranchId,
+		long layoutSetId) {
+
+		long recentLayoutSetBranchId = counterLocalService.increment();
+
+		RecentLayoutSetBranch recentLayoutSetBranch =
+			recentLayoutSetBranchPersistence.create(recentLayoutSetBranchId);
+
+		recentLayoutSetBranch.setGroupId(groupId);
+		recentLayoutSetBranch.setCompanyId(companyId);
+		recentLayoutSetBranch.setUserId(userId);
+		recentLayoutSetBranch.setLayoutSetBranchId(layoutSetBranchId);
+		recentLayoutSetBranch.setLayoutSetId(layoutSetId);
+
+		return recentLayoutSetBranchPersistence.update(recentLayoutSetBranch);
+	}
+
+	@Override
+	public void deleteRecentLayoutSetBranches(LayoutSetBranch layoutSetBranch) {
+		recentLayoutSetBranchPersistence.removeByLayoutSetBranchId(
+			layoutSetBranch.getLayoutSetBranchId());
+	}
+
+	@Override
+	public void deleteRecentLayoutSetBranches(User user) {
+		recentLayoutSetBranchPersistence.removeByUserId(user.getUserId());
+	}
+
+	@Override
+	public RecentLayoutSetBranch fetchRecentLayoutSetBranch(
+		long userId, long layoutSetId) {
+
+		return recentLayoutSetBranchPersistence.fetchByU_L(userId, layoutSetId);
+	}
+
 }
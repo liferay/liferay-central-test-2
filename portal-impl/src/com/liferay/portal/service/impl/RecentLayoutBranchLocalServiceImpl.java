888233e70d0879fd4612f3f1c71609ae472ae2f5
Preston-Crary <preston.crary@liferay.com>
2015-11-11 11:29:11 -0800

    LPS-59894 Fill in generated services

--- portal-impl/src/com/liferay/portal/service/impl/RecentLayoutBranchLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/RecentLayoutBranchLocalServiceImpl.java
@@ -16,12 +16,56 @@ package com.liferay.portal.service.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.portal.model.LayoutBranch;
+import com.liferay.portal.model.RecentLayoutBranch;
+import com.liferay.portal.model.User;
 import com.liferay.portal.service.base.RecentLayoutBranchLocalServiceBaseImpl;
 
 /**
  * @author Brian Wing Shun Chan
+ * @author Preston Crary
  */
 @ProviderType
 public class RecentLayoutBranchLocalServiceImpl
 	extends RecentLayoutBranchLocalServiceBaseImpl {
+
+	@Override
+	public RecentLayoutBranch addRecentLayoutBranch(
+		long companyId, long groupId, long userId, long layoutBranchId,
+		long layoutSetBranchId, long plid) {
+
+		long recentLayoutBranchId = counterLocalService.increment();
+
+		RecentLayoutBranch recentLayoutBranch =
+			recentLayoutBranchPersistence.create(recentLayoutBranchId);
+
+		recentLayoutBranch.setGroupId(groupId);
+		recentLayoutBranch.setCompanyId(companyId);
+		recentLayoutBranch.setUserId(userId);
+		recentLayoutBranch.setLayoutBranchId(layoutBranchId);
+		recentLayoutBranch.setLayoutSetBranchId(layoutSetBranchId);
+		recentLayoutBranch.setPlid(plid);
+
+		return recentLayoutBranchPersistence.update(recentLayoutBranch);
+	}
+
+	@Override
+	public void deleteRecentLayoutBranches(LayoutBranch layoutBranch) {
+		recentLayoutBranchPersistence.removeByLayoutBranchId(
+			layoutBranch.getLayoutBranchId());
+	}
+
+	@Override
+	public void deleteRecentLayoutBranches(User user) {
+		recentLayoutBranchPersistence.removeByUserId(user.getUserId());
+	}
+
+	@Override
+	public RecentLayoutBranch fetchRecentLayoutBranch(
+		long userId, long layoutSetBranchId, long plid) {
+
+		return recentLayoutBranchPersistence.fetchByU_L_P(
+			userId, layoutSetBranchId, plid);
+	}
+
 }
