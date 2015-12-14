ac444a3b6d528a835e2853ef2eeecdc5745d6eb7
Roberto Díaz <roberto.diaz@liferay.com>
2015-12-14 12:02:33 +0100

    LPS-61229  Fix order by comparator logic in finder

--- modules/apps/wiki/wiki-service/src/main/java/com/liferay/wiki/service/persistence/impl/WikiPageFinderImpl.java
+++ modules/apps/wiki/wiki-service/src/main/java/com/liferay/wiki/service/persistence/impl/WikiPageFinderImpl.java
@@ -417,7 +417,7 @@ public class WikiPageFinderImpl
 					groupId);
 			}
 
-			CustomSQLUtil.replaceOrderBy(
+			sql = CustomSQLUtil.replaceOrderBy(
 				sql, queryDefinition.getOrderByComparator("WikiPage"));
 
 			SQLQuery q = session.createSynchronizedSQLQuery(sql);
