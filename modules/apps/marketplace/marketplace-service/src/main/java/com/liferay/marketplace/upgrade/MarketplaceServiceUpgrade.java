80996d50496ceb2e92b2d955070aa0fdeff11852
Cristina González Castellano <cristina.gonzalez@liferay.com>
2015-12-14 11:56:29 +0100

    LPS-61295 Marketplace: Correct way to deal with plugins converted from the legacy SDK to the new architecture (related to upgrades)

--- modules/apps/marketplace/marketplace-service/src/main/java/com/liferay/marketplace/upgrade/MarketplaceServiceUpgrade.java
+++ modules/apps/marketplace/marketplace-service/src/main/java/com/liferay/marketplace/upgrade/MarketplaceServiceUpgrade.java
@@ -36,12 +36,18 @@ public class MarketplaceServiceUpgrade implements UpgradeStepRegistrator {
 	@Override
 	public void register(Registry registry) {
 		registry.register(
-			"com.liferay.marketplace.service", "0.0.1", "1.0.0",
-			new UpgradeCompanyId(),
+			"com.liferay.portal.workflow.kaleo.service", "0.0.1", "1.0.0",
 			new UpgradeExpando(
 				_expandoColumnLocalService, _expandoTableLocalService,
-				_expandoValueLocalService),
+				_expandoValueLocalService));
+
+		registry.register(
+			"com.liferay.marketplace.service", "1.0.0", "1.0.1",
 			new UpgradeModule());
+
+		registry.register(
+			"com.liferay.marketplace.service", "1.0.1", "1.0.2",
+			new UpgradeCompanyId());
 	}
 
 	@Reference(unbind = "-")
