fe003c2c258fada2725ff3b64c6889bbbe160110
Bruno Basto <bruno.basto@liferay.com>
2015-12-16 15:43:46 -0300

    LPS-60571 Regen

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-options/src/main/resources/META-INF/resources/options.soy.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-options/src/main/resources/META-INF/resources/options.soy.js
@@ -5,5 +5,5 @@ if (typeof ddm == 'undefined') { var ddm = {}; }
 
 
 ddm.options = function(opt_data, opt_ignored) {
-return '\t<div class="form-group' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + ' liferay-ddm-form-field-options" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '">' + ((opt_data.showLabel) ? '<label class="control-label">' + soy.$$escapeHtml(opt_data.label) + ((opt_data.required) ? '<b>*</b>' : '') + '</label>' : '') + '<input name="' + soy.$$escapeHtml(opt_data.name) + '" type="hidden" /><div class="options"></div></div>';
+return '\t<div class="form-group' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + ' liferay-ddm-form-field-options" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '">' + ((opt_data.showLabel) ? '<label class="control-label">' + soy.$$escapeHtml(opt_data.label) + ((opt_data.required) ? '<span class="icon-asterisk text-warning"></span>' : '') + '</label>' : '') + '<input name="' + soy.$$escapeHtml(opt_data.name) + '" type="hidden" /><div class="options"></div></div>';
 };
