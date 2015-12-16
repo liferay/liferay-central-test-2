e0410eeeba262d5e8756acf173b10f3062383cc7
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-12-16 14:44:07 +0100

    LPS-61402 Tests for get methods in local service

--- portal-impl/test/integration/com/liferay/portlet/messageboards/service/MBCategoryLocalServiceTest.java
+++ portal-impl/test/integration/com/liferay/portlet/messageboards/service/MBCategoryLocalServiceTest.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.ObjectValuePair;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.model.Group;
@@ -29,8 +30,14 @@ import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portlet.messageboards.model.MBCategory;
 import com.liferay.portlet.messageboards.model.MBCategoryConstants;
+import com.liferay.portlet.messageboards.model.MBMessage;
+import com.liferay.portlet.messageboards.model.MBMessageConstants;
+import com.liferay.portlet.messageboards.model.MBThreadConstants;
+
+import java.io.InputStream;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Assert;
@@ -42,6 +49,7 @@ import org.junit.Test;
 /**
  * @author Igor Beslic
  * @author Roberto Díaz
+ * @author Sergio González
  */
 public class MBCategoryLocalServiceTest {
 
@@ -53,6 +61,127 @@ public class MBCategoryLocalServiceTest {
 	@Before
 	public void setUp() throws Exception {
 		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test
+	public void testGetCategoriesAndThreadsInRootCategory() throws Exception {
+		MBMessage message1 = addMessage(
+			MBCategoryConstants.DEFAULT_PARENT_CATEGORY_ID);
+
+		MBCategory category1 = addCategory();
+
+		addMessage(category1.getCategoryId());
+
+		MBCategory category2 = addCategory();
+
+		addMessage(category2.getCategoryId());
+
+		MBMessage message2 = addMessage(
+			MBCategoryConstants.DEFAULT_PARENT_CATEGORY_ID);
+
+		List<Object> categoriesAndThreads =
+			MBCategoryLocalServiceUtil.getCategoriesAndThreads(
+				_group.getGroupId(),
+				MBCategoryConstants.DEFAULT_PARENT_CATEGORY_ID,
+				WorkflowConstants.STATUS_APPROVED);
+
+		Assert.assertEquals(4, categoriesAndThreads.size());
+		Assert.assertEquals(category1, categoriesAndThreads.get(0));
+		Assert.assertEquals(category2, categoriesAndThreads.get(1));
+		Assert.assertEquals(message1.getThread(), categoriesAndThreads.get(2));
+		Assert.assertEquals(message2.getThread(), categoriesAndThreads.get(3));
+	}
+
+	@Test
+	public void testGetCategoriesAndThreadsInRootCategoryWithOnlyCategories()
+		throws Exception {
+
+		MBCategory category1 = addCategory();
+
+		addCategory(category1.getCategoryId());
+
+		MBCategory category2 = addCategory();
+
+		List<Object> categoriesAndThreads =
+			MBCategoryLocalServiceUtil.getCategoriesAndThreads(
+				_group.getGroupId(),
+				MBCategoryConstants.DEFAULT_PARENT_CATEGORY_ID,
+				WorkflowConstants.STATUS_APPROVED);
+
+		Assert.assertEquals(2, categoriesAndThreads.size());
+		Assert.assertEquals(category1, categoriesAndThreads.get(0));
+		Assert.assertEquals(category2, categoriesAndThreads.get(1));
+	}
+
+	@Test
+	public void testGetCategoriesAndThreadsInRootCategoryWithOnlyThreads()
+		throws Exception {
+
+		MBMessage message1 = addMessage(
+			MBCategoryConstants.DEFAULT_PARENT_CATEGORY_ID);
+
+		MBMessage message2 = addMessage(
+			MBCategoryConstants.DEFAULT_PARENT_CATEGORY_ID);
+
+		List<Object> categoriesAndThreads =
+			MBCategoryLocalServiceUtil.getCategoriesAndThreads(
+				_group.getGroupId(),
+				MBCategoryConstants.DEFAULT_PARENT_CATEGORY_ID,
+				WorkflowConstants.STATUS_APPROVED);
+
+		Assert.assertEquals(2, categoriesAndThreads.size());
+		Assert.assertEquals(message1.getThread(), categoriesAndThreads.get(0));
+		Assert.assertEquals(message2.getThread(), categoriesAndThreads.get(1));
+	}
+
+	@Test
+	public void testGetCategoriesAndThreadsWithOnlyCategories()
+		throws Exception {
+
+		MBCategory category1 = addCategory();
+
+		MBCategory subcategory1 = addCategory(category1.getCategoryId());
+
+		addCategory();
+
+		List<Object> categoriesAndThreads =
+			MBCategoryLocalServiceUtil.getCategoriesAndThreads(
+				_group.getGroupId(), category1.getCategoryId(),
+				WorkflowConstants.STATUS_APPROVED);
+
+		Assert.assertEquals(1, categoriesAndThreads.size());
+		Assert.assertEquals(subcategory1, categoriesAndThreads.get(0));
+	}
+
+	@Test
+	public void testGetCategoriesAndThreadsWithPriorityThread()
+		throws Exception {
+
+		MBMessage message1 = addMessage(
+			MBCategoryConstants.DEFAULT_PARENT_CATEGORY_ID);
+
+		MBCategory category1 = addCategory();
+
+		addMessage(category1.getCategoryId());
+
+		MBMessage priorityMessage = addMessage(
+			MBCategoryConstants.DEFAULT_PARENT_CATEGORY_ID, 2.0);
+
+		MBMessage message2 = addMessage(
+			MBCategoryConstants.DEFAULT_PARENT_CATEGORY_ID);
+
+		List<Object> categoriesAndThreads =
+			MBCategoryLocalServiceUtil.getCategoriesAndThreads(
+				_group.getGroupId(),
+				MBCategoryConstants.DEFAULT_PARENT_CATEGORY_ID,
+				WorkflowConstants.STATUS_APPROVED);
+
+		Assert.assertEquals(4, categoriesAndThreads.size());
+		Assert.assertEquals(category1, categoriesAndThreads.get(0));
+		Assert.assertEquals(
+			priorityMessage.getThread(), categoriesAndThreads.get(1));
+		Assert.assertEquals(message1.getThread(), categoriesAndThreads.get(2));
+		Assert.assertEquals(message2.getThread(), categoriesAndThreads.get(3));
 	}
 
 	@Test
@@ -331,6 +460,27 @@ public class MBCategoryLocalServiceTest {
 		return MBCategoryServiceUtil.addCategory(
 			TestPropsValues.getUserId(), parentCategoryId,
 			RandomTestUtil.randomString(), StringPool.BLANK, serviceContext);
+	}
+
+	protected MBMessage addMessage(long categoryId) throws Exception {
+		return addMessage(categoryId, MBThreadConstants.PRIORITY_NOT_GIVEN);
+	}
+
+	protected MBMessage addMessage(long categoryId, double priority)
+		throws Exception {
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		List<ObjectValuePair<String, InputStream>> inputStreamOVPs =
+			Collections.emptyList();
+
+		return MBMessageLocalServiceUtil.addMessage(
+			TestPropsValues.getUserId(), RandomTestUtil.randomString(),
+			_group.getGroupId(), categoryId, RandomTestUtil.randomString(),
+			RandomTestUtil.randomString(), MBMessageConstants.DEFAULT_FORMAT,
+			inputStreamOVPs, false, priority, false, serviceContext);
 	}
 
 	@DeleteAfterTestRun
