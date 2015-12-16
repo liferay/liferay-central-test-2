6e4b95b70d5c9d5b831e11d5ed4fa9d72aab3b4f
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2015-12-16 14:15:11 +0100

    LPS-61340 Create AntiSamy configuration so that portal administrator can disable the module

---
+++ modules/portal-security/portal-security-antisamy/src/main/java/com/liferay/portal/security/antisamy/configuration/AntiSamyConfiguration.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.antisamy.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.configuration.admin.ConfigurationAdmin;
+
+/**
+ * @author Tomas Polesovsky
+ */
+@ConfigurationAdmin(category = "platform")
+@Meta.OCD(
+	id = "com.liferay.portal.security.antisamy.configuration.AntiSamyConfiguration"
+)
+public interface AntiSamyConfiguration {
+
+	@Meta.AD(deflt = "true", required = false)
+	public boolean enabled();
+
+}
