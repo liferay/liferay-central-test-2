0435bd155de70669a4ec3764e14cc2e12a5b8ed7
Bruno Basto <bruno.basto@liferay.com>
2015-12-18 16:14:11 -0300

    LPS-61471 Adding servlet to render options when requested from the front-end

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-data-provider/src/main/java/com/liferay/dynamic/data/mapping/data/provider/internal/servlet/DDMDataProviderServlet.java
@@ -0,0 +1,168 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.data.provider.internal.servlet;
+
+import com.liferay.dynamic.data.mapping.io.DDMFormJSONDeserializer;
+import com.liferay.dynamic.data.mapping.model.DDMDataProviderInstance;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMFormField;
+import com.liferay.dynamic.data.mapping.service.DDMDataProviderInstanceService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.servlet.ServletResponseUtil;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.KeyValuePair;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.servlet.filters.authverifier.AuthVerifierFilter;
+
+import java.io.IOException;
+
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
+
+/**
+ * @author Bruno Basto
+ */
+@Component(immediate = true)
+public class DDMDataProviderServlet extends HttpServlet {
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		Hashtable<String, String> properties = new Hashtable<>();
+
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_PATH,
+			"/dynamic-data-mapping-data-provider");
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_FILTER_NAME,
+			"AuthVerifierFilter");
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_FILTER_PATTERN,
+			"/dynamic-data-mapping-data-provider/*");
+
+		bundleContext.registerService(
+			Filter.class, new AuthVerifierFilter(), properties);
+
+		properties = new Hashtable<>();
+
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_PATH,
+			"/dynamic-data-mapping-data-provider");
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_NAME,
+			"DDMDataProviderServlet");
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_PATTERN,
+			"/dynamic-data-mapping-data-provider/*");
+		properties.put("servlet.init.httpMethods", "GET,POST,HEAD");
+
+		bundleContext.registerService(Servlet.class, this, properties);
+	}
+
+	@Override
+	protected void doGet(
+			HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException {
+
+		String fieldName = ParamUtil.getString(request, "fieldName");
+		String serializedDDMForm = ParamUtil.getString(
+			request, "serializedDDMForm");
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		try {
+			DDMForm ddmForm = _ddmFormJSONDeserializer.deserialize(
+				serializedDDMForm);
+
+			Map<String, DDMFormField> ddmFormFieldsMap =
+				ddmForm.getDDMFormFieldsMap(true);
+
+			DDMFormField ddmFormField = ddmFormFieldsMap.get(fieldName);
+
+			long ddmDataProviderInstanceId = GetterUtil.getLong(
+				ddmFormField.getProperty("ddmDataProviderInstanceId"));
+
+			DDMDataProviderInstance ddmDataProviderInstance =
+				_ddmDataProviderInstanceService.getDataProviderInstance(
+					ddmDataProviderInstanceId);
+
+			List<KeyValuePair> data = ddmDataProviderInstance.getData();
+
+			for (KeyValuePair keyValuePair : data) {
+				JSONObject optionJSONObject =
+					JSONFactoryUtil.createJSONObject();
+
+				JSONObject labelJSONObject = JSONFactoryUtil.createJSONObject();
+
+				labelJSONObject.put(
+					LanguageUtil.getLanguageId(LocaleUtil.getDefault()),
+					keyValuePair.getKey());
+
+				optionJSONObject.put("label", labelJSONObject);
+				optionJSONObject.put("value", keyValuePair.getValue());
+
+				jsonArray.put(optionJSONObject);
+			}
+		}
+		catch (PortalException pe) {
+			pe.printStackTrace();
+		}
+
+		response.setContentType(ContentTypes.APPLICATION_JSON);
+		response.setStatus(HttpServletResponse.SC_OK);
+
+		ServletResponseUtil.write(response, jsonArray.toJSONString());
+	}
+
+	@Reference(unbind = "-")
+	protected void setDDMDataProviderInstanceService(
+		DDMDataProviderInstanceService ddmDataProviderInstanceService) {
+
+		_ddmDataProviderInstanceService = ddmDataProviderInstanceService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setDDMFormJSONDeserializer(
+		DDMFormJSONDeserializer ddmFormJSONDeserializer) {
+
+		_ddmFormJSONDeserializer = ddmFormJSONDeserializer;
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	private volatile DDMDataProviderInstanceService
+		_ddmDataProviderInstanceService;
+	private volatile DDMFormJSONDeserializer _ddmFormJSONDeserializer;
+
+}
