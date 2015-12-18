jonmak08/liferay-portal

LPS-60328 - update taglib namespace

modules/apps/site-navigation/site-navigation-taglib/src/main/java/com/liferay/site/navigation/taglib/servlet/taglib/NavigationTag.java
@@ -143,35 +143,42 @@ public class NavigationTag extends IncludeTag {
 
 	@Override
 	protected void setAttributes(HttpServletRequest request) {
-		request.setAttribute("liferay-ui:navigation:bulletStyle", _bulletStyle);
 		request.setAttribute(
-			"liferay-ui:navigation:displayStyle", getDisplayStyle());
+			"liferay-site-navigation:navigation:bulletStyle", _bulletStyle);
 		request.setAttribute(
-			"liferay-ui:navigation:displayStyleGroupId",
+			"liferay-site-navigation:navigation:displayStyle",
+			getDisplayStyle());
+		request.setAttribute(
+			"liferay-site-navigation:navigation:displayStyleGroupId",
 			String.valueOf(getDisplayStyleGroupId()));
-		request.setAttribute("liferay-ui:navigation:headerType", _headerType);
 		request.setAttribute(
-			"liferay-ui:navigation:includedLayouts", _includedLayouts);
+			"liferay-site-navigation:navigation:headerType", _headerType);
+		request.setAttribute(
+			"liferay-site-navigation:navigation:includedLayouts",
+			_includedLayouts);
 
 		try {
 			List<NavItem> navItems = getNavItems(request);
 
-			request.setAttribute("liferay-ui:navigation:navItems", navItems);
+			request.setAttribute(
+				"liferay-site-navigation:navigation:navItems", navItems);
 		}
 		catch (PortalException pe) {
 			_log.error(pe);
 		}
 
 		request.setAttribute(
-			"liferay-ui:navigation:nestedChildren",
+			"liferay-site-navigation:navigation:nestedChildren",
 			String.valueOf(_nestedChildren));
 		request.setAttribute(
-			"liferay-ui:navigation:preview", String.valueOf(_preview));
+			"liferay-site-navigation:navigation:preview",
+			String.valueOf(_preview));
 		request.setAttribute(
-			"liferay-ui:navigation:rootLayoutLevel",
+			"liferay-site-navigation:navigation:rootLayoutLevel",
 			String.valueOf(_rootLayoutLevel));
 		request.setAttribute(
-			"liferay-ui:navigation:rootLayoutType", _rootLayoutType);
+			"liferay-site-navigation:navigation:rootLayoutType",
+			_rootLayoutType);
 	}
 
 	private static final String _PAGE = "/navigation/page.jsp";

liferay/liferay-central-test-2

modules/apps/site-navigation/site-navigation-taglib/src/main/java/com/liferay/site/navigation/taglib/servlet/taglib/NavigationTag.java
@@ -167,32 +167,38 @@ public class NavigationTag extends IncludeTag {
 	@Override
 	protected void setAttributes(HttpServletRequest request) {
 		request.setAttribute(
-			"liferay-ui:navigation:displayStyle", getDisplayStyle());
+			"liferay-site-navigation:navigation:displayStyle",
+			getDisplayStyle());
 		request.setAttribute(
-			"liferay-ui:navigation:displayStyleGroupId",
+			"liferay-site-navigation:navigation:displayStyleGroupId",
 			String.valueOf(getDisplayStyleGroupId()));
 		request.setAttribute(
-			"liferay-ui:navigation:includedLayouts", _includedLayouts);
+			"liferay-site-navigation:navigation:includedLayouts",
+			_includedLayouts);
 
 		try {
 			List<NavItem> branchNavItems = getBranchNavItems(request);
 
 			request.setAttribute(
-				"liferay-ui:navigation:branchNavItems", branchNavItems);
+				"liferay-site-navigation:navigation:branchNavItems",
+				branchNavItems);
 			request.setAttribute(
-				"liferay-ui:navigation:navItems", getNavItems(branchNavItems));
+				"liferay-site-navigation:navigation:navItems",
+				getNavItems(branchNavItems));
 		}
 		catch (Exception e) {
 			_log.error(e, e);
 		}
 
 		request.setAttribute(
-			"liferay-ui:navigation:preview", String.valueOf(_preview));
+			"liferay-site-navigation:navigation:preview",
+			String.valueOf(_preview));
 		request.setAttribute(
-			"liferay-ui:navigation:rootLayoutLevel",
+			"liferay-site-navigation:navigation:rootLayoutLevel",
 			String.valueOf(_rootLayoutLevel));
 		request.setAttribute(
-			"liferay-ui:navigation:rootLayoutType", _rootLayoutType);
+			"liferay-site-navigation:navigation:rootLayoutType",
+			_rootLayoutType);
 	}
 
 	private static final String _PAGE = "/navigation/page.jsp";