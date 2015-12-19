202d1457f50b96a23a36929e2047e9e866b2922b
Chema Balsas <jose.balsas@liferay.com>
2015-12-19 20:43:59 +0100

    LPS-60718 Move taglib js component to taglib module

--- modules/frontend/frontend-taglib/src/main/resources/META-INF/resources/config.js
+++ modules/frontend/frontend-taglib/src/main/resources/META-INF/resources/config.js
@@ -7,6 +7,15 @@
 				'frontend-taglib': {
 					base: PATH_FRONTEND_TAGLIB + '/',
 					modules: {
+						'liferay-diff-version-comparator': {
+							path: 'diff_version_comparator/js/diff_version_comparator.js',
+							requires: [
+								'aui-io-request',
+								'autocomplete-base',
+								'autocomplete-filters',
+								'liferay-portlet-base'
+							]
+						},
 						'liferay-sidebar-panel': {
 							path: 'sidebar_panel/js/sidebar_panel.js',
 							requires: [
