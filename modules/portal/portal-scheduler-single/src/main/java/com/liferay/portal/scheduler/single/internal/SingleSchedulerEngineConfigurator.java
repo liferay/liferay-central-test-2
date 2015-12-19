cf62e1e390373a0f83c722f266114ba4e8ea012e
tina.tian <tina.tian@liferay.com>
2015-12-19 17:46:56 +0800

    LPS-61453 Move out single scheduler engine configurator out as a bundle

--- modules/portal/portal-scheduler-single/src/main/java/com/liferay/portal/scheduler/single/internal/SingleSchedulerEngineConfigurator.java
+++ modules/portal/portal-scheduler-single/src/main/java/com/liferay/portal/scheduler/single/internal/SingleSchedulerEngineConfigurator.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.scheduler.internal;
+package com.liferay.portal.scheduler.single.internal;
 
 import com.liferay.portal.kernel.messaging.sender.SingleDestinationMessageSenderFactory;
 import com.liferay.portal.kernel.scheduler.SchedulerEngine;
