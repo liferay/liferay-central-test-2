marcellustavares/liferay-portal

LPS-61197 Get the factory for an asset renderer

modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/asset/DDLFormAssetRenderer.java
@@ -29,6 +29,7 @@ import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.asset.model.AssetRendererFactory;
 import com.liferay.portlet.asset.model.BaseJSPAssetRenderer;
 import com.liferay.portlet.asset.model.DDMFormValuesReader;
 
@@ -69,6 +70,11 @@ public class DDLFormAssetRenderer extends BaseJSPAssetRenderer<DDLRecord> {
 	@Override
 	public DDLRecord getAssetObject() {
 		return _record;
+	}
+
+	@Override
+	public AssetRendererFactory<DDLRecord> getAssetRendererFactory() {
+		return new DDLFormAssetRendererFactory();
 	}
 
 	@Override

liferay/liferay-central-test-2

modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/asset/DDLFormAssetRenderer.java
@@ -30,6 +30,7 @@ import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.asset.model.AssetRendererFactory;
 import com.liferay.portlet.asset.model.BaseJSPAssetRenderer;
 
 import java.util.Locale;
@@ -69,6 +70,11 @@ public class DDLFormAssetRenderer extends BaseJSPAssetRenderer<DDLFormRecord> {
 	@Override
 	public DDLFormRecord getAssetObject() {
 		return _formRecord;
+	}
+
+	@Override
+	public AssetRendererFactory<DDLFormRecord> getAssetRendererFactory() {
+		return new DDLFormAssetRendererFactory();
 	}
 
 	@Override