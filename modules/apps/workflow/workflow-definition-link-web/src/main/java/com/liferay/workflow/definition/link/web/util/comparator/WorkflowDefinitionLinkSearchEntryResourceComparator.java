21f3708d37d353379c3e22923b3e5637ce9244f9
Leonardo Barros <leonardo.barros@liferay.com>
2015-12-21 14:46:22 -0300

    LPS-61363 Add sort functionality

---
+++ modules/apps/workflow/workflow-definition-link-web/src/main/java/com/liferay/workflow/definition/link/web/util/comparator/WorkflowDefinitionLinkSearchEntryResourceComparator.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.workflow.definition.link.web.util.comparator;
+
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.workflow.definition.link.web.search.WorkflowDefinitionLinkSearchEntry;
+
+/**
+ * @author Leonardo Barros
+ */
+public class WorkflowDefinitionLinkSearchEntryResourceComparator
+	implements java.util.Comparator<WorkflowDefinitionLinkSearchEntry> {
+
+	public WorkflowDefinitionLinkSearchEntryResourceComparator(
+		boolean ascending) {
+
+		_ascending = ascending;
+	}
+
+	@Override
+	public int compare(
+		WorkflowDefinitionLinkSearchEntry wdlse1,
+		WorkflowDefinitionLinkSearchEntry wdlse2) {
+
+		String resource1 = StringUtil.toLowerCase(wdlse1.getResource());
+		String resource2 = StringUtil.toLowerCase(wdlse2.getResource());
+
+		int value = resource1.compareTo(resource2);
+
+		if (_ascending) {
+			return value;
+		}
+		else {
+			return -value;
+		}
+	}
+
+	private final boolean _ascending;
+
+}
