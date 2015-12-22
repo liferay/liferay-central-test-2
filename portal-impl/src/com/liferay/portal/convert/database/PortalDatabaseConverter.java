2cf92e08ca5228fe5718d2728763718c47e83789
Cristina González Castellano <cristina.gonzalez@liferay.com>
2015-12-22 12:40:53 +0100

    LPS-61082 New util class for ModelMigrator

--- portal-impl/src/com/liferay/portal/convert/database/PortalDatabaseConverter.java
+++ portal-impl/src/com/liferay/portal/convert/database/PortalDatabaseConverter.java
@@ -16,12 +16,12 @@ package com.liferay.portal.convert.database;
 
 import com.liferay.portal.convert.DatabaseConverter;
 import com.liferay.portal.convert.util.ModelMigrator;
+import com.liferay.portal.convert.util.ModelMigratorUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.servlet.ServletContextPool;
 import com.liferay.portal.kernel.util.ClassLoaderUtil;
 import com.liferay.portal.model.BaseModel;
-import com.liferay.portal.model.ModelHintsUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,68 +44,27 @@ public class PortalDatabaseConverter implements DatabaseConverter {
 		_modelMigrator = modelMigrator;
 	}
 
-	protected Class<? extends BaseModel<?>> getImplClass(String implClassName) {
-		try {
-			ClassLoader classLoader = ClassLoaderUtil.getPortalClassLoader();
-
-			return (Class<? extends BaseModel<?>>)classLoader.loadClass(
-				implClassName);
-		}
-		catch (Exception e) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(e, e);
-			}
-		}
-
-		// Backward compatibility so legacy plugins are converted
-
-		for (String servletContextName : ServletContextPool.keySet()) {
-			try {
-				ServletContext servletContext = ServletContextPool.get(
-					servletContextName);
-
-				ClassLoader classLoader = servletContext.getClassLoader();
-
-				return (Class<? extends BaseModel<?>>)classLoader.loadClass(
-					implClassName);
-			}
-			catch (Exception e) {
-				if (_log.isDebugEnabled()) {
-					_log.debug(e, e);
-				}
-			}
-		}
-
-		return null;
-	}
-
 	protected List<Class<? extends BaseModel<?>>> getModelClassesName(
 		String regex) {
 
-		List<String> modelNames = ModelHintsUtil.getModels();
-
-		List<Class<? extends BaseModel<?>>> implClassesNames =
+		List<Class<? extends BaseModel<?>>> modelClassesName =
 			new ArrayList<>();
 
-		for (String modelName : modelNames) {
-			if (!modelName.contains(".model.")) {
-				continue;
-			}
+		modelClassesName.addAll(
+			ModelMigratorUtil.getModelClassesName(
+				ClassLoaderUtil.getPortalClassLoader(), ".*"));
 
-			String implClassName = modelName.replaceFirst(
-				"(\\.model\\.)(\\p{Upper}.*)", "$1impl.$2Impl");
+		for (String servletContextName : ServletContextPool.keySet()) {
+			ServletContext servletContext = ServletContextPool.get(
+				servletContextName);
 
-			if (implClassName.matches(regex)) {
-				Class<? extends BaseModel<?>> implClass = getImplClass(
-					implClassName);
+			ClassLoader classLoader = servletContext.getClassLoader();
 
-				if (implClass != null) {
-					implClassesNames.add(implClass);
-				}
-			}
+			modelClassesName.addAll(
+				ModelMigratorUtil.getModelClassesName(classLoader, ".*"));
 		}
 
-		return implClassesNames;
+		return modelClassesName;
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
