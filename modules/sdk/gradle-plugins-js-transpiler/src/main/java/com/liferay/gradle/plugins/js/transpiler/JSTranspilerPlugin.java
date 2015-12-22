cddecb080c3720b9261413e89f97a4faa4011704
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2015-12-21 17:01:54 -0800

    LPS-61527 Configure the task when the "java" plugin is applied

--- modules/sdk/gradle-plugins-js-transpiler/src/main/java/com/liferay/gradle/plugins/js/transpiler/JSTranspilerPlugin.java
+++ modules/sdk/gradle-plugins-js-transpiler/src/main/java/com/liferay/gradle/plugins/js/transpiler/JSTranspilerPlugin.java
@@ -21,6 +21,8 @@ import com.liferay.gradle.util.GradleUtil;
 import java.io.File;
 
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.Callable;
 
 import org.gradle.api.Action;
@@ -28,8 +30,10 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 import org.gradle.api.file.FileCollection;
+import org.gradle.api.file.SourceDirectorySet;
 import org.gradle.api.plugins.BasePlugin;
 import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.api.plugins.PluginContainer;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.SourceSetOutput;
 import org.gradle.api.tasks.TaskContainer;
@@ -113,32 +117,25 @@ public class JSTranspilerPlugin implements Plugin<Project> {
 		return downloadNodeModuleTask;
 	}
 
-	protected TranspileJSTask addTaskTranspileJS(final Project project) {
-		TranspileJSTask transpileJSTask = GradleUtil.addTask(
+	protected TranspileJSTask addTaskTranspileJS(Project project) {
+		final TranspileJSTask transpileJSTask = GradleUtil.addTask(
 			project, TRANSPILE_JS_TASK_NAME, TranspileJSTask.class);
 
 		transpileJSTask.setDescription("Transpiles JS files.");
 		transpileJSTask.setGroup(BasePlugin.BUILD_GROUP);
 
-		transpileJSTask.setOutputDir(
-			new Callable<File>() {
+		PluginContainer pluginContainer = project.getPlugins();
+
+		pluginContainer.withType(
+			JavaPlugin.class,
+			new Action<JavaPlugin>() {
 
 				@Override
-				public File call() throws Exception {
-					SourceSet sourceSet = GradleUtil.getSourceSet(
-						project, SourceSet.MAIN_SOURCE_SET_NAME);
-
-					SourceSetOutput sourceSetOutput = sourceSet.getOutput();
-
-					return sourceSetOutput.getResourcesDir();
+				public void execute(JavaPlugin javaPlugin) {
+					configureTaskTranspileJSForJavaPlugin(transpileJSTask);
 				}
 
 			});
-
-		Task classesTask = GradleUtil.getTask(
-			project, JavaPlugin.CLASSES_TASK_NAME);
-
-		classesTask.dependsOn(transpileJSTask);
 
 		return transpileJSTask;
 	}
@@ -167,6 +164,52 @@ public class JSTranspilerPlugin implements Plugin<Project> {
 			transpileJSTask.setDependsOn(Collections.emptySet());
 			transpileJSTask.setEnabled(false);
 		}
+	}
+
+	protected void configureTaskTranspileJSForJavaPlugin(
+		TranspileJSTask transpileJSTask) {
+
+		transpileJSTask.mustRunAfter(JavaPlugin.PROCESS_RESOURCES_TASK_NAME);
+
+		Project project = transpileJSTask.getProject();
+
+		final SourceSet sourceSet = GradleUtil.getSourceSet(
+			project, SourceSet.MAIN_SOURCE_SET_NAME);
+
+		transpileJSTask.setOutputDir(
+			new Callable<File>() {
+
+				@Override
+				public File call() throws Exception {
+					SourceSetOutput sourceSetOutput = sourceSet.getOutput();
+
+					return sourceSetOutput.getResourcesDir();
+				}
+
+			});
+
+		transpileJSTask.setSourceDir(
+			new Callable<File>() {
+
+				@Override
+				public File call() throws Exception {
+					return getSrcDir(sourceSet.getResources());
+				}
+
+			});
+
+		Task classesTask = GradleUtil.getTask(
+			project, JavaPlugin.CLASSES_TASK_NAME);
+
+		classesTask.dependsOn(transpileJSTask);
+	}
+
+	protected File getSrcDir(SourceDirectorySet sourceDirectorySet) {
+		Set<File> srcDirs = sourceDirectorySet.getSrcDirs();
+
+		Iterator<File> iterator = srcDirs.iterator();
+
+		return iterator.next();
 	}
 
 }
