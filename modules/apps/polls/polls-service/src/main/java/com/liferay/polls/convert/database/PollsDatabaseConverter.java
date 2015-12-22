8ff8ed9f6609f616b84cf9498485aa84a5121190
Cristina González Castellano <cristina.gonzalez@liferay.com>
2015-12-22 12:40:53 +0100

    LPS-61082 New util class for ModelMigrator

--- modules/apps/polls/polls-service/src/main/java/com/liferay/polls/convert/database/PollsDatabaseConverter.java
+++ modules/apps/polls/polls-service/src/main/java/com/liferay/polls/convert/database/PollsDatabaseConverter.java
@@ -14,17 +14,11 @@
 
 package com.liferay.polls.convert.database;
 
-import com.liferay.polls.constants.PollsPortletKeys;
 import com.liferay.portal.convert.DatabaseConverter;
 import com.liferay.portal.convert.util.ModelMigrator;
+import com.liferay.portal.convert.util.ModelMigratorUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.ClassLoaderUtil;
-import com.liferay.portal.model.BaseModel;
-import com.liferay.portal.model.ModelHintsUtil;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.sql.DataSource;
 
@@ -39,52 +33,10 @@ public class PollsDatabaseConverter implements DatabaseConverter {
 
 	@Override
 	public void convert(DataSource dataSource) throws Exception {
-		_modelMigrator.migrate(dataSource, getModelClassesName(".*Polls.*"));
-	}
-
-	protected Class<? extends BaseModel<?>> getImplClass(String implClassName) {
-		try {
-			ClassLoader classLoader = getClass().getClassLoader();
-
-			return (Class<? extends BaseModel<?>>)classLoader.loadClass(
-				implClassName);
-		}
-		catch (Exception e) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(e, e);
-			}
-		}
-
-		return null;
-	}
-
-	protected List<Class<? extends BaseModel<?>>> getModelClassesName(
-		String regex) {
-
-		List<String> modelNames = ModelHintsUtil.getModels();
-
-		List<Class<? extends BaseModel<?>>> implClassesNames =
-			new ArrayList<>();
-
-		for (String modelName : modelNames) {
-			if (!modelName.contains(".model.")) {
-				continue;
-			}
-
-			String implClassName = modelName.replaceFirst(
-				"(\\.model\\.)(\\p{Upper}.*)", "$1impl.$2Impl");
-
-			if (implClassName.matches(regex)) {
-				Class<? extends BaseModel<?>> implClass = getImplClass(
-					implClassName);
-
-				if (implClass != null) {
-					implClassesNames.add(implClass);
-				}
-			}
-		}
-
-		return implClassesNames;
+		_modelMigrator.migrate(
+			dataSource,
+			ModelMigratorUtil.getModelClassesName(
+				getClass().getClassLoader(), ".*Polls.*"));
 	}
 
 	@Reference(unbind = "-")
