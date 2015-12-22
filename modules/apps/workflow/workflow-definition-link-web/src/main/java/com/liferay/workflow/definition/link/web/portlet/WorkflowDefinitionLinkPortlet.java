07238842ade9a8fa33cc17ecf1e4fda3b3076f5e
Leonardo Barros <leonardo.barros@liferay.com>
2015-12-22 11:28:36 -0300

    LPS-61363 Move

--- modules/apps/workflow/workflow-definition-link-web/src/main/java/com/liferay/workflow/definition/link/web/portlet/WorkflowDefinitionLinkPortlet.java
+++ modules/apps/workflow/workflow-definition-link-web/src/main/java/com/liferay/workflow/definition/link/web/portlet/WorkflowDefinitionLinkPortlet.java
@@ -18,8 +18,8 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.service.WorkflowDefinitionLinkLocalService;
+import com.liferay.workflow.definition.link.web.display.context.WorkflowDefinitionLinkDisplayContext;
 import com.liferay.workflow.definition.link.web.portlet.constants.WorkflowDefinitionLinkPortletKeys;
-import com.liferay.workflow.definition.link.web.search.display.context.WorkflowDefinitionLinkDisplayContext;
 
 import java.io.IOException;
 
