c94f68f53ea15819102c20f51ede9b7458423473
Cristina González Castellano <cristina.gonzalez@liferay.com>
2015-12-23 18:01:01 +0100

    LPS-61590 Use ' instead of " in the select

--- portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeModules.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeModules.java
@@ -179,9 +179,9 @@ public class UpgradeModules extends UpgradeProcess {
 		throws IOException, SQLException {
 
 		runSQL(
-			"update Release_ set servletContextName = \"" +
-				newServletContextName + "\" where servletContextName = \"" +
-					oldServletContextName + "\"");
+			"update Release_ set servletContextName = '" +
+				newServletContextName + "' where servletContextName = '" +
+					oldServletContextName + "'");
 	}
 
 	private static final String[] _bundleSymbolicNames = new String[] {
