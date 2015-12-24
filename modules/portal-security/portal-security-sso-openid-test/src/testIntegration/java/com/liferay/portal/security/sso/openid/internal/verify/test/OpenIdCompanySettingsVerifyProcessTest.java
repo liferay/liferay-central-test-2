4447492cec5ad26826c040d91eca1e0e0857214b
Michael Han <michael.han@liferay.com>
2015-12-23 23:42:26 -0600

    LPS-61440 update to use BaseCompanySettingsVerifyPropertiesTestCase

--- modules/portal-security/portal-security-sso-openid-test/src/testIntegration/java/com/liferay/portal/security/sso/openid/internal/verify/test/OpenIdCompanySettingsVerifyProcessTest.java
+++ modules/portal-security/portal-security-sso-openid-test/src/testIntegration/java/com/liferay/portal/security/sso/openid/internal/verify/test/OpenIdCompanySettingsVerifyProcessTest.java
@@ -15,179 +15,63 @@
 package com.liferay.portal.security.sso.openid.internal.verify.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.settings.CompanyServiceSettingsLocator;
-import com.liferay.portal.kernel.settings.ModifiableSettings;
 import com.liferay.portal.kernel.settings.Settings;
-import com.liferay.portal.kernel.settings.SettingsException;
-import com.liferay.portal.kernel.settings.SettingsFactory;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.util.ArrayUtil;
-import com.liferay.portal.kernel.util.PrefsProps;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.model.Company;
 import com.liferay.portal.security.sso.openid.constants.LegacyOpenIdPropsKeys;
 import com.liferay.portal.security.sso.openid.constants.OpenIdConstants;
-import com.liferay.portal.service.CompanyLocalService;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portal.verify.VerifyException;
-import com.liferay.portal.verify.VerifyProcess;
-import com.liferay.portal.verify.test.BaseVerifyProcessTestCase;
-
-import java.io.IOException;
-
-import java.util.List;
+import com.liferay.portal.verify.test.BaseCompanySettingsVerifyProcessTestCase;
 
 import javax.portlet.PortletPreferences;
 
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
-
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
-import org.osgi.framework.InvalidSyntaxException;
-import org.osgi.framework.ServiceReference;
 
 /**
  * @author Stian Sigvartsen
  */
 @RunWith(Arquillian.class)
 public class OpenIdCompanySettingsVerifyProcessTest
-	extends BaseVerifyProcessTestCase {
+	extends BaseCompanySettingsVerifyProcessTestCase {
 
 	@ClassRule
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
 		new LiferayIntegrationTestRule();
 
-	@BeforeClass
-	public static void setUpClass() throws PortalException {
-		Bundle bundle = FrameworkUtil.getBundle(
-			OpenIdCompanySettingsVerifyProcessTest.class);
+	@Override
+	protected void doVerify(
+		PortletPreferences portletPreferences, Settings settings) {
 
-		_bundleContext = bundle.getBundleContext();
+		Assert.assertTrue(
+			Validator.isNull(
+				portletPreferences.getValue(
+					LegacyOpenIdPropsKeys.OPENID_AUTH_ENABLED,
+					StringPool.BLANK)));
 
-		ServiceReference<CompanyLocalService> companyLocalServiceReference =
-			_bundleContext.getServiceReference(CompanyLocalService.class);
-
-		_companyLocalService = _bundleContext.getService(
-			companyLocalServiceReference);
-
-		ServiceReference<PrefsProps> prefsPropsServiceReference =
-			_bundleContext.getServiceReference(PrefsProps.class);
-
-		_prefsProps = _bundleContext.getService(prefsPropsServiceReference);
-
-		ServiceReference<SettingsFactory> configurationAdminServiceReference =
-			_bundleContext.getServiceReference(SettingsFactory.class);
-
-		_settingsFactory = _bundleContext.getService(
-			configurationAdminServiceReference);
-
-		UnicodeProperties properties = new UnicodeProperties();
-
-		properties.put(LegacyOpenIdPropsKeys.OPENID_AUTH_ENABLED, "true");
-
-		List<Company> companies = _companyLocalService.getCompanies(false);
-
-		for (Company company : companies) {
-			_companyLocalService.updatePreferences(
-				company.getCompanyId(), properties);
-		}
-	}
-
-	@AfterClass
-	public static void tearDownClass()
-		throws InvalidSyntaxException, IOException {
-
-		List<Company> companies = _companyLocalService.getCompanies(false);
-
-		for (Company company : companies) {
-			Settings settings = getSettings(company.getCompanyId());
-
-			ModifiableSettings modifiableSettings =
-				settings.getModifiableSettings();
-
-			modifiableSettings.reset();
-		}
-
-		_bundleContext = null;
-	}
-
-	protected static Settings getSettings(long companyId) {
-		try {
-			Settings settings = _settingsFactory.getSettings(
-				new CompanyServiceSettingsLocator(
-					companyId, OpenIdConstants.SERVICE_NAME));
-
-			return settings;
-		}
-		catch (SettingsException e) {
-			throw new IllegalStateException(e);
-		}
-	}
-
-	protected void doVerify() throws VerifyException {
-		super.doVerify();
-
-		List<Company> companies = _companyLocalService.getCompanies(false);
-
-		for (Company company : companies) {
-			PortletPreferences portletPreferences = _prefsProps.getPreferences(
-				company.getCompanyId(), true);
-
-			Assert.assertTrue(
-				Validator.isNull(
-					portletPreferences.getValue(
-						LegacyOpenIdPropsKeys.OPENID_AUTH_ENABLED,
-						StringPool.BLANK)));
-
-			Settings settings = getSettings(company.getCompanyId());
-
-			Assert.assertEquals(
-				StringPool.TRUE,
-				settings.getValue(
-					OpenIdConstants.AUTH_ENABLED, StringPool.FALSE));
-		}
+		Assert.assertEquals(
+			StringPool.TRUE,
+			settings.getValue(OpenIdConstants.AUTH_ENABLED, StringPool.FALSE));
 	}
 
 	@Override
-	protected VerifyProcess getVerifyProcess() {
-		try {
-			ServiceReference<?>[] serviceReferences =
-				_bundleContext.getAllServiceReferences(
-					VerifyProcess.class.getName(),
-					"(&(objectClass=" + VerifyProcess.class.getName() +
-						")(verify.process.name=" +
-							"com.liferay.portal.security.sso.openid))");
-
-			if (ArrayUtil.isEmpty(serviceReferences)) {
-				throw new IllegalStateException("Unable to get verify process");
-			}
-
-			return (VerifyProcess)_bundleContext.getService(
-				serviceReferences[0]);
-		}
-		catch (InvalidSyntaxException ise) {
-			throw new IllegalStateException("Unable to get verify process");
-		}
+	protected String getSettingsId() {
+		return OpenIdConstants.SERVICE_NAME;
 	}
 
-	private static final Log _log = LogFactoryUtil.getLog(
-		OpenIdCompanySettingsVerifyProcessTest.class);
+	@Override
+	protected String getVerifyProcessName() {
+		return "com.liferay.portal.security.sso.openid";
+	}
 
-	private static BundleContext _bundleContext;
-	private static volatile CompanyLocalService _companyLocalService;
-	private static volatile PrefsProps _prefsProps;
-	private static volatile SettingsFactory _settingsFactory;
+	@Override
+	protected void populateLegacyProperties(UnicodeProperties properties) {
+		properties.put(LegacyOpenIdPropsKeys.OPENID_AUTH_ENABLED, "true");
+	}
 
 }
