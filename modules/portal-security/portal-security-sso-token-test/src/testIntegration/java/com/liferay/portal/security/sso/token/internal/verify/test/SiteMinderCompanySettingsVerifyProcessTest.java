d0ad597aaaa72b6fcbae63a416cf2365508d5d1c
Michael Han <michael.han@liferay.com>
2015-12-23 23:42:33 -0600

    LPS-61440 update to use BaseCompanySettingsVerifyPropertiesTestCase

--- modules/portal-security/portal-security-sso-token-test/src/testIntegration/java/com/liferay/portal/security/sso/token/internal/verify/test/SiteMinderCompanySettingsVerifyProcessTest.java
+++ modules/portal-security/portal-security-sso-token-test/src/testIntegration/java/com/liferay/portal/security/sso/token/internal/verify/test/SiteMinderCompanySettingsVerifyProcessTest.java
@@ -15,83 +15,85 @@
 package com.liferay.portal.security.sso.token.internal.verify.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.settings.CompanyServiceSettingsLocator;
 import com.liferay.portal.kernel.settings.Settings;
-import com.liferay.portal.kernel.settings.SettingsException;
-import com.liferay.portal.kernel.settings.SettingsFactory;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.PrefsProps;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.model.Company;
 import com.liferay.portal.security.sso.token.constants.LegacyTokenPropsKeys;
 import com.liferay.portal.security.sso.token.constants.TokenConstants;
-import com.liferay.portal.service.CompanyLocalService;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portal.verify.VerifyException;
-import com.liferay.portal.verify.VerifyProcess;
-import com.liferay.portal.verify.test.BaseVerifyProcessTestCase;
-
-import java.io.IOException;
-
-import java.util.List;
+import com.liferay.portal.verify.test.BaseCompanySettingsVerifyProcessTestCase;
 
 import javax.portlet.PortletPreferences;
 
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
-
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
-import org.osgi.framework.InvalidSyntaxException;
-import org.osgi.framework.ServiceReference;
 
 /**
  * @author Brian Greenwald
  */
 @RunWith(Arquillian.class)
 public class SiteMinderCompanySettingsVerifyProcessTest
-	extends BaseVerifyProcessTestCase {
+	extends BaseCompanySettingsVerifyProcessTestCase {
 
 	@ClassRule
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
 		new LiferayIntegrationTestRule();
 
-	@BeforeClass
-	public static void setUpClass() throws PortalException {
-		Bundle bundle = FrameworkUtil.getBundle(
-			SiteMinderCompanySettingsVerifyProcessTest.class);
+	@Override
+	protected void doVerify(
+		PortletPreferences portletPreferences, Settings settings) {
 
-		_bundleContext = bundle.getBundleContext();
+		Assert.assertTrue(
+			Validator.isNull(
+				portletPreferences.getValue(
+					LegacyTokenPropsKeys.SITEMINDER_AUTH_ENABLED,
+					StringPool.BLANK)));
 
-		ServiceReference<SettingsFactory> settingsFactoryReference =
-			_bundleContext.getServiceReference(SettingsFactory.class);
+		Assert.assertTrue(
+			Validator.isNull(
+				portletPreferences.getValue(
+					LegacyTokenPropsKeys.SITEMINDER_IMPORT_FROM_LDAP,
+					StringPool.BLANK)));
 
-		_settingsFactory = _bundleContext.getService(settingsFactoryReference);
+		Assert.assertTrue(
+			Validator.isNull(
+				portletPreferences.getValue(
+					LegacyTokenPropsKeys.SITEMINDER_USER_HEADER,
+					StringPool.BLANK)));
 
-		ServiceReference<CompanyLocalService> companyLocalServiceReference =
-			_bundleContext.getServiceReference(CompanyLocalService.class);
+		Assert.assertTrue(
+			GetterUtil.getBoolean(
+				settings.getValue(
+					TokenConstants.AUTH_ENABLED, StringPool.FALSE)));
 
-		_companyLocalService = _bundleContext.getService(
-			companyLocalServiceReference);
+		Assert.assertTrue(
+			GetterUtil.getBoolean(
+				settings.getValue(
+					TokenConstants.IMPORT_FROM_LDAP, StringPool.FALSE)));
 
-		ServiceReference<PrefsProps> prefsPropsServiceReference =
-			_bundleContext.getServiceReference(PrefsProps.class);
+		Assert.assertEquals(
+			"testSiteminder",
+			settings.getValue(TokenConstants.USER_HEADER, StringPool.BLANK));
+	}
 
-		_prefsProps = _bundleContext.getService(prefsPropsServiceReference);
+	@Override
+	protected String getSettingsId() {
+		return TokenConstants.SERVICE_NAME;
+	}
 
-		UnicodeProperties properties = new UnicodeProperties();
+	@Override
+	protected String getVerifyProcessName() {
+		return "com.liferay.portal.security.sso.token.siteminder";
+	}
 
+	@Override
+	protected void populateLegacyProperties(UnicodeProperties properties) {
 		properties.put(LegacyTokenPropsKeys.SITEMINDER_AUTH_ENABLED, "true");
 
 		properties.put(
@@ -99,126 +101,6 @@ public class SiteMinderCompanySettingsVerifyProcessTest
 
 		properties.put(
 			LegacyTokenPropsKeys.SITEMINDER_USER_HEADER, "testSiteminder");
-
-		List<Company> companies = _companyLocalService.getCompanies(false);
-
-		for (Company company : companies) {
-			_companyLocalService.updatePreferences(
-				company.getCompanyId(), properties);
-		}
 	}
-
-	@AfterClass
-	public static void tearDownClass()
-		throws InvalidSyntaxException, IOException {
-
-		List<Company> companies = _companyLocalService.getCompanies(false);
-
-		for (Company company : companies) {
-			try {
-				Settings settings = getSettings(company.getCompanyId());
-
-				settings.getModifiableSettings().reset();
-			}
-			catch (SettingsException e) {
-				throw new IOException(e);
-			}
-		}
-	}
-
-	protected static Settings getSettings(long companyId)
-		throws SettingsException {
-
-		Settings settings = _settingsFactory.getSettings(
-			new CompanyServiceSettingsLocator(
-				companyId, TokenConstants.SERVICE_NAME));
-
-		return settings;
-	}
-
-	@Override
-	protected void doVerify() throws VerifyException {
-		super.doVerify();
-
-		List<Company> companies = _companyLocalService.getCompanies(false);
-
-		for (Company company : companies) {
-			long companyId = company.getCompanyId();
-
-			PortletPreferences portletPreferences = _prefsProps.getPreferences(
-				companyId, true);
-
-			Assert.assertTrue(
-				Validator.isNull(
-					portletPreferences.getValue(
-						LegacyTokenPropsKeys.SITEMINDER_AUTH_ENABLED,
-						StringPool.BLANK)));
-
-			Assert.assertTrue(
-				Validator.isNull(
-					portletPreferences.getValue(
-						LegacyTokenPropsKeys.SITEMINDER_IMPORT_FROM_LDAP,
-						StringPool.BLANK)));
-
-			Assert.assertTrue(
-				Validator.isNull(
-					portletPreferences.getValue(
-						LegacyTokenPropsKeys.SITEMINDER_USER_HEADER,
-						StringPool.BLANK)));
-
-			Settings settings;
-
-			try {
-				settings = getSettings(companyId);
-			}
-			catch (SettingsException e) {
-				throw new VerifyException(e);
-			}
-
-			Assert.assertNotNull(settings);
-
-			Assert.assertTrue(
-				GetterUtil.getBoolean(
-					settings.getValue(
-						TokenConstants.AUTH_ENABLED, StringPool.FALSE)));
-
-			Assert.assertTrue(
-				GetterUtil.getBoolean(
-					settings.getValue(
-						TokenConstants.IMPORT_FROM_LDAP, StringPool.FALSE)));
-
-			Assert.assertEquals(
-				"testSiteminder",
-				settings.getValue(
-					TokenConstants.USER_HEADER, StringPool.BLANK));
-		}
-	}
-
-	@Override
-	protected VerifyProcess getVerifyProcess() {
-		try {
-			ServiceReference<?>[] serviceReferences =
-				_bundleContext.getAllServiceReferences(
-					VerifyProcess.class.getName(),
-					"(&(objectClass=" + VerifyProcess.class.getName() +
-						")(verify.process.name=" + "com.liferay.portal." +
-							"security.sso.token.siteminder))");
-
-			if (ArrayUtil.isEmpty(serviceReferences)) {
-				throw new IllegalStateException("Unable to get verify process");
-			}
-
-			return (VerifyProcess) _bundleContext.getService(
-				serviceReferences[0]);
-		}
-		catch (InvalidSyntaxException ise) {
-			throw new IllegalStateException("Unable to get verify process");
-		}
-	}
-
-	private static BundleContext _bundleContext;
-	private static volatile CompanyLocalService _companyLocalService;
-	private static volatile PrefsProps _prefsProps;
-	private static volatile SettingsFactory _settingsFactory;
 
 }
