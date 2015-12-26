2bc60afb1e02e387b43956248ee5659d57d3e305
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-12-26 19:06:01 -0300

    LPS-61202 Move to DDLWebConfiguration

--- modules/apps/dynamic-data-lists/dynamic-data-lists-web/src/main/java/com/liferay/dynamic/data/lists/web/configuration/DDLWebConfiguration.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-web/src/main/java/com/liferay/dynamic/data/lists/web/configuration/DDLWebConfiguration.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.dynamic.data.lists.configuration;
+package com.liferay.dynamic.data.lists.web.configuration;
 
 import aQute.bnd.annotation.metatype.Meta;
 
@@ -21,14 +21,12 @@ import com.liferay.configuration.admin.ConfigurationAdmin;
 /**
  * @author Lino Alves
  */
-@ConfigurationAdmin(
-	category = "productivity", scope = ConfigurationAdmin.Scope.GROUP
-)
+@ConfigurationAdmin(category = "productivity")
 @Meta.OCD(
-	id = "com.liferay.dynamic.data.lists.configuration.DDLServiceConfiguration",
-	localization = "content/Language", name = "%ddl.service.configuration.name"
+	id = "com.liferay.dynamic.data.lists.web.configuration.DDLWebConfiguration",
+	localization = "content/Language", name = "%ddl.web.configuration.name"
 )
-public interface DDLServiceConfiguration {
+public interface DDLWebConfiguration {
 
 	@Meta.AD(deflt = "list", required = false)
 	public String defaultDisplayView();
