1b65941f03220c7a8d04978f15037dfd3c10ba15
Julio Camarero <julio.camarero@liferay.com>
2015-12-28 16:56:17 +0100

    LPS-61533 this code is not needed anymore

--- modules/apps/product-navigation/product-navigation-product-menu-web/src/main/java/com/liferay/product/navigation/product/menu/web/control/menu/ProductMenuControlMenuEntry.java
+++ modules/apps/product-navigation/product-navigation-product-menu-web/src/main/java/com/liferay/product/navigation/product/menu/web/control/menu/ProductMenuControlMenuEntry.java
@@ -14,26 +14,14 @@
 
 package com.liferay.product.navigation.product.menu.web.control.menu;
 
-import com.liferay.application.list.PanelCategory;
-import com.liferay.application.list.PanelCategoryRegistry;
-import com.liferay.application.list.constants.PanelCategoryKeys;
 import com.liferay.control.menu.BaseJSPControlMenuEntry;
 import com.liferay.control.menu.ControlMenuEntry;
 import com.liferay.control.menu.constants.ControlMenuCategoryKeys;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.portal.theme.ThemeDisplay;
-
-import java.util.List;
 
 import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Julio Camarero
@@ -54,30 +42,6 @@ public class ProductMenuControlMenuEntry extends BaseJSPControlMenuEntry {
 	}
 
 	@Override
-	public boolean isShow(HttpServletRequest request) throws PortalException {
-		if (_panelCategoryRegistry == null) {
-			return false;
-		}
-
-		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		List<PanelCategory> panelCategories =
-			_panelCategoryRegistry.getChildPanelCategories(
-				PanelCategoryKeys.ROOT, themeDisplay.getPermissionChecker(),
-				themeDisplay.getScopeGroup());
-
-		// If only the Personal Panel is shown, then the product menu link will
-		// not be shown to users
-
-		if (panelCategories.size() <= 1) {
-			return false;
-		}
-
-		return super.isShow(request);
-	}
-
-	@Override
 	@Reference(
 		target = "(osgi.web.symbolicname=com.liferay.product.navigation.product.menu.web)",
 		unbind = "-"
@@ -85,24 +49,5 @@ public class ProductMenuControlMenuEntry extends BaseJSPControlMenuEntry {
 	public void setServletContext(ServletContext servletContext) {
 		super.setServletContext(servletContext);
 	}
-
-	@Reference(
-		cardinality = ReferenceCardinality.OPTIONAL,
-		policy = ReferencePolicy.DYNAMIC,
-		policyOption = ReferencePolicyOption.GREEDY
-	)
-	protected void setPanelCategoryRegistry(
-		PanelCategoryRegistry panelCategoryRegistry) {
-
-		_panelCategoryRegistry = panelCategoryRegistry;
-	}
-
-	protected void unsetPanelCategoryRegistry(
-		PanelCategoryRegistry panelCategoryRegistry) {
-
-		_panelCategoryRegistry = null;
-	}
-
-	private volatile PanelCategoryRegistry _panelCategoryRegistry;
 
 }
