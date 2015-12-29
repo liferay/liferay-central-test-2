f434d4ae16aea6a7ac45f9716224021102a2ee03
Inacio Nery <inacio.nery@liferay.com>
2015-12-29 10:24:06 -0300

    LPS-61361 Add class WorkflowInstanceResultRowSplitter to split the WorkflowInstance in completed and pending in the view

---
+++ modules/apps/workflow/workflow-instance-web/src/main/java/com/liferay/workflow/instance/web/dao/search/WorkflowInstanceResultRowSplitter.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.workflow.instance.web.dao.search;
+
+import com.liferay.portal.kernel.dao.search.ResultRow;
+import com.liferay.portal.kernel.dao.search.ResultRowSplitter;
+import com.liferay.portal.kernel.dao.search.ResultRowSplitterEntry;
+import com.liferay.portal.kernel.workflow.WorkflowInstance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Inácio Nery
+ */
+public class WorkflowInstanceResultRowSplitter implements ResultRowSplitter {
+
+	@Override
+	public List<ResultRowSplitterEntry> split(List<ResultRow> resultRows) {
+		List<ResultRowSplitterEntry> resultRowSplitterEntries =
+			new ArrayList<>();
+
+		List<ResultRow> workflowTaskCompletedResultRows = new ArrayList<>();
+		List<ResultRow> workflowTaskPendingResultRows = new ArrayList<>();
+
+		for (ResultRow resultRow : resultRows) {
+			WorkflowInstance workflowInstance =
+				(WorkflowInstance)resultRow.getObject();
+
+			if (workflowInstance.isComplete()) {
+				workflowTaskCompletedResultRows.add(resultRow);
+			}
+			else {
+				workflowTaskPendingResultRows.add(resultRow);
+			}
+		}
+
+		if (!workflowTaskPendingResultRows.isEmpty()) {
+			resultRowSplitterEntries.add(
+				new ResultRowSplitterEntry(
+					"Pending", workflowTaskPendingResultRows));
+		}
+
+		if (!workflowTaskCompletedResultRows.isEmpty()) {
+			resultRowSplitterEntries.add(
+				new ResultRowSplitterEntry(
+					"Completed", workflowTaskCompletedResultRows));
+		}
+
+		return resultRowSplitterEntries;
+	}
+
+}
