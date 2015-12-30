3870ad3874235660e78310f7f0a64612d45b9a36
Hugo Huijser <hugo.huijser@liferay.com>
2015-12-30 14:26:12 +0800

    LPS-61420 SourceFormatter incorrectly formatting annotation

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/JavaSourceProcessor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/JavaSourceProcessor.java
@@ -1285,7 +1285,13 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 					if (matcher.find()) {
 						String match = matcher.group();
 
-						if (!match.endsWith("\n)\n") &&
+						int closeParenthesesCount = StringUtil.count(
+							match, StringPool.CLOSE_PARENTHESIS);
+						int openParenthesesCount = StringUtil.count(
+							match, StringPool.OPEN_PARENTHESIS);
+
+						if ((closeParenthesesCount == openParenthesesCount) &&
+							!match.endsWith("\n)\n") &&
 							!match.endsWith("\t)\n")) {
 
 							String tabs = matcher.group(1);
