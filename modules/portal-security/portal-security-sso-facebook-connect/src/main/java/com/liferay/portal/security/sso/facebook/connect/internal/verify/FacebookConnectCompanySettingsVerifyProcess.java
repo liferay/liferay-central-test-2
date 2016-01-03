068b1063fa8c2000ba66b77a855da2314d2ee644
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2016-01-03 16:27:45 +0000

    LPS-61133 Add FacebookConnectCompanySettingsVerifyProcess

---
+++ modules/portal-security/portal-security-sso-facebook-connect/src/main/java/com/liferay/portal/security/sso/facebook/connect/internal/verify/FacebookConnectCompanySettingsVerifyProcess.java
@@ -0,0 +1,139 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.sso.facebook.connect.internal.verify;
+
+import com.liferay.portal.kernel.settings.SettingsFactory;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.kernel.util.PrefsProps;
+import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.security.sso.facebook.connect.constants.FacebookConnectConstants;
+import com.liferay.portal.security.sso.facebook.connect.constants.LegacyFacebookConnectPropsKeys;
+import com.liferay.portal.service.CompanyLocalService;
+import com.liferay.portal.verify.BaseCompanySettingsVerifyProcess;
+import com.liferay.portal.verify.VerifyProcess;
+
+import java.util.Dictionary;
+import java.util.Set;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Stian Sigvartsen
+ */
+@Component(
+	immediate = true,
+	property = {"verify.process.name=com.liferay.portal.security.sso.facebook.connect"},
+	service = VerifyProcess.class
+)
+public class FacebookConnectCompanySettingsVerifyProcess
+	extends BaseCompanySettingsVerifyProcess {
+
+	@Override
+	protected CompanyLocalService getCompanyLocalService() {
+		return _companyLocalService;
+	}
+
+	@Override
+	protected Set<String> getLegacyPropertyKeys() {
+		return SetUtil.fromArray(LegacyFacebookConnectPropsKeys.FACEBOOK_KEYS);
+	}
+
+	@Override
+	protected Dictionary<String, String> getPropertyValues(long companyId) {
+		Dictionary<String, String> dictionary = new HashMapDictionary<>();
+
+		dictionary.put(
+			FacebookConnectConstants.AUTH_ENABLED,
+			_prefsProps.getString(
+				companyId, LegacyFacebookConnectPropsKeys.AUTH_ENABLED,
+				StringPool.FALSE));
+
+		dictionary.put(
+				FacebookConnectConstants.APP_ID,
+				_prefsProps.getString(
+					companyId, LegacyFacebookConnectPropsKeys.APP_ID, ""));
+
+		dictionary.put(
+				FacebookConnectConstants.APP_SECRET,
+				_prefsProps.getString(
+					companyId, LegacyFacebookConnectPropsKeys.APP_SECRET, ""));
+
+		dictionary.put(
+				FacebookConnectConstants.GRAPH_URL,
+				_prefsProps.getString(
+					companyId, LegacyFacebookConnectPropsKeys.GRAPH_URL, ""));
+
+		dictionary.put(
+				FacebookConnectConstants.OAUTH_AUTH_URL,
+				_prefsProps.getString(
+					companyId, LegacyFacebookConnectPropsKeys.OAUTH_AUTH_URL,
+					""));
+
+		dictionary.put(
+				FacebookConnectConstants.OAUTH_REDIRECT_URL,
+				_prefsProps.getString(
+					companyId,
+					LegacyFacebookConnectPropsKeys.OAUTH_REDIRECT_URL, ""));
+
+		dictionary.put(
+				FacebookConnectConstants.OAUTH_TOKEN_URL,
+				_prefsProps.getString(
+					companyId, LegacyFacebookConnectPropsKeys.OAUTH_TOKEN_URL,
+					""));
+
+		dictionary.put(
+				FacebookConnectConstants.VERIFIED_ACCOUNT_REQUIRED,
+				_prefsProps.getString(
+					companyId,
+					LegacyFacebookConnectPropsKeys.VERIFIED_ACCOUNT_REQUIRED,
+					StringPool.FALSE));
+
+		return dictionary;
+	}
+
+	@Override
+	protected SettingsFactory getSettingsFactory() {
+		return _settingsFactory;
+	}
+
+	@Override
+	protected String getSettingsId() {
+		return FacebookConnectConstants.SERVICE_NAME;
+	}
+
+	@Reference(unbind = "-")
+	protected void setCompanyLocalService(
+		CompanyLocalService companyLocalService) {
+
+		_companyLocalService = companyLocalService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setPrefsProps(PrefsProps prefsProps) {
+		_prefsProps = prefsProps;
+	}
+
+	@Reference(unbind = "-")
+	protected void setSettingsFactory(SettingsFactory settingsFactory) {
+		_settingsFactory = settingsFactory;
+	}
+
+	private volatile CompanyLocalService _companyLocalService;
+	private volatile PrefsProps _prefsProps;
+	private volatile SettingsFactory _settingsFactory;
+
+}
