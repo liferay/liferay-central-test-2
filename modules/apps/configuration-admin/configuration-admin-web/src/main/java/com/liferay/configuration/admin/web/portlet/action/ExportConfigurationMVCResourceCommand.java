a801a1e443bd52d89520331735305144de512234
Jorge Ferrer <jorge.ferrer@liferay.com>
2016-01-05 00:45:22 +0100

    LPS-61709

---
+++ modules/apps/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/portlet/action/ExportConfigurationMVCResourceCommand.java
@@ -0,0 +1,193 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.configuration.admin.web.portlet.action;
+
+import com.liferay.configuration.admin.ExtendedAttributeDefinition;
+import com.liferay.configuration.admin.ExtendedMetaTypeService;
+import com.liferay.configuration.admin.web.constants.ConfigurationAdminPortletKeys;
+import com.liferay.configuration.admin.web.model.ConfigurationModel;
+import com.liferay.configuration.admin.web.util.AttributeDefinitionUtil;
+import com.liferay.configuration.admin.web.util.ConfigurationHelper;
+import com.liferay.portal.kernel.portlet.PortletResponseUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PropertiesUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.theme.ThemeDisplay;
+
+import java.util.Properties;
+
+import javax.portlet.MimeResponse;
+import javax.portlet.PortletException;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.metatype.AttributeDefinition;
+
+/**
+ * @author Brian Wing Shun Chan
+ * @author Vilmos Papp
+ * @author Eduardo Garcia
+ * @author Raymond Augé
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" +
+			ConfigurationAdminPortletKeys.CONFIGURATION_ADMIN,
+		"mvc.command.name=export"
+	},
+	service = MVCResourceCommand.class
+)
+public class ExportConfigurationMVCResourceCommand
+	implements MVCResourceCommand {
+
+	@Activate
+	public void activate(BundleContext bundleContext) {
+		_bundleContext = bundleContext;
+	}
+
+	@Deactivate
+	public void deactivate() {
+		_bundleContext = null;
+	}
+
+	@Override
+	public boolean serveResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws PortletException {
+
+		if (!(resourceResponse instanceof MimeResponse)) {
+			return false;
+		}
+
+		String factoryPid = ParamUtil.getString(resourceRequest, "factoryPid");
+
+		String pid = ParamUtil.getString(resourceRequest, "pid", factoryPid);
+
+		try {
+			String fileName = pid + ".cfg";
+
+			PortletResponseUtil.sendFile(
+				resourceRequest, resourceResponse, fileName,
+				getPropertiesAsBytes(resourceRequest, resourceResponse),
+				ContentTypes.TEXT_XML_UTF8);
+		}
+		catch (Exception e) {
+			throw new PortletException(e);
+		}
+
+		return true;
+	}
+
+	protected Properties getProperties(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)resourceRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		ConfigurationHelper configurationHelper = new ConfigurationHelper(
+			_bundleContext, _configurationAdmin, _extendedMetaTypeService,
+			themeDisplay.getLanguageId());
+
+		String factoryPid = ParamUtil.getString(resourceRequest, "factoryPid");
+
+		String pid = ParamUtil.getString(resourceRequest, "pid", factoryPid);
+
+		ConfigurationModel configurationModel =
+			configurationHelper.getConfigurationModel(pid);
+
+		if ((configurationModel == null) && Validator.isNotNull(factoryPid)) {
+			configurationModel = configurationHelper.getConfigurationModel(
+				factoryPid);
+		}
+
+		ExtendedAttributeDefinition[] attributeDefinitions =
+			configurationModel.getAttributeDefinitions(ConfigurationModel.ALL);
+
+		Properties properties = new Properties();
+
+		for (AttributeDefinition attributeDefinition : attributeDefinitions) {
+			Configuration configuration = configurationModel.getConfiguration();
+
+			String[] values = AttributeDefinitionUtil.getProperty(
+				attributeDefinition, configuration);
+
+			String value = null;
+
+			// See http://felix.apache.org/documentation/subprojects/apache-felix-file-install.html
+
+			if (values.length == 1) {
+				value = values[0];
+			}
+			else if (values.length > 1) {
+				value = StringUtil.merge(values, "\n");
+			}
+
+			properties.setProperty(attributeDefinition.getID(), value);
+		}
+
+		return properties;
+	}
+
+	protected byte[] getPropertiesAsBytes(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		Properties properties = getProperties(
+			resourceRequest, resourceResponse);
+
+		String propertiesString = PropertiesUtil.toString(properties);
+
+		propertiesString = _PROPERTIES_HELP + propertiesString;
+
+		return propertiesString.getBytes();
+	}
+
+	@Reference(unbind = "-")
+	protected void setConfigurationAdmin(
+		ConfigurationAdmin configurationAdmin) {
+
+		_configurationAdmin = configurationAdmin;
+	}
+
+	@Reference(unbind = "-")
+	protected void setExtendedMetaTypeService(
+		ExtendedMetaTypeService extendedMetaTypeService) {
+
+		_extendedMetaTypeService = extendedMetaTypeService;
+	}
+
+	private static final String _PROPERTIES_HELP =
+		"##\n" +
+			"## Deploy this file to any Liferay installation to apply its " +
+				"configuration \n" + "##\n";
+
+	private BundleContext _bundleContext;
+	private volatile ConfigurationAdmin _configurationAdmin;
+	private volatile ExtendedMetaTypeService _extendedMetaTypeService;
+
+}
