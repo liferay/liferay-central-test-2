7b55d537b8f689ccbeb8bcae1c6fcba9ec6322a2
yxingwu <yuxing.wu@liferay.com>
2016-01-04 09:53:00 +0800

    LPS-60861 Remove the deprecated methods in WikiPage

--- modules/apps/wiki/wiki-service/src/main/java/com/liferay/wiki/service/impl/WikiPageServiceImpl.java
+++ modules/apps/wiki/wiki-service/src/main/java/com/liferay/wiki/service/impl/WikiPageServiceImpl.java
@@ -338,21 +338,6 @@ public class WikiPageServiceImpl extends WikiPageServiceBaseImpl {
 		return pages;
 	}
 
-	/**
-	 * @deprecated As of 6.2.0, replaced by {@link #getNodePagesRSS(long, int,
-	 *             String, double, String, String, String, String)}
-	 */
-	@Deprecated
-	@Override
-	public String getNodePagesRSS(
-			long nodeId, int max, String type, double version,
-			String displayStyle, String feedURL, String entryURL)
-		throws PortalException {
-
-		return getNodePagesRSS(
-			nodeId, max, type, version, displayStyle, feedURL, entryURL, null);
-	}
-
 	@Override
 	public String getNodePagesRSS(
 			long nodeId, int max, String type, double version,
@@ -541,23 +526,6 @@ public class WikiPageServiceImpl extends WikiPageServiceBaseImpl {
 		}
 	}
 
-	/**
-	 * @deprecated As of 6.2.0, replaced by {@link #getPagesRSS(long, String,
-	 *             int, String, double, String, String, String, String, Locale)}
-	 */
-	@Deprecated
-	@Override
-	public String getPagesRSS(
-			long companyId, long nodeId, String title, int max, String type,
-			double version, String displayStyle, String feedURL,
-			String entryURL, Locale locale)
-		throws PortalException {
-
-		return getPagesRSS(
-			nodeId, title, max, type, version, displayStyle, feedURL, entryURL,
-			null, locale);
-	}
-
 	@Override
 	public String getPagesRSS(
 			long nodeId, String title, int max, String type, double version,
@@ -618,20 +586,6 @@ public class WikiPageServiceImpl extends WikiPageServiceBaseImpl {
 
 		return wikiPageLocalService.getTempFileNames(
 			node.getGroupId(), getUserId(), folderName);
-	}
-
-	/**
-	 * @deprecated As of 6.2.0, replaced by {@link #renamePage(long, String,
-	 *             String, ServiceContext)}
-	 */
-	@Deprecated
-	@Override
-	public void movePage(
-			long nodeId, String title, String newTitle,
-			ServiceContext serviceContext)
-		throws PortalException {
-
-		renamePage(nodeId, title, newTitle, serviceContext);
 	}
 
 	@Override
