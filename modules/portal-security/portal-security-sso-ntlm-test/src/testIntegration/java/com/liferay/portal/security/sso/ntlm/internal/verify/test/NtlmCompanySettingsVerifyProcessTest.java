fc7da43b27c57f439028d2eac8add962a6a6b071
Brian Greenwald <brian.greenwald@liferay.com>
2016-01-05 14:05:50 -0600

    LPS-61652 Update to use BaseCompanySettingsVerifyProcessTestCase

--- modules/portal-security/portal-security-sso-ntlm-test/src/testIntegration/java/com/liferay/portal/security/sso/ntlm/internal/verify/test/NtlmCompanySettingsVerifyProcessTest.java
+++ modules/portal-security/portal-security-sso-ntlm-test/src/testIntegration/java/com/liferay/portal/security/sso/ntlm/internal/verify/test/NtlmCompanySettingsVerifyProcessTest.java
@@ -15,85 +15,92 @@
 package com.liferay.portal.security.sso.ntlm.internal.verify.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.settings.CompanyServiceSettingsLocator;
-import com.liferay.portal.kernel.settings.ModifiableSettings;
 import com.liferay.portal.kernel.settings.Settings;
-import com.liferay.portal.kernel.settings.SettingsException;
-import com.liferay.portal.kernel.settings.SettingsFactory;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.PrefsProps;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.model.Company;
 import com.liferay.portal.security.sso.ntlm.constants.LegacyNtlmPropsKeys;
 import com.liferay.portal.security.sso.ntlm.constants.NtlmConstants;
-import com.liferay.portal.service.CompanyLocalService;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portal.verify.VerifyException;
-import com.liferay.portal.verify.VerifyProcess;
-import com.liferay.portal.verify.test.BaseVerifyProcessTestCase;
-
-import java.io.IOException;
-
-import java.util.List;
+import com.liferay.portal.verify.test.BaseCompanySettingsVerifyProcessTestCase;
 
 import javax.portlet.PortletPreferences;
 
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
-
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
-import org.osgi.framework.InvalidSyntaxException;
-import org.osgi.framework.ServiceReference;
 
 /**
  * @author Brian Greenwald
  */
 @RunWith(Arquillian.class)
 public class NtlmCompanySettingsVerifyProcessTest
-	extends BaseVerifyProcessTestCase {
+	extends BaseCompanySettingsVerifyProcessTestCase {
 
 	@ClassRule
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
 		new LiferayIntegrationTestRule();
 
-	@BeforeClass
-	public static void setUpClass() throws PortalException {
-		Bundle bundle = FrameworkUtil.getBundle(
-			NtlmCompanySettingsVerifyProcessTest.class);
+	protected void doVerify(
+		PortletPreferences portletPreferences, Settings settings) {
 
-		_bundleContext = bundle.getBundleContext();
+		for (String key : LegacyNtlmPropsKeys.NTLM_KEYS) {
+			Assert.assertTrue(
+				Validator.isNull(
+					portletPreferences.getValue(key, StringPool.BLANK)));
+		}
 
-		ServiceReference<CompanyLocalService> companyLocalServiceReference =
-			_bundleContext.getServiceReference(CompanyLocalService.class);
+		Assert.assertEquals(
+			"testDomain",
+			settings.getValue(NtlmConstants.AUTH_DOMAIN, StringPool.BLANK));
 
-		_companyLocalService = _bundleContext.getService(
-			companyLocalServiceReference);
+		Assert.assertEquals(
+			"testDomainController",
+			settings.getValue(
+				NtlmConstants.AUTH_DOMAIN_CONTROLLER, StringPool.BLANK));
 
-		ServiceReference<PrefsProps> prefsPropsServiceReference =
-			_bundleContext.getServiceReference(PrefsProps.class);
+		Assert.assertEquals(
+			"testDomainControllerName",
+			settings.getValue(
+				NtlmConstants.AUTH_DOMAIN_CONTROLLER_NAME, StringPool.BLANK));
 
-		_prefsProps = _bundleContext.getService(prefsPropsServiceReference);
+		Assert.assertTrue(
+			GetterUtil.getBoolean(
+				settings.getValue(
+					NtlmConstants.AUTH_ENABLED, StringPool.FALSE)));
 
-		ServiceReference<SettingsFactory> configurationAdminServiceReference =
-			_bundleContext.getServiceReference(SettingsFactory.class);
+		Assert.assertEquals(
+			"testNegotiateFlags",
+			settings.getValue(
+				NtlmConstants.AUTH_NEGOTIATE_FLAGS, StringPool.BLANK));
 
-		_settingsFactory = _bundleContext.getService(
-			configurationAdminServiceReference);
+		Assert.assertEquals(
+			"test@serviceAccount.com",
+			settings.getValue(
+				NtlmConstants.AUTH_SERVICE_ACCOUNT, StringPool.BLANK));
 
-		UnicodeProperties properties = new UnicodeProperties();
+		Assert.assertEquals(
+			"testServicePassword",
+			settings.getValue(
+				NtlmConstants.AUTH_SERVICE_PASSWORD, StringPool.BLANK));
+	}
 
+	@Override
+	protected String getSettingsId() {
+		return NtlmConstants.SERVICE_NAME;
+	}
+
+	@Override
+	protected String getVerifyProcessName() {
+		return "com.liferay.portal.security.sso.ntlm";
+	}
+
+	@Override
+	protected void populateLegacyProperties(UnicodeProperties properties) {
 		properties.put(LegacyNtlmPropsKeys.NTLM_AUTH_DOMAIN, "testDomain");
 
 		properties.put(
@@ -117,125 +124,6 @@ public class NtlmCompanySettingsVerifyProcessTest
 		properties.put(
 			LegacyNtlmPropsKeys.NTLM_AUTH_SERVICE_PASSWORD,
 			"testServicePassword");
-
-		List<Company> companies = _companyLocalService.getCompanies(false);
-
-		for (Company company : companies) {
-			_companyLocalService.updatePreferences(
-				company.getCompanyId(), properties);
-		}
 	}
-
-	@AfterClass
-	public static void tearDownClass()
-		throws InvalidSyntaxException, IOException {
-
-		List<Company> companies = _companyLocalService.getCompanies(false);
-
-		for (Company company : companies) {
-			Settings settings = getSettings(company.getCompanyId());
-
-			ModifiableSettings modifiableSettings =
-				settings.getModifiableSettings();
-
-			modifiableSettings.reset();
-		}
-
-		_bundleContext = null;
-	}
-
-	protected static Settings getSettings(long companyId) {
-		try {
-			Settings settings = _settingsFactory.getSettings(
-				new CompanyServiceSettingsLocator(
-					companyId, NtlmConstants.SERVICE_NAME));
-
-			return settings;
-		}
-		catch (SettingsException e) {
-			throw new IllegalStateException(e);
-		}
-	}
-
-	protected void doVerify() throws VerifyException {
-		super.doVerify();
-
-		List<Company> companies = _companyLocalService.getCompanies(false);
-
-		for (Company company : companies) {
-			PortletPreferences portletPreferences = _prefsProps.getPreferences(
-				company.getCompanyId(), true);
-
-			for (String key : LegacyNtlmPropsKeys.NTLM_KEYS) {
-				Assert.assertTrue(
-					Validator.isNull(
-						portletPreferences.getValue(key, StringPool.BLANK)));
-			}
-
-			Settings settings = getSettings(company.getCompanyId());
-
-			Assert.assertEquals(
-				"testDomain",
-				settings.getValue(NtlmConstants.AUTH_DOMAIN, StringPool.BLANK));
-
-			Assert.assertEquals(
-				"testDomainController",
-				settings.getValue(
-					NtlmConstants.AUTH_DOMAIN_CONTROLLER, StringPool.BLANK));
-
-			Assert.assertEquals(
-				"testDomainControllerName",
-				settings.getValue(
-					NtlmConstants.AUTH_DOMAIN_CONTROLLER_NAME,
-					StringPool.BLANK));
-
-			Assert.assertTrue(
-				GetterUtil.getBoolean(
-					settings.getValue(
-						NtlmConstants.AUTH_ENABLED, StringPool.FALSE)));
-
-			Assert.assertEquals(
-				"testNegotiateFlags",
-				settings.getValue(
-					NtlmConstants.AUTH_NEGOTIATE_FLAGS, StringPool.BLANK));
-
-			Assert.assertEquals(
-				"test@serviceAccount.com",
-				settings.getValue(
-					NtlmConstants.AUTH_SERVICE_ACCOUNT, StringPool.BLANK));
-
-			Assert.assertEquals(
-				"testServicePassword",
-				settings.getValue(
-					NtlmConstants.AUTH_SERVICE_PASSWORD, StringPool.BLANK));
-		}
-	}
-
-	@Override
-	protected VerifyProcess getVerifyProcess() {
-		try {
-			ServiceReference<?>[] serviceReferences =
-				_bundleContext.getAllServiceReferences(
-					VerifyProcess.class.getName(),
-					"(&(objectClass=" + VerifyProcess.class.getName() +
-						")(verify.process.name=" +
-						"com.liferay.portal.security.sso.ntlm))");
-
-			if (ArrayUtil.isEmpty(serviceReferences)) {
-				throw new IllegalStateException("Unable to get verify process");
-			}
-
-			return (VerifyProcess)_bundleContext.getService(
-				serviceReferences[0]);
-		}
-		catch (InvalidSyntaxException ise) {
-			throw new IllegalStateException("Unable to get verify process");
-		}
-	}
-
-	private static BundleContext _bundleContext;
-	private static volatile CompanyLocalService _companyLocalService;
-	private static volatile PrefsProps _prefsProps;
-	private static volatile SettingsFactory _settingsFactory;
 
 }
