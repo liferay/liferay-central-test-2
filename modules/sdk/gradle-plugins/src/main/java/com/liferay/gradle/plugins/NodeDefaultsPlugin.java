9bcb699adc9b16bfb0780b779c9b3611d17570a6
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-01-05 12:28:38 -0800

    LPS-61754 gradle-plugins-node already does it

--- modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/NodeDefaultsPlugin.java
+++ modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/NodeDefaultsPlugin.java
@@ -14,18 +14,10 @@
 
 package com.liferay.gradle.plugins;
 
-import aQute.bnd.osgi.Constants;
-
 import com.liferay.gradle.plugins.node.NodePlugin;
 import com.liferay.gradle.plugins.node.tasks.PublishNodeModuleTask;
 import com.liferay.gradle.util.GradleUtil;
 import com.liferay.gradle.util.Validator;
-
-import java.util.Map;
-import java.util.concurrent.Callable;
-
-import org.dm.gradle.plugins.bundle.BundleExtension;
-import org.dm.gradle.plugins.bundle.BundlePlugin;
 
 import org.gradle.api.Action;
 import org.gradle.api.Project;
@@ -41,17 +33,6 @@ public class NodeDefaultsPlugin extends BaseDefaultsPlugin<NodePlugin> {
 		final Project project, NodePlugin nodePlugin) {
 
 		configureTasksPublishNodeModule(project);
-
-		withPlugin(
-			project, BundlePlugin.class,
-			new Action<BundlePlugin>() {
-
-				@Override
-				public void execute(BundlePlugin bundlePlugin) {
-					configureTasksPublishNodeModuleForBundlePlugin(project);
-				}
-
-			});
 	}
 
 	protected void configureTaskPublishNodeModule(
@@ -114,40 +95,6 @@ public class NodeDefaultsPlugin extends BaseDefaultsPlugin<NodePlugin> {
 		}
 	}
 
-	protected void configureTaskPublishNodeModuleForBundlePlugin(
-		final PublishNodeModuleTask publishNodeModuleTask) {
-
-		publishNodeModuleTask.setModuleDescription(
-			new Callable<String>() {
-
-				@Override
-				public String call() throws Exception {
-					return getBundleInstruction(
-						publishNodeModuleTask.getProject(),
-						Constants.BUNDLE_NAME);
-				}
-
-			});
-
-		publishNodeModuleTask.setModuleName(
-			new Callable<String>() {
-
-				@Override
-				public String call() throws Exception {
-					String bundleSymbolicName = getBundleInstruction(
-						publishNodeModuleTask.getProject(),
-						Constants.BUNDLE_SYMBOLICNAME);
-
-					int pos = bundleSymbolicName.indexOf('.');
-
-					String moduleName = bundleSymbolicName.substring(pos + 1);
-
-					return moduleName.replace('.', '-');
-				}
-
-			});
-	}
-
 	protected void configureTasksPublishNodeModule(Project project) {
 		TaskContainer taskContainer = project.getTasks();
 
@@ -163,39 +110,6 @@ public class NodeDefaultsPlugin extends BaseDefaultsPlugin<NodePlugin> {
 				}
 
 			});
-	}
-
-	protected void configureTasksPublishNodeModuleForBundlePlugin(
-		Project project) {
-
-		TaskContainer taskContainer = project.getTasks();
-
-		taskContainer.withType(
-			PublishNodeModuleTask.class,
-			new Action<PublishNodeModuleTask>() {
-
-				@Override
-				public void execute(
-					PublishNodeModuleTask publishNodeModuleTask) {
-
-					configureTaskPublishNodeModuleForBundlePlugin(
-						publishNodeModuleTask);
-				}
-
-			});
-	}
-
-	protected String getBundleInstruction(Project project, String key) {
-		Map<String, String> bundleInstructions = getBundleInstructions(project);
-
-		return bundleInstructions.get(key);
-	}
-
-	protected Map<String, String> getBundleInstructions(Project project) {
-		BundleExtension bundleExtension = GradleUtil.getExtension(
-			project, BundleExtension.class);
-
-		return (Map<String, String>)bundleExtension.getInstructions();
 	}
 
 	@Override
