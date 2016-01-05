73f44274db6f5bd85ce9776d11ee5e8ac99a08c9
shuyangzhou <shuyang.zhou@liferay.com>
2016-01-05 10:07:20 -0800

    LPS-61495 Tighter looping core

--- portal-impl/src/com/liferay/portal/dao/db/BaseDB.java
+++ portal-impl/src/com/liferay/portal/dao/db/BaseDB.java
@@ -1136,7 +1136,7 @@ public abstract class BaseDB implements DB {
 		"SPECIFIC_TIMESTAMP_\\d+");
 
 	static {
-		StringBundler sb = new StringBundler(TEMPLATE.length * 5 - 7);
+		StringBundler sb = new StringBundler(TEMPLATE.length * 5 - 6);
 
 		for (int i = 0; i < TEMPLATE.length; i++) {
 			String variable = TEMPLATE[i];
@@ -1152,10 +1152,10 @@ public abstract class BaseDB implements DB {
 				sb.append("\\b");
 			}
 
-			if (i < (TEMPLATE.length - 1)) {
-				sb.append(StringPool.PIPE);
-			}
+			sb.append(StringPool.PIPE);
 		}
+
+		sb.setIndex(sb.index() - 1);
 
 		_templatePattern = Pattern.compile(sb.toString());
 	}
