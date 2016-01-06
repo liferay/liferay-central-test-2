c36b8a9fb114f9a25279b9828826d965f9fc0813
Máté Thurzó <mate.thurzo@liferay.com>
2016-01-06 14:22:54 +0100

    LPS-60199 Update the MVCActionCommand for publish templates with missing methods

--- modules/apps/staging/staging-processes-web/src/main/java/com/liferay/staging/processes/web/portlet/action/EditPublishConfigurationMVCActionCommand.java
+++ modules/apps/staging/staging-processes-web/src/main/java/com/liferay/staging/processes/web/portlet/action/EditPublishConfigurationMVCActionCommand.java
@@ -26,17 +26,24 @@ import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.model.TrashedModel;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portlet.exportimport.configuration.ExportImportConfigurationConstants;
 import com.liferay.portlet.exportimport.configuration.ExportImportConfigurationHelper;
 import com.liferay.portlet.exportimport.model.ExportImportConfiguration;
 import com.liferay.portlet.exportimport.service.ExportImportConfigurationLocalService;
+import com.liferay.portlet.exportimport.service.ExportImportConfigurationService;
 import com.liferay.portlet.exportimport.staging.StagingUtil;
+import com.liferay.portlet.trash.service.TrashEntryService;
+import com.liferay.portlet.trash.util.TrashUtil;
 import com.liferay.staging.processes.web.constants.StagingProcessesPortletKeys;
 
 import java.io.Serializable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.portlet.ActionRequest;
@@ -58,6 +65,53 @@ import org.osgi.service.component.annotations.Reference;
 )
 public class EditPublishConfigurationMVCActionCommand
 	extends BaseMVCActionCommand {
+
+	protected void deleteExportImportConfiguration(
+			ActionRequest actionRequest, boolean moveToTrash)
+		throws PortalException {
+
+		long[] deleteExportImportConfigurationIds = null;
+
+		long exportImportConfigurationId = ParamUtil.getLong(
+			actionRequest, "exportImportConfigurationId");
+
+		if (exportImportConfigurationId > 0) {
+			deleteExportImportConfigurationIds =
+				new long[] {exportImportConfigurationId};
+		}
+		else {
+			deleteExportImportConfigurationIds = StringUtil.split(
+				ParamUtil.getString(
+					actionRequest, "deleteExportImportConfigurationIds"),
+				0L);
+		}
+
+		List<TrashedModel> trashedModels = new ArrayList<>();
+
+		for (long deleteExportImportConfigurationId :
+				deleteExportImportConfigurationIds) {
+
+			if (moveToTrash) {
+				ExportImportConfiguration exportImportConfiguration =
+					_exportImportConfigurationService.
+						moveExportImportConfigurationToTrash(
+							deleteExportImportConfigurationId);
+
+				trashedModels.add(exportImportConfiguration);
+			}
+			else {
+				_exportImportConfigurationService.
+					deleteExportImportConfiguration(
+						deleteExportImportConfigurationId);
+			}
+		}
+
+		if (moveToTrash && !trashedModels.isEmpty()) {
+			TrashUtil.addTrashSessionMessages(actionRequest, trashedModels);
+
+			hideDefaultSuccessMessage(actionRequest);
+		}
+	}
 
 	@Override
 	protected void doProcessAction(
@@ -84,6 +138,12 @@ public class EditPublishConfigurationMVCActionCommand
 			}
 			else if (cmd.equals(Constants.PUBLISH_TO_REMOTE)) {
 				StagingUtil.copyRemoteLayouts(exportImportConfigurationId);
+			}
+			else if (cmd.equals(Constants.MOVE_TO_TRASH)) {
+				deleteExportImportConfiguration(actionRequest, true);
+			}
+			else if (cmd.equals(Constants.RESTORE)) {
+				restoreTrashEntries(actionRequest);
 			}
 			else if (cmd.equals(Constants.RELAUNCH)) {
 				relaunchPublishLayoutConfiguration(
@@ -131,6 +191,17 @@ public class EditPublishConfigurationMVCActionCommand
 		}
 	}
 
+	protected void restoreTrashEntries(ActionRequest actionRequest)
+		throws Exception {
+
+		long[] restoreTrashEntryIds = StringUtil.split(
+			ParamUtil.getString(actionRequest, "restoreTrashEntryIds"), 0L);
+
+		for (long restoreTrashEntryId : restoreTrashEntryIds) {
+			_trashEntryService.restoreEntry(restoreTrashEntryId);
+		}
+	}
+
 	@Reference
 	protected void setExportImportConfigurationLocalService(
 		ExportImportConfigurationLocalService
@@ -138,6 +209,18 @@ public class EditPublishConfigurationMVCActionCommand
 
 		_exportImportConfigurationLocalService =
 			exportImportConfigurationLocalService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setExportImportConfigurationService(
+		ExportImportConfigurationService exportImportConfigurationService) {
+
+		_exportImportConfigurationService = exportImportConfigurationService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setTrashEntryService(TrashEntryService trashEntryService) {
+		_trashEntryService = trashEntryService;
 	}
 
 	protected void unsetExportImportConfigurationLocalService(
@@ -188,5 +271,8 @@ public class EditPublishConfigurationMVCActionCommand
 
 	private volatile ExportImportConfigurationLocalService
 		_exportImportConfigurationLocalService;
+	private volatile ExportImportConfigurationService
+		_exportImportConfigurationService;
+	private volatile TrashEntryService _trashEntryService;
 
 }
