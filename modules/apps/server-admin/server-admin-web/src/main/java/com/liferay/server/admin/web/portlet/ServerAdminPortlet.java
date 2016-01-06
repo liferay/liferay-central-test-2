4f1f507ead3b18e157291241c747bd95e8d23930
drewbrokke <drew.brokke@liferay.com>
2016-01-06 11:51:37 -0800

    LPS-61163 lexicon - adds css file

--- modules/apps/server-admin/server-admin-web/src/main/java/com/liferay/server/admin/web/portlet/ServerAdminPortlet.java
+++ modules/apps/server-admin/server-admin-web/src/main/java/com/liferay/server/admin/web/portlet/ServerAdminPortlet.java
@@ -30,6 +30,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.css-class-wrapper=portlet-admin",
 		"com.liferay.portlet.display-category=category.hidden",
 		"com.liferay.portlet.footer-portlet-javascript=/js/main.js",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
 		"com.liferay.portlet.private-session-attributes=false",
