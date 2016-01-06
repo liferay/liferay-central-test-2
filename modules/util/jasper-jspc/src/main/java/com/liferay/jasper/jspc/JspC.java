8a935a58456b6efe5012a5b59414598b671e0c64
shuyangzhou <shuyang.zhou@liferay.com>
2016-01-05 20:02:25 -0800

    LPS-61813 Reload JspC with correct classloader.

--- modules/util/jasper-jspc/src/main/java/com/liferay/jasper/jspc/JspC.java
+++ modules/util/jasper-jspc/src/main/java/com/liferay/jasper/jspc/JspC.java
@@ -19,6 +19,11 @@ import java.io.File;
 import java.io.InputStreamReader;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import java.net.URI;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 import java.util.Iterator;
 import java.util.List;
@@ -31,19 +36,26 @@ import org.apache.jasper.JasperException;
 public class JspC extends org.apache.jasper.JspC {
 
 	public static void main(String[] args) {
-		JspC jspc = new JspC();
-
 		try {
 			BufferedReader bufferedReader = new BufferedReader(
 				new InputStreamReader(System.in));
 
 			String classPath = bufferedReader.readLine();
 
-			jspc.setArgs(args);
-
 			if ((classPath != null) && !classPath.isEmpty()) {
-				jspc.setClassPath(classPath);
+				_runWithClassPath(classPath, args);
+
+				return;
 			}
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+		JspC jspc = new JspC();
+
+		try {
+			jspc.setArgs(args);
 
 			jspc.execute();
 		}
@@ -105,6 +117,51 @@ public class JspC extends org.apache.jasper.JspC {
 			if (page.contains("/docroot/META-INF/custom_jsps/")) {
 				iterator.remove();
 			}
+		}
+	}
+
+	private static void _runWithClassPath(String classpath, String[] args)
+		throws Exception {
+
+		classpath =
+			System.getProperty("java.class.path") + File.pathSeparator +
+				classpath;
+
+		String[] files = classpath.split(File.pathSeparator);
+
+		URL[] urls = new URL[files.length];
+
+		for (int i = 0; i < files.length; i++) {
+			File file = new File(files[i]);
+
+			URI uri = file.toURI();
+
+			urls[i] = uri.toURL();
+		}
+
+		ClassLoader classLoader = new URLClassLoader(urls, null);
+
+		Class<?> jspcClass = classLoader.loadClass(JspC.class.getName());
+
+		Object jspc = jspcClass.newInstance();
+
+		Method setArgsMethod = jspcClass.getMethod("setArgs", String[].class);
+
+		setArgsMethod.invoke(jspc, new Object[] {args});
+
+		Method executeMethod = jspcClass.getMethod("execute");
+
+		Thread currentThread = Thread.currentThread();
+
+		ClassLoader contextClassLoader = currentThread.getContextClassLoader();
+
+		currentThread.setContextClassLoader(classLoader);
+
+		try {
+			executeMethod.invoke(jspc);
+		}
+		finally {
+			currentThread.setContextClassLoader(contextClassLoader);
 		}
 	}
 
