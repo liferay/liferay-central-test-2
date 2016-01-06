b02c9245fd5878081483ad417e72297944895918
Brian Chan <brian.chan@liferay.com>
2016-01-06 14:02:28 -0800

    Revert "LPS-61748 SF"
    This reverts commit e0ff0a2aa7634ad5d27f36454b6ac78a0e502b17.
    
    Revert "LPS-61748 Ray, this is a bug right?"
    
    This reverts commit 2e95e8451745e46053f70fa6d0cd9d2ed92cfca2.
    
    Revert "LPS-61748 SF"
    
    This reverts commit 83ce4e10eef1217a6aec8f861eaeff5d9453df82.
    
    Revert "LPS-61748 SF"
    
    This reverts commit cf2ce3b2a4b83825fa8b9edebaefdc0f6a44a6bb.
    
    Revert "LPS-61748 log classpath and resources in info"
    
    This reverts commit 6fd2eaa2e6fa207e108538767f74331390bb7aa7.
    
    Revert "LPS-61748 add spring bnd support"
    
    This reverts commit 91aef582dba63ffd3523a1dea95da769a5291955.
    
    Revert "LPS-61748 use latest version of plugin"
    
    This reverts commit 74766155afd0b6f3cc0803e1fc39f646d1149989.

--- modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/extensions/LiferayOSGiExtension.java
+++ modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/extensions/LiferayOSGiExtension.java
@@ -16,8 +16,6 @@ package com.liferay.gradle.plugins.extensions;
 
 import aQute.bnd.osgi.Constants;
 
-import aQute.lib.spring.SpringComponent;
-
 import com.liferay.ant.bnd.bower.BowerAnalyzerPlugin;
 import com.liferay.ant.bnd.jsp.JspAnalyzerPlugin;
 import com.liferay.ant.bnd.sass.SassAnalyzerPlugin;
@@ -102,7 +100,7 @@ public class LiferayOSGiExtension extends LiferayExtension {
 
 	private static final String[] _BND_PLUGIN_CLASS_NAMES = {
 		BowerAnalyzerPlugin.class.getName(), JspAnalyzerPlugin.class.getName(),
-		SassAnalyzerPlugin.class.getName(), SpringComponent.class.getName(),
+		SassAnalyzerPlugin.class.getName(),
 		SpringDependencyAnalyzerPlugin.class.getName()
 	};
 
