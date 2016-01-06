de2d294f920c0e56d3b8ce33f05925c2112a1dfd
Michael Han <michael.han@liferay.com>
2016-01-06 06:21:15 -0600

    LPS-61776 add FacebookConnnectSettingsConstants

--- modules/portal-security/portal-security-sso-facebook-connect/src/main/java/com/liferay/portal/security/sso/facebook/connect/constants/FacebookConnectSettingsConstants.java
+++ modules/portal-security/portal-security-sso-facebook-connect/src/main/java/com/liferay/portal/security/sso/facebook/connect/constants/FacebookConnectSettingsConstants.java
@@ -15,25 +15,11 @@
 package com.liferay.portal.security.sso.facebook.connect.constants;
 
 /**
- * @author Mika Koivisto
+ * @author Michael C. Han
  */
-public class FacebookConnectConstants {
+public class FacebookConnectSettingsConstants {
 
-	public static final String APP_ID = "appId";
-
-	public static final String APP_SECRET = "appSecret";
-
-	public static final String AUTH_ENABLED = "enabled";
-
-	public static final String GRAPH_URL = "graphURL";
-
-	public static final String OAUTH_AUTH_URL = "oauthAuthURL";
-
-	public static final String OAUTH_REDIRECT_URL = "oauthRedirectURL";
-
-	public static final String OAUTH_TOKEN_URL = "oauthTokenURL";
-
-	public static final String VERIFIED_ACCOUNT_REQUIRED =
-		"verifiedAccountRequired";
+	public static final String SERVICE_NAME =
+		"com.liferay.portal.security.sso.facebook.connect";
 
 }
