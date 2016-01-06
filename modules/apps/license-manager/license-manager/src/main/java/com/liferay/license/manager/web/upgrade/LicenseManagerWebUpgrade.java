2990d297bcd3a2d051544482a902889beaa05612
peterfellwock <peter.fellwock@liferay.com>
2016-01-05 16:20:50 -0800

    LPS-61718 Apply upgrade pattern

---
+++ modules/apps/license-manager/license-manager/src/main/java/com/liferay/license/manager/web/upgrade/LicenseManagerWebUpgrade.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.license.manager.web.upgrade;
+
+import com.liferay.license.manager.web.upgrade.v1_0_0.UpgradePortletId;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Peter Fellwock
+ */
+@Component(immediate = true, service = UpgradeStepRegistrator.class)
+public class LicenseManagerWebUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"com.liferay.license.manager.web", "0.0.1", "1.0.0",
+			new UpgradePortletId());
+	}
+
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
+	protected void setModuleServiceLifecycle(
+		ModuleServiceLifecycle moduleServiceLifecycle) {
+	}
+
+}
