584c023bb596e9268e683345493a870cdd68e5a6
Brian Chan <brian.chan@liferay.com>
2016-01-05 20:23:04 -0800

    LPS-61340 SF

--- portal-service/src/com/liferay/portal/kernel/sanitizer/SanitizerUtil.java
+++ portal-service/src/com/liferay/portal/kernel/sanitizer/SanitizerUtil.java
@@ -34,7 +34,7 @@ import java.util.Map;
 public class SanitizerUtil {
 
 	/**
-	 * @deprecated As of 7.0.0 with no direct replacement
+	 * @deprecated As of 7.0.0, with no direct replacement
 	 */
 	@Deprecated
 	public static Sanitizer getSanitizer() {
