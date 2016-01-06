f9a1a2df28d299ae5c1519ef00e5cbaf86e1327f
Dante Wang <dante.wang@liferay.com>
2016-01-06 15:30:04 +0800

    LPS-61495 Auto SF

--- modules/frontend/frontend-editor/frontend-editor-tinymce-web/src/main/java/com/liferay/frontend/editor/tinymce/web/editor/configuration/BaseTinyMCEEditorConfigConfigurator.java
+++ modules/frontend/frontend-editor/frontend-editor-tinymce-web/src/main/java/com/liferay/frontend/editor/tinymce/web/editor/configuration/BaseTinyMCEEditorConfigConfigurator.java
@@ -19,7 +19,6 @@ import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portlet.RequestBackedPortletURLFactory;
