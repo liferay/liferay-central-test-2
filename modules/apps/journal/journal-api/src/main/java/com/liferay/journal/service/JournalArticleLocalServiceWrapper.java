cae83b53cf60a72b8815633ab6d03244e6ac79c2
yxingwu <yuxing.wu@liferay.com>
2016-01-07 09:56:30 +0800

    LPS-60861 Generate

--- modules/apps/journal/journal-api/src/main/java/com/liferay/journal/service/JournalArticleLocalServiceWrapper.java
+++ modules/apps/journal/journal-api/src/main/java/com/liferay/journal/service/JournalArticleLocalServiceWrapper.java
@@ -3672,23 +3672,6 @@ public class JournalArticleLocalServiceWrapper
 	}
 
 	/**
-	* @deprecated As of 6.2.0, replaced by {@link
-	#updateArticleTranslation(long, String, double, Locale,
-	String, String, String, Map, ServiceContext)}
-	*/
-	@Deprecated
-	@Override
-	public com.liferay.journal.model.JournalArticle updateArticleTranslation(
-		long groupId, java.lang.String articleId, double version,
-		java.util.Locale locale, java.lang.String title,
-		java.lang.String description, java.lang.String content,
-		java.util.Map<java.lang.String, byte[]> images)
-		throws com.liferay.portal.kernel.exception.PortalException {
-		return _journalArticleLocalService.updateArticleTranslation(groupId,
-			articleId, version, locale, title, description, content, images);
-	}
-
-	/**
 	* Updates the translation of the web content article.
 	*
 	* @param groupId the primary key of the web content article's group
--- modules/apps/journal/journal-api/src/main/java/com/liferay/journal/service/JournalArticleService.java
+++ modules/apps/journal/journal-api/src/main/java/com/liferay/journal/service/JournalArticleService.java
@@ -1720,19 +1720,6 @@ public interface JournalArticleService extends BaseService {
 		throws PortalException;
 
 	/**
-	* @deprecated As of 6.2.0, replaced by {@link
-	#updateArticleTranslation(long, String, double, Locale,
-	String, String, String, Map, ServiceContext)}
-	*/
-	@java.lang.Deprecated
-	public com.liferay.journal.model.JournalArticle updateArticleTranslation(
-		long groupId, java.lang.String articleId, double version,
-		java.util.Locale locale, java.lang.String title,
-		java.lang.String description, java.lang.String content,
-		java.util.Map<java.lang.String, byte[]> images)
-		throws PortalException;
-
-	/**
 	* Updates the translation of the web content article.
 	*
 	* @param groupId the primary key of the web content article's group
