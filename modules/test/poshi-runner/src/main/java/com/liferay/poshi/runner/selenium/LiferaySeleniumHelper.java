6df321ba2658987d700ecf3351cab97153ae3ccc
Kenji Heigel <kenji.heigel@liferay.com>
2016-01-07 13:04:41 -0800

    LRQA-19650 Add a helper method to evaluate the current test name

--- modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/LiferaySeleniumHelper.java
+++ modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/LiferaySeleniumHelper.java
@@ -14,6 +14,7 @@
 
 package com.liferay.poshi.runner.selenium;
 
+import com.liferay.poshi.runner.PoshiRunnerContext;
 import com.liferay.poshi.runner.PoshiRunnerGetterUtil;
 import com.liferay.poshi.runner.exception.PoshiRunnerWarningException;
 import com.liferay.poshi.runner.util.AntCommands;
@@ -1062,6 +1063,14 @@ public class LiferaySeleniumHelper {
 
 	public static boolean isTCatEnabled() {
 		return PropsValues.TCAT_ENABLED;
+	}
+
+	public static boolean isTestName(String testName) {
+		if (testName.equals(PoshiRunnerContext.getTestCaseCommandName())) {
+			return true;
+		}
+
+		return false;
 	}
 
 	public static boolean isTextNotPresent(
