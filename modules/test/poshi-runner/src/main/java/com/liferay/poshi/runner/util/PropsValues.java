2512736d40c403dcbbbb994833e8e858cc67f484
Leslie Wong <leslie.wong@liferay.com>
2016-01-06 16:21:00 -0800

    LRQA-19896 Add property to pause on failure

--- modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/util/PropsValues.java
+++ modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/util/PropsValues.java
@@ -214,6 +214,9 @@ public class PropsValues {
 	public static final String[] TEST_INCLUDE_DIR_NAMES = StringUtil.split(
 		PropsUtil.get("test.include.dir.names"));
 
+	public static final boolean TEST_PAUSE_ON_FAILURE = GetterUtil.getBoolean(
+		PropsUtil.get("test.pause.on.failure"));
+
 	public static final boolean TEST_PORTAL_INSTANCE = GetterUtil.getBoolean(
 		PropsUtil.get("test.portal.instance"));
 
