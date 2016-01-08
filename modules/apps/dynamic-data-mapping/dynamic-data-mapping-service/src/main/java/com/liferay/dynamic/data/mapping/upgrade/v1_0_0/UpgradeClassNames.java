8313cd5c12b3cee82c479d553bff9ef738963981
Marcellus Tavares <marcellus.tavares@liferay.com>
2016-01-08 19:24:47 -0300

    LPS-51214 Fix old model qualified name

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/upgrade/v1_0_0/UpgradeClassNames.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/upgrade/v1_0_0/UpgradeClassNames.java
@@ -59,7 +59,7 @@ public class UpgradeClassNames extends UpgradeProcess {
 
 		ClassNameDependencyUpgrader classNameDependencyUpgrader =
 			new ClassNameDependencyUpgrader(
-				"com.liferay.portlet.dynamicdatamapping.DDMContent",
+				"com.liferay.portlet.dynamicdatamapping.model.DDMContent",
 				DDMContent.class.getName(), classNameDependencies);
 
 		classNameDependencyUpgrader.upgrade();
@@ -70,7 +70,7 @@ public class UpgradeClassNames extends UpgradeProcess {
 
 		ClassNameDependencyUpgrader classNameDependencyUpgrader =
 			new ClassNameDependencyUpgrader(
-				"com.liferay.portlet.dynamicdatamapping.DDMStructure",
+				"com.liferay.portlet.dynamicdatamapping.model.DDMStructure",
 				DDMStructure.class.getName(), classNameDependencies);
 
 		classNameDependencyUpgrader.upgrade();
@@ -81,7 +81,7 @@ public class UpgradeClassNames extends UpgradeProcess {
 
 		ClassNameDependencyUpgrader classNameDependencyUpgrader =
 			new ClassNameDependencyUpgrader(
-				"com.liferay.portlet.dynamicdatamapping.DDMTemplate",
+				"com.liferay.portlet.dynamicdatamapping.model.DDMTemplate",
 				DDMTemplate.class.getName(), classNameDependencies);
 
 		classNameDependencyUpgrader.upgrade();
