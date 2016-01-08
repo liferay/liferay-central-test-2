15a760b9842eb2c526db2ae6cede8dc73977bf74
Julio Camarero <julio.camarero@liferay.com>
2016-01-08 12:13:28 +0100

    LPS-61501 move css to the taglib

--- modules/apps/control-menu/control-menu-web/src/main/java/com/liferay/control/menu/web/portlet/ControlMenuPortlet.java
+++ modules/apps/control-menu/control-menu-web/src/main/java/com/liferay/control/menu/web/portlet/ControlMenuPortlet.java
@@ -54,7 +54,6 @@ import org.osgi.service.component.annotations.Reference;
 	property = {
 		"com.liferay.portlet.css-class-wrapper=portlet-control-menu",
 		"com.liferay.portlet.display-category=category.hidden",
-		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.instanceable=false",
 		"com.liferay.portlet.use-default-template=false",
 		"javax.portlet.init-param.view-template=/view.jsp",
