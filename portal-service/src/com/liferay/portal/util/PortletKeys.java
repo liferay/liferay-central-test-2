947bedf36b6ec4c24099a11627089f596b195744
Julio Camarero <julio.camarero@liferay.com>
2016-01-09 11:44:09 +0100

    LPS-61501 remove constant not used anymore

--- portal-service/src/com/liferay/portal/util/PortletKeys.java
+++ portal-service/src/com/liferay/portal/util/PortletKeys.java
@@ -150,9 +150,6 @@ public class PortletKeys {
 	public static final String SITE_MEMBERS_DIRECTORY =
 		"com_liferay_directory_web_portlet_SiteMembersDirectoryPortlet";
 
-	public static final String STAGING_BAR =
-		"com_liferay_staging_bar_web_portlet_StagingBarPortlet";
-
 	public static final String STOCKS = "12";
 
 	public static final String TRANSLATOR = "26";
