359b16913d69726dae41ae1c0bfad2951d0eee17
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2016-01-11 17:26:55 +0100

    LPS-61927 ExtendedProperties.save() exports only String values, Object values are missing in FastExtendedProperties after configuration is rewritten using reflection in LiferayResourceManager.initialize()

--- modules/portal/portal-template-velocity/src/main/java/com/liferay/portal/template/velocity/FastExtendedProperties.java
+++ modules/portal/portal-template-velocity/src/main/java/com/liferay/portal/template/velocity/FastExtendedProperties.java
@@ -14,12 +14,7 @@
 
 package com.liferay.portal.template.velocity;
 
-import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
-import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayOutputStream;
-import com.liferay.portal.kernel.util.StringPool;
-
 import java.io.IOException;
-import java.io.InputStream;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -41,16 +36,14 @@ public class FastExtendedProperties extends ExtendedProperties {
 	public FastExtendedProperties(ExtendedProperties extendedProperties)
 		throws IOException {
 
-		UnsyncByteArrayOutputStream unsyncByteArrayOutputStream =
-			new UnsyncByteArrayOutputStream();
+		Enumeration keys = extendedProperties.keys();
 
-		extendedProperties.save(unsyncByteArrayOutputStream, StringPool.BLANK);
+		while (keys.hasMoreElements()) {
+			String key = (String)keys.nextElement();
+			Object value = extendedProperties.get(key);
 
-		InputStream inputStream = new UnsyncByteArrayInputStream(
-			unsyncByteArrayOutputStream.unsafeGetByteArray(), 0,
-			unsyncByteArrayOutputStream.size());
-
-		load(inputStream);
+			addProperty(key, value);
+		}
 	}
 
 	@Override
