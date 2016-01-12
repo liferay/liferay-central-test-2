bbbd4690a60bbd412e2d7bef03e36ea0e1d3fba5
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2016-01-12 11:41:00 +0000

    LPS-61962 Change FacebookConnectAction Struts base path from "/facebook_connect" to "/portal"

--- modules/portal-security/portal-security-sso-facebook-connect/src/main/java/com/liferay/portal/security/sso/facebook/connect/internal/web/portlet/path/AuthPublicPath.java
+++ modules/portal-security/portal-security-sso-facebook-connect/src/main/java/com/liferay/portal/security/sso/facebook/connect/internal/web/portlet/path/AuthPublicPath.java
@@ -21,7 +21,7 @@ import org.osgi.service.component.annotations.Component;
  */
 @Component(
 	immediate = true,
-	property = {"auth.public.path=/facebook_connect/facebook_connect_oauth"},
+	property = {"auth.public.path=/portal/facebook_connect_oauth"},
 	service = Object.class
 )
 public class AuthPublicPath {
