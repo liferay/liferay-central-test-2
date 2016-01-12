587e5505be3306ffbedc1c0b206080ae0930925d
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2016-01-12 12:12:47 +0100

    LPS-61970 Create portlet configuration icons that will be displayed when rendering a thread and it will allow to subscribe or unsubscribe to a thread

---
+++ modules/apps/message-boards/message-boards-web/src/main/java/com/liferay/message/boards/web/portlet/configuration/icon/ThreadSubscriptionPortletConfigurationIconFactory.java
@@ -0,0 +1,110 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.message.boards.web.portlet.configuration.icon;
+
+import com.liferay.message.boards.web.constants.MBPortletKeys;
+import com.liferay.message.boards.web.portlet.action.ActionUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIconFactory;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIconFactory;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.service.SubscriptionLocalService;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.messageboards.MBGroupServiceSettings;
+import com.liferay.portlet.messageboards.model.MBMessage;
+import com.liferay.portlet.messageboards.model.MBMessageDisplay;
+import com.liferay.portlet.messageboards.model.MBThread;
+import com.liferay.portlet.messageboards.service.permission.MBMessagePermission;
+
+import javax.portlet.PortletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Sergio González
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + MBPortletKeys.MESSAGE_BOARDS_ADMIN,
+		"path=/message_boards/view_message"
+	},
+	service = PortletConfigurationIconFactory.class
+)
+public class ThreadSubscriptionPortletConfigurationIconFactory
+	extends BasePortletConfigurationIconFactory {
+
+	@Override
+	public PortletConfigurationIcon create(PortletRequest portletRequest) {
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		try {
+			MBGroupServiceSettings mbGroupServiceSettings =
+				MBGroupServiceSettings.getInstance(
+					themeDisplay.getScopeGroupId());
+
+			if (!mbGroupServiceSettings.isEmailMessageAddedEnabled() &&
+				!mbGroupServiceSettings.isEmailMessageUpdatedEnabled()) {
+
+				return null;
+			}
+
+			MBMessageDisplay messageDisplay = ActionUtil.getMessageDisplay(
+				portletRequest);
+
+			MBMessage message = messageDisplay.getMessage();
+
+			if (MBMessagePermission.contains(
+					themeDisplay.getPermissionChecker(), message,
+					ActionKeys.SUBSCRIBE)) {
+
+				if (_subscriptionLocalService.isSubscribed(
+						themeDisplay.getCompanyId(), themeDisplay.getUserId(),
+						MBThread.class.getName(), message.getThreadId())) {
+
+					return new UnsubscribeThreadPortletConfigurationIcon(
+						portletRequest, message);
+				}
+				else {
+					return new SubscribeThreadPortletConfigurationIcon(
+						portletRequest, message);
+				}
+			}
+		}
+		catch (PortalException pe) {
+		}
+
+		return null;
+	}
+
+	@Override
+	public double getWeight() {
+		return 101;
+	}
+
+	@Reference(unbind = "-")
+	protected void setSubscriptionLocalService(
+		SubscriptionLocalService subscriptionLocalService) {
+
+		_subscriptionLocalService = subscriptionLocalService;
+	}
+
+	private SubscriptionLocalService _subscriptionLocalService;
+
+}
