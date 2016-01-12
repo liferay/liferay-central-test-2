322edebd95ab86dee695d841813a10a34f30aa29
danielkocsis <daniel.kocsis@liferay.com>
2016-01-12 17:38:26 +0100

    LPS-61222 Wire the new finders to the local service

--- portal-impl/src/com/liferay/portlet/exportimport/service/impl/ExportImportConfigurationLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/exportimport/service/impl/ExportImportConfigurationLocalServiceImpl.java
@@ -240,6 +240,31 @@ public class ExportImportConfigurationLocalServiceImpl
 	}
 
 	@Override
+	public List<ExportImportConfiguration> getExportImportConfigurations(
+			long companyId, long groupId, String keywords, int type, int start,
+			int end,
+			OrderByComparator<ExportImportConfiguration> orderByComparator)
+		throws PortalException {
+
+		return exportImportConfigurationFinder.findByKeywords(
+			companyId, groupId, keywords, type,
+			WorkflowConstants.STATUS_APPROVED, start, end, orderByComparator);
+	}
+
+	@Override
+	public List<ExportImportConfiguration> getExportImportConfigurations(
+			long companyId, long groupId, String name, String description,
+			int type, boolean andSearch, int start, int end,
+			OrderByComparator<ExportImportConfiguration> orderByComparator)
+		throws PortalException {
+
+		return exportImportConfigurationFinder.findByC_G_N_D_T(
+			companyId, groupId, name, description, type,
+			WorkflowConstants.STATUS_APPROVED, andSearch, start, end,
+			orderByComparator);
+	}
+
+	@Override
 	public int getExportImportConfigurationsCount(long groupId) {
 		return exportImportConfigurationPersistence.countByG_S(
 			groupId, WorkflowConstants.STATUS_APPROVED);
@@ -249,6 +274,27 @@ public class ExportImportConfigurationLocalServiceImpl
 	public int getExportImportConfigurationsCount(long groupId, int type) {
 		return exportImportConfigurationPersistence.countByG_T_S(
 			groupId, type, WorkflowConstants.STATUS_APPROVED);
+	}
+
+	@Override
+	public int getExportImportConfigurationsCount(
+			long companyId, long groupId, String keywords, int type)
+		throws PortalException {
+
+		return exportImportConfigurationFinder.countByKeywords(
+			companyId, groupId, keywords, type,
+			WorkflowConstants.STATUS_APPROVED);
+	}
+
+	@Override
+	public int getExportImportConfigurationsCount(
+			long companyId, long groupId, String name, String description,
+			int type, boolean andSearch)
+		throws PortalException {
+
+		return exportImportConfigurationFinder.countByC_G_N_D_T(
+			companyId, groupId, name, description, type,
+			WorkflowConstants.STATUS_APPROVED, andSearch);
 	}
 
 	@Indexable(type = IndexableType.REINDEX)
