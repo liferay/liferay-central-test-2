cd2b65f2747b5c7e01814336e47e4dfc03231756
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-01-13 14:46:52 -0800

    LPS-56872 Sort

--- modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/extensions/AppServerFactory.java
+++ modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/extensions/AppServerFactory.java
@@ -28,11 +28,11 @@ class AppServerFactory implements NamedDomainObjectFactory<AppServer> {
 
 	@Override
 	public AppServer create(String name) {
-		if (name.equals("tomcat")) {
-			return new TomcatAppServer(_project);
-		}
-		else if (name.equals("jonas")) {
+		if (name.equals("jonas")) {
 			return new JOnASAppServer(_project);
+		}
+		else if (name.equals("tomcat")) {
+			return new TomcatAppServer(_project);
 		}
 		else {
 			return new AppServer(name, _project);
