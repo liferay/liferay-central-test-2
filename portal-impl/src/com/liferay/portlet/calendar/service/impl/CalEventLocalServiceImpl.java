53bb1bfe000948485a7b517a2b7a53a6630736fd
yxingwu <yuxing.wu@liferay.com>
2016-01-13 16:46:07 +0800

    LPS-60861 Remove the deprecated methods in CalEvent

--- portal-impl/src/com/liferay/portlet/calendar/service/impl/CalEventLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/calendar/service/impl/CalEventLocalServiceImpl.java
@@ -247,34 +247,6 @@ public class CalEventLocalServiceImpl extends CalEventLocalServiceBaseImpl {
 		return event;
 	}
 
-	/**
-	 * @deprecated As of 6.2.0, replaced by {@link #addEvent(long, String,
-	 *             String, String, int, int, int, int, int, int, int, boolean,
-	 *             boolean, String, boolean, TZSRecurrence, int, int, int,
-	 *             ServiceContext)}
-	 */
-	@Deprecated
-	@Indexable(type = IndexableType.REINDEX)
-	@Override
-	public CalEvent addEvent(
-			long userId, String title, String description, String location,
-			int startDateMonth, int startDateDay, int startDateYear,
-			int startDateHour, int startDateMinute, int endDateMonth,
-			int endDateDay, int endDateYear, int durationHour,
-			int durationMinute, boolean allDay, boolean timeZoneSensitive,
-			String type, boolean repeating, TZSRecurrence recurrence,
-			int remindBy, int firstReminder, int secondReminder,
-			ServiceContext serviceContext)
-		throws PortalException {
-
-		return addEvent(
-			userId, title, description, location, startDateMonth, startDateDay,
-			startDateYear, startDateHour, startDateMinute, durationHour,
-			durationMinute, allDay, timeZoneSensitive, type, repeating,
-			recurrence, remindBy, firstReminder, secondReminder,
-			serviceContext);
-	}
-
 	@Override
 	public void addEventResources(
 			CalEvent event, boolean addGroupPermissions,
@@ -820,34 +792,6 @@ public class CalEventLocalServiceImpl extends CalEventLocalServiceBaseImpl {
 		CalEventLocalUtil.clearEventsPool(event.getGroupId());
 
 		return event;
-	}
-
-	/**
-	 * @deprecated As of 6.2.0, replaced by {@link #updateEvent(long, long,
-	 *             String, String, String, int, int, int, int, int, int, int,
-	 *             boolean, boolean, String, boolean, TZSRecurrence, int, int,
-	 *             int, ServiceContext)}
-	 */
-	@Deprecated
-	@Indexable(type = IndexableType.REINDEX)
-	@Override
-	public CalEvent updateEvent(
-			long userId, long eventId, String title, String description,
-			String location, int startDateMonth, int startDateDay,
-			int startDateYear, int startDateHour, int startDateMinute,
-			int endDateMonth, int endDateDay, int endDateYear, int durationHour,
-			int durationMinute, boolean allDay, boolean timeZoneSensitive,
-			String type, boolean repeating, TZSRecurrence recurrence,
-			int remindBy, int firstReminder, int secondReminder,
-			ServiceContext serviceContext)
-		throws PortalException {
-
-		return updateEvent(
-			userId, eventId, title, description, location, startDateMonth,
-			startDateDay, startDateYear, startDateHour, startDateMinute,
-			durationHour, durationMinute, allDay, timeZoneSensitive, type,
-			repeating, recurrence, remindBy, firstReminder, secondReminder,
-			serviceContext);
 	}
 
 	protected File exportICal4j(
