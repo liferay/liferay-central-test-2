b69b760a81b0ef708e6ccc2b58493446816bfb90
Cristina González Castellano <cristina.gonzalez@liferay.com>
2016-01-13 12:19:39 +0100

    LPS-51833 Same pattern for UsersAdmin

--- portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradePortletId.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradePortletId.java
@@ -49,7 +49,8 @@ public class UpgradePortletId
 			new String[] {"88", PortletKeys.LAYOUTS_ADMIN},
 			new String[] {"140", PortletKeys.MY_PAGES},
 			new String[] {"156", PortletKeys.GROUP_PAGES},
-			new String[] {"127", PortletKeys.USER_GROUPS_ADMIN}
+			new String[] {"127", PortletKeys.USER_GROUPS_ADMIN},
+			new String[] {"125", PortletKeys.USERS_ADMIN}
 		};
 	}
 
