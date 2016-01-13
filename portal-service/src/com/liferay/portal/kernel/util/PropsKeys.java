48f513054ac57498af11a0bc9e1a6035e34fa90a
Michael Han <michael.han@liferay.com>
2016-01-13 13:50:01 -0600

    LPS-61763 remove SEARCH_PERMISSION_FILTER_ENABLED, will cause security hole

--- portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -1869,8 +1869,6 @@ public interface PropsKeys {
 
 	public static final String SEARCH_CONTAINER_SHOW_PAGINATION_TOP_DELTA = "search.container.show.pagination.top.delta";
 
-	public static final String SEARCH_PERMISSION_FILTER_ENABLED = "search.permission.filter.enabled";
-
 	public static final String SEARCH_USE_DEEP_SEARCH = "search.use.deep.search";
 
 	public static final String SERVLET_SERVICE_EVENTS_POST = "servlet.service.events.post";
