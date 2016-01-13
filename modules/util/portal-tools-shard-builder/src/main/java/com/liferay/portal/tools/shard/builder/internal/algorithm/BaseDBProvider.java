9aa5c03cc9a5155561b452cb8fc195cfdb92b00b
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2016-01-13 12:04:03 +0100

    LPS-59594 Use autocloseable and we can remove the utility class used to close the connections

--- modules/util/portal-tools-shard-builder/src/main/java/com/liferay/portal/tools/shard/builder/internal/algorithm/BaseDBProvider.java
+++ modules/util/portal-tools-shard-builder/src/main/java/com/liferay/portal/tools/shard/builder/internal/algorithm/BaseDBProvider.java
@@ -17,7 +17,6 @@ package com.liferay.portal.tools.shard.builder.internal.algorithm;
 import com.liferay.portal.tools.shard.builder.exporter.ShardExporter;
 import com.liferay.portal.tools.shard.builder.exporter.context.ExportContext;
 import com.liferay.portal.tools.shard.builder.internal.DBProvider;
-import com.liferay.portal.tools.shard.builder.internal.util.DBManager;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
@@ -179,6 +178,22 @@ public abstract class BaseDBProvider
 
 	protected final Properties dbProperties;
 
+	private PreparedStatement _buildPreparedStatement(
+			Connection connection, String sql, long companyId)
+		throws SQLException {
+
+		PreparedStatement ps = connection.prepareStatement(
+			sql, ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+
+		ps.setFetchSize(getFetchSize());
+
+		if (companyId > 0) {
+			ps.setLong(1, companyId);
+		}
+
+		return ps;
+	}
+
 	private List<String> _getSchemaTableNames(String sql) {
 		List<String> tableNames = new ArrayList<>();
 
@@ -213,25 +228,11 @@ public abstract class BaseDBProvider
 			sql += " WHERE companyId = ?";
 		}
 
-		Connection con = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
+		DataSource dataSource = getDataSource();
 
-		try {
-			DataSource dataSource = getDataSource();
-
-			con = dataSource.getConnection();
-
-			ps = con.prepareStatement(
-				sql, ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-
-			ps.setFetchSize(getFetchSize());
-
-			if (companyId > 0) {
-				ps.setLong(1, companyId);
-			}
-
-			rs = ps.executeQuery();
+		try (Connection con = dataSource.getConnection();
+			PreparedStatement ps = _buildPreparedStatement(con, sql, companyId);
+			ResultSet rs = ps.executeQuery() ) {
 
 			ResultSetMetaData metaData = rs.getMetaData();
 
@@ -253,15 +254,11 @@ public abstract class BaseDBProvider
 					"Error exporting the rows for table " + tableName, e);
 			}
 		}
-		finally {
-			_dbManager.cleanUp(con, ps, rs);
-		}
 	}
 
 	private static final Logger _logger = LoggerFactory.getLogger(
 		BaseDBProvider.class);
 
 	private final DataSource _dataSource;
-	private final DBManager _dbManager = new DBManager();
 
 }
