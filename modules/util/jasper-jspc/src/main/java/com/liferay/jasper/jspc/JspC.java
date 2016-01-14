liferay/liferay-central-test-2

commit 7d20e367fcea7e36d92cf054cf2430032d6732b3

LPS-61813 SF

modules/util/jasper-jspc/src/main/java/com/liferay/jasper/jspc/JspC.java
@@ -59,8 +59,8 @@ public class JspC extends org.apache.jasper.JspC {
 
 			jspc.execute();
 		}
-		catch (Exception e) {
-			e.printStackTrace();
+		catch (Exception e1) {
+			e1.printStackTrace();
 
 			try {
 				Field noDieLevelField =

commit 0200bc255f487f324a8de55a84a0a45e352cbfcf

LPS-60355 Fix compilation error caused by 7d20e367fcea7e36d92cf054cf2430032d6732b3

modules/util/jasper-jspc/src/main/java/com/liferay/jasper/jspc/JspC.java
@@ -76,7 +76,7 @@ public class JspC extends org.apache.jasper.JspC {
 				}
 			}
 			catch (Exception e2) {
-				e2.addSuppressed(e);
+				e2.addSuppressed(e1);
 
 				throw new RuntimeException(e2);
 			}