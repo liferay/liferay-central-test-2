5b34c13c95f0e9d0bcd383f388089db889d90994
Juergen Kappler <jurgen.kappler@liferay.com>
2016-01-14 14:51:29 +0100

    LPS-62071 Remove deprecated methods for search container results tag

--- util-taglib/src/com/liferay/taglib/ui/SearchContainerResultsTag.java
+++ util-taglib/src/com/liferay/taglib/ui/SearchContainerResultsTag.java
@@ -30,22 +30,6 @@ import javax.servlet.jsp.tagext.TagSupport;
  */
 public class SearchContainerResultsTag<R> extends TagSupport {
 
-	/**
-	 * @deprecated As of 6.2.0, replaced by {@link
-	 *             SearchContainer#DEFAULT_RESULTS_VAR}.
-	 */
-	@Deprecated
-	public static final String DEFAULT_RESULTS_VAR =
-		SearchContainer.DEFAULT_RESULTS_VAR;
-
-	/**
-	 * @deprecated As of 6.2.0, replaced by {@link
-	 *             SearchContainer#DEFAULT_DEPRECATED_TOTAL_VAR}.
-	 */
-	@Deprecated
-	public static final String DEFAULT_TOTAL_VAR =
-		SearchContainer.DEFAULT_DEPRECATED_TOTAL_VAR;
-
 	@Override
 	public int doEndTag() throws JspException {
 		try {
@@ -56,26 +40,10 @@ public class SearchContainerResultsTag<R> extends TagSupport {
 			SearchContainer<R> searchContainer =
 				searchContainerTag.getSearchContainer();
 
-			int total = searchContainer.getTotal();
-
-			if (_deprecatedTotal == 0) {
-				_deprecatedTotal = total;
-			}
-
 			String totalVar = searchContainer.getTotalVar();
 
-			if (!_deprecatedTotalVar.equals(
-					SearchContainer.DEFAULT_DEPRECATED_TOTAL_VAR) &&
-				totalVar.equals(SearchContainer.DEFAULT_TOTAL_VAR)) {
-
+			if (totalVar.equals(SearchContainer.DEFAULT_TOTAL_VAR)) {
 				pageContext.removeAttribute(totalVar);
-
-				searchContainer.setTotalVar(_deprecatedTotalVar);
-			}
-			else {
-				pageContext.removeAttribute(_deprecatedTotalVar);
-
-				_deprecatedTotalVar = totalVar;
 			}
 
 			if (_results == null) {
@@ -84,22 +52,9 @@ public class SearchContainerResultsTag<R> extends TagSupport {
 				if (_results.isEmpty()) {
 					_results = (List<R>)pageContext.getAttribute(_resultsVar);
 				}
-
-				_deprecatedTotal = (Integer)pageContext.getAttribute(
-					_deprecatedTotalVar);
-			}
-
-			if (_results != null) {
-				if (_deprecatedTotal < _results.size()) {
-					_deprecatedTotal = _results.size();
-				}
 			}
 
 			searchContainer.setResults(_results);
-
-			if (total == 0) {
-				searchContainer.setTotal(_deprecatedTotal);
-			}
 
 			pageContext.setAttribute(_resultsVar, _results);
 
@@ -110,9 +65,6 @@ public class SearchContainerResultsTag<R> extends TagSupport {
 		}
 		finally {
 			if (!ServerDetector.isResin()) {
-				_deprecatedTotal = 0;
-				_deprecatedTotalVar =
-					SearchContainer.DEFAULT_DEPRECATED_TOTAL_VAR;
 				_results = null;
 				_resultsVar = SearchContainer.DEFAULT_RESULTS_VAR;
 			}
@@ -130,7 +82,6 @@ public class SearchContainerResultsTag<R> extends TagSupport {
 		}
 
 		if (_results == null) {
-			pageContext.setAttribute(_deprecatedTotalVar, 0);
 			pageContext.setAttribute(_resultsVar, new ArrayList<R>());
 		}
 
@@ -145,24 +96,6 @@ public class SearchContainerResultsTag<R> extends TagSupport {
 		return _resultsVar;
 	}
 
-	/**
-	 * @deprecated As of 6.2.0, replaced by {@link
-	 *             SearchContainerTag#getTotal()}.
-	 */
-	@Deprecated
-	public int getTotal() {
-		return _deprecatedTotal;
-	}
-
-	/**
-	 * @deprecated As of 6.2.0, replaced by {@link
-	 *             SearchContainerTag#getTotalVar()}.
-	 */
-	@Deprecated
-	public String getTotalVar() {
-		return _deprecatedTotalVar;
-	}
-
 	public void setResults(List<R> results) {
 		_results = results;
 	}
@@ -171,27 +104,6 @@ public class SearchContainerResultsTag<R> extends TagSupport {
 		_resultsVar = resultsVar;
 	}
 
-	/**
-	 * @deprecated As of 6.2.0, replaced by {@link
-	 *             SearchContainerTag#setTotal(int)}.
-	 */
-	@Deprecated
-	public void setTotal(int deprecatedTotal) {
-		_deprecatedTotal = deprecatedTotal;
-	}
-
-	/**
-	 * @deprecated As of 6.2.0, replaced by {@link
-	 *             SearchContainerTag#setTotalVar(String)}.
-	 */
-	@Deprecated
-	public void setTotalVar(String deprecatedTotalVar) {
-		_deprecatedTotalVar = deprecatedTotalVar;
-	}
-
-	private int _deprecatedTotal;
-	private String _deprecatedTotalVar =
-		SearchContainer.DEFAULT_DEPRECATED_TOTAL_VAR;
 	private List<R> _results;
 	private String _resultsVar = SearchContainer.DEFAULT_RESULTS_VAR;
 
