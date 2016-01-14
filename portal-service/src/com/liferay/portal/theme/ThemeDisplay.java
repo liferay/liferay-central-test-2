08be9c980bf27f2af20fdc0f524849945110f828
tomwang2011 <tong.wang@liferay.com>
2016-01-14 14:45:17 -0800

    LPS-61952 Update java file usages

--- portal-service/src/com/liferay/portal/theme/ThemeDisplay.java
+++ portal-service/src/com/liferay/portal/theme/ThemeDisplay.java
@@ -17,6 +17,7 @@ package com.liferay.portal.theme;
 import aQute.bnd.annotation.ProviderType;
 
 import com.liferay.admin.kernel.util.PortalMyAccountApplicationType;
+import com.liferay.mobile.device.rules.kernel.MDRRuleGroupInstance;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSON;
 import com.liferay.portal.kernel.language.LanguageUtil;
@@ -49,7 +50,6 @@ import com.liferay.portal.service.LayoutLocalServiceUtil;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.PortletKeys;
 import com.liferay.portlet.exportimport.staging.StagingUtil;
-import com.liferay.portlet.mobiledevicerules.MDRRuleGroupInstance;
 
 import java.io.Serializable;
 
