5b63ca0db3c319904ef0b16fade251f8cd74f469
Michael Han <michael.han@liferay.com>
2016-01-18 10:49:41 -0600

    LPS-59429 FORMS should be flagged as a system group

--- portal-service/src/com/liferay/portal/model/GroupConstants.java
+++ portal-service/src/com/liferay/portal/model/GroupConstants.java
@@ -46,7 +46,7 @@ public class GroupConstants {
 	public static final int MEMBERSHIP_RESTRICTION_TO_PARENT_SITE_MEMBERS = 1;
 
 	public static final String[] SYSTEM_GROUPS = {
-		CONTROL_PANEL, GUEST, GroupConstants.USER_PERSONAL_SITE
+		CONTROL_PANEL, FORMS, GUEST, GroupConstants.USER_PERSONAL_SITE
 	};
 
 	public static final String TYPE_SETTINGS_KEY_INHERIT_LOCALES =
