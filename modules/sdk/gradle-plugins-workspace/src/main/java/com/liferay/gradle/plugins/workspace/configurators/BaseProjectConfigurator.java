23bdd5e5f08f54e4b9bc00c68d58e44b301c9b25
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-01-18 12:22:10 -0800

    LPS-61767 Interface and base impl of the different project configurators

---
+++ modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/configurators/BaseProjectConfigurator.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.gradle.plugins.workspace.configurators;
+
+import com.liferay.gradle.plugins.workspace.WorkspacePlugin;
+import com.liferay.gradle.plugins.workspace.util.GradleUtil;
+
+import java.io.File;
+
+import org.gradle.api.GradleException;
+import org.gradle.api.initialization.Settings;
+
+/**
+ * @author Andrea Di Giorgi
+ */
+public abstract class BaseProjectConfigurator implements ProjectConfigurator {
+
+	public BaseProjectConfigurator(Settings settings) {
+		File defaultRootDir = new File(
+			settings.getRootDir(), getDefaultRootDirName());
+
+		_defaultRootDir = GradleUtil.getProperty(
+			settings, getDefaultRootDirPropertyName(), defaultRootDir);
+	}
+
+	@Override
+	public File getDefaultRootDir() {
+		return _defaultRootDir;
+	}
+
+	@Override
+	public Iterable<File> getProjectDirs(File rootDir) {
+		try {
+			return doGetProjectDirs(rootDir);
+		}
+		catch (Exception e) {
+			throw new GradleException(
+				"Unable to get project dirs from " + rootDir, e);
+		}
+	}
+
+	protected abstract Iterable<File> doGetProjectDirs(File rootDir)
+		throws Exception;
+
+	protected String getDefaultRootDirName() {
+		return getName();
+	}
+
+	protected String getDefaultRootDirPropertyName() {
+		return WorkspacePlugin.PROPERTY_PREFIX + getName() + ".dir";
+	}
+
+	private final File _defaultRootDir;
+
+}
---
+++ modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/util/GradleUtil.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.gradle.plugins.workspace.util;
+
+import com.liferay.gradle.util.Validator;
+
+import java.io.File;
+
+import org.gradle.api.initialization.Settings;
+import org.gradle.api.internal.DynamicObject;
+import org.gradle.api.internal.DynamicObjectUtil;
+
+/**
+ * @author Andrea Di Giorgi
+ */
+public class GradleUtil extends com.liferay.gradle.util.GradleUtil {
+
+	public static Object getProperty(Object object, String name) {
+		DynamicObject dynamicObject = DynamicObjectUtil.asDynamicObject(object);
+
+		if (!dynamicObject.hasProperty(name)) {
+			return null;
+		}
+
+		Object value = dynamicObject.getProperty(name);
+
+		if ((value instanceof String) && Validator.isNull((String)value)) {
+			value = null;
+		}
+
+		return value;
+	}
+
+	public static boolean getProperty(
+		Object object, String name, boolean defaultValue) {
+
+		Object value = getProperty(object, name);
+
+		if (value == null) {
+			return defaultValue;
+		}
+
+		if (value instanceof Boolean) {
+			return (Boolean)value;
+		}
+
+		if (value instanceof String) {
+			return Boolean.parseBoolean((String)value);
+		}
+
+		return defaultValue;
+	}
+
+	public static String getProperty(
+		Object object, String name, String defaultValue) {
+
+		Object value = getProperty(object, name);
+
+		if (value == null) {
+			return defaultValue;
+		}
+
+		return toString(value);
+	}
+
+	public static File getProperty(
+		Settings settings, String name, File defaultValue) {
+
+		String value = getProperty(settings, name, (String)null);
+
+		if (Validator.isNull(value)) {
+			return defaultValue;
+		}
+
+		return new File(settings.getRootDir(), value);
+	}
+
+}
