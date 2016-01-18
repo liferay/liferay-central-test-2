48a6f441487fa73892f1e5462400897c918c2b0f
Jorge Ferrer <jorge.ferrer@liferay.com>
2016-01-18 15:39:03 -0800

    LPS-61577 Change references in last minute additions

--- modules/portal/portal-search/src/main/java/com/liferay/portal/search/configuration/SearchPermissionCheckerConfiguration.java
+++ modules/portal/portal-search/src/main/java/com/liferay/portal/search/configuration/SearchPermissionCheckerConfiguration.java
@@ -16,12 +16,12 @@ package com.liferay.portal.search.configuration;
 
 import aQute.bnd.annotation.metatype.Meta;
 
-import com.liferay.configuration.admin.ConfigurationAdmin;
+import com.liferay.portal.metatype.annotations.ExtendedObjectClassDefinition;
 
 /**
  * @author Michael C. Han
  */
-@ConfigurationAdmin(category = "platform")
+@ExtendedObjectClassDefinition(category = "platform")
 @Meta.OCD(
 	id = "com.liferay.portal.search.configuration.SearchPermissionCheckerConfiguration",
 	localization = "content/Language",
