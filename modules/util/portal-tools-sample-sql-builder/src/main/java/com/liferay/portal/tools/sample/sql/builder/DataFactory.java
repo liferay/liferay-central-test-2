6f84b457857c2f5b7307d8ac15c7175fc6709f1a
lily.chi <lily.chi@liferay.com>
2016-01-19 09:54:21 +0800

    LPS-61793 this part still need to update for SampleSQLBuilder

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -2068,7 +2068,7 @@ public class DataFactory {
 
 		layoutModels.add(
 			newLayoutModel(
-				groupId, "welcome", LoginPortletKeys.LOGIN + ",", "47,"));
+				groupId, "welcome", LoginPortletKeys.LOGIN + ",", "com_liferay_hello_world_web_portlet_HelloWorldPortlet,"));
 		layoutModels.add(
 			newLayoutModel(groupId, "blogs", "", BlogsPortletKeys.BLOGS + ","));
 		layoutModels.add(
