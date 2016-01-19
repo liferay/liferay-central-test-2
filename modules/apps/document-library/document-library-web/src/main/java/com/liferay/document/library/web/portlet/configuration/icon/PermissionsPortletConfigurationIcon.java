cfa0ecc2b8b4934d2eb4f72ac112f49b9c6f708f
Adolfo Pérez <adolfo.perez@liferay.com>
2016-01-19 19:35:32 +0100

    LPS-62250 Add permissions configuration icon

---
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/portlet/configuration/icon/PermissionsPortletConfigurationIcon.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.portlet.configuration.icon;
+
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.model.User;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portlet.documentlibrary.service.permission.DLPermission;
+import com.liferay.taglib.security.PermissionsURLTag;
+
+import javax.portlet.PortletRequest;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class PermissionsPortletConfigurationIcon
+	extends BasePortletConfigurationIcon {
+
+	public PermissionsPortletConfigurationIcon(PortletRequest portletRequest) {
+		super(portletRequest);
+	}
+
+	@Override
+	public String getMessage() {
+		return "permissions";
+	}
+
+	@Override
+	public String getURL() {
+		String url = StringPool.BLANK;
+
+		try {
+			url = PermissionsURLTag.doTag(
+				StringPool.BLANK, "com.liferay.portlet.documentlibrary",
+				themeDisplay.getScopeGroupName(), null,
+				String.valueOf(themeDisplay.getScopeGroupId()),
+				LiferayWindowState.POP_UP.toString(), null,
+				themeDisplay.getRequest());
+		}
+		catch (Exception e) {
+		}
+
+		return url;
+	}
+
+	@Override
+	public boolean isShow() {
+		User user = themeDisplay.getUser();
+
+		if (user.isDefaultUser()) {
+			return false;
+		}
+
+		PermissionChecker permissionChecker =
+			themeDisplay.getPermissionChecker();
+
+		try {
+			if (!DLPermission.contains(
+					permissionChecker, themeDisplay.getScopeGroupId(),
+					ActionKeys.PERMISSIONS)) {
+
+				return false;
+			}
+		}
+		catch (Exception e) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean isToolTip() {
+		return false;
+	}
+
+	@Override
+	public boolean isUseDialog() {
+		return true;
+	}
+
+}
