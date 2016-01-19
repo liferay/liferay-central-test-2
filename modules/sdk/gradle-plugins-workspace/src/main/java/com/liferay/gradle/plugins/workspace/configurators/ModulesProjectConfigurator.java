f7c3ce052f9a987b74c40c99035ffe65b9bc7fe6
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-01-18 17:53:36 -0800

    LPS-61767 Isolate module projects configuration

--- modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/configurators/ModulesProjectConfigurator.java
+++ modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/configurators/ModulesProjectConfigurator.java
@@ -14,24 +14,66 @@
 
 package com.liferay.gradle.plugins.workspace.configurators;
 
-import java.io.File;
+import com.liferay.gradle.plugins.LiferayDefaultsPlugin;
+import com.liferay.gradle.plugins.LiferayPlugin;
+import com.liferay.gradle.plugins.extensions.LiferayExtension;
+import com.liferay.gradle.plugins.workspace.WorkspaceExtension;
+import com.liferay.gradle.plugins.workspace.WorkspacePlugin;
+import com.liferay.gradle.plugins.workspace.util.GradleUtil;
 
-import java.util.Collections;
+import groovy.lang.Closure;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository;
+import org.gradle.api.file.CopySourceSpec;
+import org.gradle.api.file.CopySpec;
 import org.gradle.api.initialization.Settings;
+import org.gradle.api.plugins.ExtensionAware;
+import org.gradle.api.plugins.JavaPlugin;
 
 /**
  * @author Andrea Di Giorgi
+ * @author David Truong
  */
 public class ModulesProjectConfigurator extends BaseProjectConfigurator {
 
 	public ModulesProjectConfigurator(Settings settings) {
 		super(settings);
+
+		_defaultRepositoryEnabled = GradleUtil.getProperty(
+			settings,
+			WorkspacePlugin.PROPERTY_PREFIX + _NAME +
+				".default.repository.enabled",
+			_DEFAULT_REPOSITORY_ENABLED);
 	}
 
 	@Override
 	public void apply(Project project) {
+		WorkspaceExtension workspaceExtension = GradleUtil.getExtension(
+			(ExtensionAware)project.getGradle(), WorkspaceExtension.class);
+
+		GradleUtil.applyPlugin(project, LiferayPlugin.class);
+
+		addRepositoryDefault(project, workspaceExtension);
+		configureLiferay(project, workspaceExtension);
+
+		configureRootTaskDistBundle(
+			project, RootProjectConfigurator.DIST_BUNDLE_TAR_TASK_NAME);
+		configureRootTaskDistBundle(
+			project, RootProjectConfigurator.DIST_BUNDLE_ZIP_TASK_NAME);
 	}
 
 	@Override
@@ -39,11 +81,86 @@ public class ModulesProjectConfigurator extends BaseProjectConfigurator {
 		return _NAME;
 	}
 
-	@Override
-	protected Iterable<File> doGetProjectDirs(File rootDir) throws Exception {
-		return Collections.emptySet();
+	public boolean isDefaultRepositoryEnabled() {
+		return _defaultRepositoryEnabled;
 	}
 
+	public void setDefaultRepositoryEnabled(boolean defaultRepositoryEnabled) {
+		_defaultRepositoryEnabled = defaultRepositoryEnabled;
+	}
+
+	protected MavenArtifactRepository addRepositoryDefault(
+		Project project, WorkspaceExtension workspaceExtension) {
+
+		if (!isDefaultRepositoryEnabled()) {
+			return null;
+		}
+
+		return GradleUtil.addMavenRepository(
+			project, LiferayDefaultsPlugin.DEFAULT_REPOSITORY_URL);
+	}
+
+	protected void configureLiferay(
+		Project project, WorkspaceExtension workspaceExtension) {
+
+		LiferayExtension liferayExtension = GradleUtil.getExtension(
+			project, LiferayExtension.class);
+
+		liferayExtension.setAppServerParentDir(workspaceExtension.getHomeDir());
+	}
+
+	protected void configureRootTaskDistBundle(
+		final Project project, String rootTaskName) {
+
+		CopySpec copySpec = (CopySpec)GradleUtil.getTask(
+			project.getRootProject(), rootTaskName);
+
+		copySpec.into(
+			"deploy",
+			new Closure<Void>(null) {
+
+				@SuppressWarnings("unused")
+				public void doCall(CopySourceSpec copySourceSpec) {
+					Task jarTask = GradleUtil.getTask(
+						project, JavaPlugin.JAR_TASK_NAME);
+
+					copySourceSpec.from(jarTask);
+				}
+
+			});
+	}
+
+	@Override
+	protected Iterable<File> doGetProjectDirs(File rootDir) throws Exception {
+		final Set<File> projectDirs = new HashSet<>();
+
+		Files.walkFileTree(
+			rootDir.toPath(),
+			new SimpleFileVisitor<Path>() {
+
+				@Override
+				public FileVisitResult preVisitDirectory(
+						Path dirPath, BasicFileAttributes basicFileAttributes)
+					throws IOException {
+
+					if (Files.exists(dirPath.resolve("bnd.bnd"))) {
+						projectDirs.add(dirPath.toFile());
+
+						return FileVisitResult.SKIP_SUBTREE;
+					}
+
+					return FileVisitResult.CONTINUE;
+				}
+
+			});
+
+		return projectDirs;
+	}
+
+	private static final boolean _DEFAULT_REPOSITORY_ENABLED = true;
+
 	private static final String _NAME = "modules";
+
+	private boolean _defaultRepositoryEnabled;
 
 }
