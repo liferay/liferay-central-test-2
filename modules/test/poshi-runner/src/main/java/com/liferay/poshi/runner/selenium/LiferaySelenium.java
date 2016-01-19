7c7d507ea8fdd660177c799b2e8ac5cd2eb9b9c4
Kevin Yen <kevin.yen@liferay.com>
2016-01-19 12:30:53 -0800

    LRQA-21170 Add required "throws Exceptions" declaration

--- modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/LiferaySelenium.java
+++ modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/LiferaySelenium.java
@@ -143,11 +143,12 @@ public interface LiferaySelenium {
 
 	public void click(String locator) throws Exception;
 
-	public void clickAndWait(String locator);
+	public void clickAndWait(String locator) throws Exception;
 
-	public void clickAt(String locator, String coordString);
+	public void clickAt(String locator, String coordString) throws Exception;
 
-	public void clickAtAndWait(String locator, String coordString);
+	public void clickAtAndWait(String locator, String coordString)
+		throws Exception;
 
 	public void close();
 
