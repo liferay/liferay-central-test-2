f0c746c927f6f200aa48745245f981c57544df81
shuyangzhou <shuyang.zhou@liferay.com>
2016-01-19 10:06:19 -0800

    LPS-62292 Apply

--- portal-impl/src/com/liferay/portal/dao/db/HypersonicDBFactory.java
+++ portal-impl/src/com/liferay/portal/dao/db/HypersonicDBFactory.java
@@ -14,17 +14,17 @@
 
 package com.liferay.portal.dao.db;
 
+import com.liferay.portal.kernel.dao.db.BaseDBFactory;
 import com.liferay.portal.kernel.dao.db.DB;
-import com.liferay.portal.kernel.dao.db.DBFactory;
 import com.liferay.portal.kernel.dao.db.DBType;
 
 /**
  * @author Shuyang Zhou
  */
-public class HypersonicDBFactory implements DBFactory {
+public class HypersonicDBFactory extends BaseDBFactory {
 
 	@Override
-	public DB create(int dbMajorVersion, int dbMinorVersion) {
+	public DB doCreate(int dbMajorVersion, int dbMinorVersion) {
 		return new HypersonicDB(dbMajorVersion, dbMinorVersion);
 	}
 
--- portal-impl/src/com/liferay/portal/dao/db/PostgreSQLDBFactory.java
+++ portal-impl/src/com/liferay/portal/dao/db/PostgreSQLDBFactory.java
@@ -14,17 +14,17 @@
 
 package com.liferay.portal.dao.db;
 
+import com.liferay.portal.kernel.dao.db.BaseDBFactory;
 import com.liferay.portal.kernel.dao.db.DB;
-import com.liferay.portal.kernel.dao.db.DBFactory;
 import com.liferay.portal.kernel.dao.db.DBType;
 
 /**
  * @author Shuyang Zhou
  */
-public class PostgreSQLDBFactory implements DBFactory {
+public class PostgreSQLDBFactory extends BaseDBFactory {
 
 	@Override
-	public DB create(int dbMajorVersion, int dbMinorVersion) {
+	public DB doCreate(int dbMajorVersion, int dbMinorVersion) {
 		return new PostgreSQLDB(dbMajorVersion, dbMinorVersion);
 	}
 
