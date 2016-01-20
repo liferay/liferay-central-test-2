b3b5798baedc87d48308e1681ec5235083ecd043
Marcellus Tavares <marcellus.tavares@liferay.com>
2016-01-20 18:22:46 -0300

    LPS-60433 SF

--- modules/portal/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/upgrade/v1_3_0/UpgradeClassName.java
+++ modules/portal/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/upgrade/v1_3_0/UpgradeClassName.java
@@ -14,47 +14,129 @@
 
 package com.liferay.portal.workflow.kaleo.upgrade.v1_3_0;
 
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.upgrade.util.Table;
+import com.liferay.portal.workflow.kaleo.util.WorkflowContextUtil;
+
+import java.io.Serializable;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Lino Alves
  */
-
 public class UpgradeClassName extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
-		updateTableClassName("KaleoInstance");
-		updateTableClassName("KaleoInstanceToken");
-		updateTableClassName("KaleoTaskInstanceToken");
+		updateClassName("KaleoInstance");
+		updateClassName("KaleoInstanceToken");
+		updateClassName("KaleoTaskInstanceToken");
 
-		updateWorkflowContextClassName("KaleoInstance");
-		updateWorkflowContextClassName("KaleoLog");
-		updateWorkflowContextClassName("KaleoTaskInstanceToken");
+		updateWorkflowContextEntryClassName("KaleoInstance", "kaleoInstanceId");
+		updateWorkflowContextEntryClassName("KaleoLog", "kaleoLogId");
+		updateWorkflowContextEntryClassName(
+			"KaleoTaskInstanceToken", "kaleoTaskInstanceTokenId");
 	}
 
-	protected void updateTableClassName(String tableName) throws Exception {
-		StringBundler sb = new StringBundler();
-		sb.append("update "+ tableName +" set ");
-		sb.append("className = \"com.liferay.journal.model.JournalArticle\"");
-		sb.append("where className = ");
-		sb.append("\"com.liferay.portlet.journal.model.JournalArticle\"");
-		String sql = sb.toString();
-		runSQL(sql);
+	protected void updateClassName(String tableName) {
+		Table table = new Table(tableName);
+
+		for (Map.Entry<String, String> entry : _classNamesMap.entrySet()) {
+			table.updateColumnValue(
+				"className", entry.getKey(), entry.getValue());
+		}
 	}
 
-	protected void updateWorkflowContextClassName(String tableName)
+	protected void updateWorkflowContext(
+			String tableName, String primaryKeyName, long primaryKeyValue,
+			String workflowContext)
 		throws Exception {
 
-		StringBundler sb = new StringBundler();
-		sb.append("update "+ tableName +" set ");
-		sb.append("workflowContext = ");
-		sb.append("REPLACE( workflowContext,");
-		sb.append("'com.liferay.portlet.journal.model.JournalArticle',");
-		sb.append("'com.liferay.journal.model.JournalArticle' )");
-		String sql = sb.toString();
-		runSQL(sql);
+		PreparedStatement ps = null;
+
+		try {
+			ps = connection.prepareStatement(
+				"update " + tableName + " set workflowContext = ? where " +
+					primaryKeyName + " = ?");
+
+			ps.setString(1, workflowContext);
+			ps.setLong(2, primaryKeyValue);
+
+			ps.executeUpdate();
+		}
+		finally {
+			DataAccess.cleanUp(ps);
+		}
+	}
+
+	protected void updateWorkflowContextEntryClassName(
+			String tableName, String primaryKeyName)
+		throws Exception {
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			ps = connection.prepareStatement(
+				"select " + primaryKeyName + ", workflowContext from " +
+					tableName + " where workflowContext is not null");
+
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				long primaryKeyValue = rs.getLong(primaryKeyName);
+				String workflowContextJSON = rs.getString("workflowContext");
+
+				if (Validator.isNull(workflowContextJSON)) {
+					continue;
+				}
+
+				Map<String, Serializable> workflowContext =
+					WorkflowContextUtil.convert(workflowContextJSON);
+
+				String oldEntryClassName = (String)workflowContext.get(
+					"entryClassName");
+
+				String newEntryClassName = _classNamesMap.get(
+					oldEntryClassName);
+
+				if (newEntryClassName == null) {
+					continue;
+				}
+
+				workflowContext.put("entryClassName", newEntryClassName);
+
+				updateWorkflowContext(
+					tableName, primaryKeyName, primaryKeyValue,
+					WorkflowContextUtil.convert(workflowContext));
+			}
+		}
+		finally {
+			DataAccess.cleanUp(ps, rs);
+		}
+	}
+
+	private static final Map<String, String> _classNamesMap = new HashMap<>();
+
+	static {
+		_classNamesMap.put(
+			"com.liferay.portlet.journal.model.JournalArticle",
+			"com.liferay.journal.model.JournalArticle");
+
+		_classNamesMap.put(
+			"com.liferay.portlet.dynamicdatalists.model.DDLRecord",
+			"com.liferay.dynamic.data.lists.model.DDLRecord");
+
+		_classNamesMap.put(
+			"com.liferay.portlet.wiki.model.WikiPage",
+			"com.liferay.wiki.model.WikiPage");
 	}
 
 }
