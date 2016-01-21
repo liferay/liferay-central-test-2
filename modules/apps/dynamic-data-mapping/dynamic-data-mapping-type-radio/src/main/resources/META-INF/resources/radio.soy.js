538b399bc3f88ca70075b7a3539cd753ae939707
Leonardo Barros <leonardo.barros@liferay.com>
2016-01-21 10:03:59 -0300

    LPS-55710 buildSoy

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-radio/src/main/resources/META-INF/resources/radio.soy.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-radio/src/main/resources/META-INF/resources/radio.soy.js
@@ -5,13 +5,13 @@ if (typeof ddm == 'undefined') { var ddm = {}; }
 
 
 ddm.radio = function(opt_data, opt_ignored) {
-var output = '\t<div class="form-group' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + '" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '">' + ((opt_data.showLabel) ? '<label class="control-label">' + soy.$$escapeHtml(opt_data.label) + ((opt_data.required) ? '<span class="icon-asterisk text-warning"></span>' : '') + '</label>' : '') + '<div class="radio-options">';
-var optionList17 = opt_data.options;
-var optionListLen17 = optionList17.length;
-for (var optionIndex17 = 0; optionIndex17 < optionListLen17; optionIndex17++) {
-	var optionData17 = optionList17[optionIndex17];
-	output += ((! opt_data.inline) ? '<div class="radio">' : '') + '<label class="radio-default' + soy.$$escapeHtml(opt_data.inline ? ' radio-inline' : '') + '" for="' + soy.$$escapeHtml(optionData17.value) + '"><input class="field" dir="' + soy.$$escapeHtml(opt_data.dir) + '" ' + ((opt_data.readOnly) ? 'disabled' : '') + ' id="' + soy.$$escapeHtml(optionData17.value) + '" name="' + soy.$$escapeHtml(opt_data.name) + '" ' + soy.$$escapeHtml(optionData17.status) + ' type="radio" value="' + soy.$$escapeHtml(optionData17.value) + '" /> ' + soy.$$escapeHtml(optionData17.label) + '</label>' + ((! opt_data.inline) ? '</div>' : '');
-}
-output += '</div>' + soy.$$filterNoAutoescape(opt_data.childElementsHTML) + '</div>';
-return output;
+  var output = '\t<div class="form-group' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + '" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '">' + ((opt_data.showLabel) ? '<label class="control-label">' + soy.$$escapeHtml(opt_data.label) + ((opt_data.required) ? '<span class="icon-asterisk text-warning"></span>' : '') + '</label>' + ((opt_data.tip) ? '<p class="liferay-ddm-form-field-tip">' + soy.$$escapeHtml(opt_data.tip) + '</p>' : '') : '') + '<div class="radio-options">';
+  var optionList22 = opt_data.options;
+  var optionListLen22 = optionList22.length;
+  for (var optionIndex22 = 0; optionIndex22 < optionListLen22; optionIndex22++) {
+    var optionData22 = optionList22[optionIndex22];
+    output += ((! opt_data.inline) ? '<div class="radio">' : '') + '<label class="radio-default' + soy.$$escapeHtml(opt_data.inline ? ' radio-inline' : '') + '" for="' + soy.$$escapeHtml(optionData22.value) + '"><input class="field" dir="' + soy.$$escapeHtml(opt_data.dir) + '" ' + ((opt_data.readOnly) ? 'disabled' : '') + ' id="' + soy.$$escapeHtml(optionData22.value) + '" name="' + soy.$$escapeHtml(opt_data.name) + '" ' + soy.$$escapeHtml(optionData22.status) + ' type="radio" value="' + soy.$$escapeHtml(optionData22.value) + '" /> ' + soy.$$escapeHtml(optionData22.label) + '</label>' + ((! opt_data.inline) ? '</div>' : '');
+  }
+  output += '</div>' + soy.$$filterNoAutoescape(opt_data.childElementsHTML) + '</div>';
+  return output;
 };
