db457b25ee1bfb3aa4f928123290dd8acddf094c
Hugo Huijser <hugo.huijser@liferay.com>
2016-01-21 11:34:51 +0800

    LPS-61420 Add test

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/JavaSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/JavaSourceProcessorTest.java
@@ -113,11 +113,11 @@ public class JavaSourceProcessorTest extends BaseSourceProcessorTestCase {
 				"line break:", "line break:", "line break:", "line break:",
 				"line break:", "line break:", "line break:", "line break:",
 				"line break:", "line break:", "line break:", "line break:",
-				"line break:"
+				"line break:", "line break:"
 			},
 			new Integer[] {
-				31, 35, 43, 47, 49, 52, 55, 59, 62, 67, 73, 77, 83, 94, 107,
-				111, 121
+				31, 35, 43, 47, 49, 52, 55, 59, 62, 67, 71, 77, 81, 87, 98, 111,
+				115, 125
 			});
 		test("IncorrectLineBreaks2.testjava");
 	}
