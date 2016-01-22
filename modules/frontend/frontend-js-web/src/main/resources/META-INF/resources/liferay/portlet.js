933bef51d482029312454ca521cea7fec1f4af4c
Pavel Savinov <pavel.savinov@liferay.com>
2016-01-22 13:39:35 +0100

    LPS-61614 do not update layout on nested portlet removal

--- modules/frontend/frontend-js-web/src/main/resources/META-INF/resources/liferay/portlet.js
+++ modules/frontend/frontend-js-web/src/main/resources/META-INF/resources/liferay/portlet.js
@@ -26,24 +26,26 @@
 
 			event.portlet.remove(true);
 
-			A.io.request(
-				themeDisplay.getPathMain() + '/portal/update_layout',
-				{
-					after: {
-						success: function() {
-							Liferay.fire('updatedLayout');
+			if (!event.nestedPortlet) {
+				A.io.request(
+					themeDisplay.getPathMain() + '/portal/update_layout',
+					{
+						after: {
+							success: function() {
+								Liferay.fire('updatedLayout');
+							}
+						},
+						data: {
+							cmd: 'delete',
+							doAsUserId: event.doAsUserId,
+							p_auth: Liferay.authToken,
+							p_l_id: event.plid,
+							p_p_id: event.portletId,
+							p_v_l_s_g_id: themeDisplay.getSiteGroupId()
 						}
-					},
-					data: {
-						cmd: 'delete',
-						doAsUserId: event.doAsUserId,
-						p_auth: Liferay.authToken,
-						p_l_id: event.plid,
-						p_p_id: event.portletId,
-						p_v_l_s_g_id: themeDisplay.getSiteGroupId()
 					}
-				}
-			);
+				);
+			}
 		},
 
 		_loadMarkupHeadElements: function(response, loadHTML) {
