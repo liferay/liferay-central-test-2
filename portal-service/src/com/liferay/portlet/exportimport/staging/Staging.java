b40a24fbc0248926589f5e2b77499af8314207e6
akosthurzo <akos.thurzo@liferay.com>
2016-01-22 16:37:13 +0100

    LPS-58762 Add API to help determining if the contents of a group are visible from another group from staging's perspective

--- portal-service/src/com/liferay/portlet/exportimport/staging/Staging.java
+++ portal-service/src/com/liferay/portlet/exportimport/staging/Staging.java
@@ -250,6 +250,11 @@ public interface Staging {
 	public boolean hasWorkflowTask(long userId, LayoutRevision layoutRevision)
 		throws PortalException;
 
+	public boolean isGroupVisible(Group group, Group fromGroup);
+
+	public boolean isGroupVisible(long groupId, long fromGroupId)
+		throws PortalException;
+
 	public boolean isIncomplete(Layout layout, long layoutSetBranchId);
 
 	/**
