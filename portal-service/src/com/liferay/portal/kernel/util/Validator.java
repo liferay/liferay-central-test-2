30d21ca4af384a4ae719d2b7ec66973458b0c5bf
Jim Hinkey <james.hinkey@liferay.com>
2016-01-22 10:15:32 -0500

    LRDOCS-2152 Using term "whitespace" as it includes new line and tab characters

--- portal-service/src/com/liferay/portal/kernel/util/Validator.java
+++ portal-service/src/com/liferay/portal/kernel/util/Validator.java
@@ -1047,8 +1047,8 @@ public class Validator {
 	/**
 	 * Returns <code>true</code> if the string is not <code>null</code>, meaning
 	 * it is not a <code>null</code> reference, an empty string, whitespace, or
-	 * the string "<code>null</code>", with zero or more leading or trailing
-	 * spaces.
+	 * the string "<code>null</code>", with or without leading or trailing
+	 * whitespace.
 	 *
 	 * @param  s the string to check
 	 * @return <code>true</code> if the string is not <code>null</code>;
@@ -1110,7 +1110,8 @@ public class Validator {
 	/**
 	 * Returns <code>true</code> if the string is <code>null</code>, meaning it
 	 * is a <code>null</code> reference, an empty string, whitespace, or the
-	 * string "<code>null</code>", with zero or more leading or trailing spaces.
+	 * string "<code>null</code>", with or without leading or trailing
+	 * whitespace.
 	 *
 	 * @param  s the string to check
 	 * @return <code>true</code> if the string is <code>null</code>;
