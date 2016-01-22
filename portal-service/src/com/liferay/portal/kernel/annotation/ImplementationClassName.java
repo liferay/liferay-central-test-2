72fc201d795e807660158e41145163f9c99e34cf
shuyangzhou <shuyang.zhou@liferay.com>
2016-01-22 14:58:13 -0800

    LPS-61952 Rename

--- portal-service/src/com/liferay/portal/kernel/annotation/ImplementationClassName.java
+++ portal-service/src/com/liferay/portal/kernel/annotation/ImplementationClassName.java
@@ -24,7 +24,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-public @interface ImplementationPath {
+public @interface ImplementationClassName {
 
 	public String value();
 
