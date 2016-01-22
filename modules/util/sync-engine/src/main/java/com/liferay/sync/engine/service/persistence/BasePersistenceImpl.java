b911667309978a4c453e8970929e7f0339c02678
Dennis Ju <dennis.ju@liferay.com>
2016-01-22 12:41:36 -0800

    SYNC-1534 H2's MVCC setting allows shared locks, minimizing database timeouts

--- modules/util/sync-engine/src/main/java/com/liferay/sync/engine/service/persistence/BasePersistenceImpl.java
+++ modules/util/sync-engine/src/main/java/com/liferay/sync/engine/service/persistence/BasePersistenceImpl.java
@@ -183,7 +183,7 @@ public class BasePersistenceImpl<TT extends BaseModel, TID>
 		sb.append(fileSystem.getSeparator());
 
 		sb.append(PropsValues.SYNC_DATABASE_NAME);
-		sb.append(";AUTO_SERVER=TRUE;DB_CLOSE_ON_EXIT=FALSE");
+		sb.append(";AUTO_SERVER=TRUE;DB_CLOSE_ON_EXIT=FALSE;MVCC=TRUE");
 
 		_connectionSource = new JdbcPooledConnectionSource(sb.toString());
 
