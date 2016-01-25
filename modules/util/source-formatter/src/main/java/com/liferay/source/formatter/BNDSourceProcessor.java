52437ce82a1e7145d303a359dee6c538e357597a
Hugo Huijser <hugo.huijser@liferay.com>
2016-01-25 15:53:41 +0800

    LPS-61420 Sort definitions in bnd files

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/BNDSourceProcessor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/BNDSourceProcessor.java
@@ -16,9 +16,11 @@ package com.liferay.source.formatter;
 
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.io.File;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -56,6 +58,8 @@ public class BNDSourceProcessor extends BaseSourceProcessor {
 				content, matcher.group(1), StringPool.SPACE, matcher.start());
 		}
 
+		content = sortDefinitions(content);
+
 		return trimContent(content, false);
 	}
 
@@ -64,9 +68,58 @@ public class BNDSourceProcessor extends BaseSourceProcessor {
 		return getFileNames(new String[0], getIncludes());
 	}
 
+	protected String sortDefinitions(String content) {
+		String previousDefinition = null;
+
+		DefinitionComparator definitionComparator = new DefinitionComparator();
+
+		Matcher matcher = _bndDefinitionPattern.matcher(content);
+
+		while (matcher.find()) {
+			String definition = matcher.group();
+
+			if (Validator.isNotNull(matcher.group(1))) {
+				definition = definition.substring(0, definition.length() - 1);
+			}
+
+			if (Validator.isNotNull(previousDefinition) &&
+				(definitionComparator.compare(previousDefinition, definition) >
+					0)) {
+
+				content = StringUtil.replaceFirst(
+					content, previousDefinition, definition);
+				content = StringUtil.replaceLast(
+					content, definition, previousDefinition);
+
+				return content;
+			}
+
+			previousDefinition = definition;
+		}
+
+		return content;
+	}
+
 	private static final String[] _INCLUDES = new String[] {"**/*.bnd"};
 
+	private Pattern _bndDefinitionPattern = Pattern.compile(
+		"^[A-Za-z-][\\s\\S]*?([^\\\\]\n|\\Z)", Pattern.MULTILINE);
 	private Pattern _singleValueOnMultipleLinesPattern = Pattern.compile(
 		"\n.*:(\\\\\n\t).*(\n[^\t]|\\Z)");
+
+	private class DefinitionComparator implements Comparator<String> {
+
+		@Override
+		public int compare(String definition1, String definition2) {
+			if (definition1.startsWith(StringPool.DASH) ^
+				definition2.startsWith(StringPool.DASH)) {
+
+				return -definition1.compareTo(definition2);
+			}
+
+			return definition1.compareTo(definition2);
+		}
+
+	}
 
 }
