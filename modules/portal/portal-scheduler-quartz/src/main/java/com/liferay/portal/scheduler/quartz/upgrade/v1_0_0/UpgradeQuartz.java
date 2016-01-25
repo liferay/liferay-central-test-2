77dc50d996d77a7840e6f10f6b520dea23812a0b
akosthurzo <akos.thurzo@liferay.com>
2016-01-25 14:50:53 +0100

    LPS-61782 Add upgrade process to replace package name

---
+++ modules/portal/portal-scheduler-quartz/src/main/java/com/liferay/portal/scheduler/quartz/upgrade/v1_0_0/UpgradeQuartz.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.scheduler.quartz.upgrade.v1_0_0;
+
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+import java.sql.PreparedStatement;
+
+/**
+ * @author Akos Thurzo
+ */
+public class UpgradeQuartz extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		PreparedStatement ps = null;
+
+		try {
+			ps = connection.prepareStatement(
+				"update QUARTZ_JOB_DETAILS set job_class_name = ? " +
+					"where job_class_name = ?");
+
+			ps.setString(
+				1,
+				"com.liferay.portal.scheduler.quartz.internal.job." +
+					"MessageSenderJob");
+			ps.setString(
+				2, "com.liferay.portal.scheduler.job.MessageSenderJob");
+
+			ps.executeUpdate();
+		}
+		finally {
+			DataAccess.cleanUp(ps);
+		}
+	}
+
+}
