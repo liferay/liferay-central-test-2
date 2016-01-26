2caf025d758a461bdaa213cf39db1964d6e2fb93
Adolfo Pérez <adolfo.perez@liferay.com>
2016-01-26 12:16:15 +0100

    LPS-62493 Remove order dependency between `portlet` and `frontend` DefineObjects tags
    The DefineObjects tag in liferay-frontend depends on portlet:defineObjects defining
    the "liferayPortletRequest" and "liferayPortletResponse" page context variables.
    
    Change liferay-frontend:defineObjects so that it does not depend on those page context
    variables.

--- modules/frontend/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/DefineObjectsTag.java
+++ modules/frontend/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/DefineObjectsTag.java
@@ -16,6 +16,7 @@ package com.liferay.frontend.taglib.servlet.taglib;
 
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portlet.PortletURLUtil;
 import com.liferay.taglib.util.TagResourceBundleUtil;
@@ -23,9 +24,12 @@ import com.liferay.taglib.util.TagResourceBundleUtil;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
 import javax.portlet.PortletURL;
 import javax.portlet.WindowState;
 
+import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.TagSupport;
@@ -62,12 +66,10 @@ public class DefineObjectsTag extends TagSupport {
 
 	protected PortletURL getCurrentURLObj() {
 		LiferayPortletRequest liferayPortletRequest =
-			(LiferayPortletRequest)pageContext.getAttribute(
-				"liferayPortletRequest");
+			getLiferayPortletRequest();
 
 		LiferayPortletResponse liferayPortletResponse =
-			(LiferayPortletResponse)pageContext.getAttribute(
-				"liferayPortletResponse");
+			getLiferayPortletResponse();
 
 		if ((liferayPortletRequest == null) ||
 			(liferayPortletResponse == null)) {
@@ -95,14 +97,55 @@ public class DefineObjectsTag extends TagSupport {
 
 	protected WindowState getWindowState() {
 		LiferayPortletRequest liferayPortletRequest =
-			(LiferayPortletRequest)pageContext.getAttribute(
-				"liferayPortletRequest");
+			getLiferayPortletRequest();
 
 		if (liferayPortletRequest == null) {
 			return null;
 		}
 
 		return liferayPortletRequest.getWindowState();
+	}
+
+	private LiferayPortletRequest getLiferayPortletRequest() {
+		LiferayPortletRequest liferayPortletRequest =
+			(LiferayPortletRequest)pageContext.getAttribute(
+				"liferayPortletRequest");
+
+		if (liferayPortletRequest != null) {
+			return liferayPortletRequest;
+		}
+
+		ServletRequest request = pageContext.getRequest();
+
+		PortletRequest portletRequest = (PortletRequest)request.getAttribute(
+			JavaConstants.JAVAX_PORTLET_REQUEST);
+
+		if (portletRequest == null) {
+			return null;
+		}
+
+		return PortalUtil.getLiferayPortletRequest(portletRequest);
+	}
+
+	private LiferayPortletResponse getLiferayPortletResponse() {
+		LiferayPortletResponse liferayPortletResponse =
+			(LiferayPortletResponse)pageContext.getAttribute(
+				"liferayPortletResponse");
+
+		if (liferayPortletResponse != null) {
+			return liferayPortletResponse;
+		}
+
+		ServletRequest request = pageContext.getRequest();
+
+		PortletResponse portletResponse = (PortletResponse)request.getAttribute(
+			JavaConstants.JAVAX_PORTLET_RESPONSE);
+
+		if (portletResponse == null) {
+			return null;
+		}
+
+		return PortalUtil.getLiferayPortletResponse(portletResponse);
 	}
 
 	private static final String _DEFAULT_RESOURCE_BUNDLE_BASE_NAME =
