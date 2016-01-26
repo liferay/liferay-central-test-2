ea9cfa24002b158b3957b6fc69a5092cfea88f9b
Hugo Huijser <hugo.huijser@liferay.com>
2016-01-27 07:55:54 +0800

    LPS-60922 Print warning when "-dsannotations-options: inherit" is needed/redundant in bnd.bnd

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/BaseSourceProcessor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/BaseSourceProcessor.java
@@ -1699,6 +1699,10 @@ public abstract class BaseSourceProcessor implements SourceProcessor {
 	protected void postFormat() throws Exception {
 	}
 
+	protected void printError(String fileName, String message) {
+		_sourceFormatterHelper.printError(fileName, message);
+	}
+
 	protected void processErrorMessage(String fileName, String message) {
 		List<String> errorMessages = _errorMessagesMap.get(fileName);
 
