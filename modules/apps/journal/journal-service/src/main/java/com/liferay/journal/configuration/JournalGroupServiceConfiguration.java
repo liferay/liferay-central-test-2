5044c600e4bd7ffec22efef6f5c3688031f63665
Pavel Savinov <pavel.savinov@liferay.com>
2016-01-26 14:27:15 +0100

    LPS-62523 add changeable default language configuration property for web content

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/configuration/JournalGroupServiceConfiguration.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/configuration/JournalGroupServiceConfiguration.java
@@ -33,6 +33,9 @@ import com.liferay.portal.metatype.annotations.ExtendedObjectClassDefinition;
 )
 public interface JournalGroupServiceConfiguration {
 
+	@Meta.AD(deflt = "false", required = false)
+	public boolean changeableDefaultLanguage();
+
 	@Meta.AD(
 		deflt = "${resource:com/liferay/journal/dependencies/email_article_added_body.tmpl}",
 		required = false
