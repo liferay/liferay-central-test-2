71073bce6f058bc62cd464b0c7d443f0ea8f9375
Brian Greenwald <brian.greenwald@liferay.com>
2016-01-26 14:10:14 -0600

    LPS-62298 Update context name

--- modules/apps/portal-settings/portal-settings-authentication-cas-web/src/main/java/com/liferay/portal/settings/authentication/cas/web/internal/servlet/taglib/PortalSettingsCASAuthenticationDynamicInclude.java
+++ modules/apps/portal-settings/portal-settings-authentication-cas-web/src/main/java/com/liferay/portal/settings/authentication/cas/web/internal/servlet/taglib/PortalSettingsCASAuthenticationDynamicInclude.java
@@ -68,7 +68,7 @@ public class PortalSettingsCASAuthenticationDynamicInclude
 	}
 
 	@Reference(
-		target = "(osgi.web.symbolicname=com.liferay.portal.security.sso.cas)",
+		target = "(osgi.web.symbolicname=com.liferay.portal.settings.authentication.cas.web)",
 		unbind = "-"
 	)
 	protected void setServletContext(ServletContext servletContext) {
