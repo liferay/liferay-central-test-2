f521ab86928978565a6e368330e50836e5150aad
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2016-01-27 11:24:56 +0100

    LPS-62533 Fix Test - simulate existing portlet permissions for the verify process to happen

--- portal-impl/test/integration/com/liferay/portal/verify/VerifyPermissionTest.java
+++ portal-impl/test/integration/com/liferay/portal/verify/VerifyPermissionTest.java
@@ -19,12 +19,10 @@ import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
-import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.model.Company;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.LayoutConstants;
-import com.liferay.portal.model.Portlet;
 import com.liferay.portal.model.ResourceConstants;
 import com.liferay.portal.model.ResourcePermission;
 import com.liferay.portal.model.Role;
@@ -32,13 +30,12 @@ import com.liferay.portal.model.RoleConstants;
 import com.liferay.portal.model.User;
 import com.liferay.portal.service.CompanyLocalServiceUtil;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
-import com.liferay.portal.service.PortletLocalServiceUtil;
+import com.liferay.portal.service.ResourceLocalServiceUtil;
 import com.liferay.portal.service.ResourcePermissionLocalServiceUtil;
 import com.liferay.portal.service.RoleLocalServiceUtil;
 import com.liferay.portal.service.permission.PortletPermissionUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.theme.ThemeDisplay;
-import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.verify.test.BaseVerifyProcessTestCase;
 import com.liferay.portlet.util.test.PortletKeys;
 
@@ -144,14 +141,12 @@ public class VerifyPermissionTest extends BaseVerifyProcessTestCase {
 
 		Layout layout = layouts.get(0);
 
-		themeDisplay.setLayout(layout);
+		String primKey = PortletPermissionUtil.getPrimaryKey(
+			layout.getPlid(), PortletKeys.TEST);
 
-		_request.setAttribute(WebKeys.THEME_DISPLAY, themeDisplay);
-
-		Portlet portlet = PortletLocalServiceUtil.getPortletById(
-			user.getCompanyId(), PortletKeys.TEST);
-
-		PortalUtil.addPortletDefaultResource(_request, portlet);
+		ResourceLocalServiceUtil.addResources(
+			company.getCompanyId(), layout.getGroupId(), 0, PortletKeys.TEST,
+			primKey, true, true, true);
 
 		return layout;
 	}
