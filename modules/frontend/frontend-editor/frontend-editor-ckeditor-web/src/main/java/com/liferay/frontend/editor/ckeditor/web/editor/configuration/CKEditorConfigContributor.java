9d99efbec041d7fb3d797a63eb59e27a77e1faa8
Carlos Sierra <carlos.sierra@liferay.com>
2016-01-27 11:48:33 +0100

    LPS-60355 Do not access other modules' classloader

--- modules/frontend/frontend-editor/frontend-editor-ckeditor-web/src/main/java/com/liferay/frontend/editor/ckeditor/web/editor/configuration/CKEditorConfigContributor.java
+++ modules/frontend/frontend-editor/frontend-editor-ckeditor-web/src/main/java/com/liferay/frontend/editor/ckeditor/web/editor/configuration/CKEditorConfigContributor.java
@@ -14,15 +14,16 @@
 
 package com.liferay.frontend.editor.ckeditor.web.editor.configuration;
 
-import com.liferay.frontend.editor.lang.FrontendEditorLang;
 import com.liferay.portal.kernel.editor.configuration.EditorConfigContributor;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.LanguageUtil;
-import com.liferay.portal.kernel.util.AggregateResourceBundle;
+import com.liferay.portal.kernel.util.AggregateResourceBundleLoader;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.ResourceBundleLoader;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.xuggler.XugglerUtil;
@@ -32,9 +33,11 @@ import com.liferay.portlet.RequestBackedPortletURLFactory;
 
 import java.util.Locale;
 import java.util.Map;
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Ambrin Chaudhary
@@ -135,13 +138,15 @@ public class CKEditorConfigContributor extends BaseCKEditorConfigContributor {
 	protected JSONArray getStyleFormatsJSONArray(Locale locale) {
 		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
 
-		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
-			"content.Language", locale, getClass());
+		ResourceBundle resourceBundle;
 
-		resourceBundle = new AggregateResourceBundle(
-			resourceBundle,
-			ResourceBundleUtil.getBundle(
-				"content.Language", locale, FrontendEditorLang.class));
+		try {
+			resourceBundle = _resourceBundleLoader.loadResourceBundle(
+				LocaleUtil.toLanguageId(locale));
+		}
+		catch (MissingResourceException mre) {
+			resourceBundle = ResourceBundleUtil.EMPTY_RESOURCE_BUNDLE;
+		}
 
 		jsonArray.put(
 			getStyleFormatJSONObject(
@@ -411,5 +416,23 @@ public class CKEditorConfigContributor extends BaseCKEditorConfigContributor {
 
 		return jsonArray;
 	}
+
+	@Reference(
+		target = "(bundle.symbolic.name=com.liferay.frontend.editor.lang)",
+		unbind = "-"
+	)
+	protected void setResourceBundleLoader(
+		ResourceBundleLoader resourceBundleLoader) {
+
+		ClassLoader classLoader =
+			CKEditorConfigContributor.class.getClassLoader();
+
+		_resourceBundleLoader = new AggregateResourceBundleLoader(
+			ResourceBundleUtil.getResourceBundleLoader(
+				"content.Language", classLoader),
+			resourceBundleLoader);
+	}
+
+	private volatile ResourceBundleLoader _resourceBundleLoader;
 
 }
