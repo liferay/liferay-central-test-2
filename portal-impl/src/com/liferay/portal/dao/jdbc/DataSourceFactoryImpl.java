1a8c580033e4a856e9cf4d31cc90b3003d6b513e
shuyangzhou <shuyang.zhou@liferay.com>
2016-01-27 15:44:38 -0800

    LPS-61760 This one is not loaded via ModuleFrameworkClassLoader, so it has to be done manually.

--- portal-impl/src/com/liferay/portal/dao/jdbc/DataSourceFactoryImpl.java
+++ portal-impl/src/com/liferay/portal/dao/jdbc/DataSourceFactoryImpl.java
@@ -103,6 +103,8 @@ public class DataSourceFactoryImpl implements DataSourceFactory {
 
 	@Override
 	public DataSource initDataSource(Properties properties) throws Exception {
+		Class.forName(DataSourceWrapper.class.getName());
+
 		Properties defaultProperties = PropsUtil.getProperties(
 			"jdbc.default.", true);
 
