c8f07ee96584f913a72b2a8d1ceff6232c06fa4a
Julio Camarero <julio.camarero@liferay.com>
2016-01-27 18:59:07 +0100

    LPS-60642 add a new UI for velocity that just includes the taglib

--- modules/apps/control-menu/control-menu-web/src/main/java/com/liferay/control/menu/web/portlet/ControlMenuPortlet.java
+++ modules/apps/control-menu/control-menu-web/src/main/java/com/liferay/control/menu/web/portlet/ControlMenuPortlet.java
@@ -44,6 +44,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.display-category=category.hidden",
 		"com.liferay.portlet.instanceable=false",
 		"com.liferay.portlet.use-default-template=false",
+		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + ControlMenuPortletKeys.CONTROL_MENU,
 		"javax.portlet.resource-bundle=content.Language",
 		"javax.portlet.supports.mime-type=text/html"
