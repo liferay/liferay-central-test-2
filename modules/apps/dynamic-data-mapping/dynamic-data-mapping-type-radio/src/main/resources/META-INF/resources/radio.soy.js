dae4de5a5d25a25911dca6581ce6d61abba586f6
Fernando Souza <fernando.souza@liferay.com>
2016-01-27 15:30:38 -0300

    LPS-62482 Add a css class that identify each radio option

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-radio/src/main/resources/META-INF/resources/radio.soy.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-radio/src/main/resources/META-INF/resources/radio.soy.js
@@ -10,7 +10,7 @@ ddm.radio = function(opt_data, opt_ignored) {
   var optionListLen22 = optionList22.length;
   for (var optionIndex22 = 0; optionIndex22 < optionListLen22; optionIndex22++) {
     var optionData22 = optionList22[optionIndex22];
-    output += ((! opt_data.inline) ? '<div class="radio">' : '') + '<label class="radio-default' + soy.$$escapeHtml(opt_data.inline ? ' radio-inline' : '') + '" for="' + soy.$$escapeHtml(optionData22.value) + '"><input class="field" dir="' + soy.$$escapeHtml(opt_data.dir) + '" ' + ((opt_data.readOnly) ? 'disabled' : '') + ' id="' + soy.$$escapeHtml(optionData22.value) + '" name="' + soy.$$escapeHtml(opt_data.name) + '" ' + soy.$$escapeHtml(optionData22.status) + ' type="radio" value="' + soy.$$escapeHtml(optionData22.value) + '" /> ' + soy.$$escapeHtml(optionData22.label) + '</label>' + ((! opt_data.inline) ? '</div>' : '');
+    output += ((! opt_data.inline) ? '<div class="radio">' : '') + '<label class="radio-default' + soy.$$escapeHtml(opt_data.inline ? ' radio-inline' : '') + ' radio-option-' + soy.$$escapeHtml(optionData22.value) + '" for="' + soy.$$escapeHtml(optionData22.value) + '"><input class="field" dir="' + soy.$$escapeHtml(opt_data.dir) + '" ' + ((opt_data.readOnly) ? 'disabled' : '') + ' id="' + soy.$$escapeHtml(optionData22.value) + '" name="' + soy.$$escapeHtml(opt_data.name) + '" ' + soy.$$escapeHtml(optionData22.status) + ' type="radio" value="' + soy.$$escapeHtml(optionData22.value) + '" /> ' + soy.$$escapeHtml(optionData22.label) + '</label>' + ((! opt_data.inline) ? '</div>' : '');
   }
   output += '</div>' + soy.$$filterNoAutoescape(opt_data.childElementsHTML) + '</div>';
   return output;
