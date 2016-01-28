d94436ef3fe793cd47697aec141a6c6e61ae0b83
drewbrokke <drew.brokke@liferay.com>
2016-01-28 11:09:37 -0800

    LPS-62553 adds display name to Simulation Portlet

--- modules/apps/product-navigation/product-navigation-simulation-web/src/main/java/com/liferay/product/navigation/simulation/web/portlet/ProductNavigationSimulationPortlet.java
+++ modules/apps/product-navigation/product-navigation-simulation-web/src/main/java/com/liferay/product/navigation/simulation/web/portlet/ProductNavigationSimulationPortlet.java
@@ -42,6 +42,7 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.private-request-attributes=false",
 		"com.liferay.portlet.show-portlet-access-denied=false",
 		"com.liferay.portlet.use-default-template=false",
+		"javax.portlet.display-name=Simulation",
 		"javax.portlet.init-param.view-template=/portlet/view.jsp",
 		"javax.portlet.name=" + ProductNavigationSimulationPortletKeys.PRODUCT_NAVIGATION_SIMULATION,
 		"javax.portlet.security-role-ref=power-user,user",
