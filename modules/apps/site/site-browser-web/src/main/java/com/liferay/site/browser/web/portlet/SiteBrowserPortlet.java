0c8e1a675ab0e668ddf1c047ffbbbad188acc538
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-01-29 00:08:15 +0100

    LPS-62580 Removes no needed language files

--- modules/apps/site/site-browser-web/src/main/java/com/liferay/site/browser/web/portlet/SiteBrowserPortlet.java
+++ modules/apps/site/site-browser-web/src/main/java/com/liferay/site/browser/web/portlet/SiteBrowserPortlet.java
@@ -38,7 +38,6 @@ import org.osgi.service.component.annotations.Component;
 		"javax.portlet.expiration-cache=0",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.resource-bundle=content.Language",
 		"javax.portlet.security-role-ref=guest,power-user,user",
 		"javax.portlet.supports.mime-type=text/html"
 	},
