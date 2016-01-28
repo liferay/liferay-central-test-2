6b0bfaf190b762423b63af317895e75797c975ce
Cristina González Castellano <cristina.gonzalez@liferay.com>
2016-01-28 13:05:36 +0100

    LPS-62637 Fix Upgrade for com.liferay.invitation.web

--- modules/apps/invitation/invitation-web/src/main/java/com/liferay/invitation/web/upgrade/InvitationWebUpgrade.java
+++ modules/apps/invitation/invitation-web/src/main/java/com/liferay/invitation/web/upgrade/InvitationWebUpgrade.java
@@ -15,56 +15,40 @@
 package com.liferay.invitation.web.upgrade;
 
 import com.liferay.invitation.web.constants.InvitationPortletKeys;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
-import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.portal.upgrade.util.UpgradePortletId;
 
-import java.util.Collections;
-
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Raymond Augé
  * @author Peter Fellwock
  */
-@Component(immediate = true, service = InvitationWebUpgrade.class)
-public class InvitationWebUpgrade {
+@Component(
+	immediate = true,
+	service = {InvitationWebUpgrade.class, UpgradeStepRegistrator.class}
+)
+public class InvitationWebUpgrade implements UpgradeStepRegistrator {
 
-	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
-	protected void setModuleServiceLifecycle(
-		ModuleServiceLifecycle moduleServiceLifecycle) {
+	@Override
+	public void register(UpgradeStepRegistrator.Registry registry) {
+		registry.register(
+			"com.liferay.invitation.web", "0.0.0", "1.0.0",
+			new DummyUpgradeStep());
+
+		registry.register(
+			"com.liferay.invitation.web", "0.0.1", "1.0.0",
+			new UpgradePortletId() {
+
+				@Override
+				protected String[][] getRenamePortletIdsArray() {
+					return new String[][] {
+						new String[] {"100", InvitationPortletKeys.INVITATION}
+					};
+				}
+
+			});
 	}
-
-	@Reference(unbind = "-")
-	protected void setReleaseLocalService(
-		ReleaseLocalService releaseLocalService) {
-
-		_releaseLocalService = releaseLocalService;
-	}
-
-	@Activate
-	protected void upgrade() throws PortalException {
-		UpgradePortletId upgradePortletId = new UpgradePortletId() {
-
-			@Override
-			protected String[][] getRenamePortletIdsArray() {
-				return new String[][] {
-					new String[] {"100", InvitationPortletKeys.INVITATION}
-				};
-			}
-
-		};
-
-		_releaseLocalService.updateRelease(
-			"com.liferay.invitation.web",
-			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 1,
-			false);
-	}
-
-	private ReleaseLocalService _releaseLocalService;
 
 }
