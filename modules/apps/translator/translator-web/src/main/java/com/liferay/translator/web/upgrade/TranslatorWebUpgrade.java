8109528ae47be4e6a088f9fddaf58e8fe2d1ac55
Cristina González Castellano <cristina.gonzalez@liferay.com>
2016-01-28 13:53:29 +0100

    LPS-62637 Fix Upgrade for com.liferay.translator.web

--- modules/apps/translator/translator-web/src/main/java/com/liferay/translator/web/upgrade/TranslatorWebUpgrade.java
+++ modules/apps/translator/translator-web/src/main/java/com/liferay/translator/web/upgrade/TranslatorWebUpgrade.java
@@ -14,59 +14,41 @@
 
 package com.liferay.translator.web.upgrade;
 
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
-import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.portal.upgrade.util.UpgradePortletId;
+import com.liferay.translator.web.constants.TranslatorPortletKeys;
 
-import java.util.Collections;
-
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Raymond Augé
  * @author Peter Fellwock
  */
-@Component(immediate = true, service = TranslatorWebUpgrade.class)
-public class TranslatorWebUpgrade {
+@Component(
+	immediate = true,
+	service = {TranslatorWebUpgrade.class, UpgradeStepRegistrator.class}
+)
+public class TranslatorWebUpgrade implements UpgradeStepRegistrator {
 
-	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
-	protected void setModuleServiceLifecycle(
-		ModuleServiceLifecycle moduleServiceLifecycle) {
+	@Override
+	public void register(UpgradeStepRegistrator.Registry registry) {
+		registry.register(
+			"com.liferay.translator.web", "0.0.0", "1.0.0",
+			new DummyUpgradeStep());
+
+		registry.register(
+			"com.liferay.translator.web", "0.0.1", "1.0.0",
+			new UpgradePortletId() {
+
+				@Override
+				protected String[][] getRenamePortletIdsArray() {
+					return new String[][] {
+						new String[] {"26", TranslatorPortletKeys.TRANSLATOR}
+					};
+				}
+
+			});
 	}
-
-	@Reference(unbind = "-")
-	protected void setReleaseLocalService(
-		ReleaseLocalService releaseLocalService) {
-
-		_releaseLocalService = releaseLocalService;
-	}
-
-	@Activate
-	protected void upgrade() throws PortalException {
-		UpgradePortletId upgradePortletId = new UpgradePortletId() {
-
-			@Override
-			protected String[][] getRenamePortletIdsArray() {
-				return new String[][] {
-					new String[] {
-						"26",
-						"com_liferay_translator_web_portlet_TranslatorPortlet"
-					}
-				};
-			}
-
-		};
-
-		_releaseLocalService.updateRelease(
-			"com.liferay.translator.web",
-			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 1,
-			false);
-	}
-
-	private ReleaseLocalService _releaseLocalService;
 
 }
