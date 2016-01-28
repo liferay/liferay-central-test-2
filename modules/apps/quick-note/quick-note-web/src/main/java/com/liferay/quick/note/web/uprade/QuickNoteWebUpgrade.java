ee277ac7c03df641711adb192db66008b9666bec
Cristina González Castellano <cristina.gonzalez@liferay.com>
2016-01-28 13:45:48 +0100

    LPS-62637 Fix Upgrade for com.liferay.quick.note.web

--- modules/apps/quick-note/quick-note-web/src/main/java/com/liferay/quick/note/web/uprade/QuickNoteWebUpgrade.java
+++ modules/apps/quick-note/quick-note-web/src/main/java/com/liferay/quick/note/web/uprade/QuickNoteWebUpgrade.java
@@ -14,57 +14,41 @@
 
 package com.liferay.quick.note.web.uprade;
 
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
-import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.portal.upgrade.util.UpgradePortletId;
 import com.liferay.quick.note.web.constants.QuickNotePortletKeys;
 
-import java.util.Collections;
-
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Raymond Augé
  * @author Peter Fellwock
  */
-@Component(immediate = true, service = QuickNoteWebUpgrade.class)
-public class QuickNoteWebUpgrade {
+@Component(
+	immediate = true,
+	service = {QuickNoteWebUpgrade.class, UpgradeStepRegistrator.class}
+)
+public class QuickNoteWebUpgrade implements UpgradeStepRegistrator {
 
-	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
-	protected void setModuleServiceLifecycle(
-		ModuleServiceLifecycle moduleServiceLifecycle) {
+	@Override
+	public void register(UpgradeStepRegistrator.Registry registry) {
+		registry.register(
+			"com.liferay.quick.note.web", "0.0.0", "1.0.0",
+			new DummyUpgradeStep());
+
+		registry.register(
+			"com.liferay.quick.note.web", "0.0.1", "1.0.0",
+			new UpgradePortletId() {
+
+				@Override
+				protected String[][] getRenamePortletIdsArray() {
+					return new String[][] {
+						new String[] {"97", QuickNotePortletKeys.QUICK_NOTE}
+					};
+				}
+
+			});
 	}
-
-	@Reference(unbind = "-")
-	protected void setReleaseLocalService(
-		ReleaseLocalService releaseLocalService) {
-
-		_releaseLocalService = releaseLocalService;
-	}
-
-	@Activate
-	protected void upgrade() throws PortalException {
-		UpgradePortletId upgradePortletId = new UpgradePortletId() {
-
-			@Override
-			protected String[][] getRenamePortletIdsArray() {
-				return new String[][] {
-					new String[] {"97", QuickNotePortletKeys.QUICK_NOTE}
-				};
-			}
-
-		};
-
-		_releaseLocalService.updateRelease(
-			"com.liferay.quick.note.web",
-			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 1,
-			false);
-	}
-
-	private ReleaseLocalService _releaseLocalService;
 
 }
