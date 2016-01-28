60a664016eeba02f1ae3804df59282ef242c9031
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-01-28 14:02:34 +0100

    LPS-62705 Removes no needed custom css for ProductNavigationUserPersonalBarPortlet

--- modules/apps/product-navigation/product-navigation-user-personal-bar-web/src/main/java/com/liferay/product/navigation/user/personal/bar/web/portlet/ProductNavigationUserPersonalBarPortlet.java
+++ modules/apps/product-navigation/product-navigation-user-personal-bar-web/src/main/java/com/liferay/product/navigation/user/personal/bar/web/portlet/ProductNavigationUserPersonalBarPortlet.java
@@ -41,7 +41,6 @@ import org.osgi.service.component.annotations.Reference;
 	property = {
 		"com.liferay.portlet.css-class-wrapper=portlet-user-personal-bar",
 		"com.liferay.portlet.display-category=category.hidden",
-		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
 		"com.liferay.portlet.private-session-attributes=false",
