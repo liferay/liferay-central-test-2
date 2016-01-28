b7617f8321a24b508a38833c6ed08eee4a34edc2
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-01-28 12:46:11 +0100

    LPS-62705 Removes no needed custom css form TrashPortlet

--- modules/apps/trash/trash-web/src/main/java/com/liferay/trash/web/portlet/TrashPortlet.java
+++ modules/apps/trash/trash-web/src/main/java/com/liferay/trash/web/portlet/TrashPortlet.java
@@ -59,7 +59,6 @@ import org.osgi.service.component.annotations.Reference;
 	property = {
 		"com.liferay.portlet.css-class-wrapper=portlet-trash",
 		"com.liferay.portlet.display-category=category.hidden",
-		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.icon=/icons/trash.png",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
