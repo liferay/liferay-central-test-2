6ae0ddd4354e5cf6e1ace659d656e61b9e276f6f
Michael Han <michael.han@liferay.com>
2016-01-28 09:36:45 -0600

    LPS-62301 move modules

--- modules/apps/portal-settings/portal-settings-authentication-ntlm-web/src/main/java/com/liferay/portal/settings/authentication/ntlm/web/internal/language/resource/bundle/PortalSettingsPortletResourceBundlePublisher.java
+++ modules/apps/portal-settings/portal-settings-authentication-ntlm-web/src/main/java/com/liferay/portal/settings/authentication/ntlm/web/internal/language/resource/bundle/PortalSettingsPortletResourceBundlePublisher.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.security.sso.ntlm.internal.language.resource.bundle;
+package com.liferay.portal.settings.authentication.ntlm.web.internal.language.resource.bundle;
 
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.language.resource.bundle.BaseResourceBundlePublisher;
