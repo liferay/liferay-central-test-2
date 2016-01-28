d9d3407b7399aef21d14520e1465fc24a1b895e7
Michael Han <michael.han@liferay.com>
2016-01-27 20:50:17 -0600

    LPS-62302 move modules

--- modules/apps/portal-settings/portal-settings-authentication-openid-web/src/main/java/com/liferay/portal/settings/authentication/openid/web/internal/portlet/action/PortalSettingsOpenIdFormMVCActionCommand.java
+++ modules/apps/portal-settings/portal-settings-authentication-openid-web/src/main/java/com/liferay/portal/settings/authentication/openid/web/internal/portlet/action/PortalSettingsOpenIdFormMVCActionCommand.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.security.sso.openid.internal.portlet.action;
+package com.liferay.portal.settings.authentication.openid.web.internal.portlet.action;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.security.sso.openid.constants.OpenIdConstants;
