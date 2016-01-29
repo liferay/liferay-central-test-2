36985b32e6667621e335e917b42a0a083d8f2c37
Daniel Song <daniel.song@liferay.com>
2016-01-29 09:39:58 -0600

    LPS-62297 moved to portal-settings

--- modules/apps/portal-settings/portal-settings-authentication-ldap-web/src/main/java/com/liferay/portal/settings/authentication/ldap/web/internal/servlet/taglib/PortalSettingsLDAPAuthenticationDynamicInclude.java
+++ modules/apps/portal-settings/portal-settings-authentication-ldap-web/src/main/java/com/liferay/portal/settings/authentication/ldap/web/internal/servlet/taglib/PortalSettingsLDAPAuthenticationDynamicInclude.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.ldap.internal.portal.settings.web.servlet.taglib;
+package com.liferay.portal.settings.authentication.ldap.web.internal.servlet.taglib;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
