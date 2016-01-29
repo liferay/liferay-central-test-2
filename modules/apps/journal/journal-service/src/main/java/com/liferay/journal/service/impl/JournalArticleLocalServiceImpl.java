7baaf986aa70b971a36a6e11eaeaeaec773d8074
Eduardo Garcia <eduardo.garcia@liferay.com>
2016-01-29 12:08:10 +0100

    LPS-62662 For unchanged document type image fields, store the existing url

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -6485,6 +6485,16 @@ public class JournalArticleLocalServiceImpl
 
 		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(value);
 
+		String data = jsonObject.getString("data");
+
+		if (Validator.isNotNull(data)) {
+			dynamicContentElement.clearContent();
+
+			dynamicContentElement.addCDATA(data);
+
+			return;
+		}
+
 		String uuid = jsonObject.getString("uuid");
 		long groupId = jsonObject.getLong("groupId");
 		boolean tempFile = jsonObject.getBoolean("tempFile");
