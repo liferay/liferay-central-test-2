736a2cffd616a1a4af9bfd58b80413d2f37fcdb6
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-01-28 16:40:38 -0800

    LPS-62674 Use built-in method

--- modules/sdk/gradle-plugins-xml-formatter/src/main/java/com/liferay/gradle/plugins/xml/formatter/XMLFormatterPlugin.java
+++ modules/sdk/gradle-plugins-xml-formatter/src/main/java/com/liferay/gradle/plugins/xml/formatter/XMLFormatterPlugin.java
@@ -20,6 +20,7 @@ import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.DependencySet;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.tasks.TaskContainer;
 
@@ -48,20 +49,19 @@ public class XMLFormatterPlugin implements Plugin<Project> {
 		Configuration configuration = GradleUtil.addConfiguration(
 			project, CONFIGURATION_NAME);
 
-		configuration.setDescription(
-			"Configures Liferay XML Formatter for this project.");
-		configuration.setVisible(false);
-
-		GradleUtil.executeIfEmpty(
-			configuration,
-			new Action<Configuration>() {
+		configuration.defaultDependencies(
+			new Action<DependencySet>() {
 
 				@Override
-				public void execute(Configuration configuration) {
+				public void execute(DependencySet dependencySet) {
 					addDependenciesXMLFormatter(project);
 				}
 
 			});
+
+		configuration.setDescription(
+			"Configures Liferay XML Formatter for this project.");
+		configuration.setVisible(false);
 
 		return configuration;
 	}
