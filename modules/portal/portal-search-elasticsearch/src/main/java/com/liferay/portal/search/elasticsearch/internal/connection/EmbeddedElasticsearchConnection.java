6029743813f46b459182e8f903336af1963e03df
André de Oliveira <andre.oliveira@liferay.com>
2016-01-30 20:21:14 -0300

    LPS-62508 Index creation must be unequivocal, never a side effect
    https://issues.liferay.com/browse/LPS-62508

--- modules/portal/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/EmbeddedElasticsearchConnection.java
+++ modules/portal/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/EmbeddedElasticsearchConnection.java
@@ -213,6 +213,8 @@ public class EmbeddedElasticsearchConnection
 
 	@Override
 	protected void loadRequiredDefaultConfigurations(Settings.Builder builder) {
+		builder.put("action.auto_create_index", false);
+
 		builder.put(
 			"bootstrap.mlockall",
 			elasticsearchConfiguration.bootstrapMlockAll());
