896475ba7b5eeeb2156259377871eab407b9bd3c
Pavel Savinov <pavel.savinov@liferay.com>
2016-02-02 13:41:35 +0100

    LPS-62689 edit web content configuration icon impl

---
+++ modules/apps/journal/journal-content-web/src/main/java/com/liferay/journal/content/web/portlet/configuration/icon/EditJournalArticlePortletConfigurationIconFactory.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.content.web.portlet.configuration.icon;
+
+import com.liferay.journal.content.web.constants.JournalContentPortletKeys;
+import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIconFactory;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIconFactory;
+
+import javax.portlet.PortletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + JournalContentPortletKeys.JOURNAL_CONTENT,
+		"path=-"
+	},
+	service = PortletConfigurationIconFactory.class
+)
+public class EditJournalArticlePortletConfigurationIconFactory
+	extends BasePortletConfigurationIconFactory {
+
+	@Override
+	public PortletConfigurationIcon create(PortletRequest portletRequest) {
+		return new EditJournalArticlePortletConfigurationIcon(portletRequest);
+	}
+
+	@Override
+	public double getWeight() {
+		return 100.0;
+	}
+
+}
