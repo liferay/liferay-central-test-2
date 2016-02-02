a858bcf6325055e59fa465191cacd5441caa22c3
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-02-02 12:07:39 -0800

    LPS-62674 Rename Maven mojo as a verb

--- modules/util/xml-formatter/src/main/java/com/liferay/xml/formatter/maven/FormatXMLMojo.java
+++ modules/util/xml-formatter/src/main/java/com/liferay/xml/formatter/maven/FormatXMLMojo.java
@@ -25,7 +25,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 /**
  * @author Andrea Di Giorgi
  */
-public class XMLFormatterMojo extends AbstractMojo {
+public class FormatXMLMojo extends AbstractMojo {
 
 	@Override
 	public void execute() throws MojoExecutionException {
