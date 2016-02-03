e89d86893a5c46314c916a9a40f4a34af00138bf
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2016-02-03 10:50:14 +0100

    LPS-56604 Default implementations for the new method

--- portal-impl/src/com/liferay/portal/template/DDMTemplateResourceParser.java
+++ portal-impl/src/com/liferay/portal/template/DDMTemplateResourceParser.java
@@ -113,6 +113,18 @@ public class DDMTemplateResourceParser implements TemplateResourceParser {
 		}
 	}
 
+	@Override
+	@SuppressWarnings("deprecation")
+	public boolean isTemplateResourceValid(String templateId, String langType) {
+		if (templateId.contains(TemplateConstants.JOURNAL_SEPARATOR) ||
+			templateId.contains(TemplateConstants.TEMPLATE_SEPARATOR)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		DDMTemplateResourceParser.class);
 
