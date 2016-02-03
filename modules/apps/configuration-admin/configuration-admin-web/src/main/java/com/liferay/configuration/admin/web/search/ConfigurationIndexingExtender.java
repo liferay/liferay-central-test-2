338735bd2c77c46df8a765d3e44fa8ef9b3b3d50
Michael Han <michael.han@liferay.com>
2016-02-03 14:09:04 -0600

    LPS-62210 use RESOLVED

--- modules/apps/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/search/ConfigurationIndexingExtender.java
+++ modules/apps/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/search/ConfigurationIndexingExtender.java
@@ -45,7 +45,7 @@ public class ConfigurationIndexingExtender {
 	@Activate
 	protected void activate(BundleContext bundleContext) {
 		_bundleTracker = new BundleTracker<>(
-			bundleContext, Bundle.ACTIVE,
+			bundleContext, Bundle.RESOLVED,
 			new ConfigurationModelsBundleTrackerCustomizer());
 
 		_bundleTracker.open();
