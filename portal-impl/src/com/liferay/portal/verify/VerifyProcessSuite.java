467cee261d2f4526ce17ef3055d163f84efcffcf
will.newbury <william.newbury@liferay.com>
2016-02-03 09:14:32 -0800

    LPS-61952 Remove com.liferay.portlet.calendar package and remaining usages and tests

--- portal-impl/src/com/liferay/portal/verify/VerifyProcessSuite.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyProcessSuite.java
@@ -37,7 +37,6 @@ public class VerifyProcessSuite extends VerifyProcess {
 		verify(new VerifyAsset());
 		verify(new VerifyAuditedModel());
 		verify(new VerifyBlogs());
-		verify(new VerifyCalendar());
 		verify(new VerifyGroupedModel());
 		verify(new VerifyLayout());
 		verify(new VerifyMessageBoards());
