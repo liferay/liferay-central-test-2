23f8234f10ff9dae07548cae02fa89c6f201ddcf
Rafael Praxedes <rafael.praxedes@liferay.com>
2016-02-04 09:05:03 -0300

    LPS-62558 Fix typo

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-text/src/main/java/com/liferay/dynamic/data/mapping/type/text/TextDDMFormFieldTypeSettings.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-text/src/main/java/com/liferay/dynamic/data/mapping/type/text/TextDDMFormFieldTypeSettings.java
@@ -78,7 +78,7 @@ public interface TextDDMFormFieldTypeSettings
 		dataType = "string", label = "%placeholder-text",
 		properties = {
 			"placeholder=%enter-placeholder-text",
-			"tooltip=%enter-text-that-assists-the-user-but-is-not-submitted-as-a-field-vaue"
+			"tooltip=%enter-text-that-assists-the-user-but-is-not-submitted-as-a-field-value"
 		},
 		type = "text"
 	)
