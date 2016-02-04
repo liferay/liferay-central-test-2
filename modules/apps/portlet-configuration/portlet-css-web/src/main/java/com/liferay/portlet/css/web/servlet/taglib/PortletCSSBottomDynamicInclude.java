43c3491b3bc7379647b8ca53bb4b0fa9c29102f4
Julio Camarero <julio.camarero@liferay.com>
2016-02-04 16:20:45 +0100

    LPS-62910 move module (copy/paste)

--- modules/apps/portlet-configuration/portlet-css-web/src/main/java/com/liferay/portlet/css/web/servlet/taglib/PortletCSSBottomDynamicInclude.java
+++ modules/apps/portlet-configuration/portlet-css-web/src/main/java/com/liferay/portlet/css/web/servlet/taglib/PortletCSSBottomDynamicInclude.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portlet.css.web.servlet.taglib;
+package main.java.com.liferay.portlet.css.web.servlet.taglib;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
