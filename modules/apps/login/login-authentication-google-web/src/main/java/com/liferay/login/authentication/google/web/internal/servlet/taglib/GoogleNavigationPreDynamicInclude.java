28a16183396951d9cf1aa1a792336aa96447964a
Daniel Song <daniel.song@liferay.com>
2016-02-05 09:49:54 -0600

    LPS-62300 move to login

--- modules/apps/login/login-authentication-google-web/src/main/java/com/liferay/login/authentication/google/web/internal/servlet/taglib/GoogleNavigationPreDynamicInclude.java
+++ modules/apps/login/login-authentication-google-web/src/main/java/com/liferay/login/authentication/google/web/internal/servlet/taglib/GoogleNavigationPreDynamicInclude.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.login.authentication.google.internal.servlet.taglib;
+package com.liferay.login.authentication.google.web.internal.servlet.taglib;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
