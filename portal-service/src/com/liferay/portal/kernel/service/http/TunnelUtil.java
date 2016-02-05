ab42a862f1c102101034b17367ef757c3ca96668
shuyangzhou <shuyang.zhou@liferay.com>
2016-02-04 17:48:12 -0800

    LPS-61952 Move com.liferay.portal.service.http to com.liferay.portal.kernel.service.http

--- portal-service/src/com/liferay/portal/kernel/service/http/TunnelUtil.java
+++ portal-service/src/com/liferay/portal/kernel/service/http/TunnelUtil.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.service.http;
+package com.liferay.portal.kernel.service.http;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
