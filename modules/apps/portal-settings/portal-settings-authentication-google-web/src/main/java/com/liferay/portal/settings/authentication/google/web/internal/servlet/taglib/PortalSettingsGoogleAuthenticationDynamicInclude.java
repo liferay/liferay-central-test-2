dbe23a910cc17a94a4f31d20f6111ea03c2913c4
Daniel Song <daniel.song@liferay.com>
2016-02-05 09:10:01 -0600

    LPS-62300 move to portal-settings

--- modules/apps/portal-settings/portal-settings-authentication-google-web/src/main/java/com/liferay/portal/settings/authentication/google/web/internal/servlet/taglib/PortalSettingsGoogleAuthenticationDynamicInclude.java
+++ modules/apps/portal-settings/portal-settings-authentication-google-web/src/main/java/com/liferay/portal/settings/authentication/google/web/internal/servlet/taglib/PortalSettingsGoogleAuthenticationDynamicInclude.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.security.sso.google.internal.servlet.taglib;
+package com.liferay.portal.settings.authentication.google.web.internal.servlet.taglib;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
