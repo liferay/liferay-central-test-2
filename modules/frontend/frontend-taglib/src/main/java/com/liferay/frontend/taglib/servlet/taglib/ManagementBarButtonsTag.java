d3bfa0778403da161602015b7fc1fc4475d92997
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2016-02-05 15:31:42 +0100

    LPS-62701 Move interface out of base

--- modules/frontend/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/ManagementBarButtonsTag.java
+++ modules/frontend/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/ManagementBarButtonsTag.java
@@ -15,7 +15,6 @@
 package com.liferay.frontend.taglib.servlet.taglib;
 
 import com.liferay.frontend.taglib.servlet.ServletContextUtil;
-import com.liferay.frontend.taglib.servlet.taglib.base.BarTag;
 import com.liferay.taglib.util.IncludeTag;
 
 import javax.servlet.jsp.JspException;
