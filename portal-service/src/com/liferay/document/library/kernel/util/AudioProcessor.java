shuyangzhou/liferay-portal

LPS-61952 DL: move non-generated files

portal-service/src/com/liferay/document/library/kernel/util/AudioProcessor.java
@@ -1,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.kernel.util;
+
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.FileVersion;
+import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portlet.exportimport.lar.PortletDataContext;
+
+import java.io.InputStream;
+
+import java.util.Set;
+
+/**
+ * @author Sergio González
+ */
+public interface AudioProcessor {
+
+	public static final String PREVIEW_TYPE = "mp3";
+
+	public void exportGeneratedFiles(
+			PortletDataContext portletDataContext, FileEntry fileEntry,
+			Element fileEntryElement)
+		throws Exception;
+
+	public void generateAudio(
+			FileVersion sourceFileVersion, FileVersion destinationFileVersion)
+		throws Exception;
+
+	public Set<String> getAudioMimeTypes();
+
+	public InputStream getPreviewAsStream(FileVersion fileVersion, String type)
+		throws Exception;
+
+	public long getPreviewFileSize(FileVersion fileVersion, String type)
+		throws Exception;
+
+	public boolean hasAudio(FileVersion fileVersion);
+
+	public void importGeneratedFiles(
+			PortletDataContext portletDataContext, FileEntry fileEntry,
+			FileEntry importedFileEntry, Element fileEntryElement)
+		throws Exception;
+
+	public boolean isAudioSupported(FileVersion fileVersion);
+
+	public boolean isAudioSupported(String mimeType);
+
+	public boolean isSupported(String mimeType);
+
+	public void trigger(
+		FileVersion sourceFileVersion, FileVersion destinationFileVersion);
+
+}

portal-service/src/com/liferay/document/library/kernel/util/PDFProcessor.java
@@ -1,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.kernel.util;
+
+import com.liferay.portal.kernel.image.ImageTool;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.FileVersion;
+import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portlet.exportimport.lar.PortletDataContext;
+
+import java.io.InputStream;
+
+/**
+ * @author Sergio González
+ */
+public interface PDFProcessor {
+
+	public static final String PREVIEW_TYPE = ImageTool.TYPE_PNG;
+
+	public static final String THUMBNAIL_TYPE = ImageTool.TYPE_PNG;
+
+	public void exportGeneratedFiles(
+			PortletDataContext portletDataContext, FileEntry fileEntry,
+			Element fileEntryElement)
+		throws Exception;
+
+	public void generateImages(
+			FileVersion sourceFileVersion, FileVersion destinationFileVersion)
+		throws Exception;
+
+	public InputStream getPreviewAsStream(FileVersion fileVersion, int index)
+		throws Exception;
+
+	public int getPreviewFileCount(FileVersion fileVersion);
+
+	public long getPreviewFileSize(FileVersion fileVersion, int index)
+		throws Exception;
+
+	public InputStream getThumbnailAsStream(FileVersion fileVersion, int index)
+		throws Exception;
+
+	public long getThumbnailFileSize(FileVersion fileVersion, int index)
+		throws Exception;
+
+	public boolean hasImages(FileVersion fileVersion);
+
+	public void importGeneratedFiles(
+			PortletDataContext portletDataContext, FileEntry fileEntry,
+			FileEntry importedFileEntry, Element fileEntryElement)
+		throws Exception;
+
+	public boolean isDocumentSupported(FileVersion fileVersion);
+
+	public boolean isDocumentSupported(String mimeType);
+
+	public boolean isSupported(String mimeType);
+
+	public void trigger(
+		FileVersion sourceFileVersion, FileVersion destinationFileVersion);
+
+}

portal-service/src/com/liferay/document/library/kernel/util/VideoProcessor.java
@@ -1,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.kernel.util;
+
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.FileVersion;
+import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portlet.exportimport.lar.PortletDataContext;
+
+import java.io.InputStream;
+
+import java.util.Set;
+
+/**
+ * @author Sergio González
+ */
+public interface VideoProcessor {
+
+	public static final String THUMBNAIL_TYPE = "jpg";
+
+	public void exportGeneratedFiles(
+			PortletDataContext portletDataContext, FileEntry fileEntry,
+			Element fileEntryElement)
+		throws Exception;
+
+	public void generateVideo(
+			FileVersion sourceFileVersion, FileVersion destinationFileVersion)
+		throws Exception;
+
+	public InputStream getPreviewAsStream(FileVersion fileVersion, String type)
+		throws Exception;
+
+	public long getPreviewFileSize(FileVersion fileVersion, String type)
+		throws Exception;
+
+	public InputStream getThumbnailAsStream(FileVersion fileVersion, int index)
+		throws Exception;
+
+	public long getThumbnailFileSize(FileVersion fileVersion, int index)
+		throws Exception;
+
+	public Set<String> getVideoMimeTypes();
+
+	public boolean hasVideo(FileVersion fileVersion);
+
+	public void importGeneratedFiles(
+			PortletDataContext portletDataContext, FileEntry fileEntry,
+			FileEntry importedFileEntry, Element fileEntryElement)
+		throws Exception;
+
+	public boolean isSupported(String mimeType);
+
+	public boolean isVideoSupported(FileVersion fileVersion);
+
+	public boolean isVideoSupported(String mimeType);
+
+	public void trigger(
+		FileVersion sourceFileVersion, FileVersion destinationFileVersion);
+
+}

liferay/liferay-central-test-2

portal-service/src/com/liferay/document/library/kernel/util/AudioProcessor.java
@@ -1,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.kernel.util;
+
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.FileVersion;
+import com.liferay.portal.kernel.xml.Element;
+
+import java.io.InputStream;
+
+import java.util.Set;
+
+/**
+ * @author Sergio González
+ */
+public interface AudioProcessor {
+
+	public static final String PREVIEW_TYPE = "mp3";
+
+	public void exportGeneratedFiles(
+			PortletDataContext portletDataContext, FileEntry fileEntry,
+			Element fileEntryElement)
+		throws Exception;
+
+	public void generateAudio(
+			FileVersion sourceFileVersion, FileVersion destinationFileVersion)
+		throws Exception;
+
+	public Set<String> getAudioMimeTypes();
+
+	public InputStream getPreviewAsStream(FileVersion fileVersion, String type)
+		throws Exception;
+
+	public long getPreviewFileSize(FileVersion fileVersion, String type)
+		throws Exception;
+
+	public boolean hasAudio(FileVersion fileVersion);
+
+	public void importGeneratedFiles(
+			PortletDataContext portletDataContext, FileEntry fileEntry,
+			FileEntry importedFileEntry, Element fileEntryElement)
+		throws Exception;
+
+	public boolean isAudioSupported(FileVersion fileVersion);
+
+	public boolean isAudioSupported(String mimeType);
+
+	public boolean isSupported(String mimeType);
+
+	public void trigger(
+		FileVersion sourceFileVersion, FileVersion destinationFileVersion);
+
+}

portal-service/src/com/liferay/document/library/kernel/util/PDFProcessor.java
@@ -1,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.kernel.util;
+
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.image.ImageTool;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.FileVersion;
+import com.liferay.portal.kernel.xml.Element;
+
+import java.io.InputStream;
+
+/**
+ * @author Sergio González
+ */
+public interface PDFProcessor {
+
+	public static final String PREVIEW_TYPE = ImageTool.TYPE_PNG;
+
+	public static final String THUMBNAIL_TYPE = ImageTool.TYPE_PNG;
+
+	public void exportGeneratedFiles(
+			PortletDataContext portletDataContext, FileEntry fileEntry,
+			Element fileEntryElement)
+		throws Exception;
+
+	public void generateImages(
+			FileVersion sourceFileVersion, FileVersion destinationFileVersion)
+		throws Exception;
+
+	public InputStream getPreviewAsStream(FileVersion fileVersion, int index)
+		throws Exception;
+
+	public int getPreviewFileCount(FileVersion fileVersion);
+
+	public long getPreviewFileSize(FileVersion fileVersion, int index)
+		throws Exception;
+
+	public InputStream getThumbnailAsStream(FileVersion fileVersion, int index)
+		throws Exception;
+
+	public long getThumbnailFileSize(FileVersion fileVersion, int index)
+		throws Exception;
+
+	public boolean hasImages(FileVersion fileVersion);
+
+	public void importGeneratedFiles(
+			PortletDataContext portletDataContext, FileEntry fileEntry,
+			FileEntry importedFileEntry, Element fileEntryElement)
+		throws Exception;
+
+	public boolean isDocumentSupported(FileVersion fileVersion);
+
+	public boolean isDocumentSupported(String mimeType);
+
+	public boolean isSupported(String mimeType);
+
+	public void trigger(
+		FileVersion sourceFileVersion, FileVersion destinationFileVersion);
+
+}

portal-service/src/com/liferay/document/library/kernel/util/VideoProcessor.java
@@ -1,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.kernel.util;
+
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.FileVersion;
+import com.liferay.portal.kernel.xml.Element;
+
+import java.io.InputStream;
+
+import java.util.Set;
+
+/**
+ * @author Sergio González
+ */
+public interface VideoProcessor {
+
+	public static final String THUMBNAIL_TYPE = "jpg";
+
+	public void exportGeneratedFiles(
+			PortletDataContext portletDataContext, FileEntry fileEntry,
+			Element fileEntryElement)
+		throws Exception;
+
+	public void generateVideo(
+			FileVersion sourceFileVersion, FileVersion destinationFileVersion)
+		throws Exception;
+
+	public InputStream getPreviewAsStream(FileVersion fileVersion, String type)
+		throws Exception;
+
+	public long getPreviewFileSize(FileVersion fileVersion, String type)
+		throws Exception;
+
+	public InputStream getThumbnailAsStream(FileVersion fileVersion, int index)
+		throws Exception;
+
+	public long getThumbnailFileSize(FileVersion fileVersion, int index)
+		throws Exception;
+
+	public Set<String> getVideoMimeTypes();
+
+	public boolean hasVideo(FileVersion fileVersion);
+
+	public void importGeneratedFiles(
+			PortletDataContext portletDataContext, FileEntry fileEntry,
+			FileEntry importedFileEntry, Element fileEntryElement)
+		throws Exception;
+
+	public boolean isSupported(String mimeType);
+
+	public boolean isVideoSupported(FileVersion fileVersion);
+
+	public boolean isVideoSupported(String mimeType);
+
+	public void trigger(
+		FileVersion sourceFileVersion, FileVersion destinationFileVersion);
+
+}