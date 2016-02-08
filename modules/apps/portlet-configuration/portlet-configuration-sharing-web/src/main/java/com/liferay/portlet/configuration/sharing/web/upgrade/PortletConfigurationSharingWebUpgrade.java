0afe363af699848be076115277866fccb9fd09d3
Julio Camarero <julio.camarero@liferay.com>
2016-02-08 13:24:47 +0100

    LPS-62959 rename upgrade

--- modules/apps/portlet-configuration/portlet-configuration-sharing-web/src/main/java/com/liferay/portlet/configuration/sharing/web/upgrade/PortletConfigurationSharingWebUpgrade.java
+++ modules/apps/portlet-configuration/portlet-configuration-sharing-web/src/main/java/com/liferay/portlet/configuration/sharing/web/upgrade/PortletConfigurationSharingWebUpgrade.java
@@ -29,11 +29,11 @@ public class PortletConfigurationSharingWebUpgrade implements UpgradeStepRegistr
 	@Override
 	public void register(Registry registry) {
 		registry.register(
-			"com.liferay.portlet.sharing.web", "0.0.0", "1.0.0",
+			"com.liferay.portlet.configuration.sharing.web", "0.0.0", "1.0.0",
 			new DummyUpgradeStep());
 
 		registry.register(
-			"com.liferay.portlet.sharing.web", "0.0.1", "1.0.0",
+			"com.liferay.portlet.configuration.sharing.web", "0.0.1", "1.0.0",
 			new UpgradePortletId());
 	}
 
