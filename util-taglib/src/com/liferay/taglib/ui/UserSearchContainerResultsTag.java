e9daed3b728b4c6ccad95047adb2ec8dc094973f
Brian Chan <brian.chan@liferay.com>
2016-02-08 14:39:41 -0800

    LPS-61003 not needed

--- util-taglib/src/com/liferay/taglib/ui/UserSearchContainerResultsTag.java
+++ util-taglib/src/com/liferay/taglib/ui/UserSearchContainerResultsTag.java
@@ -90,7 +90,7 @@ public class UserSearchContainerResultsTag<R> extends IncludeTag {
 
 	private SearchContainer<R> _searchContainer;
 	private DisplayTerms _searchTerms;
-	private boolean _useIndexer = false;
+	private boolean _useIndexer;
 	private LinkedHashMap<String, Object> _userParams;
 
 }
