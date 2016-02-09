68c90adcb7e9820c04cc57776d8a99a609d68ac9
drewbrokke <drew.brokke@liferay.com>
2016-02-08 16:47:13 -0800

    LPS-63476 contacts-web - registers ContactsConfigurationAction

---
+++ modules/apps/foundation/contacts/contacts-web/src/main/java/com/liferay/contacts/web/portlet/action/ContactsConfigurationAction.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.contacts.web.portlet.action;
+
+import com.liferay.contacts.web.constants.ContactsPortletKeys;
+import com.liferay.portal.kernel.portlet.ConfigurationAction;
+import com.liferay.portal.kernel.portlet.DefaultConfigurationAction;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Drew Brokke
+ */
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + ContactsPortletKeys.CONTACTS_CENTER},
+	service = ConfigurationAction.class
+)
+public class ContactsConfigurationAction extends DefaultConfigurationAction {
+}
