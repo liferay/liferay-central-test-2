stian-sigvartsen/liferay-portal

LPS-62396 Migrate from PrefsPropsUtil to settings API

modules/portal-security/portal-security-sso-google/src/main/java/com/liferay/portal/security/sso/google/internal/portlet/action/GoogleLoginAction.java
@@ -19,13 +19,11 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
 import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
-import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.oauth2.Oauth2;
 import com.google.api.services.oauth2.model.Userinfoplus;
 
-import com.liferay.portal.kernel.deploy.DeployManagerUtil;
 import com.liferay.portal.kernel.portlet.LiferayWindowState;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.struts.BaseStrutsAction;
@@ -34,7 +32,6 @@ import com.liferay.portal.kernel.util.CalendarFactoryUtil;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.PrefsPropsUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
@@ -43,6 +40,7 @@ import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.model.Contact;
 import com.liferay.portal.model.User;
 import com.liferay.portal.model.UserGroupRole;
+import com.liferay.portal.security.sso.google.GoogleAuthorization;
 import com.liferay.portal.security.sso.google.constants.GoogleWebKeys;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.service.UserLocalService;
@@ -70,6 +68,7 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Sergio González
  * @author Federico Budassi
+ * @author Stian Sigvartsen
  */
 @Component(
 	immediate = true, property = {"path=/portal/google_login"},
@@ -85,11 +84,18 @@ public class GoogleLoginAction extends BaseStrutsAction {
 		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
+		if (!_googleAuthorization.isEnabled(themeDisplay.getCompanyId())) {
+			throw new PrincipalException.MustBeEnabled(
+				themeDisplay.getCompanyId(),
+				GoogleAuthorization.class.getName());
+		}
+
 		String cmd = ParamUtil.getString(request, Constants.CMD);
 
 		if (cmd.equals("login")) {
 			GoogleAuthorizationCodeFlow googleAuthorizationCodeFlow =
-				getGoogleAuthorizationCodeFlow(themeDisplay.getCompanyId());
+				getGoogleLoginAuthorizationCodeFlow(
+					themeDisplay.getCompanyId());
 
 			GoogleAuthorizationCodeRequestUrl
 				googleAuthorizationCodeRequestUrl =
@@ -195,7 +201,7 @@ public class GoogleLoginAction extends BaseStrutsAction {
 		throws Exception {
 
 		GoogleAuthorizationCodeFlow googleAuthorizationCodeFlow =
-			getGoogleAuthorizationCodeFlow(companyId);
+			getGoogleLoginAuthorizationCodeFlow(companyId);
 
 		GoogleAuthorizationCodeTokenRequest
 			googleAuthorizationCodeTokenRequest =
@@ -210,40 +216,13 @@ public class GoogleLoginAction extends BaseStrutsAction {
 			googleTokenResponse, null);
 	}
 
-	protected GoogleAuthorizationCodeFlow getGoogleAuthorizationCodeFlow(
+	protected GoogleAuthorizationCodeFlow getGoogleLoginAuthorizationCodeFlow(
 			long companyId)
 		throws Exception {
 
-		HttpTransport httpTransport = new NetHttpTransport();
-		JacksonFactory jsonFactory = new JacksonFactory();
-		String googleClientId = PrefsPropsUtil.getString(
-			companyId, "google-client-id");
-		String googleClientSecret = PrefsPropsUtil.getString(
-			companyId, "google-client-secret");
-
-		List<String> scopes = null;
-
-		if (DeployManagerUtil.isDeployed(_GOOGLE_DRIVE_CONTEXT)) {
-			scopes = _SCOPES_DRIVE;
-		}
-		else {
-			scopes = _SCOPES_LOGIN;
-		}
-
-		GoogleAuthorizationCodeFlow.Builder builder =
-			new GoogleAuthorizationCodeFlow.Builder(
-				httpTransport, jsonFactory, googleClientId, googleClientSecret,
-				scopes);
-
-		String accessType = "online";
-
-		if (DeployManagerUtil.isDeployed(_GOOGLE_DRIVE_CONTEXT)) {
-			accessType = "offline";
-		}
-
-		builder.setAccessType(accessType);
-
-		return builder.build();
+		List<String> scopes = _SCOPES_LOGIN;
+		return _googleAuthorization.getGoogleAuthorizationCodeFlow(
+			companyId, scopes);
 	}
 
 	protected String getRedirectURI(HttpServletRequest request) {
@@ -395,6 +374,13 @@ public class GoogleLoginAction extends BaseStrutsAction {
 	}
 
 	@Reference(unbind = "-")
+	protected void setGoogleAuthorization(
+		GoogleAuthorization googleAuthorization) {
+
+		_googleAuthorization = googleAuthorization;
+	}
+
+	@Reference(unbind = "-")
 	protected void setUserLocalService(UserLocalService userLocalService) {
 		_userLocalService = userLocalService;
 	}
@@ -467,6 +453,7 @@ public class GoogleLoginAction extends BaseStrutsAction {
 	private static final List<String> _SCOPES_LOGIN = Arrays.asList(
 		"email", "profile");
 
+	private GoogleAuthorization _googleAuthorization;
 	private UserLocalService _userLocalService;
 
 }

modules/portal-security/portal-security-sso-google/src/main/java/com/liferay/portal/security/sso/google/internal/portlet/action/PortalSettingsGoogleFormMVCActionCommand.java
@@ -1,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.sso.google.internal.portlet.action;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.security.sso.google.constants.GoogleConstants;
+import com.liferay.portal.settings.portlet.action.BasePortalSettingsFormMVCActionCommand;
+import com.liferay.portal.settings.web.constants.PortalSettingsPortletKeys;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Stian Sigvartsen
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + PortalSettingsPortletKeys.PORTAL_SETTINGS,
+		"mvc.command.name=/portal_settings/google"
+	},
+	service = MVCActionCommand.class
+)
+public class PortalSettingsGoogleFormMVCActionCommand
+	extends BasePortalSettingsFormMVCActionCommand {
+
+	@Override
+	protected void doValidateForm(
+		ActionRequest actionRequest, ActionResponse actionResponse) {
+
+		boolean googleEnabled = getBoolean(actionRequest, "enabled");
+
+		if (!googleEnabled) {
+			return;
+		}
+
+		String googleClientId = getString(actionRequest, "googleClientId");
+		String googleClientSecret = getString(
+			actionRequest, "googleClientSecret");
+
+		if (Validator.isNull(googleClientId)) {
+			SessionErrors.add(actionRequest, "googleClientIdInvalid");
+		}
+
+		if (Validator.isNull(googleClientSecret)) {
+			SessionErrors.add(actionRequest, "googleClientSecretInvalid");
+		}
+	}
+
+	@Override
+	protected String getParameterNamespace() {
+		return "google--";
+	}
+
+	@Override
+	protected String getSettingsId() {
+		return GoogleConstants.SERVICE_NAME;
+	}
+
+}

modules/portal-security/portal-security-sso-google/src/main/resources/META-INF/resources/com.liferay.portal.settings.web/google.jsp
@@ -17,15 +17,22 @@
 <%@ include file="/com.liferay.portal.settings.web/init.jsp" %>
 
 <%
-boolean googleAuthEnabled = PrefsPropsUtil.getBoolean(company.getCompanyId(), "google-auth-enabled", true);
-String googleClientId = PrefsPropsUtil.getString(company.getCompanyId(), "google-client-id");
-String googleClientSecret = PrefsPropsUtil.getString(company.getCompanyId(), "google-client-secret");
+GoogleAuthorizationConfiguration googleAuthorizationConfiguration = ConfigurationFactoryUtil.getConfiguration(GoogleAuthorizationConfiguration.class, new ParameterMapSettingsLocator(request.getParameterMap(), "google--", new CompanyServiceSettingsLocator(company.getCompanyId(), GoogleConstants.SERVICE_NAME)));
+
+boolean googleAuthEnabled = googleAuthorizationConfiguration.enabled();
+String googleClientId = googleAuthorizationConfiguration.googleClientId();
+String googleClientSecret = googleAuthorizationConfiguration.googleClientSecret();
 %>
 
+<liferay-ui:error key="googleClientIdInvalid" message="the-google-client-id-is-invalid" />
+<liferay-ui:error key="googleClientSecretInvalid" message="the-google-client-secret-is-invalid" />
+
 <aui:fieldset>
-	<aui:input label="enabled" name='<%= "settings--google-auth-enabled--" %>' type="checkbox" value="<%= googleAuthEnabled %>" />
+	<aui:input name="<%= ActionRequest.ACTION_NAME %>" type="hidden" value="/portal_settings/google" />
 
-	<aui:input label="google-client-id" name='<%= "settings--google-client-id--" %>' type="text" value="<%= googleClientId %>" wrapperCssClass="lfr-input-text-container" />
+	<aui:input label="enabled" name="google--enabled" type="checkbox" value="<%= googleAuthEnabled %>" />
 
-	<aui:input label="google-client-secret" name='<%= "settings--google-client-secret--" %>' type="text" value="<%= googleClientSecret %>" wrapperCssClass="lfr-input-text-container" />
+	<aui:input label="google-client-id" name="google--googleClientId" type="text" value="<%= googleClientId %>" wrapperCssClass="lfr-input-text-container" />
+
+	<aui:input label="google-client-secret" name="google--googleClientSecret" type="text" value="<%= googleClientSecret %>" wrapperCssClass="lfr-input-text-container" />
 </aui:fieldset>

liferay/liferay-central-test-2

modules/portal-security/portal-security-sso-google/src/main/java/com/liferay/portal/security/sso/google/internal/portlet/action/GoogleLoginAction.java
@@ -19,7 +19,6 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
 import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
-import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.oauth2.Oauth2;
@@ -42,12 +41,12 @@ import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.PortletKeys;
-import com.liferay.portal.kernel.util.PrefsPropsUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.security.sso.google.GoogleAuthorization;
 import com.liferay.portal.security.sso.google.constants.GoogleWebKeys;
 
 import java.util.Arrays;
@@ -69,6 +68,7 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Sergio González
  * @author Federico Budassi
+ * @author Stian Sigvartsen
  */
 @Component(
 	immediate = true, property = {"path=/portal/google_login"},
@@ -84,11 +84,18 @@ public class GoogleLoginAction extends BaseStrutsAction {
 		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
+		if (!_googleAuthorization.isEnabled(themeDisplay.getCompanyId())) {
+			throw new PrincipalException.MustBeEnabled(
+				themeDisplay.getCompanyId(),
+				GoogleAuthorization.class.getName());
+		}
+
 		String cmd = ParamUtil.getString(request, Constants.CMD);
 
 		if (cmd.equals("login")) {
 			GoogleAuthorizationCodeFlow googleAuthorizationCodeFlow =
-				getGoogleAuthorizationCodeFlow(themeDisplay.getCompanyId());
+				getGoogleLoginAuthorizationCodeFlow(
+					themeDisplay.getCompanyId());
 
 			GoogleAuthorizationCodeRequestUrl
 				googleAuthorizationCodeRequestUrl =
@@ -197,7 +204,7 @@ public class GoogleLoginAction extends BaseStrutsAction {
 		throws Exception {
 
 		GoogleAuthorizationCodeFlow googleAuthorizationCodeFlow =
-			getGoogleAuthorizationCodeFlow(companyId);
+			getGoogleLoginAuthorizationCodeFlow(companyId);
 
 		GoogleAuthorizationCodeTokenRequest
 			googleAuthorizationCodeTokenRequest =
@@ -212,29 +219,12 @@ public class GoogleLoginAction extends BaseStrutsAction {
 			googleTokenResponse, null);
 	}
 
-	protected GoogleAuthorizationCodeFlow getGoogleAuthorizationCodeFlow(
+	protected GoogleAuthorizationCodeFlow getGoogleLoginAuthorizationCodeFlow(
 			long companyId)
 		throws Exception {
 
-		HttpTransport httpTransport = new NetHttpTransport();
-		JacksonFactory jsonFactory = new JacksonFactory();
-		String googleClientId = PrefsPropsUtil.getString(
-			companyId, "google-client-id");
-		String googleClientSecret = PrefsPropsUtil.getString(
-			companyId, "google-client-secret");
-
-		List<String> scopes = _SCOPES_LOGIN;
-
-		GoogleAuthorizationCodeFlow.Builder builder =
-			new GoogleAuthorizationCodeFlow.Builder(
-				httpTransport, jsonFactory, googleClientId, googleClientSecret,
-				scopes);
-
-		String accessType = "online";
-
-		builder.setAccessType(accessType);
-
-		return builder.build();
+		return _googleAuthorization.getGoogleAuthorizationCodeFlow(
+			companyId, _SCOPES_LOGIN);
 	}
 
 	protected String getRedirectURI(HttpServletRequest request) {
@@ -387,6 +377,13 @@ public class GoogleLoginAction extends BaseStrutsAction {
 	}
 
 	@Reference(unbind = "-")
+	protected void setGoogleAuthorization(
+		GoogleAuthorization googleAuthorization) {
+
+		_googleAuthorization = googleAuthorization;
+	}
+
+	@Reference(unbind = "-")
 	protected void setUserLocalService(UserLocalService userLocalService) {
 		_userLocalService = userLocalService;
 	}
@@ -461,6 +458,7 @@ public class GoogleLoginAction extends BaseStrutsAction {
 	private static final List<String> _SCOPES_LOGIN = Arrays.asList(
 		"email", "profile");
 
+	private GoogleAuthorization _googleAuthorization;
 	private UserLocalService _userLocalService;
 
 }

modules/portal-security/portal-security-sso-google/src/main/java/com/liferay/portal/security/sso/google/internal/portlet/action/PortalSettingsGoogleFormMVCActionCommand.java
@@ -1,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.sso.google.internal.portlet.action;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.security.sso.google.constants.GoogleConstants;
+import com.liferay.portal.settings.portlet.action.BasePortalSettingsFormMVCActionCommand;
+import com.liferay.portal.settings.web.constants.PortalSettingsPortletKeys;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Stian Sigvartsen
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + PortalSettingsPortletKeys.PORTAL_SETTINGS,
+		"mvc.command.name=/portal_settings/google"
+	},
+	service = MVCActionCommand.class
+)
+public class PortalSettingsGoogleFormMVCActionCommand
+	extends BasePortalSettingsFormMVCActionCommand {
+
+	@Override
+	protected void doValidateForm(
+		ActionRequest actionRequest, ActionResponse actionResponse) {
+
+		boolean googleEnabled = getBoolean(actionRequest, "enabled");
+
+		if (!googleEnabled) {
+			return;
+		}
+
+		String googleClientId = getString(actionRequest, "clientId");
+		String googleClientSecret = getString(actionRequest, "clientSecret");
+
+		if (Validator.isNull(googleClientId)) {
+			SessionErrors.add(actionRequest, "googleClientIdInvalid");
+		}
+
+		if (Validator.isNull(googleClientSecret)) {
+			SessionErrors.add(actionRequest, "googleClientSecretInvalid");
+		}
+	}
+
+	@Override
+	protected String getParameterNamespace() {
+		return "google--";
+	}
+
+	@Override
+	protected String getSettingsId() {
+		return GoogleConstants.SERVICE_NAME;
+	}
+
+}

modules/portal-security/portal-security-sso-google/src/main/resources/META-INF/resources/com.liferay.portal.settings.web/google.jsp
@@ -17,15 +17,22 @@
 <%@ include file="/com.liferay.portal.settings.web/init.jsp" %>
 
 <%
-boolean googleAuthEnabled = PrefsPropsUtil.getBoolean(company.getCompanyId(), "google-auth-enabled", true);
-String googleClientId = PrefsPropsUtil.getString(company.getCompanyId(), "google-client-id");
-String googleClientSecret = PrefsPropsUtil.getString(company.getCompanyId(), "google-client-secret");
+GoogleAuthorizationConfiguration googleAuthorizationConfiguration = ConfigurationFactoryUtil.getConfiguration(GoogleAuthorizationConfiguration.class, new ParameterMapSettingsLocator(request.getParameterMap(), "google--", new CompanyServiceSettingsLocator(company.getCompanyId(), GoogleConstants.SERVICE_NAME)));
+
+boolean googleAuthEnabled = googleAuthorizationConfiguration.enabled();
+String googleClientId = googleAuthorizationConfiguration.clientId();
+String googleClientSecret = googleAuthorizationConfiguration.clientSecret();
 %>
 
+<liferay-ui:error key="googleClientIdInvalid" message="the-google-client-id-is-invalid" />
+<liferay-ui:error key="googleClientSecretInvalid" message="the-google-client-secret-is-invalid" />
+
 <aui:fieldset>
-	<aui:input label="enabled" name='<%= "settings--google-auth-enabled--" %>' type="checkbox" value="<%= googleAuthEnabled %>" />
+	<aui:input name="<%= ActionRequest.ACTION_NAME %>" type="hidden" value="/portal_settings/google" />
 
-	<aui:input label="google-client-id" name='<%= "settings--google-client-id--" %>' type="text" value="<%= googleClientId %>" wrapperCssClass="lfr-input-text-container" />
+	<aui:input label="enabled" name="google--enabled" type="checkbox" value="<%= googleAuthEnabled %>" />
 
-	<aui:input label="google-client-secret" name='<%= "settings--google-client-secret--" %>' type="text" value="<%= googleClientSecret %>" wrapperCssClass="lfr-input-text-container" />
+	<aui:input label="google-client-id" name="google--clientId" type="text" value="<%= googleClientId %>" wrapperCssClass="lfr-input-text-container" />
+
+	<aui:input label="google-client-secret" name="google--clientSecret" type="text" value="<%= googleClientSecret %>" wrapperCssClass="lfr-input-text-container" />
 </aui:fieldset>