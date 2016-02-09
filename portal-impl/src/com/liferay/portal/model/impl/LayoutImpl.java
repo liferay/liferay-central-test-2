c8034d786bda785223bded2cdb0a95f66b022c77
Julio Camarero <julio.camarero@liferay.com>
2016-02-09 13:37:05 +0100

    LPS-63037 add a new method to find out if a page is customizable (based on a new setting)

--- portal-impl/src/com/liferay/portal/model/impl/LayoutImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/LayoutImpl.java
@@ -903,6 +903,29 @@ public class LayoutImpl extends LayoutBaseImpl {
 		return false;
 	}
 
+	@Override
+	public boolean isCustomizable() {
+		if (!isTypePortlet()) {
+			return false;
+		}
+
+		boolean layoutCustomizable = GetterUtil.getBoolean(
+			getTypeSettingsProperty(LayoutConstants.CUSTOMIZABLE_LAYOUT));
+
+		if (layoutCustomizable) {
+			return true;
+		}
+
+		LayoutTypePortlet layoutTypePortlet =
+			(LayoutTypePortlet)getLayoutType();
+
+		if (layoutTypePortlet.isCustomizable()) {
+			return true;
+		}
+
+		return false;
+	}
+
 	/**
 	 * Returns <code>true</code> if the current layout is the first layout in
 	 * its parent's hierarchical list of children layouts.
