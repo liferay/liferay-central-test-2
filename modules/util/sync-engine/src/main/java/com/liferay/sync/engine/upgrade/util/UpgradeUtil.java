416a84777c5ffcc9565158478cde0eb9fbd2f7d2
Shinn Lok <shinn.lok@liferay.com>
2016-02-08 16:41:09 -0800

    SYNC-1546 Next version will be a feature release

--- modules/util/sync-engine/src/main/java/com/liferay/sync/engine/upgrade/util/UpgradeUtil.java
+++ modules/util/sync-engine/src/main/java/com/liferay/sync/engine/upgrade/util/UpgradeUtil.java
@@ -29,11 +29,11 @@ import com.liferay.sync.engine.service.persistence.SyncWatchEventPersistence;
 import com.liferay.sync.engine.upgrade.UpgradeProcess;
 import com.liferay.sync.engine.upgrade.v3_0_10.UpgradeProcess_3_0_10;
 import com.liferay.sync.engine.upgrade.v3_0_11.UpgradeProcess_3_0_11;
-import com.liferay.sync.engine.upgrade.v3_0_13.UpgradeProcess_3_0_13;
 import com.liferay.sync.engine.upgrade.v3_0_4.UpgradeProcess_3_0_4;
 import com.liferay.sync.engine.upgrade.v3_0_5.UpgradeProcess_3_0_5;
 import com.liferay.sync.engine.upgrade.v3_0_8.UpgradeProcess_3_0_8;
 import com.liferay.sync.engine.upgrade.v3_0_9.UpgradeProcess_3_0_9;
+import com.liferay.sync.engine.upgrade.v3_1_0.UpgradeProcess_3_1_0;
 import com.liferay.sync.engine.util.LoggerUtil;
 import com.liferay.sync.engine.util.PropsValues;
 import com.liferay.sync.engine.util.ReleaseInfo;
@@ -106,7 +106,7 @@ public class UpgradeUtil {
 		upgradeProcesses.add(new UpgradeProcess_3_0_9());
 		upgradeProcesses.add(new UpgradeProcess_3_0_10());
 		upgradeProcesses.add(new UpgradeProcess_3_0_11());
-		upgradeProcesses.add(new UpgradeProcess_3_0_13());
+		upgradeProcesses.add(new UpgradeProcess_3_1_0());
 
 		for (UpgradeProcess upgradeProcess : upgradeProcesses) {
 			if (buildNumber < upgradeProcess.getThreshold()) {
