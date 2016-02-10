ab9f0f56b985ba85bc1b8b393cc95df4ec12d73e
Brian Chan <brian.chan@liferay.com>
2016-02-10 15:42:02 -0800

    LPS-62825 SF

--- modules/apps/message-boards/message-boards-web/src/main/java/com/liferay/message/boards/web/display/context/DefaultMBListDisplayContext.java
+++ modules/apps/message-boards/message-boards-web/src/main/java/com/liferay/message/boards/web/display/context/DefaultMBListDisplayContext.java
@@ -143,12 +143,13 @@ public class DefaultMBListDisplayContext implements MBListDisplayContext {
 
 			searchContext.setKeywords(keywords);
 
+			searchContext.setStart(searchContainer.getStart());
+
 			Hits hits = indexer.search(searchContext);
 
 			searchContainer.setResults(
 				SearchResultUtil.getSearchResults(hits, _request.getLocale()));
 
-			searchContext.setStart(searchContainer.getStart());
 			searchContainer.setSearch(true);
 			searchContainer.setTotal(hits.getLength());
 		}
