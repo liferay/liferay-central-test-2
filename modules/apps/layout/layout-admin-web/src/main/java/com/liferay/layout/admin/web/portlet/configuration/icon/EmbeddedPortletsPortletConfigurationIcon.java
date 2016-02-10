ff4732579feb3634d578e5acee44e218816d5b70
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-02-10 15:50:48 +0100

    LPS-62605 Moves embeddedPortlets from FormNavigatorEntry to PorltetConfigurationIcon

---
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/portlet/configuration/icon/EmbeddedPortletsPortletConfigurationIcon.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.admin.web.portlet.configuration.icon;
+
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.model.LayoutTypePortlet;
+import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
+import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringPool;
+
+import java.util.List;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class EmbeddedPortletsPortletConfigurationIcon
+	extends BasePortletConfigurationIcon {
+
+	public EmbeddedPortletsPortletConfigurationIcon(
+		PortletRequest portletRequest, LayoutLocalService layoutLocalService) {
+
+		super(portletRequest);
+
+		_layoutLocalService = layoutLocalService;
+	}
+
+	@Override
+	public String getMessage() {
+		return "embedded-portlets";
+	}
+
+	@Override
+	public String getURL() {
+		try {
+			PortletURL portletURL = PortletURLFactoryUtil.create(
+				portletRequest, portletDisplay.getId(), themeDisplay.getPlid(),
+				PortletRequest.RENDER_PHASE);
+
+			portletURL.setParameter("mvcPath", "/embedded_portlets.jsp");
+			portletURL.setParameter("redirect", themeDisplay.getURLCurrent());
+			portletURL.setParameter("selPlid", String.valueOf(getSelPlid()));
+			portletURL.setWindowState(LiferayWindowState.POP_UP);
+
+			return portletURL.toString();
+		}
+		catch (Exception e) {
+		}
+
+		return StringPool.BLANK;
+	}
+
+	@Override
+	public boolean isShow() {
+		try {
+			Layout layout = getLayout();
+
+			if (layout == null) {
+				return false;
+			}
+
+			if (!layout.isSupportsEmbeddedPortlets()) {
+				return false;
+			}
+
+			LayoutTypePortlet layoutTypePortlet =
+				(LayoutTypePortlet)layout.getLayoutType();
+
+			List<Portlet> embeddedPortlets =
+				layoutTypePortlet.getEmbeddedPortlets();
+
+			if (!embeddedPortlets.isEmpty()) {
+				return true;
+			}
+		}
+		catch (Exception e) {
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean isUseDialog() {
+		return true;
+	}
+
+	protected Layout getLayout() throws Exception {
+		long selPlid = getSelPlid();
+
+		return _layoutLocalService.fetchLayout(selPlid);
+	}
+
+	protected long getSelPlid() {
+		return ParamUtil.getLong(
+			portletRequest, "selPlid", LayoutConstants.DEFAULT_PLID);
+	}
+
+	private final LayoutLocalService _layoutLocalService;
+
+}
