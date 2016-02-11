77941119d138bc3c5b03fb10f46f9b96f94ac2de
Michael Han <michael.han@liferay.com>
2016-02-11 11:34:24 -0600

    LPS-63116 audit wiring

---
+++ modules/portal-security/portal-security-audit-wiring/src/main/java/com/liferay/portal/security/audit/wiring/internal/messaging/AuditMessagingConfigurator.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.audit.wiring.internal.messaging;
+
+import com.liferay.portal.kernel.messaging.Destination;
+import com.liferay.portal.kernel.messaging.DestinationConfiguration;
+import com.liferay.portal.kernel.messaging.DestinationFactory;
+import com.liferay.portal.kernel.messaging.DestinationNames;
+import com.liferay.portal.kernel.messaging.proxy.ProxyMessageListener;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+
+import java.util.Dictionary;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Michael C. Han
+ */
+@Component(immediate = true, service = AuditMessagingConfigurator.class)
+public class AuditMessagingConfigurator {
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundleContext = bundleContext;
+
+		DestinationConfiguration destinationConfiguration =
+			new DestinationConfiguration(
+				DestinationConfiguration.DESTINATION_TYPE_PARALLEL,
+				DestinationNames.AUDIT);
+
+		Destination destination = _destinationFactory.createDestination(
+			destinationConfiguration);
+
+		Dictionary<String, Object> destinationDictionary =
+			new HashMapDictionary<>();
+
+		destinationDictionary.put("destination.name", destination.getName());
+
+		_destinationServiceRegistration = bundleContext.registerService(
+			Destination.class, destination, destinationDictionary);
+
+		destination.register(_proxyMessageListener);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		if (_destinationServiceRegistration != null) {
+			Destination destination = _bundleContext.getService(
+				_destinationServiceRegistration.getReference());
+
+			_destinationServiceRegistration.unregister();
+
+			destination.destroy();
+		}
+
+		_bundleContext = null;
+	}
+
+	private volatile BundleContext _bundleContext;
+
+	@Reference
+	private DestinationFactory _destinationFactory;
+
+	private volatile ServiceRegistration<Destination>
+		_destinationServiceRegistration;
+
+	@Reference(
+		service = ProxyMessageListener.class,
+		target = "(destination.name=" + DestinationNames.AUDIT + ")"
+	)
+	private ProxyMessageListener _proxyMessageListener;
+
+}
