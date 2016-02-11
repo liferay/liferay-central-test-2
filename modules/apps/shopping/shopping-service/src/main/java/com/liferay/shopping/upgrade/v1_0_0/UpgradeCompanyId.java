11178d4352e7863ad0299dba90916738e6e413c8
Cristina González Castellano <cristina.gonzalez@liferay.com>
2016-02-11 17:20:40 +0000

    LPS-62867 Fix UpgradeCompanyId of Shopping

--- modules/apps/shopping/shopping-service/src/main/java/com/liferay/shopping/upgrade/v1_0_0/UpgradeCompanyId.java
+++ modules/apps/shopping/shopping-service/src/main/java/com/liferay/shopping/upgrade/v1_0_0/UpgradeCompanyId.java
@@ -14,6 +14,10 @@
 
 package com.liferay.shopping.upgrade.v1_0_0;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+
 /**
  * @author Brian Wing Shun Chan
  */
@@ -21,12 +25,50 @@ public class UpgradeCompanyId
 	extends com.liferay.portal.upgrade.util.UpgradeCompanyId {
 
 	@Override
+	protected void doUpgrade() throws Exception {
+		super.doUpgrade();
+
+		//Upgrade ShoppingOrderItem
+
+		if (_log.isInfoEnabled()) {
+			_log.info("Adding column companyId to table ShoppingOrderItem");
+		}
+
+		runSQL("alter table ShoppingOrderItem add companyId LONG");
+
+		StringBundler sb = new StringBundler(7);
+
+		sb.append("update ShoppingOrderItem set companyId = ");
+		sb.append("(select  max(ShoppingItem.companyId) ");
+		sb.append("from ShoppingItem ");
+		sb.append("where SUBSTR(ShoppingOrderItem.itemId, 0, INSTR('|', ");
+		sb.append("ShoppingOrderItem.itemId)) = ");
+		sb.append("CAST_TEXT(ShoppingItem.itemId))");
+		sb.append("where ShoppingOrderItem.itemId like '%|%' ");
+
+		runSQL(sb.toString());
+
+		sb = new StringBundler(6);
+
+		sb.append("update ShoppingOrderItem set companyId = ");
+		sb.append("(select max(ShoppingItem.companyId) ");
+		sb.append("from ShoppingItem ");
+		sb.append("where ShoppingOrderItem.itemId = ");
+		sb.append("CAST_TEXT(ShoppingItem.itemId))");
+		sb.append("where ShoppingOrderItem.itemId not like '%|%' ");
+
+		runSQL(sb.toString());
+	}
+
+	@Override
 	protected TableUpdater[] getTableUpdaters() {
 		return new TableUpdater[] {
 			new TableUpdater("ShoppingItemField", "ShoppingItem", "itemId"),
-			new TableUpdater("ShoppingItemPrice", "ShoppingItem", "itemId"),
-			new TableUpdater("ShoppingOrderItem", "ShoppingItem", "itemId")
+			new TableUpdater("ShoppingItemPrice", "ShoppingItem", "itemId")
 		};
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		UpgradeCompanyId.class);
 
 }
