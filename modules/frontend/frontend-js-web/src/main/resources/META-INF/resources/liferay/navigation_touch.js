91f5ad047801824bccf9e2e7e789e6ad62f0a5ca
Jonathan Mak <jonathan.mak@liferay.com>
2016-02-11 13:33:52 -0800

    LPS-62559 - Use bars instead of move icon

--- modules/frontend/frontend-js-web/src/main/resources/META-INF/resources/liferay/navigation_touch.js
+++ modules/frontend/frontend-js-web/src/main/resources/META-INF/resources/liferay/navigation_touch.js
@@ -8,7 +8,7 @@ AUI.add(
 		var SELECTOR_LFR_NAV_SORTABLE = '.lfr-nav-sortable';
 
 		var TPL_DRAG_HANDLE = '<svg class="drag-handle lexicon-icon">' +
-				'<use xlink:href="' + themeDisplay.getPathThemeImages() + '/lexicon/icons.svg#move" />' +
+				'<use xlink:href="' + themeDisplay.getPathThemeImages() + '/lexicon/icons.svg#bars" />' +
 			'</svg>';
 
 		var afterMakeSortable = Liferay.Navigation.prototype._afterMakeSortable;
