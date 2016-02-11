3926a90380574062470dd4a5b1a73938d4156df3
Máté Thurzó <mate.thurzo@liferay.com>
2016-02-11 16:05:19 +0100

    LPS-61722 Fix compile error

--- modules/apps/product-navigation/product-navigation-site-administration/src/main/java/com/liferay/product/navigation/site/administration/display/context/SiteAdministrationPanelCategoryDisplayContext.java
+++ modules/apps/product-navigation/product-navigation-site-administration/src/main/java/com/liferay/product/navigation/site/administration/display/context/SiteAdministrationPanelCategoryDisplayContext.java
@@ -26,6 +26,7 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.Organization;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
