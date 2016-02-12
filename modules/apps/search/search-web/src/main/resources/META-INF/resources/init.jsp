mhan810/liferay-portal

LPS-61518 If groupId param missing (no Sites facet) decide it from Scope
https://issues.liferay.com/browse/LPS-61518

modules/apps/search/search-web/src/main/resources/META-INF/resources/init.jsp
@@ -92,6 +92,7 @@ page import="com.liferay.portlet.asset.service.permission.AssetCategoryPermissio
 page import="com.liferay.search.facet.SearchFacet" %><%@
 page import="com.liferay.search.facet.util.SearchFacetTracker" %><%@
 page import="com.liferay.search.web.constants.SearchPortletKeys" %><%@
+page import="com.liferay.search.web.constants.SearchPortletParams" %><%@
 page import="com.liferay.search.web.display.context.SearchDisplayContext" %><%@
 page import="com.liferay.search.web.facet.AssetEntriesSearchFacet" %><%@
 page import="com.liferay.taglib.aui.AUIUtil" %><%@

modules/apps/search/search-web/src/main/resources/META-INF/resources/search.jsp
@@ -23,11 +23,15 @@ if (Validator.isNotNull(redirect)) {
 	portletDisplay.setURLBack(redirect);
 }
 
-long groupId = ParamUtil.getLong(request, "groupId");
+long groupId = ParamUtil.getLong(request, SearchPortletParams.GROUP_ID);
 
-String keywords = ParamUtil.getString(request, "keywords");
+String keywords = ParamUtil.getString(request, SearchPortletParams.KEYWORDS);
 
-String format = ParamUtil.getString(request, "format");
+String format = ParamUtil.getString(request, SearchPortletParams.FORMAT);
+
+String scopeParam = ParamUtil.getString(request, SearchPortletParams.SCOPE);
+
+SearchDisplayContext.Scope scope = searchDisplayContext.getScope(scopeParam);
 
 PortletURL portletURL = PortletURLUtil.getCurrent(renderRequest, renderResponse);
 
@@ -51,6 +55,7 @@ request.setAttribute("search.jsp-returnToFullPageURL", portletDisplay.getURLBack
 
 	<aui:fieldset id="searchContainer">
 		<aui:input autoFocus="<%= windowState.equals(WindowState.MAXIMIZED) %>" inlineField="<%= true %>" label="" name="keywords" size="30" title="search" value="<%= HtmlUtil.escape(keywords) %>" />
+		<aui:input name="scope" type="hidden" value="<%= scope.getScope() %>" />
 
 		<aui:field-wrapper inlineField="<%= true %>">
 			<aui:button icon="icon-search" onClick='<%= renderResponse.getNamespace() + "search();" %>' value="search" />

modules/apps/search/search-web/src/main/resources/META-INF/resources/view.jsp
@@ -17,9 +17,17 @@
 <%@ include file="/init.jsp" %>
 
 <%
-long groupId = ParamUtil.getLong(request, "groupId");
+long groupId = ParamUtil.getLong(request, SearchPortletParams.GROUP_ID);
 
-String keywords = ParamUtil.getString(request, "keywords");
+String keywords = ParamUtil.getString(request, SearchPortletParams.KEYWORDS);
+
+String scopeParam = ParamUtil.getString(request, SearchPortletParams.SCOPE);
+
+SearchDisplayContext.Preferences preferences = searchDisplayContext.getPreferences();
+
+SearchDisplayContext.Scope scope = searchDisplayContext.getScope(scopeParam);
+
+boolean scopeEverything = (groupId == 0);
 
 PortletURL portletURL = renderResponse.createRenderURL();
 
@@ -44,25 +52,17 @@ pageContext.setAttribute("portletURL", portletURL);
 		<liferay-ui:quick-access-entry label="skip-to-search" onClick="<%= taglibOnClick %>" />
 
 		<c:choose>
-			<c:when test='<%= Validator.equals(searchDisplayContext.getSearchScope(), "let-the-user-choose") %>'>
-				<aui:select cssClass="search-select" inlineField="<%= true %>" label="" name="groupId" title="scope">
-
-					<%
-					Group group = themeDisplay.getScopeGroup();
-					%>
-
-					<c:if test="<%= !group.isStagingGroup() %>">
-						<aui:option label="everything" selected="<%= (groupId == 0) %>" value="0" />
+			<c:when test="<%= preferences.isScopeLetTheUserChoose() %>">
+				<aui:select cssClass="search-select" inlineField="<%= true %>" label="" name="scope" title="scope">
+					<c:if test="<%= preferences.isScopeEverythingAvailable() %>">
+						<aui:option label="everything" selected="<%= scopeEverything %>" value="everything" />
 					</c:if>
 
-					<aui:option label="this-site" selected="<%= (groupId != 0) %>" value="<%= group.getGroupId() %>" />
+					<aui:option label="this-site" selected="<%= !scopeEverything %>" value="this-site" />
 				</aui:select>
 			</c:when>
-			<c:when test='<%= Validator.equals(searchDisplayContext.getSearchScope(), "everything") %>'>
-				<aui:input name="groupId" type="hidden" value="0" />
-			</c:when>
 			<c:otherwise>
-				<aui:input name="groupId" type="hidden" value="<%= themeDisplay.getScopeGroupId() %>" />
+				<aui:input name="scope" type="hidden" value="<%= scope.getScope() %>" />
 			</c:otherwise>
 		</c:choose>
 

liferay/liferay-central-test-2

modules/apps/search/search-web/src/main/resources/META-INF/resources/init.jsp
@@ -88,6 +88,7 @@ page import="com.liferay.search.facet.SearchFacet" %><%@
 page import="com.liferay.search.facet.util.SearchFacetTracker" %><%@
 page import="com.liferay.search.facet.util.comparator.SearchFacetComparator" %><%@
 page import="com.liferay.search.web.constants.SearchPortletKeys" %><%@
+page import="com.liferay.search.web.constants.SearchPortletParameterNames" %><%@
 page import="com.liferay.search.web.display.context.SearchDisplayContext" %><%@
 page import="com.liferay.search.web.facet.AssetEntriesSearchFacet" %><%@
 page import="com.liferay.taglib.aui.AUIUtil" %><%@

modules/apps/search/search-web/src/main/resources/META-INF/resources/search.jsp
@@ -23,9 +23,9 @@ if (Validator.isNotNull(redirect)) {
 	portletDisplay.setURLBack(redirect);
 }
 
-long groupId = ParamUtil.getLong(request, "groupId");
+long groupId = ParamUtil.getLong(request, SearchPortletParameterNames.GROUP_ID);
 
-String format = ParamUtil.getString(request, "format");
+String format = ParamUtil.getString(request, SearchPortletParameterNames.FORMAT);
 %>
 
 <liferay-ui:header
@@ -44,6 +44,7 @@ String format = ParamUtil.getString(request, "format");
 
 	<aui:fieldset id="searchContainer">
 		<aui:input autoFocus="<%= windowState.equals(WindowState.MAXIMIZED) %>" inlineField="<%= true %>" label="" name="keywords" size="30" title="search" value="<%= HtmlUtil.escape(searchDisplayContext.getKeywords()) %>" />
+		<aui:input name="scope" type="hidden" value="<%= searchDisplayContext.getSearchScopeParameterString() %>" />
 
 		<aui:field-wrapper inlineField="<%= true %>">
 			<aui:button icon="icon-search" onClick='<%= renderResponse.getNamespace() + "search();" %>' value="search" />

modules/apps/search/search-web/src/main/resources/META-INF/resources/view.jsp
@@ -17,9 +17,9 @@
 <%@ include file="/init.jsp" %>
 
 <%
-long groupId = ParamUtil.getLong(request, "groupId");
+long groupId = ParamUtil.getLong(request, SearchPortletParameterNames.GROUP_ID);
 
-String keywords = ParamUtil.getString(request, "keywords");
+boolean scopeEverything = (groupId == 0);
 
 PortletURL portletURL = renderResponse.createRenderURL();
 
@@ -35,7 +35,7 @@ pageContext.setAttribute("portletURL", portletURL);
 	<liferay-portlet:renderURLParams varImpl="portletURL" />
 
 	<aui:fieldset>
-		<aui:input cssClass="search-input" inlineField="<%= true %>" label="" name="keywords" placeholder="search" size="30" title="search" type="text" value="<%= HtmlUtil.escapeAttribute(keywords) %>" />
+		<aui:input cssClass="search-input" inlineField="<%= true %>" label="" name="keywords" placeholder="search" size="30" title="search" type="text" value="<%= HtmlUtil.escapeAttribute(searchDisplayContext.getKeywords()) %>" />
 
 		<%
 		String taglibOnClick = "Liferay.Util.focusFormField('#" + renderResponse.getNamespace() + "keywords');";
@@ -44,25 +44,17 @@ pageContext.setAttribute("portletURL", portletURL);
 		<liferay-ui:quick-access-entry label="skip-to-search" onClick="<%= taglibOnClick %>" />
 
 		<c:choose>
-			<c:when test='<%= Validator.equals(searchDisplayContext.getSearchScope(), "let-the-user-choose") %>'>
-				<aui:select cssClass="search-select" inlineField="<%= true %>" label="" name="groupId" title="scope">
-
-					<%
-					Group group = themeDisplay.getScopeGroup();
-					%>
-
-					<c:if test="<%= !group.isStagingGroup() %>">
-						<aui:option label="everything" selected="<%= (groupId == 0) %>" value="0" />
+			<c:when test="<%= searchDisplayContext.isSearchScopePreferenceLetTheUserChoose() %>">
+				<aui:select cssClass="search-select" inlineField="<%= true %>" label="" name="scope" title="scope">
+					<c:if test="<%= searchDisplayContext.isSearchScopePreferenceEverythingAvailable() %>">
+						<aui:option label="everything" selected="<%= scopeEverything %>" value="everything" />
 					</c:if>
 
-					<aui:option label="this-site" selected="<%= (groupId != 0) %>" value="<%= group.getGroupId() %>" />
+					<aui:option label="this-site" selected="<%= !scopeEverything %>" value="this-site" />
 				</aui:select>
 			</c:when>
-			<c:when test='<%= Validator.equals(searchDisplayContext.getSearchScope(), "everything") %>'>
-				<aui:input name="groupId" type="hidden" value="0" />
-			</c:when>
 			<c:otherwise>
-				<aui:input name="groupId" type="hidden" value="<%= themeDisplay.getScopeGroupId() %>" />
+				<aui:input name="scope" type="hidden" value="<%= searchDisplayContext.getSearchScopeParameterString() %>" />
 			</c:otherwise>
 		</c:choose>
 