d2e173f43cca8b0b640c704d91208b92b6816cee
Hugo Huijser <hugo.huijser@liferay.com>
2016-02-12 14:21:28 +0800

    LPS-61420 Sort <hint-collection> in *-model-hints.xml

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/XMLSourceProcessor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/XMLSourceProcessor.java
@@ -327,6 +327,9 @@ public class XMLSourceProcessor extends BaseSourceProcessor {
 		else if (fileName.endsWith("-hbm.xml")) {
 			formatHBMXML(fileName, newContent);
 		}
+		else if (fileName.endsWith("-model-hints.xml")) {
+			formatModelHintsXML(fileName, newContent);
+		}
 		else if (fileName.endsWith("/liferay-portlet.xml") ||
 				 (portalSource && fileName.endsWith("/portlet-custom.xml")) ||
 				 (!portalSource && fileName.endsWith("/portlet.xml"))) {
@@ -759,6 +762,16 @@ public class XMLSourceProcessor extends BaseSourceProcessor {
 		checkOrder(
 			fileName, document.getRootElement(), "import", null,
 			new ElementComparator("class", true));
+	}
+
+	protected void formatModelHintsXML(String fileName, String content)
+		throws Exception {
+
+		Document document = readXML(content);
+
+		checkOrder(
+			fileName, document.getRootElement(), "hint-collection", null,
+			new ElementComparator());
 	}
 
 	protected String formatPortletXML(
