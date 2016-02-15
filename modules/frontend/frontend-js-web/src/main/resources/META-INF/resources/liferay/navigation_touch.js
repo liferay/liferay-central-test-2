0eef7454b990f648a33e10e1bbffb557eece9cd2
Chema Balsas <jose.balsas@liferay.com>
2016-02-15 13:19:39 +0000

    LPS-53223 Fix typo

--- modules/frontend/frontend-js-web/src/main/resources/META-INF/resources/liferay/navigation_touch.js
+++ modules/frontend/frontend-js-web/src/main/resources/META-INF/resources/liferay/navigation_touch.js
@@ -7,7 +7,7 @@ AUI.add(
 
 		var SELECTOR_LFR_NAV_SORTABLE = '.lfr-nav-sortable';
 
-		var TPL_DRAG_HANDLE = Liferay.Util.getLexiconIconTPL('bars', 'drag-handle');
+		var TPL_DRAG_HANDLE = Util.getLexiconIconTpl('bars', 'drag-handle');
 
 		var afterMakeSortable = Liferay.Navigation.prototype._afterMakeSortable;
 
