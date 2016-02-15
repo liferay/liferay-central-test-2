a3eee834ad5c83c0ec6022f62ee1470346fa4eac
Máté Thurzó <mate.thurzo@liferay.com>
2016-02-15 11:23:22 +0100

    LPS-63127 Do not show when staging is not turned on

--- modules/apps/staging/staging-processes-web/src/main/java/com/liferay/staging/processes/web/portlet/configuration/icon/StagingConfigurationPortletConfigurationIcon.java
+++ modules/apps/staging/staging-processes-web/src/main/java/com/liferay/staging/processes/web/portlet/configuration/icon/StagingConfigurationPortletConfigurationIcon.java
@@ -14,6 +14,7 @@
 
 package com.liferay.staging.processes.web.portlet.configuration.icon;
 
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
 import com.liferay.portal.kernel.util.PortalUtil;
@@ -55,6 +56,12 @@ public class StagingConfigurationPortletConfigurationIcon
 
 	@Override
 	public boolean isShow() {
+		Group group = themeDisplay.getScopeGroup();
+
+		if (!group.isStaged()) {
+			return false;
+		}
+
 		User user = themeDisplay.getUser();
 
 		if (user.isDefaultUser()) {
