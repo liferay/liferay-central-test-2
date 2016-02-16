d35d9a09a5c98cb2f8728279b22e6eb3f9772258
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2016-02-16 15:26:32 -0800

    LPS-55516 - General source formatting

--- modules/apps/calendar/calendar-web/src/main/resources/META-INF/resources/js/javascript.js
+++ modules/apps/calendar/calendar-web/src/main/resources/META-INF/resources/js/javascript.js
@@ -155,8 +155,7 @@ AUI.add(
 							);
 						},
 						resultFormatter: function(query, results) {
-							return AArray.map(
-								results,
+							return results.map(
 								function(result) {
 									var calendar = result.raw;
 									var calendarResourceName = calendar.calendarResourceName;
@@ -2270,8 +2269,7 @@ AUI.add(
 					_syncInviteesContent: function(contentNode, calendarResources) {
 						var instance = this;
 
-						var values = AArray.map(
-							calendarResources,
+						var values = calendarResources.map(
 							function(item) {
 								return item.name;
 							}
