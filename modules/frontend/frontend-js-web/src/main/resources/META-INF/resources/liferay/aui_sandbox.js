6cef5f9663b1c5e349e6438b6dd6c819c1ed840a
Bruno Basto <bruno.basto@liferay.com>
2016-02-15 23:57:56 -0300

    LPS-62767 Prevents callback execution in case it's not necessary anymore

--- modules/frontend/frontend-js-web/src/main/resources/META-INF/resources/liferay/aui_sandbox.js
+++ modules/frontend/frontend-js-web/src/main/resources/META-INF/resources/liferay/aui_sandbox.js
@@ -5,6 +5,26 @@
 		ALLOY.html5shiv();
 	}
 
+	var originalUse = ALLOY.use;
+
+	ALLOY.use = function() {
+		var args = Array.prototype.slice.call(arguments, 0);
+
+		var currentURL = Liferay.currentURL;
+
+		var originalCallback = args[args.length - 1];
+
+		if (typeof originalCallback === 'function') {
+			args[args.length - 1] = function() {
+				if (Liferay.currentURL === currentURL) {
+					originalCallback.apply(this, arguments);
+				}
+			};
+		}
+
+		return originalUse.apply(this, args);
+	};
+
 	window.AUI = function() {
 		return ALLOY;
 	};
