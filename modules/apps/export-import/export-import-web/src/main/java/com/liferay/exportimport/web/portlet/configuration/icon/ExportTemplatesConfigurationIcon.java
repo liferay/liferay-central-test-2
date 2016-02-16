fe1de80c47f83abaa4d9d5d7a965a17cd77b447b
Julio Camarero <julio.camarero@liferay.com>
2016-02-16 17:14:13 +0100

    LPS-63194 apply language keys to Export/Import configuration icons

--- modules/apps/export-import/export-import-web/src/main/java/com/liferay/exportimport/web/portlet/configuration/icon/ExportTemplatesConfigurationIcon.java
+++ modules/apps/export-import/export-import-web/src/main/java/com/liferay/exportimport/web/portlet/configuration/icon/ExportTemplatesConfigurationIcon.java
@@ -15,6 +15,7 @@
 package com.liferay.exportimport.web.portlet.configuration.icon;
 
 import com.liferay.exportimport.web.constants.ExportImportPortletKeys;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
 import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
@@ -41,7 +42,8 @@ public class ExportTemplatesConfigurationIcon
 
 	@Override
 	public String getMessage(PortletRequest portletRequest) {
-		return "export-templates";
+		return LanguageUtil.get(
+			getResourceBundle(getLocale(portletRequest)), "export-templates");
 	}
 
 	@Override
--- modules/apps/staging/staging-processes-web/src/main/java/com/liferay/staging/processes/web/portlet/configuration/icon/PublishTemplatesConfigurationIcon.java
+++ modules/apps/staging/staging-processes-web/src/main/java/com/liferay/staging/processes/web/portlet/configuration/icon/PublishTemplatesConfigurationIcon.java
@@ -14,6 +14,7 @@
 
 package com.liferay.staging.processes.web.portlet.configuration.icon;
 
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
@@ -42,7 +43,8 @@ public class PublishTemplatesConfigurationIcon
 
 	@Override
 	public String getMessage(PortletRequest portletRequest) {
-		return "publish-templates";
+		return LanguageUtil.get(
+			getResourceBundle(getLocale(portletRequest)), "publish-templates");
 	}
 
 	@Override
