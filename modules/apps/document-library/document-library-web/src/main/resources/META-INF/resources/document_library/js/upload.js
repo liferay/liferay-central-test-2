3b4906ad0e5450ab761972848821c325a512a7b8
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2016-02-16 15:23:46 +0100

    LPS-62661 Get the right node if there are no documents or folders

--- modules/apps/document-library/document-library-web/src/main/resources/META-INF/resources/document_library/js/upload.js
+++ modules/apps/document-library/document-library-web/src/main/resources/META-INF/resources/document_library/js/upload.js
@@ -471,7 +471,7 @@ AUI.add(
 								entriesContainerSelector = 'ul.list-unstyled:last-of-type';
 							}
 
-							entriesContainer = entriesContainer.one(entriesContainerSelector);
+							entriesContainer = entriesContainer.one(entriesContainerSelector) || entriesContainer.one('.taglib-empty-result-message');
 
 							var invisibleEntry = instance._invisibleDescriptiveEntry;
 
