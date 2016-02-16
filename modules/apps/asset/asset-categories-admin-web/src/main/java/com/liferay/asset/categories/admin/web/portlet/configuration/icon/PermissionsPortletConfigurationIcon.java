f6a4ac914d050a54bb995824f6b09583244de1d2
Julio Camarero <julio.camarero@liferay.com>
2016-02-16 17:13:45 +0100

    LPS-63194 apply language keys to Asset configuration icons

--- modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/portlet/configuration/icon/PermissionsPortletConfigurationIcon.java
+++ modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/portlet/configuration/icon/PermissionsPortletConfigurationIcon.java
@@ -14,6 +14,7 @@
 
 package com.liferay.asset.categories.admin.web.portlet.configuration.icon;
 
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.LiferayWindowState;
 import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
@@ -47,7 +48,8 @@ public class PermissionsPortletConfigurationIcon
 
 	@Override
 	public String getMessage(PortletRequest portletRequest) {
-		return "permissions";
+		return LanguageUtil.get(
+			getResourceBundle(getLocale(portletRequest)), "permissions");
 	}
 
 	@Override
