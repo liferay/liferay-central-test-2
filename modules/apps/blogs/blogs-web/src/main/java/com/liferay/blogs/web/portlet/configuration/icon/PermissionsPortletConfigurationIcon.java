b4e7c09a83a3e7709dc3663ca4f1cc2869796983
Julio Camarero <julio.camarero@liferay.com>
2016-02-16 17:16:17 +0100

    LPS-63194 apply language keys to Blogs configuration icons

--- modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/portlet/configuration/icon/PermissionsPortletConfigurationIcon.java
+++ modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/portlet/configuration/icon/PermissionsPortletConfigurationIcon.java
@@ -14,6 +14,7 @@
 
 package com.liferay.blogs.web.portlet.configuration.icon;
 
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.LiferayWindowState;
 import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
@@ -46,7 +47,8 @@ public class PermissionsPortletConfigurationIcon
 
 	@Override
 	public String getMessage(PortletRequest portletRequest) {
-		return "permissions";
+		return LanguageUtil.get(
+			getResourceBundle(getLocale(portletRequest)), "permissions");
 	}
 
 	@Override
