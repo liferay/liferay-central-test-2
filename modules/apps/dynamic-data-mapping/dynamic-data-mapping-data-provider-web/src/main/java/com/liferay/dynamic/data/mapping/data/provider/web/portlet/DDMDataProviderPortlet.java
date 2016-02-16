031503930106008af086eddeef97df09ae605328
Leonardo Barros <leonardo.barros@liferay.com>
2016-02-16 10:16:09 -0300

    LPS-62947 Set add-default-resource to true

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-data-provider-web/src/main/java/com/liferay/dynamic/data/mapping/data/provider/web/portlet/DDMDataProviderPortlet.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-data-provider-web/src/main/java/com/liferay/dynamic/data/mapping/data/provider/web/portlet/DDMDataProviderPortlet.java
@@ -40,6 +40,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	immediate = true,
 	property = {
+		"com.liferay.portlet.add-default-resource=true",
 		"com.liferay.portlet.css-class-wrapper=portlet-dynamic-data-mapping-data-provider",
 		"com.liferay.portlet.display-category=category.hidden",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
