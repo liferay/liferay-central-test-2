261f2536046fbf12ace73a80727d50311aafc454
Lino Alves <lino.alves@liferay.com>
2016-02-17 17:51:42 -0300

    LPS-63296 - setting hidden attributes on field

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/main.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/main.js
@@ -269,6 +269,21 @@ AUI.add(
 
 						field.set('strings', instance.get('strings'));
 
+						var fieldHiddenAttributeMap =
+						{
+						'ddm-separator' : instance.MAP_HIDDEN_FIELD_ATTRS.separator,
+						'checkbox' : instance.MAP_HIDDEN_FIELD_ATTRS.checkbox,
+						'default' : instance.MAP_HIDDEN_FIELD_ATTRS.DEFAULT
+						};
+
+						var hiddenAtributes = fieldHiddenAttributeMap[field.get('type')];
+
+						if (!hiddenAtributes) {
+							hiddenAtributes = fieldHiddenAttributeMap['default'];
+						}
+
+						field.set('hiddenAttributes', hiddenAtributes);
+
 						return field;
 					},
 
