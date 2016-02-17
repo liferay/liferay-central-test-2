eef629a9b3e6d736df4b04cdbf67a87447e82ce7
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2016-02-17 12:40:34 +0100

    LPS-63194 SF

--- modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/portlet/configuration/icon/PermissionsPortletConfigurationIcon.java
+++ modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/portlet/configuration/icon/PermissionsPortletConfigurationIcon.java
@@ -14,6 +14,7 @@
 
 package com.liferay.blogs.web.portlet.configuration.icon;
 
+import com.liferay.blogs.web.constants.BlogsPortletKeys;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.LiferayWindowState;
@@ -37,9 +38,7 @@ import org.osgi.service.component.annotations.Component;
  */
 @Component(
 	immediate = true,
-	property = {
-		"javax.portlet.name=" + com.liferay.blogs.web.constants.BlogsPortletKeys.BLOGS_ADMIN
-	},
+	property = {"javax.portlet.name=" + BlogsPortletKeys.BLOGS_ADMIN},
 	service = PortletConfigurationIcon.class
 )
 public class PermissionsPortletConfigurationIcon
