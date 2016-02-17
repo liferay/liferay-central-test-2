5ee0219b8c8ad073104a0149514200dede6490e9
Michael Han <michael.han@liferay.com>
2016-02-17 16:13:24 -0600

    LPS-63365 move to module

--- modules/portal/portal-monitoring/src/main/java/com/liferay/portal/monitoring/internal/messaging/MonitoringMessageListener.java
+++ modules/portal/portal-monitoring/src/main/java/com/liferay/portal/monitoring/internal/messaging/MonitoringMessageListener.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.kernel.monitoring.messaging;
+package com.liferay.portal.monitoring.internal.messaging;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
