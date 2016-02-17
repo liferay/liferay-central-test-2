96635910db1d6817a3dae5c1611fef6bed356176
shuyangzhou <shuyang.zhou@liferay.com>
2016-02-17 07:55:32 -0800

    LPS-63347 Swap out the inefficient handleUnknownType() impl

--- modules/portal-template/portal-template-freemarker/src/main/java/com/liferay/portal/template/freemarker/LiferayObjectWrapper.java
+++ modules/portal-template/portal-template-freemarker/src/main/java/com/liferay/portal/template/freemarker/LiferayObjectWrapper.java
@@ -16,10 +16,22 @@ package com.liferay.portal.template.freemarker;
 
 import com.liferay.portal.kernel.templateparser.TemplateNode;
 
+import freemarker.ext.beans.BeansWrapper;
+import freemarker.ext.beans.EnumerationModel;
+import freemarker.ext.beans.ResourceBundleModel;
+import freemarker.ext.beans.StringModel;
+import freemarker.ext.util.ModelFactory;
+
 import freemarker.template.Configuration;
 import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.ObjectWrapper;
 import freemarker.template.TemplateModel;
 import freemarker.template.TemplateModelException;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+import org.w3c.dom.Node;
 
 /**
  * @author Mika Koivisto
@@ -38,5 +50,64 @@ public class LiferayObjectWrapper extends DefaultObjectWrapper {
 
 		return super.wrap(object);
 	}
+
+	@Override
+	protected TemplateModel handleUnknownType(Object object) {
+		if (object instanceof Node) {
+			return wrapDomNode(object);
+		}
+
+		if (object instanceof TemplateNode) {
+			return new LiferayTemplateModel((TemplateNode)object, this);
+		}
+
+		if (object instanceof ResourceBundle) {
+			return _RESOURCE_BUNDLE_MODEL_FACTORY.create(object, this);
+		}
+
+		if (object instanceof Enumeration) {
+			return _ENUMERATION_MODEL_FACTORY.create(object, this);
+		}
+
+		return _STRING_MODEL_FACTORY.create(object, this);
+	}
+
+	private static final ModelFactory _ENUMERATION_MODEL_FACTORY =
+		new ModelFactory() {
+
+			@Override
+			public TemplateModel create(
+				Object object, ObjectWrapper objectWrapper) {
+
+				return new EnumerationModel(
+					(Enumeration)object, (BeansWrapper)objectWrapper);
+			}
+
+		};
+
+	private static final ModelFactory _RESOURCE_BUNDLE_MODEL_FACTORY =
+		new ModelFactory() {
+
+			@Override
+			public TemplateModel create(
+				Object object, ObjectWrapper objectWrapper) {
+
+				return new ResourceBundleModel(
+					(ResourceBundle)object, (BeansWrapper)objectWrapper);
+			}
+
+		};
+
+	private static final ModelFactory _STRING_MODEL_FACTORY =
+		new ModelFactory() {
+
+			@Override
+			public TemplateModel create(
+				Object object, ObjectWrapper objectWrapper) {
+
+				return new StringModel(object, (BeansWrapper)objectWrapper);
+			}
+
+		};
 
 }
