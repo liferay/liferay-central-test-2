b2531a80f4d2565b5b6911773d07ff6b5b4f1296
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-02-18 12:31:51 +0100

    LPS-63345 Uses the correct resource bundle

--- modules/apps/portlet-configuration/portlet-configuration-css-web/src/main/java/com/liferay/portlet/configuration/css/web/portlet/configuration/icon/PortletConfigurationCSSPortletConfigurationIcon.java
+++ modules/apps/portlet-configuration/portlet-configuration-css-web/src/main/java/com/liferay/portlet/configuration/css/web/portlet/configuration/icon/PortletConfigurationCSSPortletConfigurationIcon.java
@@ -20,9 +20,12 @@ import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigura
 import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
 import com.liferay.portal.kernel.theme.PortletDisplay;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portlet.configuration.css.web.constants.PortletConfigurationCSSPortletKeys;
+
+import java.util.ResourceBundle;
 
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletResponse;
@@ -44,9 +47,10 @@ public class PortletConfigurationCSSPortletConfigurationIcon
 
 	@Override
 	public String getMessage(PortletRequest portletRequest) {
-		return LanguageUtil.get(
-			getResourceBundle(getLocale(portletRequest)),
-			"look-and-feel-configuration");
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", getLocale(portletRequest), getClass());
+
+		return LanguageUtil.get(resourceBundle, "look-and-feel-configuration");
 	}
 
 	@Override
