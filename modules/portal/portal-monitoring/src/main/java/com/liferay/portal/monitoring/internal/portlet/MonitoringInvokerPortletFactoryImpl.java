ae5298ca64a37f5803cf5065b417ce9a7a5185ac
Michael Han <michael.han@liferay.com>
2016-02-18 14:03:57 -0600

    LPS-63423 put into correct module

--- modules/portal/portal-monitoring/src/main/java/com/liferay/portal/monitoring/internal/portlet/MonitoringInvokerPortletFactoryImpl.java
+++ modules/portal/portal-monitoring/src/main/java/com/liferay/portal/monitoring/internal/portlet/MonitoringInvokerPortletFactoryImpl.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.monitoring.web.portlet;
+package com.liferay.portal.monitoring.internal.portlet;
 
 import com.liferay.portal.kernel.monitoring.PortletMonitoringControl;
 import com.liferay.portal.kernel.portlet.InvokerFilterContainer;
