2fd678f7f93a0d17faef914783c09133902b2a09
Roberto Díaz <roberto.diaz@liferay.com>
2016-02-18 18:47:20 +0100

    LPS-63048 New resource action that will return the search results

---
+++ modules/apps/wiki/wiki-web/src/main/java/com/liferay/wiki/web/portlet/action/AutocompletePageTitleMVCResourceCommand.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wiki.web.portlet.action;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.SearchContextFactory;
+import com.liferay.portal.kernel.search.SearchResult;
+import com.liferay.portal.kernel.search.SearchResultUtil;
+import com.liferay.portal.kernel.servlet.ServletResponseUtil;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.wiki.constants.WikiPortletKeys;
+import com.liferay.wiki.model.WikiPage;
+import com.liferay.wiki.search.WikiPageTitleSearcher;
+import com.liferay.wiki.service.WikiPageService;
+
+import java.util.List;
+
+import javax.portlet.PortletException;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Roberto Díaz
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + WikiPortletKeys.WIKI,
+		"javax.portlet.name=" + WikiPortletKeys.WIKI_ADMIN,
+		"javax.portlet.name=" + WikiPortletKeys.WIKI_DISPLAY,
+		"mvc.command.name=/wiki/autocomplete_page_title"
+	},
+	service = MVCResourceCommand.class
+)
+public class AutocompletePageTitleMVCResourceCommand
+	extends BaseMVCResourceCommand {
+
+	@Override
+	public void doServeResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws PortletException {
+
+		try {
+			JSONArray jsonArray = getJSONArray(
+				resourceRequest, resourceResponse);
+
+			HttpServletResponse response = PortalUtil.getHttpServletResponse(
+				resourceResponse);
+
+			response.setContentType(ContentTypes.APPLICATION_JSON);
+
+			ServletResponseUtil.write(response, jsonArray.toString());
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
+	}
+
+	protected JSONArray getJSONArray(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws PortalException {
+
+		HttpServletRequest request = PortalUtil.getHttpServletRequest(
+			resourceRequest);
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		String query = ParamUtil.getString(request, "query");
+
+		SearchContext searchContext = SearchContextFactory.getInstance(request);
+
+		WikiPageTitleSearcher wikiPageSearcher = new WikiPageTitleSearcher();
+
+		searchContext.setKeywords(StringUtil.toLowerCase(query));
+
+		Hits hits = wikiPageSearcher.search(searchContext);
+
+		List<SearchResult> searchResults = SearchResultUtil.getSearchResults(
+			hits, resourceRequest.getLocale());
+
+		for (SearchResult searchResult : searchResults) {
+			WikiPage page = _wikiPageService.getPage(searchResult.getClassPK());
+
+			JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+			jsonObject.put("title", page.getTitle());
+
+			jsonArray.put(jsonObject);
+		}
+
+		return jsonArray;
+	}
+
+	@Reference(unbind = "-")
+	protected void setWikiPageService(WikiPageService wikiPageService) {
+		_wikiPageService = wikiPageService;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AutocompletePageTitleMVCResourceCommand.class);
+
+	private WikiPageService _wikiPageService;
+
+}
