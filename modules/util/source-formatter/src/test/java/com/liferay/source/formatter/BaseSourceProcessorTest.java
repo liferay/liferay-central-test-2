144840ce6594f9979bf7ccc86e0d0249dc0b49bb
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-02-18 14:15:42 -0800

    LPS-61099 Move to "apps/wem"

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/BaseSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/BaseSourceProcessorTest.java
@@ -40,9 +40,9 @@ public class BaseSourceProcessorTest {
 				"./modules/apps/dynamic-data-mapping/" +
 					"dynamic-data-mapping-web"));
 		Assert.assertEquals(
-			"./modules/apps/staging/staging-lang",
+			"./modules/apps/wem/staging/staging-lang",
 			_baseSourceProcessor.getModuleLangDir(
-				"./modules/apps/staging/staging-bar-web"));
+				"./modules/apps/wem/staging/staging-bar-web"));
 		Assert.assertEquals(
 			"./modules/apps/workflow/workflow-lang",
 			_baseSourceProcessor.getModuleLangDir(
