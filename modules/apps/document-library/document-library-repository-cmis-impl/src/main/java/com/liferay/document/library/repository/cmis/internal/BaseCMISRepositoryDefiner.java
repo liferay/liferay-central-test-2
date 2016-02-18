aa539cd1169740ba341f68e69ae82e057e88574c
Adolfo Pérez <adolfo.perez@liferay.com>
2016-02-18 17:17:48 +0100

    LPS-63398 Use RBLoader to get a handle on resource bundles

--- modules/apps/document-library/document-library-repository-cmis-impl/src/main/java/com/liferay/document/library/repository/cmis/internal/BaseCMISRepositoryDefiner.java
+++ modules/apps/document-library/document-library-repository-cmis-impl/src/main/java/com/liferay/document/library/repository/cmis/internal/BaseCMISRepositoryDefiner.java
@@ -14,7 +14,11 @@
 
 package com.liferay.document.library.repository.cmis.internal;
 
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.repository.registry.BaseRepositoryDefiner;
+import com.liferay.portal.kernel.util.CacheResourceBundleLoader;
+import com.liferay.portal.kernel.util.ClassResourceBundleLoader;
+import com.liferay.portal.kernel.util.ResourceBundleLoader;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 
 import java.util.Locale;
@@ -27,13 +31,23 @@ public abstract class BaseCMISRepositoryDefiner extends BaseRepositoryDefiner {
 
 	@Override
 	public String getRepositoryTypeLabel(Locale locale) {
-		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
-			"content.Language", locale, getClass());
+		ResourceBundle resourceBundle =
+			_resourceBundleLoader.loadResourceBundle(
+				LanguageUtil.getLanguageId(locale));
 
 		return ResourceBundleUtil.getString(
 			resourceBundle, _MODEL_RESOURCE_NAME_PREFIX + getClassName());
 	}
 
+	protected ResourceBundleLoader getResourceBundleLoader() {
+		return _resourceBundleLoader;
+	}
+
 	private static final String _MODEL_RESOURCE_NAME_PREFIX = "model.resource.";
+
+	private final ResourceBundleLoader _resourceBundleLoader =
+		new CacheResourceBundleLoader(
+			new ClassResourceBundleLoader(
+				"content.Language", BaseCMISRepositoryDefiner.class));
 
 }
