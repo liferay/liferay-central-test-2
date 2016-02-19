2d913acc8ba8746e892f96ceb4c22877000196be
drewbrokke <drew.brokke@liferay.com>
2016-02-18 16:49:20 -0800

    LPS-63476 contacts-web - updates paths in main.js

--- modules/apps/foundation/contacts/contacts-web/src/main/resources/META-INF/resources/js/main.js
+++ modules/apps/foundation/contacts/contacts-web/src/main/resources/META-INF/resources/js/main.js
@@ -549,7 +549,7 @@ AUI.add(
 
 						var portletURL = new Liferay.PortletURL.createURL(config.baseRenderURL);
 
-						portletURL.setParameter('mvcPath', '/contacts_center/edit_entry.jsp');
+						portletURL.setParameter('mvcPath', '/edit_entry.jsp');
 						portletURL.setParameter('redirect', contact.redirect);
 						portletURL.setParameter('entryId', contact.entryId);
 						portletURL.setPortletId('1_WAR_contactsportlet');
