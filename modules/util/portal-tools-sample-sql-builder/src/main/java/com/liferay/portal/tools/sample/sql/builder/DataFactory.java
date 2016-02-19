039c6a177058daa181bffa63decefc5467950ba6
tina.tian <tina.tian@liferay.com>
2016-02-19 16:20:48 +0800

    LPS-63441 Fix SampleSQLBuilder, enable template cache for web content by default

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -2747,7 +2747,7 @@ public class DataFactory {
 		ddmTemplateModel.setMode(DDMTemplateConstants.TEMPLATE_MODE_CREATE);
 		ddmTemplateModel.setLanguage(TemplateConstants.LANG_TYPE_FTL);
 		ddmTemplateModel.setScript("${content.getData()}");
-		ddmTemplateModel.setCacheable(false);
+		ddmTemplateModel.setCacheable(true);
 		ddmTemplateModel.setSmallImage(false);
 		ddmTemplateModel.setLastPublishDate(nextFutureDate());
 
