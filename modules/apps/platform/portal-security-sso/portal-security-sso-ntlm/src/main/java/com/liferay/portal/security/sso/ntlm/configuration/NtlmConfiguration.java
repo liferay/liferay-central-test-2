0516ec68af3d5ddc9f45189db80b799b5e7cfb71
Pei-Jung Lan <pei-jung.lan@liferay.com>
2016-02-18 18:17:17 -0800

    LPS-62941 NTLM - reorder form fields to match configuration in Instance Settings

--- modules/apps/platform/portal-security-sso/portal-security-sso-ntlm/src/main/java/com/liferay/portal/security/sso/ntlm/configuration/NtlmConfiguration.java
+++ modules/apps/platform/portal-security-sso/portal-security-sso-ntlm/src/main/java/com/liferay/portal/security/sso/ntlm/configuration/NtlmConfiguration.java
@@ -36,15 +36,6 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 )
 public interface NtlmConfiguration {
 
-	@Meta.AD(deflt = "EXAMPLE", required = false)
-	public String domain();
-
-	@Meta.AD(deflt = "127.0.0.1", required = false)
-	public String domainController();
-
-	@Meta.AD(deflt = "EXAMPLE", required = false)
-	public String domainControllerName();
-
 	@Meta.AD(
 		deflt = "false",
 		description = "Set this to true to enable NTLM single sign on. NTLM will work only if LDAP authentication is also enabled and the authentication is made by screen name.",
@@ -52,17 +43,26 @@ public interface NtlmConfiguration {
 	)
 	public boolean enabled();
 
-	@Meta.AD(
-		deflt = "0x600FFFFF",
-		description = "Negotiate flags are set according to the client's requested capabilities and the server's ServerCapabilities. See the following link: http://msdn.microsoft.com/en-us/library/cc717152%28v=PROT.10%29.aspx",
-		required = false
-	)
-	public String negotiateFlags();
+	@Meta.AD(deflt = "127.0.0.1", required = false)
+	public String domainController();
+
+	@Meta.AD(deflt = "EXAMPLE", required = false)
+	public String domainControllerName();
+
+	@Meta.AD(deflt = "EXAMPLE", required = false)
+	public String domain();
 
 	@Meta.AD(deflt = "LIFERAY$@EXAMPLE.COM", required = false)
 	public String serviceAccount();
 
 	@Meta.AD(deflt = "test", required = false)
 	public String servicePassword();
+
+	@Meta.AD(
+		deflt = "0x600FFFFF",
+		description = "Negotiate flags are set according to the client's requested capabilities and the server's ServerCapabilities. See the following link: http://msdn.microsoft.com/en-us/library/cc717152%28v=PROT.10%29.aspx",
+		required = false
+	)
+	public String negotiateFlags();
 
 }
