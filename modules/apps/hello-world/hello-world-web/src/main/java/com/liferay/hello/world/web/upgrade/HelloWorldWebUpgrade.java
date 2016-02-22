fcab256aed4ce33605cbe88b97222b7774227cd7
Cristina González Castellano <cristina.gonzalez@liferay.com>
2016-02-22 13:27:05 +0100

    LPS-63449 Set the correct bundleSymbolicName

--- modules/apps/hello-world/hello-world-web/src/main/java/com/liferay/hello/world/web/upgrade/HelloWorldWebUpgrade.java
+++ modules/apps/hello-world/hello-world-web/src/main/java/com/liferay/hello/world/web/upgrade/HelloWorldWebUpgrade.java
@@ -29,11 +29,11 @@ public class HelloWorldWebUpgrade implements UpgradeStepRegistrator {
 	@Override
 	public void register(Registry registry) {
 		registry.register(
-			"com.liferay.license.manager.web", "0.0.0", "1.0.0",
+			"com.liferay.hello.world.web", "0.0.0", "1.0.0",
 			new DummyUpgradeStep());
 
 		registry.register(
-			"com.liferay.license.manager.web", "0.0.1", "1.0.0",
+			"com.liferay.hello.world.web", "0.0.1", "1.0.0",
 			new UpgradePortletId());
 	}
 
