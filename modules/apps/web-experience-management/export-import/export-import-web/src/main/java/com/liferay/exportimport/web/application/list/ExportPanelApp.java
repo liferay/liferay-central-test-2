5b42af1a0bf1903204cbab66fea4e7c225c0e806
Máté Thurzó <mate.thurzo@liferay.com>
2016-02-22 16:29:48 +0100

    LPS-63506 Rename publishing tools category to publishing

--- modules/apps/web-experience-management/export-import/export-import-web/src/main/java/com/liferay/exportimport/web/application/list/ExportPanelApp.java
+++ modules/apps/web-experience-management/export-import/export-import-web/src/main/java/com/liferay/exportimport/web/application/list/ExportPanelApp.java
@@ -29,7 +29,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	immediate = true,
 	property = {
-		"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_PUBLISHING_TOOLS,
+		"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_PUBLISHING,
 		"service.ranking:Integer=300"
 	},
 	service = PanelApp.class
