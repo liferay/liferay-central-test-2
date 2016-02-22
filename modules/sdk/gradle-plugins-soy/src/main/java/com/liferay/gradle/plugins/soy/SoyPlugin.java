8386c9d11b2ee9d7efa67df2a63a73efe4812a3f
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-02-22 10:28:54 -0800

    LPS-63525 Isolate Soy classpath in its own configuration

--- modules/sdk/gradle-plugins-soy/src/main/java/com/liferay/gradle/plugins/soy/SoyPlugin.java
+++ modules/sdk/gradle-plugins-soy/src/main/java/com/liferay/gradle/plugins/soy/SoyPlugin.java
@@ -23,12 +23,16 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.Callable;
 
+import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.DependencySet;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.file.SourceDirectorySet;
 import org.gradle.api.plugins.BasePlugin;
 import org.gradle.api.tasks.SourceSet;
+import org.gradle.api.tasks.TaskContainer;
 
 /**
  * @author Andrea Di Giorgi
@@ -37,9 +41,42 @@ public class SoyPlugin implements Plugin<Project> {
 
 	public static final String BUILD_SOY_TASK_NAME = "buildSoy";
 
+	public static final String CONFIGURATION_NAME = "soy";
+
 	@Override
 	public void apply(Project project) {
+		Configuration soyConfiguration = addConfigurationSoy(project);
+
 		addTaskBuildSoy(project);
+
+		configureTasksBuildSoy(project, soyConfiguration);
+	}
+
+	protected Configuration addConfigurationSoy(final Project project) {
+		Configuration configuration = GradleUtil.addConfiguration(
+			project, CONFIGURATION_NAME);
+
+		configuration.defaultDependencies(
+			new Action<DependencySet>() {
+
+				@Override
+				public void execute(DependencySet dependencySet) {
+					addDependenciesSoy(project);
+				}
+
+			});
+
+		configuration.setDescription(
+			"Configures Closure Templates for this project.");
+		configuration.setVisible(false);
+
+		return configuration;
+	}
+
+	protected void addDependenciesSoy(Project project) {
+		GradleUtil.addDependency(
+			project, CONFIGURATION_NAME, "com.google.template", "soy",
+			_VERSION);
 	}
 
 	protected BuildSoyTask addTaskBuildSoy(final Project project) {
@@ -49,20 +86,6 @@ public class SoyPlugin implements Plugin<Project> {
 		buildSoyTask.setDescription(
 			"Compiles Closure Templates into JavaScript functions.");
 		buildSoyTask.setGroup(BasePlugin.BUILD_GROUP);
-
-		buildSoyTask.setClasspath(
-			new Callable<FileCollection>() {
-
-				@Override
-				public FileCollection call() throws Exception {
-					SourceSet sourceSet = GradleUtil.getSourceSet(
-						project, SourceSet.MAIN_SOURCE_SET_NAME);
-
-					return sourceSet.getCompileClasspath();
-				}
-
-			});
-
 		buildSoyTask.setIncludes(Collections.singleton("**/*.soy"));
 
 		buildSoyTask.setSource(
@@ -76,6 +99,30 @@ public class SoyPlugin implements Plugin<Project> {
 			});
 
 		return buildSoyTask;
+	}
+
+	protected void configureTaskBuildSoyClasspath(
+		BuildSoyTask buildSoyTask, FileCollection fileCollection) {
+
+		buildSoyTask.setClasspath(fileCollection);
+	}
+
+	protected void configureTasksBuildSoy(
+		Project project, final Configuration soyConfiguration) {
+
+		TaskContainer taskContainer = project.getTasks();
+
+		taskContainer.withType(
+			BuildSoyTask.class,
+			new Action<BuildSoyTask>() {
+
+				@Override
+				public void execute(BuildSoyTask buildSoyTask) {
+					configureTaskBuildSoyClasspath(
+						buildSoyTask, soyConfiguration);
+				}
+
+			});
 	}
 
 	protected File getResourcesDir(Project project) {
@@ -92,5 +139,7 @@ public class SoyPlugin implements Plugin<Project> {
 
 		return iterator.next();
 	}
+
+	private static final String _VERSION = "2012-12-21";
 
 }
