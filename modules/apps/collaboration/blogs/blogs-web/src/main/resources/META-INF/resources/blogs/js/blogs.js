d95eea10aff31cbd98afbfdd89b99eeff7dd414e
Roberto Díaz <roberto.diaz@liferay.com>
2016-02-23 12:56:37 +0100

    LPS-63508 Change interaction. Now the extra settings are going to be fieldsets as in other portlets

--- modules/apps/collaboration/blogs/blogs-web/src/main/resources/META-INF/resources/blogs/js/blogs.js
+++ modules/apps/collaboration/blogs/blogs-web/src/main/resources/META-INF/resources/blogs/js/blogs.js
@@ -139,16 +139,6 @@ AUI.add(
 							);
 						}
 
-						instance._editIcon = instance.one('#editIcon');
-						instance._settingsIcon = instance.one('#settingsIcon');
-
-						eventHandles.push(
-							instance._editIcon.on(STR_CLICK, instance._switchView, instance),
-							instance._settingsIcon.on(STR_CLICK, instance._switchView, instance)
-						);
-
-						instance._editSection = instance.one('#editSection');
-						instance._settingsSection = instance.one('#settingsSection');
 						instance._eventHandles = eventHandles;
 					},
 
@@ -417,24 +407,6 @@ AUI.add(
 
 						if (captionNode) {
 							captionNode.removeClass(CSS_INVISIBLE);
-						}
-					},
-
-					_switchView: function() {
-						var instance = this;
-
-						var descriptionEditor = window[instance.ns('descriptionEditor')];
-
-						instance._editSection.toggle();
-						instance._settingsSection.toggle();
-
-						instance._editIcon.toggle();
-						instance._settingsIcon.toggle();
-
-						if (!descriptionEditor.instanceReady) {
-							descriptionEditor.create();
-
-							instance.setDescription(window[instance.ns('contentEditor')].getText());
 						}
 					},
 
