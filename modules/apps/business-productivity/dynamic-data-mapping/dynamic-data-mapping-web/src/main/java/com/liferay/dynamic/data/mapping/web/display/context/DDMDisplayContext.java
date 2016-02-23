abd9d262701418c58c21a5819fef696a216db34d
Inacio Nery <inacio.nery@liferay.com>
2016-02-23 09:14:29 -0300

    LPS-63353 Add methods to remove static reference

--- modules/apps/business-productivity/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/display/context/DDMDisplayContext.java
+++ modules/apps/business-productivity/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/display/context/DDMDisplayContext.java
@@ -14,31 +14,71 @@
 
 package com.liferay.dynamic.data.mapping.web.display.context;
 
+import com.liferay.dynamic.data.mapping.configuration.DDMGroupServiceConfiguration;
 import com.liferay.dynamic.data.mapping.constants.DDMPortletKeys;
+import com.liferay.dynamic.data.mapping.io.DDMFormJSONDeserializer;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.web.configuration.DDMWebConfiguration;
+import com.liferay.dynamic.data.mapping.web.context.util.DDMWebRequestHelper;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.PortalPreferences;
 import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.Validator;
 
-import javax.portlet.PortletRequest;
+import javax.portlet.RenderRequest;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Rafael Praxedes
  */
 public class DDMDisplayContext {
 
-	public DDMDisplayContext(PortletRequest portletRequest)
+	public DDMDisplayContext(
+			RenderRequest renderRequest,
+			DDMFormJSONDeserializer ddmFormJSONDeserializer,
+			DDMWebConfiguration ddmWebConfiguration)
 		throws PortalException {
 
-		_portletRequest = portletRequest;
+		_renderRequest = renderRequest;
+		_ddmFormJSONDeserializer = ddmFormJSONDeserializer;
+		_ddmWebConfiguration = ddmWebConfiguration;
+
+		HttpServletRequest httpServletRequest =
+			PortalUtil.getHttpServletRequest(renderRequest);
+
+		_ddmWebRequestHelper = new DDMWebRequestHelper(httpServletRequest);
+	}
+
+	public boolean autogenerateStructureKey() {
+		return _ddmWebConfiguration.autogenerateStructureKey();
+	}
+
+	public boolean autogenerateTemplateKey() {
+		return _ddmWebConfiguration.autogenerateTemplateKey();
+	}
+
+	public boolean changeableDefaultLanguage() {
+		return _ddmWebConfiguration.changeableDefaultLanguage();
+	}
+
+	public DDMForm deserialize(String serializedDDMForm)
+		throws PortalException {
+
+		return _ddmFormJSONDeserializer.deserialize(serializedDDMForm);
+	}
+
+	public DDMGroupServiceConfiguration getDDMGroupServiceConfiguration() {
+		return _ddmWebRequestHelper.getDDMGroupServiceConfiguration();
 	}
 
 	public String getOrderByCol() {
 		PortalPreferences portalPreferences =
-			PortletPreferencesFactoryUtil.getPortalPreferences(_portletRequest);
+			PortletPreferencesFactoryUtil.getPortalPreferences(_renderRequest);
 
-		String orderByCol = ParamUtil.getString(_portletRequest, "orderByCol");
+		String orderByCol = ParamUtil.getString(_renderRequest, "orderByCol");
 
 		if (Validator.isNull(orderByCol)) {
 			orderByCol = portalPreferences.getValue(
@@ -56,10 +96,9 @@ public class DDMDisplayContext {
 
 	public String getOrderByType() {
 		PortalPreferences portalPreferences =
-			PortletPreferencesFactoryUtil.getPortalPreferences(_portletRequest);
+			PortletPreferencesFactoryUtil.getPortalPreferences(_renderRequest);
 
-		String orderByType = ParamUtil.getString(
-			_portletRequest, "orderByType");
+		String orderByType = ParamUtil.getString(_renderRequest, "orderByType");
 
 		if (Validator.isNull(orderByType)) {
 			orderByType = portalPreferences.getValue(
@@ -75,6 +114,9 @@ public class DDMDisplayContext {
 		return orderByType;
 	}
 
-	private final PortletRequest _portletRequest;
+	private final DDMFormJSONDeserializer _ddmFormJSONDeserializer;
+	private final DDMWebConfiguration _ddmWebConfiguration;
+	private final DDMWebRequestHelper _ddmWebRequestHelper;
+	private final RenderRequest _renderRequest;
 
 }
