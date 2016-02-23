06fe050d5e08e59648c87531152802f313925ae3
Brian Chan <brian.chan@liferay.com>
2016-02-23 11:13:27 -0800

    LPS-63232 SF

--- modules/apps/collaboration/invitation/invitation-invite-members-service/src/main/java/com/liferay/invitation/invite/members/model/impl/MemberRequestImpl.java
+++ modules/apps/collaboration/invitation/invitation-invite-members-service/src/main/java/com/liferay/invitation/invite/members/model/impl/MemberRequestImpl.java
@@ -17,22 +17,11 @@ package com.liferay.invitation.invite.members.model.impl;
 import aQute.bnd.annotation.ProviderType;
 
 /**
- * The extended model implementation for the MemberRequest service. Represents a row in the &quot;MemberRequest&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.invitation.invite.members.model.MemberRequest} interface.
- * </p>
- *
  * @author Brian Wing Shun Chan
  */
 @ProviderType
 public class MemberRequestImpl extends MemberRequestBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a member request model instance should use the {@link com.liferay.invitation.invite.members.model.MemberRequest} interface instead.
-	 */
 	public MemberRequestImpl() {
 	}
 
