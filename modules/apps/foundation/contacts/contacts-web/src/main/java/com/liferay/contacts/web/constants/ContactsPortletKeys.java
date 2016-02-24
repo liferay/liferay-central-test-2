4a5a6ca91edce19435310b3e02b2481178936f51
drewbrokke <drew.brokke@liferay.com>
2016-02-24 15:58:49 -0800

    LPS-63476 contacts-web - Keys - renames PortletKeys to ContactsPortletKeys

--- modules/apps/foundation/contacts/contacts-web/src/main/java/com/liferay/contacts/web/constants/ContactsPortletKeys.java
+++ modules/apps/foundation/contacts/contacts-web/src/main/java/com/liferay/contacts/web/constants/ContactsPortletKeys.java
@@ -17,7 +17,7 @@ package com.liferay.contacts.web.constants;
 /**
  * @author Ryan Park
  */
-public class PortletKeys {
+public class ContactsPortletKeys {
 
 	public static final String CONTACTS_CENTER = "1_WAR_contactsportlet";
 
