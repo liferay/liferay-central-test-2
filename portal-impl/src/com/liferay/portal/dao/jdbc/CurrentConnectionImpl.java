c9fa5d9b256a8ed11dc75eea550bef1b1f5cc87c
shuyangzhou <shuyang.zhou@liferay.com>
2016-02-23 22:11:11 -0800

    LPS-63499 The simplest right fix, not the best right fix. As there is no easy way allows us to promotely release the reference count in order to promotely release the connection. We will work on the best right fix later which requires more complex refactor.

--- portal-impl/src/com/liferay/portal/dao/jdbc/CurrentConnectionImpl.java
+++ portal-impl/src/com/liferay/portal/dao/jdbc/CurrentConnectionImpl.java
@@ -41,9 +41,10 @@ public class CurrentConnectionImpl implements CurrentConnection {
 
 			return null;
 		}
-		else {
-			return connectionHolder.getConnection();
-		}
+
+		connectionHolder.requested();
+
+		return connectionHolder.getConnection();
 	}
 
 }
