227cf05f61e280dd7b7c543ceb3c537924bf8b83
Julio Camarero <julio.camarero@liferay.com>
2016-02-24 16:35:19 +0100

    LPS-63608 move expand action to the tree

--- modules/apps/web-experience-management/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/display/context/LayoutsTreeDisplayContext.java
+++ modules/apps/web-experience-management/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/display/context/LayoutsTreeDisplayContext.java
@@ -230,6 +230,20 @@ public class LayoutsTreeDisplayContext extends BaseLayoutDisplayContext {
 			ActionKeys.MANAGE_LAYOUTS);
 	}
 
+	public boolean isShowExpandLayoutSetButton(boolean privateLayout) {
+		Group selGroup = getSelGroup();
+
+		if (privateLayout && selGroup.hasPrivateLayouts()) {
+			return true;
+		}
+
+		if (!privateLayout && selGroup.hasPublicLayouts()) {
+			return true;
+		}
+
+		return false;
+	}
+
 	public boolean isShowLayoutSetBranchesSelector() {
 		Group stagingGroup = getStagingGroup();
 
