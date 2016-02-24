a0b22a1a8bcb4ac22f034db32fa55c1493dc61ef
Raymond Auge <raymond.auge@liferay.com>
2016-02-24 12:54:05 -0500

    LPS-62693 factory module for creating ServletContextHelper instances

---
+++ modules/apps/platform/portal-osgi-web/portal-osgi-web-servlet-context-helper/src/main/java/com/liferay/portal/osgi/web/servlet/context/helper/ServletContextHelperFactory.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.osgi.web.servlet.context.helper;
+
+import com.liferay.portal.osgi.web.servlet.context.helper.internal.ServletContextHelperRegistrationFactory;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.http.runtime.HttpServiceRuntime;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(immediate = true, service = ServletContextHelperFactory.class)
+public class ServletContextHelperFactory {
+
+	@Activate
+	protected void activate(BundleContext bundleContext) throws Exception {
+		_serviceRegistration = bundleContext.registerService(
+			ServletContextHelperRegistration.class.getName(),
+			new ServletContextHelperRegistrationFactory(), null);
+	}
+
+	@Deactivate
+	protected void deactivate(BundleContext bundleContext) throws Exception {
+		_serviceRegistration.unregister();
+	}
+
+	@Reference
+	private HttpServiceRuntime _httpServiceRuntime;
+
+	private ServiceRegistration<?> _serviceRegistration;
+
+}
---
+++ modules/apps/platform/portal-osgi-web/portal-osgi-web-servlet-context-helper/src/main/java/com/liferay/portal/osgi/web/servlet/context/helper/internal/ServletContextHelperRegistrationFactory.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.osgi.web.servlet.context.helper.internal;
+
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.osgi.web.servlet.context.helper.ServletContextHelperRegistration;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import javax.servlet.ServletContextListener;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceFactory;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.http.context.ServletContextHelper;
+import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
+
+/**
+ * @author Juan Gonzalez
+ * @author Raymond Augé
+ */
+public class ServletContextHelperRegistrationFactory
+	implements ServiceFactory<ServletContextHelperRegistration> {
+
+	@Override
+	public ServletContextHelperRegistration getService(
+		Bundle bundle,
+		ServiceRegistration<ServletContextHelperRegistration>
+			registration) {
+
+		Dictionary<String, Object> properties = new Hashtable<>();
+
+		String contextPath = getContextPath(bundle);
+		String servletContextName = getServletContextName(bundle, contextPath);
+
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_NAME,
+			servletContextName);
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_PATH, contextPath);
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_SELECT,
+			"(" + HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_NAME + "=" +
+				servletContextName + ")");
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_LISTENER,
+			Boolean.TRUE.toString());
+		properties.put("rtl.required", Boolean.TRUE.toString());
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		CustomServletContextHelper customServletContextHelper =
+			new CustomServletContextHelper(bundle);
+
+		ServiceRegistration<?> serviceRegistration =
+			bundleContext.registerService(
+				new String[] {
+					ServletContextHelper.class.getName(),
+					ServletContextListener.class.getName()
+				},
+				customServletContextHelper, properties);
+
+		return new ServletContextHelperRegistrationImpl(
+			(ServiceRegistration<ServletContextHelper>)serviceRegistration,
+			customServletContextHelper);
+	}
+
+	@Override
+	public void ungetService(
+		Bundle bundle,
+		ServiceRegistration<ServletContextHelperRegistration> registration,
+		ServletContextHelperRegistration service) {
+	}
+
+	protected String getServletContextName(Bundle bundle, String contextPath) {
+		Dictionary<String, String> headers = bundle.getHeaders();
+
+		String header = headers.get("Web-ContextName");
+
+		if (Validator.isNotNull(header)) {
+			return header;
+		}
+
+		contextPath = contextPath.substring(1);
+
+		return contextPath.replaceAll("[^a-zA-Z0-9\\-]", "");
+	}
+
+	private String getContextPath(Bundle bundle) {
+		Dictionary<String, String> headers = bundle.getHeaders();
+
+		String contextPath = headers.get("Web-ContextPath");
+
+		if (Validator.isNotNull(contextPath)) {
+			return contextPath;
+		}
+
+		String symbolicName = bundle.getSymbolicName();
+
+		return '/' + symbolicName.replaceAll("[^a-zA-Z0-9]", "");
+	}
+
+}
