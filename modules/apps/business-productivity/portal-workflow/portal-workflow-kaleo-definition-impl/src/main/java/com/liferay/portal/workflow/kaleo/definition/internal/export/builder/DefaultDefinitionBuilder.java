845b54d60952a235956ec6abb738d18529b80afa
Michael Han <michael.han@liferay.com>
2016-02-25 09:45:59 -0600

    LPS-63470 SF

--- modules/apps/business-productivity/portal-workflow/portal-workflow-kaleo-definition-impl/src/main/java/com/liferay/portal/workflow/kaleo/definition/internal/export/builder/DefaultDefinitionBuilder.java
+++ modules/apps/business-productivity/portal-workflow/portal-workflow-kaleo-definition-impl/src/main/java/com/liferay/portal/workflow/kaleo/definition/internal/export/builder/DefaultDefinitionBuilder.java
@@ -30,6 +30,9 @@ import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Michael C. Han
@@ -107,14 +110,26 @@ public class DefaultDefinitionBuilder implements DefinitionBuilder {
 		return definition;
 	}
 
-	@Reference
-	private KaleoDefinitionLocalService _kaleoDefinitionLocalService;
+	@Reference(
+		cardinality = ReferenceCardinality.OPTIONAL,
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY
+	)
+	private volatile KaleoDefinitionLocalService _kaleoDefinitionLocalService;
 
-	@Reference
-	private KaleoNodeLocalService _kaleoNodeLocalService;
+	@Reference(
+		cardinality = ReferenceCardinality.OPTIONAL,
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY
+	)
+	private volatile KaleoNodeLocalService _kaleoNodeLocalService;
 
-	@Reference
-	private KaleoTransitionLocalService _kaleoTransitionLocalService;
+	@Reference(
+		cardinality = ReferenceCardinality.OPTIONAL,
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY
+	)
+	private volatile KaleoTransitionLocalService _kaleoTransitionLocalService;
 
 	@Reference
 	private NodeBuilderRegistry _nodeBuilderRegistry;
