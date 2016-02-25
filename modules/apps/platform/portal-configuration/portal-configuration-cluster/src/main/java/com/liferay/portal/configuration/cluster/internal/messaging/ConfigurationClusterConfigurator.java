e886e7f1981470b3cab47105d713284ed30cf5d9
Michael Han <michael.han@liferay.com>
2016-02-24 22:46:40 -0600

    LPS-63470 move packages

--- modules/apps/platform/portal-configuration/portal-configuration-cluster/src/main/java/com/liferay/portal/configuration/cluster/internal/messaging/ConfigurationClusterConfigurator.java
+++ modules/apps/platform/portal-configuration/portal-configuration-cluster/src/main/java/com/liferay/portal/configuration/cluster/internal/messaging/ConfigurationClusterConfigurator.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.configuration.cluster;
+package com.liferay.portal.configuration.cluster.internal.messaging;
 
 import com.liferay.portal.configuration.cluster.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.Destination;
