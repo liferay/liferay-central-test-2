e61517aeb5683a0bd1fb911b58776261d2d3c5e6
Michael Han <michael.han@liferay.com>
2016-02-24 22:19:56 -0600

    LPS-63470 move between modules

--- modules/apps/business-productivity/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/internal/runtime/action/executor/ActionExecutorManagerImpl.java
+++ modules/apps/business-productivity/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/internal/runtime/action/executor/ActionExecutorManagerImpl.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.workflow.kaleo.runtime.action.executor.internal;
+package com.liferay.portal.workflow.kaleo.internal.runtime.action.executor;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.util.ClassUtil;
