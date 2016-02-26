d98af8f78b77fea0f7b6050ddc2b58735f97b4b1
Mika Koivisto <mika.koivisto@liferay.com>
2016-02-26 12:27:46 +0200

    LPS-63076 Fix test

--- portal-impl/test/unit/com/liferay/portal/servlet/ModulePathContainerTest.java
+++ portal-impl/test/unit/com/liferay/portal/servlet/ModulePathContainerTest.java
@@ -14,10 +14,14 @@
 
 package com.liferay.portal.servlet;
 
+import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.tools.ToolDependencies;
+import com.liferay.portal.util.HttpImpl;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -31,6 +35,15 @@ public class ModulePathContainerTest {
 	@BeforeClass
 	public static void setUpClass() throws Exception {
 		ToolDependencies.wireCaches();
+
+		_http = HttpUtil.getHttp();
+
+		_httpUtil.setHttp(new HttpImpl());
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		_httpUtil.setHttp(_http);
 	}
 
 	@Test
@@ -62,5 +75,8 @@ public class ModulePathContainerTest {
 		Assert.assertEquals(
 			StringPool.BLANK, ComboServlet.getResourcePath(modulePath));
 	}
+
+	private static Http _http;
+	private static final HttpUtil _httpUtil = new HttpUtil();
 
 }
