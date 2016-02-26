fb6b0b2f5a11529e1f07ac07a70630f0154dd51f
Inacio Nery <inacio.nery@liferay.com>
2016-02-26 13:48:41 -0300

    LPS-63727 Add upgrade to create an instanceId on journal article image

--- modules/apps/web-experience-management/journal/journal-service/src/main/java/com/liferay/journal/upgrade/v1_0_0/UpgradeJournalArticleImage.java
+++ modules/apps/web-experience-management/journal/journal-service/src/main/java/com/liferay/journal/upgrade/v1_0_0/UpgradeJournalArticleImage.java
@@ -16,6 +16,8 @@ package com.liferay.journal.upgrade.v1_0_0;
 
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.upgrade.AutoBatchPreparedStatementUtil;
 
 import java.sql.PreparedStatement;
@@ -28,10 +30,44 @@ public class UpgradeJournalArticleImage extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
-		updateJournalArticleImages();
+		updateJournalArticleImagesInstanceId();
+		updateJournalArticleImagesName();
 	}
 
-	protected void updateJournalArticleImages() throws Exception {
+	protected void updateJournalArticleImagesInstanceId() throws Exception {
+		try (PreparedStatement ps1 = connection.prepareStatement(
+				"select articleid, elInstanceId, elName from " +
+					"JournalArticleImage group by articleid, elName");
+			ResultSet rs = ps1.executeQuery()) {
+
+			try (PreparedStatement ps2 =
+					AutoBatchPreparedStatementUtil.autoBatch(
+						connection.prepareStatement(
+							"update JournalArticleImage set elInstanceId = ? " +
+								"where articleid = ? and elName = ?"))) {
+
+				while (rs.next()) {
+					long articleid = rs.getLong(1);
+					String elInstanceId = rs.getString(2);
+					String elName = rs.getString(3);
+
+					if (Validator.isNull(elInstanceId)) {
+						elInstanceId = StringUtil.randomString(4);
+					}
+
+					ps2.setString(1, elInstanceId);
+					ps2.setLong(2, articleid);
+					ps2.setString(3, elName);
+
+					ps2.addBatch();
+				}
+
+				ps2.executeBatch();
+			}
+		}
+	}
+
+	protected void updateJournalArticleImagesName() throws Exception {
 		try (PreparedStatement ps1 = connection.prepareStatement(
 				"select articleImageId, elName from JournalArticleImage");
 			ResultSet rs = ps1.executeQuery()) {
