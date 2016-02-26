fc250f180372e503e73e357692e870c2da50047b
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2016-02-26 10:33:16 +0100

    LPS-63500 Fix missing resource permissions for portal entities used in permission framework - Role, LayoutSetPrototype, LayoutPrototype and LayoutBranch

--- portal-impl/src/com/liferay/portal/verify/model/RoleVerifiableModel.java
+++ portal-impl/src/com/liferay/portal/verify/model/RoleVerifiableModel.java
@@ -14,18 +14,26 @@
 
 package com.liferay.portal.verify.model;
 
+import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.verify.model.VerifiableAuditedModel;
+import com.liferay.portal.kernel.verify.model.VerifiableResourcedModel;
 import com.liferay.portal.kernel.verify.model.VerifiableUUIDModel;
 
 /**
  * @author Miguel Pastor
  */
 public class RoleVerifiableModel
-	implements VerifiableAuditedModel, VerifiableUUIDModel {
+	implements VerifiableAuditedModel, VerifiableResourcedModel,
+			   VerifiableUUIDModel {
 
 	@Override
 	public String getJoinByTableName() {
 		return null;
+	}
+
+	@Override
+	public String getModelName() {
+		return Role.class.getName();
 	}
 
 	@Override
@@ -46,6 +54,11 @@ public class RoleVerifiableModel
 	@Override
 	public String getTableName() {
 		return "Role_";
+	}
+
+	@Override
+	public String getUserIdColumnName() {
+		return "userId";
 	}
 
 	@Override
