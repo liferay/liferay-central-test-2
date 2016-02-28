7c8837d06f3b1259f4c352b337a7738acb63b8d9
André de Oliveira <andre.oliveira@liferay.com>
2016-02-28 14:45:41 -0300

    LPS-63739 Replicate the bug with unit tests
    https://issues.liferay.com/browse/LPS-63739

---
+++ portal-impl/test/integration/com/liferay/portal/kernel/search/UserSearchFixture.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.search;
+
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author André de Oliveira
+ */
+public class UserSearchFixture {
+
+	public Group addGroup() throws Exception {
+		Group group = GroupTestUtil.addGroup();
+
+		_groups.add(group);
+
+		return group;
+	}
+
+	public void addUser(Group group, String tag) throws Exception {
+		User user = UserTestUtil.addUser(group.getGroupId());
+
+		_users.add(user);
+
+		ServiceContext serviceContext = getServiceContext(group);
+
+		serviceContext.setAssetTagNames(new String[] {tag});
+
+		UserTestUtil.updateUser(user, serviceContext);
+	}
+
+	public List<Group> getGroups() {
+		return _groups;
+	}
+
+	public List<User> getUsers() {
+		return _users;
+	}
+
+	public void setUp() {
+	}
+
+	public void tearDown() {
+	}
+
+	protected static ServiceContext getServiceContext(Group group)
+		throws Exception {
+
+		return ServiceContextTestUtil.getServiceContext(
+			group.getGroupId(), TestPropsValues.getUserId());
+	}
+
+	private final List<Group> _groups = new ArrayList<>();
+	private final List<User> _users = new ArrayList<>();
+
+}
