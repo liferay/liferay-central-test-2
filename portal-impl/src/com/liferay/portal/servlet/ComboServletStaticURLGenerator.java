1b2749c4586d3ec7b4c798e7578697764e0a5b06
Chema Balsas <jose.balsas@liferay.com>
2016-03-01 19:09:59 +0100

    LPS-62805 Add proxy path to ComboServletStaticURLGenerator

--- portal-impl/src/com/liferay/portal/servlet/ComboServletStaticURLGenerator.java
+++ portal-impl/src/com/liferay/portal/servlet/ComboServletStaticURLGenerator.java
@@ -18,6 +18,7 @@ import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.PredicateFilter;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
@@ -57,7 +58,9 @@ public class ComboServletStaticURLGenerator {
 					String url = portletResource;
 
 					if (!HttpUtil.hasProtocol(portletResource)) {
-						url = portlet.getContextPath() + portletResource;
+						url =
+							PortalUtil.getPathProxy() +
+								portlet.getContextPath() + portletResource;
 					}
 
 					if (_visitedURLs.contains(url)) {
