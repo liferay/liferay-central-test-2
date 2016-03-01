33bdc501348f5274aba441dc0e66a80ae664a344
Inacio Nery <inacio.nery@liferay.com>
2016-03-01 16:52:11 -0300

    LPS-63899 Convert StorageAdapterRegistry into a Component

--- modules/apps/business-productivity/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/storage/impl/StorageAdapterRegistryImpl.java
+++ modules/apps/business-productivity/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/storage/impl/StorageAdapterRegistryImpl.java
@@ -16,42 +16,27 @@ package com.liferay.dynamic.data.mapping.storage.impl;
 
 import com.liferay.dynamic.data.mapping.storage.StorageAdapter;
 import com.liferay.dynamic.data.mapping.storage.StorageAdapterRegistry;
-import com.liferay.osgi.util.ServiceTrackerFactory;
+import com.liferay.dynamic.data.mapping.storage.StorageType;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
-import org.osgi.framework.ServiceReference;
-import org.osgi.util.tracker.ServiceTracker;
-import org.osgi.util.tracker.ServiceTrackerCustomizer;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Marcellus Tavares
  */
+@Component(immediate = true)
 public class StorageAdapterRegistryImpl implements StorageAdapterRegistry {
-
-	public StorageAdapterRegistryImpl() {
-		Class<?> clazz = getClass();
-
-		Bundle bundle = FrameworkUtil.getBundle(clazz);
-
-		_bundleContext = bundle.getBundleContext();
-
-		_serviceTracker = ServiceTrackerFactory.open(
-			_bundleContext,
-			"(&(objectClass=" + StorageAdapter.class.getName() +
-				")(!(objectClass=" + clazz.getName() + ")))",
-			new StorageAdapterServiceTrackerCustomizer());
-	}
 
 	@Override
 	public StorageAdapter getDefaultStorageAdapter() {
-		return _storageAdaptersMap.get(_defaultStorageType);
+		return _storageAdaptersMap.get(StorageType.JSON.getValue());
 	}
 
 	@Override
@@ -64,54 +49,21 @@ public class StorageAdapterRegistryImpl implements StorageAdapterRegistry {
 		return _storageAdaptersMap.keySet();
 	}
 
-	public void setDefaultStorageType(String storageType) {
-		_defaultStorageType = storageType;
+	@Reference(
+		cardinality = ReferenceCardinality.MULTIPLE,
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY, service = StorageAdapter.class
+	)
+	public void setStorageAdapter(StorageAdapter storageAdapter) {
+		_storageAdaptersMap.put(
+			storageAdapter.getStorageType(), storageAdapter);
 	}
 
-	public void setStorageAdapters(List<StorageAdapter> storageAdapters) {
-		for (StorageAdapter storageAdapter : storageAdapters) {
-			_bundleContext.registerService(
-				StorageAdapter.class, storageAdapter, null);
-		}
+	public void unsetStorageAdapter(StorageAdapter storageAdapter) {
+		_storageAdaptersMap.remove(storageAdapter);
 	}
 
-	private final BundleContext _bundleContext;
-	private String _defaultStorageType;
-	private final ServiceTracker<StorageAdapter, StorageAdapter>
-		_serviceTracker;
 	private final Map<String, StorageAdapter> _storageAdaptersMap =
 		new ConcurrentHashMap<>();
-
-	private class StorageAdapterServiceTrackerCustomizer
-		implements ServiceTrackerCustomizer<StorageAdapter, StorageAdapter> {
-
-		@Override
-		public StorageAdapter addingService(
-			ServiceReference<StorageAdapter> serviceReference) {
-
-			StorageAdapter storageAdapter = _bundleContext.getService(
-				serviceReference);
-
-			_storageAdaptersMap.put(
-				storageAdapter.getStorageType(), storageAdapter);
-
-			return storageAdapter;
-		}
-
-		@Override
-		public void modifiedService(
-			ServiceReference<StorageAdapter> serviceReference,
-			StorageAdapter storageAdapter) {
-		}
-
-		@Override
-		public void removedService(
-			ServiceReference<StorageAdapter> serviceReference,
-			StorageAdapter storageAdapter) {
-
-			_bundleContext.ungetService(serviceReference);
-		}
-
-	}
 
 }
