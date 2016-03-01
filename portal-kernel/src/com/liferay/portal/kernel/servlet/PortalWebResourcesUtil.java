343b2c5eb62ee7d470ee0c1d0562168994b9b86d
Brian Chan <brian.chan@liferay.com>
2016-03-01 14:16:24 -0800

    LPS-62805 SF

--- portal-kernel/src/com/liferay/portal/kernel/servlet/PortalWebResourcesUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/servlet/PortalWebResourcesUtil.java
@@ -36,8 +36,9 @@ import javax.servlet.ServletContext;
 public class PortalWebResourcesUtil {
 
 	public static String getContextPath(String resourceType) {
-		return PortalUtil.getPathContext().concat(
-			getModuleContextPath(resourceType));
+		String contextPath = PortalUtil.getPathContext();
+
+		return contextPath.concat(getModuleContextPath(resourceType));
 	}
 
 	public static long getLastModified(String resourceType) {
