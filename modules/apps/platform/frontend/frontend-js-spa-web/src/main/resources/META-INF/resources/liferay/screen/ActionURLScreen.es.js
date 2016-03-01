bf5ff4ed4d4ebb5d20594f1230f19d0efc6201da
Bruno Basto <bruno.basto@liferay.com>
2016-02-29 21:13:19 -0300

    LPS-63707 Sets correct lifecycle for redirects

--- modules/apps/platform/frontend/frontend-js-spa-web/src/main/resources/META-INF/resources/liferay/screen/ActionURLScreen.es.js
+++ modules/apps/platform/frontend/frontend-js-spa-web/src/main/resources/META-INF/resources/liferay/screen/ActionURLScreen.es.js
@@ -1,12 +1,30 @@
 'use strict';
 
 import EventScreen from './EventScreen.es';
+import Uri from 'metal-uri/src/Uri';
+import utils from 'senna/src/utils/utils';
 
 class ActionURLScreen extends EventScreen {
 	constructor() {
 		super();
 
 		this.httpMethod = 'POST';
+	}
+
+	getRequestPath() {
+		var request = this.getRequest();
+
+		if (request) {
+			let uri = new Uri(request.responseURL);
+
+			if (uri.getParameterValue('p_p_lifecycle') === '1') {
+				uri.setParameterValue('p_p_lifecycle', '0');
+			}
+
+			return utils.getUrlPath(uri.toString());
+		}
+
+		return null;
 	}
 }
 
