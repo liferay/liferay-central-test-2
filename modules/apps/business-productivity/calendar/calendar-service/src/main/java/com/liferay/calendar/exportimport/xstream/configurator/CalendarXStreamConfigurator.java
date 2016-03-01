2c53cb1363a4e0fc06abd8d726f062471df6886d
Máté Thurzó <mate.thurzo@liferay.com>
2016-03-01 13:49:42 +0100

    LPS-63674 Apply to business productivity

--- modules/apps/business-productivity/calendar/calendar-service/src/main/java/com/liferay/calendar/exportimport/xstream/configurator/CalendarXStreamConfigurator.java
+++ modules/apps/business-productivity/calendar/calendar-service/src/main/java/com/liferay/calendar/exportimport/xstream/configurator/CalendarXStreamConfigurator.java
@@ -21,8 +21,8 @@ import com.liferay.calendar.model.impl.CalendarResourceImpl;
 import com.liferay.exportimport.kernel.xstream.XStreamAlias;
 import com.liferay.exportimport.kernel.xstream.XStreamConverter;
 import com.liferay.exportimport.kernel.xstream.XStreamType;
-import com.liferay.exportimport.xstream.configurator.XStreamConfigurator;
 import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.xstream.configurator.XStreamConfigurator;
 
 import java.util.List;
 
