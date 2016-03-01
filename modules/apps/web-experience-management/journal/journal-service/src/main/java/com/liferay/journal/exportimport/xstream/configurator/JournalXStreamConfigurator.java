6124f9e045c24b3bae6ff815336700b8ff216e7e
Máté Thurzó <mate.thurzo@liferay.com>
2016-03-01 16:38:33 +0100

    LPS-63674 Apply to web experience management

--- modules/apps/web-experience-management/journal/journal-service/src/main/java/com/liferay/journal/exportimport/xstream/configurator/JournalXStreamConfigurator.java
+++ modules/apps/web-experience-management/journal/journal-service/src/main/java/com/liferay/journal/exportimport/xstream/configurator/JournalXStreamConfigurator.java
@@ -17,11 +17,11 @@ package com.liferay.journal.exportimport.xstream.configurator;
 import com.liferay.exportimport.kernel.xstream.XStreamAlias;
 import com.liferay.exportimport.kernel.xstream.XStreamConverter;
 import com.liferay.exportimport.kernel.xstream.XStreamType;
-import com.liferay.exportimport.xstream.configurator.XStreamConfigurator;
 import com.liferay.journal.model.impl.JournalArticleImpl;
 import com.liferay.journal.model.impl.JournalFeedImpl;
 import com.liferay.journal.model.impl.JournalFolderImpl;
 import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.xstream.configurator.XStreamConfigurator;
 
 import java.util.List;
 
--- modules/apps/web-experience-management/layout/layout-set-prototype-impl/src/main/java/com/liferay/layout/set/prototype/internal/exportimport/xstream/configurator/LayoutSetPrototypeXStreamConfigurator.java
+++ modules/apps/web-experience-management/layout/layout-set-prototype-impl/src/main/java/com/liferay/layout/set/prototype/internal/exportimport/xstream/configurator/LayoutSetPrototypeXStreamConfigurator.java
@@ -17,9 +17,9 @@ package com.liferay.layout.set.prototype.internal.exportimport.xstream.configura
 import com.liferay.exportimport.kernel.xstream.XStreamAlias;
 import com.liferay.exportimport.kernel.xstream.XStreamConverter;
 import com.liferay.exportimport.kernel.xstream.XStreamType;
-import com.liferay.exportimport.xstream.configurator.XStreamConfigurator;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.model.impl.LayoutSetPrototypeImpl;
+import com.liferay.xstream.configurator.XStreamConfigurator;
 
 import java.util.List;
 
