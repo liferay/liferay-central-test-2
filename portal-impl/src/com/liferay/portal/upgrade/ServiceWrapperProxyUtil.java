ff5c4eca01bd8060c60544eb052980fbad0640dd
shuyangzhou <shuyang.zhou@liferay.com>
2016-03-02 10:32:54 -0800

    LPS-63538 Create ServiceWrapperProxyUtil

---
+++ portal-impl/src/com/liferay/portal/upgrade/ServiceWrapperProxyUtil.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade;
+
+import com.liferay.portal.kernel.util.ProxyUtil;
+import com.liferay.portal.spring.aop.ServiceBeanAopProxy;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+import java.lang.reflect.Constructor;
+
+import org.springframework.aop.TargetSource;
+import org.springframework.aop.framework.AdvisedSupport;
+import org.springframework.aop.target.SingletonTargetSource;
+
+/**
+ * @author Shuyang Zhou
+ */
+public class ServiceWrapperProxyUtil {
+
+	public static Closeable createProxy(
+			Object springServiceProxy, Class<?> serviceWrapperClass)
+		throws Exception {
+
+		if (!ProxyUtil.isProxyClass(springServiceProxy.getClass())) {
+			throw new IllegalArgumentException(
+				springServiceProxy + " is not a Spring service proxy");
+		}
+
+		final AdvisedSupport advisedSupport =
+			ServiceBeanAopProxy.getAdvisedSupport(springServiceProxy);
+
+		final TargetSource targetSource = advisedSupport.getTargetSource();
+
+		final Object previousService = targetSource.getTarget();
+
+		Constructor<?>[] constructors =
+			serviceWrapperClass.getDeclaredConstructors();
+
+		Constructor<?> constructor = constructors[0];
+
+		constructor.setAccessible(true);
+
+		advisedSupport.setTargetSource(
+			new SingletonTargetSource(
+				constructor.newInstance(previousService)));
+
+		return new Closeable() {
+
+			@Override
+			public void close() throws IOException {
+				advisedSupport.setTargetSource(targetSource);
+
+				try {
+					targetSource.releaseTarget(previousService);
+				}
+				catch (Exception e) {
+					throw new IOException(e);
+				}
+			}
+
+		};
+	}
+
+}
