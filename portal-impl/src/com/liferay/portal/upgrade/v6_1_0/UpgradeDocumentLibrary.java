69623ba0f5f7aaf1b94582e8bc1355ed9c6abfbb
Cristina González Castellano <cristina.gonzalez@liferay.com>
2016-03-02 13:12:06 +0100

    LPS-63538 Use a Util for updating Sync (Fix NullPointerException)

--- portal-impl/src/com/liferay/portal/upgrade/v6_1_0/UpgradeDocumentLibrary.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_1_0/UpgradeDocumentLibrary.java
@@ -25,7 +25,6 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LoggingTimer;
 import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.SetUtil;
-import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.repository.liferayrepository.model.LiferayFileVersion;
 import com.liferay.portal.upgrade.v6_1_0.util.DLFileVersionTable;
@@ -34,7 +33,6 @@ import com.liferay.portlet.documentlibrary.model.impl.DLFileVersionImpl;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Timestamp;
 
 import java.util.Set;
 
@@ -45,31 +43,6 @@ import java.util.Set;
  * @author Minhchau Dang
  */
 public class UpgradeDocumentLibrary extends UpgradeProcess {
-
-	protected void addSync(
-			long syncId, long companyId, Timestamp createDate,
-			Timestamp modifiedDate, long fileId, long repositoryId,
-			long parentFolderId, String event, String type)
-		throws Exception {
-
-		try (PreparedStatement ps = connection.prepareStatement(
-				"insert into DLSync (syncId, companyId, createDate, " +
-					"modifiedDate, fileId, repositoryId, parentFolderId, " +
-						"event, type_) values (?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
-
-			ps.setLong(1, syncId);
-			ps.setLong(2, companyId);
-			ps.setTimestamp(3, createDate);
-			ps.setTimestamp(4, createDate);
-			ps.setLong(5, fileId);
-			ps.setLong(6, repositoryId);
-			ps.setLong(7, parentFolderId);
-			ps.setString(8, event);
-			ps.setString(9, type);
-
-			ps.executeUpdate();
-		}
-	}
 
 	@Override
 	protected void doUpgrade() throws Exception {
@@ -261,41 +234,6 @@ public class UpgradeDocumentLibrary extends UpgradeProcess {
 							"update Lock_ set key_ = '" + fileEntryId +
 								"' where lockId = " + lockId);
 					}
-				}
-			}
-		}
-	}
-
-	protected void updateSyncs() throws Exception {
-		try (LoggingTimer loggingTimer = new LoggingTimer()) {
-			StringBundler sb = new StringBundler(10);
-
-			sb.append("select DLFileEntry.fileEntryId as fileId, ");
-			sb.append("DLFileEntry.groupId as groupId, DLFileEntry.companyId");
-			sb.append(" as companyId, DLFileEntry.createDate as createDate, ");
-			sb.append("DLFileEntry.folderId as parentFolderId, 'file' as ");
-			sb.append("type from DLFileEntry union all select ");
-			sb.append("DLFolder.folderId as fileId, DLFolder.groupId as ");
-			sb.append("groupId, DLFolder.companyId as companyId, ");
-			sb.append("DLFolder.createDate as createDate, ");
-			sb.append("DLFolder.parentFolderId as parentFolderId, 'folder' ");
-			sb.append("as type from DLFolder");
-
-			try (PreparedStatement ps = connection.prepareStatement(
-					sb.toString());
-				ResultSet rs = ps.executeQuery()) {
-
-				while (rs.next()) {
-					long fileId = rs.getLong("fileId");
-					long groupId = rs.getLong("groupId");
-					long companyId = rs.getLong("companyId");
-					Timestamp createDate = rs.getTimestamp("createDate");
-					long parentFolderId = rs.getLong("parentFolderId");
-					String type = rs.getString("type");
-
-					addSync(
-						increment(), companyId, createDate, createDate, fileId,
-						groupId, parentFolderId, "add", type);
 				}
 			}
 		}
