3b49c5effd4d4aea740079e224cfe1990b78f016
Bruno Basto <bruno.basto@liferay.com>
2016-03-03 13:09:06 -0300

    LPS-63867 buildSoy

--- modules/apps/business-productivity/dynamic-data-mapping/dynamic-data-mapping-type-paragraph/src/main/resources/META-INF/resources/paragraph.soy.js
+++ modules/apps/business-productivity/dynamic-data-mapping/dynamic-data-mapping-type-paragraph/src/main/resources/META-INF/resources/paragraph.soy.js
@@ -5,5 +5,5 @@ if (typeof ddm == 'undefined') { var ddm = {}; }
 
 
 ddm.paragraph = function(opt_data, opt_ignored) {
-  return '\t<div class="form-group form-inline' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + ' liferay-ddm-form-field-paragraph" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '"><p dir="' + soy.$$escapeHtml(opt_data.dir) + '">' + soy.$$changeNewlineToBr(soy.$$escapeHtml(opt_data.text)) + '</p></div>';
+  return '\t<div class="form-group form-inline' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + ' liferay-ddm-form-field-paragraph" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '"><label class="control-label" for="' + soy.$$escapeHtml(opt_data.name) + '">' + soy.$$escapeHtml(opt_data.label) + '</label><div>' + soy.$$filterNoAutoescape(opt_data.text) + '</div></div>';
 };
