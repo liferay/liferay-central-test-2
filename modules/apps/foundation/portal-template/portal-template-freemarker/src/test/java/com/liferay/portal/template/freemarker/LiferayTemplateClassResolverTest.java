98d820a91544f995fe74d7c506afdc486b7f5954
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-03-03 15:02:01 +0100

    LPS-58170 Define test lifecycle
    SetUpClass: track the service implementing TemplateClassResolver
    SetUp: capture service's configuration to be able to restore it on tearDown
    TearDown: restore service's configuration to original state

--- modules/apps/foundation/portal-template/portal-template-freemarker/src/test/java/com/liferay/portal/template/freemarker/LiferayTemplateClassResolverTest.java
+++ modules/apps/foundation/portal-template/portal-template-freemarker/src/test/java/com/liferay/portal/template/freemarker/LiferayTemplateClassResolverTest.java
@@ -14,30 +14,84 @@
 
 package com.liferay.portal.template.freemarker;
 
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.registry.Filter;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceTracker;
+
+import freemarker.core.TemplateClassResolver;
+
 import freemarker.template.TemplateException;
 
+import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
 
 /**
  * @author Tomas Polesovsky
  */
+@RunWith(Arquillian.class)
 public class LiferayTemplateClassResolverTest {
 
+	@BeforeClass
+	public static void setUpClass() {
+		Registry registry = RegistryUtil.getRegistry();
+
+		Filter filter = registry.getFilter(
+			"(&(objectClass=" + TemplateClassResolver.class.getName() + "))");
+
+		_serviceTracker = registry.trackServices(filter);
+
+		_serviceTracker.open();
+
+		_liferayTemplateClassResolver =
+			(TemplateClassResolver)_serviceTracker.getService();
+	}
+
 	@Before
-	public void setUp() {
-		_liferayTemplateClassResolver = new LiferayTemplateClassResolver();
+	public void setUp() throws Exception {
+		Bundle bundle = FrameworkUtil.getBundle(
+			_liferayTemplateClassResolver.getClass());
 
-		Map<String, Object> properties = new HashMap<>();
+		BundleContext bundleContext = bundle.getBundleContext();
 
-		_liferayTemplateClassResolver.activate(
-			_bundle.getBundleContext(), properties);
+		ServiceReference<ConfigurationAdmin> serviceReference =
+			bundleContext.getServiceReference(ConfigurationAdmin.class);
+
+		try {
+			ConfigurationAdmin configurationAdmin = bundleContext.getService(
+				serviceReference);
+
+			_freemarkerTemplateConfiguration =
+				configurationAdmin.getConfiguration(
+					"com.liferay.portal.template.freemarker.configuration." +
+						"FreeMarkerEngineConfiguration",
+					null);
+
+			_properties = _freemarkerTemplateConfiguration.getProperties();
+		}
+		finally {
+			bundleContext.ungetService(serviceReference);
+		}
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		_freemarkerTemplateConfiguration.update(_properties);
 	}
 
 	@Test()
@@ -111,8 +165,10 @@ public class LiferayTemplateClassResolverTest {
 			"com.liferay.portal.kernel.model.User", null, null);
 	}
 
-	private final Bundle _bundle = FrameworkUtil.getBundle(
-		LiferayTemplateClassResolver.class);
-	private LiferayTemplateClassResolver _liferayTemplateClassResolver;
+	private static TemplateClassResolver _liferayTemplateClassResolver;
+	private static ServiceTracker _serviceTracker;
+
+	private Configuration _freemarkerTemplateConfiguration;
+	private Dictionary<String, Object> _properties;
 
 }
