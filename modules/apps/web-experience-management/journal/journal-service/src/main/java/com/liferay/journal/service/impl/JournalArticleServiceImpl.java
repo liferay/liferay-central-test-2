shuyangzhou/liferay-portal

LPS-62450 Add new finder

modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleServiceImpl.java
@@ -977,6 +977,11 @@ public class JournalArticleServiceImpl extends JournalArticleServiceBaseImpl {
 			ddmStructureKey, status);
 	}
 
+	@Override
+	public List<JournalArticle> getArticlesWithLayouts(long groupId) {
+		return journalArticlePersistence.filterFindByG_NotL(groupId, null);
+	}
+
 	/**
 	 * Returns the web content article matching the URL title that is currently
 	 * displayed or next to be displayed if no article is currently displayed.

liferay/liferay-central-test-2

modules/apps/web-experience-management/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleServiceImpl.java
@@ -1287,6 +1287,11 @@ public class JournalArticleServiceImpl extends JournalArticleServiceBaseImpl {
 		return article;
 	}
 
+	@Override
+	public List<JournalArticle> getLayoutArticles(long groupId) {
+		return journalArticlePersistence.filterFindByG_NotL(groupId, null);
+	}
+
 	/**
 	 * Moves all versions of the web content article matching the group and
 	 * article ID to the folder.