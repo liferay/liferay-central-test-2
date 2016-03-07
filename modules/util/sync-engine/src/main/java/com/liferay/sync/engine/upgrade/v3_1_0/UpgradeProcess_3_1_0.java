4c52c27e51f1bde14870176e891afb2074965209
Shinn Lok <shinn.lok@liferay.com>
2016-03-07 15:30:15 -0800

    SYNC-1563 3010 upgrade stopped archiving if delete failed on one file

--- modules/util/sync-engine/src/main/java/com/liferay/sync/engine/upgrade/v3_1_0/UpgradeProcess_3_1_0.java
+++ modules/util/sync-engine/src/main/java/com/liferay/sync/engine/upgrade/v3_1_0/UpgradeProcess_3_1_0.java
@@ -15,6 +15,23 @@
 package com.liferay.sync.engine.upgrade.v3_1_0;
 
 import com.liferay.sync.engine.upgrade.BaseUpgradeProcess;
+import com.liferay.sync.engine.upgrade.util.UpgradeUtil;
+import com.liferay.sync.engine.util.PropsValues;
+import com.liferay.sync.engine.util.StreamUtil;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.text.SimpleDateFormat;
+
+import java.util.Calendar;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * @author Dennis Ju
@@ -25,6 +42,74 @@ public class UpgradeProcess_3_1_0 extends BaseUpgradeProcess {
 	@Override
 	public int getThreshold() {
 		return 3100;
+	}
+
+	@Override
+	public void upgrade() throws Exception {
+		Path logsFolderPath = Paths.get(
+			PropsValues.SYNC_CONFIGURATION_DIRECTORY, "logs");
+
+		Path archiveFilePath = logsFolderPath.resolve("archive");
+
+		Files.createDirectories(archiveFilePath);
+
+		Calendar calendar = Calendar.getInstance();
+
+		calendar.add(Calendar.DAY_OF_MONTH, -7);
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+		Path archiveZipFilePath = archiveFilePath.resolve(
+			"sync-" + dateFormat.format(calendar.getTime()) + ".log.zip");
+
+		FileOutputStream fileOutputStream = new FileOutputStream(
+			archiveZipFilePath.toFile());
+
+		ZipOutputStream zipOutputStream = new ZipOutputStream(fileOutputStream);
+
+		try (DirectoryStream<Path> filePaths =
+				Files.newDirectoryStream(logsFolderPath)) {
+
+			for (Path filePath : filePaths) {
+				if (filePath.equals(archiveFilePath)) {
+					continue;
+				}
+
+				ZipEntry zipEntry = new ZipEntry(
+					String.valueOf(filePath.getFileName()));
+
+				zipOutputStream.putNextEntry(zipEntry);
+
+				InputStream inputStream = null;
+
+				try {
+					inputStream = Files.newInputStream(filePath);
+
+					byte[] bytes = new byte[4096];
+					int length = 0;
+
+					while ((length = inputStream.read(bytes)) > 0) {
+						zipOutputStream.write(bytes, 0, length);
+					}
+
+					zipOutputStream.closeEntry();
+
+					Files.delete(filePath);
+				}
+				catch (Exception e) {
+				}
+				finally {
+					StreamUtil.cleanUp(inputStream);
+				}
+			}
+
+			zipOutputStream.close();
+		}
+		finally {
+			StreamUtil.cleanUp(zipOutputStream);
+		}
+
+		UpgradeUtil.copyLoggerConfiguration();
 	}
 
 	@Override
