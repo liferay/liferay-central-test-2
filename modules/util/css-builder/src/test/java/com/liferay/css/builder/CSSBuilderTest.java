fa1d1ee2779c8037489b9be0fff7f976639e00a8
Brian Chan <brian.chan@liferay.com>
2016-03-08 14:14:00 -0800

    LPS-64031 fix test

--- modules/util/css-builder/src/test/java/com/liferay/css/builder/CSSBuilderTest.java
+++ modules/util/css-builder/src/test/java/com/liferay/css/builder/CSSBuilderTest.java
@@ -102,7 +102,7 @@ public class CSSBuilderTest {
 	private void _testSassToCssBuilder(String compiler) throws Exception {
 		CSSBuilder cssBuilder = new CSSBuilder(
 			_docrootDirName, false,
-			"../../apps/platform/frontend/frontend-common-css/tmp/META-INF" +
+			"../../apps/foundation/frontend/frontend-common-css/tmp/META-INF" +
 				"/resources",
 			6, new String[0], compiler);
 
