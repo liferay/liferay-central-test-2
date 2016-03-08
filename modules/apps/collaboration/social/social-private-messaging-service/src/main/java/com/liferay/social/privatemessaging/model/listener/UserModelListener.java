850dc2f911098d20059f9772aa80b843cc480945
Brian Chan <brian.chan@liferay.com>
2016-03-07 18:31:16 -0800

    LPS-63528 move

--- modules/apps/collaboration/social/social-private-messaging-service/src/main/java/com/liferay/social/privatemessaging/model/listener/UserModelListener.java
+++ modules/apps/collaboration/social/social-private-messaging-service/src/main/java/com/liferay/social/privatemessaging/model/listener/UserModelListener.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.social.privatemessaging.hook.listeners;
+package com.liferay.social.privatemessaging.model.listener;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
