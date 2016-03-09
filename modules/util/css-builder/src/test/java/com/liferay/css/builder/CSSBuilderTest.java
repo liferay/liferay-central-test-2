55907c9b447b63ac8c170d9d1ca7c407c9df66fb
michael.bradford@liferay.com <michael.bradford@liferay.com>
2016-03-09 15:28:32 -0800

    LPS-64100 - update references from frontend.common.css to frontend.css.common

--- modules/util/css-builder/src/test/java/com/liferay/css/builder/CSSBuilderTest.java
+++ modules/util/css-builder/src/test/java/com/liferay/css/builder/CSSBuilderTest.java
@@ -102,8 +102,8 @@ public class CSSBuilderTest {
 	private void _testSassToCssBuilder(String compiler) throws Exception {
 		CSSBuilder cssBuilder = new CSSBuilder(
 			_docrootDirName, false,
-			"../../apps/foundation/frontend/frontend-common-css/tmp/META-INF" +
-				"/resources",
+			"../../apps/foundation/frontend-css/frontend-css-common/tmp" +
+				"/META-INF/resources",
 			6, new String[0], compiler);
 
 		cssBuilder.execute(Arrays.asList(new String[] {"/css"}));
