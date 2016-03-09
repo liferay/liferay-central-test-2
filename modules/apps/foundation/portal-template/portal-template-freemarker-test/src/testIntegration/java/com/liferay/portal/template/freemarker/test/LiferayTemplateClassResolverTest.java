6cc7481b167a91ab027d67ceff7dd1391a623ddd
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2016-03-09 01:18:08 +0100

    LPS-58170 Move the test to the proper location

--- modules/apps/foundation/portal-template/portal-template-freemarker-test/src/testIntegration/java/com/liferay/portal/template/freemarker/test/LiferayTemplateClassResolverTest.java
+++ modules/apps/foundation/portal-template/portal-template-freemarker-test/src/testIntegration/java/com/liferay/portal/template/freemarker/test/LiferayTemplateClassResolverTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.template.freemarker;
+package com.liferay.portal.template.freemarker.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 
