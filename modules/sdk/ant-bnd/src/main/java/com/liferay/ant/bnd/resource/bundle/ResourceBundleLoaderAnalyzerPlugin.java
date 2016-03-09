50da1ceb9cae9fbde69e3a406187cdd0dffbdea3
Carlos Sierra <carlos.sierra@liferay.com>
2016-03-09 09:22:23 +0100

    LPS-63462 Also add symbolic name to the capability

--- modules/sdk/ant-bnd/src/main/java/com/liferay/ant/bnd/resource/bundle/ResourceBundleLoaderAnalyzerPlugin.java
+++ modules/sdk/ant-bnd/src/main/java/com/liferay/ant/bnd/resource/bundle/ResourceBundleLoaderAnalyzerPlugin.java
@@ -41,6 +41,7 @@ public class ResourceBundleLoaderAnalyzerPlugin implements AnalyzerPlugin {
 
 		Attrs attrs = new Attrs();
 
+		attrs.put("bundle.symbolic.name", analyzer.getBsn());
 		attrs.put("resource.bundle.base.name", "content.Language");
 
 		parameters.add("liferay.resource.bundle", attrs);
