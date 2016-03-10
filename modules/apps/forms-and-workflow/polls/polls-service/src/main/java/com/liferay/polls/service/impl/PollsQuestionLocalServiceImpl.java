ad71d5a29d292f4200b9f3a26a0d90027ee1428c
Rafael Praxedes <rafael.praxedes@liferay.com>
2016-03-10 13:53:32 -0300

    LPS-64066 Use PollsQuestionFinderImpl

--- modules/apps/forms-and-workflow/polls/polls-service/src/main/java/com/liferay/polls/service/impl/PollsQuestionLocalServiceImpl.java
+++ modules/apps/forms-and-workflow/polls/polls-service/src/main/java/com/liferay/polls/service/impl/PollsQuestionLocalServiceImpl.java
@@ -28,6 +28,7 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.Validator;
 
@@ -223,6 +224,41 @@ public class PollsQuestionLocalServiceImpl
 	@Override
 	public int getQuestionsCount(long groupId) {
 		return pollsQuestionPersistence.countByGroupId(groupId);
+	}
+
+	@Override
+	public List<PollsQuestion> search(
+		long companyId, long[] groupIds, String keywords, int start, int end,
+		OrderByComparator<PollsQuestion> orderByComparator) {
+
+		return pollsQuestionFinder.findByKeywords(
+			companyId, groupIds, keywords, start, end, orderByComparator);
+	}
+
+	@Override
+	public List<PollsQuestion> search(
+		long companyId, long[] groupIds, String name, String description,
+		boolean andOperator, int start, int end,
+		OrderByComparator<PollsQuestion> orderByComparator) {
+
+		return pollsQuestionFinder.findByC_G_T_D(
+			companyId, groupIds, name, description, andOperator, start, end,
+			orderByComparator);
+	}
+
+	@Override
+	public int searchCount(long companyId, long[] groupIds, String keywords) {
+		return pollsQuestionFinder.countByKeywords(
+			companyId, groupIds, keywords);
+	}
+
+	@Override
+	public int searchCount(
+		long companyId, long[] groupIds, String title, String description,
+		boolean andOperator) {
+
+		return pollsQuestionFinder.countByC_G_T_D(
+			companyId, groupIds, title, description, andOperator);
 	}
 
 	@Override
