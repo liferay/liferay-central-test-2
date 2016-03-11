eafd96e7c07739d9b21ac9afffe0fd15781b86b7
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2016-03-11 11:12:48 -0300

    LPS-64164 Move modules to config.js

--- modules/apps/forms-and-workflow/calendar/calendar-web/src/main/resources/META-INF/resources/js/config.js
+++ modules/apps/forms-and-workflow/calendar/calendar-web/src/main/resources/META-INF/resources/js/config.js
@@ -6,6 +6,33 @@
 					base: MODULE_PATH + '/',
 					combine: Liferay.AUI.getCombine(),
 					modules: {
+						'liferay-calendar-date-picker-util': {
+							path: 'date_picker_util.js',
+							requires: [
+								'aui-base'
+							]
+						},
+						'liferay-calendar-interval-selector': {
+							path: 'interval_selector.js',
+							requires: [
+								'aui-base',
+								'liferay-portlet-base'
+							]
+						},
+						'liferay-calendar-list': {
+							path: 'calendar_list.js',
+							requires: [
+								'aui-template-deprecated',
+								'liferay-scheduler'
+							]
+						},
+						'liferay-calendar-message-util': {
+							path: 'message_util.js',
+							requires: [
+								'aui-alert',
+								'liferay-util-window'
+							]
+						},
 						'liferay-calendar-recurrence-converter': {
 							path: 'js/recurrence_converter.js',
 							requires: []
@@ -15,6 +42,61 @@
 							requires: [
 								'aui-base',
 								'liferay-calendar-recurrence-util'
+							]
+						},
+						'liferay-calendar-recurrence-util': {
+							path: 'recurrence_util.js',
+							requires: [
+								'aui-base',
+								'liferay-util-window'
+							]
+						},
+						'liferay-calendar-reminders': {
+							path: 'calendar_reminders.js',
+							requires: [
+								'aui-base'
+							]
+						},
+						'liferay-calendar-simple-color-picker': {
+							path: 'simple_color_picker.js',
+							requires: [
+								'aui-base',
+								'aui-template-deprecated'
+							]
+						},
+						'liferay-calendar-simple-menu': {
+							path: 'simple_menu.js',
+							requires: [
+								'aui-base',
+								'aui-template-deprecated',
+								'event-outside',
+								'event-touch',
+								'widget-modality',
+								'widget-position',
+								'widget-position-align',
+								'widget-position-constrain',
+								'widget-stack',
+								'widget-stdmod'
+							]
+						},
+						'liferay-scheduler': {
+							path: 'scheduler.js',
+							requires: [
+								'async-queue',
+								'aui-datatype',
+								'aui-io',
+								'aui-scheduler',
+								'aui-toolbar',
+								'autocomplete',
+								'autocomplete-highlighters',
+								'dd-plugin',
+								'liferay-calendar-message-util',
+								'liferay-calendar-recurrence-util',
+								'liferay-node',
+								'liferay-portlet-url',
+								'liferay-store',
+								'promise',
+								'resize-plugin'
 							]
 						}
 					},
