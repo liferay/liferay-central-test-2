26615797d7251a2be1ddc47c583e0598f11df999
Marcellus Tavares <marcellus.tavares@liferay.com>
2016-03-14 11:19:11 -0300

    LPS-64193 JavaDoc Initial commit - DDL Constants

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-api/src/main/java/com/liferay/dynamic/data/lists/constants/DDLConstants.java
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-api/src/main/java/com/liferay/dynamic/data/lists/constants/DDLConstants.java
@@ -15,26 +15,41 @@
 package com.liferay.dynamic.data.lists.constants;
 
 /**
+ * Represents a class that hold generic constants used in DDL services.
+ *
  * @author Marcellus Tavares
  * @author Eduardo Lundgren
  */
 public class DDLConstants {
 
+	/**
+	 * {@value #RESERVED_DDM_STRUCTURE_ID} Reserved String value that holds the value of the Structure ID configured for the Record Set. This value is available DDL Display Template context as injected variable value.
+	 */
 	public static final String RESERVED_DDM_STRUCTURE_ID =
 		"reserved_ddm_structure_id";
 
+	/**
+	 * {@value #RESERVED_DDM_TEMPLATE_ID} Reserved String value that holds the value of the current Template ID. This value is available DDL Display Template context as injected variable value.
+	 */
 	public static final String RESERVED_DDM_TEMPLATE_ID =
 		"reserved_ddm_template_id";
 
+	/**
+	 * {@value #RESERVED_RECORD_SET_DESCRIPTION} Reserved String value that holds the localized value of the Record Set description. This value is available DDL Display Template context as injected variable value.
+	 */
 	public static final String RESERVED_RECORD_SET_DESCRIPTION =
 		"reserved_record_set_description";
 
+	/**
+	 * {@value #RESERVED_RECORD_SET_ID} Reserved String value that holds the value of the Record Set ID. This value is available DDL Display Template context as injected variable value.
+	 */
 	public static final String RESERVED_RECORD_SET_ID =
 		"reserved_record_set_id";
 
+	/**
+	 * {@value #RESERVED_RECORD_SET_NAME} Reserved String value that holds the localized value of the Record Set name. This value is available DDL Display Template context as injected variable value.
+	 */
 	public static final String RESERVED_RECORD_SET_NAME =
 		"reserved_record_set_name";
-
-	public static final String SERVICE_NAME = "com.liferay.dynamic.data.lists";
 
 }
