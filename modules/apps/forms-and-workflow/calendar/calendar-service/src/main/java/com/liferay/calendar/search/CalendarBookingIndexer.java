51060a396cb042ea880c5f239fbb95cd43cb6f2d
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2016-03-15 17:24:05 -0300

    LPS-64288 No need to use search filtering anymore.

--- modules/apps/forms-and-workflow/calendar/calendar-service/src/main/java/com/liferay/calendar/search/CalendarBookingIndexer.java
+++ modules/apps/forms-and-workflow/calendar/calendar-service/src/main/java/com/liferay/calendar/search/CalendarBookingIndexer.java
@@ -18,7 +18,6 @@ import com.liferay.calendar.constants.CalendarActionKeys;
 import com.liferay.calendar.model.Calendar;
 import com.liferay.calendar.model.CalendarBooking;
 import com.liferay.calendar.service.CalendarBookingLocalService;
-import com.liferay.calendar.service.permission.CalendarPermission;
 import com.liferay.calendar.workflow.CalendarBookingWorkflowConstants;
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
@@ -34,7 +33,6 @@ import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.IndexWriterHelperUtil;
 import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.Summary;
-import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.util.GetterUtil;
@@ -65,7 +63,6 @@ public class CalendarBookingIndexer extends BaseIndexer<CalendarBooking> {
 			Field.COMPANY_ID, Field.ENTRY_CLASS_NAME, Field.ENTRY_CLASS_PK,
 			Field.UID);
 		setDefaultSelectedLocalizedFieldNames(Field.DESCRIPTION, Field.TITLE);
-		setFilterSearch(true);
 		setPermissionAware(true);
 	}
 
@@ -74,20 +71,12 @@ public class CalendarBookingIndexer extends BaseIndexer<CalendarBooking> {
 		return CLASS_NAME;
 	}
 
+	@Deprecated
 	@Override
 	public boolean hasPermission(
 			PermissionChecker permissionChecker, String entryClassName,
 			long entryClassPK, String actionId)
 		throws Exception {
-
-		if (actionId.equals(ActionKeys.VIEW)) {
-			CalendarBooking calendarBooking =
-				_calendarBookingLocalService.getCalendarBooking(entryClassPK);
-
-			return CalendarPermission.contains(
-				permissionChecker, calendarBooking.getCalendar(),
-				CalendarActionKeys.VIEW_BOOKING_DETAILS);
-		}
 
 		return super.hasPermission(
 			permissionChecker, entryClassName, entryClassPK, actionId);
