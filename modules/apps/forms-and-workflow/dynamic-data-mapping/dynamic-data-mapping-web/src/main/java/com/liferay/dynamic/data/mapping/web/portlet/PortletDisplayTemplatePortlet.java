b3a9219f0d4b05c6b4a06f57dc49db71790e56c6
Rafael Praxedes <rafael.praxedes@liferay.com>
2016-03-16 10:33:02 -0300

    LPS-62191 Remove the "showToolbar" autopropagated-parameter due to it is always true

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/portlet/PortletDisplayTemplatePortlet.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/portlet/PortletDisplayTemplatePortlet.java
@@ -38,7 +38,6 @@ import org.osgi.service.component.annotations.Modified;
 		"com.liferay.portlet.autopropagated-parameters=scopeTitle",
 		"com.liferay.portlet.autopropagated-parameters=showAncestorScopes",
 		"com.liferay.portlet.autopropagated-parameters=showManageTemplates",
-		"com.liferay.portlet.autopropagated-parameters=showToolbar",
 		"com.liferay.portlet.css-class-wrapper=portlet-dynamic-data-mapping",
 		"com.liferay.portlet.display-category=category.hidden",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
