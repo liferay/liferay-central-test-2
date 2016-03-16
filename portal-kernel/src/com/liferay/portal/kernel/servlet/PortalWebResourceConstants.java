9a1cab745533153716bae0bd61949a9d1c570b71
Byran Zaugg <byran.zaugg@liferay.com>
2016-03-16 15:17:09 -0700

    LPS-48334 Remove RESOURCE_TYPE_EDITOR_BBCODEEDITOR constant

--- portal-kernel/src/com/liferay/portal/kernel/servlet/PortalWebResourceConstants.java
+++ portal-kernel/src/com/liferay/portal/kernel/servlet/PortalWebResourceConstants.java
@@ -23,9 +23,6 @@ public class PortalWebResourceConstants {
 
 	public static final String RESOURCE_TYPE_EDITOR_ALLOYEDITOR = "alloyeditor";
 
-	public static final String RESOURCE_TYPE_EDITOR_BBCODEEDITOR =
-		"bbcodeeditor";
-
 	public static final String RESOURCE_TYPE_EDITOR_CKEDITOR = "ckeditor";
 
 	public static final String RESOURCE_TYPE_EDITOR_SIMPLEEDITOR =
