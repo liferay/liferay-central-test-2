598622568269f65bd208f9b3bb128544b253b289
Roberto Díaz <roberto.diaz@liferay.com>
2016-03-17 02:49:54 +0100

    LPS-64001 Move all the logic to main.js

--- modules/apps/collaboration/invitation/invitation-invite-members-web/src/main/resources/META-INF/resources/invite_members/js/main.js
+++ modules/apps/collaboration/invitation/invitation-invite-members-web/src/main/resources/META-INF/resources/invite_members/js/main.js
@@ -1,26 +1,49 @@
 AUI.add(
-	'liferay-so-invite-members',
+	'liferay-portlet-invite-members',
 	function(A) {
 		var Lang = A.Lang;
+		var Language = Liferay.Language;
 		var Util = Liferay.Util;
 
 		var KEY_ENTER = 13;
+
+		var STR_AVAILABLE_USERS_URL = 'availableUsersURL'
 
 		var STR_CLICK = 'click';
 
 		var STR_KEYUP = 'keyup';
 
-		var TPL_EMAIL_ROW = new A.Template(
-			'<div class="user" data-emailAddress="{emailAddress}">',
-				'<span class="email">{emailAddress}</span>',
-			'</div>'
-		);
+		var TPL_EMAIL_ROW =
+			'<div class="user" data-emailAddress="{emailAddress}">' +
+				'<span class="email">{emailAddress}</span>' +
+			'</div>';
+
+		var TPL_MORE_RESULTS =
+			'<div class="more-results">' +
+				'<a href="javascript:;" data-end="{end}">{message}</a>' +
+			'</div>';
+
+		var TPL_NO_USERS_MESSAGE = '<small class="text-capitalize text-muted">{message}</small>';
+
+		var TPL_USER =
+			'<div class="{cssClass}" data-userId="{userId}">' +
+			'<span class="name">{userFullName}</span>'+
+			'<span class="email">{userEmailAddress}</span>' +
+			'</div>';
 
 		var InviteMembers = A.Component.create(
 			{
 				ATTRS: {
+					availableUsersURL: {
+						validator: Lang.isString
+					},
+
 					form: {
 						validator: Lang.isObject
+					},
+
+					pageDelta: {
+						validator: Lang.isInteger
 					}
 				},
 
@@ -41,6 +64,39 @@ AUI.add(
 						}
 
 						instance._form = instance.get('form').node;
+						instance._invitedMembersList = instance.one('#invitedMembersList');
+						instance._inviteUserSearch = instance.one('#inviteUserSearch');
+						instance._membersList = instance.one('#membersList');
+
+						instance._inviteMembersList = new InviteMembersList(
+							{
+								inputNode: instance._inviteUserSearch,
+								listNode: instance._membersList,
+								minQueryLength: 0,
+								requestTemplate: function(query) {
+									return {
+										end: instance.get('pageDelta'),
+										keywords: query,
+										start: 0
+									}
+								},
+								resultTextLocator: function(response) {
+									var result = '';
+
+									if (typeof response.toString != 'undefined') {
+										result = response.toString();
+									}
+									else if (typeof response.responseText != 'undefined') {
+										result = response.responseText;
+									}
+
+									return result;
+								},
+								source: instance._createDataSource(instance.get(STR_AVAILABLE_USERS_URL))
+							}
+						);
+
+						instance._inviteMembersList.sendRequest();
 
 						instance._bindUI();
 					},
@@ -59,7 +115,8 @@ AUI.add(
 						var emailAddress = Lang.trim(emailInput.val());
 
 						if (emailAddress) {
-							var emailRow = TPL_EMAIL_ROW.render(
+							var emailRow = A.Lang.sub(
+								TPL_EMAIL_ROW,
 								{
 									emailAddress: emailAddress
 								}
@@ -78,9 +135,7 @@ AUI.add(
 					_addMemberInvite: function(user) {
 						var instance = this;
 
-						var invitedMembersList = instance.one('#invitedMembersList');
-
-						user.addClass('invited').cloneNode(true).appendTo(invitedMembersList);
+						user.addClass('invited').cloneNode(true).appendTo(instance._invitedMembersList);
 					},
 
 					_bindUI: function() {
@@ -88,10 +143,38 @@ AUI.add(
 
 						instance._eventHandles = [
 							instance.one('#emailButton').on(STR_CLICK, instance._addMemberEmail, instance),
+							instance._inviteMembersList.on('results', instance._onInviteMembersListResults, instance),
+							instance._form.on('submit', instance._syncFields, instance),
+							instance._membersList.delegate(STR_CLICK, instance._onMemberListClick, '.more-results a', instance),
 							instance._rootNode.delegate(STR_CLICK, instance._handleInvite, '.user', instance),
-							instance._rootNode.delegate(STR_KEYUP, instance._onEmailKeyup, '.controls', instance),
-							instance._form.on('submit', instance._syncFields, instance)
+							instance._rootNode.delegate(STR_KEYUP, instance._onEmailKeyup, '.controls', instance)
 						];
+					},
+
+					_createDataSource: function(url) {
+						var instance = this;
+
+						return new A.DataSource.IO(
+							{
+								ioConfig: {
+									method: "post"
+								},
+								on: {
+									request: function(event) {
+										var data = event.request;
+
+										event.cfg.data = instance.ns(
+											{
+												end: data.end || instance.get('pageDelta'),
+												keywords: data.keywords || '',
+												start: data.start || 0
+											}
+										)
+									}
+								},
+								source: url
+							}
+						);
 					},
 
 					_getByName: function(form, name) {
@@ -128,6 +211,51 @@ AUI.add(
 						}
 					},
 
+					_onInviteMembersListResults: function(event) {
+						var instance = this;
+
+						var responseData = A.JSON.parse(event.data.responseText);
+
+						instance._membersList.html(instance._renderResults(responseData).join(''));
+					},
+
+					_onMemberListClick: function(event) {
+						var instance = this;
+
+						var node = event.currentTarget;
+
+						var start = A.DataType.Number.parse(node.getAttribute('data-end'));
+
+						var pageDelta = instance.get('pageDelta');
+
+						var end = start + pageDelta;
+
+						A.io.request(
+							instance.get(STR_AVAILABLE_USERS_URL),
+							{
+								after: {
+									success: function(event, id, obj) {
+										var responseData = this.get('responseData');
+
+										var moreResults = instance._membersList.one('.more-results');
+
+										moreResults.remove();
+
+										instance._membersList.append(instance._renderResults(responseData).join(''));
+									}
+								},
+								data: instance.ns(
+									{
+										end: end,
+										keywords: instance._inviteUserSearch.get('value'),
+										start: start
+									}
+								),
+								dataType: 'json'
+							}
+						);
+					},
+
 					_removeEmailInvite: function(user) {
 						user.remove();
 					},
@@ -151,6 +279,63 @@ AUI.add(
 						var invitedUser = invitedMembersList.one('[data-userId="' + userId + '"]');
 
 						invitedUser.remove();
+					},
+
+					_renderResults: function(responseData) {
+						var instance = this;
+
+						var count = responseData.count;
+						var options = responseData.options;
+						var results = responseData.users;
+
+						var buffer = [];
+
+						if (results.length == 0) {
+							if (options.start == 0) {
+								var noUsersMessage = A.Lang.sub(
+									TPL_NO_USERS_MESSAGE,
+									{
+										message: Language.get('there-are-no-users-to-invite')
+									}
+								);
+
+								buffer.push(noUsersMessage);
+							}
+						}
+						else {
+							buffer.push(
+								A.Array.map(
+									results,
+									function(result) {
+										var invited = instance._invitedMembersList.one('[data-userId="' + result.userId + '"]');
+
+										return Lang.sub(
+											TPL_USER,
+											{
+												cssClass: result.hasPendingMemberRequest ? "pending-member-request user" : (invited ? "invited user" : "user"),
+												userEmailAddress: result.userEmailAddress,
+												userFullName: result.userFullName,
+												userId: result.userId
+											}
+										);
+									}
+								).join('')
+							);
+
+							if (count > results.length) {
+								var moreResults = Lang.Sub(
+									TPL_MORE_RESULTS,
+									{
+										end : options.end,
+										message: Language.get('view-more')
+									}
+								);
+
+								buffer.push(moreResults);
+							}
+						}
+
+						return buffer;
 					},
 
 					_syncFields: function(form) {
@@ -233,7 +418,7 @@ AUI.add(
 				AUGMENTS: [A.AutoCompleteBase],
 
 				prototype: {
-					initializer: function (config) {
+					initializer: function(config) {
 						var instance = this;
 
 						instance._listNode = A.one(config.listNode);
@@ -245,11 +430,10 @@ AUI.add(
 			}
 		);
 
-		Liferay.InviteMembers = InviteMembers;
-		Liferay.InviteMembersList = InviteMembersList;
+		Liferay.Portlet.InviteMembers = InviteMembers;
 	},
 	'',
 	{
-		requires: ['aui-base', 'aui-template-deprecated', 'autocomplete-base', 'liferay-portlet-base', 'liferay-util-window', 'node-core']
+		requires: ['aui-base', 'autocomplete-base', 'datasource-io', 'datatype-number', 'liferay-portlet-base', 'liferay-util-window', 'node-core']
 	}
 );
