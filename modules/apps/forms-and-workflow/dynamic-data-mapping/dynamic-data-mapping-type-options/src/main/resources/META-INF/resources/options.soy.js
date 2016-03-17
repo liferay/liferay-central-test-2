3927a0f0d6c04a0ec89deb0e6cc0b79cfc222710
Chema Balsas <jose.balsas@liferay.com>
2016-03-17 15:09:20 -0300

    LPS-64376 Update soy file and add wrapper

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-options/src/main/resources/META-INF/resources/options.soy.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-options/src/main/resources/META-INF/resources/options.soy.js
@@ -1,9 +1,17 @@
 // This file was automatically generated from options.soy.
 // Please don't edit this file by hand.
 
+/**
+ * @fileoverview Templates in namespace ddm.
+ * @public
+ */
+
 if (typeof ddm == 'undefined') { var ddm = {}; }
 
 
 ddm.options = function(opt_data, opt_ignored) {
-return '\t<div class="form-group' + soy.$$escapeHtml(opt_data.visible ? '' : ' hide') + ' liferay-ddm-form-field-options" data-fieldname="' + soy.$$escapeHtml(opt_data.name) + '">' + ((opt_data.showLabel) ? '<label class="control-label">' + soy.$$escapeHtml(opt_data.label) + ((opt_data.required) ? '<span class="icon-asterisk text-warning"></span>' : '') + '</label>' : '') + '<input name="' + soy.$$escapeHtml(opt_data.name) + '" type="hidden" /><div class="options"></div></div>';
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml('<div class="form-group' + soy.$$escapeHtmlAttribute(opt_data.visible ? '' : ' hide') + ' liferay-ddm-form-field-options" data-fieldname="' + soy.$$escapeHtmlAttribute(opt_data.name) + '">' + ((opt_data.showLabel) ? '<label class="control-label">' + soy.$$escapeHtml(opt_data.label) + ((opt_data.required) ? '<span class="icon-asterisk text-warning"></span>' : '') + '</label>' : '') + '<input name="' + soy.$$escapeHtmlAttribute(opt_data.name) + '" type="hidden" /><div class="options"></div></div>');
 };
+if (goog.DEBUG) {
+  ddm.options.soyTemplateName = 'ddm.options';
+}
