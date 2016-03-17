c4c6a877f0f15d7976587c8d2f906da306286727
Jim Hinkey <james.hinkey@liferay.com>
2016-03-17 11:01:48 -0400

    LPS-46193 Edit DDL comparator Javadoc

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-api/src/main/java/com/liferay/dynamic/data/lists/util/comparator/DDLRecordModifiedDateComparator.java
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-api/src/main/java/com/liferay/dynamic/data/lists/util/comparator/DDLRecordModifiedDateComparator.java
@@ -18,9 +18,9 @@ import com.liferay.dynamic.data.lists.model.DDLRecord;
 import com.liferay.exportimport.kernel.lar.StagedModelModifiedDateComparator;
 
 /**
- * Represents a OrderByComparator class used to order records according to its
- * modified date during listing operations. The order could be ascending or
- * descending and it's defined by the value specified in the class constructor.
+ * Used to order records according to their modified dates during listing
+ * operations. The order can be ascending or descending and is defined by the
+ * value specified in the class constructor.
  *
  * @author Leonardo Barros
  * @see    com.liferay.dynamic.data.lists.service.DDLRecordLocalService#getRecords(
