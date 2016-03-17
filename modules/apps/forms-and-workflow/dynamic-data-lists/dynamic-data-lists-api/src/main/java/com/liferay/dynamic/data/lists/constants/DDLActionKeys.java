bae0cf489d29122c09e9f0adec41622e0e6b0dd0
Jim Hinkey <james.hinkey@liferay.com>
2016-03-17 10:19:35 -0400

    LPS-64193 Edit Javadoc for DDLPortletKeys, DDLActionKeys, and DDLWebKeys

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-api/src/main/java/com/liferay/dynamic/data/lists/constants/DDLActionKeys.java
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-api/src/main/java/com/liferay/dynamic/data/lists/constants/DDLActionKeys.java
@@ -15,20 +15,21 @@
 package com.liferay.dynamic.data.lists.constants;
 
 /**
- * Holds action key constants used in DDL permission system.
+ * Holds action key constants used in the DDL permission system.
  *
  * @author Rafael Praxedes
  */
 public class DDLActionKeys {
 
 	/**
-	 * {@value #ADD_RECORD} Used to check permissions for adding record.
+	 * {@value #ADD_RECORD} is the action key to use to check permissions for
+	 * adding a record.
 	 */
 	public static final String ADD_RECORD = "ADD_RECORD";
 
 	/**
-	 * {@value #ADD_RECORD_SET} Used to check permissions for adding records
-	 * sets.
+	 * {@value #ADD_RECORD_SET} is the action key to use to check permissions for
+	 * adding record sets.
 	 */
 	public static final String ADD_RECORD_SET = "ADD_RECORD_SET";
 
