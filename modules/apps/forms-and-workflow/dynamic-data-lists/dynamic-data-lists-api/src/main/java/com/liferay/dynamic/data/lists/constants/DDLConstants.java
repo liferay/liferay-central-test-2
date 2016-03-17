bcfa5d0f7c38fdf57009c7c693c25ec15cc08c5c
Jim Hinkey <james.hinkey@liferay.com>
2016-03-17 09:59:58 -0400

    LPS-64193 Edit DDLConstants Javadoc

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-api/src/main/java/com/liferay/dynamic/data/lists/constants/DDLConstants.java
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-api/src/main/java/com/liferay/dynamic/data/lists/constants/DDLConstants.java
@@ -23,41 +23,43 @@ package com.liferay.dynamic.data.lists.constants;
 public class DDLConstants {
 
 	/**
-	 * {@value #RESERVED_DDM_STRUCTURE_ID} Holds the value of the Structure ID
-	 * configured for the Record Set. This Reserved String value is available in
-	 * the DDL Display Template context as an injected variable value.
+	 * {@value #RESERVED_DDM_STRUCTURE_ID} is a Reserved String and the name of
+	 * the DDL Display Template variable that holds the Structure ID configured
+	 * for the Record Set. The variable is injected into the DDL Display
+	 * Template context.
 	 */
 	public static final String RESERVED_DDM_STRUCTURE_ID =
 		"reserved_ddm_structure_id";
 
 	/**
-	 * {@value #RESERVED_DDM_TEMPLATE_ID} Holds the value of the current
-	 * Template ID. This Reserved String value is available in the DDL Display
-	 * Template context as an injected variable value.
+	 * {@value #RESERVED_DDM_TEMPLATE_ID} is a Reserved String and the name of
+	 * the DDL Display Template variable that holds the Template ID. The
+	 * variable is injected into the DDL Display Template context.
 	 */
 	public static final String RESERVED_DDM_TEMPLATE_ID =
 		"reserved_ddm_template_id";
 
 	/**
-	 * {@value #RESERVED_RECORD_SET_DESCRIPTION} Holds the localized value of
-	 * the Record Set description. This Reserved String value is available in
-	 * the DDL Display Template context as an injected variable value.
+	 * {@value #RESERVED_RECORD_SET_DESCRIPTION} is a Reserved String and the
+	 * name of the DDL Display Template variable that holds the Record Set
+	 * description. The variable is injected into the DDL Display Template
+	 * context.
 	 */
 	public static final String RESERVED_RECORD_SET_DESCRIPTION =
 		"reserved_record_set_description";
 
 	/**
-	 * {@value #RESERVED_RECORD_SET_ID} Holds the value of the Record Set ID.
-	 * This Reserved String value is available in the DDL Display Template
-	 * context as an injected variable value.
+	 * {@value #RESERVED_RECORD_SET_ID} is a Reserved String and the name of the
+	 * DDL Display Template variable that holds the Record Set ID. The variable
+	 * is injected into the DDL Display Template context.
 	 */
 	public static final String RESERVED_RECORD_SET_ID =
 		"reserved_record_set_id";
 
 	/**
-	 * {@value #RESERVED_RECORD_SET_NAME} Holds the localized value of the
-	 * Record Set name. This Reserved String value is available in the DDL
-	 * Display Template context as an injected variable value.
+	 * {@value #RESERVED_RECORD_SET_NAME} is a Reserved String and the name of
+	 * the DDL Display Template variable that holds the Record Set name. The
+	 * variable is injected into the DDL Display Template context.
 	 */
 	public static final String RESERVED_RECORD_SET_NAME =
 		"reserved_record_set_name";
