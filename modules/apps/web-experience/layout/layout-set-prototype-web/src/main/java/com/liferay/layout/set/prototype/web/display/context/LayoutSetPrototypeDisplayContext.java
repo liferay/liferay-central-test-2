00d0e4e4f819220b298adc44d161765e5b5ba1f1
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-03-18 13:34:14 +0100

    LPS-63057 Creates new display context for Layout set prototype

---
+++ modules/apps/web-experience/layout/layout-set-prototype-web/src/main/java/com/liferay/layout/set/prototype/web/display/context/LayoutSetPrototypeDisplayContext.java
@@ -0,0 +1,202 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.set.prototype.web.display.context;
+
+import com.liferay.portal.kernel.dao.search.EmptyOnClickRowChecker;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.model.LayoutSetPrototype;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.LayoutSetPrototypeLocalServiceUtil;
+import com.liferay.portal.kernel.service.permission.PortalPermissionUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.util.comparator.LayoutSetPrototypeCreateDateComparator;
+
+import java.util.List;
+
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class LayoutSetPrototypeDisplayContext {
+
+	public LayoutSetPrototypeDisplayContext(
+		RenderRequest renderRequest, RenderResponse renderResponse,
+		HttpServletRequest request) {
+
+		_renderRequest = renderRequest;
+		_renderResponse = renderResponse;
+		_request = request;
+	}
+
+	public Boolean getActive() {
+		String navigation = ParamUtil.getString(_request, "navigation");
+
+		Boolean active = null;
+
+		if (navigation.equals("active")) {
+			active = true;
+		}
+		else if (navigation.equals("inactive")) {
+			active = false;
+		}
+
+		return active;
+	}
+
+	public String getDisplayStyle() {
+		if (Validator.isNotNull(_displayStyle)) {
+			return _displayStyle;
+		}
+
+		_displayStyle = ParamUtil.getString(_request, "displayStyle", "list");
+
+		return _displayStyle;
+	}
+
+	public String getOrderByCol() {
+		if (Validator.isNotNull(_orderByCol)) {
+			return _orderByCol;
+		}
+
+		_orderByCol = ParamUtil.getString(_request, "orderByCol", "name");
+
+		return _orderByCol;
+	}
+
+	public String getOrderByType() {
+		if (Validator.isNotNull(_orderByType)) {
+			return _orderByType;
+		}
+
+		_orderByType = ParamUtil.getString(_request, "orderByType", "asc");
+
+		return _orderByType;
+	}
+
+	public PortletURL getPortletURL() {
+		PortletURL portletURL = _renderResponse.createRenderURL();
+
+		return portletURL;
+	}
+
+	public SearchContainer getSearchContainer() {
+		ThemeDisplay themeDisplay = (ThemeDisplay)_request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		SearchContainer searchContainer = new SearchContainer(
+			_renderRequest, _renderResponse.createRenderURL(), null,
+			"there-are-no-site-templates.-you-can-add-a-site-template-by-" +
+				"clicking-the-plus-button-on-the-bottom-right-corner");
+
+		searchContainer.setId("layoutSetPrototype");
+		searchContainer.setRowChecker(
+			new EmptyOnClickRowChecker(_renderResponse));
+
+		boolean orderByAsc = false;
+
+		if (getOrderByType().equals("asc")) {
+			orderByAsc = true;
+		}
+
+		OrderByComparator<LayoutSetPrototype> orderByComparator =
+			new LayoutSetPrototypeCreateDateComparator(orderByAsc);
+
+		searchContainer.setOrderByCol(getOrderByCol());
+		searchContainer.setOrderByComparator(orderByComparator);
+		searchContainer.setOrderByType(getOrderByType());
+
+		searchContainer.setTotal(getTotal());
+
+		List results = LayoutSetPrototypeLocalServiceUtil.search(
+			themeDisplay.getCompanyId(), getActive(),
+			searchContainer.getStart(), searchContainer.getEnd(),
+			searchContainer.getOrderByComparator());
+
+		searchContainer.setResults(results);
+
+		return searchContainer;
+	}
+
+	public boolean isDescriptiveView() {
+		if (getDisplayStyle().equals("descriptive")) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean isDisabledManagementBar() {
+		if (getTotal() > 0) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public boolean isIconView() {
+		if (getDisplayStyle().equals("icon")) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean isListView() {
+		if (getDisplayStyle().equals("list")) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean showAddButton() {
+		ThemeDisplay themeDisplay = (ThemeDisplay)_request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		if (PortalPermissionUtil.contains(
+				themeDisplay.getPermissionChecker(),
+				ActionKeys.ADD_LAYOUT_SET_PROTOTYPE)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	protected int getTotal() {
+		ThemeDisplay themeDisplay = (ThemeDisplay)_request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		return LayoutSetPrototypeLocalServiceUtil.searchCount(
+			themeDisplay.getCompanyId(), getActive());
+	}
+
+	private String _displayStyle;
+	private String _orderByCol;
+	private String _orderByType;
+	private final RenderRequest _renderRequest;
+	private final RenderResponse _renderResponse;
+	private final HttpServletRequest _request;
+
+}
--- modules/apps/web-experience/layout/layout-set-prototype-web/src/main/resources/META-INF/resources/init.jsp
+++ modules/apps/web-experience/layout/layout-set-prototype-web/src/main/resources/META-INF/resources/init.jsp
@@ -30,7 +30,7 @@ taglib uri="http://liferay.com/tld/util" prefix="liferay-util" %>
 page import="com.liferay.application.list.constants.PanelCategoryKeys" %><%@
 page import="com.liferay.application.list.display.context.logic.PanelCategoryHelper" %><%@
 page import="com.liferay.layout.set.prototype.constants.LayoutSetPrototypePortletKeys" %><%@
-page import="com.liferay.portal.kernel.dao.search.EmptyOnClickRowChecker" %><%@
+page import="com.liferay.layout.set.prototype.web.display.context.LayoutSetPrototypeDisplayContext" %><%@
 page import="com.liferay.portal.kernel.dao.search.ResultRow" %><%@
 page import="com.liferay.portal.kernel.exception.NoSuchLayoutSetPrototypeException" %><%@
 page import="com.liferay.portal.kernel.exception.RequiredLayoutSetPrototypeException" %><%@
@@ -42,17 +42,14 @@ page import="com.liferay.portal.kernel.security.permission.ActionKeys" %><%@
 page import="com.liferay.portal.kernel.service.LayoutSetPrototypeLocalServiceUtil" %><%@
 page import="com.liferay.portal.kernel.service.LayoutSetPrototypeServiceUtil" %><%@
 page import="com.liferay.portal.kernel.service.permission.LayoutSetPrototypePermissionUtil" %><%@
-page import="com.liferay.portal.kernel.service.permission.PortalPermissionUtil" %><%@
 page import="com.liferay.portal.kernel.util.CustomJspRegistryUtil" %><%@
 page import="com.liferay.portal.kernel.util.GetterUtil" %><%@
-page import="com.liferay.portal.kernel.util.OrderByComparator" %><%@
 page import="com.liferay.portal.kernel.util.ParamUtil" %><%@
 page import="com.liferay.portal.kernel.util.PortalUtil" %><%@
 page import="com.liferay.portal.kernel.util.StringPool" %><%@
 page import="com.liferay.portal.kernel.util.UnicodeProperties" %><%@
 page import="com.liferay.portal.kernel.util.Validator" %><%@
 page import="com.liferay.portal.kernel.util.WebKeys" %><%@
-page import="com.liferay.portal.kernel.util.comparator.LayoutSetPrototypeCreateDateComparator" %><%@
 page import="com.liferay.portal.model.impl.LayoutSetPrototypeImpl" %><%@
 page import="com.liferay.portal.util.PropsValues" %><%@
 page import="com.liferay.sites.kernel.util.SitesUtil" %>
@@ -69,5 +66,9 @@ page import="javax.portlet.WindowState" %>
 <liferay-theme:defineObjects />
 
 <portlet:defineObjects />
+
+<%
+LayoutSetPrototypeDisplayContext layoutSetPrototypeDisplayContext = new LayoutSetPrototypeDisplayContext(renderRequest, renderResponse, request);
+%>
 
 <%@ include file="/init-ext.jsp" %>
