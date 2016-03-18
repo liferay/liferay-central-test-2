45ecec3fe06be11ad3cca998f7fa594ce748d071
Marcellus Tavares <marcellus.tavares@liferay.com>
2016-03-18 11:59:14 -0300

    LPS-64368 SF

--- modules/apps/foundation/portal-portlet-bridge/portal-portlet-bridge-soy/src/main/java/com/liferay/portal/portlet/bridge/soy/SoyPortlet.java
+++ modules/apps/foundation/portal-portlet-bridge/portal-portlet-bridge-soy/src/main/java/com/liferay/portal/portlet/bridge/soy/SoyPortlet.java
@@ -15,18 +15,12 @@
 package com.liferay.portal.portlet.bridge.soy;
 
 import com.liferay.portal.kernel.io.unsync.UnsyncStringWriter;
-import com.liferay.portal.kernel.json.JSONException;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.template.Template;
 import com.liferay.portal.kernel.template.TemplateConstants;
 import com.liferay.portal.kernel.template.TemplateException;
 import com.liferay.portal.kernel.template.TemplateManagerUtil;
-import com.liferay.portal.kernel.util.FileUtil;
-import com.liferay.portal.kernel.util.HtmlUtil;
-import com.liferay.portal.kernel.util.StringBundler;
-import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.UnsyncPrintWriterPool;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -35,8 +29,6 @@ import com.liferay.portal.portlet.bridge.soy.internal.SoyTemplateResourcesCollec
 
 import java.io.IOException;
 import java.io.Writer;
-
-import java.net.URL;
 
 import java.util.Map;
 
@@ -60,16 +52,19 @@ public class SoyPortlet extends MVCPortlet {
 	public void init() throws PortletException {
 		super.init();
 
-		Bundle bundle = _getBundle();
+		propagateRequestParameters = GetterUtil.getBoolean(
+			getInitParameter("propagate-request-parameters"), true);
 
-		_controllersManager = new SoyPortletControllersManager(bundle);
-		moduleName = _getModuleName();
+		_bundle = FrameworkUtil.getBundle(this.getClass());
 
 		try {
+			_soyPortletControllersManager = new SoyPortletControllersManager(
+				_bundle);
+
 			template = _getTemplate();
 		}
-		catch (TemplateException te) {
-			throw new PortletException(te);
+		catch (Exception e) {
+			throw new PortletException(e);
 		}
 	}
 
@@ -81,19 +76,6 @@ public class SoyPortlet extends MVCPortlet {
 		renderRequest.setAttribute(WebKeys.TEMPLATE, template);
 
 		super.render(renderRequest, renderResponse);
-	}
-
-	protected String getControllerName(String path) {
-		return _controllersManager.getController(path);
-	}
-
-	protected String getPortletBoundaryId(PortletResponse portletResponse) {
-		return "#p_p_id".concat(
-			HtmlUtil.escapeJS(portletResponse.getNamespace()));
-	}
-
-	protected String getTemplateNamespaceFromPath(String path) {
-		return "Templates.".concat(path).concat(".render");
 	}
 
 	@Override
@@ -111,9 +93,11 @@ public class SoyPortlet extends MVCPortlet {
 
 			template.put(
 				TemplateConstants.NAMESPACE,
-				getTemplateNamespaceFromPath(path));
+				_soyPortletControllersManager.getTemplateNamespace(path));
 
-			injectRequestParameters(portletRequest);
+			if (propagateRequestParameters) {
+				propagateRequestParameters(portletRequest);
+			}
 
 			Writer writer = null;
 
@@ -128,7 +112,11 @@ public class SoyPortlet extends MVCPortlet {
 
 			template.processTemplate(writer);
 
-			writePortletComponent(portletResponse, writer, path);
+			String portletJavaScript =
+				_soyPortletControllersManager.getPortletJavaScript(
+					template, path, portletResponse.getNamespace());
+
+			writer.write(portletJavaScript);
 		}
 		catch (Exception e) {
 			throw new PortletException(e);
@@ -141,98 +129,7 @@ public class SoyPortlet extends MVCPortlet {
 		}
 	}
 
-	protected void writePortletComponent(
-		PortletResponse portletResponse, Writer writer, String path) {
-
-		JSONObject contextJSONObject = JSONFactoryUtil.createJSONObject();
-
-		for (String key : template.getKeys()) {
-			if (Validator.equals(key, TemplateConstants.NAMESPACE)) {
-				continue;
-			}
-
-			contextJSONObject.put(key, template.get(key));
-		}
-
-		String portletBoundaryId = getPortletBoundaryId(portletResponse);
-
-		contextJSONObject.put(
-			"element", portletBoundaryId.concat(" .portlet-content-container"));
-
-		StringBundler sb = new StringBundler(14);
-
-		String portletNamespace = portletResponse.getNamespace();
-
-		sb.append("<script>require('");
-		sb.append(moduleName);
-		sb.append(StringPool.SLASH);
-		sb.append(getControllerName(path));
-		sb.append("',function(Portlet){");
-		sb.append("Liferay.component('");
-		sb.append(portletNamespace);
-		sb.append("', new Portlet.default(");
-		sb.append(contextJSONObject.toJSONString());
-		sb.append(").render());");
-		sb.append(
-			"Liferay.once('beforeScreenFlip',function(){Liferay.component('");
-		sb.append(portletNamespace);
-		sb.append("').dispose();});");
-		sb.append("});</script>");
-
-		try {
-			writer.write(sb.toString());
-		}
-		catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-	}
-
-	protected String moduleName;
-	protected Template template;
-
-	private Bundle _getBundle() {
-		return FrameworkUtil.getBundle(this.getClass());
-	}
-
-	private String _getModuleName() {
-		Bundle bundle = _getBundle();
-
-		String moduleName = StringPool.BLANK;
-
-		try {
-			URL url = bundle.getEntry("package.json");
-
-			byte[] byteArray = FileUtil.getBytes(url.openStream());
-
-			String moduleJSON = new String(byteArray);
-
-			JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
-				moduleJSON);
-
-			moduleName = jsonObject.getString("name");
-		}
-		catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-		catch (JSONException jsone) {
-			jsone.printStackTrace();
-		}
-
-		return moduleName;
-	}
-
-	private Template _getTemplate() throws TemplateException {
-		Bundle bundle = _getBundle();
-
-		SoyTemplateResourcesCollector soyTemplateResourcesCollector =
-			new SoyTemplateResourcesCollector(bundle, templatePath);
-
-		return TemplateManagerUtil.getTemplate(
-			TemplateConstants.LANG_TYPE_SOY,
-			soyTemplateResourcesCollector.getTemplateResources(), false);
-	}
-
-	private void injectRequestParameters(PortletRequest portletRequest) {
+	protected void propagateRequestParameters(PortletRequest portletRequest) {
 		Map<String, String[]> parametersMap = portletRequest.getParameterMap();
 
 		for (String parameterName : parametersMap.keySet()) {
@@ -247,6 +144,20 @@ public class SoyPortlet extends MVCPortlet {
 		}
 	}
 
-	private SoyPortletControllersManager _controllersManager;
+	protected String moduleName;
+	protected boolean propagateRequestParameters;
+	protected Template template;
+
+	private Template _getTemplate() throws TemplateException {
+		SoyTemplateResourcesCollector soyTemplateResourcesCollector =
+			new SoyTemplateResourcesCollector(_bundle, templatePath);
+
+		return TemplateManagerUtil.getTemplate(
+			TemplateConstants.LANG_TYPE_SOY,
+			soyTemplateResourcesCollector.getTemplateResources(), false);
+	}
+
+	private Bundle _bundle;
+	private SoyPortletControllersManager _soyPortletControllersManager;
 
 }
--- modules/apps/foundation/portal-portlet-bridge/portal-portlet-bridge-soy/src/main/java/com/liferay/portal/portlet/bridge/soy/internal/SoyPortletControllersManager.java
+++ modules/apps/foundation/portal-portlet-bridge/portal-portlet-bridge-soy/src/main/java/com/liferay/portal/portlet/bridge/soy/internal/SoyPortletControllersManager.java
@@ -14,6 +14,20 @@
 
 package com.liferay.portal.portlet.bridge.soy.internal;
 
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.template.Template;
+import com.liferay.portal.kernel.template.TemplateConstants;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.io.InputStream;
+
+import java.net.URL;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,42 +38,124 @@ import org.osgi.framework.Bundle;
  */
 public class SoyPortletControllersManager {
 
-	public SoyPortletControllersManager(Bundle bundle) {
+	public SoyPortletControllersManager(Bundle bundle) throws Exception {
 		_bundle = bundle;
-		_controllersMap = new HashMap<>();
+
+		_moduleName = getModuleName();
+
+		_javaScriptTpl = getJavaScriptTpl();
 	}
 
-	public String getController(String path) {
-		final String controller;
+	public String getControllerName(String path) {
+		String controllerName = _controllersMap.get(path);
 
-		if (_hasController(path)) {
-			controller = path.concat(".es");
+		if (controllerName != null) {
+			return controllerName;
+		}
+
+		URL url = _bundle.getEntry(
+			"/META-INF/resources/".concat(path).concat(".es.js"));
+
+		if (url != null) {
+			controllerName = path.concat(".es");
 		}
 		else {
-			controller = path.concat(".soy");
+			controllerName = path.concat(".soy");
 		}
 
-		return controller;
+		_controllersMap.put(path, controllerName);
+
+		return controllerName;
 	}
 
-	private boolean _hasController(String path) {
-		if (!_controllersMap.containsKey(path)) {
-			String filePath = "/META-INF/resources/".concat(path).concat(
-				".es.js");
+	public String getPortletJavaScript(
+		Template template, String path, String portletNamespace) {
 
-			Boolean hasController = false;
-
-			if (_bundle.getEntry(filePath) != null) {
-				hasController = true;
-			}
-
-			_controllersMap.put(path, hasController);
+		if (_moduleName == null) {
+			return StringPool.BLANK;
 		}
 
-		return _controllersMap.get(path);
+		JSONObject contextJSONObject = JSONFactoryUtil.createJSONObject();
+
+		for (String key : template.getKeys()) {
+			if (Validator.equals(key, TemplateConstants.NAMESPACE)) {
+				continue;
+			}
+
+			contextJSONObject.put(key, template.get(key));
+		}
+
+		contextJSONObject.put(
+			"element", getPortletBoundaryId(portletNamespace));
+
+		String javaScript = StringUtil.replace(
+			_javaScriptTpl,
+			new String[] {
+				"$MODULE_NAME", "$CONTROLLER_NAME", "$PORTLET_NAMESPACE",
+				"$CONTEXT"
+			},
+			new String[] {
+				_moduleName, getControllerName(path), portletNamespace,
+				contextJSONObject.toJSONString()
+			});
+
+		return javaScript;
+	}
+
+	public String getTemplateNamespace(String path) {
+		if (_moduleName == null) {
+			return path;
+		}
+
+		return "Templates.".concat(path).concat(".render");
+	}
+
+	protected String getJavaScriptTpl() throws Exception {
+		if (_moduleName == null) {
+			return StringPool.BLANK;
+		}
+
+		Class<?> clazz = getClass();
+
+		InputStream inputStream = clazz.getResourceAsStream(
+			"com/liferay/portal/portlet/bridge/soy/bootstrap.js.tpl");
+
+		return StringUtil.read(inputStream);
+	}
+
+	protected String getModuleName() throws Exception {
+		URL url = _bundle.getEntry("package.json");
+
+		if (url == null) {
+			throw null;
+		}
+
+		String json = StringUtil.read(url.openStream());
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(json);
+
+		String moduleName = jsonObject.getString("name");
+
+		if (Validator.isNull(moduleName)) {
+			throw null;
+		}
+
+		return moduleName;
+	}
+
+	protected String getPortletBoundaryId(String portletNamespace) {
+		StringBundler sb = new StringBundler(3);
+
+		sb.append("#p_p_id");
+		sb.append(HtmlUtil.escapeJS(portletNamespace));
+		sb.append(" .portlet-content-container");
+
+		return sb.toString();
 	}
 
 	private final Bundle _bundle;
-	private final Map<String, Boolean> _controllersMap;
+	private final Map<String, String> _controllersMap = new HashMap<>();
+	private final String _javaScriptTpl;
+	private final String _moduleName;
 
 }
