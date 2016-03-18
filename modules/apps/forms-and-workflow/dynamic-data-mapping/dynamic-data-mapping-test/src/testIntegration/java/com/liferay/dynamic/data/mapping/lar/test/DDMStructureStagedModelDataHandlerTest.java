38f049df7c13abda9ac84b4215a675b060cf3680
Rafael Praxedes <rafael.praxedes@liferay.com>
2016-03-18 20:06:06 -0300

    LPS-64428 Add Staging tests to DDMDataProviderInstance

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/lar/test/DDMStructureStagedModelDataHandlerTest.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/lar/test/DDMStructureStagedModelDataHandlerTest.java
@@ -15,24 +15,45 @@
 package com.liferay.dynamic.data.mapping.lar.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProvider;
+import com.liferay.dynamic.data.mapping.io.DDMFormValuesJSONDeserializer;
+import com.liferay.dynamic.data.mapping.model.DDMDataProviderInstance;
+import com.liferay.dynamic.data.mapping.model.DDMDataProviderInstanceLink;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMFormField;
 import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.service.DDMDataProviderInstanceLinkLocalServiceUtil;
+import com.liferay.dynamic.data.mapping.service.DDMDataProviderInstanceLocalServiceUtil;
 import com.liferay.dynamic.data.mapping.service.DDMStructureLocalServiceUtil;
+import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
+import com.liferay.dynamic.data.mapping.test.util.DDMFormTestUtil;
+import com.liferay.dynamic.data.mapping.test.util.DDMFormValuesTestUtil;
 import com.liferay.dynamic.data.mapping.test.util.DDMStructureTestUtil;
+import com.liferay.dynamic.data.mapping.util.DDMFormFactory;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.StagedModel;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.Sync;
 import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
 import com.liferay.portal.kernel.test.rule.TransactionalTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.lar.test.BaseStagedModelDataHandlerTestCase;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
@@ -53,6 +74,13 @@ public class DDMStructureStagedModelDataHandlerTest
 			SynchronousDestinationTestRule.INSTANCE,
 			TransactionalTestRule.INSTANCE);
 
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		setUpDDMDataProvider();
+		setUpDDMFormValuesJSONDeserializer();
+	}
+
 	@Override
 	protected Map<String, List<StagedModel>> addDependentStagedModelsMap(
 			Group group)
@@ -66,8 +94,27 @@ public class DDMStructureStagedModelDataHandlerTest
 			DDMStructureTestUtil.getSampleDDMForm(
 				RandomTestUtil.randomString()));
 
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				group, TestPropsValues.getUserId());
+
+		DDMFormValues ddmFormValues = getDDMDataProviderInstanceFormValues();
+
+		Map<Locale, String> nameMap = new HashMap<>();
+
+		nameMap.put(LocaleUtil.getSiteDefault(), "Data provider");
+
+		DDMDataProviderInstance ddmDataProviderInstance =
+			DDMDataProviderInstanceLocalServiceUtil.addDataProviderInstance(
+				TestPropsValues.getUserId(), group.getGroupId(), nameMap,
+				nameMap, ddmFormValues, "rest", serviceContext);
+
 		addDependentStagedModel(
 			dependentStagedModelsMap, DDMStructure.class, ddmStructure);
+
+		addDependentStagedModel(
+			dependentStagedModelsMap, DDMDataProviderInstance.class,
+			ddmDataProviderInstance);
 
 		return dependentStagedModelsMap;
 	}
@@ -78,14 +125,79 @@ public class DDMStructureStagedModelDataHandlerTest
 			Map<String, List<StagedModel>> dependentStagedModelsMap)
 		throws Exception {
 
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				group, TestPropsValues.getUserId());
+
 		List<StagedModel> dependentStagedModels = dependentStagedModelsMap.get(
 			DDMStructure.class.getSimpleName());
 
 		DDMStructure parentStructure = (DDMStructure)dependentStagedModels.get(
 			0);
 
+		dependentStagedModels = dependentStagedModelsMap.get(
+			DDMDataProviderInstance.class.getSimpleName());
+
+		DDMDataProviderInstance ddmDataProviderInstance =
+			(DDMDataProviderInstance)dependentStagedModels.get(0);
+
+		DDMForm ddmForm = DDMFormTestUtil.createDDMForm("Name");
+
+		DDMFormField selectDDMFormField = DDMFormTestUtil.createDDMFormField(
+			"Country", "Country", "select", "string", true, false, true);
+
+		selectDDMFormField.setProperty(
+			"ddmDataProviderInstanceId",
+			ddmDataProviderInstance.getDataProviderInstanceId());
+
+		ddmForm.addDDMFormField(selectDDMFormField);
+
 		return DDMStructureTestUtil.addStructure(
-			group.getGroupId(), _CLASS_NAME, parentStructure.getStructureId());
+			group.getGroupId(), _CLASS_NAME, parentStructure.getStructureId(),
+			ddmForm, LocaleUtil.getSiteDefault(), serviceContext);
+	}
+
+	protected DDMFormValues getDDMDataProviderInstanceFormValues() {
+		Class<?> ddmDataProviderSettings = _ddmDataProvider.getSettings();
+
+		DDMForm ddmForm = DDMFormFactory.create(ddmDataProviderSettings);
+
+		DDMFormValues ddmFormValues = DDMFormValuesTestUtil.createDDMFormValues(
+			ddmForm);
+
+		ddmFormValues.addDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"cacheable", Boolean.FALSE.toString()));
+		ddmFormValues.addDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"key", "countryId"));
+		ddmFormValues.addDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"password", "test"));
+		ddmFormValues.addDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"url",
+				"http://localhost:8080/api/jsonws/country/get-countries"));
+		ddmFormValues.addDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"username", "test@liferay.com"));
+		ddmFormValues.addDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"value", "nameCurrentValue"));
+
+		return ddmFormValues;
+	}
+
+	protected DDMFormValues getDDMDataProviderInstanceFormValues(
+			DDMDataProviderInstance ddmDataProviderInstance)
+		throws PortalException {
+
+		Class<?> ddmDataProviderSettings = _ddmDataProvider.getSettings();
+
+		DDMForm ddmForm = DDMFormFactory.create(ddmDataProviderSettings);
+
+		return _ddmFormValuesJSONDeserializer.deserialize(
+			ddmForm, ddmDataProviderInstance.getDefinition());
 	}
 
 	@Override
@@ -104,6 +216,23 @@ public class DDMStructureStagedModelDataHandlerTest
 		return DDMStructure.class;
 	}
 
+	protected void setUpDDMDataProvider() throws Exception {
+		Registry registry = RegistryUtil.getRegistry();
+
+		DDMDataProvider[] ddmDataProviders = registry.getServices(
+			"com.liferay.dynamic.data.mapping.data.provider.DDMDataProvider",
+			"(ddm.data.provider.type=rest)");
+
+		_ddmDataProvider = ddmDataProviders[0];
+	}
+
+	protected void setUpDDMFormValuesJSONDeserializer() {
+		Registry registry = RegistryUtil.getRegistry();
+
+		_ddmFormValuesJSONDeserializer = registry.getService(
+			DDMFormValuesJSONDeserializer.class);
+	}
+
 	@Override
 	protected void validateImport(
 			Map<String, List<StagedModel>> dependentStagedModelsMap,
@@ -120,6 +249,21 @@ public class DDMStructureStagedModelDataHandlerTest
 
 		DDMStructureLocalServiceUtil.getDDMStructureByUuidAndGroupId(
 			ddmStructure.getUuid(), group.getGroupId());
+
+		List<StagedModel> ddmDataProviderInstanceDependentStagedModels =
+			dependentStagedModelsMap.get(
+				DDMDataProviderInstance.class.getSimpleName());
+
+		Assert.assertEquals(
+			1, ddmDataProviderInstanceDependentStagedModels.size());
+
+		DDMDataProviderInstance dataProviderInstance =
+			(DDMDataProviderInstance)
+				ddmDataProviderInstanceDependentStagedModels.get(0);
+
+		DDMDataProviderInstanceLocalServiceUtil.
+			getDDMDataProviderInstanceByUuidAndGroupId(
+				dataProviderInstance.getUuid(), group.getGroupId());
 	}
 
 	@Override
@@ -142,9 +286,42 @@ public class DDMStructureStagedModelDataHandlerTest
 		Assert.assertEquals(
 			structure.getStorageType(), importedStructure.getStorageType());
 		Assert.assertEquals(structure.getType(), importedStructure.getType());
+
+		List<DDMDataProviderInstanceLink> dataProviderInstanceLinks =
+			DDMDataProviderInstanceLinkLocalServiceUtil.
+				getDataProviderInstanceLinks(structure.getStructureId());
+
+		List<DDMDataProviderInstanceLink> importedDataProviderInstanceLinks =
+			DDMDataProviderInstanceLinkLocalServiceUtil.
+				getDataProviderInstanceLinks(
+					importedStructure.getStructureId());
+
+		Assert.assertEquals(1, dataProviderInstanceLinks.size());
+		Assert.assertEquals(1, importedDataProviderInstanceLinks.size());
+
+		long dataProviderInstanceId = dataProviderInstanceLinks.get(
+			0).getDataProviderInstanceId();
+
+		DDMDataProviderInstance dataProviderInstance =
+			DDMDataProviderInstanceLocalServiceUtil.getDataProviderInstance(
+				dataProviderInstanceId);
+
+		dataProviderInstanceId = importedDataProviderInstanceLinks.get(
+			0).getDataProviderInstanceId();
+
+		DDMDataProviderInstance importedDataProviderInstance =
+			DDMDataProviderInstanceLocalServiceUtil.getDataProviderInstance(
+				dataProviderInstanceId);
+
+		Assert.assertEquals(
+			getDDMDataProviderInstanceFormValues(dataProviderInstance),
+			getDDMDataProviderInstanceFormValues(importedDataProviderInstance));
 	}
 
 	private static final String _CLASS_NAME =
 		"com.liferay.dynamic.data.lists.model.DDLRecordSet";
+
+	private DDMDataProvider _ddmDataProvider;
+	private DDMFormValuesJSONDeserializer _ddmFormValuesJSONDeserializer;
 
 }
