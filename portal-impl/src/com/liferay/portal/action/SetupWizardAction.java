a8592392e4a2fc0cde9fe134892c095a0df9b1dd
shuyangzhou <shuyang.zhou@liferay.com>
2016-03-18 16:18:46 -0700

    LPS-64429 Allow blocked plugins to continue deployment

--- portal-impl/src/com/liferay/portal/action/SetupWizardAction.java
+++ portal-impl/src/com/liferay/portal/action/SetupWizardAction.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.action;
 
+import com.liferay.portal.kernel.deploy.hot.HotDeployUtil;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
@@ -24,6 +25,7 @@ import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalLifecycleUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -80,6 +82,10 @@ public class SetupWizardAction extends Action {
 
 				if (ParamUtil.getBoolean(request, "defaultDatabase")) {
 					PropsValues.SETUP_WIZARD_ENABLED = false;
+
+					HotDeployUtil.setCapturePrematureEvents(false);
+
+					PortalLifecycleUtil.flushInits();
 				}
 			}
 
