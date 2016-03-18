29798ceed1021c8db409629111b2c9cff33e4400
Raymond Auge <raymond.auge@liferay.com>
2016-03-18 09:07:12 -0400

    LPS-49566 Revert "LPS-63640 SF"
    This reverts commit cec213980ad6db22f87d23c876bc67b774d3bf4a.

--- portal-test-internal/src/com/liferay/portal/test/rule/PACLTestRule.java
+++ portal-test-internal/src/com/liferay/portal/test/rule/PACLTestRule.java
@@ -114,7 +114,6 @@ public class PACLTestRule implements TestRule {
 		ClassLoaderPool.register(
 			hotDeployEvent.getServletContextName(),
 			servletContext.getClassLoader());
-
 		PortletClassLoaderUtil.setServletContextName(
 			hotDeployEvent.getServletContextName());
 
