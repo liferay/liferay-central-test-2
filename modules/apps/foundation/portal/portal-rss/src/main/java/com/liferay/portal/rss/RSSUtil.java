a5c8b968ddc90b0404f942c9c6bcfe85e00bf049
peterfellwock <peter.fellwock@liferay.com>
2016-03-17 19:51:23 -0700

    LPS-64353 Duplicate Normalizer and update to use com.ibm

--- modules/apps/foundation/portal/portal-rss/src/main/java/com/liferay/portal/rss/RSSUtil.java
+++ modules/apps/foundation/portal/portal-rss/src/main/java/com/liferay/portal/rss/RSSUtil.java
@@ -16,7 +16,7 @@ package com.liferay.portal.rss;
 
 import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.StringPool;
-import com.liferay.util.Normalizer;
+import com.liferay.portal.rss.Normalizer;
 
 import com.sun.syndication.feed.synd.SyndContent;
 import com.sun.syndication.feed.synd.SyndEntry;
