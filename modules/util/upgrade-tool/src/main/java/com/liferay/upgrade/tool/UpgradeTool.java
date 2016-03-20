ef11f1be52c1b05fccae74783d35d70ae96f567a
David Truong <david.truong@liferay.com>
2016-03-19 23:00:01 -0500

    LPS-63740 SF

--- modules/util/upgrade-tool/src/main/java/com/liferay/upgrade/tool/UpgradeTool.java
+++ modules/util/upgrade-tool/src/main/java/com/liferay/upgrade/tool/UpgradeTool.java
@@ -33,7 +33,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
 import jline.console.ConsoleReader;
 
