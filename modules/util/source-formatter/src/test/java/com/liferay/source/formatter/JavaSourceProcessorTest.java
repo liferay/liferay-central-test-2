5e89171d4b6f4414aa6f36877519f373d3341b25
Hugo Huijser <hugo.huijser@liferay.com>
2016-03-21 15:43:11 +0800

    LPS-61420 Add tests

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/JavaSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/JavaSourceProcessorTest.java
@@ -51,6 +51,23 @@ public class JavaSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
+	public void testDuplicateConstructors() throws Exception {
+		test(
+			"DuplicateConstructors.testjava",
+			"Duplicate DuplicateConstructors:");
+	}
+
+	@Test
+	public void testDuplicateMethods() throws Exception {
+		test("DuplicateMethods.testjava", "Duplicate method:");
+	}
+
+	@Test
+	public void testDuplicateVariables() throws Exception {
+		test("DuplicateVariables.testjava", "Duplicate _s2:");
+	}
+
+	@Test
 	public void testExceedMaxLineLength() throws Exception {
 		test("ExceedMaxLineLength.testjava", "> 80:", 37);
 	}
