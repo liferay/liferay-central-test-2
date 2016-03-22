liferay/liferay-central-test-2

commit a736f191750bfc8da58dd05f9ca67f7ef0fc9aa5

LPS-61008 Modified Postgres reword method to add rules for handling large objects properly

portal-impl/src/com/liferay/portal/dao/db/BaseDB.java
@@ -185,6 +185,11 @@ public abstract class BaseDB implements DB {
 	}
 
 	@Override
+	public String getTemplateBlob() {
+		return getTemplate()[5];
+	}
+
+	@Override
 	public String getTemplateFalse() {
 		return getTemplate()[2];
 	}
@@ -409,6 +414,7 @@ public abstract class BaseDB implements DB {
 				}
 				else {
 					sb.append(line);
+					sb.append(StringPool.NEW_LINE);
 
 					if (line.endsWith(";")) {
 						String sql = sb.toString();
@@ -760,7 +766,7 @@ public abstract class BaseDB implements DB {
 				}
 			}
 			else if (!tablesSQLLowerCase.contains(
-						"create table " + tableNameLowerCase + " (")) {
+						CREATE_TABLE + tableNameLowerCase + " (")) {
 
 				continue;
 			}
@@ -969,7 +975,7 @@ public abstract class BaseDB implements DB {
 
 					String[] columns = StringUtil.split(line.substring(x, y));
 
-					x = portalData.indexOf("create table " + table + " (");
+					x = portalData.indexOf(CREATE_TABLE + table + " (");
 					y = portalData.indexOf(");", x);
 
 					String portalTableData = portalData.substring(x, y);
@@ -1096,6 +1102,8 @@ public abstract class BaseDB implements DB {
 	protected static final String ALTER_COLUMN_TYPE = "alter_column_type ";
 
 	protected static final String ALTER_TABLE_NAME = "alter_table_name ";
+
+	protected static final String CREATE_TABLE = "create table ";
 
 	protected static final String DROP_INDEX = "drop index";
 

portal-impl/src/com/liferay/portal/dao/db/PostgreSQLDB.java
@@ -20,6 +20,7 @@ import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
 import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
 import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 
 import java.io.IOException;
@@ -38,6 +39,59 @@ import java.util.List;
  * @author Ganesh Ram
  */
 public class PostgreSQLDB extends BaseDB {
+
+	public static String getCreateRulesSql(String table, String column) {
+		StringBundler sb = new StringBundler(45);
+
+		sb.append("create or replace rule delete_");
+		sb.append(table);
+		sb.append(StringPool.UNDERLINE);
+		sb.append(column);
+		sb.append(" as on delete to ");
+		sb.append(table);
+		sb.append(" do also select case when exists( select 1 from ");
+		sb.append("pg_catalog.pg_largeobject where (loid = old.");
+		sb.append(column);
+		sb.append(")) then lo_unlink(old.");
+		sb.append(column);
+		sb.append(") end from ");
+		sb.append(table);
+		sb.append(" where ");
+		sb.append(table);
+		sb.append(StringPool.PERIOD);
+		sb.append(column);
+		sb.append(" = old.");
+		sb.append(column);
+
+		sb.append(";\ncreate or replace rule update_");
+		sb.append(table);
+		sb.append(StringPool.UNDERLINE);
+		sb.append(column);
+		sb.append(" as on update to ");
+		sb.append(table);
+		sb.append(" where old.");
+		sb.append(column);
+		sb.append(" is distinct from new.");
+		sb.append(column);
+		sb.append(" and old.");
+		sb.append(column);
+		sb.append(" is not null do also select case when exists( select 1 ");
+		sb.append("from pg_catalog.pg_largeobject where (loid = old.");
+		sb.append(column);
+		sb.append(")) then lo_unlink(old.");
+		sb.append(column);
+		sb.append(") end from ");
+		sb.append(table);
+		sb.append(" where ");
+		sb.append(table);
+		sb.append(StringPool.PERIOD);
+		sb.append(column);
+		sb.append(" = old.");
+		sb.append(column);
+		sb.append(StringPool.SEMICOLON);
+
+		return sb.toString();
+	}
 
 	public PostgreSQLDB(int majorVersion, int minorVersion) {
 		super(DBType.POSTGRESQL, majorVersion, minorVersion);
@@ -149,6 +203,10 @@ public class PostgreSQLDB extends BaseDB {
 
 			String line = null;
 
+			String table = null;
+
+			StringBundler createRulesSqlSB = new StringBundler();
+
 			while ((line = unsyncBufferedReader.readLine()) != null) {
 				if (line.startsWith(ALTER_COLUMN_NAME)) {
 					String[] template = buildColumnNameTokens(line);
@@ -173,6 +231,11 @@ public class PostgreSQLDB extends BaseDB {
 						"alter table @old-table@ rename to @new-table@;",
 						RENAME_TABLE_TEMPLATE, template);
 				}
+				else if (line.startsWith(CREATE_TABLE)) {
+					String[] tokens = StringUtil.split(line, ' ');
+
+					table = tokens[2];
+				}
 				else if (line.contains(DROP_INDEX)) {
 					String[] tokens = StringUtil.split(line, ' ');
 
@@ -186,6 +249,13 @@ public class PostgreSQLDB extends BaseDB {
 						"alter table @table@ drop constraint @table@_pkey;",
 						"@table@", tokens[2]);
 				}
+				else if (line.contains(getTemplateBlob())) {
+					String[] tokens = StringUtil.split(line, ' ');
+
+					createRulesSqlSB.append(StringPool.NEW_LINE);
+					createRulesSqlSB.append(
+						getCreateRulesSql(table, tokens[0]));
+				}
 				else if (line.contains("\\\'")) {
 					line = StringUtil.replace(line, "\\\'", "\'\'");
 				}
@@ -193,6 +263,8 @@ public class PostgreSQLDB extends BaseDB {
 				sb.append(line);
 				sb.append("\n");
 			}
+
+			sb.append(createRulesSqlSB.toString());
 
 			return sb.toString();
 		}

portal-service/src/com/liferay/portal/kernel/dao/db/DB.java
@@ -59,6 +59,8 @@ public interface DB {
 
 	public int getMinorVersion();
 
+	public String getTemplateBlob();
+
 	public String getTemplateFalse();
 
 	public String getTemplateTrue();

commit 46eee5a7cef07e6a84c8a1c66b4c8df7bb70234f

LPS-61008 Fix regression caused by a736f191750bfc8da58dd05f9ca67f7ef0fc9aa5, that commit made the "!sql.equals("COMMIT_TRANSACTION;")" never can be true, we end up running it to the under DB, for Sybase that's fatal error.

portal-impl/src/com/liferay/portal/dao/db/BaseDB.java
@@ -381,7 +381,7 @@ public abstract class BaseDB implements DB {
 			String line = null;
 
 			while ((line = unsyncBufferedReader.readLine()) != null) {
-				if (line.startsWith("##")) {
+				if (line.isEmpty() || line.startsWith("##")) {
 					continue;
 				}
 
@@ -427,7 +427,7 @@ public abstract class BaseDB implements DB {
 						sb.setIndex(0);
 
 						try {
-							if (!sql.equals("COMMIT_TRANSACTION;")) {
+							if (!sql.equals("COMMIT_TRANSACTION;\n")) {
 								runSQL(connection, sql);
 							}
 							else {