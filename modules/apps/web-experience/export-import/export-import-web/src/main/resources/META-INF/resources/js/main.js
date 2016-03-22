d559222d03f563fba37ae6ffe3fbb77bdfd17c87
hai.yu <hai.yu@liferay.com>
2016-03-22 18:17:12 +0800

    LPS-64367 Because POST request is not making any changes on the server, the fix will change it from a POST request to a GET request

--- modules/apps/web-experience/export-import/export-import-web/src/main/resources/META-INF/resources/js/main.js
+++ modules/apps/web-experience/export-import/export-import-web/src/main/resources/META-INF/resources/js/main.js
@@ -816,6 +816,7 @@ AUI.add(
 							A.io.request(
 								instance._processesResourceURL,
 								{
+									method: 'GET',
 									on: {
 										failure: function() {
 											new Liferay.Notice(
