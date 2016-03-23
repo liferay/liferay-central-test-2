d311d8c82b46e211494f963dbe9ba976a6cf4487
Marcellus Tavares <marcellus.tavares@liferay.com>
2016-03-23 18:42:06 -0300

    LPS-64376 Add a new template constant to define the rendering mode

--- portal-kernel/src/com/liferay/portal/kernel/template/TemplateConstants.java
+++ portal-kernel/src/com/liferay/portal/kernel/template/TemplateConstants.java
@@ -49,6 +49,8 @@ public interface TemplateConstants {
 
 	public static final String NAMESPACE = "namespace";
 
+	public static final String RENDER_STRICT = "_RENDER_STRICT_";
+
 	public static final String SERVLET_SEPARATOR = "_SERVLET_CONTEXT_";
 
 	public static final String TEMPLATE_ID = "template_id";
