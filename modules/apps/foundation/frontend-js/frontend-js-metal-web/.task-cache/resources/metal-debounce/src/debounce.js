9fb83bee0c17ee011e49443bd0436f9b584881f5
Brian Chan <brian.chan@liferay.com>
2016-03-22 22:59:05 -0700

    Update JS cache

--- modules/apps/foundation/frontend-js/frontend-js-metal-web/.task-cache/resources/metal-debounce/src/debounce.js
+++ modules/apps/foundation/frontend-js/frontend-js-metal-web/.task-cache/resources/metal-debounce/src/debounce.js
@@ -1,4 +1,4 @@
-define("frontend-js-metal-web@1.0.2/metal-debounce/src/debounce", ['exports'], function (exports) {
+define("frontend-js-metal-web@1.0.3/metal-debounce/src/debounce", ['exports'], function (exports) {
 	'use strict';
 
 	/**
