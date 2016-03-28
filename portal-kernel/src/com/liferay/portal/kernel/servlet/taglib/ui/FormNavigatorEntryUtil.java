82513a8b94ef6b0fb5f041082223f6d31e25db2a
Julio Camarero <julio.camarero@liferay.com>
2016-03-28 22:14:16 +0200

    LPS-64674 SF

--- portal-kernel/src/com/liferay/portal/kernel/servlet/taglib/ui/FormNavigatorEntryUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/servlet/taglib/ui/FormNavigatorEntryUtil.java
@@ -28,7 +28,6 @@ import com.liferay.registry.collections.ServiceTrackerMap;
 import java.io.Serializable;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
