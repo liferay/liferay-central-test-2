87c9c0599bb86bb2a4df27e35d9c41abefbd6f05
Gregory Amerson <gregory.amerson@liferay.com>
2016-03-28 12:33:07 -0500

    LPS-63828 sf

--- portal-impl/src/com/liferay/portal/deploy/auto/PluginAutoDeployListenerHelper.java
+++ portal-impl/src/com/liferay/portal/deploy/auto/PluginAutoDeployListenerHelper.java
@@ -21,6 +21,7 @@ import com.liferay.portal.kernel.util.StringUtil;
 
 import java.io.File;
 import java.io.IOException;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.zip.ZipFile;
