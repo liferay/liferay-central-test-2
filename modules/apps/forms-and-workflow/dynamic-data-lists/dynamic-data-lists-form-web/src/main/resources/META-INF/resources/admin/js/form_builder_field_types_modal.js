6b25c28b5e85a9582e62961f8645cef3a2447811
Bruno Basto <bruno.basto@liferay.com>
2016-03-28 17:23:39 -0300

    LPS-64297 Reverting this way of localizing field because it makes it impossible for third-party developers to label theirs custom fields

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_builder_field_types_modal.js
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_builder_field_types_modal.js
@@ -82,8 +82,6 @@ AUI.add(
 
 								var columnNode = instance._createColumn(size);
 
-								fieldType.set('label', instance.get('strings').fieldTypes[fieldType.get('label')]);
-
 								columnNode.append(fieldType.get('node'));
 
 								rowNode.append(columnNode);
