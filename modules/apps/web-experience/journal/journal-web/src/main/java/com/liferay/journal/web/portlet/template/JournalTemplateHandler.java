4f1c298b1133cbc2c5409fb292808edb6800bcb3
Lino Alves <lino.alves@liferay.com>
2016-03-30 08:36:23 -0300

    LPS-64237 adding template for web content field

--- modules/apps/web-experience/journal/journal-web/src/main/java/com/liferay/journal/web/portlet/template/JournalTemplateHandler.java
+++ modules/apps/web-experience/journal/journal-web/src/main/java/com/liferay/journal/web/portlet/template/JournalTemplateHandler.java
@@ -178,7 +178,7 @@ public class JournalTemplateHandler extends BaseDDMTemplateHandler {
 			SetUtil.fromArray(
 				new String[] {
 					"boolean", "date", "document-library", "geolocation",
-					"image", "link-to-page"
+					"image", "journal-article", "link-to-page"
 				}));
 
 }
