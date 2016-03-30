387bc7f74f0b5200818225760fdfe8cc4e0f48a3
janditibi <tibor.jandi@liferay.com>
2016-03-30 19:35:04 +0200

    LPS-64331 StringUtil.randomId() sometimes generates the same name for the structure's option type elements in the same structure

--- modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/util/impl/JournalConverterImpl.java
+++ modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/util/impl/JournalConverterImpl.java
@@ -1053,7 +1053,10 @@ public class JournalConverterImpl implements JournalConverter {
 
 				removeAttribute(element, "index-type");
 
-				element.addAttribute("name", "option" + StringUtil.randomId());
+				element.addAttribute(
+					"name", "option" + parentElement.attributeValue("name") + 
+					StringUtil.randomString(8));
+				
 				element.addAttribute("type", "option");
 				element.addAttribute("value", decodeURL(type));
 
