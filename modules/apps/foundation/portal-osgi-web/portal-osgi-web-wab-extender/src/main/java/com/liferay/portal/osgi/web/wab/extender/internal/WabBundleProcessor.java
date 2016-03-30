4b7e6c94f1b71a8cd0886924e617fb1efd39e764
Raymond Auge <raymond.auge@liferay.com>
2016-03-30 09:36:33 -0400

    LPS-64093 extract for simplicity (note this class has to be in this module)

--- modules/apps/foundation/portal-osgi-web/portal-osgi-web-wab-extender/src/main/java/com/liferay/portal/osgi/web/wab/extender/internal/WabBundleProcessor.java
+++ modules/apps/foundation/portal-osgi-web/portal-osgi-web-wab-extender/src/main/java/com/liferay/portal/osgi/web/wab/extender/internal/WabBundleProcessor.java
@@ -192,53 +192,6 @@ public class WabBundleProcessor {
 		}
 	}
 
-	public static class JspServletWrapper extends HttpServlet {
-
-		public JspServletWrapper(String jspFile) {
-			this.jspFile = jspFile;
-		}
-
-		@Override
-		public void destroy() {
-			_servlet.destroy();
-		}
-
-		@Override
-		public ServletConfig getServletConfig() {
-			return _servlet.getServletConfig();
-		}
-
-		@Override
-		public void init(ServletConfig servletConfig) throws ServletException {
-			_servlet.init(servletConfig);
-		}
-
-		@Override
-		public void service(
-				ServletRequest servletRequest, ServletResponse servletResponse)
-			throws IOException, ServletException {
-
-			String curJspFile = (String)servletRequest.getAttribute(
-				JspServlet.JSP_FILE);
-
-			if (jspFile != null) {
-				servletRequest.setAttribute(JspServlet.JSP_FILE, jspFile);
-			}
-
-			try {
-				_servlet.service(servletRequest, servletResponse);
-			}
-			finally {
-				servletRequest.setAttribute(JspServlet.JSP_FILE, curJspFile);
-			}
-		}
-
-		protected String jspFile;
-
-		private final Servlet _servlet = new JspServlet();
-
-	}
-
 	protected void collectAnnotatedClasses(
 		String classResource, Bundle bundle, Class<?>[] handledTypesArray,
 		Set<Class<?>> annotatedClasses) {
