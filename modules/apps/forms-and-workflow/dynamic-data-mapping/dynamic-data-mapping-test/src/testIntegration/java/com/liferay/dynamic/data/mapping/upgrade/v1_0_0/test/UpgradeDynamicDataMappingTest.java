971500fdbed23c4edfda14e31a8739667acefbf6
Rafael Praxedes <rafael.praxedes@liferay.com>
2016-03-30 18:51:51 -0300

    LPS-64491 Add integration test

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/upgrade/v1_0_0/test/UpgradeDynamicDataMappingTest.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/upgrade/v1_0_0/test/UpgradeDynamicDataMappingTest.java
@@ -843,6 +843,64 @@ public class UpgradeDynamicDataMappingTest {
 	}
 
 	@Test
+	public void testUpgradeTemplateDateFields() throws Exception {
+		addStructure(
+			_structureId, DDMStructureConstants.DEFAULT_PARENT_STRUCTURE_ID,
+			DDMStructureConstants.VERSION_DEFAULT,
+			read("ddm-structure-with-two-date-fields.xsd"), "xml");
+
+		addTemplate(
+			_templateId, _structureId, null,
+			read("ddm-template-with-two-date-fields.ftl"), "ftl",
+			DDMTemplateConstants.TEMPLATE_MODE_CREATE);
+
+		_upgradeDynamicDataMapping.upgrade();
+
+		String actualDefinition = getTemplateScript(_templateId);
+
+		String[] dateFieldNames = new String[] {"date1", "date2"};
+
+		StringBundler sb = null;
+
+		for (String dateFieldName : dateFieldNames) {
+
+			// Assign Statement
+
+			sb = new StringBundler(5);
+
+			sb.append("<#assign ");
+			sb.append(dateFieldName);
+			sb.append("_Data = getterUtil.getString(");
+			sb.append(dateFieldName);
+			sb.append(".getData())>");
+
+			Assert.assertTrue(actualDefinition.contains(sb.toString()));
+
+			// If Statement
+
+			sb = new StringBundler(3);
+
+			sb.append("<#if (validator.isNotNull(");
+			sb.append(dateFieldName);
+			sb.append("_Data))>");
+
+			Assert.assertTrue(actualDefinition.contains(sb.toString()));
+
+			// DateParse Statement
+
+			sb = new StringBundler(5);
+
+			sb.append("<#assign ");
+			sb.append(dateFieldName);
+			sb.append("_DateObj = dateUtil.parseDate(\"yyyy-MM-dd\", ");
+			sb.append(dateFieldName);
+			sb.append("_Data, locale)>");
+
+			Assert.assertTrue(actualDefinition.contains(sb.toString()));
+		}
+	}
+
+	@Test
 	public void testUpgradeTemplatePermissions() throws Exception {
 		addStructure(
 			_structureId, DDMStructureConstants.DEFAULT_PARENT_STRUCTURE_ID,
