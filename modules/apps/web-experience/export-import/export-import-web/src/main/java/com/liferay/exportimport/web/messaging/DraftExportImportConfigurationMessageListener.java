3bfb8a85f3147a11f24d0267a498b92879337a28
Brian Chan <brian.chan@liferay.com>
2016-03-31 10:40:12 -0700

    LPS-64782 SF

--- modules/apps/web-experience/export-import/export-import-web/src/main/java/com/liferay/exportimport/web/messaging/DraftExportImportConfigurationMessageListener.java
+++ modules/apps/web-experience/export-import/export-import-web/src/main/java/com/liferay/exportimport/web/messaging/DraftExportImportConfigurationMessageListener.java
@@ -158,7 +158,6 @@ public class DraftExportImportConfigurationMessageListener
 				}
 
 			});
-
 		actionableDynamicQuery.setPerformActionMethod(
 			new ActionableDynamicQuery.
 				PerformActionMethod<ExportImportConfiguration>() {
