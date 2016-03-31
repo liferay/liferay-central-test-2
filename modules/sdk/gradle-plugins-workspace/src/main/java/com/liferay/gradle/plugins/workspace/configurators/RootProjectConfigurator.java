73f281b55fc5d099d720fbda060826a5a8bd5aa7
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-03-31 15:06:06 -0700

    LPS-61767 Defer evaluation of the downloadBundle.src property
    See https://github.com/michel-kraemer/gradle-download-task/blob/2.1.0/src/main/java/de/undercouch/gradle/tasks/download/DownloadAction.java#L438

--- modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/configurators/RootProjectConfigurator.java
+++ modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/configurators/RootProjectConfigurator.java
@@ -143,7 +143,7 @@ public class RootProjectConfigurator implements Plugin<Project> {
 	protected Download addTaskDownloadBundle(
 		Project project, final WorkspaceExtension workspaceExtension) {
 
-		Download download = GradleUtil.addTask(
+		final Download download = GradleUtil.addTask(
 			project, DOWNLOAD_BUNDLE_TASK_NAME, Download.class);
 
 		File destinationDir = new File(
@@ -156,20 +156,35 @@ public class RootProjectConfigurator implements Plugin<Project> {
 		download.onlyIfNewer(true);
 		download.setDescription("Downloads the Liferay bundle zip file.");
 
-		try {
-			download.src(
-				new Closure<String>(null) {
+		project.afterEvaluate(
+			new Action<Project>() {
 
-					@SuppressWarnings("unused")
-					public String doCall() {
-						return workspaceExtension.getBundleUrl();
+				@Override
+				public void execute(Project project) {
+					Object src = download.getSrc();
+
+					if (src != null) {
+						if (src instanceof List<?>) {
+							List<?> srcList = (List<?>)src;
+
+							if (!srcList.isEmpty()) {
+								return;
+							}
+						}
+						else {
+							return;
+						}
 					}
 
-				});
-		}
-		catch (MalformedURLException murle) {
-			throw new GradleException(murle.getMessage(), murle);
-		}
+					try {
+						download.src(workspaceExtension.getBundleUrl());
+					}
+					catch (MalformedURLException murle) {
+						throw new GradleException(murle.getMessage(), murle);
+					}
+				}
+
+			});
 
 		return download;
 	}
