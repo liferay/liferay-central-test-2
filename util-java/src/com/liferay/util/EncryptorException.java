7324c1e1e56ae1ebcdf8e1f99c67296c04ecfda8
peterfellwock <peter.fellwock@liferay.com>
2016-04-04 16:44:05 -0700

    LPS-64827 add @see to reference duplication

--- util-java/src/com/liferay/util/EncryptorException.java
+++ util-java/src/com/liferay/util/EncryptorException.java
@@ -16,6 +16,7 @@ package com.liferay.util;
 
 /**
  * @author Brian Wing Shun Chan
+ * @see com.liferay.petra.encryptor.util.EncryptorException
  */
 public class EncryptorException extends Exception {
 
