7ff82fb3e4d00cfd29fa0966d2a20566b513dfbc
Adolfo Pérez <adolfo.perez@liferay.com>
2016-04-04 17:27:58 +0200

    LPS-62517 Use non-standard mime type for IE

--- portal-kernel/src/com/liferay/portal/kernel/util/ContentTypes.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/ContentTypes.java
@@ -67,6 +67,8 @@ public interface ContentTypes {
 
 	public static final String IMAGE_PNG = "image/png";
 
+	public static final String IMAGE_X_MS_BMP = "image/x-ms-bmp";
+
 	public static final String MESSAGE_RFC822 = "message/rfc822";
 
 	public static final String MULTIPART = "multipart/";
