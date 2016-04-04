d2a5fbae10367590bd6ccc8700201addbd89df3b
shuyangzhou <shuyang.zhou@liferay.com>
2016-04-04 11:12:57 -0700

    LPS-64847 Add a switch to support whether to propagate CacheField getter/setter methods to intefaces.

--- modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
+++ modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
@@ -4327,13 +4327,59 @@ public class ServiceBuilder {
 	private JavaMethod[] _getMethods(
 		JavaClass javaClass, boolean superclasses) {
 
-		JavaMethod[] methods = javaClass.getMethods(superclasses);
+		List<String> cacheFieldMethods = new ArrayList<>();
+
+		for (JavaField javaField : javaClass.getFields()) {
+			Annotation[] annotations = javaField.getAnnotations();
+
+			for (Annotation annotation : annotations) {
+				Type type = annotation.getType();
+
+				String className = type.getFullyQualifiedName();
+
+				if (!className.equals(CacheField.class.getName())) {
+					continue;
+				}
+
+				if (!GetterUtil.getBoolean(
+						annotation.getNamedParameter("propagateToInterface"))) {
+
+					String methodName = null;
+
+					Object namedParameter = annotation.getNamedParameter(
+						"methodName");
+
+					if (namedParameter != null) {
+						methodName = StringUtil.unquote(
+							StringUtil.trim(namedParameter.toString()));
+					}
+
+					if (Validator.isNull(methodName)) {
+						methodName = TextFormatter.format(
+							getVariableName(javaField), TextFormatter.G);
+					}
+
+					cacheFieldMethods.add("get".concat(methodName));
+					cacheFieldMethods.add("set".concat(methodName));
+				}
+
+				break;
+			}
+		}
+
+		List<JavaMethod> methods = new ArrayList<>();
+
+		for (JavaMethod javaMethod : javaClass.getMethods(superclasses)) {
+			if (!cacheFieldMethods.contains(javaMethod.getName())) {
+				methods.add(javaMethod);
+			}
+		}
 
 		for (JavaMethod method : methods) {
 			Arrays.sort(method.getExceptions());
 		}
 
-		return methods;
+		return methods.toArray(new JavaMethod[methods.size()]);
 	}
 
 	private String _getSessionTypeName(int sessionType) {
