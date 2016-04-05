df65a50ea8f78486617de762883f8c4b93d1007f
Dennis Ju <dennis.ju@liferay.com>
2016-04-05 11:45:44 -0700

    LPS-62775 Move encryption logic to private repo

--- modules/util/sync-engine/src/main/java/com/liferay/sync/engine/util/Encryptor.java
+++ modules/util/sync-engine/src/main/java/com/liferay/sync/engine/util/Encryptor.java
@@ -14,72 +14,17 @@
 
 package com.liferay.sync.engine.util;
 
-import java.nio.charset.Charset;
-
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-
-import org.apache.commons.codec.binary.Base64;
-
 /**
  * @author Dennis Ju
  */
 public class Encryptor {
 
 	public static String decrypt(String value) throws Exception {
-		if (value == null) {
-			return "";
-		}
-
-		SecretKey secretKey = new SecretKeySpec(_PASSWORD, _ALGORITHM);
-
-		Cipher cipher = Cipher.getInstance(_ALGORITHM);
-
-		cipher.init(Cipher.DECRYPT_MODE, secretKey);
-
-		for (int i = 0; i < _ITERATIONS; i++) {
-			byte[] decodedBytes = Base64.decodeBase64(value);
-
-			byte[] decryptedBytes = cipher.doFinal(decodedBytes);
-
-			value = new String(decryptedBytes, _UTF8_CHARSET);
-		}
-
 		return value;
 	}
 
 	public static String encrypt(String value) throws Exception {
-		if (value == null) {
-			return "";
-		}
-
-		SecretKey secretKey = new SecretKeySpec(_PASSWORD, _ALGORITHM);
-
-		Cipher cipher = Cipher.getInstance(_ALGORITHM);
-
-		cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-
-		for (int i = 0; i < _ITERATIONS; i++) {
-			byte[] encryptedBytes = cipher.doFinal(
-				value.getBytes(_UTF8_CHARSET));
-
-			value = Base64.encodeBase64String(encryptedBytes);
-		}
-
 		return value;
 	}
-
-	private static final String _ALGORITHM = "AES";
-
-	private static final int _ITERATIONS = 8;
-
-	private static final byte[] _PASSWORD = {
-		(byte)0x56, (byte)0x78, (byte)0x7e, (byte)0x36, (byte)0x50, (byte)0x64,
-		(byte)0x7a, (byte)0x2e, (byte)0x2b, (byte)0x68, (byte)0x25, (byte)0x58,
-		(byte)0x45, (byte)0x39, (byte)0x4a, (byte)0x6f
-	};
-
-	private static final Charset _UTF8_CHARSET = Charset.forName("UTF-8");
 
 }
