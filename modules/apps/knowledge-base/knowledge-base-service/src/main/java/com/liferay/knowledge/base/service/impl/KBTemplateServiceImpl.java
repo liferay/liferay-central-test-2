d582ea0e9b14376446984a5a51a64e9253ab6a12
Adolfo Pérez <adolfo.perez@liferay.com>
2016-04-08 11:09:35 +0200

    LPS-64277 Copy KBTemplate custom code

--- modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/service/impl/KBTemplateServiceImpl.java
+++ modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/service/impl/KBTemplateServiceImpl.java
@@ -16,28 +16,181 @@ package com.liferay.knowledge.base.service.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.knowledge.base.constants.ActionKeys;
+import com.liferay.knowledge.base.constants.PortletKeys;
+import com.liferay.knowledge.base.model.KBTemplate;
+import com.liferay.knowledge.base.model.KBTemplateSearchDisplay;
+import com.liferay.knowledge.base.model.impl.KBTemplateSearchDisplayImpl;
 import com.liferay.knowledge.base.service.base.KBTemplateServiceBaseImpl;
+import com.liferay.knowledge.base.service.permission.AdminPermission;
+import com.liferay.knowledge.base.service.permission.DisplayPermission;
+import com.liferay.knowledge.base.service.permission.KBTemplatePermission;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
- * The implementation of the k b template remote service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.knowledge.base.service.KBTemplateService} interface.
- *
- * <p>
- * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
- * </p>
- *
+ * @author Peter Shin
  * @author Brian Wing Shun Chan
- * @see KBTemplateServiceBaseImpl
- * @see com.liferay.knowledge.base.service.KBTemplateServiceUtil
  */
 @ProviderType
 public class KBTemplateServiceImpl extends KBTemplateServiceBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Always use {@link com.liferay.knowledge.base.service.KBTemplateServiceUtil} to access the k b template remote service.
-	 */
+	@Override
+	public KBTemplate addKBTemplate(
+			String portletId, String title, String content,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		if (portletId.equals(PortletKeys.KNOWLEDGE_BASE_ADMIN)) {
+			AdminPermission.check(
+				getPermissionChecker(), serviceContext.getScopeGroupId(),
+				ActionKeys.ADD_KB_TEMPLATE);
+		}
+		else if (portletId.equals(PortletKeys.KNOWLEDGE_BASE_DISPLAY)) {
+			DisplayPermission.check(
+				getPermissionChecker(), serviceContext.getScopeGroupId(),
+				ActionKeys.ADD_KB_TEMPLATE);
+		}
+
+		return kbTemplateLocalService.addKBTemplate(
+			getUserId(), title, content, serviceContext);
+	}
+
+	@Override
+	public KBTemplate deleteKBTemplate(long kbTemplateId)
+		throws PortalException {
+
+		KBTemplatePermission.check(
+			getPermissionChecker(), kbTemplateId, ActionKeys.DELETE);
+
+		return kbTemplateLocalService.deleteKBTemplate(kbTemplateId);
+	}
+
+	@Override
+	public void deleteKBTemplates(long groupId, long[] kbTemplateIds)
+		throws PortalException {
+
+		AdminPermission.check(
+			getPermissionChecker(), groupId, ActionKeys.DELETE_KB_TEMPLATES);
+
+		kbTemplateLocalService.deleteKBTemplates(kbTemplateIds);
+	}
+
+	@Override
+	public List<KBTemplate> getGroupKBTemplates(
+		long groupId, int start, int end,
+		OrderByComparator<KBTemplate> orderByComparator) {
+
+		return kbTemplatePersistence.filterFindByGroupId(
+			groupId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getGroupKBTemplatesCount(long groupId) {
+		return kbTemplatePersistence.filterCountByGroupId(groupId);
+	}
+
+	@Override
+	public KBTemplate getKBTemplate(long kbTemplateId) throws PortalException {
+		KBTemplatePermission.check(
+			getPermissionChecker(), kbTemplateId, ActionKeys.VIEW);
+
+		return kbTemplateLocalService.getKBTemplate(kbTemplateId);
+	}
+
+	@Override
+	public KBTemplateSearchDisplay getKBTemplateSearchDisplay(
+			long groupId, String title, String content, Date startDate,
+			Date endDate, boolean andOperator, int[] curStartValues, int cur,
+			int delta, OrderByComparator<KBTemplate> orderByComparator)
+		throws PortalException {
+
+		// See LPS-9546
+
+		int start = 0;
+
+		if (curStartValues.length > (cur - SearchContainer.DEFAULT_CUR)) {
+			start = curStartValues[cur - SearchContainer.DEFAULT_CUR];
+
+			curStartValues = ArrayUtil.subset(
+				curStartValues, 0, cur - SearchContainer.DEFAULT_CUR + 1);
+		}
+		else {
+			cur = SearchContainer.DEFAULT_CUR;
+
+			curStartValues = new int[] {0};
+		}
+
+		int end = start + _INTERVAL;
+
+		List<KBTemplate> kbTemplates = new ArrayList<>();
+
+		int curStartValue = 0;
+
+		while (curStartValue == 0) {
+			List<KBTemplate> curKBTemplates = kbTemplateLocalService.search(
+				groupId, title, content, startDate, endDate, andOperator, start,
+				end, orderByComparator);
+
+			if (curKBTemplates.isEmpty()) {
+				break;
+			}
+
+			for (int i = 0; i < curKBTemplates.size(); i++) {
+				KBTemplate curKBTemplate = curKBTemplates.get(i);
+
+				if (!KBTemplatePermission.contains(
+						getPermissionChecker(), curKBTemplate,
+						ActionKeys.VIEW)) {
+
+					continue;
+				}
+
+				if (kbTemplates.size() == delta) {
+					curStartValue = start + i;
+
+					break;
+				}
+
+				kbTemplates.add(curKBTemplate);
+			}
+
+			start = start + _INTERVAL;
+			end = start + _INTERVAL;
+		}
+
+		int total = ((cur - 1) * delta) + kbTemplates.size();
+
+		if (curStartValue > 0) {
+			curStartValues = ArrayUtil.append(curStartValues, curStartValue);
+
+			total = total + 1;
+		}
+
+		return new KBTemplateSearchDisplayImpl(
+			kbTemplates, total, curStartValues);
+	}
+
+	@Override
+	public KBTemplate updateKBTemplate(
+			long kbTemplateId, String title, String content,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		KBTemplatePermission.check(
+			getPermissionChecker(), kbTemplateId, ActionKeys.UPDATE);
+
+		return kbTemplateLocalService.updateKBTemplate(
+			kbTemplateId, title, content, serviceContext);
+	}
+
+	private static final int _INTERVAL = 200;
+
 }
