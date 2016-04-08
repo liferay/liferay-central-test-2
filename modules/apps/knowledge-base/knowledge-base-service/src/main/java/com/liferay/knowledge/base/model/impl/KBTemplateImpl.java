034bd1ec716f260fcefc81a075fd53fdcc61f2ba
Adolfo Pérez <adolfo.perez@liferay.com>
2016-04-08 11:09:35 +0200

    LPS-64277 Copy KBTemplate custom code

--- modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/model/impl/KBTemplateImpl.java
+++ modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/model/impl/KBTemplateImpl.java
@@ -17,22 +17,12 @@ package com.liferay.knowledge.base.model.impl;
 import aQute.bnd.annotation.ProviderType;
 
 /**
- * The extended model implementation for the KBTemplate service. Represents a row in the &quot;KBTemplate&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.knowledge.base.model.KBTemplate} interface.
- * </p>
- *
+ * @author Peter Shin
  * @author Brian Wing Shun Chan
  */
 @ProviderType
 public class KBTemplateImpl extends KBTemplateBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a k b template model instance should use the {@link com.liferay.knowledge.base.model.KBTemplate} interface instead.
-	 */
 	public KBTemplateImpl() {
 	}
 
