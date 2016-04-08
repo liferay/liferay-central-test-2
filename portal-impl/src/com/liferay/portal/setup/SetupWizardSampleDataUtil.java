b0826f3f9fa94f91e2bf7e5f0b3a8b9622ed9358
Cristina González Castellano <cristina.gonzalez@liferay.com>
2016-04-08 14:34:36 +0200

    LPS-64652 Reuse common code for update adminUser

--- portal-impl/src/com/liferay/portal/setup/SetupWizardSampleDataUtil.java
+++ portal-impl/src/com/liferay/portal/setup/SetupWizardSampleDataUtil.java
@@ -14,16 +14,21 @@
 
 package com.liferay.portal.setup;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Account;
 import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Contact;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.model.ListTypeConstants;
 import com.liferay.portal.kernel.model.Organization;
 import com.liferay.portal.kernel.model.OrganizationConstants;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.FullNameGenerator;
+import com.liferay.portal.kernel.security.auth.FullNameGeneratorFactory;
 import com.liferay.portal.kernel.security.auth.ScreenNameGenerator;
 import com.liferay.portal.kernel.service.AccountLocalServiceUtil;
 import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
@@ -32,15 +37,18 @@ import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
 import com.liferay.portal.kernel.service.OrganizationLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.util.CalendarFactoryUtil;
 import com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.security.auth.ScreenNameGeneratorFactory;
 import com.liferay.portal.util.PropsValues;
 
 import java.util.Calendar;
+import java.util.Locale;
 
 import org.apache.commons.lang.time.StopWatch;
 
@@ -58,8 +66,8 @@ public class SetupWizardSampleDataUtil {
 	}
 
 	public static void addSampleData(
-			long companyId, String companyName, String userFirstName,
-			String userLastName, String userEmailAddress)
+			long companyId, String companyName, String adminUserFirstName,
+			String adminUserLastName, String adminUserEmailAddress)
 		throws Exception {
 
 		StopWatch stopWatch = new StopWatch();
@@ -73,6 +81,22 @@ public class SetupWizardSampleDataUtil {
 		Company company = updateCompany(
 			CompanyLocalServiceUtil.getCompanyById(companyId), companyName,
 			LocaleUtil.toLanguageId(LocaleUtil.getDefault()));
+
+		FullNameGenerator fullNameGenerator =
+			FullNameGeneratorFactory.getInstance();
+
+		String adminUserFullName = fullNameGenerator.getFullName(
+			adminUserFirstName, null, adminUserLastName);
+
+		User adminUser = updateAdminUser(
+			company, LocaleUtil.getDefault(),
+			LocaleUtil.toLanguageId(LocaleUtil.getDefault()),
+			adminUserEmailAddress, adminUserFirstName, adminUserLastName,
+			adminUserFullName);
+
+		adminUser.setPasswordReset(false);
+
+		UserLocalServiceUtil.updateUser(adminUser);
 
 		User defaultUser = company.getDefaultUser();
 
@@ -111,8 +135,21 @@ public class SetupWizardSampleDataUtil {
 			intranetLayout.getGroupId(), true, intranetLayout.getLayoutId(),
 			intranetLayout.getTypeSettings());
 
-		User user = UserLocalServiceUtil.fetchUserByEmailAddress(
-			company.getCompanyId(), userEmailAddress);
+		OrganizationLocalServiceUtil.addUserOrganization(
+			adminUser.getUserId(), organization);
+
+		addOrganizations(companyName, defaultUser, organization);
+
+		if (_log.isInfoEnabled()) {
+			_log.info("Finished adding data in " + stopWatch.getTime() + " ms");
+		}
+	}
+
+	public static User updateAdminUser(
+			Company company, Locale locale, String languageId,
+			String adminEmailAddress, String adminFirstName,
+			String adminLastName, String adminFullName)
+		throws PortalException {
 
 		ScreenNameGenerator screenNameGenerator =
 			ScreenNameGeneratorFactory.getInstance();
@@ -121,39 +158,68 @@ public class SetupWizardSampleDataUtil {
 			PropsValues.DEFAULT_ADMIN_EMAIL_ADDRESS_PREFIX, "test");
 
 		try {
-			screenName = screenNameGenerator.generate(0, 0, userEmailAddress);
+			screenName = screenNameGenerator.generate(0, 0, adminEmailAddress);
 		}
 		catch (Exception e) {
 		}
 
-		if (user == null) {
-			user = UserLocalServiceUtil.addDefaultAdminUser(
-				companyId, screenName, userEmailAddress,
-				LocaleUtil.getDefault(), userFirstName, StringPool.BLANK,
-				userLastName);
+		User adminUser = UserLocalServiceUtil.fetchUserByEmailAddress(
+			company.getCompanyId(), adminEmailAddress);
+
+		if (adminUser != null) {
+			String greeting = LanguageUtil.format(
+				locale, "welcome-x", adminFullName, false);
+
+			Contact contact = adminUser.getContact();
+
+			Calendar birthdayCal = CalendarFactoryUtil.getCalendar();
+
+			birthdayCal.setTime(contact.getBirthday());
+
+			int birthdayMonth = birthdayCal.get(Calendar.MONTH);
+			int birthdayDay = birthdayCal.get(Calendar.DAY_OF_MONTH);
+			int birthdayYear = birthdayCal.get(Calendar.YEAR);
+
+			adminUser = UserLocalServiceUtil.updateUser(
+				adminUser.getUserId(), StringPool.BLANK, StringPool.BLANK,
+				StringPool.BLANK, false, adminUser.getReminderQueryQuestion(),
+				adminUser.getReminderQueryAnswer(), screenName,
+				adminEmailAddress, adminUser.getFacebookId(),
+				adminUser.getOpenId(), false, null, languageId,
+				adminUser.getTimeZoneId(), greeting, adminUser.getComments(),
+				adminFirstName, adminUser.getMiddleName(), adminLastName,
+				contact.getPrefixId(), contact.getSuffixId(), contact.isMale(),
+				birthdayMonth, birthdayDay, birthdayYear, contact.getSmsSn(),
+				contact.getFacebookSn(), contact.getJabberSn(),
+				contact.getSkypeSn(), contact.getTwitterSn(),
+				contact.getJobTitle(), null, null, null, null, null,
+				new ServiceContext());
 		}
 		else {
-			user.setScreenName(screenName);
+			UserLocalServiceUtil.addDefaultAdminUser(
+				company.getCompanyId(), screenName, adminEmailAddress, locale,
+				adminFirstName, StringPool.BLANK, adminLastName);
 
-			user.setGreeting(
-				"Welcome " + userFirstName + " " + userLastName + "!");
+			adminUser = UserLocalServiceUtil.getUserByEmailAddress(
+				company.getCompanyId(), adminEmailAddress);
 
-			user.setFirstName(userFirstName);
-			user.setLastName(userLastName);
+			String defaultAdminEmailAddress =
+				PropsValues.DEFAULT_ADMIN_EMAIL_ADDRESS_PREFIX + "@" +
+					PropsValues.COMPANY_DEFAULT_WEB_ID;
+
+			if (!adminEmailAddress.equals(defaultAdminEmailAddress)) {
+				User testUser = UserLocalServiceUtil.fetchUserByEmailAddress(
+					company.getCompanyId(), defaultAdminEmailAddress);
+
+				if (testUser != null) {
+					UserLocalServiceUtil.updateStatus(
+						testUser.getUserId(), WorkflowConstants.STATUS_INACTIVE,
+						new ServiceContext());
+				}
+			}
 		}
 
-		user.setPasswordReset(false);
-
-		UserLocalServiceUtil.updateUser(user);
-
-		OrganizationLocalServiceUtil.addUserOrganization(
-			user.getUserId(), organization);
-
-		addOrganizations(companyName, defaultUser, organization);
-
-		if (_log.isInfoEnabled()) {
-			_log.info("Finished adding data in " + stopWatch.getTime() + " ms");
-		}
+		return adminUser;
 	}
 
 	public static Company updateCompany(
