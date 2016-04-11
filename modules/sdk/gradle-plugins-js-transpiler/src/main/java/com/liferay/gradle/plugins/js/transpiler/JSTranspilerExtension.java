cd169b7dd200485ee6ef57e1819fa1da1a4fb8d0
Chema Balsas <jose.balsas@liferay.com>
2016-04-11 09:26:56 -0700

    LPS-64875 Update metal-cli version

--- modules/sdk/gradle-plugins-js-transpiler/src/main/java/com/liferay/gradle/plugins/js/transpiler/JSTranspilerExtension.java
+++ modules/sdk/gradle-plugins-js-transpiler/src/main/java/com/liferay/gradle/plugins/js/transpiler/JSTranspilerExtension.java
@@ -43,6 +43,6 @@ public class JSTranspilerExtension {
 	}
 
 	private Object _lfrAmdLoaderVersion = "1.3.5";
-	private Object _metalCliVersion = "0.5.3";
+	private Object _metalCliVersion = "1.3.1";
 
 }
