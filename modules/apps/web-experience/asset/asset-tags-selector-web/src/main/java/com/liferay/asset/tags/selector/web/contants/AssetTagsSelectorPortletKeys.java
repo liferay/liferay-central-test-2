a86226d7de8f316b0c4bc74eb18a1e0d90680c98
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-04-11 14:51:31 +0200

    LPS-66886 Creates new module to select tags

---
+++ modules/apps/web-experience/asset/asset-tags-selector-web/src/main/java/com/liferay/asset/tags/selector/web/contants/AssetTagsSelectorPortletKeys.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.tags.selector.web.contants;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class AssetTagsSelectorPortletKeys {
+
+	public static final String ASSET_TAGS_SELECTOR =
+		"com_liferay_asset_tags_selector_web_portlet_AssetTagsSelectorPortlet";
+
+}
