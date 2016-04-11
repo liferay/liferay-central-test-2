5ff47f2d40e09c02d2e13e600b8c950077978de9
Bruno Basto <bruno.basto@liferay.com>
2016-04-11 15:49:17 -0700

    LPS-65398 Image Editor API

---
+++ modules/apps/foundation/image-editor/image-editor-api/src/main/java/com/liferay/image/editor/api/BaseImageEditorFeature.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.image.editor.api;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Bruno Basto
+ */
+public abstract class BaseImageEditorFeature implements ImageEditorFeature {
+
+	public void prepareContext(
+		Map<String, Object> context, HttpServletRequest request) {
+	}
+
+}
