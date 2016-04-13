976304e8888f7022f31131979521a2d2961d54ab
Hugo Huijser <hugo.huijser@liferay.com>
2016-04-13 15:27:22 +0800

    LPS-61420 Sort properties in *.css or *.scss

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/CSSSourceProcessor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/CSSSourceProcessor.java
@@ -15,12 +15,16 @@
 package com.liferay.source.formatter;
 
 import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.NaturalOrderStringComparator;
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.io.File;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -42,6 +46,8 @@ public class CSSSourceProcessor extends BaseSourceProcessor {
 
 		String newContent = trimContent(content, false);
 
+		newContent = sortProperties(newContent);
+
 		newContent = fixComments(newContent);
 
 		return fixHexColors(newContent);
@@ -52,7 +58,8 @@ public class CSSSourceProcessor extends BaseSourceProcessor {
 		String[] excludes = {
 			"**/.ivy/**", "**/.sass-cache/**", "**/__MACOSX/**",
 			"**/_partial.scss", "**/_unstyled/css/**", "**/aui/**",
-			"**/aui_deprecated.css", "**/bourbon/**", "**/expected/**",
+			"**/aui_deprecated.css", "**/bourbon/**",
+			"**/converter/dependencies/**", "**/expected/**",
 			"**/frontend-editors-web/**", "**/tools/node**"
 		};
 
@@ -107,11 +114,61 @@ public class CSSSourceProcessor extends BaseSourceProcessor {
 		return content;
 	}
 
+	protected String sortProperties(String content) {
+		Matcher matcher = _propertiesPattern.matcher(content);
+
+		while (matcher.find()) {
+			String parameters = StringUtil.trimTrailing(matcher.group());
+
+			String newParameters = StringUtil.removeChar(
+				parameters, CharPool.TAB);
+
+			List<String> parameterList = ListUtil.fromArray(
+				StringUtil.splitLines(newParameters));
+
+			Collections.sort(parameterList, new PropertyComparator());
+
+			String tabs = matcher.group(2);
+
+			StringBundler sb = new StringBundler(parameterList.size() * 3);
+
+			for (String parameter : parameterList) {
+				sb.append(tabs);
+				sb.append(parameter);
+				sb.append("\n");
+			}
+
+			newParameters = sb.toString();
+
+			newParameters = newParameters.substring(
+				0, newParameters.length() - 1);
+
+			content = StringUtil.replaceFirst(
+				content, parameters, newParameters, matcher.start() - 1);
+		}
+
+		return content;
+	}
+
 	private static final String[] _INCLUDES = {"**/*.css", "**/*.scss"};
 
 	private final Pattern _commentPattern =
 		Pattern.compile("/\\* -+(.+)-+ \\*/");
 	private final Pattern _hexColorPattern = Pattern.compile(
 		"#([0-9a-f]+)[\\( ;,]");
+	private final Pattern _propertiesPattern = Pattern.compile(
+		"(^(\t*)[a-z]\\S*: .+;\n)+", Pattern.MULTILINE);
+
+	private class PropertyComparator extends NaturalOrderStringComparator {
+
+		@Override
+		public int compare(String s1, String s2) {
+			int pos1 = s1.indexOf(CharPool.COLON);
+			int pos2 = s2.indexOf(CharPool.COLON);
+
+			return super.compare(s1.substring(0, pos1), s2.substring(0, pos2));
+		}
+
+	}
 
 }
