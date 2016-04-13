7163fc25de98f4208b8662d69c3ada404b6e5ebd
Bruno Basto <bruno.basto@liferay.com>
2016-04-13 19:17:31 -0300

    LPS-65014 Add category

--- modules/apps/foundation/hello-soy/hello-soy-web/src/main/java/com/liferay/hello/soy/web/portlet/HelloSoyPortlet.java
+++ modules/apps/foundation/hello-soy/hello-soy-web/src/main/java/com/liferay/hello/soy/web/portlet/HelloSoyPortlet.java
@@ -39,6 +39,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.add-default-resource=true",
 		"com.liferay.portlet.application-type=full-page-application",
 		"com.liferay.portlet.application-type=widget",
+		"com.liferay.portlet.display-category=category.sample",
 		"com.liferay.portlet.layout-cacheable=true",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
