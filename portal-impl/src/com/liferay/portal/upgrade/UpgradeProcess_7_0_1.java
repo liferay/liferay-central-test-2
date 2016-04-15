c614be9ac2abdbf0c238a8d65392ed6f79052897
Mika Koivisto <mika.koivisto@liferay.com>
2016-04-15 16:03:09 +0300

    LPS-64814 Upgrade company key

--- portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_7_0_1.java
+++ portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_7_0_1.java
@@ -16,6 +16,7 @@ package com.liferay.portal.upgrade;
 
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.ReleaseInfo;
+import com.liferay.portal.upgrade.v7_0_1.UpgradeCompany;
 
 /**
  * @author Brian Wing Shun Chan
@@ -29,6 +30,8 @@ public class UpgradeProcess_7_0_1 extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
+		upgrade(UpgradeCompany.class);
+
 		clearIndexesCache();
 	}
 
