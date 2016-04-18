brianchandotcom/liferay-portal

LRQA-18824 Refactoring - name -> jobName

modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/DownstreamBuild.java
@@ -43,7 +43,7 @@ public class DownstreamBuild extends BaseBuild {
 		}
 
 		master = invocationURLMatcher.group("master");
-		jobName = invocationURLMatcher.group("name");
+		jobName = invocationURLMatcher.group("jobName");
 
 		String queryString = invocationURLMatcher.group("queryString");
 
@@ -351,7 +351,7 @@ public class DownstreamBuild extends BaseBuild {
 	}
 
 	private static final Pattern _invocationURLPattern = Pattern.compile(
-		"\\w+://(?<master>[^/]+)/+build/+(?<name>[^/]+).*/" +
+		"\\w+://(?<master>[^/]+)/+build/+(?<jobName>[^/]+).*/" +
 			"buildWithParameters\\?(?<queryString>.*)");
 
 }

liferay/liferay-central-test-2

modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/DownstreamBuild.java
@@ -43,7 +43,7 @@ public class DownstreamBuild extends BaseBuild {
 		}
 
 		master = invocationURLMatcher.group("master");
-		jobName = invocationURLMatcher.group("name");
+		jobName = invocationURLMatcher.group("jobName");
 
 		String queryString = invocationURLMatcher.group("queryString");
 
@@ -351,7 +351,7 @@ public class DownstreamBuild extends BaseBuild {
 	}
 
 	private static final Pattern _invocationURLPattern = Pattern.compile(
-		"\\w+://(?<master>[^/]+)/+job/+(?<name>[^/]+).*/" +
+		"\\w+://(?<master>[^/]+)/+job/+(?<jobName>[^/]+).*/" +
 			"buildWithParameters\\?(?<queryString>.*)");
 
 }