fc51a7103eafc4bc5b1a4ccfda0219698a20f7b2
shuyangzhou <shuyang.zhou@liferay.com>
2016-04-18 10:37:54 -0700

    LPS-65086 Purge tomcat pools before running.

--- portal-impl/test/integration/com/liferay/counter/service/CounterLocalServiceTest.java
+++ portal-impl/test/integration/com/liferay/counter/service/CounterLocalServiceTest.java
@@ -93,6 +93,8 @@ public class CounterLocalServiceTest {
 						MBeanServer mBeanServer =
 							ManagementFactory.getPlatformMBeanServer();
 
+						// HikariCP
+
 						for (ObjectName objectName :
 								mBeanServer.queryNames(
 									null,
@@ -101,6 +103,18 @@ public class CounterLocalServiceTest {
 
 							mBeanServer.invoke(
 								objectName, "softEvictConnections", null, null);
+						}
+
+						// Tomcat JDBC Connection Pool
+
+						for (ObjectName objectName :
+								mBeanServer.queryNames(
+									null,
+									new ObjectName(
+										"TomcatJDBCPool:type=ConnectionPool," +
+											"name=*"))) {
+
+							mBeanServer.invoke(objectName, "purge", null, null);
 						}
 
 						return null;
