pyoo47/liferay-portal

LRQA-18824 Add ability to stop Jenkins jobs associated with BaseJob and topLevelJob instance

modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/JenkinsStopJobUtil.java
@@ -33,9 +33,18 @@ public class JenkinsStopJobUtil {
 			String jobURL, String username, String password)
 		throws Exception {
 
-		stopJob(jobURL, username, password);
-
 		stopDownstreamJobs(jobURL, username, password);
+
+		stopJob(jobURL, username, password);
+	}
+
+	public static void stopJenkinsJob(
+			TopLevelJob topLevelJob, String username, String password)
+		throws Exception {
+
+		stopDownstreamJobs(topLevelJob, username, password);
+
+		stopJob(topLevelJob, username, password);
 	}
 
 	protected static String encodeAuthorizationFields(
@@ -80,6 +89,25 @@ public class JenkinsStopJobUtil {
 		for (String downstreamURL : downstreamURLs) {
 			stopJob(downstreamURL, username, password);
 		}
+	}
+
+	private static void stopDownstreamJobs(
+			TopLevelJob topLevelJob, String username, String password)
+		throws Exception {
+
+		List<DownstreamJob> downstreamJobs = topLevelJob.getDownstreamJobs(
+			"running");
+
+		for (DownstreamJob downstreamJob : downstreamJobs) {
+			stopJob(downstreamJob, username, password);
+		}
+	}
+
+	private static void stopJob(
+			BaseJob baseJob, String username, String password)
+		throws Exception {
+
+		stopJob(baseJob.getBuildURL(), username, password);
 	}
 
 	private static void stopJob(String jobURL, String username, String password)

liferay/liferay-central-test-2

modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/JenkinsStopJobUtil.java
@@ -33,9 +33,18 @@ public class JenkinsStopJobUtil {
 			String jobURL, String username, String password)
 		throws Exception {
 
-		_stopJob(jobURL, username, password);
-
 		_stopDownstreamJobs(jobURL, username, password);
+
+		_stopJob(jobURL, username, password);
+	}
+
+	public static void stopJenkinsJob(
+			TopLevelJob topLevelJob, String username, String password)
+		throws Exception {
+
+		stopDownstreamJobs(topLevelJob, username, password);
+
+		stopJob(topLevelJob, username, password);
 	}
 
 	protected static String encodeAuthorizationFields(
@@ -82,8 +91,26 @@ public class JenkinsStopJobUtil {
 		}
 	}
 
+	private static void _stopDownstreamJobs(
+			TopLevelJob topLevelJob, String username, String password)
+		throws Exception {
+
+		List<DownstreamJob> downstreamJobs = topLevelJob.getDownstreamJobs(
+			"running");
+
+		for (DownstreamJob downstreamJob : downstreamJobs) {
+			stopJob(downstreamJob, username, password);
+		}
+	}
+
 	private static void _stopJob(
-			String jobURL, String username, String password)
+			BaseJob baseJob, String username, String password)
+		throws Exception {
+
+		stopJob(baseJob.getBuildURL(), username, password);
+	}
+
+	private static void stopJob(String jobURL, String username, String password)
 		throws Exception {
 
 		URL urlObject = new URL(