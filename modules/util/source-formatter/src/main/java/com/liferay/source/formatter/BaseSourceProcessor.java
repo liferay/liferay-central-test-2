72543aaaec7c03b0f395f221a04cd04fa1cb720f
Hugo Huijser <hugo.huijser@liferay.com>
2016-04-18 12:22:54 +0800

    LPS-65135 Change last case

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/BaseSourceProcessor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/BaseSourceProcessor.java
@@ -51,6 +51,7 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Properties;
 import java.util.Set;
 import java.util.regex.Matcher;
@@ -220,7 +221,7 @@ public abstract class BaseSourceProcessor implements SourceProcessor {
 				value = StringPool.BLANK;
 			}
 
-			if (Validator.equals(value, defaultValue)) {
+			if (Objects.equals(value, defaultValue)) {
 				processErrorMessage(
 					fileName,
 					"No need to pass default value: " + fileName + " " +
