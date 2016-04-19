f66ee4ad7a87d00012f6e716fb60de78aff99f86
Brian Chan <brian.chan@liferay.com>
2016-04-19 16:08:57 -0700

    LPS-63740 Rename

--- modules/util/portal-tools-upgrade-client/src/main/java/com/liferay/portal/tools/upgrade/client/AppServer.java
+++ modules/util/portal-tools-upgrade-client/src/main/java/com/liferay/portal/tools/upgrade/client/AppServer.java
@@ -21,46 +21,46 @@ import java.io.File;
  */
 public class AppServer {
 
-	public static AppServer getJBossEAP() {
+	public static AppServer getJBossEAPAppServer() {
 		return new AppServer(
 			"../jboss-eap-6.4.0", "/modules/com/liferay/portal/main",
 			"/standalone/deployments/ROOT.war");
 	}
 
-	public static AppServer getJOnAS() {
+	public static AppServer getJOnASAppServer() {
 		return new AppServer(
 			"../jonas-5.2.3", "/lib/ext", "/deploy/liferay-portal");
 	}
 
-	public static AppServer getResin() {
+	public static AppServer getResinAppServer() {
 		return new AppServer(
 			"./resin-4.0.44", "/ext-lib", "/webapps/ROOT");
 	}
 
-	public static AppServer getTCServer() {
+	public static AppServer getTCServerAppServer() {
 		return new AppServer(
 			"../tc-server-2.9.11", "/liferay/lib", "/liferay/webapps/ROOT");
 	}
 
-	public static AppServer getTomcat() {
+	public static AppServer getTomcatAppServer() {
 		return new AppServer(
 			"../tomcat-8.0.32", "/lib", "/webapps/ROOT");
 	}
 
-	public static AppServer getWeblogic() {
+	public static AppServer getWeblogicAppServer() {
 		return new AppServer(
 			"../weblogic-12.1.3", "/domains/liferay/lib",
 			"/domains/liferay/autodeploy/ROOT");
 	}
 
-	public static AppServer getWebsphere() {
+	public static AppServer getWebsphereAppServer() {
 		return new AppServer(
 			"../websphere-8.5.5.0", "/lib/ext",
 			"/profiles/liferay/installedApps/liferay-cell/liferay-portal.ear/" +
 				"liferay-portal.war");
 	}
 
-	public static AppServer getWildFly() {
+	public static AppServer getWildFlyAppServer() {
 		return new AppServer(
 			"../wildfly-10.0.0", "/modules/com/liferay/portal/main",
 			"/standalone/deployments/ROOT.war");
