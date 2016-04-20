6e4721de57c7e44ab21eb5cb4fe9201c6b9e6a14
Bruno Basto <bruno.basto@liferay.com>
2016-04-20 19:23:05 -0300

    LPS-65221 SF - Fixes typo

--- modules/apps/foundation/frontend-js/frontend-js-spa-web/src/main/resources/META-INF/resources/liferay/surface/Surface.es.js
+++ modules/apps/foundation/frontend-js/frontend-js-spa-web/src/main/resources/META-INF/resources/liferay/surface/Surface.es.js
@@ -5,14 +5,14 @@ import dom from 'metal-dom/src/dom';
 import Surface from 'senna/src/surface/Surface';
 
 class LiferaySurface extends Surface {
-	addContent(scrrenId, content) {
+	addContent(screenId, content) {
 		if (core.isString(content)) {
 			content = dom.buildFragment(content);
 		}
 
 		Liferay.DOMTaskRunner.runTasks(content);
 
-		return super.addContent(scrrenId, content);
+		return super.addContent(screenId, content);
 	}
 }
 
