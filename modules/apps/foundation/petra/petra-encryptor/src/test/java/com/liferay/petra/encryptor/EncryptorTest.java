2b47403667c493e94c63953afca68eb5502c1a28
Brian Chan <brian.chan@liferay.com>
2016-04-21 13:57:30 -0700

    LPS-64814 Add test

--- modules/apps/foundation/petra/petra-encryptor/src/test/java/com/liferay/petra/encryptor/EncryptorTest.java
+++ modules/apps/foundation/petra/petra-encryptor/src/test/java/com/liferay/petra/encryptor/EncryptorTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.util;
+package com.liferay.petra.encryptor;
 
 import com.liferay.portal.kernel.util.Props;
 import com.liferay.portal.kernel.util.PropsKeys;
