77563082c58e50f135c5d7bb04cdcd23586bbd27
Brian Chan <brian.chan@liferay.com>
2016-04-21 14:15:50 -0700

    LPS-65191 Hugo SF

--- modules/apps/web-experience/export-import/export-import-resources-importer/src/main/java/com/liferay/exportimport/resources/importer/util/FileSystemImporter.java
+++ modules/apps/web-experience/export-import/export-import-resources-importer/src/main/java/com/liferay/exportimport/resources/importer/util/FileSystemImporter.java
@@ -1909,7 +1909,8 @@ public class FileSystemImporter extends BaseImporter {
 			{"asset_entry", "com.liferay.asset.kernel.model.AssetEntry"},
 			{"asset_tag", "com.liferay.asset.kernel.model.AssetTag"},
 			{"blogs_entry", "com.liferay.blogs.kernel.model.BlogsEntry"},
-			{"bread_crumb",
+			{
+				"bread_crumb",
 				"com.liferay.portal.kernel.servlet.taglib.ui.BreadcrumbEntry"
 			},
 			{"document_library",
