df25d761f76f1e328ac85855136c60dffdb51202
Mika Koivisto <mika.koivisto@liferay.com>
2016-04-21 13:29:25 +0300

    LPS-64814 Apply to module

--- modules/apps/foundation/petra/petra-encryptor/src/main/java/com/liferay/petra/encryptor/Encryptor.java
+++ modules/apps/foundation/petra/petra-encryptor/src/main/java/com/liferay/petra/encryptor/Encryptor.java
@@ -37,10 +37,12 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  * @author Brian Wing Shun Chan
  * @author Shuyang Zhou
+ * @author Mika Koivisto
  * @see com.liferay.util.Encryptor
  */
 public class Encryptor {
@@ -115,6 +117,12 @@ public class Encryptor {
 		catch (Exception e) {
 			throw new EncryptorException(e);
 		}
+	}
+
+	public static Key deserializeKey(String base64String) {
+		byte[] encodedKey = Base64.decode(base64String);
+
+		return new SecretKeySpec(encodedKey, Encryptor.KEY_ALGORITHM);
 	}
 
 	public static String digest(String text) {
@@ -243,6 +251,10 @@ public class Encryptor {
 		}
 
 		return (Provider)providerClass.newInstance();
+	}
+
+	public static String serializeKey(Key key) {
+		return Base64.encode(key.getEncoded());
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(Encryptor.class);
