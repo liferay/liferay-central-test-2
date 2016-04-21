47819c3a30ff7d5cb9feceef87d1fa03d3ce2079
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-04-21 14:51:10 -0700

    LPS-65093 Copy custom PushNotificationsDevice code

--- modules/apps/push-notifications/push-notifications-service/src/main/java/com/liferay/push/notifications/model/impl/PushNotificationsDeviceImpl.java
+++ modules/apps/push-notifications/push-notifications-service/src/main/java/com/liferay/push/notifications/model/impl/PushNotificationsDeviceImpl.java
@@ -17,23 +17,12 @@ package com.liferay.push.notifications.model.impl;
 import aQute.bnd.annotation.ProviderType;
 
 /**
- * The extended model implementation for the PushNotificationsDevice service. Represents a row in the &quot;PushNotificationsDevice&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.push.notifications.model.PushNotificationsDevice} interface.
- * </p>
- *
- * @author Bruno Farache
+ * @author Silvio Santos
  */
 @ProviderType
 public class PushNotificationsDeviceImpl
 	extends PushNotificationsDeviceBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a push notifications device model instance should use the {@link com.liferay.push.notifications.model.PushNotificationsDevice} interface instead.
-	 */
 	public PushNotificationsDeviceImpl() {
 	}
 
