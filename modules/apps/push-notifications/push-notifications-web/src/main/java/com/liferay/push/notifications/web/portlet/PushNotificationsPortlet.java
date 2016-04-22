21a015293db803510a05dabb595cbf0410e06f6b
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-04-21 18:40:56 -0700

    LPS-65093 Add helper component to find the sender resource bundle loader

--- modules/apps/push-notifications/push-notifications-web/src/main/java/com/liferay/push/notifications/web/portlet/PushNotificationsPortlet.java
+++ modules/apps/push-notifications/push-notifications-web/src/main/java/com/liferay/push/notifications/web/portlet/PushNotificationsPortlet.java
@@ -18,10 +18,17 @@ import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.push.notifications.constants.PushNotificationsPortletKeys;
 import com.liferay.push.notifications.service.PushNotificationsDeviceService;
+import com.liferay.push.notifications.web.constants.PushNotificationsWebKeys;
+import com.liferay.push.notifications.web.util.ResourceBundleLoaderProvider;
+
+import java.io.IOException;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -60,6 +67,18 @@ public class PushNotificationsPortlet extends MVCPortlet {
 			pushNotificationsDeviceId);
 	}
 
+	@Override
+	public void render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		renderRequest.setAttribute(
+			PushNotificationsWebKeys.RESOURCE_BUNDLE_LOADER_PROVIDER,
+			_resourceBundleLoaderProvider);
+
+		super.render(renderRequest, renderResponse);
+	}
+
 	@Reference(unbind = "-")
 	protected void setPushNotificationsDeviceService(
 		PushNotificationsDeviceService pushNotificationsDeviceService) {
@@ -67,6 +86,14 @@ public class PushNotificationsPortlet extends MVCPortlet {
 		_pushNotificationsDeviceService = pushNotificationsDeviceService;
 	}
 
+	@Reference(unbind = "-")
+	protected void setResourceBundleLoaderProvider(
+		ResourceBundleLoaderProvider resourceBundleLoaderProvider) {
+
+		_resourceBundleLoaderProvider = resourceBundleLoaderProvider;
+	}
+
 	private PushNotificationsDeviceService _pushNotificationsDeviceService;
+	private ResourceBundleLoaderProvider _resourceBundleLoaderProvider;
 
 }
