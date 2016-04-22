3ba948e883928f68d148cefe28500ae07a2b4b02
Cristina González Castellano <cristina.gonzalez@liferay.com>
2016-04-22 14:22:18 +0200

    LPS-64951 Create index in upgrade

--- portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeGroup.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeGroup.java
@@ -15,6 +15,7 @@
 package com.liferay.portal.upgrade.v7_0_0;
 
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.LoggingTimer;
 import com.liferay.portal.upgrade.v7_0_0.util.GroupTable;
 
 /**
@@ -22,9 +23,21 @@ import com.liferay.portal.upgrade.v7_0_0.util.GroupTable;
  */
 public class UpgradeGroup extends UpgradeProcess {
 
+	protected void createIndexes() throws Exception {
+		try (LoggingTimer loggingTimer = new LoggingTimer()) {
+			runSQL("create index IX_8257E37B on Group_ (classNameId, classPK)");
+
+			runSQL(
+				"create index IX_B41E3F78 on Group_ " +
+					"(companyId, groupId, parentGroupId)");
+		}
+	}
+
 	@Override
 	protected void doUpgrade() throws Exception {
 		alter(GroupTable.class, new AlterColumnType("name", "STRING null"));
+
+		createIndexes();
 	}
 
 }
