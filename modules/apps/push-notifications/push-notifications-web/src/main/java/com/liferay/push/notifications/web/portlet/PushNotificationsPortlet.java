f9e997fd4f2dded022a7f7ecc0d23dd51ca2fb32
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-04-21 18:01:13 -0700

    LPS-65093 Add DS annotations in the portlet class

--- modules/apps/push-notifications/push-notifications-web/src/main/java/com/liferay/push/notifications/web/portlet/PushNotificationsPortlet.java
+++ modules/apps/push-notifications/push-notifications-web/src/main/java/com/liferay/push/notifications/web/portlet/PushNotificationsPortlet.java
@@ -21,15 +21,37 @@ import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.PrefsPropsUtil;
+import com.liferay.push.notifications.constants.PushNotificationsPortletKeys;
 import com.liferay.push.notifications.service.PushNotificationsDeviceLocalServiceUtil;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.portlet.Portlet;
 import javax.portlet.PortletPreferences;
+
+import org.osgi.service.component.annotations.Component;
 
 /**
  * @author Bruno Farache
  */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.css-class-wrapper=push-notifications",
+		"com.liferay.portlet.display-category=category.hidden",
+		"javax.portlet.display-name=Push Notifications",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.info.keywords=Push Notifications",
+		"javax.portlet.info.short-title=Push Notifications",
+		"javax.portlet.info.title=Push Notifications",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + PushNotificationsPortletKeys.PUSH_NOTIFICATIONS,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
 public class PushNotificationsPortlet extends MVCPortlet {
 
 	public void deletePushNotificationsDevice(
