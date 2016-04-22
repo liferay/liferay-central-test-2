246cdb367fed4fc6a8840763803a510c53eb2604
istvansajtos <istvan.sajtos@liferay.com>
2016-04-22 10:52:38 +0200

    LPS-35823 'ant direct-deploy' does not add liferay-portlet-ext.tld correctly

--- portal-impl/src/com/liferay/portal/tools/deploy/BaseDeployer.java
+++ portal-impl/src/com/liferay/portal/tools/deploy/BaseDeployer.java
@@ -134,7 +134,7 @@ public class BaseDeployer implements AutoDeployer, Deployer {
 		auiTaglibDTD = System.getProperty("deployer.aui.taglib.dtd");
 		portletTaglibDTD = System.getProperty("deployer.portlet.taglib.dtd");
 		portletExtTaglibDTD = System.getProperty(
-			"deployer.portlet.ext.taglib.dtd");
+			"deployer.portlet-ext.taglib.dtd");
 		securityTaglibDTD = System.getProperty("deployer.security.taglib.dtd");
 		themeTaglibDTD = System.getProperty("deployer.theme.taglib.dtd");
 		uiTaglibDTD = System.getProperty("deployer.ui.taglib.dtd");
