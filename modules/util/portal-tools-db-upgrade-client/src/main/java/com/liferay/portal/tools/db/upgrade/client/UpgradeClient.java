5d82650b4e2323f1753eaa34a7620d533aaed225
Brian Chan <brian.chan@liferay.com>
2016-04-22 14:25:08 -0700

    Revert "LPS-63740 SF"
    This reverts commit 893846003925e65582c04a1db981b79bd319d49d.

--- modules/util/portal-tools-db-upgrade-client/src/main/java/com/liferay/portal/tools/db/upgrade/client/UpgradeClient.java
+++ modules/util/portal-tools-db-upgrade-client/src/main/java/com/liferay/portal/tools/db/upgrade/client/UpgradeClient.java
@@ -495,8 +495,7 @@ public class UpgradeClient {
 			_appServerProperties.setProperty(
 				"extra.lib.dirs",
 				StringUtil.join(
-					_getRelativeFileNames(dir, _appServer.getExtraLibDirs()), ',
-					','));
+					_getRelativeFileNames(dir, _appServer.getExtraLibDirs()), ','));
 			_appServerProperties.setProperty(
 				"global.lib.dir", _getRelativeFileName(dir, globalLibDir));
 			_appServerProperties.setProperty(
