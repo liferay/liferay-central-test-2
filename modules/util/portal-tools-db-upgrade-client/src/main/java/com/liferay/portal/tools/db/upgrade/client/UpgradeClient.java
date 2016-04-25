bbc9e10204c8cb5bbf904aa8b48e67d66c9efe0d
Hugo Huijser <hugo.huijser@liferay.com>
2016-04-25 16:55:30 +0800

    LPS-63740 If you change "," back to ',', and run SF, you get an error. Hugo SF (reverted from commit 1dce2d5d29bebadf8ee59c0e98c268596ccdae2a)

--- modules/util/portal-tools-db-upgrade-client/src/main/java/com/liferay/portal/tools/db/upgrade/client/UpgradeClient.java
+++ modules/util/portal-tools-db-upgrade-client/src/main/java/com/liferay/portal/tools/db/upgrade/client/UpgradeClient.java
@@ -496,7 +496,7 @@ public class UpgradeClient {
 				"extra.lib.dirs",
 				StringUtil.join(
 					_getRelativeFileNames(dir, _appServer.getExtraLibDirs()),
-					","));
+					','));
 			_appServerProperties.setProperty(
 				"global.lib.dir", _getRelativeFileName(dir, globalLibDir));
 			_appServerProperties.setProperty(
