f088dc464e20b085aad6f4158e309f9afa1aa3da
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2016-04-25 12:43:58 +0200

    LPS-65111 Remove this since the right code is in UpgradeModules 7.0.1 and this didn't have any match in the execution of the upgrade in 7.0.0

--- portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeModules.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeModules.java
@@ -106,10 +106,6 @@ public class UpgradeModules
 			"microblogs-portlet", "com.liferay.microblogs.service", "Microblogs"
 		},
 		{
-			"notifications-portlet", "com.liferay.notifications.web",
-			"Notifications"
-		},
-		{
 			"so-portlet", "com.liferay.invitation.invite.members.service", "SO"
 		},
 		{
