a6fe85c578eaa261ec197e6b8c74d61a1e24d146
Hugo Huijser <hugo.huijser@liferay.com>
2016-04-25 17:27:52 +0800

    LPS-63740 Auto SF

--- portal-impl/src/com/liferay/portal/util/AggregateUtil.java
+++ portal-impl/src/com/liferay/portal/util/AggregateUtil.java
@@ -51,11 +51,10 @@ public class AggregateUtil {
 	};
 
 	private static final String[] _CSS_PATH_TYPES = new String[] {
-		"url('')", "url(\"\")", "url()", "url('@",
-		"url(\"@", "url(@", "url('http://", "url(\"http://", "url(http://",
-		"url('https://", "url(\"https://", "url(https://", "url('/", "url(\"/",
-		"url(/", "url('data:", "url(\"data:", "url(data:", "url('", "url(\"",
-		"url("
+		"url('')", "url(\"\")", "url()", "url('@", "url(\"@", "url(@",
+		"url('http://", "url(\"http://", "url(http://", "url('https://",
+		"url(\"https://", "url(https://", "url('/", "url(\"/", "url(/",
+		"url('data:", "url(\"data:", "url(data:", "url('", "url(\"", "url("
 	};
 
 }
