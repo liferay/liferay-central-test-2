ff8715783201ec54763ff43e69626cbae71c33e2
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-04-26 10:33:26 -0700

    LPS-65369 Fix SF error

--- modules/apps/lcs/lcs-api/src/main/java/com/liferay/lcs/rest/LCSClusterEntryServiceImpl.java
+++ modules/apps/lcs/lcs-api/src/main/java/com/liferay/lcs/rest/LCSClusterEntryServiceImpl.java
@@ -15,6 +15,7 @@
 package com.liferay.lcs.rest;
 
 import com.liferay.petra.json.web.service.client.JSONWebServiceInvocationException;
+import com.liferay.portal.kernel.util.StringUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -103,8 +104,8 @@ public class LCSClusterEntryServiceImpl
 			_getLCSProjectLCSClusterEntries(lcsProjectId);
 
 		for (LCSClusterEntry lcsClusterEntry : lcsClusterEntries) {
-			if (lcsClusterEntryName.equalsIgnoreCase(
-					lcsClusterEntry.getName())) {
+			if (StringUtil.equalsIgnoreCase(
+					lcsClusterEntryName, lcsClusterEntry.getName())) {
 
 				throw new DuplicateLCSClusterEntryNameException();
 			}
