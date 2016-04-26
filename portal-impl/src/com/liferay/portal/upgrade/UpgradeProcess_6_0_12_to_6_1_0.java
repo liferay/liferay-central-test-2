c09f21e6948f9ddef032689618d122570501c3dd
Cristina González Castellano <cristina.gonzalez@liferay.com>
2016-04-26 11:11:38 +0200

    LPS-65257 Update CompanyId before execute 6_1_0 updates

--- portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_6_0_12_to_6_1_0.java
+++ portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_6_0_12_to_6_1_0.java
@@ -16,6 +16,7 @@ package com.liferay.portal.upgrade;
 
 import com.liferay.portal.kernel.util.ReleaseInfo;
 import com.liferay.portal.upgrade.v6_0_12_to_6_1_0.UpgradeAsset;
+import com.liferay.portal.upgrade.v6_0_12_to_6_1_0.UpgradeCompanyId;
 import com.liferay.portal.upgrade.v6_0_12_to_6_1_0.UpgradeDocumentLibrary;
 import com.liferay.portal.upgrade.v6_0_12_to_6_1_0.UpgradeMessageBoards;
 import com.liferay.portal.upgrade.v6_0_12_to_6_1_0.UpgradePermission;
@@ -48,6 +49,8 @@ public class UpgradeProcess_6_0_12_to_6_1_0 extends Pre7UpgradeProcess {
 	@Override
 	protected void doUpgrade() throws Exception {
 		upgrade(UpgradeSchema.class);
+
+		upgrade(UpgradeCompanyId.class);
 
 		upgrade(UpgradeUserName.class);
 
