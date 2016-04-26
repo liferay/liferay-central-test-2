12e5b653358d9da208b1d56758a8138ee7e37ab4
Julio Camarero <julio.camarero@liferay.com>
2016-04-26 17:46:45 +0200

    LPS-65325 add logic for user2 to context contributor in users admin. this variable can now be used by themes

---
+++ modules/apps/foundation/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/theme/contributor/UsersTemplateContextContributor.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.users.admin.web.theme.contributor;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.template.TemplateContextContributor;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Julio Camarero
+ */
+@Component(
+	immediate = true,
+	property = {"type=" + TemplateContextContributor.TYPE_THEME},
+	service = TemplateContextContributor.class
+)
+public class UsersTemplateContextContributor
+	implements TemplateContextContributor {
+
+	@Override
+	public void prepare(
+		Map<String, Object> contextObjects, HttpServletRequest request) {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		Group group = themeDisplay.getSiteGroup();
+
+		if (group.isUser()) {
+			try {
+				User user2 = _userLocalService.getUserById(group.getClassPK());
+
+				contextObjects.put("user2", user2);
+			}
+			catch (PortalException pe) {
+				_log.error(pe, pe);
+			}
+		}
+	}
+
+	@Reference(unbind = "-")
+	protected void setUserLocalService(UserLocalService userLocalService) {
+		_userLocalService = userLocalService;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		UsersTemplateContextContributor.class);
+
+	private UserLocalService _userLocalService;
+
+}
