702d1a79a1d64d069261726c11c7a703e815d524
Tibor Lipusz <tibor.lipusz@liferay.com>
2016-04-26 15:16:48 +0200

    LPS-65203 Remove extra conditions: Validator.isNotNull(realUser) will always be true --> realUser is current user, realUser.isDefaultUser() --> means it's guest = not authenticated --> this is equal to  ! themeDisplay.isSignedIn() -->  so the condition is "Validator.isNotNull(realUser) && realUser.isDefaultUser()" --> true && realUser.isDefaultUser() --> realUser.isDefaultUser()  = (! themeDisplay.isSignedIn())  --> if (Validator.isNotNull(realUser) && realUser.isDefaultUser() && !company.isStrangers())  = if (!themeDisplay.isSignedIn() && !company.isStrangers())
    Howeve,this would allo authenticated users to by-pass the logic and activate accounts -->
    
    useless --> !company.isStrangers() is enough
    
    Credit goes to @topolik also ;-)

--- modules/apps/foundation/login/login-web/src/main/java/com/liferay/login/web/portlet/action/CreateAnonymousAccountMVCActionCommand.java
+++ modules/apps/foundation/login/login-web/src/main/java/com/liferay/login/web/portlet/action/CreateAnonymousAccountMVCActionCommand.java
@@ -47,7 +47,6 @@ import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StringPool;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.util.PropsValues;
@@ -162,7 +161,6 @@ public class CreateAnonymousAccountMVCActionCommand
 		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
-		User realUser = themeDisplay.getRealUser();
 		Company company = themeDisplay.getCompany();
 
 		PortletConfig portletConfig = (PortletConfig)actionRequest.getAttribute(
@@ -205,9 +203,7 @@ public class CreateAnonymousAccountMVCActionCommand
 					actionRequest, actionResponse, portletURL.toString());
 			}
 			else if (cmd.equals(Constants.UPDATE)) {
-				if (Validator.isNotNull(realUser) && realUser.isDefaultUser() &&
-					!company.isStrangers()) {
-
+				if (!company.isStrangers()) {
 					throw new PrincipalException();
 				}
 
