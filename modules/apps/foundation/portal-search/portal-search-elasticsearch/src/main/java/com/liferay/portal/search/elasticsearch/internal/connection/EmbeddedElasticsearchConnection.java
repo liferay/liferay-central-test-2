2c383ec4b43d61cefab73f44c2722dec1e449899
André de Oliveira <andre.oliveira@liferay.com>
2016-04-26 19:30:41 -0300

    LPS-65153 ES classloader locks plugin jar on Windows; check don't remove
    https://issues.liferay.com/browse/LPS-65153

--- modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/EmbeddedElasticsearchConnection.java
+++ modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/EmbeddedElasticsearchConnection.java
@@ -38,7 +38,6 @@ import java.util.Map;
 
 import org.apache.commons.lang.time.StopWatch;
 
-import org.elasticsearch.Version;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.node.Node;
@@ -203,17 +202,18 @@ public class EmbeddedElasticsearchConnection
 	}
 
 	protected void configurePlugin(String name, Settings settings) {
-		String version = Version.CURRENT.toString();
-
-		String zip = "/plugins/" + name + "-" + version + ".zip";
+		EmbeddedElasticsearchPluginManager embeddedElasticsearchPluginManager =
+			new EmbeddedElasticsearchPluginManager(
+				name, settings.get("path.plugins"),
+				new PluginManagerFactoryImpl(settings),
+				new PluginZipFactoryImpl());
 
 		try {
-			EmbeddedElasticsearchPluginManager.installPlugin(
-				name, zip, getClass(), settings);
+			embeddedElasticsearchPluginManager.install();
 		}
 		catch (IOException ioe) {
 			throw new RuntimeException(
-				"Unable to install " + name + " plugin from " + zip, ioe);
+				"Unable to install " + name + " plugin", ioe);
 		}
 	}
 
