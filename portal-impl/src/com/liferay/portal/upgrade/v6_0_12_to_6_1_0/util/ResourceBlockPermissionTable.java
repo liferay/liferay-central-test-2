cde0314ded4a3e368258e6c552d2584cbabe806e
Brian Chan <brian.chan@liferay.com>
2016-04-26 13:58:37 -0700

    LPS-65257 Regen

--- portal-impl/src/com/liferay/portal/upgrade/v6_0_12_to_6_1_0/util/ResourceBlockPermissionTable.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_0_12_to_6_1_0/util/ResourceBlockPermissionTable.java
@@ -16,11 +16,8 @@ package com.liferay.portal.upgrade.v6_0_12_to_6_1_0.util;
 
 import java.sql.Types;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
- * @author	  Cristina González
+ * @author	  Brian Wing Shun Chan
  * @generated
  */
 public class ResourceBlockPermissionTable {
@@ -28,37 +25,19 @@ public class ResourceBlockPermissionTable {
 	public static final String TABLE_NAME = "ResourceBlockPermission";
 
 	public static final Object[][] TABLE_COLUMNS = {
-		{"mvccVersion", Types.BIGINT},
 		{"resourceBlockPermissionId", Types.BIGINT},
-		{"companyId", Types.BIGINT},
 		{"resourceBlockId", Types.BIGINT},
 		{"roleId", Types.BIGINT},
 		{"actionIds", Types.BIGINT}
 	};
 
-	public static final Map<String, Integer> TABLE_COLUMNS_MAP = new HashMap<String, Integer>();
-
-static {
-TABLE_COLUMNS_MAP.put("mvccVersion", Types.BIGINT);
-
-TABLE_COLUMNS_MAP.put("resourceBlockPermissionId", Types.BIGINT);
-
-TABLE_COLUMNS_MAP.put("companyId", Types.BIGINT);
-
-TABLE_COLUMNS_MAP.put("resourceBlockId", Types.BIGINT);
-
-TABLE_COLUMNS_MAP.put("roleId", Types.BIGINT);
-
-TABLE_COLUMNS_MAP.put("actionIds", Types.BIGINT);
-
-}
-	public static final String TABLE_SQL_CREATE = "create table ResourceBlockPermission (mvccVersion LONG default 0 not null,resourceBlockPermissionId LONG not null primary key,companyId LONG,resourceBlockId LONG,roleId LONG,actionIds LONG)";
+	public static final String TABLE_SQL_CREATE = "create table ResourceBlockPermission (resourceBlockPermissionId LONG not null primary key,resourceBlockId LONG,roleId LONG,actionIds LONG)";
 
 	public static final String TABLE_SQL_DROP = "drop table ResourceBlockPermission";
 
 	public static final String[] TABLE_SQL_ADD_INDEXES = {
-		"create unique index IX_D63D20BB on ResourceBlockPermission (resourceBlockId, roleId)",
-		"create index IX_20A2E3D9 on ResourceBlockPermission (roleId)"
+		"create index IX_4AB3756 on ResourceBlockPermission (resourceBlockId)",
+		"create unique index IX_D63D20BB on ResourceBlockPermission (resourceBlockId, roleId)"
 	};
 
 }
