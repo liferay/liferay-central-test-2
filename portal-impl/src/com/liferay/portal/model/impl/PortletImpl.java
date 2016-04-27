9c007aaec78151a4a63e0a80bd7263b945702c4c
Adolfo Pérez <adolfo.perez@liferay.com>
2016-04-27 12:17:08 +0200

    LPS-65389 Avoid NPE on initialization
    The implementation assumes that the `getFriendlyURLMapperInstance`
    method will only be called on fully initialized portlets.

--- portal-impl/src/com/liferay/portal/model/impl/PortletImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/PortletImpl.java
@@ -909,6 +909,10 @@ public class PortletImpl extends PortletBaseImpl {
 	public FriendlyURLMapper getFriendlyURLMapperInstance() {
 		PortletBag portletBag = PortletBagPool.get(getRootPortletId());
 
+		if (portletBag == null) {
+			return null;
+		}
+
 		FriendlyURLMapperTracker friendlyURLMapperTracker =
 			portletBag.getFriendlyURLMapperTracker();
 
