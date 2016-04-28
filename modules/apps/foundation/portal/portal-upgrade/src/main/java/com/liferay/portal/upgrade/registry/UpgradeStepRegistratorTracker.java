8676203ecedea9c75aa328fe359f1c30cc88ae3a
Cristina González Castellano <cristina.gonzalez@liferay.com>
2016-04-28 13:48:16 +0200

    LPS-65372 New method UpgradeStepRegistratorTracker so properties can be registered for the UpgradeSteps

--- modules/apps/foundation/portal/portal-upgrade/src/main/java/com/liferay/portal/upgrade/registry/UpgradeStepRegistratorTracker.java
+++ modules/apps/foundation/portal/portal-upgrade/src/main/java/com/liferay/portal/upgrade/registry/UpgradeStepRegistratorTracker.java
@@ -44,7 +44,7 @@ public class UpgradeStepRegistratorTracker {
 	public static List<ServiceRegistration<UpgradeStep>> register(
 		BundleContext bundleContext, String bundleSymbolicName,
 		String fromSchemaVersionString, String toSchemaVersionString,
-		UpgradeStep... upgradeSteps) {
+		Dictionary<String, Object> properties, UpgradeStep... upgradeSteps) {
 
 		List<ServiceRegistration<UpgradeStep>> serviceRegistrations =
 			new ArrayList<>();
@@ -54,12 +54,23 @@ public class UpgradeStepRegistratorTracker {
 
 		for (UpgradeInfo upgradeInfo : upgradeInfos) {
 			ServiceRegistration<UpgradeStep> serviceRegistration = _register(
-				bundleContext, bundleSymbolicName, upgradeInfo);
+				bundleContext, bundleSymbolicName, upgradeInfo, properties);
 
 			serviceRegistrations.add(serviceRegistration);
 		}
 
 		return serviceRegistrations;
+	}
+
+	public static List<ServiceRegistration<UpgradeStep>> register(
+		BundleContext bundleContext, String bundleSymbolicName,
+		String fromSchemaVersionString, String toSchemaVersionString,
+		UpgradeStep... upgradeSteps) {
+
+		return register(
+			bundleContext, bundleSymbolicName, fromSchemaVersionString,
+			toSchemaVersionString, new Hashtable<String, Object>(),
+			upgradeSteps);
 	}
 
 	protected static List<UpgradeInfo> createUpgradeInfos(
@@ -124,9 +135,7 @@ public class UpgradeStepRegistratorTracker {
 
 	private static ServiceRegistration<UpgradeStep> _register(
 		BundleContext bundleContext, String bundleSymbolicName,
-		UpgradeInfo upgradeInfo) {
-
-		Dictionary<String, Object> properties = new Hashtable<>();
+		UpgradeInfo upgradeInfo, Dictionary<String, Object> properties) {
 
 		properties.put("upgrade.bundle.symbolic.name", bundleSymbolicName);
 		properties.put("upgrade.db.type", "any");
@@ -214,7 +223,8 @@ public class UpgradeStepRegistratorTracker {
 				for (UpgradeInfo upgradeInfo : upgradeInfos) {
 					ServiceRegistration<UpgradeStep> serviceRegistration =
 						_register(
-							_bundleContext, bundleSymbolicName, upgradeInfo);
+							_bundleContext, bundleSymbolicName, upgradeInfo,
+							new Hashtable<String, Object>());
 
 					_serviceRegistrations.add(serviceRegistration);
 				}
