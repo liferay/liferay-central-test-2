275ce69eac23b914e3cc47c64803a257d680a56e
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2016-04-29 14:29:33 +0200

    LPS-65441 Create a new Upgrade process to handle an issue with the default DDMStructure TIKARAWMetadata. In 6.1 we had a DDMStructure with key tikarawmetadata and the classname matched DLFileEntry.class. Howver, this changed in 6.2 and a brand new DDMStructure was created with the same key but with a classname that mached RawMetadataProcessor.class. Therefore, all the Tika metadata for documents in 6.1 was not visible either in 6.2 or 7. This upgrade takes care of that but it doesn't delete any DDMStructure to avoid losing information.

--- portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_7_0_1.java
+++ portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_7_0_1.java
@@ -17,6 +17,7 @@ package com.liferay.portal.upgrade;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.ReleaseInfo;
 import com.liferay.portal.upgrade.v7_0_1.UpgradeCompany;
+import com.liferay.portal.upgrade.v7_0_1.UpgradeDocumentLibrary;
 import com.liferay.portal.upgrade.v7_0_1.UpgradeMessageBoards;
 import com.liferay.portal.upgrade.v7_0_1.UpgradeModules;
 import com.liferay.portal.upgrade.v7_0_1.UpgradeSchema;
@@ -36,6 +37,7 @@ public class UpgradeProcess_7_0_1 extends UpgradeProcess {
 		upgrade(UpgradeSchema.class);
 
 		upgrade(UpgradeCompany.class);
+		upgrade(UpgradeDocumentLibrary.class);
 		upgrade(UpgradeMessageBoards.class);
 		upgrade(UpgradeModules.class);
 
