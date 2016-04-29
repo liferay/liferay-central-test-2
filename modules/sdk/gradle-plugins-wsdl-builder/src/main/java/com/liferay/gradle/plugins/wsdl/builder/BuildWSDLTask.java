8416cda7b0238b0c2dae391b473c33bc3fd6b5a6
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-04-29 16:03:38 -0700

    LPS-65425 Add configuration options for WSDL2Java

--- modules/sdk/gradle-plugins-wsdl-builder/src/main/java/com/liferay/gradle/plugins/wsdl/builder/BuildWSDLTask.java
+++ modules/sdk/gradle-plugins-wsdl-builder/src/main/java/com/liferay/gradle/plugins/wsdl/builder/BuildWSDLTask.java
@@ -16,6 +16,8 @@ package com.liferay.gradle.plugins.wsdl.builder;
 
 import com.liferay.gradle.util.GradleUtil;
 
+import groovy.lang.Closure;
+
 import java.io.File;
 
 import java.util.HashMap;
@@ -26,6 +28,7 @@ import org.gradle.api.Project;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputFiles;
+import org.gradle.api.tasks.Nested;
 import org.gradle.api.tasks.SkipWhenEmpty;
 
 /**
@@ -33,9 +36,20 @@ import org.gradle.api.tasks.SkipWhenEmpty;
  */
 public class BuildWSDLTask extends DefaultTask {
 
+	public void generateOptions(Closure<?> closure) {
+		Project project = getProject();
+
+		project.configure(getGenerateOptions(), closure);
+	}
+
 	@Input
 	public File getDestinationDir() {
 		return GradleUtil.toFile(getProject(), _destinationDir);
+	}
+
+	@Nested
+	public GenerateOptions getGenerateOptions() {
+		return _generateOptions;
 	}
 
 	@Input
@@ -84,6 +98,7 @@ public class BuildWSDLTask extends DefaultTask {
 
 	private boolean _buildLibs = true;
 	private Object _destinationDir;
+	private final GenerateOptions _generateOptions = new GenerateOptions();
 	private boolean _includeSource = true;
 	private Object _inputDir;
 
