0147646f38f667d58b3ede7c3b316400f8eaebef
Leonardo Barros <leonardo.barros@liferay.com>
2016-05-02 13:55:36 -0300

    LPS-65430 Do permission check using Calendar

--- modules/apps/forms-and-workflow/calendar/calendar-service/src/main/java/com/liferay/calendar/search/CalendarBookingIndexer.java
+++ modules/apps/forms-and-workflow/calendar/calendar-service/src/main/java/com/liferay/calendar/search/CalendarBookingIndexer.java
@@ -32,7 +32,9 @@ import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.IndexWriterHelperUtil;
 import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.Summary;
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.util.GetterUtil;
@@ -69,6 +71,20 @@ public class CalendarBookingIndexer extends BaseIndexer<CalendarBooking> {
 	@Override
 	public String getClassName() {
 		return CLASS_NAME;
+	}
+
+	@Override
+	public BooleanFilter getFacetBooleanFilter(
+			String className, SearchContext searchContext)
+		throws Exception {
+
+		BooleanFilter booleanFilter = super.getFacetBooleanFilter(
+			Calendar.class.getName(), searchContext);
+
+		booleanFilter.addTerm(
+			Field.ENTRY_CLASS_NAME, CalendarBooking.class.getName());
+
+		return booleanFilter;
 	}
 
 	@Deprecated
