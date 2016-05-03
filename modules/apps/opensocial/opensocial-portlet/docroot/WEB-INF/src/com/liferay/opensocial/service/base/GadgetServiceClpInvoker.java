392852e7458135928692dee2fc4b2e51c99e0ca5
Hugo Huijser <hugo.huijser@liferay.com>
2016-05-03 15:53:48 +0800

    LPS-65482 Regenerate

--- modules/apps/opensocial/opensocial-portlet/docroot/WEB-INF/src/com/liferay/opensocial/service/base/GadgetServiceClpInvoker.java
+++ modules/apps/opensocial/opensocial-portlet/docroot/WEB-INF/src/com/liferay/opensocial/service/base/GadgetServiceClpInvoker.java
@@ -27,26 +27,26 @@ import java.util.Arrays;
 @ProviderType
 public class GadgetServiceClpInvoker {
 	public GadgetServiceClpInvoker() {
-		_methodName32 = "getOSGiServiceIdentifier";
+		_methodName38 = "getOSGiServiceIdentifier";
 
-		_methodParameterTypes32 = new String[] {  };
+		_methodParameterTypes38 = new String[] {  };
 
-		_methodName37 = "addGadget";
+		_methodName43 = "addGadget";
 
-		_methodParameterTypes37 = new String[] {
+		_methodParameterTypes43 = new String[] {
 				"long", "java.lang.String", "java.lang.String",
 				"com.liferay.portal.kernel.service.ServiceContext"
 			};
 
-		_methodName38 = "deleteGadget";
+		_methodName44 = "deleteGadget";
 
-		_methodParameterTypes38 = new String[] {
+		_methodParameterTypes44 = new String[] {
 				"long", "com.liferay.portal.kernel.service.ServiceContext"
 			};
 
-		_methodName39 = "updateGadget";
+		_methodName45 = "updateGadget";
 
-		_methodParameterTypes39 = new String[] {
+		_methodParameterTypes45 = new String[] {
 				"long", "java.lang.String",
 				"com.liferay.portal.kernel.service.ServiceContext"
 			};
@@ -54,28 +54,28 @@ public class GadgetServiceClpInvoker {
 
 	public Object invokeMethod(String name, String[] parameterTypes,
 		Object[] arguments) throws Throwable {
-		if (_methodName32.equals(name) &&
-				Arrays.deepEquals(_methodParameterTypes32, parameterTypes)) {
+		if (_methodName38.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes38, parameterTypes)) {
 			return GadgetServiceUtil.getOSGiServiceIdentifier();
 		}
 
-		if (_methodName37.equals(name) &&
-				Arrays.deepEquals(_methodParameterTypes37, parameterTypes)) {
+		if (_methodName43.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes43, parameterTypes)) {
 			return GadgetServiceUtil.addGadget(((Long)arguments[0]).longValue(),
 				(java.lang.String)arguments[1], (java.lang.String)arguments[2],
 				(com.liferay.portal.kernel.service.ServiceContext)arguments[3]);
 		}
 
-		if (_methodName38.equals(name) &&
-				Arrays.deepEquals(_methodParameterTypes38, parameterTypes)) {
+		if (_methodName44.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes44, parameterTypes)) {
 			GadgetServiceUtil.deleteGadget(((Long)arguments[0]).longValue(),
 				(com.liferay.portal.kernel.service.ServiceContext)arguments[1]);
 
 			return null;
 		}
 
-		if (_methodName39.equals(name) &&
-				Arrays.deepEquals(_methodParameterTypes39, parameterTypes)) {
+		if (_methodName45.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes45, parameterTypes)) {
 			GadgetServiceUtil.updateGadget(((Long)arguments[0]).longValue(),
 				(java.lang.String)arguments[1],
 				(com.liferay.portal.kernel.service.ServiceContext)arguments[2]);
@@ -86,12 +86,12 @@ public class GadgetServiceClpInvoker {
 		throw new UnsupportedOperationException();
 	}
 
-	private String _methodName32;
-	private String[] _methodParameterTypes32;
-	private String _methodName37;
-	private String[] _methodParameterTypes37;
 	private String _methodName38;
 	private String[] _methodParameterTypes38;
-	private String _methodName39;
-	private String[] _methodParameterTypes39;
+	private String _methodName43;
+	private String[] _methodParameterTypes43;
+	private String _methodName44;
+	private String[] _methodParameterTypes44;
+	private String _methodName45;
+	private String[] _methodParameterTypes45;
 }
