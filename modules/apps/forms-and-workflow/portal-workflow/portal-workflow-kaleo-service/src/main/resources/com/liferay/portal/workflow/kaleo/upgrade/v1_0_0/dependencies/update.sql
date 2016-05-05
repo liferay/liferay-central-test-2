b5b77cbe3bcdade368aefb08bf41f061eb28f2da
Rafael Praxedes <rafael.praxedes@liferay.com>
2016-05-05 11:32:34 -0300

    LPS-65453 Add upgrade schema to include the tables KaleoTimer and KaleoTimerInstanceToken

---
+++ modules/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-service/src/main/resources/com/liferay/portal/workflow/kaleo/upgrade/v1_0_0/dependencies/update.sql
@@ -0,0 +1,42 @@
+create table KaleoTimer (
+	kaleoTimerId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(200) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	kaleoClassName VARCHAR(200) null,
+	kaleoClassPK LONG,
+	kaleoDefinitionId LONG,
+	name VARCHAR(75) null,
+	blocking BOOLEAN,
+	description STRING null,
+	duration DOUBLE,
+	scale VARCHAR(75) null,
+	recurrenceDuration DOUBLE,
+	recurrenceScale VARCHAR(75) null
+);
+
+create table KaleoTimerInstanceToken (
+	kaleoTimerInstanceTokenId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(200) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	kaleoClassName VARCHAR(200) null,
+	kaleoClassPK LONG,
+	kaleoDefinitionId LONG,
+	kaleoInstanceId LONG,
+	kaleoInstanceTokenId LONG,
+	kaleoTaskInstanceTokenId LONG,
+	kaleoTimerId LONG,
+	kaleoTimerName VARCHAR(200) null,
+	blocking BOOLEAN,
+	completionUserId LONG,
+	completed BOOLEAN,
+	completionDate DATE null,
+	workflowContext TEXT null
+);
