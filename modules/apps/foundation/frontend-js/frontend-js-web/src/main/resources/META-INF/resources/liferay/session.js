87566634f6f7e2d043e6b1daca544c2be7141f83
Dustin Ryerson <dustin.ryerson@liferay.com>
2016-05-04 17:30:12 -0700

    LPS-64627 - Use A.Lang.toInt() instead. (fixing an additional case)

--- modules/apps/foundation/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/session.js
+++ modules/apps/foundation/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/session.js
@@ -322,7 +322,7 @@ AUI.add(
 
 								var elapsed = sessionLength;
 
-								var value = parseInt(timestamp, 10);
+								var value = Lang.toInt(timestamp);
 
 								if (!value) {
 									timeOffset = Math.floor((Date.now() - timestamp) / 1000) * 1000;
