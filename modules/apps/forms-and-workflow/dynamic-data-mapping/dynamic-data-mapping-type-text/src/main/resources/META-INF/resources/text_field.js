b65120500e1cddaebd3cbdcb3dbcb6c53a590d67
Thiago Rocha <thiago.rocha@liferay.com>
2016-05-06 19:52:13 -0300

    LPS-64073 Adding focus and blur events as triggers of info tooltip of create/edit field's modal

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-text/src/main/resources/META-INF/resources/text_field.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-text/src/main/resources/META-INF/resources/text_field.js
@@ -5,6 +5,8 @@ AUI.add(
 			{
 				position: 'left',
 				trigger: '.liferay-ddm-form-field-text .help-icon',
+				triggerHideEvent: ['blur', 'mouseleave'],
+ 	            triggerShowEvent: ['focus', 'mouseover'],
 				visible: false
 			}
 		);
