ac2bb654f7380e1b20c8aec11b0c7d7e2c164947
danielkocsis <daniel.kocsis@liferay.com>
2016-05-06 17:02:27 +0200

    LPS-60027 Extend test case

--- modules/apps/web-experience/export-import/export-import-resources-importer-test/src/testIntegration/java/com/liferay/exportimport/resources/importer/test/ResourcesImporterTest.java
+++ modules/apps/web-experience/export-import/export-import-resources-importer-test/src/testIntegration/java/com/liferay/exportimport/resources/importer/test/ResourcesImporterTest.java
@@ -15,21 +15,41 @@
 package com.liferay.exportimport.resources.importer.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.asset.kernel.model.AssetTag;
+import com.liferay.asset.kernel.service.AssetEntryLocalServiceUtil;
+import com.liferay.asset.kernel.service.AssetTagLocalServiceUtil;
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.model.DLFileEntryConstants;
+import com.liferay.document.library.kernel.model.DLFolder;
 import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.document.library.kernel.service.DLFileEntryLocalServiceUtil;
+import com.liferay.document.library.kernel.service.DLFolderLocalServiceUtil;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.service.DDMStructureLocalServiceUtil;
+import com.liferay.dynamic.data.mapping.service.DDMTemplateLocalServiceUtil;
+import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.service.JournalArticleLocalServiceUtil;
+import com.liferay.portal.kernel.dao.orm.Criterion;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageListener;
 import com.liferay.portal.kernel.messaging.MessageListenerException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutPrototype;
 import com.liferay.portal.kernel.model.LayoutTypePortlet;
+import com.liferay.portal.kernel.model.LayoutTypePortletConstants;
 import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
+import com.liferay.portal.kernel.service.LayoutPrototypeLocalServiceUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.shrinkwrap.osgi.api.BndProjectBuilder;
 
@@ -40,6 +60,8 @@ import java.net.URL;
 import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -149,47 +171,21 @@ public class ResourcesImporterTest {
 	}
 
 	@Test
-	public void testImportResourcesWAR() throws Exception {
+	public void testResourcesImport() throws Exception {
 		_importedGroup = GroupLocalServiceUtil.fetchGroup(
 			TestPropsValues.getCompanyId(), "ResourcesImporterTest");
 
-		// Verify the deployment
-
 		Assert.assertNotNull(_importedGroup);
 
-		long privateLayoutsCount = LayoutLocalServiceUtil.getLayoutsCount(
-			_importedGroup, true);
+		_layoutPrototypes = getLayoutPrototypes();
 
-		Assert.assertEquals(0, privateLayoutsCount);
+		Assert.assertEquals(2, _layoutPrototypes.size());
 
-		long publicLayoutsCount = LayoutLocalServiceUtil.getLayoutsCount(
-			_importedGroup, false);
+		validateLayouts(_importedGroup);
 
-		Assert.assertEquals(4, publicLayoutsCount);
+		validateJournal(_importedGroup);
 
-		long journalArticlesCount =
-			JournalArticleLocalServiceUtil.getArticlesCount(
-				_importedGroup.getGroupId());
-
-		Assert.assertEquals(2, journalArticlesCount);
-
-		long fileEntriesCount = DLFileEntryLocalServiceUtil.getFileEntriesCount(
-			_importedGroup.getGroupId(),
-			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID);
-
-		Assert.assertEquals(2, fileEntriesCount);
-
-		Layout homeLayout = LayoutLocalServiceUtil.getFriendlyURLLayout(
-			_importedGroup.getGroupId(), false, "/home");
-
-		Assert.assertTrue(homeLayout.isTypePortlet());
-
-		LayoutTypePortlet layoutTypePortlet =
-			(LayoutTypePortlet)homeLayout.getLayoutType();
-
-		List<Portlet> portlets = layoutTypePortlet.getAllPortlets();
-
-		Assert.assertEquals(2, portlets.size());
+		validateDocumentLibrary(_importedGroup);
 	}
 
 	protected static void addWebInfResource(
@@ -204,34 +200,30 @@ public class ResourcesImporterTest {
 	protected static WebArchive buildTestWebArchive() {
 		WebArchive webArchive = ShrinkWrap.create(WebArchive.class);
 
-		// Generic files
+		// Generic resources
 
 		addWebInfResource(webArchive, "liferay-plugin-package.properties");
-
-		// Site map
-
+		addWebInfResource(webArchive, "classes/resources-importer/assets.json");
 		addWebInfResource(
 			webArchive, "classes/resources-importer/sitemap.json");
 
-		// Document Library specific files
+		// Document Library specific resources
 
 		addWebInfResource(
 			webArchive,
-			"classes/resources-importer/document_library/documents/image1.jpg");
+			"classes/resources-importer/document_library/documents/" +
+				"company_logo.png");
 		addWebInfResource(
 			webArchive,
-			"classes/resources-importer/document_library/documents/image2.jpg");
+			"classes/resources-importer/document_library/documents/" +
+				"Parent Folder/child_document.txt");
 
-		// Journal specific files
+		// Journal specific resources
 
 		addWebInfResource(
 			webArchive,
 			"classes/resources-importer/journal/articles/BASIC_WEB_CONTENT/" +
-				"article1.html");
-		addWebInfResource(
-			webArchive,
-			"classes/resources-importer/journal/articles/BASIC_WEB_CONTENT/" +
-				"article2.html");
+				"Basic Article.xml");
 		addWebInfResource(
 			webArchive,
 			"classes/resources-importer/journal/structures/" +
@@ -240,8 +232,193 @@ public class ResourcesImporterTest {
 			webArchive,
 			"classes/resources-importer/journal/templates/" +
 				"BASIC_WEB_CONTENT/BASIC_WEB_CONTENT.ftl");
+		addWebInfResource(
+			webArchive,
+			"classes/resources-importer/journal/articles/Child Template/" +
+				"Child Template Article.xml");
+		addWebInfResource(
+			webArchive,
+			"classes/resources-importer/journal/structures/Parent Structure/" +
+				"Child Structure.json");
+		addWebInfResource(
+			webArchive,
+			"classes/resources-importer/journal/templates/Child Structure/" +
+				"Child Template.ftl");
+		addWebInfResource(
+			webArchive,
+			"classes/resources-importer/journal/articles/Parent Template/" +
+				"Parent Template Article.xml");
+		addWebInfResource(
+			webArchive,
+			"classes/resources-importer/journal/structures/" +
+				"Parent Structure.json");
+		addWebInfResource(
+			webArchive,
+			"classes/resources-importer/journal/templates/Parent Structure/" +
+				"Parent Template.ftl");
+
+		// Page Templates
+
+		addWebInfResource(
+			webArchive,
+			"classes/resources-importer/templates/page/page_1.json");
+		addWebInfResource(
+			webArchive,
+			"classes/resources-importer/templates/page/page_2.json");
 
 		return webArchive;
+	}
+
+	protected List<LayoutPrototype> getLayoutPrototypes() {
+		DynamicQuery dynamicQuery =
+			LayoutPrototypeLocalServiceUtil.dynamicQuery();
+
+		Criterion layout1Criterion = RestrictionsFactoryUtil.like(
+			"name", "%Page 1%");
+		Criterion layout2Criterion = RestrictionsFactoryUtil.like(
+			"name", "%Page 2%");
+
+		dynamicQuery.add(
+			RestrictionsFactoryUtil.or(layout1Criterion, layout2Criterion));
+
+		return LayoutPrototypeLocalServiceUtil.dynamicQuery(dynamicQuery);
+	}
+
+	protected void validateDocumentLibrary(Group importedGroup) {
+		List<DLFileEntry> dlFileEntries =
+			DLFileEntryLocalServiceUtil.getFileEntries(
+				importedGroup.getGroupId(),
+				DLFolderConstants.DEFAULT_PARENT_FOLDER_ID);
+
+		Assert.assertEquals(1, dlFileEntries.size());
+
+		DLFileEntry dlFileEntry = dlFileEntries.get(0);
+
+		AssetEntry assetEntry = AssetEntryLocalServiceUtil.fetchEntry(
+			DLFileEntryConstants.getClassName(), dlFileEntry.getFileEntryId());
+
+		Assert.assertEquals(
+			2,
+			AssetTagLocalServiceUtil.getAssetEntryAssetTagsCount(
+				assetEntry.getEntryId()));
+
+		List<DLFolder> dlFolders = DLFolderLocalServiceUtil.getFolders(
+			importedGroup.getGroupId(),
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID);
+
+		Assert.assertEquals(1, dlFolders.size());
+
+		DLFolder dlFolder = dlFolders.get(0);
+
+		int dlFileEntriesCount =
+			DLFileEntryLocalServiceUtil.getFileEntriesCount(
+				importedGroup.getGroupId(), dlFolder.getFolderId());
+
+		Assert.assertEquals(1, dlFileEntriesCount);
+	}
+
+	protected void validateJournal(Group importedGroup) throws Exception {
+		List<JournalArticle> journalArticles =
+			JournalArticleLocalServiceUtil.getArticles(
+				importedGroup.getGroupId());
+
+		Assert.assertEquals(3, journalArticles.size());
+
+		int ddmStructuresCount =
+			DDMStructureLocalServiceUtil.getStructuresCount(
+				importedGroup.getGroupId(),
+				PortalUtil.getClassNameId(JournalArticle.class));
+
+		Assert.assertEquals(3, ddmStructuresCount);
+
+		int ddmTemplatesCount = DDMTemplateLocalServiceUtil.getTemplatesCount(
+			importedGroup.getGroupId(),
+			PortalUtil.getClassNameId(DDMStructure.class));
+
+		Assert.assertEquals(3, ddmTemplatesCount);
+
+		JournalArticle journalArticle =
+			JournalArticleLocalServiceUtil.getArticle(
+				importedGroup.getGroupId(), "BASIC-ARTICLE");
+
+		Assert.assertTrue(journalArticle.isSmallImage());
+
+		Map<Locale, String> descriptionMap = journalArticle.getDescriptionMap();
+
+		Assert.assertFalse(descriptionMap.isEmpty());
+
+		AssetEntry assetEntry = AssetEntryLocalServiceUtil.fetchEntry(
+			JournalArticle.class.getName(),
+			journalArticle.getResourcePrimKey());
+
+		List<AssetTag> assetTags = AssetTagLocalServiceUtil.getEntryTags(
+			assetEntry.getEntryId());
+
+		Assert.assertEquals(1, assetTags.size());
+	}
+
+	protected void validateLayouts(Group importedGroup) throws Exception {
+		long privateLayoutsCount = LayoutLocalServiceUtil.getLayoutsCount(
+			importedGroup, true);
+
+		Assert.assertEquals(1, privateLayoutsCount);
+
+		long publicLayoutsCount = LayoutLocalServiceUtil.getLayoutsCount(
+			importedGroup, false);
+
+		Assert.assertEquals(9, publicLayoutsCount);
+
+		Layout layout = LayoutLocalServiceUtil.getFriendlyURLLayout(
+			importedGroup.getGroupId(), false, "/home");
+
+		Map<Locale, String> nameMap = layout.getNameMap();
+
+		Assert.assertTrue(nameMap.containsValue("Bienvenue"));
+
+		Assert.assertTrue(layout.isTypePortlet());
+
+		LayoutTypePortlet layoutTypePortlet =
+			(LayoutTypePortlet)layout.getLayoutType();
+
+		List<Portlet> portlets = layoutTypePortlet.getAllPortlets();
+
+		Assert.assertEquals(7, portlets.size());
+
+		UnicodeProperties layoutTypeSettingsProperties =
+			layout.getTypeSettingsProperties();
+
+		String nestedColumnIds = layoutTypeSettingsProperties.get(
+			LayoutTypePortletConstants.NESTED_COLUMN_IDS);
+
+		Assert.assertTrue(
+			(nestedColumnIds != null) && nestedColumnIds.contains("column-1") &&
+			nestedColumnIds.contains("column-2"));
+
+		layout = LayoutLocalServiceUtil.getFriendlyURLLayout(
+			importedGroup.getGroupId(), false, "/layout-prototypes-page-1");
+
+		Assert.assertTrue(layout.isLayoutPrototypeLinkActive());
+		Assert.assertEquals(
+			"371647ba-3649-4039-bfe6-ae32cf404737",
+			layout.getLayoutPrototypeUuid());
+
+		layout = LayoutLocalServiceUtil.getFriendlyURLLayout(
+			importedGroup.getGroupId(), false, "/layout-prototypes-page-2");
+
+		Assert.assertFalse(layout.isLayoutPrototypeLinkActive());
+		Assert.assertEquals(
+			"c98067d0-fc10-9556-7364-238d39693bc4",
+			layout.getLayoutPrototypeUuid());
+
+		layout = LayoutLocalServiceUtil.getFriendlyURLLayout(
+			importedGroup.getGroupId(), false, "/url-page");
+
+		Assert.assertTrue(layout.isTypeURL());
+
+		layout = LayoutLocalServiceUtil.getFriendlyURLLayout(
+			importedGroup.getGroupId(), false, "/hidden-page");
+
+		Assert.assertTrue(layout.isHidden());
 	}
 
 	private static final String _RESOURCES_BASE_PATH = "dependencies/WEB-INF/";
@@ -250,6 +427,9 @@ public class ResourcesImporterTest {
 
 	@DeleteAfterTestRun
 	private Group _importedGroup;
+
+	@DeleteAfterTestRun
+	private List<LayoutPrototype> _layoutPrototypes;
 
 	private Bundle _wabBundle;
 
