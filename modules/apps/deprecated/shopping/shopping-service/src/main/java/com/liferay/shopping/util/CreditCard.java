503a266b7494bf46f432a0b398a33c410b5e3cf0
peterfellwock <peter.fellwock@liferay.com>
2016-05-06 14:47:53 -0700

    LPS-65546 Update java paths

--- modules/apps/deprecated/shopping/shopping-service/src/main/java/com/liferay/shopping/util/CreditCard.java
+++ modules/apps/deprecated/shopping/shopping-service/src/main/java/com/liferay/shopping/util/CreditCard.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.util;
+package com.liferay.shopping.util;
 
 import com.liferay.portal.kernel.util.CalendarFactoryUtil;
 import com.liferay.portal.kernel.util.CalendarUtil;
