2c851c6ec458444cc3e7a01696529e883f017a02
Matthew Tambara <matthew.tambara@liferay.com>
2016-05-06 11:31:20 -0700

    LPS-65067 Add LPKG deployment test

---
+++ modules/core/portal-lpkg-deployer-test/src/testIntegration/java/com/liferay/portal/lpkg/deployer/test/LPKGDeployerTest.java
@@ -0,0 +1,146 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.lpkg.deployer.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.lpkg.deployer.LPKGDeployer;
+import com.liferay.portal.util.PropsValues;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ * @author Matthew Tambara
+ */
+@RunWith(Arquillian.class)
+public class LPKGDeployerTest {
+
+	@Test
+	public void testDeployedLPKGs() throws Exception {
+		Bundle testBundle = FrameworkUtil.getBundle(LPKGDeployerTest.class);
+
+		BundleContext bundleContext = testBundle.getBundleContext();
+
+		String deploymentDir = GetterUtil.getString(
+			bundleContext.getProperty("lpkg.deployer.dir"),
+			PropsValues.MODULE_FRAMEWORK_BASE_DIR + "/marketplace");
+
+		Path deploymentDirPath = Paths.get(deploymentDir);
+
+		final List<File> lpkgFiles = new ArrayList<>();
+
+		Files.createDirectories(deploymentDirPath);
+
+		Files.walkFileTree(
+			deploymentDirPath,
+			new SimpleFileVisitor<Path>() {
+
+				@Override
+				public FileVisitResult visitFile(
+						Path filePath, BasicFileAttributes basicFileAttributes)
+					throws IOException {
+
+					Path fileNamePath = filePath.getFileName();
+
+					String fileName = StringUtil.toLowerCase(
+						fileNamePath.toString());
+
+					if (!fileName.endsWith(".lpkg")) {
+						return FileVisitResult.CONTINUE;
+					}
+
+					lpkgFiles.add(filePath.toFile());
+
+					return FileVisitResult.CONTINUE;
+				}
+
+			});
+
+		Assert.assertTrue(!lpkgFiles.isEmpty());
+
+		ServiceTracker<LPKGDeployer, LPKGDeployer> serviceTracker =
+			new ServiceTracker<>(bundleContext, LPKGDeployer.class, null);
+
+		serviceTracker.open();
+
+		LPKGDeployer lpkgDeployer = serviceTracker.getService();
+
+		serviceTracker.close();
+
+		Map<Bundle, List<Bundle>> bundleMap =
+			lpkgDeployer.getDeployedLPKGBundles();
+
+		List<Bundle> lpkgBundles = new ArrayList<>(bundleMap.keySet());
+
+		List<Bundle> bundles = new ArrayList<>();
+
+		for (List<Bundle> bundleList : bundleMap.values()) {
+			bundles.addAll(bundleList);
+		}
+
+		for (File file : lpkgFiles) {
+			Bundle lpkgBundle = bundleContext.getBundle(
+				file.getCanonicalPath());
+
+			Assert.assertNotNull(lpkgBundle);
+			Assert.assertTrue(lpkgBundles.contains(lpkgBundle));
+
+			ZipFile zipFile = new ZipFile(file);
+
+			Enumeration<? extends ZipEntry> zipEntries = zipFile.entries();
+
+			while (zipEntries.hasMoreElements()) {
+				ZipEntry zipEntry = zipEntries.nextElement();
+
+				String name = zipEntry.getName();
+
+				if (name.endsWith(".jar") || name.endsWith("*.war")) {
+					Bundle bundle = bundleContext.getBundle(
+						StringPool.SLASH + name);
+
+					Assert.assertNotNull(bundle);
+					Assert.assertTrue(bundles.contains(bundle));
+				}
+			}
+		}
+	}
+
+}
