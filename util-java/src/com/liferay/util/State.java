cde3f22611701350de2972307066142f3658660c
Brian Chan <brian.chan@liferay.com>
2016-05-07 11:21:35 -0700

    LPS-65546 Add deprecation comments since these classes are no longer used by anyone else. This is different from the @see comments for com.liferay.util.mail.* since those classes are still used inside portal-impl.

--- util-java/src/com/liferay/util/State.java
+++ util-java/src/com/liferay/util/State.java
@@ -19,8 +19,10 @@ import com.liferay.portal.kernel.util.HashCodeFactoryUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 
 /**
- * @author Brian Wing Shun Chan
+ * @author     Brian Wing Shun Chan
+ * @deprecated As of 7.0.0, replaced by {@link com.liferay.shopping.util.State)}
  */
+@Deprecated
 public class State {
 
 	public State(String id, String name) {
