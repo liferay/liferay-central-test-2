24dfdc24a27d2dbd7777028615111cbf4db7966b
Michael Hashimoto <michael.hashimoto@liferay.com>
2016-05-06 17:00:56 -0700

    Revert "LPS-65636 Temporarily ignoring those two files, Hashi, can you look at these?"
    This reverts commit 45cbc808f80174f4114afe4b3df69945d065dd42.

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/XMLSourceProcessor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/XMLSourceProcessor.java
@@ -331,12 +331,6 @@ public class XMLSourceProcessor extends BaseSourceProcessor {
 			String fileName, String absolutePath, String content)
 		throws Exception {
 
-		if (fileName.endsWith("build-test.xml") ||
-			fileName.endsWith("build-test-tcat.xml")) {
-
-			return;
-		}
-
 		Document document = readXML(content);
 
 		Element rootElement = document.getRootElement();
