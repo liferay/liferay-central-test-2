b5fe20897aee6f1c282184c53147eb2c404fd5bc
Máté Thurzó <mate.thurzo@liferay.com>
2016-05-09 16:16:26 +0200

    LPS-65537 Apply to PortletDataHandlerStatusMessageSenderImpl

--- modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/background/task/PortletDataHandlerStatusMessageSenderImpl.java
+++ modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/background/task/PortletDataHandlerStatusMessageSenderImpl.java
@@ -19,11 +19,11 @@ import com.liferay.exportimport.kernel.lar.PortletDataHandler;
 import com.liferay.exportimport.kernel.lar.PortletDataHandlerStatusMessageSender;
 import com.liferay.exportimport.kernel.lar.StagedModelDataHandler;
 import com.liferay.exportimport.kernel.lar.StagedModelDataHandlerRegistryUtil;
+import com.liferay.exportimport.portlet.data.handler.provider.PortletDataHandlerProvider;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskConstants;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskStatusMessageSender;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskThreadLocal;
 import com.liferay.portal.kernel.messaging.Message;
-import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.model.StagedModel;
 import com.liferay.portal.kernel.service.PortletLocalService;
 import com.liferay.portal.kernel.util.LongWrapper;
@@ -65,23 +65,26 @@ public class PortletDataHandlerStatusMessageSenderImpl
 
 		message.put("portletId", portletId);
 
-		Portlet portlet = _portletLocalService.getPortletById(portletId);
+		PortletDataHandler portletDataHandler =
+			_portletDataHandlerProvider.provide(portletId);
 
-		if (portlet != null) {
-			PortletDataHandler portletDataHandler =
-				portlet.getPortletDataHandlerInstance();
+		if (portletDataHandler == null) {
+			_backgroundTaskStatusMessageSender.sendBackgroundTaskStatusMessage(
+				message);
 
-			long portletModelAdditionCountersTotal =
-				portletDataHandler.getExportModelCount(manifestSummary);
-
-			if (portletModelAdditionCountersTotal < 0) {
-				portletModelAdditionCountersTotal = 0;
-			}
-
-			message.put(
-				"portletModelAdditionCountersTotal",
-				portletModelAdditionCountersTotal);
+			return;
 		}
+
+		long portletModelAdditionCountersTotal =
+			portletDataHandler.getExportModelCount(manifestSummary);
+
+		if (portletModelAdditionCountersTotal < 0) {
+			portletModelAdditionCountersTotal = 0;
+		}
+
+		message.put(
+			"portletModelAdditionCountersTotal",
+			portletModelAdditionCountersTotal);
 
 		_backgroundTaskStatusMessageSender.sendBackgroundTaskStatusMessage(
 			message);
@@ -152,6 +155,9 @@ public class PortletDataHandlerStatusMessageSenderImpl
 	@Reference
 	private BackgroundTaskStatusMessageSender
 		_backgroundTaskStatusMessageSender;
+
+	@Reference
+	private PortletDataHandlerProvider _portletDataHandlerProvider;
 
 	@Reference
 	private PortletLocalService _portletLocalService;
