69a50680b7bdb40e7c948548feddb625cc78d337
Shinn Lok <shinn.lok@liferay.com>
2016-05-09 12:19:33 -0700

    SYNC-1600 Include sync device services

--- modules/apps/sync/sync-security/src/main/java/com/liferay/sync/security/auth/verifier/SyncAuthVerifier.java
+++ modules/apps/sync/sync-security/src/main/java/com/liferay/sync/security/auth/verifier/SyncAuthVerifier.java
@@ -64,7 +64,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	immediate = true,
 	property = {
-		"auth.verifier.SyncAuthVerifier.urls.includes=/api/jsonws/sync-web.syncdlobject/*"
+		"auth.verifier.SyncAuthVerifier.urls.includes=/api/jsonws/sync-web.syncdevice/*,/api/jsonws/sync-web.syncdlobject/*"
 	}
 )
 public class SyncAuthVerifier implements AuthVerifier {
