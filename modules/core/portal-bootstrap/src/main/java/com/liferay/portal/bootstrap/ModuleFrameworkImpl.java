cf92aece18d6a63f5da1fcc9ec7902ab1f2f61f5
Miguel Ángel Pastor Olivar <miguelinlas3@gmail.com>
2016-05-10 23:08:04 +0200

    LPS-65739 If we try to start a module whose dependendencies are not resolved, it will fail, and this will prevent the portal to start. This is something we don't won't to happen when we are installing the 'dynamic' bundles

--- modules/core/portal-bootstrap/src/main/java/com/liferay/portal/bootstrap/ModuleFrameworkImpl.java
+++ modules/core/portal-bootstrap/src/main/java/com/liferay/portal/bootstrap/ModuleFrameworkImpl.java
@@ -1168,7 +1168,14 @@ public class ModuleFrameworkImpl implements ModuleFramework {
 			if (bundleStartLevel.getStartLevel() ==
 					PropsValues.MODULE_FRAMEWORK_DYNAMIC_INSTALL_START_LEVEL) {
 
-				bundle.start();
+				try {
+					bundle.start();
+				}
+				catch (BundleException be) {
+					_log.error(
+						"Unable to start bundle " + bundle.getSymbolicName(),
+						be);
+				}
 			}
 		}
 	}
