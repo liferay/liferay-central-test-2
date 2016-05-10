0395cfd3b026faf2940a2bd66c530d8b1294e74b
Hugo Huijser <hugo.huijser@liferay.com>
2016-05-10 10:35:41 +0800

    LPS-65716 Make maximum allowed line length for *.java configurable

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/JavaSourceProcessor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/JavaSourceProcessor.java
@@ -768,8 +768,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 
 		newContent = formatJSONObject(newContent);
 
-		newContent = formatStringBundler(
-			fileName, newContent, _MAX_LINE_LENGTH);
+		newContent = formatStringBundler(fileName, newContent, _maxLineLength);
 
 		newContent = StringUtil.replace(
 			newContent, StringPool.TAB + "for (;;) {",
@@ -1258,7 +1257,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 					matcher.group(1) + StringUtil.trimLeading(matcher.group(2)) +
 						matcher.group(3);
 
-				if (getLineLength(singleLine) <= _MAX_LINE_LENGTH) {
+				if (getLineLength(singleLine) <= _maxLineLength) {
 					content = StringUtil.replace(
 						content, matcher.group(), "\n" + singleLine);
 
@@ -1595,7 +1594,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 				matcher.group(3) + matcher.group(2) + matcher.group(4) +
 					matcher.group(5);
 
-			if (getLineLength(newLine) <= _MAX_LINE_LENGTH) {
+			if (getLineLength(newLine) <= _maxLineLength) {
 				return StringUtil.replace(
 					content, matcher.group(),
 					matcher.group(1) + "\n" + newLine + "\n");
@@ -1960,7 +1959,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 				 line.endsWith(") {")) &&
 				(previousLine.endsWith("|") || previousLine.endsWith("&&")) &&
 				((previousLineLength + trimmedLine.length()) <
-					_MAX_LINE_LENGTH) &&
+					_maxLineLength) &&
 				(lineLevel <= 0) && (previousLineLevel >= 0) &&
 				previousLineIsStartCriteria) {
 
@@ -2578,7 +2577,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 					else if (fileName.endsWith("Table.java") &&
 							 line.contains(" index IX_")) {
 					}
-					else if (lineLength > _MAX_LINE_LENGTH) {
+					else if (lineLength > _maxLineLength) {
 						if (!isExcludedPath(
 								_lineLengthExcludes, absolutePath, lineCount) &&
 							!isAnnotationParameter(content, trimmedLine)) {
@@ -2643,7 +2642,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 
 								if ((x != -1) &&
 									((getLineLength(previousLine) + x) <
-										_MAX_LINE_LENGTH) &&
+										_maxLineLength) &&
 									(trimmedLine.endsWith(
 										StringPool.OPEN_PARENTHESIS) ||
 									 (trimmedLine.charAt(x + 1) !=
@@ -3035,7 +3034,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 				replacement, new String[] {",\n", "\n"},
 				new String[] {StringPool.COMMA_AND_SPACE, StringPool.BLANK});
 
-			if (getLineLength(replacement) <= _MAX_LINE_LENGTH) {
+			if (getLineLength(replacement) <= _maxLineLength) {
 				return getCombinedLinesContent(
 					StringUtil.replace(content, match, replacement), pattern);
 			}
@@ -3109,7 +3108,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 				line, linePart, StringPool.BLANK);
 		}
 		else {
-			if (((linePart.length() + lineLength) <= _MAX_LINE_LENGTH) &&
+			if (((linePart.length() + lineLength) <= _maxLineLength) &&
 				(line.endsWith(StringPool.OPEN_CURLY_BRACE) ||
 				 line.endsWith(StringPool.SEMICOLON))) {
 
@@ -3205,7 +3204,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 
 		int previousLineLength = getLineLength(previousLine);
 
-		if ((trimmedLine.length() + previousLineLength) < _MAX_LINE_LENGTH) {
+		if ((trimmedLine.length() + previousLineLength) < _maxLineLength) {
 			if (trimmedPreviousLine.startsWith("for ") &&
 				previousLine.endsWith(StringPool.COLON) &&
 				line.endsWith(StringPool.OPEN_CURLY_BRACE)) {
@@ -3318,7 +3317,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 			}
 		}
 
-		if (((trimmedLine.length() + previousLineLength) <= _MAX_LINE_LENGTH) &&
+		if (((trimmedLine.length() + previousLineLength) <= _maxLineLength) &&
 			(previousLine.endsWith(StringPool.OPEN_BRACKET) ||
 			 previousLine.endsWith(StringPool.OPEN_PARENTHESIS) ||
 			 previousLine.endsWith(StringPool.PERIOD)) &&
@@ -3387,7 +3386,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 					if ((previousChar != CharPool.CLOSE_PARENTHESIS) &&
 						(previousChar != CharPool.OPEN_PARENTHESIS) &&
 						(previousChar != CharPool.SPACE) &&
-						(previousLineLength + 1 + x) < _MAX_LINE_LENGTH) {
+						(previousLineLength + 1 + x) < _maxLineLength) {
 
 						String linePart = trimmedLine.substring(0, x + 1);
 
@@ -3420,7 +3419,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 				x = trimmedLine.indexOf(" +", x + 1);
 
 				if ((x == -1) ||
-					(previousLineLength + 3 + x) > _MAX_LINE_LENGTH) {
+					(previousLineLength + 3 + x) > _maxLineLength) {
 
 					break;
 				}
@@ -3461,7 +3460,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 				int x = trimmedLine.indexOf(CharPool.COMMA);
 
 				if (x != -1) {
-					while ((previousLineLength + 1 + x) < _MAX_LINE_LENGTH) {
+					while ((previousLineLength + 1 + x) < _maxLineLength) {
 						String linePart = trimmedLine.substring(0, x + 1);
 
 						if (!ToolsUtil.isInsideQuotes(trimmedLine, x) &&
@@ -3494,7 +3493,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 					}
 				}
 				else if ((trimmedLine.length() + previousLineLength) <
-							_MAX_LINE_LENGTH) {
+							_maxLineLength) {
 
 					if (!trimmedLine.startsWith("new ") ||
 						!line.endsWith(StringPool.OPEN_CURLY_BRACE)) {
@@ -3522,7 +3521,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 			int x = trimmedLine.indexOf(" = ");
 
 			if ((x != -1) && !ToolsUtil.isInsideQuotes(trimmedLine, x) &&
-				((previousLineLength + 2 + x) < _MAX_LINE_LENGTH)) {
+				((previousLineLength + 2 + x) < _maxLineLength)) {
 
 				String linePart = trimmedLine.substring(0, x + 3);
 
@@ -3532,7 +3531,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 			}
 			else if (trimmedLine.endsWith(" =") &&
 					 ((trimmedLine.length() + previousLineLength) <
-						 _MAX_LINE_LENGTH)) {
+						 _maxLineLength)) {
 
 				for (int i = 0;; i++) {
 					String nextLine = getLine(content, lineCount + i + 1);
@@ -3572,7 +3571,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 			}
 		}
 
-		if ((trimmedLine.length() + previousLineLength) > _MAX_LINE_LENGTH) {
+		if ((trimmedLine.length() + previousLineLength) > _maxLineLength) {
 			return null;
 		}
 
@@ -3618,7 +3617,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 
 		outerWhile:
 		while (true) {
-			if (getLineLength(classSingleLine) <= _MAX_LINE_LENGTH) {
+			if (getLineLength(classSingleLine) <= _maxLineLength) {
 				lines.add(classSingleLine);
 
 				break;
@@ -3646,7 +3645,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 				String linePart = newLine.substring(0, x);
 
 				if ((getLevel(linePart, "<", ">") == 0) &&
-					(getLineLength(linePart) <= _MAX_LINE_LENGTH)) {
+					(getLineLength(linePart) <= _maxLineLength)) {
 
 					if (lines.isEmpty()) {
 						newIndent = newIndent + StringPool.TAB;
@@ -3656,7 +3655,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 
 					newLine = newIndent + newLine.substring(x + 1);
 
-					if (getLineLength(newLine) <= _MAX_LINE_LENGTH) {
+					if (getLineLength(newLine) <= _maxLineLength) {
 						lines.add(newLine);
 
 						break outerWhile;
@@ -3682,7 +3681,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 				String linePart = newLine.substring(0, x + 1);
 
 				if ((getLevel(linePart, "<", ">") == 0) &&
-					(getLineLength(linePart) <= _MAX_LINE_LENGTH)) {
+					(getLineLength(linePart) <= _maxLineLength)) {
 
 					lines.add(linePart);
 
@@ -3695,7 +3694,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 
 					newLine = newIndent + newLine.substring(x + 2);
 
-					if (getLineLength(newLine) <= _MAX_LINE_LENGTH) {
+					if (getLineLength(newLine) <= _maxLineLength) {
 						lines.add(newLine);
 
 						break outerWhile;
@@ -3721,8 +3720,8 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 	}
 
 	protected int getIfClauseLineBreakPos(String line) {
-		int x = line.lastIndexOf(" || ", _MAX_LINE_LENGTH - 3);
-		int y = line.lastIndexOf(" && ", _MAX_LINE_LENGTH - 3);
+		int x = line.lastIndexOf(" || ", _maxLineLength - 3);
+		int y = line.lastIndexOf(" && ", _maxLineLength - 3);
 
 		int z = Math.max(x, y);
 
@@ -3754,7 +3753,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 			return x + 1;
 		}
 
-		for (x = _MAX_LINE_LENGTH + 1;;) {
+		for (x = _maxLineLength + 1;;) {
 			x = line.lastIndexOf(StringPool.COMMA_AND_SPACE, x - 1);
 
 			if (x == -1) {
@@ -4220,7 +4219,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 					String secondLine =
 						indent + StringPool.TAB + line.substring(x + 1);
 
-					if (getLineLength(secondLine) <= _MAX_LINE_LENGTH) {
+					if (getLineLength(secondLine) <= _maxLineLength) {
 						return StringUtil.replace(
 							content, "\n" + line + "\n",
 							"\n" + firstLine + "\n" + secondLine + "\n");
@@ -4327,6 +4326,9 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 
 	@Override
 	protected void preFormat() {
+		_maxLineLength = GetterUtil.getInteger(
+			System.getProperty("source.formatter.max.line.length"));
+
 		_addMissingDeprecationReleaseVersion = GetterUtil.getBoolean(
 			getProperty("add.missing.deprecation.release.version"));
 		_allowUseServiceUtilInServiceImpl = GetterUtil.getBoolean(
@@ -4427,8 +4429,6 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 
 	private static final String[] _INCLUDES = new String[] {"**/*.java"};
 
-	private static final int _MAX_LINE_LENGTH = 80;
-
 	private static final Pattern _annotationPattern = Pattern.compile(
 		"(\t*)@(.+)\\(\n([\\s\\S]*?)\\)\n");
 
@@ -4503,6 +4503,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 			"\t*(.+)\\.class\\)");
 	private final Pattern _lowerCaseNumberOrPeriodPattern = Pattern.compile(
 		"[a-z0-9.]");
+	private int _maxLineLength;
 	private final Pattern _missingEmptyLinePattern1 = Pattern.compile(
 		"(\t| = |return )new .*\\(.*\\) \\{\n\t+[^{\t]");
 	private final Pattern _missingEmptyLinePattern2 = Pattern.compile(
