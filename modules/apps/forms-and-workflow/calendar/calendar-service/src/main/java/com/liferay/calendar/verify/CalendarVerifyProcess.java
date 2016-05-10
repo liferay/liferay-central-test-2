3f35eb4454b347caace2d27a82777a72d620046d
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2016-05-10 10:45:47 -0300

    LPS-65075 Auxiliary method to retrieve/crate calendar resources (similar to the ones from CalendarResourceUtil).

--- modules/apps/forms-and-workflow/calendar/calendar-service/src/main/java/com/liferay/calendar/verify/CalendarVerifyProcess.java
+++ modules/apps/forms-and-workflow/calendar/calendar-service/src/main/java/com/liferay/calendar/verify/CalendarVerifyProcess.java
@@ -15,12 +15,30 @@
 package com.liferay.calendar.verify;
 
 import com.liferay.calendar.model.CalendarBooking;
+import com.liferay.calendar.model.CalendarResource;
 import com.liferay.calendar.notification.NotificationType;
 import com.liferay.calendar.service.CalendarBookingLocalService;
+import com.liferay.calendar.service.CalendarResourceLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.RoleConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.verify.VerifyProcess;
 
 import java.sql.Timestamp;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -84,6 +102,95 @@ public class CalendarVerifyProcess extends VerifyProcess {
 		verifyCalEvent();
 	}
 
+	protected CalendarResource getCalendarResource(long companyId, long groupId)
+		throws PortalException {
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setCompanyId(companyId);
+
+		long userId = _userLocalService.getDefaultUserId(companyId);
+
+		serviceContext.setUserId(userId);
+
+		Group group = _groupLocalService.getGroup(groupId);
+
+		if (group.isUser()) {
+			userId = group.getClassPK();
+
+			CalendarResource calendarResource =
+				_calendarResourceLocalService.fetchCalendarResource(
+					_classNameLocalService.getClassNameId(User.class), userId);
+
+			if (calendarResource != null) {
+				return calendarResource;
+			}
+
+			User user = _userLocalService.getUser(userId);
+
+			Group userGroup = null;
+
+			String userName = user.getFullName();
+
+			if (user.isDefaultUser()) {
+				userGroup = _groupLocalService.getGroup(
+					serviceContext.getCompanyId(), GroupConstants.GUEST);
+
+				userName = GroupConstants.GUEST;
+			}
+			else {
+				userGroup = _groupLocalService.getUserGroup(
+					serviceContext.getCompanyId(), userId);
+			}
+
+			Map<Locale, String> nameMap = new HashMap<>();
+
+			nameMap.put(LocaleUtil.getDefault(), userName);
+
+			Map<Locale, String> descriptionMap = new HashMap<>();
+
+			return _calendarResourceLocalService.addCalendarResource(
+				userId, userGroup.getGroupId(),
+				_classNameLocalService.getClassNameId(User.class), userId, null,
+				null, nameMap, descriptionMap, true, serviceContext);
+		}
+		else {
+			CalendarResource calendarResource =
+				_calendarResourceLocalService.fetchCalendarResource(
+					_classNameLocalService.getClassNameId(Group.class),
+					groupId);
+
+			if (calendarResource != null) {
+				return calendarResource;
+			}
+
+			userId = group.getCreatorUserId();
+
+			User user = _userLocalService.fetchUserById(userId);
+
+			if ((user == null) || user.isDefaultUser()) {
+				Role role = _roleLocalService.getRole(
+					group.getCompanyId(), RoleConstants.ADMINISTRATOR);
+
+				long[] userIds = _userLocalService.getRoleUserIds(
+					role.getRoleId());
+
+				userId = userIds[0];
+			}
+
+			Map<Locale, String> nameMap = new HashMap<>();
+
+			nameMap.put(LocaleUtil.getDefault(), group.getDescriptiveName());
+
+			Map<Locale, String> descriptionMap = new HashMap<>();
+
+			return _calendarResourceLocalService.addCalendarResource(
+				userId, groupId,
+				_classNameLocalService.getClassNameId(Group.class), groupId,
+				null, null, nameMap, descriptionMap, true, serviceContext);
+		}
+	}
+
 	@Reference(unbind = "-")
 	protected void setCalendarBookingLocalService(
 		CalendarBookingLocalService calendarBookingLocalService) {
@@ -91,9 +198,43 @@ public class CalendarVerifyProcess extends VerifyProcess {
 		_calendarBookingLocalService = calendarBookingLocalService;
 	}
 
+	@Reference(unbind = "-")
+	protected void setCalendarResourceLocalService(
+		CalendarResourceLocalService calendarResourceLocalService) {
+
+		_calendarResourceLocalService = calendarResourceLocalService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setClassNameLocalService(
+		ClassNameLocalService classNameLocalService) {
+
+		_classNameLocalService = classNameLocalService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setGroupLocalService(GroupLocalService groupLocalService) {
+		_groupLocalService = groupLocalService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setRoleLocalService(RoleLocalService roleLocalService) {
+		_roleLocalService = roleLocalService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setUserLocalService(UserLocalService userLocalService) {
+		_userLocalService = userLocalService;
+	}
+
 	protected void verifyCalEvent() throws Exception {
 	}
 
 	private CalendarBookingLocalService _calendarBookingLocalService;
+	private CalendarResourceLocalService _calendarResourceLocalService;
+	private ClassNameLocalService _classNameLocalService;
+	private GroupLocalService _groupLocalService;
+	private RoleLocalService _roleLocalService;
+	private UserLocalService _userLocalService;
 
 }
