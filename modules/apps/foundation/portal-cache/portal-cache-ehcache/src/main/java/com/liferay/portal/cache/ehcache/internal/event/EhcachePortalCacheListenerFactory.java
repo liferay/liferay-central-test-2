79f44a4517c4bd40866d52823bdc9d857806b60b
shuyangzhou <shuyang.zhou@liferay.com>
2016-05-10 10:04:49 -0700

    LPS-65125 Revert changes to EhcachePortalCacheListenerFactory. This is wrong, the upper logic relies on create() returning null to indicate no listener in order to skip registeration. With this EhcachePortalCacheReplicator, the return value will never be null and will always be registered. However the inner wrapped listener could be null, at runtime we will get npe on such listeners. The correct fix should be on portal-cache-single, nothing needs to be changed, as it always returns null listener. On portal-cache-multiple, we should mark ClusterLinkPortalCacheReplicator with this marker interface.

--- modules/apps/foundation/portal-cache/portal-cache-ehcache/src/main/java/com/liferay/portal/cache/ehcache/internal/event/EhcachePortalCacheListenerFactory.java
+++ modules/apps/foundation/portal-cache/portal-cache-ehcache/src/main/java/com/liferay/portal/cache/ehcache/internal/event/EhcachePortalCacheListenerFactory.java
@@ -19,8 +19,6 @@ import com.liferay.portal.cache.PortalCacheReplicator;
 import com.liferay.portal.cache.PortalCacheReplicatorFactory;
 import com.liferay.portal.cache.ehcache.EhcacheConstants;
 import com.liferay.portal.cache.ehcache.event.EhcachePortalCacheListenerAdapter;
-import com.liferay.portal.kernel.cache.PortalCache;
-import com.liferay.portal.kernel.cache.PortalCacheException;
 import com.liferay.portal.kernel.cache.PortalCacheListener;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.GetterUtil;
@@ -53,8 +51,8 @@ public class EhcachePortalCacheListenerFactory
 
 		if (replicator) {
 			return
-				(PortalCacheListener<K, V>)new EhcachePortalCacheReplicator<>(
-					_portalCacheReplicatorFactory.create(properties));
+				(PortalCacheListener<K, V>)_portalCacheReplicatorFactory.create(
+					properties);
 		}
 
 		String className = properties.getProperty(
@@ -96,77 +94,5 @@ public class EhcachePortalCacheListenerFactory
 	}
 
 	private PortalCacheReplicatorFactory _portalCacheReplicatorFactory;
-
-	private class EhcachePortalCacheReplicator
-		<K extends Serializable, V extends Serializable>
-			implements PortalCacheReplicator<K, V>,
-				ConfigurableEhcachePortalCacheListener {
-
-		@Override
-		public void dispose() {
-			_portalCacheReplicator.dispose();
-		}
-
-		@Override
-		public void notifyEntryEvicted(
-				PortalCache<K, V> portalCache, K key, V value, int timeToLive)
-			throws PortalCacheException {
-
-			_portalCacheReplicator.notifyEntryEvicted(
-				portalCache, key, value, timeToLive);
-		}
-
-		@Override
-		public void notifyEntryExpired(
-				PortalCache<K, V> portalCache, K key, V value, int timeToLive)
-			throws PortalCacheException {
-
-			_portalCacheReplicator.notifyEntryExpired(
-				portalCache, key, value, timeToLive);
-		}
-
-		@Override
-		public void notifyEntryPut(
-				PortalCache<K, V> portalCache, K key, V value, int timeToLive)
-			throws PortalCacheException {
-
-			_portalCacheReplicator.notifyEntryPut(
-				portalCache, key, value, timeToLive);
-		}
-
-		@Override
-		public void notifyEntryRemoved(
-				PortalCache<K, V> portalCache, K key, V value, int timeToLive)
-			throws PortalCacheException {
-
-			_portalCacheReplicator.notifyEntryRemoved(
-				portalCache, key, value, timeToLive);
-		}
-
-		@Override
-		public void notifyEntryUpdated(
-				PortalCache<K, V> portalCache, K key, V value, int timeToLive)
-			throws PortalCacheException {
-
-			_portalCacheReplicator.notifyEntryUpdated(
-				portalCache, key, value, timeToLive);
-		}
-
-		@Override
-		public void notifyRemoveAll(PortalCache<K, V> portalCache)
-			throws PortalCacheException {
-
-			_portalCacheReplicator.notifyRemoveAll(portalCache);
-		}
-
-		private EhcachePortalCacheReplicator(
-			PortalCacheReplicator<K, V> portalCacheReplicator) {
-
-			_portalCacheReplicator = portalCacheReplicator;
-		}
-
-		private final PortalCacheReplicator<K, V> _portalCacheReplicator;
-
-	}
 
 }
