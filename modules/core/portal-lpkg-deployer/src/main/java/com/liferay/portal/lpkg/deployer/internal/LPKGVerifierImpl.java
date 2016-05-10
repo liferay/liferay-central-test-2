1fa077b774b2febfa0f3a844c4f60d167ee3aa50
Raymond Auge <raymond.auge@liferay.com>
2016-05-10 14:31:32 -0400

    LPS-65693 integrate it with the deployer (temporary)

--- modules/core/portal-lpkg-deployer/src/main/java/com/liferay/portal/lpkg/deployer/internal/LPKGVerifierImpl.java
+++ modules/core/portal-lpkg-deployer/src/main/java/com/liferay/portal/lpkg/deployer/internal/LPKGVerifierImpl.java
@@ -14,12 +14,25 @@
 
 package com.liferay.portal.lpkg.deployer.internal;
 
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.lpkg.deployer.LPKGVerifier;
 import com.liferay.portal.lpkg.deployer.LPKGVerifyException;
+import com.liferay.portal.target.platform.indexer.Indexer;
+import com.liferay.portal.target.platform.indexer.IndexerFactory;
+import com.liferay.portal.target.platform.indexer.ValidatorFactory;
+import com.liferay.portal.util.PropsValues;
 
 import java.io.File;
 import java.io.IOException;
+
+import java.net.URI;
+
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,6 +45,7 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.Version;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Shuyang Zhou
@@ -46,6 +60,93 @@ public class LPKGVerifierImpl implements LPKGVerifier {
 
 	@Override
 	public List<Bundle> verify(File lpkgFile) {
+		Path tmpDir = null;
+
+		try {
+			tmpDir = Files.createTempDirectory(null);
+
+			Indexer indexer = _indexerFactory.create(lpkgFile);
+
+			File tempIndexFile = indexer.index(tmpDir.toFile());
+
+			List<URI> uris = new ArrayList<>();
+
+			uris.add(tempIndexFile.toURI());
+
+			com.liferay.portal.target.platform.indexer.Validator validator =
+				_validatorFactory.create();
+
+			List<String> errors = validator.validate(uris);
+
+			if (!errors.isEmpty()) {
+				StringBundler sb = new StringBundler((errors.size() * 4) + 2);
+
+				sb.append("LPKG validation failed with {");
+
+				for (String error : errors) {
+					sb.append("[");
+					sb.append(error);
+					sb.append("]");
+					sb.append(",");
+				}
+
+				sb.setIndex(sb.index() - 1);
+
+				sb.append("}");
+
+				throw new LPKGVerifyException(sb.toString());
+			}
+
+			File targetPlatformDir = new File(
+				PropsValues.MODULE_FRAMEWORK_BASE_DIR, Indexer.TARGET_PLATFORM);
+
+			File indexFile = new File(
+				targetPlatformDir, tempIndexFile.getName());
+
+			Files.copy(tempIndexFile.toPath(), indexFile.toPath());
+		}
+		catch (Exception e) {
+			if (e instanceof LPKGVerifyException) {
+				throw (LPKGVerifyException)e;
+			}
+
+			throw new LPKGVerifyException(e);
+		}
+		finally {
+			if (tmpDir != null) {
+				try {
+					Files.walkFileTree(
+						tmpDir,
+						new SimpleFileVisitor<Path>() {
+
+							@Override
+							public FileVisitResult postVisitDirectory(
+									Path dir, IOException exc)
+								throws IOException {
+
+								Files.delete(dir);
+
+								return FileVisitResult.CONTINUE;
+							}
+
+							@Override
+							public FileVisitResult visitFile(
+									Path file, BasicFileAttributes attrs)
+								throws IOException {
+
+								Files.delete(file);
+
+								return FileVisitResult.CONTINUE;
+							}
+
+						});
+				}
+				catch (IOException ioe) {
+					throw new LPKGVerifyException(ioe);
+				}
+			}
+		}
+
 		try (ZipFile zipFile = new ZipFile(lpkgFile)) {
 			ZipEntry zipEntry = zipFile.getEntry(
 				"liferay-marketplace.properties");
@@ -119,5 +220,11 @@ public class LPKGVerifierImpl implements LPKGVerifier {
 	}
 
 	private BundleContext _bundleContext;
+
+	@Reference
+	private IndexerFactory _indexerFactory;
+
+	@Reference
+	private ValidatorFactory _validatorFactory;
 
 }
