af1271b99075a2b7a685c2240b1ba70e2d9f97db
Roberto Díaz <roberto.diaz@liferay.com>
2016-05-11 17:50:13 +0200

    LPS-65767 Create Configuration interfaces skeletons for KnowledgeBase

---
+++ modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/configuration/KBSectionPortletInstanceConfiguration.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledge.base.web.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Roberto Díaz
+ */
+@ExtendedObjectClassDefinition(
+	category = "collaboration",
+	scope = ExtendedObjectClassDefinition.Scope.PORTLET_INSTANCE
+)
+@Meta.OCD(
+	id = "com.liferay.knowledge.base.web.configuration.KBSectionPortletInstanceConfiguration",
+	localization = "content/Language",
+	name = "knowledge.base.section.portlet.instance.configuration.name"
+)
+public interface KBSectionPortletInstanceConfiguration {
+}
