a830bc8359fbd5b3535b1ab2901d29206fa58395
Julio Camarero <julio.camarero@liferay.com>
2016-05-11 17:52:07 +0200

    LPS-65535 reorder entries in the control menu

--- modules/apps/web-experience/product-navigation/product-navigation-control-menu-web/src/main/java/com/liferay/product/navigation/control/menu/web/AddContentProductNavigationControlMenuEntry.java
+++ modules/apps/web-experience/product-navigation/product-navigation-control-menu-web/src/main/java/com/liferay/product/navigation/control/menu/web/AddContentProductNavigationControlMenuEntry.java
@@ -39,7 +39,7 @@ import org.osgi.service.component.annotations.Reference;
 	immediate = true,
 	property = {
 		"product.navigation.control.menu.category.key=" + ProductNavigationControlMenuCategoryKeys.USER,
-		"product.navigation.control.menu.entry.order:Integer=100"
+		"product.navigation.control.menu.entry.order:Integer=200"
 	},
 	service = ProductNavigationControlMenuEntry.class
 )
