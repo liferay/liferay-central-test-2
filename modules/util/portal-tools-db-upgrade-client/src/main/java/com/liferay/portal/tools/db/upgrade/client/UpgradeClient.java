e86ac695ee38793dfc610a08e1197e58f4264de7
Victor Ware <victor.ware@liferay.com>
2016-05-12 15:10:46 -0700

    LPS-65833 Add missing path separator

--- modules/util/portal-tools-db-upgrade-client/src/main/java/com/liferay/portal/tools/db/upgrade/client/UpgradeClient.java
+++ modules/util/portal-tools-db-upgrade-client/src/main/java/com/liferay/portal/tools/db/upgrade/client/UpgradeClient.java
@@ -311,6 +311,7 @@ public class UpgradeClient {
 		File portalClassesDir = _appServer.getPortalClassesDir();
 
 		sb.append(portalClassesDir.getCanonicalPath());
+		sb.append(File.pathSeparator);
 
 		_appendClassPath(sb, _appServer.getPortalLibDir());
 
