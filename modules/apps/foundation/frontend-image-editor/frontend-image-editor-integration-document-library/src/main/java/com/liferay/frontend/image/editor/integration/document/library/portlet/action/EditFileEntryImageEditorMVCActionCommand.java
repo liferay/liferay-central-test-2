4348fe8c5557aed077d6c52ea4876c456126060d
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2016-05-12 13:16:33 +0200

    LPS-65717 Add toolbar item to edit the image with Image Editor

--- modules/apps/foundation/frontend-image-editor/frontend-image-editor-integration-document-library/src/main/java/com/liferay/frontend/image/editor/integration/document/library/portlet/action/EditFileEntryImageEditorMVCActionCommand.java
+++ modules/apps/foundation/frontend-image-editor/frontend-image-editor-integration-document-library/src/main/java/com/liferay/frontend/image/editor/integration/document/library/portlet/action/EditFileEntryImageEditorMVCActionCommand.java
@@ -60,6 +60,7 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(
 	property = {
+		"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY,
 		"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY_ADMIN,
 		"mvc.command.name=/document_library/edit_file_entry_with_image_editor"
 	},
