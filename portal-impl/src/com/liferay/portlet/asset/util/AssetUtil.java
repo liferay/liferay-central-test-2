9769e71922f2a8f99de96939284a293553108378
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2016-05-12 10:20:58 +0200

    LPS-65772 pick current language to translate correctly breadcrumbs.

--- portal-impl/src/com/liferay/portlet/asset/util/AssetUtil.java
+++ portal-impl/src/com/liferay/portlet/asset/util/AssetUtil.java
@@ -137,10 +137,15 @@ public class AssetUtil {
 
 		Collections.reverse(ancestorCategories);
 
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
 		for (AssetCategory ancestorCategory : ancestorCategories) {
 			portletURL.setParameter(
 				"categoryId", String.valueOf(ancestorCategory.getCategoryId()));
 
+			ancestorCategory.setTitleCurrentLanguageId(
+				themeDisplay.getLanguageId());
 			PortalUtil.addPortletBreadcrumbEntry(
 				request, ancestorCategory.getTitleCurrentValue(),
 				portletURL.toString());
@@ -148,6 +153,7 @@ public class AssetUtil {
 
 		portletURL.setParameter("categoryId", String.valueOf(assetCategoryId));
 
+		assetCategory.setTitleCurrentLanguageId(themeDisplay.getLanguageId());
 		PortalUtil.addPortletBreadcrumbEntry(
 			request, assetCategory.getTitleCurrentValue(),
 			portletURL.toString());
