0b713d9c6b139747842185e0660d0fbfb6cafbf7
Shinn Lok <shinn.lok@liferay.com>
2016-05-16 10:48:54 -0700

    SYNC-1607 Move model listener

--- modules/apps/sync/sync-service/src/main/java/com/liferay/sync/model/listener/ResourcePermissionModelListener.java
+++ modules/apps/sync/sync-service/src/main/java/com/liferay/sync/model/listener/ResourcePermissionModelListener.java
@@ -18,20 +18,25 @@ import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.document.library.kernel.model.DLFolder;
 import com.liferay.portal.kernel.exception.ModelListenerException;
 import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
 import com.liferay.portal.kernel.model.ResourcePermission;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
-import com.liferay.portal.kernel.service.ResourcePermissionLocalServiceUtil;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.sync.constants.SyncDLObjectConstants;
 import com.liferay.sync.model.SyncDLObject;
-import com.liferay.sync.model.SyncDLObjectConstants;
-import com.liferay.sync.service.SyncDLObjectLocalServiceUtil;
+import com.liferay.sync.service.SyncDLObjectLocalService;
 
 import java.util.Date;
 import java.util.List;
 
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 /**
  * @author Shinn Lok
  */
+@Component(immediate = true, service = ModelListener.class)
 public class ResourcePermissionModelListener
 	extends BaseModelListener<ResourcePermission> {
 
@@ -45,12 +50,12 @@ public class ResourcePermissionModelListener
 			String modelName = resourcePermission.getName();
 
 			if (modelName.equals(DLFileEntry.class.getName())) {
-				syncDLObject = SyncDLObjectLocalServiceUtil.fetchSyncDLObject(
+				syncDLObject = _syncDLObjectLocalService.fetchSyncDLObject(
 					SyncDLObjectConstants.TYPE_FILE,
 					GetterUtil.getLong(resourcePermission.getPrimKey()));
 			}
 			else if (modelName.equals(DLFolder.class.getName())) {
-				syncDLObject = SyncDLObjectLocalServiceUtil.fetchSyncDLObject(
+				syncDLObject = _syncDLObjectLocalService.fetchSyncDLObject(
 					SyncDLObjectConstants.TYPE_FOLDER,
 					GetterUtil.getLong(resourcePermission.getPrimKey()));
 			}
@@ -60,7 +65,7 @@ public class ResourcePermissionModelListener
 			}
 
 			ResourcePermission originalResourcePermission =
-				ResourcePermissionLocalServiceUtil.fetchResourcePermission(
+				_resourcePermissionLocalService.fetchResourcePermission(
 					resourcePermission.getResourcePermissionId());
 
 			if (originalResourcePermission.hasActionId(ActionKeys.VIEW) &&
@@ -69,7 +74,7 @@ public class ResourcePermissionModelListener
 				syncDLObject.setModifiedTime(System.currentTimeMillis());
 				syncDLObject.setLastPermissionChangeDate(new Date());
 
-				SyncDLObjectLocalServiceUtil.updateSyncDLObject(syncDLObject);
+				_syncDLObjectLocalService.updateSyncDLObject(syncDLObject);
 			}
 			else if (!originalResourcePermission.hasActionId(ActionKeys.VIEW) &&
 					 resourcePermission.hasActionId(ActionKeys.VIEW)) {
@@ -82,10 +87,24 @@ public class ResourcePermissionModelListener
 		}
 	}
 
+	@Reference(unbind = "-")
+	protected void setResourcePermissionLocalService(
+		ResourcePermissionLocalService resourcePermissionLocalService) {
+
+		_resourcePermissionLocalService = resourcePermissionLocalService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setSyncDLObjectLocalService(
+		SyncDLObjectLocalService syncDLObjectLocalService) {
+
+		_syncDLObjectLocalService = syncDLObjectLocalService;
+	}
+
 	protected void updateSyncDLObject(SyncDLObject syncDLObject) {
 		syncDLObject.setModifiedTime(System.currentTimeMillis());
 
-		SyncDLObjectLocalServiceUtil.updateSyncDLObject(syncDLObject);
+		_syncDLObjectLocalService.updateSyncDLObject(syncDLObject);
 
 		String type = syncDLObject.getType();
 
@@ -94,12 +113,15 @@ public class ResourcePermissionModelListener
 		}
 
 		List<SyncDLObject> childSyncDLObjects =
-			SyncDLObjectLocalServiceUtil.getSyncDLObjects(
+			_syncDLObjectLocalService.getSyncDLObjects(
 				syncDLObject.getRepositoryId(), syncDLObject.getTypePK());
 
 		for (SyncDLObject childSyncDLObject : childSyncDLObjects) {
 			updateSyncDLObject(childSyncDLObject);
 		}
 	}
+
+	private ResourcePermissionLocalService _resourcePermissionLocalService;
+	private SyncDLObjectLocalService _syncDLObjectLocalService;
 
 }
