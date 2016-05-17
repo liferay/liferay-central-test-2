434db07fc226dc7038d0ec5851a64bc01c17e087
peterfellwock <peter.fellwock@liferay.com>
2016-05-17 12:47:09 -0700

    LPS-65786 Add portlet keys

---
+++ modules/apps/wsrp/wsrp-service/src/main/java/com/liferay/wsrp/constants/WSRPPortletKeys.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wsrp.constants;
+
+/**
+ * @author Peter Fellwock
+ */
+public class WSRPPortletKeys {
+
+	public static final String WSRP_ADMIN =
+		"com_liferay_wsrp_web_portlet_WSRPAdminPortlet";
+
+	public static final String WSRP_CONSUMER =
+		"com_liferay_wsrp_portlet_WSRPConsumerPortlet";
+
+}
