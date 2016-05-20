e5742159f8d5da371d675c6af21ff7fee648689c
Adolfo Pérez <adolfo.perez@liferay.com>
2016-05-20 13:15:53 +0200

    LPS-65946 Alternative path for Directory

--- modules/apps/collaboration/directory/directory-web/src/main/java/com/liferay/directory/web/upgrade/DirectoryWebUpgrade.java
+++ modules/apps/collaboration/directory/directory-web/src/main/java/com/liferay/directory/web/upgrade/DirectoryWebUpgrade.java
@@ -29,11 +29,17 @@ public class DirectoryWebUpgrade implements UpgradeStepRegistrator {
 	@Override
 	public void register(Registry registry) {
 		registry.register(
-			"com.liferay.directory.web", "0.0.0", "1.0.0",
+			"com.liferay.directory.web", "0.0.0", "1.0.1",
 			new DummyUpgradeStep());
 
 		registry.register(
-			"com.liferay.directory.web", "0.0.1", "1.0.0",
+			"com.liferay.directory.web", "0.0.1", "1.0.1",
+			new UpgradePortletId());
+
+		// See LPS-65946
+
+		registry.register(
+			"com.liferay.directory.web", "1.0.0", "1.0.1",
 			new UpgradePortletId());
 	}
 
