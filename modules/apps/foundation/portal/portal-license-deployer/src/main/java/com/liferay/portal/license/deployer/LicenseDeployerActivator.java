d08c158ac2a4d9ea981558432567946664db934e
shuyangzhou <shuyang.zhou@liferay.com>
2016-05-23 11:06:19 -0700

    LPS-66023 Move lpkg bundle license installing logic into portal-license-deployer to solve the startup dependecies problem. This api looks incomplete to me, do we ever want to uninstall a license? For example on lpkg bundle uninstallation?

--- modules/apps/foundation/portal/portal-license-deployer/src/main/java/com/liferay/portal/license/deployer/LicenseDeployerActivator.java
+++ modules/apps/foundation/portal/portal-license-deployer/src/main/java/com/liferay/portal/license/deployer/LicenseDeployerActivator.java
@@ -19,11 +19,13 @@ import com.liferay.portal.license.deployer.installer.LicenseInstaller;
 import org.apache.felix.fileinstall.ArtifactInstaller;
 import org.apache.felix.fileinstall.ArtifactListener;
 
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.util.tracker.BundleTracker;
 
 /**
  * @author Miguel Pastor
@@ -33,8 +35,16 @@ public class LicenseDeployerActivator {
 
 	@Activate
 	protected void activate(BundleContext bundleContext) {
+		ArtifactInstaller artifactInstaller = new LicenseInstaller();
+
 		_artifactListenerServiceRegistration = registerArtifactListener(
-			bundleContext);
+			bundleContext, artifactInstaller);
+
+		_lpkgLicensedBundleTracker = new BundleTracker<>(
+			bundleContext, ~Bundle.UNINSTALLED,
+			new LPKGLicensedBundleTrackerCustomizer(artifactInstaller));
+
+		_lpkgLicensedBundleTracker.open();
 	}
 
 	@Deactivate
@@ -43,16 +53,17 @@ public class LicenseDeployerActivator {
 	}
 
 	protected ServiceRegistration<?> registerArtifactListener(
-		BundleContext bundleContext) {
+		BundleContext bundleContext, ArtifactInstaller artifactInstaller) {
 
 		return bundleContext.registerService(
 			new String[] {
 				ArtifactInstaller.class.getName(),
 				ArtifactListener.class.getName()
 			},
-			new LicenseInstaller(), null);
+			artifactInstaller, null);
 	}
 
 	private ServiceRegistration<?> _artifactListenerServiceRegistration;
+	private BundleTracker<Bundle> _lpkgLicensedBundleTracker;
 
 }
