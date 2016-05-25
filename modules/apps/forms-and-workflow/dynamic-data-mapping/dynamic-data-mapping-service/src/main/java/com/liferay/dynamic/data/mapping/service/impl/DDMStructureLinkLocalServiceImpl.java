d3914bd54df29444873925507c7e71c8c61963cf
Inacio Nery <inacio.nery@liferay.com>
2016-05-25 15:37:27 -0300

    LPS-65648 Add operations countByC_C, getStructureLinks and getStructureLinkStructures to Service

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMStructureLinkLocalServiceImpl.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMStructureLinkLocalServiceImpl.java
@@ -52,6 +52,11 @@ public class DDMStructureLinkLocalServiceImpl
 	}
 
 	@Override
+	public int countByC_C(long classNameId, long classPK) {
+		return ddmStructureLinkPersistence.countByC_C(classNameId, classPK);
+	}
+
+	@Override
 	public void deleteStructureLink(DDMStructureLink structureLink) {
 		ddmStructureLinkPersistence.remove(structureLink);
 	}
@@ -131,6 +136,14 @@ public class DDMStructureLinkLocalServiceImpl
 	}
 
 	@Override
+	public List<DDMStructureLink> getStructureLinks(
+		long classNameId, long classPK, int start, int end) {
+
+		return ddmStructureLinkPersistence.findByC_C(
+			classNameId, classPK, start, end);
+	}
+
+	@Override
 	public List<DDMStructure> getStructureLinkStructures(
 			long classNameId, long classPK)
 		throws PortalException {
@@ -139,6 +152,23 @@ public class DDMStructureLinkLocalServiceImpl
 
 		List<DDMStructureLink> structureLinks = getStructureLinks(
 			classNameId, classPK);
+
+		for (DDMStructureLink structureLink : structureLinks) {
+			structures.add(structureLink.getStructure());
+		}
+
+		return structures;
+	}
+
+	@Override
+	public List<DDMStructure> getStructureLinkStructures(
+			long classNameId, long classPK, int start, int end)
+		throws PortalException {
+
+		List<DDMStructure> structures = new ArrayList<>();
+
+		List<DDMStructureLink> structureLinks = getStructureLinks(
+			classNameId, classPK, start, end);
 
 		for (DDMStructureLink structureLink : structureLinks) {
 			structures.add(structureLink.getStructure());
