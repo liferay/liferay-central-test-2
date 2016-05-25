mikemanyoung/liferay-portal

SYNC-1605 Abstract out logic

modules/apps/sync/sync-engine/src/main/java/com/liferay/sync/engine/documentlibrary/event/BaseEvent.java
@@ -19,12 +19,12 @@ import com.liferay.sync.engine.documentlibrary.handler.Handler;
 import com.liferay.sync.engine.documentlibrary.util.BatchEvent;
 import com.liferay.sync.engine.documentlibrary.util.BatchEventManager;
 import com.liferay.sync.engine.documentlibrary.util.FileEventManager;
+import com.liferay.sync.engine.documentlibrary.util.ServerUtil;
 import com.liferay.sync.engine.model.SyncAccount;
 import com.liferay.sync.engine.model.SyncFile;
 import com.liferay.sync.engine.service.SyncAccountService;
 import com.liferay.sync.engine.session.Session;
 import com.liferay.sync.engine.session.SessionManager;
-import com.liferay.sync.engine.util.ServerInfo;
 
 import java.util.Map;
 
@@ -138,13 +138,7 @@ public abstract class BaseEvent implements Event {
 
 	@Override
 	public String getURLPath() {
-		if (ServerInfo.supportsModuleFramework(_syncAccountId)) {
-			String urlPath = _urlPath.replace("/sync-web/", "/o/sync/");
-
-			return urlPath.replace("/sync-web.", "/sync.");
-		}
-
-		return _urlPath;
+		return ServerUtil.fixUrlPath(_syncAccountId, _urlPath);
 	}
 
 	@Override

modules/apps/sync/sync-engine/src/main/java/com/liferay/sync/engine/documentlibrary/util/ServerUtil.java
@@ -1,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sync.engine.documentlibrary.util;
+
+import com.liferay.sync.engine.util.ServerInfo;
+
+/**
+ * @author Dennis Ju
+ */
+public class ServerUtil {
+
+	public static String fixUrlPath(
+		boolean supportsModuleFramework, String urlPath) {
+
+		if (supportsModuleFramework) {
+			urlPath = urlPath.replace("/sync-web/", "/o/sync/");
+
+			return urlPath.replace("/sync-web.", "/sync.");
+		}
+
+		return urlPath;
+	}
+
+	public static String fixUrlPath(long syncAccountId, String urlPath) {
+		return fixUrlPath(
+			ServerInfo.supportsModuleFramework(syncAccountId), urlPath);
+	}
+
+	public static String fixUrlPath(String pluginVersion, String urlPath) {
+		return fixUrlPath(
+			ServerInfo.supportsModuleFramework(pluginVersion), urlPath);
+	}
+
+}

liferay/liferay-central-test-2

modules/apps/sync/sync-engine/src/main/java/com/liferay/sync/engine/documentlibrary/event/BaseEvent.java
@@ -19,12 +19,12 @@ import com.liferay.sync.engine.documentlibrary.handler.Handler;
 import com.liferay.sync.engine.documentlibrary.util.BatchEvent;
 import com.liferay.sync.engine.documentlibrary.util.BatchEventManager;
 import com.liferay.sync.engine.documentlibrary.util.FileEventManager;
+import com.liferay.sync.engine.documentlibrary.util.ServerUtil;
 import com.liferay.sync.engine.model.SyncAccount;
 import com.liferay.sync.engine.model.SyncFile;
 import com.liferay.sync.engine.service.SyncAccountService;
 import com.liferay.sync.engine.session.Session;
 import com.liferay.sync.engine.session.SessionManager;
-import com.liferay.sync.engine.util.ServerInfo;
 
 import java.util.Map;
 
@@ -138,13 +138,7 @@ public abstract class BaseEvent implements Event {
 
 	@Override
 	public String getURLPath() {
-		if (ServerInfo.supportsModuleFramework(_syncAccountId)) {
-			String urlPath = _urlPath.replace("/sync-web/", "/o/sync/");
-
-			return urlPath.replace("/sync-web.", "/sync.");
-		}
-
-		return _urlPath;
+		return ServerUtil.getURLPath(_syncAccountId, _urlPath);
 	}
 
 	@Override

modules/apps/sync/sync-engine/src/main/java/com/liferay/sync/engine/documentlibrary/util/ServerUtil.java
@@ -1,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sync.engine.documentlibrary.util;
+
+import com.liferay.sync.engine.util.ServerInfo;
+
+/**
+ * @author Dennis Ju
+ */
+public class ServerUtil {
+
+	public static String getURLPath(
+		boolean supportsModuleFramework, String urlPath) {
+
+		if (supportsModuleFramework) {
+			urlPath = urlPath.replace("/sync-web/", "/o/sync/");
+
+			return urlPath.replace("/sync-web.", "/sync.");
+		}
+
+		return urlPath;
+	}
+
+	public static String getURLPath(long syncAccountId, String urlPath) {
+		return getURLPath(
+			ServerInfo.supportsModuleFramework(syncAccountId), urlPath);
+	}
+
+	public static String getURLPath(String pluginVersion, String urlPath) {
+		return getURLPath(
+			ServerInfo.supportsModuleFramework(pluginVersion), urlPath);
+	}
+
+}