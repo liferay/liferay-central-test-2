c40189a719ba3d31769e5b613e63c0b6e69634cc
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-05-25 16:11:20 -0700

    LPS-65012 Fix internal package in microblogs

--- modules/apps/collaboration/message-boards/message-boards-parser-bbcode/src/test/java/com/liferay/message/boards/parser/bbcode/HtmlBBCodeTranslatorImplTest.java
+++ modules/apps/collaboration/message-boards/message-boards-parser-bbcode/src/test/java/com/liferay/message/boards/parser/bbcode/HtmlBBCodeTranslatorImplTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.message.boards.parser.bbcode.internal;
+package com.liferay.message.boards.parser.bbcode;
 
 import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.util.HtmlImpl;
