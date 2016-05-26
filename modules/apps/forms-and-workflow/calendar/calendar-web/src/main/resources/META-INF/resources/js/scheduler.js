de91cbf34e12ee3c83af95a0b784194c0fa0a9f4
Fernando Souza <fernando.souza@liferay.com>
2016-05-26 15:53:54 -0300

    LPS-45616 Removing unnecessary condition of session state.

--- modules/apps/forms-and-workflow/calendar/calendar-web/src/main/resources/META-INF/resources/js/scheduler.js
+++ modules/apps/forms-and-workflow/calendar/calendar-web/src/main/resources/META-INF/resources/js/scheduler.js
@@ -820,39 +820,37 @@ AUI.add(
 					_onClickAddEvent: function(event) {
 						var instance = this;
 
-						if (Liferay.Session.get('sessionState') !== 'expired') {
-							var recorder = instance.get('eventRecorder');
+						var recorder = instance.get('eventRecorder');
 
-							var activeViewName = instance.get('activeView').get('name');
+						var activeViewName = instance.get('activeView').get('name');
 
-							var defaultUserCalendar = CalendarUtil.getDefaultUserCalendar();
+						var defaultUserCalendar = CalendarUtil.getDefaultUserCalendar();
 
-							var calendarId = defaultUserCalendar.get('calendarId');
+						var calendarId = defaultUserCalendar.get('calendarId');
 
-							var editCalendarBookingURL = decodeURIComponent(recorder.get('editCalendarBookingURL'));
+						var editCalendarBookingURL = decodeURIComponent(recorder.get('editCalendarBookingURL'));
 
-							var data = {
-								activeView: activeViewName,
-								calendarId: calendarId,
-								titleCurrentValue: ''
-							};
+						var data = {
+							activeView: activeViewName,
+							calendarId: calendarId,
+							titleCurrentValue: ''
+						};
 
-							Liferay.Util.openWindow(
-								{
-									dialog: {
-										after: {
-											destroy: function(event) {
-												instance.load();
-											}
-										},
-										destroyOnHide: true,
-										modal: true
+						Liferay.Util.openWindow(
+							{
+								dialog: {
+									after: {
+										destroy: function(event) {
+											instance.load();
+										}
 									},
-									title: Liferay.Language.get('new-calendar-booking'),
-									uri: Lang.sub(editCalendarBookingURL, data)
-								}
-							);
-						}
+									destroyOnHide: false,
+									modal: true
+								},
+								title: Liferay.Language.get('new-calendar-booking'),
+								uri: Lang.sub(editCalendarBookingURL, data)
+							}
+						);
 					},
 
 					_onDeleteEvent: function(event) {
