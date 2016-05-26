f8bad9706038dcf538e4389dad8e0dcc99922ef4
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2016-05-26 14:18:41 +0100

    LPS-65350 Advanced Service Access Quota persistence API (base class)

---
+++ modules/apps/foundation/portal-security/portal-security-service-access-quota-api/src/main/java/com/liferay/portal/security/service/access/quota/persistence/BaseIndexedSAQImpressionPersistence.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.service.access.quota.persistence;
+
+import com.liferay.portal.security.service.access.quota.metric.SAQContextMatcher;
+import com.liferay.portal.security.service.access.quota.metric.SAQMetricMatcher;
+
+import java.util.Iterator;
+
+/**
+ * @author Stian Sigvartsen
+ */
+public abstract class BaseIndexedSAQImpressionPersistence
+	implements SAQImpressionPersistence {
+
+	public abstract Iterator<String> findAllMetricValues(
+		long companyId, String metricName);
+
+	public abstract void findAllSAQImpressions(
+		long companyId, SAQImpressionConsumer saqImpressionConsumer);
+
+	@Override
+	public void findSAQImpressions(
+		long companyId, final SAQContextMatcher saqContextMatcher,
+		SAQImpressionConsumer saqImpressionConsumer) {
+
+		for (final String metricName : saqContextMatcher.getMetricNames()) {
+			findSAQImpressionsMatchingMetric(
+				companyId, metricName,
+				new SAQMetricMatcher() {
+
+					@Override
+					public boolean matches(String metricValue) {
+						return saqContextMatcher.matches(
+							metricName, metricValue);
+					}
+
+				},
+				saqImpressionConsumer);
+		}
+	}
+
+	public abstract void findSAQImpressionsMatchingMetric(
+		long companyId, String metric, SAQMetricMatcher saqMetricMatcher,
+		SAQImpressionConsumer saqImpressionConsumer);
+
+	public abstract int getSAQImpressionsCount(
+		long companyId, long expiryIntervalMillis);
+
+}
