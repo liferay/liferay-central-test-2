baf2e3e4e77f0688e9c2527b66725ddf5e7d5443
Juergen Kappler <jurgen.kappler@liferay.com>
2016-05-27 10:16:03 +0200

    LPS-66159 Avoid using deprecated method when upgrading

--- modules/apps/web-experience/xsl-content/xsl-content-web/src/main/java/com/liferay/xsl/content/web/upgrade/XSLContentWebUpgrade.java
+++ modules/apps/web-experience/xsl-content/xsl-content-web/src/main/java/com/liferay/xsl/content/web/upgrade/XSLContentWebUpgrade.java
@@ -14,9 +14,9 @@
 
 package com.liferay.xsl.content.web.upgrade;
 
+import com.liferay.portal.kernel.upgrade.BaseUpgradePortletId;
 import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
-import com.liferay.portal.upgrade.util.UpgradePortletId;
 import com.liferay.xsl.content.web.constants.XSLContentPortletKeys;
 
 import org.osgi.service.component.annotations.Component;
@@ -35,7 +35,7 @@ public class XSLContentWebUpgrade implements UpgradeStepRegistrator {
 
 		registry.register(
 			"com.liferay.xsl.content.web", "0.0.1", "1.0.0",
-			new UpgradePortletId() {
+			new BaseUpgradePortletId() {
 
 				@Override
 				protected String[][] getRenamePortletIdsArray() {
