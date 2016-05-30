d3f23f97349e8e86cc0dad0b1c7a84f7eccd32e0
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2016-05-30 13:52:34 +0200

    LPS-66031 Create contextual action for "edit" article

---
+++ modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/portlet/configuration/icon/EditArticlePortletConfigurationIcon.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledge.base.web.portlet.configuration.icon;
+
+import com.liferay.knowledge.base.constants.KBActionKeys;
+import com.liferay.knowledge.base.constants.KBPortletKeys;
+import com.liferay.knowledge.base.model.KBArticle;
+import com.liferay.knowledge.base.service.permission.DisplayPermission;
+import com.liferay.knowledge.base.service.permission.KBArticlePermission;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+import javax.portlet.PortletURL;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Ambrin Chaudhary
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + KBPortletKeys.KNOWLEDGE_BASE_ADMIN,
+		"path=/admin/view_article.jsp"
+	},
+	service = PortletConfigurationIcon.class
+)
+public class EditArticlePortletConfigurationIcon
+	extends BasePortletConfigurationIcon {
+
+	@Override
+	public String getMessage(PortletRequest portletRequest) {
+		return LanguageUtil.get(
+			getResourceBundle(getLocale(portletRequest)), "edit");
+	}
+
+	@Override
+	public String getURL(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		PortletURL portletURL = PortalUtil.getControlPanelPortletURL(
+			portletRequest, KBPortletKeys.KNOWLEDGE_BASE_ADMIN,
+			PortletRequest.RENDER_PHASE);
+
+		portletURL.setParameter("mvcPath", "/admin/edit_article.jsp");
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		portletURL.setParameter("redirect", themeDisplay.getURLCurrent());
+
+		KBArticle kbArticle = (KBArticle)portletRequest.getAttribute(
+			"article_icons.jsp-kb_article");
+
+		portletURL.setParameter(
+			"resourceClassNameId", String.valueOf(kbArticle.getClassNameId()));
+
+		portletURL.setParameter(
+			"resourcePrimKey", String.valueOf(kbArticle.getResourcePrimKey()));
+
+		portletURL.setParameter(
+			"status", String.valueOf(WorkflowConstants.STATUS_ANY));
+
+		return portletURL.toString();
+	}
+
+	@Override
+	public double getWeight() {
+		return 100;
+	}
+
+	@Override
+	public boolean isShow(PortletRequest portletRequest) {
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		KBArticle kbArticle = (KBArticle)portletRequest.getAttribute(
+			"article_icons.jsp-kb_article");
+
+		PermissionChecker permissionChecker =
+			themeDisplay.getPermissionChecker();
+
+		long scopeGroupId = themeDisplay.getScopeGroupId();
+
+		String rootPortletId =
+			themeDisplay.getPortletDisplay().getRootPortletId();
+
+		if ((!rootPortletId.equals(KBPortletKeys.KNOWLEDGE_BASE_DISPLAY) ||
+			 DisplayPermission.contains(
+				 permissionChecker, scopeGroupId,
+				 KBActionKeys.ADMINISTRATOR)) &&
+			KBArticlePermission.contains(
+				permissionChecker, kbArticle, KBActionKeys.UPDATE)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+}
