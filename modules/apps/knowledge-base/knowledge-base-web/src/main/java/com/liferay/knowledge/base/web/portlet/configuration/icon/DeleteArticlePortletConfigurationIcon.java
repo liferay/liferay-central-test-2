a0de4e96686451725a0874d5f8f8faa44cf3da52
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2016-05-31 13:48:49 +0200

    LPS-66031 Create contextual action for "delete" article

---
+++ modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/portlet/configuration/icon/DeleteArticlePortletConfigurationIcon.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledge.base.web.portlet.configuration.icon;
+
+import com.liferay.knowledge.base.constants.KBActionKeys;
+import com.liferay.knowledge.base.constants.KBPortletKeys;
+import com.liferay.knowledge.base.model.KBArticle;
+import com.liferay.knowledge.base.service.permission.KBArticlePermission;
+import com.liferay.knowledge.base.web.constants.KBWebKeys;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+import javax.portlet.PortletURL;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Ambrin Chaudhary
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + KBPortletKeys.KNOWLEDGE_BASE_ADMIN,
+		"path=/admin/view_article.jsp"
+	},
+	service = PortletConfigurationIcon.class
+)
+public class DeleteArticlePortletConfigurationIcon
+	extends BasePortletConfigurationIcon {
+
+	@Override
+	public String getMessage(PortletRequest portletRequest) {
+		return LanguageUtil.get(
+			getResourceBundle(getLocale(portletRequest)), "delete");
+	}
+
+	@Override
+	public String getURL(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		PortletURL portletURL = PortalUtil.getControlPanelPortletURL(
+			portletRequest, KBPortletKeys.KNOWLEDGE_BASE_ADMIN,
+			PortletRequest.ACTION_PHASE);
+
+		portletURL.setParameter("mvcPath", "/admin/view_article.jsp");
+
+		portletURL.setParameter(ActionRequest.ACTION_NAME, "deleteKBArticle");
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		String redirect = themeDisplay.getURLCurrent();
+
+		KBArticle kbArticle = (KBArticle)portletRequest.getAttribute(
+			"article_icons.jsp-kb_article");
+
+		long resourcePrimKey = ParamUtil.getLong(
+			portletRequest, "resourcePrimKey");
+
+		if (kbArticle.getResourcePrimKey() == resourcePrimKey) {
+			PortletURL homeURL = PortalUtil.getControlPanelPortletURL(
+				portletRequest, KBPortletKeys.KNOWLEDGE_BASE_ADMIN,
+				PortletRequest.RENDER_PHASE);
+
+			redirect = homeURL.toString();
+		}
+
+		portletURL.setParameter("redirect", redirect);
+
+		portletURL.setParameter(
+			"resourceClassNameId", String.valueOf(kbArticle.getClassNameId()));
+
+		portletURL.setParameter(
+			"resourcePrimKey", String.valueOf(kbArticle.getResourcePrimKey()));
+
+		portletURL.setParameter(
+			"status", String.valueOf(
+				portletRequest.getAttribute(KBWebKeys.KNOWLEDGE_BASE_STATUS)));
+
+		return portletURL.toString();
+	}
+
+	@Override
+	public double getWeight() {
+		return 104;
+	}
+
+	@Override
+	public boolean isShow(PortletRequest portletRequest) {
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		KBArticle kbArticle = (KBArticle)portletRequest.getAttribute(
+			"article_icons.jsp-kb_article");
+
+		return (KBArticlePermission.contains(
+			themeDisplay.getPermissionChecker(), kbArticle,
+			KBActionKeys.DELETE));
+	}
+
+}
