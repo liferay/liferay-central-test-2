840c4b766d044b517fc1ad692535e0ae6e7c3fbb
briangreenwald <brian.greenwald@liferay.com>
2016-06-01 16:52:50 -0500

    LPS-66262 Add ability to pass in folder ID when adding web content

--- modules/apps/web-experience/export-import/export-import-resources-importer/src/main/java/com/liferay/exportimport/resources/importer/util/FileSystemImporter.java
+++ modules/apps/web-experience/export-import/export-import-resources-importer/src/main/java/com/liferay/exportimport/resources/importer/util/FileSystemImporter.java
@@ -716,7 +716,7 @@ public class FileSystemImporter extends BaseImporter {
 
 		addJournalArticles(
 			ddmStructureKey, ddmTemplate.getTemplateKey(),
-			_JOURNAL_ARTICLES_DIR_NAME + fileName);
+			_JOURNAL_ARTICLES_DIR_NAME + fileName, 0);
 	}
 
 	protected void addDLFileEntries(String dirName) throws Exception {
@@ -850,7 +850,8 @@ public class FileSystemImporter extends BaseImporter {
 	}
 
 	protected void addJournalArticles(
-			String ddmStructureKey, String ddmTemplateKey, String dirName)
+			String ddmStructureKey, String ddmTemplateKey, String dirName,
+			long folderId)
 		throws Exception {
 
 		File dir = new File(_resourcesDir, dirName);
@@ -869,7 +870,7 @@ public class FileSystemImporter extends BaseImporter {
 					new FileInputStream(file));
 
 				addJournalArticles(
-					ddmStructureKey, ddmTemplateKey, file.getName(),
+					ddmStructureKey, ddmTemplateKey, file.getName(), folderId,
 					inputStream);
 			}
 			finally {
@@ -882,7 +883,7 @@ public class FileSystemImporter extends BaseImporter {
 
 	protected void addJournalArticles(
 			String ddmStructureKey, String ddmTemplateKey, String fileName,
-			InputStream inputStream)
+			long folderId, InputStream inputStream)
 		throws Exception {
 
 		String title = FileUtil.stripExtension(fileName);
@@ -940,7 +941,7 @@ public class FileSystemImporter extends BaseImporter {
 		try {
 			if (journalArticle == null) {
 				journalArticle = journalArticleLocalService.addArticle(
-					userId, groupId, 0, 0, 0, journalArticleId, false,
+					userId, groupId, folderId, 0, 0, journalArticleId, false,
 					JournalArticleConstants.VERSION_DEFAULT,
 					getMap(articleDefaultLocale, title), descriptionMap,
 					content, ddmStructureKey, ddmTemplateKey, StringPool.BLANK,
@@ -951,7 +952,7 @@ public class FileSystemImporter extends BaseImporter {
 			}
 			else {
 				journalArticle = journalArticleLocalService.updateArticle(
-					userId, groupId, 0, journalArticleId,
+					userId, groupId, folderId, journalArticleId,
 					journalArticle.getVersion(),
 					getMap(articleDefaultLocale, title), descriptionMap,
 					content, ddmStructureKey, ddmTemplateKey, StringPool.BLANK,
