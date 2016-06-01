21e8f2852b5efdf08e98879a122b4dadf0520098
Brian Chan <brian.chan@liferay.com>
2016-06-01 13:04:56 -0700

    LPS-66134 SF

--- modules/apps/collaboration/social/social-private-messaging-web/src/main/java/com/liferay/social/privatemessaging/web/portlet/PrivateMessagingPortlet.java
+++ modules/apps/collaboration/social/social-private-messaging-web/src/main/java/com/liferay/social/privatemessaging/web/portlet/PrivateMessagingPortlet.java
@@ -229,8 +229,6 @@ public class PrivateMessagingPortlet extends MVCPortlet {
 		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
-		long userId = themeDisplay.getUserId();
-
 		long mbThreadId = ParamUtil.getLong(uploadPortletRequest, "mbThreadId");
 		String to = ParamUtil.getString(uploadPortletRequest, "to");
 		String subject = ParamUtil.getString(uploadPortletRequest, "subject");
@@ -268,8 +266,8 @@ public class PrivateMessagingPortlet extends MVCPortlet {
 			}
 
 			_userThreadLocalService.addPrivateMessage(
-				userId, mbThreadId, to, subject, body, inputStreamOVPs,
-				themeDisplay);
+				themeDisplay.getUserId(), mbThreadId, to, subject, body,
+				inputStreamOVPs, themeDisplay);
 
 			jsonObject.put("success", Boolean.TRUE);
 		}
