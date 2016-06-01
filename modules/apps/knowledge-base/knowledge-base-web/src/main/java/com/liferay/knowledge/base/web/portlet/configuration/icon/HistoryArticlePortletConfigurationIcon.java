45ffcfb589dbe5805ae2a7ba4f27d264011d6d62
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2016-06-01 09:44:13 +0200

    LPS-66031 Create contextual action for "history"

---
+++ modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/portlet/configuration/icon/HistoryArticlePortletConfigurationIcon.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledge.base.web.portlet.configuration.icon;
+
+import com.liferay.knowledge.base.constants.KBPortletKeys;
+import com.liferay.knowledge.base.model.KBArticle;
+import com.liferay.knowledge.base.web.constants.KBWebKeys;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+import javax.portlet.PortletURL;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Ambrin Chaudhary
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + KBPortletKeys.KNOWLEDGE_BASE_ADMIN,
+		"path=/admin/view_article.jsp"
+	},
+	service = PortletConfigurationIcon.class
+)
+public class HistoryArticlePortletConfigurationIcon
+	extends BasePortletConfigurationIcon {
+
+	@Override
+	public String getMessage(PortletRequest portletRequest) {
+		return LanguageUtil.get(
+			getResourceBundle(getLocale(portletRequest)), "history");
+	}
+
+	@Override
+	public String getURL(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		PortletURL portletURL = PortalUtil.getControlPanelPortletURL(
+			portletRequest, KBPortletKeys.KNOWLEDGE_BASE_ADMIN,
+			PortletRequest.RENDER_PHASE);
+
+		portletURL.setParameter("mvcPath", "/admin/history.jsp");
+
+		KBArticle kbArticle = (KBArticle)portletRequest.getAttribute(
+			"article_icons.jsp-kb_article");
+
+		portletURL.setParameter(
+			"resourceClassNameId", String.valueOf(kbArticle.getClassNameId()));
+
+		portletURL.setParameter(
+			"resourcePrimKey", String.valueOf(kbArticle.getResourcePrimKey()));
+
+		portletURL.setParameter(
+			"status", String.valueOf(
+				portletRequest.getAttribute(KBWebKeys.KNOWLEDGE_BASE_STATUS)));
+
+		return portletURL.toString();
+	}
+
+	@Override
+	public double getWeight() {
+		return 109;
+	}
+
+	@Override
+	public boolean isShow(PortletRequest portletRequest) {
+		KBArticle kbArticle = (KBArticle)portletRequest.getAttribute(
+			KBWebKeys.KNOWLEDGE_BASE_KB_ARTICLE);
+
+		boolean enableKBArticleHistory = GetterUtil.getBoolean(
+			portletRequest.getAttribute("init.jsp-enableKBArticleHistory"));
+
+		if (enableKBArticleHistory &&
+			(kbArticle.isApproved() || !kbArticle.isFirstVersion())) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+}
