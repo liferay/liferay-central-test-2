da557a133d9cd832b18f4e76766b57e479819dd1
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-06-02 11:59:36 +0100

    LPS-65475 Adds portlet

---
+++ modules/sdk/gradle-templates/src/main/resources/standalone/panelapp/src/main/java/_package_path_/portlet/_CLASS_Portlet.java
@@ -0,0 +1,47 @@
+package _package_.portlet;
+
+import _package_.constants._CLASS_PortletKeys;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.portlet.GenericPortlet;
+import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.display-category=category.hidden",
+		"com.liferay.portlet.layout-cacheable=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=_NAME_ Portlet",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.name=" + _CLASS_PortletKeys._CLASS_,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
+public class _CLASS_Portlet extends GenericPortlet {
+
+	@Override
+	protected void doView(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		PrintWriter printWriter = renderResponse.getWriter();
+
+		printWriter.print("_NAME_ Portlet - Hello World!");
+	}
+
+}
