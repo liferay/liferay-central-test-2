a215136f246764e09ed2c0b26406fd8ac68478d8
Tamas Molnar <tamas.molnar@liferay.com>
2016-06-02 14:41:33 +0200

    LPS-66292 Autogenerated

--- modules/apps/foundation/portal-background-task/portal-background-task-api/src/main/java/com/liferay/portal/background/task/service/persistence/BackgroundTaskPersistence.java
+++ modules/apps/foundation/portal-background-task/portal-background-task-api/src/main/java/com/liferay/portal/background/task/service/persistence/BackgroundTaskPersistence.java
@@ -305,6 +305,138 @@ public interface BackgroundTaskPersistence extends BasePersistence<BackgroundTas
 	public int countByCompanyId(long companyId);
 
 	/**
+	* Returns all the background tasks where completed = &#63;.
+	*
+	* @param completed the completed
+	* @return the matching background tasks
+	*/
+	public java.util.List<BackgroundTask> findByCompleted(boolean completed);
+
+	/**
+	* Returns a range of all the background tasks where completed = &#63;.
+	*
+	* <p>
+	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link BackgroundTaskModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
+	* </p>
+	*
+	* @param completed the completed
+	* @param start the lower bound of the range of background tasks
+	* @param end the upper bound of the range of background tasks (not inclusive)
+	* @return the range of matching background tasks
+	*/
+	public java.util.List<BackgroundTask> findByCompleted(boolean completed,
+		int start, int end);
+
+	/**
+	* Returns an ordered range of all the background tasks where completed = &#63;.
+	*
+	* <p>
+	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link BackgroundTaskModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
+	* </p>
+	*
+	* @param completed the completed
+	* @param start the lower bound of the range of background tasks
+	* @param end the upper bound of the range of background tasks (not inclusive)
+	* @param orderByComparator the comparator to order the results by (optionally <code>null</code>)
+	* @return the ordered range of matching background tasks
+	*/
+	public java.util.List<BackgroundTask> findByCompleted(boolean completed,
+		int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator<BackgroundTask> orderByComparator);
+
+	/**
+	* Returns an ordered range of all the background tasks where completed = &#63;.
+	*
+	* <p>
+	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link BackgroundTaskModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
+	* </p>
+	*
+	* @param completed the completed
+	* @param start the lower bound of the range of background tasks
+	* @param end the upper bound of the range of background tasks (not inclusive)
+	* @param orderByComparator the comparator to order the results by (optionally <code>null</code>)
+	* @param retrieveFromCache whether to retrieve from the finder cache
+	* @return the ordered range of matching background tasks
+	*/
+	public java.util.List<BackgroundTask> findByCompleted(boolean completed,
+		int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator<BackgroundTask> orderByComparator,
+		boolean retrieveFromCache);
+
+	/**
+	* Returns the first background task in the ordered set where completed = &#63;.
+	*
+	* @param completed the completed
+	* @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
+	* @return the first matching background task
+	* @throws NoSuchBackgroundTaskException if a matching background task could not be found
+	*/
+	public BackgroundTask findByCompleted_First(boolean completed,
+		com.liferay.portal.kernel.util.OrderByComparator<BackgroundTask> orderByComparator)
+		throws NoSuchBackgroundTaskException;
+
+	/**
+	* Returns the first background task in the ordered set where completed = &#63;.
+	*
+	* @param completed the completed
+	* @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
+	* @return the first matching background task, or <code>null</code> if a matching background task could not be found
+	*/
+	public BackgroundTask fetchByCompleted_First(boolean completed,
+		com.liferay.portal.kernel.util.OrderByComparator<BackgroundTask> orderByComparator);
+
+	/**
+	* Returns the last background task in the ordered set where completed = &#63;.
+	*
+	* @param completed the completed
+	* @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
+	* @return the last matching background task
+	* @throws NoSuchBackgroundTaskException if a matching background task could not be found
+	*/
+	public BackgroundTask findByCompleted_Last(boolean completed,
+		com.liferay.portal.kernel.util.OrderByComparator<BackgroundTask> orderByComparator)
+		throws NoSuchBackgroundTaskException;
+
+	/**
+	* Returns the last background task in the ordered set where completed = &#63;.
+	*
+	* @param completed the completed
+	* @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
+	* @return the last matching background task, or <code>null</code> if a matching background task could not be found
+	*/
+	public BackgroundTask fetchByCompleted_Last(boolean completed,
+		com.liferay.portal.kernel.util.OrderByComparator<BackgroundTask> orderByComparator);
+
+	/**
+	* Returns the background tasks before and after the current background task in the ordered set where completed = &#63;.
+	*
+	* @param backgroundTaskId the primary key of the current background task
+	* @param completed the completed
+	* @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
+	* @return the previous, current, and next background task
+	* @throws NoSuchBackgroundTaskException if a background task with the primary key could not be found
+	*/
+	public BackgroundTask[] findByCompleted_PrevAndNext(long backgroundTaskId,
+		boolean completed,
+		com.liferay.portal.kernel.util.OrderByComparator<BackgroundTask> orderByComparator)
+		throws NoSuchBackgroundTaskException;
+
+	/**
+	* Removes all the background tasks where completed = &#63; from the database.
+	*
+	* @param completed the completed
+	*/
+	public void removeByCompleted(boolean completed);
+
+	/**
+	* Returns the number of background tasks where completed = &#63;.
+	*
+	* @param completed the completed
+	* @return the number of matching background tasks
+	*/
+	public int countByCompleted(boolean completed);
+
+	/**
 	* Returns all the background tasks where status = &#63;.
 	*
 	* @param status the status
--- modules/apps/foundation/portal-background-task/portal-background-task-api/src/main/java/com/liferay/portal/background/task/service/persistence/BackgroundTaskUtil.java
+++ modules/apps/foundation/portal-background-task/portal-background-task-api/src/main/java/com/liferay/portal/background/task/service/persistence/BackgroundTaskUtil.java
@@ -430,6 +430,168 @@ public class BackgroundTaskUtil {
 	}
 
 	/**
+	* Returns all the background tasks where completed = &#63;.
+	*
+	* @param completed the completed
+	* @return the matching background tasks
+	*/
+	public static List<BackgroundTask> findByCompleted(boolean completed) {
+		return getPersistence().findByCompleted(completed);
+	}
+
+	/**
+	* Returns a range of all the background tasks where completed = &#63;.
+	*
+	* <p>
+	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link BackgroundTaskModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
+	* </p>
+	*
+	* @param completed the completed
+	* @param start the lower bound of the range of background tasks
+	* @param end the upper bound of the range of background tasks (not inclusive)
+	* @return the range of matching background tasks
+	*/
+	public static List<BackgroundTask> findByCompleted(boolean completed,
+		int start, int end) {
+		return getPersistence().findByCompleted(completed, start, end);
+	}
+
+	/**
+	* Returns an ordered range of all the background tasks where completed = &#63;.
+	*
+	* <p>
+	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link BackgroundTaskModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
+	* </p>
+	*
+	* @param completed the completed
+	* @param start the lower bound of the range of background tasks
+	* @param end the upper bound of the range of background tasks (not inclusive)
+	* @param orderByComparator the comparator to order the results by (optionally <code>null</code>)
+	* @return the ordered range of matching background tasks
+	*/
+	public static List<BackgroundTask> findByCompleted(boolean completed,
+		int start, int end, OrderByComparator<BackgroundTask> orderByComparator) {
+		return getPersistence()
+				   .findByCompleted(completed, start, end, orderByComparator);
+	}
+
+	/**
+	* Returns an ordered range of all the background tasks where completed = &#63;.
+	*
+	* <p>
+	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link BackgroundTaskModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
+	* </p>
+	*
+	* @param completed the completed
+	* @param start the lower bound of the range of background tasks
+	* @param end the upper bound of the range of background tasks (not inclusive)
+	* @param orderByComparator the comparator to order the results by (optionally <code>null</code>)
+	* @param retrieveFromCache whether to retrieve from the finder cache
+	* @return the ordered range of matching background tasks
+	*/
+	public static List<BackgroundTask> findByCompleted(boolean completed,
+		int start, int end,
+		OrderByComparator<BackgroundTask> orderByComparator,
+		boolean retrieveFromCache) {
+		return getPersistence()
+				   .findByCompleted(completed, start, end, orderByComparator,
+			retrieveFromCache);
+	}
+
+	/**
+	* Returns the first background task in the ordered set where completed = &#63;.
+	*
+	* @param completed the completed
+	* @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
+	* @return the first matching background task
+	* @throws NoSuchBackgroundTaskException if a matching background task could not be found
+	*/
+	public static BackgroundTask findByCompleted_First(boolean completed,
+		OrderByComparator<BackgroundTask> orderByComparator)
+		throws com.liferay.portal.background.task.exception.NoSuchBackgroundTaskException {
+		return getPersistence()
+				   .findByCompleted_First(completed, orderByComparator);
+	}
+
+	/**
+	* Returns the first background task in the ordered set where completed = &#63;.
+	*
+	* @param completed the completed
+	* @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
+	* @return the first matching background task, or <code>null</code> if a matching background task could not be found
+	*/
+	public static BackgroundTask fetchByCompleted_First(boolean completed,
+		OrderByComparator<BackgroundTask> orderByComparator) {
+		return getPersistence()
+				   .fetchByCompleted_First(completed, orderByComparator);
+	}
+
+	/**
+	* Returns the last background task in the ordered set where completed = &#63;.
+	*
+	* @param completed the completed
+	* @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
+	* @return the last matching background task
+	* @throws NoSuchBackgroundTaskException if a matching background task could not be found
+	*/
+	public static BackgroundTask findByCompleted_Last(boolean completed,
+		OrderByComparator<BackgroundTask> orderByComparator)
+		throws com.liferay.portal.background.task.exception.NoSuchBackgroundTaskException {
+		return getPersistence()
+				   .findByCompleted_Last(completed, orderByComparator);
+	}
+
+	/**
+	* Returns the last background task in the ordered set where completed = &#63;.
+	*
+	* @param completed the completed
+	* @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
+	* @return the last matching background task, or <code>null</code> if a matching background task could not be found
+	*/
+	public static BackgroundTask fetchByCompleted_Last(boolean completed,
+		OrderByComparator<BackgroundTask> orderByComparator) {
+		return getPersistence()
+				   .fetchByCompleted_Last(completed, orderByComparator);
+	}
+
+	/**
+	* Returns the background tasks before and after the current background task in the ordered set where completed = &#63;.
+	*
+	* @param backgroundTaskId the primary key of the current background task
+	* @param completed the completed
+	* @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
+	* @return the previous, current, and next background task
+	* @throws NoSuchBackgroundTaskException if a background task with the primary key could not be found
+	*/
+	public static BackgroundTask[] findByCompleted_PrevAndNext(
+		long backgroundTaskId, boolean completed,
+		OrderByComparator<BackgroundTask> orderByComparator)
+		throws com.liferay.portal.background.task.exception.NoSuchBackgroundTaskException {
+		return getPersistence()
+				   .findByCompleted_PrevAndNext(backgroundTaskId, completed,
+			orderByComparator);
+	}
+
+	/**
+	* Removes all the background tasks where completed = &#63; from the database.
+	*
+	* @param completed the completed
+	*/
+	public static void removeByCompleted(boolean completed) {
+		getPersistence().removeByCompleted(completed);
+	}
+
+	/**
+	* Returns the number of background tasks where completed = &#63;.
+	*
+	* @param completed the completed
+	* @return the number of matching background tasks
+	*/
+	public static int countByCompleted(boolean completed) {
+		return getPersistence().countByCompleted(completed);
+	}
+
+	/**
 	* Returns all the background tasks where status = &#63;.
 	*
 	* @param status the status
