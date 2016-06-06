359820933b91acecb6f82dd6bcd00af87e0bd5d4
Marcellus Tavares <marcellus.tavares@liferay.com>
2016-06-06 14:57:22 -0300

    LPS-66381 Add pagination mode support to DDMFormLayout annotation

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/annotations/DDMFormLayout.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/annotations/DDMFormLayout.java
@@ -14,6 +14,8 @@
 
 package com.liferay.dynamic.data.mapping.annotations;
 
+import com.liferay.portal.kernel.util.StringPool;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -25,6 +27,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface DDMFormLayout {
+
+	public String paginationMode() default StringPool.BLANK;
 
 	public DDMFormLayoutPage[] value() default {};
 
