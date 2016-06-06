3f5f2582b4b29c76e9600d71b09689b660f530a3
peterfellwock <peter.fellwock@liferay.com>
2016-06-06 12:06:31 -0700

    LPS-66316 OSGi-fy UserModelListener

--- modules/apps/mail/mail-service/src/main/java/com/liferay/mail/model/listener/UserModelListener.java
+++ modules/apps/mail/mail-service/src/main/java/com/liferay/mail/model/listener/UserModelListener.java
@@ -21,14 +21,19 @@ import com.liferay.mail.service.AccountLocalServiceUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.util.StringPool;
 
 import java.util.List;
 
+import org.osgi.service.component.annotations.Component;
+
 /**
  * @author Scott Lee
+ * @author Peter Fellwock
  */
+@Component(immediate = true, service = ModelListener.class)
 public class UserModelListener extends BaseModelListener<User> {
 
 	@Override
