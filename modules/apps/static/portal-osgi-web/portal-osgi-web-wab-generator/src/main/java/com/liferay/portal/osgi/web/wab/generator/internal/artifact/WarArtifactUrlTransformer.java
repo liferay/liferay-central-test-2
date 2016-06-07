8ff7f09b89bd02eeb87e8448264441c11e247097
Matthew Tambara <matthew.tambara@liferay.com>
2016-06-07 11:39:10 -0700

    LPS-66413 Pull out into util class

--- modules/apps/static/portal-osgi-web/portal-osgi-web-wab-generator/src/main/java/com/liferay/portal/osgi/web/wab/generator/internal/artifact/WarArtifactUrlTransformer.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-wab-generator/src/main/java/com/liferay/portal/osgi/web/wab/generator/internal/artifact/WarArtifactUrlTransformer.java
@@ -14,28 +14,11 @@
 
 package com.liferay.portal.osgi.web.wab.generator.internal.artifact;
 
-import com.liferay.portal.kernel.util.StringBundler;
-import com.liferay.portal.kernel.util.URLCodec;
-import com.liferay.portal.osgi.web.wab.generator.internal.util.ManifestUtil;
-
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 
 import java.net.URL;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import java.util.Properties;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
-
 import org.apache.felix.fileinstall.ArtifactUrlTransformer;
-
-import org.osgi.framework.Constants;
 
 /**
  * @author Miguel Pastor
@@ -56,77 +39,7 @@ public class WarArtifactUrlTransformer implements ArtifactUrlTransformer {
 
 	@Override
 	public URL transform(URL artifact) throws Exception {
-		String path = artifact.getPath();
-
-		int x = path.lastIndexOf('/');
-		int y = path.lastIndexOf(".war");
-
-		String symbolicName = path.substring(x + 1, y);
-
-		Matcher matcher = _pattern.matcher(symbolicName);
-
-		if (matcher.matches()) {
-			symbolicName = matcher.group(1);
-		}
-
-		String contextName = null;
-
-		if ("file".equals(artifact.getProtocol())) {
-			if (ManifestUtil.isValidOSGiBundle(artifact.getPath())) {
-				return artifact;
-			}
-
-			contextName = _readServletContextName(
-				new File(URLCodec.decodeURL(artifact.getPath())));
-		}
-		else {
-			Path tempFilePath = Files.createTempFile(null, null);
-
-			try (InputStream inputStream = artifact.openStream()) {
-				Files.copy(inputStream, tempFilePath);
-
-				contextName = _readServletContextName(tempFilePath.toFile());
-			}
-			finally {
-				Files.delete(tempFilePath);
-			}
-		}
-
-		if (contextName == null) {
-			contextName = symbolicName;
-		}
-
-		StringBundler sb = new StringBundler(7);
-
-		sb.append(artifact.getPath());
-		sb.append("?");
-		sb.append(Constants.BUNDLE_SYMBOLICNAME);
-		sb.append("=");
-		sb.append(symbolicName);
-		sb.append("&Web-ContextPath=/");
-		sb.append(contextName);
-
-		URL url = new URL("file", null, sb.toString());
-
-		url = new URL("webbundle", null, url.toString());
-
-		return url;
+		return ArtifactURLUtil.transform(artifact);
 	}
-
-	private String _readServletContextName(File file) throws IOException {
-		try (ZipFile zipFile = new ZipFile(file);
-			InputStream inputStream = zipFile.getInputStream(
-				new ZipEntry("WEB-INF/liferay-plugin-package.properties"))) {
-
-			Properties properties = new Properties();
-
-			properties.load(inputStream);
-
-			return properties.getProperty("servlet-context-name");
-		}
-	}
-
-	private static final Pattern _pattern = Pattern.compile(
-		"(.*?)(-\\d+\\.\\d+\\.\\d+\\.\\d+)?");
 
 }
