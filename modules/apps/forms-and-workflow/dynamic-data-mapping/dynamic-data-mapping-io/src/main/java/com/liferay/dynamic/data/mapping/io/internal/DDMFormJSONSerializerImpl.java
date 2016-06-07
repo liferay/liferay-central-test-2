7b28c11bb19d6f410054c125a0e03cbd05e030fc
Leonardo Barros <leonardo.barros@liferay.com>
2016-06-07 14:44:05 -0300

    LPS-66420 Rename to avoid type erasure error

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-io/src/main/java/com/liferay/dynamic/data/mapping/io/internal/DDMFormJSONSerializerImpl.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-io/src/main/java/com/liferay/dynamic/data/mapping/io/internal/DDMFormJSONSerializerImpl.java
@@ -80,7 +80,17 @@ public class DDMFormJSONSerializerImpl implements DDMFormJSONSerializer {
 	protected void addFields(
 		JSONObject jsonObject, List<DDMFormField> ddmFormFields) {
 
-		jsonObject.put("fields", toJSONArray(ddmFormFields));
+		jsonObject.put("fields", fieldsToJSONArray(ddmFormFields));
+	}
+
+	protected void addFormFieldRules(
+		JSONObject jsonObject, List<DDMFormFieldRule> ddmFormFieldRules) {
+
+		if (ddmFormFieldRules.isEmpty()) {
+			return;
+		}
+
+		jsonObject.put("rules", rulesToJSONArray(ddmFormFieldRules));
 	}
 
 	protected void addNestedFields(
@@ -90,7 +100,7 @@ public class DDMFormJSONSerializerImpl implements DDMFormJSONSerializer {
 			return;
 		}
 
-		jsonObject.put("nestedFields", toJSONArray(nestedDDMFormFields));
+		jsonObject.put("nestedFields", fieldsToJSONArray(nestedDDMFormFields));
 	}
 
 	protected void addProperties(
@@ -132,6 +142,16 @@ public class DDMFormJSONSerializerImpl implements DDMFormJSONSerializer {
 		jsonObject.put(propertyName, propertyValue);
 	}
 
+	protected JSONArray fieldsToJSONArray(List<DDMFormField> ddmFormFields) {
+		JSONArray jsonArray = _jsonFactory.createJSONArray();
+
+		for (DDMFormField ddmFormField : ddmFormFields) {
+			jsonArray.put(toJSONObject(ddmFormField));
+		}
+
+		return jsonArray;
+	}
+
 	protected DDMForm getDDMFormFieldTypeSettingsDDMForm(String type) {
 		DDMFormFieldType ddmFormFieldType =
 			_ddmFormFieldTypeServicesTracker.getDDMFormFieldType(type);
@@ -147,42 +167,9 @@ public class DDMFormJSONSerializerImpl implements DDMFormJSONSerializer {
 		return DDMFormFactory.create(ddmFormFieldTypeSettings);
 	}
 
-	protected Object serializeDDMFormFieldProperty(
-		Object property, DDMFormField ddmFormFieldTypeSetting) {
+	protected JSONArray optionsToJSONArray(
+		DDMFormFieldOptions ddmFormFieldOptions) {
 
-		if (ddmFormFieldTypeSetting.isLocalizable()) {
-			return toJSONObject((LocalizedValue)property);
-		}
-
-		String dataType = ddmFormFieldTypeSetting.getDataType();
-
-		if (Objects.equals(dataType, "boolean")) {
-			return GetterUtil.getBoolean(property);
-		}
-		else if (Objects.equals(dataType, "ddm-options")) {
-			return toJSONArray((DDMFormFieldOptions)property);
-		}
-		else if (Objects.equals(dataType, "ddm-validation")) {
-			return toJSONObject((DDMFormFieldValidation)property);
-		}
-		else {
-			return String.valueOf(property);
-		}
-	}
-
-	@Reference(unbind = "-")
-	protected void setDDMFormFieldTypeServicesTracker(
-		DDMFormFieldTypeServicesTracker ddmFormFieldTypeServicesTracker) {
-
-		_ddmFormFieldTypeServicesTracker = ddmFormFieldTypeServicesTracker;
-	}
-
-	@Reference(unbind = "-")
-	protected void setJSONFactory(JSONFactory jsonFactory) {
-		_jsonFactory = jsonFactory;
-	}
-
-	protected JSONArray toJSONArray(DDMFormFieldOptions ddmFormFieldOptions) {
 		Set<String> optionValues = ddmFormFieldOptions.getOptionsValues();
 
 		if (optionValues.isEmpty()) {
@@ -205,17 +192,9 @@ public class DDMFormJSONSerializerImpl implements DDMFormJSONSerializer {
 		return jsonArray;
 	}
 
-	protected JSONArray toJSONArray(List<DDMFormField> ddmFormFields) {
-		JSONArray jsonArray = _jsonFactory.createJSONArray();
+	protected JSONArray rulesToJSONArray(
+		List<DDMFormFieldRule> ddmFormFieldRules) {
 
-		for (DDMFormField ddmFormField : ddmFormFields) {
-			jsonArray.put(toJSONObject(ddmFormField));
-		}
-
-		return jsonArray;
-	}
-
-	protected JSONArray toJSONArray(List<DDMFormFieldRule> ddmFormFieldRules) {
 		JSONArray jsonArray = _jsonFactory.createJSONArray();
 
 		for (DDMFormFieldRule ddmFormFieldRule : ddmFormFieldRules) {
@@ -223,6 +202,41 @@ public class DDMFormJSONSerializerImpl implements DDMFormJSONSerializer {
 		}
 
 		return jsonArray;
+	}
+
+	protected Object serializeDDMFormFieldProperty(
+		Object property, DDMFormField ddmFormFieldTypeSetting) {
+
+		if (ddmFormFieldTypeSetting.isLocalizable()) {
+			return toJSONObject((LocalizedValue)property);
+		}
+
+		String dataType = ddmFormFieldTypeSetting.getDataType();
+
+		if (Objects.equals(dataType, "boolean")) {
+			return GetterUtil.getBoolean(property);
+		}
+		else if (Objects.equals(dataType, "ddm-options")) {
+			return optionsToJSONArray((DDMFormFieldOptions)property);
+		}
+		else if (Objects.equals(dataType, "ddm-validation")) {
+			return toJSONObject((DDMFormFieldValidation)property);
+		}
+		else {
+			return String.valueOf(property);
+		}
+	}
+
+	@Reference(unbind = "-")
+	protected void setDDMFormFieldTypeServicesTracker(
+		DDMFormFieldTypeServicesTracker ddmFormFieldTypeServicesTracker) {
+
+		_ddmFormFieldTypeServicesTracker = ddmFormFieldTypeServicesTracker;
+	}
+
+	@Reference(unbind = "-")
+	protected void setJSONFactory(JSONFactory jsonFactory) {
+		_jsonFactory = jsonFactory;
 	}
 
 	protected JSONObject toJSONObject(DDMFormField ddmFormField) {
@@ -277,16 +291,6 @@ public class DDMFormJSONSerializerImpl implements DDMFormJSONSerializer {
 		}
 
 		return jsonObject;
-	}
-
-	protected void addFormFieldRules(
-		JSONObject jsonObject, List<DDMFormFieldRule> ddmFormFieldRules) {
-
-		if (ddmFormFieldRules.isEmpty()) {
-			return;
-		}
-
-		jsonObject.put("rules", toJSONArray(ddmFormFieldRules));
 	}
 
 	private DDMFormFieldTypeServicesTracker _ddmFormFieldTypeServicesTracker;
