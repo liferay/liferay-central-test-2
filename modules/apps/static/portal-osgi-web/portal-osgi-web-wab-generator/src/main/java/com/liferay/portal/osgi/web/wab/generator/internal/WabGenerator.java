6c2bed6369d7049f96d7d0e985be60d84d558914
Matthew Tambara <matthew.tambara@liferay.com>
2016-06-07 14:15:38 -0700

    LPS-66413 Wait for required themes to be deployed

--- modules/apps/static/portal-osgi-web/portal-osgi-web-wab-generator/src/main/java/com/liferay/portal/osgi/web/wab/generator/internal/WabGenerator.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-wab-generator/src/main/java/com/liferay/portal/osgi/web/wab/generator/internal/WabGenerator.java
@@ -16,20 +16,41 @@ package com.liferay.portal.osgi.web.wab.generator.internal;
 
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.osgi.web.wab.generator.internal.artifact.ArtifactURLUtil;
 import com.liferay.portal.osgi.web.wab.generator.internal.artifact.WarArtifactUrlTransformer;
 import com.liferay.portal.osgi.web.wab.generator.internal.handler.WabURLStreamHandlerService;
 import com.liferay.portal.osgi.web.wab.generator.internal.processor.WabProcessor;
+import com.liferay.portal.util.PropsValues;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+
+import java.net.URI;
+import java.net.URL;
+
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 import java.util.Dictionary;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import org.apache.felix.fileinstall.ArtifactUrlTransformer;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleEvent;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -37,6 +58,7 @@ import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.url.URLConstants;
 import org.osgi.service.url.URLStreamHandlerService;
+import org.osgi.util.tracker.BundleTracker;
 
 /**
  * @author Miguel Pastor
@@ -62,7 +84,38 @@ public class WabGenerator
 	public void start(BundleContext bundleContext) throws Exception {
 		registerURLStreamHandlerService(bundleContext);
 
+		final Set<String> requiredLocations = _scanForRequiredWars(
+			Paths.get(PropsValues.LIFERAY_HOME, "osgi/war"));
+
 		registerArtifactUrlTransformer(bundleContext);
+
+		final CountDownLatch countDownLatch = new CountDownLatch(1);
+
+		BundleTracker<Void> bundleTracker = new BundleTracker<Void>(
+			bundleContext, Bundle.ACTIVE, null) {
+
+			@Override
+			public Void addingBundle(Bundle bundle, BundleEvent event) {
+				String location = bundle.getLocation();
+
+				if (requiredLocations.contains(location)) {
+					requiredLocations.remove(location);
+
+					if (requiredLocations.isEmpty()) {
+						countDownLatch.countDown();
+					}
+				}
+
+				return null;
+			}
+
+		};
+
+		bundleTracker.open();
+
+		countDownLatch.await();
+
+		bundleTracker.close();
 	}
 
 	@Deactivate
@@ -108,6 +161,57 @@ public class WabGenerator
 
 	protected void unsetModuleServiceLifecycle(
 		ModuleServiceLifecycle moduleServiceLifecycle) {
+	}
+
+	private Set<String> _scanForRequiredWars(Path path) throws IOException {
+		final Set<String> locations = new HashSet<>();
+
+		Files.walkFileTree(
+			path,
+			new SimpleFileVisitor<Path>() {
+
+				@Override
+				public FileVisitResult visitFile(
+						Path filePath, BasicFileAttributes basicFileAttributes)
+					throws IOException {
+
+					Path fileNamePath = filePath.getFileName();
+
+					String fileName = StringUtil.toLowerCase(
+						fileNamePath.toString());
+
+					if (!fileName.endsWith(".war")) {
+						return FileVisitResult.CONTINUE;
+					}
+
+					URI uri = filePath.toUri();
+
+					try (ZipFile zipFile = new ZipFile(new File(uri));
+						InputStream inputStream = zipFile.getInputStream(
+							new ZipEntry(
+								"WEB-INF/liferay-plugin-package.properties"))) {
+
+						Properties properties = new Properties();
+
+						properties.load(inputStream);
+
+						if (!Boolean.valueOf(
+								properties.getProperty("startup-required"))) {
+
+							return FileVisitResult.CONTINUE;
+						}
+
+						URL url = ArtifactURLUtil.transform(uri.toURL());
+
+						locations.add(url.toString());
+					}
+
+					return FileVisitResult.CONTINUE;
+				}
+
+			});
+
+		return locations;
 	}
 
 	private ServiceRegistration<ArtifactUrlTransformer> _serviceRegistration;
