a4b3af0874b7498c9dc415e416f9333cb47d0cff
drewbrokke <drew.brokke@liferay.com>
2016-06-09 17:00:45 -0700

    LPS-66430 adds UserGroupFinderConstants

---
+++ portal-impl/src/com/liferay/portal/service/persistence/constants/UserGroupFinderConstants.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.service.persistence.constants;
+
+/**
+ * @author Drew Brokke
+ */
+public class UserGroupFinderConstants {
+
+	public static final String PARAM_USER_GROUP_GROUP_ROLE =
+		"userGroupGroupRole";
+
+	public static final String PARAM_USER_GROUPS_GROUPS = "userGroupsGroups";
+
+	public static final String PARAM_USER_GROUPS_ROLES = "userGroupsRoles";
+
+	public static final String PARAM_USER_GROUPS_TEAMS = "userGroupsTeams";
+
+	public static final String PARAM_USER_GROUPS_USERS = "userGroupsUsers";
+
+	public static final String[] PARAMS_USER_GROUP_FINDER = {
+		PARAM_USER_GROUP_GROUP_ROLE, PARAM_USER_GROUPS_GROUPS,
+		PARAM_USER_GROUPS_ROLES, PARAM_USER_GROUPS_TEAMS,
+		PARAM_USER_GROUPS_USERS
+	};
+
+}
