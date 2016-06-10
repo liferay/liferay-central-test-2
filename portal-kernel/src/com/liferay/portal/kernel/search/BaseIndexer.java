fe29b907f278eca7ee039552771e3da466823fa5
Jorge Díaz <jorge.diaz@liferay.com>
2016-06-10 11:27:43 +0200

    LPS-66509 Set CompanyThreadLocal.companyId with correct value before reindexing as it is used to retrieve locales

--- portal-kernel/src/com/liferay/portal/kernel/search/BaseIndexer.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/BaseIndexer.java
@@ -58,6 +58,7 @@ import com.liferay.portal.kernel.search.filter.TermsFilter;
 import com.liferay.portal.kernel.search.generic.BooleanQueryImpl;
 import com.liferay.portal.kernel.search.generic.MatchAllQuery;
 import com.liferay.portal.kernel.search.hits.HitsProcessorRegistryUtil;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.CountryServiceUtil;
@@ -559,12 +560,18 @@ public abstract class BaseIndexer<T> implements Indexer<T> {
 
 	@Override
 	public void reindex(String[] ids) throws SearchException {
+		long currentCompanyId = CompanyThreadLocal.getCompanyId();
+
 		try {
 			if (IndexWriterHelperUtil.isIndexReadOnly() ||
 				!isIndexerEnabled()) {
 
 				return;
 			}
+
+			long companyId = GetterUtil.getLong(ids[0]);
+
+			CompanyThreadLocal.setCompanyId(companyId);
 
 			doReindex(ids);
 		}
@@ -573,6 +580,9 @@ public abstract class BaseIndexer<T> implements Indexer<T> {
 		}
 		catch (Exception e) {
 			throw new SearchException(e);
+		}
+		finally {
+			CompanyThreadLocal.setCompanyId(currentCompanyId);
 		}
 	}
 
