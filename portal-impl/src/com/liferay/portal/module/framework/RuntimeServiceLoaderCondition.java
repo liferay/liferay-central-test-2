d957341158270d1a807168f8754b75e6cb738ded
Miguel Ángel Pastor Olivar <miguelinlas3@gmail.com>
2016-06-13 11:48:39 +0200

    LPS-66559 Defensive action, in case the end user configure the path with an slash as the last character

--- portal-impl/src/com/liferay/portal/module/framework/RuntimeServiceLoaderCondition.java
+++ portal-impl/src/com/liferay/portal/module/framework/RuntimeServiceLoaderCondition.java
@@ -16,6 +16,7 @@ package com.liferay.portal.module.framework;
 
 import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.ServiceLoaderCondition;
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.util.PropsValues;
 
@@ -36,7 +37,10 @@ public class RuntimeServiceLoaderCondition implements ServiceLoaderCondition {
 			path = "file:/" + path.substring(5, path.length());
 		}
 
-		return path.contains(PropsValues.MODULE_FRAMEWORK_BASE_DIR);
+		String moduleFrameworkBaseDir = PropsValues.MODULE_FRAMEWORK_BASE_DIR.replace(
+				StringPool.DOUBLE_SLASH, StringPool.SLASH);
+
+		return path.contains(moduleFrameworkBaseDir);
 	}
 
 }
