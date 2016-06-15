cc24d60eaba31216f856acae12ff936a157138ab
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-06-15 14:50:49 +0200

    LPS-67066 Adds method to portlet for obtain categories

--- modules/apps/web-experience/asset/asset-categories-selector-web/src/main/java/com/liferay/asset/categories/selector/web/portlet/AssetCategoriesSelectorPortlet.java
+++ modules/apps/web-experience/asset/asset-categories-selector-web/src/main/java/com/liferay/asset/categories/selector/web/portlet/AssetCategoriesSelectorPortlet.java
@@ -14,11 +14,31 @@
 
 package com.liferay.asset.categories.selector.web.portlet;
 
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.asset.kernel.model.AssetCategoryConstants;
+import com.liferay.asset.kernel.service.AssetCategoryService;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.List;
 
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.PortletRequest;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Eudaldo Alonso
@@ -42,4 +62,86 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class AssetCategoriesSelectorPortlet extends MVCPortlet {
+
+	@Override
+	public void serveResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws IOException, PortletException {
+
+		String resourceID = GetterUtil.getString(
+			resourceRequest.getResourceID());
+
+		if (resourceID.equals("getCategories")) {
+			try {
+				JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+				List<AssetCategory> categories = getCategories(resourceRequest);
+
+				for (AssetCategory category : categories) {
+					JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+					List<AssetCategory> childCategories =
+						_assetCategoryService.getChildCategories(
+							category.getCategoryId());
+
+					jsonObject.put("categoryId", category.getCategoryId());
+					jsonObject.put("childrenCount", childCategories.size());
+					jsonObject.put("hasChildren", !childCategories.isEmpty());
+					jsonObject.put("name", category.getName());
+					jsonObject.put(
+						"parentCategoryId", category.getParentCategoryId());
+					jsonObject.put(
+						"titleCurrentValue", category.getTitleCurrentValue());
+
+					jsonArray.put(jsonObject);
+				}
+
+				writeJSON(
+					resourceRequest, resourceResponse, jsonArray.toString());
+			}
+			catch (PortalException pe) {
+				throw new PortletException();
+			}
+		}
+		else {
+			super.serveResource(resourceRequest, resourceResponse);
+		}
+	}
+
+	protected List<AssetCategory> getCategories(PortletRequest portletRequest)
+		throws PortalException {
+
+		long categoryId = ParamUtil.getLong(portletRequest, "categoryId");
+		long vocabularyId = ParamUtil.getLong(portletRequest, "vocabularyId");
+		int start = ParamUtil.getInteger(
+			portletRequest, "start", QueryUtil.ALL_POS);
+		int end = ParamUtil.getInteger(
+			portletRequest, "end", QueryUtil.ALL_POS);
+
+		if ((categoryId <= 0) && (vocabularyId <= 0)) {
+			return Collections.emptyList();
+		}
+
+		if (categoryId > 0) {
+			return _assetCategoryService.getChildCategories(
+				categoryId, start, end, null);
+		}
+
+		long parentCategoryId = ParamUtil.getLong(
+			portletRequest, "parentCategoryId",
+			AssetCategoryConstants.DEFAULT_PARENT_CATEGORY_ID);
+
+		return _assetCategoryService.getVocabularyCategories(
+			parentCategoryId, vocabularyId, start, end, null);
+	}
+
+	@Reference(unbind = "-")
+	protected void setAssetCategoryService(
+		AssetCategoryService assetCategoryService) {
+
+		_assetCategoryService = assetCategoryService;
+	}
+
+	private AssetCategoryService _assetCategoryService;
+
 }
