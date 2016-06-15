e27878c28fbdf44756e089acdf0cef01d2d477ac
giros <gergely.mathe@liferay.com>
2016-06-15 13:51:44 +0200

    LPS-66258 Don't skip inserting locale into alternate URL when locale.prepend.friendly.url.style=2

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -1200,7 +1200,9 @@ public class PortalImpl implements Portal {
 				Locale siteDefaultLocale = getSiteDefaultLocale(
 					layout.getGroupId());
 
-				if (siteDefaultLocale.equals(locale)) {
+				if (siteDefaultLocale.equals(locale) &&
+					(PropsValues.LOCALE_PREPEND_FRIENDLY_URL_STYLE != 2)) {
+
 					return canonicalURL;
 				}
 
