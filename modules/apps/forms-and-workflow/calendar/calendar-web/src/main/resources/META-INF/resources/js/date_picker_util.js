94191d694f8e61809c1b77f63aef3ed41cb15c4c
Renato Rego <renato.rego@liferay.com>
2016-06-17 18:35:20 -0300

    LPS-62410 - Remove old code used to update event placeholder when event date changes.

--- modules/apps/forms-and-workflow/calendar/calendar-web/src/main/resources/META-INF/resources/js/date_picker_util.js
+++ modules/apps/forms-and-workflow/calendar/calendar-web/src/main/resources/META-INF/resources/js/date_picker_util.js
@@ -32,39 +32,6 @@ AUI.add(
 				endTimePicker.selectDates([endDate]);
 
 				intervalSelector.startDurationPreservation();
-			},
-
-			linkToSchedulerEvent: function(datePickerContainer, schedulerEvent, dateAttr) {
-				var instance = this;
-
-				var selects = A.one(datePickerContainer).all('select');
-
-				selects.on(
-					'change',
-					function(event) {
-						var currentTarget = event.currentTarget;
-
-						var date = schedulerEvent.get(dateAttr);
-
-						var selectedSetter = selects.indexOf(currentTarget);
-
-						var setters = [date.setMonth, date.setDate, date.setFullYear, date.setHours, date.setMinutes, date.setHours];
-
-						var value = toInt(currentTarget.val());
-
-						if (selectedSetter === 3 && date.getHours() > 12) {
-							value += 12;
-						}
-
-						if (selectedSetter === 5) {
-							value = date.getHours() + (value === 1 ? 12 : -12);
-						}
-
-						setters[selectedSetter].call(date, value);
-
-						schedulerEvent.get('scheduler').syncEventsUI();
-					}
-				);
 			}
 		};
 	},
