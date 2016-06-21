f28e9a304fa10fed5aa424c8d3e1403a5ac16a30
Dennis Ju <dennis.ju@liferay.com>
2016-06-21 11:52:05 -0700

    SYNC-1622 Engine - Upgrade

--- modules/apps/sync/sync-engine/src/main/java/com/liferay/sync/engine/upgrade/util/UpgradeUtil.java
+++ modules/apps/sync/sync-engine/src/main/java/com/liferay/sync/engine/upgrade/util/UpgradeUtil.java
@@ -17,12 +17,16 @@ package com.liferay.sync.engine.upgrade.util;
 import com.liferay.sync.engine.model.SyncProp;
 import com.liferay.sync.engine.service.SyncAccountService;
 import com.liferay.sync.engine.service.SyncFileService;
+import com.liferay.sync.engine.service.SyncLanClientService;
+import com.liferay.sync.engine.service.SyncLanEndpointService;
 import com.liferay.sync.engine.service.SyncPropService;
 import com.liferay.sync.engine.service.SyncSiteService;
 import com.liferay.sync.engine.service.SyncUserService;
 import com.liferay.sync.engine.service.SyncWatchEventService;
 import com.liferay.sync.engine.service.persistence.SyncAccountPersistence;
 import com.liferay.sync.engine.service.persistence.SyncFilePersistence;
+import com.liferay.sync.engine.service.persistence.SyncLanClientPersistence;
+import com.liferay.sync.engine.service.persistence.SyncLanEndpointPersistence;
 import com.liferay.sync.engine.service.persistence.SyncPropPersistence;
 import com.liferay.sync.engine.service.persistence.SyncSitePersistence;
 import com.liferay.sync.engine.service.persistence.SyncUserPersistence;
@@ -37,6 +41,7 @@ import com.liferay.sync.engine.upgrade.v3_0_9.UpgradeProcess_3_0_9;
 import com.liferay.sync.engine.upgrade.v3_1_0.UpgradeProcess_3_1_0;
 import com.liferay.sync.engine.upgrade.v3_2_1.UpgradeProcess_3_2_1;
 import com.liferay.sync.engine.upgrade.v3_3_0.UpgradeProcess_3_3_0;
+import com.liferay.sync.engine.upgrade.v3_4_0.UpgradeProcess_3_4_0;
 import com.liferay.sync.engine.util.FileUtil;
 import com.liferay.sync.engine.util.LoggerUtil;
 import com.liferay.sync.engine.util.PropsValues;
@@ -113,6 +118,7 @@ public class UpgradeUtil {
 		upgradeProcesses.add(new UpgradeProcess_3_1_0());
 		upgradeProcesses.add(new UpgradeProcess_3_2_1());
 		upgradeProcesses.add(new UpgradeProcess_3_3_0());
+		upgradeProcesses.add(new UpgradeProcess_3_4_0());
 
 		for (UpgradeProcess upgradeProcess : upgradeProcesses) {
 			if (buildNumber < upgradeProcess.getThreshold()) {
@@ -149,6 +155,20 @@ public class UpgradeUtil {
 
 		if (!syncFilePersistence.isTableExists()) {
 			syncFilePersistence.createTable();
+		}
+
+		SyncLanClientPersistence syncLanClientPersistence =
+			SyncLanClientService.getSyncLanClientPersistence();
+
+		if (!syncLanClientPersistence.isTableExists()) {
+			syncLanClientPersistence.createTable();
+		}
+
+		SyncLanEndpointPersistence syncLanEndpointPersistence =
+			SyncLanEndpointService.getSyncLanEndpointPersistence();
+
+		if (!syncLanEndpointPersistence.isTableExists()) {
+			syncLanEndpointPersistence.createTable();
 		}
 
 		SyncPropPersistence syncPropPersistence =
