e457c1cc752886010119e2c4aff1666a6f243993
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-06-21 10:54:39 -0700

    LPS-66762 Baseline plugin

---
+++ modules/sdk/gradle-plugins-baseline/src/main/java/com/liferay/gradle/plugins/baseline/BaselineExtension.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.gradle.plugins.baseline;
+
+import com.liferay.gradle.plugins.baseline.internal.util.GradleUtil;
+
+import org.gradle.api.Project;
+
+/**
+ * @author Andrea Di Giorgi
+ */
+public class BaselineExtension {
+
+	public BaselineExtension(Project project) {
+	}
+
+	public String getLowestBaselineVersion() {
+		return GradleUtil.toString(_lowestBaselineVersion);
+	}
+
+	public void setLowestBaselineVersion(Object lowestBaselineVersion) {
+		_lowestBaselineVersion = lowestBaselineVersion;
+	}
+
+	private Object _lowestBaselineVersion = "1.0.0";
+
+}
