954fed049df1dd4c0f47465ebe6d5c5265b4970a
peterfellwock <peter.fellwock@liferay.com>
2016-06-21 16:17:25 -0700

    LPS-65786 (SERVICE) Apply Configuration Object

---
+++ modules/apps/wsrp/wsrp-service/src/main/java/com/liferay/wsrp/util/WSRPConfigurationUtil.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wsrp.util;
+
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.wsrp.configuration.WSRPGroupServiceConfiguration;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Modified;
+
+/**
+ * @author Peter Fellwock
+ */
+@Component(
+	configurationPid = "com.liferay.wsrp.web.configuration.WSRPConfiguration",
+	configurationPolicy = ConfigurationPolicy.OPTIONAL, immediate = true,
+	service = WSRPConfigurationUtil.class
+)
+public class WSRPConfigurationUtil {
+
+	public static WSRPGroupServiceConfiguration getWSRPConfiguration() {
+		return _getInstance()._wSRPGroupServiceConfiguration;
+	}
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		_wSRPGroupServiceConfiguration = ConfigurableUtil.createConfigurable(
+			WSRPGroupServiceConfiguration.class, properties);
+	}
+
+	private static WSRPConfigurationUtil _getInstance() {
+		return _instance;
+	}
+
+	private static final WSRPConfigurationUtil _instance =
+		new WSRPConfigurationUtil();
+
+	private volatile WSRPGroupServiceConfiguration
+		_wSRPGroupServiceConfiguration;
+
+}
