999dc9fac5a8ed65b46de640d89a7617abab0b54
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-06-24 16:05:51 -0700

    LPS-66848 Gradle templates extractor code

---
+++ modules/sdk/gradle-templates/src/main/java/com/liferay/gradle/templates/internal/util/StringUtil.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.gradle.templates.internal.util;
+
+import java.util.Map;
+
+/**
+ * @author Andrea Di Giorgi
+ */
+public class StringUtil {
+
+	public static String capitalize(String s, char separator) {
+		StringBuilder sb = new StringBuilder(s.length());
+
+		sb.append(s);
+
+		for (int i = 0; i < sb.length(); i++) {
+			char c = sb.charAt(i);
+
+			if ((i == 0) || (sb.charAt(i - 1) == separator)) {
+				c = Character.toUpperCase(c);
+			}
+
+			sb.setCharAt(i, c);
+		}
+
+		return sb.toString();
+	}
+
+	public static String removeChar(String s, char c) {
+		int y = s.indexOf(c);
+
+		if (y == -1) {
+			return s;
+		}
+
+		StringBuilder sb = new StringBuilder(s.length());
+
+		int x = 0;
+
+		while (x <= y) {
+			sb.append(s.substring(x, y));
+
+			x = y + 1;
+			y = s.indexOf(c, x);
+		}
+
+		sb.append(s.substring(x));
+
+		return sb.toString();
+	}
+
+	public static String replace(String s, Map<String, String> replacements) {
+		for (Map.Entry<String, String> entry : replacements.entrySet()) {
+			s = s.replace(entry.getKey(), entry.getValue());
+		}
+
+		return s;
+	}
+
+}
