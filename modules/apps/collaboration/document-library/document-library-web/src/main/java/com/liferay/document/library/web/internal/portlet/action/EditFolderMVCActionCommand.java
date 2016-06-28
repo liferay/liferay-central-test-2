53b439a8d39ebde3c13be65b06785a1bd56bcd6d
Roberto Díaz <roberto.diaz@liferay.com>
2016-06-28 11:18:43 +0200

    LPS-66423 Delete unused

--- modules/apps/collaboration/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/portlet/action/EditFolderMVCActionCommand.java
+++ modules/apps/collaboration/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/portlet/action/EditFolderMVCActionCommand.java
@@ -26,17 +26,13 @@ import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.document.library.kernel.service.DLAppService;
 import com.liferay.document.library.kernel.service.DLTrashService;
 import com.liferay.document.library.web.internal.constants.DLPortletKeys;
-import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.TrashedModel;
-import com.liferay.portal.kernel.portlet.PortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.repository.LocalRepository;
 import com.liferay.portal.kernel.repository.RepositoryProviderUtil;
 import com.liferay.portal.kernel.repository.capabilities.TemporaryFileEntriesCapability;
-import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.service.ServiceContext;
@@ -44,20 +40,9 @@ import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Constants;
-import com.liferay.portal.kernel.util.ContentTypes;
-import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.StreamUtil;
-import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.portal.kernel.workflow.WorkflowConstants;
-import com.liferay.portal.kernel.zip.ZipWriter;
-import com.liferay.portal.kernel.zip.ZipWriterFactoryUtil;
 import com.liferay.trash.kernel.util.TrashUtil;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -65,8 +50,6 @@ import java.util.List;
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.PortletException;
-import javax.portlet.ResourceRequest;
-import javax.portlet.ResourceResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -76,6 +59,7 @@ import org.osgi.service.component.annotations.Reference;
  * @author Alexander Chow
  * @author Sergio González
  * @author Levente Hudák
+ * @author Roberto Díaz
  */
 @Component(
 	property = {
@@ -194,50 +178,6 @@ public class EditFolderMVCActionCommand extends BaseMVCActionCommand {
 		}
 	}
 
-	protected void downloadFolder(
-			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
-		throws Exception {
-
-		ThemeDisplay themeDisplay = (ThemeDisplay)resourceRequest.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		long repositoryId = ParamUtil.getLong(resourceRequest, "repositoryId");
-		long folderId = ParamUtil.getLong(resourceRequest, "folderId");
-
-		File file = null;
-		InputStream inputStream = null;
-
-		try {
-			String zipFileName = LanguageUtil.get(
-				themeDisplay.getLocale(), "documents-and-media");
-
-			if (folderId != DLFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
-				Folder folder = _dlAppService.getFolder(folderId);
-
-				zipFileName = folder.getName();
-			}
-
-			ZipWriter zipWriter = ZipWriterFactoryUtil.getZipWriter();
-
-			zipFolder(repositoryId, folderId, StringPool.SLASH, zipWriter);
-
-			file = zipWriter.getFile();
-
-			inputStream = new FileInputStream(file);
-
-			PortletResponseUtil.sendFile(
-				resourceRequest, resourceResponse, zipFileName, inputStream,
-				ContentTypes.APPLICATION_ZIP);
-		}
-		finally {
-			StreamUtil.cleanUp(inputStream);
-
-			if (file != null) {
-				file.delete();
-			}
-		}
-	}
-
 	@Reference(unbind = "-")
 	protected void setDLAppService(DLAppService dlAppService) {
 		_dlAppService = dlAppService;
@@ -309,35 +249,6 @@ public class EditFolderMVCActionCommand extends BaseMVCActionCommand {
 		_dlAppService.updateFolder(
 			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID, null, null,
 			serviceContext);
-	}
-
-	protected void zipFolder(
-			long repositoryId, long folderId, String path, ZipWriter zipWriter)
-		throws Exception {
-
-		List<Object> foldersAndFileEntriesAndFileShortcuts =
-			_dlAppService.getFoldersAndFileEntriesAndFileShortcuts(
-				repositoryId, folderId, WorkflowConstants.STATUS_APPROVED,
-				false, QueryUtil.ALL_POS, QueryUtil.ALL_POS);
-
-		for (Object entry : foldersAndFileEntriesAndFileShortcuts) {
-			if (entry instanceof Folder) {
-				Folder folder = (Folder)entry;
-
-				zipFolder(
-					folder.getRepositoryId(), folder.getFolderId(),
-					path.concat(StringPool.SLASH).concat(folder.getName()),
-					zipWriter);
-			}
-			else if (entry instanceof FileEntry) {
-				FileEntry fileEntry = (FileEntry)entry;
-
-				zipWriter.addEntry(
-					path + StringPool.SLASH +
-						HtmlUtil.escapeURL(fileEntry.getTitle()),
-					fileEntry.getContentStream());
-			}
-		}
 	}
 
 	private DLAppService _dlAppService;
