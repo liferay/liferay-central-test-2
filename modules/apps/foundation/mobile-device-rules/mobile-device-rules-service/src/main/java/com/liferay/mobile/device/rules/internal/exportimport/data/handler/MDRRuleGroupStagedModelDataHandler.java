aaf9127d6c50ca1a86eb9bcf1b64154492bf4868
Máté Thurzó <mate.thurzo@liferay.com>
2016-06-30 15:53:56 +0200

    LPS-66915 Move MDR data handlers to the proper folder

--- modules/apps/foundation/mobile-device-rules/mobile-device-rules-service/src/main/java/com/liferay/mobile/device/rules/internal/exportimport/data/handler/MDRRuleGroupStagedModelDataHandler.java
+++ modules/apps/foundation/mobile-device-rules/mobile-device-rules-service/src/main/java/com/liferay/mobile/device/rules/internal/exportimport/data/handler/MDRRuleGroupStagedModelDataHandler.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.mobile.device.rules.lar;
+package com.liferay.mobile.device.rules.internal.exportimport.data.handler;
 
 import com.liferay.exportimport.kernel.lar.ExportImportPathUtil;
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
