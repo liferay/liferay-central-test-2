e1a13f18688fbfdef6d015242c41c398f7d4ccfb
Hugo Huijser <hugo.huijser@liferay.com>
2016-07-02 09:28:22 +0800

    LPS-66853 Auto SF

--- portal-impl/src/com/liferay/portal/spring/aop/ServiceBeanAopCacheManager.java
+++ portal-impl/src/com/liferay/portal/spring/aop/ServiceBeanAopCacheManager.java
@@ -175,7 +175,7 @@ public class ServiceBeanAopCacheManager {
 		new ConcurrentHashMap<>();
 	private static final Annotation[] _nullAnnotations = new Annotation[0];
 
-	private
+	private final
 		Map<Class<? extends Annotation>, AnnotationChainableMethodAdvice<?>[]>
 			_annotationChainableMethodAdvices = new HashMap
 				<Class<? extends Annotation>,
