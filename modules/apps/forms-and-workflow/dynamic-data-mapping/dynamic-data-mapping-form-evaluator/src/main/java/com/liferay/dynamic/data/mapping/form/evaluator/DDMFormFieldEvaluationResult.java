40f0a2eb18432c190c81fde03ae4a15e4ee364bd
Leonardo Barros <leonardo.barros@liferay.com>
2016-07-04 11:07:34 -0300

    LPS-66472 Add properties

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-evaluator/src/main/java/com/liferay/dynamic/data/mapping/form/evaluator/DDMFormFieldEvaluationResult.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-evaluator/src/main/java/com/liferay/dynamic/data/mapping/form/evaluator/DDMFormFieldEvaluationResult.java
@@ -15,6 +15,8 @@
 package com.liferay.dynamic.data.mapping.form.evaluator;
 
 import com.liferay.portal.kernel.json.JSON;
+import com.liferay.portal.kernel.util.HashUtil;
+import com.liferay.portal.kernel.util.KeyValuePair;
 import com.liferay.portal.kernel.util.StringPool;
 
 import java.util.ArrayList;
@@ -28,6 +30,47 @@ public class DDMFormFieldEvaluationResult {
 	public DDMFormFieldEvaluationResult(String name, String instanceId) {
 		_name = name;
 		_instanceId = instanceId;
+	}
+
+	public void addOption(String key, String value) {
+		_options.add(new KeyValuePair(key, value));
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (obj == null) {
+			return false;
+		}
+
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+
+		DDMFormFieldEvaluationResult other = (DDMFormFieldEvaluationResult)obj;
+
+		if (_instanceId == null) {
+			if (other._instanceId != null) {
+				return false;
+			}
+		}
+		else if (!_instanceId.equals(other._instanceId)) {
+			return false;
+		}
+
+		if (_name == null) {
+			if (other._name != null) {
+				return false;
+			}
+		}
+		else if (!_name.equals(other._name)) {
+			return false;
+		}
+
+		return true;
 	}
 
 	public String getErrorMessage() {
@@ -47,6 +90,24 @@ public class DDMFormFieldEvaluationResult {
 		getNestedDDMFormFieldEvaluationResults() {
 
 		return _nestedDDMFormFieldEvaluationResults;
+	}
+
+	public List<KeyValuePair> getOptions() {
+		return _options;
+	}
+
+	public Object getValue() {
+		return _value;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = HashUtil.hash(0, _name);
+		return HashUtil.hash(hash, _instanceId);
+	}
+
+	public boolean isReadOnly() {
+		return _readOnly;
 	}
 
 	public boolean isValid() {
@@ -69,8 +130,16 @@ public class DDMFormFieldEvaluationResult {
 			nestedDDMFormFieldEvaluationResults;
 	}
 
+	public void setReadOnly(boolean readOnly) {
+		_readOnly = readOnly;
+	}
+
 	public void setValid(boolean valid) {
 		_valid = valid;
+	}
+
+	public void setValue(Object value) {
+		_value = value;
 	}
 
 	public void setVisible(boolean visible) {
@@ -82,7 +151,10 @@ public class DDMFormFieldEvaluationResult {
 	private final String _name;
 	private List<DDMFormFieldEvaluationResult>
 		_nestedDDMFormFieldEvaluationResults = new ArrayList<>();
+	private final List<KeyValuePair> _options = new ArrayList<>();
+	private boolean _readOnly;
 	private boolean _valid = true;
+	private Object _value;
 	private boolean _visible = true;
 
 }
