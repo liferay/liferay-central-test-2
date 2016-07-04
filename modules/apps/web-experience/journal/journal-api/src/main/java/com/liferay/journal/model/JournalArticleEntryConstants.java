eb8f4b413efe5faf57d4264b8aac49dbfcee5133
Juergen Kappler <jurgen.kappler@liferay.com>
2016-07-04 09:16:26 +0200

    LPS-66701 Add new contants for journal article entry notifications

---
+++ modules/apps/web-experience/journal/journal-api/src/main/java/com/liferay/journal/model/JournalArticleEntryConstants.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.model;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class JournalArticleEntryConstants {
+
+	public static final int NOTIFICATION_TYPE_ADD_ENTRY = 0;
+
+	public static final int NOTIFICATION_TYPE_MOVE_ENTRY_FROM_FOLDER = 2;
+
+	public static final int NOTIFICATION_TYPE_MOVE_ENTRY_TO_FOLDER = 1;
+
+	public static final int NOTIFICATION_TYPE_UPDATE_ENTRY = 3;
+
+}
