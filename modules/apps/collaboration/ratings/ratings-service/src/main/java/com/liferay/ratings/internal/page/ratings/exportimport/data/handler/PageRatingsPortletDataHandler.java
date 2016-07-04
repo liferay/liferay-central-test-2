7922b473dd5ac8462c665e308c4e6ade469a6ecc
Máté Thurzó <mate.thurzo@liferay.com>
2016-07-04 16:28:35 +0200

    LPS-66924 Move ratings related data handlers to the corect packages

--- modules/apps/collaboration/ratings/ratings-service/src/main/java/com/liferay/ratings/internal/page/ratings/exportimport/data/handler/PageRatingsPortletDataHandler.java
+++ modules/apps/collaboration/ratings/ratings-service/src/main/java/com/liferay/ratings/internal/page/ratings/exportimport/data/handler/PageRatingsPortletDataHandler.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.ratings.internal.page.ratings.lar;
+package com.liferay.ratings.internal.page.ratings.exportimport.data.handler;
 
 import com.liferay.exportimport.kernel.lar.BasePortletDataHandler;
 import com.liferay.exportimport.kernel.lar.ExportImportProcessCallbackRegistryUtil;
