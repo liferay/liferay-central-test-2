4e7630b44563d61e28535d2f6021a37dd632686a
Brian Chan <brian.chan@liferay.com>
2016-07-04 21:20:06 -0700

    LPS-66229 Avoid Validator checks for long. We should probably deprecate that method. Hugo SF

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/render/impl/DocumentLibraryDDMFormFieldValueRenderer.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/render/impl/DocumentLibraryDDMFormFieldValueRenderer.java
@@ -57,7 +57,7 @@ public class DocumentLibraryDDMFormFieldValueRenderer
 				String uuid = jsonObject.getString("uuid");
 				long groupId = jsonObject.getLong("groupId");
 
-				if (Validator.isNull(uuid) && Validator.isNull(groupId)) {
+				if (Validator.isNull(uuid) && (groupId == 0)) {
 					return StringPool.BLANK;
 				}
 
