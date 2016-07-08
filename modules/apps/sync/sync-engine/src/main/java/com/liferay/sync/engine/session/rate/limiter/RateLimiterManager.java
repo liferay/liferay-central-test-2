a1905b6ed6223db8ff3d63dc5cf8f7e79979006e
Shinn Lok <shinn.lok@liferay.com>
2016-07-08 14:40:35 -0700

    SYNC-1638 Synchronized

---
+++ modules/apps/sync/sync-engine/src/main/java/com/liferay/sync/engine/session/rate/limiter/RateLimiterManager.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sync.engine.session.rate.limiter;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+
+/**
+ * @author Jonathan McCann
+ */
+public class RateLimiterManager {
+
+	public static synchronized RateLimiter getDownloadRateLimiter(
+		long syncAccountId) {
+
+		RateLimiter rateLimiter = getRateLimiter(
+			syncAccountId, _downloadRateLimiters);
+
+		updateDownloadRateLimits(syncAccountId);
+
+		return rateLimiter;
+	}
+
+	public static synchronized RateLimiter getUploadRateLimiter(
+		long syncAccountId) {
+
+		RateLimiter rateLimiter = getRateLimiter(
+			syncAccountId, _uploadRateLimiters);
+
+		updateUploadRateLimits(syncAccountId);
+
+		return rateLimiter;
+	}
+
+	public static synchronized void removeDownloadRateLimiter(
+		long syncAccountId, RateLimiter rateLimiter) {
+
+		List<RateLimiter> rateLimiters = _downloadRateLimiters.get(
+			syncAccountId);
+
+		rateLimiters.remove(rateLimiter);
+
+		updateDownloadRateLimits(syncAccountId);
+	}
+
+	public static synchronized void removeUploadRateLimiter(
+		long syncAccountId, RateLimiter rateLimiter) {
+
+		List<RateLimiter> rateLimiters = _uploadRateLimiters.get(syncAccountId);
+
+		rateLimiters.remove(rateLimiter);
+
+		updateUploadRateLimits(syncAccountId);
+	}
+
+	protected static RateLimiter getRateLimiter(
+		long syncAccountId, Map<Long, List<RateLimiter>> rateLimiterMap) {
+
+		RateLimiter rateLimiter = RateLimiter.create(0);
+
+		List<RateLimiter> rateLimiters = rateLimiterMap.get(syncAccountId);
+
+		if (rateLimiters == null) {
+			rateLimiters = new ArrayList<>();
+
+			rateLimiters.add(rateLimiter);
+
+			rateLimiterMap.put(syncAccountId, rateLimiters);
+		}
+		else {
+			rateLimiters.add(rateLimiter);
+		}
+
+		return rateLimiter;
+	}
+
+	protected static void updateDownloadRateLimits(long syncAccountId) {
+		List<RateLimiter> rateLimiters = _downloadRateLimiters.get(
+			syncAccountId);
+
+		double rate = (2 * FileUtils.ONE_MB) / rateLimiters.size();
+
+		for (RateLimiter rateLimiter : rateLimiters) {
+			rateLimiter.setRate(rate);
+		}
+	}
+
+	protected static void updateUploadRateLimits(long syncAccountId) {
+		List<RateLimiter> rateLimiters = _uploadRateLimiters.get(syncAccountId);
+
+		double rate = (2 * FileUtils.ONE_MB) / rateLimiters.size();
+
+		for (RateLimiter rateLimiter : rateLimiters) {
+			rateLimiter.setRate(rate);
+		}
+	}
+
+	private static final Map<Long, List<RateLimiter>> _downloadRateLimiters =
+		new HashMap<>();
+	private static final Map<Long, List<RateLimiter>> _uploadRateLimiters =
+		new HashMap<>();
+
+}
