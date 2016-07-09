61e796c8f72d06864a2689bb544c7a239584d0b0
Rafael Praxedes <rafael.praxedes@liferay.com>
2016-07-09 17:37:19 -0300

    LPS-65804 Create unit tests

---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test/src/test/java/com/liferay/dynamic/data/mapping/type/checkbox/multiple/CheckboxMultipleDDMFormFieldValueAccessorTest.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.type.checkbox.multiple;
+
+import com.liferay.dynamic.data.mapping.model.UnlocalizedValue;
+import com.liferay.dynamic.data.mapping.storage.DDMFormFieldValue;
+import com.liferay.dynamic.data.mapping.test.util.DDMFormValuesTestUtil;
+import com.liferay.portal.json.JSONFactoryImpl;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringPool;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Rafael Praxedes
+ */
+public class CheckboxMultipleDDMFormFieldValueAccessorTest {
+
+	@Before
+	public void setUp() {
+		_checkboxMultipleDDMFormFieldValueAccessor =
+			new CheckboxMultipleDDMFormFieldValueAccessor();
+
+		_checkboxMultipleDDMFormFieldValueAccessor.jsonFactory = _jsonFactory;
+	}
+
+	@Test
+	public void testGetCheckboxMultipleValue() {
+		JSONArray expectedJSONArray = createJSONArray("value 1");
+
+		DDMFormFieldValue ddmFormFieldValue =
+			DDMFormValuesTestUtil.createDDMFormFieldValue(
+				"CheckboxMultiple",
+				new UnlocalizedValue(expectedJSONArray.toString()));
+
+		JSONArray actualJSONArray =
+			_checkboxMultipleDDMFormFieldValueAccessor.getValue(
+				ddmFormFieldValue, LocaleUtil.US);
+
+		Assert.assertEquals(
+			expectedJSONArray.toString(), actualJSONArray.toString());
+	}
+
+	@Test
+	public void testGetCheckboxMultipleValue1() {
+		DDMFormFieldValue ddmFormFieldValue =
+			DDMFormValuesTestUtil.createDDMFormFieldValue(
+				"CheckboxMultiple", new UnlocalizedValue("value 1"));
+
+		JSONArray actualJSONArray =
+			_checkboxMultipleDDMFormFieldValueAccessor.getValue(
+				ddmFormFieldValue, LocaleUtil.US);
+
+		Assert.assertEquals("value 1", actualJSONArray.getString(0));
+	}
+
+	@Test
+	public void testGetCheckboxMultipleValue2() {
+		DDMFormFieldValue ddmFormFieldValue =
+			DDMFormValuesTestUtil.createDDMFormFieldValue(
+				"CheckboxMultiple", new UnlocalizedValue(StringPool.BLANK));
+
+		JSONArray actualJSONArray =
+			_checkboxMultipleDDMFormFieldValueAccessor.getValue(
+				ddmFormFieldValue, LocaleUtil.US);
+
+		Assert.assertTrue(actualJSONArray.length() == 0);
+	}
+
+	protected JSONArray createJSONArray(String... strings) {
+		JSONArray jsonArray = _jsonFactory.createJSONArray();
+
+		for (String string : strings) {
+			jsonArray.put(string);
+		}
+
+		return jsonArray;
+	}
+
+	private CheckboxMultipleDDMFormFieldValueAccessor
+		_checkboxMultipleDDMFormFieldValueAccessor;
+	private final JSONFactory _jsonFactory = new JSONFactoryImpl();
+
+}
---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test/src/test/java/com/liferay/dynamic/data/mapping/type/checkbox/multiple/CheckboxMultipleDDMFormFieldValueRendererTest.java
@@ -0,0 +1,124 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.type.checkbox.multiple;
+
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMFormField;
+import com.liferay.dynamic.data.mapping.model.DDMFormFieldOptions;
+import com.liferay.dynamic.data.mapping.model.UnlocalizedValue;
+import com.liferay.dynamic.data.mapping.storage.DDMFormFieldValue;
+import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
+import com.liferay.dynamic.data.mapping.test.util.DDMFormTestUtil;
+import com.liferay.dynamic.data.mapping.test.util.DDMFormValuesTestUtil;
+import com.liferay.portal.json.JSONFactoryImpl;
+import com.liferay.portal.kernel.util.LocaleUtil;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Rafael Praxedes
+ */
+public class CheckboxMultipleDDMFormFieldValueRendererTest {
+
+	@Before
+	public void setUp() {
+		_checkboxMultipleDDMFormFieldValueRenderer =
+			new CheckboxMultipleDDMFormFieldValueRenderer();
+
+		CheckboxMultipleDDMFormFieldValueAccessor
+			checkboxMultipleDDMFormFieldValueAccessor =
+				new CheckboxMultipleDDMFormFieldValueAccessor();
+
+		checkboxMultipleDDMFormFieldValueAccessor.jsonFactory =
+			new JSONFactoryImpl();
+
+		_checkboxMultipleDDMFormFieldValueRenderer.
+			checkboxMultipleDDMFormFieldValueAccessor =
+				checkboxMultipleDDMFormFieldValueAccessor;
+	}
+
+	@Test
+	public void testRender() throws Exception {
+		DDMForm ddmForm = DDMFormTestUtil.createDDMForm();
+
+		DDMFormField ddmFormField = DDMFormTestUtil.createDDMFormField(
+			"CheckboxMultiple", "Checkbox Multiple", "checkbox-multiple",
+			"string", false, false, false);
+
+		DDMFormFieldOptions ddmFormFieldOptions =
+			ddmFormField.getDDMFormFieldOptions();
+
+		ddmFormFieldOptions.addOptionLabel(
+			"value 1", LocaleUtil.US, "option 1");
+		ddmFormFieldOptions.addOptionLabel(
+			"value 2", LocaleUtil.US, "option 2");
+
+		ddmForm.addDDMFormField(ddmFormField);
+
+		DDMFormValues ddmFormValues = DDMFormValuesTestUtil.createDDMFormValues(
+			ddmForm);
+
+		DDMFormFieldValue ddmFormFieldValue =
+			DDMFormValuesTestUtil.createDDMFormFieldValue(
+				"CheckboxMultiple", new UnlocalizedValue("[\"value 1\"]"));
+
+		ddmFormValues.addDDMFormFieldValue(ddmFormFieldValue);
+
+		Assert.assertEquals(
+			"option 1",
+			_checkboxMultipleDDMFormFieldValueRenderer.render(
+				ddmFormFieldValue, LocaleUtil.US));
+	}
+
+	@Test
+	public void testRender2() throws Exception {
+		DDMForm ddmForm = DDMFormTestUtil.createDDMForm();
+
+		DDMFormField ddmFormField = DDMFormTestUtil.createDDMFormField(
+			"CheckboxMultiple", "Checkbox Multiple", "checkbox-multiple",
+			"string", false, false, false);
+
+		DDMFormFieldOptions ddmFormFieldOptions =
+			ddmFormField.getDDMFormFieldOptions();
+
+		ddmFormFieldOptions.addOptionLabel(
+			"value 1", LocaleUtil.US, "option 1");
+		ddmFormFieldOptions.addOptionLabel(
+			"value 2", LocaleUtil.US, "option 2");
+
+		ddmForm.addDDMFormField(ddmFormField);
+
+		DDMFormValues ddmFormValues = DDMFormValuesTestUtil.createDDMFormValues(
+			ddmForm);
+
+		DDMFormFieldValue ddmFormFieldValue =
+			DDMFormValuesTestUtil.createDDMFormFieldValue(
+				"CheckboxMultiple",
+				new UnlocalizedValue("[\"value 1\",\"value 2\"]"));
+
+		ddmFormValues.addDDMFormFieldValue(ddmFormFieldValue);
+
+		Assert.assertEquals(
+			"option 1, option 2",
+			_checkboxMultipleDDMFormFieldValueRenderer.render(
+				ddmFormFieldValue, LocaleUtil.US));
+	}
+
+	private CheckboxMultipleDDMFormFieldValueRenderer
+		_checkboxMultipleDDMFormFieldValueRenderer;
+
+}
