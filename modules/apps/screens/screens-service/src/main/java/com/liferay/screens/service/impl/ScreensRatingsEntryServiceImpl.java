29eafa8213a216de5fd92c3bde251b5d4c6eda5c
Brian Chan <brian.chan@liferay.com>
2016-07-11 14:57:51 -0700

    LPS-67034 SF

--- modules/apps/screens/screens-service/src/main/java/com/liferay/screens/service/impl/ScreensRatingsEntryServiceImpl.java
+++ modules/apps/screens/screens-service/src/main/java/com/liferay/screens/service/impl/ScreensRatingsEntryServiceImpl.java
@@ -27,28 +27,11 @@ import com.liferay.screens.service.base.ScreensRatingsEntryServiceBaseImpl;
 import java.util.List;
 
 /**
- * The implementation of the screens ratings entry remote service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.screens.service.ScreensRatingsEntryService} interface.
- *
- * <p>
- * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
- * </p>
- *
  * @author Alejandro Hernández Malillos
- * @see ScreensRatingsEntryServiceBaseImpl
- * @see com.liferay.screens.service.ScreensRatingsEntryServiceUtil
  */
 @ProviderType
 public class ScreensRatingsEntryServiceImpl
 	extends ScreensRatingsEntryServiceBaseImpl {
-
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Always use {@link com.liferay.screens.service.ScreensRatingsEntryServiceUtil} to access the screens ratings entry remote service.
-	 */
 
 	public JSONObject deleteRatingEntry(
 			long classPK, String className, int stepCount)
