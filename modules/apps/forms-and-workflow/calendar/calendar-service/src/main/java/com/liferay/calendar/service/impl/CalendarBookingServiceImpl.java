cffd0c34e257c0a91637b5ae40419b09dcdfed15
Brian Chan <brian.chan@liferay.com>
2016-07-11 13:54:20 -0700

    LPS-62735 Hugo SF. We have 7.0.0, packageinfo, and BND that may all point to different numbers. For now, let's make them always follow the major 7.0.0 version. Thx.

--- modules/apps/forms-and-workflow/calendar/calendar-service/src/main/java/com/liferay/calendar/service/impl/CalendarBookingServiceImpl.java
+++ modules/apps/forms-and-workflow/calendar/calendar-service/src/main/java/com/liferay/calendar/service/impl/CalendarBookingServiceImpl.java
@@ -124,7 +124,7 @@ public class CalendarBookingServiceImpl extends CalendarBookingServiceBaseImpl {
 	}
 
 	/**
-	 * @deprecated As of 2.1.0, replaced by {@link #addCalendarBooking(long,
+	 * @deprecated As of 7.0.0, replaced by {@link #addCalendarBooking(long,
 	 *             long[], long, long, Map, Map, String, long, long, boolean,
 	 *             String, long, String, long, String, ServiceContext)}
 	 */
