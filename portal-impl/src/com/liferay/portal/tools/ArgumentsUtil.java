bc08e14634c66ff7721a8e4ecec56fe99a186859
Hugo Huijser <hugo.huijser@liferay.com>
2016-07-12 03:54:35 +0800

    LPS-66853 Add methods to retrieve values from arguments

--- portal-impl/src/com/liferay/portal/tools/ArgumentsUtil.java
+++ portal-impl/src/com/liferay/portal/tools/ArgumentsUtil.java
@@ -15,6 +15,7 @@
 package com.liferay.portal.tools;
 
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Map;
 
@@ -22,8 +23,45 @@ import java.util.Map;
  * @author Shuyang Zhou
  * @author Raymond Augé
  * @author Gregory Amerson
+ * @author Hugo Huijser
  */
 public class ArgumentsUtil {
+
+	public static boolean getBoolean(
+		Map<String, String> arguments, String key, boolean defaultValue) {
+
+		String value = arguments.get(key);
+
+		if (Validator.isNull(value) || value.startsWith("$")) {
+			return defaultValue;
+		}
+
+		return GetterUtil.getBoolean(value);
+	}
+
+	public static int getInteger(
+		Map<String, String> arguments, String key, int defaultValue) {
+
+		String value = arguments.get(key);
+
+		if (Validator.isNull(value) || value.startsWith("$")) {
+			return defaultValue;
+		}
+
+		return GetterUtil.getInteger(value);
+	}
+
+	public static String getString(
+		Map<String, String> arguments, String key, String defaultValue) {
+
+		String value = arguments.get(key);
+
+		if (Validator.isNull(value) || value.startsWith("$")) {
+			return defaultValue;
+		}
+
+		return value;
+	}
 
 	public static Map<String, String> parseArguments(String[] args) {
 		Map<String, String> arguments = new ArgumentsMap();
