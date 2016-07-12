c1037e865a77a816ad432134fb20289a7ddf0bdd
Michael Han <michael.han@liferay.com>
2016-07-12 08:50:53 -0500

    LPS-67045 add facilities to export task forms

--- modules/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-definition-impl/src/main/java/com/liferay/portal/workflow/kaleo/definition/internal/export/TaskNodeExporter.java
+++ modules/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-definition-impl/src/main/java/com/liferay/portal/workflow/kaleo/definition/internal/export/TaskNodeExporter.java
@@ -14,10 +14,16 @@
 
 package com.liferay.portal.workflow.kaleo.definition.internal.export;
 
+import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.workflow.kaleo.definition.Node;
 import com.liferay.portal.workflow.kaleo.definition.Task;
+import com.liferay.portal.workflow.kaleo.definition.TaskForm;
+import com.liferay.portal.workflow.kaleo.definition.TaskFormReference;
 import com.liferay.portal.workflow.kaleo.definition.export.NodeExporter;
+
+import java.util.Set;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -44,7 +50,61 @@ public class TaskNodeExporter extends BaseNodeExporter implements NodeExporter {
 		exportAssignmentsElement(
 			task.getAssignments(), nodeElement, "assignments");
 
+		exportTaskForms(task.getTaskForms(), nodeElement, "task-forms");
+
 		exportTimersElement(task, nodeElement, "task-timers", "task-timer");
+	}
+
+	protected void exportTaskForms(
+		Set<TaskForm> taskForms, Element parentElement,
+		String taskFormsElementName) {
+
+		if (SetUtil.isEmpty(taskForms)) {
+			return;
+		}
+
+		Element taskFormsElement = parentElement.addElement(
+			taskFormsElementName);
+
+		for (TaskForm taskForm : taskForms) {
+			Element taskFormElement = taskFormsElement.addElement("task-form");
+
+			addTextElement(taskFormElement, "name", taskForm.getName());
+			addTextElement(
+				taskFormElement, "description", taskForm.getDescription());
+
+			if (Validator.isNotNull(taskForm.getFormDefinition())) {
+				addTextElement(
+					taskFormElement, "form-definition",
+					taskForm.getFormDefinition());
+			}
+			else if (taskForm.getTaskFormReference() != null) {
+				TaskFormReference taskFormReference =
+					taskForm.getTaskFormReference();
+
+				Element formReferenceElement = taskFormElement.addElement(
+					"form-reference");
+
+				addTextElement(
+					formReferenceElement, "company-id",
+					String.valueOf(taskFormReference.getCompanyId()));
+				addTextElement(
+					formReferenceElement, "form-id",
+					String.valueOf(taskFormReference.getFormId()));
+				addTextElement(
+					formReferenceElement, "form-uuid",
+					taskFormReference.getFormUuid());
+				addTextElement(
+					formReferenceElement, "group-id",
+					String.valueOf(taskFormReference.getGroupId()));
+
+			}
+			addCDataElement(
+				taskFormElement, "metadata", taskForm.getMetadata());
+			addTextElement(
+				taskFormElement, "priority",
+				String.valueOf(taskForm.getPriority()));
+		}
 	}
 
 }
--- modules/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-definition-impl/src/main/java/com/liferay/portal/workflow/kaleo/definition/internal/export/builder/TaskNodeBuilder.java
+++ modules/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-definition-impl/src/main/java/com/liferay/portal/workflow/kaleo/definition/internal/export/builder/TaskNodeBuilder.java
@@ -15,13 +15,20 @@
 package com.liferay.portal.workflow.kaleo.definition.internal.export.builder;
 
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.workflow.kaleo.definition.Assignment;
 import com.liferay.portal.workflow.kaleo.definition.Task;
+import com.liferay.portal.workflow.kaleo.definition.TaskForm;
+import com.liferay.portal.workflow.kaleo.definition.TaskFormReference;
 import com.liferay.portal.workflow.kaleo.model.KaleoNode;
 import com.liferay.portal.workflow.kaleo.model.KaleoTask;
+import com.liferay.portal.workflow.kaleo.model.KaleoTaskForm;
+import com.liferay.portal.workflow.kaleo.service.KaleoTaskFormLocalService;
 import com.liferay.portal.workflow.kaleo.service.KaleoTaskLocalService;
 
+import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -35,6 +42,42 @@ import org.osgi.service.component.annotations.Reference;
 public class TaskNodeBuilder
 	extends BaseNodeBuilder<Task> implements NodeBuilder {
 
+	protected Set<TaskForm> buildTaskForms(String name, long kaleoTaskId)
+		throws PortalException {
+
+		List<KaleoTaskForm> kaleoTaskForms =
+			_kaleoTaskFormLocalService.getKaleoTaskForms(kaleoTaskId);
+
+		Set<TaskForm> taskForms = new TreeSet<>();
+
+		for (KaleoTaskForm kaleoTaskForm : kaleoTaskForms) {
+			TaskForm taskForm = new TaskForm(
+				kaleoTaskForm.getName(), kaleoTaskForm.getPriority());
+
+			taskForm.setDescription(kaleoTaskForm.getDescription());
+			taskForm.setMetadata(kaleoTaskForm.getMetadata());
+
+			if (Validator.isNotNull(kaleoTaskForm.getFormDefinition())) {
+				taskForm.setFormDefinition(kaleoTaskForm.getFormDefinition());
+			}
+			else {
+				TaskFormReference taskFormReference = new TaskFormReference();
+
+				taskFormReference.setCompanyId(
+					kaleoTaskForm.getFormCompanyId());
+				taskFormReference.setFormUuid(kaleoTaskForm.getFormUuid());
+				taskFormReference.setFormId(kaleoTaskForm.getFormId());
+				taskFormReference.setGroupId(kaleoTaskForm.getFormGroupId());
+
+				taskForm.setTaskFormReference(taskFormReference);
+			}
+
+			taskForms.add(taskForm);
+		}
+
+		return taskForms;
+	}
+
 	@Override
 	protected Task createNode(KaleoNode kaleoNode) throws PortalException {
 		KaleoTask kaleoTask = _kaleoTaskLocalService.getKaleoNodeKaleoTask(
@@ -47,8 +90,16 @@ public class TaskNodeBuilder
 
 		task.setAssignments(assignments);
 
+		Set<TaskForm> taskForms = buildTaskForms(
+			KaleoTask.class.getName(), kaleoTask.getKaleoTaskId());
+
+		task.addTaskForms(taskForms);
+
 		return task;
 	}
+
+	@Reference
+	private KaleoTaskFormLocalService _kaleoTaskFormLocalService;
 
 	@Reference
 	private KaleoTaskLocalService _kaleoTaskLocalService;
