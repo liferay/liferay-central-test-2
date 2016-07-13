8afb8fd746078cf39b0c6cd52c316b13ea44c9c9
Brian Chan <brian.chan@liferay.com>
2016-07-13 08:29:25 -0700

    Revert "LPS-60033 SF"
    This reverts commit 47c0503a290bb098da961239f2422dc21d885d06.

--- modules/apps/foundation/frontend-editor/frontend-editor-alloyeditor-web/src/main/java/com/liferay/frontend/editor/alloyeditor/web/editor/configuration/AlloyEditorBBCodeConfigContributor.java
+++ modules/apps/foundation/frontend-editor/frontend-editor-alloyeditor-web/src/main/java/com/liferay/frontend/editor/alloyeditor/web/editor/configuration/AlloyEditorBBCodeConfigContributor.java
@@ -24,6 +24,7 @@ import com.liferay.portal.kernel.parsers.bbcode.BBCodeTranslatorUtil;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.AggregateResourceBundleLoader;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ResourceBundleLoader;
