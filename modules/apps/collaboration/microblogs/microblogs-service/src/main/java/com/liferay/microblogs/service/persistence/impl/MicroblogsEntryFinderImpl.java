367f7f099aa4a5d17d908ea6353456195c66cc1d
Adolfo Pérez <adolfo.perez@liferay.com>
2016-07-13 16:40:15 +0200

    LPS-66986 Add finder methods and deprecate
    Add methods for the new queries and deprecate the old ones.

--- modules/apps/collaboration/microblogs/microblogs-service/src/main/java/com/liferay/microblogs/service/persistence/impl/MicroblogsEntryFinderImpl.java
+++ modules/apps/collaboration/microblogs/microblogs-service/src/main/java/com/liferay/microblogs/service/persistence/impl/MicroblogsEntryFinderImpl.java
@@ -41,17 +41,29 @@ public class MicroblogsEntryFinderImpl
 	public static final String COUNT_BY_USER_ID =
 		MicroblogsEntryFinder.class.getName() + ".countByUserId";
 
+	public static final String COUNT_BY_C_U =
+		MicroblogsEntryFinder.class.getName() + ".countByC_U";
+
 	public static final String COUNT_BY_U_MU =
 		MicroblogsEntryFinder.class.getName() + ".countByU_MU";
 
 	public static final String COUNT_BY_U_ATN =
 		MicroblogsEntryFinder.class.getName() + ".countByU_ATN";
 
+	public static final String COUNT_BY_C_U_ATN =
+		MicroblogsEntryFinder.class.getName() + ".countByC_U_ATN";
+
 	public static final String COUNT_BY_CCNI_ATN =
 		MicroblogsEntryFinder.class.getName() + ".countByCCNI_ATN";
 
+	public static final String COUNT_BY_C_CCNI_ATN =
+		MicroblogsEntryFinder.class.getName() + ".countByC_CCNI_ATN";
+
 	public static final String COUNT_BY_U_T_MU =
 		MicroblogsEntryFinder.class.getName() + ".countByU_T_MU";
+
+	public static final String COUNT_BY_C_CCNI_CCPK_ATN =
+		MicroblogsEntryFinder.class.getName() + ".countByC_CCNI_CCPK_ATN";
 
 	public static final String COUNT_BY_CCNI_CCPK_ATN =
 		MicroblogsEntryFinder.class.getName() + ".countByCCNI_CCPK_ATN";
@@ -59,14 +71,23 @@ public class MicroblogsEntryFinderImpl
 	public static final String FIND_BY_USER_ID =
 		MicroblogsEntryFinder.class.getName() + ".findByUserId";
 
+	public static final String FIND_BY_C_U =
+		MicroblogsEntryFinder.class.getName() + ".findByC_U";
+
 	public static final String FIND_BY_U_MU =
 		MicroblogsEntryFinder.class.getName() + ".findByU_MU";
 
 	public static final String FIND_BY_U_ATN =
 		MicroblogsEntryFinder.class.getName() + ".findByU_ATN";
 
+	public static final String FIND_BY_C_U_ATN =
+		MicroblogsEntryFinder.class.getName() + ".findByC_U_ATN";
+
 	public static final String FIND_BY_CCNI_ATN =
 		MicroblogsEntryFinder.class.getName() + ".findByCCNI_ATN";
+
+	public static final String FIND_BY_C_CCNI_ATN =
+		MicroblogsEntryFinder.class.getName() + ".findByC_CCNI_ATN";
 
 	public static final String FIND_BY_U_T_MU =
 		MicroblogsEntryFinder.class.getName() + ".findByU_T_MU";
@@ -74,6 +95,13 @@ public class MicroblogsEntryFinderImpl
 	public static final String FIND_BY_CCNI_CCPK_ATN =
 		MicroblogsEntryFinder.class.getName() + ".findByCCNI_CCPK_ATN";
 
+	public static final String FIND_BY_C_CCNI_CCPK_ATN =
+		MicroblogsEntryFinder.class.getName() + ".findByC_CCNI_CCPK_ATN";
+
+	/**
+	 * @deprecated As of 7.0.2, replaced by {@link #countByC_U(long, long)}
+	 */
+	@Deprecated
 	@Override
 	public int countByUserId(long userId) {
 		Session session = null;
@@ -89,6 +117,49 @@ public class MicroblogsEntryFinderImpl
 
 			QueryPos qPos = QueryPos.getInstance(q);
 
+			qPos.add(MicroblogsEntryConstants.TYPE_EVERYONE);
+			qPos.add(userId);
+			qPos.add(SocialRelationConstants.TYPE_UNI_ENEMY);
+			qPos.add(userId);
+			qPos.add(userId);
+			qPos.add(MicroblogsEntryConstants.TYPE_REPLY);
+
+			Iterator<Long> itr = q.iterate();
+
+			if (itr.hasNext()) {
+				Long count = itr.next();
+
+				if (count != null) {
+					return count.intValue();
+				}
+			}
+
+			return 0;
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
+
+	@Override
+	public int countByC_U(long companyId, long userId) {
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = CustomSQLUtil.get(getClass(), COUNT_BY_C_U);
+
+			SQLQuery q = session.createSynchronizedSQLQuery(sql);
+
+			q.addScalar(COUNT_COLUMN_NAME, Type.LONG);
+
+			QueryPos qPos = QueryPos.getInstance(q);
+
+			qPos.add(companyId);
 			qPos.add(MicroblogsEntryConstants.TYPE_EVERYONE);
 			qPos.add(userId);
 			qPos.add(SocialRelationConstants.TYPE_UNI_ENEMY);
@@ -156,6 +227,10 @@ public class MicroblogsEntryFinderImpl
 		}
 	}
 
+	/**
+	 * @deprecated As of 7.0.2, replaced by {@link #countByC_U_ATN(long, long,
+	 *             String)}
+	 */
 	@Override
 	public int countByU_ATN(long userId, String assetTagName) {
 		Session session = null;
@@ -198,6 +273,55 @@ public class MicroblogsEntryFinderImpl
 	}
 
 	@Override
+	public int countByC_U_ATN(
+		long companyId, long userId, String assetTagName) {
+
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = CustomSQLUtil.get(getClass(), COUNT_BY_C_U_ATN);
+
+			SQLQuery q = session.createSynchronizedSQLQuery(sql);
+
+			q.addScalar(COUNT_COLUMN_NAME, Type.LONG);
+
+			QueryPos qPos = QueryPos.getInstance(q);
+
+			qPos.add(companyId);
+			qPos.add(MicroblogsEntryConstants.TYPE_EVERYONE);
+			qPos.add(userId);
+			qPos.add(assetTagName);
+			qPos.add(userId);
+			qPos.add(assetTagName);
+
+			Iterator<Long> itr = q.iterate();
+
+			if (itr.hasNext()) {
+				Long count = itr.next();
+
+				if (count != null) {
+					return count.intValue();
+				}
+			}
+
+			return 0;
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
+
+	/**
+	 * @deprecated As of 7.0.2, replaced by {@link #countByC_CCNI_ATN(long,
+	 *             long, String)}
+	 */
+	@Deprecated
+	@Override
 	public int countByCCNI_ATN(long creatorClassNameId, String assetTagName) {
 		Session session = null;
 
@@ -212,6 +336,47 @@ public class MicroblogsEntryFinderImpl
 
 			QueryPos qPos = QueryPos.getInstance(q);
 
+			qPos.add(creatorClassNameId);
+			qPos.add(assetTagName);
+
+			Iterator<Long> itr = q.iterate();
+
+			if (itr.hasNext()) {
+				Long count = itr.next();
+
+				if (count != null) {
+					return count.intValue();
+				}
+			}
+
+			return 0;
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
+
+	@Override
+	public int countByC_CCNI_ATN(
+		long companyId, long creatorClassNameId, String assetTagName) {
+
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = CustomSQLUtil.get(getClass(), COUNT_BY_C_CCNI_ATN);
+
+			SQLQuery q = session.createSynchronizedSQLQuery(sql);
+
+			q.addScalar(COUNT_COLUMN_NAME, Type.LONG);
+
+			QueryPos qPos = QueryPos.getInstance(q);
+
+			qPos.add(companyId);
 			qPos.add(creatorClassNameId);
 			qPos.add(assetTagName);
 
@@ -277,6 +442,11 @@ public class MicroblogsEntryFinderImpl
 		}
 	}
 
+	/**
+	 * @deprecated As of 7.0.2, replaced by {@link #countByC_CCNI_ATN(long,
+	 *             long, String)}
+	 */
+	@Deprecated
 	@Override
 	public int countByCCNI_CCPK_ATN(
 		long creatorClassNameId, long creatorClassPK, String assetTagName,
@@ -322,6 +492,57 @@ public class MicroblogsEntryFinderImpl
 	}
 
 	@Override
+	public int countByC_CCNI_CCPK_ATN(
+		long companyId, long creatorClassNameId, long creatorClassPK,
+		String assetTagName, boolean andOperator) {
+
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = CustomSQLUtil.get(
+				getClass(), COUNT_BY_C_CCNI_CCPK_ATN);
+
+			sql = CustomSQLUtil.replaceAndOperator(sql, andOperator);
+
+			SQLQuery q = session.createSynchronizedSQLQuery(sql);
+
+			q.addScalar(COUNT_COLUMN_NAME, Type.LONG);
+
+			QueryPos qPos = QueryPos.getInstance(q);
+
+			qPos.add(companyId);
+			qPos.add(creatorClassNameId);
+			qPos.add(creatorClassPK);
+			qPos.add(assetTagName);
+
+			Iterator<Long> itr = q.iterate();
+
+			if (itr.hasNext()) {
+				Long count = itr.next();
+
+				if (count != null) {
+					return count.intValue();
+				}
+			}
+
+			return 0;
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
+
+	/**
+	 * @deprecated As of 7.0.2, replaced by {@link #findByC_U(long, long, int,
+	 *             int)}
+	 */
+	@Deprecated
+	@Override
 	public List<MicroblogsEntry> findByUserId(long userId, int start, int end) {
 		Session session = null;
 
@@ -336,6 +557,52 @@ public class MicroblogsEntryFinderImpl
 
 			QueryPos qPos = QueryPos.getInstance(q);
 
+			qPos.add(MicroblogsEntryConstants.TYPE_EVERYONE);
+			qPos.add(userId);
+			qPos.add(SocialRelationConstants.TYPE_UNI_ENEMY);
+			qPos.add(userId);
+			qPos.add(userId);
+			qPos.add(MicroblogsEntryConstants.TYPE_REPLY);
+
+			Iterator<Long> itr = (Iterator<Long>)QueryUtil.iterate(
+				q, getDialect(), start, end);
+
+			List<MicroblogsEntry> microblogsEntries = new ArrayList<>();
+
+			while (itr.hasNext()) {
+				microblogsEntries.add(
+					MicroblogsEntryLocalServiceUtil.fetchMicroblogsEntry(
+						(Long)itr.next()));
+			}
+
+			return microblogsEntries;
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
+
+	@Override
+	public List<MicroblogsEntry> findByC_U(
+		long companyId, long userId, int start, int end) {
+
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = CustomSQLUtil.get(getClass(), FIND_BY_C_U);
+
+			SQLQuery q = session.createSynchronizedSQLQuery(sql);
+
+			q.addScalar("microblogsEntryId", Type.LONG);
+
+			QueryPos qPos = QueryPos.getInstance(q);
+
+			qPos.add(companyId);
 			qPos.add(MicroblogsEntryConstants.TYPE_EVERYONE);
 			qPos.add(userId);
 			qPos.add(SocialRelationConstants.TYPE_UNI_ENEMY);
@@ -397,6 +664,11 @@ public class MicroblogsEntryFinderImpl
 		}
 	}
 
+	/**
+	 * @deprecated As of 7.0.2, replaced by {@link #findByC_U_ATN(long, long,
+	 *             String, int, int)}
+	 */
+	@Deprecated
 	@Override
 	public List<MicroblogsEntry> findByU_ATN(
 		long userId, String assetTagName, int start, int end) {
@@ -432,6 +704,46 @@ public class MicroblogsEntryFinderImpl
 	}
 
 	@Override
+	public List<MicroblogsEntry> findByC_U_ATN(
+		long companyId, long userId, String assetTagName, int start, int end) {
+
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = CustomSQLUtil.get(getClass(), FIND_BY_C_U_ATN);
+
+			SQLQuery q = session.createSynchronizedSQLQuery(sql);
+
+			q.addEntity("MicroblogsEntry", MicroblogsEntryImpl.class);
+
+			QueryPos qPos = QueryPos.getInstance(q);
+
+			qPos.add(companyId);
+			qPos.add(MicroblogsEntryConstants.TYPE_EVERYONE);
+			qPos.add(userId);
+			qPos.add(assetTagName);
+			qPos.add(userId);
+			qPos.add(assetTagName);
+
+			return (List<MicroblogsEntry>)QueryUtil.list(
+				q, getDialect(), start, end);
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
+
+	/**
+	 * @deprecated As of 7.0.2, replaced by {@link #findByC_CCNI_ATN(long, long,
+	 *             String, int, int)}
+	 */
+	@Deprecated
+	@Override
 	public List<MicroblogsEntry> findByCCNI_ATN(
 		long creatorClassNameId, String assetTagName, int start, int end) {
 
@@ -448,6 +760,39 @@ public class MicroblogsEntryFinderImpl
 
 			QueryPos qPos = QueryPos.getInstance(q);
 
+			qPos.add(creatorClassNameId);
+			qPos.add(assetTagName);
+
+			return (List<MicroblogsEntry>)QueryUtil.list(
+				q, getDialect(), start, end);
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
+
+	@Override
+	public List<MicroblogsEntry> findByC_CCNI_ATN(
+		long companyId, long creatorClassNameId, String assetTagName, int start,
+		int end) {
+
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = CustomSQLUtil.get(getClass(), FIND_BY_C_CCNI_ATN);
+
+			SQLQuery q = session.createSynchronizedSQLQuery(sql);
+
+			q.addEntity("MicroblogsEntry", MicroblogsEntryImpl.class);
+
+			QueryPos qPos = QueryPos.getInstance(q);
+
+			qPos.add(companyId);
 			qPos.add(creatorClassNameId);
 			qPos.add(assetTagName);
 
@@ -495,6 +840,11 @@ public class MicroblogsEntryFinderImpl
 		}
 	}
 
+	/**
+	 * @deprecated As of 7.0.2, replaced by {@link #findByC_CCNI_CCPK_ATN(long,
+	 *             long, long, String, boolean, int, int)}
+	 */
+	@Deprecated
 	@Override
 	public List<MicroblogsEntry> findByCCNI_CCPK_ATN(
 		long creatorClassNameId, long creatorClassPK, String assetTagName,
@@ -515,6 +865,42 @@ public class MicroblogsEntryFinderImpl
 
 			QueryPos qPos = QueryPos.getInstance(q);
 
+			qPos.add(creatorClassNameId);
+			qPos.add(creatorClassPK);
+			qPos.add(assetTagName);
+
+			return (List<MicroblogsEntry>)QueryUtil.list(
+				q, getDialect(), start, end);
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
+
+	@Override
+	public List<MicroblogsEntry> findByC_CCNI_CCPK_ATN(
+		long companyId, long creatorClassNameId, long creatorClassPK,
+		String assetTagName, boolean andOperator, int start, int end) {
+
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = CustomSQLUtil.get(getClass(), FIND_BY_C_CCNI_CCPK_ATN);
+
+			sql = CustomSQLUtil.replaceAndOperator(sql, andOperator);
+
+			SQLQuery q = session.createSynchronizedSQLQuery(sql);
+
+			q.addEntity("MicroblogsEntry", MicroblogsEntryImpl.class);
+
+			QueryPos qPos = QueryPos.getInstance(q);
+
+			qPos.add(companyId);
 			qPos.add(creatorClassNameId);
 			qPos.add(creatorClassPK);
 			qPos.add(assetTagName);
