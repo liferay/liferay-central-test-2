03844d2bb47f4d560b5cbc3237539cf80f744750
drewbrokke <drew.brokke@liferay.com>
2016-07-18 10:16:00 -0700

    LPS-66430 SF - UserGroupFinderConstants - adds KEY to Constants keys

--- portal-impl/src/com/liferay/portal/service/persistence/constants/UserGroupFinderConstants.java
+++ portal-impl/src/com/liferay/portal/service/persistence/constants/UserGroupFinderConstants.java
@@ -19,21 +19,22 @@ package com.liferay.portal.service.persistence.constants;
  */
 public class UserGroupFinderConstants {
 
-	public static final String PARAM_USER_GROUP_GROUP_ROLE =
+	public static final String PARAM_KEY_USER_GROUP_GROUP_ROLE =
 		"userGroupGroupRole";
 
-	public static final String PARAM_USER_GROUPS_GROUPS = "userGroupsGroups";
+	public static final String PARAM_KEY_USER_GROUPS_GROUPS =
+		"userGroupsGroups";
 
-	public static final String PARAM_USER_GROUPS_ROLES = "userGroupsRoles";
+	public static final String PARAM_KEY_USER_GROUPS_ROLES = "userGroupsRoles";
 
-	public static final String PARAM_USER_GROUPS_TEAMS = "userGroupsTeams";
+	public static final String PARAM_KEY_USER_GROUPS_TEAMS = "userGroupsTeams";
 
-	public static final String PARAM_USER_GROUPS_USERS = "userGroupsUsers";
+	public static final String PARAM_KEY_USER_GROUPS_USERS = "userGroupsUsers";
 
-	public static final String[] PARAMS_USER_GROUP_FINDER = {
-		PARAM_USER_GROUP_GROUP_ROLE, PARAM_USER_GROUPS_GROUPS,
-		PARAM_USER_GROUPS_ROLES, PARAM_USER_GROUPS_TEAMS,
-		PARAM_USER_GROUPS_USERS
+	public static final String[] PARAM_KEYS_USER_GROUP_FINDER = {
+		PARAM_KEY_USER_GROUP_GROUP_ROLE, PARAM_KEY_USER_GROUPS_GROUPS,
+		PARAM_KEY_USER_GROUPS_ROLES, PARAM_KEY_USER_GROUPS_TEAMS,
+		PARAM_KEY_USER_GROUPS_USERS
 	};
 
 }
