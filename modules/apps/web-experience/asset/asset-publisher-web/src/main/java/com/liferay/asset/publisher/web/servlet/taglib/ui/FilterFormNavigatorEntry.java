715264e114d4a6716453b3eec27406056bc8edcb
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-07-19 18:16:42 +0200

    LPS-67289 Show filter when we are on dynamic asset selection

--- modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/servlet/taglib/ui/FilterFormNavigatorEntry.java
+++ modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/servlet/taglib/ui/FilterFormNavigatorEntry.java
@@ -15,6 +15,7 @@
 package com.liferay.asset.publisher.web.servlet.taglib.ui;
 
 import com.liferay.asset.publisher.web.constants.AssetPublisherConstants;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.servlet.taglib.ui.FormNavigatorEntry;
 
 import javax.servlet.ServletContext;
@@ -40,6 +41,15 @@ public class FilterFormNavigatorEntry
 	@Override
 	public String getKey() {
 		return "filter";
+	}
+
+	@Override
+	public boolean isVisible(User user, Object object) {
+		if (isDynamicAssetSelection()) {
+			return true;
+		}
+
+		return false;
 	}
 
 	@Override
