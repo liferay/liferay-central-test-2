cd9f1bfadd86bf20fd86739b3ece5a6e3955d10e
Preston-Crary <preston.crary@liferay.com>
2016-07-20 12:08:29 -0700

    LPS-67209 Clear the PermissionCacheUtil whenever an inherited role is changed (GroupModelListener, TeamModelListener and UserGroupGroupRoleModelListener) or direct roles change (UserGroupRoleModelListener and UserModelListener).

---
+++ portal-impl/src/com/liferay/portal/security/permission/TeamModelListener.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.permission;
+
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.Team;
+import com.liferay.portal.kernel.model.User;
+
+/**
+ * @author Preston Crary
+ */
+public class TeamModelListener extends BaseModelListener<Team> {
+
+	@Override
+	public void onAfterAddAssociation(
+		Object classPK, String associationClassName,
+		Object associationClassPK) {
+
+		if (!associationClassName.equals(User.class.getName())) {
+			PermissionCacheUtil.clearCache();
+		}
+	}
+
+	@Override
+	public void onAfterRemove(Team group) {
+		PermissionCacheUtil.clearCache();
+	}
+
+	@Override
+	public void onAfterRemoveAssociation(
+		Object classPK, String associationClassName,
+		Object associationClassPK) {
+
+		if (!associationClassName.equals(User.class.getName())) {
+			PermissionCacheUtil.clearCache();
+		}
+	}
+
+}
