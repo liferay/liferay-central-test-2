b4f6e742ca8e8848464b55bf74b774adf05ae987
Máté Thurzó <mate.thurzo@liferay.com>
2016-07-20 11:06:14 +0200

    LPS-66930 Move roles related data handlers to the proper package

--- modules/apps/foundation/roles/roles-admin-impl/src/main/java/com/liferay/roles/admin/internal/exportimport/data/handler/ImportExportPermissionConversionFilter.java
+++ modules/apps/foundation/roles/roles-admin-impl/src/main/java/com/liferay/roles/admin/internal/exportimport/data/handler/ImportExportPermissionConversionFilter.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.roles.admin.internal.lar;
+package com.liferay.roles.admin.internal.exportimport.data.handler;
 
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.ResourceConstants;
--- modules/apps/foundation/roles/roles-admin-impl/src/main/java/com/liferay/roles/admin/internal/exportimport/data/handler/RolesAdminPortletDataHandler.java
+++ modules/apps/foundation/roles/roles-admin-impl/src/main/java/com/liferay/roles/admin/internal/exportimport/data/handler/RolesAdminPortletDataHandler.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.roles.admin.internal.lar;
+package com.liferay.roles.admin.internal.exportimport.data.handler;
 
 import com.liferay.exportimport.kernel.lar.BasePortletDataHandler;
 import com.liferay.exportimport.kernel.lar.DataLevel;
