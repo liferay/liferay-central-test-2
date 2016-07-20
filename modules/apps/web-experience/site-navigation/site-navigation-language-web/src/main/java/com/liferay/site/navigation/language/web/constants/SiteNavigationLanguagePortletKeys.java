69f462831bbbef5758babd09ebb6bedd9cc6cc4a
tomwang2011 <tong.wang@liferay.com>
2016-07-20 14:34:55 -0700

    LPS-65012 Site-Navigation move constant class back and export the package

--- modules/apps/web-experience/site-navigation/site-navigation-language-web/src/main/java/com/liferay/site/navigation/language/web/constants/SiteNavigationLanguagePortletKeys.java
+++ modules/apps/web-experience/site-navigation/site-navigation-language-web/src/main/java/com/liferay/site/navigation/language/web/constants/SiteNavigationLanguagePortletKeys.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.site.navigation.language.web.internal.constants;
+package com.liferay.site.navigation.language.web.constants;
 
 /**
  * @author Eudaldo Alonso
