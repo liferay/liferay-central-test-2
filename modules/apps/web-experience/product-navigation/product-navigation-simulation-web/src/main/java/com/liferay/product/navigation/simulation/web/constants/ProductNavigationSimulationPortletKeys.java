ed18da6f5b362850cc5033e68f11dfb5971b2675
tomwang2011 <tong.wang@liferay.com>
2016-07-20 11:14:10 -0700

    LPS-65012 Product Navigation move constant class back and export the package

--- modules/apps/web-experience/product-navigation/product-navigation-simulation-web/src/main/java/com/liferay/product/navigation/simulation/web/constants/ProductNavigationSimulationPortletKeys.java
+++ modules/apps/web-experience/product-navigation/product-navigation-simulation-web/src/main/java/com/liferay/product/navigation/simulation/web/constants/ProductNavigationSimulationPortletKeys.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.product.navigation.simulation.web.internal.constants;
+package com.liferay.product.navigation.simulation.web.constants;
 
 /**
  * @author Eduardo Garcia
