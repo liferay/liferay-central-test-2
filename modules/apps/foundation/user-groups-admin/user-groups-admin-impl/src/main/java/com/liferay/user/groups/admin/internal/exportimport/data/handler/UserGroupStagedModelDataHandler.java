6fdd87d8f033df87f66bde4247398fa0e503089a
Máté Thurzó <mate.thurzo@liferay.com>
2016-07-22 13:04:07 +0200

    LPS-66931 Move User groups admin data handlers to the correct packages

--- modules/apps/foundation/user-groups-admin/user-groups-admin-impl/src/main/java/com/liferay/user/groups/admin/internal/exportimport/data/handler/UserGroupStagedModelDataHandler.java
+++ modules/apps/foundation/user-groups-admin/user-groups-admin-impl/src/main/java/com/liferay/user/groups/admin/internal/exportimport/data/handler/UserGroupStagedModelDataHandler.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.user.groups.admin.internal.lar;
+package com.liferay.user.groups.admin.internal.exportimport.data.handler;
 
 import com.liferay.exportimport.kernel.lar.ExportImportPathUtil;
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
