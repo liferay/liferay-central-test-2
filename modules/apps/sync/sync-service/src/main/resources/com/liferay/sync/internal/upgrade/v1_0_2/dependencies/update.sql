57c5e1a6d22e50528dd1c9ed3f86e4ab27bfda3a
Shinn Lok <shinn.lok@liferay.com>
2016-07-22 13:32:51 -0700

    SYNC-1569 Fix upgrade

--- modules/apps/sync/sync-service/src/main/resources/com/liferay/sync/internal/upgrade/v1_0_2/dependencies/update.sql
+++ modules/apps/sync/sync-service/src/main/resources/com/liferay/sync/internal/upgrade/v1_0_2/dependencies/update.sql
@@ -65,7 +65,6 @@ create table SyncDevice (
 	userName VARCHAR(75) null,
 	createDate DATE null,
 	modifiedDate DATE null,
-	hostName VARCHAR(75) null,
 	type_ VARCHAR(75) null,
 	buildNumber LONG,
 	featureSet INTEGER,
--- modules/apps/sync/sync-service/src/main/resources/com/liferay/sync/internal/upgrade/v1_0_3/dependencies/update.sql
+++ modules/apps/sync/sync-service/src/main/resources/com/liferay/sync/internal/upgrade/v1_0_3/dependencies/update.sql
@@ -1,3 +1,3 @@
-alter table SyncDevice add host VARCHAR(75) null;
+alter table SyncDevice add hostname VARCHAR(75) null;
 
 COMMIT_TRANSACTION;
