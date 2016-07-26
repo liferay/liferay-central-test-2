0aaaa26fcfb64364457f65b49974bc54ed0135fd
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-07-26 13:40:18 +0200

    LPS-67289 Adds scope form navigator entry

---
+++ modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/servlet/taglib/ui/ScopeFormNavigatorEntry.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.publisher.web.servlet.taglib.ui;
+
+import com.liferay.asset.publisher.web.constants.AssetPublisherConstants;
+import com.liferay.asset.publisher.web.constants.AssetPublisherPortletKeys;
+import com.liferay.portal.kernel.model.PortletConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.servlet.taglib.ui.FormNavigatorEntry;
+import com.liferay.portal.kernel.theme.PortletDisplay;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import javax.portlet.PortletPreferences;
+import javax.servlet.ServletContext;
+import java.util.Objects;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	property = {"form.navigator.entry.order:Integer=400"},
+	service = FormNavigatorEntry.class
+)
+public class ScopeFormNavigatorEntry
+	extends BaseConfigurationFormNavigatorEntry {
+
+	@Override
+	public String getCategoryKey() {
+		return AssetPublisherConstants.CATEGORY_KEY_ASSET_SELECTION;
+	}
+
+	@Override
+	public String getKey() {
+		return "scope";
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.asset.publisher.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+	@Override
+	protected String getJspPath() {
+		return "/configuration/scope.jsp";
+	}
+
+}
