dbcd18bc2ab8606ba9373bc9d5170c501169a1fa
tomwang2011 <tong.wang@liferay.com>
2016-07-26 14:18:03 -0700

    LPS-67049 MDR export constant class used by integration tests

--- modules/apps/foundation/mobile-device-rules/mobile-device-rules-service/src/main/java/com/liferay/mobile/device/rules/exportimport/data/handler/MDRPortletDataHandler.java
+++ modules/apps/foundation/mobile-device-rules/mobile-device-rules-service/src/main/java/com/liferay/mobile/device/rules/exportimport/data/handler/MDRPortletDataHandler.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.mobile.device.rules.internal.exportimport.data.handler;
+package com.liferay.mobile.device.rules.exportimport.data.handler;
 
 import com.liferay.exportimport.kernel.lar.BasePortletDataHandler;
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
