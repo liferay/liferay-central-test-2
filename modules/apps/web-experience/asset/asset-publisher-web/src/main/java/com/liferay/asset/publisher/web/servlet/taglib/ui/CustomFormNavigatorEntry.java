1adcd9b6efcd327151d9c6380dede372155957fb
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-07-26 13:13:57 +0200

    LPS-67289 Show custom navigator entry only when we are on dynamic asset selection

--- modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/servlet/taglib/ui/CustomFormNavigatorEntry.java
+++ modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/servlet/taglib/ui/CustomFormNavigatorEntry.java
@@ -15,6 +15,7 @@
 package com.liferay.asset.publisher.web.servlet.taglib.ui;
 
 import com.liferay.asset.publisher.web.constants.AssetPublisherConstants;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.servlet.taglib.ui.FormNavigatorEntry;
 
 import javax.servlet.ServletContext;
@@ -40,6 +41,11 @@ public class CustomFormNavigatorEntry
 	@Override
 	public String getKey() {
 		return "custom";
+	}
+
+	@Override
+	public boolean isVisible(User user, Object object) {
+		return isDynamicAssetSelection();
 	}
 
 	@Override
