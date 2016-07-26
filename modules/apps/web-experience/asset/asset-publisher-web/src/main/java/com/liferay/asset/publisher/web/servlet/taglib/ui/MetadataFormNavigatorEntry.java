7afb87a5c2dbc2272faec414e265516b3e938335
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-07-26 13:40:06 +0200

    LPS-67289 Adds metadata form navigator entry

---
+++ modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/servlet/taglib/ui/MetadataFormNavigatorEntry.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.publisher.web.servlet.taglib.ui;
+
+import com.liferay.asset.publisher.web.constants.AssetPublisherConstants;
+import com.liferay.portal.kernel.servlet.taglib.ui.FormNavigatorEntry;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	property = {"form.navigator.entry.order:Integer=100"},
+	service = FormNavigatorEntry.class
+)
+public class MetadataFormNavigatorEntry
+	extends BaseConfigurationFormNavigatorEntry {
+
+	@Override
+	public String getCategoryKey() {
+		return AssetPublisherConstants.CATEGORY_KEY_DISPLAY_SETTINGS;
+	}
+
+	@Override
+	public String getKey() {
+		return "metadata";
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.asset.publisher.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+	@Override
+	protected String getJspPath() {
+		return "/configuration/metadata.jsp";
+	}
+
+}
