b5fed9b1d0ca50ba145f07d57683050480426140
Dennis Ju <dennis.ju@liferay.com>
2016-07-26 13:06:09 -0700

    SYNC-1651 Escape special chars

--- modules/apps/sync/sync-engine/src/main/java/com/liferay/sync/engine/service/persistence/SyncAccountPersistence.java
+++ modules/apps/sync/sync-engine/src/main/java/com/liferay/sync/engine/service/persistence/SyncAccountPersistence.java
@@ -17,6 +17,7 @@ package com.liferay.sync.engine.service.persistence;
 import com.j256.ormlite.dao.GenericRawResults;
 import com.j256.ormlite.dao.RawRowMapper;
 import com.j256.ormlite.stmt.QueryBuilder;
+import com.j256.ormlite.stmt.SelectArg;
 import com.j256.ormlite.stmt.Where;
 
 import com.liferay.sync.engine.model.SyncAccount;
@@ -83,7 +84,7 @@ public class SyncAccountPersistence
 
 		Where<SyncAccount, Long> where = queryBuilder.where();
 
-		where.eq("filePathName", filePathName);
+		where.eq("filePathName", new SelectArg(filePathName));
 
 		where.and(1);
 
