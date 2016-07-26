88724fdd4573ef58cab759f461b40def49b7989e
tomwang2011 <tong.wang@liferay.com>
2016-07-26 16:44:51 -0700

    LPS-67049 DL fix unit test package

--- modules/apps/collaboration/document-library/document-library-test/src/test/java/com/liferay/document/library/internal/search/SearchResultUtilDLFileEntryTest.java
+++ modules/apps/collaboration/document-library/document-library-test/src/test/java/com/liferay/document/library/internal/search/SearchResultUtilDLFileEntryTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.document.library.search;
+package com.liferay.document.library.internal.search;
 
 import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
 import com.liferay.asset.kernel.model.AssetRendererFactory;
