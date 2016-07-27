c8a0e70777435210424a7d9d46f561956b04f81b
Pei-Jung Lan <pei-jung.lan@liferay.com>
2016-07-27 15:23:48 -0700

    LPS-67399 Add finder method to get all organizations and users belonging to the parent organization

--- portal-impl/src/com/liferay/portal/service/persistence/impl/OrganizationFinderImpl.java
+++ portal-impl/src/com/liferay/portal/service/persistence/impl/OrganizationFinderImpl.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.service.persistence.impl;
 
+import com.liferay.portal.kernel.dao.orm.QueryDefinition;
 import com.liferay.portal.kernel.dao.orm.QueryPos;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.dao.orm.SQLQuery;
@@ -23,11 +24,13 @@ import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.model.Organization;
 import com.liferay.portal.kernel.service.persistence.OrganizationFinder;
 import com.liferay.portal.kernel.service.persistence.OrganizationUtil;
+import com.liferay.portal.kernel.service.persistence.UserUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.model.impl.OrganizationImpl;
 import com.liferay.util.dao.orm.CustomSQLUtil;
 
@@ -68,11 +71,17 @@ public class OrganizationFinderImpl
 	public static final String FIND_O_BY_C_P =
 		OrganizationFinder.class.getName() + ".findO_ByC_P";
 
+	public static final String FIND_O_BY_C_PO =
+		OrganizationFinder.class.getName() + ".findO_ByC_PO";
+
 	public static final String FIND_O_BY_C_PO_N_S_C_Z_R_C =
 		OrganizationFinder.class.getName() + ".findO_ByC_PO_N_S_C_Z_R_C";
 
 	public static final String FIND_O_BY_C_PO_N_L_S_C_Z_R_C =
 		OrganizationFinder.class.getName() + ".findO_ByC_PO_N_L_S_C_Z_R_C";
+
+	public static final String FIND_U_BY_C_S_O =
+		OrganizationFinder.class.getName() + ".findU_ByC_S_O";
 
 	public static final String JOIN_O_BY_ORGANIZATIONS_GROUPS =
 		OrganizationFinder.class.getName() + ".joinO_ByOrganizationsGroups";
@@ -689,6 +698,86 @@ public class OrganizationFinderImpl
 			}
 
 			return organizations;
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
+
+	@Override
+	public List<Object> findO_U_ByC_P(
+		long companyId, long parentOrganizationId,
+		QueryDefinition<?> queryDefinition) {
+
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			StringBundler sb = new StringBundler(5);
+
+			sb.append(StringPool.OPEN_PARENTHESIS);
+			sb.append(CustomSQLUtil.get(FIND_O_BY_C_PO));
+			sb.append(") UNION ALL (");
+
+			String sql = CustomSQLUtil.get(FIND_U_BY_C_S_O);
+
+			int status = queryDefinition.getStatus();
+
+			if (status == WorkflowConstants.STATUS_ANY) {
+				sql = StringUtil.replace(
+					sql, "(User_.status = ?) AND", StringPool.BLANK);
+			}
+
+			sb.append(sql);
+			sb.append(StringPool.CLOSE_PARENTHESIS);
+
+			SQLQuery q = session.createSynchronizedSQLQuery(sb.toString());
+
+			q.addScalar("organizationId", Type.LONG);
+			q.addScalar("userId", Type.LONG);
+
+			QueryPos qPos = QueryPos.getInstance(q);
+
+			qPos.add(companyId);
+			qPos.add(parentOrganizationId);
+			qPos.add(companyId);
+
+			if (status != WorkflowConstants.STATUS_ANY) {
+				qPos.add(queryDefinition.getStatus());
+			}
+
+			qPos.add(parentOrganizationId);
+
+			List<Object> models = new ArrayList<>();
+
+			Iterator<Object[]> itr = (Iterator<Object[]>)QueryUtil.iterate(
+				q, getDialect(), queryDefinition.getStart(),
+				queryDefinition.getEnd());
+
+			while (itr.hasNext()) {
+				Object[] array = itr.next();
+
+				long organizationId = (Long)array[0];
+
+				Object obj = null;
+
+				if (organizationId > 0) {
+					obj = OrganizationUtil.findByPrimaryKey(organizationId);
+				}
+				else {
+					long userId = (Long)array[1];
+
+					obj = UserUtil.findByPrimaryKey(userId);
+				}
+
+				models.add(obj);
+			}
+
+			return models;
 		}
 		catch (Exception e) {
 			throw new SystemException(e);
