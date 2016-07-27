34eadd49adb93d92770afea4f6c5101eba709d68
tomwang2011 <tong.wang@liferay.com>
2016-07-27 09:57:17 -0700

    LPS-67049 Update usages

--- modules/apps/collaboration/message-boards/message-boards-test/src/testIntegration/java/com/liferay/message/boards/lar/test/MBPortletDataHandlerTest.java
+++ modules/apps/collaboration/message-boards/message-boards-test/src/testIntegration/java/com/liferay/message/boards/lar/test/MBPortletDataHandlerTest.java
@@ -27,7 +27,7 @@ import com.liferay.message.boards.kernel.service.MBCategoryServiceUtil;
 import com.liferay.message.boards.kernel.service.MBMessageLocalServiceUtil;
 import com.liferay.message.boards.kernel.service.MBThreadFlagLocalServiceUtil;
 import com.liferay.message.boards.web.constants.MBPortletKeys;
-import com.liferay.message.boards.web.lar.MBPortletDataHandler;
+import com.liferay.message.boards.web.exportimport.data.handler.MBPortletDataHandler;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
