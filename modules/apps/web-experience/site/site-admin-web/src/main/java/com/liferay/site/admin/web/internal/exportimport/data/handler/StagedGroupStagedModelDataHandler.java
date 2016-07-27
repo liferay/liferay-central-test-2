7631d2657750284847f20ad91ea10a672022a7f6
Máté Thurzó <mate.thurzo@liferay.com>
2016-07-27 12:52:48 +0200

    LPS-66938 Move site admin related data handlers to the proper package

--- modules/apps/web-experience/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/exportimport/data/handler/StagedGroupStagedModelDataHandler.java
+++ modules/apps/web-experience/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/exportimport/data/handler/StagedGroupStagedModelDataHandler.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.site.admin.web.internal.lar;
+package com.liferay.site.admin.web.internal.exportimport.data.handler;
 
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
 import com.liferay.exportimport.kernel.lar.StagedModelDataHandler;
