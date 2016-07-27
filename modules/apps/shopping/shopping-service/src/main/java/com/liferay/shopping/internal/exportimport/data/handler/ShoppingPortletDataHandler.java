d3c9b60b9b43330856e1614b4807b444b393d3ea
Máté Thurzó <mate.thurzo@liferay.com>
2016-07-27 11:33:52 +0200

    LPS-66934 Move shopping data handler to the proper package

--- modules/apps/shopping/shopping-service/src/main/java/com/liferay/shopping/internal/exportimport/data/handler/ShoppingPortletDataHandler.java
+++ modules/apps/shopping/shopping-service/src/main/java/com/liferay/shopping/internal/exportimport/data/handler/ShoppingPortletDataHandler.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.shopping.lar;
+package com.liferay.shopping.internal.exportimport.data.handler;
 
 import com.liferay.exportimport.kernel.lar.BasePortletDataHandler;
 import com.liferay.exportimport.kernel.lar.PortletDataHandler;
