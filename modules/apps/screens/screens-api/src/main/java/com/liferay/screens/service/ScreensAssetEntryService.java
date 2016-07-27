javiergamarra/liferay-portal

LPS-67091 autogen services

modules/apps/screens/screens-api/src/main/java/com/liferay/screens/service/ScreensAssetEntryService.java
@@ -21,6 +21,7 @@ import com.liferay.asset.kernel.service.persistence.AssetEntryQuery;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
 import com.liferay.portal.kernel.security.access.control.AccessControlled;
 import com.liferay.portal.kernel.service.BaseService;
@@ -62,6 +63,10 @@ public interface ScreensAssetEntryService extends BaseService {
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public JSONArray getAssetEntries(long companyId, long groupId,
 		java.lang.String portletItemName, Locale locale, int max)
+		throws PortalException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public JSONObject getAssetEntryExtended(long entryId, Locale locale)
 		throws PortalException;
 
 	/**

modules/apps/screens/screens-api/src/main/java/com/liferay/screens/service/ScreensAssetEntryServiceUtil.java
@@ -57,6 +57,12 @@ public class ScreensAssetEntryServiceUtil {
 			locale, max);
 	}
 
+	public static com.liferay.portal.kernel.json.JSONObject getAssetEntryExtended(
+		long entryId, java.util.Locale locale)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return getService().getAssetEntryExtended(entryId, locale);
+	}
+
 	/**
 	* Returns the OSGi service identifier.
 	*

modules/apps/screens/screens-api/src/main/java/com/liferay/screens/service/ScreensAssetEntryServiceWrapper.java
@@ -50,6 +50,13 @@ public class ScreensAssetEntryServiceWrapper implements ScreensAssetEntryService
 			portletItemName, locale, max);
 	}
 
+	@Override
+	public com.liferay.portal.kernel.json.JSONObject getAssetEntryExtended(
+		long entryId, java.util.Locale locale)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _screensAssetEntryService.getAssetEntryExtended(entryId, locale);
+	}
+
 	/**
 	* Returns the OSGi service identifier.
 	*

liferay/liferay-central-test-2

modules/apps/screens/screens-api/src/main/java/com/liferay/screens/service/ScreensAssetEntryService.java
@@ -21,6 +21,7 @@ import com.liferay.asset.kernel.service.persistence.AssetEntryQuery;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
 import com.liferay.portal.kernel.security.access.control.AccessControlled;
 import com.liferay.portal.kernel.service.BaseService;
@@ -62,6 +63,10 @@ public interface ScreensAssetEntryService extends BaseService {
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public JSONArray getAssetEntries(long companyId, long groupId,
 		java.lang.String portletItemName, Locale locale, int max)
+		throws PortalException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public JSONObject getAssetEntry(long entryId, Locale locale)
 		throws PortalException;
 
 	/**

modules/apps/screens/screens-api/src/main/java/com/liferay/screens/service/ScreensAssetEntryServiceUtil.java
@@ -57,6 +57,12 @@ public class ScreensAssetEntryServiceUtil {
 			locale, max);
 	}
 
+	public static com.liferay.portal.kernel.json.JSONObject getAssetEntry(
+		long entryId, java.util.Locale locale)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return getService().getAssetEntry(entryId, locale);
+	}
+
 	/**
 	* Returns the OSGi service identifier.
 	*

modules/apps/screens/screens-api/src/main/java/com/liferay/screens/service/ScreensAssetEntryServiceWrapper.java
@@ -50,6 +50,13 @@ public class ScreensAssetEntryServiceWrapper implements ScreensAssetEntryService
 			portletItemName, locale, max);
 	}
 
+	@Override
+	public com.liferay.portal.kernel.json.JSONObject getAssetEntry(
+		long entryId, java.util.Locale locale)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _screensAssetEntryService.getAssetEntry(entryId, locale);
+	}
+
 	/**
 	* Returns the OSGi service identifier.
 	*