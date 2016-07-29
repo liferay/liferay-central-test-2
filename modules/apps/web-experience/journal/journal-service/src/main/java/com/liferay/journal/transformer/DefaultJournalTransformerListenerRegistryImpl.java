d6285dde111e9af6522593292608acc2e6e3f5db
Pavel Savinov <pavel.savinov@liferay.com>
2016-07-29 12:54:24 +0200

    LPS-58672 add journal transformer listener registry

---
+++ modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/transformer/DefaultJournalTransformerListenerRegistryImpl.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.transformer;
+
+import com.liferay.journal.constants.JournalPortletKeys;
+import com.liferay.journal.util.JournalTransformerListenerRegistry;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.templateparser.TransformerListener;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(immediate = true, service = JournalTransformerListenerRegistry.class)
+public class DefaultJournalTransformerListenerRegistryImpl
+	implements JournalTransformerListenerRegistry {
+
+	public List<TransformerListener> getTransformerListeners() {
+		return _transformerListeners;
+	}
+
+	@Reference(
+		cardinality = ReferenceCardinality.MULTIPLE,
+		policy = ReferencePolicy.DYNAMIC,
+		target = "(javax.portlet.name=" + JournalPortletKeys.JOURNAL + ")",
+		unbind = "unregisterTransformerListener"
+	)
+	public void registerTransformerListener(
+		TransformerListener transformerListener) {
+
+		_transformerListeners.add(transformerListener);
+	}
+
+	public void unregisterTransformerListener(
+		TransformerListener transformerListener) {
+
+		_transformerListeners.remove(transformerListener);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DefaultJournalTransformerListenerRegistryImpl.class);
+
+	private final List<TransformerListener> _transformerListeners =
+		new CopyOnWriteArrayList<>();
+
+}
---
+++ modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/transformer/JournalTransformerListenerRegistryUtil.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.transformer;
+
+import com.liferay.journal.util.JournalTransformerListenerRegistry;
+import com.liferay.osgi.util.ServiceTrackerFactory;
+import com.liferay.portal.kernel.templateparser.TransformerListener;
+
+import java.util.List;
+
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ * @author Pavel Savinov
+ */
+public class JournalTransformerListenerRegistryUtil {
+
+	public static List<TransformerListener> getTransformerListeners() {
+		JournalTransformerListenerRegistry journalTransformerListenerRegistry =
+			_serviceTracker.getService();
+
+		return journalTransformerListenerRegistry.getTransformerListeners();
+	}
+
+	private static final
+		ServiceTracker<?, JournalTransformerListenerRegistry> _serviceTracker =
+			ServiceTrackerFactory.open(
+				JournalTransformerListenerRegistry.class);
+
+}
