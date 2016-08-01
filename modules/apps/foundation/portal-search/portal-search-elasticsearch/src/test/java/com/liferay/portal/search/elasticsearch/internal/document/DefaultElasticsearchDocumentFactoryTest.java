49d7b2fea3f6f99ed464a6de2f78db01c62fa2dd
André de Oliveira <andre.oliveira@liferay.com>
2016-07-31 23:13:10 -0300

    LPS-59977 Test: String 'null' must be indexed as such
    https://issues.liferay.com/browse/LPS-59977

---
+++ modules/apps/foundation/portal-search/portal-search-elasticsearch/src/test/java/com/liferay/portal/search/elasticsearch/internal/document/DefaultElasticsearchDocumentFactoryTest.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.elasticsearch.internal.document;
+
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.DocumentImpl;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.search.elasticsearch.document.ElasticsearchDocumentFactory;
+import com.liferay.portal.search.unit.test.DocumentFixture;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author André de Oliveira
+ */
+public class DefaultElasticsearchDocumentFactoryTest {
+
+	@Before
+	public void setUp() throws Exception {
+		_documentFixture.setUp();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		_documentFixture.tearDown();
+	}
+
+	@Test
+	public void testNull() throws Exception {
+		assertElasticsearchDocument(null, "{}");
+	}
+
+	@Test
+	public void testSpaces() throws Exception {
+		assertElasticsearchDocument(StringPool.SPACE, "{\"field\":\"\"}");
+
+		assertElasticsearchDocument(
+			StringPool.THREE_SPACES, "{\"field\":\"\"}");
+	}
+
+	@Test
+	public void testStringBlank() throws Exception {
+		assertElasticsearchDocument(StringPool.BLANK, "{\"field\":\"\"}");
+	}
+
+	@Test
+	public void testStringNull() throws Exception {
+		assertElasticsearchDocument(StringPool.NULL, "{\"field\":\"null\"}");
+	}
+
+	protected void assertElasticsearchDocument(String value, String json)
+		throws Exception {
+
+		Document document = new DocumentImpl();
+
+		document.addText("field", new String[] {value});
+
+		Assert.assertEquals(
+			json,
+			_elasticsearchDocumentFactory.getElasticsearchDocument(document));
+	}
+
+	private final DocumentFixture _documentFixture = new DocumentFixture();
+	private final ElasticsearchDocumentFactory _elasticsearchDocumentFactory =
+		new DefaultElasticsearchDocumentFactory();
+
+}
