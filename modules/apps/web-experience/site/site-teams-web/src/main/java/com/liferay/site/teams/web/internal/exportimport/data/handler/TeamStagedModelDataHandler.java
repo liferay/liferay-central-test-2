bb5ab04c956df2e05f6a7366ab5d9007b8e8b228
Máté Thurzó <mate.thurzo@liferay.com>
2016-08-01 12:53:17 +0200

    LPS-66938 Move site teams data handlers to the proper package

--- modules/apps/web-experience/site/site-teams-web/src/main/java/com/liferay/site/teams/web/internal/exportimport/data/handler/TeamStagedModelDataHandler.java
+++ modules/apps/web-experience/site/site-teams-web/src/main/java/com/liferay/site/teams/web/internal/exportimport/data/handler/TeamStagedModelDataHandler.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.site.teams.web.internal.lar;
+package com.liferay.site.teams.web.internal.exportimport.data.handler;
 
 import com.liferay.exportimport.kernel.lar.ExportImportPathUtil;
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
