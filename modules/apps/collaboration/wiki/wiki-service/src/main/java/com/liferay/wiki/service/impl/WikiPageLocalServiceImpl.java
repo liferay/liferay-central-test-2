efd51679269d7009c64f240f12442558ef116e84
hai.yu <hai.yu@liferay.com>
2016-08-02 15:08:08 +0800

    LPS-67404 When restore attachment from Recycle Bin, we should use getDeletedAttachmentsFileEntries() because its status is 8 so that we can fetch dlfileEntry(status:8).

--- modules/apps/collaboration/wiki/wiki-service/src/main/java/com/liferay/wiki/service/impl/WikiPageLocalServiceImpl.java
+++ modules/apps/collaboration/wiki/wiki-service/src/main/java/com/liferay/wiki/service/impl/WikiPageLocalServiceImpl.java
@@ -2892,7 +2892,7 @@ public class WikiPageLocalServiceImpl extends WikiPageLocalServiceBaseImpl {
 
 		// Attachments
 
-		for (FileEntry fileEntry : page.getAttachmentsFileEntries()) {
+		for (FileEntry fileEntry : page.getDeletedAttachmentsFileEntries()) {
 			PortletFileRepositoryUtil.restorePortletFileEntryFromTrash(
 				userId, fileEntry.getFileEntryId());
 		}
