37fe5561891a6f96e481acf38d23c979f21c917f
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-08-02 11:12:32 -0700

    LPS-66906 Add option to hide the NPM progress bar

--- modules/sdk/gradle-plugins-node/src/main/java/com/liferay/gradle/plugins/node/tasks/ExecuteNpmTask.java
+++ modules/sdk/gradle-plugins-node/src/main/java/com/liferay/gradle/plugins/node/tasks/ExecuteNpmTask.java
@@ -106,12 +106,20 @@ public class ExecuteNpmTask extends ExecuteNodeScriptTask {
 		return _inheritProxy;
 	}
 
+	public boolean isProgress() {
+		return _progress;
+	}
+
 	public void setCacheDir(Object cacheDir) {
 		_cacheDir = cacheDir;
 	}
 
 	public void setInheritProxy(boolean inheritProxy) {
 		_inheritProxy = inheritProxy;
+	}
+
+	public void setProgress(boolean progress) {
+		_progress = progress;
 	}
 
 	public void setRegistry(Object registry) {
@@ -184,6 +192,9 @@ public class ExecuteNpmTask extends ExecuteNodeScriptTask {
 			completeArgs.add(logLevel);
 		}
 
+		completeArgs.add("--progress");
+		completeArgs.add(Boolean.toString(isProgress()));
+
 		if (isInheritProxy()) {
 			addProxyArg(completeArgs, "--proxy", "http");
 			addProxyArg(completeArgs, "--https-proxy", "https");
@@ -201,6 +212,7 @@ public class ExecuteNpmTask extends ExecuteNodeScriptTask {
 
 	private Object _cacheDir;
 	private boolean _inheritProxy = true;
+	private boolean _progress = true;
 	private Object _registry;
 
 }
