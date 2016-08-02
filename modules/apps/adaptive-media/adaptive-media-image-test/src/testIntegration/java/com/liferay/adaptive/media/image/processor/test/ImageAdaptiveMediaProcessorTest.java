2ef956a63e48cb9e2430872e9051c050bb4c19ff
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2016-08-02 17:11:01 +0200

    LPS-67292 Add tests for finder/processor
    This tests the tandem processor/finder:
    
      - Files that are not images generate no media, and the finder returns
        nothing.
    
      - File with images generate as many media as there are configurations,
        and the media returns that exact number.
    
      - When a file has media, but there are some configuration that is
        missing, the finder excludes it.

---
+++ modules/apps/adaptive-media/adaptive-media-image-test/src/testIntegration/java/com/liferay/adaptive/media/image/processor/test/ImageAdaptiveMediaProcessorTest.java
@@ -0,0 +1,264 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.adaptive.media.image.processor.test;
+
+import com.liferay.adaptive.media.AdaptiveMedia;
+import com.liferay.adaptive.media.image.finder.ImageAdaptiveMediaFinder;
+import com.liferay.adaptive.media.image.processor.ImageAdaptiveMediaProcessor;
+import com.liferay.adaptive.media.image.test.util.DestinationReplacer;
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.document.library.kernel.model.DLFolderConstants;
+import com.liferay.document.library.kernel.service.DLAppLocalService;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.service.test.ServiceTestUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.util.StringPlus;
+
+import java.io.IOException;
+
+import java.util.Dictionary;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+
+/**
+ * @author Adolfo Pérez
+ */
+@RunWith(Arquillian.class)
+@Sync
+public class ImageAdaptiveMediaProcessorTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			SynchronousDestinationTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		_configurationAdmin = _getService(ConfigurationAdmin.class);
+		_dlAppLocalService = _getService(DLAppLocalService.class);
+		_finder = _getService(ImageAdaptiveMediaFinder.class);
+		_processor = _getService(ImageAdaptiveMediaProcessor.class);
+
+		ServiceTestUtil.setUser(TestPropsValues.getUser());
+
+		_addTestVariant();
+	}
+
+	@Test
+	public void testAddingFileEntryWithImageCreatesMedia() throws Exception {
+		try (DestinationReplacer destinationReplacer = new DestinationReplacer(
+				"liferay/adaptive_media_processor")) {
+
+			ServiceContext serviceContext =
+				ServiceContextTestUtil.getServiceContext(
+					_group, TestPropsValues.getUserId());
+
+			final FileEntry fileEntry = _addImageFileEntry(serviceContext);
+
+			Stream<AdaptiveMedia<ImageAdaptiveMediaProcessor>> stream =
+				_finder.getAdaptiveMedia(
+					queryBuilder -> queryBuilder.allForFileEntry(fileEntry));
+
+			Assert.assertEquals(_getVariantsCount(), stream.count());
+		}
+	}
+
+	@Test
+	public void testAddingFileEntryWithNoImageCreatesNoMedia()
+		throws Exception {
+
+		try (DestinationReplacer destinationReplacer = new DestinationReplacer(
+				"liferay/adaptive_media_processor")) {
+
+			ServiceContext serviceContext =
+				ServiceContextTestUtil.getServiceContext(
+					_group, TestPropsValues.getUserId());
+
+			FileEntry fileEntry = _addNonImageFileEntry(serviceContext);
+
+			Stream<AdaptiveMedia<ImageAdaptiveMediaProcessor>> stream =
+				_finder.getAdaptiveMedia(
+					queryBuilder -> queryBuilder.allForFileEntry(fileEntry));
+
+			Assert.assertEquals(0, stream.count());
+		}
+	}
+
+	@Test
+	public void testCleaningFileEntryWithImageRemovesMedia() throws Exception {
+		try (DestinationReplacer destinationReplacer = new DestinationReplacer(
+				"liferay/adaptive_media_processor")) {
+
+			ServiceContext serviceContext =
+				ServiceContextTestUtil.getServiceContext(
+					_group, TestPropsValues.getUserId());
+
+			final FileEntry fileEntry = _addImageFileEntry(serviceContext);
+
+			_processor.cleanUp(fileEntry.getLatestFileVersion());
+
+			Stream<AdaptiveMedia<ImageAdaptiveMediaProcessor>> stream =
+				_finder.getAdaptiveMedia(
+					queryBuilder -> queryBuilder.allForFileEntry(fileEntry));
+
+			Assert.assertEquals(0, stream.count());
+		}
+	}
+
+	@Test
+	public void testCleaningFileEntryWithNoImageDoesNothing() throws Exception {
+		try (DestinationReplacer destinationReplacer = new DestinationReplacer(
+				"liferay/adaptive_media_processor")) {
+
+			ServiceContext serviceContext =
+				ServiceContextTestUtil.getServiceContext(
+					_group, TestPropsValues.getUserId());
+
+			final FileEntry fileEntry = _addNonImageFileEntry(serviceContext);
+
+			_processor.cleanUp(fileEntry.getLatestFileVersion());
+
+			Stream<AdaptiveMedia<ImageAdaptiveMediaProcessor>> stream =
+				_finder.getAdaptiveMedia(
+					queryBuilder -> queryBuilder.allForFileEntry(fileEntry));
+
+			Assert.assertEquals(0, stream.count());
+		}
+	}
+
+	private FileEntry _addImageFileEntry(ServiceContext serviceContext)
+		throws Exception {
+
+		return _dlAppLocalService.addFileEntry(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			StringUtil.randomString(), ContentTypes.IMAGE_JPEG,
+			_getImageBytes(), serviceContext);
+	}
+
+	private FileEntry _addNonImageFileEntry(ServiceContext serviceContext)
+		throws Exception {
+
+		return _dlAppLocalService.addFileEntry(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			StringUtil.randomString(), ContentTypes.APPLICATION_OCTET_STREAM,
+			_getNonImageBytes(), serviceContext);
+	}
+
+	private void _addTestVariant() throws IOException {
+		Configuration configuration = _configurationAdmin.getConfiguration(
+			"com.liferay.adaptive.media.image.internal.configuration." +
+				"ImageAdaptiveMediaCompanyConfiguration");
+
+		Dictionary<String, Object> properties = configuration.getProperties();
+
+		if (properties == null) {
+			configuration.update(new HashMapDictionary<>());
+
+			properties = configuration.getProperties();
+		}
+
+		Object value = properties.get("imageVariants");
+
+		if (value != null) {
+			return;
+		}
+
+		properties.put(
+			"imageVariants", new String[] {"small:0:width=100;height=100"});
+	}
+
+	private byte[] _getImageBytes() throws Exception {
+		return FileUtil.getBytes(
+			ImageAdaptiveMediaProcessorTest.class,
+			"/com/liferay/adaptive/media/image/processor/test/dependencies/" +
+				"image.jpg");
+	}
+
+	private byte[] _getNonImageBytes() {
+		String s = StringUtil.randomString();
+
+		return s.getBytes();
+	}
+
+	private <T> T _getService(Class<T> clazz) {
+		try {
+			Registry registry = RegistryUtil.getRegistry();
+
+			return registry.getService(clazz);
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private int _getVariantsCount() throws IOException {
+		Configuration configuration = _configurationAdmin.getConfiguration(
+			"com.liferay.adaptive.media.image.internal.configuration." +
+				"ImageAdaptiveMediaCompanyConfiguration");
+
+		Dictionary<String, Object> properties = configuration.getProperties();
+
+		Object value = properties.get("imageVariants");
+
+		if (value == null) {
+			return 0;
+		}
+
+		List<String> imageVariants = StringPlus.asList(value);
+
+		return imageVariants.size();
+	}
+
+	private ConfigurationAdmin _configurationAdmin;
+	private DLAppLocalService _dlAppLocalService;
+	private ImageAdaptiveMediaFinder _finder;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	private ImageAdaptiveMediaProcessor _processor;
+
+}
