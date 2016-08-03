8bd33b54650b58eb038229d4881af67812a33956
Brian Chan <brian.chan@liferay.com>
2016-08-03 13:18:25 -0700

    artifact:ignore frontend-image-editor-capability-resize 1.0.3 prep next

--- modules/apps/foundation/frontend-image-editor/frontend-image-editor-capability-resize/.task-cache/resources/ResizeControls.soy.js
+++ modules/apps/foundation/frontend-image-editor/frontend-image-editor-capability-resize/.task-cache/resources/ResizeControls.soy.js
@@ -1,4 +1,4 @@
-define("frontend-image-editor-capability-resize@1.0.3/ResizeControls.soy", ['exports', 'metal-component/src/Component', 'metal-soy/src/Soy'], function (exports, _Component2, _Soy) {
+define("frontend-image-editor-capability-resize@1.0.4/ResizeControls.soy", ['exports', 'metal-component/src/Component', 'metal-soy/src/Soy'], function (exports, _Component2, _Soy) {
   'use strict';
 
   Object.defineProperty(exports, "__esModule", {
