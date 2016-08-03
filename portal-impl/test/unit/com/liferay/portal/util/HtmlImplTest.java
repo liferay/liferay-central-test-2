54ff810a12d527f18c874e0eb1fe2779ea672657
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-08-03 13:12:38 +0200

    LPS-65384 Add a test verifying the bug

--- portal-impl/test/unit/com/liferay/portal/util/HtmlImplTest.java
+++ portal-impl/test/unit/com/liferay/portal/util/HtmlImplTest.java
@@ -314,6 +314,30 @@ public class HtmlImplTest {
 	}
 
 	@Test
+	public void testStripHtml() {
+		Assert.assertEquals(
+			"Hello World!",
+			_htmlImpl.stripHtml(
+				"<html><body><h1>Hello World!</h1></body></html>"));
+	}
+
+	@Test
+	public void testStripHtmlWithScripTag() {
+		Assert.assertEquals(
+			"Hello World!",
+			_htmlImpl.stripHtml(
+				"<body>Hello<script>alert('xss');</script> World!</body>"));
+	}
+
+	@Test
+	public void testStripHtmlWithStyleTag() {
+		Assert.assertEquals(
+			"Hello World!",
+			_htmlImpl.stripHtml(
+				"<body>Hello<style>p{color:#000000}</style> World!</body>"));
+	}
+
+	@Test
 	public void testStripMultipleComments() {
 		Assert.assertEquals(
 			"test",
