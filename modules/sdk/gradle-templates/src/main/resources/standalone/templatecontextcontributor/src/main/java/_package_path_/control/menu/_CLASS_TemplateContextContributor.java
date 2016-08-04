6ff54f4289349f1e87b48be3bbdbdedad376b437
Julio Camarero <julio.camarero@liferay.com>
2016-08-04 14:37:45 +0200

    LPS-65470 template for context contributor

---
+++ modules/sdk/gradle-templates/src/main/resources/standalone/templatecontextcontributor/src/main/java/_package_path_/control/menu/_CLASS_TemplateContextContributor.java
@@ -0,0 +1,30 @@
+package _package_.control.menu;
+
+import com.liferay.portal.kernel.template.TemplateContextContributor;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component(
+	immediate = true,
+	property = {"type=" + TemplateContextContributor.TYPE_THEME},
+	service = TemplateContextContributor.class
+)
+public class _CLASS_TemplateContextContributor
+	implements TemplateContextContributor {
+
+	@Override
+	public void prepare(
+		Map<String, Object> contextObjects, HttpServletRequest request) {
+
+		// This variable can be used in any theme file. For example, you could
+		// add this code to the file portal_normal.ftl in your theme:
+		// <h1>${sample_text}</h1>
+
+		contextObjects.put("sample_text", "This is some sample text");
+	}
+
+}
