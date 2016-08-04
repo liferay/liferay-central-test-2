b5b1207f1f734acb7e56d04797b912a4ceb5a744
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-08-04 15:05:02 -0700

    LRDOCS-2841 Sample

---
+++ modules/sdk/gradle-plugins-app-javadoc-builder/samples/samples-goodbye/src/main/java/com/samples/goodbye/GoodbyeBuilder.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.samples.goodbye;
+
+/**
+ * Provided methods to build a "goodbye" greeting.
+ *
+ * @author Andrea Di Giorgi
+ */
+public interface GoodbyeBuilder {
+
+	/**
+	 * Returns the "goodbye" greeting.
+	 *
+	 * @return the "goodbye" greeting.
+	 */
+	public String getGoodbye();
+
+	/**
+	 * Returns the name used to build a greeting.
+	 *
+	 * @return the name used to build a greeting.
+	 */
+	public String getName();
+
+	/**
+	 * Sets the name to use when building a greeting.
+	 *
+	 * @param name the name to use when building a greeting.
+	 */
+	public void setName(String name);
+
+}
---
+++ modules/sdk/gradle-plugins-app-javadoc-builder/samples/samples-hello/src/main/java/com/samples/hello/HelloBuilder.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.samples.hello;
+
+/**
+ * Provided methods to build a "hello" greeting.
+ *
+ * @author Andrea Di Giorgi
+ */
+public interface HelloBuilder {
+
+	/**
+	 * Returns the "hello" greeting.
+	 *
+	 * @return the "hello" greeting.
+	 */
+	public String getHello();
+
+	/**
+	 * Returns the name used to build a greeting.
+	 *
+	 * @return the name used to build a greeting.
+	 */
+	public String getName();
+
+	/**
+	 * Sets the name to use when building a greeting.
+	 *
+	 * @param name the name to use when building a greeting.
+	 */
+	public void setName(String name);
+
+}
