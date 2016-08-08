4863b16cfb406440e4490364bf2565f6b188b2f4
Michael Hashimoto <michael.hashimoto@liferay.com>
2016-08-07 18:54:23 -0700

    LRQA-26691 Add additional operator implementations to 'PQLOperatorFactory'

--- modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/pql/PQLOperatorFactory.java
+++ modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/pql/PQLOperatorFactory.java
@@ -66,6 +66,145 @@ public class PQLOperatorFactory {
 
 			};
 		}
+		else if (operator.equals("~") || operator.equals("!~")) {
+			return new PQLOperator(operator) {
+
+				public Object getValue(
+						PQLEntity pqlEntity1, PQLEntity pqlEntity2,
+						Properties properties)
+					throws Exception {
+
+					String operator = getOperator();
+					Object value1 = pqlEntity1.getValue(properties);
+					Object value2 = pqlEntity2.getValue(properties);
+
+					if ((value1 == null) || (value2 == null)) {
+						return false;
+					}
+
+					if (!(value1 instanceof String) ||
+						!(value2 instanceof String)) {
+
+						throw new Exception(
+							"The '" + operator + "' operator only works " +
+								"for string values.");
+					}
+
+					String stringValue1 = (String)value1;
+					String stringValue2 = (String)value2;
+
+					if (operator.equals("~")) {
+						return stringValue1.contains(stringValue2);
+					}
+					else if (operator.equals("!~")) {
+						return !stringValue1.contains(stringValue2);
+					}
+
+					throw new Exception(
+						"Unsupported '" + operator + "' operator.");
+				}
+
+			};
+		}
+		else if (operator.equals("==") || operator.equals("!=")) {
+			return new PQLOperator(operator) {
+
+				public Object getValue(
+						PQLEntity pqlEntity1, PQLEntity pqlEntity2,
+						Properties properties)
+					throws Exception {
+
+					String operator = getOperator();
+					Object value1 = pqlEntity1.getValue(properties);
+					Object value2 = pqlEntity2.getValue(properties);
+
+					if ((value1 == null) || (value2 == null)) {
+						return false;
+					}
+
+					if (operator.equals("==")) {
+						return value1.equals(value2);
+					}
+					else if (operator.equals("!=")) {
+						return !value1.equals(value2);
+					}
+
+					throw new Exception(
+						"Unsupported '" + operator + "' operator.");
+				}
+
+			};
+		}
+		else if (operator.equals("<") || operator.equals("<=") ||
+				 operator.equals(">") || operator.equals(">=")) {
+
+			return new PQLOperator(operator) {
+
+				public Object getValue(
+						PQLEntity pqlEntity1, PQLEntity pqlEntity2,
+						Properties properties)
+					throws Exception {
+
+					String operator = getOperator();
+					Object value1 = pqlEntity1.getValue(properties);
+					Object value2 = pqlEntity2.getValue(properties);
+
+					if ((value1 == null) || (value2 == null)) {
+						throw new Exception(
+							"The '" + operator + "' operator only works " +
+								"for number values.");
+					}
+
+					if ((value1 instanceof Double ||
+						 value1 instanceof Integer) &&
+						(value2 instanceof Double ||
+						 value2 instanceof Integer)) {
+
+						Double doubleValue1 = null;
+						Double doubleValue2 = null;
+
+						if (value1 instanceof Integer) {
+							Integer integerValue1 = (Integer)value1;
+
+							doubleValue1 = integerValue1.doubleValue();
+						}
+						else {
+							doubleValue1 = (Double)value1;
+						}
+
+						if (value2 instanceof Integer) {
+							Integer integerValue2 = (Integer)value2;
+
+							doubleValue2 = integerValue2.doubleValue();
+						}
+						else {
+							doubleValue2 = (Double)value2;
+						}
+
+						if (operator.equals("<")) {
+							return (doubleValue1 < doubleValue2);
+						}
+						else if (operator.equals("<=")) {
+							return (doubleValue1 <= doubleValue2);
+						}
+						else if (operator.equals(">")) {
+							return (doubleValue1 > doubleValue2);
+						}
+						else if (operator.equals(">=")) {
+							return (doubleValue1 >= doubleValue2);
+						}
+
+						throw new Exception(
+							"Unsupported '" + operator + "' operator.");
+					}
+
+					throw new Exception(
+						"The '" + operator + "' operator only works for " +
+							"number values.");
+				}
+
+			};
+		}
 
 		throw new Exception("Unsupported '" + operator + "' operator.");
 	}
