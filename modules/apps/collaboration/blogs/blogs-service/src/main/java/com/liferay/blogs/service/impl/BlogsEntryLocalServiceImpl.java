772b7723301fb1ec01e50855b3c0d0dd2e93bc3d
Brian Chan <brian.chan@liferay.com>
2016-08-09 16:15:12 -0700

    LPS-67352 Hugo SF, somehow this was missed in master because it was abstracted out, but not in 7.0.x since it is still in portal-impl

--- modules/apps/collaboration/blogs/blogs-service/src/main/java/com/liferay/blogs/service/impl/BlogsEntryLocalServiceImpl.java
+++ modules/apps/collaboration/blogs/blogs-service/src/main/java/com/liferay/blogs/service/impl/BlogsEntryLocalServiceImpl.java
@@ -2096,9 +2096,9 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 
 			boolean validSmallImageExtension = false;
 
-			for (String _imageExtension : imageExtensions) {
-				if (StringPool.STAR.equals(_imageExtension) ||
-					_imageExtension.equals(
+			for (String imageExtension : imageExtensions) {
+				if (StringPool.STAR.equals(imageExtension) ||
+					imageExtension.equals(
 						StringPool.PERIOD + fileEntry.getExtension())) {
 
 					validSmallImageExtension = true;
