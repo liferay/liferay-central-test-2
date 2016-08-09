08d4164cd7ed0de3f9d75e79d82f747ee1fa7fec
Brian Chan <brian.chan@liferay.com>
2016-08-09 10:24:18 -0700

    LPS-58672 Hugo SF

--- modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/configuration/JournalServiceConfigurationKeys.java
+++ modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/configuration/JournalServiceConfigurationKeys.java
@@ -78,7 +78,7 @@ public class JournalServiceConfigurationKeys {
 		"terms.of.use.journal.article.id";
 
 	/**
-	 * @deprecated As of 7.0.0 with no direct replacement
+	 * @deprecated As of 7.0.0, with no direct replacement
 	 */
 	@Deprecated
 	public static final String TRANSFORMER_LISTENER = "transformer.listener";
