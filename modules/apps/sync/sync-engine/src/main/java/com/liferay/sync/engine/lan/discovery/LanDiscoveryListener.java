4d0affdc7df241ac06c5997aba31f0fd415c0330
Dennis Ju <dennis.ju@liferay.com>
2016-08-09 14:39:49 -0700

    SYNC-1622 Engine - Reuse TDP port

--- modules/apps/sync/sync-engine/src/main/java/com/liferay/sync/engine/lan/discovery/LanDiscoveryListener.java
+++ modules/apps/sync/sync-engine/src/main/java/com/liferay/sync/engine/lan/discovery/LanDiscoveryListener.java
@@ -19,6 +19,7 @@ import com.liferay.sync.engine.util.PropsValues;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioDatagramChannel;
@@ -36,6 +37,7 @@ public class LanDiscoveryListener {
 		bootstrap.channel(NioDatagramChannel.class);
 		bootstrap.group(_eventLoopGroup);
 		bootstrap.handler(new LanDiscoveryListenerHandler());
+		bootstrap.option(ChannelOption.SO_REUSEADDR, true);
 
 		ChannelFuture channelFuture = bootstrap.bind(PropsValues.SYNC_LAN_PORT);
 
