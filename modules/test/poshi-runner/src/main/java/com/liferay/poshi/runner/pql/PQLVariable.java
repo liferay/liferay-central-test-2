1ffba5a6da6f0a7104204c88409459345eea07d0
Michael Hashimoto <michael.hashimoto@liferay.com>
2016-08-11 12:17:54 -0700

    LRQA-26691 Add 'PQLEntity' implementation called 'PQLVariable' to deal with property values

---
+++ modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/pql/PQLVariable.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.poshi.runner.pql;
+
+import com.liferay.poshi.runner.PoshiRunnerContext;
+
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * @author Michael Hashimoto
+ */
+public class PQLVariable extends PQLValue {
+
+	public static boolean isVariable(String variable) {
+		if (variable == null) {
+			return false;
+		}
+
+		if (_availablePropertyNames.contains(variable)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public PQLVariable(String variable) throws Exception {
+		super(variable);
+
+		_validateVariable(getPQL());
+	}
+
+	public Object getValue(Properties properties) throws Exception {
+		String pql = getPQL();
+
+		if (!properties.containsKey(pql)) {
+			return null;
+		}
+
+		String value = properties.getProperty(pql);
+
+		if (!(value.startsWith("'") && value.endsWith("'")) &&
+			!(value.startsWith("\"") && value.endsWith("\"")) &&
+			value.contains(" ")) {
+
+			value = "'" + value + "'";
+		}
+
+		return getObjectValue(value);
+	}
+
+	private static void _validateVariable(String variable) throws Exception {
+		if (variable == null) {
+			throw new Exception("Invalid variable: " + variable);
+		}
+
+		variable.trim();
+
+		if (!isVariable(variable)) {
+			throw new Exception(
+				"Property not found in 'test.case.available.property.names': " +
+					variable);
+		}
+	}
+
+	private static final List<String> _availablePropertyNames =
+		PoshiRunnerContext.getTestCaseAvailablePropertyNames();
+
+}
