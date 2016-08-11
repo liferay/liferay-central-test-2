576697a31b9d88953746f215816b1d4e582b0d1a
Michael Hashimoto <michael.hashimoto@liferay.com>
2016-08-11 14:05:19 -0700

    LRQA-26691 Add unit tests for 'PQLEntity'

---
+++ modules/test/poshi-runner/src/test/java/com/liferay/poshi/runner/pql/PQLEntityTest.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.poshi.runner.pql;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+/**
+ * @author Michael Hashimoto
+ */
+public class PQLEntityTest extends TestCase {
+
+	@Test
+	public void testFixPQL() throws Exception {
+		_compare(PQLEntity.fixPQL("(((test)))"), "test");
+		_compare(PQLEntity.fixPQL(" (((test)) )"), "test");
+		_compare(PQLEntity.fixPQL(" ((( test test ))) "), "test test");
+		_compare(PQLEntity.fixPQL(")test("), ")test(");
+		_compare(PQLEntity.fixPQL(" )test( "), ")test(");
+		_compare(PQLEntity.fixPQL(" ) test"), ") test");
+		_compare(PQLEntity.fixPQL(" ( test"), "( test");
+		_compare(PQLEntity.fixPQL("test ) "), "test )");
+		_compare(PQLEntity.fixPQL("test ( "), "test (");
+		_compare(PQLEntity.fixPQL("( (test) OR (test))"), "(test) OR (test)");
+		_compare(
+			PQLEntity.fixPQL(" ( (( test ( test ))) "), "( (( test ( test )))");
+	}
+
+	private void _compare(String actual, String expected) throws Exception {
+		if (!actual.equals(expected)) {
+			throw new Exception(
+				"'" + expected + "' should equal '" + actual + "'");
+		}
+	}
+
+}
