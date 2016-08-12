9e66999fb3164895cf61f4fb601fe52937cef263
Hugo Huijser <hugo.huijser@liferay.com>
2016-08-12 12:04:55 +0800

    LPS-67352 Add custom check 'LPS42924Check'

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/LPS42924Check.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checkstyle.checks;
+
+import com.liferay.source.formatter.checkstyle.util.DetailASTUtil;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+import java.util.List;
+
+/**
+ * @author Hugo Huijser
+ */
+public class LPS42924Check extends AbstractCheck {
+
+	public static final String MSG_LPS_42924 = "lps42924";
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] {TokenTypes.CLASS_DEF};
+	}
+
+	@Override
+	public void visitToken(DetailAST detailAST) {
+		List<DetailAST> methodCallASTList = DetailASTUtil.getMethodCalls(
+			detailAST, "PortalUtil", "getClassNameId");
+
+		for (DetailAST methodCallAST : methodCallASTList) {
+			log(methodCallAST.getLineNo(), MSG_LPS_42924);
+		}
+	}
+
+}
