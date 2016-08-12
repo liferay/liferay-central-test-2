d3b69ae0e6d26734d07356d8d10b245608ea58ac
Jonathan McCann <jonathan.mccann@liferay.com>
2016-08-12 09:39:17 -0700

    LPS-67393 Apply logic to 'updateDiscussionMessage' as well and extract it out to reduce duplicated code

--- portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBMessageLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBMessageLocalServiceImpl.java
@@ -177,18 +177,7 @@ public class MBMessageLocalServiceImpl extends MBMessageLocalServiceBaseImpl {
 
 		long categoryId = MBCategoryConstants.DISCUSSION_CATEGORY_ID;
 
-		if (Validator.isNull(subject)) {
-			if (Validator.isNotNull(body)) {
-				subject = HtmlUtil.extractText(body);
-
-				if (subject.length() > 50) {
-					subject = subject.substring(0, 50) + "...";
-				}
-			}
-			else {
-				throw new MessageBodyException("Body is null");
-			}
-		}
+		subject = getDiscussionMessageSubject(subject, body);
 
 		List<ObjectValuePair<String, InputStream>> inputStreamOVPs =
 			Collections.emptyList();
@@ -1556,16 +1545,7 @@ public class MBMessageLocalServiceImpl extends MBMessageLocalServiceBaseImpl {
 			String subject, String body, ServiceContext serviceContext)
 		throws PortalException {
 
-		if (Validator.isNull(subject)) {
-			if (Validator.isNotNull(body)) {
-				int pos = Math.min(body.length(), 50);
-
-				subject = body.substring(0, pos) + "...";
-			}
-			else {
-				throw new MessageBodyException("Body is null");
-			}
-		}
+		subject = getDiscussionMessageSubject(subject, body);
 
 		List<ObjectValuePair<String, InputStream>> inputStreamOVPs = null;
 		List<String> existingFiles = null;
@@ -1980,6 +1960,26 @@ public class MBMessageLocalServiceImpl extends MBMessageLocalServiceBaseImpl {
 		}
 
 		return body;
+	}
+
+	protected String getDiscussionMessageSubject(String subject, String body)
+		throws MessageBodyException {
+
+		if (Validator.isNull(subject)) {
+			if (Validator.isNotNull(body)) {
+				subject = HtmlUtil.extractText(body);
+
+				if (subject.length() > 50) {
+					subject =
+						subject.substring(0, 50) + StringPool.TRIPLE_PERIOD;
+				}
+			}
+			else {
+				throw new MessageBodyException("Body is null");
+			}
+		}
+
+		return subject;
 	}
 
 	protected String getMessageURL(
