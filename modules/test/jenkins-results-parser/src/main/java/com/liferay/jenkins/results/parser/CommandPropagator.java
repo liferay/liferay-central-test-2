c7a9f13469f1de80071eca864fe4e0a1991d946e
Peter Yoo <peter.yoo@liferay.com>
2016-08-15 09:16:43 -0700

    LRQA-26644 Remove unnecessary code.

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/CommandPropagator.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/CommandPropagator.java
@@ -30,13 +30,6 @@ public class CommandPropagator {
 		String[] fileNames, String sourceDirName, String targetDirName,
 		List<String> targetSlaves) {
 
-		for (String fileName : fileNames) {
-			_commandPropagatorTasks.add(
-				new CommandPropagatorTask(
-					sourceDirName + "/" + fileName,
-					targetDirName + "/" + fileName));
-		}
-
 		_targetSlaves.addAll(targetSlaves);
 	}
 
@@ -48,13 +41,7 @@ public class CommandPropagator {
 		return _threadsDurationTotal / _threadsCompletedCount;
 	}
 
-	public void setCleanUpCommand(String cleanUpCommand) {
-		_cleanUpCommand = cleanUpCommand;
-	}
-
 	public void start(int threadCount) {
-		_copyFromSource();
-
 		ExecutorService executorService = Executors.newFixedThreadPool(
 			threadCount);
 
@@ -118,61 +105,6 @@ public class CommandPropagator {
 		}
 	}
 
-	private void _copyFromSource() {
-		List<String> commands = new ArrayList<>();
-
-		String targetSlave = null;
-
-		for (CommandPropagatorTask commandPropagatorTask : _commandPropagatorTasks) {
-			targetSlave = _targetSlaves.get(0);
-
-			String sourceFileName = commandPropagatorTask._sourceFileName;
-
-			System.out.println("Copying from source " + sourceFileName);
-
-			String targetFileName = commandPropagatorTask._targetFileName;
-
-			commands.add(_getMkdirCommand(targetFileName));
-
-			if (sourceFileName.startsWith("http")) {
-				commands.add(
-					"curl -o " + targetFileName + " " + sourceFileName);
-			}
-			else {
-				commands.add(
-					"rsync -svI " + sourceFileName + " " + targetFileName);
-			}
-
-			String targetDirName = targetFileName.substring(
-				0, targetFileName.lastIndexOf("/"));
-
-			commands.add("ls -al " + targetDirName);
-		}
-
-		try {
-			if (_executeBashCommands(commands, targetSlave) != 0) {
-				_errorSlaves.add(targetSlave);
-				_targetSlaves.remove(targetSlave);
-
-				_copyFromSource();
-
-				targetSlave = null;
-			}
-		}
-		catch (Exception e) {
-			throw new RuntimeException(
-				"Unable to copy from source. Executed: " + commands, e);
-		}
-
-		if (targetSlave != null) {
-			_mirrorSlaves.add(targetSlave);
-
-			_targetSlaves.remove(targetSlave);
-		}
-
-		System.out.println("Finished copying from source.");
-	}
-
 	private int _executeBashCommands(List<String> commands, String targetSlave)
 		throws InterruptedException, IOException {
 
@@ -180,11 +112,6 @@ public class CommandPropagator {
 
 		sb.append(targetSlave);
 		sb.append(" '");
-
-		if ((_cleanUpCommand != null) && !_cleanUpCommand.isEmpty()) {
-			sb.append(_cleanUpCommand);
-			sb.append("; ");
-		}
 
 		for (int i = 0; i < commands.size(); i++) {
 			sb.append(commands.get(i));
@@ -202,72 +129,19 @@ public class CommandPropagator {
 		return process.exitValue();
 	}
 
-	private String _getMkdirCommand(String fileName) {
-		String dirName = fileName.substring(0, fileName.lastIndexOf("/") + 1);
-
-		return "mkdir -pv " + dirName;
-	}
-
 	private final List<String> _busySlaves = new ArrayList<>();
-	private String _cleanUpCommand;
 	private final List<String> _errorSlaves = new ArrayList<>();
-	private final List<CommandPropagatorTask> _commandPropagatorTasks =
-		new ArrayList<>();
 	private final List<String> _mirrorSlaves = new ArrayList<>();
 	private final List<String> _targetSlaves = new ArrayList<>();
 	private int _threadsCompletedCount;
 	private long _threadsDurationTotal;
 
-	private static class CommandPropagatorTask {
-
-		private CommandPropagatorTask(
-			String sourceFileName, String targetFileName) {
-
-			_sourceFileName = _escapeParentheses(sourceFileName);
-			_targetFileName = _escapeParentheses(targetFileName);
-		}
-
-		private String _escapeParentheses(String fileName) {
-			fileName = fileName.replace(")", "\\)");
-			fileName = fileName.replace("(", "\\(");
-
-			return fileName;
-		}
-
-		private final String _sourceFileName;
-		private final String _targetFileName;
-
-	}
 
 	private static class CommandPropagatorThread implements Runnable {
 
 		@Override
 		public void run() {
 			long start = System.currentTimeMillis();
-
-			List<CommandPropagatorTask> commandPropagatorTasks =
-				_commandPropagator._commandPropagatorTasks;
-
-			List<String> commands = new ArrayList<>(commandPropagatorTasks.size());
-
-			for (CommandPropagatorTask commandPropagatorTask : commandPropagatorTasks) {
-				commands.add(
-					_commandPropagator._getMkdirCommand(
-						commandPropagatorTask._targetFileName));
-
-				commands.add(
-					"rsync -svI " + _mirrorSlave + ":" +
-						commandPropagatorTask._targetFileName + " " +
-							commandPropagatorTask._targetFileName);
-			}
-
-			try {
-				_successful = _commandPropagator._executeBashCommands(
-					commands, _targetSlave) == 0;
-			}
-			catch (Exception e) {
-				_successful = false;
-			}
 
 			_duration = System.currentTimeMillis() - start;
 
