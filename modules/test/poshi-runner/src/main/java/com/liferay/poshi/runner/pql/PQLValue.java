pyoo47/liferay-portal

LRQA-26691 Add logic to use the 'PQLModifier' when evaluating values

modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/pql/PQLValue.java
@@ -65,6 +65,12 @@ public class PQLValue extends PQLEntity {
 			objectValue = value;
 		}
 
+		PQLModifier pqlModifier = getPQLModifier();
+
+		if (pqlModifier != null) {
+			objectValue = pqlModifier.modify(objectValue);
+		}
+
 		return objectValue;
 	}
 

liferay/liferay-central-test-2

modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/pql/PQLValue.java
@@ -65,6 +65,12 @@ public class PQLValue extends PQLEntity {
 			objectValue = value;
 		}
 
+		PQLModifier pqlModifier = getPQLModifier();
+
+		if (pqlModifier != null) {
+			objectValue = pqlModifier.getPQLResult(objectValue);
+		}
+
 		return objectValue;
 	}
 