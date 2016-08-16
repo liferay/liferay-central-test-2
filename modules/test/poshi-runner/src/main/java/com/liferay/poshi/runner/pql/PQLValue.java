pyoo47/liferay-portal

LRQA-26691 Add logic to remove 'PQLModifier' queries during validation

modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/pql/PQLValue.java
@@ -79,7 +79,7 @@ public class PQLValue extends PQLEntity {
 			return;
 		}
 
-		value.trim();
+		value = removeModifierFromQuery(value);
 
 		if ((value.startsWith("'") && value.endsWith("'")) ||
 			(value.startsWith("\"") && value.endsWith("\""))) {

liferay/liferay-central-test-2

modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/pql/PQLValue.java
@@ -79,7 +79,7 @@ public class PQLValue extends PQLEntity {
 			return;
 		}
 
-		value = value.trim();
+		value = removeModifierFromPQL(value);
 
 		if ((value.startsWith("'") && value.endsWith("'")) ||
 			(value.startsWith("\"") && value.endsWith("\""))) {