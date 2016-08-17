5ef44c73f24988bf1163bb3f254285376f4a36c5
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2016-08-17 11:00:07 +0200

    LPS-67418 Add polyfill for browsers that do not support picture element

---
+++ modules/apps/adaptive-media/adaptive-media-image-web/src/main/java/com/liferay/adaptive/media/image/web/servlet/AdaptiveMediaPictureDynamicInclude.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.adaptive.media.image.web.servlet;
+
+import com.liferay.portal.kernel.servlet.taglib.BaseDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Ambrin Chaudhary
+ */
+@Component(immediate = true, service = DynamicInclude.class)
+public class AdaptiveMediaPictureDynamicInclude extends BaseDynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest request, HttpServletResponse response,
+			String key)
+		throws IOException {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		PrintWriter printWriter = response.getWriter();
+
+		StringBundler sb = new StringBundler(7);
+
+		sb.append("<script src=\"");
+		sb.append(themeDisplay.getPortalURL());
+		sb.append(PortalUtil.getPathProxy());
+		sb.append(_servletContext.getContextPath());
+		sb.append("/picturefill.js\" ");
+		sb.append("type= \"text/javascript\">");
+		sb.append("</script>");
+
+		printWriter.println(sb.toString());
+	}
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register(
+			"/html/common/themes/top_head.jsp#post");
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.adaptive.media.image.web)"
+	)
+	private ServletContext _servletContext;
+
+}
