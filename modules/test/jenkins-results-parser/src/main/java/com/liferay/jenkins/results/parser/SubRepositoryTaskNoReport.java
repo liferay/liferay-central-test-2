9b1710849c9eacc62738f3fbaf5ced565a6bf8ea
Leslie Wong <leslie.wong@liferay.com>
2016-08-17 06:48:39 -0700

    LRQA-27107 Create classes for generating task result messages

---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/SubRepositoryTaskNoReport.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jenkins.results.parser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Leslie Wong
+ */
+public class SubRepositoryTaskNoReport extends SubRepositoryTask {
+
+	public SubRepositoryTaskNoReport(String console, String name)
+		throws Exception {
+
+		this.console = console;
+		this.name = name;
+
+		Matcher matcher = pattern.matcher(console);
+
+		if (matcher.find()) {
+			String taskStatus = matcher.group(1);
+
+			if (taskStatus.equals("SUCCESSFUL")) {
+				result = "SUCCESS";
+			}
+			else {
+				String errorMessage = matcher.group(2);
+
+				if (errorMessage.equals(": Parallel execution timed out")) {
+					result = "ABORTED";
+				}
+				else {
+					result = "FAILURE";
+				}
+			}
+		}
+		else {
+			result = "FAILURE";
+		}
+	}
+
+	@Override
+	public String getGitHubMessage() throws Exception {
+		StringBuilder sb = new StringBuilder();
+
+		GenericFailureMessageGenerator genericFailureMessageGenerator =
+			new GenericFailureMessageGenerator();
+
+		sb.append("<pre><code>");
+
+		sb.append(
+			genericFailureMessageGenerator.getMessage(null, console, null));
+
+		sb.append("</code></pre>");
+
+		return sb.toString();
+	}
+
+	protected static String console;
+	protected static final Pattern pattern = Pattern.compile(
+		"Task (SUCCESSFUL|FAILED)(.*)");
+
+}
