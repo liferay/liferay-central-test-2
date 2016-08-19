bb603414a3731fd5b008937ef62378cabcdff6f6
Máté Thurzó <mate.thurzo@liferay.com>
2016-08-19 11:37:32 +0200

    LPS-67650 Add new graph operations to the StagedModelRepository

--- modules/apps/web-experience/export-import/export-import-api/src/main/java/com/liferay/exportimport/staged/model/repository/StagedModelRepository.java
+++ modules/apps/web-experience/export-import/export-import-api/src/main/java/com/liferay/exportimport/staged/model/repository/StagedModelRepository.java
@@ -40,6 +40,12 @@ public interface StagedModelRepository<T extends StagedModel> {
 	public void deleteStagedModels(PortletDataContext portletDataContext)
 		throws PortalException;
 
+	public List<StagedModel> fetchChildrenStagedModels(
+		PortletDataContext portletDataContext, T stagedModel);
+
+	public List<StagedModel> fetchDependencyStagedModels(
+		PortletDataContext portletDataContext, T stagedModel);
+
 	public T fetchMissingReference(String uuid, long groupId);
 
 	public T fetchStagedModelByUuidAndGroupId(String uuid, long groupId);
