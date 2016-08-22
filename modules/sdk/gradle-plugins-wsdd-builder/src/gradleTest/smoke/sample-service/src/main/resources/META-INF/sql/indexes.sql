e052b06dda758cd410880354f2cd8b56d626fe2d
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-08-22 08:44:03 -0700

    LPS-67658 gradlew buildService in the gradle-plugins-wsdd-builder test

---
+++ modules/sdk/gradle-plugins-wsdd-builder/src/gradleTest/smoke/sample-service/src/main/resources/META-INF/sql/indexes.sql
@@ -0,0 +1,3 @@
+create index IX_B618923 on Sample_Foo (field2);
+create index IX_144FCE6B on Sample_Foo (uuid_[$COLUMN_LENGTH:75$], companyId);
+create unique index IX_C1BEF3AD on Sample_Foo (uuid_[$COLUMN_LENGTH:75$], groupId);
