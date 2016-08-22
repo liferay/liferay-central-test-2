0ccdf2c3b4039e9a3bb8b17932e7d7b30eeb78e4
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-08-22 16:12:13 +0200

    LPS-67296 Move query generation code to a single-responsibility class

---
+++ modules/util/portal-tools-data-partitioning-sql-builder/src/main/java/com/liferay/portal/tools/data/partitioning/sql/builder/exporter/InsertSQLBuilder.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.tools.data.partitioning.sql.builder.exporter;
+
+import com.liferay.portal.tools.data.partitioning.sql.builder.internal.exporter.SQLBuilder;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class InsertSQLBuilder implements SQLBuilder {
+
+	@Override
+	public String build(String[] fields, String tableName) {
+		if ((fields == null) || (fields.length == 0)) {
+			throw new IllegalArgumentException("Fields are null");
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("insert into ");
+		sb.append(tableName);
+		sb.append(" values (");
+
+		for (int i = 0; i < fields.length; i++) {
+			String field = fields[i];
+
+			sb.append(field);
+
+			if (i != (fields.length - 1)) {
+				sb.append(", ");
+			}
+		}
+
+		sb.append(")");
+
+		return sb.toString() + ";\n";
+	}
+
+}
