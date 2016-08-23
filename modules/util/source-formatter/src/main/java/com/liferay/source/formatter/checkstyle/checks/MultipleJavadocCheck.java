d37ae8f1796197e1dcec8ccb85f9aaeb704688ff
Hugo Huijser <hugo.huijser@liferay.com>
2016-08-23 16:35:37 +0800

    LPS-67352 Add custom check 'MultipleJavadocCheck'

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/MultipleJavadocCheck.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checkstyle.checks;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.FileContents;
+import com.puppycrawl.tools.checkstyle.api.TextBlock;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+/**
+ * @author Hugo Huijser
+ */
+public class MultipleJavadocCheck extends AbstractCheck {
+
+	public static final String MSG_MULTIPLE_JAVADOC = "javadoc.multiple";
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] {
+			TokenTypes.CLASS_DEF, TokenTypes.CTOR_DEF, TokenTypes.ENUM_DEF,
+			TokenTypes.INTERFACE_DEF, TokenTypes.METHOD_DEF,
+			TokenTypes.VARIABLE_DEF
+		};
+	}
+
+	@Override
+	public void visitToken(DetailAST detailAST) {
+		FileContents fileContents = getFileContents();
+
+		TextBlock javadoc = fileContents.getJavadocBefore(
+			detailAST.getLineNo());
+
+		if (javadoc == null) {
+			return;
+		}
+
+		javadoc = fileContents.getJavadocBefore(javadoc.getStartLineNo());
+
+		if (javadoc != null) {
+			DetailAST nameAST = detailAST.findFirstToken(TokenTypes.IDENT);
+
+			log(detailAST.getLineNo(), MSG_MULTIPLE_JAVADOC, nameAST.getText());
+		}
+	}
+
+}
