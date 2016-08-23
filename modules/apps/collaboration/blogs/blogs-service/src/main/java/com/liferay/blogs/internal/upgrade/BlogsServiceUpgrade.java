75806f27efe97c7c51e3dc3706625290d7295b28
Adolfo Pérez <adolfo.perez@liferay.com>
2016-08-23 15:43:14 +0200

    LPS-67467 Create upgrade process

---
+++ modules/apps/collaboration/blogs/blogs-service/src/main/java/com/liferay/blogs/internal/upgrade/BlogsServiceUpgrade.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.blogs.internal.upgrade;
+
+import com.liferay.blogs.internal.upgrade.v1_1_0.UpgradeFriendlyURL;
+import com.liferay.friendly.url.service.FriendlyURLLocalService;
+import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(immediate = true, service = UpgradeStepRegistrator.class)
+public class BlogsServiceUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"com.liferay.blogs.service", "0.0.0", "1.0.0",
+			new DummyUpgradeStep());
+		registry.register(
+			"com.liferay.blogs.service", "1.0.0", "1.1.0",
+			new UpgradeFriendlyURL());
+	}
+
+	@Reference(unbind = "-")
+	protected void setFriendlyURLLocalService(
+		FriendlyURLLocalService friendlyURLLocalService) {
+	}
+
+}
---
+++ modules/apps/collaboration/blogs/blogs-service/src/main/java/com/liferay/blogs/internal/upgrade/v1_1_0/UpgradeFriendlyURL.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.blogs.internal.upgrade.v1_1_0;
+
+import com.liferay.blogs.kernel.model.BlogsEntry;
+import com.liferay.friendly.url.service.FriendlyURLLocalServiceUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class UpgradeFriendlyURL extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		try (PreparedStatement ps1 = connection.prepareStatement(
+				"select companyId, groupId, entryId, urlTitle from " +
+					"BlogsEntry")) {
+
+			ResultSet rs = ps1.executeQuery();
+
+			while (rs.next()) {
+				long companyId = rs.getLong(1);
+				long groupId = rs.getLong(2);
+				long classPK = rs.getLong(3);
+				String urlTitle = rs.getString(4);
+
+				FriendlyURLLocalServiceUtil.addFriendlyURL(
+					companyId, groupId, BlogsEntry.class, classPK, urlTitle);
+			}
+		}
+	}
+
+}
