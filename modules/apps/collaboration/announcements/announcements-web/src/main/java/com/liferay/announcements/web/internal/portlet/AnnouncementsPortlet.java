d91d791288a2ee365b70261d7f76cc8c89402e12
Roberto Díaz <roberto.diaz@liferay.com>
2016-08-23 15:06:48 +0200

    LPS-67115 Create new  Upgrade process that will convert the ALERTS and ANNOUNCEMENTS portlet #ADD_ENTRY permission to ANNOUNCEMENTS_ADMIN#VIEW permission

--- modules/apps/collaboration/announcements/announcements-web/src/main/java/com/liferay/announcements/web/internal/portlet/AnnouncementsPortlet.java
+++ modules/apps/collaboration/announcements/announcements-web/src/main/java/com/liferay/announcements/web/internal/portlet/AnnouncementsPortlet.java
@@ -54,7 +54,7 @@ import org.osgi.service.component.annotations.Reference;
 public class AnnouncementsPortlet extends MVCPortlet {
 
 	@Reference(
-		target = "(&(release.bundle.symbolic.name=com.liferay.announcements.web)(release.schema.version=1.0.1))",
+		target = "(&(release.bundle.symbolic.name=com.liferay.announcements.web)(release.schema.version=1.0.2))",
 		unbind = "-"
 	)
 	protected void setRelease(Release release) {
