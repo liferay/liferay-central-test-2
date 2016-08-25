c6bed7dd1533b25ceabae284e7891b5776e37850
antoniopol06 <antonio.pol@liferay.com>
2016-08-25 11:41:29 +0200

    LPS-67203 Create AlloyEditor Accessibility Module

---
+++ modules/apps/foundation/frontend-editor/frontend-editor-alloyeditor-accessibility-web/src/main/java/com/liferay/frontend/editor/alloyeditor/accessibility/web/internal/AlloyEditorAccessibilityDynamicInclude.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editor.alloyeditor.accessibility.web.internal;
+
+import com.liferay.portal.kernel.servlet.taglib.BaseDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Antonio Pol
+ */
+@Component(immediate = true, service = DynamicInclude.class)
+public class AlloyEditorAccessibilityDynamicInclude extends BaseDynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest request, HttpServletResponse response,
+			String key)
+		throws IOException {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		PrintWriter printWriter = response.getWriter();
+
+		StringBundler sb = new StringBundler(7);
+
+		sb.append("<script src=\"");
+		sb.append(themeDisplay.getPortalURL());
+		sb.append(PortalUtil.getPathProxy());
+		sb.append(_servletContext.getContextPath());
+		sb.append("/js/button_alt_image.js");
+		sb.append("\" ");
+		sb.append("type=\"text/javascript\"></script>");
+
+		printWriter.println(sb.toString());
+	}
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register(
+			"com.liferay.frontend.editor.alloyeditor.accessibility.web#" +
+				"additionalModules");
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.frontend.editor.alloyeditor.accessibility.web)"
+	)
+	private ServletContext _servletContext;
+
+}
---
+++ modules/apps/foundation/frontend-editor/frontend-editor-alloyeditor-accessibility-web/src/main/resources/META-INF/resources/js/button_alt_image.js
@@ -0,0 +1,199 @@
+/* global React, AlloyEditor */
+
+(function() {
+	'use strict';
+
+	var React = AlloyEditor.React;
+
+	var ButtonAltImage = React.createClass(
+		{
+			mixins: [AlloyEditor.ButtonStateClasses, AlloyEditor.ButtonCfgProps],
+
+			displayName: 'ButtonAltImage',
+
+			propTypes: {
+				editor: React.PropTypes.object.isRequired
+			},
+
+			statics: {
+				key: 'altImage'
+			},
+
+			/**
+			 * Lifecycle. Invoked once before the component is mounted.
+			 * The return value will be used as the initial value of this.state.
+			 *
+			 * @method getInitialState
+			 */
+			getInitialState: function() {
+				var image = this.props.editor.get('nativeEditor').getSelection().getSelectedElement();
+
+				return {
+					altImage: image.getAttribute('alt'),
+					element: image
+				};
+			},
+
+			/**
+			 * Lifecycle. Renders the UI of the button.
+			 * Rendering alt button or alt form to update the alt´s image
+			 * depend on renderExclusive property
+			 *
+			 * @method render
+			 * @return {Object} The content which should be rendered.
+			 */
+			render: function() {
+				var cssClass = 'ae-button ' + this.getStateClasses();
+
+				var element;
+
+				if (this.props.renderExclusive) {
+
+					element = React.createElement(
+						'div',
+						{
+							className: 'ae-container-edit-link'
+						},
+						React.createElement(
+							'div',
+							{
+								className: 'ae-container-input xxl'
+							},
+							React.createElement(
+								'input',
+								{
+									ariaLabel: 'Alt',
+									className: 'ae-input',
+									onChange: this._handleAltChange,
+									onKeyDown: this._handleKeyDown,
+									placeholder: 'Alt',
+									ref: 'refAltInput',
+									title: 'Alt',
+									value: this.state.altImage
+								}
+							)
+						),
+						React.createElement(
+							'button',
+							{
+								className: 'ae-button',
+								onClick: this._updateAltImage
+							},
+							React.createElement(
+								'span',
+								{
+									className: 'ae-icon-ok'
+								}
+							)
+						)
+					);
+				}
+				else {
+					element = React.createElement(
+						'button',
+						{
+							className: cssClass,
+							onClick: this._requestExclusive
+						},
+						React.createElement(
+							'small',
+							{
+								className: 'ae-icon small'
+							},
+							'Alt'
+						)
+					);
+				}
+
+				return element;
+			},
+
+			/**
+			 * Focuses the user cursor on the widget's input.
+			 *
+			 * @protected
+			 * @method _focusAltInput
+			 */
+			_focusAltInput: function() {
+				var instance = this;
+
+				var focusLinkEl = function() {
+					AlloyEditor.ReactDOM.findDOMNode(instance.refs.refAltInput).focus();
+				};
+
+				if (window.requestAnimationFrame) {
+					window.requestAnimationFrame(focusLinkEl);
+				}
+				else {
+					setTimeout(focusLinkEl, 0);
+				}
+
+			},
+
+			/**
+			 * Event attached to alt input that fires when its value is changed
+			 *
+			 * @protected
+			 * @param {MouseEvent} event
+			 */
+			_handleAltChange: function(event) {
+				this.setState({ altImage: event.target.value });
+
+				this._focusAltInput();
+			},
+
+			/**
+			 * Event attached to al tinput that fires when key is down
+			 * This method check that enter key is pushed to update the component´s state
+			 *
+			 * @protected
+			 * @param {MouseEvent} event
+			 */
+			_handleKeyDown: function(event) {
+				if (event.keyCode === 13) {
+					event.preventDefault();
+					this._updateAltImage();
+				}
+			},
+
+			/**
+			 * Requests the link button to be rendered in exclusive mode to allow the creation of a link.
+			 *
+			 * @protected
+			 * @method _requestExclusive
+			 */
+			_requestExclusive: function() {
+				this.props.requestExclusive(ButtonAltImage.key);
+			},
+
+			/**
+			 * Method called by clicking ok button or pushing key enter to update altImage state and to update alt property from the image that is selected
+			 * This method calls cancelExclusive to show the previous toolbar before enter to edit alt property
+			 *
+			 * @protected
+			 */
+			_updateAltImage: function() {
+				var editor = this.props.editor.get('nativeEditor');
+
+				var newValue = this.refs.refAltInput.value;
+
+				this.setState(
+					{
+						altImage: newValue
+					}
+				);
+
+				this.state.element.setAttribute('alt', newValue);
+
+				editor.fire('actionPerformed', this);
+
+				// We need to cancelExclusive with the bound parameters in case the button is used
+				// inside another in exclusive mode (such is the case of the alt button)
+				this.props.cancelExclusive();
+
+			}
+		}
+	);
+
+	AlloyEditor.Buttons[ButtonAltImage.key] = AlloyEditor.ButtonAltImage = ButtonAltImage;
+}());
