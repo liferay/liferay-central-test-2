0aaac2a435081bca6313cb951a9e207b19ea9c62
Jorge Díaz <jorge.diaz@liferay.com>
2016-08-25 14:07:24 +0200

    Revert "LPS-66705 Do not reindex related DDMStructure webcontents when updating only default values"
    This reverts commit 4224a319be7a2ff6fa5d1f4c743150569091f617.

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMStructureLocalServiceImpl.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMStructureLocalServiceImpl.java
@@ -1724,10 +1724,6 @@ public class DDMStructureLocalServiceImpl
 			DDMStructure structure, ServiceContext serviceContext)
 		throws PortalException {
 
-		if (!serviceContext.isIndexingEnabled()) {
-			return;
-		}
-
 		Indexer<?> indexer = IndexerRegistryUtil.nullSafeGetIndexer(
 			structure.getClassName());
 
