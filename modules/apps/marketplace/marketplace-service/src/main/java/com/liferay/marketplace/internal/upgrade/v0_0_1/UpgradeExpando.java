859a5a0b5981ba1f60d4458359d9de986cbe25b7
Ryan Park <ryan.park@liferay.com>
2016-08-25 16:46:19 -0700

    LPS-67695 Move upgrade code higher for older Liferay versions that don't have a starting point

--- modules/apps/marketplace/marketplace-service/src/main/java/com/liferay/marketplace/internal/upgrade/v0_0_1/UpgradeExpando.java
+++ modules/apps/marketplace/marketplace-service/src/main/java/com/liferay/marketplace/internal/upgrade/v0_0_1/UpgradeExpando.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.marketplace.internal.upgrade.v1_0_0;
+package com.liferay.marketplace.internal.upgrade.v0_0_1;
 
 import com.liferay.expando.kernel.exception.NoSuchTableException;
 import com.liferay.expando.kernel.model.ExpandoColumn;
