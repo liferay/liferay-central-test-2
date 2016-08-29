e27a8e71ecc118b4ec1989a8eb8d9ce2de1f930c
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-08-29 11:45:55 -0700

    LPS-67552 Remove unused util methods

--- modules/sdk/project-templates/src/main/java/com/liferay/project/templates/internal/util/FileUtil.java
+++ modules/sdk/project-templates/src/main/java/com/liferay/project/templates/internal/util/FileUtil.java
@@ -14,71 +14,8 @@
 
 package com.liferay.project.templates.internal.util;
 
-import java.io.IOException;
-
-import java.nio.charset.StandardCharsets;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import java.util.Arrays;
-import java.util.Map;
-
 /**
  * @author Andrea Di Giorgi
  */
 public class FileUtil {
-
-	public static final DirectoryStream.Filter<Path> directoriesOnlyFilter =
-		new DirectoryStream.Filter<Path>() {
-
-			@Override
-			public boolean accept(Path path) throws IOException {
-				if (Files.isDirectory(path)) {
-					return true;
-				}
-
-				return false;
-			}
-
-		};
-
-	public static void replace(Path path, Map<String, String> replacements)
-		throws IOException {
-
-		String extension = _getExtension(path);
-
-		if (Arrays.binarySearch(_TEXT_EXTENSIONS, extension) < 0) {
-			return;
-		}
-
-		String content = new String(
-			Files.readAllBytes(path), StandardCharsets.UTF_8);
-
-		String newContent = StringUtil.replace(content, replacements);
-
-		if (!content.equals(newContent)) {
-			Files.write(path, newContent.getBytes(StandardCharsets.UTF_8));
-		}
-	}
-
-	private static String _getExtension(Path path) {
-		Path fileNamePath = path.getFileName();
-
-		String fileName = fileNamePath.toString();
-
-		int pos = fileName.lastIndexOf('.');
-
-		if (pos == -1) {
-			return "";
-		}
-
-		return fileName.substring(pos + 1);
-	}
-
-	private static final String[] _TEXT_EXTENSIONS = {
-		"bnd", "css", "gradle", "java", "js", "jsp", "jspf", "prefs", "project",
-		"properties", "xml"
-	};
-
 }
