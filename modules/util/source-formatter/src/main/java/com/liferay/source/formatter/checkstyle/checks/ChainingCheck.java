b84a957d8f5e195da757ae1075d7c5fac638c198
Hugo Huijser <hugo.huijser@liferay.com>
2016-08-29 12:27:10 +0800

    LPS-67352 Add 'ChainingCheck'

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/ChainingCheck.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checkstyle.checks;
+
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.source.formatter.checkstyle.util.DetailASTUtil;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+import java.util.List;
+
+/**
+ * @author Hugo Huijser
+ */
+public class ChainingCheck extends AbstractCheck {
+
+	public static final String MSG_AVOID_CHAINING = "chaining.avoid";
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] {TokenTypes.CTOR_DEF, TokenTypes.METHOD_DEF};
+	}
+
+	@Override
+	public void visitToken(DetailAST detailAST) {
+		List<DetailAST> methodCallASTList = DetailASTUtil.getAllChildTokens(
+			detailAST, TokenTypes.METHOD_CALL, true);
+
+		for (DetailAST methodCallAST : methodCallASTList) {
+			String chainedMethodNames = _getChainedMethodNames(methodCallAST);
+
+			if (!chainedMethodNames.contains(StringPool.PERIOD)) {
+				continue;
+			}
+
+			_checkMethodName(
+				chainedMethodNames, "getClass", methodCallAST, detailAST);
+		}
+	}
+
+	private void _checkMethodName(
+		String chainedMethodNames, String methodName, DetailAST methodCallAST,
+		DetailAST detailAST) {
+
+		if (chainedMethodNames.matches(methodName + "\\..*") &&
+			!_isInsideConstructorThisCall(methodCallAST, detailAST)) {
+
+			log(methodCallAST.getLineNo(), MSG_AVOID_CHAINING, methodName);
+		}
+	}
+
+	private String _getChainedMethodNames(DetailAST methodCallAST) {
+		StringBundler sb = new StringBundler();
+
+		sb.append(DetailASTUtil.getMethodName(methodCallAST));
+
+		while (true) {
+			DetailAST parentAST = methodCallAST.getParent();
+
+			if (parentAST.getType() != TokenTypes.DOT) {
+				return sb.toString();
+			}
+
+			methodCallAST = parentAST.getParent();
+
+			if (methodCallAST.getType() != TokenTypes.METHOD_CALL) {
+				return sb.toString();
+			}
+
+			sb.append(StringPool.PERIOD);
+			sb.append(DetailASTUtil.getMethodName(methodCallAST));
+		}
+	}
+
+	private boolean _isInsideConstructorThisCall(
+		DetailAST methodCallAST, DetailAST detailAST) {
+
+		if (detailAST.getType() != TokenTypes.CTOR_DEF) {
+			return false;
+		}
+
+		DetailAST parentAST = methodCallAST.getParent();
+
+		while (parentAST != null) {
+			String parentASTText = parentAST.getText();
+
+			if ((parentAST.getType() == TokenTypes.CTOR_CALL) &&
+				parentASTText.equals("this")) {
+
+				return true;
+			}
+
+			parentAST = parentAST.getParent();
+		}
+
+		return false;
+	}
+
+}
