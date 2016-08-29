10c848d0f517faa8f2a30dea6e96f72287faa13f
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-08-29 09:47:50 -0700

    LPS-67552 Remove gradle-templates

--- modules/sdk/gradle-templates/src/main/resources/standalone/panelapp/src/main/java/_package_path_/constants/_CLASS_PanelCategoryKeys.java
+++
@@ -1,7 +0,0 @@
-package _package_.constants;
-
-public class _CLASS_PanelCategoryKeys {
-
-	public static final String CONTROL_PANEL_CATEGORY = "_CLASS_";
-
-}
--- modules/sdk/gradle-templates/src/main/resources/standalone/portletprovider/src/main/resources/META-INF/resources/view.jsp
+++
@@ -1,3 +0,0 @@
-<%@ include file="/init.jsp" %>
-
-<%= request.getAttribute(_CLASS_WebKeys.PORTLET_PROVIDER_MESSAGE) %>
