4b2368f7290554f777e87b0205bf184088d08c90
Brian Chan <brian.chan@liferay.com>
2016-08-30 16:51:13 -0700

    LPS-67652 prep next version

--- portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java
@@ -99,6 +99,8 @@ public class ReleaseInfo {
 
 	public static final int RELEASE_7_0_2_BUILD_NUMBER = 7002;
 
+	public static final int RELEASE_7_0_3_BUILD_NUMBER = 7003;
+
 	public static final int RELEASE_7_0_10_BUILD_NUMBER = 7010;
 
 	public static final Date getBuildDate() {
@@ -150,7 +152,7 @@ public class ReleaseInfo {
 		return _VERSION;
 	}
 
-	private static final String _BUILD = "7002";
+	private static final String _BUILD = "7003";
 
 	private static final int _BUILD_NUMBER = GetterUtil.getInteger(_BUILD);
 
@@ -170,9 +172,9 @@ public class ReleaseInfo {
 
 	private static final String _VENDOR = "Liferay, Inc.";
 
-	private static final String _VERSION = "7.0.2";
+	private static final String _VERSION = "7.0.3";
 
-	private static final String _VERSION_DISPLAY_NAME = "7.0.2 GA3";
+	private static final String _VERSION_DISPLAY_NAME = "7.0.3 GA4";
 
 	private static String _releaseInfo;
 	private static String _serverInfo;
