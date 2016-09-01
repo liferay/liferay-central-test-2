d8d85262467df4791862ef9865d4184713ee2be9
Chema Balsas <jose.balsas@liferay.com>
2016-09-01 12:21:43 +0200

    LPS-67914 Move TopSearch to a component

---
+++ modules/apps/frontend-theme-porygon/frontend-theme-porygon/src/js/config.js
@@ -0,0 +1,8 @@
+Loader.addModule(
+	{
+		anonymous: true,
+		dependencies: ['metal/src/async/async', 'metal/src/core', 'metal-dom/src/dom', 'metal-state/src/State'],
+		name: 'porygon-theme/js/top_search.es',
+		path: MODULE_PATH + '/js/top_search.es.js'
+	}
+);
