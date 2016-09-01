a527416c6aa193c7f9050b103cf041380cfdbcb5
Chema Balsas <jose.balsas@liferay.com>
2016-09-01 12:19:20 +0200

    LPS-67914 Add support for ES2015+

--- modules/apps/frontend-theme-porygon/frontend-theme-porygon/gulpfile.js
+++ modules/apps/frontend-theme-porygon/frontend-theme-porygon/gulpfile.js
@@ -2,8 +2,32 @@
 
 var gulp = require('gulp');
 var liferayThemeTasks = require('liferay-theme-tasks');
+var metal = require('gulp-metal');
+var runSequence = require('run-sequence').use(gulp);
+
+metal.registerTasks(
+	{
+		base: 'src/js',
+		buildAmdDest: 'build',
+		buildSrc: 'src/js/**/*.es.js',
+		moduleName: 'js',
+		taskPrefix: 'metal:'
+	}
+);
 
 liferayThemeTasks.registerTasks(
 	{
-		gulp: gulp
-	});
+		gulp: gulp,
+		hookFn: function(gulp) {
+			gulp.hook(
+				'after:build:src',
+				function(done) {
+					runSequence(
+						'metal:build:amd',
+						done
+					);
+				}
+			);
+		}
+	}
+);
