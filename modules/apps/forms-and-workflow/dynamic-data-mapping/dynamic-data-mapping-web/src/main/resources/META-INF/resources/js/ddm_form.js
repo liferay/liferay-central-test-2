e309b8d0c8b0136846dc06cf9a30121d47a96955
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-09-02 14:11:29 +0200

    LPS-67761 Reset breadcrumb and navbar

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -1667,6 +1667,8 @@ AUI.add(
 
 						instance._clearedModal = true;
 
+						instance._navbar.one('.active').removeClass('active');
+
 						instance.setValue('');
 
 						instance.set('selectedLayout', instance.get('selectedLayoutPath')[0]);
@@ -1889,8 +1891,11 @@ AUI.add(
 						}
 						else {
 							if (instance._clearedModal) {
-								instance._renderLayoutsList(privateLayout);
+								var activeClass = privateLayout ? '.private' : '.public';
 
+								instance._navbar.one(activeClass).addClass('active');
+								instance._resetBreadcrumb(privateLayout);
+								instance._renderLayoutsList(privateLayout);
 								instance._clearedModal = false;
 							}
 						}
