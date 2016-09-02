8a685364b9e0e16936a8f4975233addcaed05adf
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-09-02 14:09:10 +0200

    LPS-67761 Using _clearedModal flag

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -1380,6 +1380,8 @@ AUI.add(
 
 						instance._cache = {};
 
+						instance._clearedModal = false;
+
 						instance.after('selectedLayoutChange', instance._afterSelectedLayoutChange);
 						instance.after('selectedLayoutPathChange', instance._afterSelectedLayoutPathChange);
 
@@ -1666,6 +1668,8 @@ AUI.add(
 					_handleClearButtonClick: function() {
 						var instance = this;
 
+						instance._clearedModal = true;
+
 						instance.setValue('');
 
 						instance.set('selectedLayout', instance.get('selectedLayoutPath')[0]);
@@ -1887,7 +1891,11 @@ AUI.add(
 							listNode.on('scroll', instance._handleModalScroll, instance);
 						}
 						else {
-							instance._renderLayoutsList(privateLayout);
+							if (instance._clearedModal) {
+								instance._renderLayoutsList(privateLayout);
+
+								instance._clearedModal = false;
+							}
 						}
 
 						modal.show();
