mbowerman/liferay-portal

LPS-62735 Events spawned from a recurring booking no longer display in the recycle bin

modules/apps/business-productivity/calendar/calendar-service/src/main/java/com/liferay/calendar/service/impl/CalendarBookingLocalServiceImpl.java
@@ -858,6 +858,23 @@ public class CalendarBookingLocalServiceImpl
 			calendarBooking.getCalendarBookingId(), calendarBooking,
 			serviceContext);
 
+		if (calendarBooking.isMasterRecurringBooking()) {
+			List<CalendarBooking> relatedCalendarBookings =
+				getRelatedRecurringCalendarBookings(calendarBooking);
+
+			for (CalendarBooking relatedCalendarBooking :
+					relatedCalendarBookings) {
+
+				if (relatedCalendarBooking.equals(calendarBooking)) {
+					continue;
+				}
+
+				calendarBookingLocalService.updateStatus(
+					userId, relatedCalendarBooking, trashEntry.getStatus(),
+					serviceContext);
+			}
+		}
+
 		return calendarBooking;
 	}
 
@@ -1509,26 +1526,28 @@ public class CalendarBookingLocalServiceImpl
 		}
 
 		if (status == CalendarBookingWorkflowConstants.STATUS_IN_TRASH) {
-			if (calendarBooking.isMasterBooking()) {
-				trashEntryLocalService.addTrashEntry(
-					userId, calendarBooking.getGroupId(),
-					CalendarBooking.class.getName(),
-					calendarBooking.getCalendarBookingId(),
-					calendarBooking.getUuid(), null, oldStatus, null, null);
-			}
-			else {
-				trashEntryLocalService.addTrashEntry(
-					userId, calendarBooking.getGroupId(),
-					CalendarBooking.class.getName(),
-					calendarBooking.getCalendarBookingId(),
-					calendarBooking.getUuid(), null,
-					CalendarBookingWorkflowConstants.STATUS_PENDING, null,
-					null);
-			}
+			if (calendarBooking.isMasterRecurringBooking()) {
+				if (calendarBooking.isMasterBooking()) {
+					trashEntryLocalService.addTrashEntry(
+						userId, calendarBooking.getGroupId(),
+						CalendarBooking.class.getName(),
+						calendarBooking.getCalendarBookingId(),
+						calendarBooking.getUuid(), null, oldStatus, null, null);
+				}
+				else {
+					trashEntryLocalService.addTrashEntry(
+						userId, calendarBooking.getGroupId(),
+						CalendarBooking.class.getName(),
+						calendarBooking.getCalendarBookingId(),
+						calendarBooking.getUuid(), null,
+						CalendarBookingWorkflowConstants.STATUS_PENDING, null,
+						null);
+				}
 
-			sendNotification(
-				calendarBooking, NotificationTemplateType.MOVED_TO_TRASH,
-				serviceContext);
+				sendNotification(
+					calendarBooking, NotificationTemplateType.MOVED_TO_TRASH,
+					serviceContext);
+			}
 		}
 
 		return calendarBooking;

liferay/liferay-central-test-2

modules/apps/forms-and-workflow/calendar/calendar-service/src/main/java/com/liferay/calendar/service/impl/CalendarBookingLocalServiceImpl.java
@@ -834,6 +834,23 @@ public class CalendarBookingLocalServiceImpl
 			calendarBooking.getCalendarBookingId(), calendarBooking,
 			serviceContext);
 
+		if (calendarBooking.isMasterRecurringBooking()) {
+			List<CalendarBooking> relatedCalendarBookings =
+				getRelatedCalendarBookings(calendarBooking);
+
+			for (CalendarBooking relatedCalendarBooking :
+					relatedCalendarBookings) {
+
+				if (relatedCalendarBooking.equals(calendarBooking)) {
+					continue;
+				}
+
+				calendarBookingLocalService.updateStatus(
+					userId, relatedCalendarBooking, trashEntry.getStatus(),
+					serviceContext);
+			}
+		}
+
 		return calendarBooking;
 	}
 
@@ -1324,26 +1341,28 @@ public class CalendarBookingLocalServiceImpl
 		}
 
 		if (status == CalendarBookingWorkflowConstants.STATUS_IN_TRASH) {
-			if (calendarBooking.isMasterBooking()) {
-				trashEntryLocalService.addTrashEntry(
-					userId, calendarBooking.getGroupId(),
-					CalendarBooking.class.getName(),
-					calendarBooking.getCalendarBookingId(),
-					calendarBooking.getUuid(), null, oldStatus, null, null);
-			}
-			else {
-				trashEntryLocalService.addTrashEntry(
-					userId, calendarBooking.getGroupId(),
-					CalendarBooking.class.getName(),
-					calendarBooking.getCalendarBookingId(),
-					calendarBooking.getUuid(), null,
-					CalendarBookingWorkflowConstants.STATUS_PENDING, null,
-					null);
-			}
+			if (calendarBooking.isMasterRecurringBooking()) {
+				if (calendarBooking.isMasterBooking()) {
+					trashEntryLocalService.addTrashEntry(
+						userId, calendarBooking.getGroupId(),
+						CalendarBooking.class.getName(),
+						calendarBooking.getCalendarBookingId(),
+						calendarBooking.getUuid(), null, oldStatus, null, null);
+				}
+				else {
+					trashEntryLocalService.addTrashEntry(
+						userId, calendarBooking.getGroupId(),
+						CalendarBooking.class.getName(),
+						calendarBooking.getCalendarBookingId(),
+						calendarBooking.getUuid(), null,
+						CalendarBookingWorkflowConstants.STATUS_PENDING, null,
+						null);
+				}
 
-			sendNotification(
-				calendarBooking, NotificationTemplateType.MOVED_TO_TRASH,
-				serviceContext);
+				sendNotification(
+					calendarBooking, NotificationTemplateType.MOVED_TO_TRASH,
+					serviceContext);
+			}
 		}
 
 		return calendarBooking;