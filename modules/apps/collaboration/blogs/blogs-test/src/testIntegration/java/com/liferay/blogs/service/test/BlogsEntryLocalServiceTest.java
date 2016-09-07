ae1ef8f9133ddad5b1e940fc6a3c959fec7e0b20
Adolfo Pérez <adolfo.perez@liferay.com>
2016-09-07 17:35:14 +0200

    LPS-66976 gradle formatSource

--- modules/apps/collaboration/blogs/blogs-test/src/testIntegration/java/com/liferay/blogs/service/test/BlogsEntryLocalServiceTest.java
+++ modules/apps/collaboration/blogs/blogs-test/src/testIntegration/java/com/liferay/blogs/service/test/BlogsEntryLocalServiceTest.java
@@ -98,26 +98,6 @@ public class BlogsEntryLocalServiceTest {
 	}
 
 	@Test
-	public void testAddDraftEntryWithNullTitle() throws Exception {
-		int initialCount = BlogsEntryLocalServiceUtil.getGroupEntriesCount(
-			_group.getGroupId(), _statusAnyQueryDefinition);
-
-		ServiceContext serviceContext =
-			ServiceContextTestUtil.getServiceContext(_group, _user.getUserId());
-
-		serviceContext.setWorkflowAction(WorkflowConstants.ACTION_SAVE_DRAFT);
-
-		BlogsEntryLocalServiceUtil.addEntry(
-			_user.getUserId(), null, RandomTestUtil.randomString(),
-			serviceContext);
-
-		int actualCount = BlogsEntryLocalServiceUtil.getGroupEntriesCount(
-			_group.getGroupId(), _statusAnyQueryDefinition);
-
-		Assert.assertEquals(initialCount + 1, actualCount);
-	}
-
-	@Test
 	public void testAddDraftEntryWithBlankTitle() throws Exception {
 		int initialCount = BlogsEntryLocalServiceUtil.getGroupEntriesCount(
 			_group.getGroupId(), _statusAnyQueryDefinition);
@@ -129,6 +109,26 @@ public class BlogsEntryLocalServiceTest {
 
 		BlogsEntryLocalServiceUtil.addEntry(
 			_user.getUserId(), StringPool.BLANK, RandomTestUtil.randomString(),
+			serviceContext);
+
+		int actualCount = BlogsEntryLocalServiceUtil.getGroupEntriesCount(
+			_group.getGroupId(), _statusAnyQueryDefinition);
+
+		Assert.assertEquals(initialCount + 1, actualCount);
+	}
+
+	@Test
+	public void testAddDraftEntryWithNullTitle() throws Exception {
+		int initialCount = BlogsEntryLocalServiceUtil.getGroupEntriesCount(
+			_group.getGroupId(), _statusAnyQueryDefinition);
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(_group, _user.getUserId());
+
+		serviceContext.setWorkflowAction(WorkflowConstants.ACTION_SAVE_DRAFT);
+
+		BlogsEntryLocalServiceUtil.addEntry(
+			_user.getUserId(), null, RandomTestUtil.randomString(),
 			serviceContext);
 
 		int actualCount = BlogsEntryLocalServiceUtil.getGroupEntriesCount(
@@ -553,22 +553,22 @@ public class BlogsEntryLocalServiceTest {
 	}
 
 	@Test(expected = EntryTitleException.class)
-	public void testPublishEntryWithNullTitle() throws Exception {
-		ServiceContext serviceContext =
-			ServiceContextTestUtil.getServiceContext(_group, _user.getUserId());
-
-		BlogsEntryLocalServiceUtil.addEntry(
-			_user.getUserId(), null, RandomTestUtil.randomString(),
-			serviceContext);
-	}
-
-	@Test(expected = EntryTitleException.class)
 	public void testPublishEntryWithBlankTitle() throws Exception {
 		ServiceContext serviceContext =
 			ServiceContextTestUtil.getServiceContext(_group, _user.getUserId());
 
 		BlogsEntryLocalServiceUtil.addEntry(
 			_user.getUserId(), StringPool.BLANK, RandomTestUtil.randomString(),
+			serviceContext);
+	}
+
+	@Test(expected = EntryTitleException.class)
+	public void testPublishEntryWithNullTitle() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(_group, _user.getUserId());
+
+		BlogsEntryLocalServiceUtil.addEntry(
+			_user.getUserId(), null, RandomTestUtil.randomString(),
 			serviceContext);
 	}
 
