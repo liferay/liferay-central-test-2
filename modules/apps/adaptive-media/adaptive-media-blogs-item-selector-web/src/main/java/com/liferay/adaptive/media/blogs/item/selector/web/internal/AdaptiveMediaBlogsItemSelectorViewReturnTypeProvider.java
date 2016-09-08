3786471728713c3dd42e99488eff2cdd0e07a1f5
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2016-09-08 13:50:17 +0200

    LPS-68045 Add support to the blogs view so it can support image adaptive media return type

---
+++ modules/apps/adaptive-media/adaptive-media-blogs-item-selector-web/src/main/java/com/liferay/adaptive/media/blogs/item/selector/web/internal/AdaptiveMediaBlogsItemSelectorViewReturnTypeProvider.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.adaptive.media.blogs.item.selector.web.internal;
+
+import com.liferay.adaptive.media.image.item.selector.ImageAdaptiveMediaURLItemSelectorReturnType;
+import com.liferay.blogs.item.selector.web.constants.BlogsItemSelectorViewConstants;
+import com.liferay.item.selector.ItemSelectorReturnType;
+import com.liferay.item.selector.ItemSelectorViewReturnTypeProvider;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Sergio González
+ */
+@Component(
+	property = {
+		"item.selector.view.key=" + BlogsItemSelectorViewConstants.ITEM_SELECTOR_VIEW_KEY
+	},
+	service = ItemSelectorViewReturnTypeProvider.class
+)
+public class AdaptiveMediaBlogsItemSelectorViewReturnTypeProvider
+	implements ItemSelectorViewReturnTypeProvider {
+
+	public List<ItemSelectorReturnType>
+		populateSupportedItemSelectorReturnTypes(
+			List<ItemSelectorReturnType> supportedItemSelectorReturnTypes) {
+
+		supportedItemSelectorReturnTypes.add(
+			new ImageAdaptiveMediaURLItemSelectorReturnType());
+
+		return supportedItemSelectorReturnTypes;
+	}
+
+}
