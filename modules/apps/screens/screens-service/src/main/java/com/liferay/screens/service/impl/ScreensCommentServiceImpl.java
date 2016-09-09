de96cb22d85c3426ac52343b8058bdb70200e25e
Alejandro Hernández <info@alejandrohdezma.com>
2016-09-09 12:40:54 +0200

    LPS-67386 sf

--- modules/apps/screens/screens-service/src/main/java/com/liferay/screens/service/impl/ScreensCommentServiceImpl.java
+++ modules/apps/screens/screens-service/src/main/java/com/liferay/screens/service/impl/ScreensCommentServiceImpl.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- * <p>
+ *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- * <p>
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
@@ -20,6 +20,7 @@ import com.liferay.portal.kernel.comment.DiscussionPermission;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.Function;
@@ -33,24 +34,23 @@ import java.util.Locale;
 /**
  * @author Alejandro Hernández Malillos
  */
-public class ScreensCommentServiceImpl
-	extends ScreensCommentServiceBaseImpl {
+public class ScreensCommentServiceImpl extends ScreensCommentServiceBaseImpl {
 
 	@Override
-	public JSONObject addComment(
-			String className, long classPK, String body)
-			throws PortalException {
-
-		JSONObject assetEntry =
-			screensAssetEntryService.getAssetEntry(className, classPK,
-				Locale.getDefault());
-
-		long groupId = assetEntry.getLong("groupId");
+	public JSONObject addComment(String className, long classPK, String body)
+		throws PortalException {
 
 		DiscussionPermission discussionPermission =
 			commentManager.getDiscussionPermission(getPermissionChecker());
 
-		long companyId = groupLocalService.getGroup(groupId).getCompanyId();
+		JSONObject assetEntry = screensAssetEntryService.getAssetEntry(
+			className, classPK, Locale.getDefault());
+
+		long groupId = assetEntry.getLong("groupId");
+
+		Group group = groupLocalService.getGroup(groupId);
+
+		long companyId = group.getCompanyId();
 
 		discussionPermission.checkAddPermission(
 			companyId, groupId, className, classPK);
@@ -59,33 +59,30 @@ public class ScreensCommentServiceImpl
 			getUserId(), groupId, className, classPK, getUser().getFullName(),
 			StringPool.BLANK, body, createServiceContextFunction());
 
-		JSONObject jsonObject = getJSONObject(
-			commentManager.fetchComment(commentId), discussionPermission);
+		Comment comment = commentManager.fetchComment(commentId);
 
-		return jsonObject;
+		return getJSONObject(comment, discussionPermission);
 	}
 
 	@Override
-	public JSONObject updateComment(
-		long commentId, String body)
+	public JSONObject updateComment(long commentId, String body)
 		throws PortalException {
-
-		Comment comment = commentManager.fetchComment(commentId);
-
-		String className = comment.getClassName();
-		long classPK = comment.getClassPK();
 
 		DiscussionPermission discussionPermission =
 			commentManager.getDiscussionPermission(getPermissionChecker());
 
 		discussionPermission.checkUpdatePermission(commentId);
 
+		Comment comment = commentManager.fetchComment(commentId);
+
 		commentManager.updateComment(
-			getUserId(), className, classPK, commentId, StringPool.BLANK, body,
+			getUserId(), comment.getClassName(), comment.getClassPK(),
+			commentId, StringPool.BLANK, body,
 			createServiceContextFunction(WorkflowConstants.ACTION_PUBLISH));
 
-		return getJSONObject(
-			commentManager.fetchComment(commentId), discussionPermission);
+		comment = commentManager.fetchComment(commentId);
+
+		return getJSONObject(comment, discussionPermission);
 	}
 
 	protected Function<String, ServiceContext> createServiceContextFunction() {
@@ -118,18 +115,22 @@ public class ScreensCommentServiceImpl
 
 	protected JSONObject getJSONObject(
 			Comment comment, DiscussionPermission discussionPermission)
-			throws PortalException {
+		throws PortalException {
+
 		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
 		jsonObject.put("body", comment.getBody());
 		jsonObject.put("commentId", comment.getCommentId());
 		jsonObject.put("createDate", comment.getCreateDate().getTime());
+		jsonObject.put(
+			"deletePermission",
+			discussionPermission.hasDeletePermission(comment.getCommentId()));
 		jsonObject.put("modifiedDate", comment.getModifiedDate().getTime());
+		jsonObject.put(
+			"updatePermission",
+			discussionPermission.hasUpdatePermission(comment.getCommentId()));
 		jsonObject.put("userId", comment.getUserId());
 		jsonObject.put("userName", comment.getUserName());
-		jsonObject.put("updatePermission",
-			discussionPermission.hasUpdatePermission(comment.getCommentId()));
-		jsonObject.put("deletePermission",
-			discussionPermission.hasDeletePermission(comment.getCommentId()));
 
 		return jsonObject;
 	}
@@ -139,4 +140,5 @@ public class ScreensCommentServiceImpl
 
 	@ServiceReference(type = GroupLocalService.class)
 	protected GroupLocalService groupLocalService;
+
 }
