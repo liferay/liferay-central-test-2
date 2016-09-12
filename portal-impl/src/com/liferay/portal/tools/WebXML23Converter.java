9acba2d7a4dcb149d73b2250898b20dc127a8c95
Hugo Huijser <hugo.huijser@liferay.com>
2016-09-12 21:13:27 +0800

    LPS-67352 Update source-formatter.properties

--- portal-impl/src/com/liferay/portal/tools/WebXML23Converter.java
+++ portal-impl/src/com/liferay/portal/tools/WebXML23Converter.java
@@ -79,9 +79,7 @@ public class WebXML23Converter {
 
 			int y = webXML23.indexOf(">", x);
 
-			webXML23 = webXML23.substring(
-				0,
-				x) + "<!DOCTYPE web-app PUBLIC \"-//Sun Microsystems, Inc.//DTD Web Application 2.3//EN\" \"http://java.sun.com/dtd/web-app_2_3.dtd\"><web-app>" + webXML23.substring(y + 1);
+			webXML23 = webXML23.substring(0, x) + "<!DOCTYPE web-app PUBLIC \"-//Sun Microsystems, Inc.//DTD Web Application 2.3//EN\" \"http://java.sun.com/dtd/web-app_2_3.dtd\"><web-app>" + webXML23.substring(y + 1);
 
 			webXML23 = StringUtil.removeSubstrings(
 				webXML23, "<jsp-config>", "</jsp-config>");
