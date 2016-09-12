e2a784117e092a1018c8183f4b75ac97173030cc
Chema Balsas <jose.balsas@liferay.com>
2016-09-12 13:49:56 +0200

    LPS-67914 Properly configure modules

--- modules/apps/frontend-theme-porygon/frontend-theme-porygon/gulpfile.js
+++ modules/apps/frontend-theme-porygon/frontend-theme-porygon/gulpfile.js
@@ -1,9 +1,36 @@
 'use strict';
 
+var ConfigGenerator = require('liferay-module-config-generator/lib/config-generator');
 var gulp = require('gulp');
 var liferayThemeTasks = require('liferay-theme-tasks');
 var metal = require('gulp-metal');
+var path = require('path');
 var runSequence = require('run-sequence').use(gulp);
+
+gulp.task(
+	'configModules',
+	function(done) {
+		var configGenerator = new ConfigGenerator(
+			{
+				args: [path.join(__dirname, 'build', 'js')],
+				config: '',
+				extension: '',
+				filePattern: '**/*.es.js',
+				format: ['/-/g', '_'],
+				ignorePath: true,
+				moduleConfig: path.join(__dirname, 'package.json'),
+				moduleRoot: path.join(__dirname, 'build'),
+				output: path.join(__dirname, 'build', 'META-INF', 'config.json')
+			}
+		);
+
+		configGenerator.process().then(
+			function() {
+				done();
+			}
+		);
+	}
+);
 
 metal.registerTasks(
 	{
@@ -24,6 +51,7 @@ liferayThemeTasks.registerTasks(
 				function(done) {
 					runSequence(
 						'metal:build:amd',
+						'configModules',
 						done
 					);
 				}
