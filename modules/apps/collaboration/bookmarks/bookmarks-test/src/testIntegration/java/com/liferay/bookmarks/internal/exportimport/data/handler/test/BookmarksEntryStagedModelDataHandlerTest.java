e6416f2d574358e5403fd32d8337769c94e1341c
akosthurzo <akos.thurzo@liferay.com>
2016-09-13 13:50:34 +0200

    LPS-68117 Move bookmarks tests

--- modules/apps/collaboration/bookmarks/bookmarks-test/src/testIntegration/java/com/liferay/bookmarks/internal/exportimport/data/handler/test/BookmarksEntryStagedModelDataHandlerTest.java
+++ modules/apps/collaboration/bookmarks/bookmarks-test/src/testIntegration/java/com/liferay/bookmarks/internal/exportimport/data/handler/test/BookmarksEntryStagedModelDataHandlerTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.bookmarks.lar.test;
+package com.liferay.bookmarks.internal.exportimport.data.handler.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.bookmarks.model.BookmarksEntry;
--- modules/apps/collaboration/bookmarks/bookmarks-test/src/testIntegration/java/com/liferay/bookmarks/internal/exportimport/test/BookmarksExportImportTest.java
+++ modules/apps/collaboration/bookmarks/bookmarks-test/src/testIntegration/java/com/liferay/bookmarks/internal/exportimport/test/BookmarksExportImportTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.bookmarks.lar.test;
+package com.liferay.bookmarks.internal.exportimport.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.bookmarks.constants.BookmarksPortletKeys;
