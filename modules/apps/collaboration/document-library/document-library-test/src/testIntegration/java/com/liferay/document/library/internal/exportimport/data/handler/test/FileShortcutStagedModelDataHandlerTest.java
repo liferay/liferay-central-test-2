bed9feaa6d2b19d8655f193c840cc9c3aa95d946
akosthurzo <akos.thurzo@liferay.com>
2016-09-13 14:38:35 +0200

    LPS-68120 Move DL tests

--- modules/apps/collaboration/document-library/document-library-test/src/testIntegration/java/com/liferay/document/library/internal/exportimport/data/handler/test/FileShortcutStagedModelDataHandlerTest.java
+++ modules/apps/collaboration/document-library/document-library-test/src/testIntegration/java/com/liferay/document/library/internal/exportimport/data/handler/test/FileShortcutStagedModelDataHandlerTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.document.library.lar.test;
+package com.liferay.document.library.internal.exportimport.data.handler.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.asset.kernel.model.AssetEntry;
