5d11fd69fa1fdb011d362e9e54978f3718ac0aaf
Tina Tian <tina.tian@liferay.com>
2016-09-14 09:15:05 -0700

    LPS-59425 Remove unused line checking since it would be only one line

--- portal-impl/src/com/liferay/portal/util/HttpImpl.java
+++ portal-impl/src/com/liferay/portal/util/HttpImpl.java
@@ -2062,17 +2062,17 @@ public class HttpImpl implements Http {
 	private static final ThreadLocal<Cookie[]> _cookies = new ThreadLocal<>();
 
 	private final Pattern _absoluteURLPattern = Pattern.compile(
-		"^[a-zA-Z0-9]+://");
+		"[a-zA-Z0-9]+://");
 	private final CloseableHttpClient _closeableHttpClient;
 	private final Pattern _nonProxyHostsPattern;
 	private final PoolingHttpClientConnectionManager
 		_poolingHttpClientConnectionManager;
 	private final Pattern _protocolRelativeURLPattern = Pattern.compile(
-		"^[\\s\\\\/]+");
+		"[\\s\\\\/]+");
 	private final List<String> _proxyAuthPrefs = new ArrayList<>();
 	private final CloseableHttpClient _proxyCloseableHttpClient;
 	private final Credentials _proxyCredentials;
 	private final Pattern _relativeURLPattern = Pattern.compile(
-		"^\\s*/[a-zA-Z0-9]+");
+		"\\s*/[a-zA-Z0-9]+");
 
 }
