5d086389131b89ebf8650aa17cb7ecf003e9f6eb
brunofarache <bruno.farache@liferay.com>
2016-09-15 17:22:53 -0300

    LPS-68189 Add companyId to PushNotificationsDevice

--- modules/apps/push-notifications/push-notifications-service/src/main/java/com/liferay/push/notifications/service/impl/PushNotificationsDeviceLocalServiceImpl.java
+++ modules/apps/push-notifications/push-notifications-service/src/main/java/com/liferay/push/notifications/service/impl/PushNotificationsDeviceLocalServiceImpl.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.messaging.MessageBusUtil;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.push.notifications.constants.PushNotificationsDestinationNames;
 import com.liferay.push.notifications.model.PushNotificationsDevice;
@@ -47,7 +48,10 @@ public class PushNotificationsDeviceLocalServiceImpl
 
 	@Override
 	public PushNotificationsDevice addPushNotificationsDevice(
-		long userId, String platform, String token) {
+			long userId, String platform, String token)
+		throws PortalException {
+
+		User user = userPersistence.findByPrimaryKey(userId);
 
 		long pushNotificationsDeviceId = counterLocalService.increment();
 
@@ -55,7 +59,8 @@ public class PushNotificationsDeviceLocalServiceImpl
 			pushNotificationsDevicePersistence.create(
 				pushNotificationsDeviceId);
 
-		pushNotificationsDevice.setUserId(userId);
+		pushNotificationsDevice.setCompanyId(user.getCompanyId());
+		pushNotificationsDevice.setUserId(user.getUserId());
 		pushNotificationsDevice.setCreateDate(new Date());
 		pushNotificationsDevice.setPlatform(platform);
 		pushNotificationsDevice.setToken(token);
