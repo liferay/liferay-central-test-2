c2da58d946d9f1a2cd190289f62b8ce656eab614
akosthurzo <akos.thurzo@liferay.com>
2016-09-15 10:58:47 +0200

    LPS-68158 Move LayoutSetPrototype tests

--- modules/apps/web-experience/layout/layout-set-prototype-test/src/testIntegration/java/com/liferay/layout/set/prototype/exportimport/test/LayoutSetPrototypeExportImportTest.java
+++ modules/apps/web-experience/layout/layout-set-prototype-test/src/testIntegration/java/com/liferay/layout/set/prototype/exportimport/test/LayoutSetPrototypeExportImportTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.layout.set.prototype.lar.test;
+package com.liferay.layout.set.prototype.exportimport.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.layout.set.prototype.constants.LayoutSetPrototypePortletKeys;
