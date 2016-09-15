665f5cc31a1b39c9c84ffb1c722cd7093b79de43
Miguel Ángel Pastor Olivar <miguel.pastor@liferay.com>
2016-09-15 15:48:52 +0200

    LPS-67793 Easier to read

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -5423,17 +5423,7 @@ public class PortalImpl implements Portal {
 
 			boolean unWrap = false;
 
-			for (String requestUnwrapPackage :
-					PropsValues.REQUEST_UNWRAP_PACKAGES) {
-
-				if (currentRequestClassName.startsWith(requestUnwrapPackage)) {
-					unWrap = true;
-
-					break;
-				}
-			}
-
-			if (!unWrap) {
+			if (!_needToUnWrapRequest(currentRequestClassName)) {
 				break;
 			}
 
@@ -8170,6 +8160,18 @@ public class PortalImpl implements Portal {
 		themeDisplay.setI18nLanguageId(i18nLanguageId);
 		themeDisplay.setI18nPath(i18nPath);
 		themeDisplay.setLocale(locale);
+	}
+
+	private boolean _needToUnWrapRequest(String currentRequestClassName) {
+		for (String requestUnwrapPackage :
+				PropsValues.REQUEST_UNWRAP_PACKAGES) {
+
+			if (currentRequestClassName.startsWith(requestUnwrapPackage)) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	private static final Log _logWebServerServlet = LogFactoryUtil.getLog(
