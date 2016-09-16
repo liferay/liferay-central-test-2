d170405170fcb489356ccfa6cff34e8cf4885135
shuyangzhou <shuyang.zhou@liferay.com>
2016-09-16 16:18:08 -0700

    LPS-68262 Get rid of the useless singleton instance

--- portal-kernel/src/com/liferay/portal/kernel/util/ServerDetector.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/ServerDetector.java
@@ -42,36 +42,29 @@ public class ServerDetector {
 
 	public static final String WILDFLY_ID = "wildfly";
 
+	/**
+	 * @deprecated As of 7.0.0, with no direct replacement
+	 */
+	@Deprecated
 	public static ServerDetector getInstance() {
-		if (_instance == null) {
-			_instance = new ServerDetector();
-
-			_instance._init();
-		}
-
-		return _instance;
+		return new ServerDetector();
 	}
 
 	public static String getServerId() {
-		return StringUtil.toLowerCase(getInstance()._serverType.toString());
+		return StringUtil.toLowerCase(_serverType.toString());
 	}
 
 	public static void init(String serverId) {
-		ServerDetector serverDetector = new ServerDetector();
-
 		try {
-			serverDetector._serverType = ServerType.valueOf(
-				StringUtil.toUpperCase(serverId));
+			_serverType = ServerType.valueOf(StringUtil.toUpperCase(serverId));
 		}
 		catch (IllegalArgumentException iae) {
-			serverDetector._init();
+			_init();
 		}
-
-		_instance = serverDetector;
 	}
 
 	public static boolean isGlassfish() {
-		if (getInstance()._serverType == ServerType.GLASSFISH) {
+		if (_serverType == ServerType.GLASSFISH) {
 			return true;
 		}
 
@@ -79,7 +72,7 @@ public class ServerDetector {
 	}
 
 	public static boolean isJBoss() {
-		if (getInstance()._serverType == ServerType.JBOSS) {
+		if (_serverType == ServerType.JBOSS) {
 			return true;
 		}
 
@@ -87,7 +80,7 @@ public class ServerDetector {
 	}
 
 	public static boolean isJetty() {
-		if (getInstance()._serverType == ServerType.JETTY) {
+		if (_serverType == ServerType.JETTY) {
 			return true;
 		}
 
@@ -95,7 +88,7 @@ public class ServerDetector {
 	}
 
 	public static boolean isJOnAS() {
-		if (getInstance()._serverType == ServerType.JONAS) {
+		if (_serverType == ServerType.JONAS) {
 			return true;
 		}
 
@@ -103,7 +96,7 @@ public class ServerDetector {
 	}
 
 	public static boolean isOC4J() {
-		if (getInstance()._serverType == ServerType.OC4J) {
+		if (_serverType == ServerType.OC4J) {
 			return true;
 		}
 
@@ -111,7 +104,7 @@ public class ServerDetector {
 	}
 
 	public static boolean isResin() {
-		if (getInstance()._serverType == ServerType.RESIN) {
+		if (_serverType == ServerType.RESIN) {
 			return true;
 		}
 
@@ -131,7 +124,7 @@ public class ServerDetector {
 	}
 
 	public static boolean isTomcat() {
-		if (getInstance()._serverType == ServerType.TOMCAT) {
+		if (_serverType == ServerType.TOMCAT) {
 			return true;
 		}
 
@@ -139,7 +132,7 @@ public class ServerDetector {
 	}
 
 	public static boolean isWebLogic() {
-		if (getInstance()._serverType == ServerType.WEBLOGIC) {
+		if (_serverType == ServerType.WEBLOGIC) {
 			return true;
 		}
 
@@ -147,7 +140,7 @@ public class ServerDetector {
 	}
 
 	public static boolean isWebSphere() {
-		if (getInstance()._serverType == ServerType.WEBSPHERE) {
+		if (_serverType == ServerType.WEBSPHERE) {
 			return true;
 		}
 
@@ -155,7 +148,7 @@ public class ServerDetector {
 	}
 
 	public static boolean isWildfly() {
-		if (getInstance()._serverType == ServerType.WILDFLY) {
+		if (_serverType == ServerType.WILDFLY) {
 			return true;
 		}
 
@@ -169,7 +162,7 @@ public class ServerDetector {
 	public static void setSupportsHotDeploy(boolean supportsHotDeploy) {
 	}
 
-	private boolean _detect(String className) {
+	private static boolean _detect(String className) {
 		try {
 			ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
 
@@ -178,9 +171,7 @@ public class ServerDetector {
 			return true;
 		}
 		catch (ClassNotFoundException cnfe) {
-			Class<?> clazz = getClass();
-
-			if (clazz.getResource(className) != null) {
+			if (ServerDetector.class.getResource(className) != null) {
 				return true;
 			}
 			else {
@@ -189,7 +180,7 @@ public class ServerDetector {
 		}
 	}
 
-	private boolean _hasSystemProperty(String key) {
+	private static boolean _hasSystemProperty(String key) {
 		String value = System.getProperty(key);
 
 		if (value != null) {
@@ -200,37 +191,37 @@ public class ServerDetector {
 		}
 	}
 
-	private void _init() {
-		if (_isGlassfish()) {
+	private static void _init() {
+		if (_hasSystemProperty("com.sun.aas.instanceRoot")) {
 			_serverType = ServerType.GLASSFISH;
 		}
-		else if (_isJBoss()) {
+		else if (_hasSystemProperty("jboss.home.dir")) {
 			_serverType = ServerType.JBOSS;
 		}
-		else if (_isJOnAS()) {
+		else if (_hasSystemProperty("jonas.base")) {
 			_serverType = ServerType.JONAS;
 		}
-		else if (_isOC4J()) {
+		else if (_detect("oracle.oc4j.util.ClassUtils")) {
 			_serverType = ServerType.OC4J;
 		}
-		else if (_isResin()) {
+		else if (_hasSystemProperty("resin.home")) {
 			_serverType = ServerType.RESIN;
 		}
-		else if (_isWebLogic()) {
+		else if (_detect("/weblogic/Server.class")) {
 			_serverType = ServerType.WEBLOGIC;
 		}
-		else if (_isWebSphere()) {
+		else if (_detect("/com/ibm/websphere/product/VersionInfo.class")) {
 			_serverType = ServerType.WEBSPHERE;
 		}
-		else if (_isWildfly()) {
+		else if (_hasSystemProperty("jboss.home.dir")) {
 			_serverType = ServerType.WILDFLY;
 		}
 
 		if (_serverType == null) {
-			if (_isJetty()) {
+			if (_hasSystemProperty("jetty.home")) {
 				_serverType = ServerType.JETTY;
 			}
-			else if (_isTomcat()) {
+			else if (_hasSystemProperty("catalina.base")) {
 				_serverType = ServerType.TOMCAT;
 			}
 		}
@@ -253,53 +244,15 @@ public class ServerDetector {
 		}*/
 	}
 
-	private boolean _isGlassfish() {
-		return _hasSystemProperty("com.sun.aas.instanceRoot");
-	}
-
-	private boolean _isJBoss() {
-		return _hasSystemProperty("jboss.home.dir");
-	}
-
-	private boolean _isJetty() {
-		return _hasSystemProperty("jetty.home");
-	}
-
-	private boolean _isJOnAS() {
-		return _hasSystemProperty("jonas.base");
-	}
-
-	private boolean _isOC4J() {
-		return _detect("oracle.oc4j.util.ClassUtils");
-	}
-
-	private boolean _isResin() {
-		return _hasSystemProperty("resin.home");
-	}
-
-	private boolean _isTomcat() {
-		return _hasSystemProperty("catalina.base");
-	}
-
-	private boolean _isWebLogic() {
-		return _detect("/weblogic/Server.class");
-	}
-
-	private boolean _isWebSphere() {
-		return _detect("/com/ibm/websphere/product/VersionInfo.class");
-	}
-
-	private boolean _isWildfly() {
-		return _hasSystemProperty("jboss.home.dir");
-	}
-
 	private static final boolean _SUPPORTS_COMET = false;
 
 	private static final Log _log = LogFactoryUtil.getLog(ServerDetector.class);
 
-	private static ServerDetector _instance;
+	private static ServerType _serverType;
 
-	private ServerType _serverType;
+	static {
+		_init();
+	}
 
 	private enum ServerType {
 
