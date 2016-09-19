cda52b12277ef12a08801903f30f4774ac979660
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-09-19 11:57:48 -0700

    LPS-67640 Create a more-general purpose task and a Liferay-specific one

--- modules/sdk/ant-gradle-exec/src/main/java/com/liferay/ant/gradle/exec/GradleExecTask.java
+++ modules/sdk/ant-gradle-exec/src/main/java/com/liferay/ant/gradle/exec/GradleExecTask.java
@@ -16,193 +16,175 @@ package com.liferay.ant.gradle.exec;
 
 import java.io.File;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
 import org.apache.tools.ant.taskdefs.ExecTask;
+import org.apache.tools.ant.taskdefs.condition.Os;
+import org.apache.tools.ant.types.Commandline.Argument;
+import org.apache.tools.ant.types.Environment;
+import org.apache.tools.ant.util.FileUtils;
 
 /**
  * @author Chas Austin
+ * @author Andrea Di Giorgi
  */
 public class GradleExecTask extends ExecTask {
 
 	@Override
 	public void execute() throws BuildException {
-		_gradleExecTask.setDir(_dir);
-		_gradleExecTask.setExecutable(_gradleExecutable);
-		_gradleExecTask.setFailonerror(true);
-		_gradleExecTask.setOwningTarget(getOwningTarget());
-		_gradleExecTask.setProject(getProject());
-		_gradleExecTask.setTaskName("gradle-exec");
+		_addArguments();
+		_addEnvironment();
 
-		if (_task != null) {
-			_gradleExecTask.createArg().setValue(_task);
-		}
-		else {
-			for (String task : _tasks) {
-				_gradleExecTask.createArg().setValue(task);
-			}
-		}
-
-		_addGradleExecArg(_getAppServerParentDirArg());
-		_addGradleExecArg(_getBuildFileArg());
-		_addGradleExecArg(_getForcedCacheEnabledArg());
-		_addGradleExecArg(_getLiferayHomeArg());
-		_addGradleExecArg(_getParallelArg());
-		_addGradleExecArg(_getPortalBuildArg());
-		_addGradleExecArg(_getProjectCacheDirArg());
-		_addGradleExecArg(_getQuietArg());
-		_addGradleExecArg("--no-daemon");
-		_addGradleExecArg("--stacktrace");
-
-		checkConfiguration();
-
-		_gradleExecTask.execute();
-	}
-
-	public void init() {
-		_dir = new File(getProject().getBaseDir(), "modules");
-
-		_gradleExecutable = getProject().getProperty("gradle.executable");
-	}
-
-	public void setBuildFile(String buildFile) {
-		_buildFile = new File(buildFile);
+		super.execute();
 	}
 
 	@Override
-	public void setDir(File dir) {
-		_dir = dir;
+	public void init() throws BuildException {
+		super.init();
+
+		try {
+			setExecutable(_getExecutable());
+		}
+		catch (Exception e) {
+			throw new BuildException(e);
+		}
+
+		setFailonerror(true);
 	}
 
-	@Override
-	public void setExecutable(String gradleExecutable) {
-		_gradleExecutable = gradleExecutable;
+	public void setBuildFile(File buildFile) {
+		_buildFile = buildFile;
 	}
 
-	public void setForcedCacheEnabled(boolean forcedCacheEnabled) {
-		_forcedCacheEnabled = forcedCacheEnabled;
+	public void setDaemonDisabled(boolean daemonDisabled) {
+		_daemonDisabled = daemonDisabled;
+	}
+
+	public void setInheritAntOpts(boolean inheritAntOpts) {
+		_inheritAntOpts = inheritAntOpts;
 	}
 
 	public void setParallel(boolean parallel) {
 		_parallel = parallel;
 	}
 
-	public void setPortalBuild(boolean portalBuild) {
-		_portalBuild = portalBuild;
+	public void setProjectCacheDir(File projectCacheDir) {
+		_projectCacheDir = projectCacheDir;
 	}
 
 	public void setQuiet(boolean quiet) {
 		_quiet = quiet;
 	}
 
-	public void setTask(String task) {
-		if (_tasks != null) {
-			throw new BuildException(
-				"The \"task\" and \"tasks\" attributes cannot both be" +
-					"specified");
-		}
-		else {
-			_task = task;
-		}
+	public void setStacktrace(boolean stacktrace) {
+		_stacktrace = stacktrace;
 	}
 
 	public void setTasks(String tasks) {
-		if (_task != null) {
-			throw new BuildException(
-				"The \"task\" and \"tasks\" attributes cannot both be" +
-					"specified");
-		}
-		else {
-			_tasks = Arrays.asList(tasks.split(","));
-		}
-	}
+		_tasks.clear();
 
-	@Override
-	protected void checkConfiguration() throws BuildException {
-		File gradleExecutableFile = new File(_gradleExecutable);
+		StringTokenizer stringTokenizer = new StringTokenizer(tasks, ", ");
 
-		if (!gradleExecutableFile.exists()) {
-			throw new BuildException(
-				"The specified Gradle executable does not exist: " +
-					gradleExecutableFile.getAbsolutePath());
-		}
-		else if (!_dir.exists()) {
-			throw new BuildException(
-				"The specified directory does not exist: " +
-					_dir.getAbsolutePath());
+		while (stringTokenizer.hasMoreTokens()) {
+			_tasks.add(stringTokenizer.nextToken());
 		}
 	}
 
-	private void _addGradleExecArg(String argValue) {
-		if (argValue != null) {
-			_gradleExecTask.createArg().setValue(argValue);
-		}
+	protected Argument addArgument(String value) {
+		Argument argument = createArg();
+
+		argument.setValue(value);
+
+		return argument;
 	}
 
-	private String _getAppServerParentDirArg() {
-		File appServerParentDir = new File(
-			getProject().getProperty("app.server.parent.dir"));
-
-		return "-Dapp.server.parent.dir=" + appServerParentDir.toString();
-	}
-
-	private String _getBuildFileArg() {
+	private void _addArguments() {
 		if (_buildFile != null) {
-			return "--build-file=" + _buildFile.toString();
+			addArgument("--build-file");
+			addArgument(_buildFile.getAbsolutePath());
 		}
 
-		return null;
-	}
+		if (_daemonDisabled) {
+			addArgument("--no-daemon");
+		}
 
-	private String _getForcedCacheEnabledArg() {
-		return "-Dforced.cache.enabled=" + _forcedCacheEnabled;
-	}
-
-	private String _getLiferayHomeArg() {
-		File liferayHome = new File(getProject().getProperty("liferay.home"));
-
-		return "-Dliferay.home=" + liferayHome.toString();
-	}
-
-	private String _getParallelArg() {
 		if (_parallel) {
-			return "--parallel";
+			addArgument("--parallel");
 		}
 
-		return null;
-	}
+		if (_projectCacheDir != null) {
+			addArgument("project-cache-dir");
+			addArgument(_projectCacheDir.getAbsolutePath());
+		}
 
-	private String _getPortalBuildArg() {
-		return "-Dportal.build=" + _portalBuild;
-	}
-
-	private String _getProjectCacheDirArg() {
-		File projectDir = new File(getProject().getProperty("project.dir"));
-
-		File projectCacheDir = new File(projectDir, ".gradle");
-
-		return "--project-cache-dir=" + projectCacheDir.toString();
-	}
-
-	private String _getQuietArg() {
 		if (_quiet) {
-			return "--quiet";
+			addArgument("--quiet");
 		}
 
-		return null;
+		if (_stacktrace) {
+			addArgument("--stacktrace");
+		}
+
+		for (String task : _tasks) {
+			addArgument(task);
+		}
+	}
+
+	private void _addEnvironment() {
+		String antOpts = System.getenv("ANT_OPTS");
+
+		if (_inheritAntOpts && (antOpts != null) && !antOpts.isEmpty()) {
+			Environment.Variable variable = new Environment.Variable();
+
+			variable.setKey("GRADLE_OPTS");
+			variable.setValue(antOpts);
+		}
+	}
+
+	private String _getExecutable() throws Exception {
+		Project project = getProject();
+
+		String executable = project.getProperty("gradle.executable");
+
+		if ((executable != null) && !executable.isEmpty()) {
+			return executable;
+		}
+
+		String fileName = "gradlew";
+
+		if (Os.isFamily(Os.FAMILY_WINDOWS)) {
+			fileName += ".bat";
+		}
+
+		File dir = project.getBaseDir();
+
+		while (dir != null) {
+			File file = new File(dir, fileName);
+
+			if (file.isFile()) {
+				executable = FileUtils.getRelativePath(
+					project.getBaseDir(), file);
+
+				break;
+			}
+
+			dir = dir.getParentFile();
+		}
+
+		return executable;
 	}
 
 	private File _buildFile;
-	private File _dir;
-	private boolean _forcedCacheEnabled = true;
-	private final ExecTask _gradleExecTask = new ExecTask();
-	private String _gradleExecutable;
+	private boolean _daemonDisabled = true;
+	private boolean _inheritAntOpts = true;
 	private boolean _parallel = true;
-	private boolean _portalBuild = true;
+	private File _projectCacheDir;
 	private boolean _quiet;
-	private String _task;
-	private List<String> _tasks;
+	private boolean _stacktrace = true;
+	private final List<String> _tasks = new ArrayList<String>();
 
 }
---
+++ modules/sdk/ant-gradle-exec/src/main/java/com/liferay/ant/gradle/exec/LiferayGradleExecTask.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ant.gradle.exec;
+
+import java.io.File;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+
+/**
+ * @author Andrea Di Giorgi
+ */
+public class LiferayGradleExecTask extends GradleExecTask {
+
+	@Override
+	public void execute() throws BuildException {
+		_addArguments();
+
+		super.execute();
+	}
+
+	@Override
+	public void init() throws BuildException {
+		super.init();
+
+		Project project = getProject();
+
+		setDir(new File(project.getBaseDir(), "modules"));
+	}
+
+	public void setForcedCache(boolean forcedCacheEnabled) {
+		_forcedCacheEnabled = forcedCacheEnabled;
+	}
+
+	public void setPortalBuild(boolean portalBuild) {
+		_portalBuild = portalBuild;
+	}
+
+	private void _addArguments() {
+		Project project = getProject();
+
+		String appServerParentDir = project.getProperty(
+			"app.server.parent.dir");
+
+		if ((appServerParentDir != null) && !appServerParentDir.isEmpty()) {
+			addArgument("-Dapp.server.parent.dir=" + appServerParentDir);
+		}
+
+		addArgument("-Dforced.cache.enabled=" + _forcedCacheEnabled);
+
+		String liferayHome = project.getProperty("liferay.home");
+
+		if ((liferayHome != null) && !liferayHome.isEmpty()) {
+			addArgument("-Dliferay.home=" + liferayHome);
+		}
+
+		addArgument("-Dportal.build=" + _portalBuild);
+	}
+
+	private boolean _forcedCacheEnabled = true;
+	private boolean _portalBuild;
+
+}
