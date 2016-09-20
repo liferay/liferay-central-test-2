81253f37a4f596833a90ba53a45d630cb102801b
Mladen Cikara <mladen.cikara@liferay.com>
2016-09-20 14:43:58 +0200

    CLDSVCS-2283 - petra-json-web-service-client - adding suppoprt for jcifs library

---
+++ modules/apps/foundation/petra/petra-json-web-service-client/src/main/java/com/liferay/petra/json/web/service/client/jcifs/JCIFSNTLMSchemeFactory.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.petra.json.web.service.client.jcifs;
+
+import org.apache.http.auth.AuthScheme;
+import org.apache.http.auth.AuthSchemeProvider;
+import org.apache.http.impl.auth.NTLMScheme;
+import org.apache.http.protocol.HttpContext;
+
+/**
+ * @author Mladen Cikara
+ */
+public class JCIFSNTLMSchemeFactory implements AuthSchemeProvider {
+
+	public JCIFSNTLMSchemeFactory(String domain, String workstation) {
+		this._domain = domain;
+		this._workstation = workstation;
+	}
+
+	public AuthScheme create(final HttpContext context) {
+		return new NTLMScheme(new JCIFSEngine(_domain, _workstation));
+	}
+
+	private String _domain;
+	private String _workstation;
+
+}
