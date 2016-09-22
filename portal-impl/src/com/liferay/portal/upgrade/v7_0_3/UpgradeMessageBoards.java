1a5c17fa587ec6d3cca1a86afa3b1786cf28bc68
Miguel Ángel Pastor Olivar <miguel.pastor@liferay.com>
2016-09-22 09:05:09 +0200

    LPS-68296 Some of the databases we support are case-sensitive by default (DB2, PostgreSQL, ...). Be careful with your handwritten SQL :)

--- portal-impl/src/com/liferay/portal/upgrade/v7_0_3/UpgradeMessageBoards.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_3/UpgradeMessageBoards.java
@@ -42,7 +42,7 @@ public class UpgradeMessageBoards extends UpgradeProcess {
 			sb.append("select messageId from MBMessage where threadId in (");
 			sb.append("select threadId from MBThread where categoryId = ");
 			sb.append(MBCategoryConstants.DISCUSSION_CATEGORY_ID);
-			sb.append(" and messagecount = 1)) and classNameId = ");
+			sb.append(" and messageCount = 1)) and classNameId = ");
 			sb.append(classNameId);
 
 			runSQL(sb.toString());
@@ -51,18 +51,18 @@ public class UpgradeMessageBoards extends UpgradeProcess {
 				"delete from MBMessage where threadId in (" +
 					"select threadId from MBThread where categoryId = " +
 						MBCategoryConstants.DISCUSSION_CATEGORY_ID +
-							" and messagecount = 1)");
+							" and messageCount = 1)");
 
 			runSQL(
 				"delete from MBDiscussion where threadId in (" +
 					"select threadId from MBThread where categoryId = " +
 						MBCategoryConstants.DISCUSSION_CATEGORY_ID +
-							" and messagecount = 1)");
+							" and messageCount = 1)");
 
 			runSQL(
 				"delete from MBThread where categoryId = " +
 					MBCategoryConstants.DISCUSSION_CATEGORY_ID +
-						" and messagecount = 1");
+						" and messageCount = 1");
 		}
 		catch (Exception e) {
 			throw new UpgradeException(e);
