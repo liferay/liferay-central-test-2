mdelapenya/liferay-portal

LPS-68189 Upgrade Process

modules/apps/push-notifications/push-notifications-service/src/main/java/com/liferay/push/notifications/internal/upgrade/v1_0_0/UpgradeCompanyId.java
@@ -1,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.push.notifications.internal.upgrade.v1_0_0;
+
+import com.liferay.portal.kernel.upgrade.BaseUpgradeCompanyId;
+
+/**
+ * @author Bruno Farache
+ */
+public class UpgradeCompanyId extends BaseUpgradeCompanyId {
+
+	@Override
+	protected TableUpdater[] getTableUpdaters() {
+		return new TableUpdater[] {
+			new TableUpdater("PushNotificationsDevice", "User_", "userId")
+		};
+	}
+
+}

liferay/liferay-central-test-2

modules/apps/push-notifications/push-notifications-service/src/main/java/com/liferay/push/notifications/internal/upgrade/v1_0_6/UpgradeCompanyId.java
@@ -1,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.push.notifications.internal.upgrade.v1_0_6;
+
+import com.liferay.portal.kernel.upgrade.BaseUpgradeCompanyId;
+
+/**
+ * @author Bruno Farache
+ */
+public class UpgradeCompanyId extends BaseUpgradeCompanyId {
+
+	@Override
+	protected TableUpdater[] getTableUpdaters() {
+		return new TableUpdater[] {
+			new TableUpdater("PushNotificationsDevice", "User_", "userId")
+		};
+	}
+
+}