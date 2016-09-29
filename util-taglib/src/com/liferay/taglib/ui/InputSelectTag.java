96f3a2108db53e3ac631e4af20c894c81a229855
Jonathan Mak <jonathan.mak@liferay.com>
2016-09-29 11:58:03 -0700

    LPS-68438 - Deprecate <liferay-ui:input-select /> taglib

--- util-taglib/src/com/liferay/taglib/ui/InputSelectTag.java
+++ util-taglib/src/com/liferay/taglib/ui/InputSelectTag.java
@@ -20,7 +20,9 @@ import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Brian Wing Shun Chan
+ * @deprecated As of 7.0.0, with no direct replacement
  */
+@Deprecated
 public class InputSelectTag extends IncludeTag {
 
 	public void setCssClass(String cssClass) {
