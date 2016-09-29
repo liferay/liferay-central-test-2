36ac447224710337ef7065c528db840ce04243df
Jonathan Mak <jonathan.mak@liferay.com>
2016-09-29 09:53:54 -0700

    LPS-68423 - Deprecate <liferay-ui:quick-access /> taglib

--- util-taglib/src/com/liferay/taglib/ui/QuickAccessTag.java
+++ util-taglib/src/com/liferay/taglib/ui/QuickAccessTag.java
@@ -24,7 +24,9 @@ import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Eudaldo Alonso
+ * @deprecated As of 7.0.0, with no direct replacement
  */
+@Deprecated
 public class QuickAccessTag extends IncludeTag {
 
 	public void setContentId(String contentId) {
