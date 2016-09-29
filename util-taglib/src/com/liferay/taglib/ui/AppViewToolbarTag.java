5b1b989e0dbb8e45252bd63e45441e5dcaec1d6a
Jonathan Mak <jonathan.mak@liferay.com>
2016-09-29 16:15:45 -0700

    LPS-68445 - Deprecate <liferay-ui:app-view-toolbar /> taglib

--- util-taglib/src/com/liferay/taglib/ui/AppViewToolbarTag.java
+++ util-taglib/src/com/liferay/taglib/ui/AppViewToolbarTag.java
@@ -23,7 +23,9 @@ import javax.servlet.jsp.tagext.TagSupport;
 
 /**
  * @author Sergio González
+ * @deprecated As of 7.0.0, with no direct replacement
  */
+@Deprecated
 public class AppViewToolbarTag extends TagSupport {
 
 	@Override
