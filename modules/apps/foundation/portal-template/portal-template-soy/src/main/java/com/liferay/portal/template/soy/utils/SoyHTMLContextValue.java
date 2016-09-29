15c30749300835d48a92ba8a2a13fcd4b7944b6c
Bruno Basto <bruno.basto@liferay.com>
2016-09-29 09:29:54 -0300

    LPS-68415 Adds soy context wrappers to properly handle content types

---
+++ modules/apps/foundation/portal-template/portal-template-soy/src/main/java/com/liferay/portal/template/soy/utils/SoyHTMLContextValue.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.template.soy.utils;
+
+/**
+ * @author Bruno Basto
+ */
+public class SoyHTMLContextValue implements CharSequence {
+
+	public SoyHTMLContextValue(String value) {
+		this._value = value;
+	}
+
+	@Override
+	public char charAt(int index) {
+		return _value.charAt(index);
+	}
+
+	@Override
+	public int length() {
+		return _value.length();
+	}
+
+	@Override
+	public CharSequence subSequence(int start, int end) {
+		return _value.subSequence(start, end);
+	}
+
+	@Override
+	public String toString() {
+		return _value;
+	}
+
+	private final String _value;
+
+}
