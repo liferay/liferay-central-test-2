df4ef1db7d22eb7ada6887932fb728d5aed27f5f
Jonathan Mak <jonathan.mak@liferay.com>
2016-09-29 10:15:52 -0700

    LPS-68426 - Deprecate <liferay-ui:png-image /> taglib

--- util-taglib/src/com/liferay/taglib/ui/PngImageTag.java
+++ util-taglib/src/com/liferay/taglib/ui/PngImageTag.java
@@ -20,7 +20,9 @@ import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Brian Wing Shun Chan
+ * @deprecated As of 7.0.0, with no direct replacement
  */
+@Deprecated
 public class PngImageTag extends IncludeTag {
 
 	public void setHeight(String height) {
