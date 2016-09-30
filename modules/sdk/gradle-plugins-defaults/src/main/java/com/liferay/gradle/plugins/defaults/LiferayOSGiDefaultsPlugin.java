6e885002b8be3e7ed86ee460ef7fe36023e99747
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-09-30 16:55:37 -0700

    LPS-68448 Check if the version in npm-shrinkwrap.json matches

--- modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/LiferayOSGiDefaultsPlugin.java
+++ modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/LiferayOSGiDefaultsPlugin.java
@@ -1146,6 +1146,7 @@ public class LiferayOSGiDefaultsPlugin implements Plugin<Project> {
 	}
 
 	protected void checkVersion(Project project) {
+		_checkJsonVersion(project, "npm-shrinkwrap.json");
 		_checkJsonVersion(project, "package.json");
 	}
 
