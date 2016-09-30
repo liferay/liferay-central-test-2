84a8b64f4f625f3c5b4bb300018156c158832933
Inacio Nery <inacio.nery@liferay.com>
2016-09-30 16:47:11 -0300

    LPS-68316 Fix property value

--- modules/apps/web-form/web-form-web/src/main/java/com/liferay/web/form/web/internal/portlet/WebFormPortlet.java
+++ modules/apps/web-form/web-form-web/src/main/java/com/liferay/web/form/web/internal/portlet/WebFormPortlet.java
@@ -103,7 +103,7 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.portlet-info.short-title=Web Form",
 		"javax.portlet.portlet-info.title=Web Form",
 		"javax.portlet.preferences=classpath:/META-INF/portlet-preferences/default-portlet-preferences.xml",
-		"javax.portlet.resource-bundle=contnt.Language",
+		"javax.portlet.resource-bundle=content.Language",
 		"javax.portlet.security-role-ref=administrator,guest,power-user,user",
 		"javax.portlet.supports.mime-type=text/html"
 	},
