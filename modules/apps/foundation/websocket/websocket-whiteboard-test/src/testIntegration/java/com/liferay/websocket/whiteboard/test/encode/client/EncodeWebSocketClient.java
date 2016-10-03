e5818c412dc33845cc4dddc29f6e37bdbfe2fc76
Cristina González Castellano <cristina.gonzalez@liferay.com>
2016-10-03 11:29:36 +0200

    LPS-67167 New client for the Example class

---
+++ modules/apps/foundation/websocket/websocket-whiteboard-test/src/testIntegration/java/com/liferay/websocket/whiteboard/test/encode/client/EncodeWebSocketClient.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.websocket.whiteboard.test.encode.client;
+
+import com.liferay.websocket.whiteboard.test.encode.data.Example;
+import com.liferay.websocket.whiteboard.test.encode.data.ExampleDecoder;
+import com.liferay.websocket.whiteboard.test.encode.data.ExampleEncoder;
+
+import java.io.IOException;
+
+import java.util.concurrent.BlockingQueue;
+
+import javax.websocket.ClientEndpoint;
+import javax.websocket.EncodeException;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.RemoteEndpoint.Basic;
+import javax.websocket.Session;
+
+/**
+ * @author Cristina González
+ */
+@ClientEndpoint(
+	decoders = ExampleDecoder.class, encoders = ExampleEncoder.class
+)
+public class EncodeWebSocketClient {
+
+	public EncodeWebSocketClient(BlockingQueue<Example> blockingQueue) {
+		_blockingQueue = blockingQueue;
+	}
+
+	@OnMessage
+	public void onMessage(Example text, Session session)
+		throws InterruptedException {
+
+		_blockingQueue.put(text);
+	}
+
+	@OnOpen
+	public void onOpen(Session session) {
+		_session = session;
+	}
+
+	public void sendMessage(Example data) throws EncodeException, IOException {
+		Basic basic = _session.getBasicRemote();
+
+		basic.sendObject(data);
+	}
+
+	private final BlockingQueue<Example> _blockingQueue;
+	private Session _session;
+
+}
