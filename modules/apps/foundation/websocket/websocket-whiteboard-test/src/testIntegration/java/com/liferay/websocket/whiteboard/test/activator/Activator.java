267872769671ae899ae034891f32876a691d58d5
Cristina González Castellano <cristina.gonzalez@liferay.com>
2016-10-03 11:32:12 +0200

    LPS-67167 Register the endpoint as a OSGi service in the activator of the bundle

---
+++ modules/apps/foundation/websocket/websocket-whiteboard-test/src/testIntegration/java/com/liferay/websocket/whiteboard/test/activator/Activator.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.websocket.whiteboard.test.activator;
+
+import com.liferay.websocket.whiteboard.test.encode.data.ExampleDecoder;
+import com.liferay.websocket.whiteboard.test.encode.data.ExampleEncoder;
+import com.liferay.websocket.whiteboard.test.encode.endpoint.EncodeWebSocketEndpoint;
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
+
+import javax.websocket.Decoder;
+import javax.websocket.Encoder;
+import javax.websocket.Endpoint;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ * @author Cristina González
+ */
+public class Activator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+		List<Class<? extends Decoder>> decoders = new ArrayList<>();
+
+		decoders.add(ExampleDecoder.class);
+
+		List<Class<? extends Encoder>> encoders = new ArrayList<>();
+
+		encoders.add(ExampleEncoder.class);
+
+		Dictionary<String, Object> properties = new Hashtable<>();
+
+		properties.put(
+			"org.osgi.http.websocket.endpoint.path", "/o/websocket/decoder");
+
+		properties.put("org.osgi.http.websocket.endpoint.encoders", encoders);
+
+		properties.put("org.osgi.http.websocket.endpoint.decoders", decoders);
+
+		_endpointServiceRegistration = bundleContext.registerService(
+			Endpoint.class, new EncodeWebSocketEndpoint(), properties);
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+		_endpointServiceRegistration.unregister();
+	}
+
+	private ServiceRegistration<Endpoint> _endpointServiceRegistration;
+
+}
