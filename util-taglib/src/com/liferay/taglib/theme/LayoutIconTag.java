44d639bd23d490aca4b9820efb7ca47d701a8b62
Jonathan Mak <jonathan.mak@liferay.com>
2016-10-03 15:43:08 -0700

    LPS-68472 - Deprecate <liferay-theme:layout-icon /> taglib

--- util-taglib/src/com/liferay/taglib/theme/LayoutIconTag.java
+++ util-taglib/src/com/liferay/taglib/theme/LayoutIconTag.java
@@ -24,7 +24,9 @@ import javax.servlet.jsp.tagext.BodyTag;
 
 /**
  * @author Brian Wing Shun Chan
+ * @deprecated As of 7.0.0, with no direct replacement
  */
+@Deprecated
 public class LayoutIconTag
 	extends com.liferay.taglib.util.IncludeTag implements BodyTag {
 
