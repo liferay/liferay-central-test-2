64c5f99c66bbf5282b7c1a4b3e8fe80deae8b518
Brian Chan <brian.chan@liferay.com>
2016-10-04 15:37:50 -0700

    LPS-55063 SF

--- portal-impl/test/integration/com/liferay/portal/service/GroupServiceTest.java
+++ portal-impl/test/integration/com/liferay/portal/service/GroupServiceTest.java
@@ -298,7 +298,7 @@ public class GroupServiceTest {
 			initialTagsCount + 1,
 			AssetTagLocalServiceUtil.getGroupTagsCount(group.getGroupId()));
 
-		User user = UserTestUtil.addUser(group.getGroupId());
+		UserTestUtil.addUser(group.getGroupId());
 
 		BlogsEntry blogsEntry = BlogsEntryLocalServiceUtil.addEntry(
 			user.getUserId(), RandomTestUtil.randomString(),
