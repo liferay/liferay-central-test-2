2cfac13de1dba19d8c95b54538c78a7e565f9b1a
Jonathan Mak <jonathan.mak@liferay.com>
2016-10-04 15:22:57 -0700

    LPS-68491 - Deprecate <aui:spacer /> taglib

--- util-taglib/src/com/liferay/taglib/aui/SpacerTag.java
+++ util-taglib/src/com/liferay/taglib/aui/SpacerTag.java
@@ -22,7 +22,9 @@ import javax.servlet.jsp.JspWriter;
 
 /**
  * @author Eduardo Lundgren
+ * @deprecated As of 7.0.0, with no direct replacement
  */
+@Deprecated
 public class SpacerTag extends TagSupport {
 
 	@Override
