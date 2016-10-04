f429e09cac3632781d19388d46782570556777fc
Brian Chan <brian.chan@liferay.com>
2016-10-04 15:27:22 -0700

    LPS-67437 remove unused

--- portal-impl/src/com/liferay/portal/service/persistence/impl/GroupFinderImpl.java
+++ portal-impl/src/com/liferay/portal/service/persistence/impl/GroupFinderImpl.java
@@ -1547,7 +1547,6 @@ public class GroupFinderImpl
 	private volatile Map<String, String> _joinMap;
 	private final Map<String, String> _replaceJoinAndWhereSQLCache =
 		new ConcurrentHashMap<>();
-	private volatile Long _userGroupClassNameId;
 	private volatile Map<String, String> _whereMap;
 
 }
