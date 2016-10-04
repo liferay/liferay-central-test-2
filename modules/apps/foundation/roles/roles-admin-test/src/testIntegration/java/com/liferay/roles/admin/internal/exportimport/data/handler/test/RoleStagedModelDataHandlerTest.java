904d97641eae28590a86049c898a91eacdd2a19f
Brian Chan <brian.chan@liferay.com>
2016-10-03 17:08:09 -0700

    LPS-68152 wrong package

--- modules/apps/foundation/roles/roles-admin-test/src/testIntegration/java/com/liferay/roles/admin/internal/exportimport/data/handler/test/RoleStagedModelDataHandlerTest.java
+++ modules/apps/foundation/roles/roles-admin-test/src/testIntegration/java/com/liferay/roles/admin/internal/exportimport/data/handler/test/RoleStagedModelDataHandlerTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.roles.admin.internal.exportimport.data.handler;
+package com.liferay.roles.admin.internal.exportimport.data.handler.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.model.Group;
