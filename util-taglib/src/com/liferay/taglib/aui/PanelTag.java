ce585d977c7b7a33ff8b9a57ccde789ade68cabb
Jonathan Mak <jonathan.mak@liferay.com>
2016-10-04 16:09:32 -0700

    LPS-68511 - Deprecate <aui:panel /> taglib

--- util-taglib/src/com/liferay/taglib/aui/PanelTag.java
+++ util-taglib/src/com/liferay/taglib/aui/PanelTag.java
@@ -28,7 +28,9 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * @author Julio Camarero
  * @author Brian Wing Shun Chan
+ * @deprecated As of 7.0.0, with no direct replacement
  */
+@Deprecated
 public class PanelTag extends BasePanelTag {
 
 	public void addToolTag(ToolTag toolTag) {
