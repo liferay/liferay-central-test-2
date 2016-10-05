e4cea79c90e5650d9e23a33f49a1fc4a2d75d70c
Lino Alves <lino.alves@liferay.com>
2016-10-05 15:22:50 -0300

    LPS-68228 using default languageId

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_layout_deserializer.js
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_layout_deserializer.js
@@ -160,7 +160,7 @@ AUI.add(
 					_deserializePage: function(page) {
 						var instance = this;
 
-						var languageId = themeDisplay.getLanguageId();
+						var languageId = themeDisplay.getDefaultLanguageId();
 
 						var description = page.description && page.description[languageId];
 
