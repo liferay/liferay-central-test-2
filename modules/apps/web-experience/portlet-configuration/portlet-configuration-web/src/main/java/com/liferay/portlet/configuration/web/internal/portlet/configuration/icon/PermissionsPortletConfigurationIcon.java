liferay/liferay-central-test-2

commit 8749310c46aae7db09a724864c4a48357199995d

LPS-39511 Moves code to java class

modules/apps/web-experience/portlet-configuration/portlet-configuration-web/src/main/java/com/liferay/portlet/configuration/web/internal/portlet/configuration/icon/PermissionsPortletConfigurationIcon.java
@@ -16,6 +16,8 @@ package com.liferay.portlet.configuration.web.internal.portlet.configuration.ico
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.portlet.LiferayWindowState;
 import com.liferay.portal.kernel.portlet.PortletProvider;
@@ -27,7 +29,10 @@ import com.liferay.portal.kernel.service.PortletLocalService;
 import com.liferay.portal.kernel.service.permission.PortletPermissionUtil;
 import com.liferay.portal.kernel.theme.PortletDisplay;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -105,7 +110,46 @@ public class PermissionsPortletConfigurationIcon
 
 		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
 
-		return portletDisplay.isShowPermissionsIcon();
+		String rootPortletId = portletDisplay.getRootPortletId();
+
+		if (Validator.isNotNull(portletDisplay.getPortletResource())) {
+			String portletResource = portletDisplay.getPortletResource();
+
+			Portlet portlet = _portletLocalService.getPortletById(
+				themeDisplay.getCompanyId(), portletResource);
+
+			rootPortletId = portlet.getRootPortletId();
+		}
+
+		boolean showPermissionsIcon = false;
+
+		Layout layout = themeDisplay.getLayout();
+
+		Group group = themeDisplay.getScopeGroup();
+
+		if (!group.hasStagingGroup() || _STAGING_LIVE_GROUP_LOCKING_ENABLED) {
+			try {
+				if (PortletPermissionUtil.contains(
+						themeDisplay.getPermissionChecker(), layout,
+						rootPortletId, ActionKeys.PERMISSIONS)) {
+
+					showPermissionsIcon = true;
+				}
+			}
+			catch (PortalException pe) {
+				showPermissionsIcon = false;
+			}
+		}
+
+		if (layout.isLayoutPrototypeLinkActive()) {
+			showPermissionsIcon = false;
+		}
+
+		if (group.isControlPanel()) {
+			showPermissionsIcon = false;
+		}
+
+		return showPermissionsIcon;
 	}
 
 	@Override
@@ -119,6 +163,10 @@ public class PermissionsPortletConfigurationIcon
 
 		_portletLocalService = portletLocalService;
 	}
+
+	private static final boolean _STAGING_LIVE_GROUP_LOCKING_ENABLED =
+		GetterUtil.getBoolean(
+			PropsUtil.get(PropsKeys.STAGING_LIVE_GROUP_LOCKING_ENABLED));
 
 	private PortletLocalService _portletLocalService;
 

modules/apps/web-experience/portlet-configuration/portlet-configuration-web/build.gradle
@@ -2,7 +2,7 @@ dependencies {
 	provided group: "com.liferay", name: "com.liferay.frontend.taglib", version: "2.0.0"
 	provided group: "com.liferay", name: "com.liferay.portal.upgrade", version: "2.0.0"
 	provided group: "com.liferay.portal", name: "com.liferay.portal.impl", version: "2.0.0"
-	provided group: "com.liferay.portal", name: "com.liferay.portal.kernel", version: "default"
+	provided group: "com.liferay.portal", name: "com.liferay.portal.kernel", version: "2.7.0"
 	provided group: "com.liferay.portal", name: "com.liferay.util.taglib", version: "2.0.0"
 	provided group: "javax.portlet", name: "portlet-api", version: "2.0"
 	provided group: "javax.servlet", name: "javax.servlet-api", version: "3.0.1"

portal-kernel/src/com/liferay/portal/kernel/theme/PortletDisplay.java
@@ -101,7 +101,6 @@ public class PortletDisplay implements Cloneable, Serializable {
 		_showExportImportIcon = master.isShowExportImportIcon();
 		_showHelpIcon = master.isShowHelpIcon();
 		_showMoveIcon = master.isShowMoveIcon();
-		_showPermissionsIcon = master.isShowPermissionsIcon();
 		_showPortletCssIcon = master.isShowPortletCssIcon();
 		_showPortletIcon = master.isShowPortletIcon();
 		_showPrintIcon = master.isShowPrintIcon();
@@ -173,7 +172,6 @@ public class PortletDisplay implements Cloneable, Serializable {
 		slave.setShowExportImportIcon(_showExportImportIcon);
 		slave.setShowHelpIcon(_showHelpIcon);
 		slave.setShowMoveIcon(_showMoveIcon);
-		slave.setShowPermissionsIcon(_showPermissionsIcon);
 		slave.setShowPortletCssIcon(_showPortletCssIcon);
 		slave.setShowPortletIcon(_showPortletIcon);
 		slave.setShowPrintIcon(_showPrintIcon);
@@ -463,10 +461,6 @@ public class PortletDisplay implements Cloneable, Serializable {
 		return _showMoveIcon;
 	}
 
-	public boolean isShowPermissionsIcon() {
-		return _showPermissionsIcon;
-	}
-
 	public boolean isShowPortletCssIcon() {
 		return _showPortletCssIcon;
 	}
@@ -550,7 +544,6 @@ public class PortletDisplay implements Cloneable, Serializable {
 		_showExportImportIcon = false;
 		_showHelpIcon = false;
 		_showMoveIcon = false;
-		_showPermissionsIcon = false;
 		_showPortletCssIcon = false;
 		_showPortletIcon = false;
 		_showPrintIcon = false;
@@ -751,10 +744,6 @@ public class PortletDisplay implements Cloneable, Serializable {
 		_showMoveIcon = showMoveIcon;
 	}
 
-	public void setShowPermissionsIcon(boolean showPermissionsIcon) {
-		_showPermissionsIcon = showPermissionsIcon;
-	}
-
 	public void setShowPortletCssIcon(boolean showPortletCssIcon) {
 		_showPortletCssIcon = showPortletCssIcon;
 	}
@@ -929,7 +918,6 @@ public class PortletDisplay implements Cloneable, Serializable {
 	private boolean _showExportImportIcon;
 	private boolean _showHelpIcon;
 	private boolean _showMoveIcon;
-	private boolean _showPermissionsIcon;
 	private boolean _showPortletCssIcon;
 	private boolean _showPortletIcon;
 	private boolean _showPrintIcon;

portal-kernel/src/com/liferay/portal/kernel/theme/packageinfo
@@ -1,1 +1,1 @@
-version 1.1.0
+version 1.0.0

portal-web/docroot/html/portal/render_portlet.jsp
@@ -176,7 +176,6 @@ boolean showEditGuestIcon = false;
 boolean showExportImportIcon = false;
 boolean showHelpIcon = false;
 boolean showMoveIcon = !stateMax && !themeDisplay.isStateExclusive();
-boolean showPermissionsIcon = false;
 boolean showPortletCssIcon = false;
 boolean showPortletIcon = (portletResourcePortlet != null) ? Validator.isNotNull(portletResourcePortlet.getIcon()) : Validator.isNotNull(portlet.getIcon());
 boolean showPrintIcon = portlet.hasPortletMode(responseContentType, LiferayPortletMode.PRINT);
@@ -191,36 +190,30 @@ if ((portletParallelRender != null) && (portletParallelRender.booleanValue() == 
 
 Layout curLayout = PortletConfigurationLayoutUtil.getLayout(themeDisplay);
 
-if ((!group.hasStagingGroup() || !PropsValues.STAGING_LIVE_GROUP_LOCKING_ENABLED)) {
+if ((!group.hasStagingGroup() || !PropsValues.STAGING_LIVE_GROUP_LOCKING_ENABLED) &&
+	(PortletPermissionUtil.contains(permissionChecker, themeDisplay.getScopeGroupId(), curLayout, portlet, ActionKeys.CONFIGURATION))) {
 
-	if (PortletPermissionUtil.contains(permissionChecker, themeDisplay.getScopeGroupId(), curLayout, portlet, ActionKeys.CONFIGURATION)) {
+	showConfigurationIcon = true;
 
-		showConfigurationIcon = true;
+	boolean supportsConfigurationLAR = portlet.getConfigurationActionInstance() != null;
+	boolean supportsDataLAR = !(portlet.getPortletDataHandlerInstance() instanceof DefaultConfigurationPortletDataHandler);
 
-		boolean supportsConfigurationLAR = portlet.getConfigurationActionInstance() != null;
-		boolean supportsDataLAR = !(portlet.getPortletDataHandlerInstance() instanceof DefaultConfigurationPortletDataHandler);
-
-		if (supportsConfigurationLAR || supportsDataLAR || !group.isControlPanel()) {
-			showExportImportIcon = true;
-		}
-
-		if (PropsValues.PORTLET_CSS_ENABLED) {
-			showPortletCssIcon = true;
-		}
-
-		Group checkingStagingGroup = group;
-
-		if (checkingStagingGroup.isControlPanel()) {
-			checkingStagingGroup = themeDisplay.getSiteGroup();
-		}
-
-		if ((checkingStagingGroup.isStaged() || checkingStagingGroup.isStagedRemotely()) && !checkingStagingGroup.hasLocalOrRemoteStagingGroup() && checkingStagingGroup.isStagedPortlet(portletId)) {
-			showStagingIcon = true;
-		}
+	if (supportsConfigurationLAR || supportsDataLAR || !group.isControlPanel()) {
+		showExportImportIcon = true;
 	}
 
-	if (PortletPermissionUtil.contains(permissionChecker, themeDisplay.getScopeGroupId(), curLayout, portlet, ActionKeys.PERMISSIONS)) {
-		showPermissionsIcon = true;
+	if (PropsValues.PORTLET_CSS_ENABLED) {
+		showPortletCssIcon = true;
+	}
+
+	Group checkingStagingGroup = group;
+
+	if (checkingStagingGroup.isControlPanel()) {
+		checkingStagingGroup = themeDisplay.getSiteGroup();
+	}
+
+	if ((checkingStagingGroup.isStaged() || checkingStagingGroup.isStagedRemotely()) && !checkingStagingGroup.hasLocalOrRemoteStagingGroup() && checkingStagingGroup.isStagedPortlet(portletId)) {
+		showStagingIcon = true;
 	}
 }
 
@@ -308,7 +301,6 @@ if (layout.isLayoutPrototypeLinkActive()) {
 	showCloseIcon = false;
 	showConfigurationIcon = false;
 	showMoveIcon = false;
-	showPermissionsIcon = false;
 	showPortletCssIcon = false;
 }
 
@@ -388,7 +380,6 @@ portletDisplay.setShowEditGuestIcon(showEditGuestIcon);
 portletDisplay.setShowExportImportIcon(showExportImportIcon);
 portletDisplay.setShowHelpIcon(showHelpIcon);
 portletDisplay.setShowMoveIcon(showMoveIcon);
-portletDisplay.setShowPermissionsIcon(showPermissionsIcon);
 portletDisplay.setShowPortletCssIcon(showPortletCssIcon);
 portletDisplay.setShowPortletIcon(showPortletIcon);
 portletDisplay.setShowPrintIcon(showPrintIcon);
@@ -803,7 +794,6 @@ if (group.isControlPanel()) {
 	portletDisplay.setShowBackIcon(false);
 	portletDisplay.setShowConfigurationIcon(false);
 	portletDisplay.setShowMoveIcon(false);
-	portletDisplay.setShowPermissionsIcon(false);
 	portletDisplay.setShowPortletCssIcon(false);
 
 	if (!portlet.isPreferencesUniquePerLayout() && (portlet.getConfigurationActionInstance() != null) && PortletPermissionUtil.contains(permissionChecker, themeDisplay.getScopeGroupId(), curLayout, portlet, ActionKeys.CONFIGURATION)) {

commit 2dcad49b6abb3abb4670d5e449fe59bfaff03031

LPS-67918 fix regression caused by 8749310c46aae7db09a724864c4a48357199995d, use appropriate group to perform control panel check when showing porlet permissions icon

modules/apps/web-experience/portlet-configuration/portlet-configuration-web/src/main/java/com/liferay/portlet/configuration/web/internal/portlet/configuration/icon/PermissionsPortletConfigurationIcon.java
@@ -19,6 +19,7 @@ import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.model.impl.VirtualLayout;
 import com.liferay.portal.kernel.portlet.LiferayWindowState;
 import com.liferay.portal.kernel.portlet.PortletProvider;
 import com.liferay.portal.kernel.portlet.PortletProviderUtil;
@@ -126,10 +127,20 @@ public class PermissionsPortletConfigurationIcon
 
 		Layout layout = themeDisplay.getLayout();
 
-		Group group = themeDisplay.getScopeGroup();
+		try {
+			Group group = layout.getGroup();
 
-		if (!group.hasStagingGroup() || _STAGING_LIVE_GROUP_LOCKING_ENABLED) {
-			try {
+			if (layout instanceof VirtualLayout) {
+				VirtualLayout virtualLayout = (VirtualLayout)layout;
+
+				Layout sourceLayout = virtualLayout.getSourceLayout();
+
+				group = sourceLayout.getGroup();
+			}
+
+			if (!group.hasStagingGroup() ||
+				_STAGING_LIVE_GROUP_LOCKING_ENABLED) {
+
 				if (PortletPermissionUtil.contains(
 						themeDisplay.getPermissionChecker(), layout,
 						rootPortletId, ActionKeys.PERMISSIONS)) {
@@ -137,16 +148,16 @@ public class PermissionsPortletConfigurationIcon
 					showPermissionsIcon = true;
 				}
 			}
-			catch (PortalException pe) {
+
+			if (layout.isLayoutPrototypeLinkActive()) {
+				showPermissionsIcon = false;
+			}
+
+			if (group.isControlPanel()) {
 				showPermissionsIcon = false;
 			}
 		}
-
-		if (layout.isLayoutPrototypeLinkActive()) {
-			showPermissionsIcon = false;
-		}
-
-		if (group.isControlPanel()) {
+		catch (PortalException pe) {
 			showPermissionsIcon = false;
 		}
 