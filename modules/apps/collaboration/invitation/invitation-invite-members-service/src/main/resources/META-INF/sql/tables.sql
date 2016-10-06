40844c0db6530728cfb8cab14609ae653e1339fa
Adolfo Pérez <adolfo.perez@liferay.com>
2016-10-06 10:47:45 +0200

    LPS-63232 Manually remove SQL with old namespace

--- modules/apps/collaboration/invitation/invitation-invite-members-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/apps/collaboration/invitation/invitation-invite-members-service/src/main/resources/META-INF/sql/tables.sql
@@ -12,18 +12,3 @@ create table IM_MemberRequest (
 	invitedTeamId LONG,
 	status INTEGER
 );
-
-create table SO_MemberRequest (
-	memberRequestId LONG not null primary key,
-	groupId LONG,
-	companyId LONG,
-	userId LONG,
-	userName VARCHAR(75) null,
-	createDate DATE null,
-	modifiedDate DATE null,
-	key_ VARCHAR(75) null,
-	receiverUserId LONG,
-	invitedRoleId LONG,
-	invitedTeamId LONG,
-	status INTEGER
-);
