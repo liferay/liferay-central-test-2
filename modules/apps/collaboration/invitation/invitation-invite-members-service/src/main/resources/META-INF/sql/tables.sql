fb9e4c9bb251a6d8822363435e7ba9a0948602e4
Adolfo Pérez <adolfo.perez@liferay.com>
2016-10-06 10:46:32 +0200

    LPS-63232 Autogenerated

--- modules/apps/collaboration/invitation/invitation-invite-members-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/apps/collaboration/invitation/invitation-invite-members-service/src/main/resources/META-INF/sql/tables.sql
@@ -1,3 +1,18 @@
+create table IM_MemberRequest (
+	memberRequestId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	key_ VARCHAR(75) null,
+	receiverUserId LONG,
+	invitedRoleId LONG,
+	invitedTeamId LONG,
+	status INTEGER
+);
+
 create table SO_MemberRequest (
 	memberRequestId LONG not null primary key,
 	groupId LONG,
