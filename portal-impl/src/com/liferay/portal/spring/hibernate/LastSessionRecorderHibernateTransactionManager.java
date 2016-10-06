f09a008218070af251cf5dcc74aa18a6ea86d91b
shuyangzhou <shuyang.zhou@liferay.com>
2016-10-06 11:10:40 -0700

    LPS-68576 Avoid using TransactionSynchronizationManager to access spring hibernate ThreadLocals

--- portal-impl/src/com/liferay/portal/spring/hibernate/LastSessionRecorderHibernateTransactionManager.java
+++ portal-impl/src/com/liferay/portal/spring/hibernate/LastSessionRecorderHibernateTransactionManager.java
@@ -14,17 +14,8 @@
 
 package com.liferay.portal.spring.hibernate;
 
-import com.liferay.portal.kernel.util.CentralizedThreadLocal;
-import com.liferay.portal.kernel.util.InitialThreadLocal;
-import com.liferay.portal.kernel.util.ReflectionUtil;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-
-import org.springframework.core.NamedThreadLocal;
 import org.springframework.orm.hibernate3.HibernateTransactionManager;
 import org.springframework.orm.hibernate3.SessionHolder;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 /**
  * @author Shuyang Zhou
@@ -35,51 +26,13 @@ public class LastSessionRecorderHibernateTransactionManager
 	@Override
 	protected Object doGetTransaction() {
 		SessionHolder sessionHolder =
-			(SessionHolder)TransactionSynchronizationManager.getResource(
-				getSessionFactory());
+			SpringHibernateThreadLocalUtil.getResource(getSessionFactory());
 
 		if (sessionHolder != null) {
 			LastSessionRecorderUtil.setLastSession(sessionHolder.getSession());
 		}
 
 		return super.doGetTransaction();
-	}
-
-	static {
-		try {
-			Field nameField = ReflectionUtil.getDeclaredField(
-				NamedThreadLocal.class, "name");
-
-			for (Field field : ReflectionUtil.getDeclaredFields(
-					TransactionSynchronizationManager.class)) {
-
-				if (Modifier.isStatic(field.getModifiers()) &&
-					ThreadLocal.class.isAssignableFrom(field.getType())) {
-
-					ThreadLocal<Object> threadLocal =
-						(ThreadLocal<Object>)field.get(null);
-
-					Object value = threadLocal.get();
-
-					if (threadLocal instanceof NamedThreadLocal) {
-						threadLocal = new InitialThreadLocal<>(
-							(String)nameField.get(threadLocal), null);
-					}
-					else {
-						threadLocal = new CentralizedThreadLocal<>(false);
-					}
-
-					if (value != null) {
-						threadLocal.set(value);
-					}
-
-					field.set(null, threadLocal);
-				}
-			}
-		}
-		catch (Exception e) {
-			throw new ExceptionInInitializerError(e);
-		}
 	}
 
 }
