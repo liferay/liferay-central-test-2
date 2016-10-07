6c461a0abc8cb7fcf2c04a870e408ce37a9fc15c
shuyangzhou <shuyang.zhou@liferay.com>
2016-10-06 17:02:23 -0700

    LPS-67452 Optimize StringUtil.splitLines()

--- portal-kernel/src/com/liferay/portal/kernel/util/StringUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/StringUtil.java
@@ -3542,29 +3542,9 @@ public class StringUtil {
 			return _emptyStringArray;
 		}
 
-		if ((delimiter == CharPool.RETURN) ||
-			(delimiter == CharPool.NEW_LINE)) {
-
-			return splitLines(s);
-		}
-
 		List<String> nodeValues = new ArrayList<>();
 
-		int offset = 0;
-
-		int pos = s.indexOf(delimiter, offset);
-
-		while (pos != -1) {
-			nodeValues.add(s.substring(offset, pos));
-
-			offset = pos + 1;
-
-			pos = s.indexOf(delimiter, offset);
-		}
-
-		if (offset < s.length()) {
-			nodeValues.add(s.substring(offset));
-		}
+		_split(nodeValues, s, 0, delimiter);
 
 		return nodeValues.toArray(new String[nodeValues.size()]);
 	}
@@ -3931,23 +3911,22 @@ public class StringUtil {
 
 		while (true) {
 			int returnIndex = s.indexOf(CharPool.RETURN, lastIndex);
-			int newLineIndex = s.indexOf(CharPool.NEW_LINE, lastIndex);
-
-			if ((returnIndex == -1) && (newLineIndex == -1)) {
-				break;
-			}
 
 			if (returnIndex == -1) {
-				lines.add(s.substring(lastIndex, newLineIndex));
+				_split(lines, s, lastIndex, CharPool.NEW_LINE);
 
-				lastIndex = newLineIndex + 1;
+				return lines.toArray(new String[lines.size()]);
 			}
-			else if (newLineIndex == -1) {
-				lines.add(s.substring(lastIndex, returnIndex));
 
-				lastIndex = returnIndex + 1;
+			int newLineIndex = s.indexOf(CharPool.NEW_LINE, lastIndex);
+
+			if (newLineIndex == -1) {
+				_split(lines, s, lastIndex, CharPool.RETURN);
+
+				return lines.toArray(new String[lines.size()]);
 			}
-			else if (newLineIndex < returnIndex) {
+
+			if (newLineIndex < returnIndex) {
 				lines.add(s.substring(lastIndex, newLineIndex));
 
 				lastIndex = newLineIndex + 1;
@@ -3962,12 +3941,6 @@ public class StringUtil {
 				}
 			}
 		}
-
-		if (lastIndex < s.length()) {
-			lines.add(s.substring(lastIndex));
-		}
-
-		return lines.toArray(new String[lines.size()]);
 	}
 
 	/**
@@ -5146,6 +5119,24 @@ public class StringUtil {
 		}
 
 		return Character.isWhitespace(c);
+	}
+
+	private static void _split(
+		List<String> values, String s, int offset, char delimiter) {
+
+		int pos = s.indexOf(delimiter, offset);
+
+		while (pos != -1) {
+			values.add(s.substring(offset, pos));
+
+			offset = pos + 1;
+
+			pos = s.indexOf(delimiter, offset);
+		}
+
+		if (offset < s.length()) {
+			values.add(s.substring(offset));
+		}
 	}
 
 	private static String _wrap(String text, int width, String lineSeparator)
