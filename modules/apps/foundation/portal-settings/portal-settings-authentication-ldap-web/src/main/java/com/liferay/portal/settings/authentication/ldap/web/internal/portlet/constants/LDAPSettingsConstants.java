921e560754b23a7a65a4aa39cc6fb13934c3cf14
Michael Han <michael.han@liferay.com>
2016-10-07 20:11:35 -0500

    LPS-68565 move properties that are only used by web

---
+++ modules/apps/foundation/portal-settings/portal-settings-authentication-ldap-web/src/main/java/com/liferay/portal/settings/authentication/ldap/web/internal/portlet/constants/LDAPSettingsConstants.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.settings.authentication.ldap.web.internal.portlet.constants;
+
+/**
+ * @author Michael C. Han
+ */
+public class LDAPSettingsConstants {
+
+	public static final String BCRYPT = "BCRYPT";
+
+	public static final String IMPORT_METHOD_GROUP = "group";
+
+	public static final String IMPORT_METHOD_USER = "user";
+
+	public static final String IMPORT_USER_SYNC_STRATEGY_AUTH_TYPE =
+		"auth-type";
+
+	public static final String IMPORT_USER_SYNC_STRATEGY_UUID = "uuid";
+
+	public static final String MD2 = "MD2";
+
+	public static final String MD5 = "MD5";
+
+	public static final String NONE = "NONE";
+
+	public static final String SHA = "SHA";
+
+	public static final String SHA_256 = "SHA-256";
+
+	public static final String SHA_384 = "SHA-384";
+
+	public static final String SSHA = "SSHA";
+
+	public static final String UFC_CRYPT = "UFC-CRYPT";
+
+}
