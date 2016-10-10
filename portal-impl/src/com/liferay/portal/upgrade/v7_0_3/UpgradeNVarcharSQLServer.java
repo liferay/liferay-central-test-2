1235d2e81453ef50938fe68a771b7122e3ff4470
Cristina González Castellano <cristina.gonzalez@liferay.com>
2016-10-10 17:42:06 +0200

    LPS-68410 Upgrade all NVARCHAR(2000) types in SQLServer to NVARCHAR(4000)

---
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_3/UpgradeNVarcharSQLServer.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.v7_0_3;
+
+import com.liferay.portal.kernel.dao.db.DB;
+import com.liferay.portal.kernel.dao.db.DBManagerUtil;
+import com.liferay.portal.kernel.dao.db.DBType;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.LoggingTimer;
+import com.liferay.portal.kernel.util.StringBundler;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * @author Cristina González
+ */
+public class UpgradeNVarcharSQLServer extends UpgradeProcess {
+
+	protected void alterNVarcharColumns() throws Exception {
+		StringBundler sb = new StringBundler(4);
+
+		sb.append("select table_name, column_name from ");
+		sb.append("INFORMATION_SCHEMA.COLUMNS ");
+		sb.append("where DATA_TYPE = 'nvarchar' ");
+		sb.append("and character_maximum_length = 2000");
+
+		try (LoggingTimer loggingTimer = new LoggingTimer();
+
+			PreparedStatement ps = connection.prepareStatement(sb.toString());
+
+			ResultSet rs = ps.executeQuery()) {
+
+			while (rs.next()) {
+				String tableName = rs.getString(1);
+
+				String columnName = rs.getString(2);
+
+				try {
+					runSQL(
+						"alter table " + tableName + " alter column " +
+						columnName + " nvarchar(4000)");
+				}
+				catch (SQLException sqle) {
+					if (sqle.getErrorCode() == 1441) {
+						if (_log.isWarnEnabled()) {
+							sb = new StringBundler(6);
+
+							sb.append("Unable to alter length of column ");
+							sb.append(columnName);
+							sb.append(" for table ");
+							sb.append(tableName);
+							sb.append(" because it contains values that are ");
+							sb.append("larger than the new column length");
+
+							_log.warn(sb.toString());
+						}
+					}
+					else {
+						throw sqle;
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		DB db = DBManagerUtil.getDB();
+
+		if (db.getDBType() != DBType.SQLSERVER) {
+			return;
+		}
+
+		alterNVarcharColumns();
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		UpgradeNVarcharSQLServer.class);
+
+}
