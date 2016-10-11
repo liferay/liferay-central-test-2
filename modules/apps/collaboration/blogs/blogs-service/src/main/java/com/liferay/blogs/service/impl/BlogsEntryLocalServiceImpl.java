sergiogonzalez/liferay-portal

LPS-67467 Normalize and keep older friendly URLs

modules/apps/collaboration/blogs/blogs-service/src/main/java/com/liferay/blogs/service/impl/BlogsEntryLocalServiceImpl.java
@@ -33,6 +33,7 @@ import com.liferay.blogs.kernel.util.comparator.EntryIdComparator;
 import com.liferay.blogs.service.base.BlogsEntryLocalServiceBaseImpl;
 import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.friendly.url.model.FriendlyURL;
 import com.liferay.friendly.url.service.FriendlyURLLocalService;
 import com.liferay.portal.kernel.comment.CommentManagerUtil;
 import com.liferay.portal.kernel.dao.orm.ExportActionableDynamicQuery;
@@ -299,7 +300,11 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 		entry.setSubtitle(subtitle);
 
 		if (Validator.isNotNull(urlTitle)) {
-			entry.setUrlTitle(urlTitle);
+			FriendlyURL friendlyURL = friendlyURLLocalService.addFriendlyURL(
+				user.getCompanyId(), groupId, BlogsEntry.class, entryId,
+				urlTitle);
+
+			entry.setUrlTitle(friendlyURL.getFriendlyUrl());
 		}
 
 		entry.setDescription(description);
@@ -1241,7 +1246,11 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 		entry.setSubtitle(subtitle);
 
 		if (Validator.isNotNull(urlTitle)) {
-			entry.setUrlTitle(urlTitle);
+			FriendlyURL friendlyURL = friendlyURLLocalService.addFriendlyURL(
+				entry.getCompanyId(), entry.getGroupId(), BlogsEntry.class,
+				entry.getEntryId(), urlTitle);
+
+			entry.setUrlTitle(friendlyURL.getFriendlyUrl());
 		}
 
 		entry.setDescription(description);
@@ -1477,9 +1486,14 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 		if ((status == WorkflowConstants.STATUS_APPROVED) &&
 			Validator.isNull(entry.getUrlTitle())) {
 
-			entry.setUrlTitle(
-				getUniqueUrlTitle(
-					entryId, entry.getGroupId(), entry.getTitle()));
+			String uniqueUrlTitle = getUniqueUrlTitle(
+				entryId, entry.getGroupId(), entry.getTitle());
+
+			FriendlyURL friendlyURL = friendlyURLLocalService.addFriendlyURL(
+				entry.getCompanyId(), entry.getGroupId(), BlogsEntry.class,
+				entry.getEntryId(), uniqueUrlTitle);
+
+			entry.setUrlTitle(friendlyURL.getFriendlyUrl());
 		}
 
 		blogsEntryPersistence.update(entry);

modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/service/impl/FriendlyURLLocalServiceImpl.java
@@ -22,6 +22,7 @@ import com.liferay.friendly.url.model.FriendlyURL;
 import com.liferay.friendly.url.service.base.FriendlyURLLocalServiceBaseImpl;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.ModelHintsUtil;
+import com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil;
 
 /**
  * @author Adolfo Pérez
@@ -29,6 +30,42 @@ import com.liferay.portal.kernel.model.ModelHintsUtil;
 @ProviderType
 public class FriendlyURLLocalServiceImpl
 	extends FriendlyURLLocalServiceBaseImpl {
+
+	@Override
+	public FriendlyURL addFriendlyURL(
+			long companyId, long groupId, Class<?> clazz, long classPK,
+			String friendlyUrl)
+		throws PortalException {
+
+		String normalizedFriendlyUrl = FriendlyURLNormalizerUtil.normalize(
+			friendlyUrl);
+
+		validate(companyId, groupId, clazz, normalizedFriendlyUrl);
+
+		long classNameId = classNameLocalService.getClassNameId(clazz);
+
+		FriendlyURL mainFriendlyURL = friendlyURLPersistence.fetchByC_G_C_C_M(
+			companyId, groupId, classNameId, classPK, true);
+
+		if (mainFriendlyURL != null) {
+			mainFriendlyURL.setMain(false);
+
+			friendlyURLPersistence.update(mainFriendlyURL);
+		}
+
+		long friendlyURLId = counterLocalService.increment();
+
+		FriendlyURL friendlyURL = createFriendlyURL(friendlyURLId);
+
+		friendlyURL.setCompanyId(companyId);
+		friendlyURL.setGroupId(groupId);
+		friendlyURL.setClassNameId(classNameId);
+		friendlyURL.setClassPK(classPK);
+		friendlyURL.setFriendlyUrl(normalizedFriendlyUrl);
+		friendlyURL.setMain(true);
+
+		return friendlyURLPersistence.update(friendlyURL);
+	}
 
 	@Override
 	public void validate(
@@ -44,8 +81,11 @@ public class FriendlyURLLocalServiceImpl
 
 		long classNameId = classNameLocalService.getClassNameId(clazz);
 
+		String normalizedFriendlyUrl = FriendlyURLNormalizerUtil.normalize(
+			friendlyUrl);
+
 		int count = friendlyURLPersistence.countByC_G_C_F(
-			companyId, groupId, classNameId, friendlyUrl);
+			companyId, groupId, classNameId, normalizedFriendlyUrl);
 
 		if (count > 0) {
 			throw new DuplicateFriendlyURLException();

liferay/liferay-central-test-2

modules/apps/collaboration/blogs/blogs-service/src/main/java/com/liferay/blogs/service/impl/BlogsEntryLocalServiceImpl.java
@@ -33,6 +33,7 @@ import com.liferay.blogs.kernel.util.comparator.EntryIdComparator;
 import com.liferay.blogs.service.base.BlogsEntryLocalServiceBaseImpl;
 import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.friendly.url.model.FriendlyURL;
 import com.liferay.friendly.url.service.FriendlyURLLocalService;
 import com.liferay.portal.kernel.comment.CommentManagerUtil;
 import com.liferay.portal.kernel.dao.orm.ExportActionableDynamicQuery;
@@ -300,7 +301,11 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 		entry.setSubtitle(subtitle);
 
 		if (Validator.isNotNull(urlTitle)) {
-			entry.setUrlTitle(urlTitle);
+			FriendlyURL friendlyURL = friendlyURLLocalService.addFriendlyURL(
+				user.getCompanyId(), groupId, BlogsEntry.class, entryId,
+				urlTitle);
+
+			entry.setUrlTitle(friendlyURL.getUrlTitle());
 		}
 
 		entry.setDescription(description);
@@ -1248,7 +1253,11 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 		entry.setSubtitle(subtitle);
 
 		if (Validator.isNotNull(urlTitle)) {
-			entry.setUrlTitle(urlTitle);
+			FriendlyURL friendlyURL = friendlyURLLocalService.addFriendlyURL(
+				entry.getCompanyId(), entry.getGroupId(), BlogsEntry.class,
+				entry.getEntryId(), urlTitle);
+
+			entry.setUrlTitle(friendlyURL.getUrlTitle());
 		}
 
 		entry.setDescription(description);
@@ -1482,9 +1491,14 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 		if ((status == WorkflowConstants.STATUS_APPROVED) &&
 			Validator.isNull(entry.getUrlTitle())) {
 
-			entry.setUrlTitle(
-				getUniqueUrlTitle(
-					entryId, entry.getGroupId(), entry.getTitle()));
+			String uniqueUrlTitle = getUniqueUrlTitle(
+				entryId, entry.getGroupId(), entry.getTitle());
+
+			FriendlyURL friendlyURL = friendlyURLLocalService.addFriendlyURL(
+				entry.getCompanyId(), entry.getGroupId(), BlogsEntry.class,
+				entry.getEntryId(), uniqueUrlTitle);
+
+			entry.setUrlTitle(friendlyURL.getUrlTitle());
 		}
 
 		blogsEntryPersistence.update(entry);

modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/service/impl/FriendlyURLLocalServiceImpl.java
@@ -22,6 +22,7 @@ import com.liferay.friendly.url.model.FriendlyURL;
 import com.liferay.friendly.url.service.base.FriendlyURLLocalServiceBaseImpl;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.ModelHintsUtil;
+import com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil;
 
 /**
  * @author Adolfo Pérez
@@ -29,6 +30,42 @@ import com.liferay.portal.kernel.model.ModelHintsUtil;
 @ProviderType
 public class FriendlyURLLocalServiceImpl
 	extends FriendlyURLLocalServiceBaseImpl {
+
+	@Override
+	public FriendlyURL addFriendlyURL(
+			long companyId, long groupId, Class<?> clazz, long classPK,
+			String urlTitle)
+		throws PortalException {
+
+		String normalizedUrlTitle = FriendlyURLNormalizerUtil.normalize(
+			urlTitle);
+
+		validate(companyId, groupId, clazz, normalizedUrlTitle);
+
+		long classNameId = classNameLocalService.getClassNameId(clazz);
+
+		FriendlyURL mainFriendlyURL = friendlyURLPersistence.fetchByC_G_C_C_M(
+			companyId, groupId, classNameId, classPK, true);
+
+		if (mainFriendlyURL != null) {
+			mainFriendlyURL.setMain(false);
+
+			friendlyURLPersistence.update(mainFriendlyURL);
+		}
+
+		long friendlyURLId = counterLocalService.increment();
+
+		FriendlyURL friendlyURL = createFriendlyURL(friendlyURLId);
+
+		friendlyURL.setCompanyId(companyId);
+		friendlyURL.setGroupId(groupId);
+		friendlyURL.setClassNameId(classNameId);
+		friendlyURL.setClassPK(classPK);
+		friendlyURL.setUrlTitle(normalizedUrlTitle);
+		friendlyURL.setMain(true);
+
+		return friendlyURLPersistence.update(friendlyURL);
+	}
 
 	@Override
 	public void validate(
@@ -44,8 +81,11 @@ public class FriendlyURLLocalServiceImpl
 
 		long classNameId = classNameLocalService.getClassNameId(clazz);
 
+		String normalizedUrlTitle = FriendlyURLNormalizerUtil.normalize(
+			urlTitle);
+
 		int count = friendlyURLPersistence.countByC_G_C_U(
-			companyId, groupId, classNameId, urlTitle);
+			companyId, groupId, classNameId, normalizedUrlTitle);
 
 		if (count > 0) {
 			throw new DuplicateFriendlyURLException();