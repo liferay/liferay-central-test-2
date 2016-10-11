54aa087034ae1c2b6715b209b35d02c60a406fa0
Michael Bowerman <michael.bowerman@liferay.com>
2016-10-11 09:55:34 -0700

    LPS-68647 Revert changes made for Oracle by LPS-67131, they do not work properly

--- portal-impl/src/com/liferay/portal/verify/VerifyPermission.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyPermission.java
@@ -57,7 +57,6 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.service.impl.ResourcePermissionLocalServiceImpl;
 import com.liferay.portal.util.PortalInstances;
 
-import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -424,19 +423,13 @@ public class VerifyPermission extends VerifyProcess {
 
 		runSQL(sb.toString());
 
-		try (CallableStatement cs = connection.prepareCall(
-				"{call DBMS_ERRLOG.CREATE_ERROR_LOG('ResourcePermission')}")) {
-
-			cs.execute();
-		}
-
 		for (long companyId : companyIds) {
 			Role powerUserRole = RoleLocalServiceUtil.getRole(
 				companyId, RoleConstants.POWER_USER);
 			Role userRole = RoleLocalServiceUtil.getRole(
 				companyId, RoleConstants.USER);
 
-			sb = new StringBundler(20);
+			sb = new StringBundler(19);
 
 			sb.append("update ResourcePermission r1 set roleId = ");
 			sb.append(userRole.getRoleId());
@@ -456,13 +449,10 @@ public class VerifyPermission extends VerifyProcess {
 			sb.append(userGroupClassNameId);
 			sb.append(") and Layout.type_ = '");
 			sb.append(LayoutConstants.TYPE_PORTLET);
-			sb.append("') log errors into ERR$_ResourcePermission ('') ");
-			sb.append("reject limit unlimited");
+			sb.append("')");
 
 			runSQL(sb.toString());
 		}
-
-		runSQL("drop table ERR$_ResourcePermission");
 
 		runSQL("alter table ResourcePermission drop column plid");
 	}
