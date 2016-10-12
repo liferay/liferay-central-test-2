thiago-rocha/liferay-portal

LPS-68076 Toggle visibility of the remove item button according to a number of tems

modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_builder_rule.js
@@ -1,6 +1,8 @@
 AUI.add(
 	'liferay-ddl-form-builder-rule',
 	function(A) {
+		var CSS_CAN_REMOVE_ITEM = A.getClassName('can', 'remove', 'item');
+
 		var ddl = window.ddl;
 
 		var textOperators = [
@@ -83,6 +85,9 @@ AUI.add(
 
 						contentBox.delegate('click', A.bind(instance._handleAddConditionClick, instance), '.form-builder-rule-add-condition');
 						contentBox.delegate('click', A.bind(instance._handleAddActionClick, instance), '.form-builder-rule-add-action');
+
+						instance.after(instance._toggleShowRemoveButton, instance, '_addAction');
+						instance.after(instance._toggleShowRemoveButton, instance, '_addCondition');
 
 						instance.on('*:valueChanged', A.bind(instance._handleFieldValueChanged, instance));
 					},
@@ -307,6 +312,8 @@ AUI.add(
 								instance._actionsIndexes.splice(actionIndex, 1);
 							}
 						}
+
+						instance._toggleShowRemoveButton();
 					},
 
 					_handleDeleteConditionClick: function(event) {
@@ -335,6 +342,8 @@ AUI.add(
 								instance._conditionsIndexes.splice(conditionIndex, 1);
 							}
 						}
+
+						instance._toggleShowRemoveButton();
 					},
 
 					_handleFieldValueChanged: function(event) {
@@ -572,6 +581,24 @@ AUI.add(
 						field.render(container);
 
 						instance._conditions[index + '-condition-second-operand-type'] = field;
+					},
+
+					_toggleShowRemoveButton: function() {
+						var instance = this;
+
+						var contentBox = instance.get('contentBox');
+
+						var conditionList = contentBox.one('.liferay-ddl-form-rule-builder-condition-list');
+
+						var actionList = contentBox.one('.liferay-ddl-form-rule-builder-action-list');
+
+						var conditionItems = conditionList.all('.timeline-item');
+
+						var actionItems = actionList.all('.timeline-item');
+
+						conditionList.toggleClass(CSS_CAN_REMOVE_ITEM, conditionItems.size() > 2);
+
+						actionList.toggleClass(CSS_CAN_REMOVE_ITEM, actionItems.size() > 2);
 					},
 
 					_updateSecondOperandFieldVisibility: function(index) {

liferay/liferay-central-test-2

modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_builder_rule.js
@@ -1,6 +1,8 @@
 AUI.add(
 	'liferay-ddl-form-builder-rule',
 	function(A) {
+		var CSS_CAN_REMOVE_ITEM = A.getClassName('can', 'remove', 'item');
+
 		var ddl = window.ddl;
 
 		var textOperators = [
@@ -92,6 +94,9 @@ AUI.add(
 
 						boundingBox.delegate('click', A.bind(instance._handleLogicOperatorChange, instance), '.logic-operator');
 
+						instance.after(instance._toggleShowRemoveButton, instance, '_addAction');
+						instance.after(instance._toggleShowRemoveButton, instance, '_addCondition');
+
 						instance.on('*:valueChanged', A.bind(instance._handleFieldValueChanged, instance));
 						instance.on('logicOperatorChange', A.bind(instance._onLogicOperatorChanged, instance));
 					},
@@ -99,7 +104,7 @@ AUI.add(
 					render: function(rule) {
 						var instance = this;
 
-						var boundingBox = instance.get('boundingBox');
+						var contentBox = instance.get('contentBox');
 
 						if (!rule) {
 							rule = {
@@ -112,7 +117,7 @@ AUI.add(
 							instance.set('logicOperator', rule.conditions[0]['logic-operator']);
 						}
 
-						boundingBox.setHTML(instance._getRuleContainerTemplate(rule));
+						contentBox.setHTML(instance._getRuleContainerTemplate(rule));
 
 						instance._conditionsIndexes = [];
 						instance._actionsIndexes = [];
@@ -444,6 +449,8 @@ AUI.add(
 								instance._actionsIndexes.splice(actionIndex, 1);
 							}
 						}
+
+						instance._toggleShowRemoveButton();
 					},
 
 					_handleDeleteConditionClick: function(event) {
@@ -472,6 +479,8 @@ AUI.add(
 								instance._conditionsIndexes.splice(conditionIndex, 1);
 							}
 						}
+
+						instance._toggleShowRemoveButton();
 					},
 
 					_handleFieldValueChanged: function(event) {
@@ -764,6 +773,24 @@ AUI.add(
 						field.render(container);
 
 						instance._conditions[index + '-condition-second-operand-type'] = field;
+					},
+
+					_toggleShowRemoveButton: function() {
+						var instance = this;
+
+						var contentBox = instance.get('contentBox');
+
+						var conditionList = contentBox.one('.liferay-ddl-form-builder-rule-condition-list');
+
+						var actionList = contentBox.one('.liferay-ddl-form-builder-rule-action-list');
+
+						var conditionItems = conditionList.all('.timeline-item');
+
+						var actionItems = actionList.all('.timeline-item');
+
+						conditionList.toggleClass(CSS_CAN_REMOVE_ITEM, conditionItems.size() > 2);
+
+						actionList.toggleClass(CSS_CAN_REMOVE_ITEM, actionItems.size() > 2);
 					},
 
 					_updateSecondOperandFieldVisibility: function(index) {