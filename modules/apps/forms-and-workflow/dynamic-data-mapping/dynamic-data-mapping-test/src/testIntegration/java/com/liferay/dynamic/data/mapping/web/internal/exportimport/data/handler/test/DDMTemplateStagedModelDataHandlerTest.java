8d095f85b1cb06576c18e6b4e03e8a4f9a11fa73
Brian Chan <brian.chan@liferay.com>
2016-10-13 10:20:23 -0700

    LPS-67937 SF

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/web/internal/exportimport/data/handler/test/DDMTemplateStagedModelDataHandlerTest.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/web/internal/exportimport/data/handler/test/DDMTemplateStagedModelDataHandlerTest.java
@@ -79,7 +79,6 @@ public class DDMTemplateStagedModelDataHandlerTest
 		Assert.assertNotNull(importedTemplate);
 
 		Assert.assertNotNull(importedStructure);
-
 		Assert.assertEquals(
 			importedStructure.getStructureId(), importedTemplate.getClassPK());
 	}
