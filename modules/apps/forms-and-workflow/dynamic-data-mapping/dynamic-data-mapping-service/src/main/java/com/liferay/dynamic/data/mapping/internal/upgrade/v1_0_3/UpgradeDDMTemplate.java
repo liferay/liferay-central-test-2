f39250e4989f7b6f42f7e755a0a3f8b219ec9ef0
Brian Chan <brian.chan@liferay.com>
2016-10-15 13:53:44 -0700

    LPS-68449 Rename

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v1_0_3/UpgradeDDMTemplate.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v1_0_3/UpgradeDDMTemplate.java
@@ -14,23 +14,15 @@
 
 package com.liferay.dynamic.data.mapping.internal.upgrade.v1_0_3;
 
-import com.liferay.dynamic.data.mapping.internal.upgrade.v1_0_3.util.DDMStructureTable;
-import com.liferay.dynamic.data.mapping.internal.upgrade.v1_0_3.util.DDMStructureVersionTable;
 import com.liferay.dynamic.data.mapping.internal.upgrade.v1_0_3.util.DDMTemplateTable;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 
 /**
  * @author Christopher Kian
  */
-public class UpgradeTableColumnTypes extends UpgradeProcess {
+public class UpgradeDDMTemplate extends UpgradeProcess {
 
 	protected void doUpgrade() throws Exception {
-		alter(
-			DDMStructureTable.class,
-			new UpgradeProcess.AlterColumnType("name", "TEXT null"));
-		alter(
-			DDMStructureVersionTable.class,
-			new UpgradeProcess.AlterColumnType("name", "TEXT null"));
 		alter(
 			DDMTemplateTable.class,
 			new UpgradeProcess.AlterColumnType("name", "TEXT null"));
