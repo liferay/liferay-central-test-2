91642e54dc4d7cb60e8d44be00d127ca0ea82423
Brian Chan <brian.chan@liferay.com>
2016-10-17 10:31:40 -0700

    LPS-68289 Sort

--- portal-impl/test/integration/com/liferay/counter/service/CounterLocalServiceTest.java
+++ portal-impl/test/integration/com/liferay/counter/service/CounterLocalServiceTest.java
@@ -129,9 +129,6 @@ public class CounterLocalServiceTest {
 
 		List<String> arguments = new ArrayList<>();
 
-		arguments.add("-Xmx1024m");
-		arguments.add("-XX:MaxPermSize=200m");
-
 		arguments.add("-Dliferay.mode=test");
 
 		for (String property :
@@ -139,6 +136,9 @@ public class CounterLocalServiceTest {
 
 			arguments.add("-D" + property);
 		}
+
+		arguments.add("-Xmx1024m");
+		arguments.add("-XX:MaxPermSize=200m");
 
 		Builder builder = new Builder();
 
