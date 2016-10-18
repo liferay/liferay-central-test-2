3bdbfa5d2a62e7df3b9722bd120b174061aff6d2
Adolfo Pérez <adolfo.perez@liferay.com>
2016-10-18 18:36:53 +0200

    LPS-68590 Extract into util class
    We'll need to refer to the screen name regular expression later.

---
+++ modules/apps/collaboration/mentions/mentions-api/src/main/java/com/liferay/mentions/matcher/MentionsMatcherUtil.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mentions.matcher;
+
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class MentionsMatcherUtil {
+
+	public static String getScreenNameRegularExpression() {
+		String specialCharacters = PropsUtil.get(
+			PropsKeys.USERS_SCREEN_NAME_SPECIAL_CHARACTERS);
+
+		String quotedSpecialCharacters = StringUtil.replace(
+			specialCharacters,
+			new String[] {
+				StringPool.AMPERSAND, StringPool.CARET,
+				StringPool.CLOSE_BRACKET, StringPool.DASH,
+				StringPool.OPEN_BRACKET
+			},
+			new String[] {"\\&", "\\^", "\\]", "\\-", "\\["});
+
+		return "[" + quotedSpecialCharacters + "]";
+	}
+
+}
