d73646176bed93eedc53aecb9d1083d05e3a2e5a
drewbrokke <drew.brokke@liferay.com>
2016-10-19 11:54:23 -0700

    LPS-68294 portal-configuration-persistence - adds ConfigurationModelListenerProvider

---
+++ modules/apps/static/portal-configuration/portal-configuration-persistence/src/main/java/com/liferay/portal/configuration/persistence/listener/ConfigurationModelListenerProvider.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.configuration.persistence.listener;
+
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Drew Brokke
+ */
+@Component(immediate = true)
+public class ConfigurationModelListenerProvider {
+
+	public static ConfigurationModelListener getConfigurationModelListener(
+		String configurationModelClassName) {
+
+		if (_serviceTrackerMap == null) {
+			return null;
+		}
+
+		return _serviceTrackerMap.getService(configurationModelClassName);
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, ConfigurationModelListener.class,
+			"model.class.name");
+	}
+
+	private static ServiceTrackerMap<String, ConfigurationModelListener>
+		_serviceTrackerMap;
+
+}
