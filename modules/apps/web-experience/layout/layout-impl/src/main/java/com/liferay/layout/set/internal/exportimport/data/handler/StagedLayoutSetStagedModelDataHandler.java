0fa66fed5d882cda1f51173138c4c99cbccc461d
Máté Thurzó <mate.thurzo@liferay.com>
2016-10-19 11:35:56 +0200

    LPS-68664 Add null checking for logo

--- modules/apps/web-experience/layout/layout-impl/src/main/java/com/liferay/layout/set/internal/exportimport/data/handler/StagedLayoutSetStagedModelDataHandler.java
+++ modules/apps/web-experience/layout/layout-impl/src/main/java/com/liferay/layout/set/internal/exportimport/data/handler/StagedLayoutSetStagedModelDataHandler.java
@@ -468,6 +468,10 @@ public class StagedLayoutSetStagedModelDataHandler
 
 		String logoPath = headerElement.attributeValue("logo-path");
 
+		if (Validator.isNull(logoPath)) {
+			return;
+		}
+
 		byte[] iconBytes = portletDataContext.getZipEntryAsByteArray(logoPath);
 
 		try {
