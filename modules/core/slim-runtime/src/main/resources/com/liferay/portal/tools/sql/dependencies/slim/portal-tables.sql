c3f3f160a7fd942c8788903d860d44ad976e6586
Raymond Auge <raymond.auge@liferay.com>
2016-10-19 16:14:06 -0400

    LPS-68848 the slim runtime uses a minimal DB and hibernate configuration consisting of only 4 tables from the original portal
    These tables are the minimal set required for Service Builder infrastructure for SB modules to run.
    Note that no other canned portal services are initialized or are usable.
    Sadly, we have to copy the original details as there's no other means to extract this information in a reusable manner.

---
+++ modules/core/slim-runtime/src/main/resources/com/liferay/portal/tools/sql/dependencies/slim/portal-tables.sql
@@ -0,0 +1,34 @@
+create table ClassName_ (
+	mvccVersion LONG default 0 not null,
+	classNameId LONG not null primary key,
+	value VARCHAR(200) null
+);
+
+create table Counter (
+	name VARCHAR(75) not null primary key,
+	currentId LONG
+);
+
+create table Release_ (
+	mvccVersion LONG default 0 not null,
+	releaseId LONG not null primary key,
+	createDate DATE null,
+	modifiedDate DATE null,
+	servletContextName VARCHAR(75) null,
+	schemaVersion VARCHAR(75) null,
+	buildNumber INTEGER,
+	buildDate DATE null,
+	verified BOOLEAN,
+	state_ INTEGER,
+	testString VARCHAR(1024) null
+);
+
+create table ServiceComponent (
+	mvccVersion LONG default 0 not null,
+	serviceComponentId LONG not null primary key,
+	buildNamespace VARCHAR(75) null,
+	buildNumber LONG,
+	buildDate LONG,
+	data_ TEXT null
+);
+
