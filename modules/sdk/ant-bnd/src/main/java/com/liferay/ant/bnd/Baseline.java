d22b3f21f24f5d9c042d6bf48614b276a3e05cb7
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-10-20 09:28:52 -0700

    LPS-67434 Do not cause a major increase if the package was only moved

--- modules/sdk/ant-bnd/src/main/java/com/liferay/ant/bnd/Baseline.java
+++ modules/sdk/ant-bnd/src/main/java/com/liferay/ant/bnd/Baseline.java
@@ -27,13 +27,17 @@ import aQute.lib.io.IO;
 
 import aQute.service.reporter.Reporter;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
 
@@ -98,7 +102,7 @@ public abstract class Baseline {
 
 			BundleInfo bundleInfo = baseline.getBundleInfo();
 
-			if (hasPackageDelta(infos, Delta.REMOVED)) {
+			if (hasPackageRemoved(infos)) {
 				bundleInfo.suggestedVersion = new Version(
 					bundleInfo.olderVersion.getMajor() + 1, 0, 0);
 
@@ -412,6 +416,29 @@ public abstract class Baseline {
 		return correct;
 	}
 
+	protected Set<String> getMovedPackages() throws IOException {
+		File movedPackagesFile = new File(
+			_bndFile.getParentFile(), "moved-packages.txt");
+
+		if (!movedPackagesFile.exists()) {
+			return Collections.emptySet();
+		}
+
+		Set<String> movedPackages = new HashSet<>();
+
+		try (BufferedReader bufferedReader = new BufferedReader(
+				new FileReader(movedPackagesFile))) {
+
+			String line = null;
+
+			while ((line = bufferedReader.readLine()) != null) {
+				movedPackages.add(line);
+			}
+		}
+
+		return movedPackages;
+	}
+
 	protected String getShortDelta(Delta delta) {
 		if (delta == Delta.ADDED) {
 			return "+";
@@ -437,9 +464,15 @@ public abstract class Baseline {
 		return String.valueOf(deltaString.charAt(0));
 	}
 
-	protected boolean hasPackageDelta(Iterable<Info> infos, Delta delta) {
+	protected boolean hasPackageRemoved(Iterable<Info> infos)
+		throws IOException {
+
+		Set<String> movedPackages = getMovedPackages();
+
 		for (Info info : infos) {
-			if (info.packageDiff.getDelta() == delta) {
+			if ((info.packageDiff.getDelta() == Delta.REMOVED) &&
+				!movedPackages.contains(info.packageName)) {
+
 				return true;
 			}
 		}
