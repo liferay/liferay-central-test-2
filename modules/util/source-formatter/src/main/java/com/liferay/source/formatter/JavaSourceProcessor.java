3e6074bdc220f6cd0888292a719d850fc16916f6
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2016-10-20 12:05:53 +0200

    LPS-68866 Correct class name

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/JavaSourceProcessor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/JavaSourceProcessor.java
@@ -606,7 +606,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 				sb.append("vulnerability using ");
 			}
 			else {
-				sb.append("Use SecureXMLBuilderUtil.");
+				sb.append("Use SecureXMLFactoryProviderUtil.");
 				sb.append("newDocumentBuilderFactory instead of ");
 			}
 
