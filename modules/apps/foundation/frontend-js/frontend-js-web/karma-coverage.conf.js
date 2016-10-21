648dc60bad4ee288853e5532f5d12da4c53786af
Chema Balsas <jose.balsas@liferay.com>
2016-10-21 21:28:35 +0200

    LPS-68326 Simplify tests

--- modules/apps/foundation/frontend-js/frontend-js-web/karma-coverage.conf.js
+++ modules/apps/foundation/frontend-js/frontend-js-web/karma-coverage.conf.js
@@ -1,13 +1,19 @@
 'use strict';
 
+var karmaHtml2JsPreprocessor = require('karma-html2js-preprocessor');
 var metalKarmaConfig = require('metal-karma-config/coverage');
 
 module.exports = function(config) {
 	metalKarmaConfig(config);
 
+	config.plugins.push(karmaHtml2JsPreprocessor);
+
 	config.files = [
 		'node_modules/metal*/src/**/*.js',
 		'src/**/*.es.js',
-		'test/**/*.js'
+		'test/**/*.js',
+		'test/**/fixtures/*.html'
 	];
+
+	config.preprocessors['test/**/fixtures/*.html'] = ['html2js'];
 };
--- modules/apps/foundation/frontend-js/frontend-js-web/karma.conf.js
+++ modules/apps/foundation/frontend-js/frontend-js-web/karma.conf.js
@@ -1,13 +1,19 @@
 'use strict';
 
+var karmaHtml2JsPreprocessor = require('karma-html2js-preprocessor');
 var liferayKarmaConfig = require('liferay-karma-config');
 
 module.exports = function(config) {
 	liferayKarmaConfig(config);
 
+	config.plugins.push(karmaHtml2JsPreprocessor);
+
 	config.files = [
 		'node_modules/metal*/src/**/*.js',
 		'src/**/*.es.js',
-		'test/**/*.js'
+		'test/**/*.js',
+		'test/**/fixtures/*.html'
 	];
+
+	config.preprocessors['test/**/fixtures/*.html'] = ['html2js'];
 };
