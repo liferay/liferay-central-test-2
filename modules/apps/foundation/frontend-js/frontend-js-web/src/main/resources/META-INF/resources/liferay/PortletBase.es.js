7e0ce259a2fa91cb078a6b1602d9a6d1fde07fa0
Chema Balsas <jose.balsas@liferay.com>
2016-10-21 13:19:08 +0200

    LPS-68326 Simplify

--- modules/apps/foundation/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/PortletBase.es.js
+++ modules/apps/foundation/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/PortletBase.es.js
@@ -10,48 +10,18 @@ import State from 'metal-state/src/State';
  */
 class PortletBase extends State {
 	/**
-	 * @inheritDoc
-	 */
-	constructor(config) {
-		super(config);
-
-		let namespace = config.namespace;
-
-		this.NS = namespace;
-		this.ID = namespace.replace(/^_(.*)_$/, '$1');
-
-		this.rootNode = config.rootNode;
-	}
-
-	/**
-	 * Returns the portlet's namespace.
+	 * Get all elements in the root node that matches
+	 * the specified css selector.
 	 *
-	 * @return {String} namespace
-	 * @protected
+	 * @param  {!String} selector Css selector
+	 * @param  {String} root Css selector for find inside a specific node.
+	 * If not specified, will search in portlet's rootNode or in the whole document.
+	 * @return {NodeList object} List of DOM elements
 	 */
-	getNS_() {
-		return this.NS;
-	}
+	all(selector, root) {
+		root = dom.toElement(root) || this.rootNode || document;
 
-	/**
-	 * Returns the root node element.
-	 *
-	 * @return {DOM element} The root node
-	 * @protected
-	 */
-	getRootNode_() {
-		return this.rootNode;
-	}
-
-	/**
-	 * Converts the given rootNodeId to a DOM element.
-	 *
-	 * @param {!String} nodeId The node id
-	 * @return {DOM element} The dom element with that id
-	 * @protected
-	 */
-	setRootNode_(nodeId) {
-		return dom.toElement(nodeId);
+		return root.querySelectorAll(this.namespaceSelector_(this.namespace, selector));
 	}
 
 	/**
@@ -65,23 +35,8 @@ class PortletBase extends State {
 	 * @return {String} namespaced id. '#id1' will return '#_namespace_id1'
 	 * @protected
 	 */
-	formatSelectorNamespace_(ns, selector) {
+	namespaceSelector_(ns, selector) {
 		return selector.replace(new RegExp('(#|\\[id=(\\\"|\\\'))(?!' + ns + ')', 'g'), '$1' + ns);
-	}
-
-	/**
-	 * Get all elements in the root node that matches
-	 * the specified css selector.
-	 *
-	 * @param  {!String} selector Css selector
-	 * @param  {String} root Css selector for find inside a specific node.
-	 * If not specified, will search in portlet's rootNode or in the whole document.
-	 * @return {NodeList object} List of DOM elements
-	 */
-	all(selector, root) {
-		root = dom.toElement(root) || this.rootNode || document;
-
-		return root.querySelectorAll(this.formatSelectorNamespace_(this.NS, selector));
 	}
 
 	/**
@@ -92,7 +47,7 @@ class PortletBase extends State {
 	 * @return {String} namespace + str.
 	 */
 	ns(str) {
-		return Liferay.Util.ns(this.NS, str);
+		return Liferay.Util.ns(this.namespace, str);
 	}
 
 	/**
@@ -107,7 +62,7 @@ class PortletBase extends State {
 	one(selector, root) {
 		root = dom.toElement(root) || this.rootNode || document;
 
-		return root.querySelector(this.formatSelectorNamespace_(this.NS, selector));
+		return root.querySelector(this.namespaceSelector_(this.namespace, selector));
 	}
 }
 
@@ -122,16 +77,15 @@ PortletBase.STATE = {
 	 * @type {String}
 	 */
 	namespace: {
-		validator: core.isString,
-		getter: 'getNS_'
+		validator: core.isString
 	},
+
 	/**
 	 * The portlet container
 	 * @type {String} Id of the DOM Element
 	 */
 	rootNode: {
-		getter: 'getRootNode_',
-		setter: 'setRootNode_'
+		setter: dom.toElement
 	}
 };
 
