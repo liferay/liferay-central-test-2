7eaf8caa30dd88b887969910d99b2eb4a2b6e59d
Chema Balsas <jose.balsas@liferay.com>
2016-10-21 13:16:36 +0200

    LPS-68326 Unneeded configuration

--- modules/apps/foundation/frontend-js/frontend-js-web/karma-coverage.conf.js
+++ modules/apps/foundation/frontend-js/frontend-js-web/karma-coverage.conf.js
@@ -6,8 +6,6 @@ module.exports = function(config) {
 	metalKarmaConfig(config);
 
 	config.files = [
-		'node_modules/metal-soy-bundle/build/bundle.js',
-		'node_modules/html2incdom/src/*.js',
 		'node_modules/metal*/src/**/*.js',
 		'src/**/*.es.js',
 		'test/**/*.js'
