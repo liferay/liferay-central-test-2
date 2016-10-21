db1b94328f4ca875bf93bce591872ef188893c22
Kenji Heigel <kenji.heigel@liferay.com>
2016-10-20 17:50:58 -0700

    LRQA-28415 Add portal profile class to chat-service

---
+++ modules/apps/chat/chat-service/src/main/java/com/liferay/chat/portal/profile/ModulePortalProfile.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.chat.portal.profile;
+
+import com.liferay.chat.configuration.ChatGroupServiceConfigurationBeanDeclaration;
+import com.liferay.chat.internal.events.LoginPostAction;
+import com.liferay.chat.internal.events.SessionDestroyAction;
+import com.liferay.chat.internal.model.listener.UserModelListener;
+import com.liferay.chat.internal.poller.ChatPollerProcessor;
+import com.liferay.chat.jabber.JabberImpl;
+import com.liferay.chat.jabber.JabberUtil;
+import com.liferay.chat.util.BuddyFinderUtil;
+import com.liferay.chat.util.DefaultBuddyFinderImpl;
+import com.liferay.portal.profile.BaseDSModulePortalProfile;
+import com.liferay.portal.profile.PortalProfile;
+
+import java.util.Collections;
+
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Kenji Heigel
+ */
+@Component(immediate = true, service = PortalProfile.class)
+public class ModulePortalProfile extends BaseDSModulePortalProfile {
+
+	@Activate
+	public void activate(ComponentContext componentContext) {
+		init(
+			componentContext, Collections.<String>emptySet(),
+			BuddyFinderUtil.class.getName(),
+			ChatGroupServiceConfigurationBeanDeclaration.class.getName(),
+			ChatPollerProcessor.class.getName(),
+			DefaultBuddyFinderImpl.class.getName(), JabberImpl.class.getName(),
+			JabberUtil.class.getName(), LoginPostAction.class.getName(),
+			SessionDestroyAction.class.getName(),
+			UserModelListener.class.getName());
+	}
+
+}
