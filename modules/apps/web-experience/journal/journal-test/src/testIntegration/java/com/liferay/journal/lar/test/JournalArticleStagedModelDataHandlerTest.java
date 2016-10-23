45e746b58919a59ef2a341faba532050b8d565f6
shuyangzhou <shuyang.zhou@liferay.com>
2016-10-22 20:52:32 -0700

    LPS-68908 Remove unncessary TransactionalTestRule

--- modules/apps/web-experience/journal/journal-test/src/testIntegration/java/com/liferay/journal/lar/test/JournalArticleStagedModelDataHandlerTest.java
+++ modules/apps/web-experience/journal/journal-test/src/testIntegration/java/com/liferay/journal/lar/test/JournalArticleStagedModelDataHandlerTest.java
@@ -43,7 +43,6 @@ import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.Sync;
 import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
-import com.liferay.portal.kernel.test.rule.TransactionalTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
@@ -77,8 +76,7 @@ public class JournalArticleStagedModelDataHandlerTest
 	public static final AggregateTestRule aggregateTestRule =
 		new AggregateTestRule(
 			new LiferayIntegrationTestRule(),
-			SynchronousDestinationTestRule.INSTANCE,
-			TransactionalTestRule.INSTANCE);
+			SynchronousDestinationTestRule.INSTANCE);
 
 	@Override
 	public boolean isAssetPrioritySupported() {
--- modules/apps/web-experience/journal/journal-test/src/testIntegration/java/com/liferay/journal/lar/test/JournalFeedStagedModelDataHandlerTest.java
+++ modules/apps/web-experience/journal/journal-test/src/testIntegration/java/com/liferay/journal/lar/test/JournalFeedStagedModelDataHandlerTest.java
@@ -34,7 +34,6 @@ import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.Sync;
 import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
-import com.liferay.portal.kernel.test.rule.TransactionalTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.PortalUtil;
@@ -70,8 +69,7 @@ public class JournalFeedStagedModelDataHandlerTest
 	public static final AggregateTestRule aggregateTestRule =
 		new AggregateTestRule(
 			new LiferayIntegrationTestRule(),
-			SynchronousDestinationTestRule.INSTANCE,
-			TransactionalTestRule.INSTANCE);
+			SynchronousDestinationTestRule.INSTANCE);
 
 	@Override
 	public void setUp() throws Exception {
