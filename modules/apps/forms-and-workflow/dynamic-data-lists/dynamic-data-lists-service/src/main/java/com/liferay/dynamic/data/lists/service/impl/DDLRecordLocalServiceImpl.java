4efce73465af01fb21e704c64fbf196bbde12fe7
Preston-Crary <preston.crary@liferay.com>
2016-10-24 10:05:39 -0700

    LPS-68923 Add nessesary service methods for DDLRecordLocalService.

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/service/impl/DDLRecordLocalServiceImpl.java
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/service/impl/DDLRecordLocalServiceImpl.java
@@ -521,6 +521,15 @@ public class DDLRecordLocalServiceImpl extends DDLRecordLocalServiceBaseImpl {
 			recordSetId, status, start, end, orderByComparator);
 	}
 
+	@Override
+	public List<DDLRecord> getRecords(
+		long recordSetId, int start, int end,
+		OrderByComparator<DDLRecord> obc) {
+
+		return ddlRecordPersistence.findByRecordSetId(
+			recordSetId, start, end, obc);
+	}
+
 	/**
 	 * Returns all the records matching the record set ID and user ID.
 	 *
@@ -531,6 +540,20 @@ public class DDLRecordLocalServiceImpl extends DDLRecordLocalServiceBaseImpl {
 	@Override
 	public List<DDLRecord> getRecords(long recordSetId, long userId) {
 		return ddlRecordPersistence.findByR_U(recordSetId, userId);
+	}
+
+	@Override
+	public List<DDLRecord> getRecords(
+		long recordSetId, long userId, int start, int end,
+		OrderByComparator<DDLRecord> obc) {
+
+		return ddlRecordPersistence.findByR_U(
+			recordSetId, userId, start, end, obc);
+	}
+
+	@Override
+	public int getRecordsCount(long recordSetId) {
+		return ddlRecordPersistence.countByRecordSetId(recordSetId);
 	}
 
 	/**
@@ -546,6 +569,11 @@ public class DDLRecordLocalServiceImpl extends DDLRecordLocalServiceBaseImpl {
 	@Override
 	public int getRecordsCount(long recordSetId, int status) {
 		return ddlRecordFinder.countByR_S(recordSetId, status);
+	}
+
+	@Override
+	public int getRecordsCount(long recordSetId, long userId) {
+		return ddlRecordPersistence.countByR_U(recordSetId, userId);
 	}
 
 	/**
