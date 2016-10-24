d55f4e2bc5951b1228ec8208eb6eb4c37e1d0f25
Bruno Basto <bruno.basto@liferay.com>
2016-10-24 17:08:04 -0300

    LPS-68922 Expose mvc command caches to sub classes

--- portal-kernel/src/com/liferay/portal/kernel/portlet/bridges/mvc/MVCCommandCache.java
+++ portal-kernel/src/com/liferay/portal/kernel/portlet/bridges/mvc/MVCCommandCache.java
@@ -32,6 +32,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -137,6 +138,10 @@ public class MVCCommandCache {
 
 			return _emptyMVCCommand;
 		}
+	}
+
+	public Set<String> getMVCCommandNames() {
+		return _serviceTrackerMap.keySet();
 	}
 
 	public List<? extends MVCCommand> getMVCCommands(String key) {
