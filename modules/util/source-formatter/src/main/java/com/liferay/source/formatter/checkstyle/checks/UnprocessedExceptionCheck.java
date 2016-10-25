352a6379212e613e1265ad8f38fb0297c54fa859
Hugo Huijser <hugo.huijser@liferay.com>
2016-10-25 14:44:06 +0800

    LPS-52675 Move logic to catch unprocessed exceptions out to Checkstyle

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/UnprocessedExceptionCheck.java
@@ -0,0 +1,246 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checkstyle.checks;
+
+import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
+import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.checkstyle.util.DetailASTUtil;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.FileContents;
+import com.puppycrawl.tools.checkstyle.api.FileText;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+import com.thoughtworks.qdox.JavaDocBuilder;
+import com.thoughtworks.qdox.model.JavaClass;
+import com.thoughtworks.qdox.model.JavaPackage;
+import com.thoughtworks.qdox.model.JavaSource;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Hugo Huijser
+ */
+public class UnprocessedExceptionCheck extends AbstractCheck {
+
+	public static final String MSG_UNPROCESSED_EXCEPTION =
+		"exception.unprocessed";
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] {TokenTypes.LITERAL_CATCH};
+	}
+
+	@Override
+	public void visitToken(DetailAST detailAST) {
+		FileContents fileContents = getFileContents();
+
+		String fileName = StringUtil.replace(
+			fileContents.getFileName(), CharPool.BACK_SLASH, CharPool.SLASH);
+
+		if (fileName.contains("/test/") ||
+			fileName.contains("/testIntegration/")) {
+
+			return;
+		}
+
+		DetailAST parameterDefAST = detailAST.findFirstToken(
+			TokenTypes.PARAMETER_DEF);
+
+		if (_containsVariable(
+				detailAST.findFirstToken(TokenTypes.SLIST),
+				_getName(parameterDefAST))) {
+
+			return;
+		}
+
+		String exceptionClassName = _getExceptionClassName(parameterDefAST);
+
+		if (exceptionClassName == null) {
+			return;
+		}
+
+		String originalExceptionClassName = exceptionClassName;
+
+		JavaDocBuilder javaDocBuilder = _getJavaDocBuilder();
+
+		if (!exceptionClassName.contains(StringPool.PERIOD)) {
+			for (String importedExceptionClassName :
+					_getImportedExceptionClassNames(javaDocBuilder)) {
+
+				if (importedExceptionClassName.endsWith(
+						StringPool.PERIOD + exceptionClassName)) {
+
+					exceptionClassName = importedExceptionClassName;
+
+					break;
+				}
+			}
+		}
+
+		if (!exceptionClassName.contains(StringPool.PERIOD)) {
+			exceptionClassName =
+				_getPackagePath(javaDocBuilder) + StringPool.PERIOD +
+					exceptionClassName;
+		}
+
+		JavaClass exceptionClass = javaDocBuilder.getClassByName(
+			exceptionClassName);
+
+		if (exceptionClass == null) {
+			return;
+		}
+
+		while (true) {
+			String packageName = exceptionClass.getPackageName();
+
+			if (!packageName.contains("com.liferay")) {
+				break;
+			}
+
+			exceptionClassName = exceptionClass.getName();
+
+			if (exceptionClassName.equals("PortalException") ||
+				exceptionClassName.equals("SystemException")) {
+
+				log(
+					parameterDefAST.getLineNo(), MSG_UNPROCESSED_EXCEPTION,
+					originalExceptionClassName);
+
+				break;
+			}
+
+			JavaClass exceptionSuperClass = exceptionClass.getSuperJavaClass();
+
+			if (exceptionSuperClass == null) {
+				break;
+			}
+
+			exceptionClass = exceptionSuperClass;
+		}
+	}
+
+	private boolean _containsVariable(
+		DetailAST detailAST, String variableName) {
+
+		List<DetailAST> nameASTList = DetailASTUtil.getAllChildTokens(
+			detailAST, true, TokenTypes.IDENT);
+
+		for (DetailAST nameAST : nameASTList) {
+			String name = nameAST.getText();
+
+			if (name.equals(variableName)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private String _getExceptionClassName(DetailAST parameterDefAST) {
+		DetailAST typeAST = parameterDefAST.findFirstToken(TokenTypes.TYPE);
+
+		DetailAST dotAST = typeAST.findFirstToken(TokenTypes.DOT);
+
+		if (dotAST == null) {
+			DetailAST nameAST = typeAST.findFirstToken(TokenTypes.IDENT);
+
+			if (nameAST != null) {
+				return nameAST.getText();
+			}
+
+			return null;
+		}
+
+		String name = StringPool.BLANK;
+
+		while (true) {
+			DetailAST lastChild = dotAST.getLastChild();
+
+			name = StringPool.PERIOD + lastChild.getText() + name;
+
+			DetailAST firstChild = dotAST.getFirstChild();
+
+			if (firstChild.getType() != TokenTypes.DOT) {
+				name = firstChild.getText() + name;
+
+				break;
+			}
+
+			dotAST = firstChild;
+		}
+
+		return name;
+	}
+
+	private Set<String> _getImportedExceptionClassNames(
+		JavaDocBuilder javaDocBuilder) {
+
+		Set<String> exceptionClassNames = new HashSet<>();
+
+		JavaSource javaSource = javaDocBuilder.getSources()[0];
+
+		for (String importClassName : javaSource.getImports()) {
+			if (importClassName.endsWith("Exception")) {
+				exceptionClassNames.add(importClassName);
+			}
+		}
+
+		return exceptionClassNames;
+	}
+
+	private JavaDocBuilder _getJavaDocBuilder() {
+		JavaDocBuilder javaDocBuilder = new JavaDocBuilder();
+
+		FileContents fileContents = getFileContents();
+
+		FileText fileText = fileContents.getText();
+
+		javaDocBuilder.addSource(
+			new UnsyncStringReader((String)fileText.getFullText()));
+
+		return javaDocBuilder;
+	}
+
+	private String _getName(DetailAST detailAST) {
+		DetailAST nameAST = detailAST.findFirstToken(TokenTypes.IDENT);
+
+		if (nameAST != null) {
+			return nameAST.getText();
+		}
+
+		DetailAST dotAST = detailAST.findFirstToken(TokenTypes.DOT);
+
+		if (dotAST != null) {
+			nameAST = dotAST.findFirstToken(TokenTypes.IDENT);
+
+			return nameAST.getText();
+		}
+
+		return null;
+	}
+
+	private String _getPackagePath(JavaDocBuilder javaDocBuilder) {
+		JavaPackage javaPackage = javaDocBuilder.getPackages()[0];
+
+		return javaPackage.getName();
+	}
+
+}
