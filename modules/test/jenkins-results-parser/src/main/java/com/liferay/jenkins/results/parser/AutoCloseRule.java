ff0e8692216d982e5f6fe5603834c5b855908657
Peter Yoo <peter.yoo@liferay.com>
2016-10-25 14:16:23 -0700

    LRQA-28621 When calculating failLimit from % subtract 1.

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/AutoCloseRule.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/AutoCloseRule.java
@@ -57,6 +57,10 @@ public class AutoCloseRule {
 
 		if (maxFailPercentage != -1) {
 			failLimit = (int)(maxFailPercentage * downstreamBuilds.size());
+
+			if (failLimit > 0) {
+				failLimit--;
+			}
 		}
 		else {
 			failLimit = maxFailCount;
