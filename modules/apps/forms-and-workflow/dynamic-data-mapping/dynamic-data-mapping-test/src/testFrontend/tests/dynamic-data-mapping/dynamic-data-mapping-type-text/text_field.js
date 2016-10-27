d058efef859a79033685833da80d33c9ab86fe6a
Thiago Rocha <thiago.rocha@liferay.com>
2016-10-27 20:10:54 -0300

    LPS-67567 Creating a section the add unit test and a section to regression test

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test/src/testFrontend/tests/dynamic-data-mapping/dynamic-data-mapping-type-text/text_field.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test/src/testFrontend/tests/dynamic-data-mapping/dynamic-data-mapping-type-text/text_field.js
@@ -23,143 +23,153 @@ describe(
 		);
 
 		describe(
-			'.showLoadingFeedback()',
+			'unit',
 			function() {
-				it(
-					'should show loading',
-					function(done) {
-						var textField = new Liferay.DDM.Field.Text(
-							{
-								context: {
-									errorMessage: 'error',
-									name: 'textField',
-									required: true,
-									value: 'marcellus'
-								}
+				describe(
+					'.showLoadingFeedback()',
+					function() {
+						it(
+							'should show loading',
+							function(done) {
+								var textField = new Liferay.DDM.Field.Text(
+									{
+										context: {
+											errorMessage: 'error',
+											name: 'textField',
+											required: true,
+											value: 'marcellus'
+										}
+									}
+								).render(document.body);
+
+								var container = textField.get('container');
+
+								assert.notOk(
+									container.one('.icon-spinner'),
+									'Loading icon should not be visible'
+								);
+
+								textField.showLoadingFeedback();
+
+								assert.ok(
+									container.one('.icon-spinner'),
+									'Loading icon should be visible'
+								);
+
+								textField.destroy();
+
+								done();
 							}
-						).render(document.body);
+						);
+					}
+				);
 
-						var container = textField.get('container');
+				describe(
+					'.syncInputHeight()',
+					function() {
+						it(
+							'should call syncInputHeight after render be trigged',
+							function(done) {
+								var textField = new Liferay.DDM.Field.Text(
+									{
+										context: {
+											name: 'textField',
+											displayStyle: 'multiline'
+										}
+									}
+								).render(document.body);
 
-						assert.notOk(
-							container.one('.icon-spinner'),
-							'Loading icon should not be visible'
+								var syncInputHeight = sinon.spy(textField, 'syncInputHeight');
+
+								textField.render();
+
+								sinon.assert.called(syncInputHeight);
+
+								done();
+							}
 						);
 
-						textField.showLoadingFeedback();
+						it(
+							'should call syncInputHeight after value attribute change',
+							function(done) {
+								var textField = new Liferay.DDM.Field.Text(
+									{
+										context: {
+											name: 'textField',
+											displayStyle: 'multiline'
+										}
+									}
+								).render(document.body);
 
-						assert.ok(
-							container.one('.icon-spinner'),
-							'Loading icon should be visible'
+								var syncInputHeight = sinon.spy(textField, 'syncInputHeight');
+
+								textField.set('value', 'lorem\nipsum\ndolor');
+
+								sinon.assert.called(syncInputHeight);
+
+								done();
+							}
 						);
 
-						textField.destroy();
 
-						done();
+						it(
+							'should call syncInputHeight only the display style is multiline',
+							function(done) {
+								var textField = new Liferay.DDM.Field.Text(
+									{
+										context: {
+											name: 'textField'
+										}
+									}
+								).render(document.body);
+
+								var syncInputHeight = sinon.spy(textField, 'syncInputHeight');
+
+								textField.set('value', 'lorem\nipsum\ndolor');
+
+								sinon.assert.notCalled(syncInputHeight);
+
+								textField.set('displayStyle', 'multiline');
+
+								textField.set('value', 'lorem\nipsum\ndolor\nsit\namet');
+
+								sinon.assert.called(syncInputHeight);
+
+								done();
+							}
+						);
+
+						it(
+							'should resize the textarea when syncInputHeight function is called',
+							function(done) {
+								var textField = new Liferay.DDM.Field.Text(
+									{
+										context: {
+											name: 'textField',
+											displayStyle: 'multiline'
+										}
+									}
+								).render(document.body);
+
+								var textareaHeight = textField.getInputNode().get('offsetHeight');
+
+								textField.setValue('lorem\nipsum\ndolor');
+
+								textField.syncInputHeight();
+
+								assert.isBelow(textareaHeight, textField.getInputNode().get('offsetHeight'));
+
+								done();
+							}
+						);
 					}
 				);
 			}
 		);
 
 		describe(
-			'.syncInputHeight()',
-			function() {
-				it(
-					'should call syncInputHeight after render be trigged',
-					function(done) {
-						var textField = new Liferay.DDM.Field.Text(
-							{
-								context: {
-									name: 'textField',
-									displayStyle: 'multiline'
-								}
-							}
-						).render(document.body);
-
-						var syncInputHeight = sinon.spy(textField, 'syncInputHeight');
-
-						textField.render();
-
-						sinon.assert.called(syncInputHeight);
-
-						done();
-					}
-				);
-
-				it(
-					'should call syncInputHeight after value attribute change',
-					function(done) {
-						var textField = new Liferay.DDM.Field.Text(
-							{
-								context: {
-									name: 'textField',
-									displayStyle: 'multiline'
-								}
-							}
-						).render(document.body);
-
-						var syncInputHeight = sinon.spy(textField, 'syncInputHeight');
-
-						textField.set('value', 'lorem\nipsum\ndolor');
-
-						sinon.assert.called(syncInputHeight);
-
-						done();
-					}
-				);
-
-
-				it(
-					'should call syncInputHeight only the display style is multiline',
-					function(done) {
-						var textField = new Liferay.DDM.Field.Text(
-							{
-								context: {
-									name: 'textField'
-								}
-							}
-						).render(document.body);
-
-						var syncInputHeight = sinon.spy(textField, 'syncInputHeight');
-
-						textField.set('value', 'lorem\nipsum\ndolor');
-
-						sinon.assert.notCalled(syncInputHeight);
-
-						textField.set('displayStyle', 'multiline');
-
-						textField.set('value', 'lorem\nipsum\ndolor\nsit\namet');
-
-						sinon.assert.called(syncInputHeight);
-
-						done();
-					}
-				);
-
-				it(
-					'should resize the textarea when syncInputHeight function is called',
-					function(done) {
-						var textField = new Liferay.DDM.Field.Text(
-							{
-								context: {
-									name: 'textField',
-									displayStyle: 'multiline'
-								}
-							}
-						).render(document.body);
-
-						var textareaHeight = textField.getInputNode().get('offsetHeight');
-
-						textField.setValue('lorem\nipsum\ndolor');
-
-						textField.syncInputHeight();
-
-						assert.isBelow(textareaHeight, textField.getInputNode().get('offsetHeight'));
-
-						done();
-					}
-				);
-			}
+			'regression',
+			function() {}
 		);
 	}
 );
