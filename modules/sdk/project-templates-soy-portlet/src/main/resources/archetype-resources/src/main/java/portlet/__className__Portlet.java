c7f44e5a0b7ee3ccdc7cde15c3fa751e03b94548
Bruno Basto <bruno.basto@liferay.com>
2016-10-28 16:13:50 -0300

    LPS-68405 Create Blade Template for Soy Portlet

---
+++ modules/sdk/project-templates-soy-portlet/src/main/resources/archetype-resources/src/main/java/portlet/__className__Portlet.java
@@ -0,0 +1,49 @@
+package ${package}.portlet;
+
+import com.liferay.portal.kernel.util.ReleaseInfo;
+import com.liferay.portal.portlet.bridge.soy.SoyPortlet;
+
+import java.io.IOException;
+
+import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=${artifactId} Portlet",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=View",
+		"javax.portlet.name=${portletName}",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class ${className}Portlet extends SoyPortlet {
+
+	@Override
+	public void render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		PortletURL navigationURL = renderResponse.createRenderURL();
+
+		navigationURL.setParameter("mvcRenderCommandName", "Navigation");
+
+		template.put("navigationURL", navigationURL.toString());
+
+		template.put("releaseInfo", ReleaseInfo.getReleaseInfo());
+
+		super.render(renderRequest, renderResponse);
+	}
+
+}
