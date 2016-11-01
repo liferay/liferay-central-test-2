eb01e9f1afd4fbcb41a239aa7f0e7984af7a9997
Leslie Wong <leslie.wong@liferay.com>
2016-11-01 12:17:56 -0700

    LRQA-28179 Have execution logic only in one method in ExternalMethod

--- modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/util/ExternalMethod.java
+++ modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/util/ExternalMethod.java
@@ -17,38 +17,78 @@ package com.liferay.poshi.runner.util;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
+import org.openqa.selenium.StaleElementReferenceException;
+
 /**
  * @author Kevin Yen
  */
 public class ExternalMethod {
 
-	public static String execute(
-			Object object, String methodName, String[] parameters)
+	public static Object execute(
+			Method method, Object object, Object[] parameters)
+		throws Exception {
+
+		Object returnObject = null;
+
+		try {
+			returnObject = method.invoke(object, parameters);
+		}
+		catch (Exception e1) {
+			Throwable throwable = e1.getCause();
+
+			if (throwable instanceof StaleElementReferenceException) {
+				StringBuilder sb = new StringBuilder();
+
+				sb.append("\nElement turned stale while running ");
+				sb.append(method.getName());
+				sb.append(". Retrying in ");
+				sb.append(PropsValues.TEST_RETRY_COMMAND_WAIT_TIME);
+				sb.append("seconds.");
+
+				System.out.println(sb.toString());
+
+				try {
+					returnObject = method.invoke(object, (Object[])parameters);
+				}
+				catch (Exception e2) {
+					throwable = e2.getCause();
+
+					throw new Exception(throwable.getMessage(), e2);
+				}
+			}
+			else {
+				throw new Exception(throwable.getMessage(), e1);
+			}
+		}
+
+		if (returnObject == null) {
+			return "";
+		}
+
+		return returnObject;
+	}
+
+	public static Object execute(
+			String methodName, Object object, Object[] parameters)
 		throws Exception {
 
 		Class<?> clazz = object.getClass();
 
 		Method method = getMethod(clazz, methodName, parameters);
 
-		Object returnObject = method.invoke(object, (Object[])parameters);
-
-		if (returnObject == null) {
-			returnObject = "";
-		}
-
-		return returnObject.toString();
+		return execute(method, object, parameters);
 	}
 
-	public static String execute(String className, String methodName)
+	public static Object execute(String className, String methodName)
 		throws Exception {
 
-		String[] parameters = {};
+		Object[] parameters = {};
 
 		return execute(className, methodName, parameters);
 	}
 
-	public static String execute(
-			String className, String methodName, String[] parameters)
+	public static Object execute(
+			String className, String methodName, Object[] parameters)
 		throws Exception {
 
 		Class<?> clazz = Class.forName(className);
@@ -63,13 +103,7 @@ public class ExternalMethod {
 			object = clazz.newInstance();
 		}
 
-		Object returnObject = method.invoke(object, (Object[])parameters);
-
-		if (returnObject == null) {
-			returnObject = "";
-		}
-
-		return returnObject.toString();
+		return execute(method, object, parameters);
 	}
 
 	public static Method getMethod(
@@ -82,7 +116,7 @@ public class ExternalMethod {
 	}
 
 	private static Class<?>[] _getTypes(Object[] objects) {
-		if (objects == null) {
+		if ((objects == null) || (objects.length == 0)) {
 			return new Class<?>[0];
 		}
 
