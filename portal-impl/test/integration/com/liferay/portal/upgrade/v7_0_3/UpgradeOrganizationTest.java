ba6110696c3b22f5a83b406054add4eefaa1d8b3
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-11-02 20:47:36 +0100

    LPS-68775 Add a test to verify that all organizations' type_field is upgraded following specs on LPS-62209

---
+++ portal-impl/test/integration/com/liferay/portal/upgrade/v7_0_3/UpgradeOrganizationTest.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.v7_0_3;
+
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.service.OrganizationLocalServiceUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.OrganizationTestUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class UpgradeOrganizationTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testUpgrade() throws Exception {
+		_organization = OrganizationTestUtil.addOrganization();
+
+		_organization.setType("regular-organization");
+
+		OrganizationLocalServiceUtil.updateOrganization(_organization);
+
+		_upgradeOrganization.upgrade();
+
+		List<Organization> organizations =
+			OrganizationLocalServiceUtil.getOrganizations(-1, -1);
+
+		for (Organization organization : organizations) {
+			Assert.assertEquals("organization", organization.getType());
+		}
+	}
+
+	@DeleteAfterTestRun
+	private Organization _organization;
+
+	private final UpgradeOrganization _upgradeOrganization =
+		new UpgradeOrganization();
+
+}
