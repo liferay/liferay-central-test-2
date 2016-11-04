16b272e5922d62f40f84d6440b7a49f5ffe97109
shuyangzhou <shuyang.zhou@liferay.com>
2016-11-04 00:01:15 -0700

    LPS-69090 Fix the classloading LinkageError by pulling ahead the required class loading, to make sure we won't try to load it after the ThreadPoolExecutor.shutdownNow() call.

--- modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/EmbeddedElasticsearchConnection.java
+++ modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/EmbeddedElasticsearchConnection.java
@@ -42,6 +42,8 @@ import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.node.Node;
 
+import org.jboss.netty.util.internal.ByteBufferUtil;
+
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
@@ -68,6 +70,19 @@ public class EmbeddedElasticsearchConnection
 
 		if (_node == null) {
 			return;
+		}
+
+		try {
+			Class.forName(ByteBufferUtil.class.getName());
+		}
+		catch (ClassNotFoundException cnfe) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"Unable to preload " + ByteBufferUtil.class +
+						" to prevent netty shutdown concurrent class loading " +
+							"interruption issue.",
+					cnfe);
+			}
 		}
 
 		_node.close();
