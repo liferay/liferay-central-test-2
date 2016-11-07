415be5f34c6f2f698ccf5eabd68481dee044f019
Alejandro Hernández <info@alejandrohdezma.com>
2016-11-07 17:39:50 +0100

    LPS-69238 Add entrypoint for getting list of configurations

---
+++ modules/apps/adaptive-media/adaptive-media-image-jax-rs/src/main/java/com/liferay/adaptive/media/image/jaxrs/AdaptiveMediaImageConfigRepr.java
@@ -0,0 +1,75 @@
+package com.liferay.adaptive.media.image.jaxrs;
+
+import com.liferay.adaptive.media.image.configuration.ImageAdaptiveMediaConfigurationEntry;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * It serves as a proxy for the API layer for a
+ * {@link ImageAdaptiveMediaConfigurationEntry}.
+ *
+ * @author Alejandro Hernández
+ */
+@XmlRootElement(name = "config")
+public class AdaptiveMediaImageConfigRepr {
+
+	public AdaptiveMediaImageConfigRepr() {
+	}
+
+	public AdaptiveMediaImageConfigRepr(
+		ImageAdaptiveMediaConfigurationEntry entry) {
+
+		_name = entry.getName();
+		String width = entry.getProperties().get("width");
+
+		if (width != null) {
+			_width = Integer.parseInt(width);
+		}
+
+		String height = entry.getProperties().get("height");
+
+		if (height != null) {
+			_height = Integer.parseInt(height);
+		}
+
+		_uuid = entry.getUUID();
+	}
+
+	public int getHeight() {
+		return _height;
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public String getUuid() {
+		return _uuid;
+	}
+
+	public int getWidth() {
+		return _width;
+	}
+
+	public void setHeight(int height) {
+		_height = height;
+	}
+
+	public void setName(String name) {
+		_name = name;
+	}
+
+	public void setUuid(String uuid) {
+		_uuid = uuid;
+	}
+
+	public void setWidth(int width) {
+		_width = width;
+	}
+
+	private int _height = -1;
+	private String _name;
+	private String _uuid;
+	private int _width = -1;
+
+}
--- modules/apps/adaptive-media/adaptive-media-image-jax-rs/src/main/java/com/liferay/adaptive/media/image/jaxrs/AdaptiveMediaImageConfigResource.java
+++ modules/apps/adaptive-media/adaptive-media-image-jax-rs/src/main/java/com/liferay/adaptive/media/image/jaxrs/AdaptiveMediaImageConfigResource.java
@@ -1,6 +1,15 @@
 package com.liferay.adaptive.media.image.jaxrs;
 
 import com.liferay.adaptive.media.image.configuration.ImageAdaptiveMediaConfiguration;
+import com.liferay.adaptive.media.image.configuration.ImageAdaptiveMediaConfigurationEntry;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.Response;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Alejandro Hernández
@@ -13,6 +22,25 @@ public class AdaptiveMediaImageConfigResource {
 
 		_companyId = companyId;
 		_imageAdaptiveMediaConfiguration = imageAdaptiveMediaConfiguration;
+	}
+
+	@GET
+	@Produces({"application/json", "application/xml"})
+	public Response getConfigurations() {
+		Collection<ImageAdaptiveMediaConfigurationEntry>
+			imageAdaptiveMediaConfigurationEntries =
+				_imageAdaptiveMediaConfiguration.
+					getImageAdaptiveMediaConfigurationEntries(_companyId);
+
+		List<AdaptiveMediaImageConfigRepr> configs =
+			imageAdaptiveMediaConfigurationEntries.stream().map(
+				AdaptiveMediaImageConfigRepr::new).collect(Collectors.toList());
+
+		GenericEntity<List<AdaptiveMediaImageConfigRepr>> entity =
+			new GenericEntity<List<AdaptiveMediaImageConfigRepr>>(configs) {
+			};
+
+		return Response.ok(entity).build();
 	}
 
 	private final long _companyId;
