316d0f04ffe9d6cb609510c88e41f01112422c17
Alejandro Hernández <info@alejandrohdezma.com>
2016-11-07 17:41:14 +0100

    LPS-69238 Add entrypoint for getting a configuration from an UUID

--- modules/apps/adaptive-media/adaptive-media-image-jax-rs/src/main/java/com/liferay/adaptive/media/image/jaxrs/AdaptiveMediaImageConfigResource.java
+++ modules/apps/adaptive-media/adaptive-media-image-jax-rs/src/main/java/com/liferay/adaptive/media/image/jaxrs/AdaptiveMediaImageConfigResource.java
@@ -4,11 +4,14 @@ import com.liferay.adaptive.media.image.configuration.ImageAdaptiveMediaConfigur
 import com.liferay.adaptive.media.image.configuration.ImageAdaptiveMediaConfigurationEntry;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.Response;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -22,6 +25,24 @@ public class AdaptiveMediaImageConfigResource {
 
 		_companyId = companyId;
 		_imageAdaptiveMediaConfiguration = imageAdaptiveMediaConfiguration;
+	}
+
+	@GET
+	@Path("/{uuid}")
+	@Produces({"application/json", "application/xml"})
+	public Response getConfiguration(@PathParam("uuid") String uuid) {
+		Optional<ImageAdaptiveMediaConfigurationEntry> entry =
+			_imageAdaptiveMediaConfiguration.
+				getImageAdaptiveMediaConfigurationEntry(_companyId, uuid);
+
+		if (!entry.isPresent()) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+
+		AdaptiveMediaImageConfigRepr config = new AdaptiveMediaImageConfigRepr(
+			entry.get());
+
+		return Response.ok(config).build();
 	}
 
 	@GET
