089976e177c992b7240967a08956fb3b6b878942
peterfellwock <peter.fellwock@liferay.com>
2016-11-07 16:15:56 -0800

    LPS-68937 Simplify since Util only has 1 TimelineProcessor to return

--- modules/apps/twitter/twitter-service/src/main/java/com/liferay/twitter/util/TimelineProcessorUtil.java
+++ modules/apps/twitter/twitter-service/src/main/java/com/liferay/twitter/util/TimelineProcessorUtil.java
@@ -14,35 +14,18 @@
 
 package com.liferay.twitter.util;
 
-import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.json.JSONArray;
-import com.liferay.portal.kernel.util.InstanceFactory;
-import com.liferay.twitter.configuration.TwitterGroupServiceConfiguration;
-
-import java.util.Map;
-
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.ConfigurationPolicy;
-import org.osgi.service.component.annotations.Modified;
 
 /**
  * @author Shinn Lok
  * @author Peter Fellwock
  */
-@Component(
-	configurationPid = "com.liferay.twitter.configuration.TwitterConfiguration",
-	configurationPolicy = ConfigurationPolicy.OPTIONAL, immediate = true,
-	service = TimelineProcessorUtil.class
-)
 public class TimelineProcessorUtil {
 
 	public static TimelineProcessor getInstance() {
-		if (_timelineProcessor != null) {
-			return _timelineProcessor;
+		if (_timelineProcessor == null) {
+			_timelineProcessor = new HttpTimelineProcessor();
 		}
-
-		_timelineProcessor = new HttpTimelineProcessor();
 
 		return _timelineProcessor;
 	}
@@ -54,23 +37,6 @@ public class TimelineProcessorUtil {
 			twitterScreenName, sinceId);
 	}
 
-	@Activate
-	@Modified
-	protected void activate(Map<String, Object> properties) {
-		_twitterGroupServiceConfiguration = ConfigurableUtil.createConfigurable(
-			TwitterGroupServiceConfiguration.class, properties);
-
-		try {
-			_timelineProcessor = (TimelineProcessor)InstanceFactory.newInstance(
-				_twitterGroupServiceConfiguration.usersTimelineProcessor());
-		}
-		catch (Exception e) {
-			throw new ExceptionInInitializerError(e);
-		}
-	}
-
 	private static TimelineProcessor _timelineProcessor;
-	private static volatile TwitterGroupServiceConfiguration
-		_twitterGroupServiceConfiguration;
 
 }
