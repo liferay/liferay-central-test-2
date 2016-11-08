84ba8b076367e003b15a87e164c1315b36d2a62b
Jonathan Mak <jonathan.mak@liferay.com>
2016-11-07 16:37:27 -0800

    LPS-69020 - Source formatting

--- modules/apps/foundation/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/preview.js
+++ modules/apps/foundation/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/preview.js
@@ -9,6 +9,8 @@ AUI.add(
 
 		var CSS_IMAGE_SELECTED = 'lfr-preview-file-image-selected';
 
+		var MAP_IMAGE_DATA = {};
+
 		var STR_CLICK = 'click';
 
 		var STR_CURRENT_INDEX = 'currentIndex';
@@ -18,12 +20,6 @@ AUI.add(
 		var STR_SCROLLER = 'scroller';
 
 		var STR_SRC = 'src';
-
-		var MAP_EVENT_SCROLLER = {
-			src: STR_SCROLLER
-		};
-
-		var MAP_IMAGE_DATA = {};
 
 		var TPL_IMAGES = '<a class="lfr-preview-file-image {selectedCssClass}" data-imageIndex="{index}" href="{url}" title="{displayedIndex}">' +
 				'<img src="{url}" />' +
@@ -38,6 +34,10 @@ AUI.add(
 		var TPL_MAX_ARROW_RIGHT = '<a href="javascript:;" class="image-viewer-control carousel-control right lfr-preview-file-arrow">&rsaquo;</a>';
 
 		var TPL_MAX_CONTROLS = '<span class="lfr-preview-file-image-overlay-controls"></span>';
+
+		var MAP_EVENT_SCROLLER = {
+			src: STR_SCROLLER
+		};
 
 		var Preview = A.Component.create(
 			{
