723a61d5df1862d5ad5f7b052f6b823f42125116
Diogo Moreira Lins <diogo.moreira@liferay.com>
2016-11-10 18:08:35 -0300

    LPS-60811 Add Test infrastructure

---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-select/karma.conf.js
@@ -0,0 +1,52 @@
+var liferayKarmaAlloyConfig = require('liferay-karma-alloy-config');
+var liferayKarmaConfig = require('liferay-karma-config');
+var path = require('path');
+
+module.exports = function(config) {
+	liferayKarmaConfig(config);
+
+	config.files = [];
+
+	liferayKarmaAlloyConfig(config);
+
+	var resourcesPath = 'src/main/resources/META-INF/resources';
+
+	config.files.push(
+		{
+			included: true,
+			pattern: 'node_modules/soyutils-nogoog/index.js'
+		},
+		{
+			included: true,
+			pattern: '../dynamic-data-mapping-form-renderer/' + resourcesPath + '/**/!(config)*.js'
+		},
+		{
+			included: true,
+			pattern: resourcesPath + '/**/!(config)*.js'
+		},
+		'src/testFrontend/**/*.js'
+	);
+
+	config.preprocessors[resourcesPath + '/**/*.soy.js'] = ['replacer'];
+
+	config.replacerPreprocessor = {
+		replacer: function(file, content) {
+			var filePath = file.path.split(path.sep);
+
+			var fileName = filePath.pop();
+
+			if (fileName === 'select.soy.js') {
+				content = [
+					'AUI.add(\'liferay-ddm-form-field-select-template\', function(A) {',
+					content.replace(
+						'(typeof ddm == \'undefined\') { var ddm = {}; }',
+						'(typeof ddm == \'undefined\') { window.ddm = {}; }'
+					),
+					'}, \'\', {requires: []});'
+				].join('');
+			}
+
+			return content;
+		}
+	};
+};
