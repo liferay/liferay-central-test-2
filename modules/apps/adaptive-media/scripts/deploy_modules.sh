8be2aa5bd5a2cb46b47f91deb29c1a9e0d7061b5
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2016-11-11 10:03:21 +0100

    LPS-69238 Sort

--- modules/apps/adaptive-media/scripts/deploy_modules.sh
+++ modules/apps/adaptive-media/scripts/deploy_modules.sh
@@ -3,7 +3,7 @@
 set -e
 
 PREFIX='adaptive-media-'
-MODULES='api document-library image-impl image-jax-rs image-item-selector-api image-js web'
+MODULES='api document-library image-impl image-item-selector-api image-jax-rs image-js web'
 
 for m in ${MODULES}; do
 	(cd ${PREFIX}${m} && ../scripts/run.sh ../gradlew deploy)
