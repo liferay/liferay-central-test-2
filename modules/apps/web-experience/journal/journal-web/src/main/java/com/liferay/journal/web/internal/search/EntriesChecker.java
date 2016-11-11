6ef2e99a194e8e1e154eead1a1c6b9301a59c6a2
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-11-11 10:24:49 +0100

    LPS-69167 SF - Use constant

--- modules/apps/web-experience/journal/journal-web/src/main/java/com/liferay/journal/web/internal/search/EntriesChecker.java
+++ modules/apps/web-experience/journal/journal-web/src/main/java/com/liferay/journal/web/internal/search/EntriesChecker.java
@@ -151,7 +151,7 @@ public class EntriesChecker extends EmptyOnClickRowChecker {
 		return getRowCheckBox(
 			request, checked, disabled,
 			_liferayPortletResponse.getNamespace() + RowChecker.ROW_IDS + name +
-				"",
+				StringPool.BLANK,
 			primaryKey, checkBoxRowIds, "'#" + getAllRowIds() + "'",
 			StringPool.BLANK);
 	}
