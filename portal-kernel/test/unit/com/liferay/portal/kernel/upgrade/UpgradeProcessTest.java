7384b1b60d1cbe6611c4e39008fb7d8933727519
Brian Chan <brian.chan@liferay.com>
2016-11-11 14:17:04 -0800

    LPS-69044 Rename

--- portal-kernel/test/unit/com/liferay/portal/kernel/upgrade/UpgradeProcessTest.java
+++ portal-kernel/test/unit/com/liferay/portal/kernel/upgrade/UpgradeProcessTest.java
@@ -42,95 +42,98 @@ public class UpgradeProcessTest {
 
 	@Test
 	public void testGetSQLTakesIntoAccountWholeColumnDescription() {
-		AlterColumnName alterColName = _upgradeProcess.new AlterColumnName(
-			_OLD_COLUMN_NAME, _NEW_COMPOSED_COLUMN_NAME);
+		AlterColumnName alterColumnName = _upgradeProcess.new AlterColumnName(
+			_OLD_COLUMN_NAME, _NEW_COLUMN);
 
-		String generatedSql = alterColName.getSQL(_TABLE_NAME);
+		String sql = alterColumnName.getSQL(_TABLE_NAME);
 
-		Assert.assertTrue(generatedSql.contains(_NEW_COMPOSED_COLUMN_NAME));
+		Assert.assertTrue(sql.contains(_NEW_COLUMN));
 	}
 
 	@Test
 	public void testShouldAddIndexIfColumnFromIndexRenamed() {
-		AlterColumnName alterColName = _upgradeProcess.new AlterColumnName(
+		AlterColumnName alterColumnName = _upgradeProcess.new AlterColumnName(
 			_OLD_COLUMN_NAME, _NEW_COLUMN_NAME);
 
-		Assert.assertTrue(alterColName.shouldAddIndex(_newIndexColumns));
+		Assert.assertTrue(alterColumnName.shouldAddIndex(_newIndexColumnNames));
 	}
 
 	@Test
 	public void testShouldAddIndexIfColumnFromIndexRenamedWithAttributes() {
-		AlterColumnName alterColName = _upgradeProcess.new AlterColumnName(
-			_OLD_COLUMN_NAME, _NEW_COMPOSED_COLUMN_NAME);
+		AlterColumnName alterColumnName = _upgradeProcess.new AlterColumnName(
+			_OLD_COLUMN_NAME, _NEW_COLUMN);
 
-		Assert.assertTrue(alterColName.shouldAddIndex(_newIndexColumns));
+		Assert.assertTrue(alterColumnName.shouldAddIndex(_newIndexColumnNames));
 	}
 
 	@Test
 	public void testShouldAddIndexIsCaseInsensitive() {
-		AlterColumnName alterColName = _upgradeProcess.new AlterColumnName(
+		AlterColumnName alterColumnName = _upgradeProcess.new AlterColumnName(
 			_OLD_COLUMN_NAME, StringUtil.toUpperCase(_NEW_COLUMN_NAME));
 
 		Assert.assertTrue(
-			alterColName.shouldAddIndex(_newIndexColumnsLowerCase));
+			alterColumnName.shouldAddIndex(_newIndexColumnsNamesLowerCase));
 	}
 
 	@Test
 	public void testShouldDropIndexIfColumnFromIndexRenamed() {
-		AlterColumnName alterColName = _upgradeProcess.new AlterColumnName(
+		AlterColumnName alterColumnName = _upgradeProcess.new AlterColumnName(
 			_OLD_COLUMN_NAME, _NEW_COLUMN_NAME);
 
-		Assert.assertTrue(alterColName.shouldDropIndex(_oldIndexColumns));
+		Assert.assertTrue(
+			alterColumnName.shouldDropIndex(_oldIndexColumnNames));
 	}
 
 	@Test
 	public void testShouldDropIndexIsCaseInsensitive() {
-		AlterColumnName alterColName = _upgradeProcess.new AlterColumnName(
+		AlterColumnName alterColumnName = _upgradeProcess.new AlterColumnName(
 			StringUtil.toUpperCase(_OLD_COLUMN_NAME), _NEW_COLUMN_NAME);
 
 		Assert.assertTrue(
-			alterColName.shouldDropIndex(_oldIndexColumnsLowercase));
+			alterColumnName.shouldDropIndex(_oldIndexColumnsNamesLowercase));
 	}
 
 	@Test
 	public void testShouldNotAddIndexIfColumnNotInIndexRenamed() {
-		AlterColumnName alterColName = _upgradeProcess.new AlterColumnName(
+		AlterColumnName alterColumnName = _upgradeProcess.new AlterColumnName(
 			_OLD_COLUMN_NAME, _NEW_COLUMN_NAME_NOT_IN_INDEX);
 
-		Assert.assertFalse(alterColName.shouldAddIndex(_newIndexColumns));
+		Assert.assertFalse(
+			alterColumnName.shouldAddIndex(_newIndexColumnNames));
 	}
 
 	@Test
 	public void testShouldNotDropIndexIfColumnNotInIndexRenamed() {
-		AlterColumnName alterColName = _upgradeProcess.new AlterColumnName(
+		AlterColumnName alterColumnName = _upgradeProcess.new AlterColumnName(
 			_OLD_COLUMN_NAME_NOT_IN_INDEX, _NEW_COLUMN_NAME);
 
-		Assert.assertFalse(alterColName.shouldDropIndex(_oldIndexColumns));
+		Assert.assertFalse(
+			alterColumnName.shouldDropIndex(_oldIndexColumnNames));
 	}
+
+	private static final String _NEW_COLUMN =
+		UpgradeProcessTest._NEW_COLUMN_NAME + " VARCHAR2(30) NOT NULL";
 
 	private static final String _NEW_COLUMN_NAME = "newColumnName";
 
 	private static final String _NEW_COLUMN_NAME_NOT_IN_INDEX =
 		"newNotIndexColumn";
 
-	private static final String _NEW_COMPOSED_COLUMN_NAME =
-		_NEW_COLUMN_NAME + " VARCHAR2(30) NOT NULL";
-
 	private static final String _OLD_COLUMN_NAME = "oldColumnName";
 
 	private static final String _OLD_COLUMN_NAME_NOT_IN_INDEX =
 		"oldNotIndexColumn";
 
-	private static final String _TABLE_NAME = "table";
+	private static final String _TABLE_NAME = "Table";
 
-	private static final List<String> _newIndexColumns = Arrays.asList(
+	private static final List<String> _newIndexColumnNames = Arrays.asList(
 		"newColumn1", _NEW_COLUMN_NAME, "newColumn2");
-	private static final List<String> _newIndexColumnsLowerCase = Arrays.asList(
-		StringUtil.toLowerCase(_NEW_COLUMN_NAME));
-	private static final List<String> _oldIndexColumns = Arrays.asList(
+	private static final List<String> _newIndexColumnsNamesLowerCase =
+		Arrays.asList(StringUtil.toLowerCase(_NEW_COLUMN_NAME));
+	private static final List<String> _oldIndexColumnNames = Arrays.asList(
 		"oldColumn1", _OLD_COLUMN_NAME, "oldColumn3");
-	private static final List<String> _oldIndexColumnsLowercase = Arrays.asList(
-		StringUtil.toLowerCase(_OLD_COLUMN_NAME));
+	private static final List<String> _oldIndexColumnsNamesLowercase =
+		Arrays.asList(StringUtil.toLowerCase(_OLD_COLUMN_NAME));
 
 	private UpgradeProcess _upgradeProcess;
 
