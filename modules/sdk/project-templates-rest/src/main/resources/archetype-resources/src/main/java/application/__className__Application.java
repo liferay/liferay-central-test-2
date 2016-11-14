40c292c4043e87c45f913ae209f7beda781f1203
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-11-14 14:47:46 -0800

    LPS-69230 Avoid import wildcards

--- modules/sdk/project-templates-rest/src/main/resources/archetype-resources/src/main/java/application/__className__Application.java
+++ modules/sdk/project-templates-rest/src/main/resources/archetype-resources/src/main/java/application/__className__Application.java
@@ -3,7 +3,12 @@ package ${package}.application;
 import java.util.Collections;
 import java.util.Set;
 
-import javax.ws.rs.*;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Application;
 
 import org.osgi.service.component.annotations.Component;
