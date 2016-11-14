cfaad4332c09a5c289254ffb7bbfa20514f38d1e
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-11-14 09:31:00 -0800

    LPS-68844 Test if the build script dependency is there

--- modules/sdk/project-templates/src/test/java/com/liferay/project/templates/ProjectTemplatesTest.java
+++ modules/sdk/project-templates/src/test/java/com/liferay/project/templates/ProjectTemplatesTest.java
@@ -732,7 +732,7 @@ public class ProjectTemplatesTest {
 
 		_testContains(
 			gradleProjectDir, "build.gradle",
-			"com.liferay.gradle.plugins.theme.builder",
+			"name: \"com.liferay.gradle.plugins.theme.builder\"",
 			"apply plugin: \"com.liferay.portal.tools.theme.builder\"");
 		_testContains(
 			gradleProjectDir,
