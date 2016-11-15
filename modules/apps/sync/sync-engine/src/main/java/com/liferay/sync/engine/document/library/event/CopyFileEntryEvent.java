462e584a2e9aca540ff38ab08f09c32802049eb4
Dennis Ju <dennis.ju@liferay.com>
2016-11-14 16:56:14 -0800

    SYNC-1695 Locally copied files are unavailable for LAN syncing

--- modules/apps/sync/sync-engine/src/main/java/com/liferay/sync/engine/document/library/event/CopyFileEntryEvent.java
+++ modules/apps/sync/sync-engine/src/main/java/com/liferay/sync/engine/document/library/event/CopyFileEntryEvent.java
@@ -19,6 +19,9 @@ import com.liferay.sync.engine.document.library.handler.CopyFileEntryHandler;
 import com.liferay.sync.engine.document.library.handler.Handler;
 import com.liferay.sync.engine.model.SyncFile;
 import com.liferay.sync.engine.service.SyncFileService;
+import com.liferay.sync.engine.util.FileUtil;
+
+import java.nio.file.Paths;
 
 import java.util.Map;
 
@@ -44,6 +47,9 @@ public class CopyFileEntryEvent extends BaseEvent {
 	protected void processRequest() throws Exception {
 		SyncFile syncFile = (SyncFile)getParameterValue("syncFile");
 
+		syncFile.setPreviousModifiedTime(
+			FileUtil.getLastModifiedTime(
+				Paths.get(syncFile.getFilePathName())));
 		syncFile.setState(SyncFile.STATE_IN_PROGRESS);
 		syncFile.setUiEvent(SyncFile.UI_EVENT_UPLOADING);
 
