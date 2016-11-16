4878b4000ff6293e026341b282034cbbbe2f9f0b
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-11-16 15:09:31 -0800

    LPS-66762 Add smoke test

---
+++ modules/sdk/gradle-plugins-baseline/src/gradleTest/smoke/sample-101/src/main/java/com/example/sample/GreetingBuilder.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.example.sample;
+
+/**
+ * @author Andrea Di Giorgi
+ */
+public class GreetingBuilder {
+
+	public GreetingBuilder(String name) {
+		_name = name;
+	}
+
+	public String getHello() {
+		return "Hello " + getName();
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	private final String _name;
+
+}
