3fe2d091ba437a71cfc70b2408a8e8643040ec62
peterfellwock <peter.fellwock@liferay.com>
2016-11-17 15:47:59 -0800

    LPS-69244 XML to Portlet OSGi-fy

--- modules/apps/my-subscriptions/my-subscriptions-web/src/main/java/com/liferay/my/subscriptions/web/internal/portlet/MySubscriptionsPortlet.java
+++ modules/apps/my-subscriptions/my-subscriptions-web/src/main/java/com/liferay/my/subscriptions/web/internal/portlet/MySubscriptionsPortlet.java
@@ -12,12 +12,13 @@
  * details.
  */
 
-package com.liferay.mysubscriptions.portlet;
+package com.liferay.my.subscriptions.web.internal.portlet;
 
+import com.liferay.my.subscriptions.web.internal.constants.MySubscriptionsPortletKeys;
 import com.liferay.portal.kernel.model.Subscription;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
-import com.liferay.portal.kernel.service.SubscriptionLocalServiceUtil;
+import com.liferay.portal.kernel.service.SubscriptionLocalService;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -25,11 +26,41 @@ import com.liferay.portal.kernel.util.WebKeys;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Peter Shin
  * @author Jonathan Lee
+ * @author Peter Fellwock
  */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.css-class-wrapper=my-subscriptions-portlet",
+		"com.liferay.portlet.display-category=category.collaboration",
+		"com.liferay.portlet.icon=/icons/my_subscriptions.png",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.remoteable=true",
+		"com.liferay.portlet.render-weight=0",
+		"javax.portlet.display-name=My Subscriptions",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.copy-request-parameters=true",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + MySubscriptionsPortletKeys.MY_SUBSCRIPTIONS,
+		"javax.portlet.portlet-info.keywords=My Subscriptions",
+		"javax.portlet.portlet-info.short-title=My Subscriptions",
+		"javax.portlet.portlet-info.title=My Subscriptions",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
 public class MySubscriptionsPortlet extends MVCPortlet {
 
 	public void unsubscribe(
@@ -52,14 +83,17 @@ public class MySubscriptionsPortlet extends MVCPortlet {
 			}
 
 			Subscription subscription =
-				SubscriptionLocalServiceUtil.getSubscription(subscriptionId);
+				_subscriptionLocalService.getSubscription(subscriptionId);
 
 			if (themeDisplay.getUserId() != subscription.getUserId()) {
 				throw new PrincipalException();
 			}
 
-			SubscriptionLocalServiceUtil.deleteSubscription(subscription);
+			_subscriptionLocalService.deleteSubscription(subscription);
 		}
 	}
+
+	@Reference
+	private SubscriptionLocalService _subscriptionLocalService;
 
 }
