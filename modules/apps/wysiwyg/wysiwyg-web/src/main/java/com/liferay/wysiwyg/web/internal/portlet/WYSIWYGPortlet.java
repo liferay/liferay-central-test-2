76864f5ff856c71ff27fd336985d8d41763b34c8
peterfellwock <peter.fellwock@liferay.com>
2016-11-18 11:42:37 -0800

    LPS-68072 xml to Portlet

---
+++ modules/apps/wysiwyg/wysiwyg-web/src/main/java/com/liferay/wysiwyg/web/internal/portlet/WYSIWYGPortlet.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wysiwyg.web.internal.portlet;
+
+import com.liferay.portal.kernel.model.Release;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.wysiwyg.web.internal.constants.WYSIWYGPortletKeys;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Peter Fellwock
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.css-class-wrapper=wysiwyg-portlet",
+		"com.liferay.portlet.display-category=category.cms",
+		"com.liferay.portlet.instanceable=true",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=WYSIWYG",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + WYSIWYGPortletKeys.WYSIWYG,
+		"javax.portlet.portlet-info.keywords=WYSIWYG",
+		"javax.portlet.portlet-info.short-title=WYSIWYG",
+		"javax.portlet.portlet-info.title=WYSIWYG",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator,guest,power-user,user",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
+public class WYSIWYGPortlet extends MVCPortlet {
+
+	@Reference(
+		target = "(&(release.bundle.symbolic.name=com.liferay.wysiwyg.web)(release.schema.version=1.0.0))",
+		unbind = "-"
+	)
+	protected void setRelease(Release release) {
+	}
+
+}
