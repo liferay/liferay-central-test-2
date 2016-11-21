sergiogonzalez/liferay-portal

LPS-68589 Apply new subscription sender localize capabilities where needed

portal-impl/src/com/liferay/portal/service/impl/MembershipRequestLocalServiceImpl.java
@@ -336,11 +336,17 @@ public class MembershipRequestLocalServiceImpl
 			"[$COMMENTS$]", membershipRequest.getComments(),
 			"[$REPLY_COMMENTS$]", membershipRequest.getReplyComments(),
 			"[$REQUEST_USER_ADDRESS$]", requestUser.getEmailAddress(),
-			"[$REQUEST_USER_NAME$]", requestUser.getFullName(), "[$STATUS$]",
-			LanguageUtil.get(user.getLocale(), statusKey), "[$USER_ADDRESS$]",
-			user.getEmailAddress(), "[$USER_NAME$]", user.getFullName());
+			"[$REQUEST_USER_NAME$]", requestUser.getFullName(),
+			"[$USER_ADDRESS$]", user.getEmailAddress(), "[$USER_NAME$]",
+			user.getFullName());
 		subscriptionSender.setFrom(fromAddress, fromName);
 		subscriptionSender.setHtmlFormat(true);
+
+		String finalStatusKey = statusKey;
+
+		subscriptionSender.setLocalizedContextAttribute(
+			"[$STATUS$]", locale -> LanguageUtil.get(locale, finalStatusKey));
+
 		subscriptionSender.setMailId(
 			"membership_request", membershipRequest.getMembershipRequestId());
 		subscriptionSender.setScopeGroupId(membershipRequest.getGroupId());

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portal/service/impl/MembershipRequestLocalServiceImpl.java
@@ -312,7 +312,7 @@ public class MembershipRequestLocalServiceImpl
 		String body = PrefsPropsUtil.getContent(
 			membershipRequest.getCompanyId(), bodyProperty);
 
-		String statusKey = null;
+		final String statusKey;
 
 		if (membershipRequest.getStatusId() ==
 				MembershipRequestConstants.STATUS_APPROVED) {
@@ -336,11 +336,15 @@ public class MembershipRequestLocalServiceImpl
 			"[$COMMENTS$]", membershipRequest.getComments(),
 			"[$REPLY_COMMENTS$]", membershipRequest.getReplyComments(),
 			"[$REQUEST_USER_ADDRESS$]", requestUser.getEmailAddress(),
-			"[$REQUEST_USER_NAME$]", requestUser.getFullName(), "[$STATUS$]",
-			LanguageUtil.get(user.getLocale(), statusKey), "[$USER_ADDRESS$]",
-			user.getEmailAddress(), "[$USER_NAME$]", user.getFullName());
+			"[$REQUEST_USER_NAME$]", requestUser.getFullName(),
+			"[$USER_ADDRESS$]", user.getEmailAddress(), "[$USER_NAME$]",
+			user.getFullName());
 		subscriptionSender.setFrom(fromAddress, fromName);
 		subscriptionSender.setHtmlFormat(true);
+
+		subscriptionSender.setLocalizedContextAttribute(
+			"[$STATUS$]", locale -> LanguageUtil.get(locale, statusKey));
+
 		subscriptionSender.setMailId(
 			"membership_request", membershipRequest.getMembershipRequestId());
 		subscriptionSender.setScopeGroupId(membershipRequest.getGroupId());