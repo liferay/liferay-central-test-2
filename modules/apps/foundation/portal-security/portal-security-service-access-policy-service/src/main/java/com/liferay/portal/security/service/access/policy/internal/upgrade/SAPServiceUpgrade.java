7b800558fe32bc6dc873d8f640e5b05f8f6f5fae
Marcellus Tavares <marcellus.tavares@liferay.com>
2016-11-22 12:20:06 -0300

    LPS-68893 Create upgrade process for SAP service module

---
+++ modules/apps/foundation/portal-security/portal-security-service-access-policy-service/src/main/java/com/liferay/portal/security/service/access/policy/internal/upgrade/SAPServiceUpgrade.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.service.access.policy.internal.upgrade;
+
+import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marcellus Tavares
+ */
+@Component(immediate = true, service = UpgradeStepRegistrator.class)
+public class SAPServiceUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"com.liferay.portal.security.service.access.policy.service",
+			"2.0.0", "2.0.10", new DummyUpgradeStep());
+
+		registry.register(
+			"com.liferay.portal.security.service.access.policy.service",
+			"2.0.2", "2.0.10", new DummyUpgradeStep());
+
+		registry.register(
+			"com.liferay.portal.security.service.access.policy.service",
+			"2.0.4", "2.0.10", new DummyUpgradeStep());
+
+		registry.register(
+			"com.liferay.portal.security.service.access.policy.service",
+			"2.0.6", "2.0.10", new DummyUpgradeStep());
+
+		registry.register(
+			"com.liferay.portal.security.service.access.policy.service",
+			"2.0.8", "2.0.10", new DummyUpgradeStep());
+	}
+
+}
