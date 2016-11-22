24866c03445ecaf06fbde7374bdb3eb5079a7ffb
Michael Han <michael.han@liferay.com>
2016-11-22 10:21:43 -0600

    LPS-68893 set this to next release version 2.0.13 to make sure we don't have any missed versions

--- modules/apps/foundation/portal-security/portal-security-service-access-policy-service/src/main/java/com/liferay/portal/security/service/access/policy/internal/upgrade/SAPServiceUpgrade.java
+++ modules/apps/foundation/portal-security/portal-security-service-access-policy-service/src/main/java/com/liferay/portal/security/service/access/policy/internal/upgrade/SAPServiceUpgrade.java
@@ -29,23 +29,55 @@ public class SAPServiceUpgrade implements UpgradeStepRegistrator {
 	public void register(Registry registry) {
 		registry.register(
 			"com.liferay.portal.security.service.access.policy.service",
-			"2.0.0", "2.0.10", new DummyUpgradeStep());
+			"2.0.0", "2.0.13", new DummyUpgradeStep());
 
 		registry.register(
 			"com.liferay.portal.security.service.access.policy.service",
-			"2.0.2", "2.0.10", new DummyUpgradeStep());
+			"2.0.1", "2.0.13", new DummyUpgradeStep());
 
 		registry.register(
 			"com.liferay.portal.security.service.access.policy.service",
-			"2.0.4", "2.0.10", new DummyUpgradeStep());
+			"2.0.2", "2.0.13", new DummyUpgradeStep());
 
 		registry.register(
 			"com.liferay.portal.security.service.access.policy.service",
-			"2.0.6", "2.0.10", new DummyUpgradeStep());
+			"2.0.3", "2.0.13", new DummyUpgradeStep());
 
 		registry.register(
 			"com.liferay.portal.security.service.access.policy.service",
-			"2.0.8", "2.0.10", new DummyUpgradeStep());
+			"2.0.4", "2.0.13", new DummyUpgradeStep());
+
+		registry.register(
+			"com.liferay.portal.security.service.access.policy.service",
+			"2.0.5", "2.0.13", new DummyUpgradeStep());
+
+		registry.register(
+			"com.liferay.portal.security.service.access.policy.service",
+			"2.0.6", "2.0.13", new DummyUpgradeStep());
+
+		registry.register(
+			"com.liferay.portal.security.service.access.policy.service",
+			"2.0.7", "2.0.13", new DummyUpgradeStep());
+
+		registry.register(
+			"com.liferay.portal.security.service.access.policy.service",
+			"2.0.8", "2.0.13", new DummyUpgradeStep());
+
+		registry.register(
+			"com.liferay.portal.security.service.access.policy.service",
+			"2.0.9", "2.0.13", new DummyUpgradeStep());
+
+		registry.register(
+			"com.liferay.portal.security.service.access.policy.service",
+			"2.0.10", "2.0.13", new DummyUpgradeStep());
+
+		registry.register(
+			"com.liferay.portal.security.service.access.policy.service",
+			"2.0.11", "2.0.13", new DummyUpgradeStep());
+
+		registry.register(
+			"com.liferay.portal.security.service.access.policy.service",
+			"2.0.12", "2.0.13", new DummyUpgradeStep());
 	}
 
 }
