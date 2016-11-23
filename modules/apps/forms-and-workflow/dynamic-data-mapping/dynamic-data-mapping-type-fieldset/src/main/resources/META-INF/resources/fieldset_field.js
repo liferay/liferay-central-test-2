9e016e73f68d49b1afc15c6c1c714c0d2ae4c761
Marcellus Tavares <marcellus.tavares@liferay.com>
2016-11-23 18:24:40 -0300

    LPS-68651 Update soy and js file

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-fieldset/src/main/resources/META-INF/resources/fieldset_field.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-fieldset/src/main/resources/META-INF/resources/fieldset_field.js
@@ -1,6 +1,24 @@
 AUI.add(
 	'liferay-ddm-form-field-fieldset',
 	function(A) {
+		var FieldSetField = A.Component.create(
+			{
+				ATTRS: {
+					type: {
+						value: 'fieldset'
+					}
+				},
+
+				EXTENDS: Liferay.DDM.Renderer.Field,
+
+				NAME: 'liferay-ddm-form-field-fieldset',
+
+				prototype: {
+				}
+			}
+		);
+
+		Liferay.namespace('DDM.Field').FieldSet = FieldSetField;
 	},
 	'',
 	{
