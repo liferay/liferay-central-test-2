bc4d5b8759649f4f4c89ba56f3f4e8938e3bf94e
Chema Balsas <jose.balsas@liferay.com>
2016-11-24 13:20:08 +0100

    LPS-69147 Add frontend infrastructure for tests, format, lint...

--- modules/apps/frontend-theme-1975-london/frontend-theme-1975-london/gulpfile.js
+++ modules/apps/frontend-theme-1975-london/frontend-theme-1975-london/gulpfile.js
@@ -1,7 +1,17 @@
 'use strict';
 
 var gulp = require('gulp');
+var metal = require('gulp-metal');
 var liferayThemeTasks = require('liferay-theme-tasks');
+
+var srcBlob = 'src/**/*.es.js';
+var testBlob = 'test/**/*.es.js';
+
+metal.registerTasks({
+	buildSrc: [srcBlob],
+	formatGlobs: [srcBlob, testBlob],
+	lintGlobs: [srcBlob, testBlob]
+});
 
 liferayThemeTasks.registerTasks(
 	{
