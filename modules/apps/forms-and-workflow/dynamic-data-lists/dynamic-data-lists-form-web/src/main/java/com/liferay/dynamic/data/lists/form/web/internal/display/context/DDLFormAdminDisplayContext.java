0538580622cbe1d16e59e2505bf5fec636f657e6
Inacio Nery <inacio.nery@liferay.com>
2016-11-24 11:07:48 -0300

    LPS-69214 Change the Form's list default order to Create date, desc

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/internal/display/context/DDLFormAdminDisplayContext.java
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/internal/display/context/DDLFormAdminDisplayContext.java
@@ -252,12 +252,11 @@ public class DDLFormAdminDisplayContext {
 	}
 
 	public String getOrderByCol() {
-		return ParamUtil.getString(
-			_renderRequest, "orderByCol", "modified-date");
+		return ParamUtil.getString(_renderRequest, "orderByCol", "create-date");
 	}
 
 	public String getOrderByType() {
-		return ParamUtil.getString(_renderRequest, "orderByType", "asc");
+		return ParamUtil.getString(_renderRequest, "orderByType", "desc");
 	}
 
 	public PortletURL getPortletURL() {
