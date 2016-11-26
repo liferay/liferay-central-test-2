7efd355b5b4433fdef45b3a3faa059e60b878194
Brian Chan <brian.chan@liferay.com>
2016-11-25 19:19:04 -0800

    artifact:ignore frontend-image-editor-capability-brightness 1.0.6 prep next

--- modules/apps/foundation/frontend-image-editor/frontend-image-editor-capability-brightness/.task-cache/resources/BrightnessControls.soy.js
+++ modules/apps/foundation/frontend-image-editor/frontend-image-editor-capability-brightness/.task-cache/resources/BrightnessControls.soy.js
@@ -1,4 +1,4 @@
-define("frontend-image-editor-capability-brightness@1.0.6/BrightnessControls.soy", ['exports', 'metal-component/src/Component', 'metal-soy/src/Soy'], function (exports, _Component2, _Soy) {
+define("frontend-image-editor-capability-brightness@1.0.7/BrightnessControls.soy", ['exports', 'metal-component/src/Component', 'metal-soy/src/Soy'], function (exports, _Component2, _Soy) {
   'use strict';
 
   Object.defineProperty(exports, "__esModule", {
