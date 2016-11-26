a411c6d3683555155844d5e45061e849a0bfac8f
Brian Chan <brian.chan@liferay.com>
2016-11-25 19:21:23 -0800

    artifact:ignore frontend-image-editor-capability-rotate 1.0.6 prep next

--- modules/apps/foundation/frontend-image-editor/frontend-image-editor-capability-rotate/.task-cache/resources/RotateComponent.es.js
+++ modules/apps/foundation/frontend-image-editor/frontend-image-editor-capability-rotate/.task-cache/resources/RotateComponent.es.js
@@ -1,4 +1,4 @@
-define("frontend-image-editor-capability-rotate@1.0.6/RotateComponent.es", ['exports', 'metal-component/src/Component', 'metal-soy/src/Soy', 'metal/src/core', 'metal-promise/src/promise/Promise', './RotateComponent.soy', './RotateControls.soy'], function (exports, _Component2, _Soy, _core, _Promise, _RotateComponent, _RotateControls) {
+define("frontend-image-editor-capability-rotate@1.0.7/RotateComponent.es", ['exports', 'metal-component/src/Component', 'metal-soy/src/Soy', 'metal/src/core', 'metal-promise/src/promise/Promise', './RotateComponent.soy', './RotateControls.soy'], function (exports, _Component2, _Soy, _core, _Promise, _RotateComponent, _RotateControls) {
 	'use strict';
 
 	Object.defineProperty(exports, "__esModule", {
