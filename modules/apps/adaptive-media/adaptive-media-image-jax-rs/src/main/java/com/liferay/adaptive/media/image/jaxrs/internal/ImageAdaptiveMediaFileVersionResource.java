e30d4bb1717d4e15d0562052d65c7c66b8dbc493
Alejandro Hernández <info@alejandrohdezma.com>
2016-11-28 10:59:30 +0100

    LPS-29239 Create endpoint for getting adap. media content for a query

--- modules/apps/adaptive-media/adaptive-media-image-jax-rs/src/main/java/com/liferay/adaptive/media/image/jaxrs/internal/ImageAdaptiveMediaFileVersionResource.java
+++ modules/apps/adaptive-media/adaptive-media-image-jax-rs/src/main/java/com/liferay/adaptive/media/image/jaxrs/internal/ImageAdaptiveMediaFileVersionResource.java
@@ -1,20 +1,30 @@
 package com.liferay.adaptive.media.image.jaxrs.internal;
 
 import com.liferay.adaptive.media.AdaptiveMedia;
+import com.liferay.adaptive.media.AdaptiveMediaAttribute;
 import com.liferay.adaptive.media.AdaptiveMediaException;
+import com.liferay.adaptive.media.image.configuration.ImageAdaptiveMediaConfigurationHelper;
 import com.liferay.adaptive.media.image.finder.ImageAdaptiveMediaFinder;
+import com.liferay.adaptive.media.image.finder.ImageAdaptiveMediaQueryBuilder;
+import com.liferay.adaptive.media.image.jaxrs.internal.provider.AdaptiveMediaApiQuery;
+import com.liferay.adaptive.media.image.processor.ImageAdaptiveMediaAttribute;
 import com.liferay.adaptive.media.image.processor.ImageAdaptiveMediaProcessor;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.repository.model.FileVersion;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
 /**
@@ -43,6 +53,44 @@ public class ImageAdaptiveMediaFileVersionResource {
 		return _getFirstAdaptiveMedia(stream);
 	}
 
+	@GET
+	@Path("/data")
+	@Produces("image")
+	public Response getData(@Context AdaptiveMediaApiQuery query)
+		throws AdaptiveMediaException, PortalException {
+
+		List<AdaptiveMediaApiQuery.QueryAttribute> queryList = query.select(
+			_allowedAttributes);
+
+		if (queryList.isEmpty()) {
+			throw new BadRequestException("You must provide a valid query");
+		}
+
+		Stream<AdaptiveMedia<ImageAdaptiveMediaProcessor>> stream =
+			_getAdaptiveMediaStream(queryList);
+
+		return _getFirstAdaptiveMedia(stream);
+	}
+
+	private Stream<AdaptiveMedia<ImageAdaptiveMediaProcessor>>
+			_getAdaptiveMediaStream(
+				List<AdaptiveMediaApiQuery.QueryAttribute> query)
+		throws AdaptiveMediaException, PortalException {
+
+		return _finder.getAdaptiveMedia(
+			queryBuilder -> {
+				ImageAdaptiveMediaQueryBuilder.InitialStep step =
+					queryBuilder.forVersion(_fileVersion);
+
+				query.forEach(
+					a -> step.with(
+						(AdaptiveMediaAttribute)a.getAttribute(),
+						a.getValue()));
+
+				return step.done();
+			});
+	}
+
 	private Response _getFirstAdaptiveMedia(
 		Stream<AdaptiveMedia<ImageAdaptiveMediaProcessor>> stream) {
 
@@ -55,6 +103,15 @@ public class ImageAdaptiveMediaFileVersionResource {
 
 		return Response.status(200).type(_fileVersion.getMimeType()).entity(
 			adaptiveMedia.get().getInputStream()).build();
+	}
+
+	private static final Map<String, AdaptiveMediaAttribute<?, ?>>
+		_allowedAttributes = new HashMap<>();
+
+	static {
+		_allowedAttributes.putAll(
+			ImageAdaptiveMediaAttribute.allowedAttributes());
+		_allowedAttributes.putAll(AdaptiveMediaAttribute.allowedAttributes());
 	}
 
 	private final FileVersion _fileVersion;
