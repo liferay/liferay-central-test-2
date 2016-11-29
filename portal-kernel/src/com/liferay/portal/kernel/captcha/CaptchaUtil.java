8eca011401499dea1af02674e9bfc9791833dae2
Pei-Jung Lan <pei-jung.lan@liferay.com>
2016-11-28 17:29:17 -0800

    LPS-69383 Use ServiceTrackerMap to track Captcha in CaptchaUtil

--- portal-kernel/src/com/liferay/portal/kernel/captcha/CaptchaUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/captcha/CaptchaUtil.java
@@ -15,9 +15,15 @@
 package com.liferay.portal.kernel.captcha;
 
 import com.liferay.portal.kernel.security.pacl.permission.PortalRuntimePermission;
+import com.liferay.portal.kernel.util.PrefsPropsUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.registry.collections.ServiceTrackerCollections;
+import com.liferay.registry.collections.ServiceTrackerMap;
 
 import java.io.IOException;
 
+import javax.portlet.PortletPreferences;
 import javax.portlet.PortletRequest;
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
@@ -45,7 +51,14 @@ public class CaptchaUtil {
 	public static Captcha getCaptcha() {
 		PortalRuntimePermission.checkGetBeanProperty(CaptchaUtil.class);
 
-		return _captcha;
+		if (_serviceTrackerMap == null) {
+			return null;
+		}
+
+		String captchaClassName = PrefsPropsUtil.getString(
+			PropsKeys.CAPTCHA_ENGINE_IMPL, _CAPTCHA_ENGINE_IMPL);
+
+		return _serviceTrackerMap.getService(captchaClassName);
 	}
 
 	public static String getTaglibPath() {
@@ -74,12 +87,24 @@ public class CaptchaUtil {
 		getCaptcha().serveImage(resourceRequest, resourceResponse);
 	}
 
-	public void setCaptcha(Captcha captcha) {
+	public void setCaptcha(Captcha captcha) throws Exception {
 		PortalRuntimePermission.checkSetBeanProperty(getClass());
 
-		_captcha = captcha;
+		PortletPreferences portletPreferences = PrefsPropsUtil.getPreferences();
+
+		Class<?> clazz = captcha.getClass();
+
+		portletPreferences.setValue(
+			PropsKeys.CAPTCHA_ENGINE_IMPL, clazz.getName());
+
+		portletPreferences.store();
 	}
 
-	private static Captcha _captcha;
+	private static final String _CAPTCHA_ENGINE_IMPL = PropsUtil.get(
+		PropsKeys.CAPTCHA_ENGINE_IMPL);
+
+	private static final ServiceTrackerMap<String, Captcha> _serviceTrackerMap =
+		ServiceTrackerCollections.openSingleValueMap(
+			Captcha.class, "captcha.engine.impl");
 
 }
