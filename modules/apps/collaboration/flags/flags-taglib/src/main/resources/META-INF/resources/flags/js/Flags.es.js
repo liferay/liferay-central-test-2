93b5191f7f63f51b343d7457ea15d019dc94c0c5
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2016-11-29 13:30:49 +0100

    LPS-69436 Create metal component for Flags

---
+++ modules/apps/collaboration/flags/flags-taglib/src/main/resources/META-INF/resources/flags/js/Flags.es.js
@@ -0,0 +1,81 @@
+import core from 'metal/src/core';
+import Component from 'metal-component/src/Component';
+
+import Soy from 'metal-soy/src/Soy';
+
+import templates from './Flags.soy';
+
+class FlagsPortlet extends Component {
+	/**
+	 * @inheritDoc
+	 */
+	created() {
+		this.message = Liferay.Language.get(this.message);
+	}
+
+	openReportDialog() {
+		AUI().use('aui-io-plugin-deprecated', (A) => {
+			let dialogTitle = Liferay.Language.get('report-inappropriate-content');
+
+			if (this.signedUser) {
+				let popup = Liferay.Util.Window.getWindow(
+					{
+						dialog: {
+							destroyOnHide: true,
+							height: 680,
+							width: 680
+						},
+						title: dialogTitle
+					}
+				);
+
+				popup.plug(
+					A.Plugin.IO, {
+						data: this.data,
+						uri: this.uri
+					}
+				);
+			}
+			else {
+				Liferay.Util.Window.getWindow(
+					{
+						dialog: {
+							bodyContent: Liferay.Language.get('please-sign-in-to-flag-this-as-inappropriate'),
+							destroyOnHide: true,
+							height: 680,
+							width: 680
+						},
+						title: dialogTitle
+					}
+				);
+			}
+		});
+	}
+};
+
+FlagsPortlet.STATE = {
+	data: {
+		validator: core.isObject
+	},
+	inTrash: {
+		validator: core.isBoolean
+	},
+	label: {
+		validator: core.isBoolean,
+		value: true
+	},
+	message: {
+		validator: core.isString
+	},
+	signedUser: {
+		validator: core.isBoolean
+	},
+	uri: {
+		validator: core.isString
+	}
+};
+
+// Register component
+Soy.register(FlagsPortlet, templates);
+
+export default FlagsPortlet;
--- modules/apps/collaboration/flags/flags-taglib/src/main/resources/META-INF/resources/flags/page.jsp
+++ modules/apps/collaboration/flags/flags-taglib/src/main/resources/META-INF/resources/flags/page.jsp
@@ -119,3 +119,31 @@ if (enabled) {
 		</c:otherwise>
 	</c:choose>
 </c:if>
+
+<div class="taglib-flags" id="<portlet:namespace />taglibFlag" title="<liferay-ui:message key='<%= !TrashUtil.isInTrash(className, classPK) ? message : "flags-are-disabled-because-this-entry-is-in-the-recycle-bin" %>' />">
+</div>
+
+<aui:script require="flags-taglib/flags/js/Flags.es">
+	new flagsTaglibFlagsJsFlagsEs.default(
+		{
+			cssClass: '<%= cssClass %>',
+			data: Liferay.Util.ns(
+				'<%= PortalUtil.getPortletNamespace(PortletKeys.FLAGS) %>',
+				{
+					className: '<%= className %>',
+					classPK: '<%= classPK %>',
+					contentTitle: '<%= HtmlUtil.escapeJS(contentTitle) %>',
+					contentURL: '<%= HtmlUtil.escapeJS(PortalUtil.getPortalURL(request) + currentURL) %>',
+					reportedUserId: '<%= reportedUserId %>'
+				}
+			),
+			inTrash: <%= TrashUtil.isInTrash(className, classPK) %>,
+			label: <%= label %>,
+			message: '<%= message %>',
+			pathThemeImages: '<%= themeDisplay.getPathThemeImages() %>',
+			signedUser: <%= flagsGroupServiceConfiguration.guestUsersEnabled() || themeDisplay.isSignedIn() %>,
+			uri: '<liferay-portlet:renderURL portletName="<%= PortletKeys.FLAGS %>" windowState="<%= LiferayWindowState.EXCLUSIVE.toString() %>"><portlet:param name="mvcRenderCommandName" value="/flags/edit_entry" /></liferay-portlet:renderURL>'
+		},
+		'#<%= namespace %>taglibFlag'
+	);
+</aui:script>
