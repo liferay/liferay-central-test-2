a79b47548b7a8e5805574cbb971c861c0aa0c632
Adolfo Pérez <adolfo.perez@liferay.com>
2016-11-30 10:16:04 +0100

    LPS-69371 Create classes to handle templates

---
+++ portal-kernel/src/com/liferay/portal/kernel/mail/DefaultMailTemplate.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.mail;
+
+import com.liferay.portal.kernel.io.unsync.UnsyncStringWriter;
+import com.liferay.portal.kernel.util.EscapableLocalizableFunction;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class DefaultMailTemplate implements MailTemplate {
+
+	public DefaultMailTemplate(String template, boolean escapeHtml) {
+		_template = template;
+		_escapeHtml = escapeHtml;
+	}
+
+	@Override
+	public void render(
+			Writer writer, Locale locale,
+			MailTemplateContext mailTemplateContext)
+		throws IOException {
+
+		Map<String, EscapableLocalizableFunction> replacements =
+			mailTemplateContext.getReplacements();
+
+		String content = _template;
+
+		for (Map.Entry<String, EscapableLocalizableFunction> replacement :
+				replacements.entrySet()) {
+
+			EscapableLocalizableFunction value = replacement.getValue();
+
+			final String valueString;
+
+			if (_escapeHtml) {
+				valueString = value.getEscapedValue(locale);
+			}
+			else {
+				valueString = value.getOriginalValue(locale);
+			}
+
+			content = StringUtil.replace(
+				content, replacement.getKey(), valueString);
+		}
+
+		EscapableLocalizableFunction escapableLocalizableFunction =
+			replacements.get("[$PORTAL_URL$]");
+
+		String portalURL = escapableLocalizableFunction.getOriginalValue(
+			locale);
+
+		if (Validator.isNotNull(portalURL)) {
+			content = StringUtil.replace(
+				content, new String[] {"href=\"/", "src=\"/"},
+				new String[] {
+					"href=\"" + portalURL + "/", "src=\"" + portalURL + "/"
+				});
+		}
+
+		writer.append(content);
+	}
+
+	@Override
+	public String renderAsString(
+			Locale locale, MailTemplateContext mailTemplateContext)
+		throws IOException {
+
+		UnsyncStringWriter writer = new UnsyncStringWriter();
+
+		render(writer, locale, mailTemplateContext);
+
+		return writer.toString();
+	}
+
+	private final boolean _escapeHtml;
+	private final String _template;
+
+}
---
+++ portal-kernel/src/com/liferay/portal/kernel/mail/DefaultMailTemplateContextBuilder.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.mail;
+
+import com.liferay.portal.kernel.util.EscapableLocalizableFunction;
+import com.liferay.portal.kernel.util.EscapableObject;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class DefaultMailTemplateContextBuilder
+	implements MailTemplateContextBuilder {
+
+	@Override
+	public MailTemplateContext build() {
+		return new MailTemplateContextImpl(_replacements);
+	}
+
+	@Override
+	public MailTemplateContextBuilder put(
+		String placeholder,
+		EscapableLocalizableFunction escapableLocalizableFunction) {
+
+		_replacements.put(placeholder, escapableLocalizableFunction);
+
+		return this;
+	}
+
+	@Override
+	public MailTemplateContextBuilder put(
+		String placeholder, EscapableObject<String> escapableObject) {
+
+		put(
+			placeholder,
+			new EscapableLocalizableFunction(
+				locale -> escapableObject.getOriginalValue()));
+
+		return this;
+	}
+
+	@Override
+	public MailTemplateContextBuilder put(String placeholder, String value) {
+		put(placeholder, new EscapableObject<>(value, false));
+
+		return this;
+	}
+
+	private final Map<String, EscapableLocalizableFunction> _replacements =
+		new HashMap<>();
+
+	private static class MailTemplateContextImpl
+		implements MailTemplateContext {
+
+		public MailTemplateContextImpl(
+			Map<String, EscapableLocalizableFunction> placeholders) {
+
+			_placeholders = new HashMap<>(placeholders);
+		}
+
+		@Override
+		public Map<String, EscapableLocalizableFunction> getReplacements() {
+			return Collections.unmodifiableMap(_placeholders);
+		}
+
+		private final Map<String, EscapableLocalizableFunction> _placeholders;
+
+	}
+
+}
