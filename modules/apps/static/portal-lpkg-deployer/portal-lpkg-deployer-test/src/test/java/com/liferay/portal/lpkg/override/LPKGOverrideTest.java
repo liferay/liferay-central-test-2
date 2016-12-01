7cc7d721f9b4633f04be71338103a8e3b62cad3e
shuyangzhou <shuyang.zhou@liferay.com>
2016-12-01 15:36:24 -0800

    LPS-68202 Simplify

--- modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer-test/src/test/java/com/liferay/portal/lpkg/override/LPKGOverrideTest.java
+++ modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer-test/src/test/java/com/liferay/portal/lpkg/override/LPKGOverrideTest.java
@@ -18,6 +18,7 @@ import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayOutputStream;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.util.FileImpl;
 
 import java.io.File;
 import java.io.IOException;
@@ -65,18 +66,8 @@ public class LPKGOverrideTest {
 
 		File file = new File(liferayHome, "/osgi/marketplace/override");
 
-		if (file.exists()) {
-			String[] files = file.list();
-
-			for (String childPath : files) {
-				File childFile = new File(file.getPath(), childPath);
-
-				childFile.delete();
-			}
-		}
-		else {
-			file.mkdir();
-		}
+		_fileImpl.deltree(file);
+		_fileImpl.mkdirs(file);
 
 		Map<String, String> overrides = new HashMap<>();
 
@@ -224,6 +215,7 @@ public class LPKGOverrideTest {
 		}
 	}
 
+	private static final FileImpl _fileImpl = FileImpl.getInstance();
 	private static final Pattern _pattern = Pattern.compile(
 		"(.*?)(-\\d+\\.\\d+\\.\\d+)(\\..+)?(\\.[jw]ar)");
 
