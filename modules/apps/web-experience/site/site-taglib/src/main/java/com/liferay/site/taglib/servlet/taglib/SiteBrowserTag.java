512e4b5c2480d5706ce2adc78aba4b289149c158
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-12-05 12:07:16 +0100

    LPS-69709 Creates new taglib for browse sites

---
+++ modules/apps/web-experience/site/site-taglib/src/main/java/com/liferay/site/taglib/servlet/taglib/SiteBrowserTag.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.taglib.servlet.taglib;
+
+import com.liferay.site.taglib.internal.servlet.ServletContextUtil;
+import com.liferay.taglib.util.IncludeTag;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class SiteBrowserTag extends IncludeTag {
+
+	@Override
+	public void setPageContext(PageContext pageContext) {
+		super.setPageContext(pageContext);
+
+		servletContext = ServletContextUtil.getServletContext();
+	}
+
+	@Override
+	protected void cleanUp() {
+	}
+
+	@Override
+	protected String getPage() {
+		return _PAGE;
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest request) {
+	}
+
+	private static final String _PAGE = "/site_browser/page.jsp";
+
+}
