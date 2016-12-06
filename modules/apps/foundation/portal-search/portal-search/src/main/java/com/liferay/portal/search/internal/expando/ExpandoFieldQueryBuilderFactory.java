712de0cdac1e036a144448ca785d2214fb9c153c
Bryan Engler <bryan.engler@liferay.com>
2016-12-06 14:55:23 -0800

    LPS-69574 portal-search: Add expando_keyword to substring fields
    https://issues.liferay.com/browse/LPS-69574

---
+++ modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/expando/ExpandoFieldQueryBuilderFactory.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.internal.expando;
+
+import com.liferay.portal.search.analysis.FieldQueryBuilder;
+import com.liferay.portal.search.analysis.FieldQueryBuilderFactory;
+import com.liferay.portal.search.internal.analysis.SubstringFieldQueryBuilder;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author André de Oliveira
+ */
+@Component(immediate = true, service = FieldQueryBuilderFactory.class)
+public class ExpandoFieldQueryBuilderFactory
+	implements FieldQueryBuilderFactory {
+
+	@Override
+	public FieldQueryBuilder getQueryBuilder(String field) {
+		if (field.startsWith("expando__keyword__")) {
+			return substringQueryBuilder;
+		}
+
+		return null;
+	}
+
+	@Reference
+	protected SubstringFieldQueryBuilder substringQueryBuilder;
+
+}
