a8a276637b6e7fae64e99fb60e9fe92ebb468c71
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-12-07 13:31:42 +0100

    LPS-69709 Adds parameters to site browser taglib

--- modules/apps/web-experience/site/site-taglib/src/main/java/com/liferay/site/taglib/servlet/taglib/SiteBrowserTag.java
+++ modules/apps/web-experience/site/site-taglib/src/main/java/com/liferay/site/taglib/servlet/taglib/SiteBrowserTag.java
@@ -14,8 +14,15 @@
 
 package com.liferay.site.taglib.servlet.taglib;
 
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.site.taglib.internal.servlet.ServletContextUtil;
 import com.liferay.taglib.util.IncludeTag;
+
+import java.util.List;
+
+import javax.portlet.PortletURL;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.PageContext;
@@ -25,6 +32,26 @@ import javax.servlet.jsp.PageContext;
  */
 public class SiteBrowserTag extends IncludeTag {
 
+	public void setDisplayStyle(String displayStyle) {
+		_displayStyle = displayStyle;
+	}
+
+	public void setEmptyResultsMessage(String emptyResultsMessage) {
+		_emptyResultsMessage = emptyResultsMessage;
+	}
+
+	public void setEventName(String eventName) {
+		_eventName = eventName;
+	}
+
+	public void setGroups(List<Group> groups) {
+		_groups = groups;
+	}
+
+	public void setGroupsCount(int groupsCount) {
+		_groupsCount = groupsCount;
+	}
+
 	@Override
 	public void setPageContext(PageContext pageContext) {
 		super.setPageContext(pageContext);
@@ -32,8 +59,18 @@ public class SiteBrowserTag extends IncludeTag {
 		servletContext = ServletContextUtil.getServletContext();
 	}
 
+	public void setPortletURL(PortletURL portletURL) {
+		_portletURL = portletURL;
+	}
+
 	@Override
 	protected void cleanUp() {
+		_displayStyle = "icon";
+		_emptyResultsMessage = null;
+		_eventName = null;
+		_groups = null;
+		_groupsCount = 0;
+		_portletURL = null;
 	}
 
 	@Override
@@ -43,8 +80,34 @@ public class SiteBrowserTag extends IncludeTag {
 
 	@Override
 	protected void setAttributes(HttpServletRequest request) {
+		request.setAttribute(
+			"liferay-site:site-browser:displayStyle", _displayStyle);
+		request.setAttribute(
+			"liferay-site:site-browser:emptyResultsMessage",
+			_getEmptyResultsMessage(request));
+		request.setAttribute("liferay-site:site-browser:eventName", _eventName);
+		request.setAttribute("liferay-site:site-browser:groups", _groups);
+		request.setAttribute(
+			"liferay-site:site-browser:groupsCount", _groupsCount);
+		request.setAttribute(
+			"liferay-site:site-browser:portletURL", _portletURL);
+	}
+
+	private String _getEmptyResultsMessage(HttpServletRequest request) {
+		if (Validator.isNotNull(_emptyResultsMessage)) {
+			return _emptyResultsMessage;
+		}
+
+		return LanguageUtil.get(request, "no-results-were-found");
 	}
 
 	private static final String _PAGE = "/site_browser/page.jsp";
+
+	private String _displayStyle = "icon";
+	private String _emptyResultsMessage;
+	private String _eventName;
+	private List<Group> _groups;
+	private int _groupsCount;
+	private PortletURL _portletURL;
 
 }
