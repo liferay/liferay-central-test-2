0b8dc625f6f14bd3c41f5125a4640874843f0be9
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2016-12-07 10:12:20 +0100

    LPS-68025 Set upload url for wiki page attachments

---
+++ modules/apps/collaboration/wiki/wiki-editor-configuration/src/main/java/com/liferay/wiki/editor/configuration/internal/WikiAttachmentEditorOptionsContributor.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wiki.editor.configuration.internal;
+
+
+import com.liferay.portal.kernel.editor.configuration.EditorOptions;
+import com.liferay.portal.kernel.editor.configuration.EditorOptionsContributor;
+import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
+import com.liferay.portal.kernel.theme.PortletDisplay;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.wiki.constants.WikiPortletKeys;
+import org.osgi.service.component.annotations.Component;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.PortletURL;
+import java.util.Map;
+
+/**
+ * @author Ambrin Chaudhary
+ */
+@Component(
+	property = {
+		"editor.config.key=contentEditor",
+		"javax.portlet.name=" + WikiPortletKeys.WIKI,
+		"javax.portlet.name=" + WikiPortletKeys.WIKI_ADMIN,
+		"javax.portlet.name=" + WikiPortletKeys.WIKI_DISPLAY,
+	},
+	service = EditorOptionsContributor.class
+)
+public class WikiAttachmentEditorOptionsContributor implements EditorOptionsContributor {
+
+	@Override
+	public void populateEditorOptions(
+		EditorOptions editorOptions,
+		Map<String, Object> inputEditorTaglibAttributes,
+		ThemeDisplay themeDisplay,
+		RequestBackedPortletURLFactory requestBackedPortletURLFactory) {
+
+		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+		if (Validator.isNull(portletDisplay.getId())) {
+			return;
+		}
+
+		PortletURL portletURL = requestBackedPortletURLFactory.createActionURL(
+			WikiPortletKeys.WIKI);
+
+		portletURL.setParameter(
+			ActionRequest.ACTION_NAME, "/wiki/upload_page_attachment");
+
+		Map<String, String> fileBrowserParamsMap =
+			(Map<String, String>)inputEditorTaglibAttributes.get(
+				"liferay-ui:input-editor:fileBrowserParams");
+
+		long wikiPageResourcePrimKey = 0;
+
+		if (fileBrowserParamsMap != null) {
+			wikiPageResourcePrimKey = GetterUtil.getLong(
+				fileBrowserParamsMap.get("wikiPageResourcePrimKey"));
+		}
+
+		if (wikiPageResourcePrimKey == 0) {
+			return;
+		}
+
+		portletURL.setParameter(
+			"resourcePrimKey",
+			String.valueOf(
+				wikiPageResourcePrimKey));
+
+		editorOptions.setUploadURL(portletURL.toString());
+	}
+}
