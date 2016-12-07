68e7ec388362a5ed2c51b865401b63e917cd4a21
Brian Chan <brian.chan@liferay.com>
2016-12-07 11:14:39 -0800

    LPS-69171 Not needed

--- portal-impl/src/com/liferay/portal/webserver/WebServerServlet.java
+++ portal-impl/src/com/liferay/portal/webserver/WebServerServlet.java
@@ -482,12 +482,6 @@ public class WebServerServlet extends HttpServlet {
 					if (!LayoutPermissionUtil.contains(
 							permissionChecker, layout, ActionKeys.VIEW)) {
 
-						if (_log.isWarnEnabled()) {
-							_log.warn(
-								"Error checking permissions for user " +
-									user.getUserId());
-						}
-
 						throw new PrincipalException.MustHavePermission(
 							permissionChecker, Layout.class.getName(),
 							layout.getPlid(), ActionKeys.VIEW);
@@ -514,12 +508,6 @@ public class WebServerServlet extends HttpServlet {
 					if (!GroupPermissionUtil.contains(
 							permissionChecker, layoutSet.getGroupId(),
 							ActionKeys.VIEW)) {
-
-						if (_log.isWarnEnabled()) {
-							_log.warn(
-								"Error checking permissions for user " +
-									user.getUserId());
-						}
 
 						throw new PrincipalException.MustHavePermission(
 							permissionChecker, LayoutSet.class.getName(),
