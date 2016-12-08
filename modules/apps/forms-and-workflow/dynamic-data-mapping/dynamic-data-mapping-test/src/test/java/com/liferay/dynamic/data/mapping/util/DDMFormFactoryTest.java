9caa29d121e8c4db12e695f6a269347a9ecc7a49
Marcellus Tavares <marcellus.tavares@liferay.com>
2016-12-08 18:42:35 -0300

    LPS-68642 Move integration tests to unit

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test/src/test/java/com/liferay/dynamic/data/mapping/util/DDMFormFactoryTest.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test/src/test/java/com/liferay/dynamic/data/mapping/util/DDMFormFactoryTest.java
@@ -14,20 +14,132 @@
 
 package com.liferay.dynamic.data.mapping.util;
 
+import static org.mockito.Mockito.when;
+
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTypeSettings;
+import com.liferay.dynamic.data.mapping.form.field.type.DefaultDDMFormFieldTypeSettings;
 import com.liferay.dynamic.data.mapping.model.DDMForm;
 import com.liferay.dynamic.data.mapping.model.DDMFormField;
 import com.liferay.dynamic.data.mapping.model.DDMFormRule;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.StringPool;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Matchers;
+import org.mockito.Mock;
+
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
  * @author Leonardo Barros
+ * @author Marcellus Tavares
  */
+@PrepareForTest(ResourceBundleUtil.class)
+@RunWith(PowerMockRunner.class)
 public class DDMFormFactoryTest {
+
+	@Before
+	public void setUp() {
+		setUpLanguageUtil();
+		setUpResourceBundleUtil();
+	}
+
+	@Test
+	public void testCreateDDMFormFieldTypeSettingsDDMForm() {
+		DDMForm ddmForm = DDMFormFactory.create(DDMFormFieldTypeSettings.class);
+
+		Assert.assertNotNull(ddmForm);
+
+		assertRequiredDDMFormFieldTypeSettings(
+			ddmForm.getDDMFormFieldsMap(false));
+	}
+
+	@Test
+	public void testCreateDefaultDDMFormFieldTypeSettingsDDMForm() {
+		DDMForm ddmForm = DDMFormFactory.create(
+			DefaultDDMFormFieldTypeSettings.class);
+
+		Assert.assertNotNull(ddmForm);
+
+		Map<String, DDMFormField> ddmFormFieldsMap =
+			ddmForm.getDDMFormFieldsMap(false);
+
+		assertRequiredDDMFormFieldTypeSettings(ddmFormFieldsMap);
+
+		DDMFormField indexTypeDDMFormField = ddmFormFieldsMap.get("indexType");
+
+		Assert.assertNotNull(indexTypeDDMFormField);
+		Assert.assertEquals("string", indexTypeDDMFormField.getDataType());
+		Assert.assertEquals("select", indexTypeDDMFormField.getType());
+
+		DDMFormField labelDDMFormField = ddmFormFieldsMap.get("label");
+
+		Assert.assertNotNull(labelDDMFormField);
+		Assert.assertEquals("string", labelDDMFormField.getDataType());
+		Assert.assertEquals("key_value", labelDDMFormField.getType());
+		Assert.assertEquals(true, labelDDMFormField.isLocalizable());
+
+		DDMFormField localizableDDMFormField = ddmFormFieldsMap.get(
+			"localizable");
+
+		Assert.assertNotNull(localizableDDMFormField);
+		Assert.assertEquals("boolean", localizableDDMFormField.getDataType());
+		Assert.assertEquals("checkbox", localizableDDMFormField.getType());
+
+		DDMFormField predefinedValueDDMFormField = ddmFormFieldsMap.get(
+			"predefinedValue");
+
+		Assert.assertNotNull(predefinedValueDDMFormField);
+		Assert.assertEquals(
+			"string", predefinedValueDDMFormField.getDataType());
+		Assert.assertEquals("text", predefinedValueDDMFormField.getType());
+		Assert.assertEquals(true, predefinedValueDDMFormField.isLocalizable());
+
+		DDMFormField readOnlyDDMFormField = ddmFormFieldsMap.get("readOnly");
+
+		Assert.assertNotNull(readOnlyDDMFormField);
+		Assert.assertEquals("boolean", readOnlyDDMFormField.getDataType());
+		Assert.assertEquals("checkbox", readOnlyDDMFormField.getType());
+
+		DDMFormField repeatableDDMFormField = ddmFormFieldsMap.get(
+			"repeatable");
+
+		Assert.assertNotNull(repeatableDDMFormField);
+		Assert.assertEquals("boolean", repeatableDDMFormField.getDataType());
+		Assert.assertEquals("checkbox", repeatableDDMFormField.getType());
+
+		DDMFormField requiredDDMFormField = ddmFormFieldsMap.get("readOnly");
+
+		Assert.assertNotNull(requiredDDMFormField);
+		Assert.assertEquals("boolean", requiredDDMFormField.getDataType());
+		Assert.assertEquals("checkbox", requiredDDMFormField.getType());
+
+		DDMFormField showLabelDDMFormField = ddmFormFieldsMap.get("showLabel");
+
+		Assert.assertNotNull(showLabelDDMFormField);
+		Assert.assertEquals("boolean", showLabelDDMFormField.getDataType());
+		Assert.assertEquals("checkbox", showLabelDDMFormField.getType());
+
+		DDMFormField tipDDMFormField = ddmFormFieldsMap.get("tip");
+
+		Assert.assertNotNull(tipDDMFormField);
+		Assert.assertEquals("string", tipDDMFormField.getDataType());
+		Assert.assertEquals("text", tipDDMFormField.getType());
+		Assert.assertEquals(true, tipDDMFormField.isLocalizable());
+	}
 
 	@Test
 	public void testCreateDynamicFormWithoutRules() {
@@ -98,6 +210,55 @@ public class DDMFormFactoryTest {
 		Assert.assertEquals("parameterValue", nestedDDMFormField2.getName());
 		Assert.assertEquals("text", nestedDDMFormField2.getType());
 	}
+
+	protected void assertRequiredDDMFormFieldTypeSettings(
+		Map<String, DDMFormField> ddmFormFieldsMap) {
+
+		DDMFormField dataTypeDDMFormField = ddmFormFieldsMap.get("dataType");
+
+		Assert.assertNotNull(dataTypeDDMFormField);
+		Assert.assertEquals("string", dataTypeDDMFormField.getDataType());
+		Assert.assertEquals("text", dataTypeDDMFormField.getType());
+		Assert.assertEquals(true, dataTypeDDMFormField.isRequired());
+		Assert.assertEquals(false, dataTypeDDMFormField.isLocalizable());
+
+		DDMFormField nameDDMFormField = ddmFormFieldsMap.get("name");
+
+		Assert.assertNotNull(nameDDMFormField);
+		Assert.assertEquals("string", nameDDMFormField.getDataType());
+		Assert.assertEquals("text", nameDDMFormField.getType());
+		Assert.assertEquals(true, nameDDMFormField.isRequired());
+		Assert.assertEquals(false, nameDDMFormField.isLocalizable());
+
+		DDMFormField typeDDMFormField = ddmFormFieldsMap.get("type");
+
+		Assert.assertNotNull(typeDDMFormField);
+		Assert.assertEquals("string", typeDDMFormField.getDataType());
+		Assert.assertEquals("text", typeDDMFormField.getType());
+		Assert.assertEquals(true, typeDDMFormField.isRequired());
+		Assert.assertEquals(false, typeDDMFormField.isLocalizable());
+	}
+
+	protected void setUpLanguageUtil() {
+		LanguageUtil languageUtil = new LanguageUtil();
+
+		languageUtil.setLanguage(_language);
+	}
+
+	protected void setUpResourceBundleUtil() {
+		mockStatic(ResourceBundleUtil.class);
+
+		when(
+			ResourceBundleUtil.getBundle(
+				Matchers.anyString(), Matchers.any(Locale.class),
+				Matchers.any(ClassLoader.class))
+		).thenReturn(
+			ResourceBundleUtil.EMPTY_RESOURCE_BUNDLE
+		);
+	}
+
+	@Mock
+	private Language _language;
 
 	@com.liferay.dynamic.data.mapping.annotations.DDMForm
 	private interface DynamicFormWithFieldSet {
