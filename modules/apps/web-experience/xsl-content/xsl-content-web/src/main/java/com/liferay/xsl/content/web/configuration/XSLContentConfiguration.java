c131e51f3dd5d7a9cd76fd2ddd8e02c5e99aafaa
Tina Tian <tina.tian@liferay.com>
2016-12-09 16:00:14 -0800

    LPS-69644 Fix XSLContentConfiguration, need "required = false"

--- modules/apps/web-experience/xsl-content/xsl-content-web/src/main/java/com/liferay/xsl/content/web/configuration/XSLContentConfiguration.java
+++ modules/apps/web-experience/xsl-content/xsl-content-web/src/main/java/com/liferay/xsl/content/web/configuration/XSLContentConfiguration.java
@@ -28,19 +28,33 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 )
 public interface XSLContentConfiguration {
 
-	@Meta.AD(deflt = "@portlet_context_url@", id = "valid.url.prefixes")
+	@Meta.AD(
+		deflt = "@portlet_context_url@", id = "valid.url.prefixes",
+		required = false
+	)
 	public String validUrlPrefixes();
 
-	@Meta.AD(deflt = "false", id = "xml.doctype.declaration.allowed")
+	@Meta.AD(
+		deflt = "false", id = "xml.doctype.declaration.allowed",
+		required = false
+	)
 	public boolean xmlDoctypeDeclarationAllowed();
 
-	@Meta.AD(deflt = "false", id = "xml.external.general.entities.allowed")
+	@Meta.AD(
+		deflt = "false", id = "xml.external.general.entities.allowed",
+		required = false
+	)
 	public boolean xmlExternalGeneralEntitiesAllowed();
 
-	@Meta.AD(deflt = "false", id = "xml.external.parameter.entities.allowed")
+	@Meta.AD(
+		deflt = "false", id = "xml.external.parameter.entities.allowed",
+		required = false
+	)
 	public boolean xmlExternalParameterEntitiesAllowed();
 
-	@Meta.AD(deflt = "true", id = "xsl.secure.processing.enabled")
+	@Meta.AD(
+		deflt = "true", id = "xsl.secure.processing.enabled", required = false
+	)
 	public boolean xslSecureProcessingEnabled();
 
 }
