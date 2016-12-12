593e80f93b8ad2939a3b41802dd9336a3654f861
Alejandro Hernández <info@alejandrohdezma.com>
2016-12-12 15:40:19 +0100

    LPS-69511 Create module

---
+++ modules/apps/adaptive-media/adaptive-media-image-jax-rs-test/src/testIntegration/java/com/liferay/adaptive/media/image/jaxrs/client/test/internal/ImageAdaptiveMediaJaxRsConfigurationTest.java
@@ -0,0 +1,14 @@
+package com.liferay.adaptive.media.image.jaxrs.client.test.internal;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+
+import org.junit.runner.RunWith;
+
+/**
+ * @author Alejandro Hernández
+ */
+@RunAsClient
+@RunWith(Arquillian.class)
+public class ImageAdaptiveMediaJaxRsConfigurationTest {
+}
