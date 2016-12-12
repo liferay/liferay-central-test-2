marcellustavares/liferay-portal

LPS-69400 update references for Message Boards

modules/apps/collaboration/message-boards/message-boards-web/src/main/resources/META-INF/resources/message_boards/edit_category.jsp
@@ -211,16 +211,16 @@ if (portletTitleBasedNavigation) {
 				</c:if>
 			</aui:fieldset>
 
-			<liferay-ui:custom-attributes-available className="<%= MBCategory.class.getName() %>">
+			<liferay-custom-field-ui:custom-attribute-available className="<%= MBCategory.class.getName() %>">
 				<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="custom-fields">
-					<liferay-ui:custom-attribute-list
+					<liferay-custom-field-ui:custom-attribute-list
 						className="<%= MBCategory.class.getName() %>"
 						classPK="<%= (category != null) ? category.getCategoryId() : 0 %>"
 						editable="<%= true %>"
 						label="<%= true %>"
 					/>
 				</aui:fieldset>
-			</liferay-ui:custom-attributes-available>
+			</liferay-custom-field-ui:custom-attribute-available>
 
 			<c:if test="<%= category == null %>">
 				<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="permissions">

modules/apps/collaboration/message-boards/message-boards-web/src/main/resources/META-INF/resources/message_boards/edit_message.jsp
@@ -232,16 +232,16 @@ if (portletTitleBasedNavigation) {
 				</aui:field-wrapper>
 			</aui:fieldset>
 
-			<liferay-ui:custom-attributes-available className="<%= MBMessage.class.getName() %>">
+			<liferay-custom-field-ui:custom-attribute-available className="<%= MBMessage.class.getName() %>">
 				<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="custom-fields">
-					<liferay-ui:custom-attribute-list
+					<liferay-custom-field-ui:custom-attribute-list
 						className="<%= MBMessage.class.getName() %>"
 						classPK="<%= messageId %>"
 						editable="<%= true %>"
 						label="<%= true %>"
 					/>
 				</aui:fieldset>
-			</liferay-ui:custom-attributes-available>
+			</liferay-custom-field-ui:custom-attribute-available>
 
 			<c:if test="<%= MBCategoryPermission.contains(permissionChecker, scopeGroupId, categoryId, ActionKeys.ADD_FILE) %>">
 				<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="attachments">

modules/apps/collaboration/message-boards/message-boards-web/src/main/resources/META-INF/resources/message_boards/view_thread_message.jsp
@@ -380,16 +380,16 @@ MBThread thread = (MBThread)request.getAttribute("edit_message.jsp-thread");
 			/>
 		</div>
 
-		<liferay-ui:custom-attributes-available className="<%= MBMessage.class.getName() %>">
+		<liferay-custom-field-ui:custom-attribute-available className="<%= MBMessage.class.getName() %>">
 			<div class="card-row card-row-padded custom-attributes">
-				<liferay-ui:custom-attribute-list
+				<liferay-custom-field-ui:custom-attribute-list
 					className="<%= MBMessage.class.getName() %>"
 					classPK="<%= (message != null) ? message.getMessageId() : 0 %>"
 					editable="<%= false %>"
 					label="<%= true %>"
 				/>
 			</div>
-		</liferay-ui:custom-attributes-available>
+		</liferay-custom-field-ui:custom-attribute-available>
 
 		<div class="card-row card-row-padded entry-links">
 			<liferay-ui:asset-links

liferay/liferay-central-test-2

modules/apps/collaboration/message-boards/message-boards-web/src/main/resources/META-INF/resources/message_boards/edit_category.jsp
@@ -211,16 +211,16 @@ if (portletTitleBasedNavigation) {
 				</c:if>
 			</aui:fieldset>
 
-			<liferay-ui:custom-attributes-available className="<%= MBCategory.class.getName() %>">
+			<liferay-custom-field-ui:custom-attributes-available className="<%= MBCategory.class.getName() %>">
 				<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="custom-fields">
-					<liferay-ui:custom-attribute-list
+					<liferay-custom-field-ui:custom-attribute-list
 						className="<%= MBCategory.class.getName() %>"
 						classPK="<%= (category != null) ? category.getCategoryId() : 0 %>"
 						editable="<%= true %>"
 						label="<%= true %>"
 					/>
 				</aui:fieldset>
-			</liferay-ui:custom-attributes-available>
+			</liferay-custom-field-ui:custom-attributes-available>
 
 			<c:if test="<%= category == null %>">
 				<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="permissions">

modules/apps/collaboration/message-boards/message-boards-web/src/main/resources/META-INF/resources/message_boards/edit_message.jsp
@@ -232,16 +232,16 @@ if (portletTitleBasedNavigation) {
 				</aui:field-wrapper>
 			</aui:fieldset>
 
-			<liferay-ui:custom-attributes-available className="<%= MBMessage.class.getName() %>">
+			<liferay-custom-field-ui:custom-attributes-available className="<%= MBMessage.class.getName() %>">
 				<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="custom-fields">
-					<liferay-ui:custom-attribute-list
+					<liferay-custom-field-ui:custom-attribute-list
 						className="<%= MBMessage.class.getName() %>"
 						classPK="<%= messageId %>"
 						editable="<%= true %>"
 						label="<%= true %>"
 					/>
 				</aui:fieldset>
-			</liferay-ui:custom-attributes-available>
+			</liferay-custom-field-ui:custom-attributes-available>
 
 			<c:if test="<%= MBCategoryPermission.contains(permissionChecker, scopeGroupId, categoryId, ActionKeys.ADD_FILE) %>">
 				<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="attachments">

modules/apps/collaboration/message-boards/message-boards-web/src/main/resources/META-INF/resources/message_boards/view_thread_message.jsp
@@ -380,16 +380,16 @@ MBThread thread = (MBThread)request.getAttribute("edit_message.jsp-thread");
 			/>
 		</div>
 
-		<liferay-ui:custom-attributes-available className="<%= MBMessage.class.getName() %>">
+		<liferay-custom-field-ui:custom-attributes-available className="<%= MBMessage.class.getName() %>">
 			<div class="card-row card-row-padded custom-attributes">
-				<liferay-ui:custom-attribute-list
+				<liferay-custom-field-ui:custom-attribute-list
 					className="<%= MBMessage.class.getName() %>"
 					classPK="<%= (message != null) ? message.getMessageId() : 0 %>"
 					editable="<%= false %>"
 					label="<%= true %>"
 				/>
 			</div>
-		</liferay-ui:custom-attributes-available>
+		</liferay-custom-field-ui:custom-attributes-available>
 
 		<div class="card-row card-row-padded entry-links">
 			<liferay-ui:asset-links