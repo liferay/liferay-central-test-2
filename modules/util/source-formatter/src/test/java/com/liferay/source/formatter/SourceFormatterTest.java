9e3101e9dba64a13769ed42f4bfa9b26263263d7
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-12-12 10:46:00 -0800

    LPS-69501 The SF test code is not needed anymore

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/SourceFormatterTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/SourceFormatterTest.java
@@ -14,11 +14,8 @@
 
 package com.liferay.source.formatter;
 
-import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.rule.TimeoutTestRule;
-import com.liferay.portal.kernel.util.ReleaseInfo;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -57,60 +54,7 @@ public class SourceFormatterTest {
 		Assert.assertTrue(modifiedFileNames.isEmpty());
 	}
 
-	@Test
-	public void testSourceFormatter() throws Exception {
-		SourceFormatterArgs sourceFormatterArgs = new SourceFormatterArgs();
-
-		sourceFormatterArgs.setAutoFix(false);
-		sourceFormatterArgs.setBaseDirName("../../../");
-		sourceFormatterArgs.setPrintErrors(false);
-		sourceFormatterArgs.setThrowException(true);
-		sourceFormatterArgs.setUseProperties(false);
-
-		SourceFormatter sourceFormatter = new SourceFormatter(
-			sourceFormatterArgs);
-
-		String name = ReleaseInfo.getName();
-
-		if (!name.contains(" DXP ")) {
-			List<String> defaultExcludes = new ArrayList<>(
-				ReflectionTestUtil.<List<String>>getFieldValue(
-					SourceFormatter.class, "_defaultExcludes"));
-
-			defaultExcludes.add("modules/private/**");
-
-			ReflectionTestUtil.setFieldValue(
-				SourceFormatter.class, "_defaultExcludes", defaultExcludes);
-		}
-
-		try {
-			sourceFormatter.format();
-		}
-		catch (SourceMismatchException sme) {
-			try {
-				Assert.assertEquals(
-					sme.getFileName(), sme.getFormattedSource(),
-					sme.getOriginalSource());
-			}
-			catch (AssertionError ae) {
-				String message = ae.getMessage();
-
-				if (message.length() >= _MAX_MESSAGE_SIZE) {
-					message =
-						"Truncated message :\n" +
-							message.substring(0, _MAX_MESSAGE_SIZE);
-
-					throw new AssertionError(message, ae.getCause());
-				}
-
-				throw ae;
-			}
-		}
-	}
-
 	@Rule
 	public final TestRule testRule = TimeoutTestRule.INSTANCE;
-
-	private static final int _MAX_MESSAGE_SIZE = 10000;
 
 }
