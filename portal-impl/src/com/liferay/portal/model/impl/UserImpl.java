fdb931af16d0a68d8a86cf66906b3d79b28a9651
Preston-Crary <preston.crary@liferay.com>
2016-12-12 14:07:17 -0800

    LPS-69670 Reorder logic to check the user's state first since it's cheaper than checking the password policy.

--- portal-impl/src/com/liferay/portal/model/impl/UserImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/UserImpl.java
@@ -838,7 +838,7 @@ public class UserImpl extends UserBaseImpl {
 
 	@Override
 	public boolean isEmailAddressVerificationComplete() {
-		if (isDefaultUser()) {
+		if (isDefaultUser() || isEmailAddressVerified()) {
 			return true;
 		}
 
@@ -855,7 +855,7 @@ public class UserImpl extends UserBaseImpl {
 		}
 
 		if (emailAddressVerificationRequired) {
-			return super.isEmailAddressVerified();
+			return false;
 		}
 
 		return true;
@@ -911,7 +911,7 @@ public class UserImpl extends UserBaseImpl {
 
 	@Override
 	public boolean isTermsOfUseComplete() {
-		if (isDefaultUser()) {
+		if (isDefaultUser() || isAgreedToTermsOfUse()) {
 			return true;
 		}
 
@@ -920,7 +920,7 @@ public class UserImpl extends UserBaseImpl {
 			PropsValues.TERMS_OF_USE_REQUIRED);
 
 		if (termsOfUseRequired) {
-			return super.isAgreedToTermsOfUse();
+			return false;
 		}
 
 		return true;
