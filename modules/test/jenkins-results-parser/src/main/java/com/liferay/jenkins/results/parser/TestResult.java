ef3da64f21d7e4eab5d1dcbd18faf247fdf8d18a
Leslie Wong <leslie.wong@liferay.com>
2016-12-13 13:00:02 -0800

    LRQA-29755 Add new class to wrap jenkins test results

---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TestResult.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jenkins.results.parser;
+
+/**
+ * @author Leslie Wong
+ */
+public class TestResult {
+
+	public TestResult(
+		String className, String duration, String testName, String status) {
+
+		this.className = className;
+		this.duration = duration;
+		this.testName = testName;
+		this.status = status;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public String getDuration() {
+		return duration;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public String getTestName() {
+		return testName;
+	}
+
+	public String className;
+	public String duration;
+	public String status;
+	public String testName;
+
+}
