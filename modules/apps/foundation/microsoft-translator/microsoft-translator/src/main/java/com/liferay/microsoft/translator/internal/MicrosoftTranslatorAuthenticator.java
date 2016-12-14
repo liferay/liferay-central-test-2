dc6558ef3143f60499c60df9b875093373a026c3
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-12-13 16:03:56 -0800

    LPS-69710 Move impl classes to new module

--- modules/apps/foundation/microsoft-translator/microsoft-translator/src/main/java/com/liferay/microsoft/translator/internal/MicrosoftTranslatorAuthenticator.java
+++ modules/apps/foundation/microsoft-translator/microsoft-translator/src/main/java/com/liferay/microsoft/translator/internal/MicrosoftTranslatorAuthenticator.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.microsofttranslator;
+package com.liferay.microsoft.translator.internal;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
