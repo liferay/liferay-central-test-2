59abc59ea755efd89f81e0953a736045b5738426
drewbrokke <drew.brokke@liferay.com>
2016-12-13 16:46:31 -0800

    LPS-69331 contacts-service - UpgradeEntry and ContactsServiceUpgrade - uses SQL - the EntryLocalService cannot be required because it will never be available to use if the schema version is not up to date.

--- modules/apps/foundation/contacts/contacts-service/src/main/java/com/liferay/contacts/internal/upgrade/ContactsServiceUpgrade.java
+++ modules/apps/foundation/contacts/contacts-service/src/main/java/com/liferay/contacts/internal/upgrade/ContactsServiceUpgrade.java
@@ -15,7 +15,6 @@
 package com.liferay.contacts.internal.upgrade;
 
 import com.liferay.contacts.internal.upgrade.v2_0_0.UpgradeEntry;
-import com.liferay.contacts.service.EntryLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
@@ -39,15 +38,12 @@ public class ContactsServiceUpgrade implements UpgradeStepRegistrator {
 
 		registry.register(
 			"com.liferay.contacts.service", "1.0.0", "2.0.1",
-			new UpgradeEntry(_entryLocalService, _userLocalService));
+			new UpgradeEntry(_userLocalService));
 
 		registry.register(
 			"com.liferay.contacts.service", "2.0.0", "2.0.1",
 			new DummyUpgradeStep());
 	}
-
-	@Reference
-	private EntryLocalService _entryLocalService;
 
 	@Reference
 	private UserLocalService _userLocalService;
