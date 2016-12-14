a7794c7bbad2c8ec55753db05aac3f297b4256f6
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2016-12-14 13:43:25 +0100

    LPS-69703 Use existing utils and methods to render ftl templates

--- modules/apps/document-library-google-docs/document-library-google-docs/src/main/java/com/liferay/document/library/google/docs/internal/display/context/GoogleDocsDLFilePicker.java
+++ modules/apps/document-library-google-docs/document-library-google-docs/src/main/java/com/liferay/document/library/google/docs/internal/display/context/GoogleDocsDLFilePicker.java
@@ -15,15 +15,15 @@
 package com.liferay.document.library.google.docs.internal.display.context;
 
 import com.liferay.document.library.display.context.DLFilePicker;
-import com.liferay.document.library.google.docs.internal.util.FreeMarkerRenderer;
 import com.liferay.document.library.google.docs.internal.util.GoogleDocsConfigurationHelper;
 import com.liferay.document.library.google.docs.internal.util.GoogleDocsConstants;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.io.unsync.UnsyncStringWriter;
+import com.liferay.portal.kernel.template.Template;
+import com.liferay.portal.kernel.template.TemplateConstants;
+import com.liferay.portal.kernel.template.TemplateManagerUtil;
+import com.liferay.portal.kernel.template.URLTemplateResource;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-
-import freemarker.template.TemplateException;
-
-import java.io.IOException;
 
 /**
  * @author Iván Zaera
@@ -54,29 +54,32 @@ public class GoogleDocsDLFilePicker implements DLFilePicker {
 
 	@Override
 	public String getJavaScript() throws PortalException {
-		try {
-			FreeMarkerRenderer freeMarkerRenderer = new FreeMarkerRenderer(
-				"com/liferay/document/library/google/docs/internal/display" +
-					"/context/dependencies/google_file_picker.ftl");
+		String googleFilePickerFtl =
+			"/com/liferay/document/library/google/docs/internal/display" +
+				"/context/dependencies/google_file_picker.ftl";
 
-			freeMarkerRenderer.setAttribute(
-				"googleAppsAPIKey",
-				_googleDocsConfigurationHelper.getGoogleAppsAPIKey());
-			freeMarkerRenderer.setAttribute(
-				"googleClientId",
-				_googleDocsConfigurationHelper.getGoogleClientId());
-			freeMarkerRenderer.setAttribute("namespace", _namespace);
-			freeMarkerRenderer.setAttribute(
-				"onFilePickCallback", _onFilePickCallback);
+		Class<?> clazz = getClass();
 
-			return freeMarkerRenderer.render();
-		}
-		catch (IOException ioe) {
-			throw new PortalException(ioe);
-		}
-		catch (TemplateException te) {
-			throw new PortalException(te);
-		}
+		URLTemplateResource templateResource = new URLTemplateResource(
+			googleFilePickerFtl, clazz.getResource(googleFilePickerFtl));
+
+		Template template = TemplateManagerUtil.getTemplate(
+			TemplateConstants.LANG_TYPE_FTL, templateResource, false);
+
+		template.put(
+			"googleAppsAPIKey",
+			_googleDocsConfigurationHelper.getGoogleAppsAPIKey());
+		template.put(
+			"googleClientId",
+			_googleDocsConfigurationHelper.getGoogleClientId());
+		template.put("namespace", _namespace);
+		template.put("onFilePickCallback", _onFilePickCallback);
+
+		UnsyncStringWriter unsyncStringWriter = new UnsyncStringWriter();
+
+		template.processTemplate(unsyncStringWriter);
+
+		return unsyncStringWriter.toString();
 	}
 
 	@Override
