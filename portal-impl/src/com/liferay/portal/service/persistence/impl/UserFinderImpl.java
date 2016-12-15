pei-jung/liferay-portal

LPS-68591 Calculate stringBundler size instead using a fixed value

portal-impl/src/com/liferay/portal/service/persistence/impl/UserFinderImpl.java
@@ -462,7 +462,9 @@ public class UserFinderImpl extends UserFinderBaseImpl implements UserFinder {
 				sql = StringUtil.replace(sql, _STATUS_SQL, StringPool.BLANK);
 			}
 
-			StringBundler sb = new StringBundler(20);
+			int stringBundlerSize = (paramsMapList.size() * 4) + 1;
+
+			StringBundler sb = new StringBundler(stringBundlerSize);
 
 			sb.append("SELECT COUNT(userId) AS COUNT_VALUE FROM (");
 
@@ -761,7 +763,9 @@ public class UserFinderImpl extends UserFinderBaseImpl implements UserFinder {
 				sql = StringUtil.replace(sql, _STATUS_SQL, StringPool.BLANK);
 			}
 
-			StringBundler sb = new StringBundler(20);
+			int stringBundlerSize = (paramsMapList.size() * 4) + 1;
+
+			StringBundler sb = new StringBundler(stringBundlerSize);
 
 			for (int i = 0; i < paramsMapList.size(); i++) {
 				if (i != 0) {

liferay/liferay-central-test-2

portal-impl/src/com/liferay/portal/service/persistence/impl/UserFinderImpl.java
@@ -461,7 +461,9 @@ public class UserFinderImpl extends UserFinderBaseImpl implements UserFinder {
 				sql = StringUtil.replace(sql, _STATUS_SQL, StringPool.BLANK);
 			}
 
-			StringBundler sb = new StringBundler(20);
+			int stringBundlerSize = (paramsList.size() * 4) + 1;
+
+			StringBundler sb = new StringBundler(stringBundlerSize);
 
 			sb.append("SELECT COUNT(userId) AS COUNT_VALUE FROM (");
 
@@ -764,7 +766,9 @@ public class UserFinderImpl extends UserFinderBaseImpl implements UserFinder {
 				sql = StringUtil.replace(sql, _STATUS_SQL, StringPool.BLANK);
 			}
 
-			StringBundler sb = new StringBundler(20);
+			int stringBundlerSize = (paramsList.size() * 4) + 1;
+
+			StringBundler sb = new StringBundler(stringBundlerSize);
 
 			for (int i = 0; i < paramsList.size(); i++) {
 				if (i == 0) {