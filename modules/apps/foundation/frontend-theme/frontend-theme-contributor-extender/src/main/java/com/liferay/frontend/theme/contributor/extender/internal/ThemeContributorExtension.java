597ca583c18504a5fa535991ae9857223bc47de9
shuyangzhou <shuyang.zhou@liferay.com>
2016-12-16 16:28:54 -0800

    LPS-69753 Lower access level

--- modules/apps/foundation/frontend-theme/frontend-theme-contributor-extender/src/main/java/com/liferay/frontend/theme/contributor/extender/internal/ThemeContributorExtension.java
+++ modules/apps/foundation/frontend-theme/frontend-theme-contributor-extender/src/main/java/com/liferay/frontend/theme/contributor/extender/internal/ThemeContributorExtension.java
@@ -136,12 +136,6 @@ public class ThemeContributorExtension implements Extension {
 	private class ThemeContributorPortalWebResources
 		implements PortalWebResources {
 
-		public ThemeContributorPortalWebResources(
-			ServletContext servletContext) {
-
-			_servletContext = servletContext;
-		}
-
 		@Override
 		public String getContextPath() {
 			return _servletContext.getContextPath();
@@ -160,6 +154,12 @@ public class ThemeContributorExtension implements Extension {
 		@Override
 		public ServletContext getServletContext() {
 			return _servletContext;
+		}
+
+		private ThemeContributorPortalWebResources(
+			ServletContext servletContext) {
+
+			_servletContext = servletContext;
 		}
 
 		private final ServletContext _servletContext;
