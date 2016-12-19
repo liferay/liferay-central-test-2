40545743668b0aaee5dccf85539abd3f49a99bce
Fernando Souza <fernando.souza@liferay.com>
2016-12-19 00:30:29 -0300

    LPS-69016 Adds unit test

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-select/src/test/testJS/select_field.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-select/src/test/testJS/select_field.js
@@ -2,6 +2,14 @@
 
 var A = AUI();
 
+function triggerEvent(name, element) {
+	var event = document.createEvent('HTMLEvents');
+
+	event.initEvent(name, true, true);
+
+	element.getDOMNode().dispatchEvent(event);
+}
+
 describe(
 	'Liferay.DDM.Field.Select',
 	function() {
@@ -178,6 +186,191 @@ describe(
 						assert.equal(
 							selectField.getValue(),
 							'foo'
+						);
+					}
+				);
+			}
+		);
+
+		describe(
+			'Search',
+			function() {
+				it(
+					'should filter options list',
+					function(done) {
+						selectField.set('options', [{label: 'foo', value: 'foo'}, {label: 'bar', value: 'bar'}]);
+
+						var container = selectField.get('container');
+
+						var inputElement = container.one('input');
+
+						inputElement.val('foo');
+
+						triggerEvent('input', inputElement);
+
+						window.setTimeout(
+							function() {
+								var items = container.all('.drop-chosen ul > li');
+
+								assert.equal(
+									1,
+									items.size()
+								);
+
+								done();
+							},
+							600
+						);
+					}
+				);
+
+				it(
+					'should clear the search input after closing the field',
+					function() {
+						selectField.set('options', [{label: 'foo', value: 'foo'}, {label: 'bar', value: 'bar'}]);
+
+						var container = selectField.get('container');
+
+						container.one('input').val('foo');
+
+						selectField.openList();
+
+						assert.equal(
+							'foo',
+							container.one('input').val()
+						);
+
+						selectField.closeList();
+
+						assert.equal(
+							'',
+							container.one('input').val()
+						);
+					}
+				);
+
+				it(
+					'should clear the options list after closing the field',
+					function(done) {
+						selectField.set('options', [{label: 'foo', value: 'foo'}, {label: 'bar', value: 'bar'}]);
+
+						var container = selectField.get('container');
+
+						var beforeSearchList = container.one('.drop-chosen ul').getHTML();
+
+						var inputElement = container.one('input');
+
+						inputElement.val('foo');
+
+						triggerEvent('input', inputElement);
+
+						selectField.openList();
+
+						window.setTimeout(
+							function() {
+								selectField.closeList();
+
+								var afterSearchList = container.one('.drop-chosen ul').getHTML();
+
+								assert.equal(
+									beforeSearchList,
+									afterSearchList
+								);
+
+								done();
+							},
+							600
+						);
+					}
+				);
+
+				it(
+					'should render an empety list if the term doesn`t match',
+					function(done) {
+						selectField.set('options', [{label: 'foo', value: 'foo'}, {label: 'bar', value: 'bar'}]);
+
+						var container = selectField.get('container');
+
+						var inputElement = container.one('input');
+
+						inputElement.val('foo-bar');
+
+						triggerEvent('input', inputElement);
+
+						selectField.openList();
+
+						window.setTimeout(
+							function() {
+								var items = container.all('.drop-chosen ul > li');
+
+								assert.equal(
+									items.size(),
+									0
+								);
+
+								done();
+							},
+							600
+						);
+					}
+				);
+
+				it(
+					'should not filter if no term was passed',
+					function(done) {
+						selectField.set('options', [{label: 'foo', value: 'foo'}, {label: 'bar', value: 'bar'}]);
+
+						var container = selectField.get('container');
+
+						var beforeSearchList = container.one('.drop-chosen ul').getHTML();
+
+						var inputElement = container.one('input');
+
+						inputElement.val('        ');
+
+						triggerEvent('input', inputElement);
+
+						window.setTimeout(
+							function() {
+								var afterSearchList = container.one('.drop-chosen ul').getHTML();
+
+								assert.equal(
+									beforeSearchList,
+									afterSearchList
+								);
+
+								done();
+							},
+							600
+						);
+					}
+				);
+
+				it(
+					'should highlight the filtered term',
+					function(done) {
+						selectField.set('options', [{label: 'foo', value: 'foo'}, {label: 'bar', value: 'bar'}]);
+
+						var container = selectField.get('container');
+
+						var inputElement = container.one('input');
+
+						inputElement.val('foo');
+
+						triggerEvent('input', inputElement);
+
+						window.setTimeout(
+							function() {
+								var term = container.one('.drop-chosen ul > li b').getContent();
+
+								assert.equal(
+									'foo',
+									term
+								);
+
+								done();
+							},
+							600
 						);
 					}
 				);
