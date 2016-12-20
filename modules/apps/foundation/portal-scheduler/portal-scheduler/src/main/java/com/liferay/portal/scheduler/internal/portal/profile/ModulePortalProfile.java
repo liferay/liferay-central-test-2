a94531370c06708cfbb4e857bb3f7bfeef6ab657
Tina Tian <tina.tian@liferay.com>
2016-12-20 14:09:59 -0800

    LPS-69572 Register dummy service when scheduler is disabled

--- modules/apps/foundation/portal-scheduler/portal-scheduler/src/main/java/com/liferay/portal/scheduler/internal/portal/profile/ModulePortalProfile.java
+++ modules/apps/foundation/portal-scheduler/portal-scheduler/src/main/java/com/liferay/portal/scheduler/internal/portal/profile/ModulePortalProfile.java
@@ -14,9 +14,12 @@
 
 package com.liferay.portal.scheduler.internal.portal.profile;
 
+import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.kernel.util.Props;
 import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.ProxyFactory;
 import com.liferay.portal.profile.BaseDSModulePortalProfile;
 import com.liferay.portal.profile.PortalProfile;
 import com.liferay.portal.scheduler.internal.SchedulerEngineHelperImpl;
@@ -27,6 +30,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.osgi.framework.BundleContext;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -52,6 +56,13 @@ public class ModulePortalProfile extends BaseDSModulePortalProfile {
 		}
 		else {
 			supportedPortalProfileNames = Collections.emptySet();
+
+			BundleContext bundleContext = componentContext.getBundleContext();
+
+			bundleContext.registerService(
+				SchedulerEngineHelper.class,
+				ProxyFactory.newDummyInstance(SchedulerEngineHelper.class),
+				new HashMapDictionary<String, Object>());
 		}
 
 		init(
