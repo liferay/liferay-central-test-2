9fe3795664491037618aed2dbbfa8642fbf535ad
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2016-12-27 16:04:21 +0100

    LPS-69728 SF

--- portal-impl/src/com/liferay/portlet/messageboards/service/persistence/impl/MBCategoryFinderImpl.java
+++ portal-impl/src/com/liferay/portlet/messageboards/service/persistence/impl/MBCategoryFinderImpl.java
@@ -510,16 +510,6 @@ public class MBCategoryFinderImpl
 		}
 	}
 
-	protected String replaceExcludeStatus(
-		String sql, QueryDefinition<?> queryDefinition) {
-
-		if (queryDefinition.isExcludeStatus()) {
-			sql = StringUtil.replace(sql, ".status = ?)", ".status != ?)");
-		}
-
-		return sql;
-	}
-
 	protected String updateSQL(
 		String sql, QueryDefinition<MBCategory> queryDefinition) {
 
