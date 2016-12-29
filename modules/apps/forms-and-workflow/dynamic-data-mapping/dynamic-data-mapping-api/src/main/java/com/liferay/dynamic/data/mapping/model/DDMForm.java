b2cc47eee945951085299c46632f50a18a884b83
Hugo Huijser <hugo.huijser@liferay.com>
2016-12-29 16:29:19 +0800

    LPS-66853 Auto SF

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/model/DDMForm.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/model/DDMForm.java
@@ -68,8 +68,7 @@ public class DDMForm implements Serializable {
 
 		if (Objects.equals(_availableLocales, ddmForm._availableLocales) &&
 			Objects.equals(_defaultLocale, ddmForm._defaultLocale) &&
-			Objects.equals(
-				_ddmFormFields, ddmForm._ddmFormFields)) {
+			Objects.equals(_ddmFormFields, ddmForm._ddmFormFields)) {
 
 			return true;
 		}
--- modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -7145,8 +7145,7 @@ public class JournalArticleLocalServiceImpl
 			groupId, urlTitle);
 
 		if ((urlTitleArticle != null) &&
-			!Objects.equals(
-				urlTitleArticle.getArticleId(), articleId)) {
+			!Objects.equals(urlTitleArticle.getArticleId(), articleId)) {
 
 			urlTitle = getUniqueUrlTitle(id, groupId, articleId, urlTitle);
 		}
