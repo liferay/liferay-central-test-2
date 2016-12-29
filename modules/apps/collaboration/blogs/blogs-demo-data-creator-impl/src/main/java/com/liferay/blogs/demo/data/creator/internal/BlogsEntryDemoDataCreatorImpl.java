258910ee9dc7d9a2f6b812143e1570aeab46f4da
Alejandro Hernández <info@alejandrohdezma.com>
2016-12-29 15:47:55 +0100

    LPS-69876 Create implementation for simple BlogsEntry

---
+++ modules/apps/collaboration/blogs/blogs-demo-data-creator-impl/src/main/java/com/liferay/blogs/demo/data/creator/internal/BlogsEntryDemoDataCreatorImpl.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.blogs.demo.data.creator.internal;
+
+import com.liferay.blogs.demo.data.creator.BlogsEntryDemoDataCreator;
+import com.liferay.blogs.exception.NoSuchEntryException;
+import com.liferay.blogs.model.BlogsEntry;
+import com.liferay.blogs.service.BlogsEntryLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.StringPool;
+
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Hernández
+ */
+@Component(service = BlogsEntryDemoDataCreator.class)
+public class BlogsEntryDemoDataCreatorImpl
+	implements BlogsEntryDemoDataCreator {
+
+	@Override
+	public BlogsEntry create(long userId, long groupId)
+		throws IOException, PortalException {
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setScopeGroupId(groupId);
+
+		String title = "Test title";
+
+		String subtitle = StringPool.BLANK;
+
+		Date date = new Date();
+
+		BlogsEntry blogsEntry = _blogsEntryLocalService.addEntry(
+			userId, title, subtitle, null, StringPool.BLANK, date, false, false,
+			null, null, null, null, serviceContext);
+
+		_entryIds.add(blogsEntry.getEntryId());
+
+		return blogsEntry;
+	}
+
+	@Override
+	public void delete() throws PortalException {
+		try {
+			for (long entryId : _entryIds) {
+				_blogsEntryLocalService.deleteBlogsEntry(entryId);
+				_entryIds.remove(entryId);
+			}
+		}
+		catch (NoSuchEntryException nsee) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(nsee);
+			}
+		}
+	}
+
+	@Reference(unbind = "-")
+	protected void setBlogLocalService(
+		BlogsEntryLocalService blogsEntryLocalService) {
+
+		_blogsEntryLocalService = blogsEntryLocalService;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		BlogsEntryDemoDataCreatorImpl.class);
+
+	private BlogsEntryLocalService _blogsEntryLocalService;
+	private final List<Long> _entryIds = new ArrayList<>();
+
+}
