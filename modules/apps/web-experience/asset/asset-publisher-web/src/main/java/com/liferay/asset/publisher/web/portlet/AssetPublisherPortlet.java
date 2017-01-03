a7b47952ef767dddd7eb9ac8f0ac679c8b76d637
Pavel Savinov <pavel.savinov@liferay.com>
2017-01-03 12:18:25 +0100

    LPS-68688 clean single page application cache before rendering asset publisher portlet

--- modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/portlet/AssetPublisherPortlet.java
+++ modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/portlet/AssetPublisherPortlet.java
@@ -295,6 +295,9 @@ public class AssetPublisherPortlet extends MVCPortlet {
 			renderRequest.setAttribute(
 				AssetPublisherWebKeys.ASSET_PUBLISHER_CUSTOMIZER,
 				assetPublisherCustomizer);
+
+			renderRequest.setAttribute(
+				WebKeys.SINGLE_PAGE_APPLICATION_CLEAR_CACHE, true);
 		}
 		catch (Exception e) {
 			_log.error("Unable to get asset publisher customizer", e);
