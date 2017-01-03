0bb0e42d0148d3ebece787ac595db8760b415e30
Jonathan McCann <jonathan.mccann@liferay.com>
2017-01-03 09:14:36 -0800

    LPS-68898 Improve code comment to clarity that the export is for the Group-scoped embedded portlets only

--- modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/controller/PortletExportController.java
+++ modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/controller/PortletExportController.java
@@ -746,7 +746,7 @@ public class PortletExportController implements ExportController {
 				PortletKeys.PREFS_PLID_SHARED, portlet.getRootPortletId(),
 				portletElement);
 
-			// Embedded portlets
+			// Group Embedded Portlets
 
 			exportPortletPreferences(
 				portletDataContext, portletDataContext.getScopeGroupId(),
