fba3641c00317627df25f27edc16f64d4aba67ad
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2017-01-03 10:26:21 +0100

    LPS-69943 Configure the url where the images will be automatically uploaded

---
+++ modules/apps/collaboration/message-boards/message-boards-editor-configuration/src/main/java/com/liferay/message/boards/editor/configuration/internal/MBEditorOptionsContributor.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.message.boards.editor.configuration.internal;
+
+import com.liferay.message.boards.web.constants.MBPortletKeys;
+import com.liferay.portal.kernel.editor.configuration.EditorOptions;
+import com.liferay.portal.kernel.editor.configuration.EditorOptionsContributor;
+import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.PortletKeys;
+
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.PortletURL;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Ambrín Chaudhary
+ */
+@Component(
+	property = {
+		"editor.name=alloyeditor", "editor.name=alloyeditor_bbcode",
+		"editor.name=ckeditor", "editor.name=ckeditor_bbcode",
+		"javax.portlet.name=" + MBPortletKeys.MESSAGE_BOARDS,
+		"javax.portlet.name=" + MBPortletKeys.MESSAGE_BOARDS_ADMIN
+	},
+	service = EditorOptionsContributor.class
+)
+public class MBEditorOptionsContributor
+	implements EditorOptionsContributor {
+
+	@Override
+	public void populateEditorOptions(
+		EditorOptions editorOptions,
+		Map<String, Object> inputEditorTaglibAttributes,
+		ThemeDisplay themeDisplay,
+		RequestBackedPortletURLFactory requestBackedPortletURLFactory) {
+
+		PortletURL portletURL = requestBackedPortletURLFactory.createActionURL(
+			PortletKeys.DOCUMENT_LIBRARY);
+
+		portletURL.setParameter(
+			ActionRequest.ACTION_NAME, "/document_library/upload_file_entry");
+		portletURL.setParameter("folderId", "0");
+
+		editorOptions.setUploadURL(portletURL.toString());
+	}
+
+}
