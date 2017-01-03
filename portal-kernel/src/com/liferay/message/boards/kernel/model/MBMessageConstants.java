46f59133a235ce9c177858fc86ecb8bbcad479e1
rimi saadou <rimi.saadou@liferay.com>
2017-01-03 09:32:45 +0100

    LPS-69461 SF - use constant (Comment is cut off in Activities portlet)

--- portal-kernel/src/com/liferay/message/boards/kernel/model/MBMessageConstants.java
+++ portal-kernel/src/com/liferay/message/boards/kernel/model/MBMessageConstants.java
@@ -31,6 +31,8 @@ public class MBMessageConstants {
 	public static final String[] FORMATS = PropsUtil.getArray(
 		PropsKeys.MESSAGE_BOARDS_MESSAGE_FORMATS);
 
+	public static final int MAXIMUM_SUBJECT_LENGTH = 50;
+
 	public static final String MESSAGE_SUBJECT_PREFIX_RE = "RE: ";
 
 }
