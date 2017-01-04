d806d29385710b23f908a84bee6e7585750d8ee4
Adolfo Pérez <adolfo.perez@liferay.com>
2017-01-04 12:39:12 +0100

    LPS-69924 Go back to main page after invitations sent
    The success message is displayed on the main view, which is only shown
    when the window state is NORMAL.
    
    Here we cover two cases:
    
      1. The "redirect" param is present; use it and finish the request.
      2. Otherwise, change the window state so that the main view is
      displayed.

--- modules/apps/invitation/invitation-web/src/main/java/com/liferay/invitation/web/internal/portlet/action/ViewMVCActionCommand.java
+++ modules/apps/invitation/invitation-web/src/main/java/com/liferay/invitation/web/internal/portlet/action/ViewMVCActionCommand.java
@@ -44,6 +44,7 @@ import javax.mail.internet.InternetAddress;
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.PortletPreferences;
+import javax.portlet.WindowState;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -173,10 +174,17 @@ public class ViewMVCActionCommand extends BaseMVCActionCommand {
 			ParamUtil.getString(actionRequest, "redirect"));
 
 		if (Validator.isNotNull(redirect)) {
-			actionResponse.setRenderParameter("mvcPath", redirect);
+			SessionMessages.add(
+				actionRequest,
+				PortalUtil.getPortletId(actionRequest) +
+					SessionMessages.KEY_SUFFIX_FORCE_SEND_REDIRECT);
+
+			actionResponse.sendRedirect(redirect);
+
+			return;
 		}
 
-		actionResponse.setRenderParameter("mvcPath", "/view.jsp");
+		actionResponse.setWindowState(WindowState.NORMAL);
 	}
 
 	@Reference(unbind = "-")
