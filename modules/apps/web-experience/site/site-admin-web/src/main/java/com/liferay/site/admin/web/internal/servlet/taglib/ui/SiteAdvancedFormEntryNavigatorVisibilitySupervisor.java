a4fffc9c84c9913285a7c7dfb6b36ab8f96d9718
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2017-01-04 16:32:53 +0100

    LPS-70043 Create more visibility supervisors for the remaining categories when adding/updating a site

---
+++ modules/apps/web-experience/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/servlet/taglib/ui/SiteAdvancedFormEntryNavigatorVisibilitySupervisor.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.admin.web.internal.servlet.taglib.ui;
+
+import com.liferay.frontend.taglib.form.navigator.util.FormNavigatorEntryVisibilityConfigurationHelper;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.servlet.taglib.ui.FormNavigatorConstants;
+import com.liferay.portal.kernel.servlet.taglib.ui.FormNavigatorEntry;
+import com.liferay.portal.kernel.servlet.taglib.ui.FormNavigatorEntryVisibilitySupervisor;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Sergio González
+ */
+@Component(service = FormNavigatorEntryVisibilitySupervisor.class)
+public class SiteAdvancedFormEntryNavigatorVisibilitySupervisor
+	implements FormNavigatorEntryVisibilitySupervisor<Group> {
+
+	@Override
+	public String getCategoryKey() {
+		return FormNavigatorConstants.CATEGORY_KEY_SITES_ADVANCED;
+	}
+
+	@Override
+	public String getFormNavigatorId() {
+		return FormNavigatorConstants.FORM_NAVIGATOR_ID_SITES;
+	}
+
+	@Override
+	public boolean isVisible(
+		FormNavigatorEntry<Group> formNavigatorEntry, User user, Group group) {
+
+		Class<? extends FormNavigatorEntry> clazz =
+			formNavigatorEntry.getClass();
+
+		if ((group == null) &&
+			_formNavigatorEntryVisibilityConfigurationHelper.isHidden(
+				"add.advanced", formNavigatorEntry.getFormNavigatorId(),
+				clazz.getName())) {
+
+			return false;
+		}
+		else if ((group != null) &&
+				 _formNavigatorEntryVisibilityConfigurationHelper.isHidden(
+					 "update.advanced", formNavigatorEntry.getFormNavigatorId(),
+					 clazz.getName())) {
+
+			return false;
+		}
+
+		return formNavigatorEntry.isVisible(user, group);
+	}
+
+	@Reference
+	private FormNavigatorEntryVisibilityConfigurationHelper
+		_formNavigatorEntryVisibilityConfigurationHelper;
+
+}
