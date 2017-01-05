7c8d18c3329956f9577f4e5b0eed36e04a49d366
Pavel Savinov <pavel.savinov@liferay.com>
2017-01-05 08:46:25 +0100

    LPS-68983 add user id to the portlet id in case when non instantiable portlet added to customizable page

--- portal-impl/src/com/liferay/portal/model/impl/LayoutTypePortletImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/LayoutTypePortletImpl.java
@@ -1336,7 +1336,8 @@ public class LayoutTypePortletImpl
 			}
 
 			if ((PortletConstants.hasInstanceId(portletId) ||
-				 portlet.isPreferencesUniquePerLayout()) &&
+				 portlet.isPreferencesUniquePerLayout() ||
+				 !portlet.isInstanceable()) &&
 				hasUserPreferences()) {
 
 				portletId = PortletConstants.assemblePortletId(
