e0c085496c7b1f74ccf0d2879fa013fd0dcd9ee3
Pedro Queiroz <pedro.queiroz@liferay.com>
2017-01-05 18:40:38 -0300

    LPS-69775 Add Grid Field type settings

---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-grid/src/main/java/com/liferay/dynamic/data/mapping/type/grid/internal/GridDDMFormFieldTypeSettings.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.type.grid.internal;
+
+import com.liferay.dynamic.data.mapping.annotations.DDMForm;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormField;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayout;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayoutColumn;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayoutPage;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayoutRow;
+import com.liferay.dynamic.data.mapping.form.field.type.DefaultDDMFormFieldTypeSettings;
+import com.liferay.dynamic.data.mapping.model.DDMFormFieldOptions;
+import com.liferay.dynamic.data.mapping.model.DDMFormFieldValidation;
+import com.liferay.dynamic.data.mapping.model.LocalizedValue;
+
+/**
+ * @author Marcellus Tavares
+ */
+@DDMForm
+@DDMFormLayout(
+	paginationMode = com.liferay.dynamic.data.mapping.model.DDMFormLayout.TABBED_MODE,
+	value = {
+		@DDMFormLayoutPage(
+			title = "basic",
+			value = {
+				@DDMFormLayoutRow(
+					{
+						@DDMFormLayoutColumn(
+							size = 12,
+							value = {
+								"label", "tip", "required", "predefinedValue",
+								"rows", "columns"
+							}
+						)
+					}
+				)
+			}
+		),
+		@DDMFormLayoutPage(
+			title = "properties",
+			value = {
+				@DDMFormLayoutRow(
+					{
+						@DDMFormLayoutColumn(
+							size = 12,
+							value = {
+								"visibilityExpression", "showLabel",
+								"repeatable", "fieldNamespace", "indexType",
+								"localizable", "readOnly", "dataType", "type",
+								"name", "validation"
+							}
+						)
+					}
+				)
+			}
+		)
+	}
+)
+public interface GridDDMFormFieldTypeSettings
+	extends DefaultDDMFormFieldTypeSettings {
+
+	@DDMFormField(
+		dataType = "ddm-options", label = "%columns", required = true,
+		type = "options"
+	)
+	public DDMFormFieldOptions columns();
+
+	@DDMFormField(visibilityExpression = "FALSE")
+	@Override
+	public LocalizedValue predefinedValue();
+
+	@DDMFormField(visibilityExpression = "FALSE")
+	@Override
+	public boolean repeatable();
+
+	@DDMFormField(
+		dataType = "ddm-options", label = "%rows", required = true,
+		type = "options"
+	)
+	public DDMFormFieldOptions rows();
+
+	@DDMFormField(visibilityExpression = "FALSE")
+	@Override
+	public DDMFormFieldValidation validation();
+
+}
