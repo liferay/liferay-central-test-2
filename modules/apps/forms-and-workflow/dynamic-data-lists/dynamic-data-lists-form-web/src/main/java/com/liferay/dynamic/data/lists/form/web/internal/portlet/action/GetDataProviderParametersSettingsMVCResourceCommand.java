0cb0eacff908e7153cab0f8038844015ec4e516d
Rafael Praxedes <rafael.praxedes@liferay.com>
2017-01-06 09:33:25 -0300

    LPS-70017 Provide a resourceCommand to retrieve the input and output parameters from a data provider

---
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/internal/portlet/action/GetDataProviderParametersSettingsMVCResourceCommand.java
@@ -0,0 +1,202 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.form.web.internal.portlet.action;
+
+import com.liferay.dynamic.data.lists.form.web.constants.DDLFormPortletKeys;
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProvider;
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderInputParametersSettings;
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderOutputParametersSettings;
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderParameterSettings;
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderTracker;
+import com.liferay.dynamic.data.mapping.io.DDMFormValuesJSONDeserializer;
+import com.liferay.dynamic.data.mapping.model.DDMDataProviderInstance;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.service.DDMDataProviderInstanceLocalService;
+import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
+import com.liferay.dynamic.data.mapping.util.DDMFormFactory;
+import com.liferay.dynamic.data.mapping.util.DDMFormInstanceFactory;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.portlet.PortletResponseUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.util.ClassUtil;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Rafael Praxedes
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + DDLFormPortletKeys.DYNAMIC_DATA_LISTS_FORM_ADMIN,
+		"mvc.command.name=getDataProviderParametersSettings"
+	},
+	service = MVCResourceCommand.class
+)
+public class GetDataProviderParametersSettingsMVCResourceCommand
+	extends BaseMVCResourceCommand {
+
+	protected JSONObject createParametersJSONObject(
+		DDMDataProvider dataProvider, DDMFormValues dataProviderFormValues) {
+
+		JSONObject parametersJSONObject = _jsonFactory.createJSONObject();
+
+		if (ClassUtil.isSubclass(
+				dataProvider.getSettings(),
+				DDMDataProviderParameterSettings.class)) {
+
+			DDMDataProviderParameterSettings dataProviderParameterizedSettings =
+				(DDMDataProviderParameterSettings)
+					DDMFormInstanceFactory.create(
+						dataProvider.getSettings(), dataProviderFormValues);
+
+			parametersJSONObject.put(
+				"inputs",
+				getInputParametersJSONObject(
+					dataProviderParameterizedSettings.inputParameters()));
+
+			parametersJSONObject.put(
+				"outputs",
+				getOutputParametersJSONObject(
+					dataProviderParameterizedSettings.outputParameters()));
+		}
+
+		return parametersJSONObject;
+	}
+
+	@Override
+	protected void doServeResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		DDMDataProviderInstance ddmDataProviderInstance =
+			getDDMDataProviderInstance(resourceRequest);
+
+		DDMDataProvider dataProvider =
+			_ddmDataProviderTracker.getDDMDataProvider(
+				ddmDataProviderInstance.getType());
+
+		DDMFormValues dataProviderFormValues = getDataProviderFormValues(
+			dataProvider, ddmDataProviderInstance);
+
+		JSONObject parametersJSONObject = createParametersJSONObject(
+			dataProvider, dataProviderFormValues);
+
+		resourceResponse.setContentType(ContentTypes.APPLICATION_JSON);
+
+		PortletResponseUtil.write(
+			resourceResponse, parametersJSONObject.toJSONString());
+
+		resourceResponse.flushBuffer();
+	}
+
+	protected DDMFormValues getDataProviderFormValues(
+			DDMDataProvider ddmDataProvider,
+			DDMDataProviderInstance ddmDataProviderInstance)
+		throws PortalException {
+
+		DDMForm ddmForm = DDMFormFactory.create(ddmDataProvider.getSettings());
+
+		return _ddmFormValuesJSONDeserializer.deserialize(
+			ddmForm, ddmDataProviderInstance.getDefinition());
+	}
+
+	protected DDMDataProviderInstance getDDMDataProviderInstance(
+			ResourceRequest resourceRequest)
+		throws PortalException {
+
+		long ddmDataProviderInstanceId = ParamUtil.getLong(
+			resourceRequest, "ddmDataProviderInstanceId");
+
+		return _ddmDataProviderInstanceLocalService.getDataProviderInstance(
+			ddmDataProviderInstanceId);
+	}
+
+	protected JSONArray getInputParametersJSONObject(
+		DDMDataProviderInputParametersSettings[]
+			ddmDataProviderInputParametersSettings) {
+
+		JSONArray inputsJSONArray = _jsonFactory.createJSONArray();
+
+		for (DDMDataProviderInputParametersSettings
+				ddmDataProviderInputParameter :
+					ddmDataProviderInputParametersSettings) {
+
+			JSONObject inputJSONObject = _jsonFactory.createJSONObject();
+
+			inputJSONObject.put(
+				"name", ddmDataProviderInputParameter.inputParameterName());
+
+			inputJSONObject.put(
+				"required",
+				ddmDataProviderInputParameter.inputParameterRequired());
+
+			inputJSONObject.put(
+				"type", ddmDataProviderInputParameter.inputParameterType());
+
+			inputsJSONArray.put(inputJSONObject);
+		}
+
+		return inputsJSONArray;
+	}
+
+	protected JSONArray getOutputParametersJSONObject(
+		DDMDataProviderOutputParametersSettings[]
+			ddmDataProviderOutputParametersSettings) {
+
+		JSONArray outputsJSONArray = _jsonFactory.createJSONArray();
+
+		for (DDMDataProviderOutputParametersSettings
+				ddmDataProviderOutputParameter :
+					ddmDataProviderOutputParametersSettings) {
+
+			JSONObject outputJSONObject = _jsonFactory.createJSONObject();
+
+			outputJSONObject.put(
+				"name", ddmDataProviderOutputParameter.outputParameterName());
+
+			outputJSONObject.put(
+				"type", ddmDataProviderOutputParameter.outputParameterType());
+
+			outputsJSONArray.put(outputJSONObject);
+		}
+
+		return outputsJSONArray;
+	}
+
+	@Reference
+	private DDMDataProviderInstanceLocalService
+		_ddmDataProviderInstanceLocalService;
+
+	@Reference
+	private DDMDataProviderTracker _ddmDataProviderTracker;
+
+	@Reference
+	private DDMFormValuesJSONDeserializer _ddmFormValuesJSONDeserializer;
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+}
