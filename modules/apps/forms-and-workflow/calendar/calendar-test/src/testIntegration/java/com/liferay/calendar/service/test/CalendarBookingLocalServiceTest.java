78f0d286d4bc1a04488c1075c3950f73e4bf4067
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2017-01-06 10:19:20 -0300

    LPS-69945 Test - CalendarBookingLocalService test to break regardless of time of day.

--- modules/apps/forms-and-workflow/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/service/test/CalendarBookingLocalServiceTest.java
+++ modules/apps/forms-and-workflow/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/service/test/CalendarBookingLocalServiceTest.java
@@ -105,7 +105,8 @@ public class CalendarBookingLocalServiceTest {
 			_user, _losAngelesTimeZone, serviceContext);
 
 		java.util.Calendar nowJCalendar = JCalendarUtil.getJCalendar(
-			System.currentTimeMillis(), calendar.getTimeZone());
+			2017, java.util.Calendar.JANUARY, 5, 22, 0, 0, 0,
+			calendar.getTimeZone());
 
 		CalendarBooking calendarBooking =
 			CalendarBookingTestUtil.addAllDayCalendarBooking(
