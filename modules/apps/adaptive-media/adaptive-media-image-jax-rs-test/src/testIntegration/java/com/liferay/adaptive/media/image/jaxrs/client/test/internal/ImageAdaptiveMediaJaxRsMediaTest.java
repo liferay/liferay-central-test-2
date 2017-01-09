cdc0ed43b1bb0f98ff7347fef21fe1a9c1eb04c9
Alejandro Hernández <info@alejandrohdezma.com>
2017-01-09 16:55:22 +0100

    LPS-69511 Create new test class for variants' test

---
+++ modules/apps/adaptive-media/adaptive-media-image-jax-rs-test/src/testIntegration/java/com/liferay/adaptive/media/image/jaxrs/client/test/internal/ImageAdaptiveMediaJaxRsMediaTest.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.adaptive.media.image.jaxrs.client.test.internal;
+
+
+import com.liferay.arquillian.deploymentscenario.annotations.BndFile;
+
+import java.net.URL;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Alejandro Hernández
+ */
+@BndFile("src/testIntegration/resources/bnd.bnd")
+@RunAsClient
+@RunWith(Arquillian.class)
+public class ImageAdaptiveMediaJaxRsMediaTest {
+
+	@Before
+	public void setUp() {
+
+	}
+
+
+	@ArquillianResource
+	private URL _context;
+
+}
