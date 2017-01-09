310880f93fa678dbdd6e5d62836e8d8ee236d3c4
drewbrokke <drew.brokke@liferay.com>
2017-01-09 14:30:45 -0800

    LPS-70031 portal-impl - test - adds test to make sure circular references are not allowed

--- portal-impl/test/integration/com/liferay/portal/service/OrganizationLocalServiceTest.java
+++ portal-impl/test/integration/com/liferay/portal/service/OrganizationLocalServiceTest.java
@@ -17,6 +17,7 @@ package com.liferay.portal.service;
 import com.liferay.asset.kernel.model.AssetEntry;
 import com.liferay.asset.kernel.service.AssetEntryLocalServiceUtil;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.OrganizationParentException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.model.ListTypeConstants;
@@ -608,6 +609,33 @@ public class OrganizationLocalServiceTest {
 
 		Assert.assertEquals(
 			organizationB.getGroupId(), groupAA.getParentGroupId());
+	}
+
+	@Test(expected = OrganizationParentException.class)
+	public void testNoCircularParentOrganization() throws Exception {
+		Organization organizationA = OrganizationTestUtil.addOrganization(
+			OrganizationConstants.DEFAULT_PARENT_ORGANIZATION_ID,
+			"Organization A", false);
+
+		Organization organizationAA = OrganizationTestUtil.addOrganization(
+			organizationA.getOrganizationId(), "Organization AA", false);
+
+		Organization organizationAAA = OrganizationTestUtil.addOrganization(
+			organizationAA.getOrganizationId(), "Organization AAA", false);
+
+		Organization organizationAAAA = OrganizationTestUtil.addOrganization(
+			organizationAAA.getOrganizationId(), "Organization AAAA", false);
+
+		_organizations.add(organizationAAAA);
+
+		_organizations.add(organizationAAA);
+		_organizations.add(organizationAA);
+		_organizations.add(organizationA);
+
+		organizationA.setParentOrganizationId(
+			organizationAAAA.getOrganizationId());
+
+		OrganizationTestUtil.updateOrganization(organizationA);
 	}
 
 	@Test
