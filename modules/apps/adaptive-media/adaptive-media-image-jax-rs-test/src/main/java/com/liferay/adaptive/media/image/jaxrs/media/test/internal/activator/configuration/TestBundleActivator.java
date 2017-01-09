068822db9b6b9d139e5891c9f42c611421daeb98
Alejandro Hernández <info@alejandrohdezma.com>
2017-01-09 16:41:08 +0100

    LPS-69511 Create TestBundleActivator for second array of tests

---
+++ modules/apps/adaptive-media/adaptive-media-image-jax-rs-test/src/main/java/com/liferay/adaptive/media/image/jaxrs/media/test/internal/activator/configuration/TestBundleActivator.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * <p>
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.adaptive.media.image.jaxrs.media.test.internal.activator.configuration;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author Alejandro Hernández
+ */
+public class TestBundleActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+
+	}
+
+}
