91fda486047fd54d6b09516113d1849304df5de4
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2017-01-13 13:20:31 +0100

    LPS-70142 Support updating ticket based on the ticket id

--- portal-impl/src/com/liferay/portal/service/impl/TicketLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/TicketLocalServiceImpl.java
@@ -76,4 +76,25 @@ public class TicketLocalServiceImpl extends TicketLocalServiceBaseImpl {
 		return ticketPersistence.findByKey(key);
 	}
 
+	@Override
+	public Ticket updateTicket(
+			long ticketId, String className, long classPK, int type,
+			String extraInfo, Date expirationDate)
+		throws PortalException {
+
+		long classNameId = classNameLocalService.getClassNameId(className);
+
+		Ticket ticket = ticketPersistence.findByPrimaryKey(ticketId);
+
+		ticket.setClassNameId(classNameId);
+		ticket.setClassPK(classPK);
+		ticket.setType(type);
+		ticket.setExtraInfo(extraInfo);
+		ticket.setExpirationDate(expirationDate);
+
+		ticketPersistence.update(ticket);
+
+		return ticket;
+	}
+
 }
