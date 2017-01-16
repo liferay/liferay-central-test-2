7a7f56b5c86d0a46ccc5cda1fadbce8fe1b42874
Pavel Savinov <pavel.savinov@liferay.com>
2017-01-16 11:53:18 +0100

    LPS-70110 add configuration prefix attribute for map provider selector

--- modules/apps/foundation/map/map-taglib/src/main/java/com/liferay/map/taglib/servlet/taglib/MapProviderSelectorTag.java
+++ modules/apps/foundation/map/map-taglib/src/main/java/com/liferay/map/taglib/servlet/taglib/MapProviderSelectorTag.java
@@ -30,6 +30,10 @@ import javax.servlet.jsp.PageContext;
  */
 public class MapProviderSelectorTag extends IncludeTag {
 
+	public void setConfigurationPrefix(String configurationPrefix) {
+		_configurationPrefix = configurationPrefix;
+	}
+
 	public void setMapProviderKey(String mapProviderKey) {
 		_mapProviderKey = mapProviderKey;
 	}
@@ -47,6 +51,7 @@ public class MapProviderSelectorTag extends IncludeTag {
 
 	@Override
 	protected void cleanUp() {
+		_configurationPrefix = null;
 		_mapProviderKey = null;
 		_name = null;
 	}
@@ -58,6 +63,9 @@ public class MapProviderSelectorTag extends IncludeTag {
 
 	@Override
 	protected void setAttributes(HttpServletRequest request) {
+		request.setAttribute(
+			"liferay-map:map-provider-selector:configurationPrefix",
+			_configurationPrefix);
 		request.setAttribute(
 			"liferay-map:map-provider-selector:mapProviderKey",
 			_mapProviderKey);
@@ -76,6 +84,7 @@ public class MapProviderSelectorTag extends IncludeTag {
 
 	private static final String _PAGE = "/map_provider_selector/page.jsp";
 
+	private String _configurationPrefix;
 	private String _mapProviderKey;
 	private String _name;
 
