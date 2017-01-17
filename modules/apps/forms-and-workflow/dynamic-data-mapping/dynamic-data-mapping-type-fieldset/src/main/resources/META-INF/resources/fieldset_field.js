e79ef95710cb2c75f5be2a6019a68e4390ae0ae5
Diogo Moreira Lins <diogo.moreira@liferay.com>
2017-01-16 21:51:36 -0300

    LPS-69555 New behavior for repeatable fieldset

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-fieldset/src/main/resources/META-INF/resources/fieldset_field.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-fieldset/src/main/resources/META-INF/resources/fieldset_field.js
@@ -1,14 +1,23 @@
 AUI.add(
 	'liferay-ddm-form-field-fieldset',
 	function(A) {
+		var Renderer = Liferay.DDM.Renderer;
+
+		var FieldSetUtil = Liferay.DDM.Field.FieldSetUtil;
+
+		var Util = Renderer.Util;
+
 		var FieldSetField = A.Component.create(
 			{
 				ATTRS: {
-					fields: {
-						setter: '_setFields',
-						state: true,
+					nestedFields: {
+						setter: '_setNestedFields',
 						validator: Array.isArray,
 						value: []
+					},
+
+					showLabel: {
+						value: true
 					},
 
 					type: {
@@ -22,25 +31,155 @@ AUI.add(
 
 				prototype: {
 
+					initializer: function() {
+						var instance = this;
+
+						instance._eventHandlers.push(
+							instance.after('contextChange', instance._afterContextChange)
+						);
+					},
+
+					copyConfiguration: function() {
+						var instance = this;
+
+						var config = FieldSetField.superclass.copyConfiguration.apply(instance, arguments);
+
+						config.context.nestedFields.forEach(
+							function(field) {
+								delete field.instanceId;
+								delete field.name;
+								delete field.value;
+							}
+						);
+
+						config.nestedFields = config.context.nestedFields;
+
+						return config;
+					},
+
+					getTemplateContext: function() {
+						var instance = this;
+
+						var repeatedIndex = instance.get('repeatedIndex');
+
+						var siblings = instance.getRepeatedSiblings();
+
+						var showBorderBottom = repeatedIndex === siblings.length - 1;
+
+						var showBorderTop = repeatedIndex === 0;
+
+						return A.merge(
+							FieldSetField.superclass.getTemplateContext.apply(instance, arguments),
+							{
+								showBorderBottom: showBorderBottom,
+								showBorderTop: showBorderTop,
+								showLabel: repeatedIndex === 0
+							}
+						);
+					},
+
 					getValue: function() {
 						return '';
+					},
+
+					removeChild: function(field) {
+						var instance = this;
+
+						var context = instance.get('context');
+
+						var nestedFields = context.nestedFields;
+
+						var index = instance.indexOf(field);
+
+						if (index > -1) {
+							nestedFields.splice(index, 1);
+
+							instance.set('nestedFields', nestedFields);
+						}
+					},
+
+					render: function() {
+						var instance = this;
+
+						FieldSetField.superclass.render.apply(instance, arguments);
+
+						instance.eachField(
+							function(field) {
+								field.render();
+							}
+						);
+
+						return instance;
+					},
+
+					repeat: function() {
+						var instance = this;
+
+						var siblings = instance.getRepeatedSiblings();
+
+						var newField = FieldSetField.superclass.repeat.apply(instance, arguments);
+
+						if (newField.get('repeatedIndex') === 1) {
+							siblings[0].render();
+						}
+						else if (newField.get('repeatedIndex') === siblings.length - 1) {
+							var index = siblings.indexOf(newField);
+
+							siblings[index - 1].render();
+						}
+
+						return newField;
 					},
 
 					setValue: function() {
 					},
 
+					_afterContextChange: function(event) {
+						var instance = this;
+
+						var nestedFields = event.newVal.nestedFields;
+
+						nestedFields.forEach(A.bind(instance._setNewNestedFieldContext, instance));
+					},
+
 					_createNestedField: function(config) {
 						var instance = this;
 
-						var fieldClass = instance.getFieldClass();
+						var FieldSetNestedField = FieldSetUtil.createFieldSetNestedField(config);
 
-						return new fieldClass(config);
+						var nestedFieldContext = A.merge(
+							config,
+							{
+								fieldName: Util.getFieldNameFromQualifiedName(config.name),
+								parent: instance,
+								portletNamespace: instance.get('portletNamespace'),
+								repeatedIndex: 0
+							}
+						);
+
+						return new FieldSetNestedField(nestedFieldContext);
 					},
 
-					_setFields: function(fields) {
+					_setNestedFields: function(nestedFields) {
 						var instance = this;
 
-						return fields.map(A.bind(instance._createNestedField, instance));
+						var componentFields = nestedFields.map(A.bind(instance._createNestedField, instance));
+
+						instance.set('fields', componentFields);
+					},
+
+					_setNewNestedFieldContext: function(nestedField) {
+						var instance = this;
+
+						if (nestedField.name) {
+							var name = Util.getFieldNameFromQualifiedName(nestedField.name);
+
+							var field = instance.getField(name);
+
+							if (field) {
+								field.set('context', nestedField);
+							}
+						}
 					}
 				}
 			}
@@ -50,6 +189,6 @@ AUI.add(
 	},
 	'',
 	{
-		requires: ['liferay-ddm-form-renderer-field']
+		requires: ['liferay-ddm-form-field-fieldset-util', 'liferay-ddm-form-renderer-field']
 	}
 );
