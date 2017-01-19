1073078190e4cb666fe0f65e73c12793a4015c5e
Rafael Praxedes <rafael.praxedes@liferay.com>
2017-01-19 17:23:03 -0300

    LPS-70097 Return the context value of a select field if there is no option element for it. This was necessary to send the selected value of a select field, that belongs to field settings(SideBar) and is the target of an autofill rule, to backend evaluation phase

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-select/src/main/resources/META-INF/resources/select_field.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-select/src/main/resources/META-INF/resources/select_field.js
@@ -125,6 +125,17 @@ AUI.add(
 							value = inputNode.val();
 						}
 
+						if (!value) {
+							var contextValue = instance._getContextValue();
+
+							var hasCorrespondentOption =
+								instance._hasCorrespondentOption(contextValue);
+
+							if (contextValue && !hasCorrespondentOption) {
+								value = contextValue;
+							}
+						}
+
 						return value;
 					},
 
@@ -237,6 +248,18 @@ AUI.add(
 						}
 					},
 
+					_getContextValue: function() {
+						var instance = this;
+
+						var contextValue = instance.get('value');
+
+						if (Lang.isArray(contextValue)) {
+							contextValue = contextValue[0];
+						}
+
+						return contextValue;
+					},
+
 					_getDataSourceType: function(value) {
 						if (Lang.isString(value)) {
 							try {
@@ -285,6 +308,24 @@ AUI.add(
 						var instance = this;
 
 						return instance.get('container').one('.' + CSS_SELECT_TRIGGER_ACTION);
+					},
+
+					_hasCorrespondentOption: function(value) {
+						var instance = this;
+
+						var hasCorrespondentOption = false;
+
+						var inputNode = instance.getInputNode();
+
+						inputNode.all('option').each(
+							function(optionNode) {
+								if (optionNode.val() === value) {
+									hasCorrespondentOption = true;
+								}
+							}
+						);
+
+						return hasCorrespondentOption;
 					},
 
 					_isClickingOutSide: function(event) {
