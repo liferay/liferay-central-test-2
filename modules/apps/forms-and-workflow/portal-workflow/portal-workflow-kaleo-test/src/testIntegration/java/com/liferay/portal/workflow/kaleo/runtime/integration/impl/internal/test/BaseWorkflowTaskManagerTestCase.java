afe28df502548c6ff4100538f111d1e05509483a
Inacio Nery <inacio.nery@liferay.com>
2017-01-19 13:37:54 -0300

    LPS-69532 Add integration test to WorkflowTaskManagerImpl

---
+++ modules/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-test/src/testIntegration/java/com/liferay/portal/workflow/kaleo/runtime/integration/impl/internal/test/BaseWorkflowTaskManagerTestCase.java
@@ -0,0 +1,298 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow.kaleo.runtime.integration.impl.internal.test;
+
+import com.liferay.blogs.model.BlogsEntry;
+import com.liferay.blogs.service.BlogsEntryLocalServiceUtil;
+import com.liferay.dynamic.data.lists.model.DDLRecord;
+import com.liferay.dynamic.data.lists.model.DDLRecordConstants;
+import com.liferay.dynamic.data.lists.model.DDLRecordSet;
+import com.liferay.dynamic.data.lists.model.DDLRecordSetConstants;
+import com.liferay.dynamic.data.lists.service.DDLRecordLocalServiceUtil;
+import com.liferay.dynamic.data.lists.service.DDLRecordSetLocalServiceUtil;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.storage.DDMFormFieldValue;
+import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
+import com.liferay.dynamic.data.mapping.storage.StorageType;
+import com.liferay.dynamic.data.mapping.test.util.DDMFormTestUtil;
+import com.liferay.dynamic.data.mapping.test.util.DDMFormValuesTestUtil;
+import com.liferay.dynamic.data.mapping.test.util.DDMStructureTestHelper;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.RoleConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.UserNotificationDeliveryConstants;
+import com.liferay.portal.kernel.model.UserNotificationEvent;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserGroupRoleLocalServiceUtil;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.service.UserNotificationEventLocalServiceUtil;
+import com.liferay.portal.kernel.service.WorkflowDefinitionLinkLocalServiceUtil;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.workflow.WorkflowException;
+import com.liferay.portal.kernel.workflow.WorkflowInstance;
+import com.liferay.portal.kernel.workflow.WorkflowInstanceManagerUtil;
+import com.liferay.portal.kernel.workflow.WorkflowTask;
+import com.liferay.portal.kernel.workflow.WorkflowTaskManagerUtil;
+import com.liferay.portal.test.log.CaptureAppender;
+import com.liferay.portal.test.log.Log4JLoggerTestUtil;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.apache.log4j.Level;
+
+import org.junit.Assert;
+import org.junit.Before;
+
+/**
+ * @author Inácio Nery
+ */
+public class BaseWorkflowTaskManagerTestCase {
+
+	@Before
+	public void setUp() throws Exception {
+		group = GroupTestUtil.addGroup();
+
+		serviceContext = ServiceContextTestUtil.getServiceContext(
+			group.getGroupId());
+
+		setUpUsers();
+	}
+
+	protected void activeSingleApproverWorkflow(String className, long classPK)
+		throws PortalException {
+
+		WorkflowDefinitionLinkLocalServiceUtil.updateWorkflowDefinitionLink(
+			adminUser.getUserId(), TestPropsValues.getCompanyId(),
+			group.getGroupId(), className, classPK, 0, "Single Approver@1");
+	}
+
+	protected BlogsEntry addBlogsEntry() throws PortalException {
+		try (CaptureAppender captureAppender =
+				Log4JLoggerTestUtil.configureLog4JLogger(
+					"com.liferay.util.mail.MailEngine", Level.OFF)) {
+
+			return BlogsEntryLocalServiceUtil.addEntry(
+				adminUser.getUserId(), StringUtil.randomString(),
+				StringUtil.randomString(), new Date(), serviceContext);
+		}
+	}
+
+	protected DDLRecord addRecord(DDLRecordSet recordSet)
+		throws PortalException {
+
+		try (CaptureAppender captureAppender =
+				Log4JLoggerTestUtil.configureLog4JLogger(
+					"com.liferay.util.mail.MailEngine", Level.OFF)) {
+
+			DDMForm ddmForm = DDMFormTestUtil.createDDMForm("TextField1");
+
+			DDMFormValues ddmFormValues = createDDMFormValues(ddmForm);
+
+			return DDLRecordLocalServiceUtil.addRecord(
+				adminUser.getUserId(), group.getGroupId(),
+				recordSet.getRecordSetId(),
+				DDLRecordConstants.DISPLAY_INDEX_DEFAULT, ddmFormValues,
+				serviceContext);
+		}
+	}
+
+	protected DDLRecordSet addRecordSet() throws Exception, PortalException {
+		DDMForm ddmForm = DDMFormTestUtil.createDDMForm("TextField1");
+
+		DDMStructureTestHelper ddmStructureTestHelper =
+			new DDMStructureTestHelper(
+				PortalUtil.getClassNameId(DDLRecordSet.class), group);
+
+		DDMStructure ddmStructure = ddmStructureTestHelper.addStructure(
+			ddmForm, StorageType.JSON.toString());
+
+		Map<Locale, String> nameMap = new HashMap<>();
+
+		nameMap.put(LocaleUtil.US, RandomTestUtil.randomString());
+
+		return DDLRecordSetLocalServiceUtil.addRecordSet(
+			adminUser.getUserId(), group.getGroupId(),
+			ddmStructure.getStructureId(), null, nameMap, null,
+			DDLRecordSetConstants.MIN_DISPLAY_ROWS_DEFAULT,
+			DDLRecordSetConstants.SCOPE_DYNAMIC_DATA_LISTS, serviceContext);
+	}
+
+	protected void approveWorkflowTask(User user) throws Exception {
+		try (CaptureAppender captureAppender =
+				Log4JLoggerTestUtil.configureLog4JLogger(
+					"com.liferay.util.mail.MailEngine", Level.OFF)) {
+			WorkflowTask workflowTask = getWorkflowTask();
+
+			PermissionChecker userPermissionChecker =
+				PermissionCheckerFactoryUtil.create(user);
+
+			PermissionThreadLocal.setPermissionChecker(userPermissionChecker);
+
+			WorkflowTaskManagerUtil.completeWorkflowTask(
+				group.getCompanyId(), user.getUserId(),
+				workflowTask.getWorkflowTaskId(), "approve", StringPool.BLANK,
+				null);
+		}
+	}
+
+	protected void assignWorkflowTaskToUser(User user, User assigneeUser)
+		throws Exception {
+
+		try (CaptureAppender captureAppender =
+				Log4JLoggerTestUtil.configureLog4JLogger(
+					"com.liferay.util.mail.MailEngine", Level.OFF)) {
+			WorkflowTask workflowTask = getWorkflowTask();
+
+			PermissionChecker userPermissionChecker =
+				PermissionCheckerFactoryUtil.create(user);
+
+			PermissionThreadLocal.setPermissionChecker(userPermissionChecker);
+
+			WorkflowTaskManagerUtil.assignWorkflowTaskToUser(
+				group.getCompanyId(), user.getUserId(),
+				workflowTask.getWorkflowTaskId(), assigneeUser.getUserId(),
+				StringPool.BLANK, null, null);
+		}
+	}
+
+	protected void checkUserNotificationEventsByUsers(User... users) {
+		for (User user : users) {
+			List<UserNotificationEvent> userNotificationEvents =
+				UserNotificationEventLocalServiceUtil.
+					getArchivedUserNotificationEvents(
+						user.getUserId(),
+						UserNotificationDeliveryConstants.TYPE_WEBSITE, false);
+
+			Assert.assertEquals(1, userNotificationEvents.size());
+
+			UserNotificationEvent userNotificationEvent =
+				userNotificationEvents.get(0);
+
+			userNotificationEvent.setArchived(true);
+
+			UserNotificationEventLocalServiceUtil.updateUserNotificationEvent(
+				userNotificationEvent);
+		}
+	}
+
+	protected void checkWorkflowInstance(String className, long classPK)
+		throws WorkflowException {
+
+		List<WorkflowInstance> workflowInstances =
+			WorkflowInstanceManagerUtil.getWorkflowInstances(
+				adminUser.getCompanyId(), adminUser.getUserId(), className,
+				classPK, true, QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
+
+		Assert.assertEquals(1, workflowInstances.size());
+	}
+
+	protected DDMFormValues createDDMFormValues(DDMForm ddmForm) {
+		DDMFormValues ddmFormValues = DDMFormValuesTestUtil.createDDMFormValues(
+			ddmForm);
+
+		DDMFormFieldValue ddmFormFieldValue =
+			DDMFormValuesTestUtil.createLocalizedDDMFormFieldValue(
+				"TextField1", StringPool.BLANK);
+
+		ddmFormValues.addDDMFormFieldValue(ddmFormFieldValue);
+
+		return ddmFormValues;
+	}
+
+	protected User createUser(String roleName) throws Exception {
+		User user = UserTestUtil.addUser(group.getGroupId());
+
+		Role role = RoleLocalServiceUtil.getRole(
+			TestPropsValues.getCompanyId(), roleName);
+
+		UserLocalServiceUtil.addRoleUser(role.getRoleId(), user);
+
+		long[] userIds = {user.getUserId()};
+
+		UserGroupRoleLocalServiceUtil.addUserGroupRoles(
+			userIds, group.getGroupId(), role.getRoleId());
+
+		return user;
+	}
+
+	protected void deactiveWorkflow(String className, long classPK)
+		throws PortalException {
+
+		WorkflowDefinitionLinkLocalServiceUtil.updateWorkflowDefinitionLink(
+			adminUser.getUserId(), TestPropsValues.getCompanyId(),
+			group.getGroupId(), className, classPK, 0, null);
+	}
+
+	protected WorkflowTask getWorkflowTask() throws WorkflowException {
+		List<WorkflowTask> workflowTasks =
+			WorkflowTaskManagerUtil.getWorkflowTasksBySubmittingUser(
+				adminUser.getCompanyId(), adminUser.getUserId(), false,
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
+
+		Assert.assertEquals(1, workflowTasks.size());
+
+		return workflowTasks.get(0);
+	}
+
+	protected void setUpUsers() throws Exception {
+		adminUser = createUser(RoleConstants.ADMINISTRATOR);
+
+		_users.add(adminUser);
+
+		portalContentReviewerUser = createUser(
+			RoleConstants.PORTAL_CONTENT_REVIEWER);
+
+		_users.add(portalContentReviewerUser);
+
+		siteAdminUser = createUser(RoleConstants.SITE_ADMINISTRATOR);
+
+		_users.add(siteAdminUser);
+	}
+
+	protected User adminUser;
+
+	@DeleteAfterTestRun
+	protected Group group;
+
+	protected User portalContentReviewerUser;
+	protected ServiceContext serviceContext;
+	protected User siteAdminUser;
+
+	@DeleteAfterTestRun
+	private final List<User> _users = new ArrayList<>();
+
+}
---
+++ modules/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-test/src/testIntegration/java/com/liferay/portal/workflow/kaleo/runtime/integration/impl/internal/test/WorkflowTaskManagerImplTest.java
@@ -0,0 +1,159 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow.kaleo.runtime.integration.impl.internal.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.blogs.model.BlogsEntry;
+import com.liferay.blogs.service.BlogsEntryLocalServiceUtil;
+import com.liferay.dynamic.data.lists.model.DDLRecord;
+import com.liferay.dynamic.data.lists.model.DDLRecordSet;
+import com.liferay.dynamic.data.lists.model.DDLRecordVersion;
+import com.liferay.dynamic.data.lists.service.DDLRecordLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Inácio Nery
+ */
+@RunWith(Arquillian.class)
+@Sync
+public class WorkflowTaskManagerImplTest
+	extends BaseWorkflowTaskManagerTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			SynchronousDestinationTestRule.INSTANCE);
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_originalPermissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+	}
+
+	@After
+	public void tearDown() throws PortalException {
+		PermissionThreadLocal.setPermissionChecker(_originalPermissionChecker);
+	}
+
+	@Test
+	public void testApproveWorkflowBlogsEntryAsSiteAdmin() throws Exception {
+		activeSingleApproverWorkflow(BlogsEntry.class.getName(), 0);
+
+		BlogsEntry blogsEntry = addBlogsEntry();
+
+		checkUserNotificationEventsByUsers(
+			adminUser, portalContentReviewerUser, siteAdminUser);
+
+		assignWorkflowTaskToUser(siteAdminUser, siteAdminUser);
+
+		approveWorkflowTask(siteAdminUser);
+
+		blogsEntry = BlogsEntryLocalServiceUtil.getBlogsEntry(
+			blogsEntry.getEntryId());
+
+		Assert.assertEquals(
+			WorkflowConstants.STATUS_APPROVED, blogsEntry.getStatus());
+
+		deactiveWorkflow(BlogsEntry.class.getName(), 0);
+	}
+
+	@Test
+	public void testApproveWorkflowDDLRecordAsAdmin() throws Exception {
+		DDLRecordSet recordSet = addRecordSet();
+
+		activeSingleApproverWorkflow(
+			DDLRecordSet.class.getName(), recordSet.getRecordSetId());
+
+		DDLRecord record = addRecord(recordSet);
+
+		checkUserNotificationEventsByUsers(
+			adminUser, portalContentReviewerUser, siteAdminUser);
+
+		assignWorkflowTaskToUser(adminUser, adminUser);
+
+		record = DDLRecordLocalServiceUtil.getRecord(record.getRecordId());
+
+		Assert.assertEquals(
+			WorkflowConstants.STATUS_PENDING, record.getStatus());
+
+		approveWorkflowTask(adminUser);
+
+		record = DDLRecordLocalServiceUtil.getRecord(record.getRecordId());
+
+		DDLRecordVersion recordVersion = record.getRecordVersion();
+
+		checkWorkflowInstance(
+			DDLRecord.class.getName(), recordVersion.getRecordVersionId());
+
+		Assert.assertEquals(
+			WorkflowConstants.STATUS_APPROVED, record.getStatus());
+
+		deactiveWorkflow(
+			DDLRecordSet.class.getName(), recordSet.getRecordSetId());
+	}
+
+	@Test
+	public void testAssignApproveWorkflowBlogsEntryAsPortalContentReviewer()
+		throws Exception {
+
+		activeSingleApproverWorkflow(BlogsEntry.class.getName(), 0);
+
+		BlogsEntry blogsEntry = addBlogsEntry();
+
+		checkUserNotificationEventsByUsers(
+			adminUser, portalContentReviewerUser, siteAdminUser);
+
+		assignWorkflowTaskToUser(portalContentReviewerUser, adminUser);
+
+		checkUserNotificationEventsByUsers(adminUser);
+
+		assignWorkflowTaskToUser(adminUser, portalContentReviewerUser);
+
+		checkUserNotificationEventsByUsers(portalContentReviewerUser);
+
+		approveWorkflowTask(portalContentReviewerUser);
+
+		blogsEntry = BlogsEntryLocalServiceUtil.getBlogsEntry(
+			blogsEntry.getEntryId());
+
+		Assert.assertEquals(
+			WorkflowConstants.STATUS_APPROVED, blogsEntry.getStatus());
+
+		deactiveWorkflow(BlogsEntry.class.getName(), 0);
+	}
+
+	private PermissionChecker _originalPermissionChecker;
+
+}
