df08e0fa744f4d9cf50f1787b1baa43c699e61c2
Chema Balsas <jose.balsas@liferay.com>
2017-01-23 15:36:37 -0800

    LPS-70271 Update build

--- modules/apps/frontend-theme-1975-london/frontend-theme-1975-london/gulpfile.js
+++ modules/apps/frontend-theme-1975-london/frontend-theme-1975-london/gulpfile.js
@@ -1,17 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
-var metal = require('gulp-metal');
 var liferayThemeTasks = require('liferay-theme-tasks');
-
-var srcBlob = 'src/**/*.es.js';
-var testBlob = 'test/**/*.es.js';
-
-metal.registerTasks({
-	buildSrc: [srcBlob],
-	formatGlobs: [srcBlob, testBlob],
-	lintGlobs: [srcBlob, testBlob]
-});
 
 liferayThemeTasks.registerTasks(
 	{
