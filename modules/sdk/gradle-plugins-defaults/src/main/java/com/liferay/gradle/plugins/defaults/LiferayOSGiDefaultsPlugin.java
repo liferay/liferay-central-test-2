ef37e490b18790081818f8fca1b368cf24f6399f
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-01-26 14:24:08 -0800

    LPS-69926 Make "compileInclude" dependencies available to tests

--- modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/LiferayOSGiDefaultsPlugin.java
+++ modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/LiferayOSGiDefaultsPlugin.java
@@ -1750,6 +1750,7 @@ public class LiferayOSGiDefaultsPlugin implements Plugin<Project> {
 
 		_configureConfigurationDefault(project);
 		_configureConfigurationJspC(project, liferayExtension);
+		_configureConfigurationTestCompile(project);
 
 		String projectPath = project.getPath();
 
@@ -1779,6 +1780,16 @@ public class LiferayOSGiDefaultsPlugin implements Plugin<Project> {
 				}
 
 			});
+	}
+
+	private void _configureConfigurationTestCompile(Project project) {
+		Configuration configuration = GradleUtil.getConfiguration(
+			project, JavaPlugin.TEST_COMPILE_CONFIGURATION_NAME);
+
+		Configuration compileIncludeConfiguration = GradleUtil.getConfiguration(
+			project, LiferayOSGiPlugin.COMPILE_INCLUDE_CONFIGURATION_NAME);
+
+		configuration.extendsFrom(compileIncludeConfiguration);
 	}
 
 	private void _configureConfigurationTransitive(
