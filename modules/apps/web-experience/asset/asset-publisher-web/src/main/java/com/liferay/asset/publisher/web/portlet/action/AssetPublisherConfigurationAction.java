78330385621dd976d9412479597fb99a6c4013a2
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-01-26 11:36:18 -0800

    LPS-69451 SF

--- modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/portlet/action/AssetPublisherConfigurationAction.java
+++ modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/portlet/action/AssetPublisherConfigurationAction.java
@@ -56,6 +56,7 @@ import com.liferay.portlet.PortletPreferencesImpl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -85,10 +86,10 @@ public class AssetPublisherConfigurationAction
 
 	@Override
 	public String getJspPath(HttpServletRequest request) {
-		String command = ParamUtil.getString(request, Constants.CMD);
+		String cmd = ParamUtil.getString(request, Constants.CMD);
 
-		if (Validator.isNotNull(command) && command.equals("edit_query_rule")) {
-			return "/edit_query_rule.jsp";
+		if (Objects.equals(cmd, "edit_query_rule")) {
+			return "/edit_field.jsp";
 		}
 
 		return "/configuration.jsp";
