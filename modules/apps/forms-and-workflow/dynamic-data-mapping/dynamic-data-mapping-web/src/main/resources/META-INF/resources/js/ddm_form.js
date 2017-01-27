4a237ea76c956ac6b4c1f5d3592977861c455174
Zsolt Szabo <zsolt.szabo@liferay.com>
2017-01-27 21:58:51 +0100

    LPS-70330 Clear button on link to page doesn't work

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -1670,8 +1670,6 @@ AUI.add(
 
 						instance._clearedModal = true;
 
-						instance._navbar.one('.active').removeClass('active');
-
 						instance.setValue('');
 
 						instance.set('selectedLayout', instance.get('selectedLayoutPath')[0]);
@@ -1893,6 +1891,8 @@ AUI.add(
 							listNode.on('scroll', instance._handleModalScroll, instance);
 						}
 						else if (instance._clearedModal) {
+							instance._navbar.one('.active').removeClass('active');
+
 							var activeClass = privateLayout ? '.private' : '.public';
 
 							instance._navbar.one(activeClass).addClass('active');
