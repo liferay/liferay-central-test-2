c468e2f4430bb64ae4b8004bdb57224fd5a51b44
shuyangzhou <shuyang.zhou@liferay.com>
2017-01-26 17:31:42 -0800

    LPS-70322 Avoid trying to get the logo, if the company is known not having it

--- portal-impl/src/com/liferay/portal/events/ServicePreAction.java
+++ portal-impl/src/com/liferay/portal/events/ServicePreAction.java
@@ -215,12 +215,16 @@ public class ServicePreAction extends Action {
 		int companyLogoHeight = 0;
 		int companyLogoWidth = 0;
 
-		Image companyLogoImage = ImageLocalServiceUtil.getCompanyLogo(
-			company.getLogoId());
+		long companyLogoId = company.getLogoId();
 
-		if (companyLogoImage != null) {
-			companyLogoHeight = companyLogoImage.getHeight();
-			companyLogoWidth = companyLogoImage.getWidth();
+		if (companyLogoId > 0) {
+			Image companyLogoImage = ImageLocalServiceUtil.getCompanyLogo(
+				companyLogoId);
+
+			if (companyLogoImage != null) {
+				companyLogoHeight = companyLogoImage.getHeight();
+				companyLogoWidth = companyLogoImage.getWidth();
+			}
 		}
 
 		String realCompanyLogo = companyLogo;
