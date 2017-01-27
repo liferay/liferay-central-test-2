12700edf76f82587e6d92e3842a97dd1414268cd
Andrew Kim <andrew.kim@liferay.com>
2017-01-27 11:29:11 -0800

    LRAMF-400 SF - update package path

--- modules/apps/foundation/alloy/alloy-mvc/src/main/java/com/liferay/alloy/mvc/AlloyFriendlyURLMapper.java
+++ modules/apps/foundation/alloy/alloy-mvc/src/main/java/com/liferay/alloy/mvc/AlloyFriendlyURLMapper.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.alloy.mvc.internal;
+package com.liferay.alloy.mvc;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
