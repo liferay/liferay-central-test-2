1bb22ad50a03650bdb96b072429e03ef66d7fcb5
Julio Camarero <julio.camarero@liferay.com>
2017-01-27 18:07:16 -0800

    LPS-70342 return empty String if the service is not present

--- portal-kernel/src/com/liferay/portal/kernel/servlet/PortalWebResourcesUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/servlet/PortalWebResourcesUtil.java
@@ -15,6 +15,7 @@
 package com.liferay.portal.kernel.servlet;
 
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceReference;
@@ -50,6 +51,10 @@ public class PortalWebResourcesUtil {
 	public static String getModuleContextPath(String resourceType) {
 		PortalWebResources portalWebResources = _portalWebResourcesMap.get(
 			resourceType);
+
+		if (portalWebResources == null) {
+			return StringPool.BLANK;
+		}
 
 		return portalWebResources.getContextPath();
 	}
