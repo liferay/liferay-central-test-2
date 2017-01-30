d75eb190528a34c5132bfa9a4a939525a2610660
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-01-30 14:56:25 -0800

    LPS-70362 Download Liferay bundles via CDN

--- modules/util/portal-tools-bundle-support/src/main/java/com/liferay/portal/tools/bundle/support/maven/AbstractBundleMojo.java
+++ modules/util/portal-tools-bundle-support/src/main/java/com/liferay/portal/tools/bundle/support/maven/AbstractBundleMojo.java
@@ -36,7 +36,7 @@ public abstract class AbstractBundleMojo extends AbstractLiferayMojo {
 	protected int stripComponents;
 
 	@Parameter(
-		defaultValue = "https://sourceforge.net/projects/lportal/files/Liferay%20Portal/7.0.2%20GA3/liferay-ce-portal-tomcat-7.0-ga3-20160804222206210.zip",
+		defaultValue = "https://cdn.lfrs.sl/releases.liferay.com/portal/7.0.2-ga3/liferay-ce-portal-tomcat-7.0-ga3-20160804222206210.zip",
 		required = true
 	)
 	protected URL url;
