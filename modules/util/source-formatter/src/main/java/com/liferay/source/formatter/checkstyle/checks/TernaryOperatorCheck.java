7fcf4e27ad2844e1d8755a1e9f4be1e3018c1395
Hugo Huijser <hugo.huijser@liferay.com>
2017-01-31 15:52:55 +0800

    LPS-67352 Avoid using ternary logical operators over multiple lines for better readability

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/TernaryOperatorCheck.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checkstyle.checks;
+
+import com.liferay.source.formatter.checkstyle.util.DetailASTUtil;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+/**
+ * @author Hugo Huijser
+ */
+public class TernaryOperatorCheck extends AbstractCheck {
+
+	public static final String MSG_AVOID_TERNARY_OPERATOR =
+		"ternary.operator.avoid";
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] {TokenTypes.QUESTION};
+	}
+
+	@Override
+	public void visitToken(DetailAST detailAST) {
+		if (DetailASTUtil.getStartLine(detailAST) !=
+				DetailASTUtil.getEndLine(detailAST)) {
+
+			log(detailAST.getLineNo(), MSG_AVOID_TERNARY_OPERATOR);
+		}
+	}
+
+}
