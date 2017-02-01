ff5f2f222fb67ccc5516975ca90dccf18494ba8d
Leonardo Barros <leonardo.barros@liferay.com>
2017-02-01 10:49:25 -0300

    LPS-70395 Create a class to hold form evaluation context

---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-evaluator/src/main/java/com/liferay/dynamic/data/mapping/form/evaluator/DDMFormEvaluatorContext.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.evaluator;
+
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Leonardo Barros
+ */
+public class DDMFormEvaluatorContext {
+
+	public DDMFormEvaluatorContext(
+		DDMForm ddmForm, DDMFormValues ddmFormValues, Locale locale) {
+
+		_ddmForm = ddmForm;
+		_ddmFormValues = ddmFormValues;
+		_locale = locale;
+	}
+
+	public DDMForm getDDMForm() {
+		return _ddmForm;
+	}
+
+	public DDMFormValues getDDMFormValues() {
+		return _ddmFormValues;
+	}
+
+	public Locale getLocale() {
+		return _locale;
+	}
+
+	public Map<String, Object> getProperties() {
+		return _properties;
+	}
+
+	public void setDDMForm(DDMForm ddmForm) {
+		_ddmForm = ddmForm;
+	}
+
+	public void setDDMFormValues(DDMFormValues ddmFormValues) {
+		_ddmFormValues = ddmFormValues;
+	}
+
+	public void setLocale(Locale locale) {
+		_locale = locale;
+	}
+
+	private DDMForm _ddmForm;
+	private DDMFormValues _ddmFormValues;
+	private Locale _locale;
+	private final Map<String, Object> _properties = new HashMap<>();
+
+}
