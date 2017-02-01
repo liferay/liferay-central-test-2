9bf57ce8060cf9d6cedc532f340bbd7a692ac84c
Miguel Ángel Pastor Olivar <miguel.pastor@liferay.com>
2017-02-01 08:09:14 +0100

    LPS-37417 Set the capacity of the StringBundler

--- portal-impl/src/com/liferay/portal/upgrade/v7_0_3/UpgradeExpando.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_3/UpgradeExpando.java
@@ -27,7 +27,7 @@ import java.sql.ResultSet;
 public class UpgradeExpando extends UpgradeProcess {
 
 	protected void deleteOrphanExpandoRow() throws Exception {
-		StringBundler sb = new StringBundler();
+		StringBundler sb = new StringBundler(5);
 
 		sb.append("select rowId_ from ExpandoRow ");
 		sb.append("where rowId_ not in ");
