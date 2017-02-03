ac0eb0bbeb3e5be6cca6ba241dc60bc3b2f992cf
Leonardo Barros <leonardo.barros@liferay.com>
2017-02-03 13:34:35 -0300

    LPS-69988 buildSoy

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/templates/rule_builder.soy.js
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/templates/rule_builder.soy.js
@@ -21,25 +21,25 @@ ddl.rule_list = function(opt_data, opt_ignored) {
   var output = '';
   if (opt_data.rules.length > 0) {
     output += '<ul class="ddl-form-body-content form-builder-rule-builder-rules-list tabular-list-group">';
-    var ruleList196 = opt_data.rules;
-    var ruleListLen196 = ruleList196.length;
-    for (var ruleIndex196 = 0; ruleIndex196 < ruleListLen196; ruleIndex196++) {
-      var ruleData196 = ruleList196[ruleIndex196];
+    var ruleList198 = opt_data.rules;
+    var ruleListLen198 = ruleList198.length;
+    for (var ruleIndex198 = 0; ruleIndex198 < ruleListLen198; ruleIndex198++) {
+      var ruleData198 = ruleList198[ruleIndex198];
       output += '<li class="list-group-item"><div class="clamp-horizontal list-group-item-content"><p class="form-builder-rule-builder-rule-description text-default"><b>If </b>';
-      var conditionList174 = ruleData196.conditions;
-      var conditionListLen174 = conditionList174.length;
-      for (var conditionIndex174 = 0; conditionIndex174 < conditionListLen174; conditionIndex174++) {
-        var conditionData174 = conditionList174[conditionIndex174];
-        output += ddl.condition({operandType: conditionData174.operands[0].type, operandValue: conditionData174.operands[0].label}) + '<b class="text-lowercase"><em> ' + soy.$$escapeHtml(opt_data.strings[conditionData174.operator]) + ' </em></b>' + ddl.condition({operandType: conditionData174.operands[1].type, operandValue: conditionData174.operands[1].label != null ? conditionData174.operands[1].label : conditionData174.operands[1].value}) + ((! (conditionIndex174 == conditionListLen174 - 1)) ? ', <br /><b> and </b>' : '');
+      var conditionList176 = ruleData198.conditions;
+      var conditionListLen176 = conditionList176.length;
+      for (var conditionIndex176 = 0; conditionIndex176 < conditionListLen176; conditionIndex176++) {
+        var conditionData176 = conditionList176[conditionIndex176];
+        output += ddl.condition({operandType: conditionData176.operands[0].type, operandValue: conditionData176.operands[0].label}) + '<b class="text-lowercase"><em> ' + soy.$$escapeHtml(opt_data.strings[conditionData176.operator]) + ' </em></b>' + ddl.condition({operandType: conditionData176.operands[1].type, operandValue: conditionData176.operands[1].label != null ? conditionData176.operands[1].label : conditionData176.operands[1].value}) + ((! (conditionIndex176 == conditionListLen176 - 1)) ? '<br /><b> ' + soy.$$escapeHtml(conditionData176.logicOperator) + ' </b>' : '');
       }
       output += '<br />';
-      var actionList182 = ruleData196.actions;
-      var actionListLen182 = actionList182.length;
-      for (var actionIndex182 = 0; actionIndex182 < actionListLen182; actionIndex182++) {
-        var actionData182 = actionList182[actionIndex182];
-        output += ddl.action({action: actionData182}) + ((! (actionIndex182 == actionListLen182 - 1)) ? ', <br /><b> and </b>' : '');
+      var actionList184 = ruleData198.actions;
+      var actionListLen184 = actionList184.length;
+      for (var actionIndex184 = 0; actionIndex184 < actionListLen184; actionIndex184++) {
+        var actionData184 = actionList184[actionIndex184];
+        output += ddl.action({action: actionData184}) + ((! (actionIndex184 == actionListLen184 - 1)) ? ', <br /><b> and </b>' : '');
       }
-      output += '</p></div><div class="list-group-item-field"><div class="card-col-field"><div class="dropdown"><a class="dropdown-toggle icon-monospaced" data-toggle="dropdown" href="#1">' + soy.$$filterNoAutoescape(opt_data.kebab) + '</a><ul class="dropdown-menu dropdown-menu-right"><li class="rule-card-edit" data-card-id="' + soy.$$escapeHtmlAttribute(ruleIndex196) + '"><a href="javascript:;">' + soy.$$escapeHtml(opt_data.strings.edit) + '</a></li><li class="rule-card-delete" data-card-id="' + soy.$$escapeHtmlAttribute(ruleIndex196) + '"><a href="javascript:;">' + soy.$$escapeHtml(opt_data.strings['delete']) + '</a></li></ul></div></div></div></li>';
+      output += '</p></div><div class="list-group-item-field"><div class="card-col-field"><div class="dropdown"><a class="dropdown-toggle icon-monospaced" data-toggle="dropdown" href="#1">' + soy.$$filterNoAutoescape(opt_data.kebab) + '</a><ul class="dropdown-menu dropdown-menu-right"><li class="rule-card-edit" data-card-id="' + soy.$$escapeHtmlAttribute(ruleIndex198) + '"><a href="javascript:;">' + soy.$$escapeHtml(opt_data.strings.edit) + '</a></li><li class="rule-card-delete" data-card-id="' + soy.$$escapeHtmlAttribute(ruleIndex198) + '"><a href="javascript:;">' + soy.$$escapeHtml(opt_data.strings['delete']) + '</a></li></ul></div></div></div></li>';
     }
     output += '</ul>';
   } else {
---
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/notification/form_entry_add_body.soy.js
@@ -0,0 +1,55 @@
+// This file was automatically generated from form_entry_add_body.soy.
+// Please don't edit this file by hand.
+
+/**
+ * @fileoverview Templates in namespace form.
+ * @public
+ */
+
+if (typeof form == 'undefined') { var form = {}; }
+
+
+form.field = function(opt_data, opt_ignored) {
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml((opt_data.field != null) ? '<p style="color: #9aa2a6; font-size: 18px; margin: 32px 0 16px 0;">' + soy.$$escapeHtml(opt_data.field.label) + '</p><p style="font-size: 16px; margin: 0;">' + soy.$$escapeHtml(opt_data.field.value) + '</p>' : '');
+};
+if (goog.DEBUG) {
+  form.field.soyTemplateName = 'form.field';
+}
+
+
+form.fields = function(opt_data, opt_ignored) {
+  var output = '';
+  var fieldList278 = opt_data.fields;
+  var fieldListLen278 = fieldList278.length;
+  for (var fieldIndex278 = 0; fieldIndex278 < fieldListLen278; fieldIndex278++) {
+    var fieldData278 = fieldList278[fieldIndex278];
+    output += form.field(soy.$$augmentMap(opt_data, {field: fieldData278}));
+  }
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml(output);
+};
+if (goog.DEBUG) {
+  form.fields.soyTemplateName = 'form.fields';
+}
+
+
+form.form_entry = function(opt_data, opt_ignored) {
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml('<!DOCTYPE html><html><head><meta charset="UTF-8" /><title>' + soy.$$escapeHtmlRcdata(opt_data.formName) + '</title></head><body style="margin: 0; overflow: visible; padding: 0;"><div id="container" style="font-family: helvetica, \'open sans\', arial; margin: 0 auto 40px auto; width: 660px;"><table style="background-color: #e4e9ec; padding: 40px;"><tr><td><div id="introduction" style="background-color: #fff; border-radius: 4px; margin: 0 auto 24px auto; padding: 40px;"><h1 align="center" style="margin-bottom: 24px; margin-top: 0;">' + soy.$$escapeHtml(opt_data.siteName) + '</h1><h2 align="center" style="margin-bottom: 24px; margin-top: 0;">Hi, ' + soy.$$escapeHtml(opt_data.authorName) + '</h2><p style="line-height: 30px; margin-bottom: 24px; margin-top: 0;"><b>' + soy.$$escapeHtml(opt_data.userName) + '</b> submitted an entry for the ' + soy.$$escapeHtml(opt_data.formName) + ' form in the ' + soy.$$escapeHtml(opt_data.siteName) + ' site. View all the form\'s entries by clicking <a href="' + soy.$$escapeHtmlAttribute(soy.$$filterNormalizeUri(opt_data.viewFormEntriesURL)) + '" style="color: #7bc4f4; text-decoration: none;" target="_blank">here</a>.</p><a href="' + soy.$$escapeHtmlAttribute(soy.$$filterNormalizeUri(opt_data.viewFormURL)) + '" style="background: #65b4f1; border-radius: 4px; color: #fff; display: block; padding: 18px; text-align: center; text-decoration: none;" target="_blank">Click here to access the form</a></div></td></tr><tr><td><h3 style="color: #9aa2a6; font-weight: 300; margin: 8px 0 8px; text-align: center;">Here\'s what <b>' + soy.$$escapeHtml(opt_data.userName) + '</b> entered into the form:</h3>' + form.pages(opt_data) + '<a href="' + soy.$$escapeHtmlAttribute(soy.$$filterNormalizeUri(opt_data.viewFormURL)) + '" style="background: #65b4f1; border-radius: 4px; color: #fff; display: block; padding: 18px; text-align: center; text-decoration: none;" target="_blank">Click here to access the form</a></td></tr></table></div></body></html>');
+};
+if (goog.DEBUG) {
+  form.form_entry.soyTemplateName = 'form.form_entry';
+}
+
+
+form.pages = function(opt_data, opt_ignored) {
+  var output = '';
+  var pageList312 = opt_data.pages;
+  var pageListLen312 = pageList312.length;
+  for (var pageIndex312 = 0; pageIndex312 < pageListLen312; pageIndex312++) {
+    var pageData312 = pageList312[pageIndex312];
+    output += '<div style="background-color: #fff; border-radius: 4px; margin: 0 auto 24px auto; padding: 40px;"><h4 style="color: #9aa2a6; font-size: 21px; font-weight: 500; margin: 0;">' + soy.$$escapeHtml(pageData312.title) + '</h4>' + form.fields(soy.$$augmentMap(opt_data, {fields: pageData312.fields})) + '</div>';
+  }
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml(output);
+};
+if (goog.DEBUG) {
+  form.pages.soyTemplateName = 'form.pages';
+}
