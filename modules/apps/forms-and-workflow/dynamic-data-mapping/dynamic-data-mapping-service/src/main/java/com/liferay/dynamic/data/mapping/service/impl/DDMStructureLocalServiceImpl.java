ced2895b9634f2374b1563e5effe1f1542f596fc
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-02-03 11:02:48 +0100

    LPS-70421 Javadoc

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMStructureLocalServiceImpl.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMStructureLocalServiceImpl.java
@@ -1005,6 +1005,31 @@ public class DDMStructureLocalServiceImpl
 			groupId, classNameId, start, end, orderByComparator);
 	}
 
+	/**
+	 * Returns an ordered range of all the structures matching the group,
+	 * class name ID, name, and description.
+	 *
+	 * <p>
+	 * Useful when paginating results. Returns a maximum of <code>end -
+	 * start</code> instances. <code>start</code> and <code>end</code> are not
+	 * primary keys, they are indexes in the result set. Thus, <code>0</code>
+	 * refers to the first result in the set. Setting both <code>start</code>
+	 * and <code>end</code> to {@link QueryUtil#ALL_POS} will return the full
+	 * result set.
+	 * </p>
+	 *
+	 * @param  groupId the primary key of the group
+	 * @param  classNameId the primary key of the class name for the structure's
+	 *         related model
+	 * @param  name the name keywords
+	 * @param  description the description keywords
+	 * @param  start the lower bound of the range of structures to return
+	 * @param  end the upper bound of the range of structures to return (not
+	 *         inclusive)
+	 * @param  orderByComparator the comparator to order the structures
+	 *         (optionally <code>null</code>)
+	 * @return the range of matching structures ordered by the comparator
+	 */
 	@Override
 	public List<DDMStructure> getStructures(
 		long groupId, long classNameId, String name, String description,
