d00dc97f8816386c5d762cd90a2db844c996723d
Thiago Rocha <thiago.rocha@liferay.com>
2017-02-03 16:52:56 -0300

    LPS-70446 Creating a function to filter the field list based on the type

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_builder_action.js
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_builder_action.js
@@ -1,6 +1,8 @@
 AUI.add(
 	'liferay-ddl-form-builder-action',
 	function(A) {
+		var listType = ['select', 'radio', 'options', 'checkbox'];
+
 		var FormBuilderAction = A.Component.create(
 			{
 				ATTRS: {
@@ -22,6 +24,27 @@ AUI.add(
 				NAME: 'liferay-ddl-form-builder-action',
 
 				prototype: {
+					getFieldsByType: function(type) {
+						var instance = this;
+
+						var fields = instance.get('fields');
+
+						var fieldsFiltered = [];
+
+						for (var i = 0; i < fields.length; i++) {
+							if (type === 'list') {
+								if (listType.indexOf(fields[i].type) !== -1) {
+									fieldsFiltered.push(fields[i]);
+								}
+							}
+							else if (type) {
+								fieldsFiltered.push(fields[i]);
+							}
+						}
+
+						return fieldsFiltered;
+					},
+
 					getValue: function() {}
 				}
 			}
