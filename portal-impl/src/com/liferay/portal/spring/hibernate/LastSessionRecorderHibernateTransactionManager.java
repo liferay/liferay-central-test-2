3c9ce3d68221a68412f16ed7776f7e6ba354b699
shuyangzhou <shuyang.zhou@liferay.com>
2017-02-05 11:33:15 -0800

    LPS-70458 Knock out TransactionSynchronizationManager logger

--- portal-impl/src/com/liferay/portal/spring/hibernate/LastSessionRecorderHibernateTransactionManager.java
+++ portal-impl/src/com/liferay/portal/spring/hibernate/LastSessionRecorderHibernateTransactionManager.java
@@ -14,8 +14,15 @@
 
 package com.liferay.portal.spring.hibernate;
 
+import com.liferay.portal.kernel.util.ReflectionUtil;
+
+import java.lang.reflect.Field;
+
+import org.apache.commons.logging.Log;
+
 import org.springframework.orm.hibernate3.HibernateTransactionManager;
 import org.springframework.orm.hibernate3.SessionHolder;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 /**
  * @author Shuyang Zhou
@@ -38,9 +45,96 @@ public class LastSessionRecorderHibernateTransactionManager
 	static {
 		try {
 			Class.forName(SpringHibernateThreadLocalUtil.class.getName());
+
+			Field loggerField = ReflectionUtil.getDeclaredField(
+				TransactionSynchronizationManager.class, "logger");
+
+			loggerField.set(
+				null,
+				new Log() {
+
+					@Override
+					public void debug(Object o) {
+					}
+
+					@Override
+					public void debug(Object o, Throwable t) {
+					}
+
+					@Override
+					public void error(Object o) {
+					}
+
+					@Override
+					public void error(Object o, Throwable t) {
+					}
+
+					@Override
+					public void fatal(Object o) {
+					}
+
+					@Override
+					public void fatal(Object o, Throwable t) {
+					}
+
+					@Override
+					public void info(Object o) {
+					}
+
+					@Override
+					public void info(Object o, Throwable t) {
+					}
+
+					@Override
+					public boolean isDebugEnabled() {
+						return false;
+					}
+
+					@Override
+					public boolean isErrorEnabled() {
+						return false;
+					}
+
+					@Override
+					public boolean isFatalEnabled() {
+						return false;
+					}
+
+					@Override
+					public boolean isInfoEnabled() {
+						return false;
+					}
+
+					@Override
+					public boolean isTraceEnabled() {
+						return false;
+					}
+
+					@Override
+					public boolean isWarnEnabled() {
+						return false;
+					}
+
+					@Override
+					public void trace(Object o) {
+					}
+
+					@Override
+					public void trace(Object o, Throwable t) {
+					}
+
+					@Override
+					public void warn(Object o) {
+					}
+
+					@Override
+					public void warn(Object o, Throwable t) {
+					}
+
+				});
 		}
-		catch (ClassNotFoundException cnfe) {
-			throw new ExceptionInInitializerError(cnfe);
+		catch (Exception e) {
+			throw new ExceptionInInitializerError(e);
 		}
 	}
 
