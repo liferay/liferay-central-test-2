170b409083f87699862e2cd6e1046265e4084b12
Adolfo Pérez <adolfo.perez@liferay.com>
2017-02-06 15:36:32 +0100

    LPS-69255 Call UpgradeMVCCVersion from UpgradeSchema

--- portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_7_0_3.java
+++ portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_7_0_3.java
@@ -18,12 +18,12 @@ import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.ReleaseInfo;
 import com.liferay.portal.upgrade.v7_0_3.UpgradeExpando;
 import com.liferay.portal.upgrade.v7_0_3.UpgradeGroup;
-import com.liferay.portal.upgrade.v7_0_3.UpgradeMVCCVersion;
 import com.liferay.portal.upgrade.v7_0_3.UpgradeMessageBoards;
 import com.liferay.portal.upgrade.v7_0_3.UpgradeModules;
 import com.liferay.portal.upgrade.v7_0_3.UpgradeOracle;
 import com.liferay.portal.upgrade.v7_0_3.UpgradeOrganization;
 import com.liferay.portal.upgrade.v7_0_3.UpgradeSQLServer;
+import com.liferay.portal.upgrade.v7_0_3.UpgradeSchema;
 import com.liferay.portal.upgrade.v7_0_3.UpgradeSybase;
 
 /**
@@ -38,11 +38,12 @@ public class UpgradeProcess_7_0_3 extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
+		upgrade(UpgradeSchema.class);
+
 		upgrade(UpgradeExpando.class);
 		upgrade(UpgradeGroup.class);
 		upgrade(UpgradeMessageBoards.class);
 		upgrade(UpgradeModules.class);
-		upgrade(UpgradeMVCCVersion.class);
 		upgrade(UpgradeOrganization.class);
 		upgrade(UpgradeOracle.class);
 		upgrade(UpgradeSQLServer.class);
