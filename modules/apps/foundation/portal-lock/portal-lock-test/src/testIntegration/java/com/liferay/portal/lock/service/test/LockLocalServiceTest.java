db6b2844db068a13425df68f8cd6308a3fbe340b
shuyangzhou <shuyang.zhou@liferay.com>
2017-02-07 11:08:10 -0800

    LPS-69306 Update test to cover new service method

--- modules/apps/foundation/portal-lock/portal-lock-test/src/testIntegration/java/com/liferay/portal/lock/service/test/LockLocalServiceTest.java
+++ modules/apps/foundation/portal-lock/portal-lock-test/src/testIntegration/java/com/liferay/portal/lock/service/test/LockLocalServiceTest.java
@@ -18,6 +18,13 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.dao.db.DB;
 import com.liferay.portal.kernel.dao.db.DBManagerUtil;
 import com.liferay.portal.kernel.dao.db.DBType;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.ReflectionUtil;
+import com.liferay.portal.kernel.util.Time;
+import com.liferay.portal.lock.exception.DuplicateLockException;
 import com.liferay.portal.lock.model.Lock;
 import com.liferay.portal.lock.service.LockLocalServiceUtil;
 import com.liferay.portal.test.rule.ExpectedDBType;
@@ -29,13 +36,18 @@ import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import java.sql.BatchUpdateException;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.hibernate.exception.GenericJDBCException;
 import org.hibernate.util.JDBCExceptionReporter;
 
@@ -45,6 +57,11 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
 
 /**
  * @author Shuyang Zhou
@@ -60,6 +77,191 @@ public class LockLocalServiceTest {
 	@Before
 	public void setUp() {
 		LockLocalServiceUtil.unlock("className", "key");
+	}
+
+	@ExpectedLogs(
+		expectedLogs = {
+			@ExpectedLog(
+				expectedDBType = ExpectedDBType.DB2,
+				expectedLog = "Error for batch element",
+				expectedType = ExpectedType.PREFIX
+			),
+			@ExpectedLog(
+				expectedDBType = ExpectedDBType.DB2,
+				expectedLog = "[jcc][t4][102][10040][4.16.53] Batch failure.",
+				expectedType = ExpectedType.PREFIX
+			),
+			@ExpectedLog(
+				expectedDBType = ExpectedDBType.HYPERSONIC,
+				expectedLog = "integrity constraint violation: unique constraint or index violation: IX_228562AD",
+				expectedType = ExpectedType.EXACT
+			),
+			@ExpectedLog(
+				expectedDBType = ExpectedDBType.MYSQL,
+				expectedLog = "Deadlock found when trying to get lock; try restarting transaction",
+				expectedType = ExpectedType.EXACT
+			),
+			@ExpectedLog(
+				expectedDBType = ExpectedDBType.MYSQL,
+				expectedLog = "Duplicate entry",
+				expectedType = ExpectedType.PREFIX
+			),
+			@ExpectedLog(
+				expectedDBType = ExpectedDBType.ORACLE,
+				expectedLog = "ORA-00001: unique constraint",
+				expectedType = ExpectedType.PREFIX
+			),
+			@ExpectedLog(
+				expectedDBType = ExpectedDBType.POSTGRESQL,
+				expectedLog = "Batch entry 0 insert into Lock_ ",
+				expectedType = ExpectedType.PREFIX
+			),
+			@ExpectedLog(
+				expectedDBType = ExpectedDBType.POSTGRESQL,
+				expectedLog = "ERROR: duplicate key value violates unique constraint ",
+				expectedType = ExpectedType.PREFIX
+			),
+			@ExpectedLog(
+				expectedDBType = ExpectedDBType.SYBASE,
+				expectedLog = "Attempt to insert duplicate key row",
+				expectedType = ExpectedType.CONTAINS
+			)
+		},
+		level = "ERROR", loggerClass = JDBCExceptionReporter.class
+	)
+	@Test
+	public void testLock() throws Exception {
+		final long userId = TestPropsValues.getUserId();
+
+		// Lock is new
+
+		Lock lock = LockLocalServiceUtil.lock(
+			userId, "className", "key", "owner", false, Time.DAY, false);
+
+		Assert.assertTrue(lock.isNew());
+
+		Date expirationDate = lock.getExpirationDate();
+
+		// Lock already exist, don't renew
+
+		lock = LockLocalServiceUtil.lock(
+			userId, "className", "key", "owner", false, Time.DAY, false);
+
+		Assert.assertFalse(lock.isNew());
+		Assert.assertEquals(expirationDate, lock.getExpirationDate());
+
+		// Lock already exist, renew
+
+		lock = LockLocalServiceUtil.lock(
+			userId, "className", "key", "owner", false, 2 * Time.DAY, true);
+
+		Assert.assertFalse(lock.isNew());
+		Assert.assertTrue(expirationDate.before(lock.getExpirationDate()));
+
+		// Duplicate lock exception
+
+		try {
+			LockLocalServiceUtil.lock(
+				0, "className", "key", "owner", false, Time.DAY, false);
+
+			Assert.fail();
+		}
+		catch (DuplicateLockException dle) {
+		}
+
+		// Set lock to be expired
+
+		expirationDate = new Date(System.currentTimeMillis() - 10 * Time.DAY);
+
+		lock.setExpirationDate(expirationDate);
+
+		lock = LockLocalServiceUtil.updateLock(lock);
+
+		Assert.assertEquals(expirationDate, lock.getExpirationDate());
+
+		// Lock with auto expiration
+
+		lock = LockLocalServiceUtil.lock(
+			userId, "className", "key", "owner", false, Time.DAY, false);
+
+		Assert.assertTrue(lock.isNew());
+		Assert.assertTrue(expirationDate.before(lock.getExpirationDate()));
+
+		// Remove lock
+
+		LockLocalServiceUtil.unlock("className", "key");
+
+		// Concurrent locking
+
+		Bundle bundle = FrameworkUtil.getBundle(LockLocalServiceTest.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		final Thread testThread = Thread.currentThread();
+
+		final CountDownLatch createdCountDownLatch = new CountDownLatch(1);
+		final CountDownLatch continueCountDownLatch = new CountDownLatch(1);
+
+		ServiceRegistration<ModelListener> serviceRegistration =
+			bundleContext.registerService(
+				ModelListener.class,
+				new BaseModelListener<Lock>() {
+
+					@Override
+					public void onAfterCreate(Lock model) {
+						if (Thread.currentThread() != testThread) {
+							createdCountDownLatch.countDown();
+
+							try {
+								continueCountDownLatch.await();
+							}
+							catch (InterruptedException ie) {
+								ReflectionUtil.throwException(ie);
+							}
+						}
+					}
+
+				},
+				null);
+
+		FutureTask<Lock> futureTask = new FutureTask<>(
+			new Callable<Lock>() {
+
+				@Override
+				public Lock call() throws PortalException {
+					return LockLocalServiceUtil.lock(
+						userId, "className", "key", "owner", false, Time.DAY,
+						false);
+				}
+
+			});
+
+		Thread lockCreateThread = new Thread(futureTask, "Lock create thread");
+
+		lockCreateThread.start();
+
+		createdCountDownLatch.await();
+
+		serviceRegistration.unregister();
+
+		lock = LockLocalServiceUtil.lock(
+			userId, "className", "key", "owner", false, Time.DAY, false);
+
+		Assert.assertTrue(lock.isNew());
+
+		continueCountDownLatch.countDown();
+
+		try {
+			futureTask.get();
+
+			Assert.fail();
+		}
+		catch (ExecutionException ee) {
+			Throwable throwable = ee.getCause();
+
+			Assert.assertSame(
+				ConstraintViolationException.class, throwable.getClass());
+		}
 	}
 
 	@ExpectedLogs(
