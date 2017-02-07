c1f8e5a1166fb50e74efbf4feb63c4f975b85a3f
shuyangzhou <shuyang.zhou@liferay.com>
2017-02-07 14:17:00 -0800

    LPS-70507 Knock out SessionFactoryUtils' logger

--- portal-impl/src/com/liferay/portal/spring/hibernate/LastSessionRecorderHibernateTransactionManager.java
+++ portal-impl/src/com/liferay/portal/spring/hibernate/LastSessionRecorderHibernateTransactionManager.java
@@ -21,6 +21,7 @@ import java.lang.reflect.Field;
 import org.apache.commons.logging.Log;
 
 import org.springframework.orm.hibernate3.HibernateTransactionManager;
+import org.springframework.orm.hibernate3.SessionFactoryUtils;
 import org.springframework.orm.hibernate3.SessionHolder;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
@@ -46,92 +47,97 @@ public class LastSessionRecorderHibernateTransactionManager
 		try {
 			Class.forName(SpringHibernateThreadLocalUtil.class.getName());
 
+			Log nullLog = new Log() {
+
+				@Override
+				public void debug(Object object) {
+				}
+
+				@Override
+				public void debug(Object object, Throwable throwable) {
+				}
+
+				@Override
+				public void error(Object object) {
+				}
+
+				@Override
+				public void error(Object object, Throwable throwable) {
+				}
+
+				@Override
+				public void fatal(Object object) {
+				}
+
+				@Override
+				public void fatal(Object object, Throwable throwable) {
+				}
+
+				@Override
+				public void info(Object object) {
+				}
+
+				@Override
+				public void info(Object object, Throwable throwable) {
+				}
+
+				@Override
+				public boolean isDebugEnabled() {
+					return false;
+				}
+
+				@Override
+				public boolean isErrorEnabled() {
+					return false;
+				}
+
+				@Override
+				public boolean isFatalEnabled() {
+					return false;
+				}
+
+				@Override
+				public boolean isInfoEnabled() {
+					return false;
+				}
+
+				@Override
+				public boolean isTraceEnabled() {
+					return false;
+				}
+
+				@Override
+				public boolean isWarnEnabled() {
+					return false;
+				}
+
+				@Override
+				public void trace(Object object) {
+				}
+
+				@Override
+				public void trace(Object object, Throwable throwable) {
+				}
+
+				@Override
+				public void warn(Object object) {
+				}
+
+				@Override
+				public void warn(Object object, Throwable throwable) {
+				}
+
+			};
+
 			Field loggerField = ReflectionUtil.getDeclaredField(
 				TransactionSynchronizationManager.class, "logger");
 
-			loggerField.set(
-				null,
-				new Log() {
+			loggerField.set(null, nullLog);
 
-					@Override
-					public void debug(Object object) {
-					}
+			loggerField = ReflectionUtil.getDeclaredField(
+				SessionFactoryUtils.class, "logger");
 
-					@Override
-					public void debug(Object object, Throwable throwable) {
-					}
-
-					@Override
-					public void error(Object object) {
-					}
-
-					@Override
-					public void error(Object object, Throwable throwable) {
-					}
-
-					@Override
-					public void fatal(Object object) {
-					}
-
-					@Override
-					public void fatal(Object object, Throwable throwable) {
-					}
-
-					@Override
-					public void info(Object object) {
-					}
-
-					@Override
-					public void info(Object object, Throwable throwable) {
-					}
-
-					@Override
-					public boolean isDebugEnabled() {
-						return false;
-					}
-
-					@Override
-					public boolean isErrorEnabled() {
-						return false;
-					}
-
-					@Override
-					public boolean isFatalEnabled() {
-						return false;
-					}
-
-					@Override
-					public boolean isInfoEnabled() {
-						return false;
-					}
-
-					@Override
-					public boolean isTraceEnabled() {
-						return false;
-					}
-
-					@Override
-					public boolean isWarnEnabled() {
-						return false;
-					}
-
-					@Override
-					public void trace(Object object) {
-					}
-
-					@Override
-					public void trace(Object object, Throwable throwable) {
-					}
-
-					@Override
-					public void warn(Object object) {
-					}
-
-					@Override
-					public void warn(Object object, Throwable throwable) {
-					}
-
-				});
+			loggerField.set(null, nullLog);
 		}
 		catch (Exception e) {
 			throw new ExceptionInInitializerError(e);
