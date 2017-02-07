0405d108fe3335ff1ede079ca3c814d38bb2156c
Brian Chan <brian.chan@liferay.com>
2017-02-07 09:30:38 -0800

    LPS-70420 SF

--- modules/apps/collaboration/document-library/document-library-repository-cmis-impl/src/main/java/com/liferay/document/library/repository/cmis/internal/CMISModelCache.java
+++ modules/apps/collaboration/document-library/document-library-repository-cmis-impl/src/main/java/com/liferay/document/library/repository/cmis/internal/CMISModelCache.java
@@ -28,40 +28,40 @@ import java.util.Map;
 public class CMISModelCache {
 
 	public List<FileEntry> getFileEntries(long folderId) {
-		Map<Long, List<FileEntry>> fileEntriesCache = _fileEntriesCache.get();
+		Map<Long, List<FileEntry>> fileEntriesMap = _fileEntriesMaps.get();
 
-		return fileEntriesCache.get(folderId);
+		return fileEntriesMap.get(folderId);
 	}
 
 	public FileEntry getFileEntry(long fileEntryId) {
-		Map<Long, FileEntry> fileEntryMap = _fileEntryCache.get();
+		Map<Long, FileEntry> fileEntryMap = _fileEntryMaps.get();
 
 		return fileEntryMap.get(fileEntryId);
 	}
 
 	public Folder getFolder(long folderId) {
-		Map<Long, Folder> folderMap = _folderCache.get();
+		Map<Long, Folder> folderMap = _folderMaps.get();
 
 		return folderMap.get(folderId);
 	}
 
 	public List<Folder> getFolders(long folderId) {
-		Map<Long, List<Folder>> foldersCache = _foldersCache.get();
+		Map<Long, List<Folder>> foldersMap = _foldersMap.get();
 
-		return foldersCache.get(folderId);
+		return foldersMap.get(folderId);
 	}
 
 	public List<Object> getFoldersAndFileEntries(long folderId) {
-		Map<Long, List<Object>> foldersAndFileEntriesCache =
-			_foldersAndFileEntriesCache.get();
+		Map<Long, List<Object>> foldersAndFileEntriesMap =
+			_foldersAndFileEntriesMaps.get();
 
-		return foldersAndFileEntriesCache.get(folderId);
+		return foldersAndFileEntriesMap.get(folderId);
 	}
 
 	public void putFileEntries(long folderId, List<FileEntry> fileEntries) {
-		Map<Long, List<FileEntry>> fileEntriesCache = _fileEntriesCache.get();
+		Map<Long, List<FileEntry>> fileEntriesMap = _fileEntriesMaps.get();
 
-		fileEntriesCache.put(folderId, fileEntries);
+		fileEntriesMap.put(folderId, fileEntries);
 	}
 
 	public void putFileEntry(FileEntry fileEntry) {
@@ -69,7 +69,7 @@ public class CMISModelCache {
 			return;
 		}
 
-		Map<Long, FileEntry> fileEntryMap = _fileEntryCache.get();
+		Map<Long, FileEntry> fileEntryMap = _fileEntryMaps.get();
 
 		fileEntryMap.put(fileEntry.getFileEntryId(), fileEntry);
 	}
@@ -79,46 +79,45 @@ public class CMISModelCache {
 			return;
 		}
 
-		Map<Long, Folder> folderMap = _folderCache.get();
+		Map<Long, Folder> folderMap = _folderMaps.get();
 
 		folderMap.put(folder.getFolderId(), folder);
 	}
 
 	public void putFolders(long folderId, List<Folder> folders) {
-		Map<Long, List<Folder>> foldersCache = _foldersCache.get();
+		Map<Long, List<Folder>> foldersMap = _foldersMap.get();
 
-		foldersCache.put(folderId, folders);
+		foldersMap.put(folderId, folders);
 	}
 
 	public void putFoldersAndFileEntries(
 		long folderId, List<Object> foldersAndFileEntries) {
 
-		Map<Long, List<Object>> foldersAndFileEntriesCache =
-			_foldersAndFileEntriesCache.get();
+		Map<Long, List<Object>> foldersAndFileEntriesMap =
+			_foldersAndFileEntriesMaps.get();
 
-		foldersAndFileEntriesCache.put(folderId, foldersAndFileEntries);
+		foldersAndFileEntriesMap.put(folderId, foldersAndFileEntries);
 	}
 
-	private final ThreadLocal<Map<Long, List<FileEntry>>> _fileEntriesCache =
+	private final ThreadLocal<Map<Long, List<FileEntry>>> _fileEntriesMaps =
 		new AutoResetThreadLocal<Map<Long, List<FileEntry>>>(
-			CMISRepository.class + "._fileEntriesCache",
+			CMISRepository.class + "._fileEntriesMaps",
 			new HashMap<Long, List<FileEntry>>());
-	private final ThreadLocal<Map<Long, FileEntry>> _fileEntryCache =
+	private final ThreadLocal<Map<Long, FileEntry>> _fileEntryMaps =
 		new AutoResetThreadLocal<Map<Long, FileEntry>>(
-			CMISRepository.class + "._fileEntryCache",
+			CMISRepository.class + "._fileEntryMaps",
 			new HashMap<Long, FileEntry>());
-	private final ThreadLocal<Map<Long, Folder>> _folderCache =
-		new AutoResetThreadLocal<Map<Long, Folder>>(
-			CMISRepository.class + "._folderCache",
-			new HashMap<Long, Folder>());
+	private final ThreadLocal<Map<Long, Folder>> _folderMaps =
+		new AutoResetThreadLocal<>(
+			CMISRepository.class + "._folderMaps", new HashMap<Long, Folder>());
 	private final ThreadLocal<Map<Long, List<Object>>>
-		_foldersAndFileEntriesCache =
+		_foldersAndFileEntriesMaps =
 			new AutoResetThreadLocal<Map<Long, List<Object>>>(
-				CMISRepository.class + "._foldersAndFileEntriesCache",
+				CMISRepository.class + "._foldersAndFileEntriesMaps",
 				new HashMap<Long, List<Object>>());
-	private final ThreadLocal<Map<Long, List<Folder>>> _foldersCache =
+	private final ThreadLocal<Map<Long, List<Folder>>> _foldersMap =
 		new AutoResetThreadLocal<Map<Long, List<Folder>>>(
-			CMISRepository.class + "._foldersCache",
+			CMISRepository.class + "._foldersMap",
 			new HashMap<Long, List<Folder>>());
 
 }
