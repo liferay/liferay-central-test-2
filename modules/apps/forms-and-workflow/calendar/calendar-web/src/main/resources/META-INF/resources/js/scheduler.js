9da64ff68f15fc33246f508cd278836806278051
Dustin Ryerson <dustin.ryerson@liferay.com>
2017-02-08 12:52:09 -0800

    LPS-64945 Solves the user impossibility of clicking on title field again after reopening the New Event modal.
    Create macro to close modal
    
    Add poshi test

--- modules/apps/forms-and-workflow/calendar/calendar-web/src/main/resources/META-INF/resources/js/scheduler.js
+++ modules/apps/forms-and-workflow/calendar/calendar-web/src/main/resources/META-INF/resources/js/scheduler.js
@@ -713,6 +713,12 @@ AUI.add(
 						instance.load();
 					},
 
+					_afterAddEventModalLoad: function(event) {
+						var instance = this;
+
+						event.target.node.getDOMNode().contentWindow.focus();
+					},
+
 					_afterSchedulerEventChange: function(event) {
 						var instance = this;
 
@@ -872,6 +878,12 @@ AUI.add(
 									},
 									title: Liferay.Language.get('new-calendar-booking'),
 									uri: Lang.sub(editCalendarBookingURL, data)
+								},
+								function(modal) {
+									modal.iframe.on(
+										'load',
+										A.bind(instance._afterAddEventModalLoad, instance)
+									);
 								}
 							);
 						}
