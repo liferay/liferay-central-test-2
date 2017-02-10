06e2601e5b623ad0ca9d714d9ae28d9e0bed5251
Thiago Rocha <thiago.rocha@liferay.com>
2017-02-10 14:18:03 -0300

    LPS-70529 Create Rule Validator

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/config.js
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/config.js
@@ -56,6 +56,10 @@
 							path: 'form_builder_action_property.js',
 							requires: ['liferay-ddl-form-builder-action']
 						},
+						'liferay-ddl-form-builder-rule-validator': {
+							path: 'form_builder_rule_validator.js',
+							requires: []
+						},
 						'liferay-ddl-form-builder-autocomplete-template': {
 							path: '../templates/autocomplete.soy.js',
 							requires: [
@@ -158,7 +162,7 @@
 						},
 						'liferay-ddl-form-builder-render-rule': {
 							path: 'form_builder_render_rule.js',
-							requires: ['liferay-ddl-form-builder-action-factory', 'liferay-ddl-form-builder-rule-template', 'liferay-ddm-form-renderer-field']
+							requires: ['liferay-ddl-form-builder-action-factory', 'liferay-ddl-form-builder-rule-template', 'liferay-ddl-form-builder-rule-validator', 'liferay-ddm-form-renderer-field']
 						},
 						'liferay-ddl-form-builder-render-rule-condition': {
 							path: 'form_builder_render_rule_condition.js',
---
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_builder_rule_validator.js
@@ -0,0 +1,59 @@
+AUI.add(
+	'liferay-ddl-form-builder-rule-validator',
+	function(A) {
+		var FormBuilderRuleValidator = A.Component.create(
+			{
+				ATTRS: {
+				},
+
+				AUGMENTS: [],
+
+				NAME: 'liferay-ddl-form-builder-rule-validator',
+
+				prototype: {
+					isValidRule: function(rule) {
+						var instance = this;
+
+						return instance._isValidActions(rule.actions);
+					},
+
+					_isPropertyAction: function(type) {
+						return type === 'show' | type === 'enable' | type === 'required';
+					},
+
+					_isValidAction: function(action) {
+						var instance = this;
+
+						if (instance._isPropertyAction(action.action) && action.target) {
+							return true;
+						}
+
+						return false;
+					},
+
+					_isValidActions: function(actions) {
+						var instance = this;
+
+						if (actions.length === 0) {
+							return false;
+						}
+
+						for (var i = 0; i < actions.length; i++) {
+							if (!instance._isValidAction(actions[i])) {
+								return false;
+							}
+						}
+
+						return true;
+					}
+				}
+			}
+		);
+
+		Liferay.namespace('DDL').FormBuilderRuleValidator = FormBuilderRuleValidator;
+	},
+	'',
+	{
+		requires: []
+	}
+);
