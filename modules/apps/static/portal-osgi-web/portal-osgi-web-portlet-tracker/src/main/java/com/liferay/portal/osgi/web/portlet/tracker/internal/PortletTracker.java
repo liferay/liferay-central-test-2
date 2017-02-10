2a76c9b144c53ce617563737636d5ffecfe3496b
dsanz <daniel.sanz@liferay.com>
2017-02-10 15:31:56 +0100

    LPS-69811 Replace periods and dollar signs with underscores first

--- modules/apps/static/portal-osgi-web/portal-osgi-web-portlet-tracker/src/main/java/com/liferay/portal/osgi/web/portlet/tracker/internal/PortletTracker.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-portlet-tracker/src/main/java/com/liferay/portal/osgi/web/portlet/tracker/internal/PortletTracker.java
@@ -121,10 +121,10 @@ public class PortletTracker
 			portletName = clazz.getName();
 		}
 
-		String portletId = PortalUtil.getJsSafePortletId(portletName);
+		String portletId = StringUtil.replace(
+			portletName, new char[] {'.', '$'}, new char[] {'_', '_'});
 
-		portletId = StringUtil.replace(
-			portletId, new char[] {'$'}, new char[] {'_'});
+		portletId = PortalUtil.getJsSafePortletId(portletId);
 
 		if (portletId.length() >
 				PortletInstance.PORTLET_INSTANCE_KEY_MAX_LENGTH) {
