920d5c07292d1f6617a92b0ed3e9b0ec893a86b2
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-02-13 10:01:41 -0800

    LPS-69139 Extract constants

---
+++ modules/sdk/gradle-plugins-soy/src/main/java/com/liferay/gradle/plugins/soy/internal/SoyPluginConstants.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.gradle.plugins.soy.internal;
+
+/**
+ * @author Andrea Di Giorgi
+ */
+public interface SoyPluginConstants {
+
+	public static final String CONFIG_JS_MODULES_TASK_NAME = "configJSModules";
+
+	public static final String JS_MODULE_CONFIG_GENERATOR_PLUGIN_ID =
+		"com.liferay.js.module.config.generator";
+
+	public static final String JS_TRANSPILER_PLUGIN_ID =
+		"com.liferay.js.transpiler";
+
+	public static final String TRANSPILE_JS_TASK_NAME = "transpileJS";
+
+}
