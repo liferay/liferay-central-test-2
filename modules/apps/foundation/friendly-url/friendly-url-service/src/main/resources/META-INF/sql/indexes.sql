0ebc4da8898d339f83f1d4f17a1e005513bdb870
Hugo Huijser <hugo.huijser@liferay.com>
2017-02-14 07:59:22 +0800

    LPS-70618 Remove indexes

--- modules/apps/foundation/friendly-url/friendly-url-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/foundation/friendly-url/friendly-url-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,6 +1,3 @@
-create index IX_97417FEE on FriendlyURL (companyId, groupId, classNameId, classPK, main);
-create index IX_DD283EDE on FriendlyURL (companyId, groupId, classNameId, classPK, urlTitle[$COLUMN_LENGTH:255$]);
-create index IX_F32A413F on FriendlyURL (companyId, groupId, classNameId, urlTitle[$COLUMN_LENGTH:255$]);
 create index IX_C9E74CCE on FriendlyURL (groupId, classNameId);
 create index IX_BBBC6ADE on FriendlyURL (uuid_[$COLUMN_LENGTH:75$], companyId);
 create unique index IX_1C7E10E0 on FriendlyURL (uuid_[$COLUMN_LENGTH:75$], groupId);
