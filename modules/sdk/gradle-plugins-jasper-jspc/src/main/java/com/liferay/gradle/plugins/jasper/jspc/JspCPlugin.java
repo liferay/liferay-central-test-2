0b319bfe07ff6a1aa70e8ba1f32762ba22943e3c
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-02-14 17:45:13 -0800

    LPS-67573 Make methods private to reduce API surface

--- modules/sdk/gradle-plugins-jasper-jspc/src/main/java/com/liferay/gradle/plugins/jasper/jspc/JspCPlugin.java
+++ modules/sdk/gradle-plugins-jasper-jspc/src/main/java/com/liferay/gradle/plugins/jasper/jspc/JspCPlugin.java
@@ -58,13 +58,14 @@ public class JspCPlugin implements Plugin<Project> {
 	public void apply(Project project) {
 		GradleUtil.applyPlugin(project, JavaPlugin.class);
 
-		Configuration jspCConfiguration = addConfigurationJspC(project);
-		Configuration jspCToolConfiguration = addConfigurationJspCTool(project);
+		Configuration jspCConfiguration = _addConfigurationJspC(project);
+		Configuration jspCToolConfiguration = _addConfigurationJspCTool(
+			project);
 
-		final CompileJSPTask generateJSPJavaTask = addTaskGenerateJSPJava(
+		final CompileJSPTask generateJSPJavaTask = _addTaskGenerateJSPJava(
 			project, jspCConfiguration, jspCToolConfiguration);
 
-		addTaskCompileJSP(
+		_addTaskCompileJSP(
 			generateJSPJavaTask, jspCConfiguration, jspCToolConfiguration);
 
 		project.afterEvaluate(
@@ -72,13 +73,13 @@ public class JspCPlugin implements Plugin<Project> {
 
 				@Override
 				public void execute(Project project) {
-					addDependenciesJspC(project);
+					_addDependenciesJspC(project);
 				}
 
 			});
 	}
 
-	protected Configuration addConfigurationJspC(Project project) {
+	private Configuration _addConfigurationJspC(Project project) {
 		Configuration configuration = GradleUtil.addConfiguration(
 			project, CONFIGURATION_NAME);
 
@@ -89,7 +90,7 @@ public class JspCPlugin implements Plugin<Project> {
 		return configuration;
 	}
 
-	protected Configuration addConfigurationJspCTool(final Project project) {
+	private Configuration _addConfigurationJspCTool(final Project project) {
 		Configuration configuration = GradleUtil.addConfiguration(
 			project, TOOL_CONFIGURATION_NAME);
 
@@ -98,7 +99,7 @@ public class JspCPlugin implements Plugin<Project> {
 
 				@Override
 				public void execute(DependencySet dependencySet) {
-					addDependenciesJspCTool(project);
+					_addDependenciesJspCTool(project);
 				}
 
 			});
@@ -110,7 +111,7 @@ public class JspCPlugin implements Plugin<Project> {
 		return configuration;
 	}
 
-	protected void addDependenciesJspC(Project project) {
+	private void _addDependenciesJspC(Project project) {
 		DependencyHandler dependencyHandler = project.getDependencies();
 
 		Jar jar = (Jar)GradleUtil.getTask(project, JavaPlugin.JAR_TASK_NAME);
@@ -129,7 +130,7 @@ public class JspCPlugin implements Plugin<Project> {
 			CONFIGURATION_NAME, sourceSet.getCompileClasspath());
 	}
 
-	protected void addDependenciesJspCTool(Project project) {
+	private void _addDependenciesJspCTool(Project project) {
 		GradleUtil.addDependency(
 			project, TOOL_CONFIGURATION_NAME, "org.apache.ant", "ant", "1.9.4");
 
@@ -142,7 +143,7 @@ public class JspCPlugin implements Plugin<Project> {
 			Collections.singletonMap("group", "com.liferay.portal"));
 	}
 
-	protected JavaCompile addTaskCompileJSP(
+	private JavaCompile _addTaskCompileJSP(
 		CompileJSPTask generateJSPJavaTask, Configuration jspCConfiguration,
 		Configuration jspCToolConfiguration) {
 
@@ -159,7 +160,7 @@ public class JspCPlugin implements Plugin<Project> {
 		return javaCompile;
 	}
 
-	protected CompileJSPTask addTaskGenerateJSPJava(
+	private CompileJSPTask _addTaskGenerateJSPJava(
 		Project project, Configuration jspCConfiguration,
 		Configuration jspCToolConfiguration) {
 
@@ -182,7 +183,7 @@ public class JspCPlugin implements Plugin<Project> {
 						compileJSPTask.getProject(),
 						SourceSet.MAIN_SOURCE_SET_NAME);
 
-					return getSrcDir(sourceSet.getResources());
+					return _getSrcDir(sourceSet.getResources());
 				}
 
 			});
@@ -195,7 +196,7 @@ public class JspCPlugin implements Plugin<Project> {
 
 				@Override
 				public void execute(WarPlugin warPlugin) {
-					configureTaskGenerateJSPJavaForWarPlugin(compileJSPTask);
+					_configureTaskGenerateJSPJavaForWarPlugin(compileJSPTask);
 				}
 
 			});
@@ -203,7 +204,7 @@ public class JspCPlugin implements Plugin<Project> {
 		return compileJSPTask;
 	}
 
-	protected void configureTaskGenerateJSPJavaForWarPlugin(
+	private void _configureTaskGenerateJSPJavaForWarPlugin(
 		final CompileJSPTask compileJSPTask) {
 
 		compileJSPTask.setWebAppDir(
@@ -222,7 +223,7 @@ public class JspCPlugin implements Plugin<Project> {
 			});
 	}
 
-	protected File getSrcDir(SourceDirectorySet sourceDirectorySet) {
+	private File _getSrcDir(SourceDirectorySet sourceDirectorySet) {
 		Set<File> srcDirs = sourceDirectorySet.getSrcDirs();
 
 		Iterator<File> iterator = srcDirs.iterator();
