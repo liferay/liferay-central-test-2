60c63b87a5080e3bca6288986cea7b47fd7fdd36
Chema Balsas <jose.balsas@liferay.com>
2017-02-15 16:06:15 +0100

    LPS-67821 Move to the right package. By agreed convention, soy taglibs should live inside .servlet.taglib.soy

--- modules/apps/foundation/frontend-taglib/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/soy/TranslationManagerTag.java
+++ modules/apps/foundation/frontend-taglib/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/soy/TranslationManagerTag.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.frontend.taglib.servlet.taglib;
+package com.liferay.frontend.taglib.servlet.taglib.soy;
 
 import com.liferay.frontend.taglib.soy.servlet.taglib.TemplateRendererTag;
 import com.liferay.portal.kernel.json.JSONArray;
