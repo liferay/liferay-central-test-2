4920ede3797065887bd35c8f1e52ebf8bbd96c9a
Michael Bowerman <michael.bowerman@liferay.com>
2017-02-16 12:00:57 -0800

    LPS-70514 Make the AssetCategoriesDisplayContext tolerant to Vocabularies being linked to non-existent structures

--- modules/apps/web-experience/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/display/context/AssetCategoriesDisplayContext.java
+++ modules/apps/web-experience/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/display/context/AssetCategoriesDisplayContext.java
@@ -30,8 +30,11 @@ import com.liferay.asset.kernel.service.AssetVocabularyLocalServiceUtil;
 import com.liferay.asset.kernel.service.AssetVocabularyServiceUtil;
 import com.liferay.portal.kernel.dao.search.EmptyOnClickRowChecker;
 import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.exception.NoSuchModelException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.PortalPreferences;
 import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
 import com.liferay.portal.kernel.portlet.PortletURLUtil;
@@ -99,10 +102,22 @@ public class AssetCategoriesDisplayContext {
 					ClassTypeReader classTypeReader =
 						assetRendererFactory.getClassTypeReader();
 
-					ClassType classType = classTypeReader.getClassType(
-						classTypePK, themeDisplay.getLocale());
+					try {
+						ClassType classType = classTypeReader.getClassType(
+							classTypePK, themeDisplay.getLocale());
 
-					name = classType.getName();
+						name = classType.getName();
+					}
+					catch (NoSuchModelException nsme) {
+						if (_log.isWarnEnabled()) {
+							_log.warn(
+								"Error when loading Asset Types for " +
+									"AssetVocabulary " + vocabulary.getName() +
+										": " + nsme.getMessage());
+						}
+
+						continue;
+					}
 				}
 				else {
 					name = ResourceActionsUtil.getModelResource(
@@ -119,6 +134,10 @@ public class AssetCategoriesDisplayContext {
 			}
 
 			sb.append(StringPool.COMMA);
+		}
+
+		if (sb.index() == 0) {
+			return StringPool.BLANK;
 		}
 
 		sb.setIndex(sb.index() - 1);
@@ -574,6 +593,9 @@ public class AssetCategoriesDisplayContext {
 
 		return false;
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AssetCategoriesDisplayContext.class);
 
 	private SearchContainer _categoriesSearchContainer;
 	private AssetCategory _category;
