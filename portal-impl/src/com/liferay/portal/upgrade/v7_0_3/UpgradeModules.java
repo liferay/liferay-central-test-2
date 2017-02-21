269b98b71a7fcf4147e4e126c36a60778fefc3cb
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2017-02-21 15:29:26 -0300

    LPS-70807 / LPS-65628 Ask for creation of Kaleo Forms Web release info.

--- portal-impl/src/com/liferay/portal/upgrade/v7_0_3/UpgradeModules.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_3/UpgradeModules.java
@@ -30,8 +30,10 @@ public class UpgradeModules
 		return _CONVERTED_LEGACY_MODULES;
 	}
 
-	private static final String[] _BUNDLE_SYMBOLIC_NAMES =
-		{"com.liferay.blogs.service", "com.liferay.subscription.service"};
+	private static final String[] _BUNDLE_SYMBOLIC_NAMES = {
+		"com.liferay.blogs.service", "com.liferay.subscription.service",
+		"com.liferay.portal.workflow.kaleo.forms.web"
+	};
 
 	private static final String[][] _CONVERTED_LEGACY_MODULES = {};
 
