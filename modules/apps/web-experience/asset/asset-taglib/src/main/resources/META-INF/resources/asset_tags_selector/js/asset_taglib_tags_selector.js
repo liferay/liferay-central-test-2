174408a3eaa13f0fa131d48a4ee7be85c65386e5
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-02-22 11:49:06 +0100

    LPS-70648 Keep taglib functionality

--- modules/apps/web-experience/asset/asset-taglib/src/main/resources/META-INF/resources/asset_tags_selector/js/asset_taglib_tags_selector.js
+++ modules/apps/web-experience/asset/asset-taglib/src/main/resources/META-INF/resources/asset_tags_selector/js/asset_taglib_tags_selector.js
@@ -261,7 +261,7 @@ AUI.add(
 											serviceQueryObj = {
 												end: 20,
 												groupIds: instance.get('groupIds'),
-												name: term,
+												name: '%' + term + '%',
 												start: 0,
 												tagProperties: ''
 											};
