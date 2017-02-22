fffb9a4a4f80c437ce74df96ad6cc402c8139210
Adolfo Pérez <adolfo.perez@liferay.com>
2017-02-22 15:54:18 +0100

    LPS-71312 Use correct schema version

--- modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/internal/lar/KBDisplayPortletDataHandler.java
+++ modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/internal/lar/KBDisplayPortletDataHandler.java
@@ -39,7 +39,7 @@ import org.osgi.service.component.annotations.Reference;
 )
 public class KBDisplayPortletDataHandler extends BasePortletDataHandler {
 
-	public static final String SCHEMA_VERSION = "1.0.0";
+	public static final String SCHEMA_VERSION = "2.0.0";
 
 	@Override
 	public String getSchemaVersion() {
