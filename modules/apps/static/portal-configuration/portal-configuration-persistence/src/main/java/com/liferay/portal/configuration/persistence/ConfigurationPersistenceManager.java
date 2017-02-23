70aef1769efea13d2157f8f5a1420c619612d257
Brian Chan <brian.chan@liferay.com>
2017-02-23 08:59:12 -0800

    LPS-70844 SF

--- modules/apps/static/portal-configuration/portal-configuration-persistence/src/main/java/com/liferay/portal/configuration/persistence/ConfigurationPersistenceManager.java
+++ modules/apps/static/portal-configuration/portal-configuration-persistence/src/main/java/com/liferay/portal/configuration/persistence/ConfigurationPersistenceManager.java
@@ -597,9 +597,8 @@ public class ConfigurationPersistenceManager
 		return ConfigurationHandler.read(inputStream);
 	}
 
-	@SuppressWarnings("rawtypes")
 	private Dictionary<?, ?> _copyDictionary(Dictionary<?, ?> dictionary) {
-		Dictionary newDictionary = new HashMapDictionary();
+		Dictionary newDictionary = new HashMapDictionary<>();
 
 		Enumeration<?> keys = dictionary.keys();
 
