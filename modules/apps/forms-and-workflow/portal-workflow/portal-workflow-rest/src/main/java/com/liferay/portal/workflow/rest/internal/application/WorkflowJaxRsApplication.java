4b6d91afa5dd08add09663ca55f517179d9ba4ec
Brian Chan <brian.chan@liferay.com>
2017-02-24 21:10:19 -0800

    LPS-70380 Match BlogsJaxRsApplication

--- modules/apps/forms-and-workflow/portal-workflow/portal-workflow-rest/src/main/java/com/liferay/portal/workflow/rest/internal/application/WorkflowJaxRsApplication.java
+++ modules/apps/forms-and-workflow/portal-workflow/portal-workflow-rest/src/main/java/com/liferay/portal/workflow/rest/internal/application/WorkflowJaxRsApplication.java
@@ -34,7 +34,7 @@ import org.osgi.service.component.annotations.Reference;
  */
 @ApplicationPath("/workflow")
 @Component(immediate = true, service = Application.class)
-public class WorkflowRestApplication extends Application {
+public class WorkflowJaxRsApplication extends Application {
 
 	@Override
 	public Set<Class<?>> getClasses() {
