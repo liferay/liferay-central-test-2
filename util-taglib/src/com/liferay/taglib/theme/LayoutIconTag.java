0b88d216d744f18be449a7ce2c5c3be62f2d66ae
shuyangzhou <shuyang.zhou@liferay.com>
2017-02-24 16:43:50 -0800

    Revert "LPS-68472 - Deprecate <liferay-theme:layout-icon /> taglib"
    This reverts commit 44d639bd23d490aca4b9820efb7ca47d701a8b62.

--- util-taglib/src/com/liferay/taglib/theme/LayoutIconTag.java
+++ util-taglib/src/com/liferay/taglib/theme/LayoutIconTag.java
@@ -24,9 +24,7 @@ import javax.servlet.jsp.tagext.BodyTag;
 
 /**
  * @author Brian Wing Shun Chan
- * @deprecated As of 7.0.0, with no direct replacement
  */
-@Deprecated
 public class LayoutIconTag
 	extends com.liferay.taglib.util.IncludeTag implements BodyTag {
 
