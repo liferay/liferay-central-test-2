575501d6bbc26a90e5e2c698854559cd3fbb68de
Alberto Chaparro <alberto.chaparro@liferay.com>
2017-02-27 18:37:11 +0100

    LPS-68616 DDM: adding missing companyId indexes during upgrade

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v1_0_0/UpgradeCompanyId.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v1_0_0/UpgradeCompanyId.java
@@ -22,6 +22,15 @@ import com.liferay.portal.kernel.upgrade.BaseUpgradeCompanyId;
 public class UpgradeCompanyId extends BaseUpgradeCompanyId {
 
 	@Override
+	protected void doUpgrade() throws Exception {
+		super.doUpgrade();
+
+		runSQL(
+			"create index IX_DB81EB42 on DDMStorageLink " +
+				"(uuid_[$COLUMN_LENGTH:75$], companyId)");
+	}
+
+	@Override
 	protected TableUpdater[] getTableUpdaters() {
 		return new TableUpdater[] {
 			new TableUpdater("DDMStorageLink", "DDMStructure", "structureId"),
