7012def527246a9c19400fbb3f5b2e8ebbd77fbc
Alberto Chaparro <alberto.chaparro@liferay.com>
2017-02-27 18:35:21 +0100

    LPS-68616 DDM: adding missing indexes during the upgrade

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/resources/com/liferay/dynamic/data/mapping/internal/upgrade/v1_0_0/dependencies/update.sql
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/resources/com/liferay/dynamic/data/mapping/internal/upgrade/v1_0_0/dependencies/update.sql
@@ -13,12 +13,20 @@ create table DDMDataProviderInstance (
 	type_ VARCHAR(75) null
 );
 
+create index IX_DB54A6E5 on DDMDataProviderInstance (companyId);
+create index IX_1333A2A7 on DDMDataProviderInstance (groupId);
+create index IX_C903C097 on DDMDataProviderInstance (uuid_[$COLUMN_LENGTH:75$], companyId);
+create unique index IX_B4E180D9 on DDMDataProviderInstance (uuid_[$COLUMN_LENGTH:75$], groupId);
+
 create table DDMDataProviderInstanceLink (
 	dataProviderInstanceLinkId LONG not null primary key,
 	companyId LONG,
 	dataProviderInstanceId LONG,
 	structureId LONG
 );
+
+create unique index IX_8C878342 on DDMDataProviderInstanceLink (dataProviderInstanceId, structureId);
+create index IX_CB823541 on DDMDataProviderInstanceLink (structureId);
 
 alter table DDMStructure add versionUserId LONG;
 alter table DDMStructure add versionUserName VARCHAR(75) null;
@@ -41,6 +49,12 @@ create table DDMStructureLayout (
 	definition TEXT null
 );
 
+create unique index IX_B7158C0A on DDMStructureLayout (structureVersionId);
+create index IX_A90FF72A on DDMStructureLayout (uuid_[$COLUMN_LENGTH:75$], companyId);
+create unique index IX_C9A0402C on DDMStructureLayout (uuid_[$COLUMN_LENGTH:75$], groupId);
+
+create unique index IX_E43143A3 on DDMStructureLink (classNameId, classPK, structureId);
+
 create table DDMStructureVersion (
 	structureVersionId LONG not null primary key,
 	groupId LONG,
@@ -62,6 +76,9 @@ create table DDMStructureVersion (
 	statusDate DATE null
 );
 
+create index IX_17B3C96C on DDMStructureVersion (structureId, status);
+create unique index IX_64C3C42 on DDMStructureVersion (structureId, version[$COLUMN_LENGTH:75$]);
+
 alter table DDMTemplate add versionUserId LONG;
 alter table DDMTemplate add versionUserName VARCHAR(75) null;
 alter table DDMTemplate add resourceClassNameId LONG;
@@ -78,6 +95,9 @@ create table DDMTemplateLink (
 	classPK LONG,
 	templateId LONG
 );
+
+create unique index IX_6F3B3E9C on DDMTemplateLink (classNameId, classPK);
+create index IX_85278170 on DDMTemplateLink (templateId);
 
 create table DDMTemplateVersion (
 	templateVersionId LONG not null primary key,
@@ -99,5 +119,8 @@ create table DDMTemplateVersion (
 	statusByUserName VARCHAR(75) null,
 	statusDate DATE null
 );
+
+create index IX_66382FC6 on DDMTemplateVersion (templateId, status);
+create unique index IX_8854A128 on DDMTemplateVersion (templateId, version[$COLUMN_LENGTH:75$]);
 
 COMMIT_TRANSACTION;
