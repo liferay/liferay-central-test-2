3822c6e959678b000b395d8ea7d8e29f8b60fc98
Alberto Chaparro <alberto.chaparro@liferay.com>
2017-02-27 18:22:31 +0100

    LPS-68616 Calendar: Adding missing indexes during the upgrade

--- modules/apps/forms-and-workflow/calendar/calendar-service/src/main/resources/com/liferay/calendar/upgrade/v1_0_6/dependencies/update.sql
+++ modules/apps/forms-and-workflow/calendar/calendar-service/src/main/resources/com/liferay/calendar/upgrade/v1_0_6/dependencies/update.sql
@@ -1,3 +1,5 @@
 alter table CalendarBooking add recurringCalendarBookingId LONG null;
 
+create index IX_14ADC52E on CalendarBooking (recurringCalendarBookingId);
+
 update CalendarBooking set recurringCalendarBookingId = calendarBookingId;
