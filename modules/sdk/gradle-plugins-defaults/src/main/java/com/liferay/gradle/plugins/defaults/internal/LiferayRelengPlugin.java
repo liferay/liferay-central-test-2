f43cc1003a231e4d38f4549fadff1b83893b228d
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-02-28 17:16:05 -0800

    LPS-63943 Make artifact publish commands script executable

--- modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/internal/LiferayRelengPlugin.java
+++ modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/internal/LiferayRelengPlugin.java
@@ -220,6 +220,28 @@ public class LiferayRelengPlugin implements Plugin<Project> {
 			CollectionUtils.first(
 				cleanArtifactsPublishCommandsTask.getDelete()));
 
+		mergeFilesTask.doLast(
+			new Action<Task>() {
+
+				@Override
+				public void execute(Task task) {
+					MergeFilesTask mergeFilesTask = (MergeFilesTask)task;
+
+					File file = mergeFilesTask.getOutputFile();
+
+					boolean success = file.setExecutable(true);
+
+					if (!success) {
+						Logger logger = mergeFilesTask.getLogger();
+
+						logger.error(
+							"Unable to set the owner's execute permission " +
+								"for " + file);
+					}
+				}
+
+			});
+
 		mergeFilesTask.setDescription("Merges the artifacts publish commands");
 		mergeFilesTask.setHeader(
 			"#!/bin/bash" + System.lineSeparator() + System.lineSeparator() +
