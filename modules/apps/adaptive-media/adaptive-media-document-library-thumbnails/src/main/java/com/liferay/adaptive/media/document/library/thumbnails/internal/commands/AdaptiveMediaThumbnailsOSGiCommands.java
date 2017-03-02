98e050554d583ebadaba7e49e23fdd4a4a43cf85
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2017-03-02 15:40:55 +0100

    LPS-70921 Fix rebase changes

--- modules/apps/adaptive-media/adaptive-media-document-library-thumbnails/src/main/java/com/liferay/adaptive/media/document/library/thumbnails/internal/commands/AdaptiveMediaThumbnailsOSGiCommands.java
+++ modules/apps/adaptive-media/adaptive-media-document-library-thumbnails/src/main/java/com/liferay/adaptive/media/document/library/thumbnails/internal/commands/AdaptiveMediaThumbnailsOSGiCommands.java
@@ -174,14 +174,8 @@ public class AdaptiveMediaThumbnailsOSGiCommands {
 				Collectors.toList());
 		}
 
-<<<<<<< 1d9fd4f3b1256bf3b4e4798320fbeb0ba8d9bdf0
 		return Arrays.stream(companyIds).map(Long::parseLong).collect(
 			Collectors.toList());
-=======
-		Stream<String> stream = Stream.of(companyIds);
-
-		return stream.map(Long::parseLong).collect(Collectors.toList());
->>>>>>> LPS-70921 SF - Silence chaining warning
 	}
 
 	private FileVersion _getFileVersion(String fileName)
