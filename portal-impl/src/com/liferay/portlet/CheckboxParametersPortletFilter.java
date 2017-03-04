5e2fdc6ee7395ff10893fc43b42c0fb15225cbcc
shuyangzhou <shuyang.zhou@liferay.com>
2017-03-03 23:40:10 -0800

    LPS-71061 Move CheckboxParametersPortletFilter logic into PortletRequestImpl to avoid the DynamicActionRequest/DynamicResourceRequest wrapping and portlet filter call stack overhead.

--- portal-impl/src/com/liferay/portlet/CheckboxParametersPortletFilter.java
+++ portal-impl/src/com/liferay/portlet/CheckboxParametersPortletFilter.java
@@ -37,7 +37,9 @@ import javax.portlet.filter.ResourceFilter;
 
 /**
  * @author Julio Camarero
+ * @deprecated As of 7.0.0, with no direct replacement
  */
+@Deprecated
 public class CheckboxParametersPortletFilter
 	implements ActionFilter, ResourceFilter {
 
