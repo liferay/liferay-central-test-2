aa02796ad85eb648ca7eb02061bcc8f834689ded
shuyangzhou <shuyang.zhou@liferay.com>
2017-03-03 22:05:05 -0800

    LPS-71058 Inline IconOptionsTag's page.jsp

--- util-taglib/src/com/liferay/taglib/portletext/IconOptionsTag.java
+++ util-taglib/src/com/liferay/taglib/portletext/IconOptionsTag.java
@@ -19,8 +19,11 @@ import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfiguration
 import com.liferay.portal.kernel.theme.PortletDisplay;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.JavaConstants;
+import com.liferay.portal.kernel.util.ReflectionUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.util.comparator.PortletConfigurationIconComparator;
+import com.liferay.taglib.servlet.PipingServletResponse;
+import com.liferay.taglib.ui.IconMenuTag;
 import com.liferay.taglib.ui.IconTag;
 
 import java.util.List;
@@ -29,6 +32,7 @@ import javax.portlet.PortletRequest;
 import javax.portlet.PortletResponse;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.PageContext;
 
 /**
  * @author Brian Wing Shun Chan
@@ -96,6 +100,26 @@ public class IconOptionsTag extends IconTag {
 	}
 
 	@Override
+	protected int processEndTag() throws Exception {
+		IconMenuTag iconMenuTag = new IconMenuTag();
+
+		iconMenuTag.setCssClass("portlet-options");
+		iconMenuTag.setDirection(_direction);
+		iconMenuTag.setExtended(false);
+		iconMenuTag.setIcon("ellipsis-v");
+		iconMenuTag.setMarkupView("lexicon");
+		iconMenuTag.setMessage("options");
+		iconMenuTag.setShowArrow(false);
+		iconMenuTag.setShowWhenSingleIcon(true);
+		iconMenuTag.setTriggerCssClass("icon-monospaced");
+
+		iconMenuTag.doBodyTag(
+			pageContext, this::_processPortletConfigurationIcons);
+
+		return EVAL_PAGE;
+	}
+
+	@Override
 	protected void setAttributes(HttpServletRequest request) {
 		super.setAttributes(request);
 
@@ -105,6 +129,68 @@ public class IconOptionsTag extends IconTag {
 		request.setAttribute("liferay-ui:icon:direction", _direction);
 		request.setAttribute(
 			"liferay-ui:icon:showArrow", String.valueOf(_showArrow));
+	}
+
+	private void _processPortletConfigurationIcons(PageContext pageContext) {
+		try {
+			HttpServletRequest request =
+				(HttpServletRequest)pageContext.getRequest();
+
+			PortletRequest portletRequest =
+				(PortletRequest)request.getAttribute(
+					JavaConstants.JAVAX_PORTLET_REQUEST);
+
+			PortletResponse portletResponse =
+				(PortletResponse)request.getAttribute(
+					JavaConstants.JAVAX_PORTLET_RESPONSE);
+
+			for (PortletConfigurationIcon portletConfigurationIcon :
+					_portletConfigurationIcons) {
+
+				boolean include = portletConfigurationIcon.include(
+					request, new PipingServletResponse(pageContext));
+
+				if (!include) {
+					IconTag iconTag = new IconTag();
+
+					iconTag.setAlt(portletConfigurationIcon.getAlt());
+					iconTag.setAriaRole(portletConfigurationIcon.getAriaRole());
+					iconTag.setCssClass(portletConfigurationIcon.getCssClass());
+					iconTag.setData(portletConfigurationIcon.getData());
+					iconTag.setIconCssClass(
+						portletConfigurationIcon.getIconCssClass());
+					iconTag.setId(portletConfigurationIcon.getId());
+					iconTag.setImage(portletConfigurationIcon.getImage());
+					iconTag.setImageHover(
+						portletConfigurationIcon.getImageHover());
+					iconTag.setLabel(portletConfigurationIcon.isLabel());
+					iconTag.setLang(portletConfigurationIcon.getLang());
+					iconTag.setLinkCssClass(
+						portletConfigurationIcon.getLinkCssClass());
+					iconTag.setLocalizeMessage(false);
+					iconTag.setMessage(
+						portletConfigurationIcon.getMessage(portletRequest));
+					iconTag.setMethod(portletConfigurationIcon.getMethod());
+					iconTag.setOnClick(
+						portletConfigurationIcon.getOnClick(
+							portletRequest, portletResponse));
+					iconTag.setSrc(portletConfigurationIcon.getSrc());
+					iconTag.setSrcHover(portletConfigurationIcon.getSrcHover());
+					iconTag.setTarget(portletConfigurationIcon.getTarget());
+					iconTag.setToolTip(portletConfigurationIcon.isToolTip());
+					iconTag.setUrl(
+						portletConfigurationIcon.getURL(
+							portletRequest, portletResponse));
+					iconTag.setUseDialog(
+						portletConfigurationIcon.isUseDialog());
+
+					iconTag.doTag(pageContext);
+				}
+			}
+		}
+		catch (Exception e) {
+			ReflectionUtil.throwException(e);
+		}
 	}
 
 	private String _direction = "right";
