cef1213b668a5e6313595cf2a31c3e3d1b71e363
Preston-Crary <preston.crary@liferay.com>
2017-03-06 11:35:05 -0800

    LPS-69899 Add tests.

---
+++ modules/core/portal-web-compat/src/test/java/com/liferay/portal/web/compat/PortalWebCompatTest.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.web.compat;
+
+import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
+
+import java.io.File;
+import java.io.FileReader;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Preston Crary
+ */
+public class PortalWebCompatTest {
+
+	@Test
+	public void testNoConflictingImportedFiles() throws Exception {
+		File importedClassesPropertiesFile = new File(
+			"imported-files.properties");
+
+		Assert.assertTrue(importedClassesPropertiesFile.exists());
+
+		try (FileReader fileReader = new FileReader(
+				importedClassesPropertiesFile);
+			UnsyncBufferedReader unsyncBufferedReader =
+				new UnsyncBufferedReader(fileReader)) {
+
+			String line = null;
+
+			while ((line = unsyncBufferedReader.readLine()) != null) {
+				if (line.indexOf('=') >= 0) {
+					Matcher matcher = _dependencyPattern.matcher(line);
+
+					Assert.assertTrue(matcher.matches());
+				}
+				else {
+					line = line.trim();
+
+					if (line.endsWith(",\\")) {
+						line = line.substring(0, line.length() - 2);
+					}
+
+					File file = new File("../../../portal-web/docroot/" + line);
+
+					Assert.assertFalse(file.getPath(), file.exists());
+				}
+			}
+		}
+	}
+
+	private static final Pattern _dependencyPattern = Pattern.compile(
+		"com.liferay.portal\\\\:com.liferay.portal.web\\\\:" +
+			"\\d+\\.\\d+\\.\\d+\\\\:jsp@war=\\\\");
+
+}
