3e210f36afb7fee74d95e669fadd94782b8f2203
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2017-03-06 13:56:47 +0100

    LPS-69533 New ResourceCommand that will inform about the percentage of the optimized images for a concrete uuid

---
+++ modules/apps/adaptive-media/adaptive-media-web/src/main/java/com/liferay/adaptive/media/web/internal/portlet/action/OptimizedImagesPercentageMVCResourceCommand.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.adaptive.media.web.internal.portlet.action;
+
+import com.liferay.adaptive.media.image.service.AdaptiveMediaImageLocalServiceUtil;
+import com.liferay.adaptive.media.web.constants.AdaptiveMediaPortletKeys;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Ambrín Chaudhary
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + AdaptiveMediaPortletKeys.ADAPTIVE_MEDIA,
+		"mvc.command.name=/adaptive_media/optimized_images_percentage"
+	},
+	service = MVCResourceCommand.class
+)
+public class OptimizedImagesPercentageMVCResourceCommand
+	extends BaseMVCResourceCommand {
+
+	@Override
+	protected void doServeResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)resourceRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		String entryUuid = ParamUtil.getString(resourceRequest, "entryUuid");
+
+		int percentage = AdaptiveMediaImageLocalServiceUtil.getPercentage(
+			themeDisplay.getCompanyId(), entryUuid);
+
+		jsonObject.put("percentage", String.valueOf(percentage));
+
+		JSONPortletResponseUtil.writeJSON(
+			resourceRequest, resourceResponse, jsonObject);
+	}
+
+}
