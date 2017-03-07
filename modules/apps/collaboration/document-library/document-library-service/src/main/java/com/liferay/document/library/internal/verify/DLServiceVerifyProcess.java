ea30e221c1c500fe1a3bc9436bec6b9cf541b68f
Adolfo Pérez <adolfo.perez@liferay.com>
2017-03-07 14:33:23 +0100

    LPS-71099 Reapply LPS-70465

--- modules/apps/collaboration/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/verify/DLServiceVerifyProcess.java
+++ modules/apps/collaboration/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/verify/DLServiceVerifyProcess.java
@@ -523,7 +523,6 @@ public class DLServiceVerifyProcess extends VerifyProcess {
 		updateClassNameId();
 		updateFileEntryAssets();
 		updateFolderAssets();
-		verifyTree();
 	}
 
 	protected String getMimeType(InputStream inputStream, String title) {
@@ -748,17 +747,6 @@ public class DLServiceVerifyProcess extends VerifyProcess {
 
 			if (_log.isDebugEnabled()) {
 				_log.debug("Assets verified for folders");
-			}
-		}
-	}
-
-	protected void verifyTree() throws Exception {
-		try (LoggingTimer loggingTimer = new LoggingTimer()) {
-			long[] companyIds =
-				_portalInstancesLocalService.getCompanyIdsBySQL();
-
-			for (long companyId : companyIds) {
-				_dlFolderLocalService.rebuildTree(companyId);
 			}
 		}
 	}
