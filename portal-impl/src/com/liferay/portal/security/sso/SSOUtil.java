c57d06d80aab0e7e42728093e421764a17b3f38f
Michael Han <michael.han@liferay.com>
2017-03-07 11:34:18 -0600

    LPS-71052 SF

--- portal-impl/src/com/liferay/portal/security/sso/SSOUtil.java
+++ portal-impl/src/com/liferay/portal/security/sso/SSOUtil.java
@@ -16,6 +16,7 @@ package com.liferay.portal.security.sso;
 
 import com.liferay.portal.kernel.security.sso.SSO;
 import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.util.PrefsPropsUtil;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.registry.Registry;
@@ -40,7 +41,7 @@ public class SSOUtil {
 			_instance._getSessionExpirationRedirectUrl(companyId);
 
 		if (_instance._ssoMap.isEmpty() ||
-			(ssoSessionExpirationRedirectURL == null)) {
+			Validator.isNull(ssoSessionExpirationRedirectURL)) {
 
 			return sessionExpirationRedirectURL;
 		}
