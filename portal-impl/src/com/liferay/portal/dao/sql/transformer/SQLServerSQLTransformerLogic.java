28b7475e8f363f64203a0587923612032643e9b2
Manuel de la Peña <manuel.delapenya@liferay.com>
2017-03-07 12:50:26 +0100

    LPS-67296 Add functions for SQL Server SQL transformer
    Tests cannot be written because the SQL Server DB object is not present in this branch. Let's wait until merged into ee-7.0.x

---
+++ portal-impl/src/com/liferay/portal/dao/sql/transformer/SQLServerSQLTransformerLogic.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.dao.sql.transformer;
+
+import com.liferay.portal.kernel.dao.db.DB;
+
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class SQLServerSQLTransformerLogic extends BaseSQLTransformerLogic {
+
+	public SQLServerSQLTransformerLogic(DB db) {
+		super(db);
+
+		_functions = new Function[] {
+			getBitwiseCheckFunction(), getBooleanFunction(),
+			getCastClobTextFunction(), getCastLongFunction(),
+			getCastTextFunction(), _getInStrFunction(),
+			getIntegerDivisionFunction(), getNullDateFunction(),
+			_getSubstrFunction(), _getModFunction()
+		};
+	}
+
+	@Override
+	public Function<String, String>[] getFunctions() {
+		return _functions;
+	}
+
+	@Override
+	protected String replaceCastText(Matcher matcher) {
+		return matcher.replaceAll("CAST($1 AS NVARCHAR(MAX))");
+	}
+
+	private Function<String, String> _getInStrFunction() {
+		Pattern pattern = getInstrPattern();
+
+		return (String sql) -> {
+			Matcher matcher = pattern.matcher(sql);
+
+			return matcher.replaceAll("CHARINDEX($2, $1)");
+		};
+	}
+
+	private Function<String, String> _getModFunction() {
+		Pattern pattern = getModPattern();
+
+		return (String sql) -> {
+			Matcher matcher = pattern.matcher(sql);
+
+			return matcher.replaceAll("$1 % $2");
+		};
+	}
+
+	private Function<String, String> _getSubstrFunction() {
+		Pattern pattern = getSubstrPattern();
+
+		return (String sql) -> {
+			Matcher matcher = pattern.matcher(sql);
+
+			return matcher.replaceAll("SUBSTRING($1, $2, $3)");
+		};
+	}
+
+	private final Function<String, String>[] _functions;
+
+}
