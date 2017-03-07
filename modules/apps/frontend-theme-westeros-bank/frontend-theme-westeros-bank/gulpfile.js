cd79c107621fc2db8e3db4c5d4b60d259ed224c8
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-03-07 15:20:17 -0800

    LPS-70634 Prepend theme package names with ‘liferay-‘ in prep for npm publishing

--- modules/apps/frontend-theme-westeros-bank/frontend-theme-westeros-bank/gulpfile.js
+++ modules/apps/frontend-theme-westeros-bank/frontend-theme-westeros-bank/gulpfile.js
@@ -5,5 +5,6 @@ var liferayThemeTasks = require('liferay-theme-tasks');
 
 liferayThemeTasks.registerTasks(
 	{
+		distName: require('./package.json').name.replace(/^liferay-/, ''),
 		gulp: gulp
 	});
