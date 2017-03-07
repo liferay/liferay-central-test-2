michaelhashimoto/liferay-portal

LRQA-30653 Add ability to open pull requests against a git working directory

modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/GitWorkingDirectory.java
@@ -159,6 +159,34 @@ public class GitWorkingDirectory {
 		createBranchCommand.call();
 	}
 
+	public void createPullRequest(
+			String receiverUserName, String pullRequestBranchName, String title,
+			String body)
+		throws IOException {
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("https://api.github.com/repos/");
+		sb.append(receiverUserName);
+		sb.append("/");
+		sb.append(_repositoryName);
+		sb.append("/pulls");
+
+		JSONObject requestJSONObject = new JSONObject();
+
+		requestJSONObject.put("base", _repositoryBranchName);
+		requestJSONObject.put("body", body);
+		requestJSONObject.put(
+			"head", receiverUserName + ":" + pullRequestBranchName);
+		requestJSONObject.put("title", title);
+
+		JSONObject responseJSONObject = JenkinsResultsParserUtil.toJSONObject(
+			sb.toString(), requestJSONObject.toString());
+
+		System.out.println(
+			"Created a pull request at " + responseJSONObject.getString("url"));
+	}
+
 	public void deleteBranch(String branchName) throws GitAPIException {
 		System.out.println("Delete branch " + branchName);
 

liferay/liferay-central-test-2

modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/GitWorkingDirectory.java
@@ -34,6 +34,8 @@ import org.eclipse.jgit.transport.JschConfigSessionFactory;
 import org.eclipse.jgit.transport.OpenSshConfig;
 import org.eclipse.jgit.transport.SshSessionFactory;
 
+import org.json.JSONObject;
+
 /**
  * @author Michael Hashimoto
  */
@@ -138,6 +140,34 @@ public class GitWorkingDirectory {
 		createBranchCommand.setName(branchName);
 
 		createBranchCommand.call();
+	}
+
+	public void createPullRequest(
+			String title, String body, String receiverUserName,
+			String pullRequestBranchName)
+		throws IOException {
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("https://api.github.com/repos/");
+		sb.append(receiverUserName);
+		sb.append("/");
+		sb.append(_repositoryName);
+		sb.append("/pulls");
+
+		JSONObject requestJSONObject = new JSONObject();
+
+		requestJSONObject.put("base", _upstreamBranchName);
+		requestJSONObject.put("body", body);
+		requestJSONObject.put(
+			"head", receiverUserName + ":" + pullRequestBranchName);
+		requestJSONObject.put("title", title);
+
+		JSONObject responseJSONObject = JenkinsResultsParserUtil.toJSONObject(
+			sb.toString(), requestJSONObject.toString());
+
+		System.out.println(
+			"Created a pull request at " + responseJSONObject.getString("url"));
 	}
 
 	public void deleteBranch(String branchName) throws GitAPIException {