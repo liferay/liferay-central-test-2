d60ce0967894290230069c976c24c306ceba951b
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-03-08 10:17:18 -0800

    LPS-63943 Use log placeholder

--- modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/internal/LiferayRelengPlugin.java
+++ modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/internal/LiferayRelengPlugin.java
@@ -236,7 +236,8 @@ public class LiferayRelengPlugin implements Plugin<Project> {
 
 						logger.error(
 							"Unable to set the owner's execute permission " +
-								"for " + file);
+								"for {}",
+							file);
 					}
 				}
 
