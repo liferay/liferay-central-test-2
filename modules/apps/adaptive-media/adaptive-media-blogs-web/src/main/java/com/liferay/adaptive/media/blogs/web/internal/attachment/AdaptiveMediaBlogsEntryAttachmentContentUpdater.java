46062822e35159b691efeafcb289feafbe80f951
Alejandro Tardín <alejandro.tardin@liferay.com>
2017-03-09 14:47:06 +0100

    LPS-70229 Creating a dynamic image content updater for drag and drop

---
+++ modules/apps/adaptive-media/adaptive-media-blogs-web/src/main/java/com/liferay/adaptive/media/blogs/web/internal/attachment/AdaptiveMediaBlogsEntryAttachmentContentUpdater.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.adaptive.media.blogs.web.internal.attachment;
+
+import com.liferay.blogs.util.BlogsEntryAttachmentContentUpdater;
+import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.util.StringPool;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(
+	property = "service.ranking:Integer=1",
+	service = BlogsEntryAttachmentContentUpdater.class
+)
+public class AdaptiveMediaBlogsEntryAttachmentContentUpdater
+	extends BlogsEntryAttachmentContentUpdater {
+
+	@Override
+	protected String getBlogsEntryAttachmentFileEntryImgTag(
+		FileEntry blogsEntryAttachmentFileEntry) {
+
+		String fileEntryURL = PortletFileRepositoryUtil.getPortletFileEntryURL(
+			null, blogsEntryAttachmentFileEntry, StringPool.BLANK);
+
+		return "<img src=\"" + fileEntryURL + "\" data-fileEntryId=\"" +
+			blogsEntryAttachmentFileEntry.getFileEntryId() + "\"/>";
+	}
+
+}
