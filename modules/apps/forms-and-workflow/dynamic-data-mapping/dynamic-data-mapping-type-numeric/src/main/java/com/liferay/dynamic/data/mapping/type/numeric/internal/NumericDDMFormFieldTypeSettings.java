cebe5471984ff5c49a533d8d8083a18c5ba406a4
Leonardo Barros <leonardo.barros@liferay.com>
2017-03-09 14:40:19 -0300

    LPS-71173 Add *Settings class

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-numeric/src/main/java/com/liferay/dynamic/data/mapping/type/numeric/internal/NumericDDMFormFieldTypeSettings.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-numeric/src/main/java/com/liferay/dynamic/data/mapping/type/numeric/internal/NumericDDMFormFieldTypeSettings.java
@@ -14,11 +14,103 @@
 
 package com.liferay.dynamic.data.mapping.type.numeric.internal;
 
+import com.liferay.dynamic.data.mapping.annotations.DDMForm;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormField;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayout;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayoutColumn;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayoutPage;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayoutRow;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormRule;
 import com.liferay.dynamic.data.mapping.form.field.type.DefaultDDMFormFieldTypeSettings;
+import com.liferay.dynamic.data.mapping.model.DDMFormFieldValidation;
+import com.liferay.dynamic.data.mapping.model.LocalizedValue;
 
 /**
  * @author Leonardo Barros
  */
+@DDMForm(
+	rules = {
+		@DDMFormRule(
+			actions = {
+				"setVisible('tooltip', false)",
+				"setVisible('validation', false)"
+			},
+			condition = "TRUE"
+		)
+	}
+)
+@DDMFormLayout(
+	paginationMode = com.liferay.dynamic.data.mapping.model.DDMFormLayout.TABBED_MODE,
+	value = {
+		@DDMFormLayoutPage(
+			title = "basic",
+			value = {
+				@DDMFormLayoutRow(
+					{
+						@DDMFormLayoutColumn(
+							size = 12,
+							value = {"label", "tip", "dataType", "required"}
+						)
+					}
+				)
+			}
+		),
+		@DDMFormLayoutPage(
+			title = "properties",
+			value = {
+				@DDMFormLayoutRow(
+					{
+						@DDMFormLayoutColumn(
+							size = 12,
+							value = {
+								"predefinedValue", "placeholder",
+								"visibilityExpression", "validation",
+								"fieldNamespace", "indexType", "localizable",
+								"readOnly", "type", "name", "showLabel",
+								"repeatable", "tooltip"
+							}
+						)
+					}
+				)
+			}
+		)
+	}
+)
 public interface NumericDDMFormFieldTypeSettings
 	extends DefaultDDMFormFieldTypeSettings {
+
+	@DDMFormField(
+		label = "%my-numeric-type-is", optionLabels = {"%integer", "%decimal"},
+		optionValues = {"integer", "double"}, predefinedValue = "integer",
+		type = "radio"
+	)
+	@Override
+	public String dataType();
+
+	@DDMFormField(
+		dataType = "string", label = "%placeholder-text",
+		properties = {
+			"placeholder=%enter-placeholder-text",
+			"tooltip=%enter-text-that-assists-the-user-but-is-not-submitted-as-a-field-value"
+		},
+		type = "text"
+	)
+	public LocalizedValue placeholder();
+
+	@DDMFormField(
+		label = "%predefined-value",
+		properties = {
+			"placeholder=%enter-a-default-value",
+			"tooltip=%enter-a-default-value-that-is-submitted-if-no-other-value-is-entered"
+		},
+		type = "numeric"
+	)
+	public LocalizedValue predefinedValue();
+
+	@DDMFormField
+	public LocalizedValue tooltip();
+
+	@DDMFormField
+	public DDMFormFieldValidation validation();
+
 }
