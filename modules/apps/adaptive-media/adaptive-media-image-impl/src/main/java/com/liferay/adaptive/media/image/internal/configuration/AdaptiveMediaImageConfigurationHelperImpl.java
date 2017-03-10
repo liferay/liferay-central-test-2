cc4a34f3a9ea3fa2231f5829068f088427c3dfcb
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2017-03-10 12:32:06 +0100

    LPS-70625 Optiona is present

--- modules/apps/adaptive-media/adaptive-media-image-impl/src/main/java/com/liferay/adaptive/media/image/internal/configuration/AdaptiveMediaImageConfigurationHelperImpl.java
+++ modules/apps/adaptive-media/adaptive-media-image-impl/src/main/java/com/liferay/adaptive/media/image/internal/configuration/AdaptiveMediaImageConfigurationHelperImpl.java
@@ -207,10 +207,8 @@ public class AdaptiveMediaImageConfigurationHelperImpl
 			return;
 		}
 
-		configurationEntryOptional.ifPresent(
-			configurationEntry ->
-				_imageEntryLocalService.deleteAdaptiveMediaImageEntries(
-					companyId, configurationEntry));
+		_imageEntryLocalService.deleteAdaptiveMediaImageEntries(
+			companyId, configurationEntryOptional.get());
 
 		Collection<AdaptiveMediaImageConfigurationEntry> configurationEntries =
 			getAdaptiveMediaImageConfigurationEntries(
