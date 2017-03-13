a9d951a5a88b165d14ad8b193e51f912a784ca30
Alejandro Tardín <alejandro.tardin@liferay.com>
2017-03-13 08:27:59 +0100

    LPS-70229 Extracting the creation of URLs to a separate class

---
+++ modules/apps/adaptive-media/adaptive-media-image-impl/src/main/java/com/liferay/adaptive/media/image/internal/url/AdaptiveMediaImageURLFactoryImpl.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.adaptive.media.image.internal.url;
+
+import com.liferay.adaptive.media.AdaptiveMediaRuntimeException;
+import com.liferay.adaptive.media.AdaptiveMediaURIResolver;
+import com.liferay.adaptive.media.image.configuration.AdaptiveMediaImageConfigurationEntry;
+import com.liferay.adaptive.media.image.url.AdaptiveMediaImageURLFactory;
+import com.liferay.portal.kernel.repository.model.FileVersion;
+
+import java.io.UnsupportedEncodingException;
+
+import java.net.URI;
+import java.net.URLEncoder;
+
+import java.nio.charset.StandardCharsets;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(immediate = true)
+public class AdaptiveMediaImageURLFactoryImpl
+	implements AdaptiveMediaImageURLFactory {
+
+	@Override
+	public URI createFileEntryURL(
+		FileVersion fileVersion,
+		AdaptiveMediaImageConfigurationEntry configurationEntry) {
+
+		String relativeURI = String.format(
+			"image/%d/%s/%s", fileVersion.getFileEntryId(),
+			configurationEntry.getUUID(), _encode(fileVersion.getFileName()));
+
+		return _uriResolver.resolveURI(URI.create(relativeURI));
+	}
+
+	@Override
+	public URI createFileVersionURL(
+		FileVersion fileVersion,
+		AdaptiveMediaImageConfigurationEntry configurationEntry) {
+
+		String relativeURI = String.format(
+			"image/%d/%d/%s/%s", fileVersion.getFileEntryId(),
+			fileVersion.getFileVersionId(), configurationEntry.getUUID(),
+			_encode(fileVersion.getFileName()));
+
+		return _uriResolver.resolveURI(URI.create(relativeURI));
+	}
+
+	@Reference(unbind = "-")
+	protected void setAdaptiveMediaURIResolver(
+		AdaptiveMediaURIResolver adaptiveMediaURIResolver) {
+
+		_uriResolver = adaptiveMediaURIResolver;
+	}
+
+	private String _encode(String s) {
+		try {
+			return URLEncoder.encode(s, StandardCharsets.UTF_8.name());
+		}
+		catch (UnsupportedEncodingException uee) {
+			throw new AdaptiveMediaRuntimeException.
+				UnsupportedEncodingException(uee);
+		}
+	}
+
+	private AdaptiveMediaURIResolver _uriResolver;
+
+}
---
+++ modules/apps/adaptive-media/adaptive-media-image-impl/src/test/java/com/liferay/adaptive/media/image/internal/url/AdaptiveMediaImageURLFactoryImplTest.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.adaptive.media.image.internal.url;
+
+import com.liferay.adaptive.media.AdaptiveMediaURIResolver;
+import com.liferay.adaptive.media.image.configuration.AdaptiveMediaImageConfigurationEntry;
+import com.liferay.adaptive.media.image.internal.configuration.AdaptiveMediaImageConfigurationEntryImpl;
+import com.liferay.portal.kernel.repository.model.FileVersion;
+
+import java.net.URI;
+
+import java.util.HashMap;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * @author Alejandro Tardín
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class AdaptiveMediaImageURLFactoryImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+		Mockito.when(
+			_fileVersion.getFileEntryId()
+		).thenReturn(
+			1L
+		);
+
+		Mockito.when(
+			_fileVersion.getFileVersionId()
+		).thenReturn(
+			2L
+		);
+
+		Mockito.when(
+			_fileVersion.getFileName()
+		).thenReturn(
+			"fileName"
+		);
+
+		Mockito.when(
+			_uriResolver.resolveURI(Mockito.any(URI.class))
+		).thenAnswer(
+			invocation -> URI.create("prefix/" + invocation.getArguments()[0])
+		);
+
+		_adaptiveMediaImageURLFactory.setAdaptiveMediaURIResolver(_uriResolver);
+	}
+
+	@Test
+	public void testCreatesURLForFileEntry() throws Exception {
+		URI uri = _adaptiveMediaImageURLFactory.createFileEntryURL(
+			_fileVersion, _configurationEntry);
+
+		Assert.assertEquals("prefix/image/1/theUuid/fileName", uri.toString());
+	}
+
+	@Test
+	public void testCreatesURLForFileVersion() throws Exception {
+		URI uri = _adaptiveMediaImageURLFactory.createFileVersionURL(
+			_fileVersion, _configurationEntry);
+
+		Assert.assertEquals(
+			"prefix/image/1/2/theUuid/fileName", uri.toString());
+	}
+
+	private static final String _UUID = "theUuid";
+
+	private final AdaptiveMediaImageURLFactoryImpl
+		_adaptiveMediaImageURLFactory = new AdaptiveMediaImageURLFactoryImpl();
+	private final AdaptiveMediaImageConfigurationEntry _configurationEntry =
+		new AdaptiveMediaImageConfigurationEntryImpl(
+			"small", _UUID, new HashMap<>());
+
+	@Mock
+	private FileVersion _fileVersion;
+
+	@Mock
+	private AdaptiveMediaURIResolver _uriResolver;
+
+}
