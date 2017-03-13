99c5eb62705578ca2474103b31c33554711b6f2d
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2017-03-13 18:25:01 +0100

    LPS-70632 SF

--- modules/apps/collaboration/wiki/wiki-service/src/main/java/com/liferay/wiki/internal/model/listener/WikiPageCycleDetectorListener.java
+++ modules/apps/collaboration/wiki/wiki-service/src/main/java/com/liferay/wiki/internal/model/listener/WikiPageCycleDetectorListener.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.wiki.model.listener;
+package com.liferay.wiki.internal.model.listener;
 
 import com.liferay.portal.kernel.exception.ModelListenerException;
 import com.liferay.portal.kernel.model.BaseModelListener;
