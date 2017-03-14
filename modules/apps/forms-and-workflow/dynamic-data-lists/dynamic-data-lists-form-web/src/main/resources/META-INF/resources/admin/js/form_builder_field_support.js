b441c920ff951893007f57625776f2beb2bfcf03
Marcellus Tavares <marcellus.tavares@liferay.com>
2017-03-14 10:49:15 -0300

    LPS-71173 Use dataType property from the field settings

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_builder_field_support.js
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_builder_field_support.js
@@ -116,7 +116,10 @@ AUI.add(
 					}
 				);
 
-				settings.dataType = instance.get('dataType');
+				if (!settings.dataType) {
+					settings.dataType = instance.get('dataType');
+				}
+
 				settings.readOnly = true;
 				settings.type = instance.get('type');
 				settings.value = '';
