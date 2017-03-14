659dcd5f039a8b9202f6bb68b8a3a3dc8784e3de
shuyangzhou <shuyang.zhou@liferay.com>
2017-03-13 19:12:50 -0700

    LPS-71227 Optimize Cookie deletion logic

--- portal-kernel/src/com/liferay/portal/kernel/util/CookieKeys.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/CookieKeys.java
@@ -111,6 +111,34 @@ public class CookieKeys {
 		addCookie(request, response, cookieSupportCookie);
 	}
 
+	public static void deleteCookies(
+		HttpServletRequest request, HttpServletResponse response, String domain,
+		String... cookieNames) {
+
+		if (!_SESSION_ENABLE_PERSISTENT_COOKIES) {
+			return;
+		}
+
+		Map<String, Cookie> cookieMap = _getCookieMap(request);
+
+		for (String cookieName : cookieNames) {
+			Cookie cookie = cookieMap.remove(
+				StringUtil.toUpperCase(cookieName));
+
+			if (cookie != null) {
+				if (domain != null) {
+					cookie.setDomain(domain);
+				}
+
+				cookie.setMaxAge(0);
+				cookie.setPath(StringPool.SLASH);
+				cookie.setValue(StringPool.BLANK);
+
+				response.addCookie(cookie);
+			}
+		}
+	}
+
 	public static String getCookie(HttpServletRequest request, String name) {
 		return getCookie(request, name, true);
 	}
