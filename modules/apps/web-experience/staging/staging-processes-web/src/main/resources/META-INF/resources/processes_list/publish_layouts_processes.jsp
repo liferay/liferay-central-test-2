brianchandotcom/liferay-portal

LPS-69403 Source formatting

modules/apps/web-experience/staging/staging-processes-web/src/main/resources/META-INF/resources/processes_list/publish_layouts_processes.jsp
@@ -107,11 +107,11 @@ OrderByComparator<BackgroundTask> orderByComparator = BackgroundTaskComparatorFa
 
 			String backgroundTaskName = backgroundTaskDisplay.getDisplayName(request);
 
-			boolean stagingPrivateLayout = (boolean)backgroundTask.getTaskContextMap().get("privateLayout");
+			boolean privateLayout = MapUtil.getBoolean(backgroundTask.getTaskContextMap(), "privateLayout");
 
-			String stagingPageLayoutDescription = (stagingPrivateLayout) ? LanguageUtil.get(request, "private-pages") : LanguageUtil.get(request, "public-pages");
+			String publicPagesDescription = (privateLayout) ? LanguageUtil.get(request, "private-pages") : LanguageUtil.get(request, "public-pages");
 
-			backgroundTaskName = String.format("%s (%s)", backgroundTaskName, stagingPageLayoutDescription);
+			backgroundTaskName = String.format("%s (%s)", backgroundTaskName, publicPagesDescription);
 			%>
 
 			<c:choose>

liferay/liferay-central-test-2

modules/apps/web-experience/staging/staging-processes-web/src/main/resources/META-INF/resources/processes_list/publish_layouts_processes.jsp
@@ -113,12 +113,11 @@ OrderByComparator<BackgroundTask> orderByComparator = BackgroundTaskComparatorFa
 				backgroundTaskName = LanguageUtil.get(request, "untitled");
 			}
 
-			boolean stagingPrivateLayout = (boolean)backgroundTask.getTaskContextMap().get("privateLayout");
+			boolean processPrivateLayout = MapUtil.getBoolean(backgroundTask.getTaskContextMap(), "privateLayout");
 
-			String stagingPageLayoutDescription = (stagingPrivateLayout) ? LanguageUtil.get(request, "private-pages") : LanguageUtil.get(request, "public-pages");
+			String publicPagesDescription = (processPrivateLayout) ? LanguageUtil.get(request, "private-pages") : LanguageUtil.get(request, "public-pages");
 
-			backgroundTaskName = String.format("%s (%s)", backgroundTaskName, stagingPageLayoutDescription);
-
+			backgroundTaskName = String.format("%s (%s)", backgroundTaskName, publicPagesDescription);
 			%>
 
 			<c:choose>