b9985586996d7bd73cf931369debc3885c9c98ed
Manuel de la Peña <manuel.delapenya@liferay.com>
2017-03-15 13:00:03 +0100

    LPS-69729 Add same tests for the old SQLTransformer class (they will be removed once new logic is applied)
    These tests represent the same expectations for the old SQLTransformer.transform function, and are here for the single purpose of demonstrating that the old code satisfies the expectations defined by the new code.
    
    We should have created these tests before adding the new ones (the specific for each database transformer logic), and base those on them, but in the end we did it in the inverse order. For that reason, the old tests will be removed once the new SQLTransformer pattern is applied.

---
+++ portal-impl/test/unit/com/liferay/portal/dao/orm/common/BaseSQLTransformerTestCase.java
@@ -0,0 +1,205 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.dao.orm.common;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Manuel de la Peña
+ */
+public abstract class BaseSQLTransformerTestCase {
+
+	@Before
+	public void setUp() {
+		mockDB();
+	}
+
+	@Test
+	public void testReplaceBitwiseCheck() {
+		Assert.assertEquals(
+			getBitwiseCheckTransformedSQL(),
+			SQLTransformer.transform(getBitwiseCheckOriginalSQL()));
+	}
+
+	@Test
+	public void testReplaceBoolean() {
+		Assert.assertEquals(
+			getBooleanTransformedSQL(),
+			SQLTransformer.transform(getBooleanOriginalSQL()));
+	}
+
+	@Test
+	public void testReplaceCastClobText() {
+		Assert.assertEquals(
+			getCastClobTextTransformedSQL(),
+			SQLTransformer.transform(getCastClobTextOriginalSQL()));
+	}
+
+	@Test
+	public void testReplaceCastLong() {
+		Assert.assertEquals(
+			getCastLongTransformedSQL(),
+			SQLTransformer.transform(getCastLongOriginalSQL()));
+	}
+
+	@Test
+	public void testReplaceCrossJoin() {
+		Assert.assertEquals(
+			getCrossJoinTransformedSQL(),
+			SQLTransformer.transform(getCrossJoinOriginalSQL()));
+	}
+
+	@Test
+	public void testReplaceInstr() {
+		Assert.assertEquals(
+			getInstrTransformedSQL(),
+			SQLTransformer.transform(getInstrOriginalSQL()));
+	}
+
+	@Test
+	public void testReplaceIntegerDivision() {
+		Assert.assertEquals(
+			getIntegerDivisionTransformedSQL(),
+			SQLTransformer.transform(getIntegerDivisionOriginalSQL()));
+	}
+
+	@Test
+	public void testReplaceMod() {
+		Assert.assertEquals(
+			getModTransformedSQL(),
+			SQLTransformer.transform(getModOriginalSQL()));
+	}
+
+	@Test
+	public void testReplaceNullDate() {
+		Assert.assertEquals(
+			getNullDateTransformedSQL(),
+			SQLTransformer.transform(getNullDateOriginalSQL()));
+	}
+
+	@Test
+	public void testReplaceReplace() {
+		Assert.assertEquals(
+			getReplaceTransformedSQL(),
+			SQLTransformer.transform(getReplaceOriginalSQL()));
+	}
+
+	@Test
+	public void testReplaceSubstr() {
+		Assert.assertEquals(
+			getSubstrTransformedSQL(),
+			SQLTransformer.transform(getSubstrOriginalSQL()));
+	}
+
+	@Test
+	public void testTransform() {
+		String sql = "select * from Foo";
+
+		Assert.assertEquals(sql, SQLTransformer.transform(sql));
+	}
+
+	protected String getBitwiseCheckOriginalSQL() {
+		return "select BITAND(foo, bar) from Foo";
+	}
+
+	protected String getBitwiseCheckTransformedSQL() {
+		return getBitwiseCheckOriginalSQL();
+	}
+
+	protected String getBooleanOriginalSQL() {
+		return "select * from Foo where foo = [$FALSE$] and bar = [$TRUE$]";
+	}
+
+	protected String getBooleanTransformedSQL() {
+		return "select * from Foo where foo = false and bar = true";
+	}
+
+	protected String getCastClobTextOriginalSQL() {
+		return "select CAST_CLOB_TEXT(foo) from Foo";
+	}
+
+	protected String getCastClobTextTransformedSQL() {
+		return getCastClobTextOriginalSQL();
+	}
+
+	protected String getCastLongOriginalSQL() {
+		return "select CONVERT(foo, SQL_BIGINT) from Foo";
+	}
+
+	protected String getCastLongTransformedSQL() {
+		return getCastLongOriginalSQL();
+	}
+
+	protected String getCrossJoinOriginalSQL() {
+		return "select * from Foo CROSS JOIN Bar";
+	}
+
+	protected String getCrossJoinTransformedSQL() {
+		return getCrossJoinOriginalSQL();
+	}
+
+	protected String getInstrOriginalSQL() {
+		return "select INSTR(foo) from Foo";
+	}
+
+	protected String getInstrTransformedSQL() {
+		return getInstrOriginalSQL();
+	}
+
+	protected String getIntegerDivisionOriginalSQL() {
+		return "select INTEGER_DIV(foo, bar) from Foo";
+	}
+
+	protected String getIntegerDivisionTransformedSQL() {
+		return getIntegerDivisionOriginalSQL();
+	}
+
+	protected String getModOriginalSQL() {
+		return "select MOD(foo, bar) from Foo";
+	}
+
+	protected String getModTransformedSQL() {
+		return getModOriginalSQL();
+	}
+
+	protected String getNullDateOriginalSQL() {
+		return "select [$NULL_DATE$] from Foo";
+	}
+
+	protected String getNullDateTransformedSQL() {
+		return getNullDateOriginalSQL();
+	}
+
+	protected String getReplaceOriginalSQL() {
+		return "select replace(foo) from Foo";
+	}
+
+	protected String getReplaceTransformedSQL() {
+		return getReplaceOriginalSQL();
+	}
+
+	protected String getSubstrOriginalSQL() {
+		return "select foo from Foo";
+	}
+
+	protected String getSubstrTransformedSQL() {
+		return getSubstrOriginalSQL();
+	}
+
+	protected abstract void mockDB();
+
+}
