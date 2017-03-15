81f8d87904beda1c46e37f735756d7086f7c2d89
Manuel de la Peña <manuel.delapenya@liferay.com>
2017-03-15 13:00:03 +0100

    LPS-69729 Add same tests for the old SQLTransformer class (they will be removed once new logic is applied)
    These tests represent the same expectations for the old SQLTransformer.transform function, and are here for the single purpose of demonstrating that the old code satisfies the expectations defined by the new code.
    
    We should have created these tests before adding the new ones (the specific for each database transformer logic), and base those on them, but in the end we did it in the inverse order. For that reason, the old tests will be removed once the new SQLTransformer pattern is applied.

---
+++ portal-impl/test/unit/com/liferay/portal/dao/orm/common/MySQLSQLTransformerTest.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.dao.orm.common;
+
+import com.liferay.portal.dao.db.MySQLDB;
+import com.liferay.portal.kernel.dao.db.DBManagerUtil;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mockito;
+
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ * @author Manuel de la Peña
+ */
+@PrepareForTest(DBManagerUtil.class)
+@RunWith(PowerMockRunner.class)
+public class MySQLSQLTransformerTest extends BaseSQLTransformerTestCase {
+
+	@Test
+	public void testReplaceSupportsStringCaseSensitiveQuery() {
+		_db.setSupportsStringCaseSensitiveQuery(true);
+
+		String sql = "select * from foo";
+
+		Assert.assertEquals(sql, SQLTransformer.transform(sql));
+
+		sql = "select lower(foo) from Foo";
+
+		Assert.assertEquals(sql, SQLTransformer.transform(sql));
+	}
+
+	@Override
+	protected String getBitwiseCheckTransformedSQL() {
+		return "select (foo & bar) from Foo";
+	}
+
+	@Override
+	protected String getBooleanTransformedSQL() {
+		return "select * from Foo where foo = 0 and bar = 1";
+	}
+
+	@Override
+	protected String getCastClobTextTransformedSQL() {
+		return "select foo from Foo";
+	}
+
+	@Override
+	protected String getCastLongOriginalSQL() {
+		return "select CAST_LONG(foo) from Foo";
+	}
+
+	@Override
+	protected String getCastLongTransformedSQL() {
+		return "select foo from Foo";
+	}
+
+	@Override
+	protected String getIntegerDivisionTransformedSQL() {
+		return "select foo DIV bar from Foo";
+	}
+
+	@Override
+	protected String getNullDateTransformedSQL() {
+		return "select NULL from Foo";
+	}
+
+	@Override
+	protected void mockDB() {
+		PowerMockito.mockStatic(DBManagerUtil.class);
+
+		_db = new MySQLDB(5, 7);
+
+		Mockito.when(DBManagerUtil.getDB()).thenReturn(_db);
+	}
+
+	private MySQLDB _db;
+
+}
