f42f472b0cdfa97e065e98a89a63897eda633062
Rafael Praxedes <rafael.praxedes@liferay.com>
2017-03-15 13:49:43 -0300

    LPS-70813 Build service

---
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-api/src/main/java/com/liferay/dynamic/data/lists/exception/RecordSetSettingsRedirectURLException.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.exception;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+@ProviderType
+public class RecordSetSettingsRedirectURLException extends PortalException {
+
+	public RecordSetSettingsRedirectURLException() {
+	}
+
+	public RecordSetSettingsRedirectURLException(String msg) {
+		super(msg);
+	}
+
+	public RecordSetSettingsRedirectURLException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+	public RecordSetSettingsRedirectURLException(Throwable cause) {
+		super(cause);
+	}
+
+}
