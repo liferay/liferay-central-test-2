b07554b663f78715578f0c52f45aeb3bd812499b
shuyangzhou <shuyang.zhou@liferay.com>
2017-03-15 10:28:03 -0700

    LPS-71306 Use Reference to get access to SettingsFactory

--- modules/apps/foundation/portal-configuration/portal-configuration-module-configuration/src/main/java/com/liferay/portal/configuration/module/configuration/internal/ConfigurationProviderImpl.java
+++ modules/apps/foundation/portal-configuration/portal-configuration-module-configuration/src/main/java/com/liferay/portal/configuration/module/configuration/internal/ConfigurationProviderImpl.java
@@ -26,7 +26,7 @@ import com.liferay.portal.kernel.settings.GroupServiceSettingsLocator;
 import com.liferay.portal.kernel.settings.PortletInstanceSettingsLocator;
 import com.liferay.portal.kernel.settings.Settings;
 import com.liferay.portal.kernel.settings.SettingsException;
-import com.liferay.portal.kernel.settings.SettingsFactoryUtil;
+import com.liferay.portal.kernel.settings.SettingsFactory;
 import com.liferay.portal.kernel.settings.SettingsLocator;
 import com.liferay.portal.kernel.settings.TypedSettings;
 import com.liferay.portal.kernel.util.Validator;
@@ -35,6 +35,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Jürgen Kappler
@@ -66,8 +67,7 @@ public class ConfigurationProviderImpl implements ConfigurationProvider {
 
 			Object configurationOverrideInstance = null;
 
-			Settings settings = SettingsFactoryUtil.getSettings(
-				settingsLocator);
+			Settings settings = _settingsFactory.getSettings(settingsLocator);
 
 			TypedSettings typedSettings = new TypedSettings(settings);
 
@@ -181,5 +181,8 @@ public class ConfigurationProviderImpl implements ConfigurationProvider {
 
 		return settingsId;
 	}
+
+	@Reference
+	private SettingsFactory _settingsFactory;
 
 }
