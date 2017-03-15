e8a164c78de5c5b8752dbf2d44a02157cc4314b8
Hugo Huijser <hugo.huijser@liferay.com>
2017-03-15 08:22:55 +0800

    LPS-71164 Move logic that checks for incorrect use of 'DataAccess.getConnection' in *.java to new class

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaDataAccessConnectionCheck.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Tuple;
+import com.liferay.source.formatter.checks.util.JavaSourceUtil;
+
+import java.util.Collections;
+
+/**
+ * @author Hugo Huijser
+ */
+public class JavaDataAccessConnectionCheck extends BaseFileCheck {
+
+	@Override
+	public Tuple process(String fileName, String absolutePath, String content)
+		throws Exception {
+
+		if (hasGeneratedTag(content)) {
+			return new Tuple(content, Collections.emptySet());
+		}
+
+		String packagePath = JavaSourceUtil.getPackagePath(content);
+
+		if (packagePath.startsWith("com.liferay.portal.kernel.upgrade") ||
+			packagePath.startsWith("com.liferay.portal.kernel.verify") ||
+			packagePath.startsWith("com.liferay.portal.upgrade") ||
+			packagePath.startsWith("com.liferay.portal.verify")) {
+
+			content = StringUtil.replace(
+				content, "DataAccess.getConnection",
+				"DataAccess.getUpgradeOptimizedConnection");
+		}
+
+		return new Tuple(content, Collections.emptySet());
+	}
+
+}
