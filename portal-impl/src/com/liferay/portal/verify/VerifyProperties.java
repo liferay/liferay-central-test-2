d2a75d8b5190c4f0b914ffdae2acccb1ec362cde
Michael Bowerman <michael.bowerman@liferay.com>
2017-03-16 11:41:24 -0700

    LPS-62433 Mark as obsolete a new property that we are going to introduce in 6.2.x

--- portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
@@ -1746,6 +1746,7 @@ public class VerifyProperties extends VerifyProcess {
 		"dynamic.data.lists.template.language.parser[ftl]",
 		"dynamic.data.lists.template.language.parser[vm]",
 		"dynamic.data.lists.template.language.parser[xsl]",
+		"dynamic.data.mapping.structure.index.with.thread",
 		"dynamic.data.mapping.structure.private.field.names",
 		"dynamic.data.mapping.structure.private.field.datatype[_fieldsDisplay]",
 		"dynamic.data.mapping.structure.private.field.repeatable[" +
