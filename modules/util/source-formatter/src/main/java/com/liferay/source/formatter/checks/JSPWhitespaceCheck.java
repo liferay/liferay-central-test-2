7cd6d8086360cbc69567bfbd1014d7e95d9d4f42
Hugo Huijser <hugo.huijser@liferay.com>
2017-03-16 11:07:54 +0800

    LPS-71164 Add class JSPWhitespaceCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JSPWhitespaceCheck.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.util.Tuple;
+
+import java.util.Collections;
+
+/**
+ * @author Hugo Huijser
+ */
+public class JSPWhitespaceCheck extends WhitespaceCheck {
+
+	@Override
+	public Tuple process(String fileName, String absolutePath, String content)
+		throws Exception {
+
+		if (fileName.endsWith("/jsonws/action.jsp")) {
+			return new Tuple(content, Collections.emptySet());
+		}
+
+		return super.process(fileName, absolutePath, content);
+	}
+
+}
