28dccb5d28123cd503768bf9a4a3f48c776d36d0
Máté Thurzó <mate.thurzo@liferay.com>
2017-03-16 13:52:47 +0100

    LPS-70052 Revert single asset publication changes

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/base/DDMTemplateLocalServiceBaseImpl.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/base/DDMTemplateLocalServiceBaseImpl.java
@@ -60,13 +60,11 @@ import com.liferay.portal.kernel.service.persistence.SystemEventPersistence;
 import com.liferay.portal.kernel.service.persistence.UserPersistence;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.PortalUtil;
-import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.spring.extender.service.ServiceReference;
 
 import java.io.Serializable;
 
 import java.util.List;
-import java.util.Set;
 
 import javax.sql.DataSource;
 
@@ -322,16 +320,6 @@ public abstract class DDMTemplateLocalServiceBaseImpl
 		exportActionableDynamicQuery.setAddCriteriaMethod(new ActionableDynamicQuery.AddCriteriaMethod() {
 				@Override
 				public void addCriteria(DynamicQuery dynamicQuery) {
-					Set<Serializable> primaryKeys = portletDataContext.getRegisteredExportingClassedModelPrimaryKeys(
-							"DDMTemplate");
-
-					if (SetUtil.isNotEmpty(primaryKeys)) {
-						Property primaryKeyProperty = PropertyFactoryUtil.forName(
-								"templateId");
-
-						dynamicQuery.add(primaryKeyProperty.in(primaryKeys));
-					}
-
 					portletDataContext.addDateRangeCriteria(dynamicQuery,
 						"modifiedDate");
 
