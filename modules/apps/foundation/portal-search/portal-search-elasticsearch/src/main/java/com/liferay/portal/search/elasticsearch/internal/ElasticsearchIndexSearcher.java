994e4a85bd12a74d0b3044da31ff05ff50f58f05
Michael Han <michael.han@liferay.com>
2017-03-16 11:03:04 -0500

    LPS-71317 if queryConfig has highlighting disabled, then do not process highlighting

--- modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/ElasticsearchIndexSearcher.java
+++ modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/ElasticsearchIndexSearcher.java
@@ -260,6 +260,10 @@ public class ElasticsearchIndexSearcher extends BaseIndexSearcher {
 	protected void addHighlights(
 		SearchRequestBuilder searchRequestBuilder, QueryConfig queryConfig) {
 
+		if (!queryConfig.isHighlightEnabled()) {
+			return;
+		}
+
 		for (String highlightFieldName : queryConfig.getHighlightFieldNames()) {
 			addHighlightedField(
 				searchRequestBuilder, queryConfig, highlightFieldName);
