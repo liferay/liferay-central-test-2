sergiogonzalez/liferay-portal

LPS-67246 Add method to delete group subscriptions

modules/apps/collaboration/subscription/subscription-service/src/main/java/com/liferay/subscription/service/impl/SubscriptionLocalServiceImpl.java
@@ -159,6 +159,11 @@ public class SubscriptionLocalServiceImpl
 		return subscription;
 	}
 
+	@Override
+	public void deleteGroupSubscriptions(long groupId) {
+		subscriptionPersistence.removeByG(groupId);
+	}
+
 	/**
 	 * Deletes the subscription with the primary key. A social activity with the
 	 * unsubscribe action is created.

liferay/liferay-central-test-2

modules/apps/collaboration/subscription/subscription-service/src/main/java/com/liferay/subscription/service/impl/SubscriptionLocalServiceImpl.java
@@ -159,6 +159,11 @@ public class SubscriptionLocalServiceImpl
 		return subscription;
 	}
 
+	@Override
+	public void deleteGroupSubscriptions(long groupId) {
+		subscriptionPersistence.removeByGroupId(groupId);
+	}
+
 	/**
 	 * Deletes the subscription with the primary key. A social activity with the
 	 * unsubscribe action is created.