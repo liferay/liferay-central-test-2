2ad3eb6bc6072d4c6eeb07ae04185c32469f4f64
Alejandro Tardín <alejandro.tardin@liferay.com>
2017-03-16 18:51:52 +0100

    LPS-70229 Extracting upload to a module
    Using composition over inheritance to implement the specific methods for
    file entries.

---
+++ modules/apps/collaboration/upload/upload-web/src/main/java/com/liferay/upload/web/UploadHandlerImpl.java
@@ -0,0 +1,278 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.upload.web;
+
+import com.liferay.document.library.kernel.antivirus.AntivirusScannerException;
+import com.liferay.document.library.kernel.exception.FileNameException;
+import com.liferay.document.library.kernel.exception.FileSizeException;
+import com.liferay.portal.kernel.editor.EditorConstants;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.servlet.ServletResponseConstants;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.upload.LiferayFileItemException;
+import com.liferay.portal.kernel.upload.UploadException;
+import com.liferay.portal.kernel.upload.UploadPortletRequest;
+import com.liferay.portal.kernel.upload.UploadRequestSizeException;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StreamUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.upload.UploadFileEntryHandler;
+import com.liferay.upload.UploadHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Sergio González
+ * @author Adolfo Pérez
+ * @author Roberto Díaz
+ * @author Alejandro Tardín
+ */
+@Component
+public class UploadHandlerImpl implements UploadHandler {
+
+	@Override
+	public void upload(
+			UploadFileEntryHandler handler, PortletRequest portletRequest,
+			PortletResponse portletResponse)
+		throws PortalException {
+
+		UploadPortletRequest uploadPortletRequest =
+			PortalUtil.getUploadPortletRequest(portletRequest);
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		handler.checkPermission(
+			themeDisplay.getScopeGroupId(),
+			handler.getFolderId(uploadPortletRequest),
+			themeDisplay.getPermissionChecker());
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		try {
+			UploadException uploadException =
+				(UploadException)portletRequest.getAttribute(
+					WebKeys.UPLOAD_EXCEPTION);
+
+			if (uploadException != null) {
+				Throwable cause = uploadException.getCause();
+
+				if (uploadException.isExceededFileSizeLimit()) {
+					throw new FileSizeException(cause);
+				}
+
+				if (uploadException.isExceededLiferayFileItemSizeLimit()) {
+					throw new LiferayFileItemException(cause);
+				}
+
+				if (uploadException.isExceededUploadRequestSizeLimit()) {
+					throw new UploadRequestSizeException(cause);
+				}
+
+				throw new PortalException(cause);
+			}
+
+			JSONObject imageJSONObject = _getImageJSONObject(
+				handler, portletRequest);
+
+			String randomId = ParamUtil.getString(
+				uploadPortletRequest, "randomId");
+
+			imageJSONObject.put("randomId", randomId);
+
+			jsonObject.put("file", imageJSONObject);
+
+			jsonObject.put("success", Boolean.TRUE);
+
+			JSONPortletResponseUtil.writeJSON(
+				portletRequest, portletResponse, jsonObject);
+		}
+		catch (IOException ioe) {
+			throw new SystemException(ioe);
+		}
+		catch (PortalException pe) {
+			_handleUploadException(
+				handler, portletRequest, portletResponse, pe, jsonObject);
+		}
+	}
+
+	private JSONObject _getImageJSONObject(
+			UploadFileEntryHandler handler, PortletRequest portletRequest)
+		throws PortalException {
+
+		UploadPortletRequest uploadPortletRequest =
+			PortalUtil.getUploadPortletRequest(portletRequest);
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		JSONObject imageJSONObject = JSONFactoryUtil.createJSONObject();
+
+		InputStream inputStream = null;
+
+		try {
+			imageJSONObject.put(
+				"attributeDataImageId",
+				EditorConstants.ATTRIBUTE_DATA_IMAGE_ID);
+
+			String parameterName = handler.getParameterName();
+
+			String fileName = uploadPortletRequest.getFileName(parameterName);
+			String contentType = uploadPortletRequest.getContentType(
+				parameterName);
+			long size = uploadPortletRequest.getSize(parameterName);
+
+			handler.validateFile(fileName, contentType, size);
+
+			long folderId = handler.getFolderId(uploadPortletRequest);
+
+			String uniqueFileName = _getUniqueFileName(
+				handler, themeDisplay, fileName, folderId);
+
+			inputStream = uploadPortletRequest.getFileAsStream(parameterName);
+
+			FileEntry fileEntry = handler.addFileEntry(
+				themeDisplay.getUserId(), themeDisplay.getScopeGroupId(),
+				folderId, uniqueFileName, contentType, inputStream, size,
+				handler.getServiceContext(uploadPortletRequest));
+
+			imageJSONObject.put("fileEntryId", fileEntry.getFileEntryId());
+			imageJSONObject.put("groupId", fileEntry.getGroupId());
+			imageJSONObject.put("title", fileEntry.getTitle());
+
+			imageJSONObject.put("type", "document");
+			imageJSONObject.put("url", handler.getURL(fileEntry, themeDisplay));
+			imageJSONObject.put("uuid", fileEntry.getUuid());
+
+			return imageJSONObject;
+		}
+		catch (IOException ioe) {
+			throw new SystemException(ioe);
+		}
+		finally {
+			StreamUtil.cleanUp(inputStream);
+		}
+	}
+
+	private String _getUniqueFileName(
+			UploadFileEntryHandler handler, ThemeDisplay themeDisplay,
+			String fileName, long folderId)
+		throws PortalException {
+
+		FileEntry fileEntry = handler.fetchFileEntry(
+			themeDisplay.getUserId(), themeDisplay.getScopeGroupId(), folderId,
+			fileName);
+
+		if (fileEntry == null) {
+			return fileName;
+		}
+
+		int suffix = 1;
+
+		for (int i = 0; i < _UNIQUE_FILE_NAME_TRIES; i++) {
+			String curFileName = FileUtil.appendParentheticalSuffix(
+				fileName, String.valueOf(suffix));
+
+			fileEntry = handler.fetchFileEntry(
+				themeDisplay.getUserId(), themeDisplay.getScopeGroupId(),
+				folderId, curFileName);
+
+			if (fileEntry == null) {
+				return curFileName;
+			}
+
+			suffix++;
+		}
+
+		throw new PortalException(
+			"Unable to get a unique file name for " + fileName);
+	}
+
+	private void _handleUploadException(
+			UploadFileEntryHandler handler, PortletRequest portletRequest,
+			PortletResponse portletResponse, PortalException pe,
+			JSONObject jsonObject)
+		throws PortalException {
+
+		jsonObject.put("success", Boolean.FALSE);
+
+		if (pe instanceof AntivirusScannerException ||
+			pe instanceof FileNameException ||
+			pe instanceof FileSizeException ||
+			pe instanceof UploadRequestSizeException) {
+
+			String errorMessage = StringPool.BLANK;
+			int errorType = 0;
+
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)portletRequest.getAttribute(
+					WebKeys.THEME_DISPLAY);
+
+			if (pe instanceof AntivirusScannerException) {
+				errorType =
+					ServletResponseConstants.SC_FILE_ANTIVIRUS_EXCEPTION;
+				AntivirusScannerException ase = (AntivirusScannerException)pe;
+
+				errorMessage = themeDisplay.translate(ase.getMessageKey());
+			}
+			else if (pe instanceof FileNameException) {
+				errorType = ServletResponseConstants.SC_FILE_NAME_EXCEPTION;
+			}
+			else if (pe instanceof FileSizeException) {
+				errorType = ServletResponseConstants.SC_FILE_SIZE_EXCEPTION;
+			}
+			else if (pe instanceof UploadRequestSizeException) {
+				errorType =
+					ServletResponseConstants.SC_UPLOAD_REQUEST_SIZE_EXCEPTION;
+			}
+
+			JSONObject errorJSONObject = JSONFactoryUtil.createJSONObject();
+
+			errorJSONObject.put("errorType", errorType);
+			errorJSONObject.put("message", errorMessage);
+
+			jsonObject.put("error", errorJSONObject);
+		}
+		else {
+			handler.doHandleUploadException(
+				portletRequest, portletResponse, pe, jsonObject);
+		}
+
+		try {
+			JSONPortletResponseUtil.writeJSON(
+				portletRequest, portletResponse, jsonObject);
+		}
+		catch (IOException ioe) {
+			throw new SystemException(ioe);
+		}
+	}
+
+	private static final int _UNIQUE_FILE_NAME_TRIES = 50;
+
+}
