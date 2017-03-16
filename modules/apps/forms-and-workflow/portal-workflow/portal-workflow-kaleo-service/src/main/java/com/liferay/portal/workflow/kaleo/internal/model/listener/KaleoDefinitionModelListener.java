566572a41da6fff58cb4c0ae919a6dc55689275e
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2017-03-16 16:09:02 -0300

    LPS-67908 Send a message when creating a Kaleo Definition.

--- modules/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/internal/model/listener/KaleoDefinitionModelListener.java
+++ modules/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/internal/model/listener/KaleoDefinitionModelListener.java
@@ -21,9 +21,12 @@ import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.workflow.WorkflowDefinition;
+import com.liferay.portal.workflow.kaleo.KaleoWorkflowModelConverter;
 import com.liferay.portal.workflow.kaleo.model.KaleoDefinition;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Kenneth Chang
@@ -31,6 +34,29 @@ import org.osgi.service.component.annotations.Component;
 @Component(immediate = true, service = ModelListener.class)
 public class KaleoDefinitionModelListener
 	extends BaseModelListener<KaleoDefinition> {
+
+	@Override
+	public void onAfterCreate(KaleoDefinition kaleoDefinition)
+		throws ModelListenerException {
+
+		try {
+			Message message = new Message();
+
+			message.put("command", "create");
+			message.put("serviceContext", getServiceContext(kaleoDefinition));
+
+			WorkflowDefinition workflowDefinition =
+				_kaleoWorkflowModelConverter.toWorkflowDefinition(
+					kaleoDefinition);
+
+			message.setPayload(workflowDefinition);
+
+			MessageBusUtil.sendMessage("liferay/kaleo_definition", message);
+		}
+		catch (Exception e) {
+			throw new ModelListenerException(e);
+		}
+	}
 
 	@Override
 	public void onAfterRemove(KaleoDefinition kaleoDefinition)
@@ -66,5 +92,8 @@ public class KaleoDefinitionModelListener
 
 		return serviceContext;
 	}
+
+	@Reference
+	private KaleoWorkflowModelConverter _kaleoWorkflowModelConverter;
 
 }
