c4676a18a4dab87f200368839064ec848a114728
Manuel de la Peña <manuel.delapenya@liferay.com>
2017-03-16 09:09:54 +0100

    LPS-69729 Copy HqlToJpql transformation logic to a new class

---
+++ portal-impl/src/com/liferay/portal/dao/sql/transformer/HqlToJpqlTransformerLogic.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.dao.sql.transformer;
+
+import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.util.function.Function;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class HqlToJpqlTransformerLogic {
+
+	public static final Function<String, String>
+		getCompositeIdMarkerFunction() {
+
+		return (String sql) ->
+			StringUtil.replace(
+				sql, _HQL_COMPOSITE_ID_MARKER, _JPQL_DOT_SEPARTOR);
+	}
+
+	public static final Function<String, String> getNotEqualsFunction() {
+		return (String sql) ->
+			StringUtil.replace(sql, _HQL_NOT_EQUALS, _JPQL_NOT_EQUALS);
+	}
+
+	public static Function<String, String> getPositionalParameterFunction() {
+		return (String sql) -> {
+			if (sql.indexOf(CharPool.QUESTION) == -1) {
+				return sql;
+			}
+
+			StringBundler sb = new StringBundler();
+
+			int i = 1;
+			int from = 0;
+			int to = 0;
+
+			while ((to = sql.indexOf(CharPool.QUESTION, from)) != -1) {
+				sb.append(sql.substring(from, to));
+				sb.append(StringPool.QUESTION);
+				sb.append(i++);
+
+				from = to + 1;
+			}
+
+			sb.append(sql.substring(from));
+
+			return sb.toString();
+		};
+	}
+
+	private static final String _HQL_COMPOSITE_ID_MARKER = "\\.id\\.";
+
+	private static final String _HQL_NOT_EQUALS = "!=";
+
+	private static final String _JPQL_DOT_SEPARTOR = ".";
+
+	private static final String _JPQL_NOT_EQUALS = "<>";
+
+}
---
+++ portal-impl/test/unit/com/liferay/portal/dao/sql/transformer/HqlToJpqlTransformerLogicTest.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.dao.sql.transformer;
+
+import java.util.function.Function;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class HqlToJpqlTransformerLogicTest {
+
+	@Test
+	public void testReplaceCompositeId() {
+		Function<String, String> function =
+			HqlToJpqlTransformerLogic.getCompositeIdMarkerFunction();
+
+		Assert.assertEquals(
+			"select * from Foo where foo.a = 1",
+			function.apply("select * from Foo where foo\\.id\\.a = 1"));
+	}
+
+	@Test
+	public void testReplaceNotEquals() {
+		Function<String, String> function =
+			HqlToJpqlTransformerLogic.getNotEqualsFunction();
+
+		Assert.assertEquals(
+			"select * from Foo where foo <> 1",
+			function.apply("select * from Foo where foo != 1"));
+	}
+
+	@Test
+	public void testTransformWithMultipleQuestions() {
+		String sql = "select * from Foo where";
+
+		String expectedSql = new String(sql);
+
+		for (int i = 1; i <= 100; i++) {
+			String positionalParameter = " and foo" + i + " = ?";
+
+			sql += positionalParameter;
+			expectedSql += positionalParameter + i;
+		}
+
+		Function<String, String> positionalParameterFunction =
+			HqlToJpqlTransformerLogic.getPositionalParameterFunction();
+
+		Assert.assertEquals(
+			expectedSql, positionalParameterFunction.apply(sql));
+	}
+
+	@Test
+	public void testTransformWithOneQuestion() {
+		String sql = "select * from Foo where foo = ?";
+
+		Function<String, String> positionalParameterFunction =
+			HqlToJpqlTransformerLogic.getPositionalParameterFunction();
+
+		Assert.assertEquals(
+			"select * from Foo where foo = ?1",
+			positionalParameterFunction.apply(sql));
+	}
+
+	@Test
+	public void testTransformWithZeroQuestions() {
+		String sql = "select * from Foo";
+
+		Function<String, String> positionalParameterFunction =
+			HqlToJpqlTransformerLogic.getPositionalParameterFunction();
+
+		Assert.assertEquals(sql, positionalParameterFunction.apply(sql));
+	}
+
+}
