67520bbb8e64f09ce2e3b882a116f5feecb558bb
Carlos Lancha <carlos.lancha@liferay.com>
2017-03-16 14:34:06 +0100

    LPS-64308 Create ColorPickerInput metal component

---
+++ modules/apps/web-experience/portlet-configuration/portlet-configuration-css-web/src/main/resources/META-INF/resources/js/ColorPickerInput.es.js
@@ -0,0 +1,60 @@
+import Component from 'metal-component/src/Component';
+import core from 'metal/src/core';
+import Soy from 'metal-soy/src/Soy';
+
+import templates from './ColorPickerInput.soy';
+
+class ColorPickerInput extends Component {
+
+	/**
+	 * @inheritDoc
+	 */
+	rendered() {
+		let instance = this;
+
+		AUI().use('aui-color-picker-popover', function(A) {
+			instance.colorPickerPopover = new A.ColorPickerPopover(
+				{
+					constrain: true,
+					color: instance.color,
+					trigger: '#' + instance.id,
+					zIndex: Liferay.zIndex.POPOVER
+				}
+			);
+
+			instance.colorPickerPopover.render(instance.element);
+			instance.colorPickerPopover.after('select', instance.setColor_, instance);
+		});
+	}
+
+	setColor_(event) {
+		this.color = event.color;
+	}
+}
+
+ColorPickerInput.STATE = {
+	id: {
+		validator: core.isString
+	},
+
+	label: {
+		validator: core.isString
+	},
+
+	name: {
+		validator: core.isString
+	},
+
+	color: {
+		validator: core.isString
+	},
+
+	wrapperCssClass: {
+		validator: core.isString
+	}
+}
+
+// Register component
+Soy.register(ColorPickerInput, templates);
+
+export default ColorPickerInput;
