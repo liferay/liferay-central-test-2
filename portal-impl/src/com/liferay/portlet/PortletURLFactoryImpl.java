083ce578d188d9a7f0307d0a8fb1b75e71cb8f2d
Tina Tian <tina.tian@liferay.com>
2017-03-16 16:30:15 -0700

    LPS-70962 Stop using the constructors taking in plid, passing in layout instance instead

--- portal-impl/src/com/liferay/portlet/PortletURLFactoryImpl.java
+++ portal-impl/src/com/liferay/portlet/PortletURLFactoryImpl.java
@@ -15,9 +15,11 @@
 package com.liferay.portlet;
 
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.impl.VirtualLayout;
 import com.liferay.portal.kernel.portlet.LiferayPortletURL;
 import com.liferay.portal.kernel.portlet.PortletURLFactory;
 import com.liferay.portal.kernel.security.pacl.DoPrivileged;
+import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.WebKeys;
 
@@ -44,7 +46,10 @@ public class PortletURLFactoryImpl implements PortletURLFactory {
 		HttpServletRequest request, String portletId, long plid,
 		String lifecycle) {
 
-		return new PortletURLImpl(request, portletId, plid, lifecycle);
+		return new PortletURLImpl(
+			request, portletId,
+			_getLayout((Layout)request.getAttribute(WebKeys.LAYOUT), plid),
+			lifecycle);
 	}
 
 	@Override
@@ -56,11 +61,13 @@ public class PortletURLFactoryImpl implements PortletURLFactory {
 
 		Layout layout = themeDisplay.getLayout();
 
-		if (layout != null) {
-			return create(request, portletId, layout, lifecycle);
+		if (layout == null) {
+			layout = _getLayout(
+				(Layout)request.getAttribute(WebKeys.LAYOUT),
+				themeDisplay.getPlid());
 		}
 
-		return create(request, portletId, themeDisplay.getPlid(), lifecycle);
+		return create(request, portletId, layout, lifecycle);
 	}
 
 	@Override
@@ -76,7 +83,11 @@ public class PortletURLFactoryImpl implements PortletURLFactory {
 		PortletRequest portletRequest, String portletId, long plid,
 		String lifecycle) {
 
-		return new PortletURLImpl(portletRequest, portletId, plid, lifecycle);
+		return new PortletURLImpl(
+			portletRequest, portletId,
+			_getLayout(
+				(Layout)portletRequest.getAttribute(WebKeys.LAYOUT), plid),
+			lifecycle);
 	}
 
 	@Override
@@ -88,12 +99,27 @@ public class PortletURLFactoryImpl implements PortletURLFactory {
 
 		Layout layout = themeDisplay.getLayout();
 
-		if (layout != null) {
-			return create(portletRequest, portletId, layout, lifecycle);
+		if (layout == null) {
+			layout = _getLayout(
+				(Layout)portletRequest.getAttribute(WebKeys.LAYOUT),
+				themeDisplay.getPlid());
 		}
 
-		return create(
-			portletRequest, portletId, themeDisplay.getPlid(), lifecycle);
+		return create(portletRequest, portletId, layout, lifecycle);
+	}
+
+	private Layout _getLayout(Layout layout, long plid) {
+		if ((layout != null) && (layout.getPlid() == plid) &&
+			(layout instanceof VirtualLayout)) {
+
+			return layout;
+		}
+
+		if (plid > 0) {
+			return LayoutLocalServiceUtil.fetchLayout(plid);
+		}
+
+		return null;
 	}
 
 }
