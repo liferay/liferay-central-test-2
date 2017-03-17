9d807efacf9cc52bcd9866b6f46eb9b0e4bb6685
Tibor Lipusz <tibor.lipusz@liferay.com>
2017-03-17 10:54:50 +0100

    LPS-53392 Build upgrade table for Entry
    1- Run ant setup-sdk -Dupgrade.table.dir=src/main/java/com/liferay/contacts/internal/upgrade/v2_0_1/util from workspace root
    2- Delete settings.gradle from contacts root
    3- Run gradlew buildUpgradeTable from contacts-service

--- modules/apps/foundation/contacts/contacts-service/src/main/java/com/liferay/contacts/internal/upgrade/v2_0_1/util/EntryTable.java
+++ modules/apps/foundation/contacts/contacts-service/src/main/java/com/liferay/contacts/internal/upgrade/v2_0_1/util/EntryTable.java
@@ -1,1 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.contacts.internal.upgrade.v2_0_1.util;
+
+import java.sql.Types;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author	  Brian Wing Shun Chan
+ * @generated
+ */
+public class EntryTable {
+
+	public static final String TABLE_NAME = "Contacts_Entry";
+
+	public static final Object[][] TABLE_COLUMNS = {
+		{"entryId", Types.BIGINT},
+		{"groupId", Types.BIGINT},
+		{"companyId", Types.BIGINT},
+		{"userId", Types.BIGINT},
+		{"userName", Types.VARCHAR},
+		{"createDate", Types.TIMESTAMP},
+		{"modifiedDate", Types.TIMESTAMP},
+		{"fullName", Types.VARCHAR},
+		{"emailAddress", Types.VARCHAR},
+		{"comments", Types.VARCHAR}
+	};
+
+	public static final Map<String, Integer> TABLE_COLUMNS_MAP = new HashMap<String, Integer>();
+
+static {
+TABLE_COLUMNS_MAP.put("entryId", Types.BIGINT);
+
+TABLE_COLUMNS_MAP.put("groupId", Types.BIGINT);
+
+TABLE_COLUMNS_MAP.put("companyId", Types.BIGINT);
+
+TABLE_COLUMNS_MAP.put("userId", Types.BIGINT);
+
+TABLE_COLUMNS_MAP.put("userName", Types.VARCHAR);
+
+TABLE_COLUMNS_MAP.put("createDate", Types.TIMESTAMP);
+
+TABLE_COLUMNS_MAP.put("modifiedDate", Types.TIMESTAMP);
+
+TABLE_COLUMNS_MAP.put("fullName", Types.VARCHAR);
+
+TABLE_COLUMNS_MAP.put("emailAddress", Types.VARCHAR);
+
+TABLE_COLUMNS_MAP.put("comments", Types.VARCHAR);
+
+}
+	public static final String TABLE_SQL_CREATE = "create table Contacts_Entry (entryId LONG not null primary key,groupId LONG,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,fullName VARCHAR(75) null,emailAddress VARCHAR(254) null,comments STRING null)";
+
+	public static final String TABLE_SQL_DROP = "drop table Contacts_Entry";
+
+	public static final String[] TABLE_SQL_ADD_INDEXES = {
+	};
+
+}
