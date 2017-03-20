c364f44dd6be5b0ce52d63322a39fcb857669762
hai.yu <8219754@liferay.com>
2017-03-20 11:53:24 +0800

    LPS-71271 Add same fix in WikiPage->History->Activities

--- modules/apps/collaboration/wiki/wiki-web/src/main/java/com/liferay/wiki/web/internal/display/context/util/WikiSocialActivityHelper.java
+++ modules/apps/collaboration/wiki/wiki-web/src/main/java/com/liferay/wiki/web/internal/display/context/util/WikiSocialActivityHelper.java
@@ -135,7 +135,7 @@ public class WikiSocialActivityHelper {
 
 			String url = getDownloadURL(fileEntryId);
 
-			String titleLink = getLink(title, url);
+			String titleLink = getLink(title, type, url);
 
 			return LanguageUtil.format(
 				resourceBundle, label, new Object[] {userName, titleLink},
@@ -177,7 +177,7 @@ public class WikiSocialActivityHelper {
 					false);
 			}
 			else if (type == SocialActivityConstants.TYPE_RESTORE_FROM_TRASH) {
-				String titleLink = getLink(page.getTitle(), pageURL);
+				String titleLink = getLink(page.getTitle(), type, pageURL);
 
 				return LanguageUtil.format(
 					resourceBundle, "activity-wiki-page-restore-from-trash",
@@ -185,7 +185,8 @@ public class WikiSocialActivityHelper {
 					false);
 			}
 			else if (type == WikiActivityKeys.ADD_PAGE) {
-				String titleLink = getLink(page.getTitle(), pageURL.toString());
+				String titleLink = getLink(
+					page.getTitle(), type, pageURL.toString());
 
 				return LanguageUtil.format(
 					resourceBundle, "x-added-the-page-x",
@@ -203,7 +204,7 @@ public class WikiSocialActivityHelper {
 						LanguageUtil.get(resourceBundle, "minor-edit"));
 				}
 
-				String titleURL = getLink(title, url);
+				String titleURL = getLink(title, type, url);
 
 				return LanguageUtil.format(
 					resourceBundle, "x-updated-the-page-to-version-x",
@@ -283,14 +284,24 @@ public class WikiSocialActivityHelper {
 		return StringPool.BLANK;
 	}
 
-	protected String getLink(String title, String url) {
+	protected String getLink(String title, int type, String url) {
 		if (Validator.isNull(url)) {
 			return title;
 		}
 
 		StringBundler sb = new StringBundler(5);
 
-		sb.append("<a href='");
+		if ((type == SocialActivityConstants.TYPE_ADD_ATTACHMENT) ||
+			(type == SocialActivityConstants.TYPE_MOVE_ATTACHMENT_TO_TRASH) ||
+			(type ==
+				SocialActivityConstants.TYPE_RESTORE_ATTACHMENT_FROM_TRASH)) {
+
+			sb.append("<a data-senna-off=\"true\" href='");
+		}
+		else {
+			sb.append("<a href='");
+		}
+
 		sb.append(url);
 		sb.append("'>");
 		sb.append(title);
