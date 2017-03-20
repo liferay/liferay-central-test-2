d56160ece30d05cd7b0c553b14dab5368c18268b
Pei-Jung Lan <pei-jung.lan@liferay.com>
2017-03-20 16:11:52 -0700

    LPS-67830 Only store properties that have been set

--- modules/apps/foundation/captcha/captcha-api/src/main/java/com/liferay/captcha/internal/upgrade/v1_0_0/UpgradeCaptchaConfiguration.java
+++ modules/apps/foundation/captcha/captcha-api/src/main/java/com/liferay/captcha/internal/upgrade/v1_0_0/UpgradeCaptchaConfiguration.java
@@ -16,7 +16,6 @@ package com.liferay.captcha.internal.upgrade.v1_0_0;
 
 import com.liferay.captcha.configuration.CaptchaConfiguration;
 import com.liferay.captcha.constants.LegacyCaptchaPropsKeys;
-import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.kernel.util.PrefsProps;
@@ -52,119 +51,194 @@ public class UpgradeCaptchaConfiguration extends UpgradeProcess {
 			properties = new HashMapDictionary();
 		}
 
-		CaptchaConfiguration defaultConfig =
-			ConfigurableUtil.createConfigurable(
-				CaptchaConfiguration.class, properties);
+		if (Validator.isNotNull(_prefsProps.getString(
+				LegacyCaptchaPropsKeys.CAPTCHA_ENGINE_IMPL))) {
 
-		properties.put(
-			"captchaEngine",
-			_prefsProps.getString(
-				LegacyCaptchaPropsKeys.CAPTCHA_ENGINE_IMPL,
-				defaultConfig.captchaEngine()));
-		properties.put(
-			"createAccountCaptchaEnabled",
-			_prefsProps.getBoolean(
-				LegacyCaptchaPropsKeys.CAPTCHA_CHECK_PORTAL_CREATE_ACCOUNT,
-				defaultConfig.createAccountCaptchaEnabled()));
-		properties.put(
-			"maxChallenges",
-			_prefsProps.getInteger(
-				LegacyCaptchaPropsKeys.CAPTCHA_MAX_CHALLENGES,
-				defaultConfig.maxChallenges()));
-		properties.put(
-			"messageBoardsEditCategoryCaptchaEnabled",
-			_prefsProps.getBoolean(
-				LegacyCaptchaPropsKeys.
-					CAPTCHA_CHECK_PORTLET_MESSAGE_BOARDS_EDIT_CATEGORY,
-				defaultConfig.messageBoardsEditCategoryCaptchaEnabled()));
-		properties.put(
-			"messageBoardsEditMessageCaptchaEnabled",
-			_prefsProps.getBoolean(
-				LegacyCaptchaPropsKeys.
-					CAPTCHA_CHECK_PORTLET_MESSAGE_BOARDS_EDIT_MESSAGE,
-				defaultConfig.messageBoardsEditMessageCaptchaEnabled()));
-		properties.put(
-			"reCaptchaNoScriptUrl",
-			_prefsProps.getString(
-				LegacyCaptchaPropsKeys.CAPTCHA_ENGINE_RECAPTCHA_URL_NOSCRIPT,
-				defaultConfig.reCaptchaNoScriptUrl()));
-
-		String reCaptchaPrivateKey = _prefsProps.getString(
-			LegacyCaptchaPropsKeys.CAPTCHA_ENGINE_RECAPTCHA_KEY_PRIVATE,
-			defaultConfig.reCaptchaPrivateKey());
-
-		if (Validator.isNull(reCaptchaPrivateKey)) {
-			reCaptchaPrivateKey = StringPool.BLANK;
+			properties.put(
+				"captchaEngine",
+				_prefsProps.getString(
+					LegacyCaptchaPropsKeys.CAPTCHA_ENGINE_IMPL));
 		}
 
-		properties.put("reCaptchaPrivateKey", reCaptchaPrivateKey);
+		if (Validator.isNotNull(_prefsProps.getString(
+				LegacyCaptchaPropsKeys.CAPTCHA_CHECK_PORTAL_CREATE_ACCOUNT))) {
 
-		String reCaptchaPublicKey = _prefsProps.getString(
-			LegacyCaptchaPropsKeys.CAPTCHA_ENGINE_RECAPTCHA_KEY_PUBLIC,
-			defaultConfig.reCaptchaPublicKey());
-
-		if (Validator.isNull(reCaptchaPublicKey)) {
-			reCaptchaPublicKey = StringPool.BLANK;
+			properties.put(
+				"createAccountCaptchaEnabled",
+				_prefsProps.getBoolean(
+					LegacyCaptchaPropsKeys.
+						CAPTCHA_CHECK_PORTAL_CREATE_ACCOUNT));
 		}
 
-		properties.put("reCaptchaPublicKey", reCaptchaPublicKey);
+		if (Validator.isNotNull(_prefsProps.getString(
+				LegacyCaptchaPropsKeys.CAPTCHA_MAX_CHALLENGES))) {
 
-		properties.put(
-			"reCaptchaScriptUrl",
-			_prefsProps.getString(
-				LegacyCaptchaPropsKeys.CAPTCHA_ENGINE_RECAPTCHA_URL_SCRIPT,
-				defaultConfig.reCaptchaScriptUrl()));
-		properties.put(
-			"reCaptchaVerifyUrl",
-			_prefsProps.getString(
-				LegacyCaptchaPropsKeys.CAPTCHA_ENGINE_RECAPTCHA_URL_VERIFY,
-				defaultConfig.reCaptchaVerifyUrl()));
-		properties.put(
-			"sendPasswordCaptchaEnabled",
-			_prefsProps.getBoolean(
-				LegacyCaptchaPropsKeys.CAPTCHA_CHECK_PORTAL_SEND_PASSWORD,
-				defaultConfig.sendPasswordCaptchaEnabled()));
-		properties.put(
-			"simpleCaptchaBackgroundProducers",
-			_prefsProps.getStringArray(
+			properties.put(
+				"maxChallenges",
+				_prefsProps.getInteger(
+					LegacyCaptchaPropsKeys.CAPTCHA_MAX_CHALLENGES));
+		}
+
+		if (Validator.isNotNull(_prefsProps.getString(
 				LegacyCaptchaPropsKeys.
-					CAPTCHA_ENGINE_SIMPLECAPTCHA_BACKGROUND_PRODUCERS,
-				StringPool.COMMA,
-				defaultConfig.simpleCaptchaBackgroundProducers()));
-		properties.put(
-			"simpleCaptchaGimpyRenderers",
-			_prefsProps.getStringArray(
+					CAPTCHA_CHECK_PORTLET_MESSAGE_BOARDS_EDIT_CATEGORY))) {
+
+			properties.put(
+				"messageBoardsEditCategoryCaptchaEnabled",
+				_prefsProps.getBoolean(
+					LegacyCaptchaPropsKeys.
+						CAPTCHA_CHECK_PORTLET_MESSAGE_BOARDS_EDIT_CATEGORY));
+		}
+
+		if (Validator.isNotNull(_prefsProps.getString(
 				LegacyCaptchaPropsKeys.
-					CAPTCHA_ENGINE_SIMPLECAPTCHA_GIMPY_RENDERERS,
-				StringPool.COMMA, defaultConfig.simpleCaptchaGimpyRenderers()));
-		properties.put(
-			"simpleCaptchaHeight",
-			_prefsProps.getInteger(
-				LegacyCaptchaPropsKeys.CAPTCHA_ENGINE_SIMPLECAPTCHA_HEIGHT,
-				defaultConfig.simpleCaptchaHeight()));
-		properties.put(
-			"simpleCaptchaNoiseProducers",
-			_prefsProps.getStringArray(
+					CAPTCHA_CHECK_PORTLET_MESSAGE_BOARDS_EDIT_MESSAGE))) {
+
+			properties.put(
+				"messageBoardsEditMessageCaptchaEnabled",
+				_prefsProps.getBoolean(
+					LegacyCaptchaPropsKeys.
+						CAPTCHA_CHECK_PORTLET_MESSAGE_BOARDS_EDIT_MESSAGE));
+		}
+
+		if (Validator.isNotNull(_prefsProps.getString(
 				LegacyCaptchaPropsKeys.
-					CAPTCHA_ENGINE_SIMPLECAPTCHA_NOISE_PRODUCERS,
-				StringPool.COMMA, defaultConfig.simpleCaptchaNoiseProducers()));
-		properties.put(
-			"simpleCaptchaTextProducers",
-			_prefsProps.getStringArray(
+					CAPTCHA_ENGINE_RECAPTCHA_URL_NOSCRIPT))) {
+
+			properties.put(
+				"reCaptchaNoScriptUrl",
+				_prefsProps.getString(
+					LegacyCaptchaPropsKeys.
+						CAPTCHA_ENGINE_RECAPTCHA_URL_NOSCRIPT));
+		}
+
+		if (Validator.isNotNull(_prefsProps.getString(
+				LegacyCaptchaPropsKeys.CAPTCHA_ENGINE_RECAPTCHA_KEY_PRIVATE))) {
+
+			properties.put(
+				"reCaptchaPrivateKey",
+				_prefsProps.getString(
+					LegacyCaptchaPropsKeys.
+						CAPTCHA_ENGINE_RECAPTCHA_KEY_PRIVATE));
+		}
+
+		if (Validator.isNotNull(_prefsProps.getString(
+				LegacyCaptchaPropsKeys.CAPTCHA_ENGINE_RECAPTCHA_KEY_PUBLIC))) {
+
+			properties.put(
+				"reCaptchaPublicKey",
+				_prefsProps.getString(
+					LegacyCaptchaPropsKeys.
+						CAPTCHA_ENGINE_RECAPTCHA_KEY_PUBLIC));
+		}
+
+		if (Validator.isNotNull(_prefsProps.getString(
+				LegacyCaptchaPropsKeys.CAPTCHA_ENGINE_RECAPTCHA_URL_SCRIPT))) {
+
+			properties.put(
+				"reCaptchaScriptUrl",
+				_prefsProps.getString(
+					LegacyCaptchaPropsKeys.
+						CAPTCHA_ENGINE_RECAPTCHA_URL_SCRIPT));
+		}
+
+		if (Validator.isNotNull(_prefsProps.getString(
+				LegacyCaptchaPropsKeys.CAPTCHA_ENGINE_RECAPTCHA_URL_VERIFY))) {
+
+			properties.put(
+				"reCaptchaVerifyUrl",
+				_prefsProps.getString(
+					LegacyCaptchaPropsKeys.
+						CAPTCHA_ENGINE_RECAPTCHA_URL_VERIFY));
+		}
+
+		if (Validator.isNotNull(_prefsProps.getString(
+				LegacyCaptchaPropsKeys.CAPTCHA_CHECK_PORTAL_SEND_PASSWORD))) {
+
+			properties.put(
+				"sendPasswordCaptchaEnabled",
+				_prefsProps.getBoolean(
+					LegacyCaptchaPropsKeys.CAPTCHA_CHECK_PORTAL_SEND_PASSWORD));
+		}
+
+		if (Validator.isNotNull(_prefsProps.getString(
 				LegacyCaptchaPropsKeys.
-					CAPTCHA_ENGINE_SIMPLECAPTCHA_TEXT_PRODUCERS,
-				StringPool.COMMA, defaultConfig.simpleCaptchaTextProducers()));
-		properties.put(
-			"simpleCaptchaWidth",
-			_prefsProps.getInteger(
-				LegacyCaptchaPropsKeys.CAPTCHA_ENGINE_SIMPLECAPTCHA_WIDTH,
-				defaultConfig.simpleCaptchaWidth()));
-		properties.put(
-			"simpleCaptchaWordRenderers",
-			_prefsProps.getStringArray(
+					CAPTCHA_ENGINE_SIMPLECAPTCHA_BACKGROUND_PRODUCERS))) {
+
+			properties.put(
+				"simpleCaptchaBackgroundProducers",
+				_prefsProps.getStringArray(
+					LegacyCaptchaPropsKeys.
+						CAPTCHA_ENGINE_SIMPLECAPTCHA_BACKGROUND_PRODUCERS,
+					StringPool.COMMA));
+		}
+
+		if (Validator.isNotNull(_prefsProps.getString(
 				LegacyCaptchaPropsKeys.
-					CAPTCHA_ENGINE_SIMPLECAPTCHA_WORD_RENDERERS,
-				StringPool.COMMA, defaultConfig.simpleCaptchaWordRenderers()));
+					CAPTCHA_ENGINE_SIMPLECAPTCHA_GIMPY_RENDERERS))) {
+
+			properties.put(
+				"simpleCaptchaGimpyRenderers",
+				_prefsProps.getStringArray(
+					LegacyCaptchaPropsKeys.
+						CAPTCHA_ENGINE_SIMPLECAPTCHA_GIMPY_RENDERERS,
+					StringPool.COMMA));
+		}
+
+		if (Validator.isNotNull(_prefsProps.getString(
+				LegacyCaptchaPropsKeys.CAPTCHA_ENGINE_SIMPLECAPTCHA_HEIGHT))) {
+
+			properties.put(
+				"simpleCaptchaHeight",
+				_prefsProps.getInteger(
+					LegacyCaptchaPropsKeys.
+						CAPTCHA_ENGINE_SIMPLECAPTCHA_HEIGHT));
+		}
+
+		if (Validator.isNotNull(_prefsProps.getString(
+				LegacyCaptchaPropsKeys.
+					CAPTCHA_ENGINE_SIMPLECAPTCHA_NOISE_PRODUCERS))) {
+
+			properties.put(
+				"simpleCaptchaNoiseProducers",
+				_prefsProps.getStringArray(
+					LegacyCaptchaPropsKeys.
+						CAPTCHA_ENGINE_SIMPLECAPTCHA_NOISE_PRODUCERS,
+					StringPool.COMMA));
+		}
+
+		if (Validator.isNotNull(_prefsProps.getString(
+				LegacyCaptchaPropsKeys.
+					CAPTCHA_ENGINE_SIMPLECAPTCHA_TEXT_PRODUCERS))) {
+
+			properties.put(
+				"simpleCaptchaTextProducers",
+				_prefsProps.getStringArray(
+					LegacyCaptchaPropsKeys.
+						CAPTCHA_ENGINE_SIMPLECAPTCHA_TEXT_PRODUCERS,
+					StringPool.COMMA));
+		}
+
+		if (Validator.isNotNull(_prefsProps.getString(
+				LegacyCaptchaPropsKeys.CAPTCHA_ENGINE_SIMPLECAPTCHA_WIDTH))) {
+
+			properties.put(
+				"simpleCaptchaWidth",
+				_prefsProps.getInteger(
+					LegacyCaptchaPropsKeys.CAPTCHA_ENGINE_SIMPLECAPTCHA_WIDTH));
+		}
+
+		if (Validator.isNotNull(_prefsProps.getString(
+				LegacyCaptchaPropsKeys.
+					CAPTCHA_ENGINE_SIMPLECAPTCHA_WORD_RENDERERS))) {
+
+			properties.put(
+				"simpleCaptchaWordRenderers",
+				_prefsProps.getStringArray(
+					LegacyCaptchaPropsKeys.
+						CAPTCHA_ENGINE_SIMPLECAPTCHA_WORD_RENDERERS,
+					StringPool.COMMA));
+		}
 
 		configuration.update(properties);
 
