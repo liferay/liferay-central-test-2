3204034d4be17116fc23cfe61940c2cf5a6e12ae
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2017-03-21 14:15:35 +0100

    LPS-69436 Rename method

--- modules/apps/collaboration/flags/flags-taglib/src/main/java/com/liferay/flags/taglib/servlet/taglib/soy/FlagsTag.java
+++ modules/apps/collaboration/flags/flags-taglib/src/main/java/com/liferay/flags/taglib/servlet/taglib/soy/FlagsTag.java
@@ -67,7 +67,7 @@ public class FlagsTag extends TemplateRendererTag {
 
 			putValue("cssClass", _getCssClass(randomNamespace, inTrash));
 
-			putValue("data", _getDataJSON(context, className, classPK));
+			putValue("data", _getDataJSONObject(context, className, classPK));
 
 			putValue("flagsEnabled", _isFlagsEnabled(themeDisplay));
 
@@ -170,7 +170,7 @@ public class FlagsTag extends TemplateRendererTag {
 		return currentURL;
 	}
 
-	private JSONObject _getDataJSON(
+	private JSONObject _getDataJSONObject(
 		Map<String, Object> context, String className, long classPK) {
 
 		String namespace = PortalUtil.getPortletNamespace(PortletKeys.FLAGS);
