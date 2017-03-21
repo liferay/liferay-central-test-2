c1f2ef01e319d5f6591a7f90a383f9615413c07d
Carlos Lancha <carlos.lancha@liferay.com>
2017-03-21 11:35:10 +0100

    LPS-64308 SF and jsdocs

--- modules/apps/web-experience/portlet-configuration/portlet-configuration-css-web/src/main/resources/META-INF/resources/js/ColorPickerInput.es.js
+++ modules/apps/web-experience/portlet-configuration/portlet-configuration-css-web/src/main/resources/META-INF/resources/js/ColorPickerInput.es.js
@@ -1,9 +1,14 @@
-import Component from 'metal-component/src/Component';
-import core from 'metal/src/core';
-import Soy from 'metal-soy/src/Soy';
+import Component from 'metal-component';
+import core from 'metal';
+import Soy from 'metal-soy';
 
 import templates from './ColorPickerInput.soy';
 
+/**
+ * ColorPickerInput
+ *
+ * This class creates an input with a ColorPickerPopover binded to it.
+ */
 class ColorPickerInput extends Component {
 
 	/**
@@ -27,33 +32,75 @@ class ColorPickerInput extends Component {
 		});
 	}
 
+	/**
+	 * Sets the selected color
+	 *
+	 * @param  {Event} event
+	 */
 	setColor_(event) {
 		this.color = event.color;
 	}
 }
 
+/**
+ * State definition.
+ * @type {!Object}
+ * @static
+ */
 ColorPickerInput.STATE = {
+	/**
+	 * Input css class
+	 * @type {String}
+	 */
+	cssClass: {
+		validator: core.isString
+	},
+
+	/**
+	 * Input disabled state
+	 * @type {Boolean}
+	 */
 	disabled: {
 		validator: core.isBoolean,
 		value: false
 	},
 
+	/**
+	 * Id for the input
+	 * @type {String}
+	 */
 	id: {
 		validator: core.isString
 	},
 
+	/**
+	 * Label for the input
+	 * @type {String}
+	 */
 	label: {
 		validator: core.isString
 	},
 
+	/**
+	 * Name for the input
+	 * @type {String}
+	 */
 	name: {
 		validator: core.isString
 	},
 
+	/**
+	 * Current selected color
+	 * @type {String}
+	 */
 	color: {
 		validator: core.isString
 	},
 
+	/**
+	 * Form group css class
+	 * @type {String}
+	 */
 	wrapperCssClass: {
 		validator: core.isString
 	}
--- modules/apps/web-experience/portlet-configuration/portlet-configuration-css-web/src/main/resources/META-INF/resources/js/ToggleDisableInputs.es.js
+++ modules/apps/web-experience/portlet-configuration/portlet-configuration-css-web/src/main/resources/META-INF/resources/js/ToggleDisableInputs.es.js
@@ -1,16 +1,29 @@
-import Component from 'metal-component/src/Component';
-import core from 'metal/src/core';
-import Soy from 'metal-soy/src/Soy';
+import Component from 'metal-component';
+import core from 'metal';
+import Soy from 'metal-soy';
 
 import templates from './ToggleDisableInputs.soy';
 
+/**
+ * ToggleDisableInputs
+ *
+ * This class creates a switch button that enable/disable different inputs
+ * based on its state.
+ */
 class ToggleDisableInputs extends Component {
+
+	/**
+	 * Toggles the state of the switch
+	 */
 	toggleSwitch() {
 		this.checked = !this.checked;
 
 		this.disableInputs_();
 	}
 
+	/**
+	 * Disables the inputs based on switch state and disableOnChecked logic
+	 */
 	disableInputs_() {
 		let inputs = document.querySelectorAll(this.inputSelector);
 
@@ -27,21 +40,42 @@ class ToggleDisableInputs extends Component {
 	}
 }
 
+/**
+ * State definition.
+ * @type {!Object}
+ * @static
+ */
 ToggleDisableInputs.STATE = {
+	/**
+	 * Switch state
+	 * @type {Boolean}
+	 */
 	checked: {
 		validator: core.isBoolean,
 		value: false
 	},
 
+	/**
+	 * CSS Selector for the inputs to enable/disable
+	 * @type {String}
+	 */
 	inputSelector: {
 		validator: core.isString,
 		value: []
 	},
 
+	/**
+	 * Label of the switch
+	 * @type {String}
+	 */
 	label: {
 		validator: core.isString
 	},
 
+	/**
+	 * Flag to specify the logic for disabling inputs based on switch state
+	 * @type {Boolean}
+	 */
 	disableOnChecked: {
 		validator: core.isBoolean,
 		value: true
