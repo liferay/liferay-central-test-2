e509c1e09ae919c21c7f761e5158f43a87495d3f
Hugo Huijser <hugo.huijser@liferay.com>
2017-03-23 08:30:22 +0800

    LPS-71164 Move logic that checks tag attributes in *.jsp to new class

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JSPTagAttributesCheck.java
@@ -0,0 +1,325 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
+import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
+import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.TextFormatter;
+import com.liferay.portal.kernel.util.Tuple;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.source.formatter.SourceFormatterMessage;
+
+import com.thoughtworks.qdox.model.JavaClass;
+import com.thoughtworks.qdox.model.JavaMethod;
+import com.thoughtworks.qdox.model.Type;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Hugo Huijser
+ */
+public class JSPTagAttributesCheck extends TagAttributesCheck {
+
+	public JSPTagAttributesCheck(
+		boolean portalSource, boolean subrepository,
+		Set<String> primitiveTagAttributeDataTypes,
+		Map<String, JavaClass> tagJavaClassesMap) {
+
+		_portalSource = portalSource;
+		_subrepository = subrepository;
+		_primitiveTagAttributeDataTypes = primitiveTagAttributeDataTypes;
+		_tagJavaClassesMap = tagJavaClassesMap;
+	}
+
+	@Override
+	public Tuple process(String fileName, String absolutePath, String content)
+		throws Exception {
+
+		Set<SourceFormatterMessage> sourceFormatterMessages = new HashSet<>();
+
+		content = _formatSingleLineTagAttribues(
+			sourceFormatterMessages, fileName, content);
+
+		content = _formatMultiLinesTagAttribues(
+			sourceFormatterMessages, fileName, content);
+
+		return new Tuple(content, sourceFormatterMessages);
+	}
+
+	@Override
+	protected String formatTagAttributeType(
+			String line, String tagName, String attributeAndValue)
+		throws Exception {
+
+		if (attributeAndValue.matches(
+				".*=\"<%= Boolean\\.(FALSE|TRUE) %>\".*")) {
+
+			String newAttributeAndValue = StringUtil.replace(
+				attributeAndValue,
+				new String[] {
+					"=\"<%= Boolean.FALSE %>\"", "=\"<%= Boolean.TRUE %>\""
+				},
+				new String[] {"=\"<%= false %>\"", "=\"<%= true %>\""});
+
+			return StringUtil.replace(
+				line, attributeAndValue, newAttributeAndValue);
+		}
+
+		if (!_portalSource && !_subrepository) {
+			return line;
+		}
+
+		if (!attributeAndValue.endsWith(StringPool.QUOTE) ||
+			attributeAndValue.contains("\"<%=")) {
+
+			return line;
+		}
+
+		JavaClass tagJavaClass = _tagJavaClassesMap.get(tagName);
+
+		if (tagJavaClass == null) {
+			return line;
+		}
+
+		int pos = attributeAndValue.indexOf("=\"");
+
+		String attribute = attributeAndValue.substring(0, pos);
+
+		String setAttributeMethodName =
+			"set" + TextFormatter.format(attribute, TextFormatter.G);
+
+		for (String dataType : _primitiveTagAttributeDataTypes) {
+			Type javaType = new Type(dataType);
+
+			JavaMethod setAttributeMethod = null;
+
+			while (true) {
+
+				// com.thoughtworks.qdox.model.JavaClass is not thread-safe and
+				// can throw NPE as a result of a race condition
+
+				try {
+					setAttributeMethod = tagJavaClass.getMethodBySignature(
+						setAttributeMethodName, new Type[] {javaType}, true);
+
+					break;
+				}
+				catch (Exception e) {
+				}
+			}
+
+			if (setAttributeMethod != null) {
+				String value = attributeAndValue.substring(
+					pos + 2, attributeAndValue.length() - 1);
+
+				if (!_isValidTagAttributeValue(value, dataType)) {
+					return line;
+				}
+
+				String newAttributeAndValue = StringUtil.replace(
+					attributeAndValue,
+					StringPool.QUOTE + value + StringPool.QUOTE,
+					"\"<%= " + value + " %>\"");
+
+				return StringUtil.replace(
+					line, attributeAndValue, newAttributeAndValue);
+			}
+		}
+
+		if (!attributeAndValue.matches(".*=\"(false|true)\".*")) {
+			return line;
+		}
+
+		JavaMethod setAttributeMethod = tagJavaClass.getMethodBySignature(
+			setAttributeMethodName, new Type[] {new Type("java.lang.String")},
+			true);
+
+		if (setAttributeMethod == null) {
+			return line;
+		}
+
+		String newAttributeAndValue = StringUtil.replace(
+			attributeAndValue, new String[] {"=\"false\"", "=\"true\""},
+			new String[] {
+				"=\"<%= Boolean.FALSE.toString() %>\"",
+				"=\"<%= Boolean.TRUE.toString() %>\""
+			});
+
+		return StringUtil.replace(
+			line, attributeAndValue, newAttributeAndValue);
+	}
+
+	@Override
+	protected String sortHTMLTagAttributes(
+		String line, String value, String attributeAndValue) {
+
+		if (!value.matches("([-a-z0-9]+ )+[-a-z0-9]+")) {
+			return line;
+		}
+
+		List<String> htmlAttributes = ListUtil.fromArray(
+			StringUtil.split(value, StringPool.SPACE));
+
+		Collections.sort(htmlAttributes);
+
+		String newValue = StringUtil.merge(htmlAttributes, StringPool.SPACE);
+
+		if (value.equals(newValue)) {
+			return line;
+		}
+
+		String newAttributeAndValue = StringUtil.replace(
+			attributeAndValue, value, newValue);
+
+		return StringUtil.replace(
+			line, attributeAndValue, newAttributeAndValue);
+	}
+
+	private String _formatMultiLinesTagAttribues(
+			Set<SourceFormatterMessage> sourceFormatterMessages,
+			String fileName, String content)
+		throws Exception {
+
+		Matcher matcher = _multilineTagPattern.matcher(content);
+
+		while (matcher.find()) {
+			char beforeClosingTagChar = content.charAt(matcher.start(2) - 1);
+
+			if ((beforeClosingTagChar != CharPool.NEW_LINE) &&
+				(beforeClosingTagChar != CharPool.TAB)) {
+
+				String closingTag = matcher.group(2);
+
+				String whitespace = matcher.group(1);
+
+				String tabs = StringUtil.removeChar(
+					whitespace, CharPool.NEW_LINE);
+
+				return StringUtil.replaceFirst(
+					content, closingTag, "\n" + tabs + closingTag,
+					matcher.start(2));
+			}
+
+			String tag = matcher.group();
+
+			String singlelineTag = StringUtil.removeChar(
+				StringUtil.trim(tag), CharPool.TAB);
+
+			singlelineTag = StringUtil.replace(
+				singlelineTag, CharPool.NEW_LINE, CharPool.SPACE);
+
+			String newTag = formatTagAttributes(
+				sourceFormatterMessages, fileName, tag, singlelineTag,
+				getLineCount(content, matcher.end(1)), false);
+
+			if (!tag.equals(newTag)) {
+				return StringUtil.replace(content, tag, newTag);
+			}
+		}
+
+		return content;
+	}
+
+	private String _formatSingleLineTagAttribues(
+			Set<SourceFormatterMessage> sourceFormatterMessages,
+			String fileName, String content)
+		throws Exception {
+
+		StringBundler sb = new StringBundler();
+
+		try (UnsyncBufferedReader unsyncBufferedReader =
+				new UnsyncBufferedReader(new UnsyncStringReader(content))) {
+
+			int lineCount = 0;
+
+			String line = null;
+
+			while ((line = unsyncBufferedReader.readLine()) != null) {
+				lineCount++;
+
+				String trimmedLine = StringUtil.trimLeading(line);
+
+				if (trimmedLine.matches("<\\w+ .*>.*")) {
+					line = formatTagAttributes(
+						sourceFormatterMessages, fileName, line, trimmedLine,
+						lineCount, false);
+				}
+
+				Matcher matcher = _jspTaglibPattern.matcher(line);
+
+				while (matcher.find()) {
+					line = formatTagAttributes(
+						sourceFormatterMessages, fileName, line,
+						line.substring(matcher.start()), lineCount, false);
+				}
+
+				sb.append(line);
+				sb.append("\n");
+			}
+		}
+
+		content = sb.toString();
+
+		if (content.endsWith("\n")) {
+			content = content.substring(0, content.length() - 1);
+		}
+
+		return content;
+	}
+
+	private boolean _isValidTagAttributeValue(String value, String dataType) {
+		if (dataType.equals("boolean")) {
+			return Validator.isBoolean(value);
+		}
+
+		if (dataType.equals("double")) {
+			try {
+				Double.parseDouble(value);
+			}
+			catch (NumberFormatException nfe) {
+				return false;
+			}
+
+			return true;
+		}
+
+		if (dataType.equals("int") || dataType.equals("long")) {
+			return Validator.isNumber(value);
+		}
+
+		return false;
+	}
+
+	private final Pattern _jspTaglibPattern = Pattern.compile(
+		"<[-\\w]+:[-\\w]+ .");
+	private final Pattern _multilineTagPattern = Pattern.compile(
+		"(\\s+)<[-\\w]+:[-\\w]+\n.*?(/?>)(\n|$)", Pattern.DOTALL);
+	private final boolean _portalSource;
+	private final Set<String> _primitiveTagAttributeDataTypes;
+	private final boolean _subrepository;
+	private final Map<String, JavaClass> _tagJavaClassesMap;
+
+}
---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/TagAttributesCheck.java
@@ -0,0 +1,242 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.source.formatter.SourceFormatterMessage;
+
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Hugo Huijser
+ */
+public abstract class TagAttributesCheck extends BaseFileCheck {
+
+	protected String formatTagAttributes(
+			Set<SourceFormatterMessage> sourceFormatterMessages,
+			String fileName, String line, String tag, int lineCount,
+			boolean escapeQuotes)
+		throws Exception {
+
+		String s = tag;
+
+		int y = s.indexOf(CharPool.SPACE);
+
+		if (y == -1) {
+			return line;
+		}
+
+		String tagName = s.substring(1, y);
+
+		s = s.substring(y + 1);
+
+		String previousAttribute = null;
+		String previousAttributeAndValue = null;
+
+		boolean wrongOrder = false;
+
+		for (int x = 0;;) {
+			x = s.indexOf(CharPool.EQUAL);
+
+			if ((x == -1) || (s.length() <= (x + 1))) {
+				return line;
+			}
+
+			String attribute = s.substring(0, x);
+
+			String trimmedAttribute = StringUtil.trim(attribute);
+
+			if (!_isAttributName(trimmedAttribute)) {
+				return line;
+			}
+
+			if (!attribute.equals(trimmedAttribute)) {
+				return StringUtil.replace(
+					line, attribute + "=", trimmedAttribute + "=");
+			}
+
+			if (Validator.isNotNull(previousAttribute) &&
+				(previousAttribute.compareToIgnoreCase(attribute) > 0)) {
+
+				wrongOrder = true;
+			}
+
+			s = s.substring(x + 1);
+
+			char delimeter = s.charAt(0);
+
+			if ((delimeter != CharPool.APOSTROPHE) &&
+				(delimeter != CharPool.QUOTE)) {
+
+				if (delimeter == CharPool.SPACE) {
+					return StringUtil.replace(
+						line, attribute + "= ", attribute + "=");
+				}
+
+				if (delimeter != CharPool.AMPERSAND) {
+					addMessage(
+						sourceFormatterMessages, fileName,
+						"Incorrect delimeter '" + delimeter + "'", lineCount);
+				}
+
+				return line;
+			}
+
+			s = s.substring(1);
+
+			String value = null;
+
+			y = -1;
+
+			while (true) {
+				y = s.indexOf(delimeter, y + 1);
+
+				if ((y == -1) || (s.length() <= (y + 1))) {
+					return line;
+				}
+
+				value = s.substring(0, y);
+
+				if (value.startsWith("<%")) {
+					if (getLevel(value, "<%", "%>") == 0) {
+						break;
+					}
+				}
+				else if (getLevel(
+							value, StringPool.LESS_THAN,
+							StringPool.GREATER_THAN) == 0) {
+
+					break;
+				}
+			}
+
+			if (delimeter == CharPool.APOSTROPHE) {
+				if (escapeQuotes) {
+					String newValue = StringUtil.replace(
+						value, CharPool.QUOTE, "&quot;");
+
+					return StringUtil.replace(
+						line,
+						StringPool.APOSTROPHE + value + StringPool.APOSTROPHE,
+						StringPool.QUOTE + newValue + StringPool.QUOTE);
+				}
+
+				if (!value.contains(StringPool.QUOTE) ||
+					!tagName.contains(StringPool.COLON)) {
+
+					return StringUtil.replace(
+						line,
+						StringPool.APOSTROPHE + value + StringPool.APOSTROPHE,
+						StringPool.QUOTE + value + StringPool.QUOTE);
+				}
+			}
+
+			if ((delimeter == CharPool.QUOTE) &&
+				value.contains(StringPool.QUOTE) &&
+				tagName.contains(StringPool.COLON)) {
+
+				return StringUtil.replace(
+					line, StringPool.QUOTE + value + StringPool.QUOTE,
+					StringPool.APOSTROPHE + value + StringPool.APOSTROPHE);
+			}
+
+			StringBundler sb = new StringBundler(5);
+
+			sb.append(attribute);
+			sb.append(StringPool.EQUAL);
+			sb.append(delimeter);
+			sb.append(value);
+			sb.append(delimeter);
+
+			String currentAttributeAndValue = sb.toString();
+
+			if (!tagName.contains(StringPool.COLON)) {
+				String newLine = sortHTMLTagAttributes(
+					line, value, currentAttributeAndValue);
+
+				if (!newLine.equals(line)) {
+					return newLine;
+				}
+			}
+
+			String newLine = formatTagAttributeType(
+				line, tagName, currentAttributeAndValue);
+
+			if (!newLine.equals(line)) {
+				return newLine;
+			}
+
+			if (wrongOrder) {
+				if ((StringUtil.count(line, currentAttributeAndValue) == 1) &&
+					(StringUtil.count(line, previousAttributeAndValue) == 1)) {
+
+					line = StringUtil.replaceFirst(
+						line, previousAttributeAndValue,
+						currentAttributeAndValue);
+
+					return StringUtil.replaceLast(
+						line, currentAttributeAndValue,
+						previousAttributeAndValue);
+				}
+
+				return line;
+			}
+
+			s = s.substring(y + 1);
+
+			if (s.startsWith(StringPool.GREATER_THAN)) {
+				return line;
+			}
+
+			s = StringUtil.trimLeading(s);
+
+			previousAttribute = attribute;
+			previousAttributeAndValue = currentAttributeAndValue;
+		}
+	}
+
+	protected String formatTagAttributeType(
+			String line, String tagName, String attributeAndValue)
+		throws Exception {
+
+		return line;
+	}
+
+	protected String sortHTMLTagAttributes(
+		String line, String value, String attributeAndValue) {
+
+		return line;
+	}
+
+	private boolean _isAttributName(String attributeName) {
+		if (Validator.isNull(attributeName)) {
+			return false;
+		}
+
+		Matcher matcher = _attributeNamePattern.matcher(attributeName);
+
+		return matcher.matches();
+	}
+
+	private static final Pattern _attributeNamePattern = Pattern.compile(
+		"[a-z]+[-_a-zA-Z0-9]*");
+
+}
