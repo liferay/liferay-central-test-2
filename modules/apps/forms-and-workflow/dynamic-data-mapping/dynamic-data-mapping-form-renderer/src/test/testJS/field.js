683a50a51c2d4eee06764e03dd34196c90b11daa
Bruno Basto <bruno.basto@liferay.com>
2017-03-23 12:22:45 -0300

    LPS-71453 Add tests for dynamic-data-mapping-form-renderer

---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/test/testJS/field.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var A = AUI();
+
+var FieldTest;
+
+var createField = function(config) {
+	return new FieldTest(
+		A.merge(
+			{
+				type: 'field',
+				value: ''
+			},
+			config || {}
+		)
+	).render(document.body);
+};
+
+var changeInputValue = function(container) {
+	container.one('input').set('value', 'test');
+	container.one('input').simulate('change');
+};
+
+describe(
+	'DDM Field',
+	function() {
+		before(
+			function(done) {
+				A.use(
+					'liferay-ddm-form-renderer-field',
+					function(A) {
+						Liferay.DDM.Renderer.FieldTypes.register(
+							{
+								'javaScriptClass': 'Liferay.DDM.Renderer.Field',
+								'name': 'field'
+							}
+						);
+
+						FieldTest = A.Component.create(
+							{
+								EXTENDS: Liferay.DDM.Renderer.Field,
+								prototype: {
+									getTemplateRenderer: function() {
+										return function(context) {
+											return '<input name="' + context.name + '" />';
+										};
+									}
+								}
+							}
+						);
+
+						done();
+					}
+				);
+			}
+		);
+
+		describe(
+			'Events',
+			function() {
+				it(
+					'should fire valueChange event',
+					function(done) {
+						var field = createField(
+							{
+								after: {
+									valueChange: function() {
+										done();
+									}
+								}
+							}
+						);
+
+						var container = field.get('container');
+
+						changeInputValue(container);
+					}
+				);
+			}
+		);
+	}
+);
---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/test/testJS/field_repetition_support.js
@@ -0,0 +1,106 @@
+'use strict';
+
+var A = AUI();
+
+chai.config.truncateThreshold = 0;
+
+var createField = function(config) {
+	return new TestField(
+		A.merge(
+			{
+				type: 'field',
+				value: 'test',
+				context: {}
+			},
+			config || {}
+		)
+	).render(document.body);
+};
+
+describe(
+	'DDM Repetition Event Support',
+	function() {
+		before(
+			function(done) {
+				A.use(
+					'liferay-ddm-form-renderer-field',
+					function(A) {
+						Liferay.DDM.Renderer.FieldTypes.register(
+							{
+								'javaScriptClass': 'window.TestField',
+								'name': 'field',
+								'templateNamespace': 'ddm.test'
+							}
+						);
+
+						window.TestField = A.Component.create(
+							{
+								ATTRS: {},
+								EXTENDS: Liferay.DDM.Renderer.Field,
+								prototype: {
+									getTemplateRenderer: function() {
+										return function(context) {
+											return '<input class="field-test" name="' + context.name + '" />';
+										};
+									}
+								}
+							}
+						);
+
+						done();
+					}
+				);
+			}
+		);
+
+		describe(
+			'.copy()',
+			function() {
+				it(
+					'should create a copy of the Field',
+					function(done) {
+						var field = createField();
+						var copiedField = field.copy();
+
+						var fieldContext = field.get('context');
+						var copiedFieldContext = copiedField.get('context');
+
+						assert.isTrue(A.instanceOf(copiedField, field.getFieldClass()));
+
+						delete fieldContext.fieldName;
+						delete copiedFieldContext.fieldName;
+						delete fieldContext.repeatedIndex;
+						delete copiedFieldContext.repeatedIndex;
+
+						assert.deepEqual(fieldContext, copiedFieldContext);
+
+						field.destroy();
+						copiedField.destroy();
+
+						done();
+					}
+				);
+			}
+		);
+
+		describe(
+			'.repeat()',
+			function() {
+				it(
+					'should add a copy of the Field above itself when the repeat function is called',
+					function(done) {
+						var field = createField();
+
+						assert.equal(1, document.body.getElementsByClassName('field-test').length);
+
+						field.repeat();
+
+						assert.equal(2, document.body.getElementsByClassName('field-test').length);
+
+						done();
+					}
+				);
+			}
+		);
+	}
+);
