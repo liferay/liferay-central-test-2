c94b3d7114f96cab2c50e4450a0e012a17d35f04
Alejandro Tardín <alejandro.tardin@liferay.com>
2017-03-23 17:29:49 +0100

    LPS-71965 Returning the new content in the ajax response

--- modules/apps/collaboration/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/portlet/action/EditEntryMVCActionCommand.java
+++ modules/apps/collaboration/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/portlet/action/EditEntryMVCActionCommand.java
@@ -289,6 +289,7 @@ public class EditEntryMVCActionCommand extends BaseMVCActionCommand {
 				}
 
 				jsonObject.put("blogsEntryAttachmentReferences", jsonArray);
+				jsonObject.put("content", entry.getContent());
 				jsonObject.put(
 					"coverImageFileEntryId", entry.getCoverImageFileEntryId());
 				jsonObject.put("entryId", entry.getEntryId());
