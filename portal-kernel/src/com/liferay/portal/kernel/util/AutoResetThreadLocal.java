6e6e9cdb08b9efbf71ede32ffce31762a18a470d
Preston-Crary <preston.crary@liferay.com>
2017-03-24 11:14:57 -0700

    LPS-71491 Propagate change to AutoResetThreadLocal.

--- portal-kernel/src/com/liferay/portal/kernel/util/AutoResetThreadLocal.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/AutoResetThreadLocal.java
@@ -14,15 +14,27 @@
 
 package com.liferay.portal.kernel.util;
 
+import java.util.function.Supplier;
+
 /**
  * @author Shuyang Zhou
  */
 public class AutoResetThreadLocal<T> extends InitialThreadLocal<T> {
 
 	public AutoResetThreadLocal(String name) {
-		super(name, null, true);
+		super(name, () -> null, true);
 	}
 
+	public AutoResetThreadLocal(String name, Supplier<T> supplier) {
+		super(name, supplier, true);
+	}
+
+	/**
+	 * @deprecated As of 7.0.0, replaced by {@link #AutoResetThreadLocal(
+	 *             String, Supplier)}
+	 */
+	@Deprecated
+	@SuppressWarnings("deprecation")
 	public AutoResetThreadLocal(String name, T initialValue) {
 		super(name, initialValue, true);
 	}
