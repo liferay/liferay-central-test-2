ba9c433abbb8d2ab77b597c1be9061bd3920db82
Jorge Díaz <jorge.diaz@liferay.com>
2017-03-27 15:11:38 +0200

    LPS-71278 Add custom implementation of QNameCache with concurrent thread-safe HashMaps

---
+++ portal-impl/src/com/liferay/portal/xml/QNameCache.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.xml;
+
+import com.liferay.portal.kernel.concurrent.ConcurrentReferenceKeyHashMap;
+import com.liferay.portal.kernel.memory.FinalizeManager;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.dom4j.DocumentFactory;
+import org.dom4j.QName;
+
+/**
+ * @author Jorge Díaz
+ */
+public class QNameCache extends org.dom4j.tree.QNameCache {
+
+	public QNameCache(DocumentFactory documentFactory) {
+		super(documentFactory);
+
+		namespaceCache = new ConcurrentReferenceKeyHashMap<>(
+			FinalizeManager.WEAK_REFERENCE_FACTORY);
+
+		noNamespaceCache = new ConcurrentReferenceKeyHashMap<>(
+			FinalizeManager.WEAK_REFERENCE_FACTORY);
+	}
+
+	protected Map<String, QName> createMap() {
+		return new ConcurrentHashMap<>();
+	}
+
+}
