9551faa5800616ee5c1ce3eb47b9191cfce7a658
Hugo Huijser <hugo.huijser@liferay.com>
2017-03-27 08:40:04 +0800

    LPS-71164 Move logic that checks 'Bundle-Name' and 'Bundle-SymbolicName' in *.bnd to new class

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BNDBundleNameCheck.java
@@ -0,0 +1,105 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Tuple;
+import com.liferay.source.formatter.SourceFormatterMessage;
+import com.liferay.source.formatter.checks.util.BNDSourceUtil;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Hugo Huijser
+ */
+public class BNDBundleNameCheck extends BaseFileCheck {
+
+	@Override
+	public Tuple process(String fileName, String absolutePath, String content)
+		throws Exception {
+
+		if (!fileName.endsWith("/bnd.bnd") ||
+			absolutePath.contains("/testIntegration/") ||
+			absolutePath.contains("/third-party/")) {
+
+			return new Tuple(content, Collections.emptySet());
+		}
+
+		Set<SourceFormatterMessage> sourceFormatterMessages = new HashSet<>();
+
+		_checkBundleName(
+			sourceFormatterMessages, fileName, absolutePath, content);
+
+		return new Tuple(content, sourceFormatterMessages);
+	}
+
+	private void _checkBundleName(
+		Set<SourceFormatterMessage> sourceFormatterMessages, String fileName,
+		String absolutePath, String content) {
+
+		String moduleName = BNDSourceUtil.getModuleName(absolutePath);
+
+		String bundleName = BNDSourceUtil.getDefinitionValue(
+			content, "Bundle-Name");
+
+		if (bundleName != null) {
+			String strippedBundleName = StringUtil.removeChars(
+				bundleName, CharPool.DASH, CharPool.SPACE);
+
+			strippedBundleName = strippedBundleName.replaceAll(
+				"Implementation$", "Impl");
+			strippedBundleName = strippedBundleName.replaceAll(
+				"Utilities$", "Util");
+
+			String expectedBundleName =
+				"liferay" + StringUtil.removeChars(moduleName, CharPool.DASH);
+
+			if (!StringUtil.equalsIgnoreCase(
+					strippedBundleName, expectedBundleName)) {
+
+				addMessage(
+					sourceFormatterMessages, fileName,
+					"Incorrect Bundle-Name '" + bundleName + "'");
+			}
+		}
+
+		if (moduleName.contains("-import-") ||
+			moduleName.contains("-private-")) {
+
+			return;
+		}
+
+		String bundleSymbolicName = BNDSourceUtil.getDefinitionValue(
+			content, "Bundle-SymbolicName");
+
+		if (bundleSymbolicName != null) {
+			String expectedBundleSymbolicName =
+				"com.liferay." +
+					StringUtil.replace(
+						moduleName, CharPool.DASH, CharPool.PERIOD);
+
+			if (!bundleSymbolicName.equals(expectedBundleSymbolicName)) {
+				addMessage(
+					sourceFormatterMessages, fileName,
+					"Incorrect Bundle-SymbolicName '" + bundleSymbolicName +
+						"'");
+			}
+		}
+	}
+
+}
