93ed7120a9f72d88692d6797dd7f4df77d66bf34
Hugo Huijser <hugo.huijser@liferay.com>
2017-03-27 08:41:23 +0800

    LPS-71164 Move logic that formats exports in *.bnd to new class

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BNDExportsCheck.java
@@ -0,0 +1,120 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Tuple;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.source.formatter.SourceFormatterMessage;
+import com.liferay.source.formatter.checks.util.BNDSourceUtil;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Hugo Huijser
+ */
+public class BNDExportsCheck extends BaseFileCheck {
+
+	@Override
+	public Tuple process(String fileName, String absolutePath, String content)
+		throws Exception {
+
+		if (!fileName.endsWith("/bnd.bnd") ||
+			absolutePath.contains("/testIntegration/") ||
+			absolutePath.contains("/third-party/")) {
+
+			return new Tuple(content, Collections.emptySet());
+		}
+
+		Set<SourceFormatterMessage> sourceFormatterMessages = new HashSet<>();
+
+		_checkExports(
+			sourceFormatterMessages, fileName, content, _exportContentsPattern,
+			"-exportcontents");
+		_checkExports(
+			sourceFormatterMessages, fileName, content, _exportsPattern,
+			"Export-Package");
+
+		return new Tuple(content, sourceFormatterMessages);
+	}
+
+	private void _checkExports(
+		Set<SourceFormatterMessage> sourceFormatterMessages, String fileName,
+		String content, Pattern pattern, String definitionKey) {
+
+		String bundleSymbolicName = BNDSourceUtil.getDefinitionValue(
+			content, "Bundle-SymbolicName");
+
+		if ((bundleSymbolicName == null) ||
+			bundleSymbolicName.endsWith(".compat")) {
+
+			return;
+		}
+
+		Matcher matcher = _apiOrServiceBundleSymbolicNamePattern.matcher(
+			bundleSymbolicName);
+
+		bundleSymbolicName = matcher.replaceAll(StringPool.BLANK);
+
+		matcher = pattern.matcher(content);
+
+		if (!matcher.find()) {
+			return;
+		}
+
+		String[] lines = StringUtil.splitLines(matcher.group(2));
+
+		for (int i = 0; i < lines.length; i++) {
+			String line = StringUtil.removeChar(
+				StringUtil.trim(lines[i]), CharPool.BACK_SLASH);
+
+			if (Validator.isNull(line) || !line.startsWith("com.liferay.") ||
+				line.startsWith(bundleSymbolicName)) {
+
+				continue;
+			}
+
+			StringBundler sb = new StringBundler(6);
+
+			sb.append(definitionKey);
+			sb.append(" '");
+			sb.append(line);
+			sb.append("' should match Bundle-SymbolicName '");
+			sb.append(bundleSymbolicName);
+			sb.append("'");
+
+			addMessage(
+				sourceFormatterMessages, fileName, sb.toString(),
+				getLineCount(content, matcher.start(2)) + i);
+		}
+	}
+
+	private final Pattern _apiOrServiceBundleSymbolicNamePattern =
+		Pattern.compile("\\.(api|service)$");
+	private final Pattern _exportContentsPattern = Pattern.compile(
+		"\n-exportcontents:(\\\\\n| )((.*?)(\n[^\t]|\\Z))",
+		Pattern.DOTALL | Pattern.MULTILINE);
+	private final Pattern _exportsPattern = Pattern.compile(
+		"\nExport-Package:(\\\\\n| )((.*?)(\n[^\t]|\\Z))",
+		Pattern.DOTALL | Pattern.MULTILINE);
+
+}
