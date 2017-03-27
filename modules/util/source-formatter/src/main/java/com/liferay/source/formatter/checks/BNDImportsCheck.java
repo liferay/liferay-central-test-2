9bc690577f4e2b4c17761166b63162de39343511
Hugo Huijser <hugo.huijser@liferay.com>
2017-03-27 08:41:46 +0800

    LPS-71164 Move logic that formats imports in *.bnd to new class

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BNDImportsCheck.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.util.Tuple;
+import com.liferay.portal.tools.ImportsFormatter;
+import com.liferay.source.formatter.BNDImportsFormatter;
+import com.liferay.source.formatter.SourceFormatterMessage;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Hugo Huijser
+ */
+public class BNDImportsCheck extends BaseFileCheck {
+
+	@Override
+	public Tuple process(String fileName, String absolutePath, String content)
+		throws Exception {
+
+		Set<SourceFormatterMessage> sourceFormatterMessages = new HashSet<>();
+
+		_checkWildcardImports(
+			sourceFormatterMessages, fileName, absolutePath, content,
+			_conditionalPackagePattern);
+		_checkWildcardImports(
+			sourceFormatterMessages, fileName, absolutePath, content,
+			_exportContentsPattern);
+		_checkWildcardImports(
+			sourceFormatterMessages, fileName, absolutePath, content,
+			_exportsPattern);
+
+		ImportsFormatter importsFormatter = new BNDImportsFormatter();
+
+		content = importsFormatter.format(content, _conditionalPackagePattern);
+		content = importsFormatter.format(content, _exportContentsPattern);
+		content = importsFormatter.format(content, _exportsPattern);
+		content = importsFormatter.format(content, _importsPattern);
+
+		return new Tuple(content, sourceFormatterMessages);
+	}
+
+	private void _checkWildcardImports(
+		Set<SourceFormatterMessage> sourceFormatterMessages, String fileName,
+		String absolutePath, String content, Pattern pattern) {
+
+		if (absolutePath.contains("/portal-kernel/") ||
+			absolutePath.contains("/third-party/") ||
+			absolutePath.contains("/util-bridges/") ||
+			absolutePath.contains("/util-java/") ||
+			absolutePath.contains("/util-taglib/") ||
+			fileName.endsWith("/system.packages.extra.bnd")) {
+
+			return;
+		}
+
+		Matcher matcher = pattern.matcher(content);
+
+		if (!matcher.find()) {
+			return;
+		}
+
+		String imports = matcher.group(3);
+
+		matcher = _wilcardImportPattern.matcher(imports);
+
+		while (matcher.find()) {
+			String wildcardImport = matcher.group(1);
+
+			if (wildcardImport.matches("^!?com\\.liferay\\..+")) {
+				addMessage(
+					sourceFormatterMessages, fileName,
+					"Do not use wildcard in Export-Package '" + wildcardImport +
+						"'");
+			}
+		}
+	}
+
+	private final Pattern _conditionalPackagePattern = Pattern.compile(
+		"\n-conditionalpackage:(\\\\\n| )((.*?)(\n[^\t]|\\Z))",
+		Pattern.DOTALL | Pattern.MULTILINE);
+	private final Pattern _exportContentsPattern = Pattern.compile(
+		"\n-exportcontents:(\\\\\n| )((.*?)(\n[^\t]|\\Z))",
+		Pattern.DOTALL | Pattern.MULTILINE);
+	private final Pattern _exportsPattern = Pattern.compile(
+		"\nExport-Package:(\\\\\n| )((.*?)(\n[^\t]|\\Z))",
+		Pattern.DOTALL | Pattern.MULTILINE);
+	private final Pattern _importsPattern = Pattern.compile(
+		"\nImport-Package:(\\\\\n| )((.*?)(\n[^\t]|\\Z))",
+		Pattern.DOTALL | Pattern.MULTILINE);
+	private final Pattern _wilcardImportPattern = Pattern.compile(
+		"(\\S+\\*)(,\\\\\n|\n|\\Z)");
+
+}
