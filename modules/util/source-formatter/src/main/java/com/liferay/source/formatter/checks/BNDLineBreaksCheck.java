d3b4fa86615ef5316817b30dd9de9b8dd0e3fc45
Hugo Huijser <hugo.huijser@liferay.com>
2017-03-27 08:42:53 +0800

    LPS-71164 Move logic that formats line breaks in *.bnd to new class

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BNDLineBreaksCheck.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Tuple;
+import com.liferay.source.formatter.checks.util.BNDSourceUtil;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Hugo Huijser
+ */
+public class BNDLineBreaksCheck extends BaseFileCheck {
+
+	@Override
+	public Tuple process(String fileName, String absolutePath, String content)
+		throws Exception {
+
+		int pos = fileName.lastIndexOf(StringPool.SLASH);
+
+		String shortFileName = fileName.substring(pos + 1);
+
+		content = _formatLineBreaks(shortFileName, content);
+
+		return new Tuple(content, Collections.emptySet());
+	}
+
+	private String _formatLineBreaks(
+		String content, Map<String, String> definitionsKeysMap) {
+
+		if (definitionsKeysMap == null) {
+			return content;
+		}
+
+		for (Map.Entry<String, String> entry : definitionsKeysMap.entrySet()) {
+			String definitionKey = entry.getValue();
+
+			Pattern pattern = Pattern.compile(" " + definitionKey + ":");
+
+			Matcher matcher = pattern.matcher(content);
+
+			if (matcher.find()) {
+				return StringUtil.replaceFirst(
+					content, " ", "\n", matcher.start());
+			}
+		}
+
+		return content;
+	}
+
+	private String _formatLineBreaks(String shortFileName, String content) {
+		content = _formatLineBreaks(
+			content, BNDSourceUtil.getDefinitionKeysMap());
+
+		Map<String, Map<String, String>> fileSpecificDefinitionKeysMap =
+			BNDSourceUtil.getFileSpecificDefinitionKeysMap();
+
+		return _formatLineBreaks(
+			content, fileSpecificDefinitionKeysMap.get(shortFileName));
+	}
+
+}
