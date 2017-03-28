f40fb40a9d407ec80243762b95b7ebfd9045d5cb
Leonardo Barros <leonardo.barros@liferay.com>
2017-03-28 09:35:25 -0300

    LPS-69162 Add finder methods

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,4 +1,5 @@
 create index IX_6A6C1C85 on DDLRecord (companyId);
+create index IX_F12C61D4 on DDLRecord (recordSetId, recordSetVersion[$COLUMN_LENGTH:75$]);
 create index IX_AAC564D3 on DDLRecord (recordSetId, userId);
 create index IX_384AB6F7 on DDLRecord (uuid_[$COLUMN_LENGTH:75$], companyId);
 create unique index IX_B4328F39 on DDLRecord (uuid_[$COLUMN_LENGTH:75$], groupId);
@@ -12,3 +13,4 @@ create unique index IX_94FC5485 on DDLRecordSetVersion (recordSetId, version[$CO
 
 create index IX_762ADC7 on DDLRecordVersion (recordId, status);
 create unique index IX_C79E347 on DDLRecordVersion (recordId, version[$COLUMN_LENGTH:75$]);
+create index IX_19AD75F6 on DDLRecordVersion (recordSetId, recordSetVersion[$COLUMN_LENGTH:75$]);
