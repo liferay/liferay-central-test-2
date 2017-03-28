4bdb13df23b4d2d59eb87d34d829463848beede3
Hugo Huijser <hugo.huijser@liferay.com>
2017-03-28 10:31:38 +0800

    LPS-71164 Move check to new class JavaLogClassNameCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaLogClassNameCheck.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Tuple;
+import com.liferay.source.formatter.checks.util.JavaSourceUtil;
+
+import java.util.Collections;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Hugo Huijser
+ */
+public class JavaLogClassNameCheck extends BaseFileCheck {
+
+	@Override
+	public Tuple process(String fileName, String absolutePath, String content)
+		throws Exception {
+
+		Matcher matcher = _logPattern.matcher(content);
+
+		if (matcher.find()) {
+			String className = JavaSourceUtil.getClassName(fileName);
+			String logClassName = matcher.group(1);
+
+			if (!logClassName.equals(className)) {
+				content = StringUtil.replaceLast(
+					content, logClassName + ".class)", className + ".class)");
+			}
+		}
+
+		return new Tuple(content, Collections.emptySet());
+	}
+
+	private final Pattern _logPattern = Pattern.compile(
+		"\n\tprivate static final Log _log = LogFactoryUtil.getLog\\(\n*" +
+			"\t*(.+)\\.class\\)");
+
+}
