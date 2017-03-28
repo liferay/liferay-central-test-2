fe321d3472983cc60189d7a3fc750823cb9f707d
Hugo Huijser <hugo.huijser@liferay.com>
2017-03-28 10:33:03 +0800

    LPS-71164 Move checks to new class JavaStylingCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaStylingCheck.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Tuple;
+
+import java.util.Collections;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Hugo Huijser
+ */
+public class JavaStylingCheck extends BaseFileCheck {
+
+	@Override
+	public Tuple process(String fileName, String absolutePath, String content)
+		throws Exception {
+
+		if (fileName.endsWith("JavaStylingCheck.java")) {
+			return new Tuple(content, Collections.emptySet());
+		}
+
+		if (content.contains("$\n */")) {
+			content = StringUtil.replace(content, "$\n */", "$\n *\n */");
+		}
+
+		if (content.contains(" * @author Raymond Aug") &&
+			!content.contains(" * @author Raymond Aug\u00e9")) {
+
+			content = content.replaceFirst(
+				"Raymond Aug.++", "Raymond Aug\u00e9");
+		}
+
+		content = StringUtil.replace(
+			content, " final static ", " static final ");
+
+		content = StringUtil.replace(
+			content,
+			new String[] {";\n/**", "\t/*\n\t *", ";;\n", "\n/**\n *\n *"},
+			new String[] {";\n\n/**", "\t/**\n\t *", ";\n", "\n/**\n *"});
+
+		content = StringUtil.replace(
+			content,
+			new String[] {"!Validator.isNotNull(", "!Validator.isNull("},
+			new String[] {"Validator.isNull(", "Validator.isNotNull("});
+
+		content = StringUtil.replace(
+			content, StringPool.TAB + "for (;;) {",
+			StringPool.TAB + "while (true) {");
+
+		Matcher matcher = _incorrectSynchronizedPattern.matcher(content);
+
+		content = matcher.replaceAll("$1$3 $2");
+
+		return new Tuple(content, Collections.emptySet());
+	}
+
+	private final Pattern _incorrectSynchronizedPattern = Pattern.compile(
+		"([\n\t])(synchronized) (private|public|protected)");
+
+}
