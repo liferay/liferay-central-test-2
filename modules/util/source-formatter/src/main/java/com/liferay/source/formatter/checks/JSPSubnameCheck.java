bf9cf264ce1040fee8324677c187a6ecc8085dd9
Hugo Huijser <hugo.huijser@liferay.com>
2017-03-28 14:31:39 +0800

    LPS-71164 Move logic to new class JSPSubnameCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JSPSubnameCheck.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.util.Tuple;
+import com.liferay.source.formatter.SourceFormatterMessage;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Hugo Huijser
+ */
+public class JSPSubnameCheck extends BaseFileCheck {
+
+	@Override
+	public Tuple process(String fileName, String absolutePath, String content)
+		throws Exception {
+
+		Set<SourceFormatterMessage> sourceFormatterMessages = new HashSet<>();
+
+		Matcher matcher = _subnamePattern.matcher(content);
+
+		while (matcher.find()) {
+			addMessage(
+				sourceFormatterMessages, fileName,
+				"'sub' should be followed by a lowercase character for '" +
+					matcher.group(1) + "'");
+		}
+
+		return new Tuple(content, sourceFormatterMessages);
+	}
+
+	private final Pattern _subnamePattern = Pattern.compile(
+		"\\s(_?sub[A-Z]\\w+)[; ]");
+
+}
