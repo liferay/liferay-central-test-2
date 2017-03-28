c5cd800f54d940e489e365dbecc0ba699e11b59b
Hugo Huijser <hugo.huijser@liferay.com>
2017-03-28 10:32:37 +0800

    LPS-71164 Move check to new class JavaResultSetCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaResultSetCheck.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Tuple;
+import com.liferay.source.formatter.SourceFormatterMessage;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Hugo Huijser
+ */
+public class JavaResultSetCheck extends BaseFileCheck {
+
+	@Override
+	public Tuple process(String fileName, String absolutePath, String content)
+		throws Exception {
+
+		// LPS-28266
+
+		Set<SourceFormatterMessage> sourceFormatterMessages = new HashSet<>();
+
+		for (int pos1 = -1;;) {
+			pos1 = content.indexOf(StringPool.TAB + "try {", pos1 + 1);
+
+			if (pos1 == -1) {
+				break;
+			}
+
+			int pos2 = content.indexOf(StringPool.TAB + "try {", pos1 + 1);
+			int pos3 = content.indexOf("\"select count(", pos1);
+
+			if ((pos2 != -1) && (pos3 != -1) && (pos2 < pos3)) {
+				continue;
+			}
+
+			int pos4 = content.indexOf("rs.getLong(1)", pos1);
+			int pos5 = content.indexOf(StringPool.TAB + "finally {", pos1);
+
+			if ((pos3 == -1) || (pos4 == -1) || (pos5 == -1)) {
+				break;
+			}
+
+			if ((pos3 < pos4) && (pos4 < pos5)) {
+				addMessage(
+					sourceFormatterMessages, fileName,
+					"Use rs.getInt(1) for count, see LPS-28266");
+			}
+		}
+
+		return new Tuple(content, sourceFormatterMessages);
+	}
+
+}
