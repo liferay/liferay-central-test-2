61fe3431df41241dedc09705fb769d952af15496
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2017-03-28 10:24:47 +0200

    LPS-71509 Add "Browse Server" button that allow to create links to files from Documents and Media

--- modules/apps/foundation/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/_diffs/plugins/wikilink/dialogs/link.js
+++ modules/apps/foundation/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/_diffs/plugins/wikilink/dialogs/link.js
@@ -78,6 +78,26 @@ CKEDITOR.dialog.add(
 
 										return func.apply(instance);
 									}
+								},
+								{
+									id: 'linkBrowse',
+									label: LANG_COMMON.browseServer,
+									required: true,
+									type: 'button',
+									onClick: function(event) {
+										var dialog = event.data.dialog;
+
+										var editor = dialog.getParentEditor();
+
+										var urlField = dialog.getContentElement('info', 'linkAddress');
+
+										editor.execCommand(
+											'linkselector',
+											function(newVal) {
+												urlField.setValue(newVal);
+											}
+										);
+									}
 								}
 							],
 							id: 'linkOptions',
