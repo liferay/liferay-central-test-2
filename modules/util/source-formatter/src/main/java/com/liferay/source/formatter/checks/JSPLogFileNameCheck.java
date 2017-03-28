170582e5fd371c6d6106d660620c8966ffd6b926
Hugo Huijser <hugo.huijser@liferay.com>
2017-03-28 14:27:52 +0800

    LPS-71164 Move logic to new class JSPLogFileNameCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JSPLogFileNameCheck.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Tuple;
+
+import java.util.Collections;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Hugo Huijser
+ */
+public class JSPLogFileNameCheck extends BaseFileCheck {
+
+	public JSPLogFileNameCheck(boolean subrepository) {
+		_subrepository = subrepository;
+	}
+
+	@Override
+	public Tuple process(String fileName, String absolutePath, String content)
+		throws Exception {
+
+		if (!isModulesFile(absolutePath, _subrepository) &&
+			!absolutePath.contains("/portal-web/")) {
+
+			content = _formatLogFileName(absolutePath, content);
+		}
+
+		return new Tuple(content, Collections.emptySet());
+	}
+
+	private String _formatLogFileName(String absolutePath, String content) {
+		Matcher matcher = _logPattern.matcher(content);
+
+		if (!matcher.find()) {
+			return content;
+		}
+
+		String logFileName = StringUtil.replace(
+			absolutePath, CharPool.PERIOD, CharPool.UNDERLINE);
+
+		logFileName = StringUtil.replace(
+			logFileName, CharPool.SLASH, CharPool.PERIOD);
+
+		logFileName = StringUtil.replace(
+			logFileName, CharPool.DASH, CharPool.UNDERLINE);
+
+		int x = logFileName.lastIndexOf(".portal_web.");
+
+		if (x != -1) {
+			logFileName = logFileName.substring(x + 1);
+		}
+		else {
+			x = logFileName.lastIndexOf(".docroot.");
+
+			if (x == -1) {
+				x = Math.max(
+					logFileName.lastIndexOf(
+						".src.main.resources.META_INF.resources."),
+					logFileName.lastIndexOf(".src.META_INF.resources."));
+			}
+
+			if (x == -1) {
+				return content;
+			}
+
+			x = logFileName.lastIndexOf(StringPool.PERIOD, x - 1);
+
+			logFileName = "com_liferay_" + logFileName.substring(x + 1);
+
+			logFileName = StringUtil.replace(
+				logFileName,
+				new String[] {
+					".docroot.", ".src.main.resources.META_INF.resources.",
+					".src.META_INF.resources."
+				},
+				new String[] {
+					StringPool.PERIOD, StringPool.PERIOD, StringPool.PERIOD
+				});
+		}
+
+		return StringUtil.replace(
+			content, matcher.group(),
+			"Log _log = LogFactoryUtil.getLog(\"" + logFileName + "\")");
+	}
+
+	private final Pattern _logPattern = Pattern.compile(
+		"Log _log = LogFactoryUtil\\.getLog\\(\"(.*?)\"\\)");
+	private final boolean _subrepository;
+
+}
