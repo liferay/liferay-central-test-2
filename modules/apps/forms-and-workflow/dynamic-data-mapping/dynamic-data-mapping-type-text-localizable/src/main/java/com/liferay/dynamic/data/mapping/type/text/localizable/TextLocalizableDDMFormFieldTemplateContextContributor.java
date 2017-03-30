682ddfd96e523c0ae6f38890d698ca8a5a0ec1c8
Leonardo Barros <leonardo.barros@liferay.com>
2017-03-30 19:30:08 -0300

    LPS-71595 Add available locales metadata to context

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-text-localizable/src/main/java/com/liferay/dynamic/data/mapping/type/text/localizable/TextLocalizableDDMFormFieldTemplateContextContributor.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-text-localizable/src/main/java/com/liferay/dynamic/data/mapping/type/text/localizable/TextLocalizableDDMFormFieldTemplateContextContributor.java
@@ -5,14 +5,28 @@ import com.liferay.dynamic.data.mapping.model.DDMFormField;
 import com.liferay.dynamic.data.mapping.model.LocalizedValue;
 import com.liferay.dynamic.data.mapping.model.Value;
 import com.liferay.dynamic.data.mapping.render.DDMFormFieldRenderingContext;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Leonardo Barros
@@ -27,12 +41,42 @@ import org.osgi.service.component.annotations.Component;
 public class TextLocalizableDDMFormFieldTemplateContextContributor
 	implements DDMFormFieldTemplateContextContributor {
 
+	public JSONArray createAvailableLocalesMetadata(ThemeDisplay themeDisplay) {
+		JSONArray jsonArray = _jsonFactory.createJSONArray();
+
+		Locale siteDefaultLocale = themeDisplay.getSiteDefaultLocale();
+
+		jsonArray.put(createLocaleMetadata(siteDefaultLocale, themeDisplay));
+
+		Set<Locale> availableLocales = LanguageUtil.getAvailableLocales();
+
+		for (Locale locale : availableLocales) {
+			if (Objects.equals(locale, siteDefaultLocale)) {
+				continue;
+			}
+
+			jsonArray.put(createLocaleMetadata(locale, themeDisplay));
+		}
+
+		return jsonArray;
+	}
+
 	@Override
 	public Map<String, Object> getParameters(
 		DDMFormField ddmFormField,
 		DDMFormFieldRenderingContext ddmFormFieldRenderingContext) {
 
 		Map<String, Object> parameters = new HashMap<>();
+
+		HttpServletRequest request =
+			ddmFormFieldRenderingContext.getHttpServletRequest();
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		parameters.put(
+			"availableLocalesMetadata",
+			createAvailableLocalesMetadata(themeDisplay));
 
 		parameters.put(
 			"displayStyle",
@@ -54,6 +98,40 @@ public class TextLocalizableDDMFormFieldTemplateContextContributor
 		return parameters;
 	}
 
+	protected JSONObject createLocaleMetadata(
+		Locale locale, ThemeDisplay themeDisplay) {
+
+		Locale siteDefaultLocale = themeDisplay.getSiteDefaultLocale();
+
+		JSONObject localeMetadataJSONObject = _jsonFactory.createJSONObject();
+
+		localeMetadataJSONObject.put(
+			"dir", LanguageUtil.get(locale, "lang.dir"));
+		localeMetadataJSONObject.put(
+			"icon", getLocaleIcon(locale, themeDisplay));
+		localeMetadataJSONObject.put(
+			"label",
+			HtmlUtil.escapeAttribute(locale.getDisplayName(siteDefaultLocale)));
+		localeMetadataJSONObject.put(
+			"languageId", LocaleUtil.toLanguageId(locale));
+
+		return localeMetadataJSONObject;
+	}
+
+	protected String getLocaleIcon(Locale locale, ThemeDisplay themeDisplay) {
+		String languageId = LocaleUtil.toLanguageId(locale);
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(themeDisplay.getPathThemeImages());
+		sb.append("/lexicon/icons.svg");
+		sb.append(StringPool.POUND);
+		sb.append(
+			StringUtil.toLowerCase(StringUtil.replace(languageId, '_', '-')));
+
+		return sb.toString();
+	}
+
 	protected String getValueString(Value value, Locale locale) {
 		if (value != null) {
 			return value.getString(locale);
@@ -61,5 +139,8 @@ public class TextLocalizableDDMFormFieldTemplateContextContributor
 
 		return StringPool.BLANK;
 	}
+
+	@Reference
+	private JSONFactory _jsonFactory;
 
 }
