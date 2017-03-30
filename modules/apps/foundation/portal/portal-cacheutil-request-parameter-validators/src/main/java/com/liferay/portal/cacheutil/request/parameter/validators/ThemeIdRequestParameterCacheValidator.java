37ced634c70f43ccdc17e91e6137fad12b7258e0
Carlos Sierra <carlos.sierra@liferay.com>
2017-03-30 17:16:59 +0200

    LPS-69008 New module with default validators

---
+++ modules/apps/foundation/portal/portal-cacheutil-request-parameter-validators/src/main/java/com/liferay/portal/cacheutil/request/parameter/validators/ThemeIdRequestParameterCacheValidator.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.cacheutil.request.parameter.validators;
+
+import com.liferay.portal.kernel.model.Theme;
+import com.liferay.portal.kernel.service.ThemeLocalService;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.PredicateFilter;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+@Component(immediate = true, property = "filter.request.parameter=themeId")
+public class ThemeIdRequestParameterCacheValidator
+	implements PredicateFilter<HttpServletRequest> {
+
+	@Override
+	public boolean filter(HttpServletRequest request) {
+		long companyId = PortalUtil.getCompanyId(request);
+
+		Theme theme = _themeLocalService.fetchTheme(
+			companyId, request.getParameter(_parameterName));
+
+		if (theme != null) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Activate
+	protected void activate(Map<String, Object> properties) {
+		Object parameterNameObject = properties.get("filter.request.parameter");
+
+		_parameterName = parameterNameObject.toString();
+	}
+
+	private String _parameterName;
+
+	@Reference
+	private ThemeLocalService _themeLocalService;
+
+}
