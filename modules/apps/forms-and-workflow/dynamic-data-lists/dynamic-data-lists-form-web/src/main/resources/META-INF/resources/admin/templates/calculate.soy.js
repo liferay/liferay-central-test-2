538e20534abd8c04d4d6724a9cb3802107fb9317
Thiago Rocha <thiago.rocha@liferay.com>
2017-03-30 15:41:08 -0300

    LPS-71549 BuildSoy

---
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/templates/calculate.soy.js
@@ -0,0 +1,18 @@
+// This file was automatically generated from calculate.soy.
+// Please don't edit this file by hand.
+
+/**
+ * @fileoverview Templates in namespace ddl.calculate.
+ * @public
+ */
+
+if (typeof ddl == 'undefined') { var ddl = {}; }
+if (typeof ddl.calculate == 'undefined') { ddl.calculate = {}; }
+
+
+ddl.calculate.settings = function(opt_data, opt_ignored) {
+  return '<div class="calculate-container-calculator-component col-md-4"></div><div class="calculate-container-fields col-md-8"></div>';
+};
+if (goog.DEBUG) {
+  ddl.calculate.settings.soyTemplateName = 'ddl.calculate.settings';
+}
---
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/templates/calculator.soy.js
@@ -0,0 +1,18 @@
+// This file was automatically generated from calculator.soy.
+// Please don't edit this file by hand.
+
+/**
+ * @fileoverview Templates in namespace ddl.calculator.
+ * @public
+ */
+
+if (typeof ddl == 'undefined') { var ddl = {}; }
+if (typeof ddl.calculator == 'undefined') { ddl.calculator = {}; }
+
+
+ddl.calculator.settings = function(opt_data, opt_ignored) {
+  return '<div class="calculator-add-field-button-container"><button class="btn btn-default btn-lg btn-primary calculator-add-field-button ddl-button" type="button"><span class="">' + soy.$$escapeHtml(opt_data.strings.addField) + '</span></button></div><div class="calculator-button-area"><ul class="calculator-buttons calculator-buttons-numbers"><li class="border-top-left button-padding-icons calculator-button" data-calculator-key="backspace">' + soy.$$filterNoAutoescape(opt_data.calculatorAngleLeft) + '</li><li class="calculator-button" data-calculator-key="(">(</li><li class="border-top-right calculator-button" data-calculator-key=")">)</li><li class="calculator-button" data-calculator-key="1">1</li><li class="calculator-button" data-calculator-key="2">2</li><li class="calculator-button" data-calculator-key="3">3</li><li class="calculator-button" data-calculator-key="4">4</li><li class="calculator-button" data-calculator-key="5">5</li><li class="calculator-button" data-calculator-key="6">6</li><li class="calculator-button" data-calculator-key="7">7</li><li class="calculator-button" data-calculator-key="8">8</li><li class="calculator-button" data-calculator-key="9">9</li><li class="border-bottom-left button-two-columns calculator-button" data-calculator-key="0">0</li><li class="border-bottom-right calculator-button" data-calculator-key=".">.</li></ul><ul class="calculator-buttons calculator-buttons-operators"><li class="border-top-left border-top-right button-padding-icons calculator-button">' + soy.$$filterNoAutoescape(opt_data.calculatorEllipsis) + '</li>' + ((opt_data.advancedOperators) ? soy.$$escapeHtml(opt_data.advancedOperators) : '') + '<li class="calculator-button" data-calculator-key="+">+</li><li class="calculator-button" data-calculator-key="-">-</li><li class="calculator-button" data-calculator-key="*">*</li><li class="border-bottom-left border-bottom-right calculator-button" data-calculator-key="/">/</li></ul></div>';
+};
+if (goog.DEBUG) {
+  ddl.calculator.settings.soyTemplateName = 'ddl.calculator.settings';
+}
