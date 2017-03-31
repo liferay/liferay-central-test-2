ba17b6b70c54b96b5c677bf50cf57e57d6b5d110
Jorge Ferrer <jorge.ferrer@liferay.com>
2017-03-31 11:23:57 -0700

    LPS-71620 Add the three main interfaces of the Vulcan API

---
+++ modules/apps/vulcan/vulcan-api/src/main/java/com/liferay/vulcan/contributor/ApiContributor.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.vulcan.contributor;
+
+/**
+ * Classes implementing this interface will be treated as contributors of the
+ * Liferay API.
+ *
+ * @author Alejandro Hernández
+ */
+public interface ApiContributor {
+
+	public String getPath();
+
+}
