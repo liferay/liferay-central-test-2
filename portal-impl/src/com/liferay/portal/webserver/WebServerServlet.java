2c25ca0fc686e9c8e9a39ceda3440919b2b8490e
Jose A. Jimenez <jose.jimenez@liferay.com>
2017-03-31 17:34:06 +0200

    LPS-71580 SF

--- portal-impl/src/com/liferay/portal/webserver/WebServerServlet.java
+++ portal-impl/src/com/liferay/portal/webserver/WebServerServlet.java
@@ -1480,23 +1480,21 @@ public class WebServerServlet extends HttpServlet {
 
 						Image image = getImage(request, true);
 
-						if ((image != null) &&
-							(image.getCompanyId() != user.getCompanyId()) &&
-							_processCompanyInactiveRequest(
-								request, response, image.getCompanyId())) {
-
-							return null;
-						}
-
 						if (image != null) {
+							if ((image.getCompanyId() != user.getCompanyId()) &&
+								_processCompanyInactiveRequest(
+									request, response, image.getCompanyId())) {
+
+								return null;
+							}
+
 							writeImage(image, request, response);
 						}
 						else {
 							sendDocumentLibrary(
 								request, response, user,
 								request.getServletPath() + StringPool.SLASH +
-									path,
-								pathArray);
+									path, pathArray);
 						}
 					}
 				}
