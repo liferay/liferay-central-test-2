04a88bf3260bd6fa5e908ee555a4dc2eb0a417dd
Hugo Huijser <hugo.huijser@liferay.com>
2017-03-31 15:54:19 +0800

    LPS-71164 Add JavaTermCheck, this one allows to walk through all the java terms of a class and only perform a check for specific java term types

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BaseJavaTermCheck.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.source.formatter.parser.JavaClass;
+import com.liferay.source.formatter.parser.JavaConstructor;
+import com.liferay.source.formatter.parser.JavaMethod;
+import com.liferay.source.formatter.parser.JavaTerm;
+import com.liferay.source.formatter.parser.JavaVariable;
+
+/**
+ * @author Hugo Huijser
+ */
+public abstract class BaseJavaTermCheck
+	extends BaseSourceCheck implements JavaTermCheck {
+
+	@Override
+	public String process(
+			String fileName, String absolutePath, JavaClass javaClass,
+			String content)
+		throws Exception {
+
+		clearSourceFormatterMessages(fileName);
+
+		return _walkJavaClass(fileName, absolutePath, javaClass, content);
+	}
+
+	protected abstract String doProcess(
+			String filename, String absolutePath, JavaTerm javaTerm,
+			String content)
+		throws Exception;
+
+	protected abstract String[] getCheckableJavaTermNames();
+
+	protected static final String JAVA_CLASS = JavaTerm.class.getName();
+
+	protected static final String JAVA_CONSTRUCTOR =
+		JavaConstructor.class.getName();
+
+	protected static final String JAVA_METHOD = JavaMethod.class.getName();
+
+	protected static final String JAVA_VARIABLE = JavaVariable.class.getName();
+
+	private boolean _isCheckableJavaTerm(JavaTerm javaTerm) {
+		Class<?> clazz = javaTerm.getClass();
+
+		String className = clazz.getName();
+
+		for (String name : getCheckableJavaTermNames()) {
+			if (name.equals(className)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private String _walkJavaClass(
+			String fileName, String absolutePath, JavaClass javaClass,
+			String content)
+		throws Exception {
+
+		if (_isCheckableJavaTerm(javaClass)) {
+			content = doProcess(fileName, absolutePath, javaClass, content);
+		}
+
+		for (JavaTerm javaTerm : javaClass.getChildJavaTerms()) {
+			if (javaTerm instanceof JavaClass) {
+				JavaClass childJavaClass = (JavaClass)javaTerm;
+
+				content = _walkJavaClass(
+					fileName, absolutePath, childJavaClass, content);
+			}
+			else if (_isCheckableJavaTerm(javaTerm)) {
+				content = doProcess(fileName, absolutePath, javaTerm, content);
+			}
+		}
+
+		return content;
+	}
+
+}
