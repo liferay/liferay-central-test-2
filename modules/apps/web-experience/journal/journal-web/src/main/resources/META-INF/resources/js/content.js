e3f77dbc11d05cb1f9f92fdfdda3443ee2805ed3
Sam Ziemer <Samuel.Ziemer@liferay.com>
2017-03-31 10:06:27 -0700

    LPS-71181 SF

--- modules/apps/web-experience/journal/journal-web/src/main/resources/META-INF/resources/js/content.js
+++ modules/apps/web-experience/journal/journal-web/src/main/resources/META-INF/resources/js/content.js
@@ -169,9 +169,9 @@ AUI.add(
 						var translationManager = instance.get(STR_TRANSLATION_MANAGER);
 
 						if (translationManager) {
-                            eventHandles.push(
-                                translationManager.after('deleteAvailableLocale', instance._afterDeletingAvailableLocale, instance)
-                            );
+							eventHandles.push(
+								translationManager.after('deleteAvailableLocale', instance._afterDeletingAvailableLocale, instance)
+							);
 							eventHandles.push(
 								translationManager.after('editingLocaleChange', instance._afterEditingLocaleChange, instance)
 							);
