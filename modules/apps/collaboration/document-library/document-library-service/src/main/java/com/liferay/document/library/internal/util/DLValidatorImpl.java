64ebff221bd65da771ff1d22ece3376c978bb710
drewbrokke <drew.brokke@liferay.com>
2017-03-31 17:09:36 -0700

    LPS-71626 document-library-service - DLValidatorImpl.java - updates package path

--- modules/apps/collaboration/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/util/DLValidatorImpl.java
+++ modules/apps/collaboration/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/util/DLValidatorImpl.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portlet.documentlibrary.util;
+package com.liferay.document.library.internal.util;
 
 import com.liferay.document.library.kernel.exception.FileExtensionException;
 import com.liferay.document.library.kernel.exception.FileNameException;
