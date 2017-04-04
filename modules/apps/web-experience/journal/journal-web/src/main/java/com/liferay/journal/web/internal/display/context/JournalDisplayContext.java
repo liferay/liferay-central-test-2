5de06e0822fd1430758fa084991d5c69200c7017
Jürgen Kappler <juergen.kappler@liferay.com>
2017-04-04 09:12:14 +0200

    LPS-70946 New method to obtain the available locales for the article

--- modules/apps/web-experience/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ modules/apps/web-experience/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -93,11 +93,13 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.portlet.PortletException;
 import javax.portlet.PortletPreferences;
@@ -174,6 +176,20 @@ public class JournalDisplayContext {
 			themeDisplay);
 
 		return _articleDisplay;
+	}
+
+	public Set getAvailableArticleLocales() throws PortalException {
+		Set<Locale> availableLocalesSet = new HashSet<>();
+
+		JournalArticle article = getArticle();
+
+		if (article != null) {
+			Map<Locale, String> titleMap = article.getTitleMap();
+
+			availableLocalesSet.addAll(titleMap.keySet());
+		}
+
+		return availableLocalesSet;
 	}
 
 	public String[] getCharactersBlacklist() throws PortalException {
