2bbb54e2564549b652d4521e19ba4df18df62fd8
shuyangzhou <shuyang.zhou@liferay.com>
2017-04-05 07:28:41 -0700

    Revert "LPS-69436 Avoid major change by deprecating the old taglib. We have agreed with @jbalsas that we will use the new package soy for the new soy taglibs"
    This reverts commit 601e9ba3ef1d33a0db8a63eb4777db8a31ed1d48.

--- modules/apps/collaboration/flags/flags-taglib/src/main/java/com/liferay/flags/taglib/servlet/taglib/FlagsTag.java
+++ modules/apps/collaboration/flags/flags-taglib/src/main/java/com/liferay/flags/taglib/servlet/taglib/FlagsTag.java
@@ -14,86 +14,210 @@
 
 package com.liferay.flags.taglib.servlet.taglib;
 
-import com.liferay.flags.taglib.internal.servlet.ServletContextUtil;
-import com.liferay.taglib.util.IncludeTag;
+import com.liferay.flags.configuration.FlagsGroupServiceConfiguration;
+import com.liferay.frontend.taglib.soy.servlet.taglib.TemplateRendererTag;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProviderUtil;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
+import com.liferay.portal.kernel.portlet.PortletURLUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.JavaConstants;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.PortletKeys;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.trash.kernel.util.TrashUtil;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.jsp.PageContext;
+import java.util.Map;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+import javax.portlet.PortletURL;
+import javax.portlet.WindowStateException;
 
 /**
  * @author Julio Camarero
- * @deprecated As of 2.0.0, with no direct replacement
+ * @author Ambrin Chaudhary
  */
-@Deprecated
-public class FlagsTag extends IncludeTag {
+public class FlagsTag extends TemplateRendererTag {
+
+	@Override
+	public int doStartTag() {
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		String randomNamespace = StringUtil.randomId() + StringPool.UNDERLINE;
+
+		try {
+			Map<String, Object> context = getContext();
+
+			String className = (String)context.get("className");
+
+			long classPK = (Long)context.get("classPK");
+
+			boolean inTrash = TrashUtil.isInTrash(className, classPK);
+
+			putValue("cssClass", _getCssClass(randomNamespace, inTrash));
+
+			putValue("data", _getDataJSON(context, className, classPK));
+
+			putValue("flagsEnabled", _isFlagsEnabled(themeDisplay));
+
+			putValue("id", randomNamespace + "id");
+
+			putValue("inTrash", inTrash);
+
+			boolean label = GetterUtil.getBoolean(context.get("label"), true);
+
+			putValue("label", label);
+
+			String message = GetterUtil.getString(
+				context.get("message"), LanguageUtil.get(request, "flag"));
+
+			putValue("message", message);
+
+			putValue("pathThemeImages", themeDisplay.getPathThemeImages());
+
+			String title = message;
+
+			if (inTrash) {
+				title = LanguageUtil.get(
+					request,
+					"flags-are-disabled-because-this-entry-is-in-the-recycle-" +
+						"bin");
+			}
+
+			putValue("title", title);
+
+			putValue("uri", _getUri());
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
+
+		setTemplateNamespace("Flags.render");
+
+		return super.doStartTag();
+	}
+
+	@Override
+	public String getModule() {
+		return "flags-taglib/flags/Flags.es";
+	}
 
 	public void setClassName(String className) {
-		_className = className;
+		putValue("className", className);
 	}
 
 	public void setClassPK(long classPK) {
-		_classPK = classPK;
+		putValue("classPK", classPK);
 	}
 
 	public void setContentTitle(String contentTitle) {
-		_contentTitle = contentTitle;
+		putValue("contentTitle", contentTitle);
 	}
 
 	public void setLabel(boolean label) {
-		_label = label;
+		putValue("label", label);
 	}
 
 	public void setMessage(String message) {
-		_message = message;
-	}
-
-	@Override
-	public void setPageContext(PageContext pageContext) {
-		super.setPageContext(pageContext);
-
-		setServletContext(ServletContextUtil.getServletContext());
+		putValue("message", LanguageUtil.get(request, message));
 	}
 
 	public void setReportedUserId(long reportedUserId) {
-		_reportedUserId = reportedUserId;
+		putValue("reportedUserId", reportedUserId);
 	}
 
-	@Override
-	protected void cleanUp() {
-		_className = null;
-		_classPK = 0;
-		_contentTitle = null;
-		_label = true;
-		_message = null;
-		_reportedUserId = 0;
+	private String _getCssClass(String randomNamespace, boolean inTrash) {
+		String cssClass = randomNamespace;
+
+		if (!inTrash) {
+			cssClass = randomNamespace + " flag-enable";
+		}
+
+		return cssClass;
 	}
 
-	@Override
-	protected String getPage() {
-		return _PAGE;
+	private String _getCurrentURL() {
+		PortletRequest portletRequest = (PortletRequest)request.getAttribute(
+			JavaConstants.JAVAX_PORTLET_REQUEST);
+
+		PortletResponse portletResponse = (PortletResponse)request.getAttribute(
+			JavaConstants.JAVAX_PORTLET_RESPONSE);
+
+		String currentURL;
+
+		if ((portletRequest != null) && (portletResponse != null)) {
+			PortletURL currentURLObj = PortletURLUtil.getCurrent(
+				PortalUtil.getLiferayPortletRequest(portletRequest),
+				PortalUtil.getLiferayPortletResponse(portletResponse));
+
+			currentURL = currentURLObj.toString();
+		}
+		else {
+			currentURL = PortalUtil.getCurrentURL(request);
+		}
+
+		return currentURL;
 	}
 
-	@Override
-	protected void setAttributes(HttpServletRequest request) {
-		request.setAttribute("liferay-flags:flags:className", _className);
-		request.setAttribute(
-			"liferay-flags:flags:classPK", String.valueOf(_classPK));
-		request.setAttribute("liferay-flags:flags:contentTitle", _contentTitle);
-		request.setAttribute(
-			"liferay-flags:flags:label", String.valueOf(_label));
-		request.setAttribute("liferay-flags:flags:message", _message);
-		request.setAttribute(
-			"liferay-flags:flags:reportedUserId",
-			String.valueOf(_reportedUserId));
+	private JSONObject _getDataJSON(
+		Map<String, Object> context, String className, long classPK) {
+
+		String namespace = PortalUtil.getPortletNamespace(PortletKeys.FLAGS);
+
+		JSONObject dataJSON = JSONFactoryUtil.createJSONObject();
+
+		dataJSON.put(namespace + "className", className);
+		dataJSON.put(namespace + "classPK", classPK);
+		dataJSON.put(namespace + "contentTitle", context.get("contentTitle"));
+		dataJSON.put(
+			namespace + "contentURL",
+			PortalUtil.getPortalURL(request) + _getCurrentURL());
+		dataJSON.put(
+			namespace + "reportedUserId", context.get("reportedUserId"));
+
+		return dataJSON;
 	}
 
-	private static final String _PAGE = "/flags/page.jsp";
+	private String _getUri() throws WindowStateException {
+		PortletURL portletURL = PortletURLFactoryUtil.create(
+			request, PortletKeys.FLAGS, PortletRequest.RENDER_PHASE);
 
-	private String _className;
-	private long _classPK;
-	private String _contentTitle;
-	private boolean _label = true;
-	private String _message;
-	private long _reportedUserId;
+		portletURL.setParameter("mvcRenderCommandName", "/flags/edit_entry");
+		portletURL.setWindowState(LiferayWindowState.EXCLUSIVE);
+
+		return portletURL.toString();
+	}
+
+	private boolean _isFlagsEnabled(ThemeDisplay themeDisplay)
+		throws PortalException {
+
+		FlagsGroupServiceConfiguration flagsGroupServiceConfiguration =
+			ConfigurationProviderUtil.getCompanyConfiguration(
+				FlagsGroupServiceConfiguration.class,
+				themeDisplay.getCompanyId());
+
+		boolean flagsEnabled = false;
+
+		if (flagsGroupServiceConfiguration.guestUsersEnabled() ||
+			themeDisplay.isSignedIn()) {
+
+			flagsEnabled = true;
+		}
+
+		return flagsEnabled;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(FlagsTag.class);
 
 }
