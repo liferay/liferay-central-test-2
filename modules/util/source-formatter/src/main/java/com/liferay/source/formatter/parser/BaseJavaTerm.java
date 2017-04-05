be4cce7eb91adc6cb4b25b2b96883688d90dafd3
Hugo Huijser <hugo.huijser@liferay.com>
2017-04-05 21:03:00 +0800

    LPS-71164 Add method hasAnnotation to JavaTerm

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/parser/BaseJavaTerm.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/parser/BaseJavaTerm.java
@@ -14,6 +14,8 @@
 
 package com.liferay.source.formatter.parser;
 
+import com.liferay.portal.kernel.util.StringPool;
+
 /**
  * @author Hugo Huijser
  */
@@ -51,6 +53,18 @@ public abstract class BaseJavaTerm implements JavaTerm {
 	@Override
 	public JavaSignature getSignature() {
 		return null;
+	}
+
+	@Override
+	public boolean hasAnnotation(String annotation) {
+		if (_content.contains("\t@" + annotation + "\n") ||
+			_content.contains(
+				"\t@" + annotation + StringPool.OPEN_PARENTHESIS)) {
+
+			return true;
+		}
+
+		return false;
 	}
 
 	@Override
