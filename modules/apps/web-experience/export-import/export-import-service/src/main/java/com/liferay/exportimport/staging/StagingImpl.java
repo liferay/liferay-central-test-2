57f4b10501f83c5fc4a6481816fd710aed1d0cc4
Daniel Kocsis <daniel.kocsis@liferay.com>
2017-04-05 17:29:36 +0200

    LPS-71036 SF

--- modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/staging/StagingImpl.java
+++ modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/staging/StagingImpl.java
@@ -967,21 +967,17 @@ public class StagingImpl implements Staging {
 						"status-is-not-exportable",
 					new String[] {modelResource, referrerDisplayName}, false);
 			}
+			else if (Validator.isNotNull(referrerDisplayName)) {
+				errorMessage = LanguageUtil.format(
+					resourceBundle,
+					"the-following-error-occured-while-processing-the-x-x-x",
+					new String[] {
+						modelResource, referrerDisplayName,
+						e.getLocalizedMessage()
+					});
+			}
 			else {
-				if (!referrerDisplayName.equals(StringPool.BLANK)) {
-					errorMessage = LanguageUtil.format(
-						ResourceBundleUtil.getBundle(
-							"content.Language", locale, getClass()),
-						"the-following-error-occured-while-processing-the-x-" +
-							"x-x",
-						new String[] {
-							modelResource, referrerDisplayName,
-							e.getLocalizedMessage()
-						});
-				}
-				else {
-					errorMessage = e.getLocalizedMessage();
-				}
+				errorMessage = e.getLocalizedMessage();
 			}
 
 			errorType = ServletResponseConstants.SC_FILE_CUSTOM_EXCEPTION;
