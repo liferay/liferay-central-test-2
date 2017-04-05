96eb8a29509942f5115be0ff60cdbaa1811a6f14
Hugo Huijser <hugo.huijser@liferay.com>
2017-04-05 21:04:23 +0800

    LPS-71164 Move logic to new class JavaConstructorSuperCallCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaConstructorSuperCallCheck.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.parser.JavaTerm;
+
+/**
+ * @author Hugo Huijser
+ */
+public class JavaConstructorSuperCallCheck extends BaseJavaTermCheck {
+
+	@Override
+	protected String doProcess(
+		String fileName, String absolutePath, JavaTerm javaTerm,
+		String fileContent) {
+
+		String constructorContent = javaTerm.getContent();
+
+		if (constructorContent.contains("\tsuper();")) {
+			return StringUtil.replace(
+				constructorContent, "\tsuper();", StringPool.BLANK);
+		}
+
+		return constructorContent;
+	}
+
+	protected String[] getCheckableJavaTermNames() {
+		return new String[] {JAVA_CONSTRUCTOR};
+	}
+
+}
