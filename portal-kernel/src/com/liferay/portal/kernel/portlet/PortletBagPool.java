9183284089c5e8d5d137bf21880da875037a73b1
Preston-Crary <preston.crary@liferay.com>
2017-04-06 15:21:48 -0700

    LPS-71765 Remove _instance field from PortletBagPool.

--- portal-kernel/src/com/liferay/portal/kernel/portlet/PortletBagPool.java
+++ portal-kernel/src/com/liferay/portal/kernel/portlet/PortletBagPool.java
@@ -28,19 +28,19 @@ public class PortletBagPool {
 	public static PortletBag get(String portletId) {
 		PortalRuntimePermission.checkPortletBagPool(portletId);
 
-		return _instance._get(portletId);
+		return _portletBagPool.get(portletId);
 	}
 
 	public static void put(String portletId, PortletBag portletBag) {
 		PortalRuntimePermission.checkPortletBagPool(portletId);
 
-		_instance._put(portletId, portletBag);
+		_portletBagPool.put(portletId, portletBag);
 	}
 
 	public static PortletBag remove(String portletId) {
 		PortalRuntimePermission.checkPortletBagPool(portletId);
 
-		return _instance._remove(portletId);
+		return _portletBagPool.remove(portletId);
 	}
 
 	public static void reset() {
@@ -48,31 +48,13 @@ public class PortletBagPool {
 			PACLConstants.
 				PORTAL_RUNTIME_PERMISSION_PORTLET_BAG_POOL_ALL_PORTLETS);
 
-		_instance._reset();
-	}
-
-	private PortletBagPool() {
-		_portletBagPool = new ConcurrentHashMap<>();
-	}
-
-	private PortletBag _get(String portletId) {
-		return _portletBagPool.get(portletId);
-	}
-
-	private void _put(String portletId, PortletBag portletBag) {
-		_portletBagPool.put(portletId, portletBag);
-	}
-
-	private PortletBag _remove(String portletId) {
-		return _portletBagPool.remove(portletId);
-	}
-
-	private void _reset() {
 		_portletBagPool.clear();
 	}
 
-	private static final PortletBagPool _instance = new PortletBagPool();
+	private PortletBagPool() {
+	}
 
-	private final Map<String, PortletBag> _portletBagPool;
+	private static final Map<String, PortletBag> _portletBagPool =
+		new ConcurrentHashMap<>();
 
 }
