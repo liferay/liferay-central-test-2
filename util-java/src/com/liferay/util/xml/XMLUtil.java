de9ab4dffe9efeb2903aa949f5da67ae2ede5b04
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2017-04-07 12:09:30 +0200

    LPS-71787 Allow safe surrogate pairs

--- util-java/src/com/liferay/util/xml/XMLUtil.java
+++ util-java/src/com/liferay/util/xml/XMLUtil.java
@@ -93,10 +93,21 @@ public class XMLUtil {
 
 			if ((c == 0x9) || (c == 0xA) || (c == 0xD) ||
 				((c >= 0x20) && (c <= 0xD7FF)) ||
-				((c >= 0xE000) && (c <= 0xFFFD)) ||
-				((c >= 0x10000) && (c <= 0x10FFFF))) {
+				((c >= 0xE000) && (c <= 0xFFFD))) {
 
 				sb.append(c);
+			}
+
+			if (Character.isHighSurrogate(c) && (i + 1) < xml.length()) {
+				char c2 = xml.charAt(i + 1);
+
+				if (Character.isLowSurrogate(c2)) {
+					int codePoint = Character.toCodePoint(c, c2);
+
+					if ((codePoint >= 0x10000) && (codePoint <= 0x10FFFF)) {
+						sb.appendCodePoint(codePoint);
+					}
+				}
 			}
 		}
 
