599f598cb4786722b639f425d5c8495e363ef5f0
André de Oliveira <andre.oliveira@liferay.com>
2017-04-10 11:40:19 -0700

    LPS-71773 Test: scope “this site” is a condition, other sites never counted
    https://issues.liferay.com/browse/LPS-71773

--- modules/apps/foundation/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/facet/faceted/searcher/test/ScopeFacetedSearcherTest.java
+++ modules/apps/foundation/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/facet/faceted/searcher/test/ScopeFacetedSearcherTest.java
@@ -17,10 +17,15 @@ package com.liferay.portal.search.facet.faceted.searcher.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.BooleanClause;
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Hits;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.facet.Facet;
 import com.liferay.portal.kernel.search.facet.ScopeFacetFactory;
+import com.liferay.portal.kernel.search.generic.BooleanClauseImpl;
+import com.liferay.portal.kernel.search.generic.TermQueryImpl;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.Sync;
 import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
@@ -160,6 +165,13 @@ public class ScopeFacetedSearcherTest extends BaseFacetedSearcherTestCase {
 		Facet facet = _scopeFacetFactory.newInstance(searchContext);
 
 		searchContext.addFacet(facet);
+
+		BooleanClause booleanClause = new BooleanClauseImpl(
+			new TermQueryImpl(
+				Field.GROUP_ID, String.valueOf(group1.getGroupId())),
+			BooleanClauseOccur.MUST);
+
+		searchContext.setBooleanClauses(new BooleanClause[] {booleanClause});
 
 		searchContext.setAttribute(
 			"groupId", String.valueOf(group1.getGroupId()));
