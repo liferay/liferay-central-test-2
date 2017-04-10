271e3d6e3c4a57ae3fdc1c6090f592a0b73afef8
Daniel Kocsis <daniel.kocsis@liferay.com>
2017-04-10 17:29:06 +0200

    LPS-72178 Add infrastructure to fire lifecycle event on the remote live site

--- portal-impl/src/com/liferay/portlet/exportimport/service/impl/StagingServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/exportimport/service/impl/StagingServiceImpl.java
@@ -15,14 +15,21 @@
 package com.liferay.portlet.exportimport.service.impl;
 
 import com.liferay.exportimport.kernel.lar.MissingReferences;
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.exportimport.kernel.lifecycle.ExportImportLifecycleManagerUtil;
 import com.liferay.exportimport.kernel.model.ExportImportConfiguration;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil;
 import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.service.permission.GroupPermissionUtil;
+import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portlet.exportimport.service.base.StagingServiceBaseImpl;
 
+import java.io.Serializable;
+
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -48,6 +55,38 @@ public class StagingServiceImpl extends StagingServiceBaseImpl {
 
 		return stagingLocalService.createStagingRequest(
 			getUserId(), groupId, checksum);
+	}
+
+	@Override
+	public void propagateExportImportLifecycleEvent(
+			int code, int processFlag, String processId,
+			List<Serializable> arguments)
+		throws PortalException {
+
+		Serializable serializable = arguments.get(0);
+
+		long groupId = GroupConstants.DEFAULT_LIVE_GROUP_ID;
+
+		if (serializable instanceof PortletDataContext) {
+			PortletDataContext portletDataContext =
+				(PortletDataContext)serializable;
+
+			groupId = portletDataContext.getGroupId();
+		}
+		else if (serializable instanceof ExportImportConfiguration) {
+			ExportImportConfiguration exportImportConfiguration =
+				(ExportImportConfiguration)serializable;
+
+			groupId = MapUtil.getLong(
+				exportImportConfiguration.getSettingsMap(), "targetGroupId");
+		}
+
+		GroupPermissionUtil.check(
+			getPermissionChecker(), groupId, ActionKeys.EXPORT_IMPORT_LAYOUTS);
+
+		ExportImportLifecycleManagerUtil.fireExportImportLifecycleEvent(
+			code, processFlag, processId,
+			arguments.toArray(new Serializable[arguments.size()]));
 	}
 
 	/**
