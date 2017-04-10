0a973f841fd7bd127fb5f44c9569c0b19b880133
shuyangzhou <shuyang.zhou@liferay.com>
2017-04-10 11:17:33 -0700

    LPS-71830 Optimize FastDateFormatFactoryImpl caching key generation

--- portal-impl/src/com/liferay/portal/util/FastDateFormatFactoryImpl.java
+++ portal-impl/src/com/liferay/portal/util/FastDateFormatFactoryImpl.java
@@ -17,6 +17,7 @@ package com.liferay.portal.util;
 import com.liferay.portal.kernel.security.pacl.DoPrivileged;
 import com.liferay.portal.kernel.util.FastDateFormatConstants;
 import com.liferay.portal.kernel.util.FastDateFormatFactory;
+import com.liferay.portal.kernel.util.HashUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
@@ -25,6 +26,7 @@ import java.text.Format;
 
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.TimeZone;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -38,14 +40,15 @@ public class FastDateFormatFactoryImpl implements FastDateFormatFactory {
 
 	@Override
 	public Format getDate(int style, Locale locale, TimeZone timeZone) {
-		String key = getKey(style, locale, timeZone);
+		DateOrTimeCacheKey dateOrTimeCacheKey = new DateOrTimeCacheKey(
+			style, locale, timeZone);
 
-		Format format = _dateFormats.get(key);
+		Format format = _dateFormats.get(dateOrTimeCacheKey);
 
 		if (format == null) {
 			format = FastDateFormat.getDateInstance(style, timeZone, locale);
 
-			_dateFormats.put(key, format);
+			_dateFormats.put(dateOrTimeCacheKey, format);
 		}
 
 		return format;
@@ -70,15 +73,16 @@ public class FastDateFormatFactoryImpl implements FastDateFormatFactory {
 	public Format getDateTime(
 		int dateStyle, int timeStyle, Locale locale, TimeZone timeZone) {
 
-		String key = getKey(dateStyle, timeStyle, locale, timeZone);
+		DateAndTimeCacheKey dateAndTimeCacheKey = new DateAndTimeCacheKey(
+			dateStyle, timeStyle, locale, timeZone);
 
-		Format format = _dateTimeFormats.get(key);
+		Format format = _dateTimeFormats.get(dateAndTimeCacheKey);
 
 		if (format == null) {
 			format = FastDateFormat.getDateTimeInstance(
 				dateStyle, timeStyle, timeZone, locale);
 
-			_dateTimeFormats.put(key, format);
+			_dateTimeFormats.put(dateAndTimeCacheKey, format);
 		}
 
 		return format;
@@ -115,14 +119,15 @@ public class FastDateFormatFactoryImpl implements FastDateFormatFactory {
 	public Format getSimpleDateFormat(
 		String pattern, Locale locale, TimeZone timeZone) {
 
-		String key = getKey(pattern, locale, timeZone);
+		SimpleDateCacheKey simpleDateCacheKey = new SimpleDateCacheKey(
+			pattern, locale, timeZone);
 
-		Format format = _simpleDateFormats.get(key);
+		Format format = _simpleDateFormats.get(simpleDateCacheKey);
 
 		if (format == null) {
 			format = FastDateFormat.getInstance(pattern, timeZone, locale);
 
-			_simpleDateFormats.put(key, format);
+			_simpleDateFormats.put(simpleDateCacheKey, format);
 		}
 
 		return format;
@@ -135,14 +140,15 @@ public class FastDateFormatFactoryImpl implements FastDateFormatFactory {
 
 	@Override
 	public Format getTime(int style, Locale locale, TimeZone timeZone) {
-		String key = getKey(style, locale, timeZone);
+		DateOrTimeCacheKey dateOrTimeCacheKey = new DateOrTimeCacheKey(
+			style, locale, timeZone);
 
-		Format format = _timeFormats.get(key);
+		Format format = _timeFormats.get(dateOrTimeCacheKey);
 
 		if (format == null) {
 			format = FastDateFormat.getTimeInstance(style, timeZone, locale);
 
-			_timeFormats.put(key, format);
+			_timeFormats.put(dateOrTimeCacheKey, format);
 		}
 
 		return format;
@@ -177,11 +183,134 @@ public class FastDateFormatFactoryImpl implements FastDateFormatFactory {
 		return sb.toString();
 	}
 
-	private final Map<String, Format> _dateFormats = new ConcurrentHashMap<>();
-	private final Map<String, Format> _dateTimeFormats =
+	private final Map<DateOrTimeCacheKey, Format> _dateFormats =
 		new ConcurrentHashMap<>();
-	private final Map<String, Format> _simpleDateFormats =
+	private final Map<DateAndTimeCacheKey, Format> _dateTimeFormats =
 		new ConcurrentHashMap<>();
-	private final Map<String, Format> _timeFormats = new ConcurrentHashMap<>();
+	private final Map<SimpleDateCacheKey, Format> _simpleDateFormats =
+		new ConcurrentHashMap<>();
+	private final Map<DateOrTimeCacheKey, Format> _timeFormats =
+		new ConcurrentHashMap<>();
+
+	private static class DateAndTimeCacheKey {
+
+		@Override
+		public boolean equals(Object obj) {
+			DateAndTimeCacheKey dateAndTimeCacheKey = (DateAndTimeCacheKey)obj;
+
+			if ((dateAndTimeCacheKey._dateStyle == _dateStyle) &&
+				(dateAndTimeCacheKey._timeStyle == _timeStyle) &&
+				Objects.equals(dateAndTimeCacheKey._locale, _locale) &&
+				Objects.equals(dateAndTimeCacheKey._timeZone, _timeZone)) {
+
+				return true;
+			}
+
+			return false;
+		}
+
+		@Override
+		public int hashCode() {
+			int hashCode = HashUtil.hash(0, _dateStyle);
+
+			hashCode = HashUtil.hash(hashCode, _timeStyle);
+			hashCode = HashUtil.hash(hashCode, _locale);
+
+			return HashUtil.hash(hashCode, _timeZone);
+		}
+
+		private DateAndTimeCacheKey(
+			int dateStyle, int timeStyle, Locale locale, TimeZone timeZone) {
+
+			_dateStyle = dateStyle;
+			_timeStyle = timeStyle;
+			_locale = locale;
+			_timeZone = timeZone;
+		}
+
+		private final int _dateStyle;
+		private final Locale _locale;
+		private final int _timeStyle;
+		private final TimeZone _timeZone;
+
+	}
+
+	private static class DateOrTimeCacheKey {
+
+		@Override
+		public boolean equals(Object obj) {
+			DateOrTimeCacheKey dateOrTimeCacheKey = (DateOrTimeCacheKey)obj;
+
+			if ((dateOrTimeCacheKey._style == _style) &&
+				Objects.equals(dateOrTimeCacheKey._locale, _locale) &&
+				Objects.equals(dateOrTimeCacheKey._timeZone, _timeZone)) {
+
+				return true;
+			}
+
+			return false;
+		}
+
+		@Override
+		public int hashCode() {
+			int hashCode = HashUtil.hash(0, _style);
+
+			hashCode = HashUtil.hash(hashCode, _locale);
+
+			return HashUtil.hash(hashCode, _timeZone);
+		}
+
+		private DateOrTimeCacheKey(
+			int style, Locale locale, TimeZone timeZone) {
+
+			_style = style;
+			_locale = locale;
+			_timeZone = timeZone;
+		}
+
+		private final Locale _locale;
+		private final int _style;
+		private final TimeZone _timeZone;
+
+	}
+
+	private static class SimpleDateCacheKey {
+
+		@Override
+		public boolean equals(Object obj) {
+			SimpleDateCacheKey simpleDateCacheKey = (SimpleDateCacheKey)obj;
+
+			if (Objects.equals(simpleDateCacheKey._pattern, _pattern) &&
+				Objects.equals(simpleDateCacheKey._locale, _locale) &&
+				Objects.equals(simpleDateCacheKey._timeZone, _timeZone)) {
+
+				return true;
+			}
+
+			return false;
+		}
+
+		@Override
+		public int hashCode() {
+			int hashCode = HashUtil.hash(0, _pattern);
+
+			hashCode = HashUtil.hash(hashCode, _locale);
+
+			return HashUtil.hash(hashCode, _timeZone);
+		}
+
+		private SimpleDateCacheKey(
+			String pattern, Locale locale, TimeZone timeZone) {
+
+			_pattern = pattern;
+			_locale = locale;
+			_timeZone = timeZone;
+		}
+
+		private final Locale _locale;
+		private final String _pattern;
+		private final TimeZone _timeZone;
+
+	}
 
 }
