9ee687aa2f19374dc1dd55de4830898e758a13a2
Hugo Huijser <hugo.huijser@liferay.com>
2017-04-11 21:42:22 +0800

    LPS-71164 Move logic to new class JavaIndexableCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaIndexableCheck.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.source.formatter.parser.JavaSignature;
+import com.liferay.source.formatter.parser.JavaTerm;
+
+/**
+ * @author Hugo Huijser
+ */
+public class JavaIndexableCheck extends BaseJavaTermCheck {
+
+	@Override
+	protected String doProcess(
+		String fileName, String absolutePath, JavaTerm javaTerm,
+		String fileContent) {
+
+		if (fileName.endsWith("LocalServiceImpl.java") &&
+			javaTerm.hasAnnotation("Indexable")) {
+
+			JavaSignature signature = javaTerm.getSignature();
+
+			if (Validator.isNull(signature.getReturnType())) {
+				addMessage(
+					fileName,
+					"Missing return type for method '" + javaTerm.getName() +
+						"' with @Indexable");
+			}
+		}
+
+		return javaTerm.getContent();
+	}
+
+	protected String[] getCheckableJavaTermNames() {
+		return new String[] {JAVA_METHOD};
+	}
+
+}
