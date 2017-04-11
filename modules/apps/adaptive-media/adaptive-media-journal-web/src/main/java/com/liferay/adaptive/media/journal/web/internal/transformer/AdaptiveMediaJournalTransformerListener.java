be554da5b462fc7e210ba3bb5b9a5d29968ae34b
Alejandro Tardín <alejandro.tardin@liferay.com>
2017-04-11 16:23:34 +0200

    LPS-70954 Dynamically replacing journal content

---
+++ modules/apps/adaptive-media/adaptive-media-journal-web/src/main/java/com/liferay/adaptive/media/journal/web/internal/transformer/AdaptiveMediaJournalTransformerListener.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.adaptive.media.journal.web.internal.transformer;
+
+import com.liferay.adaptive.media.content.transformer.ContentTransformerHandler;
+import com.liferay.adaptive.media.content.transformer.constants.ContentTransformerContentTypes;
+import com.liferay.journal.constants.JournalPortletKeys;
+import com.liferay.portal.kernel.templateparser.BaseTransformerListener;
+import com.liferay.portal.kernel.templateparser.TransformerListener;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(
+	immediate = true,
+	property = "javax.portlet.name=" + JournalPortletKeys.JOURNAL,
+	service = TransformerListener.class
+)
+public class AdaptiveMediaJournalTransformerListener
+	extends BaseTransformerListener {
+
+	@Override
+	public String onOutput(
+		String output, String languageId, Map<String, String> tokens) {
+
+		return _contentTransformerHandler.transform(
+			ContentTransformerContentTypes.HTML, output);
+	}
+
+	@Reference
+	private ContentTransformerHandler _contentTransformerHandler;
+
+}
