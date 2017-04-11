f2ef4f13c26d0fe5ae13110869fd85b32328b377
Preston-Crary <preston.crary@liferay.com>
2017-04-11 09:23:03 -0700

    LPS-71605 Remove usages of deprecated method.

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/util/BaseDDMDisplay.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/util/BaseDDMDisplay.java
@@ -400,6 +400,10 @@ public abstract class BaseDDMDisplay implements DDMDisplay {
 		return false;
 	}
 
+	/**
+	 * @deprecated As of 3.6.0, with no direct replacement
+	 */
+	@Deprecated
 	protected ResourceBundle getBaseDDMDisplayResourceBundle(
 		String languageId) {
 
@@ -410,6 +414,10 @@ public abstract class BaseDDMDisplay implements DDMDisplay {
 			baseDDMDisplayClazz.getClassLoader());
 	}
 
+	/**
+	 * @deprecated As of 3.6.0, with no direct replacement
+	 */
+	@Deprecated
 	protected ResourceBundle getDDMDisplayResourceBundle(String languageId) {
 		Bundle bundle = FrameworkUtil.getBundle(getClass());
 
@@ -435,6 +443,10 @@ public abstract class BaseDDMDisplay implements DDMDisplay {
 		return LanguageUtil.get(locale, "templates");
 	}
 
+	/**
+	 * @deprecated As of 3.6.0, with no direct replacement
+	 */
+	@Deprecated
 	protected ResourceBundle getPortalResourceBundle(String languageId) {
 		ResourceBundleLoader portalResourceBundleLoader =
 			ResourceBundleLoaderUtil.getPortalResourceBundleLoader();
@@ -443,21 +455,37 @@ public abstract class BaseDDMDisplay implements DDMDisplay {
 	}
 
 	protected ResourceBundle getResourceBundle(Locale locale) {
-		String languageId = LocaleUtil.toLanguageId(locale);
+		Bundle bundle = FrameworkUtil.getBundle(getClass());
 
-		ResourceBundle ddmDisplayResourceBundle = getDDMDisplayResourceBundle(
-			languageId);
+		ResourceBundleLoader resourceBundleLoader =
+			ResourceBundleLoaderUtil.
+				getResourceBundleLoaderByBundleSymbolicName(
+					bundle.getSymbolicName());
+
+		ResourceBundle ddmDisplayResourceBundle = null;
+
+		if (resourceBundleLoader != null) {
+			ddmDisplayResourceBundle = resourceBundleLoader.loadResourceBundle(
+				locale);
+		}
+
+		ResourceBundle baseDDMDisplayResourceBundle =
+			ResourceBundleUtil.getBundle(
+				"content.Language", locale,
+				BaseDDMDisplay.class.getClassLoader());
+
+		ResourceBundleLoader portalResourceBundleLoader =
+			ResourceBundleLoaderUtil.getPortalResourceBundleLoader();
 
 		if (ddmDisplayResourceBundle == null) {
 			return new AggregateResourceBundle(
-				getBaseDDMDisplayResourceBundle(languageId),
-				getPortalResourceBundle(languageId));
+				baseDDMDisplayResourceBundle,
+				portalResourceBundleLoader.loadResourceBundle(locale));
 		}
 
 		return new AggregateResourceBundle(
-			ddmDisplayResourceBundle,
-			getBaseDDMDisplayResourceBundle(languageId),
-			getPortalResourceBundle(languageId));
+			ddmDisplayResourceBundle, baseDDMDisplayResourceBundle,
+			portalResourceBundleLoader.loadResourceBundle(locale));
 	}
 
 	protected String getViewTemplatesURL(
--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-evaluator-impl/src/main/java/com/liferay/dynamic/data/mapping/form/evaluator/internal/DDMFormEvaluatorHelper.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-evaluator-impl/src/main/java/com/liferay/dynamic/data/mapping/form/evaluator/internal/DDMFormEvaluatorHelper.java
@@ -51,7 +51,6 @@ import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.AggregateResourceBundle;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ListUtil;
-import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ResourceBundleLoader;
 import com.liferay.portal.kernel.util.ResourceBundleLoaderUtil;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
@@ -232,10 +231,8 @@ public class DDMFormEvaluatorHelper {
 		ResourceBundleLoader portalResourceBundleLoader =
 			ResourceBundleLoaderUtil.getPortalResourceBundleLoader();
 
-		String languageId = LocaleUtil.toLanguageId(_locale);
-
 		ResourceBundle portalResourceBundle =
-			portalResourceBundleLoader.loadResourceBundle(languageId);
+			portalResourceBundleLoader.loadResourceBundle(_locale);
 
 		ResourceBundle portletResourceBundle = ResourceBundleUtil.getBundle(
 			"content.Language", _locale, getClass());
