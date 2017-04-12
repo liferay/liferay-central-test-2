8915303f5a77095226ef646fe8dd58875b9f601d
Preston-Crary <preston.crary@liferay.com>
2017-04-12 14:04:59 -0700

    LPS-67449 Apply to FullNameGeneratorFactory, see LPS-63698.

--- portal-kernel/src/com/liferay/portal/kernel/security/auth/FullNameGeneratorFactory.java
+++ portal-kernel/src/com/liferay/portal/kernel/security/auth/FullNameGeneratorFactory.java
@@ -14,9 +14,7 @@
 
 package com.liferay.portal.kernel.security.auth;
 
-import com.liferay.registry.Registry;
-import com.liferay.registry.RegistryUtil;
-import com.liferay.registry.ServiceTracker;
+import com.liferay.portal.kernel.util.ServiceProxyFactory;
 
 /**
  * @author Michael C. Han
@@ -25,13 +23,6 @@ import com.liferay.registry.ServiceTracker;
 public class FullNameGeneratorFactory {
 
 	public static FullNameGenerator getInstance() {
-		FullNameGenerator fullNameGenerator =
-			_instance._serviceTracker.getService();
-
-		if (fullNameGenerator != null) {
-			return fullNameGenerator;
-		}
-
 		return _fullNameGenerator;
 	}
 
@@ -40,19 +31,11 @@ public class FullNameGeneratorFactory {
 	}
 
 	private FullNameGeneratorFactory() {
-		Registry registry = RegistryUtil.getRegistry();
-
-		_serviceTracker = registry.trackServices(FullNameGenerator.class);
-
-		_serviceTracker.open();
 	}
 
-	private static final FullNameGeneratorFactory _instance =
-		new FullNameGeneratorFactory();
-
-	private static FullNameGenerator _fullNameGenerator;
-
-	private final ServiceTracker<FullNameGenerator, FullNameGenerator>
-		_serviceTracker;
+	private static volatile FullNameGenerator _fullNameGenerator =
+		ServiceProxyFactory.newServiceTrackedInstance(
+			FullNameGenerator.class, FullNameGeneratorFactory.class,
+			"_fullNameGenerator", true);
 
 }
