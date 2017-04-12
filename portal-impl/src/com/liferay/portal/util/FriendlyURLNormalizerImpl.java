de8dbf7617585e72125bac23655745ffb8009c3a
Preston-Crary <preston.crary@liferay.com>
2017-04-12 09:49:56 -0700

    LPS-67449 Spring beans are registered into OSGI through our bridge, there's no need to set it. Set the service ranking to 100. Use ServiceProxyFactory in core, this provides better performance since this is a pushing type behavior instead of pulling (the field is updated when a new service with a higher ranking is deployed instead of asking for the service each time we use it). We have to keep the setting by default for DBUpgrader since it doesn't initialize the context until after upgrade. Do not deprecate method that is still being used.

--- portal-impl/src/com/liferay/portal/util/FriendlyURLNormalizerImpl.java
+++ portal-impl/src/com/liferay/portal/util/FriendlyURLNormalizerImpl.java
@@ -15,6 +15,7 @@
 package com.liferay.portal.util;
 
 import com.liferay.portal.kernel.security.pacl.DoPrivileged;
+import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
 import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.FriendlyURLNormalizer;
 import com.liferay.portal.kernel.util.StringPool;
@@ -30,6 +31,7 @@ import java.util.regex.Pattern;
  * @author Shuyang Zhou
  */
 @DoPrivileged
+@OSGiBeanProperties(property = {"service.ranking:Integer=100"})
 public class FriendlyURLNormalizerImpl implements FriendlyURLNormalizer {
 
 	@Override
