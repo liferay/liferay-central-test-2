5dda5a7194c5eb4c28eba6a1f939f017e1d2fd51
Hugo Huijser <hugo.huijser@liferay.com>
2017-04-13 11:23:12 +0800

    LPS-71164 Move logic to new class StringMethodsCheck and JSPStringMethodsCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/StringMethodsCheck.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Hugo Huijser
+ */
+public class StringMethodsCheck extends BaseFileCheck {
+
+	public StringMethodsCheck(List<String> excludes) {
+		_excludes = excludes;
+	}
+
+	@Override
+	protected String doProcess(
+		String fileName, String absolutePath, String content) {
+
+		if (isExcludedPath(_excludes, absolutePath)) {
+			return content;
+		}
+
+		_checkInefficientStringMethods(
+			fileName, content, "\\.toLowerCase\\(\\)", "toLowerCase");
+		_checkInefficientStringMethods(
+			fileName, content, "\\.toUpperCase\\(\\)", "toUpperCase");
+		_checkInefficientStringMethods(
+			fileName, content, "(?<!StringUtil)\\.equalsIgnoreCase\\(",
+			"equalsIgnoreCase");
+
+		return content;
+	}
+
+	protected boolean isJavaSource(String content, int pos) {
+		return true;
+	}
+
+	private void _checkInefficientStringMethods(
+		String fileName, String content, String regex, String methodName) {
+
+		Pattern pattern = Pattern.compile(regex);
+
+		Matcher matcher = pattern.matcher(content);
+
+		while (matcher.find()) {
+			if (isJavaSource(content, matcher.start())) {
+				addMessage(
+					fileName, "Use StringUtil." + methodName,
+					getLineCount(content, matcher.start()));
+			}
+		}
+	}
+
+	private final List<String> _excludes;
+
+}
