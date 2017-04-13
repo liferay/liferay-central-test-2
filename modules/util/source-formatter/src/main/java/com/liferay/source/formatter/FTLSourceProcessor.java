1cb4da026fc770932c23c7b625c9fd8366e18327
Hugo Huijser <hugo.huijser@liferay.com>
2017-04-13 20:35:39 +0800

    LPS-71164 Move whitespace checking to FTLWhitespaceCheck

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/FTLSourceProcessor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/FTLSourceProcessor.java
@@ -43,8 +43,6 @@ public class FTLSourceProcessor extends BaseSourceProcessor {
 			File file, String fileName, String absolutePath, String content)
 		throws Exception {
 
-		content = StringUtil.replace(content, " >\n", ">\n");
-
 		content = sortLiferayVariables(content);
 
 		Matcher matcher = _singleParameterTagPattern.matcher(content);
