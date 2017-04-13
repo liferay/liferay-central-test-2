dae325496bc1cfcbf45e7ee601cf95847afab21e
Hugo Huijser <hugo.huijser@liferay.com>
2017-04-13 20:58:39 +0800

    LPS-71164 Move logic to new class FTLImportsCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/FTLImportsCheck.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.tools.ImportsFormatter;
+import com.liferay.source.formatter.FTLImportsFormatter;
+
+/**
+ * @author Hugo Huijser
+ */
+public class FTLImportsCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws Exception {
+
+		ImportsFormatter importsFormatter = new FTLImportsFormatter();
+
+		return importsFormatter.format(content, null, null);
+	}
+
+}
