df6285b77d25bbc7bd1ef162f0b6963fc7a90338
Hugo Huijser <hugo.huijser@liferay.com>
2017-04-13 14:11:12 +0800

    LPS-71164 Move logic to new class CSSWhitespaceCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/CSSWhitespaceCheck.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+
+/**
+ * @author Hugo Huijser
+ */
+public class CSSWhitespaceCheck extends WhitespaceCheck {
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws Exception {
+
+		content = StringUtil.replace(
+			content, StringPool.DOUBLE_SPACE, StringPool.SPACE);
+
+		return super.doProcess(fileName, absolutePath, content);
+	}
+
+}
