3567b43357c546fd6e915ccc2edf8b4427fa9045
Hugo Huijser <hugo.huijser@liferay.com>
2017-04-14 11:20:33 +0800

    LPS-71164 Move logic to new class PrimitiveWrapperInstantiationCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PrimitiveWrapperInstantiationCheck.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.util.StringUtil;
+
+/**
+ * @author Hugo Huijser
+ */
+public class PrimitiveWrapperInstantiationCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+		String fileName, String absolutePath, String content) {
+
+		if (fileName.endsWith("PrimitiveWrapperInstantiationCheck.java")) {
+			return content;
+		}
+
+		return StringUtil.replace(
+			content,
+			new String[] {
+				"new Boolean(", "new Byte(", "new Character(", "new Double(",
+				"new Float(", "new Integer(", "new Long(", "new Short("
+			},
+			new String[] {
+				"Boolean.valueOf(", "Byte.valueOf(", "Character.valueOf(",
+				"Double.valueOf(", "Float.valueOf(", "Integer.valueOf(",
+				"Long.valueOf(", "Short.valueOf("
+			});
+	}
+
+}
