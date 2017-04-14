1349e7e2c636ba8ca4f2d952d20a7c7d9f144804
Hugo Huijser <hugo.huijser@liferay.com>
2017-04-14 11:18:27 +0800

    LPS-71164 Move logic to new class JavaImportsCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaImportsCheck.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.tools.ImportsFormatter;
+import com.liferay.portal.tools.JavaImportsFormatter;
+import com.liferay.source.formatter.checks.util.JavaSourceUtil;
+
+/**
+ * @author Hugo Huijser
+ */
+public class JavaImportsCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws Exception {
+
+		ImportsFormatter importsFormatter = new JavaImportsFormatter();
+
+		String className = JavaSourceUtil.getClassName(fileName);
+		String packagePath = JavaSourceUtil.getPackagePath(content);
+
+		return importsFormatter.format(content, packagePath, className);
+	}
+
+}
