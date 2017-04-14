c7b4302397130b55af32bc0d9a23b1622df12f03
Hugo Huijser <hugo.huijser@liferay.com>
2017-04-14 11:17:29 +0800

    LPS-71164 Move logic to new class Java2HTMLCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/Java2HTMLCheck.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.source.formatter.checks.util.JavaSourceUtil;
+
+/**
+ * @author Hugo Huijser
+ */
+public class Java2HTMLCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+		String fileName, String absolutePath, String content) {
+
+		String className = JavaSourceUtil.getClassName(fileName);
+
+		if (content.contains(className + ".java.html")) {
+			addMessage(fileName, "Java2HTML");
+		}
+
+		return content;
+	}
+
+}
