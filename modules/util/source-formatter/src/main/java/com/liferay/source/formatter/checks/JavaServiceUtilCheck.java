a0e681be04f64946d8cd3bf2a8dfb5c8aa8f7433
Hugo Huijser <hugo.huijser@liferay.com>
2017-04-14 11:20:03 +0800

    LPS-71164 Move logic to new class JavaServiceUtilCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaServiceUtilCheck.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.source.formatter.checks.util.JavaSourceUtil;
+
+/**
+ * @author Hugo Huijser
+ */
+public class JavaServiceUtilCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+		String fileName, String absolutePath, String content) {
+
+		String className = JavaSourceUtil.getClassName(fileName);
+
+		if (!absolutePath.contains("/wsrp/internal/bind/") &&
+			!className.equals("BaseServiceImpl") &&
+			className.endsWith("ServiceImpl") &&
+			content.contains("ServiceUtil.")) {
+
+			addMessage(
+				fileName,
+				"Do not use *ServiceUtil in *ServiceImpl class, create a " +
+					"reference via service.xml instead");
+		}
+
+		return content;
+	}
+
+}
