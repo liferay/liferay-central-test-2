90e24ae5414adad3e0c2fad44bc798d7e72e855f
Hugo Huijser <hugo.huijser@liferay.com>
2017-04-17 20:41:06 +0800

    LPS-71164 Move logic to new class PropertiesPortletFileCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesPortletFileCheck.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
+import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
+import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+/**
+ * @author Hugo Huijser
+ */
+public class PropertiesPortletFileCheck extends BaseFileCheck {
+
+	public PropertiesPortletFileCheck(
+		boolean portalSource, boolean subrepository) {
+
+		_portalSource = portalSource;
+		_subrepository = subrepository;
+	}
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws Exception {
+
+		if (fileName.endsWith("/portlet.properties")) {
+			return _formatPortletProperties(fileName, content);
+		}
+
+		return content;
+	}
+
+	private String _formatPortletProperties(String fileName, String content)
+		throws Exception {
+
+		if (!content.contains("include-and-override=portlet-ext.properties")) {
+			content =
+				"include-and-override=portlet-ext.properties\n\n" + content;
+		}
+
+		if (!_portalSource && !_subrepository) {
+			return content;
+		}
+
+		try (UnsyncBufferedReader unsyncBufferedReader =
+				new UnsyncBufferedReader(new UnsyncStringReader(content))) {
+
+			int lineCount = 0;
+
+			String line = null;
+
+			String previousProperty = StringPool.BLANK;
+
+			while ((line = unsyncBufferedReader.readLine()) != null) {
+				lineCount++;
+
+				if (lineCount == 1) {
+					continue;
+				}
+
+				if (line.startsWith(StringPool.POUND) ||
+					line.startsWith(StringPool.SPACE) ||
+					line.startsWith(StringPool.TAB)) {
+
+					continue;
+				}
+
+				int pos = line.indexOf(CharPool.EQUAL);
+
+				if (pos == -1) {
+					continue;
+				}
+
+				String property = StringUtil.trim(line.substring(0, pos));
+
+				pos = property.indexOf(CharPool.OPEN_BRACKET);
+
+				if (pos != -1) {
+					property = property.substring(0, pos);
+				}
+
+				if (Validator.isNotNull(previousProperty) &&
+					(previousProperty.compareToIgnoreCase(property) > 0)) {
+
+					addMessage(
+						fileName, "Unsorted property '" + property + "'",
+						lineCount);
+				}
+
+				previousProperty = property;
+			}
+		}
+
+		return content;
+	}
+
+	private final boolean _portalSource;
+	private final boolean _subrepository;
+
+}
