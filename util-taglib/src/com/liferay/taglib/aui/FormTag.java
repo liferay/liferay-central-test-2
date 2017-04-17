d2b9312e86195fbba93a964fbad4b8783133adcc
Preston-Crary <preston.crary@liferay.com>
2017-04-17 12:06:23 -0700

    LPS-71840 Revert everything.

--- util-taglib/src/com/liferay/taglib/aui/FormTag.java
+++ util-taglib/src/com/liferay/taglib/aui/FormTag.java
@@ -15,27 +15,17 @@
 package com.liferay.taglib.aui;
 
 import com.liferay.portal.kernel.servlet.taglib.aui.ValidatorTag;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
-import com.liferay.portal.kernel.util.JavaConstants;
-import com.liferay.portal.kernel.util.PortalUtil;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.taglib.aui.base.BaseFormTag;
-import com.liferay.taglib.util.InlineUtil;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.portlet.PortletRequest;
-import javax.portlet.PortletResponse;
 import javax.portlet.PortletURL;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.jsp.JspWriter;
 
 /**
  * @author Julio Camarero
@@ -80,79 +70,14 @@ public class FormTag extends BaseFormTag {
 	}
 
 	@Override
-	protected int processStartTag() throws Exception {
-		JspWriter jspWriter = pageContext.getOut();
-
-		jspWriter.write("<form action=\"");
-
-		String action = getAction();
-
-		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		if (themeDisplay.isAddSessionIdToURL()) {
-			action = PortalUtil.getURLWithSessionId(
-				action, themeDisplay.getSessionId());
-		}
-
-		jspWriter.write(HtmlUtil.escapeAttribute(action));
-
-		jspWriter.write("\" class=\"form ");
-		jspWriter.write(GetterUtil.getString(getCssClass()));
-
-		if (getInlineLabels()) {
-			jspWriter.write(" field-labels-inline");
-		}
-
-		jspWriter.write("\" data-fm-namespace=\"");
-
-		String namespace = null;
-
-		PortletRequest portletRequest = (PortletRequest)request.getAttribute(
-			JavaConstants.JAVAX_PORTLET_REQUEST);
-
-		if (portletRequest != null) {
-			PortletResponse portletResponse =
-				(PortletResponse)request.getAttribute(
-					JavaConstants.JAVAX_PORTLET_RESPONSE);
-
-			namespace = AUIUtil.getNamespace(portletRequest, portletResponse);
-		}
-
-		if (Validator.isNull(namespace)) {
-			namespace = AUIUtil.getNamespace(request);
-		}
-
-		jspWriter.write(namespace);
-
-		jspWriter.write("\" id=\"");
-		jspWriter.write(namespace);
-
-		String escapedName = HtmlUtil.escapeAttribute(getName());
-
-		jspWriter.write(escapedName);
-
-		jspWriter.write("\" method=\"");
-		jspWriter.write(getMethod());
-		jspWriter.write("\" name=\"");
-		jspWriter.write(namespace);
-		jspWriter.write(escapedName);
-		jspWriter.write("\" ");
-		jspWriter.write(
-			InlineUtil.buildDynamicAttributes(getDynamicAttributes()));
-		jspWriter.write(">");
-
-		if (Validator.isNotNull(getOnSubmit())) {
-			jspWriter.write(
-				"<fieldset class=\"input-container\" disabled=\"disabled\">");
-		}
-
-		return EVAL_BODY_INCLUDE;
-	}
-
-	@Override
 	protected void setAttributes(HttpServletRequest request) {
 		super.setAttributes(request);
+
+		if (getEscapeXml()) {
+			String action = getAction();
+
+			super.setAction(HtmlUtil.escape(action));
+		}
 
 		request.setAttribute("aui:form:validatorTagsMap", _validatorTagsMap);
 		request.setAttribute(
