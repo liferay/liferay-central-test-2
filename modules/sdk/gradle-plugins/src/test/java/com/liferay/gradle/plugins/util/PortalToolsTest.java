2cc73c4a5d17cade06e0e831d86f0fa58d7aced2
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-04-17 14:13:47 -0700

    LPS-70451 Test whether portal-tools.properties versions are up-to-date

---
+++ modules/sdk/gradle-plugins/src/test/java/com/liferay/gradle/plugins/util/PortalToolsTest.java
@@ -0,0 +1,120 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.gradle.plugins.util;
+
+import com.liferay.gradle.plugins.internal.AlloyTaglibDefaultsPlugin;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.net.URL;
+
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Properties;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Andrea Di Giorgi
+ */
+public class PortalToolsTest {
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		_initDependencies();
+		_initVersions();
+	}
+
+	@Test
+	public void testVersions() {
+		for (Map.Entry<String, String> entry :
+				_dependencyPortalToolNamesMap.entrySet()) {
+
+			String dependencyName = entry.getKey();
+			String portalToolName = entry.getValue();
+
+			String dependency = _dependencies.getProperty(dependencyName);
+
+			String[] tokens = dependency.split(":");
+
+			String dependencyVersion = tokens[2];
+
+			Assert.assertEquals(
+				"Please update \"" + portalToolName + "\" version to " +
+					dependencyVersion + " in " + _file,
+				dependencyVersion, _versions.get(portalToolName));
+		}
+	}
+
+	private static void _initDependencies() throws IOException {
+		Path libDirPath = Paths.get("../../../lib");
+
+		Files.walkFileTree(
+			libDirPath,
+			new SimpleFileVisitor<Path>() {
+
+				@Override
+				public FileVisitResult preVisitDirectory(
+						Path dirPath, BasicFileAttributes basicFileAttributes)
+					throws IOException {
+
+					Path path = dirPath.resolve("dependencies.properties");
+
+					if (Files.notExists(path)) {
+						return FileVisitResult.CONTINUE;
+					}
+
+					try (InputStream inputStream = Files.newInputStream(path)) {
+						_dependencies.load(inputStream);
+					}
+
+					return FileVisitResult.CONTINUE;
+				}
+
+			});
+	}
+
+	private static void _initVersions() throws Exception {
+		ClassLoader classLoader = PortalToolsTest.class.getClassLoader();
+
+		URL url = classLoader.getResource(PortalTools.PORTAL_TOOLS_FILE_NAME);
+
+		_file = new File(url.toURI());
+
+		try (InputStream inputStream = new FileInputStream(_file)) {
+			_versions.load(inputStream);
+		}
+	}
+
+	private static final Properties _dependencies = new Properties();
+	private static final Map<String, String> _dependencyPortalToolNamesMap =
+		Collections.singletonMap(
+			"alloy-taglib", AlloyTaglibDefaultsPlugin.PORTAL_TOOL_NAME);
+	private static File _file;
+	private static final Properties _versions = new Properties();
+
+}
