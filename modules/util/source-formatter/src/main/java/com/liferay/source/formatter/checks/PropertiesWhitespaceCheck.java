e0f925170b212190c52d58de59335be3c23c9161
Hugo Huijser <hugo.huijser@liferay.com>
2017-04-17 20:41:59 +0800

    LPS-71164 Move logic to new class PropertiesWhitespaceCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesWhitespaceCheck.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
+import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+
+/**
+ * @author Hugo Huijser
+ */
+public class PropertiesWhitespaceCheck extends WhitespaceCheck {
+
+	public PropertiesWhitespaceCheck(boolean allowLeadingSpaces) {
+		super(allowLeadingSpaces);
+	}
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws Exception {
+
+		StringBundler sb = new StringBundler();
+
+		try (UnsyncBufferedReader unsyncBufferedReader =
+				new UnsyncBufferedReader(new UnsyncStringReader(content))) {
+
+			String line = null;
+
+			while ((line = unsyncBufferedReader.readLine()) != null) {
+				if (line.startsWith(StringPool.TAB)) {
+					line = line.replace(StringPool.TAB, StringPool.FOUR_SPACES);
+				}
+
+				if (line.contains(" \t")) {
+					line = line.replace(" \t", " " + StringPool.FOUR_SPACES);
+				}
+
+				sb.append(line);
+				sb.append("\n");
+			}
+		}
+
+		content = sb.toString();
+
+		if (content.endsWith("\n")) {
+			content = content.substring(0, content.length() - 1);
+		}
+
+		return super.doProcess(fileName, absolutePath, content);
+	}
+
+}
