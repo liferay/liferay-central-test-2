d52aa074836afa5944087086c18c9df0245cefeb
shuyangzhou <shuyang.zhou@liferay.com>
2017-04-17 15:50:50 -0700

    LPS-71686 Regenerate

--- modules/apps/collaboration/flags/flags-api/src/main/java/com/liferay/flags/service/FlagsEntryServiceWrapper.java
+++ modules/apps/collaboration/flags/flags-api/src/main/java/com/liferay/flags/service/FlagsEntryServiceWrapper.java
@@ -32,6 +32,16 @@ public class FlagsEntryServiceWrapper implements FlagsEntryService,
 		_flagsEntryService = flagsEntryService;
 	}
 
+	/**
+	* Returns the OSGi service identifier.
+	*
+	* @return the OSGi service identifier
+	*/
+	@Override
+	public java.lang.String getOSGiServiceIdentifier() {
+		return _flagsEntryService.getOSGiServiceIdentifier();
+	}
+
 	@Override
 	public void addEntry(java.lang.String className, long classPK,
 		java.lang.String reporterEmailAddress, long reportedUserId,
