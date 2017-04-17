eb2765ece71a3c794e26bb5b155e97099877eed3
Pavel Savinov <pavel.savinov@liferay.com>
2017-04-17 12:24:38 +0200

    LPS-71850 add appropriate alloy editor configuration contributors for journal article title and description

---
+++ modules/apps/web-experience/journal/journal-web/src/main/java/com/liferay/journal/web/internal/editor/configuration/JournalArticleDescriptionEditorConfigContributor.java
@@ -0,0 +1,103 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.web.internal.editor.configuration;
+
+import com.liferay.journal.constants.JournalPortletKeys;
+import com.liferay.portal.kernel.editor.configuration.BaseEditorConfigContributor;
+import com.liferay.portal.kernel.editor.configuration.EditorConfigContributor;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	property = {
+		"editor.config.key=descriptionMapAsXML",
+		"javax.portlet.name=" + JournalPortletKeys.JOURNAL
+	},
+	service = EditorConfigContributor.class
+)
+public class JournalArticleDescriptionEditorConfigContributor
+	extends BaseEditorConfigContributor {
+
+	@Override
+	public void populateConfigJSONObject(
+		JSONObject jsonObject, Map<String, Object> inputEditorTaglibAttributes,
+		ThemeDisplay themeDisplay,
+		RequestBackedPortletURLFactory requestBackedPortletURLFactory) {
+
+		jsonObject.put(
+			"toolbars", getToolbarsJSONObject(themeDisplay.getLocale()));
+	}
+
+	protected JSONObject getToolbarsJSONObject(Locale locale) {
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		jsonObject.put("styles", getToolbarsStylesJSONObject(locale));
+
+		return jsonObject;
+	}
+
+	protected JSONObject getToolbarsStylesJSONObject(Locale locale) {
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		jsonObject.put(
+			"selections", getToolbarsStylesSelectionsJSONArray(locale));
+		jsonObject.put("tabIndex", 1);
+
+		return jsonObject;
+	}
+
+	protected JSONArray getToolbarsStylesSelectionsJSONArray(Locale locale) {
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		jsonArray.put(getToolbarsStylesSelectionsTextJSONObject(locale));
+
+		return jsonArray;
+	}
+
+	protected JSONObject getToolbarsStylesSelectionsTextJSONObject(
+		Locale locale) {
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		jsonArray.put("bold");
+		jsonArray.put("italic");
+		jsonArray.put("underline");
+		jsonArray.put("ol");
+		jsonArray.put("ul");
+		jsonArray.put("link");
+		jsonArray.put("twitter");
+
+		jsonObject.put("buttons", jsonArray);
+
+		jsonObject.put("name", "text");
+		jsonObject.put("test", "AlloyEditor.SelectionTest.text");
+
+		return jsonObject;
+	}
+
+}
