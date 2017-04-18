1e5106b6b91e5c15f23680c3f5f47603b4014f64
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-04-18 09:20:55 +0200

    LPS-71995 Creates form navigator entries and a new category for edit categories

---
+++ modules/apps/web-experience/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/servlet/taglib/ui/CategoryPermissionsFormNavigatorEntry.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.admin.web.internal.servlet.taglib.ui;
+
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.servlet.taglib.ui.FormNavigatorEntry;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	property = {"form.navigator.entry.order:Integer=10"},
+	service = FormNavigatorEntry.class
+)
+public class CategoryPermissionsFormNavigatorEntry
+	extends BaseCategoryFormNavigatorEntry {
+
+	@Override
+	public String getKey() {
+		return "permissions";
+	}
+
+	@Override
+	public boolean isVisible(User user, AssetCategory category) {
+		if (category != null) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.asset.categories.admin.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+	@Override
+	protected String getJspPath() {
+		return "/category/permissions.jsp";
+	}
+
+}
