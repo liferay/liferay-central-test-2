b1996e7f1a96826c20a52ac8da62debe2900bacc
Brian Chan <brian.chan@liferay.com>
2017-04-18 14:17:30 -0700

    LPS-72005 SF

--- modules/apps/foundation/vulcan/vulcan-api/src/main/java/com/liferay/vulcan/contributor/PathProvider.java
+++ modules/apps/foundation/vulcan/vulcan-api/src/main/java/com/liferay/vulcan/contributor/PathProvider.java
@@ -25,6 +25,6 @@ import java.util.function.BiConsumer;
  */
 public interface PathProvider {
 
-	public <T> void getResources(BiConsumer<String, Resource<T>> match);
+	public <T> void getResources(BiConsumer<String, Resource<T>> biConsumer);
 
 }
