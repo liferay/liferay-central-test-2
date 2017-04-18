fa3513d2e4ba268eb1a1200c662409724ad7debd
Carlos Sierra <carlos.sierra@liferay.com>
2017-04-18 09:48:20 +0200

    LPS-71728 Split the logic into different classes
    The new logic does not share anything with the former one so let's save
    some ifs

---
+++ modules/sdk/ant-bnd/src/main/java/com/liferay/ant/bnd/resource/bundle/AggregateResourceBundleLoaderAnalyzerPlugin.java
@@ -0,0 +1,137 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ant.bnd.resource.bundle;
+
+import aQute.bnd.header.Attrs;
+import aQute.bnd.header.Parameters;
+import aQute.bnd.osgi.Analyzer;
+import aQute.bnd.osgi.Constants;
+import aQute.bnd.osgi.Jar;
+import aQute.bnd.service.AnalyzerPlugin;
+
+import aQute.libg.filters.AndFilter;
+import aQute.libg.filters.LiteralFilter;
+import aQute.libg.filters.NotFilter;
+import aQute.libg.filters.SimpleFilter;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Gregory Amerson
+ */
+public class AggregateResourceBundleLoaderAnalyzerPlugin
+	implements AnalyzerPlugin {
+
+	@Override
+	public boolean analyzeJar(Analyzer analyzer) throws Exception {
+		Jar jar = analyzer.getJar();
+
+		Parameters aggregateResourceBundlesParameters = new SortedParameters(
+			analyzer.getProperty("-aggregateresourcebundles"));
+
+		if (aggregateResourceBundlesParameters.size() == 0) {
+			return false;
+		}
+
+		Parameters provideCapabilityHeaders = new SortedParameters(
+			analyzer.getProperty(Constants.PROVIDE_CAPABILITY));
+
+		Parameters requireCapabilityHeaders = new SortedParameters(
+			analyzer.getProperty(Constants.REQUIRE_CAPABILITY));
+
+		List<String> aggregateKeys = new ArrayList<>(
+			aggregateResourceBundlesParameters.keySet());
+
+		Collections.sort(aggregateKeys);
+
+		for (String aggregateResourceBundlesParameter : aggregateKeys) {
+			Attrs requireAttrs = new Attrs();
+
+			requireAttrs.put(
+				"filter:",
+				new SimpleFilter(
+					"bundle.symbolic.name",
+					aggregateResourceBundlesParameter).toString());
+
+			requireCapabilityHeaders.add(
+				"liferay.resource.bundle", requireAttrs);
+		}
+
+		analyzer.setProperty(
+			Constants.REQUIRE_CAPABILITY, requireCapabilityHeaders.toString());
+
+		StringBuilder aggregateValue = new StringBuilder();
+
+		AndFilter aggregateSelfFilter = new AndFilter();
+
+		aggregateSelfFilter.addChild(
+			new SimpleFilter("bundle.symbolic.name", analyzer.getBsn()));
+		aggregateSelfFilter.addChild(
+			new NotFilter(new LiteralFilter("(aggregate=true)")));
+		aggregateSelfFilter.append(aggregateValue);
+
+		for (String aggregateResourceBundlesParameter : aggregateKeys) {
+			aggregateValue.append(",");
+
+			new SimpleFilter(
+				"bundle.symbolic.name",
+				aggregateResourceBundlesParameter).append(aggregateValue);
+		}
+
+		Attrs provideAttrs = new Attrs();
+
+		provideAttrs.put(
+			"resource.bundle.aggregate:String", aggregateValue.toString());
+		provideAttrs.put("bundle.symbolic.name", analyzer.getBsn());
+		provideAttrs.put("resource.bundle.base.name", "content.Language");
+		provideAttrs.put("service.ranking:Long", "1");
+		provideAttrs.put("aggregate", "true");
+
+		String servletContextName = analyzer.getProperty("Web-ContextPath");
+
+		if (servletContextName != null) {
+			try {
+				Path servletContextPath = Paths.get(servletContextName);
+
+				servletContextName = servletContextPath.subpath(
+					0, 1).toString();
+			}
+			catch (Exception e) {
+
+				// ignore
+
+			}
+		}
+
+		if (servletContextName == null) {
+			servletContextName = analyzer.getBsn();
+		}
+
+		provideAttrs.put("servlet.context.name", servletContextName);
+
+		provideCapabilityHeaders.add("liferay.resource.bundle", provideAttrs);
+
+		analyzer.setProperty(
+			Constants.PROVIDE_CAPABILITY, provideCapabilityHeaders.toString());
+
+		return true;
+	}
+
+}
