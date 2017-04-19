25fdcb0b717d82b7171d876e40acebbdfcad771d
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-04-19 11:54:20 +0200

    LPS-72028 Add fetch method

--- portal-kernel/src/com/liferay/portal/kernel/portletfilerepository/PortletFileRepositoryUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/portletfilerepository/PortletFileRepositoryUtil.java
@@ -349,6 +349,13 @@ public class PortletFileRepositoryUtil {
 			repositoryId, searchContext);
 	}
 
+	public FileEntry fetchPortletFileEntry(
+		long groupId, long folderId, String fileName) {
+
+		return getPortletFileRepository().fetchPortletFileEntry(
+			groupId, folderId, fileName);
+	}
+
 	public void setPortletFileRepository(
 		PortletFileRepository portletFileRepository) {
 
