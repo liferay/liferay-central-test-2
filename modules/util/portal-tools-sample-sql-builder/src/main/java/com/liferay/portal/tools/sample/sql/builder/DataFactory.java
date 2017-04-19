d884abd6c863be628325e121284ca35a6e192850
Brian Chan <brian.chan@liferay.com>
2017-04-19 14:54:27 -0700

    LPS-71958 Consistency

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -702,7 +702,7 @@ public class DataFactory {
 	}
 
 	public long getNextAssetClassNameId(long groupId) {
-		Integer counter = _assetClassIdIndexMap.get(groupId);
+		Integer counter = _assetClassNameIdsIndexes.get(groupId);
 
 		if (counter == null) {
 			counter = 0;
@@ -711,7 +711,7 @@ public class DataFactory {
 		long classNameId =
 			_assetClassNameIds[counter % _assetClassNameIds.length];
 
-		_assetClassIdIndexMap.put(groupId, ++counter);
+		_assetClassNameIdsIndexes.put(groupId, ++counter);
 
 		return classNameId;
 	}
@@ -3718,7 +3718,8 @@ public class DataFactory {
 	private Map<Long, SimpleCounter>[] _assetCategoryCounters;
 	private List<AssetCategoryModel>[] _assetCategoryModelsArray;
 	private Map<Long, List<AssetCategoryModel>>[] _assetCategoryModelsArrayMap;
-	private final Map<Long, Integer> _assetClassIdIndexMap = new HashMap<>();
+	private final Map<Long, Integer> _assetClassNameIdsIndexes =
+		new HashMap<>();
 	private final long[] _assetClassNameIds;
 	private String _assetPublisherQueryName;
 	private final Map<Long, Integer> _assetPublisherQueryStartIndex =
