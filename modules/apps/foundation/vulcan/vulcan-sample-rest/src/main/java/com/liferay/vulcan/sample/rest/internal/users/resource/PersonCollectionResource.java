89f880c85670ce80ab13eefab58792cbc80fbd18
Alejandro Hernández <info@alejandrohdezma.com>
2017-04-19 11:18:03 +0200

    LPS-72025 Add collection and single person resource, inject company

---
+++ modules/apps/foundation/vulcan/vulcan-sample-rest/src/main/java/com/liferay/vulcan/sample/rest/internal/users/resource/PersonCollectionResource.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.vulcan.sample.rest.internal.users.resource;
+
+import com.liferay.portal.kernel.exception.NoSuchUserException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.service.UserService;
+import com.liferay.vulcan.contributor.APIContributor;
+import com.liferay.vulcan.pagination.Page;
+import com.liferay.vulcan.pagination.Pagination;
+import com.liferay.vulcan.resource.CollectionResource;
+import com.liferay.vulcan.resource.SingleResource;
+
+import java.util.List;
+
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.ServerErrorException;
+import javax.ws.rs.core.Context;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Hernández
+ * @author Carlos Sierra Andrés
+ * @author Jorge Ferrer
+ */
+@Component(immediate = true)
+public class PersonCollectionResource
+	implements CollectionResource<User>, APIContributor {
+
+	@Override
+	public SingleResource<User> getCollectionItemResource(String id) {
+		try {
+			long userId = Long.valueOf(id);
+
+			User user = _userService.getUserById(userId);
+
+			return new PersonSingleResource(user);
+		}
+		catch (NoSuchUserException | PrincipalException e) {
+			throw new NotFoundException();
+		}
+		catch (NumberFormatException nfe) {
+			throw new BadRequestException();
+		}
+		catch (PortalException pe) {
+			throw new ServerErrorException(500, pe);
+		}
+	}
+
+	@Override
+	public Page<User> getPage(Pagination paginationParams) {
+		try {
+			List<User> users = _userService.getCompanyUsers(
+				company.getCompanyId(), paginationParams.getStartPosition(),
+				paginationParams.getEndPosition());
+
+			int userCount = _userService.getCompanyUsersCount(
+				company.getCompanyId());
+
+			return paginationParams.createPage(users, userCount);
+		}
+		catch (PortalException pe) {
+			throw new ServerErrorException(500, pe);
+		}
+	}
+
+	@Override
+	public String getPath() {
+		return "people";
+	}
+
+	@Context
+	protected Company company;
+
+	@Reference
+	private UserService _userService;
+
+}
