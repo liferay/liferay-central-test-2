6dd37adafb4678c512d94fced55c895aa8c645a2
Alejandro Hernández <info@alejandrohdezma.com>
2017-04-19 11:17:27 +0200

    LPS-72025 Add ContextProvider for Companies

---
+++ modules/apps/foundation/vulcan/vulcan-sample-rest/src/main/java/com/liferay/vulcan/sample/rest/internal/providers/CompanyContextProvider.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.vulcan.sample.rest.internal.providers;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.util.Portal;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.ws.rs.ServerErrorException;
+import javax.ws.rs.ext.Provider;
+
+import org.apache.cxf.jaxrs.ext.ContextProvider;
+import org.apache.cxf.message.Message;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Hernández
+ * @author Carlos Sierra Andrés
+ * @author Jorge Ferrer
+ */
+@Component(
+	immediate = true, property = "liferay-vulcan-context-provider=true",
+	service = CompanyContextProvider.class
+)
+@Provider
+public class CompanyContextProvider implements ContextProvider<Company> {
+
+	@Override
+	public Company createContext(Message message) {
+		try {
+			HttpServletRequest httpServletRequest =
+				(HttpServletRequest)
+					message.getContextualProperty("HTTP.REQUEST");
+
+			return _portal.getCompany(httpServletRequest);
+		}
+		catch (PortalException pe) {
+			throw new ServerErrorException(500, pe);
+		}
+	}
+
+	@Reference
+	private Portal _portal;
+
+}
