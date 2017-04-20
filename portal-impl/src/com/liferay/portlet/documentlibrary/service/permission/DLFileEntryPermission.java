0f99a39ce9a003d4a3f8cdea1745ae1a7883f5a3
Roberto Díaz <roberto.diaz@liferay.com>
2017-04-20 17:11:19 +0200

    LPS-70045 PortlefFileEntries should check their parent entity permissions in order to avoid security issues

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/permission/DLFileEntryPermission.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/permission/DLFileEntryPermission.java
@@ -29,8 +29,12 @@ import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.BaseModelPermissionChecker;
+import com.liferay.portal.kernel.security.permission.BaseModelPermissionCheckerUtil;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.ResourcePermissionCheckerUtil;
 import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.permission.WorkflowPermissionUtil;
 import com.liferay.portal.util.PropsValues;
 
@@ -109,6 +113,24 @@ public class DLFileEntryPermission implements BaseModelPermissionChecker {
 
 			if (hasPermission != null) {
 				return hasPermission.booleanValue();
+			}
+		}
+
+		String className = dlFileEntry.getClassName();
+		long classPK = dlFileEntry.getClassPK();
+
+		if (Validator.isNotNull(className) && (classPK > 0)) {
+			boolean hasResourcePermission = GetterUtil.getBoolean(
+				ResourcePermissionCheckerUtil.containsResourcePermission(
+					permissionChecker, className, classPK, actionId));
+
+			boolean hasBaseModelPermission = GetterUtil.getBoolean(
+				BaseModelPermissionCheckerUtil.containsBaseModelPermission(
+					permissionChecker, dlFileEntry.getGroupId(), className,
+					classPK, actionId));
+
+			if (!hasBaseModelPermission && !hasResourcePermission) {
+				return false;
 			}
 		}
 
