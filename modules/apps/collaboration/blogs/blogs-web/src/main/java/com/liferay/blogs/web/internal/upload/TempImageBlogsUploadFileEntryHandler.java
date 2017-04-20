066a3140f114df2db7ccab3c8067b08621a794c7
Alejandro Tardín <alejandro.tardin@liferay.com>
2017-04-20 18:33:24 +0200

    LPS-72078 Avoiding random strings in the final file name
    This is the way it was originally working but on the work done for
    LPS-70229 TempFileEntryUtil.getOriginalTempFileName was used to simplify
    the code without being aware of this correct but ugly behaviour.

--- modules/apps/collaboration/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/upload/TempImageBlogsUploadFileEntryHandler.java
+++ modules/apps/collaboration/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/upload/TempImageBlogsUploadFileEntryHandler.java
@@ -15,13 +15,17 @@
 package com.liferay.blogs.web.internal.upload;
 
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.TempFileEntryUtil;
+import com.liferay.upload.UniqueFileNameProvider;
 
 import java.io.InputStream;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Alejandro Tardín
@@ -36,14 +40,41 @@ public class TempImageBlogsUploadFileEntryHandler
 			ThemeDisplay themeDisplay)
 		throws PortalException {
 
-		String tempFileName = TempFileEntryUtil.getTempFileName(fileName);
+		String uniqueFileName = _uniqueFileNameProvider.provide(
+			fileName, curFileName -> _exists(themeDisplay, curFileName));
 
 		return TempFileEntryUtil.addTempFileEntry(
 			themeDisplay.getScopeGroupId(), themeDisplay.getUserId(),
-			_TEMP_FOLDER_NAME, tempFileName, inputStream, contentType);
+			_TEMP_FOLDER_NAME, uniqueFileName, inputStream, contentType);
+	}
+
+	private boolean _exists(ThemeDisplay themeDisplay, String curFileName) {
+		try {
+			if (TempFileEntryUtil.getTempFileEntry(
+					themeDisplay.getScopeGroupId(), themeDisplay.getUserId(),
+					_TEMP_FOLDER_NAME, curFileName) != null) {
+
+				return true;
+			}
+
+			return false;
+		}
+		catch (PortalException pe) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(pe, pe);
+			}
+
+			return false;
+		}
 	}
 
 	private static final String _TEMP_FOLDER_NAME =
 		TempImageBlogsUploadFileEntryHandler.class.getName();
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		TempImageBlogsUploadFileEntryHandler.class);
+
+	@Reference
+	private UniqueFileNameProvider _uniqueFileNameProvider;
 
 }
