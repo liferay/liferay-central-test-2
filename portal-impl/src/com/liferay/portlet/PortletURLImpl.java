450610b0ac329140766b89c620bc4e9308b08a09
Tina Tian <tina.tian@liferay.com>
2017-04-21 11:05:51 -0700

    LPS-71941 Porltet might be null when it is undeployed.

--- portal-impl/src/com/liferay/portlet/PortletURLImpl.java
+++ portal-impl/src/com/liferay/portlet/PortletURLImpl.java
@@ -1314,6 +1314,10 @@ public class PortletURLImpl
 			PortletLocalServiceUtil.fetchPortletById(
 				PortalUtil.getCompanyId(request), portletId),
 			portletRequest, layout, lifecycle);
+
+		if (_portlet == null) {
+			_portletId = portletId;
+		}
 	}
 
 	private void _appendNamespaceAndEncode(StringBundler sb, String name) {
