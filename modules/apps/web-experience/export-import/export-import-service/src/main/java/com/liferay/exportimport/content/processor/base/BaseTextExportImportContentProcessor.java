e8180a4e7639ae2f7bcc7942eb070d0894128ca1
Michael Bowerman <michael.bowerman@liferay.com>
2017-04-24 10:41:02 -0700

    LPS-70932 Strip the URL of a trailing slash, if it exists

--- modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/content/processor/base/BaseTextExportImportContentProcessor.java
+++ modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/content/processor/base/BaseTextExportImportContentProcessor.java
@@ -1227,6 +1227,10 @@ public class BaseTextExportImportContentProcessor
 				url = url.substring(0, endPos);
 			}
 
+			if (url.endsWith(StringPool.SLASH)) {
+				url = url.substring(0, url.length() - 1);
+			}
+
 			StringBundler urlSB = new StringBundler(1);
 
 			url = replaceExportHostname(groupId, url, urlSB);
