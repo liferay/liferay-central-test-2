7b392d5d1c9e81c3c11cc278582fb155f62cbdd2
Diogo Moreira Lins <diogo.moreira@liferay.com>
2017-04-24 09:37:13 -0300

    LPS-70564 Check valid condition and add new action properties

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_builder_rule_validator.js
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_builder_rule_validator.js
@@ -1,6 +1,15 @@
 AUI.add(
 	'liferay-ddl-form-builder-rule-validator',
 	function(A) {
+		var CONDITIONS_OPERATOR = {
+			'equals-to': 'binary',
+			'is-empty': 'unary',
+			'not-is-empty': 'unary',
+			'not-equals-to': 'binary',
+			'contains': 'binary',
+			'not-contains': 'binary'
+		};
+
 		var FormBuilderRuleValidator = A.Component.create(
 			{
 				ATTRS: {
@@ -14,11 +23,15 @@ AUI.add(
 					isValidRule: function(rule) {
 						var instance = this;
 
-						return instance._isValidActions(rule.actions);
+						var validConditions = instance._isValidConditions(rule.conditions);
+
+						var validActions = instance._isValidActions(rule.actions);
+
+						return  validConditions && validActions;
 					},
 
 					_isPropertyAction: function(type) {
-						return type === 'show' | type === 'enable' | type === 'require' | type === 'jump-to-page';
+						return type === 'show' | type === 'enable' | type === 'require' | type === 'jump-to-page' | type === 'auto-fill' | type === 'calculate';
 					},
 
 					_isValidAction: function(action) {
@@ -49,7 +62,52 @@ AUI.add(
 						}
 
 						return true;
-					}
+					},
+
+					_isValidConditon: function(condition) {
+						var instance = this;
+
+						if (condition.operands.length === 0) {
+							return false;
+						}
+
+						if (!condition.operator) {
+							return false;
+						}
+
+						var operatorType = CONDITIONS_OPERATOR[condition.operator];
+
+						if (operatorType === 'unary' && condition.operands.length > 1) {
+							return false;
+						}
+						
+						if (operatorType === 'binary' && condition.operands.length == 2){
+							if (condition.operands[1].type && condition.operands[1].value) {
+								return true;
+							} 
+							else {
+								return false;
+							}
+						}
+
+						return true;
+					},
+
+					_isValidConditions: function(conditions) {
+						var instance = this;
+
+						if (conditions.length === 0) {
+							return false;
+						}
+
+						for (var i = 0; i < conditions.length; i++) {
+							if (!instance._isValidConditon(conditions[i])) {
+								return false;
+							}
+						}
+
+						return true;
+					 }
 				}
 			}
 		);
