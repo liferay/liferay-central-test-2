1672c8c8a708a428a8034745c156307bd5767597
shuyangzhou <shuyang.zhou@liferay.com>
2017-04-25 09:17:46 -0700

    LPS-72140 Fix bad regenerate

--- modules/apps/foundation/portal-security/portal-security-wedeploy-auth-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/apps/foundation/portal-security/portal-security-wedeploy-auth-service/src/main/resources/META-INF/sql/tables.sql
@@ -9,15 +9,3 @@ create table WeDeployAuth_WeDeployAuthApp (
 	clientId VARCHAR(75) null,
 	clientSecret VARCHAR(75) null
 );
-
-create table WedeployAuth_WeDeployAuthApp (
-	weDeployAuthAppId LONG not null primary key,
-	companyId LONG,
-	userId LONG,
-	userName VARCHAR(75) null,
-	createDate DATE null,
-	modifiedDate DATE null,
-	name VARCHAR(75) null,
-	clientId VARCHAR(75) null,
-	clientSecret VARCHAR(75) null
-);
