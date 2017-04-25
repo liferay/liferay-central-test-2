ec861c19913a1ab6e9e03975ed2e3a00f96f52a3
Leslie Wong <leslie.wong@liferay.com>
2017-04-24 19:17:50 -0700

    LRQA-31988 SF

--- modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/util/StringUtil.java
+++ modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/util/StringUtil.java
@@ -15,7 +15,9 @@
 package com.liferay.poshi.runner.util;
 
 import java.util.Random;
+
 import org.apache.commons.lang3.StringUtils;
+
 /**
  * The String utility class.
  *
@@ -720,4 +722,5 @@ public class StringUtil {
 		'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
 		'u', 'v', 'w', 'x', 'y', 'z'
 	};
+
 }
