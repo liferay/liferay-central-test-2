3efe6ef915a69ac5876b6a1ecb463e295204e5fb
Alejandro Tardín <alejandro.tardin@liferay.com>
2017-04-26 13:06:45 +0200

    LPS-72195 Clearing the web content cache every time the configuration changes

---
+++ modules/apps/adaptive-media/adaptive-media-journal-web/src/main/java/com/liferay/adaptive/media/journal/web/internal/messaging/AdaptiveMediaJournalImageConfigurationMessageListener.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.adaptive.media.journal.web.internal.messaging;
+
+import com.liferay.adaptive.media.image.messaging.AdaptiveMediaImageDestinationNames;
+import com.liferay.journal.util.JournalContent;
+import com.liferay.portal.kernel.messaging.BaseMessageListener;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageListener;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(
+	immediate = true,
+	property = {
+		"destination.name=" + AdaptiveMediaImageDestinationNames.ADAPTIVE_MEDIA_IMAGE_CONFIGURATION
+	},
+	service = MessageListener.class
+)
+public class AdaptiveMediaJournalImageConfigurationMessageListener
+	extends BaseMessageListener {
+
+	@Override
+	protected void doReceive(Message message) throws Exception {
+		_journalContent.clearCache();
+	}
+
+	@Reference
+	private JournalContent _journalContent;
+
+}
