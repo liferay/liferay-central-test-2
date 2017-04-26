d090f85ef7b51bc3c70c341f18ab6a16dd99d349
shuyangzhou <shuyang.zhou@liferay.com>
2017-04-26 08:15:54 -0700

    LPS-72201 Fix import

--- modules/apps/foundation/mobile-device-rules/mobile-device-rules-service/src/main/java/com/liferay/mobile/device/rules/rule/group/action/ThemeModificationActionHandler.java
+++ modules/apps/foundation/mobile-device-rules/mobile-device-rules-service/src/main/java/com/liferay/mobile/device/rules/rule/group/action/ThemeModificationActionHandler.java
@@ -34,6 +34,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Edward Han
