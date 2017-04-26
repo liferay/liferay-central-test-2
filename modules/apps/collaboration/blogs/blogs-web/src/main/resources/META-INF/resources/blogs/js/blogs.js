9d324fda331d8166668a4bb6ce01a8096f8800c6
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2017-04-26 14:32:19 +0200

    LPS-71965 Remove ckeditor attribute to avoid corrupt images

--- modules/apps/collaboration/blogs/blogs-web/src/main/resources/META-INF/resources/blogs/js/blogs.js
+++ modules/apps/collaboration/blogs/blogs-web/src/main/resources/META-INF/resources/blogs/js/blogs.js
@@ -477,6 +477,8 @@ AUI.add(
 									var finalImage = finalContentImages[i];
 
 									if (el.get('tagName') === finalImage.tagName) {
+										el.removeAttribute('data-cke-saved-src');
+
 										for (var j = 0; j < finalImage.attributes.length; j++) {
 											var attr = finalImage.attributes[j];
 
