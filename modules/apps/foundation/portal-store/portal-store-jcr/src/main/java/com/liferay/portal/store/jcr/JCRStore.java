6d9812855c1f7685c576bfee2f226ef358247349
Manuel de la Peña <manuel.delapenya@liferay.com>
2017-04-27 12:45:32 +0200

    LPS-72058 Deprecate JCRStore service
    We only deprecate this implementation of Store, as it's the only piece used from outside this module

--- modules/apps/foundation/portal-store/portal-store-jcr/src/main/java/com/liferay/portal/store/jcr/JCRStore.java
+++ modules/apps/foundation/portal-store/portal-store-jcr/src/main/java/com/liferay/portal/store/jcr/JCRStore.java
@@ -66,6 +66,8 @@ import org.osgi.service.component.annotations.Modified;
  * @author Brian Wing Shun Chan
  * @author Edward Han
  * @author Manuel de la Peña
+ *
+ * @deprecated As of 2.0.0, with no direct replacement
  */
 @Component(
 	configurationPid = "com.liferay.portal.store.jcr.configuration.JCRStoreConfiguration",
@@ -73,6 +75,7 @@ import org.osgi.service.component.annotations.Modified;
 	property = "store.type=com.liferay.portal.store.jcr.JCRStore",
 	service = Store.class
 )
+@Deprecated
 public class JCRStore extends BaseStore {
 
 	@Override
