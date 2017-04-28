f5f3b17f2b1f550b8e3cae917b540a18a3576dae
pyoo47 <peter.yoo@liferay.com>
2017-04-28 09:36:16 -0700

    LRQA-32058 Rename daysOffset to offsetDays

--- modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/util/DateUtil.java
+++ modules/test/poshi-runner/src/main/java/com/liferay/poshi/runner/util/DateUtil.java
@@ -48,36 +48,36 @@ public class DateUtil {
 		return getFormattedCurrentDate("yyyy");
 	}
 
-	public static String getDate(String daysOffset) {
-		return getFormattedDate(daysOffset, "d");
+	public static String getDate(String offsetDays) {
+		return getFormattedDate(offsetDays, "d");
 	}
 
-	public static String getDayOfWeek(String daysOffset) {
-		return getFormattedDate(daysOffset, "EEEE");
+	public static String getDayOfWeek(String offsetDays) {
+		return getFormattedDate(offsetDays, "EEEE");
 	}
 
 	public static String getFormattedCurrentDate(String pattern) {
 		return _format(new Date(), pattern);
 	}
 
-	public static String getFormattedDate(String daysOffset, String pattern) {
-		return _format(_getOffsetDate(Integer.valueOf(daysOffset)), pattern);
+	public static String getFormattedDate(String offsetDays, String pattern) {
+		return _format(_getOffsetDate(Integer.valueOf(offsetDays)), pattern);
 	}
 
-	public static String getMonth(String daysOffset) {
-		return getFormattedDate(daysOffset, "M");
+	public static String getMonth(String offsetDays) {
+		return getFormattedDate(offsetDays, "M");
 	}
 
-	public static String getMonthName(String daysOffset) {
-		return getFormattedDate(daysOffset, "MMMM");
+	public static String getMonthName(String offsetDays) {
+		return getFormattedDate(offsetDays, "MMMM");
 	}
 
 	public static String getNanoseconds() {
 		return String.valueOf(System.nanoTime());
 	}
 
-	public static String getYear(String daysOffset) {
-		return getFormattedDate(daysOffset, "yyyy");
+	public static String getYear(String offsetDays) {
+		return getFormattedDate(offsetDays, "yyyy");
 	}
 
 	private static String _format(Date date, String pattern) {
@@ -86,10 +86,10 @@ public class DateUtil {
 		return simpleDateFormat.format(date);
 	}
 
-	private static Date _getOffsetDate(int daysOffset) {
+	private static Date _getOffsetDate(int offsetDays) {
 		Calendar calendar = Calendar.getInstance();
 
-		calendar.add(Calendar.DATE, daysOffset);
+		calendar.add(Calendar.DATE, offsetDays);
 
 		return calendar.getTime();
 	}
