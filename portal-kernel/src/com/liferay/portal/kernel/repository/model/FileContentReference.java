eed6283ac36b91464b30d3cfd8c4afe21ec54168
Adolfo Pérez <adolfo.perez@liferay.com>
2017-04-28 13:12:59 +0200

    LPS-72162 Add fileEntryId to content reference
    This will allow validators to inspect other aspects of file entries. A
    fileEntryId = 0 means there is no file entry (i.e. the file entry is
    being created); a value != 0 means we're updating a file entry.

--- portal-kernel/src/com/liferay/portal/kernel/repository/model/FileContentReference.java
+++ portal-kernel/src/com/liferay/portal/kernel/repository/model/FileContentReference.java
@@ -24,31 +24,60 @@ import java.io.InputStream;
 public class FileContentReference {
 
 	public static final FileContentReference fromBytes(
+		long fileEntryId, String sourceFileName, String extension,
+		String mimeType, byte[] bytes) {
+
+		return fromInputStream(
+			fileEntryId, sourceFileName, extension, mimeType,
+			new ByteArrayInputStream(bytes), bytes.length);
+	}
+
+	public static final FileContentReference fromBytes(
 		String sourceFileName, String extension, String mimeType,
 		byte[] bytes) {
 
 		return fromInputStream(
-			sourceFileName, extension, mimeType,
+			0, sourceFileName, extension, mimeType,
 			new ByteArrayInputStream(bytes), bytes.length);
+	}
+
+	public static final FileContentReference fromFile(
+		long fileEntryId, String sourceFileName, String extension,
+		String mimeType, File file) {
+
+		return new FileContentReference(
+			fileEntryId, sourceFileName, extension, mimeType, file, null, 0);
 	}
 
 	public static final FileContentReference fromFile(
 		String sourceFileName, String extension, String mimeType, File file) {
 
+		return fromFile(0, sourceFileName, extension, mimeType, file);
+	}
+
+	public static final FileContentReference fromInputStream(
+		long fileEntryId, String sourceFileName, String extension,
+		String mimeType, InputStream inputStream, long size) {
+
 		return new FileContentReference(
-			sourceFileName, extension, mimeType, file, null, 0);
+			fileEntryId, sourceFileName, extension, mimeType, null, inputStream,
+			size);
 	}
 
 	public static final FileContentReference fromInputStream(
 		String sourceFileName, String extension, String mimeType,
 		InputStream inputStream, long size) {
 
-		return new FileContentReference(
-			sourceFileName, extension, mimeType, null, inputStream, size);
+		return fromInputStream(
+			0, sourceFileName, extension, mimeType, inputStream, size);
 	}
 
 	public String getExtension() {
 		return _extension;
+	}
+
+	public long getFileEntryId() {
+		return _fileEntryId;
 	}
 
 	public String getMimeType() {
@@ -72,9 +101,10 @@ public class FileContentReference {
 	}
 
 	protected FileContentReference(
-		String sourceFileName, String extension, String mimeType, File file,
-		InputStream inputStream, long size) {
+		long fileEntryId, String sourceFileName, String extension,
+		String mimeType, File file, InputStream inputStream, long size) {
 
+		_fileEntryId = fileEntryId;
 		_sourceFileName = sourceFileName;
 		_extension = extension;
 		_mimeType = mimeType;
@@ -83,8 +113,21 @@ public class FileContentReference {
 		_size = size;
 	}
 
+	/**
+	 * @deprecated As of 7.0.0, replaced by {@link #FileContentReference(long,
+	 *             String, String, String, File, InputStream, long)}
+	 */
+	@Deprecated
+	protected FileContentReference(
+		String sourceFileName, String extension, String mimeType, File file,
+		InputStream inputStream, long size) {
+
+		this(0, sourceFileName, extension, mimeType, file, inputStream, size);
+	}
+
 	private final String _extension;
 	private final File _file;
+	private final long _fileEntryId;
 	private final InputStream _inputStream;
 	private final String _mimeType;
 	private final long _size;
