0cf26061a4e001385e5559b66c2aea98ec91742a
Máté Thurzó <mate.thurzo@liferay.com>
2017-04-28 13:44:05 +0200

    LPS-72133 Move to a proper package

--- modules/apps/web-experience/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/test/PortletDataHandlerExceptionTest.java
+++ modules/apps/web-experience/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/test/PortletDataHandlerExceptionTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.exportimport.kernel.lar.test;
+package com.liferay.exportimport.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.exportimport.kernel.lar.BasePortletDataHandler;
