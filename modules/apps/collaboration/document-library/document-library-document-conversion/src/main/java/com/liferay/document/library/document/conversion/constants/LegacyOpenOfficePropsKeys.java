6b8311a25da6886143b76de60610e8771d6aa65c
Pei-Jung Lan <pei-jung.lan@liferay.com>
2017-05-01 11:41:49 -0700

    LPS-71382 Add LegacyOpenOfficePropsKeys constant class

---
+++ modules/apps/collaboration/document-library/document-library-document-conversion/src/main/java/com/liferay/document/library/document/conversion/constants/LegacyOpenOfficePropsKeys.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.document.conversion.constants;
+
+/**
+ * @author Pei-Jung Lan
+ */
+public class LegacyOpenOfficePropsKeys {
+
+	public static final String[] KEYS = {
+		LegacyOpenOfficePropsKeys.OPENOFFICE_CACHE_ENABLED,
+		LegacyOpenOfficePropsKeys.OPENOFFICE_SERVER_ENABLED,
+		LegacyOpenOfficePropsKeys.OPENOFFICE_SERVER_HOST,
+		LegacyOpenOfficePropsKeys.OPENOFFICE_SERVER_PORT
+	};
+
+	public static final String OPENOFFICE_CACHE_ENABLED =
+		"openoffice.cache.enabled";
+
+	public static final String OPENOFFICE_SERVER_ENABLED =
+		"openoffice.server.enabled";
+
+	public static final String OPENOFFICE_SERVER_HOST =
+		"openoffice.server.host";
+
+	public static final String OPENOFFICE_SERVER_PORT =
+		"openoffice.server.port";
+
+}
