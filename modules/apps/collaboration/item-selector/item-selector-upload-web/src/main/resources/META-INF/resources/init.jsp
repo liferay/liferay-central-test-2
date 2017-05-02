sergiogonzalez/liferay-portal

LPS-70229 Adding source and return type parameteres in upload

modules/apps/collaboration/item-selector/item-selector-upload-web/src/main/resources/META-INF/resources/init.jsp
@@ -22,13 +22,14 @@
 taglib uri="http://liferay.com/tld/theme" prefix="liferay-theme" %><%@
 taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui" %>
 
-<%@ page import="com.liferay.item.selector.criteria.FileEntryItemSelectorReturnType" %><%@
+<%@ page import="com.liferay.item.selector.ItemSelectorReturnTypeResolver" %><%@
 page import="com.liferay.item.selector.upload.web.internal.ItemSelectorUploadView" %><%@
 page import="com.liferay.item.selector.upload.web.internal.display.context.ItemSelectorUploadViewDisplayContext" %><%@
 page import="com.liferay.portal.kernel.servlet.BrowserSnifferUtil" %><%@
 page import="com.liferay.portal.kernel.util.ArrayUtil" %><%@
 page import="com.liferay.portal.kernel.util.HtmlUtil" %><%@
-page import="com.liferay.portal.kernel.util.StringUtil" %>
+page import="com.liferay.portal.kernel.util.StringUtil" %><%@
+page import="com.liferay.portal.kernel.util.Validator" %>
 
 <liferay-theme:defineObjects />
 

liferay/liferay-central-test-2

modules/apps/collaboration/item-selector/item-selector-upload-web/src/main/resources/META-INF/resources/init.jsp
@@ -20,10 +20,11 @@
 taglib uri="http://liferay.com/tld/theme" prefix="liferay-theme" %><%@
 taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui" %>
 
-<%@ page import="com.liferay.item.selector.criteria.FileEntryItemSelectorReturnType" %><%@
+<%@ page import="com.liferay.item.selector.ItemSelectorReturnTypeResolver" %><%@
 page import="com.liferay.item.selector.upload.web.internal.ItemSelectorUploadView" %><%@
 page import="com.liferay.item.selector.upload.web.internal.display.context.ItemSelectorUploadViewDisplayContext" %><%@
-page import="com.liferay.portal.kernel.util.HtmlUtil" %>
+page import="com.liferay.portal.kernel.util.HtmlUtil" %><%@
+page import="com.liferay.portal.kernel.util.Validator" %>
 
 <liferay-theme:defineObjects />
 