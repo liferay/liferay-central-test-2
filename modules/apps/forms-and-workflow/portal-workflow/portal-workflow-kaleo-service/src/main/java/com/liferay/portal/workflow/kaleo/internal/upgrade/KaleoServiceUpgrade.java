27f30c79a2453714c9a58748535eac26f4ef5c54
Inacio Nery <inacio.nery@liferay.com>
2017-05-02 11:08:23 -0300

    LPS-72137 Expose KaleoServiceUpgrade as a service

--- modules/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/internal/upgrade/KaleoServiceUpgrade.java
+++ modules/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/internal/upgrade/KaleoServiceUpgrade.java
@@ -31,7 +31,10 @@ import org.osgi.service.component.annotations.Component;
 /**
  * @author Marcellus Tavares
  */
-@Component(immediate = true, service = UpgradeStepRegistrator.class)
+@Component(
+	immediate = true,
+	service = {KaleoServiceUpgrade.class, UpgradeStepRegistrator.class}
+)
 public class KaleoServiceUpgrade implements UpgradeStepRegistrator {
 
 	@Override
