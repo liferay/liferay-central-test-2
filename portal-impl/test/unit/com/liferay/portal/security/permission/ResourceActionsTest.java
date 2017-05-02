shuyangzhou/liferay-portal

LPS-71462 Add test

portal-impl/test/unit/com/liferay/portal/security/permission/ResourceActionsTest.java
@@ -1,0 +1,132 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.permission;
+
+import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
+import com.liferay.portal.kernel.service.PortletLocalService;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.ProxyUtil;
+import com.liferay.portal.kernel.xml.UnsecureSAXReaderUtil;
+import com.liferay.portal.model.impl.PortletImpl;
+import com.liferay.portal.xml.SAXReaderImpl;
+import com.liferay.registry.BasicRegistryImpl;
+import com.liferay.registry.RegistryUtil;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Michael Bowerman
+ */
+public class ResourceActionsTest {
+
+	@Before
+	public void setUp() {
+		RegistryUtil.setRegistry(new BasicRegistryImpl());
+
+		UnsecureSAXReaderUtil unsecureSAXReaderUtil =
+			new UnsecureSAXReaderUtil();
+
+		unsecureSAXReaderUtil.setSAXReader(new SAXReaderImpl());
+
+		ResourceActionsUtil resourceActionsUtil = new ResourceActionsUtil();
+
+		ResourceActionsImpl resourceActionsImpl = new ResourceActionsImpl();
+
+		ReflectionTestUtil.setFieldValue(
+			resourceActionsImpl, "portletLocalService",
+			ProxyUtil.newProxyInstance(
+				_classLoader, new Class<?>[] {PortletLocalService.class},
+				new InvocationHandler() {
+
+					@Override
+					public Object invoke(
+							Object proxy, Method method, Object[] args)
+						throws Throwable {
+
+						return new PortletImpl(_COMPANY_ID, (String)args[0]);
+					}
+
+				}));
+
+		resourceActionsImpl.afterPropertiesSet();
+
+		resourceActionsUtil.setResourceActions(resourceActionsImpl);
+	}
+
+	@Test
+	public void testModelGetsRemovedWhenRemovingAllReferencedPortlets()
+		throws Exception {
+
+		ResourceActionsUtil.read(
+			null, _classLoader, _SOURCE_PATH + "default.xml");
+
+		List<String> portletNames = ResourceActionsUtil.getPortletNames();
+
+		Assert.assertTrue(portletNames.contains(_PORTLET_NAME));
+		Assert.assertTrue(portletNames.contains(_PORTLET_NAME_2));
+
+		List<String> modelNames = ResourceActionsUtil.getModelNames();
+
+		Assert.assertTrue(modelNames.contains(_MODEL_NAME));
+
+		ResourceActionsUtil.removePortletResource(_PORTLET_NAME);
+
+		portletNames = ResourceActionsUtil.getPortletNames();
+
+		Assert.assertFalse(portletNames.contains(_PORTLET_NAME));
+		Assert.assertTrue(portletNames.contains(_PORTLET_NAME_2));
+
+		modelNames = ResourceActionsUtil.getModelNames();
+
+		Assert.assertTrue(modelNames.contains(_MODEL_NAME));
+
+		ResourceActionsUtil.removePortletResource(_PORTLET_NAME_2);
+
+		portletNames = ResourceActionsUtil.getPortletNames();
+
+		Assert.assertFalse(portletNames.contains(_PORTLET_NAME));
+		Assert.assertFalse(portletNames.contains(_PORTLET_NAME_2));
+
+		modelNames = ResourceActionsUtil.getModelNames();
+
+		Assert.assertFalse(modelNames.contains(_MODEL_NAME));
+	}
+
+	private static final long _COMPANY_ID = RandomTestUtil.randomLong();
+
+	private static final String _MODEL_NAME =
+		"com.liferay.test.portlet.TestModel";
+
+	private static final String _PORTLET_NAME =
+		"com_liferay_test_portlet_TestPortlet";
+
+	private static final String _PORTLET_NAME_2 =
+		"com_liferay_test_portlet_TestPortlet2";
+
+	private static final String _SOURCE_PATH =
+		"com/liferay/portal/security/permission/dependencies/";
+
+	private static final ClassLoader _classLoader =
+		ResourceActionsTest.class.getClassLoader();
+
+}

liferay/liferay-central-test-2

portal-impl/test/unit/com/liferay/portal/security/permission/ResourceActionsTest.java
@@ -1,0 +1,132 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.permission;
+
+import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
+import com.liferay.portal.kernel.service.PortletLocalService;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.ProxyUtil;
+import com.liferay.portal.kernel.xml.UnsecureSAXReaderUtil;
+import com.liferay.portal.model.impl.PortletImpl;
+import com.liferay.portal.xml.SAXReaderImpl;
+import com.liferay.registry.BasicRegistryImpl;
+import com.liferay.registry.RegistryUtil;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Michael Bowerman
+ */
+public class ResourceActionsTest {
+
+	@Before
+	public void setUp() {
+		RegistryUtil.setRegistry(new BasicRegistryImpl());
+
+		UnsecureSAXReaderUtil unsecureSAXReaderUtil =
+			new UnsecureSAXReaderUtil();
+
+		unsecureSAXReaderUtil.setSAXReader(new SAXReaderImpl());
+
+		ResourceActionsUtil resourceActionsUtil = new ResourceActionsUtil();
+
+		ResourceActionsImpl resourceActionsImpl = new ResourceActionsImpl();
+
+		ReflectionTestUtil.setFieldValue(
+			resourceActionsImpl, "portletLocalService",
+			ProxyUtil.newProxyInstance(
+				_classLoader, new Class<?>[] {PortletLocalService.class},
+				new InvocationHandler() {
+
+					@Override
+					public Object invoke(
+							Object proxy, Method method, Object[] args)
+						throws Throwable {
+
+						return new PortletImpl(_COMPANY_ID, (String)args[0]);
+					}
+
+				}));
+
+		resourceActionsImpl.afterPropertiesSet();
+
+		resourceActionsUtil.setResourceActions(resourceActionsImpl);
+	}
+
+	@Test
+	public void testModelGetsRemovedWhenRemovingAllReferencedPortlets()
+		throws Exception {
+
+		ResourceActionsUtil.read(
+			null, _classLoader, _SOURCE_PATH + "default.xml");
+
+		List<String> portletNames = ResourceActionsUtil.getPortletNames();
+
+		Assert.assertTrue(portletNames.contains(_PORTLET_NAME_1));
+		Assert.assertTrue(portletNames.contains(_PORTLET_NAME_2));
+
+		List<String> modelNames = ResourceActionsUtil.getModelNames();
+
+		Assert.assertTrue(modelNames.contains(_MODEL_NAME));
+
+		ResourceActionsUtil.removePortletResource(_PORTLET_NAME_1);
+
+		portletNames = ResourceActionsUtil.getPortletNames();
+
+		Assert.assertFalse(portletNames.contains(_PORTLET_NAME_1));
+		Assert.assertTrue(portletNames.contains(_PORTLET_NAME_2));
+
+		modelNames = ResourceActionsUtil.getModelNames();
+
+		Assert.assertTrue(modelNames.contains(_MODEL_NAME));
+
+		ResourceActionsUtil.removePortletResource(_PORTLET_NAME_2);
+
+		portletNames = ResourceActionsUtil.getPortletNames();
+
+		Assert.assertFalse(portletNames.contains(_PORTLET_NAME_1));
+		Assert.assertFalse(portletNames.contains(_PORTLET_NAME_2));
+
+		modelNames = ResourceActionsUtil.getModelNames();
+
+		Assert.assertFalse(modelNames.contains(_MODEL_NAME));
+	}
+
+	private static final long _COMPANY_ID = RandomTestUtil.randomLong();
+
+	private static final String _MODEL_NAME =
+		"com.liferay.test.portlet.TestModel";
+
+	private static final String _PORTLET_NAME_1 =
+		"com_liferay_test_portlet_TestPortlet1";
+
+	private static final String _PORTLET_NAME_2 =
+		"com_liferay_test_portlet_TestPortlet2";
+
+	private static final String _SOURCE_PATH =
+		"com/liferay/portal/security/permission/dependencies/";
+
+	private static final ClassLoader _classLoader =
+		ResourceActionsTest.class.getClassLoader();
+
+}