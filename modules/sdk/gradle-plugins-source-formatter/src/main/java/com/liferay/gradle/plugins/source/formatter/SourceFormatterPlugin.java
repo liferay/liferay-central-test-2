c7ac2b173f75ff0006b9cfcd4f31f10298241fb2
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-05-03 13:43:21 -0700

    LPS-67573 Use Gradle built-in method

--- modules/sdk/gradle-plugins-source-formatter/src/main/java/com/liferay/gradle/plugins/source/formatter/SourceFormatterPlugin.java
+++ modules/sdk/gradle-plugins-source-formatter/src/main/java/com/liferay/gradle/plugins/source/formatter/SourceFormatterPlugin.java
@@ -21,6 +21,7 @@ import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.DependencySet;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.tasks.TaskContainer;
 import org.gradle.language.base.plugins.LifecycleBasePlugin;
@@ -55,20 +56,19 @@ public class SourceFormatterPlugin implements Plugin<Project> {
 		Configuration configuration = GradleUtil.addConfiguration(
 			project, CONFIGURATION_NAME);
 
-		configuration.setDescription(
-			"Configures Liferay Source Formatter for this project.");
-		configuration.setVisible(false);
-
-		GradleUtil.executeIfEmpty(
-			configuration,
-			new Action<Configuration>() {
+		configuration.defaultDependencies(
+			new Action<DependencySet>() {
 
 				@Override
-				public void execute(Configuration configuration) {
+				public void execute(DependencySet dependencySet) {
 					_addDependenciesSourceFormatter(project);
 				}
 
 			});
+
+		configuration.setDescription(
+			"Configures Liferay Source Formatter for this project.");
+		configuration.setVisible(false);
 
 		return configuration;
 	}
