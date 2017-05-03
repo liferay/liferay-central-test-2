fecdeadd7c6b99df850a6feb577b1406694d80d8
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-05-03 09:24:43 +0200

    LPS-72213 SF

--- modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -3701,11 +3701,9 @@ public class JournalArticleLocalServiceImpl
 		JournalArticle article = journalArticlePersistence.findByG_A_V(
 			groupId, articleId, version);
 
-		String defaultLanguageId = article.getDefaultLanguageId();
-
-		if (languageId.equals(defaultLanguageId)) {
+		if (Objects.equals(languageId, article.getDefaultLanguageId())) {
 			throw new RequiredArticleLocalizationException(
-				"The default article localization cannot be removed.");
+				"Default article localization cannot be removed.");
 		}
 
 		String title = article.getTitle();
