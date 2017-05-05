6e83f6f85f057bcc191f5ca8bed1a785e50f65e2
lily.chi <lily.chi@liferay.com>
2017-05-05 19:15:03 +0800

    Revert "LPS-72343 portal-kernel: Invoke FacetPostProcessor when documents are filtered out of search results"
    This reverts commit 05217d5550f7fc03780f9ce97c3a8ba7201bbd8c.

--- portal-kernel/src/com/liferay/portal/kernel/search/DefaultSearchResultPermissionFilter.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/DefaultSearchResultPermissionFilter.java
@@ -16,16 +16,12 @@ package com.liferay.portal.kernel.search;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.search.facet.Facet;
-import com.liferay.portal.kernel.search.facet.FacetPostProcessor;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
-import com.liferay.registry.collections.ServiceTrackerCollections;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,7 +42,7 @@ public class DefaultSearchResultPermissionFilter
 	@Override
 	protected void filterHits(Hits hits, SearchContext searchContext) {
 		List<Document> docs = new ArrayList<>();
-		List<Document> excludeDocs = new ArrayList<>();
+		int excludeDocsSize = 0;
 		List<Float> scores = new ArrayList<>();
 
 		boolean companyAdmin = _permissionChecker.isCompanyAdmin(
@@ -66,15 +62,7 @@ public class DefaultSearchResultPermissionFilter
 				scores.add(hits.score(i));
 			}
 			else {
-				excludeDocs.add(documents[i]);
-			}
-		}
-
-		for (Facet facet : ListUtil.fromMapValues(searchContext.getFacets())) {
-			for (FacetPostProcessor facetPostProcessor :
-					getFacetPostProcessors()) {
-
-				facetPostProcessor.exclude(excludeDocs, facet);
+				excludeDocsSize++;
 			}
 		}
 
@@ -83,16 +71,7 @@ public class DefaultSearchResultPermissionFilter
 		hits.setSearchTime(
 			(float)(System.currentTimeMillis() - hits.getStart()) /
 				Time.SECOND);
-		hits.setLength(hits.getLength() - excludeDocs.size());
-	}
-
-	protected List<FacetPostProcessor> getFacetPostProcessors() {
-		if (_facetPostProcessors == null) {
-			_facetPostProcessors = ServiceTrackerCollections.openList(
-				FacetPostProcessor.class);
-		}
-
-		return _facetPostProcessors;
+		hits.setLength(hits.getLength() - excludeDocsSize);
 	}
 
 	@Override
@@ -167,7 +146,6 @@ public class DefaultSearchResultPermissionFilter
 		DefaultSearchResultPermissionFilter.class);
 
 	private final BaseIndexer<?> _baseIndexer;
-	private List<FacetPostProcessor> _facetPostProcessors;
 	private final PermissionChecker _permissionChecker;
 
 }
