66e9831b659e7c594bbf94d409e5e0fcf06d8533
Brian Chan <brian.chan@liferay.com>
2017-05-05 08:52:53 -0700

    LPS-72359 were these javadocs copied from somewhere? If so, please note that in https://issues.liferay.com/browse/LRDOCS-3646

--- modules/apps/collaboration/document-library/document-library-item-selector-api/src/main/java/com/liferay/document/library/item/selector/criteria/PortletFileEntryURLItemSelectorReturnType.java
+++ modules/apps/collaboration/document-library/document-library-item-selector-api/src/main/java/com/liferay/document/library/item/selector/criteria/PortletFileEntryURLItemSelectorReturnType.java
@@ -17,8 +17,6 @@ package com.liferay.document.library.item.selector.criteria;
 import com.liferay.item.selector.ItemSelectorReturnType;
 
 /**
- * This return type should return the URL of the entity as a string.
- *
  * @author Roberto Díaz
  */
 public class PortletFileEntryURLItemSelectorReturnType
--- modules/apps/collaboration/item-selector/item-selector-criteria-api/src/main/java/com/liferay/item/selector/criteria/PortletFileEntryURLItemSelectorReturnType.java
+++ modules/apps/collaboration/item-selector/item-selector-criteria-api/src/main/java/com/liferay/item/selector/criteria/PortletFileEntryURLItemSelectorReturnType.java
@@ -17,8 +17,6 @@ package com.liferay.item.selector.criteria;
 import com.liferay.item.selector.ItemSelectorReturnType;
 
 /**
- * This return type should return the URL of the entity as a string.
- *
  * @author Roberto Díaz
  */
 public class PortletFileEntryURLItemSelectorReturnType
