a5a133d58298a0a3831d3f6717768fae8f723be1
lily.chi <lily.chi@liferay.com>
2017-05-05 19:14:34 +0800

    Revert "LPS-72343 portal-kernel: Simple key value pair in toString, for debugging and tests"
    This reverts commit bcc5daef12107f0776baabfd4e3705274fba3049.

--- portal-kernel/src/com/liferay/portal/kernel/search/facet/collector/DefaultTermCollector.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/facet/collector/DefaultTermCollector.java
@@ -14,8 +14,6 @@
 
 package com.liferay.portal.kernel.search.facet.collector;
 
-import com.liferay.portal.kernel.util.StringPool;
-
 /**
  * @author Michael C. Han
  */
@@ -34,11 +32,6 @@ public class DefaultTermCollector implements TermCollector {
 	@Override
 	public String getTerm() {
 		return _term;
-	}
-
-	@Override
-	public String toString() {
-		return _term + StringPool.EQUAL + _frequency;
 	}
 
 	private final int _frequency;
