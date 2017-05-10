bf6fba0687c9c310e2121c61627a7c105745f2da
André de Oliveira <andre.oliveira@liferay.com>
2017-05-09 18:18:40 -0700

    LPS-72199 portal-search-elasticsearch: Add PaginationTest
    https://issues.liferay.com/browse/LPS-72199

---
+++ modules/apps/foundation/portal-search/portal-search-elasticsearch/src/test/java/com/liferay/portal/search/elasticsearch/internal/pagination/PaginationTest.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.elasticsearch.internal.pagination;
+
+import com.liferay.portal.search.elasticsearch.internal.ElasticsearchIndexingFixture;
+import com.liferay.portal.search.elasticsearch.internal.connection.ElasticsearchFixture;
+import com.liferay.portal.search.elasticsearch.internal.groupby.GroupByTest;
+import com.liferay.portal.search.test.util.indexing.BaseIndexingTestCase;
+import com.liferay.portal.search.test.util.indexing.IndexingFixture;
+import com.liferay.portal.search.test.util.pagination.BasePaginationTestCase;
+
+/**
+ * @author Preston Crary
+ */
+public class PaginationTest extends BasePaginationTestCase {
+
+	@Override
+	protected IndexingFixture createIndexingFixture() {
+		return new ElasticsearchIndexingFixture(
+			new ElasticsearchFixture(GroupByTest.class.getSimpleName()),
+			BaseIndexingTestCase.COMPANY_ID);
+	}
+
+}
