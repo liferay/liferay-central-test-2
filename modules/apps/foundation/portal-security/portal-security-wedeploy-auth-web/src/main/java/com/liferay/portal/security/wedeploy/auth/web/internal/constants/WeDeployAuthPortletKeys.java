00676e1e1d03988b930d28358a028ffdf3cfe808
Brian Chan <brian.chan@liferay.com>
2017-05-09 22:06:04 -0700

    LPS-72151 Should match the class name. The reason some of them don't is because the Portlet class was originally not in internal, and then we moved it out. But since this is new, we can put in the correct package.

--- modules/apps/foundation/portal-security/portal-security-wedeploy-auth-web/src/main/java/com/liferay/portal/security/wedeploy/auth/web/internal/constants/WeDeployAuthPortletKeys.java
+++ modules/apps/foundation/portal-security/portal-security-wedeploy-auth-web/src/main/java/com/liferay/portal/security/wedeploy/auth/web/internal/constants/WeDeployAuthPortletKeys.java
@@ -20,7 +20,7 @@ package com.liferay.portal.security.wedeploy.auth.web.internal.constants;
 public class WeDeployAuthPortletKeys {
 
 	public static final String WEDEPLOY_AUTH_ADMIN =
-		"com_liferay_portal_security_wedeploy_auth_web_portlet_" +
+		"com_liferay_portal_security_wedeploy_auth_web_internal_portlet_" +
 			"WeDeployAuthAdminPortlet";
 
 }
