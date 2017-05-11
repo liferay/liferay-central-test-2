8246eb589ddbb43ef5461e9549c466927544afd1
Preston-Crary <preston.crary@liferay.com>
2017-05-11 15:26:56 -0700

    Revert "LPS-72481 startsWith will always be false if portalDomain has the port number appended"
    This reverts commit 992a55866633accb087e19d36ec00025811c2645.

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -8327,12 +8327,6 @@ public class PortalImpl implements Portal {
 					canonicalDomain, themeDisplay.getServerPort(),
 					themeDisplay.isSecure());
 
-				int pos = portalDomain.lastIndexOf(CharPool.COLON);
-
-				if (pos != -1) {
-					portalDomain = portalDomain.substring(0, pos);
-				}
-
 				if ((canonicalURL ||
 					 canonicalDomain.startsWith(portalDomain)) &&
 					!controlPanel) {
