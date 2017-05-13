62dc1b41db5d8f7bb01a3e3594c47f92e95d8ab3
Pei-Jung Lan <pei-jung.lan@liferay.com>
2017-05-12 17:57:51 -0700

    LPS-72477 Add demo

---
+++ modules/apps/foundation/users-admin/users-admin-demo/src/main/java/com/liferay/users/admin/demo/internal/UsersDemo.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.users.admin.demo.internal;
+
+import com.liferay.portal.instance.lifecycle.BasePortalInstanceLifecycleListener;
+import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.site.demo.data.creator.SiteDemoDataCreator;
+import com.liferay.users.admin.demo.data.creator.SiteAdminUserDemoDataCreator;
+import com.liferay.users.admin.demo.data.creator.VirtualInstanceAdminUserDemoDataCreator;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pei-Jung Lan
+ */
+@Component(immediate = true, service = PortalInstanceLifecycleListener.class)
+public class UsersDemo extends BasePortalInstanceLifecycleListener {
+
+	@Override
+	public void portalInstanceRegistered(Company company) throws Exception {
+		Group acmeCorpGroup = _siteDemoDataCreator.create(
+			company.getCompanyId(), "Acme’s Corporation");
+
+		_siteAdminUserDemoDataCreator.create(
+			acmeCorpGroup.getGroupId(), "helen@liferay.com");
+
+		_virtualInstanceAdminUserDemoDataCreator.create(
+			company.getCompanyId(), "bruno@liferay.com");
+	}
+
+	@Deactivate
+	protected void deactivate() throws PortalException {
+		_virtualInstanceAdminUserDemoDataCreator.delete();
+		_siteAdminUserDemoDataCreator.delete();
+		_siteDemoDataCreator.delete();
+	}
+
+	@Reference
+	private SiteAdminUserDemoDataCreator _siteAdminUserDemoDataCreator;
+
+	@Reference
+	private SiteDemoDataCreator _siteDemoDataCreator;
+
+	@Reference
+	private VirtualInstanceAdminUserDemoDataCreator
+		_virtualInstanceAdminUserDemoDataCreator;
+
+}
