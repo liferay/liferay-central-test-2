1a95fd83108121f0cf2b8b6282fd7e34ec722795
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-05-15 08:09:29 -0700

    LPS-72644 Create Gogo Shell portlet

---
+++ modules/apps/gogo-shell/gogo-shell-web/src/main/java/com/liferay/gogo/shell/web/internal/application/list/GogoShellPanelApp.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.gogo.shell.web.internal.application.list;
+
+import com.liferay.application.list.BasePanelApp;
+import com.liferay.application.list.PanelApp;
+import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.gogo.shell.web.internal.constants.GogoShellPortletKeys;
+import com.liferay.portal.kernel.model.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Andrea Di Giorgi
+ */
+@Component(
+	immediate = true,
+	property = {
+		"panel.app.order:Integer=700",
+		"panel.category.key=" + PanelCategoryKeys.CONTROL_PANEL_CONFIGURATION
+	},
+	service = PanelApp.class
+)
+public class GogoShellPanelApp extends BasePanelApp {
+
+	@Override
+	public String getPortletId() {
+		return GogoShellPortletKeys.GOGO_SHELL;
+	}
+
+	@Override
+	@Reference(
+		target = "(javax.portlet.name=" + GogoShellPortletKeys.GOGO_SHELL + ")",
+		unbind = "-"
+	)
+	public void setPortlet(Portlet portlet) {
+		super.setPortlet(portlet);
+	}
+
+}
