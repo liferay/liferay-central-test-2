096b8022f546a220c63a8782b2d2a0751272e05b
Leonardo Barros <leonardo.barros@liferay.com>
2017-05-15 09:04:06 -0300

    LPS-71378 Fix ordering of key and value

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-data-provider-instance/src/main/java/com/liferay/dynamic/data/mapping/data/provider/instance/WorkflowDefinitionsDataProvider.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-data-provider-instance/src/main/java/com/liferay/dynamic/data/mapping/data/provider/instance/WorkflowDefinitionsDataProvider.java
@@ -69,7 +69,7 @@ public class WorkflowDefinitionsDataProvider implements DDMDataProvider {
 
 		data.add(
 			new KeyValuePair(
-				LanguageUtil.get(locale, "no-workflow"), "no-workflow"));
+				"no-workflow", LanguageUtil.get(locale, "no-workflow")));
 
 		if (!_workflowEngineManager.isDeployed()) {
 			return DDMDataProviderResponse.of(
