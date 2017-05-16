b7b55fe1e1157688b4082ca1e693f37c42e0c4ba
shuyangzhou <shuyang.zhou@liferay.com>
2017-05-16 11:03:17 -0700

    LPS-72598 Merge parent context into PortletApplicationContext, so that portlet's appended spring xmls can override parent spring xmls' beans.

--- portal-impl/src/com/liferay/portal/spring/context/PortletContextLoaderListener.java
+++ portal-impl/src/com/liferay/portal/spring/context/PortletContextLoaderListener.java
@@ -17,13 +17,11 @@ package com.liferay.portal.spring.context;
 import com.liferay.portal.bean.BeanLocatorImpl;
 import com.liferay.portal.kernel.bean.BeanLocator;
 import com.liferay.portal.kernel.bean.PortletBeanLocatorUtil;
-import com.liferay.portal.kernel.configuration.ConfigurationFactoryUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.PortletClassLoaderUtil;
 import com.liferay.portal.kernel.util.MethodCache;
 import com.liferay.portal.spring.bean.BeanReferenceRefreshUtil;
-import com.liferay.portal.util.PropsValues;
 
 import java.lang.reflect.Method;
 
@@ -31,8 +29,6 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.context.ConfigurableWebApplicationContext;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
@@ -71,20 +67,6 @@ public class PortletContextLoaderListener extends ContextLoaderListener {
 		}
 
 		super.contextDestroyed(servletContextEvent);
-
-		Object parentApplicationContext = servletContext.getAttribute(
-			_PARENT_APPLICATION_CONTEXT_KEY);
-
-		if (parentApplicationContext instanceof
-				ConfigurableApplicationContext) {
-
-			servletContext.removeAttribute(_PARENT_APPLICATION_CONTEXT_KEY);
-
-			ConfigurableApplicationContext configurableApplicationContext =
-				(ConfigurableApplicationContext)parentApplicationContext;
-
-			configurableApplicationContext.close();
-		}
 	}
 
 	@Override
@@ -174,27 +156,8 @@ public class PortletContextLoaderListener extends ContextLoaderListener {
 	protected ApplicationContext loadParentContext(
 		ServletContext servletContext) {
 
-		try {
-			ConfigurationFactoryUtil.getConfiguration(
-				PortletClassLoaderUtil.getClassLoader(), "service");
-		}
-		catch (Exception e) {
-			return null;
-		}
-
-		ApplicationContext applicationContext =
-			new ClassPathXmlApplicationContext(
-				PropsValues.SPRING_PORTLET_CONFIGS, true);
-
-		servletContext.setAttribute(
-			_PARENT_APPLICATION_CONTEXT_KEY, applicationContext);
-
-		return applicationContext;
+		return super.loadParentContext(servletContext);
 	}
-
-	private static final String _PARENT_APPLICATION_CONTEXT_KEY =
-		PortletContextLoaderListener.class.getName() +
-			"#parentApplicationContext";
 
 	private static final String _PORTAL_CONFIG_LOCATION_PARAM =
 		"portalContextConfigLocation";
