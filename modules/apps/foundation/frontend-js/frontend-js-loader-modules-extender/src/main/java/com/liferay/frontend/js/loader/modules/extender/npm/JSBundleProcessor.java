c7c2e7252086a2cfb7ec4f24aa59996dc6a2433b
Iván Zaera Avellón <ivan.zaera@liferay.com>
2017-05-17 11:22:48 +0200

    LPS-72526 Define an OSGi component interface to process bundles in search of NPM packages

---
+++ modules/apps/foundation/frontend-js/frontend-js-loader-modules-extender/src/main/java/com/liferay/frontend/js/loader/modules/extender/npm/JSBundleProcessor.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.js.loader.modules.extender.npm;
+
+import org.osgi.framework.Bundle;
+
+/**
+ * A service that can process an OSGi bundle to extract NPM packages from its
+ * structure.
+ * @author Iván Zaera
+ */
+public interface JSBundleProcessor {
+
+	public JSBundle process(Bundle bundle);
+
+}
