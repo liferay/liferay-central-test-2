ed48efa89a80982edab6e545729ff698aabae098
Tibor Lipusz <tibor.lipusz@liferay.com>
2017-05-17 14:12:42 +0200

    LPS-70724 Revert "LPS-65978 Reindex object when its ratings are modified"
    This reverts commit 410ee801f5e8e561df8973f073a28d87a4d47c45.

--- portal-impl/src/com/liferay/portlet/ratings/service/impl/RatingsEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/ratings/service/impl/RatingsEntryLocalServiceImpl.java
@@ -22,12 +22,9 @@ import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.search.Indexer;
-import com.liferay.portal.kernel.search.IndexerRegistryUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.social.SocialActivityManagerUtil;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
-import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portlet.ratings.service.base.RatingsEntryLocalServiceBaseImpl;
 import com.liferay.ratings.kernel.exception.EntryScoreException;
 import com.liferay.ratings.kernel.model.RatingsEntry;
@@ -92,8 +89,6 @@ public class RatingsEntryLocalServiceImpl
 		stats.setAverageScore(averageScore);
 
 		ratingsStatsPersistence.update(stats);
-
-		reindex(stats);
 	}
 
 	@Override
@@ -184,8 +179,6 @@ public class RatingsEntryLocalServiceImpl
 				stats.getTotalScore() / stats.getTotalEntries());
 
 			ratingsStatsPersistence.update(stats);
-
-			reindex(stats);
 		}
 		else {
 			newEntry = true;
@@ -216,8 +209,6 @@ public class RatingsEntryLocalServiceImpl
 				stats.getTotalScore() / stats.getTotalEntries());
 
 			ratingsStatsPersistence.update(stats);
-
-			reindex(stats);
 		}
 
 		// Blogs entry
@@ -268,14 +259,6 @@ public class RatingsEntryLocalServiceImpl
 		}
 
 		return entry;
-	}
-
-	protected void reindex(RatingsStats stats) throws PortalException {
-		String className = PortalUtil.getClassName(stats.getClassNameId());
-
-		Indexer<?> indexer = IndexerRegistryUtil.nullSafeGetIndexer(className);
-
-		indexer.reindex(className, stats.getClassPK());
 	}
 
 	protected void validate(double score) throws PortalException {
