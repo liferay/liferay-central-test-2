1c8db8c6a1a048e1728c4085f1055ad5463f6a75
Iván Zaera Avellón <ivan.zaera@liferay.com>
2017-05-17 11:21:48 +0200

    LPS-72526 Define a helper to deal with NPM module names

---
+++ modules/apps/foundation/frontend-js/frontend-js-loader-modules-extender/src/main/java/com/liferay/frontend/js/loader/modules/extender/npm/ModuleNameUtil.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.js.loader.modules.extender.npm;
+
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+
+/**
+ * @author Iván Zaera
+ */
+public class ModuleNameUtil {
+
+	public static String getModuleId(JSPackage jsPackage, String moduleName) {
+		StringBundler sb = new StringBundler();
+
+		sb.append(jsPackage.getId());
+		sb.append(StringPool.SLASH);
+		sb.append(moduleName);
+
+		return sb.toString();
+	}
+
+	public static String getPackageName(String moduleName) {
+		int i = moduleName.indexOf(StringPool.SLASH);
+
+		if (i == -1) {
+			i = moduleName.length();
+		}
+
+		return moduleName.substring(0, i);
+	}
+
+	public static String getPackagePath(String moduleName) {
+		int i = moduleName.indexOf(StringPool.SLASH);
+
+		if (i == -1) {
+			return null;
+		}
+
+		return moduleName.substring(i + 1);
+	}
+
+	public static String toFileName(String moduleName) {
+		return moduleName + ".js";
+	}
+
+	public static String toModuleName(String fileName) {
+		return fileName.substring(0, fileName.length() - 3);
+	}
+
+}
