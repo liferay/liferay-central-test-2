cfcc7fd81ea604b750434d5fa3ad32a3f8798a03
Brian Chan <brian.chan@liferay.com>
2017-05-17 10:21:33 -0700

    LPS-72463 SF

--- modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -5338,7 +5338,7 @@ public class JournalArticleLocalServiceImpl
 			smallImage, article.getSmallImageId(), smallImageFile,
 			smallImageBytes);
 
-		// Email and Workflow
+		// Email and workflow
 
 		if (expired && imported) {
 			updateStatus(
