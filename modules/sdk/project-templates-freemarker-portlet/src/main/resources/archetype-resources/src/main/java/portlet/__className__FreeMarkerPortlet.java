dad9e66c4eee73dbf66dc369671ed8ee280fda3b
dnebing <david.nebinger@liferay.com>
2017-05-18 16:15:33 -0400

    LPS-72650 Add the FreeMarker portlet project template

---
+++ modules/sdk/project-templates-freemarker-portlet/src/main/resources/archetype-resources/src/main/java/portlet/__className__FreeMarkerPortlet.java
@@ -0,0 +1,31 @@
+package ${package}.portlet;
+
+import ${package}.constants.${className}PortletKeys;
+
+import com.liferay.util.bridges.freemarker.FreeMarkerPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author ${author}
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.css-class-wrapper=portlet-freemarker",
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=${artifactId} Portlet",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/templates/view.ftl",
+		"javax.portlet.name=" + ${className}PortletKeys.${className},
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class ${className}FreeMarkerPortlet extends FreeMarkerPortlet {
+}
