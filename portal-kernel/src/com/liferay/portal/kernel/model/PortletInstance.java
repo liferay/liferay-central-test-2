a5be202c130f1efa623ec1e4fe0ff0b25a1c1bdd
Tina Tian <tina.tian@liferay.com>
2017-05-18 16:57:03 -0700

    LPS-72505 Move real logic to PortletIdCodec

--- portal-kernel/src/com/liferay/portal/kernel/model/PortletInstance.java
+++ portal-kernel/src/com/liferay/portal/kernel/model/PortletInstance.java
@@ -14,15 +14,9 @@
 
 package com.liferay.portal.kernel.model;
 
-import com.liferay.portal.kernel.util.CharPool;
-import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.portlet.PortletIdCodec;
 import com.liferay.portal.kernel.util.ObjectValuePair;
-import com.liferay.portal.kernel.util.StringBundler;
-import com.liferay.portal.kernel.util.StringPool;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
-
-import java.security.InvalidParameterException;
 
 /**
  * @author Brian Wing Shun Chan
@@ -31,8 +25,7 @@ import java.security.InvalidParameterException;
 public class PortletInstance {
 
 	public static final int PORTLET_INSTANCE_KEY_MAX_LENGTH =
-		255 - PortletInstance._INSTANCE_SEPARATOR.length() +
-			PortletInstance._USER_SEPARATOR.length() + 39;
+		PortletIdCodec.PORTLET_INSTANCE_KEY_MAX_LENGTH;
 
 	public static PortletInstance fromPortletInstanceKey(
 		String portletInstanceKey) {
@@ -53,7 +46,7 @@ public class PortletInstance {
 	}
 
 	public PortletInstance(String portletName) {
-		this(portletName, StringUtil.randomString(12));
+		this(portletName, PortletIdCodec.generateInstanceId());
 	}
 
 	public PortletInstance(String portletName, long userId) {
@@ -77,21 +70,7 @@ public class PortletInstance {
 	}
 
 	public String getPortletInstanceKey() {
-		StringBundler sb = new StringBundler(5);
-
-		sb.append(_portletName);
-
-		if (_userId > 0) {
-			sb.append(_USER_SEPARATOR);
-			sb.append(_userId);
-		}
-
-		if (Validator.isNotNull(_instanceId)) {
-			sb.append(_INSTANCE_SEPARATOR);
-			sb.append(_instanceId);
-		}
-
-		return sb.toString();
+		return PortletIdCodec.encode(_portletName, _userId, _instanceId);
 	}
 
 	public String getPortletName() {
@@ -103,22 +82,7 @@ public class PortletInstance {
 	}
 
 	public String getUserIdAndInstanceId() {
-		if ((_userId <= 0) && Validator.isBlank(_instanceId)) {
-			return null;
-		}
-
-		StringBundler sb = new StringBundler(3);
-
-		if (_userId > 0) {
-			sb.append(_userId);
-			sb.append(StringPool.UNDERLINE);
-		}
-
-		if (_instanceId != null) {
-			sb.append(_instanceId);
-		}
-
-		return sb.toString();
+		return PortletIdCodec.encodeUserIdAndInstanceId(_userId, _instanceId);
 	}
 
 	public boolean hasIdenticalPortletName(PortletInstance portletInstance) {
@@ -126,7 +90,8 @@ public class PortletInstance {
 	}
 
 	public boolean hasIdenticalPortletName(String portletName) {
-		return _portletName.equals(portletName);
+		return PortletIdCodec.hasIdenticalPortletName(
+			_portletName, portletName);
 	}
 
 	public boolean hasInstanceId() {
@@ -149,112 +114,24 @@ public class PortletInstance {
 	private static ObjectValuePair<Long, String>
 		_decodeUserIdAndInstanceId(String userIdAndInstanceId) {
 
-		if (userIdAndInstanceId == null) {
-			throw new InvalidParameterException(
-				"User ID and instance ID are null");
-		}
-
-		if (userIdAndInstanceId.isEmpty()) {
-			return new ObjectValuePair<>(0L, null);
-		}
-
-		int slashCount = StringUtil.count(userIdAndInstanceId, CharPool.SLASH);
-
-		if (slashCount > 0) {
-			throw new InvalidParameterException(
-				"User ID and instance ID contain slashes");
-		}
-
-		int underlineCount = StringUtil.count(
-			userIdAndInstanceId, CharPool.UNDERLINE);
-
-		if (underlineCount > 1) {
-			throw new InvalidParameterException(
-				"User ID and instance ID has more than one underscore");
-		}
-
-		if (underlineCount == 1) {
-			int index = userIdAndInstanceId.indexOf(CharPool.UNDERLINE);
-
-			long userId = GetterUtil.getLong(
-				userIdAndInstanceId.substring(0, index), -1);
-
-			if (userId == -1) {
-				throw new InvalidParameterException("User ID is not a number");
-			}
-
-			String instanceId = null;
-
-			if (index < (userIdAndInstanceId.length() - 1)) {
-				instanceId = userIdAndInstanceId.substring(index + 1);
-			}
-
-			return new ObjectValuePair<>(userId, instanceId);
-		}
-
-		return new ObjectValuePair<>(0L, userIdAndInstanceId);
+		return PortletIdCodec.decodeUserIdAndInstanceId(userIdAndInstanceId);
 	}
 
 	private static String _getInstanceId(String portletInstanceKey) {
-		int index = portletInstanceKey.indexOf(_INSTANCE_SEPARATOR);
-
-		if (index == -1) {
-			return null;
-		}
-
-		return portletInstanceKey.substring(
-			index + _INSTANCE_SEPARATOR.length());
+		return PortletIdCodec.decodeInstanceId(portletInstanceKey);
 	}
 
 	private static String _getPortletName(String portletInstanceKey) {
-		int x = portletInstanceKey.indexOf(_USER_SEPARATOR);
-		int y = portletInstanceKey.indexOf(_INSTANCE_SEPARATOR);
-
-		if ((x == -1) && (y == -1)) {
-			return portletInstanceKey;
-		}
-		else if (x != -1) {
-			return portletInstanceKey.substring(0, x);
-		}
-
-		return portletInstanceKey.substring(0, y);
+		return PortletIdCodec.decodePortletName(portletInstanceKey);
 	}
 
 	private static long _getUserId(String portletInstanceKey) {
-		int x = portletInstanceKey.indexOf(_USER_SEPARATOR);
-		int y = portletInstanceKey.indexOf(_INSTANCE_SEPARATOR);
-
-		if (x == -1) {
-			return 0;
-		}
-
-		if (y != -1) {
-			return GetterUtil.getLong(
-				portletInstanceKey.substring(x + _USER_SEPARATOR.length(), y));
-		}
-
-		return GetterUtil.getLong(
-			portletInstanceKey.substring(x + _USER_SEPARATOR.length()));
+		return PortletIdCodec.decodeUserId(portletInstanceKey);
 	}
 
 	private void _validatePortletName(String portletName) {
-		for (String keyword : _PORTLET_NAME_KEYWORDS) {
-			if (!portletName.contains(keyword)) {
-				continue;
-			}
-
-			throw new InvalidParameterException(
-				"The portletName '" + portletName +
-					"' must not contain the keyword " + keyword);
-		}
+		PortletIdCodec.validatePortletName(portletName);
 	}
-
-	private static final String _INSTANCE_SEPARATOR = "_INSTANCE_";
-
-	private static final String[] _PORTLET_NAME_KEYWORDS =
-		{PortletInstance._INSTANCE_SEPARATOR, PortletInstance._USER_SEPARATOR};
-
-	private static final String _USER_SEPARATOR = "_USER_";
 
 	private final String _instanceId;
 	private final String _portletName;
---
+++ portal-kernel/src/com/liferay/portal/kernel/portlet/PortletIdCodec.java
@@ -0,0 +1,217 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.portal.kernel.portlet;
+
+import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ObjectValuePair;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.security.InvalidParameterException;
+
+/**
+ * @author Tina Tian
+ */
+public class PortletIdCodec {
+
+	public static final int PORTLET_INSTANCE_KEY_MAX_LENGTH =
+		255 - PortletIdCodec._INSTANCE_SEPARATOR.length() +
+			PortletIdCodec._USER_SEPARATOR.length() + 39;
+
+	public static String decodeInstanceId(String portletId) {
+		int index = portletId.indexOf(_INSTANCE_SEPARATOR);
+
+		if (index == -1) {
+			return null;
+		}
+
+		return portletId.substring(index + _INSTANCE_SEPARATOR.length());
+	}
+
+	public static String decodePortletName(String portletId) {
+		int x = portletId.indexOf(_USER_SEPARATOR);
+		int y = portletId.indexOf(_INSTANCE_SEPARATOR);
+
+		if ((x == -1) && (y == -1)) {
+			return portletId;
+		}
+		else if (x != -1) {
+			return portletId.substring(0, x);
+		}
+
+		return portletId.substring(0, y);
+	}
+
+	public static long decodeUserId(String portletId) {
+		int x = portletId.indexOf(_USER_SEPARATOR);
+		int y = portletId.indexOf(_INSTANCE_SEPARATOR);
+
+		if (x == -1) {
+			return 0;
+		}
+
+		if (y != -1) {
+			return GetterUtil.getLong(
+				portletId.substring(x + _USER_SEPARATOR.length(), y));
+		}
+
+		return GetterUtil.getLong(
+			portletId.substring(x + _USER_SEPARATOR.length()));
+	}
+
+	public static ObjectValuePair<Long, String>
+		decodeUserIdAndInstanceId(String userIdAndInstanceId) {
+
+		if (userIdAndInstanceId == null) {
+			throw new InvalidParameterException(
+				"User ID and instance ID are null");
+		}
+
+		if (userIdAndInstanceId.isEmpty()) {
+			return new ObjectValuePair<>(0L, null);
+		}
+
+		int slashCount = StringUtil.count(userIdAndInstanceId, CharPool.SLASH);
+
+		if (slashCount > 0) {
+			throw new InvalidParameterException(
+				"User ID and instance ID contain slashes");
+		}
+
+		int underlineCount = StringUtil.count(
+			userIdAndInstanceId, CharPool.UNDERLINE);
+
+		if (underlineCount > 1) {
+			throw new InvalidParameterException(
+				"User ID and instance ID has more than one underscore");
+		}
+
+		if (underlineCount == 1) {
+			int index = userIdAndInstanceId.indexOf(CharPool.UNDERLINE);
+
+			long userId = GetterUtil.getLong(
+				userIdAndInstanceId.substring(0, index), -1);
+
+			if (userId == -1) {
+				throw new InvalidParameterException("User ID is not a number");
+			}
+
+			String instanceId = null;
+
+			if (index < (userIdAndInstanceId.length() - 1)) {
+				instanceId = userIdAndInstanceId.substring(index + 1);
+			}
+
+			return new ObjectValuePair<>(userId, instanceId);
+		}
+
+		return new ObjectValuePair<>(0L, userIdAndInstanceId);
+	}
+
+	public static String encode(String portletName) {
+		return encode(portletName, 0, StringUtil.randomString(12));
+	}
+
+	public static String encode(String portletName, long userId) {
+		return encode(portletName, userId, null);
+	}
+
+	public static String encode(
+		String portletName, long userId, String instanceId) {
+
+		StringBundler sb = new StringBundler(5);
+
+		sb.append(portletName);
+
+		if (userId > 0) {
+			sb.append(_USER_SEPARATOR);
+			sb.append(userId);
+		}
+
+		if (Validator.isNotNull(instanceId)) {
+			sb.append(_INSTANCE_SEPARATOR);
+			sb.append(instanceId);
+		}
+
+		return sb.toString();
+	}
+
+	public static String encode(String portletName, String instanceId) {
+		return encode(portletName, 0, instanceId);
+	}
+
+	public static String encodeUserIdAndInstanceId(
+		long userId, String instanceId) {
+
+		if ((userId <= 0) && Validator.isBlank(instanceId)) {
+			return null;
+		}
+
+		StringBundler sb = new StringBundler(3);
+
+		if (userId > 0) {
+			sb.append(userId);
+			sb.append(StringPool.UNDERLINE);
+		}
+
+		if (instanceId != null) {
+			sb.append(instanceId);
+		}
+
+		return sb.toString();
+	}
+
+	public static String generateInstanceId() {
+		return StringUtil.randomString(12);
+	}
+
+	public static boolean hasIdenticalPortletName(
+		String portletName1, String portletName2) {
+
+		return portletName1.equals(portletName2);
+	}
+
+	public static boolean hasInstanceId(String portletId) {
+		return portletId.contains(_INSTANCE_SEPARATOR);
+	}
+
+	public static boolean hasUserId(String portletId) {
+		return portletId.contains(_USER_SEPARATOR);
+	}
+
+	public static void validatePortletName(String portletName) {
+		String keyword = null;
+
+		if (portletName.contains(_INSTANCE_SEPARATOR)) {
+			keyword = _INSTANCE_SEPARATOR;
+		}
+		else if (portletName.contains(_USER_SEPARATOR)) {
+			keyword = _USER_SEPARATOR;
+		}
+
+		if (keyword != null) {
+			throw new InvalidParameterException(
+				"The portletName '" + portletName +
+					"' must not contain the keyword " + keyword);
+		}
+	}
+
+	private static final String _INSTANCE_SEPARATOR = "_INSTANCE_";
+
+	private static final String _USER_SEPARATOR = "_USER_";
+
+}
