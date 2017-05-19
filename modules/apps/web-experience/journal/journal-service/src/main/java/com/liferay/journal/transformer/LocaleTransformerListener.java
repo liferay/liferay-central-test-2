4875d1808568087e5cfdd924e2290479727cfecb
Brian Chan <brian.chan@liferay.com>
2017-05-19 14:56:23 -0700

    LPS-72638 SF

--- modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/transformer/LocaleTransformerListener.java
+++ modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/transformer/LocaleTransformerListener.java
@@ -189,11 +189,11 @@ public class LocaleTransformerListener extends BaseTransformerListener {
 			filterByLocalizability(
 				rootElement, articleDefaultLanguageId, ddmStructure);
 		}
-		catch (PortalException pe) {
-			_log.error("Error filtering by localizability", pe);
-		}
 		catch (NullPointerException npe) {
-			_log.error("Error filtering by localizability", npe);
+			_log.error("Unable to filter by localizability", npe);
+		}
+		catch (PortalException pe) {
+			_log.error("Unable to filter by localizability", pe);
 		}
 	}
 
