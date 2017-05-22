822d27aff36335a98d9567c4c7b70b4f861b7770
Roberto Díaz Méndez <robertodimen@gmail.com>
2017-05-22 15:17:44 +0200

    LPS-72612 Avoid conditional methods in tests to improve readability and make tests clearer.

--- modules/apps/collaboration/wiki/wiki-test/src/testIntegration/java/com/liferay/wiki/service/test/WikiPageLocalServiceTest.java
+++ modules/apps/collaboration/wiki/wiki-test/src/testIntegration/java/com/liferay/wiki/service/test/WikiPageLocalServiceTest.java
@@ -366,7 +366,22 @@ public class WikiPageLocalServiceTest {
 
 	@Test
 	public void testRenamePage() throws Exception {
-		testRenamePage(false);
+		WikiPage page = WikiTestUtil.addPage(
+			_group.getGroupId(), _node.getNodeId(), true);
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId());
+
+		WikiPageLocalServiceUtil.renamePage(
+			TestPropsValues.getUserId(), _node.getNodeId(), page.getTitle(),
+			"New Title", true, serviceContext);
+
+		WikiPage renamedPage = WikiPageLocalServiceUtil.getPage(
+			_node.getNodeId(), "New Title");
+
+		Assert.assertNotNull(renamedPage);
+
+		checkPopulatedServiceContext(serviceContext, renamedPage, false);
 	}
 
 	@Test(expected = DuplicatePageException.class)
@@ -384,7 +399,25 @@ public class WikiPageLocalServiceTest {
 
 	@Test
 	public void testRenamePageWithExpando() throws Exception {
-		testRenamePage(true);
+		WikiPage page = WikiTestUtil.addPage(
+			_group.getGroupId(), _node.getNodeId(), true);
+
+		addExpandoValueToPage(page);
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId());
+
+		WikiPageLocalServiceUtil.renamePage(
+			TestPropsValues.getUserId(), _node.getNodeId(), page.getTitle(),
+			"New Title", true, serviceContext);
+
+		WikiPage renamedPage = WikiPageLocalServiceUtil.getPage(
+			_node.getNodeId(), "New Title");
+
+		Assert.assertNotNull(renamedPage);
+
+		checkPopulatedServiceContext(
+			serviceContext, renamedPage, true);
 	}
 
 	@Test
@@ -530,30 +563,6 @@ public class WikiPageLocalServiceTest {
 
 		checkPopulatedServiceContext(
 			serviceContext, retrievedPage, hasExpandoValues);
-	}
-
-	protected void testRenamePage(boolean hasExpandoValues) throws Exception {
-		WikiPage page = WikiTestUtil.addPage(
-			_group.getGroupId(), _node.getNodeId(), true);
-
-		if (hasExpandoValues) {
-			addExpandoValueToPage(page);
-		}
-
-		ServiceContext serviceContext =
-			ServiceContextTestUtil.getServiceContext(_group.getGroupId());
-
-		WikiPageLocalServiceUtil.renamePage(
-			TestPropsValues.getUserId(), _node.getNodeId(), page.getTitle(),
-			"New Title", true, serviceContext);
-
-		WikiPage renamedPage = WikiPageLocalServiceUtil.getPage(
-			_node.getNodeId(), "New Title");
-
-		Assert.assertNotNull(renamedPage);
-
-		checkPopulatedServiceContext(
-			serviceContext, renamedPage, hasExpandoValues);
 	}
 
 	protected void testRestorePageFromTrash(boolean hasExpandoValues)
