ad18c33293a84263d66ea09f09b28e18811c0f5d
Michael Prigge <michael.prigge@liferay.com>
2017-05-23 16:35:11 -0700

    LPS-69370 Build PasswordPolicyFinder

--- portal-kernel/src/com/liferay/portal/kernel/service/persistence/PasswordPolicyFinder.java
+++ portal-kernel/src/com/liferay/portal/kernel/service/persistence/PasswordPolicyFinder.java
@@ -24,6 +24,12 @@ import aQute.bnd.annotation.ProviderType;
 public interface PasswordPolicyFinder {
 	public int countByC_N(long companyId, java.lang.String name);
 
+	public int filterCountByC_N(long companyId, java.lang.String name);
+
+	public java.util.List<com.liferay.portal.kernel.model.PasswordPolicy> filterFindByC_N(
+		long companyId, java.lang.String name, int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator<com.liferay.portal.kernel.model.PasswordPolicy> obc);
+
 	public java.util.List<com.liferay.portal.kernel.model.PasswordPolicy> findByC_N(
 		long companyId, java.lang.String name, int start, int end,
 		com.liferay.portal.kernel.util.OrderByComparator<com.liferay.portal.kernel.model.PasswordPolicy> obc);
