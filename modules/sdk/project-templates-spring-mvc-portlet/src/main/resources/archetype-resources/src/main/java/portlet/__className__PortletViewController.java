d490a42dde26a229a3bd4e28abcfc44fa94a82c5
Gregory Amerson <gregory.amerson@liferay.com>
2017-05-24 14:35:25 -0500

    LPS-72630 normalize portlet with other mvc portlet

--- modules/sdk/project-templates-spring-mvc-portlet/src/main/resources/archetype-resources/src/main/java/portlet/__className__PortletViewController.java
+++ modules/sdk/project-templates-spring-mvc-portlet/src/main/resources/archetype-resources/src/main/java/portlet/__className__PortletViewController.java
@@ -1,17 +1,11 @@
 package ${package}.portlet;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.portlet.bind.annotation.ActionMapping;
-import org.springframework.web.portlet.bind.annotation.RenderMapping;
-
-import javax.portlet.ActionRequest;
-import javax.portlet.ActionResponse;
-import javax.portlet.Portlet;
-import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
 
 /**
  * @author ${author}
@@ -30,18 +24,5 @@ public class ${className}PortletViewController {
 	@RenderMapping
 	public String view(RenderRequest request, RenderResponse response) {
 		return "view";
-	}
-
-	/**
-	 * Handles the action when the action key is <code>update</code>.
-	 *
-	 * @param  actionRequest the action request
-	 * @param  response the action response
-	 * @throws Exception if an exception occurred
-	 */
-	@ActionMapping(params = "action=update")
-	public void update(ActionRequest actionRequest, ActionResponse response)
-		throws Exception {
-		// implement update logic
 	}
 }
--- modules/sdk/project-templates-spring-mvc-portlet/src/main/resources/archetype-resources/src/main/webapp/WEB-INF/jsp/view.jsp
+++ modules/sdk/project-templates-spring-mvc-portlet/src/main/resources/archetype-resources/src/main/webapp/WEB-INF/jsp/view.jsp
@@ -1,10 +1,5 @@
 <%@ include file="/WEB-INF/jsp/init.jsp" %>
 
-<strong><liferay-ui:message key="${artifactId}.caption" /></strong>
-
-<portlet:renderURL var="updateURL">
-	<portlet:param name="action" value="update" />
-	<portlet:param name="redirect" value="<%= currentURL %>" />
-</portlet:renderURL>
-
-<aui:button href="<%= updateURL %>" value="update" />
+<p>
+	<b><liferay-ui:message key="${artifactId}.caption"/></b>
+</p>
