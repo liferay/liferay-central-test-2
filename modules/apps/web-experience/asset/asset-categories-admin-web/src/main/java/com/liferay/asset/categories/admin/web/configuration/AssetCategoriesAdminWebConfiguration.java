a960d706d96ca7a701a379e2a70403c1389d7614
Pavel Savinov <pavel.savinov@liferay.com>
2017-05-24 13:15:45 +0200

    LPS-72355 add asset categories admin web configuration to allow configure flattened tree view for asset vocabularies

---
+++ modules/apps/web-experience/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/configuration/AssetCategoriesAdminWebConfiguration.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.admin.web.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Pavel Savinov
+ */
+@ExtendedObjectClassDefinition(category = "web-experience")
+@Meta.OCD(
+	id = "com.liferay.asset.categories.admin.web.configuration.AssetCategoriesAdminWebConfiguration",
+	localization = "content/Language",
+	name = "asset.categories.admin.web.configuration.name"
+)
+public interface AssetCategoriesAdminWebConfiguration {
+
+	/**
+	 * Set this to true to enable asset categories tree should be shown as
+	 * flattened tree.
+	 *
+	 * @return <code>true</code> if dynamic export is enabled.
+	 */
+	@Meta.AD(
+		deflt = "false",
+		description = "flattened.navigation.allowed.description",
+		required = false
+	)
+	public boolean flattenedNavigationAllowed();
+
+}
