e9abff81a6ad832b9b943750a81bb3397a0474ea
Alejandro Hernández <info@alejandrohdezma.com>
2017-05-25 09:21:38 +0200

    VULCAN-3 Create page's writer

---
+++ modules/apps/foundation/vulcan/vulcan-jax-rs-writer-json/src/main/java/com/liferay/vulcan/jax/rs/writer/json/internal/PageMessageBodyWriter.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.vulcan.jax.rs.writer.json.internal;
+
+import com.liferay.vulcan.pagination.Page;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Alejandro Hernández
+ * @author Carlos Sierra Andrés
+ * @author Jorge Ferrer
+ */
+@Component(
+	immediate = true, property = "liferay.vulcan.message.body.writer=true"
+)
+@Provider
+public class PageMessageBodyWriter<T> implements MessageBodyWriter<Page<T>> {
+
+	@Override
+	public long getSize(
+		Page<T> page, Class<?> type, Type genericType, Annotation[] annotations,
+		MediaType mediaType) {
+
+		return -1;
+	}
+
+	@Override
+	public boolean isWriteable(
+		Class<?> type, Type genericType, Annotation[] annotations,
+		MediaType mediaType) {
+
+		return false;
+	}
+
+	@Override
+	public void writeTo(
+			Page<T> page, Class<?> type, Type genericType,
+			Annotation[] annotations, MediaType mediaType,
+			MultivaluedMap<String, Object> httpHeaders,
+			OutputStream entityStream)
+		throws IOException, WebApplicationException {
+	}
+
+}
