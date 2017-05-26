7d800cb13425407e534cbaf599b55e0f222b8631
IstvanD <istvan.dezsi@liferay.com>
2017-05-26 18:22:26 +0200

    LPS-72757 Server-side check whether the calendar resource is occupied

--- modules/apps/forms-and-workflow/calendar/calendar-service/src/main/java/com/liferay/calendar/service/impl/CalendarBookingLocalServiceImpl.java
+++ modules/apps/forms-and-workflow/calendar/calendar-service/src/main/java/com/liferay/calendar/service/impl/CalendarBookingLocalServiceImpl.java
@@ -210,8 +210,14 @@ public class CalendarBookingLocalServiceImpl
 				CalendarBookingWorkflowConstants.STATUS_DRAFT);
 		}
 		else {
-			calendarBooking.setStatus(
-				CalendarBookingWorkflowConstants.STATUS_MASTER_PENDING);
+			if (checkIfOccupied(calendarBooking)) {
+				calendarBooking.setStatus(
+					CalendarBookingWorkflowConstants.STATUS_DENIED);
+			}
+			else {
+				calendarBooking.setStatus(
+					CalendarBookingWorkflowConstants.STATUS_MASTER_PENDING);
+			}
 		}
 
 		calendarBooking.setStatusDate(serviceContext.getModifiedDate(now));
@@ -1691,6 +1697,35 @@ public class CalendarBookingLocalServiceImpl
 			sendNotification(
 				childCalendarBooking, notificationTemplateType, serviceContext);
 		}
+	}
+
+	protected boolean checkIfOccupied(CalendarBooking calendarBooking)
+		throws PortalException {
+
+		CalendarResource calendarResource =
+			calendarBooking.getCalendarResource();
+
+		if (calendarResource.isGroup() && calendarResource.isUser()) {
+			return false;
+		}
+
+		long startTime = calendarBooking.getStartTime();
+		long endTime = calendarBooking.getEndTime();
+
+		int[] statuses = {CalendarBookingWorkflowConstants.STATUS_PENDING};
+
+		List<CalendarBooking> calendarEvents = getCalendarBookings(
+			calendarBooking.getCalendarId(), statuses);
+
+		for (CalendarBooking calendarEvent : calendarEvents) {
+			if ((startTime < calendarEvent.getEndTime()) &&
+				(calendarEvent.getStartTime() < endTime)) {
+
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	protected String getExtraDataJSON(CalendarBooking calendarBooking) {
