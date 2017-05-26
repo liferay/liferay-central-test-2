2209833e0c475aef0de4f07645be3278debd4ad7
Daniel Kocsis <daniel.kocsis@liferay.com>
2017-05-26 11:16:45 +0200

    LPS-72643 Add new thread local to be able to disable indexing only for the current thread and not for the whole instance

---
+++ portal-kernel/src/com/liferay/portal/kernel/search/IndexingThreadLocal.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.search;
+
+import com.liferay.portal.kernel.util.AutoResetThreadLocal;
+
+/**
+ * @author Daniel Kocsis
+ */
+public class IndexingThreadLocal {
+
+	public static boolean isIndexingDisabled() {
+		return !isIndexingEnabled();
+	}
+
+	public static boolean isIndexingEnabled() {
+		return _indexingEnabled.get();
+	}
+
+	public static void setIndexingEnabled(boolean indexingEnabled) {
+		_indexingEnabled.set(indexingEnabled);
+	}
+
+	private static final ThreadLocal<Boolean> _indexingEnabled =
+		new AutoResetThreadLocal<>(
+			IndexingThreadLocal.class + "._indexingEnabled",
+			() -> Boolean.TRUE);
+
+}
