4fe459f501cf16079e9f40b71459e4f2d949b02f
Michael Han <michael.han@liferay.com>
2017-05-25 21:39:27 -0500

    LPS-72773 do not commit if in overflow mode

--- modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/buffer/DefaultIndexerRequestBufferExecutor.java
+++ modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/buffer/DefaultIndexerRequestBufferExecutor.java
@@ -79,7 +79,9 @@ public class DefaultIndexerRequestBufferExecutor
 			indexerRequestBuffer.remove(indexerRequest);
 		}
 
-		commit(searchEngineIds);
+		if (!BufferOverflowThreadLocal.isOverflowMode()) {
+			commit(searchEngineIds);
+		}
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
