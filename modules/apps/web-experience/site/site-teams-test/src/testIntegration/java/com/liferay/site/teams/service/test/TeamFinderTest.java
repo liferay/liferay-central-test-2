867cf2c486ff59351917a5e428031e6bca7935dc
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-05-30 09:10:53 +0100

    LPS-72815 Adds RunWith Arquillian annotation

--- modules/apps/web-experience/site/site-teams-test/src/testIntegration/java/com/liferay/site/teams/service/test/TeamFinderTest.java
+++ modules/apps/web-experience/site/site-teams-test/src/testIntegration/java/com/liferay/site/teams/service/test/TeamFinderTest.java
@@ -14,6 +14,7 @@
 
 package com.liferay.site.teams.service.test;
 
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Team;
 import com.liferay.portal.kernel.model.User;
@@ -38,10 +39,12 @@ import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * @author Peter Fellwock
  */
+@RunWith(Arquillian.class)
 public class TeamFinderTest {
 
 	@ClassRule
