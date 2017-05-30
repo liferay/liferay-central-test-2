4528cae6ca0a8ed23cb5f7a097906afabc73a620
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-05-30 08:25:32 +0100

    LPS-72812 Adds RunWith Arquillian annotation

--- modules/apps/web-experience/asset/asset-test/src/testIntegration/java/com/liferay/asset/service/test/AssetTagFinderTest.java
+++ modules/apps/web-experience/asset/asset-test/src/testIntegration/java/com/liferay/asset/service/test/AssetTagFinderTest.java
@@ -14,6 +14,7 @@
 
 package com.liferay.asset.service.test;
 
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.asset.kernel.model.AssetTag;
 import com.liferay.asset.kernel.service.persistence.AssetTagFinderUtil;
 import com.liferay.message.boards.kernel.model.MBCategoryConstants;
@@ -49,10 +50,12 @@ import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * @author Sergio González
  */
+@RunWith(Arquillian.class)
 public class AssetTagFinderTest {
 
 	@ClassRule
