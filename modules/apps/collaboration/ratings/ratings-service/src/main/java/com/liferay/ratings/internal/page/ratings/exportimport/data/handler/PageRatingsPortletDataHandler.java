eaff6a040b49c74d207e78909ace04c558226c1a
Brian Chan <brian.chan@liferay.com>
2017-05-30 13:48:39 -0700

    LPS-72658 SF

--- modules/apps/collaboration/ratings/ratings-service/src/main/java/com/liferay/ratings/internal/page/ratings/exportimport/data/handler/PageRatingsPortletDataHandler.java
+++ modules/apps/collaboration/ratings/ratings-service/src/main/java/com/liferay/ratings/internal/page/ratings/exportimport/data/handler/PageRatingsPortletDataHandler.java
@@ -132,7 +132,7 @@ public class PageRatingsPortletDataHandler extends BasePortletDataHandler {
 		actionableDynamicQuery.performCount();
 	}
 
-	protected long fetchRatedEntityGroupId(RatingsEntry ratingsEntry)
+	protected long getGroupId(RatingsEntry ratingsEntry)
 		throws PortalException {
 
 		PersistedModelLocalService persistedModelLocalService =
@@ -180,7 +180,7 @@ public class PageRatingsPortletDataHandler extends BasePortletDataHandler {
 				public void performAction(RatingsEntry ratingsEntry)
 					throws PortalException {
 
-					long groupId = fetchRatedEntityGroupId(ratingsEntry);
+					long groupId = getGroupId(ratingsEntry);
 
 					if ((groupId > 0) &&
 						(groupId != portletDataContext.getScopeGroupId())) {
@@ -212,7 +212,7 @@ public class PageRatingsPortletDataHandler extends BasePortletDataHandler {
 				public void performAction(RatingsEntry ratingsEntry)
 					throws PortalException {
 
-					long groupId = fetchRatedEntityGroupId(ratingsEntry);
+					long groupId = getGroupId(ratingsEntry);
 
 					if ((groupId > 0) &&
 						(groupId != portletDataContext.getScopeGroupId())) {
