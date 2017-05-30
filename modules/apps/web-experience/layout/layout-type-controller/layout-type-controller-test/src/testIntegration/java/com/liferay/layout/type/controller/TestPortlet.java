e773a0f866da76c1686fe33c9a266aada47c2904
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-05-30 16:11:09 +0100

    LPS-72716 Adds new portlet for test, copy from portal-impl portlet test

---
+++ modules/apps/web-experience/layout/layout-type-controller/layout-type-controller-test/src/testIntegration/java/com/liferay/layout/type/controller/TestPortlet.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.type.controller;
+
+import com.liferay.portlet.StrutsPortlet;
+import com.liferay.portlet.util.test.PortletKeys;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.instanceable=true",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.scopeable=true", "javax.portlet.display-name=Test",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.name=" + PortletKeys.TEST,
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
+public class TestPortlet extends StrutsPortlet {
+}
