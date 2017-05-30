a35ea10ada886a5e36e14a6d4a6dc50498ddb48d
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-05-30 10:52:18 +0100

    LPS-72816 No needed

--- modules/apps/web-experience/layout/layout-type-controller/layout-type-controller-test/src/testIntegration/java/com/liferay/layout/type/controller/test/LayoutTypeControllerTrackerTest.java
+++ modules/apps/web-experience/layout/layout-type-controller/layout-type-controller-test/src/testIntegration/java/com/liferay/layout/type/controller/test/LayoutTypeControllerTrackerTest.java
@@ -23,7 +23,6 @@ import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.model.impl.LayoutImpl;
 import com.liferay.portal.model.impl.LayoutTypeControllerImpl;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portal.test.rule.SyntheticBundleRule;
 import com.liferay.portal.util.LayoutTypeControllerTracker;
 
 import java.util.Locale;
@@ -44,9 +43,7 @@ public class LayoutTypeControllerTrackerTest {
 	@ClassRule
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
-		new AggregateTestRule(
-			new LiferayIntegrationTestRule(),
-			new SyntheticBundleRule("bundle.layouttypecontrollertracker"));
+		new LiferayIntegrationTestRule();
 
 	@Test
 	public void testGetLayoutTypeController1() {
