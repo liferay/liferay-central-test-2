2e68ef9a40a47fb50972d871d1198bc032f3c384
Jürgen Kappler <jurgen.kappler@liferay.com>
2017-05-31 12:12:17 +0200

    LPS-72802 Demo class for journal article demo data creator

---
+++ modules/apps/web-experience/journal/journal-demo/src/main/java/com/liferay/journal/demo/internal/JournalDemo.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.demo.internal;
+
+import com.liferay.journal.demo.data.creator.JournalArticleDemoDataCreator;
+import com.liferay.portal.instance.lifecycle.BasePortalInstanceLifecycleListener;
+import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.users.admin.demo.data.creator.SiteAdminUserDemoDataCreator;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(immediate = true, service = PortalInstanceLifecycleListener.class)
+public class JournalDemo extends BasePortalInstanceLifecycleListener {
+
+	@Override
+	public void portalInstanceRegistered(Company company) throws Exception {
+		Group group = _groupLocalService.getGroup(
+			company.getCompanyId(), "Guest");
+
+		User user = _siteAdminUserDemoDataCreator.create(
+			group.getGroupId(), "sharon.choi@liferay.com");
+
+		for (int i = 0; i < 5; i++) {
+			_journalArticleDemoDataCreator.create(
+				user.getUserId(), group.getGroupId());
+		}
+	}
+
+	@Deactivate
+	protected void deactivate() throws PortalException {
+		_journalArticleDemoDataCreator.delete();
+		_siteAdminUserDemoDataCreator.delete();
+	}
+
+	@Reference(unbind = "-")
+	protected void setGroupLocalService(GroupLocalService groupLocalService) {
+		_groupLocalService = groupLocalService;
+	}
+
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
+	protected void setModuleServiceLifecycle(
+		ModuleServiceLifecycle moduleServiceLifecycle) {
+	}
+
+	@Reference(unbind = "-")
+	protected void setSiteAdminUserDemoDataCreator(
+		SiteAdminUserDemoDataCreator siteAdminUserDemoDataCreator) {
+
+		_siteAdminUserDemoDataCreator = siteAdminUserDemoDataCreator;
+	}
+
+	@Reference(unbind = "-")
+	protected void setSomeJournalArticleDemoDataCreator(
+		JournalArticleDemoDataCreator journalArticleDemoDataCreator) {
+
+		_journalArticleDemoDataCreator = journalArticleDemoDataCreator;
+	}
+
+	private GroupLocalService _groupLocalService;
+	private JournalArticleDemoDataCreator _journalArticleDemoDataCreator;
+	private SiteAdminUserDemoDataCreator _siteAdminUserDemoDataCreator;
+
+}
