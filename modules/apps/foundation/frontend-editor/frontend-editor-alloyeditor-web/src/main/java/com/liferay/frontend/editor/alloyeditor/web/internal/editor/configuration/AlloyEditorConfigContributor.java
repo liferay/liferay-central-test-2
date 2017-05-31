15814be0760f8da3c27b945c727d47ce2befaf11
Preston-Crary <preston.crary@liferay.com>
2017-05-31 12:32:55 -0700

    LPS-72855 Cache toolbarsJSONObject by languageId. ThemeDisplay's languageId should always be the same as its locale, String.hashCode() is faster than Locale.hashCode() so using that as the key.

--- modules/apps/foundation/frontend-editor/frontend-editor-alloyeditor-web/src/main/java/com/liferay/frontend/editor/alloyeditor/web/internal/editor/configuration/AlloyEditorConfigContributor.java
+++ modules/apps/foundation/frontend-editor/frontend-editor-alloyeditor-web/src/main/java/com/liferay/frontend/editor/alloyeditor/web/internal/editor/configuration/AlloyEditorConfigContributor.java
@@ -32,6 +32,7 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -68,8 +69,14 @@ public class AlloyEditorConfigContributor
 
 		jsonObject.put("extraPlugins", extraPlugins);
 
-		jsonObject.put(
-			"toolbars", getToolbarsJSONObject(themeDisplay.getLocale()));
+		JSONObject toolbarsJSONObject =
+			_toolbarsJSONObjectCache.computeIfAbsent(
+				themeDisplay.getLanguageId(),
+				(key) -> {
+					return getToolbarsJSONObject(themeDisplay.getLocale());
+				});
+
+		jsonObject.put("toolbars", toolbarsJSONObject);
 	}
 
 	protected JSONObject getStyleFormatJSONObject(
@@ -300,6 +307,9 @@ public class AlloyEditorConfigContributor
 	private static final int _CKEDITOR_STYLE_BLOCK = 1;
 
 	private static final int _CKEDITOR_STYLE_INLINE = 2;
+
+	private static final Map<String, JSONObject> _toolbarsJSONObjectCache =
+		new ConcurrentHashMap<>();
 
 	private volatile ResourceBundleLoader _resourceBundleLoader;
 
