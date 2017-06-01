168c2e406547616b4298b32b76717bb6793ce98d
Brent Krone-Schmidt <brent.krone-schmidt@liferay.com>
2017-06-01 08:47:33 -0700

    LPS-72918 change imports counter.service to counter.kernel.service

--- modules/apps/tasks/tasks-portlet/docroot/WEB-INF/src/com/liferay/tasks/service/base/TasksEntryLocalServiceBaseImpl.java
+++ modules/apps/tasks/tasks-portlet/docroot/WEB-INF/src/com/liferay/tasks/service/base/TasksEntryLocalServiceBaseImpl.java
@@ -363,7 +363,7 @@ public abstract class TasksEntryLocalServiceBaseImpl
 	 *
 	 * @return the counter local service
 	 */
-	public com.liferay.counter.service.CounterLocalService getCounterLocalService() {
+	public com.liferay.counter.kernel.service.CounterLocalService getCounterLocalService() {
 		return counterLocalService;
 	}
 
@@ -373,7 +373,7 @@ public abstract class TasksEntryLocalServiceBaseImpl
 	 * @param counterLocalService the counter local service
 	 */
 	public void setCounterLocalService(
-		com.liferay.counter.service.CounterLocalService counterLocalService) {
+		com.liferay.counter.kernel.service.CounterLocalService counterLocalService) {
 		this.counterLocalService = counterLocalService;
 	}
 
@@ -599,8 +599,8 @@ public abstract class TasksEntryLocalServiceBaseImpl
 	protected TasksEntryPersistence tasksEntryPersistence;
 	@BeanReference(type = TasksEntryFinder.class)
 	protected TasksEntryFinder tasksEntryFinder;
-	@BeanReference(type = com.liferay.counter.service.CounterLocalService.class)
-	protected com.liferay.counter.service.CounterLocalService counterLocalService;
+	@BeanReference(type = com.liferay.counter.kernel.service.CounterLocalService.class)
+	protected com.liferay.counter.kernel.service.CounterLocalService counterLocalService;
 	@BeanReference(type = com.liferay.portal.kernel.service.ClassNameLocalService.class)
 	protected com.liferay.portal.kernel.service.ClassNameLocalService classNameLocalService;
 	@BeanReference(type = com.liferay.portal.kernel.service.ClassNameService.class)
