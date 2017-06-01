0bd263b349a89ff3e1cdf2fd94b3e0bc13a15e97
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2017-06-01 09:34:52 -0300

    LPS-72276 Test: toCalendarBookingJSONObject() serializes the last instance recurrence.

--- modules/apps/forms-and-workflow/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/util/test/CalendarUtilTest.java
+++ modules/apps/forms-and-workflow/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/util/test/CalendarUtilTest.java
@@ -16,6 +16,8 @@ package com.liferay.calendar.util.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.calendar.model.CalendarBooking;
+import com.liferay.calendar.recurrence.Recurrence;
+import com.liferay.calendar.recurrence.RecurrenceSerializer;
 import com.liferay.calendar.test.util.CalendarBookingTestUtil;
 import com.liferay.calendar.util.CalendarUtil;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -25,16 +27,18 @@ import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.TimeZoneUtil;
-import com.liferay.portal.security.permission.SimplePermissionChecker;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import java.util.HashSet;
@@ -69,12 +73,42 @@ public class CalendarUtilTest {
 		_permissionChecker = PermissionThreadLocal.getPermissionChecker();
 
 		PermissionThreadLocal.setPermissionChecker(
-			new SimplePermissionChecker());
+			PermissionCheckerFactoryUtil.create(_user));
 	}
 
 	@After
 	public void tearDown() {
 		PermissionThreadLocal.setPermissionChecker(_permissionChecker);
+	}
+
+	@Test
+	public void testToCalendarBookingJSONObjectSendLastInstanceRecurrence()
+		throws Exception {
+
+		ServiceContext serviceContext = createServiceContext();
+
+		CalendarBooking calendarBooking =
+			CalendarBookingTestUtil.addDailyRecurringCalendarBooking(
+				_user, serviceContext);
+
+		CalendarBooking calendarBookingInstance =
+			CalendarBookingTestUtil.updateCalendarBookingInstance(
+				calendarBooking, 2, RandomTestUtil.randomLocaleStringMap(),
+				serviceContext);
+
+		JSONObject jsonObject = CalendarUtil.toCalendarBookingJSONObject(
+			createThemeDisplay(), calendarBookingInstance,
+			calendarBookingInstance.getTimeZone());
+
+		Recurrence recurrence = RecurrenceSerializer.deserialize(
+			jsonObject.getString("recurrence"),
+			calendarBookingInstance.getTimeZone());
+
+		Assert.assertNotNull(recurrence);
+
+		Assert.assertNull(recurrence.getUntilJCalendar());
+
+		Assert.assertEquals(0, recurrence.getCount());
 	}
 
 	@Test
@@ -108,6 +142,18 @@ public class CalendarUtilTest {
 
 		Assert.assertEquals(
 			excpectedCalendarBookingIds, actualCalendarBookingIds);
+	}
+
+	protected ServiceContext createServiceContext() {
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setCompanyId(_user.getCompanyId());
+
+		serviceContext.setUserId(_user.getUserId());
+
+		serviceContext.setScopeGroupId(_user.getGroupId());
+
+		return serviceContext;
 	}
 
 	protected ThemeDisplay createThemeDisplay() throws PortalException {
