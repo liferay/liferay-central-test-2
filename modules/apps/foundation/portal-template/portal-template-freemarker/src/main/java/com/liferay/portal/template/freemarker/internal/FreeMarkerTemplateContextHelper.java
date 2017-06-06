10060ae134471343dfb7c8b7452d8b4ec55bc8fb
shuyangzhou <shuyang.zhou@liferay.com>
2017-06-06 14:58:50 -0700

    LPS-72952 Avoid repeatedly creating the same set at runtime from FreeMarkerTemplateContextHelper.getRestrictedVariables()

--- modules/apps/foundation/portal-template/portal-template-freemarker/src/main/java/com/liferay/portal/template/freemarker/internal/FreeMarkerTemplateContextHelper.java
+++ modules/apps/foundation/portal-template/portal-template-freemarker/src/main/java/com/liferay/portal/template/freemarker/internal/FreeMarkerTemplateContextHelper.java
@@ -60,8 +60,7 @@ public class FreeMarkerTemplateContextHelper extends TemplateContextHelper {
 
 	@Override
 	public Set<String> getRestrictedVariables() {
-		return SetUtil.fromArray(
-			_freemarkerEngineConfiguration.restrictedVariables());
+		return _restrictedVariables;
 	}
 
 	@Override
@@ -130,6 +129,9 @@ public class FreeMarkerTemplateContextHelper extends TemplateContextHelper {
 	protected void activate(Map<String, Object> properties) {
 		_freemarkerEngineConfiguration = ConfigurableUtil.createConfigurable(
 			FreeMarkerEngineConfiguration.class, properties);
+
+		_restrictedVariables = SetUtil.fromArray(
+			_freemarkerEngineConfiguration.restrictedVariables());
 	}
 
 	@Override
@@ -177,6 +179,7 @@ public class FreeMarkerTemplateContextHelper extends TemplateContextHelper {
 
 	private volatile FreeMarkerEngineConfiguration
 		_freemarkerEngineConfiguration;
+	private Set<String> _restrictedVariables;
 	private final List<TemplateContextContributor>
 		_templateContextContributors = new CopyOnWriteArrayList<>();
 
