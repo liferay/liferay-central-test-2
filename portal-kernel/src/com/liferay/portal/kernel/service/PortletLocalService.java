469c328c2671827631ab4140b652750095a3b313
shuyangzhou <shuyang.zhou@liferay.com>
2017-06-06 14:52:27 -0700

    LPS-72951 Regenerate

--- portal-kernel/src/com/liferay/portal/kernel/service/PortletLocalService.java
+++ portal-kernel/src/com/liferay/portal/kernel/service/PortletLocalService.java
@@ -43,6 +43,7 @@ import java.io.Serializable;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 
 import javax.servlet.ServletContext;
 
@@ -370,4 +371,7 @@ public interface PortletLocalService extends BaseLocalService,
 	@Clusterable
 	@Transactional(enabled = false)
 	public void removeCompanyPortletsPool(long companyId);
+
+	@Skip
+	public void visitPortlets(long companyId, Consumer<Portlet> consumer);
 }
