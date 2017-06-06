2d1c30a01ea9e2d71c4147171f7e25e980e06e1e
shuyangzhou <shuyang.zhou@liferay.com>
2017-06-06 14:52:47 -0700

    LPS-72951 Apply usage

--- modules/apps/foundation/frontend-js/frontend-js-spa-web/src/main/java/com/liferay/frontend/js/spa/web/internal/servlet/taglib/util/SPAUtil.java
+++ modules/apps/foundation/frontend-js/frontend-js-spa-web/src/main/java/com/liferay/frontend/js/spa/web/internal/servlet/taglib/util/SPAUtil.java
@@ -19,7 +19,6 @@ import com.liferay.frontend.js.spa.web.configuration.SPAConfigurationActivator;
 import com.liferay.frontend.js.spa.web.configuration.SPAConfigurationUtil;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.service.PortletLocalService;
 import com.liferay.portal.kernel.servlet.ServletResponseConstants;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
@@ -92,19 +91,17 @@ public class SPAUtil {
 
 		sb.append(StringPool.OPEN_CURLY_BRACE);
 
-		List<Portlet> companyPortlets = _portletLocalService.getPortlets(
-			themeDisplay.getCompanyId());
+		_portletLocalService.visitPortlets(
+			themeDisplay.getCompanyId(), portlet -> {
+				if (!portlet.isSinglePageApplication() &&
+					!portlet.isUndeployedPortlet() && portlet.isActive() &&
+					portlet.isReady()) {
 
-		for (Portlet portlet : companyPortlets) {
-			if (!portlet.isSinglePageApplication() &&
-				!portlet.isUndeployedPortlet() && portlet.isActive() &&
-				portlet.isReady()) {
-
-				sb.append(StringPool.QUOTE);
-				sb.append(portlet.getPortletId());
-				sb.append("\":true,");
-			}
-		}
+					sb.append(StringPool.QUOTE);
+					sb.append(portlet.getPortletId());
+					sb.append("\":true,");
+				}
+			});
 
 		if (sb.index() == 1) {
 			sb.append(StringPool.CLOSE_CURLY_BRACE);
