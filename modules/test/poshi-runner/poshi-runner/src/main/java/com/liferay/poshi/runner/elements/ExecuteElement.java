9c25df704534d53362dd0f480693c8c2d85e2ff0
Kenji Heigel <kenji.heigel@liferay.com>
2017-06-07 11:21:37 -0700

    LRQA-32801 Implement readable syntax parsing for execute elements

--- modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/elements/ExecuteElement.java
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/elements/ExecuteElement.java
@@ -14,13 +14,21 @@
 
 package com.liferay.poshi.runner.elements;
 
+import static com.liferay.poshi.runner.elements.ReadableSyntaxKeys.AND;
 import static com.liferay.poshi.runner.elements.ReadableSyntaxKeys.AT_LOCATOR;
+import static com.liferay.poshi.runner.elements.ReadableSyntaxKeys.GIVEN;
+import static com.liferay.poshi.runner.elements.ReadableSyntaxKeys.THEN;
 import static com.liferay.poshi.runner.elements.ReadableSyntaxKeys.THE_VALUE;
+import static com.liferay.poshi.runner.elements.ReadableSyntaxKeys.WHEN;
 
 import com.liferay.poshi.runner.util.StringUtil;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.dom4j.Element;
 
@@ -39,10 +47,38 @@ public class ExecuteElement extends PoshiElement {
 
 	@Override
 	public void addAttributes(String readableSyntax) {
+		if (readableSyntax.contains(AT_LOCATOR) ||
+			readableSyntax.contains(THE_VALUE)) {
+
+			_attributes = new TreeMap<>();
+
+			_addFunctionAttributes(readableSyntax);
+
+			return;
+		}
+
+		addAttribute("macro", _getClassCommandName(readableSyntax));
 	}
 
 	@Override
 	public void addElements(String readableSyntax) {
+		List<String> readableBlocks = StringUtil.split(
+			readableSyntax, READABLE_VARIABLE_BLOCK_KEYS);
+
+		for (String readableBlock : readableBlocks) {
+			readableBlock = readableBlock.trim();
+
+			if (readableBlock.contains(AND) || readableBlock.contains(GIVEN) ||
+				readableBlock.contains(THEN) || readableBlock.contains(WHEN)) {
+
+				continue;
+			}
+
+			Element element = PoshiElementFactory.newPoshiElement(
+				readableBlock);
+
+			add(element);
+		}
 	}
 
 	@Override
@@ -94,6 +130,85 @@ public class ExecuteElement extends PoshiElement {
 		return sb.toString();
 	}
 
+	private void _addFunctionAttribute(
+		String readableSyntax, String attributeType) {
+
+		String attributeValue = getAttributeValue("'", "'", readableSyntax);
+
+		if (attributeValue(attributeType + "1") == null) {
+			_attributes.put(attributeType + "1", attributeValue);
+
+			return;
+		}
+
+		_attributes.put(attributeType + "2", attributeValue);
+	}
+
+	private void _addFunctionAttributes(String readableSyntax) {
+		String[] keys = {AT_LOCATOR, THE_VALUE};
+
+		List<String> functionItems = StringUtil.split(
+			readableSyntax, keys);
+
+		for (String functionItem : functionItems) {
+			if (functionItem.contains(AT_LOCATOR)) {
+				_addFunctionAttribute(functionItem, "locator");
+
+				continue;
+			}
+
+			if (functionItem.contains(THE_VALUE)) {
+				_addFunctionAttribute(functionItem, "value");
+
+				continue;
+			}
+
+			_attributes.put("function", _getClassCommandName(functionItem));
+		}
+
+		for (String key : _attributes.keySet()) {
+			addAttribute(key, _attributes.get(key));
+		}
+	}
+
+	private String _getClassCommandName(String readableSyntax) {
+		int index = readableSyntax.indexOf("\n");
+
+		if (index < 0) {
+			index = readableSyntax.length();
+		}
+
+		String line = readableSyntax.substring(0, index);
+
+		for (String key : READABLE_EXECUTE_BLOCK_KEYS) {
+			if (line.startsWith(key)) {
+				Pattern pattern = Pattern.compile(
+					".*?" + key + ".*?.([A-z]*)(.*)");
+
+				Matcher matcher = pattern.matcher(line);
+
+				if (matcher.find()) {
+					StringBuilder sb = new StringBuilder();
+
+					sb.append(matcher.group(1));
+
+					String commandName = matcher.group(2);
+
+					commandName = StringUtil.removeSpaces(commandName);
+
+					if (commandName.length() > 0) {
+						sb.append("#");
+						sb.append(commandName);
+					}
+
+					return sb.toString();
+				}
+			}
+		}
+
+		return null;
+	}
+
 	private String _getReadableSyntaxCommandPhrase(String classCommandName) {
 		StringBuilder sb = new StringBuilder();
 
@@ -115,5 +230,7 @@ public class ExecuteElement extends PoshiElement {
 
 		return classCommandName;
 	}
+
+	private Map<String, String> _attributes;
 
 }
