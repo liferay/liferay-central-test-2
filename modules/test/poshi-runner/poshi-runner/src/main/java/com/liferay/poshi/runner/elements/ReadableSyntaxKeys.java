7f83efd693235497c24d0bdc47ad9d67b01b808d
Kenji Heigel <kenji.heigel@liferay.com>
2017-06-07 10:18:15 -0700

    LRQA-32801 Add class to store readable syntax keywords as constants

---
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/elements/ReadableSyntaxKeys.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.poshi.runner.elements;
+
+/**
+ * @author Kenji Heigel
+ */
+public class ReadableSyntaxKeys {
+
+	public static final String AND = "And";
+
+	public static final String AT_LOCATOR = "at locator";
+
+	public static final String BACKGROUND = "Background";
+
+	public static final String DESCRIPTION = "Description";
+
+	public static final String FEATURE = "Feature";
+
+	public static final String GIVEN = "Given";
+
+	public static final String PRIORITY = "Priority";
+
+	public static final String SCENARIO = "Scenario";
+
+	public static final String SETUP = "Setup";
+
+	public static final String TEARDOWN = "Teardown";
+
+	public static final String THE_VALUE = "the value";
+
+	public static final String THEN = "Then";
+
+	public static final String THESE_PROPERTIES = "these properties";
+
+	public static final String THESE_VARIABLES = "these variables";
+
+	public static final String WHEN = "When";
+
+}
