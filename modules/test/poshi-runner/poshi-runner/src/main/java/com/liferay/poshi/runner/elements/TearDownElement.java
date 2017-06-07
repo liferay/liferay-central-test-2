9aa0362b9adc37fbb5e175d41fc8b253ca0bcf96
Kenji Heigel <kenji.heigel@liferay.com>
2017-06-07 10:22:11 -0700

    LRQA-32801 Add Setup and Teardown elements and extend them from CommandElement

---
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/elements/TearDownElement.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.poshi.runner.elements;
+
+import org.dom4j.Element;
+
+/**
+ * @author Kenji Heigel
+ */
+public class TearDownElement extends CommandElement {
+
+	public TearDownElement(Element element) {
+		super("tear-down", element);
+	}
+
+	protected String getReadableCommandTitle() {
+		return "Teardown: This executes after each scenario";
+	}
+
+}
