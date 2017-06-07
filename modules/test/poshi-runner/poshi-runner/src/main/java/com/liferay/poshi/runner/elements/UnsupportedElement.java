c31badd985d8d49068f2a5d04b105ff6f8e8d104
Kenji Heigel <kenji.heigel@liferay.com>
2017-06-07 10:54:18 -0700

    LRQA-32801 Add class to catch unsupported elements

---
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/elements/UnsupportedElement.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.poshi.runner.elements;
+
+import org.dom4j.Element;
+
+/**
+ * @author Kenji Heigel
+ */
+public class UnsupportedElement extends PoshiElement {
+
+	public UnsupportedElement(Element element) {
+		super(element.getName(), element);
+	}
+
+	@Override
+	public String toReadableSyntax() {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("\n");
+		sb.append("##########################################################");
+		sb.append("\n");
+		sb.append("The Poshi ");
+		sb.append(getName());
+		sb.append(" element is not supported in the readable syntax. ");
+		sb.append("Please update this test.");
+		sb.append("\n");
+		sb.append("##########################################################");
+
+		return sb.toString();
+	}
+
+}
