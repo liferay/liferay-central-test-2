248561fa58e261fcf137d7627aa548210bdad028
Brian Chan <brian.chan@liferay.com>
2017-06-08 15:04:40 -0700

    VULCAN-3 Avoid chaining

--- modules/apps/foundation/vulcan/vulcan-jaxrs-writer-json/src/main/java/com/liferay/vulcan/jaxrs/writer/json/internal/PageMessageBodyWriter.java
+++ modules/apps/foundation/vulcan/vulcan-jaxrs-writer-json/src/main/java/com/liferay/vulcan/jaxrs/writer/json/internal/PageMessageBodyWriter.java
@@ -88,8 +88,10 @@ public class PageMessageBodyWriter<T> implements MessageBodyWriter<Page<T>> {
 
 		Type genericReturnType = resourceMethod.getGenericReturnType();
 
-		Type[] actualTypeArguments =
-			((ParameterizedType)genericReturnType).getActualTypeArguments();
+		ParameterizedType parameterizedType =
+			(ParameterizedType)genericReturnType;
+
+		Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
 
 		try {
 			Class<T> modelClass = (Class<T>)actualTypeArguments[0];
