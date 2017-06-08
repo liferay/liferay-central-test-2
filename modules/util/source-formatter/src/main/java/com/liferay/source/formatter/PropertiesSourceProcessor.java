4b8b948d7e35cf528b8d3b0c4069216b2cd464a4
Hugo Huijser <hugo.huijser@liferay.com>
2017-06-08 09:37:39 +0800

    LPS-72705 SF Language.properties files

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/PropertiesSourceProcessor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/PropertiesSourceProcessor.java
@@ -33,6 +33,7 @@ public class PropertiesSourceProcessor extends BaseSourceProcessor {
 				"**/app-server.properties", "**/ci.properties",
 				"**/lib/*/dependencies.properties", "**/framework.properties",
 				"**/gradle-wrapper.properties", "**/imported-files.properties",
+				"**/Language*.properties",
 				"**/liferay-plugin-package*.properties", "**/portal.properties",
 				"**/portal-ext.properties", "**/portal-legacy-*.properties",
 				"**/portlet.properties", "**/source-formatter.properties",
