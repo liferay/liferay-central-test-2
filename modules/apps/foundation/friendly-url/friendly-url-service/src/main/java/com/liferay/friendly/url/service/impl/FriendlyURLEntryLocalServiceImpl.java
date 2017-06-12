cd1996d51fc93b71a4d1230c40cf7086b24a1d85
Adolfo Pérez <adolfo.perez@liferay.com>
2017-06-12 11:25:59 +0200

    LPS-72949 Add service context to service methods

--- modules/apps/foundation/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/service/impl/FriendlyURLEntryLocalServiceImpl.java
+++ modules/apps/foundation/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/service/impl/FriendlyURLEntryLocalServiceImpl.java
@@ -26,6 +26,7 @@ import com.liferay.portal.kernel.dao.orm.Property;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.ModelHintsUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil;
 import com.liferay.portal.kernel.util.StringPool;
 
@@ -40,19 +41,19 @@ public class FriendlyURLEntryLocalServiceImpl
 	@Override
 	public FriendlyURLEntry addFriendlyURLEntry(
 			long groupId, long companyId, Class<?> clazz, long classPK,
-			String urlTitle)
+			String urlTitle, ServiceContext serviceContext)
 		throws PortalException {
 
 		long classNameId = classNameLocalService.getClassNameId(clazz);
 
 		return addFriendlyURLEntry(
-			groupId, companyId, classNameId, classPK, urlTitle);
+			groupId, companyId, classNameId, classPK, urlTitle, serviceContext);
 	}
 
 	@Override
 	public FriendlyURLEntry addFriendlyURLEntry(
 			long groupId, long companyId, long classNameId, long classPK,
-			String urlTitle)
+			String urlTitle, ServiceContext serviceContext)
 		throws PortalException {
 
 		String normalizedUrlTitle = FriendlyURLNormalizerUtil.normalize(
@@ -90,6 +91,7 @@ public class FriendlyURLEntryLocalServiceImpl
 		friendlyURLEntry.setClassNameId(classNameId);
 		friendlyURLEntry.setClassPK(classPK);
 		friendlyURLEntry.setUrlTitle(normalizedUrlTitle);
+		friendlyURLEntry.setUuid(serviceContext.getUuid());
 		friendlyURLEntry.setMain(true);
 
 		return friendlyURLEntryPersistence.update(friendlyURLEntry);
