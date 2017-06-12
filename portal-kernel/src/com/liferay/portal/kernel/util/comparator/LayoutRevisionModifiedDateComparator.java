ad86df314dd8f6b38f7e67c6efaf8df395c75d92
Máté Thurzó <mate.thurzo@liferay.com>
2017-06-12 16:55:24 +0200

    LPS-72099 Make it a provider type

--- portal-kernel/src/com/liferay/portal/kernel/util/comparator/LayoutRevisionModifiedDateComparator.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/comparator/LayoutRevisionModifiedDateComparator.java
@@ -14,6 +14,8 @@
 
 package com.liferay.portal.kernel.util.comparator;
 
+import aQute.bnd.annotation.ProviderType;
+
 import com.liferay.portal.kernel.model.LayoutRevision;
 import com.liferay.portal.kernel.util.DateUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
@@ -21,6 +23,7 @@ import com.liferay.portal.kernel.util.OrderByComparator;
 /**
  * @author Akos Thurzo
  */
+@ProviderType
 public class LayoutRevisionModifiedDateComparator
 	extends OrderByComparator<LayoutRevision> {
 
