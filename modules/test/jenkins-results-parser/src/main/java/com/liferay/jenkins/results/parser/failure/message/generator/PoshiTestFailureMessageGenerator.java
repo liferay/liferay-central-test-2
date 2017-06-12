c1f3840a6b5ba4a07424b90870f76c85118781ac
yichenroy <yi-chen.tsai@liferay.com>
2017-06-12 13:29:12 -0700

    LRQA-33127 SF

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/failure/message/generator/PoshiTestFailureMessageGenerator.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/failure/message/generator/PoshiTestFailureMessageGenerator.java
@@ -27,82 +27,83 @@ import org.dom4j.Element;
  * @author Yi-Chen Tsai
  */
 public class PoshiTestFailureMessageGenerator
-        extends BaseFailureMessageGenerator {
+	extends BaseFailureMessageGenerator {
 
-    @Override
-    public String getMessage(
-            String buildURL, String consoleOutput, Hashtable<?, ?> properties) {
+	@Override
+	public String getMessage(
+		String buildURL, String consoleOutput, Hashtable<?, ?> properties) {
 
-        Matcher poshiTestFailureMatcher = _poshiTestFailurePattern.matcher(
-                consoleOutput);
+		Matcher poshiTestFailureMatcher = _poshiTestFailurePattern.matcher(
+			consoleOutput);
 
-        if (!poshiTestFailureMatcher.find()) {
-            return null;
-        }
+		if (!poshiTestFailureMatcher.find()) {
+			return null;
+		}
 
-        String failedPoshiTaskToken = poshiTestFailureMatcher.group(1);
+		String failedPoshiTaskToken = poshiTestFailureMatcher.group(1);
 
-        int end = consoleOutput.indexOf(failedPoshiTaskToken);
+		int end = consoleOutput.indexOf(failedPoshiTaskToken);
 
-        end = consoleOutput.indexOf(_TOKEN_TRY, end);
+		end = consoleOutput.indexOf(_TOKEN_TRY, end);
 
-        end = consoleOutput.lastIndexOf("\n", end);
+		end = consoleOutput.lastIndexOf("\n", end);
 
-        int start = consoleOutput.lastIndexOf(_TOKEN_JAVA_LANG_EXCEPTION, end);
+		int start = consoleOutput.lastIndexOf(_TOKEN_JAVA_LANG_EXCEPTION, end);
 
-        start = consoleOutput.lastIndexOf("\n", start);
+		start = consoleOutput.lastIndexOf("\n", start);
 
-        StringBuilder sb = new StringBuilder();
+		StringBuilder sb = new StringBuilder();
 
-        sb.append("<p>POSHI Validation Failure: </p><strong>");
+		sb.append("<p>POSHI Validation Failure: </p><strong>");
 
-        sb.append(failedPoshiTaskToken);
+		sb.append(failedPoshiTaskToken);
 
-        sb.append("</strong><pre><code>");
+		sb.append("</strong><pre><code>");
 
-        sb.append(getConsoleOutputSnippet(consoleOutput, true, start, end));
+		sb.append(getConsoleOutputSnippet(consoleOutput, true, start, end));
 
-        sb.append("</code></pre>");
+		sb.append("</code></pre>");
 
-        return sb.toString();
-    }
+		return sb.toString();
+	}
 
-    @Override
-    public Element getMessageElement(Build build) {
-        String consoleText = build.getConsoleText();
+	@Override
+	public Element getMessageElement(Build build) {
+		String consoleText = build.getConsoleText();
 
-        Matcher poshiTestFailureMatcher = _poshiTestFailurePattern.matcher(consoleText);
+		Matcher poshiTestFailureMatcher = _poshiTestFailurePattern.matcher(
+			consoleText);
 
-        if (!poshiTestFailureMatcher.find()) {
-            return null;
-        }
+		if (!poshiTestFailureMatcher.find()) {
+			return null;
+		}
 
-        String failedPoshiTaskToken = poshiTestFailureMatcher.group(1);
+		String failedPoshiTaskToken = poshiTestFailureMatcher.group(1);
 
-        int end = consoleText.indexOf(failedPoshiTaskToken);
+		int end = consoleText.indexOf(failedPoshiTaskToken);
 
-        end = consoleText.indexOf(_TOKEN_TRY, end);
+		end = consoleText.indexOf(_TOKEN_TRY, end);
 
-        end = consoleText.lastIndexOf("\n", end);
+		end = consoleText.lastIndexOf("\n", end);
 
-        int start = consoleText.lastIndexOf(_TOKEN_JAVA_LANG_EXCEPTION, end);
+		int start = consoleText.lastIndexOf(_TOKEN_JAVA_LANG_EXCEPTION, end);
 
-        start = consoleText.lastIndexOf("\n", start);
+		start = consoleText.lastIndexOf("\n", start);
 
-        return Dom4JUtil.getNewElement(
-                "div", null,
-                Dom4JUtil.getNewElement(
-                        "p", null, "POSHI Validation Failure: ",
-                        Dom4JUtil.getNewElement("strong", null, failedPoshiTaskToken)),
-                getConsoleOutputSnippetElement(consoleText, true, start, end));
-    }
+		return Dom4JUtil.getNewElement(
+			"div", null,
+			Dom4JUtil.getNewElement(
+				"p", null, "POSHI Validation Failure: ",
+				Dom4JUtil.getNewElement("strong", null, failedPoshiTaskToken)),
+			getConsoleOutputSnippetElement(consoleText, true, start, end));
+	}
 
-    private static final String _TOKEN_JAVA_LANG_EXCEPTION =
-            "java.lang.Exception";
+	private static final String _TOKEN_JAVA_LANG_EXCEPTION =
+		"java.lang.Exception";
 
-    private static final String _TOKEN_TRY = "Try:";
+	private static final String _TOKEN_TRY = "Try:";
 
-    private static final Pattern _poshiTestFailurePattern = Pattern.compile(
-            "(?:\\n.*)(Execution failed for task.*Poshi.*)\\n");
+	private static final Pattern _poshiTestFailurePattern = Pattern.compile(
+		"(?:\\n.*)(Execution failed for task.*Poshi.*)\\n");
 
 }
