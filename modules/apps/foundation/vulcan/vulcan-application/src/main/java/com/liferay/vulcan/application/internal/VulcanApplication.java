b38ed3ab01a9bac150cb83e9284f87e0453ab6eb
Alejandro Hernández <info@alejandrohdezma.com>
2017-06-13 17:51:08 +0200

    VULCAN-18 Ensure that OSGi injects the `MessageBodyWriter` List
    The actual approach should work, but, for some reason, sometimes fail to inject them. This change solves the problem. So we could leave it like this.

--- modules/apps/foundation/vulcan/vulcan-application/src/main/java/com/liferay/vulcan/application/internal/VulcanApplication.java
+++ modules/apps/foundation/vulcan/vulcan-application/src/main/java/com/liferay/vulcan/application/internal/VulcanApplication.java
@@ -14,8 +14,12 @@
 
 package com.liferay.vulcan.application.internal;
 
+import static org.osgi.service.component.annotations.ReferenceCardinality.AT_LEAST_ONE;
+import static org.osgi.service.component.annotations.ReferencePolicyOption.GREEDY;
+
 import com.liferay.vulcan.endpoint.RootEndpoint;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -26,10 +30,9 @@ import javax.ws.rs.ext.MessageBodyWriter;
 
 import org.apache.cxf.jaxrs.ext.ContextProvider;
 
+import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Alejandro Hernández
@@ -53,17 +56,30 @@ public class VulcanApplication extends Application {
 	}
 
 	@Reference(
-		policyOption = ReferencePolicyOption.GREEDY,
-		target = "(liferay.vulcan.context.provider=true)"
+		cardinality = AT_LEAST_ONE, policyOption = GREEDY,
+		target = "(liferay.vulcan.message.body.writer=true)"
+	)
+	public <T> void setServiceReference(
+		ServiceReference<MessageBodyWriter<T>> serviceReference,
+		MessageBodyWriter<T> messageBodyWriter) {
+
+		_messageBodyWriters.add(messageBodyWriter);
+	}
+
+	public <T> void unsetServiceReference(
+		ServiceReference<MessageBodyWriter<T>> serviceReference,
+		MessageBodyWriter<T> messageBodyWriter) {
+
+		_messageBodyWriters.remove(messageBodyWriter);
+	}
+
+	@Reference(
+		policyOption = GREEDY, target = "(liferay.vulcan.context.provider=true)"
 	)
 	private List<ContextProvider> _contextProviders;
 
-	@Reference(
-		cardinality = ReferenceCardinality.AT_LEAST_ONE,
-		policyOption = ReferencePolicyOption.GREEDY,
-		target = "(liferay.vulcan.message.body.writer=true)"
-	)
-	private List<MessageBodyWriter> _messageBodyWriters;
+	private final List<MessageBodyWriter> _messageBodyWriters =
+		new ArrayList<>();
 
 	@Reference
 	private RootEndpoint _rootEndpoint;
