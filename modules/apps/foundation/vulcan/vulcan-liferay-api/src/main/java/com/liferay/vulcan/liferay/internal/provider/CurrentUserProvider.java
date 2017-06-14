dfc912f5614fe4eb3e0ca317644d6c5980a7afc5
Alejandro Hernández <info@alejandrohdezma.com>
2017-06-14 12:35:15 +0200

    VULCAN-18 Create providers for current user and group

---
+++ modules/apps/foundation/vulcan/vulcan-liferay-api/src/main/java/com/liferay/vulcan/liferay/internal/provider/CurrentUserProvider.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.vulcan.liferay.internal.provider;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.UserService;
+import com.liferay.vulcan.liferay.context.CurrentUser;
+import com.liferay.vulcan.provider.Provider;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.ws.rs.NotFoundException;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Hernández
+ */
+@Component(immediate = true)
+public class CurrentUserProvider implements Provider<CurrentUser> {
+
+	@Override
+	public CurrentUser createContext(HttpServletRequest httpServletRequest) {
+		return () -> {
+			try {
+				return _userService.getCurrentUser();
+			}
+			catch (PortalException pe) {
+				throw new NotFoundException(pe);
+			}
+		};
+	}
+
+	@Reference
+	private UserService _userService;
+
+}
