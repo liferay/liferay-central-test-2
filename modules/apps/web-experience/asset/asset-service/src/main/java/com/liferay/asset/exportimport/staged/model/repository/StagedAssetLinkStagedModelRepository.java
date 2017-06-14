cf85019545f8bcc348a8b0fadb8d73b13a123fbb
akosthurzo <akos.thurzo@liferay.com>
2017-06-14 15:56:39 +0200

    LPS-72472 Add fallback logic to asset entry fetcher

--- modules/apps/web-experience/asset/asset-service/src/main/java/com/liferay/asset/exportimport/staged/model/repository/StagedAssetLinkStagedModelRepository.java
+++ modules/apps/web-experience/asset/asset-service/src/main/java/com/liferay/asset/exportimport/staged/model/repository/StagedAssetLinkStagedModelRepository.java
@@ -16,6 +16,7 @@ package com.liferay.asset.exportimport.staged.model.repository;
 
 import com.liferay.asset.kernel.model.AssetEntry;
 import com.liferay.asset.kernel.model.AssetLink;
+import com.liferay.asset.kernel.model.AssetRenderer;
 import com.liferay.asset.kernel.model.adapter.StagedAssetLink;
 import com.liferay.asset.kernel.service.AssetEntryLocalService;
 import com.liferay.asset.kernel.service.AssetLinkLocalService;
@@ -31,12 +32,23 @@ import com.liferay.portal.kernel.dao.orm.ProjectionFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.Property;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.StagedGroupedModel;
+import com.liferay.portal.kernel.model.StagedModel;
+import com.liferay.portal.kernel.model.TrashedModel;
 import com.liferay.portal.kernel.model.adapter.ModelAdapterUtil;
+import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.StringPool;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -65,10 +77,10 @@ public class StagedAssetLinkStagedModelRepository
 		long userId = portletDataContext.getUserId(
 			stagedAssetLink.getUserUuid());
 
-		AssetEntry assetEntry1 = _assetEntryLocalService.fetchEntry(
+		AssetEntry assetEntry1 = fetchAssetEntry(
 			portletDataContext.getScopeGroupId(),
 			stagedAssetLink.getEntry1Uuid());
-		AssetEntry assetEntry2 = _assetEntryLocalService.fetchEntry(
+		AssetEntry assetEntry2 = fetchAssetEntry(
 			portletDataContext.getScopeGroupId(),
 			stagedAssetLink.getEntry2Uuid());
 
@@ -113,10 +125,18 @@ public class StagedAssetLinkStagedModelRepository
 	}
 
 	public StagedAssetLink fetchExistingAssetLink(
-		long groupId, String assetEntry1Uuid, String assetEntry2Uuid) {
+			long groupId, String assetEntry1Uuid, String assetEntry2Uuid)
+		throws PortalException {
+
+		AssetEntry assetEntry1 = fetchAssetEntry(groupId, assetEntry1Uuid);
+		AssetEntry assetEntry2 = fetchAssetEntry(groupId, assetEntry2Uuid);
+
+		if ((assetEntry1 == null) || (assetEntry2 == null)) {
+			return null;
+		}
 
 		DynamicQuery dynamicQuery = getAssetLinkDynamicQuery(
-			0, groupId, assetEntry1Uuid, assetEntry2Uuid);
+			assetEntry1.getEntryId(), assetEntry2.getEntryId());
 
 		List<AssetLink> assetLinks = _assetLinkLocalService.dynamicQuery(
 			dynamicQuery);
@@ -187,6 +207,108 @@ public class StagedAssetLinkStagedModelRepository
 			assetLink, AssetLink.class, StagedAssetLink.class);
 	}
 
+	protected AssetEntry fetchAssetEntry(long groupId, String uuid)
+		throws PortalException {
+
+		DynamicQuery dynamicQuery = _assetEntryLocalService.dynamicQuery();
+
+		Property classUuidProperty = PropertyFactoryUtil.forName("classUuid");
+
+		dynamicQuery.add(classUuidProperty.eq(uuid));
+
+		List<AssetEntry> assetEntries = _assetEntryLocalService.dynamicQuery(
+			dynamicQuery);
+
+		if (ListUtil.isEmpty(assetEntries)) {
+			return null;
+		}
+
+		Map<Long, AssetEntry> assetEntryMap = new HashMap<>();
+
+		for (AssetEntry assetEntry : assetEntries) {
+			assetEntryMap.put(assetEntry.getGroupId(), assetEntry);
+		}
+
+		// Try to fetch the existing staged model from the importing group
+
+		if (assetEntryMap.containsKey(groupId)) {
+			return assetEntryMap.get(groupId);
+		}
+
+		// Try to fetch the existing staged model from parent sites
+
+		Group group = _groupLocalService.getGroup(groupId);
+
+		Group parentGroup = group.getParentGroup();
+
+		while (parentGroup != null) {
+			if (assetEntryMap.containsKey(parentGroup.getGroupId())) {
+				AssetEntry assetEntry = assetEntryMap.get(
+					parentGroup.getGroupId());
+
+				if (isAssetEntryApplicable(assetEntry)) {
+					return assetEntry;
+				}
+			}
+
+			parentGroup = parentGroup.getParentGroup();
+		}
+
+		// Try to fetch the existing staged model from the global site
+
+		Group companyGroup = _groupLocalService.fetchCompanyGroup(
+			group.getCompanyId());
+
+		if (assetEntryMap.containsKey(companyGroup.getGroupId())) {
+			return assetEntryMap.get(companyGroup.getGroupId());
+		}
+
+		// Try to fetch the existing staged model from the company
+
+		Stream<AssetEntry> assetEntryStream = assetEntries.stream();
+
+		List<AssetEntry> companyAssetEntries = assetEntryStream.filter(
+			entry -> entry.getCompanyId() == group.getCompanyId()
+		).collect(
+			Collectors.toList()
+		);
+
+		if (ListUtil.isEmpty(companyAssetEntries)) {
+			return null;
+		}
+
+		for (AssetEntry assetEntry : companyAssetEntries) {
+			try {
+				if (isAssetEntryApplicable(assetEntry)) {
+					return assetEntry;
+				}
+			}
+			catch (PortalException pe) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(pe, pe);
+				}
+			}
+		}
+
+		return null;
+	}
+
+	protected DynamicQuery getAssetLinkDynamicQuery(
+		long entryId1, long entryId2) {
+
+		DynamicQuery dynamicQuery = _assetLinkLocalService.dynamicQuery();
+
+		Property entryId1IdProperty = PropertyFactoryUtil.forName("entryId1");
+
+		dynamicQuery.add(entryId1IdProperty.eq(entryId1));
+
+		Property entryId2IdProperty = PropertyFactoryUtil.forName("entryId2");
+
+		dynamicQuery.add(entryId2IdProperty.eq(entryId2));
+
+		return dynamicQuery;
+	}
+
 	protected DynamicQuery getAssetLinkDynamicQuery(
 		long companyId, long groupId, String assetEntry1Uuid,
 		String assetEntry2Uuid) {
@@ -253,6 +375,47 @@ public class StagedAssetLinkStagedModelRepository
 		return dynamicQuery;
 	}
 
+	protected boolean isAssetEntryApplicable(AssetEntry assetEntry)
+		throws PortalException {
+
+		AssetRenderer<? extends StagedModel> assetRenderer = null;
+
+		StagedModel stagedModel = null;
+
+		try {
+			assetRenderer =
+				(AssetRenderer<? extends StagedModel>)
+					assetEntry.getAssetRenderer();
+
+			stagedModel = assetRenderer.getAssetObject();
+		}
+		catch (Exception e) {
+			return false;
+		}
+
+		if (stagedModel instanceof TrashedModel) {
+			TrashedModel trashedModel = (TrashedModel)stagedModel;
+
+			if (trashedModel.isInTrash()) {
+				return false;
+			}
+		}
+
+		if (stagedModel instanceof StagedGroupedModel) {
+			StagedGroupedModel stagedGroupedModel =
+				(StagedGroupedModel)stagedModel;
+
+			Group group = _groupLocalService.getGroup(
+				stagedGroupedModel.getGroupId());
+
+			if (group.isStagingGroup()) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
 	protected String parseAssetEntry1Uuid(String uuid) {
 		return uuid.substring(0, uuid.indexOf(StringPool.POUND));
 	}
@@ -275,7 +438,13 @@ public class StagedAssetLinkStagedModelRepository
 		_assetLinkLocalService = assetLinkLocalService;
 	}
 
+	private static final Log _log = LogFactoryUtil.getLog(
+		StagedAssetLinkStagedModelRepository.class);
+
 	private AssetEntryLocalService _assetEntryLocalService;
 	private AssetLinkLocalService _assetLinkLocalService;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
 
 }
