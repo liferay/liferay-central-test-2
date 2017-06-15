6291d1e1131c9e98340cf1570ef08c553f3ff314
Hugo Huijser <hugo.huijser@liferay.com>
2017-06-15 09:33:09 +0800

    LPS-72705 Sort capabilities/properties in *.bnd

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BNDCapabilityCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BNDCapabilityCheck.java
@@ -14,9 +14,15 @@
 
 package com.liferay.source.formatter.checks;
 
+import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.tools.ToolsUtil;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -29,15 +35,42 @@ public class BNDCapabilityCheck extends BaseFileCheck {
 	protected String doProcess(
 		String fileName, String absolutePath, String content) {
 
-		content = _formatCapability(content, "Provide-Capability");
-		content = _formatCapability(content, "Require-Capability");
+		content = _formatCapabilities(content, "Provide-Capability");
+		content = _formatCapabilities(content, "Require-Capability");
 
 		return content;
 	}
 
-	private String _formatCapability(String content, String definitionKey) {
+	private String _fixLineBreaks(
+		List<Capability> capabilitiesList, String definitionKey) {
+
+		StringBundler sb = new StringBundler();
+
+		sb.append(definitionKey);
+		sb.append(":\\\n");
+
+		for (Capability capability : capabilitiesList) {
+			sb.append("\t");
+			sb.append(capability.getName());
+			sb.append(";\\\n");
+
+			for (String property : capability.getProperties()) {
+				sb.append("\t\t");
+				sb.append(property);
+				sb.append(";\\\n");
+			}
+
+			sb.setStringAt(",\\\n", sb.index() - 1);
+		}
+
+		sb.setIndex(sb.index() - 1);
+
+		return sb.toString();
+	}
+
+	private String _formatCapabilities(String content, String definitionKey) {
 		Pattern pattern = Pattern.compile(
-			"^" + definitionKey + ":[\\s\\S]*?([^\\\\]\n|\\Z)",
+			"^" + definitionKey + ":([\\s\\S]*?([^\\\\]\n|\\Z))",
 			Pattern.MULTILINE);
 
 		Matcher matcher = pattern.matcher(content);
@@ -46,48 +79,107 @@ public class BNDCapabilityCheck extends BaseFileCheck {
 			return content;
 		}
 
-		String match = matcher.group();
+		String match = StringUtil.trim(matcher.group());
 
-		String replacement = StringUtil.replace(match, ": ", ":\\\n\t");
+		String capabilities = StringUtil.trim(matcher.group(1));
 
-		outerLoop:
-		while (true) {
-			matcher = _capabilityLineBreakPattern1.matcher(replacement);
+		String singleLineCapabilities = StringUtil.replace(
+			capabilities,
+			new String[] {StringPool.TAB, definitionKey + ":\\\n", "\\\n"},
+			new String[] {
+				StringPool.BLANK, definitionKey + ": ", StringPool.BLANK
+			});
 
-			while (matcher.find()) {
-				if (!ToolsUtil.isInsideQuotes(replacement, matcher.start())) {
-					replacement = StringUtil.replaceFirst(
-						replacement, ",", ",\\\n\t", matcher.start());
+		List<Capability> capabilitiesList = _sortCapabilities(
+			singleLineCapabilities);
 
-					continue outerLoop;
-				}
-			}
-
-			break;
-		}
-
-		outerLoop:
-		while (true) {
-			matcher = _capabilityLineBreakPattern2.matcher(replacement);
-
-			while (matcher.find()) {
-				if (!ToolsUtil.isInsideQuotes(replacement, matcher.start())) {
-					replacement = StringUtil.replaceFirst(
-						replacement, ";", ";\\\n\t\t", matcher.start());
-
-					continue outerLoop;
-				}
-			}
-
-			break;
-		}
+		String replacement = _fixLineBreaks(capabilitiesList, definitionKey);
 
 		return StringUtil.replace(content, match, replacement);
 	}
 
-	private final Pattern _capabilityLineBreakPattern1 = Pattern.compile(
-		",[^\\\\]");
-	private final Pattern _capabilityLineBreakPattern2 = Pattern.compile(
-		";[^\\\\]");
+	private List<Capability> _sortCapabilities(String capabilities) {
+		List<Capability> capabilitiesList = new ArrayList<>();
+
+		Capability capability = null;
+
+		StringBundler sb = new StringBundler();
+
+		for (int i = 0; i < capabilities.length(); i++) {
+			char c = capabilities.charAt(i);
+
+			if (ToolsUtil.isInsideQuotes(capabilities, i)) {
+				sb.append(c);
+
+				continue;
+			}
+
+			if (c == CharPool.COMMA) {
+				capability.addProperty(sb.toString());
+
+				capability.sortProperties();
+
+				capabilitiesList.add(capability);
+
+				capability = null;
+				sb.setIndex(0);
+			}
+			else if (c == CharPool.SEMICOLON) {
+				if (capability == null) {
+					capability = new Capability(sb.toString());
+				}
+				else {
+					capability.addProperty(sb.toString());
+				}
+
+				sb.setIndex(0);
+			}
+			else {
+				sb.append(c);
+			}
+		}
+
+		capability.addProperty(sb.toString());
+
+		capability.sortProperties();
+
+		capabilitiesList.add(capability);
+
+		Collections.sort(capabilitiesList);
+
+		return capabilitiesList;
+	}
+
+	private class Capability implements Comparable<Capability> {
+
+		public Capability(String name) {
+			_name = name;
+		}
+
+		@Override
+		public int compareTo(Capability capability) {
+			return _name.compareTo(capability.getName());
+		}
+
+		protected void addProperty(String property) {
+			_properties.add(property);
+		}
+
+		protected String getName() {
+			return _name;
+		}
+
+		protected List<String> getProperties() {
+			return _properties;
+		}
+
+		protected void sortProperties() {
+			Collections.sort(_properties);
+		}
+
+		private final String _name;
+		private final List<String> _properties = new ArrayList<>();
+
+	}
 
 }
