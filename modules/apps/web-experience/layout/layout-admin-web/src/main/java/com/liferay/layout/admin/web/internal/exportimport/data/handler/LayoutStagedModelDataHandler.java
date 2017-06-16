6ca56787d99207814b49a88598d8b54054ca25bf
akosthurzo <akos.thurzo@liferay.com>
2017-06-16 12:23:03 +0200

    LPS-72893 Reflect id change in the importedLayout

--- modules/apps/web-experience/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/exportimport/data/handler/LayoutStagedModelDataHandler.java
+++ modules/apps/web-experience/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/exportimport/data/handler/LayoutStagedModelDataHandler.java
@@ -652,6 +652,8 @@ public class LayoutStagedModelDataHandler
 		}
 		else if (importedLayout.getIconImageId() > 0) {
 			_imageLocalService.deleteImage(importedLayout.getIconImageId());
+
+			importedLayout.setIconImageId(0);
 		}
 
 		if (existingLayout == null) {
