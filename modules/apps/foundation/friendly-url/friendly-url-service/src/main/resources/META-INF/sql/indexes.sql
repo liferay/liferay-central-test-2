b65f2fa10b7f868b2e8c256758fda968a93a44de
Brian Chan <brian.chan@liferay.com>
2017-06-20 12:07:20 -0700

    LPS-73135 Fix regen

--- modules/apps/foundation/friendly-url/friendly-url-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/foundation/friendly-url/friendly-url-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,11 +1,8 @@
 create index IX_88D05B8 on FriendlyURLEntry (groupId, classNameId, classPK, main);
 create index IX_E47CFA8 on FriendlyURLEntry (groupId, classNameId, classPK, urlTitle[$COLUMN_LENGTH:255$]);
 create index IX_2E40C535 on FriendlyURLEntry (groupId, classNameId, urlTitle[$COLUMN_LENGTH:255$]);
-create index IX_23FDB0B4 on FriendlyURLEntry (groupId, companyId, classNameId, classPK, main);
-create index IX_5B329CA4 on FriendlyURLEntry (groupId, companyId, classNameId, classPK, urlTitle[$COLUMN_LENGTH:255$]);
-create index IX_B5982CB9 on FriendlyURLEntry (groupId, companyId, classNameId, urlTitle[$COLUMN_LENGTH:255$]);
 create index IX_20861768 on FriendlyURLEntry (uuid_[$COLUMN_LENGTH:75$], companyId);
-create unique index IX_1C7E10E0 on FriendlyURLEntry (uuid_[$COLUMN_LENGTH:75$], groupId);
+create unique index IX_63FD57EA on FriendlyURLEntry (uuid_[$COLUMN_LENGTH:75$], groupId);
 
 create unique index IX_33F49821 on FriendlyURLEntryLocalization (groupId, friendlyURLEntryId, languageId[$COLUMN_LENGTH:75$]);
 create unique index IX_C540141 on FriendlyURLEntryLocalization (groupId, urlTitle[$COLUMN_LENGTH:255$], languageId[$COLUMN_LENGTH:75$]);
