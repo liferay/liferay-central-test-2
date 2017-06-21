5fe7f400d9376aab10fb4ea7f2d5e6087cb9ee64
shuyangzhou <shuyang.zhou@liferay.com>
2017-06-21 16:23:49 -0700

    LPS-69008 Rename files for clarity

--- modules/apps/foundation/portal/portal-cacheutil-request-parameter-validators/src/main/java/com/liferay/portal/cacheutil/request/parameter/validators/ThemeIdCacheFileNameContributor.java
+++ modules/apps/foundation/portal/portal-cacheutil-request-parameter-validators/src/main/java/com/liferay/portal/cacheutil/request/parameter/validators/ThemeIdCacheFileNameContributor.java
@@ -14,38 +14,44 @@
 
 package com.liferay.portal.cacheutil.request.parameter.validators;
 
-import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Theme;
+import com.liferay.portal.kernel.service.ThemeLocalService;
 import com.liferay.portal.kernel.util.Function;
 import com.liferay.portal.kernel.util.KeyValuePair;
-import com.liferay.portal.kernel.util.LocaleUtil;
-
-import java.util.Locale;
-import java.util.Set;
+import com.liferay.portal.kernel.util.Portal;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Carlos Sierra Andrés
  */
 @Component(immediate = true, property = "cache.file.name.contributor=true")
-public class LanguageIdRequestParameterCacheValidator
+public class ThemeIdCacheFileNameContributor
 	implements Function<HttpServletRequest, KeyValuePair> {
 
 	@Override
 	public KeyValuePair apply(HttpServletRequest request) {
-		String languageId = request.getParameter(_PARAMETER_NAME);
+		long companyId = _portal.getCompanyId(request);
+		String themeId = request.getParameter(_PARAMETER_NAME);
 
-		Set<Locale> availableLocales = LanguageUtil.getAvailableLocales();
+		Theme theme = _themeLocalService.fetchTheme(companyId, themeId);
 
-		if (availableLocales.contains(LocaleUtil.fromLanguageId(languageId))) {
-			return new KeyValuePair(_PARAMETER_NAME, languageId);
+		if (theme != null) {
+			return new KeyValuePair(_PARAMETER_NAME, themeId);
 		}
 
 		return null;
 	}
 
-	private static final String _PARAMETER_NAME = "languageId";
+	private static final String _PARAMETER_NAME = "themeId";
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private ThemeLocalService _themeLocalService;
 
 }
