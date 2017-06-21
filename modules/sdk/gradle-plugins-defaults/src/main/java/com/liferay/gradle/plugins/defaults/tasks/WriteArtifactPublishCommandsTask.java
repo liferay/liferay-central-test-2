3fe48006f24408e730cff72d6e079dac935d5a48
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-06-21 14:34:19 -0700

    LPS-73235 Remove publishing of module snapshots

--- modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/tasks/WriteArtifactPublishCommandsTask.java
+++ modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/tasks/WriteArtifactPublishCommandsTask.java
@@ -256,13 +256,6 @@ public class WriteArtifactPublishCommandsTask extends DefaultTask {
 	private void _addPublishCommands(
 		List<String> commands, boolean firstPublish) {
 
-		// Publish snapshot
-
-		commands.add(
-			_getGradleCommand(
-				BasePlugin.UPLOAD_ARCHIVES_TASK_NAME,
-				"-P" + GradleUtil.SNAPSHOT_PROPERTY_NAME));
-
 		// Publish release
 
 		String[] arguments = new String[0];
