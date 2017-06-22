736d7876cd4c9f9a55c5750e2aa993125098b556
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-06-22 19:40:23 +0200

    LPS-72246 It should be in an internal package

---
+++ modules/apps/web-experience/journal/journal-web/src/main/java/com/liferay/journal/web/internal/dynamic/data/mapping/util/JournalSelectStructureRestrictionsDDMDisplay.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.web.internal.dynamic.data.mapping.util;
+
+import com.liferay.dynamic.data.mapping.util.DDMDisplay;
+import com.liferay.journal.constants.JournalPortletKeys;
+import com.liferay.journal.web.dynamic.data.mapping.util.JournalDDMDisplay;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.util.Portal;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	property = {"javax.portlet.name=" + JournalPortletKeys.JOURNAL + ".selectStructureRestriction"},
+	service = DDMDisplay.class
+)
+public class JournalSelectStructureRestrictionsDDMDisplay
+	extends JournalDDMDisplay {
+
+	@Override
+	public String getPortletId() {
+		return JournalPortletKeys.JOURNAL + ".selectStructureRestriction";
+	}
+
+	@Override
+	public boolean isShowAddButton(Group scopeGroup) {
+		return false;
+	}
+
+	@Override
+	public boolean isShowConfirmSelectStructure() {
+		return false;
+	}
+
+	@Reference(unbind = "-")
+	protected void setPortal(Portal portal) {
+		this.portal = portal;
+	}
+
+}
