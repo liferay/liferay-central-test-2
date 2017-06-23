e370ebb0e7e0bbdf0d422bc98315eae7c0a3c5ac
Brian Chan <brian.chan@liferay.com>
2017-06-23 14:19:49 -0700

    LPS-73201 SF

--- modules/apps/foundation/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/portlet/action/ExportUsersMVCResourceCommand.java
+++ modules/apps/foundation/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/portlet/action/ExportUsersMVCResourceCommand.java
@@ -120,10 +120,7 @@ public class ExportUsersMVCResourceCommand extends BaseMVCResourceCommand {
 		for (int i = 0; i < PropsValues.USERS_EXPORT_CSV_FIELDS.length; i++) {
 			String field = PropsValues.USERS_EXPORT_CSV_FIELDS[i];
 
-			if (field.equals("fullName")) {
-				sb.append(CSVUtil.encode(user.getFullName()));
-			}
-			else if (field.contains("Date")) {
+			if (field.contains("Date")) {
 				Date date = (Date)BeanPropertiesUtil.getObject(user, field);
 
 				sb.append(CSVUtil.encode(String.valueOf(date)));
@@ -135,6 +132,9 @@ public class ExportUsersMVCResourceCommand extends BaseMVCResourceCommand {
 
 				sb.append(
 					CSVUtil.encode(expandoBridge.getAttribute(attributeName)));
+			}
+			else if (field.equals("fullName")) {
+				sb.append(CSVUtil.encode(user.getFullName()));
 			}
 			else {
 				sb.append(
